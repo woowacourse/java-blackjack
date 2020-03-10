15ca58d74cbd3b4f7af2816a9220185c183cd96d
kuenhwi <kueniyam@naver.com>
2020-03-10 16:41:01 +0900

    feat: 자동차들이 필요한 연료량을 출력하는 기능

--- src/main/java/rentCompany/RentCompany.java
+++ src/main/java/rentCompany/RentCompany.java
@@ -1,18 +1,37 @@
 package rentCompany;
 
+import rentCompany.car.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RentCompany {
+    private static final String NEWLINE = System.getProperty("line.separator");
+
+    private List<Car> cars;
+
     private RentCompany() {
+        cars = new ArrayList<>();
     }
 
     public static RentCompany create() {
         return new RentCompany();
     }
 
-    public void addCar(Object car) {
-
+    public void addCar(Car car) {
+        cars.add(car);
     }
 
     public String generateReport() {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Car car : cars) {
+            stringBuilder.append(car.getName())
+                    .append(" : ")
+                    .append(car.getChargeQuantity())
+                    .append("리터")
+                    .append(NEWLINE);
+        }
+
+        return stringBuilder.toString();
     }
 }
--- src/test/java/rentCompany/car/CarTest.java
+++ src/test/java/rentCompany/car/CarTest.java
@@ -13,4 +13,15 @@ public class CarTest {
 		assertThat(new Avante(350)).isNotNull();
 		assertThat(new K5(350)).isNotNull();
 	}
+
+	@DisplayName("자동차의 연료량을 구하는 기능")
+	@Test
+	void getChargeQuantity() {
+		assertThat(new Sonata(350).getChargeQuantity())
+				.isEqualTo(35);
+		assertThat(new Avante(150).getChargeQuantity())
+				.isEqualTo(10);
+		assertThat(new K5(400).getChargeQuantity())
+				.isEqualTo(20);
+	}
 }
