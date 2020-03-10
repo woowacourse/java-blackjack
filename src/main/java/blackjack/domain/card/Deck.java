42b2694c6020bf566e33b256425a3517033e03e3
YebinK <yebin825@gmail.com>
2020-03-10 18:05:55 +0900

    [feature] Deck 객체 구현

---
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -0,0 +1,26 @@
+package blackjack.domain.card;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class Deck {
+
+    private final Stack<Card> cards = new Stack<>();
+
+    public Deck(List<Card> cards) {
+
+        Collections.shuffle(cards);
+        this.cards.addAll(cards);
+    }
+
+    public Card pick() {
+
+        if (cards.empty()) {
+            throw new NullPointerException("카드를 모두 사용하셨습니다.");
+        }
+        return cards.pop();
+    }
+
+
+}
--- src/test/java/blackjack/domain/DeckTest.java
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -1,5 +1,22 @@
 package blackjack.domain;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardFactory;
+import blackjack.domain.card.Deck;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class DeckTest {
 
+    @Test
+    @DisplayName("카드 꺼내기")
+    void pick() {
+        List<Card> cards = CardFactory.generate();
+        Deck deck = new Deck(cards);
+        assertThat(deck.pick()).isInstanceOf(Card.class);
+    }
 }
