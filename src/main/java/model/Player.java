ffcd52425d57e525aa70ae82ec2106474bbd7e4a
taeheon <dqrd123@gmail.com>
2020-03-10 15:29:23 +0900

    feat: Player 구현

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
