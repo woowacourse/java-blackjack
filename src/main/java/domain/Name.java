49c8d516fc585fe73392b364cfd4c74ae2b07520
hamliet <hamliet@hanyang.ac.kr>
2020-03-10 14:11:27 +0900

    feat: Name 클래스 생성

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Name {
+    String name;
+    public Name(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("이름이 공백입니다.");
+        }
+    }
+}
--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,16 +1,9 @@
 package domain;
 
 public class Player {
-	private String name;
+	private Name name;
 
 	public Player(String name) {
-		validateName(name);
-		this.name = name;
-	}
-
-	private void validateName(String name) {
-		if (name == null || name.isEmpty()) {
-			throw new IllegalArgumentException("이름이 공백입니다.");
-		}
+		this.name = new Name(name);
 	}
 }
