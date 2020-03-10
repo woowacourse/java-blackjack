ddec21d2604f0defc5dffa8e20d8dbc52f9a4b52
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-03-10 15:51:18 +0900

    [feat] Symbol과 Type을 가진 Card객체 생성

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Card {
+    private Type type;
+    private Symbol symbol;
+
+    public Card(Type type, Symbol symbol) {
+        this.type = type;
+        this.symbol = symbol;
+    }
+}
---
+++ src/main/java/domain/Type.java
@@ -0,0 +1,13 @@
+package domain;
+
+public enum Type {
+    DIAMOND("다이아몬드"),
+    HEART("하트"),
+    SPADE("스페이드"),
+    CLUB("클로바");
+
+    private final String name;
+    Type(String name) {
+        this.name = name;
+    }
+}
