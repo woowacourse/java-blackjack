853ab02a9bf79c92d374e0a726ab3def8b8f361c
김예림 <kiel0103@naver.com>
2020-03-10 20:24:33 +0900

    feat : 플레이어의 이름 저장할 Name 클래스 구현

---
+++ src/main/java/domain/user/Name.java
@@ -0,0 +1,12 @@
+package domain.user;
+
+public class Name {
+    private String name;
+
+    Name(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new NullPointerException("이름이 비어있습니다.");
+        }
+        this.name = name;
+    }
+}
