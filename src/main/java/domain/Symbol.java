4571af8b0f9114b42bcdc836135b9156df2f1f87
hamliet <hamliet@hanyang.ac.kr>
2020-03-10 14:54:42 +0900

    feat: Symbol 클래스 추가

---
+++ src/main/java/domain/Symbol.java
@@ -0,0 +1,46 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum Symbol {
+    ACE(1, "A"),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10, "J"),
+    QUEEN(10, "Q"),
+    KING(10, "K");
+
+    private int point;
+    private String alias;
+
+    Symbol(int point) {
+        this.point = point;
+        this.alias = String.valueOf(point);
+    }
+
+    Symbol(int point, String alias) {
+        this.point = point;
+        this.alias = alias;
+    }
+
+    public static Symbol of(int point) {
+        return Arrays.stream(values())
+                .filter(type -> type.point == point)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 심볼입니다."));
+    }
+
+    public static Symbol of(String alias) {
+        return Arrays.stream(values())
+                .filter(type -> type.alias == alias)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 심볼입니다."));
+    }
+}
