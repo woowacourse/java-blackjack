cecba0a15482bc17275edcad738f09c8a954964c
hamliet <hamliet@hanyang.ac.kr>
2020-03-10 17:25:01 +0900

    feat: User 객체에 isWin 함수 추가

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,35 +1,39 @@
 package domain;
 
 public class User {
-	private Name name;
-	private Cards cards;
+    private Name name;
+    private Cards cards;
 
-	public User(String name) {
-		this.name = new Name(name);
-		this.cards = new Cards();
-	}
+    public User(String name) {
+        this.name = new Name(name);
+        this.cards = new Cards();
+    }
 
-	public void addCard(Card card) {
-		cards.addCard(card);
-	}
+    public void addCard(Card card) {
+        cards.addCard(card);
+    }
 
-	public String getName() {
-		return name.getValue();
-	}
+    public String getName() {
+        return name.getValue();
+    }
 
-	public int getScore() {
-		return cards.getScore();
-	}
+    public int getScore() {
+        return cards.getScore();
+    }
 
-	public int getCardSize() {
-		return cards.getSize();
-	}
+    public int getCardSize() {
+        return cards.getSize();
+    }
 
-	public boolean isBlackJack() {
-		return cards.isBlackJack();
-	}
+    public boolean isBlackJack() {
+        return cards.isBlackJack();
+    }
 
-	public boolean isBust() {
-		return cards.isBust();
-	}
+    public boolean isBust() {
+        return cards.isBust();
+    }
+
+    public boolean isWin(User that) {
+        return  !(isBust()) && (this.getScore() >= that.getScore());
+    }
 }
