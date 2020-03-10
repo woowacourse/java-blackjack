8ca8650ed12208d8f816821bed1baab95dc66038
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-03-10 14:44:27 +0900

    feat : RentCompany내부에 정적 팩토리 메서드 create() 추가

--- src/main/java/rentacar/RentCompany.java
+++ src/main/java/rentacar/RentCompany.java
@@ -1,4 +1,26 @@
 package rentacar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RentCompany {
+
+    private List<Car> cars;
+
+    public RentCompany(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static RentCompany create() {
+        List<Car> cars = new ArrayList<>();
+        return new RentCompany(cars);
+    }
+
+    public void addCar(Car car){
+        return;
+    }
+
+    public String generateReport() {
+        return null;
+    }
 }
