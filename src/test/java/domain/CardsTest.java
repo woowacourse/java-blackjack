c2a4a9055b51a13e3cd865bd00c989578dcdd975
hamliet <hamliet@hanyang.ac.kr>
2020-03-10 15:55:42 +0900

    feat: Cards 테스트코드 추가

--- src/test/java/domain/CardsTest.java
+++ src/test/java/domain/CardsTest.java
@@ -1,13 +1,40 @@
 package domain;
 
-import factory.CardFactory;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CardsTest {
     @Test
-    void create(){
+    void create() {
         assertThat(new Cards()).isInstanceOf(Cards.class);
+    }
+
+    @Test
+    void getScore() {
+        Cards cards = new Cards();
+        cards.addCard(Card.of("스페이드", "K"));
+        cards.addCard(Card.of("스페이드", "3"));
+        cards.addCard(Card.of("스페이드", "A"));
+
+        assertThat(cards.getScore()).isEqualTo(14);
+    }
+
+    @Test
+    void isBlackJack() {
+        Cards cards = new Cards();
+        cards.addCard(Card.of("스페이드", "K"));
+        cards.addCard(Card.of("스페이드", "A"));
+
+        assertThat(cards.isBlackJack()).isTrue();
+    }
+
+    @Test
+    void isLessThan() {
+        Cards cards = new Cards();
+        cards.addCard(Card.of("스페이드", "K"));
+        cards.addCard(Card.of("스페이드", "A"));
+
+        assertThat(cards.isLessThan(20)).isFalse();
     }
 }
