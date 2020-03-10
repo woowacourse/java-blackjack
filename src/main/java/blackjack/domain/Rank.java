4d06c45a3d688fdf73f9a8a24e86039855db5f45
Woonjang Ahn <woonjangahn@gmail.com>
2020-03-10 19:57:43 +0900

    feat : 카드 생성을 위한 enum 작성

--- src/main/java/blackjack/domain/Rank.java
+++ src/main/java/blackjack/domain/Rank.java
@@ -1,4 +1,23 @@
 package blackjack.domain;
 
 public enum Rank {
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10),
+    ACE(11);
+
+    private final int value;
+
+    Rank(final int value) {
+        this.value = value;
+    }
 }
