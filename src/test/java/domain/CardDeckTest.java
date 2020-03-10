cc3c3bbe743955f74e74f5f262c9943f9c300d98
hamliet <hamliet@hanyang.ac.kr>
2020-03-10 16:05:23 +0900

    feat: CardDeck 테스트코드 추가

---
+++ src/test/java/domain/CardDeckTest.java
@@ -0,0 +1,19 @@
+package domain;
+
+import factory.CardFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CardDeckTest {
+    @Test
+    void create() {
+        assertThat(new CardDeck(CardFactory.create())).isInstanceOf(CardDeck.class);
+    }
+
+    @Test
+    void drawOne() {
+        CardDeck cardDeck = new CardDeck(CardFactory.create());
+        assertThat(cardDeck.drawOne()).isInstanceOf(Card.class);
+    }
+}
