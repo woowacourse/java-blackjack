c349eff828a0687ed0361abf3bf6e6a1caf91e09
chanin <pci2676@gmail.com>
2020-03-10 16:11:42 +0900

    feat : 이름 입력받아서 list 로 반환하는 기능

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,12 +1,22 @@
 package blackjack.view;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
-	private static final Scanner scanner = new Scanner(System.in);
+    private static final String DELIMITER = ",";
+    private final Scanner scanner;
 
-	public static List<String> inputPlayNames() {
-		return null;
-	}
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public List<String> inputPlayNames() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        String inputNames = this.scanner.nextLine();
+        return Arrays.stream(inputNames.split(DELIMITER))
+                .collect(Collectors.toList());
+    }
 }
--- src/test/java/blackjack/view/InputViewTest.java
+++ src/test/java/blackjack/view/InputViewTest.java
@@ -3,10 +3,29 @@ package blackjack.view;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class InputViewTest {
-	@DisplayName("이름 입력 테스트")
-	@Test
-	void inputPlayNames() {
-		InputView.inputPlayNames();
-	}
+    @DisplayName("이름 입력 테스트")
+    @Test
+    void inputPlayNames() {
+        //given
+        String input = "allen,bebop";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        Scanner scanner = new Scanner(inputStream);
+
+        InputView inputView = new InputView(scanner);
+
+        //when
+        List<String> names = inputView.inputPlayNames();
+
+        //then
+        assertThat(names).containsExactly("allen", "bebop");
+    }
+
 }
