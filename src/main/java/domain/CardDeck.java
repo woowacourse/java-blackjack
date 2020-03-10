d1f8eabfb9bf4addd6b2a736e384d052847f764d
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-03-10 16:20:26 +0900

    [feat] CardDeck 생성/셔플, draw 구현

---
+++ src/main/java/domain/CardDeck.java
@@ -0,0 +1,28 @@
+package domain;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CardDeck {
+
+    private static final String CARD_DECK_IS_EMPTY_EXCEPTION_MESSAGE = "카드덱에 카드가 남아있지 않습니다.";
+    private List<Card> cardDeck;
+
+    public CardDeck() {
+        cardDeck = new LinkedList<>();
+        for (Type type : Type.values()) {
+            for (Symbol symbol : Symbol.values()) {
+                cardDeck.add(new Card(type, symbol));
+            }
+        }
+        Collections.shuffle(cardDeck);
+    }
+
+    public Card draw() {
+        if (cardDeck.size() == 0) {
+            throw new IndexOutOfBoundsException(CARD_DECK_IS_EMPTY_EXCEPTION_MESSAGE);
+        }
+        return cardDeck.remove(cardDeck.size() - 1);
+    }
+}
---
+++ src/test/java/domain/CardDeckTest.java
@@ -0,0 +1,19 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CardDeckTest {
+    @DisplayName("개수 이상 드로우시 예외발생 테스트")
+    @Test
+    void cardDraw() {
+        CardDeck cardDeck = new CardDeck();
+        for (int i = 0; i < 52; i++) {
+            cardDeck.draw();
+        }
+        assertThatThrownBy(() -> cardDeck.draw())
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+}
