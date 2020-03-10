a930930a2fc55f01de08d4efdcbff6c497347003
chanin <pci2676@gmail.com>
2020-03-10 20:47:44 +0900

    refactor : 카드의 점수를 계산하는 로직
     - 점수 계산 로직 수정
      - 기존에 ACE가 없어도 ACE_WEIGHT 를 더하던 버그를 발견하여 수정

--- src/main/java/blackjack/player/card/CardBundle.java
+++ src/main/java/blackjack/player/card/CardBundle.java
@@ -5,50 +5,53 @@ import java.util.List;
 import java.util.Objects;
 
 public class CardBundle {
-	private static final int ACE_WEIGHT = 10;
-	private static final int BLACKJACK_MAXIMUM_VALUE = 21;
-	private final List<Card> cards = new ArrayList<>();
+    private static final int ACE_WEIGHT = 10;
+    private static final int BLACKJACK_MAXIMUM_VALUE = 21;
+    private final List<Card> cards = new ArrayList<>();
 
-	public void addCard(Card card) {
-		cards.add(card);
-	}
+    public void addCard(Card card) {
+        cards.add(card);
+    }
 
-	public int compare(CardBundle cardBundle) {
-		return Integer.compare(this.calculateScore(), cardBundle.calculateScore());
-	}
+    public int compare(CardBundle cardBundle) {
+        return Integer.compare(this.calculateScore(), cardBundle.calculateScore());
+    }
 
-	public int calculateScore() {
-		int result = cards.stream()
-			.mapToInt(Card::getScore)
-			.sum();
+    public int calculateScore() {
+        int result = cards.stream()
+                .mapToInt(Card::getScore)
+                .sum();
 
-		if (hasAce() && isBurst(result + ACE_WEIGHT)) {
-			return result;
-		}
-		return result + ACE_WEIGHT;
-	}
+        if (hasAce() && isBurst(result + ACE_WEIGHT)) {
+            return result;
+        }
+        if (hasAce()) {
+            return result + ACE_WEIGHT;
+        }
+        return result;
+    }
 
-	private boolean hasAce() {
-		return cards.stream()
-			.anyMatch(Card::isAce);
-	}
+    private boolean hasAce() {
+        return cards.stream()
+                .anyMatch(Card::isAce);
+    }
 
-	private boolean isBurst(int result) {
-		return result > BLACKJACK_MAXIMUM_VALUE;
-	}
+    private boolean isBurst(int result) {
+        return result > BLACKJACK_MAXIMUM_VALUE;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		CardBundle that = (CardBundle)o;
-		return Objects.equals(cards, that.cards);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        CardBundle that = (CardBundle) o;
+        return Objects.equals(cards, that.cards);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(cards);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(cards);
+    }
 }
