6ad5ffa5cc48daab73c83b2a8c75ec91c3f517c7
lowoon <sangbo0229@gmail.com>
2020-03-11 11:16:16 +0900

    feat: Symbol 클래스와 getName 메서드 구현

---
+++ src/main/java/domain/Symbol.java
@@ -0,0 +1,19 @@
+package domain;
+
+public enum Symbol {
+
+    SPADE("스페이드"),
+    CLOVER("클로버"),
+    DIAMOND("다이아몬드"),
+    HEARD("하트");
+
+    private final String name;
+
+    Symbol(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
