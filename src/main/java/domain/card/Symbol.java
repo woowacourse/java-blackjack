8496dd74fecc4e20ff3d68eabb3ad2761e1b8fce
toneyparky <qkrtmddhks95@gmail.com>
2020-03-11 13:42:01 +0900

    refactor: add score value

--- src/main/java/domain/card/Symbol.java
+++ src/main/java/domain/card/Symbol.java
@@ -9,17 +9,24 @@ package domain.card;
  * 날짜 : 2020/03/11
  */
 public enum Symbol {
-	ACE,
-	TWO,
-	THREE,
-	FOUR,
-	FIVE,
-	SIX,
-	SEVEN,
-	EIGHT,
-	NINE,
-	TEN,
-	QUEEN,
-	JACK,
-	KING
+	ACE(11),
+	TWO(2),
+	THREE(3),
+	FOUR(4),
+	FIVE(5),
+	SIX(6),
+	SEVEN(7),
+	EIGHT(8),
+	NINE(9),
+	TEN(10),
+	QUEEN(10),
+	JACK(10),
+	KING(10);
+
+	private final int score;
+
+	Symbol(int score) {
+		this.score = score;
+	}
+
 }
