a19da2f6bb2417c119cc6548a32cf974d73ec63c
hamliet <hamliet@hanyang.ac.kr>
2020-03-12 06:19:20 +0900

    feat: CardFactoryTest 테스트케이스 추가

--- src/test/java/factory/CardFactoryTest.java
+++ src/test/java/factory/CardFactoryTest.java
@@ -3,12 +3,15 @@ package factory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CardFactoryTest {
     @Test
-    @DisplayName("CardFactory 생성")
+    @DisplayName("CardFactory 기능 확인")
     void create() {
+        assertThat(CardFactory.create()).isInstanceOf(List.class);
         assertThat(CardFactory.create().size()).isEqualTo(52);
     }
 }
