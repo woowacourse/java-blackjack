66ce0b0533510d0a2d391009ba75fdb827ff9f11
lowoon <sangbo0229@gmail.com>
2020-03-11 16:36:16 +0900

    refactor: 테스트 코드 분리

--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -5,10 +5,6 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import domain.deck.Card;
-import domain.deck.Symbol;
-import domain.deck.Type;
-
 class PlayerTest {
 
     @Test
@@ -16,29 +12,5 @@ class PlayerTest {
     void create() {
         assertThatCode(() -> new Player("이름"))
                 .doesNotThrowAnyException();
-    }
-
-    @Test
-    @DisplayName("빈 이름이 있는 경우 예외 처리")
-    void createPlayerWithEmptyName() {
-        assertThatThrownBy(() -> new Player(""))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("빈 이름이 있습니다.");
-    }
-
-    @Test
-    void getName() {
-        Player player = new Player("이름");
-        assertThat(player.getName()).isEqualTo("이름");
-    }
-
-    @Test
-    void draw() {
-        Player player = new Player("이름");
-        int initSize = player.cards.size();
-
-        player.draw(new Card(Symbol.CLOVER, Type.ACE));
-
-        assertThat(player.cards.size()).isEqualTo(initSize + 1);
     }
 }
---
+++ src/test/java/domain/user/UserTest.java
@@ -0,0 +1,37 @@
+package domain.user;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import domain.deck.Card;
+import domain.deck.Symbol;
+import domain.deck.Type;
+
+class UserTest {
+
+    @Test
+    @DisplayName("빈 이름이 있는 경우 예외 처리")
+    void createPlayerWithEmptyName() {
+        assertThatThrownBy(() -> new Player(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 이름이 있습니다.");
+    }
+
+    @Test
+    void getName() {
+        User user = new Player("이름");
+        assertThat(user.getName()).isEqualTo("이름");
+    }
+
+    @Test
+    void draw() {
+        User user = new Player("이름");
+        int initSize = user.cards.size();
+
+        user.draw(new Card(Symbol.CLOVER, Type.ACE));
+
+        assertThat(user.cards.size()).isEqualTo(initSize + 1);
+    }
+}
