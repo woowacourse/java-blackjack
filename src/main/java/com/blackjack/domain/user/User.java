240c9e3e32a7ce29b0df0434a103ba586b4c3ae8
begaonnuri <turtlenam0928@gmail.com>
2020-03-11 19:53:58 +0900

    feat: 플레이어에게 카드를 2장씩 나눠주는 기능 추가

--- src/main/java/com/blackjack/domain/user/User.java
+++ src/main/java/com/blackjack/domain/user/User.java
@@ -6,6 +6,8 @@ import com.blackjack.domain.card.Card;
 import com.blackjack.domain.card.CardDeck;
 
 public abstract class User {
+	private static final int FIRST_DRAW_COUNT = 2;
+
 	final Hands hands;
 
 	User() {
@@ -15,6 +17,12 @@ public abstract class User {
 	public void draw(CardDeck cardDeck) {
 		Card card = cardDeck.pop();
 		hands.add(card);
+	}
+
+	public void drawAtFirst(CardDeck cardDeck) {
+		for (int count = 0; count < FIRST_DRAW_COUNT; count++) {
+			draw(cardDeck);
+		}
 	}
 
 	public abstract boolean canDraw();
