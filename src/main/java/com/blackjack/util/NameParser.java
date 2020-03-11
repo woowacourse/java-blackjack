be7cfbd0cb3a3abd4245666004655687ea345a34
begaonnuri <turtlenam0928@gmail.com>
2020-03-11 14:37:08 +0900

    feat: NameParser 클래스 추가
    - 게임에 참여할 사람의 이름을 파싱하는 기능 추가

---
+++ src/main/java/com/blackjack/util/NameParser.java
@@ -0,0 +1,19 @@
+package com.blackjack.util;
+
+import static java.util.stream.Collectors.*;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import com.blackjack.domain.Name;
+
+public class NameParser {
+	private static final String DELIMITER = ",";
+
+	public static List<Name> parseName(String input) {
+		return Stream.of(input.split(DELIMITER))
+			.map(String::trim)
+			.map(Name::new)
+			.collect(toList());
+	}
+}
