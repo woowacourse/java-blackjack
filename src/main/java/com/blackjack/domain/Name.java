a9d8a22ac4c542dc8186c2e932e1be93eedb22bd
begaonnuri <turtlenam0928@gmail.com>
2020-03-11 14:23:00 +0900

    feat: Name 클래스 추가
    - Name 클래스 추가
    - 검증 기능 추가
    - 테스트 코드 추가

---
+++ src/main/java/com/blackjack/domain/Name.java
@@ -0,0 +1,35 @@
+package com.blackjack.domain;
+
+import java.util.Objects;
+
+public class Name {
+	private static final String BLANK_STRING = " ";
+	private static final int MAX_NAME_LENGTH = 10;
+
+	private final String name;
+
+	public Name(String name) {
+		validateNullOrEmpty(name);
+		validateBlank(name);
+		validateLength(name);
+		this.name = name;
+	}
+
+	private void validateLength(String name) {
+		if (name.length() >= MAX_NAME_LENGTH) {
+			throw new IllegalArgumentException("이름은 길이가 올바르지 않습니다.");
+		}
+	}
+
+	private void validateBlank(String name) {
+		if (name.contains(BLANK_STRING)) {
+			throw new IllegalArgumentException("이름에 공백이 포함될 수 없습니다.");
+		}
+	}
+
+	private void validateNullOrEmpty(String name) {
+		if (Objects.isNull(name) || name.isEmpty()) {
+			throw new IllegalArgumentException("이름은 null이거나 빈 문자열이 될 수 없습니다.");
+		}
+	}
+}
