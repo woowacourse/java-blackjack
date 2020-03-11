25fca987a892b0ef6b60d52e7b57d287d647c894
lowoon <sangbo0229@gmail.com>
2020-03-11 20:29:55 +0900

    feat: Dealer의 additionalDraw 메소드 구현

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -1,8 +1,11 @@
 package domain.user;
 
+import domain.deck.Deck;
+
 public class Dealer extends User {
 
     private static final String DEALER = "딜러";
+    private static final int PIVOT = 17;
 
     private Dealer() {
         super(DEALER);
@@ -15,5 +18,11 @@ public class Dealer extends User {
     @Override
     public String getFirstDrawResult() {
         return DEALER + "카드: " + cards.get(0).getName();
+    }
+
+    public void additionalDraw(Deck deck) {
+        if (calculatePoint() < PIVOT) {
+            optionalDraw(deck);
+        }
     }
 }
--- src/test/java/domain/user/DealerTest.java
+++ src/test/java/domain/user/DealerTest.java
@@ -2,14 +2,29 @@ package domain.user;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import domain.deck.Card;
+import domain.deck.DeckFactory;
 import domain.deck.Symbol;
 import domain.deck.Type;
 
 class DealerTest {
+    Dealer dealer;
+
+    private static Stream<Arguments> createOption() {
+        return Stream.of(
+                Arguments.of(new Card(Symbol.DIAMOND, Type.SIX), 1),
+                Arguments.of(new Card(Symbol.DIAMOND, Type.SEVEN), 0)
+        );
+    }
 
     @Test
     @DisplayName("생성 확인")
@@ -20,10 +35,27 @@ class DealerTest {
     @Test
     @DisplayName("첫 카드 분배 결과")
     void getFirstDrawResult() {
-        Dealer dealer = Dealer.appoint();
         dealer.draw(new Card(Symbol.CLOVER, Type.EIGHT));
         dealer.draw(new Card(Symbol.DIAMOND, Type.ACE));
 
         assertThat(dealer.getFirstDrawResult()).isEqualTo("딜러카드: 8클로버");
+    }
+
+    @BeforeEach
+    void setUp() {
+        dealer = Dealer.appoint();
+    }
+
+    @ParameterizedTest
+    @DisplayName("딜러 기준 포인트에 따른 드로우")
+    @MethodSource("createOption")
+    void additionalDraw(Card card, int expected) {
+        dealer.draw(new Card(Symbol.CLOVER, Type.TEN));
+        dealer.draw(card);
+        int initSize = dealer.cards.size();
+
+        dealer.additionalDraw(DeckFactory.getDeck());
+
+        assertThat(dealer.cards.size()).isEqualTo(initSize + expected);
     }
 }
