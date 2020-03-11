a30cc2615cbb9e856b60869920dfb37b8723587e
lowoon <sangbo0229@gmail.com>
2020-03-11 21:01:57 +0900

    feat: Player의 additionalDraw 메소드 구현

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -2,7 +2,9 @@ package domain.user;
 
 import java.util.stream.Collectors;
 
+import domain.YesOrNo;
 import domain.deck.Card;
+import domain.deck.Deck;
 
 public class Player extends User {
 
@@ -16,5 +18,11 @@ public class Player extends User {
                 + cards.stream()
                 .map(Card::getName)
                 .collect(Collectors.joining(", "));
+    }
+
+    public void additionalDraw(Deck deck, YesOrNo answer) {
+        if(answer.isYes()) {
+            optionalDraw(deck);
+        }
     }
 }
--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -2,14 +2,28 @@ package domain.user;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import domain.YesOrNo;
 import domain.deck.Card;
+import domain.deck.DeckFactory;
 import domain.deck.Symbol;
 import domain.deck.Type;
 
 class PlayerTest {
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player("이름");
+    }
 
     @Test
     @DisplayName("생성 확인")
@@ -21,10 +35,29 @@ class PlayerTest {
     @Test
     @DisplayName("첫 카드 분배 결과")
     void getFirstDrawResult() {
-        Player player = new Player("이름");
         player.draw(new Card(Symbol.CLOVER, Type.EIGHT));
         player.draw(new Card(Symbol.DIAMOND, Type.ACE));
 
         assertThat(player.getFirstDrawResult()).isEqualTo("이름카드: 8클로버, A다이아몬드");
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("딜러 기준 포인트에 따른 드로우")
+    @MethodSource("createOption")
+    void additionalDraw(YesOrNo input, int expected) {
+        player.draw(new Card(Symbol.CLOVER, Type.TEN));
+        int initSize = player.cards.size();
+
+        player.additionalDraw(DeckFactory.getDeck(), input);
+
+        assertThat(player.cards.size()).isEqualTo(initSize + expected);
+    }
+
+    private static Stream<Arguments> createOption() {
+        return Stream.of(
+                Arguments.of(YesOrNo.of("y"), 1),
+                Arguments.of(YesOrNo.of("n"), 0)
+        );
     }
 }
