d4923360d48a273903b921783ac8bbe2b8cad2ed
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-03-11 15:23:08 +0900

    [feat] 플레이어 승무패 결과 산정 기능 추가

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,16 +1,40 @@
 package domain;
 
+import util.BlackJackRule;
+
 public class Player extends User {
 
     private final String name;
+    private WinningResult winningResult;
 
     public Player(String name) {
         this.name = name;
+        this.winningResult = WinningResult.UNDEFINED;
         cards = new Cards();
     }
 
     @Override
     public boolean isAbleDrawCards() {
-        return !(cards.isBlackJack() || cards.isBust());
+        if (cards.isBust()) {
+            winningResult = WinningResult.LOSE;
+            return false;
+        }
+        if (cards.isBlackJack()) {
+            return false;
+        }
+        return true;
+    }
+
+    public WinningResult getWinningResult() {
+        return winningResult;
+    }
+
+    public void calculateWinningResult(int dealerScore) {
+        if (BlackJackRule.isBust(dealerScore) && winningResult == WinningResult.UNDEFINED) {
+            winningResult = WinningResult.WIN;
+        }
+        if (winningResult == WinningResult.UNDEFINED) {
+            winningResult = WinningResult.calculate(dealerScore, cards.getScore());
+        }
     }
 }
--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -5,18 +5,55 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
-
     @Test
     @DisplayName("드로우 테스트")
     void isAbleDrawCard() {
+        CardDeck cardDeck = new CardDeck();
         Player player = new Player("jamie");
-        player.drawCard();
+        player.drawCard(cardDeck);
         Assertions.assertThat(player.isAbleDrawCards())
                 .isTrue();
         for (int i = 0; i < 12; i++) {
-            player.drawCard();
+            player.drawCard(cardDeck);
         }
         Assertions.assertThat(player.isAbleDrawCards())
                 .isFalse();
+    }
+
+    @Test
+    @DisplayName("Bust 패배 테스트")
+    void isBustEqualLose() {
+        CardDeck cardDeck = new CardDeck();
+        Player player = new Player("하비");
+        player.isAbleDrawCards();
+        Assertions.assertThat(player.getWinningResult()).isEqualTo(WinningResult.UNDEFINED);
+
+        for (int i = 0; i < 12; i++) {
+            player.drawCard(cardDeck);
+        }
+        player.isAbleDrawCards();
+        Assertions.assertThat(player.getWinningResult()).isEqualTo(WinningResult.LOSE);
+    }
+
+    @Test
+    @DisplayName("승무패 결과 산정 테스트 - 패배")
+    void calculateWinningResult_lose() {
+        CardDeck cardDeck = new CardDeck();
+        Player player = new Player("제이미");
+        player.drawCard(cardDeck);
+        player.calculateWinningResult(17);
+        Assertions.assertThat(player.getWinningResult())
+                .isEqualTo(WinningResult.LOSE);
+    }
+
+    @Test
+    @DisplayName("승무패 결과 산정 테스트 - 승리")
+    void calculateWinningResult_win() {
+        CardDeck cardDeck = new CardDeck();
+        Player player = new Player("제이미");
+        player.drawCard(cardDeck);
+        player.calculateWinningResult(22);
+        Assertions.assertThat(player.getWinningResult())
+                .isEqualTo(WinningResult.WIN);
     }
 }
