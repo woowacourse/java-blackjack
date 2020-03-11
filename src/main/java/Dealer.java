c19b6dd0d31120f947ca027702b1527f94cefba9
gunhee lee <dlrjsgml42@naver.com>
2020-03-11 15:48:41 +0900

    [feat] 딜러 클래스 구현
    Dealer 딜러의 역할을 담당하는 클래스 구현
    DealerTest 딜러의 생성 및 결과반환에 대한 테스트 구현

---
+++ src/main/java/Dealer.java
@@ -0,0 +1,34 @@
+import card.Card;
+
+public class Dealer {
+    public static final String DEALER_NAME = "딜러";
+    private final HandCard handCard;
+
+    public Dealer() {
+        handCard = new HandCard();
+    }
+
+    public String getName() {
+        return DEALER_NAME;
+    }
+
+    public void draw(Card card) {
+        handCard.add(card);
+    }
+
+    public String getStatus() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(DEALER_NAME)
+                .append(": ")
+                .append(handCard.getNames());
+        return stringBuilder.toString();
+    }
+
+    public String getFirstStatus(){
+        return getStatus().split(",")[0];
+    }
+
+    public int getScore() {
+        return handCard.getScore();
+    }
+}
