c810aeafa8c332b448fe4e76d2c89041e4ddd551
lowoon <sangbo0229@gmail.com>
2020-03-11 22:11:11 +0900

    feat: Dealer와 Plyaers의 드로우 결과 출력

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,15 @@
+import domain.YesOrNo;
 import domain.deck.Deck;
 import domain.deck.DeckFactory;
 import domain.user.Dealer;
+import domain.user.Player;
 import domain.user.Players;
 import view.InputView;
 import view.OutputView;
 
 public class Application {
+
+    private static final int FIRST_CARD_COUNT = 2;
 
     public static void main(String[] args) {
         String names = InputView.receiveNameInput();
@@ -14,11 +18,31 @@ public class Application {
         Dealer dealer = Dealer.appoint();
         Deck deck = DeckFactory.getDeck();
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < FIRST_CARD_COUNT; i++) {
             dealer.draw(deck.dealOut());
             players.draw(deck);
         }
 
         OutputView.printFirstDealOutResult(dealer, players);
+
+        players.getPlayers()
+                .forEach(player -> playersAdditionalDraw(deck, player));
+
+        while (dealer.isAvailableToDraw()) {
+            OutputView.printDealerDealOut();
+            dealer.draw(deck.dealOut());
+        }
+    }
+
+    private static void playersAdditionalDraw(Deck deck, Player player) {
+        while(player.isAvailableToDraw() && isYes(player)) {
+            player.draw(deck.dealOut());
+            OutputView.printDealOutResult(player);
+        }
+    }
+
+    private static boolean isYes(Player player) {
+        String input = InputView.receiveYesOrNoInput(player.getName());
+        return YesOrNo.of(input).isYes();
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,13 +1,22 @@
 package view;
 
 import domain.user.Dealer;
+import domain.user.Player;
 import domain.user.Players;
 
 public class OutputView {
 
     public static void printFirstDealOutResult(Dealer dealer, Players players) {
         System.out.println(String.format("\n딜러와 %s에게 2장을 나누었습니다.", players.getAllNames()));
-        System.out.println(dealer.getFirstDrawResult());
+        System.out.println(dealer.getDrawResult());
         System.out.println(players.getAllFirstDrawResult());
+    }
+
+    public static void printDealOutResult(Player player) {
+        System.out.println(player.getDrawResult());
+    }
+
+    public static void printDealerDealOut() {
+        System.out.println("\n딜러는 16이하라 한장의 카드를 더 받았습니다.");
     }
 }
