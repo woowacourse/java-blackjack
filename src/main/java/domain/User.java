e9496d6005c1f0a18f6667ca1fb286cdc0e45001
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-03-11 15:22:10 +0900

    [refactor] CardDeck 인스턴스화에 따른 수정

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -3,8 +3,8 @@ package domain;
 public abstract class User {
     protected Cards cards;
 
-    public void drawCard() {
-        cards.add(CardDeck.draw());
+    public void drawCard(CardDeck cardDeck) {
+        cards.add(cardDeck.draw());
     }
 
     public abstract boolean isAbleDrawCards();
--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -8,12 +8,13 @@ public class DealerTest {
     @Test
     @DisplayName("딜러 드로우 테스트")
     void isAbleDrawCard() {
+        CardDeck cardDeck = new CardDeck();
         Dealer dealer = new Dealer();
-        dealer.drawCard();
+        dealer.drawCard(cardDeck);
         Assertions.assertThat(dealer.isAbleDrawCards())
                 .isTrue();
         for (int i = 0; i < 10; i++) {
-            dealer.drawCard();
+            dealer.drawCard(cardDeck);
         }
         Assertions.assertThat(dealer.isAbleDrawCards())
                 .isFalse();
