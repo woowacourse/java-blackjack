b2bbaf04c87b2f4abe437a944248cd5da1262aa3
chws <rebeccachoosir@gmail.com>
2020-03-11 13:22:08 +0900

    feat: CardFactory에서 Card가 중복이 없는지 확인

--- src/test/java/blackjack/domain/CardFactoryTest.java
+++ src/test/java/blackjack/domain/CardFactoryTest.java
@@ -3,6 +3,9 @@ package blackjack.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CardFactoryTest {
@@ -11,5 +14,12 @@ public class CardFactoryTest {
     void issueNewCardsTest() {
         assertThat(CardFactory.getInstance().issueNewCards().size())
                 .isEqualTo(52);
+    }
+
+    @Test
+    @DisplayName("카드가 중복이 되지 않게 생성됐는지 확인")
+    void newCardsNotDuplicatedTest() {
+        List<Card> cards = CardFactory.getInstance().issueNewCards();
+        assertThat(cards.size() == new HashSet<>(cards).size()).isTrue();
     }
 }
