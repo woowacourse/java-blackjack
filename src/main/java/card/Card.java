cb45d9e78f0100ca4849380cdfbfb65a9757e974
hwangbo <taekwonod714@naver.com>
2020-03-11 13:13:10 +0900

    feat: 카드 클래스 구현

---
+++ src/main/java/card/Card.java
@@ -0,0 +1,36 @@
+package card;
+
+import java.util.Objects;
+
+public class Card {
+    private final Symbol symbol;
+
+    private final Type type;
+
+    public Card(Symbol symbol, Type type) {
+        this.symbol = symbol;
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return symbol == card.symbol &&
+                type == card.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(symbol, type);
+    }
+
+    @Override
+    public String toString() {
+        return "card.Card{" +
+                "symbol=" + symbol +
+                ", type=" + type +
+                '}';
+    }
+}
---
+++ src/main/java/card/Type.java
@@ -0,0 +1,8 @@
+package card;
+
+public enum Type {
+    SPADE,
+    DIAMOND,
+    HEART,
+    CLUB
+}
