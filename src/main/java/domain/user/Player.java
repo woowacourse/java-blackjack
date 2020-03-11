61303aa9c798279c78173620bb16e5cdad5f79c7
lowoon <sangbo0229@gmail.com>
2020-03-11 17:21:09 +0900

    feat: Player의 getFirstDrawResult 메소드 구현

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -1,8 +1,20 @@
 package domain.user;
 
+import java.util.stream.Collectors;
+
+import domain.deck.Card;
+
 public class Player extends User {
 
     public Player(String name) {
         super(name);
+    }
+
+    @Override
+    public String getFirstDrawResult() {
+        return name + ": "
+                + cards.stream()
+                .map(Card::getName)
+                .collect(Collectors.joining(", "));
     }
 }
--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -5,6 +5,10 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import domain.deck.Card;
+import domain.deck.Symbol;
+import domain.deck.Type;
+
 class PlayerTest {
 
     @Test
@@ -12,5 +16,14 @@ class PlayerTest {
     void create() {
         assertThatCode(() -> new Player("이름"))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void getFirstDrawResult() {
+        Player player = new Player("이름");
+        player.draw(new Card(Symbol.CLOVER, Type.EIGHT));
+        player.draw(new Card(Symbol.DIAMOND, Type.ACE));
+
+        assertThat(player.getFirstDrawResult()).isEqualTo("이름: 8클로버, A다이아몬드");
     }
 }
