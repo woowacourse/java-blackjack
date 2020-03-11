bfffd1d3db82fc0dff37880df6e52c4f772c1647
ordinCode <ysh1346@naver.com>
2020-03-11 16:34:37 +0900

    refactor : OutputView.java
     메서드 분리

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -12,8 +12,10 @@ public class OutputView {
 	private static final String STRING_FORMAT_FIRST_DRAW_MESSAGE = "딜러와 %s에게 %d장씩 나누었습니다.";
 	private static final String STRING_FORMAT_FINAL_SCORE = "%s - 결과 : %s";
 	private static final String STRING_FORMAT_DEALER_HIT_MESSAGE = "\n딜러는 %s이하라 한장의 카드를 더 받았습니다.\n";
+	private static final String STRING_FORMAT_DEALER_RESULT = "딜러 : %s";
 	private static final String FINAL_RESULT_MESSAGE = "\n## 최종 승패";
 	private static final String COLON = " : ";
+	private static final int ZERO = 0;
 
 	public static void firstDrawMessage(String name, int firstDrawCount) {
 		System.out.println(String.format(STRING_FORMAT_FIRST_DRAW_MESSAGE, name, firstDrawCount));
@@ -42,13 +44,17 @@ public class OutputView {
 	}
 
 	public static void printDealerResult(Map<Result, Integer> dealerResult) {
-		StringBuilder resultBuilder = new StringBuilder();
+		StringBuilder builder = new StringBuilder();
 		for (Result result : dealerResult.keySet()) {
-			if (dealerResult.get(result) > 0) {
-				resultBuilder.append(String.valueOf(dealerResult.get(result)) + result);
-			}
+			resultBuild(dealerResult, builder, result);
 		}
-		System.out.println(String.format("딜러 : %s", resultBuilder));
+		System.out.println(String.format(STRING_FORMAT_DEALER_RESULT, builder));
+	}
+
+	private static void resultBuild(Map<Result, Integer> dealerResult, StringBuilder builder, Result result) {
+		if (dealerResult.get(result) > ZERO) {
+			builder.append(String.valueOf(dealerResult.get(result)) + result);
+		}
 	}
 
 	public static void printUserResult(Map<String, Result> userResult) {
