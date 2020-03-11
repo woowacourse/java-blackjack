f1dbc4cb5368773092838b03b1b73da20819fb1b
hwangbo <taekwonod714@naver.com>
2020-03-11 13:03:47 +0900

    feat: 참가자 이름이 딜러와 같은 경우 예외처리

--- src/main/java/ParticipantName.java
+++ src/main/java/ParticipantName.java
@@ -1,16 +1,24 @@
 import java.util.Objects;
 
 public class ParticipantName {
+    private static final String DEALER = "딜러";
     private String participantName;
 
     public ParticipantName(String participantName) {
         checkNullOrEmpty(participantName);
+        checkSameAsDealer(participantName);
         this.participantName = participantName;
     }
 
     private void checkNullOrEmpty(String userName) {
         if (Objects.isNull(userName) || userName.isEmpty()) {
             throw new InvalidParticipantNameException(InvalidParticipantNameException.NULL_OR_EMPTY);
+        }
+    }
+
+    private void checkSameAsDealer(String userName) {
+        if (userName.equals(DEALER)) {
+            throw new InvalidParticipantNameException(InvalidParticipantNameException.SAME_AS_DEALER);
         }
     }
 }
--- src/test/java/ParticipantNameTest.java
+++ src/test/java/ParticipantNameTest.java
@@ -1,16 +1,25 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ParticipantNameTest {
+    @DisplayName("생성자에 null이나 빈 문자열이 입력되었을 경우 예외처리")
     @ParameterizedTest
     @NullAndEmptySource
-    @DisplayName("생성자에 null이나 빈 문자열이 입력되었을 경우 예외처리")
-    void checkNullOrEmpty_NullOrEmpty_ExceptionThrown(String input) {
-        assertThatThrownBy(() -> new ParticipantName(input))
+    void checkNullOrEmpty_NullOrEmpty_ExceptionThrown(String name) {
+        assertThatThrownBy(() -> new ParticipantName(name))
                 .isInstanceOf(InvalidParticipantNameException.class)
                 .hasMessage(InvalidParticipantNameException.NULL_OR_EMPTY);
+    }
+
+    @DisplayName("생성자에 '딜러'가 입력되었을 경우 예외처리")
+    @Test
+    public void checkSameAsDealer_Dealer_ExceptionThrown() {
+        assertThatThrownBy(() -> new ParticipantName("딜러"))
+                .isInstanceOf(InvalidParticipantNameException.class)
+                .hasMessage(InvalidParticipantNameException.SAME_AS_DEALER);
     }
 }
