244a49e2d4dea800f3846ef2d0488869d70cbd8e
Seyun <ksy90101@gmail.com>
2020-03-11 20:19:52 +0900

    refactor: 승패 결과 출력 메서드 다시 추가

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,60 +1,58 @@
 package controller;
 
-import static java.util.stream.Collectors.*;
-
-import java.util.List;
-import java.util.Map;
-
 import domain.card.CardFactory;
 import domain.card.Deck;
 import domain.gamer.Dealer;
 import domain.gamer.Gamers;
 import domain.gamer.Player;
-import domain.gamer.WinOrLose;
 import domain.gamer.YesOrNo;
 import utils.InputUtils;
 import view.InputView;
 import view.OutputView;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class GameController {
-	public void run() {
-		Gamers gamers = new Gamers(generatePlayers(), new Dealer());
-		Deck deck = new Deck(CardFactory.create());
+    public void run() {
+        Gamers gamers = new Gamers(generatePlayers(), new Dealer());
+        Deck deck = new Deck(CardFactory.create());
 
-		gamers.initCard(deck);
+        gamers.initCard(deck);
 
-		OutputView.printInitCardGuide(gamers);
-		OutputView.printGamersCard(gamers);
-		addCardAtPlayers(gamers, deck);
-		addCardAtDealer(gamers, deck);
-		OutputView.printCardsResultAndScore(gamers);
-		OutputView.printPlayersWinOrLose(gamers.gameResult());
-	}
+        OutputView.printInitCardGuide(gamers);
+        OutputView.printGamersCard(gamers);
+        addCardAtPlayers(gamers, deck);
+        addCardAtDealer(gamers, deck);
+        OutputView.printCardsResultAndScore(gamers);
+        OutputView.printPlayersWinOrLose(gamers.gameResult());
+    }
 
-	private List<Player> generatePlayers() {
-		return InputUtils.splitAsDelimiter(InputView.inputAsPlayerName())
-			.stream()
-			.map(Player::new)
-			.collect(toList());
-	}
+    private List<Player> generatePlayers() {
+        return InputUtils.splitAsDelimiter(InputView.inputAsPlayerName())
+                .stream()
+                .map(Player::new)
+                .collect(toList());
+    }
 
-	private void addCardAtPlayers(Gamers gamers, Deck deck) {
-		gamers.stream()
-			.forEach(player -> drawCardOfPlayer(deck, player));
-	}
+    private void addCardAtPlayers(Gamers gamers, Deck deck) {
+        gamers.stream()
+                .forEach(player -> drawCardOfPlayer(deck, player));
+    }
 
-	private void drawCardOfPlayer(Deck deck, Player player) {
-		while (player.isDrawable()
-			&& YesOrNo.findYesOrNo(InputView.inputAsDrawable(player)).getDrawable()) {
-			player.addCard(deck.popCard(1));
-			OutputView.printGamerCard(player);
-		}
-	}
+    private void drawCardOfPlayer(Deck deck, Player player) {
+        while (player.isDrawable()
+                && YesOrNo.findYesOrNo(InputView.inputAsDrawable(player)).getDrawable()) {
+            player.addCard(deck.popCard(1));
+            OutputView.printGamerCard(player);
+        }
+    }
 
-	private void addCardAtDealer(Gamers gamers, Deck deck) {
-		while (gamers.getDealer().isDrawable()) {
-			OutputView.printAddCardAtDealer();
-			gamers.getDealer().addCard(deck.popCard(1));
-		}
-	}
+    private void addCardAtDealer(Gamers gamers, Deck deck) {
+        while (gamers.getDealer().isDrawable()) {
+            OutputView.printAddCardAtDealer();
+            gamers.getDealer().addCard(deck.popCard(1));
+        }
+    }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,17 +1,18 @@
 package view;
 
-import java.util.stream.Collectors;
-
-import domain.card.Card;
 import domain.gamer.Gamer;
 import domain.gamer.Gamers;
 import domain.gamer.Player;
+import domain.gamer.WinOrLose;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class OutputView {
     private static final String NEWLINE = System.getProperty("line.separator" );
 
     public static void printPlayerNamesGuide() {
-        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)" );
     }
 
     public static void printInitCardGuide(Gamers gamers) {
@@ -37,7 +38,7 @@ public class OutputView {
     }
 
     public static void printAddCardAtDealer() {
-        System.out.println("딜러는 16이하라 카드 한장 더 받았습니다.");
+        System.out.println("딜러는 16이하라 카드 한장 더 받았습니다." );
     }
 
     public static void printAddCardGuide(Player player) {
@@ -48,5 +49,14 @@ public class OutputView {
         System.out.println();
         System.out.printf("%s - 결과 : %s" + NEWLINE, gamers.getDealer(), gamers.getDealer().calculateWithAce());
         gamers.stream().forEach(player -> System.out.printf("%s - 결과 : %s" + NEWLINE, player, player.calculateWithAce()));
+    }
+
+    public static void printPlayersWinOrLose(Map<String, WinOrLose> playersWinOrLose) {
+        System.out.println();
+        System.out.println("## 최종 승패" );
+        System.out.println("딜러 : " + playersWinOrLose.values().stream().filter(x -> x == WinOrLose.LOSE).count() + WinOrLose.WIN.getInitial()
+                + playersWinOrLose.values().stream().filter(x -> x == WinOrLose.WIN).count() + WinOrLose.LOSE.getInitial()
+        );
+        playersWinOrLose.forEach((a, b) -> System.out.println(a + " : " + b.getInitial()));
     }
 }
