b8ad4f6d9dd38d49b7c8e6d694cbc7fefdc9533a
kuenhwi <kueniyam@naver.com>
2020-03-12 22:14:39 +0900

    test: Card 테스트 코드 작성

--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -1,15 +1,45 @@
 package blackjack.domain.card;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CardTest {
+    private Card card;
+
+    @BeforeEach
+    void setUp() {
+        card = Card.of(Symbol.ACE, Type.CLUB);
+    }
+
     @Test
     void Card() {
-        assertThat(Card.of(Symbol.ACE, Type.CLUB)).isNotNull();
+        assertThat(card).isNotNull();
         assertThat(Card.of(Symbol.TWO, Type.HEART)).isNotNull();
         assertThat(Card.of(Symbol.ACE, Type.CLUB)).isNotNull();
         assertThat(Card.of(Symbol.ACE, Type.CLUB)).isNotNull();
+    }
+
+    @Test
+    void isAce() {
+        assertThat(card.isAce()).isTrue();
+        assertThat(Card.of(Symbol.TWO, Type.CLUB).isAce())
+                .isFalse();
+    }
+
+    @Test
+    void getScore() {
+        assertThat(card.getScore()).isEqualTo(Score.of(1));
+    }
+
+    @Test
+    void testEquals() {
+        assertThat(card).isEqualTo(Card.of(Symbol.ACE, Type.CLUB));
+    }
+
+    @Test
+    void getName() {
+        assertThat(card.getName()).isEqualTo("A 클로버");
     }
 }
