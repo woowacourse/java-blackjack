55643fc68af0e9ecdda8c27af3cae6bba4df9277
chws <rebeccachoosir@gmail.com>
2020-03-12 17:39:34 +0900

    refactor: InputView의 예외 메소드 분리

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -11,9 +11,7 @@ public class InputView {
         try {
             System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
             String input = sc.nextLine();
-            if (input == null || input.isEmpty()) {
-                throw new IllegalArgumentException("이름은 최소 1개 이상이어야 합니다");
-            }
+            validateNullAndEmptyInput(input, "이름은 최소 1개 이상이어야 합니다");
             return input;
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -25,13 +23,17 @@ public class InputView {
         try {
             System.out.println(name + "은(는) 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
             String input = sc.nextLine();
-            if (input == null || input.isEmpty()) {
-                throw new IllegalArgumentException("다시 입력해 주세요.");
-            }
+            validateNullAndEmptyInput(input, "다시 입력해 주세요.");
             return Decision.of(input);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return askForHit(name);
+        }
+    }
+
+    private static void validateNullAndEmptyInput(String input, String message) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(message);
         }
     }
 }
