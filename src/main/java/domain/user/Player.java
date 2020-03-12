6cf916d5e14c12f5ff294514df94f10c4262c15f
lowoon <sangbo0229@gmail.com>
2020-03-12 15:07:27 +0900

    feat: Player의 win 메소드 구현

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -1,5 +1,7 @@
 package domain.user;
 
+import domain.WinningResult;
+
 public class Player extends User {
 
     public Player(String name) {
@@ -9,5 +11,28 @@ public class Player extends User {
     @Override
     public boolean isAvailableToDraw() {
         return !isBust() && !isBlackJack() && !isBlackJackPoint();
+    }
+
+    //todo: 내부 구현 내용 개선
+    public WinningResult win(Dealer dealer) {
+        if (isBust()) {
+            return WinningResult.LOSE;
+        }
+        if (isBlackJack()) {
+            if (dealer.isBlackJack()) {
+                return WinningResult.DRAW;
+            }
+            return WinningResult.WIN;
+        }
+        if (dealer.isBust()) {
+            return WinningResult.WIN;
+        }
+        if (calculatePointAccordingToHasAce() > dealer.calculatePointAccordingToHasAce()) {
+            return WinningResult.WIN;
+        }
+        if (calculatePointAccordingToHasAce() == dealer.calculatePointAccordingToHasAce()) {
+            return WinningResult.DRAW;
+        }
+        return WinningResult.LOSE;
     }
 }
--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import domain.WinningResult;
 import domain.deck.Card;
 import domain.deck.Symbol;
 import domain.deck.Type;
@@ -31,8 +32,6 @@ class PlayerTest {
                 .doesNotThrowAnyException();
     }
 
-
-
     @ParameterizedTest
     @DisplayName("플레이어가 드로우 가능한지 확인")
     @MethodSource("createOption")
@@ -49,6 +48,51 @@ class PlayerTest {
                 Arguments.of(new Card(Symbol.DIAMOND, Type.TWO), true),
                 Arguments.of(new Card(Symbol.DIAMOND, Type.THREE), false),
                 Arguments.of(new Card(Symbol.DIAMOND, Type.FIVE), false)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("포인트 비교로 승자 확인")
+    @MethodSource("createCardSet")
+    void win(Card playerCard, Card dealerCard, WinningResult expected) {
+        player.draw(new Card(Symbol.DIAMOND, Type.KING));
+        player.draw(new Card(Symbol.DIAMOND, Type.SIX));
+        player.draw(playerCard);
+
+        Dealer dealer = Dealer.appoint();
+        dealer.draw(new Card(Symbol.CLOVER, Type.KING));
+        dealer.draw(new Card(Symbol.CLOVER, Type.SIX));
+        dealer.draw(dealerCard);
+
+        assertThat(player.win(dealer)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> createCardSet() {
+        return Stream.of(
+                //플레이어가 21점인 경우
+                createCardsAndResult(Type.FIVE, Type.FIVE, WinningResult.DRAW),
+                createCardsAndResult(Type.FIVE, Type.FOUR, WinningResult.WIN),
+                createCardsAndResult(Type.FIVE, Type.SIX, WinningResult.WIN),
+
+                //플레이어가 버스트인 경우
+                createCardsAndResult(Type.SIX, Type.SIX, WinningResult.LOSE),
+                createCardsAndResult(Type.SIX, Type.FIVE, WinningResult.LOSE),
+                createCardsAndResult(Type.SIX, Type.FOUR, WinningResult.LOSE),
+
+                //플레이어가 21미만인 경우
+                createCardsAndResult(Type.THREE, Type.TWO, WinningResult.WIN),
+                createCardsAndResult(Type.THREE, Type.THREE, WinningResult.DRAW),
+                createCardsAndResult(Type.THREE, Type.FOUR, WinningResult.LOSE),
+                createCardsAndResult(Type.THREE, Type.FIVE, WinningResult.LOSE),
+                createCardsAndResult(Type.THREE, Type.SIX, WinningResult.WIN)
+                );
+    }
+
+    private static Arguments createCardsAndResult(Type player, Type dealer, WinningResult winningResult) {
+        return Arguments.of(
+                new Card(Symbol.HEART, player),
+                new Card(Symbol.SPADE, dealer),
+                winningResult
         );
     }
 }
