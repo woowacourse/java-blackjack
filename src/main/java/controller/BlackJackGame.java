8e9ab24141f5da0ce06ad6e0c6afc70525893342
Ramen6315 <ramen6315@gmail.com>
2020-03-12 16:51:58 +0900

    refactor : BlackJackGame 컨트롤러 코드 수정

--- src/main/java/controller/BlackJackGame.java
+++ src/main/java/controller/BlackJackGame.java
@@ -11,7 +11,6 @@ import view.OutputView;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class BlackJackGame {
@@ -24,26 +23,39 @@ public class BlackJackGame {
         Dealer dealer = new Dealer(deck.getInitCards());
 
         OutputView.printInitGamersState(GamerDto.of(dealer), players.stream().map(GamerDto::of).collect(Collectors.toList()));
+        receivePlayerCards(deck, players);
+        receiveDealerCard(deck, dealer);
+
+        OutputView.printGamerCardsStateWithScore(GamerDto.of(dealer), dealer.calculateScore());
+        for(Player player : players) {
+            OutputView.printGamerCardsStateWithScore(GamerDto.of(player), player.calculateScore());
+        }
+        Map<PlayerResult, List<Player>> gameResults = calculateGameResults(players, dealer);
+        OutputView.printGameResult(gameResults);
+}
+
+    private Map<PlayerResult, List<Player>> calculateGameResults(List<Player> players, Dealer dealer) {
+        Map<PlayerResult, List<Player>> gameResults = players.stream()
+                .collect(Collectors.groupingBy(player -> PlayerResult.match(dealer, player), Collectors.toList()));
+        for(PlayerResult playerResult : PlayerResult.values()) {
+            gameResults.putIfAbsent(playerResult, new ArrayList<>());
+        }
+        return gameResults;
+    }
+
+    private void receiveDealerCard(Deck deck, Dealer dealer) {
+        if (dealer.canGetExtraCard()) {
+            dealer.addCard(deck.handOutCard());
+            OutputView.printDealerHit();
+        }
+    }
+
+    private void receivePlayerCards(Deck deck, List<Player> players) {
         for (Player player : players) {
             while (!player.isBust() && InputView.inputGetMoreCard(player.getName()).equals("y")) {
                 player.addCard(deck.handOutCard());
                 OutputView.printGamerCardsState(GamerDto.of(player));
             }
         }
-        if (dealer.canGetExtraCard()) {
-            dealer.addCard(deck.handOutCard());
-            OutputView.printDealerHit();
-        }
-
-        OutputView.printGamerCardsStateWithScore(GamerDto.of(dealer), dealer.calculateScore());
-        for(Player player : players) {
-            OutputView.printGamerCardsStateWithScore(GamerDto.of(player), player.calculateScore());
-        }
-        Map<PlayerResult, List<Player>> gameResults = players.stream()
-                .collect(Collectors.groupingBy(player -> PlayerResult.match(dealer, player), Collectors.toList()));
-        for(PlayerResult playerResult : PlayerResult.values()) {
-            gameResults.putIfAbsent(playerResult, new ArrayList<>());
-        }
-        OutputView.printGameResult(gameResults);
-}
+    }
 }
