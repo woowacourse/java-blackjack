075177f239715d3747c6235569e10bf4214a1aa8
juyeongjung <juyeong.dev@gmail.com>
2020-03-12 14:06:53 +0900

    test : 플레이어들 카드 더받을수 있는지 여부 테스트

--- src/test/java/domain/gamer/PlayerTest.java
+++ src/test/java/domain/gamer/PlayerTest.java
@@ -4,6 +4,7 @@ import domain.card.Card;
 import domain.card.CardNumber;
 import domain.card.CardSuit;
 import exception.NameFormatException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,44 +12,64 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PlayerTest {
-    @Test
-    @DisplayName("잘못된 이름 입력시 예외처리")
-    void isValidNameTest() {
-        assertThatThrownBy(() -> new Player("po/bi")).isInstanceOf(NameFormatException.class);
-    }
+	@Test
+	@DisplayName("잘못된 이름 입력시 예외처리")
+	void isValidNameTest() {
+		assertThatThrownBy(() -> new Player("po/bi")).isInstanceOf(NameFormatException.class);
+	}
 
-    @ParameterizedTest
-    @MethodSource("generateDealerScore")
-    @DisplayName("딜러의 점수에 따라서 플레이어가 승리하는지 패패하는지에 대한 테스트")
-    void winOrLoseTest(int dealerScore, WinOrLose expected) {
-        Player player = new Player("pobi");
-        player.addCard(Arrays.asList(
-                new Card(CardSuit.CLOVER, CardNumber.EIGHT),
-                new Card(CardSuit.CLOVER, CardNumber.TEN)));
-        assertThat(player.isWinOrLose(dealerScore)).isEqualTo(expected);
-    }
+	@ParameterizedTest
+	@MethodSource("generateDealerScore")
+	@DisplayName("딜러의 점수에 따라서 플레이어가 승리하는지 패패하는지에 대한 테스트")
+	void winOrLoseTest(int dealerScore, WinOrLose expected) {
+		Player player = new Player("pobi");
+		player.addCard(Arrays.asList(
+			new Card(CardSuit.CLOVER, CardNumber.EIGHT),
+			new Card(CardSuit.CLOVER, CardNumber.TEN)));
+		assertThat(player.isWinOrLose(dealerScore)).isEqualTo(expected);
+	}
 
-    static Stream<Arguments> generateDealerScore() {
-        return Stream.of(
-                Arguments.of(20, WinOrLose.LOSE),
-                Arguments.of(17, WinOrLose.WIN),
-                Arguments.of(22, WinOrLose.WIN));
-    }
+	static Stream<Arguments> generateDealerScore() {
+		return Stream.of(
+			Arguments.of(20, WinOrLose.LOSE),
+			Arguments.of(17, WinOrLose.WIN),
+			Arguments.of(22, WinOrLose.WIN));
+	}
 
-    @Test
-    @DisplayName("플레이어가 버스트가 되었을 경우 패패 테스트")
-    void isPlayerBustTest() {
-        Player player = new Player("pobi");
-        player.addCard(Arrays.asList(
-                new Card(CardSuit.CLOVER, CardNumber.EIGHT),
-                new Card(CardSuit.CLOVER, CardNumber.TEN),
-                new Card(CardSuit.HEART, CardNumber.KING)));
-        assertThat(player.isWinOrLose(17)).isEqualTo(WinOrLose.LOSE);
-    }
+	@Test
+	@DisplayName("플레이어가 버스트가 되었을 경우 패패 테스트")
+	void isPlayerBustTest() {
+		Player player = new Player("pobi");
+		player.addCard(Arrays.asList(
+			new Card(CardSuit.CLOVER, CardNumber.EIGHT),
+			new Card(CardSuit.CLOVER, CardNumber.TEN),
+			new Card(CardSuit.HEART, CardNumber.KING)));
+		assertThat(player.isWinOrLose(17)).isEqualTo(WinOrLose.LOSE);
+	}
+
+	@ParameterizedTest
+	@MethodSource("generateCards")
+	public void isDrawableTest(List<Card> cards, boolean expected) {
+		Player player = new Player("pobi");
+		player.addCard(cards);
+		assertThat(player.isDrawable()).isEqualTo(expected);
+	}
+
+	static Stream<Arguments> generateCards() {
+		return Stream.of(
+			Arguments.of(Arrays.asList(
+				new Card(CardSuit.CLOVER, CardNumber.SIX),
+				new Card(CardSuit.CLOVER, CardNumber.TEN)), true),
+			Arguments.of(Arrays.asList(
+				new Card(CardSuit.CLOVER, CardNumber.KING),
+				new Card(CardSuit.CLOVER, CardNumber.TEN),
+				new Card(CardSuit.CLOVER, CardNumber.THREE)), false));
+	}
 }
