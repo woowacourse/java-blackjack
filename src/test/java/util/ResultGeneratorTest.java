bf20c57f8fbc2ba863b398ac206b04dc100a0959
hamliet <hamliet@hanyang.ac.kr>
2020-03-12 14:51:25 +0900

    fix: ResultGenerator 테스트 함수명 잘못된 것 수정

--- src/test/java/util/ResultGeneratorTest.java
+++ src/test/java/util/ResultGeneratorTest.java
@@ -18,17 +18,17 @@ public class ResultGeneratorTest {
     @DisplayName("결과 생성 확인")
     void create() {
         User userA = new User("유저A");
-        User playerB = new User("유저B");
+        User userB = new User("유저B");
         Dealer dealer = new Dealer();
         CardDeck cardDeck = new CardDeck(CardFactory.create());
         cardDeck.shuffle();
         CardDistributor.giveOneCard(cardDeck, userA);
         CardDistributor.giveOneCard(cardDeck, userA);
-        CardDistributor.giveOneCard(cardDeck, playerB);
-        CardDistributor.giveOneCard(cardDeck, playerB);
+        CardDistributor.giveOneCard(cardDeck, userB);
+        CardDistributor.giveOneCard(cardDeck, userB);
         dealer.addCard(cardDeck.drawOne());
         dealer.addCard(cardDeck.drawOne());
 
-        assertThat(ResultGenerator.create(dealer, new Users(Arrays.asList(userA, playerB)))).isInstanceOf(Results.class);
+        assertThat(ResultGenerator.create(dealer, new Users(Arrays.asList(userA, userB)))).isInstanceOf(Results.class);
     }
 }
