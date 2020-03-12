4cea0396768c495c9ff6561fe653611712453b20
chws <rebeccachoosir@gmail.com>
2020-03-12 17:35:04 +0900

    refactor: UserFactory의 예외 메소드 분리

--- src/main/java/blackjack/domain/user/UserFactory.java
+++ src/main/java/blackjack/domain/user/UserFactory.java
@@ -9,15 +9,18 @@ public class UserFactory {
     public static final int MAX_PLAYER = 8;
 
     public static Users generateUsers(List<String> userNames) {
-        if (userNames.size() > MAX_PLAYER) {
-            throw new IllegalArgumentException("플레이어의 수는" + MAX_PLAYER + "명을 초과할 수 없습니다");
-        }
-
+        validatePlayerNumber(userNames);
         List<User> users = new ArrayList<>();
         users.add(new Dealer());
         users.addAll(userNames.stream()
                 .map(Player::new)
                 .collect(Collectors.toList()));
         return new Users(users);
+    }
+
+    private static void validatePlayerNumber(List<String> userNames) {
+        if (userNames.size() > MAX_PLAYER) {
+            throw new IllegalArgumentException("플레이어의 수는" + MAX_PLAYER + "명을 초과할 수 없습니다");
+        }
     }
 }
