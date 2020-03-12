93465de5958c9bc62ddfe2c589ab03ce3fa48f4d
begaonnuri <turtlenam0928@gmail.com>
2020-03-12 14:17:15 +0900

    refactor: BlackjackController 변경
    - 최초에 딜러와 플레이어에게 카드를 발급하는 로직 추가
    - 최초에 받은 카드를 출력하는 로직 추가

--- src/main/java/com/blackjack/controller/BlackjackController.java
+++ src/main/java/com/blackjack/controller/BlackjackController.java
@@ -1,6 +1,34 @@
 package com.blackjack.controller;
 
+import static com.blackjack.view.InputView.*;
+import static com.blackjack.view.OutputView.*;
+
+import java.util.List;
+
+import com.blackjack.domain.card.CardDeck;
+import com.blackjack.domain.user.Dealer;
+import com.blackjack.domain.user.PlayerFactory;
+import com.blackjack.domain.user.User;
+
 public class BlackjackController {
 	public void run() {
+		CardDeck cardDeck = CardDeck.create();
+		Dealer dealer = new Dealer();
+		List<User> players = createPlayers();
+
+		drawAtFirst(cardDeck, dealer, players);
+		printCardsAtFirst(dealer, players);
+	}
+
+	private void drawAtFirst(CardDeck cardDeck, User dealer, List<User> players) {
+		dealer.drawAtFirst(cardDeck);
+		for (User player : players) {
+			player.drawAtFirst(cardDeck);
+		}
+	}
+
+	private List<User> createPlayers() {
+		String names = inputPlayerNames();
+		return PlayerFactory.createPlayers(names);
 	}
 }
