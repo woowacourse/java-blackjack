38048ca554fa9232f1a7c06b080d2c13ca7fbb29
begaonnuri <turtlenam0928@gmail.com>
2020-03-12 15:57:35 +0900

    feat: DrawDecideType 클래스 추가
    - 같은 symbol을 확인하는 기능 추가

---
+++ src/main/java/com/blackjack/domain/DrawDecideType.java
@@ -0,0 +1,25 @@
+package com.blackjack.domain;
+
+import java.util.stream.Stream;
+
+public enum DrawDecideType {
+	DRAW("y"),
+	STAND("n");
+
+	private String symbol;
+
+	DrawDecideType(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public static DrawDecideType of(String symbol) {
+		return Stream.of(values())
+			.filter(drawDecideType -> drawDecideType.isSameSymbol(symbol))
+			.findAny()
+			.orElseThrow(() -> new IllegalArgumentException("y 또는 n을 입력해주세요."));
+	}
+
+	private boolean isSameSymbol(String symbol) {
+		return this.symbol.equals(symbol);
+	}
+}
