f49086d93accb23a04bd0c7c0bfa1b17a16838bf
YebinK <yebin825@gmail.com>
2020-03-12 17:56:58 +0900

    [refactor] 상수 접근자 수정

--- src/main/java/blackjack/domain/gamer/Dealer.java
+++ src/main/java/blackjack/domain/gamer/Dealer.java
@@ -4,7 +4,7 @@ public class Dealer extends Gamer {
 
     private static final String DEALER_NAME = "딜러";
     private static final int DRAW_THRESHOLD = 16;
-    public static final int FIRST_CARD_INDEX = 0;
+    private static final int FIRST_CARD_INDEX = 0;
 
     public boolean shouldDrawCard() {
         return calculateSum() <= DRAW_THRESHOLD;
