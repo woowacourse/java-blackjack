56622eb73cf5302b26345e8ce9b8266cd07731dc
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-03-12 13:49:57 +0900

    [feat] Player List 일급 컬렉션 추가

---
+++ src/main/java/domain/Players.java
@@ -0,0 +1,29 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Players {
+    private final List<Player> players;
+
+    public Players(List<String> playerNames) {
+        List<Player> players = new ArrayList<>();
+        for (String playerName : playerNames) {
+            players.add(new Player(playerName));
+        }
+        this.players = Collections.unmodifiableList(players);
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public List<String> getPlayerNames() {
+        List<String> playerNames = new ArrayList<>();
+        for (Player player : players) {
+            playerNames.add(player.getName());
+        }
+        return Collections.unmodifiableList(playerNames);
+    }
+}
---
+++ src/test/java/domain/PlayersTest.java
@@ -0,0 +1,35 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayersTest {
+    @Test
+    @DisplayName("이름을 받아서 플레이어들을 생성해줌")
+    void players() {
+        List<String> playerNames = new ArrayList<>();
+        playerNames.add("pobi");
+        playerNames.add("jason");
+        Players players = new Players(playerNames);
+        List<Player> playerList = players.getPlayers();
+        assertThat(playerList.get(0).getName()).isEqualTo("pobi");
+        assertThat(playerList.get(1).getName()).isEqualTo("jason");
+    }
+
+    @Test
+    @DisplayName("플레이어들의 이름을 받아오는 테스트")
+    void getPlayerNames() {
+        List<String> playerNames = new ArrayList<>();
+        playerNames.add("pobi");
+        playerNames.add("jason");
+        Players players = new Players(playerNames);
+        playerNames = players.getPlayerNames();
+        assertThat(playerNames.get(0)).isEqualTo("pobi");
+        assertThat(playerNames.get(1)).isEqualTo("jason");
+    }
+}
