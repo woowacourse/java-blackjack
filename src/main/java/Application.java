a6cdd0a6b248253d8784a128495704bd7ef83184
kouz <lastpuzly@gmail.com>
2020-03-13 01:02:34 +0900

    refactor: delegate view logic, Application to Controller

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,6 +9,6 @@ public class Application {
 	public static void main(String[] args) {
 		Deck deck = new Deck(CardRepository.toList());
 		Dealer dealer = new Dealer();
-		BlackjackController.run(deck, dealer, Players.of(InputView.inputPlayerNames()));
+		BlackjackController.run(deck, dealer);
 	}
 }
--- src/main/java/controller/BlackjackController.java
+++ src/main/java/controller/BlackjackController.java
@@ -2,43 +2,47 @@ package controller;
 
 import domain.PlayerIntentionType;
 import domain.card.Deck;
+import domain.result.Result;
 import domain.user.Dealer;
 import domain.user.Player;
 import domain.user.Players;
 import service.BlackjackService;
+import view.InputView;
 
 import static view.InputView.*;
 import static view.OutputView.*;
 
 public class BlackjackController {
-	public static void run(Deck deck, Dealer dealer, Players players) {
+	public static void run(Deck deck, Dealer dealer) {
+		Players players = Players.of(InputView.inputPlayerNames());
 		BlackjackService.giveInitialCards(deck, dealer, players);
-		printInitialDistribution(players);
 		printInitialStatus(dealer.openCard(), players);
 
 		if (dealer.isBlackJack()) {
-			printResultStatus(dealer.openAllCards(), players);
-			printResult(BlackjackService.createResult(dealer, players), players);
+			Result result = BlackjackService.createResult(dealer, players);
+			printResult(result, dealer, players);
 			return;
 		}
 
-		players.forEach(player -> {
-			while (player.canDrawMore() && wantDraw(player)) {
-				BlackjackService.addCard(player, deck);
-				printCardsStatusOf(player);
-			}
-		});
+		players.forEach(player -> proceedPhaseOf(player, deck));
 
 		while (dealer.canDrawMore()) {
 			BlackjackService.addCard(dealer, deck);
-			printDealerDraw();
+			printDealerDrawing();
 		}
 
-		printResultStatus(dealer.openAllCards(), players);
-		printResult(BlackjackService.createResult(dealer, players), players);
+		Result result = BlackjackService.createResult(dealer, players);
+		printResult(result, dealer, players);
+	}
+
+	private static void proceedPhaseOf(Player player, Deck deck) {
+		while (player.canDrawMore() && wantDraw(player)) {
+			BlackjackService.addCard(player, deck);
+			printCardsStatusOf(player);
+		}
 	}
 
 	private static boolean wantDraw(Player player) {
-		return PlayerIntentionType.of(inputPlayerIntention(player)).equals(PlayerIntentionType.YES);
+		return PlayerIntentionType.YES.equals(PlayerIntentionType.of(inputPlayerIntention(player)));
 	}
 }
