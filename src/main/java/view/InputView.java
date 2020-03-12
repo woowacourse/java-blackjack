6acf46e4e9d0ffe24a6b842d63a944bdeef2a1eb
kouz <lastpuzly@gmail.com>
2020-03-13 01:00:27 +0900

    refactor: arrange view method

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,13 +7,13 @@ import java.util.Scanner;
 public class InputView {
 	private static final Scanner SCANNER = new Scanner(System.in);
 
-	public static String inputPlayerIntention(Player player) {
-		System.out.println(player.toString() + "는 한장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)");
+	public static String inputPlayerNames() {
+		System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
 		return SCANNER.nextLine();
 	}
 
-	public static String inputPlayerNames() {
-		System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+	public static String inputPlayerIntention(Player player) {
+		System.out.println(player.toString() + "는 한장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)");
 		return SCANNER.nextLine();
 	}
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,6 +5,7 @@ import domain.card.Card;
 import domain.card.Cards;
 import domain.result.Result;
 import domain.result.ResultType;
+import domain.user.Dealer;
 import domain.user.Player;
 import domain.user.Players;
 
@@ -13,72 +14,59 @@ import java.util.Map;
 import static java.util.stream.Collectors.joining;
 
 public class OutputView {
-
 	private static final String DELIMITER = ", ";
 
-	public static void printInitialDistribution(Players players) {
+	public static void printInitialStatus(Card dealerCard, Players players) {
 		emptyLine();
 		System.out.println("딜러와 " + players.getNames() + "에게 2장의 카드를 나누었습니다.");
-	}
-
-	public static void printInitialStatus(Card dealerCard, Players players) {
-		System.out.println("딜러: " + dealerCard.toString());
+		System.out.println("딜러: " + dealerCard);
 		players.forEach(OutputView::printCardsStatusOf);
 		emptyLine();
 	}
 
 	public static void printCardsStatusOf(Player player) {
-		System.out.println(
-				player.toString() +
-						"카드: " +
-						player.openAllCards().toList().stream()
-								.map(Card::toString)
-								.collect(joining(DELIMITER))
-		);
+		Cards cards = player.openAllCards();
+		System.out.println(player + "카드: " +
+			cards.toList().stream()
+				.map(Card::toString)
+				.collect(joining(DELIMITER)));
 	}
 
-	public static void printDealerDraw() {
+	public static void printDealerDrawing() {
 		emptyLine();
 		System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
-		emptyLine();
-	}
-
-	public static void printResultStatus(Cards dealerCards, Players players) {
-		System.out.println("딜러: " +
-				dealerCards.toList().stream()
-						.map(Card::toString)
-						.collect(joining(DELIMITER)) +
-				" - 결과: " +
-				Score.of(dealerCards));
-
-		players.forEach(OutputView::printCardsResultOf);
 	}
 
 	private static void printCardsResultOf(Player player) {
-		System.out.println(
-				player +
-						"카드: " +
-						player.openAllCards().toList().stream()
-								.map(Card::toString)
-								.collect(joining(DELIMITER)) +
-						" - 결과: " +
-						Score.of(player.openAllCards())
-		);
+		Cards cards = player.openAllCards();
+		System.out.println(player + "카드: " +
+			cards.toList().stream()
+				.map(Card::toString)
+				.collect(joining(DELIMITER)) +
+			" - 결과: " + Score.of(player.openAllCards()));
 	}
 
-	public static void printResult(Result result, Players players) {
+	public static void printResult(Result result, Dealer dealer, Players players) {
 		Map<ResultType, Long> dealerResult = result.createDealerResult();
 
-		emptyLine();
+		printResultStatus(dealer.openAllCards(), players);
 		System.out.println("## 최종 승패");
 		System.out.println("딜러: " +
-				dealerResult.getOrDefault(ResultType.WIN, 0L) + "승 " +
-				dealerResult.getOrDefault(ResultType.DRAW, 0L) + "무 " +
-				dealerResult.getOrDefault(ResultType.LOSE, 0L) + "패"
-		);
-		players.forEach(player ->
-				System.out.println(player.toString() + ": " + result.get(player).getName())
-		);
+			dealerResult.getOrDefault(ResultType.WIN, 0L) + "승 " +
+			dealerResult.getOrDefault(ResultType.DRAW, 0L) + "무 " +
+			dealerResult.getOrDefault(ResultType.LOSE, 0L) + "패");
+
+		players.forEach(player -> System.out.println(player + ": " + result.get(player).getName()));
+	}
+
+	private static void printResultStatus(Cards dealerCards, Players players) {
+		System.out.println("딜러: " + dealerCards.toList().stream()
+			.map(Card::toString)
+			.collect(joining(DELIMITER)) +
+			" - 결과: " + Score.of(dealerCards));
+
+		players.forEach(OutputView::printCardsResultOf);
+		emptyLine();
 	}
 
 	private static void emptyLine() {
