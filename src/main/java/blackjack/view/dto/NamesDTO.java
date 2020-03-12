5f644e40aff1c0c203d119cd182e971fda66f1f5
chanin <pci2676@gmail.com>
2020-03-13 00:51:27 +0900

    refactor : NamesDTO 멤버변수 변경

--- src/main/java/blackjack/view/dto/NamesDTO.java
+++ src/main/java/blackjack/view/dto/NamesDTO.java
@@ -10,39 +10,38 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class NamesDTO {
-	private static final String DELIMITER = ",";
+    private static final String DELIMITER = ",";
 
-	private final String inputNames;
+    private final List<String> names;
 
-	public NamesDTO(String inputNames) {
-		validate(inputNames);
-		this.inputNames = inputNames;
-	}
+    public NamesDTO(String inputNames) {
+        validate(inputNames);
+        this.names = Arrays.stream(inputNames.split(DELIMITER))
+                .collect(Collectors.toList());
+    }
 
-	private void validate(String inputNames) {
-		if (inputNames == null || inputNames.trim().isEmpty()) {
-			throw new IllegalArgumentException(String.format("%s 비어있는 값을 입력했습니다.", inputNames));
-		}
-	}
+    private void validate(String inputNames) {
+        if (inputNames == null || inputNames.trim().isEmpty()) {
+            throw new IllegalArgumentException(String.format("%s 비어있는 값을 입력했습니다.", inputNames));
+        }
+    }
 
-	public List<Player> toPlayers() {
-		return Arrays.stream(inputNames.split(DELIMITER))
-			.map(name -> new Gambler(new CardBundle(), name))
-			.collect(Collectors.toList());
-	}
+    public List<Player> toPlayers() {
+        return names.stream()
+                .map(name -> new Gambler(new CardBundle(), name))
+                .collect(Collectors.toList());
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		NamesDTO namesDTO = (NamesDTO)o;
-		return Objects.equals(inputNames, namesDTO.inputNames);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NamesDTO namesDTO = (NamesDTO) o;
+        return Objects.equals(names, namesDTO.names);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(inputNames);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(names);
+    }
 }
