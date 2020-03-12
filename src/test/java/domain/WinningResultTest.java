e4d8d06005fb938ab743cf3287d13f869c5fbcbe
jamie9504 <jamie9504@kakao.com>
2020-03-12 15:50:44 +0900

    [리팩터링] 승패산정 테스트케이스 추가

--- src/test/java/domain/WinningResultTest.java
+++ src/test/java/domain/WinningResultTest.java
@@ -2,16 +2,32 @@ package domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class WinningResultTest {
 
-    @Test
-    @DisplayName("승무패 산정 테스트")
-    void calculate() {
-        int dealerScore = 17;
-        int playerScore = 16;
+    @DisplayName("패 산정 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"17, 16", "15, 8", "20, 16"})
+    void calculateLose(int dealerScore, int playerScore) {
         Assertions.assertThat(WinningResult.calculate(dealerScore, playerScore))
             .isEqualTo(WinningResult.LOSE);
+    }
+
+    @DisplayName("무 산정 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"17, 17", "15, 15", "20, 20"})
+    void calculateDraw(int dealerScore, int playerScore) {
+        Assertions.assertThat(WinningResult.calculate(dealerScore, playerScore))
+            .isEqualTo(WinningResult.DRAW);
+    }
+
+    @DisplayName("승 산정 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"16, 17", "8, 15", "16, 20"})
+    void calculate(int dealerScore, int playerScore) {
+        Assertions.assertThat(WinningResult.calculate(dealerScore, playerScore))
+            .isEqualTo(WinningResult.WIN);
     }
 }
