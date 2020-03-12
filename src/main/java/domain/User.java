ac34c12e7637748f61f2ae7736648cabf61d1dc9
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-03-12 13:49:35 +0900

    [feat] 여러장 draw 기능 추가

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -4,8 +4,18 @@ public abstract class User {
     protected Cards cards;
 
     public void drawCard(CardDeck cardDeck) {
-        cards.add(cardDeck.draw());
+        drawCard(cardDeck, 1);
+    }
+
+    public void drawCard(CardDeck cardDeck, int cardCount) {
+        for (int i = 0; i < cardCount; i++) {
+            cards.add(cardDeck.draw());
+        }
     }
 
     public abstract boolean isAbleDrawCards();
+
+    public Cards getCards() {
+        return cards;
+    }
 }
