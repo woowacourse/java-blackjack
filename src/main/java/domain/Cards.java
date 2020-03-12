2c1088b177aaa79c117cefcb916c73027133d840
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-03-12 14:05:34 +0900

    [refactor] 불필요한 상수 제거

--- src/main/java/domain/Cards.java
+++ src/main/java/domain/Cards.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public class Cards {
     private static final String DUPLICATE_CARD_EXCEPTION_MESSAGE = "Duplicate card exception.";
-    private static final int BLACK_JACK_SCORE = 21;
     private List<Card> cards = new ArrayList<>();
 
     public void add(Card card) {
