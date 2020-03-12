ed020707464ce62e79fd02c72cfd7f1b782bc0f4
chanin <pci2676@gmail.com>
2020-03-13 00:47:36 +0900

    refactor : 누락된 DTO 테스트 추가

--- src/test/java/blackjack/card/domain/CardDeckTest.java
+++ src/test/java/blackjack/card/domain/CardDeckTest.java
@@ -1,19 +1,23 @@
 package blackjack.card.domain;
 
+import blackjack.card.domain.component.CardNumber;
+import blackjack.card.domain.component.Symbol;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CardDeckTest {
 
     @DisplayName("CardDeck 에 더 이상 없을때 뽑으려하면 Exception 발생")
     @Test
-    void drawCard() {
+    void drawCardException() {
         CardDeck cardDeck = CardDeck.getInstance(new CardCreateStrategy() {
             @Override
             public List<Card> getCards() {
@@ -25,5 +29,21 @@ class CardDeckTest {
                 .isInstanceOf(NoSuchElementException.class);
     }
 
+    @DisplayName("CardDeck에서 가장 위에 있는 한장을 뽑아온다.")
+    @Test
+    void testDrawCard() {
+        //given
+        CardDeck cardDeck = CardDeck.getInstance(new CardCreateStrategy() {
+            @Override
+            public List<Card> getCards() {
+                return Arrays.asList(Card.of(Symbol.DIAMOND, CardNumber.ACE));
+            }
+        });
 
+        //when
+        Card expect = cardDeck.drawCard();
+
+        //then
+        assertThat(expect).isEqualTo(Card.of(Symbol.DIAMOND, CardNumber.ACE));
+    }
 }
--- src/test/java/blackjack/view/InputViewTest.java
+++ src/test/java/blackjack/view/InputViewTest.java
@@ -1,17 +1,20 @@
 package blackjack.view;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.Scanner;
-
+import blackjack.card.domain.CardBundle;
+import blackjack.player.domain.Gambler;
+import blackjack.view.dto.DrawRequestDTO;
+import blackjack.view.dto.NamesDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import blackjack.view.dto.NamesDTO;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputViewTest {
 	@DisplayName("이름 입력 테스트")
@@ -41,6 +44,23 @@ class InputViewTest {
 		InputView inputView = new InputView(scanner);
 
 		assertThatThrownBy(inputView::inputPlayNames)
-			.isInstanceOf(IllegalArgumentException.class);
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("카드를 추가로 받을지 말지에 대한 요청 객체 생성")
+	@Test
+	void inputDrawRequest() {
+		//given
+		String input = "y";
+		InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+		Scanner scanner = new Scanner(inputStream);
+
+		InputView inputView = new InputView(scanner);
+
+		//when
+		DrawRequestDTO expect = inputView.inputDrawRequest(new Gambler(new CardBundle(), "bebop"));
+
+		//then
+		assertThat(expect).isInstanceOf(DrawRequestDTO.class);
 	}
 }
