d511caa10efd33faae10e30fb545505baddcb853
gunhee lee <dlrjsgml42@naver.com>
2020-03-12 11:34:17 +0900

    [feat] Answer 클래스 구현
    Answer 유저의 응답을 boolean형으로 저장하는 클래스 구현

---
+++ src/main/java/domain/Answer.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.Objects;
+
+public class Answer {
+    private static final String INPUT_TRUE = "y";
+    private static final String INPUT_FALSE = "n";
+    private final boolean answer;
+
+    public Answer(String userAnswer) {
+        validateValue(userAnswer);
+        answer = INPUT_TRUE.equals(userAnswer);
+    }
+
+    private void validateValue(String userAnswer) {
+        validateNull(userAnswer);
+        if(!INPUT_TRUE.equals(userAnswer) && !INPUT_FALSE.equals(userAnswer)){
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
+    private void validateNull(String userAnswer) {
+        if(Objects.isNull(userAnswer)){
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
+    public boolean isAgree() {
+        return answer;
+    }
+
+    public boolean isDisagree() {
+        return !answer;
+    }
+}
