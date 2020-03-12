055ab6b915db1eb8d0fdd81e0d3764bc67c066f7
hamliet <hamliet@hanyang.ac.kr>
2020-03-12 14:45:17 +0900

    fix: UserFactory 테스트 DisplayName 잘못 된 것 수정

--- src/test/java/factory/UserFactoryTest.java
+++ src/test/java/factory/UserFactoryTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserFactoryTest {
     @Test
-    @DisplayName("PlayerFactory 기능 확인")
+    @DisplayName("UserFactory 기능 확인")
     void create() {
         assertThat(UserFactory.create("UserA,UserB")).isInstanceOf(List.class);
         assertThat(UserFactory.create("UserA,UserB,UserC").size()).isEqualTo(3);
