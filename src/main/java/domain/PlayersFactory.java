5e5765021af0aafa69bf4e48efeaadb05395daed
lalize <printf@kakao.com>
2020-03-12 18:53:23 +0900

    refactor: 코드 컨벤션

--- src/main/java/domain/PlayersFactory.java
+++ src/main/java/domain/PlayersFactory.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class PlayersFactory {
+	private static final String NAME_DELIMITER = ",";
+
 	private PlayersFactory() {
 	}
-
-	private static final String NAME_DELIMITER = ",";
 
 	public static Players create(String userInput) {
 		return Arrays.stream(userInput.split(NAME_DELIMITER))
