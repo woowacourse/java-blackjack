fe9b07fd7c81d76fabf6aa180d3c616feb0d36fa
soring <jnsorn@gmail.com>
2020-03-13 21:04:07 +0900

    refactor: WinOrLose의 value값을 이용하여 문자열 처리

--- src/main/java/domains/result/GameResult.java
+++ src/main/java/domains/result/GameResult.java
@@ -1,63 +1,65 @@
 package domains.result;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import domains.user.Dealer;
 import domains.user.Player;
 import domains.user.Players;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class GameResult {
-    private Map<Player, WinOrLose> gameResult;
+	private Map<Player, WinOrLose> gameResult;
 
-    public GameResult() {
-        this.gameResult = new HashMap<>();
-    }
+	public GameResult() {
+		this.gameResult = new HashMap<>();
+	}
 
-    public void create(Players players, Dealer dealer) {
-        for (Player player : players) {
-            if (checkBurstPlayer(player)) {
-                continue;
-            }
-            gameResult.put(player, WinOrLose.checkWinOrLose(player, dealer));
-        }
-    }
+	public void create(Players players, Dealer dealer) {
+		for (Player player : players) {
+			if (checkBurstPlayer(player)) {
+				continue;
+			}
+			gameResult.put(player, WinOrLose.checkWinOrLose(player, dealer));
+		}
+	}
 
-    private boolean checkBurstPlayer(Player player) {
-        if (player.isBurst()) {
-            gameResult.put(player, WinOrLose.LOSE);
-            return true;
-        }
-        return false;
-    }
+	private boolean checkBurstPlayer(Player player) {
+		if (player.isBurst()) {
+			gameResult.put(player, WinOrLose.LOSE);
+			return true;
+		}
+		return false;
+	}
 
-    public WinOrLose getWinOrLose(Player player) {
-        return gameResult.get(player);
-    }
+	public WinOrLose getWinOrLose(Player player) {
+		return gameResult.get(player);
+	}
 
-    public String calculateDealerResult() {
-        int winCount = 0;
-        int loseCount = 0;
-        int drawCount = 0;
+	public String calculateDealerResult() {
+		int winCount = 0;
+		int loseCount = 0;
+		int drawCount = 0;
 
-        for (WinOrLose result : gameResult.values()) {
-            if (result.equals(WinOrLose.WIN)) {
-                loseCount++;
-                continue;
-            }
-            if (result.equals(WinOrLose.DRAW)) {
-                drawCount++;
-                continue;
-            }
-            if (result.equals(WinOrLose.LOSE)) {
-                winCount++;
-                continue;
-            }
-        }
-        return winCount + "승 " + drawCount + "무 " + loseCount + "패";
-    }
+		for (WinOrLose result : gameResult.values()) {
+			if (result.equals(WinOrLose.WIN)) {
+				loseCount++;
+				continue;
+			}
+			if (result.equals(WinOrLose.DRAW)) {
+				drawCount++;
+				continue;
+			}
+			if (result.equals(WinOrLose.LOSE)) {
+				winCount++;
+				continue;
+			}
+		}
+		return winCount + WinOrLose.WIN.getWinOrLose()
+			+ drawCount + WinOrLose.DRAW.getWinOrLose()
+			+ loseCount + WinOrLose.LOSE.getWinOrLose();
+	}
 
-    public Map<Player, WinOrLose> getGameResult() {
-        return gameResult;
-    }
+	public Map<Player, WinOrLose> getGameResult() {
+		return gameResult;
+	}
 }
