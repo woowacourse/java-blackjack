79009853975263217d289beb1f75c7a76de9301c
soring <jnsorn@gmail.com>
2020-03-13 21:38:38 +0900

    refactor: GameController가 생성되면 바로 게임이 시작되도록 수정

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -7,39 +7,37 @@ import view.InputView;
 import view.OutputView;
 
 class GameController {
-	private Deck deck;
-	private Players players;
-	private Dealer dealer;
-	private GameResult gameResult;
-
 	GameController() {
-		deck = new Deck();
-		gameResult = new GameResult();
+		Deck deck = new Deck();
 		OutputView.printInputPlayerNames();
-		players = new Players(InputView.inputPlayerNames(), deck);
-		dealer = new Dealer(deck);
+		Players players = new Players(InputView.inputPlayerNames(), deck);
+		Dealer dealer = new Dealer(deck);
 		OutputView.printInitialHands(players, dealer);
+
+		run(deck, dealer, players);
 	}
 
-	void run() {
-		hitOrStay();
+	private void run(Deck deck, Dealer dealer, Players players) {
+		hitOrStay(players, deck);
 		dealer.hit(deck);
+
 		if (dealer.handSize() == 3) {
 			OutputView.printDealerHitCard();
 		}
 		OutputView.printAllHands(players, dealer);
 
+		GameResult gameResult = new GameResult();
 		gameResult.create(players, dealer);
 		OutputView.printGameResult(gameResult);
 	}
 
-	private void hitOrStay() {
+	private void hitOrStay(Players players, Deck deck) {
 		for (Player player : players) {
-			needMoreCard(player);
+			needMoreCard(player, deck);
 		}
 	}
 
-	private void needMoreCard(Player player) {
+	private void needMoreCard(Player player, Deck deck) {
 		while (player.needMoreCard(getAnswerForNeedMoreCard(player), deck)) {
 			if (player.checkBurst()) {
 				OutputView.printBurst(player);
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		GameController game = new GameController();
-		game.run();
+		new GameController();
 	}
 }
