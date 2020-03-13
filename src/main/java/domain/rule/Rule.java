8b753aa4ce88e576f3b3b121f25b7cda186bd9f6
lowoon <sangbo0229@gmail.com>
2020-03-13 22:58:29 +0900

    refactor: Rule의 decideWinningReuslt 메소드 구현 수정

--- src/main/java/domain/rule/Rule.java
+++ src/main/java/domain/rule/Rule.java
@@ -1,5 +1,6 @@
 package domain.rule;
 
+import java.util.Arrays;
 import java.util.function.BiFunction;
 
 import domain.result.WinningResult;
@@ -10,6 +11,7 @@ public enum Rule {
     FIRST_USER_BUST((first, second) -> first.isBust(), WinningResult.LOSE),
     SECOND_USER_BUST((first, second) -> second.isBust(), WinningResult.WIN),
     FIRST_USER_GREATER((first, second) -> first.calculatePoint() > second.calculatePoint(), WinningResult.WIN),
+    SECOND_USER_GREATER((first, second) -> first.calculatePoint() < second.calculatePoint(), WinningResult.LOSE),
     FIRST_USER_ONLY_BLACK_JACK((first, second) -> first.isBlackJack() && !second.isBlackJack(), WinningResult.WIN),
     SECOND_USER_ONLY_BLACK_JACK((first, second) -> !first.isBlackJack() && second.isBlackJack(), WinningResult.LOSE),
     EQUALS((first, second) -> first.calculatePoint() == second.calculatePoint(), WinningResult.DRAW);
@@ -23,11 +25,10 @@ public enum Rule {
     }
 
     public static WinningResult decideWinningResult(User first, User second) {
-        for (Rule rule : Rule.values()) {
-            if (rule.condition.apply(first, second)) {
-                return rule.winningResult;
-            }
-        }
-        return WinningResult.LOSE;
+        return Arrays.stream(Rule.values())
+                .filter(rule -> rule.condition.apply(first, second))
+                .findFirst()
+                .get()
+                .winningResult;
     }
 }
