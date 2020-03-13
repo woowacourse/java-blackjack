843f058290050f9244cff6a1efd081a04696cfab
minuyim <minu_yim@naver.com>
2020-03-13 23:34:18 +0900

    refactor : deck create 메소드를 간결하게 작성

--- src/main/java/domain/card/Deck.java
+++ src/main/java/domain/card/Deck.java
@@ -2,15 +2,15 @@ package domain.card;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Stack;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Deck {
-    private static final int INIT_CARDS_SIZE = 2;
+    public static final int INIT_CARDS_SIZE = 2;
 
-    private final Stack<Card> cards;
+    private Stack<Card> cards;
 
     private Deck(Stack<Card> cards) {
         this.cards = cards;
@@ -18,14 +18,13 @@ public class Deck {
 
     public static Deck create() {
         return Arrays.stream(Type.values())
-                .flatMap(type -> createBySymbol(type).stream())
+                .flatMap(Deck::createBySymbol)
                 .collect(Collectors.collectingAndThen(Collectors.toCollection(Stack::new), Deck::new));
     }
 
-    private static List<Card> createBySymbol(Type type) {
+    private static Stream<Card> createBySymbol(Type type) {
         return Arrays.stream(Symbol.values())
-                .map(symbol -> new Card(symbol, type))
-                .collect(Collectors.toList());
+                .map(symbol -> new Card(symbol, type));
     }
 
     private void shuffle() {
