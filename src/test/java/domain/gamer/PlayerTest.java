fee31ff94017eec4e2504ff954c2f36d242763b8
hj <a301dks@naver.com>
2020-03-13 15:10:32 +0900

    refactor: 플레이어가 생성자를 테스트하도록 수정

---
+++ src/test/java/domain/gamer/PlayerTest.java
@@ -0,0 +1,20 @@
+package domain.gamer;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ *    @author AnHyungJu
+ */
+@SuppressWarnings("NonAsciiCharacters")
+public class PlayerTest {
+	@Test
+	void 생성() {
+		Player player = new Player("a");
+		assertThat(player).isNotNull();
+		assertThat(player.getName()).isEqualTo("a");
+		assertThat(player.getHands()).isNotNull();
+	}
+}
