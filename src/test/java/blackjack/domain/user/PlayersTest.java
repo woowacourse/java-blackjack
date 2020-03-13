27907f8815b67591ef9e88fbda8d20ed69e45338
kuenhwi <kueniyam@naver.com>
2020-03-14 00:26:36 +0900

    test: Players 테스트 개편

--- src/test/java/blackjack/domain/user/PlayersTest.java
+++ src/test/java/blackjack/domain/user/PlayersTest.java
@@ -6,6 +6,7 @@ import blackjack.domain.card.Type;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,10 +31,15 @@ class PlayersTest {
 
 		// when
 		players.giveCards(0, card);
+		players.giveCards(2, card, card);
 
 		// then
 		assertThat(players.getPlayers().get(0).getHand())
 				.isEqualTo(Collections.singletonList(card));
+		assertThat(players.getPlayers().get(1).getHand())
+				.isNotEqualTo(Collections.singletonList(card));
+		assertThat(players.getPlayers().get(2).getHand())
+				.isEqualTo(Arrays.asList(card, card));
 	}
 
 	@Test
