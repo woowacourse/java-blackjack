d1139aa5e5f04ddb6c613aa880867154981704ab
김효찬 <kom1230@hanyang.ac.kr>
2020-03-14 00:12:21 +0900

    [refactor] Cards 유효성 검사 추가

--- src/main/java/domain/Cards.java
+++ src/main/java/domain/Cards.java
@@ -4,16 +4,25 @@ import util.BlackJackRule;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Cards {
     private static final String DUPLICATE_CARD_EXCEPTION_MESSAGE = "Duplicate card exception.";
+    public static final String NULL_EXCEPTION_MESSAGE = "Null exception.";
     private List<Card> cards = new ArrayList<>();
 
     public void add(Card card) {
+        validateCard(card);
+        cards.add(card);
+    }
+
+    private void validateCard(Card card) {
         if (cards.contains(card)) {
             throw new IllegalArgumentException(DUPLICATE_CARD_EXCEPTION_MESSAGE);
         }
-        cards.add(card);
+        if (Objects.isNull(card)) {
+            throw new IllegalArgumentException(NULL_EXCEPTION_MESSAGE);
+        }
     }
 
     public int getScore() {
@@ -46,5 +55,11 @@ public class Cards {
 
     public List<Card> getCards() {
         return cards;
+    }
+
+    @Override
+    public String toString() {
+        String string = cards.toString();
+        return string.substring(1, string.length() - 1);
     }
 }
