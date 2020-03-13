b64d34341a7b5e92a6825f395492e1e560d72a27
jamie9504 <jamie9504@kakao.com>
2020-03-13 14:38:36 +0900

    [기능구현] 2단계 - 인터페이스 이용

--- src/main/java/RentCar/Domain/Car.java
+++ src/main/java/RentCar/Domain/Car.java
@@ -1,6 +1,6 @@
 package RentCar.Domain;
 
-public abstract class Car {
+public abstract class Car implements Transportation {
 
     protected final double distance;
 
@@ -9,24 +9,9 @@ public abstract class Car {
     }
 
     /**
-     * 리터당 이동 거리. 즉, 연비
-     */
-    abstract double getDistancePerLiter();
-
-    /**
-     * 여행하려는 거리
-     */
-    abstract double getTripDistance();
-
-    /**
-     * 차종의 이름
-     */
-    abstract String getName();
-
-    /**
      * 주입해야할 연료량을 구한다.
      */
-    double getChargeQuantity() {
+    public double getChargeQuantity() {
         return getTripDistance() / getDistancePerLiter();
     }
 }
---
+++ src/main/java/RentCar/Domain/Transportation.java
@@ -0,0 +1,24 @@
+package RentCar.Domain;
+
+public interface Transportation {
+
+    /**
+     * 리터당 이동 거리. 즉, 연비
+     */
+    double getDistancePerLiter();
+
+    /**
+     * 여행하려는 거리
+     */
+    double getTripDistance();
+
+    /**
+     * 차종의 이름
+     */
+    String getName();
+
+    /**
+     * 주입해야할 연료량을 구한다.
+     */
+    double getChargeQuantity();
+}
