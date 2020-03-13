c6d4c25d268563a55cbd7afa04b208f8024c6e73
giantim <giantim132@gmail.com>
2020-03-13 16:35:25 +0900

    refactor: BlackjackController 리팩토링

--- src/main/java/controller/BlackjackController.java
+++ src/main/java/controller/BlackjackController.java
@@ -4,6 +4,7 @@ import domain.AnswerType;
 import domain.PlayerFactory;
 import domain.WinningResult;
 import domain.card.Cards;
+import domain.player.Player;
 import domain.player.Players;
 import domain.player.User;
 import dto.RequestAnswerDTO;
@@ -16,55 +17,85 @@ import view.OutputView;
 import java.util.List;
 
 public class BlackjackController {
+    private static final int BLACK_JACK = 21;
 
-    public static final int BLACK_JACK = 21;
+    private BlackjackController() {
+    }
 
     public static void run() {
-        RequestPlayerNamesDTO requestPlayerNameDTO = InputView.inputPlayerName();
         PlayerFactory playerFactory = PlayerFactory.getInstance();
         Cards cards = new Cards();
-        Players players = playerFactory.createPlayers(cards, requestPlayerNameDTO.getPlayerName());
+
+        RequestPlayerNamesDTO requestPlayerNamesDTO = inputRequestPlayerNamesDTO();
+        Players players = playerFactory.createPlayers(cards, requestPlayerNamesDTO.getPlayerName());
         List<ResponsePlayerDTO> responsePlayerDTOS = ResponsePlayerDTO.createResponsePlayerDTOs(players);
         OutputView.printInitialResult(responsePlayerDTOS);
 
-        for (User user : players.getUsers()) {
-            AnswerType answerType = getAnswerType(user);
-
-            if (answerType.equals(AnswerType.NO)) {
-                continue;
-            }
-            do {
-                user.insertCard(cards, answerType);
-
-                OutputView.printUserCard(ResponsePlayerDTO.create(user));
-                answerType = validateBlackJack(user, answerType);
-            } while (answerType.equals(AnswerType.YES) && isBlackJack(user));
-        }
-
-        if (players.getDealer().isAdditionalCard(cards)) {
-            OutputView.printDealerAdditionalCard();
-        }
+        pickCard(cards, players);
+        pickCardDealer(cards, players);
 
         OutputView.printFinalResult(ResponsePlayerDTO.createResponsePlayerDTOs(players));
+
         WinningResult winningResult = new WinningResult(players);
         ResponseWinningResultDTO responseWinningResultDTO = ResponseWinningResultDTO.create(
                 winningResult.getWinningResult());
         OutputView.printWinningResult(responseWinningResultDTO.getWinningResult());
     }
 
-    private static AnswerType validateBlackJack(User user, AnswerType answerType) {
+    private static RequestPlayerNamesDTO inputRequestPlayerNamesDTO() {
+        try {
+            return InputView.inputPlayerName();
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return inputRequestPlayerNamesDTO();
+        }
+    }
+
+    private static void pickCard(Cards cards, Players players) {
+        for (User user : players.getUsers()) {
+            choosePickCard(cards, user);
+        }
+    }
+
+    private static void choosePickCard(Cards cards, User user) {
+        AnswerType answerType = getAnswerType(user);
+
+        if (answerType.equals(AnswerType.NO)) {
+            return;
+        }
+
+        do {
+            user.insertCard(cards, answerType);
+
+            OutputView.printUserCard(ResponsePlayerDTO.create(user));
+            answerType = validateBlackJack(user, answerType);
+        } while (AnswerType.YES.equals(answerType) && isBlackJack(user));
+    }
+
+    private static AnswerType validateBlackJack(Player user, AnswerType answerType) {
         if (isBlackJack(user)) {
             answerType = getAnswerType(user);
         }
         return answerType;
     }
 
-    private static boolean isBlackJack(User user) {
+    private static boolean isBlackJack(Player user) {
         return user.sumCardNumber() < BLACK_JACK;
     }
 
-    private static AnswerType getAnswerType(User user) {
-        RequestAnswerDTO requestAnswerDTO = InputView.inputAnswer(ResponsePlayerDTO.create(user));
-        return AnswerType.findAnswerType(requestAnswerDTO.getAnswer());
+    private static AnswerType getAnswerType(Player user) {
+        try {
+            RequestAnswerDTO requestAnswerDTO = InputView.inputAnswer(ResponsePlayerDTO.create(user));
+            return AnswerType.findAnswerType(requestAnswerDTO.getAnswer());
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return getAnswerType(user);
+        }
+    }
+
+    private static void pickCardDealer(Cards cards, Players players) {
+        if (players.getDealer().isAdditionalCard(cards)) {
+            OutputView.printDealerAdditionalCard();
+        }
     }
 }
