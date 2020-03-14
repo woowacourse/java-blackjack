e8228611b8d7cbaad9c9f1673b35423d6dcf140d
ParkDooWon <pass345@naver.com>
2020-03-15 00:51:18 +0900

    refactor : Dto를 불필요하게 재생성하지 않도록 수정

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -71,7 +71,8 @@ public class Controller {
 	}
 
 	private static void end(BlackjackGame blackjackGame) {
-		OutputView.printResult(BlackjackGameDto.from(blackjackGame));
-		OutputView.printMatchResult(BlackjackGameDto.from(blackjackGame));
+		BlackjackGameDto blackjackGameDto = BlackjackGameDto.from(blackjackGame);
+		OutputView.printResult(blackjackGameDto);
+		OutputView.printMatchResult(blackjackGameDto);
 	}
 }
