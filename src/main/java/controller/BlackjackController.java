c9838943f81c9270207e65a283a6fd4ae7e14432
toneyparky <qkrtmddhks95@gmail.com>
2020-03-14 13:32:34 +0900

    refactor: move Deck, Dealer, Players creating logic from Application to BlackjackController

--- src/main/java/controller/BlackjackController.java
+++ src/main/java/controller/BlackjackController.java
@@ -1,17 +1,29 @@
 package controller;
 
 import domain.PlayerIntentionType;
+import domain.card.CardRepository;
 import domain.card.Deck;
 import domain.user.Dealer;
 import domain.user.Player;
 import domain.user.Players;
 import service.BlackjackService;
+import view.InputView;
 
 import static view.InputView.*;
 import static view.OutputView.*;
 
 public class BlackjackController {
-	public static void run(Deck deck, Dealer dealer, Players players) {
+	private final Deck deck;
+	private final Dealer dealer;
+	private final Players players;
+
+	public BlackjackController() {
+		deck = new Deck(CardRepository.toList());
+		dealer = new Dealer();
+		players = Players.of(InputView.inputPlayerNames());
+	}
+
+	public void run() {
 		BlackjackService.giveInitialCards(deck, dealer, players);
 		printInitialStatus(dealer.openCard(), players);
 
@@ -26,19 +38,19 @@ public class BlackjackController {
 		printResult(dealer, players);
 	}
 
-	private static void printResult(Dealer dealer, Players players) {
+	private void printResult(Dealer dealer, Players players) {
 		printResultStatus(dealer.openAllCards(), players);
 		printTotalResult(BlackjackService.createResult(dealer, players), players);
 	}
 
-	private static void proceedExtraDraw(Player player, Deck deck) {
+	private void proceedExtraDraw(Player player, Deck deck) {
 		while (player.canDrawMore() && wantDraw(player)) {
 			BlackjackService.addCard(player, deck);
 			printCardsStatusOf(player);
 		}
 	}
 
-	private static boolean wantDraw(Player player) {
+	private boolean wantDraw(Player player) {
 		return PlayerIntentionType.YES.equals(PlayerIntentionType.of(inputPlayerIntention(player)));
 	}
 }
