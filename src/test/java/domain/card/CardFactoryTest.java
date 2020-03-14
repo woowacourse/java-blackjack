52539f810f4c0a50ed4b2bfd6d78015d97c94342
BEOMJUN KIM <skyblue300a@gmail.com>
2020-03-14 23:22:35 +0900

    refactor: CardFactory의 불필요한 테스트 제거

--- src/test/java/domain/card/CardFactoryTest.java
+++ src/test/java/domain/card/CardFactoryTest.java
@@ -3,6 +3,7 @@ package domain.card;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.HashSet;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,17 +13,5 @@ class CardFactoryTest {
 	@DisplayName("생성된 카드의 갯수를 확인합니다.")
 	void createTest() {
 		assertThat(CardFactory.create()).hasSize(52);
-	}
-
-	@Test
-	@DisplayName("생성된 카드의 갯수를 확인합니다.")
-	void duplicateTest() {
-		assertThat(new HashSet<>(CardFactory.create())).hasSize(52);
-	}
-
-	@Test
-	@DisplayName("동일 타입,심볼을 가진 카드는 같다.")
-	void equalsTest() {
-		assertThat(CardFactory.create()).isEqualTo(CardFactory.create());
 	}
 }
