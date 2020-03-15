66ea3fdb79292714d47ba5e9305ca48e274cb99e
junk-sound <wom2277@naver.com>
2020-03-15 18:19:31 +0900

    refac: 피드백반영 5 - DELIMITER로 분리하는 로직을 domain으로 이동

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,27 +1,20 @@
 package view;
 
+import domain.gamer.PlayerNamesParser;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final String DELIMITER = ",";
     private static final String YES = "y";
     private static final String NO = "n";
 
     public static List<String> inputPlayerNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         String playerNamesValue = scanner.nextLine();
-        List<String> playerNames = Arrays.asList(playerNamesValue.split(DELIMITER));
-
-        for(String playerName : playerNames) {
-            if(isNotNumber(playerName)) {
-                throw new IllegalArgumentException();
-            }
-        }
-
-        return playerNames;
+        return PlayerNamesParser.parse(playerNamesValue);
     }
 
     public static String inputGetMoreCard(String name) {
@@ -37,10 +30,5 @@ public class InputView {
 
     private static boolean isYesOrNo(String input) {
         return YES.equals(input) || NO.equals(input);
-    }
-
-    private static boolean isNotNumber(final String playerNamesValue) {
-        return playerNamesValue.chars()
-                .anyMatch(c -> !Character.isAlphabetic(c));
     }
 }
