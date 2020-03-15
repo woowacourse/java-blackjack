eb5d204886cfb3886d44ebfc198ddde4886f4e89
KS-KIM <nullable@kakao.com>
2020-03-15 23:53:08 +0900

    refactor: 게임 결과를 도출하는 GameRule의 null 예외처리 추가

--- src/main/java/com/blackjack/domain/GameRule.java
+++ src/main/java/com/blackjack/domain/GameRule.java
@@ -3,6 +3,7 @@ package com.blackjack.domain;
 import static java.util.stream.Collectors.toMap;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import com.blackjack.domain.user.Dealer;
@@ -13,15 +14,22 @@ public class GameRule {
 	private final List<User> players;
 
 	public GameRule(Dealer dealer, List<User> players) {
+		validateNullOrEmpty(dealer, players);
 		this.dealer = dealer;
 		this.players = players;
+	}
+
+	private void validateNullOrEmpty(Dealer dealer, List<User> players) {
+		if (Objects.isNull(dealer) || Objects.isNull(players) || players.isEmpty()) {
+			throw new IllegalArgumentException("딜러 또는 플레이어가 존재하지 않습니다.");
+		}
 	}
 
 	public PlayerRecords calculateResult() {
 		return players.stream()
 				.collect(Collectors.collectingAndThen(toMap(
 						player -> player,
-						player -> player.compareScoreWithUser(dealer)
+						player -> player.compareScoreWith(dealer)
 				), PlayerRecords::new));
 	}
 }
