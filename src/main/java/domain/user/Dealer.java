de078b611fb25c7ac04e4dce9e8b140a05179357
YerinCho <forjustice9839@gmail.com>
2020-03-16 14:02:25 +0900

    feat : Dealer
     getAnyCard 에 null, empty 체크 추가

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -5,9 +5,12 @@ import domain.card.Card;
 public class Dealer extends User {
 
     public Card getAnyCard() {
+        if (this.cards == null || this.cards.getCards().isEmpty()) {
+            throw new NullPointerException("접근이 잘못되었거나 카드의 수가 부족합니다.");
+        }
         return this.cards
-            .getCards()
-            .get(0);
+                .getCards()
+                .get(0);
     }
 
     public boolean isScoreSame(int score) {
