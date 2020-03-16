f4f1b62b5bb4b1352476892caec3812eb793b971
begaonnuri <turtlenam0928@gmail.com>
2020-03-16 20:13:49 +0900

    refactor: DrawDecideType 변경
    - isDraw에서 DrawDecideType을 생성하는 로직을 of로 분리

--- src/main/java/com/blackjack/domain/DrawDecideType.java
+++ src/main/java/com/blackjack/domain/DrawDecideType.java
@@ -12,12 +12,18 @@ public enum DrawDecideType {
 		this.symbol = symbol;
 	}
 
-	public static boolean isDraw(String symbol) {
-		Arrays.stream(values())
-				.filter(drawDecideType -> drawDecideType.symbol.equals(symbol))
+	public static DrawDecideType of(String symbol) {
+		return Arrays.stream(values())
+				.filter(drawDecideType -> drawDecideType.isSameOf(symbol))
 				.findAny()
 				.orElseThrow(() -> new IllegalArgumentException("y 또는 n을 입력해주세요."));
+	}
 
-		return DRAW.symbol.equals(symbol);
+	public static boolean isDraw(String symbol) {
+		return DRAW == of(symbol);
+	}
+
+	private boolean isSameOf(String symbol) {
+		return this.symbol.equals(symbol);
 	}
 }
