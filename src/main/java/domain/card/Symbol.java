245b5f4a2ffb6453b5872d71d6ce2a35cefd2dea
toneyparky <qkrtmddhks95@gmail.com>
2020-03-16 12:58:24 +0900

    refactor: add constructor in Symbol for better readability

--- src/main/java/domain/card/Symbol.java
+++ src/main/java/domain/card/Symbol.java
@@ -5,24 +5,28 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public enum Symbol {
-	ACE(1, "A", score -> score <= 11, () -> 1 + 10),
-	TWO(2, "2", score -> true, () -> 2),
-	THREE(3, "3", score -> true, () -> 3),
-	FOUR(4, "4", score -> true, () -> 4),
-	FIVE(5, "5", score -> true, () -> 5),
-	SIX(6, "6", score -> true, () -> 6),
-	SEVEN(7, "7", score -> true, () -> 7),
-	EIGHT(8, "8", score -> true, () -> 8),
-	NINE(9, "9", score -> true, () -> 9),
-	TEN(10, "10", score -> true, () -> 10),
-	JACK(10, "J", score -> true, () -> 10),
-	QUEEN(10, "Q", score -> true, () -> 10),
-	KING(10, "K", score -> true, () -> 10);
+	ACE(1, "A", userScore -> userScore <= 11 && userScore + 11 <= 21, () -> 1 + 10),
+	TWO(2, "2"),
+	THREE(3, "3"),
+	FOUR(4, "4"),
+	FIVE(5, "5"),
+	SIX(6, "6"),
+	SEVEN(7, "7"),
+	EIGHT(8, "8"),
+	NINE(9, "9"),
+	TEN(10, "10"),
+	JACK(10, "J"),
+	QUEEN(10, "Q"),
+	KING(10, "K");
 
 	private final int score;
 	private final String name;
 	private Predicate<Integer> scoreJudge;
 	private Supplier<Integer> scoreConverter;
+
+	Symbol(int score, String name) {
+		this(score, name, userScore -> true, () -> score);
+	}
 
 	Symbol(int score, String name, Predicate<Integer> scoreJudge, Supplier<Integer> scoreConverter) {
 		this.score = score;
