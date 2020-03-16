ab878d6bf8bc680f1bfe52f4e6f8c3ec95395dce
begaonnuri <turtlenam0928@gmail.com>
2020-03-16 23:35:20 +0900

    refactor: Score 변경
    - 점수를 캐싱해서 관리하도록 변경

--- src/main/java/com/blackjack/domain/Score.java
+++ src/main/java/com/blackjack/domain/Score.java
@@ -1,19 +1,33 @@
 package com.blackjack.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Score implements Comparable<Score> {
-	private static final int MIN_SCORE = 1;
-	private static final int MAX_SCORE = 30;
+	private static final int MIN_SCORE = 0;
+	private static final int MAX_SCORE = 21;
+
+	private static final Map<Integer, Score> SCORE_CACHE = new HashMap<>();
+
+	static {
+		for (int i = MIN_SCORE; i <= MAX_SCORE; i++) {
+			SCORE_CACHE.put(i, new Score(i));
+		}
+	}
 
 	private final int score;
 
-	public Score(int score) {
-		validateBounds(score);
+	private Score(int score) {
 		this.score = score;
 	}
 
-	private void validateBounds(int score) {
+	static Score of(int score) {
+		validateBound(score);
+		return SCORE_CACHE.get(score);
+	}
+
+	private static void validateBound(int score) {
 		if (score < MIN_SCORE || score > MAX_SCORE) {
 			throw new IllegalArgumentException("점수의 범위를 벗어났습니다.");
 		}
