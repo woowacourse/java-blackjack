ba06c040382dbb15b23875c6031bef9968655e11
begaonnuri <turtlenam0928@gmail.com>
2020-03-16 17:42:59 +0900

    refactor: GameTable 클래스 생성
    - 게임 진행을 GameTable 객체를 통해 하도록 변경

--- src/main/java/com/blackjack/controller/BlackjackController.java
+++ src/main/java/com/blackjack/controller/BlackjackController.java
@@ -1,25 +1,31 @@
 package com.blackjack.controller;
 
+import static com.blackjack.domain.GameTable.*;
+import static com.blackjack.domain.user.Dealer.*;
 import static com.blackjack.view.InputView.*;
 import static com.blackjack.view.OutputView.*;
 
 import java.util.List;
 
+import com.blackjack.domain.DrawDecideType;
 import com.blackjack.domain.GameRule;
+import com.blackjack.domain.GameTable;
 import com.blackjack.domain.PlayerRecords;
-import com.blackjack.domain.card.CardDeck;
 import com.blackjack.domain.user.Dealer;
 import com.blackjack.domain.user.PlayerFactory;
 import com.blackjack.domain.user.User;
 
 public class BlackjackController {
 	public void run() {
-		CardDeck cardDeck = CardDeck.create();
 		Dealer dealer = new Dealer();
 		List<User> players = createPlayers();
 
-		DrawController drawController = new DrawController();
-		drawController.draw(cardDeck, dealer, players);
+		GameTable gameTable = new GameTable(dealer, players);
+		gameTable.drawAtFirst();
+		printCardsAtFirst(dealer, players, FIRST_DRAW_COUNT);
+
+		drawAllPlayers(gameTable, players);
+		drawDealerUntilEndTurn(gameTable, dealer);
 
 		printUserCards(dealer, players);
 
@@ -47,5 +53,34 @@ public class BlackjackController {
 	private List<User> createPlayers() {
 		String names = inputPlayerNames();
 		return PlayerFactory.createPlayers(names);
+	}
+
+	private void drawDealerUntilEndTurn(GameTable gameTable, User dealer) {
+		while (dealer.canDraw()) {
+			gameTable.draw(dealer);
+			printDealerDrawMessage(DRAW_CONDITION);
+		}
+	}
+
+	private void drawAllPlayers(GameTable gameTable, List<User> players) {
+		for (User player : players) {
+			drawPlayerUntilEndTurn(gameTable, player);
+		}
+	}
+
+	private void drawPlayerUntilEndTurn(GameTable gameTable, User player) {
+		while (canDraw(player) && wantDraw(player)) {
+			gameTable.draw(player);
+			printUserCardInfo(player);
+			System.out.println();
+		}
+	}
+
+	private boolean canDraw(User player) {
+		return player.canDraw();
+	}
+
+	private boolean wantDraw(User player) {
+		return DrawDecideType.isDraw(inputDrawDecideType(player));
 	}
 }
--- src/main/java/com/blackjack/domain/user/User.java
+++ src/main/java/com/blackjack/domain/user/User.java
@@ -5,27 +5,17 @@ import java.util.Collections;
 import com.blackjack.domain.ResultType;
 import com.blackjack.domain.Score;
 import com.blackjack.domain.card.Card;
-import com.blackjack.domain.card.CardDeck;
 
 public abstract class User {
-	public static final int FIRST_DRAW_COUNT = 2;
-
-	private final Name name;
 	final Hand hand;
+	private final Name name;
 
 	User(Name name) {
 		this.hand = new Hand(Collections.emptyList());
 		this.name = name;
 	}
 
-	public void drawAtFirst(CardDeck cardDeck) {
-		for (int count = 0; count < FIRST_DRAW_COUNT; count++) {
-			draw(cardDeck);
-		}
-	}
-
-	public void draw(CardDeck cardDeck) {
-		Card card = cardDeck.pop();
+	public void draw(Card card) {
 		hand.add(card);
 	}
 
