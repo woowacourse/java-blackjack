7428df8fb9bbd7bfb1ff09b96a19ece5d1ed3a1d
김효찬 <kom1230@hanyang.ac.kr>
2020-03-16 21:56:04 +0900

    [feat] 이름 관련 책임을 담당하는 Name/Names 작성

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,43 @@
+package domain;
+
+import java.util.Objects;
+
+public class Name {
+    public static final int MAX_NAME_LENGTH = 100;
+    public static final String EMPTY_OR_NULL_EXCEPTION_MESSAGE = "Empty or null names exception.";
+    public static final String OVER_MAX_NAME_LENGTH_EXCEPTION_MESSAGE = "Too long name exception.";
+
+    private final String name;
+
+    public Name(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (Objects.isNull(name) || name.length() == 0) {
+            throw new IllegalArgumentException(EMPTY_OR_NULL_EXCEPTION_MESSAGE);
+        }
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(OVER_MAX_NAME_LENGTH_EXCEPTION_MESSAGE);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/main/java/domain/Names.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Names {
+    public static final String EMPTY_OR_NULL_NAMES_EXCEPTION_MESSAGE = "Empty or null names exception.";
+    public static final String DELIMITER = ",";
+
+    private List<Name> names;
+
+    public Names(List<Name> names) {
+        validateNames(names);
+        this.names = names;
+    }
+
+    private void validateNames(List<Name> names) {
+        if (Objects.isNull(names) || names.size() == 0) {
+            throw new IllegalArgumentException(EMPTY_OR_NULL_NAMES_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public Names(String input) {
+        this(Arrays.stream(input.trim().split(DELIMITER))
+                .map(String::trim)
+                .map(Name::new)
+                .collect(Collectors.toList()));
+    }
+
+    public List<Name> getNames() {
+        return names;
+    }
+}
