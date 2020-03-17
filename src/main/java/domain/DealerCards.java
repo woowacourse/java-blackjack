292b7bd95930f7366ee01597524d2b721c130116
김효찬 <kom1230@hanyang.ac.kr>
2020-03-17 23:51:53 +0900

    [refactor] 불필요한 클래스 삭제

--- src/main/java/domain/DealerCards.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-import util.BlackJackRule;
-
-public class DealerCards extends Cards {
-    public boolean canHit() {
-        return BlackJackRule.canDealerHit(getScore());
-    }
-}
