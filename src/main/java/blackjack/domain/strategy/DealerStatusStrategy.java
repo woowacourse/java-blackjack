07da0892abd47ef66f4bd9dfcf6a431ee78e58e0
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-03-17 14:37:53 +0900

    delete all

--- src/main/java/blackjack/domain/strategy/DealerStatusStrategy.java
+++
@@ -1,9 +0,0 @@
-package blackjack.domain.strategy;
-
-import blackjack.domain.UserResult;
-import blackjack.domain.player.Dealer;
-import blackjack.domain.player.User;
-
-public interface DealerStatusStrategy {
-    UserResult compute(Dealer dealer, User user);
-}
