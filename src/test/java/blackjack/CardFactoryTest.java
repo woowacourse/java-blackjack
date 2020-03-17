f3a1731530c4954a915395f9a2a1a76f00730085
yeonnseok <ljinsk3@gmail.com>
2020-03-17 16:24:20 +0900

    test: 유실된 테스트 복구

---
+++ src/test/java/blackjack/CardFactoryTest.java
@@ -0,0 +1,23 @@
+package blackjack;
+
+import blackjack.domain.Card;
+import blackjack.domain.CardFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CardFactoryTest {
+    @DisplayName("정적 cards 생성 테스트")
+    @Test
+    void createTest() {
+        List<Card> cards = CardFactory.createCardDeck();
+        Set<Card> deduplicatedCards = new HashSet<>(cards);
+        assertThat(cards.size()).isEqualTo(52);
+        assertThat(cards.size() == deduplicatedCards.size()).isTrue();
+    }
+}
