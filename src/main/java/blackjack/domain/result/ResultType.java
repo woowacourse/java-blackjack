c235aeb854150b68a8a61b8ee5975d1f05d2f547
sujin <sujinemilycho@gmail.com>
2020-03-17 16:59:21 +0900

    feat: 수익률에 따른 수익 계산

--- src/main/java/blackjack/domain/result/ResultType.java
+++ src/main/java/blackjack/domain/result/ResultType.java
@@ -3,16 +3,19 @@ package blackjack.domain.result;
 import java.util.List;
 
 public enum ResultType {
-    WIN("승"),
-    LOSE("패"),
-    DRAW("무");
+    BLACKJACK("블랙잭", 1.5),
+    WIN("승", 1),
+    LOSE("패", -1),
+    DRAW("무", 0);
 
     public static final int BUST = 21;
 
     private final String word;
+    private final double profitRate;
 
-    ResultType(String word) {
+    ResultType(String word, double profitRate) {
         this.word = word;
+        this.profitRate = profitRate;
     }
 
     public static ResultType findResultByScore(int playerScore, int dealerScore) {
@@ -49,5 +52,9 @@ public enum ResultType {
 
     public String getWord() {
         return word;
+    }
+
+    public double getProfitRate() {
+        return profitRate;
     }
 }
