724feb89a8e465b09feb2723e88ca93b97fcb7df
juyeongjung <juyeong.dev@gmail.com>
2020-03-18 13:26:28 +0900

    refactor: 승리 결과 테스트 리팩토링

--- src/test/java/domain/gamer/PlayerTest.java
+++ src/test/java/domain/gamer/PlayerTest.java
@@ -27,6 +27,7 @@ public class PlayerTest {
 
 	@ParameterizedTest
 	@MethodSource("generateCards")
+	@DisplayName("플레이어가 추가로 카드를 뽑을 수 있을지 테스트")
 	public void isDrawableTest(List<Card> cards, boolean expected) {
 		Player player = new Player("pobi", "50");
 		player.addCard(cards);
@@ -45,9 +46,9 @@ public class PlayerTest {
 	}
 
 	@ParameterizedTest
-	@DisplayName("플레이어점수와 딜러점수 비교해서 경기결과내는 테스트")
-	@MethodSource("generateCardsAndDealerScore")
-	void findMatchResultTest(List<Card> cards, List<Card> dealerCards, MatchResult expected) {
+	@MethodSource("generateWinResult")
+	@DisplayName("플레이어가 승리한 경우 테스트")
+	void resultWinTest(List<Card> cards, List<Card> dealerCards, MatchResult expected) {
 		Player player = new Player("pobi", "50");
 		Dealer dealer = new Dealer();
 		player.addCard(cards);
@@ -56,7 +57,7 @@ public class PlayerTest {
 		assertThat(player.findMatchResult(dealer)).isEqualTo(expected);
 	}
 
-	static Stream<Arguments> generateCardsAndDealerScore() {
+	static Stream<Arguments> generateWinResult() {
 		return Stream.of(
 			Arguments.of(Arrays.asList(
 				new Card(CardSuit.CLOVER, CardNumber.EIGHT),
@@ -70,7 +71,24 @@ public class PlayerTest {
 				Arrays.asList(
 					new Card(CardSuit.CLOVER, CardNumber.EIGHT),
 					new Card(CardSuit.CLOVER, CardNumber.TEN),
-					new Card(CardSuit.CLOVER, CardNumber.FOUR)), MatchResult.WIN),
+					new Card(CardSuit.CLOVER, CardNumber.FOUR)), MatchResult.WIN));
+	}
+
+	@ParameterizedTest
+	@MethodSource("generateDrawResult")
+	@DisplayName("플레이어와 딜러가 비긴 경우 테스트")
+	void resultDrawTest(List<Card> cards, List<Card> dealerCards, MatchResult expected) {
+		Player player = new Player("pobi", "50");
+		Dealer dealer = new Dealer();
+		player.addCard(cards);
+		dealer.addCard(dealerCards);
+
+		assertThat(player.findMatchResult(dealer)).isEqualTo(expected);
+
+	}
+
+	static Stream<Arguments> generateDrawResult() {
+		return Stream.of(
 			Arguments.of(Arrays.asList(
 				new Card(CardSuit.CLOVER, CardNumber.TEN),
 				new Card(CardSuit.HEART, CardNumber.KING)),
@@ -78,11 +96,56 @@ public class PlayerTest {
 					new Card(CardSuit.DIAMOND, CardNumber.TEN),
 					new Card(CardSuit.SPADE, CardNumber.KING)), MatchResult.DRAW),
 			Arguments.of(Arrays.asList(
+				new Card(CardSuit.CLOVER, CardNumber.ACE),
+				new Card(CardSuit.HEART, CardNumber.KING)),
+				Arrays.asList(
+					new Card(CardSuit.SPADE, CardNumber.ACE),
+					new Card(CardSuit.SPADE, CardNumber.KING)), MatchResult.DRAW));
+	}
+
+	@ParameterizedTest
+	@MethodSource("generateLoseResult")
+	@DisplayName("플레이어가 패배한 경우 테스트")
+	void resultLoseTest(List<Card> cards, List<Card> dealerCards, MatchResult expected) {
+		Player player = new Player("pobi", "50");
+		Dealer dealer = new Dealer();
+		player.addCard(cards);
+		dealer.addCard(dealerCards);
+
+		assertThat(player.findMatchResult(dealer)).isEqualTo(expected);
+	}
+
+	static Stream<Arguments> generateLoseResult() {
+		return Stream.of(
+			Arguments.of(Arrays.asList(
 				new Card(CardSuit.CLOVER, CardNumber.EIGHT),
 				new Card(CardSuit.CLOVER, CardNumber.TEN)),
 				Arrays.asList(
 					new Card(CardSuit.DIAMOND, CardNumber.TEN),
 					new Card(CardSuit.SPADE, CardNumber.KING)), MatchResult.LOSE),
+			Arguments.of(Arrays.asList(
+				new Card(CardSuit.HEART, CardNumber.KING),
+				new Card(CardSuit.HEART, CardNumber.JACK),
+				new Card(CardSuit.CLOVER, CardNumber.ACE)),
+				Arrays.asList(
+					new Card(CardSuit.SPADE, CardNumber.KING),
+					new Card(CardSuit.SPADE, CardNumber.ACE)), MatchResult.LOSE));
+	}
+
+	@ParameterizedTest
+	@MethodSource("generateBustResult")
+	@DisplayName("플레이어가 버스트인 경우 테스트")
+	void resultBustTest(List<Card> cards, List<Card> dealerCards, MatchResult expected) {
+		Player player = new Player("pobi", "50");
+		Dealer dealer = new Dealer();
+		player.addCard(cards);
+		dealer.addCard(dealerCards);
+
+		assertThat(player.findMatchResult(dealer)).isEqualTo(expected);
+	}
+
+	static Stream<Arguments> generateBustResult() {
+		return Stream.of(
 			Arguments.of(Arrays.asList(
 				new Card(CardSuit.CLOVER, CardNumber.EIGHT),
 				new Card(CardSuit.CLOVER, CardNumber.TEN),
@@ -97,7 +160,23 @@ public class PlayerTest {
 				Arrays.asList(
 					new Card(CardSuit.DIAMOND, CardNumber.EIGHT),
 					new Card(CardSuit.DIAMOND, CardNumber.TEN),
-					new Card(CardSuit.DIAMOND, CardNumber.FOUR)), MatchResult.BUST),
+					new Card(CardSuit.DIAMOND, CardNumber.FOUR)), MatchResult.BUST));
+	}
+
+	@ParameterizedTest
+	@MethodSource("generateBlackJackResult")
+	@DisplayName("플레이어가 블랙잭인 경우 테스트")
+	void resultBlackJackTest(List<Card> cards, List<Card> dealerCards, MatchResult expected) {
+		Player player = new Player("pobi", "50");
+		Dealer dealer = new Dealer();
+		player.addCard(cards);
+		dealer.addCard(dealerCards);
+
+		assertThat(player.findMatchResult(dealer)).isEqualTo(expected);
+	}
+
+	static Stream<Arguments> generateBlackJackResult() {
+		return Stream.of(
 			Arguments.of(Arrays.asList(
 				new Card(CardSuit.CLOVER, CardNumber.ACE),
 				new Card(CardSuit.HEART, CardNumber.KING)),
@@ -108,21 +187,8 @@ public class PlayerTest {
 				new Card(CardSuit.CLOVER, CardNumber.ACE),
 				new Card(CardSuit.HEART, CardNumber.KING)),
 				Arrays.asList(
-					new Card(CardSuit.SPADE, CardNumber.ACE),
-					new Card(CardSuit.SPADE, CardNumber.KING)), MatchResult.DRAW),
-			Arguments.of(Arrays.asList(
-				new Card(CardSuit.CLOVER, CardNumber.ACE),
-				new Card(CardSuit.HEART, CardNumber.KING)),
-				Arrays.asList(
 					new Card(CardSuit.SPADE, CardNumber.KING),
 					new Card(CardSuit.SPADE, CardNumber.JACK),
-					new Card(CardSuit.SPADE, CardNumber.ACE)), MatchResult.BLACKJACK),
-			Arguments.of(Arrays.asList(
-				new Card(CardSuit.HEART, CardNumber.KING),
-				new Card(CardSuit.HEART, CardNumber.JACK),
-				new Card(CardSuit.CLOVER, CardNumber.ACE)),
-				Arrays.asList(
-					new Card(CardSuit.SPADE, CardNumber.KING),
-					new Card(CardSuit.SPADE, CardNumber.ACE)), MatchResult.LOSE));
+					new Card(CardSuit.SPADE, CardNumber.ACE)), MatchResult.BLACKJACK));
 	}
 }
