a57b4a5a21707b415d02cd4fb2ea76acf08db5d5
icyMojito <dev.icymojito@gmail.com>
2020-03-18 18:32:49 +0900

    test : 블랙잭 참가자와 관련된 클래스의 테스트 수정
    BlackjackUser 클래스를 상속받은 클래스라면 생성자의 매개변수로 Null이나 빈 값이 들어왔을 때 예외가 발생해야 하므로,
    생성자 예외처리 테스트를 PlayerTest가 아니라 조상 클래스인 BlackjackUserTest로 옮김

--- src/test/java/domain/user/BlackjackUserTest.java
+++ src/test/java/domain/user/BlackjackUserTest.java
@@ -1,16 +1,25 @@
 package domain.user;
 
 import domain.card.CardDeck;
-import domain.card.CardFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class BlackjackUserTest {
 
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 이름으로_Null혹은빈값이_들어왔을때_예외처리_테스트(String input) {
+        Assertions.assertThatThrownBy(() -> new Player(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 입력입니다.");
+    }
+
     @Test
     void 뽑은_카드가_존재할때_첫번째_드로우_시도시_예외발생_확인_테스트() {
-        CardDeck cardDeck = CardFactory.createCardDeck();
+        CardDeck cardDeck = new CardDeck();
         Dealer dealer = new Dealer();
         dealer.draw(cardDeck);
 
--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -2,8 +2,6 @@ package domain.user;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class PlayerTest {
@@ -13,13 +11,5 @@ public class PlayerTest {
         Player player = new Player("KIM");
 
         Assertions.assertThat(player).hasFieldOrPropertyWithValue("name", "KIM");
-    }
-
-    @ParameterizedTest
-    @NullAndEmptySource
-    void 생성_예외처리_테스트(String input) {
-        Assertions.assertThatThrownBy(() -> new Player(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("잘못된 입력입니다.");
     }
 }
