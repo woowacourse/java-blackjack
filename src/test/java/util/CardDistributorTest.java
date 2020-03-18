6a6524eb28752300b515e0bdff3f6292a35e97c3
hamliet <hamliet@hanyang.ac.kr>
2020-03-18 13:52:30 +0900

    feat: CardDistributor 카드 2장 분배 테스트 추가

--- src/test/java/util/CardDistributorTest.java
+++ src/test/java/util/CardDistributorTest.java
@@ -12,13 +12,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CardDistributorTest {
     @Test
     @DisplayName("카드 분배 확인")
+    void distributeCards() {
+        final CardDeck cardDeck = new CardDeck(CardFactory.create());
+        final User user = new User("user");
+
+        CardDistributor.distributeCards(cardDeck, user);
+        assertThat(user.getCardSize()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("카드 낱장 주기 확인")
     void giveOneCard() {
         final CardDeck cardDeck = new CardDeck(CardFactory.create());
         final Dealer dealer = new Dealer();
         final User user = new User("user");
-
-        assertThat(dealer.getCardSize()).isEqualTo(0);
-        assertThat(user.getCardSize()).isEqualTo(0);
 
         CardDistributor.giveOneCard(cardDeck, dealer);
         CardDistributor.giveOneCard(cardDeck, user);
