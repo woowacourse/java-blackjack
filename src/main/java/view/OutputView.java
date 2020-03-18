bb9240352d1ac19058fb7d95e3b634eb8bde73e0
lowoon <sangbo0229@gmail.com>
2020-03-18 14:29:35 +0900

    refactor: OutputView 출력 형식 통일

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -13,10 +13,8 @@ import view.dto.UserDto;
 public class OutputView {
 
     public static final String NEWLINE = System.getProperty("line.separator");
-    public static final String COMMA = ", ";
-    private static final String CARD = "카드: ";
-    private static final String RESULT = " - 결과: ";
-    private static final String COLON = ": ";
+    public static final String DELIMITER = ", ";
+    private static final int FIRST_CARD = 0;
 
     public static void printFirstDealOutResult(UserDto dealerDto, PlayersDto playersDto) {
         printFirstDealOut(playersDto);
@@ -28,15 +26,15 @@ public class OutputView {
         String allNames = playersDto.getPlayers()
                 .stream()
                 .map(UserDto::getName)
-                .collect(Collectors.joining(COMMA));
+                .collect(Collectors.joining(DELIMITER));
 
-        System.out.printf(NEWLINE + "딜러와 %s에게 2장을 나누었습니다." + NEWLINE, allNames);
+        System.out.printf(NEWLINE + "%s와 %s에게 2장을 나누었습니다." + NEWLINE, Dealer.NAME, allNames);
     }
 
     private static void printDealerFirstDealOutResult(UserDto dealerDto) {
-        String dealerFirstDealOutResult =
-                Dealer.NAME + CARD + dealerDto.getCards().get(0).getType() + dealerDto.getCards().get(0).getSymbol();
-        System.out.println(dealerFirstDealOutResult);
+        Card firstCard = dealerDto.getCards().get(FIRST_CARD);
+        String dealerFirstDealOutResult = firstCard.getType() + firstCard.getSymbol();
+        System.out.printf("%s카드: %s"+ NEWLINE, Dealer.NAME, dealerFirstDealOutResult);
     }
 
     private static void printPlayersFirstDealOutResult(PlayersDto playersDto) {
@@ -56,29 +54,29 @@ public class OutputView {
     public static void printTotalResult(GameResultDto gameResultDto) {
         Map<UserDto, Integer> cardPoint = gameResultDto.getUserToCardPoint();
         System.out.println();
-        cardPoint.forEach((user, point) -> System.out.println(printUserDealOutResult(user) + RESULT + point));
+        cardPoint.forEach((user, point) -> System.out.printf("%s - 결과: %d" + NEWLINE, printUserDealOutResult(user), point));
 
         printTotalProfit(gameResultDto);
     }
 
     private static void printTotalProfit(GameResultDto gameResultDto) {
         System.out.println(NEWLINE + "## 최종 수익");
-        System.out.printf("%s%s%.0f" + NEWLINE, Dealer.NAME, COLON, gameResultDto.getProfitOfDealer());
+        System.out.printf("%s: %.0f" + NEWLINE, Dealer.NAME, gameResultDto.getProfitOfDealer());
         gameResultDto.getProfitOfPlayers()
-                .forEach((key, value) -> System.out.printf("%s%s%.0f" + NEWLINE, key.getName(), COLON, value));
+                .forEach((key, value) -> System.out.printf("%s: %.0f" + NEWLINE, key.getName(), value));
     }
 
     private static String printUserDealOutResult(UserDto userDto) {
-        return userDto.getName() + CARD + cardsJoin(userDto);
+        return String.format("%s카드: %s", userDto.getName(), cardsJoin(userDto));
     }
 
     private static String cardsJoin(UserDto userDto) {
-        return String.join(COMMA, printCard(userDto.getCards()));
+        return String.join(DELIMITER, printCard(userDto.getCards()));
     }
 
     private static String printCard(List<Card> cards) {
         return cards.stream()
                 .map(card -> card.getType() + card.getSymbol())
-                .collect(Collectors.joining(COMMA));
+                .collect(Collectors.joining(DELIMITER));
     }
 }
