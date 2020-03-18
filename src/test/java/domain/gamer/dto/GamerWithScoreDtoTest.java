32ee843865e5f723ce57e1623629c4d06df75095
minuyim <minu_yim@naver.com>
2020-03-18 16:52:51 +0900

    refactor : GamerWithScoreDtoTest DisplayName 추가

--- src/test/java/domain/gamer/dto/GamerWithScoreDtoTest.java
+++ src/test/java/domain/gamer/dto/GamerWithScoreDtoTest.java
@@ -6,6 +6,7 @@ import domain.card.Symbol;
 import domain.card.Type;
 import domain.gamer.Player;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
@@ -23,24 +24,27 @@ class GamerWithScoreDtoTest {
     }
 
     @Test
+    @DisplayName("생성 테스트")
     void of() {
         assertThat(GamerWithCardsDto.of(new Player(new PlayingCards(Collections.emptyList()), "testName"))).isNotNull();
     }
 
     @Test
+    @DisplayName("getter - name")
     void getName() {
         assertThat(gamerWithScoreDto.getName()).isEqualTo("testName");
     }
 
     @Test
+    @DisplayName("getter - Cards")
     void getCards() {
         List<Card> cards = Collections.singletonList(new Card(Symbol.QUEEN, Type.CLOVER));
         assertThat(gamerWithScoreDto.getCards().get(0)).isEqualTo(cards.get(0));
     }
 
     @Test
+    @DisplayName("getter - Score")
     void getScore() {
-        List<Card> cards = Collections.singletonList(new Card(Symbol.QUEEN, Type.CLOVER));
         assertThat(gamerWithScoreDto.getScore()).isEqualTo(10);
     }
 }
