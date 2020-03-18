fb77173f7b455d12e01e11905b7ba386181d1440
junk-sound <wom2277@naver.com>
2020-03-18 09:43:33 +0900

    refac: Dealer의 이름을 상수로 추출

--- src/main/java/domain/gamer/Dealer.java
+++ src/main/java/domain/gamer/Dealer.java
@@ -4,9 +4,10 @@ import domain.card.PlayingCards;
 
 public class Dealer extends Gamer {
     private static final int ADD_THRESHOLD = 17;
+    private static final String NAME = "딜러";
 
     public Dealer(PlayingCards playingCards) {
-        super(playingCards, "딜러");
+        super(playingCards, NAME);
     }
 
     public boolean canGetExtraCard() {
