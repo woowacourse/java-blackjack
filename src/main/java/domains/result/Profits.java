c250174eb58f4ef567a62a4f87ad5351a08e12d5
hwangbo <taekwonod714@naver.com>
2020-03-18 17:05:51 +0900

    feat: ProfitsFactory 클래스 생성

--- src/main/java/domains/result/Profits.java
+++ src/main/java/domains/result/Profits.java
@@ -3,22 +3,13 @@ package domains.result;
 import domains.user.Player;
 import domains.user.money.ProfitMoney;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class Profits {
     private Map<Player, ProfitMoney> playerProfits;
 
     public Profits(GameResult gameResult) {
-        playerProfits = new HashMap<>();
-        create(gameResult.getPlayerResult());
-    }
-
-    private void create(Map<Player, ResultType> gameResult) {
-        for (Player player : gameResult.keySet()) {
-            ResultType resultType = gameResult.get(player);
-            playerProfits.put(player, resultType.calculateProfitMoney(player));
-        }
+        this.playerProfits = ProfitsFactory.create(gameResult);
     }
 
     public ProfitMoney createDealerProfit() {
