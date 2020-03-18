0c6396e77dd28c9d6b18a457d4799ece4d7d280e
juyeongjung <juyeong.dev@gmail.com>
2020-03-18 15:11:49 +0900

    test : Money 객체 Test code 추가

--- src/main/java/domain/gamer/Money.java
+++ src/main/java/domain/gamer/Money.java
@@ -1,5 +1,7 @@
 package domain.gamer;
 
+import java.util.Objects;
+
 import exception.InvalidMoneyException;
 
 public class Money {
@@ -39,5 +41,22 @@ public class Money {
 
 	public int getMoney() {
 		return money;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Money money1 = (Money)o;
+		return money == money1.money;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(money);
 	}
 }
