529f25eae2f6953885bcf9741c9c432dfe57e7bc
hwookim <gusdn9754@gmail.com>
2020-03-18 21:52:22 +0900

    refactor: 불필요 코드 제거

--- src/test/java/domain/user/BettingMoneyTest.java
+++ src/test/java/domain/user/BettingMoneyTest.java
@@ -1,21 +1,14 @@
 package domain.user;
 
-import static org.assertj.core.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class BettingMoneyTest {
-
-    private BettingMoney bettingMoney;
-
-    @BeforeEach
-    void setUp() {
-        bettingMoney = new BettingMoney(1000);
-    }
 
     @Test
     @DisplayName("생성 확인")
