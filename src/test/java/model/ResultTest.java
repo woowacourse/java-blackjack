068baef3f9185a331f077dad5f8d4ab4cc6d8bbf
dd <dqrd123@gmail.com>
2020-03-18 14:34:30 +0900

    refactor: Result 테스트 구현

--- src/test/java/model/ResultTest.java
+++ src/test/java/model/ResultTest.java
@@ -1,54 +1,72 @@
 package model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static controller.BlackJackGame.ADDITIONAL_DRAW_COUNT;
-import static controller.BlackJackGame.INITIAL_DRAW_COUNT;
-import static model.UserTest.PLAYER_NAME;
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResultTest {
-    CardHand bustHand1 = new CardHand();
-    CardHand bustHand2 = new CardHand();
-    CardHand notBustHand1 = new CardHand();
-    Deck bustDeck1;
-    Deck bustDeck2;
-    Deck notBustDeck1;
+    public static final String PLAYER_NAME = "DD";
+    Player bustPlayer = new Player(PLAYER_NAME, Arrays.asList(
+            new Card(Symbol.KING, Type.DIAMOND),
+            new Card(Symbol.QUEEN, Type.DIAMOND),
+            new Card(Symbol.TWO, Type.DIAMOND)
+    ));
+    Player notBustPlayer = new Player(PLAYER_NAME, Arrays.asList(
+            new Card(Symbol.KING, Type.DIAMOND),
+            new Card(Symbol.QUEEN, Type.DIAMOND),
+            new Card(Symbol.ACE, Type.DIAMOND)
+    ));
+    Player blackJackPlayer = new Player(PLAYER_NAME, Arrays.asList(
+            new Card(Symbol.QUEEN, Type.CLUB),
+            new Card(Symbol.ACE, Type.HEART)
+    ));
+    Dealer bustDealer = new Dealer(Arrays.asList(
+            new Card(Symbol.KING, Type.DIAMOND),
+            new Card(Symbol.QUEEN, Type.DIAMOND),
+            new Card(Symbol.TWO, Type.DIAMOND)
+    ));
+    Dealer notBustDealer = new Dealer(Arrays.asList(
+            new Card(Symbol.KING, Type.DIAMOND),
+            new Card(Symbol.QUEEN, Type.DIAMOND),
+            new Card(Symbol.ACE, Type.DIAMOND)
+    ));
+    Dealer blackJackDealer = new Dealer(Arrays.asList(
+            new Card(Symbol.QUEEN, Type.CLUB),
+            new Card(Symbol.ACE, Type.HEART)
+    ));
 
-    @BeforeEach
-    void init_field() {
-        bustHand1.addCard(new Card(Symbol.KING, Type.DIAMOND));
-        bustHand1.addCard(new Card(Symbol.QUEEN, Type.DIAMOND));
-        bustHand1.addCard(new Card(Symbol.JACK, Type.DIAMOND));
-        bustHand2.addCard(new Card(Symbol.NINE, Type.DIAMOND));
-        bustHand2.addCard(new Card(Symbol.KING, Type.DIAMOND));
-        bustHand2.addCard(new Card(Symbol.KING, Type.DIAMOND));
-        notBustHand1.addCard(new Card(Symbol.KING, Type.DIAMOND));
-        notBustHand1.addCard(new Card(Symbol.JACK, Type.DIAMOND));
-
-        bustDeck1 = new Deck(bustHand1);
-        bustDeck2 = new Deck(bustHand2);
-        notBustDeck1 = new Deck(notBustHand1);
-    }
-
+    @DisplayName("딜러와 플레이어 모두 21을 넘긴 경우 LOSE")
     @Test
-    @DisplayName("딜러와 플레이어 모두 21을 넘긴 경우 플레이 DRAW")
-    void both_isBust_Test() {
-        Player player = new Player(PLAYER_NAME, bustDeck1, INITIAL_DRAW_COUNT);
-        Dealer dealer = new Dealer(bustDeck2, INITIAL_DRAW_COUNT);
-        player.drawCard(bustDeck1, ADDITIONAL_DRAW_COUNT);
-        dealer.drawCard(bustDeck2, ADDITIONAL_DRAW_COUNT);
-        assertThat(Result.compete(dealer, player) == Result.DRAW).isTrue();
+    void bothBustTest() {
+        assertThat(Result.compete(bustDealer, bustPlayer)).isEqualTo(Result.LOSE);
     }
 
     @Test
     @DisplayName("딜러만 21을 넘긴 경우 플레이어 WIN")
-    void Dealer_isBust_Test() {
-        Player player = new Player(PLAYER_NAME, notBustDeck1, INITIAL_DRAW_COUNT);
-        Dealer dealer = new Dealer(bustDeck1, INITIAL_DRAW_COUNT);
-        dealer.drawCard(bustDeck1, ADDITIONAL_DRAW_COUNT);
-        assertThat(Result.compete(dealer, player) == Result.WIN).isTrue();
+    void dealerBustTest() {
+        assertThat(Result.compete(bustDealer, notBustPlayer)).isEqualTo(Result.WIN);
     }
+
+    @Test
+    @DisplayName("딜러와 플레이어 모두 21인 경우 DRAW")
+    void bothBlackJackTest() {
+        assertThat(Result.compete(blackJackDealer, blackJackPlayer)).isEqualTo(Result.DRAW);
+    }
+
+    @Test
+    @DisplayName("딜러만 블랙잭인 경우 플레이어 Lose")
+    void dealerBlackJackTest() {
+        assertThat(Result.compete(blackJackDealer, notBustPlayer)).isEqualTo(Result.LOSE);
+    }
+
+//    @Test
+//    @DisplayName("블랙잭 우승 시 수익 계산")
+//    void playerBlackJackRevenueTest(){
+//        Result result = Result.compete(notBustDealer, blackJackPlayer);
+//        assertThat(result.calculateRevenue(blackJackPlayer)).isEqualTo(150);
+//    }
+
 }
