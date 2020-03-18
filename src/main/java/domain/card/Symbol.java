4b267c50250fd3aed2eeb1e09cdcd1a95372f9b4
junk-sound <wom2277@naver.com>
2020-03-18 16:40:33 +0900

    refac: symbol에 calculate 로직을 만듬

--- src/main/java/domain/card/Symbol.java
+++ src/main/java/domain/card/Symbol.java
@@ -1,26 +1,28 @@
 package domain.card;
 
 public enum Symbol {
-    ACE(1, "A"),
-    TWO(2, "2"),
-    THREE(3, "3"),
-    FOUR(4, "4"),
-    FIVE(5, "5"),
-    SIX(6, "6"),
-    SEVEN(7, "7"),
-    EIGHT(8, "8"),
-    NINE(9, "9"),
-    TEN(10, "10"),
-    JACK(10, "J"),
-    QUEEN(10, "Q"),
-    KING(10, "K");
+    ACE(1, "A", new AceCalculator()),
+    TWO(2, "2", (sum) -> 2),
+    THREE(3, "3", (sum) -> 3),
+    FOUR(4, "4", (sum) -> 4),
+    FIVE(5, "5", (sum) -> 5),
+    SIX(6, "6", (sum) -> 6),
+    SEVEN(7, "7", (sum) -> 7),
+    EIGHT(8, "8", (sum) -> 8),
+    NINE(9, "9", (sum) -> 9),
+    TEN(10, "10", (sum) -> 10),
+    JACK(10, "J", (sum) -> 10),
+    QUEEN(10, "Q", (sum) -> 10),
+    KING(10, "K", (sum) -> 10);
 
     private int value;
     private String pattern;
+    private SymbolCalculator calculator;
 
-    Symbol(int value, String pattern) {
+    Symbol(int value, String pattern, SymbolCalculator calculator) {
         this.value = value;
         this.pattern = pattern;
+        this.calculator = calculator;
     }
 
     public int getValue() {
@@ -29,5 +31,34 @@ public enum Symbol {
 
     public String getPattern() {
         return pattern;
+    }
+
+    int calculate(int sum) {
+        return calculator.calculate(sum);
+    }
+
+    interface SymbolCalculator {
+        int calculate(int sum);
+    }
+
+    private static class AceCalculator implements SymbolCalculator {
+        /*
+         * JOKER_VALUE는 특정 상황에서 ACE를 크게 계산하기 때문에, 새롭게 지은 명칭입니다. 특정 상황에서 ACE의 value라고 생각하면 됩니다. */
+        private static final int JOKER_VALUE = 11;
+        private static final int BLACKJACK = 21;
+
+        @Override
+        public int calculate(int sum) {
+            if (cabBeJoker(sum)) {
+                return JOKER_VALUE;
+            }
+            return ACE.value;
+        }
+
+        private boolean cabBeJoker(int sum) {
+            return sum <= BLACKJACK - JOKER_VALUE;
+        }
+
+
     }
 }
--- src/test/java/domain/card/SymbolTest.java
+++ src/test/java/domain/card/SymbolTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SymbolTest {
+class SymbolTest {
     @Test
     @DisplayName("Symbol 객체가 잘 생성되는 지 확인")
     void valueOf() {
@@ -22,5 +22,24 @@ public class SymbolTest {
     @DisplayName("Symbol value를 확인하는 테스트 코드")
     void getValue() {
         assertThat(Symbol.KING.getValue()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("#calculate() : should return value")
+    void calculateWithDefaultCalculation() {
+        int sum = 11;
+        for (Symbol symbol : Symbol.values()) {
+            int score = symbol.calculate(sum);
+            assertThat(score).isEqualTo(symbol.getValue());
+        }
+    }
+
+    @Test
+    @DisplayName("#calculate() : should return value + ADDITIONAL_ACE_VALUE with ACE")
+    void calculateAceWithSmallSum() {
+        int smallSum = 10;
+        int aceAdditionalValue = 10;
+        int score = Symbol.ACE.calculate(smallSum);
+        assertThat(score).isEqualTo(Symbol.ACE.getValue() + aceAdditionalValue);
     }
 }
