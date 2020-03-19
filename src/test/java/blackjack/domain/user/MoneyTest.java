217fc851f2c67c10ea6367d9ef63995dba6a5c22
Andy H. Jung <commaniakr@gmail.com>
2020-03-19 15:15:01 +0900

    feat: 돈이 입력되지 않는 Money 객체 생성 테스트

--- src/test/java/blackjack/domain/user/MoneyTest.java
+++ src/test/java/blackjack/domain/user/MoneyTest.java
@@ -4,19 +4,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MoneyTest {
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {10000, 30000, 100000})
     @DisplayName("Money 생성하고 값 불러오기 테스트")
-    void moneyGenerateTest() {
-        assertThat(new Money(10000).getMoney()).isEqualTo(10000);
+    void moneyGenerateTest(int value) {
+        assertThat(new Money(value).getMoney()).isEqualTo(value);
+    }
+
+    @Test
+    @DisplayName("돈이 없는 Money 객체 생성하기 테스트")
+    void zeroMoneyTest() {
+        assertThat(new Money().getMoney()).isEqualTo(0);
     }
 
     @ParameterizedTest
-    @CsvSource(value= {"5000,1,5000", "10000,1.5,15000", "30000,-1,-30000", "50000,0,0"})
+    @CsvSource(value = {"5000,1,5000", "10000,1.5,15000", "30000,-1,-30000", "50000,0,0"})
     @DisplayName("Money 곱하기 테스트")
     void moneyMultiplyTest(int moneyAmount, double rate, int expected) {
         Money money = new Money(moneyAmount);
@@ -24,7 +31,7 @@ public class MoneyTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value= {"0,5000,5000", "100000,-100000,0", "30000,45000,75000"})
+    @CsvSource(value = {"0,5000,5000", "100000,-100000,0", "30000,45000,75000"})
     @DisplayName("Money 더하기 테스트")
     void moneyAddTest(int moneyAmount, int addedMoney, int expected) {
         Money money = new Money(moneyAmount);
@@ -32,7 +39,7 @@ public class MoneyTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value= {"0,5000,-5000", "100000,-100000,200000", "30000,30000,0"})
+    @CsvSource(value = {"0,5000,-5000", "100000,-100000,200000", "30000,30000,0"})
     @DisplayName("Money 빼기 테스트")
     void moneySubtractTest(int moneyAmount, int subtractedMoney, int expected) {
         Money money = new Money(moneyAmount);
