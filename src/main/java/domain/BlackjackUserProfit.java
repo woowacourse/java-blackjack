036314504648d9eeadc0a03e85748d60ee2b96b6
icyMojito <dev.icymojito@gmail.com>
2020-03-19 16:06:29 +0900

    refactor : 플레이어 수익 계산 메서드 수정
    딜러가 이겼을 때 플레이어는 베팅금을 잃는 규칙이므로 별도의 손해금 계산 로직이 필요없어짐

--- src/main/java/domain/BlackjackUserProfit.java
+++ src/main/java/domain/BlackjackUserProfit.java
@@ -13,8 +13,7 @@ import static domain.util.NullValidator.validateNull;
 public class BlackjackUserProfit {
     public static final double DEFAULT_PROFIT = 0.0;
     public static final double BLACKJACK_WIN_PROFIT_RATE = 1.5;
-    public static final double BUST_WIN_PROFIT_RATE = 1.0;
-    public static final double SCORE_WIN_PROFIT_RATE = 1.0;
+    public static final double WIN_PROFIT_RATE = 1.0;
 
     private final Map<String, Double> playerProfit;
 
@@ -40,7 +39,7 @@ public class BlackjackUserProfit {
             return calculatePlayerGain(player);
         }
         if (dealer.isWinner(player)) {
-            return calculatePlayerLoss(player);
+            return -player.getBettingMoney();
         }
         return DEFAULT_PROFIT;
     }
@@ -49,17 +48,7 @@ public class BlackjackUserProfit {
         if (player.isBlackjack()) {
             return player.getBettingMoney() * BLACKJACK_WIN_PROFIT_RATE;
         }
-        if (player.isNotBust()) {
-            return player.getBettingMoney() * BUST_WIN_PROFIT_RATE;
-        }
-        return player.getBettingMoney() * SCORE_WIN_PROFIT_RATE;
-    }
-
-    private double calculatePlayerLoss(Player player) {
-        if (player.isBust()) {
-            return -(player.getBettingMoney() * BUST_WIN_PROFIT_RATE);
-        }
-        return -(player.getBettingMoney() * SCORE_WIN_PROFIT_RATE);
+        return player.getBettingMoney() * WIN_PROFIT_RATE;
     }
 
     public double calculateDealerProfitSum() {
