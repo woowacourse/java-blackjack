ad6839cbd48c356ef01528867d643bb9cf9b58d5
kouz <lastpuzly@gmail.com>
2020-03-19 17:31:20 +0900

    refactor: make User to abstract class

--- src/test/java/domain/user/UserTest.java
+++ src/test/java/domain/user/UserTest.java
@@ -1,35 +1,36 @@
 package domain.user;
 
 import domain.card.Card;
+import domain.card.Cards;
 import domain.card.Symbol;
 import domain.card.Type;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
-
 	@Test
 	void addCard() {
 		Card card = new Card(Symbol.ACE, Type.CLOVER);
-		User user = User.getInstance();
+		User user = new Player("a", new ArrayList<>());
 		user.receive(card);
 
-		assertEquals(user, User.of(Collections.singletonList(card)));
+		assertEquals(user.hands.toList(), (Collections.singletonList(card)));
 	}
 
 	@Test
 	void isNotBlackjack_When_User_Has_Blackjack_Return_False() {
-		User user = User.of(List.of(new Card(Symbol.KING, Type.CLOVER), new Card(Symbol.ACE, Type.CLOVER)));
+		User user = new Player("a", List.of(new Card(Symbol.KING, Type.CLOVER), new Card(Symbol.ACE, Type.CLOVER)));
 		assertFalse(user.isNotBlackJack());
 	}
 
 	@Test
 	void isNotBlackjack_When_User_Has_Blackjack_Score_But_Size_Is_Three_Return_True() {
-		User user = User.of(List.of(
+		User user = new Player("a", List.of(
 			new Card(Symbol.NINE, Type.CLOVER),
 			new Card(Symbol.ACE, Type.CLOVER),
 			new Card(Symbol.ACE, Type.DIAMOND))
