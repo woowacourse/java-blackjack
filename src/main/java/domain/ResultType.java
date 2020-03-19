d03a738ad46a373b41c6e332e994ea5107c34e8a
kouz <lastpuzly@gmail.com>
2020-03-19 17:25:38 +0900

    refactor: add calculate prize money function

--- src/main/java/domain/ResultType.java
+++ src/main/java/domain/ResultType.java
@@ -1,31 +1,55 @@
 package domain;
 
+import domain.card.Cards;
 import domain.user.User;
 
 import java.util.Arrays;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public enum ResultType {
-	WIN("승", scoreGap -> scoreGap > 0),
-	DRAW("무", scoreGap -> scoreGap == 0),
-	LOSE("패", scoreGap -> scoreGap < 0);
+	BLACKJACK_WIN(
+		scoreGap -> scoreGap > 0,
+		cards -> cards.isInitialSize() && cards.getPoint() == 21,
+		money -> money.doubleValue() * 1.5
+	),
+	WIN(
+		scoreGap -> scoreGap > 0,
+		Integer::doubleValue
+	),
+	DRAW(
+		scoreGap -> scoreGap == 0,
+		cards -> cards.getPoint() != 0,
+		money -> money.doubleValue() * 0
+	),
+	LOSE(
+		scoreGap -> scoreGap <= 0,
+		money -> money.doubleValue() * -1
+	);
 
-	private final String name;
 	private final Predicate<Integer> resultJudge;
+	private final Predicate<Cards> blackjackOrBurstJudge;
+	private final Function<Integer, Double> getPrize;
 
-	ResultType(String name, Predicate<Integer> resultJudge) {
-		this.name = name;
+	ResultType(Predicate<Integer> resultJudge, Function<Integer, Double> getPrize) {
+		this(resultJudge, cards -> true, getPrize);
+	}
+
+	ResultType(Predicate<Integer> resultJudge, Predicate<Cards> blackjackOrBurstJudge, Function<Integer, Double> getPrize) {
 		this.resultJudge = resultJudge;
+		this.blackjackOrBurstJudge = blackjackOrBurstJudge;
+		this.getPrize = getPrize;
 	}
 
 	public static ResultType from(User result, User compared) {
 		return Arrays.stream(ResultType.values())
 			.filter(type -> type.resultJudge.test(result.getScoreMinusBy(compared)))
+			.filter(type -> type.blackjackOrBurstJudge.test(result.openAllCards()))
 			.findFirst()
 			.orElseThrow(NullPointerException::new);
 	}
 
-	public String getName() {
-		return name;
+	public double getProfit(int bettingMoney) {
+		return getPrize.apply(bettingMoney);
 	}
 }
