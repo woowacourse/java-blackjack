b4b26db853d97070cb89bbfc1c34f53ff76e0d18
icyMojito <dev.icymojito@gmail.com>
2020-03-19 15:47:15 +0900

    refactor : Controller 클래스 수정
    - 새로 추가된 요구사항을 수행할 수 있도록 코드 추가
    - 이해하기 쉽도록 메서드명을 직관적으로 수정

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,77 +1,88 @@
 package controller;
 
-import domain.GameResult;
+import domain.BettingMoney;
+import domain.BlackjackUserProfit;
 import domain.PlayerAnswer;
 import domain.card.CardDeck;
-import domain.card.CardFactory;
 import domain.user.Dealer;
 import domain.user.Player;
 import domain.user.PlayerNames;
+import domain.user.Players;
 import view.InputView;
 import view.OutputView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Controller {
-    private CardDeck cardDeck = CardFactory.createCardDeck();
+    private CardDeck cardDeck = new CardDeck();
 
     public void run() {
+        PlayerNames playerNames = new PlayerNames(Arrays.asList(InputView.requestName()));
+        Players players = new Players(createPlayers(playerNames));
         Dealer dealer = new Dealer();
-        List<Player> players = createPlayers();
 
         drawFirstCards(dealer, players);
-        for (Player player : players) {
-            drawMoreCards(player);
-        }
+        drawMoreCards(players);
         drawMoreCards(dealer);
 
-        passResult(dealer, players);
+        openBlackjackUserCards(dealer, players);
+
+        displayBlackjackUserProfit(dealer, players);
+    }
+
+    private List<Player> createPlayers(PlayerNames playerNames) {
+        List<Player> players = new ArrayList<>();
+        for (String name : playerNames.get()) {
+            String bettingMoneyInput = InputView.requestBettingMoney(name);
+            Player player = new Player(name, new BettingMoney(bettingMoneyInput));
+            players.add(player);
+        }
+        return players;
+    }
+
+    private void drawFirstCards(Dealer dealer, Players players) {
+        OutputView.printFirstDrawMessage(dealer, players.get());
+        dealer.firstDraw(cardDeck);
+        OutputView.printFirstCardOnly(dealer.getName(), dealer.getCards());
+        for (Player player : players.get()) {
+            player.firstDraw(cardDeck);
+            OutputView.printCards(player.getName(), player.getCards());
+        }
     }
 
     private void drawMoreCards(Dealer dealer) {
         while (dealer.isDrawable()) {
-            OutputView.printAutoDraw(dealer);
+            OutputView.printDealerDraw(dealer.getName());
             dealer.draw(cardDeck);
         }
     }
 
-    private void drawMoreCards(Player player) {
-        while (player.isDrawable() && askPlayerDraw(player)) {
-            player.draw(cardDeck);
-            OutputView.printStatus(player.getStatus());
+    private void drawMoreCards(Players players) {
+        for (Player player : players.get()) {
+            while (player.isDrawable() && isAgreePlayerDraw(player)) {
+                player.draw(cardDeck);
+                OutputView.printCards(player.getName(), player.getCards());
+            }
         }
     }
 
-    private boolean askPlayerDraw(Player player) {
+    private boolean isAgreePlayerDraw(Player player) {
         OutputView.printCardFormat(player.getName());
-        PlayerAnswer playerAnswer = InputView.requestDraw();
-        return playerAnswer.isAgree();
+        PlayerAnswer playerAnswer = PlayerAnswer.of(InputView.requestDraw());
+        return playerAnswer.isYes();
     }
 
-    private void passResult(Dealer dealer, List<Player> players) {
-        OutputView.printStatusWithScore(dealer.getStatus(), dealer.getScore());
-        for (Player player : players) {
-            OutputView.printStatusWithScore(player.getStatus(), player.getScore());
-        }
-        OutputView.printGameResult(new GameResult(players, dealer), dealer);
-    }
-
-    private void drawFirstCards(Dealer dealer, List<Player> players) {
-        OutputView.printDrawTurn(dealer, players);
-        dealer.firstDraw(cardDeck);
-        OutputView.printStatus(dealer.getFirstStatus());
-        for (Player player : players) {
-            player.firstDraw(cardDeck);
-            OutputView.printStatus(player.getStatus());
+    private void openBlackjackUserCards(Dealer dealer, Players players) {
+        OutputView.printCardsWithScore(dealer.getName(), dealer.getCards(), dealer.getScore());
+        for (Player player : players.get()) {
+            OutputView.printCardsWithScore(player.getName(), player.getCards(), player.getScore());
         }
     }
 
-    private List<Player> createPlayers() {
-        PlayerNames playerNames = new PlayerNames(Arrays.asList(InputView.requestName()));
-        return playerNames.get().stream()
-                .map(Player::new)
-                .collect(Collectors.toList());
+    private void displayBlackjackUserProfit(Dealer dealer, Players players) {
+        BlackjackUserProfit blackjackUserProfit = new BlackjackUserProfit(players, dealer);
+        OutputView.printBlackjackUserProfit(blackjackUserProfit, dealer.getName());
     }
 }
