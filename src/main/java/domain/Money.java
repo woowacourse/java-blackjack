fcf7be3cc21d05e7440ef2f8c7a437f4425e4bbb
ordinCode <ysh1346@naver.com>
2020-03-19 14:39:09 +0900

    style

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -4,6 +4,7 @@ public class Money {
 	public static final int ZERO_MONEY = 0;
 	public static final int MINUS_CONVERTER = -1;
 	private static final String ERROR_MESSAGE_UNDER_ZERO = "0보다 작은 수를 입력하였습니다.";
+
 	private final double money;
 
 	public Money(double money) {
