a222cc291ec205d61e70cea9926d6718fece85a5
icyMojito <dev.icymojito@gmail.com>
2020-03-19 15:13:01 +0900

    feat : 베팅금을 담당하는 BettingMoney 클래스와 테스트 생성
    - 베팅금 입력값에 대한 검증과 불변성 보장을 위해 BettingMoney 클래스 생성
    - 베팅금은 블랙잭 참가자 중 플레이어에게만 필요하므로 Player 클래스에 멤버 변수로 넣음
    또한 플레이어는 자신의 베팅금을 알려줄 책임이 있기 때문에 이를 수행할 메서드 추가

--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -1,15 +1,19 @@
 package domain.user;
 
+import domain.BettingMoney;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class PlayerTest {
 
-    @Test
-    void 플레이어_생성_테스트() {
-        Player player = new Player("KIM");
+    @ParameterizedTest
+    @CsvSource(value = {"10000,10000", "55550,55550", "3,3"})
+    void 베팅금을_정상적으로_반환하는지_테스트(String input, int expected) {
+        Player player = new Player("KIM", new BettingMoney(input));
+        int result = player.getBettingMoney();
 
-        Assertions.assertThat(player).hasFieldOrPropertyWithValue("name", "KIM");
+        Assertions.assertThat(result).isEqualTo(expected);
     }
 }
