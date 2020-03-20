2632ed984d4b9ec1fddd99507dcb62ee1e9bae5f
junk-sound <wom2277@naver.com>
2020-03-20 13:59:22 +0900

    test: Card - isAce()

--- src/test/java/domain/card/CardTest.java
+++ src/test/java/domain/card/CardTest.java
@@ -1,21 +1,14 @@
 package domain.card;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CardTest {
-    private Card aceCard;
-    private Card kingCard;
-
-    @BeforeEach
-    void setUp() {
-        aceCard = new Card(Symbol.valueOf("ACE"), Type.valueOf("HEART"));
-        kingCard = new Card(Symbol.valueOf("KING"), Type.valueOf("HEART"));
-    }
 
     @Test
     @DisplayName("Symbol과 Type을 인자로 갖는 Card가 생성된다.")
@@ -34,12 +27,26 @@ public class CardTest {
     }
 
     @Test
-    @DisplayName(("#calculateExceptAce() :should throw IllegalStateException with Ace"))
+    @DisplayName("#calculateExceptAce() : should throw IllegalStateException with Ace")
     void calculateExceptAceFail() {
         Symbol symbol = Symbol.ACE;
         Card card = new Card(symbol, Type.SPADE);
         assertThatThrownBy(card::calculateExceptAce)
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage(String.format(Card.INVALID_ACE_MEESAGE, symbol.getPattern()));
+    }
+
+    @Test
+    @DisplayName("#isAce() : should return true")
+    void isAceTrue() {
+        Card card = new Card(Symbol.ACE, Type.SPADE);
+        assertTrue(card.isAce());
+    }
+
+    @Test
+    @DisplayName("#isAce() : should return false")
+    void isAceFalse() {
+        Card card = new Card(Symbol.KING, Type.SPADE);
+        assertFalse(card.isAce());
     }
 }
