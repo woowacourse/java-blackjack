10d70f2f0d5535ce7367be45fce7ab547189ab99
junk-sound <wom2277@naver.com>
2020-03-20 16:07:46 +0900

    refac: Players join

--- src/main/java/domain/user/Players.java
+++ src/main/java/domain/user/Players.java
@@ -1,4 +1,28 @@
 package domain.user;
 
+import common.PlayerDto;
+import common.PlayersDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Players {
+    private final List<Player> players;
+
+    private Players(List<Player> players) {
+        this.players = players;
+    }
+
+    private static Players of(List<Player> players) {
+        return new Players(players);
+    }
+
+    public static Players join(PlayersDto playersDto) {
+        List<Player> players = new ArrayList<>();
+        for (PlayerDto playerDto : playersDto.getPlayerDtos()) {
+            Player player = Player.join(playerDto);
+            players.add(player);
+        }
+        return of(players);
+    }
 }
