d7aa677a0af2f3315ed6753fc11d1c739572edfc
김예림 <kiel0103@naver.com>
2020-03-21 17:50:51 +0900

    refactor: Result 객체로 최종수익을 계산할 수 있도록 구현함

--- src/main/java/domain/result/Result.java
+++ src/main/java/domain/result/Result.java
@@ -1,14 +1,23 @@
 package domain.result;
 
+import java.util.function.Function;
+
 public enum Result {
-    WIN("승"),
-    DRAW("무"),
-    LOSE("패");
+    BLACKJACK_WIN("승", betAmount -> (int) (betAmount * 1.5)),
+    WIN("승", betAmount -> betAmount),
+    DRAW("무", betAmount -> 0),
+    LOSE("패", betAmount -> -betAmount);
 
     private final String korean;
+    private final Function<Integer, Integer> getRevenue;
 
-    Result(final String resultKorean) {
+    Result(final String resultKorean, final Function<Integer, Integer> getRevenue) {
         this.korean = resultKorean;
+        this.getRevenue = getRevenue;
+    }
+
+    public int revenueOf(int betAmount) {
+        return getRevenue.apply(betAmount);
     }
 
     public Result getOpponentResult() {
