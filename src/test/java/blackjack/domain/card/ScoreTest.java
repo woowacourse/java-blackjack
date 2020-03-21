173448fd6ccb2f20742e554815a7477c451ccba2
kuenhwi <kueniyam@naver.com>
2020-03-22 00:37:04 +0900

    test: ScoreTest에 DisplayName 추가

--- src/test/java/blackjack/domain/card/ScoreTest.java
+++ src/test/java/blackjack/domain/card/ScoreTest.java
@@ -2,6 +2,7 @@ package blackjack.domain.card;
 
 import blackjack.domain.card.exceptions.ScoreException;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -26,6 +27,7 @@ class ScoreTest {
 		eleven = Score.of(11);
 	}
 
+	@DisplayName("of()가 0 이상인 수가 들어왔을 때 인스턴스를 생성하는지 테스트")
 	@ParameterizedTest
 	@MethodSource("of_ZeroOrPlusNum_IsNotNull")
 	void of_ZeroOrPlusNum_IsNotNull(Score score) {
@@ -36,12 +38,14 @@ class ScoreTest {
 		return Stream.of(zero, five, six, eleven);
 	}
 
+	@DisplayName("of()가 마이너스 숫자가 들어왔을 때 예외를 던지는지 테스트")
 	@Test
 	void of_MinusNum_ThrowScoreException() {
 		assertThatThrownBy(() -> Score.of(-1))
 				.isInstanceOf(ScoreException.class);
 	}
 
+	@DisplayName("add()가 적절하게 더하는지 테스트")
 	@ParameterizedTest
 	@MethodSource("add_TwoScore_ReturnAddedScore")
 	void add_TwoScore_ReturnAddedScore(Score a, Score b, Score expect) {
@@ -55,6 +59,7 @@ class ScoreTest {
 				Arguments.of(five, six, eleven));
 	}
 
+	@DisplayName("isUnder()이 자신보다 작은 값이 들어왔을때 true를 반환하는지 테스트")
 	@ParameterizedTest
 	@MethodSource("isUnder_ScoreSmallerThanNum_ReturnTrue")
 	void isUnder_ScoreSmallerThanNum_ReturnTrue(Score score, int num) {
@@ -69,6 +74,7 @@ class ScoreTest {
 				Arguments.of(eleven, 12));
 	}
 
+	@DisplayName("isUnder()이 자신보다 큰 값이 들어왔을때 false를 반환하는지 테스트")
 	@ParameterizedTest
 	@MethodSource("isUnder_ScoreSameOrBiggerThanNum_ReturnFalse")
 	void isUnder_ScoreSameOrBiggerThanNum_ReturnFalse(Score score, int num) {
@@ -84,6 +90,7 @@ class ScoreTest {
 				Arguments.of(eleven, 11));
 	}
 
+	@DisplayName("isOver()가 자신보다 큰 값이 들어왔을때 true를 반환하는지 테스트")
 	@ParameterizedTest
 	@MethodSource("isOver_ScoreBiggerThanNum_ReturnTrue")
 	void isOver_ScoreBiggerThanNum_ReturnTrue(Score score, int num) {
@@ -98,6 +105,7 @@ class ScoreTest {
 				Arguments.of(eleven, 5));
 	}
 
+	@DisplayName("isOver()가 자신보다 작은 값이 들어왔을때 true를 반환하는지 테스트")
 	@ParameterizedTest
 	@MethodSource("isOver_ScoreSameOrSmallerThanNum_ReturnFalse")
 	void isOver_ScoreSameOrSmallerThanNum_ReturnFalse(Score score, int num) {
@@ -111,18 +119,5 @@ class ScoreTest {
 				Arguments.of(eleven, 11),
 				Arguments.of(eleven, 12),
 				Arguments.of(eleven, 18));
-	}
-
-	@ParameterizedTest
-	@MethodSource("getScore_Score_IsEqualToExpect")
-	void getScore_Score_IsEqualToExpect(Score score, int expect) {
-		assertThat(score.getScore()).isEqualTo(expect);
-	}
-
-	static Stream<Arguments> getScore_Score_IsEqualToExpect() {
-		return Stream.of(Arguments.of(zero, 0),
-				Arguments.of(five, 5),
-				Arguments.of(six, 6),
-				Arguments.of(eleven, 11));
 	}
 }
