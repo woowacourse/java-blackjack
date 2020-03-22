fc93d3d1829d1831d86a19b005982f1cc331ef3e
Seyun <ksy90101@gmail.com>
2020-03-22 21:08:45 +0900

    refactor : 카드 결과 출력 시 딜러가 먼저 나오도록 리팩토링

--- src/main/java/domain/gamer/CardsResult.java
+++ src/main/java/domain/gamer/CardsResult.java
@@ -2,18 +2,15 @@ package domain.gamer;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class CardsResult {
-    private Map<Gamer, Integer> gamersCardResult;
+    private final Map<Gamer, Integer> gamersCardResult = new LinkedHashMap<>();
 
     public CardsResult(Gamers gamers) {
-        Dealer dealer = gamers.getDealer();
-        this.gamersCardResult = gamers.getPlayers().stream()
-                .collect(Collectors.toMap(gamer -> gamer, gamer -> gamer.score.getScore(),
-                        (a, b) -> a, LinkedHashMap::new));
+        gamersCardResult.put(gamers.getDealer(), gamers.getDealer().score.getScore());
 
-        gamersCardResult.put(dealer, dealer.score.getScore());
+        gamers.getPlayers()
+                .forEach(gamer -> gamersCardResult.put(gamer, gamer.score.getScore()));
     }
 
     public Map<Gamer, Integer> getGamersCardResult() {
