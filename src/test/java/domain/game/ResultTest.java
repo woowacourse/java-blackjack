17de7d4908dce66680dbaa8bad1852b8b211aa65
hamliet <hamliet@hanyang.ac.kr>
2020-03-22 09:24:31 +0900

    feat: 수익률 테스트 추가

--- src/test/java/domain/game/ResultTest.java
+++ src/test/java/domain/game/ResultTest.java
@@ -1,7 +1,17 @@
 package domain.game;
 
+import domain.card.Card;
+import domain.player.Dealer;
+import domain.player.Name;
+import domain.player.User;
+import domain.player.Users;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import util.ResultGenerator;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -52,5 +62,81 @@ public class ResultTest {
         result.addLoseCount();
         result.addWinCount();
         assertThat(result.hasWin()).isTrue();
+    }
+
+    @Test
+    @DisplayName("수익률 테스트 - 유저 블랙잭 승리시 1.5배의 수익금")
+    void calculateWinningMoney_WhenBlackJackWin_Get150PercentMoney() {
+        User user = new User("user");
+        Dealer dealer = new Dealer();
+
+        user.addBettingMoney(new Money("1000"));
+        user.addCard(Card.of("스페이드", "10"));
+        user.addCard(Card.of("스페이드", "A"));
+        dealer.addCard(Card.of("스페이드", "9"));
+        dealer.addCard(Card.of("스페이드", "K"));
+
+        Results results = ResultGenerator.create(dealer, new Users(Arrays.asList(user)));
+        Result userResult = results.getResults().get(new Name("user"));
+        BigDecimal userWinningMoney = userResult.getWinningMoney();
+
+        assertThat(userWinningMoney.setScale(0, RoundingMode.FLOOR)).isEqualTo("1500");
+    }
+
+    @Test
+    @DisplayName("수익률 테스트 - 블랙잭 무승부시 배팅금액 반환")
+    void calculateWinningMoney_WhenBlackJackDraw_GetBackBettingMoney() {
+        User user = new User("user");
+        Dealer dealer = new Dealer();
+
+        user.addBettingMoney(new Money("1000"));
+        user.addCard(Card.of("스페이드", "10"));
+        user.addCard(Card.of("스페이드", "A"));
+        dealer.addCard(Card.of("하트", "Q"));
+        dealer.addCard(Card.of("하트", "A"));
+
+        Results results = ResultGenerator.create(dealer, new Users(Arrays.asList(user)));
+        Result userResult = results.getResults().get(new Name("user"));
+        BigDecimal userWinningMoney = userResult.getWinningMoney();
+
+        assertThat(userWinningMoney.setScale(0, RoundingMode.FLOOR)).isEqualTo("0");
+    }
+
+    @Test
+    @DisplayName("수익률 테스트 - 무승부시 배팅금액 반환")
+    void calculateWinningMoney_WhenDraw_GetBackBettingMoney() {
+        User user = new User("user");
+        Dealer dealer = new Dealer();
+
+        user.addBettingMoney(new Money("1000"));
+        user.addCard(Card.of("스페이드", "5"));
+        user.addCard(Card.of("스페이드", "4"));
+        dealer.addCard(Card.of("스페이드", "3"));
+        dealer.addCard(Card.of("스페이드", "6"));
+
+        Results results = ResultGenerator.create(dealer, new Users(Arrays.asList(user)));
+        Result userResult = results.getResults().get(new Name("user"));
+        BigDecimal userWinningMoney = userResult.getWinningMoney();
+
+        assertThat(userWinningMoney.setScale(0, RoundingMode.FLOOR)).isEqualTo("0");
+    }
+
+    @Test
+    @DisplayName("수익률 테스트 - 승리시 1배의 수익금")
+    void calculateWinningMoney_WhenNormalWin_Get100PercentMoney() {
+        User user = new User("user");
+        Dealer dealer = new Dealer();
+
+        user.addBettingMoney(new Money("1000"));
+        user.addCard(Card.of("스페이드", "5"));
+        user.addCard(Card.of("스페이드", "4"));
+        dealer.addCard(Card.of("스페이드", "3"));
+        dealer.addCard(Card.of("스페이드", "5"));
+
+        Results results = ResultGenerator.create(dealer, new Users(Arrays.asList(user)));
+        Result userResult = results.getResults().get(new Name("user"));
+        BigDecimal userWinningMoney = userResult.getWinningMoney();
+
+        assertThat(userWinningMoney.setScale(0, RoundingMode.FLOOR)).isEqualTo("1000");
     }
 }
