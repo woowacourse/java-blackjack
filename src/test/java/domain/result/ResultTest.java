569bbee7241d463e507e7ad05b42ec5da2ee7b20
junk-sound <wom2277@naver.com>
2020-03-23 16:50:27 +0900

    refac: ResultTest에서 mock 제거

--- src/test/java/domain/result/ResultTest.java
+++ src/test/java/domain/result/ResultTest.java
@@ -1,8 +1,6 @@
 package domain.result;
 
 import domain.user.Money;
-import domain.user.Dealer;
-import domain.user.Player;
 import domain.user.Profit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,52 +10,25 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyDouble;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 class ResultTest {
     @ParameterizedTest
-    @MethodSource({"getCasesForJudge"})
-    void judge(Result expected) {
-        MatchRule mockMatchRule = mock(MatchRule.class);
-        Player mockPlayer = mock(Player.class);
-        Dealer mockDealer = mock(Dealer.class);
-        when(mockMatchRule.match(mockPlayer, mockDealer)).thenReturn(expected);
-
-        Result actual = Result.judge(mockMatchRule, mockPlayer, mockDealer);
-        assertThat(actual).isEqualTo(expected);
-        verify(mockMatchRule).match(mockPlayer, mockDealer);
-    }
-
-    private static Stream<Arguments> getCasesForJudge() {
-        return Stream.of(
-                Arguments.of(Result.PLAYER_WIN_WITH_BLACKJACK),
-                Arguments.of(Result.PLAYER_WIN_WITHOUT_BLACKJACk),
-                Arguments.of(Result.DEALER_WIN),
-                Arguments.of(Result.DRAW)
-        );
-    }
-
-    @ParameterizedTest
     @MethodSource({"getCasesForTesingCalculateProfit"})
     @DisplayName("#calculateProfit() : should return profit")
-    void calculateProfit(Result result) {
-        Money money = mock(Money.class);
-        when(money.multiply(anyDouble())).thenReturn(money);
+    void calculateProfit(Result result, double rate) {
+        Money money = Money.of(1000);
 
         Profit profit = result.calculateProfit(money);
-        assertThat(profit).isEqualTo(new Profit(money));
+        assertThat(profit).isEqualTo(new Profit(money.multiply(rate)));
 
     }
 
     private static Stream<Arguments> getCasesForTesingCalculateProfit() {
         return Stream.of(
-                Arguments.of(Result.PLAYER_WIN_WITHOUT_BLACKJACk),
-                Arguments.of(Result.PLAYER_WIN_WITH_BLACKJACK),
-                Arguments.of(Result.DRAW),
-                Arguments.of(Result.DEALER_WIN)
+                Arguments.of(Result.PLAYER_WIN_WITHOUT_BLACKJACk, 1),
+                Arguments.of(Result.PLAYER_WIN_WITH_BLACKJACK, 1.5),
+                Arguments.of(Result.DRAW, 0),
+                Arguments.of(Result.DEALER_WIN, 1)
         );
     }
 }
