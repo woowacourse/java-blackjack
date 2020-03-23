d2e0076dea7f5106a66215f6fdaf0e1e58720ef6
aegis <aegis1920@gmail.com>
2020-03-23 21:26:08 +0900

    feat : Name 클래스 추가 및 테스트 코드 작성

---
+++ src/test/java/blackjack/domain/participants/NameTest.java
@@ -0,0 +1,20 @@
+package blackjack.domain.participants;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class NameTest {
+    @DisplayName("이름이 제대로 생성되는지 생성자 테스트")
+    @NullAndEmptySource
+    @Test
+    void nameConstructorTest(String input) {
+        assertThatThrownBy(() -> {
+            new Name(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
