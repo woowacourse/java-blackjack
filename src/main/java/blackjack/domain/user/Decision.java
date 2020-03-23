c814d5b7fc00217987bcb0537916117d9b4a388b
Andy H. Jung <commaniakr@gmail.com>
2020-03-23 17:30:16 +0900

    refactor: Decision 대,소문자 인식 로직 수정

--- src/main/java/blackjack/domain/user/Decision.java
+++ src/main/java/blackjack/domain/user/Decision.java
@@ -3,25 +3,22 @@ package blackjack.domain.user;
 import java.util.Arrays;
 
 public enum Decision {
-    Y("Y", "y", true),
-    N("N", "n", false);
+    HIT("y", true),
+    STAND("n", false);
 
     private static final String INVALID_INPUT= "올바르지 않은 입력입니다.";
 
-    private String uppercase;
-    private String lowercase;
+    private String character;
     private boolean intended;
 
-    Decision(String uppercase, String lowercase, boolean intended) {
-        this.uppercase = uppercase;
-        this.lowercase = lowercase;
+    Decision(String character, boolean intended) {
+        this.character = character;
         this.intended = intended;
     }
 
     public static boolean chosenBy(String decision) {
         return Arrays.stream(values())
-                .filter(x -> x.uppercase.equals(decision) ||
-                        x.lowercase.equals(decision))
+                .filter(x -> x.character.equals(decision.toLowerCase()))
                 .findFirst()
                 .map(a -> a.intended)
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_INPUT));
