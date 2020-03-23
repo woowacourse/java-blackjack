d7d9266c33ff44a1bdc150e74830beb9995a6b52
김효찬 <kom1230@hanyang.ac.kr>
2020-03-23 09:23:45 +0900

    [refactor] stream 가독성 좋게 수정

--- src/main/java/domain/gambler/Players.java
+++ src/main/java/domain/gambler/Players.java
@@ -1,20 +1,29 @@
 package domain.gambler;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 public class Players {
     private static final int MAXIMUM_PLAYER_COUNT = 7;
     private static final int MINIMUM_PLAYER_COUNT = 1;
     private static final String NULL_EXCEPTION_MESSAGE = "Null exception.";
     private static final String OUT_OF_RANGE_EXCEPTION_MESSAGE = "Player number out of range exception. (1,7)";
+
     private final List<Player> players;
 
     public Players(Map<Name, Money> playerInfo) {
         validatePlayerInfo(playerInfo);
-        this.players = Collections.unmodifiableList(playerInfo.keySet().stream()
-                .map(playerName -> new Player(playerName, playerInfo.get(playerName)))
-                .collect(Collectors.toList()));
+        this.players = playerInfo.entrySet()
+                .stream()
+                .map(entry -> new Player(entry.getKey(), entry.getValue()))
+                .collect(collectingAndThen(
+                        toList(),
+                        Collections::unmodifiableList));
     }
 
     private void validatePlayerInfo(Map<Name, Money> playerInfo) {
@@ -31,9 +40,11 @@ public class Players {
     }
 
     public List<String> getPlayerNames() {
-        return Collections.unmodifiableList(players.stream()
+        return players.stream()
                 .map(Player::getName)
                 .map(Name::toString)
-                .collect(Collectors.toList()));
+                .collect(collectingAndThen(
+                        toList(),
+                        Collections::unmodifiableList));
     }
 }
