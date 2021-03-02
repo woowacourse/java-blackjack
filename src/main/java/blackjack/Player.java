87ad846c06e87ab2231f8279cd0f86b33a232844
xrabcde <xrabcde@gmail.com>
2021-03-02 18:52:59 +0900

    feat(Player): 참가자가 카드를 받는 기능 구현

--- src/main/java/blackjack/Player.java
+++ src/main/java/blackjack/Player.java
@@ -1,9 +1,22 @@
 package blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
     private final String name;
+    private final List<Card> myCards;
 
     public Player(final String name) {
         this.name = name;
+        this.myCards = new ArrayList<>();
+    }
+
+    public void receiveCard(final Card card) {
+        myCards.add(card);
+    }
+
+    public int getCardCount() {
+        return myCards.size();
     }
 }
--- src/test/java/blackjack/PlayerTest.java
+++ src/test/java/blackjack/PlayerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PlayerTest {
     @Test
@@ -11,5 +12,13 @@ public class PlayerTest {
     void create() {
         assertThatCode(() -> new Player("bada"))
             .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("참가자가 카드를 받았는지 확인")
+    void receiveCard() {
+        Player player = new Player("joel");
+        player.receiveCard(new Card(CardNumber.ACE, CardType.CLOVER));
+        assertThat(player.getCardCount()).isEqualTo(1);
     }
 }
