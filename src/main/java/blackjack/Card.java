747a156f8771b1ec374dc8458cb8e737b7e1fa80
joanne <mathmjseo@naver.com>
2021-03-02 19:20:39 +0900

    feat: Card 객체 생성

---
+++ src/main/java/blackjack/Card.java
@@ -0,0 +1,17 @@
+package blackjack;
+
+public class Card {
+
+
+    private final String name;
+    private final String value;
+
+    private Card(final String name, final String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public static Card create(final String symbol, final String value) {
+        return new Card(symbol, value);
+    }
+}
