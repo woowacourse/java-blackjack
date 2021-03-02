a4e7f7cc07f0557aa6a0b17fca4eb1acfb642726
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-02 19:29:23 +0900

    feat: 플레이어 결과 반환 구현 추가

---
+++ src/main/java/blackjack/domain/Player.java
@@ -0,0 +1,42 @@
+package blackjack.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private static final int ACE_DIFF = 10;
+    private static final int BLACKJACK = 21;
+
+    private final List<Card> cards;
+
+    public Player() {
+        cards = new ArrayList<>();
+    }
+
+    public void drawCard(final Card card) {
+        cards.add(card);
+    }
+
+    public int getResult() {
+        int lowValue = getLowValue();
+        int highValue = getHighValue(lowValue);
+        if (highValue > BLACKJACK) {
+            return lowValue;
+        }
+        return highValue;
+    }
+
+    private int getLowValue() {
+        return cards.stream()
+            .mapToInt(card->card.getCardNumber().getValue())
+            .sum();
+    }
+
+    private int getHighValue(int lowValue) {
+        int highValue = lowValue;
+        if (cards.stream().anyMatch(card -> card.getCardNumber() == CardNumber.ACE)) {
+            highValue += ACE_DIFF;
+        }
+        return highValue;
+    }
+}
