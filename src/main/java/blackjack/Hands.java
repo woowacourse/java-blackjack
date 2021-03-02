1df8ad3cce1c19a27bf23b33416838c576bcef90
joanne <mathmjseo@naver.com>
2021-03-02 20:11:00 +0900

    feat: Hands에 카드 추가 기능 구현

--- src/main/java/blackjack/Hands.java
+++ src/main/java/blackjack/Hands.java
@@ -1,5 +1,6 @@
 package blackjack;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Hands {
@@ -8,5 +9,13 @@ public class Hands {
 
     public Hands(final List<Card> cards) {
         this.cards = cards;
+    }
+
+    public void add(Card card) {
+        cards.add(card);
+    }
+
+    public List<Card> toList() {
+        return Collections.unmodifiableList(cards);
     }
 }
--- src/test/java/blackjack/HandsTest.java
+++ src/test/java/blackjack/HandsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class HandsTest {
@@ -15,5 +16,15 @@ public class HandsTest {
     void create() {
         List<Card> cards = new ArrayList<>();
         assertThatCode(() -> new Hands(cards)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("Cards에 Card 객체 추가")
+    @Test
+    void add() {
+        List<Card> cards = new ArrayList<>();
+        Hands hands = new Hands(cards);
+        hands.add(Card.create(CardSymbol.CLUB, CardValue.KING));
+
+        assertThat(hands.toList().size()).isEqualTo(1);
     }
 }
