04f4aacb1a73a5b1649756d94481fa6c2f4983aa
rok93 <goodboy302@naver.com>
2021-03-02 21:15:48 +0900

    fix: 카드 넘버 10인 카드 추가

--- src/main/java/blackjack/domain/CardValue.java
+++ src/main/java/blackjack/domain/CardValue.java
@@ -3,7 +3,8 @@ package blackjack.domain;
 public enum CardValue {
     ACE("A", 1), TWO("2", 2), THREE("3", 3), FOUR("4", 4),
     FIVE("5", 5), SIX("6", 6), SEVEN("7", 7), EIGHT("8", 8),
-    NINE("9", 9), QUEEN("Q", 10), JACK("J", 10), KING("K", 10);
+    NINE("9", 9), TEN("10", 10), QUEEN("Q", 10), JACK("J", 10),
+    KING("K", 10);
 
     private final String value;
     private final int point;
