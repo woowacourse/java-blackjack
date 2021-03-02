6510b27af747a9d85715d36136eccd1b66796774
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-02 15:45:44 +0900

    feat: 참가자 생성 및 이름 유효성 검증 기능 구현

---
+++ src/main/java/blackjack/domain/Participant.java
@@ -0,0 +1,20 @@
+package blackjack.domain;
+
+public class Participant {
+    private static final String INVALID_PARTICIPANT_NAME = "플레이어 이름은 양쪽 공백을 제외한 1글자 이상이어야 합니다.";
+    private static final int INVALID_NAME_LENGTH = 0;
+
+    private final String name;
+
+    public Participant(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        int trimNameLength = name.trim().length();
+        if (trimNameLength == INVALID_NAME_LENGTH) {
+            throw new IllegalArgumentException(INVALID_PARTICIPANT_NAME);
+        }
+    }
+}
---
+++ src/test/java/blackjack/domain/ParticipantTest.java
@@ -0,0 +1,19 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ParticipantTest {
+    @ParameterizedTest
+    @DisplayName("양쪽 공백을 제외한 이름의 글자 수가 1글자 이상이 아닐 경우 예외 발생")
+    @ValueSource(strings = {"   ", " "})
+    public void validateParticipantName(String name) {
+        assertThatCode(() -> {
+            new Participant(name);
+        }).isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("플레이어 이름은 양쪽 공백을 제외한 1글자 이상이어야 합니다.");
+    }
+}
