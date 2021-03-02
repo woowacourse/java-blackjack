714aa1d5793ed445f3593c3948a4a13da460e206
lns13301 <rkdrbgks22@ks.ac.kr>
2021-03-02 18:12:54 +0900

    feat: 카드 덱을 생성하는 기능 추가

---
+++ src/main/java/blackjack/domain/card/CardFactory.java
@@ -0,0 +1,29 @@
+package blackjack.domain.card;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CardFactory {
+
+    private CardFactory() {
+    }
+
+    public static List<Card> create() {
+        return Collections.unmodifiableList(iterateType(new ArrayList<>()));
+    }
+
+    private static List<Card> iterateType(List<Card> cards) {
+        for (Type type : Type.values()) {
+            iterateDenomination(cards, type);
+        }
+        return cards;
+    }
+
+    private static List<Card> iterateDenomination(List<Card> cards, Type type) {
+        for (Denomination denomination : Denomination.values()) {
+            cards.add(new Card(type, denomination));
+        }
+        return cards;
+    }
+}
