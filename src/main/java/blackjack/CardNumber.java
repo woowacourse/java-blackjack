53555f4c61a64f67e0338d0a38b3d1808f1a1dd1
xrabcde <xrabcde@gmail.com>
2021-03-02 18:30:29 +0900

    feat(CardNumber): CardNumber enum 생성

---
+++ src/main/java/blackjack/CardNumber.java
@@ -0,0 +1,23 @@
+package blackjack;
+
+public enum CardNumber {
+    ACE("A"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    JACK("J"),
+    QUEEN("Q"),
+    KING("K");
+
+    private final String number;
+
+    CardNumber(String number) {
+        this.number = number;
+    }
+}
