1eede743b75d67726f524ee3133dee7037732633
xrabcde <xrabcde@gmail.com>
2021-03-02 18:30:13 +0900

    feat(CardType): CardType enum 생성

---
+++ src/main/java/blackjack/CardType.java
@@ -0,0 +1,14 @@
+package blackjack;
+
+public enum CardType {
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    SPADE("스페이드"),
+    CLOVER("클로버");
+
+    private final String type;
+
+    CardType(String type) {
+        this.type = type;
+    }
+}
