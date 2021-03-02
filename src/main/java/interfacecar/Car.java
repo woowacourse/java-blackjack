f080ffc40c28e382bc2a4a650c6af68fbbdef5cd
hyeon9mak <jinha3507@gmail.com>
2021-03-02 17:19:06 +0900

    feat : 차량에 따른 연료주입 인터페이스로 구현

---
+++ src/main/java/interfacecar/Car.java
@@ -0,0 +1,16 @@
+package interfacecar;
+
+public interface Car {
+
+    double getDistancePerLiter();
+
+    double getTripDistance();
+
+    default double getChargeQuantity() {
+        return getTripDistance() / getDistancePerLiter();
+    }
+
+    default String getName() {
+        return this.getClass().getSimpleName();
+    }
+}
