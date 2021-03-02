290bf7fc578acef4974441434d5d25536709a69d
김주원 <kjw11077@naver.com>
2021-03-02 15:11:14 +0900

    feat: Car 구현체들 정의

---
+++ src/main/java/rentcompany/K5.java
@@ -0,0 +1,23 @@
+package rentcompany;
+
+public class K5 extends Car {
+
+    public K5(int i) {
+
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return 0;
+    }
+
+    @Override
+    double getTripDistance() {
+        return 0;
+    }
+
+    @Override
+    String getName() {
+        return null;
+    }
+}
---
+++ src/main/java/rentcompany/Sonata.java
@@ -0,0 +1,22 @@
+package rentcompany;
+
+public class Sonata extends Car {
+
+    public Sonata(int i) {
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return 0;
+    }
+
+    @Override
+    double getTripDistance() {
+        return 0;
+    }
+
+    @Override
+    String getName() {
+        return null;
+    }
+}
