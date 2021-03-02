6e92f4438dd186b24d4a2c9e60d1d13cf5a70c33
김주원 <kjw11077@naver.com>
2021-03-02 15:10:55 +0900

    feat: RentCompany 클래스 정의

---
+++ src/main/java/rentcompany/RentCompany.java
@@ -0,0 +1,16 @@
+package rentcompany;
+
+public class RentCompany {
+
+    public static RentCompany create() {
+        return new RentCompany();
+    }
+
+    public void addCar(Car car) {
+
+    }
+
+    public String generateReport() {
+        return null;
+    }
+}
