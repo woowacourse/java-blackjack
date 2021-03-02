c317ea8f3cbb3aa1a944f44eefebec665982dc5b
Sehwan_Jang <whilter08@gmail.com>
2021-03-02 19:47:28 +0900

    feat(Deck): 카드 분배 기능 구현 및 테스트

--- src/main/java/blackjack/domain/Deck.java
+++ src/main/java/blackjack/domain/Deck.java
@@ -3,16 +3,28 @@ package blackjack.domain;
 import blackjack.domain.card.Card;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Deck {
     private final List<Card> cards;
+    private final Queue<Card> cardQueue;
 
     public Deck(List<Card> cards) {
         this.cards = new ArrayList<>(cards);
+        this.cardQueue = new LinkedList<>(cards);
     }
 
     public List<Card> getCards() {
         return new ArrayList<>(cards);
+    }
+
+    public List<Card> getCardQueue() {
+        return new ArrayList<>(cardQueue);
+    }
+
+    public Card draw() {
+        return cardQueue.poll();
     }
 }
--- src/test/java/blackjack/domain/DeckTest.java
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -3,6 +3,7 @@ package blackjack.domain;
 import blackjack.domain.card.Card;
 import blackjack.domain.card.CardValue;
 import blackjack.domain.card.Shape;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,15 +12,34 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DeckTest {
+    private Deck deck;
+
+    @BeforeEach
+    void setUp() {
+        deck = new Deck(Arrays.asList(
+                Card.valueOf(Shape.DIAMOND, CardValue.ACE),
+                Card.valueOf(Shape.SPADE, CardValue.ACE)));
+    }
+
     @Test
     @DisplayName("덱 생성 검사")
     void createDeck() {
-        Deck deck = new Deck(Arrays.asList(
-                Card.valueOf(Shape.DIAMOND, CardValue.ACE),
-                Card.valueOf(Shape.SPADE, CardValue.ACE)));
-
         assertThat(deck.getCards()).contains(
                 Card.valueOf(Shape.DIAMOND, CardValue.ACE),
                 Card.valueOf(Shape.SPADE, CardValue.ACE));
     }
+
+    @Test
+    @DisplayName("드로우 테스트")
+    void draw() {
+        Card card = deck.draw();
+        assertThat(card).isSameAs(Card.valueOf(Shape.DIAMOND, CardValue.ACE));
+        assertThat(deck.getCardQueue().size()).isEqualTo(1);
+    }
+
+    //    @Test
+//    @DisplayName("덱이 소진되었을 경우 검증")
+//    void draw() {
+//
+//    }
 }
