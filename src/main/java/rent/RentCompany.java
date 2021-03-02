ba72f33797fa00052e1fcb9a5171485a69ed2488
joanne <mathmjseo@naver.com>
2021-03-02 16:05:33 +0900

    feat: RentCompany 생성 및 메서드 내부 수정

--- src/main/java/rent/RentCompany.java
+++ src/main/java/rent/RentCompany.java
@@ -1,12 +1,21 @@
 package rent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RentCompany {
+    private final List<Car> cars;
+
+    private RentCompany() {
+        cars = new ArrayList<>();
+    }
+
     public static RentCompany create() {
         return new RentCompany();
     }
 
     public void addCar(Car car){
-
+        cars.add(car);
     }
 
     public String generateReport() {
