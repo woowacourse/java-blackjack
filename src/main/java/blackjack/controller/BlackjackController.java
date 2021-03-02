6b915ba12f07b06de4ec68c33c4a5ff2bcda0ad8
yjksw <cairofaith@gmail.com>
2021-03-02 22:43:28 +0900

    feat(InputView.java) : 플레이어 이름 입력

---
+++ src/main/java/blackjack/controller/BlackjackController.java
@@ -0,0 +1,10 @@
+package blackjack.controller;
+
+import blackjack.view.InputView;
+
+public class BlackjackController {
+
+    public void run() {
+        InputView.inputPlayerNames();
+    }
+}
---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,19 @@
+package blackjack.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String COMMA = ",";
+
+    public static List<String> inputPlayerNames() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        String input = SCANNER.nextLine();
+        return Arrays.stream(input.split(COMMA))
+            .map(String::trim)
+            .collect(Collectors.toList());
+    }
+}
