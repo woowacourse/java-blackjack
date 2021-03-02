7c519c4e3a46974fe4e91b313fe4f5fe85c7d14f
pika96 <52442949+pika96@users.noreply.github.com>
2021-03-02 21:27:58 +0900

    feat(CardDeck): 카드 덱 생성 및 드로우 기능 구현

---
+++ src/main/java/blackjack/domain/CardDeck.java
@@ -0,0 +1,24 @@
+package blackjack.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CardDeck {
+    private final List<Card> deck;
+
+    public CardDeck() {
+        deck = Arrays.stream(Denomination.values())
+                .flatMap(denomination -> Arrays
+                        .stream(Shape.values())
+                        .map(shape -> new Card(shape, denomination)))
+                .collect(Collectors.toList());
+    }
+
+    public Card drawCard() {
+        if(deck.size() == 0){
+            throw new IndexOutOfBoundsException("남은 카드가 없습니다.");
+        }
+        return deck.remove(0);
+    }
+}
