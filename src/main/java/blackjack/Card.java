f48898b09f57249a3a8b3c1092d11b84a54e3c0d
sjpark-dev <sjpark.dev@gmail.com>
2021-03-02 18:23:19 +0900

    feat: Card 생성 구현

---
+++ src/main/java/blackjack/Card.java
@@ -0,0 +1,36 @@
+package blackjack;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Card {
+    private static Map<String, Card> cards;
+
+    public static final int CARDS_CAPACITY = 52;
+
+    private final Suits suit;
+    private final Denominations denomination;
+
+    static{
+        cards = new HashMap<>(CARDS_CAPACITY);
+        for (Suits suit : Suits.values()){
+            for (Denominations denomination: Denominations.values()){
+                String key = denomination.getName() + suit.getName();
+                cards.put(key, new Card(suit, denomination));
+            }
+        }
+    }
+
+    private Card(Suits suit, Denominations denomination){
+        this.suit = suit;
+        this.denomination = denomination;
+    }
+    public static Card from(String value) {
+        Card card = cards.get(value);
+        if (card == null) {
+            throw new IllegalArgumentException();
+        }
+        return card;
+    }
+}
---
+++ src/test/java/blackjack/CardTest.java
@@ -0,0 +1,21 @@
+package blackjack;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CardTest {
+    @Test
+    @DisplayName("카드 생성")
+    void create_cards(){
+        assertThat(Card.from("A다이아몬드")).isSameAs(Card.from("A다이아몬드"));
+    }
+
+    @Test
+    @DisplayName("fail: 잘못된 카드 생성")
+    void create_cards2(){
+        assertThatThrownBy(() -> Card.from("11다이아몬드")).isInstanceOf(IllegalArgumentException.class);
+    }
+}
