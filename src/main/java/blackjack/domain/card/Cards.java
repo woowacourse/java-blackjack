57dfcfebf82372942586392dd8e7122cb3a03f38
pika96 <52442949+pika96@users.noreply.github.com>
2021-03-03 15:54:24 +0900

    feat(Cards): 카드 꾸러미 생성 구현

--- src/main/java/blackjack/domain/card/Cards.java
+++ src/main/java/blackjack/domain/card/Cards.java
@@ -1,2 +1,17 @@
-package blackjack.domain.card;public class Cards {
+package blackjack.domain.card;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Cards {
+    private final List<Card> cards;
+
+    public Cards(Card card) {
+        this(Collections.singletonList(card));
+    }
+
+    public Cards(List<Card> cards) {
+        this.cards = new ArrayList<>(cards);
+    }
 }
--- src/test/java/blackjacktest/domaintest/cardtest/CardsTest.java
+++ src/test/java/blackjacktest/domaintest/cardtest/CardsTest.java
@@ -1,2 +1,27 @@
-package blackjacktest.domaintest.cardtest;public class CardsTest {
+package blackjacktest.domaintest.cardtest;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Cards;
+import blackjack.domain.card.Denomination;
+import blackjack.domain.card.Shape;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CardsTest {
+
+    @Test
+    @DisplayName("카드 꾸러미 생성")
+    void createCards() {
+        Cards cards1 = new Cards(new Card(Shape.CLOVER, Denomination.FOUR));
+        Cards cards2 = new Cards(Arrays.asList(
+           new Card(Shape.SPADE, Denomination.ACE),
+           new Card(Shape.HEART, Denomination.FIVE)
+        ));
+        assertThat(cards1).isNotNull();
+        assertThat(cards2).isNotNull();
+    }
 }
