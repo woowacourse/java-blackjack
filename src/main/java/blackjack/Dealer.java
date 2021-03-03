7d9541db3242aff854f821afb916e001673fe13a
Wannte <T.wannte@gmail.com>
2021-03-03 14:41:46 +0900

    feat(dealer): participant 를 상속받는 dealer 정의

---
+++ src/main/java/blackjack/Dealer.java
@@ -0,0 +1,10 @@
+package blackjack;
+
+public class Dealer extends Participant{
+    public static final String DEALER = "딜러";
+
+    @Override
+    String getName() {
+        return DEALER;
+    }
+}
