ec603851fe42c8bb670c89d542f8cb4ada0e7d7a
김주원 <kjw11077@naver.com>
2021-03-03 11:38:28 +0900

    feat: Card - Suit, Rank를 가지는 VO 정의

---
+++ src/main/java/blackjack/Card.java
@@ -0,0 +1,31 @@
+package blackjack;
+
+import java.util.Objects;
+
+public class Card {
+
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return suit == card.suit && rank == card.rank;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(suit, rank);
+    }
+}
---
+++ src/test/java/blackjack/CardTest.java
@@ -0,0 +1,35 @@
+package blackjack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class CardTest {
+
+    @ParameterizedTest(name = "값 객체 비교")
+    @MethodSource("equals_testcase")
+    void equals(Suit suit, Rank rank) {
+        Card card1 = new Card(suit, rank);
+        Card card2 = new Card(suit, rank);
+
+        assertThat(card1).isEqualTo(card2);
+        assertThat(card1).hasSameHashCodeAs(card2);
+    }
+
+    private static Stream<Arguments> equals_testcase() {
+        return Stream.of(
+                Arguments.of(Suit.SPACE, Rank.ONE),
+                Arguments.of(Suit.SPACE, Rank.KING),
+                Arguments.of(Suit.DIAMOND, Rank.ONE),
+                Arguments.of(Suit.DIAMOND, Rank.KING),
+                Arguments.of(Suit.HEART, Rank.ONE),
+                Arguments.of(Suit.HEART, Rank.KING),
+                Arguments.of(Suit.CLOVER, Rank.ONE),
+                Arguments.of(Suit.CLOVER, Rank.KING)
+        );
+    }
+
+}
