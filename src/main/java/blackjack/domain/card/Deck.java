3f5c98368325143e9c60715381e818010263fa01
lns13301 <rkdrbgks22@ks.ac.kr>
2021-03-03 11:12:53 +0900

    feat: 덱에서 카드를 분배 및 섞는 기능 추가

---
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -0,0 +1,21 @@
+package blackjack.domain.card;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+
+    private List<Card> cards;
+
+    public Deck() {
+        cards = CardFactory.create();
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card getCard(int i) {
+        return cards.get(i);
+    }
+}
