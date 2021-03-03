12cb79d97f2aa0905f5f71b48b45c69d797b2bfb
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-03 22:14:53 +0900

    fix: Queen number 오타 수정

--- src/main/java/blackjack/domain/card/CardNumber.java
+++ src/main/java/blackjack/domain/card/CardNumber.java
@@ -12,7 +12,7 @@ public enum CardNumber {
     NINE("9", 9),
     TEN("10", 10),
     JACK("J", 10),
-    QUEEN("K", 10),
+    QUEEN("Q", 10),
     KING("K", 10);
 
     private final String number;
