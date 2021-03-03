181e7b354bd76b45589d2fc156ef0e4e1a0d8bdf
pika96 <52442949+pika96@users.noreply.github.com>
2021-03-03 12:04:24 +0900

    feat(Person): Player와 Dealer를 상속할 추상클래스 Person 구현

---
+++ src/main/java/blackjack/domain/Person.java
@@ -0,0 +1,34 @@
+package blackjack.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class Person {
+    private final List<Card> cards = new ArrayList<>();
+    protected String name;
+
+    public void receiveCard(Card card) {
+        cards.add(card);
+    }
+
+    public List<Card> getCards() {
+        return Collections.unmodifiableList(cards);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+}
