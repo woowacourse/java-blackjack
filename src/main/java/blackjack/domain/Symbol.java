dbe88d75696854edd6773dcf33a5b42bc7a8cb14
TaeJeong Kim <xowjd41@naver.com>
2021-03-03 11:49:39 +0900

    feat: Symbol 객체 생성
    보장되지 않은 값에 대해서 테스트 완료

---
+++ src/main/java/blackjack/domain/Symbol.java
@@ -0,0 +1,34 @@
+package blackjack.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public enum Symbol {
+    SPADE("스페이드"),
+    HEART("하트"),
+    CLOVER("클로버"),
+    DIAMOND("다이아몬드");
+
+    private static final Map<String, Symbol> SYMBOLS = new HashMap<>();
+
+    static {
+        for (Symbol symbol : values()) {
+            SYMBOLS.put(symbol.name, symbol);
+        }
+    }
+
+    private final String name;
+
+    Symbol(String name) {
+        this.name = name;
+    }
+
+    public static Symbol from(String name) {
+        Symbol symbol = SYMBOLS.get(name);
+        if (Objects.isNull(symbol)) {
+            throw new IllegalArgumentException("없는 문장임!");
+        }
+        return symbol;
+    }
+}
