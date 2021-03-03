4e59507a792c78d9fc05446c723e85f982c4e8a9
Solji Choi <bssj9307@gmail.com>
2021-03-03 14:32:34 +0900

    feat: 딜러 구현

---
+++ src/main/java/blackjack/domain/user/Dealer.java
@@ -0,0 +1,17 @@
+package blackjack.domain.user;
+
+import blackjack.domain.card.UserDeck;
+
+public class Dealer extends User {
+
+    private final int DEALER_CRITERIA = 16;
+
+    public Dealer(UserDeck userDeck) {
+        super(userDeck);
+    }
+
+    @Override
+    public boolean isAvailableDraw() {
+        return super.getPoint() <= DEALER_CRITERIA;
+    }
+}
