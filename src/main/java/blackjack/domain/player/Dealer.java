282ddae77598a0d10b17d681d70c15088ad3dbbc
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-03 22:26:41 +0900

    refactor: 딜러 이름 상수화

--- src/main/java/blackjack/domain/player/Dealer.java
+++ src/main/java/blackjack/domain/player/Dealer.java
@@ -2,10 +2,11 @@ package blackjack.domain.player;
 
 
 public class Dealer extends AbstractPlayer {
+    private static final String DEALER_NAME = "딜러";
     private static final int DRAW_BOUND = 16;
 
     public Dealer() {
-        super("딜러");
+        super(DEALER_NAME);
     }
 
     @Override
