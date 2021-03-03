98ea61fe3658c5323c82c3ff4a6a072e4fd128b3
Wannte <T.wannte@gmail.com>
2021-03-03 15:19:48 +0900

    feat(Game): 플레이어와 딜러가 2장씩 카드를 나눈다.

--- src/main/java/blackjack/Game.java
+++ src/main/java/blackjack/Game.java
@@ -1,6 +1,8 @@
 package blackjack;
 
+import blackjack.card.Deck;
 import blackjack.participant.Dealer;
+import blackjack.participant.Participant;
 import blackjack.participant.Player;
 
 import java.util.Collections;
@@ -8,12 +10,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Game {
-    private final List<Player> players;
     private final Dealer dealer;
+    private final List<Player> players;
 
     public Game(List<Player> players) {
-        this.players = players;
         this.dealer = new Dealer();
+        this.players = players;
     }
 
     public static Game of(List<String> playerNames) {
@@ -22,7 +24,23 @@ public class Game {
                                    .collect(Collectors.toList()));
     }
 
+    public void setUpTwoCards() {
+        addTwoCard(dealer);
+        for (Player player : players) {
+            addTwoCard(player);
+        }
+    }
+
+    private void addTwoCard(Participant participant) {
+        participant.addCard(Deck.draw());
+        participant.addCard(Deck.draw());
+    }
+
     public List<Player> getPlayers() {
         return Collections.unmodifiableList(players);
+    }
+
+    public Dealer getDealer() {
+        return dealer;
     }
 }
--- src/test/java/blackjack/GameTest.java
+++ src/test/java/blackjack/GameTest.java
@@ -1,5 +1,9 @@
 package blackjack;
 
+import blackjack.participant.Dealer;
+import blackjack.participant.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,10 +12,27 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
+    List<String> names = Arrays.asList("wannte","bepoz");
+    Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = Game.of(names);
+    }
+
     @Test
     void createGame() {
-        List<String> inputs = Arrays.asList("wannte","bepoz");
-        Game game = Game.of(inputs);
         assertThat(game.getPlayers().size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("카드 두장 셋업")
+    void setUpTwoCards() {
+        game.setUpTwoCards();
+        for (Player player : game.getPlayers()) {
+            assertThat(player.getCards().size()).isEqualTo(2);
+        }
+        Dealer dealer = game.getDealer();
+        assertThat(dealer.getCards().size()).isEqualTo(2);
     }
 }
