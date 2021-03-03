01000e7731d27bfec452b880d8145835caae7f90
김주원 <kjw11077@naver.com>
2021-03-03 11:40:07 +0900

    feat: Deck - 모든 Suit,Rank 조합의 카드들을 가진 카드뭉치

---
+++ src/main/java/blackjack/Deck.java
@@ -0,0 +1,30 @@
+package blackjack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Deck {
+
+    private final List<Card> deck;
+
+    private Deck(List<Card> deck) {
+        this.deck = new ArrayList<>(deck);
+    }
+
+    public static Deck createShuffledDeck() {
+        List<Card> deck = createDeck();
+        Collections.shuffle(deck);
+
+        return new Deck(deck);
+    }
+
+    private static List<Card> createDeck() {
+        return Arrays.stream(Suit.values())
+                .flatMap(suit -> Arrays.stream(Rank.values())
+                        .map(rank -> new Card(suit, rank)))
+                .collect(Collectors.toList());
+    }
+}
