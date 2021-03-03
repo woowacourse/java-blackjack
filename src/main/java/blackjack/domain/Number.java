cf818dccb4bff51a91439ffb0b85080d5b7f704f
TaeJeong Kim <xowjd41@naver.com>
2021-03-03 11:35:45 +0900

    feat: Number 객체 생성
    보장된 값이 아닌 값들에 대해  테스트 완료

--- src/main/java/blackjack/domain/Number.java
+++ src/main/java/blackjack/domain/Number.java
@@ -1,29 +1,55 @@
 package blackjack.domain;
 
-public enum Number {
-    ACE(1, 11),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
+public enum Number {
+    ACE("A", 1, 11),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10);
+
+    private static final Map<String, Number> NUMBERS = new HashMap<>();
+
+    static {
+        for (Number number : values()) {
+            NUMBERS.put(number.name, number);
+        }
+    }
+
+    private final String name;
     private final int score;
     private final int secondScore;
 
-    Number(int score, int secondScore) {
+    Number(String name, int score, int secondScore) {
+        this.name = name;
         this.score = score;
         this.secondScore = secondScore;
     }
 
-    Number(int score) {
-        this(score, 0);
+    Number(String name, int score) {
+        this(name, score, 0);
+    }
+
+    public static Number from(String name) {
+        Number number = NUMBERS.get(name);
+        if (Objects.isNull(number)) {
+            throw new IllegalArgumentException(String.format("입력된 숫자는 없는 카드 숫자입니다! : %s", name));
+        }
+        return number;
+    }
+
+    public String getName() {
+        return name;
     }
 }
