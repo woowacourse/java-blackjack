ec377236839cd42b9fbf92d4fc975ee0747dd480
xrabcde <xrabcde@gmail.com>
2021-03-03 15:03:37 +0900

    feat(InputView): 참가자 이름 입력받는 기능 구현

---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,19 @@
+package blackjack.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String REQUEST_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉽표 기준으로 분리)";
+
+    public static List<String> requestName() {
+        System.out.println(REQUEST_NAME_MESSAGE);
+        return Arrays.stream(scanner.nextLine().trim().split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/blackjack/view/OutputView.java
@@ -0,0 +1,9 @@
+package blackjack.view;
+
+public class OutputView {
+
+    public void distributeMessage(String players) {
+        System.out.printf("딜러와 %s에게 2장의 카드를 나누어주었습니다.\n", players);
+    }
+
+}
