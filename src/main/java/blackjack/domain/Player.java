37f46830b72e79fca86c4c61b1d09d26e9af4da8
Kimun Kim <kkm97351@gmail.com>
2021-03-03 11:40:53 +0900

    feat: 플레이어의 stay 기능 구현

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -3,6 +3,12 @@ package blackjack.domain;
 import blackjack.domain.card.Card;
 
 public class Player extends User {
+    boolean isStay = false;
+
+    public void stay() {
+        this.isStay = true;
+    }
+
     @Override
     public void hit(Card card) {
         cards.add(card);
@@ -10,6 +16,6 @@ public class Player extends User {
 
     @Override
     public boolean isStay() {
-        return false;
+        return isStay;
     }
 }
--- src/test/java/blackjack/domain/PlayerTest.java
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PlayerTest {
 
@@ -17,6 +19,21 @@ class PlayerTest {
         player.hit(cardDeck.getDeck().pop());
 
         assertThat(player.getCards()).hasSize(1);
+    }
+
+    @DisplayName("isStay:false - 플레이어가 스테이 의사를 밝히지 않으면 계속 게임 진행")
+    @Test
+    void isStayFalse() {
+        Player player = new Player();
+        assertFalse(player.isStay());
+    }
+
+    @DisplayName("isStay:true - 플레이어가 스테이 의사를 밝히면 해당 플레이어는 게임 중단")
+    @Test
+    void isStayTrue() {
+        Player player = new Player();
+        player.stay();
+        assertTrue(player.isStay());
     }
 
 }
