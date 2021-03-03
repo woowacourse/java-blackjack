c63d28b2732818e65163f500c212623d2ad40c38
rok93 <goodboy302@naver.com>
2021-03-03 17:17:19 +0900

    style: Card의 불필요한 주석 제거

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -1,8 +1,8 @@
 package blackjack.domain;
 
 public class Card {
-    private final CardType cardType; //pattern
-    private final CardValue cardValue; // 끝 수
+    private final CardType cardType;
+    private final CardValue cardValue;
 
     public Card(CardType cardType, CardValue cardValue) {
         this.cardType = cardType;
