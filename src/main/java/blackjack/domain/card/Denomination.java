11037e4342ed10a948402ddb398e586b1679282b
lns13301 <rkdrbgks22@ks.ac.kr>
2021-03-03 12:28:31 +0900

    feat: Denomination 에 value 를 갖도록 추가

--- src/main/java/blackjack/domain/card/Denomination.java
+++ src/main/java/blackjack/domain/card/Denomination.java
@@ -1,27 +1,33 @@
 package blackjack.domain.card;
 
 public enum Denomination {
-    ACE("A"),
-    TWO("2"),
-    THREE("3"),
-    FOUR("4"),
-    FIVE("5"),
-    SIX("6"),
-    SEVEN("7"),
-    EIGHT("8"),
-    NINE("9"),
-    TEN("10"),
-    JACK("J"),
-    QUEEN("Q"),
-    KING("K");
+    ACE("A", 1),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10);
 
     private final String name;
+    private final int value;
 
-    Denomination(String name) {
+    Denomination(String name, int value) {
         this.name = name;
+        this.value = value;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getValue() {
+        return value;
     }
 }
