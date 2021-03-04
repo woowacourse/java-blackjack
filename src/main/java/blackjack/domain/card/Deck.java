ed8a1ec052154434b8dbe3a7bdd1ae7a2bbd8ec9
Seed <hsik0225@gmail.com>
2021-03-04 14:22:34 +0900

    feat(deck):덱에서 카드 뽑기 기능 구현

--- src/main/java/blackjack/domain/card/Deck.java
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -7,28 +7,38 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Deck {
-
+    
+    private static final int TOP_INDEX = 0;
+    private static final String ERROR_EMPTY_DECK = "덱에 카드가 존재하지 않습니다.";
+    
     private final List<Card> deck;
-
-    private Deck(List<Card> deck) {
+    
+    Deck(List<Card> deck) {
         this.deck = new ArrayList<>(deck);
     }
-
-    public Card drawCard() {
-        return deck.remove(0);
-    }
-
+    
     public static Deck createShuffledDeck() {
         List<Card> deck = createDeck();
         Collections.shuffle(deck);
-
         return new Deck(deck);
     }
-
+    
     private static List<Card> createDeck() {
         return Arrays.stream(Suit.values())
-                .flatMap(suit -> Arrays.stream(Rank.values())
-                        .map(rank -> new Card(suit, rank)))
-                .collect(Collectors.toList());
+                     .flatMap(suit -> Arrays.stream(Rank.values())
+                                            .map(rank -> new Card(suit, rank)))
+                     .collect(Collectors.toList());
+    }
+    
+    public Card drawCard() {
+        if (deck.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_EMPTY_DECK);
+        }
+        
+        return deck.remove(TOP_INDEX);
+    }
+    
+    public List<Card> getDeck() {
+        return deck;
     }
 }
