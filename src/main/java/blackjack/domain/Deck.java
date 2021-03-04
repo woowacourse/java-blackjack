e2e2433693760128f995e2a7ad3fbee4ca7d6ef2
Sehwan-Jang <whilter08@gmail.com>
2021-03-04 15:46:31 +0900

    feat(Deck) : 덱 내 중복카드 존재 검증 기능 구현 및 테스트

--- src/main/java/blackjack/domain/Deck.java
+++ src/main/java/blackjack/domain/Deck.java
@@ -1,17 +1,22 @@
 package blackjack.domain;
 
 import blackjack.domain.card.Card;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+
+import java.util.*;
 
 public class Deck {
 
     private final Queue<Card> cards;
 
     public Deck(List<Card> cards) {
+        validateDuplication(cards);
         this.cards = new LinkedList<>(cards);
+    }
+
+    private void validateDuplication(List<Card> cards) {
+        if (new HashSet<>(cards).size() != cards.size()) {
+            throw new IllegalArgumentException("덱 내 중복되는 카드가 존재합니다.");
+        }
     }
 
     public List<Card> getCards() {
--- src/test/java/blackjack/domain/DeckTest.java
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -1,16 +1,17 @@
 package blackjack.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import blackjack.domain.card.Card;
 import blackjack.domain.card.CardValue;
 import blackjack.domain.card.Shape;
-import java.util.ArrayList;
-import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class DeckTest {
 
@@ -19,16 +20,16 @@ public class DeckTest {
     @BeforeEach
     void setUp() {
         deck = new Deck(Arrays.asList(
-            Card.valueOf(Shape.DIAMOND, CardValue.ACE),
-            Card.valueOf(Shape.SPADE, CardValue.ACE)));
+                Card.valueOf(Shape.DIAMOND, CardValue.ACE),
+                Card.valueOf(Shape.SPADE, CardValue.ACE)));
     }
 
     @Test
     @DisplayName("덱 생성 검사")
     void createDeck() {
         assertThat(deck.getCards()).contains(
-            Card.valueOf(Shape.DIAMOND, CardValue.ACE),
-            Card.valueOf(Shape.SPADE, CardValue.ACE));
+                Card.valueOf(Shape.DIAMOND, CardValue.ACE),
+                Card.valueOf(Shape.SPADE, CardValue.ACE));
     }
 
     @Test
@@ -44,7 +45,17 @@ public class DeckTest {
     void notEnoughCards() {
         deck = new Deck(new ArrayList<>());
         assertThatThrownBy(() -> deck.draw())
-            .isInstanceOf(IllegalStateException.class)
-            .hasMessage("덱이 모두 소진되었습니다.");
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("덱이 모두 소진되었습니다.");
+    }
+
+    @Test
+    @DisplayName("덱 내 중복카드가 존재하는지 검증")
+    void checkDuplication() {
+        assertThatThrownBy(() -> new Deck(Arrays.asList(
+                    Card.valueOf(Shape.SPADE, CardValue.ACE),
+                    Card.valueOf(Shape.SPADE, CardValue.ACE))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("덱 내 중복되는 카드가 존재합니다.");
     }
 }
