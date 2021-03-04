f1315b7a21096797343177cdbec8b59de4f829f8
jujubebat <bbwwpark@naver.com>
2021-03-04 21:18:53 +0900

    refactor: 트럼프 카드 세트 생성 로직 수정

--- src/main/java/blackjack/domain/card/CardFactory.java
+++ src/main/java/blackjack/domain/card/CardFactory.java
@@ -1,28 +1,18 @@
 package blackjack.domain.card;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Stack;
 
 public class CardFactory {
 
     private CardFactory() {
     }
 
-    public static List<Card> create() {
-        return iterateType(new ArrayList<>());
-    }
-
-    private static List<Card> iterateType(List<Card> cards) {
+    public static Stack<Card> create() {
+        Stack<Card> cards = new Stack<>();
         for (Type type : Type.values()) {
-            iterateDenomination(cards, type);
-        }
-        return cards;
-    }
-
-    private static List<Card> iterateDenomination(List<Card> cards, Type type) {
-        for (Denomination denomination : Denomination.values()) {
-            cards.add(new Card(type, denomination));
+            for (Denomination denomination : Denomination.values()) {
+                cards.push(new Card(type, denomination));
+            }
         }
         return cards;
     }
