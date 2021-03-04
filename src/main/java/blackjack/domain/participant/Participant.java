24c38a4182d3d56a13e25fde79c170d67bb72988
Seed <hsik0225@gmail.com>
2021-03-04 15:22:55 +0900

    feat(participant):dealer와 player가 공통적으로 사용하는 메소드 추상화

--- src/main/java/blackjack/domain/participant/Participant.java
+++ src/main/java/blackjack/domain/participant/Participant.java
@@ -2,25 +2,38 @@ package blackjack.domain.participant;
 
 import blackjack.domain.card.Card;
 import blackjack.domain.card.CardHand;
+
 import java.util.List;
 
 public abstract class Participant {
-
+    
+    public static final int MAXIMUM_THRESHOLD = 21;
+    
     protected final String name;
     protected final CardHand cardHand;
-
+    
     public Participant(String name, CardHand cardHand) {
         this.name = name;
         this.cardHand = cardHand;
     }
-
-    public List<Card> getCardHand() {
+    
+    public List<Card> getCards() {
         return cardHand.getCards();
     }
-
-    public abstract int getCardSum();
-
+    
+    public int sumCardHand() {
+        return cardHand.sum();
+    }
+    
     public String getName() {
         return name;
+    }
+    
+    public void receive(Card card) {
+        cardHand.add(card);
+    }
+    
+    public boolean canReceive() {
+        return cardHand.sum() < MAXIMUM_THRESHOLD;
     }
 }
--- src/main/java/blackjack/domain/participant/Player.java
+++ src/main/java/blackjack/domain/participant/Player.java
@@ -15,14 +15,4 @@ public class Player extends Participant {
     public static Player from(String name) {
         return new Player(name, new CardHand(new ArrayList<>()));
     }
-    
-    public boolean canReceive() {
-        return cardHand.sum() <= MAXIMUM_THRESHOLD;
-    }
-    //### 플레이어
-    //- [ ] 이름을 가짐
-    //- [ ] 카드 패를 가짐
-    //- [ ] 시작하면 카드 두 장을 받는다
-    //- [ ] 자신의 턴 동안 원하는 만큼 카드를 계속 뽑을 수 있다
-    //- [ ] 카드의 합이 21 이상이 되면 더 이상 뽑을 수 없다
 }
