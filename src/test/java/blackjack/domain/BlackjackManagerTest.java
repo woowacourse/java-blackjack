c9c614f0deed7172ee5bb034a05677bef83d9cd1
hyeon9mak <jinha3507@gmail.com>
2021-03-04 17:46:54 +0900

    test : 게임 초기화 및 결과반환 테스트 추가

--- src/test/java/blackjack/domain/BlackjackManagerTest.java
+++ src/test/java/blackjack/domain/BlackjackManagerTest.java
@@ -5,24 +5,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Players;
 import java.util.Arrays;
-import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BlackjackManagerTest {
 
-    /*@Test
+    private Players players;
+    private Dealer dealer;
+
+    @BeforeEach
+    void setUp() {
+        players = new Players(Arrays.asList("pobi", "jason"));
+        dealer = new Dealer();
+    }
+
+    @Test
     @DisplayName("각 플레이어가 초기 2장씩 소지한다.")
     void testAllPlayersGetTwoCards() {
-        List<String> names = Arrays.asList("pobi", "jason");
-        Players players = new Players(names);
-        Dealer dealer = new Dealer();
-        BlackjackManager blackjackManager = new BlackjackManager(dealer, players);
-        blackjackManager.initGame();
+        BlackjackManager.initGame(players, dealer);
+
         assertThat(players.toList()
             .stream()
             .filter(player -> player.getHand().size() == 2)
             .count())
             .isEqualTo(2);
-    }*/
+    }
+
+    @Test
+    @DisplayName("게임 플레이 결과 DTO 생성")
+    void testCreateResultDto() {
+        BlackjackManager.initGame(players, dealer);
+        GameResultDto gameResultDto = BlackjackManager.getGameResult(dealer, players);
+
+        assertThat(gameResultDto).isInstanceOf(GameResultDto.class);
+    }
 }
