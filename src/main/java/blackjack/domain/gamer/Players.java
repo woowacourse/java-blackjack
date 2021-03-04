277b22732dc7c885f5faea824fb31ab5badbb3bc
pika96 <52442949+pika96@users.noreply.github.com>
2021-03-04 21:43:20 +0900

    fix(Players): 플레이어가 한 명도 없는 경우 예외처리

--- src/main/java/blackjack/domain/gamer/Players.java
+++ src/main/java/blackjack/domain/gamer/Players.java
@@ -7,7 +7,11 @@ import java.util.*;
 import static java.util.stream.Collectors.toList;
 
 public class Players {
+
+    private static final int MIN_COUNT = 1;
     private static final int MAX_COUNT = 8;
+    private static final String COMMA = ",";
+
     private final List<Player> players;
 
     public Players(String input) {
@@ -17,13 +21,13 @@ public class Players {
     }
 
     private void validatePlayerCount(List<Player> player) {
-        if (player.size() > MAX_COUNT) {
-            throw new IllegalArgumentException("최대 플레이어 인원 수는 8명입니다.");
+        if (player.size() > MAX_COUNT || player.size() < MIN_COUNT) {
+            throw new IllegalArgumentException("플레이어 인원 수는 1명 이상 8명 이하 입니다.");
         }
     }
 
     private List<Player> splitInput(String players) {
-        return Arrays.stream(players.split(","))
+        return Arrays.stream(players.split(COMMA))
                 .map(s -> s.replaceAll(" ", ""))
                 .map(Player::new)
                 .collect(toList());
