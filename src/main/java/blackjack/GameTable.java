571458cf8eba283033ea27978feade40d5b5905e
RinSabbit <celena1004@naver.com>
2021-03-04 15:06:17 +0900

    feat: 게임이 진행되는 GameTable 구현

---
+++ src/main/java/blackjack/GameTable.java
@@ -0,0 +1,66 @@
+package blackjack;
+
+import blackjack.view.InputView;
+import blackjack.view.OutputView;
+import java.util.List;
+
+public class GameTable {
+
+    private final Deck deck;
+    private final Dealer dealer;
+    private List<Player> players;
+
+    public GameTable(List<Player> players){
+        deck = new Deck(Card.values());
+        this.dealer = new Dealer();
+        this.players = players;
+    }
+
+    public void playGame() {
+        start();
+        play();
+        finish();
+    }
+
+    private void start() {
+        drawAtFirst();
+        OutputView.printShowUsersCardMessage(players);
+        OutputView.showCards(dealer, players);
+    }
+
+    private void play() {
+        players.forEach(player -> {
+            while (player.isNotBust()) {
+                askHit(player);
+            }
+        });
+
+        while(dealer.canHit()){
+            dealer.hit(deck.pop());
+            OutputView.printDealerHitMessage();
+        }
+    }
+
+    private void finish() {
+        OutputView.printCardsAndScore(dealer, players);
+        Result result = new Result();
+        OutputView.printResult(result.getResult(dealer, players));
+    }
+
+    private void askHit(Player player) {
+        OutputView.printHitGuideMessage(player);
+        String hitValue = InputView.getHitValue();
+        if (hitValue.equals("Y")) {
+            player.hit(deck.pop());
+        }
+    }
+
+    private void drawAtFirst() {
+        players.forEach(player -> {
+            player.hit(deck.pop());
+            player.hit(deck.pop());
+        });
+        dealer.hit(deck.pop());
+        dealer.hit(deck.pop());
+    }
+}
