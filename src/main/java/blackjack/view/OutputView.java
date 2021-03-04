d0cc2db99415de24f41ba5c7848c63cd8900e505
jujubebat <bbwwpark@naver.com>
2021-03-04 21:18:11 +0900

    refactor: 컨트롤러 리팩토링에 따른 인풋 아웃풋 뷰 변경

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -2,9 +2,8 @@ package blackjack.view;
 
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Cards;
-import blackjack.domain.player.Participant;
+import blackjack.domain.player.Dealer;
 import blackjack.domain.player.Player;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,53 +13,59 @@ public class OutputView {
     private static final String DELIMITER = ", ";
     private static final String SEPARATOR = ": ";
 
-    public static void printParticipate(List<String> names) {
-        System.out.println("딜러와 " + String.join(DELIMITER, names) + "에게 2장을 나누었습니다.");
-    }
 
-    public static void printCards(String name, Cards cards) {
-        List<Card> cardList = cards.getCards();
-        List<String> result = new ArrayList<>();
-        for (Card card : cardList) {
-            result.add(card.toString());
-        }
-        System.out.println(name + "카드: " + String.join(DELIMITER, result));
-        System.out.print(NEW_LINE);
-    }
+    public static void printHandOutCardsMessage(Dealer dealer, List<Player> players) {
+        System.out.print(dealer.getName() + "와 ");
+        List<String> playerNames = new ArrayList<>();
 
-    public static void printCardScore(String name, Cards cards) {
-        List<Card> cardList = cards.getCards();
-        List<String> output = new ArrayList<>();
-        for (Card card : cardList) {
-            output.add(card.toString());
-        }
-        System.out.println(name + "카드: " + String.join(DELIMITER, output) + " - 결과: " + cards.getScore());
-        System.out.print(NEW_LINE);
-    }
-
-    public static void printOneMoreCard() {
-        System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
-        System.out.print(NEW_LINE);
-    }
-
-    public static void printDealerResult(int winCount, int drawCount, int loseCount) {
-        System.out.println("## 최종 승패");
-        System.out.print("딜러" + SEPARATOR);
-        if (winCount > 0) {
-            System.out.print(winCount + "승 ");
-        }
-        if (drawCount > 0) {
-            System.out.print(drawCount + "무 ");
-        }
-        if (loseCount > 0) {
-            System.out.print(loseCount + "패 ");
-        }
-        System.out.print(NEW_LINE);
-    }
-
-    public static void printPlayerResult(List<Player> players) {
         for (Player player : players) {
-            System.out.println(player.getName() + SEPARATOR + player.getResult().getResultName());
+            playerNames.add(player.getName());
+        }
+        System.out.println(String.join(", ", playerNames) + "에게 두 장의 카드를 나눠줬습니다.");
+    }
+
+    public static void printParticipantCards(String participantName, Cards cards) {
+        System.out.print(participantName + "카드: ");
+        List<String> results = new ArrayList<>();
+
+        for (Card card : cards.getCards()) {
+            results.add(card.getType().getName() + card.getDenomination().getName());
+        }
+        System.out.println(String.join(", ", results));
+    }
+
+    public static void printDealerCardDrawMessage() {
+        System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.\n");
+    }
+
+    public static void printParticipantCardsWithScore(String participantName, Cards cards) {
+        System.out.print(participantName + "카드: ");
+        List<String> results = new ArrayList<>();
+
+        for (Card card : cards.getCards()) {
+            results.add(card.getType().getName() + card.getDenomination().getName());
+        }
+        System.out.println(String.join(", ", results) + " - 결과: " + cards.getScore());
+    }
+
+
+    public static void printGameResult(List<Player> players, Dealer dealer) {
+        System.out.println("\n## 최종승패");
+
+        System.out.print(dealer.getName() + " ");
+        if (dealer.getWinCount() > 0) {
+            System.out.print(dealer.getWinCount() + "승 ");
+        }
+        if (dealer.getDrawCount() > 0) {
+            System.out.print(dealer.getDrawCount() + "무 ");
+        }
+        if (dealer.getLoseCount() > 0) {
+            System.out.print(dealer.getLoseCount() + "패 ");
+        }
+        System.out.println();
+
+        for (Player player : players) {
+            System.out.println(player.getName() + ": " + player.getResult().getName());
         }
     }
 }
