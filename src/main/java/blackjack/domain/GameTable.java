658496743ee1abc18faf44ac890a311c5f4a11e2
TaewanKimmmm <ktww88@gmail.com>
2021-03-04 20:47:11 +0900

    refactor: Result 클래스 역할 GameTable로 이동 후 삭제

--- src/main/java/blackjack/domain/GameTable.java
+++ src/main/java/blackjack/domain/GameTable.java
@@ -7,7 +7,7 @@ import blackjack.domain.user.Player;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
 
-import java.util.List;
+import java.util.*;
 
 public class GameTable {
     private static final String YES = "Y";
@@ -35,6 +35,15 @@ public class GameTable {
         OutputView.showCards(dealer, players);
     }
 
+    private void drawAtFirst() {
+        players.forEach(player -> {
+            player.hit(deck.pop());
+            player.hit(deck.pop());
+        });
+        dealer.hit(deck.pop());
+        dealer.hit(deck.pop());
+    }
+
     private void play() {
         players.forEach(this::askHit);
         while (dealer.canHit()) {
@@ -43,36 +52,40 @@ public class GameTable {
         }
     }
 
-    private void finish() {
-        OutputView.printCardsAndScore(dealer, players);
-        Result result = new Result();
-        List<Integer> matchResult = dealer.calculateMatchResult(result.getResult(dealer, players));
-        OutputView.printResult(matchResult, result.getResult(dealer, players));
-    }
-
     private void askHit(Player player) {
-        String hitValue = NO;
+        String doesPlayerWantMoreCard;
 
         do {
             OutputView.printHitGuideMessage(player);
-            hitValue = InputView.getHitValue();
-            draw(player, hitValue);
-        } while (player.isNotBust() && hitValue.equals(YES));
+            doesPlayerWantMoreCard = InputView.getHitValue();
+            draw(player, doesPlayerWantMoreCard);
+        } while (player.isNotBust() && doesPlayerWantMoreCard.equals(YES));
     }
 
-    private void draw(Player player, String hitValue) {
-        if (hitValue.equals(YES)) {
+    private void draw(Player player, String doesPlayerWantMoreCard) {
+        if (doesPlayerWantMoreCard.equals(YES)) {
             player.hit(deck.pop());
             OutputView.printPlayerCards(player);
         }
     }
 
-    private void drawAtFirst() {
-        players.forEach(player -> {
-            player.hit(deck.pop());
-            player.hit(deck.pop());
-        });
-        dealer.hit(deck.pop());
-        dealer.hit(deck.pop());
+    private void finish() {
+        OutputView.printCardsAndScore(dealer, players);
+        Map<Player, Status> result = getMatchResult();
+        List<Integer> matchResultCount = getMatchResultCount(result);
+        OutputView.printResult(matchResultCount, result);
+    }
+
+    private Map<Player, Status> getMatchResult() {
+        Map<Player, Status> result = new HashMap<>();
+        players.forEach(player -> result.put(player, dealer.compare(player)));
+        return result;
+    }
+
+    private List<Integer> getMatchResultCount(Map<Player, Status> result) {
+        List<Integer> matchResultCount = new ArrayList<>();
+        Arrays.stream(Status.values())
+                .forEach(status -> matchResultCount.add(Collections.frequency(result.values(), status)));
+        return matchResultCount;
     }
 }
