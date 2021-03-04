79412f6f5de671a59b6626194c6202b649cc14f4
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-03-04 20:59:05 +0900

    refactor : 상태에 따라 승무패를 검출하는 BiPredicate 구현

--- src/main/java/blackjack/domain/scoreboard/WinOrLose.java
+++ src/main/java/blackjack/domain/scoreboard/WinOrLose.java
@@ -1,46 +1,44 @@
 package blackjack.domain.scoreboard;
 
 import blackjack.domain.user.Dealer;
-import blackjack.domain.user.status.Status;
 import blackjack.domain.user.User;
 
 import java.util.Arrays;
 import java.util.function.BiPredicate;
 
+import static blackjack.domain.user.status.Status.BURST;
+
 public enum WinOrLose {
-    WIN("승", (dealerScore, userScore) -> dealerScore < userScore),
-    DRAW("무", Integer::equals),
-    LOSE("패", (dealerScore, userScore) -> dealerScore > userScore);
+    WIN("승"
+            , (dealer, user) -> dealer.isSameStatus(BURST) && user.isNotStatus(BURST)
+            , (dealerScore, userScore) -> dealerScore < userScore),
+    DRAW("무"
+            , (dealer, user) -> dealer.isSameStatus(BURST) && user.isSameStatus(BURST)
+            , Integer::equals),
+    LOSE("패"
+            , (dealer, user) -> dealer.isNotStatus(BURST) && user.isSameStatus(BURST)
+            , (dealerScore, userScore) -> dealerScore > userScore);
 
     private static final String NONE_MATCH_CONDITION_ERROR_MSG = "승무패 조건에 없는 경우입니다.";
+
     private final String character;
+    private final BiPredicate<Dealer, User> statusCompareCondition;
     private final BiPredicate<Integer, Integer> scoreCompareCondition;
 
-    WinOrLose(String character, BiPredicate<Integer, Integer> scoreCompareCondition) {
+    WinOrLose(String character, BiPredicate<Dealer, User> statusCompareCondition, BiPredicate<Integer, Integer> scoreCompareCondition) {
         this.character = character;
+        this.statusCompareCondition = statusCompareCondition;
         this.scoreCompareCondition = scoreCompareCondition;
     }
 
     public static WinOrLose decideWinOrLose(User user, Dealer dealer) {
-        if (dealer.isSameStatus(Status.BURST)) {
-            return decideWinOrLoseWhenDealerIsBurst(user);
-        }
-
-        return decideWinOrLoseWhenDealerIsNotBurst(user, dealer);
+        return Arrays.stream(values())
+                .filter(winOrLose -> winOrLose.statusCompareCondition.test(dealer, user))
+                .findFirst()
+                .orElseGet(() -> decideWinOrLoseByScore(user, dealer));
     }
 
-    private static WinOrLose decideWinOrLoseWhenDealerIsBurst(User user) {
-        if (user.isSameStatus(Status.BURST)) {
-            return DRAW;
-        }
-        return WIN;
-    }
-
-    private static WinOrLose decideWinOrLoseWhenDealerIsNotBurst(User user, Dealer dealer) {
-        if (user.isSameStatus(Status.BURST)) {
-            return LOSE;
-        }
-
+    private static WinOrLose decideWinOrLoseByScore(User user, Dealer dealer) {
         return Arrays.stream(values())
                 .filter(winOrLose -> winOrLose.scoreCompareCondition.test(dealer.calculateScore(), user.calculateScore()))
                 .findFirst()
