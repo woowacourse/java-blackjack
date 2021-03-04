db2914cf62a7697a68b896e798db08c32205e378
김주원 <kjw11077@naver.com>
2021-03-04 14:40:27 +0900

    refactor: InputView 리팩터링

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -5,17 +5,25 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final String YES = "y";
+    private static final String NO = "n";
+
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static String getPlayerName() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return scanner.nextLine();
+        return SCANNER.nextLine();
     }
 
     public static boolean wantsReceive(Player player) {
-        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)", player.getName());
+        askShouldDrawAnotherCard(player.getName());
 
-        String input = scanner.nextLine();
-        return input.equals("y");
+        //TODO n 이외의 값이 와도 false? n 이외의 값이면 어떻게 처리할지 고민
+        String input = SCANNER.nextLine();
+        return YES.equals(input);
+    }
+
+    private static void askShouldDrawAnotherCard(String playerName) {
+        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 %s, 아니오는 %s)", playerName, YES, NO);
     }
 }
