26cfba5ffcdf0b2a0deba9014d3cf396688f8a2a
Sehwan-Jang <whilter08@gmail.com>
2021-03-04 15:31:16 +0900

    style : Enum 필드에 final 키워드 추가

--- src/main/java/blackjack/domain/card/CardValue.java
+++ src/main/java/blackjack/domain/card/CardValue.java
@@ -2,25 +2,26 @@ package blackjack.domain.card;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public enum CardValue {
     ACE("A", Arrays.asList(1, 11)),
-    TWO("2", Arrays.asList(2)),
-    THREE("3", Arrays.asList(3)),
-    FOUR("4", Arrays.asList(4)),
-    FIVE("5", Arrays.asList(5)),
-    SIX("6", Arrays.asList(6)),
-    SEVEN("7", Arrays.asList(7)),
-    EIGHT("8", Arrays.asList(8)),
-    NINE("9", Arrays.asList(9)),
-    TEN("10", Arrays.asList(10)),
-    JACK("J", Arrays.asList(10)),
-    QUEEN("Q", Arrays.asList(10)),
-    KING("K", Arrays.asList(10));
+    TWO("2", Collections.singletonList(2)),
+    THREE("3", Collections.singletonList(3)),
+    FOUR("4", Collections.singletonList(4)),
+    FIVE("5", Collections.singletonList(5)),
+    SIX("6", Collections.singletonList(6)),
+    SEVEN("7", Collections.singletonList(7)),
+    EIGHT("8", Collections.singletonList(8)),
+    NINE("9", Collections.singletonList(9)),
+    TEN("10", Collections.singletonList(10)),
+    JACK("J", Collections.singletonList(10)),
+    QUEEN("Q", Collections.singletonList(10)),
+    KING("K", Collections.singletonList(10));
 
-    private String name;
-    private List<Integer> values;
+    private final String name;
+    private final List<Integer> values;
 
     CardValue(String name, List<Integer> values) {
         this.name = name;
