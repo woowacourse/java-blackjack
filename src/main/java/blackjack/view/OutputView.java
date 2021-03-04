d2b32a6c63485937b1f233805c6cae7dbe8f4fe7
Jinhong <xlffm3@gmail.com>
2021-03-04 16:38:37 +0900

    style: 메서드명 변경 및 불필요한 상수 삭제

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -12,10 +12,8 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
-
     private static final int FIRST_CARD_INDEX = 0;
     private static final String DELIMITER = ", ";
-    private static final String NEWLINE = System.getProperty("line.separator");
 
     private OutputView() {
     }
@@ -24,74 +22,77 @@ public class OutputView {
         String playerNames = players.stream()
                 .map(Player::getName)
                 .collect(Collectors.joining(DELIMITER));
-        printEmptyLine();
-        System.out.printf("%s와 %s에게 2장의 카드를 나누었습니다." + NEWLINE, dealer.getName(), playerNames);
-        printDefaultDealerCard(dealer);
-        printDefaultPlayerCards(players);
+        printNewLine();
+        System.out.printf("%s와 %s에게 2장의 카드를 나누었습니다.", dealer.getName(), playerNames);
+        printNewLine();
+        printDealerDefaultCard(dealer);
+        printPlayerAllDefaultCards(players);
     }
 
-    private static void printDefaultDealerCard(Dealer dealer) {
+    private static void printDealerDefaultCard(Dealer dealer) {
         List<Card> dealerCards = dealer.getCards();
         Card firstCardOfDealer = dealerCards.get(FIRST_CARD_INDEX);
-        System.out.printf("%s : %s" + NEWLINE, dealer.getName(),
-                getCardInformation(firstCardOfDealer));
+        System.out.printf("%s : %s", dealer.getName(), parseCardInformation(firstCardOfDealer));
+        printNewLine();
     }
 
-    private static String getCardInformation(Card card) {
-        return card.getSymbol()
-                .getName() + card.getShape()
-                .getName();
+    private static String parseCardInformation(Card card) {
+        return card.getSymbol().getName() + card.getShape().getName();
     }
 
-    private static void printDefaultPlayerCards(List<Player> players) {
+    private static void printPlayerAllDefaultCards(List<Player> players) {
         players.forEach(OutputView::printEachPlayerCards);
-        printEmptyLine();
+        printNewLine();
     }
 
     public static void printEachPlayerCards(Player player) {
-        String cards = getCardsInformation(player.getCards());
-        System.out.println(player.getName() + "카드: " + cards);
+        String cardsInformation = parseCardsInformation(player.getCards());
+        System.out.printf("%s 카드: %s", player.getName(), cardsInformation);
+        printNewLine();
     }
 
-    private static String getCardsInformation(List<Card> cards) {
+    private static String parseCardsInformation(List<Card> cards) {
         return cards.stream()
-                .map(OutputView::getCardInformation)
+                .map(OutputView::parseCardInformation)
                 .collect(Collectors.joining(DELIMITER));
     }
 
     public static void printDealerDrawingMessage(Dealer dealer) {
-        printEmptyLine();
-        System.out.println(dealer.getName() + "는 16이하라 한 장의 카드를 더 받았습니다.");
+        printNewLine();
+        System.out.printf("%s는 16이하라 한 장의 카드를 더 받았습니다.", dealer.getName());
     }
 
     public static void printFinalCardsAndScore(Participants participants) {
+        printNewLine();
         participants.toList()
                 .forEach(OutputView::printParticipantCardWithScore);
     }
 
     private static void printParticipantCardWithScore(Participant participant) {
-        String cards = getCardsInformation(participant.getCards());
-        System.out.println(
-                participant.getName() + " 카드: " + cards + " - 결과: "
-                        + participant.calculateFinalScore());
+        String cardsInformation = parseCardsInformation(participant.getCards());
+        System.out.printf("%s 카드: %s - 결과: %d", participant.getName(), cardsInformation, participant.calculateFinalScore());
+        printNewLine();
     }
 
     public static void printFinalResult(Dealer dealer, List<Player> players) {
-        printEmptyLine();
+        printNewLine();
         System.out.println("## 최종 승패");
-        Map<Result, Long> map = dealer.getStatisticResult(players);
+        Map<Result, Long> map = dealer.getStatisticsResult(players);
         long winCounts = map.get(Result.WIN);
         long lossCounts = map.get(Result.LOSE);
         long drawCounts = map.get(Result.DRAW);
-        System.out.println(
-                dealer.getName() + ": " + winCounts + "승 " + drawCounts + "무 " + lossCounts + "패");
-        for (Player player : players) {
-            Result result = player.judgeResult(dealer);
-            System.out.println(player.getName() + ": " + result.getName());
-        }
+        System.out.printf("%s: %d승 %d무 %d패", dealer.getName(), winCounts, drawCounts, lossCounts);
+        printNewLine();
+        players.forEach(player -> printEachPlayerFinalResult(player, dealer));
     }
 
-    private static void printEmptyLine() {
+    private static void printEachPlayerFinalResult(Player player, Dealer dealer) {
+        Result result = player.judgeResult(dealer);
+        System.out.printf("%s: %s", player.getName(), result.getName());
+        printNewLine();
+    }
+
+    private static void printNewLine() {
         System.out.println();
     }
 }
