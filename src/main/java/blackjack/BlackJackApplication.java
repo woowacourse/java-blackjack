1dd239f3d5f2937bedca8f02cbeab9bd87d033e8
김주원 <kjw11077@naver.com>
2021-03-04 14:38:33 +0900

    feat: main을 controller 역할로 변경

--- src/main/java/blackjack/BlackJackApplication.java
+++ src/main/java/blackjack/BlackJackApplication.java
@@ -1,12 +1,58 @@
 package blackjack;
 
-import blackjack.controller.BlackJackController;
+import blackjack.domain.game.BlackJackGame;
+import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Player;
+import blackjack.domain.result.GameResult;
+import blackjack.view.InputView;
+import blackjack.view.OutputView;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BlackJackApplication {
 
     public static void main(String[] args) {
-        BlackJackController controller = new BlackJackController();
+        Dealer dealer = new Dealer();
+        List<Player> players = getPlayers();
+        BlackJackGame blackJackGame = new BlackJackGame(dealer, players);
 
-        controller.run();
+        progressPlayersTurn(dealer, players);
+        progressDealerTurn(dealer);
+
+        GameResult result = blackJackGame.start();
+        OutputView.printResult(result);
+    }
+
+    private static List<Player> getPlayers() {
+        try {
+            String playerNames = InputView.getPlayerName();
+            return namesToPlayers(playerNames);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            return getPlayers();
+        }
+    }
+
+    private static List<Player> namesToPlayers(String playerNames) {
+        return Arrays.stream(playerNames.split(","))
+                .map(Player::from)
+                .collect(Collectors.toList());
+    }
+
+    private static void progressPlayersTurn(Dealer dealer, List<Player> players) {
+        for (Player player : players) {
+            if (player.lowerThanThreshold() && InputView.wantsReceive(player)) {
+                dealer.deal(player);
+                OutputView.printCardHandStatus(player);
+            }
+        }
+    }
+
+    private static void progressDealerTurn(Dealer dealer) {
+        while (dealer.shouldReceive()) {
+            OutputView.printDealerDrewMessage();
+            dealer.pickAnotherCard();
+        }
     }
 }
