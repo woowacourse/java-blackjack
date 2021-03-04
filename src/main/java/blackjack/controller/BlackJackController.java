c4054491ab6abbb56020c5983235dce649f7b962
perenok <perenoke@gmail.com>
2021-03-04 18:00:58 +0900

    refactor(BlackJackController) : separate run codes to check Exception and change method name

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -3,11 +3,13 @@ package blackjack.controller;
 import blackjack.domain.YesOrNo;
 import blackjack.domain.game.BlackJackGame;
 import blackjack.domain.game.Result;
-import blackjack.domain.player.Gambler;
+import blackjack.domain.player.Dealer;
 import blackjack.domain.player.Player;
 import blackjack.domain.player.Players;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
+
+import static blackjack.domain.YesOrNo.*;
 
 public class BlackJackController {
     static final BlackJackGame blackJackGame = new BlackJackGame(); // 요건 대화 필요
@@ -17,36 +19,54 @@ public class BlackJackController {
     }
 
     public static void run() {
-        Players players = blackJackGame.createPlayers(InputView.askPlayerNames());
+        Players players = createPlayers();
         blackJackGame.initPlayerCards(players);
-        OutputView.printPlayers(players);
+        OutputView.printPlayersCardInfo(players);
 
+        giveEachGamblerCard(players);
+        giveDealerCards();
+        OutputView.printNewLine();
+
+        Result result = blackJackGame.getResult(players);
+        OutputView.printResult(result);
+    }
+
+    private static Players createPlayers() {
+        try {
+            return blackJackGame.createPlayers(InputView.askPlayerNames());
+        } catch (Exception e) {
+            OutputView.printError(e);
+            return createPlayers();
+        }
+    }
+
+    private static void giveEachGamblerCard(final Players players) {
         for (Player player : players) {
             giveGamblerCard(player);
         }
-
-        giveDealerCards();
-        OutputView.printMessage("");
-        Result result = blackJackGame.getResult(players);
-        OutputView.printResult(result);
-
-
-
     }
 
-    private static void giveGamblerCard(Player player) {
-        if (!(player instanceof Gambler)) {
+    private static void giveGamblerCard(final Player player) {
+        if (player instanceof Dealer) {
             return;
         }
-
-        while (YesOrNo.of(InputView.askDrawOrNot(player)) == YesOrNo.YES ) {
+        while (decideDecision(player) == YES) {
             blackJackGame.giveCard(player);
-            OutputView.printPlayer(player);
+            OutputView.printPlayerCardInfo(player);
         }
     }
 
-    private static void giveDealerCards(){
-        while(blackJackGame.ableToDraw()){
+    private static YesOrNo decideDecision(final Player player) {
+        try {
+            return of(InputView.askDrawOrNot(player));
+        } catch (Exception e) {
+            OutputView.printError(e);
+            return decideDecision(player);
+        }
+    }
+
+    private static void giveDealerCards() {
+        while (blackJackGame.ableToDraw()) {
             OutputView.printGiveDealer();
             blackJackGame.giveDealerCard();
         }
