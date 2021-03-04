29af9d451f67eeecbbc0f9394b2194dd68aaa021
rok93 <goodboy302@naver.com>
2021-03-04 20:56:42 +0900

    test: 카드가 ACE 카드인지 확인하는 기능 테스트

--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -20,4 +20,19 @@ class CardTest {
         //then
         assertThat(card).isNotNull();
     }
+
+    @DisplayName("이 카드가 ACE 카드인지 확인한다")
+    @Test
+    void testIsAce() {
+        //given
+        CardType cardType = CardType.CLOVER;
+        CardValue cardValue = CardValue.ACE;
+        Card card = new Card(cardType, cardValue);
+
+        //when
+        boolean actual = card.isAce();
+
+        //then
+        assertThat(actual).isTrue();
+    }
 }
