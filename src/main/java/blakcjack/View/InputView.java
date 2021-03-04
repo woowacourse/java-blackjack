4393bd908fcb0556cda78694b392a5f9e0b40c40
sakjung <ssangyu123@gmail.com>
2021-03-04 17:42:48 +0900

    feat: y또는 n이 아닌 입력을 받았을 경우 다시 입력받도록 하는 기능 추가

--- src/main/java/blakcjack/View/InputView.java
+++ src/main/java/blakcjack/View/InputView.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 public class InputView {
 	private static final Scanner SCANNER = new Scanner(System.in);
+	public static final String YES = "y";
+	public static final String NO = "n";
 
 	public static List<String> takePlayerNamesInput() {
 		System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
@@ -16,7 +18,20 @@ public class InputView {
 	}
 
 	public static String takeHitOrStand(String name) {
-		System.out.println(name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
-		return SCANNER.nextLine().trim();
+		try {
+			System.out.println(name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+			String input = SCANNER.nextLine().trim();
+			validate(input);
+			return input;
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return takeHitOrStand(name);
+		}
+	}
+
+	private static void validate(final String input) {
+		if (!YES.equals(input) && !NO.equals(input)) {
+			throw new IllegalArgumentException(YES + "나 " + NO + "만 입력해주세요.");
+		}
 	}
 }
