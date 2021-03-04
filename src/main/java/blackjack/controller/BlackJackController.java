7f37284785a944b86297ba96feef322d7d02e9d9
MyaGya <aprn7950@gmail.com>
2021-03-04 14:32:29 +0900

    refactor: 컨트롤러 기능 분리

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -10,20 +10,31 @@ public class BlackJackController {
 
     public void run() {
         Dealer dealer = new Dealer();
-        Players players = new Players(InputView.enterNames(), dealer);
         Deck deck = new Deck();
+        Players players = new Players(InputView.enterNames(), dealer);
 
+        dealTwoCard(players, deck);
+        dealWantCard(players, deck);
+        printMatchResult(players);
+    }
+
+    private void printMatchResult(Players players) {
+        OutputView.noticePlayersPoint(players);
+        OutputView.noticeResult(players);
+    }
+
+    private void dealWantCard(Players players, Deck deck) {
+        while (players.startTurn(deck)) {
+            OutputView.noticePlayersCards(players);
+        }
+    }
+
+    private void dealTwoCard(Players players, Deck deck) {
         OutputView.noticeDrawTwoCards(players);
         int count = 2;
         for (int i = 0; i < count; i++) {
             players.giveCards(deck);
         }
         OutputView.noticePlayersCards(players);
-
-        while (players.startTurn(deck)) {
-            OutputView.noticePlayersCards(players);
-        }
-        OutputView.noticePlayersPoint(players);
-        OutputView.noticeResult(players);
     }
 }
