d990688a24ffb51c8632418b773aeaede0d50209
naeunkim <knae11@naver.com>
2021-03-04 17:59:53 +0900

    refactor: 상수처리

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -3,12 +3,13 @@ package blackjack.domain;
 import java.util.List;
 
 public class Dealer extends Playable {
+    private static final int MAX_AVAILABLE_TO_GET_CARD = 16;
     public Dealer(List<Card> cards) {
         super("딜러",cards);
     }
 
     @Override
     public boolean isAvailableToTake() {
-        return sumCards() <= 17;
+        return sumCards() <= MAX_AVAILABLE_TO_GET_CARD;
     }
 }
--- src/main/java/blackjack/domain/Playable.java
+++ src/main/java/blackjack/domain/Playable.java
@@ -3,6 +3,11 @@ package blackjack.domain;
 import java.util.List;
 
 public abstract class Playable {
+    public static final int BLACK_JACK = 21;
+    private static final int WIN = 1;
+    private static final int DRAW = 0;
+    private static final int LOSE = -1;
+    private static final int ACE_EXTRA_VALUE = 10;
     private final String name;
     private final Cards cards;
 
@@ -25,31 +30,31 @@ public abstract class Playable {
         int playerSum = sumCardsForResult();
 
         if (win(counterpart, playerSum)) {
-            return 1;
+            return WIN;
         }
 
         if (lose(counterpart, playerSum)) {
-            return -1;
+            return LOSE;
         }
 
-        return 0;
+        return DRAW;
     }
 
     private boolean lose(int counterpart, int playerSum) {
-        if (counterpart <= 21 && playerSum > 21) {
+        if (counterpart <= BLACK_JACK && playerSum > BLACK_JACK) {
             return true;
         }
-        if (counterpart <= 21 && counterpart > playerSum) {
+        if (counterpart <= BLACK_JACK && counterpart > playerSum) {
             return true;
         }
         return false;
     }
 
     private boolean win(int counterpart, int playerSum) {
-        if (counterpart > 21 && playerSum <= 21) {
+        if (counterpart > BLACK_JACK && playerSum <= BLACK_JACK) {
             return true;
         }
-        if (playerSum <= 21 && counterpart < playerSum) {
+        if (playerSum <= BLACK_JACK && counterpart < playerSum) {
             return true;
         }
         return false;
@@ -67,9 +72,9 @@ public abstract class Playable {
     public int sumCardsForResult() {
         List<Card> cardValues = cards.getUnmodifiableList();
         int aceCount = (int) cardValues.stream().filter(Card::isAce).count();
-        int sum = cardValues.stream().mapToInt(Card::getScore).sum() + aceCount * 10;
-        while (sum > 21 && aceCount > 0) {
-            sum -= 10;
+        int sum = cardValues.stream().mapToInt(Card::getScore).sum() + aceCount * ACE_EXTRA_VALUE;
+        while (sum > BLACK_JACK && aceCount > 0) {
+            sum -= ACE_EXTRA_VALUE;
             aceCount--;
         }
         return sum;
