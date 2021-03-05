22f94751d7489b53ef298e9ad364471ed4e5c204
Jinhong <xlffm3@gmail.com>
2021-03-06 02:16:59 +0900

    style: Player클래스 상수 분리

--- src/main/java/blackjack/domain/participant/Players.java
+++ src/main/java/blackjack/domain/participant/Players.java
@@ -12,6 +12,7 @@ public class Players {
     private static final int START_INDEX = 0;
     private static final String INVALID_PLAYER_COUNTS = "게임 참가자의 수는 딜러 제외 최소 1명 최대 7명입니다.";
     private static final String INVALID_INPUT_COUNTS_EQUALITY = "입력된 참가자의 이름 개수와 배팅 금액 개수가 일치하지 않습니다.";
+    private static final String DUPLICATION_PLAYER_NAMES = "참가자들의 이름은 중복이 없어야 합니다.";
 
     private final List<Player> players;
 
@@ -49,7 +50,7 @@ public class Players {
                 .distinct()
                 .count();
         if (playerCounts != distinctPlayerCounts) {
-            throw new IllegalArgumentException("참가자들의 이름은 중복이 없어야 합니다.");
+            throw new IllegalArgumentException(DUPLICATION_PLAYER_NAMES);
         }
     }
 
