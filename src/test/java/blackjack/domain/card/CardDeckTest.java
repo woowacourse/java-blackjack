bb96cb62c314f2d84431cb920125393eac27a5bc
cjy17 <cjy1705@naver.com>
2021-03-05 19:39:19 +0900

    test : cardDeckTest 수정

--- src/test/java/blackjack/domain/card/CardDeckTest.java
+++ src/test/java/blackjack/domain/card/CardDeckTest.java
@@ -3,13 +3,39 @@ package blackjack.domain.card;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CardDeckTest {
     @DisplayName("52장의 카드로 이루어진 덱이 생성되었는지 테스트한다.")
     @Test
     void deckCreationTest() {
-        CardDeck deck = CardDeck.createDeck();
-        assertThat(deck.getDeck()).hasSize(52);
+        CardDeck cardDeck = CardDeck.createDeck();
+        assertThat(cardDeck.getDeck()).hasSize(52);
+    }
+
+    @DisplayName("카드 드로우 기능 확인")
+    @Test
+    void cardDraw() {
+        CardDeck cardDeck = CardDeck.createDeck();
+        cardDeck.drawCard();
+
+        assertThat(cardDeck.getDeck()).hasSize(51);
+    }
+
+    @DisplayName("카드를 모두 뽑았을경우 에러발생 확인")
+    @Test
+    void noCardCheck() {
+        CardDeck cardDeck = CardDeck.createDeck();
+
+        for (int i = 0; i < 52; i++) {
+            cardDeck.drawCard();
+        }
+
+        assertThatThrownBy(()->{
+            cardDeck.drawCard();
+        }).isInstanceOf(NoSuchElementException.class);
     }
 }
