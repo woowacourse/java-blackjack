bf7e2dd9049413271cf6ba56c24e314e195b5f4d
Jinhong <xlffm3@gmail.com>
2021-03-06 01:19:21 +0900

    feat: 플레이어들의 수익금액 통계 기능 구현

---
+++ src/main/java/blackjack/domain/game/ResultStatistics.java
@@ -0,0 +1,35 @@
+package blackjack.domain.game;
+
+import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Players;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResultStatistics {
+    private static final int NEGATIVE_NUMBER_INDEX = -1;
+
+    private final Map<String, Integer> profitResultStatistics;
+
+    private ResultStatistics(Map<String, Integer> profitResultStatistics) {
+        this.profitResultStatistics = profitResultStatistics;
+    }
+
+    public static ResultStatistics of(Dealer dealer, Players players) {
+        Map<String, Integer> profitResultStatistics = new HashMap<>();
+        players.toList()
+                .forEach(player -> profitResultStatistics.put(player.getName(), player.calculateProfitMoney(dealer)));
+        return new ResultStatistics(profitResultStatistics);
+    }
+
+    public int calculateDealerProfit() {
+        return profitResultStatistics.values()
+                .stream()
+                .mapToInt(profit -> profit)
+                .sum() * NEGATIVE_NUMBER_INDEX;
+    }
+
+    public Map<String, Integer> getProfitResultStatistics() {
+        return profitResultStatistics;
+    }
+}
---
+++ src/test/java/blackjack/domain/game/ResultStatisticsTest.java
@@ -0,0 +1,73 @@
+package blackjack.domain.game;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Cards;
+import blackjack.domain.card.Shape;
+import blackjack.domain.card.Symbol;
+import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Player;
+import blackjack.domain.participant.Players;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ResultStatisticsTest {
+
+    private static final List<Card> CARDS_BLACKJACK = Arrays.asList(
+            new Card(Symbol.JACK, Shape.HEART),
+            new Card(Symbol.ACE, Shape.DIAMOND)
+    );
+    private static final List<Card> CARDS_SCORE_20 = Arrays.asList(
+            new Card(Symbol.ACE, Shape.HEART),
+            new Card(Symbol.KING, Shape.HEART),
+            new Card(Symbol.NINE, Shape.HEART)
+    );
+    private static final List<Card> CARDS_SCORE_19 = Arrays.asList(
+            new Card(Symbol.ACE, Shape.HEART),
+            new Card(Symbol.KING, Shape.HEART),
+            new Card(Symbol.EIGHT, Shape.HEART)
+    );
+
+    private Players players;
+    private Dealer dealer;
+
+    @BeforeEach
+    void setup() {
+        players = Players.of(Arrays.asList("pobi", "jason"), Arrays.asList(1000, 2000));
+        dealer = new Dealer();
+        List<Player> playerList = players.toList();
+        playerList.get(0).receiveCards(new Cards(CARDS_BLACKJACK));
+        playerList.get(1).receiveCards(new Cards(CARDS_SCORE_19));
+        dealer.receiveCards(new Cards(CARDS_SCORE_20));
+    }
+
+    @DisplayName("Player의 이름을 Key로, 각각의 이익금액을 value로 하는 Map을 생성")
+    @Test
+    void createGameResult() {
+        ResultStatistics resultStatistics = ResultStatistics.of(dealer, players);
+
+        Collection<Integer> values = resultStatistics.getProfitResultStatistics()
+                .values();
+
+        assertThat(values).containsExactly(1500, -2000);
+    }
+
+    @DisplayName("딜러의 손익은 플레이어 손익의 합 * -1과 같다.")
+    @Test
+    void calculateDealerProfit() {
+        ResultStatistics resultStatistics = ResultStatistics.of(dealer, players);
+        Collection<Integer> values = resultStatistics.getProfitResultStatistics()
+                .values();
+
+        int playerProfitTotal = values.stream().mapToInt(t -> t).sum();
+        int dealerProfit = resultStatistics.calculateDealerProfit();
+
+        assertThat(dealerProfit).isEqualTo(playerProfitTotal * -1);
+    }
+}
