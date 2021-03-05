4fa205c73b248a4480caadf36141e8aaa89b2113
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-03-05 23:33:07 +0900

    feat: y 또는 n 입력 유효성 검증 추가

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class InputView {
     private static final String INPUT_GAMER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String INPUT_HIT_OR_STAND = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
+    private static final String Y_OR_N_ERROR = "[ERROR] y 또는 n을 입력해주세요.";
+    private static final String YES = "y";
+    private static final String NO = "n";
     private static final Scanner scanner = new Scanner(System.in);
 
     private InputView() {
@@ -12,12 +15,19 @@ public class InputView {
 
     public static String[] inputGamerNames() {
         OutputView.println(INPUT_GAMER_NAMES);
-        return scanner.nextLine().split(",");
+        return scanner.nextLine().split(",", -1);
     }
 
     public static boolean inputHitOrStand(String gamerName) {
         System.out.printf(INPUT_HIT_OR_STAND, gamerName);
-        String input = scanner.nextLine();
-        return "y".equals(input);
+        String input = scanner.nextLine().toLowerCase();
+        validateInput(input);
+        return YES.equals(input);
+    }
+
+    private static void validateInput(String input) {
+        if (!YES.equals(input) && !NO.equals(input)) {
+            throw new IllegalArgumentException(Y_OR_N_ERROR);
+        }
     }
 }
