cc41c85c83465c623b24efc732b1d2cf18116128
jujubebat <bbwwpark@naver.com>
2021-03-05 17:02:32 +0900

    refactor: view에서 사용되는 문자열을 상수로 포장함

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -9,16 +9,18 @@ public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String DELIMITER = ",";
+    private static final String PLAYERS_NAME_INPUT_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String DRAW_ONE_MORE_CARD_MESSAGE_FORMAT = "%s은 한 장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)\n";
 
     public static List<String> getPlayerNames() {
-        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        System.out.println(PLAYERS_NAME_INPUT_MESSAGE);
         return Arrays.stream(SCANNER.nextLine().split(DELIMITER))
             .map(String::trim)
             .collect(Collectors.toList());
     }
 
     public static boolean getWhetherDrawCard(String name) {
-        System.out.println("\n" + name + "은 한 장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)");
+        System.out.printf(DRAW_ONE_MORE_CARD_MESSAGE_FORMAT, name);
         String input = SCANNER.nextLine();
         if (input.equals("y")) {
             return true;
--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -13,61 +13,65 @@ public class OutputView {
     private static final String NEW_LINE = System.lineSeparator();
     private static final String DELIMITER = ", ";
     private static final String NAME_SEPARATOR = ": ";
-
+    private static final String HAND_OUT_CARDS_MESSAGE_FORMAT = "%s와 %s에게 두 장의 카드를 나눠줬습니다.\n";
+    private static final String PRINT_CARDS_FORMAT = "%s카드: %s\n";
+    private static final String DEALER_ONE_MORE_DRAW_MESSAGE = "\n딜러는 16이하라 한장의 카드를 더 받았습니다.\n\n";
+    private static final String PRINT_CARDS_SCORE_FORMAT = "%s카드: %s - 결과: %d\n";
+    private static final String FINAL_RESULT_MESSAGE = "\n## 최종승패";
+    private static final String WIN_MESSAGE = "승 ";
+    private static final String DRAW_MESSAGE = "무 ";
+    private static final String LOSE_MESSAGE = "패 ";
 
     public static void printHandOutCardsMessage(Dealer dealer, List<Player> players) {
-        System.out.print(dealer.getName() + "와 ");
         List<String> playerNames = new ArrayList<>();
 
         for (Player player : players) {
             playerNames.add(player.getName());
         }
-        System.out.println(String.join(DELIMITER, playerNames) + "에게 두 장의 카드를 나눠줬습니다.");
+        System.out.printf(HAND_OUT_CARDS_MESSAGE_FORMAT, dealer.getName(),
+            String.join(DELIMITER, playerNames));
     }
 
     public static void printParticipantCards(String participantName, Cards cards) {
-        System.out.print(participantName + "카드: ");
         List<String> results = new ArrayList<>();
-
         for (Card card : cards.getCards()) {
             results.add(card.getType().getName() + card.getDenomination().getName());
         }
-        System.out.println(String.join(DELIMITER, results));
+        System.out.printf(PRINT_CARDS_FORMAT, participantName, String.join(DELIMITER, results));
     }
 
     public static void printDealerCardDrawMessage() {
-        System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.\n");
+        System.out.print(DEALER_ONE_MORE_DRAW_MESSAGE);
     }
 
     public static void printParticipantCardsWithScore(String participantName, Cards cards) {
-        System.out.print(participantName + "카드: ");
         List<String> results = new ArrayList<>();
 
         for (Card card : cards.getCards()) {
             results.add(card.getType().getName() + card.getDenomination().getName());
         }
-        System.out.println(String.join(DELIMITER, results) + " - 결과: " + cards.getScore());
+        System.out.printf(PRINT_CARDS_SCORE_FORMAT, participantName, String.join(DELIMITER, results),
+            cards.getScore());
     }
 
-
     public static void printGameResult(List<Player> players, Dealer dealer) {
-        System.out.println("\n## 최종승패");
+        System.out.println(FINAL_RESULT_MESSAGE);
         printDealerGameResult(dealer);
-        System.out.print(NEW_LINE);
         printPlayersGameResults(players);
     }
 
     private static void printDealerGameResult(Dealer dealer) {
         System.out.print(dealer.getName() + " ");
         if (dealer.getResultCount(Result.WIN) > 0) {
-            System.out.print(dealer.getResultCount(Result.WIN) + "승 ");
+            System.out.print(dealer.getResultCount(Result.WIN) + WIN_MESSAGE);
         }
         if (dealer.getResultCount(Result.DRAW) > 0) {
-            System.out.print(dealer.getResultCount(Result.DRAW) + "무 ");
+            System.out.print(dealer.getResultCount(Result.DRAW) + DRAW_MESSAGE);
         }
         if (dealer.getResultCount(Result.LOSE) > 0) {
-            System.out.print(dealer.getResultCount(Result.LOSE) + "패 ");
+            System.out.print(dealer.getResultCount(Result.LOSE) + LOSE_MESSAGE);
         }
+        System.out.print(NEW_LINE);
     }
 
     private static void printPlayersGameResults(List<Player> players) {
@@ -77,12 +81,9 @@ public class OutputView {
     }
 
     public static void printDealerCards(String participantName, Cards cards) {
-        System.out.print(participantName + "카드: ");
-
         Card card = cards.getCards().get(0);
         List<String> results = new ArrayList<>();
-
         results.add(card.getType().getName() + card.getDenomination().getName());
-        System.out.println(String.join(DELIMITER, results));
+        System.out.printf(PRINT_CARDS_FORMAT, participantName, String.join(DELIMITER, results));
     }
 }
