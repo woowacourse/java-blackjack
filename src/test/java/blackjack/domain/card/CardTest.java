9798ed0f38a47aba88c39dcbd8ff8fed597dff90
김주원 <kjw11077@naver.com>
2021-03-05 21:47:43 +0900

    test: Card 테스트 추가

--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -3,16 +3,15 @@ package blackjack.domain.card;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class CardTest {
 
-    @ParameterizedTest(name = "값 객체 비교")
-    @MethodSource("equals_testcase")
-    void equals(Suit suit, Rank rank) {
+    @ParameterizedTest(name = "값 객체 비교 - 성공")
+    @MethodSource("equals_success_testcase")
+    void equals_success(Suit suit, Rank rank) {
         Card card1 = new Card(suit, rank);
         Card card2 = new Card(suit, rank);
 
@@ -20,7 +19,7 @@ class CardTest {
         assertThat(card1).hasSameHashCodeAs(card2);
     }
 
-    private static Stream<Arguments> equals_testcase() {
+    private static Stream<Arguments> equals_success_testcase() {
         return Stream.of(
                 Arguments.of(Suit.SPADE, Rank.ACE),
                 Arguments.of(Suit.SPADE, Rank.KING),
@@ -33,11 +32,19 @@ class CardTest {
         );
     }
 
-    @Test
-    void equals_fail() {
-        Card card = new Card(Suit.SPADE, Rank.ACE);
-        Card card1 = new Card(Suit.SPADE, Rank.TWO);
+    @ParameterizedTest(name = "값 객체 비교 - 실패")
+    @MethodSource("equals_fail_testcase")
+    void equals_fail(Suit suit, Rank rank) {
+        Card card = new Card(suit, rank);
 
-        assertThat(card).isNotEqualTo(card1);
+        assertThat(card).isNotEqualTo(new Card(Suit.SPADE, Rank.ACE));
+    }
+
+    private static Stream<Arguments> equals_fail_testcase() {
+        return Stream.of(
+                Arguments.of(Suit.SPADE, Rank.TWO),
+                Arguments.of(Suit.CLOVER, Rank.ACE),
+                Arguments.of(Suit.CLOVER, Rank.TWO)
+        );
     }
 }
