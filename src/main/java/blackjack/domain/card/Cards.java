027e3a5bdad0870d73ada5a4e880024145f40b5f
Jinhong <xlffm3@gmail.com>
2021-03-05 16:10:55 +0900

    feat: Cards 생성시 중복 검사 기능 구현

--- src/main/java/blackjack/domain/card/Cards.java
+++ src/main/java/blackjack/domain/card/Cards.java
@@ -2,18 +2,29 @@ package blackjack.domain.card;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Cards {
+    private static final String DUPLICATION_CARDS = "중복된 카드는 보유할 수 없습니다.";
 
     private final List<Card> cards;
 
-    public Cards() {
-        this.cards = new ArrayList<>();
+    public Cards(List<Card> cards) {
+        validateCardDuplication(cards);
+        this.cards = cards;
     }
 
-    public Cards(List<Card> cards) {
-        this.cards = cards;
+    public Cards() {
+        this(new ArrayList<>());
+    }
+
+    private void validateCardDuplication(List<Card> cards) {
+        int cardCounts = cards.size();
+        int distinctCardCounts = new HashSet<>(cards).size();
+        if (cardCounts != distinctCardCounts) {
+            throw new IllegalArgumentException(DUPLICATION_CARDS);
+        }
     }
 
     public void add(Card card) {
--- src/test/java/blackjack/domain/card/CardsTest.java
+++ src/test/java/blackjack/domain/card/CardsTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 class CardsTest {
 
@@ -22,6 +23,18 @@ class CardsTest {
                         new Card(Symbol.NINE, Shape.CLOVER), new Card(Symbol.ACE, Shape.CLOVER)), 18),
                 Arguments.of(Arrays.asList(new Card(Symbol.ACE, Shape.CLOVER),
                         new Card(Symbol.NINE, Shape.CLOVER), new Card(Symbol.EIGHT, Shape.CLOVER)), 18));
+    }
+
+    @DisplayName("Cards 생성시 주어지는 Card가 중복되면 안 된다")
+    @Test
+    void checkDuplication() {
+        Card firstCard = new Card(Symbol.ACE, Shape.CLOVER);
+        Card secondCard = new Card(Symbol.ACE, Shape.CLOVER);
+
+        assertThatCode(() -> {
+            new Cards(Arrays.asList(firstCard, secondCard));
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 카드는 보유할 수 없습니다.");
     }
 
     @DisplayName("카드의 점수를 확인한다")
