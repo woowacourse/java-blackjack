e5e4798d3684fa51db2eaa58ec446fed18699a69
thisisyoungbin <thisisyoungbin@gmail.com>
2021-03-05 14:21:56 +0900

    test: 윈도우에선 개행 입력이 달라 테스트 실패하던 부분 수정 및 상수 설정

--- src/main/java/rentcompany/domain/RentCompany.java
+++ src/main/java/rentcompany/domain/RentCompany.java
@@ -4,25 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RentCompany {
-    private final List<Car> carList = new ArrayList<>();
+    public static final String PRINT_FORMAT = "%s : %.0f리터";
+    private final List<Car> CAR_LIST = new ArrayList<>();
+    private final String NEWLINE = System.getProperty("line.separator");
 
     public static RentCompany create() {
         return new RentCompany();
     }
 
     public void addCar(Car car) {
-        carList.add(car);
+        CAR_LIST.add(car);
     }
 
     public int size() {
-        return carList.size();
+        return CAR_LIST.size();
     }
 
     public String generateReport() {
         StringBuilder reportBuilder = new StringBuilder();
 
-        for (Car car : carList) {
-            reportBuilder.append(String.format("%s : %.0f리터\n", car.getName(), car.getChargeQuantity()));
+        for (Car car : CAR_LIST) {
+            reportBuilder.append(String.format(PRINT_FORMAT + NEWLINE, car.getName(), car.getChargeQuantity()));
         }
 
         return reportBuilder.toString();
