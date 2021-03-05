fbae66750b20c2ba45a61cd517c944c00df15340
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-03-05 11:12:20 +0900

    refactor: 일급 컬렉션이 ArrayList를 상속하도록 수정

--- src/main/java/blackjack/domain/user/Users.java
+++ src/main/java/blackjack/domain/user/Users.java
@@ -1,29 +1,18 @@
 package blackjack.domain.user;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
 
-public class Users {
+public class Users extends ArrayList<User> {
     private static final String DUPLICATE_NAME_ERROR_MSG = "중복된 이름은 존재할 수 없습니다.";
-
-    private final List<User> users;
 
     public Users(List<User> users) {
         Set<User> duplicateChecker = new HashSet<>(users);
         if (users.size() != duplicateChecker.size()) {
             throw new IllegalArgumentException(DUPLICATE_NAME_ERROR_MSG);
         }
-        this.users = users;
-    }
-
-    public void forEach(Consumer<? super User> consumer){
-        users.forEach(consumer);
-    }
-
-    public Stream<User> stream(){
-        return users.stream();
+        this.addAll(users);
     }
 }
