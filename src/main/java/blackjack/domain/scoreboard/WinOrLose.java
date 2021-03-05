e006b0c273356c47804e1599437dbf3dfdd0bd6a
thisisyoungbin <thisisyoungbin@gmail.com>
2021-03-05 21:38:12 +0900

    refactor: 승무패를 결정하는 로직 수정

--- src/main/java/blackjack/domain/scoreboard/WinOrLose.java
+++ src/main/java/blackjack/domain/scoreboard/WinOrLose.java
@@ -4,21 +4,15 @@ import blackjack.domain.user.Dealer;
 import blackjack.domain.user.Status;
 import blackjack.domain.user.User;
 
-import java.util.Arrays;
-import java.util.function.BiPredicate;
-
 public enum WinOrLose {
-    WIN("승", (dealerScore, userScore) -> dealerScore < userScore),
-    DRAW("무", Integer::equals),
-    LOSE("패", (dealerScore, userScore) -> dealerScore > userScore);
+    WIN("승"),
+    DRAW("무"),
+    LOSE("패");
 
-    private static final String NONE_MATCH_CONDITION_ERROR_MSG = "승무패 조건에 없는 경우입니다.";
     private final String character;
-    private final BiPredicate<Integer, Integer> scoreCompareCondition;
 
-    WinOrLose(String character, BiPredicate<Integer, Integer> scoreCompareCondition) {
+    WinOrLose(String character) {
         this.character = character;
-        this.scoreCompareCondition = scoreCompareCondition;
     }
 
     public static WinOrLose decideWinOrLose(User user, Dealer dealer) {
@@ -41,10 +35,11 @@ public enum WinOrLose {
             return LOSE;
         }
 
-        return Arrays.stream(values())
-                .filter(winOrLose -> winOrLose.scoreCompareCondition.test(dealer.calculateScore(), user.calculateScore()))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(NONE_MATCH_CONDITION_ERROR_MSG));
+        if (!user.isSameStatus(Status.BURST) && user.calculateScore() > dealer.calculateScore()) {
+            return WIN;
+        }
+
+        return DRAW;
     }
 
     public static WinOrLose opposite(WinOrLose winOrLose) {
