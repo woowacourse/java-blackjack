defcfb560db87c6086ea512b9386dc20324e1ff7
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-03-06 18:47:40 +0900

    refactor: 변수 이름 수정 및 유효성 검증 메서드 분리

--- src/main/java/blackjack/domain/player/Gamer.java
+++ src/main/java/blackjack/domain/player/Gamer.java
@@ -5,7 +5,7 @@ import blackjack.domain.card.Cards;
 
 public class Gamer extends Player {
     private static final String PLAYER_SCORE_RANGE_ERROR = "[ERROR] 플레이어의 점수가 21을 초과하여 카드를 추가할 수 없습니다.";
-    private static final int CAN_DRAW_MAXIMUM_SCORE = 21;
+    private static final int DRAW_MAXIMUM_SCORE = 21;
 
     public Gamer(Name name, Cards cards) {
         super(name, cards);
@@ -13,15 +13,19 @@ public class Gamer extends Player {
 
     @Override
     public void addCard(Card card) {
-        if (calculateScore() >= CAN_DRAW_MAXIMUM_SCORE) {
+        validateGamerCardScore();
+        cards.addCard(card);
+    }
+
+    private void validateGamerCardScore() {
+        if (calculateScore() >= DRAW_MAXIMUM_SCORE) {
             throw new IllegalArgumentException(PLAYER_SCORE_RANGE_ERROR);
         }
-        cards.addCard(card);
     }
 
     @Override
     public boolean canDraw() {
-        return calculateScore() < CAN_DRAW_MAXIMUM_SCORE;
+        return calculateScore() < DRAW_MAXIMUM_SCORE;
     }
 
     @Override
