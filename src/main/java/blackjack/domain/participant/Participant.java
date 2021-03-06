8d5e8b3ec6d4d71d4637220ec800bdb7b3d17199
TaewanKimmmm <ktww88@gmail.com>
2021-03-07 01:20:17 +0900

    refactor: Participant 인터페이스에서 추상 클래스로 변경

--- src/main/java/blackjack/domain/participant/Participant.java
+++ src/main/java/blackjack/domain/participant/Participant.java
@@ -1,13 +1,30 @@
 package blackjack.domain.participant;
 
 import blackjack.domain.card.Card;
+import blackjack.domain.card.Cards;
 import blackjack.domain.card.Deck;
 
-public interface Participant {
-    void drawAtFirst(Deck deck);
-    String showCardsAtFirst();
-    void hit(Card card);
-    String getCards();
-    String getName();
-    int getScore();
+public abstract class Participant {
+    private Cards cards;
+
+    public void drawAtFirst(Deck deck) {
+        hit(deck.pop());
+        hit(deck.pop());
+    }
+
+    public void hit(Card card) {
+        this.cards.addCard(card);
+    }
+
+    String getCards() {
+        return this.cards.getCards();
+    }
+
+    int getScore() {
+        return cards.getScore();
+    }
+
+    abstract String showCardsAtFirst();
+
+    abstract String getName();
 }
