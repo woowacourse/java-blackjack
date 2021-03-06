077e94cb094d7c25649103c77808835740111eef
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-07 01:35:55 +0900

    refactor: 카드 반환 전략 패턴 테스트 클래스 분리 및 패키지 경로 이동

--- src/test/java/blackjack/domain/player/DealerTest.java
+++ src/test/java/blackjack/domain/player/DealerTest.java
@@ -39,28 +39,4 @@ public class DealerTest {
         dealer.drawOneCard(sevenCard);
         assertThat(dealer.isCanDraw()).isFalse();
     }
-
-    @DisplayName("카드 반환 개수 전략패턴 테스트")
-    @Test
-    void cardOpenStrategy() {
-        Cards allCards = new Cards();
-        Dealer dealer = new Dealer();
-
-        dealer.drawRandomTwoCards(allCards);
-        assertThat(dealer.getCards().size()).isEqualTo(1);
-
-        dealer.setCardOpenStrategy(new AllCardsOpenStrategy());
-        assertThat(dealer.getCards().size()).isEqualTo(2);
-
-        dealer.setCardOpenStrategy(new OneCardOpenStrategy());
-        assertThat(dealer.getCards().size()).isEqualTo(1);
-
-        dealer.drawRandomTwoCards(allCards);
-
-        dealer.setCardOpenStrategy(new AllCardsOpenStrategy());
-        assertThat(dealer.getCards().size()).isEqualTo(4);
-
-        dealer.setCardOpenStrategy(new OneCardOpenStrategy());
-        assertThat(dealer.getCards().size()).isEqualTo(1);
-    }
 }
