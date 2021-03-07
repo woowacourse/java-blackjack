d4c788acdd650784fc09617e722be2963e3e8080
TaewanKimmmm <ktww88@gmail.com>
2021-03-08 02:35:38 +0900

    refactor: 블랙잭 스코어 21을 의미하는 상수 Cards 클래스에서 가져와서 쓰도록 변경

--- src/main/java/blackjack/domain/participant/Player.java
+++ src/main/java/blackjack/domain/participant/Player.java
@@ -1,7 +1,8 @@
 package blackjack.domain.participant;
 
+import static blackjack.domain.card.Cards.BLACKJACK_SCORE;
+
 public class Player extends Participant {
-    private static final int BLACK_JACK_SCORE= 21;
     private static final String NAME_INPUT_ERROR_MESSAGE = "이름은 1자 이상이어야 합니다.";
 
     private final String name;
@@ -18,7 +19,7 @@ public class Player extends Participant {
     }
 
     public boolean isNotBust() {
-        return cards.calculateScore() <= BLACK_JACK_SCORE;
+        return cards.calculateScore() <= BLACKJACK_SCORE;
     }
 
     @Override
