a82efa41ff99ebbc9235a14a512e9dbdfe36a448
sori <bosl95@naver.com>
2021-03-07 18:16:41 +0900

    feat: Deck을 추가하여 랜덤 카드를 관리 카드를 뽑고, 카드가 없으면 예외처리한다.

---
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -0,0 +1,28 @@
+package blackjack.domain.card;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Deck {
+    private final Deque<Card> shuffledCards;
+
+    public Deck(List<Card> shuffledCards) {
+        this.shuffledCards = new ArrayDeque<>(shuffledCards);
+    }
+
+    public Card drawCard() {
+        if (shuffledCards.isEmpty()) {
+            throw new IllegalArgumentException("뽑을 카드가 없습니다.");
+        }
+        return shuffledCards.pollLast();
+    }
+
+    public List<Card> drawCards(int number) {
+        return Stream.generate(() -> shuffledCards.pollLast())
+                .limit(number)
+                .collect(Collectors.toList());
+    }
+}
