31cab0c12916799b76e4ca38be2b4700d59fd700
sor <bosl95@naver.com>
2021-03-07 23:25:49 +0900

    refactor : AnswerTest 추가

---
+++ src/test/java/blackjack/domain/AnswerTest.java
@@ -0,0 +1,17 @@
+package blackjack.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnswerTest {
+
+    @DisplayName("'y' 입력 시 YES를, 'n' 입력 시 NO 객체를 반환한다.")
+    @Test
+    void generate_test() {
+        assertThat(Answer.YES.equals("y")).isTrue();
+        assertThat(Answer.NO .equals("n")).isTrue();
+    }
+}
