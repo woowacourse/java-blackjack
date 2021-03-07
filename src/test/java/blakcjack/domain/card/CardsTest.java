9ce01cc13e0a6e9188224909f29b76b32a1fee33
sakjung <ssangyu123@gmail.com>
2021-03-07 13:53:16 +0900

    test: cards에서 첫번째 카드를 뽑는 메서드에 대한 테스트 추가

--- src/test/java/blakcjack/domain/card/CardsTest.java
+++ src/test/java/blakcjack/domain/card/CardsTest.java
@@ -1,22 +1,32 @@
 package blakcjack.domain.card;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CardsTest {
-	private Cards cards = new Cards();
+	private final Cards cards = new Cards();
 
-	@DisplayName("스코어를 제대로 계산 해주는지")
-	@Test
-	void calculateScore() {
+	@BeforeEach
+	void setUp() {
 		cards.add(Card.of(CardSymbol.SPADE, CardNumber.ACE));
 		cards.add(Card.of(CardSymbol.HEART, CardNumber.ACE));
 		cards.add(Card.of(CardSymbol.DIAMOND, CardNumber.ACE));
 		cards.add(Card.of(CardSymbol.CLUB, CardNumber.ACE));
 		cards.add(Card.of(CardSymbol.CLUB, CardNumber.FIVE));
+	}
 
+	@DisplayName("스코어를 제대로 계산 해주는지")
+	@Test
+	void calculateScore() {
 		assertThat(cards.calculateScore()).isEqualTo(19);
+	}
+
+	@DisplayName("카드들 중에서 첫번째 인덱스의 카드를 제대로 뽑아 내는지")
+	@Test
+	void getFirstCard_multipleCards_returnCardAtFirstPosition() {
+		assertThat(cards.getFirstCard()).isEqualTo(Card.of(CardSymbol.SPADE, CardNumber.ACE));
 	}
 }
