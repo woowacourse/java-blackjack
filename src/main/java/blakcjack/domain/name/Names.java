77f9d26334c2c56f15d514172f9eb8fed6aeb8a7
sakjung <ssangyu123@gmail.com>
2021-03-07 13:52:50 +0900

    test: names에 대한 테스트 추가

--- src/main/java/blakcjack/domain/name/Names.java
+++ src/main/java/blakcjack/domain/name/Names.java
@@ -28,4 +28,17 @@ public class Names {
 	public List<Name> toList() {
 		return Collections.unmodifiableList(names);
 	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		final Names names1 = (Names) o;
+		return Objects.equals(names, names1.names);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(names);
+	}
 }
---
+++ src/test/java/blakcjack/domain/name/NamesTest.java
@@ -0,0 +1,27 @@
+package blakcjack.domain.name;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static blakcjack.domain.name.DuplicatePlayerNamesException.DUPLICATE_NAME_ERROR;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NamesTest {
+	@DisplayName("생성자 테스트")
+	@Test
+	void create() {
+		Names names = new Names(Arrays.asList("sakjung", "mediumBear"));
+		assertThat(names).isEqualTo(new Names(Arrays.asList("sakjung", "mediumBear")));
+	}
+
+	@DisplayName("중복된 이름을 받았을 떄 에러 반환하는지")
+	@Test
+	void validateDuplication_duplicatePlayerNames_throwError() {
+		assertThatThrownBy(() -> new Names(Arrays.asList("sakjung", "mediumBear", "sakjung")))
+				.isInstanceOf(DuplicatePlayerNamesException.class)
+				.hasMessage(DUPLICATE_NAME_ERROR);
+	}
+}
