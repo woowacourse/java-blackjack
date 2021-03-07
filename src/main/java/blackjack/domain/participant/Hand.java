a89aac7650511e8ce496685348838be59103c917
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-03-07 17:26:41 +0900

    feat(Hand): 참가자의 패를 관리하는 Hand 클래스 생성

--- src/main/java/blackjack/domain/participant/Hand.java
+++ src/main/java/blackjack/domain/participant/Hand.java
@@ -1,26 +1,78 @@
 package blackjack.domain.participant;
 
 import blackjack.domain.card.Card;
+import blackjack.domain.card.CardNumber;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Hand {
+    private static final int BUST_LIMIT = 22;
+
     private final List<Card> hand;
 
     public Hand() {
         this.hand = new ArrayList<>();
     }
 
-    public void receiveCard(final Card card) {
+    public void add(final Card card) {
         hand.add(card);
     }
 
     public int calculateScore() {
+        final int aceCount = (int) hand.stream()
+                .filter(Card::isAce)
+                .count();
+
+        if (aceCount == 0) {
+            return calculateSingleCase();
+        }
+        return calculateMultipleCase(aceCount);
+    }
+
+    private int calculateSingleCase() {
         return hand.stream()
                 .mapToInt(Card::getCardValue)
                 .sum();
+    }
+
+    private int calculateMultipleCase(final int aceCount) {
+        final List<Integer> possibleSum = new ArrayList<>();
+        final int sumExceptAce = hand.stream()
+                .filter(card -> !card.isAce())
+                .mapToInt(Card::getCardValue)
+                .sum();
+
+        for (final int aceSum : calculateAceSum(aceCount)) {
+            possibleSum.add(sumExceptAce + aceSum);
+        }
+        return findMaxPossibleValue(possibleSum);
+    }
+
+    private List<Integer> calculateAceSum(final int aceCount) {
+        int oneNormalRestExtra = CardNumber.ACE.getValue();
+        int allExtra = CardNumber.ACE.getExtraValue();
+
+        for (int i = 1; i < aceCount; i++) {
+            oneNormalRestExtra += CardNumber.ACE.getExtraValue();
+            allExtra += CardNumber.ACE.getExtraValue();
+        }
+
+        return new ArrayList<>(Arrays.asList(oneNormalRestExtra, allExtra));
+    }
+
+    private int findMaxPossibleValue(final List<Integer> possibleSum) {
+        return possibleSum.stream()
+                .filter(aceSum -> aceSum < BUST_LIMIT)
+                .mapToInt(Integer::intValue)
+                .max()
+                .orElse(BUST_LIMIT);
+    }
+
+    public boolean isBust() {
+        return calculateScore() >= BUST_LIMIT;
     }
 
     public List<Card> getHand() {
--- src/test/java/blackjack/domain/participant/HandTest.java
+++ src/test/java/blackjack/domain/participant/HandTest.java
@@ -3,13 +3,23 @@ package blackjack.domain.participant;
 import blackjack.domain.card.Card;
 import blackjack.domain.card.CardNumber;
 import blackjack.domain.card.CardType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class HandTest {
+    private Hand hand;
+
+    @BeforeEach
+    void setUp() {
+        hand = new Hand();
+    }
+
     @Test
     @DisplayName("Hand가 잘 생성되었는지 확인")
     void constructorTest() {
@@ -20,17 +30,28 @@ public class HandTest {
     @Test
     @DisplayName("Hand에 새로운 카드가 추가되는지 확인")
     void receiveCardTest() {
-        final Hand hand = new Hand();
-        hand.receiveCard(new Card(CardNumber.EIGHT, CardType.CLOVER));
+        hand.add(new Card(CardNumber.EIGHT, CardType.CLOVER));
         assertThat(hand.getHand()).hasSize(1);
     }
 
     @Test
     @DisplayName("Hand에 추가된 카드들의 합을 확인")
     void calculateScoreTest() {
-        final Hand hand = new Hand();
-        hand.receiveCard(new Card(CardNumber.EIGHT, CardType.CLOVER));
-        hand.receiveCard(new Card(CardNumber.SEVEN, CardType.CLOVER));
+        hand.add(new Card(CardNumber.EIGHT, CardType.CLOVER));
+        hand.add(new Card(CardNumber.SEVEN, CardType.CLOVER));
         assertThat(hand.calculateScore()).isEqualTo(15);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"ACE,ACE: 12", "ACE,ACE,ACE:13", "ACE,ACE,TEN: 12"}, delimiter = ':')
+    @DisplayName("여러개의 에이스 카드가 추가되었을 때 알맞은 합 구하기")
+    void calculateScoreWithAceTest(final String cards, final int expectedScore) {
+        final String[] cardNumbers = cards.split(",");
+        for (final String number : cardNumbers) {
+            final CardNumber cardNumber = CardNumber.valueOf(number);
+            hand.add(new Card(cardNumber, CardType.CLOVER));
+        }
+        assertThat(hand.calculateScore()).isEqualTo(expectedScore);
+    }
 }
+
