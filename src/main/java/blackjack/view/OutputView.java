f3fdad0ead076dbd0ee2dc2a5e1cc80de7bb14d7
TaewanKimmmm <ktww88@gmail.com>
2021-03-08 02:36:40 +0900

    refactor: 메소드 위치와 시그니처 변경, 상수 추가 등

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,23 +1,38 @@
 package blackjack.view;
 
-import blackjack.domain.MatchResultType;
 import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Player;
 import blackjack.domain.participant.Players;
+import blackjack.domain.result.MatchResult;
+import blackjack.domain.result.MatchResultType;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class OutputView {
     private static final String PLAYER_NAMES_INPUT_GUIDE_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String DRAW_AT_FIRST_GUIDE_MESSAGE = "달러와 %s 에게 2장의 카드를 나누었습니다.";
     private static final String DEALER_HIT_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    private static final String HIT_GUIDE_MESSAGE = "는 한장의 카드를 더 받겠습니까?(예는 y,아니오는 n)";
 
     private OutputView() {
     }
 
     public static void printPlayerNameInputGuideMessage() {
         System.out.println(PLAYER_NAMES_INPUT_GUIDE_MESSAGE);
+    }
+
+    public static void printAfterDrawAtFirstGuideMessage(Players players) {
+        String playerNames = players.getPlayers().stream()
+                .map(Player::getName)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(String.format(DRAW_AT_FIRST_GUIDE_MESSAGE, playerNames));
+    }
+
+    public static void printHitGuideMessage(Player player) {
+        System.out.println(player.getName() + HIT_GUIDE_MESSAGE);
     }
 
     public static void printDealerHitMessage() {
@@ -32,29 +47,26 @@ public class OutputView {
                 );
     }
 
-    public static void printMatchTypeResult(List<Integer> matchResult, Map<Player, MatchResultType> result) {
+    public static void printMatchTypeResult(List<Integer> matchResultTypeCount, MatchResult result) {
         System.out.println("## 최종 승패");
-        System.out.println("딜러: " + matchResult.get(2) + "승" + matchResult.get(1) + "무" + matchResult.get(0) + "패");
-        result.forEach((participant, status) -> System.out.println(participant.getName() + ": " + status.toString()));
-    }
-
-    public static void printHitGuideMessage(Player player) {
-        System.out.println(player.getName() + "는 한장의 카드를 더 받겠습니까?(예는 y,아니오는 n)");
-    }
-
-    public static void printErrorMessage(IllegalArgumentException exception) {
-        System.out.println("[ERROR] " + exception.getMessage());
+        System.out.println("딜러: " + matchResultTypeCount.get(2) + "승" + matchResultTypeCount.get(1) + "무" + matchResultTypeCount.get(0) + "패");
+        Map<Player, MatchResultType> matchResult = result.getMatchResult();
+        matchResult.forEach((player, status) -> System.out.println(player.getName() + ": " + status.toString()));
     }
 
     public static void printParticipantsCardAtFirst(Dealer dealer, Players players) {
         System.out.println(dealer.getName() + "카드: " + dealer.showCardsAtFirst());
-        players.getPlayers()
-                .forEach(participant ->
-                        System.out.println(participant.getName() + "카드: " + participant.showCardsAtFirst())
-                );
+
+        for (Player player : players.getPlayers()) {
+            System.out.println(player.getName() + "카드: " + player.showCardsAtFirst());
+        }
     }
 
     public static void printPlayerCards(Player player) {
         System.out.println(player.getName() + "카드: " + player.getCards());
+    }
+
+    public static void printErrorMessage(IllegalArgumentException exception) {
+        System.out.println("[ERROR] " + exception.getMessage());
     }
 }
