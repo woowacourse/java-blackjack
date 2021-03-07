615581c9de07a2fb3a9cbb2546cbd4e8921d61b1
sori <bosl95@naver.com>
2021-03-07 18:51:05 +0900

    refactor : Answer 객체 생성 추가 카드에 대한 답변 Enum 객체

--- src/main/java/blackjack/domain/Answer.java
+++ src/main/java/blackjack/domain/Answer.java
@@ -1,5 +1,10 @@
 package blackjack.domain;
 
+import blackjack.domain.card.Deck;
+import blackjack.domain.user.User;
+
+import java.util.Arrays;
+
 public enum Answer {
     YES("y"),
     NO("n");
@@ -10,11 +15,20 @@ public enum Answer {
         this.answer = answer;
     }
 
-    public static boolean isNo(String answer) {
-        return NO.answer.equals(answer);
+    public static Answer of(String value) {
+        return Arrays.stream(Answer.values())
+                .filter(answer -> answer.equals(value))
+                .findAny()
+                .get();
     }
 
-    public static boolean isYes(String answer) {
-        return YES.answer.equals(answer);
+    public boolean equals(String value) {
+        return answer.equals(value);
+    }
+
+    public void executeByAnswer(User user, Deck deck) {
+        if (this.equals(YES)) {
+            user.addCard(deck.drawCard());
+        }
     }
 }
