dfc7622716f497e0e46a9efed0af74aae639974c
seunghyun <ggyool@naver.com>
2021-03-07 21:07:38 +0900

    style: getter, equals, hashcode 순으로 메서드 위치 변경

--- src/main/java/blakcjack/domain/card/Card.java
+++ src/main/java/blakcjack/domain/card/Card.java
@@ -6,62 +6,62 @@ import java.util.Objects;
 import java.util.Optional;
 
 public class Card {
-	private static final Map<String, Card> cache = new HashMap<>();
+    private static final Map<String, Card> cache = new HashMap<>();
 
-	static {
-		for (final CardSymbol cardSymbol : CardSymbol.values()) {
-			for (final CardNumber cardNumber : CardNumber.values()) {
-				final String key = generateKey(cardSymbol, cardNumber);
-				final Card card = new Card(cardSymbol, cardNumber);
-				cache.put(key, card);
-			}
-		}
-	}
+    static {
+        for (final CardSymbol cardSymbol : CardSymbol.values()) {
+            for (final CardNumber cardNumber : CardNumber.values()) {
+                final String key = generateKey(cardSymbol, cardNumber);
+                final Card card = new Card(cardSymbol, cardNumber);
+                cache.put(key, card);
+            }
+        }
+    }
 
-	private final CardSymbol cardSymbol;
-	private final CardNumber cardNumber;
+    private final CardSymbol cardSymbol;
+    private final CardNumber cardNumber;
 
-	private Card(final CardSymbol cardSymbol, final CardNumber cardNumber) {
-		this.cardSymbol = cardSymbol;
-		this.cardNumber = cardNumber;
-	}
+    private Card(final CardSymbol cardSymbol, final CardNumber cardNumber) {
+        this.cardSymbol = cardSymbol;
+        this.cardNumber = cardNumber;
+    }
 
-	public static Card of(final CardSymbol cardSymbol, final CardNumber cardNumber) {
-		final String key = generateKey(cardSymbol, cardNumber);
-		return Optional.ofNullable(cache.get(key))
-				.orElseThrow(IllegalArgumentException::new);
-	}
+    public static Card of(final CardSymbol cardSymbol, final CardNumber cardNumber) {
+        final String key = generateKey(cardSymbol, cardNumber);
+        return Optional.ofNullable(cache.get(key))
+                .orElseThrow(IllegalArgumentException::new);
+    }
 
-	private static String generateKey(CardSymbol cardSymbol, CardNumber cardNumber) {
-		return cardSymbol.name() + cardNumber.name();
-	}
+    private static String generateKey(CardSymbol cardSymbol, CardNumber cardNumber) {
+        return cardSymbol.name() + cardNumber.name();
+    }
 
-	public boolean isAce() {
-		return cardNumber.equals(CardNumber.ACE);
-	}
+    public boolean isAce() {
+        return cardNumber.equals(CardNumber.ACE);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Card card = (Card) o;
-		return cardSymbol == card.cardSymbol && cardNumber == card.cardNumber;
-	}
+    public CardNumber getCardNumber() {
+        return cardNumber;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(cardSymbol, cardNumber);
-	}
+    public int getCardNumberValue() {
+        return cardNumber.getValue();
+    }
 
-	public CardNumber getCardNumber() {
-		return cardNumber;
-	}
+    public CardSymbol getCardSymbol() {
+        return cardSymbol;
+    }
 
-	public int getCardNumberValue() {
-		return cardNumber.getValue();
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return cardSymbol == card.cardSymbol && cardNumber == card.cardNumber;
+    }
 
-	public CardSymbol getCardSymbol() {
-		return cardSymbol;
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardSymbol, cardNumber);
+    }
 }
