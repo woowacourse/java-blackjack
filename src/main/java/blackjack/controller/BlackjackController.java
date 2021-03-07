92968f5c5a888ba5c29f9e1039f99a48632e59b7
pjy1368 <basejin0810@naver.com>
2021-03-08 00:38:01 +0900

    refactor(BlackjackController): 참가자 일급 컬렉션을 통해 컨트롤러의 책임을 분산

--- src/main/java/blackjack/controller/BlackjackController.java
+++ src/main/java/blackjack/controller/BlackjackController.java
@@ -3,14 +3,12 @@ package blackjack.controller;
 import blackjack.domain.GameResult;
 import blackjack.domain.Result;
 import blackjack.domain.card.CardDeck;
-import blackjack.domain.participants.Dealer;
 import blackjack.domain.participants.Name;
 import blackjack.domain.participants.Names;
 import blackjack.domain.participants.Participant;
-import blackjack.domain.participants.Player;
+import blackjack.domain.participants.Participants;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -19,25 +17,14 @@ public class BlackjackController {
 
     public void run() {
         final CardDeck cardDeck = new CardDeck();
-        final List<Participant> participants = participantsSetUp();
-        final List<Participant> players = new ArrayList<>(
-            participants.subList(1, participants.size()));
+        final Participants participants = new Participants(requestName());
 
-        distributeCard(participants, cardDeck);
-        showNameAndCardInfo(players, participants);
-        playerGameProgress(players, cardDeck);
-        dealerGameProgress(participants.get(0), cardDeck);
-        showFinalCardResult(participants);
-        showGameResult(participants.get(0), players, new GameResult());
-    }
-
-    private List<Participant> participantsSetUp() {
-        final Names names = requestName();
-        final List<Participant> participants = names.toList().stream()
-            .map(Player::new)
-            .collect(Collectors.toList());
-        participants.add(0, new Dealer());
-        return new ArrayList<>(participants);
+        participants.distributeCard(cardDeck);
+        showNameAndCardInfo(participants);
+        playerGameProgress(participants.getPlayers(), cardDeck);
+        dealerGameProgress(participants.getDealer(), cardDeck);
+        showFinalCardResult(participants.getParticipantGroup());
+        showGameResult(participants.getDealer(), participants.getPlayers());
     }
 
     private Names requestName() {
@@ -56,17 +43,9 @@ public class BlackjackController {
         return new Names(names);
     }
 
-    private void distributeCard(final List<Participant> participants, final CardDeck cardDeck) {
-        for (final Participant participant : participants) {
-            participant.receiveCard(cardDeck.distribute());
-            participant.receiveCard(cardDeck.distribute());
-        }
-    }
-
-    private void showNameAndCardInfo(final List<Participant> players,
-        List<Participant> participants) {
-        OutputView.distributeMessage(players);
-        OutputView.showParticipantsCard(participants);
+    private void showNameAndCardInfo(final Participants participants) {
+        OutputView.distributeMessage(participants.getPlayers());
+        OutputView.showParticipantsCard(participants.getParticipantGroup());
     }
 
     private void playerGameProgress(final List<Participant> participants,
@@ -109,8 +88,8 @@ public class BlackjackController {
         OutputView.showCardsResult(participants);
     }
 
-    private void showGameResult(final Participant dealer, final List<Participant> players,
-        final GameResult gameResult) {
+    private void showGameResult(final Participant dealer, final List<Participant> players) {
+        final GameResult gameResult = new GameResult();
         OutputView.showGameResult(dealer, players, gameResult);
         final Map<Name, Result> results = gameResult.makePlayerResults(players, dealer);
         OutputView.showPlayerGameResult(results);
