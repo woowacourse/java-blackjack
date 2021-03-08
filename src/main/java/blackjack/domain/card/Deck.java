f3ef223a1be798347c202e485b865a03d9ec226c
rok93 <goodboy302@naver.com>
2021-03-08 18:35:13 +0900

    refactor: 카드덱을 캐싱하여 생성할 때마다 캐싱한 카드들을 쓰도록 변경

--- src/main/java/blackjack/domain/card/Deck.java
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -11,18 +11,23 @@ public class Deck {
     private static final int TOP_OF_DECK = 0;
     private static final int NUMBER_OF_INIT_CARD = 2;
     private static final int TOP_CARD = 0;
+    private static final List<Card> CARDS;
 
     private final List<Card> cards;
+
+    static {
+        CARDS = Arrays.stream(CardType.values())
+                .flatMap(cardType -> Arrays.stream(CardValue.values())
+                        .map(cardValue -> new Card(cardType, cardValue)))
+                .collect(Collectors.toList());
+    }
 
     private Deck(List<Card> cards) {
         this.cards = new ArrayList<>(cards);
     }
 
     public static Deck generate() {
-        List<Card> cards = Arrays.stream(CardType.values())
-                .flatMap(cardType -> Arrays.stream(CardValue.values())
-                        .map(cardValue -> new Card(cardType, cardValue)))
-                .collect(Collectors.toList());
+        List<Card> cards = new ArrayList<>(CARDS);
         Collections.shuffle(cards);
         return new Deck(cards);
     }
