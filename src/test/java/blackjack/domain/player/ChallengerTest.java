41fe083756f24573301a4fc1149ca041f1fffb1b
hoony <younghoon.kwon91@gmail.com>
2021-03-08 22:32:40 +0900

    feat: 참여인원에 대한 유효성 검사 테스트 추가

--- src/test/java/blackjack/domain/player/ChallengerTest.java
+++ src/test/java/blackjack/domain/player/ChallengerTest.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ChallengerTest {
 
@@ -92,5 +93,30 @@ public class ChallengerTest {
         Challenger challenger = new Challenger(new Cards(cardList), new Name("pobi"));
 
         assertThat(Result.getPlayerResult(challenger, dealer)).isEqualTo(Result.WIN);
+    }
+
+    @Test
+    @DisplayName("참여인원이 1명 미만일 경우, 예외 처리한다")
+    void whenChallengersLessThanOneThrowsException(){
+        List<Challenger> challengers = new ArrayList<>();
+        assertThatThrownBy(() -> new Challengers(challengers)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("게임 참가자의수는 1명 이상 7명 이하여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("참여인원이 7명 초과일 경우, 예외 처리한다")
+    void whenChallengersGreaterThanSevenThrowsException(){
+        List<Challenger> challengers = new ArrayList<>();
+        for(int i = 0; i < 8; i++) {
+            List<Card> cardList = new ArrayList<>();
+
+            cardList.add(new Card(Suit.DIAMOND, Face.ACE));
+            cardList.add(new Card(Suit.DIAMOND, Face.ACE));
+
+            Challenger challenger = new Challenger(new Cards(cardList), new Name("pobi"));
+            challengers.add(challenger);
+        }
+        assertThatThrownBy(() -> new Challengers(challengers)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("게임 참가자의수는 1명 이상 7명 이하여야 합니다.");
     }
 }
