f33fcc5baa777b84be509e5ddd32bd16820d27e9
daum7766 <daum7766@naver.com>
2021-03-08 20:19:23 +0900

    test: 배팅머니 테스트코드

---
+++ src/test/java/blackjack/domain/player/BetAmountTest.java
@@ -0,0 +1,33 @@
+package blackjack.domain.player;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BetAmountTest {
+    @DisplayName("서로다른 방식의 생성자 테스트")
+    @Test
+    void create() {
+        assertThat(new BetAmount("10000")).isEqualTo(new BetAmount(10000));
+    }
+
+    @DisplayName("금액이 0 이하일시 에러 테스트")
+    @Test
+    void minusMoney() {
+        assertThatThrownBy(() -> new BetAmount("-10000"))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new BetAmount(0))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("올바르지 않은 입력시 에러 테스트")
+    @Test
+    void validateInput() {
+        assertThatThrownBy(() -> new BetAmount("-a"))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new BetAmount(null))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
