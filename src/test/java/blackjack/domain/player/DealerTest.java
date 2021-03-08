e8e4f4fd7b73118a14ae0b883c6cf8e35acbd84d
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-08 12:48:26 +0900

    test(DealerTest): 딜러 생성 테스트 추가

--- src/test/java/blackjack/domain/player/DealerTest.java
+++ src/test/java/blackjack/domain/player/DealerTest.java
@@ -1,14 +1,27 @@
 package blackjack.domain.player;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import blackjack.domain.card.Card;
 import blackjack.domain.card.type.CardNumberType;
 import blackjack.domain.card.type.CardShapeType;
+import blackjack.domain.player.strategy.AllCardsOpenStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DealerTest {
+    @DisplayName("딜러 생성 테스트")
+    @Test
+    void createDealer() {
+        Dealer dealer = new Dealer();
+        assertThat(dealer.getName()).isEqualTo(new Name("딜러"));
+        assertThatThrownBy(dealer::getCards).isInstanceOf(IndexOutOfBoundsException.class);
+
+        dealer.setCardOpenStrategy(new AllCardsOpenStrategy());
+        assertThat(dealer.getCards()).isEmpty();
+    }
+
     @DisplayName("카드 합이 16 이하이면 isCanDraw()가 true - 0일 때")
     @Test
     void canDrawCardWhen0() {
