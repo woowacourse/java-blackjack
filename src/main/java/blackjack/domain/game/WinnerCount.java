83856c5c3d7cdb1cb398969223fb07d0f627c8f5
y_woo :) <dusdn1702@naver.com>
2021-03-08 17:42:36 +0900

    refactor: winnerCount enumMap으로 수정

--- src/main/java/blackjack/domain/game/WinnerCount.java
+++ src/main/java/blackjack/domain/game/WinnerCount.java
@@ -3,22 +3,24 @@ package blackjack.domain.game;
 import blackjack.domain.participant.Player;
 import blackjack.domain.participant.Players;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
+
+import static blackjack.domain.game.WinnerFlag.*;
 
 public class WinnerCount {
     private final Map<WinnerFlag, Integer> winnerCount;
 
     public WinnerCount() {
-        this.winnerCount = new HashMap<>(3);
-        winnerCount.put(WinnerFlag.WIN, 0);
-        winnerCount.put(WinnerFlag.DRAW, 0);
-        winnerCount.put(WinnerFlag.LOSE, 0);
+        this.winnerCount = new EnumMap<>(WinnerFlag.class);
+        winnerCount.put(WIN, 0);
+        winnerCount.put(DRAW, 0);
+        winnerCount.put(LOSE, 0);
     }
 
     public Map<WinnerFlag, Integer> calculateTotalWinnings(Players players) {
         for (Player player : players.toList()) {
-            winnerCount.computeIfPresent(player.getResult(), (WinnerFlag, i) -> ++i);
+            winnerCount.put(player.getResult(), winnerCount.getOrDefault(player.getResult(), 0) + 1);
         }
         return winnerCount;
     }
