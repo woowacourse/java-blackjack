eea2bd57582f154402272ea7f221e7e20a39ec06
daum7766 <daum7766@naver.com>
2021-03-08 22:11:05 +0900

    test: 카드포함 기능 추가

--- src/test/java/blackjack/domain/card/PlayerCardsTest.java
+++ src/test/java/blackjack/domain/card/PlayerCardsTest.java
@@ -4,14 +4,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PlayerCardsTest {
-    @DisplayName("추가 테스트")
+    @DisplayName("추가 및 사이즈 테스트")
     @Test
     void create() {
         PlayerCards cards = new PlayerCards();
-        assertThat(cards.getCards()).hasSize(0);
+        assertThat(cards.size()).isEqualTo(0);
         cards.add(Card.valueOf(CardShape.CLUB, CardNumber.ACE));
-        assertThat(cards.getCards()).hasSize(1);
+        assertThat(cards.size()).isEqualTo(1);
+    }
+
+    @DisplayName("카드 포함 테스트")
+    @Test
+    void crate() {
+        PlayerCards cards = new PlayerCards();
+        assertThat(cards.isContains(CardNumber.ACE)).isFalse();
+        cards.add(Card.valueOf(CardShape.CLUB, CardNumber.ACE));
+        assertThat(cards.isContains(CardNumber.ACE)).isTrue();
     }
 }
