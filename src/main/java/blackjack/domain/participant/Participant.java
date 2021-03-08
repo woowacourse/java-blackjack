b4f31438f1f72c091fe24690eda746ff58185ae6
김주원 <kjw11077@naver.com>
2021-03-08 18:22:11 +0900

    feat: 참가자 이름 유효성 검사

--- src/main/java/blackjack/domain/participant/Participant.java
+++ src/main/java/blackjack/domain/participant/Participant.java
@@ -6,12 +6,21 @@ import java.util.List;
 
 public abstract class Participant {
 
+    public static final int MIN_NAME_LENGTH = 1;
+    public static final int MAX_NAME_LENGTH = 5;
     protected final String name;
     protected final Hand cardHand;
 
     public Participant(String name, Hand cardHand) {
+        validateName(name);
         this.name = name;
         this.cardHand = cardHand;
+    }
+
+    private void validateName(String name) {
+        if (name.length() < MIN_NAME_LENGTH || MAX_NAME_LENGTH < name.length()) {
+            throw new IllegalArgumentException("이름은 1~5자만 가능합니다");
+        }
     }
 
     public List<Card> getCards() {
