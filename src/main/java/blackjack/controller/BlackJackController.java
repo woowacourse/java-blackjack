fd59f63ec2085c035bac98102e9378feb4bb9e6c
RinSabbit <celena1004@naver.com>
2021-03-08 21:48:14 +0900

    refactor: GameTable의 역할 일부를 Controller로 위임해 GameTable이 View를 사용하지 않게 함

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -1,5 +1,8 @@
 package blackjack.controller;
 
+import blackjack.domain.GameTable;
+import blackjack.domain.Result;
+import blackjack.domain.user.Dealer;
 import blackjack.domain.user.Player;
 import blackjack.domain.user.Players;
 import blackjack.view.InputView;
@@ -8,17 +11,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class BlackJackController {
-
-    public BlackJackController() {
-    }
+    private static final String HIT_CONTINUE = "Y";
 
     public void run() {
         try {
             OutputView.printPlayersGuideMessage();
+            Dealer dealer = new Dealer();
             Players players = makePlayers(InputView.inputPlayers());
-
-            GameTableController gameTableController = new GameTableController(players);
-            gameTableController.playGame();
+            playGame(dealer,players);
+            revealResult(dealer,players);
         } catch (IllegalArgumentException exception) {
             OutputView.printErrorMessage(exception);
         }
@@ -28,5 +29,47 @@ public class BlackJackController {
         return new Players(inputPlayers.stream()
             .map(Player::new)
             .collect(Collectors.toList()));
+    }
+
+    private void playGame(Dealer dealer, Players players) {
+        GameTable gameTable = new GameTable(dealer, players);
+        gameTable.drawAtFirst();
+        drawPlayers(gameTable, players);
+        drawDealer(gameTable, dealer);
+    }
+
+    private void revealResult(Dealer dealer, Players players) {
+        OutputView.printCardsAndScore(dealer, players);
+        Result result = new Result(dealer, players);
+        List<Integer> matchResult = dealer.calculateMatchResult(result.getResult());
+        OutputView.printResult(matchResult, result.getResult());
+    }
+
+    private void drawPlayers(GameTable gameTable, Players players) {
+        tryHit(gameTable, players);
+    }
+
+    public void tryHit(GameTable gameTable, Players players) {
+        players.getPlayers()
+            .forEach(player -> askHit(gameTable, player));
+    }
+
+    private void askHit(GameTable gameTable, Player player) {
+        while (player.isNotBust() && wantCard(player)) {
+            gameTable.draw(player);
+            OutputView.printUserCards(player);
+        }
+    }
+
+    private boolean wantCard(Player player) {
+        OutputView.printHitGuideMessage(player);
+        String hitValue = InputView.getHitValue();
+        return HIT_CONTINUE.equals(hitValue);
+    }
+    private void drawDealer(GameTable gameTable, Dealer dealer) {
+        while (dealer.canHit()) {
+            gameTable.draw(dealer);
+            OutputView.printDealerHitMessage();
+        }
     }
 }
