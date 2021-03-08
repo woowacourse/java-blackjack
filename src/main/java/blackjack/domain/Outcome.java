721f679409aca9cd1a09efaed3e594700771df38
sjpark-dev <sjpark.dev@gmail.com>
2021-03-08 15:51:22 +0900

    refactor: Outcome의 미사용 메소드 제거

--- src/main/java/blackjack/domain/Outcome.java
+++ src/main/java/blackjack/domain/Outcome.java
@@ -1,12 +1,11 @@
 package blackjack.domain;
 
-import java.util.Arrays;
 import java.util.function.Predicate;
 
 public enum Outcome {
-    WIN("승",result -> result == 1),
-    LOSE("패",result -> result == -1),
-    DRAW("무",result -> result == 0);
+    WIN("승", result -> result == 1),
+    LOSE("패", result -> result == -1),
+    DRAW("무", result -> result == 0);
 
     private final String word;
     private final Predicate<Integer> expression;
@@ -14,12 +13,6 @@ public enum Outcome {
     Outcome(String word, Predicate<Integer> expression) {
         this.word = word;
         this.expression = expression;
-    }
-
-    public static Outcome getInstance(int result) {
-        return Arrays.stream(Outcome.values())
-                .filter(outcome -> outcome.expression.test(result))
-                .findFirst().orElse(null);
     }
 
     public String getWord() {
