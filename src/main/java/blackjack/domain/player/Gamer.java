da0fd028fb642b545ee3b2c6cd4e513bf87105f0
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-03-08 22:59:44 +0900

    feat: 게이머가 베팅 금액 가지도록 추가

--- src/main/java/blackjack/domain/player/Gamer.java
+++ src/main/java/blackjack/domain/player/Gamer.java
@@ -1,3 +1,4 @@
+
 package blackjack.domain.player;
 
 import blackjack.domain.card.Card;
@@ -6,9 +7,15 @@ import blackjack.domain.card.Cards;
 public class Gamer extends Player {
     private static final String PLAYER_SCORE_RANGE_ERROR = "[ERROR] 플레이어의 점수가 21을 초과하여 카드를 추가할 수 없습니다.";
     private static final int DRAW_MAXIMUM_SCORE = 21;
+    private BettingMoney bettingMoney;
 
     public Gamer(Name name, Cards cards) {
         super(name, cards);
+    }
+
+    public Gamer(Name name, Cards cards, BettingMoney bettingMoney) {
+        super(name, cards);
+        this.bettingMoney = bettingMoney;
     }
 
     @Override
@@ -21,6 +28,10 @@ public class Gamer extends Player {
         if (calculateScore() >= DRAW_MAXIMUM_SCORE) {
             throw new IllegalArgumentException(PLAYER_SCORE_RANGE_ERROR);
         }
+    }
+
+    public int getBettingMoney() {
+        return bettingMoney.getBettingMoney();
     }
 
     @Override
