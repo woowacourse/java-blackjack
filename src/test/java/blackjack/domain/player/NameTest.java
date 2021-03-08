deb3777582773cbcb79ddd0d98a50a671b29e26a
hoony <younghoon.kwon91@gmail.com>
2021-03-08 15:51:48 +0900

    refactor: 테스트 코드 추가 및 DealerTest의 몇몇 테스트 메서드 ChallengerTest로 이동

--- src/test/java/blackjack/domain/player/NameTest.java
+++ src/test/java/blackjack/domain/player/NameTest.java
@@ -1,6 +1,7 @@
 package blackjack.domain.player;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
@@ -12,13 +13,22 @@ class NameTest {
     @DisplayName("이름을 null로 생성하려고하면 예외가 발생한다.")
     @NullSource
     void nullThrowException(String name) {
-        assertThatThrownBy(() -> new Name(name)).isInstanceOf(NullPointerException.class);
+        assertThatThrownBy(() -> new Name(name)).isInstanceOf(NullPointerException.class)
+                .hasMessage("이름은 null일 수 없습니다.");
     }
 
     @ParameterizedTest
     @DisplayName("빈 이름으로 생성하려고하면 예외가 발생한다.")
     @EmptySource
     void emptyThrowException(String name) {
-        assertThatThrownBy(() -> new Name(name)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Name(name)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 비어있을 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("공백의 이름으로 생성하려고하면 예외가 발생한다.")
+    void emptySpacesThrowException() {
+        assertThatThrownBy(() -> new Name("   ")).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 비어있을 수 없습니다.");
     }
 }
