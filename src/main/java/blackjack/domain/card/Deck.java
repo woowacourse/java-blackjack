6f679dcda90a16464b063e6bcec86b49809e65ca
Jinho <jh8579@gmail.com>
2021-03-08 14:19:23 +0900

    refactor : 카드 덱을 미리 생성하도록 로직 변경

--- src/main/java/blackjack/domain/card/Deck.java
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -8,19 +8,15 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Deck {
-    private final Stack<Card> cards;
+    private static final Stack<Card> cards;
 
-    public Deck() {
-        cards = init();
-    }
-
-    private Stack<Card> init() {
-        Stack<Card> cards = new Stack<>();
+    static {
+        Stack<Card> deck = new Stack<>();
         Arrays.stream(Suit.values())
             .forEach(suit -> Arrays.stream(Denomination.values())
-                .forEach(denomination -> cards.add(new Card(denomination, suit))));
-        Collections.shuffle(cards);
-        return cards;
+                .forEach(denomination -> deck.add(new Card(denomination, suit))));
+        Collections.shuffle(deck);
+        cards = deck;
     }
 
     public List<Card> pickInitialCards() {
