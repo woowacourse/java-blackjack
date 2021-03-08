fa6bb7dedc9c14c4851e1b3cc7b95c7f6c28c294
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-08 12:37:31 +0900

    test(CardsTest): 점수 계산 테스트 추가

--- src/test/java/blackjack/domain/card/CardsTest.java
+++ src/test/java/blackjack/domain/card/CardsTest.java
@@ -42,5 +42,90 @@ public class CardsTest {
 
         assertThatThrownBy(cards::drawOneCard)
             .isInstanceOf(IndexOutOfBoundsException.class);
+
+        Card card = new Card(CardShapeType.HEART, CardNumberType.TWO);
+        cards.add(card);
+        assertThat(cards.getCards()).containsExactly(card);
+
+        assertThat(cards.drawOneCard()).isEqualTo(card);
+        assertThatThrownBy(cards::drawOneCard)
+            .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @DisplayName("A가 1로 계산되어야 할 때 총 점수 계산 테스트 - 10, 10, A")
+    @Test
+    void getScoreAceLowValue1() {
+        Cards cards = Cards.createEmptyCards();
+        Card tenCard = new Card(CardShapeType.DIAMOND, CardNumberType.TEN);
+        Card aceCard = new Card(CardShapeType.DIAMOND, CardNumberType.ACE);
+
+        cards.add(tenCard);
+        cards.add(tenCard);
+        cards.add(aceCard);
+
+        assertThat(cards.getScore()).isEqualTo(21);
+    }
+
+    @DisplayName("A가 1로 계산되어야 할 때 총 점수 계산 테스트 - 10, A, 10")
+    @Test
+    void getScoreAceLowValue2() {
+        Cards cards = Cards.createEmptyCards();
+        Card tenCard = new Card(CardShapeType.DIAMOND, CardNumberType.TEN);
+        Card aceCard = new Card(CardShapeType.DIAMOND, CardNumberType.ACE);
+
+        cards.add(tenCard);
+        cards.add(aceCard);
+        cards.add(tenCard);
+
+        assertThat(cards.getScore()).isEqualTo(21);
+    }
+
+    @DisplayName("A가 11로 계산되어야 할 때 총 점수 계산 테스트")
+    @Test
+    void getScoreAceHighValue() {
+        Cards cards = Cards.createEmptyCards();
+        Card tenCard = new Card(CardShapeType.DIAMOND, CardNumberType.TEN);
+        Card aceCard = new Card(CardShapeType.DIAMOND, CardNumberType.ACE);
+
+        cards.add(tenCard);
+        cards.add(aceCard);
+
+        assertThat(cards.getScore()).isEqualTo(21);
+    }
+
+    @DisplayName("A가 2개일 때 : A(11) + A(1) = 12")
+    @Test
+    void getScoreDoubleAce() {
+        Cards cards = Cards.createEmptyCards();
+        Card aceCard = new Card(CardShapeType.DIAMOND, CardNumberType.ACE);
+
+        cards.add(aceCard);
+        cards.add(aceCard);
+
+        assertThat(cards.getScore()).isEqualTo(12);
+    }
+
+    @DisplayName("A가 없을 때 총 점수 계산 테스트 - 총 합이 10")
+    @Test
+    void getScoreNotContainsAceTenOrLess() {
+        Cards cards = Cards.createEmptyCards();
+        Card tenCard = new Card(CardShapeType.DIAMOND, CardNumberType.TEN);
+
+        cards.add(tenCard);
+
+        assertThat(cards.getScore()).isEqualTo(10);
+    }
+
+    @DisplayName("A가 없을 때 총 점수 계산테스트 - 총 합이 11")
+    @Test
+    void getScoreNotContainsAce11OrMore() {
+        Cards cards = Cards.createEmptyCards();
+        Card fiveCard = new Card(CardShapeType.DIAMOND, CardNumberType.FIVE);
+        Card sixCard = new Card(CardShapeType.DIAMOND, CardNumberType.SIX);
+
+        cards.add(fiveCard);
+        cards.add(sixCard);
+
+        assertThat(cards.getScore()).isEqualTo(11);
     }
 }
