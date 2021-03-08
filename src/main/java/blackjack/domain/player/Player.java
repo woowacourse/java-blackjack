831f4e28a25966b5675ca6bc863c3d19e90212f0
binghe819 <binghe819@naver.com>
2021-03-08 16:36:13 +0900

    refactor: 상위 클래스의 오버라이딩이 되면 안되는 메서드 final로 리팩토링

--- src/main/java/blackjack/domain/player/Player.java
+++ src/main/java/blackjack/domain/player/Player.java
@@ -17,11 +17,11 @@ public abstract class Player {
 
     abstract boolean canDraw();
 
-    public int calculateScore() {
+    public final int calculateScore() {
         return cards.calculateScore();
     }
 
-    public boolean isBust() {
+    public final boolean isBust() {
         return cards.isBust();
     }
 
