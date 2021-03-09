2b913aab6c2e813f535445d6c0c3404323069fc7
joanne <mathmjseo@naver.com>
2021-03-09 22:39:01 +0900

    fix: 블랙잭이 아니며 카드의 합이 21 이상이 되는 경우 Stay를 리턴하도록 수정

--- src/main/java/blackjack/domain/state/Hit.java
+++ src/main/java/blackjack/domain/state/Hit.java
@@ -4,6 +4,8 @@ import blackjack.domain.card.Card;
 import blackjack.domain.card.Cards;
 
 public class Hit extends Running {
+    private static final int TWENTY_ONE = 21;
+
     public Hit(final Cards cards) {
         super(cards);
     }
@@ -13,6 +15,9 @@ public class Hit extends Running {
         cards.add(card);
         if (cards.isBust()) {
             return new Bust(cards);
+        }
+        if (cards.calculate() == TWENTY_ONE) {
+            return stay();
         }
         return new Hit(cards);
     }
