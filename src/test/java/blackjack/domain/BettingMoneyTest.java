e866ddf5e20795c73e6ba13c4d64affd24401301
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-09 16:02:51 +0900

    feat(BettingMoneyTest): 상수 접근제어자 범위 변경

--- src/test/java/blackjack/domain/BettingMoneyTest.java
+++ src/test/java/blackjack/domain/BettingMoneyTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BettingMoneyTest {
-    private static final int BETTING_BOUND = 2_000_000_000;
+    protected static final int BETTING_BOUND = 2_000_000_000;
 
     @DisplayName("배팅 금액이 20억 이하이면, 배팅 가능")
     @Test
