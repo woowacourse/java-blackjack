bc67cfcb19ee637548219b00b77d244437951e34
perenok <perenoke@gmail.com>
2021-03-09 16:06:35 +0900

    refactor(Denomination) : Score 불변객체 변경에 따른 수정

--- src/main/java/blackjack/domain/card/Denomination.java
+++ src/main/java/blackjack/domain/card/Denomination.java
@@ -3,40 +3,40 @@ package blackjack.domain.card;
 import java.util.Arrays;
 
 public enum Denomination {
-    ACE("A", 1),
-    TWO("2", 2),
-    THREE("3", 3),
-    FOUR("4", 4),
-    FIVE("5", 5),
-    SIX("6", 6),
-    SEVEN("7", 7),
-    EIGHT("8", 8),
-    NINE("9", 9),
-    TEN("10", 10),
-    JACK("J", 10),
-    QUEEN("Q", 10),
-    KING("K", 10);
+    ACE("A", Score.of(1)),
+    TWO("2", Score.of(2)),
+    THREE("3", Score.of(3)),
+    FOUR("4", Score.of(4)),
+    FIVE("5", Score.of(5)),
+    SIX("6", Score.of(6)),
+    SEVEN("7", Score.of(7)),
+    EIGHT("8", Score.of(8)),
+    NINE("9", Score.of(9)),
+    TEN("10", Score.of(10)),
+    JACK("J", Score.of(10)),
+    QUEEN("Q", Score.of(10)),
+    KING("K", Score.of(10));
 
     private final String denomination;
-    private int score;
+    private final Score score;
 
-    private Denomination(final String denomination, final int score) {
+    Denomination(final String denomination, final Score score) {
         this.denomination = denomination;
         this.score = score;
     }
 
-    public static Denomination of(final String denomination){
+    public static Denomination of(final String denomination) {
         return Arrays.stream(Denomination.values())
                 .filter(den -> den.denomination.equals(denomination))
                 .findAny()
-                .orElseThrow(()-> new IllegalArgumentException("유효하지 않은 Denomination입니다."));
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 Denomination입니다."));
     }
 
-    public String denomination() {
-        return denomination;
-    }
-
-    public int getScore() {
+    public Score getScore() {
         return score;
+    }
+
+    public String getDenomination() {
+        return denomination;
     }
 }
