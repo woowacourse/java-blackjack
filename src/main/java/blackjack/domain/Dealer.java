9352f0fe11afaa7db8f121df33827c1f834dc09e
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-09 15:47:34 +0900

    refactor: 불필요한 코드 삭제

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -3,9 +3,7 @@ package blackjack.domain;
 public class Dealer extends Participant {
 
     private static final int MAXIMUM_SCORE_FOR_ADDITIONAL_CARD = 16;
-    private static final long DEFAULT_VALUE = 0L;
     private static final String DEALER_NAME = "딜러";
-    private static final int INCREASE_COUNT = 1;
 
     public Dealer() {
         super(DEALER_NAME);
@@ -16,15 +14,4 @@ public class Dealer extends Participant {
         int score = calculateFinalScore();
         return score <= MAXIMUM_SCORE_FOR_ADDITIONAL_CARD;
     }
-
-//    public Map<Result, Long> getStatisticResult(List<Player> players) {
-//        Map<Result, Long> statisticResult = new EnumMap<>(Result.class);
-//        for (Player player : players) {
-//            Result result = player.judgeResult(this);
-//            Result replacedResult = result.replaceWinWithLose();
-//            statisticResult.put(replacedResult,
-//                statisticResult.getOrDefault(replacedResult, DEFAULT_VALUE) + INCREASE_COUNT);
-//        }
-//        return statisticResult;
-//    }
 }
