905ccc9b077c3dd8c76cb54ea328567c6e3c8a06
sakjung <ssangyu123@gmail.com>
2021-03-09 18:12:01 +0900

    fix: 베팅금액 입력 받을 때 출력 메세지 linebreak 하도록 수정

--- src/main/java/blakcjack/view/InputView.java
+++ src/main/java/blakcjack/view/InputView.java
@@ -22,7 +22,7 @@ public class InputView {
 	}
 
 	public static int takeBettingAmountInput(final Name name) {
-		System.out.printf("%s의 베팅 금액은?", name.getName());
+		System.out.printf("%s의 베팅 금액은?%n", name.getName());
 		String bettingAmount = SCANNER.nextLine();
 		validateInteger(bettingAmount);
 		validateAmount(bettingAmount);
