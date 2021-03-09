5600e2c6b065658cb5e46b7d3d497ccac8d1beb6
RinSabbit <celena1004@naver.com>
2021-03-09 21:01:21 +0900

    test: Status, HitStay, GameTable TC 구현

---
+++ src/test/java/blackjack/domain/GameTableTest.java
@@ -0,0 +1,25 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.domain.user.Dealer;
+import blackjack.domain.user.Players;
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameTableTest {
+
+    @DisplayName("게임을 시작할 때 딜러와 플레이어들에게 카드를 2장씩 나눠준다.")
+    @Test
+    void firstDraw() {
+        Dealer dealer = new Dealer();
+        Players players = new Players(Arrays.asList("choonsik","jason"));
+        GameTable gameTable = new GameTable(dealer,players);
+        gameTable.drawAtFirst();
+
+        assertThat(dealer.getScore().toInt()).isBetween(2,21);
+        players.getPlayers()
+            .forEach(player -> assertThat(player.getScore().toInt()).isBetween(2,21));
+    }
+}
