ec6f78cf337ec935de4101003c29a751d26ffc34
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-09 17:59:47 +0900

    test(BettingMoneyTest): 배팅금액 유효성 검사 테스트 보완

--- src/test/java/blackjack/domain/BettingMoneyTest.java
+++ src/test/java/blackjack/domain/BettingMoneyTest.java
@@ -8,19 +8,29 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BettingMoneyTest {
-    protected static final int BETTING_BOUND = 100_000_000;
+    private static final int MIN_BETTING_MONEY_BOUND = 1_000;
+    protected static final int MAX_BETTING_MONEY_BOUND = 100_000_000;
 
-    @DisplayName("배팅 금액이 1억 이하이면, 배팅 가능")
+    @DisplayName("배팅 금액이 1000원 이상, 1억원 이하이면, 배팅 가능")
     @Test
     void validBetting() {
-        assertThatCode(() -> new BettingMoney(BETTING_BOUND))
+        assertThatCode(() -> new BettingMoney(MAX_BETTING_MONEY_BOUND))
+            .doesNotThrowAnyException();
+        assertThatCode(() -> new BettingMoney(MIN_BETTING_MONEY_BOUND))
             .doesNotThrowAnyException();
     }
 
-    @DisplayName("배팅 금액이 1억 초과이면, 예외 발생")
+    @DisplayName("배팅 금액이 1000원 미만이면, 예외 발생")
+    @Test
+    void underBettingBoundException() {
+        assertThatThrownBy(() -> new BettingMoney(MIN_BETTING_MONEY_BOUND - 1))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("배팅 금액이 1억원 초과이면, 예외 발생")
     @Test
     void overBettingBoundException() {
-        assertThatThrownBy(() -> new BettingMoney(BETTING_BOUND + 1))
+        assertThatThrownBy(() -> new BettingMoney(MAX_BETTING_MONEY_BOUND + 1))
             .isInstanceOf(IllegalArgumentException.class);
     }
 }
