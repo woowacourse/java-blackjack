9ec300400f5538104c9d3022bdf48c50bf5d7559
pkeugine <pkeugine@gmail.com>
2021-03-09 18:15:14 +0900

    test: 블랙잭인 경우 테스트 추가

--- src/test/java/blackjack/domain/gamer/PlayersTest.java
+++ src/test/java/blackjack/domain/gamer/PlayersTest.java
@@ -1,15 +1,23 @@
 package blackjack.domain.gamer;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
+import blackjack.domain.ResultType;
+import blackjack.domain.card.Card;
 import blackjack.domain.card.CardManager;
+import blackjack.domain.card.Denomination;
+import blackjack.domain.card.Hands;
+import blackjack.domain.card.Suit;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PlayersTest {
 
-    @DisplayName("Players 객체 생성")
+    @DisplayName("Players 객체 생성 성공")
     @Test
     void create() {
         CardManager cardManager = CardManager.create();
@@ -21,5 +29,16 @@ class PlayersTest {
                         )
                 )
         ).doesNotThrowAnyException();
+    }
+
+    @DisplayName("블랙잭일 경우 상금의 1.5배 획득")
+    @Test
+    void calculateProfit() {
+        List<Card> cards = new ArrayList<>();
+        cards.add(Card.of(Suit.CLUB, Denomination.KING));
+        cards.add(Card.of(Suit.CLUB, Denomination.ACE));
+        Hands hands = new Hands(cards);
+        Player player = new Player("pk", 1000, hands);
+        assertThat(player.calculateProfit(ResultType.WIN)).isEqualTo(1500);
     }
 }
