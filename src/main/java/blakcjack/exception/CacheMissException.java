80ba7312a6a887be16209bedee204fad72e3157b
seunghyun <ggyool@naver.com>
2021-03-10 17:04:49 +0900

    refactor: 예외 네이밍 변경 (FailedCacheHit -> CacheMiss)

--- src/main/java/blakcjack/exception/CacheMissException.java
+++ src/main/java/blakcjack/exception/CacheMissException.java
@@ -3,14 +3,14 @@ package blakcjack.exception;
 import blakcjack.domain.card.CardNumber;
 import blakcjack.domain.card.CardSymbol;
 
-public class FailedCacheHitException extends RuntimeException {
+public class CacheMissException extends RuntimeException {
     private static final String DEFAULT_MESSAGE = "캐시에 없습니다";
 
-    public FailedCacheHitException() {
+    public CacheMissException() {
         super(DEFAULT_MESSAGE);
     }
 
-    public FailedCacheHitException(final CardSymbol cardSymbol, final CardNumber cardNumber) {
+    public CacheMissException(final CardSymbol cardSymbol, final CardNumber cardNumber) {
         super(String.format("%s (%s, %s)", DEFAULT_MESSAGE, cardSymbol.getName(), cardNumber.getName()));
     }
 }
