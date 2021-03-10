9ec9f46b2b5f1225bf2fdc3dc0914e358276fbec
NewWisdom <wlgp2500@gmail.com>
2021-03-10 23:47:21 +0900

    test(state-factory): 카드에 따라 state 객체 반환 테스트 추가

--- src/main/java/blackjack/domain/state/StateFactory.java
+++ src/main/java/blackjack/domain/state/StateFactory.java
@@ -4,6 +4,9 @@ import blackjack.domain.user.Cards;
 
 public class StateFactory {
     public static State generateStateByCards(Cards cards) {
+        if (cards.isBlackjack()) {
+            return new Blackjack(cards);
+        }
         return new Hit(cards);
     }
 }
--- src/test/java/blackjack/domain/state/StateFactoryTest.java
+++ src/test/java/blackjack/domain/state/StateFactoryTest.java
@@ -12,12 +12,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StateFactoryTest {
     @Test
     @DisplayName("카드에 따라 state 객체를 반환한다.")
-    void createState() {
+    void createState1() {
         Cards cards = new Cards(
                 new Card(Suit.CLOVER, Denomination.JACK),
                 new Card(Suit.HEART, Denomination.TEN));
         State state = StateFactory.generateStateByCards(cards);
 
         assertThat(state).isInstanceOf(Hit.class);
+    }
+
+    @Test
+    @DisplayName("카드에 따라 state 객체를 반환한다.")
+    void createState2() {
+        Cards cards = new Cards(
+                new Card(Suit.CLOVER, Denomination.ACE),
+                new Card(Suit.HEART, Denomination.ACE));
+        State state = StateFactory.generateStateByCards(cards);
+
+        assertThat(state).isInstanceOf(Blackjack.class);
     }
 }
