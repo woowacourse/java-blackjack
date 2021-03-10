a34258d3ff982c6a27986452d1523b12acf1f1b2
pjy1368 <basejin0810@naver.com>
2021-03-10 14:50:52 +0900

    refactor(BlackjackGame): 사용하지 않는 메소드 및 클래스 제거

--- src/main/java/blackjack/domain/BlackjackGame.java
+++ src/main/java/blackjack/domain/BlackjackGame.java
@@ -2,49 +2,11 @@ package blackjack.domain;
 
 import blackjack.domain.participants.Name;
 import blackjack.domain.participants.Participant;
-import blackjack.domain.participants.Participants;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class BlackjackGame {
-
-    public GameResult createGameResult(final Participants participants) {
-        final Participant dealer = participants.getDealer();
-        final List<Participant> players = participants.getPlayers();
-        final List<Integer> dealerCounts = makeDealerMatchCounts(dealer, players);
-        final Map<Name, Result> playerResults = makePlayerResults(players, dealer);
-        return new GameResult(dealerCounts, playerResults);
-    }
-
-    public List<Integer> makeDealerMatchCounts(final Participant dealer,
-        final List<Participant> players) {
-        final List<Result> dealerResults = Arrays.stream(Result.values())
-            .collect(Collectors.toList());
-
-        return dealerResults.stream()
-            .map(result -> calculateDealerResult(dealer, players, result))
-            .collect(Collectors.toList());
-    }
-
-    public int calculateDealerResult(final Participant dealer, final List<Participant> players,
-        final Result result) {
-        return (int) players.stream()
-            .filter(player -> dealer.decideWinner(player).isSameResult(result))
-            .count();
-    }
-
-    public Map<Name, Result> makePlayerResults(final List<Participant> players,
-        final Participant dealer) {
-        final Map<Name, Result> results = new LinkedHashMap<>();
-        for (final Participant player : players) {
-            final Result result = player.decideWinner(dealer);
-            results.put(player.getName(), result);
-        }
-        return results;
-    }
 
     public Map<Name, Integer> makeParticipantResults(final Participant dealer,
         final List<Participant> players) {
--- src/test/java/blackjack/domain/BlackjackGameTest.java
+++ src/test/java/blackjack/domain/BlackjackGameTest.java
@@ -9,15 +9,11 @@ import blackjack.domain.card.Card;
 import blackjack.domain.card.CardNumber;
 import blackjack.domain.card.CardType;
 import blackjack.domain.participants.Dealer;
-import blackjack.domain.participants.Name;
 import blackjack.domain.participants.Participant;
 import blackjack.domain.participants.Player;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 public class BlackjackGameTest {
 
@@ -35,26 +31,4 @@ public class BlackjackGameTest {
         }
     }
 
-    @Test
-    @DisplayName("딜러의 승수가 잘 구해지는지 확인")
-    void calculateDealerWinCount() {
-        assertThat(blackjackGame.calculateDealerResult(dealer, players, Result.WIN)).isEqualTo(3);
-    }
-
-    @Test
-    @DisplayName("딜러의 승무패 횟수가 잘 구해지는지 확인")
-    void makeDealerMatchCounts() {
-        final List<Integer> dealerCounts = blackjackGame.makeDealerMatchCounts(dealer, players);
-        assertThat(dealerCounts).hasSize(4)
-            .contains(3, 0, 0);
-    }
-
-    @Test
-    @DisplayName("플레이어의 경기 결과가 잘 구해지는지 확인")
-    void makePlayerResults() {
-        final Map<Name, Result> playersResult = blackjackGame.makePlayerResults(players, dealer);
-        assertThat(playersResult).hasSize(3)
-            .contains(entry(new Name(0 + ""), Result.LOSE), entry(new Name(1 + ""), Result.LOSE),
-                entry(new Name(2 + ""), Result.LOSE));
-    }
 }
