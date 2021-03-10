39bc1257d6ca3bf2fa3f9ee52cbce2634c021b74
Naeun <knae11@naver.com>
2021-03-10 21:38:27 +0900

    feat: dealer 테스트코드 작성

--- src/test/java/blackjack/domain/gamer/DealerTest.java
+++ src/test/java/blackjack/domain/gamer/DealerTest.java
@@ -3,43 +3,28 @@ package blackjack.domain.gamer;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import blackjack.domain.card.Cards;
-import blackjack.domain.gametable.GameTable;
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Denominations;
 import blackjack.domain.card.Suits;
-import blackjack.domain.gamer.Dealer;
-import blackjack.domain.gamer.Participant;
-import blackjack.domain.utils.FixedCardDeck;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DealerTest {
-    @Test
-    @DisplayName("dealer 초기에 카드 두장을 갖고 있는지 확인")
-    void create3() {
-        Participant dealer = new Dealer();
-        final GameTable gameTable = new GameTable(dealer, new FixedCardDeck());
-
-        List<Card> playerCards = dealer.getUnmodifiableCards();
-
-        assertThat(playerCards).hasSize(2);
-    }
 
     @Test
-    @DisplayName("dealer 카드 추가 지급")
-    void add_card() {
+    @DisplayName("카드 지급")
+    void takeCard() {
         Participant dealer = new Dealer();
-        final GameTable gameTable = new GameTable(dealer, new FixedCardDeck());
-        gameTable.giveCard(dealer);
+        dealer.takeCard(Card.from(Suits.DIAMOND, Denominations.KING));
 
-        assertThat(dealer.getUnmodifiableCards()).hasSize(3);
+        assertThat(dealer.getUnmodifiableCards()).hasSize(1);
     }
 
     @Test
     @DisplayName("Dealer 16 초과한 경우")
-    void sum_cards_for_result1() {
+    void isNotAbleToTake() {
         List<Card> cards = Arrays.asList(
             Card.from(Suits.DIAMOND, Denominations.KING),
             Card.from(Suits.CLOVER, Denominations.KING));
