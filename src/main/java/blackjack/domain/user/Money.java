acf0bbf7ccd3bfabbfbed0881ab89215f7db7079
daeun <leede0418@likelion.org>
2021-03-11 20:19:14 +0900

    feat(money): 베팅 금액이 0원 이하인 경우 예외를 발생하는 기능 구현

--- src/main/java/blackjack/domain/user/Money.java
+++ src/main/java/blackjack/domain/user/Money.java
@@ -1,9 +1,18 @@
 package blackjack.domain.user;
 
 public class Money {
+    private static final int MINIMUM_BOUND = 0;
+
     private final int money;
 
     public Money(int money) {
+        validate(money);
         this.money = money;
+    }
+
+    private void validate(int money) {
+        if (money <= MINIMUM_BOUND) {
+            throw new IllegalArgumentException("베팅 금액은 0원 이하일 수 없습니다.");
+        }
     }
 }
--- src/test/java/blackjack/domain/user/MoneyTest.java
+++ src/test/java/blackjack/domain/user/MoneyTest.java
@@ -3,7 +3,7 @@ package blackjack.domain.user;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class MoneyTest {
     @DisplayName("Money 객체를 생성한다.")
@@ -12,5 +12,21 @@ public class MoneyTest {
         Money money = new Money(10000);
 
         assertThat(money).isInstanceOf(Money.class);
+    }
+
+    @DisplayName("베팅 금액이 0원 이하인 경우 예외를 발생한다. - 0원인 경우")
+    @Test
+    public void validateZeroException() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Money(0);
+        }).withMessage("베팅 금액은 0원 이하일 수 없습니다.");
+    }
+
+    @DisplayName("베팅 금액이 0원 이하인 경우 예외를 발생한다. - 음수인 경우")
+    @Test
+    public void validateNegativeException() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Money(-10000);
+        }).withMessage("베팅 금액은 0원 이하일 수 없습니다.");
     }
 }
