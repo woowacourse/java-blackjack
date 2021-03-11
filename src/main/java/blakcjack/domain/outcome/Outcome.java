8478503f8d2883b788b7aaf2945376675a650df7
sakjung <ssangyu123@gmail.com>
2021-03-11 17:50:38 +0900

    refactor: outcome 의 expression을BiPredicate로 바꿈

--- src/main/java/blakcjack/domain/outcome/Outcome.java
+++ src/main/java/blakcjack/domain/outcome/Outcome.java
@@ -4,7 +4,7 @@ import blakcjack.domain.participant.Dealer;
 import blakcjack.domain.participant.Player;
 
 import java.util.Arrays;
-import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 
 public enum Outcome {
 	BLACKJACK_WIN(1.5f, Outcome::isWinningByBlackjack),
@@ -13,9 +13,9 @@ public enum Outcome {
 	LOSE(-1f, (dealer, player) -> true);
 
 	private final float earningRate;
-	private final BiFunction<Dealer, Player, Boolean> expression;
+	private final BiPredicate<Dealer, Player> expression;
 
-	Outcome(final float earningRate, final BiFunction<Dealer, Player, Boolean> expression) {
+	Outcome(final float earningRate, final BiPredicate<Dealer, Player> expression) {
 		this.earningRate = earningRate;
 		this.expression = expression;
 	}
@@ -34,7 +34,7 @@ public enum Outcome {
 
 	static Outcome of(final Dealer dealer, final Player player) {
 		return Arrays.stream(values())
-				.filter(outcome -> outcome.expression.apply(dealer, player))
+				.filter(outcome -> outcome.expression.test(dealer, player))
 				.findFirst()
 				.orElseThrow(NoSuchOutcomeException::new);
 	}
