50854338098c0f1af303ee4f3050aa1377c9e4a8
YeongBeom Heo <hybeom@gmail.com>
2021-03-11 21:51:58 +0900

    feat(Money) : 판 돈 추가

---
+++ src/test/java/blackjack/domain/money/MoneyTest.java
@@ -0,0 +1,32 @@
+package blackjack.domain.money;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import blackjack.exception.BlackJackException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+
+    @Test
+    @DisplayName("판돈 생성 테스트")
+    void moneyTest() {
+        int initialMoney = 1000;
+        Money money = new Money(initialMoney);
+
+        int rawMoney = money.getValue();
+
+        assertThat(rawMoney).isEqualTo(initialMoney);
+    }
+
+    @Test
+    @DisplayName("판돈 생성 실패 테스트 ")
+    void moneyFailTest() {
+        int initialMoney = -1000;
+
+        assertThatThrownBy(() -> new Money(initialMoney))
+            .isInstanceOf(BlackJackException.class)
+            .hasMessageContaining(Money.NEGATIVE_MONEY_MESSAGE);
+    }
+}
