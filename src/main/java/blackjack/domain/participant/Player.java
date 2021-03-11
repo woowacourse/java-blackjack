7be6d05d7b6d2b053cb8bcbee9e48a9b7dabdbd6
hoony <younghoon.kwon91@gmail.com>
2021-03-11 13:57:08 +0900

    refactor: 결과 객체가 직접 비교하도록 변경

--- src/main/java/blackjack/domain/participant/Player.java
+++ src/main/java/blackjack/domain/participant/Player.java
@@ -11,6 +11,40 @@ public class Player extends Participant {
         super(cards, name);
     }
 
+    public boolean playerBlackJack(final Dealer dealer) {
+        return this.isBlackJack() && !dealer.isBlackJack();
+    }
+
+    public boolean dealerBlackJack(final Dealer dealer) {
+        return !this.isBlackJack() && dealer.isBlackJack();
+    }
+
+    public boolean bothBlackJack(final Dealer dealer) {
+        return this.isBlackJack() && dealer.isBlackJack();
+    }
+
+    public boolean winCondition(final Dealer dealer) {
+        return dealer.isBust() && !this.isBust() ||
+                !this.isBust() && this.scoreBiggerThan(dealer);
+    }
+
+    public boolean lossCondition(final Dealer dealer) {
+        return this.isBust() ||
+                (!dealer.isBust() && this.scoreSmallerThan(dealer));
+    }
+
+    public boolean drawCondition(final Dealer dealer) {
+        return this.getScore() == dealer.getScore();
+    }
+
+    public boolean scoreBiggerThan(final Dealer dealer) {
+        return this.getScore() > dealer.getScore();
+    }
+
+    public boolean scoreSmallerThan(final Dealer dealer) {
+        return this.getScore() < dealer.getScore();
+    }
+
     @Override
     public List<Card> getInitCardsAsList() {
         return hand.getCardsAsList();
--- src/main/java/blackjack/domain/result/Result.java
+++ src/main/java/blackjack/domain/result/Result.java
@@ -8,9 +8,9 @@ import java.util.function.BiPredicate;
 
 public enum Result {
 
-    WIN("승", Result::playerBlackJack, Result::winCondition),
-    LOSE("패", Result::dealerBlackJack, Result::lossCondition),
-    DRAW("무", Result::bothBlackJack, Result::drawCondition);
+    WIN("승", Player::playerBlackJack, Player::winCondition),
+    LOSE("패", Player::dealerBlackJack, Player::lossCondition),
+    DRAW("무", Player::bothBlackJack, Player::drawCondition);
 
     private final String value;
     private final BiPredicate<Player, Dealer> blackJackCheck;
@@ -36,39 +36,5 @@ public enum Result {
 
     public String getResultAsString() {
         return this.value;
-    }
-
-    private static boolean playerBlackJack(final Player player, final Dealer dealer) {
-        return player.isBlackJack() && !dealer.isBlackJack();
-    }
-
-    private static boolean dealerBlackJack(final Player player, final Dealer dealer) {
-        return !player.isBlackJack() && dealer.isBlackJack();
-    }
-
-    private static boolean bothBlackJack(final Player player, final Dealer dealer) {
-        return player.isBlackJack() && dealer.isBlackJack();
-    }
-
-    private static boolean winCondition(final Player player, final Dealer dealer) {
-        return dealer.isBust() && !player.isBust() ||
-                !player.isBust() && scoreBiggerThan(player, dealer);
-    }
-
-    private static boolean lossCondition(final Player player, final Dealer dealer) {
-        return player.isBust() ||
-                (!dealer.isBust() && scoreSmallerThan(player, dealer));
-    }
-
-    private static boolean drawCondition(final Player player, final Dealer dealer) {
-        return player.getScore() == dealer.getScore();
-    }
-
-    private static boolean scoreBiggerThan(final Player player, final Dealer dealer) {
-        return player.getScore() > dealer.getScore();
-    }
-
-    private static boolean scoreSmallerThan(final Player player, final Dealer dealer) {
-        return player.getScore() < dealer.getScore();
     }
 }
