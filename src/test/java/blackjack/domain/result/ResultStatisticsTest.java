0e811f71f3d9f334354a735002d40cebd9fc4beb
yoonsung.jung <ybook2012@gmail.com>
2021-03-11 15:31:07 +0900

    refactor(ResultStaticsTest) : display를 명시적으로 수정

--- src/test/java/blackjack/domain/result/ResultStatisticsTest.java
+++ src/test/java/blackjack/domain/result/ResultStatisticsTest.java
@@ -18,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ResultStatisticsTest {
 
     private Dealer dealer;
+    private final int money = 1000;
 
     @BeforeEach
     void setUp() {
@@ -28,7 +29,7 @@ public class ResultStatisticsTest {
     }
 
     @Test
-    @DisplayName("챌린저가 1000원을 배팅하고, 블랙잭이며, 딜러는 블랙잭이 아니면 챌린저의 수익금은 1500원이다.")
+    @DisplayName("챌린저가 money를 배팅하고, 블랙잭이며, 딜러는 블랙잭이 아니면 챌린저의 수익금은 1.5배이다.")
     void challengerBlackJackProfit() {
         List<Card> cardList = new ArrayList<>();
         cardList.add(new Card(Suit.DIAMOND, Face.QUEEN));
@@ -36,17 +37,17 @@ public class ResultStatisticsTest {
 
         List<Challenger> challengers = new ArrayList<>();
         Challenger challenger = new Challenger(new Cards(cardList), new Name("pobi"));
-        challenger.betting(new BetMoney(1000));
+        challenger.betting(new BetMoney(money));
 
         challengers.add(challenger);
 
         ResultStatistics resultStatistics = new ResultStatistics(new Challengers(challengers), dealer);
-        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo(1500);
-        assertThat(resultStatistics.getDealerProfit()).isEqualTo(-1500);
+        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo((int) (money * 1.5));
+        assertThat(resultStatistics.getDealerProfit()).isEqualTo(-(int) (money * 1.5));
     }
 
     @Test
-    @DisplayName("챌린저가 1000원을 배팅하고, 블랙잭이며, 딜러도 블랙잭이면 챌린저의 수익금은 0원이다.")
+    @DisplayName("챌린저가 money를 배팅하고, 블랙잭이며, 딜러도 블랙잭이면 챌린저의 수익금은 0원이다.")
     void challengerBlackJackDouble() {
         List<Card> cardList = new ArrayList<>();
         cardList.add(new Card(Suit.DIAMOND, Face.QUEEN));
@@ -54,7 +55,7 @@ public class ResultStatisticsTest {
 
         List<Challenger> challengers = new ArrayList<>();
         Challenger challenger = new Challenger(new Cards(cardList), new Name("pobi"));
-        challenger.betting(new BetMoney(1000));
+        challenger.betting(new BetMoney(money));
 
         challengers.add(challenger);
 
@@ -66,7 +67,7 @@ public class ResultStatisticsTest {
     }
 
     @Test
-    @DisplayName("챌린저가 1000원을 배팅하고, 블랙잭이 아니며, 딜러보다 점수가 높으면(20점 vs 19점), 챌린저의 수익금은 1000원이다.")
+    @DisplayName("챌린저가 money를 배팅하고, 블랙잭이 아니며, 딜러보다 점수가 높으면, 챌린저의 수익금은 1.0배이다")
     void challengerWinProfit() {
         List<Card> cardList = new ArrayList<>();
         cardList.add(new Card(Suit.DIAMOND, Face.QUEEN));
@@ -74,17 +75,17 @@ public class ResultStatisticsTest {
 
         List<Challenger> challengers = new ArrayList<>();
         Challenger challenger = new Challenger(new Cards(cardList), new Name("pobi"));
-        challenger.betting(new BetMoney(1000));
+        challenger.betting(new BetMoney(money));
 
         challengers.add(challenger);
 
         ResultStatistics resultStatistics = new ResultStatistics(new Challengers(challengers), dealer);
-        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo(1000);
-        assertThat(resultStatistics.getDealerProfit()).isEqualTo(-1000);
+        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo(money);
+        assertThat(resultStatistics.getDealerProfit()).isEqualTo(-money);
     }
 
     @Test
-    @DisplayName("챌린저가 1000원을 배팅하고, 블랙잭이 아니며, 딜러보다 점수가 높으면(3장 21점 vs 19점), 챌린저의 수익금은 1000원이다.")
+    @DisplayName("챌린저가 money를 배팅하고, 블랙잭이 아니며, 딜러보다 점수가 높으면 챌린저의 수익금은 1.0배이다.")
     void challengerWinProfit21() {
         List<Card> cardList = new ArrayList<>();
         cardList.add(new Card(Suit.DIAMOND, Face.QUEEN));
@@ -93,17 +94,17 @@ public class ResultStatisticsTest {
 
         List<Challenger> challengers = new ArrayList<>();
         Challenger challenger = new Challenger(new Cards(cardList), new Name("pobi"));
-        challenger.betting(new BetMoney(1000));
+        challenger.betting(new BetMoney(money));
 
         challengers.add(challenger);
 
         ResultStatistics resultStatistics = new ResultStatistics(new Challengers(challengers), dealer);
-        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo(1000);
-        assertThat(resultStatistics.getDealerProfit()).isEqualTo(-1000);
+        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo(money);
+        assertThat(resultStatistics.getDealerProfit()).isEqualTo(-money);
     }
 
     @Test
-    @DisplayName("챌린저가 1000원을 배팅하고, 블랙잭이 아니며, 딜러보다 점수가 낮으면(18점 vs 19점), 챌린저의 수익금은 -1000원이다.")
+    @DisplayName("챌린저가 money를 배팅하고, 블랙잭이 아니며, 딜러보다 점수가 낮으면 챌린저의 수익금은 -1.0배 이다.")
     void challengerLoseProfit() {
         List<Card> cardList = new ArrayList<>();
         cardList.add(new Card(Suit.DIAMOND, Face.JACK));
@@ -111,17 +112,17 @@ public class ResultStatisticsTest {
 
         List<Challenger> challengers = new ArrayList<>();
         Challenger challenger = new Challenger(new Cards(cardList), new Name("pobi"));
-        challenger.betting(new BetMoney(1000));
+        challenger.betting(new BetMoney(money));
 
         challengers.add(challenger);
 
         ResultStatistics resultStatistics = new ResultStatistics(new Challengers(challengers), dealer);
-        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo(-1000);
-        assertThat(resultStatistics.getDealerProfit()).isEqualTo(1000);
+        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo(-money);
+        assertThat(resultStatistics.getDealerProfit()).isEqualTo(money);
     }
 
     @Test
-    @DisplayName("챌린저가 1000원을 배팅하고, 버스트(28)가 나며, 딜러는 버스트가 아니면, 챌린저의 수익금은 -1000원이다.")
+    @DisplayName("챌린저가 money를 배팅하고, 버스트가 나며, 딜러는 버스트가 아니면, 챌린저의 수익금은 -1.0배이다.")
     void challengerLoseProfitWithBust() {
         List<Card> cardList = new ArrayList<>();
         cardList.add(new Card(Suit.DIAMOND, Face.JACK));
@@ -130,17 +131,17 @@ public class ResultStatisticsTest {
 
         List<Challenger> challengers = new ArrayList<>();
         Challenger challenger = new Challenger(new Cards(cardList), new Name("pobi"));
-        challenger.betting(new BetMoney(1000));
+        challenger.betting(new BetMoney(money));
 
         challengers.add(challenger);
 
         ResultStatistics resultStatistics = new ResultStatistics(new Challengers(challengers), dealer);
-        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo(-1000);
-        assertThat(resultStatistics.getDealerProfit()).isEqualTo(1000);
+        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo(-money);
+        assertThat(resultStatistics.getDealerProfit()).isEqualTo(money);
     }
 
     @Test
-    @DisplayName("챌린저가 1000원을 배팅하고, 버스트(28)가 나며, 딜러도 버스트(28)이면 챌린저의 수익금은 -1000원이다.")
+    @DisplayName("챌린저가 money를 배팅하고, 버스트가 나며, 딜러도 버스트(28)이면 챌린저의 수익금은 -1.0배이다.")
     void challengerLoseProfitWithDoubleBust() {
         List<Card> cardList = new ArrayList<>();
         cardList.add(new Card(Suit.DIAMOND, Face.JACK));
@@ -149,20 +150,20 @@ public class ResultStatisticsTest {
 
         List<Challenger> challengers = new ArrayList<>();
         Challenger challenger = new Challenger(new Cards(cardList), new Name("pobi"));
-        challenger.betting(new BetMoney(1000));
+        challenger.betting(new BetMoney(money));
 
         challengers.add(challenger);
 
         Dealer dealerBust = new Dealer(new Cards(cardList));
 
         ResultStatistics resultStatistics = new ResultStatistics(new Challengers(challengers), dealerBust);
-        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo(-1000);
-        assertThat(resultStatistics.getDealerProfit()).isEqualTo(1000);
+        assertThat(resultStatistics.getChallengerProfit(challenger)).isEqualTo(-money);
+        assertThat(resultStatistics.getDealerProfit()).isEqualTo(money);
     }
 
 
     @Test
-    @DisplayName("챌린저가 1000원을 배팅하고, 챌린저 딜려 양측이 전부 버스트와 블랙잭이 아니며, 동점(19)이면 챌린저의 수익금은 0원이다.")
+    @DisplayName("챌린저가 money를 배팅하고, 챌린저 딜려 양측이 전부 버스트와 블랙잭이 아니며, 동점이면 챌린저의 수익금은 0원이다.")
     void challengerDraw() {
         List<Card> cardList = new ArrayList<>();
         cardList.add(new Card(Suit.DIAMOND, Face.JACK));
@@ -170,7 +171,7 @@ public class ResultStatisticsTest {
 
         List<Challenger> challengers = new ArrayList<>();
         Challenger challenger = new Challenger(new Cards(cardList), new Name("pobi"));
-        challenger.betting(new BetMoney(1000));
+        challenger.betting(new BetMoney(money));
 
         challengers.add(challenger);
         ResultStatistics resultStatistics = new ResultStatistics(new Challengers(challengers), dealer);
@@ -180,7 +181,7 @@ public class ResultStatisticsTest {
 
 
     @Test
-    @DisplayName("3명의 챌린저가 1000원을 배팅하고 승,패,패 이면, 딜러의 수익금은 1000원이다.")
+    @DisplayName("3명의 챌린저가 money를 배팅하고 승,패,패 이면, 딜러의 수익금은 1.0배이다.")
     void dealerMultiWin() {
         List<Card> cardList = new ArrayList<>();
         cardList.add(new Card(Suit.DIAMOND, Face.JACK));
@@ -188,20 +189,20 @@ public class ResultStatisticsTest {
 
         List<Challenger> challengers = new ArrayList<>();
         Challenger challenger1 = new Challenger(new Cards(cardList), new Name("pobi"));
-        challenger1.betting(new BetMoney(1000));
+        challenger1.betting(new BetMoney(money));
 
         cardList.add(new Card(Suit.DIAMOND, Face.JACK));
         Challenger challenger2 = new Challenger(new Cards(cardList), new Name("json"));
-        challenger2.betting(new BetMoney(1000));
+        challenger2.betting(new BetMoney(money));
 
         Challenger challenger3 = new Challenger(new Cards(cardList), new Name("brown"));
-        challenger3.betting(new BetMoney(1000));
+        challenger3.betting(new BetMoney(money));
 
         challengers.add(challenger1);
         challengers.add(challenger2);
         challengers.add(challenger3);
 
         ResultStatistics resultStatistics = new ResultStatistics(new Challengers(challengers), dealer);
-        assertThat(resultStatistics.getDealerProfit()).isEqualTo(1000);
+        assertThat(resultStatistics.getDealerProfit()).isEqualTo(money);
     }
 }
