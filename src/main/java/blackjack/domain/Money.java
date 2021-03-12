d605a7f2ec7b4a8eaf79fdbff8b8cb328beb71fa
Gomding <backchida@gmail.com>
2021-03-12 21:34:50 +0900

    feat : 베팅 금액을 저장할 Money 클래스 구현

---
+++ src/main/java/blackjack/domain/Money.java
@@ -0,0 +1,53 @@
+package blackjack.domain;
+
+import java.util.Objects;
+
+public class Money {
+
+    private final long value;
+
+    public Money(String input) {
+        validateNull(input);
+        validateEmpty(input);
+        validateParseNumber(input);
+        validatePositiveNumber(input);
+        this.value = Integer.parseInt(input.trim());
+    }
+
+    private void validateNull(String input) {
+        Objects.requireNonNull(input, "베팅 입력값은 null이 될 수 없습니다.");
+    }
+
+    private void validateEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("베팅 입력값은 빈값이 될 수 없습니다.");
+        }
+    }
+
+    private void validateParseNumber(String input) {
+        try {
+            Integer.parseInt(input.trim());
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("베팅 입력값은 숫자여야 합니다.");
+        }
+    }
+
+    private void validatePositiveNumber(String input) {
+        if (Integer.parseInt(input.trim()) < 1) {
+            throw new IllegalArgumentException("베팅 입력값은 1이상의 양의 정수 여야합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return value == money.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/blackjack/domain/MoneyTest.java
@@ -0,0 +1,53 @@
+package blackjack.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MoneyTest {
+
+    @Test
+    @DisplayName("Money 객체 생성된다.")
+    void createMoneyTest() {
+        Money money = new Money("10000");
+
+        assertThat(money).isInstanceOf(Money.class);
+    }
+
+    @ParameterizedTest(name = "Money 입력값이 null이면 예외가 발생합니다.")
+    @NullSource
+    void moneyInputNull(String input) {
+        assertThatThrownBy(() -> {
+            new Money(input);
+        }).isInstanceOf(NullPointerException.class).hasMessage("베팅 입력값은 null이 될 수 없습니다.");
+    }
+
+    @ParameterizedTest(name = "Money 입력값이 empty이면 예외가 발생합니다.")
+    @EmptySource
+    void moneyInputEmpty(String input) {
+        assertThatThrownBy(() -> {
+            new Money(input);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("베팅 입력값은 빈값이 될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("Money 입력값으로 문자열을 입력하면 예외가 발생합니다.")
+    void moneyInputString() {
+        assertThatThrownBy(() -> {
+           new Money("이건 문자열이다.");
+        }).isInstanceOf(NumberFormatException.class).hasMessage("베팅 입력값은 숫자여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("Money 입력값으로 음수를 입력하면 예외가 발생합니다.")
+    void moneyInputNegativeNumber() {
+        assertThatThrownBy(() -> {
+            new Money("-10000");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("베팅 입력값은 1이상의 양의 정수 여야합니다.");
+    }
+}
