531ea6e48867e68ae5d94e813d73ece32c22120b
Kimun Kim <kkm97351@gmail.com>
2021-03-12 15:22:25 +0900

    feat: Money의 음수 검증 로직 추가

--- src/main/java/blackjack/domain/Money.java
+++ src/main/java/blackjack/domain/Money.java
@@ -5,8 +5,19 @@ import java.util.Objects;
 public class Money {
     private final long money;
 
-    public Money(long money) {
+    private Money(long money) {
         this.money = money;
+    }
+
+    public static Money of(long money) {
+        validate(money);
+        return new Money(money);
+    }
+
+    private static void validate(long money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("음수는 받을 수 없습니다.");
+        }
     }
 
     @Override
--- src/test/java/blackjack/domain/MoneyTest.java
+++ src/test/java/blackjack/domain/MoneyTest.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MoneyTest {
     @DisplayName("같은 금액의 Money 인스턴스는 같은 객체로 간주한다.")
     @Test
     void equalsTest() {
-        Money money = new Money(10000);
+        Money money = Money.of(10000);
 
-        assertThat(money).isEqualTo(new Money(10000));
+        assertThat(money).isEqualTo(Money.of(10000));
+    }
+
+    @DisplayName("음수가 인수로 주어지면 예외를 발생시킨다.")
+    @Test
+    void throwExceptionOnNegativeNumber() {
+        assertThatThrownBy(() -> Money.of(-1)).isInstanceOf(IllegalArgumentException.class);
     }
 }
