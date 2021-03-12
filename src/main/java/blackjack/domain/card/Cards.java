451f6709393a0e95c5723fa5f54e5334a10d4096
bepoz <kangsy763@naver.com>
2021-03-12 17:10:33 +0900

    refactor: 카드 점수 계산 로직 수정

--- src/main/java/blackjack/domain/card/Cards.java
+++ src/main/java/blackjack/domain/card/Cards.java
@@ -7,6 +7,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cards {
+    private static final int ZERO = 0;
+    private static final int DIFFERENCE_OF_ACE_VALUE = 10;
+    private static final int BLACKJACK_NUMBER = 21;
+    private static final int BLACKJACK_CONDITION_CARDS_SIZE = 2;
     private final List<Card> cards = new ArrayList<>();
 
     public Cards(Card firstCard, Card secondCard) {
@@ -15,13 +19,39 @@ public class Cards {
     }
 
     public boolean isBlackjack() {
-        return cards.size() == 2 && sumCardsScore() == 21;
+        return cards.size() == BLACKJACK_CONDITION_CARDS_SIZE && calculateCardsScore() == BLACKJACK_NUMBER;
     }
 
-    public int sumCardsScore() {
+    public int calculateCardsScore() {
+        int currentCardsScore = sumCardsScore();
+        int possibleLoopCount = countAce();
+
+        while (canLowerCardsValue(currentCardsScore, possibleLoopCount)) {
+            currentCardsScore = lowerValueOfAce(currentCardsScore);
+            possibleLoopCount--;
+        }
+
+        return currentCardsScore;
+    }
+
+    private int countAce() {
+        return (int) cards.stream()
+                .filter(Card::isAce)
+                .count();
+    }
+
+    private int sumCardsScore() {
         return cards.stream()
                 .mapToInt(Card::getScore)
                 .sum();
+    }
+
+    private boolean canLowerCardsValue(int score, int remainLoop) {
+        return score > BLACKJACK_NUMBER && remainLoop > ZERO;
+    }
+
+    private int lowerValueOfAce(int value) {
+        return value - DIFFERENCE_OF_ACE_VALUE;
     }
 
     public String getCardsInfoToString() {
