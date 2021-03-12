6965239a4d028027d4d77ba27f23dc329fa4665f
Kimun Kim <kkm97351@gmail.com>
2021-03-12 12:14:04 +0900

    refactor: MatchRule 람다 함수 대신 이름을 가진 메소드로 재정의
    - 실제 메소드 명을 부여함으로써 가독성을 재고했습니다.

--- src/main/java/blackjack/domain/MatchRule.java
+++ src/main/java/blackjack/domain/MatchRule.java
@@ -7,16 +7,17 @@ import java.util.function.BiPredicate;
 import java.util.stream.Stream;
 
 public enum MatchRule {
-    BUST_PLAYER((player, dealer) -> player.isBust(), ResultType.LOSE),
-    BUST_DEALER((player, dealer) -> !player.isBust() && dealer.isBust(), ResultType.WIN),
-    BLACKJACK_PLAYER_WIN((player, dealer) -> player.isBlackJack() && !dealer.isBlackJack(), ResultType.WIN),
-    BLACKJACK_DEALER_WIN((player, dealer) -> !player.isBlackJack() && dealer.isBlackJack(), ResultType.LOSE),
-    BLACKJACK_DRAW((player, dealer) -> player.isBlackJack() && dealer.isBlackJack(), ResultType.DRAW),
-    SCORE_COMPARISON_PLAYER_WIN((player, dealer) -> player.getScore() > dealer.getScore(), ResultType.WIN),
-    SCORE_COMPARISON_DEALER_WIN((player, dealer) -> player.getScore() < dealer.getScore(), ResultType.LOSE),
-    SCORE_COMPARISON_DRAW((player, dealer) -> player.getScore() == dealer.getScore(), ResultType.DRAW);
+    BUST_PLAYER(MatchRule::isPlayerBust, ResultType.LOSE),
+    BUST_DEALER(MatchRule::isOnlyDealerBust, ResultType.WIN),
+    BLACKJACK_PLAYER_WIN(MatchRule::isOnlyPlayerBlackJack, ResultType.WIN),
+    BLACKJACK_DEALER_WIN(MatchRule::isOnlyDealerBlackJack, ResultType.LOSE),
+    BLACKJACK_DRAW(MatchRule::isBothBlackJack, ResultType.DRAW),
+    SCORE_COMPARISON_PLAYER_WIN(MatchRule::isPlayerScoreHigher, ResultType.WIN),
+    SCORE_COMPARISON_DEALER_WIN(MatchRule::isDealerScoreHigher, ResultType.LOSE),
+    SCORE_COMPARISON_DRAW(MatchRule::isBothScoreEquals, ResultType.DRAW);
 
     private final BiPredicate<Player, Dealer> condition;
+
     private final ResultType matchResult;
 
     MatchRule(BiPredicate<Player, Dealer> condition, ResultType matchResult) {
@@ -30,5 +31,36 @@ public enum MatchRule {
                 .findAny()
                 .map(matchRule -> matchRule.matchResult)
                 .orElseThrow(() -> new IllegalArgumentException("조건에 맞는 승부 결과가 없습니다."));
+    }
+
+    private static boolean isPlayerBust(Player player, Dealer dealer) {
+        return player.isBust();
+    }
+
+    private static boolean isOnlyDealerBust(Player player, Dealer dealer) {
+        return !player.isBust() && dealer.isBust();
+    }
+
+    private static boolean isOnlyPlayerBlackJack(Player player, Dealer dealer) {
+        return player.isBlackJack() && !dealer.isBlackJack();
+    }
+
+    private static boolean isOnlyDealerBlackJack(Player player, Dealer dealer) {
+        return !player.isBlackJack() && dealer.isBlackJack();
+    }
+
+    private static boolean isBothBlackJack(Player player, Dealer dealer) {
+        return player.isBlackJack() && dealer.isBlackJack();
+    }
+    private static boolean isPlayerScoreHigher(Player player, Dealer dealer) {
+        return player.getScore() > dealer.getScore();
+    }
+
+    private static boolean isDealerScoreHigher(Player player, Dealer dealer) {
+        return player.getScore() < dealer.getScore();
+    }
+
+    private static boolean isBothScoreEquals(Player player, Dealer dealer) {
+        return player.getScore() == dealer.getScore();
     }
 }
