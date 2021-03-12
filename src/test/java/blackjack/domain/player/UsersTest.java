bd7dda5d0ac17a035b0041be41f1ab0b0ff31a97
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-12 12:05:23 +0900

    refactor(UsersTest): 함수화 하여 중복 제거

--- src/test/java/blackjack/domain/player/UsersTest.java
+++ src/test/java/blackjack/domain/player/UsersTest.java
@@ -1,26 +1,39 @@
 package blackjack.domain.player;
 
+import static blackjack.domain.ResultType.DRAW;
+import static blackjack.domain.ResultType.LOSS;
+import static blackjack.domain.ResultType.WIN_NOT_WITH_BLACKJACK;
+import static blackjack.domain.ResultType.WIN_WITH_BLACKJACK;
 import static blackjack.domain.player.UserTest.MAX_BETTING_MONEY_BOUND;
 import static blackjack.domain.player.UserTest.MIN_BETTING_MONEY_BOUND;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import blackjack.domain.ResultType;
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Cards;
 import blackjack.domain.card.type.CardNumberType;
 import blackjack.domain.card.type.CardShapeType;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class UsersTest {
     static final String POBI = "pobi";
     private static final String JASON = "jason";
     private static final String INBI = "inbi";
     private static final String MUNGTO = "mungto";
+
+    final List<Integer> bettingMonies = new ArrayList<>(
+        Arrays.asList(
+            MIN_BETTING_MONEY_BOUND,
+            50_000,
+            12_345_678,
+            MAX_BETTING_MONEY_BOUND
+        )
+    );
 
     @DisplayName("Users 생성 테스트")
     @Test
@@ -42,136 +55,150 @@ public class UsersTest {
     }
 
     @DisplayName("게임 결과 수익 반환 테스트 - 딜러가 블랙잭 일 때")
-    @ParameterizedTest
-    @ValueSource(ints = {MIN_BETTING_MONEY_BOUND, 50_000, 12_345_678, MAX_BETTING_MONEY_BOUND})
-    void profitsDealerBlackJack(int bettingMoney) {
-        Dealer dealer = new Dealer(); // 블랙잭
-        dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.TEN));
-        dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.ACE));
+    @Test
+    void profitsDealerBlackJack() {
+        for (int bettingMoney : bettingMonies) {
+            Dealer dealer = new Dealer(); // 블랙잭
+            dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.TEN));
+            dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.ACE));
 
-        User pobi = new User(POBI, bettingMoney); // 무승부
-        pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.TEN));
-        pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.ACE));
+            User pobi = new User(POBI, bettingMoney); // 무승부
+            pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.TEN));
+            pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.ACE));
 
-        User inbi = new User(INBI, bettingMoney); // 패
-        inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.FIVE));
-        inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SIX));
+            User inbi = new User(INBI, bettingMoney); // 패
+            inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.FIVE));
+            inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SIX));
 
-        User mungto = new User(MUNGTO, bettingMoney); // 패
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            User mungto = new User(MUNGTO, bettingMoney); // 패
+            mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
 
-        Users users = new Users(Arrays.asList(pobi, inbi, mungto));
+            Users users = new Users(Arrays.asList(pobi, inbi, mungto));
+            Map<Name, Integer> profits = users.getProfits(dealer);
 
-        Map<Name, Integer> profits = users.getProfits(dealer);
+            assertProfits(profits, Arrays.asList(POBI, INBI, MUNGTO),
+                Arrays.asList(DRAW, LOSS, LOSS), bettingMoney);
+        }
+    }
 
-        assertThat(profits.get(new Name(POBI))).isEqualTo(0);
-        assertThat(profits.get(new Name(INBI))).isEqualTo(-bettingMoney);
-        assertThat(profits.get(new Name(MUNGTO))).isEqualTo(-bettingMoney);
+    private void assertProfits(Map<Name, Integer> profits, List<String> userNames,
+        List<ResultType> result, int bettingMoney) {
+
+        for (int i = 0; i < userNames.size(); i++) {
+            int actualProfit = profits.get(new Name(userNames.get(i)));
+            if (result.get(i) == WIN_WITH_BLACKJACK) {
+                assertThat(actualProfit).isEqualTo((int) (1.5 * (double) bettingMoney));
+            }
+            if (result.get(i) == WIN_NOT_WITH_BLACKJACK) {
+                assertThat(actualProfit).isEqualTo(bettingMoney);
+            }
+            if (result.get(i) == DRAW) {
+                assertThat(actualProfit).isEqualTo(0);
+            }
+            if (result.get(i) == LOSS) {
+                assertThat(actualProfit).isEqualTo(-bettingMoney);
+            }
+        }
     }
 
     @DisplayName("게임 결과 수익 반환 테스트 - 딜러가 bust 일 때")
-    @ParameterizedTest
-    @ValueSource(ints = {MIN_BETTING_MONEY_BOUND, 50_000, 12_345_678, MAX_BETTING_MONEY_BOUND})
-    void profitsDealerBust(int bettingMoney) {
-        Dealer dealer = new Dealer(); // bust
-        dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.EIGHT));
+    @Test
+    void profitsDealerBust() {
+        for (int bettingMoney : bettingMonies) {
+            Dealer dealer = new Dealer(); // bust
+            dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.EIGHT));
 
-        User pobi = new User(POBI, bettingMoney); // 블랙잭 승
-        pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.TEN));
-        pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.ACE));
+            User pobi = new User(POBI, bettingMoney); // 블랙잭 승
+            pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.TEN));
+            pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.ACE));
 
-        User inbi = new User(INBI, bettingMoney); // 블랙잭이 아닌 승
-        inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.FIVE));
-        inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SIX));
+            User inbi = new User(INBI, bettingMoney); // 블랙잭이 아닌 승
+            inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.FIVE));
+            inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SIX));
 
-        User mungto = new User(MUNGTO, bettingMoney); // 패
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.EIGHT));
+            User mungto = new User(MUNGTO, bettingMoney); // 패
+            mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.EIGHT));
 
-        Users users = new Users(Arrays.asList(pobi, inbi, mungto));
+            Users users = new Users(Arrays.asList(pobi, inbi, mungto));
+            Map<Name, Integer> profits = users.getProfits(dealer);
 
-        Map<Name, Integer> profits = users.getProfits(dealer);
-
-        assertThat(profits.get(new Name(POBI))).isEqualTo((int) (1.5 * (double) bettingMoney));
-        assertThat(profits.get(new Name(INBI))).isEqualTo(bettingMoney);
-        assertThat(profits.get(new Name(MUNGTO))).isEqualTo(-bettingMoney);
+            assertProfits(profits, Arrays.asList(POBI, INBI, MUNGTO),
+                Arrays.asList(WIN_WITH_BLACKJACK, WIN_NOT_WITH_BLACKJACK, LOSS), bettingMoney);
+        }
     }
 
     @DisplayName("게임 결과 수익 반환 테스트 - 딜러가 블랙잭이 아닌 21 일 때")
-    @ParameterizedTest
-    @ValueSource(ints = {MIN_BETTING_MONEY_BOUND, 50_000, 12_345_678, MAX_BETTING_MONEY_BOUND})
-    void profitsDealer21NotBlackJack(int bettingMoney) {
-        Dealer dealer = new Dealer(); // 블랙잭이 아닌 21
-        dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+    @Test
+    void profitsDealer21NotBlackJack() {
+       for (int bettingMoney : bettingMonies) {
+           Dealer dealer = new Dealer(); // 블랙잭이 아닌 21
+           dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+           dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+           dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
 
-        User pobi = new User(POBI, bettingMoney); // 블랙잭 승
-        pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.TEN));
-        pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.ACE));
+           User pobi = new User(POBI, bettingMoney); // 블랙잭 승
+           pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.TEN));
+           pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.ACE));
 
-        User jason = new User(JASON, bettingMoney); // 무승부
-        jason.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        jason.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        jason.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+           User jason = new User(JASON, bettingMoney); // 무승부
+           jason.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+           jason.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+           jason.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
 
-        User inbi = new User(INBI, bettingMoney); // 패
-        inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.FIVE));
-        inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SIX));
+           User inbi = new User(INBI, bettingMoney); // 패
+           inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.FIVE));
+           inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SIX));
 
-        User mungto = new User(MUNGTO, bettingMoney); // 패
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.EIGHT));
+           User mungto = new User(MUNGTO, bettingMoney); // 패
+           mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+           mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+           mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.EIGHT));
 
-        Users users = new Users(Arrays.asList(pobi, jason, inbi, mungto));
+           Users users = new Users(Arrays.asList(pobi, jason, inbi, mungto));
+           Map<Name, Integer> profits = users.getProfits(dealer);
 
-        Map<Name, Integer> profits = users.getProfits(dealer);
-
-        assertThat(profits.get(new Name(POBI))).isEqualTo((int) (1.5 * (double) bettingMoney));
-        assertThat(profits.get(new Name(JASON))).isEqualTo(0);
-        assertThat(profits.get(new Name(INBI))).isEqualTo(-bettingMoney);
-        assertThat(profits.get(new Name(MUNGTO))).isEqualTo(-bettingMoney);
+           assertProfits(profits, Arrays.asList(POBI, JASON, INBI, MUNGTO),
+               Arrays.asList(WIN_WITH_BLACKJACK, DRAW, LOSS, LOSS), bettingMoney);
+       }
     }
 
     @DisplayName("게임 결과 수익 반환 테스트 - 딜러가 21 미만일 때")
-    @ParameterizedTest
-    @ValueSource(ints = {MIN_BETTING_MONEY_BOUND, 50_000, 12_345_678, MAX_BETTING_MONEY_BOUND})
-    void profitsDealerUnder21NotBlackJack(int bettingMoney) {
-        Dealer dealer = new Dealer(); // 14
-        dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+    @Test
+    void profitsDealerUnder21NotBlackJack() {
+        for (int bettingMoney : bettingMonies) {
+            Dealer dealer = new Dealer(); // 14
+            dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            dealer.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
 
-        User pobi = new User(POBI, bettingMoney); // 블랙잭이 아닌 승
-        pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.TEN));
-        pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.FIVE));
+            User pobi = new User(POBI, bettingMoney); // 블랙잭이 아닌 승
+            pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.TEN));
+            pobi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.FIVE));
 
-        User jason = new User(JASON, bettingMoney); // 무승부
-        jason.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        jason.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            User jason = new User(JASON, bettingMoney); // 무승부
+            jason.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            jason.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
 
-        User inbi = new User(INBI, bettingMoney); // 패
-        inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.FIVE));
-        inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SIX));
+            User inbi = new User(INBI, bettingMoney); // 패
+            inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.FIVE));
+            inbi.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SIX));
 
-        User mungto = new User(MUNGTO, bettingMoney); // 패
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
-        mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.EIGHT));
+            User mungto = new User(MUNGTO, bettingMoney); // 패
+            mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.SEVEN));
+            mungto.drawOneCard(new Card(CardShapeType.SPADE, CardNumberType.EIGHT));
 
-        Users users = new Users(Arrays.asList(pobi, jason, inbi, mungto));
+            Users users = new Users(Arrays.asList(pobi, jason, inbi, mungto));
+            Map<Name, Integer> profits = users.getProfits(dealer);
 
-        Map<Name, Integer> profits = users.getProfits(dealer);
-
-        assertThat(profits.get(new Name(POBI))).isEqualTo(bettingMoney);
-        assertThat(profits.get(new Name(JASON))).isEqualTo(0);
-        assertThat(profits.get(new Name(INBI))).isEqualTo(-bettingMoney);
-        assertThat(profits.get(new Name(MUNGTO))).isEqualTo(-bettingMoney);
+            assertProfits(profits, Arrays.asList(POBI, JASON, INBI, MUNGTO),
+                Arrays.asList(WIN_NOT_WITH_BLACKJACK, DRAW, LOSS, LOSS), bettingMoney);
+        }
     }
 
     @DisplayName("모든 유저들 랜덤카드 2개 뽑기 테스트")
