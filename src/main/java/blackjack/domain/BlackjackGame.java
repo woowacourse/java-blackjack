93caffa4eb2f7d9d321e60302158ae5ee71926b8
pjy1368 <basejin0810@naver.com>
2021-03-13 23:29:03 +0900

    refactor: 참가자의 이름과 수익을 계산하는 책임을 ParticipantResults에게 부여하도록 수정

--- src/main/java/blackjack/domain/BlackjackGame.java
+++ src/main/java/blackjack/domain/BlackjackGame.java
@@ -1,30 +1,20 @@
 package blackjack.domain;
 
-import blackjack.domain.participants.Name;
 import blackjack.domain.participants.Names;
 import blackjack.domain.participants.Participant;
 import blackjack.domain.participants.Participants;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 public class BlackjackGame {
 
     private final Participants participants;
-    private final Participant dealer;
-    private final List<Participant> players;
 
     public BlackjackGame(final Names names, final List<Double> moneys) {
         participants = new Participants(names, moneys);
-        dealer = participants.getDealer();
-        players = participants.getPlayers();
     }
 
     public BlackjackGame(final Participant dealer, final List<Participant> players) {
         participants = new Participants(dealer, players);
-        this.dealer = dealer;
-        this.players = new ArrayList<>(players);
     }
 
     public void distributeCard() {
@@ -32,23 +22,7 @@ public class BlackjackGame {
     }
 
     public ParticipantResults makeParticipantResults() {
-        final Map<Name, Integer> participantResults = new LinkedHashMap<>();
-        participantResults.put(dealer.getName(), -calculateTotalPlayersRate());
-
-        for (final Participant player : players) {
-            final Result result = player.decideWinner(dealer);
-            participantResults.put(player.getName(), (int) result.calculateRate(player.getMoney()));
-        }
-        return new ParticipantResults(participantResults);
-    }
-
-    public int calculateTotalPlayersRate() {
-        double total = 0;
-        for (final Participant player : players) {
-            final Result result = player.decideWinner(dealer);
-            total += result.calculateRate(player.getMoney());
-        }
-        return (int) total;
+        return new ParticipantResults(participants.getDealer(), participants.getPlayers());
     }
 
     public Participants getParticipants() {
