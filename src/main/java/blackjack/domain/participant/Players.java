9edececefcd79745c8c014f77087c0d901224c75
TaewanKimmmm <ktww88@gmail.com>
2021-03-15 14:00:01 +0900

    feat: 플레이어 이름 중복에 대한 예외처리 기능 추가

--- src/main/java/blackjack/domain/participant/Players.java
+++ src/main/java/blackjack/domain/participant/Players.java
@@ -9,12 +9,14 @@ import java.util.List;
 public class Players {
     private static final int MIN_PLAYERS_NUMBER = 1;
     private static final String PLAYER_COUNT_ERROR_MESSAGE = "플레이어 수는 " + MIN_PLAYERS_NUMBER + "명 이상이어야 합니다.";
+    public static final String PLAYER_NAME_DUPLICATION_ERROR_MESSAGE = "플레이어 이름은 중복되지 않아야 합니다.";
 
     private final List<Player> players;
 
     public Players(List<Player> players) {
         this.players = new ArrayList<>(players);
         validateNumberOfPlayer(players);
+        validateDuplicationOfName(players);
     }
 
     private void validateNumberOfPlayer(List<Player> players) {
@@ -23,6 +25,15 @@ public class Players {
         }
     }
 
+    private void validateDuplicationOfName(List<Player> players) {
+        int distinctCount = (int) players.stream()
+                .distinct()
+                .count();
+
+        if (players.size() != distinctCount) {
+            throw new IllegalArgumentException(PLAYER_NAME_DUPLICATION_ERROR_MESSAGE);
+        }
+    }
     public void drawAtFirst(Deck deck) {
         players.forEach(player -> player.drawAtFirst(deck));
     }
