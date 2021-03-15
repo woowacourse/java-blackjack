c47fc617cc7c1d17e748b2de873e29362b9d4eb6
Kimun Kim <kkm97351@gmail.com>
2021-03-15 16:54:32 +0900

    style: Results 클래스 내부 변수명 변경
    - winOrLostMap -> matchResults
    - results -> matchResults

--- src/main/java/blackjack/domain/result/Results.java
+++ src/main/java/blackjack/domain/result/Results.java
@@ -17,24 +17,24 @@ public class Results {
     private static final long TO_NEGATIVE_VALUE = -1;
     private static final int INITIALIZE_VALUE = 0;
 
-    private final Map<Player, ResultType> results;
+    private final Map<Player, ResultType> matchResults;
     private final Map<Player, Money> profits;
 
-    public Results(Map<Player, ResultType> results, Map<Player, Money> profits) {
-        this.results = results;
+    public Results(Map<Player, ResultType> matchResults, Map<Player, Money> profits) {
+        this.matchResults = matchResults;
         this.profits = profits;
     }
 
-    public static Results of(Map<Player, ResultType> results) {
-        Map<Player, Money> profitMap = results.keySet().stream().parallel()
+    public static Results of(Map<Player, ResultType> matchResults) {
+        Map<Player, Money> profitMap = matchResults.keySet().stream().parallel()
                 .collect(toMap(
                         player -> player,
-                        player -> ProfitTable.translateBettingMoney(results.get(player), player.getBettingMoney())));
-        return new Results(results, profitMap);
+                        player -> ProfitTable.translateBettingMoney(matchResults.get(player), player.getBettingMoney())));
+        return new Results(matchResults, profitMap);
     }
 
     public ResultType getResultOf(Player player) {
-        return results.get(player);
+        return matchResults.get(player);
     }
 
     public Money getEarningMoneyOf(Player player) {
@@ -42,16 +42,16 @@ public class Results {
     }
 
     public DealerResult generateDealerResult() {
-        HashMap<ResultType, Integer> dealerWinOrLoseMap = new HashMap<>();
+        HashMap<ResultType, Integer> dealerMatchResult = new HashMap<>();
         Arrays.stream(ResultType.values())
-                .forEach(resultType -> dealerWinOrLoseMap.put(resultType, INITIALIZE_VALUE));
+                .forEach(resultType -> dealerMatchResult.put(resultType, INITIALIZE_VALUE));
         Arrays.stream(ResultType.values())
                 .forEach(resultType -> {
-                    dealerWinOrLoseMap.put(
+                    dealerMatchResult.put(
                             resultType.reverse(),
-                            dealerWinOrLoseMap.get(resultType.reverse()) + this.count(resultType));
+                            dealerMatchResult.get(resultType.reverse()) + this.count(resultType));
                 });
-        return new DealerResult(dealerWinOrLoseMap, this.getTotalProfit() * TO_NEGATIVE_VALUE);
+        return new DealerResult(dealerMatchResult, this.getTotalProfit() * TO_NEGATIVE_VALUE);
     }
 
     private long getTotalProfit() {
@@ -61,13 +61,13 @@ public class Results {
     }
 
     private int count(ResultType resultType) {
-        return (int) this.results.values().stream().parallel()
+        return (int) this.matchResults.values().stream().parallel()
                 .filter(resultType::equals)
                 .count();
     }
 
     public Set<Player> playerSet() {
-        return results.keySet();
+        return matchResults.keySet();
     }
 
     @Override
@@ -75,11 +75,11 @@ public class Results {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Results results1 = (Results) o;
-        return Objects.equals(results, results1.results) && Objects.equals(profits, results1.profits);
+        return Objects.equals(matchResults, results1.matchResults) && Objects.equals(profits, results1.profits);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(results, profits);
+        return Objects.hash(matchResults, profits);
     }
 }
