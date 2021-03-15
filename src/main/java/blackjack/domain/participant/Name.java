2c90a00ffc51fc5c059cc52da593d03a75027063
TaewanKimmmm <ktww88@gmail.com>
2021-03-15 13:37:02 +0900

    refactor: VO인 Name에서 euqlas와 hashcode 오버라이딩

--- src/main/java/blackjack/domain/participant/Name.java
+++ src/main/java/blackjack/domain/participant/Name.java
@@ -1,5 +1,7 @@
 package blackjack.domain.participant;
 
+import java.util.Objects;
+
 public class Name {
     public static final int MINIMUM_NAME_LENGTH = 1;
     private static final String NAME_INPUT_ERROR_MESSAGE = "이름은 " + MINIMUM_NAME_LENGTH + "자 이상이어야 합니다.";
@@ -20,5 +22,18 @@ public class Name {
 
     public String getName() {
         return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
