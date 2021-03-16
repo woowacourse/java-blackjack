b18838dfe87d22f5c3719642a9c6cf15f4ecae36
Seed <hsik0225@gmail.com>
2021-03-16 22:34:56 +0900

    refactor(betting-money):유효성 검사 메소드 분리

--- src/main/java/blackjack/domain/participant/BettingMoney.java
+++ src/main/java/blackjack/domain/participant/BettingMoney.java
@@ -11,11 +11,14 @@ public class BettingMoney {
     }
     
     public static BettingMoney from(String input) {
+        checkDigit(input);
+        return new BettingMoney(Double.parseDouble(input));
+    }
+    
+    private static void checkDigit(String input) {
         if (!isDigit(input)) {
             throw new IllegalArgumentException(ERROR_IS_NOT_DIGIT);
         }
-        
-        return new BettingMoney(Double.parseDouble(input));
     }
     
     private static boolean isDigit(String input) {
