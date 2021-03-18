e296b5f5a74103ef7a12f25bf429cc9eb0ab798c
Solji Choi <bssj9307@gmail.com>
2021-03-19 01:57:52 +0900

    refactor: 버스트일 때 승패결과 수정

--- src/main/java/blackjack/domain/result/Result.java
+++ src/main/java/blackjack/domain/result/Result.java
@@ -1,18 +1,19 @@
 package blackjack.domain.result;
 
-import blackjack.domain.user.User;
+import blackjack.domain.user.Dealer;
+import blackjack.domain.user.Player;
 
 public class Result {
 
     private Result() {
     }
 
-    public static double compareScoreResult(User targetUser, User compareUser) {
-        int compareResult = targetUser.compare(compareUser);
-        if (compareResult == 0) {
+    public static double compareScoreResult(Player player, Dealer dealer) {
+        int compareResult = player.compare(dealer);
+        if (!player.isBust() && compareResult == 0) {
             return BlackjackResult.TIE.getEarningRate();
         }
-        if (compareResult > 0) {
+        if (dealer.isBust() || compareResult > 0) {
             return BlackjackResult.WIN.getEarningRate();
         }
         return BlackjackResult.LOSE.getEarningRate();
