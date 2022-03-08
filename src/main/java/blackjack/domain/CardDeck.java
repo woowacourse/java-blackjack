3fc176bd5c4647ebc72dbdae51af66891bd6bf6f
Chris <bjuuuu98@gmail.com>
2022-03-08 17:51:04 +0900

    refactor: 남은 카드 개수 체크 메소드 분리

--- src/main/java/blackjack/domain/CardDeck.java
+++ src/main/java/blackjack/domain/CardDeck.java
@@ -15,16 +15,24 @@ public class CardDeck {
     }
 
     public List<Card> provideInitCards() {
-        if (cards.size() < INIT_PROVIDING_CARD_SIZE) {
-            throw new IllegalStateException("[Error] 남은 카드가 2장 미만입니다.");
-        }
+        validateEnoughDeckSize();
         return Arrays.asList(cards.poll(), cards.poll());
     }
 
+    private void validateEnoughDeckSize() {
+        if (cards.size() < INIT_PROVIDING_CARD_SIZE) {
+            throw new IllegalStateException("[Error] 남은 카드가 2장 미만입니다.");
+        }
+    }
+
     public Card provideCard() {
+        validateEmptyDeck();
+        return cards.poll();
+    }
+
+    private void validateEmptyDeck() {
         if (cards.isEmpty()) {
             throw new IllegalStateException("[Error] 남은 카드가 없습니다.");
         }
-        return cards.poll();
     }
 }
