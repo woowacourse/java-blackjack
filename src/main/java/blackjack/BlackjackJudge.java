dc71e27aaef3a76d4fd15e82b1d8024703e6d249
wilgur513 <wilgur513@naver.com>
2022-03-08 17:43:02 +0900

    feat: 버스트 발생시 승패 결정 로직

--- src/main/java/blackjack/BlackjackJudge.java
+++ src/main/java/blackjack/BlackjackJudge.java
@@ -2,17 +2,38 @@ package blackjack;
 
 public class BlackjackJudge {
 
-    public static Result judge(Cards player1, Cards player2) {
-        if (player1.isBust()) {
+    public static Result judge(Cards cards1, Cards cards2) {
+        if (hasBust(cards1, cards2)) {
+            return judgeBustCase(cards1, cards2);
+        }
+        return judgeNormalCase(cards1, cards2);
+    }
+
+    private static boolean hasBust(Cards cards1, Cards cards2) {
+        return cards1.isBust() || cards2.isBust();
+    }
+
+    private static Result judgeBustCase(Cards cards1, Cards cards2) {
+        if (cards1.isBust() && cards2.isBust()) {
+            return Result.DRAW;
+        }
+
+        if (cards1.isBust()) {
             return Result.LOSS;
         }
 
-        if (player1.score() > player2.score()) {
+        return Result.WIN;
+    }
+
+    private static Result judgeNormalCase(Cards cards1, Cards cards2) {
+        if (cards1.score() > cards2.score()) {
             return Result.WIN;
         }
-        if (player1.score() == player2.score()) {
+
+        if (cards1.score() == cards2.score()) {
             return Result.DRAW;
         }
+
         return Result.LOSS;
     }
 }
