7bec785c14f7090c46e1587e4e0a2ac79b03b641
Chris <bjuuuu98@gmail.com>
2022-03-08 16:38:18 +0900

    style: 개행 추가

--- src/test/java/blackjack/domain/CardNumberTest.java
+++ src/test/java/blackjack/domain/CardNumberTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CardNumberTest {
+
     @Test
     @DisplayName("카드 숫자 13개를 반환할 수 있다.")
     void cardNumbers() {
