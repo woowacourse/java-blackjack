b31236a16d3f4aee72efeb2dd0213b5e60e60daa
이한울 <dkfghg3@naver.com>
2022-03-08 20:46:41 +0900

    feat: 보고서 생성 기능 구현

--- src/main/java/rentcompany/RentCompany.java
+++ src/main/java/rentcompany/RentCompany.java
@@ -6,6 +6,10 @@ import java.util.Objects;
 
 public class RentCompany {
 
+    public static final String NEWLINE = System.getProperty("line.separator");
+    public static final String UNIT_OF_MEASUREMENT = "리터";
+    private static final String COLON = " : ";
+
     private List<Car> cars;
 
     public RentCompany() {
@@ -15,6 +19,16 @@ public class RentCompany {
     public void addCar(Car car) {
         Objects.requireNonNull(car);
         cars.add(car);
+    }
+
+    public String generateReport() {
+        StringBuilder sb = new StringBuilder();
+        cars.forEach(car -> sb.append(car.getName())
+                .append(COLON)
+                .append((int) car.getChargeQuantity())
+                .append(UNIT_OF_MEASUREMENT)
+                .append(NEWLINE));
+        return sb.toString();
     }
 
     public int getCountOfCar() {
--- src/test/java/rentcompany/RentCompanyTest.java
+++ src/test/java/rentcompany/RentCompanyTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 
 public class RentCompanyTest {
 
+    private final RentCompany rentCompany = new RentCompany();
+
     @Test
     @DisplayName("렌트 회사 객체를 정상적으로 생성한다.")
     void createRentCompany() {
@@ -19,22 +21,40 @@ public class RentCompanyTest {
     @Test
     @DisplayName("렌트 회사에 차량을 추가한다.")
     void addCar() {
-        RentCompany rentCompany = new RentCompany();
-
         rentCompany.addCar(new Sonata(100));
         rentCompany.addCar(new Avante(150));
         rentCompany.addCar(new K5(130));
+        final int expected = 3;
 
-        int actual = rentCompany.getCountOfCar();
-        assertThat(actual).isEqualTo(3);
+        final int actual = rentCompany.getCountOfCar();
+
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     @DisplayName("렌트 회사에서 null을 추가하면 NullPointerException을 발생시킨다.")
     void checkAddCarException() {
-        RentCompany rentCompany = new RentCompany();
-
         assertThatThrownBy(() -> rentCompany.addCar(null))
                 .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    @DisplayName("렌트 회사는 주입할 연료량 보고서를 생성한다.")
+    void generateReport() {
+        final String expected = "SONATA : 15리터" + RentCompany.NEWLINE +
+                "K5 : 20리터" + RentCompany.NEWLINE +
+                "SONATA : 12리터" + RentCompany.NEWLINE +
+                "AVANTE : 20리터" + RentCompany.NEWLINE +
+                "K5 : 30리터" + RentCompany.NEWLINE;
+
+        rentCompany.addCar(new Sonata(150));
+        rentCompany.addCar(new K5(260));
+        rentCompany.addCar(new Sonata(120));
+        rentCompany.addCar(new Avante(300));
+        rentCompany.addCar(new K5(390));
+
+        final String report = rentCompany.generateReport();
+
+        assertThat(report).isEqualTo(expected);
     }
 }
