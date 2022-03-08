2c0772eed9c40a93affd70dc4582d858cf53e3b1
wilgur513 <wilgur513@naver.com>
2022-03-08 16:42:53 +0900

    test: 버스트 발생 테스트

--- src/test/java/blackjack/CardsTest.java
+++ src/test/java/blackjack/CardsTest.java
@@ -2,7 +2,12 @@ package blackjack;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class CardsTest {
 
@@ -10,6 +15,20 @@ public class CardsTest {
     void 카드_합_계산() {
         Cards cards = new Cards("1다이아몬드", "2다이아몬드");
         assertThat(cards.score()).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name = "입력값 : {1}")
+    @MethodSource("provideBustFixture")
+    void 버스트_발생(boolean expect, List<String> values) {
+        Cards cards = new Cards(values.toArray(String[]::new));
+        assertThat(cards.isBust()).isEqualTo(expect);
+    }
+
+    protected static Stream<Arguments> provideBustFixture() {
+        return Stream.of(
+            Arguments.of(true, List.of("J다이아몬드","Q다이아몬드","2클로버")),
+            Arguments.of(false, List.of("A다이아몬드", "J다이아몬드", "4하트"))
+        );
     }
 
 }
