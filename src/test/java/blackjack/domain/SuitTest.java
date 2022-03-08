9a2afc408f4af08700394426492e5d1ed5ecc552
skullKim <kyk990328@gmail.com>
2022-03-08 20:53:19 +0900

    feat(Suit.java): 카드 문양 Enum 추가
    카드 한장에 대한 정보 중 카드 문양에 대한 정보를 나타내기 위한 Enum 추가

---
+++ src/test/java/blackjack/domain/SuitTest.java
@@ -0,0 +1,21 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@DisplayName("Suit 테스트")
+class SuitTest {
+
+	@DisplayName("카드 심볼 이름이 제대로 반환되는지 확인")
+	@ParameterizedTest(name = "{index} {displayName} symbol={0} symbolName={1}")
+	@CsvSource(value = {"HEART, 하트", "CLOVER, 클로버", "SPADE, 스페이드", "DIAMOND, 다이아몬드"})
+	void check_Symbol_Name(final Suit symbol, final String expectedName) {
+		final String actualName = symbol.getName();
+
+		assertThat(actualName).isEqualTo(expectedName);
+	}
+
+}
