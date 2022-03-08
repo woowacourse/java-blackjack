74040fae90de6e9df0fc80bf205e228bfaf24780
sun <syoun602@gmail.com>
2022-03-08 20:28:38 +0900

    feat: 중복된 카드 생성에 대한 예외 처리

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -1,5 +1,7 @@
 package blackjack.domain;
 
+import java.util.Objects;
+
 public class Card {
 
     private final CardPattern pattern;
@@ -10,4 +12,16 @@ public class Card {
         this.number = number;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return number == card.number && pattern == card.pattern;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pattern, number);
+    }
 }
