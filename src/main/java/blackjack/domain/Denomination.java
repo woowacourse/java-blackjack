dea7b7959868a59cb483e54040ab010ff8ccbc49
jayjaehunchoi <wogns0108@naver.com>
2022-03-08 15:35:09 +0900

    feat : Denomination 포인트 반환 기능 추가

--- src/main/java/blackjack/domain/Denomination.java
+++ src/main/java/blackjack/domain/Denomination.java
@@ -2,18 +2,28 @@ package blackjack.domain;
 
 public enum Denomination {
 
-    ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    KING,
-    QUEEN,
-    JACK,
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    KING(10),
+    QUEEN(10),
+    JACK(10),
+    ;
 
+    private final int point;
+
+    Denomination(int point) {
+        this.point = point;
+    }
+
+    public int getPoint() {
+        return point;
+    }
 }
