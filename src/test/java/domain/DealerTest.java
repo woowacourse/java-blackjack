1f422cf3995cc1a2935f2fd169e29a862bdfec37
shindong96 <2006peter@naver.com>
2022-03-08 21:28:26 +0900

    refactor: BeforeEach사용으로 코드 단순화

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -5,27 +5,33 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DealerTest {
+	Card card1;
+	Card card2;
+
+	@BeforeEach
+	void setup() {
+		card1 = new Card(Rank.RANK_J, Suit.SPADE);
+		card2 = new Card(Rank.RANK_A, Suit.CLOVER);
+	}
+
 	@Test
 	@DisplayName("딜러가 손패를 더 받아야 하는 경우")
 	void isEnoughCard_False() {
-		Card card1 = new Card(Rank.RANK_5, Suit.HEART);
-		Card card2 = new Card(Rank.RANK_J, Suit.SPADE);
-		Card card3 = new Card(Rank.RANK_A, Suit.CLOVER);
-		Dealer dealer = new Dealer(new ArrayList<>(List.of(card1, card2, card3)));
+		Card card = new Card(Rank.RANK_5, Suit.HEART);
+		Dealer dealer = new Dealer(new ArrayList<>(List.of(card, card1, card2)));
 		assertThat(dealer.isEnoughCard()).isFalse();
 	}
 
 	@Test
 	@DisplayName("딜러가 손패를 더 안 받아야 하는 경우")
 	void isEnoughCard_True() {
-		Card card1 = new Card(Rank.RANK_6, Suit.HEART);
-		Card card2 = new Card(Rank.RANK_J, Suit.SPADE);
-		Card card3 = new Card(Rank.RANK_A, Suit.CLOVER);
-		Dealer dealer = new Dealer(new ArrayList<>(List.of(card1, card2, card3)));
+		Card card = new Card(Rank.RANK_6, Suit.HEART);
+		Dealer dealer = new Dealer(new ArrayList<>(List.of(card, card1, card2)));
 		assertThat(dealer.isEnoughCard()).isTrue();
 	}
 }
--- src/test/java/domain/ParticipantTest.java
+++ src/test/java/domain/ParticipantTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,6 +15,19 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ParticipantTest {
+
+	Card card1;
+	Card card2;
+	Card card3;
+	Card card4;
+
+	@BeforeEach
+	void setUp() {
+		card1 = new Card(Rank.RANK_K, Suit.HEART);
+		card2 = new Card(Rank.RANK_A, Suit.SPADE);
+		card3 = new Card(Rank.RANK_A, Suit.CLOVER);
+		card4 = new Card(Rank.RANK_8, Suit.CLOVER);
+	}
 
 	@ParameterizedTest(name = "이름이 공백이거나 빈칸일 경우")
 	@NullAndEmptySource
@@ -27,30 +41,23 @@ public class ParticipantTest {
 	@Test
 	@DisplayName("자신의 손패를 문자열로 반환하는 기능")
 	void showHand() {
-		Card card1 = new Card(Rank.RANK_A, Suit.HEART);
-		Card card2 = new Card(Rank.RANK_9, Suit.SPADE);
 		Participant participant = new Participant("pobi", new ArrayList<>(List.of(card1, card2)));
-		assertThat(participant.showHand()).isEqualTo("pobi카드: A하트, 9스페이드");
+		assertThat(participant.showHand()).isEqualTo("pobi카드: K하트, A스페이드");
 	}
 
 	@Test
 	@DisplayName("손패에 새로운 카드를 추가하는 기능")
 	void addCard() {
-		Card card1 = new Card(Rank.RANK_A, Suit.HEART);
-		Card card2 = new Card(Rank.RANK_9, Suit.SPADE);
-		Card card3 = new Card(Rank.RANK_8, Suit.CLOVER);
 		Participant participant = new Participant("pobi", new ArrayList<>(List.of(card1, card2)));
-		participant.addCard(card3);
-		assertThat(participant.showHand()).isEqualTo("pobi카드: A하트, 9스페이드, 8클로버");
+		participant.addCard(card4);
+		assertThat(participant.showHand()).isEqualTo("pobi카드: K하트, A스페이드, 8클로버");
 	}
 
 	@ParameterizedTest(name = "손패가 버스트 되었는지 확인하는 기능 - case : {0}")
 	@EnumSource(mode = EnumSource.Mode.EXCLUDE, names = {"RANK_A"})
 	void isBurst(Rank rank) {
-		Card card1 = new Card(rank, Suit.HEART);
-		Card card2 = new Card(Rank.RANK_J, Suit.SPADE);
-		Card card3 = new Card(Rank.RANK_K, Suit.CLOVER);
-		Participant participant = new Participant("pobi", new ArrayList<>(List.of(card1, card2, card3)));
+		Card card = new Card(rank, Suit.HEART);
+		Participant participant = new Participant("pobi", new ArrayList<>(List.of(card, card1, card1)));
 		assertThat(participant.isBurst()).isTrue();
 	}
 
@@ -67,11 +74,6 @@ public class ParticipantTest {
 	@Test()
 	@DisplayName("베스트 스코어 계산하는 기능")
 	void getBestScore() {
-		Card card1 = new Card(Rank.RANK_K, Suit.HEART);
-		Card card2 = new Card(Rank.RANK_A, Suit.SPADE);
-		Card card3 = new Card(Rank.RANK_A, Suit.CLOVER);
-		Card card4 = new Card(Rank.RANK_8, Suit.CLOVER);
-
 		Participant participant = new Participant("pobi", new ArrayList<>(List.of(card1, card2, card3, card4)));
 		assertThat(participant.getBestScore()).isEqualTo(20);
 	}
