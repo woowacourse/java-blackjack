d881584a775bf62f0a269d9b3ed816d46767aaec
YuJeong <yjfflq4872@gmail.com>
2022-03-08 16:08:37 +0900

    feat: 버스트 처리

--- src/main/java/blackjack/BlackJack.java
+++ src/main/java/blackjack/BlackJack.java
@@ -7,7 +7,14 @@ public class BlackJack {
     public static boolean play(List<String> dealerCards, List<String> playerCards) {
         int dealerScore = totalScore(dealerCards);
         int playerScore = totalScore(playerCards);
+        if (isBust(dealerScore)) {
+            return false;
+        }
         return dealerScore > playerScore;
+    }
+
+    private static boolean isBust(int dealerScore) {
+        return dealerScore > 21;
     }
 
     private static int totalScore(List<String> dealerCards) {
@@ -22,6 +29,9 @@ public class BlackJack {
     private static int number(String value) {
         if (value.equals("J") || value.equals("Q") || value.equals("K")) {
             return 10;
+        }
+        if (value.equals(("A"))) {
+            return 11;
         }
         return Integer.parseInt(value);
     }
--- src/test/java/blackjack/BlackJackTest.java
+++ src/test/java/blackjack/BlackJackTest.java
@@ -3,7 +3,6 @@ package blackjack;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BlackJackTest {
@@ -31,4 +30,15 @@ public class BlackJackTest {
         assertThat(dealerWin).isTrue();
     }
 
+    @Test
+    void 버스트가_발생하는_경우_테스트() {
+        boolean dealerWin = BlackJack.play(List.of("Q클로버", "J하트", "K클로버"), List.of("A다이아몬드", "2하트"));
+        assertThat(dealerWin).isFalse();
+    }
+
+    @Test
+    void ACE를_1로_처리해야_이기는_경우_테스트() {
+        boolean dealerWin = BlackJack.play(List.of("Q클로버", "J하트"), List.of("A다이아몬드", "J하트", "Q클로버"));
+        assertThat(dealerWin).isFalse();
+    }
 }
