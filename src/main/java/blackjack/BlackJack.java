cfba614880fac75943ba23b709f4d3298c11bf0a
wilgur513 <wilgur513@naver.com>
2022-03-08 16:19:40 +0900

    feat: ACE를 유리한 값으로 사용 기능 추가

--- src/main/java/blackjack/BlackJack.java
+++ src/main/java/blackjack/BlackJack.java
@@ -17,21 +17,32 @@ public class BlackJack {
         return dealerScore > 21;
     }
 
-    private static int totalScore(List<String> dealerCards) {
+    private static int totalScore(List<String> value) {
+        int score1 = totalScore(value, 1);
+        int score2 = totalScore(value, 11);
+
+        if (isBust(score2)) {
+            return score1;
+        }
+
+        return score2;
+    }
+
+    private static int totalScore(List<String> dealerCards, int aceValue) {
         int score = 0;
         for (String s : dealerCards) {
             String value = s.substring(0, 1);
-            score += number(value);
+            score += number(value, aceValue);
         }
         return score;
     }
 
-    private static int number(String value) {
+    private static int number(String value, int aceValue) {
         if (value.equals("J") || value.equals("Q") || value.equals("K")) {
             return 10;
         }
-        if (value.equals(("A"))) {
-            return 11;
+        if (value.equals("A")) {
+            return aceValue;
         }
         return Integer.parseInt(value);
     }
