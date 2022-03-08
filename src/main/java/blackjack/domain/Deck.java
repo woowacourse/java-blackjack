8a4bbc4a3fbdd2cfeda309dcbe4e770a90e46064
soominsohn <soominsohn@gmail.com>
2022-03-08 17:58:14 +0900

    feat(Deck): 카드 덱 구현

---
+++ src/main/java/blackjack/domain/Deck.java
@@ -0,0 +1,32 @@
+package blackjack.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+public class Deck {
+
+    private static final List<Card> cards = new ArrayList<>();
+
+    {
+        for (Suit suit : Suit.values()) {
+            Stream.of(Denomination.values())
+                .forEach(denomination -> cards.add(new Card(suit, denomination)));
+        }
+
+        Collections.shuffle(cards);
+    }
+
+    public Card pickCard() {
+        if (cards.size() == 0) {
+            throw new IllegalArgumentException("[ERROR] 카드를 모두 사용하였습니다.");
+        }
+        return cards.remove(0);
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+}
---
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -0,0 +1,27 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DeckTest {
+
+    @Test
+    @DisplayName("Deck의 card 수는 52장 이어야 한다.")
+    void createDeck() {
+        Deck deck = new Deck();
+
+        assertThat(deck.getCards().size()).isEqualTo(52);
+    }
+
+    @Test
+    @DisplayName("Deck에서 카드를 한장 빼서 반환한다.")
+    void pickCardFromDeck() {
+        Deck deck = new Deck();
+
+        Card card = deck.pickCard();
+        assertThat(deck.getCards().size()).isEqualTo(51);
+    }
+
+}
