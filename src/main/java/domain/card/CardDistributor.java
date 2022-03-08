aab3cc1ae9c37dfe94a75f2cc3c2033ceb298e99
kimchan <gmelddl514@gmail.com>
2022-03-08 16:05:33 +0900

    feat : 카드 분배 기능

--- src/main/java/domain/card/CardDistributor.java
+++ src/main/java/domain/card/CardDistributor.java
@@ -1,2 +1,24 @@
-package domain.card;public class CardDistributor {
+package domain.card;
+
+import java.util.Collections;
+import java.util.Stack;
+
+public class CardDistributor {
+    private static final Stack<Card> deck = new Stack<>();
+
+    static {
+        for (Suit suit : Suit.values()) {
+            for (Denomination denomination : Denomination.values()) {
+                deck.push(new Card(denomination, suit));
+            }
+        }
+        Collections.shuffle(deck);
+    }
+
+    public Card distribute() {
+        if (deck == null || deck.empty()) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+        return deck.pop();
+    }
 }
--- src/test/java/domain/card/CardDistributorTest.java
+++ src/test/java/domain/card/CardDistributorTest.java
@@ -1,4 +1,4 @@
-package domain;
+package domain.card;
 
 import domain.card.CardDistributor;
 import org.junit.jupiter.api.Test;
