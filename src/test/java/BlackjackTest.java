fb606da3f9e4d8333bea7632e1ecb3784875e5f9
pup-paw <wldusdl0310@gmail.com>
2022-03-08 15:58:07 +0900

    test: 참가자의 이름 입력에 대한 예외 처리

---
+++ src/test/java/BlackjackTest.java
@@ -0,0 +1,21 @@
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BlackjackTest {
+
+	@Test
+	@DisplayName("참가자의 수가 8명을 초과하면 예외처리")
+	void Player_Number_Exceed_Exception() {
+		assertThatThrownBy(() -> {
+			List<String> players = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i");
+			new Blackjack(players);
+		}).isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("[ERROR] 참가자의 수는 8명을 초과할 수 없습니다.");
+	}
+}
