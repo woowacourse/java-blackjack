2818b1ee50bc7f897cbeb0d9f4216960b0b8546e
jochungbeom <cndqjacndqja@gmail.com>
2022-03-08 20:40:56 +0900

    refactor: CardManager 인터페이스 -> Gamer.class로 변경 및 Dealer 삭제

--- src/main/java/blackjack/domain/Gamer.java
+++ src/main/java/blackjack/domain/Gamer.java
@@ -5,34 +5,31 @@ import java.util.List;
 
 import blackjack.domain.card.Card;
 
-public class Dealer implements CardManager {
+public class Gamer {
 
 	public static final int ZERO_POINT = 0;
+
 	private final List<Card> cards;
 
-	public Dealer() {
+	public Gamer() {
 		cards = new ArrayList<>();
 	}
 
-	@Override
 	public void addCard(Card card) {
 		cards.add(card);
 	}
 
-	@Override
 	public List<Card> getCards() {
 		return new ArrayList<>(cards);
 	}
 
-	@Override
 	public boolean isOverThan(int number) {
 		return getCardsNumberSum() > number;
 	}
 
-	@Override
 	public int getCardsNumberSum() {
 		return cards.stream()
-			.map(Card::getNumber)
-			.reduce(ZERO_POINT, Integer::sum);
+				.map(Card::getNumber)
+				.reduce(ZERO_POINT, Integer::sum);
 	}
 }
--- src/test/java/blackjack/domain/GamerTest.java
+++ src/test/java/blackjack/domain/GamerTest.java
@@ -2,7 +2,6 @@ package blackjack.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,17 +14,17 @@ import blackjack.domain.card.CardFactory;
 import blackjack.domain.card.CardNumber;
 import blackjack.domain.card.CardShape;
 
-class DealerTest {
+class GamerTest {
 
 	@Test
 	@DisplayName("딜러가 카드를 추가한다.")
 	void addCard() {
-		Dealer dealer = new Dealer();
+		Gamer gamer = new Gamer();
 		CardFactory cardFactory = new CardFactory(Card.getCards());
 		Card card = cardFactory.draw();
 
-		dealer.addCard(card);
-		List<Card> cards = dealer.getCards();
+		gamer.addCard(card);
+		List<Card> cards = gamer.getCards();
 
 		assertThat(cards.size()).isEqualTo(1);
 	}
@@ -34,27 +33,27 @@ class DealerTest {
 	@CsvSource(value = {"14:true", "16:false"}, delimiter = ':')
 	@DisplayName("보유 카드 번호 합이 특정 숫자를 넘었는지 확인")
 	void checkCardsNumberSum(int input, boolean result) {
-		Dealer dealer = new Dealer();
+		Gamer gamer = new Gamer();
 		Card card1 = Card.getInstance(CardShape.SPADE, CardNumber.TEN);
 		Card card2 = Card.getInstance(CardShape.SPADE, CardNumber.FIVE);
 
-		dealer.addCard(card1);
-		dealer.addCard(card2);
+		gamer.addCard(card1);
+		gamer.addCard(card2);
 
-		assertThat(dealer.isOverThan(input)).isEqualTo(result);
+		assertThat(gamer.isOverThan(input)).isEqualTo(result);
 	}
 
 	@Test
 	@DisplayName("보유 카드 번호 합 반환")
 	void calculateCardsNumberSum() {
-		Dealer dealer = new Dealer();
+		Gamer gamer = new Gamer();
 		Card card1 = Card.getInstance(CardShape.SPADE, CardNumber.TEN);
 		Card card2 = Card.getInstance(CardShape.SPADE, CardNumber.FIVE);
 
-		dealer.addCard(card1);
-		dealer.addCard(card2);
+		gamer.addCard(card1);
+		gamer.addCard(card2);
 
-		int sum = dealer.getCardsNumberSum();
+		int sum = gamer.getCardsNumberSum();
 		assertThat(sum).isEqualTo(15);
 	}
 }
