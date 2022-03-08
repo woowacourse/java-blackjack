682619b74d7db74e4a9e43a7abf4a058ffe2af84
= <rerub0831@gmail.com>
2022-03-08 14:43:48 +0900

    feat(RentCompany): 클래스 생성 및 구조 구현

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 ## 우아한테크코스 코드리뷰
 
 - [온라인 코드 리뷰 과정](https://github.com/woowacourse/woowacourse-docs/blob/master/maincourse/README.md)
+
+## 연료 주입
+
+- [x] 회사를 생성한다
+    - [ ] 차량 별 상태를 반환한다
+- [ ] 차를 생성한다
+    - [ ] 차가 이동할 거리를 인스턴스로 받는다
+    - [ ] 차종의 이름, 여행하려는 거리, 연비를 저장한다
+    - [ ] 주입해야 할 연료량을 구한다
---
+++ src/main/java/rentCompany/RentCompany.java
@@ -0,0 +1,20 @@
+package rentCompany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RentCompany {
+    private final List<Car> cars;
+
+    private RentCompany() {
+        this.cars = new ArrayList<>();
+    }
+
+    public static RentCompany create() {
+        return new RentCompany();
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+}
