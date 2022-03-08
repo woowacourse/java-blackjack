a0b1c447da0a8af8b4f89dd706454b177fe562c8
is2js <tingstyle1@gmail.com>
2022-03-08 17:35:19 +0900

    feat: 카드덱 생성 기능 구현

---
+++ src/main/java/blackjack.domain/CardDeck.java
@@ -0,0 +1,28 @@
+package blackjack.domain;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class CardDeck {
+    private final Stack<PlayingCard> playingCards;
+
+    private CardDeck() {
+        Stack<PlayingCard> playingCards = Arrays.stream(Suit.values())
+            .flatMap(suit -> Arrays.stream(Denomination.values())
+                .map(denomination -> PlayingCard.of(suit, denomination)))
+            .collect(Collectors.toCollection(Stack::new));
+
+        Collections.shuffle(playingCards);
+        this.playingCards = playingCards;
+    }
+
+    public static CardDeck getInstance() {
+        return new CardDeck();
+    }
+
+    public PlayingCard getCard() {
+        return playingCards.pop();
+    }
+}
---
+++ src/test/java/blackjack/domain/CardDeckTest.java
@@ -0,0 +1,17 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CardDeckTest {
+
+    @Test
+    @DisplayName("카드덱 생성 테스트")
+    void deck_create() {
+        assertThatCode(
+            CardDeck::getInstance
+        ).doesNotThrowAnyException();
+    }
+}
