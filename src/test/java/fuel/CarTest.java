e71798be35b7593cbf75bc5020be819e9390ba80
devHudi <devhudi@gmail.com>
2022-03-08 17:02:00 +0900

    feat(Car): K5, Avante 구현체 구현
    - 테스트 코드 작성

--- src/test/java/fuel/CarTest.java
+++ src/test/java/fuel/CarTest.java
@@ -29,11 +29,55 @@ public class CarTest {
         assertThat(actual).isEqualTo("Sonata");
     }
 
+    @DisplayName("K5의 이름은 언제나 K5 이다.")
+    @ParameterizedTest(name = "[{index}] 거리: {0}")
+    @ValueSource(ints = {100, 500, 3000})
+    void getName_k5(int distance) {
+        Car car = new K5(distance);
+
+        String actual = car.getName();
+
+        assertThat(actual).isEqualTo("K5");
+    }
+
+    @DisplayName("아반떼의 이름은 언제나 Avante 이다.")
+    @ParameterizedTest(name = "[{index}] 거리: {0}")
+    @ValueSource(ints = {100, 500, 3000})
+    void getName_avante(int distance) {
+        Car car = new Avante(distance);
+
+        String actual = car.getName();
+
+        assertThat(actual).isEqualTo("Avante");
+    }
+
     @DisplayName("소나타는 필요한 연료를 거리와 연비를 통해 계산하여 반환한다.")
     @ParameterizedTest(name = "[{index}] 거리: {0}, 필요한 연료: {1}")
     @CsvSource(value = {"100,10", "500,50", "3000,300"})
     void getFuelNeeded_sonata(int distance, int expected) {
         Car car = new Sonata(distance);
+
+        int actual = car.getFuelNeeded();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("K5는 필요한 연료를 거리와 연비를 통해 계산하여 반환한다.")
+    @ParameterizedTest(name = "[{index}] 거리: {0}, 필요한 연료: {1}")
+    @CsvSource(value = {"130,10", "260,20", "1300,100"})
+    void getFuelNeeded_k5(int distance, int expected) {
+        Car car = new K5(distance);
+
+        int actual = car.getFuelNeeded();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("아반떼는 필요한 연료를 거리와 연비를 통해 계산하여 반환한다.")
+    @ParameterizedTest(name = "[{index}] 거리: {0}, 필요한 연료: {1}")
+    @CsvSource(value = {"150,10", "300,20", "1500,100"})
+    void getFuelNeeded_avante(int distance, int expected) {
+        Car car = new Avante(distance);
 
         int actual = car.getFuelNeeded();
 
