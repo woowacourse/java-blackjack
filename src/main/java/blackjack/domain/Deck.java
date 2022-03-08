fb5360183d6bcf0f253cb9b5091e5da92b7d6a60
jayjaehunchoi <wogns0108@naver.com>
2022-03-08 16:34:18 +0900

    feat : Deck에서 카드 뽑는 기능 추가

--- src/main/java/blackjack/domain/Deck.java
+++ src/main/java/blackjack/domain/Deck.java
@@ -1,11 +1,7 @@
 package blackjack.domain;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 import java.util.Stack;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class Deck {
 
@@ -20,6 +16,7 @@ public class Deck {
         for (Suit suit : Suit.values()) {
             pushCard(cards, suit);
         }
+        Collections.shuffle(cards);
         return new Deck(cards);
     }
 
@@ -27,6 +24,10 @@ public class Deck {
         for (Denomination denomination : Denomination.values()) {
             cards.push(new Card(suit, denomination));
         }
+    }
+
+    public Card draw() {
+        return cards.pop();
     }
 
     public int size() {
--- src/test/java/blackjack/domain/DeckTest.java
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -1,17 +1,33 @@
 package blackjack.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DeckTest {
 
+    private Deck deck;
+
+    @BeforeEach
+    void setUp() {
+        deck = Deck.init();
+    }
+
     @Test
     @DisplayName("52장의 카드가 존재한다.")
-    void checkDeckCardsSize(){
-        Deck deck = Deck.init();
-
+    void checkDeckCardsSize() {
         assertThat(deck.size()).isEqualTo(52);
+    }
+
+    @Test
+    @DisplayName("Deck에서 카드를 한 장 뽑는다.")
+    void drawCard() {
+        assertAll(
+                () -> assertThat(deck.draw()).isInstanceOf(Card.class),
+                () -> assertThat(deck.size()).isEqualTo(51)
+        );
     }
 }
