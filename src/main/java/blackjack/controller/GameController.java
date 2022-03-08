c4506d2d0300adfa83f01469b41b7566bcec9596
= <rerub0831@gmail.com>
2022-03-08 17:09:21 +0900

    feat: 한장 더 받는 지 여부 입력 기능 구현

--- src/main/java/blackjack/controller/GameController.java
+++ src/main/java/blackjack/controller/GameController.java
@@ -10,5 +10,7 @@ public class GameController {
     public void run() {
         String[] names = InputView.inputPlayerName();
         System.out.println(names[0] + names[1]);
+        boolean moreCard = InputView.inputOneMoreCard("jack");
+        System.out.println(moreCard);
     }
 }
--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.Set;
 public class InputView {
     private static final String NULL_NAMES_ERROR_MESSAGE = "이름에 공백을 입력할 수 없습니다.";
     private static final String NULL_NAMES_DUPLICATED_ERROR_MESSAGE = "이름은 중복될 수 없습니다.";
+    private static final String NULL_ANSWER_YN_ERROR_MESSAGE = "y,n 이외의 입력이 들어왔습니다.";
     private static final String NAME_INPUT_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String NAME_SEPARATE_REGEX = ",";
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -15,11 +16,24 @@ public class InputView {
     public static String[] inputPlayerName() {
         System.out.println(NAME_INPUT_MESSAGE);
         String input = SCANNER.nextLine();
-        validateName(input);
+        validateNames(input);
         return separate(input);
     }
 
-    private static void validateName(String input) {
+    public static boolean inputOneMoreCard(String name) {
+        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)" + System.lineSeparator(), name);
+        String input = SCANNER.nextLine();
+        validateAnswer(input);
+        return input.equals("y");
+    }
+
+    private static void validateAnswer(String input) {
+        if (!input.equals("y") && !input.equals("n")) {
+            throw new IllegalArgumentException(NULL_ANSWER_YN_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateNames(String input) {
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException(NULL_NAMES_ERROR_MESSAGE);
         }
@@ -34,6 +48,4 @@ public class InputView {
     private static String[] separate(String input) {
         return input.split(NAME_SEPARATE_REGEX);
     }
-    // split 검증 view
-    // 이름은 name 클래스 만들어서
 }
