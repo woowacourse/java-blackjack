661b352cc730f00347ac89669259475ff4a2d451
wilgur513 <wilgur513@naver.com>
2022-03-08 15:54:18 +0900

    feat: 카드 점수 계산 후 딜러 승패 판단

--- src/main/java/blackjack/BlackJack.java
+++ src/main/java/blackjack/BlackJack.java
@@ -3,7 +3,20 @@ package blackjack;
 import java.util.List;
 
 public class BlackJack {
+
     public static boolean play(List<String> dealerCards, List<String> playerCards) {
-        return false;
+        int dealerScore = totalScore(dealerCards);
+        int playerScore = totalScore(playerCards);
+        return dealerScore > playerScore;
+    }
+
+    private static int totalScore(List<String> dealerCards) {
+        int score = 0;
+        for (String s : dealerCards) {
+            String value = s.substring(0, 1);
+            int number = Integer.parseInt(value);
+            score += number;
+        }
+        return score;
     }
 }
--- src/test/java/blackjack/BlackJackTest.java
+++ src/test/java/blackjack/BlackJackTest.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.Test;
 public class BlackJackTest {
     @Test
     void 딜러가_패배하는_경우_테스트() {
-        boolean dealerWin = BlackJack.play(List.of("8다이아몬드", "9하트"), List.of("J클로버", "Q하트"));
+        boolean dealerWin = BlackJack.play(List.of("1다이아몬드", "2하트"), List.of("3클로버", "4하트"));
         Assertions.assertThat(dealerWin).isFalse();
     }
 
     @Test
     void 딜러가_이기는_경우_테스트() {
-        boolean dealerWin = BlackJack.play(List.of("J클로버", "Q하트"), List.of("8다이아몬드", "9하트"));
+        boolean dealerWin = BlackJack.play(List.of("3클로버", "4하트"), List.of("1다이아몬드", "2하트"));
         Assertions.assertThat(dealerWin).isTrue();
     }
 
