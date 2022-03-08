3f3d2e0ed710fac853f3f23b162df3fabca787cb
shindong96 <2006peter@naver.com>
2022-03-08 17:34:53 +0900

    feat: 덱에서 카드를 드로우 하는 기능

---
+++ src/main/java/domain/Deck.java
@@ -0,0 +1,24 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Deck {
+
+	private final List<Card> cards;
+
+	public Deck() {
+		List<Card> cards = new ArrayList<>();
+		Arrays.stream(Rank.values()).forEach(rank -> {
+			Arrays.stream(Suit.values()).map(suit -> new Card(rank, suit)).forEach(cards::add);
+		});
+		this.cards = cards;
+	}
+
+	public Card draw() {
+		Card card = cards.get(0);
+		cards.remove(0);
+		return card;
+	}
+}
---
+++ src/test/java/domain/DeckTest.java
@@ -0,0 +1,15 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DeckTest {
+
+	@Test
+	@DisplayName("덱에서 카드를 드로우 하는 기능")
+	void draw() {
+		Deck deck = new Deck();
+		Assertions.assertThat(deck.draw() instanceof Card).isTrue();
+	}
+}
