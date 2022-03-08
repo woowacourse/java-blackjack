5df6dcef95ebea742cabd421047f9820b8cffb0e
sun <syoun602@gmail.com>
2022-03-08 18:52:22 +0900

    feat: 플레이어명 공백에 대한 예외 처리

---
+++ src/main/java/blackjack/domain/Player.java
@@ -0,0 +1,18 @@
+package blackjack.domain;
+
+public class Player {
+
+    private final String name;
+
+    public Player(final String name) {
+        validatePlayerName(name);
+        this.name = name;
+    }
+
+    private static void validatePlayerName(final String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("플레이어명은 공백이 될 수 없습니다.");
+        }
+    }
+
+}
---
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -0,0 +1,21 @@
+package blackjack.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PlayerTest {
+
+    @ParameterizedTest(name = "[{index}] 플레이어명 : \"{0}\"")
+    @ValueSource(strings = {"", " "})
+    @DisplayName("플레이어명에 공백이 존재하면 예외 발생")
+    void blankPlayerNameExceptionTest(final String name) {
+
+        assertThatThrownBy(() -> new Player(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("플레이어명은 공백이 될 수 없습니다.");
+    }
+
+}
