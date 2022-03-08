26454f6a1ad37a25e5ef858a9ef66d460c269e03
Donggyu Kim <a29661498@gmail.com>
2022-03-08 17:04:48 +0900

    feat(company): 차량별로 주입해야할 연료량을 확인할 수 있는 보고서를 출력할 수 있다.

--- src/main/java/rent/RentCompany.java
+++ src/main/java/rent/RentCompany.java
@@ -2,6 +2,7 @@ package rent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import rent.car.Car;
 
 public class RentCompany {
@@ -17,5 +18,22 @@ public class RentCompany {
 
     public void addCar(Car car) {
         carList.add(car);
+    }
+
+    public String generateReport() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Car car : carList) {
+            String name = car.getName();
+            long chargeQuantity = Math.round(car.getChargeQuantity());
+
+            stringBuilder.append(name);
+            stringBuilder.append(" : ");
+            stringBuilder.append(chargeQuantity);
+            stringBuilder.append("리터");
+            stringBuilder.append("\n");
+        }
+
+        return stringBuilder.toString();
     }
 }
--- src/test/java/rent/RentCompanyTest.java
+++ src/test/java/rent/RentCompanyTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import rent.car.Avante;
+import rent.car.Car;
+import rent.car.Sonata;
 
 public class RentCompanyTest {
+
+    private static final String NEWLINE = System.getProperty("line.separator");
 
     @DisplayName("팩토리 메소드로 회사 인스턴스를 만든다.")
     @Test
@@ -29,6 +33,22 @@ public class RentCompanyTest {
 
         //when & then
         assertThatNoException().isThrownBy(() -> rentCompany.addCar(avante));
+    }
 
+    @DisplayName("회사가 차량 별 주입해야 할 연료에 대한 보고서를 출력한다.")
+    @Test
+    public void testReportGeneration() {
+        //given
+        RentCompany rentCompany = RentCompany.create();
+        Car car = new Sonata(100);
+        rentCompany.addCar(car);
+
+        //when
+        String report = rentCompany.generateReport();
+
+        //then
+        assertThat(report).isEqualTo(
+                "Sonata : 10리터" + NEWLINE
+        );
     }
 }
