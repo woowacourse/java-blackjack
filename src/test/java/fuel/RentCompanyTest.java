f79952a4891a456c7297da479e8ac9480271a389
DOES <ldk980130@gmail.com>
2022-03-08 14:31:57 +0900

    feat: 차 이동거리에 따라 연료주입 기능 구현

---
+++ src/test/java/fuel/RentCompanyTest.java
@@ -0,0 +1,45 @@
+package fuel;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RentCompanyTest {
+
+	@Test
+	@DisplayName("차의 이동거리가 마이너스이면 예외")
+	void carDistanceInvalid() {
+		assertThatThrownBy(() -> new Sonata(-1))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("이동거리에 따른 연료 주입 확인")
+	void needFuelCheck() {
+		Car car = new Sonata(150);
+		assertThat(car.getChargeQuantity()).isEqualTo(15);
+	}
+
+	// private static final String NEWLINE = System.getProperty("line.separator");
+
+	// @Test
+	// public void report() throws Exception {
+	// 	RentCompany company = RentCompany.create(); // factory method를 사용해 생성
+	// 	company.addCar(new Sonata(150));
+	// 	company.addCar(new K5(260));
+	// 	company.addCar(new Sonata(120));
+	// 	company.addCar(new Avante(300));
+	// 	company.addCar(new K5(390));
+	//
+	// 	String report = company.generateReport();
+	// 	assertThat(report).isEqualTo(
+	// 		"Sonata : 15리터" + NEWLINE +
+	// 			"K5 : 20리터" + NEWLINE +
+	// 			"Sonata : 12리터" + NEWLINE +
+	// 			"Avante : 20리터" + NEWLINE +
+	// 			"K5 : 30리터" + NEWLINE
+	// 	);
+}
+
+
