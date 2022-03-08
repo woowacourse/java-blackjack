c24ef4abc8dc70dff41c3d468b92b5c95078f13b
= <rerub0831@gmail.com>
2022-03-08 17:20:37 +0900

    feat(Name): 이름 객체 생성

--- src/main/java/blackjack/domain/Name.java
+++ src/main/java/blackjack/domain/Name.java
@@ -6,9 +6,24 @@ public class Name {
     public static final String NAME_ERROR_MESSAGE = "이름 형식에 맞게 입력해야 합니다";
     private static final Pattern NAME_PATTERN = Pattern.compile("[가-힣a-zA-Z]+");
 
+    private final String name;
+
+    private Name(String name) {
+        this.name = name;
+        validateName(name);
+    }
+
     private static void validateName(String input) {
         if (!NAME_PATTERN.matcher(input).matches()) {
             throw new RuntimeException(NAME_ERROR_MESSAGE);
         }
+    }
+
+    public static Name of(String name) {
+        return new Name(name);
+    }
+
+    public String getName() {
+        return name;
     }
 }
---
+++ src/test/java/blackjack/domain/NameTest.java
@@ -0,0 +1,27 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class NameTest {
+
+    @Test
+    public void 참여자이름_객체_생성_통과() {
+        assertThat(Name.of("jack").getName())
+                .isEqualTo("jack");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ja ck", "jac.k",""})
+    public void 참여자이름_객체_생성_실패(String input) {
+        assertThatThrownBy(() -> Name.of(input))
+                .isInstanceOf(Exception.class)
+                .hasMessage("이름 형식에 맞게 입력해야 합니다");
+    }
+}
