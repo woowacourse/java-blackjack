f23f690fd2fdd22944d0096f14be5eb5c6e3b411
Chris <bjuuuu98@gmail.com>
2022-03-08 15:29:26 +0900

    feat: 이름이 빈 문자열이 들어오는 경우, 예외 처리 및 테스트 추가

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -8,6 +8,13 @@ public class Player {
 
     public Player(final String name) {
         Objects.requireNonNull(name, "[Error] 플레이어의 이름은 null이 들어올 수 없습니다.");
+        validateEmptyName(name);
         this.name = name;
+    }
+
+    private void validateEmptyName(final String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("[Error] 플레이어의 이름은 공백이 들어올 수 없습니다.");
+        }
     }
 }
--- src/test/java/blackjack/domain/PlayerTest.java
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PlayerTest {
 
@@ -14,4 +15,13 @@ public class PlayerTest {
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("[Error] 플레이어의 이름은 null이 들어올 수 없습니다.");
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void createExceptionByEmpty(String input) {
+        assertThatThrownBy(() -> new Player(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[Error] 플레이어의 이름은 공백이 들어올 수 없습니다.");
+    }
+
 }
