4a95898479e7ddcfbd91694f146c0cf524007d4c
Chris <bjuuuu98@gmail.com>
2022-03-08 18:23:09 +0900

    test: DisplayName 추가

--- src/test/java/blackjack/domain/PlayerTest.java
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -2,6 +2,7 @@ package blackjack.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,6 +11,7 @@ public class PlayerTest {
 
     @ParameterizedTest
     @NullSource
+    @DisplayName("플레이어의 이름에 null이 들어올 경우 예외가 발생해야 한다.")
     void createExceptionByNull(String input) {
         assertThatThrownBy(() -> new Player(input))
                 .isInstanceOf(NullPointerException.class)
@@ -18,10 +20,10 @@ public class PlayerTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"", " "})
+    @DisplayName("플레이어의 이름에 공백이 들어올 경우 예외가 발생해야 한다.")
     void createExceptionByEmpty(String input) {
         assertThatThrownBy(() -> new Player(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[Error] 플레이어의 이름은 공백이 들어올 수 없습니다.");
     }
-
 }
