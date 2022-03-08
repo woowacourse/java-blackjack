0bc2252c0bdd7e7fcf83a579cc29961d4f419612
jaejae-yoo <wotj102@gmail.com>
2022-03-08 18:13:26 +0900

    feat: 카드 종류 객체 생성

---
+++ src/main/java/blackjack/domain/Suit.java
@@ -0,0 +1,10 @@
+package blackjack.domain;
+
+public enum Suit {
+
+    SPADE,
+    HEART,
+    DIAMOND,
+    CLUB,
+    ;
+}
---
+++ src/test/java/blackjack/domain/SuitTest.java
@@ -0,0 +1,20 @@
+package blackjack.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SuitTest {
+
+    @Test
+    @DisplayName("enum suit size가 4개인지 확인")
+    public void checkEnumSuitSize() {
+        List<Suit> suitValues = Arrays.stream(Suit.values()).collect(Collectors.toList());
+        assertThat(suitValues.size()).isEqualTo(4);
+    }
+}
