03d53781b7d4da19efae50fef434b8ee492c166a
YuJeong <yjfflq4872@gmail.com>
2022-03-08 16:32:58 +0900

    feat: Cards 점수 계산 기능 구현

--- src/main/java/blackjack/Cards.java
+++ src/main/java/blackjack/Cards.java
@@ -1,11 +1,46 @@
 package blackjack;
 
+import java.util.List;
+
 public class Cards {
 
+    private final List<String> cards;
+
     public Cards(String... cards) {
+        this.cards = List.of(cards);
     }
 
     public int score() {
-        throw new UnsupportedOperationException("Cards#score not implemented yet !!");
+        if (isBust(totalScore(11))) {
+            return totalScore(1);
+        }
+        return totalScore(11);
+    }
+
+    private boolean isBust(int score) {
+        return score > 21;
+    }
+
+    public boolean isBust() {
+        return score() > 21;
+    }
+
+    private int totalScore(int aceValue) {
+        int score = 0;
+        for (String s : cards) {
+            String value = s.substring(0, 1);
+            score += number(value, aceValue);
+        }
+        return score;
+    }
+
+    private int number(String value, int aceValue) {
+        if (value.equals("J") || value.equals("Q") || value.equals("K")) {
+            return 10;
+        }
+        if (value.equals("A")) {
+            return aceValue;
+        }
+        return Integer.parseInt(value);
     }
 }
