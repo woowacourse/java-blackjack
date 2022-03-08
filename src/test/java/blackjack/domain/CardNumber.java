c383e709b385cbb88da8e4b9dfbc2b0848d6ab28
Chris <bjuuuu98@gmail.com>
2022-03-08 15:38:46 +0900

    feat: 카드 숫자 enum 추가

---
+++ src/test/java/blackjack/domain/CardNumber.java
@@ -0,0 +1,19 @@
+package blackjack.domain;
+
+public enum CardNumber {
+
+    A,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ;
+}
