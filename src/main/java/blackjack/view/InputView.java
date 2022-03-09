3b5562d903d5e10e708cfa84d128c9c66bb7cd44
nailseong <nnis9857@gmail.com>
2022-03-09 17:17:21 +0900

    feat(InputView): 카드를 더 받을지 여부를 입력 받는 기능

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,5 +1,6 @@
 package blackjack.view;
 
+import blackjack.domain.card.Status;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -16,5 +17,20 @@ public class InputView {
         return Stream.of(text.split(",", -1))
                 .map(String::trim)
                 .collect(Collectors.toList());
+    }
+
+    public static Status requestHitOrStay(String name) {
+        System.out.println(name + "은(는) 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+
+        final String text = scanner.nextLine().trim();
+        if (!text.matches("[Y,y,N,n]")) {
+            throw new IllegalArgumentException("y, n 이외의 값이 입력되었습니다.");
+        }
+
+        if (text.equalsIgnoreCase("y")) {
+            return Status.HIT;
+        }
+
+        return Status.STAY;
     }
 }
