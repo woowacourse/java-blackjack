e289da0cbb0d8a3a5542d6137b2893d1ebf51f0e
SuyeonChoi <tndus970120@gmail.com>
2022-03-09 10:42:59 +0900

    test: divideCard()를 assertThat 한개로 테스트하도록 수정

--- src/test/java/blackjack/domain/BlackJackTest.java
+++ src/test/java/blackjack/domain/BlackJackTest.java
@@ -1,6 +1,6 @@
 package blackjack.domain;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,16 +10,20 @@ import org.junit.jupiter.api.Test;
 class BlackJackTest {
     @Test
     @DisplayName("카드를 2장씩 나눈다.")
-    void splitCard() {
-        Player player1 = new Player("페퍼");
-        Player player2 = new Player("애쉬");
-        PlayerGroup playerGroup = new PlayerGroup(Arrays.asList(player1, player2));
+    void divideCard() {
+        Player pepper = new Player("페퍼");
+        Player ash = new Player("애쉬");
+        PlayerGroup playerGroup = new PlayerGroup(Arrays.asList(pepper, ash));
         BlackJack blackJack = new BlackJack(playerGroup);
         blackJack.divideCards();
 
-        assertThat(player1.getCardsSize()).isEqualTo(2);
-        assertThat(player2.getCardsSize()).isEqualTo(2);
-        assertThat(blackJack.getDealer().getCardsSize()).isEqualTo(2);
+        List<Integer> cardSizes = List.of(pepper.getCardsSize(),
+                ash.getCardsSize(),
+                blackJack.getDealer().getCardsSize());
+
+        assertThat(cardSizes)
+                .containsExactly(2, 2, 2)
+                .doesNotContain(0);
     }
 
     @Test
