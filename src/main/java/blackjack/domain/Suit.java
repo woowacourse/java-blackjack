b0244acef8841d7c80bdd8d7cc25b82fe77e3735
jaejae-yoo <wotj102@gmail.com>
2022-03-09 16:54:42 +0900

    feat: 한글 이름 멤버 변수 추가

--- src/main/java/blackjack/domain/Suit.java
+++ src/main/java/blackjack/domain/Suit.java
@@ -2,9 +2,19 @@ package blackjack.domain;
 
 public enum Suit {
 
-    SPADE,
-    HEART,
-    DIAMOND,
-    CLUB,
+    SPADE("스페이드"),
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    CLUB("클로버"),
     ;
+
+    private final String name;
+
+    Suit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
