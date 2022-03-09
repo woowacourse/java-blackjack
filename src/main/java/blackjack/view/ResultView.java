c88551cae91f3d30f349d98212fb9e841717560c
woong7 <driedseas@gmail.com>
2022-03-09 18:50:41 +0900

    refactor(ResultView): 불필요한 반복 제거

--- src/main/java/blackjack/view/ResultView.java
+++ src/main/java/blackjack/view/ResultView.java
@@ -1,33 +1,40 @@
 package blackjack.view;
 
-import blackjack.domain.Blackjack;
 import blackjack.domain.BlackjackDto;
 import blackjack.domain.Participant;
 
 public class ResultView {
 
-	private static final String RESULT_DELIMITER = " - 결과: ";
+    private static final String RESULT_DELIMITER = " - 결과: ";
 
-	public void showStartingStatus(BlackjackDto blackjackDto) {
+    public void showStartingStatus(BlackjackDto blackjackDto) {
         System.out.println(blackjackDto.getDealerOpenCard());
         for (Participant player : blackjackDto.getPlayers()) {
-            System.out.println(blackjackDto.getPlayerCardStatus(player));
+            showEachPlayerCurrentStatus(blackjackDto, player);
         }
     }
 
     public void showCurrentStatus(BlackjackDto blackjackDto) {
         Participant dealer = blackjackDto.getDealer();
-        System.out.println(blackjackDto.getPlayerCardStatus(dealer));
+        showEachPlayerCurrentStatus(blackjackDto, dealer);
         for (Participant player : blackjackDto.getPlayers()) {
-            System.out.println(blackjackDto.getPlayerCardStatus(player));
+            showEachPlayerCurrentStatus(blackjackDto, player);
         }
     }
 
-	public void showFinishedStatus(BlackjackDto blackjackDto) {
-		Participant dealer = blackjackDto.getDealer();
-		System.out.println(blackjackDto.getPlayerCardStatus(dealer) + RESULT_DELIMITER + dealer.getScore());
-		for (Participant player : blackjackDto.getPlayers()) {
-			System.out.println(blackjackDto.getPlayerCardStatus(player) + RESULT_DELIMITER + player.getScore());
-		}
-	}
+    public void showEachPlayerCurrentStatus(BlackjackDto blackjackDto, Participant participant) {
+        System.out.println(getEachPlayerStatus(blackjackDto, participant));
+    }
+
+    private String getEachPlayerStatus(BlackjackDto blackjackDto, Participant participant) {
+        return blackjackDto.getPlayerCardStatus(participant);
+    }
+
+    public void showFinishedStatus(BlackjackDto blackjackDto) {
+        Participant dealer = blackjackDto.getDealer();
+        System.out.println(getEachPlayerStatus(blackjackDto, dealer) + RESULT_DELIMITER + dealer.getScore());
+        for (Participant player : blackjackDto.getPlayers()) {
+            System.out.println(getEachPlayerStatus(blackjackDto, player) + RESULT_DELIMITER + player.getScore());
+        }
+    }
 }
