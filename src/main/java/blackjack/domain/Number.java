2823a1d76f9cb8765ef005e96de6ebbc56a0b3c4
meatsby <meatsby@gmail.com>
2022-03-09 15:30:22 +0900

    feat(Number): 카드 숫자 enum 구현 및 테스트

---
+++ src/main/java/blackjack/domain/Number.java
@@ -0,0 +1,35 @@
+package blackjack.domain;
+
+public enum Number {
+
+    ACE("A", 11),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10),
+    ;
+
+    private final String name;
+    private final int score;
+
+    Number(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
