699c191c6114abf1b70f84232650bc45ec8e7c9f
Donggyu <a29661498@gmail.com>
2022-03-09 19:46:24 +0900

    feat(Player): 플레이어가 덱으로부터 카드 뽑는 로직 구현

--- src/main/java/blackjack/domain/user/Player.java
+++ src/main/java/blackjack/domain/user/Player.java
@@ -1,10 +1,26 @@
 package blackjack.domain.user;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Deck;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Player {
 
     private final String name;
+    private final List<Card> cards;
 
     public Player(String name) {
         this.name = name;
+        this.cards = new ArrayList<>();
+    }
+
+    public void drawCard(Deck deck) {
+        cards.add(deck.drawCard());
+    }
+
+    public List<Card> showCards() {
+        return Collections.unmodifiableList(cards);
     }
 }
--- src/test/java/blackjack/domain/user/PlayerTest.java
+++ src/test/java/blackjack/domain/user/PlayerTest.java
@@ -2,6 +2,9 @@ package blackjack.domain.user;
 
 import static org.assertj.core.api.Assertions.*;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Deck;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +21,20 @@ public class PlayerTest {
 
         //then
         assertThat(player).isNotNull();
+    }
+
+    @DisplayName("플레이어는 카드를 뽑을 수 있다.")
+    @Test
+    public void testDrawCard() {
+        //given
+        Deck deck = new Deck();
+        Player player = new Player("pobi");
+
+        //when
+        player.drawCard(deck);
+        List<Card> cards = player.showCards();
+
+        //then
+        assertThat(cards.size()).isEqualTo(1);
     }
 }
