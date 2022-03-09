65ea3ec1af4bd8f11200330ba0ad67dc4939d9b4
Juhyung990122 <harukawa99@naver.com>
2022-03-09 20:49:36 +0900

    feat: 카드 넘버에 이름 추가

--- src/main/java/BlackJack/domain/Number.java
+++ src/main/java/BlackJack/domain/Number.java
@@ -4,30 +4,37 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Number {
-    ACE(1, 11),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
+    ACE("A", 1, 11),
+    TWO("2",2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10",10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10);
 
+    private String denomination;
     private int value;
     private int otherValue;
 
-    Number(int value, int otherValue){
+    Number(String denomination, int value, int otherValue){
+        this.denomination = denomination;
         this.value = value;
         this.otherValue = otherValue;
     }
 
-    Number(int value){
+    Number(String denomination, int value){
+        this.denomination = denomination;
         this.value = value;
+    }
+
+    public String getDenomination() {
+        return denomination;
     }
 
     public int getValue() {
