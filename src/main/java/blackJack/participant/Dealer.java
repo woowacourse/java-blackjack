ccae94109d03e4e474d0b580889f81e30d289742
Wishoon <heonga2@gmail.com>
2022-03-09 11:02:04 +0900

    feat : 딜러 카드 합계 계산 기능 구현

--- src/main/java/blackJack/participant/Dealer.java
+++ src/main/java/blackJack/participant/Dealer.java
@@ -1,10 +1,26 @@
 package blackJack.participant;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import blackJack.domain.Card;
+
 public class Dealer {
 
     private final String name = "딜러";
+    private final List<Card> cards;
 
     public Dealer() {
+        this.cards = new ArrayList<>();
+    }
 
+    public void receiveCard(Card card) {
+        cards.add(card);
+    }
+
+    public int calculateScore() {
+        return cards.stream()
+            .mapToInt(Card::getScore)
+            .sum();
     }
 }
--- src/test/java/blackJack/participant/DealerTest.java
+++ src/test/java/blackJack/participant/DealerTest.java
@@ -5,11 +5,23 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import blackJack.domain.Card;
+import blackJack.domain.Denomination;
+import blackJack.domain.Symbol;
+
 public class DealerTest {
 
     @Test
     @DisplayName("Dealer 생성 테스트")
     void createValidDealer() {
         assertThat(new Dealer()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Dealer 카드 합계 계산 테스트")
+    void calculateScore() {
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(new Card(Symbol.CLOVER, Denomination.EIGHT));
+        assertThat(dealer.calculateScore()).isEqualTo(8);
     }
 }
