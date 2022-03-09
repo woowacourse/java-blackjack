0f8a92e4b19b3ceb3c3803580a00fe6613ed2193
sun <syoun602@gmail.com>
2022-03-09 23:33:13 +0900

    feat: 게임 진행 여부 입력 기능 추가

---
+++ src/main/java/blackjack/view/Choice.java
@@ -0,0 +1,33 @@
+package blackjack.view;
+
+import java.util.Arrays;
+
+public enum Choice {
+
+    HIT("y", true),
+    STAND("n", false);
+
+    private final String answer;
+    private final boolean continuable;
+
+    Choice(String answer, boolean continuable) {
+        this.answer = answer;
+        this.continuable = continuable;
+    }
+
+    public static Choice from(final String answer) {
+        return Arrays.stream(Choice.values())
+                .filter(it -> it.matchesAnswer(answer))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("입력은 y 또는 n이어야 합니다."));
+    }
+
+    private boolean matchesAnswer(final String answer) {
+        final String lowerCasedAnswer = answer.toLowerCase();
+        return lowerCasedAnswer.equals(this.answer);
+    }
+
+    public boolean getContinuable() {
+        return continuable;
+    }
+}
