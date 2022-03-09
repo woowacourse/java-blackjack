6d5af2794457c3717a4f2a85f7c7cbc58fc2fdae
meatsby <meatsby@gmail.com>
2022-03-09 14:58:53 +0900

    feat(Kind): 카드 문양 enum 구현 및 테스트

---
+++ src/test/java/blackjack/domain/KindTest.java
@@ -0,0 +1,18 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class KindTest {
+
+    @DisplayName("카드 문양 테스트")
+    @Test
+    void constructorTest() {
+        assertThat(Kind.SPADE.getSymbol()).isEqualTo("스페이드");
+        assertThat(Kind.DIAMOND.getSymbol()).isEqualTo("다이아몬드");
+        assertThat(Kind.CLOVER.getSymbol()).isEqualTo("클로버");
+        assertThat(Kind.HEART.getSymbol()).isEqualTo("하트");
+    }
+}
