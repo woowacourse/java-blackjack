dd2d8b93b784b0e0bb1ca90d0a962a143ba9e905
tonic523 <diddmlvkf@gmail.com>
2022-03-09 15:05:04 +0900

    feat(Card) : 블랙잭에 필요한 카드를 캐싱

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -1,13 +1,36 @@
 package blackjack.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Card {
 
+    static final List<Card> cachingCard = new ArrayList<>();
     private final CardPattern pattern;
     private final CardNumber number;
 
-    public Card(CardPattern pattern, CardNumber number) {
+    static {
+        for (CardPattern pattern : CardPattern.values()) {
+            for (CardNumber number : CardNumber.values()) {
+                cachingCard.add(new Card(pattern, number));
+            }
+        }
+    }
+
+    private Card(CardPattern pattern, CardNumber number) {
         this.pattern = pattern;
         this.number = number;
+    }
+
+    public static Card of(CardPattern pattern, CardNumber number) {
+        return cachingCard.stream()
+            .filter(card -> card.isContain(pattern, number))
+            .findAny()
+            .orElse(null);
+    }
+
+    public boolean isContain(CardPattern pattern, CardNumber number) {
+        return this.pattern.equals(pattern) && this.number.equals(number);
     }
 
     public int getNumber() {
--- src/test/java/blackjack/domain/CardTest.java
+++ src/test/java/blackjack/domain/CardTest.java
@@ -4,10 +4,24 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class CardTest {
+
+    @Nested
+    @DisplayName("of는")
+    class Of {
+
+        @Test
+        @DisplayName("캐싱된 카드를 반환한다.")
+        void returnCachedCard() {
+            Card card1 = Card.of(CardPattern.DIAMOND, CardNumber.J);
+            Card card2 = Card.of(CardPattern.DIAMOND, CardNumber.J);
+            assertThat(card1).isSameAs(card2);
+        }
+    }
 
     @Nested
     @DisplayName("getNumber는")
@@ -17,7 +31,7 @@ public class CardTest {
         @CsvSource(value = {"ACE|1", "TWO|2", "TEN|10", "Q|10", "J|10", "K|10"}, delimiter = '|')
         @DisplayName("Number를 반환한다.")
         void returnNumber(CardNumber cardNumber, int input) {
-            Card card = new Card(CardPattern.DIAMOND, cardNumber);
+            Card card = Card.of(CardPattern.DIAMOND, cardNumber);
             assertThat(card.getNumber()).isEqualTo(input);
         }
     }
@@ -28,9 +42,9 @@ public class CardTest {
 
         @ParameterizedTest
         @CsvSource(value = {"DIAMOND|다이아몬드", "CLOVER|클로버", "SPADE|스페이드", "HEART|하트"}, delimiter = '|')
-        @DisplayName("It")
+        @DisplayName("Pattern을 반환한다.")
         void returnPattern(CardPattern cardPattern, String expected) {
-            Card card = new Card(cardPattern, CardNumber.ACE);
+            Card card = Card.of(cardPattern, CardNumber.ACE);
             assertThat(card.getPattern()).isEqualTo(expected);
         }
     }
