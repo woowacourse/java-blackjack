b3f67095552a877a264c971c431dbe6448cac825
pup-paw <wldusdl0310@gmail.com>
2022-03-09 16:19:51 +0900

    feat: choice에 따라 카드 추가 여부를 결정

--- src/main/java/blackjack/Blackjack.java
+++ src/main/java/blackjack/Blackjack.java
@@ -1,10 +1,14 @@
 package blackjack;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class Blackjack {
 	private static final String ERROR_MESSAGE_PLAYER_NUMBER_EXCEED = "[ERROR] 참가자의 수는 8명을 초과할 수 없습니다.";
+	private static final String ERROR_MESSAGE_ILLEGAL_CHOICE_FORMAT = "[ERROR] y 또는 n 으로 입력해야 합니다.";
+	private static final String CHOICE_YES = "y";
+	private static final String CHOICE_NO = "n";
 	private static final int MAX_PLAYER_NUMBER = 8;
 
 	private final Participant dealer;
@@ -20,8 +24,8 @@ public class Blackjack {
 	public static Blackjack createFrom(List<String> playerNames) {
 		validatePlayerNumber(playerNames);
 		List<Participant> players = playerNames.stream()
-				.map(Participant::createPlayer)
-				.collect(Collectors.toList());
+			.map(Participant::createPlayer)
+			.collect(Collectors.toList());
 
 		return new Blackjack(players);
 	}
@@ -44,4 +48,19 @@ public class Blackjack {
 			player.receiveCard(cardDeck.pickCard());
 		}
 	}
+
+	public void handOutAdditionalCard(Participant player, String choice) {
+		choice = choice.toLowerCase(Locale.ROOT);
+		checkValidChoice(choice);
+		if (choice.equals(CHOICE_YES)) {
+			player.receiveCard(cardDeck.pickCard());
+		}
+	}
+
+	private void checkValidChoice(String choice) {
+		if (!(choice.equals(CHOICE_YES) || choice.equals(CHOICE_NO))) {
+			throw new IllegalArgumentException(ERROR_MESSAGE_ILLEGAL_CHOICE_FORMAT);
+		}
+	}
+
 }
--- src/test/java/blackjack/BlackjackTest.java
+++ src/test/java/blackjack/BlackjackTest.java
@@ -2,10 +2,10 @@ package blackjack;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import blackjack.Blackjack;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +19,17 @@ public class BlackjackTest {
 			Blackjack.createFrom(players);
 		}).isInstanceOf(IllegalArgumentException.class)
 				.hasMessage("[ERROR] 참가자의 수는 8명을 초과할 수 없습니다.");
+	}
+
+	@Test
+	@DisplayName("y 또는 n이 아니면 예외처리")
+	void Illegal_Choice_Format_Exception() {
+		List<String> playerNames = new ArrayList<>();
+		playerNames.add("panda");
+		Blackjack blackjack = Blackjack.createFrom(playerNames);
+		assertThatThrownBy(() -> {
+			blackjack.handOutAdditionalCard(Participant.createPlayer(playerNames.get(0)), "123!");
+		}).isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("[ERROR] y 또는 n 으로 입력해야 합니다.");
 	}
 }
