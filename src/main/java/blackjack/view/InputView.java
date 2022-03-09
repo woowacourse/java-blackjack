0ef43d7eae88f461e4ece1d29e9e17badfa67e20
pup-paw <wldusdl0310@gmail.com>
2022-03-09 17:13:44 +0900

    feat: 참가자에게 추가 카드를 받을지 물어보는 기능

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -5,7 +5,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private final Scanner scanner;
+	private static final String MESSAGE_ASK_PARTICIPANTS = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+	private static final String NAME_DELIMITER = ",";
+	private static final String MESSAGE_ASK_ADDITIONAL_CARD_CHOICE = "%s, 한장의 카드를 더 받겠습니까?(예는 Y/y, 아니오는 N/n)";
+	private final Scanner scanner;
 
     public InputView() {
         this.scanner = new Scanner(System.in);
@@ -15,8 +18,14 @@ public class InputView {
         scanner.close();
     }
 
-    public List<String> getPlayerNameInput() {
+    public List<String> askPlayerNameInput() {
+		System.out.println(MESSAGE_ASK_PARTICIPANTS);
         String input = scanner.nextLine();
-        return Arrays.asList(input.split(","));
+        return Arrays.asList(input.split(NAME_DELIMITER));
     }
+
+	public String askAdditionalCardInput(String name) {
+		System.out.printf(MESSAGE_ASK_ADDITIONAL_CARD_CHOICE, name);
+		return scanner.nextLine();
+	}
 }
