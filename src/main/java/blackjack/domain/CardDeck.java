02ae6b73db4e4a9db5e12dff48f94be2e649a90a
RIANAEH <ririhan217@gmail.com>
2022-03-09 14:49:26 +0900

    feat: 카드 덱 생성 및 카드 뽑기 기능 추가

---
+++ src/main/java/blackjack/domain/CardDeck.java
@@ -0,0 +1,34 @@
+package blackjack.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CardDeck {
+
+    private static final List<Card> cards;
+
+    static {
+        cards = Stream.of(Denomination.values())
+                .flatMap(denomination ->
+                        Stream.of(Suit.values())
+                                .map(suit -> new Card(denomination, suit)))
+                .collect(Collectors.toList());
+    }
+
+    public CardDeck() {
+        Collections.shuffle(cards);
+    }
+
+    public Card getCard(final int index) {
+        checkIndex(index);
+        return cards.get(index);
+    }
+
+    private void checkIndex(int index) {
+        if (index >= cards.size()) {
+            throw new IndexOutOfBoundsException("[ERROR] 카드 덱의 범위를 넘어갔습니다.");
+        }
+    }
+}
