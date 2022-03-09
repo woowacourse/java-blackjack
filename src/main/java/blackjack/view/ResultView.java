7e95e0e59250918679efaebdd8de44ad14cfc9b9
pup-paw <wldusdl0310@gmail.com>
2022-03-09 18:08:29 +0900

    feat: 최종 상태 출력 기능

--- src/main/java/blackjack/view/ResultView.java
+++ src/main/java/blackjack/view/ResultView.java
@@ -1,10 +1,14 @@
 package blackjack.view;
 
+import blackjack.domain.Blackjack;
 import blackjack.domain.BlackjackDto;
 import blackjack.domain.Participant;
 
 public class ResultView {
-    public void showStartingStatus(BlackjackDto blackjackDto) {
+
+	private static final String RESULT_DELIMITER = " - 결과: ";
+
+	public void showStartingStatus(BlackjackDto blackjackDto) {
         System.out.println(blackjackDto.getDealerOpenCard());
         for (Participant player : blackjackDto.getPlayers()) {
             System.out.println(blackjackDto.getPlayerCardStatus(player));
@@ -18,4 +22,12 @@ public class ResultView {
             System.out.println(blackjackDto.getPlayerCardStatus(player));
         }
     }
+
+	public void showFinishedStatus(BlackjackDto blackjackDto) {
+		Participant dealer = blackjackDto.getDealer();
+		System.out.println(blackjackDto.getPlayerCardStatus(dealer) + RESULT_DELIMITER + dealer.getScore());
+		for (Participant player : blackjackDto.getPlayers()) {
+			System.out.println(blackjackDto.getPlayerCardStatus(player) + RESULT_DELIMITER + player.getScore());
+		}
+	}
 }
