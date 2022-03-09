ef2393e93b2c692babac5f74a8ac628d08ddddbb
Chris <bjuuuu98@gmail.com>
2022-03-09 19:56:44 +0900

    refactor: 메소드 위치 수정 및 equals hashcode 삭제

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -2,15 +2,29 @@ package blackjack.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Card {
 
-    private static final List<Card> CARDS_CACHE;
+    private static final List<Card> CARDS_CACHE = createCards();
 
-    static {
-        CARDS_CACHE = createCards();
+    private final CardPattern pattern;
+    private final CardNumber number;
+
+    private Card(final CardPattern pattern, final CardNumber number) {
+        this.pattern = pattern;
+        this.number = number;
+    }
+
+    public static Card of(final CardPattern pattern, final CardNumber number) {
+        return CARDS_CACHE.stream()
+                .filter(card -> isEqualCard(pattern, number, card))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 카드입니다."));
+    }
+
+    private static boolean isEqualCard(final CardPattern pattern, final CardNumber number, final Card card) {
+        return card.pattern == pattern && card.number == number;
     }
 
     private static List<Card> createCards() {
@@ -27,21 +41,6 @@ public class Card {
                 .collect(Collectors.toList());
     }
 
-    private final CardPattern pattern;
-    private final CardNumber number;
-
-    private Card(final CardPattern pattern, final CardNumber number) {
-        this.pattern = pattern;
-        this.number = number;
-    }
-
-    public static Card of(final CardPattern pattern, final CardNumber number) {
-        return CARDS_CACHE.stream()
-                .filter(card -> card.pattern == pattern && card.number == number)
-                .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 카드입니다."));
-    }
-
     public static List<Card> cards() {
         return new ArrayList<>(CARDS_CACHE);
     }
@@ -52,22 +51,5 @@ public class Card {
 
     public CardNumber getNumber() {
         return number;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        final Card card = (Card) o;
-        return pattern == card.pattern && number == card.number;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(pattern, number);
     }
 }
