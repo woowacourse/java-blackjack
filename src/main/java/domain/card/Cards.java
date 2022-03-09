aa0cb24e3bcc7b0f7f83e458c1ab752f49dd0b30
RunaNam <nmj5012@naver.com>
2022-03-09 17:15:28 +0900

    feat(Cards): calculateResult 메서드 추가

--- src/main/java/domain/card/Cards.java
+++ src/main/java/domain/card/Cards.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Cards {
 
+    private static final String ACE_LETTER = "A";
+    public static final int ACE_ADDITIONAL_VALUE = 10;
+    public static final int BLACKJACK_MAX_VALUE_CRITERIA = 21;
+
     private final List<Card> cards;
 
     public Cards(List<Card> init) {
@@ -15,6 +19,30 @@ public class Cards {
         return cards.stream()
             .mapToInt(Card::getScore)
             .sum();
+    }
+
+    public int calculateResult() {
+        if (hasAce()) {
+            return judgeAdvantageResult(calculateSum());
+        }
+        return calculateSum();
+    }
+
+    private boolean hasAce() {
+        return cards.stream()
+            .anyMatch(card -> card.getDenomination().equals(ACE_LETTER));
+    }
+
+    private int judgeAdvantageResult(int sumAsOne) {
+        int sumAsEleven = sumAsOne + ACE_ADDITIONAL_VALUE;
+
+        int gapByEleven = Math.abs(BLACKJACK_MAX_VALUE_CRITERIA - sumAsEleven);
+        int gapByOne = Math.abs(BLACKJACK_MAX_VALUE_CRITERIA - sumAsOne);
+        
+        if (gapByEleven < gapByOne) {
+            return sumAsEleven;
+        }
+        return sumAsOne;
     }
 
     public void addCard(Card card) {
