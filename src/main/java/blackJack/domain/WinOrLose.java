d4126d7dcb83e10cbc391b173f1a1b2af9d18543
Wishoon <heonga2@gmail.com>
2022-03-09 13:20:48 +0900

    feat : 승무패를 판단하는 기능 구현

---
+++ src/main/java/blackJack/domain/WinOrLose.java
@@ -0,0 +1,36 @@
+package blackJack.domain;
+
+public enum WinOrLose {
+    WIN("승"), DRAW("무"), LOSE("패");
+
+    private static final int BLACK_JACK = 21;
+    private final String result;
+
+    WinOrLose(String result) {
+        this.result = result;
+    }
+
+    public static WinOrLose calculateWinOrLose(int playerScore, int dealerScore) {
+        if (overScore(playerScore)) {
+            return LOSE;
+        }
+        if (overScore(dealerScore)) {
+            return WIN;
+        }
+        return getWinOrLose(playerScore, dealerScore);
+    }
+
+    private static WinOrLose getWinOrLose(int playerScore, int dealerScore) {
+        if (playerScore > dealerScore) {
+            return WIN;
+        }
+        if (playerScore == dealerScore) {
+            return DRAW;
+        }
+        return LOSE;
+    }
+
+    private static boolean overScore(int score) {
+        return score > BLACK_JACK;
+    }
+}
