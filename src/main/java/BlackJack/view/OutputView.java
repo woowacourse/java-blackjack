898beda6737abee4c8b5e63fc6d85e3f88853d7d
Juhyung990122 <harukawa99@naver.com>
2022-03-09 20:51:43 +0900

    feat: 딜러와 참가자 카드 출력 기능 구현

---
+++ src/main/java/BlackJack/view/OutputView.java
@@ -0,0 +1,24 @@
+package BlackJack.view;
+
+import BlackJack.dto.UserDto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    private static final String DRAW_MESSAGE = "딜러와 %s에게 2장의 나누었습니다.\n";
+    private static final String CARD_FORMAT = "%s카드: %s";
+    private static final String DELIMITER = ",";
+
+    public static void printDrawMessage(List<String> userNames){
+        System.out.printf(DRAW_MESSAGE, userNames.stream().collect(Collectors.joining(DELIMITER)));
+    }
+
+    public static void printTotalUserCards(List<UserDto> users) {
+        for (UserDto userDto : users) {
+            String cards = userDto.getCards().stream().collect(Collectors.joining(DELIMITER));
+            System.out.println(String.format(CARD_FORMAT, userDto.getName(), cards));
+        }
+    }
+}
