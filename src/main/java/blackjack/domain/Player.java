d42fcf3b10aa6d1701361623542945f38c91df22
awesomeo184 <awesomeo184@gmail.com>
2022-03-10 00:40:45 +0900

    feat(Player): hit이 가능하면 참을 반환하는 기능 추가

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -1,11 +1,58 @@
 package blackjack.domain;
 
+import java.util.Collections;
 import java.util.List;
 
-public class Player extends Participant {
+public class Player extends Participant{
+    private boolean stay = false;
 
     public Player(String name, List<Card> cards) {
         super(name, cards);
+    }
+
+    private void addScore(Card card) {
+        score += card.getValue();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+        addScore(card);
+
+        if (score >= GOAL_SCORE) {
+            stay = true;
+        }
+    }
+
+    public void endTurn() {
+        int aceCount = (int) cards.stream()
+                .filter(Card::isAce)
+                .count();
+
+        boolean changed = true;
+        while (aceCount-- > 0 && changed) {
+            changed = moreScoreForAceOrNot();
+        }
+    }
+
+    private boolean moreScoreForAceOrNot() {
+        if (score + ADDITIONAL_SCORE_FOR_ACE <= GOAL_SCORE) {
+            score += ADDITIONAL_SCORE_FOR_ACE;
+            return true;
+        }
+        return false;
+    }
+
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Card> getCards() {
+        return Collections.unmodifiableList(cards);
     }
 
     public boolean canHit() {
@@ -20,5 +67,13 @@ public class Player extends Participant {
             return true;
         }
         return getScore() >= score;
+    }
+
+    public boolean isAbleToHit() {
+        return !stay;
+    }
+
+    public void stay() {
+        stay = true;
     }
 }
--- src/test/java/blackjack/domain/PlayerTest.java
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static blackjack.domain.Denomination.*;
+import static blackjack.domain.Symbol.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PlayerTest {
@@ -13,8 +15,8 @@ class PlayerTest {
     @DisplayName("hit 가능하다면 참을 반환한다")
     void canHitWhenTrue() {
         Player player = new Player("pobi", List.of(
-                new Card(Symbol.HEART, Denomination.KING),
-                new Card(Symbol.CLOVER, Denomination.QUEEN)
+                new Card(HEART, KING),
+                new Card(CLOVER, QUEEN)
         ));
 
         assertThat(player.canHit()).isTrue();
@@ -24,11 +26,11 @@ class PlayerTest {
     @DisplayName("hit 가능하지않다면 거짓을 반환한다")
     void canHitWhenFalse() {
         Player player = new Player("pobi", List.of(
-                new Card(Symbol.HEART, Denomination.KING),
-                new Card(Symbol.CLOVER, Denomination.QUEEN)
+                new Card(HEART, KING),
+                new Card(CLOVER, QUEEN)
         ));
 
-        player.addCard(new Card(Symbol.SPADE, Denomination.ACE));
+        player.addCard(new Card(SPADE, ACE));
 
         assertThat(player.canHit()).isFalse();
     }
@@ -37,8 +39,8 @@ class PlayerTest {
     @DisplayName("승패 여부를 판단한다 : 총점이 21 이하인 경우")
     void testWinOrLose() {
         Player player = new Player("pobi",List.of(
-                new Card(Symbol.SPADE,Denomination.FIVE),
-                new Card(Symbol.DIAMOND,Denomination.TWO)
+                new Card(SPADE, FIVE),
+                new Card(DIAMOND, TWO)
         ));
         assertThat(player.isWin(16)).isFalse();
         assertThat(player.isWin(6)).isTrue();
@@ -48,9 +50,9 @@ class PlayerTest {
     @DisplayName("승패 여부를 판단한다 : 플레이어의 총점이 21 초과인 경우")
     void testWinOrLose2() {
         Player player = new Player("pobi",List.of(
-                new Card(Symbol.SPADE,Denomination.KING),
-                new Card(Symbol.SPADE,Denomination.QUEEN),
-                new Card(Symbol.DIAMOND,Denomination.TWO)
+                new Card(SPADE, KING),
+                new Card(SPADE, QUEEN),
+                new Card(DIAMOND, TWO)
         ));
         assertThat(player.isWin(1)).isFalse();
     }
@@ -59,9 +61,68 @@ class PlayerTest {
     @DisplayName("승패 여부를 판단한다 : 딜러의 총점이 21 초과인 경우")
     void testWinOrLose3() {
         Player player = new Player("pobi",List.of(
-                new Card(Symbol.SPADE,Denomination.ACE),
-                new Card(Symbol.SPADE,Denomination.ACE)
+                new Card(SPADE, ACE),
+                new Card(SPADE, ACE)
         ));
         assertThat(player.isWin(22)).isTrue();
+    }
+
+    @Test
+    @DisplayName("hit이 가능한 상태라면 참을 반환한다.")
+    void testIsAbleToHit1() {
+        // given
+        List<Card> initCards = List.of(new Card(CLOVER, JACK), new Card(DIAMOND, FIVE));
+        Player player = new Player("pobi", initCards);
+
+        // when
+        boolean actual = player.isAbleToHit();
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    @DisplayName("블랙잭이면 더이상 hit을 할 수 없다.")
+    void testIsAbleToHit2() {
+        // given
+        List<Card> initCards = List.of(new Card(CLOVER, JACK), new Card(DIAMOND, FIVE));
+        Player player = new Player("pobi", initCards);
+        player.addCard(new Card(DIAMOND, SIX));
+
+        // when
+        boolean actual = player.isAbleToHit();
+
+        // then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    @DisplayName("버스트면 더이상 hit을 할 수 없다.")
+    void testIsAbleToHit3() {
+        // given
+        List<Card> initCards = List.of(new Card(CLOVER, JACK), new Card(DIAMOND, FIVE));
+        Player player = new Player("pobi", initCards);
+        player.addCard(new Card(DIAMOND, SEVEN));
+
+        // when
+        boolean actual = player.isAbleToHit();
+
+        // then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    @DisplayName("stay하면 더이상 hit을 할 수 없다.")
+    void testIsAbleToHit4() {
+        // given
+        List<Card> initCards = List.of(new Card(CLOVER, JACK), new Card(DIAMOND, FIVE));
+        Player player = new Player("pobi", initCards);
+
+        // when
+        player.stay();
+        boolean actual = player.isAbleToHit();
+
+        // then
+        assertThat(actual).isFalse();
     }
 }
