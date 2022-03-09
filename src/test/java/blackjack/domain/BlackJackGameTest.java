a56bc89ac67e9180661534db7b8d3b80b9717411
jayjaehunchoi <wogns0108@naver.com>
2022-03-09 14:28:59 +0900

    refactor : 테스트 코드에 setUp 추가

--- src/test/java/blackjack/domain/BlackJackGameTest.java
+++ src/test/java/blackjack/domain/BlackJackGameTest.java
@@ -3,17 +3,24 @@ package blackjack.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BlackJackGameTest {
 
+    private BlackJackGame blackJackGame;
+    private Deck deck;
+
+    @BeforeEach
+    void setUp() {
+        blackJackGame = initBlackJackGame();
+        deck = Deck.init();
+    }
+
     @Test
     @DisplayName("BlackJackGame의 Player에게 최초 카드 두장을 준다.")
     void initializeBlackJackGame() {
-        BlackJackGame blackJackGame = initBlackJackGame();
-
-        Deck deck = Deck.init();
         blackJackGame.giveFirstCards(deck);
 
         assertThat(deck.size()).isEqualTo(46);
