cd4a447fd43838c5ae18a927a0dbf8cb1b138553
Chris <bjuuuu98@gmail.com>
2022-03-09 19:44:26 +0900

    refactor: 개행 및 변수명 수정

--- src/main/java/blackjack/domain/DrawCommand.java
+++ src/main/java/blackjack/domain/DrawCommand.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public enum DrawCommand {
+
     YES("y"),
     NO("n"),
     ;
@@ -14,10 +15,10 @@ public enum DrawCommand {
         this.value = value;
     }
 
-    public static DrawCommand from(final String input) {
-        Objects.requireNonNull(input, "커맨드에는 null이 들어올 수 없습니다.");
+    public static DrawCommand from(final String value) {
+        Objects.requireNonNull(value, "커맨드에는 null이 들어올 수 없습니다.");
         return Arrays.stream(values())
-                .filter(command -> command.value.equals(input))
+                .filter(command -> command.value.equals(value))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("없는 커맨드입니다."));
     }
