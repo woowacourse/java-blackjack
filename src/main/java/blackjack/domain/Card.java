c8e368938588fc3229099348faab04b8a9a0e760
swcho <progress0407@gmail.com>
2022-03-09 17:07:59 +0900

    test(Card): 테스트 케이스 추가 및 요구사항 문서 수정

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -1,5 +1,7 @@
 package blackjack.domain;
 
+import java.util.Objects;
+
 public class Card {
 
     private final Denomination denomination;
@@ -15,5 +17,30 @@ public class Card {
         if (denomination == null || suit == null) {
             throw new NullPointerException("카드를 생성할때 숫자와 무늬중 null이 존재합니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return denomination == card.denomination && suit == card.suit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(denomination, suit);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "denomination=" + denomination +
+                ", suit=" + suit +
+                '}';
     }
 }
