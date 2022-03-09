96883d846af890b1c3fe9552d35cb753d3fb65f3
jaejae-yoo <wotj102@gmail.com>
2022-03-09 16:48:36 +0900

    feat: 뷰에 전달할 DTO 객체 생성

---
+++ src/main/java/blackjack/domain/GameResponse.java
@@ -0,0 +1,22 @@
+package blackjack.domain;
+
+import java.util.List;
+
+public class GameResponse {
+
+    private final String name;
+    private final Deck deck;
+
+    public GameResponse(String name, Deck deck) {
+        this.name = name;
+        this.deck = deck;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+}
