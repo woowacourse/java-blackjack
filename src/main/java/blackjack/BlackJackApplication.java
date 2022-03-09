f1f6a0e38b5b9b1e4e43ca2f743833e306f4d62d
jayjaehunchoi <wogns0108@naver.com>
2022-03-09 17:37:33 +0900

    feat : 이름이 중복되는 경우 예외처리 기능 추가

--- src/main/java/blackjack/BlackJackApplication.java
+++ src/main/java/blackjack/BlackJackApplication.java
@@ -1,10 +1,39 @@
 package blackjack;
 
+import blackjack.domain.Gamer;
+import blackjack.domain.Player;
 import blackjack.view.InputView;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BlackJackApplication {
 
     public static void main(String[] args) {
-        InputView.requestPlayerName();
+        List<Player> gamers = createGamers();
+    }
+
+    private static List<Player> createGamers() {
+        try {
+            return toGamerList();
+        } catch (IllegalArgumentException e) {
+            return createGamers();
+        }
+    }
+
+    private static List<Player> toGamerList() {
+        List<String> names = InputView.requestPlayerName();
+        checkDuplicateName(names);
+        return names.stream()
+                .map(Gamer::new)
+                .collect(Collectors.toList());
+    }
+
+    private static void checkDuplicateName(final List<String> names) {
+        Set<String> tempSet = new HashSet<>(names);
+        if (names.size() != tempSet.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 이름은 입력할 수 없습니다.");
+        }
     }
 }
