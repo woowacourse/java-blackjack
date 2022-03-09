d27a998f420a0b75764eaa622f4acaefd3413429
Chris <bjuuuu98@gmail.com>
2022-03-09 20:41:09 +0900

    refactor: 출력문 수정

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private static final String INPUT_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-    private static final String INPUT_DRAW_COMMAND_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String INPUT_DRAW_COMMAND_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
 
     private static final String PLAYER_NAME_BLANK = " ";
     private static final String PLAYER_NAME_NOT_BLANK = "";
