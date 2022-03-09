d0f90b77d117553b523c7686948c5b9d99fa5d20
jayjaehunchoi <wogns0108@naver.com>
2022-03-09 16:56:32 +0900

    feat : 게임 참여자 이름 input 기능 추가

---
+++ src/main/java/blackjack/BlackJackApplication.java
@@ -0,0 +1,10 @@
+package blackjack;
+
+import blackjack.view.InputView;
+
+public class BlackJackApplication {
+
+    public static void main(String[] args) {
+        InputView.requestPlayerName();
+    }
+}
---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,30 @@
+package blackjack.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    private static final String REQUEST_PLAYER_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String DELIMITER = ",";
+    private static final String INPUT_BLANK = " ";
+    private static final String INPUT_NOT_BLANK = "";
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static List<String> requestPlayerName() {
+        System.out.println(REQUEST_PLAYER_NAME_MESSAGE);
+
+        return Arrays.stream(scanner.nextLine().split(DELIMITER))
+                .map(InputView::removeBlank)
+                .collect(Collectors.toList());
+    }
+
+    private static String removeBlank(final String name) {
+        return name.replaceAll(INPUT_BLANK, INPUT_NOT_BLANK);
+    }
+
+
+}
