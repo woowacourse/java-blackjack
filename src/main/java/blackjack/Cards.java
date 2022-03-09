975343bcb92b57cb50e1d831b55ca37f05b00ac3
wilgur513 <wilgur513@naver.com>
2022-03-09 17:23:29 +0900

    refactor: 점수 계산에 새로운 알고리즘 적용

--- src/main/java/blackjack/Cards.java
+++ src/main/java/blackjack/Cards.java
@@ -1,6 +1,12 @@
 package blackjack;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toUnmodifiableList;
+
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Cards {
 
@@ -11,26 +17,43 @@ public class Cards {
     }
 
     public Score score() {
-        if (hasMoreThanTwoAce()) {
-            List<Integer> temp = getPossibleScore();
-            return new Score(temp.stream()
-                    .filter(tmp -> !isBust(tmp))
-                    .mapToInt(Integer::intValue)
-                    .max().getAsInt());
-
-        }
-        return getOneAceScore();
+        List<Integer> temp = getPossibleScore();
+        return new Score(temp.stream()
+            .filter(tmp -> !isBust(tmp))
+            .mapToInt(Integer::intValue)
+            .max().orElse(hardHand()));
     }
 
     private List<Integer> getPossibleScore() {
-        return null;
+        return Stream.concat(Stream.of(hardHand()), softHands().stream())
+            .collect(toUnmodifiableList());
+    }
+
+    public int hardHand() {
+        int result = 0;
+        for (String card : cards) {
+            String value = card.substring(0, 1);
+            result += number(value, 1);
+        }
+        return result;
+    }
+
+    public List<Integer> softHands() {
+        return IntStream.rangeClosed(1, numberOfAce())
+            .map(value -> 10 * value + hardHand())
+            .boxed()
+            .collect(toList());
     }
 
     private boolean hasMoreThanTwoAce() {
-        return cards.stream()
-                .map(s -> s.substring(0, 1))
-                .filter(s -> s.equals("A"))
-                .count() >= 2;
+        return numberOfAce() >= 2;
+    }
+
+    private int numberOfAce() {
+        return (int) cards.stream()
+            .map(s -> s.substring(0, 1))
+            .filter(s -> s.equals("A"))
+            .count();
     }
 
     private Score getOneAceScore() {
