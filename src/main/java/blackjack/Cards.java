061254d4b176f98d34bb66578104fad2538ccc2b
YuJeong <yjfflq4872@gmail.com>
2022-03-09 17:33:09 +0900

    refactor: Cards 메소드 분리

--- src/main/java/blackjack/Cards.java
+++ src/main/java/blackjack/Cards.java
@@ -4,7 +4,6 @@ import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toUnmodifiableList;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -17,50 +16,42 @@ public class Cards {
     }
 
     public Score score() {
-        List<Integer> temp = getPossibleScore();
-        return new Score(temp.stream()
-            .filter(tmp -> !isBust(tmp))
-            .mapToInt(Integer::intValue)
-            .max().orElse(hardHand()));
+        return new Score(bestScore(getPossibleScore()));
+    }
+
+    private int bestScore(List<Integer> temp) {
+        return temp.stream()
+                .filter(tmp -> !isBust(tmp))
+                .mapToInt(Integer::intValue)
+                .max().orElse(hardHand());
     }
 
     private List<Integer> getPossibleScore() {
         return Stream.concat(Stream.of(hardHand()), softHands().stream())
-            .collect(toUnmodifiableList());
+                .collect(toUnmodifiableList());
     }
 
     public int hardHand() {
         int result = 0;
         for (String card : cards) {
             String value = card.substring(0, 1);
-            result += number(value, 1);
+            result += number(value);
         }
         return result;
     }
 
     public List<Integer> softHands() {
         return IntStream.rangeClosed(1, numberOfAce())
-            .map(value -> 10 * value + hardHand())
-            .boxed()
-            .collect(toList());
-    }
-
-    private boolean hasMoreThanTwoAce() {
-        return numberOfAce() >= 2;
+                .map(value -> 10 * value + hardHand())
+                .boxed()
+                .collect(toList());
     }
 
     private int numberOfAce() {
         return (int) cards.stream()
-            .map(s -> s.substring(0, 1))
-            .filter(s -> s.equals("A"))
-            .count();
-    }
-
-    private Score getOneAceScore() {
-        if (isBust(totalScore(11))) {
-            return new Score(totalScore(1));
-        }
-        return new Score(totalScore(11));
+                .map(s -> s.substring(0, 1))
+                .filter(s -> s.equals("A"))
+                .count();
     }
 
     private boolean isBust(int score) {
@@ -71,21 +62,12 @@ public class Cards {
         return score().isBust();
     }
 
-    private int totalScore(int aceValue) {
-        int score = 0;
-        for (String s : cards) {
-            String value = s.substring(0, 1);
-            score += number(value, aceValue);
-        }
-        return score;
-    }
-
-    private int number(String value, int aceValue) {
+    private int number(String value) {
         if (value.equals("J") || value.equals("Q") || value.equals("K")) {
             return 10;
         }
         if (value.equals("A")) {
-            return aceValue;
+            return 1;
         }
         return Integer.parseInt(value);
     }
