bd394f5713e63e6938d29ffa9a3157acf0260867
Juhyung990122 <harukawa99@naver.com>
2022-03-09 20:52:28 +0900

    feat: player 생성 및 카드 분배 기능 구현

--- src/main/java/BlackJack/Application.java
+++ src/main/java/BlackJack/Application.java
@@ -1,12 +1,19 @@
 package BlackJack;
 
+import BlackJack.controller.BlackjackController;
+import BlackJack.dto.UserDto;
 import BlackJack.view.InputView;
+import BlackJack.view.OutputView;
 
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        BlackjackController blackjackController = new BlackjackController();
         List<String> inputPlayerNames = InputView.inputPlayerNames();
+        List<UserDto> userDtos = blackjackController.joinGame(inputPlayerNames);
+        OutputView.printDrawMessage(inputPlayerNames);
+        OutputView.printTotalUserCards(userDtos);
     }
 
 }
--- src/main/java/BlackJack/controller/BlackjackController.java
+++ src/main/java/BlackJack/controller/BlackjackController.java
@@ -1,2 +1,29 @@
-package BlackJack.controller;public class BlackjackController {
+package BlackJack.controller;
+
+import BlackJack.domain.Dealer;
+import BlackJack.domain.Player;
+import BlackJack.dto.UserDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlackjackController {
+
+    public List<UserDto> joinGame(List<String> inputPlayerNames) {
+        Dealer dealer = new Dealer("딜러");
+        List<Player> players = new ArrayList<>();
+        for (String name : inputPlayerNames) {
+            players.add(new Player(name));
+        }
+        return convertToDto(dealer, players);
+    }
+
+    private List<UserDto> convertToDto(Dealer dealer, List<Player> players) {
+        List<UserDto> userDtos = new ArrayList<>();
+        userDtos.add(UserDto.from(dealer));
+        for (Player player : players) {
+            userDtos.add(UserDto.from(player));
+        }
+        return userDtos;
+    }
 }
