cf4c86b12a72838d563d74875dffcf69efa4d29d
sun <syoun602@gmail.com>
2022-03-09 17:37:12 +0900

    feat: 참여자 추상화

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -1,11 +1,7 @@
 package blackjack.domain;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Player extends Participant {
 
-public class Player {
-
-    private final List<Card> cards = new ArrayList<>();
     private final String name;
 
     public Player(final String name) {
@@ -17,13 +13,5 @@ public class Player {
         if (name.isBlank()) {
             throw new IllegalArgumentException("플레이어명은 공백이 될 수 없습니다.");
         }
-    }
-
-    public void drawCard(final Deck deck) {
-        cards.add(deck.drawCard());
-    }
-
-    public List<Card> getCards() {
-        return List.copyOf(cards);
     }
 }
