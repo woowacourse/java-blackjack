990e96d8fc91091730b4c0bb4006c0d72342fbfa
bomi <bonita9931@gmail.com>
2022-03-10 16:25:58 +0900

    feat: 카드의 합이 21이 넘을 경우 패배

--- src/main/java/blackjack/Outcome.java
+++ src/main/java/blackjack/Outcome.java
@@ -3,7 +3,18 @@ package blackjack;
 public enum Outcome {
     WIN, LOSE, DRAW;
 
+    private static final int BLACK_JACK_NUMBER = 21;
+
     public static Outcome match(int dealerTotal, int playerTotal) {
+        if (dealerTotal > BLACK_JACK_NUMBER && playerTotal > BLACK_JACK_NUMBER) {
+            return DRAW;
+        }
+        if (dealerTotal > BLACK_JACK_NUMBER) {
+            return WIN;
+        }
+        if (playerTotal > BLACK_JACK_NUMBER) {
+            return LOSE;
+        }
         if (dealerTotal < playerTotal) {
             return WIN;
         }
--- src/test/java/blackjack/OutcomeTest.java
+++ src/test/java/blackjack/OutcomeTest.java
@@ -28,4 +28,24 @@ class OutcomeTest {
         assertThat(match).isEqualTo(Outcome.DRAW);
     }
 
+    @Test
+    @DisplayName("플레이어의 합이 21이 넘는 경우 패배를 반환한다.")
+    void playerIsLoseByOver21() {
+        Outcome match = Outcome.match(21, 22);
+        assertThat(match).isEqualTo(Outcome.LOSE);
+    }
+
+    @Test
+    @DisplayName("딜러의 합이 21이 넘는 경우 승리를 반환한다.")
+    void playerIsWinByDealerOver21() {
+        Outcome match = Outcome.match(22, 21);
+        assertThat(match).isEqualTo(Outcome.WIN);
+    }
+
+    @Test
+    @DisplayName("플레이어와 딜러 모두 21이 넘는 경우 무승부를 반환한다.")
+    void playerIsDrawByDealerAndPlayerOver21() {
+        Outcome match = Outcome.match(22, 22);
+        assertThat(match).isEqualTo(Outcome.DRAW);
+    }
 }
