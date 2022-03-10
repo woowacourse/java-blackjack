e4e796abc3d8587414f8243c75140b2bd875712b
Juhyung990122 <harukawa99@naver.com>
2022-03-10 16:31:39 +0900

    refactor: 접근제어자 수정

--- src/main/java/BlackJack/view/InputView.java
+++ src/main/java/BlackJack/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
 
     private static final String ONE_MORE_CARD_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
     private static final String DELIMITER = ",";
-    public static final String INPUT_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String INPUT_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
 
     private static String input(){
         Scanner scanner = new Scanner(System.in);
