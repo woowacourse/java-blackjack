60a6bf709f97a2d478c92d1c895192e1ea1c4bf5
SuyeonChoi <tndus970120@gmail.com>
2022-03-10 16:02:08 +0900

    feat: 플레이어 이름길이 검증기능 추가

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -1,14 +1,52 @@
 package blackjack.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player extends Gamer{
+    private static final String NAME_LENGTH_ERROR_MESSAGE = "이름은 최소 1글자, 최대 6글자로 입력해야 합니다.";
+    private static final String NAME_EMPTY_ERROR_MESSAGE = "빈 문자는 이름으로 입력할 수 없습니다.";
+    private static final int MINIMUM_NAME_LENGTH = 1;
+    private static final int MAXIMUM_NAME_LENGTH = 6;
+
     private final String name;
 
     public Player(String name) {
+        validateName(name);
         this.name = name;
     }
 
-    public String getName() {
-        return this.name;
+    public static List<Player> of(List<String> playerNames) {
+        List<Player> players = new ArrayList<>();
+        for (String playerName : playerNames) {
+            players.add(new Player(playerName));
+        }
+        return players;
+    }
+    
+    private void validateName(String name) {
+        validateEmpty(name);
+        validateLength(name);
+    }
+
+    private void validateEmpty(String name) {
+        if (isEmpty(name)) {
+            throw new IllegalArgumentException(NAME_EMPTY_ERROR_MESSAGE);
+        }
+    }
+
+    private boolean isEmpty(String name) {
+        return name == null || name.isBlank();
+    }
+
+    private void validateLength(String name) {
+        if (isProperLength(name)) {
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
+        }
+    }
+
+    private boolean isProperLength(String name) {
+        return name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH;
     }
 
     public int compareCardsSumTo(int sum) {
@@ -17,5 +55,9 @@ public class Player extends Gamer{
         }
 
         return Integer.compare(this.getCardGroupSum(), sum);
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
---
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -0,0 +1,28 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class PlayerTest {
+
+    @ParameterizedTest(name = "{index}: {1}")
+    @MethodSource("invalidParameters")
+    @DisplayName("플레이어 생성 오류 테스트")
+    void playerInvalidTest(List<String> playerNames, String testName) {
+        assertThatThrownBy(() -> Player.of(playerNames))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> invalidParameters() {
+        return Stream.of(
+                Arguments.of(List.of("pobi", "ash", ""), "빈문자 입력"),
+                Arguments.of(List.of("pobiash", "pobi"), "6글자 초과 입력")
+        );
+    }
+}
