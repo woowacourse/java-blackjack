8750ed407922c7f04d95907ab09bc7274a336d6e
Juhyung990122 <harukawa99@naver.com>
2022-03-10 15:18:34 +0900

    feat: 카드 추가 질의 기능 구현

--- src/main/java/BlackJack/view/InputView.java
+++ src/main/java/BlackJack/view/InputView.java
@@ -1,11 +1,15 @@
 package BlackJack.view;
 
+import BlackJack.dto.UserDto;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class InputView {
+
+    private static final String ONE_MORE_CARD_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
     private static String input(){
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
@@ -15,5 +19,10 @@ public class InputView {
         String[] input = input().split(",");
         return Arrays.stream(input)
                 .collect(Collectors.toList());
+    }
+
+    public static boolean askOneMoreCard(UserDto player) {
+        System.out.printf(ONE_MORE_CARD_MESSAGE,player.getName());
+        return "y".equals(input());
     }
 }
