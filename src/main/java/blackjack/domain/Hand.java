f4186b42994914806f578f6155dc7569643dcd2c
Kim Seunglae <obey1342@gmail.com>
2022-03-11 03:02:30 +0900

    refactor(Hand): 카드패의 점수 구하는 로직 수정

--- src/main/java/blackjack/domain/Hand.java
+++ src/main/java/blackjack/domain/Hand.java
@@ -2,15 +2,12 @@ package blackjack.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class Hand {
 
     private static final int BLACKJACK_CARD_SIZE = 2;
     private static final int BLACKJACK_SYMBOL_SCORE = 21;
     private static final int ACE_UPPER_SCORE = 11;
-    private static final int ACE_LOWER_SCORE = 1;
-    private static final int ACE_SCORE_DIFFERENCE = ACE_UPPER_SCORE - ACE_LOWER_SCORE;
 
     private List<Card> cards = new ArrayList<>();
 
@@ -23,31 +20,15 @@ public class Hand {
     }
 
     public int getScore() {
-        if (hasAce()) {
-            return getSumIncludingOnlyAce();
-        }
-
-        return getSumExcludingAce();
-    }
-
-    private int getSumIncludingOnlyAce() {
         int countOfAce = findCountOfAce();
         int sumOfNoAce = getSumExcludingAce() + (countOfAce * ACE_UPPER_SCORE);
 
-        List<Integer> sums = new ArrayList<>();
-        for (int aceCount = countOfAce - 1; aceCount >= 0; aceCount--) {
-            sums.add(sumOfNoAce - (aceCount * ACE_SCORE_DIFFERENCE));
+        while (countOfAce > 0 && BLACKJACK_SYMBOL_SCORE < sumOfNoAce) {
+            sumOfNoAce -= 10;
+            countOfAce--;
         }
 
-        return findLargestSum(sums, summation -> summation <= BLACKJACK_SYMBOL_SCORE);
-    }
-
-    private int findLargestSum(List<Integer> sums, Predicate<Integer> condition) {
-        return sums.stream()
-                .filter(condition)
-                .mapToInt(value -> value)
-                .max()
-                .getAsInt();
+        return sumOfNoAce;
     }
 
     private int getSumExcludingAce() {
@@ -57,14 +38,10 @@ public class Hand {
                 .sum();
     }
 
-    private boolean hasAce() {
-        return findCountOfAce() > 0;
-    }
-
     private int findCountOfAce() {
-        return (int) cards.stream()
-                .filter(Card::isAce)
-                .count();
+        return Math.toIntExact(cards.stream()
+            .filter(Card::isAce)
+            .count());
     }
 
     public boolean isBust() {
