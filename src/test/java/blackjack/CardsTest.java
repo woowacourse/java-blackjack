44936b37da96f98e20fbaf62b840176746538a62
YuJeong <yjfflq4872@gmail.com>
2022-03-10 17:19:19 +0900

    test: SoftHand 테스트 구현

--- src/test/java/blackjack/CardsTest.java
+++ src/test/java/blackjack/CardsTest.java
@@ -1,45 +1,61 @@
 package blackjack;
 
-import static blackjack.Rank.*;
-import static blackjack.Suit.*;
+import static blackjack.Rank.ACE;
+import static blackjack.Rank.JACK;
+import static blackjack.Rank.KING;
+import static blackjack.Rank.NINE;
+import static blackjack.Rank.QUEEN;
+import static blackjack.Rank.THREE;
+import static blackjack.Rank.TWO;
+import static blackjack.Suit.CLOVER;
+import static blackjack.Suit.DIAMOND;
+import static blackjack.Suit.HEART;
+import static blackjack.Suit.SPADE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import blackjack.cards.Cards;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class CardsTest {
 
-    @DisplayName("카드 합 계산")
-    @Test
-    void sumCard() {
-        Cards cards = Cards.mixHandCards(new Card(THREE, DIAMOND), new Card(TWO, DIAMOND));
-        assertThat(cards.score()).isEqualTo(new Score(5));
+    @ParameterizedTest
+    @MethodSource("provideMixHandCards")
+    @DisplayName("MixHand 카드 점수 계산")
+    void mixHandScore(Cards cards, int expect) {
+        assertThat(cards.score()).isEqualTo(new Score(expect));
     }
 
-    @Test
-    void J_Q_K_카드_점수_계산() {
-        Cards cards = Cards.mixHandCards(new Card(QUEEN, CLOVER), new Card(JACK, HEART), new Card(KING, DIAMOND));
-        assertThat(cards.score()).isEqualTo(new Score(30));
+    protected static Stream<Arguments> provideMixHandCards() {
+        return Stream.of(
+                Arguments.of(Cards.mixHandCards(new Card(ACE, SPADE), new Card(JACK, HEART)), 21),
+                Arguments.of(Cards.mixHandCards(new Card(ACE, DIAMOND), new Card(JACK, DIAMOND),
+                        new Card(KING, CLOVER)), 21),
+                Arguments.of(Cards.mixHandCards(new Card(ACE, DIAMOND), new Card(ACE, SPADE),
+                        new Card(NINE, CLOVER)), 21),
+                Arguments.of(Cards.mixHandCards(new Card(QUEEN, CLOVER), new Card(JACK, HEART),
+                        new Card(KING, DIAMOND)), 30),
+                Arguments.of(Cards.mixHandCards(new Card(THREE, DIAMOND), new Card(TWO, DIAMOND)), 5)
+        );
     }
 
     @ParameterizedTest
-    @MethodSource("provideAceData")
-    void ACE_카드_점수_계산(Cards cards) {
-        assertThat(cards.score()).isEqualTo(new Score(21));
+    @MethodSource("provideSoftHandCards")
+    @DisplayName("SoftHand 카드 점수 계산")
+    void softHandScore(Cards cards, int expect) {
+        assertThat(cards.score()).isEqualTo(new Score(expect));
     }
 
-    protected static Stream<Arguments> provideAceData() {
+    protected static Stream<Arguments> provideSoftHandCards() {
         return Stream.of(
-                Arguments.of(Cards.mixHandCards(new Card(ACE, SPADE), new Card(JACK, HEART))),
-                Arguments.of(Cards.mixHandCards(new Card(ACE, DIAMOND), new Card(JACK, DIAMOND),
-                    new Card(KING, CLOVER))),
-                Arguments.of(Cards.mixHandCards(new Card(ACE, DIAMOND), new Card(ACE, SPADE),
-                    new Card(NINE, CLOVER)))
+                Arguments.of(Cards.softHandCards(new Card(ACE, SPADE), new Card(JACK, HEART)), 21),
+                Arguments.of(Cards.softHandCards(new Card(ACE, DIAMOND), new Card(JACK, DIAMOND),
+                        new Card(KING, CLOVER)), 31),
+                Arguments.of(Cards.softHandCards(new Card(ACE, DIAMOND), new Card(ACE, SPADE),
+                        new Card(NINE, CLOVER)), 31)
         );
     }
 }
