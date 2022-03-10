4dc9cfe739389762153f96bc0a72aa002f5e6d40
yukong <kyb062666@gmail.com>
2022-03-10 20:22:28 +0900

    refactor(OutputView): 재사용하지 않는 문자열 코드로 이동
    - 재사용하지 않으며 문자열 그 자체로 의미를 분명하게 나타내므로 선언 위치 이동

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -5,16 +5,7 @@ import java.util.List;
 public class OutputView {
 
     private static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String PLAYER_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-    private static final String GIVE_INIT_CARD_MESSAGE = "\n%s와 %s에게 2장씩 나누었습니다.\n";
     private static final String DELIMITER = ", ";
-    private static final String CARD_FORMAT = "%s카드: %s";
-    private static final String TAKE_CARD_INSTRUCTION = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
-    private static final String TAKE_DEALER_CARD_MESSAGE = "%s는 %d이하라 한장의 카드를 더 받았습니다.";
-    private static final String RESULT_FORMAT = " - 결과: %d";
-    private static final String WINNER_TITLE = "## 최종 승패";
-    private static final String DEALER_SCORE_FORMAT = "%s: %d%s %d%s";
-    private static final String PLAYER_SCORE_FORMAT = "%s: %s";
     private static final String WIN = "승";
     private static final String LOSE = "패";
 
@@ -30,42 +21,45 @@ public class OutputView {
     }
 
     public static void printPlayerNameInstruction() {
-        System.out.println(PLAYER_NAME_MESSAGE);
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
     }
 
     public static void printReceiveInitCardMessage(final String dealerName, final List<String> playerNames) {
-        System.out.printf(GIVE_INIT_CARD_MESSAGE, dealerName, String.join(DELIMITER, playerNames));
+        printNewLine();
+        System.out.printf("%s와 %s에게 2장씩 나누었습니다.", dealerName, String.join(DELIMITER, playerNames));
+        printNewLine();
     }
 
     public static void printCard(final String name, final List<String> cards) {
-        System.out.printf(CARD_FORMAT, name, String.join(DELIMITER, cards));
+        System.out.printf("%s카드: %s", name, String.join(DELIMITER, cards));
     }
 
     public static void printTakeCardInstruction(final String name) {
-        System.out.printf(TAKE_CARD_INSTRUCTION, name);
+        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)", name);
+        printNewLine();
     }
 
     public static void printTakeDealerCardsMessage(final String name, final int maximum) {
-        System.out.printf(TAKE_DEALER_CARD_MESSAGE, name, maximum);
+        System.out.printf("%s는 %d이하라 한장의 카드를 더 받았습니다.", name, maximum);
         printNewLine();
     }
 
     public static void printResult(final String name, final List<String> cards, final int total) {
         printCard(name, cards);
-        System.out.printf(RESULT_FORMAT, total);
+        System.out.printf(" - 결과: %d", total);
     }
 
     public static void printWinnerTitle() {
-        System.out.println(WINNER_TITLE);
+        System.out.println("## 최종 승패");
     }
 
     public static void printDealerScore(final String name, final int winPlayersCount, final int losePlayersCount) {
-        System.out.printf(DEALER_SCORE_FORMAT, name, winPlayersCount, WIN, losePlayersCount, LOSE);
+        System.out.printf("%s: %d%s %d%s", name, winPlayersCount, WIN, losePlayersCount, LOSE);
         printNewLine();
     }
 
     public static void printPlayerScore(final String name, final boolean result) {
-        System.out.printf(PLAYER_SCORE_FORMAT, name, getResult(result));
+        System.out.printf("%s: %s", name, getResult(result));
         printNewLine();
     }
 
