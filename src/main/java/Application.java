1d798b838e2b397cc31f52a7592ccdee72362cc2
kimchan <gmelddl514@gmail.com>
2022-03-10 15:55:45 +0900

    feat : Controller 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import controller.BlackJackController;
+
+public class Application {
+    public static void main(String[] args) {
+        BlackJackController blackJackController = new BlackJackController();
+        blackJackController.run();
+    }
+}
