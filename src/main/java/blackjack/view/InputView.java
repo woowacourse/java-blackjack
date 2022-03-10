826044111afa46567fff6a96800b9dd0203e8ee0
devHudi <devhudi@gmail.com>
2022-03-10 15:39:16 +0900

    feat(InputView): 카드 추가 지급 여부를 입력받는 기능 구현

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -8,7 +8,12 @@ import java.util.stream.Stream;
 public class InputView {
 
     private static final String REQUEST_PLAYER_NAMES_INPUT_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String REQUEST_MORE_PLAYER_CARD_INPUT_TEXT = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String INVALID_MORE_PLAYER_CARD_INPUT_EXCEPTION_MESSAGE = "y 혹은 n만 입력이 가능합니다.";
+
     private static final String NAME_INPUT_DELIMITER = ",";
+    private static final String YES_TEXT = "y";
+    private static final String NO_TEXT = "n";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -27,5 +32,19 @@ public class InputView {
 
     private static void print(String text) {
         System.out.println(text);
+    }
+
+    public static boolean requestMorePlayerCardInput(String playerName) {
+        print(playerName + REQUEST_MORE_PLAYER_CARD_INPUT_TEXT);
+        String input = scanner.nextLine();
+
+        validateInvalidMoreCardInput(input);
+        return input.equals(YES_TEXT);
+    }
+
+    private static void validateInvalidMoreCardInput(String input) {
+        if (!input.equals(YES_TEXT) && !input.equals(NO_TEXT)) {
+            throw new IllegalArgumentException(INVALID_MORE_PLAYER_CARD_INPUT_EXCEPTION_MESSAGE);
+        }
     }
 }
