62de3ab64ad178bbeb7ab875354789a620824a56
Ohzzi <clover7even@naver.com>
2022-03-10 16:28:35 +0900

    fix: 카드 덱 생성 시 카드를 섞도록 수정

--- src/main/java/blackjack/domain/card/BlackJackCardsGenerator.java
+++ src/main/java/blackjack/domain/card/BlackJackCardsGenerator.java
@@ -2,6 +2,7 @@ package blackjack.domain.card;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,10 +10,12 @@ public class BlackJackCardsGenerator implements CardsGenerator {
 
     @Override
     public List<Card> generate() {
-        return Arrays.stream(Pattern.values())
+        List<Card> cards = Arrays.stream(Pattern.values())
                 .map(this::createCardsBy)
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
+        Collections.shuffle(cards);
+        return cards;
     }
 
     private List<Card> createCardsBy(Pattern pattern) {
