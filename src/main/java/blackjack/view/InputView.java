d7c9fe5776ce696693f637c4d998b0e3ddb08c5f
brorae <jhr3239@gmail.com>
2022-03-11 00:32:30 +0900

    feat: 플레이어가 카드를 더 받을 것인지 물어보는 기능 구현

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,5 +1,6 @@
 package blackjack.view;
 
+import blackjack.domain.player.Player;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -9,6 +10,10 @@ public class InputView {
 
     private static final String USER_NAME_EXCEPTION_MESSAGE = "유저의 이름이 잘못되었습니다.";
     private static final String GAME_START_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String YES = "y";
+    private static final String NO = "n";
+    private static final String DRAW_CARD_MESSAGE = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String ANSWER_EXCEPTION_MESSAGE = "잘못된 입력입니다.";
 
     private static Scanner scanner = new Scanner(System.in);
 
@@ -26,5 +31,18 @@ public class InputView {
         }
         return Arrays.stream(usersName)
                 .collect(Collectors.toList());
+    }
+
+    public static String inputDrawCardAnswer(Player user) {
+        System.out.println(user.getName() + DRAW_CARD_MESSAGE);
+        String answer = scanner.nextLine();
+        return validateDrawCardAnswer(answer);
+    }
+
+    private static String validateDrawCardAnswer(String answer) {
+        if (!answer.equals(YES) && !answer.equals(NO)) {
+            throw new IllegalArgumentException(ANSWER_EXCEPTION_MESSAGE);
+        }
+        return answer;
     }
 }
