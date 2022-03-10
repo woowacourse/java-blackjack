c90b36685a46d2696fa2169686a576ceb3f1b276
YuJeong <yjfflq4872@gmail.com>
2022-03-10 20:14:11 +0900

    test: Gamer 점수 반환 테스트 구현

--- src/test/java/blackjack/GamerTest.java
+++ src/test/java/blackjack/GamerTest.java
@@ -4,8 +4,14 @@ import static blackjack.Rank.*;
 import static blackjack.Suit.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import blackjack.cards.Cards;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class GamerTest {
 
@@ -31,7 +37,25 @@ class GamerTest {
         assertThat(gamer.openCards()).contains(new Card(QUEEN, CLOVER), new Card(FIVE, HEART));
     }
 
-    // 게이머 점수(score)
+    @ParameterizedTest
+    @MethodSource("provideGamerInfo")
+    @DisplayName("게이머 점수 반환 테스트")
+    void gamerScore(Gamer gamer, int expect) {
+        assertThat(gamer.score().getValue()).isEqualTo(expect);
+    }
+
+    protected static Stream<Arguments> provideGamerInfo() {
+        return Stream.of(
+                Arguments.of(new Gamer("player", new Card(ACE, SPADE), new Card(JACK, HEART)), 21),
+                Arguments.of(new Gamer("player", new Card(ACE, DIAMOND), new Card(JACK, DIAMOND),
+                        new Card(KING, CLOVER)), 21),
+                Arguments.of(new Gamer("player", new Card(ACE, DIAMOND), new Card(ACE, SPADE),
+                        new Card(NINE, CLOVER)), 21),
+                Arguments.of(new Gamer("player", new Card(QUEEN, CLOVER), new Card(JACK, HEART),
+                        new Card(KING, DIAMOND)), 30),
+                Arguments.of(new Gamer("player", new Card(THREE, DIAMOND), new Card(TWO, DIAMOND)), 5)
+        );
+    }
     // 게이머 카드 발급
     // 게이머 카드 발급 실패(예외)
 }
