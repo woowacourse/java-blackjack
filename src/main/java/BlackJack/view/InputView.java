002889ef6ecdf905307c008712499a8494acb128
Juhyung990122 <harukawa99@naver.com>
2022-03-10 15:21:27 +0900

    chore: 안내메세지 추가

--- src/main/java/BlackJack/view/InputView.java
+++ src/main/java/BlackJack/view/InputView.java
@@ -10,13 +10,17 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private static final String ONE_MORE_CARD_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
+    private static final String DELIMITER = ",";
+    public static final String INPUT_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+
     private static String input(){
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
     public static List<String> inputPlayerNames(){
-        String[] input = input().split(",");
+        System.out.println(INPUT_PLAYER_NAMES_MESSAGE);
+        String[] input = input().split(DELIMITER);
         return Arrays.stream(input)
                 .collect(Collectors.toList());
     }
