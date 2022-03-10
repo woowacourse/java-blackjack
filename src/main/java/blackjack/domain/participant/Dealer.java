05b8c4a8f8dad6b40f5847579e61b0da74432c11
Chris <bjuuuu98@gmail.com>
2022-03-10 17:23:38 +0900

    refactor: 딜러의 게임 진행 상태 관리 로직 수정

--- src/main/java/blackjack/domain/participant/Dealer.java
+++ src/main/java/blackjack/domain/participant/Dealer.java
@@ -2,6 +2,7 @@ package blackjack.domain.participant;
 
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Cards;
+import blackjack.domain.state.State;
 import java.util.List;
 
 public class Dealer {
@@ -9,38 +10,38 @@ public class Dealer {
     private static final String NAME = "딜러";
     private static final int DEALER_LIMIT_SCORE = 17;
 
-    private final Cards cards;
+    private State state;
 
-    private Dealer(final Cards cards) {
-        this.cards = cards;
+    private Dealer(final State state) {
+        this.state = state;
     }
 
     public Dealer(final List<Card> cards) {
-        this(new Cards(cards));
+        this(State.create(new Cards(cards)));
     }
 
     public List<Card> initCards() {
-        return cards.firstCard();
+        return state.cards().firstCard();
     }
 
     public boolean isEnd() {
-        return cards.calculateScore() >= DEALER_LIMIT_SCORE;
+        return state.cards().calculateScore() >= DEALER_LIMIT_SCORE;
     }
 
     public void draw(final Card card) {
         validateLimitScore();
-        cards.addCard(card);
+        state = state.draw(card);
     }
 
     private void validateLimitScore() {
-        if (cards.calculateScore() >= DEALER_LIMIT_SCORE) {
+        if (state.cards().calculateScore() >= DEALER_LIMIT_SCORE) {
             throw new IllegalStateException("딜러 카드가 이미 17이상입니다.");
         }
     }
 
     public int calculateResultScore() {
         validateCanCalculateResultScore();
-        return cards.calculateScore();
+        return state.cards().calculateScore();
     }
 
     private void validateCanCalculateResultScore() {
@@ -51,7 +52,7 @@ public class Dealer {
 
     public List<Card> getCards() {
         validateEndTurn();
-        return cards.values();
+        return state.cards().values();
     }
 
     private void validateEndTurn() {
