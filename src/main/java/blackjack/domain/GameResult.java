f3a9ded62bd2785243c60653317dbab14739c146
Hyunta <mohai2618@gmail.com>
2022-03-10 19:12:59 +0900

    feat(GameResult): 게임 승패를 관리하는 enum 생성

---
+++ src/main/java/blackjack/domain/GameResult.java
@@ -0,0 +1,13 @@
+package blackjack.domain;
+
+public enum GameResult {
+    WIN("승"),
+    DRAW("무"),
+    LOSE("패");
+
+    private final String result;
+
+    GameResult(String result) {
+        this.result = result;
+    }
+}
