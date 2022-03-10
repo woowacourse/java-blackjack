fe22404d6a94754a5f95aa36c04f0d305acf5ce7
Hyunta <mohai2618@gmail.com>
2022-03-10 16:45:30 +0900

    feat(Player): 플레이어 생성시 이름을 요청 및 검증

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -7,8 +7,15 @@ public class Player extends Participant {
     private final HoldingCard holdingCard;
 
     public Player(String name, List<Card> cards) {
+        validateEmptyName(name);
         this.name = name;
         this.holdingCard = new HoldingCard(cards);
+    }
+
+    private void validateEmptyName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 빈 이름이 있습니다.");
+        }
     }
 
     public boolean isFinished() {
---
+++ src/main/java/blackjack/domain/controller/BlackJackController.java
@@ -0,0 +1,22 @@
+package blackjack.domain.controller;
+
+import blackjack.domain.BlackJackGame;
+import blackjack.domain.GameMachine;
+import blackjack.view.InputView;
+import blackjack.view.OutputView;
+
+public class BlackJackController {
+
+    public void run() {
+        BlackJackGame blackJackGame = initBlackJackGame();
+    }
+
+    private BlackJackGame initBlackJackGame() {
+        try {
+            return GameMachine.createBlackJackGame(InputView.askPlayerNames());
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+            return initBlackJackGame();
+        }
+    }
+}
