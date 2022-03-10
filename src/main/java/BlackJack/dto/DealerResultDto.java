e76aa89dd63c4f06ad0d20f18fa45a8ba9c649cf
Juhyung990122 <harukawa99@naver.com>
2022-03-10 19:23:32 +0900

    feat: 결과 DTO 생성

---
+++ src/main/java/BlackJack/dto/DealerResultDto.java
@@ -0,0 +1,37 @@
+package BlackJack.dto;
+
+public class DealerResultDto {
+
+    private String name;
+    private int dealerLoseCount = 0;
+    private int dealerDrawCount = 0;
+    private int dealerWinCount = 0;
+
+    public DealerResultDto(String name, int dealerLoseCount, int dealerDrawCount, int dealerWinCount) {
+        this.name = name;
+        this.dealerDrawCount = dealerDrawCount;
+        this.dealerLoseCount = dealerLoseCount;
+        this.dealerWinCount = dealerWinCount;
+    }
+
+    public static DealerResultDto from(String name, int dealerLoseCount, int dealerDrawCount, int dealerWinCount) {
+        return new DealerResultDto(name,dealerLoseCount,dealerDrawCount, dealerWinCount);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDealerDrawCount() {
+        return dealerDrawCount;
+    }
+
+    public int getDealerLoseCount() {
+        return dealerLoseCount;
+    }
+
+    public int getDealerWinCount(){
+        return dealerWinCount;
+    }
+
+}
---
+++ src/main/java/BlackJack/dto/PlayerResultDto.java
@@ -0,0 +1,27 @@
+package BlackJack.dto;
+
+import BlackJack.domain.Result;
+
+public class PlayerResultDto {
+
+    private String name;
+    private String result;
+
+    public PlayerResultDto(String name, Result result) {
+        this.name = name;
+        this.result = result.getValue();
+    }
+
+    public static PlayerResultDto from(String name, Result compare) {
+        return new PlayerResultDto(name,compare);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
+
