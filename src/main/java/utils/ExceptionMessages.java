5b40e03f4a71ebd4ff043fc932178debf61f29dd
RunaNam <nmj5012@naver.com>
2022-03-10 18:50:36 +0900

    style(ExceptionMessages): 의미없는 공백 제거

--- src/main/java/utils/ExceptionMessages.java
+++ src/main/java/utils/ExceptionMessages.java
@@ -5,5 +5,4 @@ public class ExceptionMessages {
     public static final String EMPTY_NAME_ERROR = "[ERROR] 이름은 공백이 될 수 없습니다.";
     public static final String OVER_CARD_LIMIT_ERROR = "[ERROR] 카드의 합이 21이 넘게되면 더이상 카드를 뽑을 수 없습니다.";
 
-
 }
