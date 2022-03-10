69d9492490dfd9c9e88dfb7aca8dc7f70e2c6f2d
hyunrrr <whgusrms96@gmail.com>
2022-03-11 02:53:06 +0900

    feat: 각 카드 유일성 보장하는 기능 추가

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -1,14 +1,60 @@
 package blackjack.domain;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Card {
+    private static final String FILE_PATH = "src/main/java/blackjack/domain/CardType";
+    private static final String FILE_IO_EXCEPTION = "[ERROR] 파일 입력 에러";
+    private static final String FILE_CONTENT_DELIMITER = " ";
+    private static final String NO_AVAILABLE_CARD_EXCEPTION = "[ERROR] 더 이상 카드를 뽑을 수 없습니다.";
+    private static final int NAME_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+    private static final List<Card> CACHE = new ArrayList<>();
+    private static final List<Boolean> IS_PICKED = new ArrayList<>();
+
     private final String name;
     private final int number;
+
+    static {
+        try {
+            File file = new File(FILE_PATH);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufReader.readLine()) != null) {
+                List<String> nameAndValue = List.of(line.split(FILE_CONTENT_DELIMITER));
+                CACHE.add(new Card(nameAndValue.get(NAME_INDEX), Integer.parseInt(nameAndValue.get(VALUE_INDEX))));
+                IS_PICKED.add(false);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(FILE_IO_EXCEPTION);
+        }
+    }
 
     public Card(String name, int number) {
         this.name = name;
         this.number = number;
+    }
+
+    public static Card randomPick(NumberGenerator numberGenerator) {
+        validateCardRemain();
+        int cardIndex = numberGenerator.generateNumber();
+        while (Boolean.TRUE.equals(IS_PICKED.get(cardIndex))) {
+            cardIndex = numberGenerator.generateNumber();
+        }
+        IS_PICKED.set(cardIndex, true);
+        return CACHE.get(cardIndex);
+    }
+
+    private static void validateCardRemain() {
+        if (!IS_PICKED.contains(false)) {
+            throw new RuntimeException(NO_AVAILABLE_CARD_EXCEPTION);
+        }
     }
 
     public String getName() {
--- src/test/java/blackjack/domain/CardTest.java
+++ src/test/java/blackjack/domain/CardTest.java
@@ -1,17 +1,37 @@
 package blackjack.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 
 import blackjack.domain.Card;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import jdk.jshell.spi.ExecutionControl.RunException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CardTest {
-    @DisplayName("카드 생성자 테스트")
+    @DisplayName("카드 중복생성 안되는지 테스트")
     @Test
-    void construct() {
-        assertDoesNotThrow(() -> new Card("3다이아몬드", 3));
+    void duplicatedCardTest() {
+        List<Card> cards = new ArrayList<>();
+        boolean duplicatedCardExist = false;
+        for (int i = 0; i<48; ++i) {
+            Card card = Card.randomPick(new RandomNumberGenerator());
+            duplicatedCardExist |= cards.contains(card);
+            cards.add(card);
+        }
+        assertThat(duplicatedCardExist).isFalse();
     }
 
+    @DisplayName("카드 초과생성 방지기능 테스트")
+    @Test
+    void cardConstructOverLimitTest() {
+        for (int i = 0; i<48; ++i) {
+            Card.randomPick(new RandomNumberGenerator());
+        }
+        assertThatThrownBy(() -> Card.randomPick(new RandomNumberGenerator()))
+                .isInstanceOf(RuntimeException.class);
+    }
 }
