b8fc924f173f75964b7f7c787c0524dd7f2464b4
HyeonbinSa <shb1833@gmail.com>
2022-03-10 21:02:34 +0900

    refactor: PlayesrTest 테스트 코드 수정 - CardDeck 객체 이용

--- src/test/java/blackjack/domain/human/PlayersTest.java
+++ src/test/java/blackjack/domain/human/PlayersTest.java
@@ -2,26 +2,32 @@ package blackjack.domain.human;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import blackjack.domain.card.CardDeck;
+import blackjack.domain.card.CardDeckGenerator;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("NonAsciiCharacters")
 class PlayersTest {
 
     @Test
-    public void 생성() {
+    @DisplayName("Players 객체 생성 테스트")
+    public void createPlayersTest() {
         Player player = Player.of(Name.of("test"));
         Players players = Players.of(List.of(player));
         assertThat(players.size()).isEqualTo(1);
     }
 
     @Test
-    public void 플레이어_카드_배분_테스트() {
+    @DisplayName("Players 카드 배분 테스트")
+    public void giveCardToPlayersTest() {
+        CardDeck cardDeck = CardDeckGenerator.generate();
         Player player1 = Player.of(Name.of("pobi"));
         Player player2 = Player.of(Name.of("jason"));
         Players players = Players.of(List.of(player1, player2));
-        players.giveCard();
-        players.giveCard();
+        players.giveCard(cardDeck);
+        players.giveCard(cardDeck);
         assertThat(player1.getCards().size()).isEqualTo(2);
         assertThat(player2.getCards().size()).isEqualTo(2);
     }
