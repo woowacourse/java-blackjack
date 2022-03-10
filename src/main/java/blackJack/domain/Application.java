7a7081fe19cfad6d39d155efb5139c9bc0866541
Wishoon <heonga2@gmail.com>
2022-03-10 19:11:41 +0900

    feat : 블랙잭 어플리케이션 구현

---
+++ src/main/java/blackJack/domain/Application.java
@@ -0,0 +1,9 @@
+package blackJack.domain;
+
+public class Application {
+
+    public static void main(String[] args) {
+        final BlackJackController blackJackController = new BlackJackController();
+        blackJackController.run();
+    }
+}
