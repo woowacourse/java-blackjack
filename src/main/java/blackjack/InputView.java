d42c6a17c67ae2ce847ed627b9922858edd10666
bomi <bonita9931@gmail.com>
2022-03-10 18:14:59 +0900

    feat: 쉼표 기준으로 이름을 분리

--- src/main/java/blackjack/InputView.java
+++ src/main/java/blackjack/InputView.java
@@ -1,13 +1,17 @@
 package blackjack;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String NAME_DELIMITER = ",";
+
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static String inputNames() {
+    public static List<String> inputNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         String inputNames = SCANNER.nextLine();
-        return inputNames;
+        return Arrays.asList(inputNames.split(","));
     }
 }
