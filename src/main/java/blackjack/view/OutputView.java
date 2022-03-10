9430e0df85870e4887ead8d85b8373126a63703b
jochungbeom <cndqjacndqja@gmail.com>
2022-03-11 01:40:23 +0900

    refactor: OutputView 메서드 정렬

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -9,61 +9,44 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
+
     public static void printFirstCards(GamerDto dealer, List<GamerDto> players) {
-        StringBuilder builder = new StringBuilder();
-        printGamers(dealer, players, builder);
-        printCards(dealer, players, builder);
-        System.out.println(builder);
+        printGamers(dealer, players);
+        printDealerFirstCard(dealer);
+        printPlayerFirstCards(players);
     }
 
-    private static void printGamers(GamerDto dealer, List<GamerDto> players, StringBuilder builder) {
+    private static void printGamers(GamerDto dealer, List<GamerDto> players) {
+        StringBuilder builder = new StringBuilder();
         builder.append(dealer.getName() + "와 ");
         String names = players.stream()
                 .map(GamerDto::getName)
                 .collect(Collectors.joining(", "));
-        builder.append(names + "에게 " + dealer.getCardSize() + "장을 나누었습니다.\n");
-    }
-
-    private static void printCards(GamerDto dealer, List<GamerDto> players, StringBuilder builder) {
-        Card firstCard = dealer.getFirstCard();
-        String dealerFirstCardName = firstCard.getValue() + firstCard.getShape();
-        builder.append(dealer.getName() + "카드: " + dealerFirstCardName + "\n");
-
-        for (GamerDto player : players) {
-            String playerCard = appendPlayerCard(player);
-            builder.append(playerCard);
-        }
-    }
-
-    private static String getCardNames(GamerDto dealer) {
-        return dealer.getCards().stream()
-                .map(card -> card.getName() + card.getShape())
-                .collect(Collectors.joining(", "));
-    }
-
-    public static void printFinalCards(GamerDto dealer, List<GamerDto> players) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(dealer.getName() + "카드: ");
-        builder.append(getCardNames(dealer));
-        builder.append("- 결과: " + dealer.getCardNumberSum() + "\n");
-
-        for (GamerDto player : players) {
-            builder.append(player.getName() + "카드: ");
-            builder.append(getCardNames(player));
-            builder.append("- 결과: " + player.getCardNumberSum() + "\n");
-        }
+        builder.append(names + "에게 " + dealer.getCardSize() + "장을 나누었습니다.");
         System.out.println(builder);
     }
 
-    public static void printPlayerCard(GamerDto player) {
-        System.out.print(appendPlayerCard(player));
+    private static void printDealerFirstCard(GamerDto dealer) {
+        Card firstCard = dealer.getFirstCard();
+        String dealerFirstCardName = firstCard.getName() + firstCard.getShape();
+        System.out.println(dealer.getName() + "카드: " + dealerFirstCardName);
     }
 
-    private static String appendPlayerCard(GamerDto player) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(player.getName() + "카드: ");
-        builder.append(getCardNames(player) + "\n");
-        return builder.toString();
+    private static void printPlayerFirstCards(List<GamerDto> players) {
+        for (GamerDto player : players) {
+            printPlayerCard(player);
+        }
+        System.out.println();
+    }
+
+    public static void printPlayerCard(GamerDto player) {
+        System.out.println(player.getName() + "카드: " + getCardNames(player));
+    }
+
+    private static String getCardNames(GamerDto gamer) {
+        return gamer.getCards().stream()
+                .map(card -> card.getName() + card.getShape())
+                .collect(Collectors.joining(", "));
     }
 
     public static void printAdditionalDrawDealer(int count) {
@@ -75,16 +58,51 @@ public class OutputView {
         System.out.println("딜러는 17이상이라 카드를 더 받지 않았습니다.\n\n");
     }
 
+    public static void printFinalCards(GamerDto dealer, List<GamerDto> players) {
+        printDealerFinalCards(dealer);
+        printPlayerFinalCards(players);
+    }
+
+    private static void printDealerFinalCards(GamerDto dealer) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(dealer.getName() + "카드: ");
+        builder.append(getCardNames(dealer));
+        builder.append("- 결과: " + dealer.getCardNumberSum() );
+        System.out.println(builder);
+    }
+
+    private static void printPlayerFinalCards(List<GamerDto> players) {
+        StringBuilder builder = new StringBuilder();
+        for (GamerDto player : players) {
+            builder.append(player.getName() + "카드: ");
+            builder.append(getCardNames(player));
+            builder.append("- 결과: " + player.getCardNumberSum() + "\n");
+        }
+        System.out.println(builder);
+    }
+
     public static void printFinalResult(Map<BlackJackResult, Integer> dealerResult, Map<String, BlackJackResult> playerResult) {
         System.out.println("## 최종 승패");
-        StringBuilder builder = new StringBuilder("딜러: ");
-        for (BlackJackResult dealer : dealerResult.keySet()) {
-            if (dealerResult.get(dealer) > 0) {
-                builder.append(dealerResult.get(dealer) + dealer.getValue());
-            }
-        }
-        builder.append("\n");
+        printFinalDealerResult(dealerResult);
+        printFinalPlayerResult(playerResult);
+    }
 
+    private static void printFinalDealerResult(Map<BlackJackResult, Integer> dealerResult) {
+        System.out.print("딜러: ");
+        for (BlackJackResult dealer : dealerResult.keySet()) {
+            getDealerSingleResult(dealerResult, dealer);
+        }
+        System.out.println();
+    }
+
+    private static void getDealerSingleResult(Map<BlackJackResult, Integer> dealerResult, BlackJackResult dealer) {
+        if (dealerResult.get(dealer) > 0) {
+            System.out.print(dealerResult.get(dealer) + dealer.getValue()+ " ");
+        }
+    }
+
+    private static void printFinalPlayerResult(Map<String, BlackJackResult> playerResult) {
+        StringBuilder builder = new StringBuilder();
         for (String name : playerResult.keySet()) {
             builder.append(name+": ");
             builder.append(playerResult.get(name).getValue() + "\n");
