9944141e51729f821b6e8cac583fccd046ff649e
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-10 19:37:11 +0900

    feat: Application의 Game run 담당 클래스 추가 및 역할 전달

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -1,8 +1,6 @@
 package blackjack;
 
 import blackjack.domain.BlackJackBoard;
-import blackjack.domain.DrawCommand;
-import blackjack.dto.PlayerCards;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
 import java.util.List;
@@ -14,26 +12,10 @@ public class Application {
         final BlackJackBoard blackJackBoard = BlackJackBoard.createGame(playerNames);
         OutputView.showPlayersFirstCards(blackJackBoard.getDealerFirstCard(), blackJackBoard.getPlayersFirstCards());
 
-        runPlayerTurn(blackJackBoard);
-        runDealerTurn(blackJackBoard);
+        BlackJackGame.runPlayerTurn(blackJackBoard);
+        BlackJackGame.runDealerTurn(blackJackBoard);
 
         OutputView.printPlayerScoreResults(blackJackBoard.getPlayerScoreResults());
         OutputView.printAllOutcomeResult(blackJackBoard.calculateAllResults());
-    }
-
-    private static void runPlayerTurn(final BlackJackBoard blackJackBoard) {
-        while (!blackJackBoard.isPlayersTurnEnd()) {
-            final DrawCommand drawCommand = DrawCommand
-                    .from(InputView.inputDrawCommand(blackJackBoard.getCurrentTurnPlayerName()));
-            final PlayerCards currentPlayerCards = blackJackBoard.drawCurrentPlayer(drawCommand);
-            OutputView.printPlayerCards(currentPlayerCards);
-        }
-    }
-
-    private static void runDealerTurn(final BlackJackBoard blackJackBoard) {
-        while (!blackJackBoard.isDealerTurnEnd()) {
-            blackJackBoard.drawDealer();
-            OutputView.printDealerDraw();
-        }
     }
 }
---
+++ src/main/java/blackjack/BlackJackGame.java
@@ -0,0 +1,36 @@
+package blackjack;
+
+import blackjack.domain.BlackJackBoard;
+import blackjack.domain.DrawCommand;
+import blackjack.dto.PlayerCards;
+import blackjack.view.InputView;
+import blackjack.view.OutputView;
+
+public class BlackJackGame {
+
+    private BlackJackGame() {
+        throw new AssertionError();
+    }
+
+    public static void runPlayerTurn(final BlackJackBoard blackJackBoard) {
+        if (blackJackBoard.isPlayersTurnEnd()) {
+            return;
+        }
+        final DrawCommand drawCommand = inputDrawCommand(blackJackBoard);
+        final PlayerCards currentPlayerCards = blackJackBoard.drawCurrentPlayer(drawCommand);
+        OutputView.printPlayerCards(currentPlayerCards);
+        runPlayerTurn(blackJackBoard);
+    }
+
+    private static DrawCommand inputDrawCommand(final BlackJackBoard blackJackBoard) {
+        return DrawCommand.from(InputView.inputDrawCommand(blackJackBoard.getCurrentTurnPlayerName()));
+    }
+
+    public static void runDealerTurn(final BlackJackBoard blackJackBoard) {
+        if (blackJackBoard.isDealerTurnEnd()) {
+            return;
+        }
+        blackJackBoard.drawDealer();
+        OutputView.printDealerDraw();
+    }
+}
