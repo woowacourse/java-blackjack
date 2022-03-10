329ef550efe6ff2aab03fe04cc590676d94ad7e9
jaejae-yoo <wotj102@gmail.com>
2022-03-11 08:57:07 +0900

    refactor: InputView 상수 분리

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -9,14 +9,18 @@ public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
+    public static final String START_GAME_GUIDE_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    public static final String ASK_MORE_CARD = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    public static final String PLAYER_DELIMITER = ",";
+
     public static List<String> inputPlayerNames() {
-        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return Arrays.stream(SCANNER.nextLine().split(","))
+        System.out.println(START_GAME_GUIDE_MESSAGE);
+        return Arrays.stream(SCANNER.nextLine().split(PLAYER_DELIMITER))
                 .collect(Collectors.toList());
     }
 
     public static String requestMoreCard(String name) {
-        System.out.println(name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+        System.out.println(name + ASK_MORE_CARD);
         return SCANNER.nextLine();
     }
 }
