58a3444d1d69f1a2b05f938aa12cb9432d0a6958
유영우 <fbduddn97@naver.com>
2022-03-10 15:42:43 +0900

    refactor: Dealer cards final 처리

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -3,7 +3,7 @@ package blackjack.domain;
 public class Dealer extends Person{
 
     private static final int CONDITION_HIT = 16;
-    private Cards cards;
+    private final Cards cards;
 
     public Dealer() {
         cards = new Cards();
