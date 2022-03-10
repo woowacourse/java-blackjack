6da03c954b72517d83568a25117cf0f32a82a5f3
유영우 <fbduddn97@naver.com>
2022-03-10 19:33:32 +0900

    refactor: InputView 상수처리 및 정크 코드 정리

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,33 +1,39 @@
 package blackjack.view;
 
 import blackjack.domain.Player;
+
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final String NAME_DELIMITER = ",";
+	private static final String INPUT_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+	private static final String INPUT_ASK_ADDITIONAL_CARD = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+	private static final String ANSWER_YES = "y";
+	private static final String ANSWER_NO = "n";
 
-    public static List<String> getPlayerNames() {
-        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return List.of(scanner.nextLine()
-                .split(","));
-    }
+	private static final String ANSWER_ERROR = "[ERROR] 답은 y, n 으로 해야 합니다.";
 
-    public static boolean askAdditionalCard(Player person) {
-        System.out.println(person.getName()+"는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+	private static final Scanner scanner = new Scanner(System.in);
 
-        String answer = scanner.nextLine();
-        validateIsYesOrNo(answer);
-        if(answer.equals("y")) {
-            return true;
-        }
+	public static List<String> getPlayerNames() {
+		System.out.println(INPUT_NAMES);
+		return List.of(scanner.nextLine()
+			.split(NAME_DELIMITER));
+	}
 
-        return false;
-    }
+	public static boolean askAdditionalCard(Player person) {
+		System.out.println(person.getName() + INPUT_ASK_ADDITIONAL_CARD);
 
-    private static void validateIsYesOrNo(String answer) {
-        if(!(answer.equals("y") || answer.equals("n"))) {
-            throw new IllegalArgumentException("[ERROR] 답은 y, n 으로 해야 합니다.");
-        }
-    }
+		String answer = scanner.nextLine();
+		validateIsYesOrNo(answer);
+
+		return answer.equals(ANSWER_YES);
+	}
+
+	private static void validateIsYesOrNo(String answer) {
+		if (!(answer.equals(ANSWER_YES) || answer.equals(ANSWER_NO))) {
+			throw new IllegalArgumentException(ANSWER_ERROR);
+		}
+	}
 }
