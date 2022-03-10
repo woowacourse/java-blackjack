6188595bc1f0b98dfb86e284b03e485df74f973a
OzRagwort <ozragwort@gmail.com>
2022-03-10 20:48:26 +0900

    fix(view): 카드를 받을 때 y, n 외 입력이 들어오면 예외처리 하도록 수정

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -8,8 +8,13 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String REQUEST_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.";
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final String REQUEST_HIT_OR_STAY_MESSAGE = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String ILLEGAL_HIT_OR_STAY_INPUT_ERROR_MESSAGE = "대답은 y 또는 n으로만 가능합니다.";
     private static final String NAME_DELIMITER = ",";
+    private static final String HIT_ANSWER = "y";
+    private static final String STAY_ANSWER = "n";
+
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static List<String> requestNames() {
         System.out.println(REQUEST_NAMES_MESSAGE);
@@ -23,8 +28,17 @@ public class InputView {
     }
 
     public static String requestHitOrStay(Name name) {
-        System.out.println(name.get() + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
-        return toLowerCase(readLine());
+        System.out.println(name.get() + REQUEST_HIT_OR_STAY_MESSAGE);
+        String answer = toLowerCase(readLine());
+        validateHitOrStayAnswer(answer);
+
+        return answer;
+    }
+
+    private static void validateHitOrStayAnswer(String answer) {
+        if (!answer.equals(HIT_ANSWER) && !answer.equals(STAY_ANSWER)) {
+            throw new IllegalArgumentException(ILLEGAL_HIT_OR_STAY_INPUT_ERROR_MESSAGE);
+        }
     }
 
     private static String toLowerCase(String input) {
