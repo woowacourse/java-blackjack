e1ad95b3b9f8c5bf5106ebb89890bb280aa199c0
EastHShin <xldk78@gmail.com>
2022-03-10 18:35:46 +0900

    feat(ResultType): 플레이어가 버스트일 시 무조건 패배하는 기능 구현

--- src/main/java/domain/ResultType.java
+++ src/main/java/domain/ResultType.java
@@ -6,7 +6,9 @@ public enum ResultType {
 	DRAW;
 
 	public static ResultType generateResultType(Player player, Dealer dealer) {
-
+		if (player.isBurst()) {
+			return LOSE;
+		}
 		if (dealer.isEqaul(player)) {
 			return DRAW;
 		}
