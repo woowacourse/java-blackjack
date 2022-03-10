02dec2fcc74145a529acaebb4f6a117a63d71c54
bomi <bonita9931@gmail.com>
2022-03-10 22:05:57 +0900

    feat: 입력이 y 또는 n인지 검증

--- src/main/java/blackjack/InputView.java
+++ src/main/java/blackjack/InputView.java
@@ -9,6 +9,8 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String NAME_DELIMITER = ",";
+    private static final String YES = "y";
+    private static final String NO = "n";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
@@ -23,8 +25,15 @@ public class InputView {
                 .collect(Collectors.toList());
     }
 
-    public String inputCommand(Player player) {
+    public static String inputCommand(Player player) {
         System.out.println(MessageFormat.format("{0}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)", player.getName()));
-        return SCANNER.nextLine().trim().toLowerCase();
+        return validateCommand(SCANNER.nextLine().trim().toLowerCase());
+    }
+
+    private static String validateCommand(String value) {
+        if (!YES.equals(value) && !NO.equals(value)) {
+            throw new IllegalArgumentException("입력은 y 또는 n이어야 합니다.");
+        }
+        return value;
     }
 }
