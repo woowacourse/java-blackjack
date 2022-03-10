febf511f66d615b49241333792ab694525795c53
bomi <bonita9931@gmail.com>
2022-03-10 18:24:22 +0900

    feat: 이름 앞뒤로 공백을 제거

--- src/main/java/blackjack/InputView.java
+++ src/main/java/blackjack/InputView.java
@@ -3,6 +3,7 @@ package blackjack;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final String NAME_DELIMITER = ",";
@@ -11,7 +12,12 @@ public class InputView {
 
     public static List<String> inputNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        String inputNames = SCANNER.nextLine();
-        return Arrays.asList(inputNames.split(","));
+        return toNames(SCANNER.nextLine().trim());
+    }
+
+    private static List<String> toNames(String inputNames) {
+        return Arrays.stream(inputNames.split(NAME_DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 }
