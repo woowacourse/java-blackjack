bd7ddbdb5d65583883a06cd953269e099d251f2d
devHudi <devhudi@gmail.com>
2022-03-10 12:46:09 +0900

    test(CardStackGenerator): CardDeck에 대한 테스트 더블 생성 기능 셋업
    - CardStackGenerator.ofReverse를 통해 카드 스택 생성
    - 생성될 때 입력된 카드들의 순서대로 pop되는 구조

---
+++ src/test/java/blackjack/domain/fixture/CardStackGenerator.java
@@ -0,0 +1,30 @@
+package blackjack.domain.fixture;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardStack;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CardStackGenerator {
+
+    public static CardStack ofReverse(Card... cards) {
+        return new CardStackImpl(List.of(cards));
+    }
+
+    private static class CardStackImpl implements CardStack {
+        final LinkedList<Card> cards = new LinkedList<>();
+
+        CardStackImpl(List<Card> cards) {
+            this.cards.addAll(cards);
+        }
+
+        public Card pop() {
+            Card newCard = cards.pollFirst();
+            if (newCard == null) {
+                throw new IllegalArgumentException("카드 스택이 비어있습니다!");
+            }
+
+            return newCard;
+        }
+    }
+}
