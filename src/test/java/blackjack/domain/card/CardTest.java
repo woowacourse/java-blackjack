00921eace6b1565f7d8984c347fb01048559db42
yhh1056 <yhh1056@naver.com>
2022-03-11 22:13:25 +0900

    test: 카드 테스트 코드 추가

--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -1,5 +1,7 @@
 package blackjack.domain.card;
 
+import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,10 +18,26 @@ public class CardTest {
     }
 
     @Test
-    @DisplayName("52장의 카드를 생성한다.")
-    void create52Cards() {
+    @DisplayName("같은 모양과 숫자의 카드는 동일하다.")
+    void equalsCardsHashcode() {
         Card card = Card.valueOf(Suit.SPADE, Number.ACE);
 
         assertThat(card.hashCode() == Card.valueOf(Suit.SPADE, Number.ACE).hashCode()).isTrue();
+    }
+
+    @Test
+    @DisplayName("같은 모양과 숫자의 카드는 동등하다.")
+    void equalsCard() {
+        Card card = Card.valueOf(Suit.SPADE, Number.ACE);
+
+        assertThat(card.equals(Card.valueOf(Suit.SPADE, Number.ACE))).isTrue();
+    }
+
+    @Test
+    @DisplayName("52장의 서로 다른 카드를 생성한다.")
+    void createCards() {
+        List<Card> cards = Card.createDeck();
+
+        assertThat(Set.copyOf(cards)).hasSize(52);
     }
 }
