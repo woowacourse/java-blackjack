2ade77e02a4c9bcf67cda8d3c8fec9290dd96a98
유영우 <fbduddn97@naver.com>
2022-03-11 15:41:17 +0900

    refactor: OutputView 상수화 처리 및 정크 코드 정리

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -2,102 +2,92 @@ package blackjack.view;
 
 import blackjack.domain.Card;
 import blackjack.domain.Dealer;
+import blackjack.domain.Person;
 import blackjack.domain.Player;
-import java.util.ArrayList;
+import blackjack.domain.dto.DealerResultDto;
+import blackjack.domain.dto.PlayerResultDto;
+import blackjack.domain.dto.ResultDto;
+
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class OutputView {
+	private static final String DEALER_PLAYERS_DELIMITER = "와 ";
+	private static final String CARD_DELIMITER = ", ";
+	private static final String INIT_CARD_MESSAGE = "에게 2장의 카드를 나누었습니다.";
+	private static final String DEALER_DRAW_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+	private static final String SCORE_MESSAGE = " - 결과: ";
+	private static final String RESULT_MESSAGE = "## 최종 승패";
+	private static final String COLON = ": ";
 
+	public static void printInitStatus(Dealer dealer, List<Player> players) {
+		System.out.println();
+		List<String> playerNames = players.stream()
+			.map(Person::getName)
+			.collect(Collectors.toList());
 
-    public static void printInitStatus(Dealer dealer, List<Player> players) {
-        List<String> names = new ArrayList<>(List.of("딜러"));
-        players.stream()
-                .map(player -> player.getName())
-                .map(names::add);
-        System.out.println("딜러와 " + String.join(", ", names) + "에게 2장의 카드를 나누었습니다.");
+		System.out.println(dealer.getName() + DEALER_PLAYERS_DELIMITER
+			+ String.join(CARD_DELIMITER, playerNames) + INIT_CARD_MESSAGE);
 
-      /*  System.out.println("딜러: ");*/
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(makeStatusFormat("딜러", dealer.getMyCards()))
-                .append("\n");
-        players.forEach(player -> stringBuilder.append(makeStatusFormat(player.getName(),player.getMyCards()))
-                .append( "\n"));
+		printInitCards(dealer, players);
+	}
 
-        System.out.println(stringBuilder.toString());
-    }
+	private static void printInitCards(Dealer dealer, List<Player> players) {
+		System.out.println(makeStatusFormat(dealer.getName(), dealer.getMyCards()));
 
-    private static String makeStatusFormat(String name, List<Card> cards) {
-        List<String> cardNames = cards.stream()
-                .map(Card::getName)
-                .collect(Collectors.toList());
-        return name + ": " + String.join(", ", cardNames);
-    }
+		players.forEach(player ->
+			System.out.println(makeStatusFormat(player.getName(), player.getMyCards())));
+		System.out.println();
+	}
 
-    public static void printCards(Player person) {
-        System.out.println(makeStatusFormat(person.getName(), person.getMyCards()));
-    }
+	private static String makeStatusFormat(String name, List<Card> cards) {
+		List<String> cardNames = cards.stream()
+			.map(Card::getName)
+			.collect(Collectors.toList());
 
-    public static void printDealerAdditionalCard() {
-        System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
-    }
+		return name + COLON + String.join(CARD_DELIMITER, cardNames);
+	}
 
-    public static void printCardsAndResult(Dealer dealer, List<Player> players) {
-        System.out.println();
-        System.out.println(makeStatusFormat("딜러", dealer.getMyCards()) + printScoreResult(dealer.score()));
+	public static void printCards(Player player) {
+		System.out.println(makeStatusFormat(player.getName(), player.getMyCards()));
+	}
 
-        for (Player player: players) {
-            System.out.println(makeStatusFormat(player.getName(), player.getMyCards())
-                    + printScoreResult(player.score()));
-        }
-    }
+	public static void printDealerAdditionalCard() {
+		System.out.println();
+		System.out.println(DEALER_DRAW_MESSAGE);
+	}
 
-    private static String printScoreResult(int score) {
-        return (" - 결과: " + score);
-    }
+	public static void printCardsAndResult(Dealer dealer, List<Player> players) {
+		System.out.println();
+		System.out.println(makeStatusFormat(dealer.getName(), dealer.getMyCards()) + printScoreResult(dealer.score()));
 
-    public static void printResult(Map<Player, Integer> result) {
-        System.out.println("\n## 최종 승패");
-        System.out.println("딜러: " + getDealerResult(result));
-        Set<Player> keys = result.keySet();
-        for (Player key : keys) {
-            System.out.print(key.getName() + ": ");
-            if (result.get(key) == 1) {
-                System.out.println("승");
-            } else if (result.get(key) == 0) {
-                System.out.println("무");
-            } else {
-                System.out.println("패");
-            }
-        }
-    }
+		for (Player player : players) {
+			System.out.println(makeStatusFormat(player.getName(), player.getMyCards())
+				+ printScoreResult(player.score()));
+		}
+	}
 
-    private static String getDealerResult(Map<Player, Integer> result) {
-        Set<Player> keys = result.keySet();
-        int winCount = 0;
-        int drawCount = 0;
-        int loseCount = 0;
-        for (Player key : keys) {
-            if (result.get(key) == 1) {
-                ++winCount;
-            } else if (result.get(key) == -1) {
-                ++loseCount;
-            } else {
-                ++drawCount;
-            }
-        }
-        StringBuilder stringBuilder = new StringBuilder();
-        if (winCount > 0) {
-            stringBuilder.append(winCount + "승");
-        }
-        if (drawCount > 0) {
-            stringBuilder.append(drawCount + "무");
-        }
-        if (loseCount > 0) {
-            stringBuilder.append(loseCount + "패");
-        }
-        return stringBuilder.toString();
-    }
+	private static String printScoreResult(int score) {
+		return (SCORE_MESSAGE + score);
+	}
+
+	public static void printResult(ResultDto resultDto) {
+		System.out.println();
+		System.out.println(RESULT_MESSAGE);
+
+		DealerResultDto dealerResultDto = resultDto.getDealerResultDto();
+		System.out.print(dealerResultDto.getName() + COLON);
+		dealerResultDto.getOutcome()
+			.forEach(System.out::print);
+		System.out.println();
+
+		printPlayerResult(resultDto.getPlayerResultDto());
+	}
+
+	private static void printPlayerResult(PlayerResultDto playerResultDto) {
+		Map<String, String> outcome = playerResultDto.getOutcome();
+		outcome.keySet()
+			.forEach(player -> System.out.println(player + COLON + outcome.get(player)));
+	}
 }
