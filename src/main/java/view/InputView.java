1f5c2b17ae5d7941d2b80b3f8b6b30f7e341da68
Richard JEON <ztzy1907@gmail.com>
2022-03-11 15:51:55 +0900

    refactor(view): InputView 매직 넘버 상수화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,25 +7,25 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String INPUT_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final int IGNORE_EMPTY_INPUT = -1;
+    private static final String HIT_CHARACTER = "y";
+    private static final String COMMA_DELIMITER = ",";
     private static final String ERROR_DUPLICATE_NAME = "[ERROR] 이름은 중복될 수 없습니다.";
+    private static final String INPUT_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String INPUT_NEED_MORE_CARD = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)%n";
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputView() {
     }
 
     public static List<String> scanPlayerNames() {
         System.out.println(INPUT_PLAYER_NAMES);
-        return getPlayerNames();
-    }
-
-    private static List<String> getPlayerNames() {
         return ExceptionHandler.process(InputView::scanRawPlayerNames, InputView::validateDuplicateNames);
     }
 
     private static List<String> scanRawPlayerNames() {
         return Arrays.stream(SCANNER.nextLine()
-                        .split(",", -1))
+                        .split(COMMA_DELIMITER, IGNORE_EMPTY_INPUT))
                 .map(String::trim)
                 .collect(toList());
     }
@@ -40,13 +40,8 @@ public class InputView {
         }
     }
 
-    public static boolean scanHitOrStay(String name) {
-        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)%n", name);
-        String hitOrStay = SCANNER.nextLine().trim();
-        return isHit(hitOrStay);
-    }
-
-    private static boolean isHit(final String hitOrStay) {
-        return "y".equalsIgnoreCase(hitOrStay);
+    public static boolean scanIsHit(String name) {
+        System.out.printf(INPUT_NEED_MORE_CARD, name);
+        return HIT_CHARACTER.equalsIgnoreCase(SCANNER.nextLine().trim());
     }
 }
