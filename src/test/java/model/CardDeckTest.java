11297dabd13b7b0ef3bda5111e1c895aac82e288
yangdongjue5510 <djwhy5510@gmail.com>
2022-03-11 15:54:17 +0900

    test: CardDeck 모든 카드 소진 예외 처리 테스트

--- src/test/java/model/CardDeckTest.java
+++ src/test/java/model/CardDeckTest.java
@@ -1,6 +1,7 @@
 package model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import java.util.Set;
@@ -11,12 +12,25 @@ import org.junit.jupiter.api.Test;
 public class CardDeckTest {
 
     @Test
-    void popCard() {
+    void drawCard() {
         CardDeck cardDeck = new CardDeck();
-        List<Card> cards = IntStream.range(0, 52)
-                .mapToObj(i -> cardDeck.drawCard())
-                .collect(Collectors.toList());
+        List<Card> cards = drawAllCards(cardDeck);
         assertThat(cards.size()).isEqualTo(52);
         assertThat(cards.size()).isEqualTo(Set.copyOf(cards).size());
+    }
+
+    @Test
+    void drawEmptyDeck() {
+        CardDeck cardDeck = new CardDeck();
+        drawAllCards(cardDeck);
+        assertThatThrownBy(() -> cardDeck.drawCard())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("카드를 모두 소진했습니다!!");
+    }
+
+    private List<Card> drawAllCards(CardDeck cardDeck) {
+        return IntStream.range(0, 52)
+                .mapToObj(i -> cardDeck.drawCard())
+                .collect(Collectors.toList());
     }
 }
