e267c697acb82c9274905bbb6537fb2fedc41035
shindong96 <2006peter@naver.com>
2022-03-11 21:34:11 +0900

    refactor: 덱이 비면 오류 발생

--- src/main/java/domain/card/Deck.java
+++ src/main/java/domain/card/Deck.java
@@ -7,6 +7,7 @@ import domain.card.deckstrategy.GenerationDeckStrategy;
 public class Deck {
 
 	private static final int FRONT_CARD_INDEX = 0;
+	public static final String DECK_SIZE_ZERO_MESSAGE = "[Error] 카드가 없습니다.";
 
 	private final List<Card> cards;
 
@@ -20,6 +21,10 @@ public class Deck {
 	}
 
 	public Card draw() {
+		if (cards.size() <= 0) {
+			throw new IllegalStateException(DECK_SIZE_ZERO_MESSAGE);
+		}
+		
 		Card card = cards.get(FRONT_CARD_INDEX);
 		cards.remove(FRONT_CARD_INDEX);
 		return card;
