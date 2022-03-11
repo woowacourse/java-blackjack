ac7bf3f5f05012c4a92ba49e4b0987150406e060
YJGwon <yj970125@gmail.com>
2022-03-11 13:12:38 +0900

    feat: 중복 entry 이름 검사

--- src/main/java/blackjack/Entries.java
+++ src/main/java/blackjack/Entries.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Entries {
+    private static final String ERROR_DUPLICATE_NAME = "[ERROR] 중복된 이름이 있습니다.";
     private static final String ERROR_NO_ENTRY = "[ERROR] 더 이상 Entry가 없습니다.";
 
     private final List<Entry> entries;
@@ -14,10 +15,23 @@ public class Entries {
     }
 
     public static Entries from(List<String> names) {
+        checkDuplicate(names);
         List<Entry> entries = names.stream()
                 .map(Entry::new)
                 .collect(Collectors.toList());
         return new Entries(entries);
+    }
+
+    private static void checkDuplicate(List<String> names) {
+        if (countDistinct(names) != names.size()) {
+            throw new IllegalArgumentException(ERROR_DUPLICATE_NAME);
+        }
+    }
+
+    private static int countDistinct(List<String> names) {
+        return (int) names.stream()
+                .distinct()
+                .count();
     }
 
     public void toNextEntry() {
--- src/test/java/blackjack/EntriesTest.java
+++ src/test/java/blackjack/EntriesTest.java
@@ -8,6 +8,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class EntriesTest {
+
+    @DisplayName("중복된 이름이 있을 때 예외가 발생한다.")
+    @Test
+    void nextEntry_exception_duplicate_name() {
+        List<String> names = List.of("포키", "리버", "포키");
+
+        assertThatThrownBy(() -> Entries.from(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 중복된 이름이 있습니다.");
+    }
+
     @DisplayName("포키와 리버가 있을 때, 리버의 턴을 시작한다")
     @Test
     void nextEntry_reaver() {
