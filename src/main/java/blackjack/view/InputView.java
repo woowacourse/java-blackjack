14c9fd5dc346d2877640302260b8faf6b80012ea
HyeonbinSa <shb1833@gmail.com>
2022-03-11 16:14:26 +0900

    refactor: view 패키지 매직넘버 제거 및 상수 필드 수정

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -7,11 +7,14 @@ import java.util.Set;
 
 public class InputView {
 
-    private static final String NULL_NAMES_ERROR_MESSAGE = "이름에 공백을 입력할 수 없습니다.";
-    private static final String NULL_NAMES_DUPLICATED_ERROR_MESSAGE = "이름은 중복될 수 없습니다.";
-    private static final String NULL_ANSWER_YN_ERROR_MESSAGE = "y,n 이외의 입력이 들어왔습니다.";
+    private static final String NULL_NAMES_ERROR_MESSAGE = "[ERROR] 이름에 공백은 포함될 수 없습니다..";
+    private static final String NAMES_DUPLICATED_ERROR_MESSAGE = "[ERROR] 중복된 이름을 입력할 수 없습니다.";
+    private static final String NULL_ANSWER_INPUT_ERROR_MESSAGE = "[ERROR] y,n 이외의 문자를 입력할 수 없습니다.";
     private static final String NAME_INPUT_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String ONE_MORE_CARD_INPUT_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
     private static final String NAME_SEPARATE_REGEX = ",";
+    private static final String YES_ANSWER = "y";
+    private static final String NO_ANSWER = "n";
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static String[] inputPlayerName() {
@@ -22,26 +25,34 @@ public class InputView {
     }
 
     public static boolean inputOneMoreCard(String name) {
-        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)" + System.lineSeparator(), name);
+        System.out.printf(ONE_MORE_CARD_INPUT_MESSAGE + System.lineSeparator(), name);
         String input = SCANNER.nextLine();
         validateAnswer(input);
-        return input.equals("y");
+        return input.equals(YES_ANSWER);
     }
 
     private static void validateAnswer(String input) {
-        if (!input.equals("y") && !input.equals("n")) {
-            throw new IllegalArgumentException(NULL_ANSWER_YN_ERROR_MESSAGE);
+        if (!input.equals(YES_ANSWER) && !input.equals(NO_ANSWER)) {
+            throw new IllegalArgumentException(NULL_ANSWER_INPUT_ERROR_MESSAGE);
         }
     }
 
     private static void validateNames(String input) {
+        validateNullNameInput(input);
+        validateDuplicatedNameInput(input);
+    }
+
+    private static void validateNullNameInput(String input) {
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException(NULL_NAMES_ERROR_MESSAGE);
         }
+    }
+
+    private static void validateDuplicatedNameInput(String input) {
         String[] names = separate(input);
         Set<String> nameSet = new HashSet<>(Arrays.asList(names));
         if (nameSet.size() != names.length) {
-            throw new IllegalArgumentException(NULL_NAMES_DUPLICATED_ERROR_MESSAGE);
+            throw new IllegalArgumentException(NAMES_DUPLICATED_ERROR_MESSAGE);
         }
     }
 
--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -9,23 +9,24 @@ import blackjack.domain.human.Players;
 
 public class OutputView {
 
-    public static final String HAVE_CARD = "카드: ";
-    public static final String RESULT_PREFIX = " - 결과 : ";
     public static final String DEALER_NO_MORE_CARD_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
     public static final String TOTAL_RESULT_MASSAGE = "## 최종 승패";
     public static final String DEALER_RESULT_PREFIX = "딜러: ";
     public static final String COUNT_JOIN_DELIMITER = " ";
-    public static final String PLAYER_RESULT_DELIMITER = ": ";
     public static final String INIT_CARD_MESSAGE = "%s와 %s에게 2장의 카드를 나누었습니다.";
+    public static final String CARD_STATE_MESSAGE = "%s카드: %s";
+    public static final String POINT_STATE_MESSAGE = " - 결과 : %d";
+    public static final String PLAYER_RESULT_MESSAGE = "%s: %s";
 
     public static void printInitCardState(Players players, Dealer dealer) {
-        System.out.printf(System.lineSeparator() + INIT_CARD_MESSAGE + System.lineSeparator(),
-            dealer.getName(), players.getPlayerNames());
+        System.out.println();
+        System.out.printf(INIT_CARD_MESSAGE, dealer.getName(), players.getPlayerNames());
+        System.out.println();
     }
 
     public static void printHumanCardState(Human human) {
-        String result = human.getName() + HAVE_CARD + human.getCards().toString();
-        System.out.println(result);
+        System.out.printf(CARD_STATE_MESSAGE, human.getName(), human.getCards().toString());
+        System.out.println();
     }
 
     public static void printInitGameState(Players players, Dealer dealer) {
@@ -46,28 +47,31 @@ public class OutputView {
     }
 
     public static void printHumanCardPointState(Human human) {
-        String result =
-            human.getName() + HAVE_CARD + human.getCards().toString() + RESULT_PREFIX + human.getPoint();
-        System.out.println(result);
+        System.out.printf(CARD_STATE_MESSAGE, human.getName(), human.getCards().toString());
+        System.out.printf(POINT_STATE_MESSAGE, human.getPoint());
+        System.out.println();
     }
 
     public static void printDealerCardAdded() {
-        System.out.println(System.lineSeparator() + DEALER_NO_MORE_CARD_MESSAGE);
+        System.out.println();
+        System.out.println(DEALER_NO_MORE_CARD_MESSAGE);
     }
 
     public static void printTotalResult(Statistic statistic) {
         System.out.println(System.lineSeparator() + TOTAL_RESULT_MASSAGE);
         StringBuilder dealerResult = new StringBuilder(DEALER_RESULT_PREFIX);
         for (GameResult gameResult : GameResult.values()) {
-            dealerResult.append(
-                statistic.getCountByGameResult(gameResult) + gameResult.getResult() + COUNT_JOIN_DELIMITER);
+            int resultCount = statistic.getCountByGameResult(gameResult);
+            dealerResult.append(resultCount + gameResult.getResult() + COUNT_JOIN_DELIMITER);
         }
         System.out.println(dealerResult);
     }
 
     public static void printTotalResultByPlayer(Statistic statistic, Players players) {
         for (Player player : players.getPlayers()) {
-            System.out.println(player.getName() + PLAYER_RESULT_DELIMITER + statistic.getGameResultByPlayer(player));
+            String result = statistic.getGameResultByPlayer(player);
+            System.out.printf(PLAYER_RESULT_MESSAGE, player.getName(), result);
+            System.out.println();
         }
     }
 }
