79d39885bf764d95753eaeb9826be595acb0cf2d
DOES <ldk980130@gmail.com>
2022-03-11 23:02:07 +0900

    refactor: Card 캐싱 자료구조 List 대신 Map 사용

--- src/main/java/blackjack/domain/card/Card.java
+++ src/main/java/blackjack/domain/card/Card.java
@@ -1,24 +1,28 @@
 package blackjack.domain.card;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class Card {
 
-    private static final String NOT_EXIST_CARD_ERROR = "존재하지 않는 카드입니다.";
-    private static final List<Card> CARDS;
+    private static final Map<String, Card> CARDS = new HashMap<>();
 
     private final CardShape cardShape;
     private final CardNumber cardNumber;
 
     static {
-        CARDS = Arrays.stream(CardShape.values())
-                .flatMap(shape -> Arrays.stream(CardNumber.values())
-                        .map(number -> new Card(shape, number)))
-                .collect(Collectors.toList());
+        for (CardShape shape : CardShape.values()) {
+            cacheCards(shape);
+        }
+    }
+
+    private static void cacheCards(CardShape shape) {
+        for (CardNumber number : CardNumber.values()) {
+            CARDS.put(number.getName() + shape.getName(), new Card(shape, number));
+        }
     }
 
     private Card(CardShape cardShape, CardNumber cardNumber) {
@@ -27,14 +31,7 @@ public class Card {
     }
 
     public static Card getInstance(CardShape shape, CardNumber number) {
-        return CARDS.stream()
-                .filter(card -> card.isSame(shape, number))
-                .findAny()
-                .orElseThrow(() -> new IllegalArgumentException(NOT_EXIST_CARD_ERROR));
-    }
-
-    private boolean isSame(CardShape shape, CardNumber number) {
-        return cardShape == shape && cardNumber == number;
+        return CARDS.get(number.getName() + shape.getName());
     }
 
     public boolean isAce() {
@@ -42,7 +39,7 @@ public class Card {
     }
 
     public static List<Card> getCards() {
-        return new LinkedList<>(CARDS);
+        return new LinkedList<>(CARDS.values());
     }
 
     public int getValue() {
