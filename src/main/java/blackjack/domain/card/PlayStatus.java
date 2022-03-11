12954794da495b221c7c64dfae410428af71d523
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-03-12 01:08:29 +0900

    refactor(PlayStatus): updateStatus 기능 PlayStatus로 이동

--- src/main/java/blackjack/domain/card/PlayStatus.java
+++ src/main/java/blackjack/domain/card/PlayStatus.java
@@ -1,7 +1,16 @@
 package blackjack.domain.card;
 
+import java.util.Arrays;
+
 public enum PlayStatus {
     BUST,
     HIT,
-    STAY
+    STAY;
+
+    public static PlayStatus updateStatus(PlayStatus playStatus) {
+        return Arrays.stream(values())
+            .filter(it -> it == playStatus)
+            .findAny()
+            .orElseThrow();
+    }
 }
