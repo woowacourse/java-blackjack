9843ee188863e7fbbe869d89d97ad61fea00aa78
awesomeo184 <awesomeo184@gmail.com>
2022-03-11 12:43:33 +0900

    refactor(Deck): 메서드 추출, 구현체를 ArrayList에서 LinkedList로 변경

--- src/main/java/blackjack/domain/card/Deck.java
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -2,36 +2,45 @@ package blackjack.domain.card;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Deck {
-    private List<Card> cards = new ArrayList<>();
+    public static final int FIRST_CARD_INDEX = 0;
+
+    private final List<Card> cards;
 
     public Deck() {
-        for (Symbol symbol : Symbol.values()) {
-            addDenomination(symbol);
-        }
+        cards = initCards();
     }
 
-    private void addDenomination(Symbol symbol) {
+    private List<Card> initCards() {
+        List<Card> cards = new LinkedList<>();
+        for (Symbol symbol : Symbol.values()) {
+            addDenomination(cards, symbol);
+        }
+        Collections.shuffle(cards);
+        return cards;
+    }
+
+    private void addDenomination(List<Card> cards, Symbol symbol) {
         for (Denomination denomination : Denomination.values()) {
             cards.add(new Card(symbol, denomination));
         }
     }
 
-    public int size() {
-        return cards.size();
-    }
-
     public Card draw() {
-        Collections.shuffle(cards);
-        return cards.remove(cards.size() - 1);
+        return cards.remove(FIRST_CARD_INDEX);
     }
 
     public List<Card> initialDraw() {
-        Collections.shuffle(cards);
-        List<Card> result = cards.subList(0, 2);
-        cards = cards.subList(2, size());
+        List<Card> result = new ArrayList<>();
+        result.add(draw());
+        result.add(draw());
         return result;
+    }
+
+    public int size() {
+        return cards.size();
     }
 }
