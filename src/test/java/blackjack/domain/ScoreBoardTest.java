99823e25e831a78d5e7dc4bf01f38766db15f552
유영우 <fbduddn97@naver.com>
2022-03-11 16:29:41 +0900

    test: ScoreBoardTest 수정

--- src/test/java/blackjack/domain/ScoreBoardTest.java
+++ src/test/java/blackjack/domain/ScoreBoardTest.java
@@ -8,16 +8,23 @@ import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import blackjack.domain.dto.DealerResultDto;
+import blackjack.domain.dto.ResultDto;
+import blackjack.domain.strategy.NumberGenerator;
+
 public class ScoreBoardTest {
 
-/*    @DisplayName("승패 결과 테스트")
+    @DisplayName("승패 결과 테스트")
     @Test
     void calculateResult() {
         NumberGenerator numberGenerator = new IntendedNumberGenerator(List.of(1, 2, 3, 4, 5, 6));
         Blackjack blackjack = new Blackjack(List.of("pobi", "jason"));
         blackjack.distributeInitialCards(numberGenerator);
-        Map<Player, Integer> results = ScoreBoard.of(blackjack.getDealer(), blackjack.getPlayers());
 
-        assertThat(results.get(blackjack.getPlayers().get(0))).isEqualTo(-1);
-    }*/
+        ResultDto dto = ScoreBoard.of(blackjack.getDealer(), blackjack.getPlayers());
+		DealerResultDto dealerResultDto = dto.getDealerResultDto();
+        List<String> dealerOutcome = dealerResultDto.getOutcome();
+
+        assertThat(String.join(" ",dealerOutcome)).isEqualTo("2패 ");
+    }
 }
