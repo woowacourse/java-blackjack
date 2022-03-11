d429bf829c6c7f442573776996e5afd4a3a22bd1
이한울 <dkfghg3@naver.com>
2022-03-11 12:26:38 +0900

    refactor: 카드덱 생성하는 메서드 분리

--- src/main/java/blackjack/domain/CardDeckGenerator.java
+++ src/main/java/blackjack/domain/CardDeckGenerator.java
@@ -2,10 +2,7 @@ package blackjack.domain;
 
 import blackjack.domain.card.Card;
 import blackjack.domain.card.CardNumber;
-import blackjack.domain.card.Clover;
-import blackjack.domain.card.Diamond;
-import blackjack.domain.card.Heart;
-import blackjack.domain.card.Spade;
+import blackjack.domain.card.CardPattern;
 import java.util.Collections;
 import java.util.LinkedList;
 
@@ -13,13 +10,16 @@ public class CardDeckGenerator {
 
     public static CardDeck createCardDeckByCardNumber() {
         LinkedList<Card> cards = new LinkedList<>();
-        for (CardNumber cardNumber : CardNumber.values()) {
-            cards.add(new Diamond(cardNumber));
-            cards.add(new Clover(cardNumber));
-            cards.add(new Heart(cardNumber));
-            cards.add(new Spade(cardNumber));
+        for (CardPattern cardPattern : CardPattern.values()) {
+            addCardByPattern(cards, cardPattern);
         }
         return new CardDeck(shuffleCard(cards));
+    }
+
+    private static void addCardByPattern(LinkedList<Card> cards, CardPattern cardPattern) {
+        for (CardNumber cardNumber : CardNumber.values()) {
+            cards.add(new Card(cardPattern, cardNumber));
+        }
     }
 
     private static LinkedList<Card> shuffleCard(LinkedList<Card> cards) {
