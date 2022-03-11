39c0f162c9df19cd500289d480a14d3fe49704c0
SuyeonChoi <tndus970120@gmail.com>
2022-03-11 16:15:41 +0900

    refactor: 컨트롤러 메서드 분리

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -1,9 +1,9 @@
 package blackjack.controller;
 
 import blackjack.domain.BlackJack;
-import blackjack.domain.result.GameResult;
 import blackjack.domain.gamer.Player;
 import blackjack.domain.gamer.PlayerGroup;
+import blackjack.domain.result.GameResult;
 import blackjack.dto.DealerResultDto;
 import blackjack.dto.GamerCardsDto;
 import blackjack.dto.PlayerResultDto;
@@ -12,43 +12,20 @@ import blackjack.view.OutputView;
 import java.util.List;
 
 public class BlackJackController {
+    private PlayerGroup playerGroup;
+    private BlackJack blackJack;
 
     public void run() {
-        PlayerGroup playerGroup = initializePlayerGroup();
-        BlackJack blackJack = new BlackJack(playerGroup);
+        initialize();
+        play();
+        finish();
+    }
+
+    private void initialize() {
+        playerGroup = initializePlayerGroup();
+        blackJack = new BlackJack(playerGroup);
         blackJack.divideCards();
-
         OutputView.printGamersCards(GamerCardsDto.of(blackJack.getGamers()));
-
-        List<Player> players = playerGroup.getPlayers();
-        for (Player player : players) {
-            requestCardAddition(blackJack, player);
-        }
-
-        OutputView.printDealerCardMessage(blackJack.addCardToDealer());
-
-        blackJack.openDealerCards();
-        OutputView.printGamersCardAndSum(GamerCardsDto.of(blackJack.getGamers()));
-
-        GameResult gameResult = blackJack.getGameResult();
-        OutputView.printGameResult(DealerResultDto.of(gameResult.getDealerResult()),
-                PlayerResultDto.of(gameResult.getPlayerResult()));
-    }
-
-    private void requestCardAddition(BlackJack blackJack, Player player) {
-        if (InputView.requestCardAddition(player.getName())) {
-            blackJack.addCardTo(player);
-            OutputView.printGamerCard(GamerCardsDto.of(player.getName(), player.getCardGroup()));
-            requestCardAdditionToPlayer(blackJack, player);
-            return;
-        }
-        OutputView.printGamerCard(GamerCardsDto.of(player.getName(), player.getCardGroup()));
-    }
-
-    private void requestCardAdditionToPlayer(BlackJack blackJack, Player player) {
-        if (player.isAddable()) {
-            requestCardAddition(blackJack, player);
-        }
     }
 
     private PlayerGroup initializePlayerGroup() {
@@ -58,5 +35,45 @@ public class BlackJackController {
             OutputView.printErrorMessage(exception.getMessage());
             return initializePlayerGroup();
         }
+    }
+
+    private void play() {
+        List<Player> players = playerGroup.getPlayers();
+        for (Player player : players) {
+            requestCardAddition(player);
+        }
+        OutputView.printDealerCardMessage(blackJack.addCardToDealer());
+    }
+
+    private void requestCardAddition(Player player) {
+        if (InputView.requestCardAddition(player.getName())) {
+            blackJack.addCardTo(player);
+            OutputView.printGamerCard(GamerCardsDto.of(player.getName(), player.getCardGroup()));
+            requestCardAdditionToPlayer(player);
+            return;
+        }
+        OutputView.printGamerCard(GamerCardsDto.of(player.getName(), player.getCardGroup()));
+    }
+
+    private void requestCardAdditionToPlayer(Player player) {
+        if (player.isAddable()) {
+            requestCardAddition(player);
+        }
+    }
+
+    private void finish() {
+        printCards();
+        printResult();
+    }
+
+    private void printCards() {
+        blackJack.openDealerCards();
+        OutputView.printGamersCardAndSum(GamerCardsDto.of(blackJack.getGamers()));
+    }
+
+    private void printResult() {
+        GameResult gameResult = blackJack.getGameResult();
+        OutputView.printGameResult(DealerResultDto.of(gameResult.getDealerResult()),
+                PlayerResultDto.of(gameResult.getPlayerResult()));
     }
 }
