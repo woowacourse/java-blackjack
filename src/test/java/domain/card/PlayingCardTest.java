2751da3c9a6535b7d93e63a6d4e3a32059329b0a
Richard JEON <ztzy1907@gmail.com>
2022-03-11 16:49:29 +0900

    test(playing-card): 플레잉카드 생성 및 동등성 테스트 추가

--- src/test/java/domain/card/PlayingCardTest.java
+++ src/test/java/domain/card/PlayingCardTest.java
@@ -3,21 +3,33 @@ package domain.card;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PlayingCardTest {
-    @Test
-    @DisplayName("카드 생성 테스트")
-    void createPlayingCard() {
+    @ParameterizedTest(name = "{0}+{1} - {2}")
+    @CsvSource(value = {"SPADES;ACE;(1,11)스페이드", "HEARTS;SEVEN;7하트"}, delimiter = ';')
+    @DisplayName("플레잉카드 생성 테스트")
+    void createPlayingCard(Suit suit, Denomination denomination, String expected) {
         // given
-        Suit expectedSuit = Suit.SPADES;
-        Denomination expectedDenomination = Denomination.ACE;
-        PlayingCard spadesAce = PlayingCard.of(expectedSuit, expectedDenomination);
+        PlayingCard playingCard = PlayingCard.of(suit, denomination);
 
         // when
-        String cardName = spadesAce.getCardName();
+        String cardName = playingCard.getCardName();
 
         // then
-        assertThat(cardName).isEqualTo("(1,11)스페이드");
+        assertThat(cardName).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "{0}-{1}")
+    @CsvSource(value = {"SPADES,ACE", "HEARTS,SEVEN"})
+    @DisplayName("플레잉카드 동등성 테스트")
+    void playingCardEqualityTest(Suit suit, Denomination denomination) {
+        // given
+        PlayingCard playingCard = PlayingCard.of(suit, denomination);
+        PlayingCard anotherCard = PlayingCard.of(suit, denomination);
+
+        // when & then
+        assertThat(playingCard).isEqualTo(anotherCard);
     }
 }
