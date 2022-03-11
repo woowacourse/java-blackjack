18ba77a67a5e2ef4a9cc3dd7741ffdf75b83f092
cjlee38 <cjlee38@github.com>
2022-03-11 17:11:53 +0900

    test(Users): users로부터 각 플레이어 별 승패를 구하는 로직 추가

--- src/test/java/blackjack/domain/user/UsersTest.java
+++ src/test/java/blackjack/domain/user/UsersTest.java
@@ -3,9 +3,14 @@ package blackjack.domain.user;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import blackjack.domain.MatchRecord;
+import blackjack.domain.card.Deck;
 
 class UsersTest {
 
@@ -24,4 +29,21 @@ class UsersTest {
                 () -> assertThat(users.getPlayers().size()).isEqualTo(2)
         );
     }
+
+    @Test
+    @DisplayName("플레이어의 승패를 구할 수 있다.")
+    public void testCreatePlayerMatchRecords() {
+        // given
+        Deck deck = new Deck();
+        List<String> names = List.of("pobi", "jason");
+        Users users = Users.from(names);
+
+        // when
+        users.getDealer().drawCard(deck);
+
+        // then
+        Map<Player, MatchRecord> playerMatchRecords = users.createPlayerMatchRecords();
+        assertThat(playerMatchRecords.values()).allMatch(matchRecord -> matchRecord == MatchRecord.LOSS);
+    }
+
 }
