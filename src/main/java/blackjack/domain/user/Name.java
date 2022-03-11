70d48c80c17d2d6693022ff1326c3e8609e16ed0
cjlee38 <cjlee38@github.com>
2022-03-11 15:34:29 +0900

    refactor(Name): name 생성 시 검증 로직 추가

--- src/main/java/blackjack/domain/user/Name.java
+++ src/main/java/blackjack/domain/user/Name.java
@@ -5,7 +5,14 @@ public class Name {
     private final String name;
 
     public Name(String name) {
+        validateExist(name);
         this.name = name;
+    }
+
+    private void validateExist(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException(String.format("이름에 오류가 있습니다 : %s", name));
+        }
     }
 
     public String getName() {
