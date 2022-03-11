6e44e84a69332961d032128baa01e8d52e45ce94
DOES <ldk980130@gmail.com>
2022-03-11 18:01:51 +0900

    refactor: 카드 추가 반복 로직 컨트롤러로 이동

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -7,51 +7,65 @@ import blackjack.domain.BlackJackGame;
 import blackjack.domain.gamer.Dealer;
 import blackjack.domain.gamer.Player;
 import blackjack.domain.result.BlackJackReferee;
+import blackjack.view.InputView;
+import blackjack.view.OutputView;
 
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
 public class BlackJackController {
 
-    private final BlackJackGame blackJackGame;
+	private final BlackJackGame blackJackGame;
 
-    public BlackJackController(List<String> names) {
-        blackJackGame = new BlackJackGame(names);
-        blackJackGame.distributeFirstCards();
-    }
+	public BlackJackController(List<String> names) {
+		blackJackGame = new BlackJackGame(names);
+		blackJackGame.distributeFirstCards();
+	}
 
-    public GamerDto getDealerDto() {
-        Dealer dealer = blackJackGame.getDealer();
-        return new GamerDto(dealer);
-    }
+	public GamerDto getDealerDto() {
+		Dealer dealer = blackJackGame.getDealer();
+		return new GamerDto(dealer);
+	}
 
-    public List<GamerDto> getPlayerDtos() {
-        List<Player> players = blackJackGame.getPlayers();
-        return players.stream()
-                .map((GamerDto::new))
-                .collect(Collectors.toUnmodifiableList());
-    }
+	public void askHitOrStay(List<String> names,
+							UnaryOperator<String> operator,
+							Consumer<GamerDto> consumer) {
+		for (String name : names) {
+			hitOrStay(operator, consumer, name);
+		}
+	}
 
-    public boolean isDrawPossible(String name, UnaryOperator<String> operator) {
-        return !blackJackGame.isBurst(name) && Answer.from(operator.apply(name)).isYes();
-    }
+	private void hitOrStay(UnaryOperator<String> operator, Consumer<GamerDto> consumer, String name) {
+		while (!blackJackGame.isBurst(name) && Answer.from(operator.apply(name)).isYes()) {
+			requestPlayerDrawCard(name);
+			consumer.accept(findPlayerByName(name));
+		}
+	}
 
-    public void requestPlayerDrawCard(String name) {
-        blackJackGame.distributeCardToPlayer(name);
-    }
+	private GamerDto findPlayerByName(String name) {
+		Player player = blackJackGame.findPlayerByName(name);
+		return new GamerDto(player);
+	}
 
-    public GamerDto findPlayerByName(String name) {
-        Player player = blackJackGame.findPlayerByName(name);
-        return new GamerDto(player);
-    }
+	public List<GamerDto> getPlayerDtos() {
+		List<Player> players = blackJackGame.getPlayers();
+		return players.stream()
+			.map((GamerDto::new))
+			.collect(Collectors.toUnmodifiableList());
+	}
 
-    public int getDealerAdditionalCardCount() {
-        return blackJackGame.distributeAdditionalToDealer();
-    }
+	public void requestPlayerDrawCard(String name) {
+		blackJackGame.distributeCardToPlayer(name);
+	}
 
-    public GameResultDto getGamerResult() {
-        BlackJackReferee result = blackJackGame.createResult();
-        return new GameResultDto(result);
-    }
+	public int getDealerAdditionalCardCount() {
+		return blackJackGame.distributeAdditionalToDealer();
+	}
+
+	public GameResultDto getGamerResult() {
+		BlackJackReferee result = blackJackGame.createResult();
+		return new GameResultDto(result);
+	}
 }
