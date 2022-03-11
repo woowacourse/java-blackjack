acf1aba0c8c30cf8a6ce14e2be1c348f48544f12
YJGwon <yj970125@gmail.com>
2022-03-11 21:00:15 +0900

    refactor: Application.main 메서드 분리

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -13,28 +13,30 @@ public class Application {
 
     public static void main(String[] args) {
         final InputView inputView = new InputView();
-        List<String> names = inputView.askEntryNames();
+        final ResultView resultView = new ResultView();
 
+        Game game = startGame(inputView, resultView);
+        playEntries(inputView, resultView, game);
+        playDealer(resultView, game);
+        showResults(resultView, game);
+    }
+
+    private static Game startGame(InputView inputView, ResultView resultView) {
+        List<String> names = inputView.askEntryNames();
         Game game = new Game(names);
         game.start();
 
-        final ResultView resultView = new ResultView();
         PlayersDTO playersDTO = PlayersDTO.from(game);
         resultView.printDeckInitialized(playersDTO);
         resultView.printInitializedDecks(playersDTO);
+        return game;
+    }
 
+    private static void playEntries(InputView inputView, ResultView resultView, Game game) {
         do {
             game.toNextEntry();
             playTurn(inputView, game, resultView);
         } while (game.hasNextEntry());
-
-        game.hitDealer();
-        if (game.countCardsAddedToDealer() > 0) {
-            resultView.printDealerHitCount(DealerDTO.from(game));
-        }
-
-        resultView.printScores(PlayersDTO.from(game));
-        resultView.printResults(ResultsDTO.from(game.getResults()), DealerDTO.from(game));
     }
 
     private static void playTurn(InputView inputView, Game game, ResultView resultView) {
@@ -47,8 +49,24 @@ public class Application {
             resultView.printDeck(entryDTO);
             return;
         }
+        hitCurrentEntry(inputView, game, resultView);
+    }
+
+    private static void hitCurrentEntry(InputView inputView, Game game, ResultView resultView) {
         game.hitCurrentEntry();
         resultView.printDeck(EntryDTO.fromCurrent(game));
         playTurn(inputView, game, resultView);
+    }
+
+    private static void playDealer(ResultView resultView, Game game) {
+        game.hitDealer();
+        if (game.countCardsAddedToDealer() > 0) {
+            resultView.printDealerHitCount(DealerDTO.from(game));
+        }
+    }
+
+    private static void showResults(ResultView resultView, Game game) {
+        resultView.printScores(PlayersDTO.from(game));
+        resultView.printResults(ResultsDTO.from(game.getResults()), DealerDTO.from(game));
     }
 }
