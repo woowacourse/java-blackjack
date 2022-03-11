6a4b328a91913f8b7390950789d089d7cc585aa0
RIANAEH <ririhan217@gmail.com>
2022-03-11 13:44:42 +0900

    refactor: BlackJackController에서 메서드 분리

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -15,39 +15,75 @@ import java.util.List;
 public class BlackJackController {
 
     public void run() {
-        BlackJackMachine blackJackMachine = new BlackJackMachine(new CardDeck());
-        List<String> names = InputView.getParticipantNames();
-        Dealer dealer = new Dealer();
-        Participants participants = new Participants(names);
-        blackJackMachine.giveInitialCards(dealer, participants);
+        final BlackJackMachine blackJackMachine = new BlackJackMachine(new CardDeck());
+        final Dealer dealer = new Dealer();
+        final Participants participants = getParticipants();
 
+        giveInitialCardsToPlayer(blackJackMachine, dealer, participants);
+        askAndGiveCardsToParticipants(blackJackMachine, participants);
+        giveCardsToDealer(blackJackMachine, dealer);
+
+        calculateTotalScores(dealer, participants);
+        decideResults(dealer, participants);
+    }
+
+    private Participants getParticipants() {
+        try {
+            final List<String> names = InputView.getParticipantNames();
+            return new Participants(names);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return getParticipants();
+        }
+    }
+
+    private void giveInitialCardsToPlayer(final BlackJackMachine blackJackMachine, final Dealer dealer,
+                                          final Participants participants) {
+        blackJackMachine.giveInitialCards(dealer, participants);
         OutputView.printInitialCards(dealer, participants);
+    }
+
+    private void askAndGiveCardsToParticipants(final BlackJackMachine blackJackMachine, final Participants participants) {
         for (Participant participant : participants) {
             askAndGiveCardToParticipant(blackJackMachine, participant);
         }
+    }
 
+    private void askAndGiveCardToParticipant(final BlackJackMachine blackJackMachine, final Participant participant) {
+        Choice choice;
+        do {
+            choice = getChoice(participant);
+            blackJackMachine.giveCardToParticipant(participant, choice);
+            OutputView.printPlayerCards(participant);
+        } while (choice == Choice.YES && participant.canAddCard());
+    }
+
+    private Choice getChoice(final Participant participant) {
+        try {
+            return InputView.getChoice(participant);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return getChoice(participant);
+        }
+    }
+
+    private void giveCardsToDealer(final BlackJackMachine blackJackMachine, final Dealer dealer) {
         while (dealer.canAddCard()) {
             blackJackMachine.giveCardToDealer(dealer);
             OutputView.printDealerGetCardMessage(dealer);
         }
+    }
 
+    private void calculateTotalScores(Dealer dealer, Participants participants) {
         OutputView.printTotalScore(dealer, dealer.getTotalScore());
         for (Participant participant : participants) {
             OutputView.printTotalScore(participant, participant.getTotalScore());
         }
-
-        ParticipantResult results = new ParticipantResult(dealer, participants);
-        DealerResult dealerResult = new DealerResult(results.getDealerResult());
-
-        OutputView.printResults(dealer, dealerResult, results);
     }
 
-    private void askAndGiveCardToParticipant(BlackJackMachine blackJackMachine, Participant participant) {
-        Choice choice;
-        do {
-            choice = InputView.getChoice(participant);
-            blackJackMachine.giveCardToParticipant(participant, choice);
-            OutputView.printPlayerCards(participant);
-        } while (choice == Choice.YES && participant.canAddCard());
+    private void decideResults(final Dealer dealer, final Participants participants) {
+        final ParticipantResult participantResult = new ParticipantResult(dealer, participants);
+        final DealerResult dealerResult = new DealerResult(participantResult.getDealerResult());
+        OutputView.printResults(dealer, dealerResult, participantResult);
     }
 }
