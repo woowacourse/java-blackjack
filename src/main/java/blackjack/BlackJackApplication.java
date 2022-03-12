9ccf97d1639f04e2386a36054cf2f2c22fc16147
Ohzzi <clover7even@naver.com>
2022-03-12 22:31:10 +0900

    fix: main에서 try-catch 제거

--- src/main/java/blackjack/BlackJackApplication.java
+++ src/main/java/blackjack/BlackJackApplication.java
@@ -1,15 +1,9 @@
 package blackjack;
 
-import blackjack.view.OutputView;
-
 public class BlackJackApplication {
 
     public static void main(String[] args) {
-        try {
-            BlackJackGame blackJackGame = new BlackJackGame();
-            blackJackGame.play();
-        } catch (NullPointerException | IllegalArgumentException e) {
-            OutputView.printFatalErrorMessage(e.getMessage());
-        }
+        BlackJackGame blackJackGame = new BlackJackGame();
+        blackJackGame.play();
     }
 }
--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -16,11 +16,6 @@ public class OutputView {
         System.out.println(message);
     }
 
-    public static void printFatalErrorMessage(String message) {
-        System.err.println(message);
-        System.err.println("심각한 오류가 발생하여 프로그램을 종료합니다.");
-    }
-
     public static void printStartMessage(Dealer dealer, List<Player> players) {
         List<String> playerNames = players.stream()
                 .map(Player::getName)
