89843f7e7988a1a199b3e7c13cede08d2fad67f3
yoon <duddbs774@gmail.com>
2022-03-12 15:14:37 +0900

    feat: equals, hashCode, toString 재정의

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -1,5 +1,7 @@
 package domain.card;
 
+import java.util.Objects;
+
 public final class Card {
     private final Suit suit;
     private final Face face;
@@ -15,5 +17,30 @@ public final class Card {
 
     public int getScore() {
         return face.getScore();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return suit == card.suit && face == card.face;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(suit, face);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit=" + suit +
+                ", face=" + face +
+                '}';
     }
 }
