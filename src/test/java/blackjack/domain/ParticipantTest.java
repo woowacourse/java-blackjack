8053637c1f23c6af693a22060a18a2d1f822cdf6
ulimy <ulim130@gmail.com>
2022-03-12 15:36:47 +0900

    test : 에이스를 1로 계산하는 경우의 테스트 추가

--- src/test/java/blackjack/domain/ParticipantTest.java
+++ src/test/java/blackjack/domain/ParticipantTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ParticipantTest {
 
     @Test
-    @DisplayName("점수를 올바르게 계산하는지 확인한다")
+    @DisplayName("점수를 올바르게 계산하는지 확인한다 : 에이스가 존재하지 않는 경우")
     void testCalculateScore() {
         Participant participant = new Participant("pobi");
         participant.addCard(new Card(Symbol.DIAMOND, Denomination.KING));
@@ -23,11 +23,23 @@ class ParticipantTest {
     }
 
     @Test
-    @DisplayName("더 유리한 에이스 값 선택하여 결과를 계산하는지 확인한다.")
-    void calculateScoreContainsAce() {
+    @DisplayName("더 유리한 에이스 값 선택하여 결과를 계산하는지 확인한다. - 11점 선택")
+    void computeAceAsEleven() {
         Participant participant = new Participant("pobi");
         participant.addCard(new Card(Symbol.CLOVER, Denomination.EIGHT));
         participant.addCard(new Card(Symbol.HEART, Denomination.TWO));
+        participant.addCard(new Card(Symbol.SPADE, Denomination.ACE));
+        participant.endTurn();
+
+        assertThat(participant.getScore()).isEqualTo(21);
+    }
+
+    @Test
+    @DisplayName("더 유리한 에이스 값 선택하여 결과를 계산하는지 확인한다. - 1점 선택")
+    void computeAceAsOne() {
+        Participant participant = new Participant("pobi");
+        participant.addCard(new Card(Symbol.CLOVER, Denomination.KING));
+        participant.addCard(new Card(Symbol.HEART, Denomination.QUEEN));
         participant.addCard(new Card(Symbol.SPADE, Denomination.ACE));
         participant.endTurn();
 
