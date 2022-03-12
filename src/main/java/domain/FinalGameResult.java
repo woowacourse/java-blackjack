fe507f95efc7b0df91a513c6b55c8c6de682b435
yoon <duddbs774@gmail.com>
2022-03-12 13:06:22 +0900

    feat: 게임 최종 결과를 반대로 계산하는 기능 구현

--- src/main/java/domain/FinalGameResult.java
+++ src/main/java/domain/FinalGameResult.java
@@ -1,21 +1,23 @@
 package domain;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
 
-public enum GameResult {
+public enum FinalGameResult {
     WIN((a, b) -> compareCardStatePower(a, b) > 0 || (isBothStand(a, b) && compareCardScore(a, b) > 0)),
     DRAW((a, b) -> isBothBlackJack(a, b) || (isBothStand(a, b) && compareCardScore(a, b) == 0)),
     LOSE((a, b) -> a.isBust() || compareCardStatePower(a, b) < 0 || (isBothStand(a, b) && compareCardScore(a, b) < 0));
 
     private final BiPredicate<Cards, Cards> biPredicate;
 
-    GameResult(BiPredicate<Cards, Cards> biPredicate) {
+    FinalGameResult(BiPredicate<Cards, Cards> biPredicate) {
         this.biPredicate = biPredicate;
     }
 
-    public static GameResult of(Cards playerCards, Cards dealerCards) {
-        return Arrays.stream(GameResult.values())
+    public static FinalGameResult of(Cards playerCards, Cards dealerCards) {
+        return Arrays.stream(FinalGameResult.values())
                 .filter(gameResult -> gameResult.biPredicate.test(playerCards, dealerCards))
                 .findFirst()
                 .orElseThrow(IllegalArgumentException::new);
@@ -35,5 +37,22 @@ public enum GameResult {
 
     private static boolean isBothBlackJack(Cards playerCards, Cards dealerCards) {
         return playerCards.isBlackJack() && dealerCards.isBlackJack();
+    }
+
+    public static List<FinalGameResult> reverse(List<FinalGameResult> origin) {
+        return origin
+                .stream()
+                .map(FinalGameResult::reverseOf)
+                .collect(Collectors.toList());
+    }
+
+    private static FinalGameResult reverseOf(FinalGameResult origin) {
+        if (origin == WIN) {
+            return LOSE;
+        }
+        if (origin == LOSE) {
+            return WIN;
+        }
+        return DRAW;
     }
 }
