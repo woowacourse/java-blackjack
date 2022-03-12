50c89d77c4ceac3020537d612869ddb1811b4ebe
RIANAEH <ririhan217@gmail.com>
2022-03-12 21:47:09 +0900

    feat: Participants 인원 예외 처리 추가

--- src/main/java/blackjack/domain/Participants.java
+++ src/main/java/blackjack/domain/Participants.java
@@ -6,14 +6,25 @@ import java.util.stream.Collectors;
 
 public class Participants implements Iterable<Participant> {
 
+    private static final int MIN_SIZE = 2;
+    private static final int MAX_SIZE = 8;
+
     private final List<Participant> participants;
 
     public Participants(final List<String> names) {
+        checkSize(names);
         checkDuplicatedNames(names);
 
         participants = names.stream()
                 .map(Participant::new)
                 .collect(Collectors.toList());
+    }
+
+    private void checkSize(final List<String> names) {
+        final int size = names.size();
+        if (size < MAX_SIZE || size > MAX_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 참자가 인원은 2명~8명 사이여야합니다.");
+        }
     }
 
     private void checkDuplicatedNames(final List<String> names) {
--- src/test/java/blackjack/domain/ParticipantsTest.java
+++ src/test/java/blackjack/domain/ParticipantsTest.java
@@ -3,8 +3,12 @@ package blackjack.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class ParticipantsTest {
 
@@ -15,5 +19,22 @@ public class ParticipantsTest {
         assertThatThrownBy(() -> new Participants(names))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 참가자의 이름은 중복될 수 없습니다.");
+    }
+
+    @ParameterizedTest(name = "[{index}] 참가자 {0}")
+    @MethodSource("generateNames")
+    @DisplayName("참가자의 인원이 2명~8명 사이가 아닐 경우 예외를 발생시킨다.")
+    void throwExceptionWhenOutOfBounds(List<String> names) {
+        assertThatThrownBy(() -> new Participants(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 참자가 인원은 2명~8명 사이여야합니다.");
+    }
+
+    static Stream<Arguments> generateNames() {
+        return Stream.of(
+                Arguments.of(List.of("a")),
+                Arguments.of(List.of("a", "b", "c", "d", "e", "f", "g", "h", "i")),
+                Arguments.of(List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j"))
+        );
     }
 }
