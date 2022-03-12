2bee476f409f4e734a5b19adac314d70e1d981b5
Ohzzi <clover7even@naver.com>
2022-03-12 21:45:22 +0900

    test: 더 작은 단위에서 테스트 된 내용 제거

--- src/test/java/blackjack/domain/WinResultTest.java
+++ src/test/java/blackjack/domain/WinResultTest.java
@@ -4,177 +4,49 @@ import static blackjack.domain.Outcome.DRAW;
 import static blackjack.domain.Outcome.LOSE;
 import static blackjack.domain.Outcome.WIN;
 import static blackjack.domain.card.Denomination.ACE;
-import static blackjack.domain.card.Denomination.EIGHT;
-import static blackjack.domain.card.Denomination.FIVE;
 import static blackjack.domain.card.Denomination.NINE;
-import static blackjack.domain.card.Denomination.SIX;
 import static blackjack.domain.card.Denomination.TEN;
-import static blackjack.domain.card.Denomination.TWO;
 import static blackjack.domain.card.Pattern.CLOVER;
 import static blackjack.domain.card.Pattern.DIAMOND;
-import static blackjack.domain.card.Pattern.HEART;
-import static blackjack.domain.card.Pattern.SPADE;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import blackjack.domain.card.Card;
-import blackjack.domain.card.CardDeck;
 import blackjack.domain.card.Denomination;
 import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Player;
-import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 public class WinResultTest {
 
-    @ParameterizedTest
-    @MethodSource("provideResultForNotBust")
-    @DisplayName("딜러와 플레이어 둘 다 버스트하지 않았을 경우 점수가 더 큰 쪽이 이긴다.")
-    void bothNotBust(Dealer dealer, Map<Outcome, Integer> judgementMap, Outcome playerOutcome) {
-        // given
-        Card heartTen = new Card(HEART, TEN);
-        Card spadeNine = new Card(SPADE, NINE);
-        List<Card> playerCards = List.of(heartTen, spadeNine);
-        Player player = new Player(new Name("pobi"), playerCards);
-        List<Player> players = List.of(player);
-
-        // when
-        WinResult winResult = new WinResult(dealer, players);
-        Map<Outcome, Integer> dealerResult = winResult.getDealerResult();
-        Map<String, Outcome> playersResult = winResult.getPlayersResult();
-
-        // then
-        assertAll(
-                () -> assertThat(dealerResult).isEqualTo(judgementMap),
-                () -> assertThat(playersResult.get(player.getName())).isEqualTo(playerOutcome)
-        );
-    }
-
-    private static Stream<Arguments> provideResultForNotBust() {
-        return Stream.of(
-                Arguments.of(createDealer(TEN), createJudgementMap(1, 0, 0), LOSE),
-                Arguments.of(createDealer(NINE), createJudgementMap(0, 1, 0), DRAW),
-                Arguments.of(createDealer(EIGHT), createJudgementMap(0, 0, 1), WIN)
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideForPlayerBust")
-    @DisplayName("플레이어가 버스트면 무조건 딜러가 이긴다")
-    void playerBust(CardDeck deck) {
-        // given
-        Dealer dealer = createDealer(SIX);
-        dealer.hit(deck);
-
-        Card heartTen = new Card(HEART, TEN);
-        Card spadeNine = new Card(SPADE, TEN);
-        List<Card> playerCards = List.of(heartTen, spadeNine);
-        Player player = new Player(new Name("pobi"), playerCards);
-        player.hit(deck);
-        List<Player> players = List.of(player);
-
-        // when
-        WinResult winResult = new WinResult(dealer, players);
-        Map<Outcome, Integer> dealerResult = winResult.getDealerResult();
-        Map<String, Outcome> playersResult = winResult.getPlayersResult();
-        Map<Outcome, Integer> judgementMap = createJudgementMap(1, 0, 0);
-
-        // then
-        assertAll(
-                () -> assertThat(dealerResult).isEqualTo(judgementMap),
-                () -> assertThat(playersResult.get(player.getName())).isEqualTo(LOSE)
-        );
-    }
-
-    private static Stream<Arguments> provideForPlayerBust() {
-        return Stream.of(
-                Arguments.of(new CardDeck(() -> new ArrayList<>(List.of(new Card(HEART, SIX), new Card(HEART, TWO))))),
-                Arguments.of(new CardDeck(() -> new ArrayList<>(List.of(new Card(HEART, FIVE), new Card(HEART, TWO)))))
-        );
-    }
-
     @Test
-    @DisplayName("플레이어가 버스트가 아니고 딜러가 버스트면 플레이어가 이긴다")
-    void dealerBust() {
+    @DisplayName("승패 결과를 Map에 저장한다")
+    void saveIntoMap() {
         // given
-        Dealer dealer = createDealer(SIX);
-        dealer.hit(new CardDeck(() -> new ArrayList<>(List.of(new Card(HEART, SIX)))));
+        Dealer dealer = createDealer(TEN);
 
-        Card heartTen = new Card(HEART, TEN);
-        Card spadeNine = new Card(SPADE, TEN);
-        List<Card> playerCards = List.of(heartTen, spadeNine);
-        Player player = new Player(new Name("pobi"), playerCards);
-        List<Player> players = List.of(player);
+        Player winPlayer = createPlayer("win", ACE);
+        Player drawPlayer = createPlayer("draw", TEN);
+        Player losePlayer = createPlayer("lose", NINE);
+        List<Player> players = List.of(winPlayer, drawPlayer, losePlayer);
+
+        Map<Outcome, Integer> outcomeMap = createOutcomeMap(1, 1, 1);
 
         // when
         WinResult winResult = new WinResult(dealer, players);
         Map<Outcome, Integer> dealerResult = winResult.getDealerResult();
         Map<String, Outcome> playersResult = winResult.getPlayersResult();
-        Map<Outcome, Integer> judgementMap = createJudgementMap(0, 0, 1);
 
         // then
         assertAll(
-                () -> assertThat(dealerResult).isEqualTo(judgementMap),
-                () -> assertThat(playersResult.get(player.getName())).isEqualTo(WIN)
-        );
-    }
-
-    @Test
-    @DisplayName("똑같이 21점이어도 블랙잭이 이긴다.")
-    void blackJackDoesNotDefeat() {
-        // given
-        Dealer dealer = createDealer(ACE);
-
-        Card heartTen = new Card(HEART, TEN);
-        Card spadeNine = new Card(SPADE, TEN);
-        List<Card> playerCards = List.of(heartTen, spadeNine);
-        Player player = new Player(new Name("pobi"), playerCards);
-        player.hit(new CardDeck(() -> new ArrayList<>(List.of(new Card(HEART, ACE)))));
-        List<Player> players = List.of(player);
-
-        // when
-        WinResult winResult = new WinResult(dealer, players);
-        Map<Outcome, Integer> dealerResult = winResult.getDealerResult();
-        Map<String, Outcome> playersResult = winResult.getPlayersResult();
-        Map<Outcome, Integer> judgementMap = createJudgementMap(1, 0, 0);
-
-        // then
-        assertAll(
-                () -> assertThat(dealerResult).isEqualTo(judgementMap),
-                () -> assertThat(playersResult.get(player.getName())).isEqualTo(LOSE)
-        );
-    }
-
-    @Test
-    @DisplayName("블랙잭 끼리는 비긴다")
-    void blackJackDrawWithBlackJack() {
-        // given
-        Dealer dealer = createDealer(ACE);
-
-        Card heartTen = new Card(HEART, TEN);
-        Card spadeNine = new Card(SPADE, ACE);
-        List<Card> playerCards = List.of(heartTen, spadeNine);
-        Player player = new Player(new Name("pobi"), playerCards);
-        List<Player> players = List.of(player);
-
-        // when
-        WinResult winResult = new WinResult(dealer, players);
-        Map<Outcome, Integer> dealerResult = winResult.getDealerResult();
-        Map<String, Outcome> playersResult = winResult.getPlayersResult();
-        Map<Outcome, Integer> judgementMap = createJudgementMap(0, 1, 0);
-
-        // then
-        assertAll(
-                () -> assertThat(dealerResult).isEqualTo(judgementMap),
-                () -> assertThat(playersResult.get(player.getName())).isEqualTo(DRAW)
+                () -> assertThat(dealerResult).isEqualTo(outcomeMap),
+                () -> assertThat(playersResult.get(winPlayer.getName())).isEqualTo(WIN),
+                () -> assertThat(playersResult.get(drawPlayer.getName())).isEqualTo(DRAW),
+                () -> assertThat(playersResult.get(losePlayer.getName())).isEqualTo(LOSE)
         );
     }
 
@@ -185,7 +57,14 @@ public class WinResultTest {
         return new Dealer(dealerCards);
     }
 
-    private static Map<Outcome, Integer> createJudgementMap(int win, int draw, int lose) {
+    private static Player createPlayer(String name, Denomination denomination2) {
+        Card card1 = new Card(DIAMOND, TEN);
+        Card card2 = new Card(CLOVER, denomination2);
+        List<Card> playerCards = List.of(card1, card2);
+        return new Player(new Name(name), playerCards);
+    }
+
+    private static Map<Outcome, Integer> createOutcomeMap(int win, int draw, int lose) {
         Map<Outcome, Integer> judgementMap = new EnumMap<>(Outcome.class);
         judgementMap.put(WIN, win);
         judgementMap.put(DRAW, draw);
