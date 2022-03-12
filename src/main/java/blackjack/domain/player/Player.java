41198c7a59138c534b21b6938a5203b67eeb8661
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-13 02:29:22 +0900

    feat: 플레이어 첫드로우 카드 반환기능 추가 및 테스트

--- src/main/java/blackjack/domain/player/Player.java
+++ src/main/java/blackjack/domain/player/Player.java
@@ -1,18 +1,27 @@
 package blackjack.domain.player;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Cards;
 import blackjack.domain.participant.GameStatus;
+import java.util.List;
 import java.util.Objects;
 
 public class Player {
 
     private final String name;
+    private final Cards cards;
     private GameStatus gameStatus;
 
-    public Player(final String name, final GameStatus gameStatus) {
+    private Player(final String name, final Cards cards, final GameStatus gameStatus) {
         Objects.requireNonNull(name, "이름이 null이 들어올 수 없습니다.");
         validateBlankName(name);
         this.name = name;
+        this.cards = cards;
         this.gameStatus = gameStatus;
+    }
+
+    public static Player createNewPlayer(final String name, final List<Card> cards) {
+        return new Player(name, new Cards(cards), GameStatus.RUNNING);
     }
 
     private void validateBlankName(final String name) {
@@ -30,5 +39,9 @@ public class Player {
         if (gameStatus.isFinishedGame()) {
             throw new IllegalStateException("이미 종료된 게임은 종료요청을 할 수 없습니다.");
         }
+    }
+
+    public List<Card> firstDrawCards() {
+        return List.copyOf(cards.cards().subList(0, 2));
     }
 }
--- src/test/java/blackjack/domain/player/PlayerTest.java
+++ src/test/java/blackjack/domain/player/PlayerTest.java
@@ -1,8 +1,15 @@
 package blackjack.domain.player;
 
+import static blackjack.domain.card.CardNumber.SEVEN;
+import static blackjack.domain.card.CardNumber.TEN;
+import static blackjack.domain.card.CardPattern.SPADE;
+import static blackjack.testutil.CardFixtureGenerator.createCards;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import blackjack.domain.participant.GameStatus;
+import blackjack.domain.card.Card;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,10 +17,17 @@ import org.junit.jupiter.params.provider.EmptySource;
 
 public class PlayerTest {
 
+    private List<Card> cards;
+
+    @BeforeEach
+    void setup() {
+        cards = createCards(Card.of(SPADE, TEN), Card.of(SPADE, SEVEN));
+    }
+
     @Test
     @DisplayName("플레이어의 이름이 null이 들어올 경우 예외가 발생해야 한다.")
     void createExceptionByNull() {
-        assertThatThrownBy(() -> new Player(null, GameStatus.RUNNING))
+        assertThatThrownBy(() -> Player.createNewPlayer("player", cards))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("이름이 null이 들어올 수 없습니다.");
     }
@@ -22,7 +36,7 @@ public class PlayerTest {
     @EmptySource
     @DisplayName("플레이어의 이름이 공백이 들어올 경우 예외가 발생해야 한다.")
     void createExceptionByBlank(final String input) {
-        assertThatThrownBy(() -> new Player(input, GameStatus.RUNNING))
+        assertThatThrownBy(() -> Player.createNewPlayer("player", cards))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름이 공백이 들어올 수 없습니다.");
     }
@@ -30,9 +44,17 @@ public class PlayerTest {
     @Test
     @DisplayName("현재 상태가 종료되었는데 상태를 변경하면 예외가 발생해야 한다.")
     void changeGameStatusExceptionByFinished() {
-        final Player player = new Player("player", GameStatus.FINISHED);
+        final Player player = Player.createNewPlayer("player", cards);
+        player.changeStatusFinished();
         assertThatThrownBy(() -> player.changeStatusFinished())
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("이미 종료된 게임은 종료요청을 할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("첫번째 카드 정보를 반환할 수 있다.")
+    void firstDrawCards() {
+        final Player player = Player.createNewPlayer("player", cards);
+        assertThat(player.firstDrawCards()).isEqualTo(cards);
     }
 }
