3f646fc1bf2f541a6a16ff9385ef94aeab30e448
giron <ggyucheol0@gmail.com>
2022-03-12 23:09:23 +0900

    test: 유저 입력한 만큼 참가자 생성되는지 테스트

--- src/test/java/BlackJack/domain/User/PlayersTest.java
+++ src/test/java/BlackJack/domain/User/PlayersTest.java
@@ -3,16 +3,17 @@ package BlackJack.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PlayersTest {
 
     @Test
-    @DisplayName("참가자들이 ")
-    void join() {
-    }
-
-    @Test
-    void getStatistics() {
+    @DisplayName("유저가 이름을 입력한만큼 참가자들 생성되는지 테스트")
+    void joinTest() {
+        List<String> input = List.of("기론", "열음");
+        Players players = Players.join(input);
+        assertThat(players.size()).isEqualTo(input.size());
     }
 }
