129f566fa5edf23bbe4a6e89548db6c583b4554d
Hyunta <mohai2618@gmail.com>
2022-03-12 16:52:15 +0900

    test(Participants): 턴을 다음 플레이어로 넘기는 기능 확인

--- src/test/java/blackjack/domain/participant/ParticipantsTest.java
+++ src/test/java/blackjack/domain/participant/ParticipantsTest.java
@@ -1,6 +1,8 @@
 package blackjack.domain.participant;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,5 +17,18 @@ class ParticipantsTest {
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("턴을 스킵했을 때 다음 참가자로 넘어가는지 테스트")
+    void playersSkipTest() {
+        Player pobi = new Player("pobi", new ArrayList<>());
+        Player elsa = new Player("elsa", new ArrayList<>());
+        List<Participant> players = Arrays.asList(pobi, elsa);
+
+        Participants participants = new Participants(players);
+        participants.skipTurn();
+        Participant nextPlayer = participants.getCurrentPlayer();
+
+        Assertions.assertThat(nextPlayer).isEqualTo(elsa);
+    }
 
 }
