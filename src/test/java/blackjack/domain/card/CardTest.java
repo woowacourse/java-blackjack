4c6bec990823568a9bccd10a7598b1e9e514c69b
sure-why-not <pkse1213@gmail.com>
2022-03-13 03:46:11 +0900

    refactor: 카드 생성시 원하는 모양, 끗수로 잘 생성되는지 확인하는 테스트 추가

--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -1,7 +1,9 @@
 package blackjack.domain.card;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,9 +13,15 @@ public class CardTest {
     @DisplayName("카드를 생성한다.")
     void createCard() {
         // when
-        Card card = new Card(Pattern.DIAMOND, Denomination.ACE);
+        Pattern pattern = Pattern.DIAMOND;
+        Denomination denomination = Denomination.ACE;
+        Card card = new Card(pattern, denomination);
 
         // then
-        assertThat(card).isNotNull();
+        assertAll(
+            () -> assertThat(card).isNotNull(),
+            () -> assertThat(card.getPattern()).isEqualTo(pattern),
+            () -> assertThat(card.getDenomination()).isEqualTo(denomination)
+        );
     }
 }
