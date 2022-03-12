a2f456c9ae5f4360bca2f9d05cd4ec6da0f9580e
Jeong Jinwoo <jwjeong96@gmail.com>
2022-03-12 22:28:05 +0900

    feat(ResultStatsDto): implement dto for result output view

---
+++ src/main/java/blackjack/dto/ResultStatsDto.java
@@ -0,0 +1,66 @@
+package blackjack.dto;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.game.ResultCount;
+import blackjack.domain.game.ResultType;
+import blackjack.domain.game.Score;
+import blackjack.domain.participant.Participant;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ResultStatsDto {
+
+    private static final String INVALID_RESULT_STATS_INITIALIZATION_EXCEPTION_MESSAGE = "승패 통계 정보가 존재하지 않는 데이터입니다.";
+
+    private final ParticipantCardsDto participantCardsDto;
+    private final Map<ResultType, ResultCount> participantResultStats = new EnumMap<>(ResultType.class);
+
+    public ResultStatsDto(Participant participant, Map<ResultType, ResultCount> stats) {
+        this.participantCardsDto = ParticipantCardsDto.of(participant);
+        initializeResultStats(stats);
+        validateResultStats();
+    }
+
+    private void initializeResultStats(Map<ResultType, ResultCount> stats) {
+        stats.keySet()
+                .stream()
+                .filter(resultType -> stats.get(resultType).toInt() > 0)
+                .forEach(resultType -> this.participantResultStats.put(resultType, stats.get(resultType)));
+    }
+
+    private void validateResultStats() {
+        if (participantResultStats.keySet().isEmpty()) {
+            throw new IllegalArgumentException(INVALID_RESULT_STATS_INITIALIZATION_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public boolean hasSingleResultType() {
+        return participantResultStats.keySet().size() == 1;
+    }
+
+    public String getParticipantName() {
+        return participantCardsDto.getName();
+    }
+
+    public Set<Card> getCards() {
+        return participantCardsDto.getCards();
+    }
+
+    public Score getScore() {
+        return participantCardsDto.getScore();
+    }
+
+    public Map<ResultType, ResultCount> getResultStats() {
+        return Collections.unmodifiableMap(participantResultStats);
+    }
+
+    @Override
+    public String toString() {
+        return "ResultStatsDto{" +
+                "participantCardsDto=" + participantCardsDto +
+                ", participantResultStats=" + participantResultStats +
+                '}';
+    }
+}
