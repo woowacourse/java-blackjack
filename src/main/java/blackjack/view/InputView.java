005824d1d5150b5c8a1b28feff223868656e1b14
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-13 06:54:50 +0900

    feat: replace -> trim

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -10,8 +10,6 @@ public class InputView {
     private static final String INPUT_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String INPUT_HIT_COMMAND_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)%n";
 
-    private static final String PLAYER_NAME_BLANK = " ";
-    private static final String PLAYER_NAME_NOT_BLANK = "";
     private static final String PLAYER_NAME_DELIMITER = ",";
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -23,7 +21,7 @@ public class InputView {
     public static List<String> inputPlayerNames() {
         System.out.println(INPUT_PLAYER_NAMES_MESSAGE);
         return Arrays.stream(scanner.nextLine().split(PLAYER_NAME_DELIMITER))
-                .map(name -> name.replace(PLAYER_NAME_BLANK, PLAYER_NAME_NOT_BLANK))
+                .map(String::trim)
                 .collect(Collectors.toUnmodifiableList());
     }
 
