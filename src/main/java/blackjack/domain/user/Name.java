e5e31fb309d6c845782369e4580da1ff7e95c00e
EastHShin <xldk78@gmail.com>
2022-03-13 18:28:23 +0900

    refactor(blackjack/domain/user/Name): 공백을 검사해 에러를 발생시키는 방식에서 공백을 제거하는 방식으로 변경

--- src/main/java/blackjack/domain/user/Name.java
+++ src/main/java/blackjack/domain/user/Name.java
@@ -2,21 +2,23 @@ package blackjack.domain.user;
 
 import static blackjack.domain.exceptionMessages.UserExceptionMessage.*;
 
-import java.util.regex.Pattern;
-
 public class Name {
-	private static final Pattern BLANK = Pattern.compile("[\\s]+");
-
 	private final String name;
 
 	public Name(final String name) {
+		final String blankRemovedName = removeBlank(name);
+		validateEmptyName(blankRemovedName);
+		this.name = blankRemovedName;
+	}
+
+	private void validateEmptyName(String name) {
 		if (name == null || name.isEmpty()) {
 			throw new IllegalArgumentException(EMPTY_NAME_EXCEPTION.getMessage());
 		}
-		if (BLANK.matcher(name).matches()) {
-			throw new IllegalArgumentException(BLANK_NAME_EXCEPTION.getMessage());
-		}
-		this.name = name;
+	}
+
+	private String removeBlank(final String name) {
+		return name.trim();
 	}
 
 	public String getName() {
