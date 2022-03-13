1dbaa35d8559914248a82b6d4da53ae40926b92f
leo0842 <kunga077@gmail.com>
2022-03-13 20:45:10 +0900

    test: Turn 에 대한 테스트 케이스 추가

--- src/main/java/blackjack/domain/participant/Turn.java
+++ src/main/java/blackjack/domain/participant/Turn.java
@@ -20,4 +20,28 @@ public class Turn {
     public int getNow() {
         return now;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Turn turn1 = (Turn) o;
+
+        if (turn != turn1.turn) {
+            return false;
+        }
+        return now == turn1.now;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = turn;
+        result = 31 * result + now;
+        return result;
+    }
 }
