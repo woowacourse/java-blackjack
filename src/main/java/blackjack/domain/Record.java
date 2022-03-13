84b9e99051f1265aca898dee04f75c4dbb646e0c
유영우 <fbduddn97@naver.com>
2022-03-14 02:21:58 +0900

    refactor: record에 모든 전적기능 통합

--- src/main/java/blackjack/domain/Record.java
+++ src/main/java/blackjack/domain/Record.java
@@ -1,9 +1,11 @@
 package blackjack.domain;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import blackjack.domain.dto.DealerResultDto;
 import blackjack.domain.dto.PlayerResultDto;
@@ -25,8 +27,8 @@ public enum Record {
 	}
 
 	public static ResultDto of(Dealer dealer, List<Player> players) {
-		Map<String, String> playerRecords = PlayerRecord.of(dealer, players);
-		Map<String, Integer> dealerRecords = DealerRecord.of(dealer, players);
+		Map<String, String> playerRecords = getPlayerRecords(dealer, players);
+		Map<String, Integer> dealerRecords = getDealerRecords(dealer, players);
 
 		PlayerResultDto playerResultDto = new PlayerResultDto(playerRecords);
 		DealerResultDto dealerResultDto = new DealerResultDto(dealer.getName(), dealerRecords);
@@ -34,20 +36,44 @@ public enum Record {
 		return new ResultDto(dealerResultDto, playerResultDto);
 	}
 
-	private static int compare(Person person1, Person person2) {
-		return Integer.compare(person1.score(), person2.score());
+	private static Map<String, String> getPlayerRecords(Dealer dealer, List<Player> players) {
+		Map<String, String> records = new LinkedHashMap<>();
+		players.forEach(player
+			-> records.put(player.getName(), getRecord(player, dealer).playerRecord));
+
+		return records;
+	}
+
+	private static Map<String, Integer> getDealerRecords(Dealer dealer, List<Player> players) {
+		Map<String, Integer> records = Arrays.stream(Record.values())
+			.collect(Collectors.toMap(record -> record.playerRecord, record -> 0));
+
+		for (Player player : players) {
+			String record = getRecord(player, dealer).dealerRecord;
+			records.put(record, records.getOrDefault(record, 0) + 1);
+		}
+
+		return records;
 	}
 
 	public static Record getRecord(Player player, Dealer dealer) {
+		if (isBurst(player, dealer)) {
+			return getRecordForBurst(player, dealer);
+		}
+
+		return getOrdinaryRecord(player, dealer);
+	}
+
+	public static Record getOrdinaryRecord(Player player, Dealer dealer) {
 		return Objects.requireNonNull(Arrays.stream(Record.values())
 			.filter(record
-				-> record.getRecordNumber() == compare(player, dealer))
+				-> record.recordNumber == compare(player, dealer))
 			.findFirst()
 			.orElse(null));
 	}
 
-	public static boolean isBurst(Player player, Dealer dealer) {
-		return player.isBurst() || dealer.isBurst();
+	private static int compare(Person person1, Person person2) {
+		return Integer.compare(person1.score(), person2.score());
 	}
 
 	public static Record getRecordForBurst(Player player, Dealer dealer) {
@@ -62,15 +88,7 @@ public enum Record {
 		return DEFEAT;
 	}
 
-	public String getPlayerRecord() {
-		return playerRecord;
-	}
-
-	public int getRecordNumber() {
-		return recordNumber;
-	}
-
-	public String getDealerRecord() {
-		return dealerRecord;
+	private static boolean isBurst(Player player, Dealer dealer) {
+		return player.isBurst() || dealer.isBurst();
 	}
 }
