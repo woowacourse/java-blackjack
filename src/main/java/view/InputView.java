85d19fd1537cefdfc74ec08f8e093455527d8005
Hongdonggeon <ghd700@naver.com>
2022-03-14 02:48:05 +0900

    refactor : view 개행문자 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String PLAYER_NAME_INPUT_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-    private static final String GET_MORE_CARD_OR_NOT_MESSAGE =
-        "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)" + System.lineSeparator();
+    private static final String GET_MORE_CARD_OR_NOT_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
     private static final String GET_CARD_OR_NOT_REGEX = "^[y,n]$";
     private static final String YES = "y";
 
@@ -48,6 +47,7 @@ public class InputView {
 
     public boolean inputMoreCardOrNot(String name) {
         System.out.printf(GET_MORE_CARD_OR_NOT_MESSAGE, name);
+        System.out.print(System.lineSeparator());
         String userInput = input();
         try {
             validateRegex(userInput);
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -13,8 +13,7 @@ import java.util.stream.Collectors;
 public class OutputView {
 
     private static final String COMMA = ", ";
-    private static final String PARTICIPANTS_INITIAL_HAND_OUT_MESSAGE =
-        "%s와 %s에게 2장의 카드를 나누었습니다." + System.lineSeparator();
+    private static final String PARTICIPANTS_INITIAL_HAND_OUT_MESSAGE = "%s와 %s에게 2장의 카드를 나누었습니다.";
     private static final String DELIMITER = ": ";
     private static final String DEALER_HIT_CARD_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
     private static final String CARDS_SCORE_MESSAGE = " - 결과: ";
@@ -34,17 +33,19 @@ public class OutputView {
     }
 
     public void showInitialTurnStatus(Players players, Dealer dealer) {
-        System.out.printf(PARTICIPANTS_INITIAL_HAND_OUT_MESSAGE,
-            dealer.getName(), String.join(COMMA, players.toNames()));
+        System.out.print(System.lineSeparator());
+        System.out.printf(PARTICIPANTS_INITIAL_HAND_OUT_MESSAGE, dealer.getName(), String.join(COMMA, players.toNames()));
+        System.out.print(System.lineSeparator());
 
         showInitialDealerCardStatus(dealer);
 
         for (Player player : players.getPlayers()) {
             showPlayerCardStatus(player);
         }
+        System.out.print(System.lineSeparator());
     }
 
-    public void showInitialDealerCardStatus(Dealer dealer) {
+    private void showInitialDealerCardStatus(Dealer dealer) {
         String dealerCardStatus = makeCardStatus(dealer.getName(), dealer.getCards().subList(0, 1));
         System.out.println(dealerCardStatus);
 
@@ -56,11 +57,12 @@ public class OutputView {
     }
 
     public void showFinalTurnStatus(Players players, Dealer dealer) {
+        System.out.print(System.lineSeparator());
         System.out.println(makeCardStatus(dealer.getName(), dealer.getCards()) + CARDS_SCORE_MESSAGE
-            + dealer.calculateScore());
+                + dealer.calculateScore());
         for (Player player : players.getPlayers()) {
             System.out.println(makeCardStatus(player.getName(),
-                player.getCards()) + CARDS_SCORE_MESSAGE + player.calculateScore());
+                    player.getCards()) + CARDS_SCORE_MESSAGE + player.calculateScore());
         }
     }
 
@@ -70,18 +72,20 @@ public class OutputView {
 
     private String getDeck(List<Card> cards) {
         List<String> cardRepresentation = cards.stream()
-            .map(card -> card.getDenomination() + card.getSymbol())
-            .collect(Collectors.toList());
+                .map(card -> card.getDenomination() + card.getSymbol())
+                .collect(Collectors.toList());
 
         return String.join(COMMA, cardRepresentation);
     }
 
     public void showDealerHitCardMessage() {
+        System.out.print(System.lineSeparator());
         System.out.println(DEALER_HIT_CARD_MESSAGE);
     }
 
-    public void showResult(ResultDto resultDto, List<String> playerNames,
-                           List<Result> playerResult) {
+    public void showResult(ResultDto resultDto, List<String> playerNames, List<Result> playerResult) {
+        System.out.print(System.lineSeparator());
+
         System.out.println(FINAL_RESULT_MESSAGE);
         showDealerResult(resultDto);
         showPlayersResult(playerNames, playerResult);
@@ -89,7 +93,7 @@ public class OutputView {
 
     private void showDealerResult(ResultDto resultDto) {
         System.out.printf(DEALER_MESSAGE, resultDto.getWinCount(), resultDto.getDrawCount(),
-            resultDto.getLoseCount());
+                resultDto.getLoseCount());
         System.out.print(System.lineSeparator());
     }
 
