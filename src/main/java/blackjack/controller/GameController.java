b88a204503b0c1ecbd2d717210da97f03680b0d2
nailseong <nnis9857@gmail.com>
2022-03-13 14:11:49 +0900

    refactor: Controller가 갖던 역할을 Service에 할당

--- src/main/java/blackjack/controller/GameController.java
+++ src/main/java/blackjack/controller/GameController.java
@@ -1,64 +1,34 @@
 package blackjack.controller;
 
-import blackjack.domain.RecordFactory;
-import blackjack.domain.card.CardFactory;
 import blackjack.domain.card.Status;
-import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Player;
-import blackjack.domain.participant.Players;
+import blackjack.service.Casino;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
-import blackjack.vo.ParticipantVo;
+import java.util.List;
 
 public class GameController {
 
     public void play() {
-        final CardFactory cardFactory = CardFactory.create();
-        final Dealer dealer = new Dealer();
-        final Players players = enterPlayers();
+        final Casino casino = new Casino(getPlayerNames());
+        casino.prepareParticipants();
 
-        init(dealer, players, cardFactory);
+        while (casino.isPlayerTurn()) {
+            final Player player = casino.findDrawablePlayer();
+            final Status status = getHitOrStay(player);
+            casino.progressPlayerTurn(player, status);
+        }
+        casino.progressDealerTurn();
 
-        progressPlayerTurns(players, cardFactory);
-
-        progressDealerTurn(dealer, cardFactory);
-
-        endGame(dealer, players);
+        casino.endGame();
     }
 
-    private Players enterPlayers() {
+    private List<String> getPlayerNames() {
         try {
-            return new Players(InputView.requestPlayerNames());
+            return InputView.requestPlayerNames();
         } catch (IllegalArgumentException e) {
             OutputView.printError(e.getMessage());
-            return enterPlayers();
-        }
-    }
-
-    private void init(final Dealer dealer, final Players players, final CardFactory cardFactory) {
-        dealer.prepareGame(cardFactory);
-        players.prepareGame(cardFactory);
-
-        OutputView.printInitResult(players.getNames());
-        OutputView.printDealerFirstCard(dealer.openFirstCard());
-        players.getValue().stream()
-                .map(ParticipantVo::new)
-                .forEach(OutputView::printPlayerCards);
-    }
-
-    private void progressPlayerTurns(final Players players, final CardFactory cardFactory) {
-        while (players.findHitPlayer().isPresent()) {
-            final Player player = players.findHitPlayer().get();
-            final Status status = getHitOrStay(player);
-
-            if (status == Status.HIT) {
-                player.hit(cardFactory);
-            }
-            if (status == Status.STAY) {
-                player.stay();
-            }
-
-            OutputView.printPlayerCards(new ParticipantVo(player));
+            return getPlayerNames();
         }
     }
 
@@ -69,28 +39,5 @@ public class GameController {
             OutputView.printError(e.getMessage());
             return getHitOrStay(player);
         }
-    }
-
-    private void progressDealerTurn(final Dealer dealer, final CardFactory cardFactory) {
-        if (!dealer.canDrawCard()) {
-            OutputView.printDealerNotDrawMessage();
-            return;
-        }
-
-        do {
-            dealer.hit(cardFactory);
-            OutputView.printDealerDrawMessage();
-        } while (dealer.canDrawCard());
-    }
-
-    private void endGame(final Dealer dealer, final Players players) {
-        OutputView.breakLine();
-        OutputView.printParticipantCards(new ParticipantVo(dealer));
-        players.getValue().stream()
-                .map(ParticipantVo::new)
-                .forEach(OutputView::printParticipantCards);
-
-        final RecordFactory factory = new RecordFactory(dealer.getScore(), players.getValue());
-        OutputView.printRecord(factory.getDealerRecord(), factory.getAllPlayerRecord());
     }
 }
---
+++ src/main/java/blackjack/service/Casino.java
@@ -0,0 +1,77 @@
+package blackjack.service;
+
+import blackjack.domain.RecordFactory;
+import blackjack.domain.card.CardFactory;
+import blackjack.domain.card.Status;
+import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Player;
+import blackjack.domain.participant.Players;
+import blackjack.view.OutputView;
+import blackjack.vo.ParticipantVo;
+import java.util.List;
+
+public class Casino {
+
+    private final CardFactory cardFactory;
+    private final Dealer dealer;
+    private final Players players;
+
+    public Casino(final List<String> playerNames) {
+        this.cardFactory = CardFactory.create();
+        this.dealer = new Dealer();
+        this.players = new Players(playerNames);
+    }
+
+    public void prepareParticipants() {
+        dealer.prepareGame(cardFactory);
+        players.prepareGame(cardFactory);
+
+        OutputView.printInitResult(players.getNames());
+        OutputView.printDealerFirstCard(dealer.openFirstCard());
+        players.getValue().stream()
+                .map(ParticipantVo::new)
+                .forEach(OutputView::printPlayerCards);
+    }
+
+    public boolean isPlayerTurn() {
+        return players.findHitPlayer().isPresent();
+    }
+
+    public Player findDrawablePlayer() {
+        return players.findHitPlayer().get();
+    }
+
+    public void progressPlayerTurn(final Player player, final Status status) {
+        if (status == Status.HIT) {
+            player.hit(cardFactory);
+        }
+        if (status == Status.STAY) {
+            player.stay();
+        }
+
+        OutputView.printPlayerCards(new ParticipantVo(player));
+    }
+
+    public void progressDealerTurn() {
+        if (!dealer.canDrawCard()) {
+            OutputView.printDealerNotDrawMessage();
+            return;
+        }
+
+        do {
+            dealer.hit(cardFactory);
+            OutputView.printDealerDrawMessage();
+        } while (dealer.canDrawCard());
+    }
+
+    public void endGame() {
+        OutputView.breakLine();
+        OutputView.printParticipantCards(new ParticipantVo(dealer));
+        players.getValue().stream()
+                .map(ParticipantVo::new)
+                .forEach(OutputView::printParticipantCards);
+
+        final RecordFactory factory = new RecordFactory(dealer.getScore(), players.getValue());
+        OutputView.printRecord(factory.getDealerRecord(), factory.getAllPlayerRecord());
+    }
+}
