2558a5c71a4fef78f01e148cfa5519041e1e8119
Donggyu <a29661498@gmail.com>
2022-03-14 07:18:27 +0900

    refactor(Cards): static 메소드 호출 명시
    Denomination의 values() 호출임을 명시해주었습니다.

--- src/main/java/blackjack/domain/strategy/ShuffledDeckGenerateStrategy.java
+++ src/main/java/blackjack/domain/strategy/ShuffledDeckGenerateStrategy.java
@@ -1,10 +1,10 @@
 package blackjack.domain.strategy;
 
-import static blackjack.domain.card.Denomination.values;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
 
 import blackjack.domain.card.Card;
+import blackjack.domain.card.Denomination;
 import blackjack.domain.card.Suit;
 import java.util.ArrayDeque;
 import java.util.Collections;
@@ -16,7 +16,7 @@ public class ShuffledDeckGenerateStrategy implements DeckGenerateStrategy {
     @Override
     public Deque<Card> generate() {
         List<Card> cards = stream(Suit.values())
-                .flatMap(suit -> stream(values())
+                .flatMap(suit -> stream(Denomination.values())
                         .map(denomination -> new Card(suit, denomination)))
                 .collect(toList());
 
