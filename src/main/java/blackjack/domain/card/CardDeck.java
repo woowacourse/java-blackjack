b7f5147ff1f4e4a3434dc2fcfa97f7a9d3bb7644
seong-wooo <seongwoo0513@uos.ac.kr>
2022-03-13 21:10:14 +0900

    refactor: cardDeck의 자료구조 Deque로 변경

--- src/main/java/blackjack/domain/card/CardDeck.java
+++ src/main/java/blackjack/domain/card/CardDeck.java
@@ -1,40 +1,36 @@
 package blackjack.domain.card;
 
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CardDeck {
 
-    private static final List<Card> cards;
+    private final Deque<Card> deck;
 
-    private List<Card> deck;
+    public CardDeck() {
+        List<Card> deck = getAllCards();
+        Collections.shuffle(deck);
+        this.deck = new ArrayDeque<>(deck);
+    }
 
-    static {
-        cards = Stream.of(Denomination.values())
+    private List<Card> getAllCards() {
+        return Stream.of(Denomination.values())
                 .flatMap(denomination ->
                         Stream.of(Suit.values())
                                 .map(suit -> new Card(denomination, suit)))
                 .collect(Collectors.toList());
     }
 
-    public CardDeck() {
-        deck = new ArrayList<>(cards);
-        Collections.shuffle(deck);
-    }
-
     public Card getCard() {
-        checkDeckEmpty();
-        Card card = deck.get(0);
-        deck.remove(0);
-        return card;
-    }
-
-    private void checkDeckEmpty() {
-        if (deck.isEmpty()) {
-            deck = new ArrayList<>(cards);
+        try {
+            return deck.pop();
+        } catch (NoSuchElementException exception) {
+            throw new NoSuchElementException("[ERROR] 카드가 존재하지 않습니다.");
         }
     }
 }
--- src/test/java/blackjack/domain/CardDeckTest.java
+++ src/test/java/blackjack/domain/CardDeckTest.java
@@ -1,7 +1,12 @@
 package blackjack.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import blackjack.domain.card.Card;
 import blackjack.domain.card.CardDeck;
+import blackjack.domain.player.Player;
+import java.util.NoSuchElementException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +17,19 @@ public class CardDeckTest {
     @DisplayName("카드 덱에서 카드를 뽑는다.")
     void getCard() {
         CardDeck cardDeck = new CardDeck();
-        Assertions.assertThat(cardDeck.getCard()).isInstanceOf(Card.class);
+        assertThat(cardDeck.getCard()).isInstanceOf(Card.class);
+    }
+
+    @Test
+    @DisplayName("카드 덱에서 카드를 모두 뽑고 추가로 뽑는 경우 테스트")
+    void getCardsInExcess() {
+        CardDeck cardDeck = new CardDeck();
+
+        assertThatThrownBy(() -> {
+            for (int i = 0; i < 53; i++) {
+                cardDeck.getCard();
+            }
+        }).isInstanceOf(NoSuchElementException.class)
+                .hasMessage("[ERROR] 카드가 존재하지 않습니다.");
     }
 }
