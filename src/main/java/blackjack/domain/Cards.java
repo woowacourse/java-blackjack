18dc2145803809f1cc68611d285b973f8ddeed71
jaejae-yoo <wotj102@gmail.com>
2022-03-14 02:10:02 +0900

    refactor: 전체 카드 객체 캐싱

--- src/main/java/blackjack/domain/Cards.java
+++ src/main/java/blackjack/domain/Cards.java
@@ -3,39 +3,26 @@ package blackjack.domain;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Cards {
 
-    private final List<Card> cards;
+    private static final List<Card> cards = new ArrayList<>();
+
     private final CardPickMachine cardPickMachine;
 
+    static {
+        for (Suit suit : Suit.values()) {
+            Arrays.stream(Symbols.values())
+                    .forEach(symbol -> cards.add(new Card(suit, symbol)));
+        }
+     }
+
     public Cards(CardPickMachine cardPickMachine) {
-        this.cards = makeCards();
         this.cardPickMachine = cardPickMachine;
     }
 
     public Card assignCard() {
         return cards.get(cardPickMachine.assignIndex());
-    }
-
-    private List<Card> makeCards() {
-        List<Card> allCard = new ArrayList<>();
-        List<Suit> suits = Arrays.stream(Suit.values())
-                .collect(Collectors.toList());
-        List<Symbols> symbols = Arrays.stream(Symbols.values())
-                .collect(Collectors.toList());
-
-        for (Suit suit : suits) {
-            addRankForSuit(allCard, symbols, suit);
-        }
-        return allCard;
-    }
-
-    private void addRankForSuit(List<Card> allCard, List<Symbols> symbols, Suit suit) {
-        for (Symbols symbol : symbols) {
-            allCard.add(new Card(suit, symbol));
-        }
     }
 
     @Override
