0494c6c99508d0a0955add32decf5fa81c2907bd
Richard JEON <ztzy1907@gmail.com>
2022-03-14 02:10:35 +0900

    feat(cards): 버스트여부, 블랙잭여부, 점수 계산 기능 Cards 내부에 구현 및 테스트코드 추가

--- src/main/java/domain/card/Cards.java
+++ src/main/java/domain/card/Cards.java
@@ -1,18 +1,68 @@
 package domain.card;
 
-import static java.util.Collections.unmodifiableList;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cards {
+    private static final String ERROR_TO_CHECK_IF_BLACKJACK_FOR_INSUFFICIENT_CARDS = "카드가 두 장 미만인 경우, 블랙잭 여부확인이 불가합니다.";
+    private static final int MAXIMUM_SCORE = 21;
+    private static final int MINIMUM_CARDS_TO_CHECK_IF_BLACKJACK = 2;
+    private static final int FIRST_INDEX_INCLUSIVE = 0;
+    private static final int SECOND_INDEX_EXCLUSIVE = 2;
+    private static final int ALTERNATIVE_ACE_GAP = 10;
+
     private final List<Card> cards = new ArrayList<>();
 
     public void addCard(Card card) {
         cards.add(card);
     }
 
+    public boolean isBust() {
+        return calculateScore(cards) > MAXIMUM_SCORE;
+    }
+
+    public boolean isBlackJack() {
+        if (cards.size() < MINIMUM_CARDS_TO_CHECK_IF_BLACKJACK) {
+            throw new IllegalStateException(ERROR_TO_CHECK_IF_BLACKJACK_FOR_INSUFFICIENT_CARDS);
+        }
+
+        return calculateScore(cards.subList(FIRST_INDEX_INCLUSIVE, SECOND_INDEX_EXCLUSIVE)) == MAXIMUM_SCORE;
+    }
+
+    public int getScore() {
+        return calculateScore(cards);
+    }
+
+    private static int calculateScore(List<Card> cards) {
+        if (containsAce(cards)) {
+            return calculateScoreWithAce(cards);
+        }
+
+        return calculateScoreWithoutAce(cards);
+    }
+
+    private static boolean containsAce(List<Card> cards) {
+        return cards.stream()
+                .anyMatch(Card::isAce);
+    }
+
+    private static int calculateScoreWithAce(List<Card> cards) {
+        int minimumScore = calculateScoreWithoutAce(cards);
+
+        if (minimumScore + ALTERNATIVE_ACE_GAP <= MAXIMUM_SCORE) {
+            return minimumScore + ALTERNATIVE_ACE_GAP;
+        }
+
+        return minimumScore;
+    }
+
+    private static int calculateScoreWithoutAce(List<Card> cards) {
+        return cards.stream()
+                .mapToInt(Card::getScore)
+                .sum();
+    }
+
     public List<Card> getCards() {
-        return unmodifiableList(cards);
+        return new ArrayList<>(cards);
     }
 }
--- src/test/java/domain/card/CardsTest.java
+++ src/test/java/domain/card/CardsTest.java
@@ -1,11 +1,13 @@
 package domain.card;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CardsTest {
     @Test
@@ -23,18 +25,75 @@ public class CardsTest {
         assertThat(rawCards).contains(Card.of(Suit.CLUBS, Denomination.FIVE));
     }
 
-    @Test
-    @DisplayName("카드 묶음 컬렉션 수정 시도시 UOE 발생")
-    void cardsShouldBeUnmodifiable() {
+    @ParameterizedTest(name = "{0}{1} + {2}{3} = {4}")
+    @CsvSource(value = {
+            "SPADES,ACE,CLUBS,ACE,12",
+            "SPADES,ACE,CLUBS,TEN,21",
+            "SPADES,ACE,CLUBS,NINE,20"})
+    @DisplayName("점수 계산 테스트")
+    void calculateScore(Suit suit, Denomination denomination,
+                        Suit suit2, Denomination denomination2,
+                        int expected) {
         // given
         Cards cards = new Cards();
+        cards.addCard(Card.of(suit, denomination));
+        cards.addCard(Card.of(suit2, denomination2));
 
         // when
-        List<Card> unmodifiableCards = cards.getCards();
+        int actual = cards.getScore();
 
         // then
-        assertThatThrownBy(
-                () -> unmodifiableCards.add(Card.of(Suit.DIAMONDS, Denomination.JACK)))
-                .isInstanceOf(UnsupportedOperationException.class);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "{0}{1} + {2}{3} + {4}{5} = {6}")
+    @CsvSource(value = {
+            "SPADES,ACE,CLUBS,ACE,SPADES,TEN,false",
+            "SPADES,TWO,CLUBS,TEN,SPADES,TEN,true",
+            "SPADES,ACE,CLUBS,ACE,SPADES,ACE,false"})
+    @DisplayName("버스트 여부 테스트")
+    void isBust(Suit suit, Denomination denomination,
+                Suit suit2, Denomination denomination2,
+                Suit suit3, Denomination denomination3,
+                boolean expected) {
+        // given
+        Cards cards = new Cards();
+        cards.addCard(Card.of(suit, denomination));
+        cards.addCard(Card.of(suit2, denomination2));
+        cards.addCard(Card.of(suit3, denomination3));
+
+        // when
+        boolean actual = cards.isBust();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("블랙잭 여부 테스트")
+    void isBlackjack() {
+        // given
+        Card aClubs = Card.of(Suit.CLUBS, Denomination.ACE);
+        Card tenHearts = Card.of(Suit.HEARTS, Denomination.TEN);
+        Card tenDiamonds = Card.of(Suit.DIAMONDS, Denomination.TEN);
+
+        Cards blackjackCards = new Cards();
+        blackjackCards.addCard(aClubs);
+        blackjackCards.addCard(tenHearts);
+
+        Cards notBlackjackCards = new Cards();
+        notBlackjackCards.addCard(tenHearts);
+        notBlackjackCards.addCard(tenDiamonds);
+        notBlackjackCards.addCard(aClubs);
+
+        // when
+        boolean shouldBeBlackjack = blackjackCards.isBlackJack();
+        boolean shouldNotBeBlackjack = notBlackjackCards.isBlackJack();
+
+        // then
+        assertAll(
+                () -> assertThat(shouldBeBlackjack).isTrue(),
+                () -> assertThat(shouldNotBeBlackjack).isFalse()
+        );
     }
 }
