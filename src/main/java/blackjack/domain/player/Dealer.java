5e6f6b0b7955708e59f44d13def3eb912316f13b
yxxnghwan <younghwan960@gmail.com>
2022-03-14 02:11:07 +0900

    refactor(blackjack): 불필요한 인스턴스 변수, 메서드 제거

--- src/main/java/blackjack/domain/player/Dealer.java
+++ src/main/java/blackjack/domain/player/Dealer.java
@@ -1,18 +1,13 @@
 package blackjack.domain.player;
 
 import blackjack.domain.HitFlag;
-import blackjack.domain.WinDrawLose;
 import blackjack.domain.card.Cards;
 import java.util.ArrayList;
-import java.util.EnumMap;
 import java.util.List;
-import java.util.Map;
 
 public class Dealer extends AbstractPlayer {
     private static final String DEALER_NAME = "딜러";
     private static final int HIT_FLAG_SCORE = 16;
-
-    private final Map<WinDrawLose, Integer> winDrawLose = new EnumMap<>(WinDrawLose.class);
 
     public Dealer() {
         super(DEALER_NAME, new Cards(new ArrayList<>()));
