b4a8c1ed5d9be502c4f933c03eb01a18aa3dfb77
EastHShin <xldk78@gmail.com>
2022-03-13 17:43:35 +0900

    refactor(blackjack/domain/card/Deck): 덱의 자료구조를 List에서 Deque로 변경

--- src/main/java/blackjack/domain/card/Deck.java
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -2,45 +2,45 @@ package blackjack.domain.card;
 
 import static blackjack.domain.exceptionMessages.CardExceptionMessage.*;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Deck {
-	private final List<Card> cards;
+	private final Deque<Card> deck;
 
 	public Deck() {
-		cards = new ArrayList<>();
-		setUpCards();
+		List<Card> cards = setUpCards();
 		Collections.shuffle(cards);
+		deck = new ArrayDeque<>(cards);
 	}
 
-	private void setUpCards() {
+	private List<Card> setUpCards() {
+		List<Card> cards = new ArrayList<>();
 		Stream.of(Denomination.values())
-			.forEach(denomination -> setUpCardSuitByDenomination(denomination)
-			);
+			.forEach(denomination -> cards.addAll(setUpCardSuitByDenomination(denomination)));
+		return cards;
 	}
 
-	private void setUpCardSuitByDenomination(final Denomination denomination) {
-		Stream.of(Suit.values())
-			.forEach(type -> addCardToDeck(denomination, type));
-	}
-
-	private void addCardToDeck(Denomination denomination, Suit type) {
-		Card card = new Card(denomination, type);
-		cards.add(card);
+	private List<Card> setUpCardSuitByDenomination(final Denomination denomination) {
+		return Stream.of(Suit.values())
+			.map(suit -> new Card(denomination, suit))
+			.collect(Collectors.toList());
 	}
 
 	public Card distributeCard() {
-		final int cardSize = cards.size();
+		final int cardSize = deck.size();
 		if (cardSize <= 0) {
 			throw new IllegalStateException(EMPTY_DECK_EXCEPTION.getMessage());
 		}
-		return cards.remove(cards.size() - 1);
+		return deck.removeLast();
 	}
 
-	public List<Card> getCards() {
-		return this.cards;
+	public Deque<Card> getCards() {
+		return this.deck;
 	}
 }
