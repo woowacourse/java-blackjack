559736457bd48b45c6b5285afa34f566ae634f4c
meatsby <meatsby@gmail.com>
2022-03-13 21:08:07 +0900

    test: DisplayName 재구성 및 Dealer 테스트코드 추가

--- src/test/java/blackjack/domain/participant/DealerTest.java
+++ src/test/java/blackjack/domain/participant/DealerTest.java
@@ -14,54 +14,67 @@ class DealerTest {
 
     @DisplayName("딜러 생성자 테스트")
     @Test
-    void create() {
+    void constructor_CreateDealer_HasInstance() {
         Dealer dealer = new Dealer(new Name("딜러"));
         dealer.receive(new Cards(List.of(Card.from(Number.ACE, Kind.SPADE))));
 
         assertThat(dealer).isNotNull();
     }
 
-    @DisplayName("딜러 최적 점수 계산")
-    @Test
-    void calculateBestScore() {
-        Dealer dealer = new Dealer(new Name("딜러"));
-        dealer.receive(new Cards(
-                List.of(Card.from(Number.ACE, Kind.SPADE),
-                        Card.from(Number.EIGHT, Kind.HEART))));
-
-        assertThat(dealer.calculateBestScore()).isEqualTo(19);
-    }
-
-    @DisplayName("딜러 카드 버스트 시 에이스 1로 계산 테스트")
-    @Test
-    void calculateBestScore_TwoAces_Is2() {
-        Dealer dealer = new Dealer(new Name("딜러"));
-        dealer.receive(new Cards(
-                List.of(Card.from(Number.ACE, Kind.SPADE),
-                        Card.from(Number.ACE, Kind.HEART))));
-
-        assertThat(dealer.calculateBestScore()).isEqualTo(2);
-    }
-
-    @DisplayName("딜러 카드 추가 수령 가능 여부 테스트")
+    @DisplayName("16점 이하 보유 시 카드 추가 수령 가능")
     @Test
     void isReceivable_BestScoreAs16_IsTrue() {
         Dealer dealer = new Dealer(new Name("딜러"));
-        dealer.receive(new Cards(
-                List.of(Card.from(Number.ACE, Kind.SPADE),
-                        Card.from(Number.FIVE, Kind.SPADE))));
+        dealer.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
+                Card.from(Number.FIVE, Kind.SPADE))));
 
         assertThat(dealer.isReceivable()).isTrue();
     }
 
-    @DisplayName("딜러 카드 추가 수령 실패 여부 테스트")
+    @DisplayName("17점 이상 보유 시 카드 추가 수령 불가능")
     @Test
     void isReceivable_BestScoreAs17_IsFalse() {
         Dealer dealer = new Dealer(new Name("딜러"));
-        dealer.receive(new Cards(
-                List.of(Card.from(Number.ACE, Kind.SPADE),
-                        Card.from(Number.SIX, Kind.SPADE))));
+        dealer.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
+                Card.from(Number.SIX, Kind.SPADE))));
 
         assertThat(dealer.isReceivable()).isFalse();
+    }
+
+    @DisplayName("Ace 4장 보유 시 4점 반환")
+    @Test
+    void calculateBestScore_FourAces_Returns4() {
+        Dealer dealer = new Dealer(new Name("딜러"));
+        dealer.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
+                Card.from(Number.ACE, Kind.DIAMOND),
+                Card.from(Number.ACE, Kind.CLOVER),
+                Card.from(Number.ACE, Kind.HEART))));
+
+        assertThat(dealer.calculateBestScore()).isEqualTo(4);
+    }
+
+    @DisplayName("21점 이하일 경우 Ace 를 11점으로 판단하여 계산")
+    @Test
+    void calculateBestScore_ConsideringAceAsElevenWhenUnder21_Returns19() {
+        Dealer dealer = new Dealer(new Name("딜러"));
+        dealer.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
+                Card.from(Number.EIGHT, Kind.HEART))));
+
+        assertThat(dealer.calculateBestScore()).isEqualTo(19);
+    }
+
+    @DisplayName("21점 초과 시 Ace 를 1점으로 판단하여 계산")
+    @Test
+    void calculateBestScore_ConsideringAceAsOneWhenExceeds21_Returns2() {
+        Dealer dealer = new Dealer(new Name("딜러"));
+        dealer.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
+                Card.from(Number.ACE, Kind.HEART))));
+
+        assertThat(dealer.calculateBestScore()).isEqualTo(2);
     }
 }
--- src/test/java/blackjack/domain/participant/PlayerTest.java
+++ src/test/java/blackjack/domain/participant/PlayerTest.java
@@ -14,28 +14,66 @@ class PlayerTest {
 
     @DisplayName("플레이어 생성자 테스트")
     @Test
-    void create() {
-        Player player = Player.of("Pobi");
+    void constructor_CreatePlayer_HasInstance() {
+        Player player = Player.of("쿼리치");
         player.receive(new Cards(List.of(Card.from(Number.ACE, Kind.SPADE))));
 
         assertThat(player).isNotNull();
     }
 
-    @DisplayName("Ace를 1로 판단했을 시 베스트 점수 계산 테스트")
+    @DisplayName("21점 보유 시 카드 추가 수령 가능")
     @Test
-    void calculateBestScore_ConsideringAceIsOne_IsBest() {
-        Player player = Player.of("Pobi");
-        player.receive(new Cards(List.of(Card.from(Number.ACE, Kind.SPADE),
+    void isReceivable_BestScore21_IsTrue() {
+        Player player = Player.of("쿼리치");
+        player.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
+                Card.from(Number.KING, Kind.SPADE))));
+
+        assertThat(player.isReceivable()).isTrue();
+    }
+
+    @DisplayName("22점 보유 시 카드 추가 수령 불가능")
+    @Test
+    void isReceivable_BestScore22_IsFalse() {
+        Player player = Player.of("쿼리치");
+        player.receive(new Cards(List.of(
+                Card.from(Number.TEN, Kind.SPADE),
+                Card.from(Number.TWO, Kind.SPADE),
+                Card.from(Number.TEN, Kind.HEART))));
+
+        assertThat(player.isReceivable()).isFalse();
+    }
+
+    @DisplayName("Ace 4장 보유 시 14점 반환")
+    @Test
+    void calculateBestScore_FourAces_Returns14() {
+        Player player = Player.of("쿼리치");
+        player.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
+                Card.from(Number.ACE, Kind.DIAMOND),
+                Card.from(Number.ACE, Kind.CLOVER),
+                Card.from(Number.ACE, Kind.HEART))));
+
+        assertThat(player.calculateBestScore()).isEqualTo(14);
+    }
+
+    @DisplayName("Ace 를 11점으로 판단하여 베스트 점수 계산")
+    @Test
+    void calculateBestScore_ConsideringAceAsEleven_Returns21() {
+        Player player = Player.of("쿼리치");
+        player.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
                 Card.from(Number.KING, Kind.SPADE))));
 
         assertThat(player.calculateBestScore()).isEqualTo(21);
     }
 
-    @DisplayName("Ace를 11로 판단했을 시 베스트 점수 계산 테스트")
+    @DisplayName("Ace 를 1점으로 판단하여 베스트 점수 계산")
     @Test
-    void calculateBestScore_ConsideringAceIsEleven_IsBest() {
-        Player player = Player.of("Pobi");
-        player.receive(new Cards(List.of(Card.from(Number.ACE, Kind.SPADE),
+    void calculateBestScore_ConsideringAceAsOne_Returns21() {
+        Player player = Player.of("쿼리치");
+        player.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
                 Card.from(Number.FIVE, Kind.SPADE),
                 Card.from(Number.SEVEN, Kind.SPADE),
                 Card.from(Number.EIGHT, Kind.SPADE))));
@@ -46,8 +84,9 @@ class PlayerTest {
     @DisplayName("플레이어의 점수가 더 높은 경우 승리 테스트")
     @Test
     void isWinner_Player20_isWin() {
-        Player player = Player.of("Pobi");
-        player.receive(new Cards(List.of(Card.from(Number.ACE, Kind.SPADE),
+        Player player = Player.of("쿼리치");
+        player.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
                 Card.from(Number.NINE, Kind.SPADE))));
 
         assertThat(player.isWinner(19)).isTrue();
@@ -56,8 +95,9 @@ class PlayerTest {
     @DisplayName("딜러의 점수가 더 높은 경우 패배 테스트")
     @Test
     void isWinner_Player20_isLose() {
-        Player player = Player.of("Pobi");
-        player.receive(new Cards(List.of(Card.from(Number.ACE, Kind.SPADE),
+        Player player = Player.of("쿼리치");
+        player.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
                 Card.from(Number.NINE, Kind.SPADE))));
 
         assertThat(player.isWinner(21)).isFalse();
@@ -66,8 +106,9 @@ class PlayerTest {
     @DisplayName("플레이어가 버스트된 경우 패배 테스트")
     @Test
     void isWinner_PlayerBusted_isLose() {
-        Player player = Player.of("Pobi");
-        player.receive(new Cards(List.of(Card.from(Number.NINE, Kind.SPADE),
+        Player player = Player.of("쿼리치");
+        player.receive(new Cards(List.of(
+                Card.from(Number.NINE, Kind.SPADE),
                 Card.from(Number.NINE, Kind.CLOVER),
                 Card.from(Number.NINE, Kind.HEART))));
 
@@ -77,8 +118,9 @@ class PlayerTest {
     @DisplayName("딜러가 버스트된 경우 승리 테스트")
     @Test
     void isWinner_DealerBusted_isWin() {
-        Player player = Player.of("Pobi");
-        player.receive(new Cards(List.of(Card.from(Number.ACE, Kind.SPADE),
+        Player player = Player.of("쿼리치");
+        player.receive(new Cards(List.of(
+                Card.from(Number.ACE, Kind.SPADE),
                 Card.from(Number.NINE, Kind.SPADE))));
 
         assertThat(player.isWinner(22)).isTrue();
@@ -87,44 +129,12 @@ class PlayerTest {
     @DisplayName("둘 다 버스트된 경우 패배 테스트")
     @Test
     void isWinner_BothBusted_isLose() {
-        Player player = Player.of("Pobi");
-        player.receive(new Cards(List.of(Card.from(Number.NINE, Kind.SPADE),
+        Player player = Player.of("쿼리치");
+        player.receive(new Cards(List.of(
+                Card.from(Number.NINE, Kind.SPADE),
                 Card.from(Number.NINE, Kind.CLOVER),
                 Card.from(Number.NINE, Kind.HEART))));
 
         assertThat(player.isWinner(22)).isFalse();
-    }
-
-    @DisplayName("Ace만 있을 시 베스트 점수 계산 테스트")
-    @Test
-    void calculateBestScore_FourAces_IsBest() {
-        Player player = Player.of("Pobi");
-        player.receive(new Cards(List.of(Card.from(Number.ACE, Kind.SPADE),
-                Card.from(Number.ACE, Kind.DIAMOND),
-                Card.from(Number.ACE, Kind.CLOVER),
-                Card.from(Number.ACE, Kind.HEART))));
-
-        assertThat(player.calculateBestScore()).isEqualTo(14);
-    }
-
-    @DisplayName("플레이어 카드 추가 수령 가능 여부 테스트")
-    @Test
-    void isReceivable_BestScore21_IsTrue() {
-        Player player = Player.of("Pobi");
-        player.receive(new Cards(List.of(Card.from(Number.ACE, Kind.SPADE),
-                Card.from(Number.KING, Kind.SPADE))));
-
-        assertThat(player.isReceivable()).isTrue();
-    }
-
-    @DisplayName("플레이어 카드 추가 수령 실패 여부 테스트")
-    @Test
-    void isReceivable_BestScore22_IsFalse() {
-        Player player = Player.of("Pobi");
-        player.receive(new Cards(List.of(Card.from(Number.TEN, Kind.SPADE),
-                Card.from(Number.TWO, Kind.SPADE),
-                Card.from(Number.TEN, Kind.HEART))));
-
-        assertThat(player.isReceivable()).isFalse();
     }
 }
