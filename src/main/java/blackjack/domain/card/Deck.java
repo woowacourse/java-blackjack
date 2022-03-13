84b782c6ba69569e3b2dba194795795d9d20ef91
yxxnghwan <younghwan960@gmail.com>
2022-03-13 13:07:29 +0900

    refactor(blackjack): 단순 반복문에 스트림 사용 X, 메서드 이름 변경, 카드 뭉치 구현체를 LinkedList로 명시

--- src/main/java/blackjack/domain/card/Deck.java
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -1,6 +1,5 @@
 package blackjack.domain.card;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,26 +7,30 @@ import java.util.List;
 public class Deck {
     private static final String NO_CARD_ERROR_MESSAGE = "더 이상 뽑을 수 있는 카드가 없습니다.";
 
-    private final List<Card> cards = new LinkedList<>();
+    private final LinkedList<Card> cards;
 
     public Deck() {
-        init();
+        cards = createCards();
     }
 
-    public void init() {
-        cards.clear();
-        Arrays.stream(Suit.values()).forEach(this::addAllDenomination);
+    private LinkedList<Card> createCards() {
+        LinkedList<Card> cards = new LinkedList<>();
+        for (Suit suit : Suit.values()) {
+            createSuitDenominations(cards, suit);
+        }
         Collections.shuffle(cards);
+        return cards;
     }
 
-    private void addAllDenomination(Suit suit) {
-        Arrays.stream(Denomination.values())
-                .forEach(denomination -> cards.add(new Card(denomination, suit)));
+    private void createSuitDenominations(List<Card> cards, Suit suit) {
+        for (Denomination denomination : Denomination.values()) {
+            cards.add(new Card(denomination, suit));
+        }
     }
 
     public Card draw() {
         try {
-            return cards.remove(cards.size() - 1);
+            return cards.remove();
         } catch (IndexOutOfBoundsException e) {
             throw new IllegalStateException(NO_CARD_ERROR_MESSAGE);
         }
