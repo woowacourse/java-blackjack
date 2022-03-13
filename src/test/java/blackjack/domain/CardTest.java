1160e7e8db76f2fd414c6a920e3489170395dc04
yh20studio(tiki) <yh20studio@gmail.com>
2022-03-13 18:47:35 +0900

    test(Hand): 에이스 가지고 있는지 판단하는 테스트 추가

--- src/test/java/blackjack/domain/CardTest.java
+++ src/test/java/blackjack/domain/CardTest.java
@@ -6,11 +6,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import blackjack.domain.factory.CardMockFactory;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("Card 테스트")
 class CardTest {
 
 	private final Card card = CardMockFactory.of("A클로버");
+
+	@DisplayName("카드가 Ace인지 확인")
+	@ParameterizedTest(name = "{index} {displayName} card={0} expected={1}")
+	@CsvSource(value = {"A클로버, true", "K클로버, false"})
+	void check_Has_Ace(String cardInfo, boolean expectedIsAce) {
+		final Card card = CardMockFactory.of(cardInfo);
+
+		assertThat(card.isAce()).isEqualTo(expectedIsAce);
+	}
 
 	@Test
 	@DisplayName("카드의 정보가 제대로 반환되는지 확인")
--- src/test/java/blackjack/domain/HandTest.java
+++ src/test/java/blackjack/domain/HandTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import blackjack.domain.factory.CardMockFactory;
@@ -30,6 +31,18 @@ class HandTest {
 		assertThat(actualHand.containsAll(expectedHand)).isTrue();
 	}
 
+	@DisplayName("패에 Ace 카드가 있는지 확인")
+	@ParameterizedTest(name = "{index} {displayName} card={0} expectedHasAce={1}")
+	@CsvSource(value = {"A클로버, true", "K클로버, false"})
+	void check_Has_Ace(String cardInfo, boolean expectedHasAce) {
+		final Hand hand = new Hand();
+		final Card card = CardMockFactory.of(cardInfo);
+
+		hand.addCard(card);
+
+		assertThat(hand.hasAce()).isEqualTo(expectedHasAce);
+	}
+
 	@DisplayName("현재 패에 가지고 있는 최적의 점수 계산 확인")
 	@ParameterizedTest(name = "{index} {displayName} cards={0}")
 	@MethodSource("getHandAndScore")
@@ -41,16 +54,15 @@ class HandTest {
 	private static Stream<Arguments> getHandAndScore() {
 		final Hand hand1 = CreateHand.create(CardMockFactory.of("A클로버"), CardMockFactory.of("K클로버"));
 		final Hand hand2 = CreateHand.create(CardMockFactory.of("A클로버"), CardMockFactory.of("K클로버"),
-			CardMockFactory.of("J클로버"));
+				CardMockFactory.of("J클로버"));
 		final Hand hand3 = CreateHand.create(CardMockFactory.of("10클로버"), CardMockFactory.of("K클로버"),
-			CardMockFactory.of("J클로버"));
+				CardMockFactory.of("J클로버"));
 		final Hand hand4 = CreateHand.create(CardMockFactory.of("10클로버"), CardMockFactory.of("K클로버"));
 
 		return Stream.of(
-			Arguments.of(hand1, 21),
-			Arguments.of(hand2, 21),
-			Arguments.of(hand3, 0),
-			Arguments.of(hand4, 20));
+				Arguments.of(hand1, 21),
+				Arguments.of(hand2, 21),
+				Arguments.of(hand3, 0),
+				Arguments.of(hand4, 20));
 	}
-
 }
