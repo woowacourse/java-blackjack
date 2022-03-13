f61c3930ab917e525f6b31de7e30f657a3576694
yoon <duddbs774@gmail.com>
2022-03-13 12:46:19 +0900

    refactor: 정적 팩토리 메서드 이름 수정
    - 예외 메세지 추가
    - 블랙잭인지 확인 하는 기능을 기존 cards의 메서드를 재사용

--- src/main/java/domain/CardState.java
+++ src/main/java/domain/CardState.java
@@ -8,9 +8,7 @@ public enum CardState {
     STAND(cards -> !(isBlackJack(cards)) && cards.calculateSum() <= 21, 2),
     BUST(cards -> cards.calculateSum() > 21, 1);
 
-    private static final int BLACK_JACK_SCORE = 21;
-    private static final int BLACK_JACK_CARD_COUNT = 2;
-
+    private static final String CANNOT_FIND_CARD_STATE_MESSAGE = "카드 상태를 구할 수 없습니다.";
     private final Predicate<Cards> predicate;
     private final int statePower;
 
@@ -19,16 +17,16 @@ public enum CardState {
         this.statePower = statePower;
     }
 
-    public static CardState of(final Cards cards) {
+    public static CardState from(final Cards cards) {
         return Arrays
                 .stream(CardState.values())
                 .filter(cardState -> cardState.predicate.test(cards))
                 .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(() -> new IllegalArgumentException(CANNOT_FIND_CARD_STATE_MESSAGE));
     }
 
     private static boolean isBlackJack(final Cards cards) {
-        return cards.getCardCount() == BLACK_JACK_CARD_COUNT && cards.calculateSum() == BLACK_JACK_SCORE;
+        return cards.isBlackJack();
     }
 
     public boolean isStand() {
