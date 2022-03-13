4140de830e5e380a4947d6743478ac8cb36bf630
DOES <ldk980130@gmail.com>
2022-03-13 22:27:29 +0900

    refactor: 사용자 입력 받는 메서드 이름 변경

--- src/main/java/blackjack/BlackJackApplication.java
+++ src/main/java/blackjack/BlackJackApplication.java
@@ -9,10 +9,10 @@ import blackjack.view.OutputView;
 public class BlackJackApplication {
 
     public static void main(String[] args) {
-        BlackJackGame blackJackGame = new BlackJackGame(InputView.getNames(), new CardFactory(Card.getCards()));
+        BlackJackGame blackJackGame = new BlackJackGame(InputView.askNames(), new CardFactory(Card.getCards()));
 
         blackJackGame.start(OutputView::printFirstCards);
-        blackJackGame.askPlayerHitOrStay(InputView::getAnswerOfAdditionalDraw, OutputView::printPlayerCard);
+        blackJackGame.askPlayerHitOrStay(InputView::askHitOrStay, OutputView::printPlayerCard);
         OutputView.printAdditionalDrawDealer(blackJackGame.askDealerHitOrStay());
 
         OutputView.printFinalCards(blackJackGame.getDealerDto(), blackJackGame.getPlayerDtos());
--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static List<String> getNames() {
+    public static List<String> askNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         List<String> names = Arrays.stream(SCANNER.nextLine().split(","))
                 .map(String::trim)
@@ -17,7 +17,7 @@ public class InputView {
         return names;
     }
 
-    public static boolean getAnswerOfAdditionalDraw(String name) {
+    public static boolean askHitOrStay(String name) {
         System.out.println(name + "는 한 장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
         return Answer.from(SCANNER.nextLine()).isYes();
     }
