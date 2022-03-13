7edc9b49a786822f3106fc70c4d0b0a188b1c90a
leo0842 <kunga077@gmail.com>
2022-03-13 21:49:25 +0900

    test Hit 클래스에 대한 테스트 케이스 추가

---
+++ src/test/java/blackjack/domain/machine/HitTest.java
@@ -0,0 +1,18 @@
+package blackjack.domain.machine;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class HitTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"y:true", "n:false"}, delimiter = ':')
+    @DisplayName("y와 n에 대한 응답 확인")
+    void isYesTest(String response, boolean hit) {
+        assertThat(Hit.isYes(response)).isEqualTo(hit);
+    }
+}
