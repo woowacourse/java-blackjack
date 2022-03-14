690252175cc61b67d9f7484e1ff9e6a9e835ecd5
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-03-14 15:12:47 +0900

    test : Participants 테스트 추가

--- src/test/java/blackjack/domain/participant/ParticipantsTest.java
+++ src/test/java/blackjack/domain/participant/ParticipantsTest.java
@@ -1,7 +1,12 @@
 package blackjack.domain.participant;
 
+import blackjack.domain.card.CardGenerator;
+import blackjack.domain.card.Deck;
+import blackjack.domain.result.DistributeResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -29,4 +34,42 @@ public class ParticipantsTest {
         }).isInstanceOf(RuntimeException.class);
     }
 
+    @Test
+    @DisplayName("딜러가 존재하지 않는 경우 딜러를 찾으려고 하면 예외를 발생시킨다.")
+    public void findDealerNotInParticipantsTest() {
+        Participants participants = new Participants();
+
+        assertThatThrownBy(() -> participants.getDealer()).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("참가자들 중에서 딜러가 있는 경우 해당 딜러를 찾는다.")
+    public void findDealerInParticipantsTest() {
+        Participants participants = new Participants();
+        participants.addDealer();
+
+        assertThat(participants.getDealer()).isInstanceOf(Dealer.class);
+    }
+
+    @Test
+    @DisplayName("카드를 분배받는 참가자들은 카드의 총합이 높아진다.")
+    public void distributeCardTest() {
+        Participants participants = new Participants();
+        participants.addDealer();
+        int beforeDistributeCardSum = participants.getDistributeResult().get(0).getCardSum();
+        participants.distributeCard(new Deck(new CardGenerator()));
+        int afterDistributeCardSum = participants.getDistributeResult().get(0).getCardSum();
+
+        assertThat(beforeDistributeCardSum < afterDistributeCardSum).isTrue();
+    }
+
+    @Test
+    @DisplayName("딜러가 아닌 유저의 정보만을 가져올 수 있다.")
+    public void getUserResultTest() {
+        Participants participants = new Participants();
+        participants.addDealer();
+        participants.addUsers(new String[]{"pokemon", "yugioh"});
+
+        assertThat(participants.getUsers().size()).isEqualTo(2);
+    }
 }
