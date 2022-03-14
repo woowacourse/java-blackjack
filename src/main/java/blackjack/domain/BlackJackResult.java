0eb514b3224ce9a39b70e2bff5395737cd4383f5
is2js <tingstyle1@gmail.com>
2022-03-14 19:37:21 +0900

    refactor: 전략 인터페이스 @FunctionalInterface 명시 및 result 업캐스팅 적용

--- src/main/java/blackjack/domain/BlackJackResult.java
+++ src/main/java/blackjack/domain/BlackJackResult.java
@@ -4,23 +4,22 @@ import blackjack.domain.player.Player;
 import blackjack.domain.player.Players;
 import java.util.EnumMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class BlackJackResult {
 
     private final Map<GameResult, Integer> dealerResult;
-    private final LinkedHashMap<Player, GameResult> gamblerResult;
+    private final Map<Player, GameResult> gamblerResult;
 
     private BlackJackResult(final Map<GameResult, Integer> dealerResult,
-                            final LinkedHashMap<Player, GameResult> gamblerResult) {
+                            final Map<Player, GameResult> gamblerResult) {
         this.dealerResult = dealerResult;
         this.gamblerResult = gamblerResult;
     }
 
     public static BlackJackResult of(final Players players) {
-        final LinkedHashMap<Player, GameResult> gamblerResult = new LinkedHashMap<>();
+        final Map<Player, GameResult> gamblerResult = new LinkedHashMap<>();
         final EnumMap<GameResult, Integer> dealerResult = players.getGamblers()
             .stream()
             .collect(Collectors.groupingBy(
@@ -31,19 +30,8 @@ public class BlackJackResult {
         return new BlackJackResult(dealerResult, gamblerResult);
     }
 
-    public static BlackJackResult of(Player dealer, List<Player> gamblers) {
-        final LinkedHashMap<Player, GameResult> gamblerResult = new LinkedHashMap<>();
-        final EnumMap<GameResult, Integer> dealerResult = gamblers.stream()
-            .collect(Collectors.groupingBy(
-                gambler -> getResultPlayer(dealer, gamblerResult, gambler),
-                () -> new EnumMap<>(GameResult.class),
-                Collectors.summingInt(count -> 1)
-            ));
-        return new BlackJackResult(dealerResult, gamblerResult);
-    }
-
     private static GameResult getResultPlayer(final Player dealer,
-                                              final LinkedHashMap<Player, GameResult> gamblerResult,
+                                              final Map<Player, GameResult> gamblerResult,
                                               final Player gambler) {
         final GameResult currentDealerResult = dealer.compare(gambler);
         gamblerResult.put(gambler, currentDealerResult.reverse());
@@ -54,7 +42,7 @@ public class BlackJackResult {
         return dealerResult;
     }
 
-    public LinkedHashMap<Player, GameResult> getGamblerResult() {
+    public Map<Player, GameResult> getGamblerResult() {
         return gamblerResult;
     }
 }
--- src/main/java/blackjack/domain/cardGenerator/CardGenerator.java
+++ src/main/java/blackjack/domain/cardGenerator/CardGenerator.java
@@ -3,6 +3,8 @@ package blackjack.domain.cardGenerator;
 import blackjack.domain.card.PlayingCard;
 import java.util.Stack;
 
+@FunctionalInterface
 public interface CardGenerator {
+
     Stack<PlayingCard> generate();
 }
