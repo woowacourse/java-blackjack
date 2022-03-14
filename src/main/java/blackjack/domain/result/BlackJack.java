2bfa8aa39272b52bf63a7dd2cacc8b1f9e7430ad
jayjaehunchoi <wogns0108@naver.com>
2022-03-14 15:09:55 +0900

    feat : Result 인터페이스 생성

--- src/main/java/blackjack/domain/result/BlackJack.java
+++ src/main/java/blackjack/domain/result/BlackJack.java
@@ -1,9 +1,10 @@
 package blackjack.domain.result;
 
-public class BlackJack {
+public class BlackJack implements Result{
 
     private static final double CALCULATE_UNIT = 1.5;
 
+    @Override
     public int calculateBet(int amount) {
         return (int) (amount * CALCULATE_UNIT);
     }
---
+++ src/main/java/blackjack/domain/result/Result.java
@@ -0,0 +1,6 @@
+package blackjack.domain.result;
+
+public interface Result {
+
+    int calculateBet(int amount);
+}
