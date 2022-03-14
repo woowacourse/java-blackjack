c73e88e00a5e0cd45d41cb3f47d54a4e404abd73
asebn1 <asebn1@khu.ac.kr>
2022-03-14 22:48:01 +0900

    test: Bet 테스트 추가

--- src/test/java/blackjack/domain/player/BetTest.java
+++ src/test/java/blackjack/domain/player/BetTest.java
@@ -13,19 +13,30 @@ import org.junit.jupiter.api.Test;
 class BetTest {
 
     @Test
-    @DisplayName("승리시 1.5배, 패배시 배팅돈을 잃는다.")
-    void checkParticipantProfit(){
+    @DisplayName("승리시 1.5배를 얻는다.")
+    void checkParticipantWinProfit(){
         Deck deck = new Deck(new DeckGeneratorImpl());
         List<Card> initCards = new ArrayList<>();
         initCards.add(deck.draw());
         initCards.add(deck.draw());
 
         Participant participantWin = new Participant(initCards, "pobi", new Bet(1000));
-        Participant participantLose = new Participant(initCards, "pobi", new Bet(1000));
         participantWin.win();
-        participantLose.lose();
 
         assertThat(participantWin.getBetProfit()).isEqualTo(500);
+    }
+
+    @Test
+    @DisplayName("패배시 배팅돈을 모두 잃는다.")
+    void checkParticipantLoseProfit(){
+        Deck deck = new Deck(new DeckGeneratorImpl());
+        List<Card> initCards = new ArrayList<>();
+        initCards.add(deck.draw());
+        initCards.add(deck.draw());
+
+        Participant participantLose = new Participant(initCards, "pobi", new Bet(1000));
+        participantLose.lose();
+
         assertThat(participantLose.getBetProfit()).isEqualTo(-1000);
     }
 }
