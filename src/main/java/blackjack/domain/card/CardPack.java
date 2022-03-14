b2a2db224bd095d27d36ebf1facfadafdbfe43d0
dongho <dongho108@naver.com>
2022-03-14 17:38:41 +0900

    refactor: CardPack 리팩터링
    * cardPack의 구현체를 ArrayList에서 LinkedList로 변경 (remove 자원 절약)
    * 52장의 카드 인스턴스 재사용 하도록 수정

--- src/main/java/blackjack/domain/card/CardPack.java
+++ src/main/java/blackjack/domain/card/CardPack.java
@@ -1,40 +1,45 @@
 package blackjack.domain.card;
 
-import blackjack.domain.card.property.CardNumber;
-import blackjack.domain.card.property.CardShape;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
+import blackjack.domain.card.property.CardNumber;
+import blackjack.domain.card.property.CardShape;
+
 public class CardPack {
-    private final List<Card> cards = new ArrayList<>();
+    private final List<Card> cardPack = new LinkedList<>();
+    private static final List<Card> cards = createCards();
 
     public CardPack() {
         initializeCards();
     }
 
-    private void initializeCards() {
-        addNewCards();
-        Collections.shuffle(cards);
-    }
-
-    private void addNewCards() {
+    private static List<Card> createCards() {
+        List<Card> cards = new ArrayList<>();
         for (CardShape cardShape : CardShape.values()) {
-            addCardsBy(cardShape);
+            addCards(cards, cardShape);
         }
+        return cards;
     }
 
-    private void addCardsBy(CardShape cardShape) {
+    private static void addCards(List<Card> cards, CardShape cardShape) {
         for (CardNumber cardNumber : CardNumber.values()) {
             cards.add(new Card(cardShape, cardNumber));
         }
     }
 
+    private void initializeCards() {
+        Collections.shuffle(cards);
+        cardPack.addAll(cards);
+    }
+
     public Card pickOne() {
-        if (cards.isEmpty()) {
+        if (cardPack.isEmpty()) {
             initializeCards();
         }
 
-        return cards.remove(0);
+        return cardPack.remove(0);
     }
 }
