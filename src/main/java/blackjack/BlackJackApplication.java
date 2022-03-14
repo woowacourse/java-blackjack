d9a35a253d777f7d24e20afdd5c04882b3d246f4
skullKim <kyk990328@gmail.com>
2022-03-14 19:58:17 +0900

    refactor(blackjack/): 컨트롤러 삭제
    구현에 불필요한 컨트롤러 삭제

--- src/main/java/blackjack/BlackJackApplication.java
+++ src/main/java/blackjack/BlackJackApplication.java
@@ -2,7 +2,7 @@ package blackjack;
 
 import java.util.List;
 
-import blackjack.controller.BlackJackController;
+import blackjack.domain.RedrawChoice;
 import blackjack.dto.PlayerTurnDto;
 import blackjack.dto.TableStatusDto;
 import blackjack.service.BlackJackService;
@@ -12,25 +12,26 @@ import blackjack.view.OutputView;
 public class BlackJackApplication {
 
 	public static void main(String[] args) {
-		BlackJackController controller = new BlackJackController(new BlackJackService());
-
-		controller.addPlayers(InputView.requestPlayerName());
-
-		TableStatusDto dealerStatus = controller.distributeCardToDealer();
-		List<TableStatusDto> playersStatus = controller.distributeCardToPlayers();
+		final BlackJackService blackJack = new BlackJackService();
+		blackJack.initBlackJack();
+		blackJack.joinPlayers(InputView.requestPlayerName());
+		TableStatusDto dealerStatus = blackJack.distributeCardToDealer();
+		List<TableStatusDto> playersStatus = blackJack.distributeCardToPlayers();
 		OutputView.printInitialStatus(dealerStatus, playersStatus);
 
 		while (true) {
-			PlayerTurnDto currentPlayer = controller.getWhoseTurn();
+			PlayerTurnDto currentPlayer = blackJack.whoseTurn();
 			if (hasMorePlayer(currentPlayer)) {
 				break;
 			}
 			String answer = InputView.drawOneMoreCard(currentPlayer);
-			TableStatusDto playerStatus = controller.drawPlayer(answer, currentPlayer.getName());
+			TableStatusDto playerStatus = blackJack.drawPlayer(RedrawChoice.of(answer),
+				currentPlayer.getName());
 			OutputView.printPersonalHand(playerStatus);
 		}
-		OutputView.printDealerStatus(controller.drawDealer());
-		OutputView.printFinalResult(controller.getFinalResult());
+		OutputView.printDealerStatus(blackJack.drawDealer());
+		OutputView.printFinalResult(blackJack.calculateFinalResult());
+
 	}
 
 	private static boolean hasMorePlayer(PlayerTurnDto player) {
