008d0d997e1c0f9d95974e9160d0ee763e797694
Donggyu <a29661498@gmail.com>
2022-03-14 10:53:57 +0900

    refactor(Users): `drawAdditionalCard` 로직 검증
    "추가 카드 뽑기" 메소드 로직을 검증하였습니다.
    처음 Player와 Dealer는 카드를 갖고 있지 않을 것이고, "drawAdditionalCard" 호출 이후에 각각 0개에서 1개로 카드를 한장씩 추가로 뽑았는지를 검증하도록 하였습니다.

--- src/test/java/blackjack/domain/user/UsersTest.java
+++ src/test/java/blackjack/domain/user/UsersTest.java
@@ -1,7 +1,10 @@
 package blackjack.domain.user;
 
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import blackjack.domain.card.Deck;
+import blackjack.domain.strategy.ShuffledDeckGenerateStrategy;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -23,5 +26,30 @@ class UsersTest {
                 () -> assertThat(users).isNotNull(),
                 () -> assertThat(users.getPlayers().size()).isEqualTo(2)
         );
+    }
+
+    @DisplayName("추가 카드 뽀는 로직 검증")
+    @Test
+    public void testDrawAdditionalCard() {
+        //given
+        List<String> names = List.of("pobi", "jason");
+        Users users = Users.of(names, new Dealer());
+        Deck deck = new Deck(new ShuffledDeckGenerateStrategy());
+
+        //when
+        users.drawAdditionalCard(user -> user.drawCard(deck),
+                dealer -> dealer.drawCard(deck));
+
+        //then
+        List<Integer> count = users.getPlayers()
+                .stream()
+                .map(user -> user.showCards().size())
+                .collect(toList());
+
+        int size = users.getDealer().showCards().size();
+
+        assertThat(count.get(0)).isEqualTo(1);
+        assertThat(count.get(1)).isEqualTo(1);
+        assertThat(size).isEqualTo(1);
     }
 }
