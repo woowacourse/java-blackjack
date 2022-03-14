447c5220072365efb86f1e9e8e159e5937c1d9e8
jojogreen91 <iawbg13@gmail.com>
2022-03-14 23:14:31 +0900

    refactor: Suit 를 이모지로 변경

--- src/main/java/domain/card/Suit.java
+++ src/main/java/domain/card/Suit.java
@@ -2,7 +2,7 @@ package domain.card;
 
 public enum Suit {
 
-    CLOVER("클로버"), SPADE("스페이드"), HEART("하트"), DIAMOND("다이아몬드");
+    CLOVER("♣️"), SPADE("♠️"), HEART("♥️"), DIAMOND("♦️");
 
     private final String suit;
 
