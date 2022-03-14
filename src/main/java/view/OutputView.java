e4b739837657a12ffba1b31e64520d7c836765f0
Richard JEON <ztzy1907@gmail.com>
2022-03-15 07:26:04 +0900

    refactor(view): 결과 자료구조 변경에 따른 뷰 대응처리

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,6 +3,9 @@ package view;
 import static domain.MatchResult.DRAW;
 import static domain.MatchResult.LOSE;
 import static domain.MatchResult.WIN;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
 
 import domain.BlackJackResult;
@@ -13,12 +16,13 @@ import domain.card.Suit;
 import domain.player.Dealer;
 import dto.CardsAndScoreDto;
 import dto.CardsDto;
+import dto.ResultDto;
+import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 public class OutputView {
-    private static final long ZERO_FOR_DEFAULT = 0L;
     private static final String NEW_LINE = System.lineSeparator();
     private static final String INFO_FOR_INITIAL_SPREAD = "%s와 %s에게 2장을 나누었습니다." + NEW_LINE;
     private static final String INFO_FOR_DEALER_ADD_CARD = "%s는 16이하라 한장의 카드를 더 받았습니다." + NEW_LINE;
@@ -26,34 +30,36 @@ public class OutputView {
     private static final String INFO_CARD_STATUS_AFTER_INITIAL_SPREAD = "%s: %s" + NEW_LINE;
     private static final String COLON_FOR_JOINING_NAME_AND_CARD = ": ";
     private static final String RESULT_TITLE = "## 최종 승패";
-    private static final String DEALER_RESULT_PRINT_TEMPLATE = "딜러: %d승 %d무 %d패" + NEW_LINE;
-    private static final String STRING_FOR_WIN = "승";
-    private static final String STRING_FOR_LOSE = "패";
-    private static final String STRING_FOR_DRAW = "무";
     private static final String CARD_NAME_JOIN_CHARACTER = ", ";
     private static final String GAMBLER_NAME_DELIMITER = ", ";
-    private static final Map<Denomination, String> denominationName = new EnumMap<>(Denomination.class);
-    private static final Map<Suit, String> suitName = new EnumMap<>(Suit.class);
+    private static final Map<Denomination, String> DENOMINATION_NAME_MAPPER = new EnumMap<>(Denomination.class);
+    private static final Map<Suit, String> SUIT_NAME_MAPPER = new EnumMap<>(Suit.class);
+    private static final Map<MatchResult, String> MATCH_RESULT_MAPPER = new EnumMap<>(MatchResult.class);
+    private static final String WIN_DRAW_LOSE_DELIMITER = " ";
 
     static {
-        denominationName.put(Denomination.KING, "K");
-        denominationName.put(Denomination.QUEEN, "Q");
-        denominationName.put(Denomination.JACK, "J");
-        denominationName.put(Denomination.ACE, "A");
-        denominationName.put(Denomination.TWO, "2");
-        denominationName.put(Denomination.THREE, "3");
-        denominationName.put(Denomination.FOUR, "4");
-        denominationName.put(Denomination.FIVE, "5");
-        denominationName.put(Denomination.SIX, "6");
-        denominationName.put(Denomination.SEVEN, "7");
-        denominationName.put(Denomination.EIGHT, "8");
-        denominationName.put(Denomination.NINE, "9");
-        denominationName.put(Denomination.TEN, "10");
+        DENOMINATION_NAME_MAPPER.put(Denomination.KING, "K");
+        DENOMINATION_NAME_MAPPER.put(Denomination.QUEEN, "Q");
+        DENOMINATION_NAME_MAPPER.put(Denomination.JACK, "J");
+        DENOMINATION_NAME_MAPPER.put(Denomination.ACE, "A");
+        DENOMINATION_NAME_MAPPER.put(Denomination.TWO, "2");
+        DENOMINATION_NAME_MAPPER.put(Denomination.THREE, "3");
+        DENOMINATION_NAME_MAPPER.put(Denomination.FOUR, "4");
+        DENOMINATION_NAME_MAPPER.put(Denomination.FIVE, "5");
+        DENOMINATION_NAME_MAPPER.put(Denomination.SIX, "6");
+        DENOMINATION_NAME_MAPPER.put(Denomination.SEVEN, "7");
+        DENOMINATION_NAME_MAPPER.put(Denomination.EIGHT, "8");
+        DENOMINATION_NAME_MAPPER.put(Denomination.NINE, "9");
+        DENOMINATION_NAME_MAPPER.put(Denomination.TEN, "10");
 
-        suitName.put(Suit.CLUBS, "클로버");
-        suitName.put(Suit.DIAMONDS, "다이아몬드");
-        suitName.put(Suit.HEARTS, "하트");
-        suitName.put(Suit.SPADES, "스페이드");
+        SUIT_NAME_MAPPER.put(Suit.CLUBS, "클로버");
+        SUIT_NAME_MAPPER.put(Suit.DIAMONDS, "다이아몬드");
+        SUIT_NAME_MAPPER.put(Suit.HEARTS, "하트");
+        SUIT_NAME_MAPPER.put(Suit.SPADES, "스페이드");
+
+        MATCH_RESULT_MAPPER.put(WIN, "승");
+        MATCH_RESULT_MAPPER.put(DRAW, "무");
+        MATCH_RESULT_MAPPER.put(LOSE, "패");
     }
 
     private OutputView() {
@@ -79,7 +85,7 @@ public class OutputView {
     }
 
     private static String parseCardName(Card card) {
-        return denominationName.get(card.getDenomination()) + suitName.get(card.getSuit());
+        return DENOMINATION_NAME_MAPPER.get(card.getDenomination()) + SUIT_NAME_MAPPER.get(card.getSuit());
     }
 
     public static void printLineSeparator() {
@@ -88,8 +94,7 @@ public class OutputView {
 
     public static void printCards(CardsDto playerDto) {
         System.out.println(
-                playerDto.getName() + COLON_FOR_JOINING_NAME_AND_CARD + getJoinedCardNames(
-                        playerDto.getCards()));
+                playerDto.getName() + COLON_FOR_JOINING_NAME_AND_CARD + getJoinedCardNames(playerDto.getCards()));
     }
 
     public static void printDealerAddCard(Dealer dealer) {
@@ -109,34 +114,35 @@ public class OutputView {
 
     public static void printResult(BlackJackResult blackJackResult) {
         System.out.println(System.lineSeparator() + RESULT_TITLE);
-        printDealerResult(blackJackResult);
-        blackJackResult.getGamblerResult()
+
+        blackJackResult.getBlackjackResult()
+                .entrySet()
                 .forEach(OutputView::printSingleGamblerResult);
     }
 
-    private static void printDealerResult(BlackJackResult blackJackResult) {
-        Map<MatchResult, Long> dealerResult = blackJackResult.getDealerResult();
-        System.out.printf(
-                DEALER_RESULT_PRINT_TEMPLATE,
-                dealerResult.getOrDefault(WIN, ZERO_FOR_DEFAULT),
-                dealerResult.getOrDefault(DRAW, ZERO_FOR_DEFAULT),
-                dealerResult.getOrDefault(LOSE, ZERO_FOR_DEFAULT)
-        );
+    public static void printSingleGamblerResult(Map.Entry<String, ResultDto> entry) {
+        System.out.println(
+                entry.getKey() + COLON_FOR_JOINING_NAME_AND_CARD + getResultMessageByResultDto(entry.getValue()));
     }
 
-    public static void printSingleGamblerResult(String name, MatchResult matchResult) {
-        System.out.println(name + COLON_FOR_JOINING_NAME_AND_CARD + getStringByMatchResult(matchResult));
+    private static String getResultMessageByResultDto(ResultDto resultDto) {
+        if (resultDto.isGamblerResult()) {
+            return MATCH_RESULT_MAPPER.get(resultDto.getGamblerResult());
+        }
+
+        return getDealerResult(getDealerResultByResultDto(resultDto));
     }
 
-    private static String getStringByMatchResult(MatchResult matchResult) {
-        if (matchResult == WIN) {
-            return STRING_FOR_WIN;
-        }
+    private static Map<MatchResult, Long> getDealerResultByResultDto(ResultDto resultDto) {
+        return resultDto.getMatchResults()
+                .stream()
+                .collect(groupingBy(identity(), () -> new EnumMap<>(MatchResult.class), counting()));
+    }
 
-        if (matchResult == LOSE) {
-            return STRING_FOR_LOSE;
-        }
-
-        return STRING_FOR_DRAW;
+    private static String getDealerResult(Map<MatchResult, Long> dealerResult) {
+        return Arrays.stream(MatchResult.values())
+                .filter(dealerResult::containsKey)
+                .map(matchResult -> dealerResult.get(matchResult) + MATCH_RESULT_MAPPER.get(matchResult))
+                .collect(joining(WIN_DRAW_LOSE_DELIMITER));
     }
 }
