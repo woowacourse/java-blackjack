ddcad204dbb3d9e1157c3d32316c93406c8b1a64
jayjaehunchoi <wogns0108@naver.com>
2022-03-14 14:04:07 +0900

    feat : 베팅금 Input 기능 추가

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -3,6 +3,7 @@ package blackjack.view;
 import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,6 +15,7 @@ public class InputView {
     private static final String INPUT_BLANK = " ";
     private static final String INPUT_NOT_BLANK = "";
     private static final String REQUEST_PLAYER_ANSWER_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
+    private static final String REQUEST_PLAYER_BETTING_MESSAGE = "%s의 배팅 금액은?\n";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -29,6 +31,14 @@ public class InputView {
         return name.replaceAll(INPUT_BLANK, INPUT_NOT_BLANK);
     }
 
+    public static int requestBettingMoney(final String name) {
+        System.out.printf(REQUEST_PLAYER_BETTING_MESSAGE, name);
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException ("[ERROR] 베팅금은 숫자만 입력 가능합니다.");
+        }
+    }
 
     public static String requestAnswer(final String name) {
         System.out.printf(REQUEST_PLAYER_ANSWER_MESSAGE, name);
