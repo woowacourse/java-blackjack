03141476b62715a106ef12a3f565e7248f3f8699
Chris <bjuuuu98@gmail.com>
2022-03-14 23:33:34 +0900

    test: DisplayName 수정 및 중복 테스트 코드 제거

--- src/test/java/blackjack/domain/card/CardNumberTest.java
+++ src/test/java/blackjack/domain/card/CardNumberTest.java
@@ -8,9 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CardNumberTest {
 
@@ -24,40 +27,19 @@ class CardNumberTest {
         assertThat(distinctCount).isEqualTo(13);
     }
 
-    @Nested
-    @DisplayName("카드 숫자들을 받아 카드의 합을 반환한다.(총합이 21이 넘지 않는 경우, a는 11로 계산할 수 있다.)")
-    class CalculateScore {
+    @ParameterizedTest
+    @DisplayName("카드 숫자들을 받아, 21 이하의 최대 합을 반환한다. J, Q, K는 10으로 계산하고 A는 1 혹은 11로 계산할 수 있다.")
+    @MethodSource("provideCardNumbersAndExpectedSum")
+    void calculateScore(List<CardNumber> cardNumbers, int expected) {
+        final int result = CardNumber.calculateScore(cardNumbers);
+        assertThat(result).isEqualTo(expected);
+    }
 
-        @Test
-        @DisplayName("3 7 10은 스코어가 20이다.")
-        void calculateScore_3_7_10() {
-            final List<CardNumber> cardNumbers = Arrays.asList(THREE, SEVEN, TEN);
-            final int result = CardNumber.calculateScore(cardNumbers);
-            assertThat(result).isEqualTo(20);
-        }
-
-        @Test
-        @DisplayName("a 10은 스코어가 21이다.")
-        void calculateScore_A_10() {
-            final List<CardNumber> cardNumbers = Arrays.asList(A, TEN);
-            final int result = CardNumber.calculateScore(cardNumbers);
-            assertThat(result).isEqualTo(21);
-        }
-
-        @Test
-        @DisplayName("a a 10은 스코어가 12이다.")
-        void calculateScore_A_A_10() {
-            final List<CardNumber> cardNumbers = Arrays.asList(A, A, TEN);
-            final int result = CardNumber.calculateScore(cardNumbers);
-            assertThat(result).isEqualTo(12);
-        }
-
-        @Test
-        @DisplayName("a a 10 10은 스코어가 22이다.")
-        void calculateScore_A_A_10_10() {
-            final List<CardNumber> cardNumbers = Arrays.asList(A, A, TEN, TEN);
-            final int result = CardNumber.calculateScore(cardNumbers);
-            assertThat(result).isEqualTo(22);
-        }
+    private static Stream<Arguments> provideCardNumbersAndExpectedSum() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(THREE, SEVEN, TEN), 20),
+                Arguments.of(Arrays.asList(A, TEN), 21),
+                Arguments.of(Arrays.asList(A, A, TEN), 12)
+        );
     }
 }
