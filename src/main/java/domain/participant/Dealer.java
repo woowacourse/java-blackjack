be3e178caf73b57510f3b6d1bd879ae7c24fe56e
yoon <duddbs774@gmail.com>
2022-03-14 17:16:35 +0900

    refactor: 딜러이름 상수화

--- src/main/java/domain/participant/Dealer.java
+++ src/main/java/domain/participant/Dealer.java
@@ -10,11 +10,12 @@ import java.util.Map;
 
 public final class Dealer extends Participant {
     private static final int FIRST_INDEX = 0;
+    private static final String DEALER_NAME = "딜러";
 
     private ParticipantState gameState = ParticipantState.RUNNING;
 
     public Dealer() {
-        super(HitThreshold.DEALER_THRESHOLD, "딜러");
+        super(HitThreshold.DEALER_THRESHOLD, DEALER_NAME);
     }
 
     public void stopRunning() {
