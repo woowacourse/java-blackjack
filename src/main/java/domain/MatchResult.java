3cb7d3adf30aedb526974be61a2be3036ec3213d
Richard JEON <ztzy1907@gmail.com>
2022-03-15 07:24:13 +0900

    refactor(match-result): 승패 판정 로직 Player로 이동하며 MatchREsult 내 로직 제거

--- src/main/java/domain/MatchResult.java
+++ src/main/java/domain/MatchResult.java
@@ -1,44 +1,8 @@
 package domain;
 
-import domain.player.Player;
-import java.util.Arrays;
-import java.util.function.BiPredicate;
-
 public enum MatchResult {
-    WIN((dealer, gambler) ->
-            !gambler.isBust() &&
-                    (dealer.isBust() || dealer.getScore() < gambler.getScore()
-                            || (!dealer.isBlackJack() && gambler.isBlackJack()))),
-    LOSE((dealer, gambler) ->
-            gambler.isBust()
-                    || (dealer.getScore() > gambler.getScore())
-                    || (!gambler.isBlackJack() && dealer.isBlackJack())),
-    DRAW((dealer, gambler) -> !gambler.isBust() && dealer.getScore() == gambler.getScore() &&
-            (gambler.isBlackJack() && dealer.isBlackJack() || !gambler.isBlackJack() && !dealer.isBlackJack())),
-    NO_MATCH((dealer, gambler) -> false);
-
-    private final BiPredicate<Player, Player> judge;
-
-    MatchResult(BiPredicate<Player, Player> judge) {
-        this.judge = judge;
-    }
-
-    public static MatchResult of(Player dealer, Player gambler) {
-        return Arrays.stream(values())
-                .filter(matchResult -> matchResult.judge.test(dealer, gambler))
-                .findAny()
-                .orElse(NO_MATCH);
-    }
-
-    public static MatchResult opposite(MatchResult matchResult) {
-        if (matchResult == WIN) {
-            return LOSE;
-        }
-
-        if (matchResult == LOSE) {
-            return WIN;
-        }
-
-        return DRAW;
-    }
+    WIN,
+    DRAW,
+    LOSE,
+    ;
 }
