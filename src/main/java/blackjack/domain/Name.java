0a79886f43430bfe325d9ddced7458ec32ab1b46
bcc0830 <bcc0830@naver.com>
2022-03-14 21:44:11 +0900

    refactor : 정규식 대신 String 메소드 사용

--- src/main/java/blackjack/domain/Name.java
+++ src/main/java/blackjack/domain/Name.java
@@ -5,14 +5,13 @@ import java.util.regex.Pattern;
 public class Name {
 	private static final String BLANK_NAME = "[ERROR] 이름이 빈 입력이면 안됩니다.";
 	private static final String SPACE_IN_NAME = "[ERROR] 이름에 공백이 있으면 안됩니다.";
-	private static final Pattern BLANK = Pattern.compile("[\\s]+");
 	private final String name;
 
 	public Name(String name) {
 		if (name == null || name.isEmpty()) {
 			throw new IllegalArgumentException(BLANK_NAME);
 		}
-		if (BLANK.matcher(name).matches()) {
+		if (name.isBlank()) {
 			throw new IllegalArgumentException(SPACE_IN_NAME);
 		}
 		this.name = name;
