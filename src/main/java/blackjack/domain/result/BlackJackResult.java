dc0f9946079fec77a21b11fc92c57eb46b6c0cd4
jayjaehunchoi <wogns0108@naver.com>
2022-03-14 14:57:50 +0900

    feat : BlackJackResult BlackJack, Bust, Hit 탐색 기능 추가

--- src/main/java/blackjack/domain/result/BlackJackResult.java
+++ src/main/java/blackjack/domain/result/BlackJackResult.java
@@ -1,12 +1,28 @@
 package blackjack.domain.result;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.player.Gamer;
 import blackjack.domain.player.Player;
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 public enum BlackJackResult {
 
-    BLACK_JACK;
+    BLACK_JACK(player -> player.showCards().size() == Card.START_CARD_COUNT &&
+            player.calculateResult() == Gamer.LIMIT_GAMER_TOTAL_POINT),
+    BUST(player -> player.calculateResult() > Gamer.LIMIT_GAMER_TOTAL_POINT),
+    HIT(Player::isSatisfyReceiveCondition);
+
+    private final Predicate<Player> predicate;
+
+    BlackJackResult(final Predicate<Player> predicate) {
+        this.predicate = predicate;
+    }
 
     public static BlackJackResult findBlackJackResult(final Player player) {
-        return BLACK_JACK;
+        return Arrays.stream(values())
+                .filter(value -> value.predicate.test(player))
+                .findFirst()
+                .orElse(HIT);
     }
 }
--- src/test/java/blackjack/domain/result/BlackJackResultTest.java
+++ src/test/java/blackjack/domain/result/BlackJackResultTest.java
@@ -21,4 +21,26 @@ public class BlackJackResultTest {
 
         assertThat(BlackJackResult.findBlackJackResult(gamer)).isEqualTo(BlackJackResult.BLACK_JACK);
     }
+
+    @Test
+    @DisplayName("블랙잭 게임 룰 상 결과가 버스트이다.")
+    void blackJackResultBust() {
+        Gamer gamer = new Gamer("huni", new Bet(10000));
+        gamer.receiveCard(new Card(Suit.DIAMOND, Denomination.JACK));
+        gamer.receiveCard(new Card(Suit.SPADE, Denomination.JACK));
+        gamer.receiveCard(new Card(Suit.HEART, Denomination.TWO));
+
+        assertThat(BlackJackResult.findBlackJackResult(gamer)).isEqualTo(BlackJackResult.BUST);
+    }
+
+    @Test
+    @DisplayName("블랙잭 게임 룰 상 결과가 히트 이다.")
+    void blackJackResultHit() {
+        Gamer gamer = new Gamer("huni", new Bet(10000));
+        gamer.receiveCard(new Card(Suit.DIAMOND, Denomination.JACK));
+        gamer.receiveCard(new Card(Suit.SPADE, Denomination.JACK));
+
+        assertThat(BlackJackResult.findBlackJackResult(gamer)).isEqualTo(BlackJackResult.HIT);
+    }
+
 }
