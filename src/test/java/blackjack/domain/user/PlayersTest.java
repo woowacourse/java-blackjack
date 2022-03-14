9d23f5ecb2d6742dac139b7d050d78a25781ccec
EastHShin <xldk78@gmail.com>
2022-03-14 19:48:25 +0900

    test(blackjack/domain/user/PlayersTest): 중복된 플레이어 이름 입력시 예외발생 기능에 대한 테스트 코드 작성

--- src/test/java/blackjack/domain/user/PlayersTest.java
+++ src/test/java/blackjack/domain/user/PlayersTest.java
@@ -2,7 +2,6 @@ package blackjack.domain.user;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,9 +12,18 @@ public class PlayersTest {
 	@DisplayName("빈 리스트를 받았을 때 에러 발생")
 	void check_empty_list() {
 		String lines = ",,,,";
-		List<String> strings = Arrays.asList(lines.split(","));
+		List<String> strings = List.of(lines.split(","));
 		assertThatThrownBy(() -> new Players(strings))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining(Players.EMPTY_PLAYER_EXCEPTION);
+	}
+
+	@Test
+	@DisplayName("중복된 플레이어를 받았을 경우 에러 발생")
+	void check_duplicate_player() {
+		List<String> playerNames = List.of("pobi", "pobi");
+		assertThatThrownBy(() -> new Players(playerNames))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(Players.DUPLICATE_PLAYER_EXCEPTION);
 	}
 }
