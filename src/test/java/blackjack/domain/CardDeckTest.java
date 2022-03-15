92e589c1c72337c455f42ed82f3af958dbfb09b8
이한울 <dkfghg3@naver.com>
2022-03-15 22:34:18 +0900

    test: 첫 draw 하는 메서드 테스트 코드 추가

--- src/test/java/blackjack/domain/CardDeckTest.java
+++ src/test/java/blackjack/domain/CardDeckTest.java
@@ -11,9 +11,21 @@ public class CardDeckTest {
     @DisplayName("사용한 카드는 제거한다")
     void drawCard() {
         final CardDeck cardDeck = new CardDeck(new CardDeckGenerator());
-        final int expected = 51;
+        final int expected = cardDeck.size() - 1;
 
         cardDeck.draw();
+        final int actual = cardDeck.size();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("처음 플레이어에게 draw 하는 카드 수는 2장이다.")
+    void drawInitialCards() {
+        final CardDeck cardDeck = new CardDeck(new CardDeckGenerator());
+        final int expected = cardDeck.size() - 2;
+
+        cardDeck.drawInitialCards();
         final int actual = cardDeck.size();
 
         assertThat(actual).isEqualTo(expected);
