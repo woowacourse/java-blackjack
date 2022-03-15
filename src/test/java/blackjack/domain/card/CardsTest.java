0e53da4780a667e4ddeee9e2b8b5552ef168a90f
leo0842 <kunga077@gmail.com>
2022-03-15 16:14:26 +0900

    test: Cards 도메인 테스트 케이스 추가

--- src/test/java/blackjack/domain/card/CardsTest.java
+++ src/test/java/blackjack/domain/card/CardsTest.java
@@ -26,4 +26,12 @@ public class CardsTest {
         Card compareCard = new Card(Suit.CLUB, Rank.KING);
         assertThat(card).isEqualTo(compareCard);
     }
+
+    @Test
+    @DisplayName("차례대로 받아오는 카드 두 개가 다른 카드인지 확인")
+    public void checkTwoDifferentCards() {
+        Card firstCard = cards.assignCard();
+        Card secondCard = cards.assignCard();
+        assertThat(firstCard).isNotEqualTo(secondCard);
+    }
 }
