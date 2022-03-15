66ee89552112a8095f01540e436b218a7c632d66
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-16 03:48:50 +0900

    feat: 배팅금액 0이하인 경우 예외처리 및 테스트

--- src/main/java/blackjack/domain/paticipant/Player.java
+++ src/main/java/blackjack/domain/paticipant/Player.java
@@ -5,16 +5,25 @@ import java.util.Objects;
 public class Player {
 
     private final String name;
+    private final int betMoney;
 
-    public Player(final String name) {
+    public Player(final String name, final int betMoney) {
         Objects.requireNonNull(name, "이름은 null이 들어올 수 없습니다.");
         checkEmptyName(name);
+        checkNotPositiveBetMoney(betMoney);
         this.name = name;
+        this.betMoney = betMoney;
     }
 
     private void checkEmptyName(final String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("이름은 공백이 들어올 수 없습니다.");
+        }
+    }
+
+    private void checkNotPositiveBetMoney(final int betMoney) {
+        if (betMoney <= 0) {
+            throw new IllegalArgumentException("배팅금액은 0이하의 값이 들어올 수 없습니다.");
         }
     }
 }
--- src/test/java/blackjack/domain/paticipant/PlayerTest.java
+++ src/test/java/blackjack/domain/paticipant/PlayerTest.java
@@ -5,12 +5,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayerTest {
 
     @Test
     void 플레이어의_이름이_null인_경우_예외발생() {
-        assertThatThrownBy(() -> new Player(null))
+        assertThatThrownBy(() -> new Player(null, 1000))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("이름은 null이 들어올 수 없습니다.");
     }
@@ -18,8 +19,16 @@ class PlayerTest {
     @ParameterizedTest
     @EmptySource
     void 플레이어의_이름이_공백인_경우_예외발생(final String name) {
-        assertThatThrownBy(() -> new Player(name))
+        assertThatThrownBy(() -> new Player(name, 1000))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 공백이 들어올 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1000})
+    void 배팅금액이_0이하인_경우_예외발생(final int betMoney) {
+        assertThatThrownBy(() -> new Player("name", betMoney))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("배팅금액은 0이하의 값이 들어올 수 없습니다.");
     }
 }
