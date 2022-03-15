5d39077592c59dfd762a9712a59ceb7df437cf6b
sure-why-not <pkse1213@gmail.com>
2022-03-16 01:48:19 +0900

    refactor: 승패 계산 로직을 간단하게 수정

--- src/main/java/blackjack/domain/Judgement.java
+++ src/main/java/blackjack/domain/Judgement.java
@@ -1,59 +1,35 @@
 package blackjack.domain;
 
-import java.util.function.Function;
-
 import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Player;
 
 public enum Judgement {
 
-    BLACKJACK(money -> money.calculateProfit(1.5)),
-    WIN(money -> money.calculateProfit(1)),
-    DRAW(money -> money.calculateProfit(1)),
-    LOSE(money -> money.calculateProfit(-1));
+    BLACKJACK(1.5),
+    WIN(1),
+    LOSE(-1),
+    DRAW(1);
 
-    private final Function<Money, Profit> profitFunction;
+    private final double profitMultiple;
 
-    Judgement(Function<Money, Profit> profitFunction) {
-        this.profitFunction = profitFunction;
+    Judgement(double profitMultiple) {
+        this.profitMultiple = profitMultiple;
     }
 
     public static Judgement judgePlayer(Player player, Dealer dealer) {
-        if (dealer.isBlackJack() || player.isBlackJack()) {
-            return judgePlayerByBlackJack(player, dealer);
+        if (player.isBlackJack() && !dealer.isBlackJack()) {
+            return BLACKJACK;
         }
-        if (player.isBust()) {
-            return Judgement.LOSE;
+        if (player.isBust() || (player.calculateScore() < dealer.calculateScore() && !dealer.isBust())) {
+            return LOSE;
         }
-        if (dealer.isBust()) {
-            return Judgement.WIN;
+        if (dealer.isBust() || player.calculateScore() > dealer.calculateScore()) {
+            return WIN;
         }
-        return judgePlayerByScore(player, dealer);
-    }
-
-    private static Judgement judgePlayerByBlackJack(Player player, Dealer dealer) {
-        if (dealer.isBlackJack() && player.isBlackJack()) {
-            return Judgement.DRAW;
-        }
-        if (dealer.isBlackJack()) {
-            return Judgement.LOSE;
-        }
-        return Judgement.BLACKJACK;
-    }
-
-    private static Judgement judgePlayerByScore(Player player, Dealer dealer) {
-        int dealerScore = dealer.calculateScore();
-        int playerScore = player.calculateScore();
-        if (dealerScore == playerScore) {
-            return Judgement.DRAW;
-        }
-        if (dealerScore > playerScore) {
-            return Judgement.LOSE;
-        }
-        return Judgement.WIN;
+        return DRAW;
     }
 
     public Profit calculateProfit(Money betMoney) {
-        return profitFunction.apply(betMoney);
+        return betMoney.calculateProfit(profitMultiple);
     }
 }
