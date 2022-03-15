9d0ad45cf97cfe917324ace12ae9d9eb689fdfc8
DOES <ldk980130@gmail.com>
2022-03-15 16:09:06 +0900

    refactor: 안쓰는 필드 삭제

--- src/main/java/blackjack/domain/result/BlackJackResult.java
+++ src/main/java/blackjack/domain/result/BlackJackResult.java
@@ -7,31 +7,29 @@ import blackjack.domain.card.Cards;
 
 public enum BlackJackResult {
 	
-	BLACKJACK_WIN("블랙잭", 1.5, (player, dealer) ->
+	BLACKJACK_WIN( 1.5, (player, dealer) ->
 		player.isBlackJack() && !dealer.isBlackJack()
 	),
-	WIN("승", 1, (player, dealer) ->
+	WIN( 1, (player, dealer) ->
 		(!player.isBust() && dealer.isBust()) ||
 			(!player.isBust() && !dealer.isBust() && player.isGreaterThan(dealer))
 	),
-	LOSE("패", -1, (player, dealer) ->
+	LOSE( -1, (player, dealer) ->
 		player.isBust() ||
 			(!player.isBlackJack() && dealer.isBlackJack()) ||
 			(!player.isBust() && !dealer.isBust() && dealer.isGreaterThan(player))
 	),
-	DRAW("무", 0, (player, dealer) ->
+	DRAW(0, (player, dealer) ->
 		(player.isBlackJack()) && dealer.isBlackJack() ||
 			(!player.isBlackJack() && !dealer.isBlackJack() && player.isSame(dealer))
 	);
 
 	private static final String NOT_EXIST_ERROR = "옯바른 결과를 찾을 수 없습니다.";
 
-	private final String name;
 	private final double profit;
 	private final BiPredicate<Cards, Cards> predicate;
 
-	BlackJackResult(String value, double profit, BiPredicate<Cards, Cards> predicate) {
-		this.name = value;
+	BlackJackResult(double profit, BiPredicate<Cards, Cards> predicate) {
 		this.profit = profit;
 		this.predicate = predicate;
 	}
@@ -49,10 +47,6 @@ public enum BlackJackResult {
 
 	public double getReverseEarning(double earning) {
 		return LOSE.getProfit() * earning;
-	}
-
-	public String getName() {
-		return name;
 	}
 
 	public double getProfit() {
