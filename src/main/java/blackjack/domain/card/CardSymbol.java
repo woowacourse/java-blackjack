ee595c25d7297cb803252abd3b7e8e462568f8d5
pup-paw <wldusdl0310@gmail.com>
2022-03-16 02:19:00 +0900

    style: 카드 심볼을 이름 대신 진짜 심볼로 변경

--- src/main/java/blackjack/domain/card/CardSymbol.java
+++ src/main/java/blackjack/domain/card/CardSymbol.java
@@ -1,10 +1,10 @@
 package blackjack.domain.card;
 
 public enum CardSymbol {
-	SPADE("스페이드"),
-	DIAMOND("다이아몬드"),
-	HEART("하트"),
-	CLOVER("클로버");
+	SPADE("♠️"),
+	DIAMOND("♦️"),
+	HEART("♥️"),
+	CLOVER("♣️");
 
 	private final String name;
 
