d86a377a7f2fed21cff73542644c5af6d286faf1
cjlee38 <cjlee38@github.com>
2022-03-15 16:15:47 +0900

    refactor(Game): 도메인의 entry point 역할을 맡는 game 추가

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -1,14 +1,6 @@
 package blackjack.controller;
 
-import java.util.List;
-
-import blackjack.domain.card.Deck;
-import blackjack.domain.strategy.DealerHitStrategy;
-import blackjack.domain.strategy.PlayerHitStrategy;
-import blackjack.domain.user.Dealer;
-import blackjack.domain.user.Player;
-import blackjack.domain.user.User;
-import blackjack.domain.user.Users;
+import blackjack.domain.BlackJackGame;
 import blackjack.dto.MatchRecordDto;
 import blackjack.dto.UserDto;
 import blackjack.dto.UsersDto;
@@ -26,58 +18,16 @@ public class BlackJackController {
     }
 
     public void run() {
-        Deck deck = new Deck();
-
-        Users users = inputPlayerNames();
-        drawInitialCards(deck, users);
-        hitOrStayCardPerUsers(deck, users);
-        showGameResult(users);
-    }
-
-    private Users inputPlayerNames() {
-        return Users.from(inputView.inputPlayerNames());
-    }
-
-    private void drawInitialCards(Deck deck, Users users) {
-        users.drawInitCards(deck);
-        outputView.printInitCards(UsersDto.fromInit(users));
-    }
-
-    private void hitOrStayCardPerUsers(Deck deck, Users users) {
-        hitOrStayCardPerPlayers(deck, users.getPlayers());
-        hitOrStayCardDealer(deck, users.getDealer());
-    }
-
-    private void hitOrStayCardPerPlayers(Deck deck, List<Player> players) {
-        for (Player player : players) {
-            hitOrStayCardPlayer(deck, player);
-        }
-    }
-
-    private void hitOrStayCardPlayer(Deck deck, User user) {
-        boolean isHit = user.hitOrStay(
-            deck, new PlayerHitStrategy(inputView.inputWhetherToDrawCard(UserDto.fromEvery(user)))
+        BlackJackGame game = BlackJackGame.fromPlayerNames(inputView.inputPlayerNames());
+        game.drawInitialCards(users -> outputView.printInitCards(UsersDto.fromInit(users)));
+        game.hitOrStayCardsPlayer(
+            user -> () -> inputView.inputWhetherToDrawCard(UserDto.fromEvery(user)),
+            user -> outputView.printUserCards(UserDto.fromEvery(user))
         );
-        if (isHit) {
-            outputView.printUserCards(UserDto.fromEvery(user));
-        }
-        if (isHit && !user.isBust()) {
-            hitOrStayCardPlayer(deck, user);
-        }
-    }
-
-    private void hitOrStayCardDealer(Deck deck, Dealer dealer) {
-        boolean isHit = dealer.hitOrStay(deck, new DealerHitStrategy(dealer.getScore()));
-        if (isHit) {
-            outputView.printDealerHit();
-        }
-        if (isHit && !dealer.isBust()) {
-            hitOrStayCardDealer(deck, dealer);
-        }
-    }
-
-    private void showGameResult(Users users) {
-        outputView.printAllUserCardsWithScore(UsersDto.fromEvery(users));
-        outputView.printMatchResult(MatchRecordDto.fromRecords(users.createPlayerMatchRecords()));
+        game.hitOrStayCardsDealer(user -> outputView.printDealerHit(UserDto.fromEvery(user)));
+        game.settleGame(
+            users -> outputView.printAllUserCardsWithScore(UsersDto.fromEvery(users)),
+            matchRecord -> outputView.printMatchResult(MatchRecordDto.fromRecords(matchRecord))
+        );
     }
 }
