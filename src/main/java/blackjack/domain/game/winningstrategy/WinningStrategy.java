5f62061bfa428681593bd337a51ff1571a612592
chawani <chawani@naver.com>
2022-03-15 15:03:10 +0900

    refactor: 사용하지 않는 코드 파일 삭제

--- src/main/java/blackjack/domain/game/winningstrategy/WinningStrategy.java
+++
@@ -1,9 +0,0 @@
-package blackjack.domain.game.winningstrategy;
-
-import blackjack.domain.game.WinningResult;
-import blackjack.domain.participant.Dealer;
-import blackjack.domain.participant.Player;
-
-public interface WinningStrategy {
-    WinningResult getResult(Dealer dealer, Player player);
-}
