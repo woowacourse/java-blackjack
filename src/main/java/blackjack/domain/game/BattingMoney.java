2521adb0eb2ab671c01a66868ce3fedae0a996c6
Chris <bjuuuu98@gmail.com>
2022-03-15 15:15:54 +0900

    feat: 빈 문자열로 배팅 금액 객체를 생성할 경우 예외 처리 기능 구현 및 테스트

--- src/main/java/blackjack/domain/game/BattingMoney.java
+++ src/main/java/blackjack/domain/game/BattingMoney.java
@@ -9,6 +9,7 @@ public class BattingMoney {
 
     public BattingMoney(final String value) {
         Objects.requireNonNull(value, "배팅 금액에는 null이 들어올 수 없습니다.");
+        validateBlank(value);
         validateNaturalNumber(value);
         this.value = Integer.parseInt(value);
     }
@@ -16,6 +17,12 @@ public class BattingMoney {
     private void validateNaturalNumber(final String value) {
         if (!NATURAL_NUMBER_PATTERN.matcher(value).matches()) {
             throw new IllegalArgumentException("배팅 금액은 자연수여야 합니다.");
+        }
+    }
+
+    private void validateBlank(final String value) {
+        if (value.isBlank()) {
+            throw new IllegalArgumentException("배팅 금액은 빈 값으로 만들 수 없습니다.");
         }
     }
 }
--- src/test/java/blackjack/domain/game/BattingMoneyTest.java
+++ src/test/java/blackjack/domain/game/BattingMoneyTest.java
@@ -11,7 +11,7 @@ class BattingMoneyTest {
     @ParameterizedTest
     @DisplayName("1 이상의 정수가 아닌 값으로 객체를 생성하려고 하면 예외를 발생시킨다.")
     @ValueSource(strings = {"0", "-1", "1.2", "?"})
-    void createExceptionByInvalidValue(String invalidValue) {
+    void createExceptionByNotNaturalNumberValue(String invalidValue) {
         assertThatThrownBy(() -> new BattingMoney(invalidValue))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("배팅 금액은 자연수여야 합니다.");
@@ -20,9 +20,18 @@ class BattingMoneyTest {
     @ParameterizedTest
     @DisplayName("null 값으로 배팅 금액 객체를 생성하려고 하면 예외를 발생시킨다.")
     @NullSource
-    void createExceptionBy(String nullValue) {
+    void createExceptionByNullValue(String nullValue) {
         assertThatThrownBy(() -> new BattingMoney(nullValue))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("배팅 금액에는 null이 들어올 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("빈 문자열으로 배팅 금액 객체를 생성하려고 하면 예외를 발생시킨다.")
+    @ValueSource(strings = {"", " "})
+    void createExceptionByBlankValue(String blankValue) {
+        assertThatThrownBy(() -> new BattingMoney(blankValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("배팅 금액은 빈 값으로 만들 수 없습니다.");
     }
 }
