31cc0d59a85adc3e510481aa331a7bce2bee468c
Hongdonggeon <ghd700@naver.com>
2022-03-16 02:56:31 +0900

    test: CardsTest 테스트 코드 @MethodSource 적용

--- src/test/java/domain/card/CardsTest.java
+++ src/test/java/domain/card/CardsTest.java
@@ -2,37 +2,37 @@ package domain.card;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CardsTest {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("provideCardAndScore")
     @DisplayName("List에 들어있는 Card의 총합을 계산하여 확인한다.")
-    void CalculateSumTest() {
-        Cards cards = new Cards(List.of(new Card(Symbol.SPADE, Denomination.EIGHT),
-            new Card(Symbol.SPADE, Denomination.NINE), new Card(Symbol.SPADE, Denomination.QUEEN)));
+    void CalculateSumTest(List<Card> card, int score) {
+        Cards cards = new Cards(card);
 
-        assertThat(cards.calculateSum()).isEqualTo(27);
+        assertThat(cards.calculateScore()).isEqualTo(score);
     }
 
-    @Test
-    @DisplayName("List에 Ace가 들어있을 때, 가장 유리한 값으로 계산한다. ACE만 있는 경우")
-    void calculateResultTest() {
-        Cards cards = new Cards(List.of(new Card(Symbol.SPADE, Denomination.ACE),
-            new Card(Symbol.CLOVER, Denomination.ACE), new Card(Symbol.DIAMOND, Denomination.ACE)));
-
-        assertThat(cards.calculateScore()).isEqualTo(13);
-    }
-
-    @Test
-    @DisplayName("List에 Ace가 들어있을 때, 가장 유리한 값으로 계산한다. ACE 이외의 다른 점수가 포함된 경우")
-    void calculateResultTest2() {
-        Cards cards = new Cards(List.of(new Card(Symbol.SPADE, Denomination.FIVE),
-            new Card(Symbol.CLOVER, Denomination.ACE), new Card(Symbol.DIAMOND, Denomination.ACE),
-            new Card(Symbol.HEART, Denomination.ACE)));
-
-        assertThat(cards.calculateScore()).isEqualTo(18);
+    private static Stream<Arguments> provideCardAndScore() {
+        return Stream.of(
+            Arguments.of(List.of(new Card(Symbol.SPADE, Denomination.EIGHT),
+                new Card(Symbol.SPADE, Denomination.NINE),
+                new Card(Symbol.SPADE, Denomination.QUEEN)), 27),
+            Arguments.of(List.of(new Card(Symbol.SPADE, Denomination.ACE),
+                new Card(Symbol.CLOVER, Denomination.ACE),
+                new Card(Symbol.DIAMOND, Denomination.ACE)), 13),
+            Arguments.of(List.of(new Card(Symbol.SPADE, Denomination.FIVE),
+                new Card(Symbol.CLOVER, Denomination.ACE), new Card(Symbol.DIAMOND, Denomination.ACE),
+                new Card(Symbol.HEART, Denomination.ACE)), 18)
+        );
     }
 }
