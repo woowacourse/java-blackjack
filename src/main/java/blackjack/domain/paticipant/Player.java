79be2d385ea0597d4d7840fce85226fa790a3b99
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-16 03:45:38 +0900

    feat: 플레이어의 이름이 공백인 경우 예외처리 및 테스트

--- src/main/java/blackjack/domain/paticipant/Player.java
+++ src/main/java/blackjack/domain/paticipant/Player.java
@@ -8,6 +8,13 @@ public class Player {
 
     public Player(final String name) {
         Objects.requireNonNull(name, "이름은 null이 들어올 수 없습니다.");
+        checkEmptyName(name);
         this.name = name;
+    }
+
+    private void checkEmptyName(final String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 공백이 들어올 수 없습니다.");
+        }
     }
 }
--- src/test/java/blackjack/domain/paticipant/PlayerTest.java
+++ src/test/java/blackjack/domain/paticipant/PlayerTest.java
@@ -3,6 +3,8 @@ package blackjack.domain.paticipant;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
 
 class PlayerTest {
 
@@ -11,5 +13,13 @@ class PlayerTest {
         assertThatThrownBy(() -> new Player(null))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("이름은 null이 들어올 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    void 플레이어의_이름이_공백인_경우_예외발생(final String name) {
+        assertThatThrownBy(() -> new Player(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 공백이 들어올 수 없습니다.");
     }
 }
