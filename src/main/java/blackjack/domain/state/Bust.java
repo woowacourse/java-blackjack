4b5d584d7e10cc8c5958e4f5a1b16b9de9ea4dbc
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-16 01:34:30 +0900

    feat: Bust Finished 상속

--- src/main/java/blackjack/domain/state/Bust.java
+++ src/main/java/blackjack/domain/state/Bust.java
@@ -1,39 +1,10 @@
 package blackjack.domain.state;
 
-import blackjack.domain.card.Card;
 import blackjack.domain.card.Cards;
-import java.util.List;
 
-public class Bust implements BlackjackGameState {
-
-    private final Cards cards;
+public class Bust extends Finished {
 
     public Bust(final Cards cards) {
-        this.cards = cards;
-    }
-
-    @Override
-    public BlackjackGameState hit(final Card card) {
-        return null;
-    }
-
-    @Override
-    public BlackjackGameState stand() {
-        return null;
-    }
-
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
-
-    @Override
-    public List<Card> cards() {
-        return null;
-    }
-
-    @Override
-    public int score() {
-        return 0;
+        super(cards);
     }
 }
