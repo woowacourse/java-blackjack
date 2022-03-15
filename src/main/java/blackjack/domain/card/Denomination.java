74f490eb0bd327d44d109c0e297d02d9900cc381
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-16 03:55:31 +0900

    feat: 카드 숫자 enum 추가

---
+++ src/main/java/blackjack/domain/card/Denomination.java
@@ -0,0 +1,27 @@
+package blackjack.domain.card;
+
+public enum Denomination {
+
+    A(1, "A"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    JACK(10, "J"),
+    QUEEN(10, "Q"),
+    KING(10, "K"),
+    ;
+
+    private final int defaultValue;
+    private final String printValue;
+
+    Denomination(final int defaultValue, final String printValue) {
+        this.defaultValue = defaultValue;
+        this.printValue = printValue;
+    }
+}
