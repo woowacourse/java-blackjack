5b8908cb01a6a85acfeda2baa4c6b9e87a48efd8
soominsohn <soominsohn@gmail.com>
2022-03-15 15:29:46 +0900

    refacotr(Deck): change deck data structure from stack to deque

--- src/main/java/blackjack/domain/Deck.java
+++ src/main/java/blackjack/domain/Deck.java
@@ -1,22 +1,20 @@
 package blackjack.domain;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Stack;
+import java.util.Deque;
+import java.util.List;
 import java.util.stream.Stream;
-
 
 public class Deck {
 
-    private final static String RUN_OUT_OF_CARDS_ERROR_MESSAGE = "[ERROR] 카드를 모두 사용하였습니다.";
+    private static final String RUN_OUT_OF_CARDS_ERROR_MESSAGE = "[ERROR] 카드를 모두 사용하였습니다.";
 
-    private final Stack<Card> cards = new Stack<>();
+    private final Deque<Card> cards;
 
     public Deck() {
-        for (Suit suit : Suit.values()) {
-            Stream.of(Denomination.values())
-                .forEach(denomination -> cards.push(new Card(suit, denomination)));
-        }
-        Collections.shuffle(cards);
+        this.cards = new ArrayDeque<>(generateAndShuffleCards());
     }
 
     public Card pickCard() {
@@ -24,5 +22,17 @@ public class Deck {
             throw new IllegalStateException(RUN_OUT_OF_CARDS_ERROR_MESSAGE);
         }
         return cards.pop();
+    }
+
+    private List<Card> generateAndShuffleCards() {
+        List<Card> cardsPool = new ArrayList<>();
+
+        for (Suit suit : Suit.values()) {
+            Stream.of(Denomination.values())
+                .forEach(denomination -> cardsPool.add(new Card(suit, denomination)));
+        }
+        Collections.shuffle(cardsPool);
+
+        return cardsPool;
     }
 }
