023e6ca9d64fccc469371977d9bb2eb803cfc07e
bcc0830 <bcc0830@naver.com>
2022-03-15 23:10:04 +0900

    refactor : Name의 equals와 hashCode 재정의

--- src/main/java/blackjack/domain/Name.java
+++ src/main/java/blackjack/domain/Name.java
@@ -1,6 +1,6 @@
 package blackjack.domain;
 
-import java.util.regex.Pattern;
+import java.util.Objects;
 
 public class Name {
 	private static final String BLANK_NAME = "[ERROR] 이름이 빈 입력이면 안됩니다.";
@@ -19,5 +19,22 @@ public class Name {
 
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Name name1 = (Name) o;
+		return Objects.equals(name, name1.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
 	}
 }
