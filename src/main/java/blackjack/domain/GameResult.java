95d3c2bd74f518496c25b28a8d14a46760dab331
Ohzzi <clover7even@naver.com>
2022-03-15 22:14:32 +0900

    feat: GameResult를 승무패 저장에서 수익 저장으로 변경

--- src/main/java/blackjack/domain/GameResult.java
+++ src/main/java/blackjack/domain/GameResult.java
@@ -3,43 +3,37 @@ package blackjack.domain;
 import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Player;
 import java.util.Collections;
-import java.util.EnumMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class GameResult {
 
-    private final Map<Outcome, Integer> dealerResult;
-    private final Map<String, Outcome> playersResult;
+    private final Map<String, Integer> earnings;
 
-    private GameResult(Map<Outcome, Integer> dealerResult, Map<String, Outcome> playersResult) {
-        this.dealerResult = Collections.unmodifiableMap(new EnumMap<>(dealerResult));
-        this.playersResult = Collections.unmodifiableMap(new LinkedHashMap<>(playersResult));
+    public GameResult(Map<String, Integer> earnings) {
+        this.earnings = Collections.unmodifiableMap(new LinkedHashMap<>(earnings));
     }
 
     public static GameResult of(Dealer dealer, List<Player> players) {
-        Map<Outcome, Integer> dealerResult = new EnumMap<>(Outcome.class);
-        Map<String, Outcome> playersResult = new LinkedHashMap<>();
+        Map<String, Integer> playersEarnings = new LinkedHashMap<>();
 
         for (Player player : players) {
             Outcome playerOutcome = Outcome.judge(player, dealer);
-            playersResult.put(player.getName(), playerOutcome);
-            putDealerResult(dealerResult, playerOutcome);
+            int playerEarnings = calculateEarnings(player, playerOutcome);
+            int dealerEarnings = playerEarnings * (-1);
+            playersEarnings.merge(dealer.getName(), dealerEarnings, Integer::sum);
+            playersEarnings.put(player.getName(), playerEarnings);
         }
 
-        return new GameResult(dealerResult, playersResult);
+        return new GameResult(playersEarnings);
     }
 
-    private static void putDealerResult(Map<Outcome, Integer> dealerResult, Outcome playerOutcome) {
-        dealerResult.merge(playerOutcome.getOpposite(), 1, Integer::sum);
+    private static int calculateEarnings(Player player, Outcome outcome) {
+        return (int) (player.getBetting().getValue() * outcome.getEarningsRate());
     }
 
-    public Map<Outcome, Integer> getDealerResult() {
-        return dealerResult;
-    }
-
-    public Map<String, Outcome> getPlayersResult() {
-        return playersResult;
+    public Map<String, Integer> getEarnings() {
+        return earnings;
     }
 }
--- src/test/java/blackjack/domain/GameResultTest.java
+++ src/test/java/blackjack/domain/GameResultTest.java
@@ -1,9 +1,7 @@
 package blackjack.domain;
 
-import static blackjack.domain.Outcome.DRAW;
-import static blackjack.domain.Outcome.LOSE;
-import static blackjack.domain.Outcome.WIN;
 import static blackjack.domain.card.Denomination.ACE;
+import static blackjack.domain.card.Denomination.EIGHT;
 import static blackjack.domain.card.Denomination.NINE;
 import static blackjack.domain.card.Denomination.TEN;
 import static blackjack.domain.card.Pattern.CLOVER;
@@ -16,7 +14,6 @@ import blackjack.domain.card.Denomination;
 import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Name;
 import blackjack.domain.participant.Player;
-import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
@@ -25,29 +22,29 @@ import org.junit.jupiter.api.Test;
 public class GameResultTest {
 
     @Test
-    @DisplayName("승패 결과를 Map에 저장한다")
+    @DisplayName("수익을 Map에 저장한다")
     void saveIntoMap() {
-        // given
-        Dealer dealer = createDealer(TEN);
+// given
+        Dealer dealer = createDealer(NINE);
+        Betting betting = new Betting(1000);
 
-        Player winPlayer = createPlayer("win", ACE);
-        Player drawPlayer = createPlayer("draw", TEN);
-        Player losePlayer = createPlayer("lose", NINE);
-        List<Player> players = List.of(winPlayer, drawPlayer, losePlayer);
-
-        Map<Outcome, Integer> outcomeMap = createOutcomeMap(1, 1, 1);
+        Player blackJackPlayer = createPlayer("blackjack", ACE, betting);
+        Player winPlayer = createPlayer("win", TEN, betting);
+        Player drawPlayer = createPlayer("draw", NINE, betting);
+        Player losePlayer = createPlayer("lose", EIGHT, betting);
+        List<Player> players = List.of(blackJackPlayer, winPlayer, drawPlayer, losePlayer);
 
         // when
         GameResult gameResult = GameResult.of(dealer, players);
-        Map<Outcome, Integer> dealerResult = gameResult.getDealerResult();
-        Map<String, Outcome> playersResult = gameResult.getPlayersResult();
+        Map<String, Integer> earnings = gameResult.getEarnings();
 
         // then
         assertAll(
-                () -> assertThat(dealerResult).isEqualTo(outcomeMap),
-                () -> assertThat(playersResult.get(winPlayer.getName())).isEqualTo(WIN),
-                () -> assertThat(playersResult.get(drawPlayer.getName())).isEqualTo(DRAW),
-                () -> assertThat(playersResult.get(losePlayer.getName())).isEqualTo(LOSE)
+                () -> assertThat(earnings.get("딜러")).isEqualTo(-1500),
+                () -> assertThat(earnings.get(blackJackPlayer.getName())).isEqualTo(1500),
+                () -> assertThat(earnings.get(winPlayer.getName())).isEqualTo(1000),
+                () -> assertThat(earnings.get(drawPlayer.getName())).isEqualTo(0),
+                () -> assertThat(earnings.get(losePlayer.getName())).isEqualTo(-1000)
         );
     }
 
@@ -58,19 +55,10 @@ public class GameResultTest {
         return new Dealer(dealerCards);
     }
 
-    private static Player createPlayer(String name, Denomination denomination2) {
+    private static Player createPlayer(String name, Denomination denomination2, Betting betting) {
         Card card1 = new Card(DIAMOND, TEN);
         Card card2 = new Card(CLOVER, denomination2);
         List<Card> playerCards = List.of(card1, card2);
-        return new Player(new Name(name), playerCards);
-    }
-
-    private static Map<Outcome, Integer> createOutcomeMap(int win, int draw, int lose) {
-        Map<Outcome, Integer> judgementMap = new EnumMap<>(Outcome.class);
-        judgementMap.put(WIN, win);
-        judgementMap.put(DRAW, draw);
-        judgementMap.put(LOSE, lose);
-
-        return judgementMap;
+        return new Player(new Name(name), playerCards, betting);
     }
 }
