50fc6e6972aa5000dd3ac3ccc2ce26881a464fc9
yeon-06 <rnjstldus2@hs.ac.kr>
2022-03-15 09:46:53 +0900

    refactor: 참가자의 인원 제한

--- src/main/java/blackjack/domain/participant/Participants.java
+++ src/main/java/blackjack/domain/participant/Participants.java
@@ -1,16 +1,26 @@
 package blackjack.domain.participant;
 
 import blackjack.domain.card.Card;
+import java.util.Collections;
 import java.util.List;
 
 public class Participants {
+    private static final int MAX_PLAYER_COUNT = 8;
+    private static final String ERROR_OVER_PLAYER_COUNT = String.format("게임에 참여하는 최대 인원은 %d명 입니다.", MAX_PLAYER_COUNT);
 
     private final List<Player> players;
     private final Dealer dealer;
 
-    public Participants(List<Player> players, Dealer dealer) {
-        this.players = players;
+    public Participants(final List<Player> players, final Dealer dealer) {
+        this.players = Collections.unmodifiableList(players);
         this.dealer = dealer;
+        validatePlayers(players);
+    }
+
+    private void validatePlayers(List<Player> players) {
+        if (players.size() > MAX_PLAYER_COUNT) {
+            throw new IllegalArgumentException(ERROR_OVER_PLAYER_COUNT);
+        }
     }
 
     public void drawCard(Participant participant, Card card) {
