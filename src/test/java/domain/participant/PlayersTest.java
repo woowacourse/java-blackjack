1c93c6b38ebf8f74712b30ac3ab6120194324982
RunaNam <nmj5012@naver.com>
2022-03-15 17:01:53 +0900

    refactor(playerTest): BeforeEach 추가

--- src/test/java/domain/participant/PlayersTest.java
+++ src/test/java/domain/participant/PlayersTest.java
@@ -4,11 +4,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.card.Deck;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utils.ExceptionMessages;
 
 class PlayersTest {
+
+    private Players players;
+    private Deck deck;
+
+    @BeforeEach
+    void init() {
+        players = Players.of("runa, kun");
+        deck = Deck.getInstance();
+    }
 
     @Test
     @DisplayName("입력 받은 플레이어 이름만큼 Player 객체가 잘 만들어졌는지 확인한다.")
@@ -24,19 +34,16 @@ class PlayersTest {
     void playersCommaErrorTest() {
         String names = "pobi,,,jason";
 
-        assertThatThrownBy(()-> Players.of(names))
+        assertThatThrownBy(() -> Players.of(names))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage(ExceptionMessages.EMPTY_NAME_ERROR);
     }
 
     @Test
     @DisplayName("첫 턴에서 모든 참가자가 카드를 두개씩 뽑는지 확인")
-    void initialTurnTest(){
-        Players players = Players.of("runa, kun");
-        Deck deck = Deck.getInstance();
-
+    void initialTurnTest() {
         players.runInitialTurn(deck);
-        int actual = (int)players.getPlayers().stream()
+        int actual = (int) players.getPlayers().stream()
             .filter(participant -> participant.getCards().size() == 2)
             .count();
 
