952d893ebefe8e9e226daf7cdad2c6b89d1dfaeb
YJGwon <yj970125@gmail.com>
2022-03-15 15:21:34 +0900

    test: 딜러의 승패 판정에 대한 테스트 추가

--- src/test/java/blackjack/model/player/DealerTest.java
+++ src/test/java/blackjack/model/player/DealerTest.java
@@ -11,11 +11,16 @@ import org.junit.jupiter.api.Test;
 
 public class DealerTest {
     private Dealer dealer;
+    private Entry entry;
 
     @BeforeEach
     void initializeDealer() {
         dealer = new Dealer();
         dealer.addCard(new TrumpCard(TrumpNumber.NINE, TrumpSymbol.CLOVER));
+
+        entry = new Entry("포키");
+        entry.addCard(new TrumpCard(TrumpNumber.TEN, TrumpSymbol.DIAMOND));
+        entry.addCard(new TrumpCard(TrumpNumber.TWO, TrumpSymbol.HEART));
     }
 
     @DisplayName("딜러의 점수가 16점이면 딜러는 hit할 수 있다")
@@ -32,5 +37,43 @@ public class DealerTest {
         dealer.addCard(new TrumpCard(TrumpNumber.EIGHT, TrumpSymbol.CLOVER));
 
         assertThat(dealer.canHit()).isFalse();
+    }
+
+    @DisplayName("참가자가 bust이면 무조건 딜러가 승리한다")
+    @Test
+    void compareWith_entry_bust() {
+        dealer.addCard(new TrumpCard(TrumpNumber.TEN, TrumpSymbol.CLOVER));
+        dealer.addCard(new TrumpCard(TrumpNumber.THREE, TrumpSymbol.HEART));
+        entry.addCard(new TrumpCard(TrumpNumber.TEN, TrumpSymbol.SPADE));
+
+        assertThat(dealer.compareWith(entry).isWin()).isFalse();
+    }
+
+    @DisplayName("딜러만 bust이면 참가자가 승리한다")
+    @Test
+    void compareWith_dealer_bust() {
+        dealer.addCard(new TrumpCard(TrumpNumber.TEN, TrumpSymbol.CLOVER));
+        dealer.addCard(new TrumpCard(TrumpNumber.THREE, TrumpSymbol.HEART));
+        entry.addCard(new TrumpCard(TrumpNumber.ACE, TrumpSymbol.SPADE));
+
+        assertThat(dealer.compareWith(entry).isWin()).isTrue();
+    }
+
+    @DisplayName("둘다 bust가 아니고 동점이면 참가자가 승리한다")
+    @Test
+    void compareWith_same_score() {
+        dealer.addCard(new TrumpCard(TrumpNumber.TEN, TrumpSymbol.CLOVER));
+        entry.addCard(new TrumpCard(TrumpNumber.SEVEN, TrumpSymbol.SPADE));
+
+        assertThat(dealer.compareWith(entry).isWin()).isTrue();
+    }
+
+    @DisplayName("딜러가 19, 엔트리가 18이면 딜러가 승리한다")
+    @Test
+    void compareWith_dealer_19_entry_20() {
+        dealer.addCard(new TrumpCard(TrumpNumber.TEN, TrumpSymbol.CLOVER));
+        entry.addCard(new TrumpCard(TrumpNumber.SIX, TrumpSymbol.SPADE));
+
+        assertThat(dealer.compareWith(entry).isWin()).isFalse();
     }
 }
