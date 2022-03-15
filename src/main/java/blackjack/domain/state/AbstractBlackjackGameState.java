5e6ecf1ca11e00408b9fce22e4af9ea1b367e1c8
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-16 00:45:31 +0900

    feat: AbstractBlackjackGameState 공통 부분 통합

--- src/main/java/blackjack/domain/state/AbstractBlackjackGameState.java
+++ src/main/java/blackjack/domain/state/AbstractBlackjackGameState.java
@@ -1,12 +1,21 @@
 package blackjack.domain.state;
 
+import blackjack.domain.card.Card;
 import blackjack.domain.card.Cards;
+import java.util.List;
+import java.util.Objects;
 
 public abstract class AbstractBlackjackGameState implements BlackjackGameState {
 
-    private final Cards cards;
+    final Cards cards;
 
     public AbstractBlackjackGameState(final Cards cards) {
+        Objects.requireNonNull(cards, "cards는 null이 들어올 수 없습니다.");
         this.cards = cards;
+    }
+
+    @Override
+    public List<Card> cards() {
+        return cards.cards();
     }
 }
--- src/main/java/blackjack/domain/state/Running.java
+++ src/main/java/blackjack/domain/state/Running.java
@@ -2,16 +2,11 @@ package blackjack.domain.state;
 
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Cards;
-import java.util.List;
-import java.util.Objects;
 
-public class Running implements BlackjackGameState {
-
-    private final Cards cards;
+public class Running extends AbstractBlackjackGameState {
 
     public Running(final Cards cards) {
-        Objects.requireNonNull(cards, "cards는 null이 들어올 수 없습니다.");
-        this.cards = cards;
+        super(cards);
     }
 
     @Override
@@ -31,11 +26,6 @@ public class Running implements BlackjackGameState {
     @Override
     public boolean isFinished() {
         return false;
-    }
-
-    @Override
-    public List<Card> cards() {
-        return null;
     }
 
     @Override
