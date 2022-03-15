3e4dfc3df727d35a2fd6d38fe6532899539e4362
nailseong <nnis9857@gmail.com>
2022-03-15 19:52:50 +0900

    refactor: Service 삭제

--- src/main/java/blackjack/controller/GameController.java
+++ src/main/java/blackjack/controller/GameController.java
@@ -1,61 +1,92 @@
 package blackjack.controller;
 
+import blackjack.domain.Record;
 import blackjack.domain.card.Deck;
 import blackjack.domain.card.RandomGeneratingStrategy;
+import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Player;
+import blackjack.domain.participant.Players;
+import blackjack.dto.CardDto;
 import blackjack.dto.DealerRecordDto;
 import blackjack.dto.DealerTurnResultDto;
 import blackjack.dto.ParticipantDto;
 import blackjack.dto.ParticipantResultDto;
 import blackjack.dto.PlayerRecordDto;
-import blackjack.service.DealerService;
-import blackjack.service.PlayerService;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
 import blackjack.view.PlayerAnswer;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameController {
 
-    private final PlayerService playerService;
-    private final DealerService dealerService;
+    private final Deck deck;
+    private final Players players;
+    private final Dealer dealer;
 
     public GameController() {
-        final Deck deck = Deck.from(new RandomGeneratingStrategy());
-        this.playerService = initPlayerService(deck);
-        this.dealerService = new DealerService(deck);
+        this.deck = Deck.from(new RandomGeneratingStrategy());
+        this.players = initPlayers();
+        this.dealer = new Dealer();
     }
 
-    private PlayerService initPlayerService(final Deck deck) {
+    private Players initPlayers() {
         try {
-            return new PlayerService(deck, InputView.getPlayerNames());
+            return new Players(InputView.getPlayerNames());
         } catch (IllegalArgumentException e) {
             OutputView.printError(e.getMessage());
-            return initPlayerService(deck);
+            return initPlayers();
         }
     }
 
     public void initParticipants() {
-        dealerService.initCards();
-        playerService.initCards();
+        dealer.initCards(deck);
+        players.initCards(deck);
 
-        OutputView.printAssignmentResult(playerService.findAllNames());
-        OutputView.printDealerFirstCard(dealerService.findFirstCard());
-        playerService.findAllPlayers().forEach(OutputView::printCards);
+        final List<String> allNames = players.getNames();
+        final CardDto firstCard = CardDto.from(dealer.openFirstCard());
+
+        OutputView.printAssignmentResult(allNames);
+        OutputView.printDealerFirstCard(firstCard);
+
+        final List<ParticipantDto> allPlayers = players.getValue().stream()
+                .map(ParticipantDto::from)
+                .collect(Collectors.toList());
+        for (ParticipantDto allPlayer : allPlayers) {
+            OutputView.printCards(allPlayer);
+        }
     }
 
     public void progressPlayerTurns() {
-        while (playerService.isPlayerTurn()) {
-            final String playerName = playerService.findDrawablePlayerName();
+        while (players.isDrawablePlayerExist()) {
+            final String playerName = players.findHitPlayer().getName();
             final PlayerAnswer playerAnswer = InputView.getHitOrStay(playerName);
 
-            final ParticipantDto dto = playerService.progressTurn(playerName, playerAnswer);
+            final ParticipantDto dto = progressPlayerTurn(playerName, playerAnswer);
             OutputView.printCards(dto);
         }
     }
 
+    private ParticipantDto progressPlayerTurn(final String playerName, final PlayerAnswer playerAnswer) {
+        final Player player = players.findByName(playerName);
+        if (playerAnswer.isDraw()) {
+            player.hit(deck);
+        }
+        if (!playerAnswer.isDraw()) {
+            player.stay();
+        }
+
+        return ParticipantDto.from(player);
+    }
+
     public void progressDealerTurn() {
-        final DealerTurnResultDto dto = dealerService.progressTurn();
-        OutputView.printDealerTurnResult(dto);
+        int count = 0;
+        while (dealer.canDrawCard()) {
+            dealer.hit(deck);
+            count++;
+        }
+
+        OutputView.printDealerTurnResult(new DealerTurnResultDto(count));
     }
 
     public void endGame() {
@@ -64,15 +95,20 @@ public class GameController {
     }
 
     private void printAllCards() {
-        final List<ParticipantResultDto> dto = playerService.findAllResult();
-        dto.add(0, dealerService.getResult());
+        final List<ParticipantResultDto> dto = players.getValue().stream()
+                .map(ParticipantResultDto::from)
+                .collect(Collectors.toList());
+        dto.add(0, ParticipantResultDto.from(dealer));
 
         OutputView.breakLine();
         dto.forEach(OutputView::printCardsAndScore);
     }
 
     private void printAllRecords() {
-        final List<PlayerRecordDto> playerRecordDtos = playerService.getRecord(dealerService.getScore());
+        final int dealerScore = dealer.getScore();
+        final List<PlayerRecordDto> playerRecordDtos = players.getValue().stream()
+                .map(player -> PlayerRecordDto.of(player.getName(), Record.of(dealerScore, player.getScore())))
+                .collect(Collectors.toList());
         final DealerRecordDto dealerRecordDto = DealerRecordDto.from(playerRecordDtos);
 
         OutputView.printDealerRecord(dealerRecordDto);
