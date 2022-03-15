ad0a79db225774f00c26981c64813429cab3881b
jayjaehunchoi <wogns0108@naver.com>
2022-03-15 22:01:18 +0900

    refactor : BlackJack, Bust 체크 로직 메서드 분리

--- src/main/java/blackjack/domain/result/BlackJackResult.java
+++ src/main/java/blackjack/domain/result/BlackJackResult.java
@@ -8,24 +8,32 @@ import java.util.function.Predicate;
 
 public enum BlackJackResult {
 
-    BLACK_JACK(new BlackJack(), player -> player.showCards().size() == Card.START_CARD_COUNT &&
-            player.calculateResult() == Gamer.LIMIT_GAMER_TOTAL_POINT),
-    BUST(new Lose(), player -> player.calculateResult() > Gamer.LIMIT_GAMER_TOTAL_POINT),
+    BLACK_JACK(new BlackJack(), player -> isBlackJack(player)),
+    BUST(new Lose(), player -> isBust(player)),
     HIT(new Keep(), Player::isSatisfyReceiveCondition);
 
     private final Result result;
+
     private final Predicate<Player> predicate;
 
     BlackJackResult(final Result result, final Predicate<Player> predicate) {
         this.result = result;
         this.predicate = predicate;
     }
-
     public static BlackJackResult findBlackJackResult(final Player player) {
         return Arrays.stream(values())
                 .filter(value -> value.predicate.test(player))
                 .findFirst()
                 .orElse(HIT);
+    }
+
+    private static boolean isBlackJack(Player player) {
+        return player.showCards().size() == Card.START_CARD_COUNT &&
+                player.calculateResult() == Gamer.LIMIT_GAMER_TOTAL_POINT;
+    }
+
+    private static boolean isBust(Player player) {
+        return player.calculateResult() > Gamer.LIMIT_GAMER_TOTAL_POINT;
     }
 
     public Result getResult() {
