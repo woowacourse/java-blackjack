e7faf180264ba14001817a8ae7f4ceb47d2e6dc0
유영우 <fbduddn97@naver.com>
2022-03-15 15:37:42 +0900

    refactor: RandomNumberGenerator 변수 final 처리

--- src/main/java/blackjack/domain/strategy/RandomNumberGenerator.java
+++ src/main/java/blackjack/domain/strategy/RandomNumberGenerator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import blackjack.domain.machine.Card;
 
 public class RandomNumberGenerator implements NumberGenerator {
-    private static NumberGenerator numberGenerator = new RandomNumberGenerator();
+    private static final NumberGenerator numberGenerator = new RandomNumberGenerator();
 
     public static NumberGenerator getInstance() {
         return numberGenerator;
