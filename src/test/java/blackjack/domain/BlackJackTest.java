966697db73dcbc829860a196c43b48b71d426727
Donggyu <a29661498@gmail.com>
2022-03-15 17:26:44 +0900

    fix: `BlackJackTest` 수정
    BlackJackTest `from` 메소드 수정하였습니다.

--- src/test/java/blackjack/domain/BlackJackTest.java
+++ src/test/java/blackjack/domain/BlackJackTest.java
@@ -4,19 +4,26 @@ import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import blackjack.domain.strategy.ShuffledDeckGenerateStrategy;
+import blackjack.domain.user.BettingMoney;
 import blackjack.domain.user.User;
 import blackjack.domain.user.Users;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BlackJackTest {
 
+    private static final int MINIMUM_BETTING_AMOUNT = 1;
+
     @DisplayName("플레이어 초기 카드 세팅 테스트")
     @Test
     public void setInitCardsPerPlayer() {
         //given
-        BlackJack blackJack = BlackJack.from(List.of("pobi", "jason"), new ShuffledDeckGenerateStrategy());
+        Map<String, BettingMoney> playerInfo = createPlayerInfo(List.of("pobi", "jason"));
+
+        BlackJack blackJack = BlackJack.from(playerInfo, new ShuffledDeckGenerateStrategy());
 
         //when
         blackJack.setInitCardsPerPlayer();
@@ -35,5 +42,14 @@ class BlackJackTest {
         assertThat(counts.get(0)).isEqualTo(2);
         assertThat(counts.get(1)).isEqualTo(2);
         assertThat(size).isEqualTo(2);
+    }
+
+    private Map<String, BettingMoney> createPlayerInfo(List<String> names) {
+        Map<String, BettingMoney> playerInfo = new HashMap<>();
+
+        names.stream()
+                .forEach(name -> playerInfo.put(name, new BettingMoney(MINIMUM_BETTING_AMOUNT)));
+
+        return playerInfo;
     }
 }
