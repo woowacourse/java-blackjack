ebe3b00100e1164e9d80a51766bee454a1fb59b3
nailseong <nnis9857@gmail.com>
2022-03-15 20:44:24 +0900

    test(Deck): 테스트 개선

--- src/test/java/blackjack/domain/card/DeckTest.java
+++ src/test/java/blackjack/domain/card/DeckTest.java
@@ -2,27 +2,45 @@ package blackjack.domain.card;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Stack;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DeckTest {
 
-    @Test
-    @DisplayName("카드를 한 장 만들어서 반환한다.")
-    void createCard() {
-        // give
-        final Card card = Card.of(CardSymbol.DIAMOND, CardNumber.ACE);
-        final Deck deck = Deck.from(() -> {
-            final Stack<Card> cards = new Stack<>();
-            cards.push(card);
-            return cards;
-        });
+    private static Deck deck;
+    private static Card card;
 
+    @BeforeAll
+    static void initDeck() {
+        card = Card.of(CardSymbol.DIAMOND, CardNumber.ACE);
+        deck = Deck.from(() -> List.of(card));
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("한 장 뽑는다.")
+    void drawCard() {
         // when
         final Card actual = deck.drawCard();
 
         // then
         assertThat(actual).isEqualTo(card);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("덱이 비어있으면 Null을 반환한다.")
+    void emptyDeck() {
+        // when
+        final Card actual = deck.drawCard();
+
+        // then
+        assertThat(actual).isNull();
     }
 }
