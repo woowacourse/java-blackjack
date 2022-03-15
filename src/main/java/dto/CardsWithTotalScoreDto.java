6e1b61ce51e3fa55ae31d30b36f7883dec746b9d
yoon <duddbs774@gmail.com>
2022-03-15 17:52:41 +0900

    refactor: dto 생성자 파라미터 변경

--- src/main/java/dto/CardsWithTotalScoreDto.java
+++ src/main/java/dto/CardsWithTotalScoreDto.java
@@ -1,23 +1,38 @@
 package dto;
 
 import domain.card.Card;
+import domain.participant.Dealer;
+import domain.participant.Player;
+import domain.participant.Players;
 
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public final class CardsWithTotalScoreDto {
-    private final Map<NameWithCardsDto, Integer> cardsWithTotalScore = new LinkedHashMap<>();
+    private final Map<String, List<Card>> nameWithCards;
+    private final int totalScore;
 
-    public CardsWithTotalScoreDto(final Map<String, List<Card>> cardsWithName, final Map<String, Integer> totalScoreWithName) {
-        for (final Entry<String, List<Card>> entry : cardsWithName.entrySet()) {
-            final NameWithCardsDto nameWithCards = new NameWithCardsDto(Map.of(entry.getKey(), entry.getValue()));
-            cardsWithTotalScore.put(nameWithCards, totalScoreWithName.get(entry.getKey()));
-        }
+    public CardsWithTotalScoreDto(final Map<String, List<Card>> nameWithCards, final int totalScore) {
+        this.nameWithCards = nameWithCards;
+        this.totalScore = totalScore;
     }
 
-    public Map<NameWithCardsDto, Integer> get() {
-        return new LinkedHashMap<>(cardsWithTotalScore);
+    public static List<CardsWithTotalScoreDto> generateDtos(final Dealer dealer, final Players players) {
+        final List<CardsWithTotalScoreDto> dtos = new ArrayList<>();
+        dtos.add(new CardsWithTotalScoreDto(dealer.getNameWithCards(), dealer.getTotalScore()));
+        for (final Player player : players.getPlayers()) {
+            dtos.add(new CardsWithTotalScoreDto(player.getNameWithCards(), player.getTotalScore()));
+        }
+        return dtos;
+    }
+
+    public Map<String, List<Card>> getNameWithCards() {
+        return new HashMap<>(nameWithCards);
+    }
+
+    public int getTotalScore() {
+        return totalScore;
     }
 }
