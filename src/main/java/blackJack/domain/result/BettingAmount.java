7bf62e90b56a4a51292498bc5a4295d99e778346
summerlunaa <summerlunaa9@gmail.com>
2022-03-15 23:39:54 +0900

    feat: 베팅금액 검증 로직 구현

---
+++ src/main/java/blackJack/domain/result/BettingAmount.java
@@ -0,0 +1,29 @@
+package blackJack.domain.result;
+
+public class BettingAmount {
+
+    private final int bettingAmount;
+
+    public BettingAmount(int bettingAmount) {
+        validatePositiveNumber(bettingAmount);
+        this.bettingAmount = bettingAmount;
+    }
+
+    public static BettingAmount newInstanceByString(String input) {
+        return new BettingAmount(toInt(input));
+    }
+
+    private static int toInt(String bettingAmount) {
+        try {
+            return Integer.parseInt(bettingAmount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("베팅 금액은 정수여야 합니다.");
+        }
+    }
+
+    private void validatePositiveNumber(int bettingAmount) {
+        if (bettingAmount <= 0) {
+            throw new IllegalArgumentException("베팅 금액은 양수여야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/blackJack/domain/result/BettingAmountTest.java
@@ -0,0 +1,33 @@
+package blackJack.domain.result;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BettingAmountTest {
+    @Test
+    @DisplayName("베팅금액 생성 테스트")
+    void createBettingAmount() {
+        assertThat(new BettingAmount(3000)).isNotNull();
+    }
+
+    @ParameterizedTest(name = "베팅 금액이 정수가 아닌 경우 예외 발생")
+    @ValueSource(strings = {"", " ", "a", "1.5"})
+    void checkNotInteger(String value) {
+        assertThatThrownBy(() -> BettingAmount.newInstanceByString(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("베팅 금액은 정수여야 합니다.");
+    }
+
+    @ParameterizedTest(name = "베팅 금액이 양수가 아닌 경우 예외 발생")
+    @ValueSource(strings = {"0", "-1"})
+    void checkNotPositiveNumbeer(String value) {
+        assertThatThrownBy(() -> BettingAmount.newInstanceByString(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("베팅 금액은 양수여야 합니다.");
+    }
+}
