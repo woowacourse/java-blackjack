554178fb2dc9c74bc893ae00504ac5c59901e9d0
jayjaehunchoi <wogns0108@naver.com>
2022-03-15 17:58:57 +0900

    refactor : 승, 무, 패 체크 로직 메서드 분리

--- src/main/java/blackjack/domain/result/CompareResult.java
+++ src/main/java/blackjack/domain/result/CompareResult.java
@@ -6,29 +6,41 @@ import java.util.function.BiPredicate;
 
 public enum CompareResult {
 
-    WIN(new Keep(), (dealerResult, gamerResult) -> (dealerResult > Gamer.LIMIT_GAMER_TOTAL_POINT &&
-            gamerResult <= Gamer.LIMIT_GAMER_TOTAL_POINT)
-            || (dealerResult < gamerResult &&
-            gamerResult <= Gamer.LIMIT_GAMER_TOTAL_POINT)),
-    DRAW(new Draw(), (dealerResult, gamerResult) -> dealerResult <= Gamer.LIMIT_GAMER_TOTAL_POINT &&
-            gamerResult <= Gamer.LIMIT_GAMER_TOTAL_POINT &&
-            dealerResult == gamerResult),
-    LOSE(new Lose(), (dealerResult, gamerResult) -> dealerResult > gamerResult ||
-            gamerResult > Gamer.LIMIT_GAMER_TOTAL_POINT);
+    WIN(new Keep(), CompareResult::isWin),
+    DRAW(new Draw(), CompareResult::isDraw),
+    LOSE(new Lose(), (dealerResult, gamerResult) -> isLose(dealerResult, gamerResult));
 
     private final Result result;
+
     private final BiPredicate<Integer, Integer> predicate;
 
     CompareResult(final Result result, final BiPredicate<Integer, Integer> predicate) {
         this.result = result;
         this.predicate = predicate;
     }
-
     public static CompareResult findCompareResult(final int dealerResult, final int gamerResult) {
         return Arrays.stream(values())
                 .filter((result) -> result.predicate.test(dealerResult, gamerResult))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하는 결과가 없습니다."));
+    }
+
+    private static boolean isWin(Integer dealerResult, Integer gamerResult) {
+        return (dealerResult > Gamer.LIMIT_GAMER_TOTAL_POINT &&
+                gamerResult <= Gamer.LIMIT_GAMER_TOTAL_POINT)
+                || (dealerResult < gamerResult &&
+                gamerResult <= Gamer.LIMIT_GAMER_TOTAL_POINT);
+    }
+
+    private static boolean isDraw(Integer dealerResult, Integer gamerResult) {
+        return dealerResult <= Gamer.LIMIT_GAMER_TOTAL_POINT &&
+                gamerResult <= Gamer.LIMIT_GAMER_TOTAL_POINT &&
+                dealerResult == gamerResult;
+    }
+
+    private static boolean isLose(Integer dealerResult, Integer gamerResult) {
+        return dealerResult > gamerResult ||
+                gamerResult > Gamer.LIMIT_GAMER_TOTAL_POINT;
     }
 
     public Result getResult() {
