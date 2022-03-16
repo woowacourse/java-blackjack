b2648c57eebf112f7dddb411057254f747c490b5
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-16 18:39:32 +0900

    test: Bust score 테스트 공통부분 통합

--- src/test/java/blackjack/domain/state/BustTest.java
+++ src/test/java/blackjack/domain/state/BustTest.java
@@ -12,7 +12,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Cards;
 import java.util.Set;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BustTest {
 
@@ -24,19 +28,19 @@ class BustTest {
         assertThat(bust.earningRate(bust)).isEqualTo(-1);
     }
 
-    @Test
-    void 기본_스코어_계산이_버스트면_기본_스코어_계산을_반환() {
+    @ParameterizedTest
+    @MethodSource("generateScoreValues")
+    void 스코어_계산() {
         final Cards cards = new Cards(Set.of(Card.of(SPADES, KING), Card.of(SPADES, QUEEN), Card.of(SPADES, JACK)));
         final Bust bust = new Bust(cards);
 
         assertThat(bust.score()).isEqualTo(30);
     }
 
-    @Test
-    void 기본_스코어_계산이_버스트가_아니면_최대_스코어_계산을_반환() {
-        final Cards cards = new Cards(Set.of(Card.of(SPADES, SEVEN), Card.of(SPADES, EIGHT), Card.of(SPADES, A)));
-        final Bust bust = new Bust(cards);
-
-        assertThat(bust.score()).isEqualTo(26);
+    private static Stream<Arguments> generateScoreValues() {
+        return Stream.of(
+                Arguments.of(Set.of(Card.of(SPADES, KING), Card.of(SPADES, QUEEN), Card.of(SPADES, JACK)), 30),
+                Arguments.of(Set.of(Set.of(Card.of(SPADES, SEVEN), Card.of(SPADES, EIGHT), Card.of(SPADES, A))), 26)
+        );
     }
 }
