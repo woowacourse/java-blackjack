24c15b4c86f630b54df706bbd66b664e919e17f7
is2js <tingstyle1@gmail.com>
2022-03-16 19:05:39 +0900

    test: gambler 생성 테스트 invalid case 추가 구현

--- src/test/java/blackjack/domain/player/GamblerTest.java
+++ src/test/java/blackjack/domain/player/GamblerTest.java
@@ -1,14 +1,25 @@
 package blackjack.domain.player;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import blackjack.domain.BetMoney;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GamblerTest {
 
+    @DisplayName("배팅 금액을 받아 겜블러를 정상적으로 생성하는지 확인한다.")
     @Test
-    void create() {
+    void create_valid() {
         assertDoesNotThrow( () -> new Gambler("돌범", new BetMoney(1000)));
+    }
+
+    @DisplayName("잘못된 범위의 배팅 금액을 받아 겜블러를 생성시 예외를 발생시키는지 확인한다.")
+    @Test
+    void create_invalid() {
+        assertThatThrownBy(() -> new Gambler("돌범", new BetMoney(-1000)))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("범위");
     }
 }
