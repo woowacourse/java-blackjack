4e33465a887579d50f015fda5141fccdad218f96
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-16 11:15:40 +0900

    feat: carddeck 자료구조 변경

--- src/main/java/blackjack/domain/card/CardDeck.java
+++ src/main/java/blackjack/domain/card/CardDeck.java
@@ -1,25 +1,26 @@
 package blackjack.domain.card;
 
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class CardDeck {
 
     private static final int CARD_DECK_DEFAULT_SIZE = 52;
 
-    private final List<Card> cards;
+    private final Queue<Card> cards;
 
-    public CardDeck(final List<Card> cards) {
+    public CardDeck(final Queue<Card> cards) {
         checkCardDeckSize(cards);
-        this.cards = cards;
+        this.cards = new ArrayDeque<>(cards);
     }
 
-    private void checkCardDeckSize(final List<Card> cards) {
+    private void checkCardDeckSize(final Queue<Card> cards) {
         if (cardsDistinctCount(cards) != CARD_DECK_DEFAULT_SIZE) {
             throw new IllegalArgumentException("카드 덱은 중복되지 않은 52장으로만 생성할 수 있습니다.");
         }
     }
 
-    private long cardsDistinctCount(final List<Card> cards) {
+    private long cardsDistinctCount(final Queue<Card> cards) {
         return cards.stream()
                 .distinct().count();
     }
--- src/test/java/blackjack/domain/card/CardDeckTest.java
+++ src/test/java/blackjack/domain/card/CardDeckTest.java
@@ -2,15 +2,15 @@ package blackjack.domain.card;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import org.junit.jupiter.api.Test;
 
 class CardDeckTest {
 
     @Test
     void 카드덱에_중복되지않은_52장이_들어오지_않는_경우_예외발생() {
-        final List<Card> cards = new ArrayList<>();
+        final Queue<Card> cards = new ArrayDeque<>();
         assertThatThrownBy(() -> new CardDeck(cards))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("카드 덱은 중복되지 않은 52장으로만 생성할 수 있습니다.");
