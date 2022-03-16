58a9bb2edfd67118d4bf6b00a8bf6c43e1161d2b
Hongdonggeon <ghd700@naver.com>
2022-03-16 10:47:23 +0900

    test : hit의 메서드 시그니처 변경으로 인한 테스트 코드 수정

--- src/test/java/domain/participant/DealerTest.java
+++ src/test/java/domain/participant/DealerTest.java
@@ -1,57 +1,54 @@
 package domain.participant;
 
-import domain.card.Card;
-import domain.card.Denomination;
-import domain.card.Symbol;
-import dto.ResultDto;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import domain.card.Deck;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utils.ExceptionMessages;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 class DealerTest {
 
     private Dealer dealer;
+    private Deck deck;
 
     @BeforeEach
     void makePlayer(){
         dealer = new Dealer();
+        deck = Deck.initDeck();
     }
 
-//    @Test
-//    @DisplayName("16이 넘은 상태에서 카드를 뽑을 경우 에러를 발생시킨다.")
-//    void hitCardOverLimitError() {
-//        dealer.hit(new Card(Symbol.SPADE, Denomination.EIGHT));
-//        dealer.hit(new Card(Symbol.SPADE, Denomination.NINE));
-//
-//        assertThatThrownBy(() ->  dealer.hit(new Card(Symbol.SPADE, Denomination.ACE)))
-//            .isInstanceOf(IllegalStateException.class)
-//            .hasMessage(ExceptionMessages.OVER_CARD_LIMIT_ERROR);
-//    }
-//
-//    @Test
-//    @DisplayName("숫자가 16이 넘는 경우 False를 반환한다.")
-//    void canDrawCardFalseTest() {
-//        dealer.hit(new Card(Symbol.SPADE, Denomination.EIGHT));
-//        dealer.hit(new Card(Symbol.SPADE, Denomination.NINE));
-//
-//        assertThat(dealer.canDrawCard()).isFalse();
-//    }
-//
-//    @Test
-//    @DisplayName("숫자가 16이 넘지 않는 경우 True를 반환한다.")
-//    void canDrawCardTrueTest() {
-//        dealer.hit(new Card(Symbol.SPADE, Denomination.EIGHT));
-//        dealer.hit(new Card(Symbol.SPADE, Denomination.FIVE));
-//
-//        assertThat(dealer.canDrawCard()).isTrue();
-//    }
+    @Test
+    @DisplayName("16이 넘은 상태에서 카드를 뽑을 경우 에러를 발생시킨다.")
+    void hitCardOverLimitError() {
+        while (dealer.canHit()) {
+            dealer.hit(deck);
+        }
+
+        assertThatThrownBy(() ->  dealer.hit(deck))
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage(ExceptionMessages.OVER_CARD_LIMIT_ERROR);
+    }
+
+    @Test
+    @DisplayName("숫자가 16이 넘는 경우 False를 반환한다.")
+    void canDrawCardFalseTest() {
+        while (dealer.canHit()) {
+            dealer.hit(deck);
+        }
+
+        assertThat(dealer.canHit()).isFalse();
+    }
+
+    @Test
+    @DisplayName("숫자가 16이 넘지 않는 경우 True를 반환한다.")
+    void canDrawCardTrueTest() {
+        dealer.hit(deck);
+
+        assertThat(dealer.canHit()).isTrue();
+    }
 //
 //    @Test
 //    @DisplayName("딜러의 결과값을 받는다.")
--- src/test/java/domain/participant/PlayerTest.java
+++ src/test/java/domain/participant/PlayerTest.java
@@ -1,12 +1,9 @@
 package domain.participant;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import domain.card.Card;
 import domain.card.Deck;
-import domain.card.Denomination;
-import domain.card.Symbol;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,76 +33,66 @@ class PlayerTest {
             .hasMessage(ExceptionMessages.EMPTY_NAME_ERROR);
     }
 
-//    @Test
-//    @DisplayName("제대로 카드를 뽑는지 확인.")
-//    void hitCardTest() {
-//        player.hit(deck);
-//
-//        assertThat(deck.getCards()).contains(player.getCards().get(0));
-//    }
+    @Test
+    @DisplayName("21이 넘은 상태에서 카드를 뽑을 경우 에러를 발생시킨다.")
+    void hitCardOverLimitError() {
+        while (player.canHit()) {
+            player.hit(deck);
+        }
 
-//    @Test
-//    @DisplayName("21이 넘은 상태에서 카드를 뽑을 경우 에러를 발생시킨다.")
-//    void hitCardOverLimitError() {
-//        player.hit(new Card(Symbol.SPADE, Denomination.EIGHT));
-//        player.hit(new Card(Symbol.SPADE, Denomination.NINE));
-//        player.hit(new Card(Symbol.SPADE, Denomination.TEN));
-//
-//        assertThatThrownBy(() -> player.hit(new Card(Symbol.SPADE, Denomination.ACE)))
-//            .isInstanceOf(IllegalStateException.class)
-//            .hasMessage(ExceptionMessages.OVER_CARD_LIMIT_ERROR);
-//    }
-//
-//    @Test
-//    @DisplayName("숫자가 21이 넘는 경우 False를 반환한다.")
-//    void canDrawCardFalseTest() {
-//        player.hit(new Card(Symbol.SPADE, Denomination.EIGHT));
-//        player.hit(new Card(Symbol.SPADE, Denomination.NINE));
-//        player.hit(new Card(Symbol.SPADE, Denomination.TEN));
-//
-//        assertThat(player.canDrawCard()).isFalse();
-//    }
-//
-//    @Test
-//    @DisplayName("숫자가 21이 넘지 않는 경우 True를 반환한다.")
-//    void canDrawCardTrueTest() {
-//        player.hit(new Card(Symbol.SPADE, Denomination.EIGHT));
-//        player.hit(new Card(Symbol.SPADE, Denomination.NINE));
-//
-//        assertThat(player.canDrawCard()).isTrue();
-//    }
-//
-//    @Test
-//    @DisplayName("Player가 21이 넘고, 딜러가 21이 넘지 않을 경우 Lose를 반환한다.")
-//    void isWinTest() {
-//        player.hit(new Card(Symbol.SPADE, Denomination.EIGHT));
-//        player.hit(new Card(Symbol.SPADE, Denomination.NINE));
-//        player.hit(new Card(Symbol.HEART, Denomination.NINE));
-//
-//        dealer.hit(new Card(Symbol.SPADE, Denomination.SEVEN));
-//
-//        assertThat(player.isWin(dealer)).isEqualTo(Result.LOSE);
-//    }
-//
+        assertThatThrownBy(() -> player.hit(deck))
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage(ExceptionMessages.OVER_CARD_LIMIT_ERROR);
+    }
+
+    @Test
+    @DisplayName("숫자가 21이 넘는 경우 False를 반환한다.")
+    void canDrawCardFalseTest() {
+        while (player.canHit()) {
+            player.hit(deck);
+        }
+
+        assertThat(player.canHit()).isFalse();
+    }
+
+    @Test
+    @DisplayName("숫자가 21이 넘지 않는 경우 True를 반환한다.")
+    void canDrawCardTrueTest() {
+        player.hit(deck);
+
+        assertThat(player.canHit()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Player가 21이 넘고, 딜러가 21이 넘지 않을 경우 Lose를 반환한다.")
+    void isWinTest() {
+        while (player.canHit()) {
+            player.hit(deck);
+        }
+
+        dealer.hit(deck);
+
+        assertThat(player.judgeResult(dealer)).isEqualTo(Result.LOSE);
+    }
+
 //    @Test
 //    @DisplayName("Player가 21이 넘고, 딜러도 21을 경우 Win을 반환한다.")
 //    void isWinTest2() {
-//        player.hit(new Card(Symbol.SPADE, Denomination.EIGHT));
-//        player.hit(new Card(Symbol.SPADE, Denomination.NINE));
-//        player.hit(new Card(Symbol.HEART, Denomination.NINE));
+//        while (player.canHit()) {
+//            player.hit(deck);
+//        }
 //
-//        dealer.hit(new Card(Symbol.SPADE, Denomination.SEVEN));
-//        dealer.hit(new Card(Symbol.SPADE, Denomination.EIGHT));
-//        dealer.hit(new Card(Symbol.SPADE, Denomination.QUEEN));
+//        while (dealer.calculateScore() < 21) {
+//            dealer.hit(deck);
+//        }
 //
-//        assertThat(player.isWin(dealer)).isEqualTo(Result.WIN);
+//        assertThat(player.judgeResult(dealer)).isEqualTo(Result.WIN);
 //    }
-//
+
 //    @Test
 //    @DisplayName("Player와 딜러 모두 21을 넘지 않고, Player가 총 점수가 클 경우 Win을 반환한다.")
 //    void isWinTest3() {
-//        player.hit(new Card(Symbol.SPADE, Denomination.EIGHT));
-//        player.hit(new Card(Symbol.SPADE, Denomination.NINE));
+//        playe
 //
 //        dealer.hit(new Card(Symbol.SPADE, Denomination.SEVEN));
 //        dealer.hit(new Card(Symbol.SPADE, Denomination.EIGHT));
