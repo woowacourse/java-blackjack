37d42b1de386ecaa1d44755a72f7011753a7f8c3
hyeonic <dev.hyeonic@gmail.com>
2022-03-16 16:09:06 +0900

    refactor: InputView 컨벤션 수정

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private final static Scanner SCANNER = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputView() {
     }
