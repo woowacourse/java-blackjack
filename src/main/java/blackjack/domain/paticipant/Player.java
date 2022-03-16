233c9d9bceb4fc44080fc2be69d427cdf1de477f
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-16 17:16:27 +0900

    feat: 게임의 상태가 null인 경우 플레이어 예외처리 및 테스트

--- src/main/java/blackjack/domain/paticipant/Player.java
+++ src/main/java/blackjack/domain/paticipant/Player.java
@@ -1,18 +1,22 @@
 package blackjack.domain.paticipant;
 
+import blackjack.domain.state.BlackjackGameState;
 import java.util.Objects;
 
 public class Player {
 
     private final String name;
     private final int betMoney;
+    private BlackjackGameState gameState;
 
-    public Player(final String name, final int betMoney) {
+    public Player(final String name, final int betMoney, final BlackjackGameState gameState) {
         Objects.requireNonNull(name, "이름은 null이 들어올 수 없습니다.");
+        Objects.requireNonNull(gameState, "게임 상태는 null이 들어올 수 없습니다.");
         checkEmptyName(name);
         checkNotPositiveBetMoney(betMoney);
         this.name = name;
         this.betMoney = betMoney;
+        this.gameState = gameState;
     }
 
     private void checkEmptyName(final String name) {
--- src/test/java/blackjack/domain/paticipant/PlayerTest.java
+++ src/test/java/blackjack/domain/paticipant/PlayerTest.java
@@ -1,7 +1,16 @@
 package blackjack.domain.paticipant;
 
+import static blackjack.domain.card.Denomination.FIVE;
+import static blackjack.domain.card.Denomination.KING;
+import static blackjack.domain.card.Suit.SPADES;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Cards;
+import blackjack.domain.state.BlackjackGameState;
+import blackjack.domain.state.Running;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
@@ -9,9 +18,17 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayerTest {
 
+    private BlackjackGameState runningState;
+
+    @BeforeEach
+    void setup() {
+        final Cards cards = new Cards(Set.of(Card.of(SPADES, KING), Card.of(SPADES, FIVE)));
+        runningState = new Running(cards);
+    }
+
     @Test
     void 플레이어의_이름이_null인_경우_예외발생() {
-        assertThatThrownBy(() -> new Player(null, 1000))
+        assertThatThrownBy(() -> new Player(null, 1000, runningState))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("이름은 null이 들어올 수 없습니다.");
     }
@@ -19,7 +36,7 @@ class PlayerTest {
     @ParameterizedTest
     @EmptySource
     void 플레이어의_이름이_공백인_경우_예외발생(final String name) {
-        assertThatThrownBy(() -> new Player(name, 1000))
+        assertThatThrownBy(() -> new Player(name, 1000, runningState))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 공백이 들어올 수 없습니다.");
     }
@@ -27,8 +44,15 @@ class PlayerTest {
     @ParameterizedTest
     @ValueSource(ints = {0, -1000})
     void 배팅금액이_0이하인_경우_예외발생(final int betMoney) {
-        assertThatThrownBy(() -> new Player("name", betMoney))
+        assertThatThrownBy(() -> new Player("name", betMoney, runningState))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("배팅금액은 0이하의 값이 들어올 수 없습니다.");
+    }
+
+    @Test
+    void 게임상태가_null인_경우_예외발생() {
+        assertThatThrownBy(() -> new Player("name", 1000, null))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("게임 상태는 null이 들어올 수 없습니다.");
     }
 }
