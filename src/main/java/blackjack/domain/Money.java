3b21c567d18f86566a88133a5b0be37238b34943
nailseong <nnis9857@gmail.com>
2022-03-16 19:38:13 +0900

    feat(Money): 초기화 기능

---
+++ src/main/java/blackjack/domain/Money.java
@@ -0,0 +1,32 @@
+package blackjack.domain;
+
+public class Money {
+
+    private int value;
+
+    public Money() {
+        this.value = 0;
+    }
+
+    public void init(final int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final int value) {
+        validateBound(value);
+        validateLastIndex(value);
+    }
+
+    private void validateBound(final int value) {
+        if (value < 100 || value > 1_000_000_000) {
+            throw new IllegalArgumentException("베팅 금액은 100이상 10억 이하여야 합니다.");
+        }
+    }
+
+    private void validateLastIndex(final int value) {
+        if (value % 10 != 0) {
+            throw new IllegalArgumentException("베팅 금액은 10으로 나누어 떨어져야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/blackjack/domain/MoneyTest.java
@@ -0,0 +1,38 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MoneyTest {
+
+    @ParameterizedTest
+    @DisplayName("범위를 벗어난 값으로 초기화하면 예외를 던진다.")
+    @ValueSource(ints = {99, 1_000_000_001})
+    void validateBound(int value) {
+        // give
+        final Money money = new Money();
+
+        // when
+        // then
+        assertThatThrownBy(() -> money.init(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("베팅 금액은 100이상 10억 이하여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("10으로 나누어 떨어지지 않는 값으로 초기화하면 예외를 던진다.")
+    @ValueSource(ints = {101, 103, 109})
+    void validateLastIndex(int value) {
+        // give
+        final Money money = new Money();
+
+        // when
+        // then
+        assertThatThrownBy(() -> money.init(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("베팅 금액은 10으로 나누어 떨어져야 합니다.");
+    }
+}
