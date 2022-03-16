6ad7eb66913280046e4d39ad548899e525c671c5
유영우 <fbduddn97@naver.com>
2022-03-16 18:09:13 +0900

    fix: 상수 final 처리

--- src/main/java/blackjack/domain/machine/Card.java
+++ src/main/java/blackjack/domain/machine/Card.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 
 public enum Card {
     A_DIAMOND("A다이아몬드", 1),
-    TWO_DIAMOND( "2다이아몬드", 2),
-    THREE_DIAMOND( "3다이아몬드", 3),
-    FOUR_DIAMOND( "4다이아몬드", 4),
-    FIVE_DIAMOND( "5다이아몬드", 5),
-    SIX_DIAMOND( "6다이아몬드", 6),
-    SEVEN_DIAMOND( "7다이아몬드", 7),
-    EIGHT_DIAMOND( "8다이아몬드", 8),
-    NINE_DIAMOND( "9다이아몬드", 9),
-    J_DIAMOND( "J다이아몬드", 10),
-    Q_DIAMOND( "Q다이아몬드", 10),
-    K_DIAMOND( "K다이아몬드", 10),
+    TWO_DIAMOND("2다이아몬드", 2),
+    THREE_DIAMOND("3다이아몬드", 3),
+    FOUR_DIAMOND("4다이아몬드", 4),
+    FIVE_DIAMOND("5다이아몬드", 5),
+    SIX_DIAMOND("6다이아몬드", 6),
+    SEVEN_DIAMOND("7다이아몬드", 7),
+    EIGHT_DIAMOND("8다이아몬드", 8),
+    NINE_DIAMOND("9다이아몬드", 9),
+    J_DIAMOND("J다이아몬드", 10),
+    Q_DIAMOND("Q다이아몬드", 10),
+    K_DIAMOND("K다이아몬드", 10),
 
     A_HEART("A하트", 1),
     TWO_HEART("2하트", 2),
@@ -24,18 +24,18 @@ public enum Card {
     SIX_HEART("6하트", 6),
     SEVEN_HEART("7하트", 7),
     EIGHT_HEART("8하트", 8),
-    NINE_HEART( "9하트", 9),
+    NINE_HEART("9하트", 9),
     J_HEART("J하트", 10),
     Q_HEART("Q하트", 10),
     K_HEART("K하트", 10),
 
     A_SPADE("A스페이드", 1),
-    TWO_SPADE( "2스페이드", 2),
-    THREE_SPADE( "3스페이드", 4),
-    FOUR_SPADE( "4스페이드", 4),
+    TWO_SPADE("2스페이드", 2),
+    THREE_SPADE("3스페이드", 4),
+    FOUR_SPADE("4스페이드", 4),
     FIVE_SPADE("5스페이드", 5),
-    SIX_SPADE( "6스페이드", 6),
-    SEVEN_SPADE( "7스페이드", 7),
+    SIX_SPADE("6스페이드", 6),
+    SEVEN_SPADE("7스페이드", 7),
     EIGHT_SPADE("8스페이드", 8),
     NINE_SPADE("9스페이드", 9),
     J__SPADE("J스페이드", 10),
@@ -44,18 +44,18 @@ public enum Card {
 
     A_CLOVER("A클로버", 1),
     TWO__CLOVER("2클로버", 2),
-    THREE__CLOVER( "3클로버", 3),
+    THREE__CLOVER("3클로버", 3),
     FOUR_CLOVER("4클로버", 4),
     FIVE_CLOVER("5클로버", 5),
-    SIX_CLOVER( "6클로버", 6),
+    SIX_CLOVER("6클로버", 6),
     SEVEN_CLOVER("7클로버", 7),
-    EIGHT_CLOVER( "8클로버", 8),
+    EIGHT_CLOVER("8클로버", 8),
     NINE_CLOVER("9클로버", 9),
     J_CLOVER("J클로버", 10),
     Q_CLOVER("Q클로버", 10),
     K_CLOVER("K클로버", 10);
 
-    public static int SIZE = 48;
+    public static final int SIZE = 48;
 
     private String name;
     private int number;
