fb7b965806908a00af77b080108b74721e2e3099
koo <gudonghee2000@gmail.com>
2022-03-16 19:23:46 +0900

    test: TrumpNumber.sumTo()를 테스트하는 케이스 추가

--- src/test/java/blackjack/model/card/TrumpNumberTest.java
+++ src/test/java/blackjack/model/card/TrumpNumberTest.java
@@ -6,9 +6,22 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class TrumpNumberTest {
+
     @ParameterizedTest(name = "{0}에 8을 더하면 {1}이다")
     @CsvSource(value = {"2:10", "7:15", "25:33"}, delimiterString = ":")
-    void sumTo_value(int value, int expected) {
+    void sumTo_value_EIGHT(int value, int expected) {
         assertThat(TrumpNumber.EIGHT.sumTo(value)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "{0}에 ACE을 더하면 {1}이다")
+    @CsvSource(value = {"2:3", "7:8", "25:26"}, delimiterString = ":")
+    void sumTo_value_ACE(int value, int expected) {
+        assertThat(TrumpNumber.ACE.sumTo(value)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "{0}에 8을 더하면 {1}이다")
+    @CsvSource(value = {"2:12", "7:17", "25:35"}, delimiterString = ":")
+    void sumTo_value(int value, int expected) {
+        assertThat(TrumpNumber.KING.sumTo(value)).isEqualTo(expected);
     }
 }
