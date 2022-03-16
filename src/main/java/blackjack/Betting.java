7dfab6b9797e413ca63f81dd01f999e2ad7a6a93
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-03-16 15:58:52 +0900

    feat(Betting): 전적에 따라 배팅 금액 결정 기능

---
+++ src/main/java/blackjack/Betting.java
@@ -0,0 +1,26 @@
+package blackjack;
+
+import blackjack.domain.PlayRecord;
+import blackjack.domain.participant.Name;
+
+public class Betting {
+    private final Name name;
+    private final long money;
+
+    public Betting(Name name, long money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public long result(PlayRecord playRecord) {
+        if (playRecord == PlayRecord.LOSS) {
+            return -money;
+        }
+
+        if (playRecord == PlayRecord.WIN) {
+            return money;
+        }
+
+        return 0;
+    }
+}
---
+++ src/test/java/blackjack/BettingTest.java
@@ -0,0 +1,43 @@
+package blackjack;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import blackjack.domain.PlayRecord;
+import blackjack.domain.participant.Name;
+
+class BettingTest {
+    private static final Betting betting = new Betting(Name.of("pobi"), 10000);
+
+    @Test
+    @DisplayName("LOSS인 경우 배팅 금액을 전부 잃는다.")
+    void bettingLoss() {
+        //when
+        long actual = betting.result(PlayRecord.LOSS);
+
+        //then
+        assertThat(actual).isEqualTo(-10000);
+    }
+
+    @Test
+    @DisplayName("WIN인 경우 배팅 금액을 수익으로 받는다.")
+    void bettingWin() {
+        //when
+        long actual = betting.result(PlayRecord.WIN);
+
+        //then
+        assertThat(actual).isEqualTo(10000);
+    }
+
+    @Test
+    @DisplayName("PUSH인 경우 수익은 0이다.")
+    void bettingPush() {
+        //when
+        long actual = betting.result(PlayRecord.PUSH);
+
+        //then
+        assertThat(actual).isEqualTo(0);
+    }
+}
