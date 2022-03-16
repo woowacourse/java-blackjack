3808a7df636d0487cdab67cb6fb3d10cf7369b4c
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-16 14:29:15 +0900

    feat: Running상태 Bust카드입력 예외처리 및 테스트

--- src/main/java/blackjack/domain/state/Running.java
+++ src/main/java/blackjack/domain/state/Running.java
@@ -1,17 +1,22 @@
 package blackjack.domain.state;
 
-import blackjack.domain.card.Card;
-import java.util.ArrayList;
-import java.util.List;
+import blackjack.domain.card.Cards;
 import java.util.Objects;
 
 public class Running implements BlackjackGameState {
 
-    private final List<Card> cards;
+    private final Cards cards;
 
-    public Running(final List<Card> cards) {
+    public Running(final Cards cards) {
         Objects.requireNonNull(cards, "cards는 null이 들어올 수 없습니다.");
-        this.cards = new ArrayList<>(cards);
+        this.cards = cards;
+        checkBustCards(cards);
+    }
+
+    private void checkBustCards(final Cards cards) {
+        if (cards.isBust()) {
+            throw new IllegalArgumentException("Running상태는 버스트된 카드가 들어올 수 없습니다.");
+        }
     }
 
     @Override
--- src/test/java/blackjack/domain/state/RunningTest.java
+++ src/test/java/blackjack/domain/state/RunningTest.java
@@ -7,7 +7,8 @@ import static blackjack.domain.card.Suit.SPADES;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import blackjack.domain.card.Card;
-import java.util.Arrays;
+import blackjack.domain.card.Cards;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class RunningTest {
@@ -21,9 +22,9 @@ class RunningTest {
 
     @Test
     void 생성_시_score가_bust_score인_경우_예외발생() {
-        assertThatThrownBy(
-                () -> new Running(Arrays.asList(Card.of(SPADES, KING), Card.of(SPADES, QUEEN), Card.of(SPADES, JACK))))
+        final Set<Card> cards = Set.of(Card.of(SPADES, KING), Card.of(SPADES, QUEEN), Card.of(SPADES, JACK));
+        assertThatThrownBy(() -> new Running(new Cards(cards)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Running상태는 21초과된 수가 들어올 수 없습니다.");
+                .hasMessage("Running상태는 버스트된 카드가 들어올 수 없습니다.");
     }
 }
