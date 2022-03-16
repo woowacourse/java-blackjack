ffd0612bfd1ec49d65cdd56f9a5a23aa6cf52f6d
summerlunaa <summerlunaa9@gmail.com>
2022-03-16 13:20:03 +0900

    feat: 플레이어들의 최종 수익 계산 로직 구현

---
+++ src/main/java/blackJack/domain/result/ResultOfProfit.java
@@ -0,0 +1,28 @@
+package blackJack.domain.result;
+
+import blackJack.domain.participant.Dealer;
+import blackJack.domain.participant.Player;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class ResultOfProfit {
+
+    private final Map<Player, BettingAmount> bettingAmounts;
+
+    public ResultOfProfit(Map<Player, String> inputs) {
+        bettingAmounts = new LinkedHashMap<>();
+        for (Entry<Player, String> entry : inputs.entrySet()) {
+            bettingAmounts.put(entry.getKey(), BettingAmount.newInstanceByString(entry.getValue()));
+        }
+    }
+
+    public Map<Player, Integer> getPlayersProfit(Dealer dealer) {
+        Map<Player, Integer> playersProfit = new LinkedHashMap<>();
+        for (Entry<Player, BettingAmount> entry : bettingAmounts.entrySet()) {
+            MatchResult playerMatchResult = entry.getKey().getMatchResult(dealer);
+            playersProfit.put(entry.getKey(), entry.getValue().calculateProfit(playerMatchResult));
+        }
+        return playersProfit;
+    }
+}
---
+++ src/test/java/blackJack/domain/result/ResultOfProfitTest.java
@@ -0,0 +1,45 @@
+package blackJack.domain.result;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackJack.domain.card.Card;
+import blackJack.domain.card.Denomination;
+import blackJack.domain.card.Symbol;
+import blackJack.domain.participant.Dealer;
+import blackJack.domain.participant.Player;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ResultOfProfitTest {
+
+    Dealer dealer;
+    Player player1;
+    Player player2;
+    ResultOfProfit resultOfProfit;
+
+    @BeforeEach
+    void setUp() {
+        dealer = new Dealer();
+        dealer.hit(Card.from(Symbol.CLOVER, Denomination.JACK));
+        dealer.hit(Card.from(Symbol.CLOVER, Denomination.NINE));
+
+        player1 = new Player("rookie");
+        player1.hit(Card.from(Symbol.CLOVER, Denomination.ACE));
+        player1.hit(Card.from(Symbol.CLOVER, Denomination.QUEEN));
+
+        player2 = new Player("parang");
+        player2.hit(Card.from(Symbol.CLOVER, Denomination.SEVEN));
+        player2.hit(Card.from(Symbol.CLOVER, Denomination.KING));
+
+        resultOfProfit = new ResultOfProfit(Map.of(player1, "10000", player2, "20000"));
+    }
+
+    @Test
+    void getPlayersProfit() {
+        assertThat(resultOfProfit.getPlayersProfit(dealer)).contains(
+                Map.entry(player1, 10000),
+                Map.entry(player2, -20000)
+        );
+    }
+}
