7bf7e2d077dc91da577380d922369e00aa47bd0d
shindong96 <2006peter@naver.com>
2022-03-17 02:19:51 +0900

    fix: main 오류 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,8 @@
 import controller.Controller;
->>>>>>>step1
 
 public class Application {
 
 	public static void main(String[] args) {
-<<<<<<<HEAD
-		new MainController().run();
-=======
 		new Controller().run();
->>>>>>>step1
 	}
 }
