6bcb7a736a06067f7d24b8e11347b335a2ce2a9f
summerlunaa <summerlunaa9@gmail.com>
2022-03-16 13:29:20 +0900

    feat: 플레이어의 베팅 금액을 입력받는 기능 구현

--- src/main/java/blackJack/view/InputView.java
+++ src/main/java/blackJack/view/InputView.java
@@ -1,7 +1,10 @@
 package blackJack.view;
 
+import blackJack.domain.participant.Player;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -12,6 +15,7 @@ public class InputView {
     private static final String NEWLINE = System.getProperty("line.separator");
     private static final String INPUT_MESSAGE_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String INPUT_DELIMITER_PLAYER_NAMES = ",";
+    public static final String INPUT_MESSAGE_BETTING_AMOUNT = "%s의 베팅 금액은?".concat(NEWLINE);
     private static final String INPUT_MESSAGE_ONE_MORE_CARD =
             "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)".concat(NEWLINE);
 
@@ -24,6 +28,16 @@ public class InputView {
         return Arrays.stream(input.split(INPUT_DELIMITER_PLAYER_NAMES))
                 .map(String::trim)
                 .collect(Collectors.toList());
+    }
+
+    private static Map<Player, String> inputBettingAmount(List<Player> players) {
+        Map<Player, String> inputs = new LinkedHashMap<>();
+        for (Player player : players) {
+            System.out.printf(INPUT_MESSAGE_BETTING_AMOUNT, player.getName());
+            inputs.put(player, scanner.nextLine());
+            System.out.println();
+        }
+        return inputs;
     }
 
     public static String inputOneMoreCard(String name) {
