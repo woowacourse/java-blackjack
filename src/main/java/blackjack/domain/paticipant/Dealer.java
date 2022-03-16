94d3874dd54138f156ba9e6a876013c300c39344
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-16 19:53:59 +0900

    feat: 딜러 Card 생성 기능 구현

--- src/main/java/blackjack/domain/paticipant/Dealer.java
+++ src/main/java/blackjack/domain/paticipant/Dealer.java
@@ -1,14 +1,20 @@
 package blackjack.domain.paticipant;
 
+import blackjack.domain.card.Cards;
 import blackjack.domain.state.BlackjackGameState;
+import blackjack.domain.state.DealerRunning;
 import java.util.Objects;
 
 public class Dealer {
 
     private BlackjackGameState gameState;
 
-    public Dealer(final BlackjackGameState gameState) {
+    private Dealer(final BlackjackGameState gameState) {
         Objects.requireNonNull(gameState, "게임 상태는 null이 들어올 수 없습니다.");
         this.gameState = gameState;
+    }
+
+    public Dealer(final Cards cards) {
+        this(DealerRunning.createDealerGameState(cards));
     }
 }
