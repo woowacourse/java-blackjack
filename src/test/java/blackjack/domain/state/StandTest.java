548ab54033eb8dc0ff095fd46f1df7585392b2dc
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-16 15:53:59 +0900

    feat: Finished상태에서 hit하는 경우 예외처리 및 테스트

---
+++ src/test/java/blackjack/domain/state/StandTest.java
@@ -0,0 +1,25 @@
+package blackjack.domain.state;
+
+import static blackjack.domain.card.Denomination.A;
+import static blackjack.domain.card.Denomination.KING;
+import static blackjack.domain.card.Denomination.QUEEN;
+import static blackjack.domain.card.Suit.SPADES;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Cards;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+class StandTest {
+
+    @Test
+    void stand상태에서_hit하는_경우_예외발생() {
+        final Set<Card> cards = Set.of(Card.of(SPADES, KING), Card.of(SPADES, QUEEN));
+        final Stand stand = new Stand(new Cards(cards));
+
+        assertThatThrownBy(() -> stand.hit(Card.of(SPADES, A)))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Finish상태는 hit할 수 없습니다.");
+    }
+}
