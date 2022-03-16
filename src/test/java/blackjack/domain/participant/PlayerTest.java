f6fe5103de916a16febedf6a18abf834526ab6e1
nailseong <nnis9857@gmail.com>
2022-03-16 23:48:55 +0900

    test(Player): 상금 계산 기능 테스트

--- src/test/java/blackjack/domain/participant/PlayerTest.java
+++ src/test/java/blackjack/domain/participant/PlayerTest.java
@@ -1,8 +1,11 @@
 package blackjack.domain.participant;
 
+import static blackjack.domain.card.CardNumber.FIVE;
 import static blackjack.domain.card.CardNumber.JACK;
 import static blackjack.domain.card.CardNumber.KING;
 import static blackjack.domain.card.CardNumber.QUEEN;
+import static blackjack.domain.card.CardNumber.TEN;
+import static blackjack.domain.card.CardSymbol.CLUB;
 import static blackjack.domain.card.CardSymbol.DIAMOND;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,6 +18,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayerTest {
 
@@ -59,5 +63,46 @@ class PlayerTest {
 
         // then
         assertThat(actual).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @DisplayName("플레이어가 버스트한 경우 상금을 계산한다.")
+    @ValueSource(ints = {20, 22})
+    void calculatePrize_playerBust(int dealerScore) {
+        // give
+        final Player player = new Player("rick");
+        player.initMoney(1000);
+
+        final Deck deck = Deck.from(() -> List.of(Card.of(CLUB, TEN), Card.of(CLUB, KING), Card.of(CLUB, FIVE)));
+        IntStream.range(0, 3)
+                .forEach(i -> player.hit(deck));
+
+        // when
+        player.calculatePrize(false, dealerScore);
+        final int actual = player.getPrize();
+
+        // then
+        assertThat(actual).isEqualTo(-1000);
+    }
+
+    @ParameterizedTest
+    @DisplayName("상금을 계산한다.")
+    @CsvSource(value = {"true:21:ACE:0", "false:20:ACE:1500", "false:20:TEN:0", "false:20:NINE:-1000",
+            "false:22:NINE:1000", "false:19:TEN:1000"}, delimiter = ':')
+    void calculatePrize_playerNotBust(boolean isDealerBlackjack, int dealerScore, CardNumber cardNumber, int expected) {
+        // give
+        final Player player = new Player("rick");
+        player.initMoney(1000);
+
+        final Deck deck = Deck.from(() -> List.of(Card.of(CLUB, cardNumber), Card.of(CLUB, KING)));
+        IntStream.range(0, 2)
+                .forEach(i -> player.hit(deck));
+
+        // when
+        player.calculatePrize(isDealerBlackjack, dealerScore);
+        final int actual = player.getPrize();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 }
