ce27d329e5ce5937065a51d7b7e64f6b8828aa3e
TaeHyeon Kim <kth990303@naver.com>
2022-03-16 22:19:13 +0900

    refactor: 덱 생성 시 복사본을 shuffle 하도록 변경

--- src/main/java/blackJack/domain/card/Deck.java
+++ src/main/java/blackJack/domain/card/Deck.java
@@ -10,12 +10,12 @@ public class Deck {
     private final Queue<Card> deck;
 
     private Deck(LinkedList<Card> deck) {
+        Collections.shuffle(deck);
         this.deck = deck;
     }
 
     public static Deck create() {
         List<Card> cards = Card.initializeDeck();
-        Collections.shuffle(cards);
         return new Deck(new LinkedList<>(cards));
     }
 
