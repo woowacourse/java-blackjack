ccc48557149da477117d42e971857d9bd204b6b8
Chris <bjuuuu98@gmail.com>
2022-03-16 15:10:05 +0900

    feat: 배팅 금액 입력 기능 구현

--- src/main/java/blackjack/view/input/InputView.java
+++ src/main/java/blackjack/view/input/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
 
     private static final String INPUT_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String INPUT_DRAW_COMMAND_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
+    private static final String INPUT_BATTING_MONEY_MESSAGE = "의 배팅 금액은?";
 
     private static final String PLAYER_NAME_BLANK = " ";
     private static final String PLAYER_NAME_NOT_BLANK = "";
@@ -26,6 +27,11 @@ public class InputView {
         return Arrays.stream(scanner.nextLine().split(PLAYER_NAME_DELIMITER))
                 .map(name -> name.replace(PLAYER_NAME_BLANK, PLAYER_NAME_NOT_BLANK))
                 .collect(Collectors.toUnmodifiableList());
+    }
+
+    public static String inputBattingMoney(final String name) {
+        System.out.println(name + INPUT_BATTING_MONEY_MESSAGE);
+        return scanner.nextLine();
     }
 
     public static String inputDrawCommand(final CurrentTurnParticipant currentTurnParticipant) {
