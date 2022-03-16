2a0a10f911652e7cdd8c9c192acd649ee7d172c5
Chris <bjuuuu98@gmail.com>
2022-03-16 13:41:42 +0900

    feat: 배팅 금액이 10 단위가 아닌 경우 예외 발생 기능 구현 및 테스트

--- src/main/java/blackjack/domain/game/BattingMoney.java
+++ src/main/java/blackjack/domain/game/BattingMoney.java
@@ -4,14 +4,18 @@ import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class BattingMoney {
+
     private static final Pattern NATURAL_NUMBER_PATTERN = Pattern.compile("^[1-9][0-9]*$");
+    private static final int BATTING_MONEY_UNIT = 10;
     private final int value;
 
     public BattingMoney(final String value) {
         Objects.requireNonNull(value, "배팅 금액에는 null이 들어올 수 없습니다.");
         validateBlank(value);
         validateNaturalNumber(value);
-        this.value = Integer.parseInt(value);
+        final int battingMoney = Integer.parseInt(value);
+        validateUnit(battingMoney);
+        this.value = battingMoney;
     }
 
     private void validateNaturalNumber(final String value) {
@@ -23,6 +27,12 @@ public class BattingMoney {
     private void validateBlank(final String value) {
         if (value.isBlank()) {
             throw new IllegalArgumentException("배팅 금액은 빈 값으로 만들 수 없습니다.");
+        }
+    }
+
+    private void validateUnit(final int value) {
+        if (value % BATTING_MONEY_UNIT != 0) {
+            throw new IllegalArgumentException("배팅 금액의 단위는 10입니다.");
         }
     }
 }
--- src/test/java/blackjack/domain/game/BattingMoneyTest.java
+++ src/test/java/blackjack/domain/game/BattingMoneyTest.java
@@ -3,13 +3,15 @@ package blackjack.domain.game;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BattingMoneyTest {
+
     @ParameterizedTest
-    @DisplayName("1 이상의 정수가 아닌 값으로 객체를 생성하려고 하면 예외를 발생시킨다.")
+    @DisplayName("자연수가 아닌 값으로 객체를 생성하려고 하면 예외를 발생시킨다.")
     @ValueSource(strings = {"0", "-1", "1.2", "?"})
     void createExceptionByNotNaturalNumberValue(String invalidValue) {
         assertThatThrownBy(() -> new BattingMoney(invalidValue))
@@ -33,5 +35,13 @@ class BattingMoneyTest {
         assertThatThrownBy(() -> new BattingMoney(blankValue))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("배팅 금액은 빈 값으로 만들 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("10의 배수가 아닌 값으로 배팅 금액 객체를 생성하려고 하면 예외를 발생시킨다.")
+    void createExceptionByInvalidUnitValue() {
+        assertThatThrownBy(() -> new BattingMoney("1001"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("배팅 금액의 단위는 10입니다.");
     }
 }
