052c5087c77db00979c3566f0bc63141f3e9fd41
yoon <duddbs774@gmail.com>
2022-03-17 20:20:50 +0900

    refactor: 게임 결과 구하는 기능 리펙토링
    - 가독성이 떨어지는 함수적 인터페이스 제거
    - 정적 메서드로 구현

--- src/main/java/domain/GameResult.java
+++ src/main/java/domain/GameResult.java
@@ -1,44 +1,42 @@
 package domain;
 
+import domain.card.CardState;
 import domain.card.Cards;
 
-import java.util.Arrays;
-import java.util.function.BiPredicate;
-
 public enum GameResult {
-    WIN((a, b) -> compareCardStatePower(a, b) > 0 || (isBothStand(a, b) && compareCardScore(a, b) > 0)),
-    DRAW((a, b) -> isBothBlackJack(a, b) || (isBothStand(a, b) && compareCardScore(a, b) == 0)),
-    LOSE((a, b) -> a.isBust() || compareCardStatePower(a, b) < 0 || (isBothStand(a, b) && compareCardScore(a, b) < 0));
-
-    private static final String CANNOT_FIND_GAME_RESULT_MESSAGE = "게임결과를 찾을 수 없습니다.";
-
-    private final BiPredicate<Cards, Cards> biPredicate;
-
-    GameResult(final BiPredicate<Cards, Cards> biPredicate) {
-        this.biPredicate = biPredicate;
-    }
+    WIN,
+    DRAW,
+    LOSE,
+    ;
 
     public static GameResult getPlayerResult(final Cards playerCards, final Cards dealerCards) {
-        return Arrays.stream(GameResult.values())
-                .filter(gameResult -> gameResult.biPredicate.test(playerCards, dealerCards))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(CANNOT_FIND_GAME_RESULT_MESSAGE));
+        final CardState playerCardsState = playerCards.getCardState();
+        final CardState dealerCardsState = dealerCards.getCardState();
+
+        if (playerCardsState.isStand() && dealerCardsState.isStand()) {
+            return getPlayerResult(playerCards.calculateSum(), dealerCards.calculateSum());
+        }
+        return getPlayerResult(playerCardsState, dealerCardsState);
     }
 
-    private static int compareCardStatePower(final Cards playerCards, final Cards dealerCards) {
-        return playerCards.getCardStatePower() - dealerCards.getCardStatePower();
+    private static GameResult getPlayerResult(final int playerCardSum, final int dealerCardSum) {
+        if (playerCardSum > dealerCardSum) {
+            return WIN;
+        }
+        if (playerCardSum < dealerCardSum) {
+            return LOSE;
+        }
+        return DRAW;
     }
 
-    private static boolean isBothStand(final Cards playerCards, final Cards dealerCards) {
-        return playerCards.isStand() && dealerCards.isStand();
-    }
-
-    private static boolean isBothBlackJack(final Cards playerCards, final Cards dealerCards) {
-        return playerCards.isBlackJack() && dealerCards.isBlackJack();
-    }
-
-    private static int compareCardScore(final Cards playerCards, final Cards dealerCards) {
-        return playerCards.calculateSum() - dealerCards.calculateSum();
+    private static GameResult getPlayerResult(final CardState playerCardsState, final CardState dealerCardsState) {
+        if ((playerCardsState.isBlackJack() && dealerCardsState.isBlackJack())) {
+            return DRAW;
+        }
+        if (playerCardsState.isBlackJack() || playerCardsState.isStand() && dealerCardsState.isBust()) {
+            return WIN;
+        }
+        return LOSE;
     }
 
     private static GameResult reverseFrom(final GameResult origin) {
