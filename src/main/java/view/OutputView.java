8e818fb764e1d6f8a3fa5f3aedd68cd36dcdc79e
Richard JEON <ztzy1907@gmail.com>
2022-03-18 03:54:03 +0900

    refactor(view): 출력 형식 변경 대응. 승무패 -> 수익

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,35 +1,31 @@
 package view;
 
-import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
 
 import domain.BlackJackResult;
-import domain.MatchResult;
 import domain.card.PlayingCard;
 import domain.player.Dealer;
 import dto.CardsAndScoreDto;
 import dto.CardsDto;
 import dto.NameDto;
-import dto.ResultDto;
-import java.util.Arrays;
-import java.util.EnumMap;
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
 import view.util.NameMapper;
+import vo.Name;
+import vo.Revenue;
 
 public class OutputView {
     private static final String INFO_FOR_INITIAL_SPREAD = "%s와 %s에게 2장을 나누었습니다.%n";
     private static final String INFO_FOR_DEALER_ADD_CARD = "%s는 16이하라 한장의 카드를 더 받았습니다.%n";
     private static final String INFO_PLAYER_CARD_AND_SCORE = "%s카드: %s - 결과: %d%n";
     private static final String INFO_CARD_STATUS_AFTER_INITIAL_SPREAD = "%s: %s%n";
-    private static final String COLON_FOR_JOINING_NAME_AND_CARD = ": ";
-    private static final String RESULT_TITLE = "## 최종 승패";
+    private static final String COLON_FOR_NAME_AND_REVENUE = ": ";
+    private static final String RESULT_TITLE = "## 최종 수익";
     private static final String CARD_NAME_JOIN_CHARACTER = ", ";
     private static final String GAMBLER_NAME_DELIMITER = ", ";
-    private static final String WIN_DRAW_LOSE_DELIMITER = " ";
     private static final String ERROR_FOR_NO_DEALER_FOUND = "[ERROR] 딜러를 확인하지 못했습니다";
+    private static final DecimalFormat REVENUE_FORMAT = new DecimalFormat("#");
 
     private OutputView() {
     }
@@ -77,7 +73,7 @@ public class OutputView {
 
     public static void printCards(CardsDto playerDto) {
         System.out.println(
-                playerDto.getName() + COLON_FOR_JOINING_NAME_AND_CARD + getJoinedCardNames(playerDto.getCards()));
+                playerDto.getName() + COLON_FOR_NAME_AND_REVENUE + getJoinedCardNames(playerDto.getCards()));
     }
 
     public static void printDealerAddCard(Dealer dealer) {
@@ -103,29 +99,9 @@ public class OutputView {
                 .forEach(OutputView::printSingleGamblerResult);
     }
 
-    public static void printSingleGamblerResult(Map.Entry<String, ResultDto> entry) {
-        System.out.println(
-                entry.getKey() + COLON_FOR_JOINING_NAME_AND_CARD + getResultMessageByResultDto(entry.getValue()));
-    }
-
-    private static String getResultMessageByResultDto(ResultDto resultDto) {
-        if (resultDto.isGamblerResult()) {
-            return NameMapper.getResultName(resultDto.getGamblerResult());
-        }
-
-        return getDealerResult(getDealerResultByResultDto(resultDto));
-    }
-
-    private static Map<MatchResult, Long> getDealerResultByResultDto(ResultDto resultDto) {
-        return resultDto.getMatchResults()
-                .stream()
-                .collect(groupingBy(identity(), () -> new EnumMap<>(MatchResult.class), counting()));
-    }
-
-    private static String getDealerResult(Map<MatchResult, Long> dealerResult) {
-        return Arrays.stream(MatchResult.values())
-                .filter(dealerResult::containsKey)
-                .map(matchResult -> dealerResult.get(matchResult) + NameMapper.getResultName(matchResult))
-                .collect(joining(WIN_DRAW_LOSE_DELIMITER));
+    public static void printSingleGamblerResult(Map.Entry<Name, Revenue> entry) {
+        Name name = entry.getKey();
+        Revenue revenue = entry.getValue();
+        System.out.println(name.getName() + COLON_FOR_NAME_AND_REVENUE + REVENUE_FORMAT.format(revenue.getRevenue()));
     }
 }
