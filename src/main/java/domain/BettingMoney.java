1345c9259f2fa4f8c041525aae2d59cde1fb1754
yoon <duddbs774@gmail.com>
2022-03-17 23:25:54 +0900

    feat: BettingMoney 베팅금액 클래스 생성

---
+++ src/main/java/domain/BettingMoney.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class BettingMoney {
+    private final int value;
+
+    public BettingMoney(int number) {
+        this.value = number;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
