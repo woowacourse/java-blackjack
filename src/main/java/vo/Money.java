4828d102e0ef16a2956b96693b6aa0b02ffb24f0
Richard JEON <ztzy1907@gmail.com>
2022-03-17 20:24:17 +0900

    refactor(money): Money 음수 생성시 예외 발생 추가

--- src/main/java/vo/Money.java
+++ src/main/java/vo/Money.java
@@ -1,10 +1,19 @@
 package vo;
 
 public class Money {
+    private static final String ERROR_LESS_THAN_MINIMUM_MONEY = "[ERROR] 배팅액을 1원 이상으로 입력해주세요. ";
+    private static final int MINIMUM_MONEY = 0;
     private final int money;
 
     private Money(int money) {
         this.money = money;
+        validateIsPositive(money);
+    }
+
+    private void validateIsPositive(int money) {
+        if (money < MINIMUM_MONEY) {
+            throw new IllegalArgumentException(ERROR_LESS_THAN_MINIMUM_MONEY + money);
+        }
     }
 
     public static Money from(int money) {
