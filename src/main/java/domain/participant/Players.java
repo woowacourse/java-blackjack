a0839fc8a6bb01dff29f4ae273e5b0cbec209db9
yoon <duddbs774@gmail.com>
2022-03-17 20:30:50 +0900

    refactor: getCardsWithName 메서드 삭제
    - 도메인 객체를 직접 넘겨줘서 필요 없어진 기능

--- src/main/java/domain/participant/Players.java
+++ src/main/java/domain/participant/Players.java
@@ -1,12 +1,9 @@
 package domain.participant;
 
-import domain.card.Card;
 import domain.card.CardDeck;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public final class Players {
@@ -22,14 +19,6 @@ public final class Players {
         for (final Player player : players) {
             player.drawCard(CardDeck.draw());
         }
-    }
-
-    public Map<String, List<Card>> getCardsWithName() {
-        final Map<String, List<Card>> cardsWithNameTotal = new LinkedHashMap<>();
-        for (final Player player : players) {
-            cardsWithNameTotal.putAll(player.getNameWithCards());
-        }
-        return cardsWithNameTotal;
     }
 
     public List<Player> getPlayers() {
