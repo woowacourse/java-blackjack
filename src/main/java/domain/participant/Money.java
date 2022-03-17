b8c38f30f2187e947f70da9c6b83ba2c5e984aad
Hongdonggeon <ghd700@naver.com>
2022-03-17 17:02:40 +0900

    feat: 베팅 금액을 관리하는 클래스 추가
    음수나 0원을 입력할 경우 예외를 발생한다.

---
+++ src/main/java/domain/participant/Money.java
@@ -0,0 +1,20 @@
+package domain.participant;
+
+import utils.ExceptionMessages;
+
+public class Money {
+
+    public static final int ZERO_MONEY = 0;
+    private final int money;
+
+    public Money(final int money) {
+        validatePositiveMoney(money);
+        this.money = money;
+    }
+
+    private void validatePositiveMoney(int money) {
+        if (money <= ZERO_MONEY) {
+            throw new IllegalArgumentException(ExceptionMessages.NOT_POSITIVE_MONEY_ERROR);
+        }
+    }
+}
--- src/main/java/utils/ExceptionMessages.java
+++ src/main/java/utils/ExceptionMessages.java
@@ -4,5 +4,6 @@ public class ExceptionMessages {
 
     public static final String EMPTY_NAME_ERROR = "[ERROR] 이름은 공백이 될 수 없습니다.";
     public static final String OVER_CARD_LIMIT_ERROR = "[ERROR] 카드의 합이 21이 넘게되면 더이상 카드를 뽑을 수 없습니다.";
+    public static final String NOT_POSITIVE_MONEY_ERROR = "[ERROR] 베팅은 0보다 큰 금액으로 할 수 있습니다.";
 
 }
