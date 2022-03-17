fde336dec128ca4faff2f4821c961b3a7975f6c4
yoon <duddbs774@gmail.com>
2022-03-17 20:19:28 +0900

    refactor: 카드 상태 구하는 기능 리펙토링
    - 가독성이 떨어지는 함수적 인터페이스 제거

--- src/main/java/domain/card/CardState.java
+++ src/main/java/domain/card/CardState.java
@@ -1,34 +1,22 @@
 package domain.card;
 
-import java.util.Arrays;
-import java.util.function.Predicate;
-
 public enum CardState {
-    BLACKJACK(CardState::isBlackJack, 3),
-    STAND(cards -> !(isBlackJack(cards)) && cards.calculateSum() <= 21, 2),
-    BUST(cards -> cards.calculateSum() > 21, 1);
+    BLACKJACK,
+    STAND,
+    BUST,
+    ;
 
     private static final int BLACK_JACK_CARD_SCORE = 21;
     private static final int BLACK_JACK_CARD_COUNT = 2;
-    private static final String CANNOT_FIND_CARD_STATE_MESSAGE = "카드 상태를 구할 수 없습니다.";
-
-    private final Predicate<Cards> predicate;
-    private final int statePower;
-
-    CardState(final Predicate<Cards> predicate, final int statePower) {
-        this.predicate = predicate;
-        this.statePower = statePower;
-    }
 
     public static CardState from(final Cards cards) {
-        return Arrays.stream(CardState.values())
-                .filter(cardState -> cardState.predicate.test(cards))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(CANNOT_FIND_CARD_STATE_MESSAGE));
-    }
-
-    private static boolean isBlackJack(final Cards cards) {
-        return cards.getCardsCount() == BLACK_JACK_CARD_COUNT && cards.calculateSum() == BLACK_JACK_CARD_SCORE;
+        if (cards.getCardsCount() == BLACK_JACK_CARD_COUNT && cards.calculateSum() == BLACK_JACK_CARD_SCORE) {
+            return BLACKJACK;
+        }
+        if (cards.calculateSum() > BLACK_JACK_CARD_SCORE) {
+            return BUST;
+        }
+        return STAND;
     }
 
     public boolean isStand() {
@@ -41,9 +29,5 @@ public enum CardState {
 
     public boolean isBlackJack() {
         return this == BLACKJACK;
-    }
-
-    public int getStatePower() {
-        return statePower;
     }
 }
