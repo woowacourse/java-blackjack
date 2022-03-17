ca02baa2aa50c00092fda2e3f81a07feff7748db
woong7 <driedseas@gmail.com>
2022-03-17 19:14:15 +0900

    test: 테스트코드 추가

---
+++ src/test/java/blackjack/domain/ResultTest.java
@@ -0,0 +1,62 @@
+package blackjack.domain;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.participant.Participant;
+import blackjack.domain.participant.Participants;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ResultTest {
+    private Participants participants;
+
+    @BeforeEach
+    void setGame() {
+        List<String> playerNames = Arrays.asList("a", "b");
+        BlackJack blackJack = BlackJack.createFrom(playerNames);
+        participants = blackJack.getParticipants();
+        List<Participant> players = participants.getPlayers();
+        Participant dealer = participants.getDealer();
+
+        players.get(0).bet(1000);
+        players.get(1).bet(1200);
+        participants.handOutCardTo(dealer, new Card("하트8", 8));
+        participants.handOutCardTo(dealer, new Card("하트9", 9));
+        participants.handOutCardTo(players.get(0), new Card("클로버8", 8));
+        participants.handOutCardTo(players.get(0), new Card("하트10", 10));
+        participants.handOutCardTo(players.get(1), new Card("클로버7", 7));
+        participants.handOutCardTo(players.get(1), new Card("스페이드9", 9));
+    }
+
+    @Test
+    @DisplayName("딜러의 수익을 잘 계산하는지")
+    void Calculate_Dealer_Revenue() {
+
+        Result result = new Result(participants);
+        result.calculatePlayersRevenue();
+        assertThat(result.getDealerRevenue()).isEqualTo(200);
+    }
+
+    @Test
+    @DisplayName("플레이어가 승리한 경우 수익을 잘 계산하는지")
+    void Calculate_Winning_Player_Revenue() {
+
+        Result result = new Result(participants);
+        result.calculatePlayersRevenue();
+        assertThat(result.getPlayerRevenue(participants.getPlayers().get(0))).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 패배한 경우 수익을 잘 계산하는지")
+    void Calculate_Losing_Player_Revenue() {
+
+        Result result = new Result(participants);
+        result.calculatePlayersRevenue();
+        assertThat(result.getPlayerRevenue(participants.getPlayers().get(1))).isEqualTo(-1200);
+    }
+}
---
+++ src/test/java/blackjack/domain/participant/ParticipantsTest.java
@@ -0,0 +1,41 @@
+package blackjack.domain.participant;
+
+import blackjack.domain.BlackJack;
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardDeck;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ParticipantsTest {
+    @Test
+    @DisplayName("모든 플레이어에게 한 장씩 카드를 잘 나눠주는지")
+    void Hand_Out_Card_To_All() {
+        List<String> playerNames = Arrays.asList("a", "b");
+        BlackJack blackJack = BlackJack.createFrom(playerNames);
+        Participants participants = blackJack.getParticipants();
+
+        participants.handOutCardToAll(new CardDeck());
+        assertThat(participants.getPlayers().stream()
+            .allMatch(player -> player.getCards().size() == 1))
+            .isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("16 이하 일때 딜러가 한장의 카드를 더 받는지")
+    void Dealer_Receive_Additional_Card_Under_16() {
+        List<String> playerNames = Arrays.asList("a", "b");
+        BlackJack blackJack = BlackJack.createFrom(playerNames);
+        Participants participants = blackJack.getParticipants();
+        Participant dealer = participants.getDealer();
+        dealer.receiveCard(new Card("8스페이드", 8));
+        dealer.receiveCard(new Card("5하트", 5));
+        assertThat(participants.isDealerNeedAdditionalCard()).isEqualTo(true);
+        dealer.receiveCard(new Card("7클로버", 7));
+        assertThat(participants.isDealerNeedAdditionalCard()).isEqualTo(false);
+    }
+}
