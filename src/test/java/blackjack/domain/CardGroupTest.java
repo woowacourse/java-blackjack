39a181d90d0545c938093c280f52ab6a430370dc
dongho <dongho108@naver.com>
2022-03-17 17:34:41 +0900

    feat: 블랙잭 판단기능 추가

--- src/test/java/blackjack/domain/CardGroupTest.java
+++ src/test/java/blackjack/domain/CardGroupTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -32,5 +33,24 @@ class CardGroupTest {
                 Arguments.of(new Card(CardShape.HEART, CardNumber.THREE), 15, "카드 숫자 합 12에서 3 추가시에 15"),
                 Arguments.of(new Card(CardShape.HEART, CardNumber.K), 22, "카드 숫자 합 12에서 10 추가시에 22")
         );
+    }
+
+    @Test
+    @DisplayName("카드의 수가 2개이고, 점수가 21점이면 블랙잭이다.")
+    void isBlackJack() {
+        CardGroup cardGroup = new CardGroup();
+        cardGroup.addCard(new Card(CardShape.HEART, CardNumber.A));
+        cardGroup.addCard(new Card(CardShape.HEART, CardNumber.Q));
+        assertThat(cardGroup.isBlackJack()).isTrue();
+    }
+
+    @Test
+    @DisplayName("카드의 수가 3개이고, 점수가 21점이면 블랙잭이 아니다.")
+    void isNotBlackJack() {
+        CardGroup cardGroup = new CardGroup();
+        cardGroup.addCard(new Card(CardShape.HEART, CardNumber.K));
+        cardGroup.addCard(new Card(CardShape.HEART, CardNumber.NINE));
+        cardGroup.addCard(new Card(CardShape.HEART, CardNumber.TWO));
+        assertThat(cardGroup.isBlackJack()).isFalse();
     }
 }
