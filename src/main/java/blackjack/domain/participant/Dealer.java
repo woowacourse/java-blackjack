71d530d08afa3ea861c6d09cbd3f03c70e3f5ae8
sure-why-not <pkse1213@gmail.com>
2022-03-18 03:45:46 +0900

    refactor: 딜러 이름을 상수로 선언

--- src/main/java/blackjack/domain/participant/Dealer.java
+++ src/main/java/blackjack/domain/participant/Dealer.java
@@ -7,11 +7,12 @@ import blackjack.domain.card.Cards;
 
 public class Dealer extends Participant {
 
+    private static final String DEALER_NAME = "딜러";
     private static final int HIT_STANDARD = 17;
     private static final int FIRST_OPEN_CARD_SIZE = 1;
 
     public Dealer(Cards cards) {
-        super(new Name("딜러"), cards);
+        super(new Name(DEALER_NAME), cards);
     }
 
     @Override
