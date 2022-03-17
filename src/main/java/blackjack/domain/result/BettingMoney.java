4b59bc11e992c30e73e0f9d783276a2140271f8e
TaeHyeon Kim <kth990303@naver.com>
2022-03-17 18:34:48 +0900

    feat: 배팅 금액 VO 생성

---
+++ src/main/java/blackjack/domain/result/BettingMoney.java
@@ -0,0 +1,24 @@
+package blackjack.domain.result;
+
+public class BettingMoney {
+
+    private static final String ERROR_MESSAGE_INVALID_MONEY = "배팅 금액을 넣어주세요.";
+    private static final int MONEY_ZERO = 0;
+
+    private final int money;
+
+    public BettingMoney(int money) {
+        checkValidProfit(money);
+        this.money = money;
+    }
+
+    private static void checkValidProfit(int money) {
+        if (money <= MONEY_ZERO) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_MONEY);
+        }
+    }
+
+    public double calculateProfit(double profitRatio) {
+        return money * profitRatio;
+    }
+}
---
+++ src/test/java/blackjack/domain/result/BettingMoneyTest.java
@@ -0,0 +1,17 @@
+package blackjack.domain.result;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BettingMoneyTest {
+
+    @ParameterizedTest(name = "플레이어 금액 범위 검증 테스트")
+    @ValueSource(ints = {-1, 0})
+    void checkValidMoney(int value) {
+        assertThatThrownBy(() -> new BettingMoney(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("배팅 금액을 넣어주세요.");
+    }
+}
