6d8807b83263e39fcfc56612119fe49b49ca79cd
Juhyung990122 <harukawa99@naver.com>
2022-03-18 18:48:00 +0900

    feat: 배팅금액 음수 유효성 검사 추가

--- src/main/java/BlackJack/domain/User/BettingMoney.java
+++ src/main/java/BlackJack/domain/User/BettingMoney.java
@@ -1,10 +1,19 @@
 package blackJack.domain.User;
 
+import blackJack.utils.ExeptionMessage;
+
 public class BettingMoney {
+    public static final int NO_MONEY = 0;
     private int money;
 
     public BettingMoney(int money) {
         this.money = money;
+    }
+
+    private void validatePositive(int money){
+        if(money <= NO_MONEY){
+            throw new IllegalArgumentException(ExeptionMessage.NOT_ENOUGH_BETTING_MONEY);
+        }
     }
 
     public int getMoney() {
--- src/main/java/blackJack/utils/ExeptionMessage.java
+++ src/main/java/blackJack/utils/ExeptionMessage.java
@@ -5,4 +5,5 @@ public class ExeptionMessage {
     public static final String INPUT_IS_BLANK = "[ERROR] 입력에 빈칸이 있을 수 없습니다.";
     public static final String INPUT_IS_WRONG_FORMAT = "[ERROR] 입력은 y 혹은 n 이어야 합니다.";
     public static final String INPUT_IS_NOT_NUMBER = "[ERROR] 베팅금액은 숫자여야합니다.";
+    public static final String NOT_ENOUGH_BETTING_MONEY = "[ERROR] 배팅금액은 1이상의 양수만 가능합니다.";
 }
