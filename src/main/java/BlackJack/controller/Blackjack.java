31b3027af27430dc3925186a94f2fdfe67b9ed03
Juhyung990122 <harukawa99@naver.com>
2022-03-18 17:49:56 +0900

    refactor: Game 도메인 삭제

--- src/main/java/BlackJack/controller/Blackjack.java
+++ src/main/java/BlackJack/controller/Blackjack.java
@@ -1,6 +1,8 @@
 package blackJack.controller;
 
-import blackJack.domain.Game;
+import blackJack.domain.Card.Cards;
+import blackJack.domain.Result.DealerScore;
+import blackJack.domain.Result.PlayerScore;
 import blackJack.domain.User.Dealer;
 import blackJack.domain.User.Player;
 import blackJack.domain.User.Players;
@@ -17,27 +19,43 @@ import static blackJack.domain.Card.CardFactory.CARD_CACHE;
 
 public class Blackjack {
 
+    private static final int HAND_OUT_COUNT = 2;
+
     public void run() {
+        final Dealer dealer = new Dealer();
+        final Cards cards = new Cards(CARD_CACHE);
+        PlayerScore playerScore = new PlayerScore();
+        DealerScore dealerScore = new DealerScore();
         List<String> playerNames = InputView.inputPlayerNames();
         Map<String, Integer> bettingMoneys = getBettingMoneys(playerNames);
-        Game game = new Game(playerNames, new Dealer(), bettingMoneys);
-        OutputView.printDrawMessage(playerNames);
-        OutputView.printTotalUserCards(game.getDealer(), game.getPlayers());
+        final Players players = new Players(playerNames, bettingMoneys);
 
-        if (game.checkDealerIsBlackJack()) {
-            printBlackJackResult(game);
+        handOutInitCard(dealer, players);
+
+        OutputView.printDrawMessage(playerNames);
+        OutputView.printTotalUserCards(dealer, players);
+
+        if (checkDealerIsBlackJack(dealer)) {
+            playerScore.makeBlackjackResult(players);
+            dealerScore.makeDealerResult(playerScore);
+            printFinalResult(dealer, playerScore, dealerScore);
             return;
         }
 
-        playGame(game.getDealer(), game.getPlayers());
-        OutputView.printTotalResult(makeUserList(game));
+        playGame(dealer, players);
+        OutputView.printTotalResult(makeUserList(dealer, players));
 
-        game.makeFinalResult();
+        playerScore = makeFinalPlayersResult(dealer, players);
+        dealerScore = makeFinalDealerResult(playerScore);
 
+        printFinalResult(dealer, playerScore, dealerScore);
+    }
+
+    private void printFinalResult(Dealer dealer, PlayerScore playerScore, DealerScore dealerScore) {
         OutputView.printFinalResult(
-                game.getDealer().getName(),
-                game.getDealerScore(),
-                game.getPlayerScore()
+                dealer.getName(),
+                dealerScore,
+                playerScore
         );
     }
 
@@ -49,20 +67,13 @@ public class Blackjack {
         return bettingMoneys;
     }
 
-    private List<User> makeUserList(Game game) {
+    private List<User> makeUserList(Dealer dealer, Players players) {
         List<User> users = new ArrayList<>();
-        users.add(game.getDealer());
-        for (Player player : game.getPlayers().getPlayers()) {
+        users.add(dealer);
+        for (Player player : players.getPlayers()) {
             users.add(player);
         }
         return users;
-    }
-
-    private void printBlackJackResult(Game game) {
-        OutputView.printFinalResult(
-                game.getDealer().getName(),
-                game.getDealerScore(),
-                game.getPlayerScore());
     }
 
     private void playGame(Dealer dealer, Players players) {
@@ -80,5 +91,31 @@ public class Blackjack {
             player.requestCard(CARD_CACHE.poll());
             OutputView.printPlayerCard(player);
         }
+    }
+
+    private void handOutInitCard(Dealer dealer, Players players) {
+        for (int i = 0; i < HAND_OUT_COUNT; i++) {
+            dealer.dealCard(CARD_CACHE.poll());
+            players.dealCardToPlayers();
+        }
+    }
+
+    public PlayerScore makeFinalPlayersResult(Dealer dealer, Players players) {
+        PlayerScore playerScore = new PlayerScore();
+        playerScore.makePlayerResult(dealer, players);
+        return playerScore;
+    }
+
+    public DealerScore makeFinalDealerResult(PlayerScore playerScore) {
+        DealerScore dealerScore = new DealerScore();
+        dealerScore.makeDealerResult(playerScore);
+        return dealerScore;
+    }
+
+    public boolean checkDealerIsBlackJack(Dealer dealer) {
+        if (dealer.isBlackJack()) {
+            return true;
+        }
+        return false;
     }
 }
