20331dc9cf6cbe4762c50d8631ad6ae7474513a8
DOES <ldk980130@gmail.com>
2022-03-18 18:32:31 +0900

    test: 람다 파라미터 의미 있는 이름으로 변경

--- src/test/java/blackjack/domain/BlackJackGameTest.java
+++ src/test/java/blackjack/domain/BlackJackGameTest.java
@@ -10,21 +10,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import blackjack.domain.card.Card;
-import blackjack.domain.card.CardNumber;
-import blackjack.domain.card.CardShape;
 import blackjack.domain.card.Deck;
 import blackjack.domain.game.BlackJackGame;
+import blackjack.domain.game.BlackJackReferee;
 import blackjack.domain.gamer.Gamers;
 import blackjack.domain.gamer.Player;
-import blackjack.domain.game.BlackJackReferee;
 
 class BlackJackGameTest {
+
     @Test
     @DisplayName("플레이어에게 게임 시작 시 2장씩 배분한다.")
     void initDistribution() {
         BlackJackGame blackJackGame = new BlackJackGame(
-            Arrays.asList("a", "b"), s -> 10, new Deck(Card.getCards()));
-        blackJackGame.play(answer -> false, (s, c) -> {});
+            Arrays.asList("pobi", "jason"), betting -> 1000, new Deck(Card.getCards()));
+
+        blackJackGame.play(hitRequest -> false, (name, cards) -> {});
 
         Gamers gamers = blackJackGame.getGamers();
         List<Player> players = gamers.getPlayers();
@@ -37,8 +37,10 @@ class BlackJackGameTest {
     @Test
     @DisplayName("딜러의 점수가 17이상일 때 까지 카드를 1장씩 받는다.")
     void dealerDistribution() {
-        BlackJackGame blackJackGame = new BlackJackGame(List.of("name"), s -> 10, new Deck(Card.getCards()));
-        blackJackGame.play(answer -> false, (s, c) -> {});
+        BlackJackGame blackJackGame = new BlackJackGame(
+            List.of("name"), betting -> 10, new Deck(Card.getCards()));
+
+        blackJackGame.play(hitRequest -> false, (name, cards) -> {});
 
         Gamers gamers = blackJackGame.getGamers();
         int cardNumberSum = gamers.getDealer().sumCardsNumber();
@@ -50,9 +52,9 @@ class BlackJackGameTest {
     @DisplayName("1000원 배팅일 때 지면 수익 -1000원")
     void createResultLose() {
         BlackJackGame blackJackGame = new BlackJackGame(
-            List.of("name"), s -> 1000, new Deck(Card.getCards()));
+            List.of("name"), betting -> 1000, new Deck(Card.getCards()));
 
-        BlackJackReferee result = blackJackGame.play(answer -> true, (s, c) -> {});
+        BlackJackReferee result = blackJackGame.play(hitRequest -> true, (name, cards) -> {});
 
         int dealerEarning = result.getDealerEarning();
         Map<String, Integer> playerEarnings = result.getPlayerEarnings();
