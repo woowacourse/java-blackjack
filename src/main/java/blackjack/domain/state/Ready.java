fe96f4c520184440bf310c37544d4f965cf7d6af
yukong <kyb062666@gmail.com>
2022-03-18 19:09:00 +0900

    refactor: Ready 생성자에서 Betting 을 삭제하여 그에 맞는 검증 추가
    - bet 메소드를 호출하지 않고 다른 상태로 바뀔 경우 null 이 전달될 수 있으므로 검증 로직 추가

--- src/main/java/blackjack/domain/state/Ready.java
+++ src/main/java/blackjack/domain/state/Ready.java
@@ -7,14 +7,21 @@ import blackjack.domain.game.PlayingCards;
 public class Ready implements State {
 
     private final PlayingCards playingCards = new PlayingCards();
-    private final Betting betting;
+    private Betting betting;
 
-    public Ready(final Betting betting) {
+    public void bet(final Betting betting) {
         this.betting = betting;
+    }
+
+    private void validateBetting() {
+        if (betting == null) {
+            throw new IllegalStateException("베팅 금액을 입력 해야 합니다.");
+        }
     }
 
     @Override
     public State draw(final Card card) {
+        validateBetting();
         playingCards.add(card);
 
         if (playingCards.isMoreDeal()) {
@@ -28,6 +35,7 @@ public class Ready implements State {
 
     @Override
     public State stay() {
+        validateBetting();
         return new Stay(playingCards, betting);
     }
 
--- src/test/java/blackjack/domain/state/ReadyTest.java
+++ src/test/java/blackjack/domain/state/ReadyTest.java
@@ -7,9 +7,8 @@ import blackjack.domain.card.Suit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReadyTest {
@@ -19,37 +18,63 @@ class ReadyTest {
     @DisplayName("draw 를 한 번 실행하여 Ready 상태가 되는 것을 확인한다.")
     @Test
     void draw_ready() {
-        State ready = new Ready(betting);
+        Ready ready = new Ready();
+        ready.bet(betting);
 
-        assertThat(ready.draw(Card.of(Denomination.KING, Suit.SPADE))).isInstanceOf(Ready.class);
+        assertThat(ready.draw(Card.of(Denomination.KING, Suit.SPADE)))
+                .isInstanceOf(Ready.class);
     }
 
     @DisplayName("draw 를 두 번 실행하여 Hit 상태가 되는 것을 확인한다.")
     @Test
     void draw_hit() {
-        State ready = new Ready(betting);
-        ready = ready.draw(Card.of(Denomination.KING, Suit.SPADE));
+        Ready ready = new Ready();
+        ready.bet(betting);
 
-        assertThat(ready.draw(Card.of(Denomination.KING, Suit.SPADE))).isInstanceOf(Hit.class);
+        assertThat(ready.draw(Card.of(Denomination.KING, Suit.SPADE))
+                .draw(Card.of(Denomination.KING, Suit.SPADE)))
+                .isInstanceOf(Hit.class);
     }
 
     @DisplayName("draw 를 두 번 실행하여 Blackjack 상태가 되는 것을 확인한다.")
     @Test
     void draw_blackjack() {
-        State ready = new Ready(betting);
-        ready = ready.draw(Card.of(Denomination.KING, Suit.SPADE));
+        Ready ready = new Ready();
+        ready.bet(betting);
 
-        assertThat(ready.draw(Card.of(Denomination.ACE, Suit.SPADE))).isInstanceOf(Blackjack.class);
+        assertThat(ready.draw(Card.of(Denomination.ACE, Suit.SPADE))
+                .draw(Card.of(Denomination.KING, Suit.SPADE)))
+                .isInstanceOf(Blackjack.class);
+    }
+
+    @DisplayName("draw 시 베팅 금액을 입력하지 않았을 경우 예외가 발생하는 것을 확인한다.")
+    @Test
+    void draw_exception() {
+        Ready ready = new Ready();
+
+        assertThatThrownBy(() -> ready.draw(Card.of(Denomination.KING, Suit.SPADE)))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("베팅 금액을 입력 해야 합니다.");
     }
 
     @DisplayName("stay 를 실행하여 Stay 상태가 되는 것을 확인한다.")
     @Test
     void stay() {
-        State ready = new Ready(betting);
-        ready = ready.draw(Card.of(Denomination.KING, Suit.SPADE));
+        Ready ready = new Ready();
+        ready.bet(betting);
 
         State state = ready.stay();
 
         assertThat(state).isInstanceOf(Stay.class);
+    }
+
+    @DisplayName("stay 시 베팅 금액을 입력하지 않았을 경우 예외가 발생하는 것을 확인한다.")
+    @Test
+    void stay_exception() {
+        Ready ready = new Ready();
+
+        assertThatThrownBy(ready::stay)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("베팅 금액을 입력 해야 합니다.");
     }
 }
