a54f50272e56130bf54559fa02feec5610f74569
유영우 <fbduddn97@naver.com>
2022-03-18 18:13:21 +0900

    test: 블랙잭일 경우 테스트 추가

--- src/test/java/blackjack/domain/ProfitResultTest.java
+++ src/test/java/blackjack/domain/ProfitResultTest.java
@@ -2,6 +2,7 @@ package blackjack.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import blackjack.domain.card.Card;
 import blackjack.domain.machine.Blackjack;
 import blackjack.domain.machine.Profit;
 import blackjack.domain.machine.ProfitResult;
@@ -24,7 +25,7 @@ public class ProfitResultTest {
     @BeforeEach
     void setUp() {
         Map<String, Long> playersInfo = new LinkedHashMap<>();
-        playersInfo.put("범블비",10000L);
+        playersInfo.put("범블비", 10000L);
 
         players = new Players(playersInfo);
         dealer = new Dealer();
@@ -41,26 +42,50 @@ public class ProfitResultTest {
     void playerProfit() {
         Map<Participant, Profit> result = profitResult.getResult();
 
-        assertThat(result.get(new Player("범블비", 0L))).isEqualTo(new Profit(10000L));
+        assertThat(result.get(new Player("범블비", 0L)))
+                .isEqualTo(new Profit(10000L));
     }
 
     @DisplayName("플레이어의 수익률 결과를 테스트")
     @Test
     void dealerProfit() {
-        Map<Participant,Profit> result = profitResult.getResult();
+        Map<Participant, Profit> result = profitResult.getResult();
 
-        assertThat(result.get(new Dealer())).isEqualTo(new Profit(-10000L));
+        assertThat(result.get(new Dealer()))
+                .isEqualTo(new Profit(-10000L));
     }
 
     @DisplayName("플레이어가 블랙잭인 경우 수익률 테스트")
     @Test
     void OnlyPlayerBlackjack() {
+        Player player = players.getPlayers().get(0);
+        player.addCard(Card.A_CLOVER);
+        player.addCard(Card.J_CLOVER);
 
+        Dealer dealer = new Dealer();
+        dealer.addCard(Card.EIGHT_CLOVER);
+        dealer.addCard(Card.FIVE_CLOVER);
+
+        ProfitResult profitResult = ProfitResult.of(dealer, players);
+
+        assertThat(profitResult.getResult().get(player))
+                .isEqualTo(new Profit(15000L));
     }
 
     @DisplayName("플레이어와 딜러가 모두 블랙잭인 경우 수익률 테스트")
     @Test
     void alsoDealerBlackjack() {
+        Player player = players.getPlayers().get(0);
+        player.addCard(Card.A_CLOVER);
+        player.addCard(Card.J_CLOVER);
 
+        Dealer dealer = new Dealer();
+        dealer.addCard(Card.A_SPADE);
+        dealer.addCard(Card.J_SPADE);
+
+        ProfitResult profitResult = ProfitResult.of(dealer, players);
+
+        assertThat(profitResult.getResult().get(player))
+                .isEqualTo(new Profit(10000L));
     }
 }
