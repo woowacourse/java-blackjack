d67c3899c00872b45437a63b7c8fcfe2c5a980a8
김찬 <gmelddl514@gmail.com>
2022-03-18 20:00:15 +0900

    feat : 블랙잭 체크 로직 추가

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -13,7 +13,6 @@ import blackjack.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class BlackJackController {
 
@@ -30,24 +29,33 @@ public class BlackJackController {
         Players players = initializePlayers(InputView.inputPlayerNames());
         OutputView.printInitialCards(players, dealer);
 
-        playAllPlayers(players);
-        playDealer(dealer);
+        if (!isGameEnd(players, dealer)) {
+            playAllPlayers(players);
+            playDealer(dealer);
+        }
 
         showGameResult(players, dealer);
     }
 
     private Players initializePlayers(List<Name> names) {
-        return new Players(names.stream()
-                .map(name -> new Player(name, drawInitialCards()))
-                .collect(Collectors.toUnmodifiableList()));
+        List<Player> players = new ArrayList<>();
+        for (int count = 0; count < names.size(); count++) {
+            Name playerName = names.get(count);
+            players.add(new Player(playerName, drawInitialCards(), InputView.inputPlayerMoney(playerName.getValue())));
+        }
+        return new Players(players);
     }
-
+    
     private Cards drawInitialCards() {
         List<Card> cards = new ArrayList<>();
         for (int i = 0; i < INITIAL_DRAW_CARD_COUNT; i++) {
             cards.add(cardDistributor.distribute());
         }
         return new Cards(cards);
+    }
+
+    private boolean isGameEnd(Players players, Dealer dealer) {
+        return dealer.isBlackJack() || players.isAllBlackJack();
     }
 
     private void playAllPlayers(Players players) {
@@ -65,21 +73,12 @@ public class BlackJackController {
     }
 
     private void decideContinueToPlay(Players players) {
-        boolean isDrawable = wantDraw(players.findPresentPlayer());
+        boolean isDrawable = InputView.inputWantDraw(players.findPresentPlayer());
         if (isDrawable) {
             players.drawCardPresentPlayer(cardDistributor.distribute());
             return;
         }
         players.makePresentPlayerStay();
-    }
-
-    private boolean wantDraw(Player player) {
-        try {
-            return InputView.inputWantDraw(player.getName());
-        } catch (IllegalArgumentException e) {
-            OutputView.printException(e);
-            return wantDraw(player);
-        }
     }
 
     private void playDealer(Dealer dealer) {
