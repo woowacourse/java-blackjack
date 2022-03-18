322ee9e0926e97463b12873b55e4b8a1fa646b67
Ohzzi <clover7even@naver.com>
2022-03-19 00:56:29 +0900

    feat: Stand 상태 세부 사항 구현

--- src/main/java/blackjack/domain/state/Stand.java
+++ src/main/java/blackjack/domain/state/Stand.java
@@ -4,17 +4,20 @@ import blackjack.domain.card.Card;
 import blackjack.domain.card.Cards;
 
 public class Stand implements State {
-    public Stand(Cards cards) {
-        
+
+    private final Cards cards;
+
+    Stand(Cards cards) {
+        this.cards = cards;
     }
 
     @Override
     public State hit(Card card) {
-        return null;
+        throw new UnsupportedOperationException("[ERROR] 게임을 진행할 수 없습니다.");
     }
 
     @Override
     public State stand() {
-        return null;
+        throw new UnsupportedOperationException("[ERROR] 게임을 진행할 수 없습니다.");
     }
 }
