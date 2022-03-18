ed83024a5fb44af7d84e2877e019aa72b7b1a94a
jochungbeom <cndqjacndqja@gmail.com>
2022-03-18 17:07:37 +0900

    test: Dealer 케이스 추가

--- src/test/java/blackjack/domain/gamer/DealerTest.java
+++ src/test/java/blackjack/domain/gamer/DealerTest.java
@@ -3,6 +3,7 @@ package blackjack.domain.gamer;
 import blackjack.domain.card.Card;
 import blackjack.domain.card.CardNumber;
 import blackjack.domain.card.CardShape;
+import blackjack.domain.card.Deck;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,11 +14,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DealerTest {
 
     @Test
-    @DisplayName("보유 카드 번호 합이 특정 숫자를 넘었는지 확인")
+    @DisplayName("보유 카드 번호 합이 16을 초과하는지 확인한다.")
     void checkCardsNumberSum() {
         Card card1 = new Card(CardShape.SPADE, CardNumber.TEN);
         Card card2 = new Card(CardShape.SPADE, CardNumber.FIVE);
         Dealer dealer = new Dealer(List.of(card1, card2));
         assertThat(dealer.canDraw()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("보유 카드 번호의 합이 16을 초과할 때 까지 카드를 뽑는다.")
+    void distribute() {
+        Deck deck = new Deck();
+        Dealer dealer = new Dealer(List.of(deck.draw(), deck.draw()));
+        dealer.distribute(deck);
+        assertThat(dealer.getCardsNumberSum()).isGreaterThan(16);
     }
 }
