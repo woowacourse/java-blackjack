f2e9d7b0516270712ac564e3017e0f596ecf3671
hyunrrr <whgusrms96@gmail.com>
2022-03-18 11:37:52 +0900

    feat: 캐시 추가

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -1,15 +1,23 @@
 package blackjack.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Card {
+    private static final Map<BlackjackCardType, Card> CACHE = new HashMap<>();
+
     private final BlackjackCardType type;
 
     private Card(BlackjackCardType type) {
         this.type = type;
+        CACHE.put(type, this);
     }
 
     public static Card generateCard(BlackjackCardType type) {
+        if (CACHE.containsKey(type)) {
+            return CACHE.get(type);
+        }
         return new Card(type);
     }
 
