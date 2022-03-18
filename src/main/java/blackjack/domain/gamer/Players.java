c9af3c0129eda3be86582f93583e85dfeac1d88a
jochungbeom <cndqjacndqja@gmail.com>
2022-03-18 22:19:30 +0900

    feat: Players 인원 검증 로직 구현

--- src/main/java/blackjack/domain/gamer/Players.java
+++ src/main/java/blackjack/domain/gamer/Players.java
@@ -1,22 +1,30 @@
 package blackjack.domain.gamer;
 
-import blackjack.domain.card.Card;
-import blackjack.domain.card.Deck;
-
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-// 외부에서 만들어서 넣어주자. 이것의 장점과 단점에 대해서 생각해보자.
 public class Players {
     private static final String DUPLICATION_NAME_ERROR = "중복된 이름이 존재합니다.";
+    private static final int MIN_PLAYER_SIZE = 2;
+    private static final int MAX_PLAYER_SIZE = 8;
 
     private final List<Player> players;
 
     public Players(List<Player> players) {
         validateDuplicationNames(players);
+        validatePlayerSize(players);
         this.players = players;
+    }
+
+    private void validatePlayerSize(List<Player> players) {
+        if (isSuitableSize(players)) {
+            throw new IllegalArgumentException("플레이어의 인원은 최소 2인 ~ 최대 8인 입니다.");
+        }
+    }
+
+    private boolean isSuitableSize(List<Player> players) {
+        return players.size() < MIN_PLAYER_SIZE || players.size() > MAX_PLAYER_SIZE;
     }
 
     private void validateDuplicationNames(List<Player> players) {
--- src/test/java/blackjack/domain/gamer/PlayersTest.java
+++ src/test/java/blackjack/domain/gamer/PlayersTest.java
@@ -21,4 +21,34 @@ class PlayersTest {
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("중복된 이름이 존재합니다.");
     }
+
+    @Test
+    @DisplayName("플레이어의 인원이 2명 미만일 경우, 예외가 발생한다.")
+    void validatePlayerMinSize() {
+        Card card = new Card(CardShape.CLOVER, CardNumber.FIVE);
+        Player player = new Player("범고래", List.of(card), 2000);
+        Assertions.assertThatThrownBy(() -> {
+                    new Players(List.of(player));
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("플레이어의 인원은 최소 2인 ~ 최대 8인 입니다.");
+    }
+
+    @Test
+    @DisplayName("플레이어의 인원이 8명 초과일 경우, 예외가 발생한다.")
+    void validatePlayerMaxSize() {
+        Card card = new Card(CardShape.CLOVER, CardNumber.FIVE);
+        Player player1 = new Player("범고래1", List.of(card), 2000);
+        Player player2 = new Player("범고래2", List.of(card), 2000);
+        Player player3 = new Player("범고래3", List.of(card), 2000);
+        Player player4 = new Player("범고래4", List.of(card), 2000);
+        Player player5 = new Player("범고래5", List.of(card), 2000);
+        Player player6 = new Player("범고래6", List.of(card), 2000);
+        Player player7 = new Player("범고래7", List.of(card), 2000);
+        Player player8 = new Player("범고래8", List.of(card), 2000);
+        Player player9 = new Player("범고래9", List.of(card), 2000);
+        Assertions.assertThatThrownBy(() -> {
+                    new Players(List.of(player1, player2, player3, player4, player5, player6, player7, player8, player9));
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("플레이어의 인원은 최소 2인 ~ 최대 8인 입니다.");
+    }
 }
