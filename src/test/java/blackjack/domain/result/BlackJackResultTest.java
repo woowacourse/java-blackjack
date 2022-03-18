55b7cf105392c0f15ebeb9e4b20e806a9eb05b15
jochungbeom <cndqjacndqja@gmail.com>
2022-03-18 17:08:11 +0900

    test: BlackJackResultTest 케이스 추가

--- src/test/java/blackjack/domain/result/BlackJackResultTest.java
+++ src/test/java/blackjack/domain/result/BlackJackResultTest.java
@@ -13,16 +13,37 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BlackJackResultTest {
-
     @Test
     @DisplayName("플레이어와 딜러의 점수를 입력 받아, 둘 다 버스트가 아니면서 플레이어가 승리")
-    void of() {
+    void playerWinNotBust() {
         List<Card> dealerCards = List.of(new Card(CardShape.DIAMOND, CardNumber.THREE), new Card(CardShape.CLOVER, CardNumber.EIGHT));
         List<Card> playerCards = List.of(new Card(CardShape.CLOVER, CardNumber.TWO), new Card(CardShape.CLOVER, CardNumber.TEN));
         Player player = new Player("범고래", playerCards, 1000);
         Dealer dealer = new Dealer(dealerCards);
         BlackJackResult blackJackResult = BlackJackResult.of(player, dealer);
         assertThat(blackJackResult).isEqualTo(BlackJackResult.valueOf("WIN"));
+    }
+
+    @Test
+    @DisplayName("플레이어가 블랙잭이고, 딜러가 블랙잭이 아닐 경우 플레이어는 블랙잭 승리를 한다.")
+    void playerBlackJackWin() {
+        List<Card> dealerCards = List.of(new Card(CardShape.DIAMOND, CardNumber.THREE), new Card(CardShape.CLOVER, CardNumber.EIGHT));
+        List<Card> playerCards = List.of(new Card(CardShape.CLOVER, CardNumber.ACE), new Card(CardShape.CLOVER, CardNumber.TEN));
+        Player player = new Player("범고래", playerCards, 1000);
+        Dealer dealer = new Dealer(dealerCards);
+        BlackJackResult blackJackResult = BlackJackResult.of(player, dealer);
+        assertThat(blackJackResult).isEqualTo(BlackJackResult.valueOf("BLACK_JACK_WIN"));
+    }
+
+    @Test
+    @DisplayName("플레이어와 딜러 모두 블랙잭일 경우, 무승부다.")
+    void draw() {
+        List<Card> dealerCards = List.of(new Card(CardShape.DIAMOND, CardNumber.ACE), new Card(CardShape.CLOVER, CardNumber.TEN));
+        List<Card> playerCards = List.of(new Card(CardShape.CLOVER, CardNumber.ACE), new Card(CardShape.CLOVER, CardNumber.TEN));
+        Player player = new Player("범고래", playerCards, 1000);
+        Dealer dealer = new Dealer(dealerCards);
+        BlackJackResult blackJackResult = BlackJackResult.of(player, dealer);
+        assertThat(blackJackResult).isEqualTo(BlackJackResult.valueOf("DRAW"));
     }
 
     @Test
@@ -39,11 +60,12 @@ class BlackJackResultTest {
     @Test
     @DisplayName("점수가 21점으로 같아도, 블랙잭이면 승리한다.")
     void blackJack() {
-        List<Card> dealerCards = List.of(new Card(CardShape.DIAMOND, CardNumber.THREE), new Card(CardShape.CLOVER, CardNumber.EIGHT));
-        List<Card> playerCards = List.of(new Card(CardShape.CLOVER, CardNumber.TWO), new Card(CardShape.CLOVER, CardNumber.TEN));
+        List<Card> dealerCards = List.of(new Card(CardShape.DIAMOND, CardNumber.TEN), new Card(CardShape.CLOVER, CardNumber.TEN),
+                new Card(CardShape.CLOVER, CardNumber.ACE));
+        List<Card> playerCards = List.of(new Card(CardShape.CLOVER, CardNumber.ACE), new Card(CardShape.CLOVER, CardNumber.TEN));
         Player player = new Player("범고래", playerCards, 1000);
         Dealer dealer = new Dealer(dealerCards);
         BlackJackResult blackJackResult = BlackJackResult.of(player, dealer);
-        assertThat(blackJackResult).isEqualTo(BlackJackResult.valueOf("WIN"));
+        assertThat(blackJackResult).isEqualTo(BlackJackResult.valueOf("BLACK_JACK_WIN"));
     }
 }
