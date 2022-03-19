545dfa0b042a411a8b686efd5227f49ba5a31450
soominsohn <soominsohn@gmail.com>
2022-03-19 11:30:51 +0900

    test(BettingMoney): add betting money test case

--- src/test/java/blackjack/domain/betting/BettingMoneyTest.java
+++ src/test/java/blackjack/domain/betting/BettingMoneyTest.java
@@ -15,17 +15,30 @@ public class BettingMoneyTest {
         BettingMoney bettingMoney = new BettingMoney(1000);
 
         assertThat(bettingMoney.getBettingMoney()).isEqualTo(1000);
-
     }
 
     @Test
-    @DisplayName("배팅금액이 0이하이면 예외가 발생한다.")
-    void createBettingMoneyNegativeFail() {
-        assertThatThrownBy(() -> new BettingMoney(-1000))
+    @DisplayName("배팅금액이 100미만이면 예외가 발생한다.")
+    void createBettingMoneyNegativeFail1() {
+        assertThatThrownBy(() -> new BettingMoney(99))
             .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("[ERROR] 배팅 금액에는 0이하의 값은 올 수 없습니다.");
+            .hasMessage("[ERROR] 배팅 금액에는 100미만의 값은 올 수 없습니다.");
     }
 
+    @Test
+    @DisplayName("배팅금액이 100미만이면 예외가 발생한다.")
+    void createBettingMoneyNegativeFail2() {
+        assertThatThrownBy(() -> new BettingMoney(-1))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 배팅 금액에는 100미만의 값은 올 수 없습니다.");
+    }
 
+    @Test
+    @DisplayName("배팅금액이 100으로 나누어떨어지지 않으면 예외가 발생한다.")
+    void createBettingMoneyNegativeFail() {
+        assertThatThrownBy(() -> new BettingMoney(1111))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 배팅 금액은 100원 단위여야 합니다.");
+    }
 
 }
