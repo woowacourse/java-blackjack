073b296cd5d98ec2346e743882eaa6e5e845e682
corinne <yoo77hyeon@gmail.com>
2022-03-19 14:03:22 +0900

    feat: Money validate 추가

--- src/main/java/blackjack/domain/betting/Money.java
+++ src/main/java/blackjack/domain/betting/Money.java
@@ -4,14 +4,23 @@ import java.util.Objects;
 
 public class Money {
 
+    public static final int MINIMUM = 1000;
+
     private final int betting;
 
     public Money(int betting) {
+        validateBetting(betting);
         this.betting = betting;
     }
 
     public int betting() {
         return betting;
+    }
+
+    private void validateBetting(int betting) {
+        if (betting < MINIMUM) {
+            throw new IllegalArgumentException("[ERROR] 배팅 금액은 " + MINIMUM + "이상이어야 합니다.");
+        }
     }
 
     @Override
--- src/test/java/blackjack/domain/betting/MoneyTest.java
+++ src/test/java/blackjack/domain/betting/MoneyTest.java
@@ -7,6 +7,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 public class MoneyTest {
 
-
-
+    @Test
+    @DisplayName("배팅금액은 1000원 이상이어야 한다.")
+    void thrownWhenMoneyUnderMinimum() {
+        assertThatThrownBy(() -> new Money(999))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 배팅 금액은 1000이상이어야 합니다.");
+    }
 }
