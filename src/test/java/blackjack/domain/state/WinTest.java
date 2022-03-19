5713ab6b4e9b636e9c460abff9e1ca7b01c5e66c
yxxnghwan <younghwan960@gmail.com>
2022-03-19 15:34:02 +0900

    test(blackjack): 스탠드 상태에서 상대가 버스트면 무조건 승리하는지 테스트 코드 작성

---
+++ src/test/java/blackjack/domain/state/WinTest.java
@@ -0,0 +1,32 @@
+package blackjack.domain.state;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Denomination;
+import blackjack.domain.card.Suit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinTest {
+    @Test
+    @DisplayName("블랙잭이 아니면서 상대보다 점수가 높으면 Win 상태가 되는지 확인한다.")
+    void standWin() {
+        State stand1 = Ready.start(new Card(Denomination.JACK, Suit.CLUBS), new Card(Denomination.TEN, Suit.CLUBS))
+                .stand();
+        State stand2 = Ready.start(new Card(Denomination.JACK, Suit.SPADES), new Card(Denomination.NINE, Suit.SPADES))
+                .stand();
+        assertThat(stand1.judge(stand2)).isInstanceOf(Win.class);
+    }
+
+    @Test
+    @DisplayName("블랙잭이 아니면서 상대가 버스트면 Win 상태가 되는지 확인한다.")
+    void bustWin() {
+        State stand1 = Ready.start(new Card(Denomination.JACK, Suit.CLUBS), new Card(Denomination.TEN, Suit.CLUBS))
+                .stand();
+        State stand2 = Ready.start(new Card(Denomination.JACK, Suit.SPADES), new Card(Denomination.NINE, Suit.SPADES))
+                .hit(new Card(Denomination.THREE, Suit.SPADES));
+        assertThat(stand1.judge(stand2)).isInstanceOf(Win.class);
+    }
+}
