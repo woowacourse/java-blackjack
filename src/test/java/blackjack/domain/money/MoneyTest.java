db1b4492b03baae543b51e3e4c5a8b25ebf96a8f
SeungCheol <ssc6839@gmail.com>
2022-03-19 23:36:50 +0900

    test(blackjack): Result, Money 테스트 추가 및 파일 끝 줄 바꿈 추가

--- src/test/java/blackjack/domain/money/MoneyTest.java
+++ src/test/java/blackjack/domain/money/MoneyTest.java
@@ -2,15 +2,17 @@ package blackjack.domain.money;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class MoneyTest {
 
-    @Test
+    @ParameterizedTest
     @DisplayName("입력한 금액이 0 또는 음수일 때 예외 처리 확인")
-    void zeroOrNegativeTest() {
-        Assertions.assertThatThrownBy(() -> new Money(-10000))
+    @ValueSource(ints = {0, -1})
+    void zeroOrNegativeTest(int value) {
+        Assertions.assertThatThrownBy(() -> new Money(value))
             .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining("0 또는 음수는 입력할 수 없습니다. 정수를 입력해 주세요.");
+            .hasMessageContaining("양수를 입력해 주세요.");
     }
 }
--- src/test/java/blackjack/domain/result/ResultTest.java
+++ src/test/java/blackjack/domain/result/ResultTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Denomination;
@@ -26,25 +28,30 @@ class ResultTest {
     }
 
     @Test
-    @DisplayName("플레이어가 진 경우")
-    void dealerWinTest() {
-        player.drawCard(new Card(Denomination.TWO, Suit.CLUBS));
-        player.drawCard(new Card(Denomination.THREE, Suit.DIAMONDS));
+    @DisplayName("플레이어가 이긴 경우(플레이어만 블랙잭)")
+    void playerBlackjackTest() {
+        player.drawCard(new Card(Denomination.ACE, Suit.CLUBS));
+        player.drawCard(new Card(Denomination.JACK, Suit.CLUBS));
+
+        Result result = Result.judge(dealer, player);
+
+        assertThat(result).isEqualTo(Result.BLACKJACK);
+    }
+
+    @ParameterizedTest
+    @DisplayName("플레이어가 진 경우(1. 플레이어가 버스트 2. 딜러보다 점수 낮음)")
+    @CsvSource(value = {
+        "NINE,TEN,SEVEN",
+        "FOUR,TWO,THREE"
+    })
+    void playerLoseTest(Denomination d1, Denomination d2, Denomination d3) {
+        player.drawCard(new Card(d1, Suit.CLUBS));
+        player.drawCard(new Card(d2, Suit.CLUBS));
+        player.drawCard(new Card(d3, Suit.CLUBS));
 
         Result result = Result.judge(dealer, player);
 
         assertThat(result).isEqualTo(Result.LOSE);
-    }
-
-    @Test
-    @DisplayName("플레이어가 이긴 경우")
-    void playerWinTest() {
-        player.drawCard(new Card(Denomination.TWO, Suit.DIAMONDS));
-        player.drawCard(new Card(Denomination.TEN, Suit.DIAMONDS));
-
-        Result result = Result.judge(dealer, player);
-
-        assertThat(result).isEqualTo(Result.WIN);
     }
 
     @Test
@@ -59,9 +66,26 @@ class ResultTest {
     }
 
     @Test
+    @DisplayName("플레이어가 이긴 경우")
+    void playerWinTest() {
+        player.drawCard(new Card(Denomination.TWO, Suit.DIAMONDS));
+        player.drawCard(new Card(Denomination.TEN, Suit.DIAMONDS));
+
+        Result result = Result.judge(dealer, player);
+
+        assertThat(result).isEqualTo(Result.WIN);
+    }
+
+    @ParameterizedTest
     @DisplayName("승무패 결과에 따른 수익 계산이 잘되는지 확인")
-    void calculateRevenueTest() {
-        assertThat(Result.BLACKJACK.calculateRevenue(1000))
-            .isEqualTo(1500);
+    @CsvSource(value = {
+        "BLACKJACK,1000,1500",
+        "LOSE,1000,-1000",
+        "DRAW,1000,0",
+        "WIN,1000,1000"
+    })
+    void calculateRevenueTest(Result result, int money, int profit) {
+        assertThat(result.calculateRevenue(money))
+            .isEqualTo(profit);
     }
 }
