3acc2cf4aa719abb09584d36042e844cd7f711c4
김찬 <gmelddl514@gmail.com>
2022-03-19 21:59:06 +0900

    refactor : Cards 불변 객체로 변경

--- src/main/java/blackjack/domain/card/Cards.java
+++ src/main/java/blackjack/domain/card/Cards.java
@@ -6,61 +6,63 @@ import java.util.List;
 
 public class Cards {
 
-    private static final int BLACKJACK_COUNT = 21;
-    private static final int BLACKJACK_CARD_SIZE = 2;
-    private static final int ACE_ADDITIONAL_VALUE = 10;
+	private static final int BLACKJACK_COUNT = 21;
+	private static final int BLACKJACK_CARD_SIZE = 2;
+	private static final int ACE_ADDITIONAL_VALUE = 10;
 
-    private final List<Card> value;
+	private final List<Card> value;
 
-    public Cards(List<Card> cards) {
-        this.value = new ArrayList<>(cards);
-    }
+	public Cards(List<Card> cards) {
+		this.value = new ArrayList<>(cards);
+	}
 
-    public void add(Card card) {
-        this.value.add(card);
-    }
+	public Cards add(Card card) {
+		List<Card> cards = this.value;
+		cards.add(card);
+		return new Cards(cards);
+	}
 
-    public boolean isBust() {
-        return sum() > BLACKJACK_COUNT;
-    }
+	public boolean isBust() {
+		return sum() > BLACKJACK_COUNT;
+	}
 
-    public boolean isBlackJack() {
-        return value.size() == BLACKJACK_CARD_SIZE && sum() == BLACKJACK_COUNT;
-    }
+	public boolean isBlackJack() {
+		return value.size() == BLACKJACK_CARD_SIZE && sum() == BLACKJACK_COUNT;
+	}
 
-    public int sum() {
-        int sum = value.stream()
-                .mapToInt(Card::getDenominationValue)
-                .sum();
+	public int sum() {
+		int sum = value.stream()
+			.mapToInt(Card::getDenominationValue)
+			.sum();
 
-        if (canAddAdditionalValue(sum)) {
-            sum += ACE_ADDITIONAL_VALUE;
-        }
+		if (canAddAdditionalValue(sum)) {
+			sum += ACE_ADDITIONAL_VALUE;
+		}
 
-        return sum;
-    }
+		return sum;
+	}
 
-    private boolean canAddAdditionalValue(int sum) {
-        return hasAce() && !exceedBust(sum);
-    }
+	private boolean canAddAdditionalValue(int sum) {
+		return hasAce() && !exceedBust(sum);
+	}
 
-    private boolean hasAce() {
-        return value.stream()
-                .anyMatch(Card::isAce);
-    }
+	private boolean hasAce() {
+		return value.stream()
+			.anyMatch(Card::isAce);
+	}
 
-    private boolean exceedBust(int sum) {
-        return sum + ACE_ADDITIONAL_VALUE > BLACKJACK_COUNT;
-    }
+	private boolean exceedBust(int sum) {
+		return sum + ACE_ADDITIONAL_VALUE > BLACKJACK_COUNT;
+	}
 
-    public List<Card> getValue() {
-        return Collections.unmodifiableList(value);
-    }
+	public List<Card> getValue() {
+		return Collections.unmodifiableList(value);
+	}
 
-    @Override
-    public String toString() {
-        return "Cards{" +
-                "value=" + value +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Cards{" +
+			"value=" + value +
+			'}';
+	}
 }
