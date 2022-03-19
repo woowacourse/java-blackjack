c0d7d602f9a4242b78a656e162a4c8eaa058f28c
Hyunta <mohai2618@gmail.com>
2022-03-19 18:48:43 +0900

    fix: 상속받은 클래스 수정

--- src/main/java/blackjack_statepattern/state/Blackjack.java
+++ src/main/java/blackjack_statepattern/state/Blackjack.java
@@ -3,9 +3,8 @@ package blackjack_statepattern.state;
 import blackjack_statepattern.Cards;
 
 public class Blackjack extends Finished {
-    private final Cards cards;
 
     Blackjack(Cards cards) {
-        this.cards = cards;
+        super(cards);
     }
 }
--- src/main/java/blackjack_statepattern/state/Bust.java
+++ src/main/java/blackjack_statepattern/state/Bust.java
@@ -3,9 +3,8 @@ package blackjack_statepattern.state;
 import blackjack_statepattern.Cards;
 
 public final class Bust extends Finished {
-    private final Cards cards;
 
     Bust(Cards cards) {
-        this.cards = cards;
+        super(cards);
     }
 }
