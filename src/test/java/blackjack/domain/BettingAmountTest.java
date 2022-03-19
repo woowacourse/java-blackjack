10ac0e7ef689c4574a3f794a0ddb1d5554930e55
BeomsooKim <kbsat@naver.com>
2022-03-19 13:21:00 +0900

    test(BettingAmount): 배팅 금액 생성자 성공 케이스 추가

--- src/test/java/blackjack/domain/BettingAmountTest.java
+++ src/test/java/blackjack/domain/BettingAmountTest.java
@@ -1,10 +1,21 @@
 package blackjack.domain;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BettingAmountTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10000})
+    @DisplayName("배팅 금액은 0 이상의 수를 입력받는다.")
+    void constructor(int money) {
+        assertDoesNotThrow(() -> new BettingAmount(money));
+    }
 
     @Test
     @DisplayName("배팅 금액은 음수이면 안된다.")
