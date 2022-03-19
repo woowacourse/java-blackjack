0c906a9a8bd50e1257050b00a78f62ad2d81ad65
koo <gudonghee2000@gmail.com>
2022-03-19 21:00:36 +0900

    refactor: CardSymbol을 한글에서 트럼프 문양으로 변경

--- src/main/java/blackjack/model/card/CardSymbol.java
+++ src/main/java/blackjack/model/card/CardSymbol.java
@@ -2,10 +2,10 @@ package blackjack.model.card;
 
 public enum CardSymbol {
 
-    HEART("하트"),
-    DIAMOND("다이아몬드"),
-    CLOVER("클로버"),
-    SPADE("스페이트"),
+    HEART("♥"),
+    DIAMOND("♦"),
+    CLOVER("♣"),
+    SPADE("♠"),
     ;
 
     private final String value;
