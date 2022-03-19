751da0e00331158e2a56ebd444ff2d44b596e39d
sinb57 <sinb57@naver.com>
2022-03-20 05:03:04 +0900

    test: MatchCalculator, provider 추출 및 테스트 보충

--- src/test/java/blackjack/domain/result/MatchCalculatorTest.java
+++ src/test/java/blackjack/domain/result/MatchCalculatorTest.java
@@ -1,9 +1,13 @@
 package blackjack.domain.result;
 
-import static blackjack.Fixture.SPADE_ACE;
+import static blackjack.Fixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -12,10 +16,32 @@ import blackjack.domain.participant.Player;
 
 class MatchCalculatorTest {
 
+    @DisplayName("플레이어의 턴이 종료되지 않은 상태에서는 승패를 결정할 수 없어야 한다.")
+    @Test
+    void playerStateNotFinishedExceptionTest() {
+        final Player player = Player.readyToPlay("name", List.of(SPADE_ACE, SPADE_TWO));
+        final Dealer dealer = Dealer.readyToPlay(List.of(SPADE_ACE, SPADE_TEN));
+
+        assertThatThrownBy(() -> MatchCalculator.judgeMatchStatusOfPlayer(player, dealer))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("플레이어의 턴이 종료되지 않았습니다.");
+    }
+
+    @DisplayName("딜러의 턴이 종료되지 않은 상태에서는 승패를 결정할 수 없어야 한다.")
+    @Test
+    void dealerStateNotFinishedExceptionTest() {
+        final Player player = Player.readyToPlay("name", List.of(SPADE_ACE, SPADE_TEN));
+        final Dealer dealer = Dealer.readyToPlay(List.of(SPADE_ACE, SPADE_TWO));
+
+        assertThatThrownBy(() -> MatchCalculator.judgeMatchStatusOfPlayer(player, dealer))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("딜러의 턴이 종료되지 않았습니다.");
+    }
+
     @DisplayName("플레이어가 버스트인 경우, 플레이어가 패배한다.")
     @ParameterizedTest
     @MethodSource("blackjack.domain.result.provider.MatchCalculatorTestProvider#provideForPlayerLossIfPlayerBustTest")
-    void playerLossIfPlayerBustTest(final Dealer dealer, final Player player) {
+    void playerLossIfPlayerBustTest(final Player player, final Dealer dealer) {
         final MatchStatus actualMatchStatus = MatchCalculator.judgeMatchStatusOfPlayer(player, dealer);
         assertThat(actualMatchStatus).isEqualTo(MatchStatus.LOSS);
     }
@@ -23,7 +49,7 @@ class MatchCalculatorTest {
     @DisplayName("플레이어와 딜러 모두 블랙잭이면 무승부이다.")
     @ParameterizedTest
     @MethodSource("blackjack.domain.result.provider.MatchCalculatorTestProvider#provideForPlayerDrawIfBothBlackjackTest")
-    void playerDrawIfBothBlackjackTest(final Dealer dealer, final Player player) {
+    void playerDrawIfBothBlackjackTest(final Player player, final Dealer dealer) {
         final MatchStatus actualMatchStatus = MatchCalculator.judgeMatchStatusOfPlayer(player, dealer);
         assertThat(actualMatchStatus).isEqualTo(MatchStatus.DRAW);
     }
@@ -31,7 +57,7 @@ class MatchCalculatorTest {
     @DisplayName("플레이어가 블랙잭이고 딜러가 블랙잭이 아니면, 플레이어는 블랙잭으로 승리한다.")
     @ParameterizedTest
     @MethodSource("blackjack.domain.result.provider.MatchCalculatorTestProvider#provideForPlayerWinIfOnlyPlayerBlackjackTest")
-    void playerWinIfOnlyPlayerBlackjackTest(final Dealer dealer, final Player player) {
+    void playerWinIfOnlyPlayerBlackjackTest(final Player player, final Dealer dealer) {
         final MatchStatus actualMatchStatus = MatchCalculator.judgeMatchStatusOfPlayer(player, dealer);
         assertThat(actualMatchStatus).isEqualTo(MatchStatus.BLACKJACK);
     }
@@ -39,9 +65,7 @@ class MatchCalculatorTest {
     @DisplayName("둘다 블랙잭이 아닌 상태에서 동점이면 무승부이다.")
     @ParameterizedTest
     @MethodSource("blackjack.domain.result.provider.MatchCalculatorTestProvider#provideForPlayerDrawIfBothNotBlackjackTest")
-    void playerDrawIfBothNotBlackjackTest(final Dealer dealer, final Player player) {
-        player.drawCard(SPADE_ACE);
-
+    void playerDrawIfBothNotBlackjackTest(final Player player, final Dealer dealer) {
         final MatchStatus actualMatchStatus = MatchCalculator.judgeMatchStatusOfPlayer(player, dealer);
         assertThat(actualMatchStatus).isEqualTo(MatchStatus.DRAW);
     }
@@ -49,11 +73,9 @@ class MatchCalculatorTest {
     @DisplayName("플레이어가 스탠드인 상황에서, 플레이어의 승패를 계산할 수 있어야 한다.")
     @ParameterizedTest(name = "[{index}] 플레이어 : {2}")
     @MethodSource("blackjack.domain.result.provider.MatchCalculatorTestProvider#provideForJudgeMatchStatusIfPlayerStandTest")
-    void judgeMatchStatusIfPlayerStandTest(final Dealer dealer,
-                                           final Player player,
+    void judgeMatchStatusIfPlayerStandTest(final Player player,
+                                           final Dealer dealer,
                                            final MatchStatus expectedMatchStatus) {
-        player.drawCard(SPADE_ACE);
-
         final MatchStatus actualMatchStatus = MatchCalculator.judgeMatchStatusOfPlayer(player, dealer);
         assertThat(actualMatchStatus).isEqualTo(expectedMatchStatus);
     }
--- src/test/java/blackjack/domain/result/provider/MatchCalculatorTestProvider.java
+++ src/test/java/blackjack/domain/result/provider/MatchCalculatorTestProvider.java
@@ -1,23 +1,21 @@
 package blackjack.domain.result.provider;
 
-import static blackjack.Fixture.DIAMOND_ACE;
-import static blackjack.Fixture.DIAMOND_KING;
 import static blackjack.Fixture.DIAMOND_SIX;
-import static blackjack.Fixture.DIAMOND_TEN;
-import static blackjack.Fixture.DIAMOND_THREE;
-import static blackjack.Fixture.DIAMOND_TWO;
-import static blackjack.Fixture.HEART_ACE;
 import static blackjack.Fixture.HEART_FIVE;
-import static blackjack.Fixture.HEART_KING;
 import static blackjack.Fixture.HEART_SEVEN;
 import static blackjack.Fixture.SPADE_ACE;
+import static blackjack.Fixture.SPADE_FIVE;
 import static blackjack.Fixture.SPADE_KING;
+import static blackjack.Fixture.SPADE_SIX;
 import static blackjack.Fixture.SPADE_TEN;
+import static blackjack.Fixture.SPADE_THREE;
+import static blackjack.Fixture.SPADE_TWO;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.params.provider.Arguments;
 
 import blackjack.domain.card.Card;
@@ -27,40 +25,35 @@ import blackjack.domain.result.MatchStatus;
 
 public class MatchCalculatorTestProvider {
 
-    public static final List<Card> BLACKJACK_INITIAL_CARDS = List.of(
-            SPADE_ACE,
-            DIAMOND_KING
-    );
-
     public static Stream<Arguments> provideForPlayerLossIfPlayerBustTest() {
-        final Player bustStateOfPlayer = generatePlayer(
-                List.of(
-                        SPADE_KING,
-                        DIAMOND_KING
-                ),
-                List.of(
-                        HEART_KING
-                )
-        );
         return Stream.of(
                 Arguments.of(
-                        generateDealer(
-                                List.of(
-                                        SPADE_TEN,
-                                        HEART_SEVEN
-                                ),
-                                List.of(
-                                        DIAMOND_TWO
+                        Named.of("Player : Bust(22)",
+                                generatePlayer(
+                                        List.of(SPADE_TEN, SPADE_KING),
+                                        List.of(SPADE_TWO)
                                 )
                         ),
-                        bustStateOfPlayer
+                        Named.of("Dealer : Bust(26)",
+                                generateDealer(
+                                        List.of(SPADE_TEN, SPADE_SIX),
+                                        List.of(SPADE_KING)
+                                )
+                        )
                 ),
                 Arguments.of(
-                        generateDealer(
-                                BLACKJACK_INITIAL_CARDS,
-                                Collections.emptyList()
+                        Named.of("Player : Bust(22)",
+                                generatePlayer(
+                                        List.of(SPADE_TEN, SPADE_KING),
+                                        List.of(SPADE_TWO)
+                                )
                         ),
-                        bustStateOfPlayer
+                        Named.of("Dealer : Blackjack(21)",
+                                generateDealer(
+                                        List.of(SPADE_TEN, SPADE_ACE),
+                                        Collections.emptyList()
+                                )
+                        )
                 )
         );
     }
@@ -68,47 +61,66 @@ public class MatchCalculatorTestProvider {
     public static Stream<Arguments> provideForPlayerDrawIfBothBlackjackTest() {
         return Stream.of(
                 Arguments.of(
-                        generateDealer(
-                                BLACKJACK_INITIAL_CARDS,
-                                Collections.emptyList()
+                        Named.of("Player : Blackjack(21)",
+                                generatePlayer(
+                                        List.of(SPADE_ACE, SPADE_KING),
+                                        Collections.emptyList()
+                                )
                         ),
-                        generatePlayer(
-                                BLACKJACK_INITIAL_CARDS,
-                                Collections.emptyList()
+                        Named.of("Dealer : Blackjack(21)",
+                                generateDealer(
+                                        List.of(SPADE_ACE, SPADE_KING),
+                                        Collections.emptyList()
+                                )
                         )
                 )
         );
     }
 
     public static Stream<Arguments> provideForPlayerWinIfOnlyPlayerBlackjackTest() {
-        final Player blackjackStateOfPlayer = generatePlayer(
-                BLACKJACK_INITIAL_CARDS,
-                Collections.emptyList()
-        );
         return Stream.of(
                 Arguments.of(
-                        generateDealer(
-                                List.of(
-                                        SPADE_TEN,
-                                        HEART_SEVEN
-                                ),
-                                List.of(
-                                        DIAMOND_TWO
+                        Named.of("Player : Blackjack(21)",
+                                generatePlayer(
+                                        List.of(SPADE_ACE, SPADE_KING),
+                                        Collections.emptyList()
                                 )
                         ),
-                        blackjackStateOfPlayer
+                        Named.of("Dealer : Stand(17)",
+                                generateDealer(
+                                        List.of(SPADE_TEN, HEART_SEVEN),
+                                        Collections.emptyList()
+                                )
+                        )
                 ),
                 Arguments.of(
-                        generateDealer(
-                                List.of(
-                                        SPADE_TEN,
-                                        HEART_SEVEN
-                                ),
-                                List.of(
-                                        DIAMOND_KING
+                        Named.of("Player : Blackjack(21)",
+                                generatePlayer(
+                                        List.of(SPADE_ACE, SPADE_KING),
+                                        Collections.emptyList()
                                 )
                         ),
-                        blackjackStateOfPlayer
+                        Named.of("Dealer : Stand(21)",
+                                generateDealer(
+                                        List.of(SPADE_TEN, SPADE_FIVE),
+                                        List.of(SPADE_SIX)
+                                )
+                        )
+                ),
+                Arguments.of(
+                        Named.of("Player : Blackjack(21)",
+                                generatePlayer(
+                                        List.of(SPADE_ACE, SPADE_KING),
+                                        Collections.emptyList()
+                                )
+                        ),
+                        Named.of("Dealer : Bust(25)",
+                                generateDealer(
+                                        List.of(SPADE_TEN, SPADE_FIVE),
+                                        List.of(SPADE_KING)
+
+                                )
+                        )
                 )
         );
     }
@@ -116,43 +128,30 @@ public class MatchCalculatorTestProvider {
     public static Stream<Arguments> provideForPlayerDrawIfBothNotBlackjackTest() {
         return Stream.of(
                 Arguments.of(
-                        generateDealer(
-                                List.of(
-                                        SPADE_TEN,
-                                        HEART_FIVE
-                                ),
-                                List.of(
-                                        DIAMOND_SIX
+                        Named.of("Player : Stand(21)",
+                                generatePlayer(
+                                        List.of(SPADE_TEN, HEART_FIVE),
+                                        List.of(DIAMOND_SIX)
                                 )
                         ),
-                        generatePlayer(
-                                List.of(
-                                        SPADE_TEN,
-                                        HEART_FIVE
-                                ),
-                                List.of(
-                                        DIAMOND_SIX
+                        Named.of("Dealer : Stand(21)",
+                                generateDealer(
+                                        List.of(SPADE_TEN, SPADE_FIVE),
+                                        List.of(SPADE_SIX)
                                 )
                         )
                 ),
                 Arguments.of(
-                        generateDealer(
-                                List.of(
-                                        SPADE_TEN,
-                                        HEART_FIVE
-                                ),
-                                List.of(
-                                        DIAMOND_THREE,
-                                        HEART_ACE
+                        Named.of("Player : Stand(18)",
+                                generatePlayer(
+                                        List.of(SPADE_TEN, SPADE_FIVE),
+                                        List.of(SPADE_THREE)
                                 )
                         ),
-                        generatePlayer(
-                                List.of(
-                                        SPADE_TEN,
-                                        HEART_FIVE
-                                ),
-                                List.of(
-                                        DIAMOND_THREE
+                        Named.of("Dealer : Stand(18)",
+                                generateDealer(
+                                        List.of(SPADE_TEN, SPADE_FIVE),
+                                        List.of(SPADE_THREE)
                                 )
                         )
                 )
@@ -162,44 +161,61 @@ public class MatchCalculatorTestProvider {
     public static Stream<Arguments> provideForJudgeMatchStatusIfPlayerStandTest() {
         return Stream.of(
                 Arguments.of(
-                        generateDealer(
-                                List.of(
-                                        SPADE_TEN,
-                                        HEART_FIVE
-                                ),
-                                List.of(
-                                        DIAMOND_TEN
+                        Named.of("Player : Stand(21)",
+                                generatePlayer(
+                                        List.of(SPADE_TEN, SPADE_FIVE),
+                                        List.of(SPADE_SIX)
                                 )
                         ),
-                        generatePlayer(
-                                List.of(
-                                        SPADE_TEN,
-                                        HEART_FIVE
-                                ),
-                                List.of(
-                                        DIAMOND_SIX
+                        Named.of("Dealer : Bust(25)",
+                                generateDealer(
+                                        List.of(SPADE_TEN, SPADE_FIVE),
+                                        List.of(SPADE_TEN)
                                 )
                         ),
                         MatchStatus.WIN
                 ),
                 Arguments.of(
-                        generateDealer(
-                                List.of(
-                                        SPADE_TEN,
-                                        HEART_FIVE
-                                ),
-                                List.of(
-                                        DIAMOND_THREE,
-                                        HEART_ACE
+                        Named.of("Player : Stand(21)",
+                                generatePlayer(
+                                        List.of(SPADE_TEN, SPADE_FIVE),
+                                        List.of(SPADE_SIX)
                                 )
                         ),
-                        generatePlayer(
-                                List.of(
-                                        SPADE_TEN,
-                                        HEART_FIVE
-                                ),
-                                List.of(
-                                        DIAMOND_ACE
+                        Named.of("Dealer : Blackjack(21)",
+                                generateDealer(
+                                        List.of(SPADE_TEN, SPADE_ACE),
+                                        Collections.emptyList()
+                                )
+                        ),
+                        MatchStatus.LOSS
+                ),
+                Arguments.of(
+                        Named.of("Player : Stand(21)",
+                                generatePlayer(
+                                        List.of(SPADE_TEN, SPADE_FIVE),
+                                        List.of(SPADE_SIX)
+                                )
+                        ),
+                        Named.of("Dealer : Stand(20)",
+                                generateDealer(
+                                        List.of(SPADE_TEN, SPADE_KING),
+                                        Collections.emptyList()
+                                )
+                        ),
+                        MatchStatus.WIN
+                ),
+                Arguments.of(
+                        Named.of("Player : Stand(16)",
+                                generatePlayer(
+                                        List.of(SPADE_TEN, SPADE_FIVE),
+                                        List.of(SPADE_ACE)
+                                )
+                        ),
+                        Named.of("Dealer : Stand(18)",
+                                generateDealer(
+                                        List.of(SPADE_TEN, SPADE_FIVE),
+                                        List.of(SPADE_THREE)
                                 )
                         ),
                         MatchStatus.LOSS
@@ -207,17 +223,20 @@ public class MatchCalculatorTestProvider {
         );
     }
 
-    private static Dealer generateDealer(final List<Card> initializedCards, List<Card> drewCards) {
-        final Dealer dealer = Dealer.readyToPlay(initializedCards);
-        drewCards.forEach(dealer::drawCard);
-        return dealer;
-    }
-
     private static Player generatePlayer(final List<Card> initializedCards, List<Card> drewCards) {
         final Player player = Player.readyToPlay("name", initializedCards);
         player.betAmount(1000);
         drewCards.forEach(player::drawCard);
+        if (player.isPossibleToDrawCard()) {
+            player.stay();
+        }
         return player;
+    }
+
+    private static Dealer generateDealer(final List<Card> initializedCards, List<Card> drewCards) {
+        final Dealer dealer = Dealer.readyToPlay(initializedCards);
+        drewCards.forEach(dealer::drawCard);
+        return dealer;
     }
 
 }
