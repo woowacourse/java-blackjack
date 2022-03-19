66ca08a0aeaf71f0923527123bdac191720b8047
Hyunta <mohai2618@gmail.com>
2022-03-19 15:29:33 +0900

    test: ParameterizedTest를 이용해서 입력값에 대한 테스트 묶기

--- src/test/java/blackjack/domain/DrawCommandTest.java
+++ src/test/java/blackjack/domain/DrawCommandTest.java
@@ -3,25 +3,34 @@ package blackjack.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class DrawCommandTest {
 
-    @Test
-    @DisplayName("y를 받았을 때 YES를 반환하는가?")
-    void drawCommand_Y() {
-        String inputValue = "y";
-        DrawCommand drawCommand = DrawCommand.from(inputValue);
-        assertThat(drawCommand).isEqualTo(DrawCommand.YES);
+    @ParameterizedTest
+    @DisplayName("n,y 를 입력받았을 때 예상값을 반환하는지 그리고 이외에 값에 대한 예외처리")
+    @MethodSource("drawCommandInputProvide")
+    void drawCommand_inputValue(String inputValue, DrawCommand expectedDrawCommand) {
+        try {
+            DrawCommand drawCommand = DrawCommand.from(inputValue);
+            assertThat(drawCommand).isEqualTo(expectedDrawCommand);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(IllegalArgumentException.class);
+        }
+
     }
 
-    @Test
-    @DisplayName("n를 받았을 때 YES를 반환하는가?")
-    void drawCommand_N() {
-        String inputValue = "n";
-        DrawCommand drawCommand = DrawCommand.from(inputValue);
-        assertThat(drawCommand).isEqualTo(DrawCommand.NO);
+    public static Stream<Arguments> drawCommandInputProvide() {
+        return Stream.of(
+                Arguments.arguments("y", DrawCommand.YES),
+                Arguments.arguments("n", DrawCommand.NO),
+                Arguments.arguments("x", null)
+        );
     }
 
     @Test
