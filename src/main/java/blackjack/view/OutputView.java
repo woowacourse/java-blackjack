b7128668e4ff0bb8d26d72482115e078857d3112
DOES <ldk980130@gmail.com>
2022-03-19 17:44:23 +0900

    refactor: filterNames -> connectCardsWithName으로 메서드 이름 변경

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -3,11 +3,10 @@ package blackjack.view;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import blackjack.domain.game.BlackJackGame;
 import blackjack.domain.card.Card;
+import blackjack.domain.game.BlackJackGame;
 import blackjack.domain.gamer.Dealer;
 import blackjack.domain.gamer.Player;
-import blackjack.domain.game.BlackJackReferee;
 import blackjack.domain.result.BettingResult;
 
 public class OutputView {
@@ -21,7 +20,7 @@ public class OutputView {
 	}
 
 	public static void checkHoldingCards(String name, List<Card> cards) {
-		System.out.println(name + "카드: " + filterNames(cards));
+		System.out.println(name + "카드: " + connectCardsWithName(cards));
 	}
 
 	public static void printAdditionalDrawDealer(int count) {
@@ -40,18 +39,18 @@ public class OutputView {
 
 	private static void printDealerFinalCards(Dealer dealer) {
 		System.out.printf("%s카드: %s- 결과: %d\n",
-			dealer.getName(), filterNames(dealer.getCards()), dealer.sumCardsNumber());
+			dealer.getName(), connectCardsWithName(dealer.getCards()), dealer.sumCardsNumber());
 	}
 
 	private static void printPlayerFinalCards(List<Player> players) {
 		for (Player player : players) {
 			System.out.printf("%s카드: %s- 결과: %d\n",
-				player.getName(), filterNames(player.getCards()), player.sumCardsNumber());
+				player.getName(), connectCardsWithName(player.getCards()), player.sumCardsNumber());
 		}
 		System.out.println();
 	}
 
-	private static String filterNames(List<Card> cards) {
+	private static String connectCardsWithName(List<Card> cards) {
 		return cards.stream()
 			.map(card -> card.getName() + card.getSymbol())
 			.collect(Collectors.joining(", "));
