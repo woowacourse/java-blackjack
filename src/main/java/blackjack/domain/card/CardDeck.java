b94abc515a71d868e7968ff360b6c2476d3dd33f
Jeong Jinwoo <jwjeong96@gmail.com>
2022-03-19 22:29:58 +0900

    refactor(CardDeck): change constructor logic
    - assign cards field with non-empty initialized list
    - prev : assign empty list and then add cards at private methods

--- src/main/java/blackjack/domain/card/CardDeck.java
+++ src/main/java/blackjack/domain/card/CardDeck.java
@@ -1,31 +1,36 @@
 package blackjack.domain.card;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 public class CardDeck implements CardStack {
 
     private static final String EMPTY_CARD_DECK_EXCEPTION_MESSAGE = "카드가 모두 소진되었습니다!";
 
-    private final LinkedList<Card> cards = new LinkedList<>();
+    private final LinkedList<Card> cards;
 
     public CardDeck() {
-        initCards();
+        this.cards = getAllCards();
         Collections.shuffle(cards);
     }
 
-    private void initCards() {
-        for (CardRank rank : CardRank.values()) {
-            initAndAddAllSymbolsOf(rank);
-        }
+    private LinkedList<Card> getAllCards() {
+        LinkedList<Card> cards = new LinkedList<>();
+        Arrays.stream(CardRank.values())
+                .map(this::initAllCardsOfRank)
+                .forEach(cards::addAll);
+
+        return cards;
     }
 
-    private void initAndAddAllSymbolsOf(final CardRank rank) {
-        for (CardSymbol symbol : CardSymbol.values()) {
-            Card card = Card.of(rank, symbol);
-            cards.add(card);
-        }
+    private List<Card> initAllCardsOfRank(final CardRank rank) {
+        return Arrays.stream(CardSymbol.values())
+                .map(symbol -> Card.of(rank, symbol))
+                .collect(Collectors.toList());
     }
 
     @Override
