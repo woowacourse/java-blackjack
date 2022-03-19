9f8d81e09e7d29b73f363ace6d7ab8491fcddb5e
hyeonic <dev.hyeonic@gmail.com>
2022-03-19 11:55:05 +0900

    feat: Dealer 상태 추가

--- src/main/java/blackjack/domain/participant/Dealer.java
+++ src/main/java/blackjack/domain/participant/Dealer.java
@@ -2,34 +2,38 @@ package blackjack.domain.participant;
 
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Cards;
+import blackjack.domain.state.Ready;
+import blackjack.domain.state.State;
 import java.util.Collections;
 import java.util.List;
 
 public class Dealer {
 
     private final Name name;
-    private final Cards cards;
+    private State state;
 
     public Dealer() {
         this.name = new Name("딜러");
-        this.cards = new Cards();
+        this.state = new Ready();
     }
 
     public boolean isReady() {
-        return cards.isReady();
+        return state.isRunning();
     }
 
     public boolean isFinished() {
+        Cards cards = state.cards();
         return cards.totalScore() > 16;
     }
 
     public void hit(Card card) {
         if (!isFinished()) {
-            cards.append(card);
+            state = state.draw(card);
         }
     }
 
     public int getTotalScore() {
+        Cards cards = state.cards();
         return cards.totalScore();
     }
 
@@ -38,6 +42,7 @@ public class Dealer {
     }
 
     public List<Card> getCards() {
+        Cards cards = state.cards();
         return Collections.unmodifiableList(cards.getValue());
     }
 }
