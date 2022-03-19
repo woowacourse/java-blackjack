c820175e77a7c280014cca0583cb9a5f9aeb5cca
dongho <dongho108@naver.com>
2022-03-19 21:42:03 +0900

    refactor: Money 기본생성자의 검증기능 제거
    * Money는 기본생성자로만 생성되고, 0원으로 자동초기화

--- src/main/java/blackjack/domain/gamer/Money.java
+++ src/main/java/blackjack/domain/gamer/Money.java
@@ -1,12 +1,12 @@
 package blackjack.domain.gamer;
 
 public class Money {
-    private int amount;
+    private static final int MIN = 10;
+    private static final int DIVIDED = 10;
+    private static final String MIN_ERROR_MESSAGE = MIN + "원 이상의 돈을 입력해주세요.";
+    private static final String UNIT_ERROR_MESSAGE = DIVIDED + "원 단위의 금액을 입력해주세요.";
 
-    public Money(int amount) {
-        validate(amount);
-        this.amount = amount;
-    }
+    private int amount = 0;
 
     public void add(int amount) {
         validate(amount);
@@ -14,8 +14,19 @@ public class Money {
     }
 
     private void validate(int amount) {
-        if (amount % 10 != 0) {
-            throw new IllegalArgumentException("10원 단위의 금액을 입력해주세요.");
+        validateMin(amount);
+        validateDivisible(amount);
+    }
+
+    private void validateMin(int amount) {
+        if (amount < MIN) {
+            throw new IllegalArgumentException(MIN_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateDivisible(int amount) {
+        if (amount % DIVIDED != 0) {
+            throw new IllegalArgumentException(UNIT_ERROR_MESSAGE);
         }
     }
 
--- src/test/java/blackjack/domain/gamer/MoneyTest.java
+++ src/test/java/blackjack/domain/gamer/MoneyTest.java
@@ -13,14 +13,16 @@ class MoneyTest {
 
     @ParameterizedTest(name = "{index}: {1}")
     @MethodSource("invalidParameters")
-    @DisplayName("금액 생성 테스트")
+    @DisplayName("금액 투입 테스트")
     void addCard(int amount, String testName) {
-        assertThatThrownBy(() -> new Money(amount))
+        Money money = new Money();
+        assertThatThrownBy(() -> money.add(amount))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
     private static Stream<Arguments> invalidParameters() {
         return Stream.of(
+
             Arguments.of(5, "5 투입"),
             Arguments.of(15, "15 투입")
         );
