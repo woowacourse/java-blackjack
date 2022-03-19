79ffafded8f493e27f8177eff87e740489695b3b
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-03-19 17:23:46 +0900

    test(StateParticipant): 초기화 후 상태가 Blackjack인지 확인

--- src/test/java/blackjack/domain/state/stateparticipant/StateParticipantTest.java
+++ src/test/java/blackjack/domain/state/stateparticipant/StateParticipantTest.java
@@ -1,16 +1,17 @@
 package blackjack.domain.state.stateparticipant;
 
+import static blackjack.domain.card.CardNumber.*;
+import static blackjack.domain.card.CardSymbol.*;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import blackjack.domain.card.Card;
-import blackjack.domain.card.CardNumber;
-import blackjack.domain.card.CardSymbol;
 import blackjack.domain.participant.Name;
 import blackjack.domain.state.State;
 import blackjack.domain.state.started.Started;
+import blackjack.domain.state.started.finished.Blackjack;
 import blackjack.domain.state.started.running.Running;
 
 public class StateParticipantTest {
@@ -35,11 +36,26 @@ public class StateParticipantTest {
         StateParticipant participant = new StatePlayer(Name.of("pobi"));
 
         //when
-        participant.init(new Card(CardSymbol.DIAMOND, CardNumber.JACK),
-            new Card(CardSymbol.HEART, CardNumber.JACK));
+        participant.init(new Card(DIAMOND, JACK),
+            new Card(HEART, JACK));
         State actual = participant.getState();
 
         //then
         assertThat(actual).isInstanceOf(Running.class);
+    }
+
+    @Test
+    @DisplayName("초기화 후 카드 합이 21이면 상태가 Blackjack인지 확인")
+    void checkBlackjack() {
+        //given
+        StateParticipant participant = new StatePlayer(Name.of("pobi"));
+
+        //when
+        participant.init(new Card(DIAMOND, JACK),
+            new Card(HEART, ACE));
+        State actual = participant.getState();
+
+        //then
+        assertThat(actual).isInstanceOf(Blackjack.class);
     }
 }
