0767ae000204dd6ed62ea145d3cca75fef352f58
Richard JEON <ztzy1907@gmail.com>
2022-03-20 13:01:31 +0900

    feat(gamblers): 생성자에 겜블러 최대 인원 검증 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -12,12 +12,12 @@ public class InputView {
     private static final int MAXIMUM_GAMBLER_NUMBER = 7;
     private static final String HIT_CHARACTER = "y";
     private static final String COMMA_DELIMITER = ",";
-    private static final String ERROR_DUPLICATE_NAME = "[ERROR] 이름은 중복될 수 없습니다.";
     private static final String INPUT_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String INPUT_NEED_MORE_CARD = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)%n";
+    private static final String INPUT_BETTING_MONEY = "%s%s의 배팅 금액은?%n";
+    private static final String ERROR_TOO_MANY_GAMBLERS = "[ERROR] 겜블러는 최대 7명까지 참여 가능합니다 : ";
     private static final String NEWLINE = System.lineSeparator();
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String INPUT_BETTING_MONEY = "%s%s의 배팅 금액은?%n";
 
     private InputView() {
     }
@@ -25,7 +25,6 @@ public class InputView {
     public static List<Wallet> scanGamblerInfos() {
         System.out.println(INPUT_PLAYER_NAMES);
         List<String> names = scanRawPlayerNames();
-        validateDuplicateNames(names);
         validateMaximumGamblers(names.size());
 
         return names.stream()
@@ -45,19 +44,9 @@ public class InputView {
         return Integer.parseInt(SCANNER.nextLine());
     }
 
-    private static void validateDuplicateNames(List<String> names) {
-        boolean isDuplicate = names.size() != names.stream()
-                .distinct()
-                .count();
-
-        if (isDuplicate) {
-            throw new IllegalArgumentException(ERROR_DUPLICATE_NAME);
-        }
-    }
-
     private static void validateMaximumGamblers(int numberOfGamblers) {
         if (numberOfGamblers > MAXIMUM_GAMBLER_NUMBER) {
-            throw new IllegalArgumentException("[ERROR] 겜블러는 최대 7명까지 참여 가능합니다 : " + numberOfGamblers);
+            throw new IllegalArgumentException(ERROR_TOO_MANY_GAMBLERS + numberOfGamblers);
         }
     }
 
