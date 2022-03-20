d5376e48cd1594dbcbdecaea0ab0336c3be70907
shindong96 <2006peter@naver.com>
2022-03-20 21:03:54 +0900

    refactor: 단위테스트 추가

--- src/test/java/domain/participant/info/HandTest.java
+++ src/test/java/domain/participant/info/HandTest.java
@@ -10,9 +10,16 @@ import org.junit.jupiter.api.Test;
 
 public class HandTest {
 	@Test
-	@DisplayName("에이스가 있는지 테스트")
+	@DisplayName("에이스가 있는지 테스트_있는 경우")
 	void hasAce() {
 		Hand hand = new Hand(List.of(ACE_CLOVER, QUEEN_CLOVER));
 		assertThat(hand.hasAce()).isTrue();
+	}
+
+	@Test
+	@DisplayName("에이스가 있는지 테스트_없는 경우")
+	void hasNotAce() {
+		Hand hand = new Hand(List.of(TWO_CLOVER, QUEEN_CLOVER));
+		assertThat(hand.hasAce()).isFalse();
 	}
 }
