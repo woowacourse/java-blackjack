3c388e7dc2b47f0016f9cfc1ce94c3db3d287f95
TaeHyeon Kim <kth990303@naver.com>
2022-03-20 19:01:53 +0900

    refactor: 승무패 판단을 각 상태에게 위임하여 BlackjackMatch 역할 삭제하도록 변경

--- src/main/java/blackjack/domain/result/BlackjackMatch.java
+++ src/main/java/blackjack/domain/result/BlackjackMatch.java
@@ -1,94 +1,18 @@
 package blackjack.domain.result;
 
-import blackjack.domain.participant.Dealer;
-import blackjack.domain.participant.Player;
-
 public enum BlackjackMatch {
 
-    WIN_BLACKJACK("승", true),
-    WIN("승", false),
-    DRAW("무", false),
-    LOSE("패", false),
-    LOSE_BLACK_JACK("패", true);
+    WIN("승"),
+    DRAW("무"),
+    LOSE("패");
 
     private final String result;
-    private final boolean blackjack;
 
-    BlackjackMatch(String result, boolean blackjack) {
+    BlackjackMatch(String result) {
         this.result = result;
-        this.blackjack = blackjack;
-    }
-
-    public static BlackjackMatch of(Player player, Dealer dealer) {
-        if (player.isBust() || dealer.isBust()) {
-            return getWinLoseByBust(player);
-        }
-        if (player.isBlackjack() || dealer.isBlackjack()) {
-            return getWinDrawByBlackjack(player, dealer);
-        }
-        return getWinDrawLoseByScore(player, dealer);
-    }
-
-    private static BlackjackMatch getWinLoseByBust(Player player) {
-        if (player.isBust()) {
-            return LOSE;
-        }
-        return WIN;
-    }
-
-    private static BlackjackMatch getWinDrawByBlackjack(Player player, Dealer dealer) {
-        if (isWinBlackjack(player, dealer)) {
-            return WIN_BLACKJACK;
-        }
-        if (isDrawBlackjack(player, dealer)) {
-            return DRAW;
-        }
-        return LOSE_BLACK_JACK;
-    }
-
-    private static boolean isWinBlackjack(Player player, Dealer dealer) {
-        return player.isBlackjack() && !dealer.isBlackjack();
-    }
-
-    private static boolean isDrawBlackjack(Player player, Dealer dealer) {
-        return player.isBlackjack() && dealer.isBlackjack();
-    }
-
-    private static BlackjackMatch getWinDrawLoseByScore(Player player, Dealer dealer) {
-        if (player.getScore() > dealer.getScore()) {
-            return WIN;
-        }
-        if (player.getScore() == dealer.getScore()) {
-            return DRAW;
-        }
-        return LOSE;
-    }
-
-    public BlackjackMatch swapResult() {
-        if (blackjack) {
-            return swapResultBlackjack();
-        }
-        if (result.equals(WIN.getResult())) {
-            return LOSE;
-        }
-        if (result.equals(LOSE.getResult())) {
-            return WIN;
-        }
-        return DRAW;
-    }
-
-    private BlackjackMatch swapResultBlackjack() {
-        if (result.equals(WIN_BLACKJACK.result)) {
-            return LOSE_BLACK_JACK;
-        }
-        return WIN_BLACKJACK;
     }
 
     public String getResult() {
         return result;
-    }
-
-    public boolean isBlackjack() {
-        return blackjack;
     }
 }
