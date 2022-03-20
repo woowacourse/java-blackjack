7dce42f2e0a2b76e8a87e665697547e006a412b1
yoon <duddbs774@gmail.com>
2022-03-21 04:42:39 +0900

    test: mock 객체 추가

--- src/test/java/blakjack/domain/card/MockCard.java
+++ src/test/java/blakjack/domain/card/MockCard.java
@@ -4,4 +4,5 @@ public final class MockCard {
     public final static Card CLUB_TEN = new Card(Suit.CLUB, Face.TEN);
     public final static Card HEART_ACE = new Card(Suit.HEART, Face.ACE);
     public final static Card CLUB_NINE = new Card(Suit.CLUB, Face.NINE);
+    public final static Card CLUB_TWO = new Card(Suit.CLUB, Face.TWO);
 }
