5b89956551a3ca04387aa23c965d6c771af453ba
Kim Seunglae <obey1342@gmail.com>
2022-03-21 01:56:42 +0900

    refactor(Money): 1000원 단위만 입력하게 validate검사 추가

--- src/main/java/blackjack/domain/participant/Money.java
+++ src/main/java/blackjack/domain/participant/Money.java
@@ -4,6 +4,8 @@ import blackjack.utils.IntegerUtils;
 
 public class Money {
 
+    private static final int UNIT = 1000;
+
     private final int amount;
 
     public Money(String amount) {
@@ -11,7 +13,14 @@ public class Money {
     }
 
     public Money(int amount) {
+        validate(amount);
         this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        if (amount % UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 베팅 금액은 1000원 단위여야 합니다.");
+        }
     }
 
     public int getAmount() {
