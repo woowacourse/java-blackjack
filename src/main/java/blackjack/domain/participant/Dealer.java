b75406324d8f0d688d6c1691df98daabe1a0082b
hyeonic <dev.hyeonic@gmail.com>
2022-03-20 20:30:40 +0900

    refactor: Dealer 누락된 상수 추출

--- src/main/java/blackjack/domain/participant/Dealer.java
+++ src/main/java/blackjack/domain/participant/Dealer.java
@@ -4,14 +4,15 @@ import blackjack.domain.state.Ready;
 
 public class Dealer extends Participant {
 
-    private static final String DEALER_NAME = "딜러";
+    private static final Name DEALER_NAME = new Name("딜러");
+    private static final int DEALER_DRAWABLE_SCORE = 16;
 
     public Dealer() {
-        super(new Name(DEALER_NAME), new Ready());
+        super(DEALER_NAME, new Ready());
     }
 
     @Override
     public boolean isDrawable() {
-        return getTotalScore() <= 16;
+        return getTotalScore() <= DEALER_DRAWABLE_SCORE;
     }
 }
