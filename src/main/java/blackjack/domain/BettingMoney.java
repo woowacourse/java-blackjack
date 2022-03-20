3f07ccf80fc84f8ba95194d2880f15e0a1b7e205
swcho <progress0407@gmail.com>
2022-03-20 17:33:33 +0900

    refactor(BettingMoney): `validateInput` 메서드를 하위 단위 메서드 추출

--- src/main/java/blackjack/domain/BettingMoney.java
+++ src/main/java/blackjack/domain/BettingMoney.java
@@ -14,24 +14,36 @@ public class BettingMoney {
     private int value;
 
     public BettingMoney(String name, String inputMoney) {
-        validateMoney(inputMoney);
+        validateInput(inputMoney);
         this.ownerName = name;
         this.value = Integer.parseInt(inputMoney);
     }
 
-    private void validateMoney(String inputMoney) {
+    private void validateInput(String inputMoney) {
         Objects.requireNonNull(inputMoney, NOT_ALLOW_NULL_MESSAGE);
-        if (inputMoney.isBlank()) {
-            throw new IllegalArgumentException(NOT_ALLOW_BLANK_MESSAGE);
+        isBlank(inputMoney);
+        isNumber(inputMoney);
+        int money = Integer.parseInt(inputMoney);
+        isUnitOfThousand(money);
+    }
+
+    private void isUnitOfThousand(int money) {
+        if (money < BETTING_AMOUNT_UNIT || Math.floorMod(money, BETTING_AMOUNT_UNIT) != 0) {
+            throw new IllegalArgumentException(NOT_ALLOW_UNIT_OF_THOUSAND_MESSAGE);
         }
-        int money = 0;
+    }
+
+    private void isNumber(String inputMoney) {
         try {
-            money = Integer.parseInt(inputMoney);
+            Integer.parseInt(inputMoney);
         } catch (NumberFormatException e) {
             throw new NumberFormatException(NOT_ALLOW_NUMBER_MESSAGE);
         }
-        if (money < BETTING_AMOUNT_UNIT || Math.floorMod(money, BETTING_AMOUNT_UNIT) != 0) {
-            throw new IllegalArgumentException(NOT_ALLOW_UNIT_OF_THOUSAND_MESSAGE);
+    }
+
+    private void isBlank(String inputMoney) {
+        if (inputMoney.isBlank()) {
+            throw new IllegalArgumentException(NOT_ALLOW_BLANK_MESSAGE);
         }
     }
 
