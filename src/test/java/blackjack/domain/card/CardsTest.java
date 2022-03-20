1543b1d88ceb5cc01e0fdc113b4623149ef77520
DOES <ldk980130@gmail.com>
2022-03-20 19:12:47 +0900

    test: CardsTest 테스트 케이스 추가

--- src/test/java/blackjack/domain/card/CardsTest.java
+++ src/test/java/blackjack/domain/card/CardsTest.java
@@ -6,9 +6,33 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class CardsTest {
+
+	@Test
+	@DisplayName("Ace와 TEN을 받으면 합이 21이다.")
+	void sum() {
+		Cards cards = new Cards();
+		cards.add(getAce());
+		cards.add(getTen());
+
+		assertThat(cards.sum()).isEqualTo(21);
+	}
+
+	@Test
+	@DisplayName("숫자 합이 더 큰 Cards를 판별한다.")
+	void greaterThan() {
+		Cards cards1 = new Cards();
+		cards1.add(getAce());
+		cards1.add(getTen());
+
+		Cards cards2 = new Cards();
+		cards2.add(getAce());
+		cards2.add(getAce());
+		cards2.add(getTen());
+
+		assertThat(cards2.isGreaterThan(cards1)).isTrue();
+	}
 
 	@Test
 	@DisplayName("카드가 2장이고 번호 합이 21이면 블랙잭이다.")
