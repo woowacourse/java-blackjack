e952158ca13b1e1229437aacc57130f1bfe45794
Kim Seunglae <obey1342@gmail.com>
2022-03-21 00:44:58 +0900

    refactor(BlackjackGame): 참가자 로직 변경에 따른 수정

--- src/main/java/blackjack/domain/BlackjackGame.java
+++ src/main/java/blackjack/domain/BlackjackGame.java
@@ -1,58 +1,82 @@
 package blackjack.domain;
 
-import blackjack.domain.card.Deck;
 import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Participant;
 import blackjack.domain.participant.Player;
-import blackjack.domain.result.GameScoreBoard;
-import java.util.Arrays;
+import blackjack.domain.result.PlayerProfit;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class BlackjackGame {
 
-    private static final String DELIMITER = ",";
+    private static final int CHANGE_TO_NEGATIVE = -1;
 
-    private final List<Participant> players;
-    private final Participant dealer;
+    private final List<Player> players;
+    private final Dealer dealer;
 
-    public BlackjackGame(List<Participant> players, Participant dealer) {
+    public BlackjackGame(List<Player> players, Dealer dealer) {
         this.players = players;
         this.dealer = dealer;
     }
 
-    public static BlackjackGame create(String inputNames) {
-        List<Participant> players = Arrays.stream(inputNames.split(DELIMITER))
-            .map(playerName -> new Player(playerName.trim()))
-            .collect(Collectors.toList());
-        return new BlackjackGame(players, new Dealer());
+    public static BlackjackGame create(List<Player> players) {
+        return new BlackjackGame(players, Dealer.create());
     }
 
-    public void drawBaseCards(Deck deck) {
-        for (Participant player : players) {
-            drawBaseCardsByParticipant(deck, player);
+    public void drawBaseCards() {
+        drawCardsByParticipant(dealer);
+        players.forEach(player -> drawCardsByParticipant(dealer, player));
+    }
+
+    private void drawCardsByParticipant(Dealer dealer) {
+        if (!dealer.isReady()) {
+            dealer.hit(dealer.draw());
+            drawCardsByParticipant(dealer);
         }
-        drawBaseCardsByParticipant(deck, dealer);
     }
 
-    private void drawBaseCardsByParticipant(Deck deck, Participant participant) {
-        participant.receiveCard(deck.draw());
-        participant.receiveCard(deck.draw());
+    private void drawCardsByParticipant(Dealer dealer, Player player) {
+        if (!player.isReady()) {
+            player.hit(dealer.draw());
+            drawCardsByParticipant(dealer, player);
+        }
     }
 
-    public void takeMoreCard(Participant participant, Deck deck) {
-        participant.receiveCard(deck.draw());
+    public void takeMoreCard(Player player) {
+        player.hit(dealer.draw());
     }
 
-    public GameScoreBoard calculateGameScore() {
-        return GameScoreBoard.recordGameScore(dealer, players);
+    public void takeMoreCard() {
+        dealer.hit(dealer.draw());
     }
 
-    public List<Participant> getPlayers() {
+    public List<PlayerProfit> calculatePlayerProfit() {
+        return players.stream()
+            .map(player -> new PlayerProfit(player.getName(), player.calculateProfit(dealer)))
+            .collect(Collectors.toList());
+    }
+
+    public int calculateDealerProfit() {
+        return calculatePlayerProfit().stream()
+            .mapToInt(player -> player.getProfit() * CHANGE_TO_NEGATIVE)
+            .sum();
+    }
+
+    public List<Player> getPlayers() {
         return players;
     }
 
-    public Participant getDealer() {
+    public Dealer getDealer() {
         return dealer;
+    }
+
+    public Participant getParticipant() {
+        return dealer.getParticipant();
+    }
+
+    public List<Participant> getParticipants() {
+        return players.stream()
+            .map(Player::getParticipant)
+            .collect(Collectors.toList());
     }
 }
