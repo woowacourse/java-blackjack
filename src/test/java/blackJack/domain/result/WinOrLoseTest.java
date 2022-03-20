2e31a41da3d67ec686d9c0ca38f0cd18c23396d6
Wishoon <heonga2@gmail.com>
2022-03-20 11:55:52 +0900

    refactor : @Nested를 사용하여 테스트를 계층형 구조로 변경

--- src/test/java/blackJack/domain/result/WinOrLoseTest.java
+++ src/test/java/blackJack/domain/result/WinOrLoseTest.java
@@ -8,6 +8,7 @@ import blackJack.domain.card.Suit;
 import blackJack.domain.participant.Dealer;
 import blackJack.domain.participant.Player;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class WinOrLoseTest {
@@ -15,19 +16,141 @@ class WinOrLoseTest {
     private Dealer dealer = new Dealer();
     private Player player = new Player("rookie");
 
-    @Test
-    @DisplayName("플레이어의 점수가 블랙잭, 딜러의 점수가 14라 플레이어가 승리하는 경우")
-    void calculatePlayerBlackJackWin() {
-        player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
-        player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.THREE));
+    @Nested
+    @DisplayName("플레이어의 점수가 블랙잭이고")
+    class playerIsBlackjack {
+        @Test
+        @DisplayName("딜러의 점수가 블랙잭인 경우 플레이어는 무승부이다.")
+        void dealerIsBlackJackOfDraw() {
+            player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
+            player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
+            dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.ACE));
+            dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
 
-        assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.BLACKJACK);
+            assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.DRAW);
+        }
+
+        @Test
+        @DisplayName("딜러의 점수가 21점인 경우 플레이어가 승리한다(블랙잭).")
+        void dealerIs21ScoreOfBlackjack() {
+            player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
+            player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
+            dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.THREE));
+            dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.EIGHT));
+            dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
+
+            assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.BLACKJACK);
+        }
+
+        @Test
+        @DisplayName("딜러의 점수가 14점인 경우 플레이어가 승리한다(블랙잭).")
+        void dealerIs14ScoreOfBlackjack() {
+            player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
+            player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.THREE));
+
+            assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.BLACKJACK);
+        }
+    }
+
+    @Nested
+    @DisplayName("딜러의 점수가 블랙잭이고")
+    class dealerIsBlackjack {
+        @Test
+        @DisplayName("플레이어의 점수가 블랙잭인 경우 플레이어는 무승부이다.")
+        void playerIsBlackjackOfDraw() {
+            player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
+            player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
+            dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.ACE));
+            dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
+
+            assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.DRAW);
+        }
+
+        @Test
+        @DisplayName("플레이어의 점수가 21인 경우 플레이어가 패배한다.")
+        void playerIs21ScoreOfLose() {
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.THREE));
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.EIGHT));
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
+
+            assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.LOSE);
+        }
+
+        @Test
+        @DisplayName("플레이어의 점수가 14인 경우 플레이어가 패배한다.")
+        void playerIs14ScoreOfLose() {
+            player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
+            player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.THREE));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
+
+            assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.LOSE);
+        }
+    }
+
+    @Nested
+    @DisplayName("플레이어가 버스트이고")
+    class playerIsBust {
+        @Test
+        @DisplayName("딜러가 버스트인 경우 플레이어가 패배한다.")
+        void dealerIsBustOfLose() {
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.KING));
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.EIGHT));
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.TEN));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.SIX));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.KING));
+
+            assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.LOSE);
+        }
+
+        @Test
+        @DisplayName("딜러의 점수가 20인 경우 플레이어가 패배한다.")
+        void dealerIs21ScoreOfLose() {
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.KING));
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.EIGHT));
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.TEN));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
+
+            assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.LOSE);
+        }
+    }
+
+    @Nested
+    @DisplayName("딜러가 버스트이고")
+    class dealerIsBust {
+        @Test
+        @DisplayName("플레이어가 버스트인 경우 플레이어가 패배한다.")
+        void playerIsBustOfLose() {
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.KING));
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.EIGHT));
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.TEN));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.SIX));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.KING));
+
+            assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.LOSE);
+        }
+        @Test
+        @DisplayName("플레이어의 점수가 20인 경우 플레이어가 승리한다.")
+        void playerIs20ScoreOfWin() {
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.KING));
+            player.receiveCard(Card.valueOf(Suit.HEART, Denomination.TEN));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.TEN));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.SIX));
+            dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.KING));
+
+            assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.WIN);
+        }
     }
 
     @Test
-    @DisplayName("플레이어의 점수가 20, 딜러의 점수가 14여서 플레이어가 승리하는 경우")
+    @DisplayName("플레이어의 점수가 20, 딜러의 점수가 14인 경우 플레이어가 승리한다.")
     void calculatePlayerScoreWin() {
         player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.TEN));
         player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
@@ -38,95 +161,12 @@ class WinOrLoseTest {
     }
 
     @Test
-    @DisplayName("플레이어의 점수가 14, 딜러의 점수가 블랙잭이라 플레이가 패배하는 경우")
-    void calculateDealerBlackJackLose() {
-        player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
-        player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.THREE));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
-
-        assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.LOSE);
-    }
-
-    @Test
-    @DisplayName("플레이어의 점수가 14, 딜러의 점수가 20여서 플레이어가 패배하는 경우")
+    @DisplayName("플레이어의 점수가 14, 딜러의 점수가 20인 경우 플레이어가 패배한다.")
     void calculatePlayerScoreLose() {
         player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
         player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.THREE));
         dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.TEN));
         dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
-
-        assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.LOSE);
-    }
-
-    @Test
-    @DisplayName("플레이어가 블랙잭, 딜러도 블랙잭이여서 플레이어가 무승부 경우")
-    void calculatePlayerAndDealerScoreDraw() {
-        player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
-        player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
-        dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.ACE));
-        dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
-
-        assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.DRAW);
-    }
-
-    @Test
-    @DisplayName("플레이어가 블랙잭, 딜러의 점수가 21점이여서 플레이어가 승리하는 경우")
-    void calculatePlayerBlackJackAndDealerScoreWin() {
-        player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
-        player.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
-        dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.THREE));
-        dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.EIGHT));
-        dealer.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
-
-        assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.BLACKJACK);
-    }
-
-    @Test
-    @DisplayName("플레이어가 21, 딜러가 블랙잭 이여서 플레이어가 패배하는 경우")
-    void calculatePlayerScoreAndDealerBlackJackLose() {
-        player.receiveCard(Card.valueOf(Suit.HEART, Denomination.THREE));
-        player.receiveCard(Card.valueOf(Suit.HEART, Denomination.EIGHT));
-        player.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.ACE));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
-
-        assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.LOSE);
-    }
-
-    @Test
-    @DisplayName("플레이어가 버스트, 딜러의 점수가 20점이여서 플레이어가 패배하는 경우")
-    void calculatePlayerBustAndDealerScoreLose() {
-        player.receiveCard(Card.valueOf(Suit.HEART, Denomination.KING));
-        player.receiveCard(Card.valueOf(Suit.HEART, Denomination.EIGHT));
-        player.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.TEN));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.JACK));
-
-        assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.LOSE);
-    }
-
-    @Test
-    @DisplayName("플레이어의 점수가 20점, 딜러가 버스트여서 플레이어가 승리하는 경우")
-    void calculatePlayerScoreAndDealerBustWinner() {
-        player.receiveCard(Card.valueOf(Suit.HEART, Denomination.KING));
-        player.receiveCard(Card.valueOf(Suit.HEART, Denomination.TEN));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.TEN));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.SIX));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.KING));
-
-        assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.WIN);
-    }
-
-    @Test
-    @DisplayName("플레이어가 버스트, 딜러도 버스트여서 플레이어가 패배하는 경우")
-    void calculatePlayerBustAndDealerBustLose() {
-        player.receiveCard(Card.valueOf(Suit.HEART, Denomination.KING));
-        player.receiveCard(Card.valueOf(Suit.HEART, Denomination.EIGHT));
-        player.receiveCard(Card.valueOf(Suit.HEART, Denomination.JACK));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.TEN));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.SIX));
-        dealer.receiveCard(Card.valueOf(Suit.SPADE, Denomination.KING));
 
         assertThat(OutCome.ofJudge(dealer, player)).isEqualTo(OutCome.LOSE);
     }
