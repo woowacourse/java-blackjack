f6160c990ac61aa5736add573e3758027aa476b9
summerlunaa <summerlunaa9@gmail.com>
2022-03-20 17:18:34 +0900

    refactor: 점수 객체 생성

---
+++ src/main/java/blackJack/domain/card/Score.java
@@ -0,0 +1,64 @@
+package blackJack.domain.card;
+
+import java.util.Objects;
+
+public class Score implements Comparable<Score> {
+
+    private static final int BLACK_JACK = 21;
+    private static final int DEALER_MAXIMUM_RECEIVE_CARD_SCORE = 16;
+    private static final int STANDARD_SCORE_OF_CHANGE_ACE = 11;
+    private static final int OTHER_SCORE_OF_ACE_DENOMINATION = 11;
+
+    private final int score;
+
+    public Score(int score) {
+        this.score = score;
+    }
+
+    public boolean isBurst() {
+        return score > BLACK_JACK;
+    }
+
+    public boolean isBlackJack() {
+        return score == BLACK_JACK;
+    }
+
+    public boolean isDealerCanHit() {
+        return score <= DEALER_MAXIMUM_RECEIVE_CARD_SCORE;
+    }
+
+    public boolean isChangeAceScore() {
+        return score <= STANDARD_SCORE_OF_CHANGE_ACE;
+    }
+
+    public Score changeAceScore() {
+        return new Score(score + OTHER_SCORE_OF_ACE_DENOMINATION - Denomination.ACE.getScore());
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(score);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Score score1 = (Score) o;
+        return score == score1.score;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score);
+    }
+
+    @Override
+    public int compareTo(Score o) {
+        return Integer.compare(this.score, o.score);
+    }
+}
---
+++ src/test/java/blackJack/domain/card/ScoreTest.java
@@ -0,0 +1,55 @@
+package blackJack.domain.card;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.shouldHaveThrown;
+
+import blackJack.domain.result.MatchResult;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ScoreTest {
+
+    Score score;
+
+    @ParameterizedTest(name = "점수가 burst인지 확인한다.")
+    @CsvSource(value = {"20,false", "21,false", "22,true"})
+    void isBurst(int value, boolean expected) {
+        score = new Score(value);
+
+        assertThat(score.isBurst()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "점수가 21인지 확인한다.")
+    @CsvSource(value = {"20,false", "21,true", "22,false"})
+    void isBlackJack(int value, boolean expected) {
+        score = new Score(value);
+
+        assertThat(score.isBlackJack()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "딜러가 hit이 가능한 점수인지 확인한다.")
+    @CsvSource(value = {"16,true", "17,false"})
+    void isDealerCanHit(int value, boolean expected) {
+        score = new Score(value);
+
+        assertThat(score.isDealerCanHit()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "Ace가 11로 바뀌어야할 점수인지 확인한다.")
+    @CsvSource(value = {"11,true", "12,false"})
+    void isChangeAceScore(int value, boolean expected) {
+        score = new Score(value);
+
+        assertThat(score.isChangeAceScore()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Ace의 점수를 11로 바꿔 계산한 점수를 반환한다.")
+    void changeAceScore() {
+        score = new Score(11);
+
+        assertThat(score.changeAceScore()).isEqualTo(new Score(21));
+    }
+}
