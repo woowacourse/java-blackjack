2af04ca5e124577d5257241fdaab6283bc272ca3
SuyeonChoi <tndus970120@gmail.com>
2022-03-21 17:50:06 +0900

    feat: 유틸클래스 private 생성자 추가

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -9,6 +9,10 @@ import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class OutputView {
+
+    private OutputView() {
+    }
+
     public static void printErrorMessage(String message) {
         System.out.println(message);
     }
