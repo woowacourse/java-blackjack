98494085052457511b828c48b0bf446e99ed1628
leo0842 <kunga077@gmail.com>
2022-03-21 13:57:19 +0900

    refactor: 불필요한 메서드 삭제

--- src/main/java/blackjack/domain/machine/MatchJudge.java
+++ src/main/java/blackjack/domain/machine/MatchJudge.java
@@ -2,14 +2,13 @@ package blackjack.domain.machine;
 
 import java.util.Arrays;
 import java.util.function.BiPredicate;
-import java.util.function.Predicate;
 
 public enum MatchJudge {
 
-    BLACKJACK_WIN(MatchJudge::isBlackjack, 1.5),
-    LOSE(MatchJudge::isLose, -1),
-    DRAW(MatchJudge::isDraw, 0),
-    WIN(MatchJudge::isWin, 1),
+    BLACKJACK_WIN(Score::isBlackjackWin, 1.5),
+    LOSE(Score::isLose, -1),
+    DRAW(Score::isDraw, 0),
+    WIN(Score::isWin, 1),
     ;
 
     private final BiPredicate<Score, Score> condition;
@@ -24,32 +23,12 @@ public enum MatchJudge {
     public static MatchJudge judgeMatch(Score playerScore, Score dealerScore) {
 
         return Arrays.stream(MatchJudge.values())
-                .filter(findMatchResult(playerScore, dealerScore))
+                .filter(matchJudge -> matchJudge.condition.test(playerScore, dealerScore))
                 .findFirst()
                 .orElse(WIN);
     }
 
     public double calculateProfit(double money) {
         return money * rate;
-    }
-
-    private static Predicate<MatchJudge> findMatchResult(Score playerScore, Score dealerScore) {
-        return profit -> profit.condition.test(playerScore, dealerScore);
-    }
-
-    private static boolean isBlackjack(Score playerScore, Score dealerScore) {
-        return playerScore.isBlackjackWin(dealerScore);
-    }
-
-    private static boolean isLose(Score playerScore, Score dealerScore) {
-        return playerScore.isLose(dealerScore);
-    }
-
-    private static boolean isDraw(Score playerScore, Score dealerScore) {
-        return playerScore.isDraw(dealerScore);
-    }
-
-    private static boolean isWin(Score playerScore, Score dealerScore) {
-        return playerScore.isWin(dealerScore);
     }
 }
