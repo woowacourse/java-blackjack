489244ab9b178aa8c8214191ef91a45ca51e92c9
is2js <tingstyle1@gmail.com>
2022-03-21 17:20:00 +0900

    refactor: 메인 로직 내부의 상수 제거를 위해 Player#isNotFinished 추상메서드화

--- src/main/java/blackjack/domain/player/Dealer.java
+++ src/main/java/blackjack/domain/player/Dealer.java
@@ -5,10 +5,16 @@ import blackjack.domain.BetMoney;
 public class Dealer extends Player {
 
     private static final String NAME = "딜러";
+    private static final int GET_CARD_UPPER_BOUND = 17;
     private static final String NOT_ALLOW_METHOD_MESSAGE = "딜러는 배탱액을 조회할 수 없습니다.";
 
     public Dealer() {
         super(NAME);
+    }
+
+    @Override
+    public boolean isNotFinished() {
+        return playingCards.isNotFinishedWithBound(GET_CARD_UPPER_BOUND);
     }
 
     @Override
--- src/main/java/blackjack/domain/player/Gambler.java
+++ src/main/java/blackjack/domain/player/Gambler.java
@@ -4,11 +4,18 @@ import blackjack.domain.BetMoney;
 
 public class Gambler extends Player {
 
+    private static final int GAMBLER_GET_CARD_UPPER_BOUND = 21;
+
     private final BetMoney betMoney;
 
     public Gambler(final String name, final BetMoney betMoney) {
         super(name);
         this.betMoney = betMoney;
+    }
+
+    @Override
+    public boolean isNotFinished() {
+        return playingCards.isNotFinishedWithBound(GAMBLER_GET_CARD_UPPER_BOUND);
     }
 
     @Override
