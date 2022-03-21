169ce42ddf105f2e79ed72aa8a752118be6fbc0d
koo <gudonghee2000@gmail.com>
2022-03-21 16:44:58 +0900

    test: CardNumberTest 카드 숫자합 테스트 추가

--- src/test/java/blackjack/model/card/CardNumberTest.java
+++ src/test/java/blackjack/model/card/CardNumberTest.java
@@ -9,24 +9,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CardNumberTest {
 
-    @DisplayName("ACE, JACK의 합은 21 이다.")
-    @Test
-    void score_21() {
-        assertThat(calculateScore(List.of(ACE, JACK))).isEqualTo(21);
+    @ParameterizedTest(name = "{0}과 {1}을 합치면 21이다.")
+    @CsvSource(value = {"ACE,JACK", "ACE,TEN", "ACE,KING"})
+    void score_21(CardNumber first, CardNumber two) {
+        assertThat(calculateScore(List.of(first, two))).isEqualTo(21);
+    }
+
+    @ParameterizedTest(name = "{0}과 {1}을 합치면 21이 아니다.")
+    @CsvSource(value = {"ACE,NINE", "TWO,EIGHT", "ACE,ACE"})
+    void score_not_21(CardNumber first, CardNumber two) {
+        assertThat(calculateScore(List.of(first, two))).isNotEqualTo(21);
     }
 
     @DisplayName("ACE, JACK, TEN의 합은 21 이다.")
     @Test
     void score_21_2() {
         assertThat(calculateScore(List.of(ACE, JACK, TEN))).isEqualTo(21);
-    }
-
-    @DisplayName("ACE, ACE의 합은 12 이다.")
-    @Test
-    void score_12() {
-        assertThat(calculateScore(List.of(ACE, ACE))).isEqualTo(12);
     }
 }
