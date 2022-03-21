1c58d4416464002c6f0893b46f2225876737110c
jochungbeom <cndqjacndqja@gmail.com>
2022-03-21 17:47:28 +0900

    refactor: PlayerTest equals 피드백 반영

--- src/test/java/blackjack/domain/gamer/PlayerTest.java
+++ src/test/java/blackjack/domain/gamer/PlayerTest.java
@@ -24,10 +24,11 @@ class PlayerTest {
 
     @Test
     @DisplayName("플레이어와 이름이 같다면 같은 객체로 판단한다.")
-    void isSameName() {
-        Card card = new Card(CardShape.CLOVER, CardNumber.FIVE);
-        Player player1 = new Player("범고래", List.of(card), 1000);
-        Player player2 = new Player("범고래", List.of(card), 2000);
+    void equals() {
+        Card card1 = new Card(CardShape.CLOVER, CardNumber.FIVE);
+        Card card2 = new Card(CardShape.HEART, CardNumber.TEN);
+        Player player1 = new Player("범고래", List.of(card1), 1000);
+        Player player2 = new Player("범고래", List.of(card2), 2000);
         assertThat(player1).isEqualTo(player2);
     }
 
