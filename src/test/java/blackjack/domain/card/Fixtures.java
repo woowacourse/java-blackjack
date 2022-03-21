22005500be9b8ef3e6bada56b4196c7e5dcea138
leo0842 <kunga077@gmail.com>
2022-03-21 16:07:25 +0900

    refactor: 불필요한 공백 제거

--- src/test/java/blackjack/domain/card/Fixtures.java
+++ src/test/java/blackjack/domain/card/Fixtures.java
@@ -2,7 +2,6 @@ package blackjack.domain.card;
 
 public class Fixtures {
 
-
     public static final Card SPADE_JACK = new Card(Suit.SPADE, Rank.JACK);
     public static final Card SPADE_TEN = new Card(Suit.SPADE, Rank.TEN);
     public static final Card SPADE_EIGHT = new Card(Suit.SPADE, Rank.EIGHT);
@@ -11,5 +10,4 @@ public class Fixtures {
     public static final Card HEART_ACE = new Card(Suit.HEART, Rank.ACE);
     public static final Card SPADE_TWO = new Card(Suit.SPADE, Rank.TWO);
     public static final Card SPADE_FOUR = new Card(Suit.SPADE, Rank.FOUR);
-
 }
