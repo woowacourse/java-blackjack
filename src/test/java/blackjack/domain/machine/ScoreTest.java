791bc135c46af65e0c38fc017a7b240c345f3b0c
leo0842 <kunga077@gmail.com>
2022-03-21 16:08:13 +0900

    refactor: 불필요한 테스트 제거 및 인스턴스 변수 제거

--- src/test/java/blackjack/domain/machine/ScoreTest.java
+++ src/test/java/blackjack/domain/machine/ScoreTest.java
@@ -1,102 +1,64 @@
 package blackjack.domain.machine;
 
+import static blackjack.domain.card.Fixtures.SPADE_ACE;
+import static blackjack.domain.card.Fixtures.SPADE_EIGHT;
+import static blackjack.domain.card.Fixtures.SPADE_FOUR;
+import static blackjack.domain.card.Fixtures.SPADE_JACK;
+import static blackjack.domain.card.Fixtures.SPADE_TWO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Rank;
 import blackjack.domain.card.Suit;
 import java.util.Set;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ScoreTest {
 
-    private Score score;
-
-    @BeforeEach
-    void setUp() {
-        score = new Score(Set.of(new Card(Suit.SPADE, Rank.THREE), new Card(Suit.SPADE, Rank.FOUR)));
-    }
-
     @Test
     @DisplayName("스코어 클래스 생성 확인")
     void createScore() {
+        Score score = new Score(Set.of(SPADE_ACE, SPADE_JACK));
+
         assertThat(score).isInstanceOf(Score.class);
     }
 
     @Test
-    @DisplayName("점수 합산 확인")
-    void checkSumPoint() {
-        assertThat(score.value()).isEqualTo(7);
-    }
-
-    @Test
-    @DisplayName("score card 2개일 때 점수 합산")
-    public void checkSumScoreCardsPoint() {
-        score = new Score(Set.of(new Card(Suit.SPADE, Rank.EIGHT), new Card(Suit.SPADE, Rank.TWO)));
+    @DisplayName("card 점수 합산 확인")
+    public void checkSumPoints() {
+        Score score = new Score(Set.of(SPADE_EIGHT, SPADE_TWO));
         int sumPoint = score.value();
         assertThat(sumPoint).isEqualTo(10);
     }
 
     @Test
-    @DisplayName("score card 3개일 때 점수 합산")
-    public void checkSumScoreThreeCardsPoint() {
-        score = new Score(Set.of(new Card(Suit.SPADE, Rank.EIGHT), new Card(Suit.SPADE, Rank.TWO),
-                new Card(Suit.SPADE, Rank.JACK)));
-        int sumPoint = score.value();
-        assertThat(sumPoint).isEqualTo(20);
-    }
-
-    @Test
-    @DisplayName("ace 4개 일때 점수 확인")
-    public void checkPointsForFourAces() {
-        score = new Score(
-                Set.of(new Card(Suit.SPADE, Rank.ACE), new Card(Suit.CLUB, Rank.ACE), new Card(Suit.DIAMOND, Rank.ACE),
-                        new Card(Suit.HEART, Rank.ACE)));
-        int sumPoint = score.value();
-        assertThat(sumPoint).isEqualTo(14);
-    }
-
-    @Test
-    @DisplayName("ace 3개 일때 13점 점수 확인")
-    public void checkPointsForThreeAces() {
-        score = new Score(Set.of(new Card(Suit.SPADE, Rank.ACE), new Card(Suit.CLUB, Rank.ACE),
-                new Card(Suit.DIAMOND, Rank.ACE), new Card(Suit.DIAMOND, Rank.TWO)));
+    @DisplayName("ace 가 있을 때 21점 넘어가면 1점으로 계산 확인")
+    public void checkAcePointWillBeOne() {
+        Score score = new Score(Set.of(SPADE_ACE, SPADE_JACK, SPADE_FOUR));
         int sumPoint = score.value();
         assertThat(sumPoint).isEqualTo(15);
     }
 
     @Test
-    @DisplayName("ace 2개 일때 12점 점수 확인")
-    public void checkPointsForTwoAces() {
-        score = new Score(
-                Set.of(new Card(Suit.SPADE, Rank.ACE), new Card(Suit.CLUB, Rank.ACE),
-                        new Card(Suit.DIAMOND, Rank.NINE)));
+    @DisplayName("ace 가 있을 때 21점 넘어가지 않으면 11점으로 계산 확인")
+    public void checkAcePointWillBeEleven() {
+        Score score = new Score(Set.of(SPADE_ACE, SPADE_JACK, SPADE_FOUR));
         int sumPoint = score.value();
-        assertThat(sumPoint).isEqualTo(21);
-    }
-
-    @Test
-    @DisplayName("ace 2개 일때 2점 점수 확인")
-    public void checkPointsForTwoAcesOverLimit() {
-        score = new Score(Set.of(new Card(Suit.SPADE, Rank.ACE), new Card(Suit.CLUB, Rank.ACE),
-                new Card(Suit.DIAMOND, Rank.JACK)));
-        int sumPoint = score.value();
-        assertThat(sumPoint).isEqualTo(12);
+        assertThat(sumPoint).isEqualTo(15);
     }
 
     @Test
     @DisplayName("blackjack 카드 확인")
     public void checkBlackjackCards() {
-        score = new Score(Set.of(new Card(Suit.SPADE, Rank.ACE), new Card(Suit.SPADE, Rank.JACK)));
+        Score score = new Score(Set.of(new Card(Suit.SPADE, Rank.ACE), new Card(Suit.SPADE, Rank.JACK)));
         assertThat(score.isBlackjack()).isTrue();
     }
 
     @Test
     @DisplayName("3개 카드의 합이 21일 때 블랙잭이 아님을 확인")
     public void checkNoBlackjackWithThreeCards() {
-        score = new Score(Set.of(new Card(Suit.SPADE, Rank.ACE), new Card(Suit.SPADE, Rank.FIVE),
+        Score score = new Score(Set.of(new Card(Suit.SPADE, Rank.ACE), new Card(Suit.SPADE, Rank.FIVE),
                 new Card(Suit.SPADE, Rank.SIX)));
         assertThat(score.isBlackjack()).isFalse();
     }
