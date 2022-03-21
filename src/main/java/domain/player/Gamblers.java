78014024f0d26abb207ddedefacaea7287d39756
Richard JEON <ztzy1907@gmail.com>
2022-03-21 17:24:22 +0900

    refactor(blackjack-game): dto제거, 컨트롤러 제거

--- src/main/java/domain/player/Gamblers.java
+++ src/main/java/domain/player/Gamblers.java
@@ -5,18 +5,27 @@ import java.util.List;
 import java.util.Objects;
 
 public class Gamblers {
+    private static final int MAXIMUM_VALID_NUMBER_OF_GAMBLERS = 7;
     private static final String ERROR_NULL_OR_EMPTY_GAMBLERS = "[ERROR] 겜블러 목록을 확인해주세요";
+    private static final String ERROR_TOO_MANY_GAMBLERS = "[ERROR] 겜블러는 최대 7명까지 참여 가능합니다";
 
     private final List<Gambler> gamblers;
 
     public Gamblers(List<Gambler> gamblers) {
         this.gamblers = List.copyOf(gamblers);
         validateEmpty(gamblers);
+        validateNumberOfGamblers(gamblers);
     }
 
     private void validateEmpty(List<Gambler> gamblers) {
         if (Objects.isNull(gamblers) || gamblers.isEmpty()) {
             throw new IllegalArgumentException(ERROR_NULL_OR_EMPTY_GAMBLERS);
+        }
+    }
+
+    private void validateNumberOfGamblers(List<Gambler> gamblers) {
+        if (gamblers.size() > MAXIMUM_VALID_NUMBER_OF_GAMBLERS) {
+            throw new IllegalArgumentException(ERROR_TOO_MANY_GAMBLERS);
         }
     }
 
