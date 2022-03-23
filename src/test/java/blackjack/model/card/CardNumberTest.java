4ae4e1a7d55c084ea31996244bfe1169f7a5495c
koo <gudonghee2000@gmail.com>
2022-03-24 00:24:35 +0900

    test: CardNumber에 대한 테스트 추가

--- src/test/java/blackjack/model/card/CardNumberTest.java
+++ src/test/java/blackjack/model/card/CardNumberTest.java
@@ -1,34 +1,42 @@
 package blackjack.model.card;
 
 import static blackjack.model.card.CardNumber.ACE;
+import static blackjack.model.card.CardNumber.EIGHT;
+import static blackjack.model.card.CardNumber.FOUR;
 import static blackjack.model.card.CardNumber.JACK;
-import static blackjack.model.card.CardNumber.TEN;
-import static blackjack.model.card.CardNumber.calculateScore;
+import static blackjack.model.card.CardNumber.KING;
+import static blackjack.model.card.CardNumber.SIX;
+import static blackjack.model.card.CardNumber.THREE;
+import static blackjack.model.card.CardNumber.TWO;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CardNumberTest {
 
-    @ParameterizedTest(name = "{0}과 {1}을 합치면 21이다.")
-    @CsvSource(value = {"ACE,JACK", "ACE,TEN", "ACE,KING"})
-    void score_21(CardNumber first, CardNumber two) {
-        assertThat(calculateScore(List.of(first, two))).isEqualTo(21);
+    @ParameterizedTest
+    @MethodSource("generateCardNumbers")
+    void scoreTest(List<CardNumber> cardNumbers, int expectedScore) {
+        assertThat(calculateScore(cardNumbers)).isEqualTo(expectedScore);
     }
 
-    @ParameterizedTest(name = "{0}과 {1}을 합치면 21이 아니다.")
-    @CsvSource(value = {"ACE,NINE", "TWO,EIGHT", "ACE,ACE"})
-    void score_not_21(CardNumber first, CardNumber two) {
-        assertThat(calculateScore(List.of(first, two))).isNotEqualTo(21);
+    static Stream<Arguments> generateCardNumbers() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(ACE, JACK), 21),
+                Arguments.of(Arrays.asList(ACE, ACE), 12),
+                Arguments.of(Arrays.asList(TWO, EIGHT), 10),
+                Arguments.of(Arrays.asList(ACE, JACK, KING), 21),
+                Arguments.of(Arrays.asList(KING, JACK, SIX), 26),
+                Arguments.of(Arrays.asList(THREE, FOUR), 7)
+        );
     }
 
-    @DisplayName("ACE, JACK, TEN의 합은 21 이다.")
-    @Test
-    void score_21_2() {
-        assertThat(calculateScore(List.of(ACE, JACK, TEN))).isEqualTo(21);
+    static int calculateScore(List<CardNumber> cardNumbers) {
+        return CardNumber.calculateScore(cardNumbers);
     }
 }
