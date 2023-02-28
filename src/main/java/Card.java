c19915ce43f74cafb39c811fb98830d9b0488ecb
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-28 15:45:19 +0900

    feat: 카드 생성 로직 구현

---
+++ src/main/java/Card.java
@@ -0,0 +1,10 @@
+public class Card {
+
+    private final Shape shape;
+    private final Number number;
+
+    public Card(Shape shape, Number number) {
+        this.shape = shape;
+        this.number = number;
+    }
+}
