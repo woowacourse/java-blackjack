2c6eba3c9995d70a0ed9a29e236183d4d0bb096a
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-28 14:50:42 +0900

    feat: 이름 길이 검증 로직 구현

--- src/main/java/PlayerName.java
+++ src/main/java/PlayerName.java
@@ -1,4 +1,5 @@
 public class PlayerName {
+    private static final int MAX_LENGTH = 5;
     private final String name;
     public PlayerName(String name) {
         validate(name);
@@ -7,11 +8,18 @@ public class PlayerName {
 
     private void validate(String name) {
         validateIsNull(name);
+        validateLength(name);
     }
 
     private void validateIsNull(String name) {
         if(name == null){
             throw new NullPointerException("플레이어 이름은 null을 허용하지 않습니다.");
+        }
+    }
+
+    private void validateLength(String name) {
+        if(name.isBlank() || name.length() > MAX_LENGTH){
+            throw new IllegalArgumentException("이름 길이는 1자 이상 5자 이하여야 합니다.");
         }
     }
 }
