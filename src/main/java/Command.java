083e2bf0b954070e6d79165fbcef9a455dd26ecf
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-28 20:39:51 +0900

    feat: 입력값에 따른 Command 매핑 로직 구현

---
+++ src/main/java/Command.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public enum Command {
+    HIT("y"),
+    HOLD("n"),
+    ;
+
+    private final String command;
+
+    Command(String command) {
+        this.command = command;
+    }
+
+    public static Command from(String input) {
+        return Arrays.stream(Command.values())
+                .filter(c -> c.command.equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("y와 n만 입력 가능합니다."));
+    }
+}
