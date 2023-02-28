83e87550c09c9273d9d8d8d7e7ac05e166fd8b78
BackFoxx <backfoxxx@gmail.com>
2023-02-28 15:16:06 +0900

    feat : 넘버 객체 완성

---
+++ src/main/java/domain/Number.java
@@ -0,0 +1,22 @@
+package domain;
+
+public enum Number {
+    A(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    KING(10),
+    QUEEN(10),
+    JACK(10);
+
+    private final int score;
+
+    Number(int score) {
+        this.score = score;
+    }
+}
