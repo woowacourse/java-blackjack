ab04caa503178de7baf4e245da686d2f9130527e
kokodak <kokodakadokok@gmail.com>
2023-02-28 17:50:30 +0900

    feat: 금지된 이름 검증 기능 구현

--- src/test/java/blackjack/domain/NameTest.java
+++ src/test/java/blackjack/domain/NameTest.java
@@ -26,11 +26,20 @@ class NameTest {
         assertThatThrownBy(() -> new Name(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    void 이름이_딜러_라면_예외를_던진다() {
+        final String input = "딜러";
+
+        assertThatThrownBy(() -> new Name(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
 
 class Name {
 
     private static final int UPPER_BOUND = 10;
+    private static final String RESTRICT = "딜러";
 
     private final String value;
 
@@ -40,11 +49,26 @@ class Name {
     }
 
     private void validate(final String value) {
+        validateBlank(value);
+        validateLength(value);
+        validateRestrictWord(value);
+    }
+
+    private void validateBlank(final String value) {
         if (value.isBlank()) {
             throw new IllegalArgumentException("이름은 존재해야 합니다. 현재 이름: " + value);
         }
+    }
+
+    private void validateLength(final String value) {
         if (value.length() > UPPER_BOUND) {
             throw new IllegalArgumentException("이름은 " + UPPER_BOUND + "글자 이하여야 합니다. 현재 이름: " + value);
+        }
+    }
+
+    private void validateRestrictWord(final String value) {
+        if (value.equals(RESTRICT)) {
+            throw new IllegalArgumentException("이름은 " + RESTRICT + "일 수 없습니다. 현재 이름: " + value);
         }
     }
 }
