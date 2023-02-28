a88f01a64cb6f2de26230fc3d7e2b2bf3e429ac2
Eunkeee <eunkeeeee@gmail.com>
2023-02-28 15:25:23 +0900

    feat(Card): 특정 값과 모양을 가지는 객체 생성
    Co-Authored-By: Go-Jaecheol <gojaech@naver.com>

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,18 @@
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CardTest {
+    @DisplayName("카드의 값과 모양으로 카드를 생성한다.")
+    @Test
+    void createTest() {
+        assertDoesNotThrow(() -> new Card("A", "스페이드"));
+        assertThatThrownBy(() -> new Card("깃짱", "하트"))
+                .isInstanceOf(NoSuchElementException.class);
+        assertThatThrownBy(() -> new Card("11", "망고"))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+}
