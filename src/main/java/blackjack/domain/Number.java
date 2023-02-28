7ed1c7626a7d3850f651ecc2275e4ae90be334a3
kdkdhoho <hkim4410@naver.com>
2023-02-28 16:51:13 +0900

    feat(Number): Number 클래스 생성

---
+++ src/main/java/blackjack/domain/Number.java
@@ -0,0 +1,23 @@
+package blackjack.domain;
+
+public enum Number {
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    J(11),
+    Q(12),
+    K(13);
+
+    private final int number;
+
+    Number(final int number) {
+        this.number = number;
+    }
+}
