dc035e5e197fe69f94aa9ffd6f09e50a25ee2636
dev_kong <ahdjd5@gmail.com>
2023-02-28 16:22:47 +0900

    refa: 생성자에서 검증로직 분리

--- src/main/java/player/Name.java
+++ src/main/java/player/Name.java
@@ -4,10 +4,14 @@ public class Name {
     private final String value;
 
     public Name(String value) {
+        validateNameLength(value);
+        this.value = value;
+    }
+
+    private static void validateNameLength(String value) {
         if (value.length() < 1 || value.length() > 5) {
             throw new IllegalArgumentException("이름은 1글자 이상 5글자 이하만 가능합니다.");
         }
-        this.value = value;
     }
 
     public String getValue() {
