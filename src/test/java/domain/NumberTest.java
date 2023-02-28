a8aea8c0c5393fbe4e42d0ee5d2e933fb6ac4817
bjk1649 <qhwns1649@naver.com>
2023-02-28 16:13:10 +0900

    feat: 숫자를 나타내는 기능 추가

---
+++ src/test/java/domain/NumberTest.java
@@ -0,0 +1,25 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class NumberTest {
+    @Nested
+    class 점수반환 {
+        @ParameterizedTest(name = "ENUM->{0}, 점수->{1}")
+        @CsvSource(value = {"ACE:-1", "TWO:2", "THREE:3", "FOUR:4", "FIVE:5", "SIX:6", "SEVEN:7", "EIGHT:8", "NINE:9",
+                "TEN:10", "KING:10", "QUEEN:10", "JACK:10"}, delimiter = ':')
+        void test_score_should_가진점수반환_when_ACE를제외한숫자들의score호출(Number enumName, int expected) {
+            //given
+
+            //when
+            int actual = enumName.score();
+
+            //then
+            assertThat(actual).isEqualTo(expected);
+        }
+    }
+}
