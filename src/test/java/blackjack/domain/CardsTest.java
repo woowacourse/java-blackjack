56d18b4f6768fe10c4b12f3c4551929e15c6a3c3
kim0914 <kosa0914@naver.com>
2023-02-28 20:12:38 +0900

    test(Cards): 카드 패에서 에이스 개수 반환 테스트 추가

--- src/test/java/blackjack/domain/CardsTest.java
+++ src/test/java/blackjack/domain/CardsTest.java
@@ -42,7 +42,30 @@ public class CardsTest {
                 Arguments.of(List.of(new Card(Symbol.SPADE, Number.SEVEN), new Card(Symbol.CLOVER, Number.TWO)), 9),
                 Arguments.of(List.of(new Card(Symbol.HEART, Number.NINE), new Card(Symbol.HEART, Number.TWO)), 11),
                 Arguments.of(List.of(new Card(Symbol.DIAMOND, Number.TEN), new Card(Symbol.SPADE, Number.TEN)), 20),
-                Arguments.of(List.of(new Card(Symbol.CLOVER, Number.THREE), new Card(Symbol.CLOVER, Number.TWO)), 5)
+                Arguments.of(List.of(new Card(Symbol.CLOVER, Number.THREE), new Card(Symbol.CLOVER, Number.TWO)), 5),
+                Arguments.of(List.of(new Card(Symbol.CLOVER, Number.SEVEN), new Card(Symbol.SPADE, Number.SEVEN), new Card(Symbol.DIAMOND, Number.SEVEN)), 21),
+                Arguments.of(List.of(new Card(Symbol.CLOVER, Number.TEN), new Card(Symbol.SPADE, Number.ACE), new Card(Symbol.DIAMOND, Number.SEVEN)), 28)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateCardsWithACE")
+    @DisplayName("에이스가 포함된 카드의 줌수 합을 계산한다.")
+    void getSumWithACE(List<Card> createdCards, int expectedCount) {
+        Cards cards = new Cards();
+
+        for (Card card : createdCards) {
+            cards.add(card);
+        }
+
+        assertThat(cards.getAceCount()).isEqualTo(expectedCount);
+    }
+
+    static Stream<Arguments> generateCardsWithACE() {
+        return Stream.of(
+                Arguments.of(List.of(new Card(Symbol.SPADE, Number.ACE), new Card(Symbol.CLOVER, Number.ACE)), 2),
+                Arguments.of(List.of(new Card(Symbol.SPADE, Number.ACE), new Card(Symbol.HEART, Number.ACE), new Card(Symbol.CLOVER, Number.ACE)), 3),
+                Arguments.of(List.of(new Card(Symbol.SPADE, Number.ACE), new Card(Symbol.HEART, Number.ACE), new Card(Symbol.CLOVER, Number.ACE), new Card(Symbol.DIAMOND, Number.ACE)), 4)
         );
     }
 }
