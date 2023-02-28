c9e9ed9923539d292fd88f2cd0224a2af280e9b7
junpakPark <junpak.park@gmail.com>
2023-02-28 20:58:47 +0900

    feat : 추상 클래스 생성 및 상속 구현

--- src/main/java/domain/Participant.java
+++ src/main/java/domain/Participant.java
@@ -1,27 +1,12 @@
 package domain;
 
-import java.util.List;
-
-public class Participant implements Player {
-
-    private final Name name;
-    private final Cards cards;
+public final class Participant extends Player {
 
     private Participant(final Name name, final Cards cards) {
-        this.name = name;
-        this.cards = cards;
+        super(name, cards);
     }
 
     public static Participant from(final Name name) {
         return new Participant(name, new Cards());
-    }
-
-    @Override
-    public List<Card> displayCards() {
-        return cards.displayCards();
-    }
-
-    public void takeCard(final Card card) {
-        cards.takeCard(card);
     }
 }
--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -2,7 +2,24 @@ package domain;
 
 import java.util.List;
 
-@FunctionalInterface
-public interface Player {
-    List<Card> displayCards();
+public abstract class Player {
+    private final Name name;
+    private final Cards cards;
+
+    Player(final Name name, final Cards cards) {
+        this.name = name;
+        this.cards = cards;
+    }
+
+    public List<Card> displayCards() {
+        return cards.displayCards();
+    }
+
+    public void takeCard(final Card card) {
+        cards.takeCard(card);
+    }
+
+    public int getScore() {
+        return cards.getScore();
+    }
 }
