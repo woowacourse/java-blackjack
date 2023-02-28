be3d91eada728046c64b02aaf97c171348d2801f
kim0914 <kosa0914@naver.com>
2023-02-28 20:12:53 +0900

    feat(Cards): 카드 패에서 에이스 개수 반환 메서드 추가

---
+++ src/main/java/blackjack/domain/Cards.java
@@ -0,0 +1,33 @@
+package blackjack.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cards {
+
+    private final List<Card> cards = new ArrayList<>();
+
+    public void add(Card card) {
+        cards.add(card);
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (Card card : cards) {
+            sum += card.getNumber().getValue();
+        }
+
+        return sum;
+    }
+
+    public int getAceCount() {
+        return (int) cards.stream()
+                .filter(card -> card.getNumber().equals(Number.ACE))
+                .count();
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+}
