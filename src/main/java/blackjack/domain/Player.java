03cc80fda71c6707faf0fc0101a140f994bfbb62
kim0914 <kosa0914@naver.com>
2023-02-28 21:08:01 +0900

    feat(Player): 카드 추가, 스코어 계산 메서드 추가

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -2,12 +2,17 @@ package blackjack.domain;
 
 public class Player {
 
+    private static final int BLACK_JACK_SCORE = 21;
+    private static final int ACE_ALTER_VALUE = 10;
+
     private final String name;
+    private final Cards cards;
 
     public Player(String name) {
         validateSpace(name);
         validateLength(name);
         this.name = name;
+        this.cards = new Cards();
     }
 
     private void validateLength(String name) {
@@ -20,6 +25,25 @@ public class Player {
         if(name.isBlank() || name.contains(" ")) {
             throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
         }
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public int calculateScore() {
+        int sum = cards.sum();
+        int aceCount = cards.getAceCount();
+
+        while (sum > BLACK_JACK_SCORE && aceCount > 0) {
+            sum -= ACE_ALTER_VALUE;
+            aceCount -= 1;
+        }
+        return sum;
+    }
+
+    public Cards getCards() {
+        return cards;
     }
 
 }
