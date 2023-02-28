2bfac0f16ec401b3cd91209bfaafaa4330ccb66d
yoondgu <doyou2ing@naver.com>
2023-02-28 15:48:54 +0900

    feat: 카드 생성 기능 구현

---
+++ src/main/java/blackjack/domain/Card.java
@@ -0,0 +1,12 @@
+package blackjack.domain;
+
+public class Card {
+
+    private final Symbol symbol;
+    private final Number number;
+
+    public Card(Symbol symbol, Number number) {
+        this.symbol = symbol;
+        this.number = number;
+    }
+}
