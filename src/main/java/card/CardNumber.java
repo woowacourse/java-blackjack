9ed134f85b815b51a298aab5ab4b160a4e977fe7
dev_kong <ahdjd5@gmail.com>
2023-02-28 17:51:20 +0900

    feat: CardNumber getValue 기능 구현

--- src/main/java/card/CardNumber.java
+++ src/main/java/card/CardNumber.java
@@ -1,27 +1,33 @@
 package card;
 
 public enum CardNumber {
-    ACE("A"),
-    TWO("2"),
-    THREE("3"),
-    FOUR("4"),
-    FIVE("5"),
-    SIX("6"),
-    SEVEN("7"),
-    EIGHT("8"),
-    NINE("9"),
-    TEN("10"),
-    JACK("J"),
-    QUEEN("Q"),
-    KING("K");
+    ACE("A", 1),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10);
 
     private final String label;
+    private final int value;
 
-    CardNumber(String label) {
+    CardNumber(String label, int value) {
         this.label = label;
+        this.value = value;
     }
 
     public String getLabel() {
         return this.label;
+    }
+
+    public int getValue() {
+        return this.value;
     }
 }
--- src/test/java/card/CardNumberTest.java
+++ src/test/java/card/CardNumberTest.java
@@ -11,13 +11,25 @@ class CardNumberTest {
 
     @Test
     @DisplayName("ACE의 label A를 가져올 수 있다.")
-    void getValueOne() {
+    void getLabelOne() {
         assertThat(ACE.getLabel()).isEqualTo("A");
     }
 
     @Test
     @DisplayName("TWO의 label \"2\"를 가져올 수 있다.")
-    void getValueTwo() {
+    void getLabelTwo() {
         assertThat(TWO.getLabel()).isEqualTo("2");
+    }
+
+    @Test
+    @DisplayName("ACE의 value 1을 가져올 수 있다.")
+    void getValueOne() {
+        assertThat(ACE.getValue()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("TWO의 value 1을 가져올 수 있다.")
+    void getValueTwo() {
+        assertThat(TWO.getValue()).isEqualTo(2);
     }
 }
