65b0162247d9abc7d848b7c9d6eed1656fcbbac0
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-28 14:48:00 +0900

    test: 이름 길이 검증 테스트 구현

--- src/test/java/PlayerNameTest.java
+++ src/test/java/PlayerNameTest.java
@@ -1,6 +1,8 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PlayerNameTest {
 
@@ -10,5 +12,13 @@ public class PlayerNameTest {
         Assertions.assertThatThrownBy(() -> new PlayerName(null))
                 .isInstanceOf(NullPointerException.class);
     }
+
+    @ParameterizedTest(name = "이름의 길이는 1글자 이상 5글자 이하여야 합니다.")
+    @ValueSource(strings = {"", "123456"})
+    void createNameFailTestByLength(String name) {
+        Assertions.assertThatThrownBy(() -> new PlayerName(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 
 }
