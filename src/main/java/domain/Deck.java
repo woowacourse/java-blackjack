83d13c4831fad739f09445789cd4fe46bdb607cd
eunbii0213 <eunbii0213@naver.com>
2023-02-28 15:49:40 +0900

    refactor: Deck 초기화 메서드 리팩터링

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -1,19 +1,25 @@
 package domain;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Deck {
 
-    private final List<Card> deck = new ArrayList<>();
+    private final List<Card> deck;
 
     public Deck() {
-        Arrays.stream(Denomination.values())
-                .forEach(value -> Arrays.stream(Suit.values())
-                        .map(suit -> new Card(suit, value))
-                        .forEach(deck::add));
+        this.deck = createDeck();
         Collections.shuffle(deck);
+    }
+
+    private List<Card> createDeck() {
+        return Arrays.stream(Denomination.values())
+                .flatMap(
+                        denomination -> Arrays.stream(Suit.values())
+                                .map(suit -> new Card(suit, denomination))
+                )
+                .collect(Collectors.toList());
     }
 }
