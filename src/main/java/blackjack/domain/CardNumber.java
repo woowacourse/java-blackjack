fe3ed4808d69f44b2f7a2ea47bf772f4c22d06fb
BGuga <tmdwns9913@gmail.com>
2023-02-28 17:35:22 +0900

    refactor : 카드 숫자를 정적 팩터리 메서드를 통해서 캐싱된 값을 반환하도록 변경

--- src/main/java/blackjack/domain/CardNumber.java
+++ src/main/java/blackjack/domain/CardNumber.java
@@ -1,18 +1,56 @@
 package blackjack.domain;
 
-public class CardNumber {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class CardNumber implements Comparable<CardNumber> {
     private static final int MAX_RANGE = 13;
     private static final int MIN_RANGE = 1;
+
+    private static final List<CardNumber> cache;
     private final int value;
 
-    public CardNumber(final int value) {
+    static {
+        cache = new ArrayList<>();
+        for (int i = MIN_RANGE; i <= MAX_RANGE; i++) {
+            cache.add(new CardNumber(i));
+        }
+    }
+
+    private CardNumber(final int value) {
         validateRange(value);
         this.value = value;
+    }
+
+    public static CardNumber of(final int value) {
+        return cache.stream()
+                .filter(cardNumber -> cardNumber.value == value)
+                .findAny()
+                .orElseThrow(() -> new AssertionError());
     }
 
     private void validateRange(final int value) {
         if (value < MIN_RANGE || value > MAX_RANGE) {
             throw new IllegalArgumentException("범위를 초과하였습니다.");
         }
+    }
+
+    @Override
+    public int compareTo(final CardNumber o) {
+        return Integer.compare(this.value, o.value);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final CardNumber that = (CardNumber) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
--- src/test/java/blackjack/domain/CardNumberTest.java
+++ src/test/java/blackjack/domain/CardNumberTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Objects;
+
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class CardNumberTest {
@@ -16,7 +19,7 @@ public class CardNumberTest {
     @DisplayName("카드 숫자는 1부터 13사이의 숫자이다")
     void validCardNUmberTest(int value) {
         assertDoesNotThrow(()->{
-            new CardNumber(value);
+            CardNumber.of(value);
         });
     }
 
@@ -24,6 +27,31 @@ public class CardNumberTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 14})
     void invalidCardNumberTest(int value) {
-        assertThatThrownBy(() -> new CardNumber(value));
+        assertThatThrownBy(() -> CardNumber.of(value));
+    }
+
+    @Test
+    @DisplayName("CardNumber는 Comparable을 구현한 비교가능한 객체이다.")
+    void cardNumberComparableTest() {
+        final CardNumber targetNumber = CardNumber.of(2);
+
+        final CardNumber smallNumber = CardNumber.of(1);
+        final CardNumber sameNumber = CardNumber.of(2);
+        final CardNumber largerNumber = CardNumber.of(3);
+
+        assertAll(
+                ()-> assertThat(targetNumber.compareTo(smallNumber)).isGreaterThanOrEqualTo(1),
+                ()-> assertThat(targetNumber.compareTo(sameNumber)).isEqualTo(0),
+                ()-> assertThat(targetNumber.compareTo(largerNumber)).isLessThanOrEqualTo(-1)
+        );
+    }
+
+    @Test
+    @DisplayName("같은 값을 같는 CardNumber는 같은 주소값을 갖는 객체이다.")
+    void cardNumberTest() {
+        final CardNumber number1 = CardNumber.of(1);
+        final CardNumber number2 = CardNumber.of(1);
+
+        assertThat(number1 == number2).isTrue();
     }
 }
