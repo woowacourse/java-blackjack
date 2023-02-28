3f4c5570cab49b81059cb67c7e6bca63c2fbe3f5
“jaehee329” <jjhs9803@gmail.com>
2023-02-28 18:02:57 +0900

    test: Scores 점수 추가 테스트 작성

---
+++ src/test/java/domain/model/ScoreTest.java
@@ -0,0 +1,24 @@
+package domain.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ScoreTest {
+
+    @Test
+    @DisplayName("점수 추가를 테스트")
+    public void testAddScore() {
+        //given
+        int initialValue = 1;
+        Score score = new Score(initialValue);
+        int additionalValue = 2;
+
+        //when
+        score.add(additionalValue);
+
+        //then
+        assertThat(score.getValue()).isEqualTo(initialValue + additionalValue);
+    }
+}
