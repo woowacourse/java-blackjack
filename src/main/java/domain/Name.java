18a8fa7160ba1cde88f1baa16edf9c4072dc09a0
aiaiaiai1 <cvc100326@naver.com>
2023-02-28 17:00:33 +0900

    feat: 이름 공백 검증 기능 추가

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -2,13 +2,21 @@ package domain;
 
 public class Name {
     private static final String INVALID_NAME_LENGTH_MESSAGE = "이름길이는 5자를 초과 할 수 없습니다.";
+    private static final String INVALID_NAME_BLANK_MESSAGE = "이름은 공백일 수 없습니다.";
     private static final int MAX_LENGTH = 5;
 
     private final String name;
 
     public Name(String name) {
+        validateNameBlank(name);
         validNameLength(name);
         this.name = name;
+    }
+
+    private void validateNameBlank(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(INVALID_NAME_BLANK_MESSAGE);
+        }
     }
 
     private void validNameLength(String name) {
