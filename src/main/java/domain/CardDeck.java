125782b90b96948020d3702f5204cfb4d0aca466
aiaiaiai1 <cvc100326@naver.com>
2023-02-28 19:55:00 +0900

    feat: 카드덱 생성 기능 추가

---
+++ src/main/java/domain/CardDeck.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class CardDeck {
+    private final Queue<Card> cards;
+
+    public CardDeck(Queue<Card> cards) {
+        this.cards = cards;
+    }
+    
+}
