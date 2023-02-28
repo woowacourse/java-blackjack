10b27bde7dd4b02b8f7ce24df5a033cd6a2bc2ef
sosow0212 <sosow0212@naver.com>
2023-02-28 15:15:56 +0900

    feat: Name 객체 추가

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,32 @@
+package domain;
+
+import java.util.Objects;
+
+public class Name {
+
+    private static final int MAX_NAME_LENGTH = 10;
+
+    private final String name;
+
+    public Name(final String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(final String name) {
+        validateIsNullOrBlank(name);
+        validateNameLength(name);
+    }
+
+    private void validateIsNullOrBlank(final String name) {
+        if (Objects.isNull(name) || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 비어있을 수 없습니다.");
+        }
+    }
+
+    private void validateNameLength(final String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름의 길이는 10자를 초과할 수 없습니다.");
+        }
+    }
+}
---
+++ src/test/java/domain/NameTest.java
@@ -0,0 +1,42 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+class NameTest {
+
+    @DisplayName("요구사항에 맞게 이름의 조건을 충족하면 정상적으로 객체를 생성한다.")
+    @Test
+    void create_success() {
+        // when && then
+        assertThatNoException()
+                .isThrownBy(() -> new Name("pobi"));
+    }
+
+    @DisplayName("이름이 Null이거나 공백이면 예외를 반환한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void create_fail_by_blank_value(String input) {
+        // when & then
+        assertThatThrownBy(() -> new Name(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름은 비어있을 수 없습니다.");
+    }
+
+    @DisplayName("이름의 길이는 10자를 초과하면 예외를 반환한다.")
+    @Test
+    void dsa() {
+        // given
+        String givenName = "1234567890a";
+
+        // when & then
+        assertThatThrownBy(() -> new Name(givenName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름의 길이는 10자를 초과할 수 없습니다.");
+    }
+}
