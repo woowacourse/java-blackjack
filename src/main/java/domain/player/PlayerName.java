515a5a5c29dc1780e6133af0764ecd8a22434264
jiwon <ljwon77@daum.net>
2023-02-28 16:30:57 +0900

    feat: 플레이어의 이름의 길이를 검증한다
    Co-authored-by: apptie <appti@naver.com>

--- src/main/java/domain/player/PlayerName.java
+++ src/main/java/domain/player/PlayerName.java
@@ -4,11 +4,14 @@ import java.util.Objects;
 
 public class PlayerName {
 
+    private static final int MAX_LENGTH = 20;
+
     private final String name;
 
     private PlayerName(final String name) {
         String trimName = name.trim();
         validateBlankName(trimName);
+        validateNameLength(name);
         this.name = trimName;
     }
 
@@ -19,6 +22,12 @@ public class PlayerName {
     private void validateBlankName(final String name) {
         if (Objects.isNull(name) || name.isBlank()) {
             throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
+        }
+    }
+
+    private void validateNameLength(final String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("이름은 1글자에서 20글자 사이여야 합니다.");
         }
     }
 }
--- src/test/java/domain/player/PlayerNameTest.java
+++ src/test/java/domain/player/PlayerNameTest.java
@@ -2,6 +2,7 @@ package domain.player;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -13,5 +14,13 @@ class PlayerNameTest {
         assertThatThrownBy(() -> PlayerName.create(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 공백일 수 없습니다.");
+    }
+
+    @ParameterizedTest(name = "create()는 파라미터로 입력된 name의 길이가 1~20글자 사이가 아니라면 예외가 발생한다")
+    @ValueSource(strings = {"abcdeabcdeabcdeabcdea"})
+    void create_givenInvalidName_thenFail(final String invalidName) {
+        assertThatThrownBy(() -> PlayerName.create(invalidName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 1글자에서 20글자 사이여야 합니다.");
     }
 }
