30d5ba576e483724c3056b7a87aa2063085bb54f
aiaiaiai1 <cvc100326@naver.com>
2023-02-28 17:28:38 +0900

    feat: 카드 생성 기능 추가

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Card {
+    private final CardNumber cardNumber;
+    private final CardPattern cardPattern;
+
+    public Card(CardNumber cardNumber, CardPattern cardPattern) {
+        this.cardNumber = cardNumber;
+        this.cardPattern = cardPattern;
+    }
+}
