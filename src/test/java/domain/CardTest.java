856ecd74345afa278ccd968442d1c4e3bcc76cc8
yujamint <woawoa0513@gmail.com>
2023-02-28 19:48:49 +0900

    test: 블랙잭 게임 카드 생성 테스트 작성

---
+++ src/test/java/domain/CardTest.java
@@ -0,0 +1,37 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import domain.Card.CloverCard;
+import domain.Card.DiamondCard;
+import domain.Card.HeartCard;
+import domain.Card.SpadeCard;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("블랙잭 게임의 ")
+public class CardTest {
+    @Test
+    @DisplayName("하트 카드는 문양 당 13장을 생성한다.")
+    void generateHeartCardTest() {
+        assertThat(HeartCard.values().length).isEqualTo(13);
+    }
+
+    @Test
+    @DisplayName("다이아몬드 카드는 문양 당 13장을 생성한다.")
+    void generateDiamondCardTest() {
+        assertThat(DiamondCard.values().length).isEqualTo(13);
+    }
+
+    @Test
+    @DisplayName("클로버 카드는 문양 당 13장을 생성한다.")
+    void generateCloverCardTest() {
+        assertThat(CloverCard.values().length).isEqualTo(13);
+    }
+
+    @Test
+    @DisplayName("스페이드 카드는 문양 당 13장을 생성한다.")
+    void generateSpadeCardTest() {
+        assertThat(SpadeCard.values().length).isEqualTo(13);
+    }
+}
