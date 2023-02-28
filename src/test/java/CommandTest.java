35b1e98236903bae96fa7fe8052342361ea1f448
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-28 20:39:32 +0900

    test: 입력값에 따른 Command 매핑 테스트 구현

---
+++ src/test/java/CommandTest.java
@@ -0,0 +1,34 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CommandTest {
+
+    @DisplayName("입력값에 따라 명령이 생성된다. y -> HIT")
+    @Test
+    void createCommandHitSuccessTest() {
+        String input = "y";
+
+        Command command = Command.from(input);
+        Assertions.assertThat(command).isEqualTo(Command.HIT);
+    }
+
+    @DisplayName("입력값에 따라 명령이 생성된다. n -> HOLD")
+    @Test
+    void createCommandHoldSuccessTest() {
+        String input = "n";
+
+        Command command = Command.from(input);
+        Assertions.assertThat(command).isEqualTo(Command.HOLD);
+    }
+
+    @ParameterizedTest(name = "y 혹은 n 만 입력할 수 있다.")
+    @ValueSource(strings = {"yy","nn","hold","hit"})
+    void createCommandFailTest(String input) {
+        Assertions.assertThatThrownBy(() -> Command.from(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
