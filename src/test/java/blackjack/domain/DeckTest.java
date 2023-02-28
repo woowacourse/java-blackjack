16a04e4e2459b0fc14c498926ebaa984a784f7a2
kim0914 <kosa0914@naver.com>
2023-02-28 16:46:23 +0900

    test(Deck): 덱 생성 테스트 추가

---
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -0,0 +1,52 @@
+package blackjack.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class DeckTest {
+
+    @Test
+    @DisplayName("덱이 정상적으로 생성된다.")
+    void createDeck() {
+        int size = 52;
+        List<Card> cards = createCard(52);
+
+        Deck deck = Deck.create(cards);
+
+        assertThat(deck).isNotNull();
+    }
+
+    @Test
+    @DisplayName("덱이 생성되면 52장이 아니면 예외가 발생한다.")
+    void createDeckWithWrongSizeCards() {
+        List<Card> cards = createCard(51);
+        assertThatThrownBy(() -> Deck.create(cards))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("덱에 중복된 카드가 존재하면 예외가 발생한다.")
+    void createDeckWithDuplicatedCard() {
+        List<Card> cards = new ArrayList<>();
+        for (int i = 0; i < 51; i++) {
+            cards.add(new Card(Symbol.SPADE, Number.ACE));
+        }
+        assertThatThrownBy(() -> Deck.create(cards))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private List<Card> createCard(int size) {
+        List<Symbol> symbols = Symbol.getAll();
+        List<Number> numbers = Number.getAll();
+        List<Card> cards = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            cards.add(new Card(symbols.get(i / 13), numbers.get(i % 13)));
+        }
+        return cards;
+    }
+}
