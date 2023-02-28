f204aa2639db1cc0187336b692fd339f6a2088b5
kim0914 <kosa0914@naver.com>
2023-02-28 21:31:22 +0900

    test(Players): 참가자를 가지고 있는 객체 생성 테스트 추가

---
+++ src/test/java/blackjack/domain/PlayersTest.java
@@ -0,0 +1,43 @@
+package blackjack.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayersTest {
+
+    @Test
+    @DisplayName("플레이어가 1~7명이면 정상적으로 생성된다.")
+    void createPlayers() {
+        List<String> inputNames = List.of("gray", "luca", "pobi", "neo", "hoy");
+
+        Players players = Players.create(inputNames);
+
+        assertThat(players).isNotNull();
+    }
+
+    @ParameterizedTest
+    @MethodSource("generatePlayerNames")
+    @DisplayName("플레이어가 1~7명 이외의 경우에 예외가 발생한다 ")
+    void createPlayersWrongSize(List<String> inputNames) {
+        assertThatThrownBy(() -> Players.create(inputNames))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> generatePlayerNames() {
+        return Stream.of(
+                Arguments.of(Collections.EMPTY_LIST),
+                Arguments.of(List.of("a", "b", "c", "d", "e", "f", "g", "h"))
+        );
+    }
+}
