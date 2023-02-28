66b2063f7bc61a6f01543a9fcb4483f9d6b44ed2
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-28 15:44:48 +0900

    test: 카드 생성 테스트 구현

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,15 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CardTest {
+
+    @DisplayName("카드는 모양과 숫자를 가진다.")
+    @Test
+    void createCardSuccess() {
+        Card card = new Card(Shape.HEART, Number.A);
+        Assertions.assertThat(card).extracting("shape")
+                .isEqualTo(Shape.HEART);
+    }
+}
+
