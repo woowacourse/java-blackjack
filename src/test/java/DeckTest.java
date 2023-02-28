7c31405a8172bc7caac77ae7321733ead20a0082
sh111-coder <ohk9134@naver.com>
2023-02-28 17:45:07 +0900

    test(DeckTest): 일반 카드 숫자 추출 테스트

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,6 +1,8 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class DeckTest {
 
@@ -11,4 +13,10 @@ class DeckTest {
         Assertions.assertThat(Deck.cards.size()).isEqualTo(52);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"3하트, 3", "5다이아몬드, 5", "10클로버, 10"})
+    @DisplayName("각 카드에 해당하는 숫자를 정상적으로 추출한다.")
+    void shouldSuccessExtractCardNumber(String card, int cardNumber) {
+        Assertions.assertThat(Deck.extractCardNumber(card)).isEqualTo(cardNumber);
+    }
 }
