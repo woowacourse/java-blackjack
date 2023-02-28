e8620678a5efa9e4562890d9c99effc683b7d831
yujamint <woawoa0513@gmail.com>
2023-02-28 20:59:20 +0900

    test: 블랙잭 게임 카드 뽑기 기능 테스트 작성
    세부사항
    - isEmpty() 메서드를 private 메서드로 변경함에 따른 테스트 코드 삭제

--- src/test/java/domain/Card/CardsTest.java
+++ src/test/java/domain/Card/CardsTest.java
@@ -1,21 +1,22 @@
 package domain.Card;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CardsTest {
     @Test
-    @DisplayName("블랙잭 전체 카드를 생성한 직후에는 비어있으면 안된다.")
-    void generateCardsTest() {
+    @DisplayName("블랙잭 게임 카드는 52장까지 뽑을 수 있다.")
+    void drawCardTest() {
         //given
         Cards cards = new Cards();
 
         //when
-        boolean result = cards.isEmpty();
+        IntStream.range(0,52).forEach(index -> cards.drawCard());
 
         //then
-        assertThat(result).isFalse();
+        assertThrows(IllegalArgumentException.class, cards::drawCard);
     }
 }
