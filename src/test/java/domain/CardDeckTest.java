c8aec319ee6f305f45778ada29d3c04d9ffb4b02
aiaiaiai1 <cvc100326@naver.com>
2023-02-28 20:11:17 +0900

    test: 카드덱 맨 윗장 뽑는 테스트 추가

--- src/test/java/domain/CardDeckTest.java
+++ src/test/java/domain/CardDeckTest.java
@@ -1,8 +1,13 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CardDeckTest {
     @Test
     @DisplayName("카드덱을 생성한다.")
@@ -12,5 +17,20 @@ public class CardDeckTest {
         Assertions.assertDoesNotThrow(
                 () -> new CardDeck(generator.generate()));
     }
-    
+
+    @Test
+    @DisplayName("카드덱의 맨 윗장을 뽑는다.")
+    void pollCard() {
+        Queue<Card> cards = new LinkedList<>();
+        Card firstCard = new Card(CardNumber.ACE,CardPattern.SPADE);
+        Card secondCard = new Card(CardNumber.ACE,CardPattern.HEART);
+        cards.add(firstCard);
+        cards.add(secondCard);
+        CardDeck cardDeck = new CardDeck(cards);
+
+        assertThat(cardDeck.poll()).isEqualTo(firstCard);
+    }
+
+
+
 }
