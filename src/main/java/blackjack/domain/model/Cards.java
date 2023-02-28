42518b9f64c5a7d27a01ff810abd659c6973571d
waterricecake <fruturum@nate.com>
2023-02-28 17:38:46 +0900

    feat(Cards): 카드 추가 기능 구현

--- src/main/java/blackjack/domain/model/Cards.java
+++ src/main/java/blackjack/domain/model/Cards.java
@@ -2,11 +2,20 @@ package blackjack.domain.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Cards {
     private final List<Card> cards;
 
     public Cards() {
         this.cards = new ArrayList<>();
+    }
+
+    public void add(final Card card) {
+        cards.add(card);
+    }
+
+    public List<Card> getCards() {
+        return this.cards;
     }
 }
--- src/test/java/blackjack/domain/model/CardsTest.java
+++ src/test/java/blackjack/domain/model/CardsTest.java
@@ -1,8 +1,13 @@
 package blackjack.domain.model;
 
+import blackjack.domain.vo.Letter;
+import blackjack.domain.vo.Shape;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 public class CardsTest {
@@ -10,5 +15,20 @@ public class CardsTest {
     @DisplayName("패 생성 테스트")
     void constructCards() {
         assertThatNoException().isThrownBy(() -> new Cards());
+    }
+
+    @Test
+    @DisplayName("카드를 추가한다")
+    void addCardsTest() {
+        // given
+        Cards cards = new Cards();
+        Card card = new Card(Shape.CLOVER, Letter.ACE);
+        List<Card> expectedCards = List.of(card);
+
+        // when
+        cards.add(card);
+
+        // that
+        assertThat(expectedCards).isEqualTo(cards.getCards());
     }
 }
