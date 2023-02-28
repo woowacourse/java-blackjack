d42c7e5704e81ea044b76bf0d8c63f2f4935d965
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-28 15:22:53 +0900

    test: 참여자 수 검증 테스트 구현

--- src/test/java/PlayerNamesTest.java
+++ src/test/java/PlayerNamesTest.java
@@ -1,14 +1,34 @@
+import java.util.ArrayList;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PlayerNamesTest {
 
     @DisplayName("이름은 중복을 허용하지 않는다.")
     @Test
     void createNamesFailTestByDuplication() {
-        Assertions.assertThatThrownBy(() -> PlayerNames.from(List.of("pobi","pobi")))
+        Assertions.assertThatThrownBy(() -> PlayerNames.from(List.of("pobi", "pobi")))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "참여자 수는 1명 이상 8명 이하여야 합니다.")
+    @ValueSource(ints = {0, 9})
+    void createNamesFailTestByNumberOfPlayers(int count) {
+        Assertions.assertThatThrownBy(() -> PlayerNames.from(createPlayerNamesByCount(count)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    List<String> createPlayerNamesByCount(int count) {
+        List<String> names = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            names.add(String.valueOf(i));
+        }
+
+        return names;
     }
 }
