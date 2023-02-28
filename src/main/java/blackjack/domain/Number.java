b03697dba72530c20d9e536b0352276c5e8906ba
yoondgu <doyou2ing@naver.com>
2023-02-28 15:41:47 +0900

    feat: 카드 숫자 값 구현

--- src/main/java/blackjack/domain/Number.java
+++ src/main/java/blackjack/domain/Number.java
@@ -2,17 +2,27 @@ package blackjack.domain;
 
 public enum Number {
 
-    ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    KING,
-    QUEEN,
-    JACK
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    KING(10),
+    QUEEN(10),
+    JACK(10);
+
+    private final int value;
+
+    Number(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
--- src/test/java/blackjack/domain/NumberTest.java
+++ src/test/java/blackjack/domain/NumberTest.java
@@ -2,7 +2,9 @@ package blackjack.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.api.Assertions;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -13,12 +15,22 @@ class NumberTest {
 
     @DisplayName("카드는 숫자(2~10, Ace, King, Queen, Jack)을 가진다.")
     @Test
-    void should_HaveAllNumberValues() {
-        Assertions.assertThat(Number.values())
+    void should_HaveAllNumberTypes() {
+        assertThat(Number.values())
                 .containsExactlyInAnyOrder(
                         Number.ACE, Number.TWO, Number.THREE, Number.FOUR, Number.FIVE,
                         Number.SIX, Number.SEVEN, Number.EIGHT, Number.NINE,
                         Number.TEN, Number.KING, Number.QUEEN, Number.JACK
                 );
+    }
+
+    @DisplayName("각 숫자는 값을 가진다.")
+    @Test
+    void should_HaveAllNumberValues() {
+        List<Integer> numberValues = Arrays.stream(Number.values())
+                .map(Number::getValue)
+                .collect(Collectors.toList());
+        assertThat(numberValues)
+                .containsExactly(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10);
     }
 }
