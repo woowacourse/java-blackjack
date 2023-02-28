36c3b37d39518f69abc1a7fd70505884fe9f4eed
jyeost <pig9619@naver.com>
2023-02-28 16:09:39 +0900

    feat: 카드의 문양을 저장하는 Enum 추가

---
+++ src/main/java/blackjackgame/domain/Symbol.java
@@ -0,0 +1,18 @@
+package blackjackgame.domain;
+
+public enum Symbol {
+    HEART("하트"),
+    SPADE("스페이드"),
+    CLOVER("클로버"),
+    DIAMOND("다이아몬드");
+
+    private final String name;
+
+    Symbol(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
