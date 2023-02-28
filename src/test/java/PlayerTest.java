9ef2f878d0401007d02abdd7fe58a932633ca5e0
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-28 16:51:56 +0900

    test: 플레이어 카드 점수 계산 테스트 구현

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
+
     @DisplayName("플레이어는 카드를 받을 수 있다.")
     @Test
     void receiveCardSuccessTest(){
@@ -16,4 +17,18 @@ class PlayerTest {
                 .asList()
                 .hasSize(1);
     }
+
+    @DisplayName("플레이어가 받은 카드의 점수를 확인할 수 있다.")
+    @Test
+    void calculateScoreSuccessTest(){
+        Player player = new Player(new PlayerName("pobi"));
+
+        player.receiveCard(new Card(Shape.CLUB, Number.A));
+        player.receiveCard(new Card(Shape.HEART, Number.THREE));
+
+        Assertions.assertThat(player.calculateScore())
+                .isEqualTo(14);
+
+    }
+
 }
