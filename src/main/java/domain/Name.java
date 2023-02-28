a1bf32cf99133c9e46723fc68a99f625ee5d3a42
Wugawuga <dnr3655@naver.com>
2023-02-28 15:56:37 +0900

    feat: 이름 생성 구현
    Co-authored-by: rawfishthelgh <rg970604@naver.com>

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.Objects;
+
+public class Name {
+
+    private final String name;
+
+    public Name(final String name) {
+        if (Objects.isNull(name) || name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+}
---
+++ src/test/java/domain/NameTest.java
@@ -0,0 +1,37 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class NameTest {
+
+    @Test
+    void 이름이_5글자_초과시_예외를_발생한다() {
+        assertThatThrownBy(() -> new Name("jackson"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 이름이_null이거나_비어있을_경우_예외를_발생한다(String name) {
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "kt", "lee", "wuga", "hamad"})
+    void 이름은_1글자에서_5글자일_경우_생성된다(String name) {
+        assertDoesNotThrow(
+                () -> new Name(name)
+        );
+    }
+}
