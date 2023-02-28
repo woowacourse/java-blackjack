4ca2020b40f965c3252cad15f72083776144cd10
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-28 17:11:05 +0900

    feat : Card 동등성 검사 구현

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.util.Objects;
+
+public class Card {
+
+    private final String name;
+
+    public Card(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Card card = (Card) o;
+        return Objects.equals(name, card.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
