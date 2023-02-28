257a523f2e0ebe8ea6e06bbc88b749a71e64f4e3
eunbii0213 <eunbii0213@naver.com>
2023-02-28 17:23:51 +0900

    test: PlayerName 테스트 코드 작성

---
+++ src/test/java/domain/PlayerNameTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class PlayerNameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void playerName이_Blank_라면_예외처리(String nameInput) {
+        Assertions.assertThatThrownBy(() -> new PlayerName(nameInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void playerName이_Null이라면_예외처리() {
+        String nameInput = null;
+        Assertions.assertThatThrownBy(() -> new PlayerName(nameInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
