b70013b0e570d0e800eac036c51c39b21422a05c
junpakPark <junpak.park@gmail.com>
2023-02-28 16:39:28 +0900

    feat : Name 객체 구현

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,18 @@
+package domain;
+
+public class Name {
+
+    private final String name;
+
+    private Name(final String name) {
+        this.name = name;
+    }
+
+    public static Name from(final String name) {
+        return new Name(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
