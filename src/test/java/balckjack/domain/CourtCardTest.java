f9b9713a81238a1d08f015c1250a3c2f7ee047d4
amaran-th <songsy405@gmail.com>
2023-02-28 21:12:56 +0900

    test(CourtCardTest): 메서드명 수정 및 ParameterizedTest 적용

--- src/test/java/balckjack/domain/CourtCardTest.java
+++ src/test/java/balckjack/domain/CourtCardTest.java
@@ -9,14 +9,15 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class CourtCardTest {
 
-    @Test
-    void all() {
-        Assertions.assertThatNoException().isThrownBy(() -> new CourtCard("K"));
+    @ParameterizedTest
+    @ValueSource(strings = {"J", "Q", "K"})
+    void symbolIsValid(String input) {
+        Assertions.assertThatNoException().isThrownBy(() -> new CourtCard(input));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"k", "a", "A", "E"})
-    void allx(String input) {
+    void symbolIsInvalid(String input) {
         Assertions.assertThatThrownBy(() -> new CourtCard(input))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage(String.format("심볼은 J, Q, K 중 하나여야 합니다. 입력된 값 : %s", input));
