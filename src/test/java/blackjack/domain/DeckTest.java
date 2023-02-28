bf19efccb01af9dd60dd4bd09ff12d30fead5cbc
kim0914 <kosa0914@naver.com>
2023-02-28 17:37:33 +0900

    test(Deck): 덱에서 카드를 뽑는 테스트 추가

--- src/test/java/blackjack/domain/DeckTest.java
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class DeckTest {
 
@@ -14,7 +14,7 @@ public class DeckTest {
     @DisplayName("덱이 정상적으로 생성된다.")
     void createDeck() {
         int size = 52;
-        List<Card> cards = createCard(52);
+        List<Card> cards = createCard(size);
 
         Deck deck = Deck.create(cards);
 
@@ -38,6 +38,29 @@ public class DeckTest {
         }
         assertThatThrownBy(() -> Deck.create(cards))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("덱 가장 위에 있는 카드 한 장을 가져온다.")
+    void drawCardFromTopOfDeck() {
+        List<Card> cards = createCard(52);
+        Deck deck = Deck.create(cards);
+
+        Card card = deck.draw();
+
+        assertThat(card).isNotNull();
+    }
+
+    @Test
+    @DisplayName("덱에 카드가 없는데 카드를 뽑는 경우 예외가 발생한다.")
+    void drawCardFromEmptyDeck() {
+        List<Card> cards = createCard(52);
+        Deck deck = Deck.create(cards);
+        for (int i = 0; i < 52; i++) {
+            deck.draw();
+        }
+        assertThatThrownBy(() -> deck.draw())
+                .isInstanceOf(IllegalStateException.class);
     }
 
     private List<Card> createCard(int size) {
