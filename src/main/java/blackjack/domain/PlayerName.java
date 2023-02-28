81e37f9245e860dc833b17aa0a2cd4fb728ad3e4
seokhwan-an <asok369258@naver.com>
2023-02-28 20:59:47 +0900

    feat : 플레이어 이름 객체 생성

---
+++ src/main/java/blackjack/domain/PlayerName.java
@@ -0,0 +1,36 @@
+package blackjack.domain;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PlayerName {
+
+    public static final String PLAYER_NAME_ERROR_MESSAGE = "플레이어의 이름은 숫자,영어,한글만 가능합니다.";
+    private static final Pattern pattern = Pattern.compile("^[ㄱ-ㅎ가-힣a-zA-Z0-9]*$");
+    public static final String NULL_OR_BLANK_ERROR_MESSAGE = "null 또는 빈칸이 들어올 수 없습니다.";
+
+    private final String name;
+
+    public PlayerName(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        checkNull(name);
+        checkName(name);
+    }
+
+    private void checkName(String name) {
+        Matcher nameMatcher = pattern.matcher(name);
+        if (!nameMatcher.matches()) {
+            throw new IllegalArgumentException(PLAYER_NAME_ERROR_MESSAGE);
+        }
+    }
+
+    private void checkNull(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(NULL_OR_BLANK_ERROR_MESSAGE);
+        }
+    }
+}
---
+++ src/test/java/blackjack/domain/PlayerNameTest.java
@@ -0,0 +1,28 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PlayerNameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"아코", "beaver", "애쉬1"})
+    @DisplayName("올바른 이름일 때 생성 성공")
+    void createSuccess(String name) {
+        assertDoesNotThrow(() -> new PlayerName(name));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"!", " ", "", "   "})
+    @DisplayName("잘못된 이름일 때 생성 실패")
+    void createFalse(String name) {
+        assertThatThrownBy(() -> new PlayerName(name))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
