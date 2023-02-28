c68097e3f742b39d1136fdf5d92126bbc95013b4
kim0914 <kosa0914@naver.com>
2023-02-28 21:20:17 +0900

    feat(Dealer): 카드 추가, 스코어 계산 메서드 추가

---
+++ src/main/java/blackjack/domain/Dealer.java
@@ -0,0 +1,34 @@
+package blackjack.domain;
+
+public class Dealer {
+
+    private static final int BLACK_JACK_SCORE = 21;
+    private static final int ACE_ALTER_VALUE = 10;
+
+    private final Cards cards;
+
+    public Dealer() {
+        this.cards = new Cards();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public int calculateScore() {
+        int sum = cards.sum();
+        int aceCount = cards.getAceCount();
+
+        while (sum > BLACK_JACK_SCORE && aceCount > 0) {
+            sum -= ACE_ALTER_VALUE;
+            aceCount -= 1;
+        }
+        return sum;
+    }
+
+    public Cards getCards() {
+        return cards;
+    }
+
+
+}
