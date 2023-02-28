6295fdfa41f6b21428012fc8e912b5356d8c952d
amaran-th <songsy405@gmail.com>
2023-02-28 19:36:34 +0900

    refactor(NameTest): 메서드명 수정

--- src/test/java/balckjack/domain/NameTest.java
+++ src/test/java/balckjack/domain/NameTest.java
@@ -8,21 +8,21 @@ import org.junit.jupiter.params.provider.ValueSource;
 class NameTest {
 
     @Test
-    void create_instance() {
+    void createInstance() {
         Name name = new Name("아마란스");
         Assertions.assertThat(name).isInstanceOf(Name.class);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "     "})
-    void name_is_not_blank(String input) {
+    void nameIsNotBlank(String input) {
         Assertions.assertThatThrownBy(() -> new Name(input))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("이름은 공백이 될 수 없습니다.");
     }
 
     @Test
-    void name_is_not_null() {
+    void nameIsNotNull() {
         Assertions.assertThatThrownBy(() -> new Name(null))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("이름이 입력되지 않았습니다.");
