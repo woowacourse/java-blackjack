cb790f6a0d6efb34933de6696d20435082f469e1
aiaiaiai1 <cvc100326@naver.com>
2023-02-28 17:19:29 +0900

    feat: 카드 숫자와 문양 enum 타입 추가

---
+++ src/main/java/domain/CardPattern.java
@@ -0,0 +1,13 @@
+package domain;
+
+public enum CardPattern {
+    SPADE("스페이드"),
+    DIAMOND("다이아몬드"),
+    HEART("하트"),
+    CLOVER("클로버");
+
+    private final String pattern;
+    CardPattern(String pattern) {
+        this.pattern = pattern;
+    }
+}
