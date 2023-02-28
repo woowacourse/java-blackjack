6f0cd813902bd131ad2740d053e6ace7f000f099
aiaiaiai1 <cvc100326@naver.com>
2023-02-28 16:59:26 +0900

    test: 이름이 공백인경우 예외 발생 테스트 추가

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -1,8 +1,11 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,9 +13,18 @@ public class NameTest {
     @Test
     @DisplayName("이름길이가 5자를 초과하는 경우 예외가 발생한다.")
     void validateNameLengthTest() {
-        Assertions.assertThatThrownBy(() -> new Name("pobipobi"))
+        assertThatThrownBy(() -> new Name("pobipobi"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름길이는 5자를 초과 할 수 없습니다.");
 
+    }
+
+    @ParameterizedTest
+    @DisplayName("이름이 공백일 수 없다.")
+    @NullAndEmptySource
+    void validateNameBlank(String name) {
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(Exception.class)
+                .hasMessage("이름은 공백일 수 없습니다.");
     }
 }
