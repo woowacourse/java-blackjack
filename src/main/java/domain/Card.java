1a2ddc95da13da434110b06dd23214e761511a0a
jaeyeon kim <jakind@naver.com>
2023-02-28 17:43:43 +0900

    feat: Card 를 생성한다.
    - domain.Card
    - domain.Shape
    - domain.Value

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,44 @@
+package domain;
+
+import java.util.Objects;
+
+public class Card {
+
+    private final Shape shape;
+    private final Value value;
+
+    public Card(Shape shape, Value value) {
+        this.shape = shape;
+        this.value = value;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return shape == card.shape && value == card.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(shape, value);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "shape=" + shape +
+                ", value=" + value +
+                '}';
+    }
+
+}
---
+++ src/main/java/domain/Shape.java
@@ -0,0 +1,8 @@
+package domain;
+
+public enum Shape {
+    CLOVER,
+    DIAMOND,
+    HEART,
+    SPADE
+}
