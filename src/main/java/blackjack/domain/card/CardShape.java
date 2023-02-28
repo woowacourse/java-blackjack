b4ebcff45663ca6506b1d44c058a702c1da6e26a
songusika <sws981202@gmail.com>
2023-02-28 16:48:03 +0900

    refactor: 카드 숫자, 모양 분리
    Co-authored-by: choi-jjunho <junho5336@gmail.com>

---
+++ src/main/java/blackjack/domain/card/CardShape.java
@@ -0,0 +1,8 @@
+package blackjack.domain.card;
+
+public enum CardShape {
+    SPADE,
+    DIAMOND,
+    HEART,
+    CLOVER;
+}
--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -1,11 +1,9 @@
 package blackjack.domain.card;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -14,52 +12,22 @@ class CardTest {
     @Test
     void 카드는_숫자를_가진다() {
         // given
-        Card card = new Card(1, "다이아몬드");
-
-        // when
-        int number = card.getNumber();
+        final CardNumber number = CardNumber.SIX;
+        Card card = new Card(number, CardShape.DIAMOND);
 
         // then
-        assertThat(number).isEqualTo(1);
+        assertThat(card.getNumber()).isEqualTo(number);
     }
 
     @Test
     void 카드는_모양을_가진다() {
         // given
-        Card card = new Card(1, "다이아몬드");
+        Card card = new Card(CardNumber.SIX, CardShape.DIAMOND);
 
         // when
-        String shape = card.getShape();
+        CardShape shape = card.getShape();
 
         // then
-        assertThat(shape).isEqualTo("다이아몬드");
+        assertThat(shape).isEqualTo(CardShape.DIAMOND);
     }
-
-    @Nested
-    class 카드의_숫자가 {
-        @Test
-        void _1미만이면_예외() {
-            // then
-            assertThatThrownBy(() -> new Card(0, "다이아몬드"))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
-
-        @Test
-        void _10초과면_예외() {
-            // then
-            assertThatThrownBy(() -> new Card(11, "다이아몬드"))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
-    }
-
-    @Nested
-    class 카드의_모양이 {
-        @Test
-        void _아닐_경우_예외() {
-            // then
-            assertThatThrownBy(() -> new Card(1, "코끼리"))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
-    }
-
 }
