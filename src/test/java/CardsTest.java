811cb60df057f419dd9e35353bb6822c9b936cc4
Eunkeee <eunkeeeee@gmail.com>
2023-02-28 16:13:00 +0900

    test(Cards): 테스트 항목 추가
    Co-Authored-By: Go-Jaecheol <gojaech@naver.com>

--- src/test/java/CardsTest.java
+++ src/test/java/CardsTest.java
@@ -1,13 +1,18 @@
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 
 class CardsTest {
-    @Test
-    void createTest() {
+
+    @DisplayName("52개의 카드를 생성할 수 있다.")
+    @ParameterizedTest
+    @CsvSource({"A,스페이드", "2,하트", "3,다이아몬드", "J,하트", "K,클로버"})
+    void createCardsTest(String value, String shape) {
         Cards cards = new Cards();
-        Card card = new Card("A", "스페이드");
+        Card card = new Card(value, shape);
         assertThat(cards.contains(card)).isTrue();
     }
 }
