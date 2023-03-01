a4c86d0160d50bb65fefafbcbfbb5bc1c05088c0
ReO <yt930310@gmail.com>
2023-03-01 17:07:03 +0900

    test(Participant): Participant에 대한 테스트 코드 작성

---
+++ src/test/java/domain/ParticipantTest.java
@@ -0,0 +1,29 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ParticipantTest {
+    private Participant participant;
+
+    @BeforeEach
+    void setUp() {
+        participant = new Participant(new ArrayList<>(List.of(new Card("2하트", 2), new Card("K다이아", 10))));
+    }
+
+    @Test
+    void receiveCard() {
+        participant.receiveCard(new Card("10클로버", 10));
+        assertThat(participant.getCardNames().size()).isEqualTo(3);
+    }
+
+    @Test
+    void getHandValue() {
+        assertThat(participant.getHandValue()).isEqualTo(12);
+    }
+}
