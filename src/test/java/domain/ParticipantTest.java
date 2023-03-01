8df2cc5eb4d266a7511ac80ac52a7b06ce2fb5e5
ReO <yt930310@gmail.com>
2023-03-02 01:31:04 +0900

    test: 참가자들의 유일성을 보장하기 위해서 오버라이딩

--- src/test/java/domain/ParticipantTest.java
+++ src/test/java/domain/ParticipantTest.java
@@ -26,4 +26,15 @@ class ParticipantTest {
     void getHandValue() {
         assertThat(participant.getHandValue()).isEqualTo(12);
     }
+
+    @Test
+    void equalsTest() {
+        assertThat(new Dealer().equals(new Dealer())).isTrue();
+    }
+
+    @Test
+    void eqaulsTestWhenDifferentHand() {
+        assertThat(new Participant(new ArrayList<>(List.of(new Card("2하트", 2), new Card("K다이아", 10))), "Leo").equals(
+            new Participant(new ArrayList<>(List.of(new Card("4하트", 4), new Card("2다이아", 2))), "Leo"))).isTrue();
+    }
 }
