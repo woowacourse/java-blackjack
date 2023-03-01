6d39f300f5838845755d1d803f3bd4a55b4ac904
xxeol2 <jhj01177@naver.com>
2023-03-01 17:05:35 +0900

    feat: 참여자 이름 중복 및 인원수 유효성 검사 기능 구현
    Co-authored-by: kiarakim <kiarakim413@gmail.com>
    Co-authored-by: hgo641 <hgo641@gmail.com>

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -2,7 +2,9 @@ package domain;
 
 import static java.util.stream.Collectors.collectingAndThen;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Players {
@@ -14,8 +16,27 @@ public class Players {
     }
 
     public static Players from(final List<String> names) {
+        validate(names);
         return names.stream()
             .map(Player::new)
             .collect(collectingAndThen(Collectors.toList(), Players::new));
+    }
+
+    private static void validate(List<String> names) {
+        validateDuplication(names);
+        validateSize(names);
+    }
+
+    private static void validateDuplication(List<String> names) {
+        Set<String> distinctNames = new HashSet<>(names);
+        if (distinctNames.size() != names.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateSize(List<String> names) {
+        if (names.size() < 1 || names.size() > 4) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,9 +1,13 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -13,7 +17,7 @@ public class PlayersTest {
     @ParameterizedTest
     @MethodSource("parameterProvider")
     void playerCount1_4(List<String> names) {
-        Assertions.assertThatNoException()
+        assertThatNoException()
             .isThrownBy(() -> Players.from(names));
     }
 
@@ -22,5 +26,24 @@ public class PlayersTest {
             List.of("a"),
             List.of("a", "kiara", "ash", "woowa")
         );
+    }
+
+    @DisplayName("참여 인원은 1명미만이 될 수 없다")
+    @Test
+    void playerCount_shouldNotBeUnder1() {
+        assertThatThrownBy(() -> Players.from(Collections.emptyList()));
+    }
+
+    @DisplayName("참여 인원은 4명초과가 될 수 없다")
+    @Test
+    void playerCount_shouldNotBeOver4() {
+        assertThatThrownBy(() -> Players.from(List.of("a", "b", "c", "d", "e")));
+    }
+
+    @DisplayName("플레이어의 이름은 중복될 수 없다")
+    @Test
+    void validateDuplication() {
+        assertThatThrownBy(() -> Players.from(List.of("hongo", "hongo", "ash", "kiara")))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
