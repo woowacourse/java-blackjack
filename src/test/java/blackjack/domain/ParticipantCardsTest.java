c35f4e63e775109043317664da7a0dec057609c5
hyunseo <goatsfish@gmail.com>
2023-03-01 19:53:25 +0900

    test: 참여자의 카드 숫자 합을 구하는 기능 테스트 추가
    Co-authored-by: Cyma-s <ssol0319@naver.com>

--- src/test/java/blackjack/domain/ParticipantCardsTest.java
+++ src/test/java/blackjack/domain/ParticipantCardsTest.java
@@ -2,11 +2,14 @@ package blackjack.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("참여자 카드")
 class ParticipantCardsTest {
@@ -46,5 +49,28 @@ class ParticipantCardsTest {
 
         assertThatThrownBy(() -> participantCards.receive(new Card(CardShape.DIAMOND, CardNumber.THREE)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("calculateDummy")
+    @DisplayName("가지고 있는 카드의 합을 계산한다.")
+    void calculate(final List<Card> cards, final int expectedSum) {
+        ParticipantCards participantCards = new ParticipantCards(cards);
+        int cardSum = participantCards.calculate();
+
+        assertThat(cardSum).isEqualTo(expectedSum);
+    }
+
+    static Stream<Arguments> calculateDummy() {
+        return Stream.of(
+                Arguments.arguments(List.of(
+                        new Card(CardShape.DIAMOND, CardNumber.THREE),
+                        new Card(CardShape.HEART, CardNumber.FOUR)
+                ), CardNumber.THREE.getValue() + CardNumber.FOUR.getValue()),
+                Arguments.arguments(List.of(
+                        new Card(CardShape.SPADE, CardNumber.ACE),
+                        new Card(CardShape.CLOVER, CardNumber.FOUR)
+                ), CardNumber.ACE.getValue() + CardNumber.FOUR.getValue())
+        );
     }
 }
