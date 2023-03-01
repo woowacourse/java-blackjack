09780779233fa6432220e882a826b5ec28df4636
ReO <yt930310@gmail.com>
2023-03-01 20:55:47 +0900

    test(Hand): 카드 합에 따라 에이스를 1 또는 11로 사용하는 기능 테스트 작성

--- src/test/java/domain/HandTest.java
+++ src/test/java/domain/HandTest.java
@@ -4,9 +4,14 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class HandTest {
 
@@ -14,7 +19,12 @@ class HandTest {
 
     @BeforeEach
     void setUp() {
-        hand = new Hand(new ArrayList<>(List.of(new Card("2하트", 2), new Card("K다이아", 10))));
+        hand = new Hand(new ArrayList<>(
+            List.of(
+                new Card("2하트", 2),
+                new Card("K다이아", 10)
+            ))
+        );
     }
 
     @Test
@@ -23,8 +33,17 @@ class HandTest {
         assertThat(hand.getCards().size()).isEqualTo(3);
     }
 
-    @Test
-    void calculateValue() {
-        assertThat(hand.calculateValue()).isEqualTo(12);
+    @ParameterizedTest
+    @MethodSource("cardProvider")
+    void calculateValue(Card card, int handValue) {
+        hand.addCard(card);
+        assertThat(hand.calculateValue()).isEqualTo(handValue);
+    }
+
+    private static Stream<Arguments> cardProvider() {
+        return Stream.of(
+            Arguments.of(new Card("A클로버", 11),13),
+            Arguments.of(new Card("9하트", 9),21)
+        );
     }
 }
