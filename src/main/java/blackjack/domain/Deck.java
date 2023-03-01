9d3b80efab9f2036274fff3a9ff0ef3f577337a4
hyunseo <goatsfish@gmail.com>
2023-03-01 16:00:41 +0900

    feat: 덱 카드 뽑기 기능 추가
    Co-authored-by: Cyma-s <ssol0319@naver.com>

---
+++ src/main/java/blackjack/domain/Deck.java
@@ -0,0 +1,23 @@
+package blackjack.domain;
+
+import java.util.*;
+
+public class Deck {
+    private static final int TOP = 0;
+    private final List<Card> cards = new ArrayList<>();
+
+    public Deck() {
+        for (CardShape shape : CardShape.values()) {
+            Arrays.stream(CardNumber.values())
+                    .forEach(number -> cards.add(new Card(shape, number)));
+        }
+        Collections.shuffle(cards);
+    }
+
+    public Card draw() {
+        if (cards.isEmpty()) {
+            throw new IllegalStateException("덱이 비어 있을 때 카드를 뽑을 수 없습니다.");
+        }
+        return cards.remove(TOP);
+    }
+}
