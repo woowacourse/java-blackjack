dab72489811a8243fa82154b65379a8e8fb4a6c0
Woojin Lee <dhkep03@gmail.com>
2023-03-01 09:55:19 +0900

    feat: CardNumber 구현

---
+++ src/main/java/CardNumber.java
@@ -0,0 +1,31 @@
+public enum CardNumber {
+    ACE("A", 1),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10);
+
+    private final String numberToPrint;
+    private final int score;
+
+    CardNumber(String numberToPrint, int score) {
+        this.numberToPrint = numberToPrint;
+        this.score = score;
+    }
+
+    public String getNumberToPrint() {
+        return numberToPrint;
+    }
+
+    public int getScore(){
+        return score;
+    }
+}
---
+++ src/test/java/CardNumberTest.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardNumberTest {
+
+    @DisplayName("13개의 숫자가 존재한다.")
+    @Test
+    void 모든_숫자_존재(){
+        String[] numbers = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        List<String> numberToPrints = Arrays.stream(CardNumber.values())
+                .map(CardNumber::getNumberToPrint)
+                .collect(Collectors.toList());
+        assertThat(numberToPrints).containsExactlyInAnyOrder(numbers);
+    }
+
+    @DisplayName("각 카드는 점수를 가진다.")
+    @ParameterizedTest
+    @CsvSource(value = {"ACE:1", "TWO:2", "THREE:3", "FOUR:4", "FIVE:5", "SIX:6", "SEVEN:7", "EIGHT:8", "NINE:9", "TEN:10", "JACK:10", "QUEEN:10", "KING:10"}, delimiter = ':')
+    void 모든_카드_점수_존재(CardNumber cardNumber, int score){
+        assertThat(cardNumber.getScore()).isEqualTo(score);
+    }
+}
