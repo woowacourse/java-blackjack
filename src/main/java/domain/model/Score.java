d9fe49e32f5c4f584fd1628488190be28eebfed6
“jaehee329” <jjhs9803@gmail.com>
2023-03-01 17:44:15 +0900

    feat: 열거형 생성에 따른 패키지 변화

--- src/main/java/domain/model/Score.java
+++ src/main/java/domain/model/Score.java
@@ -1,15 +1,31 @@
 package domain.model;
 
+import domain.type.Letter;
+
 public class Score {
 
-    private int value;
+    public static final int BLACK_JACK = 21;
+    public static final int ACE_SUB_NUMBER = 1;
+    private final int value;
 
-    public Score(final int value) {
+    private Score(final int value) {
         this.value = value;
     }
 
-    public void add(final int additionalValue) {
-        this.value += additionalValue;
+    public static Score of(final Cards cards) {
+        int score = getInitialScore(cards);
+        if (score > BLACK_JACK && cards.contains(Letter.ACE)) {
+            score -= Letter.ACE.getNumber();
+            score += ACE_SUB_NUMBER;
+        }
+        return new Score(score);
+    }
+
+    private static int getInitialScore(final Cards cards) {
+        return cards.getCards()
+            .stream()
+            .mapToInt(Card::getNumber)
+            .sum();
     }
 
     public int getValue() {
