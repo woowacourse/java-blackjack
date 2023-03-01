f3cdaae4bcaaf002d3c18292e4c030cf89625994
ReO <yt930310@gmail.com>
2023-03-01 17:01:38 +0900

    feat(Hand): 손패에 카드를 더하는 기능과 handValue를 계산하는 기능 구현

---
+++ src/main/java/domain/Hand.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+    private List<Card> hand;
+
+    public Hand(List<Card> cards) {
+        this.hand = cards;
+    };
+성
+    public void addCard(Card card) {
+        hand.add(card);
+    }
+
+    public int calculateValue() {
+        int value = 0;
+
+        for (Card card : hand) {
+            value += card.getValue();
+        }
+
+        return value;
+    }
+
+    public List<String> getCards() {
+        List<String> cards = new ArrayList<>();
+        for (Card card : hand) {
+            cards.add(card.getName());
+        }
+        return cards;
+    }
+}
