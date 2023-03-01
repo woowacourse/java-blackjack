37faf748f7a62a69e502c3db7c90931516c2a619
ChoiYoungHoon <dudgns2865@naver.com>
2023-03-01 17:42:09 +0900

    feat: 문자, 문양 열거형 타입 생성

---
+++ src/main/java/domain/type/Suit.java
@@ -0,0 +1,15 @@
+package domain.type;
+
+public enum Suit {
+    SPADE("스페이드"), HEART("하트"), DIAMOND("다이아몬드"), CLUB("클로버");
+
+    private final String name;
+
+    Suit(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
