1f35d3acef1ad2a5b2213704173102b9f17814f0
chaewon121 <cw121@naver.com>
2023-03-01 21:47:51 +0900

    refactor: 카드 팩토리 함수 리팩터링

--- src/main/java/blackjack/domain/CardFactory.java
+++ src/main/java/blackjack/domain/CardFactory.java
@@ -1,18 +1,18 @@
 package blackjack.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CardFactory {
 
     public static List<Card> of() {
         List<Card> cards = new ArrayList<>();
+
         for (CardSymbol cardSymbol : CardSymbol.values()) {
-            for (CardNumber cardNumber : CardNumber.values()) {
-                cards.add(new Card(cardNumber, cardSymbol));
-            }
+            Arrays.stream(CardNumber.values())
+                    .forEach(cardNumber -> cards.add(new Card(cardNumber, cardSymbol)));
         }
         return cards;
-
     }
 }
