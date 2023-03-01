2152a15a08969e62eba86475bdae5825c6a9a114
ReO <yt930310@gmail.com>
2023-03-01 17:51:23 +0900

    test(Participants): 모든 플레이어한테 카드 나눠주는 기능에 대한 테스트 작성

--- src/test/java/domain/ParticipantsTest.java
+++ src/test/java/domain/ParticipantsTest.java
@@ -4,12 +4,23 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ParticipantsTest {
     @Test
-    void playersTest() {
+    void participantsTest() {
         assertThatCode(() -> Participants.from(List.of("leo", "reo", "reoleo")))
             .doesNotThrowAnyException();
+    }
+
+    @Test
+    void participantsDealTest() {
+        Participants participants = Participants.from(List.of("leo", "reo", "reoleo"));
+        participants.deal(Deck.from(new RandomShuffleStrategy()));
+        List<Participant> participantsList = participants.getParticipants();
+        for (Participant participant : participantsList) {
+            Assertions.assertThat(participant.getCardNames().size()).isEqualTo(1);
+        }
     }
 }
