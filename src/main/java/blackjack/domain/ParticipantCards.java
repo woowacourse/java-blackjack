21e90603e703280bb2ee59129d8818dbf5b114bc
hyunseo <goatsfish@gmail.com>
2023-03-01 16:02:48 +0900

    feat: 참여자 카드 생성 및 중복 검증 기능 추가
    Co-authored-by: Cyma-s <ssol0319@naver.com>

---
+++ src/main/java/blackjack/domain/ParticipantCards.java
@@ -0,0 +1,31 @@
+package blackjack.domain;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ParticipantCards {
+
+    private static final int INITIAL_SIZE = 2;
+
+    private final Set<Card> cards;
+
+    public ParticipantCards(final List<Card> cards) {
+        if (cards.size() != INITIAL_SIZE) {
+            throw new IllegalArgumentException("첫 카드는 두 장이어야 합니다.");
+        }
+        
+        if (new HashSet<>(cards).size() != INITIAL_SIZE) {
+            throw new IllegalArgumentException("카드는 중복될 수 없습니다.");
+        }
+        this.cards = new LinkedHashSet<>(cards);
+    }
+
+    public void receive(final Card card) {
+        if(cards.contains(card)) {
+            throw new IllegalArgumentException("중복되는 카드를 가질 수 없습니다.");
+        }
+        cards.add(card);
+    }
+}
