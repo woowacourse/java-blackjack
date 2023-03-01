821ac813be7a85a282b4871a04285d1b37ced923
chaewon121 <cw121@naver.com>
2023-03-01 18:27:12 +0900

    feat: 카드 생성자 구현

---
+++ src/main/java/blackjack/domain/Card.java
@@ -0,0 +1,14 @@
+package blackjack.domain;
+
+public class Card {
+
+    private final int number;
+    private final String symbol;
+
+    public Card(int number, String symbol) {
+        this.number = number;
+        this.symbol = symbol;
+    }
+}
+
+
---
+++ src/test/java/blackjack/domain/CardTest.java
@@ -0,0 +1,18 @@
+package blackjack.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CardTest {
+    @Test
+    @DisplayName("Card 생성자는 숫자와 기호를 받아 카드를 생성한다.")
+    void generate_card_test() {
+        // given & when
+        int number = 3;
+        String symbol = "다이아몬드";
+
+        // then
+        Assertions.assertThatNoException().isThrownBy(() -> new Card(number, symbol));
+    }
+}
