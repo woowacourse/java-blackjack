6cf8c406384246733fa5c1495c837f2f2bc71074
ChoiYoungHoon <dudgns2865@naver.com>
2023-03-01 17:43:19 +0900

    feat: card 문자 타입 일치 구현

--- src/main/java/domain/model/Cards.java
+++ src/main/java/domain/model/Cards.java
@@ -1,5 +1,6 @@
 package domain.model;
 
+import domain.type.Letter;
 import java.util.Set;
 
 public class Cards {
@@ -10,11 +11,15 @@ public class Cards {
         this.cards = cards;
     }
 
+    public boolean contains(final Letter letter) {
+        return cards.stream().anyMatch(card -> card.isMatch(letter));
+    }
+
     public void add(final Card card) {
         cards.add(card);
     }
 
-    public boolean contains(final Card card) {
-        return cards.contains(card);
+    public Set<Card> getCards() {
+        return cards;
     }
 }
