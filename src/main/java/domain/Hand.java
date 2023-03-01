f43b871e1c481f59b0e546b96d05f4ca76745cda
ReO <yt930310@gmail.com>
2023-03-01 20:56:26 +0900

    feat(Hand): 카드 합에 따라 에이스를 1 또는 11로 사용하는 기능 구현

--- src/main/java/domain/Hand.java
+++ src/main/java/domain/Hand.java
@@ -4,23 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hand {
-    private List<Card> hand;
+    private final List<Card> hand;
+    private int aceCount;
 
     public Hand(List<Card> cards) {
         this.hand = cards;
+        this.aceCount = 0;
     }
 
     public void addCard(Card card) {
+        if (card.getValue() == 11) {
+            aceCount += 1;
+        }
         hand.add(card);
     }
 
     public int calculateValue() {
         int value = 0;
-
         for (Card card : hand) {
             value += card.getValue();
         }
 
+        if (value > 21 && aceCount > 0) {
+            value -= 10;
+            aceCount -= 1;
+        }
         return value;
     }
 
@@ -31,4 +39,5 @@ public class Hand {
         }
         return cards;
     }
+
 }
