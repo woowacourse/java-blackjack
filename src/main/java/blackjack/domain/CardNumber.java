20c1487f97d50815c429e4b49f2d978ab886ccac
chaewon121 <cw121@naver.com>
2023-03-01 21:02:32 +0900

    feat: 카드 넘버 value 반환 메서드 구현

---
+++ src/main/java/blackjack/domain/CardNumber.java
@@ -0,0 +1,34 @@
+package blackjack.domain;
+
+import java.util.Arrays;
+
+public enum CardNumber {
+    ACE("A", 1),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    KING("K", 10),
+    QUEEN("Q", 10),
+    JACK("J", 10);
+
+    private final String number;
+    private final int value;
+
+    CardNumber(String number, int value) {
+        this.number = number;
+        this.value = value;
+    }
+
+    public static CardNumber of(String inputNumber) {
+
+        return Arrays.stream(CardNumber.values())
+                .filter(cardNumber -> cardNumber.number.equals(inputNumber))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("찾을 수 없는 카드넘버입니다."));
+    }
+}
---
+++ src/test/java/blackjack/domain/CardNumberTest.java
@@ -0,0 +1,20 @@
+package blackjack.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CardNumberTest {
+
+    @ParameterizedTest(name = "input : {0}")
+    @CsvSource(value = {"A:ACE", "2:TWO", "3:THREE", "4:FOUR", "5:FIVE", "6:SIX", "7:SEVEN", "8:EIGHT", "9:NINE", "K:KING", "Q:QUEEN", "J:JACK"}, delimiter = ':')
+    @DisplayName("of()는 카드넘버를 문자로 받아 값을 반환해 준다.")
+    void get_value_of_card_number_test(String number, String value) {
+        // given & when
+        CardNumber expected = CardNumber.valueOf(value);
+
+        // then
+        Assertions.assertThat(CardNumber.of(number)).isEqualTo(expected);
+    }
+}
