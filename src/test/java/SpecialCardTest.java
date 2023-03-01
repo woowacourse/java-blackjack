9339a1f8dff4f0d2434f8c8aa66080925b86cf62
sh111-coder <ohk9134@naver.com>
2023-03-01 15:13:19 +0900

    test(SpecialCardTest): 특수 카드 변환 테스트

--- src/test/java/SpecialCardTest.java
+++ src/test/java/SpecialCardTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class SpecialCardTest {
@@ -20,5 +21,21 @@ class SpecialCardTest {
     @DisplayName("A, J, Q, K가 들어오면 성공한다.")
     void shouldSuccessSpecial(String value) {
         Assertions.assertThat(SpecialCard.isSpecial(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "M", "B", "A1"})
+    @DisplayName("특수 카드 값이 아니면 예외 처리한다.")
+    void shouldThrowDoesNotSpecial(String value) {
+        Assertions.assertThatThrownBy(() -> SpecialCard.convertNumber(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("특수 카드가 아닙니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"A, 1", "J, 10", "Q, 10", "K, 10"})
+    @DisplayName("특수 카드면 해당하는 숫자로 변환한다.")
+    void shouldSuccessConvertIsSpecial(String value, int number) {
+        Assertions.assertThat(SpecialCard.convertNumber(value)).isEqualTo(number);
     }
 }
