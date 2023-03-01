9fa395decec45f1df80d7362a26f32116c243535
sh111-coder <ohk9134@naver.com>
2023-03-01 17:22:59 +0900

    test(NameTest): 이름 조건 검증 테스트

---
+++ src/test/java/NameTest.java
@@ -0,0 +1,28 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class NameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "po bi", " ", "   "})
+    @DisplayName("이름에 빈칸이 포함되면 예외 처리한다.")
+    void shouldThrowContainBlank(String name) {
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Name.NAME_EMPTY_ERROR_MESSAGE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"가", "가A", "A1", "#", "A#"})
+    @DisplayName("이름이 영어가 아니면 예외 처리한다.")
+    void shouldThrowDoesNotEnglish(String name) {
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Name.NAME_FORMAT_ERROR_MESSAGE);
+    }
+}
