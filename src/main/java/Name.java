c39c408f606b31ccab73c80a4d6d5d321e0ae96d
Woojin Lee <dhkep03@gmail.com>
2023-03-01 09:10:27 +0900

    feat: 공백, 빈 값, null에 대한 Name의 유효성 검증

--- src/main/java/Name.java
+++ src/main/java/Name.java
@@ -2,6 +2,13 @@ public class Name {
     private final String name;
 
     public Name(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름에는 공백이나 빈 값이 들어갈 수 없습니다.");
+        }
     }
 }
--- src/test/java/NameTest.java
+++ src/test/java/NameTest.java
@@ -1,11 +1,26 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class NameTest {
     @DisplayName("이름을 생성한다.")
     @Test
     void 이름_생성() {
-        assertDoesNotThrow(()->new Name("name"));
+        assertDoesNotThrow(() -> new Name("name"));
+    }
+
+    @DisplayName("이름에 적절하지 않은 값이 입력되면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {" "})
+    @NullAndEmptySource
+    void 이름_적절하지_않은_값(String name) {
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름에는 공백이나 빈 값이 들어갈 수 없습니다.");
     }
 }
