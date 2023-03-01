24a228ee981a8054cc943c5a7e6033976f86d144
chaewon121 <cw121@naver.com>
2023-03-01 21:23:10 +0900

    feat: 카드 Symbol 이름 반환 메서드 구현

---
+++ src/main/java/blackjack/domain/CardSymbol.java
@@ -0,0 +1,23 @@
+package blackjack.domain;
+
+import java.util.Arrays;
+
+public enum CardSymbol {
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    SPADE("스페이드"),
+    CLOVER("클로버");
+
+    private final String symbol;
+
+    CardSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public static CardSymbol of(String symbolName) {
+        return Arrays.stream(CardSymbol.values())
+                .filter(cardSymbol -> cardSymbol.symbol.equals(symbolName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("찾을 수 없는 카드심볼 입니다."));
+    }
+}
---
+++ src/test/java/blackjack/domain/CardSymbolTest.java
@@ -0,0 +1,21 @@
+package blackjack.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CardSymbolTest {
+
+    @ParameterizedTest(name = "input : {0}")
+    @CsvSource(value = {"하트:HEART", "다이아몬드:DIAMOND", "스페이드:SPADE", "클로버:CLOVER"}, delimiter = ':')
+    @DisplayName("of()는 심볼 이름를 받아 해당 enum 을 반환한다.")
+    void test_(String symbolName, String symbol) {
+        // given & when
+        CardSymbol expected = CardSymbol.valueOf(symbol);
+
+        // then
+        Assertions.assertThat(CardSymbol.of(symbolName)).isEqualTo(expected);
+    }
+
+}
