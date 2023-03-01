5d8c73efb1599c5d6bee7c9eb8e4f407043e4bdc
sh111-coder <ohk9134@naver.com>
2023-03-01 16:37:52 +0900

    feat(InputValidator): 입력에 대한 빈 값 검증 기능 추가

---
+++ src/main/java/InputValidator.java
@@ -0,0 +1,10 @@
+public class InputValidator {
+
+    public static final String INPUT_EMPTY_ERROR_MESSAGE = "[ERROR] 입력값이 존재하지 않습니다.";
+
+    public static void validateBlank(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(INPUT_EMPTY_ERROR_MESSAGE);
+        }
+    }
+}
