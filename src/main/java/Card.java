febcc9dcb66a487fbbb2ee7971623fc3509c9a4d
sh111-coder <ohk9134@naver.com>
2023-03-01 19:51:59 +0900

    feat(Card): 카드 객체 구현

---
+++ src/main/java/Card.java
@@ -0,0 +1,10 @@
+public class Card {
+
+    private final String name;
+    private final int value;
+
+    public Card(final String name, final int value) {
+        this.name = name;
+        this.value = value;
+    }
+}
