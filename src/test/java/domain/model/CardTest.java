31951c9881da4da3cb4509d7c798419bd882eab3
ChoiYoungHoon <dudgns2865@naver.com>
2023-03-01 17:45:39 +0900

    test: Card Letter 일치 테스트 코드 작성

--- src/test/java/domain/model/CardTest.java
+++ src/test/java/domain/model/CardTest.java
@@ -10,11 +10,24 @@ import org.junit.jupiter.api.Test;
 class CardTest {
 
     @Test
+    @DisplayName("문자 일치 테스트")
+    public void testIsMatch() {
+        //given
+        Card card = new Card(Suit.DIAMOND, Letter.ACE);
+
+        //when
+        boolean result = card.isMatch(Letter.ACE);
+
+        //then
+        assertTrue(result);
+    }
+
+    @Test
     @DisplayName("동등성 검사 테스트")
     public void testEquals() {
         //given
-        Card card1 = new Card("card1", List.of(1));
-        Card card2 = new Card("card1", List.of(1));
+        Card card1 = new Card(Suit.DIAMOND, Letter.ACE);
+        Card card2 = new Card(Suit.DIAMOND, Letter.ACE);
 
         //when
         boolean result = card1.equals(card2);
