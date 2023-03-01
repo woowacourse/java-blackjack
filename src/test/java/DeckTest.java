fa44d88ff9e1ca9bda21702057b3e5c3d0619442
sh111-coder <ohk9134@naver.com>
2023-03-01 15:14:57 +0900

    test: cardTest 리팩토링
    불필요한 import 제거 & static import

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,4 +1,5 @@
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,13 +11,13 @@ class DeckTest {
     @DisplayName("초기 덱의 카드 수는 52장이어야한다.")
     void checkDeckSize() {
 
-        Assertions.assertThat(Deck.cards.size()).isEqualTo(52);
+        assertThat(Deck.cards.size()).isEqualTo(52);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"A하트, 1", "3하트, 3", "5다이아몬드, 5", "K스페이드, 10", "Q클로버, 10", "J하트, 10"})
     @DisplayName("각 카드에 해당하는 숫자를 정상적으로 추출한다.")
     void shouldSuccessExtractCardNumber(String card, int cardNumber) {
-        Assertions.assertThat(Deck.extractCardNumber(card)).isEqualTo(cardNumber);
+        assertThat(Deck.extractCardNumber(card)).isEqualTo(cardNumber);
     }
 }
--- src/test/java/SpecialCardTest.java
+++ src/test/java/SpecialCardTest.java
@@ -1,8 +1,7 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -13,21 +12,21 @@ class SpecialCardTest {
     @ValueSource(strings = {"1", "2", "M", "B", "A1"})
     @DisplayName("A, J, Q, K 이외의 값이 들어오면 실패한다.")
     void shouldFailDoesNotSpecial(String value) {
-        Assertions.assertThat(SpecialCard.isSpecial(value)).isFalse();
+        assertThat(SpecialCard.isSpecial(value)).isFalse();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"A", "J", "Q", "K"})
     @DisplayName("A, J, Q, K가 들어오면 성공한다.")
     void shouldSuccessSpecial(String value) {
-        Assertions.assertThat(SpecialCard.isSpecial(value)).isTrue();
+        assertThat(SpecialCard.isSpecial(value)).isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "2", "M", "B", "A1"})
     @DisplayName("특수 카드 값이 아니면 예외 처리한다.")
     void shouldThrowDoesNotSpecial(String value) {
-        Assertions.assertThatThrownBy(() -> SpecialCard.convertNumber(value))
+        assertThatThrownBy(() -> SpecialCard.convertNumber(value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("특수 카드가 아닙니다.");
     }
@@ -36,6 +35,6 @@ class SpecialCardTest {
     @CsvSource(value = {"A, 1", "J, 10", "Q, 10", "K, 10"})
     @DisplayName("특수 카드면 해당하는 숫자로 변환한다.")
     void shouldSuccessConvertIsSpecial(String value, int number) {
-        Assertions.assertThat(SpecialCard.convertNumber(value)).isEqualTo(number);
+        assertThat(SpecialCard.convertNumber(value)).isEqualTo(number);
     }
 }
