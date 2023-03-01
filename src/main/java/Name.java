d13898317436bce2a8233f9cfbcaf47d05d37f78
sh111-coder <ohk9134@naver.com>
2023-03-01 17:23:20 +0900

    feat(Name): 이름 조건 검증 기능 추가

---
+++ src/main/java/Name.java
@@ -0,0 +1,26 @@
+public class Name {
+
+    public static final String NAME_REGEX_FORMAT = "^[a-zA-Z]+$";
+    public static final String NAME_EMPTY_ERROR_MESSAGE = "[ERROR] 이름은 빈칸이 포함될 수 없습니다.";
+    public static final String NAME_FORMAT_ERROR_MESSAGE = "[ERROR] 이름은 영어만 가능합니다.";
+
+    private final String name;
+
+    public Name(final String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank() || isContainBlank(name)) {
+            throw new IllegalArgumentException(NAME_EMPTY_ERROR_MESSAGE);
+        }
+        if (!name.matches(NAME_REGEX_FORMAT)) {
+            throw new IllegalArgumentException(NAME_FORMAT_ERROR_MESSAGE);
+        }
+    }
+
+    private boolean isContainBlank(String name) {
+        return name.replaceAll("\\s", "").length() != name.length();
+    }
+}
