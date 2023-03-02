44ceb01b628b752047338d6990b1450a03199406
dev_kong <ahdjd5@gmail.com>
2023-03-02 21:31:11 +0900

    feat: BlackjackGame#addPlayer 이름을 전달받아 새로운 플레이어를 추가하는 기능

--- src/main/java/BlackjackGame.java
+++ src/main/java/BlackjackGame.java
@@ -3,8 +3,11 @@ import player.Player;
 import player.Players;
 
 public class BlackjackGame {
-    private final Players players = new Players();
+    private final Players players;
 
+    public BlackjackGame(Players players) {
+        this.players = players;
+    }
 
     public void addPlayer(String playerName) {
         players.add(new Player(new Name(playerName)));
--- src/test/java/BlackjackGameTest.java
+++ src/test/java/BlackjackGameTest.java
@@ -1,19 +1,27 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import player.Players;
+
 class BlackjackGameTest {
     BlackjackGame blackjackGame;
+    Players players;
 
     @BeforeEach
     void setUp() {
-        blackjackGame = new BlackjackGame();
+        players = new Players();
+        blackjackGame = new BlackjackGame(players);
     }
 
     @Test
     @DisplayName("플레이어를 만들고 플레이어즈에 추가할 수 있다")
     void addPlayer() {
-
+        blackjackGame.addPlayer("폴로");
         blackjackGame.addPlayer("로지");
+
+        assertThat(players.count()).isEqualTo(2);
     }
 }
