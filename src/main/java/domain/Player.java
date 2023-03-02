162d1e055d67ee50b6ecf2b6fda7ae0762c43e11
jaeyeon kim <jakind@naver.com>
2023-03-02 20:09:24 +0900

    feat: Player 의 이름은 딜러일 수 없다.
    - domain.Player

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -3,13 +3,22 @@ package domain;
 public class Player {
 
     private static final int BUST_LIMIT = 21;
+    private static final String BAN_NAME = "딜러";
+    private static final String BAN_NAME_ERROR_MESSAGE = "Player 의 이름은 딜러일 수 없습니다.";
 
     private final Name name;
     private final Cards cards;
 
     public Player(Name name, Cards cards) {
+        validate(name);
         this.name = name;
         this.cards = cards;
+    }
+
+    private void validate(Name name) {
+        if (name.getName().equals(BAN_NAME)) {
+            throw new IllegalArgumentException(BAN_NAME_ERROR_MESSAGE);
+        }
     }
 
     public void pick(Card card) {
