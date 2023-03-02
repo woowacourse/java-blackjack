5559bf036ba7d7c51ec606198ccf3bddc0544efa
Woojin Lee <dhkep03@gmail.com>
2023-03-02 16:36:37 +0900

    feat: Player 이름 반환 기능 구현

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,11 +1,23 @@
 public class Player {
+    private final Name name;
     private final CardHand cardHand;
 
-    public Player(CardHand cardHand) {
+
+    public Player(String nameValue, CardHand cardHand) {
         this.cardHand = cardHand;
+        this.name = new Name(nameValue);
+    }
+
+    public Player(String nameValue) {
+        this.cardHand = new CardHand();
+        this.name = new Name(nameValue);
     }
 
     public int calculateScore() {
         return this.cardHand.calculateScore();
+    }
+
+    public String getNameValue() {
+        return this.name.getValue();
     }
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -10,8 +10,15 @@ class PlayerTest {
         CardHand cardHand = new CardHand();
         cardHand.add(new Card(Symbol.SPADE, CardNumber.TWO));
         cardHand.add(new Card(Symbol.CLOVER, CardNumber.KING));
-        Player player = new Player(cardHand);
+        Player player = new Player("name", cardHand);
         assertThat(player.calculateScore()).isEqualTo(12);
+    }
 
+    @Test
+    @DisplayName("이름을 반환한다.")
+    void 이름_반환() {
+        String nameValue = "name";
+        Player player = new Player(nameValue);
+        assertThat(player.getNameValue()).isEqualTo(nameValue);
     }
 }
