92e99c10943793acca19a14b6d3dfd3dd5ebf4b2
yoondgu <doyou2ing@naver.com>
2023-03-02 15:37:13 +0900

    refactor: ACE 기본값 변경
    Co-authored-by: odo27 <odo27@naver.com>

--- src/test/java/blackjack/domain/NumberTest.java
+++ src/test/java/blackjack/domain/NumberTest.java
@@ -31,6 +31,6 @@ class NumberTest {
                 .map(Number::getValue)
                 .collect(Collectors.toList());
         assertThat(numberValues)
-                .containsExactly(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10);
+                .containsExactly(11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10);
     }
 }
