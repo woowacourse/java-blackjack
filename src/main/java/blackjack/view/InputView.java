297206803e2fdf2e7420409f98f86a8202ff6924
yenawee <yaena0319@naver.com>
2023-03-02 17:30:09 +0900

    feat(InputView) : 플레이어 이름, 카드 더 받을지 여부 입력
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,30 @@
+package blackjack.view;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private static final InputView INSTANCE = new InputView();
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String DELIMITER = ",";
+
+    private InputView() {
+    }
+
+    public static InputView getInstance() {
+        return INSTANCE;
+    }
+
+    public List<String> readPlayerNames() {
+        String input = scanner.nextLine();
+        return List.of(splitNames(input));
+    }
+
+    private String[] splitNames(String input) {
+        return input.split(DELIMITER);
+    }
+
+    public String readPlayerIntention() {
+        return scanner.nextLine();
+    }
+}
