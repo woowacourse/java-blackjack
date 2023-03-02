72a8ca7c533ba391c4de8160bcd262ec833f2da5
yenawee <yaena0319@naver.com>
2023-03-02 16:28:36 +0900

    feat : 현재 가지고 있는 카드의 점수를 계산해서 더 받을 수 있는지 여부를 확인하는 기능 구현
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -1,16 +1,32 @@
 package blackjack.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Player {
-    List<Card> cards ;
+    private static final int MAX_SCORE_TO_RECEIVE = 21;
+
+    List<Card> cards;
+    Score score;
 
     public Player(List<Card> cards) {
         this.cards = cards;
+        this.score = new Score();
     }
 
-    public void receiveCard(Card card){
+    private List<TrumpNumber> extractNumbers() {
+        return cards.stream()
+                .map(Card::getTrumpNumber)
+                .collect(Collectors.toList());
+    }
+
+    public void receiveCard(Card card) {
         cards.add(card);
+    }
+
+    public boolean isAbleToReceive() {
+        score.calculateScore(extractNumbers());
+        return score.getScore() <= MAX_SCORE_TO_RECEIVE;
     }
 
     public List<Card> getCards() {
