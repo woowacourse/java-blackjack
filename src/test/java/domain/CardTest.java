f9706eeea7719cd7ada785375fc4530498ab39d3
aiaiaiai1 <cvc100326@naver.com>
2023-03-02 14:07:58 +0900

    test: 카드의 숫자값을 가져오는 테스트 추가

--- src/test/java/domain/CardTest.java
+++ src/test/java/domain/CardTest.java
@@ -1,13 +1,22 @@
 package domain;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class CardTest {
     @Test
     @DisplayName("카드를 생성한다.")
     void createCardTest() {
-        Assertions.assertDoesNotThrow(()->new Card(CardNumber.ACE ,CardPattern.SPADE));
+        assertDoesNotThrow(()->new Card(CardNumber.ACE ,CardPattern.SPADE));
+    }
+
+    @Test
+    @DisplayName("카드의 숫자값을 가져온다.")
+    void getCardValueTest() {
+        Card card = new Card(CardNumber.ACE, CardPattern.SPADE);
+        assertThat(card.getCardValue()).isEqualTo(11);
     }
 }
