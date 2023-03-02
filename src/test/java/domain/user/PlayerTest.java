b1bc0e4bcc83db9114b9b68d7c710044407be377
yujamint <woawoa0513@gmail.com>
2023-03-02 20:33:10 +0900

    test: Player 초기 세팅 테스트 작성
    세부사항
    - User 추상 클래스 정의

---
+++ src/test/java/domain/user/PlayerTest.java
@@ -0,0 +1,25 @@
+package domain.user;
+
+import static org.assertj.core.api.Assertions.*;
+
+import domain.Card.Card;
+import domain.Card.CloverCard;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+    @Test
+    @DisplayName("플레이어는 처음에 2장의 카드를 받는다.")
+    void generatePlayerTest() {
+        //given
+        List<Card> firstTurnCards = List.of(CloverCard.CLOVER_ACE, CloverCard.CLOVER_FIVE);
+        Player player = new Player(firstTurnCards);
+
+        //when
+        List<Card> cards = player.getCards();
+
+        //then
+        assertThat(cards.size()).isEqualTo(2);
+    }
+}
