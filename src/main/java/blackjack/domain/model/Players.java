fe678545a69c6ceff01ce48cc7a13a83d527d4cf
waterricecake <fruturum@nate.com>
2023-03-02 17:30:14 +0900

    feat(Players): 플레이어들의 이름과 목록 반환 기능 구현

--- src/main/java/blackjack/domain/model/Players.java
+++ src/main/java/blackjack/domain/model/Players.java
@@ -1,12 +1,32 @@
 package blackjack.domain.model;
 
+import blackjack.domain.vo.Name;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Players {
     private final List<Player> players;
 
-    public Players(List<Player> players) {
-        this.players = players;
+    public Players(final String playersNames) {
+        this.players = makePlayers(playersNames);
     }
 
+    private List<Player> makePlayers(final String playerNames) {
+        return Arrays.stream(playerNames.split(","))
+                .map(name -> new Player(new Name(name), new Cards()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public List<String> getNames() {
+        return players.stream()
+                .map(Player::getName)
+                .collect(Collectors.toList());
+    }
 }
--- src/test/java/blackjack/domain/model/PlayersTest.java
+++ src/test/java/blackjack/domain/model/PlayersTest.java
@@ -1,10 +1,13 @@
 package blackjack.domain.model;
 
 import blackjack.domain.vo.Name;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
 
@@ -13,7 +16,37 @@ class PlayersTest {
     @Test
     @DisplayName("플레이어들 생성 테스트")
     void constructorPlayersTest() {
-        assertThatNoException().isThrownBy(()->new Players(List.of(new Player(new Name("test"), new Cards()))));
+        assertThatNoException().isThrownBy(()->new Players("test"));
     }
 
+    @Test
+    @DisplayName("플레이어의 리스트를 반환하는 테스트")
+    void getPlayersTest() {
+        // given
+        String playerNames = "pobi,crong";
+        Players players = new Players(playerNames);
+
+        // when
+        List<Player> actual = players.getPlayers();
+
+        // then
+        for (int i = 0; i < actual.size(); i++){
+            Assertions.assertThat(players.getPlayers().get(i).getName()).isEqualTo(playerNames.split(",")[i]);
+        }
+    }
+
+    @Test
+    @DisplayName("플레이어의 이름들을 반환하는 테스트")
+    void getNamesTest() {
+        // given
+        String playerNames = "pobi,crong";
+        Players players = new Players(playerNames);
+        List<String> expected = Arrays.stream(playerNames.split(",")).collect(Collectors.toList());
+
+        // when
+        List<String> actual = players.getNames();
+
+        // then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
 }
