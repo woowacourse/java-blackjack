174ffe5b61f542e624807be09a96d4cb0014d617
eunbii0213 <eunbii0213@naver.com>
2023-03-02 14:25:49 +0900

    test: 16이하면 카드를 뽑고 17이상이면 카드를 뽑지 않는 테스트 코드 작성

---
+++ src/test/java/domain/DealerTest.java
@@ -0,0 +1,39 @@
+package domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class DealerTest {
+
+    @Test
+    void 점수가_16이하면_카드를_뽑는다() {
+        //given,when
+        Dealer dealer = new Dealer(new Cards());
+        int expected = 1;
+        int actual = dealer.getCardsSize();
+
+        //then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void 점수가_17이상이면_카드를_뽑지_않는다() {
+        //given
+        Dealer dealer = new Dealer(
+                new Cards(List.of(
+                        new Card(Suit.CLOVER, Denomination.KING),
+                        new Card(Suit.CLOVER, Denomination.SEVEN))
+                ));
+        //when
+        int expected = 2;
+        int actual = dealer.getCardsSize();
+
+        //then
+        Assertions.assertEquals(expected, actual);
+    }
+}
