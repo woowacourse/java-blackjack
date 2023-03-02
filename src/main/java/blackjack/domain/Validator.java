efdadf23419b54db98901c884c3d93b415c56b7a
yenawee <yaena0319@naver.com>
2023-03-02 18:01:57 +0900

    feat : 플레이어 수 유효성 검사 기능 구현
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

---
+++ src/main/java/blackjack/domain/Validator.java
@@ -0,0 +1,22 @@
+package blackjack.domain;
+
+import java.util.List;
+
+public class Validator {
+
+    public static final String PLAYER_COUNT_ERROR_MESSAGE = "플레이어 수는 1명 이상 7명 이하여야 합니다.";
+    public static final String PLAYER_NAME_DUPLICATE_ERROR_MESSAGE = "플레이어 이름은 중복될 수 없습니다.";
+    public static final String PLAYER_INTENTION_ERROR_MESSAGE = "y 혹은 n 만 입력 가능 합니다.";
+    public static final int MIN_PLAYER_COUNT = 1;
+    public static final int MAX_PLAYER_COUNT = 7;
+
+    public void validatePlayerNames(List<String> playerNames) {
+        validatePlayerCount(playerNames);
+    }
+
+    private void validatePlayerCount(List<String> playerNames) {
+        if (playerNames.size() < MIN_PLAYER_COUNT || playerNames.size() > MAX_PLAYER_COUNT) {
+            throw new IllegalArgumentException(PLAYER_COUNT_ERROR_MESSAGE);
+        }
+    }
+}
