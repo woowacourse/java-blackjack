b4e15960e299ac98e52d6b8fe43d01b4e26ee559
“jaehee329” <jjhs9803@gmail.com>
2023-03-02 14:44:58 +0900

    feat: Letter sign 추가

--- src/main/java/domain/model/Card.java
+++ src/main/java/domain/model/Card.java
@@ -37,7 +37,7 @@ public class Card {
 
     @Override
     public String toString() {
-        return letter.getNumber() + suit.getName();
+        return letter.getSign() + suit.getName();
     }
 
     public int getNumber() {
--- src/main/java/domain/type/Letter.java
+++ src/main/java/domain/type/Letter.java
@@ -1,27 +1,33 @@
 package domain.type;
 
 public enum Letter {
-    ACE(11),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    KING(10),
-    QUEEN(10),
-    JACK(10);
+    ACE(11, "A"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    KING(10, "K"),
+    QUEEN(10, "Q"),
+    JACK(10, "J");
 
     private final int number;
+    private final String sign;
 
-    Letter(final int number) {
+    Letter(final int number, final String sign) {
         this.number = number;
+        this.sign = sign;
     }
 
     public int getNumber() {
         return number;
+    }
+
+    public String getSign() {
+        return sign;
     }
 }
