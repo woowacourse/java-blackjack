19cf2da8bec5849c44a920340f5926fc1ee50787
jaeyeon kim <jakind@naver.com>
2023-03-02 20:34:49 +0900

    test: Players 의 참여자 이름은 중복될 수 없다.
    - domain.PlayersTest

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -7,6 +7,7 @@ import util.CardDeckMaker;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PlayersTest {
@@ -21,12 +22,22 @@ public class PlayersTest {
 
     @Test
     @DisplayName("참여자 수는 최대 9명이다.")
-    void createPlayersFail() {
+    void createPlayersCountFail() {
         List<String> playerNames = List.of("a", "b", "c", "d", "f", "q", "w", "e", "r", "z");
 
         assertThatThrownBy(() -> Players.from(playerNames, distributor))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("참여자는 최대 9명까지 가능합니다.");
+    }
+
+    @Test
+    @DisplayName("참여자 이름은 중복을 허용하지 않는다.")
+    void createPlayersDuplicateFail() {
+        List<String> playerNames = List.of("a", "a", "a");
+
+        assertThatThrownBy(() -> Players.from(playerNames, distributor))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("참여자 이름은 중복될 수 없습니다.");
     }
 
 }
