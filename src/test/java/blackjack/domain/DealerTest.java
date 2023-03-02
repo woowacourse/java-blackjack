7ab51e7091ef8baa61c88f42526423156a588036
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-03-02 17:19:49 +0900

    refactor: 테스트에서 사용되는 fixture 메서드로 분리

--- src/test/java/blackjack/domain/DealerTest.java
+++ src/test/java/blackjack/domain/DealerTest.java
@@ -1,6 +1,5 @@
 package blackjack.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -9,22 +8,16 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SuppressWarnings({"NonAsciiCharacters", "SpellCheckingInspection"})
+@SuppressWarnings({"NonAsciiCharacters"})
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class DealerTest {
-    private static List<Card> cards;
+    private final CardPocket cardPocketUnderDrawPoint = generateUnderDrawPoint();
+    private final CardPocket cardPocketOverDrawPoint = generateOverDrawPoint();
 
-    private CardPocket cardPocket;
-
-    @BeforeEach
-    void setCardPocket() {
-        cards = List.of(new Card(Shape.CLOVER, Symbol.TWO), new Card(Shape.HEART, Symbol.EIGHT));
-        cardPocket = new CardPocket(cards);
-    }
 
     @Test
     void 딜러의_카드가_16_이하의_점수라면_드로우_합니다() {
-        final Dealer dealer = new Dealer(cardPocket);
+        final Dealer dealer = new Dealer(cardPocketUnderDrawPoint);
 
         assertThat(dealer.isDrawable())
                 .isTrue();
@@ -32,12 +25,23 @@ class DealerTest {
 
     @Test
     void 딜러의_카드가_17_이상의_점수라면_스테이_합니다() {
-        cardPocket.addCard(new Card(Shape.CLOVER, Symbol.TEN));
-
-        final Dealer dealer = new Dealer(cardPocket);
+        final Dealer dealer = new Dealer(cardPocketOverDrawPoint);
 
         assertThat(dealer.isDrawable())
                 .isFalse();
     }
 
+    private CardPocket generateUnderDrawPoint() {
+        final List<Card> cardsUnderDrawPoint = List.of(
+                new Card(Shape.CLOVER, Symbol.TWO),
+                new Card(Shape.HEART, Symbol.EIGHT));
+        return new CardPocket(cardsUnderDrawPoint);
+    }
+
+    private CardPocket generateOverDrawPoint() {
+        final List<Card> cardsOverDrawPoint = List.of(
+                new Card(Shape.CLOVER, Symbol.ACE),
+                new Card(Shape.HEART, Symbol.KING));
+        return new CardPocket(cardsOverDrawPoint);
+    }
 }
