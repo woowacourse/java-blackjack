dda91423cd88d804b24cfb0a70a762a52ee559ca
sh111-coder <ohk9134@naver.com>
2023-03-02 15:47:04 +0900

    test(DealerTest): 가지고 있는 카드 계산 테스트

--- src/test/java/DealerTest.java
+++ src/test/java/DealerTest.java
@@ -12,4 +12,50 @@ public class DealerTest {
         dealer.takeCard(new Card("3다이아몬드", 3));
         assertThat(dealer.getCards().size()).isEqualTo(2);
     }
+
+    @Test
+    @DisplayName("가지고 있는 카드의 합이 17 미만아면 true를 반환한다. (소프트 17 케이스는 제외)")
+    void isCardValueUnder17() {
+        Dealer dealer = new Dealer();
+        dealer.takeCard(new Card("A다이아몬드", 1));
+        dealer.takeCard(new Card("6다이아몬드", 6));
+        assertThat(dealer.checkCardsCondition()).isFalse();
+    }
+
+    @Test
+    @DisplayName("가지고 있는 카드의 합이 16 초과이면 false를 반환한다. (소프트 17 케이스는 제외)")
+    void isCardValueOver16() {
+        Dealer dealer = new Dealer();
+        dealer.takeCard(new Card("10다이아몬드", 10));
+        dealer.takeCard(new Card("9다이아몬드", 9));
+        assertThat(dealer.checkCardsCondition()).isFalse();
+    }
+
+    @Test
+    @DisplayName("소프트 17의 경우에는 false를 반환한다")
+    void isFalseWhenSoft17() {
+        Dealer dealer = new Dealer();
+        dealer.takeCard(new Card("A다이아몬드", 1));
+        dealer.takeCard(new Card("6다이아몬드", 6));
+        assertThat(dealer.checkCardsCondition()).isFalse();
+    }
+
+    @Test
+    @DisplayName("가지고 있는 카드가 A 2장일 경우 true를 반환한다.")
+    void isTrueWhenDoubleAce() {
+        Dealer dealer = new Dealer();
+        dealer.takeCard(new Card("A다이아몬드", 1));
+        dealer.takeCard(new Card("A하트", 1));
+        assertThat(dealer.checkCardsCondition()).isTrue();
+    }
+
+    @Test
+    @DisplayName("딜러는 Bust가 나지 않는 선에서 최선의 값을 선택한다.")
+    void isDealerAlwaysChooseOptimalValue() {
+        Dealer dealer = new Dealer();
+        dealer.takeCard(new Card("A다이아몬드", 1));
+        dealer.takeCard(new Card("A하트", 1));
+        dealer.takeCard(new Card("10다이아몬드", 10));
+        assertThat(dealer.checkCardsCondition()).isTrue();
+    }
 }
