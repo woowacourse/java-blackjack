b6dfff41b8963bf84e1556db2f03134cefab4279
yenawee <yaena0319@naver.com>
2023-03-02 15:58:23 +0900

    test : 21 점 초과일 때 ACE 점수 처리 테스트
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- src/test/java/blackjack/domain/ScoreTest.java
+++ src/test/java/blackjack/domain/ScoreTest.java
@@ -6,17 +6,39 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ScoreTest {
 
     @Test
     @DisplayName("점수 계산")
-    void calculateScore(){
+    void calculateScore() {
         Score score = new Score();
         score.calculateScore(List.of(TrumpNumber.FIVE, TrumpNumber.NINE));
 
         Assertions.assertThat(score.getScore()).isEqualTo(14);
     }
 
+    // 21점 초과일 때 -> ACE 개수 세기 -> 21점 안넘을때까지 10점빼주기
+    @Test
+    @DisplayName("21점 초과일 때 ACE 처리 (ACE 1장인 경우)")
+    void calculateScoreIncludeAce() {
+        Score score = new Score();
+        score.calculateScore(List.of(TrumpNumber.FIVE, TrumpNumber.NINE, TrumpNumber.ACE));
+        Assertions.assertThat(score.getScore()).isEqualTo(15);
+    }
+
+    @Test
+    @DisplayName("21점 초과일 때 ACE 처리 (ACE 3장일 경우)")
+    void calculateScoreIncludeAce3() {
+        Score score = new Score();
+        score.calculateScore(List.of(TrumpNumber.FIVE, TrumpNumber.NINE, TrumpNumber.ACE, TrumpNumber.ACE, TrumpNumber.ACE));
+        Assertions.assertThat(score.getScore()).isEqualTo(17);
+    }
+
+    @Test
+    @DisplayName("ACE 가 4장일 경우")
+    void calculateScoreAce4() {
+        Score score = new Score();
+        score.calculateScore(List.of(TrumpNumber.ACE, TrumpNumber.ACE, TrumpNumber.ACE, TrumpNumber.ACE));
+        Assertions.assertThat(score.getScore()).isEqualTo(14);
+    }
 }
