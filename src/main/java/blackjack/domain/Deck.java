40ce6c5f8f7d963a210746c9ab9f8c70946f5bfc
moonchaewon <codnjs8989@gmail.com>
2023-03-02 14:11:20 +0900

    feat: Deck 생성 로직 구현

---
+++ src/main/java/blackjack/domain/Deck.java
@@ -0,0 +1,35 @@
+package blackjack.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Deck {
+
+    private static final Map<Integer, Card> deck = new HashMap<>();
+
+    static {
+        makeDeck();
+    }
+
+    private Deck() {
+    }
+
+    private static void makeDeck() {
+        int count = 0;
+        for (Rank rank : Rank.values()) {
+            count = addCardsToDeck(rank, count);
+        }
+    }
+
+    private static int addCardsToDeck(final Rank rank, int count) {
+        for (Suit suit : Suit.values()) {
+            deck.put(count, new Card(rank, suit));
+            count++;
+        }
+        return count;
+    }
+
+    public static Card from(final int index) {
+        return deck.get(index);
+    }
+}
