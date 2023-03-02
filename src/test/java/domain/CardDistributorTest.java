90f88ab1c5e0d63858f17e60b2c4b2115d4c92d3
jaeyeon kim <jakind@naver.com>
2023-03-02 17:37:36 +0900

    test: CardDistributor 가 초기 카드를 분배한다.
    - domain.CardDistributorTest

--- src/test/java/domain/CardDistributorTest.java
+++ src/test/java/domain/CardDistributorTest.java
@@ -1,9 +1,13 @@
 package domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import util.CardDeckMaker;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CardDistributorTest {
@@ -13,10 +17,20 @@ public class CardDistributorTest {
     void removeCardWhenPicked() {
         CardDistributor cardDistributor = new CardDistributor(CardDeckMaker.generate());
 
-        Card card = cardDistributor.pick();
+        Card card = cardDistributor.distribute();
 
         assertThat(cardDistributor.getDeckSize()).isEqualTo(51);
         assertThat(card).isNotNull();
+    }
+
+    @Test
+    @DisplayName("게임 시작시 카드를 2장씩 분배한다.")
+    void distributeTwoCardWhenStartGame() {
+        CardDistributor cardDistributor = new CardDistributor(CardDeckMaker.generate());
+
+        List<Card> cards = cardDistributor.distributeInitialCard();
+
+        assertThat(cards.size()).isEqualTo(2);
     }
 
 }
