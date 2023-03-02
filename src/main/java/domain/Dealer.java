95e072cca26492ebee784f90511098282132b8e0
splitube <voicesplit@daum.net>
2023-03-02 22:18:02 +0900

    feat: Dealer 구현
    Co-authored-by: echo724 <echo.backend@gmail.com>

---
+++ src/main/java/domain/Dealer.java
@@ -0,0 +1,18 @@
+package domain;
+
+import java.util.List;
+
+public class Dealer extends Participant {
+
+    public Dealer(String name) {
+        super(name);
+    }
+
+    @Override
+    public List<Card> getReadyCards() {
+        if (cards.size() < 1) {
+            throw new IllegalStateException();
+        }
+        return List.of(cards.get(0));
+    }
+}
---
+++ src/test/java/domain/DealerTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DealerTest {
+
+    @Test
+    @DisplayName("처음에 카드를 지급받지 않은 경우 카드 조회시 오류를 던진다.")
+    void getReadyCardsTestFailed() {
+        Dealer dealer = new Dealer("echo");
+        Assertions.assertThatThrownBy(dealer::getReadyCards)
+            .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("처음에 지급받은 카드를 반환한다.")
+    void getReadyCardsTestSuccess() {
+        Dealer dealer = new Dealer("echo");
+        dealer.addCard(new Card(CardNumber.ACE, CardShape.SPADE));
+        dealer.addCard(new Card(CardNumber.THREE, CardShape.HEART));
+        Assertions.assertThat(dealer.getReadyCards())
+            .containsExactly(new Card(CardNumber.ACE, CardShape.SPADE));
+    }
+}
