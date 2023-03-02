b9111f0fc923c2048636575248941c1d31308afb
hum02 <ehdgma02@gmail.com>
2023-03-02 14:21:35 +0900

    feat: 이름 생성 기능 구현

---
+++ src/main/java/blackjack/model/Name.java
@@ -0,0 +1,17 @@
+package blackjack.model;
+
+public class Name {
+
+    private final String name;
+
+    public Name(String name) {
+        validateIsEmpty(name);
+        this.name = name;
+    }
+
+    private void validateIsEmpty(String name){
+        if(name.isBlank()){
+            throw new IllegalArgumentException("이름은 빈 문자열일 수 없습니다.");
+        }
+    }
+}
