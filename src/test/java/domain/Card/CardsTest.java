752192673de8c5bb06106a1e1fa32281ed81a374
yujamint <woawoa0513@gmail.com>
2023-03-02 20:24:30 +0900

    test: 게임 시작 직후 카드 두 장씩 분배하는 기능 테스트 작성

--- src/test/java/domain/Card/CardsTest.java
+++ src/test/java/domain/Card/CardsTest.java
@@ -1,7 +1,9 @@
 package domain.Card;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.List;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,5 +20,15 @@ class CardsTest {
 
         //then
         assertThrows(IllegalArgumentException.class, cards::drawCard);
+    }
+
+    @Test
+    @DisplayName("블랙잭 게임 시작 직후 카드를 두 장씩 분배한다.")
+    void drawForFirstTurnTest() {
+        Cards cards = new Cards();
+
+        List<Card> firstTurnCards = cards.drawForFirstTurn();
+
+        assertThat(firstTurnCards.size()).isEqualTo(2);
     }
 }
