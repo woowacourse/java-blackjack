aab4545e469a467ddecc7fa637af43fe6e6fee3a
eunbii0213 <eunbii0213@naver.com>
2023-03-02 14:26:13 +0900

    feat: 16이하면 카드를 뽑고 17이상이면 카드를 뽑지 않는다

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -1,6 +1,8 @@
 package domain;
 
-public class Dealer implements Gambler{
+public class Dealer implements Gambler {
+
+    private static final int PICK_BOUNDARY = 16;
 
     private final Cards cards;
 
@@ -11,11 +13,17 @@ public class Dealer implements Gambler{
 
     @Override
     public void pickCard() {
-        cards.addCard(Deck.pickCard());
+        if (cards.calculateScore() <= PICK_BOUNDARY) {
+            cards.addCard(Deck.pickCard());
+        }
     }
 
     @Override
     public void initialPick() {
         pickCard();
+    }
+
+    public int getCardsSize() {
+        return cards.getCards().size();
     }
 }
