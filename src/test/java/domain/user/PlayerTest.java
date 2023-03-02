1ee0180edcbfa5eab1f88516cf3de85846fcd328
yujamint <woawoa0513@gmail.com>
2023-03-02 21:33:47 +0900

    test: 플레이어의 버스트 판단 기능 테스트 작성

--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -37,4 +37,22 @@ class PlayerTest {
         //then
         assertThat(cards.size()).isEqualTo(3);
     }
+
+    @Test
+    @DisplayName("플레이어 합산 점수가 21을 초과하면 Bust 상태가 된다.")
+    void checkBustByScoreTest() {
+        //given
+        List<Card> firstTurnCards = List.of(CloverCard.CLOVER_TEN, CloverCard.CLOVER_KING);
+        Player player = new Player(firstTurnCards);
+        ScoreCalculator scoreCalculator = new ScoreCalculator();
+
+        //when
+        player.receiveCard(CloverCard.CLOVER_QUEEN);
+        int score = scoreCalculator.calculate(player.getCards());
+        player.checkBustByScore(score);
+        boolean result = player.isBust();
+
+        //then
+        assertThat(result).isTrue();
+    }
 }
