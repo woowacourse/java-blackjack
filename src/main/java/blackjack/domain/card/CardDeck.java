44f7607f1313c3305d4bcfc9bcbd94b29f1b181c
hanueleee <ezzeezz@naver.com>
2023-03-02 13:24:59 +0900

    feat: 52장의 카드를 생성하는 기능 구현

---
+++ src/main/java/blackjack/domain/card/CardDeck.java
@@ -0,0 +1,43 @@
+package blackjack.domain.card;
+
+import blackjack.domain.card.exception.NoMoreCardException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardDeck {
+
+    private final List<Card> cards;
+
+    private CardDeck(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public static CardDeck create() {
+        List<Card> cards = new ArrayList<>();
+        addShapeSets(cards);
+        return new CardDeck(cards);
+    }
+
+    private static void addShapeSets(List<Card> cards) {
+        for (Shape shape : Shape.values()) {
+            addNumberSets(cards, shape);
+        }
+    }
+
+    private static void addNumberSets(List<Card> cards, Shape shape) {
+        for (Number number : Number.values()) {
+            cards.add(new Card(shape, number));
+        }
+    }
+
+    public Card pick() {
+        validateCardExist();
+        return cards.remove(0);
+    }
+
+    private void validateCardExist() {
+        if (cards.isEmpty()) {
+            throw new NoMoreCardException();
+        }
+    }
+}
