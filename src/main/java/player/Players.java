5bb34818db0203e5c134db5e5617c8485cde3519
dev_kong <ahdjd5@gmail.com>
2023-03-02 20:41:49 +0900

    feat: 인덱스에 해당하는 플레이어에게 카드를 주는 기능

--- src/main/java/player/Players.java
+++ src/main/java/player/Players.java
@@ -3,10 +3,17 @@ package player;
 import java.util.ArrayList;
 import java.util.List;
 
+import card.Card;
+
 public class Players {
     private final List<Player> players = new ArrayList<>();
 
     public void add(Player player) {
         players.add(player);
+    }
+
+    public void takeCard(int playerIndex, Card card) {
+        Player player = players.get(playerIndex);
+        player.hit(card);
     }
 }
--- src/test/java/player/PlayersTest.java
+++ src/test/java/player/PlayersTest.java
@@ -1,9 +1,14 @@
 package player;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import card.Card;
+import card.CardNumber;
+import card.Pattern;
 
 class PlayersTest {
     @DisplayName("생성할 수 있다")
@@ -18,5 +23,18 @@ class PlayersTest {
         Players players = new Players();
         Player player = new Player(new Name("로지"));
         players.add(player);
+    }
+
+    @DisplayName("인덱스에 해당하는 플레이어가 카드를 받을 수 있다.")
+    @Test
+    void takeCard() {
+        Players players = new Players();
+        Player player = new Player(new Name("폴로"));
+        players.add(player);
+        Card card = new Card(CardNumber.ACE, Pattern.CLOVER);
+
+        players.takeCard(0, card);
+
+        assertThat(player.showCards()).contains(card);
     }
 }
