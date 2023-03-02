b6a81b98674868f9bffc986aa342bf679329b383
eunbii0213 <eunbii0213@naver.com>
2023-03-02 13:26:59 +0900

    style: 불필요한 import문 삭제

--- src/test/java/domain/DeckTest.java
+++ src/test/java/domain/DeckTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class DeckTest {
 
--- src/test/java/domain/PlayerNameTest.java
+++ src/test/java/domain/PlayerNameTest.java
@@ -3,7 +3,6 @@ package domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
