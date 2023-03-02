00a6fc02cb2ff393d5e2b4c214159677b79f2922
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-03-02 17:00:08 +0900

    test: 카드 뽑기 판별 테스트

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -1,6 +1,8 @@
 package domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,12 +18,12 @@ public class DealerTest {
         Deck deck = new Deck();
         names = List.of("crong", "pobi", "jerry", "hardy");
         dealer = new Dealer(deck, names);
-        dealer.init();
     }
 
     @DisplayName("처음 카드를 나눠주면 플레이어의 패의 크기는 2이다.")
     @Test
     void distributeTwoCardsToPlayersTest() {
+        dealer.init();
         int expectedCardSize = 2;
         for (String name : names) {
             assertEquals(expectedCardSize, dealer.getCards(name).size());
@@ -31,10 +33,35 @@ public class DealerTest {
     @DisplayName("처음 카드를 나눠주면 플레이어의 패의 크기는 2이다.")
     @Test
     void drawCardTest() {
+        dealer.init();
         int expectedCardSize = 3;
         for (String name : names) {
             dealer.drawCard(name);
             assertEquals(expectedCardSize, dealer.getCards(name).size());
         }
+    }
+
+    @DisplayName("딜러의 점수가 16이하이면 더 뽑아야 한다.")
+    @Test
+    void shouldDealerDrawTest() {
+        Card firstCard = new Card(Suit.CLOVER, Rank.FOUR);
+        Card secondCard = new Card(Suit.CLOVER, Rank.KING);
+
+        dealer.drawCard(firstCard);
+        dealer.drawCard(secondCard);
+
+        assertTrue(dealer.isDealerDraw());
+    }
+
+    @DisplayName("딜러의 점수가 17이상이면 더 뽑으면 안된다.")
+    @Test
+    void shouldNotDealerDrawTest() {
+        Card firstCard = new Card(Suit.CLOVER, Rank.ACE);
+        Card secondCard = new Card(Suit.CLOVER, Rank.KING);
+
+        dealer.drawCard(firstCard);
+        dealer.drawCard(secondCard);
+
+        assertFalse(dealer.isDealerDraw());
     }
 }
