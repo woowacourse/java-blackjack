bcd1ed79e9f74f9abb90925ab13e5be328c05c62
Eunkeee <eunkeeeee@gmail.com>
2023-03-02 15:47:01 +0900

    feat(InputView): 입력 값 받기

--- src/main/java/domain/Participant.java
+++ src/main/java/domain/Participant.java
@@ -23,6 +23,4 @@ public class Participant {
         }
         return totalScore;
     }
-
-
 }
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final InputView instance = new InputView();
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static InputView getInstance() {
+        return instance;
+    }
+
+    private InputView() {
+    }
+
+    public List<String> readPlayerNames() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        String input = scanner.nextLine();
+        return List.of(input.split(","));
+    }
+
+    public String readHit(String name) {
+        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)" + System.lineSeparator(), name);\
+        return scanner.nextLine();
+    }
+}
