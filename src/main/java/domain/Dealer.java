576dad0102eb43d922e8f5f4b8125cbc8bbb3797
aiaiaiai1 <cvc100326@naver.com>
2023-03-02 15:03:17 +0900

    feat: 카드를 받는 기능 추가

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -3,13 +3,23 @@ package domain;
 public class Dealer {
     private static final String NAME = "딜러";
     private final Name name;
+    private final Cards cards;
 
     public Dealer() {
         this.name = new Name(NAME);
+        cards = new Cards();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
     }
 
     public String getName() {
         return name.getName();
+    }
+
+    public Card getCard(int index) {
+        return cards.getCard(index);
     }
 
 }
