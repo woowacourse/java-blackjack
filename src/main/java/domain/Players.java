d4d4e2e8401c0c558e7501e1a5cf8f5318380abb
aiaiaiai1 <cvc100326@naver.com>
2023-03-02 15:32:16 +0900

    feat: 중복된 이름인 경우 예외 발생 기능 추가

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -5,14 +5,25 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Players {
+    private static final String INVALID_NAME = "중복된 이름입니다.";
+
     private List<Player> players = new ArrayList<>();
+
     public Players(String names) {
-        for(String name : splitName(names)) {
+        List<String> splitedName = splitName(names);
+        validateDuplicatedName(splitedName);
+        for (String name : splitName(names)) {
             players.add(new Player(name));
         }
     }
 
-    private  List<String> splitName(String names) {
+    private void validateDuplicatedName(List<String> splitedName) {
+        if (splitedName.size() != splitedName.stream().distinct().count()) {
+            throw new IllegalArgumentException(INVALID_NAME);
+        }
+    }
+
+    private List<String> splitName(String names) {
         return Arrays.asList(names.split(","));
     }
 }
