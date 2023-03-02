d9e615d50e5fa31286aefa40c95bb2645721f766
seokjin8678 <seokjin8678@gmail.com>
2023-03-02 16:52:24 +0900

    feat: view 구현
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>

---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,22 @@
+package blackjack.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String DELIMITER = ",";
+
+    private InputView() {
+    }
+
+    public static String[] readPersonNames() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        String names = scanner.nextLine();
+        return names.split(DELIMITER);
+    }
+
+    public static String readDrawCardDecision(String personName) {
+        System.out.println(personName + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+        return scanner.nextLine();
+    }
+}
---
+++ src/main/java/blackjack/view/OutputView.java
@@ -0,0 +1,59 @@
+package blackjack.view;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
+
+import blackjack.domain.GameResult;
+import java.util.List;
+import java.util.Map;
+
+public class OutputView {
+    private static final int DEALER_DRAW_BOUNDARY = 16;
+
+    private OutputView() {
+    }
+
+    public static void printDefaultDrawCardMessage(List<String> personNames) {
+        final String names = String.join(", ", personNames);
+        System.out.println("딜러와 " + names + "에게 2장을 나누었습니다.");
+    }
+
+    public static void printCardsStatus(String personName, List<String> cards) {
+        System.out.println(getCardStatus(personName, cards));
+    }
+
+    private static String getCardStatus(String personName, List<String> cards) {
+        final String allCards = String.join(", ", cards);
+        return personName + " 카드: " + allCards;
+    }
+
+    public static void printCardsStatus(String personName, List<String> cards, int score) {
+        System.out.println(getCardStatus(personName, cards) + "- 결과: " + score);
+    }
+
+    public static void printDealerDrawCardMessage(int score) {
+        if (score > DEALER_DRAW_BOUNDARY) {
+            System.out.println("딜러는 " + DEALER_DRAW_BOUNDARY + "초과라 카드를 받지 않았습니다.");
+            return;
+        }
+        System.out.println("딜러는 " + DEALER_DRAW_BOUNDARY + "이하라 한장의 카드를 더 받았습니다.");
+    }
+
+    public static void printGameEndMessage() {
+        System.out.println("## 최종 승패");
+    }
+
+    public static void printDealerResult(List<GameResult> gameResults) {
+        Map<String, Long> collect = gameResults.stream()
+                .collect(groupingBy(GameResult::getName, counting()));
+        String result = collect.entrySet().stream()
+                .map(entry -> entry.getValue() + entry.getKey())
+                .collect(joining(" "));
+        System.out.println("딜러: " + result);
+    }
+
+    public static void printPersonResult(String personName, GameResult gameResult) {
+        System.out.println(personName + ": " + gameResult.getName());
+    }
+}
