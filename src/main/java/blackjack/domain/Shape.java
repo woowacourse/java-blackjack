9c4913d39e8a65ed737a8a2f1366902c06bd8258
greeng00se <thegreengoose7@gmail.com>
2023-03-02 13:32:01 +0900

    feat: 모양 생성 기능 추가
    Co-authored-by: Combi153 <ssrno009@gmail.com>

---
+++ src/main/java/blackjack/domain/Shape.java
@@ -0,0 +1,18 @@
+package blackjack.domain;
+
+enum Shape {
+    DIAMOND("다이아몬드"),
+    SPADE("스페이드"),
+    CLOVER("클로버"),
+    HEART("하트");
+
+    private final String letter;
+
+    Shape(final String letter) {
+        this.letter = letter;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+}
---
+++ src/test/java/blackjack/domain/ShapeTest.java
@@ -0,0 +1,17 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+public class ShapeTest {
+
+    @Test
+    void 심볼을_반환한다() {
+        assertThat(Shape.DIAMOND.getLetter()).isEqualTo("다이아몬드");
+    }
+}
