301946addced07ea53959a2775cbd2e23159c3d1
hyunseo <goatsfish@gmail.com>
2023-03-02 14:06:56 +0900

    refactor: 영문이 아닌 이름을 허용하도록 리팩터링
    Co-authored-by: Cyma-s <ssol0319@naver.com>

--- src/main/java/blackjack/domain/Name.java
+++ src/main/java/blackjack/domain/Name.java
@@ -1,11 +1,6 @@
 package blackjack.domain;
 
-import java.util.regex.Pattern;
-
 public class Name {
-
-    private static final Pattern pattern = Pattern.compile("([A-Z][a-z]+)");
-
     private final String value;
 
     public Name(final String value) {
@@ -16,9 +11,6 @@ public class Name {
     private void validate(final String value) {
         if (value.isBlank()) {
             throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
-        }
-        if (!pattern.matcher(value).matches()) {
-            throw new IllegalArgumentException("이름은 영문이어야 합니다.");
         }
     }
 }
--- src/test/java/blackjack/domain/NameTest.java
+++ src/test/java/blackjack/domain/NameTest.java
@@ -3,7 +3,6 @@ package blackjack.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -13,14 +12,6 @@ class NameTest {
     @EmptySource
     @DisplayName("이름이 공백일 경우 예외가 발생한다.")
     void throwExceptionWhenNameIsBlank(final String value) {
-        assertThatThrownBy(() -> new Name(value))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"123", "1a3", "베로", "헤나"})
-    @DisplayName("이름이 영문이 아닐 경우 예외가 발생한다.")
-    void throwExceptionWhenNameIsNotAlphabet(final String value) {
         assertThatThrownBy(() -> new Name(value))
                 .isInstanceOf(IllegalArgumentException.class);
     }
