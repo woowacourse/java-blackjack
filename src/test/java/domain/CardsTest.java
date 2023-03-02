baea3391debe69d168b65b2bb69479a2cf55419b
jaeyeon kim <jakind@naver.com>
2023-03-02 15:31:29 +0900

    test: Cards 가 점수를 계산한다.
    - domain.CardsTest

--- src/test/java/domain/CardsTest.java
+++ src/test/java/domain/CardsTest.java
@@ -19,4 +19,44 @@ public class CardsTest {
         assertThat(cards.getSize()).isEqualTo(2);
     }
 
+    @Test
+    @DisplayName("Player 의 카드 점수를 계산한다.")
+    void calculateCardScorePlayer() {
+        List<Card> initialCards = List.of(new Card(Shape.DIAMOND, Value.TWO), new Card(Shape.HEART, Value.ACE));
+
+        Cards cards = new Cards(initialCards);
+
+        assertThat(cards.calculateScore(21)).isEqualTo(13);
+    }
+
+    @Test
+    @DisplayName("Player 의 Ace 를 2개 낮춰야 하는 경우 카드 점수를 계산한다.")
+    void calculateCardScoreWhenAceDecreaseTwice() {
+        List<Card> initialCards = List.of(new Card(Shape.DIAMOND, Value.ACE), new Card(Shape.DIAMOND, Value.JACK),new Card(Shape.HEART, Value.ACE));
+
+        Cards cards = new Cards(initialCards);
+
+        assertThat(cards.calculateScore(21)).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("Dealer 의 카드 점수를 계산한다.")
+    void calculateCardScoreOfDealer() {
+        List<Card> initialCards = List.of(new Card(Shape.DIAMOND, Value.ACE), new Card(Shape.DIAMOND, Value.NINE));
+
+        Cards cards = new Cards(initialCards);
+
+        assertThat(cards.calculateScore(16)).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("블랙잭일 때 Dealer 의 카드 점수를 계산한다.")
+    void calculateCardScoreOfDealerWhenBlackJack() {
+        List<Card> initialCards = List.of(new Card(Shape.DIAMOND, Value.ACE), new Card(Shape.DIAMOND, Value.JACK));
+
+        Cards cards = new Cards(initialCards);
+
+        assertThat(cards.calculateScore(16)).isEqualTo(21);
+    }
+
 }
