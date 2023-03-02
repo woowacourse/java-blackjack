f4315ad5fc8c8f8f6140242e1e628ccb275a42c5
yujamint <woawoa0513@gmail.com>
2023-03-02 17:47:35 +0900

    feat: 점수 계산 기능 매개변수 수정
    세부사항
    - List<Card>를 받는 것으로 수정

--- src/main/java/domain/user/ScoreCalculator.java
+++ src/main/java/domain/user/ScoreCalculator.java
@@ -1,7 +1,9 @@
 package domain.user;
 
+import domain.Card.Card;
 import domain.Number;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ScoreCalculator {
 
@@ -9,24 +11,21 @@ public class ScoreCalculator {
     private static final int ZERO = 0;
     private static final int ACE_ONE = 1;
 
-    public int calculate(List<Integer> numbers) {
+    public int calculate(List<Card> cards) {
+        List<Integer> numbers = convertCardsToNumbers(cards);
         int aceCount = countOfAce(numbers);
         int sum = sum(numbers);
 
         if (sum > BLACKJACK) {
             sum = calculateAceAsOne(aceCount, sum);
         }
-
         return sum;
     }
 
-    private int calculateAceAsOne(int aceCount, int sum) {
-        while(aceCount > ZERO && sum > BLACKJACK) {
-            sum -= Number.ACE.getNumber();
-            sum += ACE_ONE;
-            aceCount--;
-        }
-        return sum;
+    private List<Integer> convertCardsToNumbers(List<Card> cards) {
+        return cards.stream()
+                .map(Card::getNumber)
+                .collect(Collectors.toUnmodifiableList());
     }
 
     private int countOfAce(List<Integer> numbers) {
@@ -39,5 +38,14 @@ public class ScoreCalculator {
         return numbers.stream()
                 .mapToInt(number -> number)
                 .sum();
+    }
+
+    private int calculateAceAsOne(int aceCount, int sum) {
+        while(aceCount > ZERO && sum > BLACKJACK) {
+            sum -= Number.ACE.getNumber();
+            sum += ACE_ONE;
+            aceCount--;
+        }
+        return sum;
     }
 }
