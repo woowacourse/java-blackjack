dc0cec8dd5263b3dd43a9abf43cb573a0f992a03
ReO <yt930310@gmail.com>
2023-03-02 14:49:24 +0900

    test(Player): 이름이 "딜러"인 예외 상황에 대한 테스트

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -2,6 +2,8 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -10,5 +12,13 @@ class PlayerTest {
     @NullAndEmptySource
     void validatePlayerNameTest(String name) {
         assertThatThrownBy(() -> new Player(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
+    @Test
+    @DisplayName("플레이어의 이름은 '딜러'일 수 없다.")
+    void validateNoDealer() {
+        assertThatThrownBy(() -> new Player("딜러")).isInstanceOf(IllegalArgumentException.class);
     }
 }
