bd3df498a4a94245abe05f801831e5e4c9cc673e
shin-mallang <huipulci1@naver.com>
2023-03-02 21:25:49 +0900

    refactor: CardDect 생성하는 로직 수정

--- src/main/java/domain/deck/CardDeck.java
+++ src/main/java/domain/deck/CardDeck.java
@@ -5,12 +5,15 @@ import domain.card.CardShape;
 import domain.card.CardValue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
 
 public class CardDeck {
+
+    private static final int DRAW_CARD_INDEX = 0;
 
     private final List<Card> cards;
 
@@ -19,21 +22,24 @@ public class CardDeck {
     }
 
     public static CardDeck shuffledFullCardDeck() {
-        List<Card> cards = new ArrayList<>();
-        for (final CardShape cardShape : CardShape.values()) {
-            cards.addAll(Arrays.stream(CardValue.values())
-                    .map(it -> new Card(cardShape, it))
-                    .collect(Collectors.toList()));
-        }
+        final List<Card> cards = makeTotalCards();
         Collections.shuffle(cards);
         return new CardDeck(cards);
     }
 
+    private static List<Card> makeTotalCards() {
+        return stream(CardShape.values())
+                .flatMap(shape ->
+                        stream(CardValue.values())
+                                .map(value -> new Card(shape, value))
+                ).collect(toList());
+    }
+
     public List<Card> cards() {
-        return List.copyOf(cards);
+        return new ArrayList<>(cards);
     }
 
     public Card draw() {
-        return this.cards.remove(0);
+        return this.cards.remove(DRAW_CARD_INDEX);
     }
 }
