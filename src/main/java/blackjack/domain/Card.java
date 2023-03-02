6fe343610efc47654ad783340a37ad8f110435e2
kdkdhoho <hkim4410@naver.com>
2023-03-02 17:26:08 +0900

    feat(Card): equals&hashCode 재정의

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -1,5 +1,7 @@
 package blackjack.domain;
 
+import java.util.Objects;
+
 public class Card {
 
     private final Number number;
@@ -16,5 +18,18 @@ public class Card {
 
     public boolean isAce() {
         return this.number == Number.ACE;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return number == card.number && pattern == card.pattern;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, pattern);
     }
 }
