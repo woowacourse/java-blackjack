def0edfb7d261f3cca4c01ff0c375a61beec13ab
yeonkkk <tjddus0302@gmail.com>
2023-03-02 19:57:23 +0900

    feat: 입력 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,33 @@
+package view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public List<String> readNames() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+
+        String input = scanner.nextLine();
+        return splitWithComma(input);
+    }
+
+    public boolean askForAnotherCard(String name) {
+        System.out.println(name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+
+        String input = scanner.nextLine();
+
+        if (input.equals("y")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private List<String> splitWithComma(String input) {
+        return Arrays.asList(input.split(","));
+    }
+
+}
