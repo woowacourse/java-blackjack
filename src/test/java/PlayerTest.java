92acf40e951c20dbeabb17f68c4c29a189ef6e7f
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-03-02 13:15:00 +0900

    test: 버스트 조건 검증 테스트 구현

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,14 +1,20 @@
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
 
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player(new PlayerName("pobi"));
+    }
+
     @DisplayName("플레이어는 카드를 받을 수 있다.")
     @Test
-    void receiveCardSuccessTest(){
-        Player player = new Player(new PlayerName("pobi"));
-
+    void receiveCardSuccessTest() {
         player.receiveCard(new Card(Shape.SPADE, Number.A));
 
         Assertions.assertThat(player).extracting("cards")
@@ -18,8 +24,7 @@ class PlayerTest {
 
     @DisplayName("플레이어가 받은 카드의 점수를 확인할 수 있다.")
     @Test
-    void calculateScoreSuccessTest(){
-        Player player = new Player(new PlayerName("pobi"));
+    void calculateScoreSuccessTest() {
 
         player.receiveCard(new Card(Shape.CLUB, Number.A));
         player.receiveCard(new Card(Shape.HEART, Number.THREE));
@@ -31,8 +36,7 @@ class PlayerTest {
 
     @DisplayName("21을 넘은 경우 ACE는 1로 계산한다.")
     @Test
-    void calculateScoreSuccessTestWhenHasAce(){
-        Player player = new Player(new PlayerName("pobi"));
+    void calculateScoreSuccessTestWhenHasAce() {
 
         player.receiveCard(new Card(Shape.CLUB, Number.A));
         player.receiveCard(new Card(Shape.HEART, Number.THREE));
@@ -45,8 +49,7 @@ class PlayerTest {
 
     @DisplayName("21을 넘지 않는 경우 까지 ACE는 1로 계산한다.")
     @Test
-    void calculateScoreSuccessTestWhenHasAceUntilNotBusted(){
-        Player player = new Player(new PlayerName("pobi"));
+    void calculateScoreSuccessTestWhenHasAceUntilNotBusted() {
 
         player.receiveCard(new Card(Shape.CLUB, Number.A));
         player.receiveCard(new Card(Shape.HEART, Number.A));
@@ -55,6 +58,28 @@ class PlayerTest {
         Assertions.assertThat(player.calculateScore())
                 .isEqualTo(12);
 
+    }
+
+    @DisplayName("카드의 합이 21이 넘으면 버스트 된다.")
+    @Test
+    void isBustedSuccessTest() {
+
+        player.receiveCard(new Card(Shape.CLUB, Number.J));
+        player.receiveCard(new Card(Shape.HEART, Number.TEN));
+        player.receiveCard(new Card(Shape.HEART, Number.Q));
+
+        Assertions.assertThat(player.isBusted()).isTrue();
+    }
+
+    @DisplayName("카드의 합이 21이 넘지 않으면 버스트 되지 않는다.")
+    @Test
+    void isNotBustedSuccessTest() {
+
+        player.receiveCard(new Card(Shape.CLUB, Number.A));
+        player.receiveCard(new Card(Shape.HEART, Number.A));
+        player.receiveCard(new Card(Shape.SPADE, Number.A));
+
+        Assertions.assertThat(player.isBusted()).isFalse();
     }
 
 }
