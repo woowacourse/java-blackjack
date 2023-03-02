7603a6b4b944772322df8c916fb2b8dce373cae2
kokodak <kokodakadokok@gmail.com>
2023-03-02 21:11:40 +0900

    feat: 참가자 인원 검증 기능 구현
    Co-authored-by: woo-chang <clllickme@naver.com>

---
+++ src/main/java/blackjack/domain/Participants.java
@@ -0,0 +1,40 @@
+package blackjack.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Participants {
+
+    private static final int MAXIMUM_PARTICIPANT_COUNT = 6;
+
+    private final List<Participant> participants;
+
+    public Participants(final List<Participant> participants) {
+        validate(participants);
+        this.participants = List.copyOf(participants);
+    }
+
+    private void validate(final List<Participant> participants) {
+        validateCount(participants);
+        validateDuplicate(participants);
+    }
+
+    private void validateCount(final List<Participant> participants) {
+        if (participants.size() > MAXIMUM_PARTICIPANT_COUNT) {
+            throw new IllegalArgumentException("참가자는 " + MAXIMUM_PARTICIPANT_COUNT + "명을 초과할 수 없습니다");
+        }
+    }
+
+    private void validateDuplicate(final List<Participant> participants) {
+        final Set<Participant> uniqueParticipants = new HashSet<>(participants);
+
+        if (uniqueParticipants.size() != participants.size()) {
+            throw new IllegalArgumentException("참가자 이름은 중복될 수 없습니다.");
+        }
+    }
+
+    public List<Participant> getParticipants() {
+        return participants;
+    }
+}
--- src/test/java/blackjack/domain/ParticipantsTest.java
+++ src/test/java/blackjack/domain/ParticipantsTest.java
@@ -4,9 +4,7 @@ import static blackjack.domain.Number.ACE;
 import static blackjack.domain.Suit.SPADE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -29,26 +27,23 @@ class ParticipantsTest {
         assertThatThrownBy(() -> new Participants(participants))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-}
 
-class Participants {
+    @Test
+    void 딜러_포함_참여자가_6명_초과라면_예외를_던진다() {
+        final Cards cards = new Cards(List.of(
+                new Card(ACE, SPADE)
+        ));
+        final List<Participant> participants = List.of(
+                new Dealer(cards),
+                new Player("dazzle", cards),
+                new Player("kokodak", cards),
+                new Player("odo", cards),
+                new Player("hoi", cards),
+                new Player("gray", cards),
+                new Player("pobi", cards)
+        );
 
-    private final List<Participant> participants;
-
-    public Participants(final List<Participant> participants) {
-        validate(participants);
-        this.participants = List.copyOf(participants);
-    }
-
-    private void validate(final List<Participant> participants) {
-        final Set<Participant> uniqueParticipants = new HashSet<>(participants);
-
-        if (uniqueParticipants.size() != participants.size()) {
-            throw new IllegalArgumentException("참가자 이름은 중복될 수 없습니다.");
-        }
-    }
-
-    public List<Participant> getParticipants() {
-        return participants;
+        assertThatThrownBy(() -> new Participants(participants))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
