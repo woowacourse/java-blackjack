757c4eb0c699b5f1f80024fa80f23b4bc16103ed
sosow0212 <sosow0212@naver.com>
2023-03-02 16:07:40 +0900

    feat: 입력을 받는 InputView 클래스 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,42 @@
+package view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final List<String> VOWEL = List.of("a", "e", "i", "o", "u");
+    private static final String DELIMITER = ",";
+    private static final String END_VOWEL = "는";
+    private static final String END_CONSONANT = "은";
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public List<String> readPlayerNames() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        String playerNames = scanner.next();
+        String[] splitNames = playerNames.split(DELIMITER);
+        return Arrays.asList(splitNames);
+    }
+
+    public String readChoiceOfDrawCard(final String name) {
+        System.out.println(name + generatePreposition(name) + " 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+        return scanner.next();
+    }
+
+    private String generatePreposition(final String name) {
+        String lowerCaseName = name.toLowerCase();
+
+        if (isEndWithVowel(lowerCaseName)) {
+            return END_VOWEL;
+        }
+
+        return END_CONSONANT;
+    }
+
+    private boolean isEndWithVowel(final String lowerCaseName) {
+        return VOWEL.stream()
+                .anyMatch(i -> lowerCaseName.endsWith(i));
+    }
+}
