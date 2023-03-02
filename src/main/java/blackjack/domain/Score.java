174135eb1639a2c4e5af28b19dd0359df604d52e
yenawee <yaena0319@naver.com>
2023-03-02 15:58:54 +0900

    feat : 21점 초과일 때 ACE 점수 처리 기능 구현
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- src/main/java/blackjack/domain/Score.java
+++ src/main/java/blackjack/domain/Score.java
@@ -3,11 +3,32 @@ package blackjack.domain;
 import java.util.List;
 
 public class Score {
+    private static final int MAX_BLACKJACK_SCORE = 21;
+    private static final int ACE_GAP = 10;
 
     private int score;
 
-    public void calculateScore(List<TrumpNumber> trumpNumbers){
-        score = trumpNumbers.stream().mapToInt(TrumpNumber::getScore).sum();
+    public void calculateScore(List<TrumpNumber> trumpNumbers) {
+        score = trumpNumbers.stream()
+                .mapToInt(TrumpNumber::getScore)
+                .sum();
+        if (score > MAX_BLACKJACK_SCORE) {
+            handleBurst(trumpNumbers);
+        }
+    }
+
+    private void handleBurst(List<TrumpNumber> trumpNumbers) {
+        int aceCount = countAce(trumpNumbers);
+        while (score > MAX_BLACKJACK_SCORE && aceCount > 0) {
+            score -= ACE_GAP;
+            aceCount -= 1;
+        }
+    }
+
+    private int countAce(List<TrumpNumber> trumpNumbers) {
+        return (int) trumpNumbers.stream()
+                .filter(e -> e.equals(TrumpNumber.ACE))
+                .count();
     }
 
     public int getScore() {
