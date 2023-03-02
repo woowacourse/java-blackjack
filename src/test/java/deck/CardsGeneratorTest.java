54741dd5df2de2d67394510aa48400e4f216a6fd
dev_kong <ahdjd5@gmail.com>
2023-03-02 16:48:50 +0900

    test: 중복인 카드가 없는지 확인하는 테스트

--- src/test/java/deck/CardsGeneratorTest.java
+++ src/test/java/deck/CardsGeneratorTest.java
@@ -4,17 +4,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Stack;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import card.Card;
 
 class CardsGeneratorTest {
+    CardsGenerator cardsGenerator;
+
+    @BeforeEach
+    void setUp() {
+        cardsGenerator = new ShuffledCardsGenerator();
+    }
+
     @DisplayName("생성되는 카드는 52장어야한다.")
     @Test
     void generate() {
-        CardsGenerator cardsGenerator = new ShuffledCardsGenerator();
         Stack<Card> cards = cardsGenerator.generate();
         assertThat(cards.size()).isEqualTo(52);
     }
+
+    @DisplayName("중복인 카드가 생성되지 않는다.")
+    @Test
+    void generateUnique() {
+        Stack<Card> cards = cardsGenerator.generate();
+        assertThat(cards.stream().distinct()).hasSize(52);
+    }
+
+
 }
