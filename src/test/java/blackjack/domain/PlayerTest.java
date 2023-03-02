4c63f344ff3eef61ec98290cfb9aae7946348ed0
hyunseo <goatsfish@gmail.com>
2023-03-02 14:52:21 +0900

    test: 플레이어 카드 오픈, 히트 테스트 추가
    Co-authored-by: Cyma-s <ssol0319@naver.com>

---
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -0,0 +1,34 @@
+package blackjack.domain;
+
+import blackjack.fixture.ParticipantCardsFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PlayerTest {
+    @Test
+    @DisplayName("플레이어가 카드를 뽑는다.")
+    void hit() {
+        Card cardOne = new Card(CardShape.DIAMOND, CardNumber.THREE);
+        Card cardTwo = new Card(CardShape.DIAMOND, CardNumber.TWO);
+        Participant player = new Player(ParticipantCardsFixture.createParticipantsCards(List.of(cardOne, cardTwo)));
+        int beforeHitPoint = player.getTotalPoint();
+        player.hit(new Card(CardShape.SPADE, CardNumber.ACE));
+        int afterHitPoint = player.getTotalPoint();
+
+        assertThat(afterHitPoint).isGreaterThan(beforeHitPoint);
+    }
+
+    @Test
+    @DisplayName("플레이어가 카드를 보여준다.")
+    void open() {
+        Card cardOne = new Card(CardShape.DIAMOND, CardNumber.THREE);
+        Card cardTwo = new Card(CardShape.DIAMOND, CardNumber.TWO);
+        Participant player = new Player(ParticipantCardsFixture.createParticipantsCards(List.of(cardOne, cardTwo)));
+
+        assertThat(player.open(2)).containsAll(List.of(cardOne, cardTwo));
+    }
+}
--- src/test/java/blackjack/fixture/ParticipantFixture.java
+++ src/test/java/blackjack/fixture/ParticipantFixture.java
@@ -4,11 +4,22 @@ import blackjack.domain.Card;
 import blackjack.domain.Participant;
 import blackjack.domain.ParticipantCards;
 
+import java.util.Collections;
 import java.util.List;
 
 public abstract class ParticipantFixture {
     public static Participant create(final List<Card> cards) {
         ParticipantCards participantsCards = ParticipantCardsFixture.createParticipantsCards(cards);
-        return new Participant(participantsCards) {};
+        return new Participant(participantsCards) {
+            @Override
+            protected void hit(final Card card) {
+                /* NOT IMPLEMENTED */
+            }
+
+            @Override
+            protected List<Card> open(final int cardCount) {
+                return Collections.emptyList();
+            }
+        };
     }
 }
