3c4fba39d911dc57b6a8abbafc836dc9b17e7a60
jaeyeon kim <jakind@naver.com>
2023-03-02 15:27:02 +0900

    feat: Cards 를 생성한다.
    - domain.Cards

---
+++ src/main/java/domain/Cards.java
@@ -0,0 +1,17 @@
+package domain;
+
+import java.util.List;
+
+public class Cards {
+
+    private final List<Card> cards;
+
+    public Cards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public int getSize() {
+        return cards.size();
+    }
+
+}
