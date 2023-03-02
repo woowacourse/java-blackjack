0249eb10e84b5f585e6484d9c39b974c0f31b90e
shin-mallang <huipulci1@naver.com>
2023-03-02 20:29:08 +0900

    refactor: 뷰 로직 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,16 +11,17 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
+    private static final String DELIMITER = ", ";
+
     public static List<String> readParticipantsName() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         final String input = scanner.nextLine();
-
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(DELIMITER))
                 .collect(Collectors.toList());
     }
 
     public static String readMoreCard(final Participant participant) {
-        System.out.println("\n" + participant.name().value() + "는 한장의 카드를 더 받으시겠습니다?(예는 y, 아니오는 n)");
+        System.out.printf("\n%s는 한장의 카드를 더 받으시겠습니까?(예는 y, 아니오는 n)\n", participant.name().value());
         return scanner.nextLine();
     }
 }
