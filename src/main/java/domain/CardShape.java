df1dbbba1e5476464ba27f10db3fb7b741883d69
splitube <voicesplit@daum.net>
2023-03-02 21:54:28 +0900

    feat: Card 객채 생성
    Co-authored-by: echo724 <echo.backend@gmail.com>

---
+++ src/main/java/domain/CardShape.java
@@ -0,0 +1,8 @@
+package domain;
+
+public enum CardShape {
+    SPADE,
+    HEART,
+    CLOVER,
+    DIAMOND
+}
---
+++ src/test/java/domain/CardTest.java
@@ -0,0 +1,18 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CardTest {
+
+    @Test
+    @DisplayName("카드는 자신의 점수를 반환한다.")
+    void getScore() {
+        Card card = new Card(CardNumber.KING, CardShape.SPADE);
+        CardNumber cardNumber = card.getCardNumber();
+        CardShape shape = card.getCardShape();
+        Assertions.assertThat(cardNumber).isEqualTo(CardNumber.KING);
+        Assertions.assertThat(shape).isEqualTo(CardShape.SPADE);
+    }
+}
