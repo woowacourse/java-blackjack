48de821fed253ee8b074981ba7e05e97f3f0dace
java-saeng <gusdn85554@naver.com>
2023-03-02 17:01:32 +0900

    refactor : Dealer 최대 점수 상수화

--- src/main/java/domain/participant/Dealer.java
+++ src/main/java/domain/participant/Dealer.java
@@ -4,12 +4,14 @@ import domain.area.CardArea;
 
 public class Dealer extends Player {
 
+    private static final int DEALER_LIMIT_SCORE = 16;
+
     public Dealer(final Name name, final CardArea cardArea) {
         super(name, cardArea);
     }
 
     @Override
     public boolean canHit() {
-        return cardArea.calculate() <= 16;
+        return cardArea.calculate() <= DEALER_LIMIT_SCORE;
     }
 }
