c06eef616343b26d4d0e41bcf1361b58cc360688
yenawee <yaena0319@naver.com>
2023-03-02 16:23:53 +0900

    test : 현재 가지고 있는 카드의 점수를 계산해서 더 받을 수 있는지 여부를 확인 test
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- src/test/java/blackjack/domain/PlayerTest.java
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -1,5 +1,7 @@
 package blackjack.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,20 +9,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
-//    새로 뽑을 수 있는지 판단
 
-//    점수 계산
+    static Player player;
 
-    @DisplayName("카드 한장씩 잘 받는지 테스트")
+    @BeforeAll
+    static void setUp() {
+        player = new Player(new ArrayList<>(List.of(new Card(TrumpShape.DIAMOND, TrumpNumber.EIGHT), new Card(TrumpShape.HEART, TrumpNumber.JACK))));
+    }
+
     @Test
-    void receiveCardSuccess(){
-        Player player = new Player(new ArrayList<>(List.of(new Card(TrumpShape.DIAMOND, TrumpNumber.EIGHT), new Card(TrumpShape.HEART, TrumpNumber.JACK))));
+    @DisplayName("카드 한장씩 잘 받는지 테스트")
+    void receiveCardSuccess() {
         Card card = new Card(TrumpShape.CLOVER, TrumpNumber.FIVE);
         player.receiveCard(card);
         List<Card> cards = player.getCards();
         assertThat(cards.get(cards.size() - 1)).isEqualTo(card);
+    }
+
+    @Test
+    @DisplayName("카드 새로 뽑을 수 있는지 판단 - 성공")
+    void canReceiveNewCard() {
+        assertThat(player.isAbleToReceive()).isTrue();
+    }
+
+    @Test
+    @DisplayName("카드 새로 뽑을 수 있는지 판단 - 실패")
+    void cannotReceiveNewCard() {
+        player.receiveCard(new Card(TrumpShape.HEART, TrumpNumber.KING));
+        assertThat(player.isAbleToReceive()).isFalse();
     }
 }
