09a66312701e7fad8d18722b401e17cd820a5876
sh111-coder <ohk9134@naver.com>
2023-03-02 21:20:03 +0900

    refactor(ResultView): 딜러의 초기 카드 출력 기능 리팩토링

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -12,11 +12,11 @@ public class ResultView {
     }
 
     public static void printParticipantResult(String name, List<String> cardName) {
-        String result = ": " + String.join(JOIN_DELIMITER, cardName);
         if (!name.equals("딜러")) {
-            result = "카드" + result;
+            System.out.println(name + "카드: " + String.join(JOIN_DELIMITER, cardName));
+            return;
         }
-        System.out.println(name + result);
+        System.out.println(name + ": " + cardName.get(0));
     }
 
     public static void printParticipantFinalResult(String name, List<String> cardName, int totalValueSum) {
