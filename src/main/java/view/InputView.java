fd7eb24710befab9cd175af57168488a7a66a94a
shin-mallang <huipulci1@naver.com>
2023-03-02 22:21:19 +0900

    fix: 사람 입력받을 때, 구분이 안되는 오류 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    private static final String DELIMITER = ", ";
+    private static final String DELIMITER = ",";
 
     public static List<String> readParticipantsName() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
