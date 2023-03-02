748a39015190b243b16ff0ac2d4de8a5bd035a7f
aiaiaiai1 <cvc100326@naver.com>
2023-03-02 15:32:03 +0900

    test: 중복된 이름인 경우 예외 발생 테스트 추가

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -4,11 +4,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class PlayersTest {
 
     @Test
     @DisplayName("플레이어들 객체를 생성한다.")
     void createPlayersTest() {
-        Assertions.assertDoesNotThrow(()->new Players("pobi,jason"));
+        Assertions.assertDoesNotThrow(() -> new Players("pobi,jason"));
+    }
+
+    @Test
+    @DisplayName("중복된 이름인경우 예외가 발생한다.")
+    void validateDuplicatedNameTest() {
+        assertThatThrownBy(() -> new Players("pobi,pobi"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 이름입니다.");
     }
 }
