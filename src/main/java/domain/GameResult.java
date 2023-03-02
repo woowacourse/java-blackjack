645b64c358126026ee5216b6c1069843b638588f
xxeol2 <jhj01177@naver.com>
2023-03-02 16:57:10 +0900

    style: GameResult static import
    Co-authored-by: kiarakim <kiarakim413@gmail.com>
    Co-authored-by: hgo641 <hgo641@gmail.com>

--- src/main/java/domain/GameResult.java
+++ src/main/java/domain/GameResult.java
@@ -7,11 +7,11 @@ public enum GameResult {
 
     public static GameResult comparePlayerWithDealer(int playerScore, int dealerScore) {
         if (playerScore < dealerScore) {
-            return GameResult.LOSE;
+            return LOSE;
         }
         if (playerScore == dealerScore) {
-            return GameResult.PUSH;
+            return PUSH;
         }
-        return GameResult.WIN;
+        return WIN;
     }
 }
