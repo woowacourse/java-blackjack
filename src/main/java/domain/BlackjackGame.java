f71abd7db75025c3654c0378257a4ec956de2bbc
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-03-02 13:39:20 +0900

    feat : 모든 딜러와 참여자에게 카드를 뽑는 명령을 내린다

--- src/main/java/domain/BlackjackGame.java
+++ src/main/java/domain/BlackjackGame.java
@@ -1,17 +1,33 @@
 package domain;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BlackjackGame {
 
     private List<Player> players;
+    private Deck deck;
 
-    public BlackjackGame(List<String> playerNames) {
-//        playerNames.stream()
-//                .map(playerName -> new Player(playerName))
+    public BlackjackGame(List<String> playerNames, String dealerName, NumberGenerator numberGenerator) {
+        initPlayers(playerNames, dealerName);
+        initDeck(numberGenerator);
+    }
+
+    private void initDeck(NumberGenerator numberGenerator) {
+        deck = new Deck(numberGenerator);
+    }
+
+    private void initPlayers(List<String> playerNames, String dealerName) {
+        players = playerNames.stream()
+                .map(playerName -> new Player(playerName, new CardPool(Collections.emptyList())))
+                .collect(Collectors.toList());
+        players.add(new Dealer(dealerName, new CardPool(Collections.emptyList())));
     }
 
     public void letPlayersToHit() {
-
+        for (Player player : players) {
+            player.draw(deck.serve());
+        }
     }
 }
