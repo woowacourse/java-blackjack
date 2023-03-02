0629e7b0543b5dedfa0cbea09436a488b9a1f1fc
waterricecake <fruturum@nate.com>
2023-03-02 13:03:12 +0900

    feat(Player): 카드 한장을 뽑는다

--- src/main/java/blackjack/domain/model/Player.java
+++ src/main/java/blackjack/domain/model/Player.java
@@ -2,11 +2,21 @@ package blackjack.domain.model;
 
 import blackjack.domain.vo.Name;
 
+import java.util.List;
+
 public class Player {
     private final Name name;
     private final Cards cards;
     public Player(Name name, Cards cards){
         this.name = name;
         this.cards = cards;
+    }
+
+    public List<Card> getCards() {
+        return cards.getCards();
+    }
+
+    public void drawCard(Card card) {
+        cards.add(card);
     }
 }
--- src/test/java/blackjack/domain/model/PlayerTest.java
+++ src/test/java/blackjack/domain/model/PlayerTest.java
@@ -1,6 +1,9 @@
 package blackjack.domain.model;
 
+import blackjack.domain.vo.Letter;
 import blackjack.domain.vo.Name;
+import blackjack.domain.vo.Shape;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,4 +15,20 @@ public class PlayerTest {
     void constructorPlayer(){
         assertThatNoException().isThrownBy(()->new Player(new Name("test"),new Cards()));
     }
+
+    @Test
+    @DisplayName("카드 한장을 가져오는지 테스트")
+    void drawCardTest() {
+        // given
+        Player player = new Player(new Name("test"), new Cards());
+        Card card = new Card(Shape.CLOVER, Letter.ACE);
+
+        // when
+        player.drawCard(card);
+
+        // then
+        Assertions.assertThat(player.getCards())
+                .contains(card);
+    }
+
 }
