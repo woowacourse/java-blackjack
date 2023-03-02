b1b5000117ba4acff5eaa90b8a9c55ac7438c5f5
dev_kong <ahdjd5@gmail.com>
2023-03-02 17:28:27 +0900

    feat: 플레이어가 카드를 받는 기능

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,9 +1,15 @@
 package player;
 
+import card.Card;
+
 public class Player {
     private final Name name;
 
     public Player(Name name) {
         this.name = name;
+    }
+
+    public void hit(Card card) {
+
     }
 }
--- src/test/java/player/PlayerTest.java
+++ src/test/java/player/PlayerTest.java
@@ -4,6 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import card.Card;
+import card.CardNumber;
+import card.Pattern;
+
 class PlayerTest {
     @Test
     @DisplayName("플레이어를 생성한다.")
@@ -11,6 +15,16 @@ class PlayerTest {
         Name name = new Name("폴로");
 
         Assertions.assertThatCode(() -> new Player(name))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("플레이어는 카드를 받을 수 있다.")
+    void hit() {
+        Player player = new Player(new Name("폴로"));
+        Card card = new Card(CardNumber.ACE, Pattern.HEART);
+
+        Assertions.assertThatCode(() -> player.hit(card))
                 .doesNotThrowAnyException();
     }
 }
