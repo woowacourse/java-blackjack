fcb4248f28d53e2f7ff13efe6150502047055b89
swonny <wonny921@gmail.com>
2023-03-03 20:36:05 +0900

    feat: 이름이 한 글자 이상 열 글자 이하가 아니라면 예외처리

--- src/main/java/blackjack/domain/player/Name.java
+++ src/main/java/blackjack/domain/player/Name.java
@@ -1,10 +1,19 @@
 package blackjack.domain.player;
 
 public class Name {
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 10;
     private final String name;
 
     public Name(String name) {
+        validateLength(name);
         this.name = name;
+    }
+
+    private void validateLength(String name) {
+        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 한 글자 이상 다섯 글자 이하로 입력해주세요.");
+        }
     }
 
     public String getName() {
