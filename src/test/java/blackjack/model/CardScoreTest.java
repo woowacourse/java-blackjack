8f4e6cf714a3ec33f49952c77bcc12932c24800f
hum02 <ehdgma02@gmail.com>
2023-03-03 13:20:36 +0900

    test: 에이스가 1또는 11로 계산되는 기능 테스트 구현

---
+++ src/test/java/blackjack/model/CardScoreTest.java
@@ -0,0 +1,38 @@
+package blackjack.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class CardScoreTest {
+
+    @Test
+    @DisplayName("에이스가 포함된 점수계산에서 작은 점수를 가져온다.")
+    void smallScore() {
+        //given
+        List<CardNumber> numbers = List.of(CardNumber.ACE, CardNumber.FOUR, CardNumber.JACK);
+        CardScore score = new CardScore(numbers);
+
+        //when
+        int smallScore = score.smallScore();
+
+        //then
+        Assertions.assertThat(smallScore).isEqualTo(1+4+10);
+    }
+
+    @Test
+    @DisplayName("에이스가 포함된 점수계산에서 큰 점수를 가져온다.")
+    void bigScore() {
+        //given
+        List<CardNumber> numbers = List.of(CardNumber.ACE, CardNumber.FOUR, CardNumber.JACK);
+        CardScore score = new CardScore(numbers);
+
+        //when
+        int bigScore = score.bigScore();
+
+        //then
+        Assertions.assertThat(bigScore).isEqualTo(11+4+10);
+    }
+}
