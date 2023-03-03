f20f21e2a0c875b1008d251b4a2b71947a71c083
seokhwan-an <asok369258@naver.com>
2023-03-03 11:04:04 +0900

    refactor : 도메인과 뷰에 알맞게 enum 재정의

--- src/main/java/blackjack/domain/CardSuit.java
+++ src/main/java/blackjack/domain/CardSuit.java
@@ -1,19 +1,8 @@
 package blackjack.domain;
 
 public enum CardSuit {
-    SPADE("스페이드"),
-    DIAMOND("다이아몬드"),
-    HEART("하트"),
-    CLUB("클로버"),
-    ;
-
-    final String cardSuitName;
-
-    CardSuit(String cardSuitName) {
-        this.cardSuitName = cardSuitName;
-    }
-
-    public String getCardSuitName() {
-        return cardSuitName;
-    }
+    SPADE,
+    DIAMOND,
+    HEART,
+    CLUB,
 }
