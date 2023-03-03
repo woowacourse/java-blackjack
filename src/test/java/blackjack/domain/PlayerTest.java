5fb76f4046344f5e253c9ddfde9ab6dacd97064c
chaewon121 <cw121@naver.com>
2023-03-03 14:15:09 +0900

    feat: 플레이어 클래스 구현

---
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -0,0 +1,37 @@
+package blackjack.domain;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardNumber;
+import blackjack.domain.card.CardSymbol;
+import blackjack.domain.player.Name;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+    @Test
+    @DisplayName("Player의 total score가 21이 넘지 않으면 true를 반환한다.")
+    void total_score_under_21() {
+        // given & when
+        Player player = new Player(new Name("merry"));
+        player.playerCards.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
+        player.playerCards.updateCardScore(new Card(CardNumber.FIVE, CardSymbol.HEART));
+
+        // then
+        Assertions.assertThat(player.isUnderLimit()).isTrue();
+    }
+
+    @Test
+    @DisplayName("User의 total score가 21이 넘으면 false를 반환한다.")
+    void total_score_over_21() {
+        // given & when
+        Player player = new Player(new Name("메리"));
+        player.playerCards.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
+        player.playerCards.updateCardScore(new Card(CardNumber.FIVE, CardSymbol.HEART));
+        player.playerCards.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
+
+        // then
+        Assertions.assertThat(player.isUnderLimit()).isFalse();
+    }
+}
