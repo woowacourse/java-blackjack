8724df92e39dec324312e30615e716a14b6768f4
waterricecake <fruturum@nate.com>
2023-03-03 14:05:09 +0900

    refactor(Dealer): 딜러 기능 분리

---
+++ src/test/java/blackjack/domain/participant/DealerTest.java
@@ -0,0 +1,59 @@
+package blackjack.domain.participant;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Letter;
+import blackjack.domain.card.Shape;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class DealerTest {
+
+    @Test
+    @DisplayName("카드 한장을 가져오는 테스트")
+    void getOneCardTest(){
+        Dealer dealer = new Dealer();
+        Card card1 = new Card(Shape.CLOVER, Letter.ACE);
+        Card card2 = new Card(Shape.DIAMOND, Letter.JACK);
+        dealer.drawCard(card1);
+        dealer.drawCard(card2);
+
+        List<String> expected = dealer.getOneCard();
+        Assertions.assertThat(expected.size()).isEqualTo(1);
+        Assertions.assertThat(expected).contains(card1.getCardName());
+    }
+
+    @Test
+    @DisplayName("딜러의 카드의 합이 16이하 인지 테스트")
+    void canHitTest(){
+        //given
+        Dealer dealer = new Dealer();
+        Card card1 = new Card(Shape.CLOVER, Letter.SIX);
+        Card card2 = new Card(Shape.DIAMOND, Letter.JACK);
+
+        //when
+        dealer.drawCard(card1);
+        dealer.drawCard(card2);
+
+        //then
+        Assertions.assertThat(dealer.canHit()).isTrue();
+    }
+
+    @Test
+    @DisplayName("딜러의 카드의 합이 16초과 인지 테스트")
+    void cantHitTest(){
+        //given
+        Dealer dealer = new Dealer();
+        Card card1 = new Card(Shape.CLOVER, Letter.SEVEN);
+        Card card2 = new Card(Shape.DIAMOND, Letter.JACK);
+
+        //when
+        dealer.drawCard(card1);
+        dealer.drawCard(card2);
+
+        //then
+        Assertions.assertThat(dealer.canHit()).isFalse();
+    }
+}
