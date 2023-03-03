3eb14da34eb06096ced71998594aaddd136201e1
waterricecake <fruturum@nate.com>
2023-03-03 14:06:31 +0900

    feat(Player): 참가자 기능 분리

--- src/main/java/blackjack/domain/participant/Player.java
+++ src/main/java/blackjack/domain/participant/Player.java
@@ -1,38 +1,8 @@
 package blackjack.domain.participant;
 
-import blackjack.domain.card.Card;
-import blackjack.domain.card.Cards;
+public class Player extends Participant{
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class Player {
-    private final Name name;
-    private final Cards cards;
-    public Player(Name name, Cards cards){
-        this.name = name;
-        this.cards = cards;
-    }
-
-    public void drawCard(Card card) {
-        cards.add(card);
-    }
-
-    public int calculateTotal(){
-        return this.cards.calculateTotalScore();
-    }
-
-    public String getName() {
-        return name.get();
-    }
-
-    public List<String> getCards() {
-        return cards.getCards().stream()
-                .map(Card::getCardName)
-                .collect(Collectors.toList());
-    }
-
-    public List<String> getOneCard(){
-        return List.of(this.cards.getFirstCard().getCardName());
+    public Player(Name name) {
+        super(name);
     }
 }
