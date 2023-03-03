291c16e6579e5e0efaea443aff640d452e98de5f
Go-Jaecheol <gojaech@naver.com>
2023-03-03 16:50:40 +0900

    refactor(OutputView): OutputView 메서드 분리

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -12,6 +12,9 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
+    private static final String WIN_KEYWORD = "승";
+    private static final String TIE_KEYWORD = "무";
+    private static final String LOSE_KEYWORD = "패";
     private static final OutputView instance = new OutputView();
 
     public static OutputView getInstance() {
@@ -21,24 +24,32 @@ public class OutputView {
     private OutputView() {
     }
 
-    public void printInitialMessage(final List<String> names) {
+    public void printInitialState(Participants participants) {
+        printInitialMessage(participants.getPlayerNames());
+        printAllState(participants);
+    }
+
+    private void printInitialMessage(final List<String> names) {
         String dealerName = names.get(0);
         String playerNames = names.stream().skip(1).collect(Collectors.joining(", "));
         System.out.printf("%s와 %s에게 2장을 나누었습니다." + System.lineSeparator(), dealerName, playerNames);
     }
 
-    public void printAllState(final Participants participants) {
+    private void printAllState(final Participants participants) {
         Dealer dealer = participants.getDealer();
         List<Player> players = participants.getPlayers();
 
+        printInitialDealerState(dealer);
+        for (Player player : players) {
+            printSingleState(player);
+        }
+    }
+
+    private void printInitialDealerState(Dealer dealer) {
         Card dealerCard = dealer.getCards().get(0);
         String dealerCardDisplay = String.format("%s: %s%s", dealer.getName(), dealerCard.getValue(),
                 dealerCard.getShape());
         System.out.println(dealerCardDisplay);
-
-        for (Player player : players) {
-            printSingleState(player);
-        }
     }
 
     public void printSingleState(final Participant participant) {
@@ -66,41 +77,53 @@ public class OutputView {
         System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
     }
 
-    public void printFinalResult() {
+    public void printFinalResultMessage() {
         System.out.println("## 최종 승패");
     }
 
     public void printDealerResult(final Map<WinningStatus, Integer> dealerResult) {
         StringBuilder dealerDisplay = new StringBuilder();
-        int winCount = dealerResult.getOrDefault(WinningStatus.WIN, 0);
-        int tieCount = dealerResult.getOrDefault(WinningStatus.TIE, 0);
-        int loseCount = dealerResult.getOrDefault(WinningStatus.LOSE, 0);
-        if (winCount >= 1) {
-            dealerDisplay.append(winCount).append("승");
-        }
-        if (tieCount >= 1) {
-            dealerDisplay.append(tieCount).append("무");
-        }
-        if (loseCount >= 1) {
-            dealerDisplay.append(loseCount).append("패");
-        }
+        calculateWinCount(dealerResult, dealerDisplay);
+        calculateTieCount(dealerResult, dealerDisplay);
+        calculateLoseCount(dealerResult, dealerDisplay);
         System.out.printf("딜러: %s" + System.lineSeparator(), dealerDisplay);
+    }
+
+    private void calculateWinCount(Map<WinningStatus, Integer> dealerResult, StringBuilder dealerDisplay) {
+        int winCount = dealerResult.getOrDefault(WinningStatus.WIN, 0);
+        if (winCount >= 1) {
+            dealerDisplay.append(winCount).append(WIN_KEYWORD);
+        }
+    }
+
+    private void calculateTieCount(Map<WinningStatus, Integer> dealerResult, StringBuilder dealerDisplay) {
+        int tieCount = dealerResult.getOrDefault(WinningStatus.TIE, 0);
+        if (tieCount >= 1) {
+            dealerDisplay.append(tieCount).append(TIE_KEYWORD);
+        }
+    }
+
+    private void calculateLoseCount(Map<WinningStatus, Integer> dealerResult, StringBuilder dealerDisplay) {
+        int loseCount = dealerResult.getOrDefault(WinningStatus.LOSE, 0);
+        if (loseCount >= 1) {
+            dealerDisplay.append(loseCount).append(LOSE_KEYWORD);
+        }
     }
 
     public void printPlayerResult(final Map<Player, WinningStatus> playersResult) {
         for (Player player : playersResult.keySet()) {
-            String playerDisplay = "";
             WinningStatus winningStatus = playersResult.get(player);
-            if (winningStatus.equals(WinningStatus.WIN)) {
-                playerDisplay = "승";
-            }
-            if (winningStatus.equals(WinningStatus.TIE)) {
-                playerDisplay = "무";
-            }
-            if (winningStatus.equals(WinningStatus.LOSE)) {
-                playerDisplay = "패";
-            }
-            System.out.printf("%s: %s" + System.lineSeparator(), player.getName(), playerDisplay);
+            System.out.printf("%s: %s" + System.lineSeparator(), player.getName(), printWinningStatus(winningStatus));
         }
+    }
+
+    private String printWinningStatus(WinningStatus winningStatus) {
+        if (winningStatus.equals(WinningStatus.WIN)) {
+            return WIN_KEYWORD;
+        }
+        if (winningStatus.equals(WinningStatus.TIE)) {
+            return TIE_KEYWORD;
+        }
+        return LOSE_KEYWORD;
     }
 }
