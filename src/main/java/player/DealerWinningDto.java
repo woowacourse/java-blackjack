f619526c4800b08b76bcd9d4ef1a7848c0a178a6
dev_kong <ahdjd5@gmail.com>
2023-03-03 15:16:12 +0900

    feat: 딜러의 승패 결과 dto

---
+++ src/main/java/player/DealerWinningDto.java
@@ -0,0 +1,19 @@
+package player;
+
+import java.util.Map;
+
+import blackjackGame.Result;
+
+public class DealerWinningDto {
+    private final Name name;
+    private final Map<Result, Integer> winningMap;
+
+    private DealerWinningDto(Name name, Map<Result, Integer> winningMap) {
+        this.name = name;
+        this.winningMap = winningMap;
+    }
+
+    public static DealerWinningDto from(Dealer dealer) {
+        return new DealerWinningDto(dealer.getName(), dealer.getDealerResult());
+    }
+}
