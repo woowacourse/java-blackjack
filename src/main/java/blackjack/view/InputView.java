2ed5cfc46a7d797f32cc3f25063a632213fe3786
hectick <chyeon1223@cau.ac.kr>
2023-03-04 02:46:14 +0900

    refactor: 하드 코딩 제거

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -4,6 +4,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String READ_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String HIT_OR_STAND_FORMAT = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
+    private static final String YES = "y";
+    private static final String NO = "n";
+
     private static InputView instance;
     private final Scanner scanner = new Scanner(System.in);
 
@@ -19,21 +24,21 @@ public class InputView {
     }
 
     public List<String> readNames() {
-        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        System.out.println(READ_NAMES_MESSAGE);
         String[] names = scanner.nextLine().split(",");
         return List.of(names);
     }
 
     public boolean readHitOrStand(String name) {
-        System.out.println(name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+        System.out.printf(HIT_OR_STAND_FORMAT, name);
         String input;
         do {
             input = scanner.nextLine();
         } while (!isValidCommand(input));
-        return input.equals("y");
+        return input.equals(YES);
     }
 
     private boolean isValidCommand(String input) {
-        return "y".equals(input) || "n".equals(input);
+        return YES.equals(input) || NO.equals(input);
     }
 }
