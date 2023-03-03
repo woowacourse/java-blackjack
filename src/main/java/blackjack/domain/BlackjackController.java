f3b151f562433ef9eec6e1d5bbd76b3534cc8175
chaewon121 <cw121@naver.com>
2023-03-03 14:16:58 +0900

    feat: 과정 출력 및 컨트롤러 구현

---
+++ src/main/java/blackjack/domain/BlackjackController.java
@@ -0,0 +1,73 @@
+package blackjack.domain;
+
+import blackjack.domain.card.Cards;
+import blackjack.domain.player.Name;
+import blackjack.domain.player.Names;
+import blackjack.domain.view.InputView;
+import blackjack.domain.view.OutputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BlackjackController {
+    private Players players;
+    private Dealer dealer;
+
+    public void run() {
+        getReady();
+        giveFirstCards();
+        OutputView.printDealerCurrentCards(dealer);
+        OutputView.printPlayersCurrentCards(players);
+        giveAdditionalCards();
+        giveAdditionalCardToDealer();
+        OutputView.printResult(dealer, players);
+    }
+
+    private void giveAdditionalCards() {
+        for (Player player : players.getPlayers()) {
+            String answer = InputView.askAdditionalCard(player.getPlayerName());
+            giveAdditionalCard(answer, player);
+        }
+    }
+
+    private static void giveAdditionalCard(String answer, Player player) {
+        // TODO : y/n 와 총점이 21이 넘어가는 경우 출력
+        while (answer.equals("y") && player.isUnderLimit()) {
+            player.playerCards.updateCardScore(Cards.giveFirstCard());
+            OutputView.printPlayerCurrentCards(player);
+            System.out.println(player.getTotalScore());
+            answer = InputView.askAdditionalCard(player.getPlayerName());
+        }
+        OutputView.printPlayerCurrentCards(player);
+    }
+
+    public void giveAdditionalCardToDealer() {
+        while (dealer.isUnderLimit()) {
+            System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+            dealer.playerCards.updateCardScore(Cards.giveFirstCard());
+        }
+    }
+
+    private void getReady() {
+        List<Name> names = new Names(InputView.inputPeopleNames()).getNames();
+        List<Player> players = names.stream().map(Player::new)
+                .collect(Collectors.toUnmodifiableList());
+        this.players = new Players(players);
+        this.dealer = new Dealer();
+        OutputView.printReadyMessage(names);
+    }
+
+    public void giveFirstCards() {
+        Cards.init();
+        for (Player player : players.getPlayers()) {
+            giveFirstCard(player);
+        }
+        giveFirstCard(dealer);
+    }
+
+    private void giveFirstCard(User user) {
+        for (int i = 0; i < 2; i++) {
+            user.playerCards.updateCardScore(Cards.giveFirstCard());
+        }
+    }
+}
---
+++ src/main/java/blackjack/domain/view/OutputView.java
@@ -0,0 +1,52 @@
+package blackjack.domain.view;
+
+import blackjack.domain.Dealer;
+import blackjack.domain.Player;
+import blackjack.domain.Players;
+import blackjack.domain.User;
+import blackjack.domain.player.Name;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    private OutputView() {
+    }
+
+    public static void printReadyMessage(List<Name> names) {
+        String allName = names.stream().map(Name::getName).collect(Collectors.joining(", "));
+        System.out.println("딜러와 " + allName + "2장을 나누었습니다.");
+    }
+
+    public static void printPlayersCurrentCards(Players players) {
+        for (Player player : players.getPlayers()) {
+            printPlayerCurrentCards(player);
+        }
+    }
+
+    public static void printPlayerCurrentCards(Player player) {
+        String playerCards = getPlayerCards(player);
+        System.out.println(player.getPlayerName() + "카드: " + playerCards);
+    }
+
+    private static String getPlayerCards(User user) {
+        return user.getPlayerCards().stream()
+                .map(card -> card.getNumber().getNumber() + card.getSymbol().getSymbol())
+                .collect(Collectors.joining(", "));
+    }
+
+    public static void printDealerCurrentCards(Dealer dealer) {
+        String dealerCards = dealer.getPlayerCards().stream()
+                .map(card -> card.getNumber().getNumber() + card.getSymbol().getSymbol())
+                .limit(1)
+                .collect(Collectors.joining(""));
+        System.out.println("딜러 카드: " + dealerCards);
+    }
+
+    public static void printResult(Dealer dealer, Players players) {
+        System.out.println("딜러 카드: " + getPlayerCards(dealer) + " - 결과: " + dealer.getTotalScore());
+        for (Player player : players.getPlayers()) {
+            System.out.println(getPlayerCards(player) + " - 결과: " + player.getTotalScore());
+        }
+    }
+}
