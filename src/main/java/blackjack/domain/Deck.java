e017bbccc836672d54deb2aeeeb3175e5a50882b
hong-sile <gurwns9325@naver.com>
2023-03-03 22:51:10 +0900

    docs: 해결한 TODO 삭제

--- src/main/java/blackjack/domain/Deck.java
+++ src/main/java/blackjack/domain/Deck.java
@@ -7,7 +7,6 @@ public class Deck {
 
     private final Queue<Card> cards;
 
-    //TODO: basedeck을 유지하고, temp도 안쓰는 방법이 생각나면 리팩터링
     public Deck(DeckGenerator deckGenerator) {
         this.cards = new LinkedList<>(deckGenerator.generate());
     }
