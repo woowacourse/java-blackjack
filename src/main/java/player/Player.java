c374e75b2df48871ffb051f961369a336e805f2e
dev_kong <ahdjd5@gmail.com>
2023-03-03 15:00:24 +0900

    feat: 플레이어가 승패를 설정하는 기능

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -2,11 +2,13 @@ package player;
 
 import java.util.List;
 
+import blackjackGame.Result;
 import card.Card;
 
 public class Player {
     private final Name name;
     private final Hand hand = new Hand();
+    private Result result;
 
     public Player(Name name) {
         this.name = name;
@@ -30,5 +32,21 @@ public class Player {
 
     public Name getName() {
         return name;
+    }
+
+    public void win() {
+        result = Result.WIN;
+    }
+
+    public void lose() {
+        result = Result.LOSE;
+    }
+
+    public void tie() {
+        result = Result.TIE;
+    }
+
+    public Result getResult() {
+        return result;
     }
 }
