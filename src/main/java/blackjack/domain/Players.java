38a978de76eda048941327bb643f5723235c9797
ezzanzzan <cjsdmswjd821@naver.com>
2023-03-03 09:43:16 +0900

    feat: Players 생성
    - 플레이어의 수를 검증하는 기능 구현
    
    Co-authored-by: kong-hana01 <gksqlsl11@khu.ac.kr>

---
+++ src/main/java/blackjack/domain/Players.java
@@ -0,0 +1,19 @@
+package blackjack.domain;
+
+import java.util.List;
+
+public class Players {
+    private final List<Player> players;
+
+    public Players(List<Player> players) {
+        validatePlayersCount(players);
+        this.players = players;
+    }
+
+    private void validatePlayersCount(List<Player> players) {
+        if (players.isEmpty() || players.size() > 6) {
+            throw new IllegalArgumentException("게임을 진행하는 플레이어의 수는 1명에서 6명 사이여야 합니다.");
+        }
+    }
+
+}
---
+++ src/test/java/blackjack/domain/PlayersTest.java
@@ -0,0 +1,35 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PlayersTest {
+
+    @DisplayName("생성 테스트")
+    @Test
+    void Should_Create_When_NewPlayers() {
+        List<Player> players = new ArrayList<>();
+        assertDoesNotThrow(() -> new Players(players));
+    }
+
+    @DisplayName("플레이어 수는 1에서 6사이여야 한다.")
+    @ParameterizedTest()
+    @ValueSource(ints = {0, 7})
+    void Should_ThrowException_When_Between1And6(int size) {
+        List<Player> players = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            players.add(new Player(new Name("newName")));
+        }
+        assertThatThrownBy(() -> new Players(players))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("게임을 진행하는 플레이어의 수는 1명에서 6명 사이여야 합니다.");
+    }
+
+}
