61c7058401558c9ce3c8274982e04b1200e36208
waterricecake <fruturum@nate.com>
2023-03-03 14:04:17 +0900

    refactor(Participant): 게임 참가자와 딜러 역할 분리

---
+++ src/main/java/blackjack/domain/participant/Participant.java
@@ -0,0 +1,41 @@
+package blackjack.domain.participant;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Cards;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Participant {
+    private final Name name;
+    private final Cards cards;
+    public Participant(Name name){
+        this.name = name;
+        this.cards = new Cards();
+    }
+
+    public void drawCard(Card card) {
+        cards.add(card);
+    }
+
+    public int getTotalScore(){
+        return this.cards.calculateTotalScore();
+    }
+
+    public boolean isBust(){
+        return getTotalScore()>21;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+    public List<Card> getCards(){
+        return this.cards.getCards();
+    }
+
+    public List<String> getCardNames() {
+        return cards.getCards().stream()
+                .map(Card::getCardName)
+                .collect(Collectors.toList());
+    }
+}
