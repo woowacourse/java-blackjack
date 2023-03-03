cbec9e9691c601d0e13945847174868ed777d037
hum02 <ehdgma02@gmail.com>
2023-03-03 15:39:22 +0900

    test: 플레이어는 블랙잭 상태면 카드를 뽑을 수 없고, 버스트 상태가 될 때까지 카드를 뽑을 수 있는 테스트 구현

--- src/test/java/blackjack/model/participant/PlayerTest.java
+++ src/test/java/blackjack/model/participant/PlayerTest.java
@@ -6,11 +6,13 @@ import blackjack.model.card.Card;
 import blackjack.model.card.CardDeck;
 import blackjack.model.card.CardNumber;
 import blackjack.model.card.CardSuit;
+import blackjack.model.state.DrawState;
 import blackjack.model.state.InitialState;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,5 +35,48 @@ class PlayerTest {
 
         //then
         Assertions.assertThat(player.getCards()).containsExactly(card2, card1);
+    }
+
+    @Test
+    @DisplayName("draw상태의 플레이어는 버스트가 될 때까지 카드를 뽑을 수 있다.")
+    void player_can_draw_until_bust() {
+        //given
+        Card card1 = Card.of(CardSuit.CLUB, CardNumber.EIGHT);
+        Card card2 = Card.of(CardSuit.HEART, CardNumber.FIVE);
+        Card card3 = Card.of(CardSuit.HEART, CardNumber.NINE);
+        Card card4 = Card.of(CardSuit.HEART, CardNumber.EIGHT);
+
+        List<Card> cards = List.of(card4, card3);
+        Player player = new Player(new Name("도치"), new DrawState(new Hand(new ArrayList<>(List.of(card1, card2)))));
+        CardDeck cardDeck = new CardDeck(cards);
+
+        // when
+        player.play(cardDeck);
+
+        //then
+        Assertions.assertThatThrownBy(() -> player.play(cardDeck))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("버스트 상태에서는 카드를 더 뽑을 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("blackjack 상태의 플레이어는 카드를 뽑을 수 없다.")
+    void player_when_blackjack_can_not_draw() {
+        //given
+        Card card1 = Card.of(CardSuit.CLUB, CardNumber.JACK);
+        Card card2 = Card.of(CardSuit.HEART, CardNumber.ACE);
+        Card card3 = Card.of(CardSuit.HEART, CardNumber.KING);
+
+        List<Card> cards = List.of(card1, card2, card3);
+        Player player = new Player(new Name("도치"), new InitialState(new Hand()));
+        CardDeck cardDeck = new CardDeck(cards);
+
+        // when
+        player.play(cardDeck);
+
+        //then
+        Assertions.assertThatThrownBy(() -> player.play(cardDeck))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("블랙잭 상태에서는 카드를 더 뽑을 수 없습니다.");
     }
 }
