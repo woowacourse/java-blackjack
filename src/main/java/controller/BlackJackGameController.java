23bc4883d25d770ff3ab35b71e1e5bcf307f742a
yujamint <woawoa0513@gmail.com>
2023-03-03 14:54:57 +0900

    feat: 블랙잭 게임 초기세팅 결과 출력 기능 구현

--- src/main/java/controller/BlackJackGameController.java
+++ src/main/java/controller/BlackJackGameController.java
@@ -1,10 +1,13 @@
 package controller;
 
+import domain.card.Card;
 import domain.card.Cards;
 import domain.game.BlackJackGame;
 import domain.user.Dealer;
 import domain.user.Name;
 import domain.user.Player;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import view.InputView;
@@ -19,17 +22,29 @@ public class BlackJackGameController {
         this.outputView = outputView;
     }
 
+    public void run() {
+        BlackJackGame blackJackGame = setUp();
+    }
+
     private BlackJackGame setUp() {
         Cards cards = new Cards();
-
         Dealer dealer = new Dealer(cards.drawForFirstTurn());
+        List<Player> players = setUpPlayers(cards);
+        showSetUpResult(dealer, players);
+        return new BlackJackGame(players, dealer, cards);
+    }
 
-        List<String> usersName = readUsersName();
-        List<Player> players =  usersName.stream()
+    private List<Player> setUpPlayers(Cards cards) {
+        return readUsersName().stream()
                 .map(name -> new Player(new Name(name), cards.drawForFirstTurn()))
                 .collect(Collectors.toList());
+    }
 
-        return new BlackJackGame(players, dealer, cards);
+    private void showSetUpResult(Dealer dealer, List<Player> players) {
+        HashMap<String, List<Card>> setUpResult = new LinkedHashMap<>();
+        setUpResult.put("딜러", dealer.getCards().subList(0,1));
+        players.forEach(player -> setUpResult.put(player.getName(), player.getCards()));
+        outputView.printSetUpResult(setUpResult);
     }
 
     private List<String> readUsersName() {
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,37 @@
 package view;
 
+import domain.card.Card;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
 public class OutputView {
     public void printInputPlayerNameMessage() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+    }
+
+    public void printSetUpResult(HashMap<String, List<Card>> setUpResult) {
+        printSetUpCompleteMessage(setUpResult);
+        printUserCards(setUpResult);
+    }
+
+    private void printSetUpCompleteMessage(HashMap<String, List<Card>> setUpResult) {
+        String playerNames = String.join(", ",
+                new ArrayList<>(setUpResult.keySet()).subList(1, setUpResult.size()));
+
+        System.out.printf("딜러와 %s에게 2장을 나누었습니다.\n", playerNames);
+    }
+
+    private void printUserCards(HashMap<String, List<Card>> setUpResult) {
+        for (Entry<String, List<Card>> cardsByUser : setUpResult.entrySet()) {
+            String name = cardsByUser.getKey();
+            String cards = cardsByUser.getValue().stream()
+                    .map(Card::getSymbol)
+                    .collect(Collectors.joining(", "));
+
+            System.out.println(name + "카드 : " + cards);
+        }
     }
 }
