8e07811b7b08412093e092c29ece94375f31743a
yujamint <woawoa0513@gmail.com>
2023-03-03 11:13:17 +0900

    feat: 플레이어 이름 값 포장 클래스 구현

---
+++ src/main/java/domain/user/Name.java
@@ -0,0 +1,13 @@
+package domain.user;
+
+public class Name {
+    private final String name;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
