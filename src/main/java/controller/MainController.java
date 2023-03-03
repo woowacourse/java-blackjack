880e25af3be78fe54a11db61665a6c438277e1d7
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-03-03 14:50:13 +0900

    feat: MainController 구현

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,88 @@
+package controller;
+
+import domain.BlackJackGame;
+import domain.Deck;
+import java.util.List;
+import view.InputView;
+import view.OutputView;
+
+public class MainController {
+    private static final String DEALER_NAME = "딜러";
+
+    private List<String> names;
+    private BlackJackGame blackJackGame;
+
+    public void start() {
+        names = inputNames();
+        blackJackGame = generateBlackJackGame();
+
+        outputCards();
+        drawCardsPlayer();
+        drawCardsDealer();
+
+        outputCardResult();
+    }
+
+    private void outputCardResult() {
+        OutputView.printCardResult(
+                DEALER_NAME,
+                blackJackGame.getCards(DEALER_NAME),
+                blackJackGame.getScore(DEALER_NAME)
+        );
+
+        names.forEach(name ->
+                OutputView.printCardResult(name, blackJackGame.getCards(name), blackJackGame.getScore(name))
+        );
+    }
+
+    private void drawCardsDealer() {
+        while (blackJackGame.isDealerDraw()) {
+            OutputView.printDealerDrawCard();
+            blackJackGame.drawCard(DEALER_NAME);
+        }
+    }
+
+    private void drawCardsPlayer() {
+        for (String playerName : names) {
+            drawWhileYes(playerName);
+        }
+    }
+
+    private void drawWhileYes(final String playerName) {
+        boolean answer = true;
+        while (answer) {
+            OutputView.printOneMoreCard(playerName);
+            answer = isOneMore(playerName);
+        }
+    }
+
+    private boolean isOneMore(final String playerName) {
+        if (InputView.readAnswer()) {
+            blackJackGame.drawCard(playerName);
+            OutputView.printPlayerCard(playerName, blackJackGame.getCards(playerName));
+            return true;
+        }
+        OutputView.printPlayerCard(playerName, blackJackGame.getCards(playerName));
+        return false;
+    }
+
+    private void outputCards() {
+        OutputView.printDealerCard(blackJackGame.getCards(DEALER_NAME).get(0));
+        names.forEach(name ->
+                OutputView.printPlayerCard(name, blackJackGame.getCards(name))
+        );
+    }
+
+    private BlackJackGame generateBlackJackGame() {
+        Deck deck = new Deck();
+        deck.shuffleDeck();
+        BlackJackGame blackJackGame = new BlackJackGame(deck, names);
+        OutputView.printDistributeCard(names);
+        return blackJackGame;
+    }
+
+    private List<String> inputNames() {
+        OutputView.printInputNames();
+        return InputView.readNames();
+    }
+}
