e3280075a63c7e2382edc6e01b2de8a2bac53e67
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-03-03 15:03:58 +0900

    feat : 플레이어 이름 입력 시 메세지 출력 기능 생성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final String INPUT_PLAYER_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String INPUT_CARD_COMMAND_MESSAGE = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
 
     private final Scanner scanner;
@@ -17,6 +18,7 @@ public class InputView {
     }
 
     public List<String> inputParticipantsName() {
+        System.out.println(INPUT_PLAYER_MESSAGE);
         String line = scanner.nextLine();
         validator.validateNotBlank(line);
 
