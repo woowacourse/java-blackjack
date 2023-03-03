53f8ccccf245a52e8133e8d2bcb203dec94896e3
Cyma-s <ssol0319@naver.com>
2023-03-03 17:34:55 +0900

    refactor: 마지막 개행 추가 및 스타일 통일

--- src/test/java/blackjack/domain/NameTest.java
+++ src/test/java/blackjack/domain/NameTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.params.provider.EmptySource;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NameTest {
-
     @ParameterizedTest
     @EmptySource
     @DisplayName("이름이 공백일 경우 예외가 발생한다.")
