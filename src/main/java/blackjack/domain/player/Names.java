42faf257c9851f55e13f0ace15a1a7af6c2a50e6
swonny <wonny921@gmail.com>
2023-03-03 20:32:54 +0900

    feat: 이름 입력시 연속으로 입력된 쉼표는 예외 처리

--- src/main/java/blackjack/domain/player/Names.java
+++ src/main/java/blackjack/domain/player/Names.java
@@ -2,20 +2,31 @@ package blackjack.domain.player;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Names {
 
     private static final int MINIMUM_NAMES_RANGE = 1;
     private static final int MAXIMUM_NAMES_RANGE = 20;
+    private static final Pattern INPUT_NAMES_PATTERN = Pattern.compile("([a-zA-Z]{1,5})(,[a-zA-Z]{1,5})*");
 
     private final List<Name> names;
 
     public Names(String names) {
+        validateContinuousComma(names);
         List<Name> splitNames = splitNames(names);
         validateDuplicateNames(splitNames);
         validateRange(splitNames);
         this.names = splitNames;
+    }
+
+    private void validateContinuousComma(String names) {
+        Matcher matcher = INPUT_NAMES_PATTERN.matcher(names);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("입력된 플레이어들의 이름 형식이 올바르지 않습니다.");
+        }
     }
 
     private List<Name> splitNames(String names) {
