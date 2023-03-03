ee4a935c3e1754e9e9ccd3e379a5b10f635a780d
ezzanzzan <cjsdmswjd821@naver.com>
2023-03-03 15:57:32 +0900

    refactor: InputView 내 readTryComment -> readTryCommand 메서드명 변경
    Co-authored-by: kong-hana01 <gksqlsl11@khu.ac.kr>

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
         return List.of(input.split(","));
     }
 
-    public String readTryComment(String name) {
+    public String readTryCommand(String name) {
         System.out.println(name+"는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
         return scanner.nextLine();
     }
