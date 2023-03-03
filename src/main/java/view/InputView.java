85bad3ae6535679a196ca39965877a227317dbfe
splitube <voicesplit@daum.net>
2023-03-03 17:39:04 +0900

    feat: InputView 구현
    Co-authored-by: echo724 <echo.backend@gmail.com>

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,22 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String inputNeedMoreCard(String participantName) {
+        System.out.println(participantName + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+        return readLine();
+    }
+
+    public static String getParticipantNames() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        return readLine();
+    }
+
+    private static String readLine() {
+        return scanner.nextLine();
+    }
+}
