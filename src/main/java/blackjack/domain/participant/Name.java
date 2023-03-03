3735109d9af9c48a0591b6d3a31988561c3bdb53
waterricecake <fruturum@nate.com>
2023-03-03 18:38:44 +0900

    reformat(Name): 불필요한 코드 제거

--- src/main/java/blackjack/domain/participant/Name.java
+++ src/main/java/blackjack/domain/participant/Name.java
@@ -5,7 +5,6 @@ import java.util.regex.Pattern;
 public class Name {
 
     private static final Pattern NAME_FORMAT = Pattern.compile("[a-zA-Z]+");
-    private static final String DEALER_NAME = "딜러";
     private static final String NAME_FORMAT_ERROR_MESSAGE = "사람 이름은 영문자만 가능합니다.";
 
     private final String value;
