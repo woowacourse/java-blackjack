5f8247a8fd3b96d2900fbd73272fe95a41a0b7a1
greeng00se <thegreengoose7@gmail.com>
2023-03-03 13:13:25 +0900

    feat: 명령어(y/n)를 생성하는 기능 추가
    Co-authored-by: Combi153 <ssrno009@gmail.com>

---
+++ src/main/java/blackjack/controller/BlackjackCommand.java
@@ -0,0 +1,31 @@
+package blackjack.controller;
+
+import java.util.Arrays;
+
+public enum BlackjackCommand {
+    HIT("y"),
+    STAY("n");
+
+    static final String INVALID_COMMAND_MESSAGE = "Y 또는 N을 입력해야 합니다. 입력값:";
+
+    private final String command;
+
+    BlackjackCommand(final String command) {
+        this.command = command;
+    }
+
+    public static BlackjackCommand from(final String command) {
+        return Arrays.stream(values())
+                .filter(value -> value.isSameCommand(command))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_COMMAND_MESSAGE + command));
+    }
+
+    private boolean isSameCommand(final String command) {
+        return this.command.equals(command);
+    }
+
+    public boolean isHit() {
+        return this == HIT;
+    }
+}
