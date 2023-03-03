b5b15bf520c96eac08e3f8436d90e312d96c717d
yujamint <woawoa0513@gmail.com>
2023-03-03 13:19:14 +0900

    feat: 블랙잭 게임 Input View 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,32 @@
+package view;
+
+import domain.DrawCommand;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final int MAX_PLAYER_SIZE = 5;
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public List<String> readPlayersName() {
+        List<String> playersName = Arrays.asList(scanner.nextLine().split(","));
+        validatePlayersSize(playersName);
+
+        return playersName.stream()
+                .map(String::strip)
+                .collect(Collectors.toList());
+    }
+
+    public DrawCommand readDrawCommand() {
+        return DrawCommand.of(scanner.nextLine());
+    }
+
+    private void validatePlayersSize(List<String> players) {
+        if (players.size() > MAX_PLAYER_SIZE) {
+            throw new IllegalArgumentException("플레이어는 최대 5명입니다.");
+        }
+    }
+}
