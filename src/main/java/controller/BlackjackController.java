5e368ecfbddad37347938d10bb7ae8ef928d9e0e
Woojin Lee <dhkep03@gmail.com>
2023-03-03 15:03:36 +0900

    feat: 참가자의 이름을 입력받고, 딜러와 참가자들에게 카드를 2장씩 지급

---
+++ src/main/java/controller/BlackjackController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import domain.BlackjackGame;
+import domain.Deck;
+import ui.InputView;
+
+public class BlackjackController {
+    private final BlackjackGame blackjackGame;
+    private final Deck deck;
+
+    public BlackjackController() {
+        this.blackjackGame = new BlackjackGame(InputView.readPlayersName());
+        this.deck = new Deck();
+    }
+
+    public void run() {
+        this.blackjackGame.initGame(deck);
+    }
+}
---
+++ src/main/java/domain/BlackjackGame.java
@@ -0,0 +1,29 @@
+package domain;
+
+import domain.user.AbstractUser;
+import domain.user.Dealer;
+import domain.user.Players;
+import java.util.List;
+
+public class BlackjackGame {
+    private final int INITIAL_CARDS_COUNT = 2;
+
+    private final Dealer dealer;
+    private final Players players;
+
+    public BlackjackGame(List<String> nameValues) {
+        this.dealer = new Dealer();
+        this.players = new Players(nameValues);
+    }
+
+    public void initGame(Deck deck){
+        addInitialCards(dealer, deck);
+        players.getPlayers().forEach(player -> addInitialCards(player,deck));
+    }
+
+    private void addInitialCards(AbstractUser user, Deck deck) {
+        for (int i = 0; i < INITIAL_CARDS_COUNT; i++) {
+            user.addCard(deck.draw());
+        }
+    }
+}
