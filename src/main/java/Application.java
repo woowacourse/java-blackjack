6c768f37047e112c3ede990f4532a7cd7b59a653
yujamint <woawoa0513@gmail.com>
2023-03-03 16:57:21 +0900

    feat: 블랙잭 게임 Application 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import controller.BlackJackGameController;
+import view.InputView;
+import view.OutputView;
+
+public class Application {
+    public static void main(String[] args) {
+        InputView inputview = new InputView();
+        OutputView outputView = new OutputView();
+        BlackJackGameController blackJackGameController = new BlackJackGameController(inputview, outputView);
+        blackJackGameController.run();
+    }
+}
