2296a11c1914ae25fb256af1a317ce26cde8e479
kdkdhoho <hkim4410@naver.com>
2023-03-03 15:36:54 +0900

    feat(Number): Number의 상태 추가

--- src/main/java/blackjack/domain/card/Number.java
+++ src/main/java/blackjack/domain/card/Number.java
@@ -5,24 +5,26 @@ import java.util.Collections;
 import java.util.List;
 
 public enum Number {
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    J(11),
-    Q(12),
-    K(13);
+    ACE(1, "A"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    J(11, "J"),
+    Q(12, "Q"),
+    K(13, "K");
 
     private final int number;
+    private final String state;
 
-    Number(final int number) {
+    Number(final int number, final String state) {
         this.number = number;
+        this.state = state;
     }
 
     public static int convertNumberToScore(Number number) {
@@ -37,5 +39,9 @@ public enum Number {
         Collections.shuffle(numbers);
 
         return numbers.get(0);
+    }
+
+    public String getState() {
+        return state;
     }
 }
