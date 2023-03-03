556a7aff6485f3e03a8e44008f5e3dc3d91aef87
BackFoxx <backfoxxx@gmail.com>
2023-03-03 16:25:56 +0900

    feat : 참가자 이름 입력 예외처리 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,17 +3,41 @@ package view;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputView {
     private static final BufferedReader BUFFERED_READER = new BufferedReader(new InputStreamReader(System.in));
+    private static final Pattern INPUT_PARTICIPANT_NAMES_FORMAT = Pattern.compile("([가-힣|a-zA-Z0-9]+)(,[가-힣|a-zA-Z0-9]+)*");
 
     public static String inputParticipantNames() {
         try {
-            return BUFFERED_READER.readLine();
+            String inputParticipantNames = BUFFERED_READER.readLine();
+            validateInputParticipantNames(inputParticipantNames);
+            return inputParticipantNames;
         } catch (IOException ioException) {
             OutputView.println(ioException.getMessage());
             return inputParticipantNames();
+        }
+    }
+
+    private static void validateInputParticipantNames(String inputParticipantNames) {
+        validateNullOrBlank(inputParticipantNames);
+        validateInputParticipantNamesFormat(inputParticipantNames);
+    }
+
+    private static void validateNullOrBlank(String inputParticipantNames) {
+        if (Objects.isNull(inputParticipantNames) || inputParticipantNames.isBlank()) {
+            throw new IllegalArgumentException("Null 또는 빈 문자열을 입력할 수 없습니다.");
+        }
+    }
+
+    private static void validateInputParticipantNamesFormat(String inputParticipantNames) {
+        Matcher matcher = INPUT_PARTICIPANT_NAMES_FORMAT.matcher(inputParticipantNames);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("참가자 이름 입력 형식이 잘못 되었습니다. 다시 입력해주세요.");
         }
     }
 
