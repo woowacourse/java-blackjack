99a398a989007ba195d30a92b951f3643d2a27d7
chaewon121 <cw121@naver.com>
2023-03-03 15:09:18 +0900

    feat: 결과 계산 구현

---
+++ src/main/java/blackjack/domain/result/Result.java
@@ -0,0 +1,40 @@
+package blackjack.domain.result;
+
+public enum Result {
+    WIN("승"),
+    TIE("무"),
+    LOSE("패");
+
+    private final String playerResult;
+
+    Result(String playerResult) {
+        this.playerResult = playerResult;
+    }
+
+    public static Result calculateResult(int playerScore, int dealerScore) {
+        if(playerScore>21){
+            playerScore =0;
+        }
+        if(dealerScore>21){
+            dealerScore =0;
+        }
+        if (playerScore < dealerScore) {
+            return LOSE;
+        }
+        if (playerScore > dealerScore) {
+            return WIN;
+        }
+        return TIE;
+    }
+
+    public Result ofOppositeResult() {
+        if (this.equals(WIN)) {
+            return LOSE;
+        }
+        if (this.equals(LOSE)) {
+            return WIN;
+        }
+        return TIE;
+    }
+
+}
