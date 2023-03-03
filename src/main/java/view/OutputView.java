4825ece4e23169015d02c12c4ddf0e232fde4d12
yujamint <woawoa0513@gmail.com>
2023-03-03 16:57:12 +0900

    feat: 블랙잭 게임 OutputView 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,9 +1,11 @@
 package view;
 
 import domain.card.Card;
+import domain.user.User;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
@@ -17,11 +19,23 @@ public class OutputView {
         printUserCards(setUpResult);
     }
 
+    public void printAskOneMoreCardMessage(String name) {
+        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n", name);
+    }
+
+    public void printPlayerDrawResult(String name, List<Card> cards) {
+        String cardSymbols = cards.stream()
+                .map(Card::getSymbol)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(name + "카드 : " + cardSymbols);
+    }
+
     private void printSetUpCompleteMessage(HashMap<String, List<Card>> setUpResult) {
         String playerNames = String.join(", ",
                 new ArrayList<>(setUpResult.keySet()).subList(1, setUpResult.size()));
 
-        System.out.printf("딜러와 %s에게 2장을 나누었습니다.\n", playerNames);
+        System.out.printf("\n딜러와 %s에게 2장을 나누었습니다.\n", playerNames);
     }
 
     private void printUserCards(HashMap<String, List<Card>> setUpResult) {
@@ -32,6 +46,36 @@ public class OutputView {
                     .collect(Collectors.joining(", "));
 
             System.out.println(name + "카드 : " + cards);
+        }
+        System.out.println();
+    }
+
+    public void printDealerDrawResult(int dealerDrawCount) {
+        System.out.printf("\n딜러는 16이하라 %d장의 카드를 더 받았습니다.\n\n", dealerDrawCount);
+    }
+
+    public void printUsersCardResult(Map<User, List<Card>> userResult) {
+        for (Entry<User, List<Card>> cardsByUser : userResult.entrySet()) {
+            String name = cardsByUser.getKey().getName();
+            String cards = cardsByUser.getValue().stream()
+                    .map(Card::getSymbol)
+                    .collect(Collectors.joining(", "));
+            int score = cardsByUser.getKey().getScore();
+
+            System.out.println(name + "카드 : " + cards + " - 결과: " + score);
+        }
+        System.out.println();
+    }
+
+    public void printFinalResult(Map<Boolean, Integer> dealerResult, Map<String, Boolean> userResult) {
+        System.out.println("## 최종 승패");
+        System.out.printf("딜러: %d승 %d패\n", dealerResult.get(true), dealerResult.get(false));
+        for (Entry<String, Boolean> userFinalResult : userResult.entrySet()) {
+            if (userFinalResult.getValue()) {
+                System.out.println(userFinalResult.getKey() + ": 승");
+                continue;
+            }
+            System.out.println(userFinalResult.getKey() + ": 패");
         }
     }
 }
