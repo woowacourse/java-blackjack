e03eab30360126a4eb63d5d94dc2c518e90568ac
kim0914 <kosa0914@naver.com>
2023-03-03 16:38:09 +0900

    feat(InputView): 사용자 입력 메서드 추가

---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,55 @@
+package blackjack.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    private static final String NAME_INPUT_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String CARD_INPUT_MESSAGE = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String YES_INPUT_MESSAGE = "y";
+    private static final String NO_INPUT_MESSAGE = "n";
+
+    private static final String DELIMITER = ",";
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public List<String> readNames() {
+        printMessage(NAME_INPUT_MESSAGE);
+        String names = input();
+        return Arrays.stream(names.split(DELIMITER))
+                .collect(Collectors.toList());
+    }
+
+    public String readReceiveIntention(String name) {
+        printMessage(name + CARD_INPUT_MESSAGE);
+        try {
+            String intention = input().toLowerCase();
+            validateIntention(intention);
+            return intention;
+        } catch (IllegalArgumentException e) {
+            printMessage(e.getMessage());
+            return readReceiveIntention(name);
+        }
+    }
+
+    private void validateIntention(String intention) {
+        if (!intention.equals(YES_INPUT_MESSAGE) && !intention.equals(NO_INPUT_MESSAGE)) {
+            throw new IllegalArgumentException("y 또는 n만 입력 가능합니다.");
+        }
+    }
+
+    private String input() {
+        String inputValue = scanner.nextLine();
+        if (inputValue.isBlank()) {
+            throw new IllegalArgumentException("값을 입력해주세요.");
+        }
+        return inputValue;
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
