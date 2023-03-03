d91a0d7e04818325f96a446c09bfabad3717a677
dev_kong <ahdjd5@gmail.com>
2023-03-03 15:52:26 +0900

    feat: 첫번째 드로우 결과를 출력하는 기능

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,11 @@
 package view;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import card.Card;
+import player.DealerFirstOpenDto;
+import player.PlayerOpenDto;
 
 public class OutputView {
 
@@ -9,4 +14,19 @@ public class OutputView {
         System.out.printf("딜러와 %s에게 2장을 나누었습니다.", joinedNames);
     }
 
+    public void printFirstOpenCards(DealerFirstOpenDto dealerFirstOpen, List<PlayerOpenDto> playersCards) {
+        System.out.println(dealerFirstOpen.getName().getValue() + ": " + dealerFirstOpen.getCard());
+        playersCards.forEach(this::printPlayerCard);
+    }
+
+    private void printPlayerCard(PlayerOpenDto playerOpenDto) {
+        System.out.println(
+                playerOpenDto.getName().getValue() + "카드: " + parseCards(playerOpenDto));
+    }
+
+    private String parseCards(PlayerOpenDto playerOpenDto) {
+        return playerOpenDto.getCards().stream()
+                .map(Card::toString)
+                .collect(Collectors.joining(", "));
+    }
 }
