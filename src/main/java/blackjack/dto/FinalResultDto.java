efbd27b45e1192f148642bd05f08f71c71f5e0ff
be-student <presentsong@naver.com>
2023-03-03 16:55:48 +0900

    chore: 사용하지 않는 메서드 제거

--- src/main/java/blackjack/dto/FinalResultDto.java
+++ src/main/java/blackjack/dto/FinalResultDto.java
@@ -1,12 +1,12 @@
 package blackjack.dto;
 
 import blackjack.domain.ResultType;
-
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class FinalResultDto {
+
     private final Map<String, ResultType> playersResult;
     private final Map<ResultType, Long> dealerResult;
 
@@ -29,28 +29,6 @@ public class FinalResultDto {
                 .collect(Collectors.toMap(Map.Entry::getKey, (var result) -> result.getValue()
                         .getPlayerResultType(), (x, y) -> y, LinkedHashMap::new));
     }
-
-    private int calculateDealerWinCount(final Map<String, ResultType> playerNameToResultType) {
-        return (int) playerNameToResultType.values()
-                .stream()
-                .filter(resultType -> resultType == ResultType.WIN)
-                .count();
-    }
-
-    private int calculateDealerLoseCount(final Map<String, ResultType> playerNameToResultType) {
-        return (int) playerNameToResultType.values()
-                .stream()
-                .filter(resultType -> resultType == ResultType.LOSE)
-                .count();
-    }
-
-    private int calculateDealerTieCount(final Map<String, ResultType> playerNameToResultType) {
-        return (int) playerNameToResultType.values()
-                .stream()
-                .filter(resultType -> resultType == ResultType.TIE)
-                .count();
-    }
-
 
     public Map<String, ResultType> getPlayersResult() {
         return playersResult;
