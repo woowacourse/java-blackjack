404df49582f4c6858dff1605c8725596a0547707
hum02 <ehdgma02@gmail.com>
2023-03-03 13:21:10 +0900

    feat: 카드 숫자 계산 기능 구현

---
+++ src/main/java/blackjack/model/CardScore.java
@@ -0,0 +1,27 @@
+package blackjack.model;
+
+import java.util.List;
+
+public class CardScore {
+    private final List<CardNumber> numbers;
+
+    public CardScore(List<CardNumber> numbers) {
+        this.numbers = numbers;
+    }
+
+    public int smallScore() {
+        int score = 0;
+        for (CardNumber number : numbers) {
+            score += number.getSmallScore();
+        }
+        return score;
+    }
+
+    public int bigScore() {
+        int score = 0;
+        for (CardNumber number : numbers) {
+            score += number.getBigScore();
+        }
+        return score;
+    }
+}
