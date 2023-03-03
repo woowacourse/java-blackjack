ac6e9cbb55a108ffad2321c80b8acdfff45c6a6d
sh111-coder <ohk9134@naver.com>
2023-03-03 15:02:59 +0900

    study: 제네릭 미니 미션4 구현

--- src/test/java/techcourse/jcf/mission/ListStudy.java
+++ src/test/java/techcourse/jcf/mission/ListStudy.java
@@ -46,6 +46,13 @@ public class ListStudy {
 
         System.out.println("double = " + doubleTotal);
         System.out.println("int = " + intTotal);
+
+        // 미션4
+        final SimpleList<Double> doubleValues2 = new SimpleArrayList<Double>(-0.1, 0.5, 0.7);
+        final SimpleList<Integer> intValues2 = new SimpleArrayList<Integer>(-10, 1, 2);
+
+        final SimpleList<Double> filteredDoubleValues = SimpleList.filterNegative(doubleValues2);
+        final SimpleList<Integer> filteredIntValues = SimpleList.filterNegative(intValues2);
     }
 }
 
