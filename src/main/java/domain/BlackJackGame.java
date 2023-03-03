fbd5088e2eb7592896b2906448b73ffe9fc3fd78
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-03-03 14:19:39 +0900

    feat: 게임 결과 결정 기능 구현

--- src/main/java/domain/BlackJackGame.java
+++ src/main/java/domain/BlackJackGame.java
@@ -1,11 +1,14 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BlackJackGame {
-    private final Deck deck;
+    private static final int BLACK_JACK_NUMBER = 21;
 
+    private final Deck deck;
     private final List<Player> players;
 
     public BlackJackGame(Deck deck, List<String> playerNames) {
@@ -55,5 +58,47 @@ public class BlackJackGame {
 
     public int getScore(String playerName) {
         return findPlayer(playerName).getScore();
+    }
+
+    public Map<String, Outcome> decidePlayersOutcome() {
+        Map<String, Outcome> result = new HashMap<>();
+        final int dealerScore = findDealer().getScore();
+        final List<Player> players = this.players.subList(1, this.players.size());
+
+        players.forEach((player ->
+                result.put(player.getName(), decideOutcome(dealerScore, player.getScore()))
+        ));
+
+        return result;
+    }
+
+    private Outcome decideOutcome(final int dealerScore, final int playerScore) {
+        if (isPlayerWin(dealerScore, playerScore)) {
+            return Outcome.WIN;
+        }
+        if (isPlayerDraw(dealerScore, playerScore)) {
+            return Outcome.DRAW;
+        }
+        return Outcome.LOSE;
+    }
+
+    private boolean isPlayerWin(int dealerScore, int playerScore) {
+        if (dealerScore > BLACK_JACK_NUMBER && playerScore <= BLACK_JACK_NUMBER) {
+            return true;
+        }
+        if (playerScore <= BLACK_JACK_NUMBER && playerScore > dealerScore) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isPlayerDraw(int dealerScore, int playerScore) {
+        if (dealerScore > BLACK_JACK_NUMBER && playerScore > BLACK_JACK_NUMBER) {
+            return true;
+        }
+        if (dealerScore == playerScore) {
+            return true;
+        }
+        return false;
     }
 }
