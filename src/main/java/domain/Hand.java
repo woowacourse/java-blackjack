c0c85a2eee2005394de87a695c8d7264472ffe8e
ReO <yt930310@gmail.com>
2023-03-04 14:42:15 +0900

    fix(Hand): 잘못된 로직 수정

--- src/main/java/domain/Hand.java
+++ src/main/java/domain/Hand.java
@@ -4,35 +4,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hand {
-    private static final int INIT_ACE_COUNT = 0;
     private static final int HIGH_ACE_VALUE = 11;
     private static final int LOW_ACE_VALUE = 1;
     private static final int BUST_BOUNDARY_VALUE = 21;
     private final List<Card> hand;
-    private int aceCount;
 
     public Hand(List<Card> cards) {
         this.hand = cards;
-        this.aceCount = INIT_ACE_COUNT;
+
     }
 
     public void addCard(Card card) {
-        if (card.getValue() == HIGH_ACE_VALUE) {
-            aceCount += 1;
-        }
         hand.add(card);
     }
 
     public int calculateValue() {
         int value = 0;
+        int aceCount = 0;
 
+        aceCount = countAce(aceCount);
+
+        value = sumValue(value);
+
+        value = chooseAceValue(value, aceCount);
+        return value;
+    }
+
+    private int countAce(int aceCount) {
         for (Card card : hand) {
-            value += card.getValue();
+            aceCount = addAceCount(aceCount, card);
         }
+        return aceCount;
+    }
 
-        if (value > BUST_BOUNDARY_VALUE && aceCount > INIT_ACE_COUNT) {
+    private int addAceCount(int aceCount, Card card) {
+        if (card.getValue() == HIGH_ACE_VALUE) {
+            aceCount += 1;
+        }
+        return aceCount;
+    }
+
+    private int chooseAceValue(int value, int aceCount) {
+        while (value > BUST_BOUNDARY_VALUE && aceCount > 0) {
             value -= HIGH_ACE_VALUE - LOW_ACE_VALUE;
             aceCount -= 1;
+        }
+        return value;
+    }
+
+    private int sumValue(int value) {
+        for (Card card : hand) {
+            value += card.getValue();
         }
         return value;
     }
