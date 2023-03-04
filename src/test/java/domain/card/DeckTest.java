94ead7310410389675c746f6bebcb5bfeb2704d6
sh111-coder <ohk9134@naver.com>
2023-03-04 20:11:24 +0900

    test(DeckTest): 카드 드로우 기능 테스트

--- src/test/java/domain/card/DeckTest.java
+++ src/test/java/domain/card/DeckTest.java
@@ -1,25 +1,18 @@
 package domain.card;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class DeckTest {
 
     @Test
-    @DisplayName("초기 덱의 카드 수는 52장이어야한다.")
-    void checkDeckSize() {
+    @DisplayName("덱의 맨 위에 있는 카드를 뽑아서 반환한다.")
+    void getCardTopOfDeck() {
+        DeckGenerator deckGenerator = new DeckGenerator();
+        Deck deck = deckGenerator.generate();
+        Card topOfDeckCard = deck.getCards().get(0);
 
-        assertThat(Deck.cards.size()).isEqualTo(52);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"A하트, 1", "3하트, 3", "5다이아몬드, 5", "K스페이드, 10", "Q클로버, 10", "J하트, 10"})
-    @DisplayName("각 카드에 해당하는 숫자를 정상적으로 추출한다.")
-    void shouldSuccessExtractCardNumber(String card, int cardNumber) {
-        assertThat(Deck.extractCardNumber(card)).isEqualTo(cardNumber);
+        Assertions.assertThat(deck.drawCard()).isEqualTo(topOfDeckCard);
     }
 }
