f3c59b4d8460acf6dd8dff9804e8e3202428e1c0
apptie <appti@naver.com>
2023-03-05 01:05:51 +0900

    refactor: Application에서 수행하던 도메인을 생성하는 책임을 별도의 클래스로 분리한다

--- src/main/java/GameApplication.java
+++ src/main/java/GameApplication.java
@@ -1,16 +1,11 @@
 import controller.GameController;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import view.InputView;
-import view.OutputView;
 
 public class GameApplication {
-    public static void main(String[] args) {
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        final InputView inputView = new InputView(bufferedReader);
-        final OutputView outputView = new OutputView();
 
-        final GameController gamePrepareController = new GameController(inputView, outputView);
+    public static void main(String[] args) {
+        final GameApplicationConfig config = new GameApplicationConfig();
+        final GameController gamePrepareController = config.gameController();
+
         gamePrepareController.start();
     }
 }
