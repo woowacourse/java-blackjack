0eb572817275eca724f0cb8d2dc35551d44eeba3
Wugawuga <dnr3655@naver.com>
2023-03-04 15:32:06 +0900

    test: playerTest 케이스 추가

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,6 +1,5 @@
 package domain;
 
-import static domain.CardBoxFixture.카드박스;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
@@ -8,42 +7,39 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class PlayerTest {
 
-//    @ParameterizedTest
-//    @CsvSource(value = {
-//            "Y,0,false",
-//            "Y,1,true",
-//            "N,0,false",
-//    })
-//    void Y_입력시_카드를_추가하고_N_입력시_카드를_추가하지_않는다(String input, int randomCardIndex, boolean expected) {
-//        Name name = new Name("hamad");
-//        List<Card> cardsByCardBox = new ArrayList<>();
-//        cardsByCardBox.add(new Card("A하트", 11));
-//        cardsByCardBox.add(new Card("3하트", 3));
-//        Cards cards = new Cards(cardsByCardBox);
-//
-//        Player player = new Player(name, cards);
-//
-//        assertThat(player.selectToPickOtherCard(input, 카드박스, () -> randomCardIndex)).isEqualTo(expected);
-//    }
-
     @Test
     void 본인의_카드뭉치의_총합을_반환한다() {
-        Name name = new Name("hamad");
-        List<Card> cardsByCardBox = new ArrayList<>();
-        cardsByCardBox.add(new Card("A하트", 11));
-        cardsByCardBox.add(new Card("3하트", 3));
-        Cards cards = new Cards(cardsByCardBox);
+        Name name = getName();
+        Cards cards = getCards();
 
         Player player = new Player(name, cards);
 
         assertThat(player.sumOfPlayerCards()).isEqualTo(cards.sumOfCards());
     }
 
+    @Test
+    void 카드박스에서_카드를_뽑을_수_있다() {
+        Name name = getName();
+        Cards cards = getCards();
+
+        Player player = new Player(name, cards);
+
+        assertThat(player.selectToPickOtherCard(new CardBox(), () -> 0)).isTrue();
+    }
+
+    private static Cards getCards() {
+        List<Card> cardsByCardBox = new ArrayList<>();
+        cardsByCardBox.add(new Card("A하트", 11));
+        cardsByCardBox.add(new Card("3하트", 3));
+        return new Cards(cardsByCardBox);
+    }
+
+    private static Name getName() {
+        return new Name("hamad");
+    }
 }
