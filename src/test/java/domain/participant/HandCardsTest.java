55f1a3e2bef40f1607bd8eabce1a48fbf1795c1d
sh111-coder <ohk9134@naver.com>
2023-03-04 20:55:57 +0900

    test(HandCardsTest): 손패의 카드 합을 반환하는 기능 테스트

--- src/test/java/domain/participant/HandCardsTest.java
+++ src/test/java/domain/participant/HandCardsTest.java
@@ -7,24 +7,36 @@ import domain.card.Card;
 import domain.card.Denomination;
 import domain.card.Suit;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class HandCardsTest {
 
-    @Test
-    @DisplayName("카드를 받아서 손패에 추가한다.")
-    void addHandCards() {
+    private HandCards handCards;
+
+    @BeforeEach
+    void init() {
         List<Card> cards = new ArrayList<>();
         cards.add(new Card(Suit.CLOVER, Denomination.JACK));
         cards.add(new Card(Suit.CLOVER, Denomination.TWO));
-        HandCards handCards = new HandCards(cards);
+        this.handCards = new HandCards(cards);
+    }
 
+    @Test
+    @DisplayName("카드를 받아서 손패에 추가한다.")
+    void addHandCards() {
         Card cardToAdd = new Card(Suit.SPADE, Denomination.ACE);
         handCards.addCard(cardToAdd);
         List<Card> afterAddCards = handCards.getCards();
 
         Assertions.assertThat(afterAddCards.size()).isEqualTo(3);
         Assertions.assertThat(afterAddCards).contains(cardToAdd);
+    }
+
+    @Test
+    @DisplayName("손패의 카드 합을 반환한다.")
+    void getSumCardValue() {
+        Assertions.assertThat(handCards.getSumCardValue()).isEqualTo(12);
     }
 }
