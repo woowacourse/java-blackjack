9b78f15f3b43443f6bc462eb433ec3940ea34695
apptie <appti@naver.com>
2023-03-04 12:03:01 +0900

    feat: 카드를 랜덤하게 선택하는 기능을 추가한다

---
+++ src/main/java/domain/CardSelector.java
@@ -0,0 +1,6 @@
+package domain;
+
+public interface CardSelector {
+
+    int selectCardOrder(final int cardSize);
+}
---
+++ src/test/java/domain/card/RandomUniqueCardSelectorTest.java
@@ -0,0 +1,47 @@
+package domain.card;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.CardSelector;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+class RandomUniqueCardSelectorTest {
+
+    @RepeatedTest(10)
+    @DisplayName("selectCardOrder()는 카드의 총 개수를 건네주면 카드의 순서를 반환한다")
+    void selectCardOrder_givenCardSize_thenReturnCardOrder() {
+        // given
+        final CardSelector cardSelector = new RandomUniqueCardSelector();
+
+        // when
+        int actual = cardSelector.selectCardOrder(52);
+
+        // then
+        assertThat(actual)
+                .isLessThan(52)
+                .isGreaterThanOrEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("selectCardOrder()는 카드의 총 개수를 건네주면 고유한 카드의 순서를 반환한다")
+    void selectCardOrder_givenCardSize_thenReturnUniqueCardOrder() {
+        // given
+        final CardSelector cardSelector = new RandomUniqueCardSelector();
+        final Set<Integer> cardOrder = new HashSet<>();
+        int count = 0;
+
+        // when
+        while (count++ < 52) {
+            int randomCardOrder = cardSelector.selectCardOrder(52);
+            cardOrder.add(randomCardOrder);
+        }
+
+        // then
+        assertThat(cardOrder.size())
+                .isSameAs(52);
+    }
+}
