d6e533aaffa6ddcc5d9dae8165a9443ea731fbca
splitube <voicesplit@daum.net>
2023-03-04 21:49:19 +0900

    refactor: 테스트 로직 수정

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import domain.user.Player;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,23 +9,17 @@ import org.junit.jupiter.api.Test;
 class PlayerTest {
 
     @Test
-    @DisplayName("딜러는 참가자에게 카드를 분배한다.")
+    @DisplayName("참가자가 받은 카드를 조회한다.")
     void dealCardToPlayerTest() {
         Player player = new Player("echo");
         player.dealt(new Card(Denomination.ACE, Suit.SPADE));
-        Assertions.assertThat(player)
-            .extracting("hand")
-            .asList()
-            .hasSize(1);
-        Assertions.assertThat(player)
-            .extracting("hand")
-            .asList()
-            .first()
-            .isEqualTo(new Card(Denomination.ACE, Suit.SPADE));
+        List<Card> playerHand = player.showHand();
+        Assertions.assertThat(playerHand).hasSize(1);
+        Assertions.assertThat(playerHand).containsExactly(new Card(Denomination.ACE, Suit.SPADE));
     }
 
     @Test
-    @DisplayName("처음에 카드를 지급받지 않은 경우 카드 조회시 오류를 던진다.")
+    @DisplayName("카드가 없는 플레이어의 카드를 조회할 경우 오류를 던진다.")
     void getReadyCardsTestFailed() {
         Player player = new Player("echo");
         Assertions.assertThatThrownBy(player::faceUpInitialHand)
@@ -32,17 +27,7 @@ class PlayerTest {
     }
 
     @Test
-    @DisplayName("처음에 지급받은 카드를 반환한다.")
-    void getReadyCardsTestSuccess() {
-        Player player = new Player("echo");
-        player.dealt(new Card(Denomination.ACE, Suit.SPADE));
-        player.dealt(new Card(Denomination.THREE, Suit.HEART));
-        Assertions.assertThat(player.faceUpInitialHand())
-            .containsExactly(new Card(Denomination.ACE, Suit.SPADE), new Card(Denomination.THREE, Suit.HEART));
-    }
-
-    @Test
-    @DisplayName("현재의 점수를 반환한다.")
+    @DisplayName("현재의 점수를 계산하여 반환한다.")
     void calculateScore() {
         Player player = new Player("echo");
         player.dealt(new Card(Denomination.ACE, Suit.SPADE));
