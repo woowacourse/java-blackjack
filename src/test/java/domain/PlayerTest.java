0365644c76bed35291ced143b793818600b02b02
splitube <voicesplit@daum.net>
2023-03-04 21:12:38 +0900

    refactor: 클래스 네이밍 수정

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -5,18 +5,18 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class ParticipantTest {
+class PlayerTest {
 
     @Test
     @DisplayName("딜러는 참가자에게 카드를 분배한다.")
     void dealCardToPlayerTest() {
-        Player participant = new Player("echo");
-        participant.dealt(new Card(Denomination.ACE, Suit.SPADE));
-        Assertions.assertThat(participant)
+        Player player = new Player("echo");
+        player.dealt(new Card(Denomination.ACE, Suit.SPADE));
+        Assertions.assertThat(player)
             .extracting("hand")
             .asList()
             .hasSize(1);
-        Assertions.assertThat(participant)
+        Assertions.assertThat(player)
             .extracting("hand")
             .asList()
             .first()
@@ -26,27 +26,27 @@ class ParticipantTest {
     @Test
     @DisplayName("처음에 카드를 지급받지 않은 경우 카드 조회시 오류를 던진다.")
     void getReadyCardsTestFailed() {
-        Player participant = new Player("echo");
-        Assertions.assertThatThrownBy(participant::faceUpInitialHand)
+        Player player = new Player("echo");
+        Assertions.assertThatThrownBy(player::faceUpInitialHand)
             .isExactlyInstanceOf(IllegalStateException.class);
     }
 
     @Test
     @DisplayName("처음에 지급받은 카드를 반환한다.")
     void getReadyCardsTestSuccess() {
-        Player participant = new Player("echo");
-        participant.dealt(new Card(Denomination.ACE, Suit.SPADE));
-        participant.dealt(new Card(Denomination.THREE, Suit.HEART));
-        Assertions.assertThat(participant.faceUpInitialHand())
+        Player player = new Player("echo");
+        player.dealt(new Card(Denomination.ACE, Suit.SPADE));
+        player.dealt(new Card(Denomination.THREE, Suit.HEART));
+        Assertions.assertThat(player.faceUpInitialHand())
             .containsExactly(new Card(Denomination.ACE, Suit.SPADE), new Card(Denomination.THREE, Suit.HEART));
     }
 
     @Test
     @DisplayName("현재의 점수를 반환한다.")
     void calculateScore() {
-        Player participant = new Player("echo");
-        participant.dealt(new Card(Denomination.ACE, Suit.SPADE));
-        participant.dealt(new Card(Denomination.THREE, Suit.HEART));
-        Assertions.assertThat(participant.calculatePoint()).isEqualTo(4);
+        Player player = new Player("echo");
+        player.dealt(new Card(Denomination.ACE, Suit.SPADE));
+        player.dealt(new Card(Denomination.THREE, Suit.HEART));
+        Assertions.assertThat(player.calculatePoint()).isEqualTo(4);
     }
 }
