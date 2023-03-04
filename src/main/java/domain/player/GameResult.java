7dbd01f4a30b64d565af586025722d114f940cb1
BackFoxx <backfoxxx@gmail.com>
2023-03-04 13:29:46 +0900

    refactor : 사용하지 않는 메소드 제거

--- src/main/java/domain/player/GameResult.java
+++ src/main/java/domain/player/GameResult.java
@@ -1,17 +1,5 @@
 package domain.player;
 
 public enum GameResult {
-    WIN, LOSE, DRAW;
-
-    public boolean isWinning() {
-        return this == WIN;
-    }
-
-    public boolean isLose() {
-        return this == LOSE;
-    }
-
-    public boolean isDraw() {
-        return this == DRAW;
-    }
+    WIN, LOSE, DRAW
 }
