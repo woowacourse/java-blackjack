898e357baaf7adb22ddff0153198fc6a32dca9f7
HubCreator <chocomilk8604@gmail.com>
2023-03-05 15:30:07 +0900

    docs : 기능 목록 갱신

---
+++ src/test/java/domain/participant/PlayerTest.java
@@ -0,0 +1,13 @@
+package domain.participant;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void name() {
+
+    }
+}
