5265e246c28c718f97660ff0a3945471021e1627
Miseong Kim <miseongk@naver.com>
2023-03-06 01:46:58 +0900

    refactor(Dealer, Players): 게임 결과 계산하던 로직 제거

--- src/main/java/blackjack/domain/participant/Dealer.java
+++ src/main/java/blackjack/domain/participant/Dealer.java
@@ -1,54 +1,14 @@
 package blackjack.domain.participant;
 
-import static blackjack.domain.Result.DRAW;
-import static blackjack.domain.Result.LOSE;
-import static blackjack.domain.Result.WIN;
-
-import blackjack.domain.Result;
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Cards;
-import blackjack.domain.card.Deck;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Dealer extends Participant {
 
     private static final int SPECIFIC_SCORE_OF_DEALER = 16;
 
-    private final Players players;
-    private final Deck deck;
-
-    public Dealer(final Players players) {
+    public Dealer() {
         super(Cards.generateEmptyCards());
-        this.players = players;
-        this.deck = new Deck();
-    }
-
-    public void distributeTwoCards() {
-        List<Card> cardList = new ArrayList<>();
-
-        while (cardList.size() < players.size() * 2) {
-            Card drawnCard = deck.drawCard();
-            cardList.add(drawnCard);
-        }
-        players.receiveSettingCards(cardList);
-    }
-
-    public void drawSelfInitCards() {
-        for (int count = 0; count < 2; count++) {
-            Card drawnCard = deck.drawCard();
-            receiveCard(drawnCard);
-        }
-    }
-
-    public void giveOneMoreCard(Player player) {
-        player.receiveCard(deck.drawCard());
-    }
-
-    public Map<Player, Result> decideResult() {
-        return players.decideResults(cards.calculateScoreForBlackjack());
     }
 
     public List<Integer> decideSelfResult() {
@@ -80,13 +40,5 @@ public class Dealer extends Participant {
 
     public boolean canDraw() {
         return cards.calculateScoreForBlackjack() <= SPECIFIC_SCORE_OF_DEALER;
-    }
-
-    public void drawOneMoreCard() {
-        receiveCard(deck.drawCard());
-    }
-
-    public Players getPlayers() {
-        return players;
     }
 }
--- src/main/java/blackjack/domain/participant/Players.java
+++ src/main/java/blackjack/domain/participant/Players.java
@@ -1,21 +1,13 @@
 package blackjack.domain.participant;
 
-import static blackjack.domain.Result.DRAW;
-import static blackjack.domain.Result.LOSE;
-import static blackjack.domain.Result.WIN;
-
-import blackjack.domain.Result;
-import blackjack.domain.card.Card;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Players {
 
     private final List<Player> players;
 
-    public Players(final List<Player> players) {
+    protected Players(final List<Player> players) {
         this.players = players;
     }
 
@@ -25,46 +17,5 @@ public class Players {
 
     public int size() {
         return players.size();
-    }
-
-    public void receiveSettingCards(List<Card> settingCards) {
-        for (int cardIndex = 0, playerIndex = 0; cardIndex < settingCards.size(); cardIndex += 2, playerIndex++) {
-            players.get(playerIndex).receiveCard(settingCards.get(cardIndex));
-            players.get(playerIndex).receiveCard(settingCards.get(cardIndex + 1));
-        }
-    }
-
-    public Map<Player, Result> decideResults(int dealerScore) {
-        Map<Player, Result> results = new HashMap<>();
-
-        for (Player player : players) {
-            compareScore(results, dealerScore, player);
-        }
-
-        return results;
-    }
-
-    private void compareScore(final Map<Player, Result> results, final int dealerScore, final Player player) {
-        if (dealerScore > 21 && player.isBust()) {
-            results.put(player, DRAW);
-            return;
-        }
-        if (dealerScore > 21 && !player.isBust()) {
-            results.put(player, WIN);
-            return;
-        }
-        if (dealerScore <= 21 && player.isBust()) {
-            results.put(player, LOSE);
-            return;
-        }
-        if (dealerScore < player.calculateTotalScore()) {
-            results.put(player, WIN);
-        }
-        if (dealerScore > player.calculateTotalScore()) {
-            results.put(player, LOSE);
-        }
-        if (dealerScore == player.calculateTotalScore()) {
-            results.put(player, DRAW);
-        }
     }
 }
