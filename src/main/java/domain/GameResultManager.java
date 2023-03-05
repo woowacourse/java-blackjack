7a41d25c2cdaf7d86db3002ffb38774a122d7a5d
Youngho <cyh6099@gmail.com>
2023-03-05 23:12:49 +0900

    refactor: 주석 제거

--- src/main/java/domain/GameResultManager.java
+++ src/main/java/domain/GameResultManager.java
@@ -7,20 +7,13 @@ import java.util.Map;
 public class GameResultManager {
     public Map<String, List<String>> getParticipantsCard(final Participants participants) {
         final Participant dealer = participants.findDealer();
-
         Map<String, List<String>> participantsHands = new LinkedHashMap<>();
 
         participantsHands.put(dealer.getName(), dealer.getCardNames().subList(0, 1));
-//        participantsHands.put(participants.findDealer().getName(), participants.findDealer().getCardNames());
+
         for (Participant participant : participants.findPlayers()) {
             participantsHands.put(participant.getName(), participant.getCardNames());
         }
-
-//        participantsHands.put(participants.findDealer().getName(), participants.findDealer().getCardNames());
-//        participantsHands.replace("딜러", participantsHands.get("딜러").subList(0, 1));
-//        for (Participant participant : participants.findPlayers()) {
-//            participantsHands.put(participant.getName(), participant.getCardNames());
-//        }
         return participantsHands;
     }
 }
