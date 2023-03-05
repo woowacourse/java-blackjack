68c1353f082d6b58067cde6e89f9f3033565976f
dev_kong <ahdjd5@gmail.com>
2023-03-05 15:10:22 +0900

    feat: Count class for DealerResult class

--- src/main/java/player/DealerResult.java
+++ src/main/java/player/DealerResult.java
@@ -11,28 +11,31 @@ import java.util.Map;
 import blackjackgame.Result;
 
 public class DealerResult {
-    private final EnumMap<Result, Integer> dealerResult = new EnumMap<>(Result.class) {
+    private final EnumMap<Result, Count> dealerResult = new EnumMap<>(Result.class) {
         {
-            put(WIN, 0);
-            put(TIE, 0);
-            put(LOSE, 0);
+            put(WIN, new Count());
+            put(TIE, new Count());
+            put(LOSE, new Count());
         }
     };
 
     public void addWin() {
-        dealerResult.put(WIN, dealerResult.get(WIN) + 1);
+        dealerResult.get(WIN).addCount();
     }
 
     public void addTie() {
-        dealerResult.put(TIE, dealerResult.get(TIE) + 1);
+        dealerResult.get(TIE).addCount();
     }
 
     public void addLose() {
-        dealerResult.put(LOSE, dealerResult.get(LOSE) + 1);
+        dealerResult.get(LOSE).addCount();
     }
 
     public Map<Result, Integer> getDealerResult() {
-        Map<Result, Integer> newMap = new HashMap<>(dealerResult);
+        Map<Result, Integer> newMap = new HashMap<>();
+        dealerResult.forEach((key, value) -> {
+            newMap.put(key, value.getCount());
+        });
         return Map.copyOf(newMap);
     }
 }
