b549495c1f85b60935f657141b814f13f7b1f975
Youngho <cyh6099@gmail.com>
2023-03-06 01:33:19 +0900

    refactor: 참가자 이름 관련된 유효성 검사 로직 Participants로 이동

--- src/main/java/domain/Participants.java
+++ src/main/java/domain/Participants.java
@@ -6,6 +6,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Participants {
+    private static final int MINIMUM_PLAYER_COUNT = 1;
+    private static final int MAXIMUM_PLAYER_COUNT = 7;
     private static final int BUST_HAND_VALUE = 0;
     private final List<Participant> participants;
 
@@ -14,6 +16,7 @@ public class Participants {
     }
 
     public static Participants from(List<String> names) {
+        validate(names);
         List<Participant> participants = new ArrayList<>();
         participants.add(new Dealer());
         names.forEach(name -> participants.add(new Player(name)));
@@ -42,6 +45,19 @@ public class Participants {
         return players;
     }
 
+    public boolean shouldDealerHit() {
+        final Dealer dealer = (Dealer) findDealer();
+        return dealer.shouldHit();
+    }
+
+    public Map<Participant, Integer> makeParticipantFinalHandValue() {
+        final LinkedHashMap<Participant, Integer> participantsHandValue = new LinkedHashMap<>();
+        for (Participant participant : participants) {
+            participantsHandValue.put(participant, participant.getHandValue());
+        }
+        return participantsHandValue;
+    }
+
     public Map<Participant, Boolean> getParticipantsBustStatus() {
         Map<Participant, Boolean> scores = new LinkedHashMap<>();
         for (Participant participant : participants) {
@@ -49,11 +65,6 @@ public class Participants {
         }
 
         return scores;
-    }
-
-    public boolean shouldDealerHit() {
-        final Dealer dealer = (Dealer) findDealer();
-        return dealer.shouldHit();
     }
 
     public Map<Result, Integer> getDealerStatus(Map<String, Result> results) {
@@ -73,6 +84,23 @@ public class Participants {
         }
 
         return playerResults;
+    }
+
+    private static void validate(List<String> names) {
+        validateNumberOfNames(names);
+        validateNoDuplication(names);
+    }
+
+    private static void validateNumberOfNames(List<String> names) {
+        if (names.size() < MINIMUM_PLAYER_COUNT || names.size() > MAXIMUM_PLAYER_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_OF_PLAYER.getMessage());
+        }
+    }
+
+    private static void validateNoDuplication(List<String> names) {
+        if (names.stream().distinct().count() != names.size()) {
+            throw new IllegalArgumentException(ErrorMessage.NAME_IS_DUPLICATED.getMessage());
+        }
     }
 
     private void addParticipantIfPlayer(ArrayList<Participant> players, Participant participant) {
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,11 +5,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static final int MINIMUM_PLAYER_COUNT = 1;
-    private static final int MAXIMUM_PLAYER_COUNT = 7;
     private static final String HIT_OR_STAY_REQUEST_MESSAGE = "%s은(는) 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
     private static final String ASK_INPUT_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-
     private static final String HIT_REQUEST = "y";
     private static final String STAY_REQUEST = "n";
     private static final String DELIMITER = ",";
@@ -21,27 +18,10 @@ public class InputView {
 
     public List<String> requestNames() {
         askInputNames();
-        List<String> names = Arrays.asList(scanner.nextLine().split(DELIMITER, -1));
-        validate(names);
-        return names;
+        return Arrays.asList(scanner.nextLine().split(DELIMITER, -1));
     }
 
-    private void validate(List<String> names) {
-        validateNumberOfNames(names);
-        validateNoDuplication(names);
-    }
 
-    private void validateNumberOfNames(List<String> names) {
-        if (names.size() < MINIMUM_PLAYER_COUNT || names.size() > MAXIMUM_PLAYER_COUNT) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_OF_PLAYER.getMessage());
-        }
-    }
-
-    private void validateNoDuplication(List<String> names) {
-        if (names.stream().distinct().count() != names.size()) {
-            throw new IllegalArgumentException(ErrorMessage.NAME_IS_DUPLICATED.getMessage());
-        }
-    }
 
     private void askDrawingCard(String name) {
         System.out.printf(HIT_OR_STAY_REQUEST_MESSAGE, name);
