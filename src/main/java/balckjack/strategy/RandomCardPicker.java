3d15436ab382a22511dd0dc6174b4f74cf886394
amaran-th <songsy405@gmail.com>
2023-03-05 20:26:38 +0900

    feat : Exception 객체 변경
    - IndexOutOfBoundsException 예외를 IllegalStateException 예외로 변경

--- src/main/java/balckjack/strategy/RandomCardPicker.java
+++ src/main/java/balckjack/strategy/RandomCardPicker.java
@@ -5,13 +5,13 @@ import balckjack.domain.CardPool;
 import java.util.Collections;
 import java.util.List;
 
-public class RandomCardPicker implements CardPicker{
+public class RandomCardPicker implements CardPicker {
 
     @Override
     public Card pick() {
         final List<Card> cards = CardPool.getCards();
         if (cards.isEmpty()) {
-            throw new IndexOutOfBoundsException("더 이상 카드가 없습니다.");
+            throw new IllegalStateException("더 이상 카드가 없습니다.");
         }
         Collections.shuffle(cards);
         return cards.get(0);
