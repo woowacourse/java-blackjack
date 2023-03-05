a3a83bab15b153e1b14e976cf725b2aa4d4c61f6
sh111-coder <ohk9134@naver.com>
2023-03-05 22:34:38 +0900

    refactor(ResultView): 참여자들의 카드 출력 기능 리팩토링

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,22 +1,33 @@
 package view;
 
 import java.util.List;
+import java.util.Map;
 
 public class ResultView {
 
     public static final String JOIN_DELIMITER = ", ";
+    private static final String DEALER_NAME = "딜러";
+    private static final int OPEN_CARD_INDEX = 0;
 
     public static void printInitMessage(List<String> playerNames) {
         String names = String.join(JOIN_DELIMITER, playerNames);
         System.out.println("\n딜러와 " + names + "에게 2장을 나누었습니다.");
     }
 
-    public static void printParticipantResult(String name, List<String> cardName) {
-        if (!name.equals("딜러")) {
-            System.out.println(name + "카드: " + String.join(JOIN_DELIMITER, cardName));
-            return;
+    public static void printParticipantResult(Map<String, List<String>> cardNameByParticipantName) {
+        for (String participantName : cardNameByParticipantName.keySet()) {
+            List<String> cardNames = cardNameByParticipantName.get(participantName);
+            printResultByParticipantName(participantName, cardNames);
         }
-        System.out.println(name + ": " + cardName.get(0));
+    }
+
+    private static void printResultByParticipantName(String participantName, List<String> cardNames) {
+        if (!participantName.equals(DEALER_NAME)) {
+            System.out.println(participantName + "카드: " + String.join(JOIN_DELIMITER, cardNames));
+        }
+        if (participantName.equals(DEALER_NAME)) {
+            System.out.println(participantName + ": " + cardNames.get(OPEN_CARD_INDEX));
+        }
     }
 
     public static void printParticipantFinalResult(String name, List<String> cardName, int totalValueSum) {
