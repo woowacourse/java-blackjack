654d77aacb8caee8460ab4fc64a80f78002d2aaa
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-03-05 21:16:56 +0900

    refactor: 미사용 필드 제거

--- src/main/java/domain/game/Outcome.java
+++ src/main/java/domain/game/Outcome.java
@@ -1,11 +1,5 @@
 package domain.game;
 
 public enum Outcome {
-    WIN("승"), DRAW("무"), LOSE("패");
-
-    private final String outcome;
-
-    Outcome(final String outcome) {
-        this.outcome = outcome;
-    }
+    WIN, DRAW, LOSE
 }
