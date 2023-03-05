87c2b8672ec314506d642cde31c96f369a160141
amaran-th <songsy405@gmail.com>
2023-03-05 21:45:42 +0900

    refactor(controller) : 메서드 분리

--- src/main/java/balckjack/controller/BlackJackController.java
+++ src/main/java/balckjack/controller/BlackJackController.java
@@ -21,43 +21,60 @@ public class BlackJackController {
         final Players players = Repeater.repeatIfError(this::inputPlayerNames,
             OutputView::printErrorMessage);
         final Dealer dealer = new Dealer();
-        Referee referee = new Referee();
+        final Referee referee = new Referee();
+
+        initialSetting(cardPicker, players, dealer);
+        tryPlayersTurn(cardPicker, players, referee);
+        tryDealerTurn(cardPicker, dealer, referee);
+        reportResult(players, dealer, referee);
+    }
+
+    private void initialSetting(CardPicker cardPicker, Players players, Dealer dealer) {
         dealer.initHit(cardPicker);
         players.initHit(cardPicker);
-        OutputView.printInitCardDeck(dealer, players);
 
+        OutputView.printInitCardDeck(dealer, players);
+    }
+
+    private void tryPlayersTurn(CardPicker cardPicker, Players players, Referee referee) {
+        System.out.println();
         for (Player player : players.getPlayers()) {
             askPlayer(referee, player, cardPicker);
         }
-        System.out.println();
-        while (referee.calculateDeckScore(dealer.getCardDeck()) <= DEALER_HIT_NUMBER
-            && referee.calculateDeckScore(dealer.getCardDeck()) != BURST_CODE) {
-            dealer.hit(cardPicker);
-            OutputView.printDealerPickMessage(dealer);
-        }
-        List<Result> results = referee.judgeResult(dealer, players);
-        OutputView.printFinalCardDeck(dealer, players, referee);
-        OutputView.printResult(referee.countDealerResult(results), dealer, players,
-            results);
     }
 
     private void askPlayer(Referee referee, Player player, CardPicker cardPicker) {
-        Command command;
-        while (true) {
-            command = Repeater.repeatIfError(() -> inputCommand(player),
-                OutputView::printErrorMessage);
-            if (command == Command.NO) {
-                break;
-            }
-            player.hit(cardPicker);
-            OutputView.printParticipantCardDeck(player);
-            int score = referee.calculateDeckScore(player.getCardDeck());
-            if (score == BURST_CODE) {
-                OutputView.printBurstMessage();
-                break;
-            }
+        while (isContinue(referee, player, cardPicker)) {
         }
     }
+
+    private boolean isContinue(Referee referee, Player player, CardPicker cardPicker) {
+        if (isReplyNo(player)) {
+            return false;
+        }
+        player.hit(cardPicker);
+        OutputView.printParticipantCardDeck(player);
+        if (isBurst(referee, player)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isReplyNo(Player player) {
+        Command command = Repeater.repeatIfError(() -> inputCommand(player),
+            OutputView::printErrorMessage);
+        return command == Command.NO;
+    }
+
+    private boolean isBurst(Referee referee, Player player) {
+        int score = referee.calculateDeckScore(player.getCardDeck());
+        if (score == BURST_CODE) {
+            OutputView.printBurstMessage();
+            return true;
+        }
+        return false;
+    }
+
 
     private Players inputPlayerNames() {
         return new Players(InputView.inputPlayerNames());
@@ -66,5 +83,19 @@ public class BlackJackController {
     private Command inputCommand(Player player) {
         return Command.toCommand(InputView.inputReply(player.getName().getValue()));
     }
-}
 
+    private void tryDealerTurn(CardPicker cardPicker, Dealer dealer, Referee referee) {
+        while (referee.calculateDeckScore(dealer.getCardDeck()) <= DEALER_HIT_NUMBER
+            && referee.calculateDeckScore(dealer.getCardDeck()) != BURST_CODE) {
+            dealer.hit(cardPicker);
+            OutputView.printDealerPickMessage(dealer);
+        }
+    }
+
+    private void reportResult(Players players, Dealer dealer, Referee referee) {
+        List<Result> results = referee.judgeResult(dealer, players);
+        OutputView.printFinalCardDeck(dealer, players, referee);
+        OutputView.printResult(referee.countDealerResult(results), dealer, players,
+            results);
+    }
+}
