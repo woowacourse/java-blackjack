7bc04e0859ff36513da9cbf77d3157a9634c6a3a
chaewon121 <cw121@naver.com>
2023-03-05 23:38:07 +0900

    refactor: stream 수정

--- src/main/java/blackjack/domain/result/GameResult.java
+++ src/main/java/blackjack/domain/result/GameResult.java
@@ -3,10 +3,7 @@ package blackjack.domain.result;
 import blackjack.domain.player.Dealer;
 import blackjack.domain.player.Player;
 
-import java.util.EnumMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GameResult {
     private final Map<Player, ResultMatcher> playerGameResults = new LinkedHashMap<>();
@@ -27,9 +24,8 @@ public class GameResult {
 
     public EnumMap<ResultMatcher, Integer> calculateDealerScore(Map<Player, ResultMatcher> playerGameResults) {
         EnumMap<ResultMatcher, Integer> dealerScore = new EnumMap<>(ResultMatcher.class);
-        for (ResultMatcher resultMatcher : ResultMatcher.values()) {
-            dealerScore.put(resultMatcher, 0);
-        }
+        Arrays.stream(ResultMatcher.values())
+                .forEach(resultMatcher->dealerScore.put(resultMatcher, 0));
         for (ResultMatcher playerResult : playerGameResults.values()) {
             ResultMatcher dealerResult = ResultMatcher.ofOppositeResult(playerResult);
             dealerScore.put(dealerResult, dealerScore.get(dealerResult) + 1);
