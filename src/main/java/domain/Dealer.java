64431788aaa558a4f7f34d074a0607fbd5f4e939
Youngho <cyh6099@gmail.com>
2023-03-06 00:33:50 +0900

    refactor: 에이스 반영해 카드 총합 구하는 로직 수정

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 
 public class Dealer extends Participant {
     private static final String DEALER_NAME = "딜러";
+    private static final int DEALER_MINIMUM_VALUE= 17;
     public Dealer() {
         super(new ArrayList<>(), DEALER_NAME);
+    }
+
+    public boolean shouldHit() {
+        return super.getHandValue() < DEALER_MINIMUM_VALUE;
     }
 }
