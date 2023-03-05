ede93f7ab288792d2d678fcef5602bae8a7d1220
iamjooon2 <iamjooon2@gmail.com>
2023-03-06 00:12:18 +0900

    chore: 패키지 구조 변경

--- src/test/java/domain/CommandTest.java
+++ src/test/java/domain/CommandTest.java
@@ -2,11 +2,10 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
+import domain.Command;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("Command 는")
 class CommandTest {
