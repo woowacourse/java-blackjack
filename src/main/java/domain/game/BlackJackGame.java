1cc6497e25750e1c0306eb6f68308a3b5fb62f65
JunDonghyuk <ehdgur4814@naver.com>
2023-03-06 01:08:53 +0900

    refactor: BlackJackGame 역할 Players, Player로 이관

--- src/main/java/domain/game/BlackJackGame.java
+++ src/main/java/domain/game/BlackJackGame.java
@@ -4,48 +4,25 @@ import domain.deck.Card;
 import domain.deck.Deck;
 import domain.player.Dealer;
 import domain.player.Player;
-import java.util.ArrayList;
+import domain.player.Players;
+
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class BlackJackGame {
-    private static final int BLACK_JACK_NUMBER = 21;
 
     private final Deck deck;
-    private final List<Player> players;
+    private final Players players;
 
     public BlackJackGame(final Deck deck, final List<String> playerNames) {
         this.deck = deck;
-        this.players = new ArrayList<>();
-        addPlayers(playerNames);
-        distributeTwoCards();
-    }
-
-    private void addPlayers(final List<String> playerNames) {
-        this.players.add(new Dealer());
-        playerNames.forEach(name ->
-                this.players.add(new Player(name))
-        );
-    }
-
-    private void distributeTwoCards() {
-        for (Player player : players) {
-            player.drawCard(deck.popCard());
-            player.drawCard(deck.popCard());
-        }
+        this.players = new Players(playerNames);
+        players.drawTwoCardsAtFirstTime(deck);
     }
 
     public void drawCard(final String playerName) {
-        Player player = findPlayer(playerName);
-        player.drawCard(deck.popCard());
-    }
-
-    private Player findPlayer(final String playerName) {
-        return players.stream()
-                .filter(player -> player.getName().equals(playerName))
-                .findFirst()
-                .orElseThrow();
+        players.findPlayer(playerName).drawCard(deck.popCard());
     }
 
     public boolean isDealerDraw() {
@@ -53,50 +30,36 @@ public class BlackJackGame {
     }
 
     private Dealer findDealer() {
-        return (Dealer) players.get(0);
+        return players.findDealer();
     }
 
     public Map<String, Outcome> decidePlayersOutcome() {
         Map<String, Outcome> result = new LinkedHashMap<>();
         final int dealerScore = findDealer().getScore();
-        final List<Player> players = this.players.subList(1, this.players.size());
+        final List<Player> players = this.players.getPlayersWithOutDealer();
 
         players.forEach((player ->
-                result.put(player.getName(), decideOutcome(dealerScore, player.getScore()))
+                result.put(player.getName(), decideOutcome(dealerScore, player))
         ));
 
         return result;
     }
 
-    private Outcome decideOutcome(final int dealerScore, final int playerScore) {
-        if (isPlayerWin(dealerScore, playerScore)) {
+    private Outcome decideOutcome(final int dealerScore, final Player player) {
+        if (player.isWin(dealerScore)) {
             return Outcome.WIN;
         }
-        if (isPlayerDraw(dealerScore, playerScore)) {
+        if (player.isDraw(dealerScore)) {
             return Outcome.DRAW;
         }
         return Outcome.LOSE;
     }
 
-    private boolean isPlayerWin(final int dealerScore, final int playerScore) {
-        if (dealerScore > BLACK_JACK_NUMBER && playerScore <= BLACK_JACK_NUMBER) {
-            return true;
-        }
-        return playerScore <= BLACK_JACK_NUMBER && playerScore > dealerScore;
-    }
-
-    private boolean isPlayerDraw(final int dealerScore, final int playerScore) {
-        if (dealerScore > BLACK_JACK_NUMBER && playerScore > BLACK_JACK_NUMBER) {
-            return true;
-        }
-        return dealerScore == playerScore;
-    }
-
     public List<Card> getCards(final String playerName) {
-        return findPlayer(playerName).getCards();
+        return players.findPlayer(playerName).getCards();
     }
 
     public int getScore(final String playerName) {
-        return findPlayer(playerName).getScore();
+        return players.findPlayer(playerName).getScore();
     }
 }
