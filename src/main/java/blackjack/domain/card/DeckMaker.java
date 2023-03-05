f184f4ee90cf56642c00bbcbdf6868e30fe986c0
70825 <hello70825@gmail.com>
2023-03-05 23:49:34 +0900

    refactor: 캐싱 적용

--- src/main/java/blackjack/domain/card/DeckMaker.java
+++ src/main/java/blackjack/domain/card/DeckMaker.java
@@ -5,10 +5,17 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DeckMaker {
-    public List<Card> makeDeck() {
-        return Stream.of(Shape.values())
+
+    private static final List<Card> CARDS;
+
+    static {
+        CARDS = Stream.of(Shape.values())
                 .flatMap(shape -> Stream.of(Letter.values())
                         .map(letter -> new Card(shape, letter)))
                 .collect(Collectors.toList());
+    }
+
+    public List<Card> makeDeck() {
+        return CARDS;
     }
 }
