5a9f2e353986067f137530c0c39f01fd7b351bfc
junpakPark <junpak.park@gmail.com>
2023-03-05 13:25:13 +0900

    feat : Participant 객체 메소드 추가

---
+++ src/main/java/domain/player/Participant.java
@@ -0,0 +1,32 @@
+package domain.player;
+
+import domain.card.Cards;
+
+public final class Participant extends Player {
+
+    private static final int BURST_NUMBER = 22;
+
+    private Participant(final Name name, final Cards cards) {
+        super(name, cards);
+    }
+
+    public static Participant from(final String name) {
+        validateImpersonate(name);
+
+        return new Participant(Name.of(name), new Cards());
+    }
+
+    private static void validateImpersonate(final String name) {
+        if (name.equals("딜러")) {
+            throw new IllegalArgumentException("참가자의 이름은 딜러가 될 수 없습니다.");
+        }
+    }
+
+    public boolean isKeepGaming(boolean isHit) {
+        return isBurst() && isHit;
+    }
+
+    private boolean isBurst() {
+        return getScore() < BURST_NUMBER;
+    }
+}
