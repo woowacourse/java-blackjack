1813a971154eb5da78b175d4ad6858648acb2378
kiarakim <kiarakim413@gmail.com>
2023-03-05 15:00:50 +0900

    refactor: 생성한 Deck을 shuffle한 후 index에 따라 반환하는 기능 구현

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -1,36 +1,40 @@
 package domain;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
 
 public class Deck {
+	private static final List<Card> deck;
+	private static int index = 0;
 
-    private final CardIndexGenerator cardIndexGenerator;
-    private final List<Card> unusedCards;
+	static {
+		deck = makeDeck();
+		Collections.shuffle(deck);
+	}
 
-    private Deck(CardIndexGenerator cardIndexGenerator, List<Card> unusedCards) {
-        this.cardIndexGenerator = cardIndexGenerator;
-        this.unusedCards = new LinkedList<>(unusedCards);
-    }
+	private Deck() {
+	}
 
-    public static Deck from(final CardIndexGenerator cardIndexGenerator) {
-        List<Card> cards = new ArrayList<>();
-        for (Suits suit : Suits.values()) {
-            int denominationLength = Denomination.values().length;
-            addCard(cards, suit, denominationLength);
-        }
-        return new Deck(cardIndexGenerator, cards);
-    }
+	private static List<Card> makeDeck() {
+		List<Card> cards = new ArrayList<>();
+		for (Denomination denomination : Denomination.values()) {
+			makeCard(cards, denomination);
+		}
+		return cards;
+	}
 
-    private static void addCard(final List<Card> cards,final Suits suit,final int denominationLength) {
-        for (int i = 0; i < denominationLength; i++) {
-            cards.add(new Card(Denomination.values()[i], suit));
-        }
-    }
+	private static void makeCard(List<Card> cards, Denomination denomination) {
+		for (Suits suit : Suits.values()) {
+			cards.add(new Card(denomination, suit));
+		}
+	}
 
-    public Card pickCard() {
-        int index = cardIndexGenerator.chooseIndex(unusedCards.size());
-        return unusedCards.remove(index);
-    }
+	public static Card pickCard() {
+		return deck.get(index++);
+	}
+
+	public static List<Card> getDeck() {
+		return new ArrayList<>(deck);
+	}
 }
--- src/test/java/domain/DeckTest.java
+++ src/test/java/domain/DeckTest.java
@@ -7,26 +7,11 @@ import org.junit.jupiter.api.Test;
 
 public class DeckTest {
 
-    private static class ZeroIndexGenerator implements CardIndexGenerator {
-
-        @Override
-        public int chooseIndex(int deckSize) {
-            return 0;
-        }
-    }
-
-    @DisplayName("카드를 뽑는다")
+    @DisplayName("카드를 52장 생성한다")
     @Test
-    void pickCard() {
-        Deck deck = Deck.from(new ZeroIndexGenerator());
+    void makeCard() {
+        int size = Deck.getDeck().size();
 
-        Card card1 = deck.pickCard();
-        Card card2 = deck.pickCard();
-
-        assertThat(card1.getSuit()).isEqualTo(Suits.values()[0]);
-        assertThat(card1.getDenomination()).isEqualTo(Denomination.values()[0]);
-
-        assertThat(card2.getSuit()).isEqualTo(Suits.values()[0]);
-        assertThat(card2.getDenomination()).isEqualTo(Denomination.values()[1]);
+        assertThat(size).isEqualTo(52);
     }
 }
