5a0a8f7b5111468d10128b9d41391fbfffa93afd
Miseong Kim <miseongk@naver.com>
2023-03-05 10:47:33 +0900

    refactor(InputView): 메소드 분리

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -13,25 +13,30 @@ public class InputView {
 
     public List<String> receivePlayersName() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        String input = scanner.nextLine();
+        String input = getInput();
 
         return Arrays.stream(input.split(DELIMITER_WITH_BLANK))
                 .collect(Collectors.toList());
+    }
+
+    public String askReceiveMoreCard(String playerName) {
+        System.out.println(playerName + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+        String input = getInput();
+
+        isBlank(input);
+        validateCorrectResponse(input);
+
+        return input;
+    }
+
+    private String getInput() {
+        return scanner.nextLine();
     }
 
     private void isBlank(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 공백은 입력할 수 없습니다.");
         }
-    }
-
-    public String askReceiveMoreCard(String playerName) {
-        System.out.println(playerName + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
-        String input = scanner.nextLine();
-        isBlank(input);
-        validateCorrectResponse(input);
-
-        return input;
     }
 
     private void validateCorrectResponse(final String input) {
