19c13c37729434dd346e9b8c82189fa9482d184f
waterricecake <fruturum@nate.com>
2023-03-05 22:21:13 +0900

    reformat(Name): 불필요한 validate 제거

--- src/main/java/blackjack/domain/participant/Name.java
+++ src/main/java/blackjack/domain/participant/Name.java
@@ -4,24 +4,10 @@ import java.util.regex.Pattern;
 
 public class Name {
 
-    private static final Pattern NAME_FORMAT = Pattern.compile("[a-zA-Z]+");
-    private static final String NAME_FORMAT_ERROR_MESSAGE = "사람 이름은 영문자만 가능합니다.";
-
     private final String value;
 
     public Name(final String value) {
-        validate(value);
         this.value = value;
-    }
-
-    private void validate(final String value) {
-        if (isNotEnglish(value)) {
-            throw new IllegalArgumentException(NAME_FORMAT_ERROR_MESSAGE);
-        }
-    }
-
-    private boolean isNotEnglish(final String value) {
-        return !NAME_FORMAT.matcher(value).matches();
     }
 
     public String getValue() {
