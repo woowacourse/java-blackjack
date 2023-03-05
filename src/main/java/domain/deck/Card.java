178b4df215c2c9528a88f200b6ce24528475d1cf
JunDonghyuk <ehdgur4814@naver.com>
2023-03-05 23:25:06 +0900

    refactor: 카드 저장 리스트 이름 변경

--- src/main/java/domain/deck/Card.java
+++ src/main/java/domain/deck/Card.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Card {
-    private static List<Card> cardStorage;
+    private static List<Card> cards;
     private final Suit suit;
     private final Rank rank;
 
@@ -15,12 +15,12 @@ public class Card {
     }
 
     public static Card getCard(final Suit suit, final Rank rank) {
-        if (cardStorage == null) {
-            cardStorage = new ArrayList<>();
+        if (cards == null) {
+            cards = new ArrayList<>();
             initializeCardStorage();
         }
 
-        return cardStorage.stream()
+        return cards.stream()
                 .filter(card -> card.getRank() == rank && card.getSuit() == suit)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR]: 해당 카드가 없습니다."));
@@ -29,7 +29,7 @@ public class Card {
     private static void initializeCardStorage() {
         for (Suit suit : Suit.values()) {
             Arrays.stream(Rank.values())
-                    .forEach(rank -> cardStorage.add(new Card(suit, rank)));
+                    .forEach(rank -> cards.add(new Card(suit, rank)));
         }
     }
 
