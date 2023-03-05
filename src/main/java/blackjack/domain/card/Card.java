c0922f0484088419d893e114b5027e56adc773d4
chaewon121 <cw121@naver.com>
2023-03-05 23:12:51 +0900

    feat: 게터 수정

--- src/main/java/blackjack/domain/card/Card.java
+++ src/main/java/blackjack/domain/card/Card.java
@@ -2,23 +2,23 @@ package blackjack.domain.card;
 
 public class Card {
 
-    private final CardNumber number;
+    private final CardNumber cardNumber;
     private final CardSymbol symbol;
 
-    public Card(CardNumber number, CardSymbol symbol) {
-        this.number = number;
+    public Card(CardNumber CardNumber, CardSymbol symbol) {
+        this.cardNumber = CardNumber;
         this.symbol = symbol;
     }
 
-    public CardNumber getNumber() {
-        return this.number;
+    public CardNumber getCardNumber() {
+        return this.cardNumber;
     }
 
-    public CardSymbol getSymbol() {
-        return this.symbol;
+    public String getSymbol() {
+        return this.symbol.getSymbol();
     }
 
     public boolean isAce() {
-        return number.equals(CardNumber.ACE);
+        return cardNumber.equals(CardNumber.ACE);
     }
 }
