ac6a4ccaa466166a9263cb512ba278a61817ada6
sh111-coder <ohk9134@naver.com>
2023-03-06 21:41:19 +0900

    refactor: 최종 결과 출력 기능 리팩토링

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -8,20 +8,14 @@ public class ResultView {
     public static final String JOIN_DELIMITER = ", ";
     private static final String DEALER_NAME = "딜러";
     private static final int OPEN_CARD_INDEX = 0;
+    private static final String FINAL_RESULT_MESSAGE = "\n## 최종 승패";
 
     public static void printInitMessage(List<String> playerNames) {
         String names = String.join(JOIN_DELIMITER, playerNames);
         System.out.println("\n딜러와 " + names + "에게 2장을 나누었습니다.");
     }
 
-    public static void printParticipantResult(Map<String, List<String>> cardNameByParticipantName) {
-        for (String participantName : cardNameByParticipantName.keySet()) {
-            List<String> cardNames = cardNameByParticipantName.get(participantName);
-            printResultByParticipantName(participantName, cardNames);
-        }
-    }
-
-    private static void printResultByParticipantName(String participantName, List<String> cardNames) {
+    public static void printParticipantResult(String participantName, List<String> cardNames) {
         if (!participantName.equals(DEALER_NAME)) {
             System.out.println(participantName + "카드: " + String.join(JOIN_DELIMITER, cardNames));
         }
@@ -34,10 +28,10 @@ public class ResultView {
         System.out.println(name + "카드: " + String.join(JOIN_DELIMITER, cardName) + " - 결과: " + totalValueSum);
     }
 
-    public static void printFinalFightResult(List<String> finalFightResults) {
-        System.out.println("\n## 최종 승패");
-        for (String result : finalFightResults) {
-            System.out.println(result);
+    public static void printFinalFightResult(Map<String, String> finalFightResults) {
+        System.out.println(FINAL_RESULT_MESSAGE);
+        for (String participantName : finalFightResults.keySet()) {
+            System.out.println(participantName + ": " + finalFightResults.get(participantName));
         }
     }
 }
