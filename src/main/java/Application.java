bbbeb856beb0b66965bee4d5e3e682d1193b8175
JunH <tjdtls690@gmail.com>
2023-03-06 11:38:17 +0900

    :art: refactor : 컨트롤러의 역할에 맞지 않는 컨트롤러 제거

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,82 @@
-import controller.BlackJackController;
+import domain.BlackJack;
+import domain.player.Player;
+import domain.strategy.RandomBasedIndexGenerator;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        BlackJackController blackJackController = new BlackJackController();
-        blackJackController.startGame();
+        new Application().startGame();
+    }
+    
+    public void startGame() {
+        BlackJack blackJack = new BlackJack(getParticipantNames(), new RandomBasedIndexGenerator());
+        initializedBlackjackGame(blackJack);
+        
+        giveCardToPlayers(blackJack);
+        blackJack.battle();
+        OutputView.printPlayersGameResults(blackJack.getPlayers());
+    }
+    
+    private String getParticipantNames() {
+        OutputView.printParticipantNamesGuide();
+        return InputView.repeat(InputView::inputParticipantNames);
+    }
+    
+    private void initializedBlackjackGame(BlackJack blackJack) {
+        blackJack.giveTwoCardToPlayers();
+        OutputView.printPlayersInformation(blackJack.getPlayers());
+    }
+    
+    private void giveCardToPlayers(BlackJack blackJack) {
+        giveCardToParticipants(blackJack);
+        giveCardToDealer(blackJack);
+        OutputView.printPlayersFinalInformation(blackJack.getPlayers());
+    }
+    
+    private void giveCardToParticipants(BlackJack blackJack) {
+        List<Player> participants = blackJack.getParticipants();
+        for (Player participant : participants) {
+            giveCardToParticipant(blackJack, participant);
+        }
+    }
+    
+    private void giveCardToParticipant(BlackJack blackJack, Player participant) {
+        String command = getCommand(participant);
+        if ("y".equals(command)) {
+            blackJack.giveCard(participant.getName());
+            OutputView.printParticipantCardCondition(List.of(participant));
+        }
+        
+        if ("n".equals(command) || participant.isBurst()) {
+            stopGivingCard(participant, command);
+            return;
+        }
+        giveCardToParticipant(blackJack, participant);
+    }
+    
+    private String getCommand(Player participant) {
+        OutputView.printAddCardGuide(participant.getName());
+        return InputView.repeat(InputView::inputAddCardCommand);
+    }
+    
+    private void stopGivingCard(Player participant, String command) {
+        if ("n".equals(command)) {
+            OutputView.printParticipantCardCondition(List.of(participant));
+            return;
+        }
+        
+        if (participant.isBurst()) {
+            OutputView.printBurstMessage(participant.getName());
+        }
+    }
+    
+    private void giveCardToDealer(BlackJack blackJack) {
+        if (blackJack.shouldDealerGetCard()) {
+            blackJack.giveDealerCard();
+            OutputView.printGiveDealerCardMessage();
+        }
     }
 }
