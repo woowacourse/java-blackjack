aef47098f504524afb630bfebbcec1973c8609ea
xxeol2 <jhj01177@naver.com>
2023-03-06 14:13:22 +0900

    test: 게임 결과 테스트 검증 로직 추가

--- src/test/java/domain/GameResultTest.java
+++ src/test/java/domain/GameResultTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,18 +11,21 @@ class GameResultTest {
     @DisplayName("플레이어와 딜러가 둘 다 버스트가 아니고, 플레이어의 점수가 더 크면 이긴다")
     @Test
     void player_dealer_neither_bust_WIN() {
-        GameResult.comparePlayerWithDealer(13, 10);
+        GameResult gameResult = GameResult.comparePlayerWithDealer(13, 10);
+        assertThat(gameResult).isEqualTo(GameResult.WIN);
     }
 
     @DisplayName("플레이어만 버스트이면 진다")
     @Test
     void player_bust_LOSE() {
-        GameResult.comparePlayerWithDealer(23, 10);
+        GameResult gameResult = GameResult.comparePlayerWithDealer(23, 10);
+        assertThat(gameResult).isEqualTo(GameResult.LOSE);
     }
 
     @DisplayName("플레이어와 딜러가 둘 다 버스트이면 비긴다")
     @Test
     void player_dealer_both_bust_PUSH() {
-        GameResult.comparePlayerWithDealer(25, 23);
+        GameResult gameResult = GameResult.comparePlayerWithDealer(25, 23);
+        assertThat(gameResult).isEqualTo(GameResult.PUSH);
     }
 }
