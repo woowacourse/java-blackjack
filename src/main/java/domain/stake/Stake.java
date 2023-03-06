af1481da55f294845a80af228eea0bdb8d7274a8
kevstevie <jianna442@gmail.com>
2023-03-06 21:24:18 +0900

    refactor: 최소 베팅값 변경

--- src/main/java/domain/stake/Stake.java
+++ src/main/java/domain/stake/Stake.java
@@ -4,7 +4,7 @@ import domain.player.DealerStatus;
 
 public class Stake {
 
-    private static final int MIN_BET = 0;
+    private static final int MIN_BET = 100;
     private static final int MAX_BET = 100_000;
     private static final int PLAYER_NEGATE = -1;
     private final int value;
@@ -16,7 +16,7 @@ public class Stake {
 
     private void validate(final int value) {
         if (value < MIN_BET) {
-            throw new IllegalArgumentException("[ERROR] 베팅 금액은 0 미만일 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 베팅 금액은 100 미만일 수 없습니다.");
         }
         if (value > MAX_BET) {
             throw new IllegalArgumentException("[ERROR] 베팅 금액은 100,000 초과일 수 없습니다.");
