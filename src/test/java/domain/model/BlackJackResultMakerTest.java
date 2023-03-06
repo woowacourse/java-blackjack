d77594927ad759b8770ed0685926bb63b0bcc213
jaehee329 <jjhs9803@gmail.com>
2023-03-06 22:04:17 +0900

    test: 무승부 결과 계산 테스트 추가

--- src/test/java/domain/model/BlackJackResultMakerTest.java
+++ src/test/java/domain/model/BlackJackResultMakerTest.java
@@ -9,18 +9,26 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BlackJackResultMakerTest {
 
     private final BlackJackResultMaker blackJackResultMaker = new BlackJackResultMaker();
-    private final Cards dealerCards = new Cards(Set.of(new Card(Suit.SPADE, Letter.TWO)));
-    private final Dealer dealer = new Dealer(dealerCards);
-    private final Cards playerCards = new Cards(Set.of(new Card(Suit.SPADE, Letter.SIX)));
-    private final List<Player> players = IntStream.range(0, 10)
-        .mapToObj(i -> new Player(playerCards, "test"))
-        .collect(Collectors.toList());
+    private Dealer dealer;
+    private Cards playerCards;
+    private List<Player> players;
+
+    @BeforeEach
+    public void beforeEach() {
+        Cards dealerCards = new Cards(Set.of(new Card(Suit.SPADE, Letter.TWO)));
+        dealer = new Dealer(dealerCards);
+        playerCards = new Cards(Set.of(new Card(Suit.SPADE, Letter.SIX)));
+        players = IntStream.range(0, 10)
+            .mapToObj(i -> new Player(playerCards, "test"))
+            .collect(Collectors.toList());
+    }
 
     @Test
     @DisplayName("플레이어들 승패 결과 만들기 테스트(플레이어 모두 승)")
@@ -31,8 +39,9 @@ class BlackJackResultMakerTest {
 
         //then
         for (Player player : results.keySet()) {
-            assertThat(results.get(player).getVictory()).isEqualTo(1);
-            assertThat(results.get(player).getDefeat()).isEqualTo(0);
+            assertThat(results.get(player).getVictory()).isOne();
+            assertThat(results.get(player).getDraw()).isZero();
+            assertThat(results.get(player).getDefeat()).isZero();
         }
     }
 
@@ -44,8 +53,30 @@ class BlackJackResultMakerTest {
         Result result = blackJackResultMaker.makeDealerResult(dealer, players);
 
         //then
-        assertThat(result.getDefeat()).isEqualTo(10);
         assertThat(result.getVictory()).isEqualTo(0);
         assertThat(result.getDraw()).isEqualTo(0);
+        assertThat(result.getDefeat()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("무승부 결과 만들기 테스트(딜러 10무, 플레이어 모두 무)")
+    public void testPlayerDealerDraw() {
+        //given
+        dealer = new Dealer(playerCards);
+
+        //when
+        Result dealerResult = blackJackResultMaker.makeDealerResult(dealer, players);
+        Map<Player, Result> playerResults = blackJackResultMaker.makeParticipantsResult(dealer, players);
+
+        //then
+        assertThat(dealerResult.getVictory()).isEqualTo(0);
+        assertThat(dealerResult.getDraw()).isEqualTo(10);
+        assertThat(dealerResult.getDefeat()).isEqualTo(0);
+
+        for (Player player : playerResults.keySet()) {
+            assertThat(playerResults.get(player).getVictory()).isZero();
+            assertThat(playerResults.get(player).getDraw()).isOne();
+            assertThat(playerResults.get(player).getDefeat()).isZero();
+        }
     }
 }
