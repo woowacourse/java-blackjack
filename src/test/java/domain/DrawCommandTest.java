deb265ac23238a6ee2155dfc27db1baead411540
MoonJeWoong <munjin0201@naver.com>
2023-03-06 18:01:18 +0900

    test: 사용자 입력값에 따른 DrawCommand 반환 테스트 코드 작성

---
+++ src/test/java/domain/DrawCommandTest.java
@@ -0,0 +1,29 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Named;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+@DisplayName("DrawCommand는 ")
+class DrawCommandTest {
+    @ParameterizedTest(name = "{0} 입력값이 들어오면 {1}")
+    @MethodSource("createDrawCommandTestCase")
+    void createDrawCommandTest(String input, DrawCommand expected) {
+        DrawCommand drawCommand = DrawCommand.of(input);
+
+        assertThat(drawCommand).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> createDrawCommandTestCase() {
+        return Stream.of(
+                Arguments.of("y", Named.of("카드를 뽑는다.", DrawCommand.DRAW)),
+                Arguments.of("n", Named.of("카드를 뽑지 않는다.", DrawCommand.STOP))
+        );
+    }
+}
