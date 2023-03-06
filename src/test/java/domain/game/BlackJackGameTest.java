96eda57dcb0c63c5c94e7c5e98e428abec2f91f0
yujamint <woawoa0513@gmail.com>
2023-03-06 18:58:54 +0900

    test: 전략 패턴 구현에 따른 테스트 작성

--- src/test/java/domain/game/BlackJackGameTest.java
+++ src/test/java/domain/game/BlackJackGameTest.java
@@ -2,12 +2,19 @@ package domain.game;
 
 import static org.assertj.core.api.Assertions.*;
 
+import domain.card.Card;
 import domain.card.Cards;
-import domain.card.ShuffledCardsGenerator;
+import domain.card.CardsGenerator;
+import domain.card.CloverCard;
+import domain.card.DiamondCard;
+import domain.card.HeartCard;
+import domain.card.SpadeCard;
 import domain.user.Dealer;
 import domain.user.Player;
 import domain.user.Players;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,9 +24,9 @@ class BlackJackGameTest {
 
     @BeforeEach
     void setUp() {
-        Players players = new Players(List.of("유자", "민트"));
+        Players players = new Players(List.of("민트"));
         Dealer dealer = new Dealer();
-        Cards cards = new Cards(new ShuffledCardsGenerator());
+        Cards cards = new Cards(new FixedCardsGenerator());
 
         blackJackGame = new BlackJackGame(players, dealer, cards);
     }
@@ -42,9 +49,95 @@ class BlackJackGameTest {
 
         blackJackGame.drawDefaultCard();
 
-        assertThat(player.getCards().size()).isEqualTo(2);
-        assertThat(dealer.getCards().size()).isEqualTo(2);
+        assertThat(player.getCards()).isEqualTo(List.of(CloverCard.CLOVER_FIVE, HeartCard.HEART_JACK));
+        assertThat(dealer.getCards()).isEqualTo(List.of(SpadeCard.SPADE_FOUR, DiamondCard.DIAMOND_SEVEN));
     }
 
-    // TODO: 2023/03/06 테스트가 가능하도록 Card 주입 방식 바꾼 뒤 getSetUpResult() 메서드 테스트
+    @DisplayName("딜러는 합산 점수가 17 이상이 될 때까지 카드를 뽑는다")
+    @Test
+    void drawDealerCardUntilSatisfyingConditionTest() {
+        Dealer dealer = blackJackGame.getDealer();
+        blackJackGame.drawDealerCardUntilSatisfyingCondition();
+
+        List<Card> expectedCards = List.of(CloverCard.CLOVER_FIVE, HeartCard.HEART_JACK, SpadeCard.SPADE_FOUR);
+
+        assertThat(dealer.getCards()).isEqualTo(expectedCards);
+    }
+
+    @DisplayName("플레이어가 버스트일 경우, 딜러가 승리한다.")
+    @Test
+    void judgeWinnerTest_playerBust() {
+        Map<Boolean, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
+        Player player = blackJackGame.getPlayers().get(0);
+        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 , 딜러 : 4스페이드, 7다이아몬드
+        blackJackGame.drawOneMoreCard(player); // 플레이어 : 5클로버, J하트, A하트
+        blackJackGame.drawOneMoreCard(player); // 플레이어 : 5클로버, J하트, A하트, 6클로버
+
+        blackJackGame.judgeWinner();
+
+        assertThat(player.isWinner()).isFalse();
+        assertThat(winningRecord.get(true)).isEqualTo(1);
+        assertThat(winningRecord.get(false)).isEqualTo(0);
+    }
+
+    @DisplayName("플레이어는 버스트가 아니고, 딜러는 버스트일 경우 플레이어가 승리한다.")
+    @Test
+    void judgeWinnerTest_playerNormalAndDealerBust() {
+        Map<Boolean, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
+        Player player = blackJackGame.getPlayers().get(0);
+        Dealer dealer = blackJackGame.getDealer();
+        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 , 딜러 : 4스페이드, 7다이아몬드
+        dealer.receiveCards(List.of(HeartCard.HEART_TEN, DiamondCard.DIAMOND_TEN)); // 딜러 : 4스페이드, 7다이아몬드, 10하트, 10다이아몬드
+
+
+        blackJackGame.judgeWinner();
+
+        assertThat(player.isWinner()).isTrue();
+        assertThat(winningRecord.get(true)).isEqualTo(0);
+        assertThat(winningRecord.get(false)).isEqualTo(1);
+    }
+
+    @DisplayName("딜러의 점수가 플레이어의 점수보다 높을 경우, 딜러가 승리한다.")
+    @Test
+    void judgeWinnerTest_dealerWin() {
+        Map<Boolean, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
+        Player player = blackJackGame.getPlayers().get(0);
+        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 , 딜러 : 4스페이드, 7다이아몬드
+        blackJackGame.drawDealerCardUntilSatisfyingCondition(); // 딜러 : 4스페이드, 7다이아몬드, A하트, 6클로버
+
+        blackJackGame.judgeWinner();
+
+        assertThat(player.isWinner()).isFalse();
+        assertThat(winningRecord.get(true)).isEqualTo(1);
+        assertThat(winningRecord.get(false)).isEqualTo(0);
+    }
+
+    @DisplayName("플레이어의 점수가 딜러의 점수보다 높을 경우, 플레이어가 승리한다.")
+    @Test
+    void judgeWinnerTest_playerWin() {
+        Map<Boolean, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
+        Player player = blackJackGame.getPlayers().get(0);
+        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 , 딜러 : 4스페이드, 7다이아몬드
+
+        blackJackGame.judgeWinner();
+
+        assertThat(player.isWinner()).isTrue();
+        assertThat(winningRecord.get(true)).isEqualTo(0);
+        assertThat(winningRecord.get(false)).isEqualTo(1);
+    }
+
+    private class FixedCardsGenerator implements CardsGenerator {
+        /*
+         5클로버, J하트, 4스페이드, 7다이아몬드, A하트, 6클로버, 8클로버, 9다이아몬드, 7하트
+         위 순서대로 담겨져 있는 List<Card>를 반환
+         */
+        @Override
+        public List<Card> generate() {
+            return new ArrayList<>(
+                    List.of(CloverCard.CLOVER_FIVE, HeartCard.HEART_JACK, SpadeCard.SPADE_FOUR,
+                            DiamondCard.DIAMOND_SEVEN, HeartCard.HEART_ACE, CloverCard.CLOVER_SIX, CloverCard.CLOVER_EIGHT,
+                            DiamondCard.DIAMOND_NINE, HeartCard.HEART_SEVEN)
+            );
+        }
+    }
 }
