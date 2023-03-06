6c2a105399b9c926036ddaa66d9f8c1238314260
be-student <presentsong@naver.com>
2023-03-06 16:04:50 +0900

    refactor: String format 대신 MessageFormat 사용

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,5 +1,6 @@
 package blackjack.view;
 
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -9,7 +10,7 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-    private static final String INPUT_COMMAND_MESSAGE = "%s은/는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String INPUT_COMMAND_MESSAGE = "{0}은/는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
     private static final String INPUT_DELIMITER = ",";
 
     public List<String> inputPlayerNames() {
@@ -21,7 +22,7 @@ public class InputView {
     }
 
     public DrawCommand inputCommand(final String playerName) {
-        System.out.println(String.format(INPUT_COMMAND_MESSAGE, playerName));
+        System.out.println(MessageFormat.format(INPUT_COMMAND_MESSAGE, playerName));
         final String input = scanner.nextLine();
         return DrawCommand.from(input);
     }
