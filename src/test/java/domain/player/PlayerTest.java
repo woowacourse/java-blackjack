9606814677ceed7034517d4a5e7240a6028bcf99
JunDonghyuk <ehdgur4814@naver.com>
2023-03-06 12:33:51 +0900

    test: Player 테스트 작성

--- src/test/java/domain/player/PlayerTest.java
+++ src/test/java/domain/player/PlayerTest.java
@@ -1,26 +1,73 @@
 package domain.player;
 
+import domain.deck.Card;
 import domain.deck.Deck;
-import domain.player.Player;
-import org.junit.jupiter.api.Assertions;
+import domain.deck.Rank;
+import domain.deck.Suit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @TestInstance(Lifecycle.PER_CLASS)
 public class PlayerTest {
     Deck deck = new Deck();
 
-    @DisplayName("플레이어가 처음 카드를 뽑으면 패의 크기는 1이다.")
     @Test
+    @DisplayName("플레이어가 처음 카드를 뽑으면 패의 크기는 1이다.")
     void drawTest() {
 
         final String testName = "test";
         Player player = new Player(testName);
 
-        Assertions.assertEquals(0, player.getCards().size());
+        assertEquals(0, player.getCards().size());
         player.drawCard(deck.popCard());
-        Assertions.assertEquals(1, player.getCards().size());
+        assertEquals(1, player.getCards().size());
+    }
+
+    @Test
+    @DisplayName("플레이어가 딜러보다 점수가 높은 경우 isWin은 True를 반환한다.")
+    void isWinTrueTest() {
+        Player player = new Player("hardy");
+        player.drawCard(Card.getCard(Suit.DIAMOND, Rank.QUEEN));
+        player.drawCard(Card.getCard(Suit.DIAMOND, Rank.KING));
+
+        int dealerScore = 19;
+        assertTrue(player.isWin(dealerScore));
+    }
+
+    @Test
+    @DisplayName("플레이어가 딜러보다 점수가 높지 않을 경우 isWin은 False를 반환한다.")
+    void isWinFalseTest() {
+        Player player = new Player("hardy");
+        player.drawCard(Card.getCard(Suit.DIAMOND, Rank.QUEEN));
+        player.drawCard(Card.getCard(Suit.DIAMOND, Rank.KING));
+
+        int dealerScore = 21;
+        assertFalse(player.isWin(dealerScore));
+    }
+
+    @Test
+    @DisplayName("플레이어가 딜러와 점수가 같을 경우 isDraw은 True를 반환한다.")
+    void isDrawTrueTest() {
+        Player player = new Player("hardy");
+        player.drawCard(Card.getCard(Suit.DIAMOND, Rank.QUEEN));
+        player.drawCard(Card.getCard(Suit.DIAMOND, Rank.KING));
+
+        int dealerScore = 20;
+        assertTrue(player.isDraw(dealerScore));
+    }
+
+    @Test
+    @DisplayName("플레이어가 딜러와 점수가 같지 않을 경우 isDraw은 False를 반환한다.")
+    void isDrawFalseTest() {
+        Player player = new Player("hardy");
+        player.drawCard(Card.getCard(Suit.DIAMOND, Rank.QUEEN));
+        player.drawCard(Card.getCard(Suit.DIAMOND, Rank.KING));
+
+        int dealerScore = 19;
+        assertFalse(player.isDraw(dealerScore));
     }
 }
