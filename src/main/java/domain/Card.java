22b8a60bb5496e99b85f15b79bbf1d1be165d44e
jaeyeon kim <jakind@naver.com>
2023-03-06 20:20:39 +0900

    refactor: VO 객체에 Equals, HashCode 를 오버라이딩한다.
    - domain.Card equals(), hashCode()

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -3,6 +3,8 @@ package domain;
 import type.Letter;
 import type.Shape;
 
+import java.util.Objects;
+
 public class Card {
 
     private final Shape shape;
@@ -27,6 +29,22 @@ public class Card {
 
     public boolean isAce() {
         return letter == Letter.ACE;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (getClass() != object.getClass()) {
+            return false;
+        }
+
+        Card card = (Card) object;
+        return this.shape == card.shape
+                && this.letter == card.letter;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(shape, letter);
     }
 
 }
