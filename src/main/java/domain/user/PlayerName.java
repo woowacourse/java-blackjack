3c480fd4fb454b69ef61d07b3ed6d33b552abb9b
woosung1223 <dntjd991223@naver.com>
2023-03-06 16:17:30 +0900

    refactor: 리터럴, 매직 넘버 상수화

--- src/main/java/domain/user/PlayerName.java
+++ src/main/java/domain/user/PlayerName.java
@@ -2,6 +2,11 @@ package domain.user;
 
 public class PlayerName {
 
+    private static final String NAME_LENGTH_EXCEED_MESSAGE = "[ERROR] 허용 가능한 이름 크기를 벗어났습니다.";
+
+    private static final int NAME_MIN_LENGTH = 1;
+    private static final int NAME_MAX_LENGTH = 5;
+
     private final String name;
 
     public PlayerName(final String name) {
@@ -10,16 +15,12 @@ public class PlayerName {
     }
 
     private void validate(final String target) {
-        if (target.length() < 1 || target.length() > 5) {
-            throw new IllegalArgumentException("[ERROR]");
+        if (target.length() < NAME_MIN_LENGTH || target.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(NAME_LENGTH_EXCEED_MESSAGE);
         }
     }
 
     public String getName() {
         return name;
-    }
-
-    public boolean isSameWith(final String otherName) {
-        return otherName.equals(this.name);
     }
 }
