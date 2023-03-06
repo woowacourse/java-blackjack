0fbf655bd315a0b544bdd4afe4c0722aac35820c
iamjooon2 <iamjooon2@gmail.com>
2023-03-06 16:14:44 +0900

    refactor: 덱에서 카드 생성시 캐싱을 사용하도록 변경

--- src/main/java/domain/card/Deck.java
+++ src/main/java/domain/card/Deck.java
@@ -1,5 +1,6 @@
 package domain.card;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -9,6 +10,14 @@ import java.util.stream.Collectors;
 
 public class Deck {
     private static final String ERROR_EMPTY_CARDS = "[ERROR] 뽑을 수 있는 카드가 존재하지 않습니다";
+    private static final List<Card> CARDS;
+
+    static {
+        CARDS = Arrays.stream(Denomination.values())
+                .flatMap(denomination -> Arrays.stream(Suit.values())
+                        .map(suit -> new Card(denomination, suit)))
+                .collect(Collectors.toUnmodifiableList());
+    }
 
     private final Queue<Card> cards;
 
@@ -17,17 +26,10 @@ public class Deck {
     }
 
     public static Deck create() {
-        List<Card> initialCards = generateCards();
+        List<Card> initialCards = new ArrayList<>(CARDS);
         Collections.shuffle(initialCards);
 
         return new Deck(new LinkedList<>(initialCards));
-    }
-
-    private static List<Card> generateCards() {
-        return Arrays.stream(Denomination.values())
-                .flatMap(denomination -> Arrays.stream(Suit.values())
-                        .map(suit -> new Card(denomination, suit)))
-                .collect(Collectors.toList());
     }
 
     public Card pollAvailableCard() {
