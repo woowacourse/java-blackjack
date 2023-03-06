e796476064ea68bf1821718593df4f2a54fc4f7e
yujamint <woawoa0513@gmail.com>
2023-03-06 15:58:34 +0900

    refactor: Pattern 테스트 코드 가독성 개선
    - 검증 비교 대상이 될 변수명을 expectedPatterns로 수정
    - 기존에는 IntStream을 돌며 인덱스끼리 비교를 하던 방식으로 테스트 진행
    - containsOnly 메서드를 통해 검증하도록 수정

--- src/test/java/domain/PatternTest.java
+++ src/test/java/domain/PatternTest.java
@@ -1,9 +1,8 @@
 package domain;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,10 +11,12 @@ public class PatternTest {
     @Test
     @DisplayName("블랙잭 게임 카드 문양은 하트, 다이아몬드, 클로버, 스페이드의 값만 가진다.")
     void generatePatternTest() {
-        List<Pattern> patterns = new ArrayList<>(Arrays.asList(Pattern.values()));
-        List<String> answers = List.of("하트", "다이아몬드", "클로버", "스페이드");
+        List<String> patterns = Arrays.stream(Pattern.values())
+                .map(Pattern::getPattern)
+                .collect(Collectors.toList());
 
-        IntStream.range(0, answers.size())
-                .forEach(index -> Assertions.assertThat(patterns.get(index).getPattern()).isEqualTo(answers.get(index)));
+        String[] expectedPatterns = {"하트", "다이아몬드", "클로버", "스페이드"};
+
+        Assertions.assertThat(patterns).containsOnly(expectedPatterns);
     }
 }
