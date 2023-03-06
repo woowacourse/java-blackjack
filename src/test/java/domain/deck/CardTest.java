f1feccaa90ceb6815f22bb693b96b68d63e6b159
JunDonghyuk <ehdgur4814@naver.com>
2023-03-06 13:53:56 +0900

    test: Card 한 개의 인스턴스 보장 테스트 추가

--- src/test/java/domain/deck/CardTest.java
+++ src/test/java/domain/deck/CardTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
 
-    @Test
     @DisplayName("Suit과 Rank가 똑같은 카드는 두 번 생성되지 않는다.")
+    @Test
     void getCardTest() {
         Card card1 = Card.getCard(Suit.DIAMOND, Rank.ACE);
         Card card2 = Card.getCard(Suit.DIAMOND, Rank.ACE);
