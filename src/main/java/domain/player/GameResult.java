7f146526d4a62f1fa5cadffdb9dcf2f92c2c13ab
JunH <tjdtls690@gmail.com>
2023-03-06 14:47:48 +0900

    :art: refactor : 쓰이지 않는 메서드 제거

--- src/main/java/domain/player/GameResult.java
+++ src/main/java/domain/player/GameResult.java
@@ -2,16 +2,4 @@ package domain.player;
 
 public enum GameResult {
     WIN, LOSE, DRAW;
-
-    public boolean isWinning() {
-        return this == WIN;
-    }
-
-    public boolean isLose() {
-        return this == LOSE;
-    }
-
-    public boolean isDraw() {
-        return this == DRAW;
-    }
 }
