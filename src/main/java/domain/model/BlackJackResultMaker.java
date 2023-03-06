5a3a172aad59f4c93607dd62a26c351915a1d361
ChoiYoungHoon <dudgns2865@naver.com>
2023-03-06 20:49:46 +0900

    refactor: Result에 decide를 BlackJackResultMaker로 책임 전환

--- src/main/java/domain/model/BlackJackResultMaker.java
+++ src/main/java/domain/model/BlackJackResultMaker.java
@@ -10,14 +10,51 @@ public class BlackJackResultMaker {
     public Map<Player, Result> makePlayersResult(final Dealer dealer, final List<Player> players) {
         return players.stream()
             .collect(Collectors.toMap(player -> player,
-                player -> Result.decide(player.getScore(), dealer.getScore()), (a, b) -> b,
-                LinkedHashMap::new));
+                player -> decide(player.getScore(), dealer.getScore()), (a, b) -> b, LinkedHashMap::new));
+    }
+
+    private Result decide(final Score score, final Score comparedScore) {
+        if (isVictory(score, comparedScore)) {
+            return new Result(1, 0, 0);
+        }
+        if (isDraw(score, comparedScore)) {
+            return new Result(0, 1, 0);
+        }
+        return new Result(0, 0, 1);
+    }
+
+    private boolean isVictory(final Score score, final Score comparedScore) {
+        return !score.isBust()
+            && (comparedScore.isBust() || score.getValue() > comparedScore.getValue());
+    }
+
+    private boolean isDraw(final Score score, final Score comparedScore) {
+        return score.isBust()
+            && comparedScore.isBust() || score.getValue() == comparedScore.getValue();
     }
 
     public Result makeDealerResult(final Dealer dealer, final List<Player> players) {
         final List<Score> scores = players.stream()
             .map(Player::getScore)
             .collect(Collectors.toList());
-        return Result.decide(dealer.getScore(), scores);
+        return decide(dealer.getScore(), scores);
+    }
+
+    private Result decide(final Score score, final List<Score> comparedScores) {
+        Result result = new Result(0, 0, 0);
+        comparedScores.forEach(comparedScore -> addResult(score, comparedScore, result));
+        return result;
+    }
+
+    private void addResult(final Score score, final Score comparedScore, final Result result) {
+        if (isDraw(score, comparedScore)) {
+            result.addDraw();
+            return;
+        }
+        if (isVictory(score, comparedScore)) {
+            result.addVictory();
+            return;
+        }
+        result.addDefeat();
     }
 }
--- src/main/java/domain/model/Result.java
+++ src/main/java/domain/model/Result.java
@@ -1,55 +1,15 @@
 package domain.model;
 
-import java.util.List;
-
 public class Result {
 
     private int victory;
     private int draw;
     private int defeat;
 
-    private Result(final int victory, final int draw, final int defeat) {
+    public Result(final int victory, final int draw, final int defeat) {
         this.victory = victory;
         this.draw = draw;
         this.defeat = defeat;
-    }
-
-    public static Result decide(final Score score, final Score comparedScore) {
-        if (isVictory(score, comparedScore)) {
-            return new Result(1, 0, 0);
-        }
-        if (isDraw(score, comparedScore)) {
-            return new Result(0, 1, 0);
-        }
-        return new Result(0, 0, 1);
-    }
-
-    private static boolean isDraw(final Score score, final Score comparedScore) {
-        return score.isBust() && comparedScore.isBust()
-            || score.getValue() == comparedScore.getValue();
-    }
-
-    private static boolean isVictory(final Score score, final Score comparedScore) {
-        return !score.isBust()
-            && (comparedScore.isBust() || score.getValue() > comparedScore.getValue());
-    }
-
-    public static Result decide(final Score score, final List<Score> comparedScores) {
-        Result result = new Result(0, 0, 0);
-        comparedScores.forEach(comparedScore -> addResult(score, comparedScore, result));
-        return result;
-    }
-
-    private static void addResult(final Score score, final Score comparedScore, final Result result) {
-        if (isDraw(score, comparedScore)) {
-            result.addDraw();
-            return;
-        }
-        if (isVictory(score, comparedScore)) {
-            result.addVictory();
-            return;
-        }
-        result.addDefeat();
     }
 
     public void addVictory() {
