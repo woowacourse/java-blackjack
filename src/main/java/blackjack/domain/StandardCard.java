312b7e0a3b5fd90429dacc18e2e9c7ff681b754b
parkmuhyeun <pjhg410@gmail.com>
2023-03-06 20:58:42 +0900

    feat: StandardCard Symbol 예외처리 기능 추가

--- src/main/java/blackjack/domain/StandardCard.java
+++ src/main/java/blackjack/domain/StandardCard.java
@@ -2,8 +2,19 @@ package blackjack.domain;
 
 class StandardCard extends Card {
 
+    private static final int MIN_INCLUSIVE = 2;
+    private static final int MAX_INCLUSIVE = 10;
+
     public StandardCard(Pattern pattern, String symbol) {
         super(pattern, symbol);
+    }
+
+    @Override
+    protected void validateSymbol(String symbol) {
+        Integer value = Integer.valueOf(symbol);
+        if (value < MIN_INCLUSIVE || value > MAX_INCLUSIVE) {
+            throw new IllegalArgumentException(String.format("심볼은 2 ~ 10 사이 숫자여야 합니다. 입력된 값 : %d", value));
+        }
     }
 
     @Override
