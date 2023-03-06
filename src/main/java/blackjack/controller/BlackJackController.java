37d526c9cca507e138c3da2b62ffc4613bb265c1
odo27 <odo27@naver.com>
2023-03-06 15:35:42 +0900

    refactor: 기능에 따라 메서드 분리

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -14,17 +14,14 @@ public class BlackJackController {
     private BlackJackGame blackJackGame;
 
     public void run() {
+        generateGame();
+        startGame();
+        showResult();
+    }
+
+    private void generateGame() {
         List<String> playerNames = InputView.askPlayerNames();
         blackJackGame = new BlackJackGame(new BlackJackDeckGenerator(), playerNames);
-
-        startGame();
-        hitOrStayForAvailablePlayers(blackJackGame.findAvailablePlayerNames());
-        int hitCount = blackJackGame.hitOrStayForDealer();
-        OutputView.showDealerHitResult(hitCount);
-        OutputView.showParticipantGameResults(blackJackGame.computeDealerGameResult(),
-                blackJackGame.computePlayerGameResults());
-
-        OutputView.showFinalResult(blackJackGame.computePlayerWinResults());
     }
 
     private void startGame() {
@@ -32,6 +29,16 @@ public class BlackJackController {
         Card dealerFirstCard = blackJackGame.openDealerFirstCard();
         Map<String, List<Card>> playersCards = blackJackGame.openPlayersCards();
         OutputView.showOpenCards(dealerFirstCard, playersCards);
+        hitOrStayForAvailablePlayers(blackJackGame.findAvailablePlayerNames());
+        int hitCount = blackJackGame.hitOrStayForDealer();
+        OutputView.showDealerHitResult(hitCount);
+    }
+
+    private void showResult() {
+        OutputView.showParticipantGameResults(blackJackGame.computeDealerGameResult(),
+                blackJackGame.computePlayerGameResults());
+
+        OutputView.showFinalResult(blackJackGame.computePlayerWinResults());
     }
 
     private void hitOrStayForAvailablePlayers(List<String> playerNames) {
