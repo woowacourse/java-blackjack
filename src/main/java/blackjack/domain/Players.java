9bb0c5cb0825717635a3d63bc8d2baa5077c0750
yoondgu <doyou2ing@naver.com>
2023-03-07 01:08:16 +0900

    feat: 플레이어 인원수 검증 로직 구현

--- src/main/java/blackjack/domain/Players.java
+++ src/main/java/blackjack/domain/Players.java
@@ -4,11 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Players {
-    
+
+    private final static int COUNT_MINIMUM = 2;
+    private final static int COUNT_MAXIMUM = 8;
+
     private final List<Player> players;
 
     public Players(List<Player> players) {
+        validatePlayersCount(players);
         this.players = players;
+    }
+
+    private void validatePlayersCount(List<Player> players) {
+        if (players.size() < COUNT_MINIMUM || players.size() > COUNT_MAXIMUM) {
+            throw new IllegalArgumentException("플레이어 인원 수는 최소 " + COUNT_MINIMUM + "명 최대 " + COUNT_MAXIMUM + "명입니다.");
+        }
     }
 
     public List<String> findAvailablePlayerNames() {
---
+++ src/test/java/blackjack/domain/PlayersTest.java
@@ -0,0 +1,32 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class PlayersTest {
+
+    private static Stream<List<Player>> playersCountOutOfBounds() {
+        return Stream.of(
+                List.of(new Player("test")),
+                Collections.nCopies(9, new Player("test"))
+        );
+    }
+
+    @DisplayName("플레이어 인원수는 최소 2명, 최대 8명이다.")
+    @ParameterizedTest(name = "인원수 예외 케이스 {index}")
+    @MethodSource("playersCountOutOfBounds")
+    void should_ThrowException_When_PlayersCountOutOfBounds(List<Player> players) {
+        assertThatThrownBy(() -> new Players(players))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("플레이어 인원 수는 최소 2명 최대 8명입니다.");
+    }
+}
