f23cdacc24e859386107af488796b12aca458c41
eunbii0213 <eunbii0213@naver.com>
2023-03-06 17:53:50 +0900

    refactor: main method 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,11 @@
 import controller.Controller;
+import controller.EntityCreator;
 
 public class Application {
     public static void main(String[] args) {
         Controller controller = new Controller();
+        EntityCreator entityCreator = new EntityCreator();
 
-        controller.start();
+        controller.startGame(entityCreator.getPlayers(), entityCreator.getDealer());
     }
 }
