11ca69f87d13b893040f6b17fb8a2b824ab73abe
yujamint <woawoa0513@gmail.com>
2023-03-06 21:14:01 +0900

    feat: 무승부 결과 추가 구현에 따른 테스트 작성

--- src/test/java/domain/game/BlackJackGameTest.java
+++ src/test/java/domain/game/BlackJackGameTest.java
@@ -50,91 +50,127 @@ class BlackJackGameTest {
         blackJackGame.drawDefaultCard();
 
         assertThat(player.getCards()).isEqualTo(List.of(CloverCard.CLOVER_FIVE, HeartCard.HEART_JACK));
-        assertThat(dealer.getCards()).isEqualTo(List.of(SpadeCard.SPADE_FOUR, DiamondCard.DIAMOND_SEVEN));
+        assertThat(dealer.getCards()).isEqualTo(List.of(SpadeCard.SPADE_FIVE, DiamondCard.DIAMOND_SEVEN));
     }
 
     @DisplayName("딜러는 합산 점수가 17 이상이 될 때까지 카드를 뽑는다")
     @Test
     void drawDealerCardUntilSatisfyingConditionTest() {
         Dealer dealer = blackJackGame.getDealer();
-        blackJackGame.drawDealerCardUntilSatisfyingCondition();
+        List<Card> expectedCards = List.of(CloverCard.CLOVER_FIVE, HeartCard.HEART_JACK, SpadeCard.SPADE_FIVE);
 
-        List<Card> expectedCards = List.of(CloverCard.CLOVER_FIVE, HeartCard.HEART_JACK, SpadeCard.SPADE_FOUR);
+        blackJackGame.drawDealerCardUntilSatisfyingCondition();
 
         assertThat(dealer.getCards()).isEqualTo(expectedCards);
     }
 
-    @DisplayName("플레이어가 버스트일 경우, 딜러가 승리한다.")
+    @DisplayName("플레이어만 버스트일 경우, 딜러가 승리한다.")
     @Test
     void judgeWinnerTest_playerBust() {
-        Map<Boolean, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
+        Map<Result, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
         Player player = blackJackGame.getPlayers().get(0);
-        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 , 딜러 : 4스페이드, 7다이아몬드
-        blackJackGame.drawOneMoreCard(player); // 플레이어 : 5클로버, J하트, A하트
-        blackJackGame.drawOneMoreCard(player); // 플레이어 : 5클로버, J하트, A하트, 6클로버
+        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 , 딜러 : 5스페이드, 7다이아몬드
+        player.receiveCard(DiamondCard.DIAMOND_JACK); // 플레이어 : 5클로버, J하트, J다이아몬드
 
         blackJackGame.judgeWinner();
 
-        assertThat(player.isWinner()).isFalse();
-        assertThat(winningRecord.get(true)).isEqualTo(1);
-        assertThat(winningRecord.get(false)).isEqualTo(0);
+        assertThat(player.getResult()).isEqualTo(Result.LOSE);
+        assertThat(winningRecord.get(Result.WIN)).isEqualTo(1);
+        assertThat(winningRecord.get(Result.DRAW)).isEqualTo(0);
+        assertThat(winningRecord.get(Result.LOSE)).isEqualTo(0);
     }
 
-    @DisplayName("플레이어는 버스트가 아니고, 딜러는 버스트일 경우 플레이어가 승리한다.")
+    @DisplayName("딜러만 버스트일 경우 플레이어가 승리한다.")
     @Test
     void judgeWinnerTest_playerNormalAndDealerBust() {
-        Map<Boolean, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
+        Map<Result, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
         Player player = blackJackGame.getPlayers().get(0);
         Dealer dealer = blackJackGame.getDealer();
-        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 , 딜러 : 4스페이드, 7다이아몬드
-        dealer.receiveCards(List.of(HeartCard.HEART_TEN, DiamondCard.DIAMOND_TEN)); // 딜러 : 4스페이드, 7다이아몬드, 10하트, 10다이아몬드
-
+        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 , 딜러 : 5스페이드, 7다이아몬드
+        dealer.receiveCards(List.of(HeartCard.HEART_TEN, DiamondCard.DIAMOND_TEN)); // 딜러 : 5스페이드, 7다이아몬드, 10하트, 10다이아몬드
 
         blackJackGame.judgeWinner();
 
-        assertThat(player.isWinner()).isTrue();
-        assertThat(winningRecord.get(true)).isEqualTo(0);
-        assertThat(winningRecord.get(false)).isEqualTo(1);
+        assertThat(player.getResult()).isEqualTo(Result.WIN);
+        assertThat(winningRecord.get(Result.WIN)).isEqualTo(0);
+        assertThat(winningRecord.get(Result.DRAW)).isEqualTo(0);
+        assertThat(winningRecord.get(Result.LOSE)).isEqualTo(1);
     }
 
-    @DisplayName("딜러의 점수가 플레이어의 점수보다 높을 경우, 딜러가 승리한다.")
+    @DisplayName("딜러와 플레이어 모두 버스트가 아니고, 딜러의 점수가 플레이어의 점수보다 높으면 딜러가 승리한다.")
     @Test
     void judgeWinnerTest_dealerWin() {
-        Map<Boolean, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
+        Map<Result, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
         Player player = blackJackGame.getPlayers().get(0);
-        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 , 딜러 : 4스페이드, 7다이아몬드
-        blackJackGame.drawDealerCardUntilSatisfyingCondition(); // 딜러 : 4스페이드, 7다이아몬드, A하트, 6클로버
+        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 , 딜러 : 5스페이드, 7다이아몬드
+        blackJackGame.drawDealerCardUntilSatisfyingCondition(); // 딜러 : 5스페이드, 7다이아몬드, A하트, 6클로버
 
         blackJackGame.judgeWinner();
 
-        assertThat(player.isWinner()).isFalse();
-        assertThat(winningRecord.get(true)).isEqualTo(1);
-        assertThat(winningRecord.get(false)).isEqualTo(0);
+        assertThat(player.getResult()).isEqualTo(Result.LOSE);
+        assertThat(winningRecord.get(Result.WIN)).isEqualTo(1);
+        assertThat(winningRecord.get(Result.DRAW)).isEqualTo(0);
+        assertThat(winningRecord.get(Result.LOSE)).isEqualTo(0);
     }
 
-    @DisplayName("플레이어의 점수가 딜러의 점수보다 높을 경우, 플레이어가 승리한다.")
+    @DisplayName("딜러와 플레이어 모두 버스트가 아니고, 플레이어의 점수가 딜러의 점수보다 높으면 플레이어가 승리한다.")
     @Test
     void judgeWinnerTest_playerWin() {
-        Map<Boolean, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
+        Map<Result, Integer> winningRecord = blackJackGame.getDealer().getWinningRecord();
         Player player = blackJackGame.getPlayers().get(0);
-        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 , 딜러 : 4스페이드, 7다이아몬드
+        blackJackGame.drawDefaultCard(); // 플레이어 : 5클로버, J하트 / 딜러 : 5스페이드, 7다이아몬드
 
         blackJackGame.judgeWinner();
 
-        assertThat(player.isWinner()).isTrue();
-        assertThat(winningRecord.get(true)).isEqualTo(0);
-        assertThat(winningRecord.get(false)).isEqualTo(1);
+        assertThat(player.getResult()).isEqualTo(Result.WIN);
+        assertThat(winningRecord.get(Result.WIN)).isEqualTo(0);
+        assertThat(winningRecord.get(Result.DRAW)).isEqualTo(0);
+        assertThat(winningRecord.get(Result.LOSE)).isEqualTo(1);
+    }
+
+    @DisplayName("딜러와 플레이어 모두 버스트면 무승부다.")
+    @Test
+    void judgeWinnerTest_bothBust() {
+        Dealer dealer = blackJackGame.getDealer();
+        Map<Result, Integer> winningRecord = dealer.getWinningRecord();
+        Player player = blackJackGame.getPlayers().get(0);
+
+        player.receiveCards(List.of(CloverCard.CLOVER_JACK, CloverCard.CLOVER_QUEEN, CloverCard.CLOVER_KING));
+        dealer.receiveCards(List.of(DiamondCard.DIAMOND_JACK, DiamondCard.DIAMOND_QUEEN, DiamondCard.DIAMOND_KING));
+        blackJackGame.judgeWinner();
+
+        assertThat(player.getResult()).isEqualTo(Result.DRAW);
+        assertThat(winningRecord.get(Result.WIN)).isEqualTo(0);
+        assertThat(winningRecord.get(Result.DRAW)).isEqualTo(1);
+        assertThat(winningRecord.get(Result.LOSE)).isEqualTo(0);
+    }
+
+    @DisplayName("딜러와 플레이어의 점수가 같으면 무승부다.")
+    @Test
+    void judgeWinnerTest_bothNormalSameScore() {
+        Dealer dealer = blackJackGame.getDealer();
+        Map<Result, Integer> winningRecord = dealer.getWinningRecord();
+        Player player = blackJackGame.getPlayers().get(0);
+
+        player.receiveCard(CloverCard.CLOVER_TEN); // 플레이어 : 10클로버
+        dealer.receiveCard(DiamondCard.DIAMOND_TEN); // 딜러 : 10다이아몬드
+        blackJackGame.judgeWinner();
+
+        assertThat(player.getResult()).isEqualTo(Result.DRAW);
+        assertThat(winningRecord.get(Result.WIN)).isEqualTo(0);
+        assertThat(winningRecord.get(Result.DRAW)).isEqualTo(1);
+        assertThat(winningRecord.get(Result.LOSE)).isEqualTo(0);
     }
 
     private class FixedCardsGenerator implements CardsGenerator {
         /*
-         5클로버, J하트, 4스페이드, 7다이아몬드, A하트, 6클로버, 8클로버, 9다이아몬드, 7하트
+         5클로버, J하트, 5스페이드, 7다이아몬드, A하트, 6클로버, 8클로버, 9다이아몬드, 7하트
          위 순서대로 담겨져 있는 List<Card>를 반환
          */
         @Override
         public List<Card> generate() {
             return new ArrayList<>(
-                    List.of(CloverCard.CLOVER_FIVE, HeartCard.HEART_JACK, SpadeCard.SPADE_FOUR,
+                    List.of(CloverCard.CLOVER_FIVE, HeartCard.HEART_JACK, SpadeCard.SPADE_FIVE,
                             DiamondCard.DIAMOND_SEVEN, HeartCard.HEART_ACE, CloverCard.CLOVER_SIX, CloverCard.CLOVER_EIGHT,
                             DiamondCard.DIAMOND_NINE, HeartCard.HEART_SEVEN)
             );
