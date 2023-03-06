d9b2a8424352681c001527bf37f08bd76f93c5e4
jaeyeon kim <jakind@naver.com>
2023-03-06 17:18:31 +0900

    refactor: Cards 를 캐싱해놓는다.
    - domain.util.CardDeckMaker generate()

--- src/main/java/util/CardDeckMaker.java
+++ src/main/java/util/CardDeckMaker.java
@@ -10,15 +10,19 @@ import java.util.List;
 
 public class CardDeckMaker {
 
-    public static List<Card> generate() {
-        List<Card> cards = new ArrayList<>();
+    private static final List<Card> cards;
+
+    static {
+        cards = new ArrayList<>();
 
         for (Shape shape : Shape.values()) {
             Arrays.stream(Letter.values())
                     .forEach(value -> cards.add(new Card(shape, value)));
         }
+    }
 
-        return cards;
+    public static List<Card> generate() {
+        return new ArrayList<>(cards);
     }
 
 }
