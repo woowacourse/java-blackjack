8f424c12d67959be87890da571dc1cd7f2647a5b
kdkdhoho <hkim4410@naver.com>
2023-03-07 14:20:59 +0900

    test: 불필요한 import문 제거

--- src/test/java/blackjack/domain/card/NumberTest.java
+++ src/test/java/blackjack/domain/card/NumberTest.java
@@ -1,13 +1,10 @@
 package blackjack.domain.card;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class NumberTest {
 
