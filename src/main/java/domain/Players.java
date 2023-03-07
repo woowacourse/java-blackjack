6c83a1f9a746d915679f56d6b9de53a583c0c054
cpot5620 <=cpot5620@naver.com>
2023-03-07 19:28:17 +0900

    feat: Players 정적 팩토리 메서드 추가

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// TODO: 2023-03-07 정적 팩터리 메서드에 BetAmount 추가
 public class Players {
     private final List<Player> players;
 
@@ -16,6 +17,10 @@ public class Players {
                 .stream()
                 .map(Player::from)
                 .collect(Collectors.toList()));
+    }
+
+    public static Players from(List<Player> players) {
+        return new Players(players);
     }
 
     public List<Player> getPlayers() {
