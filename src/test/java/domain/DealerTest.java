7d78a1591b3e3a0670fec7492b13db4045f1b5b3
cpot5620 <=cpot5620@naver.com>
2023-03-07 20:24:42 +0900

    test: 카드 합이 같더라도 블랙잭이면 승리하는  테스트 구현

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -1,6 +1,5 @@
 package domain;
 
-import domain.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,13 +9,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DealerTest {
 
+    private static final Card DIAMOND_ACE = Card.of(CardShape.DIAMOND, CardRank.ACE);
     private static final Card DIAMOND_TEN = Card.of(CardShape.DIAMOND, CardRank.TEN);
     private static final Card DIAMOND_NINE = Card.of(CardShape.DIAMOND, CardRank.NINE);
     private static final Card DIAMOND_EIGHT = Card.of(CardShape.DIAMOND, CardRank.EIGHT);
+    private static final Card DIAMOND_FOUR = Card.of(CardShape.DIAMOND, CardRank.FOUR);
 
     @DisplayName("딜러의 카드 합을 기준으로 플레이어의 승패를 결정한다.")
     @Test
-    void resultSuccessTest() {
+    void decideResultSuccessTest() {
         Dealer dealer = new Dealer();
         Players players = Players.from(Names.from(List.of("pobi", "crong")));
         Player pobi = players.getPlayers().get(0);
@@ -31,6 +32,22 @@ class DealerTest {
 
         assertThat(dealer.getGameResult().values())
                 .containsExactly(Result.LOSE, Result.WIN);
+    }
+
+    @DisplayName("카드의 합이 같더라도, 딜러가 블랙잭일 경우, 딜러가 승리한다. ")
+    @Test
+    void decideResultWithBlackjackTest() {
+        Dealer dealer = new Dealer();
+        Players players = Players.from(Names.from(List.of("pobi")));
+        Player pobi = players.getPlayers().get(0);
+
+        giveCardsTo(dealer, List.of(DIAMOND_ACE, DIAMOND_TEN));
+        giveCardsTo(pobi, List.of(DIAMOND_NINE, DIAMOND_EIGHT, DIAMOND_FOUR));
+
+        dealer.decideResults(players);
+
+        assertThat(dealer.getGameResult().values())
+                .containsExactly(Result.WIN);
     }
 
     public void giveCardsTo(Participant participant, List<Card> cards) {
