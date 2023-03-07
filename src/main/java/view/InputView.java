36363b3ef8e35e29d79cacbc6731742c2f66fab1
yeonkkk <tjddus0302@gmail.com>
2023-03-07 20:46:38 +0900

    feat: 입력 값 검증 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,35 +3,57 @@ package view;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final String ENTER_PLAYER_NAME_NOTICE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String ENTER_ADD_CARD_NOTICE = "\n%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
     private static final String DELIMITER = ",";
     private static final String YES = "y";
+    private static final String NO = "n";
+    private static final int MIN_PLAYER_NUM = 1;
+    private static final int MAX_PLAYER_NUM = 10;
 
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> readNames() {
         System.out.println(ENTER_PLAYER_NAME_NOTICE);
-
         String input = scanner.nextLine();
-        return splitWithComma(input);
+
+        List<String> playerNames = splitWithComma(input);
+        validatePlayerSize(playerNames.size());
+
+        return playerNames;
     }
 
     public boolean askForAnotherCard(String name) {
         System.out.printf(ENTER_ADD_CARD_NOTICE, name);
-        String input = scanner.nextLine();
+        String input = scanner.nextLine().strip();
+        validateResponse(input);
 
-        if (input.equals(YES)) {
-            return true;
-        }
+        return isYes(input);
+    }
 
-        return false;
+    private boolean isYes(String input) {
+        return input.equals(YES);
     }
 
     private List<String> splitWithComma(String input) {
-        return Arrays.asList(input.split(DELIMITER));
+        return Arrays.stream(input.split(DELIMITER))
+                        .map(String::strip)
+                .collect(Collectors.toList());
+    }
+
+    private void validatePlayerSize(int size) {
+        if (size < MIN_PLAYER_NUM || size > MAX_PLAYER_NUM) {
+            throw new IllegalArgumentException("플레이어는 1명 이상 10명 이하여야 합니다.");
+        }
+    }
+
+    private void validateResponse(String response) {
+        if (!(response.equals(NO) || response.equals(YES))) {
+            throw new IllegalArgumentException("추가 카드에 대한 응답은 y와 n으로 해야합니다.");
+        }
     }
 
 }
