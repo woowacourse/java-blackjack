faf50bdc7e394286079666f1dc00103a9ac8e14a
seokho <kim76744dev@gmail.com>
2023-03-07 14:33:35 +0900

    test: canReceiveCardTest에서 true가 나오는 테스트 추가

--- src/test/java/model/DealerTest.java
+++ src/test/java/model/DealerTest.java
@@ -7,10 +7,16 @@ import model.user.Dealer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static model.card.Shape.DIAMOND;
 import static model.card.Value.KING;
 import static model.card.Value.SEVEN;
+import static model.card.Value.SIX;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DealerTest {
@@ -21,15 +27,24 @@ class DealerTest {
     void init() {
         dealer = new Dealer();
     }
-    @Test
+
+    @ParameterizedTest
+    @MethodSource("receiveCardTestValues")
     @DisplayName("딜러의 카드 총 합이 16 이하인지 판단한다.")
-    void canReceiveCardTest() {
+    void canReceiveCardTest(final Card firstCard, final Card secondCard, final boolean result) {
         // given
-        dealer.receiveCard(new Card(DIAMOND, SEVEN));
-        dealer.receiveCard(new Card(DIAMOND, KING));
+        dealer.receiveCard(firstCard);
+        dealer.receiveCard(secondCard);
 
         // when, then
-        assertThat(dealer.canReceiveCard()).isFalse();
+        assertThat(dealer.canReceiveCard()).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> receiveCardTestValues() {
+        return Stream.of(
+                Arguments.of(new Card(DIAMOND, SIX), new Card(DIAMOND, KING), true),
+                Arguments.of(new Card(DIAMOND, SEVEN), new Card(DIAMOND, KING), false)
+        );
     }
 
     @DisplayName("receiveInitialCards가 두 장의 카드를 주는지 확인한다.")
