5450578f08c05886fd851b6f9a5cf68c54565b4d
SproutMJ <mjk54565@gmail.com>
2023-03-07 22:43:26 +0900

    test: Symbol을 직관적인 테스트로 변경

--- src/test/java/domain/SymbolTest.java
+++ src/test/java/domain/SymbolTest.java
@@ -15,10 +15,6 @@ class SymbolTest {
     @DisplayName("하트, 스페이드, 다이아몬드, 클로버가 존재한다.")
     @Test
     void 하트_스페이드_다이아몬드_클로버_존재(){
-        String[] names = {"하트", "스페이드", "다이아몬드", "클로버"};
-        List<String> namesInSymbol = Arrays.stream(Symbol.values())
-                .map(Symbol::getName)
-                .collect(Collectors.toList());
-        assertThat(namesInSymbol).containsExactlyInAnyOrder(names);
+        assertThat(Symbol.values().length).isEqualTo(4);
     }
 }
