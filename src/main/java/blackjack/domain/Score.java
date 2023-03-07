82b7cb2ee7ad70f815f3f1df2bf75b99c65e6405
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-03-07 23:15:50 +0900

    refactor: 주어진 카드의 점수를 계산하는 기능 구현
    - 점수 계산 방식 변경: 통일되지 않은 점수 계산 방식 -> 딜러와 플레이어에게 일관성 있는 점수 계산법 적용

--- src/main/java/blackjack/domain/Score.java
+++ src/main/java/blackjack/domain/Score.java
@@ -1,10 +1,45 @@
 package blackjack.domain;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardNumber;
+
+import java.util.Collections;
+import java.util.List;
+
+
 public class Score {
+
+    private static final int MAX_ACE_SCORE = 11;
+    private static final int DIFFERENCE_WITH_ACE_NUMBER = 10;
 
     private final int score;
 
-    public Score(int score) {
+    private Score(int score) {
         this.score = score;
+    }
+
+    public static Score from(List<Card> cards) {
+        int totalScore = getTotalScore(cards);
+
+        if (totalScore <= MAX_ACE_SCORE && hasACE(cards)) {
+            return new Score(totalScore + DIFFERENCE_WITH_ACE_NUMBER);
+        }
+
+        return new Score(totalScore);
+    }
+
+    private static boolean hasACE(List<Card> cards) {
+        return cards.stream()
+                .anyMatch(card -> card.getCardNumberToString().equals(CardNumber.ACE.getNumber()));
+    }
+
+    private static int getTotalScore(List<Card> cards) {
+        return cards.stream()
+                .map(card -> Collections.min(card.getScore()))
+                .reduce(0, Integer::sum);
+    }
+
+    public int getScore() {
+        return score;
     }
 }
