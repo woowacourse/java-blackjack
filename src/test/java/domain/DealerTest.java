53d399d448bc9afbc7904404b231c41f0fbbceb1
0chil <0@chll.it>
2023-03-07 21:49:35 +0900

    refactor(DealerTest): 테스트 이름 개선

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class DealerTest extends AbstractTestFixture {
 
-    @ParameterizedTest(name = "Hit 가 가능한지 확인한다")
+    @ParameterizedTest(name = "점수가 17미만이면 더 뽑을 수 있고, 17이상이면 불가하다")
     @CsvSource({"8,9,false", "10,6,true"})
-    void test_over_16(String letter1, String letter2, boolean canHit) {
+    void test_can_hit(String letter1, String letter2, boolean canHit) {
         var dealer = new Dealer(createCards(letter1, letter2));
 
         assertThat(dealer.canHit()).isEqualTo(canHit);
