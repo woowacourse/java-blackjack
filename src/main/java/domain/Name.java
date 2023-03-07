4535d01ce033aa1ae3050fc56efad4c4f4964e37
SproutMJ <mjk54565@gmail.com>
2023-03-07 17:50:41 +0900

    refactor: Name에서 equals와 hashcode를 오버라이딩

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -19,4 +19,21 @@ public class Name {
     public String getValue() {
         return this.value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name = (Name) o;
+        return Objects.equals(value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
