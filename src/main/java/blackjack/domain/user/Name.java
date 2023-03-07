8aa8c84c44c9e02aa3b042546d55d9bcf2a6ef9d
hong-sile <gurwns9325@naver.com>
2023-03-08 04:37:00 +0900

    feat: 플레이어의 이름이 '딜러'인 경우 예외처리

--- src/main/java/blackjack/domain/user/Name.java
+++ src/main/java/blackjack/domain/user/Name.java
@@ -3,15 +3,23 @@ package blackjack.domain.user;
 public class Name {
 
     static final String BLANK_NAME_EXCEPTION_MESSAGE = "이름은 공백일 수 없습니다.";
+    static final String NAME_IS_DEALER_NAME_EXCEPTION_MESSAGE = "플레이어의 이름은 '딜러'일 수 없습니다.";
 
     private final String value;
 
-    public Name(String name) {
+    public Name(final String name) {
         validateBlank(name);
+        validateIsDealerName(name);
         this.value = name;
     }
 
-    private void validateBlank(String name) {
+    private void validateIsDealerName(final String name) {
+        if (Dealer.DEALER_NAME.equals(name)) {
+            throw new IllegalArgumentException(NAME_IS_DEALER_NAME_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void validateBlank(final String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException(BLANK_NAME_EXCEPTION_MESSAGE);
         }
--- src/test/java/blackjack/domain/user/NameTest.java
+++ src/test/java/blackjack/domain/user/NameTest.java
@@ -1,25 +1,33 @@
 package blackjack.domain.user;
 
-import blackjack.domain.user.Name;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class NameTest {
 
     @Test
     @DisplayName("이름 초기화 테스트")
     void nameInitTest() {
-        assertDoesNotThrow(() -> new Name("test"));
+        final Name name = new Name("test");
+        assertThat(name.getValue()).isEqualTo("test");
     }
 
     @Test
     @DisplayName("공백이 입력되면 예외 발생 테스트")
-    void blankNameExceptionTest() {
-        assertThatThrownBy(() -> new Name(" ")).isInstanceOf(IllegalArgumentException.class)
+    void throwExceptionIfNameIsBlank() {
+        assertThatThrownBy(() -> new Name(" "))
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(Name.BLANK_NAME_EXCEPTION_MESSAGE);
     }
 
+    @Test
+    @DisplayName("입력된 이름으로 딜러의 이름이 올 경우 예외처리")
+    void throwExceptionIfNameIsDealerName() {
+        assertThatThrownBy(() -> new Name(Dealer.DEALER_NAME))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Name.NAME_IS_DEALER_NAME_EXCEPTION_MESSAGE);
+    }
 }
