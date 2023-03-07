4f79b6424f8b5fdc2ff567363de4ea675161ac1e
cpot5620 <=cpot5620@naver.com>
2023-03-07 14:30:08 +0900

    feat: 배팅 금액 검증 기능 구현

--- src/main/java/domain/BetAmount.java
+++ src/main/java/domain/BetAmount.java
@@ -1,2 +1,19 @@
-package domain;public class BetAmount {
+package domain;
+
+public class BetAmount {
+    private static final int MINIMUM = 10;
+    private static final int MAXIMUM = 100_000_000;
+
+    private final int money;
+
+    public BetAmount(int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int money) {
+        if (money < MINIMUM || money > MAXIMUM) {
+            throw new IllegalArgumentException("배팅 금액은 10원 이상, 1억 이하여야 합니다.");
+        }
+    }
 }
