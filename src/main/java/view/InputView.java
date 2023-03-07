f5a0f319e3ec816ca5122e5c95e41fa25d9b75dc
MoonJeWoong <munjin0201@naver.com>
2023-03-08 00:42:44 +0900

    refactor: 입력된 이름 값이 5개를 초과할 경우 예외처리 기능 구현
    세부사항
    - 기존 inputView에서 수행하던 검증 기능을 Names 클래스로 이관

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,23 +7,14 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class InputView {
-    private static final int MAX_PLAYER_SIZE = 5;
-
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> readPlayersName() {
         List<String> playersName = Arrays.asList(scanner.nextLine().split(","));
-        validatePlayersSize(playersName);
 
         return playersName.stream()
                 .map(String::strip)
                 .collect(Collectors.toList());
-    }
-
-    private void validatePlayersSize(List<String> players) {
-        if (players.size() > MAX_PLAYER_SIZE) {
-            throw new IllegalArgumentException("[Error] 플레이어는 최대 5명입니다.");
-        }
     }
 
     public DrawCommand readDrawCommand() {
