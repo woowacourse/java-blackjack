46a2811d32280f47ca53a28ca53015594da6de58
cpot5620 <=cpot5620@naver.com>
2023-03-07 17:44:26 +0900

    feat: 배팅 금액 입력 로직 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String DELIMITER = ",";
+    private static final String NUMERIC = "^[0-9]*$";
 
     public List<String> requestPlayerNames() {
         System.out.println(Message.PLAYER_NAMES_INPUT.message);
@@ -24,9 +25,29 @@ public class InputView {
         return SCANNER.nextLine();
     }
 
+    public int requestBetAmount(String name) {
+        String format = String.format(Message.PLAYER_BET_AMOUNT.message, name);
+        System.out.println(format);
+
+        return readBetAmount();
+    }
+
+    private int readBetAmount() {
+        String input = SCANNER.nextLine();
+
+        if (!input.matches(NUMERIC)) {
+            throw new IllegalArgumentException(Message.NOT_NUMERIC_ERROR.message);
+        }
+
+        return Integer.parseInt(input);
+    }
+
     private enum Message {
         PLAYER_NAMES_INPUT("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"),
-        PLAYER_WANT_MORE_CARD("%s은(는) 한장의 카드를 더 받겠습니까?(예는 y, 아니오 n)");
+        PLAYER_WANT_MORE_CARD("%s은(는) 한장의 카드를 더 받겠습니까?(예는 y, 아니오 n)"),
+        PLAYER_BET_AMOUNT("%s의 배팅 금액은?"),
+        NOT_NUMERIC_ERROR("배팅 금액은 숫자만 입력해주세요");
+
 
         private final String message;
 
