22e63fcd215afeb501214053f05140440058aaca
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-03-07 16:37:32 +0900

    feat: 명령어를 찾는 기능 구현

--- src/main/java/blackjack/domain/Command.java
+++ src/main/java/blackjack/domain/Command.java
@@ -1,16 +1,27 @@
 package blackjack.domain;
 
+import java.util.Arrays;
+
 public enum Command {
     HIT("y"),
     BUST("n");
 
+
+    private static final String INVALID_COMMAND = "올바른 명령어를 입력해주세요.";
     private final String command;
 
     Command(String command) {
         this.command = command;
     }
 
-    public String getCommand() {
+    private String getCommand() {
         return command;
+    }
+
+    public Command findCommand(String command) {
+        return Arrays.stream(values())
+                .filter(x -> x.getCommand().equals(command))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_COMMAND));
     }
 }
