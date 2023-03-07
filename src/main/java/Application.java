a30d541e7aceadcc59ee6acfe7ab40c10e5df0e7
BackFoxx <backfoxxx@gmail.com>
2023-03-07 14:26:02 +0900

    refactor : 잘못된 변수명 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,7 @@ import controller.BlackJackApplication;
 
 public class Application {
     public static void main(String[] args) {
-        BlackJackApplication blackJackController = new BlackJackApplication();
-        blackJackController.startGame();
+        BlackJackApplication application = new BlackJackApplication();
+        application.startGame();
     }
 }
