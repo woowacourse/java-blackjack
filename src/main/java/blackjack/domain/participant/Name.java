6de9da6117bd52efc206ead6811e5816d9c65a1f
LJW25 <dbjwlee@gmail.com>
2023-03-07 16:11:51 +0900

    feat(Name): 플레이어 이름이 딜러인 경우 예외 처리 구현

--- src/main/java/blackjack/domain/participant/Name.java
+++ src/main/java/blackjack/domain/participant/Name.java
@@ -2,19 +2,31 @@ package blackjack.domain.participant;
 
 public class Name {
     private static final String NAME_BLANK_ERROR_MESSAGE = "공백으로만 이루어진 이름은 사용할 수 없습니다.";
+    private static final String NAME_DEALER_ERROR_MESSAGE = "'딜러'는 플레이어 이름으로 사용할 수 없습니다.";
     private static final String BLANK = " ";
     private static final String EMPTY_STRING = "";
 
     private final String name;
 
     public Name(String name) {
-        validate(name);
+        validateName(name);
         this.name = name;
     }
 
-    private void validate(String name) {
+    private void validateName(String name) {
+        validateNameBlank(name);
+        validateNameNotDealer(name);
+    }
+
+    private static void validateNameBlank(String name) {
         if (name.replaceAll(BLANK, EMPTY_STRING).length() == 0) {
             throw new IllegalArgumentException(NAME_BLANK_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateNameNotDealer(String name) {
+        if (name.equals(Dealer.DEALER_NAME)){
+            throw new IllegalArgumentException(NAME_DEALER_ERROR_MESSAGE);
         }
     }
 
