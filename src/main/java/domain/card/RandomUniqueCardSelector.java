32943222dbdf0f8a9e54b29ff7ea5c1e6e6817a5
apptie <appti@naver.com>
2023-03-07 13:32:09 +0900

    refactor: 랜덤한 카드 순서를 뽑는 기능을 공통 처리한다

--- src/main/java/domain/card/RandomUniqueCardSelector.java
+++ src/main/java/domain/card/RandomUniqueCardSelector.java
@@ -4,7 +4,7 @@ import domain.CardSelector;
 import java.util.HashSet;
 import java.util.Set;
 
-public class RandomUniqueCardSelector implements CardSelector {
+public final class RandomUniqueCardSelector implements CardSelector {
 
     private final Set<Integer> beforeOrder;
 
@@ -14,12 +14,16 @@ public class RandomUniqueCardSelector implements CardSelector {
 
     @Override
     public int selectCardOrder(final int cardSize) {
-        int cardOrder = (int) (Math.random() * cardSize);
+        int cardOrder = calculateRandomCardOrder(cardSize);
 
         while (!beforeOrder.add(cardOrder)) {
-            cardOrder = (int) (Math.random() * cardSize);
+            cardOrder = calculateRandomCardOrder(cardSize);
         }
 
         return cardOrder;
+    }
+
+    private int calculateRandomCardOrder(final int cardSize) {
+        return (int) (Math.random() * cardSize);
     }
 }
