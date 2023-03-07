e611cd99289391d94c8217e93397c0dc89bfde6a
kiarakim <kiarakim413@gmail.com>
2023-03-07 15:50:34 +0900

    refactor: Player의 name 검증로직 User로 이전

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -2,41 +2,14 @@ package domain;
 
 public class Player extends User {
 
-    private static final int NAME_MAX_LENGTH = 5;
-    private static final int BLACK_JACK_SCORE = 21;
-    private static final String DEALER_NAME = "딜러";
+	private static final int BLACK_JACK_SCORE = 21;
 
-    public Player(final String name) {
-        validateName(name);
-        this.name = name;
-    }
+	public Player(final String name) {
+		super(name);
+	}
 
-    private void validateName(final String name) {
-        validateBlank(name);
-        validateLength(name);
-        validateNotProperName(name);
-    }
-
-    private void validateBlank(final String name) {
-        if (name.isBlank()) {
-            throw new IllegalArgumentException("플레이어의 이름은 공백으로만 이루어질 수 없습니다.");
-        }
-    }
-
-    private void validateLength(final String name) {
-        if (name.length() > NAME_MAX_LENGTH) {
-            throw new IllegalArgumentException(String.format("플레이어의 이름은 %d보다 길 수 없습니다.", NAME_MAX_LENGTH));
-        }
-    }
-
-    private void validateNotProperName(final String name) {
-        if (name.equals(DEALER_NAME)) {
-            throw new IllegalArgumentException("딜러라는 이름은 사용할 수 없습니다.");
-        }
-    }
-
-    @Override
-    public boolean isHittable() {
-        return cards.isUnder(BLACK_JACK_SCORE);
-    }
+	@Override
+	public boolean isHittable() {
+		return cards.isUnder(BLACK_JACK_SCORE);
+	}
 }
--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -4,15 +4,37 @@ import java.util.List;
 
 public abstract class User {
 
+	private static final int NAME_MAX_LENGTH = 5;
+
 	protected Cards cards;
 	protected String name;
 
-	public User() {
+	public User(final String name) {
 		cards = new Cards();
+		validate(name);
+		this.name = name;
 	}
 
 	public void hit(Card card) {
 		cards.addCard(card);
+	}
+
+	private void validate(final String name) {
+		validateBlank(name);
+		validateLength(name);
+	}
+
+	private void validateBlank(final String name) {
+		if (name.isBlank()) {
+			throw new IllegalArgumentException("플레이어의 이름은 공백으로만 이루어질 수 없습니다.");
+		}
+	}
+
+	private void validateLength(final String name) {
+		if (name.length() > NAME_MAX_LENGTH) {
+			throw new IllegalArgumentException(String.format("플레이어의 이름은 %d보다 길 수 없습니다.", NAME_MAX_LENGTH));
+		}
+
 	}
 
 	public abstract boolean isHittable();
