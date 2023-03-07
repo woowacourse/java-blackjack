026f5d69d471b44bef7685367d6d877a864f0f04
kim0914 <kosa0914@naver.com>
2023-03-07 21:36:20 +0900

    feat(Name): 추가된 이름 검증 메서드 추가

--- src/main/java/blackjack/domain/participant/Name.java
+++ src/main/java/blackjack/domain/participant/Name.java
@@ -6,24 +6,32 @@ public class Name {
 
     private static final int MAX_LENGTH = 5;
     private static final String SPACE = " ";
+    private static final String BAN_NAME = "딜러";
 
     private final String name;
 
     public Name(String name) {
         validateSpace(name);
         validateLength(name);
+        validateIsNameDealer(name);
         this.name = name;
     }
 
-    private void validateLength(String name) {
-        if (name.length() > MAX_LENGTH) {
-            throw new IllegalArgumentException("이름의 길이는 " + MAX_LENGTH + "자 이상일 수 없습니다.");
+    private void validateIsNameDealer(String name) {
+        if (BAN_NAME.equals(name)) {
+            throw new IllegalArgumentException("이름은 딜러가 될 수 없습니다.");
         }
     }
 
     private void validateSpace(String name) {
         if (name.isBlank() || name.contains(SPACE)) {
             throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
+        }
+    }
+
+    private void validateLength(String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("이름의 길이는 " + MAX_LENGTH + "자 이상일 수 없습니다.");
         }
     }
 
