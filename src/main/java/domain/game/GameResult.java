f4e83ba91fd48ecb0d8c7c6296912f88113fb4c7
apptie <appti@naver.com>
2023-03-07 19:51:27 +0900

    feat: GameResult가 결과에 따라 배팅 비율을 관리하는 기능을 추가한다

--- src/main/java/domain/game/GameResult.java
+++ src/main/java/domain/game/GameResult.java
@@ -1,7 +1,18 @@
 package domain.game;
 
 public enum GameResult {
-    WIN,
-    LOSE,
-    DRAW
+    BLACKJACK_WIN(2.5),
+    WIN(2),
+    LOSE(-1),
+    DRAW(1);
+
+    private final double prizeRatio;
+
+    GameResult(final double prizeRatio) {
+        this.prizeRatio = prizeRatio;
+    }
+
+    public double prizeRatio() {
+        return prizeRatio;
+    }
 }
