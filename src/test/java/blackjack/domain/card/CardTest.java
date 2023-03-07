1ac7610725e1709bbd96a119a54814ccbfcfa963
kim0914 <kosa0914@naver.com>
2023-03-08 01:34:14 +0900

    refactor(Card): 카드 생성 테스트 수정

--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -3,15 +3,13 @@ package blackjack.domain.card;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class CardTest {
 
     @Test
     @DisplayName("카드 생성 테스트")
     void createCard() {
-        Card card = new Card(Suit.SPADE, Denomination.ACE);
-
-        assertThat(card).isNotNull();
+        assertDoesNotThrow(() -> new Card(Suit.SPADE, Denomination.ACE));
     }
 }
