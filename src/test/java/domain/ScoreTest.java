0953389a5a52578215b39849d68928ee814b6aab
java-saeng <gusdn85554@naver.com>
2023-03-07 16:38:52 +0900

    style : 제출 전 code reformatting

--- src/test/java/domain/ScoreTest.java
+++ src/test/java/domain/ScoreTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ScoreTest {
 
