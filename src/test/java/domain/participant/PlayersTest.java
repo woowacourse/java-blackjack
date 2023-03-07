d5d8c573b0a8d7df89346cc9cdb3bc6d6144742f
sh111-coder <ohk9134@naver.com>
2023-03-07 09:31:12 +0900

    test(PlayersTest): 플레이어 이름으로 플레이어를 찾아서 반환하는 기능 테스트

--- src/test/java/domain/participant/PlayersTest.java
+++ src/test/java/domain/participant/PlayersTest.java
@@ -10,6 +10,7 @@ import domain.card.Suit;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import util.Constants;
 
 class PlayersTest {
 
@@ -24,5 +25,36 @@ class PlayersTest {
 
         Assertions.assertThat(players.getPlayerNames().size()).isEqualTo(2);
         Assertions.assertThat(players.getPlayerNames()).containsExactly("seongha", "dino");
+    }
+
+    @Test
+    @DisplayName("플레이어의 이름으로 플레이어를 찾아서 반환한다.")
+    void findPlayerByPlayerName() {
+        List<Card> cards1 = List.of(new Card(Suit.CLOVER, Denomination.SIX), new Card(Suit.SPADE, Denomination.TEN));
+        List<Card> cards2 = List.of(new Card(Suit.CLOVER, Denomination.NINE), new Card(Suit.SPADE, Denomination.SEVEN));
+        Player player1 = new Player(new Name("seongha"), new HandCards(cards1));
+        Player player2 = new Player(new Name("dino"), new HandCards(cards2));
+        List<Player> gamePlayers = List.of(player1, player2);
+
+        Players players = new Players(gamePlayers);
+
+        Assertions.assertThat(players.findPlayerByPlayerName("seongha")).isEqualTo(player1);
+        Assertions.assertThat(players.findPlayerByPlayerName("dino")).isEqualTo(player2);
+    }
+
+    @Test
+    @DisplayName("플레이어의 이름으로 플레이어를 찾을 때 해당하는 플레이어가 없으면 예외를 던진다.")
+    void findPlayerByPlayerNameNotFoundPlayer() {
+        List<Card> cards1 = List.of(new Card(Suit.CLOVER, Denomination.SIX), new Card(Suit.SPADE, Denomination.TEN));
+        List<Card> cards2 = List.of(new Card(Suit.CLOVER, Denomination.NINE), new Card(Suit.SPADE, Denomination.SEVEN));
+        Player player1 = new Player(new Name("seongha"), new HandCards(cards1));
+        Player player2 = new Player(new Name("dino"), new HandCards(cards2));
+        List<Player> gamePlayers = List.of(player1, player2);
+
+        Players players = new Players(gamePlayers);
+
+        Assertions.assertThatThrownBy(() -> players.findPlayerByPlayerName("abcd"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Constants.ERROR_PREFIX + "해당 플레이어 이름과 일치하는 플레이어가 없습니다.");
     }
 }
