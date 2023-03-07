7329265e030b936c1985179ad746398dd778ec85
bjk1649 <qhwns1649@naver.com>
2023-03-07 17:11:38 +0900

    refactor: dto 끼리의 상속을 조합으로 변경

--- src/main/java/controller/ParticipantDtoWithScore.java
+++ src/main/java/controller/ParticipantDtoWithScore.java
@@ -1,13 +1,24 @@
 package controller;
 
 import domain.Participant;
+import java.util.List;
 
-public class ParticipantDtoWithScore extends ParticipantDto {
+public class ParticipantDtoWithScore {
+
+    private final ParticipantDto participantDto;
     private final int score;
 
     public ParticipantDtoWithScore(Participant participant) {
-        super(participant);
+        this.participantDto = new ParticipantDto(participant);
         this.score = participant.score();
+    }
+
+    public String name() {
+        return participantDto.name();
+    }
+
+    public List<String> cards() {
+        return participantDto.cards();
     }
 
     public int score() {
