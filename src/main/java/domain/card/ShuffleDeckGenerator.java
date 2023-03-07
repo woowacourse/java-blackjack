3a72ffb2afa51263823713508fbf38fbd9d576bc
MoonJeWoong <munjin0201@naver.com>
2023-03-07 17:18:10 +0900

    feat: 전략패턴을 위한 DeckGenerator 구현

---
+++ src/main/java/domain/card/ShuffleDeckGenerator.java
@@ -0,0 +1,25 @@
+package domain.card;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ShuffleDeckGenerator implements DeckGenerator{
+    @Override
+    public List<Card> generate() {
+        List<Card> originDeck = addAllCards();
+        Collections.shuffle(originDeck);
+        return originDeck;
+    }
+
+    private List<Card> addAllCards() {
+        List<Card> cards = new ArrayList<>();
+        cards.addAll(Arrays.asList(CloverCard.values()));
+        cards.addAll(Arrays.asList(DiamondCard.values()));
+        cards.addAll(Arrays.asList(HeartCard.values()));
+        cards.addAll(Arrays.asList(SpadeCard.values()));
+
+        return cards;
+    }
+}
