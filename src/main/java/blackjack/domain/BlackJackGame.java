66206286e5e778dc5dad3e21497a10507dd02768
parkmuhyeun <pjhg410@gmail.com>
2023-03-07 15:19:03 +0900

    feat: 블랙잭게임에서 hit을 진행하는 기능 추가

--- src/main/java/blackjack/domain/BlackJackGame.java
+++ src/main/java/blackjack/domain/BlackJackGame.java
@@ -1,6 +1,7 @@
 package blackjack.domain;
 
 import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Participant;
 import blackjack.strategy.CardPicker;
 
 public class BlackJackGame {
@@ -14,6 +15,9 @@ public class BlackJackGame {
     public void initHit(Players players, Dealer dealer, CardPicker cardPicker) {
         dealer.initHit(cardPool, cardPicker);
         players.initHit(cardPool, cardPicker);
+    }
 
+    public void hit(Participant participant, CardPicker cardPicker) {
+        participant.hit(cardPool.draw(cardPicker));
     }
 }
--- src/test/java/blackjack/domain/BlackJackGameTest.java
+++ src/test/java/blackjack/domain/BlackJackGameTest.java
@@ -1,6 +1,7 @@
 package blackjack.domain;
 
 import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Player;
 import blackjack.strategy.RandomCardPicker;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,14 +10,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BlackJackGameTest {
 
+    private final BlackJackGame blackJackGame = new BlackJackGame();
+
     @Test
     void initHit() {
         Players players = new Players("a,b,c");
         Dealer dealer = new Dealer();
-        BlackJackGame blackJackGame = new BlackJackGame();
 
         blackJackGame.initHit(players, dealer, new RandomCardPicker());
         players.getPlayers().stream().allMatch(player -> player.getCardDeck().getCardCount() == 2);
         Assertions.assertThat(dealer.getCardDeck().getCardCount()).isEqualTo(2);
+    }
+
+    @Test
+    void hit() {
+        Player player = new Player("a");
+
+        blackJackGame.hit(player, new RandomCardPicker());
+
+        Assertions.assertThat(player.getCardDeck().getCardCount()).isEqualTo(1);
     }
 }
