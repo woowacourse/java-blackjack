34c36c7bfa5cbf0348d1ddf069e20ae9a727051a
Eunkeee <eunkeeeee@gmail.com>
2023-03-07 12:16:24 +0900

    refactor(Dealer): 코드 성능 개선

--- src/main/java/domain/participant/Dealer.java
+++ src/main/java/domain/participant/Dealer.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class Dealer extends Participant {
 
     private static final int FILL_BOUNDARY_INCLUSIVE = 16;
-    private static final String DEFAULT_DEALER_NAME = "딜러";
+    private static final Name DEFAULT_DEALER = new Name("딜러");
 
     public Dealer(final List<Card> cards) {
-        super(new Name(DEFAULT_DEALER_NAME), cards);
+        super(DEFAULT_DEALER, cards);
     }
 
     public boolean isFill() {
