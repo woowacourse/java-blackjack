cbe5225c195bdf555d161d379f71ae5187511460
ChoiYoungHoon <dudgns2865@naver.com>
2023-03-07 17:30:41 +0900

    feat: CardDistributor에서 초기에 한명에게 카드 배분, 여러명에게 배분 기능 구현

--- src/main/java/domain/service/CardDistributor.java
+++ src/main/java/domain/service/CardDistributor.java
@@ -1,6 +1,5 @@
 package domain.service;
 
-import domain.model.Dealer;
 import domain.model.Player;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -9,6 +8,7 @@ public class CardDistributor {
 
     private static final int INIT_DEAL_COUNT = 2;
     private static final String CANNOT_GIVE_CARD_ERROR_MESSAGE = "더 이상 카드를 받을 수 없습니다.";
+    private static final String ALREADY_CARD_EXIST_ERROR_MESSAGE = "이미 카드를 가지고 있습니다.";
     private final CardGenerator cardGenerator;
 
     public CardDistributor(final CardGenerator cardGenerator) {
@@ -16,17 +16,29 @@ public class CardDistributor {
     }
 
     public void giveCard(final Player player) {
-        if (player.isBust()) {
-            throw new IllegalArgumentException(CANNOT_GIVE_CARD_ERROR_MESSAGE);
-        }
+        checkBust(player);
         player.addCard(cardGenerator.generate());
     }
 
-    public void giveInitCards(final Dealer dealer, final List<Player> players) {
+    private static void checkBust(final Player player) {
+        if (player.isBust()) {
+            throw new IllegalArgumentException(CANNOT_GIVE_CARD_ERROR_MESSAGE);
+        }
+    }
+
+    public void giveInitCards(final Player player) {
+        checkCardEmpty(player);
         IntStream.range(0, INIT_DEAL_COUNT)
-            .forEach(i -> {
-                giveCard(dealer);
-                players.forEach(this::giveCard);
-            });
+            .forEach(count -> player.addCard(cardGenerator.generate()));
+    }
+
+    public void giveInitCards(final List<Player> players) {
+        players.forEach(this::giveInitCards);
+    }
+
+    private void checkCardEmpty(final Player player) {
+        if (player.cardsIsNotEmpty()) {
+            throw new IllegalArgumentException(ALREADY_CARD_EXIST_ERROR_MESSAGE);
+        }
     }
 }
