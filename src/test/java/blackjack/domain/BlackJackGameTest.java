0e12c0c6cc3172e56312c4e8d289393a92d1af40
yoondgu <doyou2ing@naver.com>
2023-03-07 20:00:57 +0900

    test: 불필요한 Mock 객체 사용 삭제

--- src/test/java/blackjack/domain/BlackJackGameTest.java
+++ src/test/java/blackjack/domain/BlackJackGameTest.java
@@ -1,12 +1,5 @@
 package blackjack.domain;
 
-import static blackjack.domain.card.Denomination.ACE;
-import static blackjack.domain.card.Denomination.FOUR;
-import static blackjack.domain.card.Denomination.THREE;
-import static blackjack.domain.card.Denomination.TWO;
-import static blackjack.domain.card.Suit.DIAMOND;
-import static blackjack.domain.card.Suit.HEART;
-import static blackjack.domain.card.Suit.SPADE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import blackjack.domain.card.Card;
@@ -25,13 +18,13 @@ class BlackJackGameTest {
     @DisplayName("모든 참가자에게 카드를 2장씩 나눠준다.")
     @Test
     void should_AllParticipantsHas2Cards_When_HandOut() {
-        List<String> playerNames = List.of("pobi", "odo", "jason");
-        BlackJackGame blackJackGame = new BlackJackGame(new BlackJackDeckGenerator(), dealerName, playerNames);
+        final List<String> playerNames = List.of("pobi", "odo", "jason");
+        final BlackJackGame blackJackGame = new BlackJackGame(new BlackJackDeckGenerator(), dealerName, playerNames);
 
         blackJackGame.handOut();
-        
+
         assertThat(blackJackGame.openCardsByName(dealerName)).hasSize(2);
-        for (String name : playerNames) {
+        for (final String name : playerNames) {
             assertThat(blackJackGame.openCardsByName(name)).hasSize(2);
         }
     }
@@ -39,19 +32,14 @@ class BlackJackGameTest {
     @DisplayName("카드 오픈 시 참가자의 보유 카드를 모두 확인하되, 딜러 카드는 첫 카드만 확인한다.")
     @Test
     void should_OpenCards_Of_AllParticipants() {
-        DeckGenerator mockGenerator = new MockDeckGenerator((List.of(
-                new Card(SPADE, ACE), new Card(SPADE, TWO),
-                new Card(DIAMOND, THREE), new Card(DIAMOND, FOUR),
-                new Card(HEART, THREE), new Card(HEART, FOUR)
-        )));
-        List<String> playerNames = List.of("odo", "doy");
-        BlackJackGame blackJackGame = new BlackJackGame(mockGenerator, dealerName, playerNames);
+        final List<String> playerNames = List.of("odo", "doy");
+        final BlackJackGame blackJackGame = new BlackJackGame(new BlackJackDeckGenerator(), dealerName, playerNames);
 
         blackJackGame.handOut();
 
-        Map<String, List<Card>> handOutCards = blackJackGame.openHandOutCards();
+        final Map<String, List<Card>> handOutCards = blackJackGame.openHandOutCards();
         assertThat(handOutCards.get(dealerName)).hasSize(1);
-        for (String name : playerNames) {
+        for (final String name : playerNames) {
             assertThat(handOutCards.get(name)).hasSize(2);
         }
     }
