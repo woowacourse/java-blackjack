806f54a35f5044d1f6d3d7ee39a5f1cc54df04ab
SproutMJ <mjk54565@gmail.com>
2023-03-07 17:46:18 +0900

    refactor: Deck의 자료구조를 Deque으로 변경

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -1,49 +1,55 @@
 package domain;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Deck {
-    private final static List<Card> deck = new ArrayList<>();
-    private List<Symbol> symbols;
-    private List<CardNumber> cardNumbers;
+    private Deque<Card> deck;
+    private final List<Symbol> symbols;
+    private final List<CardNumber> cardNumbers;
 
-    private Deck(List<Symbol> symbols, List<CardNumber> cardNumbers) {
+    private Deck(List<Symbol> symbols, List<CardNumber> cardNumbers, Deque<Card> cards) {
         this.symbols = symbols;
         this.cardNumbers = cardNumbers;
+        this.deck = cards;
     }
 
     public static Deck of(List<Symbol> symbols, List<CardNumber> cardNumbers) {
-        Deck deck = new Deck(symbols, cardNumbers);
-        generateCards(symbols, cardNumbers);
+        List<Card> cards = generateCards(symbols, cardNumbers);
+        Deck deck = new Deck(symbols, cardNumbers, new LinkedList<>(cards));
         return deck;
     }
 
-    private static void generateCards(List<Symbol> symbols, List<CardNumber> cardNumbers) {
+    private static List<Card> generateCards(List<Symbol> symbols, List<CardNumber> cardNumbers) {
+        List<Card> cards = new LinkedList<>();
         if(Objects.isNull(symbols) || Objects.isNull(cardNumbers)) {
             symbols = Arrays.stream(Symbol.values()).collect(Collectors.toList());
             cardNumbers = Arrays.stream(CardNumber.values()).collect(Collectors.toList());
         }
         for (Symbol symbol : symbols) {
-            addCardsWithSymbolOf(symbol, cardNumbers);
+            addCardsWithSymbolOf(symbol, cardNumbers, cards);
         }
-        Collections.shuffle(deck);
+        Collections.shuffle(cards);
+        return cards;
     }
 
-    private static void addCardsWithSymbolOf(Symbol symbol, List<CardNumber> cardNumbers) {
+    private static void addCardsWithSymbolOf(Symbol symbol, List<CardNumber> cardNumbers, List<Card> cards) {
         for (CardNumber cardNumber : cardNumbers) {
-            deck.add(new Card(symbol, cardNumber));
+            cards.add(new Card(symbol, cardNumber));
         }
     }
 
     public Card draw() {
         if (deck.size() == 0) {
-            generateCards(this.symbols, this.cardNumbers);
+            this.deck = new LinkedList<>(generateCards(this.symbols, this.cardNumbers));
         }
-        return deck.remove(0);
+        return this.deck.removeFirst();
     }
 }
