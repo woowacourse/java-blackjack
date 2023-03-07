cc610595736fd288268289532ed38356193e6d3b
LJW25 <dbjwlee@gmail.com>
2023-03-07 16:11:25 +0900

    test(NameTest): 플레이어 이름이 딜러인 예외 테스트 추가

--- src/test/java/blackjack/domain/NameTest.java
+++ src/test/java/blackjack/domain/NameTest.java
@@ -1,8 +1,10 @@
 package blackjack.domain;
 
+import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Name;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,6 +17,12 @@ class NameTest {
     @ValueSource(strings = {"", "   ", "       "})
     void blankNameError(String input) {
         Assertions.assertThatThrownBy(() -> new Name(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("유효성 검증 확인 - 이름이 딜러인 경우")
+    void dealerNameError() {
+        Assertions.assertThatThrownBy(() -> new Name(Dealer.DEALER_NAME)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
