37864cfaf84064edef51a2e3c85b1daa2f70942c
MoonJeWoong <munjin0201@naver.com>
2023-03-08 00:43:24 +0900

    refactor: player 객체 간 동일성 비교를 위한 equals & hashCode 삭제

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -30,21 +30,4 @@ public class Player extends User {
     public String getName() {
         return name.getName();
     }
-
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) {
-            return true;
-        }
-        if (other == null || getClass() != other.getClass()) {
-            return false;
-        }
-        Player player = (Player) other;
-        return name.equals(player.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
 }
