1607f4b23843550d1688d104d685d3a96a9d29c8
yeonkkk <tjddus0302@gmail.com>
2023-03-07 16:34:12 +0900

    refactor: denomination 변수명, 메서드명 변경

--- src/main/java/domain/Denomination.java
+++ src/main/java/domain/Denomination.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 
 public enum Denomination {
 
-    LETTER_A("A", 11),
-    LETTER_2("2", 2),
-    LETTER_3("3", 3),
-    LETTER_4("4", 4),
-    LETTER_5("5", 5),
-    LETTER_6("6", 6),
-    LETTER_7("7", 7),
-    LETTER_8("8", 8),
-    LETTER_9("9", 9),
-    LETTER_10("10", 10),
-    LETTER_K("K", 10),
-    LETTER_Q("Q", 10),
-    LETTER_J("J", 10);
+    ACE("A", 1),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    KING("K", 10),
+    QUEEN("Q", 10),
+    JACK("J", 10);
 
     private final String letter;
 
@@ -29,32 +29,21 @@ public enum Denomination {
 
     public static Denomination of(String letter) {
         return Arrays.stream(values())
-                .filter(letter1 -> letter1.getLetter().equals(letter))
+                .filter(denomination1 -> denomination1.letter().equals(letter))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 카드 글자입니다"));
     }
 
-    public boolean isNotA() {
-        return this != LETTER_A;
-    }
-
-    public String getLetter() {
+    public String letter() {
         return letter;
     }
 
-    public int getScore() {
-        return this.score;
+    public Score score() {
+        return new Score(this.score);
     }
 
-    public static int getScoreFromA(int number) {
-        if (isBustedByA(number)) {
-            return 1;
-        }
-
-        return LETTER_A.score;
+    public boolean isAce() {
+        return ACE == this;
     }
 
-    private static boolean isBustedByA(int number) {
-        return number + LETTER_A.score > 21;
-    }
 }
