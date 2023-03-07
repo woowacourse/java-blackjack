df662feb4adec056aa20280570455e0056aa455c
seokho <kim76744dev@gmail.com>
2023-03-07 09:48:07 +0900

    refactor: 덱에서 카드를 뽑는 방법을 전략 패턴으로 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,10 @@
 import controller.BlackJackController;
 import model.card.Deck;
+import model.card.RandomShuffleMaker;
 
 public class Application {
 
     public static void main(String[] args) {
-        new BlackJackController().init(Deck.create());
+        new BlackJackController().init(Deck.create(new RandomShuffleMaker()));
     }
 }
--- src/test/java/model/DeckTest.java
+++ src/test/java/model/DeckTest.java
@@ -1,5 +1,38 @@
 package model;
 
+import model.card.Card;
+import model.card.Deck;
+import model.card.ShuffleStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static model.card.Shape.DIAMOND;
+import static model.card.Shape.HEART;
+import static model.card.Value.ACE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 class DeckTest {
 
+    @Test
+    @DisplayName("덱이 섞였는지 테스트한다.")
+    void cardShuffleTest() {
+        // given
+        final ShuffleStrategy shuffleStrategy = cards -> {
+            cards.add(0,new Card(DIAMOND, ACE));
+            cards.add(1, new Card(HEART, ACE));
+        };
+
+        final Deck deck = Deck.create(shuffleStrategy);
+
+        // when
+        final Card firstCard = deck.pick();
+        final Card secondCard = deck.pick();
+
+        // then
+        assertAll(
+                () -> assertThat(firstCard).isEqualTo(new Card(DIAMOND, ACE)),
+                () -> assertThat(secondCard).isEqualTo(new Card(HEART, ACE))
+        );
+    }
 }
