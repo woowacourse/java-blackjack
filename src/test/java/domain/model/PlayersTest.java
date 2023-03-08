b2ed8f711a6bfe504aa3905bfd77e2d0df4e25de
ChoiYoungHoon <dudgns2865@naver.com>
2023-03-08 20:36:06 +0900

    refactor: Players test 작성

---
+++ src/test/java/domain/model/PlayersTest.java
@@ -0,0 +1,54 @@
+package domain.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayersTest {
+
+    @Test
+    @DisplayName("중복된 원소가 있는 문자열 리스트로 플레이어 생성 실패 테스트")
+    public void testCreatByFromWhenDuplicateNameExist() {
+        //given
+        final List<String> names = List.of("a", "a", "b");
+
+        //when
+        //then
+        assertThatThrownBy(() -> Players.from(names))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("중복된 원소가 없는 문자열 리스트로 플레이어 생성 성공 테스트")
+    public void testCreateSuccess() {
+        //given
+        final List<String> names = List.of("a", "b");
+
+        //when
+        //then
+        assertDoesNotThrow(() -> Players.from(names));
+    }
+
+    @Test
+    @DisplayName("크기 반환 테스트")
+    public void testSize() {
+        //given
+        final int size = 10;
+        List<String> names = IntStream.range(0, size)
+            .mapToObj(i -> "test" + i)
+            .collect(Collectors.toList());
+        Players players = Players.from(names);
+
+        //when
+        int result = players.size();
+
+        //then
+        assertThat(result).isEqualTo(size);
+    }
+}
