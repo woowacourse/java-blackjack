cc14ae89e56116b016737683bd367a98a17b072a
ChoiYoungHoon <dudgns2865@naver.com>
2023-03-08 16:14:51 +0900

    test: 승리, 무승부, 패배 생성 및 추가 테스트 작성

--- src/test/java/domain/model/ResultTest.java
+++ src/test/java/domain/model/ResultTest.java
@@ -9,19 +9,99 @@ import org.junit.jupiter.api.Test;
 class ResultTest {
 
     @Test
-    @DisplayName("결과 생성 테스트")
+    @DisplayName("빈 결과 생성 테스트")
     public void testCreate() {
         //given
-        final int victory = 1;
-        final int draw = 2;
-        final int defeat = 3;
-
         //when
-        final Result result = Result.of(victory, draw, defeat);
+        Result result = Result.empty();
 
         //then
-        assertThat(result.getVictory()).isEqualTo(victory);
-        assertThat(result.getDraw()).isEqualTo(draw);
-        assertThat(result.getDefeat()).isEqualTo(defeat);
+        assertThat(result.getVictory()).isEqualTo(0);
+        assertThat(result.getDraw()).isEqualTo(0);
+        assertThat(result.getDefeat()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("결과 승리 생성 테스트")
+    public void testVictoryCreate() {
+        //given
+        //when
+        Result result = Result.victory();
+
+        //then
+        assertThat(result.getVictory()).isEqualTo(1);
+        assertThat(result.getDraw()).isEqualTo(0);
+        assertThat(result.getDefeat()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("결과 무승부 생성 테스트")
+    public void testDrawCreate() {
+        //given
+        //when
+        Result result = Result.draw();
+
+        //then
+        assertThat(result.getDraw()).isEqualTo(1);
+        assertThat(result.getVictory()).isEqualTo(0);
+        assertThat(result.getDefeat()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("결과 패배 생성 테스트")
+    public void testDefeatCreate() {
+        //given
+        //when
+        Result result = Result.defeat();
+
+        //then
+        assertThat(result.getDefeat()).isEqualTo(1);
+        assertThat(result.getVictory()).isEqualTo(0);
+        assertThat(result.getDraw()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("승리 추가 테스트")
+    public void testAddVictory() {
+        //given
+        Result result = Result.empty();
+
+        //when
+        result = result.addVictory();
+
+        //then
+        assertThat(result.getVictory()).isEqualTo(1);
+        assertThat(result.getDraw()).isEqualTo(0);
+        assertThat(result.getDefeat()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("무승부 추가 테스트")
+    public void testAddDraw() {
+        //given
+        Result result = Result.empty();
+
+        //when
+        result = result.addDraw();
+
+        //then
+        assertThat(result.getDraw()).isEqualTo(1);
+        assertThat(result.getVictory()).isEqualTo(0);
+        assertThat(result.getDefeat()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("패배 추가 테스트")
+    public void testAddDefeat() {
+        //given
+        Result result = Result.empty();
+
+        //when
+        result = result.addDefeat();
+
+        //then
+        assertThat(result.getDefeat()).isEqualTo(1);
+        assertThat(result.getVictory()).isEqualTo(0);
+        assertThat(result.getDraw()).isEqualTo(0);
     }
 }
