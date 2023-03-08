5b7cc6a55d9632bb7c49539fd162ccfbc1e032c4
Cyma-s <ssol0319@naver.com>
2023-03-08 13:37:53 +0900

    feat: 참가자의 정보를 저장하는 dto 추가

---
+++ src/main/java/blackjack/dto/ParticipantCardsDTO.java
@@ -0,0 +1,42 @@
+package blackjack.dto;
+
+import blackjack.domain.Dealer;
+import blackjack.domain.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ParticipantCardsDTO {
+    private final List<ParticipantStatusDTO> participantCards;
+
+    private ParticipantCardsDTO(final List<ParticipantStatusDTO> participantCards) {
+        this.participantCards = participantCards;
+    }
+
+    public static ParticipantCardsDTO ofStart(final Dealer dealer, final List<Player> players) {
+        List<ParticipantStatusDTO> participantInitialCards = new ArrayList<>();
+        participantInitialCards.add(ParticipantStatusDTO.ofStart(dealer));
+        players.forEach(player ->
+                participantInitialCards.add(ParticipantStatusDTO.ofStart(player)));
+        return new ParticipantCardsDTO(participantInitialCards);
+    }
+
+    public static ParticipantCardsDTO of(final Dealer dealer, final List<Player> players) {
+        List<ParticipantStatusDTO> participantCards = new ArrayList<>();
+        participantCards.add(ParticipantStatusDTO.of(dealer));
+        players.forEach(player ->
+                participantCards.add(ParticipantStatusDTO.of(player)));
+        return new ParticipantCardsDTO(participantCards);
+    }
+
+    public List<ParticipantStatusDTO> getParticipantCards() {
+        return participantCards;
+    }
+
+    public List<String> getPlayerNames() {
+        return participantCards.subList(1, participantCards.size()).stream()
+                .map(ParticipantStatusDTO::getName)
+                .collect(Collectors.toList());
+    }
+}
