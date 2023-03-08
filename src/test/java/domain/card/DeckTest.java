e00448995eb9d16a70385e18a921a3d80429e25c
Go-Jaecheol <gojaech@naver.com>
2023-03-08 20:06:05 +0900

    test(DeckTest): 52개의 카드가 다 생성되었는지 확인하도록 테스트 수정

--- src/test/java/domain/card/DeckTest.java
+++ src/test/java/domain/card/DeckTest.java
@@ -2,6 +2,7 @@ package domain.card;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,8 +14,9 @@ class DeckTest {
     @DisplayName("52개의 카드를 생성할 수 있다.")
     @Test
     void createCardsTest() {
-        Card card = new Card(Value.ACE, Shape.SPADE);
-        assertThat(deck.contains(card)).isTrue();
+        Arrays.stream(Value.values())
+                .forEach(value -> Arrays.stream(Shape.values())
+                        .forEach(shape -> assertThat(deck.contains(new Card(value, shape))).isTrue()));
     }
 
     @DisplayName("카드를 한 장씩 반환받을 수 있다.")
