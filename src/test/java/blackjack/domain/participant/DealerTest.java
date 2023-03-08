9a42b2d29ff940d78729a0577160891a8b5118f1
kokodak <kokodakadokok@gmail.com>
2023-03-08 16:52:05 +0900

    test: 딜러 테스트 작성

--- src/test/java/blackjack/domain/participant/DealerTest.java
+++ src/test/java/blackjack/domain/participant/DealerTest.java
@@ -17,6 +17,8 @@ import blackjack.domain.card.Cards;
 import blackjack.domain.result.Result;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -101,5 +103,104 @@ class DealerTest {
         Result result = dealer.compareScoreTo(player);
 
         assertThat(result).isEqualTo(Result.WIN);
+    }
+
+    @Test
+    void 플레이어의_점수가_21_이하이고_딜러의_점수가_21_초과이면_딜러가_진다() {
+        final Cards playerCards = new Cards(List.of(
+                new Card(KING, CLOVER),
+                new Card(JACK, HEART),
+                new Card(ACE, HEART)
+        ));
+        final Cards dealerCards = new Cards(List.of(
+                new Card(KING, CLOVER),
+                new Card(JACK, HEART),
+                new Card(QUEEN, HEART)
+        ));
+        final Player player = new Player("kokodak", playerCards);
+        final Dealer dealer = new Dealer(dealerCards);
+
+        Result result = dealer.compareScoreTo(player);
+
+        assertThat(result).isEqualTo(Result.LOSE);
+    }
+
+    @Test
+    void 플레이어와_딜러의_점수_모두_21_이하이고_플레이어의_점수가_딜러보다_작다면_딜러가_이긴다() {
+        final Cards playerCards = new Cards(List.of(
+                new Card(KING, CLOVER),
+                new Card(JACK, HEART)
+        ));
+        final Cards dealerCards = new Cards(List.of(
+                new Card(KING, CLOVER),
+                new Card(JACK, HEART),
+                new Card(ACE, HEART)
+        ));
+        final Player player = new Player("kokodak", playerCards);
+        final Dealer dealer = new Dealer(dealerCards);
+
+        Result result = dealer.compareScoreTo(player);
+
+        assertThat(result).isEqualTo(Result.WIN);
+    }
+
+    @Test
+    void 플레이어와_딜러의_점수_모두_21_이하이고_플레이어의_점수가_딜러보다_크다면_딜러가_진다() {
+        final Cards playerCards = new Cards(List.of(
+                new Card(KING, CLOVER),
+                new Card(JACK, HEART),
+                new Card(ACE, HEART)
+        ));
+        final Cards dealerCards = new Cards(List.of(
+                new Card(KING, CLOVER),
+                new Card(JACK, HEART)
+        ));
+        final Player player = new Player("kokodak", playerCards);
+        final Dealer dealer = new Dealer(dealerCards);
+
+        Result result = dealer.compareScoreTo(player);
+
+        assertThat(result).isEqualTo(Result.LOSE);
+    }
+
+    @Test
+    void 플레이어와_딜러의_점수_모두_21_이하이고_플레이어와_딜러의_점수가_같다면_무승부이다() {
+        final Cards playerCards = new Cards(List.of(
+                new Card(KING, CLOVER),
+                new Card(JACK, HEART)
+        ));
+        final Cards dealerCards = new Cards(List.of(
+                new Card(KING, CLOVER),
+                new Card(JACK, HEART)
+        ));
+        final Player player = new Player("kokodak", playerCards);
+        final Dealer dealer = new Dealer(dealerCards);
+
+        Result result = dealer.compareScoreTo(player);
+
+        assertThat(result).isEqualTo(Result.DRAW);
+    }
+
+    @Test
+    void 딜러는_자신의_승패_결과를_계산한다() {
+        final Cards playerCards = new Cards(List.of(
+                new Card(KING, CLOVER),
+                new Card(JACK, HEART)
+        ));
+        final Cards dealerCards = new Cards(List.of(
+                new Card(KING, CLOVER),
+                new Card(JACK, HEART),
+                new Card(ACE, HEART)
+        ));
+        final Player player = new Player("kokodak", playerCards);
+        final Dealer dealer = new Dealer(dealerCards);
+
+        Map<Result, Integer> dealerResult = dealer.getDealerResult(List.of(player));
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(dealerResult.get(Result.WIN)).isEqualTo(1);
+        softAssertions.assertThat(dealerResult.get(Result.DRAW)).isEqualTo(0);
+        softAssertions.assertThat(dealerResult.get(Result.LOSE)).isEqualTo(0);
+        softAssertions.assertAll();
     }
 }
