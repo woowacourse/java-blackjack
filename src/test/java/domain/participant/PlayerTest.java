a3d48ad427663d2e944fcaf99098b2de60d03e7d
iamjooon2 <iamjooon2@gmail.com>
2023-03-08 17:48:40 +0900

    test: ParameterizedTest 파라미터 적용

--- src/test/java/domain/participant/PlayerTest.java
+++ src/test/java/domain/participant/PlayerTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 @DisplayName("Player는 ")
 class PlayerTest {
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "이름은 공백을 제거하고 2~10글자이다 name = {0}")
     @ValueSource(strings = {"가비", "열_글자_입니다..", "공백도 포함합니다."})
     void 이름은_공백을_제외하고_2에서_10글자이다(String name) {
         //given
@@ -28,7 +28,7 @@ class PlayerTest {
         assertDoesNotThrow(() -> Player.from(name));
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "이름은 공백을 제거하고 2~10글자가 아니면 예외가 발생한다 name = {0}")
     @ValueSource(strings = {"", "1", "abcdqwertyuiopdfghjk"})
     void 이름은_공백을_제외하고_2에서_10글자가_아니면_예외가_발생한다(String name) {
         //given
