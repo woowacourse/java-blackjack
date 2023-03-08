301544c1ba13e97bbcaf1825df8d9bc7d55d4c31
somsom13 <somin1113@naver.com>
2023-03-08 16:58:35 +0900

    test: Answer 에 따라 플레이어의 추가 카드 요청이 결정된다.
    - type.AnswerTest

---
+++ src/test/java/type/AnswerTest.java
@@ -0,0 +1,30 @@
+package type;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class AnswerTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"y:true", "n:false"}, delimiter = ':')
+    @DisplayName("입력값이 y이면 계속 진행, n이면 멈춤이다")
+    void isMoreCardRequested(String answer, boolean isHit) {
+        assertThat(Answer.isMoreCardRequested(answer)).isEqualTo(isHit);
+    }
+
+    @Test
+    @DisplayName("입력값이 y, n 외의 값이 아니면 예외가 발생한다")
+    void createWrongAnswer() {
+        String answer = "a";
+
+        assertThatThrownBy(() -> Answer.validate(answer))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("y 나 n 만을 입력해주세요.");
+    }
+
+}
