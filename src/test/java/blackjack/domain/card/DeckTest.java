3805a860eb6efbd9732a1be9a1c398a86f702ea4
70825 <hello70825@gmail.com>
2023-03-08 13:22:29 +0900

    test: 52장의 트럼프 카드를 모두 생성하는지 확인하는 테스트 추가

--- src/test/java/blackjack/domain/card/DeckTest.java
+++ src/test/java/blackjack/domain/card/DeckTest.java
@@ -1,5 +1,6 @@
 package blackjack.domain.card;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,16 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 public class DeckTest {
+
+    @Test
+    @DisplayName("52장의 트럼프 카드를 모두 생성하는지 테스트")
+    void createAllCardTest() {
+        Deck deck = Deck.createAllCard();
+
+        for (int i = 0; i < 52; i++) {
+            Assertions.assertThatNoException().isThrownBy(deck::draw);
+        }
+    }
 
     @Test
     @DisplayName("중복된 카드를 주입하면 에러를 반환한다")
