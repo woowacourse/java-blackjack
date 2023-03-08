68ea4a06bf9e5b0e7abf456dd050df8425faf3cc
hyunseo <goatsfish@gmail.com>
2023-03-08 12:18:04 +0900

    test: 카드 번호 값, 번호 기호 조회 기능 테스트 추가

--- src/test/java/blackjack/domain/card/CardNumberTest.java
+++ src/test/java/blackjack/domain/card/CardNumberTest.java
@@ -5,38 +5,58 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CardNumberTest {
     @ParameterizedTest
-    @MethodSource("getMaxValueNear21Dummy")
-    @DisplayName("카드 숫자 합이 21에 가장 가까운 값이 되도록 구한다.")
-    void getMaxValueNear21(final List<CardNumber> cardNumbers, final int expectedValue) {
-        int maxValue = CardNumber.getMaxValueNearBlackJack(cardNumbers, 21);
-        assertThat(maxValue).isEqualTo(expectedValue);
+    @MethodSource("getValueDummy")
+    @DisplayName("번호의 값을 가져온다.")
+    void getValue(final CardNumber cardNumber, final int expectedValue) {
+        assertThat(cardNumber.getValue()).isEqualTo(expectedValue);
     }
 
-    static Stream<Arguments> getMaxValueNear21Dummy() {
+    @ParameterizedTest
+    @MethodSource("getNumberDummy")
+    @DisplayName("번호의 기호를 가져온다.")
+    void getNumber(final CardNumber cardNumber, final String expectedNumber) {
+        assertThat(cardNumber.getNumber()).isEqualTo(expectedNumber);
+    }
+
+    static Stream<Arguments> getValueDummy() {
         return Stream.of(
-                Arguments.arguments(List.of(
-                        CardNumber.THREE,
-                        CardNumber.FOUR), 7),
-                Arguments.arguments(List.of(
-                        CardNumber.ACE,
-                        CardNumber.ACE,
-                        CardNumber.NINE), 21),
-                Arguments.arguments(List.of(
-                        CardNumber.TWO,
-                        CardNumber.ACE,
-                        CardNumber.FOUR), 17),
-                Arguments.arguments(List.of(
-                        CardNumber.QUEEN,
-                        CardNumber.JACK,
-                        CardNumber.ACE,
-                        CardNumber.ACE), 22)
+                Arguments.arguments(CardNumber.ACE, 1),
+                Arguments.arguments(CardNumber.TWO, 2),
+                Arguments.arguments(CardNumber.THREE, 3),
+                Arguments.arguments(CardNumber.FOUR, 4),
+                Arguments.arguments(CardNumber.FIVE, 5),
+                Arguments.arguments(CardNumber.SIX, 6),
+                Arguments.arguments(CardNumber.SEVEN, 7),
+                Arguments.arguments(CardNumber.EIGHT, 8),
+                Arguments.arguments(CardNumber.NINE, 9),
+                Arguments.arguments(CardNumber.TEN, 10),
+                Arguments.arguments(CardNumber.KING, 10),
+                Arguments.arguments(CardNumber.QUEEN, 10),
+                Arguments.arguments(CardNumber.JACK, 10)
+        );
+    }
+
+    static Stream<Arguments> getNumberDummy() {
+        return Stream.of(
+                Arguments.arguments(CardNumber.ACE, "A"),
+                Arguments.arguments(CardNumber.TWO, "2"),
+                Arguments.arguments(CardNumber.THREE, "3"),
+                Arguments.arguments(CardNumber.FOUR, "4"),
+                Arguments.arguments(CardNumber.FIVE, "5"),
+                Arguments.arguments(CardNumber.SIX, "6"),
+                Arguments.arguments(CardNumber.SEVEN, "7"),
+                Arguments.arguments(CardNumber.EIGHT, "8"),
+                Arguments.arguments(CardNumber.NINE, "9"),
+                Arguments.arguments(CardNumber.TEN, "10"),
+                Arguments.arguments(CardNumber.KING, "K"),
+                Arguments.arguments(CardNumber.QUEEN, "Q"),
+                Arguments.arguments(CardNumber.JACK, "J")
         );
     }
 }
