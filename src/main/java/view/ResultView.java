17af4d6c4cecc550cb3fd275371f0692bc4b6024
sh111-coder <ohk9134@naver.com>
2023-03-08 21:57:14 +0900

    refactor(ResultView): 상수 유틸 클래스 딜러 이름 사용하도록 리팩토링

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -3,10 +3,11 @@ package view;
 import java.util.List;
 import java.util.Map;
 
+import util.Constants;
+
 public class ResultView {
 
     public static final String JOIN_DELIMITER = ", ";
-    private static final String DEALER_NAME = "딜러";
     private static final int OPEN_CARD_INDEX = 0;
     private static final String FINAL_RESULT_MESSAGE = "\n## 최종 승패";
 
@@ -16,10 +17,10 @@ public class ResultView {
     }
 
     public static void printParticipantResult(String participantName, List<String> cardNames) {
-        if (!participantName.equals(DEALER_NAME)) {
+        if (!participantName.equals(Constants.DEALER_NAME)) {
             System.out.println(participantName + "카드: " + String.join(JOIN_DELIMITER, cardNames));
         }
-        if (participantName.equals(DEALER_NAME)) {
+        if (participantName.equals(Constants.DEALER_NAME)) {
             System.out.println(participantName + ": " + cardNames.get(OPEN_CARD_INDEX));
         }
     }
