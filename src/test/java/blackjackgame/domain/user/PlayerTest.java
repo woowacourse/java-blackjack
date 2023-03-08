fa57c66ebf712db084e422d75db8c6343651e75b
yujamint <woawoa0513@gmail.com>
2023-03-09 00:13:52 +0900

    refactor: Player 생성 테스트 케이스 작성
    Player 생성 직후엔 빈 카드패를 들고 있는 것을 검증한다.

--- src/test/java/blackjackgame/domain/user/PlayerTest.java
+++ src/test/java/blackjackgame/domain/user/PlayerTest.java
@@ -10,15 +10,24 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("플레이어는 ")
 class PlayerTest {
+    @DisplayName("생성 직후엔 빈 카드패를 가지고 있다.")
+    @Test
+    void generatePlayerTest_noCard() {
+        Player player = new Player(new Name("플레이어"));
+
+        assertThat(player.getCards()).isEmpty();
+    }
+
     @DisplayName("카드 한 장을 받아 패에 넣는다.")
     @Test
     void receiveCardTest() {
         Player player = new Player(new Name("플레이어"));
         List<Card> cards = player.getCards();
+        int initialPlayerCardsSize = cards.size();
 
         player.receiveCard(CloverCard.CLOVER_FOUR);
 
-        assertThat(cards.size()).isEqualTo(1);
+        assertThat(cards.size()).isEqualTo(initialPlayerCardsSize + 1);
     }
 
     @DisplayName("합산 점수가 21을 초과하면 Bust 상태가 된다.")
