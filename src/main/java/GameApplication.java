ad7ae0ef2be6e60051e9da255b3b102cfda46e43
apptie <appti@naver.com>
2023-03-08 17:01:59 +0900

    refactor: 도메인과 직접적인 연관이 없는 의존성은 최상위 Application에서 관리하도록 변경한다

--- src/main/java/GameApplication.java
+++ src/main/java/GameApplication.java
@@ -1,11 +1,18 @@
 import controller.GameController;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import view.InputView;
+import view.OutputView;
 
 public class GameApplication {
 
     public static void main(String[] args) {
-        final GameApplicationConfig config = new GameApplicationConfig();
-        final GameController gamePrepareController = config.gameController();
+        final InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        final InputView inputView = new InputView(bufferedReader);
+        final OutputView outputView = new OutputView();
 
-        gamePrepareController.start();
+        final GameController gameController = new GameController(inputView, outputView);
+        gameController.start();
     }
 }
