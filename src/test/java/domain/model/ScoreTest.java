91121ac2676c7e445227ffaafbafc2034e103709
ChoiYoungHoon <dudgns2865@naver.com>
2023-03-08 15:57:11 +0900

    test: Score 이하 확인 기능 테스트 작성

--- src/test/java/domain/model/ScoreTest.java
+++ src/test/java/domain/model/ScoreTest.java
@@ -166,4 +166,18 @@ class ScoreTest {
         //then
         assertThat(result).isFalse();
     }
+
+    @Test
+    @DisplayName("스코어가 비교 대상보다 더 작은지 테스트")
+    public void testIsLessThan() {
+        //given
+        Score score = Score.of(1);
+        Score comparedScore = Score.of(2);
+
+        //when
+        boolean result = score.isLessThan(comparedScore);
+
+        //then
+        assertThat(result).isTrue();
+    }
 }
