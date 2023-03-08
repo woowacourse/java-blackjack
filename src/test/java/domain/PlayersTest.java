3213e83c55edb1cc78397579be7db6aa29998815
eunbii0213 <eunbii0213@naver.com>
2023-03-09 02:45:17 +0900

    test: Result 테스트 코드 작성

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -15,21 +16,33 @@ class PlayersTest {
 
     @Test
     void player는_5명까지_참여_가능하다() {
-        //given,//when
+        //given, when
         List<String> playerNames = List.of("judy", "jude", "pobi", "lio", "neo", "joan");
+        List<Player> players = getPlayers(playerNames);
 
         //then
-        assertThatThrownBy(() -> new Players(playerNames))
+        assertThatThrownBy(() -> new Players(players))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 플레어이는 5명까지 참가 가능합니다.");
+    }
+
+    private List<Player> getPlayers(List<String> playerNames) {
+        List<Player> players = new ArrayList<>();
+
+        for (String playerName : playerNames) {
+            players.add(new Player(new PlayerName(playerName), new Cards(new ArrayList<>())));
+        }
+        return players;
     }
 
     @Test
     void player는_중복된_이름을_가질_수_없다() {
         //given, when
         List<String> playerNames = List.of("judy", "judy");
+        List<Player> players = getPlayers(playerNames);
+
         //then
-        assertThatThrownBy(() -> new Players(playerNames))
+        assertThatThrownBy(() -> new Players(players))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 플레이어 이름은 중복일 수 없습니다.");
     }
@@ -37,15 +50,15 @@ class PlayersTest {
     @Test
     void 올바른_입력이_들어왔을_때_잘_생성되는지_확인한다() {
         //given
-        new Deck();
         List<String> playerNames = List.of("judy", "kevin");
-        Players players = new Players(playerNames);
+        List<Player> players = getPlayers(playerNames);
+        Players playersEntity = new Players(players);
 
         // when
         String[] expected = {"judy", "kevin"};
         String[] actual = new String[2];
 
-        List<Player> playersPlayers = players.getPlayers();
+        List<Player> playersPlayers = playersEntity.getPlayers();
         for (int i = 0; i < playersPlayers.size(); i++) {
             Player player = playersPlayers.get(i);
             actual[i] = player.getName();
