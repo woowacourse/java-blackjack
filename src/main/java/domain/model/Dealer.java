e902189c1951983f4fb8e5c10cf02dba82cec1f6
ChoiYoungHoon <dudgns2865@naver.com>
2023-03-08 15:58:18 +0900

    refactor: canReceiveCard 로직 변경

--- src/main/java/domain/model/Dealer.java
+++ src/main/java/domain/model/Dealer.java
@@ -1,14 +1,20 @@
 package domain.model;
 
+import domain.vo.Score;
+
 public class Dealer extends Participant {
 
     private static final int MIN_SCORE_THRESHOLD = 16;
+
+    public Dealer() {
+        this(Cards.makeEmptyCards());
+    }
 
     public Dealer(final Cards cards) {
         super(cards);
     }
 
     public boolean canReceiveCard() {
-        return super.getScore().getValue() <= MIN_SCORE_THRESHOLD;
+        return super.getScore().isLessThan(Score.of(MIN_SCORE_THRESHOLD));
     }
 }
