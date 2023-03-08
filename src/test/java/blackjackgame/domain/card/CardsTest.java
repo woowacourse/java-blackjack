f171733bd8b49aa9b1084bf2987b7d73fbf8e307
yujamint <woawoa0513@gmail.com>
2023-03-09 00:44:27 +0900

    refactor: Cards 테스트 개선
    - FixedCardsGenerator은 하나의 메서드에서만 쓰였기 때문에 해당 클래스를 사용하지 않고 람다식을 통해 CardsGenerator 인터페이스 구현하며 List를 반환하는 방법으로 변경
    - 변수명 개선

--- src/test/java/blackjackgame/domain/card/CardsTest.java
+++ src/test/java/blackjackgame/domain/card/CardsTest.java
@@ -26,13 +26,15 @@ class CardsTest {
     @DisplayName("가장 위에 있는 카드를 뽑고, 덱에서 없앤다.")
     @Test
     void drawCardTest() {
-        Cards cards = new Cards(new FixedCardsGenerator());
+        Cards cards = new Cards(() -> new ArrayList<>(
+                List.of(CloverCard.CLOVER_FIVE, HeartCard.HEART_JACK, SpadeCard.SPADE_FOUR, DiamondCard.DIAMOND_SEVEN)
+        ));
 
-        Card firstCards = cards.drawCard();
-        Card secondCards = cards.drawCard();
+        Card firstCard = cards.drawCard();
+        Card secondCard = cards.drawCard();
 
-        assertThat(firstCards).isEqualTo(CloverCard.CLOVER_FIVE);
-        assertThat(secondCards).isEqualTo(HeartCard.HEART_JACK);
+        assertThat(firstCard).isEqualTo(CloverCard.CLOVER_FIVE);
+        assertThat(secondCard).isEqualTo(HeartCard.HEART_JACK);
     }
 
     @DisplayName("카드를 두 장씩 뽑아서 반환할 수 있다.")
@@ -43,22 +45,5 @@ class CardsTest {
         List<Card> firstTurnCards = cards.drawCards(2);
 
         assertThat(firstTurnCards.size()).isEqualTo(2);
-    }
-
-    private class FixedCardsGenerator implements CardsGenerator {
-
-        /*
-        5클로버, J하트, 4스페이드, 7다이아몬드, A하트, 6클로버, 8클로버, 9다이아몬드, 7하트
-        위 순서대로 담겨져 있는 List<Card>를 반환
-        */
-        @Override
-        public List<Card> generate() {
-            return new ArrayList<>(
-                    List.of(CloverCard.CLOVER_FIVE, HeartCard.HEART_JACK, SpadeCard.SPADE_FOUR,
-                            DiamondCard.DIAMOND_SEVEN, HeartCard.HEART_ACE, CloverCard.CLOVER_SIX,
-                            CloverCard.CLOVER_EIGHT,
-                            DiamondCard.DIAMOND_NINE, HeartCard.HEART_SEVEN)
-            );
-        }
     }
 }
