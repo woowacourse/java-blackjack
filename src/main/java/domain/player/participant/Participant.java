ef49791af44234344c1677b5b2f0d5425fbed225
java-saeng <gusdn85554@naver.com>
2023-03-08 16:35:45 +0900

    feat : 참여자는 자신의 베팅 결과에 따라 베팅액 결과 금액을 반환한다.

--- src/main/java/domain/player/participant/Participant.java
+++ src/main/java/domain/player/participant/Participant.java
@@ -7,9 +7,11 @@ import domain.player.participant.betresult.BetResultState;
 public class Participant extends Player {
 
     private BetResultState betResultState;
+    private final Money money;
 
-    public Participant(final Name name) {
+    public Participant(final Name name, final Money money) {
         super(name);
+        this.money = money;
         betResultState = null;
     }
 
@@ -20,5 +22,9 @@ public class Participant extends Player {
 
     public void determineBetState(final BetResultState betResultState) {
         this.betResultState = betResultState;
+    }
+
+    public Money determineBetMoney() {
+        return betResultState.calculateBetOutComeOf(money);
     }
 }
--- src/test/java/domain/player/participant/ParticipantTest.java
+++ src/test/java/domain/player/participant/ParticipantTest.java
@@ -4,22 +4,30 @@ import domain.card.Card;
 import domain.player.Name;
 import domain.player.participant.betresult.BetResultState;
 import domain.player.participant.betresult.BreakEvenState;
+import domain.player.participant.betresult.LoseState;
+import domain.player.participant.betresult.WinState;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static domain.card.CardShape.CLOVER;
 import static domain.card.CardShape.SPADE;
 import static domain.card.CardValue.ACE;
 import static domain.card.CardValue.TEN;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Participant 은")
 class ParticipantTest {
 
-    final Participant participant = new Participant(new Name("name"));
+    final Participant participant = new Participant(new Name("name"), Money.wons(0));
 
     @BeforeEach
     void makeCardScoreTwenty() {
@@ -57,5 +65,31 @@ class ParticipantTest {
         //then
         assertThat(participant).extracting("betResultState")
                                .isInstanceOf(BetResultState.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("betResultState")
+    @DisplayName("determineBetMoney() : 참여자는 베팅 결과에 따라 베팅 금액을 반환할 수 있다.")
+    void test_determineBetMoney(final Participant participant, final Money bettingResultMoney) throws Exception {
+        //when & then
+        assertEquals(bettingResultMoney, participant.determineBetMoney());
+    }
+
+    static Stream<Arguments> betResultState() {
+
+        Participant participant1 = new Participant(new Name("name1"), Money.wons(10000));
+        participant1.determineBetState(new WinState());
+
+        Participant participant2 = new Participant(new Name("name2"), Money.wons(5000));
+        participant2.determineBetState(new LoseState());
+
+        Participant participant3 = new Participant(new Name("name3"), Money.wons(1000));
+        participant3.determineBetState(new BreakEvenState());
+
+        return Stream.of(
+                Arguments.of(participant1, Money.wons(15000)),
+                Arguments.of(participant2, Money.wons(-5000)),
+                Arguments.of(participant3, Money.wons(1000))
+        );
     }
 }
