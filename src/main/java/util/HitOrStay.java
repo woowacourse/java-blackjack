d6ec6f21df595dc0051460e386fdb1891c30aad9
junpakPark <junpak.park@gmail.com>
2023-03-08 23:13:31 +0900

    refactor : 함수형 인터페이스 어노테이션 추가

--- src/main/java/util/HitOrStay.java
+++ src/main/java/util/HitOrStay.java
@@ -2,6 +2,7 @@ package util;
 
 import domain.player.Participant;
 
+@FunctionalInterface
 public interface HitOrStay {
 
     boolean isHit(Participant participant);
--- src/main/java/util/Notice.java
+++ src/main/java/util/Notice.java
@@ -1,5 +1,6 @@
 package util;
 
+@FunctionalInterface
 public interface Notice<T> {
 
     void print(T t);
