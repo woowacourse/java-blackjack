b4a7fb3a2943a9b8bc94943b35632ab11caa8627
JunDonghyuk <ehdgur4814@naver.com>
2023-03-09 03:03:26 +0900

    refactor: Name final 클래스로 변경

--- src/test/java/domain/player/NameTest.java
+++ src/test/java/domain/player/NameTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class NameTest {
+
     @DisplayName("이름의 길이는 1글자 미만 15글자 초과일 경우 예외처리한다.")
     @ParameterizedTest
     @ValueSource(strings = {"", "1234567890123456", "12345678901234567"})
