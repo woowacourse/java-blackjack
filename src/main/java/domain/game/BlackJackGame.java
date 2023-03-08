20dcce627b1a3fb922748341d1fc2a52e3c1fa50
JunDonghyuk <ehdgur4814@naver.com>
2023-03-08 16:22:23 +0900

    refactor: 블랙젝게임 결과 책임 Outcome으로 이관

--- src/main/java/domain/game/BlackJackGame.java
+++ src/main/java/domain/game/BlackJackGame.java
@@ -3,11 +3,9 @@ package domain.game;
 import domain.deck.Card;
 import domain.deck.Deck;
 import domain.player.Dealer;
-import domain.player.Player;
 import domain.player.Players;
 
 import java.util.EnumMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -35,55 +33,17 @@ public class BlackJackGame {
 
     public EnumMap<Outcome, Integer> decideDealerOutcome() {
         Map<String, Outcome> playerOutcome = decidePlayersOutcome();
-        EnumMap<Outcome, Integer> dealerOutcome = initializeDealerOutcome();
+        EnumMap<Outcome, Integer> dealerOutcome = Outcome.initializeOutcomes();
         for (String key : playerOutcome.keySet()) {
             Outcome outcome = playerOutcome.get(key);
-            Outcome dealerEachOutcome = reverseOutcome(outcome);
+            Outcome dealerEachOutcome = Outcome.reverseOutcome(outcome);
             dealerOutcome.put(dealerEachOutcome, dealerOutcome.get(dealerEachOutcome) + 1);
         }
         return dealerOutcome;
     }
 
-    private static EnumMap<Outcome, Integer> initializeDealerOutcome() {
-        EnumMap<Outcome, Integer> dealerOutcome = new EnumMap<>(Outcome.class);
-
-        for (Outcome outcome : Outcome.values()) {
-            dealerOutcome.put(outcome, 0);
-        }
-
-        return dealerOutcome;
-    }
-
     public Map<String, Outcome> decidePlayersOutcome() {
-        Map<String, Outcome> playerOutcome = new LinkedHashMap<>();
-        final int dealerScore = findDealer().getScore();
-        final List<Player> players = this.players.getPlayersWithOutDealer();
-
-        players.forEach((player ->
-                playerOutcome.put(player.getName(), decideOutcome(dealerScore, player))
-        ));
-
-        return playerOutcome;
-    }
-
-    private Outcome reverseOutcome(final Outcome outcome) {
-        if (outcome == Outcome.WIN) {
-            return Outcome.LOSE;
-        }
-        if(outcome == Outcome.LOSE) {
-            return Outcome.WIN;
-        }
-        return Outcome.DRAW;
-    }
-
-    private Outcome decideOutcome(final int dealerScore, final Player player) {
-        if (player.isWin(dealerScore)) {
-            return Outcome.WIN;
-        }
-        if (player.isDraw(dealerScore)) {
-            return Outcome.DRAW;
-        }
-        return Outcome.LOSE;
+        return Outcome.decidePlayersOutcome(findDealer().getScore(), this.players.getPlayersWithOutDealer());
     }
 
     public boolean isOver21(final String playerName) {
--- src/main/java/domain/game/Outcome.java
+++ src/main/java/domain/game/Outcome.java
@@ -1,5 +1,51 @@
 package domain.game;
 
+import domain.player.Player;
+
+import java.util.EnumMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public enum Outcome {
-    WIN, DRAW, LOSE
+    WIN, DRAW, LOSE;
+
+    public static EnumMap<Outcome, Integer> initializeOutcomes() {
+        EnumMap<Outcome, Integer> dealerOutcome = new EnumMap<>(Outcome.class);
+
+        for (Outcome outcome : Outcome.values()) {
+            dealerOutcome.put(outcome, 0);
+        }
+
+        return dealerOutcome;
+    }
+
+    public static Outcome reverseOutcome(final Outcome outcome) {
+        if (outcome == Outcome.WIN) {
+            return Outcome.LOSE;
+        }
+        if(outcome == Outcome.LOSE) {
+            return Outcome.WIN;
+        }
+        return Outcome.DRAW;
+    }
+
+    public static Map<String, Outcome> decidePlayersOutcome(final int dealerScore, final List<Player> players) {
+        Map<String, Outcome> playerOutcome = new LinkedHashMap<>();
+        players.forEach((player ->
+                playerOutcome.put(player.getName(), decideOutcome(dealerScore, player))
+        ));
+
+        return playerOutcome;
+    }
+
+    private static Outcome decideOutcome(final int dealerScore, final Player player) {
+        if (player.isWin(dealerScore)) {
+            return Outcome.WIN;
+        }
+        if (player.isDraw(dealerScore)) {
+            return Outcome.DRAW;
+        }
+        return Outcome.LOSE;
+    }
 }
