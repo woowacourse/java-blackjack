e45920587844b2d9bb4388f22d5a298655451294
splitube <voicesplit@daum.net>
2023-03-09 21:58:25 +0900

    style: @DisplayName 내용 수정, 주석 삭제

--- src/test/java/domain/BlackJackGameTest.java
+++ src/test/java/domain/BlackJackGameTest.java
@@ -30,7 +30,7 @@ class BlackJackGameTest {
     class playTurn {
 
         @Test
-        @DisplayName("카드를 받음")
+        @DisplayName("플레이어가 카드를 받음")
         void playTurnHit() {
             PlayerBoards playerBoards = PlayerBoards.from(List.of("split"));
             BlackJackGame blackJackGame = BlackJackGame.from(playerBoards);
@@ -40,7 +40,7 @@ class BlackJackGameTest {
         }
 
         @Test
-        @DisplayName("카드를 받지 않음")
+        @DisplayName("플레이어가 카드를 받지 않음")
         void playTurnStand() {
             PlayerBoards playerBoards = PlayerBoards.from(List.of("split"));
             BlackJackGame blackJackGame = BlackJackGame.from(playerBoards);
@@ -49,42 +49,4 @@ class BlackJackGameTest {
             Assertions.assertThat(currentPlayerBoard.getPlayer().getHand()).hasSize(0);
         }
     }
-//
-//    @DisplayName("딜러의 점수가 16이하인지 확인한다.")
-//    @Nested
-//    class isDealerUnderThresholds {
-//
-//        @Test
-//        @DisplayName("16점일 때")
-//        void isDealerUnderThresholdsTrue() {
-//            BlackJackGame blackJackGame = BlackJackGame.from("split");
-//            Dealer dealer = new Dealer();
-//            dealer.dealt(new Card(Denomination.JACK, Suit.DIAMOND));
-//            dealer.dealt(new Card(Denomination.SIX, Suit.DIAMOND));
-//            Assertions.assertThat(blackJackGame.dealerNeedMoreCard(dealer)).isTrue();
-//        }
-//
-//        @Test
-//        @DisplayName("17점일 때")
-//        void isDealerUnderThresholdsFalse() {
-//            BlackJackGame blackJackGame = BlackJackGame.from("split");
-//            Dealer dealer = new Dealer();
-//            dealer.dealt(new Card(Denomination.JACK, Suit.DIAMOND));
-//            dealer.dealt(new Card(Denomination.SEVEN, Suit.DIAMOND));
-//            Assertions.assertThat(blackJackGame.dealerNeedMoreCard(dealer)).isFalse();
-//        }
-//    }
-//
-//
-//    @Test
-//    @DisplayName("플레이어들의 게임결과로 딜러의 게임결과를 생성한다.")
-//    void playTurn() {
-//        BlackJackGame blackJackGame = BlackJackGame.from("split");
-//        GameResult dealerBoxResult = blackJackGame.getDealerGameResult(List.of(
-//            new GameResult(5, 0),
-//            new GameResult(0, 3))
-//        );
-//        Assertions.assertThat(dealerBoxResult.getWinCount()).isEqualTo(3);
-//        Assertions.assertThat(dealerBoxResult.getLoseCount()).isEqualTo(5);
-//    }
 }
