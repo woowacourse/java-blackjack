0cd17eb65e79879c1b2522cd10f3363a59e57c8a
hectick <chyeon1223@cau.ac.kr>
2023-03-09 16:40:22 +0900

    test: 플레이어가 버스트 일 때 승패 판단 테스트 작성

--- src/test/java/blackjack/model/result/ResultTest.java
+++ src/test/java/blackjack/model/result/ResultTest.java
@@ -7,6 +7,7 @@ import blackjack.model.card.CardSuit;
 import blackjack.model.participant.*;
 import blackjack.model.state.InitialState;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -15,75 +16,101 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ResultTest {
 
-    @Test
-    @DisplayName("플레이어가 딜러를 이겼을 때 WIN 을 반환한다.")
-    void player_win() {
-        //given
-        Card card1 = Card.of(CardSuit.CLUB, CardNumber.ACE);
-        Card card2 = Card.of(CardSuit.HEART, CardNumber.JACK);
-        Card card3 = Card.of(CardSuit.CLUB, CardNumber.TWO);
-        Card card4 = Card.of(CardSuit.HEART, CardNumber.THREE);
-        CardDeck cardDeck = new CardDeck(List.of(card1, card2, card3, card4));
-        Dealer dealer = new Dealer(new InitialState(new Hand()));
-        Player player = new Player(new Name("이리내"), new InitialState(new Hand()));
+    @Nested
+    @DisplayName("플레이어가 버스트일 때")
+    class player_bust {
 
-        //when
-        dealer.play(cardDeck);
-        player.play(cardDeck);
-        Result result = Result.checkPlayerResult(player, dealer);
+        @Nested
+        @DisplayName("딜러가 버스트라면")
+        class dealer_bust {
+            @Test
+            @DisplayName("플레이어가 패배한다")
+            void player_lose() {
+                //given
+                Card card1 = Card.of(CardSuit.SPADE, CardNumber.SIX);
+                Card card2 = Card.of(CardSuit.HEART, CardNumber.NINE);
+                Card card3 = Card.of(CardSuit.CLUB, CardNumber.QUEEN);
+                Card card4 = Card.of(CardSuit.HEART, CardNumber.TEN);
+                Card card5 = Card.of(CardSuit.DIAMOND, CardNumber.SIX);
+                Card card6 = Card.of(CardSuit.SPADE, CardNumber.TEN);
+                CardDeck cardDeck = new CardDeck(List.of(card1, card2, card3, card4, card5, card6));
+                Dealer dealer = new Dealer(new InitialState(new Hand()));
+                Player player = new Player(new Name("이리내"), new InitialState(new Hand()));
+                Participants participants = new Participants(dealer, List.of(player));
 
-        //then
-        assertThat(result).isEqualTo(Result.WIN);
-    }
+                //when
+                participants.distributeTwoCardsToEach(cardDeck);
+                participants.hitOrStandByPlayer(cardDeck, player, true);
+                participants.hitOrStandByDealer(cardDeck);
+                Result result = Result.checkPlayerResult(player, dealer);
 
-    @Test
-    @DisplayName("플레이어와 딜러가 비겼을 때 TIE 를 반환한다.")
-    void player_tie() {
-        //given
-        Card card1 = Card.of(CardSuit.CLUB, CardNumber.FOUR);
-        Card card2 = Card.of(CardSuit.HEART, CardNumber.FOUR);
-        Card card3 = Card.of(CardSuit.CLUB, CardNumber.THREE);
-        Card card4 = Card.of(CardSuit.HEART, CardNumber.TEN);
-        Card card5 = Card.of(CardSuit.DIAMOND, CardNumber.THREE);
-        Card card6 = Card.of(CardSuit.SPADE, CardNumber.TEN);
-        CardDeck cardDeck = new CardDeck(List.of(card1, card2, card3, card4, card5, card6));
-        Dealer dealer = new Dealer(new InitialState(new Hand()));
-        Player player = new Player(new Name("이리내"), new InitialState(new Hand()));
-        Participants participants = new Participants(dealer, List.of(player));
+                //then
+                assertThat(player.isBust()).isTrue();
+                assertThat(dealer.isBust()).isTrue();
+                assertThat(result).isEqualTo(Result.LOSE);
+            }
+        }
 
-        //when
-        participants.distributeTwoCardsToEach(cardDeck);
-        participants.hitOrStandByPlayer(cardDeck, player, true);
-        participants.hitOrStandByPlayer(cardDeck, player, false);
-        participants.hitOrStandByDealer(cardDeck);
-        Result result = Result.checkPlayerResult(player, dealer);
+        @Nested
+        @DisplayName("딜러가 블랙잭이라면")
+        class dealer_blackjack {
+            @Test
+            @DisplayName("플레이어가 패배한다")
+            void player_lose() {
+                //given
+                Card card1 = Card.of(CardSuit.HEART, CardNumber.FIVE);
+                Card card2 = Card.of(CardSuit.CLUB, CardNumber.QUEEN);
+                Card card3 = Card.of(CardSuit.HEART, CardNumber.TEN);
+                Card card4 = Card.of(CardSuit.DIAMOND, CardNumber.ACE);
+                Card card5 = Card.of(CardSuit.SPADE, CardNumber.TEN);
+                CardDeck cardDeck = new CardDeck(List.of(card1, card2, card3, card4, card5));
+                Dealer dealer = new Dealer(new InitialState(new Hand()));
+                Player player = new Player(new Name("이리내"), new InitialState(new Hand()));
+                Participants participants = new Participants(dealer, List.of(player));
 
-        //then
-        assertThat(result).isEqualTo(Result.TIE);
-    }
+                //when
+                participants.distributeTwoCardsToEach(cardDeck);
+                participants.hitOrStandByPlayer(cardDeck, player, true);
+                participants.hitOrStandByDealer(cardDeck);
+                Result result = Result.checkPlayerResult(player, dealer);
 
-    @Test
-    @DisplayName("플레이어가 딜러에게 졌을 때 LOSE 를 반환한다.")
-    void player_lose() {
-        //given
-        Card card1 = Card.of(CardSuit.HEART, CardNumber.FIVE);
-        Card card2 = Card.of(CardSuit.CLUB, CardNumber.QUEEN);
-        Card card3 = Card.of(CardSuit.HEART, CardNumber.TEN);
-        Card card4 = Card.of(CardSuit.DIAMOND, CardNumber.ACE);
-        Card card5 = Card.of(CardSuit.SPADE, CardNumber.TEN);
-        CardDeck cardDeck = new CardDeck(List.of(card1, card2, card3, card4, card5));
-        Dealer dealer = new Dealer(new InitialState(new Hand()));
-        Player player = new Player(new Name("이리내"), new InitialState(new Hand()));
-        Participants participants = new Participants(dealer, List.of(player));
+                //then
+                assertThat(player.isBust()).isTrue();
+                assertThat(dealer.isBlackjack()).isTrue();
+                assertThat(result).isEqualTo(Result.LOSE);
+            }
+        }
 
-        //when
-        participants.distributeTwoCardsToEach(cardDeck);
-        participants.hitOrStandByPlayer(cardDeck, player, true);
-        participants.hitOrStandByDealer(cardDeck);
-        Result result = Result.checkPlayerResult(player, dealer);
+        @Nested
+        @DisplayName("딜러가 스탠드라면")
+        class dealer_stand {
+            @Test
+            @DisplayName("플레이어가 패배한다")
+            void player_lose() {
+                //given
+                Card card1 = Card.of(CardSuit.HEART, CardNumber.FIVE);
+                Card card2 = Card.of(CardSuit.CLUB, CardNumber.QUEEN);
+                Card card3 = Card.of(CardSuit.HEART, CardNumber.TEN);
+                Card card4 = Card.of(CardSuit.DIAMOND, CardNumber.SEVEN);
+                Card card5 = Card.of(CardSuit.SPADE, CardNumber.TEN);
+                CardDeck cardDeck = new CardDeck(List.of(card1, card2, card3, card4, card5));
+                Dealer dealer = new Dealer(new InitialState(new Hand()));
+                Player player = new Player(new Name("이리내"), new InitialState(new Hand()));
+                Participants participants = new Participants(dealer, List.of(player));
 
-        //then
-        assertThat(result).isEqualTo(Result.LOSE);
+                //when
+                participants.distributeTwoCardsToEach(cardDeck);
+                participants.hitOrStandByPlayer(cardDeck, player, true);
+                participants.hitOrStandByDealer(cardDeck);
+                Result result = Result.checkPlayerResult(player, dealer);
+
+                //then
+                assertThat(player.isBust()).isTrue();
+                assertThat(dealer.isStand()).isTrue();
+                assertThat(result).isEqualTo(Result.LOSE);
+            }
+        }
+
     }
 
 }
