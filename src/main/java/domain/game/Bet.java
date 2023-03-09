ef765e378b159f502ca29658edda256532c97a6a
echo <echo.backend@gmail.com>
2023-03-10 08:58:46 +0900

    feat: Bet 예외 추가

--- src/main/java/domain/game/Bet.java
+++ src/main/java/domain/game/Bet.java
@@ -2,10 +2,18 @@ package domain.game;
 
 public class Bet {
     
+    public static final String NEGATIVE_BET_NUMBER_ERROR_MESSAGE = "베팅 금액은 0보다 커야 합니다.";
     private final int bet;
     
     public Bet(int bet) {
+        this.validateNegativeBet(bet);
         this.bet = bet;
+    }
+    
+    private void validateNegativeBet(final int bet) {
+        if (bet < 0) {
+            throw new IllegalArgumentException(NEGATIVE_BET_NUMBER_ERROR_MESSAGE);
+        }
     }
     
     public int getBet() {
--- src/test/java/domain/BetTest.java
+++ src/test/java/domain/BetTest.java
@@ -13,4 +13,12 @@ class BetTest {
         Bet bet = new Bet(1000);
         Assertions.assertThat(bet.getBet()).isEqualTo(1000);
     }
+    
+    @Test
+    @DisplayName("Bet 생성 예외 테스트 - 0보다 작은 값")
+    void createException() {
+        Assertions.assertThatThrownBy(() -> new Bet(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("베팅 금액은 0보다 커야 합니다.");
+    }
 }
