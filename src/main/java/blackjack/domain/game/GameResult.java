f1b19d3d0dbddd14eb704767a73c277392ce6e9a
Choi-JJunho <junho5336@gmail.com>
2023-03-09 13:53:25 +0900

    feat: 배당률 추가

--- src/main/java/blackjack/domain/game/GameResult.java
+++ src/main/java/blackjack/domain/game/GameResult.java
@@ -2,14 +2,21 @@ package blackjack.domain.game;
 
 public enum GameResult {
 
-    WIN("승"),
-    LOSE("패"),
-    DRAW("무");
+    WIN("승", 1),
+    LOSE("패", -1),
+    DRAW("무", 0),
+    BLACK_JACK("블랙잭", 1.5);
 
     private final String view;
+    private final double times;
 
-    GameResult(final String view) {
+    GameResult(final String view, final double times) {
         this.view = view;
+        this.times = times;
+    }
+
+    public double getTimes() {
+        return times;
     }
 
     public String getView() {
