d22b1b38c32e8b351d4a7da32b902c1364737c3b
hyunseo <goatsfish@gmail.com>
2023-03-10 04:01:54 +0900

    refactor: 애플리케이션 덱 다형성 수정

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -1,11 +1,12 @@
 package blackjack;
 
 import blackjack.controller.BlackJackController;
+import blackjack.domain.card.CardDeck;
 import blackjack.domain.card.Deck;
 
 public class Application {
     public static void main(String[] args) {
-        final Deck deck = new Deck();
+        final Deck deck = new CardDeck();
         final BlackJackController blackJackController = new BlackJackController(deck);
 
         blackJackController.showParticipantsInitCards();
