fae79203a5c461645341708023dade91b3bbc686
hyunseo <goatsfish@gmail.com>
2023-03-10 03:39:15 +0900

    test: 참여자 카드 생성 픽스처 수정

--- src/test/java/blackjack/fixture/ParticipantCardsFixture.java
+++ src/test/java/blackjack/fixture/ParticipantCardsFixture.java
@@ -1,13 +1,27 @@
 package blackjack.fixture;
 
 import blackjack.domain.card.Card;
+import blackjack.domain.card.Deck;
 import blackjack.domain.game.ParticipantCards;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class ParticipantCardsFixture {
     public static ParticipantCards create(final Card one, final Card two, final List<Card> cards) {
-        ParticipantCards participantCards = new ParticipantCards(List.of(one, two));
+        final List<Card> newCards = new ArrayList<>();
+        newCards.add(one);
+        newCards.add(two);
+
+        final Deck deck = DeckMock.create(newCards);
+        final ParticipantCards participantCards = new ParticipantCards(deck);
+        cards.forEach(participantCards::receive);
+
+        return participantCards;
+    }
+
+    public static ParticipantCards create(final Deck deck, final List<Card> cards) {
+        ParticipantCards participantCards = new ParticipantCards(deck);
         cards.forEach(participantCards::receive);
         return participantCards;
     }
