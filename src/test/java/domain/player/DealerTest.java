5b70cbff52dfd9b647d920036f70591eaa3c510e
cpot5620 <=cpot5620@naver.com>
2023-03-09 18:03:18 +0900

    test: given/when/then 적용

--- src/test/java/domain/player/DealerTest.java
+++ src/test/java/domain/player/DealerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,6 +23,7 @@ class DealerTest {
     @DisplayName("딜러의 카드 합을 기준으로 플레이어의 승패를 결정한다.")
     @Test
     void decideResultSuccessTest() {
+        //given
         Dealer dealer = new Dealer();
         Players players = Players.from(
                 List.of(
@@ -32,33 +34,40 @@ class DealerTest {
         Player pobi = players.getPlayers().get(0);
         Player crong = players.getPlayers().get(1);
 
+        //when
         giveCardsTo(dealer, List.of(DIAMOND_TEN, DIAMOND_NINE)); // 19
-
         giveCardsTo(pobi, List.of(DIAMOND_TEN, DIAMOND_TEN)); // 20
         giveCardsTo(crong, List.of(DIAMOND_TEN, DIAMOND_EIGHT)); // 18
 
         dealer.decideResults(players);
 
-        assertThat(dealer.getGameResult().values())
-                .containsExactly(Result.LOSE, Result.WIN);
+        //then
+        Map<Player, Result> gameResult = dealer.getGameResult();
+        assertThat(gameResult.get(pobi))
+                .isEqualTo(Result.WIN);
+        assertThat(gameResult.get(crong))
+                .isEqualTo(Result.LOSE);
     }
 
     @DisplayName("카드의 합이 같더라도, 딜러가 블랙잭일 경우, 딜러가 승리한다. ")
     @Test
     void decideResultWithBlackjackTest() {
+        //given
         Dealer dealer = new Dealer();
         Players players = Players.from(
                 List.of(Player.of(Name.from("pobi"), BettingMoney.from(1000)))
         );
         Player pobi = players.getPlayers().get(0);
 
+        //when
         giveCardsTo(dealer, List.of(DIAMOND_ACE, DIAMOND_TEN));
         giveCardsTo(pobi, List.of(DIAMOND_NINE, DIAMOND_EIGHT, DIAMOND_FOUR));
 
         dealer.decideResults(players);
 
+        //then
         assertThat(dealer.getGameResult().values())
-                .containsExactly(Result.WIN);
+                .containsExactly(Result.LOSE);
     }
 
     public void giveCardsTo(Participant participant, List<Card> cards) {
@@ -67,3 +76,4 @@ class DealerTest {
         }
     }
 }
+
