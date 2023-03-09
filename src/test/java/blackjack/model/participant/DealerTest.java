0b076836249186d424f253d24f22a0154182f52c
hectick <chyeon1223@cau.ac.kr>
2023-03-09 19:39:32 +0900

    test: 딜러 테스트 코드 작성

--- src/test/java/blackjack/model/participant/DealerTest.java
+++ src/test/java/blackjack/model/participant/DealerTest.java
@@ -5,15 +5,53 @@ import blackjack.model.card.CardDeck;
 import blackjack.model.card.CardNumber;
 import blackjack.model.card.CardSuit;
 import blackjack.model.state.DealerDrawState;
+import blackjack.model.state.InitialState;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class DealerTest {
+
+    @Test
+    @DisplayName("게임을 시작하면 딜러는 두 장의 카드를 지급받는다.")
+    void player_initial_state_draw() {
+        //given
+        Card card1 = Card.of(CardSuit.HEART, CardNumber.FIVE);
+        Card card2 = Card.of(CardSuit.HEART, CardNumber.EIGHT);
+        List<Card> cards = List.of(card1, card2);
+        Dealer dealer = new Dealer(new InitialState(new Hand()));
+        CardDeck cardDeck = new CardDeck(cards);
+
+        // when
+        dealer.play(cardDeck);
+
+        //then
+        assertThat(dealer.getHand().getCards()).containsExactly(card2, card1);
+    }
+
+    @Test
+    @DisplayName("딜러는 점수가 16 이하면 카드를 뽑을 수 있다.")
+    void dealer_can_draw_under_16() {
+        //given
+        Card card1 = Card.of(CardSuit.HEART, CardNumber.FIVE);
+        Card card2 = Card.of(CardSuit.HEART, CardNumber.THREE);
+        Card card3 = Card.of(CardSuit.HEART, CardNumber.EIGHT);
+        List<Card> cards = List.of(card1, card2, card3);
+        Dealer dealer = new Dealer(new InitialState(new Hand()));
+        CardDeck cardDeck = new CardDeck(cards);
+
+        // when
+        dealer.play(cardDeck);
+
+        //then
+        assertThatCode(() -> dealer.play(cardDeck)).doesNotThrowAnyException();
+    }
 
     @Test
     @DisplayName("딜러는 점수가 16 초과이면 카드를 더 뽑지 못한다.")
@@ -35,5 +73,62 @@ class DealerTest {
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("스탠드 상태에서는 카드를 더 뽑을 수 없습니다.");
     }
+
+    @Test
+    @DisplayName("딜러는 점수가 16 초과이면 스탠드 상태로 전환된다.")
+    void dealer_transit_to_stand_over_16() {
+        //given
+        Card card1 = Card.of(CardSuit.CLUB, CardNumber.EIGHT);
+        Card card2 = Card.of(CardSuit.HEART, CardNumber.FIVE);
+        Card card3 = Card.of(CardSuit.HEART, CardNumber.FIVE);
+        Card card4 = Card.of(CardSuit.HEART, CardNumber.EIGHT);
+        List<Card> cards = List.of(card1, card2, card3, card4);
+        Dealer dealer = new Dealer(new InitialState(new Hand()));
+        CardDeck cardDeck = new CardDeck(cards);
+
+        // when
+        dealer.play(cardDeck);
+        dealer.play(cardDeck);
+
+        //then
+        assertThat(dealer.isStand()).isTrue();
+    }
+
+    @Test
+    @DisplayName("딜러는 점수가 21 초과이면 버스트 상태로 전환된다.")
+    void dealer_transit_to_bust_over_21() {
+        //given
+        Card card1 = Card.of(CardSuit.HEART, CardNumber.TEN);
+        Card card2 = Card.of(CardSuit.HEART, CardNumber.FIVE);
+        Card card3 = Card.of(CardSuit.HEART, CardNumber.EIGHT);
+        List<Card> cards = List.of(card1, card2, card3);
+        Dealer dealer = new Dealer(new InitialState(new Hand()));
+        CardDeck cardDeck = new CardDeck(cards);
+
+        // when
+        dealer.play(cardDeck);
+        dealer.play(cardDeck);
+
+        //then
+        assertThat(dealer.isBust()).isTrue();
+    }
+
+    @Test
+    @DisplayName("딜러의 처음 두 카드의 합이 21이면 블랙잭이다.")
+    void player_when_blackjack_can_not_draw() {
+        //given
+        Card card1 = Card.of(CardSuit.HEART, CardNumber.ACE);
+        Card card2 = Card.of(CardSuit.HEART, CardNumber.KING);
+        List<Card> cards = List.of(card1, card2);
+        Dealer dealer = new Dealer(new InitialState(new Hand()));
+        CardDeck cardDeck = new CardDeck(cards);
+
+        // when
+        dealer.play(cardDeck);
+
+        //then
+        assertThat(dealer.isBlackjack()).isTrue();
+    }
+
 
 }
