eac70da77192c750d5463fa6eea9d5158f3234e6
hyunseo <goatsfish@gmail.com>
2023-03-09 11:32:15 +0900

    test: 카드 오픈, 버스트, 블랙잭 판단 기능 테스트 추가

--- src/test/java/blackjack/domain/game/ParticipantCardsTest.java
+++ src/test/java/blackjack/domain/game/ParticipantCardsTest.java
@@ -3,12 +3,14 @@ package blackjack.domain.game;
 import blackjack.domain.card.Card;
 import blackjack.domain.card.CardNumber;
 import blackjack.domain.card.CardShape;
+import blackjack.domain.card.Deck;
 import blackjack.fixture.ParticipantCardsFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -66,6 +68,35 @@ class ParticipantCardsTest {
     }
 
     @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("카드를 사이즈만큼 오픈한다.")
+    void open(final int openCount) {
+        final Deck deck = new Deck();
+        final List<Card> cards = List.of(deck.draw(), deck.draw());
+        final ParticipantCards participantCards = new ParticipantCards(cards);
+        participantCards.receive(deck.draw());
+        final List<Card> openCards = participantCards.open(openCount);
+
+        assertThat(openCards).hasSize(openCount);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("모든 카드를 오픈한다.")
+    void openAll(final int drawCount) {
+        final Deck deck = new Deck();
+        final List<Card> cards = List.of(deck.draw(), deck.draw());
+        final ParticipantCards participantCards = new ParticipantCards(cards);
+        for (int count = 0; count < drawCount; count++) {
+            participantCards.receive(deck.draw());
+        }
+        final List<Card> openCards = participantCards.openAll();
+        final int expectedSize = cards.size() + drawCount;
+
+        assertThat(openCards).hasSize(expectedSize);
+    }
+
+    @ParameterizedTest
     @MethodSource("isBustDummy")
     @DisplayName("가지고 있는 카드가 버스트인지 판단한다.")
     void isBust(final Card one, final Card two, final List<Card> additionalCards, final boolean expected) {
@@ -73,6 +104,16 @@ class ParticipantCardsTest {
         boolean isBust = participantsCards.isBust();
 
         assertThat(isBust).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("isBlackJackDummy")
+    @DisplayName("가지고 있는 카드가 버스트인지 판단한다.")
+    void isBlackJack(final Card one, final Card two, final List<Card> additionalCards, final boolean expected) {
+        ParticipantCards participantsCards = ParticipantCardsFixture.create(one, two, additionalCards);
+        boolean isBlackJack = participantsCards.isBlackJack();
+
+        assertThat(isBlackJack).isEqualTo(expected);
     }
 
     static Stream<Arguments> calculateDummy() {
@@ -113,6 +154,53 @@ class ParticipantCardsTest {
                                 new Card(CardShape.HEART, CardNumber.JACK),
                                 new Card(CardShape.DIAMOND, CardNumber.THREE)
                         ), true)
+        );
+    }
+
+    static Stream<Arguments> isBlackJackDummy() {
+        return Stream.of(
+                // 블랙잭, 2 + 4 + 5 + 10 = 21
+                Arguments.arguments(
+                        new Card(CardShape.SPADE, CardNumber.TWO),
+                        new Card(CardShape.SPADE, CardNumber.FOUR),
+                        List.of(
+                                new Card(CardShape.SPADE, CardNumber.FIVE),
+                                new Card(CardShape.SPADE, CardNumber.KING)
+                        ), true
+                ),
+                // 블랙잭, 1 + 10 + 10 = 21
+                Arguments.arguments(
+                        new Card(CardShape.SPADE, CardNumber.ACE),
+                        new Card(CardShape.SPADE, CardNumber.KING),
+                        List.of(
+                                new Card(CardShape.SPADE, CardNumber.QUEEN)
+                        ), true
+                ),
+                // 블랙잭, 11 + 10 = 21
+                Arguments.arguments(
+                        new Card(CardShape.SPADE, CardNumber.ACE),
+                        new Card(CardShape.SPADE, CardNumber.KING),
+                        List.of(
+                        ), true
+                ),
+                // 블랙잭이 아닌 경우, 2 + 1 + 5 + 10 = 18
+                Arguments.arguments(
+                        new Card(CardShape.SPADE, CardNumber.TWO),
+                        new Card(CardShape.SPADE, CardNumber.ACE),
+                        List.of(
+                                new Card(CardShape.SPADE, CardNumber.FIVE),
+                                new Card(CardShape.SPADE, CardNumber.KING)
+                        ), false
+                ),
+                // 블랙잭이 아닌 경우 3 + 1 + 5 + 10 == 19
+                Arguments.arguments(
+                        new Card(CardShape.SPADE, CardNumber.THREE),
+                        new Card(CardShape.SPADE, CardNumber.ACE),
+                        List.of(
+                                new Card(CardShape.SPADE, CardNumber.FIVE),
+                                new Card(CardShape.SPADE, CardNumber.KING)
+                        ), false
+                )
         );
     }
 }
