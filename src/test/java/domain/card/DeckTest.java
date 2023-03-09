02d79ec1082fd4dc94b32ca9f4686f2904e4a5bc
splitube <voicesplit@daum.net>
2023-03-09 22:07:41 +0900

    test: 덱 테스트 추가

--- src/test/java/domain/card/DeckTest.java
+++ src/test/java/domain/card/DeckTest.java
@@ -1,24 +1,23 @@
 package domain.card;
 
-import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.Test;
 
 class DeckTest {
 
-    @DisplayName("덱 제일 앞에서 카드를 지급한다.")
-    @TestFactory
-    Stream<DynamicTest> getSizeDynamicTest() {
+    @DisplayName("덱에 있는 카드는 총 52개이다")
+    @Test
+    void deckHaveFiftyTwoCards() {
         Deck deck = new Deck();
-        return Stream.of(
-            DynamicTest.dynamicTest("처음에는 52개의 카드를 가진다.",
-                () -> Assertions.assertThat(deck).extracting("cards").asList().hasSize(52)),
-            DynamicTest.dynamicTest("카드 1개를 반환하면 1이 줄어든다.", () -> {
-                Assertions.assertThat(deck.draw()).isEqualTo(new Card(Denomination.ACE, Suit.SPADE));
-                Assertions.assertThat(deck).extracting("cards").asList().hasSize(51);
-            })
-        );
+        Assertions.assertThat(deck).extracting("cards").asList().hasSize(52);
+    }
+
+    @DisplayName("덱 제일 앞에서 카드를 지급한다.")
+    @Test
+    void getSizeDynamicTest() {
+        Deck deck = new Deck();
+        Assertions.assertThat(deck.draw()).isEqualTo(new Card(Denomination.ACE, Suit.SPADE));
+        Assertions.assertThat(deck).extracting("cards").asList().hasSize(51);
     }
 }
