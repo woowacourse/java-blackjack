f592f123c024a684b63dc87e265fe8bf46b8da13
java-saeng <gusdn85554@naver.com>
2023-03-09 16:13:19 +0900

    refactor : 참여자와 딜러 점수 비교하는 것을 Participant 에 위임

--- src/main/java/domain/player/participant/Participant.java
+++ src/main/java/domain/player/participant/Participant.java
@@ -2,7 +2,12 @@ package domain.player.participant;
 
 import domain.player.Name;
 import domain.player.Player;
+import domain.player.dealer.Dealer;
 import domain.player.participant.betresult.BetResultState;
+import domain.player.participant.betresult.BreakEvenState;
+import domain.player.participant.betresult.LoseState;
+import domain.player.participant.betresult.NotYetState;
+import domain.player.participant.betresult.WinState;
 
 public class Participant extends Player {
 
@@ -28,7 +33,40 @@ public class Participant extends Player {
         return betResultState.calculateBetOutcomeOf(money);
     }
 
+    public Money determineBetMoney(final BetResultState betResultState) {
+        return betResultState.calculateBetOutcomeOf(money);
+    }
+
     public boolean hasNotBetState() {
         return betResultState == null;
+    }
+
+    public BetResultState firstMatchWith(final Dealer dealer) {
+        if (isBlackjack() && dealer.isBlackjack()) {
+            return new BreakEvenState();
+        }
+
+        if (isBlackjack() && !dealer.isBlackjack()) {
+            return new WinState();
+        }
+
+        if (!isBlackjack() && dealer.isBlackjack()) {
+            return new LoseState();
+        }
+
+        return new NotYetState();
+    }
+
+    public BetResultState finalMatchWith(final Dealer dealer) {
+        if (isBust()) {
+            return new LoseState();
+        }
+        if (dealer.isBust()) {
+            return new BreakEvenState();
+        }
+        if (score().isLessThan(dealer.score())) {
+            return new LoseState();
+        }
+        return new BreakEvenState();
     }
 }
--- src/test/java/domain/player/participant/ParticipantTest.java
+++ src/test/java/domain/player/participant/ParticipantTest.java
@@ -2,9 +2,11 @@ package domain.player.participant;
 
 import domain.card.Card;
 import domain.player.Name;
+import domain.player.dealer.Dealer;
 import domain.player.participant.betresult.BetResultState;
 import domain.player.participant.betresult.BreakEvenState;
 import domain.player.participant.betresult.LoseState;
+import domain.player.participant.betresult.NotYetState;
 import domain.player.participant.betresult.WinState;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,8 +18,12 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static domain.card.CardShape.CLOVER;
+import static domain.card.CardShape.DIAMOND;
+import static domain.card.CardShape.HEART;
 import static domain.card.CardShape.SPADE;
 import static domain.card.CardValue.ACE;
+import static domain.card.CardValue.EIGHT;
+import static domain.card.CardValue.NINE;
 import static domain.card.CardValue.TEN;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -71,9 +77,13 @@ class ParticipantTest {
     @ParameterizedTest
     @MethodSource("betResultState")
     @DisplayName("determineBetMoney() : 참여자는 베팅 결과에 따라 베팅 금액을 반환할 수 있다.")
-    void test_determineBetMoney(final Participant participant, final Money bettingResultMoney) throws Exception {
-        //when & then
-        assertEquals(bettingResultMoney, participant.determineBetMoney());
+    void test_determineBetMoney(final Participant participant, final Money bettingResultMoney,
+                                final BetResultState betResultState) throws Exception {
+        //when
+        Money money = participant.determineBetMoney(betResultState);
+
+        //then
+        assertEquals(bettingResultMoney, money);
     }
 
     static Stream<Arguments> betResultState() {
@@ -88,9 +98,9 @@ class ParticipantTest {
         participant3.determineBetState(new BreakEvenState());
 
         return Stream.of(
-                Arguments.of(participant1, Money.wons(15000)),
-                Arguments.of(participant2, Money.wons(-5000)),
-                Arguments.of(participant3, Money.wons(1000))
+                Arguments.of(participant1, Money.wons(15000), new WinState()),
+                Arguments.of(participant2, Money.wons(-5000), new LoseState()),
+                Arguments.of(participant3, Money.wons(1000), new BreakEvenState())
         );
     }
 
@@ -99,5 +109,130 @@ class ParticipantTest {
     void test_hasNotBetState() throws Exception {
         //when & then
         assertTrue(participant.hasNotBetState());
+    }
+
+    @ParameterizedTest
+    @MethodSource("firstMatch")
+    @DisplayName("firstMatchWith() : 처응 두 장 카드를 받고나서 딜러와 블랙젹 여부를 비교하여 배팅 결정 상태를 결정할 수 있다.")
+    void test_firstMatchWith(final Participant participant,
+                             final Dealer dealer,
+                             final BetResultState resultState) throws Exception {
+
+        //when
+        BetResultState betResultState = participant.firstMatchWith(dealer);
+
+        //then
+        assertThat(betResultState).isInstanceOf(resultState.getClass());
+    }
+
+    static Stream<Arguments> firstMatch() {
+
+        /**
+         * 참여자 : 블랙잭
+         * 딜러 : 블랙잭
+         * 결과 : 무승부
+         */
+        final Participant participant1 = new Participant(new Name("name1"), Money.wons(0));
+        participant1.hit(new Card(DIAMOND, TEN));
+        participant1.hit(new Card(DIAMOND, ACE));
+
+        final Dealer dealer1 = new Dealer();
+        dealer1.hit(new Card(HEART, TEN));
+        dealer1.hit(new Card(HEART, ACE));
+
+        /**
+         * 참여자 : 블랙잭
+         * 딜러 : 블랙잭 X
+         * 결과 : 참여자 승리
+         */
+        final Participant participant2 = new Participant(new Name("name2"), Money.wons(0));
+        participant2.hit(new Card(DIAMOND, TEN));
+        participant2.hit(new Card(DIAMOND, ACE));
+
+        final Dealer dealer2 = new Dealer();
+        dealer2.hit(new Card(HEART, TEN));
+        dealer2.hit(new Card(CLOVER, NINE));
+
+        /**
+         * 참여자 : 블랙잭 X
+         * 딜러 : 블랙잭
+         * 결과 : 딜러 승
+         */
+        final Participant participant3 = new Participant(new Name("name3"), Money.wons(0));
+        participant3.hit(new Card(HEART, TEN));
+        participant3.hit(new Card(CLOVER, NINE));
+
+        final Dealer dealer3 = new Dealer();
+        dealer3.hit(new Card(DIAMOND, TEN));
+        dealer3.hit(new Card(DIAMOND, ACE));
+
+        /**
+         * 참여자 : 블랙잭 X
+         * 딜러 : 블랙잭 X
+         * 결과 : 승부가 아직 안난 상태
+         */
+        final Participant participant4 = new Participant(new Name("name3"), Money.wons(0));
+        participant4.hit(new Card(HEART, TEN));
+        participant4.hit(new Card(CLOVER, NINE));
+
+        final Dealer dealer4 = new Dealer();
+        dealer4.hit(new Card(DIAMOND, TEN));
+        dealer4.hit(new Card(DIAMOND, EIGHT));
+
+        return Stream.of(
+                Arguments.of(participant1, dealer1, new BreakEvenState()),
+                Arguments.of(participant2, dealer2, new WinState()),
+                Arguments.of(participant3, dealer3, new LoseState()),
+                Arguments.of(participant4, dealer4, new NotYetState())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("finalMatch")
+    @DisplayName("finalMatchWith() : 플레이어와 딜러가 카드를 전부 다 받고 나서 승부 결정 상태를 결정할 수 있다.")
+    void test_finalMatchWith(final Participant participant,
+                             final Dealer dealer,
+                             final BetResultState resultState) throws Exception {
+        //when
+        BetResultState betResultState = participant.finalMatchWith(dealer);
+
+        //then
+        assertThat(betResultState).isInstanceOf(resultState.getClass());
+    }
+
+    static Stream<Arguments> finalMatch() {
+
+        //무승부
+        final Participant participant1 = new Participant(new Name("name1"), Money.wons(10000));
+        participant1.hit(new Card(DIAMOND, TEN));
+        participant1.hit(new Card(SPADE, TEN));
+
+        final Dealer dealer1 = new Dealer();
+        dealer1.hit(new Card(HEART, TEN));
+        dealer1.hit(new Card(CLOVER, TEN));
+
+        //참여자가 이길 경우
+        final Participant participant2 = new Participant(new Name("name2"), Money.wons(1000));
+        participant2.hit(new Card(SPADE, TEN));
+        participant2.hit(new Card(DIAMOND, TEN));
+
+        final Dealer dealer2 = new Dealer();
+        dealer2.hit(new Card(HEART, TEN));
+        dealer2.hit(new Card(CLOVER, NINE));
+
+        //딜러가 이길 경우
+        final Participant participant3 = new Participant(new Name("name3"), Money.wons(4000));
+        participant3.hit(new Card(SPADE, TEN));
+        participant3.hit(new Card(DIAMOND, NINE));
+
+        final Dealer dealer3 = new Dealer();
+        dealer3.hit(new Card(HEART, TEN));
+        dealer3.hit(new Card(CLOVER, TEN));
+
+        return Stream.of(
+                Arguments.of(participant1, dealer1, new BreakEvenState()),
+                Arguments.of(participant2, dealer2, new BreakEvenState()),
+                Arguments.of(participant3, dealer3, new LoseState())
+        );
     }
 }
