71df0731b02f4637114185ef1267868598c2b96f
Combi153 <jcm04@naver.com>
2023-03-09 22:44:35 +0900

    feat: 배팅 금액 입력 로직 추가

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,19 +1,21 @@
 package blackjack.view;
 
-import static java.util.stream.Collectors.collectingAndThen;
-import static java.util.stream.Collectors.toList;
-
 import blackjack.controller.BlackjackCommand;
 import blackjack.domain.player.Player;
 import blackjack.domain.player.Players;
+
 import java.util.Arrays;
 import java.util.Scanner;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 public class InputView {
     private static final String INPUT_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String DELIMITER = ",";
     private static final int LIMIT = -1;
     private static final String INPUT_PLAYER_COMMAND_MESSAGE = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String NEW_LINE = System.lineSeparator();
 
     private final Scanner scanner;
 
@@ -28,6 +30,22 @@ public class InputView {
         return Arrays.stream(names.split(DELIMITER, LIMIT))
                 .map(String::trim)
                 .collect(collectingAndThen(toList(), Players::from));
+    }
+
+    public int readBetMoney(final Player player) {
+        System.out.println(NEW_LINE + player.getName() + "의 배팅 금액은?");
+
+        final String betMoney = scanner.nextLine();
+
+        return toInt(betMoney);
+    }
+
+    private int toInt(final String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해야 합니다. 입력 값 :" + value);
+        }
     }
 
     public BlackjackCommand readCommand(final Player player) {
