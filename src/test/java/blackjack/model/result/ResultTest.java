8d9219c71d5118666167a290bfd92eeab4555807
hectick <chyeon1223@cau.ac.kr>
2023-03-09 16:45:55 +0900

    test: 플레이어가 블랙잭일 때 승패 판단 테스트 작성

--- src/test/java/blackjack/model/result/ResultTest.java
+++ src/test/java/blackjack/model/result/ResultTest.java
@@ -113,4 +113,93 @@ class ResultTest {
 
     }
 
+    @Nested
+    @DisplayName("플레이어가 블랙잭일 때")
+    class player_blackjack {
+
+        @Nested
+        @DisplayName("딜러가 버스트라면")
+        class dealer_bust {
+            @Test
+            @DisplayName("플레이어가 승리한다")
+            void player_lose() {
+                //given
+                Card card1 = Card.of(CardSuit.HEART, CardNumber.NINE);
+                Card card2 = Card.of(CardSuit.CLUB, CardNumber.ACE);
+                Card card3 = Card.of(CardSuit.HEART, CardNumber.TEN);
+                Card card4 = Card.of(CardSuit.DIAMOND, CardNumber.SIX);
+                Card card5 = Card.of(CardSuit.SPADE, CardNumber.TEN);
+                CardDeck cardDeck = new CardDeck(List.of(card1, card2, card3, card4, card5));
+                Dealer dealer = new Dealer(new InitialState(new Hand()));
+                Player player = new Player(new Name("이리내"), new InitialState(new Hand()));
+                Participants participants = new Participants(dealer, List.of(player));
+
+                //when
+                participants.distributeTwoCardsToEach(cardDeck);
+                participants.hitOrStandByDealer(cardDeck);
+                Result result = Result.checkPlayerResult(player, dealer);
+
+                //then
+                assertThat(player.isBlackjack()).isTrue();
+                assertThat(dealer.isBust()).isTrue();
+                assertThat(result).isEqualTo(Result.WIN);
+            }
+        }
+
+        @Nested
+        @DisplayName("딜러가 블랙잭이라면")
+        class dealer_blackjack {
+            @Test
+            @DisplayName("플레이어는 무승부다")
+            void player_lose() {
+                //given
+                Card card1 = Card.of(CardSuit.CLUB, CardNumber.ACE);
+                Card card2 = Card.of(CardSuit.HEART, CardNumber.TEN);
+                Card card3 = Card.of(CardSuit.DIAMOND, CardNumber.ACE);
+                Card card4 = Card.of(CardSuit.SPADE, CardNumber.TEN);
+                CardDeck cardDeck = new CardDeck(List.of(card1, card2, card3, card4));
+                Dealer dealer = new Dealer(new InitialState(new Hand()));
+                Player player = new Player(new Name("이리내"), new InitialState(new Hand()));
+                Participants participants = new Participants(dealer, List.of(player));
+
+                //when
+                participants.distributeTwoCardsToEach(cardDeck);
+                Result result = Result.checkPlayerResult(player, dealer);
+
+                //then
+                assertThat(player.isBlackjack()).isTrue();
+                assertThat(dealer.isBlackjack()).isTrue();
+                assertThat(result).isEqualTo(Result.TIE);
+            }
+        }
+
+        @Nested
+        @DisplayName("딜러가 스탠드라면")
+        class dealer_stand {
+            @Test
+            @DisplayName("플레이어가 승리한다")
+            void player_lose() {
+                //given
+                Card card1 = Card.of(CardSuit.CLUB, CardNumber.ACE);
+                Card card2 = Card.of(CardSuit.HEART, CardNumber.TEN);
+                Card card3 = Card.of(CardSuit.DIAMOND, CardNumber.SEVEN);
+                Card card4 = Card.of(CardSuit.SPADE, CardNumber.TEN);
+                CardDeck cardDeck = new CardDeck(List.of(card1, card2, card3, card4));
+                Dealer dealer = new Dealer(new InitialState(new Hand()));
+                Player player = new Player(new Name("이리내"), new InitialState(new Hand()));
+                Participants participants = new Participants(dealer, List.of(player));
+
+                //when
+                participants.distributeTwoCardsToEach(cardDeck);
+                Result result = Result.checkPlayerResult(player, dealer);
+
+                //then
+                assertThat(player.isBlackjack()).isTrue();
+                assertThat(dealer.isStand()).isTrue();
+                assertThat(result).isEqualTo(Result.WIN);
+            }
+        }
+
+    }
+
 }
