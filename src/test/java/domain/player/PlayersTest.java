890302023ad87a2757c1206d10662bb30d79dc1c
JunDonghyuk <ehdgur4814@naver.com>
2023-03-09 11:48:22 +0900

    refactor: Players 기능 축소

--- src/test/java/domain/player/PlayersTest.java
+++ src/test/java/domain/player/PlayersTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayersTest {
+
     Players players;
 
     @BeforeEach
@@ -19,10 +20,10 @@ class PlayersTest {
 
     @DisplayName("처음 카드를 나눠주면 플레이어의 패의 크기는 2이다.")
     @Test
-    void drawTwoCardsAtFirstTimeTest() {
+    void hitTwiceTest() {
         int expectedCardSize = 2;
-        players.drawTwoCardsAtFirstTime(new Deck());
-        for (Player player : players.getPlayersWithDealer()) {
+        players.hitTwice(new Deck());
+        for (Player player : players.getPlayers()) {
             assertEquals(expectedCardSize, player.getCards().size());
         }
     }
@@ -40,35 +41,5 @@ class PlayersTest {
     void findPlayerSuccessTest() {
         String expectedPlayerName = "crong";
         assertEquals(expectedPlayerName, players.findPlayer("crong").getName());
-    }
-
-    @DisplayName("딜러의 이름은 딜러이다.")
-    @Test
-    void findDealerTest() {
-        String expectedDealerName = "딜러";
-        assertTrue(players.findDealer() instanceof Dealer);
-        assertEquals(expectedDealerName, players.findDealer().getName());
-    }
-
-    @DisplayName("딜러를 포함한 플레이어들을 반환시 첫 번째 원소는 딜러이다.")
-    @Test
-    void getPlayersWithDealerTest() {
-        int expectedPlayersWithDealerSize = 4;
-        String expectedDealerName = "딜러";
-        List<Player> playersWithDealer = players.getPlayersWithDealer();
-
-        assertEquals(expectedPlayersWithDealerSize, playersWithDealer.size());
-        assertEquals(expectedDealerName, playersWithDealer.get(0).getName());
-    }
-
-    @DisplayName("딜러를 포함하지 않은 플레이어들을 반환시 첫 번째 원소는 딜러가 아니다.")
-    @Test
-    void getPlayersWithOutDealerTest() {
-        int expectedPlayersWithOutDealerSize = 3;
-        String expectedDealerName = "딜러";
-        List<Player> playersWithOutDealer = players.getPlayersWithOutDealer();
-
-        assertEquals(expectedPlayersWithOutDealerSize, playersWithOutDealer.size());
-        assertNotEquals(expectedDealerName, playersWithOutDealer.get(0).getName());
     }
 }
