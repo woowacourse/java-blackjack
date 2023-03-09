e0c81a971f4b3af51fa7d3bbc74354a06622e05f
sh111-coder <ohk9134@naver.com>
2023-03-09 20:47:13 +0900

    feat(Dealer): 플레이어의 배팅 금액 저장 기능 구현

--- src/main/java/domain/participant/Dealer.java
+++ src/main/java/domain/participant/Dealer.java
@@ -1,15 +1,31 @@
 package domain.participant;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import domain.betting.BettingMoney;
 import util.Constants;
 
 public class Dealer extends Participant {
     private static final int LIMIT_TAKE_CARD_VALUE = 17;
 
+    private final Map<Player, BettingMoney> bettingMoneyByPlayer;
+
     public Dealer(HandCards handCards) {
         super(new Name(Constants.DEALER_NAME), handCards);
+        this.bettingMoneyByPlayer = new HashMap<>();
     }
 
     public boolean checkCardsCondition() {
         return getOptimalCardValueSum() < LIMIT_TAKE_CARD_VALUE;
+    }
+
+    public void savePlayerBettingMoney(Player player, BettingMoney bettingMoney) {
+        bettingMoneyByPlayer.put(player, bettingMoney);
+    }
+
+    public Map<Player, BettingMoney> getBettingMoneyByPlayer() {
+        return Map.copyOf(bettingMoneyByPlayer);
     }
 }
