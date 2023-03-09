7528c54090049a7bdc1a29b2497d6f3de63a4437
hyunseo <goatsfish@gmail.com>
2023-03-09 11:16:14 +0900

    feat: 플레이어 배팅 결과 계산 기능 추가

--- src/main/java/blackjack/domain/game/BettingPlayers.java
+++ src/main/java/blackjack/domain/game/BettingPlayers.java
@@ -1,23 +1,66 @@
 package blackjack.domain.game;
 
+import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Money;
 import blackjack.domain.participant.Player;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.*;
 
 public class BettingPlayers {
-    private final Map<Player, Money> players = new ConcurrentHashMap<>();
+    private static final double BLACK_JACK_PROFIT_RATE = 1.5;
+
+    private final Map<Player, Money> players = new LinkedHashMap<>();
 
     public BettingPlayers(final List<Player> players, final List<Money> moneys) {
         validate(players, moneys);
+        saveBettingInformation(players, moneys);
+    }
+
+    public Map<Player, Money> decideBetResultBy(final Dealer dealer) {
+        players.keySet().forEach(player -> {
+            final ResultType playerResult = dealer.judgeResult(player);
+            final Money currentMoney = players.get(player);
+            reflectPlayerMoney(player, playerResult, currentMoney);
+        });
+        return new HashMap<>(players);
+    }
+
+    private void saveBettingInformation(final List<Player> players, final List<Money> moneys) {
         final int playerSize = players.size();
         for (int index = 0; index < playerSize; index++) {
             final Player currentPlayer = players.get(index);
             final Money currentMoney = moneys.get(index);
             this.players.put(currentPlayer, currentMoney);
         }
+    }
+
+    private void reflectPlayerMoney(final Player player, final ResultType playerResult, final Money currentMoney) {
+        reflectIfBlackJack(player, playerResult, currentMoney);
+        reflectIfWin(player, playerResult, currentMoney);
+        reflectIfLose(player, playerResult, currentMoney);
+    }
+
+    private void reflectIfBlackJack(final Player player, final ResultType playerResult, final Money currentMoney) {
+        if (playerResult == ResultType.BLACK_JACK) {
+            final Money earnMoney = currentMoney.times(BLACK_JACK_PROFIT_RATE);
+            players.put(player, currentMoney.earn(earnMoney));
+        }
+    }
+
+    private void reflectIfWin(final Player player, final ResultType playerResult, final Money currentMoney) {
+        if (playerResult == ResultType.WIN) {
+            players.put(player, currentMoney.earn(currentMoney));
+        }
+    }
+
+    private void reflectIfLose(final Player player, final ResultType playerResult, final Money currentMoney) {
+        if (playerResult == ResultType.LOSE) {
+            players.put(player, currentMoney.spend(currentMoney));
+        }
+    }
+
+    public List<Player> getPlayers() {
+        return new ArrayList<>(players.keySet());
     }
 
     private void validate(final List<Player> players, final List<Money> moneys) {
