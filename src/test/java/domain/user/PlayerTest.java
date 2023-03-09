473c4e524face7de2f2a754928296e9c86a62601
splitube <voicesplit@daum.net>
2023-03-09 21:20:28 +0900

    test: Player 테스트 구현

--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -2,59 +2,106 @@ package domain.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 import domain.card.Card;
 import domain.card.Denomination;
 import domain.card.Suit;
 import java.util.List;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 class PlayerTest {
 
-    @DisplayName("플레이어 테스트.")
-    @TestFactory
-    Stream<DynamicTest> getSizeDynamicTest() {
+    @DisplayName("플레이어의 이름과 상태로 플레이어를 생성한다.")
+    @Test
+    void create() {
         Player player = new Player("split");
-        Card firstCardTwoSpade = new Card(Denomination.TWO, Suit.SPADE);
-        Card secondCardAceDiamond = new Card(Denomination.ACE, Suit.DIAMOND);
-        Card thirdCardNineHeart = new Card(Denomination.NINE, Suit.HEART);
-        return Stream.of(
-            dynamicTest("이름과 플레이어 여부를 확인한다.", () -> {
-                assertThat(player.getName()).isEqualTo("split");
-                assertThat(player.isPlayer()).isTrue();
-            }),
-            dynamicTest("카드 1개를 지급한다.", () -> {
-                player.dealt(firstCardTwoSpade);
-                assertThat(player.showHand()).containsExactly(firstCardTwoSpade);
-            }),
-            dynamicTest("게임할 준비가 되지 않은 채 게임할 카드를 조회하면 오류를 던진다.", () -> {
-                assertThatThrownBy(() -> player.faceUpInitialHand())
-                    .isExactlyInstanceOf(IllegalStateException.class)
-                    .hasMessage("모든 플레이어는 카드 두장을 받고 시작해야 합니다.");
-            }),
-            dynamicTest("1개를 더 지급하여 준비완료하고 게임할 2장의 카드를 조회한다.", () -> {
-                player.dealt(secondCardAceDiamond);
-                assertThat(player.faceUpInitialHand())
-                    .containsExactly(firstCardTwoSpade, secondCardAceDiamond);
-            }),
-            dynamicTest("가지고 있는 카드의 점수를 조회한다.(ACE가 11로 계산되면 유리한 경우)", () -> {
-                assertThat(player.hasAce()).isTrue();
-                assertThat(player.calculatePoint()).isEqualTo(13);
-            }),
-            dynamicTest("한장을 더 지급하고 가지고 있는 카드의 점수를 조회한다.(ACE가 11로 계산되면 불리한 경우)", () -> {
-                player.dealt(thirdCardNineHeart);
-                assertThat(player.hasAce()).isTrue();
-                assertThat(player.calculatePoint()).isEqualTo(12);
-            }),
-            dynamicTest("가지고 있는 모드 카드를 조회한다.", () -> {
-                List<Card> cards = player.showHand();
-                assertThat(cards).hasSize(3);
-                assertThat(cards).containsExactly(firstCardTwoSpade, secondCardAceDiamond, thirdCardNineHeart);
-            })
-        );
+        assertThat(player.getName()).isEqualTo("split");
+    }
+
+    @DisplayName("플레이어에게 카드를 지급한다.")
+    @Test
+    void dealt() {
+        Player player = new Player("split");
+        Card card = new Card(Denomination.JACK, Suit.DIAMOND);
+        player.dealt(card);
+        List<Card> hand = player.getHand();
+        assertThat(hand).containsExactly(card);
+    }
+
+    @DisplayName("게임을 시작할 카드를 반환한다")
+    @Test
+    void getInitialHand() {
+        Player player = new Player("split");
+        Card firstCard = new Card(Denomination.JACK, Suit.DIAMOND);
+        Card secondCard = new Card(Denomination.ACE, Suit.DIAMOND);
+        player.dealt(firstCard);
+        player.dealt(secondCard);
+        assertThat(player.getInitialHand()).containsExactly(firstCard, secondCard);
+    }
+
+    @DisplayName("카드를 지급받지 않고 게임을 시작할 카드를 조회하면 오류를 던진다")
+    @Test
+    void getInitialHandWithoutCard() {
+        Player player = new Player("split");
+        assertThatThrownBy(player::getInitialHand)
+            .isExactlyInstanceOf(IllegalStateException.class)
+            .hasMessage("모든 플레이어는 카드 두장을 받고 시작해야 합니다.");
+    }
+
+    @DisplayName("가지고 있는 모든 카드를 반환한다.")
+    @Test
+    void getHand() {
+        Player player = new Player("split");
+        Card firstCard = new Card(Denomination.SIX, Suit.DIAMOND);
+        Card secondCard = new Card(Denomination.ACE, Suit.DIAMOND);
+        Card thirdCard = new Card(Denomination.TWO, Suit.DIAMOND);
+        Card fourthCard = new Card(Denomination.THREE, Suit.DIAMOND);
+        player.dealt(firstCard);
+        player.dealt(secondCard);
+        player.dealt(thirdCard);
+        player.dealt(fourthCard);
+        assertThat(player.getHand()).containsExactly(firstCard, secondCard, thirdCard, fourthCard);
+    }
+
+    @DisplayName("가지고 있는 카드로 점수를 계산하여 반환한다.")
+    @Nested
+    class GetPoint {
+
+        @DisplayName("ACE를 가지고 있지 않은 경우")
+        @Test
+        void notContainsAce() {
+            Player player = new Player("split");
+            Card firstCard = new Card(Denomination.SIX, Suit.DIAMOND);
+            Card secondCard = new Card(Denomination.SEVEN, Suit.DIAMOND);
+            player.dealt(firstCard);
+            player.dealt(secondCard);
+            assertThat(player.getPoint()).isEqualTo(13);
+        }
+
+        @DisplayName("ACE를 11로 계산하는 경우")
+        @Test
+        void useAceAsEleven() {
+            Player player = new Player("split");
+            Card firstCard = new Card(Denomination.ACE, Suit.DIAMOND);
+            Card secondCard = new Card(Denomination.SEVEN, Suit.DIAMOND);
+            player.dealt(firstCard);
+            player.dealt(secondCard);
+            assertThat(player.getPoint()).isEqualTo(18);
+        }
+
+        @DisplayName("ACE를 1로 계산하는 경우")
+        @Test
+        void useAceAsOne() {
+            Player player = new Player("split");
+            Card firstCard = new Card(Denomination.JACK, Suit.DIAMOND);
+            Card secondCard = new Card(Denomination.QUEEN, Suit.DIAMOND);
+            Card thirdCard = new Card(Denomination.ACE, Suit.DIAMOND);
+            player.dealt(firstCard);
+            player.dealt(secondCard);
+            player.dealt(thirdCard);
+            assertThat(player.getPoint()).isEqualTo(21);
+        }
     }
 }
