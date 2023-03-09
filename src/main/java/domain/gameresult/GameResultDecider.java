a6fec8176302bf6c3620be1dcf947d9508e921c7
BackFoxx <backfoxxx@gmail.com>
2023-03-09 12:01:31 +0900

    refactor : 게임 결과를 계산하는 작업을 Dealer가 하지 않고 GameResultDecider 라는 별도의 유틸 클래스에서 작업하도록 변경

---
+++ src/main/java/domain/gameresult/GameResultDecider.java
@@ -0,0 +1,48 @@
+package domain.gameresult;
+
+import domain.player.Dealer;
+import domain.player.Player;
+
+public class GameResultDecider {
+    private GameResultDecider() {}
+
+    public static void decideGameResult(Dealer dealer, Player participant, GameResult gameResult) {
+        if (dealer.isBust() || participant.isBust()) {
+            decideGameResultOnBurst(dealer, participant, gameResult);
+            return;
+        }
+        decideGameResultOnScore(dealer, participant, gameResult);
+    }
+
+    private static void decideGameResultOnBurst(Dealer dealer, Player participant, GameResult gameResult) {
+        if (dealer.isBust() && participant.isBust()) {
+            gameResult.addDrawCount(participant);
+            return;
+        } // 둘 다 버스트
+
+        if (dealer.isBust()) {
+            gameResult.addParticipantWinningCase(participant);
+            return;
+        } // 딜러가 버스트
+
+        gameResult.addParticipantLosingCase(participant);
+        // 참가자가 버스트
+    }
+
+    private static void decideGameResultOnScore(Dealer dealer, Player participant, GameResult gameResult) {
+        int dealerScore = dealer.getTotalScore();
+        int participantScore = participant.getTotalScore();
+
+        if (dealerScore > participantScore) {
+            gameResult.addParticipantLosingCase(participant);
+            return;
+        }
+
+        if (dealerScore < participantScore) {
+            gameResult.addParticipantWinningCase(participant);
+            return;
+        }
+
+        gameResult.addDrawCount(participant);
+    }
+}
--- src/main/java/domain/player/Dealer.java
+++ src/main/java/domain/player/Dealer.java
@@ -2,6 +2,7 @@ package domain.player;
 
 import domain.card.CardHolder;
 import domain.gameresult.GameResult;
+import domain.gameresult.GameResultDecider;
 
 public class Dealer extends Player {
 
@@ -13,43 +14,7 @@ public class Dealer extends Player {
     }
 
     public void battle(Player participant, GameResult gameResult) {
-        if (isBust() || participant.isBust()) {
-            decideGameResultOnBurst(participant, gameResult);
-            return;
-        }
-        decideGameResultOnScore(participant, gameResult);
-    }
-
-    private void decideGameResultOnBurst(Player participant, GameResult gameResult) {
-        if (isBust() && participant.isBust()) {
-            gameResult.addDrawCount(participant);
-            return;
-        } // 둘 다 버스트
-
-        if (isBust()) {
-            gameResult.addParticipantWinningCase(participant);
-            return;
-        } // 딜러가 버스트
-
-        gameResult.addParticipantLosingCase(participant);
-        // 참가자가 버스트
-    }
-
-    private void decideGameResultOnScore(Player participant, GameResult gameResult) {
-        int dealerScore = getTotalScore();
-        int participantScore = participant.getTotalScore();
-
-        if (dealerScore > participantScore) {
-            gameResult.addParticipantLosingCase(participant);
-            return;
-        }
-
-        if (dealerScore < participantScore) {
-            gameResult.addParticipantWinningCase(participant);
-            return;
-        }
-
-        gameResult.addDrawCount(participant);
+        GameResultDecider.decideGameResult(this, participant, gameResult);
     }
 
     @Override
