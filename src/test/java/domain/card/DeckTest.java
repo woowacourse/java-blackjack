9be24cc3e3c0ba0a21e77e5877b2fe492c2af3ac
splitube <voicesplit@daum.net>
2023-03-09 21:21:56 +0900

    refactor: Deck 테스트 수정

--- src/test/java/domain/card/DeckTest.java
+++ src/test/java/domain/card/DeckTest.java
@@ -13,12 +13,11 @@ class DeckTest {
     Stream<DynamicTest> getSizeDynamicTest() {
         Deck deck = new Deck();
         return Stream.of(
-            DynamicTest.dynamicTest("처음에는 52개의 카드를 가진다.", () -> {
-                Assertions.assertThat(deck.getCurrentSize()).isEqualTo(52);
-            }),
+            DynamicTest.dynamicTest("처음에는 52개의 카드를 가진다.",
+                () -> Assertions.assertThat(deck).extracting("cards").asList().hasSize(52)),
             DynamicTest.dynamicTest("카드 1개를 반환하면 1이 줄어든다.", () -> {
                 Assertions.assertThat(deck.draw()).isEqualTo(new Card(Denomination.ACE, Suit.SPADE));
-                Assertions.assertThat(deck.getCurrentSize()).isEqualTo(51);
+                Assertions.assertThat(deck).extracting("cards").asList().hasSize(51);
             })
         );
     }
