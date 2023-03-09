0cb4ebe3ca7ad0a141077588c3dbc8cd3862fb61
parkmuhyeun <pjhg410@gmail.com>
2023-03-09 17:49:35 +0900

    refactor: 결과 테스트 좀 더 명확히 하도록 수정

--- src/test/java/blackjack/domain/game/RefereeTest.java
+++ src/test/java/blackjack/domain/game/RefereeTest.java
@@ -5,9 +5,8 @@ import java.util.Map;
 import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Participant;
 import blackjack.domain.card.*;
+import blackjack.domain.participant.Player;
 import blackjack.domain.participant.Players;
-import blackjack.domain.game.Referee;
-import blackjack.domain.game.Result;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -18,49 +17,40 @@ class RefereeTest {
     @Test
     void testResult() {
         Participant dealer = new Dealer();
-        dealer.hit(createStandCard(Pattern.CLUB, "10"));
+        hit(dealer, List.of("10"));
         Players players = createPlayers();
 
         List<Result> result = referee.judgeResult(dealer, players);
 
-        Assertions.assertThat(result).isEqualTo(List.of(Result.WIN, Result.LOSE, Result.DRAW, Result.LOSE));
+        Assertions.assertThat(result).isEqualTo(List.of(Result.WIN, Result.LOSE));
     }
 
     private Players createPlayers() {
-        Players players = new Players("무민,아마란스,프리지아,수국");
-        players.getPlayers().get(0).hit(createAceCard(Pattern.SPADE));
-        players.getPlayers().get(1).hit(createStandCard(Pattern.CLUB, "9"));
-        players.getPlayers().get(2).hit(createStandCard(Pattern.SPADE, "10"));
-        players.getPlayers().get(3).hit(createStandCard(Pattern.HEART, "10"));
-        players.getPlayers().get(3).hit(createStandCard(Pattern.DIAMOND, "10"));
-        players.getPlayers().get(3).hit(createCourtCard(Pattern.DIAMOND, "K"));
+        Players players = new Players("무민, 아마란스");
+        Player score11Player = players.getPlayers().get(0);
+        Player scoreBurstPlayer = players.getPlayers().get(1);
+
+        hit(score11Player, List.of("9", "2"));
+        hit(scoreBurstPlayer, List.of("10", "5", "8"));
         return players;
     }
 
-    private CourtCard createCourtCard(Pattern pattern, String symbol) {
-        return new CourtCard(pattern, symbol);
-    }
-
-    private AceCard createAceCard(Pattern pattern) {
-        return new AceCard(pattern);
-    }
-
-    private StandardCard createStandCard(Pattern pattern, String symbol) {
-        return new StandardCard(pattern, symbol);
+    private void hit(Participant participant, List<String> symbols) {
+        for (String symbol : symbols) {
+            participant.hit(new StandardCard(Pattern.CLUB, symbol));
+        }
     }
 
     @Test
     void testDealerBurstResult() {
-        Participant dealer = new Dealer();
-        dealer.hit(createStandCard(Pattern.CLUB, "10"));
-        dealer.hit(createStandCard(Pattern.CLUB, "4"));
-        dealer.hit(createStandCard(Pattern.CLUB, "9"));
         Players players = createPlayers();
+        Participant dealer = new Dealer();
+        hit(dealer, List.of("10", "4", "9"));
 
         List<Result> result = referee.judgeResult(dealer, players);
 
         Assertions.assertThat(result)
-            .isEqualTo(List.of(Result.WIN, Result.WIN, Result.WIN, Result.LOSE));
+            .isEqualTo(List.of(Result.WIN, Result.LOSE));
     }
 
     @Test
