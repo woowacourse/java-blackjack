52d636b5595325ab8371fb06ab87887b8ac4c0e3
hyunseo <goatsfish@gmail.com>
2023-03-10 03:55:20 +0900

    refactor: 돈 불변 객체 equals, hashcode 재정의

--- src/main/java/blackjack/domain/participant/Money.java
+++ src/main/java/blackjack/domain/participant/Money.java
@@ -1,5 +1,7 @@
 package blackjack.domain.participant;
 
+import java.util.Objects;
+
 public class Money {
     public static final Money ZERO = new Money(0);
 
@@ -30,5 +32,22 @@ public class Money {
         if (money.value < 0) {
             throw new IllegalArgumentException("번 금액이 음수일 수 없습니다.");
         }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Money)) {
+            return false;
+        }
+        final Money money = (Money) o;
+        return value == money.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
