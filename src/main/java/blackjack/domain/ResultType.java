36eb098af7c4328b6bce33365c0a619fcb6d40b1
Be-student <presentsong@naver.com>
2023-03-09 21:58:29 +0900

    feat: 각각의 playerProfit 데이터 추가

--- src/main/java/blackjack/domain/ResultType.java
+++ src/main/java/blackjack/domain/ResultType.java
@@ -1,31 +1,31 @@
 package blackjack.domain;
 
 public enum ResultType {
-    BLACKJACK_WIN("블랙잭 승리") {
+    BLACKJACK_WIN("블랙잭 승리", 1.5) {
         @Override
         public ResultType getOppositeResult() {
             return BLACKJACK_LOSE;
         }
     },
-    WIN("승") {
+    WIN("승", 1) {
         @Override
         public ResultType getOppositeResult() {
             return LOSE;
         }
     },
-    TIE("무") {
+    TIE("무", 0) {
         @Override
         public ResultType getOppositeResult() {
             return TIE;
         }
     },
-    LOSE("패") {
+    LOSE("패", -1) {
         @Override
         public ResultType getOppositeResult() {
             return WIN;
         }
     },
-    BLACKJACK_LOSE("블랙잭 패배") {
+    BLACKJACK_LOSE("블랙잭 패배", -1) {
         @Override
         public ResultType getOppositeResult() {
             return BLACKJACK_WIN;
@@ -33,14 +33,20 @@ public enum ResultType {
     };
 
     private final String name;
+    private final double playerProfit;
 
-    ResultType(final String name) {
+    ResultType(final String name, final double playerProfit) {
         this.name = name;
+        this.playerProfit = playerProfit;
     }
 
     public abstract ResultType getOppositeResult();
 
     public String getName() {
         return name;
+    }
+
+    public double getPlayerProfit() {
+        return playerProfit;
     }
 }
