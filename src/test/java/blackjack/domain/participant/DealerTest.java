0156373c4b021e7ece76842a216c3828dabf70f9
hyunseo <goatsfish@gmail.com>
2023-03-09 11:46:47 +0900

    test: 딜러의 플레이어 결과 판단, 블랙잭 결과 추가로 인한 테스트 수정

--- src/test/java/blackjack/domain/participant/DealerTest.java
+++ src/test/java/blackjack/domain/participant/DealerTest.java
@@ -93,6 +93,21 @@ class DealerTest {
     static Stream<Arguments> judgeResultDummy() {
         return Stream.of(
                 Arguments.arguments(
+                        // 플레이어가 승리한다.
+                        // 플레이어
+                        new Card(CardShape.DIAMOND, CardNumber.TWO),
+                        new Card(CardShape.DIAMOND, CardNumber.FIVE),
+                        List.of(new Card(CardShape.SPADE, CardNumber.THREE),
+                                new Card(CardShape.HEART, CardNumber.QUEEN)),
+                        // 딜러
+                        new Card(CardShape.HEART, CardNumber.TWO),
+                        new Card(CardShape.HEART, CardNumber.FIVE),
+                        List.of(new Card(CardShape.CLOVER, CardNumber.TWO),
+                                new Card(CardShape.CLOVER, CardNumber.KING)),
+                        // 플레이어 승리
+                        ResultType.WIN
+                ),
+                Arguments.arguments(
                         // 플레이어 딜러 모두 버스트하는 경우
                         // 플레이어
                         new Card(CardShape.DIAMOND, CardNumber.NINE),
@@ -104,8 +119,8 @@ class DealerTest {
                         new Card(CardShape.HEART, CardNumber.QUEEN),
                         List.of(new Card(CardShape.CLOVER, CardNumber.JACK),
                                 new Card(CardShape.CLOVER, CardNumber.KING)),
-                        // 딜러 승리
-                        ResultType.WIN
+                        // 플레이어 패배
+                        ResultType.LOSE
                 ),
                 Arguments.arguments(
                         // 플레이어만 버스트하는 경우
@@ -119,22 +134,7 @@ class DealerTest {
                         new Card(CardShape.HEART, CardNumber.FIVE),
                         List.of(new Card(CardShape.CLOVER, CardNumber.THREE),
                                 new Card(CardShape.CLOVER, CardNumber.KING)),
-                        // 딜러 승리
-                        ResultType.WIN
-                ),
-                Arguments.arguments(
-                        // 플레이어가 승리한다.
-                        // 플레이어
-                        new Card(CardShape.DIAMOND, CardNumber.TWO),
-                        new Card(CardShape.DIAMOND, CardNumber.FIVE),
-                        List.of(new Card(CardShape.SPADE, CardNumber.THREE),
-                                new Card(CardShape.HEART, CardNumber.QUEEN)),
-                        // 딜러
-                        new Card(CardShape.HEART, CardNumber.TWO),
-                        new Card(CardShape.HEART, CardNumber.FIVE),
-                        List.of(new Card(CardShape.CLOVER, CardNumber.TWO),
-                                new Card(CardShape.CLOVER, CardNumber.KING)),
-                        // 딜러 패배
+                        // 플레이어 패배
                         ResultType.LOSE
                 ),
                 Arguments.arguments(
@@ -149,11 +149,11 @@ class DealerTest {
                         new Card(CardShape.HEART, CardNumber.FIVE),
                         List.of(new Card(CardShape.CLOVER, CardNumber.THREE),
                                 new Card(CardShape.CLOVER, CardNumber.KING)),
-                        // 딜러 승리
-                        ResultType.WIN
+                        // 플레이어 패배
+                        ResultType.LOSE
                 ),
                 Arguments.arguments(
-                        // 플레이어 딜러 모두 무승부한다.
+                        // 플레이어 딜러은 점수가 동일하고 블랙잭이 아닐 경우 플레이어가 패배한다.
                         // 플레이어
                         new Card(CardShape.DIAMOND, CardNumber.TWO),
                         new Card(CardShape.DIAMOND, CardNumber.FIVE),
@@ -164,7 +164,22 @@ class DealerTest {
                         new Card(CardShape.HEART, CardNumber.FIVE),
                         List.of(new Card(CardShape.CLOVER, CardNumber.TWO),
                                 new Card(CardShape.CLOVER, CardNumber.KING)),
-                        // 딜러 무승부
+                        // 플레이어 패배
+                        ResultType.LOSE
+                ),
+                Arguments.arguments(
+                        // 플레이어 딜러 모두 블랙잭일 떄 무승부한다.
+                        // 플레이어
+                        new Card(CardShape.DIAMOND, CardNumber.TWO),
+                        new Card(CardShape.DIAMOND, CardNumber.FIVE),
+                        List.of(new Card(CardShape.SPADE, CardNumber.FOUR),
+                                new Card(CardShape.HEART, CardNumber.QUEEN)),
+                        // 딜러
+                        new Card(CardShape.HEART, CardNumber.TWO),
+                        new Card(CardShape.HEART, CardNumber.FIVE),
+                        List.of(new Card(CardShape.CLOVER, CardNumber.FOUR),
+                                new Card(CardShape.CLOVER, CardNumber.KING)),
+                        // 플레이어
                         ResultType.PUSH
                 )
         );
