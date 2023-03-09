a0fc83bbabe8deb504a72782a6f5df10dff6466d
greeng00se <thegreengoose7@gmail.com>
2023-03-09 11:45:59 +0900

    refactor: InputView 상수 제거

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -8,13 +8,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String NEW_LINE = System.lineSeparator();
-    private static final String INPUT_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String DELIMITER = ",";
     private static final int LIMIT = -1;
-    private static final String INPUT_PLAYER_BET_MESSAGE = NEW_LINE + "의 배팅 금액은?";
-    private static final String INVALID_NUMBER_MESSAGE = "베팅 금액은 숫자만 입력해야 합니다.";
-    private static final String INPUT_PLAYER_COMMAND_MESSAGE = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
 
     private final Scanner scanner;
 
@@ -23,7 +18,7 @@ public class InputView {
     }
 
     public List<String> readPlayers() {
-        System.out.println(INPUT_PLAYER_NAMES_MESSAGE);
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         final String names = scanner.nextLine();
 
         return Arrays.stream(names.split(DELIMITER, LIMIT))
@@ -31,19 +26,19 @@ public class InputView {
                 .collect(toList());
     }
 
-    public String readCommand(final Player player) {
-        System.out.println(player.getNameValue() + INPUT_PLAYER_COMMAND_MESSAGE);
-
-        return scanner.nextLine();
-    }
-
     public int readBet(final Player player) {
-        System.out.println(player.getNameValue() + INPUT_PLAYER_BET_MESSAGE);
+        System.out.println(player.getNameValue() + "의 배팅 금액은?");
 
         try {
             return Integer.parseInt(scanner.nextLine());
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(INVALID_NUMBER_MESSAGE);
+            throw new IllegalArgumentException("베팅 금액은 숫자만 입력해야 합니다.");
         }
+    }
+
+    public String readCommand(final Player player) {
+        System.out.println(player.getNameValue() + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+
+        return scanner.nextLine();
     }
 }
