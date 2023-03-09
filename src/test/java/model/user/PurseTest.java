785ef23da0d61e59a2fe8fe1c0013992e89b8bbb
Ethan <kim76744dev@gmail.com>
2023-03-09 17:21:10 +0900

    feat: Purse에 돈을 추가하는 기능 구현

--- src/test/java/model/user/PurseTest.java
+++ src/test/java/model/user/PurseTest.java
@@ -1,6 +1,7 @@
 package model.user;
 
 import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,20 +12,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PurseTest {
 
+    private Player bebe;
+    private Player ethan;
+    private Purse purse;
+
+    @BeforeEach
+    void init() {
+        bebe = new Player("bebe");
+        ethan = new Player("ethan");
+        final List<Player> players = List.of(bebe, ethan);
+
+        purse = Purse.create(players);
+    }
+
     @Test
     @DisplayName("지갑을 생성할 때, 기본적으로 0원을 들고 있게 한다.")
     void whenCreatePurse_thenHaveZeroMoney() {
-        // given
-        final Player bebe = new Player("bebe");
-        final Player ethan = new Player("ethan");
-        final List<Player> players = List.of(bebe, ethan);
-
-        // when
-        final Purse purse = Purse.create(players);
-
-        // then
         assertThat(purse)
                 .extracting("purses", InstanceOfAssertFactories.map(Player.class, Money.class))
                 .containsExactly(Map.entry(bebe, Money.zero()), Map.entry(ethan, Money.zero()));
+    }
+
+    @Test
+    @DisplayName("지갑에 돈을 추가할 수 있다.")
+    void whenAddMoney_thenSuccess() {
+        // when
+        purse.addMoney(bebe, new Money(10_000L));
+
+        // then
+        assertThat(purse.getMoney(bebe)).isEqualTo(new Money(10_000L));
     }
 }
