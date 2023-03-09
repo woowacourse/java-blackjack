1a557dc83049cfe85cd1effa6a685c73628a1b58
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-03-09 11:17:42 +0900

    feat: 베팅 금액 저장 객체 구현

---
+++ src/main/java/domain/player/Batting.java
@@ -0,0 +1,19 @@
+package domain.player;
+
+public class Batting {
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 100_000_000;
+
+    private final int amount;
+
+    public Batting(final int amount) {
+        validateRange(amount);
+        this.amount = amount;
+    }
+
+    private static void validateRange(final int amount) {
+        if (amount < MIN_RANGE || amount > MAX_RANGE) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
