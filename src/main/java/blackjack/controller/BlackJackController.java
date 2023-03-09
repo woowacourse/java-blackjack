767d041850d0a2ed81168ace6b3532df43e2da6f
parkmuhyeun <pjhg410@gmail.com>
2023-03-09 14:48:08 +0900

    refactor: BlackJackGame 상태 추가

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -13,18 +13,23 @@ import java.util.List;
 
 public class BlackJackController {
 
+    private final BlackJackGame blackJackGame;
+
+    public BlackJackController() {
+        blackJackGame = new BlackJackGame(new RandomCardShuffle());
+    }
+
     public void run() {
         final Players players = Repeater.repeatIfError(this::inputPlayerNames, OutputView::printErrorMessage);
         final Dealer dealer = new Dealer();
-        final BlackJackGame blackJackGame = new BlackJackGame(new RandomCardShuffle());
 
-        initHit(players, dealer, blackJackGame);
-        askPlayers(players, blackJackGame);
-        hitCardByDealer(dealer, blackJackGame);
-        printFinal(players, dealer, blackJackGame);
+        initHit(players, dealer);
+        askPlayers(players);
+        hitCardByDealer(dealer);
+        printFinal(players, dealer);
     }
 
-    private void initHit(Players players, Dealer dealer, BlackJackGame blackJackGame) {
+    private void initHit(Players players, Dealer dealer) {
         blackJackGame.initHit(players, dealer);
         OutputView.printInitCardDeck(dealer, players);
     }
@@ -33,26 +38,26 @@ public class BlackJackController {
         return new Players(InputView.inputPlayerNames());
     }
 
-    private void askPlayers(Players players, BlackJackGame blackJackGame) {
+    private void askPlayers(Players players) {
         for (Player player : players.getPlayers()) {
-            askPlayer(player, blackJackGame);
+            askPlayer(player);
         }
     }
 
-    private void askPlayer(Player player, BlackJackGame blackJackGame) {
+    private void askPlayer(Player player) {
         Command command = Command.CONTINUE;
         int score = 0;
 
-        while (isContinueToAsk(command, score, blackJackGame)) {
+        while (isContinueToAsk(command, score)) {
             command = Repeater.repeatIfError(() -> inputCommand(player), OutputView::printErrorMessage);
-            hitByCommand(player, command, blackJackGame);
+            hitByCommand(player, command);
             OutputView.printPlayerCardDeck(player);
             score = blackJackGame.calculateScore(player);
-            checkBurst(blackJackGame, score);
+            checkBurst(score);
         }
     }
 
-    private boolean isContinueToAsk(Command command, int score, BlackJackGame blackJackGame) {
+    private boolean isContinueToAsk(Command command, int score) {
         return Command.isContinue(command) && blackJackGame.isValidScore(score);
     }
 
@@ -60,19 +65,19 @@ public class BlackJackController {
         return Command.toCommand(InputView.inputReply(player.getName().getValue()));
     }
 
-    private void hitByCommand(Player player, Command command, BlackJackGame blackJackGame) {
+    private void hitByCommand(Player player, Command command) {
         if (Command.isContinue(command)) {
             blackJackGame.hit(player);
         }
     }
 
-    private void checkBurst(BlackJackGame blackJackGame, int score) {
+    private void checkBurst(int score) {
         if (blackJackGame.isBurst(score)) {
             OutputView.printBurstMessage();
         }
     }
 
-    private void hitCardByDealer(Dealer dealer, BlackJackGame blackJackGame) {
+    private void hitCardByDealer(Dealer dealer) {
         OutputView.println();
         int dealerScore = blackJackGame.calculateScore(dealer);
 
@@ -83,7 +88,7 @@ public class BlackJackController {
         }
     }
 
-    private void printFinal(Players players, Dealer dealer, BlackJackGame blackJackGame) {
+    private void printFinal(Players players, Dealer dealer) {
         List<Result> results = blackJackGame.getPlayersResult(dealer, players);
 
         OutputView.printFinalCardDeckAndScore(dealer, players);
