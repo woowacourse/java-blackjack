6e83521173d76e3157b1557cc3ab7da4cc0bd898
yoondgu <doyou2ing@naver.com>
2023-03-09 13:50:32 +0900

    test: 카드 끗수 에이스 확인 테스트 추가

--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -1,5 +1,6 @@
 package blackjack.domain.card;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.DisplayName;
@@ -17,4 +18,17 @@ class CardTest {
                 .doesNotThrowAnyException();
     }
 
+    @DisplayName("카드의 끗수가 에이스이면 참을 반환한다.")
+    @Test
+    void should_ReturnTrue_When_SuitIsAce() {
+        assertThat(new Card(Suit.SPADE, Denomination.ACE).isACE())
+                .isTrue();
+    }
+
+    @DisplayName("카드의 끗수가 에이스가 아니면 거짓을 반환한다.")
+    @Test
+    void should_ReturnFalse_When_SuitIsNotAce() {
+        assertThat(new Card(Suit.SPADE, Denomination.TEN).isACE())
+                .isFalse();
+    }
 }
--- src/test/java/blackjack/domain/participants/ParticipantsTest.java
+++ src/test/java/blackjack/domain/participants/ParticipantsTest.java
@@ -1,7 +1,8 @@
 package blackjack.domain.participants;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -17,7 +18,7 @@ class ParticipantsTest {
     void should_ThrowException_When_PlayerNamesDuplicated() {
         final List<String> playerNames = List.of("pobi", "pobi", "jason");
 
-        Assertions.assertThatThrownBy(() -> Participants.of(dealerName, playerNames))
+        assertThatThrownBy(() -> Participants.of(dealerName, playerNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("플레이어 이름은 중복될 수 없습니다.");
     }
@@ -27,7 +28,7 @@ class ParticipantsTest {
     void should_ThrowException_When_PlayerNameDuplicatedWithDealerName() {
         final List<String> playerNames = List.of("pobi", "딜러");
 
-        Assertions.assertThatThrownBy(() -> Participants.of(dealerName, playerNames))
+        assertThatThrownBy(() -> Participants.of(dealerName, playerNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("플레이어 이름은 딜러 이름(" + dealerName + ")과 중복될 수 없습니다.");
     }
