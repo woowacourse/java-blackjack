c6fd7e24d75c796f363a79b2bf8c3e5ae3475d3d
hyunseo <goatsfish@gmail.com>
2023-03-09 11:15:51 +0900

    test: 플레이어 배팅 결과 계산 기능 테스트 추가

--- src/test/java/blackjack/domain/game/BettingPlayersTest.java
+++ src/test/java/blackjack/domain/game/BettingPlayersTest.java
@@ -1,17 +1,25 @@
 package blackjack.domain.game;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardNumber;
+import blackjack.domain.card.CardShape;
 import blackjack.domain.card.Deck;
+import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Money;
 import blackjack.domain.participant.Player;
 import blackjack.fixture.ParticipantCardsFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class BettingPlayersTest {
     @Test
@@ -46,5 +54,167 @@ class BettingPlayersTest {
 
         assertThatThrownBy(() -> new BettingPlayers(List.of(player), Collections.emptyList()))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("decideBetResultByDummy")
+    @DisplayName("배팅 결과를 계산한다.")
+    void decideBetResultBy(
+            final Card playerCard1,
+            final Card playerCard2,
+            final List<Card> playerAdditionalCards,
+            final Card dealerCard1,
+            final Card dealerCard2,
+            final List<Card> dealerAdditionalCards,
+            final int playerBeforeBetMoney,
+            final int playerAfterBetMoney
+    ) {
+        final ParticipantCards dealerCards = ParticipantCardsFixture.create(dealerCard1, dealerCard2, dealerAdditionalCards);
+        final ParticipantCards participantCard = ParticipantCardsFixture.create(playerCard1, playerCard2, playerAdditionalCards);
+
+        final Dealer dealer = new Dealer(dealerCards);
+        final Player player = new Player(participantCard, "헤나");
+        final BettingPlayers bettingPlayers = new BettingPlayers(List.of(player), List.of(new Money(playerBeforeBetMoney)));
+
+        final Map<Player, Money> players = bettingPlayers.decideBetResultBy(dealer);
+        final Money currentMoney = players.get(player);
+
+        assertThat(currentMoney.getValue()).isEqualTo(playerAfterBetMoney);
+    }
+
+    @Test
+    @DisplayName("플레이어 리스트를 가져온다.")
+    void getPlayers() {
+        final Deck deck = new Deck();
+        final ParticipantCards cards = ParticipantCardsFixture.create(deck.draw(), deck.draw(), List.of());
+        final Player player = new Player(cards, "헤나");
+        final BettingPlayers bettingPlayers = new BettingPlayers(List.of(player), List.of(Money.ZERO));
+        final List<Player> players = bettingPlayers.getPlayers();
+
+        assertThat(players).contains(player);
+    }
+
+    static Stream<Arguments> decideBetResultByDummy() {
+        return Stream.of(
+                Arguments.arguments(
+                        // 플레이어 패배, 플레이어 딜러 모두 버스트하는 경우
+                        // 플레이어
+                        new Card(CardShape.DIAMOND, CardNumber.NINE),
+                        new Card(CardShape.DIAMOND, CardNumber.QUEEN),
+                        List.of(new Card(CardShape.SPADE, CardNumber.JACK),
+                                new Card(CardShape.HEART, CardNumber.KING)),
+                        // 딜러
+                        new Card(CardShape.HEART, CardNumber.TWO),
+                        new Card(CardShape.HEART, CardNumber.QUEEN),
+                        List.of(new Card(CardShape.CLOVER, CardNumber.JACK),
+                                new Card(CardShape.CLOVER, CardNumber.KING)),
+                        // 플레이어 배팅 이전 금액
+                        10000,
+                        // 플레이어 배팅 이후 금액
+                        0
+                ),
+                Arguments.arguments(
+                        // 플레이어 패배, 플레이어만 버스트하는 경우
+                        // 플레이어
+                        new Card(CardShape.DIAMOND, CardNumber.NINE),
+                        new Card(CardShape.DIAMOND, CardNumber.QUEEN),
+                        List.of(new Card(CardShape.SPADE, CardNumber.JACK),
+                                new Card(CardShape.HEART, CardNumber.KING)),
+                        // 딜러
+                        new Card(CardShape.HEART, CardNumber.TWO),
+                        new Card(CardShape.HEART, CardNumber.FIVE),
+                        List.of(new Card(CardShape.CLOVER, CardNumber.THREE),
+                                new Card(CardShape.CLOVER, CardNumber.KING)),
+                        // 플레이어 배팅 이전 금액
+                        10000,
+                        // 플레이어 배팅 이후 금액
+                        0
+                ),
+                Arguments.arguments(
+                        // 플레이어 승리, 플레이어의 점수가 딜러보다 높을 경우
+                        // 플레이어
+                        new Card(CardShape.DIAMOND, CardNumber.TWO),
+                        new Card(CardShape.DIAMOND, CardNumber.FIVE),
+                        List.of(new Card(CardShape.SPADE, CardNumber.THREE),
+                                new Card(CardShape.HEART, CardNumber.QUEEN)),
+                        // 딜러
+                        new Card(CardShape.HEART, CardNumber.TWO),
+                        new Card(CardShape.HEART, CardNumber.FIVE),
+                        List.of(new Card(CardShape.CLOVER, CardNumber.TWO),
+                                new Card(CardShape.CLOVER, CardNumber.KING)),
+                        // 플레이어 배팅 이전 금액
+                        10000,
+                        // 플레이어 배팅 이후 금액
+                        20000
+                ),
+                Arguments.arguments(
+                        // 플레이어 패배, 플레이어의 점수보다 딜러가 높을 경우
+                        // 플레이어
+                        new Card(CardShape.DIAMOND, CardNumber.TWO),
+                        new Card(CardShape.DIAMOND, CardNumber.FIVE),
+                        List.of(new Card(CardShape.SPADE, CardNumber.TWO),
+                                new Card(CardShape.HEART, CardNumber.QUEEN)),
+                        // 딜러
+                        new Card(CardShape.HEART, CardNumber.TWO),
+                        new Card(CardShape.HEART, CardNumber.FIVE),
+                        List.of(new Card(CardShape.CLOVER, CardNumber.THREE),
+                                new Card(CardShape.CLOVER, CardNumber.KING)),
+                        // 플레이어 배팅 이전 금액
+                        10000,
+                        // 플레이어 배팅 이후 금액
+                        0
+                ),
+                Arguments.arguments(
+                        // 플레이어 패배, 플레이어와 딜러의 점수가 동일하고 블랙잭이 아닐 경우
+                        // 플레이어
+                        new Card(CardShape.DIAMOND, CardNumber.TWO),
+                        new Card(CardShape.DIAMOND, CardNumber.FIVE),
+                        List.of(new Card(CardShape.SPADE, CardNumber.TWO),
+                                new Card(CardShape.HEART, CardNumber.QUEEN)),
+                        // 딜러
+                        new Card(CardShape.HEART, CardNumber.TWO),
+                        new Card(CardShape.HEART, CardNumber.FIVE),
+                        List.of(new Card(CardShape.CLOVER, CardNumber.TWO),
+                                new Card(CardShape.CLOVER, CardNumber.KING)),
+                        // 플레이어 배팅 이전 금액
+                        10000,
+                        // 플레이어 배팅 이후 금액
+                        0
+                ),
+                Arguments.arguments(
+                        // 플레이어 무승부, 플레이어와 딜러의 점수가 블랙잭일 경우
+                        // 플레이어
+                        new Card(CardShape.DIAMOND, CardNumber.TWO),
+                        new Card(CardShape.DIAMOND, CardNumber.FIVE),
+                        List.of(new Card(CardShape.SPADE, CardNumber.FOUR),
+                                new Card(CardShape.HEART, CardNumber.QUEEN)),
+                        // 딜러
+                        new Card(CardShape.HEART, CardNumber.TWO),
+                        new Card(CardShape.HEART, CardNumber.FIVE),
+                        List.of(new Card(CardShape.CLOVER, CardNumber.FOUR),
+                                new Card(CardShape.CLOVER, CardNumber.KING)),
+                        // 플레이어 배팅 이전 금액
+                        10000,
+                        // 플레이어 배팅 이후 금액
+                        10000
+                ),
+                Arguments.arguments(
+                        // 플레이어 승리, 플레이어가 블랙잭이고 딜러는 아닐 경우
+                        // 플레이어
+                        new Card(CardShape.DIAMOND, CardNumber.TWO),
+                        new Card(CardShape.DIAMOND, CardNumber.FIVE),
+                        List.of(new Card(CardShape.SPADE, CardNumber.FOUR),
+                                new Card(CardShape.HEART, CardNumber.QUEEN)),
+                        // 딜러
+                        new Card(CardShape.HEART, CardNumber.TWO),
+                        new Card(CardShape.HEART, CardNumber.FIVE),
+                        List.of(new Card(CardShape.CLOVER, CardNumber.THREE),
+                                new Card(CardShape.CLOVER, CardNumber.KING)),
+                        // 플레이어 배팅 이전 금액
+                        10000,
+                        // 플레이어 배팅 이후 금액
+                        25000
+                )
+        );
     }
 }
