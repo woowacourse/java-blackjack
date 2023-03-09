32badbc83e9adc01b36cccd69cc0d56f794e9813
hyunseo <goatsfish@gmail.com>
2023-03-10 03:56:46 +0900

    feat: 블랙잭 참여자 배팅 필드 추가

--- src/main/java/blackjack/domain/game/BlackJackParticipants.java
+++ src/main/java/blackjack/domain/game/BlackJackParticipants.java
@@ -2,33 +2,42 @@ package blackjack.domain.game;
 
 import blackjack.domain.card.Deck;
 import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Money;
+import blackjack.domain.participant.Participant;
 import blackjack.domain.participant.Player;
-import blackjack.domain.participant.Players;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class BlackJackParticipants {
     private final Dealer dealer;
-    private final Players players;
+    private final BettingPlayers bettingPlayers;
 
-    public BlackJackParticipants(final Deck deck, final List<String> nameValues) {
+    public BlackJackParticipants(final Deck deck, final List<String> nameValues, final List<Integer> moneyValues) {
         this.dealer = new Dealer(initCards(deck));
-        this.players = new Players(createPlayers(deck, nameValues));
+        this.bettingPlayers = new BettingPlayers(createPlayers(deck, nameValues), createMoneys(moneyValues));
     }
 
-    public BlackJackResults createBlackJackResults() {
-        return new BlackJackResults(dealer, players.getPlayers());
+    public BettingResults createBettingResults() {
+        final Map<Participant, Money> bettingResults = bettingPlayers.findBettingResultsBy(dealer);
+        return new BettingResults(bettingResults, dealer);
     }
 
-    private List<Player> createPlayers(final Deck deck, final List<String> names) {
-        return names.stream()
-                .map(name -> new Player(initCards(deck), name))
+    private List<Player> createPlayers(final Deck deck, final List<String> nameValues) {
+        return nameValues.stream()
+                .map(nameValue -> new Player(initCards(deck), nameValue))
                 .collect(Collectors.toList());
     }
 
     private ParticipantCards initCards(final Deck deck) {
-        return new ParticipantCards(List.of(deck.draw(), deck.draw()));
+        return new ParticipantCards(deck);
+    }
+
+    private List<Money> createMoneys(final List<Integer> moneyValues) {
+        return moneyValues.stream()
+                .map(Money::new)
+                .collect(Collectors.toList());
     }
 
     public Dealer getDealer() {
@@ -36,6 +45,6 @@ public class BlackJackParticipants {
     }
 
     public List<Player> getPlayers() {
-        return players.getPlayers();
+        return bettingPlayers.getPlayers();
     }
 }
