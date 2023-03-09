8efa5190bbb3028a3e6fa80b19ec736bb69269de
iamjooon2 <iamjooon2@gmail.com>
2023-03-09 13:16:20 +0900

    test: parameterizedTest 활용

--- src/test/java/domain/participant/ParticipantsTest.java
+++ src/test/java/domain/participant/ParticipantsTest.java
@@ -4,13 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import domain.PlayerGameResult;
-import domain.card.Card;
 import domain.card.Deck;
-import domain.card.Denomination;
-import domain.card.Suit;
 import java.util.List;
-import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,8 +14,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 @DisplayName("Participants는 ")
 class ParticipantsTest {
 
+    @ParameterizedTest(name = "player가 총 1~7명 일때만 생성된다 name = {0}")
     @ValueSource(strings = {"가비", "연어,가비,애쉬,우가,럿고,비버,아코"})
-    void 모두_1에서_7명이다(String names) {
+    void player_가_총_1에서_7명이다(String names) {
         //given
 
         //when
@@ -29,8 +25,9 @@ class ParticipantsTest {
         assertDoesNotThrow(() -> Participants.of(List.of(names.split(","))));
     }
 
+    @ParameterizedTest(name = "player가 총 1~7명이 아니면 예외가 발생한다 name = {0}")
     @ValueSource(strings = {",", "연어,가비,애쉬,우가,럿고,비버,아코,네오"})
-    void 총_1에서_7명이_아니면_예외가_발생한다(String names) {
+    void player_가_총_1에서_7명이_아니면_예외가_발생한다(String names) {
         //given
 
         //when
