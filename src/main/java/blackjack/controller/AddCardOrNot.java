11f9ec92cacc68d6c1acd65e09f3897cfda67879
Rosie <dev.kayoung@gmail.com>
2023-03-09 13:30:41 +0900

    feat: 카드 추가 명령어가 올바르지 않으면 예외처리

--- src/main/java/blackjack/controller/AddCardOrNot.java
+++ src/main/java/blackjack/controller/AddCardOrNot.java
@@ -1,23 +1,17 @@
 package blackjack.controller;
 
 public enum AddCardOrNot {
-    YES("y"),
-    NO("n");
+    YES,
+    NO;
 
-    private final String type;
-
-    AddCardOrNot(String type) {
-        this.type = type;
-    }
-
-    public static AddCardOrNot of(String type) {
-        if (type.equals("y")) {
+    public static AddCardOrNot of(String command) {
+        String lowerCommand = command.toLowerCase();
+        if (lowerCommand.equals("y")) {
             return YES;
         }
-        if (type.equals("n")) {
+        if (lowerCommand.equals("n")) {
             return NO;
         }
-        // TODO: y/n 이 아닐 경우 예외처리
-        return NO;
+        throw new IllegalArgumentException("올바른 카드 추가 명령어가 아닙니다.");
     }
 }
