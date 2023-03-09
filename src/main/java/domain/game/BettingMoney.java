4ecab9b62bade93d6bbd836d1d38f54448bf27e9
jiwon <ljwon77@daum.net>
2023-03-09 16:19:05 +0900

    feat: 베팅 금액이 정수값인지 검증한다

--- src/main/java/domain/game/BettingMoney.java
+++ src/main/java/domain/game/BettingMoney.java
@@ -1,13 +1,25 @@
 package domain.game;
 
 public class BettingMoney {
+    
     private final int money;
 
     private BettingMoney(final int money) {
         this.money = money;
     }
 
-    public static BettingMoney create(final int money) {
-        return new BettingMoney(money);
+    public static BettingMoney create(final String money) {
+        int validMoney = validType(money);
+        return new BettingMoney(validMoney);
+    }
+
+    private static int validType(final String money) {
+        int validMoney;
+        try {
+            validMoney = Integer.parseInt(money);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("베팅 금액은 정수 값이어야 합니다.");
+        }
+        return validMoney;
     }
 }
--- src/test/java/domain/game/BettingMoneyTest.java
+++ src/test/java/domain/game/BettingMoneyTest.java
@@ -1,18 +1,35 @@
 package domain.game;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class BettingMoneyTest {
 
-    @Test
-    @DisplayName("create()는 호출하면 BettingMoney를 생성한다.")
+    private BettingMoney bettingMoney;
+
+    @BeforeEach
+    void init() {
+        bettingMoney = BettingMoney.create("10000");
+    }
+
+    @ParameterizedTest(name = "create()는 호출하면 BettingMoney를 생성한다.")
+    @ValueSource(strings = {"1", "100000000"})
     void create_whenCall_thenSuccess() {
-        BettingMoney bettingMoney = assertDoesNotThrow(() -> BettingMoney.create(1000));
+        BettingMoney bettingMoney = assertDoesNotThrow(() -> BettingMoney.create("10000"));
         assertThat(bettingMoney)
                 .isInstanceOf(BettingMoney.class);
+    }
+
+    @ParameterizedTest(name = "create()는 정수 값이 아닌 금액이 주어지면, 예외를 반환한다.")
+    @ValueSource(strings = {"string", "!#@($", "", "12wow34"})
+    void create_givenInvalidTypeMoney_thenFail(final String money) {
+        assertThatThrownBy(() -> BettingMoney.create(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("베팅 금액은 정수 값이어야 합니다.");
     }
 }
