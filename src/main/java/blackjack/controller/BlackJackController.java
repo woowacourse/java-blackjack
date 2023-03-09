2547f7202f54da6cf272e4023c4622353053a2eb
Be-student <presentsong@naver.com>
2023-03-09 22:09:57 +0900

    docs: 질문 정리

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -24,6 +24,51 @@ public class BlackJackController {
         this.outputView = outputView;
     }
 
+    //이 메서드는 10라인도 훨씬 넘었고, 되게 복잡하게 되어있는데, 이렇게 두는 것이 좋은가요?
+    //원래대로 돌렸을 경우에, 훨씬 간단해보였던 것 같아요
+//    public void play(final DeckFactory deckFactory) {
+//        final BlackJackGame blackJackGame = repeatUntilNoException(
+//                () -> BlackJackGame.of(
+//                        inputPlayerNames(),
+//                        deckFactory,
+//                        new BlackJackRuleImpl()),
+//                outputView::printError);
+//
+//        for (final String playerName : blackJackGame.getPlayerNames()) {
+//            blackJackGame.addPlayerMoney(playerName, inputPlayerMoney(playerName));
+//        }
+//
+//        blackJackGame.distributeInitialCard();
+//
+//        outputView.printInitialCards(
+//                convertCard(blackJackGame.getDealerFirstCard()),
+//                getPlayerCards(blackJackGame.getPlayers()));
+//
+//        for (final String playerName : blackJackGame.getPlayerNames()) {
+//            DrawCommand playerChoice = DrawCommand.DRAW;
+//            while (blackJackGame.isPlayerDrawable(playerName) && playerChoice != DrawCommand.STAY) {
+//                playerChoice = inputPlayerChoice(playerName);
+//                if (playerChoice == DrawCommand.DRAW) {
+//                    blackJackGame.drawPlayerCard(playerName);
+//                }
+//                outputView.printCardStatusOfPlayer(playerName, convertCards(blackJackGame.getPlayerCards(playerName)));
+//            }
+//        }
+//
+//        while (blackJackGame.isDealerDrawable()) {
+//            blackJackGame.drawDealerCard();
+//            outputView.printDealerCardDrawMessage();
+//        }
+//
+//        outputView.printFinalStatusOfDealer(blackJackGame.getDealerScore(),
+//                convertCards(blackJackGame.getDealerCards()));
+//        outputView.printFinalStatusOfPlayers(convertPlayersCards(blackJackGame.getPlayersCards()),
+//                blackJackGame.getPlayersScores());
+//
+//        outputView.printFinalMoney(blackJackGame.calculatePlayersMoney());
+//    }
+
+    //한 메서드가 너무 길어지는 느낌이 드는 것 같은데요 이것은 어떤가요?
     public void play(final DeckFactory deckFactory) {
         final BlackJackGame blackJackGame = repeatUntilNoException(
                 () -> BlackJackGame.of(
@@ -43,14 +88,7 @@ public class BlackJackController {
                 getPlayerCards(blackJackGame.getPlayers()));
 
         for (final String playerName : blackJackGame.getPlayerNames()) {
-            DrawCommand playerChoice = DrawCommand.DRAW;
-            while (blackJackGame.isPlayerDrawable(playerName) && playerChoice != DrawCommand.STAY) {
-                playerChoice = inputPlayerChoice(playerName);
-                if (playerChoice == DrawCommand.DRAW) {
-                    blackJackGame.drawPlayerCard(playerName);
-                }
-                outputView.printCardStatusOfPlayer(playerName, convertCards(blackJackGame.getPlayerCards(playerName)));
-            }
+            drawPlayerCard(blackJackGame, playerName);
         }
 
         while (blackJackGame.isDealerDrawable()) {
@@ -64,6 +102,17 @@ public class BlackJackController {
                 blackJackGame.getPlayersScores());
 
         outputView.printFinalMoney(blackJackGame.calculatePlayersMoney());
+    }
+
+    private void drawPlayerCard(final BlackJackGame blackJackGame, final String playerName) {
+        DrawCommand playerChoice = DrawCommand.DRAW;
+        while (blackJackGame.isPlayerDrawable(playerName) && playerChoice != DrawCommand.STAY) {
+            playerChoice = inputPlayerChoice(playerName);
+            if (playerChoice == DrawCommand.DRAW) {
+                blackJackGame.drawPlayerCard(playerName);
+            }
+            outputView.printCardStatusOfPlayer(playerName, convertCards(blackJackGame.getPlayerCards(playerName)));
+        }
     }
 
     private int inputPlayerMoney(final String playerName) {
