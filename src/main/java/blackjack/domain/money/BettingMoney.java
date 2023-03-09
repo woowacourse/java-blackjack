f4168dcee691cff6cdbd1a91cb899df717b48bca
seokhwan-an <asok369258@naver.com>
2023-03-09 12:27:37 +0900

    feat : 배팅금액 객체 추가

---
+++ src/main/java/blackjack/domain/money/BettingMoney.java
@@ -0,0 +1,33 @@
+package blackjack.domain.money;
+
+public class BettingMoney {
+
+    private final int money;
+
+    public BettingMoney(final String money) {
+        validateNullOrBlank(money);
+        validateDigit(money);
+        validatePositive(money);
+        this.money = Integer.parseInt(money);
+    }
+
+    private void validateNullOrBlank(final String money) {
+        if (money == null || money.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 배팅 금액은 null이나 공백이면 안됩니다.");
+        }
+    }
+
+    private void validateDigit(final String moeny) {
+        try {
+            Integer.parseInt(moeny);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 배팅 금액은 숫자여야 합니다.");
+        }
+    }
+
+    private void validatePositive(final String money) {
+        if (Integer.parseInt(money) <= 0) {
+            throw new IllegalArgumentException("[ERROR] 배팅 금액은 양수이어야 합니다.");
+        }
+    }
+}
