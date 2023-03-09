bddaeae93ce78b5f5a3b79f2e18ddf9caf831f55
jiwon <ljwon77@daum.net>
2023-03-09 10:50:15 +0900

    refactor: inputView를 생성자 주입을 사용하도록 변경한다

--- src/main/java/GameApplication.java
+++ src/main/java/GameApplication.java
@@ -2,9 +2,13 @@ import controller.GameController;
 import view.InputView;
 import view.OutputView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public final class GameApplication {
     public static void main(String[] args) {
-        final InputView inputView = new InputView();
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final InputView inputView = new InputView(bufferedReader);
         final OutputView outputView = new OutputView();
 
         final GameController gameController = new GameController(inputView, outputView);
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,7 +2,6 @@ package view;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -18,8 +17,8 @@ public final class InputView {
 
     private final BufferedReader bufferedReader;
 
-    public InputView() {
-        this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    public InputView(final BufferedReader bufferedReader) {
+        this.bufferedReader = bufferedReader;
     }
 
     public <T> T getInputWithRetry(Supplier<T> inputReader) {
