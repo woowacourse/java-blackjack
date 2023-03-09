e5596e2e1843c1ad766d124e04e820c6da304fd2
hyunseo <goatsfish@gmail.com>
2023-03-10 03:50:46 +0900

    test: 덱 네이밍 수정

--- src/test/java/blackjack/domain/card/CardDeckTest.java
+++ src/test/java/blackjack/domain/card/CardDeckTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class DeckTest {
+class CardDeckTest {
     @Test
     @DisplayName("덱이 비어있을 때 카드를 뽑으면 예외가 발생한다.")
     void throwExceptionWhenDeckIsEmpty() {
         final int cardNumberSize = CardNumber.values().length;
         final int cardShapeSize = CardShape.values().length;
         final int deckSize = cardNumberSize * cardShapeSize;
-        final Deck deck = new Deck();
+        final Deck deck = new CardDeck();
         for (int count = 0; count < deckSize; count++) {
             deck.draw();
         }
@@ -24,7 +24,7 @@ class DeckTest {
     @Test
     @DisplayName("카드를 뽑는다.")
     void drawCard() {
-        assertThat(new Deck().draw())
+        assertThat(new CardDeck().draw())
                 .isNotNull();
     }
 }
