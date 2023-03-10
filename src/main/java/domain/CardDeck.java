92dfc35e3484e81a5872ff09a52dfbe2df78a5ef
the9kim <96895686+the9kim@users.noreply.github.com>
2023-03-10 15:46:07 +0900

    feat(CardDeck): 모든 종류의 카드를 가진 카드덱을 생성한다

--- src/main/java/domain/CardDeck.java
+++ src/main/java/domain/CardDeck.java
@@ -1,6 +1,46 @@
 package domain;
 
+import java.util.*;
+
 public class CardDeck {
+
+    private final Deque<Card> cardDeck;
+
+    private CardDeck(Deque<Card> cardDeck) {
+        this.cardDeck = cardDeck;
+    }
+
+    public static CardDeck generateCardDeck() {
+        List<Card> cards = new ArrayList<>();
+        makeCards(cards);
+        shuffleCards(cards);
+        Deque<Card> cardDeck = new ArrayDeque<>(cards);
+        return new CardDeck(cardDeck);
+    }
+
+    private static void makeCards(List<Card> cards) {
+        for (CardType cardType : CardType.values()) {
+            addCards(cards, cardType);
+        }
+    }
+
+    private static void addCards(List<Card> cards, CardType cardType) {
+        for (CardValue cardValue : CardValue.values()) {
+            cards.add(new Card(cardType, cardValue));
+        }
+    }
+
+    private static void shuffleCards(List<Card> cards) {
+        Collections.shuffle(cards);
+    }
+
+    public Card pickCard() {
+        return cardDeck.pop();
+    }
+
+    public Deque<Card> getCardDeck() {
+        return cardDeck;
+    }
 
 //    private final Stack<Card> initPool = new Stack<>();
 //
--- src/test/java/domain/CardDeckTest.java
+++ src/test/java/domain/CardDeckTest.java
@@ -1,10 +1,38 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 class CardDeckTest {
+    @Test
+    @DisplayName("카드덱을 생성하면 카드 타입과 값 별로 52장의 카드가 만들어진다")
+    void generateCardDeck() {
+
+        CardDeck cardDeck = CardDeck.generateCardDeck();
+
+        assertThat(cardDeck.getCardDeck().size()).isEqualTo(52);
+    }
+
+    @Test
+    @DisplayName("pick 메서드를 실행하면 카드덱 스택에서 맨 뒤의 카드가 제거된 후 출력된다")
+    void pickCard() {
+
+        CardDeck cardDeck = CardDeck.generateCardDeck();
+
+        Card canPoppedCard = cardDeck.getCardDeck().peek();
+        Card poppedCard = cardDeck.pickCard();
+        System.out.println(canPoppedCard);
+        System.out.println(poppedCard);
+
+        assertAll(
+                () -> assertThat(poppedCard).isSameAs(canPoppedCard),
+                () -> assertThat(cardDeck.getCardDeck()).doesNotContain(poppedCard)
+        );
+    }
+
 //
 //    @Test
 //    @DisplayName("카드 덱에서 카드를 뽑는다")
