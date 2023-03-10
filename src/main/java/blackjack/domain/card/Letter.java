66a6e5d907528bce2d00530789460bad95a95a58
70825 <hello70825@gmail.com>
2023-03-10 17:19:24 +0900

    refactor: 정적 팩토리 메서드 추가 Letter, Shape

--- src/main/java/blackjack/domain/card/Letter.java
+++ src/main/java/blackjack/domain/card/Letter.java
@@ -1,5 +1,7 @@
 package blackjack.domain.card;
 
+import java.util.Arrays;
+
 public enum Letter {
 
     ACE("A", 11),
@@ -17,12 +19,20 @@ public enum Letter {
     KING("K", 10);
 
 
+    private static final String INVALID_NAME_ERROR_MESSAGE = "해당 이름을 가지는 글자가 존재하지 않습니다.";
     private final String name;
     private final int value;
 
     Letter(final String name, final int value) {
         this.name = name;
         this.value = value;
+    }
+
+    public static Letter from(final String name) {
+        return Arrays.stream(Letter.values())
+                .filter(letter -> letter.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_NAME_ERROR_MESSAGE));
     }
 
     public boolean isAce() {
