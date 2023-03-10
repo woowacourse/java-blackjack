716a4f26d973590680540175c7a013c8ca5d305d
yeonkkk <tjddus0302@gmail.com>
2023-03-10 16:10:14 +0900

    refactor: 필드 및 반환 타입 변경

--- src/main/java/domain/Denomination.java
+++ src/main/java/domain/Denomination.java
@@ -18,16 +18,16 @@ public enum Denomination {
     JACK("J", 10);
 
     private final String letter;
-    private final int score;
+    private final Score score;
 
     Denomination(String letter, int score) {
         this.letter = letter;
-        this.score = score;
+        this.score = new Score(score);
     }
 
     public static Denomination of(String letter) {
         return Arrays.stream(values())
-                .filter(denomination1 -> denomination1.letter().equals(letter))
+                .filter(denomination -> denomination.letter.equals(letter))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 카드 글자입니다"));
     }
@@ -41,7 +41,7 @@ public enum Denomination {
     }
 
     public Score score() {
-        return new Score(this.score);
+        return score;
     }
 
 }
