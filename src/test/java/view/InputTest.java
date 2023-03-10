59d84318114ab09a512e4c7c9d125902a8692b6e
jjongwa <troas96@naver.com>
2023-03-11 02:37:00 +0900

    refactor: 테스트 클래스 이름 변경

--- src/test/java/view/InputTest.java
+++ src/test/java/view/InputTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class InputValidatorTest {
+public class InputTest {
     @Test
     @DisplayName("빈 값이 들어오면 예외처리한다.")
     void shouldThrowInputIsEmpty() {
