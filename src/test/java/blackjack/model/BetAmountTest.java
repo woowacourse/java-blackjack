781324c95031703edc0b7cefb9da3c8d46da8bda
hectick <chyeon1223@cau.ac.kr>
2023-03-10 15:22:43 +0900

    test: 베팅 금액이 양의 정수인지 테스트 작성

---
+++ src/test/java/blackjack/model/BetAmountTest.java
@@ -0,0 +1,19 @@
+package blackjack.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class BetAmountTest {
+
+    @ParameterizedTest
+    @DisplayName("베팅 금액이 양의 정수가 아니면 예외처리한다.")
+    @ValueSource(ints = {0, -10, -9999999})
+    void validate_not_positive_amount(int input) {
+        assertThatThrownBy(() -> new BetAmount(input)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유효한 금액이 아닙니다. 베팅 금액은 양의 정수여야 합니다.");
+    }
+
+}
