48864c1215708a153300f5de0c60bd8a3adc63f8
songusika <sws981202@gmail.com>
2023-03-10 16:29:33 +0900

    feat: 블랙잭 게임 점수 구현
    Co-authored-by: choi-jjunho <junho5336@gmail.com>

---
+++ src/main/java/blackjack/domain/game/Score.java
@@ -0,0 +1,53 @@
+package blackjack.domain.game;
+
+import java.util.Objects;
+
+public final class Score {
+
+    private static final Score step = new Score(10);
+    private static final Score max = new Score(21);
+
+    private final int score;
+
+    public Score(final int score) {
+        this.score = score;
+    }
+
+    public Score minus(final Score score) {
+        return new Score(this.score - score.score);
+    }
+
+    public boolean isOverThen(final Score compareScore) {
+        return this.score > compareScore.score;
+    }
+
+    public boolean isBust() {
+        return this.isOverThen(max);
+    }
+
+    public Score minusIfBust() {
+        if (isBust()) {
+            return this.minus(step);
+        }
+
+        return this;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Score score1 = (Score) o;
+        return score == score1.score;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score);
+    }
+
+}
--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,15 +1,12 @@
 package blackjack.view;
 
-import blackjack.domain.user.name.UserName;
+import blackjack.domain.user.User;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
-
-    public InputView() {
-    }
 
     public List<String> readParticipantName() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
@@ -21,9 +18,9 @@ public class InputView {
         return List.of(input.split(","));
     }
 
-    public boolean readCommand(final UserName userName) {
+    public boolean readCommand(final User user) {
         System.out.println(
-                userName.getName() + "는 한장의 카드를 더 받겠습니까?"
+                user.getName() + "는 한장의 카드를 더 받겠습니까?"
                         + "(예는 " + Command.YES.value()
                         + "아니오는 " + Command.NO.value() + ")");
 
