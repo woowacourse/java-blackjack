b2f42d6ae9ea7f15aad29d8be2f63b1a0b968860
yujamint <woawoa0513@gmail.com>
2023-03-10 22:28:30 +0900

    test: Bet 클래스 구현

---
+++ src/main/java/blackjackgame/domain/user/Bet.java
@@ -0,0 +1,35 @@
+package blackjackgame.domain.user;
+
+public class Bet {
+    private static final int PROFIT_INITIAL_VALUE = 0;
+
+    private final int amount;
+    private int profit = PROFIT_INITIAL_VALUE;
+
+    Bet(int amount) {
+        validateBetAmount(amount);
+        this.amount = amount;
+    }
+
+    private void validateBetAmount(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("베팅 금액은 1원 이상입니다.");
+        }
+    }
+
+    public void win() {
+        this.profit = amount;
+    }
+
+    public void draw() {
+        this.profit = 0;
+    }
+
+    public void lose() {
+        this.profit = -amount;
+    }
+
+    public int profit() {
+        return this.profit;
+    }
+}
