abac190f8e37857d5f8ffecea1ed866b6a098a84
kdkdhoho <hkim4410@naver.com>
2023-03-10 10:56:10 +0900

    feat(Money): Money 클래스 구현

---
+++ src/main/java/blackjack/domain/Money.java
@@ -0,0 +1,14 @@
+package blackjack.domain;
+
+public class Money {
+
+    private final int money;
+
+    public Money(final int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}
