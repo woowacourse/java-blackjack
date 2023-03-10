6dcdbc560a8e08bb4da9ab2a5990fdd4ba2fade8
hanueleee <ezzeezz@naver.com>
2023-03-10 13:45:34 +0900

    feat(GameResult): 도전자의 최종 승패 결과에 따라 수익을 계산하는 기능 구현

---
+++ src/main/java/blackjack/domain/result/GameResult.java
@@ -0,0 +1,36 @@
+package blackjack.domain.result;
+
+import blackjack.domain.player.Challenger;
+import blackjack.domain.player.Dealer;
+import blackjack.domain.player.Player;
+import blackjack.domain.player.Players;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GameResult {
+
+    private final Map<Player, Integer> results;
+
+    private GameResult(Map<Player, Integer> results) {
+        this.results = results;
+    }
+
+    public static GameResult from(Players players, List<Integer> betAmounts) {
+        Map<Player, Integer> results = new LinkedHashMap<>();
+        Dealer dealer = players.getDealer();
+        List<Challenger> challengers = players.getChallengers();
+
+        for (int i = 0; i < challengers.size(); i++) {
+            Challenger challenger = challengers.get(i);
+            Result result = dealer.judge(challenger);
+            int revenue = (int) Math.round(betAmounts.get(i) * result.getRateOfReturn());
+            results.put(challenger, revenue);
+        }
+        return new GameResult(results);
+    }
+
+    public Integer getChallengerRevenue(Player player) {
+        return results.get(player);
+    }
+}
