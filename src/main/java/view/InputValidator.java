8558f2a373f982f0ac2f15f71661691aad1498cd
sh111-coder <ohk9134@naver.com>
2023-03-10 15:44:37 +0900

    feat(InputValidator): 배팅 금액 검증 기능 구현

--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -1,9 +1,14 @@
 package view;
 
+import util.Constants;
+
 public class InputValidator {
 
-    public static final String INPUT_EMPTY_ERROR_MESSAGE = "[ERROR] 입력값이 존재하지 않습니다.";
-    public static final String INPUT_DOES_NOT_Y_OR_N = "[ERROR] 추가 카드 요청 여부는 y 또는 n을 입력해 주세요.";
+    private static final String INPUT_EMPTY_ERROR_MESSAGE = Constants.ERROR_PREFIX + "입력값이 존재하지 않습니다.";
+    private static final String INPUT_DOES_NOT_Y_OR_N = Constants.ERROR_PREFIX + "추가 카드 요청 여부는 y 또는 n을 입력해 주세요.";
+
+    private static final String INPUT_BETTING_MONEY_NOT_NUMERIC_ERROR_MESSAGE = Constants.ERROR_PREFIX + "배팅 금액은 숫자만 가능합니다.";
+    private static final String NUMERIC_REGEX = "^[0-9]*$";
 
     public static void validateBlank(String input) {
         if (input.isEmpty()) {
@@ -14,6 +19,12 @@ public class InputValidator {
     public static void validateReceiveOrNot(String receiveOrNot) {
         if (!(receiveOrNot.equals("y") || receiveOrNot.equals("n"))) {
             throw new IllegalArgumentException(INPUT_DOES_NOT_Y_OR_N);
+        }
+    }
+
+    public static void validateBettingMoney(String bettingMoney) {
+        if (!bettingMoney.matches(NUMERIC_REGEX)) {
+            throw new IllegalArgumentException(INPUT_BETTING_MONEY_NOT_NUMERIC_ERROR_MESSAGE);
         }
     }
 }
