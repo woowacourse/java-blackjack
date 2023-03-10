cef9df9e80dc8dc012a7c5576028daa43ec957e4
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-03-10 14:41:46 +0900

    refactor: OutputView 내 상수 MSG -> MESSAGE로 변경

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -10,13 +10,13 @@ import java.util.Map;
 
 public class OutputView {
 
-    private static final String INIT_END_MSG = "에게 2장을 나누었습니다.";
-    private static final String INIT_START_MSG = "딜러와 ";
-    private static final String CARD_MSG = "카드: ";
-    private static final String DEALER_MSG = "딜러: ";
-    private static final String DEALER_HIT_MSG = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
-    private static final String RESULT_MSG = " - 결과: ";
-    private static final String FINAL_START_MSG = "## 최종 승패";
+    private static final String INIT_END_MESSAGE = "에게 2장을 나누었습니다.";
+    private static final String INIT_START_MESSAGE = "딜러와 ";
+    private static final String CARD_MESSAGE = "카드: ";
+    private static final String DEALER_MESSAGE = "딜러: ";
+    private static final String DEALER_HIT_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    private static final String RESULT_MESSAGE = " - 결과: ";
+    private static final String FINAL_START_MESSAGE = "## 최종 승패";
     private static final String RESULT_DELIMITER = ": ";
     private static final String CARD_DELIMITER = ", ";
     private static final String BLANK = " ";
@@ -26,25 +26,25 @@ public class OutputView {
     public void printStartMsg(List<String> names) {
         System.out.println();
         int lastIndex = names.size() - 1;
-        System.out.print(INIT_START_MSG);
+        System.out.print(INIT_START_MESSAGE);
         for (int i = 0; i < lastIndex; i++) {
             System.out.print(names.get(i) + CARD_DELIMITER);
         }
-        System.out.println(names.get(lastIndex) + INIT_END_MSG);
+        System.out.println(names.get(lastIndex) + INIT_END_MESSAGE);
     }
 
     public void printPlayerCards(Player player, String end) {
         String name = player.showName();
         List<Card> cards = player.showCards();
         int lastIndex = cards.size() - 1;
-        System.out.print(name + CARD_MSG);
+        System.out.print(name + CARD_MESSAGE);
         printCards(cards, end, lastIndex);
     }
 
     public void printDealerCards(Dealer dealer, String end) {
         List<Card> cards = dealer.showAllCards();
         int lastIndex = cards.size() - 1;
-        System.out.print(DEALER_MSG);
+        System.out.print(DEALER_MESSAGE);
         printCards(cards, end, lastIndex);
     }
 
@@ -59,25 +59,25 @@ public class OutputView {
 
     public void printDealerHit() {
         System.out.println();
-        System.out.println(DEALER_HIT_MSG);
+        System.out.println(DEALER_HIT_MESSAGE);
     }
 
     public void printDealerResult(Dealer dealer) {
         int score = dealer.calculateScore().getScore();
         System.out.println();
         printDealerCards(dealer, EMPTY);
-        System.out.println(RESULT_MSG + score);
+        System.out.println(RESULT_MESSAGE + score);
     }
 
     public void printPlayerResult(Player player) {
         printPlayerCards(player, EMPTY);
         int score = player.calculateScore().getScore();
-        System.out.println(RESULT_MSG + score);
+        System.out.println(RESULT_MESSAGE + score);
     }
 
     public void printEndMsg() {
         System.out.println();
-        System.out.println(FINAL_START_MSG);
+        System.out.println(FINAL_START_MESSAGE);
     }
 
     public void printPlayerWinningResult(Map<Player, Result> playerResult) {
@@ -87,7 +87,7 @@ public class OutputView {
     }
 
     public void printDealerWinningResult(Map<Result, Integer> dealerResult) {
-        StringBuilder dealerResultMsg = new StringBuilder(DEALER_MSG);
+        StringBuilder dealerResultMsg = new StringBuilder(DEALER_MESSAGE);
         for (Result result : dealerResult.keySet()) {
             if (dealerResult.get(result) != ZERO) {
                 dealerResultMsg.append(dealerResult.get(result)).append(result.getResult()).append(BLANK);
