dcc992f3df39fa654700817e0fff600269d60da1
hyunseo <goatsfish@gmail.com>
2023-03-10 14:05:55 +0900

    refactor: 블랙잭 참여자 생성자 수정

--- src/main/java/blackjack/domain/game/BlackJackParticipants.java
+++ src/main/java/blackjack/domain/game/BlackJackParticipants.java
@@ -8,36 +8,19 @@ import blackjack.domain.participant.Player;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class BlackJackParticipants {
     private final Dealer dealer;
     private final BettingPlayers bettingPlayers;
 
     public BlackJackParticipants(final Deck deck, final List<String> nameValues, final List<Integer> moneyValues) {
-        this.dealer = new Dealer(initCards(deck));
-        this.bettingPlayers = new BettingPlayers(createPlayers(deck, nameValues), createMoneys(moneyValues));
+        this.dealer = new Dealer(deck);
+        this.bettingPlayers = new BettingPlayers(deck, nameValues, moneyValues);
     }
 
     public BettingResults createBettingResults() {
         final Map<Participant, Money> bettingResults = bettingPlayers.findBettingResultsBy(dealer);
         return new BettingResults(bettingResults, dealer);
-    }
-
-    private List<Player> createPlayers(final Deck deck, final List<String> nameValues) {
-        return nameValues.stream()
-                .map(nameValue -> new Player(initCards(deck), nameValue))
-                .collect(Collectors.toList());
-    }
-
-    private ParticipantCards initCards(final Deck deck) {
-        return new ParticipantCards(deck);
-    }
-
-    private List<Money> createMoneys(final List<Integer> moneyValues) {
-        return moneyValues.stream()
-                .map(Money::new)
-                .collect(Collectors.toList());
     }
 
     public Dealer getDealer() {
