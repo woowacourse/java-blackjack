183f0ed2e9e00f37e4324bca3b6928e57f6cdee2
sh111-coder <ohk9134@naver.com>
2023-03-10 15:44:07 +0900

    test(InputValiddatorTest): 배팅 금액이 숫자가 아니면 예외 처리 테스트

--- src/test/java/view/InputValidatorTest.java
+++ src/test/java/view/InputValidatorTest.java
@@ -3,6 +3,9 @@ package view;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import util.Constants;
 
 public class InputValidatorTest {
     @Test
@@ -10,6 +13,16 @@ public class InputValidatorTest {
     void shouldThrowInputIsEmpty() {
         Assertions.assertThatThrownBy(() -> InputValidator.validateBlank(""))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(InputValidator.INPUT_EMPTY_ERROR_MESSAGE);
+                .hasMessage(Constants.ERROR_PREFIX + "입력값이 존재하지 않습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"가", "가1", "A", "A1", "!"})
+    @DisplayName("배팅 금액이 숫자가 아니면 예외 처리한다.")
+    void inputBettingMoneyNotNumeric(String bettingMoney) {
+        Assertions.assertThatThrownBy(() -> InputValidator.validateBettingMoney(bettingMoney))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Constants.ERROR_PREFIX + "배팅 금액은 숫자만 가능합니다.");
+
     }
 }
