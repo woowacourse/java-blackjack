195d4d05c3013bfa0497b8b99965e8a97678fbe4
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-03-10 15:04:28 +0900

    feat: Betting 클래스 내 입력 값을 검증하는 기능 구현

--- src/main/java/blackjack/domain/gameplayer/Betting.java
+++ src/main/java/blackjack/domain/gameplayer/Betting.java
@@ -2,9 +2,24 @@ package blackjack.domain.gameplayer;
 
 public class Betting {
 
+    private static final String INVALID_RANGE_MESSAGE = "배팅 금액은 0원부터 30만원까지 입력받을 수 있습니다.";
+    private static final int MAX_BOUND = 300000;
+    private static final int MIN_BOUND = 0;
+
     private final int betting;
 
     public Betting(int betting) {
+        validate(betting);
         this.betting = betting;
+    }
+
+    private void validate(int betting) {
+        if (isInvalidRange(betting)) {
+            throw new IllegalArgumentException(INVALID_RANGE_MESSAGE);
+        }
+    }
+
+    private boolean isInvalidRange(int betting) {
+        return betting < MIN_BOUND || betting > MAX_BOUND;
     }
 }
--- src/test/java/blackjack/domain/gameplayer/BettingTest.java
+++ src/test/java/blackjack/domain/gameplayer/BettingTest.java
@@ -1,8 +1,10 @@
 package blackjack.domain.gameplayer;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,5 +14,21 @@ class BettingTest {
     @Test
     void Should_Success_When_Create() {
         assertDoesNotThrow(() -> new Betting(1000));
+    }
+
+    @DisplayName("배팅 금액은 0 ~ 30만원까지 입력받을 수 있다._성공")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 300000})
+    void Shuold_Success_When_InputBettingBetween0And300000(int betting) {
+        assertDoesNotThrow(() -> new Betting(betting));
+    }
+
+    @DisplayName("배팅 금액은 0 ~ 30만원까지 입력받을 수 있다._실패")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 300001})
+    void Shuold_ThrowException_When_InputBettingNotBetween0And300000(int betting) {
+        Assertions.assertThatThrownBy(() -> new Betting(betting))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("배팅 금액은 0원부터 30만원까지 입력받을 수 있습니다.");
     }
 }
