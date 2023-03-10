dcb1e4f7438aba8fafaff27d597409408e9b3af9
jaehee329 <jjhs9803@gmail.com>
2023-03-10 19:21:47 +0900

    feat(Results): 결과를 별도로 계산하는 클래스 구현

---
+++ src/main/java/domain/result/Results.java
@@ -0,0 +1,33 @@
+package domain.result;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import domain.Dealer;
+import domain.Player;
+
+public class Results {
+
+	private final List<PlayerResult> playerResults;
+
+	private Results(final List<PlayerResult> playerResults) {
+		this.playerResults = playerResults;
+	}
+
+	public static Results of(final Dealer dealer, final List<Player> players) {
+		List<PlayerResult> playerResult = new ArrayList<>();
+		for (Player player : players) {
+			playerResult.add(PlayerResult.decide(player, dealer));
+		}
+		return new Results(playerResult);
+	}
+
+	public DealerResult getDealerResult() {
+		return DealerResult.from(playerResults);
+	}
+
+	public List<PlayerResult> getPlayerResults() {
+		return Collections.unmodifiableList(playerResults);
+	}
+}
