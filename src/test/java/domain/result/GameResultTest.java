2cb887bf19955b4e30d8e314837a19abe4deeff1
JunDonghyuk <ehdgur4814@naver.com>
2023-03-10 12:00:02 +0900

    refactor: 게임결과 테스트 수정

--- src/test/java/domain/result/GameResultTest.java
+++ src/test/java/domain/result/GameResultTest.java
@@ -1,6 +1,9 @@
 package domain.result;
 
+import domain.deck.Card;
 import domain.deck.Deck;
+import domain.deck.Rank;
+import domain.deck.Suit;
 import domain.player.Dealer;
 import domain.player.Player;
 import domain.player.Players;
@@ -8,49 +11,91 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameResultTest {
 
     GameResult gameResult;
+    Players players;
+    Dealer dealer;
 
     @BeforeEach
     void setup() {
-        Players players = new Players(List.of("hardy", "ddoring"));
-        Dealer dealer = new Dealer();
         Deck deck = new Deck();
-
+        gameResult = new GameResult();
+        players = new Players(List.of("hardy", "ddoring", "crong", "doodoom"));
+        dealer = new Dealer();
         // 2장씩 뽑는다.
-        dealer.hit(deck.popCard());
-        dealer.hit(deck.popCard());
-        players.hitTwice(deck);
 
-        /*
-            딜러: K, Q
-            hardy: J 10
-            ddoring: 9 8
-         */
-        gameResult = new GameResult(dealer, players);
-    }
+        // 19
+        dealer.hit(Card.of(Suit.HEART, Rank.NINE));
+        dealer.hit(Card.of(Suit.DIAMOND, Rank.TEN));
 
-    @Test
-    void makeDealerResultTest() {
-        EnumMap<Outcome, Integer> dealerResult = gameResult.makeDealerResult();
+        // 21 블랙잭으로 이김
+        Player hardy = players.findPlayer("hardy");
+        hardy.hit(Card.of(Suit.DIAMOND, Rank.ACE));
+        hardy.hit(Card.of(Suit.DIAMOND, Rank.TEN));
 
-        assertEquals(1, dealerResult.get(Outcome.WIN));
-        assertEquals(1, dealerResult.get(Outcome.DRAW));
-        assertEquals(0, dealerResult.get(Outcome.LOSE));
+        // 20 그냥 이김
+        Player ddoring = players.findPlayer("ddoring");
+        ddoring.hit(Card.of(Suit.DIAMOND, Rank.KING));
+        ddoring.hit(Card.of(Suit.DIAMOND, Rank.KING));
+
+        // 19 무승부
+        Player crong = players.findPlayer("crong");
+        crong.hit(Card.of(Suit.DIAMOND, Rank.NINE));
+        crong.hit(Card.of(Suit.DIAMOND, Rank.KING));
+
+        // 18 짐
+        Player doodoom = players.findPlayer("doodoom");
+        doodoom.hit(Card.of(Suit.DIAMOND, Rank.EIGHT));
+        doodoom.hit(Card.of(Suit.DIAMOND, Rank.KING));
     }
 
     @Test
     void makePlayerResultsTest() {
-        Map<String, Outcome> playerResults = gameResult.makePlayerResults();
+        Map<String, Outcome> playerResults = gameResult.makePlayerResults(dealer, players);
 
-        assertEquals(Outcome.DRAW, playerResults.get("hardy"));
-        assertEquals(Outcome.LOSE, playerResults.get("ddoring"));
+        assertEquals(Outcome.WIN, playerResults.get("hardy"));
+        assertEquals(Outcome.DRAW, playerResults.get("crong"));
+        assertEquals(Outcome.LOSE, playerResults.get("doodoom"));
+    }
+
+    @Test
+    void makePlayerBetMoneyResultsTest() {
+        players.getPlayers()
+                .forEach(player -> player.initBet(10000));
+        Map<String, Integer> playerBetMoneyResults = gameResult.makePlayerBetMoneyResults(dealer, players);
+
+        Integer hardyMoney = playerBetMoneyResults.get("hardy");
+        Integer ddoringMoney = playerBetMoneyResults.get("ddoring");
+        Integer crongMoney = playerBetMoneyResults.get("crong");
+        Integer doodoomMoney = playerBetMoneyResults.get("doodoom");
+
+        // 블랙잭으로 이김
+        assertEquals(15000, hardyMoney);
+
+        // 그냥 이김
+        assertEquals(10000, ddoringMoney);
+
+        // 무승부
+        assertEquals(0, crongMoney);
+
+        // 짐
+        assertEquals(-10000, doodoomMoney);
+    }
+
+    @Test
+    void calculateDealerBetMoneyResultTest() {
+        int expectedDealerProfit = -15000;
+        players.getPlayers()
+                .forEach(player -> player.initBet(10000));
+        Map<String, Integer> playerBetMoneyResults = gameResult.makePlayerBetMoneyResults(dealer, players);
+        int dealerProfit = gameResult.calculateDealerBetMoneyResult(playerBetMoneyResults);
+
+        assertEquals(expectedDealerProfit, dealerProfit);
     }
 }
