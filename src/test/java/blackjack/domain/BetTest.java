7b10a5927c860966e83c07c24514504b3dde8bca
seokjin8678 <seokjin8678@gmail.com>
2023-03-10 20:48:46 +0900

    test: Bet 테스트 코드 수정, 추가

--- src/test/java/blackjack/domain/BetTest.java
+++ src/test/java/blackjack/domain/BetTest.java
@@ -1,10 +1,11 @@
 package blackjack.domain;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BetTest {
@@ -13,9 +14,12 @@ class BetTest {
     @ValueSource(ints = {1, 1000, 1_000_000, 99_999_999})
     @DisplayName("배팅금이 정상적으로 생성되어야 한다.")
     void create_success(int input) {
+        // given
+        Bet bet = Bet.of(input);
+
         // expect
-        assertThatCode(() -> Bet.of(input))
-                .doesNotThrowAnyException();
+        assertThat(bet.getBet())
+                .isEqualTo(input);
     }
 
     @ParameterizedTest
@@ -26,5 +30,20 @@ class BetTest {
         assertThatIllegalArgumentException().isThrownBy(() -> {
             Bet.of(input);
         }).withMessage("[ERROR] 배팅금은 0보다 크거나 100,000,000보다 작아야 합니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"WIN,1000", "LOSE,-1000", "DRAW,0", "BLACKJACK,1500"})
+    @DisplayName("게임의 결과에 따라 배팅금이 반환되어야 한다.")
+    void calculateResult_success(GameResult result, int expect) {
+        // given
+        Bet bet = Bet.of(1000);
+
+        // when
+        Bet resultBet = bet.calculateResult(result);
+
+        // then
+        assertThat(resultBet.getBet())
+                .isEqualTo(expect);
     }
 }
