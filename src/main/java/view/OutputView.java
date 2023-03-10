d5a964b02ecc75d9e5bca1df04e18e6d6e6c661e
sh111-coder <ohk9134@naver.com>
2023-03-10 19:16:02 +0900

    feat(OutputView): 배팅 금액 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     public static final String INPUT_PLAYER_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     public static final String INPUT_RECEIVE_YES_OR_NOT_MESSAGE = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String BETTING_MONEY_MESSAGE = "의 배팅 금액은?";
     public static final String DEALER_RECEIVED_MESSAGE = "\n딜러는 16이하라 한장의 카드를 더 받았습니다.";
 
     public static void printInputPlayerNameMessage() {
@@ -12,6 +13,10 @@ public class OutputView {
 
     public static void printInputReceiveYesOrNotMessage(String playerName) {
         printMessage(playerName + INPUT_RECEIVE_YES_OR_NOT_MESSAGE);
+    }
+
+    public static void printInputBettingMoney(String playerName) {
+        printMessage(playerName + BETTING_MONEY_MESSAGE);
     }
 
     public static void printDealerReceivedMessage() {
