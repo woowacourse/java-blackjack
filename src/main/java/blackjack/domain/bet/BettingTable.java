179a6e79b10963a9f72dd01bad891ba0af756496
pilyang <30036534+pilyang@users.noreply.github.com>
2023-03-10 17:02:53 +0900

    refactor: 배팅 금액 validation 위치 변경
    - 배팅 금액 validation 위치 BettingTable로 이동
    - 베팅금액이 반환시 배팅금액(기록)이 없으면 0원 반환

--- src/main/java/blackjack/domain/bet/BettingTable.java
+++ src/main/java/blackjack/domain/bet/BettingTable.java
@@ -5,6 +5,12 @@ import java.util.Map;
 
 public class BettingTable {
 
+    private static final int UNIT_AMOUNT = 1_000;
+    private static final int MIN_AMOUNT = 1_000;
+    private static final int MAX_AMOUNT = 100_000;
+    static final String OUT_OF_RANGE_EXCEPTION_MESSAGE = MIN_AMOUNT + "이상 " + MAX_AMOUNT + "이하여야 합니다.";
+    static final String UNIT_AMOUNT_EXCEPTION_TEST = UNIT_AMOUNT + "원 단위로 입력해야 합니다.";
+
     private final Map<String, Money> table;
 
     public BettingTable() {
@@ -12,10 +18,24 @@ public class BettingTable {
     }
 
     public void bet(final String name, final Money money) {
+        validateRange(money);
+        validateUnitAmount(money);
         table.put(name, money);
     }
 
+    private void validateUnitAmount(final Money money) {
+        if (money.getAmount() % UNIT_AMOUNT != 0) {
+            throw new IllegalArgumentException(UNIT_AMOUNT_EXCEPTION_TEST);
+        }
+    }
+
+    private void validateRange(final Money money) {
+        if (money.getAmount() < MIN_AMOUNT || money.getAmount() > MAX_AMOUNT) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_EXCEPTION_MESSAGE);
+        }
+    }
+
     public Money get(final String name) {
-        return table.get(name);
+        return table.getOrDefault(name, new Money(0));
     }
 }
--- src/test/java/blackjack/domain/bet/MoneyTest.java
+++ src/test/java/blackjack/domain/bet/MoneyTest.java
@@ -2,10 +2,7 @@ package blackjack.domain.bet;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class MoneyTest {
@@ -14,23 +11,6 @@ class MoneyTest {
     @DisplayName("금액이 정상 생성된다")
     void moneyInitializeTest() {
         assertDoesNotThrow(() -> new Money(5000));
-    }
-
-    @ParameterizedTest(name = "입력금액 : {0}")
-    @DisplayName("1000원 이상 100,000원 이하의 범위에서 벗어나면 예외를 발생시킨다")
-    @ValueSource(ints = {900, 101_000})
-    void moneyRangeExceptionTest(int amount) {
-        assertThatThrownBy(() -> new Money(amount))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(Money.OUT_OF_RANGE_EXCEPTION_MESSAGE);
-    }
-
-    @Test
-    @DisplayName("1000원단위 외의 입력에서 예외를 발생한다")
-    void unitAmountExceptionTest() {
-        assertThatThrownBy(() -> new Money(5100))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(Money.UNIT_AMOUNT_EXCEPTION_TEST);
     }
 
 }
