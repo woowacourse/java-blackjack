22ad462f016da200d08b55fdeb2c7cffffe71fe9
kdkdhoho <hkim4410@naver.com>
2023-03-10 14:25:05 +0900

    feat(Player): equals & hashCode 재정의

--- src/main/java/blackjack/domain/participant/Player.java
+++ src/main/java/blackjack/domain/participant/Player.java
@@ -1,14 +1,30 @@
 package blackjack.domain.participant;
 
-public class Player {
+import java.util.Objects;
+
+public class Player extends Participant {
 
     private final String name;
 
     public Player(final String name) {
+        super();
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
