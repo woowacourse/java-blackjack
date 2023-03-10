e7ea3fe7de672ec81a367373e7b2c368ff3f913d
ChoiYoungHoon <dudgns2865@naver.com>
2023-03-10 11:48:17 +0900

    feat: BlackJackGame에 플레이어들 손익 계산 기능 구현

--- src/main/java/domain/service/BlackJackGame.java
+++ src/main/java/domain/service/BlackJackGame.java
@@ -4,25 +4,32 @@ import domain.model.Dealer;
 import domain.model.Participant;
 import domain.model.Player;
 import domain.model.Players;
+import domain.model.Profit;
 import domain.vo.Result;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BlackJackGame {
 
     private final CardDistributor cardDistributor;
     private final BlackJackResultMaker blackJackResultMaker;
+    private final ProfitCalculator profitCalculator;
 
-    public BlackJackGame(final CardDistributor cardDistributor, final BlackJackResultMaker blackJackResultMaker) {
+    public BlackJackGame(final CardDistributor cardDistributor, final BlackJackResultMaker blackJackResultMaker,
+        final ProfitCalculator profitCalculator) {
         this.cardDistributor = cardDistributor;
         this.blackJackResultMaker = blackJackResultMaker;
+        this.profitCalculator = profitCalculator;
     }
 
     public void giveCard(final Participant participant) {
         cardDistributor.giveCard(participant);
     }
 
-    public void giveInitCards(final Participant participant, final Players players) {
-        cardDistributor.giveInitCards(participant);
+    public void giveInitCards(final Dealer dealer, final Players players) {
+        cardDistributor.giveInitCards(dealer);
         cardDistributor.giveInitCards(players);
     }
 
@@ -32,5 +39,11 @@ public class BlackJackGame {
 
     public Result makeDealerResult(final Dealer dealer, final Players players) {
         return blackJackResultMaker.makeDealerResult(dealer, players);
+    }
+
+    public List<Profit> calculatePlayersProfit(final Players players, final Dealer dealer) {
+        return IntStream.range(0, players.count())
+            .mapToObj(index -> profitCalculator.calculatePlayerProfit(players.get(index), dealer))
+            .collect(Collectors.toList());
     }
 }
