0e9f5f26d4051ac77deaba43cf3163931cfff8ff
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-03-10 19:23:47 +0900

    feat: Betting 금액을 변경할 수 있는 기능 추가

--- src/main/java/blackjack/domain/gameplayer/Betting.java
+++ src/main/java/blackjack/domain/gameplayer/Betting.java
@@ -3,6 +3,7 @@ package blackjack.domain.gameplayer;
 public class Betting {
 
     private static final String INVALID_RANGE_MESSAGE = "배팅 금액은 0원부터 30만원까지 입력받을 수 있습니다.";
+    private static final String INVALID_CHANGE_BETTING = "배팅 금액을 변경할 수 없습니다.";
     private static final int MAX_BOUND = 300000;
     private static final int MIN_BOUND = 1;
 
@@ -27,5 +28,16 @@ public class Betting {
 
     private static boolean isInvalidRange(int betting) {
         return betting < MIN_BOUND || betting > MAX_BOUND;
+    }
+
+    public Betting changeBetting(int betting) {
+        if (!isDefaultBetting()) {
+            throw new IllegalArgumentException(INVALID_CHANGE_BETTING);
+        }
+        return new Betting(betting);
+    }
+
+    private boolean isDefaultBetting() {
+        return this.equals(defaultBetting);
     }
 }
--- src/test/java/blackjack/domain/gameplayer/BettingTest.java
+++ src/test/java/blackjack/domain/gameplayer/BettingTest.java
@@ -1,11 +1,11 @@
 package blackjack.domain.gameplayer;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class BettingTest {
@@ -19,16 +19,30 @@ class BettingTest {
     @DisplayName("배팅 금액은 1 ~ 30만원까지 입력받을 수 있다._성공")
     @ParameterizedTest
     @ValueSource(ints = {1, 300000})
-    void Shuold_Success_When_InputBettingBetween0And300000(int betting) {
+    void Should_Success_When_InputBettingBetween0And300000(int betting) {
         assertDoesNotThrow(() -> Betting.of(betting));
     }
 
     @DisplayName("배팅 금액은 1 ~ 30만원까지 입력받을 수 있다._실패")
     @ParameterizedTest
     @ValueSource(ints = {0, 300001})
-    void Shuold_ThrowException_When_InputBettingNotBetween0And300000(int betting) {
-        Assertions.assertThatThrownBy(() -> Betting.of(betting))
+    void Should_ThrowException_When_InputBettingNotBetween0And300000(int betting) {
+        assertThatThrownBy(() -> Betting.of(betting))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("배팅 금액은 0원부터 30만원까지 입력받을 수 있습니다.");
+    }
+
+    @DisplayName("배팅 금액을 이미 설정한 경우에는 금액을 바꿀 수 없다.")
+    @Test
+    void Should_ThrowException_When_ChangeBettingAfterBet() {
+        assertThatThrownBy(() -> Betting.of(2000).changeBetting(1000))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("배팅 금액을 변경할 수 없습니다.");
+    }
+
+    @DisplayName("배팅 금액을 설정하지 않은 경우에는 금액을 바꿀 수 있다.")
+    @Test
+    void Should_Success_When_ChangeBettingBeforeBet() {
+        assertDoesNotThrow(() -> Betting.defaultBetting.changeBetting(1000));
     }
 }
