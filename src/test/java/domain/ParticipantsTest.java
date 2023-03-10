aea1513a59f56844a899955d2c12f59da4aa2a8e
Woojin Lee <dhkep03@gmail.com>
2023-03-10 17:41:04 +0900

    test(Participants): 결과 계산하는 테스트 케이스 수정

--- src/test/java/domain/ParticipantsTest.java
+++ src/test/java/domain/ParticipantsTest.java
@@ -38,94 +38,22 @@ class ParticipantsTest {
         players.forEach(player -> assertThat(player.getCards()).hasSize(2));
     }
 
-//    @DisplayName("모든 플레이어의 결과를 반환한다.")
-//    @Test
-//    void 결과_계산() {
-//        Map<String, Integer> playerBettingAmountTable = new LinkedHashMap<>() {{
-//            put("Player1", 1_000);
-//            put("Player2", 2_000);
-//        }};
-//        Participants participants = Participants.from(playerBettingAmountTable);
-//        participants.drawInitialCardsEachParticipant(ShuffledDeck.createByCount(6));
-//
-//        Map<Player, Result> results = participants.calculateAllResults();
-//        List<Player> players = participants.getPlayers();
-//
-//        players.forEach(player -> assertThat(results.get(player)).isInstanceOf(Result.class));
-//    }
-//
-//    @DisplayName("플레이어의 점수가 딜러보다 높으면 플레이어가 승리하는 결과를 반환한다.")
-//    @Test
-//    void 플레이어_승리() {
-//        // given
-//        Player player = Player.of("Player", 1_000);
-//        Participants participants = new Participants(new Players(List.of(player)), new Dealer());
-//        participants.drawInitialCardsEachParticipant(DeterminedDeck.from(List.of(
-//                new Card(Suit.CLOVER, Rank.FIVE), // Player1에게 주어지는 카드 2
-//                new Card(Suit.CLOVER, Rank.FIVE), // Player1에게 주어지는 카드 1
-//                new Card(Suit.CLOVER, Rank.FOUR), // 딜러에게 주어지는 카드 2
-//                new Card(Suit.CLOVER, Rank.FIVE) // 딜러에게 주어지는 카드 1
-//        )));
-//        // when
-//        Map<Player, Result> results = participants.calculateAllResults();
-//        // then
-//        assertThat(results.get(player)).isEqualTo(Result.WIN);
-//    }
-//
-//    @DisplayName("플레이어의 점수가 딜러와 같으면 무승부 결과를 반환한다.")
-//    @Test
-//    void 플레이어_무승부() {
-//        // given
-//        Player player = Player.of("Player", 1_000);
-//        Participants participants = new Participants(new Players(List.of(player)), new Dealer());
-//        participants.drawInitialCardsEachParticipant(DeterminedDeck.from(List.of(
-//                new Card(Suit.CLOVER, Rank.FIVE), // Player1에게 주어지는 카드 2
-//                new Card(Suit.CLOVER, Rank.FIVE), // Player1에게 주어지는 카드 1
-//                new Card(Suit.CLOVER, Rank.FIVE), // 딜러에게 주어지는 카드 2
-//                new Card(Suit.CLOVER, Rank.FIVE) // 딜러에게 주어지는 카드 1
-//        )));
-//        // when
-//        Map<Player, Result> results = participants.calculateAllResults();
-//        // then
-//        assertThat(results.get(player)).isEqualTo(Result.DRAW);
-//    }
-//
-//    @DisplayName("플레이어의 점수가 딜러보다 낮으면 플레이어가 패배하는 결과를 반환한다.")
-//    @Test
-//    void 플레이어_패배_점수() {
-//        // given
-//        Player player = Player.of("Player", 1_000);
-//        Participants participants = new Participants(new Players(List.of(player)), new Dealer());
-//        participants.drawInitialCardsEachParticipant(DeterminedDeck.from(List.of(
-//                new Card(Suit.CLOVER, Rank.FOUR), // Player1에게 주어지는 카드 2
-//                new Card(Suit.CLOVER, Rank.FIVE), // Player1에게 주어지는 카드 1
-//                new Card(Suit.CLOVER, Rank.FIVE), // 딜러에게 주어지는 카드 2
-//                new Card(Suit.CLOVER, Rank.FIVE) // 딜러에게 주어지는 카드 1
-//        )));
-//        // when
-//        Map<Player, Result> results = participants.calculateAllResults();
-//        // then
-//        assertThat(results.get(player)).isEqualTo(Result.LOSE);
-//    }
-//
-//    @DisplayName("플레이어의 점수가 딜러보다 낮으면 플레이어가 패배하는 결과를 반환한다.")
-//    @Test
-//    void 플레이어_패배_버스트() {
-//        // given
-//        Player player = Player.of("Player", 1_000);
-//        Participants participants = new Participants(new Players(List.of(player)), new Dealer());
-//        participants.drawInitialCardsEachParticipant(DeterminedDeck.from(List.of(
-//                new Card(Suit.CLOVER, Rank.KING), // Player1에게 주어지는 카드 2
-//                new Card(Suit.CLOVER, Rank.KING), // Player1에게 주어지는 카드 1
-//                new Card(Suit.CLOVER, Rank.FIVE), // 딜러에게 주어지는 카드 2
-//                new Card(Suit.CLOVER, Rank.FIVE) // 딜러에게 주어지는 카드 1
-//        )));
-//        player.addCard(new Card(Suit.CLOVER, Rank.TWO));
-//        // when
-//        Map<Player, Result> results = participants.calculateAllResults();
-//        // then
-//        assertThat(results.get(player)).isEqualTo(Result.LOSE);
-//    }
+    @DisplayName("모든 플레이어의 결과를 반환한다.")
+    @Test
+    void 결과_계산() {
+        Map<String, Integer> playerBettingAmountTable = new LinkedHashMap<>() {{
+            put("Player1", 1_000);
+            put("Player2", 2_000);
+        }};
+        Participants participants = Participants.from(playerBettingAmountTable);
+        participants.drawInitialCardsEachParticipant(ShuffledDeck.createByCount(6));
+
+        AllWinningAmountDto allWinningAmountDto = participants.calculateWinningAmountOfAllPlayers();
+        List<Player> players = participants.getPlayers();
+
+        players.forEach(player ->
+                assertThat(allWinningAmountDto.getPlayerNameWinningAmounts().get(player.getNameValue())).isInstanceOf(Integer.class));
+    }
 
     @DisplayName("딜러의 점수가 16 이하이면 hit 한다.")
     @Test
@@ -176,8 +104,8 @@ class ParticipantsTest {
         Player player = Player.of("Player", 1_000);
         Participants participants = new Participants(new Players(List.of(player)), new Dealer());
         participants.drawInitialCardsEachParticipant(DeterminedDeck.from(List.of(
-                new Card(Suit.CLOVER, Rank.FIVE), // Player1에게 주어지는 카드 2
-                new Card(Suit.CLOVER, Rank.FOUR), // Player1에게 주어지는 카드 1
+                new Card(Suit.CLOVER, Rank.FIVE), // Player에게 주어지는 카드 2
+                new Card(Suit.CLOVER, Rank.FOUR), // Player에게 주어지는 카드 1
                 new Card(Suit.CLOVER, Rank.FIVE), // 딜러에게 주어지는 카드 2
                 new Card(Suit.CLOVER, Rank.FIVE) // 딜러에게 주어지는 카드 1
         )));
@@ -185,5 +113,80 @@ class ParticipantsTest {
         AllWinningAmountDto allWinningAmountDto = participants.calculateWinningAmountOfAllPlayers();
         // then
         assertThat(allWinningAmountDto.getPlayerNameWinningAmounts().get(player.getNameValue())).isEqualTo(-1000);
+    }
+
+    @DisplayName("플레이어가 bust이면 베팅 금액만큼의 돈을 잃는다.")
+    @Test
+    void 플레이어_패배_버스트() {
+        // given
+        Player player = Player.of("Player", 1_000);
+        Participants participants = new Participants(new Players(List.of(player)), new Dealer());
+        participants.drawInitialCardsEachParticipant(DeterminedDeck.from(List.of(
+                new Card(Suit.CLOVER, Rank.KING), // Player에게 주어지는 카드 2
+                new Card(Suit.CLOVER, Rank.KING), // Player에게 주어지는 카드 1
+                new Card(Suit.CLOVER, Rank.FIVE), // 딜러에게 주어지는 카드 2
+                new Card(Suit.CLOVER, Rank.FIVE) // 딜러에게 주어지는 카드 1
+        )));
+        player.addCard(new Card(Suit.CLOVER, Rank.TWO));
+        // when
+        AllWinningAmountDto allWinningAmountDto = participants.calculateWinningAmountOfAllPlayers();
+        // then
+        assertThat(allWinningAmountDto.getPlayerNameWinningAmounts().get(player.getNameValue())).isEqualTo(-1000);
+    }
+
+    @DisplayName("플레이어가 딜러보다 점수가 높으면 베팅 금액만큼의 돈을 얻는다.")
+    @Test
+    void 플레이어_승리() {
+        // given
+        Player player = Player.of("Player", 1_000);
+        Participants participants = new Participants(new Players(List.of(player)), new Dealer());
+        participants.drawInitialCardsEachParticipant(DeterminedDeck.from(List.of(
+                new Card(Suit.CLOVER, Rank.KING), // Player에게 주어지는 카드 2
+                new Card(Suit.CLOVER, Rank.KING), // Player에게 주어지는 카드 1
+                new Card(Suit.CLOVER, Rank.FIVE), // 딜러에게 주어지는 카드 2
+                new Card(Suit.CLOVER, Rank.FIVE) // 딜러에게 주어지는 카드 1
+        )));
+        // when
+        AllWinningAmountDto allWinningAmountDto = participants.calculateWinningAmountOfAllPlayers();
+        // then
+        assertThat(allWinningAmountDto.getPlayerNameWinningAmounts().get(player.getNameValue())).isEqualTo(1000);
+    }
+
+    @DisplayName("플레이어와 딜러가 같은 점수이면 베팅 금액을 돌려받는다.")
+    @Test
+    void 플레이어_무승부() {
+        // given
+        Player player = Player.of("Player", 1_000);
+        Participants participants = new Participants(new Players(List.of(player)), new Dealer());
+        participants.drawInitialCardsEachParticipant(DeterminedDeck.from(List.of(
+                new Card(Suit.CLOVER, Rank.FIVE), // Player에게 주어지는 카드 2
+                new Card(Suit.CLOVER, Rank.FIVE), // Player에게 주어지는 카드 1
+                new Card(Suit.CLOVER, Rank.FIVE), // 딜러에게 주어지는 카드 2
+                new Card(Suit.CLOVER, Rank.FIVE) // 딜러에게 주어지는 카드 1
+        )));
+        // when
+        AllWinningAmountDto allWinningAmountDto = participants.calculateWinningAmountOfAllPlayers();
+        // then
+        assertThat(allWinningAmountDto.getPlayerNameWinningAmounts().get(player.getNameValue())).isEqualTo(0);
+    }
+
+    @DisplayName("딜러만 bust이면 플레이어는 베팅 금액만큼의 돈을 얻는다.")
+    @Test
+    void 플레이어_승리_딜러_버스트() {
+        // given
+        Player player = Player.of("Player", 1_000);
+        Dealer dealer = new Dealer();
+        Participants participants = new Participants(new Players(List.of(player)), dealer);
+        participants.drawInitialCardsEachParticipant(DeterminedDeck.from(List.of(
+                new Card(Suit.CLOVER, Rank.KING), // Player에게 주어지는 카드 2
+                new Card(Suit.CLOVER, Rank.KING), // Player에게 주어지는 카드 1
+                new Card(Suit.CLOVER, Rank.KING), // 딜러에게 주어지는 카드 2
+                new Card(Suit.CLOVER, Rank.SIX) // 딜러에게 주어지는 카드 1
+        )));
+        dealer.addCard(new Card(Suit.CLOVER, Rank.TWO));
+        // when
+        AllWinningAmountDto allWinningAmountDto = participants.calculateWinningAmountOfAllPlayers();
+        // then
+        assertThat(allWinningAmountDto.getPlayerNameWinningAmounts().get(player.getNameValue())).isEqualTo(1000);
     }
 }
