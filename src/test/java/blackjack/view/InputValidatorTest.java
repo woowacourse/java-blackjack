7e585ed050f0a75643389dfc4492abb200d28dec
seokhwan-an <asok369258@naver.com>
2023-03-10 17:28:03 +0900

    style : 불필요한 코드 삭제

--- src/test/java/blackjack/view/InputValidatorTest.java
+++ src/test/java/blackjack/view/InputValidatorTest.java
@@ -1,7 +1,7 @@
 package blackjack.view;
 
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
