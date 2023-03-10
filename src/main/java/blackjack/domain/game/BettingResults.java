29677a1c4ba8ee388df68691d8198c969ecefaf0
hyunseo <goatsfish@gmail.com>
2023-03-10 14:34:43 +0900

    refactor: 배팅 결과 딜러 저장 후 플레이어 저장 로직으로 수정

--- src/main/java/blackjack/domain/game/BettingResults.java
+++ src/main/java/blackjack/domain/game/BettingResults.java
@@ -4,22 +4,23 @@ import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Money;
 import blackjack.domain.participant.Participant;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class BettingResults {
-    private final Map<Participant, Money> results;
+    private final Map<Participant, Money> results = new LinkedHashMap<>();
 
     public BettingResults(final Map<Participant, Money> results, final Dealer dealer) {
         validate(results);
-        this.results = results;
-        this.results.put(dealer, getDealerProfit());
+        this.results.put(dealer, getDealerProfit(results));
+        this.results.putAll(results);
     }
 
     public Map<Participant, Money> getPlayerBettingResults() {
         return results;
     }
 
-    private Money getDealerProfit() {
+    private Money getDealerProfit(final Map<Participant, Money> results) {
         return results.values()
                 .stream()
                 .reduce(Money.ZERO, Money::spend);
