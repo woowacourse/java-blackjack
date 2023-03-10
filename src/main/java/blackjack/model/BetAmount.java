286dc78ac33c5a916e9537b0a89134b66c69ab04
hectick <chyeon1223@cau.ac.kr>
2023-03-10 21:16:40 +0900

    refactor: 메소드 분리

--- src/main/java/blackjack/model/BetAmount.java
+++ src/main/java/blackjack/model/BetAmount.java
@@ -16,12 +16,15 @@ public class BetAmount {
             throw new IllegalArgumentException("유효한 금액이 아닙니다. 베팅 금액은 양의 정수여야 합니다.");
         }
     }
-
-    //todo 테스트 코드 짜기
+    
     private void validateIsMinimumUnitSatisfied(int betAmount){
-        if(betAmount % MIN_BET_UNIT != 0){
+        if(isNotSatisfiedBetUnit(betAmount)){
             throw new IllegalArgumentException("베팅은 만원 단위로 가능합니다.");
         }
+    }
+
+    private static boolean isNotSatisfiedBetUnit(int betAmount) {
+        return betAmount % MIN_BET_UNIT != 0;
     }
 
     public int getBetAmount() {
