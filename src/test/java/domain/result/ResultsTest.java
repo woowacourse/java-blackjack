450e35cbbbb6604341ad973359dd26677986e3fc
jjongwa <troas96@naver.com>
2023-03-11 01:47:29 +0900

    refactor: 기능 추가에 따른 ResultTest 리팩터링

--- src/test/java/domain/result/ResultsTest.java
+++ src/test/java/domain/result/ResultsTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import domain.card.Card;
 import domain.participant.Dealer;
+import domain.participant.Money;
 import domain.participant.Name;
 import domain.participant.Player;
 import domain.participant.Players;
@@ -16,23 +17,30 @@ import view.OutputView;
 
 public class ResultsTest {
 
-    public static final Player DINO = new Player(new Name("dino"));
+    public static final Player dino = new Player(new Name("dino"));
     public static final Player SUNGHA = new Player(new Name("sungha"));
     public static final Player POBI = new Player(new Name("pobi"));
-    public static final Dealer DEALER = new Dealer();
+    public static final Dealer dealer = new Dealer();
+    public static final BettingResults BETTING_RESULT = new BettingResults();
 
     @Test
     @DisplayName("대결 시 승패 여부가 올바르게 저장된다.")
     void shouldSuccessSaveFightWinOrLoseResult() {
-        Results resultCalculator = new Results(new Players(List.of(DINO)), DEALER);
-        DINO.drawCard(new Card("5다이아몬드", 5));
-        DINO.drawCard(new Card("10다이아몬드", 10));
-        DEALER.drawCard(new Card("3하트", 3));
-        DEALER.drawCard(new Card("8클로버", 8));
-        resultCalculator.fight(DINO, DEALER);
+        BettingResults bettingResults = new BettingResults();
+        Player dino = new Player(new Name("dino"));
+        Dealer dealer = new Dealer();
+        bettingResults.initParticipantBet(dino, new Money("10000"));
+        bettingResults.initParticipantBet(dealer, new Money());
+        Results result = new Results(bettingResults, new Players(List.of(dino)), dealer);
 
-        List<Integer> playerResults = OutputView.getResultsByName(resultCalculator.getResults(), "dino");
-        List<Integer> dealerResults = OutputView.getResultsByName(resultCalculator.getResults(), "딜러");
+        dino.drawCard(new Card("5다이아몬드", 5));
+        dino.drawCard(new Card("10다이아몬드", 10));
+        dealer.drawCard(new Card("3하트", 3));
+        dealer.drawCard(new Card("8클로버", 8));
+        result.fight(dino, dealer);
+
+        List<Integer> playerResults = OutputView.getResultsByName(result.getResults(), "dino");
+        List<Integer> dealerResults = OutputView.getResultsByName(result.getResults(), "딜러");
 
         assertAll(
                 () -> assertThat(playerResults).isEqualTo(List.of(1, 0, 0)),
@@ -43,15 +51,21 @@ public class ResultsTest {
     @Test
     @DisplayName("대결 시 무승부 여부가 올바르게 저장된다.")
     void shouldSuccessSaveFightDrawResult() {
-        Results resultCalculator = new Results(new Players(List.of(DINO)), DEALER);
-        DINO.drawCard(new Card("5다이아몬드", 5));
-        DINO.drawCard(new Card("10다이아몬드", 10));
-        DEALER.drawCard(new Card("10하트", 10));
-        DEALER.drawCard(new Card("5클로버", 5));
-        resultCalculator.fight(DINO, DEALER);
+        BettingResults bettingResults = new BettingResults();
+        Player dino = new Player(new Name("dino"));
+        Dealer dealer = new Dealer();
+        bettingResults.initParticipantBet(dino, new Money("10000"));
+        bettingResults.initParticipantBet(dealer, new Money());
+        Results result = new Results(bettingResults, new Players(List.of(dino)), dealer);
 
-        List<Integer> playerResults = OutputView.getResultsByName(resultCalculator.getResults(), "dino");
-        List<Integer> dealerResults = OutputView.getResultsByName(resultCalculator.getResults(), "딜러");
+        dino.drawCard(new Card("5다이아몬드", 5));
+        dino.drawCard(new Card("10다이아몬드", 10));
+        dealer.drawCard(new Card("10하트", 10));
+        dealer.drawCard(new Card("5클로버", 5));
+        result.fight(dino, dealer);
+
+        List<Integer> playerResults = OutputView.getResultsByName(result.getResults(), "dino");
+        List<Integer> dealerResults = OutputView.getResultsByName(result.getResults(), "딜러");
 
         assertSoftly(softly -> {
             softly.assertThat(playerResults).isEqualTo(List.of(0, 1, 0));
@@ -62,28 +76,39 @@ public class ResultsTest {
     @Test
     @DisplayName("대결 시 딜러가 Bust면 모든 참가자가 승리한다.")
     void everyPlayerWinWhenDealerBust() {
-        Players players = new Players(List.of(DINO, SUNGHA, POBI));
-        Results resultCalculator = new Results(players, DEALER);
-        DINO.drawCard(new Card("5다이아몬드", 5));
-        DINO.drawCard(new Card("10다이아몬드", 10));
-        DINO.drawCard(new Card("10스페이드", 10));
+        BettingResults bettingResults = new BettingResults();
+        Player dino = new Player(new Name("dino"));
+        Player sungha = new Player(new Name("sungha"));
+        Player pobi = new Player(new Name("pobi"));
+        Dealer dealer = new Dealer();
+        Players players = new Players(List.of(dino, sungha, pobi));
+        bettingResults.initParticipantBet(dino, new Money("10000"));
+        bettingResults.initParticipantBet(sungha, new Money("10000"));
+        bettingResults.initParticipantBet(pobi, new Money("10000"));
+        bettingResults.initParticipantBet(dealer, new Money());
+        Results result = new Results(bettingResults, players, dealer);
 
-        DEALER.drawCard(new Card("10하트", 10));
-        DEALER.drawCard(new Card("5클로버", 5));
-        DEALER.drawCard(new Card("7다이아몬드", 7));
 
-        SUNGHA.drawCard(new Card("2하트", 2));
-        SUNGHA.drawCard(new Card("7스페이드", 7));
+        dino.drawCard(new Card("5다이아몬드", 5));
+        dino.drawCard(new Card("10다이아몬드", 10));
+        dino.drawCard(new Card("10스페이드", 10));
 
-        POBI.drawCard(new Card("6다이아몬드", 6));
-        POBI.drawCard(new Card("8클로버", 8));
+        dealer.drawCard(new Card("10하트", 10));
+        dealer.drawCard(new Card("5클로버", 5));
+        dealer.drawCard(new Card("7다이아몬드", 7));
 
-        resultCalculator.executeGame(players, DEALER);
+        sungha.drawCard(new Card("2하트", 2));
+        sungha.drawCard(new Card("7스페이드", 7));
 
-        List<Integer> dinoResult = OutputView.getResultsByName(resultCalculator.getResults(), "dino");
-        List<Integer> sunghaResult = OutputView.getResultsByName(resultCalculator.getResults(), "sungha");
-        List<Integer> pobiResult = OutputView.getResultsByName(resultCalculator.getResults(), "pobi");
-        List<Integer> dealerResults = OutputView.getResultsByName(resultCalculator.getResults(), "딜러");
+        pobi.drawCard(new Card("6다이아몬드", 6));
+        pobi.drawCard(new Card("8클로버", 8));
+
+        result.executeGame(players, dealer);
+
+        List<Integer> dinoResult = OutputView.getResultsByName(result.getResults(), "dino");
+        List<Integer> sunghaResult = OutputView.getResultsByName(result.getResults(), "sungha");
+        List<Integer> pobiResult = OutputView.getResultsByName(result.getResults(), "pobi");
+        List<Integer> dealerResults = OutputView.getResultsByName(result.getResults(), "딜러");
 
         assertSoftly(softly -> {
             softly.assertThat(dinoResult).isEqualTo(List.of(1, 0, 0));
