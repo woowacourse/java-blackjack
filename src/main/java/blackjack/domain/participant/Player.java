a20a614138fbea833880c4639a51d5376d1e46d1
kdkdhoho <hkim4410@naver.com>
2023-03-10 10:52:35 +0900

    feat(Player): Player 클래스 구현

---
+++ src/main/java/blackjack/domain/participant/Player.java
@@ -0,0 +1,14 @@
+package blackjack.domain.participant;
+
+public class Player {
+
+    private final String name;
+
+    public Player(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
