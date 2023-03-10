fdec493f6ce9d4a920a7aaf61abcab4290338fda
JunH <tjdtls690@gmail.com>
2023-03-10 17:36:24 +0900

    :sparkles: feat : 현재 상태에서 스코어 반환하는 기능 구현

--- src/main/java/domain/state/State.java
+++ src/main/java/domain/state/State.java
@@ -2,6 +2,7 @@ package domain.state;
 
 import domain.card.Card;
 import domain.card.Hand;
+import domain.card.Score;
 
 public abstract class State {
     
@@ -13,6 +14,10 @@ public abstract class State {
     
     public abstract State draw(Card card);
     public abstract double calculateProfit(int betAmount);
+    
+    protected Score score() {
+        return hand.getTotalScore();
+    }
     
     protected Hand getHand() {
         return hand;
