f8358192aebd7a22c8b9e0e30bac2e9c2229781e
kdkdhoho <hkim4410@naver.com>
2023-03-10 09:55:26 +0900

    test(DeckTest): Deck 클래스 기능 구현
    - 52장의 트럼프 카드를 가진다.
    - 가지고 있는 카드 한 장을 준다.

---
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -0,0 +1,41 @@
+package blackjack.domain.card;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Deck {
+
+    private static final int INITIAL_CAPACITY = 52;
+
+    private final Set<Card> deck = new HashSet<>(INITIAL_CAPACITY);
+
+    public Deck() {
+        init();
+    }
+
+    private void init() {
+        for (Denomination denomination : Denomination.values()) {
+            fillCards(denomination);
+        }
+    }
+
+    private void fillCards(final Denomination denomination) {
+        for (Suit suit : Suit.values()) {
+            deck.add(new Card(denomination, suit));
+        }
+    }
+
+    public Card drawCard() {
+        Card drawCard = deck.stream()
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("현재 남아있는 카드가 존재하지 않습니다."));
+        deck.remove(drawCard);
+
+        return drawCard;
+    }
+
+    public Set<Card> getDeck() {
+        return Collections.unmodifiableSet(deck);
+    }
+}
---
+++ src/test/java/blackjack/domain/card/DeckTest.java
@@ -0,0 +1,44 @@
+package blackjack.domain.card;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DeckTest {
+
+    private Deck deck;
+
+    @BeforeEach
+    void setUp() {
+        this.deck = new Deck();
+    }
+
+    @Test
+    @DisplayName("Deck을 새로 생성하면 52장의 카드가 존재한다.")
+    void createTest() {
+        assertThat(deck.getDeck().size()).isEqualTo(52);
+    }
+
+    @Test
+    @DisplayName("deck에 카드가 남아있지 않은 경우 예외가 발생한다.")
+    void drawCard_empty_exception() {
+        for (int i = 1; i <= 52; i++) {
+            deck.drawCard();
+        }
+
+        assertThatIllegalStateException().isThrownBy(
+                () -> deck.drawCard()
+        ).withMessage("현재 남아있는 카드가 존재하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("deck에서 카드를 한 장 뽑는다.")
+    void drawCardTest() {
+        deck.drawCard();
+
+        assertThat(deck.getDeck().size()).isEqualTo(51);
+    }
+}
