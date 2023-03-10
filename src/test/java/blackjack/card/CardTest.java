75b1ca536907bfd86eefb4001eb8f9f933419bcd
kdkdhoho <hkim4410@naver.com>
2023-03-10 09:37:10 +0900

    test(CardTest): 파라미터에 final 키워드 추가

--- src/test/java/blackjack/card/CardTest.java
+++ src/test/java/blackjack/card/CardTest.java
@@ -10,7 +10,7 @@ class CardTest {
 
     @ParameterizedTest(name = "{0} 카드의 점수를 반환한다.")
     @CsvSource(value = {"ACE:1", "TWO:2", "J:10", "Q:10", "K:10"}, delimiter = ':')
-    void scoreTest(Denomination denomination, int expect) {
+    void scoreTest(final Denomination denomination, final int expect) {
         Card card = new Card(denomination, HEART);
 
         assertThat(card.score()).isEqualTo(expect);
