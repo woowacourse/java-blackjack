0f5b8a6fd5e5ba9e62c38bfd97c9dd032b228eb4
0chil <0@chll.it>
2023-03-10 15:09:48 +0900

    refactor(Result): 사용하지 않는 기능 제거

--- src/main/java/domain/Result.java
+++ src/main/java/domain/Result.java
@@ -2,24 +2,5 @@ package domain;
 
 public enum Result {
 
-    DRAW {
-        @Override
-        public Result reverse() {
-            return DRAW;
-        }
-    },
-    WIN {
-        @Override
-        public Result reverse() {
-            return LOSE;
-        }
-    },
-    LOSE {
-        @Override
-        public Result reverse() {
-            return WIN;
-        }
-    };
-
-    public abstract Result reverse();
+    DRAW, WIN, LOSE
 }
