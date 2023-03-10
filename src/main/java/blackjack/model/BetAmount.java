e474b9dfb00d176bf91892957a77006187983168
hectick <chyeon1223@cau.ac.kr>
2023-03-10 19:21:09 +0900

    feat: 베팅 금액이 만원 단위가 아니면 예외처리 구현

--- src/main/java/blackjack/model/BetAmount.java
+++ src/main/java/blackjack/model/BetAmount.java
@@ -2,16 +2,25 @@ package blackjack.model;
 
 public class BetAmount {
 
+    private static final int MIN_BET_UNIT = 10000;
     private final int betAmount;
 
     public BetAmount(int betAmount) {
         validateIsPositive(betAmount);
+        validateIsMinimumUnitSatisfied(betAmount);
         this.betAmount = betAmount;
     }
 
     private void validateIsPositive(int betAmount){
         if(betAmount <= 0){
             throw new IllegalArgumentException("유효한 금액이 아닙니다. 베팅 금액은 양의 정수여야 합니다.");
+        }
+    }
+
+    //todo 테스트 코드 짜기
+    private void validateIsMinimumUnitSatisfied(int betAmount){
+        if(betAmount % MIN_BET_UNIT != 0){
+            throw new IllegalArgumentException("베팅은 만원 단위로 가능합니다.");
         }
     }
 
