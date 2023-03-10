897c87e2bae4605b8afa825d724a7a205d17836e
the9kim <96895686+the9kim@users.noreply.github.com>
2023-03-10 16:16:21 +0900

    feat(Cars): 참가자가 받은 카드 그룹을 생성한다
    - 카드의 합 계산 기능
    - 카드를 한 장 추가하는 기능

--- src/main/java/domain/Cards.java
+++ src/main/java/domain/Cards.java
@@ -5,50 +5,98 @@ import java.util.List;
 public class Cards {
 
     public static final int BLACKJACK_NUMBER = 21;
-    public static final int ADDITIONAL_A_VALUE = 10;
-
     private final List<Card> cards;
 
     public Cards(List<Card> cards) {
         this.cards = cards;
     }
 
-    public int getScore() {
-        int sum = getSum();
-        if (hasA()) {
-            return calculateAValues(sum);
+    public int calculateSum() {
+        int initialSum = addAllCardValues();
+        if (hasAce()) {
+            return selectAceValue(initialSum);
         }
-        return sum;
+        return initialSum;
     }
 
-    private int getSum() {
+    private boolean hasAce() {
         return cards.stream()
-            .map(Card::getNumber)
-            .mapToInt(Number::getValue)
-            .sum();
+                .map(Card::getCardValue)
+                .anyMatch(value -> value == CardValue.ACE);
     }
 
-    private boolean hasA() {
+    private int addAllCardValues() {
         return cards.stream()
-            .anyMatch(c -> c.is(Number.A));
+                .map(Card::getCardValue)
+                .mapToInt(CardValue::getCardValue)
+                .sum();
     }
 
-    private int calculateAValues(int sum) {
-        if (sum + ADDITIONAL_A_VALUE > BLACKJACK_NUMBER) {
-            return sum;
+    private int selectAceValue(int initialSum) {
+        if (initialSum > 11) {
+            return initialSum;
         }
-        return sum + ADDITIONAL_A_VALUE;
+        return initialSum + 10;
     }
 
-    public int getSize() {
-        return cards.size();
+    public boolean isBlackjack() {
+        return calculateSum() == BLACKJACK_NUMBER;
     }
 
-    public void add(Card card) {
+    public void addCard(Card card) {
         cards.add(card);
     }
 
-    public Card get(int index) {
-        return cards.get(index);
+    public List<Card> getCards() {
+        return cards;
     }
+
+    //
+//    public static final int BLACKJACK_NUMBER = 21;
+//    public static final int ADDITIONAL_A_VALUE = 10;
+//
+//    private final List<Card> cards;
+//
+//    public Cards(List<Card> cards) {
+//        this.cards = cards;
+//    }
+//
+//    public int getScore() {
+//        int sum = getSum();
+//        if (hasA()) {
+//            return calculateAValues(sum);
+//        }
+//        return sum;
+//    }
+//
+//    private int getSum() {
+//        return cards.stream()
+//            .map(Card::getNumber)
+//            .mapToInt(Number::getValue)
+//            .sum();
+//    }
+//
+//    private boolean hasA() {
+//        return cards.stream()
+//            .anyMatch(c -> c.is(Number.A));
+//    }
+//
+//    private int calculateAValues(int sum) {
+//        if (sum + ADDITIONAL_A_VALUE > BLACKJACK_NUMBER) {
+//            return sum;
+//        }
+//        return sum + ADDITIONAL_A_VALUE;
+//    }
+//
+//    public int getSize() {
+//        return cards.size();
+//    }
+//
+//    public void add(Card card) {
+//        cards.add(card);
+//    }
+//
+//    public Card get(int index) {
+//        return cards.get(index);
+//    }
 }
