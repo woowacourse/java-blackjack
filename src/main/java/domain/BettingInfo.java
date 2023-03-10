d4be2ccac59321db37d474d9a2d9644c3a28f3e2
dhkim <akdlzmf1123@gmail.com>
2023-03-10 23:45:39 +0900

    feat: 수익금 계산하는 기능 구현

---
+++ src/main/java/domain/BettingInfo.java
@@ -0,0 +1,22 @@
+package domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BettingInfo {
+    private final Map<String, Integer> bettingAmounts;
+
+    public BettingInfo(final Map<String, Integer> bettingAmounts) {
+        this.bettingAmounts = Map.copyOf(bettingAmounts);
+    }
+
+    public Map<String, Integer> getEarnings(final Map<String, PlayerOutcome> playersOutcome) {
+        Map<String, Integer> earnings = new HashMap<>();
+        for (String name : bettingAmounts.keySet()) {
+            final PlayerOutcome playerOutcome = playersOutcome.get(name);
+            final int earning = playerOutcome.calculateEarning(bettingAmounts.get(name));
+            earnings.put(name, earning);
+        }
+        return earnings;
+    }
+}
