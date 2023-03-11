53798928ec4c9f34913e43c5a4f57f86a8557481
Rosie <dev.kayoung@gmail.com>
2023-03-11 19:44:13 +0900

    feat: 베팅금액 범위 검증 기능

--- src/main/java/blackjack/domain/BetAmount.java
+++ src/main/java/blackjack/domain/BetAmount.java
@@ -1,6 +1,13 @@
 package blackjack.domain;
 
 public class BetAmount {
+
+    public static final int MIN = 10000;
+    public static final int MAX = 100000;
+
     public BetAmount(int amount) {
+        if (MIN > amount || amount > MAX) {
+            throw new IllegalArgumentException("범위가 올바르지 않습니다.");
+        }
     }
 }
--- src/test/java/blackjack/domain/BetAmountTest.java
+++ src/test/java/blackjack/domain/BetAmountTest.java
@@ -1,6 +1,7 @@
 package blackjack.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,5 +16,27 @@ class BetAmountTest {
         final var betAmount = new BetAmount(amount);
         //then
         assertThat(betAmount).isInstanceOf(BetAmount.class);
+    }
+
+    @DisplayName("최소 베팅금액을 넘지 않으면 예외가 발생한다.")
+    @Test
+    void throwExceptionWhenBetAmountIsLowerThanMin() {
+        //given
+        int amount = 9000;
+        //when
+        //then
+        assertThatThrownBy(() -> new BetAmount(amount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("최대 베팅금액을 넘으면 예외가 발생한다.")
+    @Test
+    void throwExceptionWhenBetAmountIsHigherThanMax() {
+        //given
+        int amount = 120000;
+        //when
+        //then
+        assertThatThrownBy(() -> new BetAmount(amount))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
