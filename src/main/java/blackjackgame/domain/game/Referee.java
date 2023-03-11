e966f2994280cd6bdde8a2c57d0ab3c804e725da
yujamint <woawoa0513@gmail.com>
2023-03-12 03:23:33 +0900

    feat: 승자 결정의 책임을 가지는 Referee 클래스 구현

---
+++ src/main/java/blackjackgame/domain/game/Referee.java
@@ -0,0 +1,62 @@
+package blackjackgame.domain.game;
+
+import blackjackgame.domain.user.Dealer;
+import blackjackgame.domain.user.DealerStatus;
+import blackjackgame.domain.user.Player;
+import blackjackgame.domain.user.PlayerStatus;
+import blackjackgame.domain.user.Players;
+import blackjackgame.domain.user.User;
+
+public class Referee {
+    private final Players players;
+    private final Dealer dealer;
+
+    public Referee(Players players, Dealer dealer) {
+        this.players = players;
+        this.dealer = dealer;
+    }
+
+    public void judgeWinner() {
+        for (Player player : players.getPlayers()) {
+            judgeWinnerBetweenDealer(player);
+        }
+    }
+
+    private void judgeWinnerBetweenDealer(Player player) {
+        if (isDealerWin(player)) {
+            player.applyResult(Result.LOSE);
+            return;
+        }
+        if (isPlayerWin(player)) {
+            applyPlayerWinByHands(player);
+            return;
+        }
+        player.applyResult(Result.DRAW);
+    }
+
+    private boolean isDealerWin(Player player) {
+        boolean isPlayerBust = PlayerStatus.BUST == player.getStatus();
+        boolean isDealerHasHigherScore = dealer.isLessThanBustScore() && player.getScore() < dealer.getScore();
+        boolean isOnlyDealerBlackJack = isBlackJack(dealer) && !isBlackJack(player);
+        return isPlayerBust || isDealerHasHigherScore || isOnlyDealerBlackJack;
+    }
+
+    private boolean isBlackJack(User user) {
+        return user.cards().size() == 2 && user.getScore() == 21;
+    }
+
+    private boolean isPlayerWin(Player player) {
+        boolean isDealerBust = DealerStatus.BUST == dealer.getStatus();
+        boolean isPlayerHasHigherScore = player.isLessThanBustScore() && player.getScore() > dealer.getScore();
+        boolean isOnlyPlayerBlackJack = isBlackJack(player) && !isBlackJack(dealer);
+        return isDealerBust || isPlayerHasHigherScore || isOnlyPlayerBlackJack;
+    }
+
+    private void applyPlayerWinByHands(Player player) {
+        if (isBlackJack(player)) {
+            player.applyResult(Result.WIN_BLACKJACK);
+            return;
+        }
+        player.applyResult(Result.WIN);
+    }
+}
