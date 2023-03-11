456ff4e5b1bac285263b7f1fa6ca5db9aa30bda6
hum02 <ehdgma02@gmail.com>
2023-03-11 23:46:19 +0900

    test: 점수 비교에 대한 테스트 추가

--- src/test/java/blackjack/model/CardScoreTest.java
+++ src/test/java/blackjack/model/CardScoreTest.java
@@ -23,6 +23,34 @@ class CardScoreTest {
     }
 
     @Test
+    @DisplayName("카드 점수 비교 - 한명 blakcjack, 한 명 stand 상태")
+    void card_score_compare_when_blackjack_stand() {
+        //given
+        CardScore cardScore1 = new CardScore(21, ResultState.BLACKJACK);
+        CardScore cardScore2 = new CardScore(18, ResultState.STAND);
+
+        // when
+        int resultCompare = cardScore1.compareTo(cardScore2);
+
+        //then
+        assertThat(resultCompare).isPositive();
+    }
+
+    @Test
+    @DisplayName("카드 점수 비교 - 둘 다 blakcjack 상태")
+    void card_score_compare_when_blackjack_draw() {
+        //given
+        CardScore cardScore1 = new CardScore(21, ResultState.BLACKJACK);
+        CardScore cardScore2 = new CardScore(21, ResultState.BLACKJACK);
+
+        // when
+        int resultCompare = cardScore1.compareTo(cardScore2);
+
+        //then
+        assertThat(resultCompare).isEqualTo(0);
+    }
+
+    @Test
     @DisplayName("카드 점수 비교 - 딜러와 플레이어 모두 버스트 : 딜러가 승리")
     void card_score_compare_when_bust() {
         //given
