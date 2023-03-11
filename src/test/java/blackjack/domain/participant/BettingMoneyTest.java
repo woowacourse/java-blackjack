e3750dcf07a2c7f85158d7de5277fb51f87cbab8
Be-student <presentsong@naver.com>
2023-03-11 21:18:03 +0900

    test: 빠진 displayGenerator 추가

--- src/test/java/blackjack/domain/participant/BettingMoneyTest.java
+++ src/test/java/blackjack/domain/participant/BettingMoneyTest.java
@@ -3,8 +3,12 @@ package blackjack.domain.participant;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings({"NonAsciiCharacters"})
+@DisplayNameGeneration(ReplaceUnderscores.class)
 class BettingMoneyTest {
 
     @Test
