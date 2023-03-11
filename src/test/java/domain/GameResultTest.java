84d61d2a48ab71dbde5870cb2fa80201da638594
aiaiaiai1 <cvc100326@naver.com>
2023-03-12 01:53:01 +0900

    refactor: 불필요한 테스트 제거

--- src/test/java/domain/GameResultTest.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-public class GameResultTest {
-    Dealer dealer = new Dealer();
-    Players players = new Players("pobi,jason");
-    CardDeck cardDeck = new CardDeck(new CardDeckGenerator().generate());
-
-}
-
