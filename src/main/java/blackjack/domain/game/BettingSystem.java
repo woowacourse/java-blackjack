fdcc806c1c532d052e12fe4913cb189034219d2f
Combi153 <jcm04@naver.com>
2023-03-11 21:23:47 +0900

    feat: 수익 계산 로직 추가

---
+++ src/main/java/blackjack/domain/game/BettingSystem.java
@@ -0,0 +1,40 @@
+package blackjack.domain.game;
+
+import blackjack.domain.player.Player;
+import blackjack.domain.player.Result;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static blackjack.domain.player.Result.LOSE;
+import static blackjack.domain.player.Result.WIN;
+
+public class BettingSystem {
+    private final Map<Player, BetMoney> betMoneyByPlayer;
+
+    public BettingSystem(final Map<Player, BetMoney> betMoneyByPlayer) {
+        this.betMoneyByPlayer = betMoneyByPlayer;
+    }
+
+    public Map<Player, BetMoney> getProfitResult(final Map<Player, Result> resultByPlayers) {
+        Map<Player, BetMoney> profitByPlayers = new LinkedHashMap<>();
+        for (Player player : resultByPlayers.keySet()) {
+            final BetMoney income = getIncome(player, resultByPlayers.get(player));
+            profitByPlayers.put(player, income);
+        }
+        return profitByPlayers;
+    }
+
+    private BetMoney getIncome(final Player player, final Result result) {
+        final BetMoney betMoney = betMoneyByPlayer.get(player);
+        if (result == WIN) {
+            final BetMoney resultMoney = betMoney.winMoney(player.isBlackjack());
+            return resultMoney.subtract(betMoney);
+        }
+        if (result == LOSE) {
+            final BetMoney resultMoney = betMoney.loseMoney();
+            return resultMoney.subtract(betMoney);
+        }
+        return betMoney.subtract(betMoney);
+    }
+}
---
+++ src/test/java/blackjack/domain/game/BettingSystemTest.java
@@ -0,0 +1,44 @@
+package blackjack.domain.game;
+
+import blackjack.domain.player.Player;
+import blackjack.domain.player.Players;
+import blackjack.util.FixedDeck;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static blackjack.util.CardFixtures.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+public class BettingSystemTest {
+
+    @Test
+    void 겜블러의_수입을_반환한다() {
+        final Players players = Players.from(List.of("후추", "허브"));
+        Map<Player, BetMoney> betMoneyByPlayer = new HashMap<>();
+        for (Player player : players.getPlayers()) {
+            betMoneyByPlayer.put(player, BetMoney.createBetMoney(10000));
+        }
+        final BettingSystem bettingSystem = new BettingSystem(betMoneyByPlayer);
+
+        final BlackjackGame blackjackGame = new BlackjackGame(players, bettingSystem);
+        blackjackGame.initialDraw(new FixedDeck(List.of(
+                ACE_DIAMOND,
+                JACK_SPADE,
+                EIGHT_SPADE,
+                NINE_SPADE,
+                KING_SPADE,
+                QUEEN_SPADE
+        )));
+
+        assertThat(bettingSystem.getProfitResult(blackjackGame.play()).values())
+                .extracting(BetMoney::getAmount)
+                .containsExactly(-10000, -10000);
+    }
+}
