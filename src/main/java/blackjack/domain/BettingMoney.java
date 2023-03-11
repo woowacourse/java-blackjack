8938cdf9ae88403be4d126651c89c8617005c70d
Cyma-s <ssol0319@naver.com>
2023-03-12 01:24:47 +0900

    feat: 베팅 금액 검증 기능 추가

---
+++ src/main/java/blackjack/domain/BettingMoney.java
@@ -0,0 +1,27 @@
+package blackjack.domain;
+
+public class BettingMoney {
+    private final int value;
+
+    public BettingMoney(final int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final int value) {
+        validateNotPositive(value);
+        validateInappropriateUnit(value);
+    }
+
+    private void validateNotPositive(int uncheckedValue) {
+        if (uncheckedValue <= 0) {
+            throw new IllegalArgumentException("베팅 금액은 0보다 커야 합니다.");
+        }
+    }
+
+    private void validateInappropriateUnit(int uncheckedValue) {
+        if(uncheckedValue % 100 != 0) {
+            throw new IllegalArgumentException("베팅 금액은 100 단위어야 합니다.");
+        }
+    }
+}
