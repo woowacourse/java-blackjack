617277fd89d97776ceff097d9d07c2b9e35087b1
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-03-11 19:06:09 +0900

    test : Player, Dealer 테스트 추가

--- src/test/java/domain/user/DealerTest.java
+++ src/test/java/domain/user/DealerTest.java
@@ -1,36 +1,99 @@
-//package domain.user;
-//
-//import domain.card.Card;
-//import domain.card.Denomination;
-//import domain.card.Suit;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.List;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//
-//class DealerTest {
-//
-//    @Test
-//    @DisplayName("17이면 히트가 아니다")
-//    void notHit() {
-//        Dealer dealer = new Dealer("deal", new Hand(List.of(
-//                Card.of(Suit.HEART, Denomination.SIX),
-//                Card.of(Suit.HEART, Denomination.ACE)
-//        )));
-//
-//        assertThat(dealer.canHit()).isFalse();
-//    }
-//
-//    @Test
-//    @DisplayName("16이면 히트다")
-//    void isHit() {
-//        Dealer dealer = new Dealer("deal", new Hand(List.of(
-//                Card.of(Suit.HEART, Denomination.FIVE),
-//                Card.of(Suit.HEART, Denomination.ACE)
-//        )));
-//
-//        assertThat(dealer.canHit()).isTrue();
-//    }
-//}
+package domain.user;
+
+import domain.state.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static domain.Fixtures.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class DealerTest {
+    @DisplayName("Dealer를 생성하면 State는 Ready상태이다")
+    @Test
+    void newDealer() {
+        Dealer dealer = new Dealer("maco");
+
+        assertThat(dealer.getState()).isInstanceOf(Ready.class);
+    }
+
+    @DisplayName("hit했을 때 정상 상태")
+    @Nested
+    class NormalStateByHit{
+
+        @DisplayName("ACE를 hit하면 State는 Hit상태이다")
+        @Test
+        void hit_ace_Hit() {
+            Dealer dealer = new Dealer("maco");
+
+            State state = dealer.hit(SPADE_ACE);
+
+            assertThat(state).isInstanceOf(Hit.class);
+        }
+
+        @DisplayName("ACE, TEN을 hit하면 Blackjack상태이다")
+        @Test
+        void hit_aceTen_Blackjack() {
+            Dealer dealer = new Dealer("maco");
+
+            dealer.hit(SPADE_ACE);
+            dealer.hit(SPADE_TEN);
+
+            assertThat(dealer.getState()).isInstanceOf(Blackjack.class);
+        }
+
+        @DisplayName("TEN, TEN, FIVE를 hit하면 Blackjack상태이다")
+        @Test
+        void hit_aceTen_Bust() {
+            Dealer dealer = new Dealer("maco");
+
+            dealer.hit(SPADE_TEN);
+            dealer.hit(HEART_TEN);
+            dealer.hit(SPADE_FIVE);
+
+            assertThat(dealer.getState()).isInstanceOf(Bust.class);
+        }
+    }
+
+    @DisplayName("hit했을 때 비정상 상태")
+    @Nested
+    class AbnormalStateByHit{
+
+        @DisplayName("Bust상태에서 hit할 수 없다.")
+        @Test
+        void hit_Bust_Exception() {
+            Dealer dealer = new Dealer("maco");
+
+            dealer.hit(SPADE_TEN);
+            dealer.hit(CLOVER_TEN);
+            dealer.hit(DIAMOND_TEN);
+
+            assertThatThrownBy(() -> dealer.hit(CLOVER_FIVE)).isInstanceOf(IllegalStateException.class);
+        }
+
+        @DisplayName("Stay상태에서 hit할 수 없다.")
+        @Test
+        void hit_Stay_Exception() {
+            Dealer dealer = new Dealer("maco");
+
+            dealer.hit(SPADE_TEN);
+            dealer.hit(CLOVER_TEN);
+            dealer.stay();
+
+            assertThatThrownBy(() -> dealer.hit(CLOVER_FIVE)).isInstanceOf(IllegalStateException.class);
+        }
+    }
+
+    @DisplayName("stay하면 Stay상태이다")
+    @Test
+    void stay() {
+        Dealer dealer = new Dealer("maco");
+
+        dealer.hit(SPADE_TEN);
+        dealer.hit(HEART_TEN);
+        dealer.stay();
+
+        assertThat(dealer.getState()).isInstanceOf(Stay.class);
+    }
+}
--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -1,18 +1,100 @@
 package domain.user;
 
+import domain.state.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static domain.Fixtures.SPADE_ACE;
-import static domain.Fixtures.SPADE_TWO;
+import static domain.Fixtures.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class PlayerTest {
 
+    @DisplayName("Player를 생성하면 State는 Ready상태이다")
     @Test
-    void test() {
-        Player player = new Player("pobi");
+    void newParticipant() {
+        Player player = new Player("maco");
 
-        player.hit(SPADE_ACE);
-        player.hit(SPADE_TWO);
+        assertThat(player.getState()).isInstanceOf(Ready.class);
+    }
 
+    @DisplayName("hit했을 때 정상 상태")
+    @Nested
+    class NormalStateByHit{
+
+        @DisplayName("ACE를 hit하면 State는 Hit상태이다")
+        @Test
+        void hit_ace_Hit() {
+            Player player = new Player("maco");
+
+            State state = player.hit(SPADE_ACE);
+
+            assertThat(state).isInstanceOf(Hit.class);
+        }
+
+        @DisplayName("ACE, TEN을 hit하면 Blackjack상태이다")
+        @Test
+        void hit_aceTen_Blackjack() {
+            Player player = new Player("maco");
+
+            player.hit(SPADE_ACE);
+            player.hit(SPADE_TEN);
+
+            assertThat(player.getState()).isInstanceOf(Blackjack.class);
+        }
+
+        @DisplayName("TEN, TEN, FIVE를 hit하면 Blackjack상태이다")
+        @Test
+        void hit_aceTen_Bust() {
+            Player player = new Player("maco");
+
+            player.hit(SPADE_TEN);
+            player.hit(HEART_TEN);
+            player.hit(SPADE_FIVE);
+
+            assertThat(player.getState()).isInstanceOf(Bust.class);
+        }
+    }
+
+    @DisplayName("hit했을 때 비정상 상태")
+    @Nested
+    class AbnormalStateByHit{
+
+        @DisplayName("Bust상태에서 hit할 수 없다.")
+        @Test
+        void hit_Bust_Exception() {
+            Player player = new Player("maco");
+
+            player.hit(SPADE_TEN);
+            player.hit(CLOVER_TEN);
+            player.hit(DIAMOND_TEN);
+
+            assertThatThrownBy(() -> player.hit(CLOVER_FIVE)).isInstanceOf(IllegalStateException.class);
+        }
+
+        @DisplayName("Stay상태에서 hit할 수 없다.")
+        @Test
+        void hit_Stay_Exception() {
+            Player player = new Player("maco");
+
+            player.hit(SPADE_TEN);
+            player.hit(CLOVER_TEN);
+            player.stay();
+
+            assertThatThrownBy(() -> player.hit(CLOVER_FIVE)).isInstanceOf(IllegalStateException.class);
+        }
+    }
+
+    @DisplayName("stay하면 Stay상태이다")
+    @Test
+    void stay() {
+        Player player = new Player("maco");
+
+        player.hit(SPADE_TEN);
+        player.hit(HEART_TEN);
+        player.stay();
+
+        assertThat(player.getState()).isInstanceOf(Stay.class);
     }
 }
