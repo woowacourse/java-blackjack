77a66d9a093a53f6fd7d686b53702e54da1dd13f
pilyang <30036534+pilyang@users.noreply.github.com>
2023-03-11 20:29:48 +0900

    feat: 게임의 결과가 없는 사용자 결과 조회시 예외 발생 구현

--- src/main/java/blackjack/domain/result/ResultTable.java
+++ src/main/java/blackjack/domain/result/ResultTable.java
@@ -6,18 +6,27 @@ import java.util.Map;
 
 public class ResultTable {
 
+    static final String USER_NOT_FOUND_EXCEPTION_MESSAGE = "사용자의 게임 결과가 존재하지 않습니다.";
+
     private final Map<String, GameResult> table;
 
     public ResultTable() {
         table = new HashMap<>();
     }
 
-    public void put(final String name, final GameResult gameResult) {
-        table.put(name, gameResult);
+    public void put(final String userName, final GameResult gameResult) {
+        table.put(userName, gameResult);
     }
 
-    public GameResult get(final String name) {
-        return table.get(name);
+    public GameResult get(final String userName) {
+        validateUserName(userName);
+        return table.get(userName);
+    }
+
+    private void validateUserName(final String userName) {
+        if (!table.containsKey(userName)) {
+            throw new IllegalArgumentException(USER_NOT_FOUND_EXCEPTION_MESSAGE);
+        }
     }
 
     public List<String> getGameEndedPlayerNames() {
--- src/test/java/blackjack/domain/result/ResultTableTest.java
+++ src/test/java/blackjack/domain/result/ResultTableTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ResultTableTest {
 
@@ -16,5 +17,19 @@ class ResultTableTest {
         resultTable.put(name, GameResult.NORMAL_WIN);
 
         assertThat(resultTable.get(name)).isEqualTo(GameResult.NORMAL_WIN);
+    }
+
+    @Test
+    @DisplayName("없는 사용자의 결과 요청시 예외를 발생시킨다.")
+    void userNameExceptionTest() {
+        final ResultTable resultTable = new ResultTable();
+        final String userName = "test1";
+        final String wrongUserName = "test2";
+
+        resultTable.put(userName, GameResult.NORMAL_WIN);
+
+        assertThatThrownBy(() -> resultTable.get(wrongUserName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ResultTable.USER_NOT_FOUND_EXCEPTION_MESSAGE);
     }
 }
