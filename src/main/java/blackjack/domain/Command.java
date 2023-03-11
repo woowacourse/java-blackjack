62070c71585aab0b93905458a7567307452ae620
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-03-12 03:17:57 +0900

    feat: 사용자에게 카드를 더 받을건지 입력받을 때 명령어에 있는 값을 입력하는지 조회하는 기능 구현

--- src/main/java/blackjack/domain/Command.java
+++ src/main/java/blackjack/domain/Command.java
@@ -1,8 +1,12 @@
 package blackjack.domain;
 
+import java.util.Arrays;
+
 public enum Command {
     HIT("y"),
     BUST("n");
+
+    private static final String INVALID_COMMAND_MESSAGE = "올바른 명령어가 아닙니다.";
 
     private final String command;
 
@@ -11,7 +15,18 @@ public enum Command {
     }
 
     public static boolean isHit(String command) {
+        validate(command);
         return HIT.getCommand().equals(command);
+    }
+
+    public static void validate(String input) {
+        if (!isValidCommand(input)) {
+            throw new IllegalArgumentException(INVALID_COMMAND_MESSAGE);
+        }
+    }
+
+    private static boolean isValidCommand(String input) {
+        return Arrays.stream(values()).anyMatch(x -> x.command.equals(input));
     }
 
     private String getCommand() {
---
+++ src/test/java/blackjack/domain/CommandTest.java
@@ -0,0 +1,31 @@
+package blackjack.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CommandTest {
+
+
+    @DisplayName("올바른 명령어를 입력하지 않는 경우 에러를 발생시킨다.")
+    @Test
+    void Should_ThrowException_When_WrongInput() {
+        assertThatThrownBy(() -> Command.isHit("a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("올바른 명령어가 아닙니다.");
+    }
+
+    @DisplayName("y를 입력하는 경우 hit이 입력된 것으로 판단되어 true를 반환할 수 있다.")
+    @Test
+    void Should_Success_When_CorrectInput_y() {
+        assertThat(Command.isHit("y")).isTrue();
+    }
+
+    @DisplayName("n을 입력하는 경우 hit이 아닌 걸로 판단되어 false를 반환할 수 있다.")
+    @Test
+    void Should_Success_When_CorrectInput_n() {
+        assertThat(Command.isHit("n")).isFalse();
+    }
+}
