2a1ac75302f2f3f36ed32b9c605f379279dfc735
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-03-12 03:19:36 +0900

    refactor: ResultReferee 내 사용하지 않는 기능 삭제

--- src/main/java/blackjack/domain/ResultReferee.java
+++ src/main/java/blackjack/domain/ResultReferee.java
@@ -36,14 +36,4 @@ public class ResultReferee {
         }
         return Result.BLACKJACK;
     }
-
-    public static Result getOpponentResult(Result result) {
-        if (result == Result.WIN || result == Result.BLACKJACK) {
-            return Result.LOSE;
-        }
-        if (result == Result.DRAW) {
-            return Result.DRAW;
-        }
-        return Result.WIN;
-    }
 }
