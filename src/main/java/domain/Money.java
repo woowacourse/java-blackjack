fc9e0f785226cbf97d6c6eb1bb0a5bc5e6694170
yeonkkk <tjddus0302@gmail.com>
2023-03-11 13:09:16 +0900

    feat: Money의 연산 메서드 생성

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -3,10 +3,24 @@ package domain;
 import java.util.Objects;
 
 public class Money {
+    private final static Money ZERO = new Money(0);
+
     private final int amount;
 
     public Money(int amount) {
         this.amount = amount;
+    }
+
+    public static Money zero() {
+        return ZERO;
+    }
+
+    public Money add(Money other) {
+        return new Money(this.amount + other.amount);
+    }
+
+    public Money subtract(Money other) {
+        return new Money(this.amount - other.amount);
     }
 
     @Override
--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -2,7 +2,10 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
@@ -12,4 +15,31 @@ class MoneyTest {
     void test_create() {
         assertDoesNotThrow(() -> new Money(1_000));
     }
+
+    @Test
+    @DisplayName("amount가 0인 money를 생성한다.")
+    void test_zero() {
+        assertThat(Money.zero())
+                .isEqualTo(new Money(0));
+    }
+
+    @ParameterizedTest(name = "두 money의 합을 구한다.")
+    @CsvSource({"1_000,2_000,3_000", "2_000,4_000,6_000"})
+    void test_add(int number1, int number2, int result) {
+        Money money = new Money(number1);
+
+        assertThat(money.add(new Money(number2)))
+                .isEqualTo(new Money(result));
+    }
+
+
+    @ParameterizedTest(name = "두 money의 차를 구한다.")
+    @CsvSource({"1_000,2_000,-1_000", "4_000,2_000,2_000"})
+    void test_subtract(int number1, int number2, int result) {
+        Money money = new Money(number1);
+
+        assertThat(money.subtract(new Money(number2)))
+                .isEqualTo(new Money(result));
+    }
+
 }
