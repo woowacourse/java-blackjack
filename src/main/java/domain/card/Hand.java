22ce2d71af36cf787c02f2e59699ac31dd0e945e
MoonJeWoong <munjin0201@naver.com>
2023-03-12 02:59:14 +0900

    feat: hand 객체 생성 기능 구현

---
+++ src/main/java/domain/card/Hand.java
@@ -0,0 +1,12 @@
+package domain.card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+    private final List<Card> cards;
+
+    public Hand() {
+        cards = new ArrayList<>();
+    }
+}
