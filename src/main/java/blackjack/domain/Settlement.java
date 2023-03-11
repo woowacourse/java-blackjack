453fc9d887dd97d9fef37e115832c3a05310a045
BGuga <tmdwns9913@gmail.com>
2023-03-11 16:02:32 +0900

    feat : 딜러 수익 계산 기능 구현

--- src/main/java/blackjack/domain/Settlement.java
+++ src/main/java/blackjack/domain/Settlement.java
@@ -2,19 +2,57 @@ package blackjack.domain;
 
 import blackjack.domain.game.Result;
 import blackjack.domain.user.Name;
+import blackjack.domain.user.User;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Settlement {
 
     private final Map<Name, Stake> userPrize;
+    private final Map<Name, Integer> profit;
 
-    public Settlement(Map<Name, Stake> userPrize){
+    public Settlement(Map<Name, Stake> userPrize) {
         this.userPrize = Map.copyOf(userPrize);
+        this.profit = initializeSetting();
+    }
+
+    private Map<Name, Integer> initializeSetting() {
+        final LinkedHashMap<Name, Integer> profit = new LinkedHashMap<>();
+        profit.put(new Name("딜러"), 0);
+        return profit;
     }
 
     public int getProfit(final Name name, final Result result) {
         final Stake stake = userPrize.get(name);
         return result.getProfit(stake.getStake());
+    }
+
+    public void enrollResult(Map<Result, List<User>> resultData) {
+        for (Result result : resultData.keySet()) {
+            calculateProfit(resultData.get(result), result);
+        }
+    }
+
+    private void calculateProfit(final List<User> users, final Result result) {
+        for (User user : users) {
+            final Name userName = user.getName();
+            final int userProfit = getProfit(userName, result);
+            profit.computeIfPresent(userName, (name, value) -> value + userProfit);
+            profit.putIfAbsent(userName, userProfit);
+        }
+    }
+
+    public int getDealerProfit() {
+        final int profitSum = profit.values().stream()
+                .mapToInt(value -> value.intValue())
+                .sum();
+        return (-1) * profitSum;
+    }
+
+    public Map<Name, Integer> getProfit() {
+        return Collections.unmodifiableMap(profit);
     }
 }
--- src/test/java/blackjack/domain/SettlementTest.java
+++ src/test/java/blackjack/domain/SettlementTest.java
@@ -1,12 +1,18 @@
 package blackjack.domain;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardNumber;
+import blackjack.domain.card.Cards;
+import blackjack.domain.card.Shape;
 import blackjack.domain.game.Result;
 import blackjack.domain.user.Name;
-import org.assertj.core.api.Assertions;
+import blackjack.domain.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,44 +21,136 @@ public class SettlementTest {
     @Test
     @DisplayName("승리한 유저를 정산기에 전달한다면 수익금을 베팅금 만큼 반환한다.")
     void winProfitTest() {
+        //given
         final HashMap<Name, Stake> nameStake = new HashMap<>();
         final Name pooh = new Name("푸우");
         nameStake.put(pooh, new Stake(1000));
-
         final Settlement settlement = new Settlement(nameStake);
+
+        //when,then
         assertThat(settlement.getProfit(pooh, Result.WIN)).isEqualTo(1000);
     }
 
     @Test
     @DisplayName("블랙잭 승리한 유저를 정산기에 전달한다면 수익금을 베팅금 1.5배 만큼 반환한다.")
     void blackJackWinProfitTest() {
+        //given
         final HashMap<Name, Stake> nameStake = new HashMap<>();
         final Name pooh = new Name("푸우");
         nameStake.put(pooh, new Stake(1000));
-
         final Settlement settlement = new Settlement(nameStake);
+
+        //when,then
         assertThat(settlement.getProfit(pooh, Result.BLACK_JACK_WIN)).isEqualTo(1500);
     }
 
     @Test
     @DisplayName("비긴 유저를 정산기에 전달한다면 수익금은 없는 것이다.")
     void drawProfitTest() {
+        //given
         final HashMap<Name, Stake> nameStake = new HashMap<>();
         final Name pooh = new Name("푸우");
         nameStake.put(pooh, new Stake(1000));
-
         final Settlement settlement = new Settlement(nameStake);
+
+        //when,then
         assertThat(settlement.getProfit(pooh, Result.DRAW)).isEqualTo(0);
     }
 
     @Test
     @DisplayName("패배한 유저를 정산기에 전달한다면 잃은 돈은 베팅금 만큼이다.")
     void loseProfitTest() {
+        //given
         final HashMap<Name, Stake> nameStake = new HashMap<>();
         final Name pooh = new Name("푸우");
         nameStake.put(pooh, new Stake(1000));
+        final Settlement settlement = new Settlement(nameStake);
+
+        //when, then
+        assertThat(settlement.getProfit(pooh, Result.LOSE)).isEqualTo(-1000);
+    }
+
+    @Test
+    @DisplayName("수익 결과 테스트")
+    void profitTest() {
+        //given
+        final HashMap<Name, Stake> nameStake = new HashMap<>();
+
+        final Name poohName = new Name("푸우");
+        nameStake.put(poohName, new Stake(1000));
+
+        final Name hupkName = new Name("헙크");
+        nameStake.put(hupkName, new Stake(2000));
+
+        final Name youngName = new Name("영이");
+        nameStake.put(youngName, new Stake(3000));
+
+        final Name nuguName = new Name("누구");
+        nameStake.put(nuguName, new Stake(4000));
 
         final Settlement settlement = new Settlement(nameStake);
-        assertThat(settlement.getProfit(pooh, Result.LOSE)).isEqualTo(-1000);
+
+        final User pooh = new User(poohName, new Cards(List.of(new Card(Shape.HEART, CardNumber.ACE), new Card(Shape.HEART, CardNumber.ACE))));
+        final User hupk = new User(hupkName, new Cards(List.of(new Card(Shape.HEART, CardNumber.ACE), new Card(Shape.HEART, CardNumber.ACE))));
+        final User youngE = new User(youngName, new Cards(List.of(new Card(Shape.HEART, CardNumber.ACE), new Card(Shape.HEART, CardNumber.ACE))));
+        final User nugu = new User(nuguName, new Cards(List.of(new Card(Shape.HEART, CardNumber.ACE), new Card(Shape.HEART, CardNumber.ACE))));
+
+        final HashMap<Result, List<User>> resultData = new HashMap<>();
+
+        resultData.put(Result.BLACK_JACK_WIN, List.of(pooh));
+        resultData.put(Result.WIN, List.of(hupk));
+        resultData.put(Result.LOSE, List.of(youngE));
+        resultData.put(Result.DRAW, List.of(nugu));
+
+        //when
+        settlement.enrollResult(resultData);
+        final HashMap<Name, Integer> expectedProfit = new HashMap<>();
+        expectedProfit.put(poohName, 1500);
+        expectedProfit.put(hupkName, 2000);
+        expectedProfit.put(youngName, -3000);
+        expectedProfit.put(nuguName, 0);
+
+        //then
+        assertThat(settlement.getProfit()).containsAllEntriesOf(expectedProfit);
+    }
+
+    @Test
+    @DisplayName("딜러 수익 결과 테스트")
+    void dealerProfitTest() {
+        //given
+        final HashMap<Name, Stake> nameStake = new HashMap<>();
+
+        final Name poohName = new Name("푸우");
+        nameStake.put(poohName, new Stake(1000));
+
+        final Name hupkName = new Name("헙크");
+        nameStake.put(hupkName, new Stake(2000));
+
+        final Name youngName = new Name("영이");
+        nameStake.put(youngName, new Stake(3000));
+
+        final Name nuguName = new Name("누구");
+        nameStake.put(nuguName, new Stake(4000));
+
+        final Settlement settlement = new Settlement(nameStake);
+
+        final User pooh = new User(poohName, new Cards(List.of(new Card(Shape.HEART, CardNumber.ACE), new Card(Shape.HEART, CardNumber.ACE))));
+        final User hupk = new User(hupkName, new Cards(List.of(new Card(Shape.HEART, CardNumber.ACE), new Card(Shape.HEART, CardNumber.ACE))));
+        final User youngE = new User(youngName, new Cards(List.of(new Card(Shape.HEART, CardNumber.ACE), new Card(Shape.HEART, CardNumber.ACE))));
+        final User nugu = new User(nuguName, new Cards(List.of(new Card(Shape.HEART, CardNumber.ACE), new Card(Shape.HEART, CardNumber.ACE))));
+
+        final HashMap<Result, List<User>> resultData = new HashMap<>();
+
+        resultData.put(Result.BLACK_JACK_WIN, List.of(pooh));
+        resultData.put(Result.WIN, List.of(hupk));
+        resultData.put(Result.LOSE, List.of(youngE));
+        resultData.put(Result.DRAW, List.of(nugu));
+
+        //when
+        settlement.enrollResult(resultData);
+        final int dealerProfit = settlement.getDealerProfit();
+
+        //then
+        assertThat(dealerProfit).isEqualTo(-500);
     }
 }
