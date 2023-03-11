162ec03c8a7dc849096cb40f9b51cac37b46c804
yujamint <woawoa0513@gmail.com>
2023-03-12 03:25:23 +0900

    test: Bet 테스트 케이스 추가 작성
    블랙잭으로 이기면 1.5배의 수익을 얻는다.

--- src/test/java/blackjackgame/domain/user/BetTest.java
+++ src/test/java/blackjackgame/domain/user/BetTest.java
@@ -2,6 +2,7 @@ package blackjackgame.domain.user;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import blackjackgame.domain.game.Result;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,37 +17,48 @@ class BetTest {
         assertThrows(IllegalArgumentException.class, () -> new Bet(amount));
     }
 
-    @DisplayName("베팅한 금액만큼 수익을 잃는다.")
+    @DisplayName("내기에서 지면 베팅한 금액만큼 수익을 잃는다.")
     @Test
     void lose() {
         int amount = 10_000;
         Bet bet = new Bet(amount);
 
-        bet.lose();
+        bet.applyResult(Result.LOSE);
 
         Assertions.assertThat(bet.profit()).isEqualTo(-amount);
     }
 
-    @DisplayName("베팅한 금액만큼의 수익을 얻는다.")
+    @DisplayName("내기에서 이기면 베팅한 금액만큼의 수익을 얻는다.")
     @Test
     void win() {
         int amount = 10_000;
         Bet bet = new Bet(amount);
 
-        bet.win();
+        bet.applyResult(Result.WIN);
 
         Assertions.assertThat(bet.profit()).isEqualTo(amount);
     }
 
-    @DisplayName("베팅한 금액을 돌려받아 수익이 0원이다.")
+    @DisplayName("내기에서 비기면 베팅한 금액을 돌려받아 수익이 0원이다.")
     @Test
     void draw() {
         int amount = 10_000;
         Bet bet = new Bet(amount);
 
-        bet.draw();
+        bet.applyResult(Result.DRAW);
 
         Assertions.assertThat(bet.profit()).isEqualTo(0);
+    }
+
+    @DisplayName("내기에서 블랙잭으로 이기면 베팅한 금액의 1.5배만큼의 수익을 얻는다.")
+    @Test
+    void win_blackjack() {
+        int amount = 10_000;
+        Bet bet = new Bet(amount);
+
+        bet.applyResult(Result.WIN_BLACKJACK);
+
+        Assertions.assertThat(bet.profit()).isEqualTo(15_000);
     }
 
 }
