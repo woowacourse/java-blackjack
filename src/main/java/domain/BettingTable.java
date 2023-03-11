9d4fa62f3726cb95615f8c8843cc38d58129bfdf
aiaiaiai1 <cvc100326@naver.com>
2023-03-12 01:08:51 +0900

    feat: 결과에 따른 배팅금액 계산하는 기능 추가

---
+++ src/main/java/domain/BettingTable.java
@@ -0,0 +1,31 @@
+package domain;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class BettingTable {
+    private final Map<String, Money> bettingTable = new LinkedHashMap<>();
+
+    public void add(String name, Money money) {
+        bettingTable.put(name, money);
+    }
+
+    public void calculate(Map<String, ResultType> playerResult) {
+        for (String name : playerResult.keySet()) {
+            ResultType resultType = playerResult.get(name);
+            Money money = bettingTable.get(name);
+            bettingTable.put(name, resultType.calculateBetting(money));
+        }
+    }
+
+    public Map<String, Money> getBettingTable() {
+        return bettingTable;
+    }
+
+    public int sum() {
+        return bettingTable.values().stream()
+                .mapToInt(Money::getMoney)
+                .sum();
+    }
+
+}
