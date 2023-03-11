b957044c32118cef9fec0212c450a76c551bb65b
amaran-th <songsy405@gmail.com>
2023-03-11 14:42:13 +0900

    feat(RefereeTest) : @MethodSource 적용

--- src/test/java/balckjack/domain/RefereeTest.java
+++ src/test/java/balckjack/domain/RefereeTest.java
@@ -1,123 +1,73 @@
 package balckjack.domain;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class RefereeTest {
 
-    private CardDeck dealer20;
-    private CardDeck dealer21;
-    private CardDeck dealer23;
-    private CardDeck dealerBlackJack;
+    @ParameterizedTest
+    @MethodSource("makeRefereeCase")
+    void testRefereeCase(Referee referee, List<Double> playerMoneys, Double dealerMoney) {
+        List<Double> playerWinningMoney = referee.calculateWinningMoneys();
+        Double dealerWinningMoney = referee.calculateDealerWinningMoney();
+        Assertions.assertThat(playerWinningMoney).isEqualTo(playerMoneys);
+        Assertions.assertThat(dealerWinningMoney).isEqualTo(dealerMoney);
+    }
 
-    private List<CardDeck> players;
-    private List<CardDeck> playersHaveBlackJack;
-
-    private List<Money> moneys;
-
-    @BeforeEach
-    void init() {
-        dealer20 = new CardDeck();
-
-        dealer20.addCard(new Card(Pattern.CLOVER, Denomination.TEN));
-        dealer20.addCard(new Card(Pattern.CLOVER, Denomination.JACK));
-
-        dealer21 = new CardDeck();
-
-        dealer21.addCard(new Card(Pattern.CLOVER, Denomination.TEN));
-        dealer21.addCard(new Card(Pattern.CLOVER, Denomination.FOUR));
-        dealer21.addCard(new Card(Pattern.CLOVER, Denomination.SEVEN));
-
-        dealer23 = new CardDeck();
-
-        dealer23.addCard(new Card(Pattern.CLOVER, Denomination.TEN));
-        dealer23.addCard(new Card(Pattern.CLOVER, Denomination.FOUR));
-        dealer23.addCard(new Card(Pattern.CLOVER, Denomination.JACK));
-
-        dealerBlackJack = new CardDeck();
-
-        dealerBlackJack.addCard(new Card(Pattern.CLOVER, Denomination.TEN));
-        dealerBlackJack.addCard(new Card(Pattern.CLOVER, Denomination.ACE));
-
-        CardDeck player1 = new CardDeck();
-        CardDeck player2 = new CardDeck();
-        CardDeck player3 = new CardDeck();
-        CardDeck player4 = new CardDeck();
-        CardDeck playerBlackJack = new CardDeck();
-
-        player1.addCard(new Card(Pattern.SPADE, Denomination.EIGHT));
-        player1.addCard(new Card(Pattern.SPADE, Denomination.ACE));
-        player1.addCard(new Card(Pattern.SPADE, Denomination.TWO));
-
-        player2.addCard(new Card(Pattern.CLOVER, Denomination.NINE));
-
-        player3.addCard(new Card(Pattern.HEART, Denomination.TEN));
-        player3.addCard(new Card(Pattern.HEART, Denomination.FOUR));
-        player3.addCard(new Card(Pattern.HEART, Denomination.SIX));
-
-        player4.addCard(new Card(Pattern.HEART, Denomination.TEN));
-        player4.addCard(new Card(Pattern.DIAMOND, Denomination.TEN));
-        player4.addCard(new Card(Pattern.DIAMOND, Denomination.KING));
-
-        playerBlackJack.addCard(new Card(Pattern.SPADE, Denomination.TEN));
-        playerBlackJack.addCard(new Card(Pattern.SPADE, Denomination.ACE));
-
-        players = List.of(player1, player2, player3, player4);
-        playersHaveBlackJack = List.of(playerBlackJack, player2, player3, player4);
-        moneys = List.of(new Money(1000.0), new Money(1000.0), new Money(1500.0),
+    static Stream<Arguments> makeRefereeCase() {
+        List<CardDeck> players = makePlayers();
+        List<CardDeck> playersHaveBlackJack = makePlayersHaveBlackJack();
+        CardDeck commonDealer = makeCardDeck(Pattern.SPADE, Denomination.TEN, Denomination.JACK);
+        CardDeck maxDealer = makeCardDeck(Pattern.SPADE, Denomination.TEN, Denomination.FOUR,
+            Denomination.SEVEN);
+        CardDeck bustDealer = makeCardDeck(Pattern.SPADE, Denomination.TEN, Denomination.FOUR,
+            Denomination.JACK);
+        CardDeck blackJackDealer = makeCardDeck(Pattern.SPADE, Denomination.TEN, Denomination.ACE);
+        List<Money> moneys = List.of(new Money(1000.0), new Money(1000.0), new Money(1500.0),
             new Money(2000.0));
+        return Stream.of(
+            Arguments.arguments(new Referee(commonDealer, players, moneys),
+                List.of(1000.0, -1000.0, 0.0, -2000.0), 2000.0),
+            Arguments.arguments(new Referee(bustDealer, players, moneys),
+                List.of(1000.0, 1000.0, 1500.0, -2000.0), -1500.0),
+            Arguments.arguments(new Referee(bustDealer, playersHaveBlackJack, moneys),
+                List.of(1500.0, 1000.0, 1500.0, -2000.0), -2000.0),
+            Arguments.arguments(new Referee(blackJackDealer, players, moneys),
+                List.of(-1000.0, -1000.0, -1500.0, -2000.0), 5500.0),
+            Arguments.arguments(new Referee(blackJackDealer, playersHaveBlackJack, moneys),
+                List.of(0.0, -1000.0, -1500.0, -2000.0), 4500.0),
+            Arguments.arguments(new Referee(maxDealer, playersHaveBlackJack, moneys),
+                List.of(1500.0, -1000.0, -1500.0, -2000.0), 3000.0)
+        );
     }
 
-    @Test
-    void testCommonResult() {
-        Referee referee = new Referee(dealer20, players, moneys);
-        List<Double> playerWinningMoney = referee.calculateWinningMoneys();
-        Double dealerWinningMoney = referee.calculateDealerWinningMoney();
-        Assertions.assertThat(playerWinningMoney).isEqualTo(List.of(1000.0, -1000.0, 0.0, -2000.0));
-        Assertions.assertThat(dealerWinningMoney).isEqualTo(2000.0);
+    private static List<CardDeck> makePlayers() {
+        return List.of(
+            makeCardDeck(Pattern.SPADE, Denomination.EIGHT, Denomination.ACE, Denomination.TWO),
+            makeCardDeck(Pattern.SPADE, Denomination.NINE),
+            makeCardDeck(Pattern.SPADE, Denomination.TEN, Denomination.FOUR, Denomination.SIX),
+            makeCardDeck(Pattern.CLOVER, Denomination.TEN, Denomination.TEN, Denomination.KING)
+        );
     }
 
-    @Test
-    void testDealerBustResult() {
-        Referee referee = new Referee(dealer23, players, moneys);
-        List<Double> playerWinningMoney = referee.calculateWinningMoneys();
-        Double dealerWinningMoney = referee.calculateDealerWinningMoney();
-        Assertions.assertThat(playerWinningMoney)
-            .isEqualTo(List.of(1000.0, 1000.0, 1500.0, -2000.0));
-        Assertions.assertThat(dealerWinningMoney).isEqualTo(-1500.0);
+    private static CardDeck makeCardDeck(Pattern pattern, Denomination... denominations) {
+        CardDeck deck = new CardDeck();
+        for (Denomination denomination : denominations) {
+            deck.addCard(new Card(pattern, denomination));
+        }
+        return deck;
     }
 
-    @Test
-    void testIncludeBlackJackPlayerResult() {
-        Referee referee = new Referee(dealer23, playersHaveBlackJack, moneys);
-        List<Double> playerWinningMoney = referee.calculateWinningMoneys();
-        Double dealerWinningMoney = referee.calculateDealerWinningMoney();
-        Assertions.assertThat(playerWinningMoney)
-            .isEqualTo(List.of(1500.0, 1000.0, 1500.0, -2000.0));
-        Assertions.assertThat(dealerWinningMoney).isEqualTo(-2000.0);
+    private static List<CardDeck> makePlayersHaveBlackJack() {
+        return List.of(
+            makeCardDeck(Pattern.HEART, Denomination.ACE, Denomination.KING),
+            makeCardDeck(Pattern.SPADE, Denomination.NINE),
+            makeCardDeck(Pattern.SPADE, Denomination.TEN, Denomination.FOUR, Denomination.SIX),
+            makeCardDeck(Pattern.CLOVER, Denomination.TEN, Denomination.TEN, Denomination.KING)
+        );
     }
-
-    @Test
-    void testDealerBlackJackResult() {
-        Referee referee = new Referee(dealerBlackJack, players, moneys);
-        List<Double> playerWinningMoney = referee.calculateWinningMoneys();
-        Double dealerWinningMoney = referee.calculateDealerWinningMoney();
-        Assertions.assertThat(playerWinningMoney)
-            .isEqualTo(List.of(-1000.0, -1000.0, -1500.0, -2000.0));
-        Assertions.assertThat(dealerWinningMoney).isEqualTo(5500.0);
-    }
-
-    @Test
-    void testDealerAndPlayerBlackJackResult() {
-        Referee referee = new Referee(dealerBlackJack, playersHaveBlackJack, moneys);
-        List<Double> playerWinningMoney = referee.calculateWinningMoneys();
-        Double dealerWinningMoney = referee.calculateDealerWinningMoney();
-        Assertions.assertThat(playerWinningMoney)
-            .isEqualTo(List.of(0.0, -1000.0, -1500.0, -2000.0));
-        Assertions.assertThat(dealerWinningMoney).isEqualTo(4500.0);
-    }
-
-
 }
