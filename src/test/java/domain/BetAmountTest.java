d834863062a86033fcabbbc262ae0485c654c445
Youngho <cyh6099@gmail.com>
2023-03-11 21:49:30 +0900

    test: 배팅금액에서 최종 상금 계산하는 테스트 작성

--- src/test/java/domain/BetAmountTest.java
+++ src/test/java/domain/BetAmountTest.java
@@ -1,15 +1,38 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BetAmountTest {
 
     @ParameterizedTest
     @ValueSource(ints = {999, 0, -1000})
     void validBetAmount(int betAmount) {
-        Assertions.assertThatThrownBy(() -> new BetAmount(betAmount))
+        assertThatThrownBy(() -> new BetAmount(new BigDecimal(betAmount)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void calculateBlackjackPrize() {
+        BetAmount betAmount = new BetAmount(new BigDecimal(1000));
+        assertThat(betAmount.calculatePrize(new BigDecimal("1.5"))).isEqualTo(1500);
+    }
+
+    @Test
+    void calculateLosePrize() {
+        BetAmount betAmount = new BetAmount(new BigDecimal(1000));
+        assertThat(betAmount.calculatePrize(new BigDecimal(-1))).isEqualTo(-1000);
+    }
+
+    @Test
+    void calculateDrawOrWinPrize() {
+        BetAmount betAmount = new BetAmount(new BigDecimal(1000));
+        assertThat(betAmount.calculatePrize(new BigDecimal(1))).isEqualTo(1000);
     }
 }
