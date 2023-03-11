2cfa1e24e29f119eab0fad46f246e7f8cedc3081
aiaiaiai1 <cvc100326@naver.com>
2023-03-12 01:54:16 +0900

    refactor: 하드코딩 제거 + 불필요한 상수 제거

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -11,9 +11,9 @@ public class OutputView {
     private static final String DEALER_DISTRIBUTE_MESSAGE = "%s 16이하라 한장의 카드를 더 받았습니다." + System.lineSeparator();
     private static final String DEALER_CARDS_RESULT_MESSAGE = "%s 카드: %s - 결과: %d" + System.lineSeparator();
     private static final String PLAYER_CARDS_RESULT_MESSAGE = "%s카드: %s - 결과: %d" + System.lineSeparator();
-    private static final String DEALER_RESULT_MESSAGE = "%s: %d승 %d무 %d패" + System.lineSeparator();
     private static final String PLAYER_RESULT_MESSAGE = "%s: %s" + System.lineSeparator();
     private static final String SPLIT_DELIMITER = ", ";
+    private static final String NAME_DELIMITER = ": ";
     public static final String FINAL_PROFIT_MESSAGE = "## 최종 수익";
 
     public String namimgCard(Card card) {
@@ -25,7 +25,7 @@ public class OutputView {
         System.out.printf(INITIAL_DISTRUIBUTE_MESSAGE, dealer.getName(), players.getPlayersName().stream()
                 .collect(Collectors.joining(SPLIT_DELIMITER)));
         Card dealerCard = dealer.getCards().get(0);
-        System.out.println(dealer.getName() + ": " + namimgCard(dealerCard));
+        System.out.println(dealer.getName() + NAME_DELIMITER + namimgCard(dealerCard));
         for (String key : players.getInfo().keySet()) {
             List<Card> value = players.getInfo().get(key);
             System.out.println(key + "카드: " + value.stream()
@@ -63,7 +63,7 @@ public class OutputView {
         System.out.println();
         System.out.println(FINAL_PROFIT_MESSAGE);
         Map<String, Money> result = bettingTable.getBettingTable();
-        System.out.println(dealer.getName() +": " + -bettingTable.sum());
+        System.out.println(dealer.getName() + NAME_DELIMITER + -bettingTable.sum());
         for (String playerName : result.keySet()) {
             System.out.printf(PLAYER_RESULT_MESSAGE, playerName,
                     result.get(playerName).getMoney());
