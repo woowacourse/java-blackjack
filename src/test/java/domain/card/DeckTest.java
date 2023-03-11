34729479af22c5d77b40f24799a79b45e17356b0
JunH <tjdtls690@gmail.com>
2023-03-12 01:48:48 +0900

    :white_check_mark: test : 객체명이 바뀐것에 맞춰서 테스트 메서드명도 개선

--- src/test/java/domain/card/DeckTest.java
+++ src/test/java/domain/card/DeckTest.java
@@ -9,14 +9,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException
 class DeckTest {
     @Test
     @DisplayName("Deck이 정상적으로 생성된다.")
-    void initializingCardRepository() {
+    void initializingDeck() {
         assertThatNoException()
                 .isThrownBy(() -> Deck.from(cards -> cards));
     }
     
     @Test
     @DisplayName("Deck에서 draw하면 가장 늦게 push 된 카드를 가져온다.")
-    void findCardByIndex() {
+    void draw() {
         Deck deck = Deck.from(cards -> cards);
         Card card = deck.draw();
         
