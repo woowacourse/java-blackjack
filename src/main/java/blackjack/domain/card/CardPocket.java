680486e27e1f92c583aecd2ff7d58b1542b805be
Be-student <presentsong@naver.com>
2023-03-11 14:12:58 +0900

    refactor: 캐싱 제거

--- src/main/java/blackjack/domain/card/CardPocket.java
+++ src/main/java/blackjack/domain/card/CardPocket.java
@@ -9,7 +9,6 @@ public class CardPocket {
     private static final int BLACKJACK_SCORE = 21;
     private static final int VALUE_ACE = 10;
     private final List<Card> cards;
-    private int cachedScore;
 
     private CardPocket(final List<Card> cards) {
         validateCardPocket(cards);
@@ -31,20 +30,17 @@ public class CardPocket {
         calculateCurrentScore();
     }
 
-    private void calculateCurrentScore() {
+    private int calculateCurrentScore() {
         final int countOfAce = countAce();
         int scoreOfCards = calculateMinimumScore();
         for (int i = 0; i < countOfAce; i++) {
             scoreOfCards = calculateAceScore(scoreOfCards);
         }
-        cachedScore = scoreOfCards;
+        return scoreOfCards;
     }
 
     public int calculateScore() {
-        // 캐싱된 값이 없는 경우에 계산 같은 작업이 없이, 그냥 반환하고 끝 처럼 보이는데요 메서드가 괜찮아보이시나요?
-        //아니면 계산하는 메서드를 호출하는 시늉이라도 주는게 좋을까요?
-        return cachedScore;
-        // return calculateCurrentScore(); 같은 느낌으로 주석을 남겨두는 느낌은 어떤가요?
+        return calculateCurrentScore();
     }
 
     private int countAce() {
