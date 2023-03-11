8c7cc04330f661016246f17e1e08287570a31fd5
MoonJeWoong <munjin0201@naver.com>
2023-03-12 06:04:38 +0900

    feat: score 추가 기능 구현
    세부사항
    - Ace를 1 혹은 11로 계산하는 기능 구현
    - 주어진 점수 이하인지 판단하는 기능 구현
    - 대소 비교 기능 구현
    - 21점 초과 판단 기능 구현
    - Ace를 1 혹은 11로 계산하는 기능 구현

--- src/main/java/domain/user/Score.java
+++ src/main/java/domain/user/Score.java
@@ -1,39 +1,40 @@
 package domain.user;
 
-import domain.card.Card;
-import domain.Number;
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class Score {
-    private static final int BLACKJACK = 21;
-    private static final int ZERO = 0;
-    private static final int ACE_ONE = 1;
+    private static final Score max = new Score(21);
+    private static final int DIFFERENCE_BETWEEN_ACE_ONE_AND_ELEVEN = 10;
 
-    private int score;
-
-    public Score(List<Card> cards) {
-        calculate(cards);
-    }
+    private final int score;
 
     public Score(int score) {
         this.score = score;
     }
 
-    public void calculate(List<Card> cards) {
-        List<Integer> numbers = convertCardsToNumbers(cards);
-        int aceCount = countOfAce(numbers);
-        int sum = sum(numbers);
-
-        if (sum > BLACKJACK) {
-            sum = calculateAceAsOne(aceCount, sum);
-        }
-        score = sum;
-    }
-
     public Score add(Score other) {
         return new Score(this.score + other.getScore());
+    }
+
+    public boolean isLessThanOrEqual(Score other) {
+        return isLessThan(other) || score == other.getScore();
+    }
+
+    public boolean isLessThan(Score other) {
+        return score < other.getScore();
+    }
+
+    public boolean isOverMax() {
+        return max.isLessThan(this);
+    }
+
+    public Score calculateAceAsOne(int aceCount) {
+        Score newScore = new Score(this.score);
+        while (aceCount > 0 && newScore.isOverMax()) {
+            newScore = new Score(newScore.getScore() - DIFFERENCE_BETWEEN_ACE_ONE_AND_ELEVEN);
+            aceCount--;
+        }
+        return newScore;
     }
 
     public int getScore() {
@@ -51,32 +52,5 @@ public class Score {
     @Override
     public int hashCode() {
         return Objects.hash(score);
-    }
-
-    private List<Integer> convertCardsToNumbers(List<Card> cards) {
-        return cards.stream()
-                .map(Card::getScore)
-                .collect(Collectors.toUnmodifiableList());
-    }
-
-    private int countOfAce(List<Integer> numbers) {
-        return (int) numbers.stream()
-                .filter(number -> number == Number.ACE.getScore())
-                .count();
-    }
-
-    private int sum(List<Integer> numbers) {
-        return numbers.stream()
-                .mapToInt(number -> number)
-                .sum();
-    }
-
-    private int calculateAceAsOne(int aceCount, int sum) {
-        while (aceCount > ZERO && sum > BLACKJACK) {
-            sum -= Number.ACE.getScore();
-            sum += ACE_ONE;
-            aceCount--;
-        }
-        return sum;
     }
 }
