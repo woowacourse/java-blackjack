c396cf6bee20940f146d328540fba5cfabeb71e8
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-03-11 22:03:10 +0900

    feat : Dealer는 점수가 16이하인지 판단할 수 있다

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -1,9 +1,12 @@
 package domain.user;
 
 import domain.card.Card;
+import domain.game.Score;
 import domain.state.State;
 
 public class Dealer {
+
+    private static final Score hitScoreLimit = new Score(16);
 
     private final Participant participant;
 
@@ -11,8 +14,19 @@ public class Dealer {
         this.participant = new Participant(dealerName);
     }
 
+    public boolean canHit() {
+        return participantScore().isLessThanOrEqual(hitScoreLimit);
+    }
+    private Score participantScore() {
+        return getState().score();
+    }
+
     public State hit(Card card) {
-        return participant.hit(card);
+        if (canHit()) {
+            return participant.hit(card);
+        }
+
+        throw new IllegalStateException("카드를 뽑을 수 없는 상태");
     }
 
     public State stay() {
--- src/test/java/domain/user/DealerTest.java
+++ src/test/java/domain/user/DealerTest.java
@@ -18,7 +18,7 @@ class DealerTest {
         assertThat(dealer.getState()).isInstanceOf(Ready.class);
     }
 
-    @DisplayName("hit했을 때 정상 상태")
+    @DisplayName("16이하에서 hit할 수 있다")
     @Nested
     class NormalStateByHit{
 
@@ -43,33 +43,31 @@ class DealerTest {
             assertThat(dealer.getState()).isInstanceOf(Blackjack.class);
         }
 
-        @DisplayName("TEN, TEN, FIVE를 hit하면 Blackjack상태이다")
+        @DisplayName("TEN, SIX를 hit하면 Hit 상태이다")
         @Test
         void hit_aceTen_Bust() {
             Dealer dealer = new Dealer("maco");
 
             dealer.hit(SPADE_TEN);
-            dealer.hit(HEART_TEN);
-            dealer.hit(SPADE_FIVE);
+            dealer.hit(HEART_SIX);
 
-            assertThat(dealer.getState()).isInstanceOf(Bust.class);
+            assertThat(dealer.getState()).isInstanceOf(Hit.class);
         }
     }
 
-    @DisplayName("hit했을 때 비정상 상태")
+    @DisplayName("16초과에서 hit할 수 없다")
     @Nested
     class AbnormalStateByHit{
 
-        @DisplayName("Bust상태에서 hit할 수 없다.")
+        @DisplayName("처음 뽑은 카드 두장의 합이 17이면 카드를 뽑지 못한다.")
         @Test
-        void hit_Bust_Exception() {
+        void canHit() {
             Dealer dealer = new Dealer("maco");
 
-            dealer.hit(SPADE_TEN);
-            dealer.hit(CLOVER_TEN);
-            dealer.hit(DIAMOND_TEN);
+            dealer.hit(DIAMOND_EIGHT);
+            dealer.hit(CLOVER_NINE);
 
-            assertThatThrownBy(() -> dealer.hit(CLOVER_FIVE)).isInstanceOf(IllegalStateException.class);
+            assertThat(dealer.canHit()).isFalse();
         }
 
         @DisplayName("Stay상태에서 hit할 수 없다.")
@@ -85,6 +83,18 @@ class DealerTest {
         }
     }
 
+    @DisplayName("22이면 Bust상태이다")
+    @Test
+    void hit_eightEightSix_Bust() {
+        Dealer dealer = new Dealer("maco");
+
+        dealer.hit(CLOVER_EIGHT);
+        dealer.hit(SPADE_EIGHT);
+        State state = dealer.hit(HEART_SIX);
+
+        assertThat(state).isInstanceOf(Bust.class);
+    }
+
     @DisplayName("stay하면 Stay상태이다")
     @Test
     void stay() {
@@ -96,4 +106,17 @@ class DealerTest {
 
         assertThat(dealer.getState()).isInstanceOf(Stay.class);
     }
+
+    @DisplayName("처음 뽑은 카드 두장의 합이 16이면 한장 더 뽑을 수 있다.")
+    @Test
+    void canHit_sixteen_true() {
+        Dealer dealer = new Dealer("maco");
+
+        dealer.hit(DIAMOND_EIGHT);
+        dealer.hit(CLOVER_EIGHT);
+
+        assertThat(dealer.canHit()).isTrue();
+    }
+
+
 }
