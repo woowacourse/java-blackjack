c181fe47384a585934ba13c4b08272baf8636102
Youngho <cyh6099@gmail.com>
2023-03-11 17:08:42 +0900

    test: 플레이어 이름 유효성 검사 테스트 작성

---
+++ src/test/java/domain/participant/NameTest.java
@@ -0,0 +1,35 @@
+package domain.participant;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameTest {
+
+    @ParameterizedTest(name = "플레이어의 이름은 null이거나 공백일 수 없다.")
+    @NullAndEmptySource
+    void validatePlayerNameTest(String name) {
+        assertThatThrownBy(() -> new Name(name));
+    }
+
+    @Test
+    @DisplayName("플레이어의 이름은 '딜러'일 수 없다.")
+    void validateNoDealer() {
+        assertThatThrownBy(() -> new Name("딜러"));
+    }
+
+    @Test
+    @DisplayName("플레이어의 이름에 쉼표(,)가 포함 될 수 없다.")
+    void validateNoComma() {
+        assertThatThrownBy(() -> new Name("이,름"));
+    }
+
+    @Test
+    @DisplayName("플레이어의 이름의 길이가 10이상이 될 수 없다.")
+    void validateNameLength() {
+        assertThatThrownBy(() -> new Name("이건 열 글자 넘는."));
+    }
+}
