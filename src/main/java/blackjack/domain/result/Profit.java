6dece3d98f6cee6062ff8a1ff73befefb42c23da
Miseong Kim <miseongk@naver.com>
2023-03-11 14:03:47 +0900

    feat(Profit): 플레이어 수익 계산하는 기능 구현

---
+++ src/main/java/blackjack/domain/result/Profit.java
@@ -0,0 +1,83 @@
+package blackjack.domain.result;
+
+import blackjack.domain.Money;
+import blackjack.domain.card.Score;
+import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Participants;
+import blackjack.domain.participant.Player;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Profit {
+
+    private final Participants participants;
+    private Map<Player, Money> profit;
+
+    public Profit(Participants participants) {
+        this.participants = participants;
+        initProfit();
+    }
+
+    private void initProfit() {
+        profit = new HashMap<>();
+        for (Player player : participants.getPlayers()) {
+            profit.put(player, Money.init());
+        }
+    }
+
+    public Map<Player, Money> makePlayersProfit() {
+        Dealer dealer = participants.getDealer();
+
+        for (Player player : participants.getPlayers()) {
+            changePlayersProfit(dealer, player);
+        }
+
+        return profit;
+    }
+
+    private void changePlayersProfit(Dealer dealer, Player player) {
+        if (hasState(dealer, player)) {
+            return;
+        }
+        compareScore(dealer.calculateTotalScore(), player);
+    }
+
+    private boolean hasState(Dealer dealer, Player player) {
+        if (isPlayerBlackjack(dealer, player)) {
+            return true;
+        }
+        return isBust(dealer, player);
+    }
+
+    private boolean isPlayerBlackjack(Dealer dealer, Player player) {
+        if (player.isBlackjack() && !dealer.isBlackjack()) {
+            profit.put(player, player.getBettingMoney().getBlackjackPrize());
+            return true;
+        }
+        if (player.isBlackjack() && dealer.isBlackjack()) {
+            profit.put(player, player.getBettingMoney().getBettingPrize());
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isBust(Dealer dealer, Player player) {
+        if (player.isBust()) {
+            profit.put(player, player.getBettingMoney().loseBettingPrize());
+            return true;
+        }
+        if (dealer.isBust()) {
+            profit.put(player, player.getBettingMoney().getBettingPrize());
+            return true;
+        }
+        return false;
+    }
+
+    private void compareScore(final Score dealerScore, final Player player) {
+        if (dealerScore.isLess(player.calculateTotalScore())) {
+            profit.put(player, player.getBettingMoney().getBettingPrize());
+            return;
+        }
+        profit.put(player, player.getBettingMoney().loseBettingPrize());
+    }
+}
---
+++ src/test/java/blackjack/domain/result/ProfitTest.java
@@ -0,0 +1,106 @@
+package blackjack.domain.result;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import blackjack.domain.Money;
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Cards;
+import blackjack.domain.card.Number;
+import blackjack.domain.card.Pattern;
+import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Participant;
+import blackjack.domain.participant.Participants;
+import blackjack.domain.participant.Player;
+import blackjack.domain.participant.Players;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProfitTest {
+
+    private Participants participants;
+    private Profit profit;
+
+    @BeforeEach
+    void setUp() {
+        Players players = Players.from(List.of("a", "b"));
+        Dealer dealer = new Dealer(new Participant(Cards.generateEmptyCards()));
+        participants = new Participants(dealer, players);
+        profit = new Profit(participants);
+        bettingMoney();
+        settingPlayer2();
+    }
+
+    private void bettingMoney() {
+        participants.getPlayers().get(0).betting(10000);
+        participants.getPlayers().get(1).betting(10000);
+    }
+
+    private void settingPlayer2() {
+        // player2 점수 : 20
+        participants.getPlayers().get(1).receiveCard(new Card(Number.K, Pattern.HEART));
+        participants.getPlayers().get(1).receiveCard(new Card(Number.TEN, Pattern.HEART));
+    }
+
+    @Test
+    @DisplayName("플레이어들의 수익을 계산한다 - 플레이어와 딜러 모두 블랙잭인 경우")
+    void makePlayersProfit_blackjack() {
+        // player1 점수 : 21 (블랙잭)
+        participants.getPlayers().get(0).receiveCard(new Card(Number.ACE, Pattern.CLUB));
+        participants.getPlayers().get(0).receiveCard(new Card(Number.TEN, Pattern.CLUB));
+        // dealer 점수 : 21 (블랙잭)
+        participants.getDealer().receiveCard(new Card(Number.ACE, Pattern.DIAMOND));
+        participants.getDealer().receiveCard(new Card(Number.TEN, Pattern.DIAMOND));
+
+        Map<Player, Money> playersProfit = profit.makePlayersProfit();
+        assertThat(playersProfit.get(participants.getPlayers().get(0))).isEqualTo(new Money(10000));
+    }
+
+    @Test
+    @DisplayName("플레이어들의 수익을 계산한다 - 플레이어만 블랙잭인 경우")
+    void makePlayersProfit_playerBlackjack() {
+        // player1 점수 : 21 (블랙잭)
+        participants.getPlayers().get(0).receiveCard(new Card(Number.ACE, Pattern.CLUB));
+        participants.getPlayers().get(0).receiveCard(new Card(Number.TEN, Pattern.CLUB));
+        // dealer 점수 : 19
+        participants.getDealer().receiveCard(new Card(Number.TEN, Pattern.DIAMOND));
+        participants.getDealer().receiveCard(new Card(Number.NINE, Pattern.DIAMOND));
+
+        Map<Player, Money> playersProfit = profit.makePlayersProfit();
+        assertThat(playersProfit.get(participants.getPlayers().get(0))).isEqualTo(new Money(15000));
+    }
+
+    @Test
+    @DisplayName("플레이어들의 수익을 계산한다 - 플레이어가 버스트인 경우")
+    void makePlayersProfit_playerBust() {
+        // player1 점수 : 27 (버스트)
+        participants.getPlayers().get(0).receiveCard(new Card(Number.EIGHT, Pattern.CLUB));
+        participants.getPlayers().get(0).receiveCard(new Card(Number.TEN, Pattern.CLUB));
+        participants.getPlayers().get(0).receiveCard(new Card(Number.NINE, Pattern.CLUB));
+        // dealer 점수 : 19
+        participants.getDealer().receiveCard(new Card(Number.TEN, Pattern.DIAMOND));
+        participants.getDealer().receiveCard(new Card(Number.NINE, Pattern.DIAMOND));
+
+        Map<Player, Money> playersProfit = profit.makePlayersProfit();
+        assertThat(playersProfit.get(participants.getPlayers().get(0))).isEqualTo(new Money(-10000));
+    }
+
+    @Test
+    @DisplayName("플레이어들의 수익을 계산한다 - 딜러가 버스트인 경우")
+    void makePlayersProfit_dealerBust() {
+        // player1 점수 : 18
+        participants.getPlayers().get(0).receiveCard(new Card(Number.EIGHT, Pattern.CLUB));
+        participants.getPlayers().get(0).receiveCard(new Card(Number.TEN, Pattern.CLUB));
+        // dealer 점수 : 24 (버스트)
+        participants.getDealer().receiveCard(new Card(Number.TEN, Pattern.DIAMOND));
+        participants.getDealer().receiveCard(new Card(Number.SIX, Pattern.DIAMOND));
+        participants.getDealer().receiveCard(new Card(Number.EIGHT, Pattern.DIAMOND));
+
+        Map<Player, Money> playersProfit = profit.makePlayersProfit();
+        assertThat(playersProfit.get(participants.getPlayers().get(0))).isEqualTo(new Money(10000));
+    }
+}
