9bcaec640e77aa8450f20926ed8840370301fe04
Youngho <cyh6099@gmail.com>
2023-03-11 19:08:50 +0900

    feat: 예외 테스트에서 예외 클래스 확인하도록 수정

--- src/test/java/domain/participant/NameTest.java
+++ src/test/java/domain/participant/NameTest.java
@@ -12,24 +12,21 @@ class NameTest {
     @ParameterizedTest(name = "플레이어의 이름은 null이거나 공백일 수 없다.")
     @NullAndEmptySource
     void validatePlayerNameTest(String name) {
-        assertThatThrownBy(() -> new Name(name));
-    }
-
-    @Test
-    @DisplayName("플레이어의 이름은 '딜러'일 수 없다.")
-    void validateNoDealer() {
-        assertThatThrownBy(() -> new Name("딜러"));
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("플레이어의 이름에 쉼표(,)가 포함 될 수 없다.")
     void validateNoComma() {
-        assertThatThrownBy(() -> new Name("이,름"));
+        assertThatThrownBy(() -> new Name("이,름"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("플레이어의 이름의 길이가 10이상이 될 수 없다.")
     void validateNameLength() {
-        assertThatThrownBy(() -> new Name("이건 열 글자 넘는."));
+        assertThatThrownBy(() -> new Name("이건 열 글자 넘는."))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
