8b5b9e1c0ed40dbb1a46fff65245b892ff4cb9f8
0chil <0@chll.it>
2023-03-11 15:55:21 +0900

    feat(Bank): 결과에 따라 배당을 주어 반환한다

--- src/main/java/domain/Result.java
+++ src/main/java/domain/Result.java
@@ -2,5 +2,15 @@ package domain;
 
 public enum Result {
 
-    DRAW, WIN, LOSE
+    DRAW(0), WIN(1), BLACKJACK(1.5), LOSE(-1);
+
+    private final double dividend;
+
+    Result(double dividend) {
+        this.dividend = dividend;
+    }
+
+    public double getDividend() {
+        return dividend;
+    }
 }
--- src/test/java/domain/BankTest.java
+++ src/test/java/domain/BankTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class BankTest {
 
@@ -19,7 +21,6 @@ public class BankTest {
         assertThat(bank.pay(user, Result.WIN)).isEqualTo(Money.of(246));
     }
 
-
     @Test
     @DisplayName("100원이상 배팅해야 한다")
     void test_bet_under_100_throws() {
@@ -28,5 +29,17 @@ public class BankTest {
 
         assertThatThrownBy(() -> bank.bet(user, Money.of(99)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("결과에 따라 배당을 주어 반환한다")
+    @ParameterizedTest(name = "{0}이면 총 {1}원이 반환된다")
+    @CsvSource({"BLACKJACK,1250", "WIN,1000", "LOSE,0", "DRAW,500"})
+    void test_pay_by_result(Result result, int expectedValue) {
+        var bank = new Bank();
+        var user = new User("gd");
+
+        bank.bet(user, Money.of(500));
+
+        assertThat(bank.pay(user, result)).isEqualTo(Money.of(expectedValue));
     }
 }
