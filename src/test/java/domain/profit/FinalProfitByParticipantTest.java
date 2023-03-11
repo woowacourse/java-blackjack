0c8fa2b3ad8641d601766853d2537db48c61b657
sh111-coder <ohk9134@naver.com>
2023-03-11 18:37:13 +0900

    test(FinalProfitByParticipantTest): 딜러의 최종 수익 계산 테스트

---
+++ src/test/java/domain/profit/FinalProfitByParticipantTest.java
@@ -0,0 +1,36 @@
+package domain.profit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import domain.betting.BettingMoney;
+import domain.card.Card;
+import domain.card.Denomination;
+import domain.card.Suit;
+import domain.participant.Hand;
+import domain.participant.Name;
+import domain.participant.Player;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FinalProfitByParticipantTest {
+
+    @Test
+    @DisplayName("딜러의 최종 수익을 계산한다.")
+    void calculateDealerFinalProfit() {
+        // given
+        Player player = new Player(
+                new Name("seongha"),
+                new Hand(List.of(new Card(Suit.SPADE, Denomination.SIX), new Card(Suit.DIAMOND, Denomination.TEN))));
+        BettingMoney bettingMoney = new BettingMoney(10000);
+
+        // when
+        FinalProfitByParticipant finalProfitByParticipant = new FinalProfitByParticipant();
+        finalProfitByParticipant.putParticipantFinalProfit(player, new FinalProfit(bettingMoney.getValue()));
+
+        // then
+        Assertions.assertThat(finalProfitByParticipant.calculateDealerFinalProfit()).isEqualTo(-10000.0);
+    }
+}
