c146c63023ff2e74592f6403eb285370fa6635c7
echo <echo.backend@gmail.com>
2023-03-11 11:06:31 +0900

    refactor: OutputView 리펙토링

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,7 +3,7 @@ package view;
 import domain.card.Card;
 import domain.card.Hand;
 import domain.result.ResultStatus;
-import domain.user.Participants;
+import domain.user.GameMember;
 import domain.user.Playable;
 import java.util.List;
 import java.util.Map;
@@ -14,15 +14,13 @@ public class OutputView {
     public static final String FINAL_STATUS_MESSAGE = "## 최종 승패";
     public static final String DEALER_GET_CARD_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
     
-    public static void printReadyMessage(Participants participants) {
-        List<String> participantNames = participants.stream()
+    public static void printReadyMessage(GameMember gameMember) {
+        List<String> playerNames = gameMember.getPlayers().stream()
                 .map(Playable::getName)
                 .collect(Collectors.toList());
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("딜러와 ");
-        List<String> playerNames = participantNames.subList(1, participantNames.size());
-        stringBuilder.append(String.join(", ", playerNames));
-        stringBuilder.append("에게 2장을 나누었습니다.");
+        final String stringBuilder = "딜러와 "
+                + String.join(", ", playerNames)
+                + "에게 2장을 나누었습니다.";
         System.out.println(stringBuilder);
     }
     
@@ -51,17 +49,15 @@ public class OutputView {
     }
     
     
-    public static void printReadyParticipantsNameAndCards(Participants participants) {
-        participants.forEach(
+    public static void printReadyParticipantsNameAndCards(GameMember gameMember) {
+        gameMember.forEach(
                 (participant) -> OutputView.printNameAndCards(participant.getName(),
                         participant.getReadyCards()));
         System.out.println();
     }
     
     public static void printNameAndCards(String name, Hand cards) {
-        String formattedNameAndCards = name
-                + ": "
-                + cards.stream()
+        String formattedNameAndCards = name + ": " + cards.stream()
                 .map(OutputView::formatCard)
                 .collect(Collectors.joining(", "));
         System.out.println(formattedNameAndCards);
@@ -74,24 +70,26 @@ public class OutputView {
     
     public static void printPlayersGameResult(final Map<Playable, ResultStatus> resultMap) {
         System.out.println(System.lineSeparator() + FINAL_STATUS_MESSAGE);
-        resultMap.forEach((player, result) -> {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(player.getName());
-            stringBuilder.append(": ");
-            if (result == ResultStatus.WIN || result == ResultStatus.WIN_BLACKJACK) {
-                stringBuilder.append("승");
-            } else if (result == ResultStatus.LOSE) {
-                stringBuilder.append("패");
-            } else {
-                stringBuilder.append("무");
-            }
-            System.out.println(stringBuilder);
-        });
+        resultMap.forEach(OutputView::printPlayerStatusResult);
     }
     
-    public static void printParticipantsNameCardsAndScore(final Participants participants) {
+    private static void printPlayerStatusResult(final Playable player, final ResultStatus result) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(player.getName());
+        stringBuilder.append(": ");
+        if (result == ResultStatus.WIN || result == ResultStatus.WIN_BLACKJACK) {
+            stringBuilder.append("승");
+        } else if (result == ResultStatus.LOSE) {
+            stringBuilder.append("패");
+        } else {
+            stringBuilder.append("무");
+        }
+        System.out.println(stringBuilder);
+    }
+    
+    public static void printParticipantsNameCardsAndScore(final GameMember gameMember) {
         System.out.println();
-        participants.forEach((participant) -> OutputView.printNameCardsScore(participant.getName(),
+        gameMember.forEach((participant) -> OutputView.printNameCardsScore(participant.getName(),
                 participant.getCards(), participant.getScore()));
         System.out.println();
     }
