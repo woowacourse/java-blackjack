e85b4b17daa491a3260cb4c8d133c1416696d1eb
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-03-11 12:44:05 +0900

    feat: 배팅 금액 생성 로직 구현

---
+++ src/main/java/money/BetAmount.java
@@ -0,0 +1,29 @@
+package money;
+
+public class BetAmount {
+    private static final String NOT_INTEGER = "배팅 금액은 정수만 가능합니다.";
+    private static final String UNDER_MIN_AMOUNT = "배팅 금액은 0보다 커야 합니다.";
+    private static final int MIN_AMOUNT = 0;
+
+    private final int amount;
+
+    private BetAmount(int amount) {
+        validateMinAmount(amount);
+        this.amount = amount;
+    }
+
+    public static BetAmount from(String input) {
+        try {
+            int amount = Integer.parseInt(input);
+            return new BetAmount(amount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_INTEGER);
+        }
+    }
+
+    private void validateMinAmount(int amount) {
+        if (amount < MIN_AMOUNT) {
+            throw new IllegalArgumentException(UNDER_MIN_AMOUNT);
+        }
+    }
+}
--- src/test/java/money/BetAmountTest.java
+++ src/test/java/money/BetAmountTest.java
@@ -1,14 +1,15 @@
 package money;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class MoneyTest {
+public class BetAmountTest {
     @ParameterizedTest(name = "배팅 금액은 1보다 큰 정수이어야 한다.")
     @ValueSource(strings = {"천원", "-1999", "1000.24"})
     void createMoneyFailTest(String input) {
-        Assertions.assertThatThrownBy(() -> new Money(input))
+        assertThatThrownBy(() -> BetAmount.from(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
