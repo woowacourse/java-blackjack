9b5ea1818a6f1702574c96e957ebb5add3a4f971
jyeost <pig9619@naver.com>
2023-03-13 08:34:38 +0900

    feat: 카드를 캐싱하도록 함

--- src/main/java/blackjackgame/domain/card/Card.java
+++ src/main/java/blackjackgame/domain/card/Card.java
@@ -1,12 +1,25 @@
 package blackjackgame.domain.card;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Card {
+    private final static Map<String, Card> cache = new HashMap<>(52);
+
     private final Symbol symbol;
     private final CardValue cardValue;
 
-    public Card(final Symbol symbol, final CardValue cardValue) {
+    private Card(final Symbol symbol, final CardValue cardValue) {
         this.symbol = symbol;
         this.cardValue = cardValue;
+    }
+
+    public static Card of(Symbol symbol, CardValue cardValue) {
+        return cache.computeIfAbsent(toKey(symbol, cardValue), ignore -> new Card(symbol, cardValue));
+    }
+
+    private static String toKey(Symbol symbol, CardValue cardValue) {
+        return symbol.getSymbol() + cardValue.getScore();
     }
 
     public String getSymbol() {
