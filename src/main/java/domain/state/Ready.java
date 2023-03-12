4520d4ac5d042e7aa4a9f2c8280119611844d8f6
MoonJeWoong <munjin0201@naver.com>
2023-03-12 16:27:22 +0900

    feat: Ready 상태에서 드로우 기능 구현

--- src/main/java/domain/state/Ready.java
+++ src/main/java/domain/state/Ready.java
@@ -5,13 +5,26 @@ import domain.card.Hand;
 
 import java.util.List;
 
-public final class Ready implements State{
+public final class Ready implements State {
     private final Hand hand;
 
     public Ready() {
         this.hand = new Hand();
     }
 
+    public Ready(Hand hand) {
+        this.hand = hand;
+    }
+
+    @Override
+    public State draw(Card card) {
+        if (hand.isEmpty()) {
+            return new Ready(hand.add(card));
+        }
+        return new Hit(hand.add(card));
+    }
+
+    @Override
     public List<Card> getCards() {
         return hand.getCards();
     }
