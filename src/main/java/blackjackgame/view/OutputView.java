4cfc4da0d145facc41787ebfb0d106a93c90db38
yujamint <woawoa0513@gmail.com>
2023-03-12 18:10:39 +0900

    style: 출력 형식 개선

--- src/main/java/blackjackgame/view/OutputView.java
+++ src/main/java/blackjackgame/view/OutputView.java
@@ -15,7 +15,7 @@ public class OutputView {
     private static final String INPUT_PLAYER_BET_AMOUNT_MESSAGE_FORMAT = "%n%s의 배팅 금액은?%n";
     private static final String ASK_ONE_MORE_CARD_MESSAGE_FORMAT = "%s은(는) 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n) %n";
     private static final String SETUP_COMPLETE_MESSAGE_FORMAT = "%n딜러와 %s에게 2장을 나누었습니다.%n";
-    private static final String DEALER_DRAW_RESULT_MESSAGE_FORMAT = "%n 딜러는 16이하라 %d장의 카드를 더 받았습니다.%n%n";
+    private static final String DEALER_DRAW_RESULT_MESSAGE_FORMAT = "%n딜러는 16이하라 %d장의 카드를 더 받았습니다.%n";
 
     private static final String INPUT_PLAYER_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String FINAL_RESULT_NOTICE_MESSAGE = "## 최종 수익";
