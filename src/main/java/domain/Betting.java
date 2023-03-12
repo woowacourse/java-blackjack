7188400621429a9e88d53b15baacb6c977d60241
이건회 <rg970604@naver.com>
2023-03-12 17:54:10 +0900

    refactor : 베팅 금액 최소 조건

--- src/main/java/domain/Betting.java
+++ src/main/java/domain/Betting.java
@@ -2,13 +2,21 @@ package domain;
 
 public class Betting {
 
+    private static final int MINIMUM_MONEY = 1;
     private final Integer bettingMoney;
     private final Integer profitMoney;
 
 
     public Betting(Integer bettingMoney, Integer profitMoney) {
+        validateBettingInput(bettingMoney);
         this.bettingMoney = bettingMoney;
         this.profitMoney = profitMoney;
+    }
+
+    private static void validateBettingInput(Integer bettingMoney) {
+        if (bettingMoney < MINIMUM_MONEY) {
+            throw new IllegalArgumentException("베팅금액은 0원 초과 금액이여야 합니다. 입력한 숫자 : " + bettingMoney);
+        }
     }
 
     public Betting giveBonus() {
@@ -18,7 +26,7 @@ public class Betting {
 
     public Betting calculateProfitByResult(Result result) {
         if (result == Result.LOSE) {
-            return new Betting(bettingMoney, profitMoney + (bettingMoney*2));
+            return new Betting(bettingMoney, profitMoney + (bettingMoney * 2));
         }
         if (result == Result.DRAW) {
             return new Betting(bettingMoney, bettingMoney);
