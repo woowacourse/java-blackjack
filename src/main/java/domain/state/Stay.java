25bf1d0ce7af08abe1e22915e3cb9184e7b64d03
MoonJeWoong <munjin0201@naver.com>
2023-03-12 17:17:26 +0900

    feat: Stay 상태 기능 구현

---
+++ src/main/java/domain/state/Stay.java
@@ -0,0 +1,20 @@
+package domain.state;
+
+import domain.card.Card;
+import domain.card.Hand;
+
+public class Stay extends State {
+    public Stay(Hand hand) {
+        super(hand);
+    }
+
+    @Override
+    public State draw(Card card) {
+        throw new IllegalStateException("[ERROR] 게임이 종료되어 카드를 뽑을 수 없습니다.");
+    }
+
+    @Override
+    public State stay() {
+        throw new IllegalStateException("[ERROR] 게임이 종료되어 상태를 조작할 수 없습니다.");
+    }
+}
