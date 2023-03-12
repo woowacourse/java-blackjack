92d6bb6945efbddf4baf460e0ae98c7c48ec7427
waterricecake <fruturum@nate.com>
2023-03-13 01:34:42 +0900

    test(ResultGameTest): 플레이어에게 배팅금을 계산하는 테스트

--- src/test/java/blackjack/domain/game/ResultGameTest.java
+++ src/test/java/blackjack/domain/game/ResultGameTest.java
@@ -4,7 +4,6 @@ import blackjack.domain.card.Card;
 import blackjack.domain.card.Letter;
 import blackjack.domain.card.Shape;
 import blackjack.domain.participant.Dealer;
-import blackjack.domain.participant.Name;
 import blackjack.domain.participant.Participants;
 import blackjack.domain.participant.Player;
 import org.assertj.core.api.Assertions;
@@ -13,7 +12,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -28,9 +26,9 @@ public class ResultGameTest {
     void setting() {
         dealer = new Dealer();
         Map<String,Integer> players = new HashMap<>();
-        players.put("pobi",0);
-        players.put("crong",0);
-        players.put("dali",0);
+        players.put("pobi",1000);
+        players.put("crong",1000);
+        players.put("dali",1000);
         participants = new Participants(dealer, players);
     }
 
@@ -40,24 +38,9 @@ public class ResultGameTest {
         assertThatNoException().isThrownBy(() -> new ResultGame(participants));
     }
 
-    @Test
-    @DisplayName("플레이어의 결과를 출력하는 테스트")
-    void getPlayerResultTest() {
-        // given
-        ResultGame resultGame = new ResultGame(participants);
-        dealer.drawCard(new Card(Shape.CLOVER, Letter.EIGHT));
-        Player player = participants.getPlayers().get(0);
-        player.drawCard(new Card(Shape.CLOVER, Letter.NINE));
-
-        // when
-        resultGame.calculateResult();
-
-        // then
-        assertThat(resultGame.getPlayerResult(player)).isEqualTo(WinTieLose.WIN);
-    }
 
     @Test
-    @DisplayName("버스트된 딜러와 버스트 안된 참가자의 게임 결과를 입력하는 테스트")
+    @DisplayName("참가자가 이긴경우")
     void calculateResultWithBustedDealerTest() {
 
         dealer.drawCard(new Card(Shape.CLOVER, Letter.TEN));
@@ -69,11 +52,11 @@ public class ResultGameTest {
         ResultGame resultGame = new ResultGame(participants);
         resultGame.calculateResult();
 
-        assertThat(resultGame.getPlayerResult(player)).isEqualTo(WinTieLose.WIN);
+        assertThat(participants.getPlayers().get(0).getRevenue()).isEqualTo(1000);
     }
 
     @Test
-    @DisplayName("버스트된 딜러와 버스트 된 참가자의 게임 결과를 입력하는 테스트")
+    @DisplayName("딜러와 비기는 경우")
     void calculateResultWithBustedDealerPlayerTest() {
 
         dealer.drawCard(new Card(Shape.CLOVER, Letter.TEN));
@@ -86,12 +69,11 @@ public class ResultGameTest {
         player.drawCard(new Card(Shape.DIAMOND, Letter.QUEEN));
         ResultGame resultGame = new ResultGame(participants);
         resultGame.calculateResult();
-
-        assertThat(resultGame.getPlayerResult(player)).isEqualTo(WinTieLose.TIE);
+        assertThat(participants.getPlayers().get(0).getRevenue()).isEqualTo(0);
     }
 
     @Test
-    @DisplayName("딜러와 버스트 된 참가자의 게임 결과를 입력하는 테스트")
+    @DisplayName("딜러가 이기는 경우")
     void calculateResultWithNonBustedDealerAndBustedPlayerTest() {
 
         dealer.drawCard(new Card(Shape.CLOVER, Letter.TEN));
@@ -103,56 +85,55 @@ public class ResultGameTest {
         player.drawCard(new Card(Shape.DIAMOND, Letter.QUEEN));
         ResultGame resultGame = new ResultGame(participants);
         resultGame.calculateResult();
-
-        assertThat(resultGame.getPlayerResult(player)).isEqualTo(WinTieLose.LOSE);
+        assertThat(participants.getPlayers().get(0).getRevenue()).isEqualTo(-1000);
     }
 
     @Test
-    @DisplayName("버스트 안된 딜러와 참가자 중 딜러가 이기는 경우 테스트")
+    @DisplayName("딜러와 플레이어 둘다 블랙잭인 경우")
     void calculateResultWithNonBustedDealerWinNonBustedPlayerTest() {
 
         dealer.drawCard(new Card(Shape.CLOVER, Letter.TEN));
         dealer.drawCard(new Card(Shape.DIAMOND, Letter.ACE));
 
         Player player = participants.getPlayers().get(0);
-        player.drawCard(new Card(Shape.CLOVER, Letter.JACK));
+        player.drawCard(new Card(Shape.CLOVER, Letter.ACE));
         player.drawCard(new Card(Shape.DIAMOND, Letter.QUEEN));
         ResultGame resultGame = new ResultGame(participants);
         resultGame.calculateResult();
-
-        assertThat(resultGame.getPlayerResult(player)).isEqualTo(WinTieLose.LOSE);
+        assertThat(participants.getPlayers().get(0).getRevenue()).isEqualTo(0);
     }
 
     @Test
-    @DisplayName("버스트 안된 딜러와 참가자 중 참가자가 이기는 경우 테스트")
+    @DisplayName("딜러가 블랙젝인 경우 플레이어는 3장 합 21인 경우")
     void calculateResultWithNonBustedDealerLoseNonBustedPlayerTest() {
-
-        dealer.drawCard(new Card(Shape.CLOVER, Letter.TEN));
-        dealer.drawCard(new Card(Shape.DIAMOND, Letter.JACK));
-
-        Player player = participants.getPlayers().get(0);
-        player.drawCard(new Card(Shape.CLOVER, Letter.JACK));
-        player.drawCard(new Card(Shape.DIAMOND, Letter.ACE));
-        ResultGame resultGame = new ResultGame(participants);
-        resultGame.calculateResult();
-
-        assertThat(resultGame.getPlayerResult(player)).isEqualTo(WinTieLose.WIN);
-    }
-
-    @Test
-    @DisplayName("버스트 안된 딜러와 참가자 중 비기는 경우 테스트")
-    void calculateResultWithNonBustedDealerTieNonBustedPlayerTest() {
 
         dealer.drawCard(new Card(Shape.CLOVER, Letter.ACE));
         dealer.drawCard(new Card(Shape.DIAMOND, Letter.JACK));
 
         Player player = participants.getPlayers().get(0);
         player.drawCard(new Card(Shape.CLOVER, Letter.JACK));
-        player.drawCard(new Card(Shape.DIAMOND, Letter.ACE));
+        player.drawCard(new Card(Shape.DIAMOND, Letter.TEN));
+        player.drawCard(new Card(Shape.HEART,Letter.ACE));
         ResultGame resultGame = new ResultGame(participants);
         resultGame.calculateResult();
 
-        assertThat(resultGame.getPlayerResult(player)).isEqualTo(WinTieLose.TIE);
+        assertThat(participants.getPlayers().get(0).getRevenue()).isEqualTo(-1000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 블랙잭 딜러가 3잡 합 21인 경우")
+    void calculateResultWithNonBustedDealerTieNonBustedPlayerTest() {
+        Player player = participants.getPlayers().get(0);
+        player.drawCard(new Card(Shape.CLOVER, Letter.ACE));
+        player.drawCard(new Card(Shape.DIAMOND, Letter.JACK));
+
+        dealer.drawCard(new Card(Shape.CLOVER, Letter.JACK));
+        dealer.drawCard(new Card(Shape.DIAMOND, Letter.TEN));
+        dealer.drawCard(new Card(Shape.HEART,Letter.ACE));
+        ResultGame resultGame = new ResultGame(participants);
+        resultGame.calculateResult();
+
+        assertThat(participants.getPlayers().get(0).getRevenue()).isEqualTo(1500);
     }
 
     @Test
@@ -165,4 +146,5 @@ public class ResultGameTest {
                 .contains(participants.getPlayers().get(0)
                         , participants.getPlayers().get(1));
     }
+
 }
