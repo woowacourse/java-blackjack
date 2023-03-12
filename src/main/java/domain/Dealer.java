c456631e3b2e8625e7109d40ffabefabb0b56025
the9kim <96895686+the9kim@users.noreply.github.com>
2023-03-12 10:49:27 +0900

    feat(Dealer): 딜러를 생성한다

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -1,8 +1,33 @@
 package domain;
 
-public class Dealer extends Participant{
+import java.util.List;
 
-//    public static final int STANDARD_FOR_HIT = 16;
+public class Dealer {
+
+    public static final int STANDARD_TO_GET_ONE_MORE_CARD = 16;
+
+    private final Name name;
+    private final Cards cards;
+
+    public Dealer(Name name, Cards cards) {
+        this.name = name;
+        this.cards = cards;
+    }
+
+    public boolean isWhetherToGetMoreCard() {
+        return cards.calculateSum() <= STANDARD_TO_GET_ONE_MORE_CARD;
+    }
+
+    public void pickCard(CardDeck cardDeck) {
+        Card card = cardDeck.pickCard();
+        cards.addCard(card);
+    }
+
+    public List<Card> getCards() {
+        return cards.getCards();
+    }
+
+    //    public static final int STANDARD_FOR_HIT = 16;
 //
 //    public Dealer(Cards cards) {
 //        this("딜러", cards);
--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -7,9 +7,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class DealerTest {
-//
+
+    @Test
+    @DisplayName("초기 카드 두장을 파라미터로 입력하여 딜러 객체가 정상적으로 생성된다")
+    void generateDealer() {
+        Name name = Name.generateDealerName();
+        Cards cards = new Cards(List.of(new Card(CardType.SPADE, CardValue.JACK), new Card(CardType.HEART, CardValue.EIGHT)));
+
+        assertDoesNotThrow(() -> new Dealer(name, cards));
+    }
+
+    @Test
+    @DisplayName("딜러 카드의 합이 16이하일 경우 true를 리턴한다")
+    void checkToGetOneMoreCard() {
+        Name name = Name.generateDealerName();
+        Cards cards = new Cards(List.of(new Card(CardType.SPADE, CardValue.SEVEN), new Card(CardType.HEART, CardValue.EIGHT)));
+
+        Dealer dealer = new Dealer(name, cards);
+
+        assertThat(dealer.isWhetherToGetMoreCard()).isTrue();
+    }
+
+    @Test
+    @DisplayName("딜러 카드의 합이 16을 초과할 경우 false를 리턴한다")
+    void checkNotToGetOneMoreCard() {
+        Name name = Name.generateDealerName();
+        Cards cards = new Cards(List.of(new Card(CardType.SPADE, CardValue.TEN), new Card(CardType.HEART, CardValue.EIGHT)));
+
+        Dealer dealer = new Dealer(name, cards);
+
+        assertThat(dealer.isWhetherToGetMoreCard()).isFalse();
+    }
+
+    @Test
+    @DisplayName("기존의 카드 그룹에 새로운 카드를 추가하면 카드의 개수가 1만큼 증가한다")
+    void pickCard() {
+        Name name = Name.generateDealerName();
+        Cards cards = new Cards(new ArrayList<>(List.of(new Card(CardType.SPADE, CardValue.SEVEN), new Card(CardType.HEART, CardValue.EIGHT))));
+        Dealer dealer = new Dealer(name, cards);
+        CardDeck cardDeck = CardDeck.generateCardDeck();
+        int expectedCardSize = 3;
+
+        dealer.pickCard(cardDeck);
+
+        assertThat(dealer.getCards().size()).isEqualTo(expectedCardSize);
+    }
+
+    //
 //    @Test
 //    @DisplayName("딜러의 카드가 16이하라면 카드를 한장 더 받을 수 있다")
 //    void canAddCardTrue() {
