1095f7c171ccfb78d4fbe118a5c49fdf6ab57ddd
jaehee329 <jjhs9803@gmail.com>
2023-03-13 00:46:46 +0900

    test(PlayerResult): 베팅 관련 테스트 구현

--- src/test/java/domain/TestData.java
+++ src/test/java/domain/TestData.java
@@ -12,16 +12,7 @@ public class TestData {
 		List<Card> cards = List.of(
 			new Card(Suit.DIAMOND, Denomination.NINE),
 			new Card(Suit.HEART, Denomination.TEN));
-		Player player = new Player("playerScore19", 0);
-		cards.forEach(player::addCard);
-		return player;
-	}
-
-	public static Player getScore21Player() {
-		List<Card> cards = List.of(
-			new Card(Suit.DIAMOND, Denomination.ACE),
-			new Card(Suit.HEART, Denomination.TEN));
-		Player player = new Player("playerScore21", 0);
+		Player player = new Player("playerScore19", 19);
 		cards.forEach(player::addCard);
 		return player;
 	}
@@ -30,9 +21,47 @@ public class TestData {
 		List<Card> cards = List.of(
 			new Card(Suit.DIAMOND, Denomination.TEN),
 			new Card(Suit.HEART, Denomination.TEN));
-		Player player = new Player("playerScore20", 0);
+		Player player = new Player("playerScore20", 20);
 		cards.forEach(player::addCard);
 		return player;
+	}
+
+	public static Player getScore21Player() {
+		List<Card> cards = List.of(
+			new Card(Suit.DIAMOND, Denomination.TEN),
+			new Card(Suit.HEART, Denomination.TEN));
+		Player player = new Player("playerScore21", 21);
+		cards.forEach(player::addCard);
+		player.addCard(new Card(Suit.CLOVER, Denomination.ACE));
+		return player;
+	}
+
+	public static Player getBlackJackPlayer() {
+		List<Card> cards = List.of(
+			new Card(Suit.DIAMOND, Denomination.ACE),
+			new Card(Suit.HEART, Denomination.TEN));
+		Player player = new Player("playerBlackJack", 21);
+		cards.forEach(player::addCard);
+		return player;
+	}
+
+	public static Player getBustPlayer() {
+		List<Card> cards = List.of(
+			new Card(Suit.DIAMOND, Denomination.TEN),
+			new Card(Suit.HEART, Denomination.TEN));
+		Player player = new Player("playerBust", 22);
+		cards.forEach(player::addCard);
+		player.addCard(new Card(Suit.CLOVER, Denomination.TWO));
+		return player;
+	}
+
+	public static Dealer getScore19Dealer() {
+		List<Card> cards = List.of(
+			new Card(Suit.DIAMOND, Denomination.NINE),
+			new Card(Suit.HEART, Denomination.TEN));
+		Dealer dealer = new Dealer();
+		cards.forEach(dealer::addCard);
+		return dealer;
 	}
 
 	public static Dealer getScore20Dealer() {
@@ -41,6 +70,25 @@ public class TestData {
 			new Card(Suit.HEART, Denomination.TEN));
 		Dealer dealer = new Dealer();
 		cards.forEach(dealer::addCard);
+		return dealer;
+	}
+
+	public static Dealer getBlackJackDealer() {
+		List<Card> cards = List.of(
+			new Card(Suit.DIAMOND, Denomination.ACE),
+			new Card(Suit.HEART, Denomination.TEN));
+		Dealer dealer = new Dealer();
+		cards.forEach(dealer::addCard);
+		return dealer;
+	}
+
+	public static Dealer getBustDealer() {
+		List<Card> cards = List.of(
+			new Card(Suit.DIAMOND, Denomination.TEN),
+			new Card(Suit.HEART, Denomination.TEN));
+		Dealer dealer = new Dealer();
+		cards.forEach(dealer::addCard);
+		dealer.addCard(new Card(Suit.CLOVER, Denomination.TWO));
 		return dealer;
 	}
 }
