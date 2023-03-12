0fc22ef703c3bc9b57c4f9cab1fe948790bcfa28
Eunkeee <eunkeeeee@gmail.com>
2023-03-13 00:55:16 +0900

    test(PlayerCommand): assertAll 적용

--- src/test/java/domain/PlayerCommandTest.java
+++ src/test/java/domain/PlayerCommandTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,9 @@ class PlayerCommandTest {
     @DisplayName("y를 입력하면 HIT, n을 입력하면 STAND로 변환할 수 있다.")
     @Test
     void convertCommandTest() {
-        assertThat(PlayerCommand.from(true)).isEqualTo(PlayerCommand.HIT);
-        assertThat(PlayerCommand.from(false)).isEqualTo(PlayerCommand.STAND);
+        assertAll(
+                () -> assertThat(PlayerCommand.from(true)).isEqualTo(PlayerCommand.HIT),
+                () -> assertThat(PlayerCommand.from(false)).isEqualTo(PlayerCommand.STAND)
+        );
     }
 }
