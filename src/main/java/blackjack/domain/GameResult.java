7f430005e91bee638355ce2b7e60e62c172d5066
Cyma-s <ssol0319@naver.com>
2023-03-12 11:56:53 +0900

    feat: 딜러 수익 계산 기능 추가

---
+++ src/main/java/blackjack/domain/GameResult.java
@@ -0,0 +1,29 @@
+package blackjack.domain;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class GameResult {
+    private final Map<Player, ResultType> bettingResult;
+
+    public GameResult(final Dealer dealer, final List<Player> players) {
+        this.bettingResult = new LinkedHashMap<>();
+        players.forEach(player -> bettingResult.put(player, dealer.judgePlayerResult(player)));
+    }
+
+    public int getDealerRevenue() {
+        return bettingResult.entrySet().stream()
+                .map(getReverseBettingMoney())
+                .reduce(0, Integer::sum);
+    }
+
+    private Function<Map.Entry<Player, ResultType>, Integer> getReverseBettingMoney() {
+        return entry -> ResultType.getReverseType(entry.getValue()).getRevenue(entry.getKey().getBettingMoney());
+    }
+
+    public Map<Player, ResultType> getBettingResult() {
+        return bettingResult;
+    }
+}
