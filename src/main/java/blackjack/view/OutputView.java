dd08e341aeb1bb6a9a863b2cef7a3d4e2028cade
kim0914 <kosa0914@naver.com>
2023-03-12 17:06:35 +0900

    feat(OutputView): 최종 수익을 보여주는 메서드 추가

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -2,13 +2,12 @@ package blackjack.view;
 
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Hand;
-import blackjack.domain.game.Result;
 import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Player;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class OutputView {
@@ -23,7 +22,7 @@ public class OutputView {
     private static final String DEALER_POSSIBLE_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
     private static final String DEALER_IMPOSSIBLE_MESSAGE = "딜러는 17이상이라 카드를 받지 못합니다.";
     private static final String RESULT_MESSAGE = " - 결과: ";
-    private static final String FINAL_RESULT = "## 최종 승패";
+    private static final String FINAL_RESULT = "## 최종 수익";
 
     public void showInitStatus(List<Player> players) {
         printMessage(LINE_SEPARATOR);
@@ -35,7 +34,7 @@ public class OutputView {
     }
 
     public void showDealerFirstCard(Card card) {
-        System.out.println(DEALER_NAME + DELIMITER + mapCard(card));
+        printMessage(DEALER_NAME + DELIMITER + mapCard(card));
     }
 
     public void showPlayer(Player player) {
@@ -70,12 +69,14 @@ public class OutputView {
         }
     }
 
-    public void showFinalResult(Map<Player, Result> results, List<Player> players) {
+    public void showProfits(Map<String, Integer> results, int dealerProfit) {
         printMessage(LINE_SEPARATOR);
         printMessage(FINAL_RESULT);
-        printMessage(DEALER_NAME + DELIMITER + ResultMapper.getDealerResult(new ArrayList<>(results.values())));
-        for (Player player : players) {
-            printMessage(player.getName() + DELIMITER + ResultMapper.map(results.get(player)));
+        printMessage(DEALER_NAME + DELIMITER + dealerProfit);
+        Set<Map.Entry<String, Integer>> entries = results.entrySet();
+
+        for (Map.Entry<String, Integer> entry : entries) {
+            printMessage(entry.getKey() + DELIMITER + entry.getValue());
         }
     }
 
