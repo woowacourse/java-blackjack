ca1ff012d2bc860aafe4ced0f97a77efab163e35
songusika <sws981202@gmail.com>
2023-03-12 12:33:37 +0900

    refactor: 이름 검증 기능 분리
    Co-authored-by: choi-jjunho <junho5336@gmail.com>

--- src/main/java/blackjack/domain/user/Player.java
+++ src/main/java/blackjack/domain/user/Player.java
@@ -1,10 +1,35 @@
 package blackjack.domain.user;
 
-import blackjack.domain.user.name.PlayerName;
-
 public class Player extends User {
 
-    public Player(final PlayerName name) {
+    public Player(final String name) {
         super(name);
+        validationName();
     }
+
+    private void validationName() {
+        validateBlank(getName());
+        validateLength(getName());
+        validateReservedWord(getName());
+    }
+
+    private static void validateReservedWord(final String name) {
+        if (DEALER_NAME.equals(name)) {
+            throw new IllegalArgumentException("사용자 이름은 딜러일 수 없습니다.");
+        }
+    }
+
+    private static void validateLength(final String name) {
+        if (name.length() > 10) {
+            throw new IllegalArgumentException("사용자 이름은 10자 이하여야 합니다.");
+        }
+    }
+
+    private static void validateBlank(final String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("사용자 이름은 한글자 이상이여야 합니다.");
+        }
+    }
+
+
 }
