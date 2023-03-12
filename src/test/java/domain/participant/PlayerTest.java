204f1c476e10a9215f90c4a641af486273947af9
sh111-coder <ohk9134@naver.com>
2023-03-13 01:23:11 +0900

    test(PlayerTest): 플레이어와 딜러의 상태를 비교하여 배팅 결과를 반환하는 기능 테스트

--- src/test/java/domain/participant/PlayerTest.java
+++ src/test/java/domain/participant/PlayerTest.java
@@ -3,7 +3,9 @@ package domain.participant;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collections;
+import java.util.List;
 
+import domain.betting.PlayerBettingResult;
 import domain.card.Card;
 import domain.card.Denomination;
 import domain.card.Suit;
@@ -35,5 +37,91 @@ public class PlayerTest {
         player.takeCard(new Card(Suit.DIAMOND, Denomination.SIX));
         player.takeCard(new Card(Suit.DIAMOND, Denomination.EIGHT));
         assertThat(player.canHit()).isFalse();
+    }
+
+    @Test
+    @DisplayName("딜러가 블랙잭이면 배팅 결과는 LOSE이다.")
+    void calculateBettingResult1() {
+        // given
+        Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.ACE),
+                new Card(Suit.HEART, Denomination.TEN)));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.TEN));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.SIX));
+
+        // when, then
+        assertThat(player.calculateBettingResult(dealerHand)).isEqualTo(PlayerBettingResult.LOSE);
+    }
+
+    @Test
+    @DisplayName("플레이어가 버스트면 배팅 결과는 LOSE이다.")
+    void calculateBettingResult2() {
+        // given
+        Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.SIX),
+                new Card(Suit.HEART, Denomination.TEN)));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.TEN));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.SIX));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.NINE));
+
+        // when, then
+        assertThat(player.calculateBettingResult(dealerHand)).isEqualTo(PlayerBettingResult.LOSE);
+    }
+
+    @Test
+    @DisplayName("플레이어와 딜러가 스테이일 때 플레이어의 카드 합이 딜러의 카드 합보다 적으면 배팅 결과는 LOSE이다.")
+    void calculateBettingResult3() {
+        // given
+        Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.SIX),
+                new Card(Suit.HEART, Denomination.TEN)));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.THREE));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.FIVE));
+
+        // when, then
+        assertThat(player.calculateBettingResult(dealerHand)).isEqualTo(PlayerBettingResult.LOSE);
+    }
+
+    @Test
+    @DisplayName("플레이어와 스테이일 때 딜러가 버스트면 배팅 결과는 WIN이다.")
+    void calculateBettingResult4() {
+        // given
+        Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.SIX),
+                new Card(Suit.HEART, Denomination.TEN),
+                new Card(Suit.SPADE, Denomination.TEN)));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.THREE));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.FIVE));
+
+        // when, then
+        assertThat(player.calculateBettingResult(dealerHand)).isEqualTo(PlayerBettingResult.WIN);
+    }
+
+    @Test
+    @DisplayName("플레이어와 딜러가 스테이일 때 플레이어의 카드 합이 딜러의 카드 합보다 적으면 배팅 결과는 LOSE이다.")
+    void calculateBettingResult5() {
+        // given
+        Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.THREE),
+                new Card(Suit.HEART, Denomination.FIVE)));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.JACK));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.SEVEN));
+
+        // when, then
+        assertThat(player.calculateBettingResult(dealerHand)).isEqualTo(PlayerBettingResult.WIN);
+    }
+
+    @Test
+    @DisplayName("딜러가 블랙잭이 아닐 때 플레이어가 블랙잭이면 배팅 결과는 BLACKJACK이다.")
+    void calculateBettingResult6() {
+        // given
+        Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.THREE),
+                new Card(Suit.HEART, Denomination.FIVE)));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.JACK));
+        player.takeCard(new Card(Suit.DIAMOND, Denomination.ACE));
+
+        // when, then
+        assertThat(player.calculateBettingResult(dealerHand)).isEqualTo(PlayerBettingResult.BLACKJACK);
     }
 }
