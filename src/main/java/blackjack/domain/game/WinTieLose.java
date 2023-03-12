1b4afd2f174cb86419dbf669604540f2faf31cdd
waterricecake <fruturum@nate.com>
2023-03-13 01:15:03 +0900

    feat(WinTieLose): 결과별 가산점 부여

--- src/main/java/blackjack/domain/game/WinTieLose.java
+++ src/main/java/blackjack/domain/game/WinTieLose.java
@@ -1,8 +1,13 @@
 package blackjack.domain.game;
 
 public enum WinTieLose {
-    WIN,
-    TIE,
-    LOSE,
-    BLACKJACK;
+    WIN(1),
+    TIE(0),
+    LOSE(-1),
+    BLACKJACK(1.5);
+    private final double rate;
+    WinTieLose(double rate){
+        this.rate = rate;
+    }
+    public double rate(){return this.rate;}
 }
