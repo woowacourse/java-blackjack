7df6cd0f36e3e5d295c7b4cc348db05489959658
kim0914 <kosa0914@naver.com>
2023-03-12 19:35:50 +0900

    refactor(Player): '딜러' 이름 검증 책임 Player로 분리

--- src/main/java/blackjack/domain/participant/Name.java
+++ src/main/java/blackjack/domain/participant/Name.java
@@ -6,21 +6,13 @@ public class Name {
 
     private static final int MAX_LENGTH = 5;
     private static final String SPACE = " ";
-    private static final String BAN_NAME = "딜러";
 
     private final String name;
 
     public Name(String name) {
         validateSpace(name);
         validateLength(name);
-        validateIsNameDealer(name);
         this.name = name;
-    }
-
-    private void validateIsNameDealer(String name) {
-        if (BAN_NAME.equals(name)) {
-            throw new IllegalArgumentException("이름은 딜러가 될 수 없습니다.");
-        }
     }
 
     private void validateSpace(String name) {
