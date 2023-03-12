c9d081c7a58a7cecdb86dd31ec1bb89f7f445039
kim0914 <kosa0914@naver.com>
2023-03-12 17:03:10 +0900

    feat(Result): 참가자 상태에 따라 수익을 계산하는 메서드 추가

--- src/main/java/blackjack/domain/game/Result.java
+++ src/main/java/blackjack/domain/game/Result.java
@@ -1,24 +1,43 @@
 package blackjack.domain.game;
 
+
 import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Player;
 
-public enum Result {
-    WIN,
-    DRAW,
-    LOSE;
+import java.util.function.IntUnaryOperator;
 
-    public static Result calculate(Player player, Dealer dealer) {
+public enum Result {
+
+    BLACKJACK(betting -> (int) (betting * 1.5)),
+    WIN(betting -> betting),
+    LOSE(betting -> betting * -1),
+    DRAW(betting -> 0);
+
+    private final IntUnaryOperator operator;
+    Result(IntUnaryOperator operator) {
+        this.operator = operator;
+    }
+
+    public static Result calculatePlayerResult(Player player, Dealer dealer) {
         int playerScore = player.calculateFinalScore();
         int dealerScore = dealer.calculateFinalScore();
+
+        if (!dealer.isBlackJack() && player.isBlackJack()) {
+            return BLACKJACK;
+        }
 
         if (playerScore > dealerScore) {
             return WIN;
         }
 
-        if (playerScore == dealerScore) {
-            return DRAW;
+        if (player.isBust() || playerScore < dealerScore) {
+            return LOSE;
         }
-        return LOSE;
+
+        return DRAW;
+    }
+
+    public int calculateProfit(Betting betting) {
+        return operator.applyAsInt(betting.getMoney());
     }
 }
