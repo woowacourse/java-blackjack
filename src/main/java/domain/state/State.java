3b72a482978c12cd0b78ff549eabd6f48ee4622f
MoonJeWoong <munjin0201@naver.com>
2023-03-12 17:15:58 +0900

    feat: 게임 상태 공통 기능 추상 클래스화

--- src/main/java/domain/state/State.java
+++ src/main/java/domain/state/State.java
@@ -1,4 +1,26 @@
 package domain.state;
 
-public interface State {
+import domain.card.Card;
+import domain.card.Hand;
+
+import java.util.List;
+
+public abstract class State {
+    private final Hand hand;
+
+    protected State(Hand hand) {
+        this.hand = hand;
+    }
+
+    public abstract State draw(Card card);
+
+    public abstract State stay();
+
+    public final List<Card> getCards() {
+        return hand.getCards();
+    }
+
+    protected final Hand getHand() {
+        return hand;
+    }
 }
