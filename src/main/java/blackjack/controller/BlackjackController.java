21c069615244c88d706d10def2ca3125ff78f301
songusika <sws981202@gmail.com>
2023-03-12 12:38:21 +0900

    refactor: 컨트롤러 클래스 리팩터링
    Co-authored-by: choi-jjunho <junho5336@gmail.com>

--- src/main/java/blackjack/controller/BlackjackController.java
+++ src/main/java/blackjack/controller/BlackjackController.java
@@ -1,26 +1,23 @@
 package blackjack.controller;
 
-import blackjack.domain.card.Card;
+
+import blackjack.domain.cardpack.CardPack;
+import blackjack.domain.cardpack.MasterShuffleStrategy;
 import blackjack.domain.game.BlackjackGame;
-import blackjack.domain.game.GameResult;
-import blackjack.domain.game.ResultReferee;
-import blackjack.domain.game.ScoreBoard;
-import blackjack.domain.game.ScoreReferee;
+import blackjack.domain.game.Result;
 import blackjack.domain.user.Dealer;
 import blackjack.domain.user.Player;
 import blackjack.domain.user.Players;
-import blackjack.domain.user.name.PlayerName;
-import blackjack.domain.user.name.UserName;
+import blackjack.domain.user.User;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class BlackjackController {
 
-    private static final int BUST_SCORE = -1;
     private final InputView inputView;
     private final OutputView outputView;
 
@@ -32,7 +29,7 @@ public class BlackjackController {
     public void run() {
         Players players = getPlayers();
         Dealer dealer = new Dealer();
-        BlackjackGame blackjackGame = new BlackjackGame();
+        BlackjackGame blackjackGame = new BlackjackGame(new CardPack(), new MasterShuffleStrategy());
 
         startGame(players, dealer, blackjackGame);
         processGame(players, dealer, blackjackGame);
@@ -40,64 +37,73 @@ public class BlackjackController {
     }
 
     private void startGame(final Players players, final Dealer dealer, final BlackjackGame blackjackGame) {
-        blackjackGame.initGame(dealer);
-        players.getPlayers().forEach(blackjackGame::initGame);
-        outputView.printUsersCard(dealer, players);
+        blackjackGame.initDraw(dealer);
+        players.getPlayers().forEach(blackjackGame::initDraw);
+        outputView.printCardInitDrawResultMessage(players, dealer);
+        outputView.printUserCards(dealer.getName(), dealer.getDealerOneCards());
+        players.getPlayers().forEach((player -> outputView.printUserCards(player.getName(), player.getCards())));
     }
 
     private void processGame(final Players players, final Dealer dealer, final BlackjackGame blackjackGame) {
         players.getPlayers().forEach((player -> processPlayerDraw(blackjackGame, player)));
-
         processDealerDraw(dealer, blackjackGame);
     }
 
     private void processPlayerDraw(final BlackjackGame blackjackGame, final Player player) {
-        while (inputView.readCommand(player.getName()) && !isBust(player.showCards())) {
+        while (!player.isBust() && readCommand(player)) {
             blackjackGame.draw(player);
-            outputView.printUserCards(player);
+            printDrawResult(player);
+        }
+    }
+
+    private boolean readCommand(final User user) {
+        return repeatInput(() -> inputView.readCommand(user));
+    }
+
+    private void printDrawResult(final Player player) {
+        outputView.printUserCards(player.getName(), player.getCards());
+        if (player.isBust()) {
+            OutputView.printBust();
         }
     }
 
     private void processDealerDraw(final Dealer dealer, final BlackjackGame blackjackGame) {
-        while (!blackjackGame.isDealerEnd(dealer)) {
+        while (!dealer.isSatisfiedMinScore()) {
             blackjackGame.draw(dealer);
             outputView.printDealerDraw();
         }
     }
 
     private void endGame(final Players players, final Dealer dealer) {
-        ScoreBoard scoreBoard = new ScoreBoard(dealer, players);
-        outputView.printCardResult(scoreBoard);
-
-        printResult(players, scoreBoard);
+        outputView.printCardResult(dealer);
+        players.getPlayers().forEach((player -> outputView.printCardResult(player)));
+        printResult(players, dealer);
     }
 
-    private void printResult(final Players players, final ScoreBoard scoreBoard) {
-        ResultReferee referee = new ResultReferee(scoreBoard);
-        final Map<UserName, GameResult> playerScore = getPlayerScore(players, referee);
-        final Map<GameResult, Integer> dealerScore = referee.getDealerScore();
-        outputView.printGameResult(dealerScore, playerScore);
-    }
-
-    private boolean isBust(final List<Card> showCards) {
-        return ScoreReferee.calculateScore(showCards) == BUST_SCORE;
+    private void printResult(final Players players, final Dealer dealer) {
+        final Map<User, Result> result = dealer.getResult(players);
+        outputView.introduceGameResult();
+        result.keySet().forEach((user) -> {
+                    outputView.printGameResult(user, result.get(user));
+                }
+        );
     }
 
     private Players getPlayers() {
-        final List<String> strings = inputView.readParticipantName();
-        return new Players(strings.stream()
-                .map(name -> new Player(new PlayerName(name)))
-                .collect(Collectors.toList()));
+        return repeatInput(() -> {
+            List<String> playerNames = inputView.readParticipantName();
+            return new Players(playerNames.stream()
+                    .map(Player::new)
+                    .collect(Collectors.toList()));
+        });
     }
 
-    public Map<UserName, GameResult> getPlayerScore(final Players players, final ResultReferee referee) {
-        Map<UserName, GameResult> result = new HashMap<>();
-
-        players.getPlayers().forEach((player) -> {
-            final GameResult score = referee.askResultByUserName(player.getName());
-            result.put(player.getName(), score);
-        });
-
-        return result;
+    private <T> T repeatInput(Supplier<T> input) {
+        try {
+            return input.get();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return repeatInput(input);
+        }
     }
 }
