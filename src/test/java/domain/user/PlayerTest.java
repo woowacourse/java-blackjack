e0884cea6d609c61959103a83f8b5519a94d3d7c
MoonJeWoong <munjin0201@naver.com>
2023-03-13 02:38:44 +0900

    test: player 리팩토링으로 인한 테스트 코드 수정

--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -8,7 +8,7 @@ import domain.card.CloverCard;
 import java.util.List;
 import java.util.stream.Stream;
 
-import domain.state.PlayerStatus;
+import domain.state.State;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.Test;
@@ -22,8 +22,8 @@ class PlayerTest {
     @DisplayName("처음에 2장의 카드를 받는다.")
     void generatePlayerTest() {
         //given
-        List<Card> firstTurnCards = List.of(CloverCard.ACE, CloverCard.FIVE);
-        Player player = new Player(new Name("플레이어"), firstTurnCards);
+        Player player = new Player(new Name("player"));
+        player.receiveCards(List.of(CloverCard.ACE, CloverCard.FIVE));
 
         //when
         List<Card> cards = player.getCards();
@@ -36,35 +36,13 @@ class PlayerTest {
     @DisplayName("카드 한 장을 받아 패에 넣는다.")
     void receiveCardTest() {
         //given
-        List<Card> firstTurnCards = List.of(CloverCard.ACE, CloverCard.FIVE);
-        Player player = new Player(new Name("플레이어"), firstTurnCards);
+        Player player = new Player(new Name("player"));
+        player.receiveCards(List.of(CloverCard.ACE, CloverCard.FIVE));
 
         //when
         player.receiveCard(CloverCard.FOUR);
 
         //then
         assertThat(player.getCards().size()).isEqualTo(3);
-    }
-
-    @ParameterizedTest(name = "{2} 상태값을 확인할 수 있다.")
-    @MethodSource("isPlayerStatusTestCase")
-    @DisplayName("자신의 상태값을 확인할 수 있다.")
-    void isPlayerStatusTest(List<Card> firstTurnCards, Card drawCard, PlayerStatus playerStatus) {
-        //given
-        Player player = new Player(new Name("플레이어"), firstTurnCards);
-
-        //when
-        player.receiveCard(drawCard);
-
-        //then
-        assertThat(player.isUserStatus(playerStatus)).isTrue();
-
-    }
-
-    static Stream<Arguments> isPlayerStatusTestCase() {
-        return Stream.of(
-                Arguments.of(List.of(CloverCard.TEN, CloverCard.KING), CloverCard.QUEEN, Named.of("버스트", PlayerStatus.BUST)),
-                Arguments.of(List.of(CloverCard.TWO, CloverCard.THREE), CloverCard.FOUR, Named.of("노멀", PlayerStatus.NORMAL))
-        );
     }
 }
