5eac99062a15a21fc3c0bcd9d28cf7a1b40ca9f6
swonny <wonny921@gmail.com>
2023-03-13 02:15:00 +0900

    refactor: test 코드 중복되는 코드 제거

--- src/test/java/blackjack/domain/user/UsersTest.java
+++ src/test/java/blackjack/domain/user/UsersTest.java
@@ -4,6 +4,7 @@ import blackjack.domain.card.Card;
 import blackjack.domain.card.CardNumber;
 import blackjack.domain.card.CardSymbol;
 import blackjack.domain.result.UserResults;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,95 +14,85 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class UsersTest {
 
-    @DisplayName("Players가 받을 금액을 계산한다.")
+    private Users users;
+    private Players players;
+    private Dealer dealer;
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        this.players = Players.from(new Names(List.of("merry")));
+        this.dealer = new Dealer();
+        this.users = new Users(dealer, players);
+        player = players.getPlayers().get(0);
+        player.setBetAmount(10000);
+    }
+
+
+    @DisplayName("Dealer가 블랙잭이 아닌 경우, Player가 블랙잭이라면 Player는 베팅 금액의 1.5배를 반환한다.")
     @Test
     void player_blackjack_dealer_not_blackjack() {
-        // given
-        Players players = Players.from(new Names(List.of("merry")));
-        Dealer dealer = new Dealer();
-        Users users = new Users(dealer, players);
-        Player merry = players.getPlayers().get(0);
-        merry.setBetAmount(10000);
-
-        merry.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
-        merry.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
-
+        // when
+        player.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
+        player.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
         dealer.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
         dealer.updateCardScore(new Card(CardNumber.TWO, CardSymbol.HEART));
 
         UserResults userResults = UserResults.of(users);
+        users.calculateRewards(userResults);
 
-        users.calculateReceivingAmount(userResults);
-
-        assertThat(merry.getReceivingAmount())
+        // then
+        assertThat(player.getReceivingAmount())
                 .isEqualTo(10000 * 1.5);
     }
 
-    @DisplayName("Players가 받을 금액을 계산한다.")
+    @DisplayName("Dealer가 블랙잭인 경우, Player가 블랙잭이라면 Player는 베팅 금액을 받는다.")
     @Test
     void player_blackjack_dealer_blackjack() {
-        // given
-        Players players = Players.from(new Names(List.of("merry")));
-        Dealer dealer = new Dealer();
-        Users users = new Users(dealer, players);
-        Player merry = players.getPlayers().get(0);
-        merry.setBetAmount(10000);
-
-        merry.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
-        merry.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
+        // when
+        player.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
+        player.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
 
         dealer.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
         dealer.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
 
         UserResults userResults = UserResults.of(users);
 
-        users.calculateReceivingAmount(userResults);
+        users.calculateRewards(userResults);
 
-        assertThat(merry.getReceivingAmount())
+        // then
+        assertThat(player.getReceivingAmount())
                 .isEqualTo(10000);
     }
 
-    @DisplayName("Players가 받을 금액을 계산한다.")
+    @DisplayName("Dealer와 Player 모두 블랙잭으로 무승부라면 Dealer는 Player의 베팅 금액 만큼의 손해를 본다.")
     @Test
-    void calculate_dealer_receiving_amount() {
+    void calculate_dealer_rewards_when_dealer_tie() {
         // given
-        Players players = Players.from(new Names(List.of("merry")));
-        Dealer dealer = new Dealer();
-        Users users = new Users(dealer, players);
-        Player merry = players.getPlayers().get(0);
-        merry.setBetAmount(10000);
-
-        merry.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
-        merry.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
+        player.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
+        player.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
 
         dealer.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
         dealer.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
 
         UserResults userResults = UserResults.of(users);
 
-        users.calculateReceivingAmount(userResults);
+        users.calculateRewards(userResults);
 
-        assertThat(users.calculateDealerReceivingAmount())
-                .isEqualTo(0);
+        assertThat(users.calculateDealerRewards())
+                .isEqualTo(-10000);
     }
 
-    @DisplayName("Players가 받을 금액을 계산한다.")
+    @DisplayName("Player가 블랙잭이고 Dealer가 블랙잭이 아닌 경우 Dealer는 Player의 베팅 금액 만큼 손해를 본다.")
     @Test
-    void player_blackjack_dealer_not_blackjack_calculate_dealer() {
-        // given
-        Players players = Players.from(new Names(List.of("merry")));
-        Dealer dealer = new Dealer();
-        Users users = new Users(dealer, players);
-        Player merry = players.getPlayers().get(0);
-        merry.setBetAmount(10000);
-
-        merry.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
-        merry.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
+    void calculate_dealer_rewards_when_dealer_lose() {
+        player.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
+        player.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
 
         dealer.updateCardScore(new Card(CardNumber.ACE, CardSymbol.HEART));
         dealer.updateCardScore(new Card(CardNumber.TWO, CardSymbol.HEART));
 
-        assertThat(users.calculateDealerReceivingAmount())
-                .isEqualTo(-5000);
+        assertThat(users.calculateDealerRewards())
+                .isEqualTo(-10000);
     }
 }
