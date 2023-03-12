7ef48749555bd0ff3b4727b1a36b836b598a0a98
MoonJeWoong <munjin0201@naver.com>
2023-03-12 13:09:04 +0900

    style: user 클래스 내 불필요한 코드 주석 삭제

--- src/main/java/domain/user/User.java
+++ src/main/java/domain/user/User.java
@@ -6,26 +6,11 @@ import domain.card.Hand;
 import java.util.List;
 
 public abstract class User {
-    protected static final int BLACKJACK = 21;
-
-//    private final List<Card> cards;
-//    protected final Score score;
     protected Hand hand;
-
-//    public User(List<Card> firstTurnCards) {
-//        cards = new ArrayList<>(firstTurnCards);
-//        score = new Score(firstTurnCards);
-//    }
 
     public User(List<Card> firstTurnCards) {
         hand = new Hand(firstTurnCards);
     }
-
-//    public void receiveCard(Card card) {
-//        cards.add(card);
-//        score.calculate(cards);
-//        checkBustByScore();
-//    }
 
     public void receiveCard(Card card) {
         hand = hand.add(card);
