b1175c996b047436e8eed7f8ee38a89c29fceed9
greeng00se <thegreengoose7@gmail.com>
2023-03-12 22:17:16 +0900

    refactor: 재입력 횟수 초과시 메시지 출력 후 게임 종료

--- src/main/java/blackjack/BlackjackApplication.java
+++ src/main/java/blackjack/BlackjackApplication.java
@@ -3,6 +3,7 @@ package blackjack;
 import blackjack.controller.BlackjackController;
 import blackjack.controller.Retry;
 import blackjack.domain.game.BlackjackGame;
+import blackjack.view.OutputView;
 
 public final class BlackjackApplication {
 
@@ -11,6 +12,11 @@ public final class BlackjackApplication {
                 new BlackjackGame(),
                 new Retry(5)
         );
-        blackjackController.run();
+        try {
+            blackjackController.run();
+        } catch (IllegalArgumentException e) {
+            final OutputView outputView = new OutputView();
+            outputView.printException("재입력 횟수를 초과하여 게임을 종료합니다.");
+        }
     }
 }
