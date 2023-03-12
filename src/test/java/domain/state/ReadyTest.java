00569e1e6ba3ce2d9c3c68520eb22438a45638ca
MoonJeWoong <munjin0201@naver.com>
2023-03-12 16:26:56 +0900

    test: Ready 상태에서 드로우 기능 테스트 작성

--- src/test/java/domain/state/ReadyTest.java
+++ src/test/java/domain/state/ReadyTest.java
@@ -1,16 +1,47 @@
 package domain.state;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import domain.card.CloverCard;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 
 @DisplayName("Ready 상태는 ")
 class ReadyTest {
     @Test
     @DisplayName("패가 0장인 상태로 생성된다.")
     void createReadyTest() {
-        final Ready ready = new Ready();
+        Ready ready = new Ready();
         assertThat(ready.getCards()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("패를 1장 드로우한 뒤 Ready 상태를 유지한다.")
+    void afterDrawOnceTest() {
+        State state = new Ready().draw(CloverCard.FOUR);
+
+        assertThat(state).isInstanceOf(Ready.class);
+    }
+
+    @Test
+    @DisplayName("패를 드로우한 뒤 hand에 넣는다.")
+    void drawTest() {
+        State state = new Ready()
+                .draw(CloverCard.FOUR)
+                .draw(CloverCard.FIVE);
+
+        assertThat(state.getCards()).containsExactly(CloverCard.FOUR, CloverCard.FIVE);
+    }
+
+    @Test
+    @DisplayName("패를 2장 드로우한 뒤 Hit 상태가 된다.")
+    void transferToHitTest() {
+        Ready ready = new Ready();
+
+        State state = ready.draw(CloverCard.FOUR).draw(CloverCard.FIVE);
+
+        assertThat(state).isInstanceOf(Hit.class);
     }
 }
