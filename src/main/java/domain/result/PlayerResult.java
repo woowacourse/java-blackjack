6534a9d00eecfe82eba741da2ccb6a77b15b1e82
jaehee329 <jjhs9803@gmail.com>
2023-03-13 02:18:29 +0900

    feat(PlayerResult): 베팅 관련 결과 기능 구현

--- src/main/java/domain/result/PlayerResult.java
+++ src/main/java/domain/result/PlayerResult.java
@@ -1,42 +1,53 @@
 package domain.result;
 
+import domain.Dealer;
 import domain.Player;
 
-public class PlayerResult {
+public class PlayerResult implements Result {
 
 	private final String name;
-	private final ResultState resultState;
+	private final int profit;
 
-	private PlayerResult(final String name, final ResultState resultState) {
+	private PlayerResult(final String name, final int profit) {
 		this.name = name;
-		this.resultState = resultState;
+		this.profit = profit;
 	}
 
-	public static PlayerResult decide(final Player targetPlayer, final Player comparedPlayer) {
-		if (isVictory(targetPlayer, comparedPlayer)) {
-			return new PlayerResult(targetPlayer.getName(), ResultState.WIN);
+	public static PlayerResult decide(final Player player, final Dealer dealer) {
+		if (isBlackJackVictory(player, dealer)) {
+			return new PlayerResult(player.getName(), (int)(ResultState.BLACKJACK.getMultiplier() * player.getBet()));
 		}
-		if (isDraw(targetPlayer, comparedPlayer)) {
-			return new PlayerResult(targetPlayer.getName(), ResultState.DRAW);
+		if (isVictory(player, dealer)) {
+			return new PlayerResult(player.getName(), (int)(ResultState.WIN.getMultiplier() * player.getBet()));
 		}
-		return new PlayerResult(targetPlayer.getName(), ResultState.LOSS);
+		if (isDraw(player, dealer)) {
+			return new PlayerResult(player.getName(), (int)(ResultState.DRAW.getMultiplier() * player.getBet()));
+		}
+		return new PlayerResult(player.getName(), (int)(ResultState.DEFEAT.getMultiplier() * player.getBet()));
 	}
 
-	private static boolean isVictory(final Player targetPlayer, final Player comparedPlayer) {
-		return !targetPlayer.isBust()
-			&& (comparedPlayer.isBust() || targetPlayer.getScore() > comparedPlayer.getScore());
+	private static boolean isBlackJackVictory(final Player player, final Dealer dealer) {
+		return player.isBlackJack() && !dealer.isBlackJack();
 	}
 
-	private static boolean isDraw(final Player targetPlayer, final Player comparedPlayer) {
-		return targetPlayer.isBust() && comparedPlayer.isBust()
-			|| targetPlayer.getScore() == comparedPlayer.getScore();
+	private static boolean isVictory(final Player player, final Dealer dealer) {
+		return (!player.isBust() && !dealer.isBust() && player.getScore() > dealer.getScore())
+			|| (!player.isBust() && dealer.isBust());
 	}
 
+	private static boolean isDraw(final Player player, final Dealer dealer) {
+		return (player.isBust() && dealer.isBust())
+			|| (!player.isBlackJack() && !dealer.isBlackJack() && (player.getScore() == dealer.getScore()))
+			|| (player.isBlackJack() && dealer.isBlackJack());
+	}
+
+	@Override
 	public String getName() {
 		return name;
 	}
 
-	public ResultState getResultState() {
-		return resultState;
+	@Override
+	public int getProfit() {
+		return profit;
 	}
 }
--- src/test/java/domain/result/PlayerResultTest.java
+++ src/test/java/domain/result/PlayerResultTest.java
@@ -19,37 +19,37 @@ class PlayerResultTest {
 		@Test
 		@DisplayName("BlackJack으로 승리한 플레이어는 건 돈의 1.5배의 수익을 올려야 한다.")
 		void blackJackProfitTest() {
-			Player blackJackPlayer = TestData.getBlackJackPlayer();
+			Player blackJackPlayer = TestData.getBlackJackPlayer(100);
 			Dealer score20Dealer = TestData.getScore20Dealer();
 
 			PlayerResult result = PlayerResult.decide(blackJackPlayer, score20Dealer);
 
 			assertThat(result.getProfit())
-				.isEqualTo(blackJackPlayer.getBet() * ResultState.BLACKJACK.getMultiplier());
+				.isEqualTo((int)(blackJackPlayer.getBet() * ResultState.BLACKJACK.getMultiplier()));
 		}
 
 		@Test
 		@DisplayName("높은 점수로 승리한 플레이어는 건 돈 만큼의 수익을 올려야 한다.")
 		void playerWinProfitTest() {
-			Player score20Player = TestData.getScore20Player();
+			Player score20Player = TestData.getScore20Player(100);
 			Dealer score19Dealer = TestData.getScore19Dealer();
 
 			PlayerResult result = PlayerResult.decide(score20Player, score19Dealer);
 
 			assertThat(result.getProfit())
-				.isEqualTo(score20Player.getBet() * ResultState.WIN.getMultiplier());
+				.isEqualTo((int)(score20Player.getBet() * ResultState.WIN.getMultiplier()));
 		}
 
 		@Test
 		@DisplayName("플레이어가 blackjack이나 bust가 아니고 딜러가 bust라면 건 돈 만큼의 수익을 올려야 한다.")
 		void playerWinByDealerBustProfitTest() {
-			Player score20Player = TestData.getScore20Player();
+			Player score20Player = TestData.getScore20Player(100);
 			Dealer bustDealer = TestData.getBustDealer();
 
 			PlayerResult result = PlayerResult.decide(score20Player, bustDealer);
 
 			assertThat(result.getProfit())
-				.isEqualTo(score20Player.getBet() * ResultState.WIN.getMultiplier());
+				.isEqualTo((int)(score20Player.getBet() * ResultState.WIN.getMultiplier()));
 		}
 	}
 
@@ -60,37 +60,37 @@ class PlayerResultTest {
 		@Test
 		@DisplayName("bust나 blackJack이 아닌 같은 점수로 무승부인 결과에서는 0의 수익을 올려야 한다.")
 		public void playerDealerDrawProfitTest() {
-			Player score20Player = TestData.getScore20Player();
+			Player score20Player = TestData.getScore20Player(100);
 			Dealer score20Dealer = TestData.getScore20Dealer();
 
 			PlayerResult result = PlayerResult.decide(score20Player, score20Dealer);
 
 			assertThat(result.getProfit())
-				.isEqualTo(score20Player.getBet() * ResultState.DRAW.getMultiplier());
+				.isEqualTo((int)(score20Player.getBet() * ResultState.DRAW.getMultiplier()));
 		}
 
 		@Test
 		@DisplayName("둘 다 BlackJack으로 무승부인 결과에서는 0의 수익을 올려야 한다.")
 		public void bothBlackJackProfitTest() {
-			Player blackJackPlayer = TestData.getBlackJackPlayer();
+			Player blackJackPlayer = TestData.getBlackJackPlayer(100);
 			Dealer score20Dealer = TestData.getBlackJackDealer();
 
 			PlayerResult result = PlayerResult.decide(blackJackPlayer, score20Dealer);
 
 			assertThat(result.getProfit())
-				.isEqualTo(blackJackPlayer.getBet() * ResultState.DRAW.getMultiplier());
+				.isEqualTo((int)(blackJackPlayer.getBet() * ResultState.DRAW.getMultiplier()));
 		}
 
 		@Test
 		@DisplayName("플레이어와 딜러가 모두 bust인 결과에서는 무승부 처리되어 0의 수익을 올려야 한다.")
 		public void playerBustDealerBustDrawProfitTest() {
-			Player score20Player = TestData.getScore20Player();
+			Player score20Player = TestData.getScore20Player(100);
 			Dealer score20Dealer = TestData.getScore20Dealer();
 
 			PlayerResult result = PlayerResult.decide(score20Player, score20Dealer);
 
 			assertThat(result.getProfit())
-				.isEqualTo(score20Player.getBet() * ResultState.DRAW.getMultiplier());
+				.isEqualTo((int)(score20Player.getBet() * ResultState.DRAW.getMultiplier()));
 		}
 	}
 
@@ -101,37 +101,37 @@ class PlayerResultTest {
 		@Test
 		@DisplayName("플레이어만 bust라면 건 돈 만큼의 손실이 발생한다.")
 		public void playerOnlyBustProfitTest() {
-			Player bustPlayer = TestData.getBustPlayer();
+			Player bustPlayer = TestData.getBustPlayer(100);
 			Dealer score20Dealer = TestData.getScore20Dealer();
 
 			PlayerResult result = PlayerResult.decide(bustPlayer, score20Dealer);
 
 			assertThat(result.getProfit())
-				.isEqualTo(bustPlayer.getBet() * ResultState.DEFEAT.getMultiplier());
+				.isEqualTo((int)(bustPlayer.getBet() * ResultState.DEFEAT.getMultiplier()));
 		}
 
 		@Test
 		@DisplayName("플레이어가 딜러보다 점수가 낮으며 둘 다 bust가 아니라면 건 돈 만큼의 손실이 발생한다.")
 		public void playerDefeatProfitTest() {
-			Player score19Player = TestData.getScore19Player();
+			Player score19Player = TestData.getScore19Player(100);
 			Dealer score20Dealer = TestData.getScore20Dealer();
 
 			PlayerResult result = PlayerResult.decide(score19Player, score20Dealer);
 
 			assertThat(result.getProfit())
-				.isEqualTo(score19Player.getBet() * ResultState.DEFEAT.getMultiplier());
+				.isEqualTo((int)(score19Player.getBet() * ResultState.DEFEAT.getMultiplier()));
 		}
 
 		@Test
 		@DisplayName("플레이어가 추가 히트로 21점이 되어도 딜러가 blackJack이라면 건 돈 만큼의 손실이 발생한다.")
 		public void sameScoreButDealerBlackJackProfitTest() {
-			Player score21Player = TestData.getScore21Player();
+			Player score21Player = TestData.getScore21Player(100);
 			Dealer blackJackDealer = TestData.getBlackJackDealer();
 
 			PlayerResult result = PlayerResult.decide(score21Player, blackJackDealer);
 
 			assertThat(result.getProfit())
-				.isEqualTo(score21Player.getBet() * ResultState.DEFEAT.getMultiplier());
+				.isEqualTo((int)(score21Player.getBet() * ResultState.DEFEAT.getMultiplier()));
 		}
 	}
 }
