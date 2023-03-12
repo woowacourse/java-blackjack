6711aee4261f653377c09f9cb03ea0b3096ba413
sh111-coder <ohk9134@naver.com>
2023-03-13 01:24:00 +0900

    feat(Player): 플레이어와 딜러의 상태를 비교하여 배팅 결과를 반환하는 기능 구현

--- src/main/java/domain/participant/Player.java
+++ src/main/java/domain/participant/Player.java
@@ -1,5 +1,7 @@
 package domain.participant;
 
+import domain.betting.PlayerBettingResult;
+
 public class Player extends Participant {
 
     private static final int LIMIT_HIT_VALUE = 21;
@@ -10,5 +12,25 @@ public class Player extends Participant {
 
     public boolean canHit() {
         return this.calculateOptimalCardValueSum() <= LIMIT_HIT_VALUE;
+    }
+
+    public PlayerBettingResult calculateBettingResult(Hand dealerHand) {
+        if ((dealerHand.isBlackjack() || hand.isBust()) ||
+                (hand.isStay() && dealerHand.isStay() && isLoseWhenStay(dealerHand))) {
+            return PlayerBettingResult.LOSE;
+        }
+        if ((hand.isStay() && dealerHand.isBust()) ||
+                (hand.isStay() && dealerHand.isStay() && isWinWhenStay(dealerHand))) {
+            return PlayerBettingResult.WIN;
+        }
+        return PlayerBettingResult.BLACKJACK;
+    }
+
+    private boolean isWinWhenStay(Hand dealerHand) {
+        return hand.calculateOptimalCardValueSum() >= dealerHand.calculateOptimalCardValueSum();
+    }
+
+    private boolean isLoseWhenStay(Hand dealerHand) {
+        return hand.calculateOptimalCardValueSum() < dealerHand.calculateOptimalCardValueSum();
     }
 }
