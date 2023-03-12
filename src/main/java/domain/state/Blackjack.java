2305bbf06d3e4fc93bd5501011104e47f8ff8294
MoonJeWoong <munjin0201@naver.com>
2023-03-12 20:26:20 +0900

    feat: finished state 기능 추가 구현

--- src/main/java/domain/state/Blackjack.java
+++ src/main/java/domain/state/Blackjack.java
@@ -1,20 +1,9 @@
 package domain.state;
 
-import domain.card.Card;
 import domain.card.Hand;
 
-public final class Blackjack extends State{
+public final class Blackjack extends Finished{
     public Blackjack(Hand hand) {
         super(hand);
-    }
-
-    @Override
-    public State draw(Card card) {
-        throw new IllegalStateException("[ERROR] 게임이 종료되어 카드를 뽑을 수 없습니다.");
-    }
-
-    @Override
-    public State stay() {
-        throw new IllegalStateException("[ERROR] 게임이 종료되어 상태를 조작할 수 없습니다.");
     }
 }
---
+++ src/main/java/domain/state/Finished.java
@@ -0,0 +1,20 @@
+package domain.state;
+
+import domain.card.Card;
+import domain.card.Hand;
+
+public abstract class Finished extends State{
+    public Finished(Hand hand) {
+        super(hand);
+    }
+
+    @Override
+    public final State draw(Card card) {
+        throw new IllegalStateException("[ERROR] 게임이 종료되어 카드를 뽑을 수 없습니다.");
+    }
+
+    @Override
+    public final State stay() {
+        throw new IllegalStateException("[ERROR] 게임이 종료되어 상태를 조작할 수 없습니다.");
+    }
+}
