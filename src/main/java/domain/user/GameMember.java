3cc16c153ff752263a8d2aa6540f5131cb474ddc
echo <echo.backend@gmail.com>
2023-03-13 22:57:47 +0900

    refactor: 플레이어 리스트 검증 로직 InputView로 이동

--- src/main/java/domain/user/GameMember.java
+++ src/main/java/domain/user/GameMember.java
@@ -1,16 +1,12 @@
 package domain.user;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class GameMember implements Iterable<Playable> {
     
-    public static final String DUPLICATED_NAME_ERROR_ESSAGE = "중복된 플레이어 이름이 있습니다.";
     private final Dealer dealer;
     private final List<Player> players;
     
@@ -19,21 +15,14 @@ public class GameMember implements Iterable<Playable> {
         this.players = players;
     }
     
-    public static GameMember of(final String playerNames) {
-        List<Player> players = Arrays.stream(playerNames.split(","))
+    public static GameMember of(final List<String> playerNames) {
+        List<Player> players = playerNames.stream()
                 .map(String::trim)
                 .map(Player::new)
                 .collect(Collectors.toList());
-        validateUniqueness(players);
         return new GameMember(new Dealer(), players);
     }
     
-    private static void validateUniqueness(final List<Player> players) {
-        Set<Player> playerSet = new HashSet<>(players);
-        if (playerSet.size() != players.size()) {
-            throw new IllegalArgumentException(DUPLICATED_NAME_ERROR_ESSAGE);
-        }
-    }
     
     public Dealer getDealer() {
         return this.dealer;
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,27 +1,44 @@
 package view;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class InputView {
     
     public static final String ILLEGAL_ANSWER_MESSAGE = "잘못된 입력입니다. y 또는 n을 입력해주세요.";
+    
+    public static final String DUPLICATED_NAME_ERROR_ESSAGE = "중복된 플레이어 이름이 있습니다.";
     public static final String CONTINUE = "y";
     public static final String STOP = "n";
     public static final String PARTICIPANTS_NAME_REQUEST_PROMPT_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     public static final String MORE_CARD_REQUEST_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
     public static final String BETTING_MONEY_REQUEST_MESSAGE = "%s의 배팅 금액은?";
     public static final String NOT_NUMBER_ERROR_MESSAGE = "잘못된 입력입니다. 숫자만 입력 가능합니다.";
+    public static final String DELIMITER = ",";
     private static final Scanner scanner = new Scanner(System.in);
     
-    public static String readPlayerNames() {
+    public static List<String> readPlayerNames() {
         System.out.println(PARTICIPANTS_NAME_REQUEST_PROMPT_MESSAGE);
-        String names = readLine();
+        String line = readLine();
+        List<String> names = Arrays.stream(line.split(DELIMITER)).collect(Collectors.toList());
+        validateUniqueness(names);
         System.out.println();
         return names;
     }
     
     private static String readLine() {
         return scanner.nextLine();
+    }
+    
+    private static void validateUniqueness(final List<String> playerNames) {
+        Set<String> playerSet = new HashSet<>(playerNames);
+        if (playerSet.size() != playerNames.size()) {
+            throw new IllegalArgumentException(DUPLICATED_NAME_ERROR_ESSAGE);
+        }
     }
     
     public static int readBet(String participantName) {
