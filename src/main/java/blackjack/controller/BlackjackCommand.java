30d25f237869b064a8c58040973604040458d073
greeng00se <thegreengoose7@gmail.com>
2023-03-13 12:52:29 +0900

    refactor: Command 확장성 고려해서 상수 반복 추가

--- src/main/java/blackjack/controller/BlackjackCommand.java
+++ src/main/java/blackjack/controller/BlackjackCommand.java
@@ -1,5 +1,7 @@
 package blackjack.controller;
 
+import static java.util.stream.Collectors.joining;
+
 import java.util.Arrays;
 
 public enum BlackjackCommand {
@@ -13,12 +15,13 @@ public enum BlackjackCommand {
     }
 
     public static BlackjackCommand from(final String command) {
+        var commandMessages = Arrays.stream(values())
+                .map(value -> value.command)
+                .collect(joining(" 또는 "));
         return Arrays.stream(values())
                 .filter(value -> value.isSameCommand(command))
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException(
-                        HIT.command + " 또는 " + STAY.command + "을 입력해야 합니다. 입력값:" + command)
-                );
+                .orElseThrow(() -> new IllegalArgumentException(commandMessages + "을 입력해야 합니다. 입력값:" + command));
     }
 
     private boolean isSameCommand(final String command) {
