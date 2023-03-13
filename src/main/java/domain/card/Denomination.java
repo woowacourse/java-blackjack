7529f511842f136b715b44a64fb99d6e2cbaf468
yeonkkk <tjddus0302@gmail.com>
2023-03-13 09:07:20 +0900

    refactor: Denomination 생성 인자를 int에서 Score로 변경

--- src/main/java/domain/card/Denomination.java
+++ src/main/java/domain/card/Denomination.java
@@ -5,26 +5,26 @@ import domain.Score;
 import java.util.Arrays;
 
 public enum Denomination {
-    ACE("A", 1),
-    TWO("2", 2),
-    THREE("3", 3),
-    FOUR("4", 4),
-    FIVE("5", 5),
-    SIX("6", 6),
-    SEVEN("7", 7),
-    EIGHT("8", 8),
-    NINE("9", 9),
-    TEN("10", 10),
-    KING("K", 10),
-    QUEEN("Q", 10),
-    JACK("J", 10);
+    ACE("A", new Score(1)),
+    TWO("2", new Score(2)),
+    THREE("3", new Score(3)),
+    FOUR("4", new Score(4)),
+    FIVE("5", new Score(5)),
+    SIX("6", new Score(6)),
+    SEVEN("7", new Score(7)),
+    EIGHT("8", new Score(8)),
+    NINE("9", new Score(9)),
+    TEN("10", new Score(10)),
+    KING("K", new Score(10)),
+    QUEEN("Q", new Score(10)),
+    JACK("J", new Score(10));
 
     private final String letter;
     private final Score score;
 
-    Denomination(String letter, int score) {
+    Denomination(String letter, Score score) {
         this.letter = letter;
-        this.score = new Score(score);
+        this.score = score;
     }
 
     public static Denomination of(String letter) {
