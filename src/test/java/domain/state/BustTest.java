520f94890ab1b9a2c86b003f9b7ff5a1cd398324
Go-Jaecheol <gojaech@naver.com>
2023-03-13 14:17:41 +0900

    test(BustTest): 현재 상태가 Bust인지 확인하는 테스트 추가

---
+++ src/test/java/domain/state/BustTest.java
@@ -0,0 +1,29 @@
+package domain.state;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.card.Card;
+import domain.card.Shape;
+import domain.card.Value;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BustTest {
+    private State bust;
+
+    @BeforeEach
+    void setUp() {
+        List<Card> initialCards = List.of(new Card(Value.JACK, Shape.HEART), new Card(Value.QUEEN, Shape.HEART), new Card(Value.KING, Shape.HEART));
+        Hand hand = new Hand(new ArrayList<>(initialCards));
+        bust = new Bust(hand);
+    }
+
+    @DisplayName("현재 상태가 Bust인지 확인하는 테스트")
+    @Test
+    void isBustTest() {
+        assertThat(bust.isBust()).isTrue();
+    }
+}
