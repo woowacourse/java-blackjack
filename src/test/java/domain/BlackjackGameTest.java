da484bb28cf969351a21400e7e2eb2e4369eb075
bjk1649 <qhwns1649@naver.com>
2023-03-13 19:29:26 +0900

    test: decision에 따라 게임을 진행하는 기능의 테스트 코드 추가

--- src/test/java/domain/BlackjackGameTest.java
+++ src/test/java/domain/BlackjackGameTest.java
@@ -7,6 +7,7 @@ import domain.card.Card;
 import domain.card.Number;
 import domain.card.Suit;
 import domain.participant.BettingMoney;
+import domain.participant.Decision;
 import domain.participant.Hand;
 import domain.participant.Name;
 import domain.participant.Player;
@@ -46,6 +47,68 @@ class BlackjackGameTest {
                     () -> assertThat(outcome.get("포이")).isEqualTo(1000),
                     () -> assertThat(outcome.get("에밀")).isEqualTo(-2000)
             );
+        }
+    }
+    @Nested
+    class 게임진행 {
+        @Test
+        void should_이번턴의플레이어에게카드르준다_when_decision이HIT일시() {
+            //given
+            Players players = new Players(List.of(
+                    new Player(new Name("포이"), new Hand(), new BettingMoney(1000)),
+                    new Player(new Name("에밀"), new Hand(), new BettingMoney(2000))
+            ));
+            //given
+            BlackjackGame blackjackGame = new BlackjackGame(players);
+            blackjackGame.handOutInitialCards((cards) -> {
+                cards.clear();
+                cards.add(new Card(Suit.SPADE, Number.TWO));
+                cards.add(new Card(Suit.SPADE, Number.THREE));
+                cards.add(new Card(Suit.DIAMOND, Number.TWO));
+                cards.add(new Card(Suit.DIAMOND, Number.THREE));
+                cards.add(new Card(Suit.CLUB, Number.TWO));
+                cards.add(new Card(Suit.CLUB, Number.THREE));
+                cards.add(new Card(Suit.HEART, Number.TWO));
+                cards.add(new Card(Suit.HEART, Number.THREE));
+            });
+            int expected = players.values().get(0).hand().size() + 1;
+
+            //when
+            blackjackGame.hitOrStand(Decision.HIT);
+            int actual = players.values().get(0).hand().size();
+
+            //then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void should_현재플레이어가드로우를할수있더라도더이상카드를뽑지않는다_when_decision이STAND일시() {
+            //given
+            Player expected = new Player(new Name("에밀"), new Hand(), new BettingMoney(2000));
+            Players players = new Players(List.of(
+                    new Player(new Name("포이"), new Hand(), new BettingMoney(1000)),
+                    expected)
+            );
+            //given
+            BlackjackGame blackjackGame = new BlackjackGame(players);
+            blackjackGame.handOutInitialCards((cards) -> {
+                cards.clear();
+                cards.add(new Card(Suit.SPADE, Number.TWO));
+                cards.add(new Card(Suit.SPADE, Number.THREE));
+                cards.add(new Card(Suit.DIAMOND, Number.TWO));
+                cards.add(new Card(Suit.DIAMOND, Number.THREE));
+                cards.add(new Card(Suit.CLUB, Number.TWO));
+                cards.add(new Card(Suit.CLUB, Number.THREE));
+                cards.add(new Card(Suit.HEART, Number.TWO));
+                cards.add(new Card(Suit.HEART, Number.THREE));
+            });
+
+            //when
+            blackjackGame.hitOrStand(Decision.STAND);
+            Player actual = blackjackGame.findCurrentDrawablePlayer();
+
+            //then
+            assertThat(actual).isEqualTo(expected);
         }
     }
 }
