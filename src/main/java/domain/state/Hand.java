4938ca377429a3d9cc6f332823dad6b4b6d19721
Go-Jaecheol <gojaech@naver.com>
2023-03-13 10:57:28 +0900

    feat(Hand): Hand에 카드 추가하는 기능 추가

--- src/main/java/domain/state/Hand.java
+++ src/main/java/domain/state/Hand.java
@@ -10,4 +10,12 @@ public class Hand {
     public Hand(final List<Card> cards) {
         this.cards = cards;
     }
+
+    public void addCard(final Card card) {
+        this.cards.add(card);
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
 }
