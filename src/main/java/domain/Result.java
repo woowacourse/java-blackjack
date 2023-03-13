2cb2d9dfdc1acf3002638370a9a63a336f609068
Go-Jaecheol <gojaech@naver.com>
2023-03-13 10:04:02 +0900

    test(ResultTest): 플레이어가 승인 경우 수익 계산 테스트 추가

---
+++ src/main/java/domain/Result.java
@@ -0,0 +1,15 @@
+package domain;
+
+public enum Result {
+
+    WIN(1),
+    TIE(0),
+    LOSE(-1),
+    BLACKJACK(1.5);
+
+    private final double odds;
+
+    Result(double odds) {
+        this.odds = odds;
+    }
+}
---
+++ src/test/java/domain/ResultTest.java
@@ -0,0 +1,15 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+    @DisplayName("플레이어가 승인 경우 베팅 금액만큼 수익을 얻는다.")
+    @Test
+    void winTest() {
+        int bettingMoney = 1000;
+        assertThat(Result.WIN.calculateResult(bettingMoney)).isEqualTo(1000);
+    }
+}
