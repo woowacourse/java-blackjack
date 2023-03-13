d3f49b22f73cd523e3a13f449aa72a24cee58043
Go-Jaecheol <gojaech@naver.com>
2023-03-13 15:05:12 +0900

    test(StandTest): 딜러와 점수 비교해서 결과 값 계산하는 테스트 추가

--- src/test/java/domain/state/StandTest.java
+++ src/test/java/domain/state/StandTest.java
@@ -2,6 +2,7 @@ package domain.state;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import domain.Result;
 import domain.card.Card;
 import domain.card.Shape;
 import domain.card.Value;
@@ -33,5 +34,32 @@ class StandTest {
     @Test
     void isBlackJackTest() {
         assertThat(playerState.isBlackJack()).isFalse();
+    }
+
+    @DisplayName("딜러보다 점수가 크면, 결과 값은 WIN으로 계산된다.")
+    @Test
+    void calculateResultTestWhenGreaterThanDealer() {
+        Hand dealerHand = new Hand(new ArrayList<>(List.of(new Card(Value.THREE, Shape.HEART))));
+        State dealerState = new Stand(dealerHand);
+
+        assertThat(playerState.calculateResult(dealerState)).isEqualTo(Result.WIN);
+    }
+
+    @DisplayName("딜러보다 점수가 작으면, 결과 값은 LOSE로 계산된다.")
+    @Test
+    void calculateResultTestWhenLessThanDealer() {
+        Hand dealerHand = new Hand(new ArrayList<>(List.of(new Card(Value.QUEEN, Shape.HEART), new Card(Value.KING, Shape.HEART))));
+        State dealerState = new Stand(dealerHand);
+
+        assertThat(playerState.calculateResult(dealerState)).isEqualTo(Result.LOSE);
+    }
+
+    @DisplayName("딜러와 점수가 같으면, 결과 값은 TIE로 계산된다.")
+    @Test
+    void calculateResultTestWhenEqualThanDealer() {
+        Hand dealerHand = new Hand(new ArrayList<>(List.of(new Card(Value.JACK, Shape.HEART), new Card(Value.THREE, Shape.HEART))));
+        State dealerState = new Stand(dealerHand);
+
+        assertThat(playerState.calculateResult(dealerState)).isEqualTo(Result.TIE);
     }
 }
