01e8536124aed3980b96a416b04c12e0422ea919
odo27 <odo27@naver.com>
2023-03-13 14:51:27 +0900

    refactor: 선언시 할당하게 변경

--- src/main/java/blackjack/domain/bet/BetManager.java
+++ src/main/java/blackjack/domain/bet/BetManager.java
@@ -7,11 +7,7 @@ import blackjack.domain.participant.Player;
 
 public class BetManager {
 
-    private final Map<Player, Money> betManager;
-
-    public BetManager() {
-        betManager = new HashMap<>();
-    }
+    private final Map<Player, Money> betManager = new HashMap<>();
 
     public void add(Player player, Money betMoney) {
         betManager.put(player, betMoney);
