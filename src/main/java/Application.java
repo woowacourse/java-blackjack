00f6d784eedd1a201645f02cae1684884a57eb4a
splitube <voicesplit@daum.net>
2023-03-13 15:59:53 +0900

    feat: Application main 작성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,9 @@
-import controller.GameController;
-
 public class Application {
 
     public static void main(String[] args) {
-        GameController gameController = GameController.makeWithInput();
-        gameController.ready();
-        gameController.play();
-        gameController.printFinalGameResult();
+        GameMachine gameMachine = new GameMachine();
+        gameMachine.ready();
+        gameMachine.play();
+        gameMachine.printResult();
     }
 }
