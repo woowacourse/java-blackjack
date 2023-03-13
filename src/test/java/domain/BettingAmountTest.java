de49877044d1108499cdfcee7571fa53f9ff2e03
SproutMJ <mjk54565@gmail.com>
2023-03-13 17:40:19 +0900

    test: 경곗값 테스트 추가

--- src/test/java/domain/BettingAmountTest.java
+++ src/test/java/domain/BettingAmountTest.java
@@ -2,7 +2,7 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
@@ -11,5 +11,17 @@ class BettingAmountTest {
     @Test
     void 음수인_경우() {
         assertThatThrownBy(() -> new BettingAmount(-10000));
+    }
+
+    @DisplayName("0인 경우 검증")
+    @Test
+    void ZERO인_경우() {
+        assertDoesNotThrow(() -> new BettingAmount(0));
+    }
+
+    @DisplayName("자연수인 경우 검증")
+    @Test
+    void 자연수인_경우() {
+        assertDoesNotThrow(() -> new BettingAmount(10000));
     }
 }
