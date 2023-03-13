08948754e135bcf01d143a7659b26aa827285af7
Youngho <cyh6099@gmail.com>
2023-03-13 22:40:13 +0900

    refactor: Participants 인스턴스 변수 구조 수정

--- src/main/java/domain/participant/Participants.java
+++ src/main/java/domain/participant/Participants.java
@@ -1,7 +1,5 @@
 package domain.participant;
 
-import domain.DomainException;
-import domain.ExceptionCode;
 import domain.card.Deck;
 
 import java.util.ArrayList;
@@ -12,44 +10,31 @@ import java.util.stream.Collectors;
 
 // TODO: 2023/03/13 list, dealer분리 
 public class Participants {
-//    private final List<Participant> players;
-//    private final Participant dealer;
+    private final List<Player> players;
+    private final Participant dealer;
 
-    private final List<Participant> participants;
-
-    private Participants(List<Participant> participants) {
-//        this.players = participants;
-//        dealer = new Dealer();
-        this.participants = participants;
+    private Participants(List<Player> participants) {
+        this.players = participants;
+        dealer = new Dealer();
     }
 
     public static Participants from(List<Player> players) {
-        List<Participant> participants = new ArrayList<>();
-        participants.add(new Dealer());
-        participants.addAll(players);
-        return new Participants(participants);
+        return new Participants(players);
     }
 
     public void deal(Deck deck) {
-        for (Participant participant : participants) {
-            participant.receiveCard(deck.draw());
+        dealer.receiveCard(deck.draw());
+        for (Participant player : players) {
+            player.receiveCard(deck.draw());
         }
     }
 
     public Participant findDealer() {
-        return participants.stream()
-                .filter(participant -> participant.getClass().equals(Dealer.class))
-                .findFirst()
-                .orElseThrow(() -> new DomainException(ExceptionCode.NO_DEALER)
-                );
+        return dealer;
     }
 
     public List<Participant> findPlayers() {
-        List<Participant> players = new ArrayList<>();
-        for (Participant participant : participants) {
-            addParticipantIfPlayer(players, participant);
-        }
-        return players;
+        return new ArrayList<>(players);
     }
 
     public boolean shouldDealerHit() {
@@ -63,12 +48,6 @@ public class Participants {
             participantsHandValue.put(participant, participant.getHandValue());
         }
         return participantsHandValue;
-    }
-
-    private void addParticipantIfPlayer(List<Participant> players, Participant participant) {
-        if (participant.getClass().equals(Player.class)) {
-            players.add(participant);
-        }
     }
 
     public List<String> getPlayersName() {
