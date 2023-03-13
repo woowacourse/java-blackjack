2b0844b8f06269227149d5796d197f346343dc15
Ethan <kim76744dev@gmail.com>
2023-03-13 17:25:16 +0900

    refactor: Bet에서 사용하지 않는 메서드 삭제

--- src/main/java/model/money/Bet.java
+++ src/main/java/model/money/Bet.java
@@ -1,13 +1,8 @@
 package model.money;
 
-import java.util.Objects;
-import model.user.GameState;
-
 public class Bet {
 
-    private static final double MULTIPLE_VALUE = 1.5;
     private static final int MINIMUM_BET = 10_000;
-    private static final Bet minimumBet = new Bet(MINIMUM_BET);
 
     private final long money;
 
@@ -28,51 +23,6 @@ public class Bet {
 
     public Bet add(final Bet bet) {
         return new Bet(this.money + bet.money);
-    }
-
-    public Bet calculateBet(final GameState gameState) {
-        if (gameState.isBlackJack()) {
-            return blackJack();
-        }
-
-        return judgeFinalBet(gameState);
-    }
-
-    private Bet judgeFinalBet(final GameState gameState) {
-        if (gameState.isWin()) {
-            return this;
-        }
-
-        if (gameState.isLose()) {
-            return lose();
-        }
-
-        return draw();
-    }
-
-    private Bet lose() {
-        return new Bet(-money);
-    }
-
-    private Bet draw() {
-        return minimumBet;
-    }
-
-    public Bet blackJack() {
-        return new Bet((long) (money * MULTIPLE_VALUE));
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        final Bet bet1 = (Bet) o;
-        return Objects.equals(money, bet1.money);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(money);
     }
 
     @Override
