79d11697ed84e2e372a6a1bc62b1f08ea0436dbc
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-03-13 11:21:17 +0900

    test: 카드 추가 시 버스트된 상황 테스트 구현

--- src/test/java/domain/blackjack/gamestate/StartTest.java
+++ src/test/java/domain/blackjack/gamestate/StartTest.java
@@ -16,6 +16,7 @@ class StartTest {
     private static final Card SPADE_TEN = new Card(TrumpCardType.SPADE, TrumpCardNumber.TEN);
     private static final Card SPADE_ACE = new Card(TrumpCardType.SPADE, TrumpCardNumber.ACE);
     private static final Card SPADE_NINE = new Card(TrumpCardType.SPADE, TrumpCardNumber.NINE);
+    private static final Card HEART_NINE = new Card(TrumpCardType.HEART, TrumpCardNumber.NINE);
 
     @DisplayName("게임은 카드와 함께 시작한다.")
     @Test
@@ -49,7 +50,7 @@ class StartTest {
         assertThat(gameState).isNotInstanceOf(Start.class);
     }
 
-    @DisplayName("게임 시작시 카드를 추가로 받을 수 있다.")
+    @DisplayName("게임 시작 후 카드를 추가로 받을 수 있다.")
     @Test
     void receiveSuccessTest() {
         Cards cards = Cards.of(SPADE_TEN, SPADE_NINE);
@@ -58,5 +59,19 @@ class StartTest {
         gameState = gameState.receive(SPADE_ACE);
         Cards stateCards = gameState.getCards();
         assertThat(stateCards.getCards()).hasSize(3);
+    }
+
+    @DisplayName("게임 시작 후 추가로 받은 카드의 합이 21을 넘으면 버스타 상태가 된다.")
+    @Test
+    void createBustFromStartSuccessTest() {
+        Cards cards = Cards.of(SPADE_TEN, SPADE_NINE);
+        GameState gameState = Start.from(cards);
+
+        gameState = gameState.receive(HEART_NINE);
+        BlackjackScore stateScore = BlackjackScore.from(gameState.getCards());
+        assertThat(stateScore.isGreaterThan(BlackjackScore.getMaxScore())).isTrue();
+
+        assertThat(gameState).isInstanceOf(Bust.class);
+        assertThat(gameState).isNotInstanceOf(Start.class);
     }
 }
