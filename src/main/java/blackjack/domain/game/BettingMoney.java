bbc36779dc33b96cc4d938144c97ffbe6e467d73
kim0914 <kosa0914@naver.com>
2023-03-14 01:02:43 +0900

    feat(BettingMoney): 배팅 금액 검증 사항 추가

--- src/main/java/blackjack/domain/game/BettingMoney.java
+++ src/main/java/blackjack/domain/game/BettingMoney.java
@@ -4,17 +4,25 @@ public class BettingMoney {
 
     private static final int BETTING_MIN_VALUE = 1_000;
     private static final int BETTING_MAX_VALUE = 100_000_000;
+    private static final int MONEY_UNIT = 100;
 
     private final int money;
 
     public BettingMoney(int money) {
         validateRange(money);
+        validateUnit(money);
         this.money = money;
     }
 
     private void validateRange(int value) {
         if (value < BETTING_MIN_VALUE || value > BETTING_MAX_VALUE) {
             throw new IllegalArgumentException("배팅 금액은 최소 1천원, 최대 1억원까지 가능합니다.");
+        }
+    }
+
+    private void validateUnit(int money) {
+        if (money % MONEY_UNIT > 0) {
+            throw new IllegalArgumentException("배팅 금액은 백원 단위로 입력 가능합니다.");
         }
     }
 
--- src/test/java/blackjack/domain/game/BettingMoneyTest.java
+++ src/test/java/blackjack/domain/game/BettingMoneyTest.java
@@ -17,10 +17,20 @@ public class BettingMoneyTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {-500, 0, 100, 500_000_000})
+    @ValueSource(ints = {-1000, 0, 900, 100_000_100, 500_000_000})
     @DisplayName("배팅 금액 허용 범위와 다르면 예외가 발생한다.")
     void createBettingWithWrongRange(int betting) {
         assertThatThrownBy(() -> new BettingMoney(betting))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("배팅 금액은 최소 1천원, 최대 1억원까지 가능합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1001, 1250, 15550, 55123, 9999_9999})
+    @DisplayName("배팅 금액 단위가 백원 단위가 아니면 예외가 발생한다.")
+    void createBettingWithWrongMoneyUnit(int betting) {
+        assertThatThrownBy(() -> new BettingMoney(betting))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("배팅 금액은 백원 단위로 입력 가능합니다.");
     }
 }
