05d0da94424c9f4b144442969878170e5c175e20
java-saeng <gusdn85554@naver.com>
2023-03-13 12:14:35 +0900

    feat : 배팅 상태 판단하는 클래스 추가

---
+++ src/main/java/domain/player/participant/betresult/resultfinder/DealerBlackjack.java
@@ -0,0 +1,19 @@
+package domain.player.participant.betresult.resultfinder;
+
+import domain.player.dealer.Dealer;
+import domain.player.participant.Participant;
+import domain.player.participant.betresult.resultstate.BetResultState;
+import domain.player.participant.betresult.resultstate.LoseState;
+
+public class DealerBlackjack implements BetResult {
+
+    @Override
+    public Boolean canApply(final Participant participant, final Dealer dealer) {
+        return !participant.isBlackjack() && dealer.isBlackjack();
+    }
+
+    @Override
+    public BetResultState state() {
+        return new LoseState();
+    }
+}
---
+++ src/main/java/domain/player/participant/betresult/resultfinder/DealerBust.java
@@ -0,0 +1,19 @@
+package domain.player.participant.betresult.resultfinder;
+
+import domain.player.dealer.Dealer;
+import domain.player.participant.Participant;
+import domain.player.participant.betresult.resultstate.BetResultState;
+import domain.player.participant.betresult.resultstate.BreakEvenState;
+
+public class DealerBust implements BetResult {
+
+    @Override
+    public Boolean canApply(final Participant participant, final Dealer dealer) {
+        return participant.canHit() && dealer.isBust();
+    }
+
+    @Override
+    public BetResultState state() {
+        return new BreakEvenState();
+    }
+}
