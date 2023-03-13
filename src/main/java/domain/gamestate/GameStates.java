7f58acb657a6eedafb1fb57d880f3e70489c8b7c
This2sho <zx00018@naver.com>
2023-03-13 12:45:26 +0900

    style(GameStates): 개행 추가

--- src/main/java/domain/gamestate/GameStates.java
+++ src/main/java/domain/gamestate/GameStates.java
@@ -1,6 +1,7 @@
 package domain.gamestate;
 
 public class GameStates {
+
     public static Win WIN = new Win();
     public static BlackJack BLACKJACK = new BlackJack();
     public static Lose LOSE = new Lose();
