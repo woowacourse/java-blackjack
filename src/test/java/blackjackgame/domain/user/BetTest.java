4e00644316c24d42a8da81c1f932b4c346ce4d3e
yujamint <woawoa0513@gmail.com>
2023-03-13 14:31:23 +0900

    test: 메서드명 오타 수정 및 Bet 생성 성공 케이스 추가

--- src/test/java/blackjackgame/domain/user/BetTest.java
+++ src/test/java/blackjackgame/domain/user/BetTest.java
@@ -10,7 +10,14 @@ class BetTest {
     @DisplayName("0원 이하의 금액으로 베팅하면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {0, -5000, -10000})
-    void vetAmountLessThanZero(int amount) {
+    void betAmountLessThanMinimumValue(int amount) {
         assertThrows(IllegalArgumentException.class, () -> new Bet(amount));
+    }
+
+    @DisplayName("0원 초과의 금액으로 베팅하면 Bet 객체가 생성된다.")
+    @ParameterizedTest
+    @ValueSource(ints = {5000, 15000, 200000})
+    void betAmountGreaterThanMinimumValue(int amount) {
+        assertDoesNotThrow(() -> new Bet(amount));
     }
 }
