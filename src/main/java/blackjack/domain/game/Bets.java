eea5ba3a34ee481b836096bed9a00bd7904227a4
songusika <sws981202@gmail.com>
2023-03-13 15:04:00 +0900

    feat: 금액을 베팅하는 기능 구현
    Co-authored-by: choi-jjunho <junho5336@gmail.com>

---
+++ src/main/java/blackjack/domain/game/Bets.java
@@ -0,0 +1,53 @@
+package blackjack.domain.game;
+
+import blackjack.domain.user.Player;
+import blackjack.domain.user.User;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public final class Bets {
+
+    final Map<User, Money> bets = new LinkedHashMap<>();
+
+    public void betting(final Player player, final Money money) {
+        bets.put(player, money);
+    }
+
+
+    public void updatePrizes(final Map<User, Result> result) {
+        bets.keySet().stream().forEach((user) -> {
+            updateUserPrize(user, result.get(user));
+        });
+    }
+
+    private void updateUserPrize(final User user, final Result userResult) {
+        Money money = bets.get(user);
+        final Map<GameResult, Integer> userGameResult = userResult.getResult();
+        final List<GameResult> positiveResult = userGameResult.keySet()
+                .stream()
+                .filter(gameResult -> userGameResult.get(gameResult) != 0)
+                .collect(Collectors.toList());
+
+        for (GameResult result : positiveResult) {
+            money = money.calculatePrize(result);
+        }
+
+        bets.put(user, money);
+    }
+
+    public Money getDealerProfit() {
+        Money dealerMoney = Money.getMin();
+
+        for (User user : bets.keySet()) {
+            dealerMoney = dealerMoney.add(bets.get(user));
+        }
+
+        return dealerMoney.multiply(-1);
+    }
+
+    public Map<User, Money> getBets() {
+        return new LinkedHashMap<>(bets);
+    }
+}
