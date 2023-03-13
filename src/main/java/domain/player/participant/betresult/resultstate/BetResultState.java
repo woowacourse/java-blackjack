69398fe604482fd081990241a1c8858e64cd36d4
java-saeng <gusdn85554@naver.com>
2023-03-13 14:49:25 +0900

    refactor : @FunctionalInterface 추가 및 NULL object 삭제

--- src/main/java/domain/player/participant/betresult/resultstate/BetResultState.java
+++ src/main/java/domain/player/participant/betresult/resultstate/BetResultState.java
@@ -2,9 +2,8 @@ package domain.player.participant.betresult.resultstate;
 
 import domain.player.participant.Money;
 
+@FunctionalInterface
 public interface BetResultState {
-
-    BetResultState NULL = money -> Money.MIN;
 
     Money calculateBetOutcomeOf(final Money money);
 }
