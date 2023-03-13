837ef7197b702c90ee80c58c82f7b43ee8283ed3
Wugawuga <dnr3655@naver.com>
2023-03-13 14:06:58 +0900

    remove: 구현 완료되지 않은 학습 테스트 주석

--- src/test/java/jcf/mission/GenericStudy.java
+++ src/test/java/jcf/mission/GenericStudy.java
@@ -52,17 +52,17 @@ public class GenericStudy {
         final SimpleList<Integer> filteredIntValues = SimpleList.filterNegative(intValues);
     }
 
-    @Test
-    void test5() {
-        final var laserPrinter = new LaserPrinter();
-
-        final SimpleList<Printer> printers = new SimpleArrayList<Printer>();
-        final SimpleList<LaserPrinter> laserPrinters = new SimpleArrayList<LaserPrinter>(laserPrinter);
-
-        SimpleList.copy(laserPrinters, printers);
-
-        System.out.println(printers.get(0) == laserPrinter); // true
-    }
+//    @Test
+//    void test5() {
+//        final var laserPrinter = new LaserPrinter();
+//
+//        final SimpleList<Printer> printers = new SimpleArrayList<Printer>();
+//        final SimpleList<LaserPrinter> laserPrinters = new SimpleArrayList<LaserPrinter>(laserPrinter);
+//
+//        SimpleList.copy(laserPrinters, printers);
+//
+//        System.out.println(printers.get(0) == laserPrinter); // true
+//    }
 
     public class Printer {
     }
