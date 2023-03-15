43cddd5890449bf2e11f7db98b9851bc14b6cafa
SproutMJ <mjk54565@gmail.com>
2023-03-15 11:57:20 +0900

    refactor: Deck 생성자 로직 변경

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -1,37 +1,30 @@
 package domain;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Queue;
 import java.util.stream.Collectors;
 
 public class Deck {
     private final Queue<Card> deck;
 
-    private Deck(Queue<Card> cards) {
-        this.deck = cards;
+    private Deck(List<Symbol> symbols, List<CardNumber> cardNumbers) {
+        this.deck = new LinkedList<>(generateCards(symbols, cardNumbers));
     }
 
     public static Deck of(List<Symbol> symbols, List<CardNumber> cardNumbers) {
-        List<Card> cards = new LinkedList<>();
-        generateCards(symbols, cardNumbers, cards);
-        Deck deck = new Deck(new LinkedList<>(cards));
-        return deck;
+        return new Deck(symbols, cardNumbers);
     }
 
-    private static void generateCards(List<Symbol> symbols, List<CardNumber> cardNumbers, List<Card> cards) {
-        if (Objects.isNull(symbols) || Objects.isNull(cardNumbers)) {
-            symbols = Arrays.stream(Symbol.values()).collect(Collectors.toList());
-            cardNumbers = Arrays.stream(CardNumber.values()).collect(Collectors.toList());
-        }
+    private static List<Card> generateCards(List<Symbol> symbols, List<CardNumber> cardNumbers) {
+        List<Card> cards = new LinkedList<>();
         for (Symbol symbol : symbols) {
             addCardsWithSymbolOf(symbol, cardNumbers, cards);
         }
         Collections.shuffle(cards);
+        return cards;
     }
 
     private static void addCardsWithSymbolOf(Symbol symbol, List<CardNumber> cardNumbers, List<Card> cards) {
@@ -42,9 +35,8 @@ public class Deck {
 
     public Card draw() {
         if (deck.isEmpty()) {
-            List<Card> cards = new ArrayList<>();
-            generateCards(null, null, cards);
-            this.deck.addAll(cards);
+            this.deck.addAll(generateCards(Arrays.stream(Symbol.values()).collect(Collectors.toList()),
+                    Arrays.stream(CardNumber.values()).collect(Collectors.toList())));
         }
         return deck.remove();
     }
