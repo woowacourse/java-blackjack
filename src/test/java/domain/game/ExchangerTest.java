3ab8f9f1c9f4fbc949f6f6b454871031aced57de
hgo641 <hgo641@gmail.com>
2023-03-15 09:55:32 +0900

    refactor: card를 상수로 분리

--- src/test/java/domain/game/ExchangerTest.java
+++ src/test/java/domain/game/ExchangerTest.java
@@ -14,18 +14,16 @@ import domain.user.Dealer;
 import domain.user.Player;
 import domain.user.Players;
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ExchangerTest {
-    /*
-  - [ ] 수익 계산
-  - [ ] 플레이어의 핸드가 `Blackjack`인 상태로 승리했다면, 플레이어는 베팅한 금액의 1.5배인 수익을 얻는다
-  - [ ] 플레이어의 핸드가 `Blackjack`이 아닌 상태로 승리했다면, 플레이어는 베팅한 금액만큼의 수익을 얻는다
-  - [ ] 플레이어와 딜러가 무승부이면 아무도 수익을 얻지 않는다
-  - [ ] 플레이어가 패배했다면, 플레이어는 베팅한 금액만큼 돈을 잃는다
-  - [ ] 딜러는 플레이어가 수익을 얻은 만큼 돈을 잃고, 플레이어가 돈을 잃은 만큼 수익을 얻는다
-     */
+    public static final Card ACE = new Card(Denomination.ACE, Suits.HEART);
+    public static final Card SIX = new Card(Denomination.SIX, Suits.HEART);
+    public static final Card EIGHT = new Card(Denomination.EIGHT, Suits.HEART);
+    public static final Card NINE = new Card(Denomination.NINE, Suits.HEART);
+    public static final Card TEN = new Card(Denomination.TEN, Suits.HEART);
     private final Player player = new Player(new Name("hongo"));
     private final Dealer dealer = new Dealer();
     private final Players players = Players.of(List.of(player));
@@ -36,12 +34,12 @@ public class ExchangerTest {
     @DisplayName("플레이어가 블랙잭으로 승리하여 1.5배의 수익을 얻는다")
     @Test
     void getWinningMoneyOfPlayer_WhenPlayerIsBlackjackWin() {
-        player.hit(new Card(Denomination.ACE, Suits.HEART));
-        player.hit(new Card(Denomination.TEN, Suits.HEART));
+        player.hit(ACE);
+        player.hit(TEN);
 
-        dealer.hit(new Card(Denomination.TEN, Suits.HEART));
-        dealer.hit(new Card(Denomination.TEN, Suits.SPADE));
-        dealer.hit(new Card(Denomination.ACE, Suits.HEART));
+        dealer.hit(TEN);
+        dealer.hit(TEN);
+        dealer.hit(ACE);
 
         assertThat(exchanger.getWinningMoneyOfPlayer(player, dealer)).isEqualTo(new Money(15000));
     }
@@ -49,11 +47,11 @@ public class ExchangerTest {
     @DisplayName("플레이어가 승리하여 베팅 금액만큼의 수익을 얻는다")
     @Test
     void getWinningMoneyOfPlayer_WhenPlayerIsWin() {
-        player.hit(new Card(Denomination.NINE, Suits.HEART));
-        player.hit(new Card(Denomination.TEN, Suits.HEART));
+        player.hit(NINE);
+        player.hit(TEN);
 
-        dealer.hit(new Card(Denomination.EIGHT, Suits.HEART));
-        dealer.hit(new Card(Denomination.TEN, Suits.SPADE));
+        dealer.hit(EIGHT);
+        dealer.hit(TEN);
 
         assertThat(exchanger.getWinningMoneyOfPlayer(player, dealer)).isEqualTo(new Money(10000));
     }
@@ -61,11 +59,11 @@ public class ExchangerTest {
     @DisplayName("플레이어가 패배하여 베팅 금액만큼의 손해를 본다")
     @Test
     void getWinningMoneyOfPlayer_WhenPlayerIsLOSE() {
-        player.hit(new Card(Denomination.SIX, Suits.HEART));
-        player.hit(new Card(Denomination.TEN, Suits.HEART));
+        player.hit(SIX);
+        player.hit(TEN);
 
-        dealer.hit(new Card(Denomination.EIGHT, Suits.HEART));
-        dealer.hit(new Card(Denomination.TEN, Suits.SPADE));
+        dealer.hit(EIGHT);
+        dealer.hit(TEN);
 
         assertThat(exchanger.getWinningMoneyOfPlayer(player, dealer)).isEqualTo(new Money(-10000));
     }
@@ -73,11 +71,11 @@ public class ExchangerTest {
     @DisplayName("플레이어가 무승부를 하여 수익을 얻지 않는다")
     @Test
     void getWinningMoneyOfPlayer_WhenPlayerIsPush() {
-        player.hit(new Card(Denomination.EIGHT, Suits.HEART));
-        player.hit(new Card(Denomination.TEN, Suits.HEART));
+        player.hit(EIGHT);
+        player.hit(TEN);
 
-        dealer.hit(new Card(Denomination.EIGHT, Suits.HEART));
-        dealer.hit(new Card(Denomination.TEN, Suits.SPADE));
+        dealer.hit(EIGHT);
+        dealer.hit(TEN);
 
         assertThat(exchanger.getWinningMoneyOfPlayer(player, dealer)).isEqualTo(new Money(0));
     }
