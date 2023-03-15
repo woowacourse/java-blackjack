e0ba22c7785af6acb95f47b975e2802ec59bbd51
echo <echo.backend@gmail.com>
2023-03-16 00:20:02 +0900

    feat(InputView): 배팅 단위 메세지 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -14,7 +14,7 @@ public class InputView {
     public static final String STOP = "n";
     public static final String PARTICIPANTS_NAME_REQUEST_PROMPT_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     public static final String MORE_CARD_REQUEST_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
-    public static final String BETTING_MONEY_REQUEST_MESSAGE = "%s의 배팅 금액은?";
+    public static final String BETTING_MONEY_REQUEST_MESSAGE = "%s의 배팅 금액은?(100원 단위로 입력)";
     public static final String NOT_NUMBER_ERROR_MESSAGE = "잘못된 입력입니다. 숫자만 입력 가능합니다.";
     public static final String DELIMITER = ",";
     private static final Scanner scanner = new Scanner(System.in);
