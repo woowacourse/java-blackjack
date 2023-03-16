ef81dd05531aa9ce9ac1639a8afe4c55ec2ec30a
zillionme <beatus96@naver.com>
2023-03-17 08:18:22 +0900

    refactor: User에 hashcode, equals 오버라이딩

--- src/main/java/blackjack/domain/participants/User.java
+++ src/main/java/blackjack/domain/participants/User.java
@@ -1,35 +1,46 @@
 package blackjack.domain.participants;
 
+import java.util.Objects;
+
 public class User {
 
     private static final int MAX_NAME_LENGTH = 100;
 
-    private final String Username;
+    private final String username;
 
-    public User(final String name) {
-        validateName(name);
-        Username = name;
+    User(String username) {
+        validateName(username);
+        this.username = username;
     }
 
-    private void validateName(final String name) {
+    private void validateName(String name) {
         validateEmptyName(name);
         validateLength(name);
     }
 
-
-    private void validateEmptyName(final String name) {
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("이름을 입력하지 않았습니다");
-        }
+    private void validateEmptyName(String name) {
+        if (name == null || name.isBlank()) throw new IllegalArgumentException("이름을 입력하지 않았습니다");
     }
 
-    private void validateLength(final String name) {
-        if (name.length() > MAX_NAME_LENGTH) {
+    private void validateLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH)
             throw new IllegalArgumentException("이름이 " + MAX_NAME_LENGTH + "글자를 초과했습니다");
-        }
     }
 
-    public String getName() {
-        return Username;
+    String getName() {
+        return username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
     }
 }
