8c3d2641b43afe2670f3a165dec3119bbf98499f
iamjooon2 <iamjooon2@gmail.com>
2023-03-17 00:16:53 +0900

    refactor: OutputView의 인스턴스화를 막기 위한 private 생성자 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,6 +9,9 @@ import java.util.stream.Collectors;
 public class OutputView {
     private static final String ENTER_LINE = System.lineSeparator();
 
+    private OutputView() {
+    }
+
     public static void printStartMessage(List<ParticipantDto> ParticipantDtos) {
         List<String> playerNames = ParticipantDtos.stream()
                 .map(ParticipantDto -> ParticipantDto.getName())
