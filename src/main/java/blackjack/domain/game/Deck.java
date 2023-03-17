6ff3f3f07e5606dfa2c350859fbf8b49e1ff80f3
parkmuhyeun <pjhg410@gmail.com>
2023-03-17 22:54:37 +0900

    feat: Deck 캐싱 기능 추가

--- src/main/java/blackjack/domain/game/Deck.java
+++ src/main/java/blackjack/domain/game/Deck.java
@@ -10,31 +10,38 @@ public class Deck {
     private static final int STANDARD_LOWER_BOUNDARY = 2;
     private static final int STANDARD_UPPER_BOUNDARY = 10;
     private static final List<String> COURT_VALUES = List.of("J", "Q", "K");
+    private static final List<Card> CACHE;
 
-    private List<Card> cards = new ArrayList<>();
+    private final List<Card> cards;
 
     public Deck(CardShuffle cardShuffle) {
+        final ArrayList<Card> copyCards = new ArrayList<>(CACHE);
+        this.cards = cardShuffle.shuffle(copyCards);
+    }
+
+    static {
         final Pattern[] values = Pattern.values();
 
+        List<Card> cards = new ArrayList<>();
         for (Pattern pattern : values) {
-            addAllCardByPattern(pattern);
+            addAllCardByPattern(cards, pattern);
         }
-        this.cards = cardShuffle.shuffle(cards);
+        CACHE = cards;
     }
 
-    private void addAllCardByPattern(Pattern pattern) {
+    private static void addAllCardByPattern(List<Card> cards, Pattern pattern) {
         cards.add(new AceCard(pattern));
-        addStandardCard(pattern);
-        addCourtCard(pattern);
+        addStandardCard(cards, pattern);
+        addCourtCard(cards, pattern);
     }
 
-    private void addCourtCard(Pattern pattern) {
+    private static void addCourtCard(List<Card> cards, Pattern pattern) {
         for (String courtValue : COURT_VALUES) {
             cards.add(new CourtCard(pattern, courtValue));
         }
     }
 
-    private void addStandardCard(Pattern pattern) {
+    private static void addStandardCard(List<Card> cards, Pattern pattern) {
         for (int value = STANDARD_LOWER_BOUNDARY; value <= STANDARD_UPPER_BOUNDARY; value++) {
             cards.add(new StandardCard(pattern, String.valueOf(value)));
         }
