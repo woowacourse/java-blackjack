7f2fe26334063384b4982152efb427a9c58f35df
kiarakim <kiarakim413@gmail.com>
2023-03-19 02:27:12 +0900

    feat: 플레이어에게 배팅 금액을 묻는 기능 구현

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -23,6 +23,7 @@ public class Controller {
 		List<String> playerNames = askPlayerNames();
 		Deck deck = new Deck(new RandomShuffleStrategy());
 		Users players = Users.from(playerNames, deck);
+		bet(players.getPlayers());
 		BlackJack blackJack = new BlackJack(players, deck);
 		printInitMessage(players);
 		printDealerCardHidden(blackJack.getDealerCardHidden());
@@ -30,7 +31,14 @@ public class Controller {
 		return blackJack;
 	}
 
+	private void bet(List<Player> players) {
+		for(Player player : players){
+			player.setBet(askPlayerBet(player.getName()));
+		}
+	}
+
 	private void play(final BlackJack blackJack) {
+		blank();
 		for (Player player : blackJack.getPlayers()) {
 			askPlayerHit(player, blackJack);
 		}
