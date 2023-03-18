57ceef86abd3a413a3523cadbe18c2f85ca66575
chaewon121 <cw121@naver.com>
2023-03-19 06:40:38 +0900

    refactor: 게터 수정

--- src/main/java/blackjack/domain/player/User.java
+++ src/main/java/blackjack/domain/player/User.java
@@ -2,28 +2,31 @@ package blackjack.domain.player;
 
 import blackjack.domain.card.Card;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class User {
+
     protected Hand hand;
     protected Name name;
 
     public User(Name name) {
-        this.hand = new Hand();
+        this.hand = new Hand(new ArrayList<>());
         this.name = name;
     }
 
-    public List<Card> getPlayerCards() {
-        return hand.getPlayerCards();
+    public void addCard(Card card){
+        List<Card> cards = this.hand.getHand();
+        cards.add(card);
+        this.hand = new Hand(cards);
     }
 
-    public int getTotalScore() {
-        return hand.getTotalScore();
+    public List<Card> getHand() {
+        return this.hand.getHand();
     }
 
-    public void updateCardScore(Card card) {
-        hand.addCard(card);
-        hand.updateTotalScore();
+    public String getUserName() {
+        return name.getName();
     }
 
     public abstract boolean isUnderLimit();
--- src/test/java/blackjack/domain/player/PlayerTest.java
+++ src/test/java/blackjack/domain/player/PlayerTest.java
@@ -12,10 +12,12 @@ class PlayerTest {
     @Test
     @DisplayName("Player의 total score가 21이 넘지 않으면 true를 반환한다.")
     void total_score_under_21() {
-        // given & when
-        Player player = new Player(new Name("merry"));
-        player.hand.addCard(new Card(CardNumber.ACE, CardSymbol.HEART));
-        player.hand.addCard(new Card(CardNumber.FIVE, CardSymbol.HEART));
+        // given
+        Player player = new Player(new Name("채채"), new BetAmount(1000));
+
+        // when
+        player.addCard(new Card(CardNumber.ACE, CardSymbol.HEART));
+        player.addCard(new Card(CardNumber.FIVE, CardSymbol.HEART));
 
         // then
         Assertions.assertThat(player.isUnderLimit()).isTrue();
@@ -24,11 +26,13 @@ class PlayerTest {
     @Test
     @DisplayName("User의 total score가 21이 넘으면 false를 반환한다.")
     void total_score_over_21() {
-        // given & when
-        Player player = new Player(new Name("메리"));
-        player.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
-        player.updateCardScore(new Card(CardNumber.FIVE, CardSymbol.HEART));
-        player.updateCardScore(new Card(CardNumber.KING, CardSymbol.HEART));
+        // given
+        Player player = new Player(new Name("메리"), new BetAmount(1000));
+
+        // when
+        player.addCard(new Card(CardNumber.KING, CardSymbol.HEART));
+        player.addCard(new Card(CardNumber.FIVE, CardSymbol.HEART));
+        player.addCard(new Card(CardNumber.KING, CardSymbol.HEART));
 
         // then
         Assertions.assertThat(player.isUnderLimit()).isFalse();
