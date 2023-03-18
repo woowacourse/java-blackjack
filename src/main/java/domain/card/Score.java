b10e86b7b4dc4b692fc8f46ed04c8f89b4ce3dd3
MoonJeWoong <munjin0201@naver.com>
2023-03-18 19:35:37 +0900

    refactor: score 내부 연산 기능 수정
    세부사항
    - score 객체 내부에서는 객체 간 연산이 아니라 원시타입 값으로 연산하도록 수정

--- src/main/java/domain/card/Score.java
+++ src/main/java/domain/card/Score.java
@@ -3,7 +3,7 @@ package domain.card;
 import java.util.Objects;
 
 public class Score {
-    private static final Score max = new Score(21);
+    private static final int MAX_SCORE = 21;
     private static final int DIFFERENCE_BETWEEN_ACE_ONE_AND_ELEVEN = 10;
 
     private final int score;
@@ -13,32 +13,32 @@ public class Score {
     }
 
     public Score add(Score other) {
-        return new Score(this.score + other.getScore());
+        return new Score(this.score + other.score);
     }
 
     public boolean isLessThanOrEqual(Score other) {
-        return isLessThan(other) || score == other.getScore();
+        return isLessThan(other) || score == other.score;
     }
 
     public boolean isLessThan(Score other) {
-        return score < other.getScore();
+        return score < other.score;
     }
 
     public boolean isOverMax() {
-        return max.isLessThan(this);
+        return MAX_SCORE < score;
     }
 
     public boolean isMax() {
-        return max.equals(this);
+        return score == MAX_SCORE;
     }
 
     public Score calculateAceAsOne(int aceCount) {
-        Score newScore = new Score(this.score);
-        while (aceCount > 0 && newScore.isOverMax()) {
-            newScore = new Score(newScore.getScore() - DIFFERENCE_BETWEEN_ACE_ONE_AND_ELEVEN);
+        int newScore = score;
+        while (0 < aceCount && MAX_SCORE < newScore) {
+            newScore -= DIFFERENCE_BETWEEN_ACE_ONE_AND_ELEVEN;
             aceCount--;
         }
-        return newScore;
+        return new Score(newScore);
     }
 
     public int getScore() {
