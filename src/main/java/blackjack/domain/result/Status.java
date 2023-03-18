7e700acedffdf19b22200fef6c1eb8ca686acef2
chaewon121 <cw121@naver.com>
2023-03-19 06:39:04 +0900

    refactor: 상태 이넘 연산 수정

--- src/main/java/blackjack/domain/result/Status.java
+++ src/main/java/blackjack/domain/result/Status.java
@@ -1,12 +1,16 @@
-package blackjack.domain;
+package blackjack.domain.result;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.player.TotalScore;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiPredicate;
 
 public enum Status {
     BLACKJACK((count, sum) -> count == 2 && sum == 21),
     BUST((count, sum) -> sum > 21),
-    NONE((count, sum) -> sum < 21);
+    NONE((count, sum) -> sum <= 21);
 
     private final BiPredicate<Integer, Integer> status;
 
@@ -14,22 +18,13 @@ public enum Status {
         this.status = status;
     }
 
-    public static Status of(int count, int sum) {
+    public static Status of(List<Card> cards) {
+        int count = cards.size();
+        int sum = TotalScore.calculateTotalScore(cards).getTotalScore();
+
         return Arrays.stream(Status.values())
                 .filter(status -> status.status.test(count, sum))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("찾을 수 없는 값 입니다."));
-    }
-
-    public boolean isBlackjack() {
-        return this == BLACKJACK;
-    }
-
-    public boolean isBust() {
-        return this == BUST;
-    }
-
-    public boolean isNone() {
-        return this == NONE;
     }
 }
--- src/test/java/blackjack/domain/StatusTest.java
+++ src/test/java/blackjack/domain/StatusTest.java
@@ -1,29 +1,38 @@
 package blackjack.domain;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardNumber;
+import blackjack.domain.card.CardSymbol;
+import blackjack.domain.result.Status;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 class StatusTest {
 
     @Test
     @DisplayName("of()는 두개의 인자를 받아 해당 이넘을 반환한다.")
     void blackjack() {
+        List<Card> cards = List.of(new Card(CardNumber.QUEEN, CardSymbol.HEART),new Card(CardNumber.ACE, CardSymbol.HEART));
         // then
-        Assertions.assertThat(Status.of(2,21)).isEqualTo(Status.BLACKJACK);
+        Assertions.assertThat(Status.of(cards)).isEqualTo(Status.BLACKJACK);
     }
 
     @Test
     @DisplayName("of()는 두개의 인자를 받아 해당 이넘을 반환한다.")
     void bust() {
+        List<Card> cards = List.of(new Card(CardNumber.QUEEN, CardSymbol.HEART),new Card(CardNumber.TWO, CardSymbol.HEART),new Card(CardNumber.JACK, CardSymbol.HEART));
         // then
-        Assertions.assertThat(Status.of(2,22)).isEqualTo(Status.BUST);
+        Assertions.assertThat(Status.of(cards)).isEqualTo(Status.BUST);
     }
 
     @Test
     @DisplayName("of()는 두개의 인자를 받아 해당 이넘을 반환한다.")
     void none() {
+        List<Card> cards = List.of(new Card(CardNumber.QUEEN, CardSymbol.HEART),new Card(CardNumber.ACE, CardSymbol.HEART),new Card(CardNumber.JACK, CardSymbol.HEART));
         // then
-        Assertions.assertThat(Status.of(2,20)).isEqualTo(Status.NONE);
+        Assertions.assertThat(Status.of(cards)).isEqualTo(Status.NONE);
     }
 }
