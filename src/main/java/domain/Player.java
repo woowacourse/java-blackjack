5640d03b244f392d03c8e121d1878442f2db8275
kiarakim <kiarakim413@gmail.com>
2023-03-19 02:25:42 +0900

    feat: 플레이어에 수익 정보 추가

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -4,7 +4,17 @@ import domain.card.Cards;
 
 public class Player extends User {
 
+	private double bet;
+
 	public Player(final String name, Cards cards) {
 		super(name, cards);
+	}
+
+	public void setBet(double bet){
+		this.bet = bet;
+	}
+
+	public double getBet(){
+		return bet;
 	}
 }
