bc1e734d8c6648448e727f42ee108656a4da938b
이건회 <rg970604@naver.com>
2023-03-21 03:21:02 +0900

    refactor : 결과 계산 역할 제거

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,9 +1,8 @@
 package view;
 
-import domain.Player;
-import domain.Result;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -17,10 +16,10 @@ public class OutputView {
     }
 
 
-    public void printCardsPerPlayer(final List<String> namesCopy, final List<List<String>> cardsCardsCopy) {
-        printCardsPerDealer(cardsCardsCopy.get(0));
-        for (int i = 0; i < namesCopy.size(); i++) {
-            printCurrentPlayerResult(namesCopy.get(i), cardsCardsCopy.get(i+1));
+    public void printCardsPerPlayer(final List<String> names, final List<List<String>> cards) {
+        printCardsPerDealer(cards.get(0));
+        for (int i = 0; i < names.size(); i++) {
+            printCurrentPlayerResult(names.get(i), cards.get(i + 1));
         }
     }
 
@@ -41,67 +40,26 @@ public class OutputView {
         String cardsMessage = String.join(", ", cards);
         System.out.println("딜러카드: " + cardsMessage + " - 결과 : " + cardSum);
     }
+
     public void printAllCardResult(final String name, final List<String> cards, final int cardSum) {
         String cardsMessage = String.join(", ", cards);
         System.out.println(name + "카드: " + cardsMessage + " - 결과 : " + cardSum);
     }
 
-    private static void addResultIfExists(int[] result, StringBuilder sb) {
-        if (result[0] != 0) {
-            sb.append(result[0]).append("승 ");
-        }
-        if (result[1] != 0) {
-            sb.append(result[1]).append("무 ");
-        }
-        if (result[2] != 0) {
-            sb.append(result[2]).append("패");
+    public void noticePlayerCannotReceiveCard(String playerName, int sumOfParticipantCards, boolean isPlayerCanAddCard) {
+        if (isPlayerCanAddCard){
+            System.out.println(playerName + "는 카드를 추가할 수 없습니다. 현재 총합 : " + sumOfParticipantCards);
         }
     }
 
-    private static void printResultsIfExists(List<Result> winningResult, int i) {
-        if (winningResult.get(i) == Result.LOSE) {
-            System.out.print("승" + System.lineSeparator());
-        }
-        if (winningResult.get(i) == Result.DRAW) {
-            System.out.print("무" + System.lineSeparator());
-        }
-        if (winningResult.get(i) == Result.WIN) {
-            System.out.print("패" + System.lineSeparator());
-        }
-    }
-
-    private static void makeDealerResult(final int[] results, final Result result) {
-        if (result == Result.WIN) {
-            results[0] = results[0] + 1;
-            return;
-        }
-        if (result == Result.DRAW) {
-            results[1] = results[1] + 1;
-            return;
-        }
-        results[2] = results[2] + 1;
-    }
-
-    public void noticePlayerCannotReceiveCard(Player player) {
-        System.out.println(player.getName() + "는 카드를 추가할 수 없습니다. 현재 총합 : " + player.sumOfParticipantCards());
-    }
-
-    public void printEachPlayersProfit(List<Player> players){
+    public void printDealerProfit(int dealerProfit) {
         System.out.println("## 최종 수익");
-        int finalDealerProfit = sumDelaerProfit(players);
-        System.out.println("딜러: " + finalDealerProfit);
-        for (Player player : players) {
-            System.out.println(player.getName()+": "+player.calculateFinalProfit());
-        }
+        System.out.println("딜러: " + dealerProfit);
     }
 
-    private static int sumDelaerProfit(List<Player> players) {
-        List<Integer> finalProfits = new ArrayList<>();
-        players.forEach(
-            player -> finalProfits.add(player.calculateFinalProfit())
-        );
-        int finalDealerProfit = - finalProfits.stream()
-                .mapToInt(Integer::intValue).sum();
-        return finalDealerProfit;
+    public void printEachPlayersProfit(Map<String, Integer> playerWinningResults) {
+        for (Entry<String, Integer> playerResult : playerWinningResults.entrySet()) {
+            System.out.println(playerResult.getKey() + ": " + playerResult.getValue());
+        }
     }
 }
