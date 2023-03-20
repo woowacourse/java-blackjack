cf400bee8a351997e114abbf9adf1d39b67ea6cf
0chil <0@chll.it>
2023-03-20 21:24:54 +0900

    refactor(Letter): 미리 만든 Score 객체를 반환한다

--- src/main/java/domain/card/Letter.java
+++ src/main/java/domain/card/Letter.java
@@ -4,24 +4,24 @@ import domain.hand.Score;
 
 public enum Letter {
 
-    ACE("A", 1),
-    TWO("2", 2),
-    THREE("3", 3),
-    FOUR("4", 4),
-    FIVE("5", 5),
-    SIX("6", 6),
-    SEVEN("7", 7),
-    EIGHT("8", 8),
-    NINE("9", 9),
-    TEN("10", 10),
-    KING("K", 10),
-    QUEEN("Q", 10),
-    JACK("J", 10);
+    ACE("A", new Score(1)),
+    TWO("2", new Score(2)),
+    THREE("3", new Score(3)),
+    FOUR("4", new Score(4)),
+    FIVE("5", new Score(5)),
+    SIX("6", new Score(6)),
+    SEVEN("7", new Score(7)),
+    EIGHT("8", new Score(8)),
+    NINE("9", new Score(9)),
+    TEN("10", new Score(10)),
+    KING("K", new Score(10)),
+    QUEEN("Q", new Score(10)),
+    JACK("J", new Score(10));
 
     private final String letter;
-    private final int score;
+    private final Score score;
 
-    Letter(String letter, int score) {
+    Letter(String letter, Score score) {
         this.letter = letter;
         this.score = score;
     }
@@ -35,6 +35,6 @@ public enum Letter {
     }
 
     public Score getScore() {
-        return new Score(score);
+        return score;
     }
 }
