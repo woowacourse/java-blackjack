2ad8f44a4068878c8b5580a16a78400b10c7e355
the9kim <the9rla@gmail.com>
2023-03-22 13:26:18 +0900

    refactor: 카드 생성로직 수정
    - 52장의 카드를 미리 만든 다음, CardDeck 객체가 생성 될 때 해당 카드 덱을 랜덤으로 섞음

--- src/main/java/domain/card/CardDeck.java
+++ src/main/java/domain/card/CardDeck.java
@@ -3,34 +3,33 @@ package domain.card;
 import java.util.*;
 
 public class CardDeck {
-    private static final int FIRST_CARD_INDEX_IN_DECK = 0;
-    private static final List<Card> CARDDECK;
+    private static Deque<Card> cardDeck;
 
     static {
-        CARDDECK = makeCards();
+        cardDeck = makeCards();
     }
 
-    private static List<Card> makeCards() {
-        List<Card> cards = new ArrayList<>();
+    private static Deque<Card> makeCards() {
+        Deque<Card> cards = new ArrayDeque<>();
         for (CardType cardType : CardType.values()) {
             addCards(cards, cardType);
         }
         return cards;
     }
 
-    private static void addCards(List<Card> cards, CardType cardType) {
+    private static void addCards(Deque<Card> cards, CardType cardType) {
         for (CardValue cardValue : CardValue.values()) {
             cards.add(new Card(cardType, cardValue));
         }
     }
 
-
-    public Card pickCard() {
-        Collections.shuffle(CARDDECK);
-        return CARDDECK.get(FIRST_CARD_INDEX_IN_DECK);
+    public CardDeck() {
+        List<Card> cardDeck = new ArrayList<>(CardDeck.cardDeck);
+        Collections.shuffle(cardDeck);
+        CardDeck.cardDeck = new ArrayDeque<>(cardDeck);
     }
 
-    public List<Card> getCardDeck() {
-        return CARDDECK;
+    public Card pickCard() {
+        return cardDeck.pop();
     }
 }
--- src/test/java/domain/card/CardDeckTest.java
+++ src/test/java/domain/card/CardDeckTest.java
@@ -8,23 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CardDeckTest {
 
     @Test
-    @DisplayName("카드덱을 생성하면 카드 타입과 값 별로 52장의 카드가 만들어진다")
-    void generateCardDeck() {
-
-        CardDeck cardDeck = new CardDeck();
-
-        assertThat(cardDeck.getCardDeck().size()).isEqualTo(52);
-    }
-
-    @Test
-    @DisplayName("pick 메서드를 실행하면 카드덱 스택이 섞인 후 맨 앞의 카드가 뽑힌다")
+    @DisplayName("pick 메서드를 실행하면 카드가 출력된다.")
     void pickCard() {
 
         CardDeck cardDeck = new CardDeck();
 
         Card poppedCard = cardDeck.pickCard();
-        Card firstCardInDeck = cardDeck.getCardDeck().get(0);
 
-        assertThat(poppedCard).isSameAs(firstCardInDeck);
+        assertThat(poppedCard).isInstanceOf(Card.class);
     }
 }
