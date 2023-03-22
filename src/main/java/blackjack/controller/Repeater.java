cd930888b9442211895a5c1657e1ff9fc526f3cd
zillionme <beatus96@naver.com>
2023-03-22 13:33:35 +0900

    refactor: 오류 발생 방지 반복 클래스의 메서드 인자 제네릭 타입 변경

--- src/main/java/blackjack/controller/Repeater.java
+++ src/main/java/blackjack/controller/Repeater.java
@@ -5,8 +5,11 @@ import java.util.function.Supplier;
 
 public class Repeater {
 
+    private Repeater() {
+    }
+
     public static <T> T repeatUntilNoException(final Supplier<T> supplier,
-                                               final Consumer<Exception> exceptionHandler) {
+                                               final Consumer<String> exceptionHandler) {
         T result = null;
         while (result == null) {
             result = createOutputOrNull(supplier, exceptionHandler);
@@ -14,12 +17,12 @@ public class Repeater {
         return result;
     }
 
-    public static <T> T createOutputOrNull(final Supplier<T> inputSupplier,
-                                           final Consumer<Exception> exceptionHandler) {
+    private static <T> T createOutputOrNull(final Supplier<T> inputSupplier,
+                                            final Consumer<String> exceptionHandler) {
         try {
             return inputSupplier.get();
         } catch (final IllegalArgumentException e) {
-            exceptionHandler.accept(e);
+            exceptionHandler.accept(e.getMessage());
             return null;
         }
     }
