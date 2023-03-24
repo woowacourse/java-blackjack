6b9c3251afc08dd14abf9abae29e965af15ede20
zillionme <beatus96@naver.com>
2023-03-24 14:46:05 +0900

    refactor: 플레이어 카드 bust시, 무조건 패로 변경

--- src/main/java/blackjack/domain/participants/status/stopped/Bust.java
+++ src/main/java/blackjack/domain/participants/status/stopped/Bust.java
@@ -1,23 +1,17 @@
 package blackjack.domain.participants.status.stopped;
 
 import blackjack.domain.game.ResultType;
-import blackjack.domain.game.Score;
 import blackjack.domain.participants.Hand;
 import blackjack.domain.participants.status.Status;
 
 public class Bust extends Stopped {
 
-    public Bust(Hand cards) {
+    public Bust(final Hand cards) {
         super(cards);
     }
 
     @Override
-    public ResultType findResultType(Status opponent) {
-        Score scoreOfOpponent = opponent.score();
-        if (scoreOfOpponent.isOverThanMax()) {
-            return ResultType.TIE;
-        }
-
+    public ResultType findResultType(final Status opponent) {
         return ResultType.LOSE;
     }
 
