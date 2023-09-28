75caf84ba892e19038fbce861135c70e35dff87b
devholic22 <hyunjoon.tech@gmail.com>
2023-09-28 15:03:10 +0900

    feat: 단일 입력값을 토대로 Name 객체를 만들 수도 있다.

---
+++ src/main/java/model/Name.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Name {
+
+    private final String name;
+
+    private Name(final String name) {
+        this.name = name;
+    }
+
+    public static Name from(final String name) {
+        return new Name(name);
+    }
+}
