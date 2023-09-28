cc2e35fac33530f7160889d0a47b79ae6c74c8b9
devholic22 <hyunjoon.tech@gmail.com>
2023-09-28 16:24:08 +0900

    feat: Cards 생성 시 점수와 Name 리스트로 만들 수 있도록 변경

--- src/main/java/model/Cards.java
+++ src/main/java/model/Cards.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cards {
 
@@ -10,7 +11,10 @@ public class Cards {
         this.cards = cards;
     }
 
-    public static Cards from(final List<Card> cards) {
+    public static Cards withScore(final int score, final List<Name> names) {
+        List<Card> cards = names.stream()
+                .map(name -> Card.createDefault(name, score))
+                .collect(Collectors.toList());
         return new Cards(cards);
     }
 }
