eddef73d46dce623afaa2cecfd430f16dc938eca
devholic22 <hyunjoon.tech@gmail.com>
2023-09-28 17:56:49 +0900

    feat: Application에서 GameView 생성 시 기본으로 플레이어 이름 묻도록 설정

--- src/main/java/CardApplication.java
+++ src/main/java/CardApplication.java
@@ -3,14 +3,22 @@ import model.Deck;
 import view.GameView;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CardApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Deck cardDeck = new Deck();
-        GameView view = new GameView(new BufferedReader(new InputStreamReader(System.in)));
+        GameView view = createGameView();
 
         GameController controller = new GameController(cardDeck, view);
+        controller.play();
+    }
+
+    private static GameView createGameView() {
+        GameView view = new GameView(new BufferedReader(new InputStreamReader(System.in)));
+        view.askPlayerNames();
+        return view;
     }
 }
