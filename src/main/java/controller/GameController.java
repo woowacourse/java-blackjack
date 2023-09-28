4f13aadb420bb2069bb2209dd174d688d574c706
devholic22 <hyunjoon.tech@gmail.com>
2023-09-28 17:58:03 +0900

    feat: 게임 컨트롤러 생성 시 플레이어 입력 연결

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,15 +1,31 @@
 package controller;
 
 import model.Deck;
+import model.Players;
 import view.GameView;
 
-public class GameController {
+import java.io.IOException;
 
+public class GameController {
+    
     private final Deck deck;
     private final GameView view;
+    private final Players players;
 
-    public GameController(final Deck deck, final GameView view) {
+    public GameController(final Deck deck, final GameView view) throws IOException {
         this.deck = deck;
         this.view = view;
+        this.players = joinPlayers(view.getPlayerNameInput());
+    }
+
+    public void play() {
+        
+    }
+
+    private Players joinPlayers(String nameInput) {
+        Players players = Players.createDefault();
+        players.joinPlayer(nameInput);
+
+        return players;
     }
 }
