8871a3f41b0e2b64d8bb033b8c1daa22ee818cbe
devholic22 <hyunjoon.tech@gmail.com>
2023-09-28 17:57:33 +0900

    feat: 플레이어 이름 묻고 입력받도록 메서드 추가

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 
 public class GameView {
 
@@ -8,5 +9,13 @@ public class GameView {
 
     public GameView(BufferedReader inputReader) {
         this.inputReader = inputReader;
+    }
+
+    public void askPlayerNames() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요. (쉼표 기준으로 분리)");
+    }
+
+    public String getPlayerNameInput() throws IOException {
+        return inputReader.readLine();
     }
 }
