6a9b3a84188f377887ba48caa78115f75f6f6a6f
libienz <leekh10195@gmail.com>
2024-03-05 17:20:01 +0900

    feat: Card 동등성 비교 및 해싱 함수 재정의

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -1,5 +1,7 @@
 package blackjack.domain;
 
+import java.util.Objects;
+
 public class Card {
 
     private final CardShape cardShape;
@@ -8,6 +10,23 @@ public class Card {
     public Card(CardShape cardShape, CardNumber cardNumber) {
         this.cardShape = cardShape;
         this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return cardShape == card.cardShape && cardNumber == card.cardNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardShape, cardNumber);
     }
 
     public int getCardNumber() {
