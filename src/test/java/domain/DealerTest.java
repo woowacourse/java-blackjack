0ca4608d71c9925d639b47e01d5f0c135db2f728
nayonsoso <panda0329@naver.com>
2024-03-05 19:42:09 +0900

    feat: 딜러가 히트를 할 수 있는지 반환하는 기능 구현

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -1,10 +1,11 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DealerTest {
 
@@ -16,6 +17,32 @@ public class DealerTest {
         Hand hand = new Hand(List.of(card1, card2));
         Dealer dealer = new Dealer(hand);
 
-        Assertions.assertThat(dealer).isInstanceOf(Player.class);
+        assertThat(dealer).isInstanceOf(Player.class);
+    }
+
+    @DisplayName("손패가 16이하이면 히트한다.")
+    @Test
+    void canHit() {
+        Card card1 = new Card(Letter.TEN, Mark.HEART);
+        Card card2 = new Card(Letter.SIX, Mark.HEART);
+        Hand hand = new Hand(List.of(card1, card2));
+        Dealer dealer = new Dealer(hand);
+
+        boolean canHit = dealer.canHit();
+
+        assertThat(canHit).isTrue();
+    }
+
+    @DisplayName("손패가 17이상이면 스테이해야 한다.")
+    @Test
+    void cantHit() {
+        Card card1 = new Card(Letter.TEN, Mark.HEART);
+        Card card2 = new Card(Letter.SEVEN, Mark.HEART);
+        Hand hand = new Hand(List.of(card1, card2));
+        Dealer dealer = new Dealer(hand);
+
+        boolean canHit = dealer.canHit();
+
+        assertThat(canHit).isFalse();
     }
 }
