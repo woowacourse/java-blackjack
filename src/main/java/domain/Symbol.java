87a1d5ffe50c087b4c37a3a279a450b53dadd112
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-05 17:24:20 +0900

    feat : 인덱스로 문양을 찾는 기능 구현

--- src/main/java/domain/Symbol.java
+++ src/main/java/domain/Symbol.java
@@ -1,8 +1,17 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum Symbol {
     HEART,
     CLOVER,
     SPACE,
-    DIAMOND
+    DIAMOND;
+
+    public static Symbol findBy(int index) {
+        return Arrays.stream(values())
+                .filter(symbol -> symbol.ordinal() == index)
+                .findFirst()
+                .orElseThrow();
+    }
 }
