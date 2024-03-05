469e9a9f77258ec5351d61f2275cf1bbfe941247
eun-byeol <joeunbyeol98@gmail.com>
2024-03-05 17:53:09 +0900

    feat: 카드 모양은 하트, 클로버, 다이아, 스페이드 중 하나의 값을 갖는다
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    Co-authored-by: lilychoibb <choiyoonseo02@gmail.com>

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -3,14 +3,10 @@ package model;
 public class Card {
 
     private final CardNumber number;
-    private final String shape;
+    private final CardShape shape;
 
-    public Card(CardNumber number, String shape) {
+    public Card(CardNumber number, CardShape shape) {
         this.number = number;
         this.shape = shape;
-    }
-
-    public CardNumber getNumber() {
-        return number;
     }
 }
