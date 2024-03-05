24ca440d77265d0ab58139682ef3837b379352f7
nayonsoso <panda0329@naver.com>
2024-03-05 17:04:17 +0900

    feat: 이름 길이 검증 로직 구현

---
+++ src/test/java/domain/NameTest.java
@@ -0,0 +1,29 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static domain.Name.NAME_LENGTH_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameTest {
+
+    @DisplayName("참가자의 이름은 1글자 이상 5글자 이하이다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "abcde"})
+    void createSuccess(String input) {
+        assertThatCode(() -> new Name(input))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("이름이 1글자 미만, 5글자 초과면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "abcdef"})
+    void createFail(String input) {
+        assertThatThrownBy(() -> new Name(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NAME_LENGTH_MESSAGE);
+    }
+}
--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -12,7 +12,7 @@ class PlayerTest {
     @DisplayName("이름과, 손 패를 가지고 있다.")
     @Test
     void createSuccess() {
-        String name = "wiib";
+        Name name = new Name("wiib");
         List<Card> cards = List.of(new Card(Letter.J, Mark.CLOVER), new Card(Letter.FIVE, Mark.SPADE));
         Hand hand = new Hand(cards);
 
