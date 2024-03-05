9665086f6f536c8d9b6b280244fc6baebaff08cc
kelly6bf <kelly6bf@gmail.com>
2024-03-05 17:55:12 +0900

    feat: Deck 을 초기화 하는 기능 구현

---
+++ src/main/java/domain/Deck.java
@@ -0,0 +1,27 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+public class Deck {
+    private final List<PlayingCard> playingCards;
+
+    private Deck(final List<PlayingCard> playingCards) {
+        this.playingCards = playingCards;
+    }
+
+    public static Deck init() {
+        return Arrays.stream(PlayingCardShape.values())
+                .flatMap(playingCardShape -> generateCardByShape(playingCardShape).stream())
+                .collect(collectingAndThen(toList(), Deck::new));
+    }
+
+    private static List<PlayingCard> generateCardByShape(PlayingCardShape playingCardShape) {
+        return Arrays.stream(PlayingCardValue.values())
+                .map(playingCardValue -> new PlayingCard(playingCardShape, playingCardValue))
+                .toList();
+    }
+}
---
+++ src/test/java/domain/DeckTest.java
@@ -0,0 +1,19 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DeckTest {
+
+    @DisplayName("트럼프 카드 덱을 초기화한다.")
+    @Test
+    void initDeckTest() {
+        // When
+        Deck deck = Deck.init();
+
+        // Then
+        assertThat(deck).isNotNull();
+    }
+}
