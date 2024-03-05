b827dbafc9d77e1ecdace6e1fc6c229ac29a6c2e
Choo <choo000407@naver.com>
2024-03-05 16:52:48 +0900

    feat: 참여자 이름은 한 글자 이상이다

---
+++ src/main/java/blackjack/model/Player.java
@@ -0,0 +1,18 @@
+package blackjack.model;
+
+public class Player {
+    private final String INVALID_NAME_LENGTH = "참여자 이름은 한 글자 이상이다";
+
+    private final String name;
+
+    public Player(final String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(final String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH);
+        }
+    }
+}
