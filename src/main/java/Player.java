e6837575f7f5803bd11335eadf75da1dadf1a79b
yechop <channetattoo@gmail.com>
2024-03-05 18:13:57 +0900

    feat: 플레이어 도메인 구현

---
+++ src/main/java/Player.java
@@ -0,0 +1,29 @@
+import java.util.Collections;
+import java.util.List;
+
+public class Player {
+
+    private final PlayerName playerName;
+    private final List<Card> cards;
+    private final Score score;
+
+    public Player(String name, List<Card> cards) {
+        this.playerName = new PlayerName(name);
+        this.cards = cards;
+        this.score = new Score(0);
+        cards.forEach(score::addScore);
+    }
+
+    public void hit(Card card) {
+        cards.add(card);
+        score.addScore(card);
+    }
+
+    public boolean hasScoreUnderThreshold() {
+        return score.isLowerThanThreshold();
+    }
+
+    public List<Card> getCards() {
+        return Collections.unmodifiableList(cards);
+    }
+}
