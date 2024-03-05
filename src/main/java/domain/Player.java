d2eb9a38fd3707abfa9963548d489cbb3a3d889f
nayonsoso <panda0329@naver.com>
2024-03-05 17:04:17 +0900

    feat: 이름 길이 검증 로직 구현

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -2,10 +2,10 @@ package domain;
 
 public class Player {
 
-    private final String name;
+    private final Name name;
     private final Hand hand;
 
-    public Player(String name, Hand hand) {
+    public Player(Name name, Hand hand) {
         this.name = name;
         this.hand = hand;
     }
