9a66483da785a823612094295157a4dfdfb8e696
BurningFalls <sherlocky31415@gmail.com>
2024-03-05 20:30:26 +0900

    feat: 카드 받을지 여부 입력 기능 구현

---
+++ src/main/java/view/YNInputView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class YNInputView {
+
+    public static Boolean getYNAsBoolean() {
+        String input = Console.getInputFromConsole();
+        if (input.equals("y")) {
+            return true;
+        }
+        if (input.equals("n")) {
+            return false;
+        }
+        throw new IllegalArgumentException("입력 형식이 올바르지 않습니다.");
+    }
+}
