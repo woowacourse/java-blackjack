64e38ed019786b87aa29fe532e0cf5b9d5ff83c9
eun-byeol <joeunbyeol98@gmail.com>
2024-03-05 17:39:22 +0900

    feat: 카드 숫자는 2~10와 A,J,K,Q 중 하나의 값을 갖는다
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    Co-authored-by: lilychoibb <choiyoonseo02@gmail.com>

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -2,11 +2,15 @@ package model;
 
 public class Card {
 
-    private final String number;
+    private final CardNumber number;
     private final String shape;
 
-    public Card(String number, String shape) {
+    public Card(CardNumber number, String shape) {
         this.number = number;
         this.shape = shape;
+    }
+
+    public CardNumber getNumber() {
+        return number;
     }
 }
