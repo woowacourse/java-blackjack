98dc0124c39ac7bc5687c800620d5aad2c405fa5
HyungUk Ryu <hw0603@naver.com>
2024-03-05 17:37:45 +0900

    test(playerCardsTest): 플레이어의 패에 카드한장을 추가하는 테스트 작성
    Co-authored-by: haiseong <wjdgotjd9908@gmail.com

---
+++ src/test/java/blackjack/domain/PlayerCardsTest.java
@@ -0,0 +1,23 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Shape;
+import blackjack.domain.card.Value;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayerCardsTest {
+    @Test
+    @DisplayName("한 장의 카드를 추가할 수 있다.")
+    void addCardTest() {
+        Card card = new Card(Shape.DIAMOND, Value.ACE);
+        PlayerCards playerCards = new PlayerCards();
+
+        playerCards.append(card);
+        List<Card> cards = playerCards.getCards();
+        assertThat(cards).hasSize(1);
+    }
+}
