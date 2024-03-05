30c483eaa7a6901567b82e39aed85ca2890cb90d
lemone <lejinhy@gmail.com>
2024-03-05 15:56:58 +0900

    feat: 플레이어가 없을 경우 에러 발생
    Co-Authored-By: Gyeongho Yang <chchch1213@naver.com>

--- src/main/java/blackjack/domain/Players.java
+++ src/main/java/blackjack/domain/Players.java
@@ -4,14 +4,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Players {
+    private static final String NAMES_EMPTY_ERROR = "공백이 아닌 플레이어를 입력해 주세요.";
     private final List<Player> values;
 
-    public Players(List<Player> values) {
+    private Players(List<Player> values) {
         this.values = values;
     }
 
     public static Players from(String names) {
+        validateEmpty(names);
         return new Players(parseNames(names).stream().map(Player::new).toList());
+    }
+
+    private static void validateEmpty(String names) {
+        if (names.isBlank()) {
+            throw new IllegalArgumentException(NAMES_EMPTY_ERROR);
+        }
     }
 
     private static List<String> parseNames(String names) {
--- src/test/java/blackjack/domain/PlayersTest.java
+++ src/test/java/blackjack/domain/PlayersTest.java
@@ -2,10 +2,13 @@ package blackjack.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class PlayersTest {
     @Test
@@ -22,5 +25,15 @@ public class PlayersTest {
         assertThat(players.getNames())
                 .usingRecursiveComparison()
                 .isEqualTo(expectedNames);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "    "})
+    @DisplayName("플레이어가 없을 경우 예외가 발생한다.")
+    void validateNoPlayer(String names) {
+        // given & when & then
+        assertThatCode(() -> Players.from(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("공백이 아닌 플레이어를 입력해 주세요.");
     }
 }
