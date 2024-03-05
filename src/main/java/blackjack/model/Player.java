87c85ee741f0e8ebf8da870273eaa6c540ecc977
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-05 15:30:52 +0900

    feat(Player): 이름을 가지는 플레이어 객체 생성

---
+++ src/main/java/blackjack/model/Player.java
@@ -0,0 +1,13 @@
+package blackjack.model;
+
+public class Player {
+    private final String name;
+
+    public Player(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/blackjack/model/PlayerTest.java
@@ -0,0 +1,17 @@
+package blackjack.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+    @Test
+    @DisplayName("참여할 사람은 이름을 가진다.")
+    void createPlayer() {
+        String name = "리브";
+        Player player = new Player(name);
+        assertThat(player.getName()).isEqualTo(name);
+    }
+}
