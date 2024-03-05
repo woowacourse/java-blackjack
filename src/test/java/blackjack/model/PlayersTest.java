8a9e9fdcf26219becd74ea8be65aecfc0f71f470
jongmee <ra123221@gmail.com>
2024-03-05 17:09:35 +0900

    test: 참여자 이름은 중복될 수 없다

--- src/test/java/blackjack/model/PlayersTest.java
+++ src/test/java/blackjack/model/PlayersTest.java
@@ -2,8 +2,11 @@ package blackjack.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -14,5 +17,21 @@ public class PlayersTest {
         // when & then
         assertThatThrownBy(() -> new Players(List.of()))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDuplicatedNames")
+    @DisplayName("참여자 이름은 중복되면 예외가 발생한다")
+    void validateDuplicatedNamesTest(List<String> names) {
+        // when & then
+        assertThatThrownBy(() -> new Players(names))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<List<String>> provideDuplicatedNames() {
+        return Stream.of(
+                List.of("mia", "mia", "dora"),
+                List.of("dora", "dora", "dora")
+        );
     }
 }
