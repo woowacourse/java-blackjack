9450f3bccb39d9c9debdd7db93e22b6f2413ea60
reddevilmidzy <midzy0228@gmail.com>
2024-03-05 18:07:14 +0900

    feat: 딜러가 카드 2장을 분해하다.

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -3,12 +3,15 @@ package domain;
 public class Dealer {
 
     private static final int INIT_CARD_NUMBER = 2; //TODO 이름 생각해보기
+
     private final Players players;
     private final CardDeck cardDeck;
+    private final Packet packet;
 
     public Dealer(final Players players, final CardDeck cardDeck) {
         this.players = players;
         this.cardDeck = cardDeck;
+        this.packet = Packet.createEmptyPacket();
     }
 
     public void startDeal() {
@@ -16,6 +19,7 @@ public class Dealer {
 
         for (int i = 0; i < INIT_CARD_NUMBER; i++) {
             players.forEach(player -> player.add(cardDeck.pop()));
+            packet.add(cardDeck.pop());
         }
     }
 }
