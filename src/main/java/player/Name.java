a10af0caa4f028768bb846be10da555442dec3d2
hyxrxn <gpfls981220@gmail.com>
2024-03-05 20:26:10 +0900

    feat(Name): 이름 생성 및 검증
    Co-authored-by: donghoony <aru0504@naver.com>

---
+++ src/main/java/player/Name.java
@@ -0,0 +1,28 @@
+package player;
+
+public class Name {
+
+    private final String name;
+
+    public Name(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        validateNonBlankName(name);
+        validateNameLength(name);
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() < 2 || name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 이름은 2글자 이상 5글자 이하여야 합니다.");
+        }
+    }
+
+    private void validateNonBlankName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 이름은 공백일 수 없습니다.");
+        }
+    }
+}
