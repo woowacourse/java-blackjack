7e9ae3462ae14993b97c61745b28ed4b99edd35d
Choo <choo000407@naver.com>
2024-03-05 18:02:10 +0900

    test: 카드는 끗수와 슈트의 조합이다

---
+++ src/test/java/blackjack/model/CardTest.java
@@ -0,0 +1,38 @@
+package blackjack.model;
+
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class CardTest {
+    @ParameterizedTest
+    @MethodSource("provideDenominationAndSuit")
+    @DisplayName("카드는 끗수와 슈트의 조합이다")
+    void generateCardTest(Denomination denomination, Suit suit) {
+        Card card = new Card() {
+            @Override
+            protected Denomination generateDenomination() {
+                return denomination;
+            }
+
+            @Override
+            protected Suit generateSuit() {
+                return suit;
+            }
+        };
+
+        assertThat(card.getDenomination).isEqualTo(denomination);
+        asserThat(card.getSuit).isEqualTo(suit);
+    }
+
+    private static Stream<Arguments> provideDenominationAndSuit() {
+        return Stream.of(
+                Arguments.arguments(Denomination.TWO, Suit.SPADE),
+                Arguments.arguments(Denomination.THREE, Suit.SPADE),
+                Arguments.arguments(Denomination.QUEEN, Suit.HEART)
+        );
+    }
+}
