85a9f442af4ea77fb086676d27b58c3e8dfff295
HyungUk Ryu <hw0603@naver.com>
2024-03-05 20:32:36 +0900

    feat(player): 덱으로 부터 카드 한장을 받아오는 기능 구현
    Co-authored-by: haiseong <wjdgotjd9908@gmail.com>

---
+++ src/main/java/blackjack/domain/Player.java
@@ -0,0 +1,21 @@
+package blackjack.domain;
+
+import blackjack.domain.card.Card;
+import java.util.List;
+
+public class Player {
+    PlayerCards playerCards;
+
+    public Player() {
+        this.playerCards = PlayerCards.createEmptyCards();
+    }
+
+    public void draw(Deck deck) {
+        Card card = deck.draw();
+        playerCards.append(card);
+    }
+
+    public List<Card> getCards() {
+        return playerCards.getCards();
+    }
+}
