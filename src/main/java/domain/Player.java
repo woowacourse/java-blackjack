d719d08327de1d4e811d2bee1d6388a983791bb0
jinwoo22 <jenny010925@naver.com>
2024-03-05 15:18:18 +0900

    feat: null 이나 공백인 경우 예외가 발생 기능 구현

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -4,6 +4,24 @@ public class Player {
     private final String name;
 
     public Player(final String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(final String name) {
+        validateNull(name);
+        validateBlank(name);
+    }
+
+    private void validateNull(final String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 이름입니다.");
+        }
+    }
+
+    private void validateBlank(final String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 이름에 공백을 입력할 수 없습니다.");
+        }
     }
 }
--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -3,6 +3,9 @@ package domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PlayerTest {
     @DisplayName("이름으로 참여자를 생성한다.")
@@ -10,5 +13,14 @@ public class PlayerTest {
     void createPlayerWithName() {
         Assertions.assertThatCode(() -> new Player("pobi"))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("null이나 공백을 입력하면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  "})
+    @NullSource
+    void nullOrBlankInputThrowException(String name) {
+        Assertions.assertThatThrownBy(() -> new Player(name))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
