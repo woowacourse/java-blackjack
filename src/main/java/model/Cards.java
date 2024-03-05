e90ee88faa5fbbd7750f2851451d5ed3024c7516
eunhee <dmsgml9903@gmail.com>
2024-03-05 15:01:41 +0900

    feat(Cards): 램덤하게 카드를 선택하고 선택된 카드는 삭제된다.

---
+++ src/main/java/model/Cards.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Cards {//TODO 싱글톤
+
+    private final List<Card> cards;
+
+    public Cards() {
+        cards = new ArrayList<>();
+        for (CardShape cardShape : CardShape.values()) {
+            createSameShapeCards(cardShape);
+        }
+    }
+
+    private void createSameShapeCards(CardShape cardShape) {
+        for (CardNumber cardNumber : CardNumber.values()) {
+            cards.add(new Card(cardShape, cardNumber));
+        }
+    }
+
+    public Card selectRandomCard() {
+        Collections.shuffle(cards);
+        return cards.remove(0);
+    }
+
+    public boolean contains(Card card) {
+        return cards.contains(card);
+    }
+}
---
+++ src/test/java/model/CardsTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CardsTest {
+
+    @DisplayName("램덤하게 카드를 선택한다.")
+    @Test
+    void selectRandomCard() {
+        Cards cards = new Cards();
+        Assertions.assertThat(cards.selectRandomCard()).isInstanceOf(Card.class);
+    }
+
+    @DisplayName("선택된 카드는 삭제된다.")
+    @Test
+    void deleteSelectedCard() {
+        Cards cards = new Cards();
+        Card card = cards.selectRandomCard();
+        Assertions.assertThat(cards.contains(card)).isFalse();
+    }
+
+
+}
