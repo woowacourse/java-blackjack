2cd9c047a2b2db392622775b5d242f1f945a96b3
eunhee <dmsgml9903@gmail.com>
2024-03-05 18:11:56 +0900

    feat(Players) : Player 일급 컬렉션 구현

---
+++ src/main/java/model/player/Players.java
@@ -0,0 +1,12 @@
+package model.player;
+
+import java.util.List;
+
+public class Players {
+
+    private final List<Player> players;
+
+    public Players(List<Player> players) {
+        this.players = players;
+    }
+}
