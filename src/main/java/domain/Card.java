5e3220a12bbe7e8a02022fac4a694d649065c5f8
nayonsoso <panda0329@naver.com>
2024-03-05 14:44:44 +0900

    feat: 카드 값을 반환하는 기능 구현

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.List;
+
 public class Card {
 
     private final String letter;
@@ -8,5 +10,15 @@ public class Card {
     public Card(String letter, String mark) {
         this.letter = letter;
         this.mark = mark;
+    }
+
+    public int getValue() {
+        if ("A".equals(letter)) {
+            return 1;
+        }
+        if (List.of("K", "Q", "J").contains(letter)) {
+            return 10;
+        }
+        return Integer.parseInt(letter);
     }
 }
--- src/test/java/domain/CardTest.java
+++ src/test/java/domain/CardTest.java
@@ -2,6 +2,9 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -15,5 +18,16 @@ class CardTest {
 
         assertThatCode( () -> new Card(letter,mark))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("숫자로 계산한 값을 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"2:2", "10:10", "A:1", "J:10", "K:10", "Q:10"}, delimiter = ':')
+    void getValue(String letter, int value) {
+        String mark = "다이아몬드";
+
+        Card card = new Card(letter, mark);
+
+        assertThat(card.getValue()).isEqualTo(value);
     }
 }
