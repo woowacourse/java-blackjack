f7e1231310f12289da1603e011e183f1e1912912
lemone <lejinhy@gmail.com>
2024-03-05 17:11:04 +0900

    feat: Card의 score을 List<Integer>로 사용
    Co-Authored-By: Gyeongho Yang <chchch1213@naver.com>

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -1,73 +1,75 @@
 package blackjack.domain;
 
+import java.util.List;
+
 import static blackjack.domain.CardSymbol.*;
 
 public enum Card {
-    SPADE_TWO(SPADE, 2),
-    SPADE_THREE(SPADE, 3),
-    SPADE_FOUR(SPADE, 4),
-    SPADE_FIVE(SPADE, 5),
-    SPADE_SIX(SPADE, 6),
-    SPADE_SEVEN(SPADE, 7),
-    SPADE_EIGHT(SPADE, 8),
-    SPADE_NINE(SPADE, 9),
-    SPADE_JACK(SPADE, 10),
-    SPADE_QUEEN(SPADE, 10),
-    SPADE_KING(SPADE, 10),
-    SPADE_ACE(SPADE, 1),
+
+    SPADE_TWO(SPADE, List.of(2)),
+    SPADE_THREE(SPADE, List.of(3)),
+    SPADE_FOUR(SPADE, List.of(4)),
+    SPADE_FIVE(SPADE, List.of(5)),
+    SPADE_SIX(SPADE, List.of(6)),
+    SPADE_SEVEN(SPADE, List.of(7)),
+    SPADE_EIGHT(SPADE, List.of(8)),
+    SPADE_NINE(SPADE, List.of(9)),
+    SPADE_JACK(SPADE, List.of(10)),
+    SPADE_QUEEN(SPADE, List.of(10)),
+    SPADE_KING(SPADE, List.of(10)),
+    SPADE_ACE(SPADE, List.of(1, 11)),
 
 
-    DIAMOND_TWO(DIAMOND, 2),
-    DIAMOND_THREE(DIAMOND, 3),
-    DIAMOND_FOUR(DIAMOND, 4),
-    DIAMOND_FIVE(DIAMOND, 5),
-    DIAMOND_SIX(DIAMOND, 6),
-    DIAMOND_SEVEN(DIAMOND, 7),
-    DIAMOND_EIGHT(DIAMOND, 8),
-    DIAMOND_NINE(DIAMOND, 9),
-    DIAMOND_JACK(DIAMOND, 10),
-    DIAMOND_QUEEN(DIAMOND, 10),
-    DIAMOND_KING(DIAMOND, 10),
-    DIAMOND_ACE(DIAMOND, 1),
+    DIAMOND_TWO(DIAMOND, List.of(2)),
+    DIAMOND_THREE(DIAMOND, List.of(3)),
+    DIAMOND_FOUR(DIAMOND, List.of(4)),
+    DIAMOND_FIVE(DIAMOND, List.of(5)),
+    DIAMOND_SIX(DIAMOND, List.of(6)),
+    DIAMOND_SEVEN(DIAMOND, List.of(7)),
+    DIAMOND_EIGHT(DIAMOND, List.of(8)),
+    DIAMOND_NINE(DIAMOND, List.of(9)),
+    DIAMOND_JACK(DIAMOND, List.of(10)),
+    DIAMOND_QUEEN(DIAMOND, List.of(10)),
+    DIAMOND_KING(DIAMOND, List.of(10)),
+    DIAMOND_ACE(DIAMOND, List.of(1, 11)),
 
 
-    HEART_TWO(HEART, 2),
-    HEART_THREE(HEART, 3),
-    HEART_FOUR(HEART, 4),
-    HEART_FIVE(HEART, 5),
-    HEART_SIX(HEART, 6),
-    HEART_SEVEN(HEART, 7),
-    HEART_EIGHT(HEART, 8),
-    HEART_NINE(HEART, 9),
-    HEART_JACK(HEART, 10),
-    HEART_QUEEN(HEART, 10),
-    HEART_KING(HEART, 10),
-    HEART_ACE(HEART, 1),
+    HEART_TWO(HEART, List.of(2)),
+    HEART_THREE(HEART, List.of(3)),
+    HEART_FOUR(HEART, List.of(4)),
+    HEART_FIVE(HEART, List.of(5)),
+    HEART_SIX(HEART, List.of(6)),
+    HEART_SEVEN(HEART, List.of(7)),
+    HEART_EIGHT(HEART, List.of(8)),
+    HEART_NINE(HEART, List.of(9)),
+    HEART_JACK(HEART, List.of(10)),
+    HEART_QUEEN(HEART, List.of(10)),
+    HEART_KING(HEART, List.of(10)),
+    HEART_ACE(HEART, List.of(1, 11)),
 
 
-    CLUB_TWO(CLUB, 2),
-    CLUB_THREE(CLUB, 3),
-    CLUB_FOUR(CLUB, 4),
-    CLUB_FIVE(CLUB, 5),
-    CLUB_SIX(CLUB, 6),
-    CLUB_SEVEN(CLUB, 7),
-    CLUB_EIGHT(CLUB, 8),
-    CLUB_NINE(CLUB, 9),
-    CLUB_JACK(CLUB, 10),
-    CLUB_QUEEN(CLUB, 10),
-    CLUB_KING(CLUB, 10),
-    CLUB_ACE(CLUB, 1);
-
+    CLUB_TWO(CLUB, List.of(2)),
+    CLUB_THREE(CLUB, List.of(3)),
+    CLUB_FOUR(CLUB, List.of(4)),
+    CLUB_FIVE(CLUB, List.of(5)),
+    CLUB_SIX(CLUB, List.of(6)),
+    CLUB_SEVEN(CLUB, List.of(7)),
+    CLUB_EIGHT(CLUB, List.of(8)),
+    CLUB_NINE(CLUB, List.of(9)),
+    CLUB_JACK(CLUB, List.of(10)),
+    CLUB_QUEEN(CLUB, List.of(10)),
+    CLUB_KING(CLUB, List.of(10)),
+    CLUB_ACE(CLUB, List.of(1, 11));
 
     private final CardSymbol symbol;
-    private final int score;
+    private final List<Integer> scores;
 
-    Card(CardSymbol symbol, int score) {
+    Card(CardSymbol symbol, List<Integer> scores) {
         this.symbol = symbol;
-        this.score = score;
+        this.scores = scores;
     }
 
-    public int getScore() {
-        return score;
+    public List<Integer> getScores() {
+        return scores;
     }
 }
--- src/main/java/blackjack/domain/Cards.java
+++ src/main/java/blackjack/domain/Cards.java
@@ -1,9 +1,12 @@
 package blackjack.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Cards {
+    private static final int MAX_SCORE = 21;
     private final List<Card> values;
 
     public Cards(List<Card> values) {
@@ -15,8 +18,20 @@ public class Cards {
     }
 
     public int totalScore() {
-        return values.stream()
-                .mapToInt(Card::getScore)
-                .reduce(0, Integer::sum);
+        Set<Integer> scoreCases = new HashSet<>();
+        calculateScoreCases(scoreCases, values.stream().map(Card::getScores).toList(), 0, 0);
+
+
+        return 0;
+    }
+
+    private void calculateScoreCases(Set<Integer> scoreCases, List<List<Integer>> scores, int sum, int index) {
+        if (index == scores.size()) {
+            scoreCases.add(sum);
+            return;
+        }
+        for (int score : scores.get(index)) {
+            calculateScoreCases(scoreCases, scores, sum + score, index + 1);
+        }
     }
 }
