e8fe05033e35c4fdfa8a20230b602e066d084f18
nayonsoso <panda0329@naver.com>
2024-03-05 16:43:33 +0900

    feat: 참가자 생성 기능 구현

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Player {
+
+    private final String name;
+    private final Hand hand;
+
+    public Player(String name, Hand hand) {
+        this.name = name;
+        this.hand = hand;
+    }
+}
