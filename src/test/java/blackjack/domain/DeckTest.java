0605ed31c1a202ac21a433adbe5438be89dcf56b
HyungUk Ryu <hw0603@naver.com>
2024-03-05 17:14:59 +0900

    test(deckTest): 덱에서 카드 한 장을 뽑는 기능의 테스트 코드 추가
    Co-authored-by: haiseong <wjdgotjd9908@gmail.com>

---
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -0,0 +1,27 @@
+package blackjack.domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.domain.card.Shape;
+import blackjack.domain.card.Value;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DeckTest {
+    @Test
+    @DisplayName("자신이 가지고 있는 카드 중 한 장을 플레이어에게 제공할 수 있다.")
+    void draw() {
+        List<Card> cards = new ArrayList<>(List.of(
+                new Card(Shape.SPADE, Value.ACE),
+                new Card(Shape.CLOVER, Value.FOUR),
+                new Card(Shape.HEART, Value.KING)
+        ));
+        Deck deck = Deck.from(cards);
+
+        Card expected = new Card(Shape.SPADE, Value.ACE);
+        assertThat(deck.draw()).isEqualTo(expected);
+    }
+}
