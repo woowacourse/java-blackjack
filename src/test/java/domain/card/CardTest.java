2e08723a2f3aff0bca197baef83f63d9317ff822
takoyakimchi <c4nyou@gmail.com>
2024-03-05 18:33:35 +0900

    refactor: Card 테스트를 MethodSource 사용하는 방식으로 변경
    Co-authored-by: tsulocalize <tsulocalizing@gmail.com>

--- src/test/java/domain/card/CardTest.java
+++ src/test/java/domain/card/CardTest.java
@@ -15,30 +15,28 @@ import static domain.card.Rank.THREE;
 import static domain.card.Rank.TWO;
 import static domain.card.Symbol.DIAMOND;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CardTest {
 
-    @Test
-    @DisplayName("성공: 점수 반환")
-    void score_NoException() {
-        Assertions.assertAll(
-            () -> assertThat(new Card(KING, DIAMOND).score()).isEqualTo(10),
-            () -> assertThat(new Card(QUEEN, DIAMOND).score()).isEqualTo(10),
-            () -> assertThat(new Card(JACK, DIAMOND).score()).isEqualTo(10),
-            () -> assertThat(new Card(TEN, DIAMOND).score()).isEqualTo(10),
-            () -> assertThat(new Card(NINE, DIAMOND).score()).isEqualTo(9),
-            () -> assertThat(new Card(EIGHT, DIAMOND).score()).isEqualTo(8),
-            () -> assertThat(new Card(SEVEN, DIAMOND).score()).isEqualTo(7),
-            () -> assertThat(new Card(SIX, DIAMOND).score()).isEqualTo(6),
-            () -> assertThat(new Card(FIVE, DIAMOND).score()).isEqualTo(5),
-            () -> assertThat(new Card(FOUR, DIAMOND).score()).isEqualTo(4),
-            () -> assertThat(new Card(THREE, DIAMOND).score()).isEqualTo(3),
-            () -> assertThat(new Card(TWO, DIAMOND).score()).isEqualTo(2),
-            () -> assertThat(new Card(ACE, DIAMOND).score()).isEqualTo(1)
+    @ParameterizedTest
+    @MethodSource("rankScoreStream")
+    void score_NoException_WithMethodSource(Rank rank, int score) {
+        Card card = new Card(rank, DIAMOND);
+        assertThat(card.score()).isEqualTo(score);
+    }
+
+    static Stream<Arguments> rankScoreStream() {
+        return Stream.of(
+            arguments(KING, 10), arguments(QUEEN, 10), arguments(JACK, 10), arguments(TEN, 10),
+            arguments(NINE, 9), arguments(EIGHT, 8), arguments(SEVEN, 7),
+            arguments(SIX, 6), arguments(FIVE, 5), arguments(FOUR, 4),
+            arguments(THREE, 3), arguments(TWO, 2), arguments(ACE, 1)
         );
     }
 }
