144ab8d8052403a6dcd650b2334e26f442aa6db3
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-03-05 14:36:07 +0900

    feat: Name 클래스 생성

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Name {
+
+    private final String name;
+
+    public Name(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 한 글자 이상이어야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/NameTest.java
@@ -0,0 +1,28 @@
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import domain.Name;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+public class NameTest {
+
+    @Test
+    @DisplayName("이름이 잘 생성된다.")
+    void nameConstructSuccessTest() {
+        Assertions.assertThatNoException()
+                .isThrownBy(() -> new Name("이름"));
+    }
+
+    @ParameterizedTest
+    @DisplayName("이름이 없을 때 예외가 발생한다.")
+    @NullAndEmptySource
+    void noNameExceptionTest(String name) {
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
