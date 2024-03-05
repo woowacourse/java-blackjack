b415b4e50a8fca7c4dedd60cad3a627cde5bdf66
JUHA <84626225+khabh@users.noreply.github.com>
2024-03-05 17:56:02 +0900

    feat: Order로 Number, Shape 인덱싱

--- src/main/java/blackjack/domain/card/Number.java
+++ src/main/java/blackjack/domain/card/Number.java
@@ -1,5 +1,8 @@
 package blackjack.domain.card;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Number {
     ACE(1, "A"),
     TWO(2, "2"),
@@ -15,8 +18,16 @@ public enum Number {
     QUEEN(10, "Q"),
     KING(10, "K");
 
+    private static final Map<Integer, Number> numberByOrder = new HashMap<>();
+
     private final int score;
     private final String symbol;
+
+    static {
+        for (Number number : Number.values()) {
+            numberByOrder.put(numberByOrder.size(), number);
+        }
+    }
 
     Number(int score, String symbol) {
         this.score = score;
@@ -29,5 +40,12 @@ public enum Number {
 
     public String getSymbol() {
         return symbol;
+    }
+
+    public static Number getByOrder(int order) {
+        if (numberByOrder.containsKey(order)) {
+            throw new NullPointerException("존재하지 않는 카드 숫자입니다.");
+        }
+        return numberByOrder.get(order);
     }
 }
--- src/main/java/blackjack/domain/card/Shape.java
+++ src/main/java/blackjack/domain/card/Shape.java
@@ -1,12 +1,23 @@
 package blackjack.domain.card;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Shape {
     SPADE("스페이드"),
     DIAMOND("다이아몬드"),
     HEART("하트"),
     CLOVER("클로버");
 
+    private static final Map<Integer, Shape> shapeByOrder = new HashMap<>();
+
     private final String value;
+
+    static {
+        for (Shape cardShape : Shape.values()) {
+            shapeByOrder.put(shapeByOrder.size(), cardShape);
+        }
+    }
 
     Shape(String value) {
         this.value = value;
@@ -14,5 +25,12 @@ public enum Shape {
 
     public String getValue() {
         return value;
+    }
+
+    public static Shape getByOrder(int order) {
+        if (!shapeByOrder.containsKey(order)) {
+            throw new NullPointerException("존재하지 않는 카드 모양입니다.");
+        }
+        return shapeByOrder.get(order);
     }
 }
