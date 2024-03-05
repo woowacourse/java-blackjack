e743554d4080ca06293d2774da41b9e85d415b25
le2sky <leehaneul990623@gmail.com>
2024-03-05 14:33:52 +0900

    feat: 카드 생성 기능 구현

---
+++ src/main/java/blackjack/domain/Card.java
@@ -0,0 +1,12 @@
+package blackjack.domain;
+
+class Card {
+
+    private final CardRank cardRank;
+    private final CardShape cardShape;
+
+    public Card(CardRank cardRank, CardShape cardShape) {
+        this.cardRank = cardRank;
+        this.cardShape = cardShape;
+    }
+}
---
+++ src/main/java/blackjack/domain/CardRank.java
@@ -0,0 +1,26 @@
+package blackjack.domain;
+
+enum CardRank {
+
+    ACE("A", 1),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10);
+
+    private final String rank;
+    private final int score;
+
+    CardRank(String rank, int score) {
+        this.rank = rank;
+        this.score = score;
+    }
+}
