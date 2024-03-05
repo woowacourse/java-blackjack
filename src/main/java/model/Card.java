6e99bfc161460cda474cb269068ffe29899252f4
eun-byeol <joeunbyeol98@gmail.com>
2024-03-05 16:41:30 +0900

    feat: 숫자와 모양을 가진 카드를 생성한다
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    Co-authored-by: lilychoibb <choiyoonseo02@gmail.com>

---
+++ src/main/java/model/Card.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Card {
+
+    private final String number;
+    private final String shape;
+
+    public Card(String number, String shape) {
+        this.number = number;
+        this.shape = shape;
+    }
+}
---
+++ src/test/java/model/CardTest.java
@@ -0,0 +1,15 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CardTest {
+
+    @DisplayName("숫자와 모양을 가진 카드를 생성한다")
+    @Test
+    void shouldCardHaveNumberAndShape() {
+        assertThatCode(() -> new Card("A", "하트")).doesNotThrowAnyException();
+    }
+}
