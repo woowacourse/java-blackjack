af5cb26c65975f82562ddf54d0fd9df683476cde
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-05 15:56:33 +0900

    feat(Player): 이름 입력 시 null 또는 빈 값이 들어오면 예외를 던지는 기능 구현
    Co-authored-by: Minjoo522 <m0522inju@gmail.com>

--- src/main/java/blackjack/model/Player.java
+++ src/main/java/blackjack/model/Player.java
@@ -4,7 +4,14 @@ public class Player {
     private final String name;
 
     public Player(final String name) {
+        validateNullAndEmptyName(name);
         this.name = name;
+    }
+
+    private void validateNullAndEmptyName(final String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름에는 공백을 사용할 수 없습니다.");
+        }
     }
 
     public String getName() {
--- src/test/java/blackjack/model/PlayerTest.java
+++ src/test/java/blackjack/model/PlayerTest.java
@@ -1,9 +1,12 @@
 package blackjack.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 class PlayerTest {
 
@@ -13,5 +16,13 @@ class PlayerTest {
         String name = "리브";
         Player player = new Player(name);
         assertThat(player.getName()).isEqualTo(name);
+    }
+
+    @ParameterizedTest(name = "이름 입력 시 blank가 들어온 경우 예외를 발생한다.")
+    @NullAndEmptySource
+    void createPlayerByBlank(String name) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Player(name))
+                .withMessage("이름에는 공백을 사용할 수 없습니다.");
     }
 }
