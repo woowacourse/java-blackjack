6c15afc3faf9e2439c9061270df57a3c4f7a37c2
eunhee <dmsgml9903@gmail.com>
2024-03-05 17:21:44 +0900

    feat(Participants) : 참가자 리스트를 가지고 있는 일급 컬렉션 구현

---
+++ src/main/java/model/Participants.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.List;
+
+public class Participants {
+    private final List<Participant> participants;
+
+    Participants(List<Participant> participants) {
+        this.participants = participants;
+    }
+
+}
