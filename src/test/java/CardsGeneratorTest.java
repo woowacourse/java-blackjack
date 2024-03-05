8a64811607e4df3e4c914564225182e3b45a58ba
yechop <channetattoo@gmail.com>
2024-03-05 17:15:57 +0900

    test: 랜덤순서 카드팩 생성기 테스트

---
+++ src/test/java/CardsGeneratorTest.java
@@ -0,0 +1,16 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class CardsGeneratorTest {
+
+    @DisplayName("서로 다른 52장의 카드팩을 생성한다.")
+    @Test
+    void generateCardsTest() {
+        CardsGenerator cardsGenerator = new CardsGenerator();
+        List<Card> cards = cardsGenerator.generateRandomCards();
+        Assertions.assertThat(cards).hasSize(52);
+    }
+}
