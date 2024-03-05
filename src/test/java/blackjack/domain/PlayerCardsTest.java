57acebe2c62bb678c141bc28cf387e84bfb10990
HyungUk Ryu <hw0603@naver.com>
2024-03-05 19:55:27 +0900

    test(playerCardsTest): 플레이어의 패에 ACE가 포함된 경우 테스트 코드 추가
    Co-authored-by: haiseong <wjdgotjd9908@gmail.com>

--- src/test/java/blackjack/domain/PlayerCardsTest.java
+++ src/test/java/blackjack/domain/PlayerCardsTest.java
@@ -38,6 +38,7 @@ class PlayerCardsTest {
         assertThat(score).isEqualTo(9);
     }
 
+    // TODO: ParameterizedTest 활용
     @Test
     @DisplayName("J, Q, K 카드는 모두 10으로 계산된다.")
     void calculateScoreWithAlphabetTest() {
@@ -48,9 +49,67 @@ class PlayerCardsTest {
         );
         PlayerCards playerCards = new PlayerCards(cards);
 
-        // TODO: score를 래핑해야 할 지도.
         int score = playerCards.calculateScore();
 
         assertThat(score).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("ACE가 11점으로 계산되는 경우")
+    void calculateScoreWithAceAsEleven() {
+        List<Card> cards = List.of(
+                new Card(Shape.DIAMOND, Value.JACK),
+                new Card(Shape.DIAMOND, Value.ACE)
+        );
+        PlayerCards playerCards = new PlayerCards(cards);
+
+        int score = playerCards.calculateScore();
+
+        assertThat(score).isEqualTo(21);
+    }
+
+    @Test
+    @DisplayName("ACE가 1점으로 계산되는 경우")
+    void calculateScoreWithAceAsOne() {
+        List<Card> cards = List.of(
+                new Card(Shape.DIAMOND, Value.NINE),
+                new Card(Shape.DIAMOND, Value.ACE),
+                new Card(Shape.SPADE, Value.ACE)
+        );
+        PlayerCards playerCards = new PlayerCards(cards);
+
+        int score = playerCards.calculateScore();
+
+        assertThat(score).isEqualTo(21);
+    }
+
+    @Test
+    @DisplayName("ACE가 1점으로 계산되는 경우")
+    void calculateScoreWithAceAsOne2() {
+        List<Card> cards = List.of(
+                new Card(Shape.DIAMOND, Value.KING),
+                new Card(Shape.DIAMOND, Value.QUEEN),
+                new Card(Shape.DIAMOND, Value.ACE)
+        );
+        PlayerCards playerCards = new PlayerCards(cards);
+
+        int score = playerCards.calculateScore();
+
+        assertThat(score).isEqualTo(21);
+    }
+
+    @Test
+    @DisplayName("ACE가 1점으로 계산되는 경우")
+    void calculateScoreWithAceAsOne3() {
+        List<Card> cards = List.of(
+                new Card(Shape.DIAMOND, Value.ACE),
+                new Card(Shape.CLOVER, Value.ACE),
+                new Card(Shape.SPADE, Value.ACE)
+        );
+        PlayerCards playerCards = new PlayerCards(cards);
+
+        int score = playerCards.calculateScore();
+
+        assertThat(score).isEqualTo(13);
     }
 }
