c47d73cd8823f047ce9ca1e91281d7404a0c3c63
yechop <channetattoo@gmail.com>
2024-03-05 15:24:10 +0900

    feat: 플레이어 이름 도메인 구현

---
+++ src/main/java/PlayerName.java
@@ -0,0 +1,18 @@
+public class PlayerName {
+
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 20;
+
+    private final String value;
+
+    public PlayerName(String value) {
+        validateLength(value);
+        this.value = value;
+    }
+
+    private void validateLength(String value) {
+        if (value.length() < MIN_LENGTH || value.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 이름의 길이는 1 ~ 20 글자 사이로 입력해주세요.");
+        }
+    }
+}
