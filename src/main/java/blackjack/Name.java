cf2ae67bda138854a91ef5f1cb19cc0fa7559f72
HoeSeong123 <ghltjd3@gmail.com>
2024-03-05 15:12:01 +0900

    feat(Name) : 이름에 공백이 포함되었는지 검증하는 기능 구현

--- src/main/java/blackjack/Name.java
+++ src/main/java/blackjack/Name.java
@@ -4,10 +4,17 @@ class Name {
     private String value;
 
     public Name(String value) {
+        validateContainBlank(value);
         this.value = value;
     }
 
     public String asString() {
         return value;
+    }
+
+    private void validateContainBlank(String value) {
+        if (value.contains(" ")) {
+            throw new IllegalArgumentException(String.format("%s 는 공백을 포함 하고 있습니다", value));
+        }
     }
 }
--- src/test/java/blackjack/NameTest.java
+++ src/test/java/blackjack/NameTest.java
@@ -10,11 +10,16 @@ public class NameTest {
 
     @Test
     @DisplayName("문자열을 통해서 이름을 생성한다.")
-    void Name_createInstanceWithString() {
+    void Name_Instance_create_with_String() {
         assertThatCode(() -> {
             Name name = new Name("초롱");
             Assertions.assertEquals(name.asString(), "초롱");
-        })
-                .doesNotThrowAnyException();
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("문자열에 공백이 포함되는 이름을 생성될 수 없다.")
+    void Name_Instance_not_include_blank() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Name("초 롱"));
     }
 }
