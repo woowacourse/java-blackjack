c1f009e47b68d7e2531969fe5b7b5e3b3321afab
eunhee <dmsgml9903@gmail.com>
2024-03-05 14:38:56 +0900

    feat(Card): 카드는 카드 모양과 숫자를 가지며 같은 카드의 정의는 카드 모양과 숫자가 같은 경우이다.

---
+++ src/main/java/model/Card.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.util.Objects;
+
+public class Card {
+    private final CardShape cardShape;
+    private final CardNumber cardNumber;
+
+    public Card(CardShape cardShape, CardNumber cardNumber) {
+        this.cardShape = cardShape;
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return cardShape == card.cardShape && cardNumber == card.cardNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardShape, cardNumber);
+    }
+}
