4b19daeb5a80e1136fe68266b27278eba7e411d9
takoyakimchi <c4nyou@gmail.com>
2024-03-05 18:31:52 +0900

    feat: Name 테스트 및 클래스 생성
    Co-authored-by: tsulocalize <tsulocalizing@gmail.com>

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,40 @@
+package domain;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Name {
+
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final Pattern HANGUL_ALPHABET_NUMBER = Pattern.compile("^[가-힣a-zA-Z0-9]*$");
+
+    public Name(String rawName) {
+        validate(rawName);
+    }
+
+    private void validate(String rawName) {
+        validateNull(rawName);
+        validateLength(rawName);
+        validateCharacter(rawName);
+    }
+
+    private void validateNull(String rawName) {
+        if (rawName == null) {
+            throw new IllegalArgumentException("[ERROR] null을 입력할 수 없습니다.");
+        }
+    }
+
+    private void validateLength(String rawName) {
+        if (rawName.isEmpty() || rawName.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 1~5자의 이름만 입력 가능합니다.");
+        }
+    }
+
+    private void validateCharacter(String rawName) {
+        Matcher matcher = HANGUL_ALPHABET_NUMBER.matcher(rawName);
+        if (matcher.matches()) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 한글, 영어, 숫자만 입력 가능합니다.");
+    }
+}
---
+++ src/test/java/domain/NameTest.java
@@ -0,0 +1,48 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class NameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "가나다a1", "abcde"})
+    @DisplayName("성공: 1~5자의 이름 입력")
+    void construct_NoException_OneToFiveLetters(String rawName) {
+        assertThatCode(() -> new Name(rawName))
+            .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "123456"})
+    @DisplayName("실패: 글자수 위반 - 경계값(0, 6)")
+    void construct_Exception_IllegalNameLength(String rawName) {
+        assertThatThrownBy(() -> new Name(rawName))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 1~5자의 이름만 입력 가능합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a!", "!@#$%", "  "})
+    @DisplayName("실패: 이름 조건 위반 - 비허용 문자 입력")
+    void construct_Exception_IllegalLetters(String rawName) {
+        assertThatThrownBy(() -> new Name(rawName))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 한글, 영어, 숫자만 입력 가능합니다.");
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @DisplayName("실패: 이름 조건 위반 - 비허용 문자 입력")
+    void construct_Exception_null(String rawName) {
+        assertThatThrownBy(() -> new Name(rawName))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] null을 입력할 수 없습니다.");
+    }
+
+}
