1fbc78cb5da99a9ff7ab65057e573d46da620910
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-05 16:17:09 +0900

    feat(CardNumber): 카드 숫자 도메인 구현

---
+++ src/main/java/blackjack/model/CardNumber.java
@@ -0,0 +1,31 @@
+package blackjack.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum CardNumber {
+
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    KING(10),
+    QUEEN(10),
+    JACK(10);
+
+    private final int number;
+
+    CardNumber(int number) {
+        this.number = number;
+    }
+
+    public static List<CardNumber> getCardNumber() {
+        return Arrays.asList(values());
+    }
+}
