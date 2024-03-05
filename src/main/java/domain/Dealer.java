920503dc777af801588e182a8d47a27cb720a50a
nayonsoso <panda0329@naver.com>
2024-03-05 19:42:09 +0900

    feat: 딜러가 히트를 할 수 있는지 반환하는 기능 구현

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -2,7 +2,13 @@ package domain;
 
 public class Dealer extends Player {
 
+    private static final int MAX_SCORE_TO_HIT = 16;
+
     public Dealer(Hand hand) {
         super(new Name("딜러"), hand);
+    }
+
+    public boolean canHit() {
+        return this.getTotalScore() <= MAX_SCORE_TO_HIT;
     }
 }
