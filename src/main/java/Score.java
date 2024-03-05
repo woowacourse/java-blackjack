b4cb461f47cf200fcae3dca01a678bcd0269f36a
yechop <channetattoo@gmail.com>
2024-03-05 16:31:33 +0900

    feat: 개인 별 점수 도메인 구현

---
+++ src/main/java/Score.java
@@ -0,0 +1,25 @@
+public class Score {
+
+    private static final int THRESHOLD = 21;
+    private static final int A_SCORE_CONDITION = 10;
+    private static final int A_SCORE_GAP = 10;
+
+    private int value;
+
+    public Score() {
+        this.value = 0;
+    }
+
+    public int addScore(Card card) {
+        if (card.isAce() && value > A_SCORE_CONDITION) {
+            value += card.score() - A_SCORE_GAP;
+            return value;
+        }
+        value += card.score();
+        return value;
+    }
+
+    public boolean isLowerThanThreshold() {
+        return value < THRESHOLD;
+    }
+}
