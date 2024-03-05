f8ef8d057ed97beb32280c218828918ac8c51ef1
HyungUk Ryu <hw0603@naver.com>
2024-03-05 22:42:55 +0900

    feat(deck): 셔플된 댁 생성 하는 팩토리 메서드 추가
    Co-authored-by: haiseong <wjdgotjd9908@gmail.com>

--- src/main/java/blackjack/domain/Deck.java
+++ src/main/java/blackjack/domain/Deck.java
@@ -1,6 +1,10 @@
 package blackjack.domain;
 
 import blackjack.domain.card.Card;
+import blackjack.domain.card.Shape;
+import blackjack.domain.card.Value;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,11 +12,23 @@ public class Deck {
     private final List<Card> cards;
 
     private Deck(List<Card> cards) {
+        // TODO: 중복되는 카드 검사 로직 추가
         this.cards = cards;
     }
 
     public static Deck from(List<Card> cards) {
         return new Deck(new LinkedList<>(cards));
+    }
+
+    public static Deck createShuffledDeck() {
+        List<Card> cards = new ArrayList<>();
+
+        // TODO: 구조개선
+        for (Shape shape : Shape.values()) {
+            Arrays.stream(Value.values())
+                    .forEach(value -> cards.add(new Card(shape, value)));
+        }
+        return new Deck(cards);
     }
 
     public Card draw() {
