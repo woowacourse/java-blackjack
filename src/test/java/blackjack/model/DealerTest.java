3944d1d5e4f2606d6a320c3a7669d6a6ffea937a
jongmee <ra123221@gmail.com>
2024-03-06 17:24:07 +0900

    test: 딜러는 카드의 합이 17점 이상이 될 때까지 카드를 받는다

---
+++ src/test/java/blackjack/model/DealerTest.java
@@ -0,0 +1,24 @@
+package blackjack.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DealerTest {
+    @Test
+    @DisplayName("딜러는 카드의 합이 17점 이상이 될 때까지 카드를 받는다")
+    void canHitTest() {
+        // given
+        Dealer dealer = new Dealer(new SequentialNumberGenerator(List.of(0, 0, 0, 0)));
+
+        // when
+        dealer.doAction();
+
+        // then
+        int cardsTotal = dealer.getHand().calculateCardsTotal();
+        assertThat(cardsTotal).isGreaterThan(17);
+    }
+}
