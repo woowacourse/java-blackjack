d7c49a2d09c8dcf9bd134ea6a3823125d67d18f5
tackyu <tack970913@gmail.com>
2024-03-06 15:26:59 +0900

    feat: enum에 이름을 추가

--- src/main/java/domain/card/Number.java
+++ src/main/java/domain/card/Number.java
@@ -3,28 +3,35 @@ package domain.card;
 import java.util.List;
 
 public enum Number {
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
+    ACE(1, "A"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    JACK(10, "J"),
+    QUEEN(10, "Q"),
+    KING(10, "K");
 
-    private int number;
+    private final int number;
 
-    Number(int number) {
+    private String name;
+
+    Number(int number, String name) {
         this.number = number;
+        this.name = name;
     }
 
     public int getNumber() {
         return this.number;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public static List<Number> giveNumbers() {
--- src/main/java/domain/card/Shape.java
+++ src/main/java/domain/card/Shape.java
@@ -3,10 +3,20 @@ package domain.card;
 import java.util.List;
 
 public enum Shape {
-    SPADE,
-    DIAMOND,
-    HEART,
-    CLOVER;
+    SPADE("스페이드"),
+    DIAMOND("다이아몬드"),
+    HEART("하트"),
+    CLOVER("클로버");
+
+    private final String name;
+
+    Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 
     public static List<Shape> giveShapes() {
         return List.of(values());
