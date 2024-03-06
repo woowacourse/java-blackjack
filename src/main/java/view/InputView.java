69ef7d51d9520a32badc661eacf8f0c90c14d7b6
Eugene Jang <kyunellroll@gmail.com>
2024-03-06 17:52:26 +0900

    feat(InputView): 입력을 받는 객체 생성
    - 플레이어의 입력을 받는 메서드 구현
    - 플레이어가 카드를 더 받을 지 물어보는 메서드 구현
    
    Co-authored-by: hoyeonyy <highoption@naver.com>

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,27 @@
+package view;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final String SPLIT_SYMBOL = ",";
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public List<String> inputPlayerNames() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(" + SPLIT_SYMBOL + " 기준으로 분리)");
+        return List.of(scanner.nextLine().split(SPLIT_SYMBOL));
+    }
+
+    public boolean inputPlayerCommand(String name) {
+        System.out.println(
+                name + "는 한장의 카드를 더 받겠습니까?(예는 " + GameCommand.GET_CARD + ", 아니오는 " + GameCommand.REFUSE_CARD + ")");
+        String inputCommand = scanner.nextLine();
+
+        return GameCommand.isGetCardCommand(inputCommand);
+    }
+}
