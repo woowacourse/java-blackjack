9f6b0096d4f07d578fe6e19a2b8f757f5644b07e
libienz <leekh10195@gmail.com>
2024-03-06 20:20:49 +0900

    feat: Player의 일급 컬렉션 도메인 Players 구현

---
+++ src/main/java/blackjack/domain/Players.java
@@ -0,0 +1,51 @@
+package blackjack.domain;
+
+import java.util.List;
+
+public class Players {
+
+    private static final int MAX_COUNT = 3;
+
+    private final List<Player> players;
+
+    public Players(List<Player> players) {
+        validate(players);
+        this.players = players;
+    }
+
+    private void validate(List<Player> players) {
+        validateEachPlayerNameUnique(players);
+        validateEntryNotEmpty(players);
+        validatePlayerCountRange(players);
+    }
+
+    private void validateEachPlayerNameUnique(List<Player> players) {
+        if (countUniquePlayer(players) != players.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복되는 플레이어의 이름이 존재합니다");
+        }
+    }
+
+    private int countUniquePlayer(List<Player> players) {
+        return (int) players.stream()
+                .distinct()
+                .count();
+    }
+
+    private void validateEntryNotEmpty(List<Player> players) {
+        if (players.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 플레이어가 없습니다");
+        }
+    }
+
+    private void validatePlayerCountRange(List<Player> players) {
+        if (players.size() > MAX_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 플레이어의 수는 " + MAX_COUNT + "이하여야 합니다");
+        }
+    }
+
+    public int countPlayerWithSumExceed(int target) {
+        return (int) players.stream()
+                .filter(player -> !player.handSummationExceed(target))
+                .count();
+    }
+}
