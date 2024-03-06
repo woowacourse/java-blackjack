1ab946fe9aa1d861e22c340e8ae4ba23ca39c8bb
이상진 <priceelees@gmail.com>
2024-03-06 14:49:07 +0900

    style(DeckTest): 중복 코드 제거
    Co-authored-by: seokmyungham <gkatjraud1@gmail.com>

--- src/test/java/blackjack/domain/card/DeckTest.java
+++ src/test/java/blackjack/domain/card/DeckTest.java
@@ -2,24 +2,28 @@ package blackjack.domain.card;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DeckTest {
 
+	Deck deck;
+
+	@BeforeEach
+	void setUP() {
+		deck = new Deck();
+	}
+
 	@Test
 	@DisplayName("카드 덱은 52장의 카드로 이루어져 있다.")
 	void deckSizeTest() {
-		Deck deck = new Deck();
-
 		assertThat(deck.getCards().size()).isEqualTo(52);
 	}
 
 	@Test
 	@DisplayName("맨 위의 카드를 한 장 뽑는다.")
 	void drawTest() {
-		Deck deck = new Deck();
-
 		assertThat(deck.draw()).isEqualTo(new Card(CardShape.HEART, CardNumber.ACE));
 	}
 }
