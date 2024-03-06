796140909714ff137bad4ac9d3caae11d006223d
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-06 17:41:51 +0900

    feat(GameRule): 도메인 구현 및 테스트 코드 작성
    - selectwinner 메서드는 일단 테스트만 성공하도록 구현, 이후 리팩토링 예정

---
+++ src/main/java/blackjack/model/GameRule.java
@@ -0,0 +1,70 @@
+package blackjack.model;
+
+import blackjack.model.gamer.Dealer;
+import blackjack.model.gamer.Player;
+
+public class GameRule {
+
+    private static final int BUST_STANDARD = 21;
+    private static final int PLAYER_HIT_MAX_SCORE = 21;
+    private static final int DEALER_HIT_MAX_SCORE = 16;
+
+    public static void adjustAceValue(Deck deck) {
+        int aceCount = deck.countElevenAce();
+
+        while (aceCount > 0 && deck.calculateTotalScore() > BUST_STANDARD) {
+            deck.switchAceValueInRow();
+            aceCount--;
+        }
+    }
+
+    private static boolean isBusted(int cardScore) {
+        return cardScore > BUST_STANDARD;
+    }
+
+    private static boolean isBlackjack(int cardScore) {
+        return cardScore == BUST_STANDARD;
+    }
+
+    public static boolean playerHitRule(int cardScore) {
+        return cardScore <= PLAYER_HIT_MAX_SCORE;
+    }
+
+    public static boolean dealerHitRule(int cardScore) {
+        return cardScore <= DEALER_HIT_MAX_SCORE;
+    }
+
+    public static int selectWinner(Dealer dealer, Player player) {
+        int dealerScore = dealer.calculateTotalScore();
+        int playerScore = player.calculateTotalScore();
+
+        boolean isDealerBust = isBusted(dealerScore);
+        boolean isPlayerBust = isBusted(playerScore);
+
+        if (isDealerBust && !isPlayerBust) {
+            return -1;
+        }
+
+        if (!isDealerBust && isPlayerBust) {
+            return 1;
+        }
+
+        if (isDealerBust && isPlayerBust) {
+            return 1;
+        }
+
+        if (dealerScore == playerScore && isBlackjack(dealerScore)) {
+            return -1;
+        }
+
+        if (dealerScore == playerScore) {
+            return 0;
+        }
+
+        if (dealerScore > playerScore) {
+            return 1;
+        }
+
+        return -1;
+    }
+}
