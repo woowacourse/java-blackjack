26ff2e3d1657c0ad63cc2419b5726909040428a3
kelly6bf <kelly6bf@gmail.com>
2024-03-06 16:31:00 +0900

    feat: Dealer의 드로우 가능 여부 판단 기능 구현

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -1,13 +1,25 @@
 package domain;
 
-public class Dealer {
+public class Dealer implements Participant {
+    private static final int MAX_DEALER_HAND_VALUE = 16;
+
     private final Hand hand;
 
-    private Dealer(final Hand hand) {
+    Dealer(final Hand hand) {
         this.hand = hand;
     }
 
     public static Dealer init() {
         return new Dealer(Hand.init());
+    }
+
+    @Override
+    public boolean isDrawable() {
+        return hand.getCardsNumberSum() <= MAX_DEALER_HAND_VALUE;
+    }
+
+    @Override
+    public void draw(final Deck deck) {
+
     }
 }
--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -3,6 +3,10 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static domain.PlayingCardShape.DIAMOND;
+import static domain.PlayingCardValue.EIGHT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DealerTest {
@@ -15,5 +19,20 @@ public class DealerTest {
 
         // Then
         assertThat(dealer).isNotNull();
+    }
+
+    @DisplayName("딜러의 손패 합이 16이하이면 true를 반환한다.")
+    @Test
+    void isDrawableTest() {
+        // Given
+        List<PlayingCard> playingCards = List.of(new PlayingCard(DIAMOND, EIGHT));
+        Hand hand = new Hand(playingCards);
+        Dealer dealer = new Dealer(hand);
+
+        // When
+        boolean isDrawable = dealer.isDrawable();
+
+        // Then
+        assertThat(isDrawable).isTrue();
     }
 }
