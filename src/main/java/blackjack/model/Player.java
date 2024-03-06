e3b1ec0040c0c7e814a09c16215ce86cd7e10a5a
jongmee <ra123221@gmail.com>
2024-03-06 17:19:24 +0900

    feat: 21을 넘지 않을 경우 얼마든지 카드를 계속 뽑을 수 있다

--- src/main/java/blackjack/model/Player.java
+++ src/main/java/blackjack/model/Player.java
@@ -2,6 +2,7 @@ package blackjack.model;
 
 public class Player {
     private static final String INVALID_NAME_LENGTH = "참여자 이름은 한 글자 이상이다";
+    private static final int HIT_CONDITION = 21;
 
     private final String name;
     private final Hand hand;
@@ -16,5 +17,10 @@ public class Player {
         if (name.isEmpty()) {
             throw new IllegalArgumentException(INVALID_NAME_LENGTH);
         }
+    }
+
+    public boolean canHit() {
+        int cardsTotal = hand.calculateCardsTotal();
+        return cardsTotal <= 21;
     }
 }
--- src/test/java/blackjack/model/PlayerTest.java
+++ src/test/java/blackjack/model/PlayerTest.java
@@ -1,10 +1,12 @@
 package blackjack.model;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PlayerTest {
     @Test
