04924ed676d2214674a1d06adf05d129c4ee1aa8
tackyu <tack970913@gmail.com>
2024-03-06 15:33:24 +0900

    feat: 한 장의 카드 보여주는 기능 구현

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -1,4 +1,13 @@
 package domain.user;
 
+import domain.card.Card;
+
 public class Dealer extends User {
+    Dealer() {
+        super();
+    }
+
+    public Card getVisibleCard() {
+        return this.userDeck.getCards().get(0);
+    }
 }
