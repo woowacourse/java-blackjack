1a4498230ba555cba054fa5ddf2a3fba96514a0a
Choo <choo000407@naver.com>
2024-03-06 17:49:19 +0900

    feat: 각 참여자마다 Hit 혹은 Stand 여부를 y 혹은 n으로 입력 받는다

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String ASK_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String ASK_HIT_OR_STAND = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String INVALID_HIT_OR_STAND_COMMAND = "y 또는 n을 입력하세요.";
 
     private final Scanner scanner;
 
@@ -14,6 +16,7 @@ public class InputView {
     }
 
     public List<String> askPlayerNames() {
+        System.out.println(ASK_PLAYER_NAMES);
         String input = scanner.nextLine();
         return formatPlayerNames(input);
     }
@@ -22,5 +25,21 @@ public class InputView {
         return Arrays.stream(input.split(","))
                 .map(String::trim)
                 .toList();
+    }
+
+    public boolean askHitOrStandCommand(final String name) {
+        System.out.printf(ASK_HIT_OR_STAND, name);
+        String input = scanner.nextLine();
+        return formatHitOrStandCommand(input);
+    }
+
+    private boolean formatHitOrStandCommand(String input) {
+        if (input.equals("y")) {
+            return true;
+        }
+        if (input.equals("n")) {
+            return false;
+        }
+        throw new IllegalArgumentException(INVALID_HIT_OR_STAND_COMMAND);
     }
 }
