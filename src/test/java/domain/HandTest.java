b5c97bdc08f942c9c5733a93ffd64cf6fd3f688f
nayonsoso <panda0329@naver.com>
2024-03-06 15:14:58 +0900

    feat: Ace를 고려한 손 패 점수를 반환하는 기능 구현

--- src/test/java/domain/HandTest.java
+++ src/test/java/domain/HandTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -45,5 +46,115 @@ class HandTest {
         int actual1 = hasAceHand.countAceCard();
 
         assertThat(actual1).isEqualTo(1);
+    }
+
+    @Nested
+    @DisplayName("손패의 점수를 계산한다.")
+    class calculateScore {
+
+        @DisplayName("블랙잭인 경우")
+        @Test
+        void blackJack() {
+            List<Card> cards = List.of(
+                    new Card(Letter.A, Mark.SPADE),
+                    new Card(Letter.J, Mark.SPADE));
+
+            Hand hand = new Hand(cards);
+            int score = hand.calculateScore();
+
+            assertThat(score).isEqualTo(21);
+        }
+
+        @DisplayName("A가 하나 있고, A를 1로 계산해야 하는 경우")
+        @Test
+        void oneAceOneRegard() {
+            List<Card> cards = List.of(
+                    new Card(Letter.A, Mark.SPADE),
+                    new Card(Letter.J, Mark.SPADE),
+                    new Card(Letter.K, Mark.SPADE)
+            );
+
+            Hand hand = new Hand(cards);
+            int score = hand.calculateScore();
+
+            assertThat(score).isEqualTo(21);
+        }
+
+        @DisplayName("A가 두개 있고, A를 둘 다 1로 계산해야 하는 경우")
+        @Test
+        void TwoAceTwoRegard() {
+            List<Card> cards = List.of(
+                    new Card(Letter.A, Mark.SPADE),
+                    new Card(Letter.A, Mark.SPADE),
+                    new Card(Letter.K, Mark.SPADE)
+            );
+
+            Hand hand = new Hand(cards);
+            int score = hand.calculateScore();
+
+            assertThat(score).isEqualTo(12);
+        }
+
+        @DisplayName("A가 세개 있고, 하나만 11로 계산해야 하는 경우")
+        @Test
+        void ThreeAceTwoRegard() {
+            List<Card> cards = List.of(
+                    new Card(Letter.A, Mark.SPADE),
+                    new Card(Letter.A, Mark.SPADE),
+                    new Card(Letter.A, Mark.SPADE),
+                    new Card(Letter.TWO, Mark.SPADE)
+            );
+
+            Hand hand = new Hand(cards);
+            int score = hand.calculateScore();
+
+            assertThat(score).isEqualTo(15);
+        }
+
+        @DisplayName("A가 없는 경우 - 버스트")
+        @Test
+        void NoAceBust() {
+            List<Card> cards = List.of(
+                    new Card(Letter.K, Mark.SPADE),
+                    new Card(Letter.K, Mark.SPADE),
+                    new Card(Letter.K, Mark.SPADE)
+            );
+
+            Hand hand = new Hand(cards);
+            int score = hand.calculateScore();
+
+            assertThat(score).isEqualTo(30);
+        }
+
+        @DisplayName("A가 없는 경우 - 버스트아님")
+        @Test
+        void NoAceNotBust() {
+            List<Card> cards = List.of(
+                    new Card(Letter.J, Mark.SPADE),
+                    new Card(Letter.K, Mark.SPADE)
+            );
+
+            Hand hand = new Hand(cards);
+            int score = hand.calculateScore();
+
+            assertThat(score).isEqualTo(20);
+        }
+
+        @DisplayName("A가 있으면서, A를 1로 생각해도 버스트가 나는 경우")
+        @Test
+        void HasAceBust() {
+            List<Card> cards = List.of(
+                    new Card(Letter.J, Mark.SPADE),
+                    new Card(Letter.A, Mark.SPADE),
+                    new Card(Letter.EIGHT, Mark.SPADE),
+                    new Card(Letter.K, Mark.SPADE)
+            );
+
+            Hand hand = new Hand(cards);
+            int score = hand.calculateScore();
+
+            assertThat(score).isEqualTo(29);
+        }
+
     }
 }
