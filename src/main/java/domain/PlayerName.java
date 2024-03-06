ae44081298ed622ebdc3ccccdcedd883e6d0fd5b
kelly6bf <kelly6bf@gmail.com>
2024-03-06 15:02:35 +0900

    feat: 플레이어 이름 길이 검증 기능 구현

--- src/main/java/domain/PlayerName.java
+++ src/main/java/domain/PlayerName.java
@@ -1,4 +1,13 @@
 package domain;
 
 public record PlayerName (String value) {
+    public PlayerName {
+        validateLength(value);
+    }
+
+    private void validateLength(String value) {
+        if (value == null || value.isEmpty() || value.length() > 5) {
+            throw new IllegalArgumentException("플레이어 이름은 1글자 이상 5글자 이하여야 합니다.");
+        }
+    }
 }
--- src/test/java/domain/PlayerNameTest.java
+++ src/test/java/domain/PlayerNameTest.java
@@ -1,7 +1,11 @@
 package domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,5 +22,16 @@ public class PlayerNameTest {
 
         // Then
         assertThat(playerName).isNotNull();
+    }
+
+    @DisplayName("유효하지 않은 길이의 이름이 입력되면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"kellykelly"})
+    void playerNameLengthTest(String invalidName) {
+        // When & Then
+        Assertions.assertThatThrownBy(() -> new PlayerName(invalidName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("플레이어 이름은 1글자 이상 5글자 이하여야 합니다.");
     }
 }
