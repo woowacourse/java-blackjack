22f942accc4a3917b59f62ccf0b4afe57b1d10ec
JUHA <84626225+khabh@users.noreply.github.com>
2024-03-06 17:14:06 +0900

    feat: players 생성 로직 추가

--- src/main/java/blackjack/domain/participant/Players.java
+++ src/main/java/blackjack/domain/participant/Players.java
@@ -3,12 +3,22 @@ package blackjack.domain.participant;
 import blackjack.domain.HandGenerator;
 import blackjack.domain.Name;
 import blackjack.exception.InvalidPlayerCountException;
+
 import java.util.List;
 
 public class Players {
+    private final List<Player> values;
+
     public Players(List<Name> playerNames, HandGenerator handGenerator) {
         if (playerNames.isEmpty()) {
             throw new InvalidPlayerCountException();
         }
+        this.values = playerNames.stream()
+                .map(playerName -> new Player(playerName, handGenerator))
+                .toList();
+    }
+
+    public List<Player> getValues() {
+        return values;
     }
 }
