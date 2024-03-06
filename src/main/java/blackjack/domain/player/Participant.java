72467cffde8635217135cb3ca3d8ff699e446158
JUHA <84626225+khabh@users.noreply.github.com>
2024-03-06 15:10:04 +0900

    feat: Participant 추상 클래스 정의

---
+++ src/main/java/blackjack/domain/player/Participant.java
@@ -0,0 +1,15 @@
+package blackjack.domain.player;
+
+import blackjack.domain.HandGenerator;
+import blackjack.domain.Name;
+import blackjack.domain.card.Hand;
+
+public abstract class Participant {
+    private final Name name;
+    private final Hand hand;
+
+    public Participant(Name name, HandGenerator handGenerator) {
+        this.name = name;
+        this.hand = handGenerator.generate();
+    }
+}
