838cb42d8b680b83602c215a10c9196122149319
dwax1324 <dwax1324@gmail.com>
2024-03-06 16:08:44 +0900

    refactor: Ace의 값을 판단하는 로직 개선

--- src/main/java/domain/Denomination.java
+++ src/main/java/domain/Denomination.java
@@ -4,37 +4,32 @@ import java.util.Arrays;
 import java.util.function.Function;
 
 public enum Denomination {
-    ACE(determineAceValue()),
-    TWO(number -> 2),
-    THREE(number -> 3),
-    FOUR(number -> 4),
-    FIVE(number -> 5),
-    SIX(number -> 6),
-    SEVEN(number -> 7),
-    EIGHT(number -> 8),
-    NINE(number -> 9),
-    TEN(number -> 10),
-    JACK(number -> 10),
-    QUEEN(number -> 10),
-    KING(number -> 10);
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10);
 
-    private static Function<Integer, Integer> determineAceValue() {
-        return number -> {
-            if (number + 11 <= 21) {
-                return 11;
-            }
-            return 1;
-        };
+
+    private final int value;
+
+    Denomination(final int value) {
+        this.value = value;
     }
 
-    private final Function<Integer, Integer> function;
-
-    Denomination(final Function<Integer, Integer> function) {
-        this.function = function;
-    }
-
-    public int apply(final int score) {
-        return this.function.apply(score);
+    public int getValue(int score) {
+        if (this == ACE && score + 11 <= 21) {
+            return 11;
+        }
+        return this.value;
     }
 
     public static Denomination getDenomination(int number) {
