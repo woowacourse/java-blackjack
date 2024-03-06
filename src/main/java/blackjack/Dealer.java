87e8ef13203996dd10c17715d9ba2edecf37f85b
HoeSeong123 <ghltjd3@gmail.com>
2024-03-06 15:00:23 +0900

    feat(Dealer) : 기본 이름을 가진 딜러 도메인 생성 기능 구현
    - 이에 따른 테스트 추가

--- src/main/java/blackjack/Dealer.java
+++ src/main/java/blackjack/Dealer.java
@@ -1,6 +1,8 @@
 package blackjack;
 
 public class Dealer {
+    private static final Integer RECEIVE_SIZE = 16;
+    private static final String DEFAULT_DEALER_NAME = "딜러";
 
     private final Name name;
     private final Cards cards;
@@ -8,5 +10,13 @@ public class Dealer {
     public Dealer(Name name, Cards cards) {
         this.name = name;
         this.cards = cards;
+    }
+
+    public static Dealer createDefaultDealer(Cards cards) {
+        return new Dealer(new Name(DEFAULT_DEALER_NAME), cards);
+    }
+
+    public String getName() {
+        return name.asString();
     }
 }
--- src/test/java/blackjack/DealerTest.java
+++ src/test/java/blackjack/DealerTest.java
@@ -1,6 +1,8 @@
 package blackjack;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -11,8 +13,27 @@ public class DealerTest {
     @DisplayName("이름과 카드 일급 컬렉션을 통해서 딜러를 생성 한다.")
     public void Dealer_Instance_create_with_name_and_cards() {
         Name name = new Name("딜러");
-        Cards cards = new Cards(List.of(CardValue.EIGHT, CardValue.FOUR));
+        Cards cards = 카드_목록_생성(List.of(CardValue.EIGHT, CardValue.FOUR));
 
         assertThatCode(() -> new Dealer(name, cards)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("기본 이름(딜러)을 가진 딜러를 생성 한다.")
+    public void Dealer_Instance_create_with_default_name_and_cards() {
+        Cards cards = 카드_목록_생성(List.of(CardValue.EIGHT, CardValue.FOUR));
+
+        assertThatCode(() -> {
+                    var sut = Dealer.createDefaultDealer(cards);
+                    assertThat(sut.getName()).isEqualTo("딜러");
+                }
+        ).doesNotThrowAnyException();
+    }
+
+
+    private Cards 카드_목록_생성(List<CardValue> cardValues) {
+        return new Cards(cardValues.stream()
+                                   .map(cardValue -> new Card(cardValue, CardSymbol.DIAMOND))
+                                   .toList());
     }
 }
