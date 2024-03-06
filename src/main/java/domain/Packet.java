50e902d7b1d0b15bafa19474e116dd7202c0b0e8
jinwoo22 <jenny010925@naver.com>
2024-03-06 15:06:53 +0900

    feat: 카드 합 계산시 ACE 고려하는 로직 추가

--- src/main/java/domain/Packet.java
+++ src/main/java/domain/Packet.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class Packet { //TODO: 더 좋은 이름이 있다면 날 설득해줘
 
+    private static final int BLACK_JACK = 21;
+
     private final List<Card> cards;
 
-    private Packet(final List<Card> cards) {
+    public Packet(final List<Card> cards) { //TODO validation
         this.cards = cards;
     }
 
@@ -15,8 +17,25 @@ public class Packet { //TODO: 더 좋은 이름이 있다면 날 설득해줘
         return new Packet(new ArrayList<>());
     }
 
+    public int sum() {
+        int total = cards.stream()
+                .mapToInt(Card::getCardNumber)
+                .sum();
+
+        if (hasAce() && total + 10 <= BLACK_JACK) { //TODO 메서드분리
+            return total + 10;
+        }
+
+        return total;
+    }
+
     public void add(final Card card) {
         cards.add(card);
+    }
+
+    private boolean hasAce() {
+        return cards.stream()
+                .anyMatch(Card::isAce);
     }
 
     public int size() {
--- src/test/java/domain/PacketTest.java
+++ src/test/java/domain/PacketTest.java
@@ -3,15 +3,19 @@ package domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 class PacketTest {
 
     @Test
     @DisplayName("카드를 가지고 있는 객체를 생성한다.")
     void createPacket() {
-        Assertions.assertThatCode(() -> Packet.createEmptyPacket())
+        Assertions.assertThatCode(Packet::createEmptyPacket)
                 .doesNotThrowAnyException();
     }
 
@@ -26,5 +30,81 @@ class PacketTest {
 
         //then
         Assertions.assertThat(packet.size()).isEqualTo(1);
+    }
+
+    @DisplayName("카드의 합을 구한다.")
+    @ParameterizedTest
+    @MethodSource("sumParameterProvider")
+    void sum(final Packet packet, final int expected) {
+        // given & when
+        final int result = packet.sum();
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    //TODO: 디스플레이네임 수정 필요, 좀더 명확하고, 이해하기 쉽게
+    // 코드 중복이 발생한다.
+    @DisplayName("에이스를 11로 계산한다.")
+    @ParameterizedTest
+    @MethodSource("sumAce11ParameterProvider")
+    void sumAce11(final Packet packet, final int expected) {
+        // given & when
+        final int result = packet.sum();
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("에이스를 1로 계산한다.")
+    @ParameterizedTest
+    @MethodSource("sumAce1ParameterProvider")
+    void sumAce1(final Packet packet, final int expected) {
+        // given & when
+        final int result = packet.sum();
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> sumParameterProvider() {
+        return Stream.of(
+                Arguments.of(new Packet(List.of(new Card(CardNumber.TWO, CardShape.HEART),
+                                new Card(CardNumber.EIGHT, CardShape.SPADE),
+                                new Card(CardNumber.JACK, CardShape.CLOVER))),
+                        20),
+                Arguments.of(new Packet(List.of(new Card(CardNumber.THREE, CardShape.DIAMOND),
+                                new Card(CardNumber.NINE, CardShape.CLOVER),
+                                new Card(CardNumber.NINE, CardShape.CLOVER))),
+                        21)
+        );
+    }
+
+    static Stream<Arguments> sumAce11ParameterProvider() {
+        return Stream.of(
+                Arguments.of(new Packet(List.of(new Card(CardNumber.ACE, CardShape.HEART),
+                                new Card(CardNumber.EIGHT, CardShape.SPADE),
+                                new Card(CardNumber.TWO, CardShape.CLOVER))),
+                        21),
+                Arguments.of(new Packet(List.of(new Card(CardNumber.ACE, CardShape.DIAMOND),
+                                new Card(CardNumber.TWO, CardShape.CLOVER),
+                                new Card(CardNumber.FOUR, CardShape.CLOVER),
+                                new Card(CardNumber.TWO, CardShape.CLOVER))),
+                        19)
+        );
+    }
+
+    static Stream<Arguments> sumAce1ParameterProvider() {
+        return Stream.of(
+                Arguments.of(new Packet(List.of(new Card(CardNumber.ACE, CardShape.HEART),
+                                new Card(CardNumber.NINE, CardShape.SPADE),
+                                new Card(CardNumber.NINE, CardShape.CLOVER))),
+                        19),
+                Arguments.of(new Packet(List.of(new Card(CardNumber.ACE, CardShape.DIAMOND),
+                                new Card(CardNumber.EIGHT, CardShape.CLOVER),
+                                new Card(CardNumber.FIVE, CardShape.CLOVER),
+                                new Card(CardNumber.SIX, CardShape.CLOVER))),
+                        20)
+        );
     }
 }
