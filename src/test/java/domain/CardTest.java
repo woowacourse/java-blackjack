7fc2881342826ee27c94c53fc20a12ef55270bf4
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-03-06 17:31:24 +0900

    test: Card테스트 추가

--- src/test/java/domain/CardTest.java
+++ src/test/java/domain/CardTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,5 +13,21 @@ public class CardTest {
     void constructorSuccessTest() {
         Assertions.assertThatNoException()
                 .isThrownBy(() -> new Card(Shape.HEART, Rank.ACE));
+    }
+
+    @Test
+    @DisplayName("카드가 에이스임이 참이다.")
+    void isAceTrueTest() {
+        Card card = new Card(Shape.HEART, Rank.ACE);
+
+        assertThat(card.isAce()).isTrue();
+    }
+
+    @Test
+    @DisplayName("카드가 에이스임이 거짓이다.")
+    void isAceFailTest() {
+        Card card = new Card(Shape.HEART, Rank.JACK);
+
+        assertThat(card.isAce()).isFalse();
     }
 }
