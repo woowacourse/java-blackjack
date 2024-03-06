2eb6d0a13da156e177a7c703532a7cf34aa4f1bb
MingyeomKim <mingyum119@naver.com>
2024-03-06 15:16:59 +0900

    feat(Dealer): 딜러가 가지고 있는 카드의 값이 16 이하인지 확인한다.

---
+++ src/main/java/domain/Dealer.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Dealer extends Player {
+    public Dealer(final String name) {
+        super(name);
+    }
+
+    public boolean isUpToThreshold(final int threshold) {
+        return this.calculateScore() > threshold;
+    }
+}
---
+++ src/test/java/DealerTest.java
@@ -0,0 +1,21 @@
+import domain.Card;
+import domain.Dealer;
+import domain.constants.CardValue;
+import domain.constants.Shape;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DealerTest {
+    @DisplayName("딜러가 가지고 있는 카드가 16 초과이면 참을 반환한다.")
+    @Test
+    void isUpToThreshold() {
+        int threshold = 16;
+        Dealer dealer = new Dealer("딜러");
+        dealer.add(new Card(CardValue.TEN, Shape.CLOVER));
+        dealer.add(new Card(CardValue.SEVEN, Shape.CLOVER));
+
+        boolean isUp = dealer.isUpToThreshold(threshold);
+        Assertions.assertTrue(isUp);
+    }
+}
