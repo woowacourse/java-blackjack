244190a5a2e1768a2fa5c291598e6dec00d2048b
JUHA <84626225+khabh@users.noreply.github.com>
2024-03-06 15:09:39 +0900

    feat: Players 클래스 생성 및 테스트

---
+++ src/main/java/blackjack/domain/player/Players.java
@@ -0,0 +1,15 @@
+package blackjack.domain.player;
+
+import blackjack.domain.HandGenerator;
+import blackjack.domain.Name;
+import blackjack.exception.InvalidPlayerCountException;
+
+import java.util.List;
+
+public class Players {
+    public Players(List<Name> playerNames, HandGenerator handGenerator) {
+        if (playerNames.isEmpty()) {
+            throw new InvalidPlayerCountException();
+        }
+    }
+}
---
+++ src/test/java/blackjack/domain/player/PlayersTest.java
@@ -0,0 +1,24 @@
+package blackjack.domain.player;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import blackjack.domain.Deck;
+import blackjack.domain.HandGenerator;
+import blackjack.domain.RandomDeck;
+import blackjack.exception.InvalidPlayerCountException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+class PlayersTest {
+    @DisplayName("플레이어가 한 명 미만이면 예외가 발생한다.")
+    @Test
+    void invalidPlayerCountTest() {
+        Deck deck = RandomDeck.getInstance();
+        HandGenerator handGenerator = new HandGenerator(deck);
+
+        assertThatThrownBy(() -> new Players(new ArrayList<>(), handGenerator))
+                .isInstanceOf(InvalidPlayerCountException.class);
+    }
+}
