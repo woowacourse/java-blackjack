15878eb3e47c61d19f2c09cf441209ebf983f2a7
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-03-06 18:00:44 +0900

    test: Player 테스트 추가

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,11 +16,37 @@ public class PlayerTest {
     }
 
     @Test
+    @DisplayName("플레이어가 덱을 받는다.")
+    void receiveDeckTest() {
+        Player player = new Player(new Name("이름"));
+        Deck deck = new Deck();
+        deck.addCard(new Card(Shape.HEART, Rank.ACE));
+        deck.addCard(new Card(Shape.HEART, Rank.TWO));
+        player.receiveDeck(deck);
+
+        assertThat(player.getDeck().size()).isEqualTo(2);
+    }
+
+    @Test
     @DisplayName("플레이어가 카드를 잘 받는다.")
     void playerReceiveCardTest() {
         Player player = new Player(new Name("이름"));
         Assertions.assertThatNoException()
                 .isThrownBy(() -> player.receiveCard(new Card(Shape.HEART, Rank.ACE)));
+    }
+
+    @Test
+    @DisplayName("플레이어 점수를 계산한다.")
+    void calculateScoreTest() {
+        Player player = new Player(new Name("이름"));
+        Deck deck = new Deck();
+        deck.addCard(new Card(Shape.HEART, Rank.TEN));
+        deck.addCard(new Card(Shape.HEART, Rank.TWO));
+
+        player.receiveDeck(deck);
+        int result = player.calculateScore();
+
+        assertThat(result).isEqualTo(12);
     }
 
     @Test
@@ -29,7 +57,7 @@ public class PlayerTest {
         player.receiveCard(new Card(Shape.HEART, Rank.ACE));
         player.receiveCard(new Card(Shape.HEART, Rank.NINE));
 
-        Assertions.assertThat(player.isNotOver(21)).isTrue();
+        assertThat(player.isNotOver(21)).isTrue();
     }
 
     @Test
@@ -41,6 +69,6 @@ public class PlayerTest {
         player.receiveCard(new Card(Shape.SPADE, Rank.NINE));
         player.receiveCard(new Card(Shape.DIAMOND, Rank.FOUR));
 
-        Assertions.assertThat(player.isNotOver(21)).isFalse();
+        assertThat(player.isNotOver(21)).isFalse();
     }
 }
