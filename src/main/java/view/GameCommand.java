5369ead8c8b1fa5f0ebd59a1f5c51bf9f11e468c
Eugene Jang <kyunellroll@gmail.com>
2024-03-06 17:51:35 +0900

    feat(GameCommand): 게임의 Command 집합을 생성
    - 받은 Command와 일치하는지 확인하는 메서드 구현
    
    Co-authored-by: hoyeonyy <highoption@naver.com>

---
+++ src/main/java/view/GameCommand.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.util.Arrays;
+
+public enum GameCommand {
+
+    GET_CARD("Y"),
+    REFUSE_CARD("N");
+
+    final String command;
+
+    GameCommand(String command) {
+        this.command = command;
+    }
+
+    public static boolean isGetCardCommand(String inputCommand) {
+        return Arrays.stream(values())
+                .anyMatch(gameCommand -> gameCommand.command.equals(inputCommand));
+    }
+}
