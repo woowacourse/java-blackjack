e5f77b905a2451a8c98124bb1f7673041c3a6daf
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-06 17:39:54 +0900

    feat(CardProperties): 도메인 구현

---
+++ src/main/java/blackjack/model/CardProperties.java
@@ -0,0 +1,20 @@
+package blackjack.model;
+
+public class CardProperties {
+
+    private final CardPattern cardPattern;
+    private final CardNumber cardNumber;
+
+    public CardProperties(CardPattern cardPattern, CardNumber cardNumber) {
+        this.cardPattern = cardPattern;
+        this.cardNumber = cardNumber;
+    }
+
+    public CardNumber getCardNumber() {
+        return cardNumber;
+    }
+
+    public CardPattern getCardPattern() {
+        return cardPattern;
+    }
+}
