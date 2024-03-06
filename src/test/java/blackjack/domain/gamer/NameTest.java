939882c368128e30766b143608c8898659b0b6cd
이상진 <priceelees@gmail.com>
2024-03-06 17:16:19 +0900

    test(NameTest): 정상 입력에 대한 테스트 추가
    Co-authored-by: seokmyungham <gkatjraud1@gmail.com>

--- src/test/java/blackjack/domain/gamer/NameTest.java
+++ src/test/java/blackjack/domain/gamer/NameTest.java
@@ -9,9 +9,16 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class NameTest {
 
 	@ParameterizedTest
+	@ValueSource(strings = {"a", "abcde"})
+	@DisplayName("이름의 길이가 1글자에서 5글자인 경우 예외가 발생되지 않는다.")
+	void validNameLengthTest(String name) {
+		assertThatCode(() -> new Name(name)).doesNotThrowAnyException();
+	}
+
+	@ParameterizedTest
 	@ValueSource(strings = {"", "abcdef"})
 	@DisplayName("이름의 길이가 0글자이거나, 5글자를 초과하면 예외가 발생된다.")
-	void nameLengthExceptionTest(String name) {
+	void invalidNameLengthTest(String name) {
 		assertThatThrownBy(() -> new Name(name))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("이름의 길이는 최소 1글자부터 최대 5글자까지 가능합니다.");
