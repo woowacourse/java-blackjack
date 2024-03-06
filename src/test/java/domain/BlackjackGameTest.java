83e8fcbe4dda756c75395c0b53711f5edd7ee3ed
Nam Gi Beom <rlqja0523@naver.com>
2024-03-06 18:07:39 +0900

    refactor: 리플렉션으로 임시 테스팅 추가
    Co-authored-by: jcoding-play <namejgc@naver.com>

--- src/test/java/domain/BlackjackGameTest.java
+++ src/test/java/domain/BlackjackGameTest.java
@@ -1,31 +1,22 @@
 package domain;
 
 import static fixture.PlayersFixture.플레이어들;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
 
 class BlackjackGameTest {
     @Test
-    void 게임을_시작하기_전에_카드를_나눈다() {
+    void 게임을_시작하기_전에_카드를_나눈다() { // todo 플레이어의 카드 개수도 검증
         CardDeck cardDeck = new CardDeck();
         Dealer dealer = new Dealer(cardDeck);
-        Player roro = new Player("뽀로로");
-        Player prin = new Player("프린");
-        Player pobi = new Player("포비");
-        Players players = 플레이어들(roro, prin, pobi);
+        Players players = 플레이어들("뽀로로", "프린", "포비");
         BlackjackGame blackjackGame = new BlackjackGame(dealer, players);
 
         blackjackGame.initGame();
 
-//        assertThat(roro.getCards()).hasSize(2);
-//        assertThat(prin.getCards()).hasSize(2);
-//        assertThat(dealer).extracting("cards").extracting("cards", InstanceOfAssertFactories.list(Card.class))
-//                .hasSize(2);
-//        assertThat(roro).extracting("cards").extracting("cards", InstanceOfAssertFactories.list(Card.class))
-//                .hasSize(2);
-//        assertThat(prin).extracting("cards").extracting("cards", InstanceOfAssertFactories.list(Card.class))
-//                .hasSize(2);
-//        assertThat(pobi).extracting("cards").extracting("cards", InstanceOfAssertFactories.list(Card.class))
-//                .hasSize(2);
+        assertThat(dealer).extracting("cards").extracting("cards", InstanceOfAssertFactories.list(Card.class))
+                .hasSize(2);
     }
 }
