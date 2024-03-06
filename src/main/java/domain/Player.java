f21f356bc883525b4602f9f28292fd2cd8772c6d
dwax1324 <dwax1324@gmail.com>
2024-03-06 16:03:04 +0900

    feat: 플레이어는 21을 넘을 경우 패배

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -4,13 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
+    private String name;
     private final List<Card> cards = new ArrayList<>();
 
+    public Player(final String name) {
+        this.name = name;
+    }
+
+    public Player() {
+    }
 
     public void addCard(final Card card) {
         cards.add(card);
     }
 
+
+    public boolean alive() {
+        return calculateScore() < 21;
+    }
 
     public int calculateScore() {
         int score = 0;
@@ -18,5 +29,13 @@ public class Player {
             score += card.getValue(score);
         }
         return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Card> getCards() {
+        return cards;
     }
 }
