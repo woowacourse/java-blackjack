c400ba4f263b6628ce18aba7f9494fa193c3d2de
lemone <lejinhy@gmail.com>
2024-03-06 14:34:57 +0900

    test: 카드 hit 테스트 완료
    Co-Authored-By: Gyeongho Yang <chchch1213@naver.com>

--- src/test/java/blackjack/domain/GameRoundTest.java
+++ src/test/java/blackjack/domain/GameRoundTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GameRoundTest {
     @Test
     @DisplayName("플레이어 한명이 처음 받은 카드 두장을 반환한다.")
-    void gameRunTest() {
+    void gameDealTest() {
         // given
         Player player = new Player("lemone");
         GameRound gameRound = new GameRound(player, new Dealer());
@@ -18,6 +18,20 @@ public class GameRoundTest {
 
         // then
         assertThat(player.getCards().size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("플레이어 한명이 카드를 뽑기로 결정했을 때 카드 한장을 반환한다.")
+    void gameHitTest() {
+        // given
+        Player player = new Player("lemone");
+        GameRound gameRound = new GameRound(player, new Dealer());
+        CardPicker cardPicker = new CardPicker();
+        // when
+        gameRound.hit(cardPicker);
+
+        // then
+        assertThat(player.getCards().size()).isEqualTo(1);
     }
 
 }
