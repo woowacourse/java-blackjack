33d4b7be44afe79be6e5d75bacf9513660472a15
eun-byeol <joeunbyeol98@gmail.com>
2024-03-06 15:03:01 +0900

    test: 접근제어자 삭제
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    Co-authored-by: lilychoibb <choiyoonseo02@gmail.com>

--- src/test/java/model/CardTest.java
+++ src/test/java/model/CardTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CardTest {
+class CardTest {
 
     @DisplayName("숫자와 모양을 가진 카드를 생성한다")
     @Test
