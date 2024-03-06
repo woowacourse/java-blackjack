973e4de93f3628cb4c4f86cad5b0ef8833051b19
masonkimseoul <masonkimseoul@gmail.com>
2024-03-06 16:17:40 +0900

    test: CustomDeck 생성 및 테스트 적용

--- src/test/java/blackjack/domain/HandGeneratorTest.java
+++ src/test/java/blackjack/domain/HandGeneratorTest.java
@@ -3,6 +3,10 @@ package blackjack.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import blackjack.domain.card.Hand;
+import blackjack.domain.card.Number;
+import blackjack.domain.card.Shape;
+import blackjack.testutil.CustomDeck;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +14,9 @@ class HandGeneratorTest {
     @DisplayName("카드를 2장 가진 Hand를 생성한다.")
     @Test
     void generateHandTest() {
-        // TODO : Test용 구현체로 수정할 것
-        Deck deck = RandomDeck.getInstance();
+        List<Number> numbers = List.of(Number.ACE, Number.EIGHT);
+        List<Shape> shapes = List.of(Shape.SPADE, Shape.CLOVER);
+        Deck deck = new CustomDeck(numbers, shapes);
         HandGenerator handGenerator = new HandGenerator(deck);
 
         Hand hand = handGenerator.generate();
---
+++ src/test/java/blackjack/testutil/CustomDeck.java
@@ -0,0 +1,31 @@
+package blackjack.testutil;
+
+import blackjack.domain.Deck;
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Number;
+import blackjack.domain.card.Shape;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class CustomDeck implements Deck {
+    private final Iterator<Card> cards;
+
+    public CustomDeck(List<Number> numbers, List<Shape> shapes) {
+        if (shapes.size() != numbers.size()) {
+            throw new IllegalArgumentException("모양과 숫자의 개수가 일치하지 않습니다.");
+        }
+        cards = IntStream.range(0, shapes.size())
+                .mapToObj((index) ->
+                        new Card(numbers.get(index), shapes.get(index)))
+                .iterator();
+    }
+
+    @Override
+    public Card drawCard() {
+        if (!cards.hasNext()) {
+            throw new IllegalArgumentException("더 이상 뽑을 수 있는 카드가 없습니다.");
+        }
+        return cards.next();
+    }
+}
