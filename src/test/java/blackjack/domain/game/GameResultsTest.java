4d092f177954b45b66c5757b5a716076a4075157
lemone <lejinhy@gmail.com>
2024-03-07 19:16:46 +0900

    test: 딜러 승패 결과 테스트
    Co-Authored-By: Gyeongho Yang <chchch1213@naver.com>

--- src/test/java/blackjack/domain/game/GameResultsTest.java
+++ src/test/java/blackjack/domain/game/GameResultsTest.java
@@ -24,4 +24,24 @@ public class GameResultsTest {
         // then
         assertThat(gameResults.getPlayersResults()).isEqualTo(expectedGameResults);
     }
+
+    @Test
+    @DisplayName("딜러 승패 결과를 Map으로 반환한다.")
+    void createDealerResulTest() {
+        // given
+        GameResults gameResults = new GameResults();
+        Map<GameResult, Integer> expectedResult = new HashMap<>();
+        expectedResult.put(GameResult.WIN, 1);
+        expectedResult.put(GameResult.LOSE, 2);
+        expectedResult.put(GameResult.DRAW, 1);
+
+        // when
+        gameResults.put("lemone", GameResult.WIN);
+        gameResults.put("seyang", GameResult.LOSE);
+        gameResults.put("pobi", GameResult.DRAW);
+        gameResults.put("club", GameResult.WIN);
+
+        // then
+        assertThat(gameResults.getDealerResult()).isEqualTo(expectedResult);
+    }
 }
