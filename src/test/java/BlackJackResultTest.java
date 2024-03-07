157dac9d0c4aca1e7a31e507e6809ad4e4d553da
unifolio0 <boho82ha@gmail.com>
2024-03-07 15:29:24 +0900

    test : BlackJackResult 테스트 코드 작성

---
+++ src/test/java/BlackJackResultTest.java
@@ -0,0 +1,35 @@
+import domain.BlackJackResult;
+import domain.Name;
+import domain.Participant;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BlackJackResultTest {
+    @DisplayName("딜러가 이긴 횟수 반환")
+    @Test
+    void getDealerWinCount() {
+        Map<Participant, Boolean> result = new HashMap<>();
+        result.put(new Participant(new Name("Dgs")), true);
+        result.put(new Participant(new Name("dsf")), true);
+        result.put(new Participant(new Name("dewf")), false);
+        BlackJackResult blackJackResult = new BlackJackResult(result);
+
+        Assertions.assertThat(blackJackResult.getDealerWinCount()).isEqualTo(1);
+    }
+
+    @DisplayName("승패의 총 합계 반환")
+    @Test
+    void getTotalCount() {
+        Map<Participant, Boolean> result = new HashMap<>();
+        result.put(new Participant(new Name("Dgs")), true);
+        result.put(new Participant(new Name("dsf")), true);
+        result.put(new Participant(new Name("dewf")), false);
+        BlackJackResult blackJackResult = new BlackJackResult(result);
+
+        Assertions.assertThat(blackJackResult.getTotalCount()).isEqualTo(3);
+    }
+}
