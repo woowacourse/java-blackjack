8aed4bf57361cefccdef67498417d4c7bd8fa7e0
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-07 12:04:36 +0900

    feat(DeckGenerator): 도메인 구현 및 테스트 코드 작성

---
+++ src/main/java/blackjack/model/DeckGenerator.java
@@ -0,0 +1,35 @@
+package blackjack.model;
+
+import blackjack.model.card.Card;
+import blackjack.model.card.CardNumber;
+import blackjack.model.card.CardPattern;
+import blackjack.model.card.CardProperties;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DeckGenerator {
+
+    private static final List<Card> deck;
+
+    static {
+        deck = createDeck();
+    }
+
+    public static List<Card> generateDeck() {
+        return new ArrayList<>(deck);
+    }
+
+    private static List<Card> createDeck() {
+        return Arrays.stream(CardPattern.values())
+                .flatMap(DeckGenerator::createCard)
+                .collect(Collectors.toList());
+    }
+
+    private static Stream<Card> createCard(CardPattern cardPattern) {
+        return Arrays.stream(CardNumber.values())
+                .map(cardNumber -> new Card(new CardProperties(cardPattern, cardNumber)));
+    }
+}
---
+++ src/test/java/blackjack/model/DeckGeneratorTest.java
@@ -0,0 +1,26 @@
+package blackjack.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.model.card.Card;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DeckGeneratorTest {
+
+    @DisplayName("패턴과 숫자가 모두 다른 52장의 카드를 만든다.")
+    @Test
+    void generateDeck() {
+        //given
+        List<Card> deck = DeckGenerator.generateDeck();
+
+        //when
+        Set<Card> deckToValidate = new HashSet<>(deck);
+
+        //then
+        assertThat(deckToValidate).hasSize(52);
+    }
+}
