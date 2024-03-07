c920fb7fdf5d473998d8b914c258203ddb23ec5a
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-07 16:08:46 +0900

    refactor(CardPattern): 이름 추가

--- src/main/java/blackjack/model/card/CardPattern.java
+++ src/main/java/blackjack/model/card/CardPattern.java
@@ -2,11 +2,18 @@ package blackjack.model.card;
 
 public enum CardPattern {
 
-    SPADE,
-    HEART,
-    DIAMOND,
-    CLOVER;
+    SPADE("스페이드"),
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    CLOVER("클로버");
 
-    CardPattern() {
+    private final String name;
+
+    CardPattern(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
