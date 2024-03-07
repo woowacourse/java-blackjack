23a4321ea215b60f169372639f96527c6764e069
이상진 <priceelees@gmail.com>
2024-03-08 02:42:49 +0900

    feat(InputView): 입력을 받아들이는 InputView 클래스 생성
    Co-authored-by: seokmyungham <gkatjraud1@gmail.com>

---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,24 @@
+package blackjack.view;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+	private final Scanner scanner;
+
+	public InputView() {
+		scanner = new Scanner(System.in);
+	}
+
+	public List<String> receivePlayerNames() {
+		System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+		String input = scanner.nextLine();
+		return List.of(input.split(","));
+	}
+
+	public String receiveCommand(String name) {
+		System.out.println(name + "는(은) 한장의 카드를 더 받겠습니까?(예는 y, 아니오: n)");
+		return scanner.nextLine();
+	}
+}
