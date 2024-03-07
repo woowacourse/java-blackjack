16b0642535737473d32b976ed9f63a9e3188cd73
Zeus6768 <akaps6768@gmail.com>
2024-03-07 17:42:52 +0900

    feat(dto): 생성자 추가

--- src/main/java/view/dto/DealerDto.java
+++ src/main/java/view/dto/DealerDto.java
@@ -2,12 +2,17 @@ package view.dto;
 
 import java.util.List;
 
+import domain.Card;
 import domain.Cards;
 import domain.Dealer;
 
 public class DealerDto extends ParticipantDto {
 
     public DealerDto(final Dealer dealer) {
-        super(dealer.name(), new Cards(List.of(dealer.peek())));
+        super(dealer.name(), dealer.cards());
+    }
+
+    public DealerDto(final Dealer dealer, final Card card) {
+        super(dealer.name(), new Cards(List.of(card)));
     }
 }
