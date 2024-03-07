e3e34dc8c3ce8d546a5c06e79f1dfa925dbc98b9
HoeSeong123 <ghltjd3@gmail.com>
2024-03-07 17:48:50 +0900

    refactor(Players) : 딜러와 게임 플레이어 분리

--- src/main/java/blackjack/domain/player/Players.java
+++ src/main/java/blackjack/domain/player/Players.java
@@ -3,36 +3,19 @@ package blackjack.domain.player;
 import java.util.List;
 
 public class Players {
-    private static final int DEALER_SIZE = 1;
-    private final List<Player> players;
+    private final Dealer dealer;
+    private final List<GamePlayer> gamePlayers;
 
-    public Players(List<Player> players) {
-        validateDealer(players);
-        this.players = players;
-    }
-
-    private void validateDealer(List<Player> players) {
-        long count = players.stream()
-                            .filter(player -> player instanceof Dealer)
-                            .count();
-
-        if (count != DEALER_SIZE) {
-            throw new IllegalStateException(String.format("딜러가 %d명이 아닙니다.", DEALER_SIZE));
-        }
+    public Players(Dealer dealer, List<GamePlayer> gamePlayers) {
+        this.dealer = dealer;
+        this.gamePlayers = gamePlayers;
     }
 
     public Dealer getDealer() {
-        return players.stream()
-                      .filter(player -> player instanceof Dealer)
-                      .findAny()
-                      .map(Dealer.class::cast)
-                      .get();
+        return dealer;
     }
 
     public List<GamePlayer> getGamePlayers() {
-        return players.stream()
-                      .filter(player -> player instanceof GamePlayer)
-                      .map(GamePlayer.class::cast)
-                      .toList();
+        return gamePlayers;
     }
 }
--- src/test/java/blackjack/PlayersTest.java
+++ src/test/java/blackjack/PlayersTest.java
@@ -20,45 +20,7 @@ class PlayersTest {
         var dealer = PlayerFixture.딜러_생성(new Name("딜러"));
 
         assertThatCode(() -> {
-            new Players(List.of(player1, player2, dealer));
+            new Players(dealer, List.of(player1, player2));
         }).doesNotThrowAnyException();
-    }
-
-    @Test
-    @DisplayName("플레이어 목록에는 한 명의 딜러만 가능하다.")
-    public void Players_Instance_contain_one_dealer() {
-        var player = PlayerFixture.게임_플레이어_생성(new Name("초롱"));
-        var dealer1 = PlayerFixture.딜러_생성(new Name("딜러"));
-        var dealer2 = PlayerFixture.딜러_생성(new Name("보조딜러"));
-
-        assertThrows(IllegalStateException.class, () -> {
-            new Players(List.of(player, dealer1, dealer2));
-        });
-    }
-
-    @Test
-    @DisplayName("플레이어 목록에서 딜러를 가져온다")
-    public void Players_Get_dealer() {
-        var player1 = PlayerFixture.게임_플레이어_생성(new Name("초롱"));
-        var player2 = PlayerFixture.게임_플레이어_생성(new Name("조이썬"));
-        var dealer = PlayerFixture.딜러_생성(new Name("딜러"));
-        var sut = new Players(List.of(player1, player2, dealer));
-
-        var result = sut.getDealer();
-
-        assertThat(result).isEqualTo(dealer);
-    }
-
-    @Test
-    @DisplayName("플레이어 목록에서 게임 플레이어 리스트를 가져온다")
-    public void Players_Get_gamePlayers() {
-        var player1 = PlayerFixture.게임_플레이어_생성(new Name("초롱"));
-        var player2 = PlayerFixture.게임_플레이어_생성(new Name("조이썬"));
-        var dealer = PlayerFixture.딜러_생성(new Name("딜러"));
-        var sut = new Players(List.of(player1, player2, dealer));
-
-        var result = sut.getGamePlayers();
-
-        assertThat(result).isEqualTo(List.of(player1, player2));
     }
 }
