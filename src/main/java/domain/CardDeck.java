75d216bee31b14b98bd458e861d9ff2b4d7d6075
reddevilmidzy <midzy0228@gmail.com>
2024-03-07 17:23:53 +0900

    refactor: CardDeck의 generate메서드 스트림 활용

--- src/main/java/domain/CardDeck.java
+++ src/main/java/domain/CardDeck.java
@@ -3,9 +3,9 @@ package domain;
 import domain.card.Card;
 import domain.card.Rank;
 import domain.card.Shape;
-
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
@@ -19,27 +19,29 @@ public class CardDeck {
         this.cards = cards;
     }
 
+    //TODO 모르는 사람이 봐도 이해할 수 있도록 수정해보기
+    // DECK_SIZE 파라미터로 받아오는 거 고려
     public static CardDeck generate() {
-        final List<Card> result = new ArrayList<>();
+        final List<Card> deck = new ArrayList<>();
 
         for (int i = 0; i < DECK_SIZE; i++) {
-            result.addAll(generateOneCardDeck());
+            deck.addAll(generateOneCardDeck());
         }
 
-        Collections.shuffle(result);
-        return new CardDeck(new ArrayDeque<>(result));
+        Collections.shuffle(deck);
+        return new CardDeck(new ArrayDeque<>(deck));
     }
 
-    private static List<Card> generateOneCardDeck() { //TODO 인덴트 줄이기
-        final List<Card> cards = new ArrayList<>();
+    private static List<Card> generateOneCardDeck() {
+        return Arrays.stream(Shape.values())
+                .flatMap(shape -> matching(shape).stream())
+                .toList();
+    }
 
-        for (Shape shape : Shape.values()) {
-            for (Rank rank : Rank.values()) {
-                cards.add(new Card(rank, shape));
-            }
-        }
-
-        return cards;
+    private static List<Card> matching(final Shape shape) {
+        return Arrays.stream(Rank.values())
+                .map(rank -> new Card(rank, shape))
+                .toList();
     }
 
     public Card pop() {
