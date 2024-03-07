2caf6775b1b4faa1621511d96518b43cc229edeb
le2sky <leehaneul990623@gmail.com>
2024-03-07 13:22:48 +0900

    feat: 참여자들 생성 기능 구현

---
+++ src/main/java/blackjack/domain/Participants.java
@@ -0,0 +1,47 @@
+package blackjack.domain;
+
+import java.util.List;
+
+public class Participants {
+
+    private final Dealer dealer = new Dealer();
+    private final List<Player> players;
+
+    public Participants(List<String> playerNames) {
+        validate(playerNames);
+
+        this.players = playerNames.stream()
+                .map(Player::new)
+                .toList();
+    }
+
+    private void validate(List<String> playerNames) {
+        validatePlayerSize(playerNames);
+        validateDuplicatedPlayerNames(playerNames);
+        validateDuplicatedDealerName(playerNames, dealer.getName());
+    }
+
+    private void validatePlayerSize(List<String> playerNames) {
+        if (playerNames.isEmpty()) {
+            throw new IllegalArgumentException("플레이어가 존재하지 않습니다.");
+        }
+    }
+
+    private void validateDuplicatedPlayerNames(List<String> playerNames) {
+        if (getUniqueSize(playerNames) != playerNames.size()) {
+            throw new IllegalArgumentException("플레이어 이름이 중복되었습니다.");
+        }
+    }
+
+    private long getUniqueSize(List<String> playerNames) {
+        return playerNames.stream()
+                .distinct()
+                .count();
+    }
+
+    private void validateDuplicatedDealerName(List<String> playerNames, String dealerName) {
+        if (playerNames.contains(dealerName)) {
+            throw new IllegalArgumentException(String.format("플레이어 이름은 '%s'가 될 수 없습니다.", dealerName));
+        }
+    }
+}
