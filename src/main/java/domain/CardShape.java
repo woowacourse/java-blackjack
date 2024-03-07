673e87bd26ce3a7edcb477fbe8365ed84959fa53
Zeus6768 <akaps6768@gmail.com>
2024-03-07 16:33:45 +0900

    refactor(CardShape): 카드 문양의 한글 이름 추가

--- src/main/java/domain/CardShape.java
+++ src/main/java/domain/CardShape.java
@@ -1,5 +1,15 @@
 package domain;
 
 public enum CardShape {
-    SPADE, HEART, CLOVER, DIAMOND;
+    SPADE("스페이드"), HEART("하트"), CLOVER("클로버"), DIAMOND("다이아몬드");
+
+    private final String value;
+
+    CardShape(final String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
 }
