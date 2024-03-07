2066ef3aa62730521cae07e68b9450d7d972977d
ay-eonii <naayen@naver.com>
2024-03-07 16:38:44 +0900

    feat(OutputView) : 카드와 결과값 출력
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -18,22 +18,41 @@ public class OutputView {
         System.out.println("딜러와 " + joinNames + "에게 2장을 나누었습니다.");
 
         String firstCard = dealerCards.getCards().get(0);
-        System.out.println("딜러: " + firstCard);
+        System.out.print("딜러: " + firstCard);
 
         for (PlayerCards playerCard : playerCards) {
-            String joinCards = formatCards(playerCard);
-            System.out.println(playerCard.getPlayerName() + "카드: " + joinCards);
+            System.out.println();
+            printPlayerCards(playerCard);
         }
     }
 
     public void printPlayerCards(PlayerCards cards) {
         String playerName = cards.getPlayerName();
         System.out.print(playerName + "카드: ");
-        System.out.println(formatCards(cards));
+        System.out.print(formatCards(cards));
     }
 
     private String formatCards(Cards playerCard) {
         List<String> cards = playerCard.getCards();
         return String.join(", ", cards);
+    }
+
+    public void printResults(DealerCards dealerCards, List<PlayerCards> playerCards) {
+        System.out.println();
+        printDealerCards(dealerCards);
+        printSumOfCards(dealerCards);
+        for (PlayerCards playerCard : playerCards) {
+            printPlayerCards(playerCard);
+            printSumOfCards(playerCard);
+        }
+    }
+
+    private void printDealerCards(DealerCards cards) {
+        System.out.print("딜러 카드: ");
+        System.out.print(formatCards(cards));
+    }
+
+    private void printSumOfCards(Cards cards) {
+        System.out.println(" - 결과: " + cards.sum());
     }
 }
