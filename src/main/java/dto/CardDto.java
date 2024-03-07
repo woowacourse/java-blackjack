98f82cde7c6a113c8b925596df482ae9418a0fb6
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-03-07 14:45:11 +0900

    feat: CardDto 생성

---
+++ src/main/java/dto/CardDto.java
@@ -0,0 +1,12 @@
+package dto;
+
+import domain.Card;
+
+public record CardDto (String cardName){
+
+    public static CardDto from(Card card) {
+        String rankName = RankDto.from(card.getRank()).getRankName();
+        String shapeName = ShapeDto.from(card.getShape()).getShapeName();
+        return new CardDto(rankName + shapeName);
+    }
+}
