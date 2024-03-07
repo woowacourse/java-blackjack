2756faa07612b144588dc18d6b4dab7cf71e2e05
tackyu <tack970913@gmail.com>
2024-03-07 16:21:46 +0900

    feat: 재입력을 받는 클래스 생성

---
+++ src/main/java/domain/ExceptionHandler.java
@@ -0,0 +1,14 @@
+package domain;
+
+import java.util.function.Supplier;
+
+public class ExceptionHandler {
+    public static <T> T handle(Supplier<T> callback) {
+        try {
+            return callback.get();
+        } catch (Exception e) {
+            System.out.printf("%n[ERROR] %s%n", e.getMessage());
+            return handle(callback);
+        }
+    }
+}
