e6ec72e43222358599d5f89324118e5c94a1ee7d
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-07 12:04:49 +0900

    feat(PlayingDeck): 도메인 구현 및 테스트 코드 작성

---
+++ src/main/java/blackjack/model/PlayingDeck.java
@@ -0,0 +1,28 @@
+package blackjack.model;
+
+import blackjack.model.card.Card;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class PlayingDeck {
+
+    private final Queue<Card> playingDeck;
+
+    public PlayingDeck(List<Card> playingDeck) {
+        Collections.shuffle(playingDeck);
+        this.playingDeck = new LinkedList<>(playingDeck);
+    }
+
+    public Card drawCard() {
+        validatePlayingDeckEmpty();
+        return playingDeck.poll();
+    }
+
+    private void validatePlayingDeckEmpty() {
+        if (playingDeck.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 카드를 모두 사용했습니다.");
+        }
+    }
+}
