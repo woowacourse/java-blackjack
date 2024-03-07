015e7f9345d2c3f11a4b004b30771ae5519224ca
masonkimseoul <masonkimseoul@gmail.com>
2024-03-07 21:47:14 +0900

    refactor: 에러 메세지 수정

--- src/main/java/blackjack/exception/NonAlphabeticNameException.java
+++ src/main/java/blackjack/exception/NonAlphabeticNameException.java
@@ -1,7 +1,7 @@
 package blackjack.exception;
 
 public class NonAlphabeticNameException extends IllegalArgumentException {
-    private final static String MESSAGE = "이름에 알파벳이 아닌 문자가 들어있습니다.";
+    private final static String MESSAGE = "이름에 영문, 한글이 아닌 문자가 들어있습니다.";
 
     public NonAlphabeticNameException() {
         super(MESSAGE);
