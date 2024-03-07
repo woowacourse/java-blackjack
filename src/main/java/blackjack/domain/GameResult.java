2528360ae69c4ab46a1b3a292eec6e76f6bbfab7
lemone <lejinhy@gmail.com>
2024-03-07 10:29:44 +0900

    feat: 게임 결과와 반대의 결과를 반환하는 메소드 추가
    Co-Authored-By: Gyeongho Yang <chchch1213@naver.com>

--- src/main/java/blackjack/domain/GameResult.java
+++ src/main/java/blackjack/domain/GameResult.java
@@ -1,7 +1,27 @@
 package blackjack.domain;
 
 public enum GameResult {
-    WIN,
-    LOSE,
-    DRAW;
+    WIN("승"),
+    LOSE("패"),
+    DRAW("무");
+
+    private final String value;
+
+    GameResult(String value) {
+        this.value = value;
+    }
+
+    public String get() {
+        return value;
+    }
+
+    public GameResult reverse() {
+        if (this == WIN) {
+            return LOSE;
+        }
+        if (this == LOSE) {
+            return WIN;
+        }
+        return this;
+    }
 }
