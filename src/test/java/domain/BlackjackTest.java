0a40c6f2cbd8af1d03416c978df6230487f2e858
dwax1324 <dwax1324@gmail.com>
2024-03-07 11:38:59 +0900

    test: 게임의 결과가 제대로 계산됐는지 확인

--- src/test/java/domain/BlackjackTest.java
+++ src/test/java/domain/BlackjackTest.java
@@ -1,6 +1,10 @@
 package domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -32,5 +36,24 @@ class BlackjackTest {
         blackjack.dealCardsToPlayer(player);
 
         assertThat(player.getCards().size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("게임의 결과가 제대로 계산됐는지 확인한다")
+    void gameResultTest() {
+        Player dealer = new Player("딜러");
+        Player teba = new Player("테바");
+        Player jonge = new Player("종이");
+        dealer.addCard(Card.makeRandomCard(new RandomNumberGeneartor(0, 1)));
+        teba.addCard(Card.makeRandomCard(new RandomNumberGeneartor(1, 2)));
+        jonge.addCard(Card.makeRandomCard(new RandomNumberGeneartor(1, 2)));
+        Blackjack blackjack = new Blackjack(new Players(List.of(teba, jonge)), dealer, new Deck());
+
+        Map<Player, Entry<Integer, Integer>> playerEntryMap = blackjack.finishGame();
+
+        Integer dealerLose = playerEntryMap.get(dealer).getValue();
+        Integer tebaWin = playerEntryMap.get(teba).getKey();
+        Integer jongeWin = playerEntryMap.get(jonge).getKey();
+        assertThat(tebaWin + jongeWin).isEqualTo(dealerLose);
     }
 }
