bca4e224e9bfcf5419d0dac3081452f750c577c7
HyungUk Ryu <hw0603@naver.com>
2024-03-07 20:22:02 +0900

    test(scoreTest): 점수에 따른 버스트 여부 테스트 추가
    Co-authored-by: haiseong <wjdgotjd9908@gmail.com>

--- src/test/java/blackjack/domain/ScoreTest.java
+++ src/test/java/blackjack/domain/ScoreTest.java
@@ -1,9 +1,12 @@
 package blackjack.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ScoreTest {
     @Test
@@ -15,4 +18,12 @@ class ScoreTest {
                 .hasMessage("음수는 점수로 사용될 수 없습니다.");
     }
 
+    @ParameterizedTest
+    @CsvSource({"21, false", "22, true"})
+    @DisplayName("점수가 21점을 초과하면 버스트")
+    void burstIfExceed21Test(int value, boolean expected) {
+        Score score = new Score(value);
+        
+        assertThat(score.isBusted()).isEqualTo(expected);
+    }
 }
