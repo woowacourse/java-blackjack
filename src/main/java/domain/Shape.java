dd759efb9467eff5806c8dbbf7134d88e2cfbc39
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-03-07 14:44:56 +0900

    feat: ShapeDto 생성

--- src/main/java/domain/Shape.java
+++ src/main/java/domain/Shape.java
@@ -1,15 +1,5 @@
 package domain;
 
 public enum Shape {
-    HEART("하트"), DIAMOND("다이아몬드"), CLOVER("클로버"), SPADE("스페이드");
-
-    private final String shapeName;
-
-    Shape(String shapeName) {
-        this.shapeName = shapeName;
-    }
-
-    public String getShapeName() {
-        return shapeName;
-    }
+    HEART, DIAMOND, CLOVER, SPADE;
 }
---
+++ src/main/java/dto/ShapeDto.java
@@ -0,0 +1,22 @@
+package dto;
+
+import domain.Shape;
+
+public enum ShapeDto {
+
+    HEART("하트"), DIAMOND("다이아몬드"), CLOVER("클로버"), SPADE("스페이드");
+
+    private final String shapeName;
+
+    ShapeDto(String shapeName) {
+        this.shapeName = shapeName;
+    }
+
+    public static ShapeDto from(Shape shape) {
+        return valueOf(shape.name());
+    }
+
+    public String getShapeName() {
+        return shapeName;
+    }
+}
