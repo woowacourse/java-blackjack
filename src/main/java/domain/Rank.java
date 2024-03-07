bbb87c9dc45ffca3c99e22d5b0a42a32087538f0
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-07 21:32:13 +0900

    refactor : 속성값 추가

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -4,24 +4,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Rank {
-    SMALL_ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10),
-    BIG_ACE(11);
+    SMALL_ACE("A", 1),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10),
+    BIG_ACE("A", 11);
 
+    private final String name;
     private final int score;
 
-    Rank(final int score) {
+    Rank(String name, final int score) {
+        this.name = name;
         this.score = score;
     }
 
@@ -31,11 +33,15 @@ public enum Rank {
                 .toList();
     }
 
-    public int getScore() {
-        return score;
-    }
-
     public boolean isAce() {
         return name().contains("ACE");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
     }
 }
--- src/main/java/domain/Symbol.java
+++ src/main/java/domain/Symbol.java
@@ -3,15 +3,18 @@ package domain;
 import java.util.Arrays;
 
 public enum Symbol {
-    HEART,
-    CLOVER,
-    SPADE,
-    DIAMOND;
+    HEART("하트"),
+    CLOVER("클로버"),
+    SPADE("스페이드"),
+    DIAMOND("다이아몬드");
 
-    public static Symbol findBy(int index) {
-        return Arrays.stream(values())
-                .filter(symbol -> symbol.ordinal() == index)
-                .findFirst()
-                .orElseThrow();
+    private final String name;
+
+    Symbol(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
