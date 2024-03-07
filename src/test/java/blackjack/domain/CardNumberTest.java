0f130c8c99837798d51e75cdfb1314b0aeb8d9b2
libienz <leekh10195@gmail.com>
2024-03-07 19:39:05 +0900

    test: 카드 숫자가 에이스에 해당하는 숫자인지 검증하는 기능 테스트 추가

---
+++ src/test/java/blackjack/domain/CardNumberTest.java
@@ -0,0 +1,20 @@
+package blackjack.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("카드 숫자 enum 테스트")
+class CardNumberTest {
+
+    @DisplayName("Ace에 해당하는 지 확인할 수 있다")
+    @Test
+    void testIsCardNumberAce() {
+        assertAll(
+                () -> assertThat(CardNumber.ACE.isAce()).isTrue(),
+                () -> assertThat(CardNumber.TWO.isAce()).isFalse()
+        );
+    }
+}
