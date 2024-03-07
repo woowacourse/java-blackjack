c37d63579e1675a613618b19b4648d37be591a8c
HoeSeong123 <ghltjd3@gmail.com>
2024-03-07 13:51:32 +0900

    feat(Deck) : 덱 도매인 생성 기능 구현
    - 카드팩을 하나 생성하는 createPack
    - 카드를 위에서부터 하나씩 뽑는 darw
    - 이에 따른 테스트 코드 추가

---
+++ src/main/java/blackjack/Deck.java
@@ -0,0 +1,39 @@
+package blackjack;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Deck {
+    private final ArrayDeque<Card> deck;
+
+    public Deck(ArrayDeque<Card> cards) {
+        this.deck = cards;
+    }
+
+    public static Deck createPack() {
+        List<Card> cards = createCards();
+        Collections.shuffle(cards);
+        return new Deck(new ArrayDeque<>(cards));
+    }
+
+    private static List<Card> createCards() {
+        return Arrays.stream(CardSymbol.values())
+                     .flatMap(Deck::addCard)
+                     .toList();
+    }
+
+    private static Stream<Card> addCard(final CardSymbol cardSymbol) {
+        return EnumSet.allOf(CardValue.class)
+                      .stream()
+                      .map(cardNumber -> new Card(cardNumber, cardSymbol));
+    }
+
+    public Card draw() {
+        return deck.pollLast();
+    }
+
+}
---
+++ src/test/java/blackjack/DeckTest.java
@@ -0,0 +1,45 @@
+package blackjack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DeckTest {
+    @Test
+    @DisplayName("52개의 카드를 가진 덱을 생성 한다.")
+    public void Deck_Instance_create_one_pack() {
+
+        assertThatCode(() -> {
+            var sut = Deck.createPack();
+            var size = IntStream.range(0, 52)
+                                .mapToObj(i -> {
+                                    return sut.draw();
+                                })
+                                .toList()
+                                .size();
+            assertThat(size).isEqualTo(52);
+        });
+    }
+
+    @Test
+    @DisplayName("카드를 위에서 부터 하나씩 뽑는다.")
+    public void Deck_Draw_one_card() {
+        ArrayDeque<Card> cards = new ArrayDeque<>();
+        Card card1 = new Card(CardValue.ACE, CardSymbol.DIAMOND);
+        Card card2 = new Card(CardValue.FOUR, CardSymbol.HEART);
+        cards.add(card1);
+        cards.add(card2);
+
+        var sut = new Deck(cards);
+
+        assertThat(sut.draw()).isEqualTo(card2);
+        assertThat(sut.draw()).isEqualTo(card1);
+    }
+}
