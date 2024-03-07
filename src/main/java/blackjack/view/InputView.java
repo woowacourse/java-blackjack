2d769bbbb33c5e90831d8a04d4e997758bacf58a
HoeSeong123 <ghltjd3@gmail.com>
2024-03-07 14:48:25 +0900

    feat(InputView) : 플레이어 이름 입력받는 기능 구현
    - 쉼표를 기준으로 분리

---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,23 @@
+package blackjack.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private static final String SPLIT_DELIMITER = ",";
+
+    private static String input() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
+
+    public static List<String> inputPlayerNames() {
+        System.out.println(
+                String.format("게임에 참여할 사람의 이름을 입력하세요.(구분자(%s) 기준으로 분리)", SPLIT_DELIMITER));
+        String initialInput = input();
+        return Arrays.stream(initialInput.split(SPLIT_DELIMITER))
+                     .map(String::trim)
+                     .toList();
+    }
+}
