5729a1d47c2070bb8ad07a9f3c46b91577899306
hyxrxn <gpfls981220@gmail.com>
2024-03-07 20:03:13 +0900

    style: 불필요한 개행 삭제
    - 컨벤션에 맞게 import 순서 변경
    
    Co-authored-by: donghoony <aru0504@naver.com>

--- src/main/java/blackjack/BlackJackMain.java
+++ src/main/java/blackjack/BlackJackMain.java
@@ -7,7 +7,6 @@ import blackjack.view.OutputView;
 public class BlackJackMain {
 
     public static void main(String[] args) {
-
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         BlackJackGame blackJackGame = new BlackJackGame(inputView, outputView);
