8969a9ac19a9eb08f0e310d5e56e9699df1c6518
hyxrxn <gpfls981220@gmail.com>
2024-03-07 20:37:18 +0900

    test(MatchResult): 승리 조건 테스트 데이터 보강
    Co-authored-by: donghoony <aru0504@naver.com>

--- src/test/java/blackjack/game/MatchResultTest.java
+++ src/test/java/blackjack/game/MatchResultTest.java
@@ -1,43 +1,40 @@
 package blackjack.game;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class MatchResultTest {
 
-    @Test
-    @DisplayName("플레이어의 점수가 21점을 초과하면 딜러가 승리한다.")
-    void playerBurstTest() {
+    @ParameterizedTest
+    @CsvSource(value = {"20, 22", "20, 19", "21, 20"})
+    @DisplayName("플레이어가 이기는 경우를 올바르게 판단한다.")
+    void playerWinningTest(int playerScore, int dealerScore) {
         // when
-        MatchResult result = MatchResult.chooseWinner(22, 22);
-        // then
-        assertThat(result).isEqualTo(MatchResult.DEALER_WIN);
-    }
-
-    @Test
-    @DisplayName("플레이어의 점수가 21점 이하이고, 딜러의 점수가 21점을 초과하면 플레이어가 승리한다.")
-    void dealerBurstTest() {
-        // when
-        MatchResult result = MatchResult.chooseWinner(20, 22);
+        MatchResult result = MatchResult.chooseWinner(playerScore, dealerScore);
         // then
         assertThat(result).isEqualTo(MatchResult.PLAYER_WIN);
     }
 
-    @Test
-    @DisplayName("양 쪽 모두 21점 이하일 때, 점수가 높은 사람이 승리한다.")
-    void comparingScoreTest() {
+    @ParameterizedTest
+    @CsvSource(value = {"22, 20", "22, 22", "20, 21", "14, 18"})
+    @DisplayName("딜러가 이기는 경우를 올바르게 판단한다.")
+    void dealerWinningTest(int playerScore, int dealerScore) {
         // when
-        MatchResult expectPlayerWin = MatchResult.chooseWinner(21, 20);
-        MatchResult expectTie = MatchResult.chooseWinner(20, 20);
-        MatchResult expectDealerWin = MatchResult.chooseWinner(20, 21);
+        MatchResult result = MatchResult.chooseWinner(playerScore, dealerScore);
         // then
-        assertAll(
-                () -> assertThat(expectPlayerWin).isEqualTo(MatchResult.PLAYER_WIN),
-                () -> assertThat(expectTie).isEqualTo(MatchResult.TIE),
-                () -> assertThat(expectDealerWin).isEqualTo(MatchResult.DEALER_WIN)
-        );
+        assertThat(result).isEqualTo(MatchResult.DEALER_WIN);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"21, 21", "20, 20"})
+    @DisplayName("무승부인 경를 올바르게 판단한다.")
+    void tieTest(int playerScore, int dealerScore) {
+        // when
+        MatchResult result = MatchResult.chooseWinner(playerScore, dealerScore);
+        // then
+        assertThat(result).isEqualTo(MatchResult.TIE);
     }
 }
