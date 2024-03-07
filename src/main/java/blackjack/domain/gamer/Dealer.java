9fd5c3d1fe1707aafad9d802a4c2cb04e6121740
이상진 <priceelees@gmail.com>
2024-03-08 02:25:51 +0900

    style(Dealer): 상수 추가 및 dto 생성 메서드명 수정
    Co-authored-by: seokmyungham <gkatjraud1@gmail.com>

--- src/main/java/blackjack/domain/gamer/Dealer.java
+++ src/main/java/blackjack/domain/gamer/Dealer.java
@@ -8,26 +8,29 @@ import blackjack.dto.DealerResultDto;
 
 public class Dealer extends BlackjackGamer {
 
+	private static final String DEFAULT_DEALER_NAME = "딜러";
+	private static final int DEALER_DRAW_THRESHOLD = 16;
+
 	public Dealer() {
-		super(new Name("딜러"));
+		super(new Name(DEFAULT_DEALER_NAME));
 	}
 
 	@Override
 	public boolean canReceiveCard() {
-		return getScore() <= 16;
+		return getScore() <= DEALER_DRAW_THRESHOLD;
 	}
 
-	public DealerResultDto getResult(List<GameResult> playerResults) {
+	public DealerResultDto convertDealerToResultDto(List<GameResult> playerResults) {
 		Name name = getName();
 		int winCount = (int)playerResults.stream()
-			.filter(result -> result == GameResult.WIN)
+			.filter(GameResult::isLose)
 			.count();
 		int loseCount = playerResults.size() - winCount;
 
 		return new DealerResultDto(name.value(), winCount, loseCount);
 	}
 
-	public DealerInitialHandDto convertDealerToDto() {
+	public DealerInitialHandDto convertDealerToInitialHandDto() {
 		String dealerName = getName().value();
 		Card first = getFirstCard();
 
