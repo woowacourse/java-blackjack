645e6109365f8dfc2c754a93f4b7e79a0bfc5c63
tackyu <tack970913@gmail.com>
2024-03-07 16:22:04 +0900

    test: PlayerResults 테스트 코드 작성

---
+++ src/test/java/domain/PlayerResultsTest.java
@@ -0,0 +1,22 @@
+package domain;
+
+import domain.user.Name;
+import domain.user.Player;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static domain.Result.WIN;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlayerResultsTest {
+
+    @Test
+    void generateDealerResult() {
+        PlayerResults playerResults = new PlayerResults(
+                Map.of(new Player(new Name("a")), WIN));
+
+        DealerResult dealerResult = playerResults.generateDealerResult();
+        assertThat(dealerResult.getInformation()).isEqualTo("1패");
+    }
+}
