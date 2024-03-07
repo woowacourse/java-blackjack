a7211da864e6c7026977b6e95fc416470e0164d4
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-07 16:08:32 +0900

    refactor(CardNumber): 이름 추가

--- src/main/java/blackjack/model/card/CardNumber.java
+++ src/main/java/blackjack/model/card/CardNumber.java
@@ -2,27 +2,33 @@ package blackjack.model.card;
 
 public enum CardNumber {
 
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    KING(10),
-    QUEEN(10),
-    JACK(10),
-    ACE(11);
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    KING(10, "K"),
+    QUEEN(10, "Q"),
+    JACK(10, "J"),
+    ACE(11, "A");
 
     private final int number;
+    private final String name;
 
-    CardNumber(int number) {
+    CardNumber(int number, String name) {
         this.number = number;
+        this.name = name;
     }
 
     public int getNumber() {
         return number;
+    }
+
+    public String getName() {
+        return name;
     }
 }
