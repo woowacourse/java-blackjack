6f88ab4c86f84972df7158216f7fd51150b44b8b
JUHA <84626225+khabh@users.noreply.github.com>
2024-03-07 11:05:06 +0900

    feat: 카드 점수 관련 기능 구현

--- src/main/java/blackjack/domain/card/Hand.java
+++ src/main/java/blackjack/domain/card/Hand.java
@@ -4,6 +4,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class Hand {
+    private static final int BLACKJACK = 21;
+    private static final int ACE_ALTERNATIVE_SCORE = 10;
+
     private final List<Card> cards;
 
     public Hand(List<Card> cards) {
@@ -12,5 +15,40 @@ public class Hand {
 
     public List<Card> getCards() {
         return Collections.unmodifiableList(cards);
+    }
+
+    public boolean isBust() {
+        return isTotalScoreGreaterThan(BLACKJACK);
+    }
+
+    public boolean isBlackjack() {
+        return getOptimizedScore() == BLACKJACK;
+    }
+
+    public int getOptimizedScore() {
+        int cardTotalScore = getCardTotalScore();
+        if (cardTotalScore >= BLACKJACK) {
+            return cardTotalScore;
+        }
+        int aceCountForAlter = countAceForAlter();
+        return aceCountForAlter * ACE_ALTERNATIVE_SCORE + cardTotalScore;
+    }
+
+    public boolean isTotalScoreGreaterThan(int score) {
+        return getCardTotalScore() > score;
+    }
+
+    private int getCardTotalScore() {
+        return cards.stream()
+                .mapToInt(Card::getScore)
+                .sum();
+    }
+
+    private int countAceForAlter() {
+        int aceCount = (int)cards.stream()
+                .filter(Card::isAce)
+                .count();
+        int cardTotalScore = getCardTotalScore();
+        return Math.min(aceCount, (BLACKJACK - cardTotalScore) / ACE_ALTERNATIVE_SCORE);
     }
 }
