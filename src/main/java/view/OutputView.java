09f36573703de809b5f6189cc6c6fbf309e7b043
DobbyMac <kimdobby@g.skku.edu>
2024-03-07 21:03:32 +0900

    feat(OutputView): 게임 초기 카드 분배 결과 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,10 @@
 package view;
 
+import java.util.List;
+import model.dto.IndividualFaceUpResult;
+
 public class OutputView {
+    private static final int HOLE_CARD_INDEX = 0;
 
     private OutputView() {
     }
@@ -13,4 +17,25 @@ public class OutputView {
         System.out.println(toBePrint);
     }
 
+    public static void printInitialCardSetting(IndividualFaceUpResult dealerResult,
+                                               List<IndividualFaceUpResult> playersResult) {
+        List<String> playerNameTexts = playersResult.stream()
+                .map(IndividualFaceUpResult::getPartipantNameAsString)
+                .toList();
+        String playerNameFormattedText = String.join(", ", playerNameTexts);
+
+        print("딜러와 " + playerNameFormattedText + "에게 2장을 나누었습니다.");
+        printDealerHoleCard(dealerResult);
+        printPlayerFaceUp(playersResult);
+    }
+
+    private static void printDealerHoleCard(IndividualFaceUpResult faceUpResult) {
+        System.out.println(faceUpResult.getPartipantNameAsString() + ": " + faceUpResult.getCardsAsStrings()
+                .get(HOLE_CARD_INDEX));
+    }
+
+    private static void printPlayerFaceUp(List<IndividualFaceUpResult> faceUpResults) {
+        faceUpResults.forEach(result -> System.out.println(
+                result.getPartipantNameAsString() + ": " + String.join(", ", result.getCardsAsStrings())));
+    }
 }
