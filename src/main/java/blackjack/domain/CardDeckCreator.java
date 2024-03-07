0e340ad6309192620d0aa6dc7e188b15ac12fc30
libienz <leekh10195@gmail.com>
2024-03-07 17:26:54 +0900

    feat: 정해진 카드 구성으로 덱을 만드는 Creator 구현

---
+++ src/main/java/blackjack/domain/CardDeckCreator.java
@@ -0,0 +1,22 @@
+package blackjack.domain;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CardDeckCreator {
+
+    public CardDeck create() {
+        List<Card> cards = createCards(CardShape.values(), CardNumber.values());
+        Collections.shuffle(cards);
+        return new CardDeck(cards);
+    }
+
+    private List<Card> createCards(CardShape[] cardShapes, CardNumber[] cardNumbers) {
+        return Arrays.stream(cardShapes)
+                .flatMap(shape -> Arrays.stream(cardNumbers)
+                        .map(number -> new Card(shape, number)))
+                .collect(Collectors.toList());
+    }
+}
