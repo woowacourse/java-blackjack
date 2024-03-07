a6a633d0937045d9ba461aa5d07dbb4a4d1dca36
kelly6bf <kelly6bf@gmail.com>
2024-03-07 11:29:30 +0900

    fix: playerNames 중복 검증 로직 정정 및 Record로 구조 변경

--- src/main/java/domain/PlayerNames.java
+++ src/main/java/domain/PlayerNames.java
@@ -6,12 +6,10 @@ import java.util.Set;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
 
-public class PlayerNames {
-    private final List<PlayerName> playerNames;
+public record PlayerNames(List<PlayerName> values) {
 
-    private PlayerNames(List<PlayerName> playerNames) {
-        validateDuplicated(playerNames);
-        this.playerNames = playerNames;
+    public PlayerNames {
+        validateDuplicated(values);
     }
 
     public static PlayerNames of(List<String> playerNames) {
@@ -21,7 +19,8 @@ public class PlayerNames {
     }
 
     private void validateDuplicated(List<PlayerName> playerNames) {
-        if (Set.of(playerNames).size() != playerNames.size()) {
+        Set<PlayerName> playerNamesSet = Set.copyOf(playerNames);
+        if (playerNamesSet.size() != playerNames.size()) {
             throw new IllegalArgumentException("플레이어 이름에 중복이 존재합니다.");
         }
     }
