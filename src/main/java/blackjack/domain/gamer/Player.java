9b0e50df1c229583c40c11f4b53cf5c732d66e54
이상진 <priceelees@gmail.com>
2024-03-07 16:55:32 +0900

    feat(Player): 딜러의 점수를 입력받아 플레이어의 승패 여부를 결정하는 기능 추가
    Co-authored-by: seokmyungham <gkatjraud1@gmail.com>

--- src/main/java/blackjack/domain/gamer/Player.java
+++ src/main/java/blackjack/domain/gamer/Player.java
@@ -12,4 +12,17 @@ public class Player extends BlackJackGamer {
 	public boolean canReceiveCard() {
 		return sumCardNumbers() <= 21;
 	}
+
+	public boolean isWin(int dealerScore) {
+		int playerScore = sumCardNumbers();
+
+		if (playerScore > 21) {
+			return false;
+		}
+		if (dealerScore > 21) {
+			return true;
+		}
+
+		return playerScore > dealerScore;
+	}
 }
--- src/test/java/blackjack/domain/gamer/PlayerTest.java
+++ src/test/java/blackjack/domain/gamer/PlayerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import blackjack.domain.card.Card;
@@ -36,5 +37,63 @@ class PlayerTest {
 		player.addCard(new Card(CardShape.HEART, CardNumber.SEVEN));
 
 		assertThat(player.canReceiveCard()).isFalse();
+	}
+
+	@Nested
+	@DisplayName("플레이어가 승리한다.")
+	class IsWinTest {
+
+		@Test
+		@DisplayName("플레이어 점수가 21 이하이고, 딜러의 점수보다 큰 경우 승리한다.")
+		void bustTest() {
+			player.addCard(new Card(CardShape.CLOVER, CardNumber.KING));
+			player.addCard(new Card(CardShape.HEART, CardNumber.FOUR));
+			player.addCard(new Card(CardShape.HEART, CardNumber.SEVEN));
+
+			assertThat(player.isWin(20)).isTrue();
+		}
+
+		@Test
+		@DisplayName("플레이어 점수가 21 이하이고, 딜러의 점수가 21을 초과하면 승리한다.")
+		void bustTest1() {
+			player.addCard(new Card(CardShape.CLOVER, CardNumber.KING));
+			player.addCard(new Card(CardShape.HEART, CardNumber.FIVE));
+
+			assertThat(player.isWin(22)).isTrue();
+		}
+	}
+
+	@Nested
+	@DisplayName("플레이어가 패배한다.")
+	class LoseTest {
+
+		@Test
+		@DisplayName("점수가 21을 초과하면 딜러의 점수와 무관하게 패배한다")
+		void bustTest2() {
+			player.addCard(new Card(CardShape.CLOVER, CardNumber.KING));
+			player.addCard(new Card(CardShape.HEART, CardNumber.FIVE));
+			player.addCard(new Card(CardShape.HEART, CardNumber.SEVEN));
+
+			assertThat(player.isWin(21)).isFalse();
+			assertThat(player.isWin(22)).isFalse();
+		}
+
+		@Test
+		@DisplayName("플레이어와 딜러의 점수가 모두 21 이하일 때, 딜러의 점수보다 낮으면 패배한다.")
+		void bustTest3() {
+			player.addCard(new Card(CardShape.CLOVER, CardNumber.KING));
+			player.addCard(new Card(CardShape.HEART, CardNumber.SIX));
+
+			assertThat(player.isWin(17)).isFalse();
+		}
+
+		@Test
+		@DisplayName("플레이어와 딜러의 점수가 모두 21 이하이고 동점인 경우 플레이어가 패배한다.")
+		void bustTest4() {
+			player.addCard(new Card(CardShape.CLOVER, CardNumber.KING));
+			player.addCard(new Card(CardShape.HEART, CardNumber.SEVEN));
+
+			assertThat(player.isWin(17)).isFalse();
+		}
 	}
 }
