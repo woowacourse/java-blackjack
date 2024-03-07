215fd5c0e979949e1df5adac23c5aefd2c53fe9d
HyungUk Ryu <hw0603@naver.com>
2024-03-07 19:55:09 +0900

    feat(players): 중복된 플레이어 이름 검사
    Co-authored-by: haiseong <wjdgotjd9908@gmail.com>

---
+++ src/main/java/blackjack/domain/Players.java
@@ -0,0 +1,35 @@
+package blackjack.domain;
+
+import java.util.List;
+
+public class Players {
+    private final List<Player> players;
+
+    public Players(List<Player> players) {
+        validate(players);
+        this.players = players;
+    }
+
+    private static void validate(List<Player> players) {
+        if (duplicatedNameExist(players)) {
+            throw new IllegalArgumentException("중복된 이름이 존재합니다.");
+        }
+    }
+
+    private static boolean duplicatedNameExist(List<Player> players) {
+        int distinctCount = (int) players.stream()
+                .map(Player::getName)
+                .distinct()
+                .count();
+
+        return distinctCount != players.size();
+    }
+
+    public static Players from(List<String> names) {
+        List<Player> players = names.stream()
+                .map(Player::new)
+                .toList();
+
+        return new Players(players);
+    }
+}
