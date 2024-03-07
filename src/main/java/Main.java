3b47a6dd22c79276d58caef9c787f15c56595859
BurningFalls <sherlocky31415@gmail.com>
2024-03-07 10:55:15 +0900

    fix: Main 메서드에서 플레이어 이름을 입력받지 않는 오류 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,17 @@ import domain.Deck;
 import domain.Gamer;
 import domain.HoldingCards;
 import java.util.List;
+import view.NameInputView;
+import view.OutputView;
 
 public class Main {
     public static void main(String[] args) {
         Gamer dealer = new Gamer("딜러", HoldingCards.of());
-        Gamer player = new Gamer("aaa", HoldingCards.of());
-        BlackjackController blackjackController = new BlackjackController(dealer, List.of(player));
+        OutputView.print("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        List<Gamer> players = NameInputView.getNames().stream()
+                .map(name -> new Gamer(name, HoldingCards.of()))
+                .toList();
+        BlackjackController blackjackController = new BlackjackController(dealer, players);
         blackjackController.startBlackjackGame(Deck.fullDeck());
     }
 }
--- src/main/java/view/NameInputView.java
+++ src/main/java/view/NameInputView.java
@@ -1,18 +1,16 @@
 package view;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class NameInputView {
     private static final String SEPARATOR = ",";
 
-    // TODO: 배열 대신 컬렉션 사용하기 (요구사항)
     public static List<String> getNames() {
         String input = Console.getInputFromConsole();
         validateSeparator(input);
-        String[] splitInput = input.split(SEPARATOR);
+        List<String> splitInput = List.of(input.split(SEPARATOR));
         validateBlank(splitInput);
-        return Arrays.stream(splitInput).toList();
+        return splitInput;
     }
 
     private static void validateSeparator(String input) {
@@ -21,8 +19,8 @@ public class NameInputView {
         }
     }
 
-    private static void validateBlank(String[] split) {
-        boolean isInputContainsBlank = Arrays.stream(split).anyMatch(String::isBlank);
+    private static void validateBlank(List<String> split) {
+        boolean isInputContainsBlank = split.stream().anyMatch(String::isBlank);
         if (isInputContainsBlank) {
             throw new IllegalArgumentException("입력 형식이 올바르지 않습니다.");
         }
