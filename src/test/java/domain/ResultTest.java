c0b6a1cabd43f46e92879e10dcf3f50cae02d185
tackyu <tack970913@gmail.com>
2024-03-07 16:39:18 +0900

    test: enum에 관한 테스트 작성

---
+++ src/test/java/domain/ResultTest.java
@@ -0,0 +1,31 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static domain.Result.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ResultTest {
+    @Test
+    @DisplayName("정반대의 결과를 반환한다.")
+    void reverseTest() {
+        List<Result> results = List.of(WIN, DRAW, LOSE);
+        List<Result> reverseResult = results.stream().map(Result::reverse).toList();
+
+        assertThat(reverseResult).isEqualTo(List.of(LOSE, DRAW, WIN));
+    }
+
+    @ParameterizedTest()
+    @CsvSource(value = {"1,LOSE", "2,DRAW", "3,WIN"})
+    @DisplayName("두 결과를 비교하여 승패를 판단한다.")
+    void compareTest(int current, Result result) {
+        int opponentValue = 2;
+
+        assertThat(Result.compare(current, opponentValue)).isEqualTo(result);
+    }
+}
