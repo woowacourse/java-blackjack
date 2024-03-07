82a450d097bb4fac8590df676fa3bba95307c242
lemone <lejinhy@gmail.com>
2024-03-07 19:12:33 +0900

    test: 게임 결과 반대로 반환하는 기능 테스트
    Co-Authored-By: Gyeongho Yang <chchch1213@naver.com>

---
+++ src/test/java/blackjack/domain/game/GameResultTest.java
@@ -0,0 +1,19 @@
+package blackjack.domain.game;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static blackjack.domain.game.GameResult.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameResultTest {
+
+    @Test
+    @DisplayName("게임 결과가 반대로도 출력되는지 확인한다.")
+    void reverseTest() {
+        // given &  when & then
+        assertThat(WIN.reverse()).isEqualTo(LOSE);
+        assertThat(LOSE.reverse()).isEqualTo(WIN);
+        assertThat(DRAW.reverse()).isEqualTo(DRAW);
+    }
+}
