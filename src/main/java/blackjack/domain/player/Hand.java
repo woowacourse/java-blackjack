8ba77c3e6395d14330e1da50e6038cc39541af2d
3juhwan <13selfesteem91@naver.com>
2024-03-08 17:00:21 +0900

    feat: 에이스 관련 처리 기능 추가
    Co-authored-by: MangCho <13selfesteem91@naver.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/blackjack/domain/player/Hand.java
+++ src/main/java/blackjack/domain/player/Hand.java
@@ -9,10 +9,26 @@ import java.util.List;
 public class Hand {
     private final List<Card> cards = new ArrayList<>();
 
+    // TODO: 메서드 분리, 상수 분리 필요
     public int getSum() {
-        return cards.stream()
+        int minimumSum = cards.stream()
                 .mapToInt(Card::getNumber)
                 .sum();
+
+        final int aceCount = getAceCount();
+        for (int i = 0; i < aceCount; i++) {
+            if (minimumSum + 10 <= 21) {
+                minimumSum += 10;
+            }
+        }
+
+        return minimumSum;
+    }
+
+    private int getAceCount() {
+        return (int) cards.stream()
+                .filter(Card::isAceCard)
+                .count();
     }
 
     public void add(final Card card) {
