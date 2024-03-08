32cc01a4fe1ad191c2165de8287a95e73b2062e5
libienz <leekh10195@gmail.com>
2024-03-08 13:09:55 +0900

    feat: CardNumber 열거형 필드 추가

--- src/main/java/blackjack/domain/CardNumber.java
+++ src/main/java/blackjack/domain/CardNumber.java
@@ -2,24 +2,26 @@ package blackjack.domain;
 
 public enum CardNumber {
 
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
+    ACE(1, "A"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    JACK(10, "J"),
+    QUEEN(10, "Q"),
+    KING(10, "K");
 
     private final int value;
+    private final String name;
 
-    CardNumber(int value) {
+    CardNumber(int value, String name) {
         this.value = value;
+        this.name = name;
     }
 
     public boolean isAce() {
@@ -29,4 +31,9 @@ public enum CardNumber {
     public int getValue() {
         return value;
     }
+
+    public String getName() {
+        return name;
+    }
 }
+
