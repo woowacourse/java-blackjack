5f6c28fb7ebb168451b358c981b43aabc308939e
nayonsoso <panda0329@naver.com>
2024-03-08 16:41:51 +0900

    refactor: 사용하지 않는 코드 삭제

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,7 +1,6 @@
 package domain;
 
 import java.util.List;
-import java.util.Objects;
 
 public class Player {
 
@@ -42,18 +41,5 @@ public class Player {
 
     public String getName() {
         return name.name();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Player player = (Player) o;
-        return Objects.equals(name, player.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, hand);
     }
 }
