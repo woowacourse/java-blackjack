9ff0103b35f2d3539d1b538d17a13a0f917a7c95
nayonsoso <panda0329@naver.com>
2024-03-08 13:46:43 +0900

    refactor: 함수 분리

--- src/main/java/controller/BlackJackController.java
+++ src/main/java/controller/BlackJackController.java
@@ -4,7 +4,9 @@ import domain.BlackJackGame;
 import domain.CardDeck;
 import domain.Player;
 import domain.PlayerNames;
+import dto.DealerResultDto;
 import dto.PlayerDto;
+import dto.PlayersResultDto;
 import view.InputView;
 import view.OutputView;
 
@@ -21,39 +23,62 @@ public class BlackJackController {
     }
 
     public void run() throws IOException {
-        List<String> inputNames = inputView.readPlayerNames();
-        PlayerNames playerNames = new PlayerNames(inputNames);
+        PlayerNames playerNames = readPlayerNames();
         CardDeck cardDeck = new CardDeck();
         BlackJackGame blackJackGame = new BlackJackGame(playerNames, cardDeck);
 
-        List<PlayerDto> playerinitDtos = blackJackGame.getEveryParticipants().stream()
-                .map(PlayerDto::from).toList();
-        outputView.printDealingMessage(playerinitDtos);
-        outputView.printInitialHand(playerinitDtos);
-
-        for (Player player : blackJackGame.getPlayers()) {
-            repeatUntilStand(blackJackGame, player);
-        }
-
-        repeatUntilDealerStand(blackJackGame);
-
-        List<PlayerDto> playerDtos = blackJackGame.getEveryParticipants().stream()
-                .map(PlayerDto::from).toList();
-        outputView.printGameScore(playerDtos);
-
-        outputView.printGameResult(blackJackGame.getGameResults());
+        printInitialDealAndHand(blackJackGame);
+        repeatHitUntilStand(blackJackGame);
+        printGameResult(blackJackGame);
     }
 
-    private void repeatUntilStand(BlackJackGame blackJackGame, Player player) throws IOException {
+    private PlayerNames readPlayerNames() throws IOException {
+        List<String> inputNames = inputView.readPlayerNames();
+
+        return new PlayerNames(inputNames);
+    }
+
+    private void repeatHitUntilStand(BlackJackGame blackJackGame) throws IOException {
+        for (Player player : blackJackGame.getPlayers()) {
+            repeatHitUntilPlayerStand(blackJackGame, player);
+        }
+
+        repeatHitUntilDealerStand(blackJackGame);
+    }
+
+    private void repeatHitUntilPlayerStand(BlackJackGame blackJackGame, Player player) throws IOException {
         while (player.isHittable() && inputView.readCommand(player.getName().name()).equals("y")) {
             blackJackGame.hitPlayer(player);
-            outputView.printHansAfterHit(PlayerDto.from(player));
+            outputView.printHandAfterHit(PlayerDto.from(player));
         }
     }
 
-    private void repeatUntilDealerStand(BlackJackGame blackJackGame) {
+    private void repeatHitUntilDealerStand(BlackJackGame blackJackGame) {
         while (blackJackGame.hitDealer()) {
             outputView.printDealerHitMessage();
         }
+    }
+
+    private void printInitialDealAndHand(BlackJackGame blackJackGame) {
+        List<Player> initialParticipants = blackJackGame.getEveryParticipants();
+        List<PlayerDto> playerInitDtos = toPlayerDtos(initialParticipants);
+
+        outputView.printInitialDeal(playerInitDtos);
+        outputView.printInitialHand(playerInitDtos);
+    }
+
+    private void printGameResult(BlackJackGame blackJackGame) {
+        List<PlayerDto> playerDtos = toPlayerDtos(blackJackGame.getEveryParticipants());
+        DealerResultDto dealerResultDto = DealerResultDto.from(blackJackGame.getGameResults());
+        PlayersResultDto playersResultDto = PlayersResultDto.from(blackJackGame.getGameResults());
+
+        outputView.printFinalHandAndScore(playerDtos);
+        outputView.printWinLoss(dealerResultDto, playersResultDto);
+    }
+
+    private List<PlayerDto> toPlayerDtos(List<Player> players) {
+        return players.stream()
+                .map(PlayerDto::from)
+                .toList();
     }
 }
