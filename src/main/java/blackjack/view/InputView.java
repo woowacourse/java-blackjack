c045bfbf191461e9b1c0672d779ec33a388208c4
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-08 11:52:35 +0900

    feat(InputView): view 구현

---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,31 @@
+package blackjack.view;
+
+import blackjack.model.gamer.Player;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final String SEPARATOR = ",";
+
+    public static List<String> readPlayersName() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        String input = SCANNER.nextLine();
+        return Arrays.asList(input.split(SEPARATOR));
+    }
+
+    //TODO : 입력값 예외처리 추가 예정
+    public static boolean askPlayerForCard(Player player) {
+        System.out.printf(NEW_LINE + "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)" + NEW_LINE, player.getName());
+        String input = SCANNER.nextLine();
+
+        if (input.equals("y")) {
+            return true;
+        }
+
+        return false;
+    }
+}
