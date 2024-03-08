9f802ac90d52656d3ab219fedf697d7d4cd28e47
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-08 16:34:13 +0900

    feat : 이름 형식 예외 처리 추가

--- src/main/java/domain/gamer/Name.java
+++ src/main/java/domain/gamer/Name.java
@@ -3,10 +3,21 @@ package domain.gamer;
 import java.util.Objects;
 
 public class Name {
+    private static final int MINIMUM_NAME_LENGTH = 1;
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+    public static final String INVALID_NAME_LENGTH = String.format("이름은 %d글자 이상, %d글자 이하 이어야 합니다.", MINIMUM_NAME_LENGTH,
+            MAXIMUM_NAME_LENGTH);
     private final String value;
 
     public Name(final String value) {
+        validateNameLength(value);
         this.value = value;
+    }
+
+    private void validateNameLength(final String value) {
+        if (value.isBlank() || value.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH);
+        }
     }
 
     public String getValue() {
---
+++ src/test/java/domain/gamer/NameTest.java
@@ -0,0 +1,19 @@
+package domain.gamer;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NameTest {
+
+    @DisplayName("이름의 길이가 1 미만, 5초과면 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "liniri"})
+    void invalidNameLength(String name) {
+        assertThatThrownBy(() -> new Name(name)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Name.INVALID_NAME_LENGTH);
+    }
+
+}
