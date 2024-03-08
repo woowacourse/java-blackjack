c8a57f4bfe5492b68c4c6d8a0fe05ef3f6f7c1c0
HyunJoong Kim <hjk0761@naver.com>
2024-03-08 15:02:09 +0900

    style: InputView 오타 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -13,7 +13,7 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static List<String> inputPlayerNames() {
-        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으롭 분리)");
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         String inputString = scanner.nextLine();
         System.out.println();
         return Arrays.stream(inputString.split(",")).toList();
