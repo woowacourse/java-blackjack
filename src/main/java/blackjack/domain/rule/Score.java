3e4259840d9e8a7de1957575ec1460df1632c532
libienz <leekh10195@gmail.com>
2024-03-08 14:16:09 +0900

    feat: Score 클래스 구현

---
+++ src/main/java/blackjack/domain/rule/Score.java
@@ -0,0 +1,14 @@
+package blackjack.domain.rule;
+
+public class Score {
+
+    private final int value;
+
+    public Score(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
