475e2690d7267211a603a6c11bf34c00b8ae01da
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-08 16:01:27 +0900

    test : @BeforeEach 적용 및 성공 실패 테스트 분리

--- src/test/java/domain/HandTest.java
+++ src/test/java/domain/HandTest.java
@@ -8,22 +8,38 @@ import domain.card.Rank;
 import domain.card.Symbol;
 import domain.gamer.Hand;
 import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class HandTest {
+    Stack<Card> cards;
+    Hand hand;
+
+    @BeforeEach
+    void init() {
+        Card card1 = new Card(Symbol.SPADE, Rank.KING);
+        Card card2 = new Card(Symbol.HEART, Rank.THREE);
+        Card card3 = new Card(Symbol.CLOVER, Rank.NINE);
+
+        cards = Stream.of(card1, card2, card3)
+                .collect(Collectors.toCollection(Stack::new));
+        hand = new Hand();
+    }
 
     @DisplayName("뽑은 카드를 저장한다.")
     @Test
     void addCardTest() {
         // given
-        Hand hand = new Hand();
-        Card expectedCard = new Card(Symbol.HEART, Rank.BIG_ACE);
+        Card expectedCard = new Card(Symbol.CLOVER, Rank.NINE);
 
         // when
-        hand.add(expectedCard);
+        hand.add(cards.pop());
         List<Card> result = hand.getCards();
 
         // then
@@ -37,16 +53,12 @@ public class HandTest {
     @Test
     void sumScoreTest() {
         // given
-        Hand hand = new Hand();
-        Card card1 = new Card(Symbol.SPADE, Rank.BIG_ACE);
-        Card card2 = new Card(Symbol.HEART, Rank.TWO);
-        Card card3 = new Card(Symbol.CLOVER, Rank.NINE);
+        Card aceCard = new Card(Symbol.CLOVER, Rank.BIG_ACE);
+        hand.add(aceCard);
+        hand.add(cards.pop());
+        hand.add(cards.pop());
 
-        hand.add(card1);
-        hand.add(card2);
-        hand.add(card3);
-
-        int expectedScore = 12;
+        int expectedScore = 13;
 
         // when
         int result = hand.sum();
@@ -55,24 +67,32 @@ public class HandTest {
         assertThat(result).isEqualTo(expectedScore);
     }
 
-    @DisplayName("버스트인지 확인한다.")
-    @ParameterizedTest
-    @CsvSource(value = {"THREE,true", "TWO,false"})
-    void isBurstTest(Rank rank, boolean expected) {
+    @DisplayName("점수 합계가 21이하면 버스트가 아니다.")
+    @Test
+    void isNotBustTest() {
         // given
-        Hand hand = new Hand();
-        Card card1 = new Card(Symbol.HEART, rank);
-        Card card2 = new Card(Symbol.CLOVER, Rank.NINE);
-        Card card3 = new Card(Symbol.SPADE, Rank.QUEEN);
-
-        hand.add(card1);
-        hand.add(card2);
-        hand.add(card3);
+        hand.add(cards.pop());
+        hand.add(cards.pop());
 
         // when
         boolean result = hand.isOverBlackJack();
 
         // then
-        assertThat(result).isEqualTo(expected);
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("점수 합계가 21이상이면 버스트다.")
+    @Test
+    void isBustTest() {
+        // given
+        hand.add(cards.pop());
+        hand.add(cards.pop());
+        hand.add(cards.pop());
+
+        // when
+        boolean result = hand.isOverBlackJack();
+
+        // then
+        assertThat(result).isTrue();
     }
 }
