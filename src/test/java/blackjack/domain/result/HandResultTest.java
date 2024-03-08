1769d406261ae83a2a798dc54c60943b4d061472
masonkimseoul <masonkimseoul@gmail.com>
2024-03-08 15:13:06 +0900

    Test: HandResult의 getOpposite 기능 테스트

---
+++ src/test/java/blackjack/domain/result/HandResultTest.java
@@ -0,0 +1,26 @@
+package blackjack.domain.result;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class HandResultTest {
+    @DisplayName("입력된 결과와 반대되는 결과를 반환한다.")
+    @ParameterizedTest
+    @MethodSource("provideHandResultWithOppositeResult")
+    void getOppositeTest(HandResult handResult, HandResult oppositeResult) {
+        assertThat(handResult.getOpposite()).isEqualTo(oppositeResult);
+    }
+
+    private static Stream<Arguments> provideHandResultWithOppositeResult() {
+        return Stream.of(
+                Arguments.of(HandResult.WIN, HandResult.LOSE),
+                Arguments.of(HandResult.DRAW, HandResult.DRAW),
+                Arguments.of(HandResult.LOSE, HandResult.WIN)
+        );
+    }
+}
