d2b2b584dee1234a8f2bef38a862f6e9fd9aa814
DobbyMac <kimdobby@g.skku.edu>
2024-03-08 14:43:46 +0900

    feat(Application): main 함수 실행 Application 클래스 작성

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import controller.Controller;
+import view.OutputView;
+
+public class Application {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.execute();
+    }
+}
