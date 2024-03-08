05fde2d345bb0fd32cb293737d1dab86c3a066cb
ay-eonii <naayen@naver.com>
2024-03-08 17:59:07 +0900

    refactor(Rule) : ScoreBoard Rule에서 관리
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

--- src/main/java/domain/game/Rule.java
+++ src/main/java/domain/game/Rule.java
@@ -1,11 +1,22 @@
 package domain.game;
 
 import domain.card.Cards;
+import domain.card.DealerCards;
+import domain.card.PlayerCards;
+import domain.score.ScoreBoard;
 import domain.score.Status;
+
+import java.util.List;
 
 public class Rule {
 
     private static final int STANDARD = 21;
+
+    private final ScoreBoard scoreBoard;
+
+    public Rule(ScoreBoard scoreBoard) {
+        this.scoreBoard = scoreBoard;
+    }
 
     public Status decideStatus(Cards targetCards, Cards otherCards) {
         if (targetCards.bestSum() > STANDARD) {
@@ -24,5 +35,16 @@ public class Rule {
             return Status.LOSE;
         }
         return Status.TIE;
+    }
+
+    public void decideResult(DealerCards dealer, List<PlayerCards> players) {
+        players.forEach(player -> decideResult(dealer, player));
+    }
+
+    private void decideResult(DealerCards dealer, PlayerCards player) {
+        Status dealerStatus = decideStatus(dealer, player);
+        Status playerStatus = decideStatus(player, dealer);
+        scoreBoard.updateDealerScore(dealerStatus);
+        scoreBoard.updatePlayerScore(player.getPlayerName(), playerStatus);
     }
 }
--- src/test/java/domain/RuleTest.java
+++ src/test/java/domain/RuleTest.java
@@ -5,6 +5,7 @@ import domain.card.DealerCards;
 import domain.card.PlayerCards;
 import domain.card.Shape;
 import domain.game.Rule;
+import domain.score.ScoreBoard;
 import domain.score.Status;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,15 +22,35 @@ class RuleTest {
     void decideStatus() {
         DealerCards dealerCards = new DealerCards(List.of(new Card(10, Shape.CLUB), new Card(6, Shape.CLUB)));
 
-        PlayerCards playerCards = new PlayerCards(new Name("capy"), List.of(new Card(10, Shape.CLUB), new Card(11, Shape.CLUB)));
+        Name capy = new Name("capy");
+        PlayerCards playerCards = new PlayerCards(capy, List.of(new Card(10, Shape.CLUB), new Card(11, Shape.CLUB)));
 
-        Rule rule = new Rule();
+        Rule rule = new Rule(ScoreBoard.from(List.of(capy)));
         Status dealerStatus = rule.decideStatus(dealerCards, playerCards);
         Status playerStatus = rule.decideStatus(playerCards, dealerCards);
 
         assertAll(
                 () -> assertThat(dealerStatus).isEqualTo(Status.LOSE),
                 () -> assertThat(playerStatus).isEqualTo(Status.WIN)
+        );
+    }
+
+    @Test
+    @DisplayName("플레이어와 딜러의 승패를 반영한다.")
+    void decideResult() {
+        DealerCards dealerCards = new DealerCards(List.of(new Card(10, Shape.CLUB), new Card(6, Shape.CLUB)));
+
+        Name capy = new Name("capy");
+        PlayerCards playerCards = new PlayerCards(capy, List.of(new Card(10, Shape.CLUB), new Card(11, Shape.CLUB)));
+
+        ScoreBoard scoreBoard = ScoreBoard.from(List.of(capy));
+        new Rule(scoreBoard).decideResult(dealerCards, List.of(playerCards));
+
+        assertAll(
+                () -> assertThat(scoreBoard.getDealerScore().getWinScore()).isEqualTo(0),
+                () -> assertThat(scoreBoard.getDealerScore().getTieScore()).isEqualTo(0),
+                () -> assertThat(scoreBoard.getDealerScore().getLoseScore()).isEqualTo(1),
+                () -> assertThat(scoreBoard.getPlayerScore().get(capy)).isEqualTo(Status.WIN)
         );
     }
 }
