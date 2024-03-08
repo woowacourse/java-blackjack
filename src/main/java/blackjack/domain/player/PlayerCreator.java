563bedffd39b476af79056351d265010752dff90
libienz <leekh10195@gmail.com>
2024-03-08 20:48:08 +0900

    feat: 게임 규칙에 맞게 플레이어를 생성하는 기능 구현

---
+++ src/main/java/blackjack/domain/player/PlayerCreator.java
@@ -0,0 +1,22 @@
+package blackjack.domain.player;
+
+import blackjack.domain.card.CardDeck;
+
+public class PlayerCreator {
+
+    private static final String DEALER_NAME = "딜러";
+
+    private final HandCreator handCreator;
+
+    public PlayerCreator(HandCreator handCreator) {
+        this.handCreator = handCreator;
+    }
+
+    public Player createPlayerFrom(PlayerName playerName, CardDeck cardDeck) {
+        return new Player(playerName, handCreator.createFromDeck(cardDeck));
+    }
+
+    public Player createDealerFrom(CardDeck cardDeck) {
+        return new Player(new PlayerName(DEALER_NAME), handCreator.createFromDeck(cardDeck));
+    }
+}
