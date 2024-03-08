4ea9b34fd967ecceb0dd607747a74d7d1d9f3daa
DobbyMac <kimdobby@g.skku.edu>
2024-03-08 11:06:46 +0900

    feat(Player): player 클래스 클래스 변수 추가
    - isTurnOver bool 값 추가.
    - 각 player의 턴이 끝났는지 표시하는 flag 역할

--- src/main/java/model/participant/Player.java
+++ src/main/java/model/participant/Player.java
@@ -1,13 +1,19 @@
 package model.participant;
 
 public class Player extends Participant {
+    private boolean isTurnOver;
 
     public Player(Name name) {
         super(name);
+        isTurnOver = false;
     }
 
     @Override
     public boolean canHit() {
-        return !cardDeck.isBust();
+        return !cardDeck.isBust() && !isTurnOver;
+    }
+
+    public void turnOver() {
+        isTurnOver = true;
     }
 }
