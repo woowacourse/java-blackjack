a5d81e132e754b383bce22613936218258182035
coli-geonwoo <kkwoo001021@naver.com>
2024-03-08 16:27:37 +0900

    refactor: deck 외부 주입으로 변경

--- src/main/java/blackjack/domain/Game.java
+++ src/main/java/blackjack/domain/Game.java
@@ -1,30 +1,27 @@
 package blackjack.domain;
 
 public class Game {
-    //TODO 멤버 줄이기
-    private final Deck deck;
     private final Dealer dealer;
     private final Players players;
 
-    private Game(Deck deck, Dealer dealer, Players players) {
-        this.deck = deck;
+    private Game(Dealer dealer, Players players) {
         this.dealer = dealer;
         this.players = players;
     }
 
     public static Game of(Deck deck, Dealer dealer, Players players) {
-        Game game = new Game(deck, dealer, players);
-        game.handOutInitialCards();
+        Game game = new Game(dealer, players);
+        game.handOutInitialCards(deck);
         return game;
     }
 
-    private void handOutInitialCards() {
+    public GameResult makeGameResult() {
+        return GameResult.of(dealer, players);
+    }
+
+    private void handOutInitialCards(Deck deck) {
         players.handOutInitialCards(deck);
         dealer.addCard(deck.draw());
         dealer.addCard(deck.draw());
-    }
-
-    public GameResult makeGameResult(){
-        return GameResult.of(dealer, players);
     }
 }
