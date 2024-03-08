70b2927054b23b12a16b69ccb29ffb1b6e85dff7
DobbyMac <kimdobby@g.skku.edu>
2024-03-08 14:40:03 +0900

    feat(Controller): 블랙잭 인스턴스를 이용한 게임 실행 로직 추가

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,12 +1,17 @@
 package controller;
 
 import static util.InputRetryHelper.inputRetryHelper;
+import static view.InputView.input;
 import static view.InputView.inputNames;
 
+import java.util.List;
+import model.Choice;
 import model.casino.Casino;
+import model.casino.RandomCardShuffleMachine;
+import model.dto.IndividualFaceUpResult;
 import model.participant.Entrant;
 import model.participant.Names;
-import model.casino.RandomCardShuffleMachine;
+import view.OutputView;
 
 public class Controller {
 
@@ -15,6 +20,30 @@ public class Controller {
         Entrant entrant = new Entrant(names);
         Casino casino = new Casino(entrant, new RandomCardShuffleMachine());
         casino.initializeGame();
+        IndividualFaceUpResult dealerFaceUpResult = casino.getDealerFaceUpResult();
+        List<IndividualFaceUpResult> playerFaceUpResults = casino.getPlayerFaceUpResult();
+        OutputView.printInitialCardSetting(dealerFaceUpResult, playerFaceUpResults);
+        while (casino.hasAvailablePlayer()) {
+            IndividualFaceUpResult currentPlayerFaceUpInfo = casino.getNextPlayerFaceUpInfo();
+            Choice playerChoice = inputRetryHelper(() -> Choice.from(
+                    input(currentPlayerFaceUpInfo.getPartipantNameAsString() + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")));
+            casino.distinctPlayerChoice(playerChoice);
+            showPlayerChoiceResult(playerChoice, currentPlayerFaceUpInfo);
+        }
 
+        while(casino.isDealerHitAllowed()){
+            casino.hitCardToDealer();
+            OutputView.print("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+        }
+
+
+
+    }
+
+    private void showPlayerChoiceResult(Choice playerChoice, IndividualFaceUpResult currentPlayerFaceUpInfo) {
+        if (playerChoice.isYes() || (!playerChoice.isYes() && currentPlayerFaceUpInfo.cards()
+                .size() == 2)) {
+            OutputView.printPlayersFaceUp(currentPlayerFaceUpInfo);
+        }
     }
 }
