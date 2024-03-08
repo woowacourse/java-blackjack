a5a2607ec85c6f384ead5841cab1d3f8b0c2726f
slimsha2dy <chy2581@naver.com>
2024-03-08 15:11:09 +0900

    refactor: Application 가독성 개선
    Co-authored-by: fromitive <fromitive@gmail.com>

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -12,38 +12,59 @@ public class Application {
     private static final ConsoleReader CONSOLE_READER = new ConsoleReader();
 
     public static void main(String[] args) {
+        BlackJackGame blackJackGame = setGame();
+        List<GamerDto> gamerDtos = showInitialStatus(blackJackGame);
+        playMainGame(gamerDtos, blackJackGame);
+        showFinalStatus(blackJackGame);
+    }
+
+    private static BlackJackGame setGame() {
         List<String> inputs = InputView.readNames(CONSOLE_READER);
         List<Gamer> gamers = inputs.stream()
                 .map(Name::new)
                 .map(Gamer::new)
                 .toList();
-        BlackJackGame blackJackGame = new BlackJackGame(gamers);
+        return new BlackJackGame(gamers);
+    }
+
+    private static List<GamerDto> showInitialStatus(BlackJackGame blackJackGame) {
         blackJackGame.initialDealing();
         GameStatusDto gameInitStatusDto = blackJackGame.getGameStatusDto();
         OutputView.printInitialStatus(gameInitStatusDto);
+        return gameInitStatusDto.getGamerDtos();
+    }
 
-        gameInitStatusDto.getGamerDtos().stream().forEach(gamer ->
+    private static void playMainGame(List<GamerDto> gamerDtos, BlackJackGame blackJackGame) {
+        gamerDtos.stream().forEach(gamer ->
                 askForGamer(gamer, blackJackGame));
-
         blackJackGame.drawDealerCard();
-
-        OutputView.printTotalStatus(blackJackGame.getGameStatusDto());
-        OutputView.printGameResult(blackJackGame.getGameResult());
     }
 
     private static void askForGamer(GamerDto gamer, BlackJackGame blackJackGame) {
-        int count = 1;
-        while (!blackJackGame.isBustFromName(gamer.getName())) {
-            String answer = InputView.readAnswer(CONSOLE_READER, gamer.getName());
-            if (answer.equals("n")) {
-                if (count == 1) {
-                    OutputView.printGamerStatus(gamer);
-                }
-                return;
-            }
-            count++;
-            blackJackGame.drawCardFromName(gamer.getName());
+        boolean isFirstTurn = true;
+        String answer = "y";
+        while (!blackJackGame.isBustFromName(gamer.getName()) && answer.equals("y")) {
+            answer = InputView.readAnswer(CONSOLE_READER, gamer.getName());
+            drawIfAnswerIsYes(answer, gamer, blackJackGame);
+            printPlayerStatus(answer, gamer, isFirstTurn);
+            isFirstTurn = false;
+        }
+    }
+
+    private static void drawIfAnswerIsYes(String answer, GamerDto gamerDto, BlackJackGame blackJackGame) {
+        if (answer.equals("y")) {
+            blackJackGame.drawCardFromName(gamerDto.getName());
+        }
+    }
+
+    private static void printPlayerStatus(String answer, GamerDto gamer, boolean isFirstTurn) {
+        if (isFirstTurn || answer.equals("y")) {
             OutputView.printGamerStatus(gamer);
         }
+    }
+
+    private static void showFinalStatus(BlackJackGame blackJackGame) {
+        OutputView.printTotalStatus(blackJackGame.getGameStatusDto());
+        OutputView.printGameResult(blackJackGame.getGameResult());
     }
 }
