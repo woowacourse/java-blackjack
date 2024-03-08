a617bfd8b62f6a727b3a70e6f9e6c7e50fd43d4d
BurningFalls <sherlocky31415@gmail.com>
2024-03-08 14:30:52 +0900

    refactor: 매직 넘버 10을 MAX_NUMBER 상수화

--- src/main/java/domain/blackjack/CardPointCalculator.java
+++ src/main/java/domain/blackjack/CardPointCalculator.java
@@ -4,11 +4,13 @@ import domain.card.Card;
 import domain.card.CardName;
 
 class CardPointCalculator {
+    private static final int MAX_NUMBER = 10;
+
     static CardPoint calculate(Card card) {
         CardName cardName = card.name();
         int cardNumber = cardName.getCardNumber();
-        if (cardNumber > 10) {
-            return new CardPoint(10);
+        if (cardNumber > MAX_NUMBER) {
+            return new CardPoint(MAX_NUMBER);
         }
         return new CardPoint(cardNumber);
     }
