655c2e3c4f5d39477a8ddc6109d6c58004b32c48
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-08 16:20:31 +0900

    test : @BeforeEach 적용

--- src/test/java/domain/PlayerResultsTest.java
+++ src/test/java/domain/PlayerResultsTest.java
@@ -6,15 +6,15 @@ import domain.gamer.Name;
 import domain.gamer.Player;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PlayerResultsTest {
+    PlayerResults playerResults;
 
-    @DisplayName("우승한 플레이어의 수를 구한다.")
-    @Test
-    void findWinCountTest() {
-        // given
+    @BeforeEach
+    void init() {
         Name name1 = new Name("pobi");
         Name name2 = new Name("jason");
         Name name3 = new Name("solar");
@@ -30,8 +30,13 @@ class PlayerResultsTest {
         results.put(solar, Result.WIN);
         results.put(neo, Result.TIE);
 
-        PlayerResults playerResults = new PlayerResults(results);
+        playerResults = new PlayerResults(results);
+    }
 
+    @DisplayName("우승한 플레이어의 수를 구한다.")
+    @Test
+    void findWinCountTest() {
+        // given
         int expectedWinCount = 2;
 
         // when
@@ -45,23 +50,6 @@ class PlayerResultsTest {
     @Test
     void findLoseCountTest() {
         // given
-        Name name1 = new Name("pobi");
-        Name name2 = new Name("jason");
-        Name name3 = new Name("solar");
-        Name name4 = new Name("neo");
-        Player pobi = new Player(name1);
-        Player jason = new Player(name2);
-        Player solar = new Player(name3);
-        Player neo = new Player(name4);
-
-        Map<Player, Result> results = new LinkedHashMap<>();
-        results.put(pobi, Result.WIN);
-        results.put(jason, Result.LOSE);
-        results.put(solar, Result.WIN);
-        results.put(neo, Result.TIE);
-
-        PlayerResults playerResults = new PlayerResults(results);
-
         int expectedLoseCount = 1;
 
         // when
@@ -75,23 +63,6 @@ class PlayerResultsTest {
     @Test
     void findTieCountTest() {
         // given
-        Name name1 = new Name("pobi");
-        Name name2 = new Name("jason");
-        Name name3 = new Name("solar");
-        Name name4 = new Name("neo");
-        Player pobi = new Player(name1);
-        Player jason = new Player(name2);
-        Player solar = new Player(name3);
-        Player neo = new Player(name4);
-
-        Map<Player, Result> results = new LinkedHashMap<>();
-        results.put(pobi, Result.WIN);
-        results.put(jason, Result.LOSE);
-        results.put(solar, Result.WIN);
-        results.put(neo, Result.TIE);
-
-        PlayerResults playerResults = new PlayerResults(results);
-
         int expectedTieCount = 1;
 
         // when
