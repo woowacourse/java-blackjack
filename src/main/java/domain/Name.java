9aafad71ea3816625a74dd3a9c5904b40e25a024
nayonsoso <panda0329@naver.com>
2024-03-08 13:44:42 +0900

    refactor: 불변 객체 record로 변경

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -1,41 +1,18 @@
 package domain;
 
-import java.util.Objects;
-
-public class Name {
+public record Name(String name) {
     static final int MIN_NAME_LENGTH = 1;
     static final int MAX_NAME_LENGTH = 5;
     static final String NAME_LENGTH_MESSAGE
             = String.format("이름은 %d에서 %d글자 사이여야 합니다", MIN_NAME_LENGTH, MAX_NAME_LENGTH);
 
-    private final String name;
-
-    public Name(String name) {
+    public Name {
         validateLength(name);
-
-        this.name = name;
-    }
-
-    public String name() {
-        return name;
     }
 
     private void validateLength(String name) {
         if (name.length() < MIN_NAME_LENGTH || MAX_NAME_LENGTH < name.length()) {
             throw new IllegalArgumentException(NAME_LENGTH_MESSAGE);
         }
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (Name) obj;
-        return Objects.equals(this.name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
     }
 }
