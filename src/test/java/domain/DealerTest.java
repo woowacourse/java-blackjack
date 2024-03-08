698373e8a9e46de69994a5adfb4ecd98a1cd334c
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-08 15:43:04 +0900

    test : @BeforeEach 적용과 실패 테스트 추가

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -6,22 +6,35 @@ import domain.card.Card;
 import domain.card.Rank;
 import domain.card.Symbol;
 import domain.gamer.Dealer;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DealerTest {
 
+    Stack<Card> cards;
+    Dealer dealer;
+
+    @BeforeEach
+    void init() {
+        Card card1 = new Card(Symbol.HEART, Rank.KING);
+        Card card2 = new Card(Symbol.SPADE, Rank.QUEEN);
+        Card card3 = new Card(Symbol.SPADE, Rank.THREE);
+
+        cards = Stream.of(card1, card2, card3).collect(Collectors.toCollection(Stack::new));
+        dealer = new Dealer();
+    }
+
     //TODO : 생성 부분 리팩터링
     @DisplayName("딜러가 카드를 뽑아서 저장한다.")
     @Test
     void hitTest() {
-        // given
-        Card card = new Card(Symbol.HEART, Rank.NINE);
-
-        Dealer dealer = new Dealer();
-
         // when
-        dealer.hit(card);
+        dealer.hit(cards.pop());
 
         // then
         assertThat(dealer.getHand()).hasSize(1);
@@ -31,14 +44,9 @@ public class DealerTest {
     @Test
     void calculateTotalScoreTest() {
         // given
-        Card card1 = new Card(Symbol.HEART, Rank.NINE);
-        Card card2 = new Card(Symbol.SPADE, Rank.QUEEN);
-
-        Dealer dealer = new Dealer();
-
-        dealer.hit(card1);
-        dealer.hit(card2);
-        int expectedScore = 19;
+        dealer.hit(cards.pop());
+        dealer.hit(cards.pop());
+        int expectedScore = 13;
 
         // when
         int result = dealer.calculateTotalScore();
@@ -51,15 +59,9 @@ public class DealerTest {
     @Test
     void isBust() {
         // given
-        Card card1 = new Card(Symbol.HEART, Rank.NINE);
-        Card card2 = new Card(Symbol.SPADE, Rank.QUEEN);
-        Card card3 = new Card(Symbol.SPADE, Rank.THREE);
-
-        Dealer dealer = new Dealer();
-
-        dealer.hit(card1);
-        dealer.hit(card2);
-        dealer.hit(card3);
+        dealer.hit(cards.pop());
+        dealer.hit(cards.pop());
+        dealer.hit(cards.pop());
 
         // when
         boolean bust = dealer.isBust();
@@ -68,19 +70,27 @@ public class DealerTest {
         assertThat(bust).isTrue();
     }
 
-    @DisplayName("딜러가 카드를 더 받을 수 있는지 판단한다.")
+    @DisplayName("딜러가 카드를 더 받을 수 있다.")
+    @Test
+    void isNotStayTest() {
+        // given
+        dealer.hit(cards.pop());
+        dealer.hit(cards.pop());
+
+        // when
+        boolean stay = dealer.isStay();
+
+        // then
+        assertThat(stay).isFalse();
+    }
+
+    @DisplayName("딜러가 카드를 더 받을 수 없다.")
     @Test
     void isStayTest() {
         // given
-        Card card1 = new Card(Symbol.HEART, Rank.NINE);
-        Card card2 = new Card(Symbol.SPADE, Rank.QUEEN);
-        Card card3 = new Card(Symbol.SPADE, Rank.THREE);
-
-        Dealer dealer = new Dealer();
-
-        dealer.hit(card1);
-        dealer.hit(card2);
-        dealer.hit(card3);
+        dealer.hit(cards.pop());
+        dealer.hit(cards.pop());
+        dealer.hit(cards.pop());
 
         // when
         boolean stay = dealer.isStay();
