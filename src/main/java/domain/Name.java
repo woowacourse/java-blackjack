408fc553f47eb6d7626d9a17cb1b29aa07d4f44c
Nam Gi Beom <rlqja0523@naver.com>
2024-03-08 14:48:20 +0900

    feat: 이름에 특수문자와 숫자가 포함되어 있을 때 예외가 발생하는 기능 추가
    Co-authored-by: jcoding-play <namejgc@naver.com>

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -1,15 +1,19 @@
 package domain;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class Name {
     private static final int MIN_NAME_LENGTH = 2;
     private static final int MAX_NAME_LENGTH = 5;
+    private static final Pattern INVALID_NAME_PATTERN = Pattern.compile("\\P{L}");
 
     private final String name;
 
     public Name(String name) {
         validateLength(name);
+        validateContainsSpace(name);
+        validateNamePattern(name);
         this.name = name;
     }
 
@@ -19,6 +23,22 @@ public class Name {
         if (length < MIN_NAME_LENGTH || MAX_NAME_LENGTH < length) {
             throw new IllegalArgumentException(
                     String.format("rejected value : %s - 이름은 %d ~ %d 글자여야 합니다", name, MIN_NAME_LENGTH, MAX_NAME_LENGTH)
+            );
+        }
+    }
+
+    private void validateContainsSpace(String name) {
+        if (name.contains(" ")) {
+            throw new IllegalArgumentException(
+                    String.format("rejected value : %s - 이름에 공백이 포함되어 있습니다.", name)
+            );
+        }
+    }
+
+    private void validateNamePattern(String name) {
+        if (INVALID_NAME_PATTERN.matcher(name).find()) {
+            throw new IllegalArgumentException(
+                    String.format("rejected value : %s - 이름에 특수문자와 숫자는 허용하지 않습니다.", name)
             );
         }
     }
