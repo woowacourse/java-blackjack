33ac5e182f2abedf922da3fb2fcbbe390275c918
seunghye218 <seunghye@student.42seoul.kr>
2024-03-08 17:01:27 +0900

    feat: 플레이어 인원수 검증 기능 구현
    Co-authored-by: kunsanglee <gosmdochee2@gmail.com>

--- src/main/java/blackjack/domain/Players.java
+++ src/main/java/blackjack/domain/Players.java
@@ -8,10 +8,21 @@ import java.util.stream.Collectors;
 
 public class Players {
 
+    private static final int MIN_PLAYER_SIZE = 1;
+    private static final int MAX_PLAYER_SIZE = 4;
     private final List<Player> players;
 
     public Players(List<Player> players) {
+        validateSize(players);
         this.players = players;
+    }
+
+    private static void validateSize(List<Player> players) {
+        int size = players.size();
+        if (size < MIN_PLAYER_SIZE || size > MAX_PLAYER_SIZE) {
+            throw new IllegalArgumentException(
+                    String.format("플레이어의 수는 %d ~ %d명 이어야 한다.", MIN_PLAYER_SIZE, MAX_PLAYER_SIZE));
+        }
     }
 
     public static Players convertTo(List<String> playerNames) {
