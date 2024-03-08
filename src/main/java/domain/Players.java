cf29c16a5dd8bfd26854103d6ae87081776ec646
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-08 16:57:28 +0900

    feat : 플레이어 이름 중복 예외 처리 추가

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -6,19 +6,27 @@ import java.util.List;
 public class Players {
     public static final int MINIMUM_PLAYER_COUNT = 2;
     public static final int MAXIMUM_PLAYER_COUNT = 8;
-
     public static final String INVALID_PLAYER_COUNT = String.format("플레이어는 %d명에서 %d명까지만 참가 가능합니다.",
             MINIMUM_PLAYER_COUNT, MAXIMUM_PLAYER_COUNT);
+    public static final String DUPLICATED_PLAYER_NAME = "플레이어의 이름은 중복될 수 없습니다.";
     private final List<Player> players;
 
     public Players(final List<Player> players) {
         validatePlayerCount(players);
+        validateDuplicatedPlayer(players);
         this.players = List.copyOf(players);
     }
 
     private void validatePlayerCount(final List<Player> players) {
         if (players.size() < MINIMUM_PLAYER_COUNT || players.size() > MAXIMUM_PLAYER_COUNT) {
             throw new IllegalArgumentException(INVALID_PLAYER_COUNT);
+        }
+    }
+
+    private void validateDuplicatedPlayer(final List<Player> players) {
+        int deduplicatedCount = (int) players.stream().map(player -> player.getName()).distinct().count();
+        if (players.size() != deduplicatedCount) {
+            throw new IllegalArgumentException(DUPLICATED_PLAYER_NAME);
         }
     }
 
--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,13 +1,13 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import domain.gamer.Name;
 import domain.gamer.Player;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,19 +15,34 @@ class PlayersTest {
 
     @DisplayName("플레이어 수가 2명 미만, 8명 초과이면 예외를 던진다.")
     @ParameterizedTest
-    @ValueSource(ints = {1,9})
-    void invalidPlayerCountTest(int count){
+    @ValueSource(ints = {1, 9})
+    void invalidPlayerCountTest(int count) {
         // given
         List<Player> players = new ArrayList<>();
-        for(int i = 0;i<count;i++){
-            String name = "이름"+i;
+        for (int i = 0; i < count; i++) {
+            String name = "이름" + i;
             players.add(new Player(new Name(name)));
         }
 
         // then
-        assertThatThrownBy(()-> new Players(players))
+        assertThatThrownBy(() -> new Players(players))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(Players.INVALID_PLAYER_COUNT);
     }
 
+    @DisplayName("플레이어의 이름이 중복되면 예외를 던진다.")
+    @Test
+    void duplicatedPlayerNameTest() {
+        // given
+        List<Player> players = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            String name = "이름";
+            players.add(new Player(new Name(name)));
+        }
+
+        // then
+        assertThatThrownBy(() -> new Players(players))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Players.DUPLICATED_PLAYER_NAME);
+    }
 }
