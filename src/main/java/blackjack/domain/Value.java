29b0445672d5c9cf378a28a6c3f004d18ce486e6
coli-geonwoo <kkwoo001021@naver.com>
2024-03-08 09:53:32 +0900

    fix: 에이스 다운그레이드시 value 출력 버그 개선
    ACELOW valueName "" > "A"

--- src/main/java/blackjack/domain/Value.java
+++ src/main/java/blackjack/domain/Value.java
@@ -2,7 +2,7 @@ package blackjack.domain;
 
 public enum Value {
 
-    ACELOW("", 1),
+    ACELOW("A", 1),
     ACEHIGH("A", 11), TWO("2", 2), THREE("3", 3),
     FOUR("4", 4), FIVE("5", 5), SIX("6", 6),
     SEVEN("7", 7), EIGHT("8", 8), NINE("9", 9),
