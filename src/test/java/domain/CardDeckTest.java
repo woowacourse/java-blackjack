162927e70cb11243ad7f47cf4088bb5a71c4303b
reddevilmidzy <midzy0228@gmail.com>
2024-03-08 16:46:51 +0900

    refactor: cardDeckTest 클래스 접근제한자 변경

--- src/test/java/domain/CardDeckTest.java
+++ src/test/java/domain/CardDeckTest.java
@@ -4,14 +4,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CardDeckTest {
+class CardDeckTest {
     @DisplayName("카드를 52 * 6 만큼 생성한다.")
     @Test
     void generate() {
-        // given & when
+        // given
         CardDeck cardDeck = CardDeck.generate();
 
-        // then
+        // when && then
         Assertions.assertThat(cardDeck.size()).isEqualTo(52 * 6);
     }
 }
