25cd59aac7b219aa8acc59fc566bef0918297a66
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-08 15:50:36 +0900

    test : @BeforeEach 적용

--- src/main/java/domain/gamer/Name.java
+++ src/main/java/domain/gamer/Name.java
@@ -1,5 +1,7 @@
 package domain.gamer;
 
+import java.util.Objects;
+
 public class Name {
     private final String value;
 
@@ -9,5 +11,22 @@ public class Name {
 
     public String getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name = (Name) o;
+        return Objects.equals(value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
--- src/test/java/domain/GamersTest.java
+++ src/test/java/domain/GamersTest.java
@@ -7,50 +7,47 @@ import domain.gamer.Dealer;
 import domain.gamer.Name;
 import domain.gamer.Player;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GamersTest {
+    Players players;
+    Gamers gamers;
+
+    @BeforeEach
+    void init() {
+        Dealer dealer = new Dealer();
+
+        Name name1 = new Name("pobi");
+        Name name2 = new Name("jason");
+        Player pobi = new Player(name1);
+        Player jason = new Player(name2);
+
+        players = new Players(List.of(pobi, jason));
+        gamers = Gamers.of(players, dealer);
+    }
 
     @DisplayName("게이머들에서 딜러를 찾는다.")
     @Test
     void findDealerTest() {
         // given
-        Dealer dealer = new Dealer();
-
-        Name name1 = new Name("pobi");
-        Name name2 = new Name("jason");
-        Player pobi = new Player(name1);
-        Player jason = new Player(name2);
-
-        Players players = new Players(List.of(pobi, jason));
-        Gamers gamers = Gamers.of(players, dealer);
+        Name expectedName = new Name("딜러");
 
         // when
         Dealer result = gamers.findDealer();
 
         // then
-        assertThat(result.getName()).isEqualTo(dealer.getName());
+        assertThat(result.getName()).isEqualTo(expectedName);
     }
 
     @DisplayName("게이머들에서 플레이어들을 찾는다.")
     @Test
     void findPlayerTest() {
-        // given
-        Dealer dealer = new Dealer();
-
-        Name name1 = new Name("pobi");
-        Name name2 = new Name("jason");
-        Player pobi = new Player(name1);
-        Player jason = new Player(name2);
-
-        Players players = new Players(List.of(pobi, jason));
-        Gamers gamers = Gamers.of(players, dealer);
-
         // when
         List<Player> results = gamers.findPlayers();
 
         // then
-        assertThat(results).containsExactly(pobi, jason);
+        assertThat(results).containsAll(players.getPlayers());
     }
 }
