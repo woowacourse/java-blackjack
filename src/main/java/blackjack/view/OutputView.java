bf0fa4b3befbce050fb3fa48e327347144db2da7
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-09 18:49:47 +0900

    refactor(OutputView): 서식 지정자 출력을 위한 String.format 사용을 printf로 대체

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -22,7 +22,7 @@ public class OutputView {
 
     public static void printDistributionSubject(final List<String> names) {
         String formattedName = String.join(", ", names);
-        System.out.println(String.format("딜러와 %s에게 2장을 나누었습니다.", formattedName));
+        System.out.printf("딜러와 %s에게 2장을 나누었습니다.%n", formattedName);
     }
 
     public static void printNameAndCards(final String name, final List<Card> cards) {
