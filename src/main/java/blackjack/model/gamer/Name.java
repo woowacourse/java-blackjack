d7a4331d94fdd2485d8a50aa67da2456a50f9fd7
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-10 02:13:34 +0900

    refactor(Name): 매직 넘버 상수화

--- src/main/java/blackjack/model/gamer/Name.java
+++ src/main/java/blackjack/model/gamer/Name.java
@@ -2,6 +2,9 @@ package blackjack.model.gamer;
 
 public class Name {
 
+    private static final String BLANK = " ";
+    private static final int MAX_NAME_LENGTH = 10;
+
     private final String name;
 
     public Name(String name) {
@@ -11,13 +14,13 @@ public class Name {
     }
 
     private void validateNameContainBlank(String name) {
-        if (name.contains(" ")) {
+        if (name.contains(BLANK)) {
             throw new IllegalArgumentException("[ERROR] 이름에는 공백이 포함될 수 없습니다.");
         }
     }
 
     private void validateNameLength(String name) {
-        if (name.isEmpty() || name.length() > 10) {
+        if (name.isEmpty() || name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 이름 길이는 0~10자만 가능합니다.");
         }
     }
