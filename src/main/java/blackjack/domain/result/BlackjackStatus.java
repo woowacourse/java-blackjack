e4ca13b8791376db43ec7415bec8fa68d7b418ce
Arachneee <jhg2819@naver.com>
2024-03-10 00:43:50 +0900

    refactor: 선택 조건 필드로 변경

--- src/main/java/blackjack/domain/result/BlackjackStatus.java
+++ src/main/java/blackjack/domain/result/BlackjackStatus.java
@@ -1,21 +1,27 @@
 package blackjack.domain.result;
 
-public enum BlackjackStatus {
-    DEAD,
-    ALIVE,
-    BLACKJACK;
+import java.util.Arrays;
+import java.util.function.BiPredicate;
 
-    public static final int BLACKJACK_NUMBER = 21;
+public enum BlackjackStatus {
+    DEAD(Score::isBiggerThan),
+    ALIVE((score, blackjackScore) -> blackjackScore.isBiggerThan(score)),
+    BLACKJACK(Score::equals);
+
+    private static final int BLACKJACK_NUMBER = 21;
     private static final Score BLACKJACK_SCORE = new Score(BLACKJACK_NUMBER);
 
+    private final BiPredicate<Score, Score> condition;
+
+    BlackjackStatus(final BiPredicate<Score, Score> condition) {
+        this.condition = condition;
+    }
+
     public static BlackjackStatus from(final Score score) {
-        if (score.isBiggerThan(BLACKJACK_SCORE)) {
-            return DEAD;
-        }
-        if (score.equals(BLACKJACK_SCORE)) {
-            return BLACKJACK;
-        }
-        return ALIVE;
+        return Arrays.stream(values())
+                .filter(blackjackStatus -> blackjackStatus.condition.test(score, BLACKJACK_SCORE))
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException("잘못된 점수입니다."));
     }
 
     public boolean isBlackjack() {
