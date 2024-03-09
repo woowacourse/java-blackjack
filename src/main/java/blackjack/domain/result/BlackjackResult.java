2eca9b22863be51840ea85dcd06776637449cc8a
JUHA <84626225+khabh@users.noreply.github.com>
2024-03-09 21:39:55 +0900

    refactor: Dealer 결과 생성 코드 수정 및 생성자에 unmodifiableMap 적용

--- src/main/java/blackjack/domain/result/BlackjackResult.java
+++ src/main/java/blackjack/domain/result/BlackjackResult.java
@@ -2,6 +2,7 @@ package blackjack.domain.result;
 
 import blackjack.domain.participant.Player;
 
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -9,15 +10,14 @@ public class BlackjackResult {
     private final Map<Player, HandResult> playersResult;
 
     public BlackjackResult(Map<Player, HandResult> playersResult) {
-        this.playersResult = playersResult;
+        this.playersResult = Collections.unmodifiableMap(playersResult);
     }
 
     public Map<HandResult, Integer> getDealerResults() {
         Map<HandResult, Integer> dealerResults = initializeDealerResults();
         for (HandResult playerResult : playersResult.values()) {
             HandResult dealerResult = playerResult.getOpposite();
-            int currentResultCount = dealerResults.get(dealerResult);
-            dealerResults.put(dealerResult, currentResultCount + 1);
+            dealerResults.computeIfPresent(dealerResult, (handResult, count) -> count + 1);
         }
         return dealerResults;
     }
