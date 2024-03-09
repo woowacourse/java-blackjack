78b93cd877a4d2a9c22e4e831c0d4ffd45104551
tsulocalize <tsulocalizing@gmail.com>
2024-03-09 19:31:23 +0900

    test: 누락된 메소드 테스트 추가(findShowingCard())

--- src/test/java/domain/participant/DealerTest.java
+++ src/test/java/domain/participant/DealerTest.java
@@ -1,5 +1,6 @@
 package domain.participant;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import domain.card.Card;
@@ -16,11 +17,11 @@ public class DealerTest {
     void receive_NoException_TotalScoreSixteen() {
         Participant dealer = new Dealer();
         dealer.receive(List.of(
-            new Card(Rank.KING, Symbol.CLUB),
-            new Card(Rank.SIX, Symbol.SPADE)
+                new Card(Rank.KING, Symbol.CLUB),
+                new Card(Rank.SIX, Symbol.SPADE)
         ));
         assertThatCode(() -> dealer.receive(new Card(Rank.QUEEN, Symbol.DIAMOND)))
-            .doesNotThrowAnyException();
+                .doesNotThrowAnyException();
     }
 
     @Test
@@ -28,11 +29,31 @@ public class DealerTest {
     void receive_Exception_TotalScoreSeventeen() {
         Participant dealer = new Dealer();
         dealer.receive(List.of(
-            new Card(Rank.KING, Symbol.CLUB),
-            new Card(Rank.SEVEN, Symbol.HEART)
+                new Card(Rank.KING, Symbol.CLUB),
+                new Card(Rank.SEVEN, Symbol.HEART)
         ));
         assertThatCode(() -> dealer.receive(new Card(Rank.QUEEN, Symbol.DIAMOND)))
-            .isInstanceOf(IllegalStateException.class)
-            .hasMessage("[ERROR] 카드를 받을 수 없는 상태입니다.");
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("[ERROR] 카드를 받을 수 없는 상태입니다.");
+    }
+
+    @Test
+    @DisplayName("성공: 딜러의 공개 카드를 가져올 수 있다.(첫번째 카드)")
+    void findShowingCard() {
+        Dealer dealer = new Dealer();
+        Card givenCard = new Card(Rank.KING, Symbol.CLUB);
+        dealer.receive(givenCard);
+        Card showingCard = dealer.findShowingCard();
+
+        assertThat(showingCard).isEqualTo(givenCard);
+    }
+
+    @Test
+    @DisplayName("실패: 딜러에게 카드가 없으면 공개 카드를 가져올 수 없다.")
+    void findShowingCard_Exception_DoesNotHaveCard() {
+        Dealer dealer = new Dealer();
+        assertThatCode(dealer::findShowingCard)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("[ERROR] 딜러 카드가 비어 있습니다.");
     }
 }
