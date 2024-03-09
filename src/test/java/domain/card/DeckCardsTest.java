82b06c3864fe2c393169cc751575385288f73ed6
tsulocalize <tsulocalizing@gmail.com>
2024-03-09 19:04:30 +0900

    test: 누락된 메소드 테스트 추가(drawCards())

--- src/test/java/domain/card/DeckCardsTest.java
+++ src/test/java/domain/card/DeckCardsTest.java
@@ -1,5 +1,6 @@
 package domain.card;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,6 +25,20 @@ class DeckCardsTest {
     }
 
     @Test
+    @DisplayName("성공: 덱에서 카드 여러 장 빼기")
+    void drawCards_NoException() {
+        DeckCards deckCards = DeckCards.from(new SequentialCardGenerator());
+        List<Card> drawnCards = deckCards.drawCards(2);
+        Card firstCard = drawnCards.get(0);
+        Card secondCard = drawnCards.get(1);
+
+        Assertions.assertThat(firstCard.getRank()).isEqualTo(Rank.ACE);
+        Assertions.assertThat(firstCard.getSymbol()).isEqualTo(Symbol.SPADE);
+        Assertions.assertThat(secondCard.getRank()).isEqualTo(Rank.TWO);
+        Assertions.assertThat(secondCard.getSymbol()).isEqualTo(Symbol.SPADE);
+    }
+
+    @Test
     @DisplayName("실패: 덱에 카드가 없는 상태에서 카드 한 장 빼기")
     void drawCard_Exception_NoCardsLeft() {
         DeckCards deckCards = DeckCards.from(new OneCardGenerator());
@@ -31,5 +46,15 @@ class DeckCardsTest {
         Assertions.assertThatThrownBy(deckCards::drawCard)
             .isInstanceOf(IllegalStateException.class)
             .hasMessage("[ERROR] 카드를 모두 사용하였습니다.");
+    }
+
+    @Test
+    @DisplayName("실패: 덱에 카드가 없는 상태에서 카드 여러 장 빼기")
+    void drawCards_Exception_NoCardsLeft() {
+        DeckCards deckCards = DeckCards.from(new OneCardGenerator());
+        deckCards.drawCard();
+        Assertions.assertThatThrownBy(() -> deckCards.drawCards(2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]덱에 남은 카드 수가 요청한 카드 수보다 적습니다.");
     }
 }
