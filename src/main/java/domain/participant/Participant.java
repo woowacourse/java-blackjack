8865c95bb3a63c5e0acbd2d765545712f8b2779c
HyunJoong Kim <hjk0761@naver.com>
2024-03-09 21:26:37 +0900

    refactor: 불필요한 isDealer() 메서드 제거

--- src/main/java/domain/participant/Participant.java
+++ src/main/java/domain/participant/Participant.java
@@ -18,10 +18,6 @@ public abstract class Participant {
         hand.addCard(deck.drawn());
     }
 
-    public boolean isDealer() {
-        return this instanceof Dealer;
-    }
-
     public int getHandSum() {
         return hand.getCardsNumberSum();
     }
