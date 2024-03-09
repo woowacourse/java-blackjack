4cd2b0962aa86a38e7bee2d89a0e77e4d9544f3a
Minjoo <m0522inju@gmail.com>
2024-03-09 17:41:45 +0900

    style(Deck) : 필요없는 static 제거 및 메소드 위치 변경

--- src/main/java/blackjack/model/deck/Deck.java
+++ src/main/java/blackjack/model/deck/Deck.java
@@ -18,21 +18,21 @@ public class Deck {
         this.deck = makeDeck();
     }
 
-    private static Deque<Card> makeDeck() {
+    private Deque<Card> makeDeck() {
         List<Card> originDeck = Arrays.stream(Shape.values())
                 .flatMap(Deck::matchScore)
                 .collect(Collectors.toList());
         return new ArrayDeque<>(shuffleDeck(originDeck));
     }
 
-    private static Deque<Card> shuffleDeck(final List<Card> originDeck) {
-        Collections.shuffle(originDeck);
-        return new ArrayDeque<>(originDeck);
-    }
-
     private static Stream<Card> matchScore(Shape shape) {
         return Arrays.stream(Score.values())
                 .map(score -> new Card(shape, score));
+    }
+
+    private Deque<Card> shuffleDeck(final List<Card> originDeck) {
+        Collections.shuffle(originDeck);
+        return new ArrayDeque<>(originDeck);
     }
 
     public Hand distributeInitialCard() {
