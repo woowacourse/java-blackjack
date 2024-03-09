2e1cec7866487d2521273c5f9ed01428c933d7e0
HyunJoong Kim <hjk0761@naver.com>
2024-03-09 23:20:37 +0900

    style: 불필요한 공백 제거

--- src/main/java/domain/dto/DealerHandStatusDto.java
+++ src/main/java/domain/dto/DealerHandStatusDto.java
@@ -5,7 +5,6 @@ import domain.participant.Dealer;
 import java.util.List;
 
 public record DealerHandStatusDto(List<PlayingCardDto> playingCards, int playingCardSum) {
-
     public static DealerHandStatusDto of(Dealer dealer) {
         List<PlayingCardDto> playingCars = dealer.getHandCards().stream()
                 .map(PlayingCardDto::of)
