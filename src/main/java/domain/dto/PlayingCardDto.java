747b2e5ee364d63239eaf38e76260d844449690f
HyunJoong Kim <hjk0761@naver.com>
2024-03-09 23:32:06 +0900

    style: 불필요한 공백 제거

--- src/main/java/domain/dto/PlayingCardDto.java
+++ src/main/java/domain/dto/PlayingCardDto.java
@@ -5,7 +5,6 @@ import domain.PlayingCardShape;
 import domain.PlayingCardValue;
 
 public record PlayingCardDto(PlayingCardShape playingCardShape, PlayingCardValue playingCardValue) {
-
     public static PlayingCardDto of(PlayingCard playingCard) {
         return new PlayingCardDto(playingCard.playingCardShape(), playingCard.playingCardValue());
     }
