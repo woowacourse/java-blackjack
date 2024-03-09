80c33d6ae7b9d3f346291529524c7f89f91675f4
jhon3242 <rldotnr2@gmail.com>
2024-03-09 21:30:12 +0900

    Test : Hands에 대한 테스트 수정

--- src/test/java/blackjack/domain/HandsTest.java
+++ src/test/java/blackjack/domain/HandsTest.java
@@ -1,9 +1,13 @@
 package blackjack.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import blackjack.domain.deck.Card;
+import blackjack.domain.deck.Deck;
 import blackjack.domain.deck.Hands;
 import blackjack.domain.deck.Rank;
 import blackjack.domain.deck.Shape;
+import java.util.ArrayList;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -28,13 +32,74 @@ public class HandsTest {
     @DisplayName("손패에 있는 카드 점수를 계산한다.")
     void calculateScoreTest() {
         Hands hands = new Hands(List.of(
-                new Card(Shape.HEART, Rank.ACE),
+                new Card(Shape.HEART, Rank.JACK),
                 new Card(Shape.HEART, Rank.TEN)
         ));
 
-        int score = hands.calculateScore();
+        int result = hands.calculateScore();
 
-        Assertions.assertThat(score).isEqualTo(21);
+        Assertions.assertThat(result).isEqualTo(20);
     }
 
+    @Test
+    @DisplayName("최적의 에이스 값, 11을 선택한다.")
+    void calculateAceScoreWithSingleTest() {
+        Hands hands = new Hands(List.of(
+                new Card(Shape.HEART, Rank.ACE),
+                new Card(Shape.HEART, Rank.JACK)
+        ));
+
+        int result = hands.calculateScore();
+
+        assertThat(result).isEqualTo(21);
+    }
+
+    @Test
+    @DisplayName("최적의 에이스 값, 11과 1을 선택한다.")
+    void calculateAceScoreWithDoubleTest() {
+        Hands hands = new Hands(List.of(
+                new Card(Shape.HEART, Rank.ACE),
+                new Card(Shape.HEART, Rank.ACE)
+        ));
+
+        int result = hands.calculateScore();
+
+        assertThat(result).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("최적의 에이스 값, 11, 1, 1을 선택한다.")
+    void calculateAceScoreWithTripleTest() {
+        Hands hands = new Hands(List.of(
+                new Card(Shape.HEART, Rank.ACE),
+                new Card(Shape.DIAMOND, Rank.ACE),
+                new Card(Shape.SPADE, Rank.ACE)
+        ));
+
+        int result = hands.calculateScore();
+
+        assertThat(result).isEqualTo(13);
+    }
+
+    @Test
+    @DisplayName("최적의 에이스 값, 1과 1을 선택한다.")
+    void calculateAceScoreWithDiverseTest() {
+        Hands hands = new Hands(List.of(
+                new Card(Shape.HEART, Rank.JACK),
+                new Card(Shape.CLOVER, Rank.NINE),
+                new Card(Shape.DIAMOND, Rank.ACE),
+                new Card(Shape.DIAMOND, Rank.ACE)
+        ));
+
+        int result = hands.calculateScore();
+
+        assertThat(result).isEqualTo(21);
+    }
+
+    @Test
+    @DisplayName("카드의 개수를 확인한다.")
+    void sizeTest() {
+        Hands hands = new Hands(new ArrayList<>());
+        assertThat(hands.size()).isEqualTo(0);
+    }
 }
