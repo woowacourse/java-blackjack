0c025152344e7cfe833c4a9e0cfbca1a87a7c7fe
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-11 03:12:27 +0900

    refactor: 결과 생성 로직을 필드로 갖도록 변경

--- src/main/java/domain/Result.java
+++ src/main/java/domain/Result.java
@@ -1,22 +1,25 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+
 public enum Result {
-    DEALER_WIN,
-    PLAYER_WIN,
-    PUSH;
+    DEALER_WIN(
+            (dealer, player) -> player.isBust() || !dealer.isBust() && dealer.getTotalScore() > player.getTotalScore()),
+    PLAYER_WIN(
+            (dealer, player) -> dealer.isBust() || !player.isBust() && dealer.getTotalScore() < player.getTotalScore()),
+    PUSH((dealer, player) -> dealer.getTotalScore() == player.getTotalScore());
+
+    private final BiPredicate<Dealer, Player> judgeResult;
+
+    Result(final BiPredicate<Dealer, Player> judgeResult) {
+        this.judgeResult = judgeResult;
+    }
 
     public static Result of(Dealer dealer, Player player) {
-        int playerTotalScore = player.getTotalScore();
-        int dealerTotalScore = dealer.getTotalScore();
-
-        if (player.isBust() || !dealer.isBust() && dealerTotalScore > playerTotalScore) {
-            return DEALER_WIN;
-        }
-
-        if (dealer.isBust() || !player.isBust() && dealerTotalScore < playerTotalScore) {
-            return PLAYER_WIN;
-        }
-
-        return PUSH;
+        return Arrays.stream(values())
+                .filter(value -> value.judgeResult.test(dealer, player))
+                .findAny()
+                .orElseThrow();
     }
 }
