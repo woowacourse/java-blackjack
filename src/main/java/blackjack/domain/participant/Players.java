be13efd98e2eda580ee782457d963e567a978a9d
masonkimseoul <masonkimseoul@gmail.com>
2024-03-10 22:24:06 +0900

    refactor: 입력된 player의 수 검증 메서드 분리

--- src/main/java/blackjack/domain/participant/Players.java
+++ src/main/java/blackjack/domain/participant/Players.java
@@ -8,12 +8,16 @@ public class Players {
     private final List<Player> values;
 
     public Players(List<Name> playerNames, HandGenerator handGenerator) {
-        if (playerNames.isEmpty()) {
-            throw new InvalidPlayerCountException();
-        }
+        checkPlayersEmpty(playerNames);
         this.values = playerNames.stream()
                 .map(playerName -> new Player(playerName, handGenerator))
                 .toList();
+    }
+
+    private void checkPlayersEmpty(List<Name> playerNames) {
+        if (playerNames.isEmpty()) {
+            throw new InvalidPlayerCountException();
+        }
     }
 
     public List<Player> getValues() {
