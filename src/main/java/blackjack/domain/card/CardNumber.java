d2c0c4285dd5390558de327b21085d5211f7d755
libienz <leekh10195@gmail.com>
2024-03-10 17:28:47 +0900

    refactor: 도메인 내 뷰로직 제거 개선

--- src/main/java/blackjack/domain/card/CardNumber.java
+++ src/main/java/blackjack/domain/card/CardNumber.java
@@ -2,26 +2,24 @@ package blackjack.domain.card;
 
 public enum CardNumber {
 
-    ACE(1, "A"),
-    TWO(2, "2"),
-    THREE(3, "3"),
-    FOUR(4, "4"),
-    FIVE(5, "5"),
-    SIX(6, "6"),
-    SEVEN(7, "7"),
-    EIGHT(8, "8"),
-    NINE(9, "9"),
-    TEN(10, "10"),
-    JACK(10, "J"),
-    QUEEN(10, "Q"),
-    KING(10, "K");
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10);
 
     private final int value;
-    private final String name;
 
-    CardNumber(int value, String name) {
+    CardNumber(int value) {
         this.value = value;
-        this.name = name;
     }
 
     public boolean isAce() {
@@ -30,10 +28,6 @@ public enum CardNumber {
 
     public int getValue() {
         return value;
-    }
-
-    public String getName() {
-        return name;
     }
 }
 
--- src/main/java/blackjack/domain/card/CardShape.java
+++ src/main/java/blackjack/domain/card/CardShape.java
@@ -2,18 +2,8 @@ package blackjack.domain.card;
 
 public enum CardShape {
 
-    HEART("하트"),
-    DIAMOND("다이아몬드"),
-    CLUB("클로버"),
-    SPADE("스페이드");
-
-    private final String name;
-
-    CardShape(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+    HEART,
+    DIAMOND,
+    CLUB,
+    SPADE;
 }
