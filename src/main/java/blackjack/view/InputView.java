c1e1739e985096790726ebdd09f56d8dcf481c35
김민수 MINSOO KIM <alstn113@gmail.com>
2024-03-11 00:31:33 +0900

    feat: 베팅 금액 입력 구현

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -6,14 +6,33 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final String PLAYER_NAME_DELIMITER = ",";
+
     private final Scanner scanner = new Scanner(System.in);
+
+    private String readLine(String message) {
+        System.out.println(message);
+
+        return scanner.nextLine().trim();
+    }
 
     public List<String> readPlayerNames() {
         String input = readLine("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
 
-        return Arrays.stream(input.split(",", -1))
+        return Arrays.stream(input.split(PLAYER_NAME_DELIMITER, -1))
                 .map(String::trim)
                 .toList();
+    }
+
+    public int readBettingMoney(String name) {
+        String message = String.format("%s의 배팅 금액은?", name);
+        String input = readLine(message);
+
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("베팅 금액은 숫자로 입력해주세요.");
+        }
     }
 
     public boolean readCommand(String name) {
@@ -29,11 +48,5 @@ public class InputView {
         }
 
         throw new IllegalArgumentException("y나 n를 입력해주세요.");
-    }
-
-    private String readLine(String message) {
-        System.out.println(message);
-
-        return scanner.nextLine().trim();
     }
 }
