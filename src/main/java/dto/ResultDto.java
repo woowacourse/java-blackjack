9483aadbb67345f3b1cea3eb133b4499f4ed86d1
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-11 02:12:16 +0900

    refactor: DealearResultDto, PlayerResultDto -> ResultDto
    결과를 출력하는 Dto를 하나로 묶어서 관리

---
+++ src/main/java/dto/ResultDto.java
@@ -0,0 +1,53 @@
+package dto;
+
+import domain.Player;
+import domain.Result;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public record ResultDto(Map<String, String> gameResults) {
+
+    public static ResultDto from(Map<Player, Result> gameResult) {
+        Map<String, String> results = new LinkedHashMap<>();
+        for (Map.Entry<Player, Result> entry : gameResult.entrySet()) {
+            String playerName = entry.getKey().getName();
+            String result = entry.getValue().name();
+            results.put(playerName, result);
+        }
+
+        return new ResultDto(results);
+    }
+
+    public Map<String, Integer> getDealerResult() {
+        Map<String, Integer> result = new HashMap<>();
+        result.put(Result.DEALER_WIN.name(), count(Result.DEALER_WIN));
+        result.put(Result.PLAYER_WIN.name(), count(Result.PLAYER_WIN));
+        result.put(Result.PUSH.name(), count(Result.PUSH));
+        return result;
+    }
+
+    private int count(final Result status) {
+        return (int) gameResults.values().stream()
+                .filter(result -> result.equals(status.name()))
+                .count();
+    }
+
+    public Map<String, String> getPlayerResult() {
+        Map<String, String> results = new LinkedHashMap<>();
+        for (Map.Entry<String, String> result : gameResults.entrySet()) {
+            results.put(result.getKey(), convertPlayerWinLoss(result.getValue()));
+        }
+        return results;
+    }
+
+    private String convertPlayerWinLoss(String resultText) {
+        if (resultText.equals("PLAYER_WIN")) {
+            return "승";
+        }
+        if (resultText.equals("DEALER_WIN")) {
+            return "패";
+        }
+        return "무";
+    }
+}
