a44d5de18ea7bb2dda1a3e2b038058f081157cc9
hyun <osh123chm@gmail.com>
2024-03-10 15:10:18 +0900

    feat: 한벌의 카드 생성 기능 추가

---
+++ src/main/java/blackjack/model/generator/DeckGenerator.java
@@ -0,0 +1,22 @@
+package blackjack.model.generator;
+
+import blackjack.model.cards.Card;
+import blackjack.model.cards.CardNumber;
+import blackjack.model.cards.CardShape;
+import java.util.Arrays;
+import java.util.List;
+
+public class DeckGenerator {
+    public List<Card> generate() {
+        return Arrays.stream(CardNumber.values())
+                .map(this::generateCardsByCardNumber)
+                .flatMap(List::stream)
+                .toList();
+    }
+
+    private List<Card> generateCardsByCardNumber(CardNumber cardNumber) {
+        return Arrays.stream(CardShape.values())
+                .map(cardShape -> new Card(cardNumber, cardShape))
+                .toList();
+    }
+}
---
+++ src/test/java/blackjack/model/generator/DeckGeneratorTest.java
@@ -0,0 +1,23 @@
+package blackjack.model.generator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.model.cards.Card;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DeckGeneratorTest {
+    @DisplayName("모양과 숫자가 모두 다른 카드 한벌을 생성한다.")
+    @Test
+    void generateDeck() {
+        DeckGenerator deckGenerator = new DeckGenerator();
+        List<Card> deck = deckGenerator.generate();
+
+        Set<Card> nonDuplicateDeck = new HashSet<>(deck);
+
+        assertThat(nonDuplicateDeck).hasSize(52);
+    }
+}
