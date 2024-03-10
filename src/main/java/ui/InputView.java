472f4ed4c35aa08f8922a3f28d4663d776110fd8
Nam Gi Beom <rlqja0523@naver.com>
2024-03-11 02:01:08 +0900

    refactor: set 변경, y/n 하드코딩

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -1,14 +1,14 @@
 package ui;
 
-import domain.blackjackgame.BlackjackAction;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class InputView {
     private static final String DEFAULT_DELIMITER = ",";
     private static final String DEFAULT_DELIMITER_NAME = "쉼표";
-    private static final List<String> INVALID_PLAYER_NAMES = List.of("딜러", "dealer");
+    private static final Set<String> INVALID_PLAYER_NAMES = Set.of("딜러", "dealer");
 
     private final Scanner scanner;
 
@@ -42,8 +42,7 @@ public class InputView {
     }
 
     public String readBlackjackAction(String name) {
-        System.out.printf("%s는 한 장의 카드를 더 받겠습니까?(예는 %s, 아니오는 %s)%n",
-                name, BlackjackAction.HIT.getExpression(), BlackjackAction.STAY.getExpression());
+        System.out.printf("%s는 한 장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)%n", name);
         String action = scanner.nextLine();
         validateInput(action);
         return action.trim();
