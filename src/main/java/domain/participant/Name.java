28512428fa4644dd1b26879742d07327095ad161
Zeus6768 <akaps6768@gmail.com>
2024-03-10 17:57:58 +0900

    chore: 불필요한 메서드 삭제

--- src/main/java/domain/participant/Name.java
+++ src/main/java/domain/participant/Name.java
@@ -1,13 +1,8 @@
 package domain.participant;
 
 public record Name(String value) {
-
     public Name {
-        validateNotBlank(value);
-    }
-
-    private void validateNotBlank(final String name) {
-        if (name.isBlank()) {
+        if (value.isBlank()) {
             throw new IllegalArgumentException("플레이어 이름을 입력해주세요. 예) 포비, 호티, 제우스");
         }
     }
