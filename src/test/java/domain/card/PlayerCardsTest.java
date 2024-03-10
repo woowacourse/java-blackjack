beaafe35b1cf9dba2378eeba0b6281702ef39f15
ay-eonii <naayen@naver.com>
2024-03-10 13:47:47 +0900

    style : 개행 및 중복코드 제거

--- src/test/java/domain/card/PlayerCardsTest.java
+++ src/test/java/domain/card/PlayerCardsTest.java
@@ -4,7 +4,6 @@ import domain.Name;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,14 +14,21 @@ public class PlayerCardsTest {
     @Test
     @DisplayName("처음 지급받는 카드의 수가 두 장이 아니라면 예외가 발생한다.")
     void player_OverSize_ExceptionThrown() {
-        assertThatThrownBy(() -> new PlayerCards(new Name("capy"), List.of(new Card(1, Shape.CLUB), new Card(2, Shape.CLUB), new Card(3, Shape.CLUB))))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new PlayerCards(new Name("capy"),
+                List.of(
+                        new Card(1, Shape.CLUB),
+                        new Card(2, Shape.CLUB),
+                        new Card(3, Shape.CLUB)
+                ))).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("플러이어의 카드 숫자의 합이 최소 점수 조건 이하면 뽑을 수 있다.")
     void sum_IsOverMax_True() {
-        PlayerCards cards = new PlayerCards(new Name("capy"), new ArrayList<>(List.of(new Card(10, Shape.CLUB), new Card(11, Shape.CLUB))));
+        PlayerCards cards = new PlayerCards(new Name("capy"), List.of(
+                new Card(10, Shape.CLUB),
+                new Card(11, Shape.CLUB)
+        ));
 
         assertThat(cards.canDraw()).isTrue();
     }
@@ -30,7 +36,10 @@ public class PlayerCardsTest {
     @Test
     @DisplayName("플레이어의 카드 숫자의 합이 최대 점수 조건 초과면 뽑을 수 없다.")
     void sum_IsBelowMax_False() {
-        PlayerCards cards = new PlayerCards(new Name("capy"), new ArrayList<>(List.of(new Card(10, Shape.CLUB), new Card(10, Shape.CLUB))));
+        PlayerCards cards = new PlayerCards(new Name("capy"), List.of(
+                new Card(10, Shape.CLUB),
+                new Card(10, Shape.CLUB)
+        ));
         cards.receive(new Card(2, Shape.CLUB));
 
         assertThat(cards.canDraw()).isFalse();
@@ -39,7 +48,10 @@ public class PlayerCardsTest {
     @Test
     @DisplayName("딜러가 카드를 뽑는다.")
     void draw_SizeUp() {
-        PlayerCards playerCards = new PlayerCards(new Name("capy"), new ArrayList<>(List.of(new Card(6, Shape.CLUB), new Card(10, Shape.CLUB))));
+        PlayerCards playerCards = new PlayerCards(new Name("capy"), List.of(
+                new Card(6, Shape.CLUB),
+                new Card(10, Shape.CLUB)
+        ));
 
         playerCards.receive(new Card(1, Shape.CLUB));
 
@@ -48,7 +60,9 @@ public class PlayerCardsTest {
 
     @Test
     void bestSum() {
-        PlayerCards playerCards = new PlayerCards(new Name("capy"), new ArrayList<>(List.of(new Card(1, Shape.CLUB), new Card(1, Shape.CLUB))));
+        PlayerCards playerCards = new PlayerCards(new Name("capy"), List.of(
+                new Card(1, Shape.CLUB),
+                new Card(1, Shape.CLUB)));
 
         assertThat(playerCards.bestSum()).isEqualTo(12);
     }
--- src/test/java/domain/game/RuleTest.java
+++ src/test/java/domain/game/RuleTest.java
@@ -1,12 +1,13 @@
-package domain;
+package domain.game;
 
+import domain.Name;
 import domain.card.Card;
 import domain.card.DealerCards;
 import domain.card.PlayerCards;
 import domain.card.Shape;
-import domain.game.Rule;
 import domain.score.ScoreBoard;
 import domain.score.Status;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,15 +18,31 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 class RuleTest {
 
+    private final Name capy = new Name("capy");
+    private DealerCards dealerCards;
+    private PlayerCards playerCards;
+    private ScoreBoard scoreBoard;
+    private Rule rule;
+
+    @BeforeEach
+    void setUp() {
+        dealerCards = new DealerCards(List.of(
+                new Card(6, Shape.CLUB),
+                new Card(10, Shape.CLUB)
+        ));
+
+        playerCards = new PlayerCards(capy, List.of(
+                new Card(10, Shape.CLUB),
+                new Card(11, Shape.CLUB)
+        ));
+
+        scoreBoard = ScoreBoard.from(List.of(capy));
+        rule = new Rule(scoreBoard);
+    }
+
     @Test
     @DisplayName("타겟 카드의 승무패를 결정한다.")
     void decideStatus() {
-        DealerCards dealerCards = new DealerCards(List.of(new Card(10, Shape.CLUB), new Card(6, Shape.CLUB)));
-
-        Name capy = new Name("capy");
-        PlayerCards playerCards = new PlayerCards(capy, List.of(new Card(10, Shape.CLUB), new Card(11, Shape.CLUB)));
-
-        Rule rule = new Rule(ScoreBoard.from(List.of(capy)));
         Status dealerStatus = rule.decideStatus(dealerCards, playerCards);
         Status playerStatus = rule.decideStatus(playerCards, dealerCards);
 
@@ -38,13 +55,7 @@ class RuleTest {
     @Test
     @DisplayName("플레이어와 딜러의 승패를 반영한다.")
     void decideResult() {
-        DealerCards dealerCards = new DealerCards(List.of(new Card(10, Shape.CLUB), new Card(6, Shape.CLUB)));
-
-        Name capy = new Name("capy");
-        PlayerCards playerCards = new PlayerCards(capy, List.of(new Card(10, Shape.CLUB), new Card(11, Shape.CLUB)));
-
-        ScoreBoard scoreBoard = ScoreBoard.from(List.of(capy));
-        new Rule(scoreBoard).decideResult(dealerCards, List.of(playerCards));
+        rule.decideResult(dealerCards, List.of(playerCards));
 
         assertAll(
                 () -> assertThat(scoreBoard.getDealerScore().getScore(Status.WIN)).isEqualTo(0),
