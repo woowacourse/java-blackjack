c576cf13122bda51f537e7be39b2e50cf77c16b7
Zeus6768 <akaps6768@gmail.com>
2024-03-11 00:25:59 +0900

    refactor: GameResultStatus -> BlackjackResultStatus 클래스명 변경

---
+++ src/main/java/domain/BlackjackResultStatus.java
@@ -0,0 +1,16 @@
+package domain;
+
+public enum BlackjackResultStatus {
+
+    WIN("승"), LOSE("패"), PUSH("무");
+
+    private final String value;
+
+    BlackjackResultStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
