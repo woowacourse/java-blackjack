5115ab5e51db56d13d02e724de0ff534bb994aad
linirini <2001yerin@naver.com>
2024-03-10 20:35:03 +0900

    feat : 카드가 모두 소진된 경우 예외 처리 추가

--- src/main/java/domain/Decks.java
+++ src/main/java/domain/Decks.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Stack;
 
 public class Decks {
+    private static final String NO_CARDS = "카드가 모두 소진되었습니다.";
     private final Stack<Card> cards;
 
     private Decks(Stack<Card> cards) {
@@ -18,6 +19,9 @@ public class Decks {
     }
 
     public Card draw() {
+        if(cards.empty()){
+            throw new IllegalArgumentException(NO_CARDS);
+        }
         return cards.pop();
     }
 
--- src/test/java/domain/DecksTest.java
+++ src/test/java/domain/DecksTest.java
@@ -1,10 +1,17 @@
 package domain;
 
+import domain.card.Card;
+import domain.card.Rank;
+import domain.card.Symbol;
+import domain.strategy.SettedDecksGenerator;
 import domain.strategy.ShuffledDecksGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class DecksTest {
 
@@ -20,5 +27,19 @@ public class DecksTest {
 
         // then
         assertThat(decks.getCards()).hasSize(expectedSize);
+    }
+
+    @DisplayName("52 * 6개의 카드를 생성한다.")
+    @Test
+    void emptyDecksTest() {
+        // given
+        SettedDecksGenerator decksGenerator = new SettedDecksGenerator(List.of(new Card(Symbol.CLOVER,Rank.BIG_ACE)));
+        Decks decks = Decks.createByStrategy(decksGenerator);
+        decks.draw();
+
+        // then
+        assertThatThrownBy(decks::draw)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("카드가 모두 소진되었습니다.");
     }
 }
