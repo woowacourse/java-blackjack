c8b347e9a3a0cbcf97582b22f3dd2639d2ab0228
Nam Gi Beom <rlqja0523@naver.com>
2024-03-11 01:28:39 +0900

    refactor: if문으로 변경 및 사용하지 않는 메서드 삭제

--- src/main/java/domain/blackjackgame/BlackjackAction.java
+++ src/main/java/domain/blackjackgame/BlackjackAction.java
@@ -1,12 +1,9 @@
 package domain.blackjackgame;
 
-import java.util.Arrays;
-
 public enum BlackjackAction {
     HIT("y"),
     STAY("n");
 
-    private static final BlackjackAction[] VALUES = values();
     private final String expression;
 
     BlackjackAction(String expression) {
@@ -14,21 +11,16 @@ public enum BlackjackAction {
     }
 
     public static BlackjackAction from(String expression) {
-        return Arrays.stream(VALUES)
-                .filter(action -> action.isMatchExpression(expression))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("잘못된 입력 값입니다."));
-    }
-
-    private boolean isMatchExpression(String expression) {
-        return this.expression.equals(expression);
+        if (HIT.expression.equals(expression)) {
+            return HIT;
+        }
+        if (STAY.expression.equals(expression)) {
+            return STAY;
+        }
+        throw new IllegalArgumentException("잘못된 입력 값입니다.");
     }
 
     public boolean isHit() {
         return this == HIT;
-    }
-
-    public String getExpression() {
-        return this.expression;
     }
 }
