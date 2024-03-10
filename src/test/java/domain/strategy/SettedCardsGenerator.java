bd3f4bf9eefe3959033909e9f0276904a3125ed9
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-10 21:24:34 +0900

    refactor : 클래스 변수 타입을 인터페이스 반환값과 동일하게 List로 수정

--- src/test/java/domain/strategy/SettedCardsGenerator.java
+++ src/test/java/domain/strategy/SettedCardsGenerator.java
@@ -2,21 +2,23 @@ package domain.strategy;
 
 import domain.card.Card;
 import domain.CardsGenerator;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 import java.util.Stack;
 import java.util.stream.Collectors;
 
 
 public class SettedCardsGenerator implements CardsGenerator {
-    private final Stack<Card> cards;
+    private final List<Card> cards;
 
     public SettedCardsGenerator(final List<Card> cards) {
         this.cards = cards.stream()
-                .collect(Collectors.toCollection(Stack::new));
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Stack<Card> generate() {
+    public List<Card> generate() {
         return cards;
     }
 }
