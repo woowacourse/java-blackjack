3195e77b2509587651b32bfbd2ad05581dfca276
SCY <helenason@naver.com>
2024-03-10 15:01:51 +0900

    refactor: 도메인과 뷰의 역할 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,63 @@
 import domain.BlackJackGame;
+import domain.cards.Card;
+import domain.gamer.Dealer;
+import domain.gamer.Gamer;
+import domain.gamer.Gamers;
+import domain.gamer.Player;
+import java.util.List;
 import view.InputView;
 import view.ResultView;
 
-public class Application {
+public class Application { // TODO: refactoring
+
+    private static final InputView inputView = new InputView();
+    private static final ResultView resultView = new ResultView();
+
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        ResultView resultView = new ResultView();
-        BlackJackGame blackJackGame = new BlackJackGame(inputView, resultView);
-        blackJackGame.play();
+        BlackJackGame game = new BlackJackGame();
+
+        List<String> rawPlayersNames = inputView.readPlayersNames();
+        game.prepareGamers(rawPlayersNames);
+        game.prepareCardPack();
+
+        Gamers gamers = game.getGamers();
+
+        game.setUpGame();
+        resultView.printInitialCards(gamers);
+
+        progressPlayersGame(game, gamers.callPlayers());
+        progressDealerGame(game, gamers.callDealer());
+        resultView.printAllGamersCardsResult(gamers);
+
+        game.makeFinalResult();
+        resultView.printFinalResults(gamers.callDealer(), game.getJudge()); // TODO
+    }
+
+    private static void progressPlayersGame(BlackJackGame game, List<Player> players) {
+        for (Player player : players) {
+            progressPlayerGame(game, player);
+        }
+    }
+
+    private static void progressPlayerGame(BlackJackGame game, Player player) {
+        boolean doPlayerHit = true;
+        while (player.isNotBust() && doPlayerHit) {
+            String rawHitOption = inputView.readHitOrNot(player);
+            doPlayerHit = game.hitByPlayer(rawHitOption, player);
+            printPlayerHitCard(player, doPlayerHit);
+        }
+    }
+
+    private static void printPlayerHitCard(Gamer player, boolean doPlayerHit) {
+        if (doPlayerHit) {
+            resultView.printPlayerCards(player);
+        }
+    }
+
+    private static void progressDealerGame(BlackJackGame game, Dealer dealer) {
+        while (dealer.canHit()) {
+            Card pickedCard = game.hitByDealer(dealer);
+            resultView.printDealerHitMessage(dealer, pickedCard);
+        }
     }
 }
--- src/main/java/domain/BlackJackGame.java
+++ src/main/java/domain/BlackJackGame.java
@@ -6,9 +6,6 @@ import domain.cards.CardsGenerator;
 import domain.gamer.Dealer;
 import domain.gamer.Gamer;
 import domain.gamer.Gamers;
-import domain.gamer.Player;
-import view.InputView;
-import view.ResultView;
 
 import java.util.List;
 
@@ -16,76 +13,59 @@ public class BlackJackGame {
 
     private static final int INIT_CARD_COUNT = 2;
 
-    private final InputView inputView;
-    private final ResultView resultView;
+    // TODO: remove to 2
+    private Gamers gamers;
+    private Judge judge = new Judge();
+    private CardPack cardPack;
 
-    public BlackJackGame(InputView inputView, ResultView resultView) {
-        this.inputView = inputView;
-        this.resultView = resultView;
+    public BlackJackGame() {
     }
 
-    public void play() {
-        Gamers gamers = new Gamers(inputView.readPlayersNames());
-        CardPack cardPack = generateCardPack();
-
-        configureSetup(gamers, cardPack);
-        progressGame(gamers, cardPack);
-
-        makeFinalResult(gamers);
+    public void prepareGamers(List<String> rawPlayersNames) {
+        this.gamers = new Gamers(rawPlayersNames);
     }
 
-    private CardPack generateCardPack() {
+    public void prepareCardPack() {
         List<Card> cards = CardsGenerator.generateRandomCards();
-        return new CardPack(cards);
+        this.cardPack = new CardPack(cards);
     }
 
-    private void configureSetup(Gamers gamers, CardPack cardPack) {
+    public void setUpGame() {
         for (Gamer gamer : gamers.getGamers()) {
-            shareInitCards(gamer, cardPack);
+            shareInitCards(gamer);
         }
-        resultView.printInitialCards(gamers);
     }
 
-    private void shareInitCards(Gamer gamer, CardPack cardPack) {
+    private void shareInitCards(Gamer gamer) {
         for (int i = 0; i < INIT_CARD_COUNT; i++) {
             gamer.hit(cardPack.pickOneCard());
         }
     }
 
-    private void progressGame(Gamers gamers, CardPack cardPack) {
-        progressPlayersGame(gamers.callPlayers(), cardPack);
-        progressDealerGame(gamers.callDealer(), cardPack);
-        resultView.printAllGamersCardsResult(gamers);
-    }
-
-    private void progressPlayersGame(List<Player> players, CardPack cardPack) {
-        for (Gamer player : players) {
-            progressPlayerGame(player, cardPack);
-        }
-    }
-
-    private void progressPlayerGame(Gamer player, CardPack cardPack) {
-        while (player.isNotBust()) {
-            HitOption hitOption = new HitOption(inputView.readHitOrNot(player));
-            if (!hitOption.doHit()) {
-                break;
-            }
+    public boolean hitByPlayer(String rawHitOption, Gamer player) {
+        HitOption hitOption = new HitOption(rawHitOption);
+        if (hitOption.doHit()) {
             player.hit(cardPack.pickOneCard());
-            resultView.printPlayerCards(player);
+            return true;
         }
+        return false;
     }
 
-    private void progressDealerGame(Dealer dealer, CardPack cardPack) {
-        while (dealer.canHit()) {
-            Card pickedCard = cardPack.pickOneCard();
-            dealer.hit(pickedCard);
-            resultView.printDealerHitMessage(dealer, pickedCard);
-        }
+    public Card hitByDealer(Dealer dealer) {
+        Card pickedCard = cardPack.pickOneCard();
+        dealer.hit(pickedCard);
+        return pickedCard;
     }
 
-    private void makeFinalResult(Gamers gamers) {
-        Judge judge = new Judge();
+    public void makeFinalResult() {
         judge.decideResult(gamers);
-        resultView.printFinalResults(gamers.callDealer(), judge);
+    }
+
+    public Judge getJudge() {
+        return judge;
+    }
+
+    public Gamers getGamers() {
+        return gamers;
     }
 }
