efaebf6983664811f5ba5e1b2674597a1cebb9d9
ay-eonii <naayen@naver.com>
2024-03-10 21:09:42 +0900

    fix(InputView) : split limit 인자값 변경 빈 문자열 버려지지 않도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
 
     public List<String> readPlayerNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        String[] input = scanner.nextLine().split(",");
+        String[] input = scanner.nextLine().split(",", -1);
         return Arrays.stream(input).toList();
     }
 
