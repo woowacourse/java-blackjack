9e56e1d91a7c4641417451b520b96ae44e4666bb
libienz <leekh10195@gmail.com>
2024-03-10 15:12:41 +0900

    refactor: 상속 구조에 따른 일급컬렉션 클래스 변경

--- src/main/java/blackjack/domain/player/PlayerCreator.java
+++ src/main/java/blackjack/domain/player/PlayerCreator.java
@@ -12,11 +12,11 @@ public class PlayerCreator {
         this.handCreator = handCreator;
     }
 
-    public Participant createPlayerFrom(PlayerName playerName, CardDeck cardDeck) {
-        return new Participant(playerName, handCreator.createPlayerHandFrom(cardDeck));
+    public Player createPlayerFrom(PlayerName playerName, CardDeck cardDeck) {
+        return new Player(playerName, handCreator.createPlayerHandFrom(cardDeck));
     }
 
-    public Participant createDealerFrom(CardDeck cardDeck) {
-        return new Participant(new PlayerName(DEALER_NAME), handCreator.createDealerHandFrom(cardDeck));
+    public Dealer createDealerFrom(CardDeck cardDeck) {
+        return new Dealer(handCreator.createDealerHandFrom(cardDeck));
     }
 }
--- src/main/java/blackjack/domain/player/Players.java
+++ src/main/java/blackjack/domain/player/Players.java
@@ -8,55 +8,55 @@ public class Players {
 
     private static final int MAX_COUNT = 3;
 
-    private final List<Participant> participants;
+    private final List<Player> players;
 
-    public Players(List<Participant> participants) {
-        validate(participants);
-        this.participants = participants;
+    public Players(List<Player> players) {
+        validate(players);
+        this.players = players;
     }
 
     public int countPlayer() {
-        return participants.size();
+        return players.size();
     }
 
     public int countPlayerWithScoreAbove(Score target, ScoreCalculateStrategy scoreCalculateStrategy) {
-        return (int) participants.stream()
+        return (int) players.stream()
                 .map(Participant::calculateHandScore)
                 .filter(playerScore -> playerScore.isAbove(target))
                 .count();
     }
 
-    private void validate(List<Participant> participants) {
+    private void validate(List<Player> participants) {
         validateEachPlayerNameUnique(participants);
         validateEntryNotEmpty(participants);
         validatePlayerCountRange(participants);
     }
 
-    private void validateEachPlayerNameUnique(List<Participant> participants) {
+    private void validateEachPlayerNameUnique(List<Player> participants) {
         if (countUniquePlayer(participants) != participants.size()) {
             throw new IllegalArgumentException("[ERROR] 중복되는 플레이어의 이름이 존재합니다");
         }
     }
 
-    private int countUniquePlayer(List<Participant> participants) {
+    private int countUniquePlayer(List<Player> participants) {
         return (int) participants.stream()
                 .distinct()
                 .count();
     }
 
-    private void validateEntryNotEmpty(List<Participant> participants) {
+    private void validateEntryNotEmpty(List<Player> participants) {
         if (participants.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 플레이어가 없습니다");
         }
     }
 
-    private void validatePlayerCountRange(List<Participant> participants) {
+    private void validatePlayerCountRange(List<Player> participants) {
         if (participants.size() > MAX_COUNT) {
             throw new IllegalArgumentException("[ERROR] 플레이어의 수는 " + MAX_COUNT + "이하여야 합니다");
         }
     }
 
-    public List<Participant> getPlayers() {
-        return participants;
+    public List<Player> getPlayers() {
+        return players;
     }
 }
