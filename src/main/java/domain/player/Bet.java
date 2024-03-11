0a27042ab05a3c8eb8cb42863536973780f9dd46
ay-eonii <naayen@naver.com>
2024-03-11 14:35:13 +0900

    feat(Bet) : 베팅금액 도메인

---
+++ src/main/java/domain/player/Bet.java
@@ -0,0 +1,37 @@
+package domain.player;
+
+import java.util.Objects;
+
+public class Bet {
+
+    private static final int MIN_AMOUNT = 1;
+    private final int amount;
+
+    public Bet(int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        if (amount < MIN_AMOUNT) {
+            throw new IllegalArgumentException("최소 베팅 금액은 1원입니다.");
+        }
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bet revenue = (Bet) o;
+        return amount == revenue.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
