1b2852102a4aa5480881b1dc9553689a51df4973
SCY <helenason@naver.com>
2024-03-11 22:38:57 +0900

    refactor: 메인 함수 가독성 높이기 위해 메서드 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,27 +9,37 @@ import java.util.List;
 import view.InputView;
 import view.ResultView;
 
-public class Application { // TODO: refactoring
+public class Application {
 
     private static final InputView inputView = new InputView();
     private static final ResultView resultView = new ResultView();
 
     public static void main(String[] args) {
+        BlackJackGame game = startGame();
+
+        setUpGame(game);
+
+        progressGame(game);
+
+        makeResult(game);
+    }
+
+    private static BlackJackGame startGame() {
         List<String> rawPlayersNames = inputView.readPlayersNames();
+        return new BlackJackGame(rawPlayersNames);
+    }
 
-        BlackJackGame game = new BlackJackGame(rawPlayersNames);
-
-        Gamers gamers = game.getGamers();
-
+    private static void setUpGame(BlackJackGame game) {
         game.setUpGame();
+        Gamers gamers = game.getGamers();
         resultView.printInitialCards(gamers.getDealer(), gamers.getPlayers());
+    }
 
+    private static void progressGame(BlackJackGame game) {
+        Gamers gamers = game.getGamers();
         progressPlayersGame(game, gamers.getPlayers());
         progressDealerGame(game, gamers.getDealer());
         resultView.printGamersCardsScore(gamers);
-
-        Judge judge = game.makeFinalResult();
-        resultView.printFinalResult(gamers.getDealer(), judge);
     }
 
     private static void progressPlayersGame(BlackJackGame game, List<Player> players) {
@@ -58,5 +68,11 @@ public class Application { // TODO: refactoring
             Card pickedCard = game.hitByDealer(dealer);
             resultView.printDealerHitMessage(dealer, pickedCard);
         }
+    }
+
+    private static void makeResult(BlackJackGame game) {
+        Gamers gamers = game.getGamers();
+        Judge judge = game.makeFinalResult();
+        resultView.printFinalResult(gamers.getDealer(), judge);
     }
 }
