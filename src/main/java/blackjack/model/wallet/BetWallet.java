8f7b8ac5df413fcf362952d6cdfe8ffa931d6107
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-11 22:58:07 +0900

    feat(PlayerBetWallet): 도메인 구현 및 테스트 작성

--- src/main/java/blackjack/model/wallet/BetWallet.java
+++ src/main/java/blackjack/model/wallet/BetWallet.java
@@ -2,10 +2,15 @@ package blackjack.model.wallet;
 
 public abstract class BetWallet {
 
+    private static final int MIN_BET_AMOUNT = 100;
+    private static final int BET_AMOUNT_UNIT = 10;
+
     protected final int betAmount;
     protected int profitAmount;
 
     protected BetWallet(int betAmount) {
+        validateMinimumBetAmount(betAmount);
+        validateBetAmountUnit(betAmount);
         this.betAmount = betAmount;
         this.profitAmount = 0;
     }
@@ -16,5 +21,17 @@ public abstract class BetWallet {
 
     public int getProfitAmount() {
         return profitAmount;
+    }
+
+    private void validateMinimumBetAmount(int betAmount) {
+        if (betAmount < MIN_BET_AMOUNT) {
+            throw new IllegalArgumentException("[ERROR] 배팅은 100원부터 가능합니다.");
+        }
+    }
+
+    private void validateBetAmountUnit(int betAmount) {
+        if (betAmount % BET_AMOUNT_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 배팅은 10원 단위로만 가능합니다.");
+        }
     }
 }
