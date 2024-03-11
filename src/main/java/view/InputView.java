014181a7192ba032213145c6a3b493a012e01723
ay-eonii <naayen@naver.com>
2024-03-11 14:35:59 +0900

    feat(InputView) : 베팅금액 입력받기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -28,6 +28,16 @@ public class InputView {
         }
     }
 
+    public int readBets(Name name) {
+        try {
+            System.out.println(name + "의 배팅 금액은?");
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("숫자를 입력해주세요.");
+            return readBets(name);
+        }
+    }
+
     private enum Opinion {
         POSITIVE("y", true),
         NEGATIVE("n", false);
