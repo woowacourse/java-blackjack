1206f571c19cee35fba72fe9e58d026960cf3342
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-03-11 18:38:42 +0900

    feat(Command) : 뷰에서 입력을 확인 시 사용되는 문자 enum으로 분리

---
+++ src/main/java/view/Command.java
@@ -0,0 +1,16 @@
+package view;
+
+public enum Command {
+    YES("y"),
+    NO("n"),
+    ;
+
+    private String command;
+    Command(String command) {
+        this.command = command;
+    }
+
+    public boolean compareTo(String command) {
+        return this.command.equals(command);
+    }
+}
