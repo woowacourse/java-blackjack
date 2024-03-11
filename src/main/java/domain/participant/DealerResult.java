69f8fb1edd5e61bc80de5d84312ef4c08c47440a
yoonjuho <dbswn990@naver.com>
2024-03-11 15:59:49 +0900

    refactor(DealerResult): 외부에서 EnumMap 주입

--- src/main/java/domain/participant/DealerResult.java
+++ src/main/java/domain/participant/DealerResult.java
@@ -7,17 +7,13 @@ import domain.GameResultStatus;
 
 public class DealerResult {
 
-    private final EnumMap<GameResultStatus, Integer> result;
+    private final Map<GameResultStatus, Integer> result;
 
-    public DealerResult(final EnumMap<GameResultStatus, Integer> result) {
+    public DealerResult(final Map<GameResultStatus, Integer> result) {
         this.result = new EnumMap<>(result);
     }
 
     public void put(final GameResultStatus resultStatus) {
         result.put(resultStatus, result.getOrDefault(resultStatus, 0) + 1);
-    }
-
-    public Map<GameResultStatus, Integer> getResult() {
-        return Map.copyOf(result);
     }
 }
