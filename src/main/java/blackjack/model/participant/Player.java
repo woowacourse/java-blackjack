fbb3b76ced39e831ce7f9472ba267c80f58fd0af
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-11 14:40:52 +0900

    refactor(Player): equals, hashCode() 제거

--- src/main/java/blackjack/model/participant/Player.java
+++ src/main/java/blackjack/model/participant/Player.java
@@ -2,7 +2,6 @@ package blackjack.model.participant;
 
 import blackjack.model.deck.Card;
 import java.util.List;
-import java.util.Objects;
 
 public class Player {
     private static final int HITTABLE_THRESHOLD = 21;
@@ -45,22 +44,5 @@ public class Player {
 
     public String getName() {
         return name.getValue();
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Player player = (Player) o;
-        return Objects.equals(name, player.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
     }
 }
