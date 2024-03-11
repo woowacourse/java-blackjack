f464b0d4590128468cfe8145ecb399b689dfaaff
SungKyum Kim <sungkyum1@naver.com>
2024-03-11 19:26:59 +0900

    refactor: 변수명 일관성 개선

--- src/main/java/domain/Blackjack.java
+++ src/main/java/domain/Blackjack.java
@@ -33,8 +33,8 @@ public class Blackjack {
 
 
     public BlackjackResult finishGame() {
-        final BlackjackRule blackjackResult = new BlackjackRule();
-        return blackjackResult.finishGame(players.getPlayers(), dealer);
+        final BlackjackRule blackjackRule = new BlackjackRule();
+        return blackjackRule.finishGame(players.getPlayers(), dealer);
     }
 
     public List<Player> getPlayers() {
