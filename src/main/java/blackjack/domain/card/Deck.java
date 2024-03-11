4d8727c71cc95ba57ebfdf607c648ebe2036da7c
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-03-11 15:10:45 +0900

    feat: 카드 덱에 카드가 다 떨어졌을 때 카드를 뽑을 경우 예외 처리

--- src/main/java/blackjack/domain/card/Deck.java
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -4,6 +4,7 @@ import java.util.Stack;
 
 public class Deck {
 
+    static final String ERROR_DECK_IS_EMPTY = "더이상 카드를 뽑을 수 없습니다.";
     private final Stack<Card> deck;
 
     public Deck(final DeckFactory deckFactory) {
@@ -11,6 +12,13 @@ public class Deck {
     }
 
     public Card pop() {
+        if (deck.isEmpty()) {
+            throw new IllegalStateException(ERROR_DECK_IS_EMPTY);
+        }
         return deck.pop();
+    }
+
+    Stack<Card> getDeck() {
+        return deck;
     }
 }
--- src/test/java/blackjack/domain/card/DeckTest.java
+++ src/test/java/blackjack/domain/card/DeckTest.java
@@ -1,6 +1,7 @@
 package blackjack.domain.card;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,5 +16,20 @@ class DeckTest {
         Card card = deck.pop();
 
         assertThat(card).isEqualTo(new Card(Number.KING, Suit.DIAMOND));
+    }
+
+    @DisplayName("카드 덱에 카드가 다 떨어졌을 때 카드를 뽑으면 예외가 발생한다.")
+    @Test
+    void occurExceptionWhenDeckIsEmpty() {
+        Deck deck = new Deck(new TestDeckFactory());
+
+        int size = deck.getDeck().size();
+        for (int i=0; i<size; i++) {
+            deck.pop();
+        }
+
+        assertThatThrownBy(() -> deck.pop())
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage(Deck.ERROR_DECK_IS_EMPTY);
     }
 }
