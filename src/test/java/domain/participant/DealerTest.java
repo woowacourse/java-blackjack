2a6049c9e3c6b225de83b4d75695ea4e5d01c2ce
unifolio0 <boho82ha@gmail.com>
2024-03-11 14:48:47 +0900

    test : WinStatus 반환 테스트 추가

--- src/test/java/domain/participant/DealerTest.java
+++ src/test/java/domain/participant/DealerTest.java
@@ -31,7 +31,7 @@ class DealerTest {
 
         Assertions.assertThat(dealer.shouldHit()).isFalse();
     }
-    
+
     @DisplayName("참가자와 딜러 중 점수가 높은 사람이 승리한다.")
     @Test
     void isWinner() {
@@ -62,5 +62,37 @@ class DealerTest {
 
         WinStatus winStatus = dealer.isWinner(participant);
         assertThat(winStatus).isEqualTo(WinStatus.LOSE);
+    }
+
+    @DisplayName("참가자와 딜러 모두 블랙잭인 경우 무승부(DRAW)로 처리한다.")
+    @Test
+    void isWinnerWhenAllBlackJack() {
+        Dealer dealer = new Dealer();
+
+        Participant participant = new Participant(new Name("one"));
+        participant.receiveCard(new Card(Shape.HEART, Rank.KING));
+        participant.receiveCard(new Card(Shape.DIA, Rank.ACE));
+
+        dealer.receiveCard(new Card(Shape.HEART, Rank.QUEEN));
+        dealer.receiveCard(new Card(Shape.HEART, Rank.ACE));
+
+        WinStatus winStatus = dealer.isWinner(participant);
+        assertThat(winStatus).isEqualTo(WinStatus.DRAW);
+    }
+
+    @DisplayName("참가자만 블랙잭인 경우 BlackJack으로 처리한다.")
+    @Test
+    void isWinnerWhenParticipantBlackJack() {
+        Dealer dealer = new Dealer();
+
+        Participant participant = new Participant(new Name("one"));
+        participant.receiveCard(new Card(Shape.HEART, Rank.KING));
+        participant.receiveCard(new Card(Shape.DIA, Rank.ACE));
+
+        dealer.receiveCard(new Card(Shape.HEART, Rank.QUEEN));
+        dealer.receiveCard(new Card(Shape.HEART, Rank.TWO));
+
+        WinStatus winStatus = dealer.isWinner(participant);
+        assertThat(winStatus).isEqualTo(WinStatus.BLACKJACK);
     }
 }
