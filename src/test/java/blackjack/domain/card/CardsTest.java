47717c3613dacd029756fc141d44a3ebd69062f9
HoeSeong123 <ghltjd3@gmail.com>
2024-03-11 17:43:57 +0900

    refactor : 테스트에서 경계값을 이용하도록 변경

--- src/test/java/blackjack/domain/card/CardsTest.java
+++ src/test/java/blackjack/domain/card/CardsTest.java
@@ -67,21 +67,21 @@ public class CardsTest {
     @Test
     @DisplayName("숫자 합이 21이 넘지 않으면 플레이어는 에이스를 11로 결정한다.")
     public void GamePlayer_Determine_ace_is_11_if_not_exceed_21() {
-        var sut = CardFixture.카드_목록_생성(List.of(CardValue.EIGHT, CardValue.ACE));
+        var sut = CardFixture.카드_목록_생성(List.of(CardValue.TEN, CardValue.ACE));
 
         var result = sut.calculateScore();
 
-        assertThat(result).isEqualTo(19);
+        assertThat(result).isEqualTo(21);
     }
 
     @Test
     @DisplayName("숫자 합이 21이 넘으면 플레이어는 에이스를 1로 결정한다.")
     public void GamePlayer_Determine_ace_is_1_if_exceed_21() {
-        var sut = CardFixture.카드_목록_생성(List.of(CardValue.EIGHT, CardValue.TEN, CardValue.ACE));
+        var sut = CardFixture.카드_목록_생성(List.of(CardValue.EIGHT, CardValue.TEN, CardValue.ACE, CardValue.TWO));
 
         var result = sut.calculateScore();
 
-        assertThat(result).isEqualTo(19);
+        assertThat(result).isEqualTo(21);
     }
 
     @Test
