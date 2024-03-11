f26f1b87f8302cb3f4d71ad1ec72d308a6d14fc0
masonkimseoul <masonkimseoul@gmail.com>
2024-03-12 01:40:58 +0900

    refactor: CsvSource를 사용하도록 수정

--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -4,32 +4,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CardTest {
     @DisplayName("올바르게 카드의 Number와 Shape를 반환한다.")
-    @Test
-    void getSymbolAndShape() {
-        Number number = Number.EIGHT;
-        Shape shape = Shape.CLOVER;
+    @ParameterizedTest
+    @CsvSource({
+            "EIGHT, CLOVER, 8클로버",
+    })
+    void getSymbolAndShape(Number number, Shape shape, String expected) {
         Card card = new Card(number, shape);
-        assertThat(card.getSymbol() + card.getShape()).isEqualTo("8클로버");
+        assertThat(card.getSymbol() + card.getShape()).isEqualTo(expected);
     }
 
     @DisplayName("올바르게 점수를 반환한다.")
-    @Test
-    void getScore() {
-        Number number = Number.FIVE;
-        Shape shape = Shape.CLOVER;
+    @ParameterizedTest
+    @CsvSource({
+            "FIVE, HEART, 5",
+    })
+    void getScore(Number number, Shape shape, Integer expected) {
         Card card = new Card(number, shape);
-        assertThat(card.getScore()).isEqualTo(5);
+        assertThat(card.getScore()).isEqualTo(expected);
     }
 
     @DisplayName("에이스를 판별한다")
-    @Test
-    void isAce() {
-        Number number = Number.ACE;
-        Shape shape = Shape.HEART;
+    @ParameterizedTest
+    @CsvSource({
+            "ACE, DIAMOND"
+    })
+    void isAce(Number number, Shape shape) {
         Card card = new Card(number, shape);
         assertTrue(card.isAce());
     }
