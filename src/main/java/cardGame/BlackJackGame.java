aeabaf081cadb92f4272bf73ebc5031a22330873
Eugene Jang <kyunellroll@gmail.com>
2024-03-11 17:11:16 +0900

    refactor(BlackJackGame): 블랙잭 게임의 전체를 책임지도록 필드 CardDeck 추가, Players 삭제
    - 이름을 받아 카드 두개를 주고 GamePlayer로 만드는 책임 부여
    - 딜러가 가진 카드중 첫번째 카드만 가져오는 메서드 생성
    - 함수를 받아, 각 Player와의 Match를 만들고 함수를 전달하는 역할 생성
    - Dealer Turn을 끝마치고 점수를 반환하는 메서드 생성
    - 각 플레이어와 딜러의 점수를 담아서 주도록 메서드 생성

--- src/main/java/cardGame/BlackJackGame.java
+++ src/main/java/cardGame/BlackJackGame.java
@@ -1,61 +1,62 @@
 package cardGame;
 
-import cardGame.dto.BlackJackGameStatusDto;
-import cardGame.dto.ParticipantsTotalGameResultDto;
+import card.Card;
+import card.CardDeck;
+import controller.dto.SinglePlayerResultDto;
 import dealer.Dealer;
+import dealer.dto.DealerGameResult;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import player.Name;
 import player.Player;
 import player.Players;
+import player.dto.SinglePlayerStatusDto;
 
 public class BlackJackGame {
 
-    private final Players players;
+    private static final String DEALER_NAME = "딜러";
+
     private final Dealer dealer;
+    private final CardDeck cardDeck;
 
-    public BlackJackGame(List<String> names) {
-        this.dealer = new Dealer();
-        this.players = initGamePlayer(names);
+    public BlackJackGame() {
+        this.cardDeck = new CardDeck();
+        this.dealer = new Dealer(cardDeck);
     }
 
-    private Players initGamePlayer(List<String> names) {
-        List<Name> playerNames = names.stream()
-                .map(Name::new)
-                .toList();
-
-        return initPlayersCard(playerNames);
+    public Players initGamePlayer(List<String> names) {
+        return Players.from(names, cardDeck);
     }
 
-    private Players initPlayersCard(List<Name> playerNames) {
-        Players players = Players.from(playerNames);
+    public Card getInitDealerStatus() {
+        return dealer.getCards().getFirstCard();
+    }
+
+    public List<SinglePlayerStatusDto> playGame(BiConsumer<SingleMatch, CardDeck> playSingleMatch, Players players) {
+        List<SinglePlayerStatusDto> playerResult = new ArrayList<>();
 
         for (Player player : players.getPlayers()) {
-            player.receiveCard(dealer.giveCard());
-            player.receiveCard(dealer.giveCard());
+            SingleMatch singleMatch = new SingleMatch(player);
+            playerResult.add(singleMatch.play(playSingleMatch, cardDeck));
         }
-        return players;
+
+        return playerResult;
     }
 
-    public List<SingleMatch> readySingleMatches() {
-        ArrayList<SingleMatch> matches = new ArrayList<>();
-
-        for (Player player : players.getPlayers()) {
-            matches.add(new SingleMatch(player, dealer));
-        }
-        return matches;
+    public SinglePlayerStatusDto playDealerTurn() {
+        dealer.getExtraCard(cardDeck);
+        return new SinglePlayerStatusDto(new Name(DEALER_NAME), dealer.getCards());
     }
 
-    public BlackJackGameStatusDto getBackJackGameStatus() {
-        return BlackJackGameStatusDto.of(players, dealer);
+    public List<SinglePlayerResultDto> getPlayersResult(Players players) {
+        return players.getPlayers().stream()
+                .map(player -> new SinglePlayerResultDto(player.getName(), player.isWinner(dealer.getCardScore())))
+                .collect(Collectors.toList());
     }
 
-    public ParticipantsTotalGameResultDto getParticipantScore() {
-        return ParticipantsTotalGameResultDto.of(players, dealer);
-    }
-
-    public int countDealerExtraCard() {
-        dealer.checkNeedExtraCard();
-        return dealer.countExtraCard();
+    public DealerGameResult getDealerResult(Players players) {
+        return dealer.getWinningResult(players);
     }
 }
