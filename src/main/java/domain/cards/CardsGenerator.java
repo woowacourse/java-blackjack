252fef148671c7df6c02836a113af55eed8cd521
yechop <channetattoo@gmail.com>
2024-03-11 14:39:53 +0900

    refactor: CardsGenerator 인덴트 깊이 1로 수정

--- src/main/java/domain/cards/CardsGenerator.java
+++ src/main/java/domain/cards/CardsGenerator.java
@@ -12,11 +12,15 @@ public class CardsGenerator {
     public static List<Card> generateRandomCards() {
         List<Card> cards = new ArrayList<>();
         for (CardShape cardShape : CardShape.values()) {
-            for (CardNumber cardNumber : CardNumber.values()) {
-                cards.add(new Card(cardNumber, cardShape));
-            }
+            addCardWithCardNumber(cardShape, cards);
         }
         Collections.shuffle(cards);
         return cards;
+    }
+
+    private static void addCardWithCardNumber(CardShape cardShape, List<Card> cards) {
+        for (CardNumber cardNumber : CardNumber.values()) {
+            cards.add(new Card(cardNumber, cardShape));
+        }
     }
 }
