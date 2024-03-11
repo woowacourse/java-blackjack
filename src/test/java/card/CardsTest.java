274d8723b4945c5e6cde69d005d92eecea160f8b
Eugene Jang <kyunellroll@gmail.com>
2024-03-11 16:57:59 +0900

    refactor(CardsTest): ACE 최대 점수 합산 가능 여부에 따른 return 값 테스트 추가
    - 최솟값 상수 선언

--- src/test/java/card/CardsTest.java
+++ src/test/java/card/CardsTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 
 class CardsTest {
 
+    private static final int ADDITIONAL_ACE_CARD_SCORE = CardNumber.ACE.getAdditionalScore();
+
     @DisplayName("해당 라운드에서 카드 점수의 최솟값이 21을 넘는지 확인한다.")
     @Test
     void countAceCard() {
@@ -24,5 +26,28 @@ class CardsTest {
                 new Card(CardNumber.EIGHT, CardPattern.SPADE_PATTERN)));
 
         Assertions.assertThat(cards.countMaxScore()).isEqualTo(20);
+    }
+
+    @DisplayName("추가 ACE 점수가 더해질 때 Bust 일 경우 ACE가 1인 경우를 계산한 값을 return 한다.")
+    @Test
+    void notConvertToAdditionalAceCardScore() {
+        Cards cards = new Cards(List.of(new Card(CardNumber.ACE, CardPattern.CLOVER_PATTERN),
+                new Card(CardNumber.KING, CardPattern.DIA_PATTERN),
+                new Card(CardNumber.QUEEN, CardPattern.DIA_PATTERN)));
+
+        int matchScore = cards.countMatchScore();
+
+        Assertions.assertThat(cards.countMaxScore()).isEqualTo(matchScore);
+    }
+
+    @DisplayName("추가 ACE 점수가 더해질 때 Bust가 아닐 경우 ACE가 11인 경우를 계산한 값을 return 한다.")
+    @Test
+    void convertToAdditionalAceCardScore() {
+        Cards cards = new Cards(List.of(new Card(CardNumber.ACE, CardPattern.CLOVER_PATTERN),
+                new Card(CardNumber.FOUR, CardPattern.DIA_PATTERN)));
+
+        int matchScore = cards.countMatchScore();
+
+        Assertions.assertThat(cards.countMaxScore()).isEqualTo(matchScore + ADDITIONAL_ACE_CARD_SCORE);
     }
 }
