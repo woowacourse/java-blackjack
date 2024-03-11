d164f3b646b96b0d2a7a45ad78515e78061d6083
kunsanglee <gosmdochee2@gmail.com>
2024-03-11 16:33:46 +0900

    refactor: CardFactory 블랙잭 카드 만드는 메서드 분리

--- src/main/java/blackjack/domain/CardFactory.java
+++ src/main/java/blackjack/domain/CardFactory.java
@@ -3,13 +3,18 @@ package blackjack.domain;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CardFactory {
 
     public List<Card> createBlackJackCard() {
         return Arrays.stream(CardSuit.values())
-                .flatMap(suit -> Arrays.stream(CardNumber.values())
-                        .map(number -> new Card(suit, number)))
+                .flatMap(this::createCardWith)
                 .collect(Collectors.toList());
+    }
+
+    private Stream<Card> createCardWith(CardSuit suit) {
+        return Arrays.stream(CardNumber.values())
+                .map(number -> new Card(suit, number));
     }
 }
