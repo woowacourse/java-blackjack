78e94fd90e73099359ebeafd44dccc4c1ba834ad
3juhwan <13selfesteem91@naver.com>
2024-03-11 14:42:45 +0900

    test: Deck에 52장의 카드가 있는지 검증하는 테스트

--- src/test/java/blackjack/domain/cardgame/CardDeckTest.java
+++ src/test/java/blackjack/domain/cardgame/CardDeckTest.java
@@ -4,6 +4,7 @@ import blackjack.domain.card.Card;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CardDeckTest {
 
@@ -14,5 +15,18 @@ public class CardDeckTest {
         Card card = deck.draw();
 
         assertThat(card).isNotNull();
+    }
+
+    @Test
+    void 덱에_정확히_52장의_카드가_존재한다() {
+        CardDeck deck = CardDeck.create();
+
+        for (int ignored = 0; ignored < 52; ignored++) {
+            deck.draw();
+        }
+
+        assertThatThrownBy(deck::draw)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("카드가 존재하지 않습니다.");
     }
 }
