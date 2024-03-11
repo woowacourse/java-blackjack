c99586985cdf2198281cceea8ba29aecc7ed0ac2
DobbyMac <kimdobby@g.skku.edu>
2024-03-11 16:09:00 +0900

    feat(Name): '딜러'와 같거나 '딜러'를 포함한 이름을 가진 사용자명 검증 기능 구현

--- src/main/java/model/participant/Name.java
+++ src/main/java/model/participant/Name.java
@@ -13,6 +13,7 @@ public class Name {
     private void validate(String inputName) {
         validateBlank(inputName);
         validateContainBlankInName(inputName);
+        validateAllowedName(inputName);
     }
 
     private void validateBlank(String initialInput) {
@@ -24,6 +25,12 @@ public class Name {
     private void validateContainBlankInName(String inputName) {
         if (inputName.contains(" ")) {
             throw new IllegalArgumentException("중간에 공백이 포함된 값은 입력할 수 없습니다.");
+        }
+    }
+
+    private void validateAllowedName(String inputName){
+        if (inputName.contains("딜러")){
+            throw new IllegalArgumentException("'딜러'가 포함된 사용자명은 사용할 수 없습니다.");
         }
     }
 
