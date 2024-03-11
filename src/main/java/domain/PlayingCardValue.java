d86289d5ea23ff7f21f5a39cceadaadd2ea807bd
HyunJoong Kim <hjk0761@naver.com>
2024-03-11 14:39:34 +0900

    refactor: 매직 넘버 상수화 및 변수명이 의미를 가지도록 수정

--- src/main/java/domain/PlayingCardValue.java
+++ src/main/java/domain/PlayingCardValue.java
@@ -5,15 +5,16 @@ import static domain.constant.GameOption.BLACKJACK_CONDITION;
 public enum PlayingCardValue {
     TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
 
+    private static final int ACE_GAP = 10;
     private final int value;
 
     PlayingCardValue(final int value) {
         this.value = value;
     }
 
-    public int getValue(final int inputValue) {
-        if (this == ACE && inputValue + this.value > BLACKJACK_CONDITION) {
-            return ACE.value - 10;
+    public int getValue(final int previousSum) {
+        if (this == ACE && previousSum + this.value > BLACKJACK_CONDITION) {
+            return ACE.value - ACE_GAP;
         }
         return value;
     }
