f30962b8e5a9cea86ced7da09dc5f46506961283
hoyeonyy <highoption@naver.com>
2024-03-11 20:47:42 +0900

    refactor(Dealer) : 딜러 메서드 수정 및 생성

---
+++ src/main/java/domain/gamer/Dealer.java
@@ -0,0 +1,44 @@
+package domain.gamer;
+
+import domain.card.Card;
+import domain.card.CardDeck;
+import domain.card.Cards;
+import java.util.List;
+
+public class Dealer {
+
+    private static final int MIN_DEALER_SCORE = 16;
+    private final CardDeck cardDeck;
+    private final Cards cards;
+
+    public Dealer() {
+        cards = new Cards();
+        this.cardDeck = new CardDeck();
+        initDealerCard();
+    }
+
+    private void initDealerCard() {
+        cards.addCard(dealCard());
+    }
+
+    public Card dealCard() {
+        cardDeck.shuffleCard();
+        return cardDeck.pickCard();
+    }
+
+    public void receiveCard() {
+        cards.addCard(dealCard());
+    }
+
+    public boolean isDrawable() {
+        return cards.getMaxGameScore() <= MIN_DEALER_SCORE;
+    }
+
+    public int getMaxGameScore() {
+        return cards.countMaxScore();
+    }
+
+    public List<String> getCardStatus() {
+        return cards.getCardsName();
+    }
+}
