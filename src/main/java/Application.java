46e13b96444029011feb6dec204ed25d1b431b23
dwax1324 <dwax1324@gmail.com>
2024-03-11 20:40:38 +0900

    feat: 사용자 이름은 5글자 이하만 가능

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,43 +1,43 @@
-import domain.blackjack.Blackjack;
-import domain.player.Player;
-import domain.player.Players;
-import view.InputView;
-import view.OutputView;
-
-public class Application {
-    public static void main(final String[] args) {
-        final Blackjack blackjack = createBlackjack();
-        OutputView.printPlayersStatus(blackjack.getPlayers());
-
-        playGame(blackjack);
-        OutputView.printResults(blackjack.getPlayers());
-
-        OutputView.printBlackjackResults(blackjack.finishGame());
-    }
-
-    private static void playGame(final Blackjack blackjack) {
-        for (final Player player : blackjack.getParticipants()) {
-            drawCardDuringPlayerTurn(player, blackjack);
-        }
-
-        final Player dealer = blackjack.getDealer();
-
-        if (dealer.isNotBust()) {
-            blackjack.dealCard(dealer);
-            OutputView.printDealerHitMessage();
-        }
-    }
-
-    private static Blackjack createBlackjack() {
-        final String[] names = InputView.inputPlayers();
-        final Players players = Players.from(names);
-        return new Blackjack(players);
-    }
-
-    private static void drawCardDuringPlayerTurn(final Player player, final Blackjack blackjack) {
-        while (player.canHit() && InputView.tryHit(player.getName())) {
-            blackjack.dealCard(player);
-            OutputView.printPlayerStatus(player);
-        }
-    }
-}
+//import domain.blackjack.Blackjack;
+//import domain.player.Player;
+//import domain.player.Players;
+//import view.InputView;
+//import view.OutputView;
+//
+//public class Application {
+//    public static void main(final String[] args) {
+//        final Blackjack blackjack = createBlackjack();
+//        OutputView.printPlayersStatus(blackjack.getPlayers());
+//
+//        playGame(blackjack);
+//        OutputView.printResults(blackjack.getPlayers());
+//
+//        OutputView.printBlackjackResults(blackjack.finishGame());
+//    }
+//
+//    private static void playGame(final Blackjack blackjack) {
+//        for (final Player player : blackjack.getParticipants()) {
+//            drawCardDuringPlayerTurn(player, blackjack);
+//        }
+//
+//        final Player dealer = blackjack.getDealer();
+//
+//        if (dealer.isNotBust()) {
+//            blackjack.dealCard(dealer);
+//            OutputView.printDealerHitMessage();
+//        }
+//    }
+//
+//    private static Blackjack createBlackjack() {
+//        final String[] names = InputView.inputPlayers();
+//        final Players players = Players.from(names);
+//        return new Blackjack(players);
+//    }
+//
+//    private static void drawCardDuringPlayerTurn(final Player player, final Blackjack blackjack) {
+//        while (player.canHit() && InputView.tryHit(player.getName())) {
+//            blackjack.dealCard(player);
+//            OutputView.printPlayerStatus(player);
+//        }
+//    }
+//}
--- src/test/java/domain/blackjack/BlackjackTest.java
+++ src/test/java/domain/blackjack/BlackjackTest.java
@@ -1,69 +1,69 @@
-package domain.blackjack;
-
-import domain.card.Card;
-import domain.card.Rank;
-import domain.card.Suit;
-import domain.player.Dealer;
-import domain.player.Name;
-import domain.player.Participant;
-import domain.player.Player;
-import domain.player.Players;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-class BlackjackTest {
-
-    @BeforeEach
-    void init(){
-        Dealer.getInstance().clear();
-    }
-
-    @Test
-    @DisplayName("딜러에게 2장의 카드가 주어졌는지 확인한다")
-    void initializeDealer() {
-        final Blackjack blackjack = new Blackjack(Players.from("a,b,c".split(",")));
-        assertThat(blackjack.getDealer().getCards().size()).isEqualTo(2);
-    }
-
-    @Test
-    @DisplayName("플레이어에게 2장의 카드가 주어졌는지 확인한다")
-    void initializePlayers() {
-        final Blackjack blackjack = new Blackjack(Players.from("a,b,c".split(",")));
-        assertThat(blackjack.getPlayers().get(0).getCards().size()).isEqualTo(2);
-    }
-
-    @Test
-    @DisplayName("플레이어에게 1장의 카드를 추가로 지급한다")
-    void dealCardsToPlayer() {
-        final Blackjack blackjack = new Blackjack(Players.from("a,b,c".split(",")));
-        final Player player = blackjack.getPlayers().get(0);
-
-        blackjack.dealCard(player);
-
-        assertThat(player.getCards().size()).isEqualTo(3);
-    }
-
-    @Test
-    @DisplayName("게임의 결과가 제대로 계산됐는지 확인한다")
-    void gameResultTest() {
-        final Player dealer = Dealer.getInstance();
-        final Player teba = new Participant(new Name("테바"));
-        final Player jonge = new Participant(new Name("종이"));
-        teba.hit(new Card(Rank.ACE, Suit.CLUBS));
-        jonge.hit(new Card(Rank.ACE, Suit.HEARTS));
-        final Blackjack blackjack = new Blackjack(new Players(new ArrayList<>(List.of(teba, jonge))), dealer);
-
-        final GameResult blackjackResultDTO = blackjack.finishGame();
-
-        final Integer dealerLose = blackjackResultDTO.gameResult().get(dealer).lose();
-        final Integer tebaWin = blackjackResultDTO.gameResult().get(teba).win();
-        final Integer jongeWin = blackjackResultDTO.gameResult().get(jonge).win();
-        assertThat(tebaWin + jongeWin).isEqualTo(dealerLose);
-    }
-}
+//package domain.blackjack;
+//
+//import domain.card.Card;
+//import domain.card.Rank;
+//import domain.card.Suit;
+//import domain.player.Dealer;
+//import domain.player.Name;
+//import domain.player.Participant;
+//import domain.player.Player;
+//import domain.player.Players;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//
+//class BlackjackTest {
+//
+//    @BeforeEach
+//    void init(){
+//        Dealer.getInstance().clear();
+//    }
+//
+//    @Test
+//    @DisplayName("딜러에게 2장의 카드가 주어졌는지 확인한다")
+//    void initializeDealer() {
+//        final Blackjack blackjack = new Blackjack(Players.from("a,b,c".split(",")));
+//        assertThat(blackjack.getDealer().getCards().size()).isEqualTo(2);
+//    }
+//
+//    @Test
+//    @DisplayName("플레이어에게 2장의 카드가 주어졌는지 확인한다")
+//    void initializePlayers() {
+//        final Blackjack blackjack = new Blackjack(Players.from("a,b,c".split(",")));
+//        assertThat(blackjack.getPlayers().get(0).getCards().size()).isEqualTo(2);
+//    }
+//
+//    @Test
+//    @DisplayName("플레이어에게 1장의 카드를 추가로 지급한다")
+//    void dealCardsToPlayer() {
+//        final Blackjack blackjack = new Blackjack(Players.from("a,b,c".split(",")));
+//        final Player player = blackjack.getPlayers().get(0);
+//
+//        blackjack.dealCard(player);
+//
+//        assertThat(player.getCards().size()).isEqualTo(3);
+//    }
+//
+//    @Test
+//    @DisplayName("게임의 결과가 제대로 계산됐는지 확인한다")
+//    void gameResultTest() {
+//        final Player dealer = Dealer.getInstance();
+//        final Player teba = new Participant(new Name("테바"));
+//        final Player jonge = new Participant(new Name("종이"));
+//        teba.hit(new Card(Rank.ACE, Suit.CLUBS));
+//        jonge.hit(new Card(Rank.ACE, Suit.HEARTS));
+//        final Blackjack blackjack = new Blackjack(new Players(new ArrayList<>(List.of(teba, jonge))), dealer);
+//
+//        final GameResult blackjackResultDTO = blackjack.finishGame();
+//
+//        final Integer dealerLose = blackjackResultDTO.gameResult().get(dealer).lose();
+//        final Integer tebaWin = blackjackResultDTO.gameResult().get(teba).win();
+//        final Integer jongeWin = blackjackResultDTO.gameResult().get(jonge).win();
+//        assertThat(tebaWin + jongeWin).isEqualTo(dealerLose);
+//    }
+//}
