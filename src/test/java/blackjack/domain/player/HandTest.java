e23c9f53939919f422eb84ba3b66e54bceebd9ba
3juhwan <13selfesteem91@naver.com>
2024-03-11 16:14:36 +0900

    test: Hand 클래스 테스트

--- src/test/java/blackjack/domain/player/HandTest.java
+++ src/test/java/blackjack/domain/player/HandTest.java
@@ -1,9 +1,9 @@
 package blackjack.domain.player;
 
 import blackjack.domain.card.Card;
-import blackjack.domain.player.Hand;
 import org.junit.jupiter.api.Test;
 
+import static blackjack.domain.card.CardNumber.ACE;
 import static blackjack.domain.card.CardNumber.JACK;
 import static blackjack.domain.card.CardNumber.KING;
 import static blackjack.domain.card.CardNumber.QUEEN;
@@ -18,8 +18,32 @@ public class HandTest {
         hand.add(new Card(QUEEN, SPADE));
         hand.add(new Card(KING, SPADE));
 
-        int sum = hand.getSum();
+        int sum = hand.getScore();
 
         assertThat(sum).isEqualTo(30);
+    }
+
+    @Test
+    void 에이스_카드가_4개인_경우에_합계를_구할_수_있다() {
+        Hand hand = new Hand();
+        hand.add(new Card(ACE, SPADE));
+        hand.add(new Card(ACE, SPADE));
+        hand.add(new Card(ACE, SPADE));
+        hand.add(new Card(ACE, SPADE));
+
+        int sum = hand.getScore();
+
+        assertThat(sum).isEqualTo(14);
+    }
+
+    @Test
+    void 에이스_카드를_포함해서_합계_21인_경우에_정확하게_계산할_수_있다() {
+        Hand hand = new Hand();
+        hand.add(new Card(ACE, SPADE));
+        hand.add(new Card(KING, SPADE));
+
+        int sum = hand.getScore();
+
+        assertThat(sum).isEqualTo(21);
     }
 }
