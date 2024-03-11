5ef034306ecb69ab13c10e6cf40d3d69d2d61db4
robinjoon <robin980108@naver.com>
2024-03-11 14:19:10 +0900

    refactor: 테스트 코드에서 사용하는 테스트 객체 생성 역할을 분리

--- src/test/java/domain/blackjack/GameResultCalculatorTest.java
+++ src/test/java/domain/blackjack/GameResultCalculatorTest.java
@@ -3,14 +3,12 @@ package domain.blackjack;
 import static domain.blackjack.GameResult.LOSE;
 import static domain.blackjack.GameResult.TIE;
 import static domain.blackjack.GameResult.WIN;
-import static domain.card.Card.ACE_HEART;
-import static domain.card.Card.JACK_HEART;
-import static domain.card.Card.NINE_HEART;
-import static domain.card.Card.QUEEN_HEART;
-import static domain.card.Card.SEVEN_HEART;
-import static domain.card.Card.SIX_DIAMOND;
-import static domain.card.Card.SIX_HEART;
-import static domain.card.Card.TWO_HEART;
+import static domain.blackjack.TestHoldingCards.DEAD_CARDS;
+import static domain.blackjack.TestHoldingCards.ONLY_SEVEN_HEART;
+import static domain.blackjack.TestHoldingCards.ONLY_SIX_HEART;
+import static domain.blackjack.TestHoldingCards.TWO_SIX_CARDS;
+import static domain.blackjack.TestHoldingCards.WIN_CARDS_WITHOUT_ACE;
+import static domain.blackjack.TestHoldingCards.WIN_CARDS_WITH_ACE;
 
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
@@ -20,15 +18,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class GameResultCalculatorTest {
-    private static final HoldingCards ONLY_SIX_HEART = HoldingCards.of(SIX_HEART);
-    private static final HoldingCards ONLY_SEVEN_HEART = HoldingCards.of(SEVEN_HEART);
-    private static final HoldingCards DEAD_CARDS = HoldingCards.of(TWO_HEART, JACK_HEART, QUEEN_HEART);
-
-    private static final HoldingCards WIN_CARDS_WITH_ACE = HoldingCards.of(ACE_HEART, QUEEN_HEART);
-
-    private static final HoldingCards WIN_CARDS_WITHOUT_ACE = HoldingCards.of(TWO_HEART, JACK_HEART, NINE_HEART);
-
-    private static final HoldingCards TWO_SIX_CARDS = HoldingCards.of(SIX_HEART, SIX_DIAMOND);
 
     public static Stream<Arguments> getGameResultParameters() {
         return Stream.of(
--- src/test/java/domain/card/FirstCardSelectStrategy.java
+++ src/test/java/domain/card/FirstCardSelectStrategy.java
@@ -3,6 +3,9 @@ package domain.card;
 import java.util.List;
 
 public class FirstCardSelectStrategy implements CardSelectStrategy {
+
+    public static final FirstCardSelectStrategy FIRST_CARD_SELECT_STRATEGY = new FirstCardSelectStrategy();
+
     @Override
     public Card select(List<Card> cards) {
         return cards.get(0);
