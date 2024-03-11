68c08c3616c843355003b02718172661042df177
unifolio0 <boho82ha@gmail.com>
2024-03-11 13:44:16 +0900

    test : DisplayName 명시

--- src/test/java/domain/blackjack/BettingResultTest.java
+++ src/test/java/domain/blackjack/BettingResultTest.java
@@ -3,13 +3,16 @@ package domain.blackjack;
 import domain.participant.Name;
 import domain.participant.Participant;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashMap;
 
 class BettingResultTest {
+
+    @DisplayName("참가자가 승리했을 시 수익을 계산한다.")
     @Test
-    void name() {
+    void getPayoutWhenWin() {
         LinkedHashMap<Participant, BetAmount> bet = new LinkedHashMap<>();
         Participant participant = new Participant(new Name("one"));
         LinkedHashMap<Participant, WinStatus> result = new LinkedHashMap<>();
@@ -22,8 +25,9 @@ class BettingResultTest {
         Assertions.assertThat(payout).isEqualTo(10_000);
     }
 
+    @DisplayName("참가자가 패배했을 시 수익을 계산한다.")
     @Test
-    void name1() {
+    void getPayoutWhenLose() {
         LinkedHashMap<Participant, BetAmount> bet = new LinkedHashMap<>();
         Participant participant = new Participant(new Name("one"));
         LinkedHashMap<Participant, WinStatus> result = new LinkedHashMap<>();
@@ -36,8 +40,9 @@ class BettingResultTest {
         Assertions.assertThat(payout).isEqualTo(-10_000);
     }
 
+    @DisplayName("참가자가 블랙잭이고 딜러는 블랙잭이 아닐 시 수익을 계산한다.")
     @Test
-    void name2() {
+    void getPayoutWhenBlackJack() {
         LinkedHashMap<Participant, BetAmount> bet = new LinkedHashMap<>();
         Participant participant = new Participant(new Name("one"));
         LinkedHashMap<Participant, WinStatus> result = new LinkedHashMap<>();
@@ -50,8 +55,23 @@ class BettingResultTest {
         Assertions.assertThat(payout).isEqualTo(15_000);
     }
 
+    @DisplayName("참가자와 딜러 모두 블랙잭일 시 수익을 계산한다.")
     @Test
-    void name3() {
+    void getPayoutWhenDraw() {
+        LinkedHashMap<Participant, BetAmount> bet = new LinkedHashMap<>();
+        Participant participant = new Participant(new Name("one"));
+        LinkedHashMap<Participant, WinStatus> result = new LinkedHashMap<>();
+        result.put(participant, WinStatus.DRAW);
+        BlackJackResult blackJackResult = new BlackJackResult(result);
+        bet.put(participant, new BetAmount(10_000));
+        BettingResult bettingResult = new BettingResult(bet, blackJackResult);
+        double payout = bettingResult.getPayout(participant);
+
+        Assertions.assertThat(payout).isZero();
+    }
+
+    @Test
+    void getDealerPayout() {
         LinkedHashMap<Participant, BetAmount> bet = new LinkedHashMap<>();
         LinkedHashMap<Participant, WinStatus> result = new LinkedHashMap<>();
         Participant one = new Participant(new Name("one"));
