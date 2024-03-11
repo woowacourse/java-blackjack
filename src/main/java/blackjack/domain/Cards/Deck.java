639ee1526682aef6d5023996952ba31e0d1f48fb
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-03-11 16:54:05 +0900

    refactor: Deck 역할 재정립

--- src/main/java/blackjack/domain/Cards/Deck.java
+++ src/main/java/blackjack/domain/Cards/Deck.java
@@ -1,28 +1,34 @@
-package blackjack.domain.Cards;
+package blackjack.domain.cards;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 public class Deck {
     private static final Random RANDOM = new Random();
-    private static final int ACE_BONUS_SCORE = 10;
-    private static final int NO_BONUS_SCORE = 0;
 
     private final List<Card> cards;
 
     public Deck() {
         this.cards = new LinkedList<>();
-    }
-
-    public void addCard(Card card) {
-        cards.add(card);
+        addAllCard();
     }
 
     public Card pickRandomCard() {
         validateDeck();
         int pickCardIndex = RANDOM.nextInt(cards.size());
         return cards.remove(pickCardIndex);
+    }
+
+    private void addAllCard() {
+        Arrays.stream(Rank.values())
+                .forEach(this::addAllShape);
+    }
+
+    private void addAllShape(Rank rank) {
+        Arrays.stream(Shape.values())
+                .forEach((shape) -> cards.add(new Card(shape, rank)));
     }
 
     private void validateDeck() {
