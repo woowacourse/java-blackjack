483485f086e5096c60fd93d59217f855d40a405e
yoonseo choi <choiyoonseo02@gmail.com>
2024-03-11 18:41:43 +0900

    feat: 카드를 랜덤으로 지급한다
    카드덱을 하나만 쓰지 않으므로 변경하였습니다.

---
+++ src/main/java/model/card/CardDispenser.java
@@ -0,0 +1,20 @@
+package model.card;
+
+import java.util.Random;
+
+public class CardDispenser {
+
+    private static final Random random = new Random();
+
+    private CardDispenser() {
+        throw new IllegalStateException();
+    }
+
+    public static CardNumber generateCardNumber() {
+        return CardNumber.values()[(random.nextInt(CardNumber.values().length))];
+    }
+
+    public static CardShape generateCardShape() {
+        return CardShape.values()[(random.nextInt(CardShape.values().length))];
+    }
+}
--- src/main/java/model/controller/BlackjackGameController.java
+++ src/main/java/model/controller/BlackjackGameController.java
@@ -1,5 +1,12 @@
 package model.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.blackjackgame.BlackjackGame;
+import model.card.Card;
+import model.card.CardDispenser;
+import model.card.Cards;
+import model.dealer.Dealer;
 import model.player.Players;
 import model.view.InputView;
 
@@ -13,5 +20,12 @@ public class BlackjackGameController {
 
     public void run() {
         Players players = inputView.requestPlayersName();
+        BlackjackGame blackjackGame = new BlackjackGame(new Dealer(), players);
+        List<Card> generatedCards = new ArrayList<>();
+        for (int i = 0; i < (players.getPlayers().size() + 1) * 2; i++) {
+            generatedCards.add(new Card(CardDispenser.generateCardNumber(), CardDispenser.generateCardShape()));
+        }
+        Cards cards = new Cards(generatedCards);
+        blackjackGame.distributeCardsForSetting(cards);
     }
 }
