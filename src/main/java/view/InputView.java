7650e172c20cc81acbaca632e03161624717bbc5
takoyakimchi <c4nyou@gmail.com>
2024-03-11 17:05:48 +0900

    feat: 콤마(,)를 이어서 입력했을 때의 예외처리 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -20,7 +20,7 @@ public class InputView {
 
     public List<String> readNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return Arrays.asList(scanner.nextLine().split(DELIMITER));
+        return Arrays.asList(scanner.nextLine().split(DELIMITER, -1));
     }
 
     public HitStay readHitOrStay(Player player) {
