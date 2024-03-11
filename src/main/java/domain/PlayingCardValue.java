2c02a0d36b566a28d41cc2029b53dbae29626f06
HyunJoong Kim <hjk0761@naver.com>
2024-03-11 17:44:28 +0900

    style: 공백 추가

--- src/main/java/domain/PlayingCardValue.java
+++ src/main/java/domain/PlayingCardValue.java
@@ -6,6 +6,7 @@ public enum PlayingCardValue {
     TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
 
     private static final int ACE_GAP = 10;
+
     private final int value;
 
     PlayingCardValue(final int value) {
