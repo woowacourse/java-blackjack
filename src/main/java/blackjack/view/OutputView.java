83e7d4a3625b4b7a45bf0f4887d05d750e2b8d09
lemone <lejinhy@gmail.com>
2024-03-11 16:23:24 +0900

    refactor: dealer 이름을 받아 출력하는 형식으로 수정

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -7,23 +7,23 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static blackjack.domain.game.BlackjackGame.DEAL_CARDS_COUNT;
 import static blackjack.domain.gamer.Dealer.HIT_UPPER_BOUND;
-import static blackjack.domain.gamer.Gamer.DEAL_CARD_COUNT;
 
 public class OutputView {
     static final String LINE_SEPARATOR = System.lineSeparator();
     private static final String ERROR_PREFIX = "[ERROR] ";
     private static final String DELIMITER = ", ";
-    private static final String DEAL_ANNOUNCE_FORMAT = String.format("딜러와 %%s에게 %d장을 나누었습니다.", DEAL_CARD_COUNT);
+    private static final String DEAL_ANNOUNCE_FORMAT = String.format("%%s와 %%s에게 %d장을 나누었습니다.", DEAL_CARDS_COUNT);
     private static final String DEALER_HIT_FORMAT = String.format("딜러는 %d 이하라 한장의 카드를 더 받았습니다.", HIT_UPPER_BOUND);
     private static final String CARD_FORMAT = "%s 카드 : %s";
     private static final String RESULT_SCORES_FORMAT = " - 결과: %d";
     private static final String WIN_STATUS_FORMAT = "%s: %s";
     private static final String WIN_ANNOUNCE = "## 최종 승패";
 
-    public static void printDealAnnounce(List<String> names) {
-        String nameFormat = String.join(DELIMITER, names);
-        System.out.printf(LINE_SEPARATOR + DEAL_ANNOUNCE_FORMAT + LINE_SEPARATOR, nameFormat);
+    public static void printDealAnnounce(String dealerName, List<String> playersName) {
+        String nameFormat = String.join(DELIMITER, playersName);
+        System.out.printf(LINE_SEPARATOR + DEAL_ANNOUNCE_FORMAT + LINE_SEPARATOR, dealerName, nameFormat);
     }
 
     public static void printDealCards(String name, List<Card> cards) {
