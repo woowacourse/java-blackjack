5dc32acdbfb0271c6e545ff01b50c70e44ed5875
coli-geonwoo <kkwoo001021@naver.com>
2024-03-11 21:48:58 +0900

    refactor: Batting eq/hc 재정의

--- src/main/java/blackjack/domain/Batting.java
+++ src/main/java/blackjack/domain/Batting.java
@@ -1,5 +1,7 @@
 package blackjack.domain;
 
+import java.util.Objects;
+
 public class Batting {
     private static final int MIN_BAT = 1;
     private static final int MAX_BAT = 10_000_000;
@@ -20,6 +22,19 @@ public class Batting {
             throw new IllegalArgumentException("배팅은 최소 " + MIN_BAT + "부터 "
                     + MAX_BAT + "까지 가능합니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Batting batting = (Batting) o;
+        return Objects.equals(bat, batting.bat);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bat);
     }
 
     public Double getBat() {
