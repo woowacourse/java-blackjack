01e05a377d3db1e9171a781d97e8b10fb9d4b8fe
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-03-11 18:51:35 +0900

    refactor(BlackJackController) : !를 활용한 부정표현 변경

--- src/main/java/controller/BlackJackController.java
+++ src/main/java/controller/BlackJackController.java
@@ -57,7 +57,7 @@ public class BlackJackController {
     }
 
     private void decideParticipantPlay(Participant participant, BlackJack blackJack) {
-        while (!participant.isNotHit() && inputView.isOneMoreCard(participant.getName())) {
+        while (participant.isNotHit() == false && inputView.isOneMoreCard(participant.getName())) {
             blackJack.offerCardToParticipant(participant, CardSize.ONE);
             outputView.printPlayerCardMessage(participant);
         }
