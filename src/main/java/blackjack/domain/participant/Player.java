b07bf1d5709bae5947b1ea7a53f9f8823e58fbdc
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-03-11 16:53:00 +0900

    feat: 딜러 이름 생성 불가능 처리

--- src/main/java/blackjack/domain/participant/Player.java
+++ src/main/java/blackjack/domain/participant/Player.java
@@ -2,12 +2,15 @@ package blackjack.domain.participant;
 
 import blackjack.domain.card.Card;
 import blackjack.util.Constants;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Player extends Gamer {
 
     private static final Pattern PATTERN = Pattern.compile("^[a-zA-Z가-힣]+(?:\\s+[a-zA-Z가-힣]+)*$");
     private static final String INVALID_NAME_FORMAT_EXCEPTION = "이름 형식에 맞지 않습니다. 이름은 영어 또는 한글로 입력해주세요.";
+    private static final String RESERVED_NAME_EXCEPTION = "%s라는 이름으로 지을 수 없습니다. 다른 이름을 입력해주세요.";
+    private static final Set<String> reservedNames = Set.of("딜러");
 
     private final String name;
 
@@ -21,6 +24,9 @@ public class Player extends Gamer {
     private void validate(final String name) {
         if (!PATTERN.matcher(name).matches()) {
             throw new IllegalArgumentException(INVALID_NAME_FORMAT_EXCEPTION);
+        }
+        if (reservedNames.contains(name)) {
+            throw new IllegalArgumentException(String.format(RESERVED_NAME_EXCEPTION, name));
         }
     }
 
