a9309fd36b548fe417d3c0e15ab190797eee717a
takoyakimchi <c4nyou@gmail.com>
2024-03-11 17:06:01 +0900

    feat: 예외 발생 시 예외 메시지만 콘솔에 출력

--- src/main/java/BlackjackApplication.java
+++ src/main/java/BlackjackApplication.java
@@ -6,6 +6,10 @@ public class BlackjackApplication {
 
     public static void main(String[] args) {
         BlackjackMachine machine = new BlackjackMachine(new InputView(), new OutputView());
-        machine.run();
+        try {
+            machine.run();
+        } catch (IllegalArgumentException | IllegalStateException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
