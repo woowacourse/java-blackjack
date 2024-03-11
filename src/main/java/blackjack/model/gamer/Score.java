22a29039b9a971744e091e664b1fc03182578d99
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-11 23:54:58 +0900

    feat(Score): 원시값 포장 클래스 구현 및 테스트 작성

---
+++ src/main/java/blackjack/model/gamer/Score.java
@@ -0,0 +1,22 @@
+package blackjack.model.gamer;
+
+public class Score {
+
+    private final int score;
+
+    public Score(int score) {
+        validateNotNegative(score);
+        this.score = score;
+    }
+
+    private void validateNotNegative(int score) {
+        if (score < 0) {
+            String errorMessage = String.format("[ERROR] 점수는 음수일 수 없습니다.(등록 score : %d", score);
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
