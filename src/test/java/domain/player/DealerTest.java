30f5b697fa07d5c0a2573dcf0ac82b0aa8875df3
dwax1324 <dwax1324@gmail.com>
2024-03-12 16:14:45 +0900

    test: 딜러는 플레이어의 핸드를 보고 승패무를 결정한다

--- src/test/java/domain/player/DealerTest.java
+++ src/test/java/domain/player/DealerTest.java
@@ -30,4 +30,24 @@ public class DealerTest {
 
         Assertions.assertThat(dealer.canHit()).isFalse();
     }
+
+    @Test
+    @DisplayName("딜러는 플레이어의 핸드를 보고 승패무를 결정할 수 있다")
+    void compare() {
+        final Dealer dealer = new Dealer();
+        final Player playerWin = new Player(new Name("win"));
+        final Player playerLose = new Player(new Name("lose"));
+        final Player playerTie = new Player(new Name("tie"));
+
+
+        dealer.hit(new Card(Rank.THREE, Suit.CLUBS));
+        playerWin.hit(new Card(Rank.FOUR, Suit.CLUBS));
+        playerLose.hit(new Card(Rank.TWO, Suit.CLUBS));
+        playerTie.hit(new Card(Rank.THREE, Suit.CLUBS));
+
+        Assertions.assertThat(dealer.compareHandsWith(playerWin)).isSameAs(PlayerResult.LOSE);
+        Assertions.assertThat(dealer.compareHandsWith(playerLose)).isSameAs(PlayerResult.WIN);
+        Assertions.assertThat(dealer.compareHandsWith(playerTie)).isSameAs(PlayerResult.TIE);
+    }
+
 }
