999b0a38c43dc3e5ab960eeea2cdb2ea7384443b
haiseong <wjdgotjd9908@gmail.com>
2024-03-12 21:13:48 +0900

    remove(money, moneyTest): 필요없는 메서드 삭제

--- src/main/java/blackjack/domain/result/Money.java
+++ src/main/java/blackjack/domain/result/Money.java
@@ -1,14 +1,6 @@
 package blackjack.domain.result;
 
 public record Money(int money) {
-    public Money add(Money other) {
-        return new Money(this.money + other.money);
-    }
-
-    public Money sub(Money other) {
-        return new Money(this.money - other.money);
-    }
-
     public Money multiply(double rate) {
         return new Money((int) (this.money * rate));
     }
--- src/test/java/blackjack/domain/MoneyTest.java
+++ src/test/java/blackjack/domain/MoneyTest.java
@@ -7,25 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MoneyTest {
-
-    @Test
-    @DisplayName("돈을 더해줄 수 있다.")
-    void addTest() {
-        Money money = new Money(1000);
-        Money other = new Money(1000);
-
-        assertThat(money.add(other)).isEqualTo(new Money(2000));
-    }
-
-    @Test
-    @DisplayName("돈을 뺄 수 있다.")
-    void subTest() {
-        Money money = new Money(1000);
-        Money other = new Money(1000);
-
-        assertThat(money.sub(other)).isEqualTo(new Money(0));
-    }
-
     @Test
     @DisplayName("돈을 곱할 수 있다.")
     void calculateBlackJackBonusTest() {
