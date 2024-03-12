a9c7fdeec63987e73cd044bdf2a690ebd3a0d1f4
lemone <lejinhy@gmail.com>
2024-03-12 16:47:10 +0900

    refactor: 점수 계산 알고리즘 변경

--- src/main/java/blackjack/domain/card/Hand.java
+++ src/main/java/blackjack/domain/card/Hand.java
@@ -1,9 +1,7 @@
 package blackjack.domain.card;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Hand {
     private static final int MAX_LIMIT_SCORE = 21;
@@ -22,44 +20,31 @@ public class Hand {
         myCards.add(card);
     }
 
-    public int compareScoreTo(Hand otherHand) {
-        return Integer.compare(findMaximumScore(), otherHand.findMaximumScore());
-    }
+    public int calculateScore() {
+        boolean hasAce = false;
+        int sum = 0;
 
-    // TODO: 간단한 로직으로 수정
-    public int findMaximumScore() {
-        Set<Integer> scoreCases = new HashSet<>();
-        List<List<Integer>> cardsScores = myCards.stream().map(card -> card.score().get()).toList();
-        calculateScoreCases(scoreCases, cardsScores, 0, 0);
-
-        return scoreCases.stream()
-                .filter(score -> score <= MAX_LIMIT_SCORE)
-                .max(Integer::compare)
-                .orElse(scoreCases.stream().min(Integer::compare).get());
-    }
-
-    private void calculateScoreCases(Set<Integer> scoreCases, List<List<Integer>> cardsScores, int sum, int index) {
-        if (index == cardsScores.size()) {
-            scoreCases.add(sum);
-            return;
+        for (Card card : myCards) {
+            if (card.rank() == Rank.ACE && !hasAce) {
+                hasAce = true;
+                sum += card.rank().get(1);
+                continue;
+            }
+            sum += card.rank().get(0);
         }
-        for (int score : cardsScores.get(index)) {
-            calculateScoreCases(scoreCases, cardsScores, sum + score, index + 1);
+
+        if (sum > MAX_LIMIT_SCORE && hasAce) {
+            sum = sum - Rank.ACE.get(1) + Rank.ACE.get(0);
         }
-    }
-
-    public void tempMaximumScore() {
-        // 만약 에이스가 한번 나오면 다음 에이스는 1로 계산.
-
-
+        return sum;
     }
 
     public boolean isOverLimitScore() {
-        return findMaximumScore() > MAX_LIMIT_SCORE;
+        return calculateScore() > MAX_LIMIT_SCORE;
     }
 
     public boolean isLimitScore() {
-        return findMaximumScore() == MAX_LIMIT_SCORE;
+        return calculateScore() == MAX_LIMIT_SCORE;
     }
 
     public int getNumberOfCards() {
