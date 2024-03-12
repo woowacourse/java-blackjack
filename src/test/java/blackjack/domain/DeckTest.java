f696541e0e6577b0d1b182d70bed9ff7fd13c8b3
eunjungL <dldmswjd322@gmail.com>
2024-03-12 09:51:45 +0900

    test: Deck 누락 테스트 추가

--- src/test/java/blackjack/domain/DeckTest.java
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -3,6 +3,7 @@ package blackjack.domain;
 import blackjack.domain.card.TrumpCard;
 import blackjack.domain.stategy.NoShuffleStrategy;
 import blackjack.strategy.ShuffleStrategy;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,15 +13,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("덱")
 public class DeckTest {
 
+    private Deck deck;
+
+    @BeforeEach
+    void setUp() {
+        ShuffleStrategy shuffleStrategy = new NoShuffleStrategy();
+        deck = new Deck(shuffleStrategy);
+    }
+
     @DisplayName("원하는 순서로 생성에 성공한다.")
     @Test
     void shuffleCardInMyOrder() {
         // given
-        ShuffleStrategy shuffleStrategy = new NoShuffleStrategy();
-        Deck deck = new Deck(shuffleStrategy);
         TrumpCard trumpCardAceSpade = aceSpadeTrumpCard();
 
         // when & then
         assertThat(deck.getCards().poll()).isEqualTo(trumpCardAceSpade);
+    }
+
+    @DisplayName("카드 뭉치의 첫 번째 카드를 반환한다.")
+    @Test
+    void draw() {
+        //given
+        TrumpCard trumpCardAceSpade = aceSpadeTrumpCard();
+
+        //when & then
+        assertThat(deck.draw()).isEqualTo(trumpCardAceSpade);
     }
 }
