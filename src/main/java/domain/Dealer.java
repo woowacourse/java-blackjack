983b478feff6d0f1211e7842426c4707b200662f
nayonsoso <panda0329@naver.com>
2024-03-12 15:13:44 +0900

    refactor: 참가자와 딜러가 상속하는 추상 클래스 생성

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -1,33 +1,24 @@
 package domain;
 
-import java.util.List;
+public class Dealer extends Participant {
 
-public class Dealer extends Player {
-
+    public static final String DEALER_NAME = "딜러";
     private static final int MAX_SCORE_TO_HIT = 16;
 
     private final CardDeck cardDeck;
 
     public Dealer(CardDeck cardDeck) {
-        super(new Name("딜러"), cardDeck.initHand());
+        super(DEALER_NAME);
         this.cardDeck = cardDeck;
-    }
-
-    Dealer(List<Card> cards) {
-        super(new Name("딜러"), new Hand(cards));
-        this.cardDeck = null;
-    }
-
-    public Hand dealHand() {
-        return cardDeck.initHand();
     }
 
     public Card dealCard() {
         return cardDeck.draw();
     }
 
-    @Override
     public boolean isHittable() {
-        return this.getTotalScore() <= MAX_SCORE_TO_HIT;
+        int totalScore = getTotalScore();
+
+        return totalScore <= MAX_SCORE_TO_HIT;
     }
 }
