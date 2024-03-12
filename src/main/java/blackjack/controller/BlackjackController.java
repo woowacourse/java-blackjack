c9a2ce0ab4e0b979de4693006565c4af0bd36f1d
lemone <lejinhy@gmail.com>
2024-03-12 15:28:12 +0900

    refactor: 블랙잭 컨트롤러 메서드 분리

--- src/main/java/blackjack/controller/BlackjackController.java
+++ src/main/java/blackjack/controller/BlackjackController.java
@@ -1,7 +1,5 @@
 package blackjack.controller;
 
-import blackjack.domain.card.Card;
-import blackjack.domain.card.CardPicker;
 import blackjack.domain.card.Deck;
 import blackjack.domain.card.Hand;
 import blackjack.domain.game.BlackjackGame;
@@ -14,7 +12,6 @@ import blackjack.view.OutputView;
 import blackjack.view.PlayerCommand;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -23,62 +20,73 @@ public class BlackjackController {
     public void run() {
         Dealer dealer = new Dealer(new Hand(new ArrayList<>()));
         Players players = requestUntilValid(() -> Players.from(InputView.readPlayersName()));
-        CardPicker cardPicker = new CardPicker(new Deck(new ArrayList<>(Arrays.asList(Card.values()))));
-        BlackjackGame blackjackGame = new BlackjackGame(cardPicker);
+        BlackjackGame blackjackGame = new BlackjackGame(Deck.make());
 
-        // 딜 후 딜러, 플레이어 카드 출력
-        blackjackGame.deal(dealer, players);
-        OutputView.printDealAnnounce(dealer.getName(), players.getNames());
-        OutputView.printDealCards(dealer.getName(), dealer.getCards());
+        dealAndPrintResult(blackjackGame, dealer, players);
+
+        askToPlayersHit(players, blackjackGame);
+        drawToDealer(dealer, blackjackGame);
+        printAllPlayersHandResult(dealer, players);
+
+        calculateGameResultAndPrint(dealer, players);
+    }
+
+    private void printAllPlayersHandResult(Dealer dealer, Players players) {
+        OutputView.printGamerCards(dealer.getName(), dealer.getCards(), dealer.getScore());
         players.forEach(player ->
-                OutputView.printDealCards(player.getName(), player.getCards()));
-        OutputView.printNewLine();
+                OutputView.printGamerCards(player.getName(), player.getCards(), player.getScore()));
+    }
 
-        // 모든 사용자에게 y or n 요청
-        for (Player player : players.getPlayers()) {
-            // 블랙잭이면 안물어봄
-            if (player.isBlackjack()) {
-                continue;
-            }
-            // y or n 반복
-            while (true) {
-                PlayerCommand command = requestUntilValid(() -> PlayerCommand.from(InputView.readPlayerCommand(player.getName())));
-                blackjackGame.hitOrStand(player, command);
-                if (command == PlayerCommand.STAND) {
-                    if (player.getCards().size() == 2) {
-                        OutputView.printDealCards(player.getName(), player.getCards());
-                    }
-                    break;
-                }
-                OutputView.printDealCards(player.getName(), player.getCards());
-                if (player.isBust()) {
-                    break;
-                }
-            }
-        }
-
-        OutputView.printNewLine();
-
-        // 딜러 16 넘을때까지 뽑
+    private void drawToDealer(Dealer dealer, BlackjackGame blackjackGame) {
         while (dealer.isScoreUnderBound()) {
             blackjackGame.drawIfScoreUnderBound(dealer);
             OutputView.printDealerHitAnnounce();
         }
+    }
 
-        OutputView.printNewLine();
+    private void dealAndPrintResult(BlackjackGame blackjackGame, Dealer dealer, Players players) {
+        blackjackGame.deal(dealer, players);
 
-        OutputView.printGamerCards(dealer.getName(), dealer.getCards(), dealer.getScore());
+        OutputView.printDealAnnounce(dealer.getName(), players.getNames());
+        OutputView.printDealCards(dealer.getName(), dealer.getCards());
         players.forEach(player ->
-                OutputView.printGamerCards(player.getName(), player.getCards(), player.getScore()));
+                OutputView.printDealCards(player.getName(), player.getCards()));
+    }
 
+    private void askToPlayersHit(Players players, BlackjackGame blackjackGame) {
+        for (Player player : players.getPlayers()) {
+            processHitOrStand(player, blackjackGame);
+        }
+    }
 
+    private void processHitOrStand(Player player, BlackjackGame blackjackGame) {
+        if (player.isBlackjack()) {
+            return;
+        }
+
+        boolean isRun = true;
+        while (isRun) {
+            PlayerCommand command = requestUntilValid(() -> PlayerCommand.from(InputView.readPlayerCommand(player.getName())));
+            blackjackGame.hitOrStand(player, command);
+            isRun = blackjackGame.isPlayerCanHit(player, command);
+            printDealCards(player, command);
+        }
+    }
+
+    private void printDealCards(Player player, PlayerCommand command) {
+        if (command == PlayerCommand.STAND) {
+            if (player.getCards().size() == 2) {
+                OutputView.printDealCards(player.getName(), player.getCards());
+            }
+            return;
+        }
+        OutputView.printDealCards(player.getName(), player.getCards());
+    }
+
+    private void calculateGameResultAndPrint(Dealer dealer, Players players) {
         Referee referee = new Referee();
         referee.calculatePlayersResults(players, dealer);
 
-        printAllGameResult(dealer, referee);
-    }
-
-    private void printAllGameResult(Dealer dealer, Referee referee) {
         OutputView.printWinAnnounce();
         OutputView.printDealerWinStatus(dealer.getName(), referee.getDealerResult());
         referee.getPlayersResults().forEach(OutputView::printPlayerWinStatus);
