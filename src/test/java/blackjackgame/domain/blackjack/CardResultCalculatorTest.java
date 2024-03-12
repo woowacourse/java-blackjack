2dad99de0c9497fc9b1968ee3cadb0c1867d8e91
BurningFalls <sherlocky31415@gmail.com>
2024-03-12 23:13:37 +0900

    test: BLACKJACK 결과가 잘 나오는지 테스트

---
+++ src/test/java/blackjackgame/domain/blackjack/CardResultCalculatorTest.java
@@ -0,0 +1,25 @@
+package blackjackgame.domain.blackjack;
+
+import static blackjackgame.domain.blackjack.CardResult.BLACKJACK;
+import static blackjackgame.domain.card.CardName.ACE;
+import static blackjackgame.domain.card.CardName.JACK;
+import static blackjackgame.domain.card.CardType.SPADE;
+
+import blackjackgame.domain.card.Card;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CardResultCalculatorTest {
+
+    @Test
+    @DisplayName("손패를 추가로 받지 않았으면서 손패의 합이 21이면 결과가 BLACKJACK이다.")
+    void gameResultBlackjackTest() {
+        HoldingCards holdingCards = HoldingCards.of(
+                new Card(JACK, SPADE), new Card(ACE, SPADE)
+        );
+        CardResult cardResult = CardResultCalculator.calculate(holdingCards);
+
+        Assertions.assertThat(cardResult).isEqualTo(BLACKJACK);
+    }
+}
