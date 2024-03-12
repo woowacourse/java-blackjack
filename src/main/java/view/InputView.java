f640ae10b2dfa4a4a89bd52f51511a6396620508
SungKyum Kim <sungkyum1@naver.com>
2024-03-13 00:34:18 +0900

    fix: 공백 입력시 중복된 이름을 허용하는 버그 해결

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
 
     public static List<String> inputNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return toList(split(scanner.nextLine()));
+        return trimAndToList(split(scanner.nextLine()));
     }
 
     public static String inputHitCommand(final String name) {
@@ -21,7 +21,7 @@ public class InputView {
         return input.split(",");
     }
 
-    private static List<String> toList(final String[] input) {
-        return Arrays.asList(input);
+    private static List<String> trimAndToList(final String[] input) {
+        return Arrays.stream(input).sequential().map(String::trim).toList();
     }
 }
