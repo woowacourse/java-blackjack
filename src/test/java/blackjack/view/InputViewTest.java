700a3c1df4b591749bdf6cc492f909957b12fa71
Minjoo <m0522inju@gmail.com>
2024-03-12 20:13:10 +0900

    refactor(InputViewTest): 테스트에서도 인스턴스 생성해서 사용하도록 수정

--- src/test/java/blackjack/view/InputViewTest.java
+++ src/test/java/blackjack/view/InputViewTest.java
@@ -14,23 +14,29 @@ class InputViewTest {
     @DisplayName("입력받은 이름을 List 형태로 반환한다.")
     void readPlayerNames() {
         String names = "몰리,리브";
-        assertThat(InputView.readPlayerNames(() -> names)).containsExactly("몰리", "리브");
+        InputView inputView = new InputView(() -> names);
+
+        assertThat(inputView.readPlayerName()).containsExactly("몰리", "리브");
     }
 
     @Test
     @DisplayName("추가 선택 입력 시 y 또는 n이 아닌 경우 예외를 던진다.")
     void readHitOrNotByInvalidInput() {
+        InputView inputView = new InputView(() -> "yes");
+
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> InputView.readHitOrNot("리브", () -> "yes"))
+                .isThrownBy(() -> inputView.readHitOrNot("리브"))
                 .withMessage("y 혹은 n만 입력할 수 있습니다.");
     }
 
     @ParameterizedTest
     @NullAndEmptySource
-    @DisplayName("추가 선택 입력 시 blank가 들어온 경우")
+    @DisplayName("추가 선택 입력 시 blank가 들어오면 예외를 던진다.")
     void readHitOrNotByInvalid(String input) {
+        InputView inputView = new InputView(() -> input);
+
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> InputView.readHitOrNot("리브", () -> input))
+                .isThrownBy(() -> inputView.readHitOrNot("리브"))
                 .withMessage("y 혹은 n만 입력할 수 있습니다.");
     }
 }
