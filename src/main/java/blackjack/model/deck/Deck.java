fbecfd352b02469db9ae42f771fa74288c930c81
Minjoo <m0522inju@gmail.com>
2024-03-12 20:16:04 +0900

    refactor(Deck): 사용하지 않는 메서드 삭제

--- src/main/java/blackjack/model/deck/Deck.java
+++ src/main/java/blackjack/model/deck/Deck.java
@@ -1,6 +1,5 @@
 package blackjack.model.deck;
 
-import blackjack.model.participant.Hand;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Collections;
@@ -8,7 +7,6 @@ import java.util.Deque;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Deck {
@@ -45,26 +43,11 @@ public class Deck {
         return new ArrayDeque<>(originDeck);
     }
 
-    // TODO: 아래 애들 없애기
-    public Hand distributeInitialCard() {
-        return new Hand(List.of(distribute(), distribute()));
-    }
-
-    public List<Hand> distributeInitialCard(final int playerCount) {
-        return IntStream.range(0, playerCount)
-                .mapToObj(i -> distributeInitialCard())
-                .toList();
-    }
-
     public Card distribute() {
         try {
             return deck.removeFirst();
         } catch (NoSuchElementException e) {
             throw new NoSuchElementException("카드가 부족합니다.");
         }
-    }
-
-    public Deque<Card> getDeck() {
-        return deck;
     }
 }
--- src/test/java/blackjack/model/deck/DeckTest.java
+++ src/test/java/blackjack/model/deck/DeckTest.java
@@ -1,10 +1,10 @@
 package blackjack.model.deck;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.NoSuchElementException;
-import org.assertj.core.api.Assertions;
+import java.util.stream.IntStream;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,30 +18,17 @@ class DeckTest {
     }
 
     @Test
-    @DisplayName("카드 덱을 생성한다.")
-    void createDeck() {
-        assertThat(deck.getDeck()).hasSize(52);
-    }
-
-    @Test
-    @DisplayName("인원 수만큼 카드 목록을 생성한다.")
-    void distributeInitialCardByPlayerCount() {
-        assertThat(deck.distributeInitialCard(4)).hasSize(4);
-    }
-
-    @Test
     @DisplayName("카드 한 세트(52장)을 다 소진할 경우 예외가 발생한다.")
     void throwErrorWhenNoCard() {
-        assertThatThrownBy(() -> deck.distributeInitialCard(27))
+        assertThatThrownBy(repeatDistribute(() -> deck.distribute(), 53))
                 .isInstanceOf(NoSuchElementException.class)
                 .hasMessage("카드가 부족합니다.");
     }
 
-    @Test
-    @DisplayName("Deck에서 가장 앞에 있는 카드를 반환한다.")
-    void distribute() {
-        Card expected = deck.getDeck().getFirst();
-        assertThat(deck.distribute()).isEqualTo(expected);
+    static ThrowingCallable repeatDistribute(Runnable distributor, int repeatCount) {
+        return () -> {
+            IntStream.rangeClosed(1, repeatCount)
+                    .forEach(ignore -> distributor.run());
+        };
     }
-
 }
