bf7f038e95f9739bbcac92be9f5e375ea7987c9a
mzeong <mjeong@sookmyung.ac.kr>
2024-03-12 20:13:49 +0900

    refactor: 승패 계산 관련 로직을 Judge로 이동

--- src/main/java/blackjack/domain/rule/Judge.java
+++ src/main/java/blackjack/domain/rule/Judge.java
@@ -1,16 +1,54 @@
 package blackjack.domain.rule;
 
+import blackjack.domain.Result;
+import blackjack.domain.player.Dealer;
+import blackjack.domain.player.Participant;
+import blackjack.domain.player.Participants;
+import blackjack.domain.player.Player;
+import blackjack.domain.player.Players;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Judge {
 
-    private static final int BLACK_JACK = 21;
+    private final Map<Participant, Result> results;
 
-    public boolean isPlayerWin(Score dealerScore, Score playerScore) {
-        if (playerScore.isAbove(BLACK_JACK)) {
-            return false;
+    public Judge(Participants participants) {
+        this.results = new LinkedHashMap<>();
+        judgePlayers(participants.getPlayers(), participants.getDealer());
+        judgeDealer(participants.getDealer());
+    }
+
+    private void judgePlayers(Players players, Dealer dealer) {
+        Score dealerScore = dealer.getHandScore();
+        for (Player player : players.getPlayers()) {
+            Result playerResult = judgePlayer(player.getHandScore(), dealerScore);
+            results.put(player, playerResult);
         }
-        if (dealerScore.isAbove(BLACK_JACK)) {
-            return true;
+    }
+
+    private Result judgePlayer(Score playerScore, Score dealerScore) {
+        if (playerScore.isBust()) {
+            return Result.createLossResult();
         }
-        return playerScore.isBiggerThan(dealerScore);
+        if (dealerScore.isBust()) {
+            return Result.createWinResult();
+        }
+
+        if (playerScore.isBiggerThan(dealerScore)) {
+            return Result.createWinResult();
+        }
+        return Result.createLossResult();
+    }
+
+    private void judgeDealer(Dealer dealer) {
+        List<Result> playerResults = results.values().stream().toList();
+        Result result = Result.create(playerResults);
+        results.put(dealer, result);
+    }
+
+    public Map<Participant, Result> getResults() {
+        return results;
     }
 }
--- src/test/java/blackjack/domain/rule/JudgeTest.java
+++ src/test/java/blackjack/domain/rule/JudgeTest.java
@@ -1,25 +1,110 @@
 package blackjack.domain.rule;
 
-import org.junit.jupiter.api.BeforeEach;
+import blackjack.domain.Result;
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardRank;
+import blackjack.domain.card.CardSuit;
+import blackjack.domain.card.Hand;
+import blackjack.domain.player.Dealer;
+import blackjack.domain.player.Participants;
+import blackjack.domain.player.Player;
+import blackjack.domain.player.PlayerName;
+import blackjack.domain.player.Players;
+import fixture.DealerFixture;
+import fixture.HandFixture;
+import fixture.PlayerFixture;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class JudgeTest {
 
-    private Judge judge;
+    @DisplayName("플레이어가 21을 초과하면 플레이어는 패배하고 딜러는 승리한다.")
+    @Test
+    void testPlayerBust() {
+        // given
+        Dealer dealer = new Dealer(new Hand());
 
-    @BeforeEach
-    void setUp() {
-        judge = new Judge();
+        Hand hand = HandFixture.createHandWithScoreTotal21();
+        hand.append(new Card(CardRank.ACE, CardSuit.HEART));
+        Player player = new Player(hand, new PlayerName("pobi"));
+
+        Participants participants = new Participants(new Players(List.of(player)), dealer);
+
+        // when
+        Judge judge = new Judge(participants);
+
+        // then
+        Result dealerResult = judge.getResults().get(dealer);
+        Result playerResult = judge.getResults().get(player);
+
+        assertAll(
+                () -> assertThat(dealerResult.getWinCount()).isEqualTo(1),
+                () -> assertThat(dealerResult.getLoseCount()).isEqualTo(0),
+
+                () -> assertThat(playerResult.getWinCount()).isEqualTo(0),
+                () -> assertThat(playerResult.getLoseCount()).isEqualTo(1)
+        );
     }
 
-    @DisplayName("딜러와 플레이어 중 누가 이겼는지 알 수 있다")
+    @DisplayName("플레이어가 21을 초과하지 않고 딜러가 21을 초과하면 플레이어는 승리하고 딜러는 패배한다.")
     @Test
-    void testSelectWinner() {
-        Score dealerScore = new Score(10);
-        Score playerScore = new Score(5);
-        assertThat(judge.isPlayerWin(dealerScore, playerScore)).isFalse();
+    void testDealerBust() {
+        // given
+        Hand hand = HandFixture.createHandWithScoreTotal21();
+        hand.append(new Card(CardRank.ACE, CardSuit.HEART));
+        Dealer dealer = new Dealer(hand);
+
+        Player player = new Player(new Hand(), new PlayerName("pobi"));
+
+        Participants participants = new Participants(new Players(List.of(player)), dealer);
+
+        // when
+        Judge judge = new Judge(participants);
+
+        // then
+        Result dealerResult = judge.getResults().get(dealer);
+        Result playerResult = judge.getResults().get(player);
+
+        assertAll(
+                () -> assertThat(dealerResult.getWinCount()).isEqualTo(0),
+                () -> assertThat(dealerResult.getLoseCount()).isEqualTo(1),
+
+                () -> assertThat(playerResult.getWinCount()).isEqualTo(1),
+                () -> assertThat(playerResult.getLoseCount()).isEqualTo(0)
+        );
+    }
+
+    @DisplayName("플레이어와 딜러가 21을 초과하지 않으면 플레이어가 딜러보다 합이 높을 때 승리한다.")
+    @Test
+    void testJudge() {
+        // given
+        Dealer dealer = DealerFixture.createDealer();
+        Player pobi = PlayerFixture.createPobi();
+        Player jason = PlayerFixture.createJason();
+
+        Participants participants = new Participants(new Players(List.of(pobi, jason)), dealer);
+
+        // when
+        Judge judge = new Judge(participants);
+
+        // then
+        Result dealerResult = judge.getResults().get(dealer);
+        Result pobiResult = judge.getResults().get(pobi);
+        Result jasonResult = judge.getResults().get(jason);
+
+        assertAll(
+                () -> assertThat(dealerResult.getWinCount()).isEqualTo(1),
+                () -> assertThat(dealerResult.getLoseCount()).isEqualTo(1),
+
+                () -> assertThat(pobiResult.getWinCount()).isEqualTo(1),
+                () -> assertThat(pobiResult.getLoseCount()).isEqualTo(0),
+
+                () -> assertThat(jasonResult.getWinCount()).isEqualTo(0),
+                () -> assertThat(jasonResult.getLoseCount()).isEqualTo(1)
+        );
     }
 }
