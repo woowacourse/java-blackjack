8c6fdc6d223adc4cc3c499f5aea3bddbcef15ce0
haiseong <wjdgotjd9908@gmail.com>
2024-03-12 20:41:14 +0900

    feat(gameResult): 게임 결과에 돈 계산 비율 추가

--- src/main/java/blackjack/domain/result/GameResult.java
+++ src/main/java/blackjack/domain/result/GameResult.java
@@ -1,5 +1,18 @@
 package blackjack.domain.result;
 
 public enum GameResult {
-    WIN, LOSE, DRAW;
+    WIN_BY_BLACK_JACK(1.5),
+    WIN(1.0),
+    LOSE(-1),
+    DRAW(0);
+
+    private final double moneyReturnRate;
+
+    GameResult(double moneyReturnRate) {
+        this.moneyReturnRate = moneyReturnRate;
+    }
+
+    public double getMoneyRate() {
+        return moneyReturnRate;
+    }
 }
