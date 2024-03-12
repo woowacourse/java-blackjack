a3b79222e6b4c52cd3b0cc2adf52ff7717dd485b
libienz <leekh10195@gmail.com>
2024-03-12 20:10:49 +0900

    feat: Bet도메인 InputView 구현

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -2,7 +2,11 @@ package blackjack.view;
 
 import blackjack.InputMapper;
 import blackjack.domain.DrawDecision;
+import blackjack.domain.bet.Bet;
+import blackjack.domain.bet.Bets;
+import blackjack.domain.player.Player;
 import blackjack.domain.player.PlayerName;
+import blackjack.domain.player.Players;
 import java.util.List;
 import java.util.Scanner;
 
@@ -26,5 +30,17 @@ public class InputView {
         String message = String.format("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)", name);
         System.out.println(String.join("", LINE_SEPARATOR, message));
         return inputMapper.mapToDrawDecision(scanner.nextLine());
+    }
+
+    public Bets readBets(Players players) {
+        return new Bets(players.getPlayers().stream()
+                .map(Player::getName)
+                .map(this::readBet)
+                .toList());
+    }
+
+    public Bet readBet(String playerName) {
+        System.out.println(String.format("%s의 배팅 금액은?", playerName));
+        return inputMapper.mapToBet(playerName, scanner.nextInt());
     }
 }
