46ae768cb0a7fba769881d30fabfd1dc74e14202
yoonjuho <dbswn990@naver.com>
2024-03-12 11:30:24 +0900

    chore(GameResultDto): 코드 컨벤션 준수

--- src/main/java/view/dto/result/GameResultDto.java
+++ src/main/java/view/dto/result/GameResultDto.java
@@ -1,19 +1,19 @@
 package view.dto.result;
 
-import domain.result.GameResultStatus;
-import domain.result.GameResult;
 import domain.participant.Player;
+import domain.result.GameResult;
+import domain.result.GameResultStatus;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public record GameResultDto(Map<String, String> gameResult, Map<String, Integer> dealerResult) {
 
-    public static GameResultDto from(GameResult gameResult, GameResult dealerResult) {
+    public static GameResultDto from(final GameResult gameResult, final GameResult dealerResult) {
         return new GameResultDto(convertToPlayerResult(gameResult), convertToDealerResult(dealerResult));
     }
 
-    private static Map<String, String> convertToPlayerResult(GameResult gameResult) {
+    private static Map<String, String> convertToPlayerResult(final GameResult gameResult) {
         Map<String, String> resultDto = new LinkedHashMap<>();
         gameResult.getResult()
                   .forEach((key, value) -> {
@@ -22,17 +22,18 @@ public record GameResultDto(Map<String, String> gameResult, Map<String, Integer>
         return resultDto;
     }
 
-    private static Map<String, Integer> convertToDealerResult(GameResult dealerResult) {
+    private static Map<String, Integer> convertToDealerResult(final GameResult dealerResult) {
         Map<Player, GameResultStatus> result = dealerResult.getResult();
         Map<String, Integer> resultDto = new LinkedHashMap<>();
-        for(GameResultStatus gameResultStatus : GameResultStatus.values()){
+        for (GameResultStatus gameResultStatus : GameResultStatus.values()) {
             int count = (int) result.values()
-                                          .stream()
-                                          .filter(status -> status == gameResultStatus)
-                                          .count();
+                                    .stream()
+                                    .filter(status -> status == gameResultStatus)
+                                    .count();
             resultDto.put(gameResultStatus.getValue(), count);
         }
-        resultDto.entrySet().removeIf(entry -> entry.getValue() == 0);
+        resultDto.entrySet()
+                 .removeIf(entry -> entry.getValue() == 0);
         return resultDto;
     }
 }
