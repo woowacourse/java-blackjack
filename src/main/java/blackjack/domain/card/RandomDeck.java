70d6e3bc1f7214e5b5665206dea252ca0dabfebb
masonkimseoul <masonkimseoul@gmail.com>
2024-03-12 22:36:25 +0900

    refactor: RandomDeck을 일급 컬렉션으로 수정

--- src/main/java/blackjack/domain/card/RandomDeck.java
+++ src/main/java/blackjack/domain/card/RandomDeck.java
@@ -1,23 +1,21 @@
 package blackjack.domain.card;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RandomDeck implements Deck {
     private static final RandomDeck INSTANCE = new RandomDeck();
     private static final Random RANDOM = new Random();
 
-    private final Map<Integer, Number> numberByOrder = new HashMap<>();
-    private final Map<Integer, Shape> shapeByOrder = new HashMap<>();
+    private final List<Card> cards;
 
     private RandomDeck() {
-        for (Number number : Number.values()) {
-            numberByOrder.put(numberByOrder.size(), number);
-        }
-        for (Shape cardShape : Shape.values()) {
-            shapeByOrder.put(shapeByOrder.size(), cardShape);
-        }
+        this.cards = Stream.of(Number.values())
+                .flatMap(number -> Stream.of(Shape.values())
+                        .map(shape -> new Card(number, shape)))
+                .collect(Collectors.toList());
     }
 
     public static RandomDeck getInstance() {
@@ -26,12 +24,6 @@ public class RandomDeck implements Deck {
 
     @Override
     public Card drawCard() {
-        int orderOfNumber = RANDOM.nextInt(numberByOrder.size());
-        int orderOfShape = RANDOM.nextInt(shapeByOrder.size());
-
-        Number number = numberByOrder.get(orderOfNumber);
-        Shape shape = shapeByOrder.get(orderOfShape);
-
-        return new Card(number, shape);
+        return cards.get(RANDOM.nextInt(cards.size()));
     }
 }
