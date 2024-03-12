ff843506d368516a193ade6336f9540397c7c3c0
haiseong <wjdgotjd9908@gmail.com>
2024-03-12 20:27:03 +0900

    test(bettingBoardTest): 돈을 관리할 객체 테스트 생성

--- src/test/java/blackjack/domain/BettingBoardTest.java
+++ src/test/java/blackjack/domain/BettingBoardTest.java
@@ -1,12 +1,34 @@
 package blackjack.domain;
 
+import static blackjack.domain.card.Shape.CLOVER;
+import static blackjack.domain.card.Shape.DIAMOND;
+import static blackjack.domain.card.Shape.SPADE;
+import static blackjack.domain.card.Value.ACE;
+import static blackjack.domain.card.Value.EIGHT;
+import static blackjack.domain.card.Value.KING;
+import static blackjack.domain.card.Value.NINE;
+import static blackjack.domain.card.Value.QUEEN;
+import static blackjack.domain.card.Value.TEN;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Deck;
+import blackjack.domain.player.Dealer;
+import blackjack.domain.player.Player;
 import blackjack.domain.player.PlayerName;
 import blackjack.domain.result.BettingBoard;
+import blackjack.domain.result.GameResult;
+import blackjack.domain.result.GameResultBoard;
 import blackjack.domain.result.Money;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BettingBoardTest {
 
@@ -18,5 +40,52 @@ class BettingBoardTest {
         bettingBoard.bet(playerName, new Money(1000));
         Money money = bettingBoard.findByPlayerName(playerName);
         assertThat(money).isEqualTo(new Money(1000));
+    }
+
+    @ParameterizedTest
+    @MethodSource("resultAndMoney")
+    @DisplayName("결과에 따른 돈을 계산할 수 있다.")
+    void calculateMoneyTest(GameResult gameResult, Money expected) {
+        PlayerName playerName = new PlayerName("loki");
+        BettingBoard bettingBoard = new BettingBoard();
+        bettingBoard.bet(playerName, new Money(1000));
+        Map<PlayerName, GameResult> resultBoard = Map.of(playerName, gameResult);
+        bettingBoard.calculateMoney(resultBoard);
+        Money money = bettingBoard.findByPlayerName(playerName);
+        assertThat(money).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> resultAndMoney() {
+        return Stream.of(
+                Arguments.arguments(
+                        GameResult.WIN_BY_BLACK_JACK, new Money(1500)
+                ),
+                Arguments.arguments(
+                        GameResult.WIN, new Money(1000)
+                ),
+                Arguments.arguments(
+                        GameResult.DRAW, new Money(0)
+                ),
+                Arguments.arguments(
+                        GameResult.LOSE, new Money(-1000)
+                )
+        );
+    }
+
+    @Test
+    @DisplayName("딜러의 돈을 계산할 수 있다.")
+    void calculateDealerMoneyTest() {
+        PlayerName playerName1 = new PlayerName("loki");
+        PlayerName playerName2 = new PlayerName("pedro");
+        BettingBoard bettingBoard = new BettingBoard();
+        bettingBoard.bet(playerName1, new Money(1000));
+        bettingBoard.bet(playerName2, new Money(1000));
+        Map<PlayerName, GameResult> resultBoard = Map.of(
+                playerName1, GameResult.WIN,
+                playerName2, GameResult.WIN_BY_BLACK_JACK
+        );
+        bettingBoard.calculateMoney(resultBoard);
+        Money dealerMoney = bettingBoard.calculateDealerMoney();
+        assertThat(dealerMoney.money()).isEqualTo(-2500);
     }
 }
