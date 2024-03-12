3d91258ad4eb72e195edf3d75911139b30f9f546
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-12 22:39:14 +0900

    refactor : 캐싱 적용

--- src/main/java/domain/card/ShuffledCardsGenerator.java
+++ src/main/java/domain/card/ShuffledCardsGenerator.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 
 public class ShuffledCardsGenerator {
     private static final int DUPLICATES_COUNT = 6;
+    private static final List<Card> cachedCards = generateCardsOfOnePack();
 
     public List<Card> generate() {
         List<Card> cards = generateBlackJackCards();
@@ -16,15 +17,13 @@ public class ShuffledCardsGenerator {
 
     private List<Card> generateBlackJackCards() {
         List<Card> blackJackCards = new ArrayList<>();
-        List<Card> cards = generateCardsOfOnePack();
-
         for (int count = 0; count < DUPLICATES_COUNT; count++) {
-            blackJackCards.addAll(cards);
+            blackJackCards.addAll(new ArrayList<>(cachedCards));
         }
         return blackJackCards;
     }
 
-    private List<Card> generateCardsOfOnePack() {
+    private static List<Card> generateCardsOfOnePack() {
         return Stream.of(Symbol.values())
                 .flatMap(symbol -> Rank.getRanks().stream()
                         .map(rank -> new Card(symbol, rank)))
