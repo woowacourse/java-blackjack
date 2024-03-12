0d1bed9126bb75d8d61f5edb7e92206e519daecc
이상진 <priceelees@gmail.com>
2024-03-12 18:01:38 +0900

    refactor(GameResult): 기존의 승,패에 블랙잭 승과 무승부를 추가

--- src/main/java/blackjack/domain/gamer/GameResult.java
+++ src/main/java/blackjack/domain/gamer/GameResult.java
@@ -2,21 +2,15 @@ package blackjack.domain.gamer;
 
 public enum GameResult {
 
+	BLACKJACK_WIN("블랙잭승"),
 	WIN("승"),
+	DRAW("무승부"),
 	LOSE("패");
 
 	private final String name;
 
 	GameResult(String name) {
 		this.name = name;
-	}
-
-	public boolean isWin() {
-		return this == WIN;
-	}
-
-	public boolean isLose() {
-		return this == LOSE;
 	}
 
 	public String getName() {
