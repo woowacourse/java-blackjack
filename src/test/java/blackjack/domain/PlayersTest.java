0f50c12f2c6e536d87076ea1eb6c927cf6a742ea
haiseong <wjdgotjd9908@gmail.com>
2024-03-12 21:05:38 +0900

    test(playersTest): 모두가 버스트인지 검사하는 테스트 작성

--- src/test/java/blackjack/domain/PlayersTest.java
+++ src/test/java/blackjack/domain/PlayersTest.java
@@ -1,9 +1,23 @@
 package blackjack.domain;
 
+import static blackjack.domain.card.Shape.DIAMOND;
+import static blackjack.domain.card.Shape.SPADE;
+import static blackjack.domain.card.Value.ACE;
+import static blackjack.domain.card.Value.KING;
+import static blackjack.domain.card.Value.NINE;
+import static blackjack.domain.card.Value.QUEEN;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Deck;
+import blackjack.domain.player.Dealer;
+import blackjack.domain.player.Player;
+import blackjack.domain.player.PlayerName;
 import blackjack.domain.player.Players;
+import blackjack.domain.result.GameResult;
+import blackjack.domain.result.GameResultBoard;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,5 +50,30 @@ class PlayersTest {
 
         Players players = new Players(names);
         assertThat(players.getPlayerNames()).containsAll(names);
+    }
+
+
+    @Test
+    @DisplayName("플레이어들이 모두 버스트인지 검사한다..")
+    void playersAllBustTest() {
+        List<String> names = List.of("player1", "player2");
+        Players players = new Players(names);
+        List<Player> playerGroup = players.getPlayers();
+
+        List<Card> cards = new ArrayList<>(
+                List.of(new Card(DIAMOND, KING), new Card(DIAMOND, QUEEN), new Card(DIAMOND, NINE),
+                        new Card(SPADE, KING), new Card(SPADE, QUEEN), new Card(SPADE, NINE)));
+        Deck deck = new Deck(cards);
+
+        giveCardToPlayer(playerGroup.get(0), deck, 3);
+        giveCardToPlayer(playerGroup.get(1), deck, 3);
+
+        assertThat(players.isAllBusted()).isTrue();
+    }
+
+    private void giveCardToPlayer(Player player, Deck deck, int drawAmount) {
+        for (int i = 0; i < drawAmount; i++) {
+            player.draw(deck);
+        }
     }
 }
