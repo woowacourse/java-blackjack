3c5656de73425f35bbfbd56e24c13491dd8ce9eb
yoonjuho <dbswn990@naver.com>
2024-03-12 11:30:01 +0900

    chore(ParticipantDto): 사용되지 않는 메서드 삭제

--- src/main/java/view/dto/participant/ParticipantDto.java
+++ src/main/java/view/dto/participant/ParticipantDto.java
@@ -12,15 +12,15 @@ import java.util.List;
 
 public record ParticipantDto(String name, CardsDto cards) {
     public ParticipantDto(final String name) {
-        this(name, new CardsDto(new Cards(), 0));
+        this(name, CardsDto.from(new Cards(), 0));
     }
 
     public ParticipantDto(final Participant participant) {
-        this(participant.name().value(), new CardsDto(participant.hand(), participant.cardSum()));
+        this(participant.name().value(), CardsDto.from(participant.hand(), participant.cardSum()));
     }
 
     public ParticipantDto(final Participant participant, final Card card) {
-        this(participant.name().value(), new CardsDto(List.of(card), participant.cardSum()));
+        this(participant.name().value(), CardsDto.from(new Cards(List.of(card)), participant.cardSum()));
     }
 
     public static List<ParticipantDto> fromPlayers(final Players players) {
@@ -30,13 +30,9 @@ public record ParticipantDto(String name, CardsDto cards) {
                       .toList();
     }
 
-    public static Players toPlayers(List<ParticipantDto> players) {
+    public static Players toPlayers(final List<ParticipantDto> players) {
         return new Players(players.stream()
                                   .map(player -> new Player(new Name(player.name)))
                                   .toList());
-    }
-
-    public int score() {
-        return cards.getScore();
     }
 }
