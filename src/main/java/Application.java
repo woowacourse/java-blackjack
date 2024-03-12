db0f199c5fca5cecfc1ee589afd39c199b1e60c1
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-12 14:13:48 +0900

    style: 불필요한 개행 제거

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,11 @@
 import controller.BlackJackController;
+import java.io.IOException;
 import view.InputView;
 import view.OutputView;
-
-import java.io.IOException;
 
 public class Application {
 
     public static void main(String[] args) throws IOException {
-
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         BlackJackController blackJackController = new BlackJackController(inputView, outputView);
