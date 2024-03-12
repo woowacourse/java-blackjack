f9ee5d36bf57ed73cffc00b4af9212d0e604dc2a
libienz <leekh10195@gmail.com>
2024-03-12 20:01:46 +0900

    feat: Bets 속성 구현

---
+++ src/main/java/blackjack/domain/bet/Bets.java
@@ -0,0 +1,12 @@
+package blackjack.domain.bet;
+
+import java.util.List;
+
+public class Bets {
+
+    private final List<Bet> bets;
+
+    public Bets(List<Bet> bets) {
+        this.bets = bets;
+    }
+}
