4d85b040d4b5a7786395450f9acffe35fdda6943
ashsty <qkralsdk991@naver.com>
2024-03-12 14:05:03 +0900

    refactor: CardPattern->Pattern 뷰 로직 분리

---
+++ src/main/java/blackjack/view/PatternView.java
@@ -0,0 +1,27 @@
+package blackjack.view;
+
+import blackjack.model.card.Pattern;
+
+import java.util.Arrays;
+
+public enum PatternView {
+
+    SPADE("스페이드"),
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    CLOVER("클로버");
+
+    private final String patternName;
+
+    PatternView(String patternName) {
+        this.patternName = patternName;
+    }
+
+    public static String convertPatternName(Pattern pattern) {
+        return Arrays.stream(PatternView.values())
+                .filter(patternView -> patternView.name().equals(pattern.name()))
+                .findFirst()
+                .orElseThrow()
+                .patternName;
+    }
+}
