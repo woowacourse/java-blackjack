046f41b60ebf6ed1c2c205c2f6da90560486e291
fromitive <fromitive@gmail.com>
2024-03-12 17:08:05 +0900

    refactor: stream으로 처리하도록 변경

--- src/main/java/domain/BlackJackResult.java
+++ src/main/java/domain/BlackJackResult.java
@@ -2,34 +2,38 @@ package domain;
 
 import domain.constant.GamerResult;
 import java.util.Collections;
-import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class BlackJackResult {
     private final Map<Name, GamerResult> playersResult;
     private final Map<GamerResult, Integer> dealerResult;
 
     public BlackJackResult(int dealerScore, Map<Name, Integer> playersScore) {
-        dealerResult = new EnumMap<>(GamerResult.class);
+        dealerResult = calculateDealerResult(dealerScore, playersScore);
         this.playersResult = calculatePlayersResult(dealerScore, playersScore);
     }
 
+    private Map<GamerResult, Integer> calculateDealerResult(int dealerScore, Map<Name, Integer> playersScore) {
+        return playersScore.values()
+                .stream()
+                .collect(Collectors.groupingBy(
+                        value -> getJudgePlayerResult(dealerScore, value),
+                        Collectors.summingInt(count -> 1)));
+    }
+
     private Map<Name, GamerResult> calculatePlayersResult(int dealerScore, Map<Name, Integer> playersScore) {
-        Map<Name, GamerResult> playersResult = new HashMap<>();
-        playersScore.forEach((name, playerScore) -> playersResult.put(name,
-                getJudgePlayerResult(dealerScore, playerScore)));
-        return playersResult;
+        return playersScore.entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Entry::getKey,
+                        entry -> getJudgePlayerResult(dealerScore, entry.getValue())));
     }
 
     private GamerResult getJudgePlayerResult(int dealerScore, int playerScore) {
         GamerResult dealerJudgeResult = GamerResult.judge(dealerScore, playerScore);
-        addDealerResult(dealerJudgeResult);
         return dealerJudgeResult.getOpponentGameResult();
-    }
-
-    private void addDealerResult(GamerResult gamerResult) {
-        dealerResult.put(gamerResult, dealerResult.computeIfAbsent(gamerResult, score -> 0) + 1);
     }
 
     public Map<Name, GamerResult> getPlayersResult() {
