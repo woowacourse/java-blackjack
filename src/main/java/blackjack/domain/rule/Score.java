16faec8f2b37bef207b39207b4524c42d7090df1
mzeong <mjeong@sookmyung.ac.kr>
2024-03-12 18:50:07 +0900

    refactor: 점수 계산 관련 로직을 Score로 이동

--- src/main/java/blackjack/domain/rule/Score.java
+++ src/main/java/blackjack/domain/rule/Score.java
@@ -1,6 +1,12 @@
 package blackjack.domain.rule;
 
+import blackjack.domain.card.CardRank;
+
 public class Score {
+
+    private static final int BLACKJACK = 21;
+    private static final int ACE_SCORE_DIFFERENCE =
+            CardRank.ACE.getSpecialScore().value - CardRank.ACE.getScore().value;
 
     private final int value;
 
@@ -8,15 +14,31 @@ public class Score {
         this.value = value;
     }
 
-    public boolean isAbove(int bustCondition) {
-        return bustCondition < value;
+    public Score add(Score other) {
+        return new Score(this.value + other.value);
     }
 
-    public boolean isBiggerThan(Score target) {
-        return this.value > target.value;
+    public boolean canHit(Score threshold) {
+        return value <= threshold.value;
     }
 
-    public int getValue() {
-        return value;
+    public boolean isBust() {
+        return value > BLACKJACK;
+    }
+
+    private boolean isNotBust() {
+        return !isBust();
+    }
+
+    public boolean isBiggerThan(Score other) {
+        return this.value > other.value;
+    }
+
+    public Score adjustAceScore() {
+        Score newScore = new Score(this.value + ACE_SCORE_DIFFERENCE);
+        if (newScore.isNotBust()) {
+            return newScore;
+        }
+        return this;
     }
 }
--- src/test/java/blackjack/domain/rule/ScoreTest.java
+++ src/test/java/blackjack/domain/rule/ScoreTest.java
@@ -1,46 +1,126 @@
 package blackjack.domain.rule;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("점수 테스트")
 class ScoreTest {
 
+    @DisplayName("점수끼리 더한다.")
+    @Test
+    void testAdd() {
+        // given
+        Score score = new Score(1);
 
-    @DisplayName("점수가 파라미터를 넘는지 확인할 수 있다")
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5})
-    void testScoreAbove(int value) {
-        Score score = new Score(6);
-        assertThat(score.isAbove(value)).isTrue();
+        // when
+        Score actual = score.add(new Score(2));
+
+        // then
+        assertThat(actual).extracting("value").isEqualTo(3);
     }
 
-    @DisplayName("점수가 파라미터를 넘지 않는지 확인할 수 있다")
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5})
-    void testScoreBelow(int value) {
-        Score score = new Score(0);
-        assertThat(score.isAbove(value)).isFalse();
+    @DisplayName("주어진 한계값 이하이면 히트가 가능하다.")
+    @Test
+    void testCanHit() {
+        // given
+        Score score = new Score(2);
+
+        // when
+        boolean actual = score.canHit(new Score(2));
+
+        // then
+        assertThat(actual).isTrue();
     }
 
-    @DisplayName("더 낮은 점수와 비교할 수 있다")
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5})
-    void testScoreCompareWithBigger(int value) {
-        Score score = new Score(0);
-        Score target = new Score(value);
-        assertThat(score.isBiggerThan(target)).isFalse();
+    @DisplayName("주어진 한계값 초과이면 히트가 불가능하다.")
+    @Test
+    void testCanNotHit() {
+        // given
+        Score score = new Score(3);
+
+        // when
+        boolean actual = score.canHit(new Score(2));
+
+        // then
+        assertThat(actual).isFalse();
     }
 
-    @DisplayName("점수가 파라미터를 넘지 않는지 확인할 수 있다")
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5})
-    void testScoreCompareWithLower(int value) {
-        Score score = new Score(6);
-        Score target = new Score(value);
-        assertThat(score.isBiggerThan(target)).isTrue();
+    @DisplayName("블랙잭을 초과하면 버스트다.")
+    @Test
+    void testIsBust() {
+        // given
+        Score score = new Score(22);
+
+        // when
+        boolean actual = score.isBust();
+
+        // when & then
+        assertThat(actual).isTrue();
+    }
+
+    @DisplayName("블랙잭을 초과하지 않으면 버스트가 아니다.")
+    @Test
+    void testIsNotBust() {
+        // given
+        Score score = new Score(21);
+
+        // when
+        boolean actual = score.isBust();
+
+        // when & then
+        assertThat(actual).isFalse();
+    }
+
+    @DisplayName("주어진 점수보다 큼을 확인한다.")
+    @Test
+    void testisBiggerThan() {
+        // given
+        Score score = new Score(2);
+
+        // when
+        boolean actual = score.isBiggerThan(new Score(1));
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @DisplayName("주어진 점수보다 작음을 확인한다.")
+    @Test
+    void testIsSmallerThan() {
+        // given
+        Score score = new Score(2);
+
+        // when
+        boolean actual = score.isBiggerThan(new Score(3));
+
+        // then
+        assertThat(actual).isFalse();
+    }
+
+    @DisplayName("만약 합이 21을 초과하지 않으면 Ace는 11로 계산한다.")
+    @Test
+    void testAdjustAceScore() {
+        // given
+        Score score = new Score(11);
+
+        // when
+        Score actual = score.adjustAceScore();
+
+        // then
+        assertThat(actual).extracting("value").isEqualTo(21);
+    }
+
+    @DisplayName("만약 합이 21을 초과하면 Ace를 11로 계산하지 않는다.")
+    @Test
+    void testNotAdjustAceScore() {
+        // given
+        Score score = new Score(12);
+
+        // when
+        Score actual = score.adjustAceScore();
+
+        // then
+        assertThat(actual).extracting("value").isEqualTo(12);
     }
 }
