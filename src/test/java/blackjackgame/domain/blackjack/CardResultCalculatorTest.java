0b60f8514a7d82160e938d2219bc3627e43b3e09
BurningFalls <sherlocky31415@gmail.com>
2024-03-12 23:15:17 +0900

    test: BUST 결과가 잘 나오는지 테스트

--- src/test/java/blackjackgame/domain/blackjack/CardResultCalculatorTest.java
+++ src/test/java/blackjackgame/domain/blackjack/CardResultCalculatorTest.java
@@ -1,8 +1,11 @@
 package blackjackgame.domain.blackjack;
 
 import static blackjackgame.domain.blackjack.CardResult.BLACKJACK;
+import static blackjackgame.domain.blackjack.CardResult.BUST;
 import static blackjackgame.domain.card.CardName.ACE;
 import static blackjackgame.domain.card.CardName.JACK;
+import static blackjackgame.domain.card.CardName.KING;
+import static blackjackgame.domain.card.CardName.TWO;
 import static blackjackgame.domain.card.CardType.SPADE;
 
 import blackjackgame.domain.card.Card;
@@ -21,5 +24,16 @@ public class CardResultCalculatorTest {
         CardResult cardResult = CardResultCalculator.calculate(holdingCards);
 
         Assertions.assertThat(cardResult).isEqualTo(BLACKJACK);
+    }
+
+    @Test
+    @DisplayName("손패의 합이 21을 넘기면 결과가 BUST이다.")
+    void gameResultBustTest() {
+        HoldingCards holdingCards = HoldingCards.of(
+                new Card(JACK, SPADE), new Card(KING, SPADE), new Card(TWO, SPADE)
+        );
+        CardResult cardResult = CardResultCalculator.calculate(holdingCards);
+
+        Assertions.assertThat(cardResult).isEqualTo(BUST);
     }
 }
