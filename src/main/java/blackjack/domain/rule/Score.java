1f9bf86316d22a6f5786e5829fcafe5525e2b082
libienz <leekh10195@gmail.com>
2024-03-12 17:35:52 +0900

    feat: 최고 점수인지 확인하는 기능 구현

--- src/main/java/blackjack/domain/rule/Score.java
+++ src/main/java/blackjack/domain/rule/Score.java
@@ -2,6 +2,8 @@ package blackjack.domain.rule;
 
 public class Score {
 
+    private static final int MAX_SCORE = 21;
+
     private final int value;
 
     public Score(int value) {
@@ -10,6 +12,10 @@ public class Score {
 
     public boolean isAbove(Score target) {
         return this.value > target.value;
+    }
+
+    public boolean isMaxScore() {
+        return value == MAX_SCORE;
     }
 
     public int getValue() {
--- src/test/java/blackjack/domain/rule/ScoreTest.java
+++ src/test/java/blackjack/domain/rule/ScoreTest.java
@@ -3,6 +3,7 @@ package blackjack.domain.rule;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -25,5 +26,12 @@ class ScoreTest {
         Score score = new Score(6);
         Score target = new Score(value);
         assertThat(score.isAbove(target)).isTrue();
+    }
+
+    @DisplayName("최고 점수(21)인지 확인할 수 있다")
+    @Test
+    void testIsMaxScore() {
+        Score score = new Score(21);
+        assertThat(score.isMaxScore()).isTrue();
     }
 }
