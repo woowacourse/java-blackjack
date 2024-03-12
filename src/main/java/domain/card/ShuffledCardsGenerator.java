f849836732b2ed90206f2b427bb26526e6714a8f
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-12 14:48:53 +0900

    refactor : indent 1로 수정

--- src/main/java/domain/card/ShuffledCardsGenerator.java
+++ src/main/java/domain/card/ShuffledCardsGenerator.java
@@ -2,22 +2,31 @@ package domain.card;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class ShuffledCardsGenerator {
     private static final int DUPLICATES_COUNT = 6;
 
     public List<Card> generate() {
-        List<Card> cards = Stream.generate(() ->
-                        Stream.of(Symbol.values())
-                                .flatMap(symbol -> Rank.getRanks().stream()
-                                        .map(rank -> new Card(symbol, rank)))
-                ).limit(DUPLICATES_COUNT)
-                .flatMap(Function.identity())
-                .collect(Collectors.toList());
+        List<Card> cards = generateBlackJackCards();
         Collections.shuffle(cards);
         return cards;
+    }
+
+    private List<Card> generateBlackJackCards() {
+        List<Card> cards = generateCardsOfOnePack();
+
+        return IntStream.range(0, DUPLICATES_COUNT)
+                .mapToObj(i -> cards)
+                .flatMap(List::stream)
+                .toList();
+    }
+
+    private List<Card> generateCardsOfOnePack() {
+        return Stream.of(Symbol.values())
+                .flatMap(symbol -> Rank.getRanks().stream()
+                        .map(rank -> new Card(symbol, rank)))
+                .toList();
     }
 }
