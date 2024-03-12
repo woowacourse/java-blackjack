3904f80df99be9240aa75c806395dd2d7d183798
donghoony <aru0504@naver.com>
2024-03-12 20:08:55 +0900

    refactor(PlayerBet): 레코드 형식으로 수정

--- src/main/java/blackjack/money/PlayerBet.java
+++ src/main/java/blackjack/money/PlayerBet.java
@@ -1,19 +1,4 @@
 package blackjack.money;
 
-import blackjack.game.MatchResult;
-
-public class PlayerBet {
-
-    private final String name;
-    private final BetMoney betMoney;
-
-    public PlayerBet(String name, BetMoney betMoney) {
-        this.name = name;
-        this.betMoney = betMoney;
-    }
-
-    public int calculateProfit(MatchResult matchResult) {
-        int money = betMoney.getAmount();
-        return matchResult.calculateProfit(money);
-    }
+public record PlayerBet(String name, BetMoney betMoney) {
 }
