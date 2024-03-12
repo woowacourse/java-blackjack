ad8212717b4545d98e51255615e07eee5d867100
donghoony <aru0504@naver.com>
2024-03-12 13:14:18 +0900

    feat(Score): 게임의 점수를 표현

---
+++ src/main/java/blackjack/player/Score.java
@@ -0,0 +1,55 @@
+package blackjack.player;
+
+public class Score {
+
+    private static final int ADDITIONAL_ACE_SCORE = 10;
+    private static final int BLACKJACK_SCORE = 21;
+
+    private final int value;
+
+    public Score(int value) {
+        this.value = value;
+    }
+
+    public static Score zero() {
+        return new Score(0);
+    }
+
+    public Score add(int other) {
+        return new Score(value + other);
+    }
+
+    public Score add(Score other) {
+        return new Score(value + other.value);
+    }
+
+    public Score addAceScoreOnNotBust() {
+        if (value + ADDITIONAL_ACE_SCORE <= BLACKJACK_SCORE) {
+            return this.add(ADDITIONAL_ACE_SCORE);
+        }
+        return this;
+    }
+
+    public int toInt() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final Score score = (Score) o;
+
+        return value == score.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value;
+    }
+}
