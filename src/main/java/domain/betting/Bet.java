e1233eda12a478e46d3fe7b77c4da63dc60b251c
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-12 20:06:14 +0900

    feat: 베팅 getter, equals, hashCode 구현

--- src/main/java/domain/betting/Bet.java
+++ src/main/java/domain/betting/Bet.java
@@ -1,5 +1,7 @@
 package domain.betting;
 
+import java.util.Objects;
+
 class Bet {
     private static final int MAXIMUM_BETTING_MONEY = 1_000_000;
     private static final int MINIMUM_BETTING_MONEY = 1_000;
@@ -16,5 +18,26 @@ class Bet {
         if (money < MINIMUM_BETTING_MONEY || MAXIMUM_BETTING_MONEY < money) {
             throw new IllegalArgumentException(INVALID_BETTING_MESSAGE);
         }
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Bet bet = (Bet) o;
+        return money == bet.money;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
     }
 }
