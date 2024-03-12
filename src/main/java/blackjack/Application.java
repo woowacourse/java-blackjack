9f183288d707c42908036d2657fc2d85c3fa8544
lemone <lejinhy@gmail.com>
2024-03-12 15:29:27 +0900

    style: TODO 삭제

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -4,7 +4,6 @@ import blackjack.controller.BlackjackController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 메소드 위치 순서에 맞게 조정하기
         new BlackjackController().run();
     }
 }
