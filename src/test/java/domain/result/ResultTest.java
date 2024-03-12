fac51c027eaeeb285e1c032243256953ae7638a9
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-12 23:41:53 +0900

    refactor : 복잡한 결과 테스트를 간단하게 수정

--- src/test/java/domain/result/ResultTest.java
+++ src/test/java/domain/result/ResultTest.java
@@ -14,20 +14,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ResultTest {
-    Player pobi, jason, solar, neo;
+    Player pobi;
     Dealer dealer;
     Card clover6, heart8, spadeA, heart6, diamond8, cloverA, spadeK, clover7, spade8, heart2, clover9, diamond3;
 
     @BeforeEach
     void init() {
-        Name name1 = new Name("pobi");
-        Name name2 = new Name("jason");
-        Name name3 = new Name("solar");
-        Name name4 = new Name("neo");
-        pobi = new Player(name1);
-        jason = new Player(name2);
-        solar = new Player(name3);
-        neo = new Player(name4);
+        Name name = new Name("pobi");
+        pobi = new Player(name);
 
         dealer = new Dealer();
 
@@ -45,7 +39,7 @@ class ResultTest {
         diamond3 = new Card(Symbol.DIAMOND, Rank.THREE);
     }
 
-    @DisplayName("딜러가 버스트가 아니고, 블랙잭이 아닐 경우에 플레이어의 게임 결과를 구한다.")
+    @DisplayName("딜러가 버스트가 아니고, 블랙잭이 아닐 경우에 플레이어의 플레이어 우승 결과를 구한다.")
     @Test
     void getDealerIsNotBustCaseTest() {
         // given
@@ -53,37 +47,17 @@ class ResultTest {
         pobi.hit(clover9);
         pobi.hit(heart2); // pobi 점수 총합 : 14
 
-        jason.hit(spade8);
-        jason.hit(clover7);
-        jason.hit(spadeK); // jason 점수 총합 : 25 (버스트)
-
-        solar.hit(cloverA);
-        solar.hit(diamond8); // solar 점수 총합 : 19
-
-        neo.hit(heart6);
-        neo.hit(spadeA);
-        neo.hit(heart8);
-        neo.hit(clover6); // neo 점수 총합 : 21
-
         dealer.hit(clover9);
         dealer.hit(diamond8); // 딜러 점수 총합 : 17
 
         // when
         Result pobiResult = Result.getPlayerResultWith(pobi, dealer);
-        Result jasonResult = Result.getPlayerResultWith(jason, dealer);
-        Result solarResult = Result.getPlayerResultWith(solar, dealer);
-        Result neoResult = Result.getPlayerResultWith(neo, dealer);
 
         // then
-        assertAll(
-                () -> assertThat(pobiResult).isEqualTo(Result.LOSE),
-                () -> assertThat(jasonResult).isEqualTo(Result.LOSE),
-                () -> assertThat(solarResult).isEqualTo(Result.WIN),
-                () -> assertThat(neoResult).isEqualTo(Result.WIN)
-        );
+        assertThat(pobiResult).isEqualTo(Result.LOSE);
     }
 
-    @DisplayName("딜러가 버스트 일 때 플레이어의 게임 결과를 구한다.")
+    @DisplayName("딜러가 버스트 일 때 플레이어의 플레이어 우승 결과를 구한다.")
     @Test
     void getDealerIsBustCaseTest() {
         // given
@@ -91,72 +65,50 @@ class ResultTest {
         pobi.hit(clover9);
         pobi.hit(heart2); // pobi 점수 총합 : 14
 
-        jason.hit(spade8);
-        jason.hit(clover7);
-        jason.hit(spadeK); // jason 점수 총합 : 25 (버스트)
-
-        solar.hit(cloverA);
-        solar.hit(diamond8); // solar 점수 총합 : 19
-
-        neo.hit(heart6);
-        neo.hit(spadeA);
-        neo.hit(heart8);
-        neo.hit(clover6); // neo 점수 총합 : 21
-
         dealer.hit(clover9);
         dealer.hit(heart6);
         dealer.hit(diamond8); // 딜러 점수 총합 : 23
 
         // when
         Result pobiResult = Result.getPlayerResultWith(pobi, dealer);
-        Result jasonResult = Result.getPlayerResultWith(jason, dealer);
-        Result solarResult = Result.getPlayerResultWith(solar, dealer);
-        Result neoResult = Result.getPlayerResultWith(neo, dealer);
 
         // then
-        assertAll(
-                () -> assertThat(pobiResult).isEqualTo(Result.WIN),
-                () -> assertThat(jasonResult).isEqualTo(Result.LOSE),
-                () -> assertThat(solarResult).isEqualTo(Result.WIN),
-                () -> assertThat(neoResult).isEqualTo(Result.WIN)
-        );
+        assertThat(pobiResult).isEqualTo(Result.WIN);
     }
 
-    @DisplayName("딜러가 블랙잭 일 때 플레이어의 게임 결과를 구한다.")
+    @DisplayName("딜러가 블랙잭 일 때 플레이어의 플레이어 우승 결과를 구한다.")
     @Test
     void getDealerIsBlackJackCaseTest() {
         // given
         pobi.hit(diamond3);
         pobi.hit(clover9);
-        pobi.hit(heart2); // pobi 점수 총합 : 14
-
-        jason.hit(spade8);
-        jason.hit(clover7);
-        jason.hit(spadeK); // jason 점수 총합 : 25 (버스트)
-
-        solar.hit(cloverA);
-        solar.hit(spadeK); // solar 점수 총합 : 21 (블랙잭)
-
-        neo.hit(heart6);
-        neo.hit(spadeA);
-        neo.hit(heart8);
-        neo.hit(clover6); // neo 점수 총합 : 21
+        pobi.hit(heart2);
+        pobi.hit(clover7);// pobi 점수 총합 : 21
 
         dealer.hit(spadeK);
         dealer.hit(spadeA); // 딜러 점수 총합 : 21 (블랙잭)
 
         // when
         Result pobiResult = Result.getPlayerResultWith(pobi, dealer);
-        Result jasonResult = Result.getPlayerResultWith(jason, dealer);
-        Result solarResult = Result.getPlayerResultWith(solar, dealer);
-        Result neoResult = Result.getPlayerResultWith(neo, dealer);
 
         // then
-        assertAll(
-                () -> assertThat(pobiResult).isEqualTo(Result.LOSE),
-                () -> assertThat(jasonResult).isEqualTo(Result.LOSE),
-                () -> assertThat(solarResult).isEqualTo(Result.TIE),
-                () -> assertThat(neoResult).isEqualTo(Result.LOSE)
-        );
+        assertThat(pobiResult).isEqualTo(Result.LOSE);
+    }
+
+    @DisplayName("둘 다 버스트, 블랙잭이 아닐 때 점수로 비교하여 플레이어 우승 결과를 구한다.")
+    @Test
+    void compareScoreWithTest() {
+        // given
+        pobi.hit(diamond3);
+        pobi.hit(clover9);
+        pobi.hit(heart2); // pobi 점수 총합 : 14
+
+        dealer.hit(spadeK); // 딜러 점수 총합 : 10
+
+        // when
+        Result pobiResult = Result.getPlayerResultWith(pobi, dealer);
+
+        // then
+        assertThat(pobiResult).isEqualTo(Result.WIN);
     }
 }
