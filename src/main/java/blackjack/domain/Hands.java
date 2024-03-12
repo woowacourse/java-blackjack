3b47f8edbf6bfa3e7bb55bc01397ffcecd46f249
coli-geonwoo <kkwoo001021@naver.com>
2024-03-12 14:58:42 +0900

    refactor: 에이스 업그레이드 책임을 HandsScore로 이전

--- src/main/java/blackjack/domain/Hands.java
+++ src/main/java/blackjack/domain/Hands.java
@@ -2,55 +2,45 @@ package blackjack.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class Hands {
-    private static final int DOWNGRADE_SCORE = 10;
-
+    private static final HandsScore BLACK_JACK = new HandsScore(21);
     private final List<Card> hands;
-    private final HandsScore handsScore;
 
     public Hands() {
         this.hands = new ArrayList<>();
-        this.handsScore = new HandsScore();
     }
 
     public void addCard(Card card) {
-        handsScore.addScore(card.getScore());
         hands.add(card);
     }
 
-    public void downgradeAce() {
-        Optional<Card> aceHighCard = hands.stream()
-                .filter(card -> card.getValue() == Value.ACEHIGH)
-                .findAny();
-
-        if (aceHighCard.isPresent()) {
-            aceHighCard.get().downgradeAce();
-            handsScore.addScore(-DOWNGRADE_SCORE);
-            return;
+    public HandsScore getHandsScore() {
+        int score = calculateScoreSum();
+        if (this.hasAce()) {
+            return HandsScore.upgradeAceWhenNotBust(score);
         }
-        throw new IllegalStateException("다운그레이드 할 수 있는 에이스가 없습니다.");
-    }
-
-    public boolean hasHighAce() {
-        Optional<Value> aceHigh = hands.stream()
-                .map(Card::getValue)
-                .filter(value -> value == Value.ACEHIGH)
-                .findAny();
-        return aceHigh.isPresent();
+        return new HandsScore(score);
     }
 
     public boolean isBurst() {
-        return handsScore.isBurst();
+        return getHandsScore().isHigherThan(BLACK_JACK);
     }
 
     public boolean isBlackJack() {
-        return handsScore.isBlackJack();
+        return getHandsScore().isSame(BLACK_JACK);
     }
 
-    public int getHandsScore() {
-        return handsScore.getScore();
+    private boolean hasAce() {
+        return hands.stream()
+                .map(Card::getValue)
+                .anyMatch(value -> value == Value.ACE);
+    }
+
+    private int calculateScoreSum() {
+        return hands.stream()
+                .mapToInt(Card::getScore)
+                .sum();
     }
 
     public List<Card> getHands() {
