6d6894000fc50cc9889a1ef6a1734dd26afc0984
Zeus6768 <akaps6768@gmail.com>
2024-03-12 11:07:39 +0900

    refactor: Casino, BlackjackGameRunner를 BlackjackGame으로 통합

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,11 @@
-import domain.BlackjackGame;
-import domain.Casino;
+import game.BlackjackGame;
 import view.InputView;
 import view.ResultView;
 
 public class Application {
     public static void main(String[] args) throws Exception {
-        try(InputView inputView = new InputView()){
-            new Casino(inputView, new ResultView()).play(new BlackjackGame());
+        try (InputView inputView = new InputView()){
+            new BlackjackGame(inputView, new ResultView()).play();
         }
-
     }
 }
