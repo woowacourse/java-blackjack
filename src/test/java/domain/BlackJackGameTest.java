5dc00e5209ad322b066a670819be0fc711acb1be
nayonsoso <panda0329@naver.com>
2024-03-13 12:21:13 +0900

    refactor: 테스트 코드 실패 해결

--- src/test/java/domain/BlackJackGameTest.java
+++ src/test/java/domain/BlackJackGameTest.java
@@ -32,10 +32,10 @@ class BlackJackGameTest {
         Dealer dealer = new Dealer(cardDeck);
         BlackJackGame blackJackGame = new BlackJackGame(players, dealer);
 
-        blackJackGame.initHand(); // 산초 : A,K / 딜러 : Q, J
+        blackJackGame.initHand(); // 딜러 : A,K / 산초 : Q, J
         Map<Player, Result> resultMap = blackJackGame.getGameResults();
-        Result result1 = resultMap.get(new Player(name));
+        Result result = resultMap.get(new Player(name));
 
-        assertThat(result1).isEqualTo(Result.PLAYER_WIN);
+        assertThat(result).isEqualTo(Result.DEALER_WIN);
     }
 }
