c98b8baa6aa8ffb192b7813f14f7ef6972af4a66
jongmee <ra123221@gmail.com>
2024-03-14 00:56:50 +0900

    test: 플레이어가 지면 베팅 금액을 잃는다

--- src/test/java/blackjack/model/result/BettingMoneyTest.java
+++ src/test/java/blackjack/model/result/BettingMoneyTest.java
@@ -7,8 +7,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BettingMoneyTest {
     @Test
-    @DisplayName("승리하면 베팅 금액만큼 금액을 더 받는다")
-    void fixByMatchResultTest() {
+    @DisplayName("플레이어가 승리하면 베팅 금액만큼 금액을 더 받는다")
+    void determineWinnerFinalBettingMoneyTest() {
         // given
         BettingMoney bettingMoney = new BettingMoney(1000);
 
@@ -17,5 +17,18 @@ class BettingMoneyTest {
 
         // then
         assertThat(finalBettingMoney.getAmount()).isEqualTo(2000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 지면 베팅 금액을 잃는다")
+    void determineLoserFinalBettingMoneyTest() {
+        // given
+        BettingMoney bettingMoney = new BettingMoney(1000);
+
+        // when
+        BettingMoney finalBettingMoney = bettingMoney.fixByMatchResult(MatchResult.LOSE);
+
+        // then
+        assertThat(finalBettingMoney.getAmount()).isEqualTo(0);
     }
 }
