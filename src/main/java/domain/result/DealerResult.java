0e543f7561fcde07c064f8014af4df657d481f39
SCY <helenason@naver.com>
2024-03-13 17:33:51 +0900

    feat: 딜러 수익 계산 메서드 추가

--- src/main/java/domain/result/DealerResult.java
+++ src/main/java/domain/result/DealerResult.java
@@ -1,6 +1,8 @@
 package domain.result;
 
+import domain.Profit;
 import domain.WinState;
+import domain.gamer.Player;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -8,16 +10,28 @@ import java.util.Map;
 public class DealerResult {
 
     private final Map<WinState, Integer> result;
+    private Profit profitResult;
 
     public DealerResult() {
         this.result = new LinkedHashMap<>();
+        this.profitResult = new Profit(0);
     }
 
     public void addResult(WinState winState, int winStateCount) {
         result.put(winState, winStateCount);
     }
 
+    public void calculateProfit(Map<Player, Profit> playersProfit) {
+        profitResult = new Profit(-playersProfit.values().stream()
+                .mapToInt(Profit::getValue)
+                .sum());
+    }
+
     public Map<WinState, Integer> getResult() {
         return Collections.unmodifiableMap(result);
+    }
+
+    public Profit getProfitResult() {
+        return profitResult;
     }
 }
