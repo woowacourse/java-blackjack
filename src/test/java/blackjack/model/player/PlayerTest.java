d4a06e8854776ad6476332c0bd0c8d3275e7e52b
Choo <choo000407@naver.com>
2024-03-13 20:40:30 +0900

    test: 카드 합 관련한 승패 결정 테스트 추가

--- src/test/java/blackjack/model/player/PlayerTest.java
+++ src/test/java/blackjack/model/player/PlayerTest.java
@@ -42,8 +42,8 @@ class PlayerTest {
         // given
         Player player = new Player("dora");
         player.dealCards(new SequentialCardGenerator(List.of(
-                new Card(Suit.HEART, Denomination.QUEEN),
-                new Card(Suit.HEART, Denomination.QUEEN)
+                new Card(Suit.HEART, Denomination.KING),
+                new Card(Suit.HEART, Denomination.KING)
         )));
 
         // when
@@ -59,8 +59,8 @@ class PlayerTest {
         // given
         Player player = new Player("dora");
         player.dealCards(new SequentialCardGenerator(List.of(
-                new Card(Suit.HEART, Denomination.QUEEN),
-                new Card(Suit.HEART, Denomination.QUEEN)
+                new Card(Suit.HEART, Denomination.KING),
+                new Card(Suit.HEART, Denomination.KING)
         )));
 
         // when & then
@@ -134,5 +134,103 @@ class PlayerTest {
 
         // then
         assertThat(playerMatchResultOutcome.matchResult()).isEqualTo(MatchResult.PUSH);
+    }
+
+    @Test
+    @DisplayName("플레이어와 딜러 둘다 블랙잭이 아니고, 플레이어만 버스트면, 플레이어는 패배한다")
+    void determineMatchResultWhenPlayerOnlyBustTest() {
+        // given
+        Player player = new Player("dora");
+        player.dealCards(new SequentialCardGenerator(List.of(
+                new Card(Suit.HEART, Denomination.KING),
+                new Card(Suit.HEART, Denomination.KING)
+        )));
+        player.drawCard(() -> new Card(Suit.HEART, Denomination.KING));
+
+        Dealer dealer = new Dealer();
+        dealer.dealCards(new SequentialCardGenerator(List.of(
+                new Card(Suit.HEART, Denomination.KING),
+                new Card(Suit.HEART, Denomination.KING)
+        )));
+
+        // when
+        PlayerMatchResultOutcome playerMatchResultOutcome = player.determineMatchResult(dealer);
+
+        // then
+        assertThat(playerMatchResultOutcome.matchResult()).isEqualTo(MatchResult.LOSE);
+    }
+
+    @Test
+    @DisplayName("플레이어와 딜러 둘다 블랙잭이 아니고, 딜러만 버스트면, 플레이어는 승리한다")
+    void determineMatchResultWhenDealerOnlyBustTest() {
+        // given
+        Player player = new Player("dora");
+        player.dealCards(new SequentialCardGenerator(List.of(
+                new Card(Suit.HEART, Denomination.KING),
+                new Card(Suit.HEART, Denomination.KING)
+        )));
+
+        Dealer dealer = new Dealer();
+        dealer.dealCards(new SequentialCardGenerator(List.of(
+                new Card(Suit.HEART, Denomination.KING),
+                new Card(Suit.HEART, Denomination.FIVE)
+        )));
+        dealer.drawCards(() -> new Card(Suit.HEART, Denomination.KING));
+
+        // when
+        PlayerMatchResultOutcome playerMatchResultOutcome = player.determineMatchResult(dealer);
+
+        // then
+        assertThat(playerMatchResultOutcome.matchResult()).isEqualTo(MatchResult.WIN);
+    }
+
+    @Test
+    @DisplayName("플레이어와 딜러 둘다 블랙잭이 아니고, 플레이어와 딜러 둘다 버스트면, 플레이어는 패배한다")
+    void determineMatchResultWhenDealerAndPlayerBustTest() {
+        // given
+        Player player = new Player("dora");
+        player.dealCards(new SequentialCardGenerator(List.of(
+                new Card(Suit.HEART, Denomination.KING),
+                new Card(Suit.HEART, Denomination.KING)
+        )));
+        player.drawCard(() -> new Card(Suit.HEART, Denomination.KING));
+
+        Dealer dealer = new Dealer();
+        dealer.dealCards(new SequentialCardGenerator(List.of(
+                new Card(Suit.HEART, Denomination.KING),
+                new Card(Suit.HEART, Denomination.FIVE)
+        )));
+        dealer.drawCards(() -> new Card(Suit.HEART, Denomination.KING));
+
+        // when
+        PlayerMatchResultOutcome playerMatchResultOutcome = player.determineMatchResult(dealer);
+
+        // then
+        assertThat(playerMatchResultOutcome.matchResult()).isEqualTo(MatchResult.LOSE);
+    }
+
+    @Test
+    @DisplayName("플레이어와 딜러 둘다 블랙잭이 아니고, 플레이어와 딜러 둘다 버스트가 아니면, 플레이어는 딜러보다 카드 합이 더 높아야 승리한다")
+    void determineMatchResultWhenCompareScoreTest() {
+        // given
+        Player player = new Player("dora");
+        player.dealCards(new SequentialCardGenerator(List.of(
+                new Card(Suit.HEART, Denomination.KING),
+                new Card(Suit.HEART, Denomination.FIVE)
+        )));
+        player.drawCard(() -> new Card(Suit.HEART, Denomination.FIVE));
+
+        Dealer dealer = new Dealer();
+        dealer.dealCards(new SequentialCardGenerator(List.of(
+                new Card(Suit.HEART, Denomination.KING),
+                new Card(Suit.HEART, Denomination.FIVE)
+        )));
+        dealer.drawCards(() -> new Card(Suit.HEART, Denomination.FOUR));
+
+        // when
+        PlayerMatchResultOutcome playerMatchResultOutcome = player.determineMatchResult(dealer);
+
+        // then
+        assertThat(playerMatchResultOutcome.matchResult()).isEqualTo(MatchResult.WIN);
     }
 }
