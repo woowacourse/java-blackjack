50e22e75d6dcbd76fadf2d19c8abad5849257936
Choo <choo000407@naver.com>
2024-03-13 11:14:52 +0900

    refactor: MatchResult 결정 로직을 MatchResult 가 가지게 수정

--- src/main/java/blackjack/model/referee/MatchResult.java
+++ src/main/java/blackjack/model/referee/MatchResult.java
@@ -1,8 +1,39 @@
 package blackjack.model.referee;
 
+import blackjack.model.dealer.Dealer;
+import blackjack.model.player.Player;
+
 public enum MatchResult {
     BLACKJACK_WIN,
     WIN,
     LOSE,
     PUSH;
+
+    public static MatchResult determineMatchResult(final Dealer dealer, final Player player) {
+        if (dealer.isBlackJack() && player.isBlackJack()) {
+            return MatchResult.PUSH;
+        }
+        if (dealer.isBlackJack() && !player.isBlackJack()) {
+            return MatchResult.LOSE;
+        }
+        if (!dealer.isBlackJack() && player.isBlackJack()) {
+            return MatchResult.BLACKJACK_WIN;
+        }
+        return determineMatchResultByTotalScore(dealer, player);
+    }
+
+    private static MatchResult determineMatchResultByTotalScore(final Dealer dealer, final Player player) {
+        int playerTotal = player.calculateCardsTotalScore();
+        int dealerTotal = dealer.calculateCardsTotalScore();
+        if (player.isBust()) {
+            return MatchResult.LOSE;
+        }
+        if ((dealer.isBust() || playerTotal > dealerTotal)) {
+            return MatchResult.WIN;
+        }
+        if (playerTotal == dealerTotal) {
+            return MatchResult.PUSH;
+        }
+        return MatchResult.LOSE;
+    }
 }
--- src/main/java/blackjack/model/referee/Referee.java
+++ src/main/java/blackjack/model/referee/Referee.java
@@ -22,34 +22,6 @@ public class Referee {
     }
 
     private PlayerMatchResult determinePlayerMatchResult(final Player player) {
-        return new PlayerMatchResult(player.getName(), determineMatchResult(player));
-    }
-
-    private MatchResult determineMatchResult(final Player player) {
-        if (dealer.isBlackJack() && player.isBlackJack()) {
-            return MatchResult.PUSH;
-        }
-        if (dealer.isBlackJack()) {
-            return MatchResult.LOSE;
-        }
-        if (player.isBlackJack()) {
-            return MatchResult.BLACKJACK_WIN;
-        }
-        return determineMatchResultByTotalScore(player);
-    }
-
-    private MatchResult determineMatchResultByTotalScore(final Player player) {
-        int playerTotal = player.calculateCardsTotalScore();
-        int dealerTotal = dealer.calculateCardsTotalScore();
-        if (player.isBust()) {
-            return MatchResult.LOSE;
-        }
-        if (dealer.isBust() || playerTotal > dealerTotal) {
-            return MatchResult.WIN;
-        }
-        if (playerTotal == dealerTotal) {
-            return MatchResult.PUSH;
-        }
-        return MatchResult.LOSE;
+        return new PlayerMatchResult(player.getName(), MatchResult.determineMatchResult(dealer, player));
     }
 }
