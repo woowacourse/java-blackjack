82214d79c752bb31759ca2e761a93f78ad8a4a55
gyungchan Jo <namejgc@naver.com>
2024-03-13 18:56:05 +0900

    feat(Bust) Bust 상태 구현

---
+++ src/main/java/blackjack/domain/state/Bust.java
@@ -0,0 +1,22 @@
+package blackjack.domain.state;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardHand;
+
+public class Bust implements State {
+    private final CardHand cardHand;
+
+    public Bust(final CardHand cardHand) {
+        this.cardHand = cardHand;
+    }
+
+    @Override
+    public State draw(final Card card) {
+        throw new UnsupportedOperationException("해당 플레이어의 이미 턴이 종료되었습니다.");
+    }
+
+    @Override
+    public int calculateScore() {
+        return cardHand.sumAllScore();
+    }
+}
