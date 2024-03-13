12ba5dbb3bcdd5c70577349fa93e1945dfedcf00
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-14 00:24:27 +0900

    test(PlayerResultTest): 검증 기능 테스트 추가

--- src/test/java/blackjack/model/result/PlayersResultTest.java
+++ src/test/java/blackjack/model/result/PlayersResultTest.java
@@ -1,10 +1,12 @@
 package blackjack.model.result;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import blackjack.model.gamer.Name;
 import blackjack.model.gamer.Player;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
@@ -25,5 +27,16 @@ class PlayersResultTest {
 
         //then
         assertThat(playerResult).isEqualTo(result);
+    }
+
+    @DisplayName("플레이어의 결과 확인 시, 등록된 결과가 없을 경우 예외를 발생시킨다.")
+    @Test
+    void findPlayerResultByNoneResult() {
+        //given
+        Player player = Player.of(Name.from("test"), 100);
+
+        //when, then
+        assertThatThrownBy(() -> playersResult.findPlayerResult(player))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
