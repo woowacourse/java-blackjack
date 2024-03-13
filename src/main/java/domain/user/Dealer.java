73a012d4ae29ab7f02857d80209e3128db5cfb40
tackyu <tack970913@gmail.com>
2024-03-13 12:45:19 +0900

    refactor: DealerDeck 클래스 제거

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -1,21 +1,25 @@
 package domain.user;
 
 import domain.card.Card;
-import domain.deck.DealerDeck;
+import domain.deck.UserDeck;
+
+import java.util.List;
 
 public class Dealer extends User {
+    private static final int FIRST_INDEX = 0;
+    private static final int DEALER_CARD_CONDITION = 16;
+
     Dealer() {
-        super(new DealerDeck(), new Name("딜러"));
+        super(new UserDeck(), new Name("딜러"));
     }
 
     public Card getVisibleCard() {
-        DealerDeck deck = (DealerDeck) userDeck;
-        return deck.getFirstCard();
+        List<Card> dealerDeck = userDeck.getCards();
+        return dealerDeck.get(FIRST_INDEX);
     }
 
     public boolean isDealerCardAddable() {
-        DealerDeck deck = (DealerDeck) userDeck;
-        return deck.isAddable();
+        return userDeck.sumCard() <= DEALER_CARD_CONDITION;
     }
 
 }
