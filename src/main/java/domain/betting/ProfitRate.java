e18069e63af89378b7edc50e5547e19092dbca94
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-13 13:38:51 +0900

    refactor: profit -> profitRate

--- src/main/java/domain/betting/ProfitRate.java
+++ src/main/java/domain/betting/ProfitRate.java
@@ -4,30 +4,30 @@ import domain.game.Result;
 import java.util.Arrays;
 import java.util.function.Predicate;
 
-public enum Profit {
+public enum ProfitRate {
 
     WIN_PROFIT(Result::isPlayerWin, 1.0),
     LOSS_PROFIT(Result::isDealerWin, -1.0),
     PUSH_PROFIT(Result::isPush, 0),
-    BALCK_JACK_PROFIT(Result::isPlayerBlackJack, 1.5);
+    BLACK_JACK_PROFIT(Result::isPlayerBlackJack, 1.5);
 
     private final Predicate<Result> judgeProfit;
     private final double rate;
 
-    Profit(final Predicate<Result> judgeProfit, final double some) {
+    ProfitRate(final Predicate<Result> judgeProfit, final double rate) {
         this.judgeProfit = judgeProfit;
-        this.rate = some;
+        this.rate = rate;
     }
 
-    public static Profit of(final Result result) {
-        return Arrays.stream(Profit.values())
+    public static ProfitRate of(final Result result) {
+        return Arrays.stream(ProfitRate.values())
                 .filter(value -> value.judgeProfit.test(result))
                 .findAny()
                 .orElseThrow();
 
     }
 
-    public int getMoney(final Bet bet) {
-        return (int) (bet.getMoney() * rate);
+    public double get() {
+        return rate;
     }
 }
--- src/test/java/domain/betting/ProfitRateTest.java
+++ src/test/java/domain/betting/ProfitRateTest.java
@@ -7,45 +7,41 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class ProfitTest {
+public class ProfitRateTest {
 
     @Nested
-    @DisplayName("bet와 Result를 통해 수익을 생성한다.")
-    class createProfit {
+    @DisplayName("bet와 Result를 통해 수익률을 생성한다.")
+    class createProfitRate {
         @Test
         @DisplayName("Result가 플레이어승이면 베팅만큼 수익이 발생한다.")
         void createPlayerWin() {
-            Bet bet = new Bet(20000);
-            Profit profit = Profit.of(Result.PLAYER_WIN);
+            ProfitRate profitRate = ProfitRate.of(Result.PLAYER_WIN);
 
-            assertThat(profit.getMoney(bet)).isEqualTo(bet.getMoney());
+            assertThat(profitRate.get()).isEqualTo(1.0);
         }
 
         @Test
         @DisplayName("Result가 딜러승이면 베팅만큼 -수익이 발생한다.")
         void createDealerWin() {
-            Bet bet = new Bet(20000);
-            Profit profit = Profit.of(Result.DEALER_WIN);
+            ProfitRate profitRate = ProfitRate.of(Result.DEALER_WIN);
 
-            assertThat(profit.getMoney(bet)).isEqualTo(bet.getMoney() * -1);
+            assertThat(profitRate.get()).isEqualTo(-1.0);
         }
 
         @Test
         @DisplayName("Result가 Push면 수익이 0이다")
         void createPush() {
-            Bet bet = new Bet(20000);
-            Profit profit = Profit.of(Result.PUSH);
+            ProfitRate profitRate = ProfitRate.of(Result.PUSH);
 
-            assertThat(profit.getMoney(bet)).isEqualTo(0);
+            assertThat(profitRate.get()).isEqualTo(0);
         }
 
         @Test
         @DisplayName("Result가 PlayerBlackJack이면 베팅의 1.5배의 수익이 발생한다.")
         void createPlayerBlackJack() {
-            Bet bet = new Bet(20000);
-            Profit profit = Profit.of(Result.PLAYER_BLACK_JACK);
+            ProfitRate profitRate = ProfitRate.of(Result.PLAYER_BLACK_JACK);
 
-            assertThat(profit.getMoney(bet)).isEqualTo(30000);
+            assertThat(profitRate.get()).isEqualTo(1.5);
         }
     }
 }
