08040168ea7a4b6b2f37742dc15921fa0be1c561
dwax1324 <dwax1324@gmail.com>
2024-03-13 12:32:44 +0900

    refactor: dto 패키지 내부에 domain 의존성 제거

--- src/main/java/dto/CardResponse.java
+++ src/main/java/dto/CardResponse.java
@@ -1,10 +1,4 @@
 package dto;
 
-import domain.card.Card;
-
 public record CardResponse(String rank, String suit) {
-
-    public static CardResponse of(final Card card) {
-        return new CardResponse(card.getRank().name(), card.getSymbol().name());
-    }
 }
