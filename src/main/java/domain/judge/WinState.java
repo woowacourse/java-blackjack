3e0ed1084986610fd347db13173c11f5cd437604
yechop <channetattoo@gmail.com>
2024-03-13 14:13:00 +0900

    refactor: 승패여부가 베팅 계산 비율을 알고있도록 리팩토링

--- src/main/java/domain/judge/WinState.java
+++ src/main/java/domain/judge/WinState.java
@@ -2,8 +2,18 @@ package domain.judge;
 
 public enum WinState {
 
-    WIN,
-    DRAW,
-    LOSE,
-    BLACK_JACK;
+    WIN(1),
+    DRAW(0),
+    LOSE(-1),
+    BLACK_JACK(1.5);
+
+    private final double profitRate;
+
+    WinState(double profitRate) {
+        this.profitRate = profitRate;
+    }
+
+    public double getProfitRate() {
+        return profitRate;
+    }
 }
