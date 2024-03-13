ebcbe859c6b4ad768c0e4a5a7b7895af2b8c8932
HyungHoKim <hkim1109@naver.com>
2024-03-13 13:42:47 +0900

    test: 승패 결과에 따라 베팅 금액을 곱하는 기능 테스트 및 구현

--- src/main/java/domain/user/BetAmount.java
+++ src/main/java/domain/user/BetAmount.java
@@ -1,7 +1,17 @@
 package domain.user;
 
+import domain.game.GameResult;
+
 public record BetAmount(int value) {
-    public BetAmount times(double multiplier) {
+    public BetAmount change(GameResult gameResult) {
+        return switch (gameResult) {
+            case WIN -> times(1);
+            case DRAW -> times(0);
+            case LOSE -> times(-1);
+        };
+    }
+
+    private BetAmount times(double multiplier) {
         return new BetAmount((int) (this.value * multiplier));
     }
 }
--- src/test/java/domain/user/BetAmountTest.java
+++ src/test/java/domain/user/BetAmountTest.java
@@ -1,5 +1,8 @@
 package domain.user;
 
+import static domain.game.GameResult.DRAW;
+import static domain.game.GameResult.LOSE;
+import static domain.game.GameResult.WIN;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
@@ -7,12 +10,32 @@ import org.junit.jupiter.api.Test;
 
 public class BetAmountTest {
     @Test
-    @DisplayName("베팅 금액을 입력한 배수로 곱한다.")
-    void betAmountTest() {
+    @DisplayName("승리 시, 베팅 금액만큼 금액을 얻는다.")
+    void betAmountTimesTest() {
         BetAmount betAmount = new BetAmount(10000);
 
-        BetAmount newAmount = betAmount.times(1.5);
+        BetAmount newAmount = betAmount.change(WIN);
 
-        assertThat(newAmount.value()).isEqualTo(15000);
+        assertThat(newAmount.value()).isEqualTo(10000);
+    }
+
+    @Test
+    @DisplayName("무승부 시, 0원이 된다.")
+    void betAmountDrawTest() {
+        BetAmount betAmount = new BetAmount(10000);
+
+        BetAmount newAmount = betAmount.change(DRAW);
+
+        assertThat(newAmount.value()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("패배 시, 베팅 금액을 잃는다.")
+    void betAmountLoseTest() {
+        BetAmount betAmount = new BetAmount(10000);
+
+        BetAmount newAmount = betAmount.change(LOSE);
+
+        assertThat(newAmount.value()).isEqualTo(-10000);
     }
 }
