1d260512bb1590331c88ebcca6d718e5d069583a
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-13 23:54:10 +0900

    feat(DealerBetWallet): 도메인 구현

---
+++ src/main/java/blackjack/model/wallet/DealerBetWallet.java
@@ -0,0 +1,37 @@
+package blackjack.model.wallet;
+
+import blackjack.model.gamer.Player;
+
+public class DealerBetWallet {
+
+    private final int playersBetAmount;
+    private int payoutAmount;
+
+    private DealerBetWallet(int playersBetAmount) {
+        validateNotNegative(playersBetAmount);
+        this.playersBetAmount = playersBetAmount;
+        this.payoutAmount = 0;
+    }
+
+    public static DealerBetWallet from(int playersBetAmount) {
+        return new DealerBetWallet(playersBetAmount);
+    }
+
+    public void registerPayoutAmount(Player player) {
+        payoutAmount += player.profitAmount();
+    }
+
+    public int calculateNetProfit() {
+        return playersBetAmount - payoutAmount;
+    }
+
+    private void validateNotNegative(int playersBetAmount) {
+        if (playersBetAmount < 0) {
+            throw new IllegalArgumentException("[ERROR] 플레이어 배팅 금액은 음수일 수 없습니다.");
+        }
+    }
+
+    public int getPayoutAmount() {
+        return payoutAmount;
+    }
+}
