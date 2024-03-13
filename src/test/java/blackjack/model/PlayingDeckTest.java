cff1de32bd4aecaec8c830d07b61814a223643b8
ashsty <qkralsdk991@naver.com>
2024-03-13 14:17:08 +0900

    test: card shuffle 테스트 추가

--- src/test/java/blackjack/model/PlayingDeckTest.java
+++ src/test/java/blackjack/model/PlayingDeckTest.java
@@ -1,17 +1,37 @@
 package blackjack.model;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import blackjack.model.card.Card;
+import blackjack.model.card.Pattern;
+import blackjack.model.card.Rank;
 import blackjack.model.deck.DeckGenerator;
 import blackjack.model.deck.PlayingDeck;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayingDeckTest {
+
+    @DisplayName("playingDeck은 List<Card>를 shuffle한다.")
+    @Test
+    void shuffleCard() {
+        //given
+        List<Card> generatedDeck = DeckGenerator.generateDeck();
+        PlayingDeck playingDeck = new PlayingDeck(generatedDeck);
+
+        Card card = playingDeck.drawCard();
+        Card firstCard = new Card(Pattern.SPADE, Rank.ACE);
+
+        //when
+        boolean isShuffled = !card.equals(firstCard);
+
+        //then
+        assertThat(isShuffled).isTrue();
+    }
 
     @DisplayName("덱에서 카드를 한 장 뽑는다.")
     @Test
