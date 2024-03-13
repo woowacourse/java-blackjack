20f218415dfdc3faf9fcf2db62935818992ce570
jongmee <ra123221@gmail.com>
2024-03-14 00:53:47 +0900

    feat: 승리하면 베팅 금액만큼 금액을 더 받는다

---
+++ src/main/java/blackjack/model/result/BettingMoney.java
@@ -0,0 +1,28 @@
+package blackjack.model.result;
+
+public final class BettingMoney {
+    private static final BettingMoney ZERO_MONEY = new BettingMoney(0);
+
+    private final double amount;
+
+    public BettingMoney(final double amount) {
+        this.amount = amount;
+    }
+
+    public BettingMoney fixByMatchResult(final MatchResult matchResult) {
+        if (matchResult == MatchResult.LOSE) {
+            return ZERO_MONEY;
+        } else if (matchResult == MatchResult.WIN) {
+            return this.plus(this);
+        }
+        return this;
+    }
+
+    public BettingMoney plus(final BettingMoney otherMoney) {
+        return new BettingMoney(amount + otherMoney.amount);
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
