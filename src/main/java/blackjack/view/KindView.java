43682f4c24cf65be45aaa48c8b10541c4b933d0e
coli-geonwoo <kkwoo001021@naver.com>
2024-03-13 22:13:49 +0900

    refactor: 스트림의 orElseThrow를 활용한 예외처리

--- src/main/java/blackjack/view/KindView.java
+++ src/main/java/blackjack/view/KindView.java
@@ -2,7 +2,6 @@ package blackjack.view;
 
 import blackjack.domain.card.Kind;
 
-import java.util.Optional;
 import java.util.stream.Stream;
 
 public enum KindView {
@@ -18,15 +17,12 @@ public enum KindView {
         this.kind = kind;
         this.kindName = kindName;
     }
-    // TODO Optional의 orElseThrow
-    public static String findKindName(Kind kind) {
-        Optional<KindView> targetKindView = Stream.of(values())
-                .filter(view -> view.kind == kind)
-                .findAny();
 
-        if (targetKindView.isPresent()) {
-            return targetKindView.get().kindName;
-        }
-        throw new IllegalStateException("존재하지 않는 카드 종류를 조회하였습니다.");
+    public static String findKindName(Kind targetKind) {
+        KindView targetKindView = Stream.of(values())
+                .filter(view -> view.kind == targetKind)
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException("존재하지 않는 카드 종류를 조회하였습니다."));
+        return targetKindView.kindName;
     }
 }
