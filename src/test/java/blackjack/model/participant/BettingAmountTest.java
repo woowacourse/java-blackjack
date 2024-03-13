8f60a4754e4e189dc55e6bce1b86ef8d32494d15
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-14 01:35:31 +0900

    test(BettingAmount): 배팅 금액의 곱을 구하는 테스트 작성

--- src/test/java/blackjack/model/participant/BettingAmountTest.java
+++ src/test/java/blackjack/model/participant/BettingAmountTest.java
@@ -1,5 +1,6 @@
 package blackjack.model.participant;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,5 +14,12 @@ class BettingAmountTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new BettingAmount(-1))
                 .withMessage("배팅 금액은 양수만 가능합니다.");
+    }
+
+    @Test
+    @DisplayName("배팅 금액의 곱을 구한다.")
+    void multiple() {
+        BettingAmount bettingAmount = new BettingAmount(1_000);
+        assertThat(bettingAmount.multiple(1.5)).isEqualTo(new BettingAmount(1_500));
     }
 }
