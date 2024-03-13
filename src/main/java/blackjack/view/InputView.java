f7e64750b079d21065e32d034d96ca59d5b116db
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-03-13 13:56:00 +0900

    refactor: inputView 개행 상수화

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -9,11 +9,12 @@ import java.util.List;
 
 public class InputView {
 
+    private static final String NEW_LINE = System.lineSeparator();
     private static final String PLAYER_NAME_INPUT = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-    private static final String MORE_CARD_CHOICE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String MORE_CARD_CHOICE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)" + NEW_LINE;
     private static final String NAME_SEPARATOR = ",";
     private static final String IOEXCEPTION_ERROR = "입력 과정 도중 에러가 발생했습니다.";
-    public static final String BETTING_AMOUNT_INPUT = "%s의 배팅 금액은?";
+    public static final String BETTING_AMOUNT_INPUT = "%s의 배팅 금액은?" + NEW_LINE;
 
     private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     private static final InputView instance = new InputView();
@@ -38,7 +39,7 @@ public class InputView {
     }
 
     public String readMoreCardChoice(final String name) {
-        System.out.printf(MORE_CARD_CHOICE + System.lineSeparator(), name);
+        System.out.printf(MORE_CARD_CHOICE, name);
         try {
             return bufferedReader.readLine();
         } catch (IOException exception) {
@@ -55,7 +56,7 @@ public class InputView {
     }
 
     private PlayerInfo readBettingAmount(final String name) {
-        System.out.printf(BETTING_AMOUNT_INPUT + System.lineSeparator(), name);
+        System.out.printf(BETTING_AMOUNT_INPUT, name);
         try {
             String input = bufferedReader.readLine();
             System.out.println();
