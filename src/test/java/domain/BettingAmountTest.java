3a9ac742b6ead4832e49855ed73a19069b320d33
HyunJoong Kim <hjk0761@naver.com>
2024-03-13 23:53:01 +0900

    test: 정적 임포트로 수정

--- src/test/java/domain/BettingAmountTest.java
+++ src/test/java/domain/BettingAmountTest.java
@@ -1,12 +1,12 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static domain.BettingAmount.MAX_AMOUNT;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BettingAmountTest {
     @DisplayName("유효하지 않은 배팅 금액이 입력되면 예외를 발생시킨다.")
@@ -15,7 +15,7 @@ public class BettingAmountTest {
     @ValueSource(strings = {"1million", "100000001", "-15000", "0"})
     void playerNameLengthTest(String invalidAmount) {
         // When & Then
-        Assertions.assertThatThrownBy(() -> new BettingAmount(invalidAmount))
+        assertThatThrownBy(() -> new BettingAmount(invalidAmount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(String.format("배팅 금액은 %d원 이하의 양의 정수만 가능합니다.", MAX_AMOUNT));
     }
