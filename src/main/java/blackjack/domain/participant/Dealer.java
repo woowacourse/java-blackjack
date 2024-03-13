f0361880bed313fff6b39f962135339f26237c4c
gyungchan Jo <namejgc@naver.com>
2024-03-13 21:33:31 +0900

    refactor(Dealer): 카드를 셔플하는 기능 변경

--- src/main/java/blackjack/domain/participant/Dealer.java
+++ src/main/java/blackjack/domain/participant/Dealer.java
@@ -9,15 +9,13 @@ public class Dealer extends Participant {
     private static final int DEALER_MIN_SCORE_POLICY = 16;
 
     private final CardDeck cardDeck;
-    private final CardShuffleStrategy cardShuffleStrategy;
 
-    public Dealer(final CardDeck cardDeck, final CardShuffleStrategy cardShuffleStrategy) {
+    public Dealer(final CardDeck cardDeck) {
         super(DEFAULT_NAME_OF_DEALER);
         this.cardDeck = cardDeck;
-        this.cardShuffleStrategy = cardShuffleStrategy;
     }
 
-    public void shuffleCards() {
+    public void shuffleCards(final CardShuffleStrategy cardShuffleStrategy) {
         cardDeck.shuffle(cardShuffleStrategy);
     }
 
--- src/test/java/blackjack/domain/participant/DealerTest.java
+++ src/test/java/blackjack/domain/participant/DealerTest.java
@@ -4,9 +4,7 @@ import blackjack.domain.card.Card;
 import blackjack.domain.card.CardDeck;
 import blackjack.domain.card.Denomination;
 import blackjack.domain.card.Suit;
-import blackjack.domain.strategy.CardShuffleStrategy;
 import blackjack.domain.strategy.ReverseCardShuffleStrategy;
-import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,9 +16,8 @@ class DealerTest {
     @BeforeEach
     void setUp() {
         final CardDeck cardDeck = new CardDeck();
-        final CardShuffleStrategy cardShuffleStrategy = new ReverseCardShuffleStrategy();
 
-        dealer = new Dealer(cardDeck, cardShuffleStrategy);
+        dealer = new Dealer(cardDeck);
     }
 
     @Test
@@ -32,7 +29,7 @@ class DealerTest {
 
     @Test
     void 딜러는_카드를_셔플할_수_있다() {
-        dealer.shuffleCards();
+        dealer.shuffleCards(new ReverseCardShuffleStrategy());
         final Card card = dealer.pickCard();
 
         assertThat(card).isEqualTo(new Card(Suit.HEART, Denomination.ACE));
