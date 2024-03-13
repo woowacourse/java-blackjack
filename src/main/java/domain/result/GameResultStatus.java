1de85afa1ed4fc978e2783cbfc3fed8d469a5c49
yoonjuho <dbswn990@naver.com>
2024-03-13 17:24:39 +0900

    refactor(GameResultStatus): 상태와 행위를 한곳에서 관리

--- src/main/java/domain/result/GameResultStatus.java
+++ src/main/java/domain/result/GameResultStatus.java
@@ -1,26 +1,36 @@
 package domain.result;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+import static domain.BlackjackGame.BLACKJACK_SCORE;
+
 public enum GameResultStatus {
 
-    WIN("승"), LOSE("패"), PUSH("무");
+    WIN("승", (standardTarget, comparisonTarget) -> standardTarget <= BLACKJACK_SCORE
+            && (comparisonTarget > BLACKJACK_SCORE || (comparisonTarget < standardTarget))),
+    PUSH("무", (standardTarget, comparisonTarget) -> (comparisonTarget > BLACKJACK_SCORE
+            && standardTarget > BLACKJACK_SCORE) || (comparisonTarget == standardTarget)),
+    LOSE("패", (standardTarget, comparisonTarget) -> comparisonTarget > standardTarget);
+
 
     private final String value;
+    private final BiFunction<Integer, Integer, Boolean> match;
 
-    GameResultStatus(String value) {
+    GameResultStatus(String value, BiFunction<Integer, Integer, Boolean> match) {
         this.value = value;
+        this.match = match;
     }
 
-    public GameResultStatus opposite() {
-        if (this == WIN) {
-            return LOSE;
-        }
-        if (this == LOSE) {
-            return WIN;
-        }
-        return PUSH;
+    public static GameResultStatus comparedTo(Integer standardTarget, Integer comparisonTarget) {
+        return Arrays.stream(values())
+                     .filter(status -> status.match.apply(standardTarget, comparisonTarget))
+                     .findFirst()
+                     .orElse(LOSE);
     }
 
     public String getValue() {
         return value;
     }
+
 }
---
+++ src/test/java/domain/result/GameResultStatusTest.java
@@ -0,0 +1,63 @@
+package domain.result;
+
+import domain.card.Card;
+import domain.card.Cards;
+import domain.participant.Dealer;
+import domain.participant.Name;
+import domain.participant.Participant;
+import domain.participant.Player;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static domain.card.CardNumber.*;
+import static domain.card.CardShape.SPADE;
+import static domain.result.GameResultStatus.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameResultStatusTest {
+    @DisplayName("참가자들의 카드를 비교해 승패무를 정한다.")
+    @MethodSource
+    @ParameterizedTest
+    void resultStatusOf(Cards dealerCards, Cards playerCards, GameResultStatus expected) {
+        Dealer dealer = new Dealer(dealerCards);
+        Player player = new Player(new Name("hotea"));
+        receiveCards(dealer, dealerCards);
+        receiveCards(player, playerCards);
+        GameResultStatus status = GameResultStatus.comparedTo(player.score(), dealer.score());
+        assertThat(status).isEqualTo(expected);
+    }
+
+    private void receiveCards(Participant participant, Cards cards) {
+        for (Card card : cards.toList()) {
+            participant.receive(card);
+        }
+    }
+
+    static Stream<Arguments> resultStatusOf() {
+        return Stream.of(
+                Arguments.of(cardOf22(), cardOf22(), PUSH),
+                Arguments.of(cardOf22(), cardOf20(), WIN),
+                Arguments.of(cardOf20(), cardOf22(), LOSE),
+                Arguments.of(cardOf20(), cardOf15(), LOSE),
+                Arguments.of(cardOf15(), cardOf20(), WIN),
+                Arguments.of(cardOf20(), cardOf20(), PUSH)
+        );
+    }
+
+    static Cards cardOf22() {
+        return new Cards(List.of(new Card(SPADE, KING), new Card(SPADE, KING), new Card(SPADE, TWO)));
+    }
+
+    static Cards cardOf20() {
+        return new Cards(List.of(new Card(SPADE, KING), new Card(SPADE, KING)));
+    }
+
+    static Cards cardOf15() {
+        return new Cards(List.of(new Card(SPADE, KING), new Card(SPADE, FIVE)));
+    }
+}
