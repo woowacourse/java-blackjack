119ca9f2665bade8584df37497a7744cba950efa
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-13 17:38:51 +0900

    feat(BettingPool): 플레이어가 만약 블랙잭으로 무승부인 경우, 베팅 금액을 돌려받는 기능 구현

--- src/main/java/blackjack/model/BettingPool.java
+++ src/main/java/blackjack/model/BettingPool.java
@@ -6,14 +6,21 @@ import java.util.Map;
 
 public class BettingPool {
     private static final double BLACKJACK_PROFIT_RATE = 1.5;
+
     private final Map<Player, BettingAmount> moneys;
 
     public BettingPool(final Map<Player, BettingAmount> moneys) {
         this.moneys = moneys;
     }
 
-    public BettingAmount giveBlackJackMoney(final Player player) {
+    public BettingAmount giveWinnerMoneyByBlackJack(final Player player) {
         BettingAmount bettingAmount = moneys.get(player);
         return bettingAmount.multiple(BLACKJACK_PROFIT_RATE);
+    }
+
+    public BettingAmount giveTieMoneyByBlackJack(final Player player) {
+        BettingAmount bettingAmount = moneys.get(player);
+        // TODO: 딜러 계산을 위해 초기값에서 빼야함
+        return bettingAmount;
     }
 }
--- src/test/java/blackjack/model/BettingPoolTest.java
+++ src/test/java/blackjack/model/BettingPoolTest.java
@@ -17,12 +17,23 @@ class BettingPoolTest {
 
     @Test
     @DisplayName("플레이어가 만약 블랙잭으로 승리한 경우, 베팅 금액의 1.5배를 딜러에게 받는다.")
-    void winByBlackJack() {
+    void giveWinnerMoneyByBlackJack() {
         Player player = Player.of("몰리", new Hand(List.of(new Card(Shape.DIA, Score.TEN), new Card(Shape.DIA, Score.ACE))));
         BettingAmount bettingAmount = new BettingAmount(1000);
         Map<Player, BettingAmount> moneyByPlayer = Map.of(player, bettingAmount);
 
         BettingPool bettingPool = new BettingPool(moneyByPlayer);
-        assertThat(bettingPool.giveBlackJackMoney(player)).isEqualTo(bettingAmount.multiple(1.5));
+        assertThat(bettingPool.giveWinnerMoneyByBlackJack(player)).isEqualTo(bettingAmount.multiple(1.5));
+    }
+
+    @Test
+    @DisplayName("플레이어가 만약 블랙잭으로 무승부인 경우, 베팅 금액을 돌려받는다.")
+    void giveTieMoneyByBlackJack() {
+        Player player = Player.of("몰리", new Hand(List.of(new Card(Shape.DIA, Score.TEN), new Card(Shape.DIA, Score.ACE))));
+        BettingAmount bettingAmount = new BettingAmount(1000);
+        Map<Player, BettingAmount> moneyByPlayer = Map.of(player, bettingAmount);
+
+        BettingPool bettingPool = new BettingPool(moneyByPlayer);
+        assertThat(bettingPool.giveTieMoneyByBlackJack(player)).isEqualTo(bettingAmount);
     }
 }
