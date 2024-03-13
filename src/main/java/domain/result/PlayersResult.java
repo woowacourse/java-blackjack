91d055fad5c1b68086503ca2f37927b88a073b58
SCY <helenason@naver.com>
2024-03-13 13:52:05 +0900

    feat: 게임 결과를 통해 수익률 계산하는 메서드 추가

--- src/main/java/domain/result/PlayersResult.java
+++ src/main/java/domain/result/PlayersResult.java
@@ -1,21 +1,48 @@
 package domain.result;
 
+import domain.Profit;
 import domain.WinState;
 import domain.gamer.Player;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class PlayersResult {
 
     private final Map<Player, WinState> result;
+    private final Map<Player, Profit> profitResult;
 
     public PlayersResult() {
         this.result = new LinkedHashMap<>();
+        this.profitResult = new LinkedHashMap<>();
     }
 
     public void addResult(Player player, WinState winState) {
         result.put(player, winState);
+    }
+
+    public void calculateProfit() {
+        for (Entry<Player, WinState> playerWinState : result.entrySet()) {
+            Player player = playerWinState.getKey();
+            WinState winState = playerWinState.getValue();
+            Profit profit = matchWinStateToProfit(player, winState);
+            profitResult.replace(player, profit);
+        }
+    }
+
+    private Profit matchWinStateToProfit(Player player, WinState winState) {
+        Profit battingAmount = profitResult.get(player);
+        if (winState == WinState.WIN) {
+            return battingAmount.win();
+        }
+        if (winState == WinState.LOSE) {
+            return battingAmount.lose();
+        }
+        if (winState == WinState.BLACKJACK) {
+            return battingAmount.specialWin();
+        }
+        return battingAmount.keep();
     }
 
     public int countWinState(WinState winState) {
