c8d4ae08c93ef272d4d9543a646bf1ea6c28c4c7
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-14 11:20:29 +0900

    refactor: 사용되지 않는 클래스 삭제

--- src/main/java/blackjack/model/wallet/PlayerBetInfo.java
+++ src/main/java/blackjack/model/wallet/PlayerBetInfo.java
@@ -7,42 +7,42 @@ import java.util.Map;
 
 public class PlayerBetInfo {
 
-    private final Map<Player, PlayerBetWallet> playerBetInfo = new HashMap<>();
-
-    public void registerBetAmount(Player player, int betAmount) {
-        validatePlayerAlreadySet(player);
-        PlayerBetWallet playerBetWallet = PlayerBetWallet.from(betAmount);
-        playerBetInfo.put(player, playerBetWallet);
-    }
-
-    public void registerProfitAmount(Player player, Result result) {
-        PlayerBetWallet playerBetWallet = playerBetInfo.get(player);
-        playerBetWallet.registerProfitAmount(result);
-    }
-
-    public int playerBetAmount(Player player) {
-        PlayerBetWallet playerBetWallet = playerBetInfo.get(player);
-        return playerBetWallet.getBetAmount();
-    }
-
-    public int playerProfitAmount(Player player) {
-        PlayerBetWallet playerBetWallet = playerBetInfo.get(player);
-        return playerBetWallet.getProfitAmount();
-    }
-
-    public int playerNetProfit(Player player) {
-        PlayerBetWallet playerBetWallet = playerBetInfo.get(player);
-        return playerBetWallet.calculateNetProfit();
-    }
-
-    private void validatePlayerAlreadySet(Player player) {
-        if (playerBetInfo.containsKey(player)) {
-            String playerName = player.name();
-            int betAmount = playerBetInfo.get(player).getBetAmount();
-            String errorMessage = String.format("[ERROR] 이미 배팅 금액이 등록된 플레이어 입니다. (%s 배팅 금액 : %d)",
-                    playerName,
-                    betAmount);
-            throw new IllegalArgumentException(errorMessage);
-        }
-    }
+//    private final Map<Player, PlayerBetWallet> playerBetInfo = new HashMap<>();
+//
+//    public void registerBetAmount(Player player, int betAmount) {
+//        validatePlayerAlreadySet(player);
+//        PlayerBetWallet playerBetWallet = PlayerBetWallet.from(betAmount);
+//        playerBetInfo.put(player, playerBetWallet);
+//    }
+//
+//    public void registerProfitAmount(Player player, Result result) {
+//        PlayerBetWallet playerBetWallet = playerBetInfo.get(player);
+//        playerBetWallet.registerProfitAmount(result);
+//    }
+//
+//    public int playerBetAmount(Player player) {
+//        PlayerBetWallet playerBetWallet = playerBetInfo.get(player);
+//        return playerBetWallet.getBetAmount();
+//    }
+//
+//    public int playerProfitAmount(Player player) {
+//        PlayerBetWallet playerBetWallet = playerBetInfo.get(player);
+//        return playerBetWallet.getProfitAmount();
+//    }
+//
+//    public int playerNetProfit(Player player) {
+//        PlayerBetWallet playerBetWallet = playerBetInfo.get(player);
+//        return playerBetWallet.calculateNetProfit();
+//    }
+//
+//    private void validatePlayerAlreadySet(Player player) {
+//        if (playerBetInfo.containsKey(player)) {
+//            String playerName = player.name();
+//            int betAmount = playerBetInfo.get(player).getBetAmount();
+//            String errorMessage = String.format("[ERROR] 이미 배팅 금액이 등록된 플레이어 입니다. (%s 배팅 금액 : %d)",
+//                    playerName,
+//                    betAmount);
+//            throw new IllegalArgumentException(errorMessage);
+//        }
+//    }
 }
