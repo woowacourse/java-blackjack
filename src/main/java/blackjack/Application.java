34fbec46c334c243381a1f80f5d5b41094c2282d
seunghye218 <seunghye@student.42seoul.kr>
2024-03-14 15:29:01 +0900

    refactor: main에 있는Controller 책임 이동
    - Main -> Application

---
+++ src/main/java/blackjack/Application.java
@@ -0,0 +1,11 @@
+package blackjack;
+
+import blackjack.controller.Controller;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.play();
+    }
+}
--- src/main/java/blackjack/controller/Controller.java
+++ src/main/java/blackjack/controller/Controller.java
@@ -1,66 +1,75 @@
 package blackjack.controller;
 
+import blackjack.domain.CardFactory;
 import blackjack.domain.Dealer;
+import blackjack.domain.Deck;
 import blackjack.domain.Player;
 import blackjack.domain.Players;
 import blackjack.domain.Result;
 import blackjack.domain.ResultStatus;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
+import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
-public class Game {
+public class Controller {
 
     private static final int BLACKJACK_SCORE = 21;
 
-    private final Dealer dealer;
-    private final Players players;
-
-
-    public Game(Dealer dealer, Players players) {
-        this.dealer = dealer;
-        this.players = players;
-    }
-
     public void play() {
-        initializeHands();
-        playerTurn();
-        dealerTurn();
-        printScoreAndResult();
+        Dealer dealer = createDealer();
+        Players players = createPlayers();
+
+        initializeHands(dealer, players.getPlayers());
+        playerTurn(dealer, players);
+        dealerTurn(dealer);
+        printScoreAndResult(dealer, players);
     }
 
-    private void initializeHands() {
+    private Dealer createDealer() {
+        CardFactory cardFactory = new CardFactory();
+        Deck deck = new Deck(cardFactory.createBlackJackCard(), Collections::shuffle);
+        return new Dealer(deck);
+    }
+
+    private Players createPlayers() {
+        List<String> playerNames = InputView.readName();
+        return Players.from(playerNames);
+    }
+
+    private void initializeHands(Dealer dealer, List<Player> players) {
         dealer.shuffleDeck();
         dealer.initializeHand(dealer.draw(), dealer.draw());
-        for (Player player : players.getPlayers()) {
+        for (Player player : players) {
             player.initializeHand(dealer.draw(), dealer.draw());
         }
 
-        OutputView.printInitialHands(dealer, players.getPlayers());
+        OutputView.printInitialHands(dealer, players);
     }
 
-    private void playerTurn() {
+    private void playerTurn(Dealer dealer, Players players) {
         for (Player player : players.getPlayers()) {
-            hitOrStand(player);
+            hitOrStand(dealer, player);
         }
     }
 
-    private void dealerTurn() {
+    private void dealerTurn(Dealer dealer) {
         while (dealer.canHit()) {
             dealer.putCard(dealer.draw());
             OutputView.printDealerDraw(dealer);
         }
     }
 
-    private void printScoreAndResult() {
+    private void printScoreAndResult(Dealer dealer, Players players) {
         OutputView.printHandsWithScore(dealer, players.getPlayers());
-        OutputView.printResult(calculateResult(), dealer);
+        OutputView.printResult(calculateResult(dealer, players), dealer);
     }
 
-    private Result calculateResult() {
+    private Result calculateResult(Dealer dealer, Players players) {
         int dealerScore = dealer.calculate();
         Map<Player, Integer> playersScore = players.calculate();
 
@@ -98,15 +107,12 @@ public class Game {
         return ResultStatus.WIN;
     }
 
-    private void hitOrStand(Player player) {
+    private void hitOrStand(Dealer dealer, Player player) {
         while (player.canHit() && InputView.readHitOrStand(player)) {
             player.putCard(dealer.draw());
             OutputView.printTotalHand(player);
-            ifBust(player);
         }
-    }
 
-    private void ifBust(Player player) {
         if (!player.canHit()) {
             OutputView.printBust();
         }
