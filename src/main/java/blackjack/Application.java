c24b79418f96bf1a3374e012b815340254a54bba
ashsty <qkralsdk991@naver.com>
2024-03-14 10:42:02 +0900

    test: 동기화 테스트

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -1,7 +1,7 @@
 package blackjack;
 
 public class Application {
-
+    // 동기화 테스트
     public static void main(String[] args) {
         BlackjackGamePlay blackjackGamePlay = new BlackjackGamePlay();
         blackjackGamePlay.run();
