14d16eb46fa4dee6ac02ae5cdf7631c21b708903
donghoony <aru0504@naver.com>
2024-03-14 12:21:16 +0900

    feat(PlayerBets): 플레이어의 배팅 정보를 저장하고 수익을 계산

---
+++ src/main/java/blackjack/money/PlayerBets.java
@@ -0,0 +1,30 @@
+package blackjack.money;
+
+import blackjack.resultstate.MatchResult;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerBets {
+
+    private final Map<String, Money> betMoney;
+
+    public PlayerBets() {
+        this.betMoney = new HashMap<>();
+    }
+
+    public void placeBet(String playerName, Money money) {
+        betMoney.put(playerName, money);
+    }
+
+    public Money calculateProfitByName(String name, MatchResult result) {
+        Money playerBetMoney = getBetMoneyByName(name);
+        return result.calculateProfit(playerBetMoney);
+    }
+
+    private Money getBetMoneyByName(String name) {
+        if (!betMoney.containsKey(name)) {
+            throw new IllegalArgumentException("[ERROR] 배팅 정보가 존재하지 않습니다.");
+        }
+        return betMoney.get(name);
+    }
+}
