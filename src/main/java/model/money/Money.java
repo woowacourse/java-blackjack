afc5bd77e35bae30493d9ae89cc45b9498d281bb
fram1998 <koust6u@pusan.ac.kr>
2024-03-14 11:35:33 +0900

    fix: 돈 생성기능 이중화하여 수정
    돈은 배팅 금액과 블랙잭 게임 이후의 배당금액 두가지를 표현하는데 배팅 금액은 음수가 들어올수 없는 반면 배당금액은 음수가 될수도 있기 때문에 생성 로직, 기본 생성자와 정적 메소드로 두개로 나누어둠

--- src/main/java/model/money/Money.java
+++ src/main/java/model/money/Money.java
@@ -1,17 +1,47 @@
 package model.money;
 
+import java.util.Objects;
+
 public class Money {
     private final int amount;
 
-    public Money(int money) {
-        validate(money);
+    Money(int money) {
         this.amount = money;
     }
 
-    public void validate(int amount) {
+    public static Money createBettingAmount(int money) {
+        validate(money);
+        return new Money(money);
+    }
+
+    private static void validate(int amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("베팅 금액은 0보다 커야합니다.");
         }
     }
 
+    public Money createMultiplyOf(int operand) {
+        return new Money(amount * operand);
+    }
+
+    public Money createMultiplyOf(double operand) {
+        return new Money((int) (amount * operand));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Money money = (Money) o;
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
 }
--- src/test/java/model/money/MoneyTest.java
+++ src/test/java/model/money/MoneyTest.java
@@ -1,5 +1,6 @@
 package model.money;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -8,10 +9,23 @@ import org.junit.jupiter.api.Test;
 class MoneyTest {
 
     @Test
-    @DisplayName("돈은 음수라면 예외를 발생시킨다.")
-    void money_ShouldThrowException_WhenMoneyAmountIsNotInteger() {
-        assertThatThrownBy(() -> new Money(-1))
+    @DisplayName("생성자를 통해 생성되는 돈은 일때 예외를 발생시킨다.")
+    void createBettingAmount_ShouldThrowException_WhenMoneyAmountIsNotInteger() {
+        assertThatThrownBy(() -> Money.createBettingAmount(-1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("돈은 N(실수)을 곱한 만큼의 돈을 가지는 새로운 인스턴스를 생성한다.")
+    void createMultiplyOf_ShouldGenerateInstanceOfMultiplyNOfOrigin_WhenNIsDouble() {
+        assertThat(new Money(1000).createMultiplyOf(1.5))
+                .isEqualTo(new Money(1500));
+    }
+
+    @Test
+    @DisplayName("돈은 N(정수)을 곱한 만큼의 돈을 가지는 새로운 인스턴스를 생성한다.")
+    void createMultiplyOf_ShouldGenerateInstanceOfMultiplyNOfOrigin_WhenNIsInteger() {
+        assertThat(new Money(1000).createMultiplyOf(2))
+                .isEqualTo(new Money(2000));
+    }
 }
