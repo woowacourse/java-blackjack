5ebe3798c34dcd6d393f49407c07b6b8f7e59902
이상진 <priceelees@gmail.com>
2024-03-14 17:49:42 +0900

    style(Money): 메서드 배치 순서 수정

--- src/main/java/blackjack/domain/gamer/Money.java
+++ src/main/java/blackjack/domain/gamer/Money.java
@@ -4,12 +4,12 @@ public record Money(int amount) {
 
 	private static final Money ZERO_AMOUNT = new Money(0);
 
-	public Money add(Money money) {
-		return new Money(amount + money.amount());
-	}
-
 	public static Money getZeroAmountMoney() {
 		return ZERO_AMOUNT;
+	}
+
+	public Money add(Money money) {
+		return new Money(amount + money.amount());
 	}
 
 	public Money multiplyByRatio(double ratio) {
