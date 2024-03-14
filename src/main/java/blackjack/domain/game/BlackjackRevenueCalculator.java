4e1004b942ecc5e4816240e504f0004a85f6aff7
이상진 <priceelees@gmail.com>
2024-03-14 16:40:02 +0900

    style(BlackjackRevenueCalculator): 필드명 수정 및 상수화

--- src/main/java/blackjack/domain/game/BlackjackRevenueCalculator.java
+++ src/main/java/blackjack/domain/game/BlackjackRevenueCalculator.java
@@ -5,27 +5,30 @@ import blackjack.domain.gamer.Player;
 
 public class BlackjackRevenueCalculator {
 
-	private final PlayerResultHandler handler;
+	private static final double BLACKJACK_REVENUE_RATIO = 1.5D;
+	private static final double LOSE_REVENUE_RATIO = -1.0D;
 
-	public BlackjackRevenueCalculator(PlayerResultHandler handler) {
-		this.handler = handler;
+	private final PlayerResultHandler playerResultHandler;
+
+	public BlackjackRevenueCalculator(PlayerResultHandler playerResultHandler) {
+		this.playerResultHandler = playerResultHandler;
 	}
 
 	public Money calculatePlayerRevenue(Player player, Money betAmount) {
-		GameResult gameResult = handler.notifyResultToPlayer(player);
+		GameResult gameResult = playerResultHandler.getPlayerResult(player);
 
 		return getRevenue(gameResult, betAmount);
 	}
 
 	private Money getRevenue(GameResult gameResult, Money betAmount) {
 		if (gameResult == GameResult.BLACKJACK_WIN) {
-			return betAmount.multiplyByRatio(1.5);
+			return betAmount.multiplyByRatio(BLACKJACK_REVENUE_RATIO);
 		}
 		if (gameResult == GameResult.WIN) {
 			return betAmount;
 		}
 		if (gameResult == GameResult.LOSE) {
-			return betAmount.multiplyByRatio(-1);
+			return betAmount.multiplyByRatio(LOSE_REVENUE_RATIO);
 		}
 		return new Money(0);
 	}
