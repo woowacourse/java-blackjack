cc74f61bd44eb4cf0fdba232540979a2d65a88b2
HoeSeong123 <ghltjd3@gmail.com>
2024-03-14 11:35:06 +0900

    feat(ResultStatus) : 승패에 따라 수익률 비율이 달라진다
    - 승 : 1배
    - 패 : -1배
    - 무 : 0배
    - 블랙잭 : 1.5배

--- src/main/java/blackjack/domain/result/ResultStatus.java
+++ src/main/java/blackjack/domain/result/ResultStatus.java
@@ -1,15 +1,23 @@
 package blackjack.domain.result;
 
 public enum ResultStatus {
-    WIN,
-    LOSE,
-    DRAW;
+    WIN(1),
+    LOSE(-1),
+    DRAW(0),
+    BLACKJACK(1.5);
+
+    private double rate;
+    
+    ResultStatus(double rate) {
+        this.rate = rate;
+    }
 
     public ResultStatus reverse() {
         return switch (this) {
             case LOSE -> WIN;
             case DRAW -> DRAW;
             case WIN -> LOSE;
+            case BLACKJACK -> WIN;
         };
     }
 }
