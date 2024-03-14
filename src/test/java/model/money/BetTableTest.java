db3d7873c4d0addd45aa30bbbeadd5568128de58
fram1998 <koust6u@pusan.ac.kr>
2024-03-14 19:41:24 +0900

    refactor: 가독성 개선 위한 클래스명 변경

---
+++ src/test/java/model/money/BetTableTest.java
@@ -0,0 +1,21 @@
+package model.money;
+
+import model.participant.Name;
+import model.participant.Participant;
+import model.participant.Player;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BetTableTest {
+    
+    /*@Test
+    @DisplayName("베팅 테이블에 참가자와 베팅금액을 추가할 수 있다.")
+    void add_ShouldAdditionBettingTable_WhenAdditionParticipant() {
+        BetTable betTable = new BetTable();
+        Participant participant = new Player(new Name("프람"));
+        Money money = new Money(1000);
+        betTable.add(participant, money);
+
+        //TODO
+    }*/
+}
