a4f7bc6b40a3490f9d8075c9eb98a4c26be7e810
donghoony <aru0504@naver.com>
2024-03-14 12:00:39 +0900

    feat(Money): 게임 결과의 수익을 계산

--- src/main/java/blackjack/money/BetMoney.java
+++ src/main/java/blackjack/money/BetMoney.java
@@ -1,28 +1,26 @@
 package blackjack.money;
 
-public class BetMoney {
+public class BetMoney extends Money {
 
     private static final int BET_MONEY_UNIT = 1_000;
 
-    private final int amount;
-
     private BetMoney(int amount) {
-        validatePositiveAmount(amount);
-        validateAmountDivisibleByUnit(amount);
-        this.amount = amount;
+        super(amount);
     }
 
     public static BetMoney of(int amount) {
+        validatePositiveAmount(amount);
+        validateAmountDivisibleByUnit(amount);
         return new BetMoney(amount);
     }
 
-    private void validatePositiveAmount(int amount) {
+    private static void validatePositiveAmount(int amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("[ERROR] 베팅 금액은 양의 정수여야 합니다.");
         }
     }
 
-    private void validateAmountDivisibleByUnit(int amount) {
+    private static void validateAmountDivisibleByUnit(int amount) {
         if (amount % BET_MONEY_UNIT != 0) {
             throw new IllegalArgumentException("[ERROR] 베팅은 1,000원 단위로만 가능합니다.");
         }
---
+++ src/main/java/blackjack/money/Money.java
@@ -0,0 +1,67 @@
+package blackjack.money;
+
+public class Money {
+
+    private static final double BLACKJACK_PROFIT_RATE = 1.5;
+
+    private final int amount;
+
+    protected Money(int amount) {
+        this.amount = amount;
+    }
+
+    public static Money of(int amount) {
+        return new Money(amount);
+    }
+
+    public static Money zero() {
+        return new Money(0);
+    }
+
+    public Money profitOnTie() {
+        return zero();
+    }
+
+    public Money profitOnBlackJackWin() {
+        return this.multipliedBy(BLACKJACK_PROFIT_RATE);
+    }
+
+    private Money multipliedBy(double rate) {
+        return Money.of((int) (amount * rate));
+    }
+
+    public Money profitOnWin() {
+        return Money.of(amount);
+    }
+
+    public Money profitOnLose() {
+        return this.negate();
+    }
+
+    private Money negate() {
+        return new Money(-amount);
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final Money money = (Money) o;
+
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return amount;
+    }
+}
