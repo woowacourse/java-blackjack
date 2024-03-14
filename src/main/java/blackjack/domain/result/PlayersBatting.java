83c53ff6c067bfdd1549340c5b71f641b8b9df3c
ashsty <qkralsdk991@naver.com>
2024-03-14 16:39:25 +0900

    feat: 플레이어 승패 -> 배팅 금액 계산 로직으로 변경

---
+++ src/main/java/blackjack/domain/result/PlayersBatting.java
@@ -0,0 +1,25 @@
+package blackjack.domain.result;
+
+import blackjack.domain.gamer.Player;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class PlayersBatting {
+
+    private final Map<Player, Double> playerBatting = new LinkedHashMap<>();
+
+    public PlayersBatting() {
+    }
+
+    public void registerPlayerBatting(Player player, double batting) {
+        playerBatting.put(player, batting);
+    }
+
+    public void calculateBattingReward(Player player, double winningRate) {
+        playerBatting.put(player, playerBatting.get(player) * winningRate);
+    }
+
+    public Map<Player, Double> getPlayerBatting() {
+        return playerBatting;
+    }
+}
