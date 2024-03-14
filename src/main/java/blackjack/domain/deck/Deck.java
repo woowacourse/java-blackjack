fed1e8098645a2115121822a92232733e0e217e8
ehtjsv2 <ehtjsv2@naver.com>
2024-03-14 22:25:24 +0900

    Test: HandsScoreTest에 Ace에 관한 테스트 추가 및 테스트 한개에 단언문 한개로 변경

--- src/main/java/blackjack/domain/deck/Deck.java
+++ src/main/java/blackjack/domain/deck/Deck.java
@@ -1,11 +1,14 @@
 package blackjack.domain.deck;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Deck {
 
--- src/test/java/blackjack/domain/participant/HandsScoreTest.java
+++ src/test/java/blackjack/domain/participant/HandsScoreTest.java
@@ -15,25 +15,39 @@ import org.junit.jupiter.api.Test;
 
 class HandsScoreTest {
 
+    @DisplayName("카드패의 점수가 21을 초과하지 않았는 지 판단한다")
+    @Test
+    void should_bust_when_Below_BlackJack() {
+        List<Card> overCards = List.of(new Card(SPADE, JACK), new Card(DIAMOND, ACE));
+
+        HandsScore handsScore = HandsScore.of(overCards);
+
+        assertTrue(handsScore.isBust());
+    }
 
     @DisplayName("카드패의 점수가 21을 초과했는지 판단한다")
     @Test
-    void should_judgeBurst() {
-        List<Card> notOverCards = List.of(new Card(SPADE, ACE), new Card(SPADE, JACK));
-        HandsScore handsScore = HandsScore.of(notOverCards);
-
-        assertFalse(handsScore.isBust());
-
+    void should_bust_when_Over_BlackJack() {
         List<Card> overCards = List.of(new Card(SPADE, ACE), new Card(DIAMOND, ACE), new Card(SPADE, JACK));
-        handsScore = HandsScore.of(overCards);
+
+        HandsScore handsScore = HandsScore.of(overCards);
+
         assertTrue(handsScore.isBust());
     }
 
     @DisplayName("에이스는 21이 초과되지 않으면 11로 사용할 수 있다")
     @Test
-    void should_downgradeAce() {
+    void should_getHighAce_When_Under_BlackJack() {
         HandsScore handsScore = HandsScore.of(List.of(new Card(SPADE, ACE), new Card(SPADE, JACK)));
 
         assertThat(handsScore.getScore()).isEqualTo(21);
+    }
+
+    @DisplayName("에이스는 21이 초과이 초과되면 1로 사용할 수 있다")
+    @Test
+    void should_getLowACE_When_Over_BlackJack() {
+        HandsScore handsScore = HandsScore.of(List.of(new Card(SPADE, ACE), new Card(DIAMOND, ACE)));
+
+        assertThat(handsScore.getScore()).isEqualTo(12);
     }
 }
