3fbb924fa48e573061026b28dbc3754ab99a89e7
libienz <leekh10195@gmail.com>
2024-03-14 10:34:41 +0900

    feat: 푸쉬, 블랙잭 승리 등 새로운 판정 추가

--- src/main/java/blackjack/domain/rule/Judge.java
+++ src/main/java/blackjack/domain/rule/Judge.java
@@ -1,5 +1,10 @@
 package blackjack.domain.rule;
 
+import static blackjack.domain.rule.GameResult.BLACKJACK_WIN;
+import static blackjack.domain.rule.GameResult.PLAYER_LOSE;
+import static blackjack.domain.rule.GameResult.PLAYER_WIN;
+import static blackjack.domain.rule.GameResult.PUSH;
+
 import blackjack.domain.DealerGameResult;
 import blackjack.domain.player.Dealer;
 import blackjack.domain.player.Player;
@@ -27,5 +32,30 @@ public class Judge {
             return true;
         }
         return playerScore.isAbove(dealerScore);
+    }
+
+    public PlayerResults calculatePlayerResults(Dealer dealer, Players players) {
+        return new PlayerResults(players.getPlayers().stream()
+                .map(player -> new PlayerResult(player.getName(), calculatePlayerResult(dealer, player)))
+                .toList());
+    }
+
+    public GameResult calculatePlayerResult(Dealer dealer, Player player) {
+        Score playerScore = player.calculateHandScore();
+        Score dealerScore = dealer.calculateHandScore();
+
+        if (player.hasBlackJackHand() && !dealer.hasBlackJackHand()) {
+            return BLACKJACK_WIN;
+        }
+        if (player.isBusted()) {
+            return PLAYER_LOSE;
+        }
+        if (dealer.isBusted() || playerScore.isAbove(dealerScore)) {
+            return PLAYER_WIN;
+        }
+        if (playerScore.equals(dealerScore)) {
+            return PUSH;
+        }
+        return PLAYER_LOSE;
     }
 }
--- src/test/java/blackjack/domain/rule/JudgeTest.java
+++ src/test/java/blackjack/domain/rule/JudgeTest.java
@@ -1,5 +1,9 @@
 package blackjack.domain.rule;
 
+import static blackjack.domain.rule.GameResult.BLACKJACK_WIN;
+import static blackjack.domain.rule.GameResult.PLAYER_LOSE;
+import static blackjack.domain.rule.GameResult.PLAYER_WIN;
+import static blackjack.domain.rule.GameResult.PUSH;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -31,6 +35,46 @@ class JudgeTest {
         assertThat(judge.isPlayerWin(dealer, player)).isFalse();
     }
 
+    @DisplayName("플레이어 블랙잭이고 딜러가 블랙잭이 아니면 플레이어가 블랙잭 승리를 한다.")
+    @Test
+    void testPlayerBlackJackWin() {
+        Player player = TestPlayerCreator.of("리비", 1, 10);
+        Dealer dealer = new Dealer(TestHandCreator.of(3, 4));
+        assertThat(judge.calculatePlayerResult(dealer, player)).isEqualTo(BLACKJACK_WIN);
+    }
+
+    @DisplayName("딜러와 플레이어 모두 21점이더라도 블랙잭 핸드가 있다면 블랙잭쪽이 이긴다.")
+    @Test
+    void testPlayerBlackJackWinMaxScore() {
+        Player player = TestPlayerCreator.of("리비", 1, 10);
+        Dealer dealer = new Dealer(TestHandCreator.of(9, 2, 10));
+        assertThat(judge.calculatePlayerResult(dealer, player)).isEqualTo(BLACKJACK_WIN);
+    }
+
+    @DisplayName("딜러와 플레이어 모두 블랙잭인 경우 결과는 PUSH이다.")
+    @Test
+    void testPlayerAndDealerBlackJack() {
+        Player player = TestPlayerCreator.of("리비", 1, 10);
+        Dealer dealer = new Dealer(TestHandCreator.of(1, 10));
+        assertThat(judge.calculatePlayerResult(dealer, player)).isEqualTo(PUSH);
+    }
+
+    @DisplayName("딜러가 버스트 되고 플레이어가 살아있다면 플레이어가 승리한다")
+    @Test
+    void testPlayerWin() {
+        Player player = TestPlayerCreator.of("리비", 10, 10);
+        Dealer dealer = new Dealer(TestHandCreator.of(2, 10));
+        assertThat(judge.calculatePlayerResult(dealer, player)).isEqualTo(PLAYER_WIN);
+    }
+
+    @DisplayName("딜러 플레이어 모두 버스트 되지 않은 경우 딜러의 점수보다 플레이어의 점수가 낮다면 플레이어가 패한다")
+    @Test
+    void testPlayerLose() {
+        Player player = TestPlayerCreator.of("리비", 2, 10);
+        Dealer dealer = new Dealer(TestHandCreator.of(10, 10));
+        assertThat(judge.calculatePlayerResult(dealer, player)).isEqualTo(PLAYER_LOSE);
+    }
+    
     @DisplayName("딜러의 전적을 계산할 수 있다")
     @Test
     void testCalculateDealerResult() {
