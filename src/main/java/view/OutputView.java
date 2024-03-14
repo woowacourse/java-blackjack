5d90bb734f7ca36afe4d7666cdcfdaf7e0aea684
fram1998 <koust6u@pusan.ac.kr>
2024-03-14 16:46:55 +0900

    refactor: 미사용 상수 및 의존 관계 DTO 삭제
    변경된 요구사항에 따라 필요 없어진 승무패 관련 DTO 삭제함.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,9 +1,7 @@
 package view;
 
 import java.util.List;
-import model.dto.DealerScoreResult;
 import model.dto.GameCompletionResult;
-import model.dto.PlayerScoreResult;
 
 public class OutputView {
     private static final int HOLE_CARD_INDEX = 0;
@@ -53,28 +51,6 @@ public class OutputView {
     private static void printSinglePlayerFinalFaceUp(GameCompletionResult result) {
         print(result.getPartipantNameAsString() + NAME_CARD_DELIMITER + String.join(CARD_DELIMITER,
                 result.getCardsAsStrings()) + " - 결과: " + result.hand());
-    }
-
-    public static void printScoreResults(DealerScoreResult dealerScoreResult,
-                                         List<PlayerScoreResult> playerScoreResults) {
-        StringBuilder dealerScoresText = generateDealerScoreTexts(dealerScoreResult);
-
-        System.out.print(System.lineSeparator());
-        print("## 최종 승패");
-        print(dealerScoresText.toString());
-        playerScoreResults.forEach(result -> print(playerScoreText(result)));
-    }
-
-    private static String playerScoreText(PlayerScoreResult result) {
-        return result.getNameAsString() + ": " + result.getVictoryAsString();
-    }
-
-    private static StringBuilder generateDealerScoreTexts(DealerScoreResult dealerScoreResult) {
-        StringBuilder dealerScoresText = new StringBuilder("딜러: ");
-        for (Victory victory : Victory.values()) {
-            dealerScoresText.append(dealerScoreResult.getVictoryScoreAsString(victory) + " ");
-        }
-        return dealerScoresText;
     }
 
     public static void printRetryMessage() {
