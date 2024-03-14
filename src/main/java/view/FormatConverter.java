ac03ade056fd6be5e3ddb29326bdd890cc342e41
HyunJoong Kim <hjk0761@naver.com>
2024-03-14 17:22:05 +0900

    refactor: convertInputToDecision 메서드 위치 수정

--- src/main/java/view/FormatConverter.java
+++ src/main/java/view/FormatConverter.java
@@ -10,6 +10,8 @@ import static domain.PlayingCardValue.*;
 import static java.util.Map.entry;
 
 public final class FormatConverter {
+    private static final String YES = "y";
+    private static final String NO = "n";
     private static final Map<PlayingCardShape, String> PLAYING_CARD_SHAPE_STRING = Map.of(
             SPADE, "스페이드", DIAMOND, "다이아몬드", CLOVER, "클로버", HEART, "하트"
     );
@@ -35,5 +37,17 @@ public final class FormatConverter {
 
     public static String convertPlayingCardValueToString(final PlayingCardValue playingCardValue) {
         return PLAYING_CARD_VALUE_STRING.get(playingCardValue);
+    }
+
+    public static boolean convertInputToDecision(final String input) {
+        if (input.equals(YES)) {
+            return true;
+        }
+
+        if (input.equals(NO)) {
+            return false;
+        }
+
+        throw new IllegalArgumentException(String.format("%s는 올바른 입력이 아닙니다. 카드를 받을지 여부는 y/n만 입력할 수 있습니다.", input));
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,10 +6,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class InputView {
-    private static final String YES = "y";
-    private static final String NO = "n";
+import static view.FormatConverter.convertInputToDecision;
 
+public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static List<String> inputPlayerNames() {
@@ -23,18 +22,6 @@ public class InputView {
         System.out.println(playerName.value() + "은(는) 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
         String inputString = scanner.nextLine();
         return convertInputToDecision(inputString);
-    }
-
-    private static boolean convertInputToDecision(final String input) {
-        if (input.equals(YES)) {
-            return true;
-        }
-
-        if (input.equals(NO)) {
-            return false;
-        }
-
-        throw new IllegalArgumentException(String.format("%s는 올바른 입력이 아닙니다. 카드를 받을지 여부는 y/n만 입력할 수 있습니다.", input));
     }
 
     public static String inputAmount(final String playerName) {
