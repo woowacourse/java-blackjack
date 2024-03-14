596e7fac3a68c63370968ee5d67b17a37ea86678
gyungchan Jo <namejgc@naver.com>
2024-03-15 00:41:22 +0900

    feat(BattingAmount): 배팅 금액을 검증하는 기능 구현

--- src/main/java/blackjack/domain/batting/BattingAmount.java
+++ src/main/java/blackjack/domain/batting/BattingAmount.java
@@ -3,20 +3,33 @@ package blackjack.domain.batting;
 import java.util.Objects;
 
 public class BattingAmount {
-    private static final int MINIMUM_BATTING_AMOUNT = 1_000;
+    private static final int MINIMUM_BATTING_AMOUNT = 10_000;
     private static final int MAXIMUM_BATTING_AMOUNT = 1_000_000;
+    private static final int VALID_BATTING_AMOUNT_UNIT = 1_000;
 
     private final int battingAmount;
 
     public BattingAmount(final int battingAmount) {
-        validateRange(battingAmount);
+        validateBattingAmount(battingAmount);
         this.battingAmount = battingAmount;
+    }
+
+    private void validateBattingAmount(final int battingAmount) {
+        validateRange(battingAmount);
+        validateUnit(battingAmount);
     }
 
     private void validateRange(final int battingAmount) {
         if (battingAmount < MINIMUM_BATTING_AMOUNT || battingAmount > MAXIMUM_BATTING_AMOUNT) {
             throw new IllegalArgumentException(
                     String.format("배팅 금액은 %,d ~ %,d원 사이어야 합니다.", MINIMUM_BATTING_AMOUNT, MAXIMUM_BATTING_AMOUNT));
+        }
+    }
+
+    private void validateUnit(final int battingAmount) {
+        if (battingAmount % VALID_BATTING_AMOUNT_UNIT != 0) {
+            throw new IllegalArgumentException(
+                    String.format("배팅 금액은 %,d원 단위어야 합니다.", VALID_BATTING_AMOUNT_UNIT));
         }
     }
 
--- src/test/java/blackjack/domain/batting/BattingAmountTest.java
+++ src/test/java/blackjack/domain/batting/BattingAmountTest.java
@@ -9,17 +9,25 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class BattingAmountTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {1_000, 1_000_000})
+    @ValueSource(ints = {10_000, 1_000_000})
     void 플레이어의_배팅_금액을_알_수_있다(int money) {
         final BattingAmount battingAmount = new BattingAmount(money);
         assertThat(battingAmount).isEqualTo(new BattingAmount(money));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {999, 1_000_001})
-    void 배팅_금액이_1000원보다_작거나_1000000원보다_크다면_예외가_발생한다(int money) {
+    @ValueSource(ints = {9_999, 1_000_001})
+    void 배팅_금액이_10000원보다_작거나_1000000원보다_크다면_예외가_발생한다(int money) {
         assertThatThrownBy(() -> new BattingAmount(money))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("배팅 금액은 1,000 ~ 1,000,000원 사이어야 합니다.");
+                .hasMessageContaining("배팅 금액은 10,000 ~ 1,000,000원 사이어야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {11_001, 11_010, 11_100})
+    void 배팅_금액이_1_000원_단위가_아니라면_예외가_발생한다(int money) {
+        assertThatThrownBy(() -> new BattingAmount(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("배팅 금액은 1,000원 단위어야 합니다.");
     }
 }
