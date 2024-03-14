573b2cc107efa8e489e811ba0c90803dcbc185ed
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-03-14 18:50:39 +0900

    feat(GameMoney) : 카드 상태와 승패에 따라 최종 수익을 알 수 있다.

--- src/main/java/model/GameMoney.java
+++ src/main/java/model/GameMoney.java
@@ -1,6 +1,12 @@
 package model;
 
 public class GameMoney {
+
+    private static final double BLACKJACK_RATE = 1.5;
+    private static final double WIN_RATE = 1;
+    private static final double LOSE_RATE = -1;
+    private static final double DRAW_RATE = 0;
+
     private final int value;
 
     public GameMoney(int value) {
@@ -12,5 +18,18 @@ public class GameMoney {
         if(value < 1) {
             throw new IllegalArgumentException("게임 배팅 금액은 0보다 커야 합니다.");
         }
+    }
+
+    public int calculateRevenue(Outcome outcome, BlackJackState blackJackState) {
+        if(blackJackState == BlackJackState.BLACKJACK && outcome == Outcome.WIN) {
+            return (int) (value * BLACKJACK_RATE);
+        }
+        if(outcome == Outcome.WIN) {
+            return (int) (value * WIN_RATE);
+        }
+        if(outcome == Outcome.LOSE) {
+            return (int) (value * LOSE_RATE);
+        }
+        return (int) (value * DRAW_RATE);
     }
 }
--- src/test/java/model/GameMoneyTest.java
+++ src/test/java/model/GameMoneyTest.java
@@ -1,9 +1,11 @@
 package model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GameMoneyTest {
@@ -14,5 +16,33 @@ class GameMoneyTest {
     void validateGameMoneyOverZero(int gameMoney) {
         assertThatThrownBy(() -> new GameMoney(gameMoney))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("카드 상태가 블랙잭이고 승리했을 경우, 1.5배의 수익을 얻는다.")
+    @Test
+    void calculateRevenueBlackJackWin() {
+        int revenue = new GameMoney(1000).calculateRevenue(Outcome.WIN, BlackJackState.BLACKJACK);
+        assertThat(revenue).isEqualTo(1500);
+    }
+
+    @DisplayName("승리했을 경우, 1배의 수익을 얻는다.")
+    @Test
+    void calculateRevenueWin() {
+        int revenue = new GameMoney(1000).calculateRevenue(Outcome.WIN, BlackJackState.STAND);
+        assertThat(revenue).isEqualTo(1000);
+    }
+
+    @DisplayName("패했을 경우, -1배의 수익을 얻는다.")
+    @Test
+    void calculateRevenueLose() {
+        int revenue = new GameMoney(1000).calculateRevenue(Outcome.LOSE, BlackJackState.STAND);
+        assertThat(revenue).isEqualTo(-1000);
+    }
+
+    @DisplayName("무했을 경우, 0배의 수익을 얻는다.")
+    @Test
+    void calculateRevenueBlackJack() {
+        int revenue = new GameMoney(1000).calculateRevenue(Outcome.DRAW, BlackJackState.STAND);
+        assertThat(revenue).isEqualTo(0);
     }
 }
