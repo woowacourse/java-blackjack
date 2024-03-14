417e078f161ebed68f6a3600a5eaab4d8594f91f
hoyeonyy <highoption@naver.com>
2024-03-14 13:40:14 +0900

    feat(Money) : validate 생성

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -4,6 +4,14 @@ public class Money {
     private final int money;
 
     public Money(int money) {
+        validateMoneyRange(money);
         this.money = money;
     }
+
+    private void validateMoneyRange(int money) {
+        if (money <= 0) {
+            throw new IllegalArgumentException("돈은 0보다 큰 자연수여야 합니다.");
+        }
+    }
+
 }
--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -3,6 +3,8 @@ package domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MoneyTest {
 
@@ -10,5 +12,14 @@ class MoneyTest {
     @Test
     void createMoneyTest() {
         Assertions.assertThatCode(() -> new Money(10000)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("돈은 0보다 큰 자연수 여야 한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -9999})
+    void validateMoneyRange(int money) {
+        Assertions.assertThatThrownBy(() -> new Money(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("돈은 0보다 큰 자연수여야 합니다.");
     }
 }
