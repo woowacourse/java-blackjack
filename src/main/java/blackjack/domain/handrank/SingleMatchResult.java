51e5232d63b884350dac4d088f92d47f25ab8926
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-03-14 11:45:29 +0900

    feat (SingleMatchResult) : 매치 결과에 따라 플레이어, 딜러의 이익을 계산하는 기능 구현

--- src/main/java/blackjack/domain/handrank/SingleMatchResult.java
+++ src/main/java/blackjack/domain/handrank/SingleMatchResult.java
@@ -1,8 +1,27 @@
 package blackjack.domain.handrank;
 
+import blackjack.domain.money.BetAmount;
+import blackjack.domain.money.Profit;
+import java.util.function.Function;
+
 public enum SingleMatchResult {
-    PLAYER_BLACKJACK,
-    PLAYER_WIN,
-    DRAW,
-    DEALER_WIN
+
+    PLAYER_BLACKJACK(amount -> Profit.win(amount, 1.5)),
+    PLAYER_WIN(Profit::win),
+    DRAW(amount -> Profit.ZERO),
+    DEALER_WIN(Profit::lose);
+
+    private final Function<BetAmount, Profit> playerProfitFunction;
+
+    SingleMatchResult(Function<BetAmount, Profit> playerProfitFunction) {
+        this.playerProfitFunction = playerProfitFunction;
+    }
+
+    public Profit calculatePlayerProfit(BetAmount betAmount) {
+        return playerProfitFunction.apply(betAmount);
+    }
+
+    public Profit calculateDealerProfit(BetAmount betAmount) {
+        return calculatePlayerProfit(betAmount).reverse();
+    }
 }
---
+++ src/test/java/blackjack/domain/handrank/SingleMatchResultTest.java
@@ -0,0 +1,45 @@
+package blackjack.domain.handrank;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.domain.money.BetAmount;
+import blackjack.domain.money.Profit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class SingleMatchResultTest {
+
+
+    @DisplayName("승패 결과에 따라, 플레이어의 이익을 반환할 수 있다.")
+    @ParameterizedTest
+    @CsvSource({
+            "1000, PLAYER_BLACKJACK, 1500",
+            "1000, PLAYER_WIN, 1000",
+            "1000, DRAW, 0",
+            "1000, DEALER_WIN, -1000"
+    })
+    void calculatePlayerProfitTest(int amount, SingleMatchResult result, int expected) {
+        BetAmount betAmount = new BetAmount(amount);
+
+        Profit actual = result.calculatePlayerProfit(betAmount);
+
+        assertThat(actual.toInt()).isEqualTo(expected);
+    }
+
+    @DisplayName("승패 결과에 따라, 딜러의 이익을 반환할 수 있다.")
+    @ParameterizedTest
+    @CsvSource({
+            "1000, PLAYER_BLACKJACK, -1500",
+            "1000, PLAYER_WIN, -1000",
+            "1000, DRAW, 0",
+            "1000, DEALER_WIN, 1000"
+    })
+    void calculateDealerProfitTest(int amount, SingleMatchResult result, int expected) {
+        BetAmount betAmount = new BetAmount(amount);
+
+        Profit actual = result.calculateDealerProfit(betAmount);
+
+        assertThat(actual.toInt()).isEqualTo(expected);
+    }
+}
