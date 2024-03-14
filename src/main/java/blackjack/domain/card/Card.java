4225436807031e8e9bfa5064eb37d003bce46dc9
nhlee98 <e5slnh9876@gmail.com>
2024-03-14 16:23:42 +0900

    refactor: 안쓰는 메소드 제거

--- src/main/java/blackjack/domain/card/Card.java
+++ src/main/java/blackjack/domain/card/Card.java
@@ -3,54 +3,50 @@ package blackjack.domain.card;
 import java.util.Objects;
 
 public class Card {
-	private final Suit suit;
-	private final Rank rank;
+    private final Suit suit;
+    private final Rank rank;
 
-	public Card(Suit suit, Rank rank) {
-		this.suit = suit;
-		this.rank = rank;
-	}
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
 
-	public boolean isAce() {
-		return rank == Rank.ACE;
-	}
+    public boolean isAce() {
+        return rank == Rank.ACE;
+    }
 
-	public boolean isRank(Rank rank) {
-		return this.rank == rank;
-	}
+    public int getScore() {
+        return rank.getScore();
+    }
 
-	public int getScore() {
-		return rank.getScore();
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Card card = (Card) o;
+        return suit == card.suit && rank == card.rank;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Card card = (Card)o;
-		return suit == card.suit && rank == card.rank;
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(suit, rank);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(suit, rank);
-	}
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit=" + suit +
+                ", rank=" + rank +
+                '}';
+    }
 
-	@Override
-	public String toString() {
-		return "Card{" +
-				"suit=" + suit +
-				", rank=" + rank +
-				'}';
-	}
+    public String getSuitName() {
+        return suit.getName();
+    }
 
-	public String getSuitName() {
-		return suit.getName();
-	}
-
-	public String getRankName() {
-		return rank.getName();
-	}
+    public String getRankName() {
+        return rank.getName();
+    }
 }
--- src/main/java/blackjack/domain/card/Deck.java
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -5,88 +5,84 @@ import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-	static final int INIT_SIZE = 52;
-	private static final String SIZE_ERROR_MESSAGE = String.format("덱의 사이즈가 %d장이 아닙니다.", INIT_SIZE);
-	private static final String DUPLICATION_ERROR_MESSAGE = "카드는 중복될 수 없습니다.";
+    static final int INIT_SIZE = 52;
+    private static final String SIZE_ERROR_MESSAGE = String.format("덱의 사이즈가 %d장이 아닙니다.", INIT_SIZE);
+    private static final String DUPLICATION_ERROR_MESSAGE = "카드는 중복될 수 없습니다.";
 
-	private final List<Card> cards;
+    private final List<Card> cards;
 
-	public Deck(List<Card> cards) {
-		List<Card> copyDeck = new ArrayList<>(cards);
-		validate(copyDeck);
-		this.cards = copyDeck;
-	}
+    public Deck(List<Card> cards) {
+        List<Card> copyDeck = new ArrayList<>(cards);
+        validate(copyDeck);
+        this.cards = copyDeck;
+    }
 
-	public static Deck createShuffled() {
-		Deck deck = new Deck(createCards());
-		deck.shuffle();
-		return deck;
-	}
+    public static Deck createShuffled() {
+        Deck deck = new Deck(createCards());
+        deck.shuffle();
+        return deck;
+    }
 
-	private static List<Card> createCards() {
-		List<Card> cards = new ArrayList<>();
-		for (Suit suit : Suit.values()) {
-			cards.addAll(createCardsOfSuit(suit));
-		}
+    private static List<Card> createCards() {
+        List<Card> cards = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            cards.addAll(createCardsOfSuit(suit));
+        }
 
-		return cards;
-	}
+        return cards;
+    }
 
-	private static List<Card> createCardsOfSuit(Suit suit) {
-		List<Card> cards = new ArrayList<>();
-		for (Rank rank : Rank.values()) {
-			cards.add(new Card(suit, rank));
-		}
+    private static List<Card> createCardsOfSuit(Suit suit) {
+        List<Card> cards = new ArrayList<>();
+        for (Rank rank : Rank.values()) {
+            cards.add(new Card(suit, rank));
+        }
 
-		return cards;
-	}
+        return cards;
+    }
 
-	private void validate(List<Card> cards) {
-		validateDuplicate(cards);
-		validateSize(cards);
-	}
+    private void validate(List<Card> cards) {
+        validateDuplicate(cards);
+        validateSize(cards);
+    }
 
-	private void validateDuplicate(List<Card> cards) {
-		long distinctCount = cards.stream()
-			.distinct()
-			.count();
+    private void validateDuplicate(List<Card> cards) {
+        long distinctCount = cards.stream()
+                .distinct()
+                .count();
 
-		if (cards.size() != distinctCount) {
-			throw new IllegalArgumentException(DUPLICATION_ERROR_MESSAGE);
-		}
-	}
+        if (cards.size() != distinctCount) {
+            throw new IllegalArgumentException(DUPLICATION_ERROR_MESSAGE);
+        }
+    }
 
-	private void validateSize(List<Card> deck) {
-		if (deck.size() != INIT_SIZE) {
-			throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
-		}
-	}
+    private void validateSize(List<Card> deck) {
+        if (deck.size() != INIT_SIZE) {
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
+        }
+    }
 
-	public List<Card> drawCards(int count) {
-		List<Card> cards = new ArrayList<>();
-		for (int i = 0; i < count; i++) {
-			cards.add(drawCard());
-		}
+    public List<Card> drawCards(int count) {
+        List<Card> cards = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            cards.add(drawCard());
+        }
 
-		return cards;
-	}
+        return cards;
+    }
 
-	public Card drawCard() {
-		return cards.remove(0);
-	}
+    public Card drawCard() {
+        return cards.remove(0);
+    }
 
-	public void shuffle() {
-		Collections.shuffle(cards);
-	}
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
 
-	public int size() {
-		return cards.size();
-	}
-
-	@Override
-	public String toString() {
-		return "Deck{" +
-				"cards=" + cards +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "Deck{" +
+                "cards=" + cards +
+                '}';
+    }
 }
