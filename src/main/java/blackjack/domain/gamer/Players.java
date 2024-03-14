3f8c6d3132ada1c76efdd8f4f00b1992a3fe2cad
이상진 <priceelees@gmail.com>
2024-03-14 16:26:44 +0900

    refactor(Players): List<Player> 필드를 베팅 금액을 추가한 Map<Player, Money>로 수정

--- src/main/java/blackjack/domain/gamer/Players.java
+++ src/main/java/blackjack/domain/gamer/Players.java
@@ -1,7 +1,9 @@
 package blackjack.domain.gamer;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import blackjack.domain.card.Deck;
@@ -11,22 +13,14 @@ public class Players {
 	private static final int MIN_PLAYER_COUNT = 2;
 	private static final int MAX_PLAYER_COUNT = 8;
 
-	private final List<Player> players;
+	private final Map<Player, Money> playerBetAmountMap;
 
-	public Players(List<String> names) {
-		validateNames(names);
-
-		this.players = names.stream()
-			.map(name -> new Player(new Name(name)))
-			.toList();
+	public Players(Map<Player, Money> playerBetAmountMap) {
+		validatePlayerCount(playerBetAmountMap.size());
+		this.playerBetAmountMap = playerBetAmountMap;
 	}
 
-	private void validateNames(List<String> names) {
-		validateDuplicate(names);
-		validateCount(names.size());
-	}
-
-	private void validateCount(int count) {
+	private void validatePlayerCount(int count) {
 		if (count < MIN_PLAYER_COUNT || count > MAX_PLAYER_COUNT) {
 			throw new IllegalArgumentException(
 				"플레이어는 최소 " + MIN_PLAYER_COUNT + "명에서 최대 " + MAX_PLAYER_COUNT + "명까지 가능합니다"
@@ -43,10 +37,14 @@ public class Players {
 	}
 
 	public void initAllPlayersCard(Deck deck, int initialCardCount) {
-		players.forEach(player -> player.initCard(deck, initialCardCount));
+		playerBetAmountMap.keySet().forEach(player -> player.initCard(deck, initialCardCount));
 	}
 
-	public List<Player> getPlayers() {
-		return List.copyOf(players);
+	public Money getBetAmount(Player player) {
+		return playerBetAmountMap.get(player);
+	}
+
+	public Collection<Player> getPlayers() {
+		return List.copyOf(playerBetAmountMap.keySet());
 	}
 }
--- src/test/java/blackjack/domain/gamer/PlayersTest.java
+++ src/test/java/blackjack/domain/gamer/PlayersTest.java
@@ -1,9 +1,10 @@
 package blackjack.domain.gamer;
 
-import static java.util.List.*;
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,35 +12,38 @@ import org.junit.jupiter.api.Test;
 class PlayersTest {
 
 	@Test
-	@DisplayName("유효한 입력에 대해서는 예외가 발생되지 않는다.")
+	@DisplayName("플레이어는 2명부터 8명까지 허용된다.")
 	void validPlayerNamesTest() {
-		assertThatCode(() -> new Players(List.of("a", "b")))
+		assertThatCode(() -> createTestPlayersFromNames(List.of("a", "b")))
 			.doesNotThrowAnyException();
-		assertThatCode(() -> new Players(List.of("a", "b", "c", "d", "e", "f", "g", "h")))
+		assertThatCode(() -> createTestPlayersFromNames(List.of("a", "b", "c", "d", "e", "f", "g", "h")))
 			.doesNotThrowAnyException();
-	}
-
-	@Test
-	@DisplayName("플레이어의 이름은 중복을 허용하지 않는다.")
-	void duplicatePlayerNamesTest() {
-		assertThatThrownBy(() -> new Players(of("hogi", "sang", "hogi")))
-			.isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@Test
 	@DisplayName("플레이어는 적어도 2명 이상이어야 한다.")
 	void minimumPlayerCountTest() {
-		assertThatThrownBy(() -> new Players(of("a")))
+		assertThatThrownBy(() -> createTestPlayersFromNames(List.of("a")))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("플레이어는 최소 2명에서 최대 8명까지 가능합니다");
-
 	}
 
 	@Test
 	@DisplayName("플레이어는 최대 8명까지만 가능하다.")
 	void maximumPlayerCountTest() {
-		assertThatThrownBy(() -> new Players(of("a", "b", "c", "d", "e", "f", "g", "h", "g")))
+		assertThatThrownBy(() -> createTestPlayersFromNames(List.of("a", "b", "c", "d", "e", "f", "g", "h", "g")))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("이름은 중복될 수 없습니다.");
+	}
+
+	private Players createTestPlayersFromNames(List<String> names) {
+		Map<Player, Money> playerBetAmountMap = new LinkedHashMap<>();
+
+		for (String name : names) {
+			Player player = new Player(name);
+			playerBetAmountMap.put(player, new Money(0));
+		}
+
+		return new Players(playerBetAmountMap);
 	}
 }
