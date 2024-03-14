108d83a3836ed29509cbc7745c28c761751c880b
SCY <helenason@naver.com>
2024-03-14 21:39:08 +0900

    test: PlayersResult 도메인 테스트 코드 작성

---
+++ src/test/java/domain/result/PlayersResultTest.java
@@ -0,0 +1,40 @@
+package domain.result;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.Profit;
+import domain.WinState;
+import domain.cards.Hand;
+import domain.gamer.Player;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayersResultTest {
+
+    @DisplayName("플레이어들의 승패 상태를 바탕으로 수익을 계산한다.")
+    @Test
+    void calculateProfitOfPlayers() {
+        Player player1 = new Player("p1", new Hand());
+        Player player2 = new Player("p2", new Hand());
+        Player player3 = new Player("p3", new Hand());
+        Player player4 = new Player("p4", new Hand());
+        PlayersResult playersResult = new PlayersResult();
+        playersResult.addResult(player1, WinState.WIN);
+        playersResult.addResult(player2, WinState.LOSE);
+        playersResult.addResult(player3, WinState.BLACKJACK);
+        playersResult.addResult(player4, WinState.DRAW);
+        playersResult.addProfit(player1, new Profit(10));
+        playersResult.addProfit(player2, new Profit(10));
+        playersResult.addProfit(player3, new Profit(10));
+        playersResult.addProfit(player4, new Profit(10));
+
+        playersResult.calculateProfit();
+
+        Map<Player, Profit> profitResult = playersResult.getProfitResult();
+        assertThat(profitResult.get(player1).getValue()).isEqualTo(10);
+        assertThat(profitResult.get(player2).getValue()).isEqualTo(-10);
+        assertThat(profitResult.get(player3).getValue()).isEqualTo(15);
+        assertThat(profitResult.get(player4).getValue()).isEqualTo(0);
+    }
+}
