0b73b893654df5d5bde22e9583ee3c684e2700a9
nayonsoso <panda0329@naver.com>
2024-03-14 19:43:31 +0900

    refactor: 문자가 풀네임을 사용하도록 변경
    - J를 JACK 으로

--- src/test/java/domain/FixtureCard.java
+++ src/test/java/domain/FixtureCard.java
@@ -5,5 +5,5 @@ public class FixtureCard {
     static final Card SIX_HEART = Card.of(Rank.SIX, Suit.HEARTS);
     static final Card SEVEN_HEART = Card.of(Rank.SEVEN, Suit.HEARTS);
     static final Card TEN_HEART = Card.of(Rank.TEN, Suit.HEARTS);
-    static final Card ACE_HEART = Card.of(Rank.A, Suit.HEARTS);
+    static final Card ACE_HEART = Card.of(Rank.ACE, Suit.HEARTS);
 }
