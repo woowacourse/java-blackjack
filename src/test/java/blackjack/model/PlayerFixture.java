5e593f0f611b90a2320efdd0ab3a3bb4b04bb403
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-14 13:35:52 +0900

    test(PlayerFixture): player에 대한 테스트용 픽스처 이넘 생성

--- src/test/java/blackjack/model/PlayerFixture.java
+++ src/test/java/blackjack/model/PlayerFixture.java
@@ -1,2 +1,31 @@
-package blackjack.model;public enum PlayerFixture {
+package blackjack.model;
+
+import blackjack.model.deck.Card;
+import blackjack.model.deck.Score;
+import blackjack.model.deck.Shape;
+import blackjack.model.participant.Hand;
+import blackjack.model.participant.Player;
+import java.util.List;
+
+public enum PlayerFixture {
+
+    BLACKJACK_PLAYER(Player.of("몰리",
+            new Hand(List.of(new Card(Shape.DIA, Score.TEN), new Card(Shape.DIA, Score.ACE))))),
+    BUST_PLAYER(Player.of("몰리",
+            new Hand(List.of(new Card(Shape.DIA, Score.TEN), new Card(Shape.DIA, Score.ACE), new Card(Shape.DIA, Score.JACK))))),
+    NOT_BLACKJACK_21_PLAYER(Player.of("몰리",
+            new Hand(List.of(new Card(Shape.DIA, Score.TEN), new Card(Shape.DIA, Score.SIX), new Card(Shape.DIA, Score.FIVE))))),
+    UNDER_21_PLAYER(Player.of("몰리",
+            new Hand(List.of(new Card(Shape.DIA, Score.TEN), new Card(Shape.DIA, Score.JACK))))),
+    ;
+
+    private final Player player;
+
+    PlayerFixture(final Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
 }
