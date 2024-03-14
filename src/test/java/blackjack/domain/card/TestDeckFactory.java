9955a31db44856b94c40c892aa500cfe8c7853c9
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-03-14 15:49:09 +0900

    refactor: 테스트용 덱 팩토리 변경

--- src/test/java/blackjack/domain/card/TestDeckFactory.java
+++ src/test/java/blackjack/domain/card/TestDeckFactory.java
@@ -1,26 +1,17 @@
 package blackjack.domain.card;
 
-import java.util.List;
 import java.util.Stack;
-import java.util.stream.Stream;
 
 public class TestDeckFactory implements DeckFactory {
 
-    /**
-     * 카드 순서 : Ace - HEART, SPADE, CLUB, DIAMOND
-     * 2 - HEART, SPADE, CLUB, DIAMOND
-     * ...
-     * KING - HEART, SPADE, CLUB, DIAMOND
-     */
+    private final Stack<Card> deck;
+
+    public TestDeckFactory(final Stack<Card> cards) {
+        this.deck = cards;
+    }
+
     @Override
     public Stack<Card> generate() {
-        final List<Card> cards = Stream.of(Number.values())
-                .flatMap(number -> Stream.of(Suit.values())
-                        .map(suit -> new Card(number, suit)))
-                .toList();
-
-        final Stack<Card> deck = new Stack<>();
-        deck.addAll(cards);
         return deck;
     }
 }
