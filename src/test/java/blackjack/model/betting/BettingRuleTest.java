71f4b2e8d1c9030caa868554fba8a047919cc780
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-15 03:56:59 +0900

    refactor(BettingRuleTest): @Nest 사용하여 이긴 경우 그룹화

--- src/test/java/blackjack/model/betting/BettingRuleTest.java
+++ src/test/java/blackjack/model/betting/BettingRuleTest.java
@@ -13,28 +13,37 @@ import blackjack.model.participant.Dealer;
 import blackjack.model.participant.Hand;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class BettingRuleTest {
 
-    @Test
-    @DisplayName("플레이어가 BlackJack으로 승리한 경우, 배팅 금액의 1.5배를 준다.")
-    void findProfitAmountWhenPlayerWinByBlackJack() {
-        BettingRule bettingRule = new BettingRule(new Dealer(new Hand(List.of(new Card(Shape.SPADE, Score.ACE), new Card(Shape.HEART, Score.TWO)))));
-        assertThat(bettingRule.calculateProfitRate(BLACKJACK_PLAYER.getPlayer(), WIN)).isEqualTo(1.5);
-    }
+    @Nested
+    class whenPlayerWin {
 
-    @Test
-    @DisplayName("딜러가 버스트여서 플레이어가 승리한 경우, 배팅 금액의 0배를 준다. (= 추가 수익이 없다.)")
-    void findProfitAmountWhenPlayerWinByDealerBust() {
-        BettingRule bettingRule = new BettingRule(new Dealer(new Hand(List.of(new Card(Shape.SPADE, Score.TEN), new Card(Shape.HEART, Score.TEN), new Card(Shape.DIA, Score.TEN)))));
-        assertThat(bettingRule.calculateProfitRate(NOT_BLACKJACK_21_PLAYER.getPlayer(), WIN)).isEqualTo(0);
-    }
+        @Test
+        @DisplayName("플레이어가 BlackJack으로 승리한 경우, 배팅 금액의 1.5배를 준다.")
+        void calculateProfitRateByBlackJack() {
+            BettingRule bettingRule = new BettingRule(
+                    new Dealer(new Hand(List.of(new Card(Shape.SPADE, Score.ACE), new Card(Shape.HEART, Score.TWO)))));
+            assertThat(bettingRule.calculateProfitRate(BLACKJACK_PLAYER.getPlayer(), WIN)).isEqualTo(1.5);
+        }
 
-    @Test
-    @DisplayName("딜러가 버스트가 아닌데도 플레이어가 승리한 경우, 배팅 금액의 1배를 준다.")
-    void findProfitAmountWhenPlayerWinByDealerNotBust() {
-        BettingRule bettingRule = new BettingRule(new Dealer(new Hand(List.of(new Card(Shape.SPADE, Score.TWO), new Card(Shape.HEART, Score.TWO)))));
-        assertThat(bettingRule.calculateProfitRate(NOT_BLACKJACK_21_PLAYER.getPlayer(), WIN)).isEqualTo(1);
+        @Test
+        @DisplayName("딜러가 버스트여서 플레이어가 승리한 경우, 배팅 금액의 0배를 준다. (= 추가 수익이 없다.)")
+        void calculateProfitRateByDealerBust() {
+            BettingRule bettingRule = new BettingRule(new Dealer(new Hand(
+                    List.of(new Card(Shape.SPADE, Score.TEN), new Card(Shape.HEART, Score.TEN),
+                            new Card(Shape.DIA, Score.TEN)))));
+            assertThat(bettingRule.calculateProfitRate(NOT_BLACKJACK_21_PLAYER.getPlayer(), WIN)).isEqualTo(0);
+        }
+
+        @Test
+        @DisplayName("딜러가 버스트가 아닌데도 플레이어가 승리한 경우, 배팅 금액의 1배를 준다.")
+        void calculateProfitRateByDealerNotBust() {
+            BettingRule bettingRule = new BettingRule(
+                    new Dealer(new Hand(List.of(new Card(Shape.SPADE, Score.TWO), new Card(Shape.HEART, Score.TWO)))));
+            assertThat(bettingRule.calculateProfitRate(NOT_BLACKJACK_21_PLAYER.getPlayer(), WIN)).isEqualTo(1);
+        }
     }
 }
