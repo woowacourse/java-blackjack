fc24c1170d3575656d9d9a79c7cfff4ddbb39e10
yoonjuho <dbswn990@naver.com>
2024-03-14 15:52:51 +0900

    feat(BettingMoney): 배팅 금액이 양수가 아닌 경우 예외가 발생

---
+++ src/main/java/vo/BettingMoney.java
@@ -0,0 +1,13 @@
+package vo;
+
+public record BettingMoney(int value) {
+    public BettingMoney {
+        validateNotPositive(value);
+    }
+
+    private void validateNotPositive(int value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("배팅 금액은 양수로 구성되어야 합니다.");
+        }
+    }
+}
