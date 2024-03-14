f88e4d34f6e2626d6b0d1ce449c93328e29acda9
fram1998 <koust6u@pusan.ac.kr>
2024-03-14 11:34:14 +0900

    feat: 베팅금액에 따른 배당금액 정책 기능 구현

---
+++ src/main/java/model/money/DividendPolicy.java
@@ -0,0 +1,20 @@
+package model.money;
+
+import java.util.function.Function;
+
+public enum DividendPolicy {
+    INIT_BLACKJACK((money) -> money.createMultiplyOf(1.5)),
+    NORMAL_WIN((money) -> money.createMultiplyOf(1)),
+    NORMAL_DRAW((money) -> money.createMultiplyOf(0)),
+    NORMAL_LOSE((money) -> money.createMultiplyOf(-1));
+
+    private final Function<Money, Money> policy;
+
+    DividendPolicy(Function<Money, Money> policy) {
+        this.policy = policy;
+    }
+
+    public Money apply(Money money) {
+        return policy.apply(money);
+    }
+}
