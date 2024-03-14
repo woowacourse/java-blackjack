6d049543707da317414b6b61cebd708f14dca4d4
seunghye218 <seunghye@student.42seoul.kr>
2024-03-14 12:08:51 +0900

    test: 테스트 추가
    - 테스트 의미에 맞는 assertJ api 활용 isEqualTo -> hasSize

--- src/test/java/blackjack/domain/HandTest.java
+++ src/test/java/blackjack/domain/HandTest.java
@@ -22,17 +22,30 @@ public class HandTest {
 
         hand.put(CardFixture.heartJack());
 
-        assertThat(hand.getCards().size()).isEqualTo(1);
+        assertThat(hand.getCards()).hasSize(1);
     }
 
-    @DisplayName("패의 모든 수를 계산한다")
+
+    @DisplayName("Jack 과 Ace 를 더한 값은 21이다")
     @Test
-    public void calculate() {
+    public void calculate1() {
+        Hand hand = new Hand();
+
+        hand.put(CardFixture.heartJack());
+        hand.put(CardFixture.cloverAce());
+
+        assertThat(hand.calculate()).isEqualTo(21);
+    }
+
+    @DisplayName("Jack, 3, ACE 를 다 더하면 14이다")
+    @Test
+    public void calculate2() {
         Hand hand = new Hand();
 
         hand.put(CardFixture.heartJack());
         hand.put(CardFixture.diamond3());
+        hand.put(CardFixture.cloverAce());
 
-        assertThat(hand.calculate()).isEqualTo(13);
+        assertThat(hand.calculate()).isEqualTo(14);
     }
 }
