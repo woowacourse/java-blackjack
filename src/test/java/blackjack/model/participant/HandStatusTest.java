3a8e53a4ea329441e677223c64b861bdb51dbf8c
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-15 01:50:50 +0900

    test(HandStatus): 카드 목록이 Bust인 경우는 총 스코어가 21을 초과한 경우임에 대한 테스트 작성

--- src/test/java/blackjack/model/participant/HandStatusTest.java
+++ src/test/java/blackjack/model/participant/HandStatusTest.java
@@ -8,10 +8,19 @@ import org.junit.jupiter.api.Test;
 class HandStatusTest {
 
     @Test
+    @DisplayName("카드 목록이 Bust인 경우는 총 스코어가 21을 초과한 경우다.")
+    void getHandStatusWhenBust() {
+        int score = 22;
+        int cardSize = 3;
+        assertThat(HandStatus.of(score, cardSize)).isEqualTo(HandStatus.BUST);
+    }
+
+    @Test
     @DisplayName("카드 목록이 BlackJack인 경우는 총 스코어가 21이면서 카드의 수가 2장인 경우이다.")
     void getHandStatusWhenBlackJack() {
         int score = 21;
         int cardSize = 2;
         assertThat(HandStatus.of(score, cardSize)).isEqualTo(HandStatus.BLACKJACK);
     }
+
 }
