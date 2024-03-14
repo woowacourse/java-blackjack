3febfa9faca428703264ae37d5786967c1ed3354
BurningFalls <sherlocky31415@gmail.com>
2024-03-14 09:59:16 +0900

    refactor: BetMaker에 string name 인스턴스변수 추가

--- src/main/java/blackjackgame/domain/gamers/BetMaker.java
+++ src/main/java/blackjackgame/domain/gamers/BetMaker.java
@@ -1,9 +1,11 @@
 package blackjackgame.domain.gamers;
 
 public class BetMaker {
+    private final String name;
     private final Double betMoney;
 
     public BetMaker(String name, Double betMoney) {
+        this.name = name;
         this.betMoney = betMoney;
     }
 
