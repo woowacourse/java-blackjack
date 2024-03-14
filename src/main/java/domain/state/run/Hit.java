3b2d1a441741669eadf1ef9d753d1c8da3d08ac2
dwax1324 <dwax1324@gmail.com>
2024-03-14 16:02:54 +0900

    feat: 카드를 뽑은 후 카드의 합이 21미만일 때 상태는 히트다

--- src/main/java/domain/state/run/Hit.java
+++ src/main/java/domain/state/run/Hit.java
@@ -3,6 +3,8 @@ package domain.state.run;
 import domain.card.Card;
 import domain.player.Hands;
 import domain.state.State;
+import domain.state.fininsh.Blackjack;
+import domain.state.fininsh.Bust;
 
 public class Hit extends Running {
     public Hit(final Hands hands) {
@@ -11,8 +13,14 @@ public class Hit extends Running {
 
     @Override
     public State draw(final Card card) {
-//        getHands().
-        return null;
+        getHands().hit(card);
+        if (getHands().getValue().size() == 2 && getHands().calculateScore() == 21) {
+            return new Blackjack(getHands());
+        }
+        if (getHands().calculateScore() > 21) {
+            return new Bust(getHands());
+        }
+        return new Hit(getHands());
     }
 
     @Override
