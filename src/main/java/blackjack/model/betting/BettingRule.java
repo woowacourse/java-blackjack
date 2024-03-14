b59b0571d782529d4aacb1b6876dab816303864b
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-15 03:12:40 +0900

    feat(BettingRule): 플레이어가 BlackJack으로 승리한 경우, 수익률인 1.5를 반환하는 기능 구현

--- src/main/java/blackjack/model/betting/BettingRule.java
+++ src/main/java/blackjack/model/betting/BettingRule.java
@@ -1,2 +1,20 @@
-package blackjack.model.betting;public class Rule {
+package blackjack.model.betting;
+
+import blackjack.model.participant.Player;
+import blackjack.model.result.ResultCommand;
+
+public class BettingRule {
+    public double calculateProfitRate(final Player player, final ResultCommand resultCommand) {
+        if (resultCommand.equals(ResultCommand.WIN)) {
+            return calculateProfitRateWhenPlayerWin(player);
+        }
+        return 0;
+    }
+
+    private double calculateProfitRateWhenPlayerWin(final Player player) {
+        if (player.isBlackJack()) {
+            return 1.5;
+        }
+        return 0;
+    }
 }
--- src/test/java/blackjack/model/betting/BettingRuleTest.java
+++ src/test/java/blackjack/model/betting/BettingRuleTest.java
@@ -1,2 +1,19 @@
-package blackjack.model.betting;public class Rule {
+package blackjack.model.betting;
+
+
+import static blackjack.model.PlayerFixture.BLACKJACK_PLAYER;
+import static blackjack.model.result.ResultCommand.WIN;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BettingRuleTest {
+
+    @Test
+    @DisplayName("플레이어가 BlackJack으로 승리한 경우, 배팅 금액의 1.5배를 준다.")
+    void findProfitAmountWhen() {
+        BettingRule bettingRule = new BettingRule();
+        assertThat(bettingRule.calculateProfitRate(BLACKJACK_PLAYER.getPlayer(), WIN)).isEqualTo(1.5);
+    }
 }
