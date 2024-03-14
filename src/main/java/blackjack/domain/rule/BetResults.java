5cb43317e94a53efcba6475c2c9ceeddd6974c47
libienz <leekh10195@gmail.com>
2024-03-14 11:58:55 +0900

    feat: 베팅 결과를 모아놓은 도메인 구현

---
+++ src/main/java/blackjack/domain/rule/BetResults.java
@@ -0,0 +1,19 @@
+package blackjack.domain.rule;
+
+import blackjack.domain.bet.Money;
+import java.util.List;
+
+public class BetResults {
+
+    private final List<BetResult> betResults;
+
+    public BetResults(List<BetResult> betResults) {
+        this.betResults = betResults;
+    }
+
+    public Money calculatePlayersEarned() {
+        return new Money(betResults.stream()
+                .map(BetResult::getEarned)
+                .reduce(0, Integer::sum));
+    }
+}
---
+++ src/test/java/blackjack/domain/rule/BetResultsTest.java
@@ -0,0 +1,22 @@
+package blackjack.domain.rule;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.domain.bet.Money;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("베팅 결과들 도메인 테스트")
+class BetResultsTest {
+
+    @DisplayName("플레이어 전체가 얼마의 수익을 냈는지 계산할 수 있다")
+    @Test
+    void testCalculatePalyersProfit() {
+        BetResult betResult1 = new BetResult("리비", new Money(10000));
+        BetResult betResult2 = new BetResult("썬", new Money(20000));
+        BetResults betResults = new BetResults(List.of(betResult1, betResult2));
+
+        assertThat(betResults.calculatePlayersEarned().getAmount()).isEqualTo(30000);
+    }
+}
