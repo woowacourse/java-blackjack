adaecfddb5360070fe7daa0ddd9a4081b9b9fb73
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-15 03:53:00 +0900

    feat(BettingRule): 플레이어가 딜러가 버스트여서 승리한 경우, 수익률인 0를 반환하는 기능 구현

--- src/main/java/blackjack/model/betting/BettingRule.java
+++ src/main/java/blackjack/model/betting/BettingRule.java
@@ -1,9 +1,16 @@
 package blackjack.model.betting;
 
+import blackjack.model.participant.Dealer;
 import blackjack.model.participant.Player;
 import blackjack.model.result.ResultCommand;
 
 public class BettingRule {
+    private final Dealer dealer;
+
+    public BettingRule(final Dealer dealer) {
+        this.dealer = dealer;
+    }
+
     public double calculateProfitRate(final Player player, final ResultCommand resultCommand) {
         if (resultCommand.equals(ResultCommand.WIN)) {
             return calculateProfitRateWhenPlayerWin(player);
@@ -15,6 +22,11 @@ public class BettingRule {
         if (player.isBlackJack()) {
             return 1.5;
         }
-        return 0;
+
+        if (dealer.isBust()) {
+            return 0;
+        }
+
+        return 1;
     }
 }
--- src/test/java/blackjack/model/betting/BettingRuleTest.java
+++ src/test/java/blackjack/model/betting/BettingRuleTest.java
@@ -2,9 +2,16 @@ package blackjack.model.betting;
 
 
 import static blackjack.model.PlayerFixture.BLACKJACK_PLAYER;
+import static blackjack.model.PlayerFixture.NOT_BLACKJACK_21_PLAYER;
 import static blackjack.model.result.ResultCommand.WIN;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import blackjack.model.deck.Card;
+import blackjack.model.deck.Score;
+import blackjack.model.deck.Shape;
+import blackjack.model.participant.Dealer;
+import blackjack.model.participant.Hand;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,8 +19,15 @@ class BettingRuleTest {
 
     @Test
     @DisplayName("플레이어가 BlackJack으로 승리한 경우, 배팅 금액의 1.5배를 준다.")
-    void findProfitAmountWhen() {
-        BettingRule bettingRule = new BettingRule();
+    void findProfitAmountWhenPlayerWinByBlackJack() {
+        BettingRule bettingRule = new BettingRule(new Dealer(new Hand(List.of(new Card(Shape.SPADE, Score.ACE), new Card(Shape.HEART, Score.TWO)))));
         assertThat(bettingRule.calculateProfitRate(BLACKJACK_PLAYER.getPlayer(), WIN)).isEqualTo(1.5);
+    }
+
+    @Test
+    @DisplayName("딜러가 버스트여서 플레이어가 승리한 경우, 배팅 금액의 0배를 준다. (= 추가 수익이 없다.)")
+    void findProfitAmountWhenPlayerWinByDealerBust() {
+        BettingRule bettingRule = new BettingRule(new Dealer(new Hand(List.of(new Card(Shape.SPADE, Score.TEN), new Card(Shape.HEART, Score.TEN), new Card(Shape.DIA, Score.TEN)))));
+        assertThat(bettingRule.calculateProfitRate(NOT_BLACKJACK_21_PLAYER.getPlayer(), WIN)).isEqualTo(0);
     }
 }
