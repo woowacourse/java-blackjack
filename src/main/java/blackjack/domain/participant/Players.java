02d8cf101ea50bb291df26129dcbb5c1cd191017
gyungchan Jo <namejgc@naver.com>
2024-03-14 10:44:38 +0900

    feat(Players): 플레이어들을 검증하는 기능 구현

---
+++ src/main/java/blackjack/domain/participant/Players.java
@@ -0,0 +1,42 @@
+package blackjack.domain.participant;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Players {
+    private static final int MINIMUM_PLAYER_SIZE = 1;
+    private static final int MAXIMUM_PLAYER_SIZE = 6;
+
+    private final List<Player> players;
+
+    public Players(final List<String> playerNames) {
+        validatePlayers(playerNames);
+        this.players = mapPlayer(playerNames);
+    }
+
+    private void validatePlayers(final List<String> playerNames) {
+        validateDuplicatedName(playerNames);
+        validatePlayerSize(playerNames);
+    }
+
+    private void validateDuplicatedName(final List<String> playerNames) {
+        if (playerNames.size() != new HashSet<>(playerNames).size()) {
+            throw new IllegalArgumentException("플레이어 이름 중 중복된 이름이 존재할 수 없습니다.");
+        }
+    }
+
+    private void validatePlayerSize(final List<String> playerNames) {
+        final int size = playerNames.size();
+
+        if (size < MINIMUM_PLAYER_SIZE || size > MAXIMUM_PLAYER_SIZE) {
+            throw new IllegalArgumentException(
+                    String.format("게임에 참여하는 플레이어는 %d ~ %d명이어야 합니다.", MINIMUM_PLAYER_SIZE, MAXIMUM_PLAYER_SIZE));
+        }
+    }
+
+    private List<Player> mapPlayer(final List<String> playerNames) {
+        return playerNames.stream()
+                .map(Player::new)
+                .toList();
+    }
+}
---
+++ src/test/java/blackjack/domain/participant/PlayersTest.java
@@ -0,0 +1,44 @@
+package blackjack.domain.participant;
+
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PlayersTest {
+
+    @Test
+    void 블랙잭_게임에_참여하는_플레이어들을_알_수_있다() {
+        final List<String> playerNames = List.of("pobi", "jason");
+        final Players players = new Players(playerNames);
+
+        assertThat(players).extracting("players", InstanceOfAssertFactories.list(Player.class))
+                .extracting("name")
+                .containsExactly(new Name("pobi"), new Name("jason"));
+    }
+
+    @Test
+    void 플레이어_이름_중_중복된_이름을_가지면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Players(List.of("pobi", "pobi")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("플레이어 이름 중 중복된 이름이 존재할 수 없습니다.");
+    }
+
+    @Test
+    void 게임에_참여하는_플레이어의_수가_1보다_작으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Players(Collections.emptyList()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("게임에 참여하는 플레이어는 1 ~ 6명이어야 합니다.");
+    }
+
+    @Test
+    void 게임에_참여하는_플레이어의_수가_6보다_크면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Players(List.of("11", "22", "33", "44", "55", "66", "77")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("게임에 참여하는 플레이어는 1 ~ 6명이어야 합니다.");
+    }
+}
