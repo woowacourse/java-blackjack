36ee1c74387834d1ac54872f03f0d8ca1e263e09
SCY <helenason@naver.com>
2024-03-14 21:00:10 +0900

    refactor: Assertions static import 처리

--- src/test/java/domain/ProfitTest.java
+++ src/test/java/domain/ProfitTest.java
@@ -1,6 +1,7 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,27 +11,27 @@ class ProfitTest {
     @Test
     void loseProfitWhenLoseGame() {
         Profit profit = new Profit(10);
-        Assertions.assertThat(profit.lose().getValue()).isEqualTo(-10);
+        assertThat(profit.lose().getValue()).isEqualTo(-10);
     }
 
     @DisplayName("저장된 금액을 0으로 바꾸어 반환한다.")
     @Test
     void noProfitWhenDraw() {
         Profit profit = new Profit(10);
-        Assertions.assertThat(profit.keep().getValue()).isEqualTo(0);
+        assertThat(profit.keep().getValue()).isEqualTo(0);
     }
 
     @DisplayName("저장된 금액을 반환한다.")
     @Test
     void winProfitWhenWinGame() {
         Profit profit = new Profit(10);
-        Assertions.assertThat(profit.win().getValue()).isEqualTo(10);
+        assertThat(profit.win().getValue()).isEqualTo(10);
     }
 
     @DisplayName("저장된 금액의 1.5배를 반환한다.")
     @Test
     void winSpecialProfitWhenBlackjack() {
         Profit profit = new Profit(10);
-        Assertions.assertThat(profit.specialWin().getValue()).isEqualTo(15);
+        assertThat(profit.specialWin().getValue()).isEqualTo(15);
     }
 }
