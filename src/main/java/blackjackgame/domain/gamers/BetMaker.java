e40912cccac9ba13ba67072d4511307f0c6d892b
BurningFalls <sherlocky31415@gmail.com>
2024-03-14 10:42:17 +0900

    feat: 0 이하의 값은 가질 수 없는 기능 구현

--- src/main/java/blackjackgame/domain/gamers/BetMaker.java
+++ src/main/java/blackjackgame/domain/gamers/BetMaker.java
@@ -5,8 +5,15 @@ public class BetMaker {
     private final Double betMoney;
 
     public BetMaker(String name, Double betMoney) {
+        validateBetMoneyBiggerThanZero(betMoney);
         this.name = name;
         this.betMoney = betMoney;
+    }
+
+    private void validateBetMoneyBiggerThanZero(Double betMoney) {
+        if (betMoney <= 0) {
+            throw new IllegalArgumentException("0 이하의 값은 가질 수 없습니다.");
+        }
     }
 
     public Double getBetMoney() {
