49774c95ddd42d4bee26c4c1c31be0c35007cef5
dwax1324 <dwax1324@gmail.com>
2024-03-14 16:06:04 +0900

    feat: 두 장의 카드를 뽑은 후 카드의 합이 21일 때 상태는 블랙잭이다

--- src/test/java/domain/state/run/HitTest.java
+++ src/test/java/domain/state/run/HitTest.java
@@ -7,6 +7,7 @@ import domain.card.Rank;
 import domain.card.Suit;
 import domain.player.Hands;
 import domain.state.State;
+import domain.state.fininsh.Blackjack;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -15,13 +16,23 @@ import org.junit.jupiter.api.Test;
 class HitTest {
 
     @Test
-    @DisplayName("카드를 뽑은 후 카드의 합이 21미만일 때 상태는 히트가 된다")
+    @DisplayName("카드를 뽑은 후 카드의 합이 21미만일 때 상태는 히트다")
     void hit() {
         final State hit = new Hit(new Hands(new ArrayList<>(List.of(new Card(Rank.SEVEN, Suit.CLUBS)))));
 
         final State newState = hit.draw(new Card(Rank.THREE, Suit.CLUBS));
 
         assertThat(newState).isInstanceOf(Hit.class);
+    }
+
+    @Test
+    @DisplayName("두 장의 카드를 뽑은 후 카드의 합이 21일 때 상태는 블랙잭이다")
+    void blackjack() {
+        final State hit = new Hit(new Hands(new ArrayList<>(List.of(new Card(Rank.ACE, Suit.CLUBS)))));
+
+        final State newState = hit.draw(new Card(Rank.TEN, Suit.CLUBS));
+
+        assertThat(newState).isInstanceOf(Blackjack.class);
     }
 
 }
