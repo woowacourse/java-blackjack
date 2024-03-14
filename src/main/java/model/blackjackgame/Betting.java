ac10150f8e310be5a96006731a459f35727f5efa
yoonseo choi <choiyoonseo02@gmail.com>
2024-03-14 10:49:34 +0900

    feat: 0원 이하의 배팅을 하면 예외가 발생한다

---
+++ src/main/java/model/blackjackgame/Betting.java
@@ -0,0 +1,23 @@
+package model.blackjackgame;
+
+import model.player.Player;
+
+public class Betting {
+
+    private static final int MIN_BETTING_MONEY = 0;
+
+    private final Player player;
+    private final int money;
+
+    public Betting(Player player, int money) {
+        validate(money);
+        this.player = player;
+        this.money = money;
+    }
+
+    private void validate(int money) {
+        if (money <= MIN_BETTING_MONEY) {
+            throw new IllegalArgumentException("0원 이하의 베팅은 하실 수 없습니다");
+        }
+    }
+}
