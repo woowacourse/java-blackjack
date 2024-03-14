930b671a9bee88acdff2e2ea03f6c408a7c2bb5d
libienz <leekh10195@gmail.com>
2024-03-14 10:45:38 +0900

    feat: 이름으로 결과 조회하는 기능 추가

--- src/main/java/blackjack/domain/rule/PlayerResults.java
+++ src/main/java/blackjack/domain/rule/PlayerResults.java
@@ -10,6 +10,14 @@ public class PlayerResults {
         this.results = results;
     }
 
+    public GameResult findResultByName(String name) {
+        return results.stream()
+                .filter(result -> result.getPlayerName().equals(name))
+                .findFirst()
+                .map(PlayerResult::getGameResult)
+                .orElseThrow(() -> new IllegalArgumentException("[INTERNAL ERROR] 이름으로 결과를 조회하는데에 실패했습니다"));
+    }
+
     public List<PlayerResult> getResults() {
         return results;
     }
---
+++ src/test/java/blackjack/domain/rule/PlayerResultsTest.java
@@ -0,0 +1,23 @@
+package blackjack.domain.rule;
+
+import static blackjack.domain.rule.GameResult.PLAYER_LOSE;
+import static blackjack.domain.rule.GameResult.PLAYER_WIN;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("플레이어 결과 도메인 테스트")
+class PlayerResultsTest {
+
+    @DisplayName("이름으로 결과를 조회할 수 있다")
+    @Test
+    void testFindGameResultByName() {
+        PlayerResult playerResult1 = new PlayerResult("리비", PLAYER_WIN);
+        PlayerResult playerResult2 = new PlayerResult("리비", PLAYER_LOSE);
+        PlayerResults playerResults = new PlayerResults(List.of(playerResult1, playerResult2));
+
+        assertThat(playerResults.findResultByName("리비")).isEqualTo(PLAYER_WIN);
+    }
+}
