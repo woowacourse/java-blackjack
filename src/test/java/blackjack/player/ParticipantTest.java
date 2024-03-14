58173b159b38a8eefb053b90c2fc0301671ba782
donghoony <aru0504@naver.com>
2024-03-14 15:45:32 +0900

    test(Participant): 최초 카드를 가져오는 테스트 작성

--- src/test/java/blackjack/player/ParticipantTest.java
+++ src/test/java/blackjack/player/ParticipantTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import blackjack.card.Card;
 import blackjack.card.Number;
 import blackjack.card.Shape;
+import java.util.Iterator;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,5 +42,27 @@ class ParticipantTest {
         boolean isDrawable = Participant.hasDrawableScore();
         // then
         assertThat(isDrawable).isFalse();
+    }
+
+    @Test
+    @DisplayName("플레이어는 처음에 두 장의 카드를 오픈한다.")
+    void revealFirstCardTest() {
+        // given
+        List<Card> cards = List.of(
+                Card.of(Shape.HEART, Number.JACK),
+                Card.of(Shape.DIAMOND, Number.TEN),
+                Card.of(Shape.SPADE, Number.ACE)
+        );
+        List<Card> expectedCards = List.of(
+                Card.of(Shape.HEART, Number.JACK),
+                Card.of(Shape.DIAMOND, Number.TEN)
+        );
+        Iterator<Card> iterator = cards.iterator();
+        Participant participant = new Participant("aru");
+        participant.drawCards(iterator::next, 3);
+        // when
+        List<Card> revealedCards = participant.revealCardsOnFirstPhase();
+        // then
+        assertThat(revealedCards).containsExactlyElementsOf(expectedCards);
     }
 }
