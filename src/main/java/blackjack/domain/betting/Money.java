a733953ba2c1e14fa31fd2475b1ec53affffa2b4
3juhwan <13selfesteem91@naver.com>
2024-03-15 00:12:37 +0900

    refactor: Money 클래스 매개변수로 문자열을 받는 생성자 삭제

--- src/main/java/blackjack/domain/betting/Money.java
+++ src/main/java/blackjack/domain/betting/Money.java
@@ -1,24 +1,13 @@
 package blackjack.domain.betting;
 
-public class Money {
-    private final int value;
-
-    private Money(final int value) {
-        validateValue(value);
-        this.value = value;
+public record Money(int value) {
+    public Money {
+        validateMinimum(value);
     }
 
-    public Money(final String value) {
-        this(Integer.parseInt(value));
-    }
-
-    private void validateValue(final int value) {
+    private void validateMinimum(final int value) {
         if (value < 0) {
             throw new IllegalArgumentException("돈의 액수는 0 이상이어야 합니다.");
         }
-    }
-
-    public int getValue() {
-        return this.value;
     }
 }
--- src/test/java/blackjack/domain/betting/MoneyTest.java
+++ src/test/java/blackjack/domain/betting/MoneyTest.java
@@ -9,17 +9,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MoneyTest {
     @ParameterizedTest
-    @ValueSource(strings = {"0", "1", "100000000"})
-    void 돈이_정상적으로_생성된다(final String value) {
+    @ValueSource(ints = {0, 1, 100000000})
+    void 돈이_정상적으로_생성된다(final int value) {
         assertThatCode(() -> new Money(value))
                 .doesNotThrowAnyException();
     }
 
     @Test
     void 음수의_돈은_만들어지지_않는다() {
-        assertThatThrownBy(() -> new Money("-1"))
+        assertThatThrownBy(() -> new Money(-1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("돈의 액수는 0 이상이어야 합니다.");
     }
-
 }
