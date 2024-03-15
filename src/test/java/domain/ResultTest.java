67ba0619a88caaac35085ee2b705132cc0693505
nayonsoso <panda0329@naver.com>
2024-03-15 15:36:13 +0900

    feat: 플레이러가 블랙잭으로 우승했는지 판단 기능 구현

--- src/test/java/domain/ResultTest.java
+++ src/test/java/domain/ResultTest.java
@@ -15,11 +15,14 @@ class ResultTest {
     @Test
     void getGameResult() {
         List<Card> bustCards = List.of(TEN_HEART, TEN_HEART, TWO_HEART);
-        List<Card> maxScoreCards = List.of(ACE_HEART, TEN_HEART);
+        List<Card> blackJackCards = List.of(TEN_HEART, ACE_HEART);
+        List<Card> maxScoreCards = List.of(TEN_HEART, TEN_HEART, ACE_HEART);
         List<Card> minScoreCards = List.of(TWO_HEART);
 
         Dealer dealerWithBust = new Dealer(NOT_SHUFFLED_CARD_DECK);
         dealerWithBust.initHand(bustCards);
+        Dealer dealerWithBlackJack = new Dealer(NOT_SHUFFLED_CARD_DECK);
+        dealerWithBlackJack.initHand(blackJackCards);
         Dealer dealerWithMaxScore = new Dealer(NOT_SHUFFLED_CARD_DECK);
         dealerWithMaxScore.initHand(maxScoreCards);
         Dealer dealerWithMinScore = new Dealer(NOT_SHUFFLED_CARD_DECK);
@@ -27,18 +30,27 @@ class ResultTest {
 
         Player playerWithBust = new Player("버스트");
         playerWithBust.initHand(bustCards);
+        Player playerWithBlackJack = new Player("블랙잭");
+        playerWithBlackJack.initHand(blackJackCards);
         Player playerWithMaxScore = new Player("최고 점수");
         playerWithMaxScore.initHand(maxScoreCards);
         Player playerWithMinScore = new Player("최저 점수");
         playerWithMinScore.initHand(minScoreCards);
 
-        assertAll(
-                () -> assertThat(Result.of(dealerWithBust, playerWithBust)).isEqualTo(Result.DEALER_WIN),
-                () -> assertThat(Result.of(dealerWithMinScore, playerWithBust)).isEqualTo(Result.DEALER_WIN),
-                () -> assertThat(Result.of(dealerWithMaxScore, playerWithMinScore)).isEqualTo(Result.DEALER_WIN),
-                () -> assertThat(Result.of(dealerWithMaxScore, playerWithMaxScore)).isEqualTo(Result.PUSH),
+        assertAll("한쪽만 버스트되면 상대가 이긴다. 둘 다 버스트되면 딜러가 이긴다.",
                 () -> assertThat(Result.of(dealerWithBust, playerWithMinScore)).isEqualTo(Result.PLAYER_WIN),
-                () -> assertThat(Result.of(dealerWithMinScore, playerWithMaxScore)).isEqualTo(Result.PLAYER_WIN)
+                () -> assertThat(Result.of(dealerWithMinScore, playerWithBust)).isEqualTo(Result.DEALER_WIN),
+                () -> assertThat(Result.of(dealerWithBust, playerWithBust)).isEqualTo(Result.DEALER_WIN));
+
+        assertAll("둘 다 버스트되지 않으면 21에 가까운 쪽이 이긴다. 점수가 같으면 비긴다.",
+                () -> assertThat(Result.of(dealerWithMaxScore, playerWithMinScore)).isEqualTo(Result.DEALER_WIN),
+                () -> assertThat(Result.of(dealerWithMinScore, playerWithMaxScore)).isEqualTo(Result.PLAYER_WIN),
+                () -> assertThat(Result.of(dealerWithMaxScore, playerWithMaxScore)).isEqualTo(Result.PUSH));
+
+        assertAll("한쪽만 블랙잭이면 그쪽이 이긴다. 둘 다 블랙잭이면 비긴다.",
+                () -> assertThat(Result.of(dealerWithMaxScore, playerWithBlackJack)).isEqualTo(Result.PLAYER_BLACK_JACK_WIN),
+                () -> assertThat(Result.of(dealerWithBlackJack, playerWithMaxScore)).isEqualTo(Result.DEALER_WIN),
+                () -> assertThat(Result.of(dealerWithBlackJack, playerWithBlackJack)).isEqualTo(Result.PUSH)
         );
     }
 }
