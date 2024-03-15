240d86682ee040fced1c02531833511200f4d97c
gyungchan Jo <namejgc@naver.com>
2024-03-15 17:14:11 +0900

    refactor: view 영역 클래스들의 final 키워드 제거

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -11,13 +11,13 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
     private final InputValidator inputValidator;
 
-    public InputView(final InputValidator inputValidator) {
+    public InputView(InputValidator inputValidator) {
         this.inputValidator = inputValidator;
     }
 
     public String readPlayerNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        final String input = scanner.nextLine();
+        String input = scanner.nextLine();
 
         inputValidator.validatePlayerNames(input);
         return input;
@@ -25,17 +25,17 @@ public class InputView {
 
     public int readBattingAmount(String name) {
         System.out.printf("%n%s의 배팅 금액은?%n", name);
-        final String input = scanner.nextLine();
+        String input = scanner.nextLine();
 
         inputValidator.validateBattingAmount(input);
         return Integer.parseInt(input);
     }
 
     public String readReceiveMoreCardOrNot(String name) {
-        final String message = String.format(
+        String message = String.format(
                 "%s는 한장의 카드를 더 받겠습니까?(예는 %s, 아니오는 %s)", name, EXPRESSION_OF_YES, EXPRESSION_OF_NO);
         System.out.println(message);
-        final String input = scanner.nextLine();
+        String input = scanner.nextLine();
 
         inputValidator.validateReceiveMoreCardOrNot(input);
         return input;
--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -15,111 +15,111 @@ public class OutputView {
     private static final String DELIMITER = ", ";
     private static final String ERROR_SUFFIX = "[ERROR]";
 
-    public void printInitialSettingMessage(final ParticipantDto dealer, final ParticipantsDto players) {
-        final String dealerName = dealer.name();
-        final List<String> playerNames = getParticipantNames(players.participants());
-        final String playerNamesMessage = joinWithComma(playerNames);
+    public void printInitialSettingMessage(ParticipantDto dealer, ParticipantsDto players) {
+        String dealerName = dealer.name();
+        List<String> playerNames = getParticipantNames(players.participants());
+        String playerNamesMessage = joinWithComma(playerNames);
 
         System.out.printf("%n%s와 %s에게 %d장을 나누었습니다.%n", dealerName, playerNamesMessage, INITIAL_CARD_COUNT);
     }
 
-    private List<String> getParticipantNames(final List<ParticipantDto> participants) {
+    private List<String> getParticipantNames(List<ParticipantDto> participants) {
         return participants.stream()
                 .map(ParticipantDto::name)
                 .toList();
     }
 
-    private String joinWithComma(final List<String> source) {
+    private String joinWithComma(List<String> source) {
         return String.join(DELIMITER, source);
     }
 
-    public void printFirstCardOfDealer(final ParticipantDto participant) {
-        final String dealerName = participant.name();
-        final List<CardDto> cardDtos = participant.cards();
-        final List<CardDto> firstCardOfDealer = List.of(cardDtos.get(0));
-        final String cardsInfoMessage = generateCardsInfoMessage(firstCardOfDealer);
+    public void printFirstCardOfDealer(ParticipantDto participant) {
+        String dealerName = participant.name();
+        List<CardDto> cardDtos = participant.cards();
+        List<CardDto> firstCardOfDealer = List.of(cardDtos.get(0));
+        String cardsInfoMessage = generateCardsInfoMessage(firstCardOfDealer);
 
         System.out.printf("%s: %s%n", dealerName, cardsInfoMessage);
     }
 
-    private String generateCardsInfoMessage(final List<CardDto> cards) {
-        final List<String> cardsInformation = getCardsInformation(cards);
+    private String generateCardsInfoMessage(List<CardDto> cards) {
+        List<String> cardsInformation = getCardsInformation(cards);
 
         return joinWithComma(cardsInformation);
     }
 
-    private List<String> getCardsInformation(final List<CardDto> cards) {
+    private List<String> getCardsInformation(List<CardDto> cards) {
         return cards.stream()
                 .map(cardDto -> cardDto.denomination() + cardDto.suit())
                 .toList();
     }
 
-    public void printParticipantsCards(final ParticipantsDto participants) {
-        final List<ParticipantDto> participantDtos = participants.participants();
-        final List<String> participantsCardsMessage = generateParticipantsCardsMessage(participantDtos);
+    public void printParticipantsCards(ParticipantsDto participants) {
+        List<ParticipantDto> participantDtos = participants.participants();
+        List<String> participantsCardsMessage = generateParticipantsCardsMessage(participantDtos);
 
-        final String message = joinWithBlankLine(participantsCardsMessage);
+        String message = joinWithBlankLine(participantsCardsMessage);
         System.out.println(message + NEWLINE);
     }
 
-    private String joinWithBlankLine(final List<String> source) {
+    private String joinWithBlankLine(List<String> source) {
         return String.join(NEWLINE, source);
     }
 
-    private List<String> generateParticipantsCardsMessage(final List<ParticipantDto> participantDtos) {
+    private List<String> generateParticipantsCardsMessage(List<ParticipantDto> participantDtos) {
         return participantDtos.stream()
                 .map(this::generateParticipantCardsMessage)
                 .toList();
     }
 
     private String generateParticipantCardsMessage(ParticipantDto participant) {
-        final String name = participant.name();
-        final String cardsInfoMessage = generateCardsInfoMessage(participant.cards());
+        String name = participant.name();
+        String cardsInfoMessage = generateCardsInfoMessage(participant.cards());
 
         return String.format("%s카드: %s", name, cardsInfoMessage);
     }
 
-    public void printParticipantCards(final ParticipantDto participant) {
-        final String participantCardsMessage = generateParticipantCardsMessage(participant);
+    public void printParticipantCards(ParticipantDto participant) {
+        String participantCardsMessage = generateParticipantCardsMessage(participant);
         System.out.println(participantCardsMessage);
     }
 
     public void printDealerReceiveCardMessage() {
-        final String message = String.format("%n%s는 %d이하라 한장의 카드를 더 받았습니다.", DEFAULT_NAME_OF_DEALER, DEALER_MIN_SCORE_POLICY);
+        String message = String.format("%n%s는 %d이하라 한장의 카드를 더 받았습니다.", DEFAULT_NAME_OF_DEALER, DEALER_MIN_SCORE_POLICY);
         System.out.println(message);
     }
 
-    public void printParticipantsCardsWithScore(final ParticipantsDto participantsDto) {
-        final List<ParticipantDto> participants = participantsDto.participants();
-        final List<String> cardsWithScoreMessages = generateCardsWithScoreMessages(participants);
+    public void printParticipantsCardsWithScore(ParticipantsDto participantsDto) {
+        List<ParticipantDto> participants = participantsDto.participants();
+        List<String> cardsWithScoreMessages = generateCardsWithScoreMessages(participants);
 
         System.out.println(NEWLINE + joinWithBlankLine(cardsWithScoreMessages));
     }
 
-    private List<String> generateCardsWithScoreMessages(final List<ParticipantDto> participants) {
+    private List<String> generateCardsWithScoreMessages(List<ParticipantDto> participants) {
         return participants.stream()
                 .map(this::generateCardsWithScoreMessage)
                 .toList();
     }
 
-    private String generateCardsWithScoreMessage(final ParticipantDto participant) {
-        final String participantCardsMessage = generateParticipantCardsMessage(participant);
-        final int score = participant.score();
+    private String generateCardsWithScoreMessage(ParticipantDto participant) {
+        String participantCardsMessage = generateParticipantCardsMessage(participant);
+        int score = participant.score();
 
         return String.format("%s - 결과: %d", participantCardsMessage, score);
     }
 
-    public void printProfits(final ProfitResultDto profitResult) {
+    public void printProfits(ProfitResultDto profitResult) {
         System.out.println(NEWLINE + "## 최종 수익");
 
-        final Map<String, Integer> result = profitResult.result();
+        Map<String, Integer> result = profitResult.result();
         result.forEach((name, profit) -> {
-            final String profitMessage = String.format("%s: %d", name, profit);
+            String profitMessage = String.format("%s: %d", name, profit);
             System.out.println(profitMessage);
         });
     }
 
-    public void printErrorMessage(final String errorMessage) {
+    public void printErrorMessage(String errorMessage) {
         System.out.printf("%s %s%n", ERROR_SUFFIX, errorMessage);
     }
 }
