e0cbfd1602751df9c84ac18e6dafddc24b388a78
lemone <lejinhy@gmail.com>
2024-03-15 23:52:41 +0900

    feat: 금액이 0 이하일 경우 검증하는 메서드 추가

--- src/main/java/blackjack/domain/supplies/Chip.java
+++ src/main/java/blackjack/domain/supplies/Chip.java
@@ -4,7 +4,14 @@ public class Chip {
     private int money;
 
     public Chip(int money) {
+        validateNaturalNumber(money);
         this.money = money;
+    }
+
+    private void validateNaturalNumber(int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("0 이상의 정수를 입력해 주세요.");
+        }
     }
 
     public void add(int money) {
