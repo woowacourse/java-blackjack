d733590ca2e1ca6c54ae97c5a0263f93471ba92e
nayonsoso <panda0329@naver.com>
2024-03-15 16:54:33 +0900

    feat: 딜러의 배팅 정산 기능 구현

--- src/main/java/domain/BettingPot.java
+++ src/main/java/domain/BettingPot.java
@@ -11,7 +11,7 @@ public class BettingPot {
         betRecord.put(player, bet);
     }
 
-    public Map<Player, Integer> settle(Map<Player, Result> playerResults) {
+    public Map<Player, Integer> settlePlayer(Map<Player, Result> playerResults) {
         Map<Player, Integer> settlement = new LinkedHashMap<>();
         for (Map.Entry<Player, Result> playerResult : playerResults.entrySet()) {
             Player player = playerResult.getKey();
@@ -21,6 +21,17 @@ public class BettingPot {
         }
 
         return settlement;
+    }
+
+    public int settleDealer(Map<Player, Result> playerResults) {
+        int allPlayersWinning = 0;
+        for (Map.Entry<Player, Result> playerResult : playerResults.entrySet()) {
+            Player player = playerResult.getKey();
+            int amount = betRecord.get(player).getAmount();
+            allPlayersWinning += calculateAmount(playerResult.getValue(), amount);
+        }
+
+        return allPlayersWinning * -1;
     }
 
     private int calculateAmount(Result result, int amount) {
