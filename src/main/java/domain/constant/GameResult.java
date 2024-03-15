a37c99b52c7529409fea31be65f44b452679a2f8
HyunJoong Kim <hjk0761@naver.com>
2024-03-15 23:19:34 +0900

    style: 가독성을 위한 개행 추가

--- src/main/java/domain/constant/GameResult.java
+++ src/main/java/domain/constant/GameResult.java
@@ -3,7 +3,8 @@ package domain.constant;
 public enum GameResult {
     WIN(1),
     LOSE(-1),
-    WIN_BY_BLACKJACK(1.5);
+    WIN_BY_BLACKJACK(1.5),
+    ;
 
     private final double profitRatio;
 
