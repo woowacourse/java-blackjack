c62c7b995043e2a87332230d43e47bf2e77aeb30
mzeong <mjeong@sookmyung.ac.kr>
2024-03-15 22:10:01 +0900

    refactor: 의미 없는 equals 코드 제거

--- src/main/java/blackjack/domain/participant/PlayerName.java
+++ src/main/java/blackjack/domain/participant/PlayerName.java
@@ -1,23 +1,4 @@
 package blackjack.domain.participant;
 
-import java.util.Objects;
-
 public record PlayerName(String value) {
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        PlayerName playerName = (PlayerName) o;
-        return Objects.equals(value, playerName.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
 }
