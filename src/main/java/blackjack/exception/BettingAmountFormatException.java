ad1f22c5c6c9a47a639986c4bc51b07bbeacb1de
masonkimseoul <masonkimseoul@gmail.com>
2024-03-15 09:08:03 +0900

    refactor: 예외 처리 클래스 분리

--- src/main/java/blackjack/exception/BettingAmountFormatException.java
+++ src/main/java/blackjack/exception/BettingAmountFormatException.java
@@ -1,7 +1,7 @@
 package blackjack.exception;
 
 public class BettingAmountFormatException extends IllegalArgumentException{
-    private final static String MESSAGE = "베팅 금액은 양의 정수만 입력될 수 있습니다.";
+    private final static String MESSAGE = "베팅 금액은 정수만 입력될 수 있습니다.";
 
     public BettingAmountFormatException() {
         super(MESSAGE);
