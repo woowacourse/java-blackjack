37eb92120e6a03c39effcc6ffb61c25ffe3fc23a
KimSangHyeok <tkdgur0906@naver.com>
2024-03-15 23:43:33 +0900

    feat(Incomes) : Incomes가 모든 플레이어의 수익 반환하는 기능 추가

--- src/main/java/domain/result/Incomes.java
+++ src/main/java/domain/result/Incomes.java
@@ -1,47 +1,54 @@
 package domain.result;
 
-import domain.participant.Name;
+import domain.participant.Dealer;
+import domain.participant.Player;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Incomes {
 
-    private final Map<Name, Income> incomes;
+    private final List<Player> players;
 
-    public Incomes(Map<Name, Income> incomes) {
-        this.incomes = incomes;
+    public Incomes(List<Player> players) {
+        this.players = players;
     }
 
-    public int getDealerIncome() {
-        int sumOfPlayerIncome = incomes.values()
-                .stream()
+    public Income dealerIncome(Dealer dealer) {
+        int sumOfPlayerIncome = players.stream()
+                .map(player -> player.determineIncome(dealer.decideStatus(player)))
                 .mapToInt(Income::getIncome)
                 .sum();
-        return -sumOfPlayerIncome;
+        return new Income(-sumOfPlayerIncome);
     }
 
-    public Map<Name, Income> getIncomes() {
-        return incomes;
+    public Map<Player, Income> playersIncomes(Dealer dealer) {
+        return players.stream()
+                .collect(Collectors.toMap(
+                        player -> player,
+                        player -> player.determineIncome(dealer.decideStatus(player))
+                ));
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Incomes incomes1 = (Incomes) o;
-        return Objects.equals(incomes, incomes1.incomes);
+        Incomes incomes = (Incomes) o;
+        return Objects.equals(players, incomes.players);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(incomes);
+        return Objects.hash(players);
     }
 
     @Override
     public String toString() {
         return "Incomes{" +
-                "incomes=" + incomes +
+                "players=" + players +
                 '}';
     }
 }
--- src/test/java/domain/IncomesTest.java
+++ src/test/java/domain/IncomesTest.java
@@ -1,24 +1,50 @@
 package domain;
 
+import domain.card.Card;
+import domain.card.Shape;
+import domain.participant.BetAmount;
+import domain.participant.Dealer;
 import domain.participant.Name;
+import domain.participant.Player;
 import domain.result.Income;
 import domain.result.Incomes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class IncomesTest {
 
     @Test
-    @DisplayName("딜러의 최종 수익을 반환한다.")
-    void getDealerIncome() {
-        Incomes positiveIncome = new Incomes(Map.of(new Name("capy1"), new Income(-1000), new Name("capy2"), new Income(-2000)));
-        Incomes negativeIncome = new Incomes(Map.of(new Name("capy1"), new Income(1000), new Name("capy2"), new Income(2000)));
+    @DisplayName("딜러의 최종 수익이 양수이다.")
+    void dealerIncome_Win() {
+        Player capy1 = new Player(new Name("capy1"), new BetAmount(1000),
+                List.of(new Card(5, Shape.CLUB), new Card(6, Shape.CLUB)));
+        Player capy2 = new Player(new Name("capy2"), new BetAmount(2000),
+                List.of(new Card(5, Shape.HEART), new Card(6, Shape.HEART)));
+        Dealer dealer = new Dealer(new ArrayList<>(
+                List.of(new Card(6, Shape.SPADE), new Card(10, Shape.SPADE))));
 
-        assertThat(positiveIncome.getDealerIncome()).isEqualTo(3000);
-        assertThat(negativeIncome.getDealerIncome()).isEqualTo(-3000);
+        Incomes incomes = new Incomes(List.of(capy1, capy2));
+
+        assertThat(incomes.dealerIncome(dealer)).isEqualTo(new Income(3000));
+    }
+
+    @Test
+    @DisplayName("딜러의 최종 수익이 음수이다.")
+    void dealerIncome_Lose() {
+        Player capy1 = new Player(new Name("capy1"), new BetAmount(1000),
+                List.of(new Card(10, Shape.CLUB), new Card(11, Shape.CLUB)));
+        Player capy2 = new Player(new Name("capy2"), new BetAmount(2000),
+                List.of(new Card(10, Shape.HEART), new Card(11, Shape.HEART)));
+        Dealer dealer = new Dealer(new ArrayList<>(
+                List.of(new Card(6, Shape.SPADE), new Card(10, Shape.SPADE))));
+
+        Incomes incomes = new Incomes(List.of(capy1, capy2));
+
+        assertThat(incomes.dealerIncome(dealer)).isEqualTo(new Income(-3000));
     }
 }
