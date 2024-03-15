156b714988cf35c98b1abd8ed9f332cae0cb3e85
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-15 16:02:21 +0900

    test(Dealer): 딜러는 이름을 가지고 카드를 받을 수 있다는 누락된 테스트 추가

--- src/test/java/blackjack/model/participant/DealerTest.java
+++ src/test/java/blackjack/model/participant/DealerTest.java
@@ -2,11 +2,13 @@ package blackjack.model.participant;
 
 import static blackjack.model.HandFixture.OVER_16_UNDER_21_HAND;
 import static blackjack.model.HandFixture.UNDER_16_HAND;
+import static blackjack.model.deck.Score.ACE;
 import static blackjack.model.deck.Score.FIVE;
-import static blackjack.model.deck.Score.SEVEN;
-import static blackjack.model.deck.Score.SIX;
 import static blackjack.model.deck.Score.TEN;
+import static blackjack.model.deck.Score.THREE;
+import static blackjack.model.deck.Score.TWO;
 import static blackjack.model.deck.Shape.CLOVER;
+import static blackjack.model.deck.Shape.HEART;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import blackjack.model.deck.Card;
@@ -17,7 +19,25 @@ import org.junit.jupiter.api.Test;
 class DealerTest {
 
     @Test
-    @DisplayName("딜러는 16이하이면 카드를 추가로 받는다.")
+    @DisplayName("딜러는 딜러라는 이름을 가진다.")
+    void getName() {
+        Dealer dealer = new Dealer(UNDER_16_HAND.getHand());
+
+        assertThat(dealer.getName()).isEqualTo(new Name("딜러"));
+    }
+
+    @Test
+    @DisplayName("딜러는 카드를 받는다.")
+    void hitDealer() {
+        Dealer dealer = new Dealer(new Hand(List.of(new Card(CLOVER, ACE), new Card(CLOVER, THREE))));
+        dealer.receiveCard(new Card(HEART, TWO));
+
+        assertThat(dealer.openCards())
+                .isEqualTo(List.of(new Card(CLOVER, ACE), new Card(CLOVER, THREE), new Card(HEART, TWO)));
+    }
+
+    @Test
+    @DisplayName("딜러는 16이하이면 카드를 추가로 받을 수 있다.")
     void canReceive() {
         Dealer dealer = new Dealer(UNDER_16_HAND.getHand());
         assertThat(dealer.canHit()).isTrue();
