a514de4eec017f7115efd6241400258d3f9884db
nayonsoso <panda0329@naver.com>
2024-03-15 21:51:24 +0900

    refactor: 깨진 테스트 해결

--- src/main/java/domain/Bet.java
+++ src/main/java/domain/Bet.java
@@ -2,7 +2,7 @@ package domain;
 
 public class Bet {
 
-    static final int MIN_BET = 1;
+    static final int MIN_BET = 10;
     static final int BET_UNIT = 10;
     static final String BET_RANGE_MESSAGE = String.format("배팅 금액은 %d 보다 커야합니다.", MIN_BET);
     static final String BET_UNIT_MESSAGE = String.format("배팅 금액은 %d 단위여야 합니다.", BET_UNIT);
