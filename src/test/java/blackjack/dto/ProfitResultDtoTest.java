2f2968f3dd908944d37f7e28dfd2d3d6ddd23dc6
gyungchan Jo <namejgc@naver.com>
2024-03-15 16:24:57 +0900

    feat(ProfitResultDto): 결과 출력을 위한 ProfitResultDto 구현

---
+++ src/test/java/blackjack/dto/ProfitResultDtoTest.java
@@ -0,0 +1,28 @@
+package blackjack.dto;
+
+import blackjack.domain.card.CardDeck;
+import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Player;
+import blackjack.domain.profit.ProfitResult;
+import blackjack.domain.strategy.ReverseCardShuffleStrategy;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+class ProfitResultDtoTest {
+
+    @Test
+    void dto로_변환할_수_있다() {
+        final Dealer dealer = new Dealer(new CardDeck(), new ReverseCardShuffleStrategy());
+        final Player player = new Player("pobi");
+        final ProfitResult profitResult = new ProfitResult();
+        profitResult.recordParticipantsProfit(dealer, player, 10000);
+
+        final ProfitResultDto profitResultDto = ProfitResultDto.from(profitResult);
+
+        assertThat(profitResultDto.result()).containsExactly(
+                entry("딜러", -10000), entry("pobi", 10000)
+        );
+    }
+}
