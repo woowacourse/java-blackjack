56760ab2559488b5d8b14aaf618040a8ab3d4ee7
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-03-15 09:48:10 +0900

    refactor(BlackJackTest): 참가자 생성 코드 중복 제거

--- src/test/java/model/BlackJackTest.java
+++ src/test/java/model/BlackJackTest.java
@@ -20,10 +20,7 @@ class BlackJackTest {
     @DisplayName("참가자가 null일 시 예외가 발생한다.")
     @Test
     void validateParticipantIsNotNull() {
-        Dealer dealer = new Dealer(new CardDeck(CardDeck.createCards()), () ->
-                List.of(new Card(CardShape.SPACE, CardNumber.NINE),
-                        new Card(CardShape.SPACE, CardNumber.TWO)));
-
+        Dealer dealer = createBustDealer();
         Assertions.assertThatThrownBy(() -> new BlackJack(null, dealer))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -33,38 +30,26 @@ class BlackJackTest {
     void validateDealerIsNotNull() {
         List<Card> participantCards = List.of(new Card(CardShape.SPACE, CardNumber.NINE),
                 new Card(CardShape.SPACE, CardNumber.FIVE));
-        Assertions.assertThatThrownBy(
-                        () -> new BlackJack(
-                                new Participants(
-                                        List.of(new Participant("배키", participantCards, new BettingMoney(100)))),
+        Assertions.assertThatThrownBy(() -> new BlackJack(new Participants(
+                List.of(new Participant("배키", participantCards, new BettingMoney(100)),
+                        new Participant("켬미", participantCards, new BettingMoney(200)))),
                                 null))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("딜러는 버스트가 아니고 참가자가 버스트라면, 참가자의 수익은 베팅금액의 -1배다.")
     @Test
-    void findLoseOutcomeParticipantOverThreshold() {
-        Participant participant = new Participant("배키",
-                List.of(new Card(CardShape.SPACE, CardNumber.NINE),
-                        new Card(CardShape.DIAMOND, CardNumber.NINE)),
-                new BettingMoney(100));
-        participant.addCard(new Card(CardShape.CLOVER, CardNumber.NINE));
-        Participant participant2 = new Participant("켬미",
-                List.of(new Card(CardShape.CLOVER, CardNumber.EIGHT),
-                        new Card(CardShape.HEART, CardNumber.SEVEN)),
-                new BettingMoney(200));
-        participant2.addCard(new Card(CardShape.SPACE, CardNumber.KING));
+    void findLoseOutcomeParticipantOverThreshold() { //todo 이름 클래스
+        Participant participant1 = createBustParticipant("배키", new BettingMoney(100));
+        Participant participant2 = createBustParticipant("켬미", new BettingMoney(200));
 
-        Dealer notBustDealer = new Dealer(new CardDeck(CardDeck.createCards()), () ->
-                List.of(new Card(CardShape.SPACE, CardNumber.KING),
-                        new Card(CardShape.CLOVER, CardNumber.KING)));
+        Dealer notBustDealer = createNotBustDealer();
 
-        Participants participants = new Participants(List.of(participant, participant2));
+        Participants participants = new Participants(List.of(participant1, participant2));
         BlackJack blackJack = new BlackJack(participants, notBustDealer);
 
         Map<Participant, Double> playerProfits = blackJack.matchParticipantProfit();
-        //todo : double이 아닌 Money 객체 만들기
-        assertThat(playerProfits).isEqualTo(Map.of(participant, -100.0, participant2, -200.0));
+        assertThat(playerProfits).isEqualTo(Map.of(participant1, -100.0, participant2, -200.0));
     }
 
     @DisplayName("딜러와 참가자 모두 버스트가 안된 경우, 딜러 점수가 21과 가까우면 참가자 수익률은 베팅금액의 -1배다.")
@@ -93,20 +78,10 @@ class BlackJackTest {
     @DisplayName("딜러가 버스트면 참가자의 버스트여부와 관련없이 참가자 수익은 베팅금액의 1배다.")
     @Test
     void participantWinWhenDealerBust() {
-        Participant bustParticipant = new Participant("배키",
-                List.of(new Card(CardShape.SPACE, CardNumber.NINE),
-                        new Card(CardShape.DIAMOND, CardNumber.NINE)),
-                new BettingMoney(100));
-        bustParticipant.addCard(new Card(CardShape.CLOVER, CardNumber.NINE));
-        Participant notBustParticipant = new Participant("켬미",
-                List.of(new Card(CardShape.CLOVER, CardNumber.EIGHT),
-                        new Card(CardShape.HEART, CardNumber.SEVEN)),
-                new BettingMoney(200));
+        Participant bustParticipant = createBustParticipant("배키", new BettingMoney(100));
+        Participant notBustParticipant = createNotBustParticipant("켬미", new BettingMoney(200));
 
-        Dealer bustDealer = new Dealer(new CardDeck(CardDeck.createCards()), () ->
-                List.of(new Card(CardShape.SPACE, CardNumber.KING),
-                        new Card(CardShape.CLOVER, CardNumber.KING)));
-        bustDealer.addCard(new Card(CardShape.SPACE, CardNumber.FIVE));
+        Dealer bustDealer = createBustDealer();
 
         Participants participants = new Participants(List.of(bustParticipant, notBustParticipant));
         BlackJack blackJack = new BlackJack(participants, bustDealer);
@@ -143,17 +118,9 @@ class BlackJackTest {
     @DisplayName("참가자, 딜러 둘다 버스트가 아닐 때, 참가자 카드의 합이 딜러 카드의 합이 동일하면 참가자의 수익은 0이다.")
     @Test
     void findDrawOutcome() {
-        Participant notBustParticipant1 = new Participant("켬미",
-                List.of(new Card(CardShape.SPACE, CardNumber.NINE),
-                        new Card(CardShape.DIAMOND, CardNumber.NINE)),
-                new BettingMoney(100));
-        Participant notBustParticipant2 = new Participant("배키",
-                List.of(new Card(CardShape.SPACE, CardNumber.EIGHT),
-                        new Card(CardShape.DIAMOND, CardNumber.TEN)),
-                new BettingMoney(200));
-        Dealer notBustDealer = new Dealer(new CardDeck(CardDeck.createCards()), () ->
-                List.of(new Card(CardShape.HEART, CardNumber.NINE),
-                        new Card(CardShape.CLOVER, CardNumber.NINE)));
+        Participant notBustParticipant1 = createNotBustParticipant("켬미", new BettingMoney(100));
+        Participant notBustParticipant2 = createNotBustParticipant("배키", new BettingMoney(200));
+        Dealer notBustDealer = createNotBustDealer();
 
         Participants participants = new Participants(List.of(notBustParticipant1, notBustParticipant2));
         BlackJack blackJack = new BlackJack(participants, notBustDealer);
@@ -161,5 +128,33 @@ class BlackJackTest {
         Map<Participant, Double> playerProfit = blackJack.matchParticipantProfit();
         assertThat(playerProfit).isEqualTo(
                 Map.of(notBustParticipant1, 0.0, notBustParticipant2, 0.0));
+    }
+
+    public Participant createBustParticipant(String name, BettingMoney bettingMoney) {
+        Participant bustParticipant = new Participant(name,
+                List.of(new Card(CardShape.SPACE, CardNumber.NINE),
+                        new Card(CardShape.DIAMOND, CardNumber.NINE)), bettingMoney);
+        bustParticipant.addCard(new Card(CardShape.HEART, CardNumber.NINE));
+        return bustParticipant;
+    }
+
+    public Participant createNotBustParticipant(String name, BettingMoney bettingMoney) {
+        return new Participant(name,
+                List.of(new Card(CardShape.SPACE, CardNumber.NINE),
+                        new Card(CardShape.DIAMOND, CardNumber.NINE)), bettingMoney);
+    }
+
+    public Dealer createNotBustDealer() {
+        return new Dealer(new CardDeck(CardDeck.createCards()), () ->
+                List.of(new Card(CardShape.HEART, CardNumber.NINE),
+                        new Card(CardShape.CLOVER, CardNumber.NINE)));
+    }
+
+    public Dealer createBustDealer() {
+        Dealer bustDealer = new Dealer(new CardDeck(CardDeck.createCards()), () ->
+                List.of(new Card(CardShape.SPACE, CardNumber.KING),
+                        new Card(CardShape.CLOVER, CardNumber.KING)));
+        bustDealer.addCard(new Card(CardShape.SPACE, CardNumber.FIVE));
+        return bustDealer;
     }
 }
