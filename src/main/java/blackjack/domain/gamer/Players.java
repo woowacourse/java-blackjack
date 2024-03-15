76b556c78acbb5ac1eb897f94bbc37f2dce92607
PgmJun <chltmdwns96@gmail.com>
2024-03-15 11:19:09 +0900

    refactor: 정적 팩토리 메서드 적용으로 인해 생성자 접근 제어자 private으로 변경

--- src/main/java/blackjack/domain/gamer/Players.java
+++ src/main/java/blackjack/domain/gamer/Players.java
@@ -14,7 +14,7 @@ public class Players {
 
 	private final List<Player> players;
 
-	public Players(final List<Player> players) {
+	private Players(final List<Player> players) {
 		this.players = players;
 	}
 
