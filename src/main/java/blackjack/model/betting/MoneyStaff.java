b8fa92e814e49cb3f57db14141b12fc547aba9d3
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-15 09:57:50 +0900

    feat(MoneyStaff): 수익률 목록을 받아 플레이어들의 수익금을 계산하는 기능 구현

--- src/main/java/blackjack/model/betting/MoneyStaff.java
+++ src/main/java/blackjack/model/betting/MoneyStaff.java
@@ -1,2 +1,27 @@
-package blackjack.model.betting;public class MoneyStaff {
+package blackjack.model.betting;
+
+import blackjack.model.participant.Player;
+import blackjack.model.result.ResultCommand;
+import java.util.List;
+import java.util.Map;
+
+public class MoneyStaff {
+    private final BettingRule bettingRule;
+    private final Map<Player, Money> bettingMoneys;
+
+    MoneyStaff(final BettingRule bettingRule, final Map<Player, Money> bettingMoneys) {
+        this.bettingRule = bettingRule;
+        this.bettingMoneys = bettingMoneys;
+    }
+
+    public List<Money> calculateProfitMoneys(final Map<Player, ResultCommand> playerGameResult) {
+        return playerGameResult.keySet().stream()
+                .map(player -> calculateProfitMoney(player, playerGameResult.get(player)))
+                .toList();
+    }
+
+    public Money calculateProfitMoney(final Player player, final ResultCommand resultCommand) {
+        double profitRate = bettingRule.calculateProfitRate(player, resultCommand);
+        return bettingMoneys.get(player).multiple(profitRate);
+    }
 }
--- src/test/java/blackjack/model/betting/MoneyStaffTest.java
+++ src/test/java/blackjack/model/betting/MoneyStaffTest.java
@@ -1,2 +1,32 @@
-package blackjack.model.betting;public class MoneyStaffTest {
+package blackjack.model.betting;
+
+import static blackjack.model.PlayerFixture.BLACKJACK_PLAYER;
+import static blackjack.model.PlayerFixture.NOT_BLACKJACK_21_PLAYER;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.model.deck.Card;
+import blackjack.model.deck.Score;
+import blackjack.model.deck.Shape;
+import blackjack.model.participant.Dealer;
+import blackjack.model.participant.Hand;
+import blackjack.model.result.ResultCommand;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoneyStaffTest {
+
+    @Test
+    @DisplayName("수익률 목록을 받아 플레이어들의 수익금을 계산한다.")
+    void calculateProfitMoney() {
+        BettingRule bettingRule = new BettingRule(new Dealer(new Hand(List.of(new Card(Shape.DIA, Score.TWO), new Card(Shape.SPADE, Score.TWO)))));
+        MoneyStaff moneyStaff = new MoneyStaff(bettingRule, Map.of(
+                BLACKJACK_PLAYER.getPlayer(), new Money(1_000),
+                NOT_BLACKJACK_21_PLAYER.getPlayer(), new Money(2_000)));
+
+        assertThat(moneyStaff.calculateProfitMoneys(Map.of(
+                BLACKJACK_PLAYER.getPlayer(), ResultCommand.WIN,
+                NOT_BLACKJACK_21_PLAYER.getPlayer(), ResultCommand.DRAW))).isEqualTo(List.of(new Money(1_500), new Money(0)));
+    }
 }
