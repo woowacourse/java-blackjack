c5f3e90b612303556ec25c649ca66e75db1b955e
kunsanglee <gosmdochee2@gmail.com>
2024-03-15 23:52:47 +0900

    refactor: Dealer 리팩토링

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -2,26 +2,47 @@ package blackjack.domain;
 
 import blackjack.domain.state.InitialState;
 import blackjack.domain.state.State;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dealer extends Participant {
+
+    private static final int STAND_LIMIT_VALUE = 17;
 
     public Dealer(Name name, State state) {
         super(name, state);
     }
 
-    public static Dealer createInitialStateDealer(Name name) {
-        return new Dealer(name, new InitialState());
+    public static Dealer createInitialStateDealer() {
+        return new Dealer(new Name("딜러"), new InitialState());
     }
 
     @Override
     public Dealer draw(Deck deck) {
-        State newState = state().draw(deck);
-        return new Dealer(name(), newState);
+        State newState = getState().draw(deck);
+        return new Dealer(getName(), newState);
     }
 
     @Override
     public Dealer stand() {
-        State newState = state().stand();
-        return new Dealer(name(), newState);
+        State newState = getState().stand();
+        return new Dealer(getName(), newState);
+    }
+
+    @Override
+    public boolean canDraw() {
+        State state = getState();
+        Score score = state.calculateHand();
+        return !state.isFinished() && isDealerScoreUnderLimit(score);
+    }
+
+    private boolean isDealerScoreUnderLimit(Score score) {
+        return Score.from(STAND_LIMIT_VALUE).isBiggerThanOrEqual(score);
+    }
+
+    public List<Card> getVisibleCards() {
+        List<Card> cards = getCards();
+        ArrayList<Card> copiedCards = new ArrayList<>(cards);
+        return copiedCards.subList(0, copiedCards.size() - 1);
     }
 }
--- src/test/java/blackjack/domain/DealerTest.java
+++ src/test/java/blackjack/domain/DealerTest.java
@@ -12,7 +12,7 @@ public class DealerTest {
     @DisplayName("딜러는 생성시 초기 상태를 갖는다")
     @Test
     public void createInitialStateDealer() {
-        Dealer dealer = Dealer.createInitialStateDealer(new Name("딜러"));
+        Dealer dealer = Dealer.createInitialStateDealer();
 
         assertThat(dealer.getState()).isInstanceOf(InitialState.class);
     }
@@ -20,7 +20,7 @@ public class DealerTest {
     @DisplayName("딜러는 카드를 뽑으면 새로운 상태를 가진 딜러가 반환된다")
     @Test
     public void draw() {
-        Dealer dealer = Dealer.createInitialStateDealer(new Name("딜러"));
+        Dealer dealer = Dealer.createInitialStateDealer();
 
         Dealer newDealer = dealer.draw(Deck.of(new CardFactory(), cards -> cards));
     }
@@ -28,7 +28,7 @@ public class DealerTest {
     @DisplayName("딜러의 상태가 종료되지 않았다면 true를 반환한다")
     @Test
     public void canDrawTrue() {
-        Dealer dealer = Dealer.createInitialStateDealer(new Name("딜러"));
+        Dealer dealer = Dealer.createInitialStateDealer();
 
         assertThat(dealer.canDraw()).isTrue();
     }
@@ -36,7 +36,7 @@ public class DealerTest {
     @DisplayName("딜러의 상태가 종료되었다면 false를 반환한다")
     @Test
     public void canDrawFalse() {
-        Dealer dealer = Dealer.createInitialStateDealer(new Name("딜러"));
+        Dealer dealer = Dealer.createInitialStateDealer();
         Deck deck = Deck.of(new CardFactory(), cards -> cards);
         dealer = dealer.draw(deck); // 2장 초기화 KING, QUEEN -> 20
 
@@ -48,7 +48,7 @@ public class DealerTest {
     @DisplayName("딜러가 스탠드를 하면 새로운 상태를 가진 딜러를 반환한다")
     @Test
     public void stand() {
-        Dealer dealer = Dealer.createInitialStateDealer(new Name("딜러"));
+        Dealer dealer = Dealer.createInitialStateDealer();
         Deck deck = Deck.of(new CardFactory(), cards -> cards);
         dealer = dealer.draw(deck);
 
@@ -60,7 +60,7 @@ public class DealerTest {
     @DisplayName("딜러의 스코어를 계산한다")
     @Test
     public void calculate() {
-        Dealer dealer = Dealer.createInitialStateDealer(new Name("딜러"));
+        Dealer dealer = Dealer.createInitialStateDealer();
         Deck deck = Deck.of(new CardFactory(), cards -> cards);
         dealer = dealer.draw(deck);
 
