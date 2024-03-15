0a94c06d21058aefe770d6d1862d65f032a5a30c
nayonsoso <panda0329@naver.com>
2024-03-15 14:50:32 +0900

    feat: 배팅 금액 검증 기능 구현

--- src/main/java/domain/Bet.java
+++ src/main/java/domain/Bet.java
@@ -2,8 +2,9 @@ package domain;
 
 public class Bet {
 
-    public static final int MIN_BET = 0;
-    public static final String BET_RANGE_MESSAGE = String.format("배팅 금액은 %d 보다 커야합니다.", MIN_BET);
+    static final int MIN_BET = 1;
+    static final String BET_RANGE_MESSAGE = String.format("배팅 금액은 %d 보다 커야합니다.", MIN_BET);
+
     private final int amount;
 
     public Bet(int amount) {
@@ -12,7 +13,7 @@ public class Bet {
     }
 
     void validateRange(int amount) {
-        if (amount <= MIN_BET) {
+        if (amount < MIN_BET) {
             throw new IllegalArgumentException(BET_RANGE_MESSAGE);
         }
     }
