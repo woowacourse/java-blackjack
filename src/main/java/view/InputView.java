a96bf41639de3a1d44cab0ff5de3c9bce7f73723
DobbyMac <kimdobby@g.skku.edu>
2024-03-15 18:23:37 +0900

    feat(InputView): 플레이어 배팅 금액 입력 및 검증 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -18,6 +18,13 @@ public class InputView {
         return scanner.nextLine();
     }
 
+    public static List<String> inputNames() {
+        String initialInput = validateContainsDealerName(input("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"));
+        return Arrays.stream(initialInput.split(INPUT_PARSE_DELIMITER))
+                .map(String::trim)
+                .toList();
+    }
+
     public static boolean inputPlayerHitChoice(String inputRequestMessage) {
         String initialInput = input(inputRequestMessage + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
         return validateChoiceInputReturnDecision(initialInput);
@@ -33,11 +40,18 @@ public class InputView {
         throw new IllegalArgumentException("[ERROR] y 혹은 n만 입력 가능합니다.");
     }
 
-    public static List<String> inputNames() {
-        String initialInput = validateContainsDealerName(input("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"));
-        return Arrays.stream(initialInput.split(INPUT_PARSE_DELIMITER))
-                .map(String::trim)
-                .toList();
+    public static int inputBettingAmount(String targetName) {
+        System.out.println();
+        String initialInput = input(targetName + "의 배팅 금액은?");
+        validateBettingAmount(initialInput);
+        return Integer.parseInt(initialInput);
+    }
+
+    private static void validateBettingAmount(String initialInput) {
+        if (initialInput.matches("\\d+")) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 숫자만 입력가능합니다.");
     }
 
     private static String validateContainsDealerName(String inputName) {
