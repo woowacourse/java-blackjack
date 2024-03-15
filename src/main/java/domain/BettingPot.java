816ccdf9e279516ec974bda909c37530e8b8a0d6
nayonsoso <panda0329@naver.com>
2024-03-15 13:28:01 +0900

    feat: 배팅 금액 정산 기능 구현

--- src/main/java/domain/BettingPot.java
+++ src/main/java/domain/BettingPot.java
@@ -10,4 +10,24 @@ public class BettingPot {
     public void collect(Player player, Bet bet) {
         betRecord.put(player, bet);
     }
+
+    public Map<Player, Integer> settle(Map<Player, Result> playerResults) {
+        Map<Player, Integer> settlement = new LinkedHashMap<>();
+
+        for (Map.Entry<Player, Result> playerResult : playerResults.entrySet()) {
+            Player player = playerResult.getKey();
+            int amount = betRecord.get(player).getAmount();
+
+            if (playerResult.getValue() == Result.DEALER_WIN) {
+                amount *= -1;
+            }
+            if (playerResult.getValue() == Result.PLAYER_WIN) {
+                amount *= 2;
+            }
+
+            settlement.put(player, amount);
+        }
+
+        return settlement;
+    }
 }
