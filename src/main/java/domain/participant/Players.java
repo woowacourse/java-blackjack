4cb9770c68e7c6729652fbfd72ad04c11f805ed5
yoonjuho <dbswn990@naver.com>
2024-03-15 21:25:29 +0900

    refactor(Names): 플레이어의 수 검증 역할 위임

--- src/main/java/domain/participant/Players.java
+++ src/main/java/domain/participant/Players.java
@@ -4,22 +4,11 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class Players {
-
-    private static final int MIN_SIZE = 1;
-    private static final int MAX_SIZE = 8;
-
     private final List<Player> players;
 
     public Players(final List<Player> players) {
-        validateSize(players);
-        this.players = List.copyOf(players);
-    }
 
-    private void validateSize(final List<Player> players) {
-        if (players.isEmpty() || players.size() > MAX_SIZE) {
-            throw new IllegalArgumentException("플레이어의 수는 최소 %d명 최대 %d명입니다 : 현재 %d명"
-                    .formatted(MIN_SIZE, MAX_SIZE, players.size()));
-        }
+        this.players = List.copyOf(players);
     }
 
     public void forEach(final Consumer<Player> action) {
--- src/test/java/domain/name/NamesTest.java
+++ src/test/java/domain/name/NamesTest.java
@@ -16,4 +16,13 @@ class NamesTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("중복된 이름을 가진 플레이어는 존재할 수 없습니다.");
     }
+
+    @DisplayName("플레이어의 수가 최소 1명 최대 8명으로 이루어져 있지 않은 경우 예외가 발생한다.")
+    @Test
+    void validateSize() {
+        List<Name> players = List.of();
+        assertThatThrownBy(() -> new Names(players))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("플레이어의 수는 최소 1명 최대 8명입니다 : 현재 %d명".formatted(0));
+    }
 }
