17329254716063df16a68e69dfe857fd48498ed4
fram1998 <koust6u@pusan.ac.kr>
2024-03-15 14:05:36 +0900

    fix: 배당 정책 생성 로직 하나로 합치도록 수정
    불필요한 배당 정책 생성 로직 하나로 합치도록 코드 수정함

--- src/main/java/model/casino/DividendPolicyFactory.java
+++ src/main/java/model/casino/DividendPolicyFactory.java
@@ -2,16 +2,21 @@ package model.casino;
 
 import model.money.DividendPolicy;
 import model.participant.Dealer;
+import model.participant.Participant;
 import model.participant.Player;
 
 public class DividendPolicyFactory {
-    private final Dealer dealer;
 
-    public DividendPolicyFactory(Dealer dealer) {
-        this.dealer = dealer;
+    public static DividendPolicy findPolicy(Participant dealer, Participant player) {
+        DividendPolicy policyInProceed = findPolicyInProceed(player);
+        if (policyInProceed != DividendPolicy.UNCERTAIN) {
+            return policyInProceed;
+        }
+        return findPolicyAfterProceed((Dealer)dealer, (Player)player);
     }
 
-    public DividendPolicy findPolicyInProceed(Player player) {
+    private static DividendPolicy findPolicyInProceed(Participant participant) {
+        Player player = (Player) participant;
         if (player.isInitBlackjack()) {
             return DividendPolicy.INIT_BLACKJACK;
         }
@@ -21,9 +26,9 @@ public class DividendPolicyFactory {
         return DividendPolicy.UNCERTAIN;
     }
 
-    public DividendPolicy findPolicyAfterProceed(Player player) {
+    private static DividendPolicy findPolicyAfterProceed(Dealer dealer, Player player) {
         if (!dealer.isBustHand() && !player.isBustHand()) {
-            return getDividendPolicyHasNoBust(player);
+            return getDividendPolicyHasNoBust(dealer, player);
         }
         if (dealer.isBustHand() && player.isBustHand()) {
             return DividendPolicy.NORMAL_DRAW;
@@ -34,7 +39,7 @@ public class DividendPolicyFactory {
         throw new IllegalStateException();
     }
 
-    private DividendPolicy getDividendPolicyHasNoBust(Player player) {
+    private static DividendPolicy getDividendPolicyHasNoBust(Dealer dealer, Player player) {
         if (dealer.getHand() > player.getHand()) {
             return DividendPolicy.NORMAL_LOSE;
         }
@@ -43,6 +48,4 @@ public class DividendPolicyFactory {
         }
         return DividendPolicy.NORMAL_WIN;
     }
-
-
 }
--- src/test/java/model/casino/DividendPolicyFactoryTest.java
+++ src/test/java/model/casino/DividendPolicyFactoryTest.java
@@ -21,133 +21,112 @@ class DividendPolicyFactoryTest {
 
         @Test
         @DisplayName("플레이어는 초기 뽑은 두 장의 카드가 블랙잭일 경우 DividendPolicy.BLACKJACK을 반환한다.")
-        void findPolicyInProceed_ShouldGenerateInitBlackjackPolicy_WhenPlayerHasInitBlackjack () {
+        void findPolicy_ShouldGenerateInitBlackjackPolicy_WhenPlayerHasInitBlackjack() {
             Dealer dealer = new Dealer();
-            DividendPolicyFactory factory = new DividendPolicyFactory(dealer);
 
             Player player = new Player(new Name("프람"));
             player.hitCard(Card.from(Number.ACE, Emblem.HEART));
             player.hitCard(Card.from(Number.TEN, Emblem.CLUB));
 
-            assertThat(factory.findPolicyInProceed(player))
+            assertThat(DividendPolicyFactory.findPolicy(dealer, player))
                     .isSameAs(DividendPolicy.INIT_BLACKJACK);
         }
 
 
         @Test
         @DisplayName("플레이어가 게임 중 버스트 된다면 DividendPolicy.NORMAL_LOSE를 반환한다.")
-        void findPolicyInProceed_ShouldGenerateNormalLosePolicy_WhenPlayerIsBust() {
+        void findPolicy_ShouldGenerateNormalLosePolicy_WhenPlayerIsBust() {
             Dealer dealer = new Dealer();
-            DividendPolicyFactory factory = new DividendPolicyFactory(dealer);
-
             Player player = new Player(new Name("프람"));
             player.hitCard(Card.from(Number.TEN, Emblem.HEART));
             player.hitCard(Card.from(Number.TEN, Emblem.CLUB));
             player.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
 
-            assertThat(factory.findPolicyInProceed(player))
+            assertThat(DividendPolicyFactory.findPolicy(dealer, player))
                     .isSameAs(DividendPolicy.NORMAL_LOSE);
         }
 
-        @Test
-        @DisplayName("플레이어는 게임중 버스트 되지 않거나 초기 두장이 블랙잭이 아닐경우 Dividend.UNCERTAIN을 반환한다.")
-        void findPolicyInProceed_ShouldGenerateUncertain_WhenPlayerIsNotBustAndInitBlackjack() {
-            Dealer dealer = new Dealer();
-            DividendPolicyFactory factory = new DividendPolicyFactory(dealer);
 
-            Player player = new Player(new Name("프람"));
-            player.hitCard(Card.from(Number.TEN, Emblem.HEART));
-            player.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
+        @Nested
+        @DisplayName("게임 진행 종료 후에 판별되는 승패에 따른 배당정책을 만든다.")
+        class findPolicy_ShouldGenerateDividendPolicy_WhenAfterGameProceed {
+            @Test
+            @DisplayName("딜러와 플레이어가 모두 버스트가 아닌 경우에서,플레이어 핸드 > 딜러 핸드일 경우 플레이어 NORMAL_WIN")
+            void findPolicyAfterProceed_ShouldGenerateDividendPolicy_WhenBothNotBustAndPlayerGreaterThenDealer() {
+                Dealer dealer = new Dealer();
+                dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
+                dealer.hitCard(Card.from(Number.TWO, Emblem.DIAMOND));
 
-            assertThat(factory.findPolicyInProceed(player))
-                    .isSameAs(DividendPolicy.UNCERTAIN);
+                Player player = new Player(new Name("프람"));
+                player.hitCard(Card.from(Number.TEN, Emblem.HEART));
+                player.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
+
+                assertThat(DividendPolicyFactory.findPolicy(dealer, player))
+                        .isSameAs(DividendPolicy.NORMAL_WIN);
+            }
+
+            @Test
+            @DisplayName("딜러와 플레이어가 모두 버스트가 아닌 경우에서,플레이어 핸드 < 딜러 핸드일 경우 플레이어 NORMAL_LOSE")
+            void findPolicy_ShouldGenerateDividendPolicy_WhenBothNotBustAndPlayerLessThenDealer() {
+                Dealer dealer = new Dealer();
+                dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
+                dealer.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
+
+                Player player = new Player(new Name("프람"));
+                player.hitCard(Card.from(Number.TEN, Emblem.HEART));
+                player.hitCard(Card.from(Number.TWO, Emblem.DIAMOND));
+
+                assertThat(DividendPolicyFactory.findPolicy(dealer, player))
+                        .isSameAs(DividendPolicy.NORMAL_LOSE);
+            }
+
+            @Test
+            @DisplayName("딜러와 플레이어가 모두 버스트가 아닌 경우에서,플레이어 핸드 = 딜러 핸드일 경우 플레이어 NORMAL_WIN")
+            void findPolicy_ShouldGenerateDividendPolicy_WhenBothNotBustAndPlayerEqualThenDealer() {
+                Dealer dealer = new Dealer();
+                dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
+                dealer.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
+
+                Player player = new Player(new Name("프람"));
+                player.hitCard(Card.from(Number.TEN, Emblem.HEART));
+                player.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
+
+                assertThat(DividendPolicyFactory.findPolicy(dealer, player))
+                        .isSameAs(DividendPolicy.NORMAL_DRAW);
+            }
+
+            @Test
+            @DisplayName("딜러는 버스트이고 플레이어는 아닐 경우, DividendPolicy.NORMAL_WIN을 반환한다.")
+            void findPolicy_ShouldGenerateDividendPolicy_WhenDealerBustPayerNotBust() {
+                Dealer dealer = new Dealer();
+                dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
+                dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
+                dealer.hitCard(Card.from(Number.THREE, Emblem.CLUB));
+
+                Player player = new Player(new Name("프람"));
+                player.hitCard(Card.from(Number.TEN, Emblem.HEART));
+                player.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
+
+                assertThat(DividendPolicyFactory.findPolicy(dealer, player))
+                        .isSameAs(DividendPolicy.NORMAL_WIN);
+            }
+
+            @Test
+            @DisplayName("플레이어의 경우 초기 블랙잭이 아닌 블랙잭일 경우 그리고 딜러 역시 블랙잭일 경우 Dividend.NORMAL_DRAW를 반환한다.")
+            void findPolicy_ShouldGenerateDividendPolicy_WhenBothBlackjackExceptInitBlackjack() {
+                Dealer dealer = new Dealer();
+                dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
+                dealer.hitCard(Card.from(Number.ACE, Emblem.DIAMOND));
+
+                Player player = new Player(new Name("프람"));
+                player.hitCard(Card.from(Number.TEN, Emblem.HEART));
+                player.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
+                player.hitCard(Card.from(Number.EIGHT, Emblem.CLUB));
+
+                assertThat(DividendPolicyFactory.findPolicy(dealer, player))
+                        .isSameAs(DividendPolicy.NORMAL_DRAW);
+            }
         }
+
     }
-
-    @Nested
-    @DisplayName("게임 진행 종료 후에 판별되는 승패에 따른 배당정책을 만든다.")
-    class findPolicyAfterProceed_ShouldGenerateDividendPolicy_WhenAfterGameProceed {
-        @Test
-        @DisplayName("딜러와 플레이어가 모두 버스트가 아닌 경우에서,플레이어 핸드 > 딜러 핸드일 경우 플레이어 NORMAL_WIN")
-        void findPolicyAfterProceed_ShouldGenerateDividendPolicy_WhenBothNotBustAndPlayerGreaterThenDealer() {
-            Dealer dealer = new Dealer();
-            dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
-            dealer.hitCard(Card.from(Number.TWO, Emblem.DIAMOND));
-            DividendPolicyFactory factory = new DividendPolicyFactory(dealer);
-
-            Player player = new Player(new Name("프람"));
-            player.hitCard(Card.from(Number.TEN, Emblem.HEART));
-            player.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
-
-            assertThat(factory.findPolicyAfterProceed(player))
-                    .isSameAs(DividendPolicy.NORMAL_WIN);
-        }
-
-        @Test
-        @DisplayName("딜러와 플레이어가 모두 버스트가 아닌 경우에서,플레이어 핸드 < 딜러 핸드일 경우 플레이어 NORMAL_LOSE")
-        void findPolicyAfterProceed_ShouldGenerateDividendPolicy_WhenBothNotBustAndPlayerLessThenDealer() {
-            Dealer dealer = new Dealer();
-            dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
-            dealer.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
-            DividendPolicyFactory factory = new DividendPolicyFactory(dealer);
-
-            Player player = new Player(new Name("프람"));
-            player.hitCard(Card.from(Number.TEN, Emblem.HEART));
-            player.hitCard(Card.from(Number.TWO, Emblem.DIAMOND));
-
-            assertThat(factory.findPolicyAfterProceed(player))
-                    .isSameAs(DividendPolicy.NORMAL_LOSE);
-        }
-        @Test
-        @DisplayName("딜러와 플레이어가 모두 버스트가 아닌 경우에서,플레이어 핸드 = 딜러 핸드일 경우 플레이어 NORMAL_WIN")
-        void findPolicyAfterProceed_ShouldGenerateDividendPolicy_WhenBothNotBustAndPlayerEqualThenDealer() {
-            Dealer dealer = new Dealer();
-            dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
-            dealer.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
-            DividendPolicyFactory factory = new DividendPolicyFactory(dealer);
-
-            Player player = new Player(new Name("프람"));
-            player.hitCard(Card.from(Number.TEN, Emblem.HEART));
-            player.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
-
-            assertThat(factory.findPolicyAfterProceed(player))
-                    .isSameAs(DividendPolicy.NORMAL_DRAW);
-        }
-
-        @Test
-        @DisplayName("딜러는 버스트이고 플레이어는 아닐 경우, DividendPolicy.NORMAL_WIN을 반환한다.")
-        void findPolicyAfterProceed_ShouldGenerateDividendPolicy_WhenDealerBustPayerNotBust() {
-            Dealer dealer = new Dealer();
-            dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
-            dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
-            dealer.hitCard(Card.from(Number.THREE, Emblem.CLUB));
-            DividendPolicyFactory factory = new DividendPolicyFactory(dealer);
-
-            Player player = new Player(new Name("프람"));
-            player.hitCard(Card.from(Number.TEN, Emblem.HEART));
-            player.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
-
-            assertThat(factory.findPolicyAfterProceed(player))
-                    .isSameAs(DividendPolicy.NORMAL_WIN);
-        }
-
-        @Test
-        @DisplayName("플레이어의 경우 초기 블랙잭이 아닌 블랙잭일 경우 그리고 딜러 역시 블랙잭일 경우 Dividend.NORMAL_DRAW를 반환한다.")
-        void findPolicyAfterProceed_ShouldGenerateDividendPolicy_WhenBothBlackjackExceptInitBlackjack() {
-            Dealer dealer = new Dealer();
-            dealer.hitCard(Card.from(Number.TEN, Emblem.DIAMOND));
-            dealer.hitCard(Card.from(Number.ACE, Emblem.DIAMOND));
-            DividendPolicyFactory factory = new DividendPolicyFactory(dealer);
-
-            Player player = new Player(new Name("프람"));
-            player.hitCard(Card.from(Number.TEN, Emblem.HEART));
-            player.hitCard(Card.from(Number.THREE, Emblem.DIAMOND));
-            player.hitCard(Card.from(Number.EIGHT, Emblem.CLUB));
-
-            assertThat(factory.findPolicyAfterProceed(player))
-                    .isSameAs(DividendPolicy.NORMAL_DRAW);
-        }
-    }
-
-
 }
