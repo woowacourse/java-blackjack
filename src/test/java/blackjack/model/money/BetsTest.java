16acf7a2f1196ec341cfaccd1eab6a84cd4961ec
Minjoo <m0522inju@gmail.com>
2024-03-15 16:00:35 +0900

    refactor(BetsTest): TestFixture 분리 및 ParameterizedTest 개별 테스트로 분리

--- src/test/java/blackjack/model/money/BetsTest.java
+++ src/test/java/blackjack/model/money/BetsTest.java
@@ -1,5 +1,9 @@
 package blackjack.model.money;
 
+import static blackjack.model.result.ResultCommand.BLACK_JACK_WIN;
+import static blackjack.model.result.ResultCommand.DRAW;
+import static blackjack.model.result.ResultCommand.LOSE;
+import static blackjack.model.result.ResultCommand.WIN;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
 
@@ -7,44 +11,128 @@ import blackjack.model.participant.Player;
 import blackjack.model.result.ResultCommand;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 public class BetsTest {
+    @Nested
+    @DisplayName("플레이어 수익률 계산 시")
+    class PlayerProfit {
+
+        @Test
+        @DisplayName("승리하면 베팅한 금액을 모두 돌려받는다.")
+        void calculateProfitWhenWin() {
+            Player player = Fixtures.createPlayer();
+            Bets bets = Fixtures.createBets();
+            bets.addBet(player, 10_000);
+            Map<Player, ResultCommand> playerResultCommand = Fixtures.createPlayerResultCommand(player, WIN);
+
+            assertThat(bets.calculatePlayersProfit(playerResultCommand))
+                    .contains(entry(player, new Profit(10_000)));
+        }
+
+        @Test
+        @DisplayName("패배하면 베팅한 금액을 모두 잃는다.")
+        void calculateProfitWhenLose() {
+            Player player = Fixtures.createPlayer();
+            Bets bets = Fixtures.createBets();
+            bets.addBet(player, 10_000);
+            Map<Player, ResultCommand> playerResultCommand = Fixtures.createPlayerResultCommand(player, LOSE);
+
+            assertThat(bets.calculatePlayersProfit(playerResultCommand))
+                    .contains(entry(player, new Profit(-10_000)));
+        }
+
+        @Test
+        @DisplayName("무승부이면 베팅한 금액을 모두 돌려받는다.")
+        void calculateProfitWhenDraw() {
+            Player player = Fixtures.createPlayer();
+            Bets bets = Fixtures.createBets();
+            bets.addBet(player, 10_000);
+            Map<Player, ResultCommand> playerResultCommand = Fixtures.createPlayerResultCommand(player, DRAW);
+
+            assertThat(bets.calculatePlayersProfit(playerResultCommand))
+                    .contains(entry(player, new Profit(10_000)));
+        }
+
+        @Test
+        @DisplayName("블랙잭으로 승리하면 베팅한 금액의 1.5배를 받는다.")
+        void calculateProfitWhenBlackJackWin() {
+            Player player = Fixtures.createPlayer();
+            Bets bets = Fixtures.createBets();
+            bets.addBet(player, 10_000);
+            Map<Player, ResultCommand> playerResultCommand = Fixtures.createPlayerResultCommand(player, BLACK_JACK_WIN);
+
+            assertThat(bets.calculatePlayersProfit(playerResultCommand))
+                    .contains(entry(player, new Profit(15_000)));
+        }
+    }
 
     @Nested
-    @DisplayName("수익률 계산")
-    class calculateProfit {
-        private Bets bets;
-        private Player player;
+    @DisplayName("딜러 수익률 계산 시")
+    class DealerProfit {
 
+        @Test
+        @DisplayName("플레이어가 승리하면 베팅 금액을 모두 플레이어에게 준다.")
+        void calculateProfitWhenPlayerWin() {
+            Player player = Fixtures.createPlayer();
+            Bets bets = Fixtures.createBets();
+            bets.addBet(player, 10_000);
+            Map<Player, ResultCommand> playerResultCommand = Fixtures.createPlayerResultCommand(player, WIN);
 
-        @BeforeEach
-        void init() {
-            bets = new Bets();
-            player = new Player("리브");
+            assertThat(bets.calculateDealerProfit(playerResultCommand))
+                    .isEqualTo(new Profit(-10_000));
         }
 
-        @ParameterizedTest
-        @CsvSource(value = {"WIN,10_000", "LOSE,-10_000", "DRAW,10_000", "BLACK_JACK_WIN,15_000"})
-        @DisplayName("플레이어의 수익을 계산한다.")
-        void calculatePlayerProfit(ResultCommand resultCommand, int expected) {
+        @Test
+        @DisplayName("플레이어가 패배하면 플레이어의 베팅 금액을 받는다.")
+        void calculateProfitWhenPlayerLose() {
+            Player player = Fixtures.createPlayer();
+            Bets bets = Fixtures.createBets();
             bets.addBet(player, 10_000);
-            Map<Player, ResultCommand> playerResultCommand = new LinkedHashMap<>(Map.of(player, resultCommand));
-            assertThat(bets.calculatePlayersProfit(playerResultCommand))
-                    .contains(entry(player, new Profit(expected)));
+            Map<Player, ResultCommand> playerResultCommand = Fixtures.createPlayerResultCommand(player, LOSE);
+
+            assertThat(bets.calculateDealerProfit(playerResultCommand))
+                    .isEqualTo(new Profit(10_000));
         }
 
-        @ParameterizedTest
-        @CsvSource(value = {"WIN,-10_000", "LOSE,10_000", "DRAW,-10_000", "BLACK_JACK_WIN,-15_000"})
-        @DisplayName("딜러의 수익을 계산한다.")
-        void calculateDealerProfit(ResultCommand resultCommand, int expected) {
+        @Test
+        @DisplayName("무승부이면 베팅 금액을 플레이어에게 모두 준다.")
+        void calculateProfitWhenDraw() {
+            Player player = Fixtures.createPlayer();
+            Bets bets = Fixtures.createBets();
             bets.addBet(player, 10_000);
-            Map<Player, ResultCommand> playerResultCommand = new LinkedHashMap<>(Map.of(player, resultCommand));
-            assertThat(bets.calculateDealerProfit(playerResultCommand)).isEqualTo(new Profit(expected));
+            Map<Player, ResultCommand> playerResultCommand = Fixtures.createPlayerResultCommand(player, DRAW);
+
+            assertThat(bets.calculateDealerProfit(playerResultCommand))
+                    .isEqualTo(new Profit(-10_000));
+        }
+
+        @Test
+        @DisplayName("플레이어가 블랙잭으로 승리하면 플레이어 베팅 금액의 1.5배를 플레이어에게 준다.")
+        void calculateProfitWhenPlayerBlackJackWin() {
+            Player player = Fixtures.createPlayer();
+            Bets bets = Fixtures.createBets();
+            bets.addBet(player, 10_000);
+            Map<Player, ResultCommand> playerResultCommand = Fixtures.createPlayerResultCommand(player, BLACK_JACK_WIN);
+
+            assertThat(bets.calculateDealerProfit(playerResultCommand))
+                    .isEqualTo(new Profit(-15_000));
+        }
+    }
+
+    static class Fixtures {
+        static Bets createBets() {
+            return new Bets();
+        }
+
+        static Player createPlayer() {
+            return new Player("리브");
+        }
+
+        static Map<Player, ResultCommand> createPlayerResultCommand(Player player, ResultCommand resultCommand) {
+            return new LinkedHashMap<>(Map.of(player, resultCommand));
         }
     }
 }
