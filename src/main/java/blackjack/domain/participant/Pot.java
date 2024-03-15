b7f9562f1e82921defc6e66892ef0f0ed4ad7f64
masonkimseoul <masonkimseoul@gmail.com>
2024-03-15 09:11:20 +0900

    feat: Pot 검증 로직 추가

--- src/main/java/blackjack/domain/participant/Pot.java
+++ src/main/java/blackjack/domain/participant/Pot.java
@@ -1,10 +1,31 @@
 package blackjack.domain.participant;
 
+import blackjack.exception.BettingAmountFormatException;
+import blackjack.exception.BettingAmountNonPositiveException;
+
 public class Pot {
+    private static final int MIN_BETTING_AMOUNT = 1;
+
     private final int amount;
 
     public Pot(String amount) {
-        this.amount = Integer.parseInt(amount);
+        int parsedAmount = parseInteger(amount);
+        validateNonPositiveAmount(parsedAmount);
+        this.amount = parsedAmount;
+    }
+
+    private int parseInteger(String amount) {
+        try {
+            return Integer.parseInt(amount);
+        } catch(NumberFormatException e) {
+            throw new BettingAmountFormatException();
+        }
+    }
+
+    private void validateNonPositiveAmount(int amount) {
+        if (amount < MIN_BETTING_AMOUNT) {
+            throw new BettingAmountNonPositiveException();
+        }
     }
 
     public int getAmount() {
--- src/test/java/blackjack/domain/participant/PotTest.java
+++ src/test/java/blackjack/domain/participant/PotTest.java
@@ -3,6 +3,7 @@ package blackjack.domain.participant;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import blackjack.exception.BettingAmountFormatException;
+import blackjack.exception.BettingAmountNonPositiveException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,10 +11,17 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class PotTest {
     @DisplayName("베팅 금액으로 정수가 아닌 값이 입력되면 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"", "!", "3만원"})
+    @ValueSource(strings = {"", "!", "3만원", "0.0001"})
     void invalidPotAmountNumberFormat(String amount) {
         assertThatThrownBy(() -> new Pot(amount))
                 .isInstanceOf(BettingAmountFormatException.class);
     }
 
+    @DisplayName("베팅 금액으로 양의 정수가 아닌 값이 입력되면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1000", "-1"})
+    void invalidPotAmountNonPositive(String amount) {
+        assertThatThrownBy(() -> new Pot(amount))
+                .isInstanceOf(BettingAmountNonPositiveException.class);
+    }
 }
