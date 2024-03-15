e49ede703eb0e71882a1b89103cf871f3773263a
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-03-15 17:10:12 +0900

    style : 사용하지 않는 코드 제거

--- src/main/java/model/Outcome.java
+++ src/main/java/model/Outcome.java
@@ -1,22 +1,8 @@
 package model;
 
 public enum Outcome {
-    WIN {
-        public Outcome reverse() {
-            return Outcome.LOSE;
-        }
-    },
-    LOSE {
-        public Outcome reverse() {
-            return Outcome.WIN;
-        }
-    },
-    DRAW {
-        public Outcome reverse() {
-            return Outcome.DRAW;
-        }
-    },
+    WIN,
+    LOSE,
+    DRAW
     ;
-
-    public abstract Outcome reverse();
 }
