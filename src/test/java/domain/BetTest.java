1bc9addae4ce8140fb7ba3ad5a0a25c7586d1bd9
nayonsoso <panda0329@naver.com>
2024-03-15 14:50:32 +0900

    feat: 배팅 금액 검증 기능 구현

---
+++ src/test/java/domain/BetTest.java
@@ -0,0 +1,25 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static domain.Bet.BET_RANGE_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+class BetTest {
+
+    @DisplayName("최소 금액 이상만 허용한다.")
+    @Test
+    void validateRangeSuccess() {
+        assertThatCode(() -> new Bet(Bet.MIN_BET))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("최소 금액보다 작은 경우 예외가 발생한다.")
+    @Test
+    void validateRangeFail() {
+        assertThatCode(() -> new Bet(Bet.MIN_BET - 1))
+                .hasMessage(BET_RANGE_MESSAGE)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
