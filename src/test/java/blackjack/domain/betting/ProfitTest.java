100a87da1ea0b65f41b95b630a1b75db2b952b5a
3juhwan <13selfesteem91@naver.com>
2024-03-15 16:36:07 +0900

    test: Profit 테스트 추가

--- src/test/java/blackjack/domain/betting/ProfitTest.java
+++ src/test/java/blackjack/domain/betting/ProfitTest.java
@@ -14,4 +14,22 @@ public class ProfitTest {
 
         assertThat(profit).isEqualTo(new Profit(1000));
     }
+
+    @Test
+    void 진_경우에_수익률은_베팅_금액의_마이너스_값이다() {
+        Money money = new Money(1000);
+        WinningStatus status = WinningStatus.LOSE;
+        Profit profit = Profit.of(money, status);
+
+        assertThat(profit).isEqualTo(new Profit(-1000));
+    }
+
+    @Test
+    void 비긴_경우에_수익률은_0이다() {
+        Money money = new Money(1000);
+        WinningStatus status = WinningStatus.PUSH;
+        Profit profit = Profit.of(money, status);
+
+        assertThat(profit).isEqualTo(new Profit(0));
+    }
 }
