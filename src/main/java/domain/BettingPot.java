d9e15f03d322add3713920b6cf4c4c91fbd6f275
nayonsoso <panda0329@naver.com>
2024-03-15 21:15:07 +0900

    refactor: 함수 분리 및 이름 변경

--- src/main/java/domain/BettingPot.java
+++ src/main/java/domain/BettingPot.java
@@ -7,35 +7,38 @@ public class BettingPot {
 
     private final Map<Player, Bet> betRecord = new LinkedHashMap<>();
 
-    public void collect(Player player, Bet bet) {
+    public void put(Player player, Bet bet) {
         betRecord.put(player, bet);
     }
 
-    public Map<Player, Integer> settlePlayer(Map<Player, Result> playerResults) {
+    public Map<Player, Integer> settlePlayerBet(Map<Player, PlayerGameResult> playerResults) {
         Map<Player, Integer> settlement = new LinkedHashMap<>();
-        for (Map.Entry<Player, Result> playerResult : playerResults.entrySet()) {
+
+        for (Map.Entry<Player, PlayerGameResult> playerResult : playerResults.entrySet()) {
             Player player = playerResult.getKey();
-            int amount = getPlayerBetAmount(player);
-            amount = calculateAmount(playerResult.getValue(), amount);
-            settlement.put(player, amount);
+            PlayerGameResult playerGameResult = playerResult.getValue();
+            int playerSettlement = settleAccordingToResult(player, playerGameResult);
+            settlement.put(player, playerSettlement);
         }
 
         return settlement;
     }
 
-    public int settleDealer(Map<Player, Result> playerResults) {
+    public int settleDealerBet(Map<Player, PlayerGameResult> playerResults) {
         int allPlayersWinning = 0;
-        for (Map.Entry<Player, Result> playerResult : playerResults.entrySet()) {
+
+        for (Map.Entry<Player, PlayerGameResult> playerResult : playerResults.entrySet()) {
             Player player = playerResult.getKey();
-            int amount = getPlayerBetAmount(player);
-            allPlayersWinning += calculateAmount(playerResult.getValue(), amount);
+            PlayerGameResult playerGameResult = playerResult.getValue();
+            allPlayersWinning += settleAccordingToResult(player, playerGameResult);
         }
 
         return allPlayersWinning * -1;
     }
 
-    private int calculateAmount(Result result, int amount) {
-        return (int) (amount * result.getPlayerWinningRate());
+    private int settleAccordingToResult(Player player, PlayerGameResult playerGameResult) {
+        int playerBetAmount = getPlayerBetAmount(player);
+        return (int) (playerBetAmount * playerGameResult.getWinningRate());
     }
 
     private int getPlayerBetAmount(Player player) {
