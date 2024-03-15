993a21327a8b2200e6c3e40db13dcc8b73a5ee3f
ay-eonii <naayen@naver.com>
2024-03-15 17:26:10 +0900

    test(CardsTest) : bestSum, sumInitCards 테스트

--- src/test/java/domain/card/CardsTest.java
+++ src/test/java/domain/card/CardsTest.java
@@ -2,6 +2,8 @@ package domain.card;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -9,16 +11,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CardsTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"1,12", "10,21"})
     @DisplayName("처음 뽑은 두 개 카드의 합을 구한다.")
-    void sumInitCards() {
+    void sumInitCards(int number, int sum) {
         Cards cards = new Cards(List.of(
-                new Card(2, Shape.CLUB),
-                new Card(3, Shape.CLUB)
+                new Card(number, Shape.CLUB),
+                new Card(1, Shape.SPADE)
+        ));
+        cards.receive(new Card(1, Shape.HEART));
+
+        assertThat(cards.sumInitCards()).isEqualTo(sum);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,12", "10,21", "13,21"})
+    @DisplayName("Ace가 있을 때의 최선 합을 구한다.")
+    void sumContainsAce(int number, int sum) {
+        Cards cards = new Cards(List.of(
+                new Card(1, Shape.CLUB),
+                new Card(number, Shape.CLUB)
         ));
 
-        cards.receive(new Card(1, Shape.CLUB));
+        assertThat(cards.bestSum()).isEqualTo(sum);
+    }
 
-        assertThat(cards.sumInitCards()).isEqualTo(5);
+    @Test
+    @DisplayName("Ace가 여러 개인 경우 최선 합을 구한다.")
+    void sumOnlyContainsAce() {
+        Cards cards = new Cards(List.of(
+                new Card(1, Shape.CLUB),
+                new Card(1, Shape.SPADE)
+        ));
+        cards.receive(new Card(1, Shape.HEART));
+        cards.receive(new Card(1, Shape.DIAMONDS));
+
+        assertThat(cards.bestSum()).isEqualTo(14);
     }
 }
