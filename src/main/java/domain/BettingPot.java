f31fbfef07dbf643ab43fc62b383f1113efcdd2e
nayonsoso <panda0329@naver.com>
2024-03-15 14:56:30 +0900

    refactor: 함수 분리

--- src/main/java/domain/BettingPot.java
+++ src/main/java/domain/BettingPot.java
@@ -13,21 +13,23 @@ public class BettingPot {
 
     public Map<Player, Integer> settle(Map<Player, Result> playerResults) {
         Map<Player, Integer> settlement = new LinkedHashMap<>();
-
         for (Map.Entry<Player, Result> playerResult : playerResults.entrySet()) {
             Player player = playerResult.getKey();
             int amount = betRecord.get(player).getAmount();
-
-            if (playerResult.getValue() == Result.DEALER_WIN) {
-                amount *= -1;
-            }
-            if (playerResult.getValue() == Result.PLAYER_WIN) {
-                amount *= 2;
-            }
-
+            amount = calculateAmount(playerResult.getValue(), amount);
             settlement.put(player, amount);
         }
 
         return settlement;
+    }
+
+    private int calculateAmount(Result result, int amount) {
+        if (result == Result.DEALER_WIN) {
+            return amount * -1;
+        }
+        if (result == Result.PLAYER_WIN) {
+            return amount * 2;
+        }
+        return amount;
     }
 }
