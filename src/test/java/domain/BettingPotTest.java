00384dc71e7ee1f74fbb6b03b6906c371b78cd7e
nayonsoso <panda0329@naver.com>
2024-03-15 13:28:01 +0900

    feat: 배팅 금액 정산 기능 구현

--- src/test/java/domain/BettingPotTest.java
+++ src/test/java/domain/BettingPotTest.java
@@ -1,7 +1,13 @@
 package domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
@@ -9,12 +15,29 @@ public class BettingPotTest {
 
     @DisplayName("사용자와 배팅 기록을 기록한다.")
     @Test
-    void create() {
+    void collect() {
         BettingPot bettingPot = new BettingPot();
         Player player = new Player("산초");
         Bet bet = new Bet(100);
 
         assertThatCode(() -> bettingPot.collect(player, bet))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("결과에 따라 수익을 정산한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"DEALER_WIN:10:-10", "PLAYER_WIN:10:20", "PUSH:10:10"}, delimiter = ':')
+    void settle(Result result, int betAmount, int expectedWinning) {
+        BettingPot bettingPot = new BettingPot();
+        Player player = new Player("산초");
+        Bet bet = new Bet(betAmount);
+        bettingPot.collect(player, bet);
+        Map<Player, Result> playerResult = new HashMap<>();
+        playerResult.put(player, result);
+
+        Map<Player, Integer> settlement = bettingPot.settle(playerResult);
+        Integer actualWinning = settlement.get(player);
+
+        Assertions.assertThat(actualWinning).isEqualTo(expectedWinning);
     }
 }
