36d2862a1035cb6ea2fd23290c89418d301c2428
DobbyMac <kimdobby@g.skku.edu>
2024-03-15 18:04:16 +0900

    refactor(MatchResult): 게임 결과 변경에 따른 기존 클래스 코드 수정
    - MatchResult에 BLACK_JACK enum 추가
    - '승', '무'. '패' value 제거

--- src/main/java/model/participant/MatchResult.java
+++ src/main/java/model/participant/MatchResult.java
@@ -1,15 +1,8 @@
 package model.participant;
 
 public enum MatchResult {
-    WIN("승"), DRAW("무"), LOSE("패");
-
-    private final String value;
-
-    MatchResult(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    WIN,
+    DRAW,
+    LOSE,
+    BLACK_JACK;
 }
--- src/main/java/model/participant/dto/PlayerMatchResult.java
+++ src/main/java/model/participant/dto/PlayerMatchResult.java
@@ -4,11 +4,7 @@ import model.participant.MatchResult;
 import model.participant.Name;
 
 public record PlayerMatchResult(Name name, MatchResult matchResult) {
-    public String getNameAsString(){
+    public String getNameAsString() {
         return name.getValue();
-    }
-
-    public String getVictoryAsString(){
-        return matchResult.getValue();
     }
 }
