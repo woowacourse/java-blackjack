79a851b309200653bacbd6657b0d4808f4aad141
libienz <leekh10195@gmail.com>
2024-03-15 16:50:47 +0900

    feat: 플레이어들 수익 계산 기능 추가

--- src/main/java/blackjack/domain/bet/BettingBank.java
+++ src/main/java/blackjack/domain/bet/BettingBank.java
@@ -1,7 +1,11 @@
 package blackjack.domain.bet;
 
 import blackjack.domain.player.Player;
+import blackjack.domain.result.GameResult;
+import blackjack.domain.result.PlayerGameResult;
+import blackjack.domain.result.PlayerProfitResult;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BettingBank {
 
@@ -9,5 +13,18 @@ public class BettingBank {
 
     public BettingBank(Map<Player, Money> playerMoneyMap) {
         this.playerMoneyMap = playerMoneyMap;
+    }
+
+    public PlayerProfitResult calculateProfitResult(PlayerGameResult playerGameResult) {
+        return new PlayerProfitResult(playerMoneyMap.keySet().stream()
+                .collect(Collectors.toMap(
+                        player -> player,
+                        player -> calculatePlayerProfit(player, playerGameResult))));
+    }
+
+    private Profit calculatePlayerProfit(Player player, PlayerGameResult playerGameResult) {
+        GameResult gameResultOfPlayer = playerGameResult.findGameResultOfPlayer(player);
+        Money betOfPlayer = playerMoneyMap.get(player);
+        return betOfPlayer.calculateProfit(gameResultOfPlayer.getProfitLeverage());
     }
 }
---
+++ src/test/java/blackjack/domain/bet/BettingBankTest.java
@@ -0,0 +1,43 @@
+package blackjack.domain.bet;
+
+import static blackjack.domain.result.GameResult.BLACKJACK_WIN;
+import static blackjack.domain.result.GameResult.PLAYER_LOSE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import blackjack.domain.player.Player;
+import blackjack.domain.player.TestPlayerCreator;
+import blackjack.domain.result.GameResult;
+import blackjack.domain.result.PlayerGameResult;
+import blackjack.domain.result.PlayerProfitResult;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("베팅 관리자 도메인 테스트")
+class BettingBankTest {
+
+    @DisplayName("플레이어들의 통합 수익을 계산할 수 있다")
+    @Test
+    void testCalculatePlayerProfit() {
+        Player player1 = TestPlayerCreator.of("리비", 1, 10);
+        Player player2 = TestPlayerCreator.of("썬", 3, 4, 5, 6);
+
+        Map<Player, Money> playerMoneyMap = new HashMap<>();
+        playerMoneyMap.put(player1, new Money(1000));
+        playerMoneyMap.put(player2, new Money(1000));
+        BettingBank bettingBank = new BettingBank(playerMoneyMap);
+
+        Map<Player, GameResult> playerGameResultMap = new HashMap<>();
+        playerGameResultMap.put(player1, BLACKJACK_WIN);
+        playerGameResultMap.put(player2, PLAYER_LOSE);
+        PlayerGameResult playerGameResult = new PlayerGameResult(playerGameResultMap);
+
+        PlayerProfitResult playerProfitResult = bettingBank.calculateProfitResult(playerGameResult);
+        assertAll(
+                () -> assertThat(playerProfitResult.findProfitOfPlayer(player1).getValue()).isEqualTo(1500),
+                () -> assertThat(playerProfitResult.findProfitOfPlayer(player2).getValue()).isEqualTo(-1000)
+        );
+    }
+}
