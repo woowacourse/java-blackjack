fcc59c3c4a7c9928ddc205419129a5762a879a0e
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-03-16 10:48:38 +0900

    feat(PrizeChecker) : 경기 결과에 따른 금액 계산 기능 구현
    - 이에 따른 테스트 코드 추가

---
+++ src/main/java/blackjack/domain/result/PrizeChecker.java
@@ -0,0 +1,42 @@
+package blackjack.domain.result;
+
+import blackjack.domain.player.Dealer;
+import blackjack.domain.player.GamePlayer;
+
+import java.util.function.BiPredicate;
+
+
+public enum PrizeChecker {
+    BLACKJACK((gamePlayer, dealer) -> gamePlayer.isBlackjack() && !dealer.isBlackjack(), 1.5),
+    WIN((gamePlayer, dealer) -> !gamePlayer.isBust() && dealer.isBust() || (
+            isStand(gamePlayer, dealer) && compare(gamePlayer, dealer) > 0), 1),
+    DRAW(((gamePlayer, dealer) -> (gamePlayer.isBlackjack() && dealer.isBlackjack()) || (
+            isStand(gamePlayer, dealer) && compare(gamePlayer, dealer) == 0)), 0),
+
+    LOSE((gamePlayer, dealer) -> gamePlayer.isBust() || (
+            isStand(gamePlayer, dealer) && compare(gamePlayer, dealer) < 0), -1);
+    private final BiPredicate<GamePlayer, Dealer> predicate;
+    private final double profitRate;
+
+    PrizeChecker(final BiPredicate<GamePlayer, Dealer> predicate, final double profitRate) {
+        this.predicate = predicate;
+        this.profitRate = profitRate;
+    }
+
+    public static final int check(final GamePlayer gamePlayer, final Dealer dealer) {
+        for (final var checker : PrizeChecker.values()) {
+            if (checker.predicate.test(gamePlayer, dealer)) {
+                return (int) (gamePlayer.getBettingMoney() * checker.profitRate);
+            }
+        }
+        throw new IllegalStateException("조건에 맞지 않는 계산입니다");
+    }
+
+    private static boolean isStand(final GamePlayer gamePlayer, final Dealer dealer) {
+        return gamePlayer.isStand() && dealer.isStand();
+    }
+
+    private static int compare(final GamePlayer gamePlayer, final Dealer dealer) {
+        return Integer.compare(gamePlayer.calculateScore(), dealer.calculateScore());
+    }
+}
---
+++ src/test/java/blackjack/domain/result/PrizeCheckerTest.java
@@ -0,0 +1,129 @@
+package blackjack.domain.result;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardSymbol;
+import blackjack.domain.card.CardValue;
+import blackjack.domain.player.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PrizeCheckerTest {
+    @Test
+    @DisplayName("플레이어가 블랙잭 , 딜러가 블랙잭이 아니면 1.5배의 금액을 받는다.")
+    void player_blackjack_case() {
+        final GamePlayer gamePlayer = 플레이어_생성(1000, List.of(CardValue.ACE, CardValue.JACK));
+
+        final Dealer dealer = 딜러_생성(List.of(CardValue.EIGHT, CardValue.EIGHT));
+
+        final var prize = PrizeChecker.check(gamePlayer, dealer);
+
+        assertThat(prize).isEqualTo(1500);
+    }
+
+    @Test
+    @DisplayName("플레이어가 블랙잭 , 딜러가 블랙잭이면 배팅 금액을 돌려받는다.")
+    void player_blackjack_dealer_blackjack_case() {
+        final GamePlayer gamePlayer = 플레이어_생성(1000, List.of(CardValue.ACE, CardValue.JACK));
+
+        final Dealer dealer = 딜러_생성(List.of(CardValue.ACE, CardValue.JACK));
+
+        final var prize = PrizeChecker.check(gamePlayer, dealer);
+
+        assertThat(prize).isZero();
+    }
+
+    @Test
+    @DisplayName("플레이어가 버스트 , 딜러가 버스트이면 배팅 금액을 잃는다.")
+    void player_buse_dealer_bust_case() {
+        final GamePlayer gamePlayer = 플레이어_생성(1000, List.of(CardValue.JACK, CardValue.JACK, CardValue.EIGHT));
+
+        final Dealer dealer = 딜러_생성(List.of(CardValue.TWO, CardValue.JACK, CardValue.JACK));
+
+        final var prize = PrizeChecker.check(gamePlayer, dealer);
+
+        assertThat(prize).isEqualTo(-1000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 버스트 , 딜러가 버스트가 아니면 배팅 금액을 잃는다.")
+    void player_bust_case() {
+        final GamePlayer gamePlayer = 플레이어_생성(1000, List.of(CardValue.JACK, CardValue.JACK, CardValue.EIGHT));
+
+        final Dealer dealer = 딜러_생성(List.of(CardValue.TWO, CardValue.JACK));
+
+        final var prize = PrizeChecker.check(gamePlayer, dealer);
+
+        assertThat(prize).isEqualTo(-1000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 버스트가 아니고 , 딜러가 버스트이면 배팅 금액만큼 받는다.")
+    void dealer_bust_case() {
+        final GamePlayer gamePlayer = 플레이어_생성(1000, List.of(CardValue.JACK, CardValue.JACK));
+        final Dealer dealer = 딜러_생성(List.of(CardValue.TWO, CardValue.JACK, CardValue.JACK));
+
+        final var prize = PrizeChecker.check(gamePlayer, dealer);
+
+        assertThat(prize).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 스탠드 , 딜러가 스탠드 일때 플레이어가 숫자가 더 크면 배팅 금액만큼 받는다.")
+    void player_score_high_case() {
+        final GamePlayer gamePlayer = 플레이어_생성(1000, List.of(CardValue.JACK, CardValue.JACK));
+        gamePlayer.stand();
+        final Dealer dealer = 딜러_생성(List.of(CardValue.TWO, CardValue.JACK));
+        dealer.stand();
+
+        final var prize = PrizeChecker.check(gamePlayer, dealer);
+
+        assertThat(prize).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 스탠드 , 딜러가 스탠드 일때 숫자가 같으면 배팅 금액만큼 돌려받는다.")
+    void player_score_dealer_score_equal_case() {
+        final GamePlayer gamePlayer = 플레이어_생성(1000, List.of(CardValue.JACK, CardValue.JACK));
+        gamePlayer.stand();
+        final Dealer dealer = 딜러_생성(List.of(CardValue.JACK, CardValue.JACK));
+        dealer.stand();
+
+        final var prize = PrizeChecker.check(gamePlayer, dealer);
+
+        assertThat(prize).isZero();
+    }
+
+    @Test
+    @DisplayName("플레이어가 스탠드 , 딜러가 스탠드 일때 플레이어 숫자가 작으면 배팅 금액을 잃는다.")
+    void dealer_score_high_case() {
+        final GamePlayer gamePlayer = 플레이어_생성(1000, List.of(CardValue.JACK, CardValue.FIVE));
+        gamePlayer.stand();
+        final Dealer dealer = 딜러_생성(List.of(CardValue.JACK, CardValue.JACK));
+        dealer.stand();
+
+        final var prize = PrizeChecker.check(gamePlayer, dealer);
+
+        assertThat(prize).isEqualTo(-1000);
+    }
+
+
+    private GamePlayer 플레이어_생성(final Integer bettingMoney, final List<CardValue> cardValues) {
+        final PlayerInfo playerInfo = new PlayerInfo(new Name("조이썬"), new BettingMoney(bettingMoney));
+        final GamePlayer gamePlayer = new GamePlayer(playerInfo);
+        cardValues.stream()
+                  .forEach(cardValue -> gamePlayer.drawCard(new Card(cardValue, CardSymbol.DIAMOND)));
+        return gamePlayer;
+    }
+
+    private Dealer 딜러_생성(final List<CardValue> cardValues) {
+        final PlayerInfo playerInfo = new PlayerInfo(new Name("조이썬"), new BettingMoney(1000));
+        final Dealer dealer = new Dealer(playerInfo);
+        cardValues.stream()
+                  .forEach(cardValue -> dealer.drawCard(new Card(cardValue, CardSymbol.DIAMOND)));
+        return dealer;
+    }
+}
