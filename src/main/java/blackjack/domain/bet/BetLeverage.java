b12083c7bba3904b070520a9de6d40d93f4b78dd
Arachneee <jhg2819@naver.com>
2024-03-16 15:31:02 +0900

    style: 메소드 분리

--- src/main/java/blackjack/domain/bet/BetLeverage.java
+++ src/main/java/blackjack/domain/bet/BetLeverage.java
@@ -1,24 +1,14 @@
 package blackjack.domain.bet;
 
-import blackjack.domain.rule.Score;
 import blackjack.domain.rule.state.State;
 import java.util.Arrays;
 import java.util.function.BiPredicate;
 
 public enum BetLeverage {
-    BLACKJACK(1.5,
-            (playerState, dealerState) -> playerState.isBlackjack() && dealerState.isNotBlackjack()),
-    WIN(1,
-            (playerState, dealerState) -> playerState.isNotBust()
-                    && (dealerState.isBust() || isScoreBigger(playerState, dealerState))),
-    LOSE(-1,
-            (playerState, dealerState) -> playerState.isBust()
-                    || (dealerState.isBlackjack() && playerState.isNotBlackjack())
-                    || (dealerState.isNotBust() && isScoreBigger(dealerState, playerState))),
-    PUSH(0,
-            (playerState, dealerState) -> dealerState.isNotBust() && playerState.isNotBust()
-                    && isScoreSame(playerState, dealerState));
-
+    BLACKJACK(1.5, BetLeverage::isBlackjackWin),
+    WIN(1, BetLeverage::isWin),
+    LOSE(-1, BetLeverage::isLose),
+    PUSH(0, BetLeverage::isPush);
 
     private final double leverage;
     private final BiPredicate<State, State> condition;
@@ -30,11 +20,27 @@ public enum BetLeverage {
 
     public static BetLeverage of(final State playerState, final State dealerState) {
         validateState(playerState, dealerState);
-
         return Arrays.stream(values())
                 .filter(betStatus -> betStatus.condition.test(playerState, dealerState))
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException("잘못된 점수입니다."));
+    }
+
+    private static boolean isBlackjackWin(final State playerState, final State dealerState) {
+        return playerState.isBlackjack() && !dealerState.isBlackjack();
+    }
+
+    private static boolean isWin(final State playerState, final State dealerState) {
+        return playerState.isNotBust() && (dealerState.isBust() || isScoreBigger(playerState, dealerState));
+    }
+
+    private static boolean isLose(final State playerState, final State dealerState) {
+        return playerState.isBust() || (dealerState.isBlackjack() && !playerState.isBlackjack())
+                || (!dealerState.isBust() && isScoreBigger(dealerState, playerState));
+    }
+
+    private static boolean isPush(final State playerState, final State dealerState) {
+        return !dealerState.isBust() && !playerState.isBust() && isScoreSame(playerState, dealerState);
     }
 
     private static void validateState(final State playerState, final State dealerState) {
@@ -44,21 +50,14 @@ public enum BetLeverage {
     }
 
     private static boolean isScoreBigger(final State first, final State second) {
-        final Score firstScore = first.hands().calculateScore();
-        final Score secondScore = second.hands().calculateScore();
-
-        return firstScore.compareTo(secondScore) > 0;
+        return first.hands().calculateScore().compareTo(second.hands().calculateScore()) > 0;
     }
 
     private static boolean isScoreSame(final State first, final State second) {
-        final Score firstScore = first.hands().calculateScore();
-        final Score secondScore = second.hands().calculateScore();
-
-        return firstScore.equals(secondScore);
+        return first.hands().calculateScore().equals(second.hands().calculateScore());
     }
 
-    public BetRevenue applyLeverage(final BetAmount batAmount) {
-        return new BetRevenue(batAmount.multiply(leverage));
+    public BetRevenue applyLeverage(final BetAmount betAmount) {
+        return new BetRevenue(betAmount.multiply(leverage));
     }
-
 }
