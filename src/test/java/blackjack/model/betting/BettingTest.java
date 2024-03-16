90c4161a0f55d806046869ba0d0b138a8be5b29e
Choo <choo000407@naver.com>
2024-03-17 02:30:13 +0900

    test: 플레이어 수익 계산 테스트 결과 올바르게 수정

--- src/test/java/blackjack/model/betting/BettingTest.java
+++ src/test/java/blackjack/model/betting/BettingTest.java
@@ -4,87 +4,59 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import blackjack.model.player.MatchResult;
 import blackjack.view.dto.PlayerBettingProfitOutcome;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BettingTest {
-    @Test
-    @DisplayName("플레이어 이름으로 플레이어의 배팅 금액을 등록한다")
-    void addPlayerBettingMoneyTest() {
-        // given
-        Betting betting = new Betting();
-        String playerName = "dora";
-        BettingMoney bettingMoney = new BettingMoney(1000);
+    private String playerName;
+    private Betting betting;
 
-        // when
-        betting.addPlayerBettingMoney(playerName, bettingMoney);
-
-        // then
-        assertThat(betting.findBettingMoneyByPlayerName(playerName)).isEqualTo(bettingMoney);
+    @BeforeEach
+    void setUp() {
+        betting = new Betting();
+        playerName = "dora";
+        betting.addPlayerBettingMoney(playerName, new BettingMoney(1000));
     }
 
     @Test
     @DisplayName("승리한 플레이어는 배팅 금액의 1배를 추가로 받는다")
     void calculateWinningPlayerProfitTest() {
-        // given
-        Betting betting = new Betting();
-        String playerName = "dora";
-        BettingMoney bettingMoney = new BettingMoney(1000);
-        betting.addPlayerBettingMoney(playerName, bettingMoney);
-
         // when
         PlayerBettingProfitOutcome outcome = betting.calculatePlayerBettingProfit(playerName, MatchResult.WIN);
-
-        // then
-        assertThat(outcome.profit()).isEqualTo(2000);
-    }
-
-    @Test
-    @DisplayName("패배한 플레이어는 배팅 금액을 잃는다")
-    void calculateLosingPlayerProfitTest() {
-        // given
-        Betting betting = new Betting();
-        String playerName = "dora";
-        BettingMoney bettingMoney = new BettingMoney(1000);
-        betting.addPlayerBettingMoney(playerName, bettingMoney);
-
-        // when
-        PlayerBettingProfitOutcome outcome = betting.calculatePlayerBettingProfit(playerName, MatchResult.LOSE);
-
-        // then
-        assertThat(outcome.profit()).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("무승부인 플레이어는 배팅 금액을 돌려받는다")
-    void calculatePushPlayerProfitTest() {
-        // given
-        Betting betting = new Betting();
-        String playerName = "dora";
-        BettingMoney bettingMoney = new BettingMoney(1000);
-        betting.addPlayerBettingMoney(playerName, bettingMoney);
-
-        // when
-        PlayerBettingProfitOutcome outcome = betting.calculatePlayerBettingProfit(playerName, MatchResult.PUSH);
 
         // then
         assertThat(outcome.profit()).isEqualTo(1000);
     }
 
     @Test
-    @DisplayName("블랙잭 승리한 플레이어는 배팅 금액의 1.5배를 추가로 받는다")
-    void calculateBlackjackWinningPlayerProfitTest() {
-        // given
-        Betting betting = new Betting();
-        String playerName = "dora";
-        BettingMoney bettingMoney = new BettingMoney(1000);
-        betting.addPlayerBettingMoney(playerName, bettingMoney);
-
+    @DisplayName("패배한 플레이어는 배팅 금액을 잃는다")
+    void calculateLosingPlayerProfitTest() {
         // when
-        PlayerBettingProfitOutcome outcome = betting.calculatePlayerBettingProfit(playerName,
-                MatchResult.BLACKJACK_WIN);
+        PlayerBettingProfitOutcome outcome = betting.calculatePlayerBettingProfit(playerName, MatchResult.LOSE);
 
         // then
-        assertThat(outcome.profit()).isEqualTo(2500);
+        assertThat(outcome.profit()).isEqualTo(-1000);
+    }
+
+    @Test
+    @DisplayName("무승부인 플레이어는 배팅 금액을 돌려받는다")
+    void calculatePushPlayerProfitTest() {
+        // when
+        PlayerBettingProfitOutcome outcome = betting.calculatePlayerBettingProfit(playerName, MatchResult.PUSH);
+
+        // then
+        assertThat(outcome.profit()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("블랙잭 승리한 플레이어는 배팅 금액의 1.5배를 추가로 받는다")
+    void calculateBlackjackWinningPlayerProfitTest() {
+        // when
+        PlayerBettingProfitOutcome outcome
+                = betting.calculatePlayerBettingProfit(playerName, MatchResult.BLACKJACK_WIN);
+
+        // then
+        assertThat(outcome.profit()).isEqualTo(1500);
     }
 }
