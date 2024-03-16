4e114bfde5f4750c6ccacb973ac807fc2dfa7439
Choo <choo000407@naver.com>
2024-03-17 03:35:11 +0900

    refactor: 딜러 이름 정의해 outputView 공통 로직 제거

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -12,34 +12,31 @@ import java.util.stream.Collectors;
 
 public class OutputView {
     private static final String DEALING_RESULT_INTRO = "\n딜러와 %s에게 2장을 나누었습니다.\n";
-    private static final String PLAYER_CARDS_FORM = "%s카드: %s";
-    private static final String DEALER_DRAWING_FORM = "\n딜러는 16이하라 1장의 카드를 더 받았습니다.";
-    private static final String DEALER_CARDS_FORM = "\n딜러카드: %s";
-    private static final String TOTAL_SCORE_FORM = " - 결과: %s\n";
+    private static final String CARDS_FORM = "%s 카드: %s";
+    private static final String DEALER_DRAWING_FORM = "\n딜러는 16이하라 1장의 카드를 더 받았습니다.\n";
+    private static final String TOTAL_SCORE_FORM = " - 결과: ";
     private static final String BETTING_PROFIT_INTRO = "\n## 최종 수익";
-    private static final String DEALER_BETTING_PROFIT_PREFIX = "딜러: ";
+    private static final String DEALER_NAME = "딜러";
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
 
     public void printDealingCards(final Players players, final Dealer dealer) {
-        String names = String.join(", ", players.getNames());
-        System.out.printf(DEALING_RESULT_INTRO, names);
-        System.out.printf(DEALER_CARDS_FORM, formatCard(dealer.getFirstCard()));
-        System.out.println();
+        System.out.println(formatDealingResultIntro(players));
+        System.out.println(formatCards(List.of(dealer.getFirstCard()), DEALER_NAME));
         for (Player player : players.getPlayers()) {
-            System.out.println(formatPlayerCards(player));
+            System.out.println(formatCards(player.getCards(), player.getName()));
         }
     }
 
-    private String formatPlayerCards(final Player player) {
-        List<Card> cards = player.getCards();
-        String joinedCards = formatCards(cards);
-        return String.format(PLAYER_CARDS_FORM, player.getName(), joinedCards);
+    private String formatDealingResultIntro(final Players players) {
+        String names = String.join(", ", players.getNames());
+        return String.format(DEALING_RESULT_INTRO, names);
     }
 
-    private String formatCards(final List<Card> cards) {
-        return cards.stream()
+    private String formatCards(final List<Card> cards, String name) {
+        String joinedCards = cards.stream()
                 .map(this::formatCard)
                 .collect(Collectors.joining(", "));
+        return String.format(CARDS_FORM, name, joinedCards);
     }
 
     private String formatCard(final Card card) {
@@ -47,7 +44,7 @@ public class OutputView {
     }
 
     public void printPlayerDrawingCards(final Player player) {
-        System.out.println(formatPlayerCards(player));
+        System.out.println(formatCards(player.getCards(), player.getName()));
     }
 
     public void printDealerDrawingCards(final Dealer dealer) {
@@ -58,18 +55,24 @@ public class OutputView {
     public void printDealerFinalCards(final DealerFinalCardsOutcome dealerFinalCardsOutcome) {
         List<Card> dealerCards = dealerFinalCardsOutcome.cards();
         int dealerTotalScore = dealerFinalCardsOutcome.totalScore();
-        System.out.printf(DEALER_CARDS_FORM, formatCards(dealerCards));
-        System.out.printf(TOTAL_SCORE_FORM, dealerTotalScore);
+        System.out.println(formatFinalCards(dealerCards, DEALER_NAME, dealerTotalScore));
     }
 
     public void printPlayersFinalCards(final List<PlayerFinalCardsOutcome> playerFinalCardsOutcomes) {
-        for (PlayerFinalCardsOutcome playerOutcome : playerFinalCardsOutcomes) {
-            String playerName = playerOutcome.name();
-            List<Card> playerCards = playerOutcome.cards();
-            int playerTotalScore = playerOutcome.totalScore();
-            System.out.printf(PLAYER_CARDS_FORM, playerName, formatCards(playerCards));
-            System.out.printf(TOTAL_SCORE_FORM, playerTotalScore);
+        for (PlayerFinalCardsOutcome playerFinalCardsOutcome : playerFinalCardsOutcomes) {
+            List<Card> playerCards = playerFinalCardsOutcome.cards();
+            String playerName = playerFinalCardsOutcome.name();
+            int playerTotalScore = playerFinalCardsOutcome.totalScore();
+            System.out.println(formatFinalCards(playerCards, playerName, playerTotalScore));
         }
+    }
+
+    private String formatFinalCards(final List<Card> cards, final String name, final int totalScore) {
+        return formatCards(cards, name) + formatTotalScore(totalScore);
+    }
+
+    private String formatTotalScore(final int totalScore) {
+        return TOTAL_SCORE_FORM + totalScore;
     }
 
     public void printBettingProfit(final int dealerBettingProfit,
@@ -80,7 +83,7 @@ public class OutputView {
     }
 
     private String formatDealerBettingProfit(final int dealerBettingProfit) {
-        return DEALER_BETTING_PROFIT_PREFIX + dealerBettingProfit;
+        return formatBettingProfit(DEALER_NAME, dealerBettingProfit);
     }
 
     private String formatPlayerBettingProfits(final List<PlayerBettingProfitOutcome> playerBettingProfitOutcomes) {
@@ -90,7 +93,11 @@ public class OutputView {
     }
 
     private String formatPlayerBettingProfit(final PlayerBettingProfitOutcome playerBettingProfitOutcome) {
-        return playerBettingProfitOutcome.name() + ": " + playerBettingProfitOutcome.profit();
+        return formatBettingProfit(playerBettingProfitOutcome.name(), playerBettingProfitOutcome.profit());
+    }
+
+    private String formatBettingProfit(final String name, final int profit) {
+        return name + ": " + profit;
     }
 
     public void printException(final String errorMessage) {
