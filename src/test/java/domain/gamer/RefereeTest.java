4a655ab471c1e53e9a109a26ef3dba9363a57894
hoyeonyy <highoption@naver.com>
2024-03-16 16:27:23 +0900

    feat(Referee) : 둘다 블랙잭일 경우 무승부 추가

--- src/test/java/domain/gamer/RefereeTest.java
+++ src/test/java/domain/gamer/RefereeTest.java
@@ -12,18 +12,19 @@ import org.junit.jupiter.api.Test;
 
 class RefereeTest {
 
+    Cards playerCards = new Cards();
+    Cards dealerCards = new Cards();
+    Referee referee = new Referee();
+
+
     @DisplayName("심판을 카드를 받아 블랙잭인지 계산할 수 있다.")
     @Test
     void makePlayerAndDealerScore() {
-        Cards playerCards = new Cards();
         playerCards.addCard(new Card(CardNumber.KING, CardPattern.CLOVER_PATTERN));
         playerCards.addCard(new Card(CardNumber.ACE, CardPattern.SPADE_PATTERN));
 
-        Cards dealerCards = new Cards();
         dealerCards.addCard(new Card(CardNumber.TWO, CardPattern.CLOVER_PATTERN));
         dealerCards.addCard(new Card(CardNumber.KING, CardPattern.HEART_PATTERN));
-
-        Referee referee = new Referee();
 
         Assertions.assertAll(
                 () -> assertThat(referee.isBlackJack(playerCards)).isTrue(),
@@ -33,18 +34,27 @@ class RefereeTest {
 
     @DisplayName("플레이어가 블랙잭으로 이긴 경우 BLACKJACKWIN을 반환한다")
     @Test
-    void makeResult() {
-        Cards playerCards = new Cards();
+    void makeBlackJackWinResult() {
         playerCards.addCard(new Card(CardNumber.KING, CardPattern.CLOVER_PATTERN));
         playerCards.addCard(new Card(CardNumber.ACE, CardPattern.SPADE_PATTERN));
 
-        Cards dealerCards = new Cards();
         dealerCards.addCard(new Card(CardNumber.TWO, CardPattern.CLOVER_PATTERN));
         dealerCards.addCard(new Card(CardNumber.KING, CardPattern.HEART_PATTERN));
 
-        Referee referee = new Referee();
-
         referee.judgeResult(playerCards, dealerCards);
         assertThat(referee.judgeResult(playerCards, dealerCards)).isEqualTo(PlayerResult.BLACKJACKWIN);
+    }
+
+    @DisplayName("플레이어가 블랙잭, 딜러도 블랙잭인 경우 DRAW를 반환한다")
+    @Test
+    void makeBlackJackDrawResult() {
+        playerCards.addCard(new Card(CardNumber.KING, CardPattern.CLOVER_PATTERN));
+        playerCards.addCard(new Card(CardNumber.ACE, CardPattern.SPADE_PATTERN));
+
+        dealerCards.addCard(new Card(CardNumber.QUEEN, CardPattern.CLOVER_PATTERN));
+        dealerCards.addCard(new Card(CardNumber.ACE, CardPattern.HEART_PATTERN));
+
+        referee.judgeResult(playerCards, dealerCards);
+        assertThat(referee.judgeResult(playerCards, dealerCards)).isEqualTo(PlayerResult.DRAW);
     }
 }
