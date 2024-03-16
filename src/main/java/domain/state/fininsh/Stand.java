4812d26ab630103420b067b20a2e4861105840fd
woojong <dwax1324@gmail.com>
2024-03-17 06:58:35 +0900

    refactor: 예외 클래스 변경 및 메시지 추가

--- src/main/java/domain/state/fininsh/Stand.java
+++ src/main/java/domain/state/fininsh/Stand.java
@@ -12,7 +12,7 @@ public final class Stand extends Finished {
     @Override
     public Result compareWith(final State otherState) {
         if (!otherState.isFinished()) {
-            throw new IllegalStateException();
+            throw new ClassCastException("완료된 상태가 아닙니다");
         }
         final Finished otherFinished = (Finished) otherState;
 
