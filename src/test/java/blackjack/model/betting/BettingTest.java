7ef5a5fb66d1dd292e9eb7777037f967964a7bfd
Choo <choo000407@naver.com>
2024-03-16 18:58:37 +0900

    test: 플레이어 이름으로 플레이어의 배팅 금액을 등록한다

---
+++ src/test/java/blackjack/model/betting/BettingTest.java
@@ -0,0 +1,21 @@
+package blackjack.model.betting;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BettingTest {
+    @Test
+    @DisplayName("플레이어 이름으로 플레이어의 배팅 금액을 등록한다")
+    void addPlayerBettingMoneyTest() {
+        // given
+        Betting betting = new Betting();
+        String playerName = "dora";
+        Money money = new Money(1000);
+
+        // when
+        betting.addPlayerBettingMoney(playerName, money);
+
+        // then
+        assertThat(betting.getBettingMoneyByPlayerName(playerName)).isEqualTo(money);
+    }
+}
