3a10127bbac38505dcfd0e2e88432e31ddcd0f6f
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-03-17 14:54:29 +0900

    test(CardDeckTest) : 테스트에서 반복문 stream으로 변환

--- src/test/java/model/card/CardDeckTest.java
+++ src/test/java/model/card/CardDeckTest.java
@@ -3,17 +3,22 @@ package model.card;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CardDeckTest {
 
-    @DisplayName("덱이 비어있을 때 카드를 뽑으면 예외가 발생한다.")
+    @DisplayName("52개의 카드를 다 뽑아서, 덱이 비어있을 때 카드를 뽑으면 예외가 발생한다.")
     @Test
     void validateNotEmpty() {
         CardDeck cardDeck = new CardDeck(Card.createCardDeck());
-        for (int i = 0; i < 52; i++) {
-            cardDeck.selectRandomCard();
-        }
+        IntStream.range(0, 52)
+                .forEach(i -> cardDeck.selectRandomCard());
+
         assertThatThrownBy(cardDeck::selectRandomCard)
                 .isInstanceOf(IllegalStateException.class);
 
