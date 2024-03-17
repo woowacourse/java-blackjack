33c0900e0166ecf510ed0af931d6d7b5b8a9930f
mzeong <mjeong@sookmyung.ac.kr>
2024-03-17 14:43:43 +0900

    feat: 배팅 배수 결정하는 로직 구현

--- src/main/java/blackjack/domain/game/Result.java
+++ src/main/java/blackjack/domain/game/Result.java
@@ -5,49 +5,41 @@ import blackjack.domain.participant.Player;
 
 public enum Result {
 
-    WIN,
-    LOSE,
-    TIE,
+    BLACKJACK_WIN(1.5),
+    WIN(1),
+    LOSE(-1),
+    TIE(0),
     ;
 
-    Result() {
+    private final double multiplier;
+
+    Result(double multiplier) {
+        this.multiplier = multiplier;
     }
 
-    public boolean isWin() {
-        return this == WIN;
-    }
-
-    public boolean isLoss() {
-        return this == LOSE;
-    }
-
-    public boolean isTie() {
-        return this == TIE;
-    }
-
-    public static Result determineResult(Player player, Dealer dealer) {
+    public static double determineMultiplier(Player player, Dealer dealer) {
         if (player.isBust()) {
-            return Result.LOSE;
+            return Result.LOSE.multiplier;
         }
         if (player.isBlackjack() && dealer.isNotBlackjack()) {
-            return Result.WIN;
+            return Result.BLACKJACK_WIN.multiplier;
         }
         if (dealer.isBust()) {
-            return Result.WIN;
+            return Result.WIN.multiplier;
         }
         return compareScore(player, dealer);
     }
 
-    private static Result compareScore(Player player, Dealer dealer) {
+    private static double compareScore(Player player, Dealer dealer) {
         Score playerScore = player.handScore();
         Score dealerScore = dealer.handScore();
 
         if (playerScore.isBiggerThan(dealerScore)) {
-            return Result.WIN;
+            return Result.WIN.multiplier;
         }
         if (playerScore.isLessThan(dealerScore)) {
-            return Result.LOSE;
+            return Result.LOSE.multiplier;
         }
-        return Result.TIE;
+        return Result.TIE.multiplier;
     }
 }
