812ad1f1f9df8a77f2ba96a4292beefa7d0d5770
이상진 <priceelees@gmail.com>
2024-03-17 13:02:58 +0900

    refactor(Player): Name 필드를 Player 클래스에서만 정의하도록 수정 및 equals 재정의

--- src/main/java/blackjack/domain/gamer/Player.java
+++ src/main/java/blackjack/domain/gamer/Player.java
@@ -3,19 +3,46 @@ package blackjack.domain.gamer;
 import blackjack.domain.BlackjackConstants;
 import blackjack.domain.card.Card;
 import java.util.List;
+import java.util.Objects;
 
 public class Player extends BlackjackGamer {
 
+    private final Name name;
+
     public Player(String name) {
-        super(name);
+        super();
+        this.name = new Name(name);
     }
 
     public Player(String name, List<Card> cards) {
-        super(name, cards);
+        super(cards);
+        this.name = new Name(name);
     }
 
     @Override
     public boolean canReceiveCard() {
         return !isBlackjack() && getScore() <= BlackjackConstants.BLACKJACK_VALUE.getValue();
+    }
+
+    @Override
+    public String getName() {
+        return name.value();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
