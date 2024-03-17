6597df42a4a9f5d54c596a0ef01441b6c65572d3
zangsu <zangsu_@naver.com>
2024-03-17 16:51:45 +0900

    refactor : 가독성 증진을 위해 생성자 오버로딩

--- src/main/java/blackjack/domain/card/Value.java
+++ src/main/java/blackjack/domain/card/Value.java
@@ -2,18 +2,18 @@ package blackjack.domain.card;
 
 public enum Value {
     ACE(1, 11),
-    TWO(2, 2),
-    THREE(3, 3),
-    FOUR(4, 4),
-    FIVE(5, 5),
-    SIX(6, 6),
-    SEVEN(7, 7),
-    EIGHT(8, 8),
-    NINE(9, 9),
-    TEN(10, 10),
-    JACK(10, 10),
-    QUEEN(10, 10),
-    KING(10, 10);
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10);
 
     private final int minScore;
     private final int maxScore;
@@ -21,6 +21,10 @@ public enum Value {
     Value(int minScore, int maxScore) {
         this.minScore = minScore;
         this.maxScore = maxScore;
+    }
+
+    Value(int score) {
+        this(score, score);
     }
 
     public int getMinScore() {
