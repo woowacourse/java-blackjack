06330575c4f060972236aceb5c7f7b1966300599
gyungchan Jo <namejgc@naver.com>
2024-03-17 23:30:05 +0900

    refactor: 카드 덱을 생성할 때 캐싱하여 사용하도록 변경

--- src/main/java/blackjack/domain/card/CardDeck.java
+++ src/main/java/blackjack/domain/card/CardDeck.java
@@ -3,10 +3,14 @@ package blackjack.domain.card;
 import blackjack.domain.card.strategy.CardShuffleStrategy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CardDeck {
-    private static final List<Card> INITIAL_CARD_DECK = CardFactory.create();
+    private static final List<Card> INITIAL_CARD_DECK = Arrays.stream(Suit.values())
+            .flatMap(suit -> Arrays.stream(Denomination.values())
+                    .map(denomination -> new Card(suit, denomination)))
+            .toList();
 
     private List<Card> cardDeck;
 
