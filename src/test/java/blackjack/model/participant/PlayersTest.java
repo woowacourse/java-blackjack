0081a9fa9fac8295cbf89c0e33410bdb06160e56
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-18 01:44:50 +0900

    test(Player): 카드를 추가로 받을 수 있는지에 대해 Hand별 테스트 추가

--- src/test/java/blackjack/model/participant/PlayersTest.java
+++ src/test/java/blackjack/model/participant/PlayersTest.java
@@ -28,20 +28,16 @@ class PlayersTest {
     @DisplayName("플레이어의 이름과 초기 카드들을 받아서 플레이어 그룹을 생성한다.")
     void createPlayers() {
         List<String> names = List.of("리브", "몰리");
-        Deck deck = new Deck(new ArrayDeque<>(List.of(new Card(CLOVER, ACE), new Card(CLOVER, FIVE),
-                new Card(CLOVER, FOUR), new Card(CLOVER, EIGHT))));
-        assertThatCode(() -> Players.of(names, deck));
+        assertThatCode(() -> Players.of(names, Deck.createByRandomOrder()));
     }
 
     @Test
     @DisplayName("플레이어의 이름이 중복되는 경우 예외를 던진다.")
     void createPlayersByDuplicatedName() {
         List<String> names = List.of("몰리", "몰리");
-        Deck deck = new Deck(new ArrayDeque<>(List.of(new Card(CLOVER, ACE), new Card(CLOVER, FIVE),
-                new Card(CLOVER, FOUR), new Card(CLOVER, EIGHT))));
 
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> Players.of(names, deck))
+                .isThrownBy(() -> Players.of(names, Deck.createByRandomOrder()))
                 .withMessage("중복되는 이름을 입력할 수 없습니다.");
     }
 
@@ -65,21 +61,20 @@ class PlayersTest {
     @DisplayName("플레이어들의 이름 목록을 반환한다.")
     void getNames() {
         List<String> names = List.of("리브", "몰리");
-        Deck deck = new Deck(new ArrayDeque<>(List.of(new Card(CLOVER, ACE), new Card(CLOVER, FIVE),
-                new Card(CLOVER, FOUR), new Card(CLOVER, EIGHT))));
-        assertThat(Players.of(names, deck).getNames()).containsExactly(new Name("리브"), new Name("몰리"));
+        assertThat(Players.of(names, Deck.createByRandomOrder()).getNames()).containsExactly(new Name("리브"),
+                new Name("몰리"));
     }
 
     @Test
     @DisplayName("각 플레이어의 이름과 카드들을 모아서 반환한다.")
     void collectCardsOfEachPlayer() {
         List<String> names = List.of("리브", "몰리");
-        Deck deck = new Deck(new ArrayDeque<>(List.of(new Card(CLOVER, ACE), new Card(CLOVER, FIVE),
-                new Card(CLOVER, FOUR), new Card(CLOVER, EIGHT))));
+        Deck deck = new Deck(new ArrayDeque<>(List.of(Card.from(CLOVER, ACE), Card.from(CLOVER, FIVE),
+                Card.from(CLOVER, FOUR), Card.from(CLOVER, EIGHT))));
 
         Map<Name, List<Card>> expected = new LinkedHashMap<>();
-        expected.put(new Name("리브"), List.of(new Card(CLOVER, ACE), new Card(CLOVER, FIVE)));
-        expected.put(new Name("몰리"), (List.of(new Card(CLOVER, FOUR), new Card(CLOVER, EIGHT))));
+        expected.put(new Name("리브"), List.of(Card.from(CLOVER, ACE), Card.from(CLOVER, FIVE)));
+        expected.put(new Name("몰리"), (List.of(Card.from(CLOVER, FOUR), Card.from(CLOVER, EIGHT))));
         assertThat(Players.of(names, deck).collectCardsOfEachPlayer())
                 .containsExactlyEntriesOf(expected);
     }
