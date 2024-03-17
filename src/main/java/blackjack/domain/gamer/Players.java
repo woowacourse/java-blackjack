0084cc9ab7e19ac50a314808fdce000c233b1ad9
lemone <lejinhy@gmail.com>
2024-03-18 00:22:21 +0900

    refactor: 메서드 위치 변경

--- src/main/java/blackjack/domain/gamer/Players.java
+++ src/main/java/blackjack/domain/gamer/Players.java
@@ -1,6 +1,9 @@
 package blackjack.domain.gamer;
 
 import blackjack.domain.card.Card;
+import blackjack.domain.game.PlayerResult;
+import blackjack.domain.game.Referee;
+import blackjack.domain.money.ProfitCalculator;
 
 import java.util.Collections;
 import java.util.List;
@@ -19,17 +22,25 @@ public class Players {
         }
     }
 
+    public Long allProfit() {
+        return values.stream()
+                .mapToLong(Player::profit)
+                .sum();
+    }
+
+    public void calculateProfit(Dealer dealer, Referee referee,
+                                ProfitCalculator calculator) {
+        for (Player player : values) {
+            PlayerResult result = referee.judgePlayerResult(dealer, player);
+            calculator.playerProfit(player, result);
+        }
+    }
+
     public List<String> names() {
         return values.stream().map(Player::name).toList();
     }
 
     public List<Player> values() {
         return Collections.unmodifiableList(values);
-    }
-
-    public Long allProfit() {
-        return values.stream()
-                .mapToLong(Player::profit)
-                .sum();
     }
 }
