8b734748cfc4cac4ae0ae5b3cf7de60d64bd26cd
Choo <choo000407@naver.com>
2024-03-18 03:02:55 +0900

    refactor: 매직넘버 상수화

--- src/main/java/blackjack/model/betting/BettingMoney.java
+++ src/main/java/blackjack/model/betting/BettingMoney.java
@@ -1,15 +1,17 @@
 package blackjack.model.betting;
 
 public record BettingMoney(int amount) {
-    private static final String INVALID_BETTING_MONEY_AMOUNT = "배팅 금액은 0원보다 커야 합니다.";
+    private static final String INVALID_BETTING_MONEY = "배팅 금액은 0원보다 커야 합니다.";
+
+    private static final int BETTING_MONEY_LOWER_BOUND = 0;
 
     public BettingMoney {
         validateAmount(amount);
     }
 
     private void validateAmount(final int amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException(INVALID_BETTING_MONEY_AMOUNT);
+        if (amount <= BETTING_MONEY_LOWER_BOUND) {
+            throw new IllegalArgumentException(INVALID_BETTING_MONEY);
         }
     }
 }
