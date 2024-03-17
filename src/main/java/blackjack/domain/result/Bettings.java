6a9b19efc4643a7452fc54721e8eb76d0f6f5683
devhoya97 <diajbh123@gmail.com>
2024-03-17 16:09:11 +0900

    refactor: PlayerOutcome마다 Profit을 위해 곱해줘야 하는 상수 연결

--- src/main/java/blackjack/domain/result/Bettings.java
+++ src/main/java/blackjack/domain/result/Bettings.java
@@ -7,10 +7,6 @@ import java.util.Map;
 
 public class Bettings {
 
-    private static final int NORMAL_WIN_PROFIT_MULTIPLIER = 1;
-    private static final double BLACKJACK_WIN_PROFIT_MULTIPLIER = 1.5;
-    private static final int PUSH_PROFIT_MULTIPLIER = 0;
-
     private final Map<Player, Money> bettings;
 
     public Bettings(final Map<Player, Money> bettings) {
@@ -19,23 +15,12 @@ public class Bettings {
 
     public Map<Name, Money> calculatePlayerProfits(final Judge judge) {
         final Map<Name, Money> playerProfits = new LinkedHashMap<>();
-        bettings.forEach((player, betting) ->
-                playerProfits.put(player.getName(), calculateProfit(betting, judge.calculatePlayerOutcome(player))));
+        bettings.forEach((player, betting) -> {
+            final PlayerOutcome playerOutcome = judge.calculatePlayerOutcome(player);
+            playerProfits.put(player.getName(), betting.multiply(playerOutcome.getProfitMultiplier()));
+        });
 
         return playerProfits;
-    }
-
-    private Money calculateProfit(final Money betting, final PlayerOutcome playerOutcome) {
-        if (playerOutcome == PlayerOutcome.BLACKJACK_WIN) {
-            return betting.multiply(BLACKJACK_WIN_PROFIT_MULTIPLIER);
-        }
-        if (playerOutcome == PlayerOutcome.NORMAL_WIN) {
-            return betting.multiply(NORMAL_WIN_PROFIT_MULTIPLIER);
-        }
-        if (playerOutcome == PlayerOutcome.PUSH) {
-            return betting.multiply(PUSH_PROFIT_MULTIPLIER);
-        }
-        return betting.toNegative();
     }
 
     public Money calculateDealerProfit(final Judge judge) {
--- src/main/java/blackjack/domain/result/PlayerOutcome.java
+++ src/main/java/blackjack/domain/result/PlayerOutcome.java
@@ -2,8 +2,18 @@ package blackjack.domain.result;
 
 public enum PlayerOutcome {
 
-    BLACKJACK_WIN,
-    NORMAL_WIN,
-    LOSE,
-    PUSH
+    BLACKJACK_WIN(1.5),
+    NORMAL_WIN(1),
+    LOSE(-1),
+    PUSH(0);
+
+    private final double profitMultiplier;
+
+    PlayerOutcome(final double profitMultiplier) {
+        this.profitMultiplier = profitMultiplier;
+    }
+
+    public double getProfitMultiplier() {
+        return profitMultiplier;
+    }
 }
