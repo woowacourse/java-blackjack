0ab8beaa449c3f573dfb228be231a3fa5e2c734f
tackyu <tack970913@gmail.com>
2024-03-17 21:03:01 +0900

    refactor: 검증 이후에 값을 설정하도록 수정

--- src/main/java/domain/money/Money.java
+++ src/main/java/domain/money/Money.java
@@ -5,16 +5,17 @@ import domain.game.Result;
 public class Money {
     private final int value;
 
-    public Money(String value) {
+    public Money(String inputValue) {
         try {
-            this.value = Integer.parseInt(value);
+            int value = Integer.parseInt(inputValue);
+            validate(value);
+            this.value = value;
         } catch (NumberFormatException ne) {
             throw new NumberFormatException("양의 정수만 입력하세요.");
         }
-        validate();
     }
 
-    private void validate() {
+    private void validate(int value) {
         if (value <= 0) {
             throw new IllegalArgumentException("양의 정수만 입력하세요.");
         }
