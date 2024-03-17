75ecc4eeee904a522de19e779f1e2f7908894cd2
Nam Gi Beom <rlqja0523@naver.com>
2024-03-17 21:45:44 +0900

    refactor: 테스트 명확하게 수정, 필요없는 클래스 제거

--- src/test/java/domain/participant/ParticipantsTest.java
+++
@@ -1,5 +0,0 @@
-package domain.participant;
-
-class ParticipantsTest {
-
-}
--- src/test/java/domain/participant/PlayerTest.java
+++ src/test/java/domain/participant/PlayerTest.java
@@ -7,32 +7,40 @@ import domain.card.Denomination;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
+    private final Name name = new Name("prin");
+    private final BetAmount betAmount = new BetAmount(1);
+
     @Test
-    void 점수가_bust면_true를_반환한다() {
-        // given
-        Player player = new Player(new Name("prin"), new BetAmount(1)); // todo 수정
+    void 점수가_bust면_끝난_상태이므로_false를_반환한다() {
+        Player player = new Player(name, betAmount);
         player.receiveAdditionalCard(카드(Denomination.TEN));
         player.receiveAdditionalCard(카드(Denomination.TEN));
         player.receiveAdditionalCard(카드(Denomination.TEN));
 
-        // when
         boolean result = player.isNotFinished();
 
-        // then
-        assertThat(result).isTrue();
+        assertThat(result).isFalse();
     }
 
     @Test
-    void 점수가_bust가_아니면_false를_반환한다() { // todo 수정
-        // given
-        Player player = new Player(new Name("prin"), new BetAmount(1));
+    void 점수가_blackjack이면_끝난_상태이므로_false를_반환한다() {
+        Player player = new Player(name, betAmount);
+        player.receiveAdditionalCard(카드(Denomination.ACE));
+        player.receiveAdditionalCard(카드(Denomination.TEN));
+
+        boolean result = player.isNotFinished();
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void 점수가_bust도_아니고_blackjack도_아니면_끝나지_않은_상태이므로_true를_반환한다() {
+        Player player = new Player(name, betAmount);
         player.receiveAdditionalCard(카드(Denomination.FIVE));
         player.receiveAdditionalCard(카드(Denomination.FOUR));
 
-        // when
         boolean result = player.isNotFinished();
 
-        // then
-        assertThat(result).isFalse();
+        assertThat(result).isTrue();
     }
 }
