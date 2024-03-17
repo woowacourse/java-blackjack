620acf733b9ee8534cdca6d9f44aae72233e5a93
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-03-17 16:24:41 +0900

    feat(Blackjack) : 게임 플레이어 정보를 통해 생성하게 변경
    - 이에 따른 테스트 코드 변경

--- src/main/java/blackjack/domain/Blackjack.java
+++ src/main/java/blackjack/domain/Blackjack.java
@@ -1,16 +1,9 @@
 package blackjack.domain;
 
 import blackjack.domain.card.Deck;
-import blackjack.domain.card.Card;
-import blackjack.domain.card.Cards;
-import blackjack.domain.player.Names;
-import blackjack.domain.player.Dealer;
-import blackjack.domain.player.GamePlayer;
-import blackjack.domain.player.Participant;
-import blackjack.domain.player.Players;
+import blackjack.domain.player.*;
 import blackjack.domain.result.*;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,24 +19,38 @@ public class Blackjack {
         deck = Deck.createPack();
     }
 
-    public void acceptPlayers(final Names names) {
-        final Dealer dealer = Dealer.createDefaultDealer(drawTwo());
-        final List<GamePlayer> gamePlayers = names.stream()
-                                                  .map(name -> new GamePlayer(name, drawTwo()))
-                                                  .toList();
+    public void acceptPlayers(final List<PlayerInfo> playerInfos) {
+        final Dealer dealer = createDealer();
+        final List<GamePlayer> gamePlayers = createGamePlayers(playerInfos);
         this.players = new Players(dealer, gamePlayers);
     }
 
-    private Cards drawTwo() {
-        return new Cards(List.of(deck.draw(), deck.draw()));
+    private Dealer createDealer() {
+        final Dealer dealer = Dealer.createDefaultNameDealer();
+        participantDrawCard(dealer);
+        participantDrawCard(dealer);
+        return dealer;
+    }
+
+    private List<GamePlayer> createGamePlayers(final List<PlayerInfo> playerInfos) {
+        return playerInfos.stream()
+                          .map(this::createGamePlayer)
+                          .toList();
+    }
+
+    private GamePlayer createGamePlayer(final PlayerInfo playerInfo) {
+        final GamePlayer gamePlayer = new GamePlayer(playerInfo);
+        participantDrawCard(gamePlayer);
+        participantDrawCard(gamePlayer);
+        return gamePlayer;
     }
 
     public void participantHitCard(final Participant participant) {
-        participant.drawCard(draw());
+        participantDrawCard(participant);
     }
 
-    private Card draw() {
-        return deck.draw();
+    private void participantDrawCard(final Participant participant) {
+        participant.drawCard(deck.draw());
     }
 
     public Result checkResult() {
@@ -61,7 +68,7 @@ public class Blackjack {
     }
 
     private GamePlayerResult checkPlayerResult(final Dealer dealer, final GamePlayer gamePlayer) {
-        return new GamePlayerResult(gamePlayer.getName(), ResultChecker.calculate(dealer, gamePlayer));
+        return new GamePlayerResult(gamePlayer.getName(), PrizeChecker.check(dealer, gamePlayer));
     }
 
     public Dealer getDealer() {
--- src/test/java/blackjack/domain/BlackjackTest.java
+++ src/test/java/blackjack/domain/BlackjackTest.java
@@ -2,10 +2,8 @@ package blackjack.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import blackjack.domain.player.Names;
-import blackjack.domain.player.Participant;
+import blackjack.domain.player.*;
 import blackjack.domain.result.GamePlayerResult;
-import blackjack.domain.result.ResultStatus;
 import blackjack.fixture.CardFixture;
 
 import java.util.List;
@@ -16,12 +14,12 @@ import org.junit.jupiter.api.Test;
 class BlackjackTest {
 
     @Test
-    @DisplayName("이름 목록을 통해 플레이어를 생성하고 딜러와 플레이어에게 카드를 두장씩 나눠준다.")
+    @DisplayName("플레이어 정보를 통해 플레이어를 생성하고 딜러와 플레이어에게 카드를 두장씩 나눠준다.")
     void accept_players() {
         final Blackjack blackjack = new Blackjack(CardFixture.카드_덱_생성());
-        final Names names = Names.from(List.of("초롱", "조이썬"));
-
-        blackjack.acceptPlayers(names);
+        final PlayerInfo playerInfo1 = new PlayerInfo(new Name("초롱"), new BettingMoney(1000));
+        final PlayerInfo playerInfo2 = new PlayerInfo(new Name("조이썬"), new BettingMoney(1500));
+        blackjack.acceptPlayers(List.of(playerInfo1, playerInfo2));
 
         assertPlayer(blackjack.getDealer(), 8);
 
@@ -34,28 +32,31 @@ class BlackjackTest {
     @Test
     @DisplayName("게임 결과를 종합한다.")
     void dealer_Count_result() {
-        final var names = Names.from(List.of("초롱", "조이썬"));
+        final PlayerInfo playerInfo1 = new PlayerInfo(new Name("초롱"), new BettingMoney(1000));
+        final PlayerInfo playerInfo2 = new PlayerInfo(new Name("조이썬"), new BettingMoney(1500));
         final var sut = new Blackjack(CardFixture.카드_덱_생성());
-        sut.acceptPlayers(names);
+        sut.acceptPlayers(List.of(playerInfo1, playerInfo2));
+        sut.getGamePlayers()
+           .forEach(GamePlayer::stand);
+        sut.getDealer()
+           .stand();
 
         final var result = sut.checkResult();
 
         assertGamePlayerResult(result.getGamePlayerResults()
-                                     .get(0), ResultStatus.WIN);
-
+                                     .get(0), 1000);
         assertGamePlayerResult(result.getGamePlayerResults()
-                                     .get(1), ResultStatus.WIN);
-
+                                     .get(1), 1500);
         assertThat(result.getDealerResult()
-                         .getCountWithResultStatus(ResultStatus.WIN)).isZero();
+                         .getPrizeMoney()).isEqualTo(-2500);
     }
 
     private void assertPlayer(final Participant participant, final int value) {
         assertThat(participant.calculateScore()).isEqualTo(value);
     }
 
-    private void assertGamePlayerResult(final GamePlayerResult gamePlayerResult, ResultStatus resultStatus) {
-        assertThat(gamePlayerResult.getResultStatus()).isEqualTo(resultStatus);
+    private void assertGamePlayerResult(final GamePlayerResult gamePlayerResult, final int prize) {
+        assertThat(gamePlayerResult.getPrizeMoney()).isEqualTo(prize);
     }
 
 }
