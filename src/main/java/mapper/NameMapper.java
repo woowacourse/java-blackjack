ec0005c0a903600ec389f71f11edb79ce82eb26d
yoonjuho <dbswn990@naver.com>
2024-03-18 01:44:30 +0900

    chore(NameMapper): 코드의 일관성 유지

--- src/main/java/mapper/NameMapper.java
+++ src/main/java/mapper/NameMapper.java
@@ -7,7 +7,7 @@ import view.dto.participant.NameDto;
 
 import java.util.List;
 
-public final class NameMapper {
+public class NameMapper {
     public static Names namesDtoToNames(final List<NameDto> namesDto) {
         return new Names(namesDto.stream()
                                  .map(nameDto -> new Name(nameDto.name()))
