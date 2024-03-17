748464218f3f39b41ed9f9933bb355222eeb8d09
le2sky <leehaneul990623@gmail.com>
2024-03-17 15:11:17 +0900

    refactor: players 생성 책임 분리

--- src/main/java/blackjack/domain/participant/Participants.java
+++ src/main/java/blackjack/domain/participant/Participants.java
@@ -6,67 +6,16 @@ import blackjack.domain.common.Money;
 
 public class Participants {
 
-    private final Dealer dealer = new Dealer();
+    private final Dealer dealer;
     private final List<Player> players;
 
     public Participants(List<String> playerNames, List<Money> playersMoney) {
-        validate(playerNames, playersMoney);
-
-        this.players = createPlayers(playerNames, playersMoney);
-    }
-
-    private void validate(List<String> playerNames, List<Money> playersMoney) {
-        validatePlayerSize(playerNames);
-        validateDuplicatedPlayerNames(playerNames);
-        validateSameSize(playerNames, playersMoney);
-        validateInvalidPlayerName(playerNames, dealer.getName());
-    }
-
-    private void validatePlayerSize(List<String> playerNames) {
-        if (playerNames.isEmpty()) {
-            throw new IllegalArgumentException("사용자가 존재하지 않습니다.");
-        }
-    }
-
-    private void validateDuplicatedPlayerNames(List<String> playerNames) {
-        if (getUniqueSize(playerNames) != playerNames.size()) {
-            throw new IllegalArgumentException("사용자 이름이 중복되었습니다.");
-        }
-    }
-
-    private long getUniqueSize(List<String> playerNames) {
-        return playerNames.stream()
-                .distinct()
-                .count();
-    }
-
-    private void validateSameSize(List<String> playerNames, List<Money> playersMoney) {
-        if (playerNames.size() != playersMoney.size()) {
-            throw new IllegalArgumentException("사용자의 이름의 수와 돈의 수가 일치하지 않습니다.");
-        }
-    }
-
-    private void validateInvalidPlayerName(List<String> playerNames, String dealerName) {
-        if (playerNames.contains(dealerName)) {
-            throw new IllegalArgumentException(String.format("사용자 이름은 '%s'가 될 수 없습니다.", dealerName));
-        }
-    }
-
-    private List<Player> createPlayers(List<String> playerNames, List<Money> playersMoney) {
-        List<Player> returningPlayers = new ArrayList<>();
-        int totalPlayerSize = playerNames.size();
-
-        for (int index = 0; index < totalPlayerSize; index++) {
-            Player player = new Player(playerNames.get(index), playersMoney.get(index));
-            returningPlayers.add(player);
-        }
-
-        return returningPlayers;
+        this.dealer = new Dealer();
+        this.players = PlayerFactory.createPlayers(playerNames, playersMoney, dealer);
     }
 
     public List<Participant> getParticipants() {
         List<Participant> participants = new ArrayList<>();
-
         participants.add(dealer);
         participants.addAll(players);
 
