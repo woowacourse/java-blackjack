1ba7d235f45fe7fb6b74ba1fa55c6f8a242576a8
mzeong <mjeong@sookmyung.ac.kr>
2024-03-17 16:20:27 +0900

    refactor: 반복되는 코드 줄이기 위해 record로 변경

--- src/main/java/blackjack/domain/card/Card.java
+++ src/main/java/blackjack/domain/card/Card.java
@@ -1,17 +1,8 @@
 package blackjack.domain.card;
 
 import blackjack.domain.participant.Score;
-import java.util.Objects;
 
-public class Card {
-
-    private final CardRank cardRank;
-    private final CardSuit cardSuit;
-
-    public Card(CardRank cardRank, CardSuit cardSuit) {
-        this.cardRank = cardRank;
-        this.cardSuit = cardSuit;
-    }
+public record Card(CardRank cardRank, CardSuit cardSuit) {
 
     public boolean isAce() {
         return cardRank.isAce();
@@ -19,30 +10,5 @@ public class Card {
 
     public Score score() {
         return new Score(cardRank.getScore());
-    }
-
-    public CardRank getCardRank() {
-        return cardRank;
-    }
-
-    public CardSuit getCardSuit() {
-        return cardSuit;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Card card = (Card) o;
-        return cardSuit == card.cardSuit && cardRank == card.cardRank;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(cardSuit, cardRank);
     }
 }
