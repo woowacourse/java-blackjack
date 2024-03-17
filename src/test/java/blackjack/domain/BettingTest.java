59baf03f0d7cfa9a8c0a569b3792c7d87e6cb79f
eunjungL <dldmswjd322@gmail.com>
2024-03-18 01:28:49 +0900

    refactor: BettingTest의 수익률 계산 테스트 ParameterizedTest로 변경

--- src/test/java/blackjack/domain/BettingTest.java
+++ src/test/java/blackjack/domain/BettingTest.java
@@ -1,19 +1,29 @@
 package blackjack.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("배팅 금액")
 class BettingTest {
+
+    private static Stream<Arguments> calculateProfitSource() {
+        return Stream.of(
+                Arguments.arguments(GameResult.BLACKJACK, new BigDecimal("1500.0")),
+                Arguments.arguments(GameResult.WIN, new BigDecimal("1000")),
+                Arguments.arguments(GameResult.DRAW, new BigDecimal("0")),
+                Arguments.arguments(GameResult.LOSE, new BigDecimal("-1000"))
+        );
+    }
 
     @DisplayName("배팅 금액이 0 초과의 자연수가 아니면 예외가 발생한다.")
     @ParameterizedTest
@@ -26,17 +36,16 @@ class BettingTest {
     }
 
     @DisplayName("게임 결과에 따른 수익률을 계산한다.")
-    @Test
-    void calculateProfit() {
+    @ParameterizedTest
+    @MethodSource("calculateProfitSource")
+    void calculateProfit(final GameResult gameResult, final BigDecimal expected) {
         // given
         Betting betting = new Betting("1000");
 
-        // when & then
-        assertAll(
-                () -> assertThat(betting.calculateProfit(GameResult.BLACKJACK)).isEqualTo(new BigDecimal("1500.0")),
-                () -> assertThat(betting.calculateProfit(GameResult.WIN)).isEqualTo(new BigDecimal("1000")),
-                () -> assertThat(betting.calculateProfit(GameResult.DRAW)).isEqualTo(new BigDecimal("0")),
-                () -> assertThat(betting.calculateProfit(GameResult.LOSE)).isEqualTo(new BigDecimal("-1000"))
-        );
+        // when
+        BigDecimal actual = betting.calculateProfit(gameResult);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 }
