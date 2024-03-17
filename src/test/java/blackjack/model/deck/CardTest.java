18a426d218ed8b242baf529442c61096f41c6ab0
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-18 01:43:18 +0900

    test(Card): 생성된 카드 목록의 크기 확인 테스트 위치 변경

--- src/test/java/blackjack/model/deck/CardTest.java
+++ src/test/java/blackjack/model/deck/CardTest.java
@@ -1,2 +1,23 @@
-package blackjack.model.deck;public class CardTest {
+package blackjack.model.deck;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CardTest {
+    private List<Card> cards;
+
+    @BeforeEach
+    void init() {
+        cards = Card.getCards();
+    }
+
+    @Test
+    @DisplayName("카드 목록을 생성한다.")
+    void createDeck() {
+        assertThat(cards).hasSize(52);
+    }
 }
