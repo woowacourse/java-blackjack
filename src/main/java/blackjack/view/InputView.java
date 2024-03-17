6024b731a02c6d8df09f307ce4bc7379859bc718
Choo <choo000407@naver.com>
2024-03-18 02:44:14 +0900

    refactor: 예외 메시지 정확하게 수정

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
     private static final String ASK_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String ASK_BETTING_MONEY_TO_PLAYER = "\n%s의 배팅 금액은?";
     private static final String ASK_DRAW_OR_STAND_COMMAND_TO_PLAYER = "\n%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
-    private static final String INVALID_BETTING_MONEY_AMOUNT = "배팅 금액은 숫자로 입력해야 합니다.";
+    private static final String INVALID_BETTING_MONEY_AMOUNT = "배팅 금액은 정수로 입력해야 합니다.";
 
     private final Scanner scanner;
 
