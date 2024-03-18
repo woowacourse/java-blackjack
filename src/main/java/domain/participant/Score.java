7ce5ae691617e54bd68503522b94f75074b9c6dd
kelly6bf <kelly6bf@gmail.com>
2024-03-18 11:13:36 +0900

    refactor: Scores의 역할을 Score에 위임

--- src/main/java/domain/participant/Score.java
+++ src/main/java/domain/participant/Score.java
@@ -1,13 +1,28 @@
 package domain.participant;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public record Score(int value) {
+    private static final int MINIMUM_SCORE_VALUE = 1;
+    private static final int MAXIMUM_SCORE_VALUE = 31;
+    private static final Map<Integer, Score> cached = init();
+
+    private static Map<Integer, Score> init() {
+        final Map<Integer, Score> scores = new HashMap<>();
+        for (int score = MINIMUM_SCORE_VALUE; score <= MAXIMUM_SCORE_VALUE; score++) {
+            scores.put(score, new Score(score));
+        }
+
+        return scores;
+    }
+
     public static Score valueOf(final int value) {
-        final Score score = Scores.getScore(value);
-        if (score == null) {
+        if (!cached.containsKey(value)) {
             return new Score(value);
         }
 
-        return score;
+        return cached.get(value);
     }
 
     public boolean isBigger(final Score other) {
