4396a3bd81fb9c63e3c5b45dbae24422324aa676
jongmee <ra123221@gmail.com>
2024-03-18 13:04:03 +0900

    refactor: Player의 Name으로 동등성 비교

--- src/main/java/blackjack/model/player/Player.java
+++ src/main/java/blackjack/model/player/Player.java
@@ -4,6 +4,8 @@ import blackjack.model.card.Cards;
 import blackjack.model.card.Score;
 import blackjack.model.cardgenerator.CardGenerator;
 
+import java.util.Objects;
+
 public class Player {
     private final Name name;
     private final Cards cards;
@@ -31,6 +33,19 @@ public class Player {
 
     public void hit(final CardGenerator cardGenerator) {
         cards.addCard(cardGenerator);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     public Cards getCards() {
