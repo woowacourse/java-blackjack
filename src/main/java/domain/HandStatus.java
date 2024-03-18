1202a037f24a8bb9b4a5ee08c4fd419bc26e69b5
slimsha2dy <chy2581@naver.com>
2024-03-18 12:57:58 +0900

    refactor: HandStatus 클래스를 record로 수정

--- src/main/java/domain/HandStatus.java
+++ src/main/java/domain/HandStatus.java
@@ -1,19 +1,7 @@
 package domain;
 
-public class HandStatus {
-    private final int score;
-    private final int handCount;
-
-    public HandStatus(int score, int handCount) {
-        this.score = score;
-        this.handCount = handCount;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public int getHandCount() {
-        return handCount;
-    }
+public record HandStatus(
+        int score,
+        int handCount
+) {
 }
