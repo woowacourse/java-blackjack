62edc433f2ec2c243370a78536b4d3a706cc25fb
hyxrxn <gpfls981220@gmail.com>
2024-03-18 16:08:22 +0900

    test(Money): 정상 생성 경계값 테스트 추가

--- src/test/java/blackjack/game/MoneyTest.java
+++ src/test/java/blackjack/game/MoneyTest.java
@@ -1,6 +1,7 @@
 package blackjack.game;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,10 +11,17 @@ class MoneyTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, -1000})
-    @DisplayName("금액이 양수가 아니면 예외를 발생시킨다.")
-    void validateNaturalTest(int money) {
+    @DisplayName("금액이 자연수가 아니면 예외를 발생시킨다.")
+    void validateNaturalNumberTest(int money) {
         assertThatThrownBy(() -> new Money(money))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 금액은 양수여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 10, 100})
+    @DisplayName("금액이 자연수이면 잘 생성된다.")
+    void creatSuccessfullyTest(int money) {
+        assertDoesNotThrow(() -> new Money(money));
     }
 }
