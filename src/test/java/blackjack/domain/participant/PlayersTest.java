624ed3837ba362778814d3102a8411786cf7b4d8
mzeong <mjeong@sookmyung.ac.kr>
2024-03-21 22:11:36 +0900

    refactor: PlayerNames 생성

--- src/test/java/blackjack/domain/participant/PlayersTest.java
+++ src/test/java/blackjack/domain/participant/PlayersTest.java
@@ -1,8 +1,6 @@
 package blackjack.domain.participant;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import blackjack.domain.profit.PlayersProfit;
 import blackjack.domain.profit.Profit;
@@ -15,49 +13,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PlayersTest {
-
-    @DisplayName("플레이어가 없으면 예외가 발생한다.")
-    @Test
-    void testCreatePlayersWithEmptyEntry() {
-        assertThatThrownBy(() -> new Players(List.of()))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("중복되는 플레이어의 이름이 존재하면 예외가 발생한다.")
-    @Test
-    void testCreatePlayersWithDuplicateNames() {
-        Player player1 = new Player(new PlayerName("pobi"));
-        Player player = new Player(new PlayerName("pobi"));
-
-        assertThatThrownBy(() -> new Players(List.of(player1, player)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("플레이어들을 생성한다.")
-    @Test
-    void testCreateWithValidPlayers() {
-        Player pobi = new Player(new PlayerName("pobi"));
-        Player jason = new Player(new PlayerName("jason"));
-
-        assertThatCode(() -> new Players(List.of(pobi, jason)))
-                .doesNotThrowAnyException();
-    }
-
-    @DisplayName("주어진 플레이어 이름 리스트로 플레이어들을 생성한다.")
-    @Test
-    void testCreateByNameList() {
-        // given
-        List<String> playerNames = List.of("pobi", "jason");
-
-        // when
-        Players players = Players.create(playerNames);
-
-        // then
-        assertThat(players.getPlayers()).containsExactly(
-                new Player(new PlayerName("pobi")),
-                new Player(new PlayerName("jason"))
-        );
-    }
 
     @DisplayName("모든 플레이어가 배팅한다.")
     @Test
