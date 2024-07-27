d4378c0abf340d715bf79f9b4c38a953f91eac4f
yuho9 <146949649+yuho9@users.noreply.github.com>
2024-07-27 20:25:53 +0900

    분리한 이름으로 Player 객체 리스트 생성

--- src/main/java/BlackJack/BlackjackGame.java
+++ src/main/java/BlackJack/BlackjackGame.java
@@ -1,11 +1,19 @@
 package BlackJack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlackjackGame {
 
 	public static void main(String[] args) {
 		
 		String playerNamesInput = InputView.getPlayerNames();
 		String[] playerNames = playerNamesInput.split(",");
+		
+		List<Player> players = new ArrayList<>();
+		  for(String name : playerNames) {
+			  players.add(new Player(name));
+		}
 
 	}
 
---
+++ src/main/java/BlackJack/Player.java
@@ -0,0 +1,11 @@
+package BlackJack;
+
+public class Player {
+
+	private String name;
+	
+	public Player(String name) {
+		this.name = name;
+	}
+	
+}
