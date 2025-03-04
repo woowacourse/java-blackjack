3965e9edf671e901ccc1f731462d1366400e4430
rladmstn <oz33177351@gmail.com>
2025-03-04 16:08:22 +0900

    feat: 플레이어 목록 관리 로직 구현

---
+++ src/test/java/domain/PlayersTest.java
@@ -0,0 +1,40 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class PlayersTest {
+
+    @Test
+    void 플레이어_리스트로부터_모델을_생성한다() {
+        // given
+        List<Player> players = List.of(Player.of("pobi1"),
+                Player.of("pobi2"),
+                Player.of("pobi3"),
+                Player.of("pobi4"),
+                Player.of("pobi5"));
+
+        // when & then
+        assertThatCode(() -> Players.of(players))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 플레이어가_5명_초과일_때_예외가_발생한다() {
+        // given
+        List<Player> players = List.of(Player.of("pobi1"),
+                Player.of("pobi2"),
+                Player.of("pobi3"),
+                Player.of("pobi4"),
+                Player.of("pobi5"),
+                Player.of("pobi6"));
+
+        // when & then
+        assertThatThrownBy(() -> Players.of(players))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참여 가능한 플레이어는 최대 5명 입니다.");
+    }
+}
