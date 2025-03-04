fd0d9459be03df17e351bc6d1ded006a0f691376
jhpark1227 <junhyeok0246@naver.com>
2025-03-04 16:50:19 +0900

    🟢green: 덱은 항상 다른 카드 반환 테스트 통과 코드 작성

--- src/test/java/Deck.java
+++ src/test/java/Deck.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
+    private final List<String> numbers = List.of(
+            "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "K", "J"
+    );
+    private final List<String> shapes = List.of("spade", "heart", "clover", "diamond");
+
+    private final List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        init();
+    }
 
     public Card pick() {
-        return new Card("2", "spade");
+        Card card = cards.getLast();
+        cards.removeLast();
+        return card;
+    }
+    
+    private void init() {
+        for (String number : numbers) {
+            for (String shape : shapes) {
+                cards.add(new Card(number, shape));
+            }
+        }
     }
 }
