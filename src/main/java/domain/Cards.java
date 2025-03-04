9f45919ff972b9572a645a4c6a5bff2f29e8164c
yeonnhuu <yeonnhuu@gmail.com>
2025-03-04 19:23:40 +0900

    :sparkles: feat: `Cards` 점수 총합 계산 및 `GameStatus` 계산 기능 구현

---
+++ src/main/java/domain/Cards.java
@@ -0,0 +1,57 @@
+package domain;
+
+import domain.card.Card;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cards {
+
+    private final List<Card> cards;
+
+    public Cards() {
+        this.cards = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        this.cards.add(card);
+    }
+
+    public int calculateScore() {
+        return cards.stream()
+                .map(Card::calculateScore)
+                .reduce(0, Integer::sum);
+    }
+
+    public GameStatus determineGameStatus(Cards otherCards) {
+        int cardsScore = calculateScore();
+        int otherCardsScore = otherCards.calculateScore();
+        if (!isGameOver(cardsScore) && !isGameOver(otherCardsScore)) {
+            return evaluateStatusByScore(cardsScore, otherCardsScore);
+        }
+        return evaluateStatusByGameOver(cardsScore, otherCardsScore);
+    }
+
+    private GameStatus evaluateStatusByScore(int cardsScore, int otherCardsScore) {
+        if (cardsScore > otherCardsScore) {
+            return GameStatus.WIN;
+        }
+        if (cardsScore < otherCardsScore) {
+            return GameStatus.LOSE;
+        }
+        return GameStatus.TIE;
+    }
+
+    private GameStatus evaluateStatusByGameOver(int cardsScore, int otherCardsScore) {
+        if (!isGameOver(cardsScore) && isGameOver(otherCardsScore)) {
+            return GameStatus.WIN;
+        }
+        if (isGameOver(cardsScore) && !isGameOver(otherCardsScore)) {
+            return GameStatus.LOSE;
+        }
+        return GameStatus.TIE;
+    }
+
+    private boolean isGameOver(int score) {
+        return score > 21;
+    }
+}
---
+++ src/test/java/domain/CardsTest.java
@@ -0,0 +1,66 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import domain.card.Card;
+import domain.card.Rank;
+import domain.card.Suit;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CardsTest {
+
+    @Test
+    @DisplayName("카드 묶음의 점수 계산 기능 테스트")
+    void calculateCardsScoreTest() {
+        // given
+        List<Rank> ranks = List.of(Rank.JACK, Rank.ACE);
+        Cards cards = createCardsOfRanks(ranks);
+        // when & then
+        assertEquals(11, cards.calculateScore());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"TWO:WIN", "JACK,QUEEN,KING:TIE"}, delimiterString = ":")
+    @DisplayName("GameOver인 카드 묶음이 있는 GameStatus 계산 기능 테스트")
+    void determineCardsOver21Test(String rankNames, String gameStatusName) {
+        // given
+        List<Rank> ranks = List.of(Rank.JACK, Rank.QUEEN, Rank.KING);
+        Cards cards = createCardsOfRanks(ranks);
+        List<Rank> otherRanks = createRanks(rankNames);
+        Cards otherCards = createCardsOfRanks(otherRanks);
+        // then & when
+        assertEquals(GameStatus.valueOf(gameStatusName), otherCards.determineGameStatus(cards));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"JACK,QUEEN:WIN", "QUEEN,TWO:TIE", "JACK:LOSE", "JACK,QUEEN,KING:LOSE"}, delimiterString = ":")
+    @DisplayName("GameOver인 카드 묶음이 없는 GameStatus 계산 기능 테스트")
+    void determineCardsUnder21Test(String rankNames, String gameStatusName) {
+        // given
+        List<Rank> ranks = List.of(Rank.JACK, Rank.TWO);
+        Cards cards = createCardsOfRanks(ranks);
+        List<Rank> otherRanks = createRanks(rankNames);
+        Cards otherCards = createCardsOfRanks(otherRanks);
+        // then & when
+        assertEquals(GameStatus.valueOf(gameStatusName), otherCards.determineGameStatus(cards));
+    }
+
+    private static Cards createCardsOfRanks(List<Rank> ranks) {
+        Cards cardsOfRanks = new Cards();
+        ranks.stream()
+                .map(rank -> new Card(rank, Suit.DIAMOND))
+                .forEach(cardsOfRanks::addCard);
+        return cardsOfRanks;
+    }
+
+    private static List<Rank> createRanks(String rankNames) {
+        return Arrays.stream(rankNames.split(","))
+                .map(Rank::valueOf)
+                .toList();
+    }
+}
