5c2e0cf604d9b491fe5f5c447cdf021144744c54
ryan-dia <cjfdnjs158@naver.com>
2025-03-04 17:48:14 +0900

    feat(Gamer): 추상 클래스 Gamer 및 구현체 Player 구현
    Co-authored-by:cheche903<cheche903@naver.com>

---
+++ src/main/java/domain/Gamer.java
@@ -0,0 +1,20 @@
+package domain;
+
+public abstract class Gamer {
+
+    private final Nickname nickname;
+    private final Hand hand;
+
+    public Gamer(final Nickname nickname, final Hand hand) {
+        this.nickname = nickname;
+        this.hand = hand;
+    }
+
+    public void receiveCard(final Card card) {
+        hand.add(card);
+    }
+
+    public int calculateSumOfRank() {
+        return hand.getSumOfRank();
+    }
+}
---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Player extends Gamer {
+    public Player(final Nickname nickname, final Hand hand) {
+        super(nickname, hand);
+    }
+}
