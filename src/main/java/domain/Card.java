ec11402a970d0789afae3e1be8dff78f62a40bfc
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-04 17:08:56 +0900

    GREEN) feat(domain) : 카드 취급값 추가

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -3,9 +3,11 @@ package domain;
 public class Card {
     private Denomination denomination;
     private Suit suit;
+    private int value;
 
     public Card(Denomination denomination, Suit suit) {
         this.denomination = denomination;
         this.suit = suit;
+        this.value = 2;
     }
 }
