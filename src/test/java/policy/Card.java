777f48106709332357db83c969b4201a2f11caf4
sonjh919 <kingjh1125@gmail.com>
2025-03-04 16:35:37 +0900

    feat(green): 카드 모양 확인 기능 구현

--- src/test/java/policy/Card.java
+++ src/test/java/policy/Card.java
@@ -13,6 +13,18 @@ public class Card {
         if(!shape.equals("스페이드")){
             throw new IllegalArgumentException("[ERROR] 카드의 모양은 스페이드, 다이아몬드, 하트, 클로버가 있습니다.");
         }
+
+        if(!shape.equals("다이아몬드")){
+            throw new IllegalArgumentException("[ERROR] 카드의 모양은 스페이드, 다이아몬드, 하트, 클로버가 있습니다.");
+        }
+
+        if(!shape.equals("하트")){
+            throw new IllegalArgumentException("[ERROR] 카드의 모양은 스페이드, 다이아몬드, 하트, 클로버가 있습니다.");
+        }
+
+        if(!shape.equals("클로버")){
+            throw new IllegalArgumentException("[ERROR] 카드의 모양은 스페이드, 다이아몬드, 하트, 클로버가 있습니다.");
+        }
         return shape;
     }
 }
