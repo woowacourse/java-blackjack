1182c77d2ba804d2414e1ba8d65b75b56a9000c6
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-04 17:38:48 +0900

    RED) test(domain) : ACE 여부 테스트

--- src/test/java/SomeTest.java
+++ src/test/java/SomeTest.java
@@ -1,36 +1,54 @@
+import static domain.Denomination.*;
+import static domain.Suit.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import domain.Card;
 import domain.Denomination;
 import domain.Suit;
-
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class SomeTest {
 
+    @Nested
+    @DisplayName("카드 생성 테스트")
+    class CardCreateTest {
+        @Test
+        @DisplayName("카드는 모양과 끗수를 가진다")
+        public void test1() {
+            assertThat(new Card(ACE, CLUB)).isInstanceOf(Card.class);
+        }
+
+        @Test
+        @DisplayName("카드는 고유의 숫자값을 갖는다.")
+        public void test2() {
+            assertThat(new Card(TWO, CLUB)).extracting("value").isEqualTo(2);
+        }
+
+        @Test
+        @DisplayName("카드는 고유의 숫자값을 갖는다. - 2")
+        public void test3() {
+            assertThat(new Card(THREE, CLUB)).extracting("value").isEqualTo(3);
+        }
+
+        @Test
+        @DisplayName("카드는 고유의 숫자값을 갖는다. - 3")
+        public void test4() {
+            assertThat(new Card(JACK, CLUB)).extracting("value").isEqualTo(10);
+        }
+    }
+
+
     @Test
-    @DisplayName("카드는 모양과 끗수를 가진다")
+    @DisplayName("ACE는 ACE임을 알릴 수 있다")
     public void test1() {
-        assertThat(new Card(Denomination.ACE, Suit.CLUB)).isInstanceOf(Card.class);
-    }
+        Card clubAce = new Card(ACE, CLUB);
+        Card clubTwo = new Card(TWO, CLUB);
 
-    @Test
-    @DisplayName("카드는 고유의 숫자값을 갖는다.")
-    public void test2() {
-        assertThat(new Card(Denomination.TWO, Suit.CLUB)).extracting("value").isEqualTo(2);
-    }
-
-    @Test
-    @DisplayName("카드는 고유의 숫자값을 갖는다. - 2")
-    public void test3() {
-        assertThat(new Card(Denomination.THREE, Suit.CLUB)).extracting("value").isEqualTo(3);
-    }
-
-    @Test
-    @DisplayName("카드는 고유의 숫자값을 갖는다. - 3")
-    public void test4() {
-        assertThat(new Card(Denomination.JACK, Suit.CLUB)).extracting("value").isEqualTo(10);
+        assertAll(() -> assertThat(clubAce.isAce()).isTrue(),
+                () -> assertThat(clubTwo.isAce()).isFalse());
     }
 
 }
