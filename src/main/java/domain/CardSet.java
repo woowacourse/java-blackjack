f4af6945e36fa2014b2796dbdee709723666db3e
bowook <mbosx316@naver.com>
2025-03-04 16:27:16 +0900

    refactor(CardSet) : 메서드 의미에 맞게 분리

--- src/main/java/domain/CardSet.java
+++ src/main/java/domain/CardSet.java
@@ -18,22 +18,27 @@ public class CardSet {
 
     private static List<Card> initializeCardSet() {
         List<Card> initCard = new ArrayList<>();
+        addShapes(initCard);
+        addRoles(initCard);
+        return initCard;
+    }
+
+    private static void addRoles(List<Card> initCard) {
+        List<CardType> roles = List.of(CardType.JACK, CardType.KING, CardType.QUEEN);
+        for (CardType role : roles) {
+            for (int i = 1; i <= 4; i++) {
+                initCard.add(new Card(10, role));
+            }
+        }
+    }
+
+    private static void addShapes(List<Card> initCard) {
         List<CardType> shapes = List.of(CardType.CLOVER, CardType.HEART, CardType.SPADE, CardType.DIAMOND);
         for (CardType shape : shapes) {
             for (int number = 1; number <= 10; number++) {
                 initCard.add(new Card(number, shape));
             }
         }
-
-        // J, K, Q
-        List<CardType> roles = List.of(CardType.JACK, CardType.KING, CardType.QUEEN);
-        for (CardType role : roles) {
-            for (int i = 0; i < 4; i++) {
-                initCard.add(new Card(10, role));
-            }
-        }
-
-        return initCard;
     }
 
     public List<Card> getCards() {
