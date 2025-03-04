cec174a25311624ed5b428b05b7d2e5f1ec234a5
sonjh919 <kingjh1125@gmail.com>
2025-03-04 17:13:25 +0900

    refactor: 카드 모양 확인 기능 enum 분리

--- src/test/java/policy/Card.java
+++ src/test/java/policy/Card.java
@@ -1,30 +1,11 @@
 package policy;
 
 public class Card {
-    String shape;
+    Shape shape;
     String number;
 
     public Card(String shape, String number) {
-        this.shape = validate(shape);
+        this.shape = Shape.check(shape);
         this.number = number;
-    }
-
-    private String validate(String shape) {
-        if(!shape.equals("스페이드")){
-            throw new IllegalArgumentException("[ERROR] 카드의 모양은 스페이드, 다이아몬드, 하트, 클로버가 있습니다.");
-        }
-
-        if(!shape.equals("다이아몬드")){
-            throw new IllegalArgumentException("[ERROR] 카드의 모양은 스페이드, 다이아몬드, 하트, 클로버가 있습니다.");
-        }
-
-        if(!shape.equals("하트")){
-            throw new IllegalArgumentException("[ERROR] 카드의 모양은 스페이드, 다이아몬드, 하트, 클로버가 있습니다.");
-        }
-
-        if(!shape.equals("클로버")){
-            throw new IllegalArgumentException("[ERROR] 카드의 모양은 스페이드, 다이아몬드, 하트, 클로버가 있습니다.");
-        }
-        return shape;
     }
 }
--- src/test/java/policy/CardPolicyTest.java
+++ src/test/java/policy/CardPolicyTest.java
@@ -3,15 +3,16 @@ package policy;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CardPolicyTest {
-    @ParameterizedTest
+    @Test
     @DisplayName("카드의 모양이 맞지 않는다면 예외가 발생한다.")
-    @ValueSource(strings = {"스페이드", "다이아몬드", "하트", "클로버"})
-    void shapeTest(String shape) {
+    void shapeTest() {
         //given
+        String shape = "스페이트";
         String number = "1";
 
         //when-then
