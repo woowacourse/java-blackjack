270155e796e824486558651fd6c5fe47fd949c90
sonjh919 <kingjh1125@gmail.com>
2025-03-04 16:34:00 +0900

    test(red): 카드 모양 확인 기능 테스트 추가

--- src/test/java/policy/CardPolicy.java
+++ src/test/java/policy/CardPolicy.java
@@ -6,17 +6,19 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CardPolicy {
-    @Test
+    @ParameterizedTest
     @DisplayName("카드의 모양이 맞지 않는다면 예외가 발생한다.")
-    void shapeTest(){
+    @ValueSource(strings = {"스페이드", "다이아몬드", "하트", "클로버"})
+    void shapeTest(String shape){
         //given
-        String shape = "스페이드드";
         String number = "1";
 
         //when-then
-        assertThatThrownBy(() -> new Card(shape,number))
+        assertThatThrownBy(() -> new Card(shape, number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 카드의 모양은 스페이드, 다이아몬드, 하트, 클로버가 있습니다.");
     }
