7513cc39f7515380eeeb7c22d49f97b0ad1b086d
sonjh919 <kingjh1125@gmail.com>
2025-03-04 17:06:52 +0900

    test(red): 카드 모양 확인 테스트 추가

--- src/test/java/policy/ShapeTest.java
+++ src/test/java/policy/ShapeTest.java
@@ -1,18 +1,17 @@
 package policy;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static policy.Shape.SPADE;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class ShapeTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"스페이드"})
+    @CsvSource(value = {"스페이드:SPADE", "다이아몬드:DIAMOND", "하트:HEART", "클로버:CLOVER"}, delimiter = ':')
     @DisplayName("문양 확인 테스트")
-    void shapeTest(String shape) {
-        assertThat(Shape.check(shape)).isEqualTo(SPADE);
+    void shapeTest(String input, Shape shape) {
+        assertThat(Shape.check(input)).isEqualTo(shape);
     }
 }
