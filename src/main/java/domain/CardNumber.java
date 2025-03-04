ef8cdcba8bff019f7ee99719a7003575aa4cf02c
ppparkta <pppppark2@gmail.com>
2025-03-04 17:02:30 +0900

    test(red): 카드 숫자 선택 테스트

---
+++ src/main/java/domain/CardNumber.java
@@ -0,0 +1,30 @@
+package domain;
+
+public enum CardNumber {
+    ACE(1, 1),
+    TWO(2, 2),
+    THREE(3, 3),
+    FOUR(4, 4),
+    FIVE(5, 5),
+    SIX(6, 6),
+    SEVEN(7, 7),
+    EIGHT(8, 8),
+    NINE(9, 9),
+    TEN(10, 10),
+    J(11, 10),
+    Q(12, 10),
+    K(13, 10);
+
+    private final int index;
+    private final int value;
+
+    CardNumber(int index, int value) {
+        this.index = index;
+        this.value = value;
+    }
+
+    public static CardNumber pick(int oneIndex) {
+        return null;
+    }
+
+}
---
+++ src/test/java/CardNumberTest.java
@@ -0,0 +1,20 @@
+import domain.CardNumber;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CardNumberTest {
+    @DisplayName("열 세개의 숫자 중 하나의 숫자를 뽑는다.")
+    @Test
+    void test(){
+
+        // given
+        int oneIndex = 1;
+
+        // when
+        CardNumber cardNumber = CardNumber.pick(oneIndex);
+
+        // then
+        Assertions.assertThat(cardNumber).isEqualTo(CardNumber.Q);
+    }
+}
