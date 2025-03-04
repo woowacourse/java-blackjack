ceb9653a4c29acc110a0116fa440c4c944035ca3
sonjh919 <kingjh1125@gmail.com>
2025-03-04 16:26:24 +0900

    test(red): 카드 모양 확인 테스트 작성

---
+++ src/test/java/policy/CardPolicy.java
@@ -0,0 +1,24 @@
+package policy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CardPolicy {
+    @Test
+    @DisplayName("카드의 모양이 맞지 않는다면 예외가 발생한다.")
+    void shapeTest(){
+        //given
+        String shape = "스페이드";
+        String number = "1";
+
+        //when-then
+        assertThatThrownBy(() -> new Card(shape,number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 카드의 모양은 스페이드, 다이아몬드, 하트, 클로버가 있습니다.");
+    }
+
+}
