ffcc9d547d2247edfc26932d1b14a81d3c71c95f
ppparkta <pppppark2@gmail.com>
2025-03-04 16:25:13 +0900

    test(red): 유저 중복 불가 테스트

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,9 +1,9 @@
-import domain.User;
 import domain.UserPick;
 import java.util.List;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -22,4 +22,34 @@ public class UserTest {
                 Arguments.arguments(List.of("수양"))
         );
     }
+
+    @DisplayName("등록한 유저가 기준보다 적거나 많으면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @MethodSource("userExceptionTestCase")
+    void test2(List<String> names) {
+        Assertions.assertThatThrownBy(() -> new UserPick(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유저는 1명 이상 7명 이하로 등록해야 합니다.");
+    }
+
+    private static Stream<Arguments> userExceptionTestCase() {
+        return Stream.of(
+                Arguments.arguments(List.of("수양", "레몬", "키키", "나나", "모모", "부부", "롸롸", "뫄뫄")),
+                Arguments.arguments(List.of())
+        );
+    }
+
+    @Test
+    @DisplayName("유저는 중복될 수 없다.")
+    void test3() {
+
+        List<String> names = List.of("수양", "레몬", "수양", "레몬", "모모", "부부", "롸롸", "뫄뫄");
+        Assertions.assertThatThrownBy(() -> new UserPick(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유저는 중복될 수 없습니다.");
+    }
+
+
+
+
 }
