2163f65fb9d27206663c7d53ba606c1dd38487b9
byesol <bsj5967@naver.com>
2025-03-04 17:03:25 +0900

    feat : bust여부를 판단하는 메소드 작성

--- src/test/java/blackjack/model/ReceivedCardsTest.java
+++ src/test/java/blackjack/model/ReceivedCardsTest.java
@@ -18,4 +18,33 @@ public class ReceivedCardsTest {
         // then
         assertThat(receivecdCards.size()).isEqualTo(2);
     }
+
+    @Test
+    void 보유한_카드의_점수를_계산한다() {
+        // given
+        ReceivedCards receivecdCards = new ReceivedCards();
+
+        // when
+        receivecdCards.receive(new NormalCard(2, CardShape.CLOVER));
+        receivecdCards.receive(new NormalCard(2, CardShape.HEART));
+        receivecdCards.receive(new SpecialCard('J', CardShape.HEART));
+        receivecdCards.receive(new AceCard(CardShape.HEART));
+
+        // then
+        assertThat(receivecdCards.calculateTotalPoint()).isEqualTo(15);
+    }
+
+    @Test
+    void 보유한_카드_점수의_합이_21_초과면_버스트다() {
+        // given
+        ReceivedCards receivecdCards = new ReceivedCards();
+
+        // when
+        receivecdCards.receive(new NormalCard(2, CardShape.CLOVER));
+        receivecdCards.receive(new SpecialCard('J', CardShape.HEART));
+        receivecdCards.receive(new SpecialCard('J', CardShape.HEART));
+
+        // then
+        assertThat(receivecdCards.isBust()).isTrue();
+    }
 }
