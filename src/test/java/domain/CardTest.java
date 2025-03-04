cfcb64cfe8e915d97f25f264d6681efaa6b1addb
ryan-dia <cjfdnjs158@naver.com>
2025-03-04 16:58:33 +0900

    test(CardTest): 카드 점수 반환 실패하는 테스트 작성
    Co-authored-by:cheche903<cheche903@naver.com>

---
+++ src/test/java/domain/CardTest.java
@@ -0,0 +1,24 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CardTest {
+
+    @DisplayName("주어진 카드의 점수를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "SPADE,ACE,1", "SPADE,THREE,3", "SPADE,JACK,10"
+    })
+    void 주어진_카드의_점수를_반환한다(Shape shape, Rank rank, int expected) {
+
+        // given
+        Card card = new Card(rank, shape);
+
+        // when & then
+        Assertions.assertThat(card.getScore()).isEqualTo(expected);
+
+    }
+}
