afd50b0816af9fca5bdc1a675fdcc5a7cdf1a6aa
ppparkta <pppppark2@gmail.com>
2025-03-04 17:20:13 +0900

    feat(green): 카드 생성자 로직 추가

--- src/main/java/domain/TrumpCard.java
+++ src/main/java/domain/TrumpCard.java
@@ -1,19 +1,21 @@
 package domain;
 
 public class TrumpCard {
+    private final NumberGenerator numberGenerator;
     private final CardShape cardShape;
     private final CardNumber cardNumber;
 
     public TrumpCard(NumberGenerator numberGenerator) {
-        this.cardShape = null;
-        this.cardNumber = null;
+        this.numberGenerator = numberGenerator;
+        this.cardShape = CardShape.pickCardShape(numberGenerator.pickRangeOfNumber(1, 4));
+        this.cardNumber = CardNumber.pick(numberGenerator.pickRangeOfNumber(1, 13));
     }
 
     public CardShape getCardShape() {
-        return null;
+        return cardShape;
     }
 
     public CardNumber getCardNumber() {
-        return null;
+        return cardNumber;
     }
 }
---
+++ src/test/java/TrumpCardTest.java
@@ -0,0 +1,18 @@
+import domain.CardNumber;
+import domain.CardShape;
+import domain.TrumpCard;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TrumpCardTest {
+
+    @DisplayName("카드 숫자와 카드 모양을 조합하여 카드를 생성한다.")
+    @Test
+    public void test1() {
+        TrumpCard trumpCard = new TrumpCard(new TestNumberGenerator());
+
+        Assertions.assertThat(trumpCard.getCardShape()).isEqualTo(CardShape.CLOVER);
+        Assertions.assertThat(trumpCard.getCardNumber()).isEqualTo(CardNumber.K);
+    }
+}
