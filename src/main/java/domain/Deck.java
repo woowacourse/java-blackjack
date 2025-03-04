a9e73a06871cd7f9d199db34f29d2484168c5483
changuii <rhljh7410@gmail.com>
2025-03-04 15:17:11 +0900

    feat(Deck): Deck에 동일한 카드가 들어오면, 예외가 발생한다.

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -7,7 +7,14 @@ public class Deck {
     private final Queue<Card> deck;
 
     public Deck(final Queue<Card> deck) {
+        validateDuplicateCard(deck);
         this.deck = new ArrayDeque<>(deck);
+    }
+
+    private void validateDuplicateCard(final Queue<Card> deck) {
+        if (deck.stream().distinct().count() != deck.size()) {
+            throw new IllegalArgumentException("덱에는 중복된 카드가 들어올 수 없습니다.!");
+        }
     }
 
     public Card pickCard() {
