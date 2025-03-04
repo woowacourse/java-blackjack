dd65ac00cc590c27c59e8c8058c2f4f2dc70f310
jhpark1227 <junhyeok0246@naver.com>
2025-03-04 15:29:13 +0900

    🔴red: 2 ~ 10 카드 숫자 반환 테스트 케이스 추가

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,21 +1,22 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CardTest {
 
     @DisplayName("카드의 숫자를 반환한다")
-    @Test
-    void test() {
+    @ParameterizedTest
+    @ValueSource(strings = {"2", "3", "4", "5", "6", "7", "8", "9", "10"})
+    void test(String value) {
         //given
-        Card card1 = new Card("2", "spade");
+        Card card1 = new Card(value, "spade");
         //when
         int number = card1.getNumber();
         //then
-        assertThat(number).isEqualTo(2);
+        assertThat(number).isEqualTo(Integer.parseInt(value));
     }
 
     @DisplayName("Q, K, J는 10을 반환한다")
