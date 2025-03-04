5920720d0e693362c6786940d4dc7a2ef8cec39f
taek2222 <oot309@daum.net>
2025-03-04 16:35:04 +0900

    test: 카드팩 생성 테스트 추가

--- src/test/java/BlackjackTest.java
+++ src/test/java/BlackjackTest.java
@@ -1,8 +1,11 @@
 import blackjack.domain.Card;
 import blackjack.domain.CardNumber;
+import blackjack.domain.CardPack;
 import blackjack.domain.CardShape;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,5 +26,18 @@ public class BlackjackTest {
                 () -> assertThat(card.getNumber()).isEqualTo(8),
                 () -> assertThat(card.getShape()).isEqualTo(CardShape.CLOVER)
         );
+    }
+
+    @Test
+    @DisplayName("카드팩 객체에 52장의 카드를 생성한다")
+    void 카드팩_객체에_52장의_카드를_생성한다() {
+        // given
+        CardPack cardPack = new CardPack();
+
+        // when
+        List<Card> cards = cardPack.getCards();
+
+        // then
+        assertThat(cards.size()).isEqualTo(52);
     }
 }
