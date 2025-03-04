345bdb55bf1dfc9792f8f2bd87d1e20606704be3
ryan-dia <cjfdnjs158@naver.com>
2025-03-04 17:33:14 +0900

    feat(Nickname): 닉네임 유효성 검사 기능 추가
    Co-authored-by:cheche903<cheche903@naver.com>

---
+++ src/main/java/domain/Nickname.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class Nickname {
+
+    private final String displayName;
+
+    public Nickname(final String displayName) {
+        validateNaming(displayName);
+        this.displayName = displayName;
+    }
+
+    private void validateNaming(final String displayName) {
+        String regex = "^[a-zA-Z]{2,5}$";
+        if (displayName.matches(regex)) {
+            return;
+        }
+        throw new IllegalArgumentException("이름은 영어로만 작성이 가능하며 2~5글자여야합니다.");
+    }
+}
