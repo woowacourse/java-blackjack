db3bdfbd9cfc8a0735f0ffddb8078f691c289b02
7i.w__k <tizmfns1218@Naver.com>
2025-03-04 17:06:36 +0900

    feat: 카드 객체 추가
    Co-authored-by: Jin409 <sualng123@gmail.com>

---
+++ src/main/java/Card.java
@@ -0,0 +1,30 @@
+import common.CardType;
+import java.util.Objects;
+
+public class Card {
+    private final CardType cardType;
+    private final int value;
+
+    public Card(CardType cardType, int value) {
+        this.cardType = cardType;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return value == card.value && cardType == card.cardType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardType, value);
+    }
+}
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,3 +1,5 @@
+import common.CardType;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -6,7 +8,24 @@ class CardTest {
     @DisplayName("객체 생성 테스트")
     @Test
     void test1() {
+        // given & when
+        Card card = new Card(CardType.ACE, 1);
 
+        // then
+        Assertions.assertThat(card.getValue()).isEqualTo(1);
     }
 
+    @DisplayName("값과 타입으로 동일한 객체인지 확인한다")
+    @Test
+    void test2() {
+        // given
+        Card card = new Card(CardType.ACE, 1);
+        Card comparedCard = new Card(CardType.ACE, 1);
+
+        // when
+        boolean actual = card.equals(comparedCard);
+
+        // then
+        Assertions.assertThat(actual).isTrue();
+    }
 }
