181b80cb25e571e0e6c82860c42a04aaf9132c2e
changuii <rhljh7410@gmail.com>
2025-03-04 15:15:53 +0900

    test(DeckTest): Deck에 동일한 카드가 들어오면, 예외가 발생한다.

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.Card;
 import domain.Deck;
@@ -9,6 +10,25 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class DeckTest {
+
+
+    @Nested
+    @DisplayName("덱 생성")
+    class CreateDeck {
+
+        @DisplayName("Deck에 동일한 카드가 들어오면, 예외가 발생한다.")
+        @Test
+        public void validateDuplicate() throws Exception {
+            // given
+            final Card club = new Card(1, "club");
+            final var q = new ArrayDeque<>(List.of(club, club));
+
+            // when & then
+            assertThatThrownBy(() -> new Deck(q))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+    }
 
     @Nested
     @DisplayName("카드를 순서대로 뽑는다.")
