86508f60460430be2907e8d4b8ac8bcd0382ef66
황재웅 <wodnd0131@naver.com>
2025-03-04 15:54:29 +0900

    feat(Dealer): Dealer는 카드를 뽑아라

---
+++ src/main/java/domain/Dealer.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public record Dealer(List<Card> hand) {
+
+    public Dealer(){
+        this(new ArrayList<>());
+    }
+
+    public void pickUpCard(Deck deck) {
+        var card = deck.pickCard();
+        hand.add(card);
+    }
+}
