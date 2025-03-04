cd42c0d2f20ef4df38658534b3f4212d1f97c2a8
byesol <bsj5967@naver.com>
2025-03-04 15:22:39 +0900

    feat: 스페셜 카드를 나타내는 도메인 작성

---
+++ src/main/java/blackjack/model/SpecialCard.java
@@ -0,0 +1,13 @@
+package blackjack.model;
+
+public class SpecialCard {
+    private final String character;
+
+    public SpecialCard(String character) {
+        this.character = character;
+    }
+
+    public String getCharacter() {
+        return character;
+    }
+}
