bf0219840729809afca042adc46336e26c755aad
jhpark1227 <junhyeok0246@naver.com>
2025-03-04 15:56:54 +0900

    🛠️refactor: 숫자 결정 책임 삭제 및 카드 숫자 범위 정의

--- src/test/java/Card.java
+++ src/test/java/Card.java
@@ -1,8 +1,9 @@
 import java.util.List;
 
 public class Card {
-    private final List<String> numbers = List.of("2", "3", "4", "5", "6", "7", "8", "9", "10");
-    private final List<String> specialNumbers = List.of("Q", "K", "J");
+
+    private static final List<String> numbers = List.of("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "K",
+            "J");
 
     private final String value;
     private final String type;
@@ -13,15 +14,8 @@ public class Card {
         this.type = type;
     }
 
-    public int getNumber() {
-        if (specialNumbers.contains(value)) {
-            return 10;
-        }
-        return Integer.parseInt(value);
-    }
-
     private void validate(String value) {
-        if (!(numbers.contains(value) || specialNumbers.contains(value))) {
+        if (!numbers.contains(value)) {
             throw new IllegalArgumentException("카드 숫자는 2 ~ 10 사이 숫자만 가능합니다.");
         }
     }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,36 +1,10 @@
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CardTest {
-
-    @DisplayName("카드의 숫자를 반환한다")
-    @ParameterizedTest
-    @ValueSource(strings = {"2", "3", "4", "5", "6", "7", "8", "9", "10"})
-    void test(String value) {
-        //given
-        Card card1 = new Card(value, "spade");
-        //when
-        int number = card1.getNumber();
-        //then
-        assertThat(number).isEqualTo(Integer.parseInt(value));
-    }
-
-    @DisplayName("Q, K, J는 10을 반환한다")
-    @ParameterizedTest
-    @CsvSource({"Q", "K", "J"})
-    void 문자로_설정된_카드의_숫자를_반환한다(String value) {
-        //given
-        Card card1 = new Card(value, "spade");
-        //when
-        final int number = card1.getNumber();
-        //then
-        assertThat(number).isEqualTo(10);
-    }
 
     @DisplayName("2 ~ 10 범위에 해당하지 않는 값의 카드는 생성할 수 없다.")
     @ParameterizedTest
