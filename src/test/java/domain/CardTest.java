8cb66ad91c25cff59905a9d6df7e7634e90416f1
bowook <mbosx316@naver.com>
2025-03-04 15:58:55 +0900

    test(CardTest) : 카드 생성 테스트 (RED)

---
+++ src/test/java/domain/CardTest.java
@@ -0,0 +1,18 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CardTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, SPADE", "2, CLOVER", "3, HEART", "4, DIAMOND"})
+    void 카드_생성_테스트(int number, CardType cardType) {
+        //when
+        Card card = new Card(number, cardType);
+
+        //then
+        Assertions.assertThat(card).isInstanceOf(Card.class);
+    }
+}
