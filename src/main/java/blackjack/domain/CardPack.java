42e6d94d7af26b3fd5d79434635b30d093749366
taek2222 <oot309@daum.net>
2025-03-04 16:39:09 +0900

    refactor: 카드팩 생성 로직 메서드 분리

--- src/main/java/blackjack/domain/CardPack.java
+++ src/main/java/blackjack/domain/CardPack.java
@@ -9,20 +9,24 @@ public class CardPack {
     private final List<Card> cards;
 
     public CardPack() {
-        List<Card> cards = new ArrayList<>();
-
-        Arrays.stream(CardShape.values())
-                .forEach(shape -> {
-                    Arrays.stream(CardNumber.values())
-                            .forEach(number -> {
-                                cards.add(new Card(number, shape));
-                            });
-                });
-
-        this.cards = cards;
+        this.cards = initCards();
     }
 
     public List<Card> getCards() {
         return cards;
+    }
+
+    private List<Card> initCards() {
+        List<Card> cards = new ArrayList<>();
+        Arrays.stream(CardShape.values())
+                .forEach(shape -> makeCard(shape, cards));
+        return cards;
+    }
+
+    private void makeCard(final CardShape shape, final List<Card> cards) {
+        Arrays.stream(CardNumber.values())
+                .forEach(number -> {
+                    cards.add(new Card(number, shape));
+                });
     }
 }
