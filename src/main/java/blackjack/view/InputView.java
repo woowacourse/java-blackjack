bfc0cc9730ebf6a451b781a7b636446722e8fafd
MinJun Choi <mj043000@naver.com>
2025-03-04 17:10:11 +0900

    refactor: 상수 추출

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     private final String TITLE_INPUT_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
 
     public String readNames() {
@@ -12,7 +14,6 @@ public class InputView {
     }
 
     private String readLine() {
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 }
