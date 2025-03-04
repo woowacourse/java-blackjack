6e3fa640e63c65dcf908b316ecc1bdd96e91fb8e
yeonnhuu <yeonnhuu@gmail.com>
2025-03-04 17:16:28 +0900

    :sparkles: feat: `Card` 점수 계산 기능 추가

---
+++ src/main/java/domain/card/Card.java
@@ -0,0 +1,16 @@
+package domain.card;
+
+public class Card {
+
+    private final Rank rank;
+    private final Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public int calculateScore() {
+        return rank.getValue();
+    }
+}
