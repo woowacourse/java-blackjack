0708393ec419b751ffe0e2c3dd28e5dda67b6d41
sonjh919 <kingjh1125@gmail.com>
2025-03-04 17:18:22 +0900

    test(red): 숫자 정책 테스트

--- src/test/java/policy/CardPolicyTest.java
+++ src/test/java/policy/CardPolicyTest.java
@@ -13,12 +13,25 @@ public class CardPolicyTest {
     void shapeTest() {
         //given
         String shape = "스페이트";
-        String number = "1";
+        String number = "2";
 
         //when-then
         assertThatThrownBy(() -> new Card(shape, number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 카드의 모양은 스페이드, 다이아몬드, 하트, 클로버가 있습니다.");
+    }
+
+    @Test
+    @DisplayName("정책에 벗어난 숫자는 예외가 발생한다.")
+    void numberTest() {
+        // given
+        String shape = "스페이드";
+        String number = "1";
+
+        // when - then
+        assertThatThrownBy(() -> new Card(shape, number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 트럼프 카드에 맞는 숫자를 입력해 주세요.");
     }
 
 }
