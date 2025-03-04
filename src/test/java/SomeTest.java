fcc5d60eccf3edf171ac24f0e1373a07a7e2d959
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-04 17:01:35 +0900

    RED) test(domain) : 카드 생성 테스트

---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,17 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import domain.Denomination;
+import domain.Suit;
+
+import javax.smartcardio.Card;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SomeTest {
+
+    @Test
+    @DisplayName("카드는 모양과 끗수를 가진다")
+    public void test1() {
+        assertThat(new Card(Denomination.ACE, Suit.CLUB)).isInstanceOf(Card.class);
+    }
+    
+}
