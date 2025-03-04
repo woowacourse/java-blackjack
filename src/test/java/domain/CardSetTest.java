571450c8e4bb69e48233241e6d2a7713423fa7f0
bowook <mbosx316@naver.com>
2025-03-04 16:02:04 +0900

    test(CardSetTest) : 카드셋 생성 테스트 (RED)

---
+++ src/test/java/domain/CardSetTest.java
@@ -0,0 +1,21 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CardSetTest {
+
+    @Test
+    void 카드셋_생성_테스트() {
+        //given
+        List<Card> cards = new ArrayList<>();
+
+        //when
+        CardSet cardSet = new CardTest(cards);
+
+        //then
+        Assertions.assertThat(cardSet).isInstanceOf(CardSet.class);
+    }
+}
