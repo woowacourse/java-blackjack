0ac7bf8ce831f75b3b4377de427dc223ce69f66e
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-04 19:34:17 +0900

    refactor: deque로 변경

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -9,7 +9,8 @@ import domain.TrumpCard;
 import domain.strategy.BlackjackDrawStrategy;
 import domain.strategy.TestDrawStrategy;
 import except.BlackJackException;
-import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -20,22 +21,19 @@ public class DeckTest {
 
     @Test
     void 트럼프_카드를_덱에서_뽑을_수_있다() {
-        List<TrumpCard> trumpCards = new ArrayList<>(List.of(new TrumpCard(Suit.CLOVER, CardValue.EIGHT),
+        Deque<TrumpCard> trumpCards = new LinkedList<>(List.of(new TrumpCard(Suit.CLOVER, CardValue.EIGHT),
                 new TrumpCard(Suit.DIAMOND, CardValue.EIGHT)));
         Deck deck = new Deck(new BlackjackDeckGenerator(), new TestDrawStrategy(trumpCards));
-        assertThat(deck.drawCard())
-                .isEqualTo(trumpCards.get(0));
+        assertThat(deck.drawCard()).isEqualTo(trumpCards.getFirst());
     }
 
     @Test
     void 덱에서_카드가_없으면_예외가_발생한다() {
         Deck deck = new Deck(new BlackjackDeckGenerator(), new BlackjackDrawStrategy());
-
         for (int i = 0; i < 52; i++) {
             deck.drawCard();
         }
 
-        assertThatThrownBy(deck::drawCard)
-                .isInstanceOf(BlackJackException.class);
+        assertThatThrownBy(deck::drawCard).isInstanceOf(BlackJackException.class);
     }
 }
