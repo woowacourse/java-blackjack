7a4be06ff4387709944ff1ff632ed2c57f32a3c4
bowook <mbosx316@naver.com>
2025-03-04 15:24:28 +0900

    feat(Player) : 참가자 생성 성공 (GREEN)

---
+++ src/main/java/domain/participant/Player.java
@@ -0,0 +1,10 @@
+package domain.participant;
+
+public class Player {
+    private final String nickname;
+
+    public Player(final String nickname) {
+        this.nickname = nickname;
+    }
+
+}
--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import domain.participant.Player;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
