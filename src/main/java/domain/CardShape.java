f0c72b96acb916d557a0a72924b4429402836691
yush <yush2202@gmail.com>
2025-03-04 17:00:26 +0900

    feat: Card 구현

---
+++ src/main/java/domain/CardShape.java
@@ -0,0 +1,14 @@
+package domain;
+
+public enum CardShape {
+    DIA("다이아몬드"),
+    SPADE("스페이드"),
+    CLOVER("클로버"),
+    HEART("하트");
+
+    private final String name;
+
+    CardShape(String name) {
+        this.name = name;
+    }
+}
