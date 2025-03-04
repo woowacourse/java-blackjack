2381aafd1c9dba03a918d73f41bd34252c98a88e
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-04 17:55:24 +0900

    RED) test(domain) : ACE 초기값 11, ACE 초기값 변경 테스트 추가

--- src/test/java/SomeTest.java
+++ src/test/java/SomeTest.java
@@ -3,6 +3,7 @@ import static domain.Suit.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import domain.Ace;
 import domain.Card;
 import domain.Denomination;
 import domain.Suit;
@@ -24,19 +25,25 @@ public class SomeTest {
         @Test
         @DisplayName("카드는 고유의 숫자값을 갖는다.")
         public void test2() {
-            assertThat(new Card(TWO, CLUB)).extracting("value").isEqualTo(2);
+            assertThat(new Card(TWO, CLUB).getValue()).isEqualTo(2);
         }
 
         @Test
         @DisplayName("카드는 고유의 숫자값을 갖는다. - 2")
         public void test3() {
-            assertThat(new Card(THREE, CLUB)).extracting("value").isEqualTo(3);
+            assertThat(new Card(THREE, CLUB).getValue()).isEqualTo(3);
         }
 
         @Test
         @DisplayName("카드는 고유의 숫자값을 갖는다. - 3")
         public void test4() {
-            assertThat(new Card(JACK, CLUB)).extracting("value").isEqualTo(10);
+            assertThat(new Card(JACK, CLUB).getValue()).isEqualTo(10);
+        }
+
+        @Test
+        @DisplayName("ACE의 초기 숫자값은 11이다.")
+        public void test5() {
+            assertThat(new Card(ACE, CLUB).getValue()).isEqualTo(11);
         }
     }
 
@@ -49,6 +56,21 @@ public class SomeTest {
 
         assertAll(() -> assertThat(clubAce.isAce()).isTrue(),
                 () -> assertThat(clubTwo.isAce()).isFalse());
+    }
+
+
+
+    @Test
+    @DisplayName("ACE는 고유의 숫자값을 1로 바꿀 수 있다.")
+    public void test2() {
+        // given
+        Ace clubAce = new Ace(ACE, CLUB);
+
+        // when
+        clubAce.setValueToOne();
+
+        // then
+        assertThat(clubAce.getValue()).isEqualTo(1);
     }
 
 }
