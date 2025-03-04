a90243b135bf325bfa6c7217a5b28f502606e934
gabean13 <choegabin171@gmail.com>
2025-03-04 17:30:36 +0900

    feat: 카드 모델 구현
    - 카드는 모양과 숫자를 가집니다.
    
    Co-authored-by: YehyeokBang <qkddpgur318@gmail.com>

---
+++ src/main/java/blackjack/Card.java
@@ -0,0 +1,48 @@
+package blackjack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Card {
+
+    private final Suit suit;
+    private final CardValue cardValue;
+
+    public Card(Suit suit, CardValue cardValue) {
+        this.suit = suit;
+        this.cardValue = cardValue;
+    }
+
+    public static List<Card> createDeck() {
+        List<Card> deck = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            addCardsForSuit(suit, deck);
+        }
+        return deck;
+    }
+
+    private static void addCardsForSuit(Suit suit, List<Card> deck) {
+        for (CardValue cardValue : CardValue.values()) {
+            deck.add(new Card(suit, cardValue));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return suit == card.suit
+                && cardValue == card.cardValue;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(suit, cardValue);
+    }
+}
---
+++ src/main/java/blackjack/Suit.java
@@ -0,0 +1,16 @@
+package blackjack;
+
+public enum Suit {
+
+    HEARTS("하트"),
+    SPADES("스페이드"),
+    CLUBS("클로버"),
+    DIAMONDS("다이아몬드"),
+    ;
+
+    private final String label;
+
+    Suit(String label) {
+        this.label = label;
+    }
+}
