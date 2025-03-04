3e5f3dd5e927c3ddc6520d64ac7d139fde9a5fa0
songsunkook <songsunkook@gmail.com>
2025-03-04 16:43:12 +0900

    feat: J, Q, K는 각각 10으로 계산한다

--- src/test/java/BlackjackTest.java
+++ src/test/java/BlackjackTest.java
@@ -3,11 +3,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BlackjackTest {
+class BlackjackTest {
 
     @Test
     @DisplayName("카드는 4가지 문양을 가질 수 있다")
     void cardTypeTest() {
         assertThat(CardType.values()).hasSize(4);
+    }
+
+    @Test
+    @DisplayName("J, Q, K는 각각 10으로 계산한다")
+    void cardNumberTest() {
+        assertThat(CardNumber.JACK.getNumber()).isEqualTo(10);
+        assertThat(CardNumber.QUEEN.getNumber()).isEqualTo(10);
+        assertThat(CardNumber.KING.getNumber()).isEqualTo(10);
     }
 }
