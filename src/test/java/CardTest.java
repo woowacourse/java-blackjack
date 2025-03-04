74ce9d67d9f57144ca1c2ef09ea7fc2279ca992b
jhpark1227 <junhyeok0246@naver.com>
2025-03-04 15:18:01 +0900

    🔴red: Q, K, J는 10으로 계산하는 규칙 테스트

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -2,6 +2,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CardTest {
 
@@ -14,5 +16,17 @@ public class CardTest {
         int number = card1.getNumber();
         //then
         assertThat(number).isEqualTo(2);
+    }
+
+    @DisplayName("Q, K, J는 10을 반환한다")
+    @ParameterizedTest
+    @CsvSource({"Q", "K", "J"})
+    void 문자로_설정된_카드의_숫자를_반환한다(String value) {
+        //given
+        Card card1 = new Card(value, "spade");
+        //when
+        final int number = card1.getNumber();
+        //then
+        assertThat(number).isEqualTo(10);
     }
 }
