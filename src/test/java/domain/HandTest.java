0dc70795431747c7855564bae96de063ee4bf928
ryan-dia <cjfdnjs158@naver.com>
2025-03-04 17:06:30 +0900

    test(HandTest): 손에 있는 카드 숫자 합 실패하는 테스트 작성
    Co-authored-by:cheche903<cheche903@naver.com>

--- src/test/java/domain/HandTest.java
+++ src/test/java/domain/HandTest.java
@@ -1,28 +1,53 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class HandTest {
+
+    List<Card> cards;
+    Card card1;
+    Card card2;
+    Hand hand;
+
+    @BeforeEach
+    void setUp() {
+        cards = new ArrayList<>();
+        card1 = new Card(Rank.ACE, Shape.CLOVER);
+        card2 = new Card(Rank.FIVE, Shape.CLOVER);
+        hand = new Hand(cards);
+    }
 
     @DisplayName("카드를 손에 추가한다.")
     @Test
     void 카드를_손에_추가한다() {
 
         // given
-        final List<Card> cards = new ArrayList<>();
-        final Card card1 = new Card(Rank.ACE, Shape.CLOVER);
-        final Card card2 = new Card(Rank.FIVE, Shape.CLOVER);
-        final Hand hand = new Hand(cards);
 
         // when & then
         assertThatCode(() -> {
             hand.add(card1);
             hand.add(card2);
         }).doesNotThrowAnyException();
+    }
+
+    @DisplayName("손에 있는 카드의 합을 가져온다.")
+    @Test
+    void 손에_있는_카드의_합을_가져온다() {
+
+        // given
+
+        // when
+        hand.add(card1);
+        hand.add(card2);
+
+        // then
+        assertThat(hand.getSumOfRank()).isEqualTo(6);
     }
 }
