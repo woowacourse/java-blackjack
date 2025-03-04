a72a88429340ecae3901e219e44d58e00679d344
ryan-dia <cjfdnjs158@naver.com>
2025-03-04 15:45:22 +0900

    test(RankTest): 에이스 판별 메서드 실패하는 테스트 작성
    Co-authored-by:cheche903<cheche903@naver.com>

---
+++ src/test/java/domain/RankTest.java
@@ -0,0 +1,25 @@
+package domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class RankTest {
+
+    @DisplayName("랭크가 에이스면 true 아니면 false를 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "ACE, true", "THREE, false", "JACK, false"
+    })
+    void 랭크가_예이스면_true_아니면_false를_반환한다(Rank rank, boolean expected) {
+
+        // given
+
+        // when & then
+        assertThat(rank.isAce()).isEqualTo(expected);
+    }
+
+}
