b98e2573b7dcddbab0b6963f304957b4a0b42a59
RIWON <121172065+awrion3@users.noreply.github.com>
2025-03-04 19:57:45 +0900

    feat(GameFinalResult): 딜러와 플레이어의 게임 최종 결과를 계산하는 기능 구현

---
+++ src/main/java/blackjack/domain/GameFinalResult.java
@@ -0,0 +1,26 @@
+package blackjack.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GameFinalResult {
+    private final GameRule gameRule;
+
+    public GameFinalResult(GameRule gameRule) {
+        this.gameRule = gameRule;
+    }
+
+    public Map<GameResult, Integer> getDealerFinalResult(Dealer dealer, List<Player> players) {
+        Map<GameResult, Integer> gameFinalResult = new HashMap<>();
+        for (Player player : players) {
+            GameResult result = gameRule.evaluateDealerWin(player, dealer);
+            gameFinalResult.put(result, gameFinalResult.getOrDefault(result, 0) + 1);
+        }
+        return gameFinalResult;
+    }
+
+    public GameResult getGameResultFromPlayer(Player player, Dealer dealer) {
+        return gameRule.evaluateDealerWin(player, dealer);
+    }
+}
