28fb8efb824198caea675e91032bbf3863ecb89c
2jin2.1031 <2jin2.1031@gmail.com>
2025-03-05 19:57:06 +0900

    refactor: RankType enum 생성자 추가

--- src/main/java/model/RankType.java
+++ src/main/java/model/RankType.java
@@ -4,22 +4,27 @@ import java.util.List;
 
 public enum RankType {
 
-    TWO("2",List.of(2)),
-    THREE("3",List.of(3)),
-    FOUR("4",List.of(4)),
-    FIVE("5",List.of(5)),
-    SIX("6",List.of(6)),
-    SEVEN("7",List.of(7)),
-    EIGHT("8",List.of(8)),
-    NINE("9",List.of(9)),
-    TEN("10",List.of(10)),
-    KING("K",List.of(10)),
-    JACK("J",List.of(10)),
-    QUEEN("Q",List.of(10)),
+    TWO("2",2),
+    THREE("3",3),
+    FOUR("4",4),
+    FIVE("5",5),
+    SIX("6",6),
+    SEVEN("7",7),
+    EIGHT("8",8),
+    NINE("9",9),
+    TEN("10",10),
+    KING("K",10),
+    JACK("J",10),
+    QUEEN("Q",10),
     ACE("A",List.of(1,11));
 
     private final String displayName;
     private final List<Integer> score;
+
+    RankType(String displayName, int score) {
+        this.displayName = displayName;
+        this.score = List.of(score);
+    }
 
     RankType(String displayName, List<Integer> score) {
         this.displayName = displayName;
