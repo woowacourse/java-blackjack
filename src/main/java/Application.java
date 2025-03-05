8192659326666acf5f0917155da89389dd6ad866
soeun2537 <soeun2537@gmail.com>
2025-03-05 11:56:02 +0900

    feat: Application 클래스 추가

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import controller.BlackjackController;
+
+public class Application {
+
+    public static void main(String[] args) {
+        BlackjackController blackjackController = new BlackjackController();
+        blackjackController.run();
+    }
+}
