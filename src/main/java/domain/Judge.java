e9e868c8058c114d956a65819730344ec81e2a0d
supernovaMK <minkikim2002@gmail.com>
2025-03-05 11:43:50 +0900

    feat: 블랙잭에 필요한 심판 추가

---
+++ src/main/java/domain/Judge.java
@@ -0,0 +1,21 @@
+package domain;
+
+public class Judge {
+
+    private Deck deck;
+
+    public Judge(Deck deck) {
+        validate(deck);
+        this.deck = deck;
+    }
+
+    private void validate(Deck deck) {
+        validateNotNull(deck);
+    }
+
+    private void validateNotNull(Deck deck) {
+        if (deck == null) {
+            throw new IllegalStateException("심판은 덱을 가지고 있어야합니다.");
+        }
+    }
+}
