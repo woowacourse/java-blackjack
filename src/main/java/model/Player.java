acfcd22c8389e330e74b13c5f13b185905b49cd2
민경윤 <ruddbs3308@naver.com>
2025-03-05 13:37:53 +0900

    feat(Player): 게임을 시작하면 플레이어는 두 장의 카드를 지급 받는다

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Player {
+
+    private final String name;
+    private final Cards cards;
+
+    public Player(final String name, final Cards cards) {
+        this.name = name;
+        this.cards = cards;
+    }
+}
