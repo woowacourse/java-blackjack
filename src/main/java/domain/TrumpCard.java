730386dc2fd7076a1c3c5f4b9b4ff2adaf8ae285
jeyong <joon6093@naver.com>
2025-03-05 11:08:23 +0900

    feat: 블랙잭에 필요한 카드 추가

---
+++ src/main/java/domain/TrumpCard.java
@@ -0,0 +1,67 @@
+package domain;
+
+public enum TrumpCard {
+    ACE_OF_SPADES(Suit.SPADES, Rank.ACE),
+    TWO_OF_SPADES(Suit.SPADES, Rank.TWO),
+    THREE_OF_SPADES(Suit.SPADES, Rank.THREE),
+    FOUR_OF_SPADES(Suit.SPADES, Rank.FOUR),
+    FIVE_OF_SPADES(Suit.SPADES, Rank.FIVE),
+    SIX_OF_SPADES(Suit.SPADES, Rank.SIX),
+    SEVEN_OF_SPADES(Suit.SPADES, Rank.SEVEN),
+    EIGHT_OF_SPADES(Suit.SPADES, Rank.EIGHT),
+    NINE_OF_SPADES(Suit.SPADES, Rank.NINE),
+    TEN_OF_SPADES(Suit.SPADES, Rank.TEN),
+    JACK_OF_SPADES(Suit.SPADES, Rank.JACK),
+    QUEEN_OF_SPADES(Suit.SPADES, Rank.QUEEN),
+    KING_OF_SPADES(Suit.SPADES, Rank.KING),
+
+    ACE_OF_HEARTS(Suit.HEARTS, Rank.ACE),
+    TWO_OF_HEARTS(Suit.HEARTS, Rank.TWO),
+    THREE_OF_HEARTS(Suit.HEARTS, Rank.THREE),
+    FOUR_OF_HEARTS(Suit.HEARTS, Rank.FOUR),
+    FIVE_OF_HEARTS(Suit.HEARTS, Rank.FIVE),
+    SIX_OF_HEARTS(Suit.HEARTS, Rank.SIX),
+    SEVEN_OF_HEARTS(Suit.HEARTS, Rank.SEVEN),
+    EIGHT_OF_HEARTS(Suit.HEARTS, Rank.EIGHT),
+    NINE_OF_HEARTS(Suit.HEARTS, Rank.NINE),
+    TEN_OF_HEARTS(Suit.HEARTS, Rank.TEN),
+    JACK_OF_HEARTS(Suit.HEARTS, Rank.JACK),
+    QUEEN_OF_HEARTS(Suit.HEARTS, Rank.QUEEN),
+    KING_OF_HEARTS(Suit.HEARTS, Rank.KING),
+
+    ACE_OF_DIAMONDS(Suit.DIAMONDS, Rank.ACE),
+    TWO_OF_DIAMONDS(Suit.DIAMONDS, Rank.TWO),
+    THREE_OF_DIAMONDS(Suit.DIAMONDS, Rank.THREE),
+    FOUR_OF_DIAMONDS(Suit.DIAMONDS, Rank.FOUR),
+    FIVE_OF_DIAMONDS(Suit.DIAMONDS, Rank.FIVE),
+    SIX_OF_DIAMONDS(Suit.DIAMONDS, Rank.SIX),
+    SEVEN_OF_DIAMONDS(Suit.DIAMONDS, Rank.SEVEN),
+    EIGHT_OF_DIAMONDS(Suit.DIAMONDS, Rank.EIGHT),
+    NINE_OF_DIAMONDS(Suit.DIAMONDS, Rank.NINE),
+    TEN_OF_DIAMONDS(Suit.DIAMONDS, Rank.TEN),
+    JACK_OF_DIAMONDS(Suit.DIAMONDS, Rank.JACK),
+    QUEEN_OF_DIAMONDS(Suit.DIAMONDS, Rank.QUEEN),
+    KING_OF_DIAMONDS(Suit.DIAMONDS, Rank.KING),
+
+    ACE_OF_CLUBS(Suit.CLUBS, Rank.ACE),
+    TWO_OF_CLUBS(Suit.CLUBS, Rank.TWO),
+    THREE_OF_CLUBS(Suit.CLUBS, Rank.THREE),
+    FOUR_OF_CLUBS(Suit.CLUBS, Rank.FOUR),
+    FIVE_OF_CLUBS(Suit.CLUBS, Rank.FIVE),
+    SIX_OF_CLUBS(Suit.CLUBS, Rank.SIX),
+    SEVEN_OF_CLUBS(Suit.CLUBS, Rank.SEVEN),
+    EIGHT_OF_CLUBS(Suit.CLUBS, Rank.EIGHT),
+    NINE_OF_CLUBS(Suit.CLUBS, Rank.NINE),
+    TEN_OF_CLUBS(Suit.CLUBS, Rank.TEN),
+    JACK_OF_CLUBS(Suit.CLUBS, Rank.JACK),
+    QUEEN_OF_CLUBS(Suit.CLUBS, Rank.QUEEN),
+    KING_OF_CLUBS(Suit.CLUBS, Rank.KING);
+
+    private final Suit suit;
+    private final Rank rank;
+
+    TrumpCard(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+}
