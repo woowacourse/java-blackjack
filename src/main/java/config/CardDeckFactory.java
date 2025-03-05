33a3da4c1499674d4ef321735f0921e9acf12995
sonjh919 <kingjh1125@gmail.com>
2025-03-05 10:56:08 +0900

    test(red): 카드 생성 테스트 추가

---
+++ src/main/java/config/CardDeckFactory.java
@@ -0,0 +1,9 @@
+package config;
+
+import domain.card.CardDeck;
+
+public class CardDeckFactory {
+    public CardDeck create() {
+        return null;
+    }
+}
---
+++ src/test/java/CardDeckFactoryTest.java
@@ -0,0 +1,19 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import config.CardDeckFactory;
+import domain.card.CardDeck;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CardDeckFactoryTest {
+    @Test
+    @DisplayName("카드 덱 생성 테스트")
+    void createCardDeckTest(){
+        //given
+        CardDeckFactory cardDeckFactory = new CardDeckFactory();
+
+        assertThat(cardDeckFactory.create()).isInstanceOf(CardDeck.class);
+    }
+
+
+}
