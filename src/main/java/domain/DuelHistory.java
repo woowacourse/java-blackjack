52db6b8c107e38d6c780c80bab0335dd0d708aab
changuii <rhljh7410@gmail.com>
2025-03-05 19:49:48 +0900

    test(DuelHistoryTest): 승패를 기록하라

---
+++ src/main/java/domain/DuelHistory.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class DuelHistory {
+	public void write(boolean duelResult) {
+	}
+
+	public int getWinCount() {
+		return -1;
+	}
+
+	public int getLoseCount() {
+		return -1;
+	}
+}
