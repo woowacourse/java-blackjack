280558e8950cecf94a84741db698bd5d6b87768b
bowook <mbosx316@naver.com>
2025-03-05 17:17:09 +0900

    test(GameBoardTest) : 카드 2장 나눠주기 테스트 (RED)

--- src/test/java/domain/GameBoardTest.java
+++ src/test/java/domain/GameBoardTest.java
@@ -1,8 +1,12 @@
 package domain;
 
 import domain.card.CardDeck;
+import domain.participant.Dealer;
+import domain.participant.Participant;
 import domain.participant.Player;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +24,27 @@ public class GameBoardTest {
 
         //then
         Assertions.assertThat(gameBoard).isInstanceOf(GameBoard.class);
+    }
+
+    @Test
+    void 카드를_모두에게_2장씩_나눠준다() {
+        //given
+        List<Participant> participants = List.of(
+                new Player("우가"),
+                new Player("히스타"),
+                new Dealer("딜러")
+        );
+        CardDeck cardDeck = CardDeck.generateFullSet();
+        GameBoard gameBoard = new GameBoard(cardDeck, participants);
+
+        //when
+        gameBoard.drawTwoCards();
+
+        Map<Participant, CardDeck> cardDeckOfParticipant = gameBoard.getCardDeckOfParticipant();
+
+        for(Map.Entry<Participant, CardDeck> entry : cardDeckOfParticipant.entrySet()) {
+            CardDeck ownedCardDeck = entry.getValue();
+            Assertions.assertThat(ownedCardDeck.getCards().size()).isEqualTo(2);
+        }
     }
 }
