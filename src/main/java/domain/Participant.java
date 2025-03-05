413222d412c5f6a4cd7522092cf4cfde43121542
changuii <rhljh7410@gmail.com>
2025-03-05 20:02:57 +0900

    test(Participant): 두 참가자의 승패를 결정하라

--- src/main/java/domain/Participant.java
+++ src/main/java/domain/Participant.java
@@ -1,29 +1,36 @@
 package domain;
 
 public abstract class Participant {
-    private final CardHand hand;
+	private final CardHand hand;
 
-    public Participant() {
-        hand = new CardHand();
-    }
+	public Participant() {
+		hand = new CardHand();
+	}
 
-    public Participant(final CardHand hand) {
-        this.hand = hand;
-    }
+	public Participant(final CardHand hand) {
+		this.hand = hand;
+	}
 
-    public abstract boolean isPickCard();
+	public abstract boolean isPickCard();
 
-    public void pickCard(final Deck deck) {
-        final Card card = deck.pickCard();
-        hand.add(card);
-    }
+	public void pickCard(final Deck deck) {
+		final Card card = deck.pickCard();
+		hand.add(card);
+	}
 
-    public int calculateAllScore() {
-        return hand.calculateAllScore();
-    }
+	public int calculateAllScore() {
+		return hand.calculateAllScore();
+	}
 
-    public CardHand getHand() {
-        return hand;
-    }
+	public CardHand getHand() {
+		return hand;
+	}
+
+	public void duel(final Participant loser) {
+	}
+
+	public DuelHistory getDuelHistory() {
+		return null;
+	}
 }
 
