9c18030df91fd09018fbe04fa8dbba8c749a85b6
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-05 19:51:31 +0900

    feat: 플레이어가 카드를 추가로 뽑을 수 있는 지 알려주는 기능 구현

--- src/main/java/blackjack/model/Rule.java
+++ src/main/java/blackjack/model/Rule.java
@@ -1,9 +1,17 @@
 package blackjack.model;
 
+import java.util.function.Predicate;
+
 public class Rule {
 
+    private static final Predicate<Dealer> DEALER_DRAWABLE_SCORE = dealer -> dealer.getCards().sumAll() < 17;
+    private static final Predicate<Player> PLAYER_DRAWABLE_SCORE = player -> player.getCards().sumAll() < 21;
+
     public boolean shouldDealerDraw(final Dealer dealer) {
-        return dealer.getCards().sumAll() < 17;
+        return DEALER_DRAWABLE_SCORE.test(dealer);
     }
 
+    public boolean canPlayerDrewMoreCard(final Player player) {
+        return PLAYER_DRAWABLE_SCORE.test(player);
+    }
 }
--- src/test/java/blackjack/model/RuleTest.java
+++ src/test/java/blackjack/model/RuleTest.java
@@ -1,11 +1,9 @@
 package blackjack.model;
 
 import static blackjack.model.CardCreator.createCard;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.List;
 import java.util.stream.Stream;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -37,6 +35,31 @@ class RuleTest {
         );
     }
 
+    private static Stream<Arguments> 플레이어가_카드를_뽑아야_하는지_반환한다_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        new Cards(
+                                List.of(
+                                        createCard(CardNumber.TEN),
+                                        createCard(CardNumber.SEVEN),
+                                        createCard(CardNumber.THREE)
+                                )
+                        ),
+                        true
+                ),
+                Arguments.of(
+                        new Cards(
+                                List.of(
+                                        createCard(CardNumber.TEN),
+                                        createCard(CardNumber.SEVEN),
+                                        createCard(CardNumber.FOUR)
+                                )
+                        ),
+                        false
+                )
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("딜러가_카드를_뽑아야_하는지_반환한다_테스트_케이스")
     void 딜러가_카드를_뽑아야_하는지_반환한다(final Cards cards, final boolean expected) {
@@ -46,4 +69,12 @@ class RuleTest {
         assertThat(rule.shouldDealerDraw(dealer)).isEqualTo(expected);
     }
 
+    @ParameterizedTest
+    @MethodSource("플레이어가_카드를_뽑아야_하는지_반환한다_테스트_케이스")
+    void 플레이어가_카드를_뽑아야_하는지_반환한다(final Cards cards, final boolean expected) {
+        Player player = new Player();
+        player.receiveCards(cards);
+
+        assertThat(rule.canPlayerDrewMoreCard(player)).isEqualTo(expected);
+    }
 }
