c06928a21e38a95d21771f41a56974e9af75570e
sonjh919 <kingjh1125@gmail.com>
2025-03-05 13:30:10 +0900

    test(red): 카드 한 장 뽑기 테스트 추가

--- src/test/java/CardDeckTest.java
+++ src/test/java/CardDeckTest.java
@@ -1,6 +1,7 @@
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import config.CardDeckFactory;
+import domain.card.Card;
 import domain.card.CardDeck;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,5 +14,14 @@ public class CardDeckTest {
         CardDeck cardDeck = cardDeckFactory.create();
 
         assertThat(cardDeck.getCardsSize()).isEqualTo(52);
+    }
+
+    @Test
+    @DisplayName("카드 한 장 뽑기 테스트")
+    void hitCardTest() {
+        CardDeckFactory cardDeckFactory = new CardDeckFactory();
+        CardDeck cardDeck = cardDeckFactory.create();
+
+        assertThat(cardDeck.hitCard()).isInstanceOf(Card.class);
     }
 }
