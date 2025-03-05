991c9e6ee49677957c7eabbd2ddfc76a9e8da89c
supernovaMK <minkikim2002@gmail.com>
2025-03-05 13:20:57 +0900

    feat: 블랙잭에 참여하는 플레이어 추가

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,23 @@
+package domain;
+
+public class Player {
+
+    private final String name;
+    private final Hand hand;
+
+    public Player(String name, Hand hand) {
+        validate(name, hand);
+        this.name = name;
+        this.hand = hand;
+    }
+
+    private void validate(String name, Hand hand) {
+        validateNotNull(name, hand);
+    }
+
+    private void validateNotNull(String name, Hand hand) {
+        if (name == null || hand == null) {
+            throw new IllegalStateException("플레이어는 이름과 손패를 가져야합니다.");
+        }
+    }
+}
