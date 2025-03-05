22730ee578ab79e41a0283d0203042ffc1ae9f30
Chaeyoung714 <202102760@hufs.ac.kr>
2025-03-05 18:45:53 +0900

    feat : 입력 뷰 로직 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,43 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class InputView {
+    private static Scanner SCANNER = new Scanner(System.in);
+
+    public static List<String> readPlayerNames(){
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요. (쉼표 기준으로 분리)");
+        String [] names = SCANNER.nextLine().split(",", -1);
+        validateDelimeter(names);
+        return Arrays.stream(names).toList();
+    }
+
+    private static void validateDelimeter(String [] names) {
+        String regex = "^[a-zA-Zㄱ-ㅎ가-힣]+$";
+
+         for (String name : names){
+            if (!Pattern.matches(regex, name)){
+                throw new IllegalArgumentException("[ERROR] 잘못된 이름 형식입니다. 입력값 : " + name);
+            }
+        }
+    }
+
+    public static boolean readHit() {
+        String hit = SCANNER.nextLine();
+        validateHit(hit);
+        if (hit.equals("y")) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void validateHit(String hit) {
+        if (!(hit.equals("y") || hit.equals("n"))) {
+            throw new IllegalArgumentException("[ERROR] y 또는 n을 입력해주세요. 입력값 : " + hit);
+        }
+    }
+}
--- src/test/java/model/DealerTest.java
+++ src/test/java/model/DealerTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import view.InputView;
 
 class DealerTest {
     @DisplayName("카드의 합이 16이하이면 true를 반환한다.")
@@ -29,4 +30,5 @@ class DealerTest {
 
         assertFalse(dealer.checkScoreUnderSixteen());
     }
+
 }
