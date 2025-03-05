660e9f54821e698ffb28612ea3cac8d0b2996b88
MinJun Choi <mj043000@naver.com>
2025-03-05 20:56:56 +0900

    feat: 카드를 분배 여부 입력 기능 구현
    Co-authored-by: Starlight258 <audwl03071@gmail.com>

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,15 +1,23 @@
 package blackjack.view;
 
+import blackjack.domain.participant.Player;
 import java.util.Scanner;
 
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String LINE = System.lineSeparator();
 
+    private final String TITLE_ASK_MORE_CARD = "%s는 한장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)";
     private final String TITLE_INPUT_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
 
     public String readNames() {
         System.out.println(TITLE_INPUT_NAMES);
+        return readLine();
+    }
+
+    public String askMoreCard(Player player) {
+        System.out.printf(LINE + TITLE_ASK_MORE_CARD + LINE, player.getNickname());
         return readLine();
     }
 
