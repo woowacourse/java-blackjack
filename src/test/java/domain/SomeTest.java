ce94593cbdbabec04aa35ceb1b7a0bd4d14c0685
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-05 19:13:38 +0900

    RED) test(domain) : Player 핸드 총합 반환 테스트

---
+++ src/test/java/domain/SomeTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SomeTest {
+
+    @Test
+    @DisplayName("Player 는 핸드의 총합값을 반환할 수 있다")
+    void test1() {
+        Player player = new Player("모루");
+        player.addCard(new Card(Denomination.TWO, Suit.CLUB));
+        player.addCard(new Card(Denomination.FOUR, Suit.CLUB));
+        player.addCard(new Card(Denomination.FIVE, Suit.DIAMOND));
+
+        assertThat(player.getHandTotal()).isEqualTo(11);
+    }
+}
