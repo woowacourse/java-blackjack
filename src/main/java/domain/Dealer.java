8b0ad7c92528ba7a9d1f7e7433c3119dc78a819c
supernovaMK <minkikim2002@gmail.com>
2025-03-05 13:42:33 +0900

    feat: 블랙잭에 참여하는 딜러 추가

---
+++ src/main/java/domain/Dealer.java
@@ -0,0 +1,21 @@
+package domain;
+
+public class Dealer {
+
+    private final Hand hand;
+
+    public Dealer(Hand hand) {
+        validate(hand);
+        this.hand = hand;
+    }
+
+    private void validate(Hand hand) {
+        validateNotNull(hand);
+    }
+
+    private void validateNotNull(Hand hand) {
+        if (hand == null) {
+            throw new IllegalStateException("딜러는 손패를 가져야합니다.");
+        }
+    }
+}
