d5efe199fdc03b5294385fcdb386b0e0cf4fb5af
sonjh919 <kingjh1125@gmail.com>
2025-03-05 19:45:49 +0900

    test(red): 플레이어 인원 검증 테스트 추가

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import config.CardDeckFactory;
@@ -35,5 +36,13 @@ public class PlayersTest {
 
         assertDoesNotThrow(() -> players.hitCards(dealer));
 
+    }
+
+    @Test
+    @DisplayName("플레이어 인원 검증 테스트")
+    void validatePlayerNumbers() {
+        assertThatThrownBy(() -> Players.from(List.of("포비", "이든", "네오", "저스틴", "링크", "말론", "리사")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 플레이어 인원은 1~6명 입니다.");
     }
 }
