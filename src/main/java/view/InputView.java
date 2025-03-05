e7789f917ea75bffaafd088c7987f24567d13268
egaeng09 <egaeng09@gmail.com>
2025-03-05 17:54:25 +0900

    feat: 참여자 이름 입력 기능 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,12 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String inputParticipantName() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        return scanner.nextLine();
+    }
+}
