3859b92ca30248242357cafea4e82d3bf5fe0d21
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-05 21:00:26 +0900

    test : 딜러와 참여자의 카드 합을 비교해 참여자의 승패 여부를 결정 테스트 코드 작성

---
+++ src/test/java/domain/BlackjackRefereeTest.java
@@ -0,0 +1,49 @@
+package domain;
+
+import static org.assertj.core.api.SoftAssertions.*;
+
+import java.util.List;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BlackjackRefereeTest {
+
+    @DisplayName("딜러와 참여자의 카드 합을 비교해 참여자의 승패 여부를 결정한다")
+    @Test
+    void test1() {
+        //given
+        Participant dealer = Participant.createDealer();
+        dealer.cards().add(new Card(CardNumberType.JACK, CardType.DIAMOND));
+        dealer.cards().add(new Card(CardNumberType.QUEEN, CardType.DIAMOND));
+
+        Cards winCards = new Cards(List.of(
+                new Card(CardNumberType.ACE, CardType.DIAMOND),
+                new Card(CardNumberType.QUEEN, CardType.DIAMOND)
+        ));
+        Cards drawCards = new Cards(List.of(
+                new Card(CardNumberType.JACK, CardType.DIAMOND),
+                new Card(CardNumberType.QUEEN, CardType.SPACE)
+        ));
+        Cards loseCards = new Cards(List.of(
+                new Card(CardNumberType.FIVE, CardType.DIAMOND),
+                new Card(CardNumberType.EIGHT, CardType.DIAMOND)
+        ));
+        Participant winner = new Participant("mimi", winCards, Role.PLAYER);
+        Participant drawer = new Participant("wade", drawCards, Role.PLAYER);
+        Participant loser = new Participant("pobi", loseCards, Role.PLAYER);
+        List<Participant> participants = List.of(winner, drawer, loser);
+
+        BlackjackReferee blackjackReferee = new BlackjackReferee();
+
+        //when
+        Map<Participant, GameResultStatus> gameResult = blackjackReferee.judge(dealer, participants);
+
+        //then
+        assertSoftly(softly -> {
+            softly.assertThat(gameResult.get(winner)).isEqualTo(GameResultStatus.WIN);
+            softly.assertThat(gameResult.get(drawer)).isEqualTo(GameResultStatus.DRAW);
+            softly.assertThat(gameResult.get(loser)).isEqualTo(GameResultStatus.LOSER);
+        });
+    }
+}
