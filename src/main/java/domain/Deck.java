9961a6a8bff529afe569096a33c5f096a7701d1e
ryan-dia <cjfdnjs158@naver.com>
2025-03-05 11:52:17 +0900

    feat(Deck): 카드 셔플 기능 구현
    Co-authored-by:cheche903<cheche903@naver.com>

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -1,12 +1,23 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class Deck {
 
     private final List<Card> cards;
 
     public Deck(final List<Card> cards) {
-        this.cards = List.copyOf(cards);
+        this.cards = new ArrayList<>(cards);
+    }
+
+    public void shuffle(final Random random) {
+        Collections.shuffle(cards, random);
+    }
+
+    public List<Card> getCards() {
+        return cards;
     }
 }
