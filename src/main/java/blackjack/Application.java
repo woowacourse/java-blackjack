9ad13d5964600467424f22309eeb3c1137cdf50f
kjyyjk <rlawnsdud920@gmail.com>
2025-03-05 17:47:08 +0900

    feat: 카드를 생성해 덱을 구성

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -1,15 +1,20 @@
 package blackjack;
 
+import blackjack.domain.CardDeck;
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardShape;
+import blackjack.domain.card.CardType;
 import blackjack.domain.gambler.Name;
 import blackjack.view.InputView;
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
         List<Name> playerNames = getPlayerNames();
-
-
+        CardDeck cardDeck = createCardDeck();
     }
 
     private static List<Name> getPlayerNames() {
@@ -19,5 +24,16 @@ public class Application {
             System.out.println(e.getMessage());
             return getPlayerNames();
         }
+    }
+
+    private static CardDeck createCardDeck() {
+        List<Card> cards = new ArrayList<>();
+        for (CardShape shape : CardShape.values()) {
+            for (CardType type : CardType.values()) {
+                Card card = new Card(shape, type);
+                cards.add(card);
+            }
+        }
+        return new CardDeck(cards);
     }
 }
