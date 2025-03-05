a4e06bc5de096afffd45ca46337f6d46be7e7667
황재웅 <wodnd0131@naver.com>
2025-03-05 20:21:19 +0900

    feat(Participant): 두 참가자의 승패를 결정하라

--- src/test/java/domain/ParticipantTest.java
+++ src/test/java/domain/ParticipantTest.java
@@ -13,62 +13,60 @@ import constant.CardNumber;
 import constant.Emblem;
 
 class ParticipantTest {
-	@Nested
-	@DisplayName("참가자 카드 뽑기")
-	class PickCard {
+    @Nested
+    @DisplayName("참가자 카드 뽑기")
+    class PickCard {
 
-		@DisplayName("참가자는 덱으로부터, 올바르게 카드를 뽑는다.")
-		@Test
-		public void pickCard() {
-			// given
-			final Participant participant = new MockParticipant();
-			final List<Card> cards = List.of(new Card(CardNumber.ACE, Emblem.CLUB));
-			final Deck deck = new Deck(new ArrayDeque<>(cards));
+        @DisplayName("참가자는 덱으로부터, 올바르게 카드를 뽑는다.")
+        @Test
+        public void pickCard() {
+            // given
+            final Participant participant = new MockParticipant();
+            final List<Card> cards = List.of(new Card(CardNumber.ACE, Emblem.CLUB));
+            final Deck deck = new Deck(new ArrayDeque<>(cards));
 
-			// when
-			participant.pickCard(deck);
+            // when
+            participant.pickCard(deck);
 
-			// then
-			assertThat(participant.getHand().hand()).isNotEmpty();
-		}
-	}
+            // then
+            assertThat(participant.getHand().hand()).isNotEmpty();
+        }
+    }
 
-	@Nested
-	@DisplayName("결투를 진행한다.")
-	class Duel {
+    @Nested
+    @DisplayName("결투를 진행한다.")
+    class Duel {
 
-		@DisplayName("올바르게 승자를 가려낸다.")
-		@Test
-		public void duel() throws Exception {
-			// given
-			final Participant winner = new MockParticipant();
-			final List<Card> winnerCards = List.of(new Card(CardNumber.ACE, Emblem.CLUB));
-			final Deck winnerDeck = new Deck(new ArrayDeque<>(winnerCards));
-			winner.pickCard(winnerDeck);
-			winner.pickCard(winnerDeck);
+        @DisplayName("올바르게 승자를 가려낸다.")
+        @Test
+        public void duel() throws Exception {
+            // given
+            final Participant winner = new MockParticipant();
+            final List<Card> winnerCards = List.of(new Card(CardNumber.ACE, Emblem.CLUB));
+            final Deck winnerDeck = new Deck(new ArrayDeque<>(winnerCards));
+            winner.pickCard(winnerDeck);
 
-			final Participant loser = new MockParticipant();
-			final List<Card> loserCards = List.of(new Card(CardNumber.TEN, Emblem.CLUB));
-			final Deck loserDeck = new Deck(new ArrayDeque<>(loserCards));
-			loser.pickCard(loserDeck);
-			loser.pickCard(loserDeck);
+            final Participant loser = new MockParticipant();
+            final List<Card> loserCards = List.of(new Card(CardNumber.TEN, Emblem.CLUB));
+            final Deck loserDeck = new Deck(new ArrayDeque<>(loserCards));
+            loser.pickCard(loserDeck);
 
-			// when
-			winner.duel(loser);
+            // when
+            winner.duel(loser);
 
-			// then
-			assertThat(winner.getDuelHistory().getWinCount()).isEqualTo(1);
-			assertThat(winner.getDuelHistory().getLoseCount()).isEqualTo(0);
-			assertThat(loser.getDuelHistory().getLoseCount()).isEqualTo(1);
-			assertThat(loser.getDuelHistory().getWinCount()).isEqualTo(0);
-		}
-	}
+            // then
+            assertThat(winner.getDuelHistory().getWinCount()).isEqualTo(1);
+            assertThat(winner.getDuelHistory().getLoseCount()).isEqualTo(0);
+            assertThat(loser.getDuelHistory().getLoseCount()).isEqualTo(1);
+            assertThat(loser.getDuelHistory().getWinCount()).isEqualTo(0);
+        }
+    }
 
-	private static class MockParticipant extends Participant {
+    private static class MockParticipant extends Participant {
 
-		@Override
-		public boolean isPickCard() {
-			return false;
-		}
-	}
+        @Override
+        public boolean isPickCard() {
+            return false;
+        }
+    }
 }
