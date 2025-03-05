94bc324371188228f98d473c23ee43182bbac1bb
jeyong <joon6093@naver.com>
2025-03-05 13:24:15 +0900

    feat: 플레이어와 딜러의 손패 추가

---
+++ src/main/java/domain/Hand.java
@@ -0,0 +1,31 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+
+    private final List<TrumpCard> cards;
+
+    public Hand(List<TrumpCard> cards) {
+        validate(cards);
+        this.cards = new ArrayList<>(cards);
+    }
+
+    private void validate(List<TrumpCard> cards) {
+        validateNotNull(cards);
+        validateDuplicate(cards);
+    }
+
+    private void validateNotNull(List<TrumpCard> cards) {
+        if (cards == null) {
+            throw new IllegalArgumentException("손패는 카드를 가지고 있어야합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<TrumpCard> cards) {
+        if (cards.stream().distinct().count() != cards.size()) {
+            throw new IllegalArgumentException("손패에 중복된 카드가 있습니다.");
+        }
+    }
+}
