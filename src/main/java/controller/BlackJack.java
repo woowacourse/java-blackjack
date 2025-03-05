c82ffb8b3cac62e1f56b6ae96a738902c3a30e21
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-05 19:46:44 +0900

    feat(controller) : BlackJack 기능 작성
    - 게임 시작 준비
    - 카드 셔플 및 분배

---
+++ src/main/java/controller/BlackJack.java
@@ -0,0 +1,46 @@
+package controller;
+
+import domain.Dealer;
+import domain.Deck;
+import domain.Player;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class BlackJack {
+
+    public static final String DELIMITER = ",";
+
+    private final List<Player> players = new ArrayList<>();
+    private final Dealer dealer = new Dealer();
+
+    public void play() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        String playersInput = sc.nextLine();
+        List<String> playerNames = Arrays.asList(playersInput.split(DELIMITER));
+
+        if(playerNames.size() > 5) {
+            throw new IllegalArgumentException("플레이어는 5명까지만 입력해주세요.");
+        }
+
+        for (String playerName : playerNames) {
+            players.add(new Player(playerName));
+        }
+
+        Deck deck = new Deck();
+        deck.shuffle();
+        drawTwoCardFromDeck(dealer, deck);
+        for (Player player : players) {
+            drawTwoCardFromDeck(player, deck);
+        }
+
+
+    }
+
+    private void drawTwoCardFromDeck(Player player, Deck deck) {
+        player.addCard(deck.draw());
+        player.addCard(deck.draw());
+    }
+}
