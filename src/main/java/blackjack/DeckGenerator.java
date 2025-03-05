7eeeb4f48a5d0d56f86fef99933d83ca1fdf8bfa
7i.w__k <tizmfns1218@Naver.com>
2025-03-05 10:53:23 +0900

    docs: 구현 사항 추가
    - hand, rules 추가
    
    Co-authored-by: Jin409 <sualng123@gmail.com>

--- src/main/java/blackjack/DeckGenerator.java
+++ src/main/java/blackjack/DeckGenerator.java
@@ -12,5 +12,4 @@ public class DeckGenerator {
                 .toList();
     }
 
-
 }
--- src/main/java/blackjack/common/ErrorMessage.java
+++ src/main/java/blackjack/common/ErrorMessage.java
@@ -4,7 +4,7 @@ public enum ErrorMessage {
     INVALID_CARD_VALUE("카드 타입에 맞지 않는 값입니다."),
     INVALID_DECK_SIZE("카드 개수는 52개여야 합니다.");
 
-    private String message;
+    private final String message;
 
     ErrorMessage(String message) {
         this.message = message;
