ba99a3ef91911763cc2ffcda04fb939f4670b1a7
RIWON <121172065+awrion3@users.noreply.github.com>
2025-03-05 19:58:30 +0900

    feat(inputView): 입력값 검증 추가

--- src/main/java/blackjack/view/InputVIew.java
+++ src/main/java/blackjack/view/InputVIew.java
@@ -10,11 +10,19 @@ public class InputVIew {
     public String[] readPlayerName() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         String playerNames = scanner.nextLine();
+        validateNullOrBlank(playerNames);
         return playerNames.split(DELIMITER);
+    }
+
+    private void validateNullOrBlank(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalStateException("입력 값이 null이거나 비어있습니다.");
+        }
     }
 
     public String readOneMoreCard(String playerName) {
         System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n", playerName.trim());
         return scanner.nextLine();
     }
+
 }
