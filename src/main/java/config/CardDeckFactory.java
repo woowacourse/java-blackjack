8d9c2a231a39e11b6e37670dd964a8fb3bde2ae8
sonjh919 <kingjh1125@gmail.com>
2025-03-05 11:38:46 +0900

    refactor: 카드 생성 기능 리팩터링

--- src/main/java/config/CardDeckFactory.java
+++ src/main/java/config/CardDeckFactory.java
@@ -5,16 +5,22 @@ import domain.card.CardDeck;
 import domain.card.Number;
 import domain.card.Shape;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CardDeckFactory {
     public CardDeck create() {
         List<Card> cards = new ArrayList<>();
         for (Shape shape : Shape.values()) {
-            for (Number number : Number.values()) {
-                cards.add(new Card(shape, number));
-            }
+            selectNumbers(shape, cards);
         }
+        Collections.shuffle(cards);
         return new CardDeck(cards);
+    }
+
+    private static void selectNumbers(Shape shape, List<Card> cards) {
+        for (Number number : Number.values()) {
+            cards.add(new Card(shape, number));
+        }
     }
 }
