77c1b9ea323e993673a77ba5d2faa76ecde39bdb
bowook <mbosx316@naver.com>
2025-03-05 17:26:57 +0900

    test(GameBoardTest) : 카드덱 카드 2장 빠지기 테스트 (GREEN)

--- src/test/java/domain/GameBoardTest.java
+++ src/test/java/domain/GameBoardTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GameBoardTest {
@@ -46,5 +47,23 @@ public class GameBoardTest {
             CardDeck ownedCardDeck = entry.getValue();
             Assertions.assertThat(ownedCardDeck.getCards().size()).isEqualTo(2);
         }
+    }
+
+    @DisplayName("총 플레잉 카드 52장에서 참가자 3명이 각각 2장씩 가져가 총 46장이 남는다.")
+    @Test
+    void 카드덱_2장_빠지기() {
+        //given
+        List<Participant> participants = List.of(
+                new Player("우가"),
+                new Player("히스타"),
+                new Dealer("딜러")
+        );
+        CardDeck cardDeck = CardDeck.generateFullSet();
+        GameBoard gameBoard = new GameBoard(cardDeck, participants);
+
+        //when
+        gameBoard.drawTwoCards();
+
+        Assertions.assertThat(cardDeck.getCards().size()).isEqualTo(46);
     }
 }
