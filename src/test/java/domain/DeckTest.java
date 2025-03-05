b2bab8a7b931c8d2bda586ea728e55a22b09f099
ryan-dia <cjfdnjs158@naver.com>
2025-03-05 17:37:52 +0900

    test(DeckTest): 카드 한 장을 뽑는 로직 실패 테스트 작성
    Co-authored-by:cheche903<cheche903@naver.com>

--- src/test/java/domain/DeckTest.java
+++ src/test/java/domain/DeckTest.java
@@ -1,6 +1,9 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +17,23 @@ class DeckTest {
         final DeckGenerator deckGenerator = new DeckGenerator();
 
         // when & then
-        Assertions.assertThatCode(() -> new Deck(deckGenerator.generate()))
+        assertThatCode(() -> new Deck(deckGenerator.generate()))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("덱에서 카드를 1장 뽑는다.")
+    @Test
+    void 덱에서_카드를_1장_뽑는다() {
+
+        // given
+        final Card card1 = new Card(Rank.ACE, Shape.CLOVER);
+        final Card card2 = new Card(Rank.KING, Shape.CLOVER);
+        final Deck deck = new Deck(List.of(card1, card2));
+
+        // when
+        Card card = deck.drawCard();
+
+        // then
+        assertThat(card).isEqualTo(card1);
     }
 }
