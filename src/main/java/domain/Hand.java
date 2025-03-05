f93ce9bd5f090acf33bc74704455a597fb5562ac
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-05 11:33:48 +0900

    GREEN) feat(domain) : Hand 카드 추가 구현

---
+++ src/main/java/domain/Hand.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Hand {
+
+    private final List<Card> cards;
+
+    public Hand() {
+        cards = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public List<Card> getCards() {
+        return Collections.unmodifiableList(cards);
+    }
+}
