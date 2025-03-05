5e0d81dcfe300e395cfc313dfcd17c6ae24ff234
changuii <rhljh7410@gmail.com>
2025-03-05 19:51:48 +0900

    feat(DuelHistory): 올바르게 승패를 기록하라

--- src/main/java/domain/DuelHistory.java
+++ src/main/java/domain/DuelHistory.java
@@ -1,14 +1,28 @@
 package domain;
 
 public class DuelHistory {
+
+	private int winCount;
+	private int loseCount;
+
+	public DuelHistory() {
+		this.winCount = 0;
+		this.loseCount = 0;
+	}
+
 	public void write(boolean duelResult) {
+		if (duelResult) {
+			winCount++;
+			return;
+		}
+		loseCount++;
 	}
 
 	public int getWinCount() {
-		return -1;
+		return winCount;
 	}
 
 	public int getLoseCount() {
-		return -1;
+		return loseCount;
 	}
 }
--- src/test/java/domain/DuelHistoryTest.java
+++ src/test/java/domain/DuelHistoryTest.java
@@ -16,7 +16,7 @@ public class DuelHistoryTest {
 		@DisplayName("올바르게 승패를 기록한다.")
 		@ParameterizedTest
 		@CsvSource(value = {"true:1:0", "false:0:1"}, delimiter = ':')
-		public void writeDuelHistory(final boolean duelResult, final boolean winCount, final boolean loseCount) throws
+		public void writeDuelHistory(final boolean duelResult, final int winCount, final int loseCount) throws
 			Exception {
 			// given
 			final var duelHistory = new DuelHistory();
