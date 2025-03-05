997dcbfbe27d5f5bff17d4320cf0b8fbb9ee7eca
soeun2537 <soeun2537@gmail.com>
2025-03-05 11:47:55 +0900

    feat: 블랙잭(21)과 가까운 수 반환 기능 구현

--- src/test/java/ScoreJudgementTest.java
+++ src/test/java/ScoreJudgementTest.java
@@ -1,26 +1,41 @@
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ScoreJudgementTest {
 
-    @DisplayName("숫자 합이 21이 넘으면 진다.")
+    @DisplayName("21이 넘지 않으면서 가장 큰 수를 반환한다.")
     @Test
     void test1() {
         // given
-        int total = 22;
-        ScoreJudgement judgement = new ScoreJudgement();
+        ScoreJudgement scoreJudgement = new ScoreJudgement();
+        List<Integer> candidates = List.of(19, 20, 3, 7, 22);
 
         // when
-        boolean result = judgement.isOverCriterion(total);
+        int result = scoreJudgement.determineNearest(candidates);
 
         // then
-        Assertions.assertThat(result).isFalse();
+        Assertions.assertThat(result).isEqualTo(20);
+    }
+
+    @DisplayName("모든 수가 21이 넘으면 0을 반환한다.")
+    @Test
+    void test2() {
+        // given
+        ScoreJudgement scoreJudgement = new ScoreJudgement();
+        List<Integer> candidates = List.of(22, 23, 24, 25, 26);
+
+        // when
+        int result = scoreJudgement.determineNearest(candidates);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(0);
     }
 
     @DisplayName("숫자 합이 21에 가까우면 이긴다.")
     @Test
-    void test2() {
+    void test3() {
         // given
         int total1 = 20;
         int total2 = 19;
@@ -35,7 +50,7 @@ public class ScoreJudgementTest {
 
     @DisplayName("숫자 합이 둘이 같으면 무승부")
     @Test
-    void test3() {
+    void test4() {
         // given
         int total1 = 10;
         int total2 = 10;
