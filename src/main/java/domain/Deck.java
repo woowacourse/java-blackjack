c6b4f3a40d6c7eaab98d033818924eeeec05906c
황재웅 <wodnd0131@naver.com>
2025-03-05 20:36:24 +0900

    feat(Deck): 무작위로 섞인 덱을 생성하라

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -1,7 +1,13 @@
 package domain;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Queue;
+
+import constant.CardNumber;
+import constant.Emblem;
 
 public class Deck {
     private final Queue<Card> deck;
@@ -11,12 +17,23 @@ public class Deck {
         this.deck = new ArrayDeque<>(deck);
     }
 
+    public static Deck createShuffledDeck() {
+        final List<Card> cards = new ArrayList<>();
+        for (final CardNumber cardNumber : CardNumber.values()) {
+            for (final Emblem emblem : Emblem.values()) {
+                cards.add(new Card(cardNumber, emblem));
+            }
+        }
+        Collections.shuffle(cards);
+        return new Deck(new ArrayDeque<>(cards));
+    }
+
     private void validateDuplicateCard(final Queue<Card> deck) {
         if (deck.stream().distinct().count() != deck.size()) {
             throw new IllegalArgumentException("덱에는 중복된 카드가 들어올 수 없습니다!");
         }
     }
- 
+
     public Card pickCard() {
         return deck.poll();
     }
