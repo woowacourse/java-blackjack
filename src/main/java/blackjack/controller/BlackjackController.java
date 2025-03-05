a95e631645d89b028498bde41590e6be916da21c
Dompoo <dlckdrms0517@gmail.com>
2025-03-05 17:54:50 +0900

    refactor: 컨트롤러의 복잡한 로직 흐름을 메서드로 추출

--- src/main/java/blackjack/controller/BlackjackController.java
+++ src/main/java/blackjack/controller/BlackjackController.java
@@ -21,72 +21,82 @@ public class BlackjackController {
     }
     
     public void run() {
-        // 이름 입력을 받기
-        final List<String> playerNames = inputView.getPlayerNames();
-        final List<Player> players = Player.createPlayers(playerNames);
         final Deck deck = new Deck();
-        
-        final List<PlayerCardHand> playerCardHands = players.stream()
-                .map(player -> new PlayerCardHand(player, deck))
-                .toList();
-        
+        final List<PlayerCardHand> playerCardHands = createPlayerCardHands(deck);
         final DealerCardHand dealerCardHand = new DealerCardHand(deck);
+        processInitialCardOpening(dealerCardHand, playerCardHands);
         
+        for (PlayerCardHand playerCardHand : playerCardHands) {
+            processPlayerAddingCards(playerCardHand, deck);
+        }
+        processDealerAddingCards(dealerCardHand, deck);
+        
+        processCardOpening(dealerCardHand, playerCardHands);
+        processFinalWinOrLoss(dealerCardHand, playerCardHands);
+    }
+    
+    private void processInitialCardOpening(final DealerCardHand dealerCardHand, final List<PlayerCardHand> playerCardHands) {
+        final List<String> playerNames = playerCardHands.stream()
+                .map(PlayerCardHand::getPlayerName)
+                .toList();
         outputView.outputDealing(playerNames);
         outputView.outputDealerCards(dealerCardHand.getInitialCards());
         for (PlayerCardHand playerCardHand : playerCardHands) {
             outputView.outputPlayerCards(playerCardHand.getPlayerName(), playerCardHand.getInitialCards());
         }
-        
-        for (PlayerCardHand playerCardHand : playerCardHands) {
-            outputView.outputStartAdding(playerCardHand.getPlayerName());
-            boolean addingCardDecision;
-            do {
-                if (playerCardHand.is21()) {
-                    outputView.is21Warning();
-                    break;
-                }
-                
-                if (playerCardHand.isBurst()) {
-                    outputView.burstWarning();
-                    break;
-                }
-                
-                
-                addingCardDecision = inputView.getAddingCardDecision(playerCardHand.getPlayerName());
-                if (addingCardDecision) {
-                    playerCardHand.addCard(deck.draw());
-                    outputView.outputCardsAndSum(playerCardHand.getCards(), playerCardHand.getSum());
-                }
-            } while (addingCardDecision);
-        }
-        
-        // 딜러는 한장의 카드 머시기
+    }
+    
+    private List<PlayerCardHand> createPlayerCardHands(final Deck deck) {
+        final List<String> playerNames = inputView.getPlayerNames();
+        final List<Player> players = Player.createPlayers(playerNames);
+        return players.stream()
+                .map(player -> new PlayerCardHand(player, deck))
+                .toList();
+    }
+    
+    private void processPlayerAddingCards(final PlayerCardHand playerCardHand, final Deck deck) {
+        outputView.outputStartAdding(playerCardHand.getPlayerName());
+        boolean addingCardDecision;
+        do {
+            if (playerCardHand.is21()) {
+                outputView.is21Warning();
+                break;
+            }
+            if (playerCardHand.isBurst()) {
+                outputView.burstWarning();
+                break;
+            }
+            addingCardDecision = inputView.getAddingCardDecision(playerCardHand.getPlayerName());
+            if (addingCardDecision) {
+                playerCardHand.addCard(deck.draw());
+                outputView.outputCardsAndSum(playerCardHand.getCards(), playerCardHand.getSum());
+            }
+        } while (addingCardDecision);
+    }
+    
+    private void processDealerAddingCards(final DealerCardHand dealerCardHand, final Deck deck) {
         final int beforeCount = dealerCardHand.getCards().size();
         dealerCardHand.startAdding(deck);
         final int afterCount = dealerCardHand.getCards().size();
         outputView.outputDealerAddedCards(afterCount - beforeCount);
-        
-        // 딜러 카드 오픈
+    }
+    
+    private void processCardOpening(final DealerCardHand dealerCardHand, final List<PlayerCardHand> playerCardHands) {
         outputView.outputDealerResult(dealerCardHand.getCards(), dealerCardHand.getSum());
-        
         for (PlayerCardHand playerCardHand : playerCardHands) {
-            // 플레이어 카드 오픈
             outputView.outputPlayerResult(playerCardHand.getPlayerName(), playerCardHand.getCards(), playerCardHand.getSum());
         }
-        
-        // 딜러 승패 개수 확인
+    }
+    
+    private void processFinalWinOrLoss(final DealerCardHand dealerCardHand, final List<PlayerCardHand> playerCardHands) {
         outputView.outputFinalWinOrLossHeader();
         final BlackjackJudge blackjackJudge = new BlackjackJudge(dealerCardHand, playerCardHands);
-        
         outputView.outputDealerFinalWinOrLoss(
                 blackjackJudge.getDealerWinningCount(),
                 blackjackJudge.getDealerDrawingCount(),
                 blackjackJudge.getDealerLosingCount()
         );
-        
         for (PlayerCardHand playerCardHand : playerCardHands) {
-            // 플레이어 승패여부 확인
             outputView.outputPlayerFinalWinOrLoss(playerCardHand.getPlayerName(), blackjackJudge.getWinningStatusOf(playerCardHand));
         }
     }
