76b85e27b155746d1c9d239041ed81627f1dc844
norang <norang@gmail.com>
2025-03-05 11:43:32 +0900

    feat : 이름이 중복될 경우 예외가 발생한다.

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -3,19 +3,37 @@ package domain;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Game {
-    public static final int MAX_PLAYER_COUNT = 5;
+    private static final int MAX_PLAYER_COUNT = 5;
     private final Map<Player, GameResult> players = new HashMap<>();
+    private final Deck deck;
 
-    public Game(List<Player> players) {
-        validatePlayerCount(players);
-        players.forEach(player -> this.players.put(player, GameResult.NONE));
+    public Game(List<String> playerNames) {
+        deck = new Deck();
+        validatePlayerCount(playerNames);
+        validateDuplicateName(playerNames);
+        playerNames.forEach(this::registerPlayer);
     }
 
-    private void validatePlayerCount(List<Player> players) {
-        if (players.size() > MAX_PLAYER_COUNT) {
+    private void registerPlayer(String playerName) {
+        Card firstCard = deck.random(new RandomNumberGenerator());
+        Card secondCard = deck.random(new RandomNumberGenerator());
+        CardHand cardHand = new CardHand(Set.of(firstCard, secondCard));
+        Player player = new Player(playerName, cardHand);
+        players.put(player, GameResult.NONE);
+    }
+
+    private void validatePlayerCount(List<String> playerNames) {
+        if (playerNames.size() > MAX_PLAYER_COUNT) {
             throw new IllegalArgumentException("[ERROR] 참여자는 최대 5명입니다.");
+        }
+    }
+
+    private void validateDuplicateName(List<String> playerNames) {
+        if (playerNames.size() != Set.of(playerNames).size()) {
+            throw new IllegalArgumentException("[ERROR] 이름은 중복될 수 없습니다.");
         }
     }
 }
--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -11,16 +11,21 @@ class GameTest {
     @DisplayName("참여자가 5명이 넘어갈 경우 예외가 발생한다.")
     void testValidatePlayerCount() {
         // given
-        Player player1 = new Player("pobi", null);
-        Player player2 = new Player("woni", null);
-        Player player3 = new Player("brie", null);
-        Player player4 = new Player("neo", null);
-        Player player5 = new Player("norang", null);
-        Player player6 = new Player("haru", null);
-        List<Player> players = List.of(player1, player2, player3, player4, player5, player6);
+        List<String> playerNames = List.of("pobi", "woni", "brie", "neo", "norang", "haru");
         // when && then
-        assertThatThrownBy(() -> new Game(players))
+        assertThatThrownBy(() -> new Game(playerNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 참여자는 최대 5명입니다.");
+    }
+
+    @Test
+    @DisplayName("이름이 중복될 경우 예외가 발생한다.")
+    void testValidateDuplicateName() {
+        // given
+        List<String> playerNames = List.of("pobi", "pobi");
+        // when && then
+        assertThatThrownBy(() -> new Game(playerNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 이름은 중복될 수 없습니다.");
     }
 }
