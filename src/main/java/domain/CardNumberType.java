0fdca221e1fbdecd115651acd7f87b062bb1499c
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-06 20:15:55 +0900

    refactor : 메서드 및 상수 분리

--- src/main/java/domain/CardNumberType.java
+++ src/main/java/domain/CardNumberType.java
@@ -20,6 +20,7 @@ public enum CardNumberType {
     QUEEN(10),
     KING(10);
 
+    private static final String CARD_NUMBER_INDEX_NO_EXISTED = "해당하는 카드 숫자의 인덱스가 존재하지 않습니다.";
     private final List<Integer> cardNumbers;
 
     CardNumberType(int... cardNumbers) {
@@ -41,13 +42,17 @@ public enum CardNumberType {
     }
 
     public static CardNumberType findByRandom(int randomIndex) {
-        if(randomIndex >= CardNumberType.values().length) {
-            throw new IllegalArgumentException(ERROR_HEADER + "해당하는 카드 숫자의 인덱스가 존재하지 않습니다.");
-        }
+        validateIndex(randomIndex);
         return CardNumberType.values()[randomIndex];
     }
 
     public boolean isAce() {
         return this == ACE;
+    }
+
+    private static void validateIndex(int index) {
+        if(index >= CardNumberType.values().length) {
+            throw new IllegalArgumentException(ERROR_HEADER + CARD_NUMBER_INDEX_NO_EXISTED);
+        }
     }
 }
