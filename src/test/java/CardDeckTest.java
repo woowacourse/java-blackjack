3e0bd1596f50e9feb5b6a4f14d64698bc60e2060
sonjh919 <kingjh1125@gmail.com>
2025-03-06 15:31:54 +0900

    test: 카드 합계 테스트 추가

--- src/test/java/CardDeckTest.java
+++ src/test/java/CardDeckTest.java
@@ -1,6 +1,11 @@
-import static domain.card.Number.A;
+import static domain.card.Number.ACE;
+import static domain.card.Number.FIVE;
+import static domain.card.Number.JACK;
+import static domain.card.Number.QUEEN;
 import static domain.card.Number.TWO;
+import static domain.card.Shape.CLOVER;
 import static domain.card.Shape.DIAMOND;
+import static domain.card.Shape.HEART;
 import static domain.card.Shape.SPADE;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -8,8 +13,13 @@ import config.CardDeckFactory;
 import domain.card.Card;
 import domain.card.CardDeck;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class CardDeckTest {
     @Test
@@ -43,4 +53,24 @@ public class CardDeckTest {
         assertThat(cardDeck.getCards().size()).isEqualTo(1);
     }
 
+    @ParameterizedTest
+    @DisplayName("카드 합계 테스트")
+    @MethodSource("provideCardDeckForSum")
+    void sumTest(CardDeck cardDeck, int expectedSum){
+        assertThat(cardDeck.sum()).isEqualTo(expectedSum);
+    }
+
+    private static Stream<Arguments> provideCardDeckForSum(){
+        return Stream.of(Arguments.of(
+                new CardDeck(List.of(new Card(DIAMOND, ACE))), 11,
+                new CardDeck(List.of(new Card(DIAMOND, ACE), new Card(SPADE, ACE))), 12,
+                new CardDeck(List.of(new Card(DIAMOND, ACE), new Card(SPADE, ACE), new Card(HEART, ACE))), 13,
+                new CardDeck(List.of(new Card(DIAMOND, ACE), new Card(SPADE, ACE), new Card(HEART, ACE), new Card(CLOVER, ACE))), 14,
+                new CardDeck(List.of(new Card(SPADE, QUEEN), new Card(DIAMOND, ACE))), 21,
+                new CardDeck(List.of(new Card(SPADE, QUEEN), new Card(DIAMOND, ACE), new Card(HEART, ACE))), 12,
+                new CardDeck(List.of(new Card(SPADE, QUEEN), new Card(DIAMOND, QUEEN), new Card(HEART, ACE))), 21,
+                new CardDeck(List.of(new Card(SPADE, QUEEN), new Card(DIAMOND, FIVE), new Card(DIAMOND, ACE), new Card(SPADE, ACE), new Card(HEART, ACE), new Card(CLOVER, ACE))), 19,
+                new CardDeck(List.of(new Card(SPADE, JACK), new Card(DIAMOND, JACK))), 20
+        ));
+    }
 }
