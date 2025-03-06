7bc977c2ca5d0339220dc6bfcd18aaab22f71c3c
gabean13 <choegabin171@gmail.com>
2025-03-06 19:27:48 +0900

    refactor: 코드 리팩토링
    - 가독성을 위한 메서드 분리 작업을 진행했습니다.
    
    Co-authored-by: YehyeokBang <qkddpgur318@gmail.com>

--- src/main/java/blackjack/controller/BlackjackController.java
+++ src/main/java/blackjack/controller/BlackjackController.java
@@ -4,10 +4,10 @@ import blackjack.model.Card;
 import blackjack.model.Dealer;
 import blackjack.model.Deck;
 import blackjack.model.Game;
-import blackjack.view.InputView;
-import blackjack.view.OutputView;
 import blackjack.model.Player;
 import blackjack.model.RandomCardShuffler;
+import blackjack.view.InputView;
+import blackjack.view.OutputView;
 import java.util.List;
 
 public class BlackjackController {
@@ -22,18 +22,30 @@ public class BlackjackController {
 
     public void run() {
         List<Player> players = getPlayers();
-        Game game = initializeGame(players);
+        Game game = createGame(players);
+        dealInitialHand(game);
+        askHitForAllPlayer(game);
+        dealerHitOrNot(game);
+        displayResult(game);
+    }
+
+    private Game createGame(List<Player> players) {
+        Dealer dealer = new Dealer(Deck.createShuffledDeck(Card.createDeck(), new RandomCardShuffler()));
+        return new Game(dealer, players);
+    }
+
+    private void dealInitialHand(Game game) {
         game.dealInitialCards();
         outputView.printInitialCards(game.getDealerVisibleCard(), game.getPlayers());
+    }
+
+    private void askHitForAllPlayer(Game game) {
         game.askHitForAllPlayer(inputView::readHitOrNot, outputView::printPlayerHand);
+    }
+
+    private void dealerHitOrNot(Game game) {
         boolean isDealerHit = game.dealerHit();
         outputView.printDealerHit(isDealerHit);
-
-        outputView.printDealerHandAndTotal(game.getDealerHand(), game.getDealerTotal());
-        outputView.printPlayerHandAndTotal(game.getPlayers());
-
-        outputView.printMatchResult(game.judgeMatchResults());
-
     }
 
     private List<Player> getPlayers() {
@@ -43,8 +55,9 @@ public class BlackjackController {
                 .toList();
     }
 
-    private Game initializeGame(List<Player> players) {
-        Dealer dealer = new Dealer(Deck.createShuffledDeck(Card.createDeck(), new RandomCardShuffler()));
-        return new Game(dealer, players);
+    private void displayResult(Game game) {
+        outputView.printDealerHandAndTotal(game.getDealerHand(), game.getDealerTotal());
+        outputView.printPlayerHandAndTotal(game.getPlayers());
+        outputView.printMatchResult(game.judgeMatchResults());
     }
 }
