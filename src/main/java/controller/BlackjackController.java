2998e75c1b0cb5b1eaee404636f22dacfc4ea4a6
soeun2537 <soeun2537@gmail.com>
2025-03-06 20:03:07 +0900

    feat: 블랙잭 게임 컨트롤러 구현

--- src/main/java/controller/BlackjackController.java
+++ src/main/java/controller/BlackjackController.java
@@ -1,10 +1,41 @@
 package controller;
 
+import java.util.List;
+import java.util.Map;
+import model.BlackjackGame;
+import model.GameResult;
+import model.participant.Player;
 import view.InputView;
+import view.OutputView;
 
 public class BlackjackController {
 
     public void run() {
-        String nickname = InputView.readNickname();
+        List<String> playerNames = InputView.readNicknames();
+        BlackjackGame game = new BlackjackGame(playerNames);
+
+        game.startGame();
+        OutputView.printInitialCards(game.getDealer(), game.getPlayers());
+
+        for (Player player : game.getPlayers().getPlayers()) {
+            while (player.canReceiveCard()) {
+                boolean drawOneMore = InputView.readDrawOneMore(player.getNickname());
+                if (!drawOneMore) {
+                    break;
+                }
+                game.runPlayerTurn(player);
+                OutputView.printPlayerCards(player);
+            }
+        }
+
+        while (game.getDealer().canReceiveCard()) {
+            OutputView.printDealerReceivedCard();
+            game.runDealerTurn();
+        }
+
+        OutputView.printAllCardAndScore(game.getPlayers(), game.getDealer());
+        Map<Player, GameResult> playerGameResults = game.calculatePlayerGameResult();
+        Map<GameResult, Integer> dealerResults = game.calculateDealerGameResult(playerGameResults);
+        OutputView.printResult(dealerResults, playerGameResults);
     }
 }
