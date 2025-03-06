1ba113476f2456900f5755aa13c45e7d6e14f31e
jhan0121 <jhan0121@gmail.com>
2025-03-06 19:04:19 +0900

    test: 게임 결과 계산 기능 테스트 추가

--- src/test/java/domain/GameResultTest.java
+++ src/test/java/domain/GameResultTest.java
@@ -4,74 +4,7 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-public class DealerTest {
-
-    @Test
-    void 딜러는_게임_시작_시_두_장의_카드를_받는다() {
-        //given
-        CardDeck cardDeck = CardDeck.createCards();
-        Dealer dealer = new Dealer();
-
-        //when
-        Hand actual = dealer.drawCardWhenStart(cardDeck);
-
-        //then
-        assertThat(actual.getCards()).hasSize(2);
-    }
-
-    @Test
-    void 딜러는_한_장의_카드를_받는다() {
-        //given
-        CardDeck cardDeck = CardDeck.createCards();
-        Dealer dealer = new Dealer();
-
-        //when
-        Hand actual = dealer.drawCard(cardDeck);
-
-        //then
-        assertThat(actual.getCards()).hasSize(1);
-    }
-
-    @Test
-    void 딜러가_보유한_카드의_합계를_구한다() {
-        //given
-        Dealer dealer = new Dealer();
-        Hand hand = dealer.getHand();
-        List<Card> cards = hand.getCards();
-        cards.add(new Card(Pattern.SPADE, CardNumber.TEN));
-        cards.add(new Card(Pattern.CLOVER, CardNumber.TEN));
-
-        //when & then
-        assertThat(dealer.calculateTotalCardNumber()).isEqualTo(20);
-    }
-
-    @Test
-    void 딜러가_보유한_카드의_합계가_21을_넘었는지_판정한다() {
-        //given
-        Dealer dealer = new Dealer();
-        Hand hand = dealer.getHand();
-        List<Card> cards = hand.getCards();
-        cards.add(new Card(Pattern.SPADE, CardNumber.TEN));
-        cards.add(new Card(Pattern.CLOVER, CardNumber.ACE));
-
-        //when & then
-        assertThat(dealer.isOverBurstBound()).isFalse();
-    }
-
-    @Test
-    void 딜러가_보유한_카드의_합계가_16을_넘었는지_판정한다() {
-        //given
-        Dealer dealer = new Dealer();
-        Hand hand = dealer.getHand();
-        List<Card> cards = hand.getCards();
-        cards.add(new Card(Pattern.SPADE, CardNumber.TEN));
-        cards.add(new Card(Pattern.CLOVER, CardNumber.SEVEN));
-
-        //when & then
-        assertThat(dealer.isOverDrawBound()).isTrue();
-    }
+public class GameResultTest {
 
     @Test
     void 모든_플레이어들과_딜러_사이의_승패를_판정한다() {
@@ -89,10 +22,10 @@ public class DealerTest {
         playerCards.add(new Card(Pattern.CLOVER, CardNumber.SIX));
 
         //when
-        List<GameResult> gameResult = dealer.judgeGameResult(List.of(player));
+        GameResult gameResult = GameResult.of(dealer, player);
 
         //then
-        Assertions.assertThat(gameResult).containsExactlyElementsOf(List.of(GameResult.LOSE));
+        Assertions.assertThat(gameResult).isEqualTo(GameResult.LOSE);
     }
 
     @Test
@@ -113,10 +46,10 @@ public class DealerTest {
         playerCards.add(new Card(Pattern.HEART, CardNumber.TEN));
 
         //when
-        List<GameResult> gameResult = dealer.judgeGameResult(List.of(player));
+        GameResult gameResult = GameResult.of(dealer, player);
 
         //then
-        Assertions.assertThat(gameResult).containsExactlyElementsOf(List.of(GameResult.LOSE));
+        Assertions.assertThat(gameResult).isEqualTo(GameResult.LOSE);
     }
 
     @Test
@@ -136,10 +69,10 @@ public class DealerTest {
         playerCards.add(new Card(Pattern.CLOVER, CardNumber.TEN));
 
         //when
-        List<GameResult> gameResult = dealer.judgeGameResult(List.of(player));
+        GameResult gameResult = GameResult.of(dealer, player);
 
         //then
-        Assertions.assertThat(gameResult).containsExactlyElementsOf(List.of(GameResult.WIN));
+        Assertions.assertThat(gameResult).isEqualTo(GameResult.WIN);
     }
 
     @Test
@@ -158,10 +91,10 @@ public class DealerTest {
         playerCards.add(new Card(Pattern.CLOVER, CardNumber.ACE));
 
         //when
-        List<GameResult> gameResult = dealer.judgeGameResult(List.of(player));
+        GameResult gameResult = GameResult.of(dealer, player);
 
         //then
-        Assertions.assertThat(gameResult).containsExactlyElementsOf(List.of(GameResult.DRAW));
+        Assertions.assertThat(gameResult).isEqualTo(GameResult.DRAW);
     }
 
     @Test
@@ -181,10 +114,10 @@ public class DealerTest {
         playerCards.add(new Card(Pattern.CLOVER, CardNumber.ACE));
 
         //when
-        List<GameResult> gameResult = dealer.judgeGameResult(List.of(player));
+        GameResult gameResult = GameResult.of(dealer, player);
 
         //then
-        Assertions.assertThat(gameResult).containsExactlyElementsOf(List.of(GameResult.LOSE));
+        Assertions.assertThat(gameResult).isEqualTo(GameResult.LOSE);
     }
 
     @Test
@@ -204,9 +137,9 @@ public class DealerTest {
         playerCards.add(new Card(Pattern.CLOVER, CardNumber.ACE));
 
         //when
-        List<GameResult> gameResult = dealer.judgeGameResult(List.of(player));
+        GameResult gameResult = GameResult.of(dealer, player);
 
         //then
-        Assertions.assertThat(gameResult).containsExactlyElementsOf(List.of(GameResult.WIN));
+        Assertions.assertThat(gameResult).isEqualTo(GameResult.WIN);
     }
 }
