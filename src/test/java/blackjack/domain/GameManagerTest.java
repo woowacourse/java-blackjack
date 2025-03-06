c2bddd0482737c423f1ec36aa03d85c2cd719f19
taek2222 <oot309@daum.net>
2025-03-06 15:49:32 +0900

    test: 플레이어의 카드 버스트 테스트 추가

--- src/test/java/blackjack/domain/GameManagerTest.java
+++ src/test/java/blackjack/domain/GameManagerTest.java
@@ -9,12 +9,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class GameManagerTest {
 
+    private static final BlackjackShuffle blackjackShuffle = new SortShuffle();
+
     @Test
     @DisplayName("참가자를 추가한다")
     void 참가자를_추가한다() {
         List<Player> names = List.of(new Player("비타"));
 
-        GameManager gameManager = new GameManager();
+        GameManager gameManager = new GameManager(blackjackShuffle);
         gameManager.addGamblers(names);
 
         assertThat(gameManager.getPlayers().getGamblers().size())
@@ -26,10 +28,25 @@ class GameManagerTest {
     void deal_card_to_gambler_test() {
         Player player = new Player("두리");
 
-        GameManager gameManager = new GameManager();
+        GameManager gameManager = new GameManager(blackjackShuffle);
         gameManager.addGamblers(List.of(player));
 
         gameManager.dealAddCard(player);
         assertThat(player.getCards().size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("플레이어의 카드가 버스트면 TRUE를 반환한다")
+    void 플레이어의_카드가_버스트면_TRUE를_반환한다() {
+        Player player = new Player("비타");
+        List<Player> names = List.of(player);
+
+        GameManager gameManager = new GameManager(blackjackShuffle);
+        gameManager.addGamblers(names);
+        gameManager.dealAddCard(player);
+
+        boolean result = gameManager.isPlayerBust(player);
+
+        assertThat(result).isTrue();
     }
 }
