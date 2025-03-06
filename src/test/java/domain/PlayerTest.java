99f72c939dc7daffde6d02c7c908a5f6a7b42fab
sonjh919 <kingjh1125@gmail.com>
2025-03-06 15:58:18 +0900

    test: 버스트 확인 테스트 추가

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,7 +1,11 @@
 package domain;
 
 import static domain.card.Number.ACE;
+import static domain.card.Number.JACK;
+import static domain.card.Number.KING;
+import static domain.card.Number.QUEEN;
 import static domain.card.Shape.DIAMOND;
+import static domain.card.Shape.HEART;
 import static domain.card.Shape.SPADE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -60,4 +64,19 @@ public class PlayerTest {
         assertThat(player.sum()).isEqualTo(12);
     }
 
+    @Test
+    @DisplayName("버스트 테스트")
+    void isBustTest(){
+        //given
+        CardDeck cardDeck = new CardDeck(List.of(new Card(DIAMOND, QUEEN), new Card(SPADE, JACK), new Card(HEART, KING)));
+        Dealer dealer = new Dealer(cardDeck);
+        Player player = new Player("pobi");
+
+        //when
+        player.hitCards(dealer);
+        player.addCard(dealer);
+
+        //then
+        assertThat(player.isBust()).isTrue();
+    }
 }
