a1b02225c680d455dda0e6473ec2152d5272ef1f
sonjh919 <kingjh1125@gmail.com>
2025-03-06 17:12:49 +0900

    test: 카드 드로우 테스트 추가

--- src/test/java/domain/BlackJackTest.java
+++ src/test/java/domain/BlackJackTest.java
@@ -15,9 +15,15 @@ import domain.card.Card;
 import domain.card.CardDeck;
 import domain.participant.Dealer;
 import domain.participant.Players;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.List;
+import java.util.Scanner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import view.InputView;
 
 public class BlackJackTest {
     @Test
@@ -31,7 +37,30 @@ public class BlackJackTest {
         //when
         BlackJack blackJack = new BlackJack(players, dealer);
 
-        // then
+        //then
         assertDoesNotThrow(blackJack::hitCardsToParticipant);
+    }
+
+    @Test
+    @DisplayName("카드 드로우 테스트")
+    void drawTest(){
+        //given
+        String input = "y\nn\ny\nn\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        InputView testInputView = new InputView(new Scanner(System.in));
+
+        CardDeckFactory cardDeckFactory = new CardDeckFactory();
+        Dealer dealer = new Dealer(cardDeckFactory.create());
+        Players players = Players.from(List.of("pobi", "lisa"));
+
+        BlackJack blackJack = new BlackJack(players, dealer);
+
+        //when-then
+        assertDoesNotThrow(() -> blackJack.draw(testInputView::askPlayerForHitOrStand, testInputView::printPlayerDeck));
     }
 }
