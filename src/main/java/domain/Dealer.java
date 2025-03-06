3f61a36235e6904c68f9da946fae2241660409ae
황재웅 <wodnd0131@naver.com>
2025-03-06 21:44:35 +0900

    refactor(Player): Player가 Dealer와의 승패를 결정하라

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -1,33 +1,29 @@
 package domain;
 
 public class Dealer {
-	private final Participant participant;
+    private final Participant participant;
 
-	public Dealer() {
-		this.participant = new Participant();
-	}
+    public Dealer() {
+        this.participant = new Participant();
+    }
 
-	public Dealer(final CardHand hand) {
-		this.participant = new Participant(hand);
-	}
+    public Dealer(final CardHand hand) {
+        this.participant = new Participant(hand);
+    }
 
-	public boolean isPickCard() {
-		return participant.calculateAllScore() <= 16;
-	}
+    public boolean isPickCard() {
+        return participant.calculateAllScore() <= 16;
+    }
 
-	public void pickCardOnFirstHandOut(final Deck deck) {
-		participant.pickCardOnFirstHandOut(deck);
-	}
+    public void pickCardOnFirstHandOut(final Deck deck) {
+        participant.pickCardOnFirstHandOut(deck);
+    }
 
-	public void pickCard(final Deck deck) {
-		participant.pickCard(deck);
-	}
+    public void pickCard(final Deck deck) {
+        participant.pickCard(deck);
+    }
 
-	public void duel(final Player player) {
-		player.duel(participant);
-	}
-
-	public Participant getParticipant() {
-		return participant;
-	}
+    public Participant getParticipant() {
+        return participant;
+    }
 }
--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,40 +1,51 @@
 package domain;
 
 public class Player {
-	private final String name;
-	private final Participant participant;
+    private final String name;
+    private final Participant participant;
+    private boolean isWinDuel;
 
-	public Player(String name) {
-		this.participant = new Participant();
-		this.name = name;
-	}
+    public Player(String name) {
+        this.participant = new Participant();
+        this.name = name;
+        isWinDuel = false;
+    }
 
-	public Player(final Participant participant) {
-		this.participant = participant;
-		this.name = "";
-	}
+    public Player(final Participant participant) {
+        this.participant = participant;
+        this.name = "";
+        isWinDuel = false;
+    }
 
-	public boolean isPickCard() {
-		return participant.calculateAllScore() <= 21;
-	}
+    public boolean isPickCard() {
+        return participant.calculateAllScore() <= 21;
+    }
 
-	public void pickCardOnFirstHandOut(final Deck deck) {
-		participant.pickCardOnFirstHandOut(deck);
-	}
+    public void pickCardOnFirstHandOut(final Deck deck) {
+        participant.pickCardOnFirstHandOut(deck);
+    }
 
-	public void pickCard(final Deck deck) {
-		participant.pickCard(deck);
-	}
+    public void pickCard(final Deck deck) {
+        participant.pickCard(deck);
+    }
 
-	public void duel(final Participant other) {
-		participant.duel(other);
-	}
+    public void duel(final Participant other) {
+        final int score = participant.calculateAllScore();
+        if (score > 21) {
+            return;
+        }
+        isWinDuel = score > other.calculateAllScore();
+    }
 
-	public String getName() {
-		return name;
-	}
+    public boolean getIsWinDuel() {
+        return isWinDuel;
+    }
 
-	public Participant getParticipant() {
-		return participant;
-	}
+    public String getName() {
+        return name;
+    }
+
+    public Participant getParticipant() {
+        return participant;
+    }
 }
