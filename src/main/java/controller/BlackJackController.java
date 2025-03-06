c4589c764581955fe6c338639872aa2ebb8dfac7
Yeongjae Kim <sees77770@gmail.com>
2025-03-06 19:42:48 +0900

    feat: 뷰, 컨트롤러 기본 구조 추가

---
+++ src/main/java/controller/BlackJackController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import view.InputView;
+import view.OutputView;
+
+public class BlackJackController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BlackJackController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+
+    }
+}
