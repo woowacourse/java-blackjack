65e4f86b717bf17ecd2a187e454a2068ddfba026
junho <wnsgh732@gmail.com>
2025-03-06 13:44:32 +0900

    test: 참가자에게 카드를 한장 추가 발부하는 테스트 작성

--- src/test/java/blackjack/domain/GameManagerTest.java
+++ src/test/java/blackjack/domain/GameManagerTest.java
@@ -12,12 +12,24 @@ class GameManagerTest {
     @Test
     @DisplayName("참가자를 추가한다")
     void 참가자를_추가한다() {
-        List<String> names = List.of("비타", "두리");
+        List<Player> names = List.of(new Player("비타"));
 
         GameManager gameManager = new GameManager();
         gameManager.addGamblers(names);
 
         assertThat(gameManager.getPlayers().getGamblers().size())
                 .isEqualTo(2);
+    }
+
+    @DisplayName("참가자에게 카드를 한장 추가 발부한다")
+    @Test
+    void deal_card_to_gambler_test() {
+        Player player = new Player("두리");
+
+        GameManager gameManager = new GameManager();
+        gameManager.addGamblers(List.of(player));
+
+        gameManager.dealAddCard(player);
+        assertThat(player.getCards().size()).isEqualTo(3);
     }
 }
