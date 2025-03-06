839191407307e68fb5d304cec14dd62d0c716644
sonjh919 <kingjh1125@gmail.com>
2025-03-06 21:37:52 +0900

    fix: 플레이어 패배 조건 추가

--- src/main/java/domain/MatchResult.java
+++ src/main/java/domain/MatchResult.java
@@ -5,8 +5,10 @@ public enum MatchResult {
     DRAW,
     LOSE;
 
+    private static final int BLACKJACK_NUMBER = 21;
+
     public static MatchResult calculateWinner(int dealerSum, int playerSum) {
-        if (dealerSum > playerSum) {
+        if (dealerSum > playerSum || playerSum > BLACKJACK_NUMBER) {
             return LOSE;
         }
         if (dealerSum < playerSum) {
