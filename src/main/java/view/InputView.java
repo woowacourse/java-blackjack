60d21179f3d9e760db38f59318b7dd21bbe80062
yush <yush2202@gmail.com>
2025-03-06 16:54:30 +0900

    feat: 사용자 입력 검증 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
     public List<String> insertUsernames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         String rawNames = readLine();
+        InputValidator.validateUsernames(rawNames);
         return Parser.splitByDelimiter(rawNames, ",");
     }
 
@@ -18,5 +19,15 @@ public class InputView {
 
     private String readLine() {
         return new Scanner(System.in).nextLine();
+    }
+
+    private static class InputValidator {
+        private static final String PATTERN = "^[가-힣a-zA-Z, ]+$";
+
+        public static void validateUsernames(String names) {
+            if (!names.matches(PATTERN)) {
+                throw new IllegalArgumentException("한글, 숫자, 공백, 쉼표만 입력가능합니다.");
+            }
+        }
     }
 }
