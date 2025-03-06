ba8ac4a826ce7d53c61ffca4ef45a7fea8b42418
soeun2537 <soeun2537@gmail.com>
2025-03-06 20:02:20 +0900

    feat: InputView 클래스 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -9,8 +11,15 @@ public class InputView {
     private InputView() {
     }
 
-    public static String readNickname() {
+    public static List<String> readNicknames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return scanner.nextLine();
+        String rawNicknames = scanner.nextLine();
+        return Arrays.stream(rawNicknames.split(",")).toList();
+    }
+
+    public static boolean readDrawOneMore(String nickname) {
+        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)%n", nickname);
+        String rawAnswer = scanner.nextLine();
+        return "y".equalsIgnoreCase(rawAnswer.trim());
     }
 }
