23b95ab42d998ac4be6d87e6c27e4c2ccd04adfb
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-06 11:30:17 +0900

    🔴red(AnswerCommand): 플레이어가 커멘드 유효성 체크 레드 테스트

--- src/test/java/domain/AnswerCommandTest.java
+++ src/test/java/domain/AnswerCommandTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +19,15 @@ public class AnswerCommandTest {
         assertThat(byAnswer).isNotNull()
                 .isEqualTo(AnswerCommand.YES);
 
+    }
+
+    @Test
+    void 플레이어가_유효하지_않은_커맨드를_입력하여_예외가_발생한다() {
+        //given
+        final String command = "d";
+
+        //when
+        //then
+        assertThatIllegalArgumentException().isThrownBy(() -> AnswerCommand.findByAnswer(command));
     }
 }
