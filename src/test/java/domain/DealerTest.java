7267a721ec71bdac77283334529fda31c9077331
jhpark1227 <junhyeok0246@naver.com>
2025-03-06 19:33:16 +0900

    🔴red: 플레이어들의 승패를 결정한다 테스트 작성

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -3,8 +3,10 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -32,5 +34,40 @@ class DealerTest {
                 Arguments.of(List.of(new Card(CardNumber.SEVEN, CardShape.CLOVER),
                         new Card(CardNumber.KING, CardShape.CLOVER)), false)
         );
+    }
+
+    @DisplayName("플레이어들의 승패를 결정한다")
+    @Test
+    void test() {
+        //given
+        Cards cards1 = Cards.of(
+                List.of(
+                        new Card(CardNumber.TEN, CardShape.CLOVER),
+                        new Card(CardNumber.THREE, CardShape.CLOVER)
+                )
+        );
+        Cards cards2 = Cards.of(
+                List.of(
+                        new Card(CardNumber.TEN, CardShape.CLOVER),
+                        new Card(CardNumber.TWO, CardShape.CLOVER)
+                )
+        );
+        Cards cards3 = Cards.of(
+                List.of(
+                        new Card(CardNumber.A, CardShape.CLOVER),
+                        new Card(CardNumber.FOUR, CardShape.CLOVER)
+                )
+        );
+        Dealer dealer = Dealer.of(cards1);
+        Player player1 = Player.from("플레이어", cards2);
+        Player player2 = Player.from("플레이어2", cards3);
+        Map<Player, GameResult> expected = Map.of(
+                player1, GameResult.WIN,
+                player2, GameResult.LOSE
+        );
+        //when
+        Map<Participant, GameResult> actual = dealer.getGameResult(List.of(player1, player2));
+        //then
+        assertThat(actual).isEqualTo(expected);
     }
 }
