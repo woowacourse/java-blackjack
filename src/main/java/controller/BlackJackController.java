0471866a726ed7c46db71d8cca15cc3bbf95832f
ryan-dia <cjfdnjs158@naver.com>
2025-03-06 22:07:52 +0900

    feat: 뷰 작성 및 기능 조합
    Co-authored-by:cheche903<cheche903@naver.com>

--- src/main/java/controller/BlackJackController.java
+++ src/main/java/controller/BlackJackController.java
@@ -1,13 +1,16 @@
 package controller;
 
 import domain.Card;
+import domain.Command;
 import domain.Dealer;
 import domain.Deck;
 import domain.DeckGenerator;
+import domain.FinalResult;
 import domain.Nickname;
 import domain.Player;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import view.InputView;
 import view.OutputView;
@@ -15,60 +18,108 @@ import view.OutputView;
 public class BlackJackController {
 
     public void run() {
+        final List<Nickname> nicknames = readNicknames();
+        final List<Player> players = generatePlayers(nicknames);
+        final Deck deck = generateDeck();
+        setGame(players, deck);
+        final Dealer dealer = generateDealer();
+        dealer.receiveInitialCards(List.of(deck.drawCard(), deck.drawCard()));
+        printGameSetting(dealer, nicknames, players);
+
+        processPlayerHit(players, deck);
+        processDealerHit(dealer, deck);
+        processFinalResult(dealer, players);
+    }
+
+    private List<Nickname> readNicknames() {
         final String inputNicknames = InputView.readPlayerName();
-        final List<Nickname> nicknames = Arrays.stream(inputNicknames.split(",")).map(Nickname::new).toList();
+        return Arrays.stream(inputNicknames.split(",")).map(Nickname::new).toList();
+    }
 
-        DeckGenerator deckGenerator = new DeckGenerator();
-        final List<Card> cards = deckGenerator.generate();
-        deckGenerator.shuffle(cards, new Random());
-        final Deck deck = new Deck(cards);
-
-        final List<Player> players = nicknames.stream()
+    private List<Player> generatePlayers(final List<Nickname> nicknames) {
+        return nicknames.stream()
                 .map(Player::new)
                 .toList();
+    }
 
+    private void setGame(final List<Player> players, final Deck deck) {
         players.forEach(player -> {
             final Card firstCard = deck.drawCard();
             final Card secondCard = deck.drawCard();
             player.receiveInitialCards(List.of(firstCard, secondCard));
         });
+    }
 
-        final Dealer dealer = new Dealer(new Nickname("딜러"));
+    private Deck generateDeck() {
+        DeckGenerator deckGenerator = new DeckGenerator();
+        final List<Card> cards = deckGenerator.generate();
+        deckGenerator.shuffle(cards, new Random());
+        return new Deck(cards);
+    }
 
-        dealer.receiveInitialCards(List.of(deck.drawCard(), deck.drawCard()));
+    private Dealer generateDealer() {
+        return new Dealer(new Nickname("딜러"));
+    }
 
-        OutputView.printInitialSettingMessage("딜러", nicknames, 2);
-
-        OutputView.printCardsInHand(dealer.getDisplayName(), dealer.getCards());
-
+    private void printGameSetting(final Dealer dealer, final List<Nickname> nicknames,
+                                  final List<Player> players) {
+        OutputView.printInitialSettingMessage(dealer.getDisplayName(), nicknames, 2);
+        OutputView.printCardsInHand(dealer.getDisplayName(), List.of(dealer.getCards().getFirst()));
         players.forEach(player -> OutputView.printCardsInHand(player.getDisplayName(), player.getCards()));
+    }
 
+    private void processPlayerHit(final List<Player> players, final Deck deck) {
         for (Player player : players) {
-            String input = InputView.readQuestOneMoreCard(player.getDisplayName());
-            if (input.equals("y")) {
-                final Card card = deck.drawCard();
-                player.hit(card);
-
-            }
-            OutputView.printCardsInHand(player.getDisplayName(), player.getCards());
-            if (player.isBust()) {
-                OutputView.printBustMessage(player.getDisplayName());
+            if (isNotMoreCard(deck, player)) {
                 continue;
             }
+            processAdditionalHit(deck, player);
+        }
+    }
 
-            if (input.equals("n")) {
-                continue;
-            }
-            while (InputView.readQuestOneMoreCard(player.getDisplayName()).equals("y")) {
-                final Card card = deck.drawCard();
-                player.hit(card);
-                OutputView.printCardsInHand(player.getDisplayName(), player.getCards());
-                if (player.isBust()) {
-                    OutputView.printBustMessage(player.getDisplayName());
-                    break;
-                }
-            }
+    private static boolean isNotMoreCard(final Deck deck, final Player player) {
+        String input = InputView.readQuestOneMoreCard(player.getDisplayName());
+        if (Command.isYes(input)) {
+            final Card card = deck.drawCard();
+            player.hit(card);
+        }
+        OutputView.printCardsInHand(player.getDisplayName(), player.getCards());
+
+        if (player.isBust()) {
+            OutputView.printBustMessage(player.getDisplayName());
+            return true;
         }
 
+        return Command.isNo(input);
+    }
+
+    private static void processAdditionalHit(final Deck deck, final Player player) {
+        while (Command.isYes(InputView.readQuestOneMoreCard(player.getDisplayName()))) {
+            final Card card = deck.drawCard();
+            player.hit(card);
+            OutputView.printCardsInHand(player.getDisplayName(), player.getCards());
+
+            if (player.isBust()) {
+                OutputView.printBustMessage(player.getDisplayName());
+                break;
+            }
+        }
+    }
+
+    private void processDealerHit(final Dealer dealer, final Deck deck) {
+        while (dealer.canHit(16)) {
+            final Card card = deck.drawCard();
+            dealer.hit(card);
+
+            OutputView.printDealerHit(16, dealer.getDisplayName());
+        }
+    }
+
+    private void processFinalResult(final Dealer dealer, final List<Player> players) {
+        OutputView.printCardsInHandWithResults(dealer, players);
+        Map<Player, FinalResult> playerResults = FinalResult.makePlayerResult(players, dealer);
+        Map<FinalResult, Integer> resultCounts = FinalResult.makeDealerResult(playerResults);
+        OutputView.printFinalResults(dealer.getDisplayName(), resultCounts, playerResults);
     }
 }
+
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,8 +1,13 @@
 package view;
 
 import domain.Card;
+import domain.Dealer;
+import domain.FinalResult;
+import domain.Gamer;
 import domain.Nickname;
+import domain.Player;
 import java.util.List;
+import java.util.Map;
 
 public final class OutputView {
 
@@ -27,18 +32,58 @@ public final class OutputView {
     public static void printCardsInHand(String name, List<Card> cards) {
 
         final List<String> cardGroup = cards.stream()
-                .map(card -> card.getScore() + card.getShape().getTitle())
+                .map(card -> card.getDisplayName() + card.getShape().getTitle())
                 .toList();
         String message = String.format("%s카드: %s", name, String.join(", ", cardGroup));
         print(message);
+    }
+
+    public static void printDealerHit(final int threshold, final String dealerName) {
+        print(String.format("%s는 %d이하라 한 장의 카드를 더 받았습니다.", dealerName, threshold));
     }
 
     public static void printBustMessage(final String name) {
         print(String.format("%s는 버스트입니다.", name));
     }
 
+    public static void printCardsInHandWithResults(final Dealer dealer, final List<Player> players) {
+        print(getMessage(dealer));
+        for (Player player : players) {
+            print(getMessage(player));
+        }
+    }
+
+    private static String getMessage(final Gamer gamer) {
+        List<Card> cards = gamer.getCards();
+        final List<String> cardGroup = cards.stream()
+                .map(card -> card.getDisplayName() + card.getShape().getTitle())
+                .toList();
+        final int result = gamer.calculateSumOfRank();
+        return String.format(
+                "%s카드: %s - 결과: %d",
+                gamer.getDisplayName(),
+                String.join(", ", cardGroup),
+                result
+        );
+    }
+
+    public static void printFinalResults(final String dealerName, final Map<FinalResult, Integer> resultCounts,
+                                         final Map<Player, FinalResult> playerResults) {
+
+        print("## 최종 승패");
+        final Integer win = resultCounts.getOrDefault(FinalResult.WIN, 0);
+        final Integer lose = resultCounts.getOrDefault(FinalResult.LOSE, 0);
+        final Integer draw = resultCounts.getOrDefault(FinalResult.DRAW, 0);
+        final String dealerMessage = String.format("%s: %d승 %d패 %d무", dealerName, lose, win, draw);
+        print(dealerMessage);
+
+        playerResults.forEach((player, finalResult) -> {
+            final String playerMessage = String.format("%s: %s", player.getDisplayName(), finalResult.getTitle());
+            print(playerMessage);
+        });
+    }
+
     private static void print(final String message) {
         System.out.println(message);
     }
 }
-
