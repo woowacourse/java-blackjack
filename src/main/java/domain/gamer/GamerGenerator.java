11b3dda58a97762ed5b362681f3d099b1d683fa0
jumdo12 <jumdo12@gmail.com>
2025-03-06 16:52:11 +0900

    🔴red(GamerGenerator): 플레이어 생성 기능 레드 테스트1

---
+++ src/main/java/domain/gamer/GamerGenerator.java
@@ -0,0 +1,12 @@
+package domain.gamer;
+
+import domain.card.CardGenerator;
+import domain.card.CardGroup;
+
+import java.util.List;
+
+public class GamerGenerator {
+    public static List<Player> generatePlayer(List<String> playerNames, CardGenerator cardGenerator){
+        return null;
+    }
+}
---
+++ src/test/java/domain/gamer/GamerGeneratorTest.java
@@ -0,0 +1,19 @@
+package domain.gamer;
+
+import domain.card.RandomCardGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GamerGeneratorTest {
+    @Test
+    void 플레이어_생성_테스트(){
+        List<String> playerNames = List.of("윌슨","가이온");
+        List<Player> playerList = GamerGenerator.generatePlayer(playerNames,new RandomCardGenerator());
+
+        assertThat(playerList).hasSize(2);
+    }
+}
