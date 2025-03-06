ea45721a85646af010ff1dc1eca463e606452e7c
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-06 17:07:53 +0900

    test : 메서드 수정에 따른 테스트 코드 수정

--- src/test/java/domain/CardsTest.java
+++ src/test/java/domain/CardsTest.java
@@ -8,20 +8,17 @@ import org.junit.jupiter.api.Test;
 
 public class CardsTest {
 
-    @DisplayName("기존 카드의 합이 21이 넘는다면 예외를 발생시킨다")
+    @DisplayName("기존 카드의 합이 21 이상이라면 true를 반환한다")
     @Test
     void test1() {
         //given
-        List<Card> testCards = List.of(new Card(CardNumberType.SIX, CardType.CLOVER),
+        List<Card> testCards = List.of(new Card(CardNumberType.THREE, CardType.CLOVER),
                 new Card(CardNumberType.EIGHT, CardType.DIAMOND),
                 new Card(CardNumberType.JACK, CardType.DIAMOND));
         Cards cards = new Cards(testCards);
 
         //when & then
-        assertThatThrownBy(
-                () -> cards.checkMaxSum()
-        ).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]");
+        assertThat(cards.isEqualAndMoreMaxSum()).isTrue();
     }
 
     @DisplayName("추가 배분 시 카드의 합을 구할 때, ACE는 모두 1로 계산하며 예외가 발생하지 않는다")
@@ -35,7 +32,7 @@ public class CardsTest {
         Cards cards = new Cards(testCards);
 
         //when
-        assertThatCode(() -> cards.checkMaxSum()).doesNotThrowAnyException();
+        assertThat(cards.isEqualAndMoreMaxSum()).isFalse();
     }
 
     @DisplayName("새로 배분된 카드를 저장한다")
