859b82ee29236a2b8be9e15147145e193d7564ef
jhpark1227 <junhyeok0246@naver.com>
2025-03-06 20:14:20 +0900

    feat: 딜러의 승패를 멤버변수로 저장하는 로직 구현

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -1,15 +1,19 @@
 package domain;
 
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Dealer extends Participant {
-
     public static final int DRAW_BOUNDARY = 16;
+
+    private final Map<GameResult, Integer> result;
 
     private Dealer(Cards cards) {
         super(cards);
+        this.result = new EnumMap<>(GameResult.class);
     }
 
     public static Dealer init() {
@@ -24,13 +28,18 @@ public class Dealer extends Participant {
         return this.getCardScore() <= DRAW_BOUNDARY;
     }
 
-    public Map<Participant, GameResult> getGameResult(List<Player> players) {
-        Map<Participant, GameResult> gameResult = new HashMap<>();
+    public Map<Player, GameResult> getGameResult(List<Player> players) {
+        Map<Player, GameResult> gameResult = new HashMap<>();
         for (Player player : players) {
             GameResult judge = GameResult.judge(player, this);
             gameResult.put(player, judge);
+            GameResult dealerResult = judge.getReverse();
+            result.put(dealerResult, result.getOrDefault(dealerResult, 0) + 1);
         }
-
         return gameResult;
+    }
+
+    public Map<GameResult, Integer> getResult() {
+        return Collections.unmodifiableMap(result);
     }
 }
