45018fbb365eb318662521f280229939cce9a2f3
Minuring <mw9505@gmail.com>
2025-03-06 15:38:50 +0900

    feat: 게임 결과값 도메인 구현

---
+++ src/main/java/domain/GameResult.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.Map;
+
+public class GameResult {
+
+    private final Map<Player, Winning> playerWinningResult;
+
+    public GameResult(Map<Player, Winning> playerWinningResult) {
+        this.playerWinningResult = playerWinningResult;
+    }
+
+    public int countDealerWin() {
+        return (int) playerWinningResult.values().stream()
+            .filter(winning -> winning == Winning.LOSE)
+            .count();
+    }
+
+    public int countDealerDraw() {
+        return (int) playerWinningResult.values().stream()
+            .filter(winning -> winning == Winning.DRAW)
+            .count();
+    }
+
+    public int countDealerLose() {
+        return (int) playerWinningResult.values().stream()
+            .filter(winning -> winning == Winning.WIN)
+            .count();
+    }
+}
---
+++ src/test/java/domain/GameResultTest.java
@@ -0,0 +1,39 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class GameResultTest {
+
+    private final Map<Player, Winning> playerWinningResult
+        = Map.ofEntries(
+        Map.entry(new Player("이름1"), Winning.WIN),
+        Map.entry(new Player("이름2"), Winning.DRAW),
+        Map.entry(new Player("이름3"), Winning.DRAW),
+        Map.entry(new Player("이름4"), Winning.LOSE),
+        Map.entry(new Player("이름5"), Winning.LOSE),
+        Map.entry(new Player("이름6"), Winning.LOSE)
+    );
+
+
+    @Test
+    void 딜러의_승무패_횟수를_계산한다() {
+        //given
+        GameResult gameResult = new GameResult(playerWinningResult);
+
+        //when
+        int dealerWinCount = gameResult.countDealerWin();
+        int dealerDrawCount = gameResult.countDealerDraw();
+        int dealerLoseCount = gameResult.countDealerLose();
+
+        //then
+        assertAll(
+            () -> assertThat(dealerWinCount).isEqualTo(3),
+            () -> assertThat(dealerDrawCount).isEqualTo(2),
+            () -> assertThat(dealerLoseCount).isEqualTo(1)
+        );
+    }
+}
