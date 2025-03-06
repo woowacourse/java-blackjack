f4750e62ef02f063432a82ef62e6192a42727514
changuii <rhljh7410@gmail.com>
2025-03-06 21:21:01 +0900

    refactor(CardHand): CardHand의 ACE 점수 의존을 CardNumber로 옮겨라

--- src/main/java/domain/CardHand.java
+++ src/main/java/domain/CardHand.java
@@ -6,40 +6,37 @@ import java.util.List;
 
 public class CardHand {
 
-    private final List<Card> cards;
+	private final List<Card> cards;
 
-    public CardHand() {
-        this.cards = new ArrayList<>();
-    }
+	public CardHand() {
+		this.cards = new ArrayList<>();
+	}
 
-    public CardHand(List<Card> cards) {
-        this.cards = new ArrayList<>(cards);
-    }
+	public CardHand(List<Card> cards) {
+		this.cards = new ArrayList<>(cards);
+	}
 
-    public void add(final Card card) {
-        cards.add(card);
-    }
+	public void add(final Card card) {
+		cards.add(card);
+	}
 
-    public int calculateAllScore() {
-        int score = 0;
-        int aceCount = calculateAceCount();
-        for (final Card card : cards) {
-            score = card.sumNumber(score);
-            if (score > 21 && aceCount > 0) {
-                aceCount--;
-                score -= 10;
-            }
-        }
-        return score;
-    }
+	public int calculateAllScore() {
+		int score = 0;
 
-    private int calculateAceCount() {
-        return (int)cards.stream()
-            .filter(card -> card.isMatchNumber(CardNumber.ACE))
-            .count();
-    }
+		for (final Card card : cards) {
+			score = card.sumNumber(score);
+		}
 
-    public List<Card> getCards() {
-        return Collections.unmodifiableList(cards);
-    }
+		return CardNumber.ifOverThanBustScoreAceIsMIN(score, calculateAceCount());
+	}
+
+	private int calculateAceCount() {
+		return (int)cards.stream()
+			.filter(card -> card.isMatchNumber(CardNumber.ACE))
+			.count();
+	}
+
+	public List<Card> getCards() {
+		return Collections.unmodifiableList(cards);
+	}
 }
--- src/main/java/domain/CardNumber.java
+++ src/main/java/domain/CardNumber.java
@@ -1,27 +1,36 @@
 package domain;
 
 public enum CardNumber {
-    ACE(11),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
+	ACE(11),
+	TWO(2),
+	THREE(3),
+	FOUR(4),
+	FIVE(5),
+	SIX(6),
+	SEVEN(7),
+	EIGHT(8),
+	NINE(9),
+	TEN(10),
+	JACK(10),
+	QUEEN(10),
+	KING(10);
 
-    private final int score;
+	private static final int ACE_MIN = 1;
+	private final int score;
 
-    CardNumber(final int score) {
-        this.score = score;
-    }
+	CardNumber(final int score) {
+		this.score = score;
+	}
 
-    public int sum(final int score) {
-        return this.score + score;
-    }
+	public static int ifOverThanBustScoreAceIsMIN(int score, int aceCount) {
+		while (score > 21 && aceCount-- > 0) {
+			score -= ACE.score;
+			score += ACE_MIN;
+		}
+		return score;
+	}
+
+	public int sum(final int score) {
+		return this.score + score;
+	}
 }
