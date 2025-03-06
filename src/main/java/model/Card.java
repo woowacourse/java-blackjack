de2372f50101c552e1c892485e64be443098f258
soeun2537 <soeun2537@gmail.com>
2025-03-06 17:21:19 +0900

    feat: Card 클래스 구현

---
+++ src/main/java/model/Card.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Card {
+
+    private final CardType cardType;
+    private final CardNumber cardNumber;
+
+    public Card(CardType cardType, CardNumber cardNumber) {
+        this.cardType = cardType;
+        this.cardNumber = cardNumber;
+    }
+
+    public boolean isAce() {
+        return cardNumber == CardNumber.ACE;
+    }
+
+    public CardNumber getCardNumber() {
+        return cardNumber;
+    }
+
+    public CardType getCardType() {
+        return cardType;
+    }
+}
---
+++ src/main/java/model/CardType.java
@@ -0,0 +1,9 @@
+package model;
+
+public enum CardType {
+
+    SPADE,
+    DIAMOND,
+    HEART,
+    CLOVER
+}
