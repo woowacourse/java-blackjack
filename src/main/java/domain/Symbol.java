b1307d0ebb901096297631eb9d271d7ad7738392
DongYeop <s01054830455@gmail.com>
2025-03-07 14:12:14 +0900

    feat: Symbol Enum 객체에 name 필드 추가

--- src/main/java/domain/Symbol.java
+++ src/main/java/domain/Symbol.java
@@ -1,8 +1,18 @@
 package domain;
 
 public enum Symbol {
-    SPADE,
-    HEART,
-    DIAMOND,
-    COLVER;
+    SPADE("스페이드"),
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    COLVER("클로버");
+
+    private final String name;
+
+    Symbol(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
