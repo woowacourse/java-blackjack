cd48050ff8c97a396d9ae69c498c4202dcd283ce
jhpark1227 <junhyeok0246@naver.com>
2025-03-07 16:23:10 +0900

    🟢green: 플레이어 중복 시 예외 발생 기능 구현

---
+++ src/main/java/domain/participant/Players.java
@@ -0,0 +1,21 @@
+package domain.participant;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Players {
+
+    private final Set<Player> players;
+
+    public Players(List<Player> players) {
+        validateDuplicate(players);
+        this.players = new HashSet<>(players);
+    }
+
+    private void validateDuplicate(List<Player> players) {
+        if (players.stream().distinct().count() != players.size()) {
+            throw new IllegalArgumentException("중복된 플레이어가 있습니다.");
+        }
+    }
+}
