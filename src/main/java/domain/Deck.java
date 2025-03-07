20e55f4fe4c0e0802bef55b658b388399445e32e
황재웅 <wodnd0131@naver.com>
2025-03-07 16:49:19 +0900

    feat(Deck): 카드를 모두 뽑았다면, 예외 처리하라

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -12,34 +12,37 @@ import java.util.stream.Stream;
 import constant.Emblem;
 
 public class Deck {
-	private final Queue<Card> deck;
+    private final Queue<Card> deck;
 
-	public Deck(final Queue<Card> deck) {
-		validateDuplicateCard(deck);
-		this.deck = new ArrayDeque<>(deck);
-	}
+    public Deck(final Queue<Card> deck) {
+        validateDuplicateCard(deck);
+        this.deck = new ArrayDeque<>(deck);
+    }
 
-	public static Deck createShuffledDeck() {
-		final List<Card> cards = Arrays.stream(CardNumber.values())
-			.flatMap(Deck::makeCards)
-			.collect(Collectors.toList());
-		Collections.shuffle(cards);
-		return new Deck(new ArrayDeque<>(cards));
-	}
+    public static Deck createShuffledDeck() {
+        final List<Card> cards = Arrays.stream(CardNumber.values())
+            .flatMap(Deck::makeCards)
+            .collect(Collectors.toList());
+        Collections.shuffle(cards);
+        return new Deck(new ArrayDeque<>(cards));
+    }
 
-	private static Stream<Card> makeCards(CardNumber cardNumber) {
-		return Arrays.stream(Emblem.values())
-			.map(emblem -> new Card(cardNumber, emblem));
-	}
+    private static Stream<Card> makeCards(CardNumber cardNumber) {
+        return Arrays.stream(Emblem.values())
+            .map(emblem -> new Card(cardNumber, emblem));
+    }
 
-	private void validateDuplicateCard(final Queue<Card> deck) {
-		final HashSet<Card> notDuplicateCards = new HashSet<>(deck);
-		if (deck.size() != notDuplicateCards.size()) {
-			throw new IllegalArgumentException("덱에는 중복된 카드가 들어올 수 없습니다!");
-		}
-	}
+    private void validateDuplicateCard(final Queue<Card> deck) {
+        final HashSet<Card> notDuplicateCards = new HashSet<>(deck);
+        if (deck.size() != notDuplicateCards.size()) {
+            throw new IllegalArgumentException("덱에는 중복된 카드가 들어올 수 없습니다!");
+        }
+    }
 
-	public Card pickCard() {
-		return deck.poll();
-	}
+    public Card pickCard() {
+        if (deck.isEmpty()) {
+            throw new NullPointerException("덱에 남아있는 카드가 없습니다.");
+        }
+        return deck.poll();
+    }
 }
