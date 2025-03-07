ae863e4a7beb1f86d306b8f0302f3bd47f0accf5
supernovaMK <minkikim2002@gmail.com>
2025-03-07 14:32:58 +0900

    test: 덱은 52장의 카드를 가지고 있어야함을 검증

--- src/test/java/domain/DeckTest.java
+++ src/test/java/domain/DeckTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -15,8 +16,24 @@ class DeckTest {
         @DisplayName("덱은 카드를 가지고 있어야한다.")
         @Test
         void validateNotNull() {
+            // given
             List<TrumpCard> nullCards = null;
+
+            // when & then
             assertThatThrownBy(() -> new Deck(nullCards)).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("덱의 카드는 52장 이어야한다.")
+        @Test
+        void validateSize() {
+            // given
+            List<TrumpCard> cards = new ArrayList<>();
+
+            // when
+            cards.add(TrumpCard.ACE_OF_CLUBS);
+
+            // then
+            assertThatThrownBy(() -> new Deck(cards)).isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
