2673a1fa68459da346c81e14ce106bc4e4d01a93
AppleMint98 <applemint9821@gmail.com>
2025-03-07 16:19:07 +0900

    refactor: Controller 메서드 정리

--- src/main/java/controller/BlackJackController.java
+++ src/main/java/controller/BlackJackController.java
@@ -22,25 +22,29 @@ public class BlackJackController {
     }
 
     public void run() {
-
         Deck deck = Deck.initialize();
         Players players = registerPlayers(inputView.readParticipantsNames(), deck);
         Dealer dealer = new Dealer(deck.drawInitialCards());
-
         outputView.printInitialGameSettings(players, dealer);
 
+        selectPlayersHitOrStand(players, deck);
+        checkDealerSumUnderSixteen(dealer, deck);
+        outputView.printGameSummary(players, dealer);
+        outputView.printGameResult(players.deriveResults(dealer.sumCardNumbers()));
+    }
+
+    private Players registerPlayers(List<String> names, Deck deck) {
+        return new Players(names.stream()
+                .map(Nickname::new)
+                .map(nickname -> new Player(nickname, deck.drawInitialCards()))
+                .toList());
+    }
+
+    private void selectPlayersHitOrStand(Players players, Deck deck) {
         for (Player player : players.getPlayers()) {
             selectHitOrStand(player, deck);
             outputView.printPlayerCards(player);
         }
-
-        while (dealer.isSumUnderSixteen()) {
-            outputView.printDealerOneMoreCardMessage();
-            dealer.addOneCard(deck.drawOneCard());
-        }
-
-        outputView.printGameSummary(players, dealer);
-        outputView.printGameResult(players.deriveResults(dealer.sumCardNumbers()));
     }
 
     private void selectHitOrStand(Player player, Deck deck) {
@@ -54,10 +58,10 @@ public class BlackJackController {
         }
     }
 
-    private Players registerPlayers(List<String> names, Deck deck) {
-        return new Players(names.stream()
-                .map(Nickname::new)
-                .map(nickname -> new Player(nickname, deck.drawInitialCards()))
-                .toList());
+    private void checkDealerSumUnderSixteen(Dealer dealer, Deck deck) {
+        while (dealer.isSumUnderSixteen()) {
+            outputView.printDealerOneMoreCardMessage();
+            dealer.addOneCard(deck.drawOneCard());
+        }
     }
 }
