989b74d2945290c322710e032dafaefc8f9f275e
taek2222 <oot309@daum.net>
2025-03-07 15:55:28 +0900

    test: Dealer, Gambler 테스트 추가

---
+++ src/test/java/blackjack/domain/DealerTest.java
@@ -0,0 +1,18 @@
+package blackjack.domain;
+
+import blackjack.domain.card.CardPack;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DealerTest {
+
+    @Test
+    @DisplayName("딜러는 한장의 카드만 공개한다")
+    void test() {
+        Players players = new Players();
+        players.initPlayers(new CardPack(new SortShuffle()));
+        int openedCardsSize = players.getDealer().getOpenedCards().size();
+        Assertions.assertThat(openedCardsSize).isEqualTo(1);
+    }
+}
