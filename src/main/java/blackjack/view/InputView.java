a9cb0021e92cdd0392f19029f8c0b2d1d4f70a49
MinJun Choi <mj043000@naver.com>
2025-03-07 13:52:47 +0900

    refactor: 변수를 상수로 변경
    Co-authored-by: Starlight258 <audwl03071@gmail.com>

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -7,9 +7,8 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
     private static final String LINE = System.lineSeparator();
-
-    private final String TITLE_ASK_MORE_CARD = "%s는 한장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)";
-    private final String TITLE_INPUT_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String TITLE_ASK_MORE_CARD = "%s는 한장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)";
+    private static final String TITLE_INPUT_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
 
     public String readNames() {
         System.out.println(TITLE_INPUT_NAMES);
