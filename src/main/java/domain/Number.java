bf85f3422779d7a199d6943ffba64d81597877ac
DongYeop <s01054830455@gmail.com>
2025-03-07 14:11:58 +0900

    feat: Number Enum 객체에 name 필드 추가

--- src/main/java/domain/Number.java
+++ src/main/java/domain/Number.java
@@ -2,25 +2,31 @@ package domain;
 
 public enum Number {
 
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10),
-    SOFT_ACE(11);
+    ACE("A", 1),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10),
+    SOFT_ACE("A", 11);
 
+    private final String name;
     private final Integer point;
 
-    Number(Integer point) {
+    Number(String name, Integer point) {
+        this.name = name;
         this.point = point;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Integer getPoint() {
