b4606992a9b0c50bd22918a31ca78fa263f80261
changuii <rhljh7410@gmail.com>
2025-03-07 15:30:49 +0900

    test(Dealer): dealer는 플레이어와 대결하고, 기록하라

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -1,29 +1,38 @@
 package domain;
 
 public class Dealer {
-    private final Participant participant;
+	private final Participant participant;
+	private int winCount = 0;
+	private int loseCount = 0;
 
-    public Dealer() {
-        this.participant = new Participant();
-    }
+	public Dealer() {
+		this.participant = new Participant();
+	}
 
-    public Dealer(final CardHand hand) {
-        this.participant = new Participant(hand);
-    }
+	public Dealer(final CardHand hand) {
+		this.participant = new Participant(hand);
+	}
 
-    public boolean isPickCard() {
-        return participant.calculateAllScore() <= 16;
-    }
+	public boolean isPickCard() {
+		return participant.calculateAllScore() <= 16;
+	}
 
-    public void pickCardOnFirstHandOut(final Deck deck) {
-        participant.pickCardOnFirstHandOut(deck);
-    }
+	public void pickCardOnFirstHandOut(final Deck deck) {
+		participant.pickCardOnFirstHandOut(deck);
+	}
 
-    public void pickCard(final Deck deck) {
-        participant.pickCard(deck);
-    }
+	public void pickCard(final Deck deck) {
+		participant.pickCard(deck);
+	}
 
-    public Participant getParticipant() {
-        return participant;
-    }
+	public void startDuel(final Player player) {
+	}
+
+	public Participant getParticipant() {
+		return participant;
+	}
+
+	public int getWinCount() {
+		return 0;
+	}
 }
--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayDeque;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,37 +13,64 @@ import constant.Emblem;
 
 public class DealerTest {
 
-    @Nested
-    @DisplayName("딜러가 카드를 받아야 하는지 여부를 반환한다.")
-    class IsPickCard {
+	@Nested
+	@DisplayName("딜러가 카드를 받아야 하는지 여부를 반환한다.")
+	class IsPickCard {
 
-        @DisplayName("16점 이하라면, 딜러가 카드를 뽑는 여부를 올바르게 반환한다.")
-        @Test
-        public void isPickCard() throws Exception {
-            // given
-            final var dealer = new Dealer();
+		@DisplayName("16점 이하라면, 딜러가 카드를 뽑는 여부를 올바르게 반환한다.")
+		@Test
+		public void isPickCard() throws Exception {
+			// given
+			final var dealer = new Dealer();
 
-            // when
-            final var actual = dealer.isPickCard();
+			// when
+			final var actual = dealer.isPickCard();
 
-            // then
-            assertThat(actual).isTrue();
-        }
+			// then
+			assertThat(actual).isTrue();
+		}
 
-        @DisplayName("16점 초과라면, 딜러가 카드를 뽑는 여부를 올바르게 반환한다.")
-        @Test
-        public void isPickCard1() throws Exception {
-            // given
-            final Card card1 = new Card(CardNumber.TEN, Emblem.CLUB);
-            final Card card2 = new Card(CardNumber.SEVEN, Emblem.HEART);
-            final CardHand cardHand = new CardHand(List.of(card1, card2));
-            final var dealer = new Dealer(cardHand);
+		@DisplayName("16점 초과라면, 딜러가 카드를 뽑는 여부를 올바르게 반환한다.")
+		@Test
+		public void isPickCard1() throws Exception {
+			// given
+			final Card card1 = new Card(CardNumber.TEN, Emblem.CLUB);
+			final Card card2 = new Card(CardNumber.SEVEN, Emblem.HEART);
+			final CardHand cardHand = new CardHand(List.of(card1, card2));
+			final var dealer = new Dealer(cardHand);
 
-            // when
-            final var actual = dealer.isPickCard();
+			// when
+			final var actual = dealer.isPickCard();
 
-            // then
-            assertThat(actual).isFalse();
-        }
-    }
+			// then
+			assertThat(actual).isFalse();
+		}
+	}
+
+	@Nested
+	@DisplayName("딜러는 플레이어와 대결한다.")
+	class StartDuel {
+
+		@DisplayName("딜러는 플레이와의 대결 결과를 올바르게, 기록한다.")
+		@Test
+		void startDuel() throws Exception {
+			// given
+			final Card card1 = new Card(CardNumber.TEN, Emblem.CLUB);
+			final Card card2 = new Card(CardNumber.SEVEN, Emblem.HEART);
+			final CardHand cardHand = new CardHand(List.of(card1, card2));
+			final var dealer = new Dealer(cardHand);
+
+			final Player player = new Player("Winner");
+			final List<Card> playerCards = List.of(new Card(CardNumber.ACE, Emblem.CLUB));
+			final Deck playerDeck = new Deck(new ArrayDeque<>(playerCards));
+			player.pickCard(playerDeck);
+
+			// when
+			dealer.startDuel(player);
+
+			// then
+			assertThat(dealer.getWinCount()).isEqualTo(1);
+		}
+
+	}
 }
