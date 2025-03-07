c66eedc00d0791ebc7c98d29996af5f3de43d7ee
ryan-dia <cjfdnjs158@naver.com>
2025-03-07 16:46:57 +0900

    test(Players): 플레이어 중복 실패하는 테스트 작성
    Co-authored-by:CheChe903<cheche903@naver.com>

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -38,7 +39,33 @@ class PlayersTest {
         // when & then
         assertThatThrownBy(() -> new Players(playerGroup))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @DisplayName("플레이어의 이름은 중복이 없으면 올바르게 작동한다.")
+    @Test
+    void 플레이어의_이름은_중복이_없으면_올바르게_작동한다() {
+        // given
+        Player player1 = new Player(new Nickname("체체"));
+        Player player2 = new Player(new Nickname("체체체"));
+        List<Player> playerGroup = List.of(player1, player2);
+
+        // when & then
+        assertThatCode(() -> new Players(playerGroup))
+                .doesNotThrowAnyException();
+
+    }
+
+    @DisplayName("플레이어의 이름은 중복될 경우 예외가 발생한다.")
+    @Test
+    void 플레이어의_이름은_중복될_경우_예외가_발생한다() {
+        // given
+        Player player1 = new Player(new Nickname("체체"));
+        Player player2 = new Player(new Nickname("체체"));
+        List<Player> playerGroup = List.of(player1, player2);
+
+        // when & then
+        assertThatThrownBy(() -> new Players(playerGroup))
+                .isInstanceOf(IllegalArgumentException.class);
 
     }
 
@@ -59,6 +86,4 @@ class PlayersTest {
                                 new Nickname("qqq")))
         );
     }
-
-
 }
