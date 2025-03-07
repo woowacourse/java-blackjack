345736d2adc06418505dea9e7b7f453e0ae46f30
7i.w__k <tizmfns1218@Naver.com>
2025-03-07 15:25:57 +0900

    style: 코드 정렬
    Co-authored-by: Jin409 <sualng123@gmail.com>

--- src/main/java/blackjack/domain/Hand.java
+++ src/main/java/blackjack/domain/Hand.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Set;
 
 public class Hand implements CardHolder {
+
     private final List<Card> cards;
 
     public Hand() {
--- src/main/java/blackjack/view/Parser.java
+++ src/main/java/blackjack/view/Parser.java
@@ -12,7 +12,6 @@ import java.util.stream.Collectors;
 
 public final class Parser {
 
-
     private Parser() {
     }
 
