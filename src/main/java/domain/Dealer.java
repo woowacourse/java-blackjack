1610ed8caac2a672dcf4a082bd0415d71224a2b3
황재웅 <wodnd0131@naver.com>
2025-03-07 17:52:17 +0900

    chore: 매직 넘버를 상수로 두어라

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -1,49 +1,50 @@
 package domain;
 
 public class Dealer {
-	private final Participant participant;
-	private int winCount = 0;
-	private int loseCount = 0;
+    private static final int DEALER_MAX_SCORE_FOR_PICK = 16;
+    private final Participant participant;
+    private int winCount = 0;
+    private int loseCount = 0;
 
-	public Dealer() {
-		this.participant = new Participant();
-	}
+    public Dealer() {
+        this.participant = new Participant();
+    }
 
-	public Dealer(final CardHand hand) {
-		this.participant = new Participant(hand);
-	}
+    public Dealer(final CardHand hand) {
+        this.participant = new Participant(hand);
+    }
 
-	public boolean isPickCard() {
-		return participant.calculateAllScore() <= 16;
-	}
+    public boolean isPickCard() {
+        return participant.calculateAllScore() <= DEALER_MAX_SCORE_FOR_PICK;
+    }
 
-	public void pickCardOnFirstHandOut(final Deck deck) {
-		participant.pickCardOnFirstHandOut(deck);
-	}
+    public void pickCardOnFirstHandOut(final Deck deck) {
+        participant.pickCardOnFirstHandOut(deck);
+    }
 
-	public void pickCard(final Deck deck) {
-		participant.pickCard(deck);
-	}
+    public void pickCard(final Deck deck) {
+        participant.pickCard(deck);
+    }
 
-	public void startDuel(final Player player) {
-		player.duel(participant);
-		final boolean isWinPlayerDuelResult = player.getIsWinDuel();
-		if (isWinPlayerDuelResult) {
-			loseCount++;
-			return;
-		}
-		winCount++;
-	}
+    public void startDuel(final Player player) {
+        player.duel(participant);
+        final boolean isWinPlayerDuelResult = player.getIsWinDuel();
+        if (isWinPlayerDuelResult) {
+            loseCount++;
+            return;
+        }
+        winCount++;
+    }
 
-	public Participant getParticipant() {
-		return participant;
-	}
+    public Participant getParticipant() {
+        return participant;
+    }
 
-	public int getWinCount() {
-		return winCount;
-	}
+    public int getWinCount() {
+        return winCount;
+    }
 
-	public int getLoseCount() {
-		return loseCount;
-	}
+    public int getLoseCount() {
+        return loseCount;
+    }
 }
--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,56 +1,58 @@
 package domain;
 
 public class Player {
-	private final String name;
-	private final Participant participant;
-	private boolean isWinDuel;
+    private static final int MAX_SCORE = 21;
+    private final String name;
+    private final Participant participant;
+	
+    private boolean isWinDuel;
 
-	public Player(final String name) {
-		this.participant = new Participant();
-		this.name = name;
-		isWinDuel = false;
-	}
+    public Player(final String name) {
+        this.participant = new Participant();
+        this.name = name;
+        isWinDuel = false;
+    }
 
-	public Player(final Participant participant) {
-		this.participant = participant;
-		this.name = "";
-		isWinDuel = false;
-	}
+    public Player(final Participant participant) {
+        this.participant = participant;
+        this.name = "";
+        isWinDuel = false;
+    }
 
-	public boolean isPickCard() {
-		return participant.calculateAllScore() <= 21;
-	}
+    public boolean isPickCard() {
+        return participant.calculateAllScore() <= MAX_SCORE;
+    }
 
-	public void pickCardOnFirstHandOut(final Deck deck) {
-		participant.pickCardOnFirstHandOut(deck);
-	}
+    public void pickCardOnFirstHandOut(final Deck deck) {
+        participant.pickCardOnFirstHandOut(deck);
+    }
 
-	public void pickCard(final Deck deck) {
-		participant.pickCard(deck);
-	}
+    public void pickCard(final Deck deck) {
+        participant.pickCard(deck);
+    }
 
-	public void duel(final Participant other) {
-		final int score = participant.calculateAllScore();
-		final int otherScore = other.calculateAllScore();
-		if (score > 21) {
-			return;
-		}
-		if (otherScore > 21) {
-			isWinDuel = true;
-			return;
-		}
-		isWinDuel = score > otherScore;
-	}
+    public void duel(final Participant other) {
+        final int score = participant.calculateAllScore();
+        final int otherScore = other.calculateAllScore();
+        if (score > MAX_SCORE) {
+            return;
+        }
+        if (otherScore > MAX_SCORE) {
+            isWinDuel = true;
+            return;
+        }
+        isWinDuel = score > otherScore;
+    }
 
-	public boolean getIsWinDuel() {
-		return isWinDuel;
-	}
+    public boolean getIsWinDuel() {
+        return isWinDuel;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Participant getParticipant() {
-		return participant;
-	}
+    public Participant getParticipant() {
+        return participant;
+    }
 }
