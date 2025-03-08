bd440a86a09790313fe5bfa20988bc50acaa16fb
7i.w__k <tizmfns1218@Naver.com>
2025-03-08 17:36:47 +0900

    test: balckjack result manager test 추가

--- src/test/java/blackjack/manager/BlackJackResultManagerTest.java
+++ src/test/java/blackjack/manager/BlackJackResultManagerTest.java
@@ -6,7 +6,10 @@ import blackjack.StubPossibleSumCardHolder;
 import blackjack.domain.Dealer;
 import blackjack.domain.GameResultType;
 import blackjack.domain.Player;
+import blackjack.domain.Players;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -52,5 +55,93 @@ class BlackJackResultManagerTest {
 
         // then
         assertThat(gameResultType).isEqualTo(GameResultType.TIE);
+    }
+
+    @DisplayName("결과를 연산한다.")
+    @Test
+    void test5() {
+        GameRuleEvaluator gameRuleEvaluator = new GameRuleEvaluator();
+
+        Dealer dealer = new Dealer(new StubPossibleSumCardHolder(List.of(15, 20, 21)));
+
+        ArrayList<Player> playerList = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            playerList.add(new Player("꾹이", new StubPossibleSumCardHolder(List.of(15, 19, 20))));
+        }
+
+        Players players = Players.from(playerList);
+
+        blackJackResultManager.calculateCardResult(players, dealer, gameRuleEvaluator);
+
+        Map<GameResultType, Integer> dealerResult = blackJackResultManager.getDealerResult();
+
+        assertThat(dealerResult).containsEntry(GameResultType.WIN, 5);
+    }
+
+    @DisplayName("플레이어와 딜러가 busted 라면 무를 반환한다.")
+    @Test
+    void test6() {
+        GameRuleEvaluator gameRuleEvaluator = new GameRuleEvaluator();
+
+        Dealer dealer = new Dealer(new StubPossibleSumCardHolder(List.of(22)));
+
+        ArrayList<Player> playerList = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            playerList.add(new Player("꾹이", new StubPossibleSumCardHolder(List.of(22))));
+        }
+
+        Players players = Players.from(playerList);
+
+        blackJackResultManager.calculateCardResult(players, dealer, gameRuleEvaluator);
+
+        Map<GameResultType, Integer> dealerResult = blackJackResultManager.getDealerResult();
+
+        assertThat(dealerResult).containsEntry(GameResultType.TIE, 5);
+    }
+
+    @DisplayName("플레이어만 busted라면 딜러가 승리한다.")
+    @Test
+    void test7() {
+        GameRuleEvaluator gameRuleEvaluator = new GameRuleEvaluator();
+
+        Dealer dealer = new Dealer(new StubPossibleSumCardHolder(List.of(21)));
+
+        ArrayList<Player> playerList = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            playerList.add(new Player("꾹이", new StubPossibleSumCardHolder(List.of(22))));
+        }
+
+        Players players = Players.from(playerList);
+
+        blackJackResultManager.calculateCardResult(players, dealer, gameRuleEvaluator);
+
+        Map<GameResultType, Integer> dealerResult = blackJackResultManager.getDealerResult();
+
+        assertThat(dealerResult).containsEntry(GameResultType.WIN, 5);
+    }
+
+    @DisplayName("딜러만 busted라면 플레이어가 승리한다.")
+    @Test
+    void test8() {
+        GameRuleEvaluator gameRuleEvaluator = new GameRuleEvaluator();
+
+        Dealer dealer = new Dealer(new StubPossibleSumCardHolder(List.of(22)));
+
+        ArrayList<Player> playerList = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            playerList.add(new Player("꾹이", new StubPossibleSumCardHolder(List.of(21))));
+        }
+
+        Players players = Players.from(playerList);
+
+        blackJackResultManager.calculateCardResult(players, dealer, gameRuleEvaluator);
+
+        Map<GameResultType, Integer> dealerResult = blackJackResultManager.getDealerResult();
+
+        assertThat(dealerResult).containsEntry(GameResultType.LOSE, 5);
     }
 }
