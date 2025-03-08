207a66a6d53c743fafc6dd053f125834366eac09
changuii <rhljh7410@gmail.com>
2025-03-08 19:45:39 +0900

    feat(DuelHistory): 승패를 기록하라

---
+++ src/main/java/constant/DuelResult.java
@@ -0,0 +1,5 @@
+package constant;
+
+public enum DuelResult {
+	WIN, LOSE
+}
---
+++ src/main/java/domain/DuelHistory.java
@@ -0,0 +1,23 @@
+package domain;
+
+import constant.DuelResult;
+
+public class DuelHistory {
+	private int winCount;
+	private int loseCount;
+
+	public void write(final DuelResult duelResult) {
+		switch (duelResult) {
+			case WIN -> winCount++;
+			case LOSE -> loseCount++;
+		}
+	}
+
+	public int getWinCount() {
+		return winCount;
+	}
+
+	public int getLoseCount() {
+		return loseCount;
+	}
+}
