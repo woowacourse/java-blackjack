dcfde71141f0a09271ab1f91c069cf38b90a48ff
jhpark1227 <junhyeok0246@naver.com>
2025-03-08 16:40:05 +0900

    refactor: 초기 상태의 카드들을 상수화

--- src/main/java/domain/card/cardsGenerator/RandomCardsGenerator.java
+++ src/main/java/domain/card/cardsGenerator/RandomCardsGenerator.java
@@ -8,15 +8,22 @@ import java.util.Collections;
 import java.util.List;
 
 public class RandomCardsGenerator implements CardsGenerator {
-    @Override
-    public List<Card> generate() {
+
+    private static final List<Card> cards = createCards();
+
+    private static List<Card> createCards() {
         List<Card> cards = new ArrayList<>();
         for (CardNumber number : CardNumber.values()) {
             for (CardShape shape : CardShape.values()) {
                 cards.add(new Card(number, shape));
             }
         }
-        Collections.shuffle(cards);
         return cards;
+    }
+
+    @Override
+    public List<Card> generate() {
+        Collections.shuffle(cards);
+        return Collections.unmodifiableList(cards);
     }
 }
