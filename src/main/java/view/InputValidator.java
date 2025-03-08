5adab06b0de4dd32f75ecd3cbc076dcb80516316
ljhee92 <weilainib7@gmail.com>
2025-03-08 20:52:48 +0900

    refactor: 정규표현식 사용하여 검증 시 Pattern 사용

--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -1,13 +1,15 @@
 package view;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
 
-    private static final String NAMES_REGEX = "([a-zA-Z]+)(,[a-zA-Z]+)*";
+    private static final Pattern NAMES_PATTERN = Pattern.compile("([a-zA-Z]+)(,[a-zA-Z]+)*");
 
     private InputValidator() {}
 
     public static void validateNameFormat(String input) {
-        if (!input.matches(NAMES_REGEX)) {
+        if (!NAMES_PATTERN.matcher(input).matches()) {
             throw new IllegalArgumentException("[ERROR] (,)로 구분된 한글자 이상의 영어 이름을 입력해주세요.");
         }
     }
