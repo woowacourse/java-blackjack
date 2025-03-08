248398d9a5027e88cad826d272e2d268d38378f8
sonjh919 <kingjh1125@gmail.com>
2025-03-08 17:50:22 +0900

    test: 딜러 버스트 상황에 대한 결과 테스트 추가

--- src/test/java/domain/MatchResultTest.java
+++ src/test/java/domain/MatchResultTest.java
@@ -14,17 +14,20 @@ import org.junit.jupiter.params.provider.MethodSource;
 class MatchResultTest {
     @ParameterizedTest
     @DisplayName("결과 산출 테스트")
-    @MethodSource("provideSumForCalculateWinner")
-    void calculateWinnerTest(int dealerSum, int playerSum, MatchResult matchResult) {
-        assertThat(MatchResult.calculateWinner(dealerSum, playerSum)).isEqualTo(matchResult);
+    @MethodSource("provideSumForCalculateResultOfPlayer")
+    void calculateResultOfPlayerTest(int dealerSum, int playerSum, MatchResult matchResult) {
+        assertThat(MatchResult.calculateResultOfPlayer(dealerSum, playerSum)).isEqualTo(matchResult);
     }
 
-    private static Stream<Arguments> provideSumForCalculateWinner() {
+    private static Stream<Arguments> provideSumForCalculateResultOfPlayer() {
         return Stream.of(Arguments.of(
+                25, 25, LOSE,
+                25, 27, LOSE,
+                21, 23, LOSE,
+                20,10,LOSE,
+                25, 20, WIN,
                 10,20,WIN,
-                20,20,DRAW,
-                20,10,LOSE
+                20,20,DRAW
         ));
     }
-
 }
