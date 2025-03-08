20dd315b961299bad04d18c6378f5989b759f6c4
jumdo12 <jumdo12@gmail.com>
2025-03-08 23:34:36 +0900

    🔵refactor(Dealer): Dealer 클래스에서 Player를 포함하도록 수정

--- src/main/java/domain/gamer/Dealer.java
+++ src/main/java/domain/gamer/Dealer.java
@@ -1,26 +1,47 @@
 package domain.gamer;
 
+import domain.card.Card;
 import domain.card.CardGenerator;
 import domain.card.CardGroup;
 
-public class Dealer extends Player {
+import java.util.List;
+
+public class Dealer{
     private static final String DEALER_NAME = "NEO";
     private final int DEALER_HIT_ROLE = 16;
 
+    private final Player player;
+
     public Dealer(CardGroup cardGroup, CardGenerator cardGenerator) {
-        super(DEALER_NAME,cardGroup, cardGenerator);
+        player = new Player(DEALER_NAME,cardGroup,cardGenerator);
     }
 
-    public boolean isLessThen(int score) {
-        return this.cardGroup.calculateScore(Player.LIMIT) <= score;
+    public int calculateScore() {
+        return player.calculateScore();
+    }
+
+    public void receiveCard(int count){
+        player.receiveCard(count);
     }
 
     public int giveCardsToDealer() {
         int count = 0;
         while (isLessThen(DEALER_HIT_ROLE)) {
-            receiveCard();
+            player.receiveCard();
             count++;
         }
         return count;
+    }
+
+    public boolean isLessThen(int score) {
+        return player.calculateScore() <= score;
+    }
+
+    public boolean isBust(){
+        return player.isBust();
+    }
+
+    public List<Card> getCards(){
+        return player.getCards();
     }
 }
