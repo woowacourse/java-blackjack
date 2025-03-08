7f9d4d0d4474e7b08ea9a09dcdecaeabf91192e5
Dompoo <dlckdrms0517@gmail.com>
2025-03-08 22:58:49 +0900

    refactor: 블랙잭 숫자합을 계산하는 로직을 가독성 좋게 리팩터링

--- src/main/java/blackjack/domain/card_hand/BlackjackCardHand.java
+++ src/main/java/blackjack/domain/card_hand/BlackjackCardHand.java
@@ -3,8 +3,6 @@ package blackjack.domain.card_hand;
 import blackjack.domain.card.Card;
 import blackjack.domain.deck.BlackjackCardHandInitializer;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public final class BlackjackCardHand {
@@ -19,52 +17,43 @@ public final class BlackjackCardHand {
     }
     
     public int getBlackjackSum() {
-        final List<List<Integer>> availableNumbers = cardHand.getCards().stream()
-                .map(Card::getBlackjackValue)
-                .toList();
+        List<Integer> sums = calculatePossibleSums();
+        int minSum = findMinimumSum(sums);
         
-        List<Integer> availableSum = createAvailableSum(availableNumbers);
-        int minAvailableSum = calculateMinAvailableSum(availableSum);
-        
-        if (minAvailableSum > BUST_THRESHOLD) {
-            return minAvailableSum;
+        if (minSum > BUST_THRESHOLD) {
+            return minSum;
         }
-        return calculateClosestToBlackJack(availableSum);
+        return findClosestToBlackjack(sums);
     }
     
-    private static Integer calculateMinAvailableSum(final List<Integer> availableSum) {
-        return availableSum.stream()
-                .min(Integer::compareTo)
-                .orElseThrow();
-    }
-    
-    private List<Integer> createAvailableSum(final List<List<Integer>> availableNumbers) {
-        return availableNumbers.stream()
+    private List<Integer> calculatePossibleSums() {
+        return cardHand.getCards().stream()
+                .map(Card::getBlackjackValue)
                 .reduce(
-                        Collections.singletonList(0),
-                        this::calculateAvailableSum
+                        List.of(0),
+                        this::addCardValues
                 );
     }
     
-    private Integer calculateClosestToBlackJack(final List<Integer> availableSum) {
-        return availableSum.stream()
-                .filter(number -> number <= BUST_THRESHOLD)
+    private List<Integer> addCardValues(List<Integer> currentSums, List<Integer> cardValues) {
+        return currentSums.stream()
+                .flatMap(currentSum -> cardValues.stream()
+                        .map(value -> currentSum + value))
+                .distinct()
+                .toList();
+    }
+    
+    private int findMinimumSum(List<Integer> sums) {
+        return sums.stream()
+                .min(Integer::compareTo)
+                .orElseThrow(IllegalStateException::new);
+    }
+    
+    private int findClosestToBlackjack(List<Integer> sums) {
+        return sums.stream()
+                .filter(sum -> sum <= BUST_THRESHOLD)
                 .max(Integer::compareTo)
-                .orElseThrow();
-    }
-    
-    private List<Integer> calculateAvailableSum(final List<Integer> availableNumber, List<Integer> availableSum) {
-        final ArrayList<Integer> newList = new ArrayList<>();
-        for (Integer currentSum : availableSum) {
-            addAvailableSums(availableNumber, currentSum, newList);
-        }
-        return newList;
-    }
-    
-    private void addAvailableSums(final List<Integer> availableNumber, final Integer currentSum, final ArrayList<Integer> newList) {
-        for (Integer number : availableNumber) {
-            newList.add(currentSum + number);
-        }
+                .orElseThrow(IllegalStateException::new);
     }
     
     public List<Card> getCards() {
