d3a40e4173eaaf0bfa8a424f2442223587d578ad
jumdo12 <jumdo12@gmail.com>
2025-03-09 01:31:46 +0900

    🔵test: DisplayName 작성

--- src/test/java/domain/CardGroupTest.java
+++ src/test/java/domain/CardGroupTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import domain.card.Card;
 import domain.card.CardGroup;
 import domain.card.CardType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -49,6 +50,7 @@ public class CardGroupTest {
             assertThat(cardGroup.countCards()).isEqualTo(2);
         }
 
+        @DisplayName("다이아몬드2 + 하트3 = 5점이다")
         @Test
         void 카드_점수를_계산한다1() {
             //given
@@ -63,6 +65,7 @@ public class CardGroupTest {
             assertThat(score).isEqualTo(5);
         }
 
+        @DisplayName("다이아3 + 하트4 = 7점")
         @Test
         void 카드_점수를_계산한다2() {
             //given
@@ -77,6 +80,7 @@ public class CardGroupTest {
             assertThat(score).isEqualTo(7);
         }
 
+        @DisplayName("다이아J + 하트A = 21점")
         @Test
         void 카드에_에이스가_포함되어_있을때_점수를_계산한다() {
             //given
@@ -92,6 +96,7 @@ public class CardGroupTest {
             assertThat(score).isEqualTo(21);
         }
 
+        @DisplayName("다이아J 하트J 하트A = 21점이다")
         @Test
         void 카드에_에이스가_포함되어_있을때_점수를_계산한다2() {
             //given
@@ -108,6 +113,7 @@ public class CardGroupTest {
             assertThat(score).isEqualTo(21);
         }
 
+        @DisplayName("다이아J 클로버A 하트A = 12점이다")
         @Test
         void 카드에_에이스가_포함되어_있을때_점수를_계산한다3() {
             //given
