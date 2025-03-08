57605bddd02276b43fbcdb6a907e645896c6c656
Dompoo <dlckdrms0517@gmail.com>
2025-03-08 21:33:26 +0900

    refactor: WinningStatus 결정 로직을 가독성 좋게 리팩터링

--- src/main/java/blackjack/domain/WinningStatus.java
+++ src/main/java/blackjack/domain/WinningStatus.java
@@ -14,30 +14,65 @@ public enum WinningStatus {
             final BlackjackWinDeterminable myHand,
             final BlackjackWinDeterminable opponentHand
     ) {
-        if (isBust(myHand) || isBust(opponentHand)) {
-            if (isBust(myHand) && isBust(opponentHand)) {
-                return WinningStatus.무승부;
-            }
-            if (isBust(myHand)) {
-                return WinningStatus.패배;
-            }
-            return WinningStatus.승리;
+        if (isBustCondition(myHand, opponentHand)) {
+            return getBustConditionResult(myHand, opponentHand);
         }
-        
-        if (myHand.getBlackjackSum() == opponentHand.getBlackjackSum()) {
-            if (isBlackjack(myHand) && !isBlackjack(opponentHand)) {
-                return WinningStatus.승리;
-            }
-            if (!isBlackjack(myHand) && isBlackjack(opponentHand)) {
-                return WinningStatus.패배;
-            }
-            return WinningStatus.무승부;
+        if (isSameScoreCondition(myHand, opponentHand)) {
+            return getSameScoreConditionResult(myHand, opponentHand);
         }
-        
         if (myHand.getBlackjackSum() > opponentHand.getBlackjackSum()) {
             return WinningStatus.승리;
         }
         return WinningStatus.패배;
+    }
+    
+    private static boolean isBustCondition(
+            final BlackjackWinDeterminable myHand,
+            final BlackjackWinDeterminable opponentHand
+    ) {
+        return isBust(myHand) || isBust(opponentHand);
+    }
+    
+    private static WinningStatus getBustConditionResult(
+            final BlackjackWinDeterminable myHand,
+            final BlackjackWinDeterminable opponentHand
+    ) {
+        final boolean isMyHandBust = isBust(myHand);
+        final boolean isOpponentHandBust = isBust(opponentHand);
+        
+        if (isMyHandBust && isOpponentHandBust) {
+            return WinningStatus.무승부;
+        }
+        if (isMyHandBust) {
+            return WinningStatus.패배;
+        }
+        if (isOpponentHandBust) {
+            return WinningStatus.승리;
+        }
+        return WinningStatus.무승부;
+    }
+    
+    private static boolean isSameScoreCondition(
+            final BlackjackWinDeterminable myHand,
+            final BlackjackWinDeterminable opponentHand
+    ) {
+        return myHand.getBlackjackSum() == opponentHand.getBlackjackSum();
+    }
+    
+    private static WinningStatus getSameScoreConditionResult(
+            final BlackjackWinDeterminable myHand,
+            final BlackjackWinDeterminable opponentHand
+    ) {
+        final boolean isMyHandBlackjack = isBlackjack(myHand);
+        final boolean isOpponentHandBlackjack = isBlackjack(opponentHand);
+        
+        if (isMyHandBlackjack && !isOpponentHandBlackjack) {
+            return WinningStatus.승리;
+        }
+        if (!isMyHandBlackjack && isOpponentHandBlackjack) {
+            return WinningStatus.패배;
+        }
+        return WinningStatus.무승부;
     }
     
     private static boolean isBust(final BlackjackWinDeterminable cardHand) {
