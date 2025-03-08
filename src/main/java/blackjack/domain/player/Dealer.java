29f7bfddfddb29c6377889d29086454a8b00264b
junho <wnsgh732@gmail.com>
2025-03-08 22:21:27 +0900

    reafctor: Dealer 객체 생성 로직 변경

--- src/main/java/blackjack/domain/player/Dealer.java
+++ src/main/java/blackjack/domain/player/Dealer.java
@@ -8,9 +8,10 @@ public class Dealer extends Player {
 
     private static final int DEALER_OPENED_CARD_LOWER_BOUND = 0;
     private static final int DEALER_OPENED_CARD_UPPER_BOUND = 1;
+    private static final String DEALER_NAME = "딜러";
 
     public Dealer() {
-        super();
+        super(DEALER_NAME);
     }
 
     @Override
