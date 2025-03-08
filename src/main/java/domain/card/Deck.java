5152896dbe8cdfd6ade65301ddda81508545c5f1
jhpark1227 <junhyeok0246@naver.com>
2025-03-08 16:14:43 +0900

    refactor: Deck의 카드들의 자료구조를 List에서 Stack으로 변경

--- src/main/java/domain/card/Deck.java
+++ src/main/java/domain/card/Deck.java
@@ -5,25 +5,24 @@ import domain.participant.Dealer;
 import domain.participant.Participant;
 import domain.participant.Player;
 import domain.participant.Players;
-import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 
 public class Deck {
     private final static int INIT_COUNT = 2;
 
-    private final List<Card> cards;
+    private final Stack<Card> cards;
 
     public Deck(CardsGenerator cardsGenerator) {
-        cards = cardsGenerator.generate();
+        cards = new Stack<>();
+        cards.addAll(cardsGenerator.generate());
     }
 
     public Card pick() {
         if (cards.isEmpty()) {
             throw new IllegalStateException("카드가 부족합니다.");
         }
-        Card card = cards.getLast();
-        cards.removeLast();
-        return card;
+        return cards.pop();
     }
 
     public void giveCardTo(Participant participant) {
