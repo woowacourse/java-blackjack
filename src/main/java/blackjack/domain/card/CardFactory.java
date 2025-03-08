7bd268b1e2bc082372f5308285bc06e49f6e8f21
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-03-09 00:37:02 +0900

    feat: 카드 캐싱 기능 추가

--- src/main/java/blackjack/domain/card/CardFactory.java
+++ src/main/java/blackjack/domain/card/CardFactory.java
@@ -1,11 +1,30 @@
 package blackjack.domain.card;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CardFactory {
+
+    private static final Map<String, Card> CACHE = new HashMap<>();
+
+    static {
+        for (CardSuit suit : CardSuit.values()) {
+            for (CardRank rank : CardRank.values()) {
+                String key = suit.name() + "_" + rank.name();
+                CACHE.put(key, resolveCard(suit, rank));
+            }
+        }
+    }
 
     private CardFactory() {
     }
 
     public static Card create(CardSuit suit, CardRank rank) {
+        String key = suit.name() + "_" + rank.name();
+        return CACHE.get(key);
+    }
+
+    private static Card resolveCard(CardSuit suit, CardRank rank) {
         if (rank.isAce()) {
             return AceCard.from(suit);
         }
