81667dc2ede939bbf022c8e772f21a43ed9c80f3
jumdo12 <jumdo12@gmail.com>
2025-03-09 15:36:57 +0900

    🔵refactor(InputView): 최대 플레이 가능한 인원 수 를 7명으로 제한

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,11 +7,20 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private final int MAX_PLAYER = 7;
 
     public List<String> readPlayers() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         final String input = readInput();
-        return Arrays.stream(input.split(",")).map(String::trim).toList();
+        final String[] parsedInput = input.split(",");
+        validatePlayerNumber(parsedInput);
+        return Arrays.stream(parsedInput).map(String::trim).toList();
+    }
+
+    private void validatePlayerNumber(String[] playerNames){
+        if(playerNames.length > MAX_PLAYER){
+            throw new IllegalArgumentException("[ERROR] 최대 플레이 가능한 인원 수는 7명 입니다.");
+        }
     }
 
     public AnswerCommand readAnswer(final String playerName) {
