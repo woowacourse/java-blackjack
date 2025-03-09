0d610e3b9d81c86f46f3db94bcf41149791e297a
junhaa <2171326@hansung.ac.kr>
2025-03-09 14:49:32 +0900

    fix: burst -> bust로 잘못된 부분 수정

--- src/main/java/domain/BlackjackParticipant.java
+++ src/main/java/domain/BlackjackParticipant.java
@@ -7,7 +7,7 @@ import java.util.List;
 public abstract class BlackjackParticipant {
 
     private static final String INVALID_CARD_STATE = "비정상적인 카드 추가입니다. 플레이어는 21장 이상 받을 수 없습니다";
-    private static final int BURST_STANDARD = 21;
+    private static final int BUST_STANDARD = 21;
     private static final int ACE_DIFF = 10;
     private static final String INVALID_NAME = "닉네임은 공백일 수 없습니다";
 
@@ -26,7 +26,7 @@ public abstract class BlackjackParticipant {
     }
 
     public void addDraw(TrumpCard trumpCard) {
-        if (isBurst(calculateCardSum())) {
+        if (isBust(calculateCardSum())) {
             throw new IllegalStateException(INVALID_CARD_STATE);
         }
         trumpCards.add(trumpCard);
@@ -47,19 +47,19 @@ public abstract class BlackjackParticipant {
     }
 
     private int calculateAceIncludeSum(int aceCount, int sum) {
-        if (isBurst(sum) && aceCount != 0) {
+        if (isBust(sum) && aceCount != 0) {
             return calculateAceIncludeSum(aceCount - 1, sum - ACE_DIFF);
         }
         return sum;
     }
 
-    private boolean isBurst(int number) {
-        return BURST_STANDARD < number;
+    private boolean isBust(int number) {
+        return BUST_STANDARD < number;
     }
 
     public boolean isDrawable() {
         int sum = calculateCardSum();
-        return !isBurst(sum);
+        return !isBust(sum);
     }
 
     public List<TrumpCard> trumpCards() {
