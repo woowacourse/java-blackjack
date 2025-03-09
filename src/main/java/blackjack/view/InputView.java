d0c953ff5dc19d6992f9645d87d696a1e8bb0a99
JO-eusan <goodsan0330@gmail.com>
2025-03-09 20:33:17 +0900

    refactor: 사용자 이름 안내 문구 변경

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
 
     public List<String> readNames() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        System.out.println("게임에 참여할 사람의 이름을 영어/한글로 입력하세요.(쉼표 기준으로 분리)");
         String input = scanner.nextLine();
 
         validateBlank(input);
