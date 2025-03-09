99c93dba0e6fb3127a904a932ad54609c182ad0c
ykmxxi <ykmxxi97@gmail.com>
2025-03-09 16:11:57 +0900

    refactor: 카드덱 생성 시 stream 연산을 사용하도록 변경

--- src/main/java/blackjack/model/card/initializer/DefaultCardDeckInitializer.java
+++ src/main/java/blackjack/model/card/initializer/DefaultCardDeckInitializer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import blackjack.model.card.Card;
 import blackjack.model.card.CardNumber;
@@ -14,19 +15,19 @@ public class DefaultCardDeckInitializer implements CardDeckInitializer {
 
     @Override
     public Cards initialize() {
-        return new Cards(initializeCardDeck());
+        return new Cards(initializeShuffledCardDeck());
     }
 
-    private List<Card> initializeCardDeck() {
-        List<Card> cards = new ArrayList<>();
-        Arrays.stream(CardType.values())
-                .forEach(cardType -> Arrays.stream(CardNumber.values())
-                        .forEach(
-                                cardNumber -> cards.add(new Card(cardType, cardNumber))
-                        )
+    private List<Card> initializeShuffledCardDeck() {
+        return Arrays.stream(CardType.values())
+                .flatMap(cardType -> Arrays.stream(CardNumber.values())
+                        .map(cardNumber -> new Card(cardType, cardNumber))
+                ).collect(Collectors.collectingAndThen(Collectors.toCollection(ArrayList::new),
+                        list -> {
+                            Collections.shuffle(list);
+                            return list;
+                        })
                 );
-        Collections.shuffle(cards);
-        return cards;
     }
 
 }
