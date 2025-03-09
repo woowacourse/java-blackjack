5a3cce11ba5cd34aa232fd63e5a15b0995a37fda
taek2222 <oot309@daum.net>
2025-03-09 17:23:30 +0900

    test: 딜러의 카드가 없는 경우 빈 리스트  반환 테스트 추가

--- src/test/java/blackjack/domain/DealerTest.java
+++ src/test/java/blackjack/domain/DealerTest.java
@@ -1,18 +1,33 @@
 package blackjack.domain;
 
+import blackjack.domain.card.Card;
 import blackjack.domain.card.CardPack;
-import org.assertj.core.api.Assertions;
+import blackjack.domain.player.Dealer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DealerTest {
 
     @Test
     @DisplayName("딜러는 한장의 카드만 공개한다")
-    void test() {
+    void dealerRevealsOnlyOneCard() {
         Players players = new Players();
         players.initPlayers(new CardPack(new SortShuffle()));
         int openedCardsSize = players.getDealer().getOpenedCards().size();
-        Assertions.assertThat(openedCardsSize).isEqualTo(1);
+        assertThat(openedCardsSize).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("딜러의 카드가 없는 경우 빈 리스트를 반환한다")
+    void returnsEmptyListWhenDealerHasNoCards() {
+        Dealer dealer = new Dealer();
+        List<Card> result = dealer.getOpenedCards();
+
+        assertThat(result)
+                .isEmpty();
     }
 }
