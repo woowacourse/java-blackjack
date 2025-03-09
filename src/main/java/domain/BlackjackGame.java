c34db7641669215fb6029c47caf4d92098daa2be
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-09 14:18:24 +0900

    fix: 중복된 이름 넣을 수 없도록 수정

--- src/main/java/domain/BlackjackGame.java
+++ src/main/java/domain/BlackjackGame.java
@@ -3,19 +3,23 @@ package domain;
 import except.BlackJackException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class BlackjackGame {
 
+    private static final String DUPLIACTE_NAME = "중복된 이름이 존재합니다.";
     private final int MIN_PEOPLE_WITHOUT_DEALER = 1;
     private final int MAX_PEOPLE_WITHOUT_DEALER = 7;
     private final String INVALID_BLACKJACK_PLAYER_SIZE = "블랙잭은 1-7명만 이용하실 수 있습니다";
 
-    private final BlackjackParticipants blackjackParticipants;
+    private final BlackjackParticipants<Player> blackjackParticipants;
     private final BlackjackDeck deck;
 
     public BlackjackGame(List<String> names, BlackjackDeck deck, Dealer dealer) {
+        validateDuplicateName(names);
         int playerSize = names.size();
         if (playerSize < MIN_PEOPLE_WITHOUT_DEALER || playerSize > MAX_PEOPLE_WITHOUT_DEALER) {
             throw new BlackJackException(INVALID_BLACKJACK_PLAYER_SIZE);
@@ -24,12 +28,20 @@ public class BlackjackGame {
                 .map(Player::new)
                 .collect(Collectors.toList());
         this.deck = deck;
-        this.blackjackParticipants = new BlackjackParticipants(players, dealer);
+        this.blackjackParticipants = new BlackjackParticipants<>(players, dealer);
         initiateGame();
     }
 
+    private void validateDuplicateName(List<String> names) {
+        Set<String> duplicateNames = new HashSet<>(names);
+        if (duplicateNames.size() != names.size()) {
+            throw new BlackJackException(DUPLIACTE_NAME);
+        }
+    }
+
     private void initiateGame() {
-        for (String name : blackjackParticipants.getPlayerNames()) {
+        List<String> playerNames = blackjackParticipants.getPlayerNames();
+        for (String name : playerNames) {
             drawCard(name);
             drawCard(name);
         }
