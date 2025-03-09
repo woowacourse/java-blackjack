f7207ddcb84299da475bf4c722c86998dc7e2314
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-09 17:48:15 +0900

    fix: 프로젝트 기능 동작 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,7 +7,7 @@ public class InputView {
 
     public String inputUsers() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return scanner.next().trim();
+        return scanner.nextLine();
     }
 
     public String inputYesOrNo(String playerName) {
