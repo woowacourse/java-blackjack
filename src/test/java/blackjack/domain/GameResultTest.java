9b6b22ee2c657f31a9bad7790cb8b1d68a925cd2
taek2222 <oot309@daum.net>
2025-03-09 20:29:03 +0900

    test: 사용하지 않은 메서드 삭제로 테스트 구조 변경

--- src/test/java/blackjack/domain/GameResultTest.java
+++ src/test/java/blackjack/domain/GameResultTest.java
@@ -1,87 +1,96 @@
 package blackjack.domain;
 
 import blackjack.domain.card.CardPack;
+import blackjack.domain.player.Dealer;
 import blackjack.domain.player.Gambler;
 import blackjack.domain.player.Player;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GameResultTest {
     @Test
     @DisplayName("딜러보다 카드 합이 높으면 WIN을 반환한다")
     void win_if_bigger_than_dealer() {
-        Players players = new Players();
-        Player gambler = new Gambler("두리");
-        players.addGamblers(List.of(gambler));
         CardPack cardPack = new CardPack(new ReversedSortShuffle());
-        players.initPlayers(cardPack);
-        players.dealAddCard(cardPack, gambler);
-        Assertions.assertThat(GameResult.getGameResult(players.getDealer(), gambler)).isEqualTo(GameResult.WIN);
+        Dealer dealer = new Dealer();
+        Player gambler = new Gambler("두리");
+
+        dealer.pushDealCard(cardPack, 1);
+        gambler.pushDealCard(cardPack, 2);
+
+        assertThat(GameResult.getGameResult(dealer, gambler))
+                .isEqualTo(GameResult.WIN);
     }
 
     @Test
     @DisplayName("딜러보다 카드 합이 낮으면 LOSE 을 반환한다")
     void win_if_bigger_than_dealer2() {
-        Players players = new Players();
-        Player gambler = new Gambler("두리");
-        players.addGamblers(List.of(gambler));
         CardPack cardPack = new CardPack(new ReversedSortShuffle());
-        players.initPlayers(cardPack);
-        players.dealAddCard(cardPack, players.getDealer());
-        Assertions.assertThat(GameResult.getGameResult(players.getDealer(), gambler)).isEqualTo(GameResult.LOSE);
+        Dealer dealer = new Dealer();
+        Player gambler = new Gambler("두리");
+
+        dealer.pushDealCard(cardPack, 2);
+        gambler.pushDealCard(cardPack, 1);
+
+        assertThat(GameResult.getGameResult(dealer, gambler))
+                .isEqualTo(GameResult.LOSE);
     }
 
     @Test
     @DisplayName("딜러와 카드 합이 같으면 DRAW 을 반환한다")
     void win_if_bigger_than_dealer3() {
-        Players players = new Players();
-        Player gambler = new Gambler("두리");
-        players.addGamblers(List.of(gambler));
         CardPack cardPack = new CardPack(new ReversedSortShuffle());
-        players.initPlayers(cardPack);
-        Assertions.assertThat(GameResult.getGameResult(players.getDealer(), gambler)).isEqualTo(GameResult.DRAW);
+        Dealer dealer = new Dealer();
+        Player gambler = new Gambler("두리");
+
+        dealer.pushDealCard(cardPack, 1);
+        gambler.pushDealCard(cardPack, 1);
+
+        assertThat(GameResult.getGameResult(dealer, gambler))
+                .isEqualTo(GameResult.DRAW);
     }
 
     @Test
     @DisplayName("딜러와 참가자가 둘다 bust 면 DRAW 를 반환한다")
     void both_bust_test() {
-        Players players = new Players();
-        Player gambler = new Gambler("두리");
-        players.addGamblers(List.of(gambler));
         CardPack cardPack = new CardPack(new SortShuffle());
-        players.initPlayers(cardPack);
-        players.dealAddCard(cardPack, gambler);
-        players.dealAddCard(cardPack, players.getDealer());
+        Dealer dealer = new Dealer();
+        Player gambler = new Gambler("두리");
 
-        Assertions.assertThat(GameResult.getGameResult(players.getDealer(), gambler)).isEqualTo(GameResult.DRAW);
+        dealer.pushDealCard(cardPack, 3); // 10, 10, 10 총합 30
+        gambler.pushDealCard(cardPack, 3);
+
+        assertThat(GameResult.getGameResult(dealer, gambler))
+                .isEqualTo(GameResult.DRAW);
     }
 
     @Test
     @DisplayName("참가자만 bust 면 LOSE 를 반환한다")
     void both_bust_test2() {
-        Players players = new Players();
-        Player gambler = new Gambler("두리");
-        players.addGamblers(List.of(gambler));
         CardPack cardPack = new CardPack(new SortShuffle());
-        players.initPlayers(cardPack);
-        players.dealAddCard(cardPack, gambler);
+        Dealer dealer = new Dealer();
+        Player gambler = new Gambler("두리");
 
-        Assertions.assertThat(GameResult.getGameResult(players.getDealer(), gambler)).isEqualTo(GameResult.LOSE);
+        dealer.pushDealCard(cardPack, 1);
+        gambler.pushDealCard(cardPack, 3); // 10, 10, 10 총합 30
+
+        assertThat(GameResult.getGameResult(dealer, gambler))
+                .isEqualTo(GameResult.LOSE);
     }
 
     @Test
     @DisplayName("딜러만 bust 면 WIN 를 반환한다")
     void both_bust_test3() {
-        Players players = new Players();
-        Player gambler = new Gambler("두리");
-        players.addGamblers(List.of(gambler));
         CardPack cardPack = new CardPack(new SortShuffle());
-        players.initPlayers(cardPack);
-        players.dealAddCard(cardPack, players.getDealer());
+        Dealer dealer = new Dealer();
+        Player gambler = new Gambler("두리");
 
-        Assertions.assertThat(GameResult.getGameResult(players.getDealer(), gambler)).isEqualTo(GameResult.WIN);
+        dealer.pushDealCard(cardPack, 3); // 10, 10, 10 총합 30
+        gambler.pushDealCard(cardPack, 1);
+
+        assertThat(GameResult.getGameResult(dealer, gambler))
+                .isEqualTo(GameResult.WIN);
     }
 }
