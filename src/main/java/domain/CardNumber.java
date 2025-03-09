ba7b78c29056c653379f52a31139943ea3bba72e
ppparkta <pppppark2@gmail.com>
2025-03-09 22:08:08 +0900

    refactor: 불필요한 필드 제거

--- src/main/java/domain/CardNumber.java
+++ src/main/java/domain/CardNumber.java
@@ -1,26 +1,24 @@
 package domain;
 
 public enum CardNumber {
-    ACE(1, 1, "A"),
-    TWO(2, 2, "2"),
-    THREE(3, 3, "3"),
-    FOUR(4, 4, "4"),
-    FIVE(5, 5, "5"),
-    SIX(6, 6, "6"),
-    SEVEN(7, 7, "7"),
-    EIGHT(8, 8, "8"),
-    NINE(9, 9, "9"),
-    TEN(10, 10, "10"),
-    J(11, 10, "J"),
-    Q(12, 10, "Q"),
-    K(13, 10, "K");
+    ACE(1, "A"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    J(10, "J"),
+    Q(10, "Q"),
+    K(10, "K");
 
-    private final int index;
     private final int value;
     private final String displayName;
 
-    CardNumber(int index, int value, String displayName) {
-        this.index = index;
+    CardNumber(int value, String displayName) {
         this.value = value;
         this.displayName = displayName;
     }
@@ -31,14 +29,5 @@ public enum CardNumber {
 
     public String getDisplayName() {
         return displayName;
-    }
-
-    public static CardNumber pick(int index) {
-        for (CardNumber value : values()) {
-            if (value.index == index) {
-                return value;
-            }
-        }
-        throw new IllegalArgumentException();
     }
 }
--- src/main/java/domain/CardShape.java
+++ src/main/java/domain/CardShape.java
@@ -1,24 +1,9 @@
 package domain;
 
-import java.util.Arrays;
-
 public enum CardShape {
-    DIA(1),
-    HEART(2),
-    SPADE(3),
-    CLOVER(4),
+    DIA,
+    HEART,
+    SPADE,
+    CLOVER,
     ;
-
-    private final int index;
-
-    CardShape(int index) {
-        this.index = index;
-    }
-
-    public static CardShape pickCardShape(int index) {
-        return Arrays.stream(values())
-                .filter((value) -> value.index == index)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("카드 모양 범위 내에서 선택해주세요 (1~4)"));
-    }
 }
