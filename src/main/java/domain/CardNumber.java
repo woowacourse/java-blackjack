f634c8686d43c773e89bdf760f863de50ecf3e38
egaeng09 <egaeng09@gmail.com>
2025-03-09 20:03:12 +0900

    refactor: 단일 score에 대한 생성자 추가

--- src/main/java/domain/CardNumber.java
+++ src/main/java/domain/CardNumber.java
@@ -1,25 +1,31 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public enum CardNumber {
     ACE("A", List.of(1, 11)),
-    TWO("2", List.of(2)),
-    THREE("3", List.of(3)),
-    FOUR("4", List.of(4)),
-    FIVE("5", List.of(5)),
-    SIX("6", List.of(6)),
-    SEVEN("7", List.of(7)),
-    EIGHT("8", List.of(8)),
-    NINE("9", List.of(9)),
-    TEN("10", List.of(10)),
-    JACK("J", List.of(10)),
-    QUEEN("Q", List.of(10)),
-    KING("K", List.of(10)),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10),
     ;
 
     private final String title;
     private final List<Integer> scores;
+
+    CardNumber(String title, int score) {
+        this.title = title;
+        scores = new ArrayList<>(List.of(score));
+    }
 
     CardNumber(String title, List<Integer> scores) {
         this.title = title;
