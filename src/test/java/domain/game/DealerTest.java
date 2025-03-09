4ce993412d38cf3c23cc61f696644877a9cbcecf
jhan0121 <jhan0121@gmail.com>
2025-03-09 15:06:33 +0900

    test: Participant와 중복되는 테스트 제거

--- src/test/java/domain/game/DealerTest.java
+++ src/test/java/domain/game/DealerTest.java
@@ -1,78 +1,13 @@
 package domain.game;
 
 import domain.card.Card;
-import domain.card.CardDeck;
 import domain.card.CardNumber;
 import domain.card.Pattern;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class DealerTest {
-
-    @Test
-    void 딜러는_게임_시작_시_두_장의_카드를_받는다() {
-        //given
-        CardDeck cardDeck = CardDeck.createCards();
-        Dealer dealer = new Dealer();
-
-        //when
-        dealer.drawCardWhenStart(cardDeck);
-
-        //then
-        assertThat(dealer.getCards()).hasSize(2);
-    }
-
-    @Test
-    void 딜러는_한_장의_카드를_받는다() {
-        //given
-        CardDeck cardDeck = CardDeck.createCards();
-        Dealer dealer = new Dealer();
-
-        //when
-        dealer.drawCard(cardDeck);
-
-        //then
-        assertThat(dealer.getCards()).hasSize(1);
-    }
-
-    @Test
-    void 딜러가_보유한_카드의_합계를_구한다() {
-        //given
-        Dealer dealer = new Dealer();
-        List<Card> cards = dealer.getCards();
-        cards.add(new Card(Pattern.SPADE, CardNumber.TEN));
-        cards.add(new Card(Pattern.CLOVER, CardNumber.TEN));
-
-        //when & then
-        assertThat(dealer.calculateTotalCardNumber()).isEqualTo(20);
-    }
-
-    @Test
-    void 딜러가_보유한_카드의_합계가_21을_넘었는지_판정한다() {
-        //given
-        Dealer dealer = new Dealer();
-        List<Card> cards = dealer.getCards();
-        cards.add(new Card(Pattern.SPADE, CardNumber.TEN));
-        cards.add(new Card(Pattern.CLOVER, CardNumber.ACE));
-
-        //when & then
-        assertThat(dealer.isOverBurstBound()).isFalse();
-    }
-
-    @Test
-    void 딜러가_보유한_카드의_합계가_16을_넘었는지_판정한다() {
-        //given
-        Dealer dealer = new Dealer();
-        List<Card> cards = dealer.getCards();
-        cards.add(new Card(Pattern.SPADE, CardNumber.TEN));
-        cards.add(new Card(Pattern.CLOVER, CardNumber.SEVEN));
-
-        //when & then
-        assertThat(dealer.isOverDrawBound()).isTrue();
-    }
 
     @Test
     void 모든_플레이어들과_딜러_사이의_승패를_판정한다() {
