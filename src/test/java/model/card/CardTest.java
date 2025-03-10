dcb7e855b3ce44ba2bdeda1d497be5e92b9fc3e9
민경윤 <ruddbs3308@naver.com>
2025-03-10 21:50:34 +0900

    test(CardTest): 해당 카드의 숫자를 반환한다

--- src/test/java/model/card/CardTest.java
+++ src/test/java/model/card/CardTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CardTest {
 
@@ -16,5 +18,19 @@ class CardTest {
                 () -> assertThat(card.isSameNumber(CardNumber.TWO)).isTrue(),
                 () -> assertThat(card.isSameNumber(CardNumber.KING)).isFalse()
         );
+    }
+
+    @DisplayName("해당 카드의 숫자를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "TWO, 2",
+            "THREE, 3",
+            "TEN, 10",
+            "KING, 10",
+            "ACE_ONE, 1"
+    })
+    void getNumberValueTest(final CardNumber cardNumber, final int value) {
+        Card card = new Card(cardNumber, CardShape.SPADE);
+        assertThat(card.getNumberValue()).isEqualTo(value);
     }
 }
