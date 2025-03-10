0c08a3bd5c4f477eb13e57eea35f743f851b1673
jhan0121 <jhan0121@gmail.com>
2025-03-10 16:32:12 +0900

    refactor: GameResult 블랙잭 승리 케이스 추가

--- src/main/java/domain/game/GameResult.java
+++ src/main/java/domain/game/GameResult.java
@@ -5,6 +5,7 @@ import java.util.List;
 public enum GameResult {
 
     WIN("승"),
+    BLACKJACK_WIN("승"),
     LOSE("패"),
     DRAW("무");
 
@@ -15,7 +16,7 @@ public enum GameResult {
     }
 
     public int countGameResultFromDealer(List<GameResult> gameResults) {
-        if (this == GameResult.WIN) {
+        if (this == GameResult.WIN || this == GameResult.BLACKJACK_WIN) {
             return (int) gameResults.stream()
                     .filter(gameResult -> gameResult == GameResult.LOSE)
                     .count();
