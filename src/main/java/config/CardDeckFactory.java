28dd57650f77ca620638b6aafb9ad4f7c32b972c
(minjae8563) <(minjae8563@naver.com)>
2025-03-10 17:24:38 +0900

    refactor: 이중 포문을 스트림으로 변경

--- src/main/java/config/CardDeckFactory.java
+++ src/main/java/config/CardDeckFactory.java
@@ -4,24 +4,21 @@ import domain.card.Card;
 import domain.card.CardDeck;
 import domain.card.Number;
 import domain.card.Shape;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CardDeckFactory {
 
     public CardDeck create() {
-        List<Card> cards = new ArrayList<>();
-        for (Shape shape : Shape.values()) {
-            selectNumbers(shape, cards);
-        }
+        List<Card> cards;
+        cards = Arrays.stream(Shape.values())
+                .flatMap(shape -> Arrays.stream(Number.values())
+                .map(number -> new Card(shape, number)))
+                .collect(Collectors.toList());
+
         Collections.shuffle(cards);
         return new CardDeck(cards);
-    }
-
-    private static void selectNumbers(final Shape shape, final List<Card> cards) {
-        for (Number number : Number.values()) {
-            cards.add(new Card(shape, number));
-        }
     }
 }
