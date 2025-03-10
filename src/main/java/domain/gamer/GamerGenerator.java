29171146b2eb96a306712781c3e177011883e170
Han <jumdo12@gmail.com>
2025-03-10 16:38:16 +0900

    refactor: 플레이어 인원 수 제한 로직 책임 변경

--- src/main/java/domain/gamer/GamerGenerator.java
+++ src/main/java/domain/gamer/GamerGenerator.java
@@ -12,6 +12,7 @@ public class GamerGenerator {
 
     public static List<Player> generatePlayer(List<String> playerNames, CardGenerator cardGenerator) {
         validateDuplicate(playerNames);
+        validatePLayerNumber(playerNames);
         return playerNames.stream()
                 .map(name -> new Player(name, new CardGroup(), cardGenerator))
                 .toList();
@@ -24,6 +25,12 @@ public class GamerGenerator {
     private static void validateDuplicate(final List<String> playerNames) {
         if (isDuplicate(playerNames)) {
             throw new IllegalArgumentException("[ERROR] 플레어는 중복될 수 없습니다.");
+        }
+    }
+
+    private static void validatePLayerNumber(final List<String> playerNames){
+        if(playerNames.size() > 7){
+            throw new IllegalArgumentException("[ERROR] 플레이어 인원 수느 7명을 초과할 수 없습니다.");
         }
     }
 
