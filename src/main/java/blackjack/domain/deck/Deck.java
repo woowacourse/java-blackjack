0619e8b8c509d44b173b5632198477fbec60a84c
songsunkook <songsunkook@gmail.com>
2025-03-10 11:39:00 +0900

    fix: Deck의 card를 모두 소진했을 때 자동으로 추가 card를 리필하도록 수정

--- src/main/java/blackjack/domain/deck/Deck.java
+++ src/main/java/blackjack/domain/deck/Deck.java
@@ -6,17 +6,22 @@ import blackjack.domain.card.Card;
 
 public class Deck {
 
+    private final CardStrategy strategy;
     private final Stack<Card> cards;
 
-    private Deck(Stack<Card> cards) {
+    private Deck(CardStrategy strategy, Stack<Card> cards) {
+        this.strategy = strategy;
         this.cards = cards;
     }
 
     public static Deck generateFrom(CardStrategy strategy) {
-        return new Deck(strategy.generateDeck());
+        return new Deck(strategy, strategy.generateDeck());
     }
 
     public Card draw() {
+        if (cards.isEmpty()) {
+            cards.addAll(strategy.generateDeck());
+        }
         return cards.pop();
     }
 }
