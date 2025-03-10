07dbebcd9cefdab072c48abdd7dcf1bc14f9dc9a
민경윤 <ruddbs3308@naver.com>
2025-03-10 22:20:53 +0900

    test(PlayerCardsTest): 카드를 추가하고, 21을 넘었는데 추가하는 경우 예외가 발생한다

--- src/test/java/model/cards/PlayerCardsTest.java
+++ src/test/java/model/cards/PlayerCardsTest.java
@@ -13,6 +13,37 @@ import org.junit.jupiter.api.Test;
 
 class PlayerCardsTest {
 
+    @DisplayName("21을 넘지 않을 경우 원한다면 얼마든지 카드를 계속 뽑을 수 있다.")
+    @Test
+    void addCardTest() {
+        Cards cards = new PlayerCards(new ArrayList<>(List.of(
+                new Card(CardNumber.KING, CardShape.SPADE),
+                new Card(CardNumber.FIVE, CardShape.SPADE)))
+        );
+        Card cardToAdd = new Card(CardNumber.JACK, CardShape.SPADE);
+        cards.addCard(cardToAdd);
+
+        assertThat(cards.getCards()).containsExactlyInAnyOrder(
+                new Card(CardNumber.KING, CardShape.SPADE),
+                new Card(CardNumber.FIVE, CardShape.SPADE),
+                new Card(CardNumber.JACK, CardShape.SPADE)
+        );
+    }
+
+    @DisplayName("21을 넘었을 때 카드를 뽑으려하면 예외를 발생시킨다.")
+    @Test
+    void addCardOver21Test() {
+        Cards cards = new PlayerCards(new ArrayList<>(List.of(
+                new Card(CardNumber.KING, CardShape.SPADE),
+                new Card(CardNumber.FIVE, CardShape.SPADE),
+                new Card(CardNumber.QUEEN, CardShape.SPADE)))
+        );
+
+        Card cardToAdd = new Card(CardNumber.JACK, CardShape.SPADE);
+
+        assertThatThrownBy(() -> cards.addCard(cardToAdd));
+    }
+
     @DisplayName("카드의 숫자 계산은 카드 문양이 아닌 카드 숫자로 한다.")
     @Test
     void test1() {
@@ -58,34 +89,6 @@ class PlayerCardsTest {
         )));
 
         assertThat(cards.calculateResult()).isEqualTo(22);
-    }
-
-    @DisplayName("21을 넘지 않을 경우 원한다면 얼마든지 카드를 계속 뽑을 수 있다.")
-    @Test
-    void test5() {
-        Cards cards = new PlayerCards(new ArrayList<>(List.of(
-                new Card(CardNumber.KING, CardShape.SPADE),
-                new Card(CardNumber.QUEEN, CardShape.SPADE)))
-        );
-
-        Card cardToAdd = new Card(CardNumber.JACK, CardShape.SPADE);
-        cards.addCard(cardToAdd);
-
-        assertThat(cards.getCards()).contains(cardToAdd);
-    }
-
-    @DisplayName("21을 넘었을 때 카드를 뽑으려하면 예외를 발생시킨다.")
-    @Test
-    void test6() {
-        Cards cards = new PlayerCards(new ArrayList<>(List.of(
-                new Card(CardNumber.KING, CardShape.SPADE),
-                new Card(CardNumber.FIVE, CardShape.SPADE),
-                new Card(CardNumber.QUEEN, CardShape.SPADE)))
-        );
-
-        Card cardToAdd = new Card(CardNumber.JACK, CardShape.SPADE);
-
-        assertThatThrownBy(() -> cards.addCard(cardToAdd));
     }
 
 }
