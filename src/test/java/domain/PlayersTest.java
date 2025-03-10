9f405fe45225a8fb3b3859d1ca8ab2a46c6e4d68
yush <yush2202@gmail.com>
2025-03-10 16:05:05 +0900

    refactor: Test 수정
    - BeforeEach를 사용해 중복 제거

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,11 +15,17 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class PlayersTest {
+    List<PlayerName> usernames;
+
+    @BeforeEach
+    void makeTestPlayerNames() {
+        usernames = Stream.of("김", "이", "박").map(PlayerName::new).toList();
+    }
+
     @Test
     @DisplayName("특정 이름을 가진 플레이어에게 카드를 제공한다.")
     void giveCardToGamerTest() {
         // given
-        List<PlayerName> usernames = Stream.of("김", "이", "박").map(PlayerName::new).toList();
         Players players = new Players(usernames);
         PlayerName username = new PlayerName("이");
         Card card = Card.HEART_JACK;
@@ -35,7 +42,6 @@ class PlayersTest {
     @DisplayName("특정 플레이어가 카드를 더 받을 수 있는지 확인합니다.")
     void canGetMoreCardTest(List<Card> cards, boolean expected) {
         //given
-        List<PlayerName> usernames = List.of(new PlayerName("김"), new PlayerName("이"), new PlayerName("박"));
         Players players = new Players(usernames);
         players.giveCard(new PlayerName("김"), new Cards(List.of(Card.HEART_TEN)));
         players.giveCard(new PlayerName("김"), new Cards(cards));
@@ -57,7 +63,6 @@ class PlayersTest {
     @DisplayName("플레이어가 모두 존재하는지 확인합니다.")
     void getPlayerInfoTest() {
         // given
-        List<PlayerName> usernames = List.of(new PlayerName("김"), new PlayerName("이"), new PlayerName("박"));
         Players players = new Players(usernames);
         // when
         Map<PlayerName, Player> playerInfo = players.getPlayersInfo();
