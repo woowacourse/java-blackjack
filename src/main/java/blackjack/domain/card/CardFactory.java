200325c0d847aa1095263f8414f049a25e20a941
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-03-10 23:49:06 +0900

    fix: 카드에 상태(앞/뒷면)가 생기며 참조 문제, 캐싱 삭제로 해결

--- src/main/java/blackjack/domain/card/CardFactory.java
+++ src/main/java/blackjack/domain/card/CardFactory.java
@@ -1,27 +1,12 @@
 package blackjack.domain.card;
 
-import java.util.EnumMap;
-import java.util.Map;
-
 public class CardFactory {
-
-    private static final Map<CardSuit, Map<CardRank, Card>> CACHE = new EnumMap<>(CardSuit.class);
-
-    static {
-        for (CardSuit suit : CardSuit.values()) {
-            Map<CardRank, Card> rankMap = new EnumMap<>(CardRank.class);
-            for (CardRank rank : CardRank.values()) {
-                rankMap.put(rank, resolveCard(suit, rank));
-            }
-            CACHE.put(suit, rankMap);
-        }
-    }
 
     private CardFactory() {
     }
 
     protected static Card create(CardSuit suit, CardRank rank) {
-        return CACHE.get(suit).get(rank);
+        return resolveCard(suit, rank);
     }
 
     private static Card resolveCard(CardSuit suit, CardRank rank) {
