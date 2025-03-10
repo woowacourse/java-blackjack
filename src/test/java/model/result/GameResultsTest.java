3aa37f4c3ff6ddb88b2a03e4cebbba01502e1b62
민경윤 <ruddbs3308@naver.com>
2025-03-10 22:37:01 +0900

    test(GameResults): 플레이어 이름을 통해 플레이어의 결과를 확인한다

--- src/test/java/model/result/GameResultsTest.java
+++ src/test/java/model/result/GameResultsTest.java
@@ -9,6 +9,25 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class GameResultsTest {
 
+    @DisplayName("플레이어의 이름을 통해 플레이어의 결과를 확인한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "pobi, WIN",
+            "jason, WIN",
+            "danny, DRAW",
+            "hotteok, LOSE"
+    })
+    void playerResultTest(final String name, final GameResult gameResult) {
+        GameResults gameResults = new GameResults(Map.of(
+                "pobi", GameResult.WIN,
+                "jason", GameResult.WIN,
+                "danny", GameResult.DRAW,
+                "hotteok", GameResult.LOSE
+        ));
+
+        assertThat(gameResults.getGameResultByName(name)).isEqualTo(gameResult);
+    }
+
     @DisplayName("플레이어의 게임 결과를 통해 딜러의 승,무,패 횟수를 계산한다.")
     @ParameterizedTest
     @CsvSource({
@@ -16,7 +35,7 @@ class GameResultsTest {
             "DRAW, 1",
             "LOSE, 2"
     })
-    void test1(final GameResult gameResult, final int count) {
+    void dealerResultCountTest(final GameResult gameResult, final int count) {
         GameResults gameResults = new GameResults(Map.of(
                 "pobi", GameResult.WIN,
                 "jason", GameResult.WIN,
