470d239ff022d1f86e00f4ac70b9dbaaf9c72e00
LeeJaeHoon <dlwogns3413@naver.com>
2025-03-10 18:23:35 +0900

    refactor: 승패 결정 로직 indent 1로 리팩터링

--- src/main/java/blackjack/domain/winning/WinningResult.java
+++ src/main/java/blackjack/domain/winning/WinningResult.java
@@ -6,36 +6,39 @@ public enum WinningResult {
     DRAW, WIN, LOSE;
 
     public static WinningResult decide(Cards mainCards, Cards subCards) {
+        if (mainCards.isBust() && subCards.isBust()) {
+            return WinningResult.DRAW;
+        }
         if (mainCards.isBust()) {
-            if (subCards.isBust()) {
-                return WinningResult.DRAW;
-            }
             return WinningResult.LOSE;
         }
-
         if (subCards.isBust()) {
             return WinningResult.WIN;
         }
 
-        if (mainCards.isBlackjack() || subCards.isBlackjack()) {
-            if (mainCards.isBlackjack() && subCards.isBlackjack()) {
-                return WinningResult.DRAW;
-            }
-            if (mainCards.isBlackjack()) {
-                return WinningResult.WIN;
-            }
+        if (mainCards.isBlackjack() && subCards.isBlackjack()) {
+            return WinningResult.DRAW;
+        }
+        if (mainCards.isBlackjack()) {
+            return WinningResult.WIN;
+        }
+        if (subCards.isBlackjack()) {
             return WinningResult.LOSE;
         }
 
-        int mainCardScore = mainCards.calculateMaxScore();
-        int subCardScore = subCards.calculateMaxScore();
+        return compareScore(mainCards, subCards);
+    }
 
-        if (mainCardScore > subCardScore) {
+    private static WinningResult compareScore(Cards mainCards, Cards subCards) {
+        int mainScore = mainCards.calculateMaxScore();
+        int subScore = subCards.calculateMaxScore();
+        if (mainScore > subScore) {
             return WinningResult.WIN;
         }
-        if (mainCardScore < subCardScore) {
+        if (mainScore < subScore) {
             return WinningResult.LOSE;
         }
         return WinningResult.DRAW;
     }
+
 }
