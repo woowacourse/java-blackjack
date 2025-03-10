468d874e7fe3b6f512524c52f40507c994f83194
geonwoo <kysub99@naver.com>
2025-03-10 12:54:37 +0900

    refactor(OutputView): 출력 개행 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -48,11 +48,13 @@ public class OutputView {
 
     public void printSetUpCardDeck(SetUpCardsDTO setUpCardsDTO) {
         Card dealerOpenCard = setUpCardsDTO.dealerOpenCard();
+        System.out.println();
         System.out.printf("딜러와 %s에게 2장을 나누었습니다.%n", playerNames(setUpCardsDTO));
 
         System.out.println("딜러카드: " + cardNameOf(dealerOpenCard));
         Map<String, List<Card>> playerCards = setUpCardsDTO.cards();
         playerCards.forEach((key, value) -> System.out.printf("%s카드: %s%n", key, cardNames(value)));
+        System.out.println();
     }
 
     public void printTakenMoreCards(String name, List<Card> cards) {
@@ -60,10 +62,12 @@ public class OutputView {
     }
 
     public void printDealerTake() {
+        System.out.println();
         System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
     }
 
     public void printFinalCardDeck(List<FinalResultDTO> dtos) {
+        System.out.println();
         dtos.forEach(
             dto -> System.out.printf("%s카드: %s - 결과: %d\n",
             dto.name(), cardNames(dto.cards()), dto.score())
@@ -71,6 +75,7 @@ public class OutputView {
     }
 
     public void printGameResult(GameResult gameResult) {
+        System.out.println();
         System.out.println("## 최종 승패");
         printDealerWinnings(gameResult);
 
