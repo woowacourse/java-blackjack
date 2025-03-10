6aead2b7eb748ee437e7f6ee033e3a6240b3968e
egaeng09 <egaeng09@gmail.com>
2025-03-10 21:10:30 +0900

    refactor: 동일한 테스트 제거

--- src/test/java/domain/DeckTest.java
+++ src/test/java/domain/DeckTest.java
@@ -7,20 +7,7 @@ import org.junit.jupiter.api.Test;
 public class DeckTest {
 
     @Test
-    void 덱에서_카드를_한_장_뽑는다() {
-        // given
-        final int cardCount = 1;
-
-        // when
-        Deck deck = DeckGenerator.generateDeck();
-        List<Card> card = deck.drawCards(cardCount);
-
-        // then
-        Assertions.assertThat(card.size()).isEqualTo(cardCount);
-    }
-
-    @Test
-    void 덱에서_카드를_두_장_뽑는다() {
+    void 덱에서_카드를_뽑는다() {
         // given
         final int cardCount = 2;
 
