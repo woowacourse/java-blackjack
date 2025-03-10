e230660130053d1ba4335b875938bc8003e261c5
rladmstn <oz33177351@gmail.com>
2025-03-10 20:08:16 +0900

    test: 카드 셔플 테스트 제거

--- src/test/java/domain/card/CardDeckTest.java
+++ src/test/java/domain/card/CardDeckTest.java
@@ -2,8 +2,6 @@ package domain.card;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class CardDeckTest {
@@ -16,20 +14,6 @@ class CardDeckTest {
         // then
         assertThat(cardDeck.getCards())
                 .hasSize(52);
-    }
-
-    @RepeatedTest(100)
-    void 카드가_셔플이_되었는지_확인한다() {
-        // given
-        CardDeck deck1 = CardDeck.of();
-        CardDeck deck2 = CardDeck.of();
-
-        // when
-        List<Card> cards1 = deck1.getCards();
-        List<Card> cards2 = deck2.getCards();
-
-        // then
-        assertThat(cards1).isNotEqualTo(cards2);
     }
 
     @Test
