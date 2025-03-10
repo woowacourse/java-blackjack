795e0785ffe0f08f59026f04dc34a31d033e6397
junho <wnsgh732@gmail.com>
2025-03-10 17:14:21 +0900

    refactor: Dealer의 openedCards를 반환하는 로직 수정

--- src/main/java/blackjack/domain/player/Dealer.java
+++ src/main/java/blackjack/domain/player/Dealer.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 public class Dealer extends Player {
 
-    private static final int DEALER_OPENED_CARD_LOWER_BOUND = 0;
-    private static final int DEALER_OPENED_CARD_UPPER_BOUND = 1;
     private static final String DEALER_NAME = "딜러";
     private static final int DEALER_HIT_MAX_VALUE = 16;
 
@@ -21,6 +19,6 @@ public class Dealer extends Player {
 
     @Override
     public List<Card> getOpenedCards() {
-        return getCards().subList(DEALER_OPENED_CARD_LOWER_BOUND, DEALER_OPENED_CARD_UPPER_BOUND);
+        return List.of(getCards().getFirst());
     }
 }
