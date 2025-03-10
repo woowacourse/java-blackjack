0168bdce07aa3db849b355682116eadcbc763cc8
supernovaMK <minkikim2002@gmail.com>
2025-03-10 17:34:37 +0900

    test: 불필요한 테스트 삭제

--- src/test/java/domain/BlackJackGameTest.java
+++ src/test/java/domain/BlackJackGameTest.java
@@ -2,7 +2,6 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,27 +31,6 @@ class BlackJackGameTest {
     @Nested
     class ValidCases {
 
-        @Test
-        @DisplayName("플레이어가 정상적으로 생성되어야 한다.")
-        void createPlayers() {
-            // given
-            BlackJackGame blackJackGame = new BlackJackGame(blackJackDeck, dealer, rule);
-            List<String> playerNames = List.of("Alice", "Bob");
-            Deck originalDeck = new Deck(Arrays.stream(TrumpCard.values()).toList());
-
-            // when
-            List<Player> players = blackJackGame.createPlayers(playerNames);
-
-            // then
-            assertSoftly(softly -> {
-                softly.assertThat(players.getFirst().getName()).isEqualTo("Alice");
-                softly.assertThat(players.getFirst().getHand()).isEqualTo(
-                        new Hand(List.of(originalDeck.draw(), originalDeck.draw())));
-                softly.assertThat(players.getLast().getName()).isEqualTo("Bob");
-                softly.assertThat(players.getLast().getHand()).isEqualTo(
-                        new Hand(List.of(originalDeck.draw(), originalDeck.draw())));
-            });
-        }
 
         @ParameterizedTest
         @DisplayName("플레이어의 히트 판단 여부를 판단한다")
