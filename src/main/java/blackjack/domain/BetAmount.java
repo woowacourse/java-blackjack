b4e9024a9b7c09b836e55eeda85e2a630df79575
MinJun Choi <mj043000@naver.com>
2025-03-10 13:41:52 +0900

    feat: 베팅 금액 도메인 구현

---
+++ src/main/java/blackjack/domain/BetAmount.java
@@ -0,0 +1,10 @@
+package blackjack.domain;
+
+public class BetAmount {
+
+    private final long money;
+
+    public BetAmount(final long money) {
+        this.money = money;
+    }
+}
---
+++ src/test/java/blackjack/domain/BetAmountTest.java
@@ -0,0 +1,20 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BetAmountTest {
+
+    @DisplayName("베팅 금액을 생성한다.")
+    @Test
+    void createBetAmount() {
+        // given
+        long money = 10_000;
+
+        // when & then
+        assertThatCode(() -> new BetAmount(money)).doesNotThrowAnyException();
+    }
+
+}
