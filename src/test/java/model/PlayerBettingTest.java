3cc405fcc8cc0719bda6d7d32f14936a556ba5dd
LJM\eueo8 <eueo8259@gmail.com>
2025-03-10 22:47:45 +0900

    test : 게임결과가 패배라면 베팅금액을 모두 잃고 무승부라면 손익/손실이 없는 로직 테스트

--- src/test/java/model/PlayerBettingTest.java
+++ src/test/java/model/PlayerBettingTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 class PlayerBettingTest {
     @Test
-    @DisplayName("게임결과가 승이라면 베팅한 금액만큼의 수익을 얻는다.")
+    @DisplayName("게임결과가 승이라면 베팅한 금액만큼의 수익을 얻는 테스트")
     void 게임결과가_승이라면_베팅한_금액만큼의_수익을_얻는다(){
         Player player = new Player("a");
         GameResult gameResult = GameResult.WIN;
@@ -21,5 +21,35 @@ class PlayerBettingTest {
         int result = playerBetting.getBettingNet(player);
         assertEquals(expect, result);
     }
+
+    @Test
+    @DisplayName("게임결과가 패라면 베팅한 금액을 잃는 테스트")
+    void 게임결과가_패라면_베팅한_금액을_잃는다(){
+        Player player = new Player("a");
+        GameResult gameResult = GameResult.LOSE;
+        Map<Player, Integer> map = new HashMap<>();
+        map.put(player, 10000);
+        PlayerBetting playerBetting = new PlayerBetting(map);
+        playerBetting.calculateBettingResult(player, gameResult);
+        int expect = 0;
+        int result = playerBetting.getBettingNet(player);
+        assertEquals(expect, result);
+    }
+
+    @Test
+    @DisplayName("게임결과가 무승부라면 베팅한 손익/손실이 없도록 테스트")
+    void 게임결과가_무승부라면_손익_손실이_없다(){
+        Player player = new Player("a");
+        GameResult gameResult = GameResult.DRAW;
+        Map<Player, Integer> map = new HashMap<>();
+        map.put(player, 10000);
+        PlayerBetting playerBetting = new PlayerBetting(map);
+        playerBetting.calculateBettingResult(player, gameResult);
+        int expect = 10000;
+        int result = playerBetting.getBettingNet(player);
+        assertEquals(expect, result);
+    }
+
+
 
 }
