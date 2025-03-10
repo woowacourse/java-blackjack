e1d95894d5ecf9c09e664003bd6ae776ad7f228b
ykmxxi <ykmxxi97@gmail.com>
2025-03-10 21:47:03 +0900

    refactor: 파라미터가 바뀐 기능 테스트 수정

--- src/test/java/blackjack/model/game/BlackJackGameTest.java
+++ src/test/java/blackjack/model/game/BlackJackGameTest.java
@@ -4,10 +4,12 @@ import static blackjack.model.card.CardCreator.createCard;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import blackjack.model.card.CardNumber;
 import blackjack.model.card.Cards;
@@ -17,19 +19,27 @@ import blackjack.model.player.User;
 
 class BlackJackGameTest {
 
-    @CsvSource(value = {
-            "true,1", "false,0"
-    })
+    @MethodSource("딜러의_카드를_뽑아야_한다면_카드를_뽑는다_테스트_케이스")
     @ParameterizedTest
-    void 딜러의_카드를_뽑아야_한다면_카드를_뽑는다(boolean expected, int expectedSize) {
-        BlackJackGame blackJackGame = new BlackJackGame(() -> new Cards(
-                List.of(createCard(CardNumber.TWO), createCard(CardNumber.THREE), createCard(CardNumber.FOUR))
-        ));
+    void 딜러의_카드를_뽑아야_한다면_카드를_뽑는다(Cards cards, Cards receivedCards, boolean expected, int expectedSize) {
+        BlackJackGame blackJackGame = new BlackJackGame(() -> cards);
 
         Dealer dealer = new Dealer();
+        dealer.receiveCards(receivedCards);
 
         assertThat(blackJackGame.canDrawMoreCard(dealer)).isEqualTo(expected);
         assertThat(dealer.getCards().getValues()).hasSize(expectedSize);
+    }
+
+    private static Stream<Arguments> 딜러의_카드를_뽑아야_한다면_카드를_뽑는다_테스트_케이스() {
+        Cards cards = new Cards(
+                List.of(createCard(CardNumber.TEN), createCard(CardNumber.SIX)));
+        return Stream.of(
+                Arguments.of(cards, cards, true, 3),
+                Arguments.of(cards, new Cards(
+                        List.of(createCard(CardNumber.TEN), createCard(CardNumber.SEVEN))
+                ), false, 2)
+        );
     }
 
     @Test
