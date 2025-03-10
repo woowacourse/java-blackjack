987c59f583e2cba3cc4ae9ce5075e5f98ac4e671
geonwoo <kysub99@naver.com>
2025-03-10 15:23:54 +0900

    refactor(Player): 함수 호출 순서대로 선언하도록 수정

--- src/main/java/domain/participant/Player.java
+++ src/main/java/domain/participant/Player.java
@@ -15,15 +15,15 @@ public class Player extends Participant {
         this.name = name;
     }
 
-    private static void validateLength(String name) {
-        if (name.length() > 10) {
-            throw new IllegalArgumentException("플레이어의 이름은 10자를 넘을 수 없습니다.");
-        }
-    }
-
     private static void validateNotBlank(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("플레이어의 이름은 비어있을 수 없습니다.");
+        }
+    }
+
+    private static void validateLength(String name) {
+        if (name.length() > 10) {
+            throw new IllegalArgumentException("플레이어의 이름은 10자를 넘을 수 없습니다.");
         }
     }
 
