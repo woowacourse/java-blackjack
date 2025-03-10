3c071f9c3cebf0bd74928ad6fb65ed4ff670048d
민경윤 <ruddbs3308@naver.com>
2025-03-10 22:31:05 +0900

    test(Deck): 덱에서 카드를 뽑는다

--- src/test/java/model/deck/DeckTest.java
+++ src/test/java/model/deck/DeckTest.java
@@ -1,17 +1,28 @@
 package model.deck;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
 import java.util.List;
+import model.card.Card;
+import model.card.CardNumber;
+import model.card.CardShape;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class DeckTest {
 
+    @DisplayName("덱에서 카드를 뽑는다.")
+    @Test
+    void getCardTest() {
+        Deck deck = new Deck(new ArrayList<>(List.of(new Card(CardNumber.SEVEN, CardShape.DIAMOND))));
+        assertThat(deck.getCard()).isEqualTo(new Card(CardNumber.SEVEN, CardShape.DIAMOND));
+    }
+
     @DisplayName("덱에 카드가 남아있지 않다면 예외가 발생한다.")
     @Test
-    void test1() {
+    void getNotExistCardTest() {
         Deck deck = new Deck(new ArrayList<>(List.of()));
         assertThatThrownBy(deck::getCard)
                 .isInstanceOf(IllegalStateException.class);
