0d00b8a4008e14fa7d7a0937792d29abed9d44a1
threepebbles <threepebbles@naver.com>
2025-03-10 10:05:53 +0900

    test: 덱에 남은 카드 수가 뽑으려는 카드 수보다 적을 때 예외 발생 테스트 추가

--- src/test/java/domain/DeckTest.java
+++ src/test/java/domain/DeckTest.java
@@ -31,4 +31,16 @@ public class DeckTest {
         // then
         Assertions.assertThat(card.size()).isEqualTo(cardCount);
     }
+
+    @Test
+    void 덱에_남은_카드_수가_뽑으려는_카드_수보다_적으면_예외가_발생한다() {
+        // given
+        Deck deck = new Deck(List.of(
+                new Card(Suit.HEART, Rank.ACE)
+        ));
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> deck.drawCards(2))
+                .isInstanceOf(IllegalStateException.class);
+    }
 }
