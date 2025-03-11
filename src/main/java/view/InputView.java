93033385e70ad6e5344feded644d55aec61136c4
(minjae8563) <(minjae8563@naver.com)>
2025-03-12 00:25:45 +0900

    refactor: static final 선언 후, 상수 처리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private final String commaDelimiter = ",";
-    private final String playerName = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-    private final String askHitOrStand = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String COMMA_DELIMITER = ",";
+    private static final String PLAYER_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String ASK_HIT_OR_STAND = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
 
     private final Scanner scanner;
 
@@ -17,16 +17,16 @@ public class InputView {
     }
 
     public List<String> readPlayersName() {
-        System.out.println(playerName);
+        System.out.println(PLAYER_NAME);
         String playerNames = scanner.nextLine();
 
-        return Arrays.stream(playerNames.split(commaDelimiter))
+        return Arrays.stream(playerNames.split(COMMA_DELIMITER))
                 .map(String::trim)
                 .toList();
     }
 
     public Boolean askPlayerForHitOrStand(final Player player) {
-        System.out.printf(askHitOrStand, player.getName());
+        System.out.printf(ASK_HIT_OR_STAND, player.getName());
         return Answer.isYes(scanner.nextLine());
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -19,17 +19,17 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class OutputView {
-    private final Map<Number, String> numberSymbolMap = new HashMap<>(
+    private static final Map<Number, String> NUMBER_SYMBOL_MAP = new HashMap<>(
             Map.of(ACE, "A", QUEEN, "Q", JACK, "J", KING, "K"));
-    private final String commaDelimiter = ", ";
-    private final String hitCards = "딜러와 %s에게 2장을 나누었습니다.%n";
-    private final String playerCards = "%s카드: ";
-    private final String dealerCards = "딜러카드: ";
-    private final String hitDealerCard = "딜러는 16이하라 한장의 카드를 더 받았습니다.\n";
-    private final String score = " - 결과: %d";
-    private final String resultIntro = "## 최종 승패";
-    private final String playerResult = "%s: %s";
-    private final String dealerResult = "딜러: ";
+    private static final String COMMA_DELIMITER = ", ";
+    private static final String HIT_CARDS = "딜러와 %s에게 2장을 나누었습니다.%n";
+    private static final String PLAYER_CARDS = "%s카드: ";
+    private static final String DEALER_CARDS = "딜러카드: ";
+    private static final String HIT_DEALER_CARD = "딜러는 16이하라 한장의 카드를 더 받았습니다.\n";
+    private static final String SCORE = " - 결과: %d";
+    private static final String RESULT_INTRO = "## 최종 승패";
+    private static final String PLAYER_RESULT = "%s: %s";
+    private static final String DEALER_RESULT = "딜러: ";
 
     public void printParticipant(final Players players, final Dealer dealer) {
         printHitNotice(players);
@@ -46,49 +46,49 @@ public class OutputView {
     }
 
     public String printDealerDeck(final Dealer dealer) {
-        System.out.printf(dealerCards);
+        System.out.printf(DEALER_CARDS);
         List<String> cardSymbols = new ArrayList<>();
         for (Card card : dealer.getHand().getCards()) {
             cardSymbols.add(toSymbol(card));
         }
-        return String.join(commaDelimiter, cardSymbols);
+        return String.join(COMMA_DELIMITER, cardSymbols);
     }
 
     public void printDrawDealer(final Dealer dealer) {
         if (dealer.isUnderThreshold()) {
             printNewLine();
-            System.out.print(hitDealerCard);
+            System.out.print(HIT_DEALER_CARD);
         }
     }
 
     public void printScore(final Players players, final Dealer dealer) {
         printNewLine();
         System.out.print(printDealerDeck(dealer));
-        System.out.printf(score, dealer.calculateSum());
+        System.out.printf(SCORE, dealer.calculateSum());
         printNewLine();
 
         for (Player player : players.getPlayers()) {
             System.out.print(resultMaker(player));
-            System.out.printf(score, player.calculateSum());
+            System.out.printf(SCORE, player.calculateSum());
             printNewLine();
         }
     }
 
     public void printResult(final Map<Player, MatchResult> playerMatchResult) {
         printNewLine();
-        System.out.println(resultIntro);
+        System.out.println(RESULT_INTRO);
 
         printDealerResult(playerMatchResult);
 
         printNewLine();
         playerMatchResult.forEach((player, matchResult) -> {
-            System.out.printf(playerResult, player.getName(), matchResult.getValue());
+            System.out.printf(PLAYER_RESULT, player.getName(), matchResult.getValue());
             printNewLine();
         });
     }
 
     private void printDealerDeckWithHidden(final Dealer dealer) {
-        System.out.println(dealerCards + toSymbol(dealer.getHand().getCards().getFirst()));
+        System.out.println(DEALER_CARDS + toSymbol(dealer.getHand().getCards().getFirst()));
     }
 
     private void printHitNotice(final Players players) {
@@ -98,26 +98,26 @@ public class OutputView {
             playersName.add(player.getName());
         }
         printNewLine();
-        System.out.printf(hitCards, String.join(commaDelimiter, playersName));
+        System.out.printf(HIT_CARDS, String.join(COMMA_DELIMITER, playersName));
     }
 
     private String resultMaker(final Player player) {
-        System.out.printf(playerCards, player.getName());
+        System.out.printf(PLAYER_CARDS, player.getName());
         List<String> cardSymbols = new ArrayList<>();
         for (Card card : player.getHand().getCards()) {
             cardSymbols.add(toSymbol(card));
         }
-        return String.join(commaDelimiter, cardSymbols);
+        return String.join(COMMA_DELIMITER, cardSymbols);
     }
 
     private String toSymbol(final Card card) {
         Number number = card.getNumber();
         Shape shape = card.getShape();
-        return numberSymbolMap.getOrDefault(number, String.valueOf(number.getScore())) + shape.getShape();
+        return NUMBER_SYMBOL_MAP.getOrDefault(number, String.valueOf(number.getScore())) + shape.getShape();
     }
 
     private void printDealerResult(Map<Player, MatchResult> playerMatchResult) {
-        StringBuilder stringBuilder = new StringBuilder(dealerResult);
+        StringBuilder stringBuilder = new StringBuilder(DEALER_RESULT);
         System.out.print(appendMatchResult(stringBuilder, MatchResult.calculateDealerResult(playerMatchResult)));
     }
 
