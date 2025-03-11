429055249f131dcc982dcc4b09d9a59678af2e77
kjyyjk <rlawnsdud920@gmail.com>
2025-03-11 16:50:31 +0900

    test(red): BettingAmount 값 객체에서 베팅 금액 검증

---
+++ src/main/java/blackjack/domain/betting/BettingAmount.java
@@ -0,0 +1,9 @@
+package blackjack.domain.betting;
+
+public class BettingAmount {
+    private final int amount;
+
+    public BettingAmount(final int amount) {
+        this.amount = amount;
+    }
+}
