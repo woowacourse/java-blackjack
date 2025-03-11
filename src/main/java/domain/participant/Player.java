47703cdf98c715b6500731bde90b47be7bcc7a24
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-11 14:24:26 +0900

    refactor: 플레이어의 이름이 딜러이면안된다 -> Player가 판단할 책임

--- src/main/java/domain/participant/Player.java
+++ src/main/java/domain/participant/Player.java
@@ -1,10 +1,20 @@
 package domain.participant;
 
+import exception.BlackJackException;
+
 public class Player extends BlackjackParticipant {
+
+    private static final String INVALID_PLAYER_NAME = "일반 플레이어는 딜러일 수 없습니다.";
 
     public Player(String name) {
         super(name);
-        validatePlayerName();
+        validatePlayerName(name);
+    }
+
+    private void validatePlayerName(String name) {
+        if (name.equals(BlackjackParticipant.dealerName())) {
+            throw new BlackJackException(INVALID_PLAYER_NAME);
+        }
     }
 
     @Override
