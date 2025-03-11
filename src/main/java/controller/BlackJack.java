f1ac2f8d0e421f40e189bfe4170a2bfce25398ae
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-11 11:54:08 +0900

    refactor(domain) : Entry -> Players 클리스 이름 변경

--- src/main/java/controller/BlackJack.java
+++ src/main/java/controller/BlackJack.java
@@ -9,14 +9,14 @@ import static view.OutputView.printWinLossResult;
 import controller.dto.WinLossCountDto;
 import domain.Dealer;
 import domain.Deck;
-import domain.Entry;
 import domain.Player;
+import domain.Players;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BlackJack {
 
-    private Entry entry;
+    private Players players;
     private final Dealer dealer = new Dealer();
     private final Deck deck = new Deck();
 
@@ -28,13 +28,13 @@ public class BlackJack {
     }
 
     private void preset() {
-        entry = new Entry(getPlayerNamesInput());
-        deck.distributeCards(dealer, entry);
-        printDistributeResult(entry, dealer);
+        players = new Players(getPlayerNamesInput());
+        deck.distributeCards(dealer, players);
+        printDistributeResult(players, dealer);
     }
 
     private void playersTurn() {
-        for (Player player : entry.getPlayers()) {
+        for (Player player : players.getPlayers()) {
             player.drawByChoice(deck);
         }
     }
@@ -45,14 +45,14 @@ public class BlackJack {
 
     private void printResult() {
         printDealerExtraCardsCount(dealer);
-        printEveryOneCardsNamesWithTotal(entry, dealer);
-        printWinLossResult(entry, dealer, computeWinLoss(entry, dealer));
+        printEveryOneCardsNamesWithTotal(players, dealer);
+        printWinLossResult(players, dealer, computeWinLoss(players, dealer));
     }
 
-    public WinLossCountDto computeWinLoss(Entry entry, Dealer dealer) {
+    public WinLossCountDto computeWinLoss(Players players, Dealer dealer) {
         ArrayList<Integer> winLossDraw = new ArrayList<>(Arrays.asList(0,0,0));
         int dealerScore = dealer.getHandTotal();
-        entry.getPlayers().forEach(player -> {
+        players.getPlayers().forEach(player -> {
             int playerScore = player.getHandTotal();
             if (playerScore < dealerScore) {
                 winLossDraw.add(0, 1);
