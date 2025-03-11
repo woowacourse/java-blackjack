3f140ce7d990783ead276293cab51c6159279b53
JO-eusan <goodsan0330@gmail.com>
2025-03-11 12:03:53 +0900

    refactor: allMatch를 anyMath로 변경

--- src/main/java/blackjack/domain/user/PlayerName.java
+++ src/main/java/blackjack/domain/user/PlayerName.java
@@ -5,17 +5,23 @@ public class PlayerName {
     private final String name;
 
     public PlayerName(final String name) {
-        validateName(name);
+        validateNameSize(name);
+        validateCharacter(name);
 
         this.name = name;
     }
 
-    private void validateName(final String name) {
-        boolean isAllLetter = name.chars().allMatch(Character::isLetter);
-        if (isAllLetter) {
-            return;
+    private void validateNameSize(final String name) {
+        if(name.length() <= 0) {
+            throw new IllegalArgumentException("이름은 한 글자 이상 입력해야 합니다.");
         }
-        throw new IllegalArgumentException("이름은 영어/한글만 입력 가능합니다.");
+    }
+
+    private void validateCharacter(final String name) {
+        boolean isNotAllLetter = name.chars().anyMatch(ch -> !Character.isLetter(ch));;
+        if (isNotAllLetter) {
+            throw new IllegalArgumentException("이름은 영어/한글만 입력 가능합니다.");
+        }
     }
 
     public String getName() {
