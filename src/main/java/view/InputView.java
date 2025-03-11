69faae8c6bdc6aae990a87f3787141deb0b69b91
(minjae8563) <(minjae8563@naver.com)>
2025-03-11 20:52:12 +0900

    refactor: 네이밍에 카멜 케이스 적용

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,15 +1,14 @@
 package view;
 
-import domain.card.Card;
 import domain.participant.Player;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private final String COMMA = ",";
-    private final String PLAYER_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-    private final String ASK_HIT_OR_STAND = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private final String commaDelimiter = ",";
+    private final String playerName = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private final String askHitOrStand = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
 
     private final Scanner scanner;
 
@@ -18,16 +17,16 @@ public class InputView {
     }
 
     public List<String> readPlayersName() {
-        System.out.println(PLAYER_NAME);
+        System.out.println(playerName);
         String playerNames = scanner.nextLine();
 
-        return Arrays.stream(playerNames.split(COMMA))
+        return Arrays.stream(playerNames.split(commaDelimiter))
                 .map(String::trim)
                 .toList();
     }
 
     public Boolean askPlayerForHitOrStand(final Player player) {
-        System.out.printf(ASK_HIT_OR_STAND, player.getName());
+        System.out.printf(askHitOrStand, player.getName());
         return Answer.isYes(scanner.nextLine());
     }
 }
