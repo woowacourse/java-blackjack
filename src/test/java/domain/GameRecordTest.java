2c2fe3ff823a41164f6e8c6e8d6e83bd7cea2d0a
Hacanna42 <dtmdgus901@gmail.com>
2025-03-11 23:09:21 +0900

    test(GameRecordTest): 게임 전적 업데이트 실패 🔴

--- src/test/java/domain/GameRecordTest.java
+++ src/test/java/domain/GameRecordTest.java
@@ -1,8 +1,12 @@
 package domain;
 
+import domain.participant.BattleResult;
 import domain.participant.GameRecord;
+import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 public class GameRecordTest {
     @Test
@@ -14,5 +18,19 @@ public class GameRecordTest {
 
         // then
         Assertions.assertThat(gameRecord).isInstanceOf(GameRecord.class);
+    }
+
+    @ParameterizedTest
+    @EnumSource(BattleResult.class)
+    void 게임_전적_업데이트_테스트(BattleResult battleResult) {
+        // given
+        GameRecord gameRecord = new GameRecord();
+
+        // when
+        gameRecord.add(battleResult);
+
+        // then
+        Map<BattleResult, Integer> record = gameRecord.getGameRecord();
+        Assertions.assertThat(record.getOrDefault(battleResult, 0)).isEqualTo(1);
     }
 }
