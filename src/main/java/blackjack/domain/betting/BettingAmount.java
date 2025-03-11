ddde207b3e6f893b4b04e0ff4ac3636b8ddc6d17
kjyyjk <rlawnsdud920@gmail.com>
2025-03-11 16:51:50 +0900

    feat(green): BettingAmount 값 객체에서 베팅 금액 검증

--- src/main/java/blackjack/domain/betting/BettingAmount.java
+++ src/main/java/blackjack/domain/betting/BettingAmount.java
@@ -4,6 +4,13 @@ public class BettingAmount {
     private final int amount;
 
     public BettingAmount(final int amount) {
+        validateRange(amount);
         this.amount = amount;
+    }
+
+    public void validateRange(final int amount) {
+        if (amount < 1_000 || amount > 1_000_000) {
+            throw new IllegalArgumentException("베팅 금액은 1,000원부터 1,000,000까지 입니다.");
+        }
     }
 }
