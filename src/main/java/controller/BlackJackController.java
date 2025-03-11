6939757b15964042a78a44f95f1e52e55bbfdf20
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-11 20:27:29 +0900

    🔵refactor(BlackjackController): 컨트롤러 내부 구조 변경

--- src/main/java/controller/BlackJackController.java
+++ src/main/java/controller/BlackJackController.java
@@ -2,12 +2,15 @@ package controller;
 
 import domain.AnswerCommand;
 import domain.GameManager;
+import domain.GameResult;
+import domain.card.CardGroup;
+import domain.card.Deck;
 import domain.card.RandomCardGenerator;
 import domain.gamer.Dealer;
-import domain.gamer.GamerGenerator;
-import domain.gamer.Player;
+import domain.gamer.PlayerGroup;
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Map;
 import util.LoopTemplate;
 import view.InputView;
 import view.OutputView;
@@ -23,54 +26,59 @@ public class BlackJackController {
     }
 
     public void gameStart() {
-        final RandomCardGenerator randomCardGenerator = new RandomCardGenerator();
-        final Dealer dealer = GamerGenerator.generateDealer(randomCardGenerator);
-        final List<Player> players = requestPlayers(randomCardGenerator);
-        final GameManager gameManager = GameManager.create(dealer, players);
-        requestHit(dealer, players);
-        printDealerReceiveCardCount(dealer);
-        outputView.printGamerCardsAndScore(dealer, players);
-        outputView.printGameResult(gameManager.calculateDealerGameResult(), gameManager.calculatePlayerGameResult());
+        final List<String> playerNames = LoopTemplate.tryCatchLoop(inputView::readPlayers);
+        final PlayerGroup playerGroup = LoopTemplate.tryCatchLoop(() -> PlayerGroup.of(playerNames));
+        final Dealer dealer = new Dealer(new CardGroup(new ArrayList<>()));
+        final Deck deck = Deck.of(new RandomCardGenerator());
+        final GameManager gameManager = new GameManager(dealer, playerGroup, deck);
+        gameManager.shuffleCards();
+        gameManager.giveCardsToDealer();
+        gameManager.giveCardsToPlayers(playerNames);
+        outputView.printDealerAndPlayersCards(dealer, playerGroup.getPlayers());
+        requestHit(playerNames, playerGroup, gameManager);
+        printDealerReceiveCardCount(gameManager);
+        responseGameResult(dealer, playerGroup);
     }
 
-    private List<Player> requestPlayers(RandomCardGenerator randomCardGenerator) {
-        return LoopTemplate.tryCatchLoop(() ->
-        {
-            final List<String> playerNames = LoopTemplate.tryCatchLoop(inputView::readPlayers);
-            return GamerGenerator.generatePlayer(playerNames, randomCardGenerator);
-        });
+    private void responseGameResult(final Dealer dealer, final PlayerGroup playerGroup) {
+        final Map<String, GameResult> playersGameResult = playerGroup.calculatePlayersGameResult(dealer);
+        outputView.printGamerCardsAndScore(dealer, playerGroup.getPlayers());
+        outputView.printGameResult(dealer.calculateDealerGameResult(playersGameResult),
+                playerGroup.calculatePlayersGameResult(dealer));
     }
 
-    private void requestHit(Dealer dealer, List<Player> players) {
-        outputView.printDealerAndPlayersCards(dealer, players);
-        for (Player player : players) {
-            receiveCard(player);
+    private void requestHit(final List<String> playerNames, final PlayerGroup playerGroup,
+                            final GameManager gameManager) {
+        for (String playerName : playerNames) {
+            giveCardToPlayer(playerName, playerGroup, gameManager);
         }
     }
 
-    private void receiveCard(final Player player) {
-        if(!player.isBust()){
-            receiveCardIsAbleToGetCard(player);
+    private void giveCardToPlayer(final String playerName, final PlayerGroup playerGroup,
+                                  final GameManager gameManager) {
+        if (!playerGroup.isBustByPlayerName(playerName)) {
+            receiveCardIsAbleToGetCard(playerName, playerGroup, gameManager);
         }
     }
 
-    private void receiveCardIsAbleToGetCard(Player player){
-        if(requestAnswerCommand(player) == AnswerCommand.NO){
-            outputView.printPlayerCards(player);
+    private void receiveCardIsAbleToGetCard(final String playerName, final PlayerGroup playerGroup,
+                                            final GameManager gameManager) {
+        if (requestAnswerCommand(playerName) == AnswerCommand.NO) {
+            outputView.printPlayerCards(playerName, playerGroup.getCardsByName(playerName));
             return;
         }
         do {
-            player.receiveCard(1);
-            outputView.printPlayerCards(player);
-        }while(!player.isBust() && requestAnswerCommand(player) == AnswerCommand.YES);
+            gameManager.giveOneCardToPlayerByName(playerName);
+            outputView.printPlayerCards(playerName, playerGroup.getCardsByName(playerName));
+        } while (playerGroup.isBustByPlayerName(playerName) && requestAnswerCommand(playerName) == AnswerCommand.YES);
     }
 
-    private AnswerCommand requestAnswerCommand(Player player) {
-        return LoopTemplate.tryCatchLoop(() -> inputView.readAnswer(player.getName()));
+    private AnswerCommand requestAnswerCommand(final String playerName) {
+        return LoopTemplate.tryCatchLoop(() -> inputView.readAnswer(playerName));
     }
 
-    private void printDealerReceiveCardCount(Dealer dealer) {
-        final int count = dealer.giveCardsToDealer();
+    private void printDealerReceiveCardCount(final GameManager gameManager) {
+        final int count = gameManager.giveCardsToDealer();
         if (count > 0) {
             outputView.printDealerReceivedCardCount(count);
         }
