a68bcc880d96a1025897d480d138402ea81ac223
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-11 20:17:30 +0900

    🔵refactor(Dealer): 딜러의 게임 결과 계산을 딜러가 담당하도록리팩토링

--- src/main/java/domain/gamer/Dealer.java
+++ src/main/java/domain/gamer/Dealer.java
@@ -1,23 +1,29 @@
 package domain.gamer;
 
-import domain.card.CardGenerator;
+import domain.GameResult;
 import domain.card.CardGroup;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Dealer extends Gamer {
-    public Dealer(CardGroup cardGroup, CardGenerator cardGenerator) {
-        super(cardGroup, cardGenerator);
+    public Dealer(CardGroup cardGroup) {
+        super(cardGroup);
     }
 
     public boolean isLessThen(int score) {
         return this.cardGroup.calculateScore(Gamer.LIMIT) <= score;
     }
 
-    public int giveCardsToDealer() {
-        int count = 0;
-        while (isLessThen(16)) {
-            receiveCard(1);
-            count++;
-        }
-        return count;
+    public Map<GameResult, Integer> calculateDealerGameResult(final Map<String, GameResult> playerGameResults) {
+        final List<GameResult> results = playerGameResults.values().stream().toList();
+        return GameResult.getAllGameResults().stream()
+                .filter(results::contains)
+                .collect(Collectors.toMap(
+                        GameResult::swapGameResult,
+                        result -> Collections.frequency(results, result),
+                        (newResult, oldResult) -> oldResult
+                ));
     }
 }
--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -4,20 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.card.Card;
 import domain.card.CardGroup;
+import domain.card.CardScore;
 import domain.card.CardType;
-import domain.card.RandomCardGenerator;
-import domain.fake.FaceCardGenerator;
 import domain.gamer.Dealer;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class DealerTest {
     @Test
     void 딜러는_점수가_16점_이하인_경우_True를_반환한다() {
-        List<Card> cards = List.of(new Card(CardType.DIAMOND_J), new Card(CardType.DIAMOND_2));
+        List<Card> cards = List.of(new Card(CardType.DIAMOND, CardScore.JACK), new Card(CardType.DIAMOND, CardScore.TWO));
         CardGroup cardGroup = new CardGroup(cards);
-        RandomCardGenerator randomCardGenerator = new RandomCardGenerator();
-        Dealer dealer = new Dealer(cardGroup, randomCardGenerator);
+        Dealer dealer = new Dealer(cardGroup);
 
         boolean status = dealer.isLessThen(16);
 
@@ -25,15 +24,16 @@ public class DealerTest {
     }
 
     @Test
-    void 딜러_점수가_16이하면_카드를_계속_뽑는다() {
+    void 딜러의_점수를_계산한다() {
         //given
-        CardGroup cardGroup = new CardGroup();
-        final FaceCardGenerator faceCardGenerator = new FaceCardGenerator();
+        final Map<String, GameResult> playerResults = Map.of("윌슨", GameResult.DRAW, "가이온", GameResult.WIN);
 
         //when
-        Dealer dealer = new Dealer(cardGroup, faceCardGenerator);
-        final int count = dealer.giveCardsToDealer();
+        final Dealer dealer = new Dealer(new CardGroup());
+        final Map<GameResult, Integer> result = dealer.calculateDealerGameResult(playerResults);
+
         //then
-        assertThat(count).isEqualTo(2);
+        assertThat(result).containsEntry(GameResult.DRAW, 1)
+                .containsEntry(GameResult.LOSE, 1);
     }
 }
