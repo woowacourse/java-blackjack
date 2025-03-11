2339b063ce7aebcabb62e6a0924bf00d97006f74
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-11 14:06:14 +0900

    refactor: Enum 미사용 인덱스 삭제

--- src/main/java/domain/CardShape.java
+++ src/main/java/domain/CardShape.java
@@ -1,18 +1,8 @@
 package domain;
 
-import java.util.Arrays;
-
 public enum CardShape {
-    DIA(1),
-    HEART(2),
-    SPADE(3),
-    CLOVER(4),
-    ;
-
-    private final int index;
-
-    CardShape(int index) {
-        this.index = index;
-    }
-
+    DIA,
+    HEART,
+    SPADE,
+    CLOVER,
 }
