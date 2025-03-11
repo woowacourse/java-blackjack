ef5b429ae90dd18330db870048c6cbca5575b85f
LeeJaeHoon <dlwogns3413@naver.com>
2025-03-11 17:41:27 +0900

    test: Player 단위 테스트 작성

--- src/test/java/blackjack/domain/participants/PlayerTest.java
+++ src/test/java/blackjack/domain/participants/PlayerTest.java
@@ -1,9 +1,53 @@
 package blackjack.domain.participants;
 
-import blackjack.domain.card.ScoreCalculator;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Cards;
+import blackjack.domain.card.Deck;
+import blackjack.domain.card.Rank;
+import blackjack.domain.card.Suit;
+import org.junit.jupiter.api.Test;
 
 class PlayerTest {
 
-    private ScoreCalculator scoreCalculator = new ScoreCalculator();
+    @Test
+    void 카드를_2장_준비한다() {
+        //given
+        Deck deck = new Deck(
+                new Card(Suit.CLUB, Rank.EIGHT),
+                new Card(Suit.DIAMOND, Rank.NINE),
+                new Card(Suit.CLUB, Rank.TEN)
+        );
+        Player player = new Player("pobi", new Cards());
+
+        //when
+        player.prepareCards(deck);
+
+        //then
+        assertThat(player).isEqualTo(new Player("pobi", new Cards(
+                new Card(Suit.CLUB, Rank.EIGHT),
+                new Card(Suit.DIAMOND, Rank.NINE)
+        )));
+    }
+
+    @Test
+    void 카드를_한장_뽑는다() {
+        //given
+        Deck deck = new Deck(
+                new Card(Suit.CLUB, Rank.EIGHT),
+                new Card(Suit.DIAMOND, Rank.NINE),
+                new Card(Suit.CLUB, Rank.TEN)
+        );
+        Player player = new Player("pobi", new Cards());
+
+        //when
+        player.drawCard(deck);
+
+        //then
+        assertThat(player).isEqualTo(new Player("pobi", new Cards(
+                new Card(Suit.CLUB, Rank.EIGHT)
+        )));
+    }
 
 }
