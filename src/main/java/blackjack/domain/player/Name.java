f39abd9af2e9b05e76f9fb01344ed34df8bcdc3c
taek2222 <oot309@daum.net>
2025-03-11 18:23:12 +0900

    feat: 이름 객체의 검증 추가

--- src/main/java/blackjack/domain/player/Name.java
+++ src/main/java/blackjack/domain/player/Name.java
@@ -7,7 +7,7 @@ public class Name {
     private final String name;
 
     public Name(final String name) {
-        validateName(name);
+        validateNameLength(name);
         this.name = name;
     }
 
@@ -15,17 +15,17 @@ public class Name {
         return name;
     }
 
-    private void validateName(final String name) {
-        if (name.isBlank()) {
-            throw new IllegalArgumentException("이름의 길이는 1이상입니다.");
+    private void validateNameLength(final String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("최소 이름의 길이는 1자 이상입니다.");
         }
     }
 
     @Override
     public boolean equals(final Object object) {
         if (object == null || getClass() != object.getClass()) return false;
-        Name name1 = (Name) object;
-        return Objects.equals(name, name1.name);
+        Name otherName = (Name) object;
+        return Objects.equals(name, otherName.name);
     }
 
     @Override
