80267d311ae6d5648ccd16b9013ee86f34227146
changuii <rhljh7410@gmail.com>
2025-03-11 22:06:17 +0900

    red(Players): Player 리스트를 일급 콜렉션으로 만들어라

---
+++ src/main/java/domain/paticipant/Players.java
@@ -0,0 +1,13 @@
+package domain.paticipant;
+
+import java.util.List;
+
+public class Players {
+
+	public Players(final List<String> names) {
+	}
+
+	public List<Player> getPlayers() {
+		return null;
+	}
+}
---
+++ src/test/java/domain/participant/PlayersTest.java
@@ -0,0 +1,32 @@
+package domain.participant;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import domain.paticipant.Players;
+
+public class PlayersTest {
+
+	@Nested
+	@DisplayName("Players 생성")
+	class ConstructPlayers {
+
+		@DisplayName("이름들로 플레이어들을 생성한다.")
+		@Test
+		void constructPlayers() {
+			// given
+			final List<String> names = List.of("부기", "구구", "파랑");
+
+			// when
+			final var players = new Players(names);
+
+			// then
+			assertThat(players.getPlayers()).hasSize(3);
+		}
+	}
+}
