9e58289c640c6cbcae9973e67ec19d8eebde2972
kjyyjk <rlawnsdud920@gmail.com>
2025-03-11 16:59:21 +0900

    refactor: BettingAmount 값 객체에서 베팅 금액 검증

--- src/main/java/blackjack/domain/betting/BettingAmount.java
+++ src/main/java/blackjack/domain/betting/BettingAmount.java
@@ -1,6 +1,11 @@
 package blackjack.domain.betting;
 
+import java.util.Objects;
+
 public class BettingAmount {
+    private static final int MIN_AMOUNT = 1_000;
+    private static final int MAX_AMOUNT = 1_000_000;
+
     private final int amount;
 
     public BettingAmount(final int amount) {
@@ -8,9 +13,23 @@ public class BettingAmount {
         this.amount = amount;
     }
 
-    public void validateRange(final int amount) {
-        if (amount < 1_000 || amount > 1_000_000) {
-            throw new IllegalArgumentException("베팅 금액은 1,000원부터 1,000,000까지 입니다.");
+    private void validateRange(final int amount) {
+        if (amount < MIN_AMOUNT || amount > MAX_AMOUNT) {
+            throw new IllegalArgumentException("베팅 금액은 %d원부터 %d원까지 입니다.".formatted(MIN_AMOUNT, MAX_AMOUNT));
         }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        BettingAmount that = (BettingAmount) object;
+        return amount == that.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(amount);
     }
 }
--- src/test/java/blackjack/domain/betting/BettingAmountTest.java
+++ src/test/java/blackjack/domain/betting/BettingAmountTest.java
@@ -3,25 +3,17 @@ package blackjack.domain.betting;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class BettingAmountTest {
     @DisplayName("베팅_금액이_1,000원_미만이면_예외를_발생한다")
-    @Test
-    void should_ThrowException_WhenBettingAmountOutRange1() {
+    @ValueSource(ints = {0, 999, 1_000_001})
+    @ParameterizedTest
+    void should_ThrowException_WhenBettingAmountOutRange() {
         // when
         // then
         assertThatThrownBy(() -> new BettingAmount(999))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("베팅 금액은 1,000원부터 1,000,000까지 입니다.");
-    }
-
-    @DisplayName("베팅_금액이_1,000,000원_초과면_예외를_발생한다")
-    @Test
-    void should_ThrowException_WhenBettingAmountOutRange2() {
-        // when
-        // then
-        assertThatThrownBy(() -> new BettingAmount(1_000_001))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("베팅 금액은 1,000원부터 1,000,000까지 입니다.");
     }
