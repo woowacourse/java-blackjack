7406e126640afa4fc245f9e6154e8e251864e4f6
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-11 12:12:47 +0900

    test(domain) : test 추가

--- src/test/java/domain/CardTest.java
+++ src/test/java/domain/CardTest.java
@@ -19,33 +19,43 @@ public class CardTest {
     class CardCreateTest {
         @Test
         @DisplayName("카드는 모양과 숫자를 가진다")
-        public void test1() {
+        void test1() {
             assertThat(new Card(ACE, CLUB)).isInstanceOf(Card.class);
         }
 
         @Test
         @DisplayName("카드는 고유의 숫자값을 갖는다.")
-        public void test2() {
+        void test2() {
             assertThat(new Card(TWO, CLUB).getValue()).isEqualTo(2);
         }
 
         @Test
         @DisplayName("카드는 고유의 숫자값을 갖는다. - 2")
-        public void test3() {
+        void test3() {
             assertThat(new Card(THREE, CLUB).getValue()).isEqualTo(3);
         }
 
         @Test
         @DisplayName("카드는 고유의 숫자값을 갖는다. - 3")
-        public void test4() {
+        void test4() {
             assertThat(new Card(JACK, CLUB).getValue()).isEqualTo(10);
         }
 
         @Test
         @DisplayName("ACE의 초기 숫자값은 11이다.")
-        public void test5() {
+        void test5() {
             assertThat(new Card(ACE, CLUB).getValue()).isEqualTo(11);
         }
+    }
+
+    @Test
+    @DisplayName("Card는 값을 0으로 변경할 수 있다")
+    void test1() {
+        Card card = new Card(THREE, CLUB);
+
+        card.setValueToZero();
+
+        assertThat(card.getValue()).isEqualTo(0);
     }
 
     @Nested
@@ -53,7 +63,7 @@ public class CardTest {
     class AceTest {
         @Test
         @DisplayName("ACE는 ACE임을 알릴 수 있다")
-        public void test1() {
+        void test1() {
             Card clubAce = new Card(ACE, CLUB);
             Card clubTwo = new Card(TWO, CLUB);
 
@@ -63,7 +73,7 @@ public class CardTest {
 
         @Test
         @DisplayName("ACE는 고유의 숫자값을 1로 바꿀 수 있다.")
-        public void test2() {
+        void test2() {
             // given
             Ace clubAce = new Ace(CLUB);
 
