2cb086019f26c1c3a817ca5dc27add8cd6485540
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-11 20:10:02 +0900

    🔵feat(PlayerGroup): 플레이어 그룹 도메인 구현
    구현한 목록은 다음과 같습니다:
    - 플레이어 게임 결과 계산
    - 이름에 해당하는 플레이어에게 카드 전달
    - 이름에 해당하는 플레이어가 버스트 인지 판단

---
+++ src/test/java/domain/PlayerGroupTest.java
@@ -0,0 +1,88 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.card.Card;
+import domain.card.CardGroup;
+import domain.card.CardScore;
+import domain.card.CardType;
+import domain.gamer.Dealer;
+import domain.gamer.PlayerGroup;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class PlayerGroupTest {
+
+    @Test
+    void 플레이어가_버스트_하지_않고_딜러보다_점수가_높아야_승리() {
+        //given
+        final List<String> playerNames = List.of("윌슨", "가이온");
+        final Dealer dealer = new Dealer(new CardGroup());
+
+        //when
+        final PlayerGroup playerGroup = PlayerGroup.of(playerNames);
+        playerGroup.giveCardByName("윌슨", new Card(CardType.HEART, CardScore.ACE));
+        playerGroup.giveCardByName("가이온", new Card(CardType.CLOVER, CardScore.ACE));
+        final Map<String, GameResult> playersGameResult = playerGroup.calculatePlayersGameResult(dealer);
+
+        //then
+        assertThat(playersGameResult).containsEntry("윌슨", GameResult.WIN)
+                .containsEntry("가이온", GameResult.WIN);
+    }
+
+
+
+
+    @Test
+    void 딜러와_플레이어의_점수가_같은_경우_무승부() {
+        //given
+        final List<String> playerNames = List.of("윌슨", "가이온");
+        final Dealer dealer = new Dealer(new CardGroup());
+
+        //when
+        final PlayerGroup playerGroup = PlayerGroup.of(playerNames);
+        playerGroup.giveCardByName("윌슨", new Card(CardType.HEART, CardScore.ACE));
+        dealer.giveCard(new Card(CardType.DIAMOND, CardScore.ACE));
+        final Map<String, GameResult> playersGameResult = playerGroup.calculatePlayersGameResult(dealer);
+
+        //then
+        assertThat(playersGameResult).containsEntry("윌슨", GameResult.DRAW);
+    }
+
+    @Test
+    void 플레이어와_딜러_둘_다_버스트_하는_경우_무승부() {
+        //given
+        final List<String> playerNames = List.of("윌슨");
+        final Dealer dealer = new Dealer(new CardGroup());
+
+        //when
+        final PlayerGroup playerGroup = PlayerGroup.of(playerNames);
+        playerGroup.giveCardByName("윌슨", new Card(CardType.HEART, CardScore.JACK));
+        playerGroup.giveCardByName("윌슨",new Card(CardType.CLOVER, CardScore.QUEEN));
+        playerGroup.giveCardByName("윌슨", new Card(CardType.DIAMOND, CardScore.TEN));
+        dealer.giveCard(new Card(CardType.HEART, CardScore.JACK));
+        dealer.giveCard(new Card(CardType.DIAMOND, CardScore.JACK));
+        dealer.giveCard(new Card(CardType.SPADE, CardScore.JACK));
+        final Map<String, GameResult> playersGameResult = playerGroup.calculatePlayersGameResult(dealer);
+
+        //then
+        assertThat(playersGameResult).containsEntry("윌슨", GameResult.DRAW);
+    }
+
+    @Test
+    void 플레이어의_점수가_21을_초과할_경우_버스트() {
+        //given
+        final List<String> playerNames = List.of("윌슨");
+
+        //when
+        final PlayerGroup playerGroup = PlayerGroup.of(playerNames);
+        playerGroup.giveCardByName("윌슨", new Card(CardType.HEART, CardScore.JACK));
+        playerGroup.giveCardByName("윌슨",new Card(CardType.CLOVER, CardScore.QUEEN));
+        playerGroup.giveCardByName("윌슨", new Card(CardType.DIAMOND, CardScore.TEN));
+        final boolean result = playerGroup.isBustByPlayerName("윌슨");
+
+        //then
+        assertThat(result).isTrue();
+    }
+}
