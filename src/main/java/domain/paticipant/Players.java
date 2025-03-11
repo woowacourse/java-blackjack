e21f841e97fddf69037306633f44c1aaa0a2e924
changuii <rhljh7410@gmail.com>
2025-03-11 22:07:32 +0900

    green: 플레이어들을 일급 콜렉션으로 만들어라

--- src/main/java/domain/paticipant/Players.java
+++ src/main/java/domain/paticipant/Players.java
@@ -1,13 +1,18 @@
 package domain.paticipant;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Players {
+	private final List<Player> players;
 
 	public Players(final List<String> names) {
+		players = names.stream()
+			.map(Player::new)
+			.collect(Collectors.toList());
 	}
 
 	public List<Player> getPlayers() {
-		return null;
+		return players;
 	}
 }
