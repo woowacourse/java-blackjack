f49a4457909d61ac43f9e8addfcedd4781334306
sung-june27 <kmymy27@gmail.com>
2025-03-11 21:30:30 +0900

    refactor(Payout): 객체 분리

---
+++ src/main/java/blackjack/domain/participant/Payout.java
@@ -0,0 +1,42 @@
+package blackjack.domain.participant;
+
+import blackjack.domain.GameResult;
+
+public record Payout(
+        int value
+) {
+
+    public static Payout zero() {
+        return new Payout(0);
+    }
+
+    public static Payout blackjackPayout(final int value) {
+        int blackjackResult = (int) Math.floor(value * 1.5);
+        return new Payout(blackjackResult);
+    }
+
+    public Payout bet(final int amount) {
+        return new Payout(this.value + amount);
+    }
+
+    public Payout calculatePayout(final Dealer dealer, final Player player) {
+        GameResult result = GameResult.getPlayerGameResultFrom(dealer, player);
+        if (result == GameResult.LOSE) {
+            return new Payout(-value);
+        }
+        if (player.isBlackjack()) {
+            return calculatePayoutWithBlackjack(dealer);
+        }
+        if (result == GameResult.DRAW) {
+            return zero();
+        }
+        return this;
+    }
+
+    private Payout calculatePayoutWithBlackjack(final Dealer dealer) {
+        if (dealer.isBlackjack()) {
+            return this;
+        }
+        return blackjackPayout(value);
+    }
+}
--- src/main/java/blackjack/domain/participant/Player.java
+++ src/main/java/blackjack/domain/participant/Player.java
@@ -1,50 +1,27 @@
 package blackjack.domain.participant;
 
-import blackjack.domain.GameResult;
 import blackjack.domain.card.Card;
 import java.util.List;
 
 public class Player extends Participant {
 
     private static final int PLAYER_DISTRIBUTE_CARD_THRESHOLD = 21;
-    private static final double BLACKJACK_BONUS_RATE = 1.5;
-    private static final int DRAW_PAYOUT = 0;
 
     private final PlayerName name;
-    private int payout;
+    private Payout payout;
 
     public Player(final PlayerName name) {
         this.name = name;
-        this.payout = 0;
+        this.payout = Payout.zero();
     }
 
     public void bet(final int amount) {
-        this.payout += amount;
+        this.payout = this.payout.bet(amount);
     }
 
     public int calculatePayout(final Dealer dealer) {
-        GameResult result = GameResult.getPlayerGameResultFrom(dealer, this);
-        if (result == GameResult.LOSE) {
-            return convertToNegativeSign(payout);
-        }
-        if (isBlackjack()) {
-            return calculatePayoutWithBlackjack(dealer);
-        }
-        if (result == GameResult.DRAW) {
-            return DRAW_PAYOUT;
-        }
-        return payout;
-    }
-
-    private int calculatePayoutWithBlackjack(final Dealer dealer) {
-        if (dealer.isBlackjack()) {
-            return payout;
-        }
-        return (int) Math.floor(payout * BLACKJACK_BONUS_RATE);
-    }
-
-    private int convertToNegativeSign(final int value) {
-        return -value;
+        this.payout = payout.calculatePayout(dealer, this);
+        return this.payout.value();
     }
 
     @Override
