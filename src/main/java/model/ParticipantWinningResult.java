d126502fbf7610fd7de6c2f0fc08675fe29f39ac
LJM\eueo8 <eueo8259@gmail.com>
2025-03-11 15:43:07 +0900

    feat : 플레이어를 받아서 게임 결과를 찾는 메서드 getPlayerGameResult 구현

--- src/main/java/model/ParticipantWinningResult.java
+++ src/main/java/model/ParticipantWinningResult.java
@@ -19,21 +19,21 @@ public class ParticipantWinningResult {
     }
 
     public ParticipantWinningResult(Map<Player, GameResult> result) {
-        this.result = result;
+        this.result = result; //TODO 나중에 리팩토링 끝나면 private 해도 될 듯 딜러 최종 승패 테스트에만 쓰이는데 이거는 이제 딜러 최종 베팅으로 넘어감
     }
 
     public Map<GameResult, Integer> decideDealerWinning() {
         Map<GameResult, Integer> dealerResult = new HashMap<>();
-        for (Player player : result.keySet()){
+        for (Player player : result.keySet()) {
             GameResult playerGameResult = result.get(player);
             calculateDealerResult(playerGameResult, dealerResult);
         }
         return dealerResult;
     }
 
-    private static GameResult checkPlayerWinningResult(Dealer dealer, Player player){
+    private static GameResult checkPlayerWinningResult(Dealer dealer, Player player) {
         GameResult playerWinningResult = checkPlayerWin(dealer, player);
-        if (playerWinningResult == GameResult.WIN && player.checkBlackjack()){
+        if (playerWinningResult == GameResult.WIN && player.checkBlackjack()) {
             return GameResult.BLACKJACK;
         }
         return playerWinningResult;
@@ -53,28 +53,32 @@ public class ParticipantWinningResult {
         int dealerScore = dealer.calculateFinalScore();
         int playerScore = player.calculateFinalScore();
 
-        if (dealerScore > playerScore){
+        if (dealerScore > playerScore) {
             return GameResult.LOSE;
         }
-        if (dealerScore < playerScore){
+        if (dealerScore < playerScore) {
             return GameResult.WIN;
         }
         return GameResult.DRAW;
     }
 
     private void calculateDealerResult(GameResult gameResult, Map<GameResult, Integer> dealerResult) {
-        if (gameResult == GameResult.WIN){
+        if (gameResult == GameResult.WIN) {
             dealerResult.merge(GameResult.LOSE, 1, Integer::sum);
         }
-        if (gameResult == GameResult.LOSE){
+        if (gameResult == GameResult.LOSE) {
             dealerResult.merge(GameResult.WIN, 1, Integer::sum);
         }
-        if (gameResult == GameResult.DRAW){
+        if (gameResult == GameResult.DRAW) {
             dealerResult.merge(GameResult.DRAW, 1, Integer::sum);
         }
     }
 
     public Map<Player, GameResult> getResult() {
         return Collections.unmodifiableMap(result);
+    }
+
+    public GameResult getPlayerGameResult(Player player){
+        return result.get(player);
     }
 }
