d064d9ebbe5776e54e29c997d3336fc4abeac87d
jhpark1227 <junhyeok0246@naver.com>
2025-03-11 20:53:53 +0900

    test: Card 단위테스트 작성

---
+++ src/test/java/domain/CardTest.java
@@ -0,0 +1,27 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.card.Card;
+import domain.card.CardNumber;
+import domain.card.CardShape;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CardTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "A, true", "TWO, false", "THREE, false", "FOUR, false", "FIVE, false",
+            "SIX, false", "SEVEN, false", "EIGHT, false", "NINE, false", "TEN, false",
+            "QUEEN, false", "KING, false", "JACK, false"
+    })
+    void 카드의_숫자가_에이스인지_아닌지_반환한다(CardNumber cardNumber, boolean expected) {
+        //given
+        Card card = new Card(cardNumber, CardShape.CLOVER);
+        //when
+        boolean actual = card.isAce();
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+}
