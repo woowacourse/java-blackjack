ce73b0b9ec3d27cd872402dda3ec2733b6749e95
songsunkook <songsunkook@gmail.com>
2025-03-12 20:18:43 +0900

    remove: 미사용 메서드 제거

--- src/main/java/blackjack/domain/gamer/Players.java
+++ src/main/java/blackjack/domain/gamer/Players.java
@@ -1,12 +1,10 @@
 package blackjack.domain.gamer;
 
 import java.util.List;
-import java.util.Optional;
 
 public class Players {
 
     private final List<Player> players;
-    private int cursor = 0;
 
     private Players(List<Player> players) {
         this.players = players;
@@ -16,29 +14,7 @@ public class Players {
         return new Players(players);
     }
 
-    public boolean hasNext() {
-        return cursor < players.size();
-    }
-
-    public Player next() {
-        if (!hasNext()) {
-            throw new IllegalStateException("[ERROR] 플레이어 수를 넘어선 접근입니다.");
-        }
-        return players.get(cursor++);
-    }
-
     public List<Player> getPlayers() {
         return players;
-    }
-
-    public Optional<Player> findByName(String name) {
-        return players.stream()
-            .filter(player -> player.getName().equals(name))
-            .findAny();
-    }
-
-    public Player getByName(String name) {
-        return findByName(name)
-            .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 플레이어명입니다."));
     }
 }
