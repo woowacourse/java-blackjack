034040b6d28cd9aa3ce750a0e70bc9d64f0a33d1
taek2222 <oot309@daum.net>
2025-03-12 22:22:58 +0900

    feat: MethodSource 사용한 테스트 간소화

--- src/test/java/blackjack/domain/GameResultTest.java
+++ src/test/java/blackjack/domain/GameResultTest.java
@@ -8,97 +8,64 @@ import blackjack.domain.player.Gambler;
 import blackjack.domain.player.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class GameResultTest {
 
-    @Test
-    @DisplayName("딜러가 이기면 참가자의 배팅 금액을 얻는다")
-    void 딜러가_이기면_참가자의_배팅_금액을_얻는다() {
-        Card ace = new Card(CardNumber.ACE, CardShape.CLOVER);
+    private static final Card ACE = new Card(CardNumber.ACE, CardShape.CLOVER);
+    private static final Card TEN = new Card(CardNumber.TEN, CardShape.CLOVER);
 
+    @ParameterizedTest
+    @MethodSource("finalResultExpectedData")
+    @DisplayName("최종 수익을 종합해 반환한다")
+    void 최종_수익을_종합해_반환한다(List<Card> dealerCard, List<Card> playerCard, int dealerExcepted, int playerExcepted) {
         Dealer dealer = new Dealer();
-        dealer.addCards(List.of(ace, ace));
+        dealer.addCards(dealerCard);
 
         Gambler gambler = new Gambler("비타", 1_000);
-        gambler.addCards(List.of(ace));
+        gambler.addCards(playerCard);
 
-        GameResults gameResults = new GameResults(dealer, List.of(gambler));
-
-        Map<Player, Integer> result = gameResults.getGameResults();
+        Map<Player, Integer> result = new GameResults(dealer, List.of(gambler)).getGameResults();
 
         assertAll(
-                () -> assertThat(result.get(dealer)).isEqualTo(1_000),
-                () -> assertThat(result.get(gambler)).isEqualTo(-1_000)
-        );
-    }
-
-    @Test
-    @DisplayName("딜러가 지면 참가자는 배팅 금액의 배로 얻는다")
-    void 딜러가_지면_참가자는_배팅_금액의_배로_얻는다() {
-        Card ace = new Card(CardNumber.ACE, CardShape.CLOVER);
-
-        Dealer dealer = new Dealer();
-        dealer.addCards(List.of(ace));
-
-        Gambler gambler = new Gambler("비타", 1_000);
-        gambler.addCards(List.of(ace, ace));
-
-        GameResults gameResults = new GameResults(dealer, List.of(gambler));
-
-        Map<Player, Integer> result = gameResults.getGameResults();
-
-        assertAll(
-                () -> assertThat(result.get(dealer)).isEqualTo(-1_000),
-                () -> assertThat(result.get(gambler)).isEqualTo(1_000)
-        );
-    }
-
-    @Test
-    @DisplayName("무승부인 경우 각자 최종 수익은 0원이다")
-    void 무승부인_경우_각자_최종_수익은_0원이다() {
-        Card ace = new Card(CardNumber.ACE, CardShape.CLOVER);
-
-        Dealer dealer = new Dealer();
-        dealer.addCards(List.of(ace));
-
-        Gambler gambler = new Gambler("비타", 1_000);
-        gambler.addCards(List.of(ace));
-
-        GameResults gameResults = new GameResults(dealer, List.of(gambler));
-
-        Map<Player, Integer> result = gameResults.getGameResults();
-
-        assertAll(
-                () -> assertThat(result.get(dealer)).isEqualTo(0),
-                () -> assertThat(result.get(gambler)).isEqualTo(0)
+                () -> assertThat(result.get(dealer)).isEqualTo(dealerExcepted),
+                () -> assertThat(result.get(gambler)).isEqualTo(playerExcepted)
         );
     }
 
     @Test
     @DisplayName("참가자가 블랙잭인 경우 수익은 1.5배이다")
     void 참가자가_블랙잭인_경우_수익은_1_5배이다() {
-        Card ace = new Card(CardNumber.ACE, CardShape.CLOVER);
-        Card ten = new Card(CardNumber.TEN, CardShape.CLOVER);
-
         Dealer dealer = new Dealer();
-        dealer.addCards(List.of(ace));
+        dealer.addCards(List.of(ACE));
 
         Gambler gambler = new Gambler("비타", 1_000);
-        gambler.addCards(List.of(ace, ten));
+        gambler.addCards(List.of(ACE, TEN));
 
         GameResults gameResults = new GameResults(dealer, List.of(gambler));
 
         Map<Player, Integer> result = gameResults.getGameResults();
 
         assertAll(
-                () -> assertThat(result.get(dealer)).isEqualTo(-2_500),
-                () -> assertThat(result.get(gambler)).isEqualTo(2_500)
+                () -> assertThat(result.get(dealer)).isEqualTo(-1_500),
+                () -> assertThat(result.get(gambler)).isEqualTo(1_500)
+        );
+    }
+
+    private static Stream<Arguments> finalResultExpectedData() {
+        return Stream.of(
+                Arguments.of(List.of(ACE), List.of(ACE, ACE), -1_000, 1_000),
+                Arguments.of(List.of(ACE, ACE), List.of(ACE), 1_000, -1_000),
+                Arguments.of(List.of(ACE), List.of(ACE), 0, 0)
         );
     }
 }
