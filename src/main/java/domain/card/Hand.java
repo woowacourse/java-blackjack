3cd17566729b6fbe19a991dd4ac31ac6d9ce1f1f
(minjae8563) <(minjae8563@naver.com)>
2025-03-12 21:31:56 +0900

    test(green): 자신의 패를 담당하는 클래스의 카드 합계 기능 구현

--- src/main/java/domain/card/Hand.java
+++ src/main/java/domain/card/Hand.java
@@ -3,6 +3,9 @@ package domain.card;
 import java.util.List;
 
 public class Hand {
+    private static final int BONUS_THRESHOLD = 11;
+    private static final int ACE_BONUS = 10;
+
     private final List<Card> hand;
 
     public Hand(List<Card> hand) {
@@ -11,6 +14,21 @@ public class Hand {
 
     public void addCard(Card card) {
         hand.add(card);
+    }
+
+    public int calculateSum() {
+        int sum = hand.stream()
+                .mapToInt(Card::getScore)
+                .sum();
+        if (sum <= BONUS_THRESHOLD && hasA()) {
+            sum += ACE_BONUS;
+        }
+        return sum;
+    }
+
+    private boolean hasA() {
+        return hand.stream()
+                .anyMatch(Card::isA);
     }
 
     public List<Card> getHand() {
