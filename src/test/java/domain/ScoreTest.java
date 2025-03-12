a383927229e54f3d6b7f575c5be98c76c2540168
Hacanna42 <dtmdgus901@gmail.com>
2025-03-12 12:06:28 +0900

    test(ScoreTest): 블랙잭, 버스트 확인 실패 🔴

--- src/test/java/domain/ScoreTest.java
+++ src/test/java/domain/ScoreTest.java
@@ -10,9 +10,35 @@ public class ScoreTest {
         // given
 
         // when
-        Score score = new Score(21, 2);
+        Score score = Score.of(21, 2);
 
         // then
         Assertions.assertThat(score).isInstanceOf(Score.class);
+    }
+
+    @Test
+    void 블랙잭_확인_테스트() {
+        // given
+        Score score = Score.of(21, 2);
+
+        // when
+        boolean actual = score.isBlackJack();
+
+        // then
+        boolean expected = true;
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 버스트_확인_테스트() {
+        // given
+        Score score = Score.of(22, 2);
+
+        // when
+        boolean actual = score.isBust();
+
+        // then
+        boolean expected = true;
+        Assertions.assertThat(actual).isEqualTo(expected);
     }
 }
