1d97e4066deba0cb6278d0cca34442d6a58f9c6a
Hacanna42 <dtmdgus901@gmail.com>
2025-03-12 21:07:31 +0900

    test(WalletTest): 월렛 베팅률 적용 실패 🔴

--- src/test/java/object/participant/WalletTest.java
+++ src/test/java/object/participant/WalletTest.java
@@ -1,5 +1,6 @@
 package object.participant;
 
+import object.game.BattleResult;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,20 @@ public class WalletTest {
 
         // then
         Assertions.assertThat(wallet).isInstanceOf(Wallet.class);
+    }
+
+    @Test
+    void 월렛_베팅률_적용_테스트() {
+        // given
+        int betMoney = 1000;
+        Wallet wallet = new Wallet(betMoney);
+
+        // when
+        wallet.applyBetRate(BattleResult.WIN);
+
+        // then
+        int expected = 2000;
+        int actual = wallet.getEarnedMoney();
+        Assertions.assertThat(actual).isEqualTo(expected);
     }
 }
