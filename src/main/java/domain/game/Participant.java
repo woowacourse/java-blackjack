f7cc4a86b9d71bf437c8b0b4457190d2e0740771
jhan0121 <jhan0121@gmail.com>
2025-03-12 16:36:43 +0900

    refactor: Participant 추상 클래스 변경

--- src/main/java/domain/game/Participant.java
+++ src/main/java/domain/game/Participant.java
@@ -6,7 +6,7 @@ import domain.card.CardNumber;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Participant {
+public abstract class Participant {
 
     protected static final int BURST_BOUND = 21;
 
