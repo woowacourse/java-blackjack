f18094c451cd17d80641ffbdeeed44d0408926dc
MinJun Choi <mj043000@naver.com>
2025-03-13 00:12:04 +0900

    fix: 플레이어 이름 받는 메서드 수정

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String INPUT_PLAYERS_NICKNAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     public static final String INPUT_BETTING_MONEY = "%s의 배팅 금액은?";
 
     public String readNames() {
+        System.out.println(INPUT_PLAYERS_NICKNAMES);
         return scanner.nextLine();
     }
 
