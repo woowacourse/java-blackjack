2de2953bf79aa535e6318c5995d85b6f4d111761
ChoiDongjun <adong0808@naver.com>
2025-03-12 23:06:50 +0900

    refactor(domain): Bet를 BetMoney로 클래스명 변경

---
+++ src/main/java/domain/bet/BetMoney.java
@@ -0,0 +1,40 @@
+package domain.bet;
+
+import java.util.Objects;
+
+public class BetMoney {
+
+    private final double amount;
+
+    public BetMoney(final double amount) {
+        this.amount = amount;
+    }
+
+    public BetMoney applyBlackJackBonus() {
+        return new BetMoney(amount * 1.5);
+    }
+
+    public BetMoney applyDealerBustBonus() {
+        return new BetMoney(amount * 2);
+    }
+
+    public BetMoney applyPlayerBustPenalty() {
+        return new BetMoney(0);
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BetMoney betMoney = (BetMoney) o;
+        return Double.compare(amount, betMoney.amount) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(amount);
+    }
+}
---
+++ src/test/java/domain/bet/BetMoneyTest.java
@@ -0,0 +1,45 @@
+package domain.bet;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BetMoneyTest {
+
+    @Test
+    @DisplayName("금액을 받아 BetMoney를 생성할 수 있다.")
+    void test1() {
+        assertThat(new BetMoney(10000)).isInstanceOf(BetMoney.class);
+    }
+
+    @Test
+    @DisplayName("플레이어가 블랙잭이면 베팅 금액의 1.5배를 받는다.")
+    void test2() {
+        BetMoney betMoney = new BetMoney(10000);
+
+        BetMoney applyedBetMoney = betMoney.applyBlackJackBonus();
+
+        assertThat(applyedBetMoney.getAmount()).isEqualTo(15000);
+    }
+
+    @Test
+    @DisplayName("딜러가 버스트 시 살아남은 플레이어라면 베팅 금액의 2배를 받는다.")
+    void test3() {
+        BetMoney betMoney = new BetMoney(10000);
+
+        BetMoney applyedBetMoney = betMoney.applyDealerBustBonus();
+
+        assertThat(applyedBetMoney.getAmount()).isEqualTo(20000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 버스트 시 베팅 금액을 모두 잃는다.")
+    void test4() {
+        BetMoney betMoney = new BetMoney(10000);
+
+        BetMoney applyedBetMoney = betMoney.applyPlayerBustPenalty();
+
+        assertThat(applyedBetMoney.getAmount()).isEqualTo(0);
+    }
+}
