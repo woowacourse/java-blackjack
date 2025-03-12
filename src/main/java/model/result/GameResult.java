0b2455091554fef7595da68dbed35badfb318ac4
LJM\eueo8 <eueo8259@gmail.com>
2025-03-12 14:12:43 +0900

    refactor : GameResult enum에서 resultMeaning 필드 제거

--- src/main/java/model/result/GameResult.java
+++ src/main/java/model/result/GameResult.java
@@ -1,17 +1,8 @@
 package model.result;
 
 public enum GameResult {
-    WIN("승"),
-    DRAW("무"),
-    LOSE("패"),
-    BLACKJACK("블랙잭");
-    private final String resultMeaning;
-
-    GameResult(String resultMeaning) {
-        this.resultMeaning = resultMeaning;
-    }
-
-    public String getResultMeaning() {
-        return resultMeaning;
-    }
+    WIN,
+    DRAW,
+    LOSE,
+    BLACKJACK;
 }
