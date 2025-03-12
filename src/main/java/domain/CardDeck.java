cce1bca3521000dd4896ce05dda011aa14d45dbc
ppparkta <pppppark2@gmail.com>
2025-03-12 17:50:05 +0900

    refactor: 필드 매개변수 네이밍 적용

--- src/main/java/domain/CardDeck.java
+++ src/main/java/domain/CardDeck.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CardDeck {
-    private final List<TrumpCard> CARD_DECK = new ArrayList<>();
+    private final List<TrumpCard> cardDeck = new ArrayList<>();
 
     public CardDeck() {
-        this.CARD_DECK.addAll(TrumpCard.cardDeckCaching());
+        this.cardDeck.addAll(TrumpCard.cardDeckCaching());
     }
 
     public TrumpCard drawCard() {
-        if (CARD_DECK.isEmpty()) {
+        if (cardDeck.isEmpty()) {
             throw new IllegalArgumentException("카드가 다 떨어졌습니다");
         }
-        return CARD_DECK.removeFirst();
+        return cardDeck.removeFirst();
     }
 
     public TrumpCard getCard(int index) {
-        return CARD_DECK.get(index);
+        return cardDeck.get(index);
     }
 
     public List<TrumpCard> getCardDeck() {
-        return new ArrayList<>(CARD_DECK);
+        return new ArrayList<>(cardDeck);
     }
 }
