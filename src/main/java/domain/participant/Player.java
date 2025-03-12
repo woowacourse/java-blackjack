90b806ac4e5ef296a74f4c6c17af76c57714a00d
ChoiDongjun <adong0808@naver.com>
2025-03-12 23:08:42 +0900

    GREEN) feat(domain) : 플레이어 베팅 기능 구현

--- src/main/java/domain/participant/Player.java
+++ src/main/java/domain/participant/Player.java
@@ -1,9 +1,26 @@
 package domain.participant;
 
+import domain.bet.BetMoney;
+
 public class Player extends Participant {
-    private static final int LOSS = -1;
+
+    private final BetMoney betMoney;
 
     public Player(final String name) {
         super(name);
+        this.betMoney = new BetMoney(0);
+    }
+
+    public Player(final String name, final double betMoney) {
+        super(name);
+        this.betMoney = new BetMoney(betMoney);
+    }
+
+    public Player bet(final double betAmount) {
+        return new Player(getName(), betAmount);
+    }
+
+    public BetMoney getBetMoney() {
+        return betMoney;
     }
 }
