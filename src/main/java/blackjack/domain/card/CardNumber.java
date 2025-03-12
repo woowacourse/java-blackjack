3a1d857bfa647b042db8e0ab75e14f8c388d1a65
songsunkook <songsunkook@gmail.com>
2025-03-12 16:38:42 +0900

    refactor: 누락된 도메인 enum의 displayName 제거

--- src/main/java/blackjack/domain/card/CardNumber.java
+++ src/main/java/blackjack/domain/card/CardNumber.java
@@ -1,34 +1,28 @@
 package blackjack.domain.card;
 
 public enum CardNumber {
-    ACE(1, "A"),
-    TWO(2, "2"),
-    THREE(3, "3"),
-    FOUR(4, "4"),
-    FIVE(5, "5"),
-    SIX(6, "6"),
-    SEVEN(7, "7"),
-    EIGHT(8, "8"),
-    NINE(9, "9"),
-    TEN(10, "10"),
-    JACK(10, "J"),
-    QUEEN(10, "Q"),
-    KING(10, "K"),
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10),
     ;
 
     private final int number;
-    private final String displayName;
 
-    CardNumber(int number, String displayName) {
+    CardNumber(int number) {
         this.number = number;
-        this.displayName = displayName;
     }
 
     public int getNumber() {
         return number;
-    }
-
-    public String getDisplayName() {
-        return displayName;
     }
 }
