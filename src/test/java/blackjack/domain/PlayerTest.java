2770a5b03f091afb424dad70f24b31033560255f
taek2222 <oot309@daum.net>
2025-03-12 21:32:47 +0900

    test: 플레이어 블랙잭인 경우 테스트 추가

--- src/test/java/blackjack/domain/PlayerTest.java
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -1,39 +1,66 @@
 package blackjack.domain;
 
-import blackjack.domain.card.CardPack;
-import blackjack.domain.player.Gambler;
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardNumber;
+import blackjack.domain.card.CardShape;
 import blackjack.domain.player.Player;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
 
 class PlayerTest {
 
+//    @Test
+//    @DisplayName("카드의 합을 계산해 반환한다")
+//    void calculate_calculation_check_rot() {
+//        // given
+//        Player player = new Gambler("두리", 0);
+//        player.addCards(new CardPack(new SortedBlackjackShuffle()), 3);
+//
+//        // when
+//        int result = player.getCardScore();
+//
+//        // then
+//        assertThat(result).isEqualTo(30);
+//    }
+//
+//    @DisplayName("에이스가 2장이면 12로 계산한다")
+//    @Test
+//    void ace_test() {
+//        Player player = new Gambler("두리", 0);
+//        player.addCards(new CardPack(new ReversedBlackjackShuffle()), 2);
+//
+//        // when
+//        int result = player.getCardScore();
+//
+//        // then
+//        assertThat(result).isEqualTo(12);
+//    }
+
     @Test
-    @DisplayName("카드의 합을 계산해 반환한다")
-    void calculate_calculation_check_rot() {
-        // given
-        Player player = new Gambler("두리", 0);
-        player.pushDealCard(new CardPack(new SortedBlackjackShuffle()), 3);
+    @DisplayName("플레이어가 블랙잭인지 판단해 반환한다")
+    void 플레이어가_블랙잭인지_판단해_반환한다() {
+        Card ace = new Card(CardNumber.ACE, CardShape.CLOVER);
+        Card ten = new Card(CardNumber.TEN, CardShape.CLOVER);
 
-        // when
-        int result = player.getCardScore();
+        Player player = new TestPlayer();
+        player.addCards(List.of(ace, ten));
 
-        // then
-        assertThat(result).isEqualTo(30);
+        boolean result = player.isBlackJack();
+        Assertions.assertThat(result).isTrue();
     }
 
-    @DisplayName("에이스가 2장이면 12로 계산한다")
-    @Test
-    void ace_test() {
-        Player player = new Gambler("두리", 0);
-        player.pushDealCard(new CardPack(new ReversedBlackjackShuffle()), 2);
+    static class TestPlayer extends Player {
 
-        // when
-        int result = player.getCardScore();
+        public TestPlayer() {
+            super("Test", 0);
+        }
 
-        // then
-        assertThat(result).isEqualTo(12);
+        @Override
+        public List<Card> getOpenedCards() {
+            return this.getCards().getCards();
+        }
     }
 }
