dfebe869798d6a2de95acd9d565cc34158d3d525
Minuring <mw9505@gmail.com>
2025-03-12 14:23:17 +0900

    refactor: 뷰 의존성 주입

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,13 @@
 import controller.BlackjackController;
+import java.util.Scanner;
 import view.InputView;
 import view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        final InputView inputView = new InputView();
-        final OutputView outputView = new OutputView();
+        final InputView inputView = new InputView(new Scanner(System.in));
+        final OutputView outputView = new OutputView(System.out);
         BlackjackController blackjackController = new BlackjackController(inputView, outputView);
         blackjackController.play();
     }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,12 +5,19 @@ import domain.game.Winning;
 import domain.participant.Dealer;
 import domain.participant.Gambler;
 import domain.participant.Player;
+import java.io.PrintStream;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class OutputView {
+    
+    private final PrintStream printStream;
+
+    public OutputView(PrintStream printStream) {
+        this.printStream = printStream;
+    }
 
     public void printSetUpCardDeck(Dealer dealer, List<Player> players) {
         Card dealerOpenCard = dealer.getOpenCard();
@@ -18,44 +25,44 @@ public class OutputView {
             .map(Player::getName)
             .collect(Collectors.joining(","));
 
-        System.out.printf("딜러와 %s에게 2장을 나누었습니다.%n", playerNames);
-        System.out.println("딜러카드: " + dealerOpenCard);
-        players.forEach(player -> System.out.printf("%s카드: %s%n", player.getName(),
+        printStream.printf("딜러와 %s에게 2장을 나누었습니다.%n", playerNames);
+        printStream.println("딜러카드: " + dealerOpenCard);
+        players.forEach(player -> printStream.printf("%s카드: %s%n", player.getName(),
             formatCards(player.getCards())));
     }
 
     public void printTakenMoreCards(String name, List<Card> cards) {
-        System.out.printf("%s카드: %s%n", name, formatCards(cards));
+        printStream.printf("%s카드: %s%n", name, formatCards(cards));
     }
 
     public void printDealerHasTaken() {
-        System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+        printStream.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
     }
 
     public void printFinalCardDeck(List<Gambler> gamblers) {
         gamblers.forEach(gambler ->
-            System.out.printf("%s카드: %s - 결과: %d\n",
+            printStream.printf("%s카드: %s - 결과: %d\n",
                 gambler.getName(), formatCards(gambler.getCards()), gambler.calculateScore())
         );
     }
 
     public void printGameResult(Map<Winning, Long> dealerWinnings,
         Map<Player, Winning> playerWinnings) {
-        System.out.println("## 최종 승패");
+        printStream.println("## 최종 승패");
         printDealerWinnings(dealerWinnings);
 
         playerWinnings.forEach((player, winning) ->
-            System.out.printf("%s: %s%n", player.getName(), winning.getName()));
+            printStream.printf("%s: %s%n", player.getName(), winning.getName()));
     }
 
     private void printDealerWinnings(Map<Winning, Long> dealerWinnings) {
-        System.out.print("딜러: ");
+        printStream.print("딜러: ");
         dealerWinnings.entrySet()
             .stream()
             .sorted(Entry.comparingByKey())
             .forEach(
-                entry -> System.out.printf("%d%s ", entry.getValue(), entry.getKey().getName()));
-        System.out.println();
+                entry -> printStream.printf("%d%s ", entry.getValue(), entry.getKey().getName()));
+        printStream.println();
     }
 
     private String formatCards(List<Card> cards) {
