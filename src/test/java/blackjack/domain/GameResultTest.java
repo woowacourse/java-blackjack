6b4a309e4929da47bdc1613c2a80abb0dabb862d
taek2222 <oot309@daum.net>
2025-03-12 21:24:25 +0900

    test: 참가자 블랙잭인 경우 1.5배 테스트 추가

--- src/test/java/blackjack/domain/GameResultTest.java
+++ src/test/java/blackjack/domain/GameResultTest.java
@@ -1,6 +1,8 @@
 package blackjack.domain;
 
-import blackjack.domain.card.CardPack;
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardNumber;
+import blackjack.domain.card.CardShape;
 import blackjack.domain.player.Dealer;
 import blackjack.domain.player.Gambler;
 import blackjack.domain.player.Player;
@@ -15,21 +17,18 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 class GameResultTest {
 
-    private static final CardPack SORT_CARD_PACK = new CardPack(new SortedBlackjackShuffle());
-    private static final CardPack REVERSE_SORT_CARD_PACK = new CardPack(new ReversedBlackjackShuffle());
-
     @Test
     @DisplayName("딜러가 이기면 참가자의 배팅 금액을 얻는다")
     void 딜러가_이기면_참가자의_배팅_금액을_얻는다() {
+        Card ace = new Card(CardNumber.ACE, CardShape.CLOVER);
+
         Dealer dealer = new Dealer();
-        dealer.pushDealCard(SORT_CARD_PACK, 2);
+        dealer.addCards(List.of(ace, ace));
 
         Gambler gambler = new Gambler("비타", 1_000);
-        gambler.pushDealCard(SORT_CARD_PACK, 1);
+        gambler.addCards(List.of(ace));
 
-        List<Gambler> gamblers = List.of(gambler);
-
-        GameResults gameResults = new GameResults(dealer, gamblers);
+        GameResults gameResults = new GameResults(dealer, List.of(gambler));
 
         Map<Player, Integer> result = gameResults.getGameResults();
 
@@ -42,15 +41,15 @@ class GameResultTest {
     @Test
     @DisplayName("딜러가 지면 참가자는 배팅 금액의 배로 얻는다")
     void 딜러가_지면_참가자는_배팅_금액의_배로_얻는다() {
+        Card ace = new Card(CardNumber.ACE, CardShape.CLOVER);
+
         Dealer dealer = new Dealer();
-        dealer.pushDealCard(SORT_CARD_PACK, 1);
+        dealer.addCards(List.of(ace));
 
         Gambler gambler = new Gambler("비타", 1_000);
-        gambler.pushDealCard(SORT_CARD_PACK, 2);
+        gambler.addCards(List.of(ace, ace));
 
-        List<Gambler> gamblers = List.of(gambler);
-
-        GameResults gameResults = new GameResults(dealer, gamblers);
+        GameResults gameResults = new GameResults(dealer, List.of(gambler));
 
         Map<Player, Integer> result = gameResults.getGameResults();
 
@@ -63,21 +62,43 @@ class GameResultTest {
     @Test
     @DisplayName("무승부인 경우 각자 최종 수익은 0원이다")
     void 무승부인_경우_각자_최종_수익은_0원이다() {
+        Card ace = new Card(CardNumber.ACE, CardShape.CLOVER);
+
         Dealer dealer = new Dealer();
-        dealer.pushDealCard(SORT_CARD_PACK, 2);
+        dealer.addCards(List.of(ace));
 
         Gambler gambler = new Gambler("비타", 1_000);
-        gambler.pushDealCard(SORT_CARD_PACK, 2);
+        gambler.addCards(List.of(ace));
 
-        List<Gambler> gamblers = List.of(gambler);
-
-        GameResults gameResults = new GameResults(dealer, gamblers);
+        GameResults gameResults = new GameResults(dealer, List.of(gambler));
 
         Map<Player, Integer> result = gameResults.getGameResults();
 
         assertAll(
                 () -> assertThat(result.get(dealer)).isEqualTo(0),
                 () -> assertThat(result.get(gambler)).isEqualTo(0)
+        );
+    }
+
+    @Test
+    @DisplayName("참가자가 블랙잭인 경우 수익은 1.5배이다")
+    void 참가자가_블랙잭인_경우_수익은_1_5배이다() {
+        Card ace = new Card(CardNumber.ACE, CardShape.CLOVER);
+        Card ten = new Card(CardNumber.TEN, CardShape.CLOVER);
+
+        Dealer dealer = new Dealer();
+        dealer.addCards(List.of(ace));
+
+        Gambler gambler = new Gambler("비타", 1_000);
+        gambler.addCards(List.of(ace, ten));
+
+        GameResults gameResults = new GameResults(dealer, List.of(gambler));
+
+        Map<Player, Integer> result = gameResults.getGameResults();
+
+        assertAll(
+                () -> assertThat(result.get(dealer)).isEqualTo(-2_500),
+                () -> assertThat(result.get(gambler)).isEqualTo(2_500)
         );
     }
 }
