5ee9422b72faf94a5b788f92fe927fa11b5153e2
taek2222 <oot309@daum.net>
2025-03-12 20:01:52 +0900

    test: 딜러가 이긴 경우 배팅 금액 테스트 추가

--- src/test/java/blackjack/domain/GameResultTest.java
+++ src/test/java/blackjack/domain/GameResultTest.java
@@ -1,95 +1,33 @@
 package blackjack.domain;
 
-import blackjack.domain.card.CardPack;
 import blackjack.domain.player.Dealer;
 import blackjack.domain.player.Gambler;
+import blackjack.domain.player.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class GameResultTest {
-    @Test
-    @DisplayName("딜러보다 카드 합이 높으면 WIN을 반환한다")
-    void win_if_bigger_than_dealer() {
-        CardPack cardPack = new CardPack(new ReversedBlackjackShuffle());
-        Dealer dealer = new Dealer();
-        Gambler gambler = new Gambler("두리", 0);
-
-        dealer.pushDealCard(cardPack, 1);
-        gambler.pushDealCard(cardPack, 2);
-
-        assertThat(GameResult.evaluateGameResult(dealer, gambler))
-                .isEqualTo(GameResult.WIN);
-    }
 
     @Test
-    @DisplayName("딜러보다 카드 합이 낮으면 LOSE 을 반환한다")
-    void win_if_bigger_than_dealer2() {
-        CardPack cardPack = new CardPack(new ReversedBlackjackShuffle());
+    @DisplayName("딜러가 이기면 참가자의 배팅 금액을 얻는다")
+    void 딜러가_이기면_참가자의_배팅_금액을_얻는다() {
         Dealer dealer = new Dealer();
-        Gambler gambler = new Gambler("두리", 0);
+        Gambler gambler = new Gambler("비타", 1_000);
+        List<Gambler> gamblers = List.of(gambler);
 
-        dealer.pushDealCard(cardPack, 2);
-        gambler.pushDealCard(cardPack, 1);
+        GameResults gameResults = new GameResults(dealer, gamblers);
 
-        assertThat(GameResult.evaluateGameResult(dealer, gambler))
-                .isEqualTo(GameResult.LOSE);
-    }
+        Map<Player, Integer> result = gameResults.getGameResults();
 
-    @Test
-    @DisplayName("딜러와 카드 합이 같으면 DRAW 을 반환한다")
-    void win_if_bigger_than_dealer3() {
-        CardPack cardPack = new CardPack(new ReversedBlackjackShuffle());
-        Dealer dealer = new Dealer();
-        Gambler gambler = new Gambler("두리", 0);
-
-        dealer.pushDealCard(cardPack, 1);
-        gambler.pushDealCard(cardPack, 1);
-
-        assertThat(GameResult.evaluateGameResult(dealer, gambler))
-                .isEqualTo(GameResult.DRAW);
-    }
-
-    @Test
-    @DisplayName("딜러와 참가자가 둘다 bust 면 DRAW 를 반환한다")
-    void both_bust_test() {
-        CardPack cardPack = new CardPack(new SortedBlackjackShuffle());
-        Dealer dealer = new Dealer();
-        Gambler gambler = new Gambler("두리", 0);
-
-        dealer.pushDealCard(cardPack, 3); // 10, 10, 10 총합 30
-        gambler.pushDealCard(cardPack, 3);
-
-        assertThat(GameResult.evaluateGameResult(dealer, gambler))
-                .isEqualTo(GameResult.DRAW);
-    }
-
-    @Test
-    @DisplayName("참가자만 bust 면 LOSE 를 반환한다")
-    void both_bust_test2() {
-        CardPack cardPack = new CardPack(new SortedBlackjackShuffle());
-        Dealer dealer = new Dealer();
-        Gambler gambler = new Gambler("두리", 0);
-
-        dealer.pushDealCard(cardPack, 1);
-        gambler.pushDealCard(cardPack, 3); // 10, 10, 10 총합 30
-
-        assertThat(GameResult.evaluateGameResult(dealer, gambler))
-                .isEqualTo(GameResult.LOSE);
-    }
-
-    @Test
-    @DisplayName("딜러만 bust 면 WIN 를 반환한다")
-    void both_bust_test3() {
-        CardPack cardPack = new CardPack(new SortedBlackjackShuffle());
-        Dealer dealer = new Dealer();
-        Gambler gambler = new Gambler("두리", 0);
-
-        dealer.pushDealCard(cardPack, 3); // 10, 10, 10 총합 30
-        gambler.pushDealCard(cardPack, 1);
-
-        assertThat(GameResult.evaluateGameResult(dealer, gambler))
-                .isEqualTo(GameResult.WIN);
+        assertAll(
+                () -> assertThat(result.get(dealer)).isEqualTo(1_000),
+                () -> assertThat(result.get(gambler)).isEqualTo(-1_000)
+        );
     }
 }
