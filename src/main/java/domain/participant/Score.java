12bd857d68dc916718924c95b32168d29b6f0c6e
Hacanna42 <dtmdgus901@gmail.com>
2025-03-12 12:07:49 +0900

    feat(Score): 블랙잭, 버스트 확인 성공 🟢

--- src/main/java/domain/participant/Score.java
+++ src/main/java/domain/participant/Score.java
@@ -1,15 +1,34 @@
 package domain.participant;
 
+import java.util.Objects;
+
 public class Score {
     private final ScoreType scoreType;
     private final int score;
 
-    public Score(int score, int cardSize) {
-        this.scoreType = getScoreType(score, cardSize);
+    private Score(ScoreType scoreType, int score) {
+        this.scoreType = scoreType;
         this.score = score;
     }
 
-    private ScoreType getScoreType(int score, int cardSize) {
+    public static Score of(int score, int cardSize) {
+        ScoreType scoreType = getScoreType(score, cardSize);
+        return new Score(scoreType, score);
+    }
+
+    public boolean isBlackJack() {
+        return Objects.equals(scoreType, ScoreType.BLACKJACK);
+    }
+
+    public boolean isBust() {
+        return Objects.equals(scoreType, ScoreType.BUST);
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    private static ScoreType getScoreType(int score, int cardSize) {
         if (score > 21) {
             return ScoreType.BUST;
         }
