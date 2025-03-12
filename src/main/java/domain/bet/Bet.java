942298d601114786de2ee0bcf66fa221ad1f2b92
ChoiDongjun <adong0808@naver.com>
2025-03-12 15:49:00 +0900

    RED) test(domain) : 플레이어 버스트 시 베팅 금액을 모두 잃는 기능 테스트 작성

--- src/main/java/domain/bet/Bet.java
+++ src/main/java/domain/bet/Bet.java
@@ -8,11 +8,19 @@ public class Bet {
         this.amount = amount;
     }
 
-    public double applyBlackJackBonus() {
-        return amount * 1.5;
+    public Bet applyBlackJackBonus() {
+        return new Bet(amount * 1.5);
     }
 
-    public double applyDealerBustBonus() {
-        return amount * 2;
+    public Bet applyDealerBustBonus() {
+        return new Bet(amount * 2);
+    }
+
+    public Bet applyPlayerBustPenalty() {
+        return new Bet(10000);
+    }
+
+    public double getAmount() {
+        return amount;
     }
 }
--- src/test/java/domain/bet/BetTest.java
+++ src/test/java/domain/bet/BetTest.java
@@ -15,18 +15,32 @@ class BetTest {
     }
 
     @Test
-    @DisplayName("플레이어가 블랙잭이면 베팅 금액의 1.5배를 리턴한다.")
+    @DisplayName("플레이어가 블랙잭이면 베팅 금액의 1.5배를 받는다.")
     void test2() {
         Bet bet = new Bet(10000);
 
-        assertThat(bet.applyBlackJackBonus()).isEqualTo(15000);
+        Bet applyedBet = bet.applyBlackJackBonus();
+
+        assertThat(applyedBet.getAmount()).isEqualTo(15000);
     }
 
     @Test
-    @DisplayName("딜러가 버스트 시 살아남은 플레이어라면 베팅 금액의 2배를 리턴한다.")
+    @DisplayName("딜러가 버스트 시 살아남은 플레이어라면 베팅 금액의 2배를 받는다.")
     void test3() {
         Bet bet = new Bet(10000);
 
-        assertThat(bet.applyDealerBustBonus()).isEqualTo(20000);
+        Bet applyedBet = bet.applyDealerBustBonus();
+
+        assertThat(applyedBet.getAmount()).isEqualTo(20000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 버스트 시 베팅 금액을 모두 잃는다.")
+    void test4() {
+        Bet bet = new Bet(10000);
+
+        Bet applyedBet = bet.applyPlayerBustPenalty();
+
+        assertThat(applyedBet.getAmount()).isEqualTo(0);
     }
 }
