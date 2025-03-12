bee8e58f437f3657a7e513702e5d323da5d08127
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-12 21:17:01 +0900

    test: 우선 BlackjackParticipantsManagerTest 중복 닉네임 검증 테스트만 구현

---
+++ src/test/java/domain/participant/BlackjackParticipantsManagerTest.java
@@ -0,0 +1,27 @@
+package domain.participant;
+
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import exception.BlackJackException;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class BlackjackParticipantsManagerTest {
+
+    @Test
+    void 중복된_닉네임을_가질수_없다() {
+
+        List<BlackjackParticipant> players = new ArrayList<>();
+        players.add(new Player("투다"));
+        players.add(new Player("투다"));
+        Dealer dealer = new Dealer();
+        assertThatThrownBy(() -> new BlackjackParticipantsManager(players, dealer))
+                .isInstanceOf(BlackJackException.class);
+    }
+}
+
