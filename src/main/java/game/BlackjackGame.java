da604632a4d1e69c27bd097da0214afc8ee1da92
soeun2537 <soeun2537@gmail.com>
2025-03-12 21:17:26 +0900

    refactor: Controller 제거 및 클래스 분리

---
+++ src/main/java/game/BlackjackGame.java
@@ -0,0 +1,63 @@
+package game;
+
+import deck.Deck;
+import participant.Dealer;
+import participant.Player;
+import participant.Players;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BlackjackGame {
+
+    private static final int INITIAL_COUNT = 2;
+
+    public void distributeTwoCardsToParticipants(Dealer dealer, Players players, Deck deck) {
+        for (int count = 0; count < INITIAL_COUNT; count++) {
+            dealer.receiveCard(deck.draw());
+            players.getPlayers().forEach(player -> player.receiveCard(deck.draw()));
+        }
+    }
+
+    public void runPlayerTurn(Player player, Deck deck, boolean drawOneMore) {
+        if (!drawOneMore || !player.canReceiveCard()) {
+            return;
+        }
+        player.receiveCard(deck.draw());
+    }
+
+    public void runDealerTurn(Dealer dealer, Deck deck) {
+        if (!dealer.canReceiveCard()) {
+            return;
+        }
+        dealer.receiveCard(deck.draw());
+    }
+
+    public Map<Player, GameResult> calculatePlayersGameResults(Dealer dealer, Players players) {
+        return players.getPlayers().stream()
+                .collect(Collectors.toMap(
+                        player -> player,
+                        player -> GameResult.judge(player, dealer),
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new
+                ));
+    }
+
+    public Map<GameResult, Integer> calculateDealerGameResults(Map<Player, GameResult> playerGameResults) {
+        Map<GameResult, Integer> dealerResults = Arrays.stream(GameResult.values())
+                .collect(Collectors.toMap(
+                        gameResult -> gameResult,
+                        gameResult -> 0,
+                        (oldValue, newValue) -> oldValue,
+                        () -> new EnumMap<>(GameResult.class)
+                ));
+
+        for (GameResult gameResult : playerGameResults.values()) {
+            dealerResults.put(gameResult.inverse(), dealerResults.get(gameResult.inverse()) + 1);
+        }
+
+        return dealerResults;
+    }
+}
---
+++ src/main/java/game/BlackjackManager.java
@@ -0,0 +1,58 @@
+package game;
+
+import deck.Deck;
+import participant.Dealer;
+import participant.Player;
+import participant.Players;
+import java.util.Map;
+import view.InputView;
+import view.OutputView;
+
+public class BlackjackManager {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BlackjackGame blackjackGame;
+
+    public BlackjackManager(InputView inputView, OutputView outputView, BlackjackGame blackjackGame) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.blackjackGame = blackjackGame;
+    }
+
+    public void start(Dealer dealer, Players players, Deck deck) {
+        processDistributionTwoCardsToParticipants(dealer, players, deck);
+        processPlayersTurn(players, deck);
+        processDealerTurn(dealer, deck);
+        processParticipantResults(dealer, players);
+    }
+
+    private void processDistributionTwoCardsToParticipants(Dealer dealer, Players players, Deck deck) {
+        blackjackGame.distributeTwoCardsToParticipants(dealer, players, deck);
+        outputView.printDistributionCards(dealer, players);
+    }
+
+    private void processPlayersTurn(Players players, Deck deck) {
+        for (Player player : players.getPlayers()) {
+            while (player.canReceiveCard()) {
+                boolean drawOneMore = inputView.readDrawOneMore(player.getNickname());
+                blackjackGame.runPlayerTurn(player, deck, drawOneMore);
+                outputView.printPlayerCards(player);
+            }
+        }
+    }
+
+    private void processDealerTurn(Dealer dealer, Deck deck) {
+        while (dealer.canReceiveCard()) {
+            blackjackGame.runDealerTurn(dealer, deck);
+            outputView.printDealerReceivedCard();
+        }
+    }
+
+    private void processParticipantResults(Dealer dealer, Players players) {
+        outputView.printAllCardAndScore(players, dealer);
+        Map<Player, GameResult> playersGameResults = blackjackGame.calculatePlayersGameResults(dealer, players);
+        Map<GameResult, Integer> dealerGameResults = blackjackGame.calculateDealerGameResults(playersGameResults);
+        outputView.printResult(dealerGameResults, playersGameResults);
+    }
+}
