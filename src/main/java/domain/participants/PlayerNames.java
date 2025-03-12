31dcd4d1ad5b33a54e54f755a86436a3bf9c034c
wogus <ind07152@naver.com>
2025-03-12 16:16:12 +0900

    feat: PlayerNames 일급 컬렉션 생성
    - 중복 이름 검증 기능 추가

---
+++ src/main/java/domain/participants/PlayerNames.java
@@ -0,0 +1,29 @@
+package domain.participants;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PlayerNames {
+    private final List<PlayerName> playerNames;
+
+    public PlayerNames(List<String> usernames) {
+        List<PlayerName> playerNames = usernames.stream()
+                .map(PlayerName::new)
+                .toList();
+        validateDuplicate(playerNames);
+        this.playerNames = playerNames;
+    }
+
+    private void validateDuplicate(List<PlayerName> playerNames) {
+        Set playerNamesSet = new HashSet(playerNames);
+        if (playerNamesSet.size() != playerNames.size()) {
+            throw new IllegalArgumentException("중복된 이름이 있습니다.");
+        }
+    }
+
+    public List<PlayerName> getPlayerNames() {
+        return Collections.unmodifiableList(playerNames);
+    }
+}
