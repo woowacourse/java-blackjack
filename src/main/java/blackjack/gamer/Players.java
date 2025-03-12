2fdff730fe453f174f85470c4fc55d5ebe910797
MinJun Choi <mj043000@naver.com>
2025-03-12 23:24:00 +0900

    feat: 공백 입력 시 재입력하도록 구현

--- src/main/java/blackjack/gamer/Players.java
+++ src/main/java/blackjack/gamer/Players.java
@@ -1,7 +1,6 @@
 package blackjack.gamer;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Players {
@@ -13,12 +12,26 @@ public class Players {
     }
 
     public void addPlayersFrom(final String names) {
-        final String[] parsedNames = names.replace(" ", "").split(",");
-        if (parsedNames.length >= 6) {
+        final List<String> parsedNames = List.of(names.replace(" ", "").split(","));
+        validateParsedNames(parsedNames);
+        parsedNames.forEach(parsedName -> players.add(new Player(parsedName)));
+    }
+
+    private void validateParsedNames(final List<String> parsedNames) {
+        validateEmpty(parsedNames);
+        validateNameCount(parsedNames);
+    }
+
+    private void validateEmpty(final List<String> parsedNames) {
+        if (parsedNames == null || parsedNames.isEmpty()) {
+            throw new IllegalArgumentException("비어있는 값을 입력했습니다. 다시 입력해주세요.");
+        }
+    }
+
+    private void validateNameCount(final List<String> parsedNames) {
+        if (parsedNames.size() >= 6) {
             throw new IllegalArgumentException("적정 인원을 초과했습니다. 다시 입력해주세요");
         }
-        Arrays.stream(parsedNames)
-                .forEach(parsedName -> players.add(new Player(parsedName)));
     }
 
     public List<Player> getPlayers() {
