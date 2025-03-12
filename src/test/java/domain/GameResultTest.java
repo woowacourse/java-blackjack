35eb5a96bdbcd11d64f61c5b3524f735eb535e09
jeyong <joon6093@naver.com>
2025-03-13 02:05:34 +0900

    feat: 게임결과에 배율 정보 추가

--- src/test/java/domain/GameResultTest.java
+++ src/test/java/domain/GameResultTest.java
@@ -43,8 +43,26 @@ class GameResultTest {
         static Stream<Arguments> provideDrawCases() {
             return Stream.of(
                     Arguments.of(Score.TWENTY_ONE, Score.TWENTY_ONE),
+                    Arguments.of(Score.BLACKJACK, Score.BLACKJACK),
                     Arguments.of(Score.TWENTY, Score.TWENTY),
                     Arguments.of(Score.SEVENTEEN, Score.SEVENTEEN)
+            );
+        }
+
+        @ParameterizedTest
+        @DisplayName("플레이어가 블랙잭이면 BLACKJACK_WIN을을 반환한다.")
+        @MethodSource("provideBlackJackWinCases")
+        void playerBlackJackWins(Score playerScore, Score dealerScore) {
+            GameResult result = GameResult.of(playerScore, dealerScore);
+            assertThat(result).isEqualTo(GameResult.BLACKJACK_WIN);
+        }
+
+        static Stream<Arguments> provideBlackJackWinCases() {
+            return Stream.of(
+                    Arguments.of(Score.BLACKJACK, Score.TWENTY),
+                    Arguments.of(Score.BLACKJACK, Score.SEVENTEEN),
+                    Arguments.of(Score.BLACKJACK, Score.SEVEN),
+                    Arguments.of(Score.BLACKJACK, Score.BUST)
             );
         }
 
