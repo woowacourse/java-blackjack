f9ec27bbabc91fa1ef00bf64c7fe686eed260ebf
jhpark1227 <junhyeok0246@naver.com>
2025-03-13 17:44:05 +0900

    🟢green: 플레이어가 가진 카드의 블랙잭 여부를 판단하는 로직 구현

--- src/main/java/domain/participant/Participant.java
+++ src/main/java/domain/participant/Participant.java
@@ -29,7 +29,7 @@ public abstract class Participant {
     }
 
     public boolean isBlackJack() {
-        return true;
+        return hand.isBlackJack();
     }
 
     private boolean isTwentyOne() {
