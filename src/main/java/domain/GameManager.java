d252f8841550ab12a7fba65b460037dbd985725d
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-14 00:48:46 +0900

    refactor: Dealer 필드 위치 변경
    - GameManager -> Users

--- src/main/java/domain/GameManager.java
+++ src/main/java/domain/GameManager.java
@@ -14,20 +14,34 @@ public class GameManager {
     public final static int MAX_PLAYER = 7;
     public final static int MAX_RANGE_HAND_OUT_CARD = 2;
     public final static int MIN_RANGE_HAND_OUT_CARD = 0;
+
     private final Users users;
-    private final User dealer;
+    private final Betting betting;
     private final TrumpCardManager trumpCardManager;
 
-    public GameManager(List<String> names, TrumpCardManager trumpCardManager) {
-        validate(names);
-        this.users = new Users(names.stream()
-                .map(Player::new)
-                .collect(Collectors.toList()));
-        this.dealer = new Dealer("딜러");
+    public GameManager(Users users, Betting betting, TrumpCardManager trumpCardManager) {
+        this.users = users;
+        this.betting = betting;
         this.trumpCardManager = trumpCardManager;
     }
 
-    private void validate(List<String> names) {
+    public static GameManager initailizeGameManager(List<String> names, TrumpCardManager trumpCardManager) {
+        validate(names);
+        List<User> users = names.stream().map(Player::new).collect(Collectors.toList());
+        User dealer = new Dealer();
+        users.add(dealer);
+        return new GameManager(
+                new Users(users),
+                new Betting(names.stream()
+                        .map(Player::new)
+                        .collect(Collectors.toList())
+                ),
+                trumpCardManager
+        );
+    }
+
+    // TODO: static을 제거하는 과정을 생각해보자
+    private static void validate(List<String> names) {
         HashSet<String> distinctNames = new HashSet<>(names);
         if (names.isEmpty() || names.size() > MAX_PLAYER) {
             throw new IllegalArgumentException("유저는 1명 이상 7명 이하로 등록해야 합니다.");
@@ -37,10 +51,14 @@ public class GameManager {
         }
     }
 
+    // TODO: 해당 메서드 구현 필요
+    public void submitBet(User user, Long money) {
+
+    }
+
     public void firstHandOutCard() {
         for (int count = MIN_RANGE_HAND_OUT_CARD; count < MAX_RANGE_HAND_OUT_CARD; count++) {
             users.userCardDraw(trumpCardManager);
-            dealer.receiveCard(trumpCardManager.drawCard());
         }
     }
 
@@ -48,49 +66,55 @@ public class GameManager {
         user.receiveCard(trumpCardManager.drawCard());
     }
 
-    public User findUserByUsername(String name) {
-        return users.findByUserName(name);
+    public Map<User, GameResult> createGameResult() {
+        Map<User, GameResult> gameResult = new LinkedHashMap<>();
+        Dealer dealer = users.findDealer();
+        List<Player> players = users.findPlayers();
+        if (dealer.isBurst()) {
+            players.forEach((user) -> putGameResultBurst(user, gameResult));
+            return gameResult;
+        }
+        players.forEach((player) -> gameResult.put(player, compare(player, dealer)));
+        return gameResult;
     }
 
-    public User getDealer() {
-        return this.dealer;
-    }
-
-    public GameResult compare(User player) {
-        if (player.isBurst()) {
+    public GameResult compare(final Player player, final Dealer dealer) {
+        if (dealer.isBurst()) {
             return GameResult.LOSE;
         }
-        if (dealer.getCardDeck().calculateScore() < player.getCardDeck().calculateScore()) {
+        if (dealer.userScore() < player.userScore()) {
             return GameResult.WIN;
         }
-        if (dealer.getCardDeck().calculateScore() > player.getCardDeck().calculateScore()) {
+        if (dealer.userScore() > player.userScore()) {
             return GameResult.LOSE;
         }
-        return compareSameScore(player);
+        return compareSameScore(player, dealer);
     }
 
-    private GameResult compareSameScore(User player) {
+    private GameResult compareSameScore(final User player, final User dealer) {
         if (dealer.getCardDeck().isBlackjack() && !player.getCardDeck().isBlackjack()) {
             return GameResult.LOSE;
         }
         return GameResult.DRAW;
     }
 
-    public Map<User, GameResult> createGameResult() {
-        Map<User, GameResult> gameResult = new LinkedHashMap<>();
-        if (dealer.isBurst()) {
-            users.getUsers().forEach((user) -> putGameResultBurst(user, gameResult));
-            return gameResult;
-        }
-        users.getUsers().forEach((user) -> gameResult.put(user, compare(user)));
-        return gameResult;
-    }
-
-    private void putGameResultBurst(User user, Map<User, GameResult> gameResult) {
+    private void putGameResultBurst(final User user, final Map<User, GameResult> gameResult) {
         if (user.isBurst()) {
             gameResult.put(user, GameResult.LOSE);
             return;
         }
         gameResult.put(user, GameResult.WIN);
+    }
+
+    public String findDealerName() {
+        return users.findDealerName();
+    }
+
+    public Player findPlayerByUsername(final String name) {
+        return users.findByPlayerName(name);
+    }
+
+    public Dealer getDealer() {
+        return users.findDealer();
     }
 }
--- src/main/java/domain/user/Users.java
+++ src/main/java/domain/user/Users.java
@@ -1,7 +1,6 @@
 package domain.user;
 
 import domain.TrumpCardManager;
-import java.util.Collections;
 import java.util.List;
 
 public class Users {
@@ -11,18 +10,36 @@ public class Users {
         this.users = users;
     }
 
-    public void userCardDraw(TrumpCardManager trumpCardManager) {
-        users.forEach((user) -> user.receiveCard(trumpCardManager.drawCard()));
+    public List<Player> findPlayers() {
+        return users.stream()
+                .filter(user -> !user.isDealer())
+                .map(user -> (Player) user)
+                .toList();
     }
 
-    public User findByUserName(String name) {
+    public Dealer findDealer() {
         return users.stream()
+                .filter(User::isDealer)
+                .map(user -> (Dealer) user)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("딜러가 존재하지 않습니다"));
+    }
+
+    public Player findByPlayerName(String name) {
+        return users.stream()
+                .filter(user -> !user.isDealer())
                 .filter(user -> user.hasName(name))
+                .map(user -> (Player) user)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
     }
 
-    public List<User> getUsers() {
-        return Collections.unmodifiableList(users);
+    public String findDealerName() {
+        return findDealer().getName();
     }
+    
+    public void userCardDraw(TrumpCardManager trumpCardManager) {
+        users.forEach((user) -> user.receiveCard(trumpCardManager.drawCard()));
+    }
+
 }
