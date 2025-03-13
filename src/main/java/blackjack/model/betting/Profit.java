71927dc17a6a33489dd5ea02df8c3c0c7c33128d
gabean13 <choegabin171@gmail.com>
2025-03-13 16:32:14 +0900

    feat: 수익률 기능 구현
    - 수익률을 가집니다.
    - 승패 결과에 따른 수익률을 계산합니다.

---
+++ src/main/java/blackjack/model/betting/Profit.java
@@ -0,0 +1,48 @@
+package blackjack.model.betting;
+
+import blackjack.model.MatchResult;
+import java.util.Objects;
+
+public class Profit {
+
+    private final int profit;
+
+    private Profit(int profit) {
+        this.profit = profit;
+    }
+
+    public static Profit of(BetAmount betAmount, MatchResult matchResult) {
+        int stake = betAmount.getStake();
+        if(matchResult == MatchResult.BLACKJACK) {
+            return new Profit((int) (1.5 * stake));
+        }
+        if (matchResult == MatchResult.WIN) {
+            return new Profit(stake);
+        }
+        if(matchResult == MatchResult.DRAW) {
+            return new Profit(0);
+        }
+        return new Profit(stake * -1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Profit profit1 = (Profit) o;
+        return profit == profit1.profit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(profit);
+    }
+
+    public int getProfit() {
+        return profit;
+    }
+}
