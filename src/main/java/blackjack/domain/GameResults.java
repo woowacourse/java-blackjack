cccd79eb4f703a05c1bb5f6cc06f4e427c35b265
taek2222 <oot309@daum.net>
2025-03-13 17:22:07 +0900

    refactor: 메서드 분리 및 로직 최적화

--- src/main/java/blackjack/domain/GameResults.java
+++ src/main/java/blackjack/domain/GameResults.java
@@ -14,53 +14,50 @@ public class GameResults {
 
     public GameResults(final Dealer dealer, final List<Gambler> gamblers) {
         gameResults = new LinkedHashMap<>();
-
         for (Gambler gambler : gamblers) {
-            int compared = compareDealerWithGambler(dealer, gambler);
-
+            int compared = compareGamblerWithDealer(gambler, dealer);
+            int multiplier = gambler.getHand().calculateBetAmountByMultiplier(1.0);
             if (compared == -1) {
-                int multiplier = gambler.getHand().calculateBetAmountByMultiplier(1.0);
-                gameResults.merge(dealer, multiplier, Integer::sum);
-                gameResults.merge(gambler, -multiplier, Integer::sum);
-            }
-
-            if (compared == 1) {
-                int multiplier = gambler.getHand().calculateBetAmountByMultiplier(1.0);
-                if (gambler.isBlackJack()) {
-                    multiplier = gambler.getHand().calculateBetAmountByMultiplier(1.5);
-                }
-                gameResults.merge(dealer, -multiplier, Integer::sum);
-                gameResults.merge(gambler, multiplier, Integer::sum);
+                multiplier = gambler.getHand().calculateBetAmountByMultiplier(-1.0);
             }
 
             if (compared == 0) {
-                int multiplier = gambler.getHand().calculateBetAmountByMultiplier(0);
-                gameResults.merge(dealer, multiplier, Integer::sum);
-                gameResults.merge(gambler, multiplier, Integer::sum);
+                multiplier = gambler.getHand().calculateBetAmountByMultiplier(0);
             }
+
+            if (gambler.isBlackJack()) {
+                multiplier = gambler.getHand().calculateBetAmountByMultiplier(1.5);
+            }
+
+            updateGameResults(dealer, gambler, multiplier);
         }
     }
 
-    public int compareDealerWithGambler(final Dealer dealer, final Gambler gambler) {
-        if (dealer.isBust() && gambler.isNotBust()) {
-            return -1;
+    private void updateGameResults(final Dealer dealer, final Gambler gambler, final int multiplier) {
+        gameResults.merge(dealer, -multiplier, Integer::sum);
+        gameResults.merge(gambler, multiplier, Integer::sum);
+    }
+
+    public int compareGamblerWithDealer(final Gambler gambler, final Dealer dealer) {
+        if (gambler.isNotBust() && dealer.isBust()) {
+            return 1;
         }
         if (dealer.isBust() && gambler.isBust()) {
             return 0;
         }
         if (dealer.isNotBust() && gambler.isBust()) {
-            return 1;
+            return -1;
         }
         if (dealer.isBlackJack() || gambler.isBlackJack()) {
             if (dealer.isBlackJack() && gambler.isBlackJack()) {
                 return 0;
             }
             if (gambler.isBlackJack()) {
-                return -1;
+                return 1;
             }
-            return 1;
+            return -1;
         }
-        return Integer.compare(dealer.getCardScore(), gambler.getCardScore());
+        return Integer.compare(gambler.getCardScore(), dealer.getCardScore());
     }
 
     public Map<Player, Integer> getGameResults() {
