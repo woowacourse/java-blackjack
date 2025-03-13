4c1a8fc18ea08106341d6d8d91aef5e620eeb13f
CheChe903 <cheche903@naver.com>
2025-03-14 08:56:24 +0900

    feat(Betting): 수익 계산 비즈니스 로직 구현
    - 블랙잭 없이 승리 시
    - 블랙잭이며 승리 시
    - 패배 시

--- src/main/java/domain/gamer/Betting.java
+++ src/main/java/domain/gamer/Betting.java
@@ -14,4 +14,16 @@ public class Betting {
             throw new IllegalArgumentException("배팅 금액은 1,000원 이상, 100,000원 이하여야 합니다.");
         }
     }
+
+    public int winBlackJackBetting(final int profit) {
+        return (int) (profit + (betAmount * 1.5));
+    }
+
+    public int winBetting(final int profit) {
+        return profit + betAmount;
+    }
+
+    public int loseBetting(final int profit) {
+        return profit - betAmount;
+    }
 }
