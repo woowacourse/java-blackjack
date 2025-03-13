48afd4c40278cf4761c0c18b7298fb93cf4b7a38
soeun2537 <soeun2537@gmail.com>
2025-03-13 17:28:47 +0900

    test: BlackJackGame 통합 테스트 작성

--- src/test/java/game/BlackjackGameTest.java
+++ src/test/java/game/BlackjackGameTest.java
@@ -5,29 +5,55 @@ import static org.assertj.core.api.Assertions.assertThat;
 import card.Card;
 import card.CardNumber;
 import card.CardShape;
+import deck.Deck;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import participant.Dealer;
 import participant.Player;
+import participant.Players;
 
 class BlackjackGameTest {
 
+    int misoBettingMoney = 10000;
+    int yulmuBettingMoney = 20000;
+    Player miso = new Player("미소", misoBettingMoney);
+    Player yulmu = new Player("율무", yulmuBettingMoney);
+
+    Dealer dealer;
+    Players players;
+    Deck deck;
+    BlackjackGame blackjackGame;
+
+    @BeforeEach
+    void beforeEach() {
+        dealer = new Dealer();
+        players = new Players(List.of(miso, yulmu));
+        deck = new Deck(() -> List.of(
+                new Card(CardShape.SPADE, CardNumber.THREE),
+                new Card(CardShape.SPADE, CardNumber.TEN),
+                new Card(CardShape.CLOVER, CardNumber.TEN),
+                new Card(CardShape.HEART, CardNumber.TEN),
+                new Card(CardShape.DIAMOND, CardNumber.TEN),
+                new Card(CardShape.DIAMOND, CardNumber.THREE)
+        ));
+        blackjackGame = new BlackjackGame();
+    }
+
     @Test
-    @DisplayName("처음 게임 실행 시 각 플레이어에게 2장의 카드를 분배한다.")
+    @DisplayName("각 플레이어에게 2장의 카드를 분배한다.")
     void test1() {
         // given
-        List<String> playerNames = List.of("미소", "율무", "헤일러");
 
         // when
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        blackjackGame.distributeTwoCardsToParticipants();
+        blackjackGame.distributeTwoCardsToParticipants(dealer, players, deck);
 
         // then
-        for (Player player : blackjackGame.getPlayers().getPlayers()) {
-            assertThat(player.getCards().size()).isEqualTo(2);
+        for (Player player : players.getPlayers()) {
+            assertThat(player.getCards().size())
+                    .isEqualTo(2);
         }
     }
 
@@ -35,257 +61,217 @@ class BlackjackGameTest {
     @DisplayName("Players 카드가 Bust가 아니라면 한 장 더 받는다.")
     void test2() {
         // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Player miso = blackjackGame.getPlayers().getPlayers().getFirst();
         miso.receiveCard(new Card(CardShape.SPADE, CardNumber.THREE));
         miso.receiveCard(new Card(CardShape.HEART, CardNumber.THREE));
         miso.receiveCard(new Card(CardShape.DIAMOND, CardNumber.THREE));
 
         // when
-        blackjackGame.runPlayerTurn(miso);
+        blackjackGame.runPlayerTurn(miso, deck);
 
         // then
-        assertThat(miso.getCards().size()).isEqualTo(4);
+        assertThat(miso.getCards().size())
+                .isEqualTo(4);
     }
 
     @Test
     @DisplayName("Players 카드가 Bust가 아니라면 한 장 더 받는다. (ACE 포함)")
     void test3() {
         // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Player miso = blackjackGame.getPlayers().getPlayers().getFirst();
         miso.receiveCard(new Card(CardShape.SPADE, CardNumber.ACE));
         miso.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
         miso.receiveCard(new Card(CardShape.DIAMOND, CardNumber.ACE));
 
         // when
-        blackjackGame.runPlayerTurn(miso);
+        blackjackGame.runPlayerTurn(miso, deck);
 
         // then
-        assertThat(miso.getCards().size()).isEqualTo(4);
+        assertThat(miso.getCards().size())
+                .isEqualTo(4);
     }
 
     @Test
     @DisplayName("Players 카드가 Bust라면 카드를 받지 않는다.")
     void test4() {
         // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Player miso = blackjackGame.getPlayers().getPlayers().getFirst();
         miso.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
         miso.receiveCard(new Card(CardShape.HEART, CardNumber.KING));
         miso.receiveCard(new Card(CardShape.DIAMOND, CardNumber.KING));
 
         // when
-        blackjackGame.runPlayerTurn(miso);
+        blackjackGame.runPlayerTurn(miso, deck);
 
         // then
-        assertThat(miso.getCards().size()).isEqualTo(3);
+        assertThat(miso.getCards().size())
+                .isEqualTo(3);
     }
 
     @Test
     @DisplayName("Dealer 카드가 Bust가 아니라면 한 장 더 받는다.")
     void test5() {
         // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Dealer dealer = blackjackGame.getDealer();
         dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.THREE));
         dealer.receiveCard(new Card(CardShape.HEART, CardNumber.THREE));
         dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.THREE));
 
         // when
-        blackjackGame.runDealerTurn();
+        blackjackGame.runDealerTurn(dealer, deck);
 
         // then
-        assertThat(dealer.getCards().size()).isEqualTo(4);
+        assertThat(dealer.getCards().size())
+                .isEqualTo(4);
     }
 
     @Test
-    @DisplayName("Dealer 카드가 Bust가 아니라면 한 장 더 받는다.")
+    @DisplayName("Dealer 카드가 Bust가 아니라면 한 장 더 받는다. (ACE 포함)")
     void test6() {
         // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Dealer dealer = blackjackGame.getDealer();
         dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.ACE));
         dealer.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
         dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.ACE));
 
         // when
-        blackjackGame.runDealerTurn();
+        blackjackGame.runDealerTurn(dealer, deck);
 
         // then
-        assertThat(dealer.getCards().size()).isEqualTo(4);
+        assertThat(dealer.getCards().size())
+                .isEqualTo(4);
     }
 
     @Test
     @DisplayName("Dealer 카드가 Bust라면 카드를 받지 않는다.")
     void test7() {
         // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Dealer dealer = blackjackGame.getDealer();
         dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
         dealer.receiveCard(new Card(CardShape.HEART, CardNumber.KING));
         dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.KING));
 
         // when
-        blackjackGame.runDealerTurn();
+        blackjackGame.runDealerTurn(dealer, deck);
 
         // then
-        assertThat(dealer.getCards().size()).isEqualTo(3);
+        assertThat(dealer.getCards().size())
+                .isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Player의 돈을 업데이트한다.")
+    void test8() {
+        // given
+        miso.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
+        miso.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
+        dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
+        dealer.receiveCard(new Card(CardShape.HEART, CardNumber.KING));
+
+        // when
+        blackjackGame.updatePlayerMoney(dealer, miso);
+
+        // then
+        assertThat(miso.calculateProfit())
+                .isEqualTo(misoBettingMoney * GameResult.BLACKJACK.getRate());
+    }
+
+    @Test
+    @DisplayName("Players 게임 결과를 반환한다. (블랙잭)")
+    void test9() {
+        // given
+        miso.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
+        miso.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
+        dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.ACE));
+        dealer.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
+        dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.ACE));
+        blackjackGame.updatePlayerMoney(dealer, miso);
+
+        // when
+        Map<Player, Double> playersGameResults = blackjackGame.calculatePlayersGameResults(players);
+        Double result = playersGameResults.get(miso);
+
+        // then
+        assertThat(result)
+                .isEqualTo(misoBettingMoney * GameResult.BLACKJACK.getRate());
     }
 
     @Test
     @DisplayName("Players 게임 결과를 반환한다. (승)")
-    void test8() {
+    void test10() {
         // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Player miso = blackjackGame.getPlayers().getPlayers().getFirst();
         miso.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
         miso.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
         miso.receiveCard(new Card(CardShape.DIAMOND, CardNumber.SEVEN));
-        Dealer dealer = blackjackGame.getDealer();
         dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.ACE));
         dealer.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
         dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.ACE));
+        blackjackGame.updatePlayerMoney(dealer, miso);
 
         // when
-        Map<Player, GameResult> playerGameResult = blackjackGame.calculatePlayerGameResult();
+        Map<Player, Double> playersGameResults = blackjackGame.calculatePlayersGameResults(players);
+        Double result = playersGameResults.get(miso);
 
         // then
-        for (Entry<Player, GameResult> entry : playerGameResult.entrySet()) {
-            assertThat(entry.getKey()).isEqualTo(miso);
-            assertThat(entry.getValue()).isEqualTo(GameResult.WIN);
-        }
+        assertThat(result)
+                .isEqualTo(misoBettingMoney * GameResult.WIN.getRate());
     }
 
     @Test
     @DisplayName("Players 게임 결과를 반환한다. (패)")
-    void test9() {
-        // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Player miso = blackjackGame.getPlayers().getPlayers().getFirst();
-        miso.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
-        miso.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
-        miso.receiveCard(new Card(CardShape.DIAMOND, CardNumber.SEVEN));
-        Dealer dealer = blackjackGame.getDealer();
-        dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
-        dealer.receiveCard(new Card(CardShape.HEART, CardNumber.JACK));
-        dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.ACE));
-
-        // when
-        Map<Player, GameResult> playerGameResult = blackjackGame.calculatePlayerGameResult();
-
-        // then
-        for (Entry<Player, GameResult> entry : playerGameResult.entrySet()) {
-            assertThat(entry.getKey()).isEqualTo(miso);
-            assertThat(entry.getValue()).isEqualTo(GameResult.LOSE);
-        }
-    }
-
-    @Test
-    @DisplayName("Players 게임 결과를 반환한다. (무)")
-    void test10() {
-        // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Player miso = blackjackGame.getPlayers().getPlayers().getFirst();
-        miso.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
-        miso.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
-        miso.receiveCard(new Card(CardShape.DIAMOND, CardNumber.SEVEN));
-        Dealer dealer = blackjackGame.getDealer();
-        dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
-        dealer.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
-        dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.SEVEN));
-
-        // when
-        Map<Player, GameResult> playerGameResult = blackjackGame.calculatePlayerGameResult();
-
-        // then
-        for (Entry<Player, GameResult> entry : playerGameResult.entrySet()) {
-            assertThat(entry.getKey()).isEqualTo(miso);
-            assertThat(entry.getValue()).isEqualTo(GameResult.DRAW);
-        }
-    }
-
-    @Test
-    @DisplayName("Dealer 게임 결과를 반환한다. (승)")
     void test11() {
         // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Player miso = blackjackGame.getPlayers().getPlayers().getFirst();
-        miso.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
-        miso.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
-        miso.receiveCard(new Card(CardShape.DIAMOND, CardNumber.SEVEN));
-        Dealer dealer = blackjackGame.getDealer();
-        dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
-        dealer.receiveCard(new Card(CardShape.HEART, CardNumber.JACK));
-        dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.ACE));
-        Map<Player, GameResult> playerGameResult = blackjackGame.calculatePlayerGameResult();
-
-        // when
-        Map<GameResult, Integer> dealerGameResult = blackjackGame.calculateDealerGameResult(playerGameResult);
-
-        // then
-        assertThat(dealerGameResult.get(GameResult.WIN)).isEqualTo(1);
-        assertThat(dealerGameResult.get(GameResult.LOSE)).isEqualTo(0);
-        assertThat(dealerGameResult.get(GameResult.DRAW)).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("Dealer 게임 결과를 반환한다. (패)")
-    void test12() {
-        // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Player miso = blackjackGame.getPlayers().getPlayers().getFirst();
-        miso.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
-        miso.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
-        miso.receiveCard(new Card(CardShape.DIAMOND, CardNumber.SEVEN));
-        Dealer dealer = blackjackGame.getDealer();
+        miso.receiveCard(new Card(CardShape.SPADE, CardNumber.THREE));
+        miso.receiveCard(new Card(CardShape.HEART, CardNumber.THREE));
         dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.ACE));
         dealer.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
         dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.ACE));
-        Map<Player, GameResult> playerGameResult = blackjackGame.calculatePlayerGameResult();
 
         // when
-        Map<GameResult, Integer> dealerGameResult = blackjackGame.calculateDealerGameResult(playerGameResult);
+        Map<Player, Double> playersGameResults = blackjackGame.calculatePlayersGameResults(players);
+        Double result = playersGameResults.get(miso);
 
         // then
-        assertThat(dealerGameResult.get(GameResult.WIN)).isEqualTo(0);
-        assertThat(dealerGameResult.get(GameResult.LOSE)).isEqualTo(1);
-        assertThat(dealerGameResult.get(GameResult.DRAW)).isEqualTo(0);
+        assertThat(result)
+                .isEqualTo(misoBettingMoney * GameResult.LOSE.getRate());
     }
 
     @Test
     @DisplayName("Players 게임 결과를 반환한다. (무)")
-    void test13() {
+    void test12() {
         // given
-        List<String> playerNames = List.of("미소");
-        BlackjackGame blackjackGame = new BlackjackGame(playerNames);
-        Player miso = blackjackGame.getPlayers().getPlayers().getFirst();
-        miso.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
+        miso.receiveCard(new Card(CardShape.SPADE, CardNumber.ACE));
         miso.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
-        miso.receiveCard(new Card(CardShape.DIAMOND, CardNumber.SEVEN));
-        Dealer dealer = blackjackGame.getDealer();
-        dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
+        miso.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
+        dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.ACE));
         dealer.receiveCard(new Card(CardShape.HEART, CardNumber.ACE));
-        dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.SEVEN));
-        Map<Player, GameResult> playerGameResult = blackjackGame.calculatePlayerGameResult();
+        dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.ACE));
+        blackjackGame.updatePlayerMoney(dealer, miso);
 
         // when
-        Map<GameResult, Integer> dealerGameResult = blackjackGame.calculateDealerGameResult(playerGameResult);
+        Map<Player, Double> playersGameResults = blackjackGame.calculatePlayersGameResults(players);
+        Double result = playersGameResults.get(miso);
 
         // then
-        assertThat(dealerGameResult.get(GameResult.WIN)).isEqualTo(0);
-        assertThat(dealerGameResult.get(GameResult.LOSE)).isEqualTo(0);
-        assertThat(dealerGameResult.get(GameResult.DRAW)).isEqualTo(1);
+        assertThat(result)
+                .isEqualTo(misoBettingMoney * GameResult.DRAW.getRate());
+    }
+
+    @Test
+    @DisplayName("Dealer 게임 결과를 반환한다.")
+    void test13() {
+        // given
+        dealer.receiveCard(new Card(CardShape.SPADE, CardNumber.THREE));
+        dealer.receiveCard(new Card(CardShape.HEART, CardNumber.THREE));
+        dealer.receiveCard(new Card(CardShape.DIAMOND, CardNumber.THREE));
+        miso.receiveCard(new Card(CardShape.SPADE, CardNumber.THREE));
+        miso.receiveCard(new Card(CardShape.HEART, CardNumber.FIVE));
+        miso.receiveCard(new Card(CardShape.HEART, CardNumber.TEN));
+        yulmu.receiveCard(new Card(CardShape.SPADE, CardNumber.KING));
+        yulmu.receiveCard(new Card(CardShape.HEART, CardNumber.TEN));
+        blackjackGame.updatePlayerMoney(dealer, miso);
+        blackjackGame.updatePlayerMoney(dealer, yulmu);
+
+        // when
+        double dealerGameResults = blackjackGame.calculateDealerGameResults(players);
+
+        // then
+        assertThat(dealerGameResults)
+                .isEqualTo((misoBettingMoney * GameResult.WIN.getRate() +
+                        yulmuBettingMoney * GameResult.WIN.getRate()) * -1);
     }
 }
