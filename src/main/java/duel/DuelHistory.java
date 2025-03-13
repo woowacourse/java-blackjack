1d73730a229a39e691f3e8a4bb0e203d8fe23fb4
changuii <rhljh7410@gmail.com>
2025-03-13 16:32:40 +0900

    green(DuelHistory): 무승부를 추가하라

--- src/main/java/duel/DuelHistory.java
+++ src/main/java/duel/DuelHistory.java
@@ -1,36 +1,42 @@
 package duel;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import card.Count;
 
 public class DuelHistory {
-	private Count winCount;
-	private Count loseCount;
+	private final Map<DuelResult, Count> count;
 
 	public DuelHistory() {
-		winCount = Count.from(0);
-		loseCount = Count.from(0);
+		count = initDuelHistory();
+	}
+
+	private HashMap<DuelResult, Count> initDuelHistory() {
+		final HashMap<DuelResult, Count> count = new HashMap<>();
+		count.put(DuelResult.WIN, Count.from(0));
+		count.put(DuelResult.DRAW, Count.from(0));
+		count.put(DuelResult.LOSE, Count.from(0));
+		return count;
 	}
 
 	public void write(final DuelResult duelResult) {
-		switch (duelResult) {
-			case WIN -> winCount = winCount.increment();
-			case LOSE -> loseCount = loseCount.increment();
-		}
+		count.put(duelResult, count.get(duelResult).increment());
 	}
 
 	public boolean isWin() {
-		return winCount.isGreaterThan(loseCount);
+		return count.get(DuelResult.WIN).isGreaterThan(count.get(DuelResult.LOSE));
 	}
 
 	public Count getWinCount() {
-		return winCount;
+		return count.get(DuelResult.WIN);
 	}
 
 	public Count getLoseCount() {
-		return loseCount;
+		return count.get(DuelResult.LOSE);
 	}
 
 	public Count getDrawCount() {
-		return null;
+		return count.get(DuelResult.DRAW);
 	}
 }
