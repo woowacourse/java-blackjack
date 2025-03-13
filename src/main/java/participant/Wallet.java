ff0c5780288a489e3eaf193fd360b6ed86f6b2a5
soeun2537 <soeun2537@gmail.com>
2025-03-13 16:31:37 +0900

    feat: 수익을 반환하는 단위 테스트 추가

---
+++ src/main/java/participant/Wallet.java
@@ -0,0 +1,24 @@
+package participant;
+
+public class Wallet {
+
+    private double earnedMoney;
+    private final int bettingMoney;
+
+    private Wallet(double earnedMoney, int bettingMoney) {
+        this.earnedMoney = earnedMoney;
+        this.bettingMoney = bettingMoney;
+    }
+
+    public static Wallet of(int bettingMoney) {
+        return new Wallet(0, bettingMoney);
+    }
+
+    public void updateMoney(double rate) {
+        earnedMoney = bettingMoney + (bettingMoney * rate);
+    }
+
+    public double calculateProfit() {
+        return earnedMoney - bettingMoney;
+    }
+}
