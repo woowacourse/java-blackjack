64122d767c5fffa2f88465d1ca543fc7e0a6a86d
jhpark1227 <junhyeok0246@naver.com>
2025-03-13 23:07:29 +0900

    🔴red: GameResult 단위테스트 작성 (승패에 따라 수익을 계산한다)

--- src/main/java/domain/GameResult.java
+++ src/main/java/domain/GameResult.java
@@ -1,15 +1,29 @@
 package domain;
 
+import domain.participant.Money;
+
 public enum GameResult {
-    WIN,
-    LOSE,
-    DRAW;
+
+    BLACKJACK(1.5),
+    WIN(1),
+    LOSE(-1),
+    DRAW(0);
+
+    private final double winningRate;
+
+    GameResult(double winningRate) {
+        this.winningRate = winningRate;
+    }
 
     public GameResult getReverse() {
         return switch (this) {
             case LOSE -> WIN;
-            case WIN -> LOSE;
+            case WIN, BLACKJACK -> LOSE;
             case DRAW -> DRAW;
         };
+    }
+
+    public Money applyWinningRate(Money money) {
+        return null;
     }
 }
---
+++ src/test/java/domain/GameResultTest.java
@@ -0,0 +1,53 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.participant.Money;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameResultTest {
+
+    @DisplayName("블랙잭으로 이긴 경우 수익은 배팅금의 1.5배이다")
+    @Test
+    void test1() {
+        // given
+        Money money = Money.of(100000);
+        // when
+        Money expected = GameResult.BLACKJACK.applyWinningRate(money);
+        // then
+        assertThat(expected).isEqualTo(Money.of(150000));
+    }
+
+    @Test
+    void 숫자로_이긴_경우_수익은_배팅금의_1배이다() {
+        // given
+        Money money = Money.of(100000);
+        // when
+        Money expected = GameResult.WIN.applyWinningRate(money);
+        // then
+        assertThat(expected).isEqualTo(Money.of(100000));
+    }
+
+    @Test
+    void 비긴_경우_수익은_배팅금의_0배이다() {
+        // given
+        Money money = Money.of(100000);
+        // when
+        Money expected = GameResult.DRAW.applyWinningRate(money);
+        // then
+        assertThat(expected).isEqualTo(Money.of(0));
+    }
+
+    @DisplayName("비긴 경우 수깅은 배팅금의 -1배이다")
+    @Test
+    void test2() {
+        // given
+        Money money = Money.of(100000);
+        // when
+        Money expected = GameResult.LOSE.applyWinningRate(money);
+        // then
+        assertThat(expected).isEqualTo(Money.of(-100000));
+    }
+
+}
