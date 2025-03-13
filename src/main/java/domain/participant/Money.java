2bd5dab8a40b2216aec2029ef0a4242c41c934c3
jhpark1227 <junhyeok0246@naver.com>
2025-03-13 13:43:55 +0900

    refactor: 메서드 분리

--- src/main/java/domain/participant/Money.java
+++ src/main/java/domain/participant/Money.java
@@ -5,15 +5,12 @@ public class Money {
     private final int value;
 
     public Money(String rawValue) {
-        if (rawValue == null) {
-            throw new IllegalArgumentException("입력이 공백이거나 null 입니다.");
-        }
-        validateBlank(rawValue);
+        validateNullOrBlank(rawValue);
         this.value = Integer.parseInt(rawValue);
     }
 
-    private void validateBlank(String rawValue) {
-        if (rawValue.isBlank()) {
+    private void validateNullOrBlank(String rawValue) {
+        if (rawValue == null || rawValue.isBlank()) {
             throw new IllegalArgumentException("입력이 공백이거나 null 입니다.");
         }
     }
