cd3a83a5c5bfc445b62cd7d9522cae850b52a41b
Jin409 <sualng123@gmail.com>
2025-03-13 09:34:44 +0900

    test: Participants 테스트 추가

--- src/test/java/blackjack/domain/ParticipantsTest.java
+++ src/test/java/blackjack/domain/ParticipantsTest.java
@@ -1,6 +1,7 @@
 package blackjack.domain;
 
 import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +23,42 @@ class ParticipantsTest {
         // when & then
         assertThatThrownBy(() -> new Participants(participants))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 참가자들의_이름을_가져온다() {
+        // given
+        List<String> names = List.of("히로", "히포");
+        List<Participant> participantsToBeSaved = new java.util.ArrayList<>(names.stream()
+                .map(name -> (Participant) new Player(name, new Hand()))
+                .toList());
+
+        participantsToBeSaved.add(new Dealer(new Hand()));
+        Participants participants = new Participants(participantsToBeSaved);
+
+        // when
+        List<String> namesOfParticipants = participants.getNamesOfParticipants();
+
+        // then
+        assertThat(namesOfParticipants).containsAll(names);
+    }
+
+    @Test
+    void defender를_찾는다() {
+        // given
+        List<String> names = List.of("히로", "히포");
+        List<Participant> participantsToBeSaved = new java.util.ArrayList<>(names.stream()
+                .map(name -> (Participant) new Player(name, new Hand()))
+                .toList());
+
+        Dealer dealer = new Dealer(new Hand());
+        participantsToBeSaved.add(dealer);
+        Participants participants = new Participants(participantsToBeSaved);
+
+        // when
+        Participant actual = participants.findDefender();
+
+        // then
+        assertThat(actual).isEqualTo(dealer);
     }
 }
