87110c53455ac6ae63411457f0709434604ddb3f
changuii <rhljh7410@gmail.com>
2025-03-13 23:51:53 +0900

    green(WagerResultCalculator): 플레이어가 Bust이고, 1회 패배라면, 모든 돈을 잃어라

--- src/main/java/money/WagerResultCalculator.java
+++ src/main/java/money/WagerResultCalculator.java
@@ -1,9 +1,15 @@
 package money;
 
+import duel.DuelResult;
 import paticipant.Player;
 
 public class WagerResultCalculator {
+	private static final double WAGER_LOSE = 0;
+
 	public Money calculate(final Player player, final Money wager) {
+		if (player.isBust() && player.calculateDuelResult() == DuelResult.LOSE) {
+			return wager.multiply(WAGER_LOSE);
+		}
 		return null;
 	}
 }
