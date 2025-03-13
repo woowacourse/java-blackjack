564b9d3f45f86482e65fe5ed90a7f7f1215361a6
(minjae8563) <(minjae8563@naver.com)>
2025-03-13 17:08:22 +0900

    feat: 52 장의 카드에 대한 클래스 구현

---
+++ src/main/java/domain/card/Deck.java
@@ -0,0 +1,17 @@
+package domain.card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+    private final List<Card> cards;
+
+    public Deck(final List<Card> cards) {
+        this.cards = new ArrayList<>(cards);
+    }
+
+    public Card hitCard() {
+        return cards.removeFirst();
+    }
+
+}
