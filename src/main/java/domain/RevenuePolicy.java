8ca04a3d0650c57499fae0986be77df02bf3b02b
minSsan <msp1125@hanyang.ac.kr>
2025-03-13 20:53:51 +0900

    🛠refactor: 모든 상태에 대한 정책 반환 테스트 통과 코드 리팩터링

--- src/main/java/domain/RevenuePolicy.java
+++ src/main/java/domain/RevenuePolicy.java
@@ -1,41 +1,31 @@
 package domain;
 
+import java.util.Arrays;
 import java.util.function.Function;
 
 public enum RevenuePolicy {
-    BLACKJACK_WIN(cost -> (int) (cost * 1.5)),
-    NORMAL_WIN(cost -> cost),
-    NORMAL_DRAW(cost -> cost),
-    BLACKJACK_DRAW(cost -> cost),
-    LOSE(cost -> -1 * cost),
+    BLACKJACK_WIN(GameResult.WIN, true, cost -> (int) (cost * 1.5)),
+    NORMAL_WIN(GameResult.WIN, false, cost -> cost),
+    BLACKJACK_DRAW(GameResult.DRAW, true, cost -> cost),
+    NORMAL_DRAW(GameResult.DRAW, false, cost -> cost),
+    LOSE(GameResult.LOSE, false, cost -> -1 * cost),
     ;
 
+    private final GameResult gameResult;
+    private final boolean isPlayerBlackjack;
     private final Function<Integer, Integer> function;
 
-    RevenuePolicy(Function<Integer, Integer> function) {
+    RevenuePolicy(GameResult gameResult, boolean isPlayerBlackjack, Function<Integer, Integer> function) {
+        this.gameResult = gameResult;
+        this.isPlayerBlackjack = isPlayerBlackjack;
         this.function = function;
     }
 
     public static RevenuePolicy from(GameResult gameResult, final boolean isPlayerBlackjack) {
-        if (gameResult == GameResult.WIN && !isPlayerBlackjack) {
-            return NORMAL_WIN;
-        }
-        if (gameResult == GameResult.WIN) {
-            return BLACKJACK_WIN;
-        }
-        if (gameResult == GameResult.DRAW && isPlayerBlackjack) {
-            return BLACKJACK_DRAW;
-        }
-        if (gameResult == GameResult.DRAW) {
-            return NORMAL_DRAW;
-        }
-        if (gameResult == GameResult.LOSE && isPlayerBlackjack) {
-            throw new IllegalArgumentException("플레이어가 블랙잭인 경우에는 패배할 수 없습니다.");
-        }
-        if (gameResult == GameResult.LOSE) {
-            return LOSE;
-        }
-        return null;
+        return Arrays.stream(values())
+                .filter(policy -> policy.gameResult == gameResult && policy.isPlayerBlackjack == isPlayerBlackjack)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("플레이어가 블랙잭인 경우에는 패배할 수 없습니다."));
     }
 
     public int apply(final int bettingCost) {
