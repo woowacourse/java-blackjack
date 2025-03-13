6b393a5a47112118811c2d22a035874a8f0bd93f
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-03-13 17:30:23 +0900

    refactor: 예외 메세지 응집도를 위한 리팩토링

--- src/main/java/error/ErrorMessage.java
+++ src/main/java/error/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     INPUT_ONLY_Y_OR_N("입력은 y 혹은 n으로만 가능합니다."),
     ONLY_PERMIT_NUMBER("숫자만 입력 가능합니다."),
     OVER_MAX_PARTICIPANT("게임의 최대 참여자는 %d명을 넘을 수 없습니다."),
-    NOT_EXIST_DEALER("딜러가 존재하지 않습니다.");
+    NOT_EXIST_DEALER("딜러가 존재하지 않습니다."),
+    INVALID_PARTICIPANT_SIZE("게임의 최대 참여자는 %d명을 넘을 수 없습니다.");
 
     private final String message;
 
--- src/main/java/error/exception/InvalidParticipantSizeException.java
+++ src/main/java/error/exception/InvalidParticipantSizeException.java
@@ -1,10 +1,10 @@
 package error.exception;
 
+import static error.ErrorMessage.INVALID_PARTICIPANT_SIZE;
+
 public class InvalidParticipantSizeException extends RuntimeException {
 
-    private static final String message = "게임의 최대 참여자는 %d명을 넘을 수 없습니다.";
-
     public InvalidParticipantSizeException(int size) {
-        super(message.formatted(size));
+        super(INVALID_PARTICIPANT_SIZE.getMessage().formatted(size));
     }
 }
