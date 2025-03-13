91eb5eff0a05503f2c862e80d328699a6ad2846e
Dompoo <dlckdrms0517@gmail.com>
2025-03-13 11:51:44 +0900

    chore: 카드문양을 이모티콘으로 이쁘게 보이도록 수정

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,6 +1,8 @@
 package blackjack.view;
 
 import blackjack.domain.card.Card;
+import blackjack.domain.card.CardNumber;
+import blackjack.domain.card.CardShape;
 import blackjack.domain.card_hand.DealerBlackjackCardHand;
 import blackjack.domain.card_hand.PlayerBettingBlackjackCardHand;
 import blackjack.view.writer.Writer;
@@ -11,15 +13,15 @@ import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public class OutputView {
-
+    
     private static final String LINE_SEPARATOR = System.lineSeparator();
-
+    
     private final Writer writer;
-
+    
     public OutputView(final Writer writer) {
         this.writer = writer;
     }
-
+    
     public void outputInitialCards(final DealerBlackjackCardHand dealerHand, final List<PlayerBettingBlackjackCardHand> playerHands) {
         StringJoiner sj = new StringJoiner(LINE_SEPARATOR);
         sj.add(outputInitialCardOpeningMessage(extractPlayerNames(playerHands)));
@@ -29,7 +31,7 @@ public class OutputView {
         }
         writer.write(sj.toString());
     }
-
+    
     private String outputInitialCardOpeningMessage(List<String> names) {
         StringJoiner sj = new StringJoiner(", ");
         for (String name : names) {
@@ -37,17 +39,17 @@ public class OutputView {
         }
         return LINE_SEPARATOR + "딜러와 %s에게 2장을 나누었습니다.".formatted(sj.toString());
     }
-
+    
     private List<String> extractPlayerNames(final List<PlayerBettingBlackjackCardHand> playerHands) {
         return playerHands.stream()
                 .map(PlayerBettingBlackjackCardHand::getPlayerName)
                 .toList();
     }
-
+    
     private String parseNameAndCards(String name, List<Card> cards) {
         return "%s카드: %s".formatted(name, parseCards(cards));
     }
-
+    
     private String parseCards(List<Card> cards) {
         StringJoiner sj = new StringJoiner(", ");
         for (Card card : cards) {
@@ -55,41 +57,54 @@ public class OutputView {
         }
         return sj.toString();
     }
-
+    
     private String parseCard(Card card) {
-        return card.getNumber().name().replace("NUMBER_", "")
-                + card.getShape().name();
+        return parseCardNumber(card.getNumber())
+                + parseCardShape(card.getShape());
     }
-
+    
+    private static String parseCardShape(CardShape shape) {
+        return switch (shape) {
+            case 하트 -> "♥︎";
+            case 다이아몬드 -> "♦︎";
+            case 스페이드 -> "♠︎";
+            case 클로버 -> "♣︎";
+        };
+    }
+    
+    private static String parseCardNumber(CardNumber number) {
+        return number.name().replace("NUMBER_", "");
+    }
+    
     public void outputAddingMessage(String name) {
         final String message = LINE_SEPARATOR + "이제 %s가 카드를 더 받는 순서입니다.".formatted(name);
         writer.write(message);
     }
-
+    
     public void addTo21Warning() {
         writer.write("카드 총합이 21이기 때문에 더 받을 수 없습니다.");
     }
-
+    
     public void bustWarning() {
         writer.write("버스트이기 때문에 더 받을 수 없습니다.");
     }
-
+    
     public void outputCardsAndSum(PlayerBettingBlackjackCardHand playerHand) {
         final String message = parseCards(playerHand.getCards()) + parseSum(playerHand.getBlackjackSum());
         writer.write(message);
     }
-
+    
     private String parseSum(int sum) {
         return " - 합계: %d".formatted(sum);
     }
-
+    
     public void outputDealerAddedCards(int addedCardsSize) {
         if (addedCardsSize != 0) {
             String message = LINE_SEPARATOR + "딜러는 16이하라 %d장의 카드를 더 받았습니다.".formatted(addedCardsSize) + LINE_SEPARATOR;
             writer.write(message);
         }
     }
-
+    
     public void outputOpenCards(
             final DealerBlackjackCardHand dealerHand,
             final List<PlayerBettingBlackjackCardHand> playerHands
@@ -101,11 +116,11 @@ public class OutputView {
         }
         writer.write(sj.toString());
     }
-
+    
     private String parseFinalSum(int sum) {
         return " - 결과: %d".formatted(sum);
     }
-
+    
     public void outputFinalProfit(
             final DealerBlackjackCardHand dealerHand,
             final List<PlayerBettingBlackjackCardHand> playerHands
@@ -121,14 +136,14 @@ public class OutputView {
         }
         writer.write(sj.toString());
     }
-
+    
     private static double getTotalProfitOfDealer(final DealerBlackjackCardHand dealerHand, final List<PlayerBettingBlackjackCardHand> playerHands) {
         final Map<String, Double> profits = playerHands.stream()
                 .collect(Collectors.toMap(
                         PlayerBettingBlackjackCardHand::getPlayerName,
                         hand -> hand.calculateIncome(dealerHand)
                 ));
-
+        
         return -profits.values().stream().mapToDouble(Double::doubleValue).sum();
     }
 }
