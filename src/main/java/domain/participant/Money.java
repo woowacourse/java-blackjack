48b2cdffe60abbc04663d3a0855584190ed70fee
jhpark1227 <junhyeok0246@naver.com>
2025-03-13 16:25:04 +0900

    chore: Money equals, hashCode 재정의

--- src/main/java/domain/participant/Money.java
+++ src/main/java/domain/participant/Money.java
@@ -1,9 +1,11 @@
 package domain.participant;
 
+import java.util.Objects;
+
 public class Money {
 
     public static final int AMOUNT_VALUE = 100000;
-    
+
     private final int value;
 
     public Money(String rawValue) {
@@ -38,5 +40,22 @@ public class Money {
         if (Integer.parseInt(rawValue) % AMOUNT_VALUE != 0) {
             throw new IllegalArgumentException("입력은 10만원 단위입니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Money money = (Money) o;
+        return value == money.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
     }
 }
