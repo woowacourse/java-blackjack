e51b27253fc10c6b9d138ac531d972c448830214
soeun2537 <soeun2537@gmail.com>
2025-03-13 16:28:35 +0900

    test: 모든 플레이어의 수익을 합해서 반환하는 단위 테스트 추가

---
+++ src/test/java/participant/PlayersTest.java
@@ -0,0 +1,33 @@
+package participant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import game.GameResult;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayersTest {
+
+    @Test
+    @DisplayName("모든 플레이어의 수익을 합해서 반환한다.")
+    void test() {
+        // given
+        int player1BettingMoney = 10000;
+        int player2BettingMoney = 10000;
+        Player player1 = new Player("미소", player1BettingMoney);
+        Player player2 = new Player("율무", player2BettingMoney);
+        Players players = new Players(List.of(player1, player2));
+        GameResult player1GameResult = GameResult.WIN;
+        GameResult player2GameResult = GameResult.WIN;
+        player1.updateMoney(player1GameResult.getRate());
+        player2.updateMoney(player2GameResult.getRate());
+
+        // when
+        double result = players.sumProfits();
+
+        // then
+        assertThat(result).isEqualTo(
+                player1BettingMoney * player1GameResult.getRate() + player2BettingMoney * player2GameResult.getRate());
+    }
+}
