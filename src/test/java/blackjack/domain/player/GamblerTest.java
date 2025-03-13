ab9edc932ecd487ab1f203c1f9c799ad6f3113c2
taek2222 <oot309@daum.net>
2025-03-14 00:43:46 +0900

    test: Gambler 테스트 구조 변경 적용

---
+++ src/test/java/blackjack/domain/player/GamblerTest.java
@@ -0,0 +1,30 @@
+package blackjack.domain.player;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardNumber;
+import blackjack.domain.card.CardShape;
+import blackjack.domain.card.Cards;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GamblerTest {
+
+    private static final Card ACE = new Card(CardNumber.ACE, CardShape.CLOVER);
+
+    @Test
+    @DisplayName("게임 참가자는 모든 카드를 공개한다")
+    void test() {
+        Gambler gambler = new Gambler("두리", 0);
+
+        Cards cards = new Cards(List.of(ACE, ACE));
+        gambler.addCards(cards);
+
+        List<Card> result = gambler.getOpenedCards();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
