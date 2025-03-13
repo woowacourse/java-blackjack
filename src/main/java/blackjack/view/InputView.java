39572c36e219176a77dfe518190ecd38c38034c5
MinJun Choi <mj043000@naver.com>
2025-03-13 13:31:19 +0900

    refactor: 에러 출력 기능 수정

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -11,14 +11,15 @@ public class InputView {
     private static final String INPUT_PLAYERS_NICKNAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     public static final String INPUT_BETTING_MONEY = "%s의 배팅 금액은?";
     private static final String INPUT_WANT_HIT = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String ERROR_HEAD = "[ERROR] ";
 
     public String readNames() {
         System.out.println(INPUT_PLAYERS_NICKNAMES);
         return scanner.nextLine();
     }
 
-    public void printErrorMessage(final Exception errorMessage) {
-        System.out.println(errorMessage);
+    public void printErrorMessage(final Exception exception) {
+        System.out.println(ERROR_HEAD + exception.getMessage());
     }
 
     public String readBettingMoney(final Player player) {
