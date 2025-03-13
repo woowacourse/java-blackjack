afb0b0de16bce6fc0acb68ebcf9bc26642e7a433
jhpark1227 <junhyeok0246@naver.com>
2025-03-13 13:41:22 +0900

    🔴red: Money 단위테스트 작성 (입력이 null이면 예외를 발생시킨다)

--- src/test/java/domain/participant/MoneyTest.java
+++ src/test/java/domain/participant/MoneyTest.java
@@ -2,6 +2,7 @@ package domain.participant;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -14,6 +15,14 @@ class MoneyTest {
     void 입력이_공백이면_예외를_발생시킨다(String input) {
         // when & then
         assertThatThrownBy(() -> new Money(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력이 공백이거나 null 입니다.");
+    }
+
+    @Test
+    void 입력이_null이면_예외를_발생시킨다() {
+        // when & then
+        assertThatThrownBy(() -> new Money(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("입력이 공백이거나 null 입니다.");
     }
