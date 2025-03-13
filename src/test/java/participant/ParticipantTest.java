183fbd82756ee94e81a9441b2a346ac9bc2d7d39
soeun2537 <soeun2537@gmail.com>
2025-03-13 16:17:33 +0900

    test: Blackjack인지 여부 검사하는 단위 테스트 추가

--- src/test/java/participant/ParticipantTest.java
+++ src/test/java/participant/ParticipantTest.java
@@ -10,11 +10,59 @@ import org.junit.jupiter.api.Test;
 class ParticipantTest {
 
     @Test
-    @DisplayName("21이 넘는 패를 가지면 Bust이다.")
+    @DisplayName("카드의 합이 21이고, 카드 개수가 2개라면 Blackjack이다.")
     void test1() {
         // given
         Participant participant = new Player("율무", 10000);
+        participant.receiveCard(new Card(CardShape.DIAMOND, CardNumber.ACE));
+        participant.receiveCard(new Card(CardShape.DIAMOND, CardNumber.TEN));
 
+        // when
+        boolean result = participant.isBlackjack();
+
+        // then
+        Assertions.assertThat(result)
+                .isTrue();
+    }
+
+    @Test
+    @DisplayName("카드의 합이 21이지만, 카드 개수가 2개 아니라면 Blackjack이 아니다.")
+    void test2() {
+        // given
+        Participant participant = new Player("율무", 10000);
+        participant.receiveCard(new Card(CardShape.DIAMOND, CardNumber.NINE));
+        participant.receiveCard(new Card(CardShape.DIAMOND, CardNumber.TEN));
+        participant.receiveCard(new Card(CardShape.DIAMOND, CardNumber.TWO));
+
+        // when
+        boolean result = participant.isBlackjack();
+
+        // then
+        Assertions.assertThat(result)
+                .isFalse();
+    }
+
+    @Test
+    @DisplayName("카드의 합이 21이 아니라면 Blackjack이 아니다.")
+    void test3() {
+        // given
+        Participant participant = new Player("율무", 10000);
+        participant.receiveCard(new Card(CardShape.DIAMOND, CardNumber.TEN));
+        participant.receiveCard(new Card(CardShape.DIAMOND, CardNumber.NINE));
+
+        // when
+        boolean result = participant.isBlackjack();
+
+        // then
+        Assertions.assertThat(result)
+                .isFalse();
+    }
+
+    @Test
+    @DisplayName("카드의 합이 21을 넘으면 Bust이다.")
+    void test4() {
+        // given
+        Participant participant = new Player("율무", 10000);
         participant.receiveCard(new Card(CardShape.DIAMOND, CardNumber.TEN));
         participant.receiveCard(new Card(CardShape.DIAMOND, CardNumber.NINE));
         participant.receiveCard(new Card(CardShape.DIAMOND, CardNumber.THREE));
@@ -29,7 +77,7 @@ class ParticipantTest {
 
     @Test
     @DisplayName("21이 넘지 않으면 Bust가 아니다.")
-    void test2() {
+    void test5() {
         // given
         Participant participant = new Player("율무", 10000);
 
@@ -47,7 +95,7 @@ class ParticipantTest {
 
     @Test
     @DisplayName("참가자가 자신 패의 합을 구할 수 있다.")
-    void test3() {
+    void test6() {
         // given
         Participant participant = new Player("율무", 10000);
 
