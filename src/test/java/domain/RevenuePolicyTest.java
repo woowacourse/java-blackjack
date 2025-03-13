5fa0a7ea1ea7f92a4b61a62f8fe53ddb017d5d29
minSsan <msp1125@hanyang.ac.kr>
2025-03-13 20:48:15 +0900

    🔴red: 모든 상태에 대한 정책 반환 테스트 작성

--- src/test/java/domain/RevenuePolicyTest.java
+++ src/test/java/domain/RevenuePolicyTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static domain.RevenuePolicy.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RevenuePolicyTest {
     @DisplayName("플레이어가 블랙잭이 아니면서 승리한 경우, NORMAL_WIN 정책을 반환할 수 있다.")
@@ -47,5 +48,46 @@ class RevenuePolicyTest {
 
         // then
         assertThat(policy).isSameAs(BLACKJACK_DRAW);
+    }
+
+    @DisplayName("플레이어가 블랙잭이 아니면서 비긴 경우, NORMAL_DRAW 정책을 반환할 수 있다.")
+    @Test
+    void drawWithoutBlackjack() {
+        // given
+        GameResult gameResult = GameResult.DRAW;
+        final boolean isBlackjack = false;
+
+        // when
+        RevenuePolicy policy = RevenuePolicy.from(gameResult, isBlackjack);
+
+        // then
+        assertThat(policy).isSameAs(NORMAL_DRAW);
+    }
+
+    @DisplayName("플레이어가 패배한 경우, LOSE 정책을 반환할 수 있다.")
+    @Test
+    void lose() {
+        // given
+        GameResult gameResult = GameResult.LOSE;
+        final boolean isBlackjack = false;
+
+        // when
+        RevenuePolicy policy = RevenuePolicy.from(gameResult, isBlackjack);
+
+        // then
+        assertThat(policy).isSameAs(BLACKJACK_DRAW);
+    }
+
+    @DisplayName("플레이어가 블랙잭이면서 패배한 경우는 존재하지 않는다 - 예외 발생")
+    @Test
+    void exception() {
+        // given
+        GameResult gameResult = GameResult.LOSE;
+        final boolean isBlackjack = true;
+
+        // when & then
+        assertThatThrownBy(() -> {
+            RevenuePolicy.from(gameResult, isBlackjack);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
