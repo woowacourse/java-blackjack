033348cb8217c94f4d27acf4e7c2ca6246aff7d7
jeyong <joon6093@naver.com>
2025-03-13 13:02:50 +0900

    feat: 배팅 금액에서 수익을 계산하는 기능 추가

--- src/test/java/domain/BettingMoneyTest.java
+++ src/test/java/domain/BettingMoneyTest.java
@@ -1,13 +1,33 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BettingMoneyTest {
+
+    @Nested
+    class ValidCases {
+
+        @DisplayName("배팅 금액은 0 이상이어야 한다.")
+        @Test
+        void calculateRevenueAmount() {
+            double multiple = 1.5;
+            int amount = 1000;
+            BettingMoney bettingMoney = new BettingMoney(amount);
+
+            // when
+            int revenueAmount = bettingMoney.calculateRevenueAmount(multiple);
+
+            // then
+            assertThat(revenueAmount).isEqualTo((int) (amount * multiple));
+        }
+    }
 
     @Nested
     class InvalidCases {
