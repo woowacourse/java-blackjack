dc955fbfd83bde95caf1d60a7d939075731e32ab
jhpark1227 <junhyeok0246@naver.com>
2025-03-13 13:39:10 +0900

    🟢green: 입력이 공백이면 예외를 발생시키는 로직 구현

--- src/main/java/domain/participant/Money.java
+++ src/main/java/domain/participant/Money.java
@@ -5,6 +5,9 @@ public class Money {
     private final int value;
 
     public Money(String rawValue) {
+        if (rawValue.isBlank()) {
+            throw new IllegalArgumentException("입력이 공백이거나 null 입니다.");
+        }
         this.value = Integer.parseInt(rawValue);
     }
 }
