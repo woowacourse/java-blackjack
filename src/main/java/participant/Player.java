c07d5b779b6ad671274e5da85604c4cac021bbf8
soeun2537 <soeun2537@gmail.com>
2025-03-13 16:12:27 +0900

    feat: 플레이어 수익 반환하는 로직 구현

--- src/main/java/participant/Player.java
+++ src/main/java/participant/Player.java
@@ -2,12 +2,25 @@ package participant;
 
 public class Player extends Participant {
 
-    public Player(String nickname, int betAmount) {
-        super(nickname, betAmount);
+    private static final int BLACKJACK_NUMBER = 21;
+
+    private final Wallet wallet;
+
+    public Player(String nickname, int bettingMoney) {
+        super(nickname);
+        this.wallet = Wallet.of(bettingMoney);
     }
 
     @Override
     public boolean canReceiveCard() {
-        return !isBust();
+        return score() < BLACKJACK_NUMBER;
+    }
+
+    public void updateMoney(double rate) {
+        wallet.updateMoney(rate);
+    }
+
+    public double calculateProfit() {
+        return wallet.calculateProfit();
     }
 }
