8bd915d1403a48b2bb55580d6731b0c14a4b703b
LJM\eueo8 <eueo8259@gmail.com>
2025-03-13 23:54:12 +0900

    refactor : PlayerChoice 객체를 서렌을 확인하는 boolean 으로 변경

--- src/main/java/model/turn/PlayerTurn.java
+++ src/main/java/model/turn/PlayerTurn.java
@@ -10,15 +10,16 @@ import view.OutputView;
 public class PlayerTurn extends Turn{
     private final Player player;
     private final Betting betting;
-    private PlayerChoice playerChoice;
+    private boolean isSurrender;
 
     public PlayerTurn(Player player, Betting betting) {
         super(player);
         this.player = player;
         this.betting = betting;
+        this.isSurrender = false;
     }
 
-    public void chooseAtOnePlayerTurn(Deck deck) {
+    public void chooseAtOnePlayerChoice(Deck deck) {
         OutputView.printHitResult(player);
         PlayerChoice playerChoice = InputView.readChoice(player);
         if (playerChoice.equals(PlayerChoice.HIT)){
@@ -26,20 +27,23 @@ public class PlayerTurn extends Turn{
         }
         if (playerChoice.equals(PlayerChoice.DOUBLE_DOWN)){
             int additionalBet = InputView.inputAdditionalBet();
-            processDoubleDown(playerTurn, additionalBet);
+            processDoubleDown(deck, additionalBet);
+        }
+        if (playerChoice.equals(PlayerChoice.SURRENDER)){
+
         }
     }
 
     private void processHit(Deck deck){
         player.receiveCard(deck.pick());
         if (!player.isBust()){
-            chooseAtOnePlayerTurn(deck);
+            chooseAtOnePlayerChoice(deck);
         }
     }
 
     private void processDoubleDown(Deck deck, int additionalBet) {
         player.receiveCard(deck.pick());
-        player.runDoubleDownBet(additionalBet);
+        betting.addBet(additionalBet);
     }
 
     public Player getPlayer() {
