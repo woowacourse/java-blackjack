aa106a628d1c31edb70fe3326e8c54cf81847ff0
Jin409 <sualng123@gmail.com>
2025-03-14 09:43:09 +0900

    refactor: 사용되지 않는 필드 제거

--- src/main/java/blackjack/domain/card/CardRank.java
+++ src/main/java/blackjack/domain/card/CardRank.java
@@ -4,23 +4,17 @@ import java.util.List;
 
 public enum CardRank {
 
-    TWO(List.of(2), "2"), THREE(List.of(3), "3"), FOUR(List.of(4), "4"), FIVE(List.of(5), "5"), SIX(List.of(6), "6"),
-    SEVEN(List.of(7), "7"), EIGHT(List.of(8), "8"), NINE(List.of(9), "9"), TEN(List.of(10), "10"),
-    JACK(List.of(10), "J"), QUEEN(List.of(10), "Q"), KING(List.of(10), "K"), ACE(List.of(1, 11), "A");
+    TWO(List.of(2)), THREE(List.of(3)), FOUR(List.of(4)), FIVE(List.of(5)), SIX(List.of(6)),
+    SEVEN(List.of(7)), EIGHT(List.of(8)), NINE(List.of(9)), TEN(List.of(10)),
+    JACK(List.of(10)), QUEEN(List.of(10)), KING(List.of(10)), ACE(List.of(1, 11));
 
     private final List<Integer> values;
-    private final String description;
 
-    CardRank(List<Integer> values, String description) {
+    CardRank(List<Integer> values) {
         this.values = values;
-        this.description = description;
     }
 
     public List<Integer> getValues() {
         return values;
-    }
-
-    public String getDescription() {
-        return description;
     }
 }
