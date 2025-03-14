5b77a3e73dccb825f8667137d0f9ac153ca70efa
BWY <quantumjump10@gmail.com>
2025-03-14 22:47:27 +0900

    feat: 최종 수익 출력 기능 추가

--- src/main/java/io/ConsoleOutput.java
+++ src/main/java/io/ConsoleOutput.java
@@ -1,5 +1,6 @@
 package io;
 
+import bet.BettingCenter;
 import game.Card;
 import participant.Dealer;
 import participant.Player;
@@ -27,11 +28,11 @@ public class ConsoleOutput {
     }
 
     public void printDealerHitMessage() {
-        System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+        System.out.println("\n딜러는 16이하라 한장의 카드를 더 받았습니다.");
     }
 
     public void printGameResults(Players players, Dealer dealer) {
-        System.out.println("딜러카드: " + processCardsInfo(dealer.openCards()) + " - 결과: " + dealer.sumCardNumbers());
+        System.out.println("\n딜러카드: " + processCardsInfo(dealer.openCards()) + " - 결과: " + dealer.sumCardNumbers());
         for (Player player : players.getPlayers()) {
             System.out.println(player.getNickname() + "카드: " + processCardsInfo(player.openCards()) + " - 결과: "
                     + player.sumCardNumbers());
@@ -65,7 +66,7 @@ public class ConsoleOutput {
 
     private void printPlayerResults(Map<Player, WinningResult> playerResults) {
         for (Entry<Player, WinningResult> entry : playerResults.entrySet()) {
-            System.out.println(entry.getKey().getNickname() + ": " + entry.getValue().getMessage());
+            System.out.println(entry.getKey().getNickname() + ": " + entry.getValue().getProfitRate());
         }
     }
 
@@ -98,5 +99,14 @@ public class ConsoleOutput {
 
     private String processCardInfo(Card card) {
         return card.getNumber().getName() + card.getEmblem().getName();
+    }
+
+    public void printFinalProfit(BettingCenter bettingCenter, Dealer dealer) {
+        System.out.println("\n## 최종 수익");
+        System.out.println("딜러: " + bettingCenter.calculateDealerProfit(dealer));
+        Map<Player, Integer> deriveBetResults = bettingCenter.deriveBettingResults(dealer);
+        for (Player player : deriveBetResults.keySet()) {
+            System.out.println(player.getNickname() + ": " + deriveBetResults.get(player));
+        }
     }
 }
