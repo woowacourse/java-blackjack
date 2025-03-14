389727a33ec8ed7e6e561eb84a8a5272520d75f7
(minjae8563) <(minjae8563@naver.com)>
2025-03-14 13:33:42 +0900

    feat: 돈 만원 단위를 검사하는 메서드 구현

--- src/main/java/domain/participant/Money.java
+++ src/main/java/domain/participant/Money.java
@@ -4,6 +4,13 @@ public class Money {
     private final int money;
 
     public Money(int money) {
+        isDivisibleByTenThousand(money);
         this.money = money;
+    }
+
+    private void isDivisibleByTenThousand(int money) {
+        if (money % 10000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 배팅 금액은 만원 단위입니다.");
+        }
     }
 }
