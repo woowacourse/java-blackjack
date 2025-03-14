02490151cd7045816b9eb5003c72d8d6a58f8412
LJM\eueo8 <eueo8259@gmail.com>
2025-03-14 10:45:06 +0900

    feat : 보험 베팅 로직 구현

--- src/main/java/model/Betting.java
+++ src/main/java/model/Betting.java
@@ -11,4 +11,20 @@ public class Betting {
     public void addBet(int bet) {
         this.bet += bet;
     }
+
+    public int calculateMaxInsuranceAmount() {
+        return bet/2;
+    }
+
+    public void takeInsurance(int insuranceBet) {
+        validateInsuranceBet(insuranceBet);
+        this.bet -= insuranceBet;
+        this.insuranceBet = insuranceBet;
+    }
+
+    private void validateInsuranceBet(int insuranceBet){
+        if (insuranceBet < 0 || insuranceBet > calculateMaxInsuranceAmount()){
+            throw new IllegalArgumentException("[ERROR] 설정가능한 보험금액이 아닙니다. 입력값 : " + insuranceBet);
+        }
+    }
 }
