9e2a7ee966a4f8113bd23d091704cdaa06f4a0e7
(minjae8563) <(minjae8563@naver.com)>
2025-03-14 13:32:08 +0900

    test: 돈 만원 단위 테스트 추가

--- src/test/java/domain/participant/MoneyTest.java
+++ src/test/java/domain/participant/MoneyTest.java
@@ -1,5 +1,6 @@
 package domain.participant;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -11,5 +12,14 @@ class MoneyTest {
     void moneyTest() {
         assertDoesNotThrow(() -> new Money(10000));
     }
+
+    @Test
+    @DisplayName("돈 단위 테스트")
+    void isDivisibleByTenThousandTest() {
+        assertThatThrownBy(() -> new Money(10001))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 배팅 금액은 만원 단위입니다.");
+    }
+
 
 }
