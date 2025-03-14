ec2e9d00477c75e61444517991709d09e098f507
minSsan <msp1125@hanyang.ac.kr>
2025-03-14 16:57:13 +0900

    🟢green: 닉네임은 빈 문자열로 설정할 수 없다 테스트 통과 로직 작성

--- src/main/java/domain/Nickname.java
+++ src/main/java/domain/Nickname.java
@@ -1,4 +1,9 @@
 package domain;
 
 public record Nickname(String value) {
+    public Nickname {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("닉네임은 빈 값으로 설정할 수 없습니다.");
+        }
+    }
 }
--- src/test/java/domain/NicknameTest.java
+++ src/test/java/domain/NicknameTest.java
@@ -1,16 +1,17 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NicknameTest {
     @DisplayName("닉네임은 빈 문자열로 설정할 수 없다.")
-    @Test
-    void couldNotEmptyValue() {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  "})
+    void couldNotEmptyValue(String value) {
         // given
-        String value = "";
 
         // when & then
         assertThatThrownBy(() -> {
