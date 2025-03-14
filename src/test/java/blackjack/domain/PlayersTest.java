39afb0d8e83fe7ec92f232e495dd8116cf444297
7i.w__k <tizmfns1218@Naver.com>
2025-03-14 23:49:58 +0900

    test: Players 검증 추가

--- src/test/java/blackjack/domain/PlayersTest.java
+++ src/test/java/blackjack/domain/PlayersTest.java
@@ -16,14 +16,14 @@ class PlayersTest {
         // given
         List<String> names = List.of("꾹이", "히로");
         List<Player> playersToBeSaved = names.stream()
-                .map(name -> new Player(name, new PlayerHand(new Hand(), Wallet.bet(1000)))).toList();
+                .map(name -> new Player(name, new PlayerHand(new Hand(), Wallet.bet(1000))))
+                .toList();
 
         // when
         Players players = Players.from(playersToBeSaved);
 
         // then
         assertThat(players.getPlayers()).hasSize(2);
-
     }
 
     @DisplayName("플레이어는 7명을 초과할 수 없다.")
@@ -32,10 +32,21 @@ class PlayersTest {
         // given
         List<String> names = List.of("듀이", "몽이", "히로", "꾹이", "히포", "비타", "라젤", "서프");
         List<Player> playersToBeSaved = names.stream()
-                .map(name -> new Player(name, new PlayerHand(new Hand(), Wallet.bet(1000)))).toList();
+                .map(name -> new Player(name, new PlayerHand(new Hand(), Wallet.bet(1000))))
+                .toList();
 
         // when & then
         assertThatThrownBy(() -> Players.from(playersToBeSaved)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.EXCEED_PLAYER_MEMBERS.getMessage());
+    }
+
+    @DisplayName("플레이어가 존재하지 않으면 안된다.")
+    @Test
+    void test3() {
+        List<Player> playersToBeSaved = List.of();
+
+        // when & then
+        assertThatThrownBy(() -> Players.from(playersToBeSaved)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NEED_PLAYER_MEMBERS.getMessage());
     }
 }
