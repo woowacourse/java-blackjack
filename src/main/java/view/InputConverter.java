42a95118ab58a7f94c5202ae06b2952ffdf1154d
민경윤 <ruddbs3308@naver.com>
2025-03-14 16:26:50 +0900

    feat(InputConverter): 숫자 문자열을 숫자로 변환한다

--- src/main/java/view/InputConverter.java
+++ src/main/java/view/InputConverter.java
@@ -10,6 +10,15 @@ public class InputConverter {
         return List.of(input.trim().split(delimiter));
     }
 
+    public static int parseToInt(final String input) {
+        validateNotNullOrBlankInput(input);
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalBlackjackInputException("입력이 숫자가 아닙니다.");
+        }
+    }
+
     public static void validateNotNullOrBlankInput(final String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalBlackjackInputException("잘못된 입력입니다. 다시 입력해주세요.");
