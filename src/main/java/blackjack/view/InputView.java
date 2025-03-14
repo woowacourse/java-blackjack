36c9ce19d4baa902ad159278300c6312285f2993
Yehyeok Bang <qkddpgur318@gmail.com>
2025-03-14 14:32:15 +0900

    refactor: 검증 로직 위치 변경
    - 열거형에서 변환합니다.

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -4,24 +4,27 @@ import blackjack.model.participant.Name;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class InputView {
+
+    private static final Pattern COMMA_WITH_OPTIONAL_SPACES_PATTERN = Pattern.compile("\\s*,\\s*");
 
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> readPlayerNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return splitWithComma(trim(scanner.nextLine()));
+        return splitWithComma(scanner.nextLine().trim());
     }
 
     private List<String> splitWithComma(String input) {
-        return Arrays.stream(input.split(",", -1))
+        return Arrays.stream(COMMA_WITH_OPTIONAL_SPACES_PATTERN.split(input, -1))
                 .toList();
     }
 
     public int readBetAmount(Name name) {
         System.out.printf("%n%s의 배팅 금액은?%n", name.value());
-        return parseInt(trim(scanner.nextLine()));
+        return parseInt(scanner.nextLine().trim());
     }
 
     private int parseInt(String input) {
@@ -34,20 +37,6 @@ public class InputView {
 
     public ParticipantAction readHitOrNot(Name playerName) {
         System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)%n", playerName.value());
-        return parseParticipantAction(trim(scanner.nextLine()));
-    }
-
-    private ParticipantAction parseParticipantAction(String input) {
-        if (input.equals("y")) {
-            return ParticipantAction.HIT;
-        }
-        if (input.equals("n")) {
-            return ParticipantAction.STAND;
-        }
-        throw new IllegalArgumentException("올바른 입력이 아닙니다. 입력: %s".formatted(input));
-    }
-
-    private String trim(String input) {
-        return input.replaceAll(" ", "");
+        return ParticipantAction.from(scanner.nextLine().trim());
     }
 }
--- src/main/java/blackjack/view/ParticipantAction.java
+++ src/main/java/blackjack/view/ParticipantAction.java
@@ -1,12 +1,26 @@
 package blackjack.view;
 
+import java.util.Arrays;
+
 public enum ParticipantAction {
 
-    HIT,
-    STAND,
-    ;
+    HIT("y"),
+    STAND("n");
+
+    private final String inputValue;
+
+    ParticipantAction(String inputValue) {
+        this.inputValue = inputValue;
+    }
 
     public boolean isHit() {
         return this == HIT;
+    }
+
+    public static ParticipantAction from(String input) {
+        return Arrays.stream(values())
+                .filter(action -> action.inputValue.equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 입력이 아닙니다. 입력: %s".formatted(input)));
     }
 }
