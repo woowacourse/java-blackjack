b31ae9b32c7e95d991841dc2aac9e75c20f1a687
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-14 23:42:15 +0900

    test : 베팅 테스트 코드 작성

---
+++ src/test/java/domain/participant/BettingTest.java
@@ -0,0 +1,20 @@
+package domain.participant;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BettingTest {
+    @DisplayName("베팅 금액을 정상적으로 지정한다")
+    @Test
+    void test1() {
+        //given
+        int money = 10000;
+
+        //when
+        Betting betting = new Betting(10000);
+
+        //then
+        Assertions.assertThat(betting).isEqualTo(new Betting(10000));
+    }
+}
