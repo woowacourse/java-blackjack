70f5536fd21d60c0de6b3608cb4c7c582401f3de
(minjae8563) <(minjae8563@naver.com)>
2025-03-14 11:32:05 +0900

    test: PlayersTest 에서 참가자 인원 검증 테스트 제거, Names 를 from 메서드에 파라미터로 주는 로직으로 변경

--- src/test/java/domain/participant/PlayersTest.java
+++ src/test/java/domain/participant/PlayersTest.java
@@ -35,9 +35,8 @@ public class PlayersTest {
     @DisplayName("카드 분배 테스트")
     void hitCardsTest(){
         //given
-        Players players = Players.from(new ArrayList<>(
-                List.of("pobi", "lisa")
-        ));
+        Names names = new Names(List.of("pobi", "lisa"));
+        Players players = Players.from(names);
 
         CardDeckFactory cardDeckFactory = new CardDeckFactory();
         Deck deck = cardDeckFactory.create();
@@ -53,29 +52,6 @@ public class PlayersTest {
 
     }
 
-    @ParameterizedTest
-    @DisplayName("플레이어 인원 검증 테스트")
-    @MethodSource("provideListForPlayers")
-    void validatePlayerNumbers(List<String> names) {
-        assertThatThrownBy(() -> Players.from(names))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 플레이어 인원은 1~6명 입니다.");
-    }
-
-    private static Stream<Arguments> provideListForPlayers(){
-        return Stream.of(Arguments.of(
-                List.of("포비", "이든", "네오", "저스틴", "링크", "말론", "리사"),
-                List.of()
-        ));
-    }
-
-    @Test
-    @DisplayName("중복된 이름 테스트")
-    void validateIsDuplicateTest() {
-        assertThatThrownBy(() -> Players.from(List.of("pobi", "pobi")))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 중복된 이름의 플레이어가 게임에 참여할 수 없습니다.");
-    }
     @Test
     @DisplayName("카드 드로우 테스트")
     void drawTest(){
@@ -91,18 +67,11 @@ public class PlayersTest {
         OutputView testOutputView = new OutputView();
 
         Deck deck = new Deck(List.of(new Card(DIAMOND, QUEEN), new Card(SPADE, JACK), new Card(HEART, KING)));
-        Dealer dealer = new Dealer(new Hand(new ArrayList<>()));
-        Players players = Players.from(List.of("pobi", "lisa"));
+        Names names = new Names(List.of("pobi", "lisa"));
+        Players players = Players.from(names);
 
         //when-then
         assertDoesNotThrow(() -> players.draw(testInputView::askPlayerForHitOrStand, testOutputView::printPlayerDeck, deck));
     }
 
-    @Test
-    @DisplayName("0명 플레이어 참가 테스트")
-    void validateZeroPlayerTest() {
-        assertThatThrownBy(() -> Players.from(Collections.emptyList()))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 플레이어 인원은 1~6명 입니다.");
-    }
 }
