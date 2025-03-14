9086160a79b6da9c026dbba243245fad370978bb
changuii <rhljh7410@gmail.com>
2025-03-14 18:16:23 +0900

    refactor: main에서 프로젝트의 흐름을 제어하라

--- src/main/java/BlackjackApp.java
+++ src/main/java/BlackjackApp.java
@@ -1,14 +1,28 @@
-import game.BlackjackManager;
+import game.Blackjack;
+import money.WagerMoney;
 import view.InputView;
 import view.OutputView;
 
 public class BlackjackApp {
 
 	public static void main(String[] args) {
-		InputView inputView = new InputView();
-		OutputView outputView = new OutputView();
-		BlackjackManager blackjackManager = new BlackjackManager(inputView, outputView);
+		final InputView inputView = new InputView();
+		final OutputView outputView = new OutputView();
+		final Blackjack blackjack = Blackjack.from(inputView.readPlayerNames());
+		final WagerMoney wagerMoney = new WagerMoney(inputView.readPlayersMoney(blackjack));
 
-		blackjackManager.run();
+		blackjack.initPickCard();
+		outputView.printHandOut(blackjack);
+
+		blackjack.pickCardPlayersIfNotBust(inputView::readPlayerAnswer);
+		blackjack.pickCardDealerIfNotMax();
+
+		outputView.printDealerPickCard(blackjack);
+		outputView.printDealerHandResult(blackjack);
+		outputView.printPlayerHandResult(blackjack);
+
+		blackjack.duel();
+
+		outputView.printWagerResult(wagerMoney);
 	}
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,54 +1,103 @@
 package view;
 
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import card.Card;
+import game.Blackjack;
 import money.Money;
+import money.WagerMoney;
 import paticipant.Player;
 
 public class OutputView {
 	private static final String PLAYER_NAME_DELIMITER = ", ";
 
-	public void printHandOutIntroduce(final List<String> playerNames) {
-		final String names = String.join(PLAYER_NAME_DELIMITER, playerNames);
-		System.out.printf("딜러와 %s에게 2장을 나누었습니다." + System.lineSeparator(), names);
+	public void printHandOut(final Blackjack blackjack) {
+		printHandOutIntroduce1(blackjack);
+		printDealerHandOutResult1(blackjack);
+		printPlayersCard1(blackjack);
 	}
 
-	public void printDealerHandOutResult(final String dealerCard) {
-		System.out.printf("딜러카드: %s" + System.lineSeparator(), dealerCard);
+	private void printHandOutIntroduce1(final Blackjack blackjack) {
+		final String playerNames = blackjack.getPlayers().getPlayers().stream()
+			.map(Player::getName)
+			.collect(Collectors.joining(PLAYER_NAME_DELIMITER));
+		System.out.printf("딜러와 %s에게 2장을 나누었습니다." + System.lineSeparator(), playerNames);
 	}
 
-	public void printPlayersCard(final Map<String, List<String>> cardsByPlayerName) {
-		cardsByPlayerName.forEach(this::printPlayerCards);
+	private void printDealerHandOutResult1(final Blackjack blackjack) {
+		final Card card = blackjack.getDealer().getParticipant().getCardHand().getCards().getFirst();
+
+		System.out.printf("딜러카드: %s" + System.lineSeparator(),
+			convertCardToString(card)
+		);
 	}
 
-	public void printPlayerCards(final String name, final List<String> card) {
-		final String cards = String.join(PLAYER_NAME_DELIMITER, card);
-		System.out.printf("%s카드: %s" + System.lineSeparator(), name, cards);
+	private void printPlayersCard1(final Blackjack blackjack) {
+		for (final Player player : blackjack.getPlayers().getPlayers()) {
+			final String cards = player.getParticipant().getCardHand().getCards().stream()
+				.map(this::convertCardToString)
+				.collect(Collectors.joining(PLAYER_NAME_DELIMITER));
+			System.out.printf("%s카드: %s" + System.lineSeparator(),
+				player.getName(),
+				String.join(PLAYER_NAME_DELIMITER, cards)
+			);
+		}
 	}
 
-	public void printDealerPickCard() {
-		System.out.println("딜러는 16이하라 한 장의 카드를 더 받았습니다.");
+	private String convertCardToString(final Card card) {
+		return RankMessage.convertRankToMessage(card.rank()) + card.suit().getName();
 	}
 
-	public void printDealerHandResult(final List<String> cards, final int score) {
-		final String joinedCards = String.join(PLAYER_NAME_DELIMITER, cards);
-		System.out.printf("딜러카드: %s - 결과: %d" + System.lineSeparator(), joinedCards, score);
+	public void printDealerPickCard(final Blackjack blackjack) {
+		final int pickCardCount = blackjack.getDealer().getParticipant().getCardHand().getCards().size()
+			- Blackjack.INIT_PICK_CARD_COUNT;
+		for (int i = 0; i < pickCardCount; i++) {
+			System.out.println("딜러는 16이하라 한 장의 카드를 더 받았습니다.");
+		}
 	}
 
-	public void printPlayerHandResult(final String name, final List<String> cards, final int score) {
-		final String joinedCards = String.join(PLAYER_NAME_DELIMITER, cards);
-		System.out.printf("%s카드: %s - 결과: %d" + System.lineSeparator(), name, joinedCards, score);
+	public void printDealerHandResult(final Blackjack blackjack) {
+		final String dealerCards = blackjack.getDealer().getParticipant().getCardHand().getCards().stream()
+			.map(this::convertCardToString)
+			.collect(Collectors.joining(PLAYER_NAME_DELIMITER));
+		System.out.printf("딜러카드: %s - 결과: %d" + System.lineSeparator(),
+			dealerCards,
+			blackjack.getDealer().getParticipant().calculateAllScore().value()
+		);
 	}
 
-	public void printWagerResult(final Map<Player, Money> playerWagerResult, final Money dealerWager) {
+	public void printPlayerHandResult(final Blackjack blackjack) {
+		for (final Player player : blackjack.getPlayers().getPlayers()) {
+			final String playerCards = player.getParticipant().getCardHand().getCards().stream()
+				.map(this::convertCardToString)
+				.collect(Collectors.joining(PLAYER_NAME_DELIMITER));
+			System.out.printf("%s카드: %s - 결과: %d" + System.lineSeparator(),
+				player.getName(),
+				playerCards,
+				player.getParticipant().calculateAllScore()
+			);
+		}
+	}
+
+	public void printWagerResult(final WagerMoney wagerMoney) {
+		final Map<Player, Money> playerWagerMoney = wagerMoney.calculateWagerResult();
 		System.out.println("## 최종 수익");
-		System.out.printf("딜러: %d" + System.lineSeparator(), dealerWager.getValue());
-		for (final Map.Entry<Player, Money> playerMoney : playerWagerResult.entrySet()) {
+		System.out.printf("딜러: %d" + System.lineSeparator(), calculateDealerWagerResult(playerWagerMoney));
+		for (final Map.Entry<Player, Money> playerMoney : playerWagerMoney.entrySet()) {
 			System.out.printf("%s: %d" + System.lineSeparator(),
 				playerMoney.getKey().getName(),
 				playerMoney.getValue().getValue()
 			);
 		}
 	}
+
+	private Money calculateDealerWagerResult(final Map<Player, Money> playerWagerResult) {
+		Money dealerMoney = new Money(0);
+		for (final Money playerMoney : playerWagerResult.values()) {
+			dealerMoney = dealerMoney.minus(playerMoney);
+		}
+		return dealerMoney;
+	}
+
 }
