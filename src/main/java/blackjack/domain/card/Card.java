336306f0d8bcabecf4054e7ed3caf20da5cdbd07
junho <wnsgh732@gmail.com>
2025-03-14 15:38:31 +0900

    chore: record 클래스에 재정의 메서드 삭제

--- src/main/java/blackjack/domain/card/Card.java
+++ src/main/java/blackjack/domain/card/Card.java
@@ -1,7 +1,5 @@
 package blackjack.domain.card;
 
-import java.util.Objects;
-
 public record Card(CardNumber number, CardShape shape) {
 
     public int getValue() {
@@ -10,18 +8,5 @@ public record Card(CardNumber number, CardShape shape) {
 
     public boolean isAce() {
         return number.equals(CardNumber.ACE);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Card card = (Card) o;
-        return shape == card.shape && number == card.number;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(number, shape);
     }
 }
--- src/main/java/blackjack/domain/player/PlayerName.java
+++ src/main/java/blackjack/domain/player/PlayerName.java
@@ -1,7 +1,5 @@
 package blackjack.domain.player;
 
-import java.util.Objects;
-
 public record PlayerName(String name) {
 
     public PlayerName {
@@ -12,12 +10,5 @@ public record PlayerName(String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("이름의 길이는 1이상입니다.");
         }
-    }
-
-    @Override
-    public boolean equals(final Object object) {
-        if (object == null || getClass() != object.getClass()) return false;
-        PlayerName playerName1 = (PlayerName) object;
-        return Objects.equals(name, playerName1.name);
     }
 }
