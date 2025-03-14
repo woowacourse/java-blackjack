1a91b928de742e6a6973436e70f7aa54c185e990
Han <jumdo12@gmail.com>
2025-03-14 09:48:18 +0900

    style(GamerGenerator): 매직넘버 상수화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private final int MAX_PLAYER = 7;
 
     public List<String> readPlayers() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
