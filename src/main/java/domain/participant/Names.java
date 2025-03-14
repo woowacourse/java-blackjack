79afc67f3f1e6b53e905b123f58dd3e9ae82e8e4
(minjae8563) <(minjae8563@naver.com)>
2025-03-14 14:08:38 +0900

    refactor: static 선언 제거

--- src/main/java/domain/participant/Names.java
+++ src/main/java/domain/participant/Names.java
@@ -15,13 +15,13 @@ public class Names {
                 .toList();
     }
 
-    private static void validatePlayerNumbers(final List<String> names) {
+    private void validatePlayerNumbers(final List<String> names) {
         if (names.isEmpty() || names.size() > MAXIMUM_PLAYER_NUMBER) {
             throw new IllegalArgumentException("[ERROR] 플레이어 인원은 1~6명 입니다.");
         }
     }
 
-    private static void validateIsDuplicate(final List<String> names) {
+    private void validateIsDuplicate(final List<String> names) {
         if (names.stream().distinct().count() != names.size()) {
             throw new IllegalArgumentException("[ERROR] 중복된 이름의 플레이어가 게임에 참여할 수 없습니다.");
         }
