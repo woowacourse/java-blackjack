24f377bacbdd1c55020d51cfa432219e34c12b00
songsunkook <songsunkook@gmail.com>
2025-03-14 14:05:00 +0900

    feat: 카드를 추가로 뽑아 21을 초과할 경우 베팅 금액을 모두 잃는다

--- src/main/java/blackjack/domain/GameRound.java
+++ src/main/java/blackjack/domain/GameRound.java
@@ -7,9 +7,22 @@ import blackjack.domain.gamer.Player;
 
 public class GameRound {
 
-    private final Map<Player, Integer> bettingRecord = new HashMap<>();
+    private final Map<Player, Integer> initialBettingMoney = new HashMap<>();
+    private final Map<Player, Integer> finalBettingMoney = new HashMap<>();
 
     public void betting(Player player, int money) {
-        bettingRecord.put(player, money);
+        initialBettingMoney.put(player, money);
+    }
+
+    public boolean loseIfBust(Player player) {
+        if (player.isBust()) {
+            finalBettingMoney.put(player, 0);
+            return true;
+        }
+        return false;
+    }
+
+    public int getFinalBettingMoney(Player player) {
+        return finalBettingMoney.get(player);
     }
 }
--- src/test/java/blackjack/BettingTest.java
+++ src/test/java/blackjack/BettingTest.java
@@ -1,12 +1,17 @@
 package blackjack;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import blackjack.domain.GameManager;
 import blackjack.domain.GameRound;
+import blackjack.domain.card.CardNumber;
 import blackjack.domain.gamer.Player;
+import blackjack.fixture.DeckFixture;
+import blackjack.fixture.GameManagerFixture;
 
 class BettingTest {
 
@@ -15,7 +20,25 @@ class BettingTest {
     void playerStartBettingTest() {
         Player player = new Player("pobi");
         GameRound round = new GameRound();
-        assertThatCode(() -> round.betting(player, 100))
+        assertThatCode(() -> round.betting(player, 1000))
             .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("카드를 추가로 뽑아 21을 초과할 경우 베팅 금액을 모두 잃는다.")
+    void additionalCardMoreThan21Test() {
+        int startMoney = 1000;
+
+        Player player = new Player("pobi");
+        GameRound round = new GameRound();
+        round.betting(player, startMoney);
+        GameManager gameManager = GameManagerFixture.GameManagerWith(
+            DeckFixture.deckOf(CardNumber.JACK, CardNumber.QUEEN, CardNumber.KING)
+        );
+        gameManager.drawStartingCards(player);
+        gameManager.drawCard(player);
+
+        assertThat(round.loseIfBust(player)).isTrue();
+        assertThat(round.getFinalBettingMoney(player)).isZero();
     }
 }
