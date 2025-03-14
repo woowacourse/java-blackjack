c97183182a79d20858c8fd2bdba907251a882141
민경윤 <ruddbs3308@naver.com>
2025-03-15 01:13:25 +0900

    feat(ParticipantsBet): 플레이어별 승패결과를 통해 플레이어별 최종 수익을 계산한다

--- src/main/java/model/bet/ParticipantsBet.java
+++ src/main/java/model/bet/ParticipantsBet.java
@@ -1,14 +1,32 @@
 package model.bet;
 
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import model.result.GameResult;
+import model.result.GameResults;
 
 public class ParticipantsBet {
 
-    private final int dealerBet;
-    private final Map<String, Integer> playerBet;
+    private final Map<String, Integer> playersBet;
 
-    public ParticipantsBet(final Map<String, Integer> playerBet) {
-        dealerBet = 0;
-        this.playerBet = playerBet;
+    public ParticipantsBet(final Map<String, Integer> playersBet) {
+        this.playersBet = playersBet;
+    }
+
+    public BettingResults calculateBettingResults(final GameResults gameResults) {
+        return new BettingResults(playersBet.keySet().stream()
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        name -> calculateBettingResultByName(name, gameResults)
+                ))
+        );
+    }
+
+    private int calculateBettingResultByName(final String name, final GameResults gameResults) {
+        GameResult gameResult =  gameResults.getGameResultByName(name);
+        BettingOdds bettingOdds = BettingOdds.from(gameResult);
+        return (int) bettingOdds.multiple(playersBet.get(name));
     }
 }
---
+++ src/test/java/model/bet/ParticipantsBetTest.java
@@ -0,0 +1,39 @@
+package model.bet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+import model.result.GameResult;
+import model.result.GameResults;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ParticipantsBetTest {
+
+    @DisplayName("GameResults를 통해 BettingResults를 계산한다.")
+    @Test
+    void calculateBettingResultsTest() {
+        ParticipantsBet participantsBet = new ParticipantsBet(Map.of(
+                "pobi", 10_000,
+                "jason", 20_000,
+                "danny", 5_000,
+                "hotteok", 50_000
+        ));
+        GameResults gameResults = new GameResults(Map.of(
+                "pobi", GameResult.WIN,
+                "jason", GameResult.BLACKJACK_WIN,
+                "danny", GameResult.DRAW,
+                "hotteok", GameResult.LOSE
+        ));
+
+        BettingResults bettingResults = participantsBet.calculateBettingResults(gameResults);
+
+        assertAll(
+                () -> assertThat(bettingResults.getBettingResultByName("pobi")).isEqualTo(10_000),
+                () -> assertThat(bettingResults.getBettingResultByName("jason")).isEqualTo(30_000),
+                () -> assertThat(bettingResults.getBettingResultByName("danny")).isEqualTo(0),
+                () -> assertThat(bettingResults.getBettingResultByName("hotteok")).isEqualTo(-50_000)
+        );
+    }
+}
