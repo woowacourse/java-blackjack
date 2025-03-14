6839ae336b9a1f2b4c23c93eae215e306527ab95
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-15 01:38:36 +0900

    test&feat : 블랙잭 승리시 수익 계산 테스트 코드 작성 및 기능 구현

--- src/test/java/domain/result/PlayerResultTest.java
+++ src/test/java/domain/result/PlayerResultTest.java
@@ -20,15 +20,9 @@ class PlayerResultTest {
             new Card(CardNumberType.JACK, CardType.CLOVER),
             new Card(CardNumberType.ACE, CardType.CLOVER)));
     Player blackjackPlayer = new Player("blackJack", blackJack, Money.bet(10000));
-    Player winner = new Player("winner", Hand.createEmpty(), Money.bet(10000));
+    Player winner = new Player("winner", Hand.createEmpty(), Money.bet(20000));
     Player drawer = new Player("drawer", Hand.createEmpty(), Money.bet(20000));
     Player loser = new Player("loser", Hand.createEmpty(), Money.bet(30000));
-
-//  - [ ] 플레이어는 블랙잭으로 승리할 경우 베팅 금액의 1.5배를 얻는다
-//  - [ ] 플레이어는 승리할 경우 베팅 금액을 얻는다
-//  - [ ] 플레이어는 무승부일 경우 베팅 금액을 돌려받는다
-//  - [ ] 플레이어는 패배할 경우 베팅 금액을 잃는다
-//  - [ ] 딜러의 수익은 모든 플레이어들의 수익 합의 반대 값이다
 
     @DisplayName("플레이어는 블랙잭으로 승리할 경우 베팅 금액의 1.5배를 얻는다")
     @Test
@@ -41,5 +35,44 @@ class PlayerResultTest {
 
         //then
         assertThat(profit).isEqualTo(new Money(15000));
+    }
+
+    @DisplayName("플레이어는 승리할 경우 베팅 금액을 얻는다")
+    @Test
+    void test11() {
+        //given
+        PlayerResult playerResult = new PlayerResult(winner, WIN);
+
+        //when
+        Money profit = playerResult.calculateProfit();
+
+        //then
+        assertThat(profit).isEqualTo(new Money(20000));
+    }
+
+    @DisplayName("플레이어는 무승부일 경우 베팅 금액을 그대로 돌려받아 수익이 없다")
+    @Test
+    void test12() {
+        //given
+        PlayerResult playerResult = new PlayerResult(drawer, DRAW);
+
+        //when
+        Money profit = playerResult.calculateProfit();
+
+        //then
+        assertThat(profit).isEqualTo(new Money(0));
+    }
+
+    @DisplayName("플레이어는 패배할 경우 베팅 금액을 잃는다")
+    @Test
+    void test13() {
+        //given
+        PlayerResult playerResult = new PlayerResult(loser, LOSE);
+
+        //when
+        Money profit = playerResult.calculateProfit();
+
+        //then
+        assertThat(profit).isEqualTo(new Money(-30000));
     }
 }
