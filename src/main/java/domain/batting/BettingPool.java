60981998fc4e9e11e971a9ba0b0551ff8fa02f99
DongchannN <ldc991104@gmail.com>
2025-03-14 10:54:24 +0900

    feat: 플레이어에 대한 배팅 금액을 관리하는 모델 구현

--- src/main/java/domain/batting/BettingPool.java
+++ src/main/java/domain/batting/BettingPool.java
@@ -1,4 +1,25 @@
 package domain.batting;
 
+import domain.participant.Player;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BettingPool {
+    private final Map<Player, Bet> bets;
+
+    private BettingPool(Map<Player, Bet> bets) {
+        this.bets = bets;
+    }
+
+    public static BettingPool of() {
+        return new BettingPool(new HashMap<>());
+    }
+
+    public boolean wager(Player player, Bet bet) {
+        if (bets.containsKey(player)) {
+            return false;
+        }
+        bets.put(player, bet);
+        return true;
+    }
 }
