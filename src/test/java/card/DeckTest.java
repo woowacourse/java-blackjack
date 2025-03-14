e249fd838c97ed4ee9198ce9a251a88a86e12b3f
egaeng09 <egaeng09@gmail.com>
2025-03-14 17:42:19 +0900

    test(DeckTest): 카드 뽑기 테스트 경우의 수 추가

--- src/test/java/card/DeckTest.java
+++ src/test/java/card/DeckTest.java
@@ -3,19 +3,19 @@ package card;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class DeckTest {
 
-    @Test
-    void 덱에서_카드를_뽑는다() {
-        // given
-        final int cardCount = 2;
-
-        // when
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9})
+    void 덱에서_카드를_뽑는다(int value) {
+        // given & when
         Deck deck = DeckGenerator.generateDeck();
-        List<Card> card = deck.drawCards(cardCount);
+        List<Card> card = deck.drawCards(value);
 
         // then
-        Assertions.assertThat(card.size()).isEqualTo(cardCount);
+        Assertions.assertThat(card.size()).isEqualTo(value);
     }
 }
