50f508d7556b1a363b6ae511351001a143f30ddd
geonwoo <kysub99@naver.com>
2025-03-16 03:51:31 +0900

    refactor: 0원 예외 처리 추가

--- src/main/java/domain/game/BetMoney.java
+++ src/main/java/domain/game/BetMoney.java
@@ -9,11 +9,15 @@ public class BetMoney {
     }
 
     private void validateAmount(int amount) {
-        if (amount < 0) {
+        if (amount <= 0) {
             throw new IllegalArgumentException("배팅 금액은 0 이나 음수가 될 수 없습니다");
         }
         if (amount > 100_000_000) {
             throw new IllegalArgumentException("배팅 금액은 1억원을 넘을 수 없습니다.");
         }
+    }
+
+    int getAmount() {
+        return amount;
     }
 }
