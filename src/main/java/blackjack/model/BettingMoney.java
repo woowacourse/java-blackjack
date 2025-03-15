5b6c6c8f348a4409e7b4aae9742ef77ae35852c8
ykmxxi <ykmxxi97@gmail.com>
2025-03-15 18:31:58 +0900

    refactor: 베팅 금액 생성 예외 메시지 수정

--- src/main/java/blackjack/model/BettingMoney.java
+++ src/main/java/blackjack/model/BettingMoney.java
@@ -17,7 +17,7 @@ public class BettingMoney {
 
     private void validateBettingMoneyRange(final int amount) {
         if (amount < MIN_BETTING_AMOUNT || amount > MAX_BETTING_AMOUNT) {
-            throw new IllegalArgumentException("베틍 금액은 최소 %d원 최대 %d원 까지 가능합니다.".formatted(
+            throw new IllegalArgumentException("베팅 금액은 최소 %d원 최대 %d원 까지 가능합니다.".formatted(
                     MIN_BETTING_AMOUNT, MAX_BETTING_AMOUNT
             ));
         }
