161805e5942c2d0dca3385a2a44803d0b5dede0d
threepebbles <threepebbles@naver.com>
2025-03-15 16:12:24 +0900

    test(BlackjackCard): 블랙잭 카드 테스트 픽스쳐 추가

---
+++ src/test/java/domain/fixture/BlackjackCardFixture.java
@@ -0,0 +1,22 @@
+package domain.fixture;
+
+import domain.card.BlackjackCard;
+import domain.card.Rank;
+import domain.card.Suit;
+
+public class BlackjackCardFixture {
+    public static BlackjackCard ACE_HEART = new BlackjackCard(Rank.ACE, Suit.HEART);
+    public static BlackjackCard TWO_HEART = new BlackjackCard(Rank.TWO, Suit.HEART);
+    public static BlackjackCard THREE_HEART = new BlackjackCard(Rank.THREE, Suit.HEART);
+    public static BlackjackCard FOUR_HEART = new BlackjackCard(Rank.FOUR, Suit.HEART);
+    public static BlackjackCard FIVE_HEART = new BlackjackCard(Rank.FIVE, Suit.HEART);
+    public static BlackjackCard SIX_HEART = new BlackjackCard(Rank.SIX, Suit.HEART);
+    public static BlackjackCard SEVEN_HEART = new BlackjackCard(Rank.SEVEN, Suit.HEART);
+    public static BlackjackCard EIGHT_HEART = new BlackjackCard(Rank.EIGHT, Suit.HEART);
+    public static BlackjackCard NINE_HEART = new BlackjackCard(Rank.NINE, Suit.HEART);
+    public static BlackjackCard TEN_HEART = new BlackjackCard(Rank.TEN, Suit.HEART);
+    public static BlackjackCard JACK_HEART = new BlackjackCard(Rank.JACK, Suit.HEART);
+    public static BlackjackCard QUEEN_HEART = new BlackjackCard(Rank.QUEEN, Suit.HEART);
+    public static BlackjackCard KING_HEART = new BlackjackCard(Rank.KING, Suit.HEART);
+
+}
