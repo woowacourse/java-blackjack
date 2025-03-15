19d3f24a1e95f810af63ae02db57c434ba744562
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-15 19:48:07 +0900

    🔵refactor(Betting): 배팅 금액이 음수인 경우 예외 처리 리팩토링

--- src/test/java/domain/Betting.java
+++ src/test/java/domain/Betting.java
@@ -4,9 +4,13 @@ public class Betting {
     private final int amount;
 
     public Betting(final int amount) {
+        validatePositive(amount);
+        this.amount = amount;
+    }
+
+    private static void validatePositive(final int amount) {
         if (amount < 0) {
             throw new IllegalArgumentException("[ERROR] 배팅 금액은 음수가 될 수 없습니다.");
         }
-        this.amount = amount;
     }
 }
