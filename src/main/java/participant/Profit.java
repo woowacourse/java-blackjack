ee84fee8e3601a4e1b7cac35f7f03ba43a8fbf11
soeun2537 <soeun2537@gmail.com>
2025-03-15 16:16:41 +0900

    feat: 수익 객체 구현

---
+++ src/main/java/participant/Profit.java
@@ -0,0 +1,26 @@
+package participant;
+
+public class Profit {
+
+    private final int amount;
+
+    private Profit(int amount) {
+        this.amount = amount;
+    }
+
+    public static Profit of(int amount) {
+        return new Profit(amount);
+    }
+
+    public static Profit from(Money bettingMoney, Money earnedMoney) {
+        return new Profit(earnedMoney.minus(bettingMoney.getAmount()).getAmount());
+    }
+
+    public Profit negate() {
+        return new Profit(this.amount * -1);
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
