5a37f20480a030d39e5b26cd76894fd2231fcc4e
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-03-15 17:12:23 +0900

    feat: 블랙잭, 버스트 관련 규칙 추가

--- src/main/java/blackjack/GameRule.java
+++ src/main/java/blackjack/GameRule.java
@@ -1,7 +1,8 @@
-package blackjack.domain;
+package blackjack;
 
 public enum GameRule {
 
+    BLACKJACK_POINT(21, "블랙잭 점수"),
     BUST_THRESHOLD_POINT(21, "버스트 경곗값"),
     INITIAL_DEALING_CARD_COUNT(2, "초기 카드 배분 개수"),
     ;
@@ -16,6 +17,17 @@ public enum GameRule {
 
     public static boolean isBust(int sumOfCards) {
         return sumOfCards > GameRule.BUST_THRESHOLD_POINT.getValue();
+    }
+
+    public static boolean isBlackjack(int cardCount, int sumOfCards) {
+        return cardCount == INITIAL_DEALING_CARD_COUNT.value && sumOfCards == BLACKJACK_POINT.value;
+    }
+
+    public static int applyBustRule(int sumOfCards) {
+        if (isBust(sumOfCards)) {
+            return 0;
+        }
+        return sumOfCards;
     }
 
     public int getValue() {
