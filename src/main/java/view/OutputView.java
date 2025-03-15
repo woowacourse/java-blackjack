e7fa28af8ddeafb53d3148415df12d95b03ad421
bowook <mbosx316@naver.com>
2025-03-15 15:25:11 +0900

    feat(OutputView) : 출력 객체 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,72 +1,76 @@
 package view;
 
-import dto.BattleResultResponse;
-import dto.BlackJackResultResponse;
-import dto.CardDeckStatusResponse;
-import dto.ParticipantResponse;
+import domain.BlackJackGame;
+import domain.GamblingStatement;
+import domain.Money;
+import domain.card.Card;
+import domain.card.ParticipantCardDeck;
+import domain.participant.Participant;
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
 
 public class OutputView {
-    public void printDrawTwoCardsMessage(ParticipantResponse response) {
-        System.out.printf("%n%s와 %s에게 2장을 나누었습니다.%n",
-                response.dealerName(),
-                String.join(", ", response.playerNames())
-        );
-    }
 
-    public void printCardDeckStatus(CardDeckStatusResponse response) {
-        Map<String, List<String>> cardDeckNamesOfParticipant = response.cardDeckNamesOfParticipant();
-
-        for (Map.Entry<String, List<String>> entry : cardDeckNamesOfParticipant.entrySet()) {
-            String participantName = entry.getKey();
-            List<String> cardNames = entry.getValue();
-
-            System.out.printf("%s 카드: %s%n",
-                    participantName,
-                    String.join(", ", cardNames)
-            );
-        }
+    public void writeDrawTwoCards(final BlackJackGame blackJackGame) {
         System.out.println();
+        for (Participant participant : blackJackGame.getParticipants().getParticipants()) {
+            String name = participant.getNickname();
+            List<Card> cards = participant.getCardDeck().getCards();
+            if (participant.isDealer()) {
+                System.out.printf("%s : %s%s%n", name, cards.getFirst().getNumber(), cards.getFirst().getCardSymbol());
+                continue;
+            }
+            System.out.printf("%s 카드 : %s%s, %s%s%n", name,
+                    cards.getFirst().getNumber(),
+                    cards.getFirst().getCardSymbol(),
+                    cards.getLast().getNumber(),
+                    cards.getLast().getCardSymbol());
+        }
     }
 
-    public void printDrawSingleCardToDealerMessage(String nickname, int stayThreshold) {
-        System.out.printf("%s는 %d이하라 한장의 카드를 더 받았습니다.%n",
-                nickname,
-                stayThreshold
-        );
+    public void writeDealerDrawOneCard(int count) {
+        for (int i = 0; i < count; i ++) {
+            System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+        }
     }
 
-    public void printBlackJackResult(List<BlackJackResultResponse> resultResponses) {
+    public void writePlayerDrawOneCard(Participant participant) {
+        ParticipantCardDeck participantCardDeck = participant.getCardDeck();
+        List<Card> cards = participantCardDeck.getCards();
+        StringJoiner stringJoiner = new StringJoiner(", ");
+        for (Card card : cards) {
+            stringJoiner.add(card.getNumber() + card.getCardSymbol());
+        }
+        System.out.printf("%s 카드 : %s%n", participant.getNickname(), stringJoiner);
+    }
+
+    public void writeParticipantsScoreResult(BlackJackGame blackJackGame) {
         System.out.println();
-        for (BlackJackResultResponse response : resultResponses) {
-            System.out.printf("%s 카드: %s - 결과: %d%n",
-                    response.nickname(),
-                    String.join(", ", response.cardNames()),
-                    response.totalScore()
-            );
+        List<Participant> participants = blackJackGame.getParticipants().getParticipants();
+        for (Participant participant : participants) {
+            List<Card> cards = participant.getCardDeck().getCards();
+            StringJoiner stringJoiner = new StringJoiner(", ");
+            for (Card card : cards) {
+                stringJoiner.add(card.getNumber() + card.getCardSymbol());
+            }
+            System.out.printf("%s 카드 : %s - 결과 : %s%n", participant.getNickname(), stringJoiner, participant.getScore());
         }
     }
 
-    public void printBattleResult(List<BattleResultResponse> resultResponses) {
-        System.out.println("\n## 최종 승패");
-        for (BattleResultResponse response : resultResponses) {
-            String nickname = response.nickname();
-            Map<String, Integer> battleResultCount = response.battleResult();
-
-            System.out.printf("%s: %s%n", nickname, parseBattleResult(battleResultCount));
+    public void writeTotalProfit(GamblingStatement gamblingStatement) {
+        System.out.println();
+        System.out.println("## 최종 수익");
+        Map<Participant, Money> participantProfit = gamblingStatement.getGamblingStatement();
+        StringBuilder playerStringBuilder = new StringBuilder();
+        double dealerProfit = 0;
+        for (Map.Entry<Participant, Money> entry : participantProfit.entrySet()) {
+            String name = entry.getKey().getNickname();
+            String profit = String.valueOf(entry.getValue().getAmount());
+            dealerProfit += (-Double.parseDouble(profit));
+            playerStringBuilder.append(name).append(" : ").append(profit).append("\n");
         }
+        System.out.println("딜러 : " + dealerProfit);
+        System.out.println(playerStringBuilder);
     }
-
-    private String parseBattleResult(Map<String, Integer> battleResultCount) {
-        StringBuilder result = new StringBuilder();
-        for (Map.Entry<String, Integer> entry : battleResultCount.entrySet()) {
-            int count = entry.getValue();
-            result.append(count)
-                    .append(entry.getKey()).append(" ");
-        }
-
-        return result.toString();
-    }
-
 }
