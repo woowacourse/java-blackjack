12b72406efc717c3530904b77591d841b20cde75
bowook <mbosx316@naver.com>
2025-03-15 17:06:34 +0900

    chore(GamblingStatement) : 의미 없는 개행 삭제

--- src/main/java/bank/GamblingStatement.java
+++ src/main/java/bank/GamblingStatement.java
@@ -24,7 +24,6 @@ public class GamblingStatement {
         return new GamblingStatement(profitStatement);
     }
 
-
     public Map<Participant, Money> getGamblingStatement() {
         return Collections.unmodifiableMap(gamblingStatement);
     }
