ff7e510d4b39df3acca77c76dbc46cca16531fda
bowook <mbosx316@naver.com>
2025-03-15 15:16:05 +0900

    refactor(DTO,Controller) : 각 객체간의 상호작용으로 수정하기 위해 삭제

--- src/main/java/dto/BattleResultResponse.java
+++
@@ -1,6 +0,0 @@
-package dto;
-
-import java.util.Map;
-
-public record BattleResultResponse(String nickname, Map<String, Integer> battleResult) {
-}
--- src/main/java/dto/BlackJackResultResponse.java
+++
@@ -1,6 +0,0 @@
-package dto;
-
-import java.util.List;
-
-public record BlackJackResultResponse(String nickname, List<String> cardNames, int totalScore) {
-}
