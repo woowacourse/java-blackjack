c2bc88ccc0f1cf96add82cefcfc9479c017a6181
ykmxxi <ykmxxi97@gmail.com>
2025-03-15 18:14:19 +0900

    refactor: 기본 생성자 ArrayList로 구현체 변경

--- src/main/java/blackjack/model/card/Cards.java
+++ src/main/java/blackjack/model/card/Cards.java
@@ -2,7 +2,6 @@ package blackjack.model.card;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.IntStream;
@@ -22,7 +21,7 @@ public class Cards {
     }
 
     private Cards() {
-        this.values = new LinkedList<>();
+        this.values = new ArrayList<>();
     }
 
     public static Cards empty() {
