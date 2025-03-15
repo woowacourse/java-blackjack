af26af16e7a7121c89ddc55e7bc2153f471fd06e
yesjuhee <juhee0924@g.skku.edu>
2025-03-15 17:49:21 +0900

    chore: 기존 코드 삭제
    - 새로운 설계를 위해 기존 코드를 삭제함

--- src/main/java/Application.java
+++
@@ -1,7 +0,0 @@
-import controller.BlackJackController;
-
-public class Application {
-    public static void main(String[] args) {
-        new BlackJackController().run();
-    }
-}
--- src/main/java/domain/Player.java
+++
@@ -1,7 +0,0 @@
-package domain;
-
-public class Player extends GameParticipant {
-    public Player(String name, CardHand cardHand) {
-        super(name, cardHand);
-    }
-}
