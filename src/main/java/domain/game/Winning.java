684b8099a9d74f6273594cc4c35f98b8045e3fff
geonwoo <kysub99@naver.com>
2025-03-15 21:45:14 +0900

    feat(Winning): 게임 결과별 배당률 구현

--- src/main/java/domain/game/Winning.java
+++ src/main/java/domain/game/Winning.java
@@ -1,16 +1,18 @@
 package domain.game;
 
 public enum Winning {
-    BLACKJACK("블랙잭"),
-    WIN("승"),
-    DRAW("무"),
-    LOSE("패"),
+    BLACKJACK("블랙잭", 1.5),
+    WIN("승", 1),
+    DRAW("무", 0),
+    LOSE("패", -1),
     ;
 
     private final String name;
+    private final double earningRate;
 
-    Winning(String name) {
+    Winning(String name, double earningRate) {
         this.name = name;
+        this.earningRate = earningRate;
     }
 
     public static Winning determine(ScoreInfo playerScoreInfo, ScoreInfo dealerScoreInfo) {
@@ -46,5 +48,9 @@ public enum Winning {
 
     public String getName() {
         return name;
+    }
+
+    public double getEarningRate() {
+        return earningRate;
     }
 }
