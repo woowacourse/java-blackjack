6450a93931c6b6d63d3438e8ab586b4796ec872e
RIWON <121172065+awrion3@users.noreply.github.com>
2025-03-15 13:09:00 +0900

    feat(card): create card with rank, suit

---
+++ src/main/java/model/cards/Card.java
@@ -0,0 +1,22 @@
+package model.cards;
+
+import java.util.List;
+
+public class Card {
+    private final Rank rank;
+    private final Suit suit;
+
+    public Card(String rank, String suit) {
+        this.rank = Rank.of(rank);
+        this.suit = Suit.of(suit);
+    }
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public List<Integer> getRank() {
+        return rank.getValue();
+    }
+}
