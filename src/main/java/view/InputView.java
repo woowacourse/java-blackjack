49c1d7ae44542af7364bdb9f5662941cf818aa51
bowook <mbosx316@naver.com>
2025-03-15 15:16:27 +0900

    refactor(Answer) : Boolean 타입 반환하도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,10 +1,20 @@
 package view;
 
+import domain.BlackJackGame;
+import domain.Money;
+import domain.participant.Participant;
+import domain.participant.Participants;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
 
     public List<String> askPlayerNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
@@ -12,12 +22,24 @@ public class InputView {
         return List.of(input.split(","));
     }
 
-    public Answer askDrawOneMore(String nickname) {
-        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)%n", nickname);
+    public Map<Participant, Money> askBettingMoney(BlackJackGame blackJackGame) {
+        Participants participants = blackJackGame.getParticipants().findPlayers();
+        Map<Participant, Money> gamblingStatement = new HashMap<>();
+        for (Participant onlyPlayer : participants.getParticipants()) {
+            System.out.printf("%s의 배팅 금액은?%n", onlyPlayer.getNickname());
+            gamblingStatement.put(onlyPlayer, new Money(scanner.nextLine()));
+        }
+
+        return gamblingStatement;
+    }
+
+    public boolean askDrawOneMore(Participant participant) {
+        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)%n", participant.getNickname());
         return Answer.from(scanner.nextLine());
     }
 
     public void closeScanner() {
         scanner.close();
     }
+
 }
