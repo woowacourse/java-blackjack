fd3a31a6f4a06022a49a4545533592145233a70d
bowook <mbosx316@naver.com>
2025-03-15 15:20:17 +0900

    refactor(BattleResults) : 승,패,무승부 횟수가 있는지 검사하고 있으면 횟수 반환 및 없으면 0 반환

--- src/main/java/domain/ScoreResult/BattleResults.java
+++ src/main/java/domain/ScoreResult/BattleResults.java
@@ -18,4 +18,28 @@ public class BattleResults {
     public Map<BattleResult, Integer> getResults() {
         return Collections.unmodifiableMap(battleResults);
     }
+
+    public int requestWinCounts() {
+        if (battleResults.containsKey(BattleResult.WIN)) {
+            return battleResults.get(BattleResult.WIN);
+        }
+
+        return 0;
+    }
+
+    public int requestDrawCounts() {
+        if (battleResults.containsKey(BattleResult.DRAW)) {
+            return battleResults.get(BattleResult.DRAW);
+        }
+
+        return 0;
+    }
+
+    public int requestLoseCounts() {
+        if (battleResults.containsKey(BattleResult.LOSE)) {
+            return battleResults.get(BattleResult.LOSE);
+        }
+
+        return 0;
+    }
 }
