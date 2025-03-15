3df2f63cbc08ab9e5fce67c000fd32d164f7a49a
RIWON <121172065+awrion3@users.noreply.github.com>
2025-03-15 15:44:28 +0900

    feat(suit): create card suit
    - convert string to suit name

---
+++ src/main/java/model/cards/Suit.java
@@ -0,0 +1,29 @@
+package model.cards;
+
+import java.util.stream.Stream;
+
+public enum Suit {
+    CLUBS("클로버"),
+    DIAMONDS("다이아몬드"),
+    HEARTS("하트"),
+    SPADES("스페이드");
+
+    private static final String WARNING_INVALID_SUIT = "[ERROR] 존재하지 않는 카드 무늬입니다.";
+
+    private final String name;
+
+    Suit(String name) {
+        this.name = name;
+    }
+
+    public static Suit of(String name) {
+        return Stream.of(Suit.values())
+                .filter(suit -> suit.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(WARNING_INVALID_SUIT));
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/model/cards/SuitTest.java
@@ -0,0 +1,17 @@
+package model.cards;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SuitTest {
+
+    @DisplayName("예외: 지정된 무늬 값만 유효하다.")
+    @Test
+    void testCard_StringError() {
+        assertThatThrownBy(() -> Suit.of("조커"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 존재하지 않는 카드 무늬입니다.");
+    }
+}
