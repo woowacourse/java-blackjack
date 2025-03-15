ab8d699c55afd89a8f338c17d3122374177fc167
Han <jumdo12@gmail.com>
2025-03-15 16:10:54 +0900

    refactor(InputView): scanner 한번만 생성하도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private final Scanner scanner;
+
+    public InputView(){
+        scanner = new Scanner(System.in);
+    }
 
     public List<String> readPlayers() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
@@ -38,7 +43,6 @@ public class InputView {
     }
 
     private String readInput() {
-        final Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 }
