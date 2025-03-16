be211f929d2338c172800b32e48682e78aa14c3a
(minjae8563) <(minjae8563@naver.com)>
2025-03-16 17:45:57 +0900

    refactor: 메서드명 변경

--- src/main/java/domain/participant/Money.java
+++ src/main/java/domain/participant/Money.java
@@ -4,11 +4,11 @@ public class Money {
     private final int money;
 
     public Money(final int money) {
-        isDivisibleByTenThousand(money);
+        validateDivisibleByTenThousand(money);
         this.money = money;
     }
 
-    private void isDivisibleByTenThousand(final int money) {
+    private void validateDivisibleByTenThousand(final int money) {
         if (money % 10000 != 0) {
             throw new IllegalArgumentException("[ERROR] 배팅 금액은 만원 단위입니다.");
         }
--- src/test/java/domain/participant/MoneyTest.java
+++ src/test/java/domain/participant/MoneyTest.java
@@ -15,7 +15,7 @@ class MoneyTest {
 
     @Test
     @DisplayName("돈 단위 테스트")
-    void isDivisibleByTenThousandTest() {
+    void validateDivisibleByTenThousandTest() {
         assertThatThrownBy(() -> new Money(10001))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 배팅 금액은 만원 단위입니다.");
