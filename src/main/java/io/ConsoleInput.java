4b0a467e234eacd33d881dcf811c1f3e59a84e08
BWY <quantumjump10@gmail.com>
2025-03-16 17:05:38 +0900

    style: import 와일드카드 제거

--- src/main/java/io/ConsoleInput.java
+++ src/main/java/io/ConsoleInput.java
@@ -4,7 +4,11 @@ import bet.BetAmount;
 import participant.Player;
 import participant.Players;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 
 public class ConsoleInput {
 
