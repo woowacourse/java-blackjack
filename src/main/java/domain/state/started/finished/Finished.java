41bd609799f050cf0dbdeb3b8c0d844e3ed17de5
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-03-16 16:23:18 +0900

    feat: 게임 종료 상태 Finished 구현

---
+++ src/main/java/domain/state/started/finished/Finished.java
@@ -0,0 +1,34 @@
+package domain.state.started.finished;
+
+import domain.card.Card;
+import domain.card.Hand;
+import domain.state.State;
+import domain.state.started.Started;
+
+public abstract class Finished extends Started {
+    public Finished(Hand hand) {
+        super(hand);
+    }
+
+    @Override
+    public State hit(Card card) {
+        throw new IllegalStateException("[ERROR] 차례가 끝나 카드를 더 뽑을 수 없습니다.");
+    }
+
+    @Override
+    public State stay() {
+        throw new IllegalStateException("[ERROR] 이미 차례가 끝났습니다.");
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public double getProfit(int bettingAmount) {
+        return getProfitRate() * bettingAmount;
+    }
+
+    public abstract double getProfitRate();
+}
