ed61feaf64504c82aeed45e3709995a8f5ef7f57
Jin409 <sualng123@gmail.com>
2025-03-16 17:21:01 +0900

    refactor: 사용되지 않는 GameEvaluator 삭제

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -1,13 +1,11 @@
 package blackjack;
 
 import blackjack.controller.BlackjackController;
-import blackjack.domain.game.GameRuleEvaluator;
 
 public class Application {
 
     public static void main(String[] args) {
-        GameRuleEvaluator gameRuleEvaluator = new GameRuleEvaluator();
-        BlackjackController blackjackController = new BlackjackController(gameRuleEvaluator);
+        BlackjackController blackjackController = new BlackjackController();
 
         blackjackController.run();
     }
