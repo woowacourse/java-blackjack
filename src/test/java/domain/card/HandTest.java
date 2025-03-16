4ed8bb78fe2f82788fc5efabdf0ba5da53bd5a9b
황재웅 <wodnd0131@naver.com>
2025-03-16 15:23:27 +0900

    test(HandTest): 방어적 복사로 카드 목록의 불변성 보장

--- src/test/java/domain/card/HandTest.java
+++ src/test/java/domain/card/HandTest.java
@@ -1,15 +1,34 @@
 package domain.card;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class HandTest {
+
+  @Nested
+  @DisplayName("핸드가 불변 객체인지 테스트")
+  class HandImmutabilityTest {
+
+    @Test
+    @DisplayName("핸드는 카드 목록을 방어적 복사로 관리한다.")
+    void test_immutability() {
+      //given
+      final List<TrumpCard> trumpCards = List.of(new TrumpCard(Rank.ACE, Suit.DIAMOND));
+      final var cards = new ArrayList<>(trumpCards);
+      final var hand = new Hand(cards);
+      //when&then
+      Assertions.assertThat(hand.getCards()).isEqualTo(trumpCards);
+      Assertions.assertThat(hand.getCards()).isNotSameAs(trumpCards);
+    }
+  }
 
   @Nested
   @DisplayName("모든 Card의 점수 합을 계산한다.")
