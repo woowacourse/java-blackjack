2ac950bdd023242262c7d4f1ce4565d2720e2b24
Mint <audwl03071@gmail.com>
2025-03-16 20:37:49 +0900

    refactor(Card): 테스트 메서드명 명확하게 변경

--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -8,7 +8,7 @@ class CardTest {
 
     @DisplayName("카드를 생성한다.")
     @Test
-    void test() {
+    void createCard() {
         // given
         final Shape shape = Shape.DIAMOND;
         final CardScore cardScore = CardScore.A;
