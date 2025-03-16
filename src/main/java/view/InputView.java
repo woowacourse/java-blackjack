37fbd76cb51958d1475ac956616d3b65a77c1558
eoehd1ek <eoehd2ek@gmail.com>
2025-03-17 01:04:40 +0900

    refector: InputView의 Scanner 필드를 주입 받도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,13 +1,16 @@
 package view;
 
-import domain.Participant;
 import java.util.Scanner;
 
 public class InputView {
 
     private static final String INPUT_USER_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
 
-    private final Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
 
     public String inputUserNames() {
         System.out.println(INPUT_USER_NAME_MESSAGE);
