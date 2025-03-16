4a18dbfe8c688a4c8ec6816d8326423f3683242c
(minjae8563) <(minjae8563@naver.com)>
2025-03-16 18:32:44 +0900

    feat: 줄 바꿈 기능하는 메서드 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String COMMA_DELIMITER = ",";
     private static final String PLAYER_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-    private static final String PLAYER_BATTING_MONEY = "%s의 배팅 금액은?\n";
+    private static final String PLAYER_BATTING_MONEY = "%s의 배팅 금액은?";
     private static final String ASK_HIT_OR_STAND = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
 
     private final Scanner scanner;
@@ -34,6 +34,11 @@ public class InputView {
 
     public int askPlayerBattingMoney(Name name) {
         System.out.printf(PLAYER_BATTING_MONEY, name.getName());
+        printNewLine();
         return Integer.parseInt(scanner.nextLine());
+    }
+
+    private void printNewLine() {
+        System.out.print(System.lineSeparator());
     }
 }
