a33bceb1bc570937ca964b4616aa866d843195cf
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-03-16 16:31:27 +0900

    test: State 추가 구현에 따른 Dealer 테스트 코드 재작성

--- src/test/java/domain/gamer/DealerTest.java
+++ src/test/java/domain/gamer/DealerTest.java
@@ -1,70 +1,95 @@
 package domain.gamer;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 import domain.card.Card;
 import domain.card.CardDeck;
 import domain.card.CardRank;
 import domain.card.CardSymbol;
-import fixture.CardsInitializerFixture;
-import java.util.ArrayList;
-import java.util.List;
+import domain.card.Hand;
+import domain.rule.BlackjackMatchResult;
+import domain.state.started.finished.Stay;
+import domain.state.started.running.Hit;
+import fixture.BettingFixture;
+import fixture.CardDeckFixture;
+import fixture.HandFixture;
+import fixture.NicknameFixture;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class DealerTest {
 
-    @DisplayName("딜러는 처음 받은 2장의 카드의 합이 16 이하라면 카드를 뽑는다")
-    @Test
-    void canHit() {
-        //given
-        Card card1 = new Card(CardSymbol.COLVER, CardRank.ACE);
-        Card card2 = new Card(CardSymbol.HEART, CardRank.TWO);
-        Card card3 = new Card(CardSymbol.SPADE, CardRank.KING);
-
-        List<Card> cards = new ArrayList<>();
-        cards.add(card1);
-        cards.add(card2);
-        cards.add(card3);
-
-        CardDeck deck = new CardDeck(new CardsInitializerFixture(cards));
-
-        Dealer dealer = new Dealer();
-        dealer.prepareGame(deck);
-
-        int initialPoint = dealer.getHand().calculateTotalPoint();
-
-        //when
-        dealer.hit(deck);
-        int actual = dealer.getHand().calculateTotalPoint();
-
-        //then
-        assertThat(actual).isNotEqualTo(initialPoint);
+    public static Stream<Arguments> provideCardDeckAndExpected() {
+        return Stream.of(
+                Arguments.of(
+                        CardDeckFixture.createCardDeck(
+                                Card.of(CardSymbol.SPADE, CardRank.SEVEN),
+                                Card.of(CardSymbol.SPADE, CardRank.KING)),
+                        Stay.class),
+                Arguments.of(
+                        CardDeckFixture.createCardDeck(
+                                Card.of(CardSymbol.SPADE, CardRank.SIX),
+                                Card.of(CardSymbol.SPADE, CardRank.KING)),
+                        Hit.class)
+        );
     }
 
-    @DisplayName("딜러는 처음 받은 2장의 카드의 합이 17 이상이라면 카드를 뽑지 않는다")
-    @Test
-    void cannotHit() {
-        //given
-        Card card1 = new Card(CardSymbol.COLVER, CardRank.JACK);
-        Card card2 = new Card(CardSymbol.HEART, CardRank.NINE);
-        Card card3 = new Card(CardSymbol.SPADE, CardRank.KING);
-
-        List<Card> cards = new ArrayList<>();
-        cards.add(card1);
-        cards.add(card2);
-        cards.add(card3);
-
-        CardDeck deck = new CardDeck(new CardsInitializerFixture(cards));
-
-        Dealer dealer = new Dealer();
-        dealer.prepareGame(deck);
-
-        //when //then
-        assertThatCode(() -> dealer.hit(deck))
-                .isInstanceOf(IllegalStateException.class);
-
+    public static Stream<Arguments> providePlayersAndExpected() {
+        return Stream.of(
+                Arguments.of(new Player(HandFixture.createHand(Card.of(CardSymbol.SPADE, CardRank.EIGHT),
+                                Card.of(CardSymbol.SPADE, CardRank.EIGHT)),
+                                NicknameFixture.createNickname("ad"), BettingFixture.createBetting(1000)),
+                        BlackjackMatchResult.LOSE
+                ),
+                Arguments.of(new Player(HandFixture.createHand(Card.of(CardSymbol.SPADE, CardRank.EIGHT),
+                                Card.of(CardSymbol.SPADE, CardRank.JACK)),
+                                NicknameFixture.createNickname("ad"), BettingFixture.createBetting(1000)),
+                        BlackjackMatchResult.WIN
+                ),
+                Arguments.of(new Player(HandFixture.createHand(Card.of(CardSymbol.SPADE, CardRank.EIGHT),
+                                Card.of(CardSymbol.SPADE, CardRank.NINE)),
+                                NicknameFixture.createNickname("ad"), BettingFixture.createBetting(1000)),
+                        BlackjackMatchResult.DRAW
+                )
+        );
     }
 
+    @DisplayName("딜러는 초기 핸드 점수에 따라 다른 상태를 가진다.")
+    @ParameterizedTest
+    @MethodSource("provideCardDeckAndExpected")
+    void checkState(CardDeck cardDeck, Class<?> expected) {
+        // given
+        Hand hand = new Hand(cardDeck);
+
+        Dealer dealer = new Dealer(hand);
+
+        // when
+        final var actual = dealer.state;
+
+        // then
+        assertThat(actual).isInstanceOf(expected);
+    }
+
+    @DisplayName("딜러는 플레이어의 승패 결과를 도출한다.")
+    @ParameterizedTest
+    @MethodSource("providePlayersAndExpected")
+    void determineMatchResult(Player player, BlackjackMatchResult expected) {
+        // given
+        CardDeck cardDeck = CardDeckFixture.createCardDeck(
+                Card.of(CardSymbol.SPADE, CardRank.SEVEN),
+                Card.of(CardSymbol.SPADE, CardRank.KING));
+
+        Hand hand = new Hand(cardDeck);
+
+        Dealer dealer = new Dealer(hand);
+
+        // when
+        BlackjackMatchResult actual = dealer.determineMatchResultFor(player);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
 }
