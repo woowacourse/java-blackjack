b2100ed825876d94e8a8f10a12a11855d5893f27
KJungW <sagakkb0707@naver.com>
2025-03-16 20:48:58 +0900

    refactor(exception): 에외메세지 수정

--- src/main/java/blackjack/exception/ExceptionMessage.java
+++ src/main/java/blackjack/exception/ExceptionMessage.java
@@ -7,7 +7,7 @@ public enum ExceptionMessage {
     CANNOT_HIT("히트가 불가능한 상태에서 히트를 시도하고 있습니다."),
     INVALID_PLAYER_COUNT("적절하지 않은 플레이어 수 입니다."),
     NOT_ALLOWED_DUPLICATED_PLAYER("중복 플레이어는 허용하지 않습니다."),
-    INVALID_BETTING_AMOUNT("배팅금액은 양수여야 합니다.");
+    INVALID_BETTING_AMOUNT("배팅금액은 음수를 허용하지 않습니다.");
 
 
     private final String content;
