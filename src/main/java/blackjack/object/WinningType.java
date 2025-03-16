58e52c252d0131265ebab57995c989913d003b8b
RaZel713 <razel7130@gmail.com>
2025-03-16 21:01:27 +0900

    refactor: WinningType의 미사용 메서드 삭제

--- src/main/java/blackjack/object/WinningType.java
+++ src/main/java/blackjack/object/WinningType.java
@@ -4,24 +4,9 @@ public enum WinningType {
     WIN("승"),
     DEFEAT("패"),
     DRAW("무");
-
     private final String displayName;
 
     WinningType(final String displayName) {
         this.displayName = displayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public WinningType reverse() {
-        if (this == WIN) {
-            return DEFEAT;
-        }
-        if (this == DEFEAT) {
-            return WIN;
-        }
-        return DRAW;
     }
 }
