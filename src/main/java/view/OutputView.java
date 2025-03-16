1a3c45499bbb023038273dc0e57d7b98b9a3b611
eunseongu <rndmstjs75@gmail.com>
2025-03-16 19:38:29 +0900

    refactor: OutputView 리팩터링
    - 중복 메서드 제거
    - 불필요한 메서드 제거
    - displayInitialDeal()에서 Dealer와 Players의 카드 정보를 공통 로직으로 출력하도록 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,54 +2,42 @@ package view;
 
 import domain.card.Card;
 import domain.game.Game;
+import domain.participant.Dealer;
 import domain.participant.GameParticipant;
-import domain.participant.Player;
 import java.util.List;
 
 public class OutputView {
     private static final String DELIMITER = ", ";
 
     public void displayInitialDeal(Game game) {
-        List<Player> players = game.getPlayers();
-        List<String> playerNames = players.stream()
-                .map(Player::getName)
-                .toList();
-        System.out.printf("%n딜러와 %s에게 2장을 나누었습니다.%n", String.join(DELIMITER, playerNames));
-        System.out.printf("딜러카드: %s", game.getDealerCards().getFirst().getNotation());
-        displayEmptyLine();
-        displayAllParticipantsAndCards(players);
+        List<GameParticipant> participants = game.getParticipants();
+        List<String> playerNames = game.getPlayerNames();
+
+        System.out.printf("%n%s와 %s에게 2장을 나누었습니다.%n", Dealer.NAME, String.join(DELIMITER, playerNames));
+        participants.forEach((participant) -> {
+            displayNameAndCards(participant.getName(), participant.getCards());
+            displayEmptyLine();
+        });
         displayEmptyLine();
     }
 
-    public void displayAllParticipantsAndCards(List<? extends GameParticipant> gameParticipants) {
-        gameParticipants.forEach((participant) -> {
-            getCardNotations(participant.getCards());
-            displayParticipantAndCards(participant);
-            displayEmptyLine();
-        });
+    public void displayNameAndCards(String name, List<Card> cards) {
+        List<String> cardNotations = cards.stream()
+                .map(Card::getNotation)
+                .toList();
+        System.out.printf("%s카드: %s", name, String.join(DELIMITER, cardNotations));
+    }
+
+    public void displayDealerHitMessage() {
+        System.out.println("딜러는 16이하라 한 장의 카드를 더 받았습니다.");
     }
 
     public void displayScore(Game game) {
         List<GameParticipant> participants = game.getParticipants();
         for (GameParticipant participant : participants) {
-            displayParticipantAndCards(participant);
+            displayNameAndCards(participant.getName(), participant.getCards());
             System.out.printf(" - 결과: %d%n", participant.calculateScore());
         }
-    }
-
-    public void displayParticipantAndCards(GameParticipant gameParticipant) {
-        String name = gameParticipant.getName();
-        List<String> cardNotations = getCardNotations(gameParticipant.getCards());
-        System.out.printf("%s카드: %s", name, String.join(DELIMITER, cardNotations));
-    }
-
-    public void displayDealerHitResult() {
-        System.out.println("딜러는 16이하라 한 장의 카드를 더 받았습니다.");
-    }
-
-    public void displayNameAndCards(String name, List<Card> cards) {
-        List<String> cardNotations = getCardNotations(cards);
-        System.out.printf("%s카드: %s", name, String.join(DELIMITER, cardNotations));
     }
 
     public void displayProfitMessage() {
@@ -60,12 +48,6 @@ public class OutputView {
     public void displayParticipantProfit(String name, double profit) {
         System.out.printf("%s: %.0f", name, profit);
         displayEmptyLine();
-    }
-
-    private List<String> getCardNotations(List<Card> cards) {
-        return cards.stream()
-                .map(Card::getNotation)
-                .toList();
     }
 
     public void displayEmptyLine() {
