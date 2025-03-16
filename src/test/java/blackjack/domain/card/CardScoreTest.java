4794cc43d9eb2c2ac36ee3c7a696a43ffcdc6f09
Mint <audwl03071@gmail.com>
2025-03-16 21:40:55 +0900

    test(CardScore): 테스트 작성

--- src/test/java/blackjack/domain/card/CardScoreTest.java
+++ src/test/java/blackjack/domain/card/CardScoreTest.java
@@ -1,17 +1,49 @@
 package blackjack.domain.card;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CardScoreTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "A, A",
+            "TWO, 2"
+    })
     @DisplayName("이름을 조회한다")
-    void findName() {
+    void findName(final CardScore cardScore, final String score) {
+        // Given
+
+        // When & Then
+        assertThat(cardScore.getName()).isEqualTo(score);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "EIGHT, 8",
+            "A, 1"
+    })
+    @DisplayName("최소 번호를 조회한다")
+    void getMinNumber(final CardScore cardScore, final int expected) {
         // Given
 
         // When
+        assertThat(cardScore.getMinNumber()).isEqualTo(expected);
+    }
 
-        // Then
+    @ParameterizedTest
+    @CsvSource({
+            "EIGHT, 8",
+            "A, 11"
+    })
+    @DisplayName("최대 번호를 조회한다")
+    void getMaxNumber(final CardScore cardScore, final int expected) {
+        // Given
+
+        // When & Then
+        assertThat(cardScore.getMaxNumber()).isEqualTo(expected);
     }
 }
