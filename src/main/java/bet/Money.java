e1646b9c1236d47b81291e61ecda8d7b433d5a62
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-16 23:14:26 +0900

    feat: Money 생성 및 검증 기능 구현

--- src/main/java/bet/Money.java
+++ src/main/java/bet/Money.java
@@ -5,6 +5,25 @@ public class Money {
     private final int value;
 
     public Money(int value) {
+        validate(value);
         this.value = value;
     }
+
+    private void validate(int value) {
+        validateMin(value);
+        validateUnit(value);
+    }
+
+    private void validateMin(int value) {
+        if (value < 1000) {
+            throw new IllegalArgumentException("배팅 금액은 1000원 이상부터 가능합니다.");
+        }
+    }
+
+    private void validateUnit(int value) {
+        if (value % 1000 != 0) {
+            throw new IllegalArgumentException("배팅 금액은 1000원 단위만 가능합니다.");
+        }
+    }
+
 }
