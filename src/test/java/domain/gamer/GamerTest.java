3c1370e4add50d0f3cbc36f3538a995a6391f256
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-03-16 16:31:49 +0900

    test: State 추가 구현에 따른 Gamer 테스트 코드 작성

---
+++ src/test/java/domain/gamer/GamerTest.java
@@ -0,0 +1,42 @@
+package domain.gamer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.card.Card;
+import domain.card.CardRank;
+import domain.card.CardSymbol;
+import domain.state.State;
+import domain.state.started.finished.Blackjack;
+import fixture.BettingFixture;
+import fixture.HandFixture;
+import fixture.NicknameFixture;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class GamerTest {
+
+    public static Stream<Arguments> provideInstancesOfGamer() {
+        return Stream.of(
+                Arguments.of(
+                        new Player(HandFixture.createHand(Card.of(CardSymbol.SPADE, CardRank.ACE),
+                                Card.of(CardSymbol.SPADE, CardRank.JACK)),
+                                NicknameFixture.createNickname("ad"), BettingFixture.createBetting(1000)),
+                        new Dealer(HandFixture.createHand(Card.of(CardSymbol.SPADE, CardRank.ACE),
+                                Card.of(CardSymbol.SPADE, CardRank.JACK)))
+                ));
+    }
+
+    @DisplayName("Gamer는 생성시에 블랙잭이라면 블랙잭 상태를가진다")
+    @ParameterizedTest
+    @MethodSource("provideInstancesOfGamer")
+    void isBlackjack(Gamer gamer) {
+        // when
+        State actual = gamer.state;
+
+        // then
+        assertThat(actual).isInstanceOf(Blackjack.class);
+    }
+}
