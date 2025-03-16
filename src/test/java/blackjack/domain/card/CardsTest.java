c7725dbd6fd91ed69c32c89c6b853d325acbd2a2
junho <wnsgh732@gmail.com>
2025-03-16 18:23:43 +0900

    test: Cards에 대한 테스트 코드 추가

--- src/test/java/blackjack/domain/card/CardsTest.java
+++ src/test/java/blackjack/domain/card/CardsTest.java
@@ -1,10 +1,12 @@
 package blackjack.domain.card;
 
+import blackjack.domain.game.GameScore;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CardsTest {
@@ -31,5 +33,29 @@ class CardsTest {
         ));
         assertThatThrownBy(() -> cards.addAll(List.of(new Card(CardNumber.EIGHT, CardShape.DIAMOND)))
         ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("카드의 합을 반환한다")
+    void testCardSum() {
+        Cards cards = new Cards();
+        cards.addAll(List.of(
+                new Card(CardNumber.JACK, CardShape.DIAMOND),
+                new Card(CardNumber.QUEEN, CardShape.DIAMOND),
+                new Card(CardNumber.KING, CardShape.DIAMOND)
+        ));
+        assertThat(cards.getGameScore()).isEqualTo(new GameScore(30, 3));
+    }
+
+    @DisplayName("에이스가 2장이면 12로 계산한다")
+    @Test
+    void ace_test() {
+        Cards cards = new Cards();
+        cards.addAll(List.of(
+                new Card(CardNumber.ACE, CardShape.DIAMOND),
+                new Card(CardNumber.ACE, CardShape.HEART)
+        ));
+
+        assertThat(cards.getGameScore()).isEqualTo(new GameScore(12, 2));
     }
 }
