fcb340f481bbc3f780c962aeb488c161fe6b816e
GiSeok Kang <ime.rokz@gmail.com>
2025-03-16 21:46:59 +0900

    feat: 배팅 금액 객체 추가
    - 배팅 금액 범위에 대한 validate 추가

---
+++ src/main/java/participant/BettingMoney.java
@@ -0,0 +1,35 @@
+package participant;
+
+public class BettingMoney {
+
+    public static final int MINIMUM_MONEY = 0;
+    public static final int MAXIMUM_MONEY = 100_000;
+
+    private final int money;
+
+    public BettingMoney(final int money) {
+        validateMoney(money);
+        this.money = money;
+    }
+
+    private void validateMoney(int money) {
+        validateMoneyOverZero(money);
+        validateMoneyUnderMaximumMoney(money);
+    }
+
+    private void validateMoneyOverZero(int money) {
+        if (money <= MINIMUM_MONEY) {
+            throw new IllegalArgumentException(String.format("[ERROR] 배팅 금액은 %d원을 초과해야 합니다.", MINIMUM_MONEY));
+        }
+    }
+
+    private void validateMoneyUnderMaximumMoney(int money) {
+        if (money > MAXIMUM_MONEY) {
+            throw new IllegalArgumentException(String.format("[ERROR] 배팅 금액은 %d원을 초과할 수 없습니다.", MAXIMUM_MONEY));
+        }
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}
