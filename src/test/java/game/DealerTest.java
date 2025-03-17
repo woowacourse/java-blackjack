f4ee1e5438427e5ebe3fe1c7f77f0b6238d0a38d
Yeongjae Kim <sees77770@gmail.com>
2025-03-17 15:45:13 +0900

    refactor: 딜러 bust, hit 분기 테스트 케이스 추가

--- src/test/java/game/DealerTest.java
+++ src/test/java/game/DealerTest.java
@@ -1,100 +1,107 @@
 package game;
 
-import static card.CardDeck.DRAW_COUNT_WHEN_HIT;
-import static card.CardDeck.DRAW_COUNT_WHEN_START;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import card.Card;
-import card.CardDeck;
 import card.CardNumber;
 import card.Pattern;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class DealerTest {
 
     @Test
     void 딜러는_게임_시작_시_두_장의_카드를_받는다() {
-        //given
-        CardDeck cardDeck = CardDeck.prepareDeck(ArrayList::new);
         Dealer dealer = new Dealer();
+        dealer.draw(List.of(
+                new Card(Pattern.CLOVER, CardNumber.ACE),
+                new Card(Pattern.DIAMOND, CardNumber.ACE)));
 
-        //when
-        dealer.draw(cardDeck.drawCard(DRAW_COUNT_WHEN_START));
-
-        //then
         assertThat(dealer.getHand().getCards()).hasSize(2);
     }
 
     @Test
-    void 딜러는_게임_시작_시_한_장의_카드를_공개한다() {
-        //given
-        CardDeck cardDeck = CardDeck.prepareDeck(ArrayList::new);
+    void 딜러는_게임_시작_시_첫_장을_공개한다() {
         Dealer dealer = new Dealer();
-        dealer.draw(cardDeck.drawCard(DRAW_COUNT_WHEN_START));
+        dealer.draw(List.of(
+                new Card(Pattern.CLOVER, CardNumber.ACE),
+                new Card(Pattern.DIAMOND, CardNumber.ACE)));
 
-        //when
         Card singleCard = dealer.getSingleCard();
 
-        //then
-        assertThat(singleCard.equals(new Card(Pattern.SPADE, CardNumber.KING))).isTrue();
+        assertThat(singleCard.equals(new Card(Pattern.CLOVER, CardNumber.ACE))).isTrue();
     }
 
     @Test
     void 딜러는_한_장의_카드를_받는다() {
-        //given
-        CardDeck cardDeck = CardDeck.prepareDeck(ArrayList::new);
         Dealer dealer = new Dealer();
 
-        //when
-        dealer.draw(cardDeck.drawCard(DRAW_COUNT_WHEN_HIT));
+        dealer.draw(List.of(new Card(Pattern.CLOVER, CardNumber.ACE)));
 
-        //then
         assertThat(dealer.getHand().getCards()).hasSize(1);
     }
 
     @Test
     void 딜러가_보유한_카드의_합계를_구한다() {
-        //given
         Dealer dealer = new Dealer();
         dealer.draw(List.of(
                 new Card(Pattern.CLOVER, CardNumber.TEN),
-                new Card(Pattern.SPADE, CardNumber.TEN)
-        ));
+                new Card(Pattern.SPADE, CardNumber.TEN)));
 
-        // when
         int actual = dealer.calculateTotalPoints();
 
-        //when & then
         assertThat(actual).isEqualTo(20);
     }
 
-    @Test
-    void 딜러가_보유한_카드의_합계가_21을_넘었는지_판정한다() {
-        //given
-        Dealer dealer = new Dealer();
-        dealer.draw(List.of(
+    @ParameterizedTest
+    @MethodSource(value = "provideDealer")
+    void 딜러가_버스트인지_확인한다(Dealer dealer, boolean expected) {
+        assertThat(dealer.isBust()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideDealer() {
+        Dealer bustDealer = new Dealer();
+        bustDealer.draw(List.of(
                 new Card(Pattern.CLOVER, CardNumber.TEN),
                 new Card(Pattern.SPADE, CardNumber.TEN),
                 new Card(Pattern.SPADE, CardNumber.TWO)
         ));
+        Dealer notBustDealer = new Dealer();
+        notBustDealer.draw(List.of(
+                new Card(Pattern.CLOVER, CardNumber.TEN),
+                new Card(Pattern.SPADE, CardNumber.ACE)
+        ));
 
-        //when & then
-        assertThat(dealer.isBust()).isTrue();
+        return Stream.of(
+                Arguments.of(bustDealer, true),
+                Arguments.of(notBustDealer, false)
+        );
     }
 
     @Test
-    void 딜러가_보유한_카드의_합계가_16을_넘었는지_판정한다() {
-        //given
+    void 딜러가_보유한_카드의_합계가_16을_넘었는지_판정한다_16초과() {
         Dealer dealer = new Dealer();
         dealer.draw(List.of(
                 new Card(Pattern.CLOVER, CardNumber.TEN),
                 new Card(Pattern.SPADE, CardNumber.SEVEN)
         ));
 
-        //when & then
         assertThat(dealer.isOverDrawBound()).isTrue();
     }
-    
+
+    @Test
+    void 딜러가_보유한_카드의_합계가_16을_넘었는지_판정한다_16이하() {
+        Dealer dealer = new Dealer();
+        dealer.draw(List.of(
+                new Card(Pattern.CLOVER, CardNumber.TEN),
+                new Card(Pattern.SPADE, CardNumber.SIX)
+        ));
+
+        assertThat(dealer.isOverDrawBound()).isFalse();
+    }
+
 }
