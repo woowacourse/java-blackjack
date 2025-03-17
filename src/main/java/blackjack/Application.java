1107be94bde7d20d7ba17247c02152b2fcda51fc
taek2222 <oot309@daum.net>
2025-03-17 15:35:17 +0900

    refactor: console 의존성 주입 형태로 변경

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -5,11 +5,15 @@ import blackjack.controller.GameManagerFactory;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
 
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         OutputView outputView = new OutputView();
-        InputView inputView = new InputView();
+        InputView inputView = new InputView(scanner);
         GameManagerFactory gameManagerFactory = new GameManagerFactory();
 
         BlackjackController controller = new BlackjackController(
--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -7,19 +7,23 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String NEW_LINE = System.lineSeparator();
-    private static final Scanner CONSOLE = new Scanner(System.in);
-
     private static final String YES = "y";
     private static final String NO = "n";
 
+    private final Scanner console;
+
+    public InputView(final Scanner console) {
+        this.console = console;
+    }
+
     public String readGamblerNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return CONSOLE.nextLine();
+        return console.nextLine();
     }
 
     public int readBetAmount(final String name) {
         System.out.printf(NEW_LINE + "%s의 배팅 금액은?" + NEW_LINE, name);
-        String inputBatAmount = CONSOLE.nextLine();
+        String inputBatAmount = console.nextLine();
         validateIsNumeric(inputBatAmount);
 
         return Integer.parseInt(inputBatAmount);
@@ -27,7 +31,7 @@ public class InputView {
 
     public boolean readOneMoreDealCard(final Player player) {
         System.out.println(player.getName().getName() + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
-        String input = CONSOLE.nextLine().toLowerCase();
+        String input = console.nextLine().toLowerCase();
         validateYesOrNo(input);
 
         return input.equals(YES);
