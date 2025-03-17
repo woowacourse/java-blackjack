a3876afb97c5c3e0bb09bf24a9084f24c078cf8d
2jin2.1031 <2jin2.1031@gmail.com>
2025-03-17 20:45:24 +0900

    refactor: 메서드 순서 조정

--- src/main/java/model/participant/Players.java
+++ src/main/java/model/participant/Players.java
@@ -14,15 +14,15 @@ public class Players {
         this.values = values;
     }
 
-    private void validateDuplication(List<Player> values) {
-        if (values.stream().distinct().count() != values.size()) {
-            throw new IllegalArgumentException("중복된 플레이어는 등록할 수 없습니다.");
-        }
-    }
-
     private void validateNumber(List<Player> values) {
         if (values.isEmpty() || values.size() > 30) {
             throw new IllegalArgumentException("게임 참가자는 1~30명까지 가능합니다.");
+        }
+    }
+
+    private void validateDuplication(List<Player> values) {
+        if (values.stream().distinct().count() != values.size()) {
+            throw new IllegalArgumentException("중복된 플레이어는 등록할 수 없습니다.");
         }
     }
 
