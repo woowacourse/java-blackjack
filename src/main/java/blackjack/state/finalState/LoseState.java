fc73612260065380939d01fa23ee2143d64302be
MinJun Choi <mj043000@naver.com>
2025-03-17 14:10:27 +0900

    refactor: 개행 추가

--- src/main/java/blackjack/state/finalState/LoseState.java
+++ src/main/java/blackjack/state/finalState/LoseState.java
@@ -5,6 +5,7 @@ import blackjack.gamer.Player;
 import blackjack.state.State;
 
 public class LoseState implements State, FinalState {
+
     @Override
     public void checkState(final Player player, final Dealer dealer) {
         calculateEarnedMoney(player);
