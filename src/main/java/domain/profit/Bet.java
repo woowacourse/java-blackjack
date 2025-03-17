15705ae2f4ad56d17e21d82d0a070b392ad7b99b
threepebbles <threepebbles@naver.com>
2025-03-17 19:05:55 +0900

    remove: 사용하지 않는 메서드 제거

--- src/main/java/domain/profit/Bet.java
+++ src/main/java/domain/profit/Bet.java
@@ -4,14 +4,9 @@ public record Bet(int value) {
 
     public static final int MIN_BET = 1_000;
     public static final int MAX_BET = 1_000_000;
-    private static final Bet DEFAULT_BET = new Bet(MIN_BET);
 
     public Bet {
         validateRange(value);
-    }
-
-    public static Bet defaultBet() {
-        return DEFAULT_BET;
     }
 
     private void validateRange(int value) {
