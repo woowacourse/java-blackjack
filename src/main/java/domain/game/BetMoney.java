2d444e20629dd8733c728c54e1cac6cdf108c339
geonwoo <kysub99@naver.com>
2025-03-17 14:28:31 +0900

    refactor(BetMoney): 배팅 금액 1000원 단위 추가

--- src/main/java/domain/game/BetMoney.java
+++ src/main/java/domain/game/BetMoney.java
@@ -12,6 +12,9 @@ public class BetMoney {
         if (amount <= 0) {
             throw new IllegalArgumentException("배팅 금액은 0 이나 음수가 될 수 없습니다");
         }
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("배팅 금액은 1000원 단위로 입력해주세요.");
+        }
         if (amount > 100_000_000) {
             throw new IllegalArgumentException("배팅 금액은 1억원을 넘을 수 없습니다.");
         }
