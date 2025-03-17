aa0829a592ef9baa5ca849c45c4a11fdcf0a7d21
BWY <quantumjump10@gmail.com>
2025-03-17 17:30:02 +0900

    refactor: Dealer와 Player가 Participant를 상속하도록 수정

--- src/main/java/participant/Dealer.java
+++ src/main/java/participant/Dealer.java
@@ -1,23 +1,16 @@
 package participant;
 
 import game.Card;
-import game.Cards;
 import game.Deck;
-import strategy.setting.DeckSettingStrategy;
 
 import java.util.Comparator;
-import java.util.List;
 
-public class Dealer {
+public class Dealer extends Participant {
 
     private static final int DEALER_HIT_THRESHOLD = 16;
 
-    private final Deck deck;
-    private final Cards cards;
-
-    public Dealer(DeckSettingStrategy strategy) {
-        this.deck = new Deck(strategy);
-        this.cards = deck.drawInitialCards();
+    public Dealer(Deck deck) {
+        super(deck);
     }
 
     public Card openOneCard() {
@@ -26,31 +19,7 @@ public class Dealer {
                 .orElse(cards.getCards().getFirst());
     }
 
-    public List<Card> openCards() {
-        return cards.getCards();
-    }
-
     public boolean shouldDealerHit() {
         return sumCardNumbers() <= DEALER_HIT_THRESHOLD;
-    }
-
-    public boolean addOneCard(Card card) {
-        return cards.addOneCard(card);
-    }
-
-    public Cards drawInitialCards() {
-        return deck.drawInitialCards();
-    }
-
-    public Card drawCard() {
-        return deck.drawOneCard();
-    }
-
-    public int sumCardNumbers() {
-        return cards.sumCardNumbers();
-    }
-
-    public boolean isBlackJack() {
-        return cards.isBlackJack();
     }
 }
--- src/main/java/participant/Player.java
+++ src/main/java/participant/Player.java
@@ -1,30 +1,15 @@
 package participant;
 
-import game.Card;
-import game.Cards;
 import constant.WinningResult;
+import game.Deck;
 
-import java.util.List;
-
-public class Player {
-
-    private static final int INITIAL_CARD_COUNT = 2;
+public class Player extends Participant {
 
     private final Nickname nickname;
-    private final Cards cards;
 
-    public Player(Nickname nickname, Cards cards) {
-        validateInitialCardsSize(cards);
+    public Player(Nickname nickname, Deck deck) {
+        super(deck);
         this.nickname = nickname;
-        this.cards = cards;
-    }
-
-    public boolean addOneCard(Card card) {
-        return cards.addOneCard(card);
-    }
-
-    public int sumCardNumbers() {
-        return cards.sumCardNumbers();
     }
 
     public WinningResult compareTo(int dealerScore) {
@@ -32,21 +17,7 @@ public class Player {
         return WinningResult.getWinningResult(playerScore, dealerScore);
     }
 
-    public List<Card> openCards() {
-        return cards.getCards();
-    }
-
-    private void validateInitialCardsSize(Cards cards) {
-        if (cards.getSize() != INITIAL_CARD_COUNT) {
-            throw new IllegalArgumentException("[ERROR] 초기 카드는 " + INITIAL_CARD_COUNT + "장을 받아야 합니다.");
-        }
-    }
-
     public String getNickname() {
         return nickname.getValue();
-    }
-
-    public boolean isBlackJack() {
-        return cards.isBlackJack();
     }
 }
