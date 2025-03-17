69af8a8caa43da68c339d073b085d6023d627e62
threepebbles <threepebbles@naver.com>
2025-03-17 15:33:55 +0900

    feat: 유저 이름 길이 검증 기능 추가(최소 2글자, 최대 10글자)

--- src/main/java/domain/player/User.java
+++ src/main/java/domain/player/User.java
@@ -6,6 +6,9 @@ import domain.state.State;
 
 public class User extends Player {
 
+    private static final int MIN_NAME_LENGTH = 2;
+    private static final int MAX_NAME_LENGTH = 10;
+
     private final Bet bet;
 
     private User(String name, Bet bet, State state) {
@@ -19,8 +22,20 @@ public class User extends Player {
     }
 
     private void validateName(String name) {
+        validateEqualToDealerName(name);
+        validateNameLength(name);
+    }
+
+    private void validateEqualToDealerName(String name) {
         if (Dealer.DEALER_NAME.equals(name)) {
             throw new IllegalArgumentException("딜러와 이름이 동일할 수 없습니다.");
+        }
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(
+                    name.length() + ": 이름의 길이는 " + MIN_NAME_LENGTH + "이상 " + MAX_NAME_LENGTH + "이하만 가능합니다.");
         }
     }
 
--- src/test/java/domain/player/UserTest.java
+++ src/test/java/domain/player/UserTest.java
@@ -2,6 +2,8 @@ package domain.player;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class UserTest {
 
@@ -10,6 +12,22 @@ class UserTest {
         // given
         String name = Dealer.DEALER_NAME;
 
+        // when & then
+        Assertions.assertThatThrownBy(() -> User.of(name, 1000))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "a"})
+    void 이름의_길이가_2미만인_경우_예외가_발생한다(String name) {
+        // when & then
+        Assertions.assertThatThrownBy(() -> User.of(name, 1000))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"12345678901", "itsnotpossiblename"})
+    void 이름의_길이가_10글자_초과인_경우_예외가_발생한다(String name) {
         // when & then
         Assertions.assertThatThrownBy(() -> User.of(name, 1000))
                 .isInstanceOf(IllegalArgumentException.class);
