b8df38a9ff991d2f0831e46b30d41ea78b649ec9
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-03-17 16:33:19 +0900

    feat : BettingMoney 검증 로직 구현

--- src/main/java/domain/BettingMoney.java
+++ src/main/java/domain/BettingMoney.java
@@ -1,13 +1,34 @@
 package domain;
 
+import static util.ExceptionConstants.ERROR_HEADER;
+
 import java.math.BigDecimal;
 
 public record BettingMoney(
         int bettingMoney
 ) {
 
+    private static final int BETTING_UNIT = 1000;
+
+    public BettingMoney {
+        validateNegative(bettingMoney);
+        validateUnit(bettingMoney);
+    }
+
     public Profit calculateProfit(GameResultStatus gameResultStatus) {
         BigDecimal profitValue = gameResultStatus.calculateProfit(bettingMoney);
         return new Profit(profitValue);
+    }
+
+    private void validateNegative(int bettingMoney) {
+        if (bettingMoney < 0) {
+            throw new IllegalArgumentException(ERROR_HEADER + "베팅 금액은 음수일 수 없습니다.");
+        }
+    }
+
+    private void validateUnit(int bettingMoney) {
+        if (bettingMoney % BETTING_UNIT != 0) {
+            throw new IllegalArgumentException(ERROR_HEADER + "베팅 금액은 1000원 단위여야 합니다.");
+        }
     }
 }
---
+++ src/test/java/domain/BettingMoneyTest.java
@@ -0,0 +1,52 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static util.ExceptionConstants.ERROR_HEADER;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BettingMoneyTest {
+
+    @DisplayName("게임 결과에 따른 수익을 반환한다")
+    @Test
+    void test() {
+        // given
+        BettingMoney bettingMoney = new BettingMoney(10000);
+        GameResultStatus gameResultStatus = GameResultStatus.WIN;
+
+        // when
+        Profit profit = bettingMoney.calculateProfit(gameResultStatus);
+
+        // then
+        assertThat(profit).isEqualTo(new Profit(BigDecimal.valueOf(10000)));
+    }
+
+    @DisplayName("베팅 금액이 음수라면 예외를 발생시킨다")
+    @Test
+    void test2() {
+        // given
+        int bettingMoney = -10000;
+
+        // when & then
+        assertThatThrownBy(() -> {
+            new BettingMoney(bettingMoney);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_HEADER + "베팅 금액은 음수일 수 없습니다.");
+    }
+
+    @DisplayName("베팅 금액이 1000원 단위가 아니라면 예외를 발생시킨다")
+    @Test
+    void test3() {
+        // given
+        int bettingMoney = 10300;
+
+        // when & then
+        assertThatThrownBy(() -> {
+            new BettingMoney(bettingMoney);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_HEADER + "베팅 금액은 1000원 단위여야 합니다.");
+    }
+}
