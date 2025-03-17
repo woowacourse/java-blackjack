40502a9b94e10b4f0e0ffdd5a6ff73d480d88e44
threepebbles <threepebbles@naver.com>
2025-03-17 14:01:30 +0900

    refactor: 메서드 분리

--- src/main/java/domain/profit/UserBattleResult.java
+++ src/main/java/domain/profit/UserBattleResult.java
@@ -12,18 +12,41 @@ public enum UserBattleResult {
     ;
 
     public static UserBattleResult compare(State user, State dealer) {
-        if (user.type() == StateType.BUST) {
-            return UserBattleResult.LOSE;
-        } else if (dealer.type() == StateType.BUST) {
-            return UserBattleResult.NORMAL_WIN;
+        if (blackjackExists(user, dealer)) {
+            return determineByBlackjack(user, dealer);
         }
-
-        if (user.type() == StateType.BLACKJACK && dealer.type() == StateType.BLACKJACK) {
-            return UserBattleResult.DRAW;
-        } else if (user.type() == StateType.BLACKJACK) {
-            return UserBattleResult.BLACKJACK_WIN;
+        if (bustExists(user, dealer)) {
+            return determineByBust(user, dealer);
         }
         return compareBySum(user.cards().computeOptimalSum(), dealer.cards().computeOptimalSum());
+    }
+
+    private static boolean blackjackExists(State user, State dealer) {
+        return user.type() == StateType.BLACKJACK || dealer.type() == StateType.BLACKJACK;
+    }
+
+    private static boolean bustExists(State user, State dealer) {
+        return user.type() == StateType.BUST || dealer.type() == StateType.BUST;
+    }
+
+    private static UserBattleResult determineByBlackjack(State user, State dealer) {
+        if (user.type() == StateType.BLACKJACK && dealer.type() == StateType.BLACKJACK) {
+            return UserBattleResult.DRAW;
+        }
+        if (user.type() == StateType.BLACKJACK) {
+            return UserBattleResult.BLACKJACK_WIN;
+        }
+        throw new IllegalStateException("블랙잭 상태 여부로 승부를 결정할 수 없습니다.");
+    }
+
+    private static UserBattleResult determineByBust(State user, State dealer) {
+        if (user.type() == StateType.BUST) {
+            return UserBattleResult.LOSE;
+        }
+        if (dealer.type() == StateType.BUST) {
+            return UserBattleResult.NORMAL_WIN;
+        }
+        throw new IllegalStateException("버스트 상태 여부로 승부를 결정할 수 없습니다.");
     }
 
     private static UserBattleResult compareBySum(int sum1, int sum2) {
