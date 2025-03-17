fe293377add9834ec5a02351e0938d2506b66a0c
CheChe903 <cheche903@naver.com>
2025-03-17 14:10:36 +0900

    refactor(FinalResult): BlackJackRules FinalResult로 이전
    - 응집도 향상을 위해 하나의 객체로 합성합니다.

--- src/main/java/domain/FinalResult.java
+++ src/main/java/domain/FinalResult.java
@@ -1,9 +1,9 @@
 package domain;
 
-import static domain.BlackJackRules.RULES;
-
 import domain.gamer.Dealer;
 import domain.gamer.Player;
+import java.util.Arrays;
+import java.util.List;
 
 public enum FinalResult {
     WIN,
@@ -17,4 +17,16 @@ public enum FinalResult {
                 .map(Rule::getFinalResult)
                 .orElse(DRAW);
     }
+
+    private static final List<Rule> RULES = Arrays.asList(
+            new Rule((player, dealer) -> player.isBlackJack() && dealer.isBlackJack(), DRAW),
+            new Rule((player, dealer) -> player.isBlackJack(), WIN),
+            new Rule((player, dealer) -> dealer.isBlackJack(), LOSE),
+            new Rule((player, dealer) -> player.isBust() && dealer.isBust(), LOSE),
+            new Rule((player, dealer) -> player.isBust(), LOSE),
+            new Rule((player, dealer) -> dealer.isBust(), WIN),
+            new Rule((player, dealer) -> player.getSumOfRank() > dealer.getSumOfRank(), WIN),
+            new Rule((player, dealer) -> player.getSumOfRank() < dealer.getSumOfRank(), LOSE),
+            new Rule((player, dealer) -> player.getSumOfRank() == dealer.getSumOfRank(), DRAW)
+    );
 }
