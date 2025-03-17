b66b49aaf8c72298bdf6c59d6d3edb120886dbee
ryan-dia <cjfdnjs158@naver.com>
2025-03-17 17:20:55 +0900

    feat: 베팅 도메인 추가
    Co-authored-by:CheChe903<cheche903@naver.com>

---
+++ src/main/java/domain/gamer/BetAmount.java
@@ -0,0 +1,37 @@
+package domain.gamer;
+
+import domain.profit.Outcome;
+import domain.profit.Profit;
+
+public class BetAmount {
+
+    private final long betAmount;
+
+    private BetAmount(final long betAmount) {
+        validateBetAmount(betAmount);
+        this.betAmount = betAmount;
+    }
+
+    public static BetAmount of(final String betAmount) {
+        try {
+            return new BetAmount(Long.parseLong(betAmount));
+        } catch (final NumberFormatException e) {
+            throw new IllegalArgumentException("베팅 금액은 숫자로만 작성해주세요.");
+        }
+    }
+
+    private void validateBetAmount(final long betAmount) {
+        if (betAmount > 5_000_000_000L || betAmount < 10_000) {
+            throw new IllegalArgumentException("배팅금액은 1만원 ~ 50억까지만 가능합니다.");
+        }
+    }
+
+    public Double calculateProfit(final Outcome outcome) {
+        final Profit profit = Profit.findByOutcome(outcome);
+        return profit.calculate(betAmount);
+    }
+
+    public long getBetAmount() {
+        return betAmount;
+    }
+}
