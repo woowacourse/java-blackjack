be4cbf41e346e742958c7a6d2685739d1d4b8c13
soeun2537 <soeun2537@gmail.com>
2025-03-17 15:16:24 +0900

    test: CardShape, CardNumber가 같을 경우, 다를 경우 boolean을 반환하는 단위 테스트 추가

--- src/test/java/card/CardTest.java
+++ src/test/java/card/CardTest.java
@@ -8,26 +8,52 @@ import org.junit.jupiter.api.Test;
 class CardTest {
 
     @Test
-    @DisplayName("ACE 카드라면 true를 반환한다.")
+    @DisplayName("CardShape가 같다면 true를 반환한다.")
     void test1() {
         // given
-        Card aceCard = new Card(CardShape.SPADE, CardNumber.ACE);
+        Card card = new Card(CardShape.SPADE, CardNumber.ACE);
 
         // when
-        boolean result = aceCard.isAce();
+        boolean result = card.isSameCardShape(CardShape.SPADE);
 
         // then
         assertThat(result).isTrue();
     }
 
     @Test
-    @DisplayName("ACE 카드가 아니라면 false를 반환한다.")
+    @DisplayName("CardShape가 다르다면 false를 반환한다.")
     void test2() {
         // given
-        Card aceCard = new Card(CardShape.SPADE, CardNumber.THREE);
+        Card card = new Card(CardShape.SPADE, CardNumber.ACE);
 
         // when
-        boolean result = aceCard.isAce();
+        boolean result = card.isSameCardShape(CardShape.HEART);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("CardNumber가 같다면 true를 반환한다.")
+    void test3() {
+        // given
+        Card card = new Card(CardShape.SPADE, CardNumber.ACE);
+
+        // when
+        boolean result = card.isSameCardNumber(CardNumber.ACE);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("CardNumber가 다르다면 false를 반환한다.")
+    void test4() {
+        // given
+        Card card = new Card(CardShape.SPADE, CardNumber.ACE);
+
+        // when
+        boolean result = card.isSameCardNumber(CardNumber.KING);
 
         // then
         assertThat(result).isFalse();
