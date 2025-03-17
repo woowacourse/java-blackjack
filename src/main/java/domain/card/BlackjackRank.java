b4fe87b79ab00b7ea6b3e116aadd9cb5f9c4d42e
threepebbles <threepebbles@naver.com>
2025-03-17 19:25:37 +0900

    refactor: 카드 숫자(Rank)에 따른 점수 목록 관리를 BlackjackRank enum 클래스에서 하도록 수정

---
+++ src/main/java/domain/card/BlackjackRank.java
@@ -0,0 +1,36 @@
+package domain.card;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum BlackjackRank {
+    ACE(Rank.ACE, List.of(1, 11)),
+    TWO(Rank.TWO, List.of(2)),
+    THREE(Rank.THREE, List.of(3)),
+    FOUR(Rank.FOUR, List.of(4)),
+    FIVE(Rank.FIVE, List.of(5)),
+    SIX(Rank.SIX, List.of(6)),
+    SEVEN(Rank.SEVEN, List.of(7)),
+    EIGHT(Rank.EIGHT, List.of(8)),
+    NINE(Rank.NINE, List.of(9)),
+    TEN(Rank.TEN, List.of(10)),
+    JACK(Rank.JACK, List.of(10)),
+    QUEEN(Rank.QUEEN, List.of(10)),
+    KING(Rank.KING, List.of(10));
+
+    private final Rank rank;
+    private final List<Integer> scores;
+
+    BlackjackRank(Rank rank, List<Integer> scores) {
+        this.rank = rank;
+        this.scores = scores;
+    }
+
+    public static List<Integer> getScoresByRank(Rank rank) {
+        return Arrays.stream(values())
+                .filter(blackjackRank -> blackjackRank.rank == rank)
+                .map(blackjackRank -> blackjackRank.scores)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(rank + ": 존재하지 않는 랭크입니다."));
+    }
+}
--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -1,25 +1,8 @@
 package domain.card;
 
 import java.util.List;
-import java.util.Map;
 
 public class Card extends TrumpCard {
-
-    private static final Map<Rank, List<Integer>> SCORES_BY_RANK = Map.ofEntries(
-            Map.entry(Rank.ACE, List.of(1, 11)),
-            Map.entry(Rank.TWO, List.of(2)),
-            Map.entry(Rank.THREE, List.of(3)),
-            Map.entry(Rank.FOUR, List.of(4)),
-            Map.entry(Rank.FIVE, List.of(5)),
-            Map.entry(Rank.SIX, List.of(6)),
-            Map.entry(Rank.SEVEN, List.of(7)),
-            Map.entry(Rank.EIGHT, List.of(8)),
-            Map.entry(Rank.NINE, List.of(9)),
-            Map.entry(Rank.TEN, List.of(10)),
-            Map.entry(Rank.JACK, List.of(10)),
-            Map.entry(Rank.QUEEN, List.of(10)),
-            Map.entry(Rank.KING, List.of(10))
-    );
 
     private boolean isOpened;
 
@@ -37,6 +20,6 @@ public class Card extends TrumpCard {
     }
 
     public List<Integer> scores() {
-        return SCORES_BY_RANK.get(rank);
+        return BlackjackRank.getScoresByRank(rank);
     }
 }
