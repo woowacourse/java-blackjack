c780fd3cbe3c2d37d0b89854ebaeadc09866a687
songsunkook <songsunkook@gmail.com>
2025-03-17 15:47:14 +0900

    refactor: 누락된 상수화 적용

--- src/main/java/blackjack/domain/BettingAmount.java
+++ src/main/java/blackjack/domain/BettingAmount.java
@@ -5,12 +5,15 @@ public record BettingAmount(
     double end
 ) {
 
+    public static final double WINNER_MULTIPLE = 2;
+    public static final double BLACKJACK_MULTIPLE = 1.5;
+
     public static BettingAmount of(double money) {
         return new BettingAmount(money, 0);
     }
 
     public BettingAmount blackjack() {
-        return new BettingAmount(initial, initial * 1.5);
+        return new BettingAmount(initial, initial * BLACKJACK_MULTIPLE);
     }
 
     public BettingAmount draw() {
@@ -18,7 +21,7 @@ public record BettingAmount(
     }
 
     public BettingAmount win() {
-        return new BettingAmount(initial, initial * 2);
+        return new BettingAmount(initial, initial * WINNER_MULTIPLE);
     }
 
     public double getProfit() {
