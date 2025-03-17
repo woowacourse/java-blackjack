bb37b95de95ebd9baacd52e15409beafa7e0ab20
Yeongjae Kim <sees77770@gmail.com>
2025-03-17 16:19:48 +0900

    refactor: 블랙잭 게임 진행 메서드 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -14,10 +14,22 @@ import view.OutputView;
 
 public class Application {
 
+    private record BlackjackContext(Dealer dealer, Players players, CardDeck cardDeck) {
+    }
+
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
 
+        BlackjackContext blackjackContext = initializeBlackjack(inputView);
+        outputView.printInitialGame(blackjackContext.dealer.getSingleCard(), blackjackContext.players.getPlayers());
+
+        handlePlayerDraws(inputView, outputView, blackjackContext.players, blackjackContext.cardDeck);
+        handleDealerDraw(outputView, blackjackContext.dealer, blackjackContext.cardDeck);
+        processResults(outputView, blackjackContext.dealer, blackjackContext.players);
+    }
+
+    private static BlackjackContext initializeBlackjack(InputView inputView) {
         CardDeck cardDeck = CardDeck.prepareDeck(new RandomCardShuffler());
         Dealer dealer = new Dealer();
 
@@ -27,23 +39,31 @@ public class Application {
 
         dealer.draw(cardDeck.drawCard(DRAW_COUNT_WHEN_START));
         players.draw(cardDeck);
-        outputView.printInitialGame(dealer.getSingleCard(), players.getPlayers());
 
+        return new BlackjackContext(dealer, players, cardDeck);
+    }
+
+    private static void handlePlayerDraws(InputView inputView, OutputView outputView, Players players,
+                                          CardDeck cardDeck) {
         for (Player player : players.getPlayers()) {
             while (!player.isBust() && inputView.readDrawMoreCard(player)) {
                 player.draw(cardDeck.drawCard(DRAW_COUNT_WHEN_HIT));
                 outputView.printPlayerDraw(player);
             }
         }
+    }
+
+    private static void handleDealerDraw(OutputView outputView, Dealer dealer, CardDeck cardDeck) {
         while (!dealer.isBust() && !dealer.isOverDrawBound()) {
             dealer.draw(cardDeck.drawCard(DRAW_COUNT_WHEN_HIT));
             outputView.printDealerDrawMessage();
         }
-        outputView.printHitProcess(dealer, players);
+    }
 
+    private static void processResults(OutputView outputView, Dealer dealer, Players players) {
         GameResults gameResults = GameResults.determine(dealer, players.getPlayers());
-        Profits profits = Profits.evaluateProfits(players.getBettingMoneys(), gameResults.getGameResults());
 
+        Profits profits = Profits.evaluateProfits(players.getBettingMoneys(), gameResults.getGameResults());
         int dealerProfit = profits.evaluateDealerProfit();
 
         outputView.printDealerProfit(dealerProfit);
