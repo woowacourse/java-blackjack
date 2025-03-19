3998e14f5ea99492ae0d730eaecf951c439b4998
Mint <audwl03071@gmail.com>
2025-03-20 00:07:58 +0900

    refactor: 주석 제거

--- src/main/java/blackjack/util/ExceptionMessage.java
+++ src/main/java/blackjack/util/ExceptionMessage.java
@@ -4,10 +4,6 @@ public class ExceptionMessage {
 
     private static final String FORMAT = "[ERROR] %s";
 
-//    public static String makeMessage(final String input) {
-//        return String.format(FORMAT, input);
-//    }
-
     public static String makeMessage(final String input, final Object... args) {
         String message = String.format(input, args);
         return String.format(FORMAT, message);
