6a3e5e6bebebca0f7413e729c1e11975063ee3d9
AppleMint98 <applemint9821@gmail.com>
2025-03-20 01:51:01 +0900

    refactor: 블랙잭 승패계산로직 Dealer와 분리

--- src/main/java/blackjack/gametable/gambler/Dealer.java
+++ src/main/java/blackjack/gametable/gambler/Dealer.java
@@ -1,13 +1,12 @@
 package blackjack.gametable.gambler;
 
 import blackjack.constant.MatchResult;
+import blackjack.gametable.BlackjackRule;
 import blackjack.gametable.card.Card;
-import blackjack.gametable.card.Cards;
 import java.util.List;
 
 public class Dealer extends Gambler {
 
-    public static final int MAX_SCORE = 21;
     public static final int DEALER_HIT_THRESHOLD = 16;
 
     @Override
@@ -19,8 +18,10 @@ public class Dealer extends Gambler {
         double dealerAmount = 0;
 
         for (Player player : players.getPlayers()) {
-            MatchResult matchResult = compareTo(player);
-            dealerAmount += handleMatchResult(matchResult, player);
+            MatchResult matchResult = BlackjackRule.evaluate(this, player);
+            double profit = calculateProfit(matchResult, player.getBetAmount());
+            player.updateBetAmount(profit);
+            dealerAmount -= profit;
         }
 
         super.updateBetAmount(dealerAmount);
@@ -30,54 +31,8 @@ public class Dealer extends Gambler {
         return sumCardScores() <= DEALER_HIT_THRESHOLD;
     }
 
-    private MatchResult compareTo(Player player) {
-        int dealerScore = sumCardScores();
-        int playerScore = player.sumCardScores();
-        Cards playerCards = player.getCards();
-
-        if (cards.isBlackjack() || playerCards.isBlackjack()) {
-            return calculateMatchResultWhenBlackjack(playerCards);
-        }
-
-        if (dealerScore > MAX_SCORE || playerScore > MAX_SCORE) {
-            return calculateMatchResultWhenOverBustStandard(playerScore);
-        }
-        return calculateMatchResult(dealerScore, playerScore);
-    }
-
-    private double handleMatchResult(MatchResult matchResult, Player player) {
-        double betAmount = player.getBetAmount();
-        double profit = matchResult.calculatePayout(betAmount);
-        player.updateBetAmount(profit);
-
-        return -profit;
-    }
-
-    private MatchResult calculateMatchResultWhenBlackjack(Cards playerCards) {
-        if (playerCards.isBlackjack() && cards.isBlackjack()) {
-            return MatchResult.PUSH;
-        }
-        if (playerCards.isBlackjack()) {
-            return MatchResult.BLACKJACK_WIN;
-        }
-        return MatchResult.LOSE;
-    }
-
-    private MatchResult calculateMatchResultWhenOverBustStandard(int playerScore) {
-        if (playerScore > MAX_SCORE) {
-            return MatchResult.LOSE;
-        }
-        return MatchResult.WIN;
-    }
-
-    private MatchResult calculateMatchResult(int dealerScore, int playerScore) {
-        if (playerScore == dealerScore) {
-            return MatchResult.PUSH;
-        }
-        if (playerScore > dealerScore) {
-            return MatchResult.WIN;
-        }
-        return MatchResult.LOSE;
+    private double calculateProfit(MatchResult matchResult, double betAmount) {
+        return matchResult.calculatePayout(betAmount);
     }
 
 }
