efe5242135d13266deef5506451dbf5b03397902
byesol <bsj5967@naver.com>
2025-03-20 20:58:42 +0900

    refactor: static 메소드 리팩토링

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -12,11 +12,11 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
-    public static void outputFirstCardDistributionResult(Players players, Dealer dealer) {
+    public void outputFirstCardDistributionResult(Players players, Dealer dealer) {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder();
         customStringBuilder.appendLine(String.format(
                 "딜러와 %s에게 2장을 나누었습니다.",
-                players.getParticipants().stream()
+                players.getPlayers().stream()
                         .map(Player::getName)
                         .collect(Collectors.joining(", "))
         ));
@@ -24,7 +24,7 @@ public class OutputView {
                 "딜러",
                 generateCardName(dealer.getReceivedCards().getFirstCard()))
         );
-        players.getParticipants().forEach(participant ->
+        players.getPlayers().forEach(participant ->
                 customStringBuilder.appendLine(outputPlayerCardStatus(
                         participant.getName(),
                         generateCardNames(participant.getReceivedCards())
@@ -32,7 +32,7 @@ public class OutputView {
         customStringBuilder.print();
     }
 
-    public static String generateCardNames(ReceivedCards receivedCards) {
+    public String generateCardNames(ReceivedCards receivedCards) {
         return receivedCards.stream()
                 .map(OutputView::generateCardName)
                 .collect(Collectors.joining(", "));
@@ -42,19 +42,19 @@ public class OutputView {
         return String.format("%s%s", card.getCardType().getDetail(), card.getShape().getDetail());
     }
 
-    public static void printPlayerCardStatus(String name, Participant participant) {
+    public void printPlayerCardStatus(String name, Participant participant) {
         System.out.println(outputPlayerCardStatus(name, generateCardNames(participant.getReceivedCards())));
     }
 
-    public static String outputPlayerCardStatus(String name, String cards) {
+    public String outputPlayerCardStatus(String name, String cards) {
         return String.format("%s카드: %s", name, cards);
     }
 
-    public static void outputFinalCardStatus(Dealer dealer, Players players) {
+    public void outputFinalCardStatus(Dealer dealer, Players players) {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder();
         customStringBuilder.appendLine(String.format("%s - 결과 : %d",
                 outputPlayerCardStatus("딜러", generateCardNames(dealer.getReceivedCards())), dealer.calculatePoint()));
-        players.getParticipants()
+        players.getPlayers()
                 .forEach(participant -> customStringBuilder.appendLine(String.format("%s - 결과 : %d",
                         outputPlayerCardStatus(participant.getName(),
                                 generateCardNames(participant.getReceivedCards())),
@@ -62,24 +62,24 @@ public class OutputView {
         customStringBuilder.print();
     }
 
-    public static void outputDealerGetCard() {
+    public void outputDealerGetCard() {
         System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
     }
 
-    public static void outputDealerCardFinish() {
+    public void outputDealerCardFinish() {
         System.out.println("딜러는 17이상이라 더이상 카드를 받을 수 없습니다.");
     }
 
-    public static void printParticipantBust(String name) {
+    public void printParticipantBust(String name) {
         System.out.println(String.format("%s는 bust입니다.", name));
     }
 
-    public static void outputFinalProfit(Map<Player, Integer> bettingResult, int dealerResult) {
+    public void outputFinalProfit(Map<Player, Double> bettingResult, int dealerResult) {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder();
         customStringBuilder.appendLine("## 최종 수익");
         customStringBuilder.appendLine(String.format("딜러 : %d", dealerResult));
-        for (Entry<Player, Integer> entry : bettingResult.entrySet()) {
-            customStringBuilder.appendLine(String.format("%s : %d원", entry.getKey().getName(), entry.getValue()));
+        for (Entry<Player, Double> entry : bettingResult.entrySet()) {
+            customStringBuilder.appendLine(String.format("%s : %f.0원", entry.getKey().getName(), entry.getValue()));
         }
         customStringBuilder.print();
     }
