7e76b22261ca22f42318091847064b5759980e42
민경윤 <ruddbs3308@naver.com>
2025-03-20 20:13:33 +0900

    refactor(BettingMoney): 베팅 금액을 포장하는 클래스 추가

---
+++ src/test/java/model/bet/BettingMoneyTest.java
@@ -0,0 +1,27 @@
+package model.bet;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import exception.IllegalBlackjackInputException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BettingMoneyTest {
+
+    @DisplayName("베팅 머니가 1000원 미만이거나 100만원 초과라면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {999, -1000, 1_000_001})
+    void validateBettingMoneyRange(final int money) {
+        assertThatThrownBy(() -> new BettingMoney(money))
+                .isInstanceOf(IllegalBlackjackInputException.class);
+    }
+
+    @DisplayName("베팅 머니가 1000원 단위가 아니라면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1001, 2002, 999_999})
+    void validateBettingMoneyUnit(final int money) {
+        assertThatThrownBy(() -> new BettingMoney(money))
+                .isInstanceOf(IllegalBlackjackInputException.class);
+    }
+}
