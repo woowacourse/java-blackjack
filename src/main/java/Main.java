0510f74e54f14a170eb2d7ec08618b7e0729cc08
Giyoul <cla12093@gmail.com>
2026-03-02 17:56:40 +0900

    feat: add card score calculation feature

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] deck = {"1", "2", "K"};
+        String[] deck2 = {"K", "Q", "J"};
+        String[] deck3 = {"1", "6", "K"};
+
+        System.out.println(claculate(deck));
+        System.out.println(claculate(deck2));
+        System.out.println(claculate(deck3));
+    }
+
+    public static int claculate(String[] deck) {
+        int sum = 0;
+        for (String card : deck) sum += modifyScore(card);
+
+        return sum;
+    }
+
+    private static int modifyScore(String card) {
+        final String CARD_DELIMITER = "[KQJ]";
+
+        if (card.matches(CARD_DELIMITER)) {
+            return 10;
+        }
+        if (card.matches("\\d")) {
+            return Integer.parseInt(card);
+        }
+        return 0;
+    }
+}
