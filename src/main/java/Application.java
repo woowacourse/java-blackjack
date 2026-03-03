481037582e4b2b48e429769e242eadb9f5334c98
zakie <realzzaekkii@gmail.com>
2026-03-04 07:53:54 +0900

    test: 테스트 케이스 총 6개

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        List<String> cards = List.of("J", "1", "Q");
+        List<String> cards = List.of("J", "1", "Q", "K", "10", "8");
 
         for (String card : cards) {
             System.out.println("카드 " + card + "의 점수: " + calculateCardScore(card));
