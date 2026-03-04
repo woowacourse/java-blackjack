8b9be06abfc86ce1bf9d057a25a8885cded6204c
gleaming9 <gleaming9@khu.ac.kr>
2026-03-04 18:05:24 +0900

    feat(Deck) : 카드 덱 구현
    - 덱을 무작위로 섞는 기능 구현
    - 덱에서 카드를 드로우하는 기능 구현

---
+++ src/main/java/domain/Deck.java
@@ -0,0 +1,32 @@
+package domain;
+
+import domain.card.Card;
+import exception.ErrorMessage;
+import factory.CardFactory;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    public List<Card> cards;
+
+    public Deck() {
+        this.cards = CardFactory.createDeck();
+        shuffle();
+    }
+
+    private void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        validateEmptyDeck();
+        return cards.removeFirst();
+    }
+
+    private void validateEmptyDeck() {
+        if (cards.isEmpty()) {
+            throw new IllegalStateException(ErrorMessage.EMPTY_DECK.getMessage());
+        }
+    }
+}
