a3828b4c5bebdcb75aaf50b32f83dd873442ccc1
dongheeHan <ark182818@gmail.com>
2026-03-04 17:37:59 +0900

    feat: 승패 결과(GameResult) 구현 - 점수 비교, 버스트 판정, 결과 반전

---
+++ src/main/java/blackjack/domain/result/GameResult.java
@@ -0,0 +1,37 @@
+package blackjack.domain.result;
+
+public enum GameResult {
+    WIN, DRAW, LOSE;
+
+    private static final int BUST_THRESHOLD = 21;
+
+    public static GameResult of(final int playerScore, final int dealerScore) {
+        if (playerScore > BUST_THRESHOLD) {
+            return LOSE;
+        }
+        if (dealerScore > BUST_THRESHOLD) {
+            return WIN;
+        }
+        return compare(playerScore, dealerScore);
+    }
+
+    private static GameResult compare(final int playerScore, final int dealerScore) {
+        if (playerScore > dealerScore) {
+            return WIN;
+        }
+        if (playerScore < dealerScore) {
+            return LOSE;
+        }
+        return DRAW;
+    }
+
+    public GameResult reverse() {
+        if (this == WIN) {
+            return LOSE;
+        }
+        if (this == LOSE) {
+            return WIN;
+        }
+        return DRAW;
+    }
+}
