20b63f7acbaa8f3eb8752cffaefc5de1a7b3cdca
sun <sun007021@gmail.com>
2026-03-04 19:36:10 +0900

    test: 카드 리스트 총합 계산 테스트

--- src/test/java/CardsSumTest.java
+++ src/test/java/CardsSumTest.java
@@ -1,17 +1,33 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class CardsSumTest {
 
     @Test
     void cards_sum_without_ace() {
-        assertThat(Cards.sumScore("2,3,J,Q")).isEqualTo(25);
+        Cards cards = new Cards(List.of(
+                new Card("2", "하트"),
+                new Card("3", "스페이드"),
+                new Card("J", "다이아몬드"),
+                new Card("Q", "클로버")));
+        assertThat(cards.sumScore()).isEqualTo(25);
     }
 
     @Test
     void cards_sum_with_ace() {
-        assertThat(Cards.sumScore("2,3,A,J,Q")).isEqualTo(26);
-        assertThat(Cards.sumScore("2,3,A")).isEqualTo(16);
+        Cards cards1 = new Cards(List.of(
+                new Card("2", "하트"),
+                new Card("3", "스페이드"),
+                new Card("A", "다이아몬드"),
+                new Card("J", "클로버"),
+                new Card("Q", "하트")));
+        Cards cards2 = new Cards(List.of(
+                new Card("2", "하트"),
+                new Card("3", "스페이드"),
+                new Card("A", "다이아몬드")));
+        assertThat(cards1.sumScore()).isEqualTo(26);
+        assertThat(cards2.sumScore()).isEqualTo(16);
     }
 }
