7a71f132f03e69320668fb232044f9b930fbe43b
kangrae <kangrae0501@gmail.com>
2026-03-04 16:42:52 +0900

    feat: 유저 이름 길이 검증 기능 구현

--- src/main/java/blackjack/domain/User.java
+++ src/main/java/blackjack/domain/User.java
@@ -5,7 +5,14 @@ public class User {
     private final String name;
 
     public User(String name) {
+        validate(name);
         this.name = name;
     }
-    
+
+    private void validate(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("유저 이름은 5자가 넘을 수 없습니다.");
+        }
+    }
+
 }
