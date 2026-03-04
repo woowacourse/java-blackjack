1820cb0fec34544f0c6ea1b6a49236ffb260e860
EveryPine <kenton.lim@gmail.com>
2026-03-04 16:23:55 +0900

    test(player): Player 닉네임 처리 테스트 추가

---
+++ src/test/java/domain/PlayerTest.java
@@ -0,0 +1,30 @@
+package domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+
+    @Test
+    @DisplayName("닉네임 처리 테스트: 닉네임이 4~10자인 경우")
+    void 정상_테스트_1() {
+        assertDoesNotThrow(() ->  new Player("pobi"));
+    }
+
+    @Test
+    @DisplayName("닉네임 처리 테스트: 닉네임이 4자 미만인 경우")
+    void 예외_테스트_1() {
+        assertThatThrownBy(() ->  new Player("pob"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("닉네임 처리 테스트: 닉네임이 10자 초과인 경우")
+    void 예외_테스트_2() {
+        assertThatThrownBy(() ->  new Player("jasonjasonj"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
