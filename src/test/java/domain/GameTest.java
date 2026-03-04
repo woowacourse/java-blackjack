5a54e31d1bdb430d55503395437926dbfe3fec3f
MODUGGAGI <kiroro0814@naver.com>
2026-03-04 22:11:01 +0900

    test(domain): 도메인 객체 생성자의 검증 로직 테스트 추가
    Game 객체 생성 시 참여하는 플레이어의 인원 수가 1명 이상, 7명 이하인지 검증하는 로직을 테스트 했습니다.
    @Nested를 사용해서 정상 경우와 예외 경우를 구분했습니다.

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -1,38 +1,69 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import org.assertj.core.api.Assertions;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class GameTest {
 
-    @DisplayName("플레이어 이름이 중복된 경우 예외가 발생한다.")
-    @Test
-    void 플레이어_이름이_중복된_경우_예외가_발생한다() {
-        List<String> duplicatedNames = List.of("피즈", "피즈", "스타크");
+    @Nested
+    @DisplayName("정상 경우")
+    class success {
+        @DisplayName("모든 플레이어가 정상적으로 생성된다.")
+        @Test
+        public void 모든_플레이어가_정상적으로_생성된다() {
+            List<String> names = List.of("피즈", "스타크");
 
-        Assertions.assertThatThrownBy(() -> new Game(duplicatedNames))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 플레이어 이름은 중복될 수 없습니다.");
+            //given
+            Game game = new Game(names);
+            //when
+            Set<Player> players = game.getPlayers();
+            Set<String> playerNames = players.stream()
+                    .map(Player::getName)
+                    .collect(Collectors.toSet());
+            //then
+            assertThat(playerNames).contains("피즈");
+            assertThat(playerNames).contains("스타크");
+        }
     }
 
-    @DisplayName("모든 플레이어가 정상적으로 생성된다.")
-    @Test
-    public void 모든_플레이어가_정상적으로_생성된다() {
-        List<String> names = List.of("피즈", "스타크");
+    @Nested
+    @DisplayName("예외 경우")
+    class failure {
 
-        //given
-        Game game = new Game(names);
-        //when
-        Set<Player> players = game.getPlayers();
-        Set<String> playerNames = players.stream()
-                .map(Player::getName)
-                .collect(Collectors.toSet());
-        //then
-        Assertions.assertThat(playerNames).contains("피즈");
-        Assertions.assertThat(playerNames).contains("스타크");
+        @DisplayName("플레이어 이름이 중복된 경우 예외가 발생한다.")
+        @Test
+        void 플레이어_이름이_중복된_경우_예외가_발생한다() {
+            List<String> duplicatedNames = List.of("피즈", "피즈", "스타크");
+
+            assertThatThrownBy(() -> new Game(duplicatedNames))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("[ERROR] 플레이어 이름은 중복될 수 없습니다.");
+        }
+
+        private static Stream<Arguments> playerNumberOutOfRange() {
+            return Stream.of(
+                    Arguments.of(List.of()),
+                    Arguments.of(List.of("피즈1","피즈2","피즈3","피즈4","피즈5","피즈6","피즈7","피즈8"))
+            );
+        }
+
+        @DisplayName("게임에 참여하는 플레이어 인원이 1~7명이 아닐 경우 예외가 발생한다.")
+        @ParameterizedTest
+        @MethodSource("playerNumberOutOfRange")
+        void 게임_참여_플레이어가_1명에서_7명_사이가_아닐_경우_예외가_발생한다(List<String> names) {
+            assertThatThrownBy(() -> new Game(names))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("[ERROR] 플레이어는 1명 이상 7명 이하여야 합니다.");
+        }
     }
 }
