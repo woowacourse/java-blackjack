655a9b1ac72f2b4e06f69ec34911df40fd06b431
LeeJaeHyeok97 <hazardous10@naver.com>
2026-03-04 20:51:56 +0900

    feat: InputParser 구현

---
+++ src/main/java/InputParser.java
@@ -0,0 +1,10 @@
+public class InputParser {
+    private static final String DELIMITER = ",";
+
+    private InputParser() {
+    }
+
+    public static String[] parseName(String input) {
+        return input.split(DELIMITER);
+    }
+}
