147ee8842019defb3a35a170ea635cb254cd3606
MODUGGAGI <kiroro0814@naver.com>
2026-03-04 18:30:43 +0900

    feat(util): 입력값에 쉼표, 공백, 한글, 영어를 제외한 문자가 입력된 경우 예외 발생 기능 구현
    입력값에 쉼표, 공백, 한글(자음+모음 조합), 영어를 제외한 문자가 입력된 경우 예외 발생시키는 기능을 구현했습니다.
    플레이어 이름을 검증하는 메서드 2개를 하나의 메서드로 통합했습니다.

--- src/test/java/util/InputValidator.java
+++ src/test/java/util/InputValidator.java
@@ -1,9 +1,20 @@
 package util;
 
 public class InputValidator {
-    public static void validateBlank(String input) {
+    public static void validatePlayerNameInput(String input) {
+        validateBlank(input);
+        validateDelimiter(input);
+    }
+
+    private static void validateBlank(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 입력값은 비어 있을 수 없습니다.");
+        }
+    }
+
+    private static void validateDelimiter(String input) {
+        if (!input.matches("^[가-힣a-z, ]+$")) {
+            throw new IllegalArgumentException("[ERROR] 쉼표(,)를 제외한 특수문자는 사용할 수 없습니다.");
         }
     }
 }
