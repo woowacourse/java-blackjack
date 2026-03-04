0e1d59736af81b3f1125ce3518ac62d62412abc0
MODUGGAGI <kiroro0814@naver.com>
2026-03-04 21:55:14 +0900

    feat(domain): 플레이어 정상 생성 기능 구현
    Game에서 플레이어를 정상적으로 생성하는 기능을 구현했습니다.

--- src/test/java/domain/Game.java
+++ src/test/java/domain/Game.java
@@ -1,12 +1,20 @@
 package domain;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Game {
 
+    private final Set<Player> players = new HashSet<>();
+
     public Game(List<String> names) {
         validateDuplicatedName(names);
+
+        players.addAll(names.stream()
+                .map(Player::new)
+                .collect(Collectors.toSet()));
     }
 
     private void validateDuplicatedName(List<String> names) {
@@ -14,5 +22,9 @@ public class Game {
         if (distinctNames.size() != names.size()) {
             throw new IllegalArgumentException("[ERROR] 플레이어 이름은 중복될 수 없습니다.");
         }
+    }
+
+    public Set<Player> getPlayers() {
+        return Set.copyOf(players);
     }
 }
---
+++ src/test/java/domain/Player.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Player {
+
+    private final String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
