3c35be022992a14e66721469725cf5d7e75c6ce1
MODUGGAGI <kiroro0814@naver.com>
2026-03-04 21:51:54 +0900

    test(domain): 플레이어 정상 생성 확인 테스트 구현
    예외 상황이 발생하지 않을 때, 플레이어가 정상적으로 생성되는지 확인하는 테스트를 구현했습니다.

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -1,6 +1,8 @@
 package domain;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,5 +17,22 @@ public class GameTest {
         Assertions.assertThatThrownBy(() -> new Game(duplicatedNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 플레이어 이름은 중복될 수 없습니다.");
+    }
+
+    @DisplayName("모든 플레이어가 정상적으로 생성된다.")
+    @Test
+    public void 모든_플레이어가_정상적으로_생성된다() {
+        List<String> names = List.of("피즈", "스타크");
+
+        //given
+        Game game = new Game(names);
+        //when
+        Set<Player> players = game.getPlayers();
+        Set<String> playerNames = players.stream()
+                .map(Player::getName)
+                .collect(Collectors.toSet());
+        //then
+        Assertions.assertThat(playerNames).contains("피즈");
+        Assertions.assertThat(playerNames).contains("스타크");
     }
 }
