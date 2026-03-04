3804fa8c539158bad8ab7438fb259e53eb2e8cb6
EveryPine <kenton.lim@gmail.com>
2026-03-04 16:30:23 +0900

    feat(player): Player 생성자 추가

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class Player {
+    private static final int NICKNAME_MINIMUM_LENGTH = 4;
+    private static final int NICKNAME_MAXIMUM_LENGTH = 10;
+
+    private final String nickname;
+    private Hand hand;
+
+    public Player(final String nickname) {
+        validateNicknameLength(nickname);
+        this.nickname = nickname;
+    }
+
+    private void validateNicknameLength(final String nickname) {
+        if (nickname.length() > NICKNAME_MAXIMUM_LENGTH || nickname.length() < NICKNAME_MINIMUM_LENGTH) {
+            throw new IllegalArgumentException("닉네임은 4~10자 이어야 합니다.");
+        }
+    }
+
+
+}
