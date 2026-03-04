cec60f96b868d2c64ab9e887491e016515571ef5
MODUGGAGI <kiroro0814@naver.com>
2026-03-04 23:09:44 +0900

    test(domain): 서로 다른 카드 52장 정상 생성 확인 테스트
    총 52장의 서로 다른 카드가 정상적으로 생성되는지 확인하는 테스트를 구현했습니다.

---
+++ src/test/java/domain/DeckTest.java
@@ -0,0 +1,21 @@
+package domain;
+
+import java.util.Set;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DeckTest {
+
+    @DisplayName("52장의 서로 다른 카드가 정상 생성된다.")
+    @Test
+    void _52장의_서로_다른_카드가_정상_생성된다() {
+        Deck deck = new Deck();
+
+        Set<Card> distinctCards = Set.copyOf(deck.getCards());
+
+        Assertions.assertThat(distinctCards.size()).isEqualTo(deck.getCards().size());
+        Assertions.assertThat(distinctCards.size()).isEqualTo(52);
+    }
+}
+
