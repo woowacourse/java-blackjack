42f6cca38cb484b9d3876e3bcb98f98ef6631a9d
gleaming9 <gleaming9@khu.ac.kr>
2026-03-04 18:45:42 +0900

    test(Deck) : 덱 테스트 구현
    - 덱 생성자를 테스트함.
    - 드로우 기능 테스트함.

---
+++ src/test/java/domain/DeckTest.java
@@ -0,0 +1,45 @@
+package domain;
+
+import domain.card.Card;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    @Test
+    void 덱을_정상적으로_생성하는_경우() {
+        // given
+        Deck deck = new Deck();
+
+        // when, then
+        assertEquals(52, deck.getCards().size());
+    }
+
+    @Test
+    void 드로우가_정상적으로_실행되는_경우() {
+        // given
+        Deck deck = new Deck();
+
+        // when
+        Card card = deck.drawCard();
+
+        // then
+        assertEquals(51, deck.getCards().size());
+        assertFalse(deck.getCards().contains(card));
+    }
+
+    @Test
+    void 드로우가_실패하는_경우() {
+        // given
+        Deck deck = new Deck();
+        for (int i = 0; i < 52; i++) {
+            deck.drawCard();
+        }
+
+        // when, then
+        assertThrows(IllegalStateException.class,
+                () -> deck.drawCard());
+    }
+
+}
