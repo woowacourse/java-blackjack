36e2fe438242cff18087a863a44945d9f364fc0b
kangrae <kangrae0501@gmail.com>
2026-03-04 16:52:52 +0900

    feat: Shape Enum 생성

---
+++ src/test/java/blackjack/domain/CardTest.java
@@ -0,0 +1,20 @@
+package blackjack.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CardTest {
+
+    @Test
+    @DisplayName("카드_생성_테스트")
+    void 카드_생성_테스트() {
+        // given
+        String value = "1";
+        String shape = "diamond";
+
+        // when
+
+        // then
+    }
+
+}
