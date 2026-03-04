969d642a437c1fde2e52c5798491a2019144c4a0
tjdakf <ksc73450056@gmail.com>
2026-03-04 21:02:34 +0900

    test: Ace 처리 테스트 코드 추가

--- src/test/java/ScoreCalculatorTest.java
+++ src/test/java/ScoreCalculatorTest.java
@@ -15,4 +15,15 @@ public class ScoreCalculatorTest {
 
         assertThat(currentScore).isEqualTo(20);
     }
+
+    @Test
+    void 핸드에_A가_있고_합산점수가_11점_이하면_10점을_추가한다() {
+        ScoreCalculator calculator = new ScoreCalculator();
+        List<Card> hand = List.of(new Card(Rank.ACE, Suit.SPADE), new Card(Rank.KING, Suit.SPADE));
+
+        int currentScore = calculator.sumScore(hand);
+        int finalScore = calculator.checkAce(currentScore, hand);
+
+        assertThat(currentScore).isEqualTo(21);
+    }
 }
