343c165769ba94fb4bc7e3fe787edd0edcb308a1
Jiihyun <cwlgusc@gmail.com>
2026-03-04 18:39:57 +0900

    test: 카드 합 계산 테스트 추가

--- src/test/java/domain/HandTest.java
+++ src/test/java/domain/HandTest.java
@@ -4,21 +4,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.card.Card;
 import domain.card.Deck;
+import domain.card.Emblem;
+import domain.card.Grade;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class HandTest {
 
     @Test
-    void test() {
+    void 카드를_받으면_손패의_카드_수가_늘어난다() {
         // given
-        Hand hand = new Hand();
         Deck deck = new Deck();
+        Hand hand = new Hand();
         Card card = deck.drawCard();
-
         // when
         hand.receiveCard(card);
         // then
         assertThat(hand.getCards().size()).isEqualTo(1);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"FIVE, TWO, 7"})
+    void 손패에_있는_카드의_합을_계산한다(Grade card1Value, Grade card2Value, int result) {
+        // given
+        Hand hand = new Hand();
+
+        hand.receiveCard(new Card(Emblem.CLOVER, card1Value));
+        hand.receiveCard(new Card(Emblem.CLOVER, card2Value));
+        // when
+        int score = hand.calculate();
+        // then
+        assertThat(score).isEqualTo(result);
+    }
 }
