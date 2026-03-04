951a4ca27d9a6846c5286d4363a2a03e8c84a121
LeeJaeHyeok97 <hazardous10@naver.com>
2026-03-04 20:52:45 +0900

    feat: Participant 일급 객체 생성

---
+++ src/main/java/Participants.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Participants {
+    private List<Participant> values;
+
+    private Participants(String[] values) {
+        this.values = Arrays.stream(values)
+                .map(Participant::from)
+                .toList();
+    }
+
+    public static Participants of(String[] splitValue) {
+        return new Participants(splitValue);
+    }
+}
