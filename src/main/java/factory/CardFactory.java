41bb8595cd13d3b08225fad9d8d6b846a0c64d80
gleaming9 <gleaming9@khu.ac.kr>
2026-03-04 18:02:39 +0900

    feat(CardFactory) : 덱을 생성하는 카드 팩토리를 구현

---
+++ src/main/java/factory/CardFactory.java
@@ -0,0 +1,21 @@
+package factory;
+
+import domain.card.Card;
+import domain.card.Rank;
+import domain.card.Suit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardFactory {
+    public static List<Card> createDeck() {
+        // TODO: 리팩토링
+        List<Card> cards = new ArrayList<>();
+        for(Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(new Card(suit, rank));
+            }
+        }
+        return cards;
+    }
+}
