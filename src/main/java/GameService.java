6b585117696f1326354022cca188b8a0a088c803
Yoonhojoon <dbsghwns1209@gmail.com>
2026-03-04 17:51:24 +0900

    feat: 점수 계산 기능 구현

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -1,2 +1,21 @@
+import java.util.List;
+
 public class GameService {
+
+
+    public int calculateScore(List<String> cards) {
+        int score = 0;
+        for (String card: cards) {
+            if (card.equals("J") || card.equals("Q") || card.equals("K")) {
+                score += 10;
+            }
+            else if (card.equals("A")) {
+
+            }
+            else {
+                score += Integer.parseInt(card);
+            }
+        }
+        return score;
+    }
 }
