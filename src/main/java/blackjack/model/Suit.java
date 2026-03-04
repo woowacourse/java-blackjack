46196e02881eaa6ada29939a3fe9b5da6f8c6d95
Kim, Joon <kjoon418@naver.com>
2026-03-04 17:28:04 +0900

    feat: Suit 구현

---
+++ src/main/java/blackjack/model/Suit.java
@@ -0,0 +1,18 @@
+package blackjack.model;
+
+public enum Suit {
+    DIAMOND("다이아몬드"),
+    CLUB("클로버"),
+    SPADE("스페이드"),
+    HEART("하트");
+
+    private final String korean;
+
+    Suit(String korean) {
+        this.korean = korean;
+    }
+
+    public String getKorean() {
+        return korean;
+    }
+}
