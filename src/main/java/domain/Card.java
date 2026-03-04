2cef230be874c596cd6aeaab106bea833f8fe279
haechanmoon <moonhc.dev@gmail.com>
2026-03-04 17:19:49 +0900

    feat(Card) : 단일 카드 구현

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Card {
+    private final CardRank rank;
+    private final CardSuit suit;
+
+    public Card(CardRank rank, CardSuit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+}
