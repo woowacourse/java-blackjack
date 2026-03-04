fc74b5a0ad8d87fa334ff52f0d667ee3a2662c0f
kangrae <kangrae0501@gmail.com>
2026-03-04 16:57:06 +0900

    feat: 카드 생성 기능 구현

---
+++ src/main/java/blackjack/domain/Card.java
@@ -0,0 +1,12 @@
+package blackjack.domain;
+
+public class Card {
+
+    private String value;
+    private Shape shape;
+
+    public Card(String value, Shape shape) {
+        this.value = value;
+        this.shape = shape;
+    }
+}
