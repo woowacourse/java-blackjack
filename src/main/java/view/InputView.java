4cad062e62275bcf6fad02a0badfa5f2f8369296
yeo-li <parkseongyeol2110@gmail.com>
2026-03-04 17:35:09 +0900

    refactor(InputView): BufferedReader 분리
    inputPlayers()와 inputHitOrStand() 에서 BufferedReader를 공통으로 사용하고 있어 재사용성을
    높이기 위해 분리했습니다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,14 +6,15 @@ import java.io.InputStreamReader;
 
 public class InputView {
 
+    private final BufferedReader bufferedReader = new BufferedReader(
+        new InputStreamReader(System.in));
+
     public String inputPlayers() throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         return bufferedReader.readLine();
     }
 
     public String inputHitOrStand(String name) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n", name);
         return bufferedReader.readLine();
     }
