5b330f977491892c6123e3de3ee35c2ebaf00f68
나의찬 <cab07036@naver.com>
2026-03-04 21:49:49 +0900

    feat(Card): 랜덤 카드 모양 생성 기능 구현
    랜덤 카드를 뽑는 기능 중 랜덤 카드 모양 생성 세부 기능을 구현하였습니다.
    랜덤 숫자를 1-4까지 생성후 카드 모양을 가져오는 방식으로 구현하였습니다.

--- src/test/java/domain/CardTest.java
+++ src/test/java/domain/CardTest.java
@@ -1,14 +1,9 @@
 package domain;
 
-import domain.model.Card;
-import domain.model.CardRank;
-import domain.model.CardShape;
 import org.junit.jupiter.api.Test;
-import repository.CardRepository;
 import util.NumberGenerator;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.fail;
 
 public class CardTest {
 
@@ -19,15 +14,8 @@ public class CardTest {
         }
     }
 
-    class DefaultRankNumberGenerator implements NumberGenerator {
-        @Override
-        public int generate() {
-            return 2;
-        }
-    }
-
     @Test
-    void 랜덤_카드_모양_생성_테스트() {
+    void 랜덤_카드_모양_테스트() {
         // given
         NumberGenerator numberGenerator = new DefaultShapeNumberGenerator();
 
@@ -36,61 +24,5 @@ public class CardTest {
 
         // then
         assertThat(cardShape).isEqualTo(CardShape.HEART);
-    }
-
-    @Test
-    void 랜덤_카드_랭크_생성_테스트() {
-        // given
-        NumberGenerator numberGenerator = new DefaultRankNumberGenerator();
-
-        // when
-        CardRank cardRank = CardRank.getRank(numberGenerator.generate());
-
-        // then
-        assertThat(cardRank).isEqualTo(CardRank.TWO);
-    }
-
-    @Test
-    void 모양과_랭크를_조합하여_카드_생성_테스트() {
-        // given
-        NumberGenerator shapeNumberGenerator = new DefaultShapeNumberGenerator();
-        NumberGenerator rankNumberGenerator = new DefaultRankNumberGenerator();
-
-        CardRank cardRank = CardRank.getRank(rankNumberGenerator.generate());
-        CardShape cardShape = CardShape.getShape(shapeNumberGenerator.generate());
-
-        // when
-        Card card = Card.of(cardRank, cardShape);
-
-        // then
-        assertThat(card.getCardShape()).isEqualTo(CardShape.HEART);
-        assertThat(card.getCardRank()).isEqualTo(CardRank.TWO);
-
-    }
-
-    @Test
-    void 생성된_카드_중복_검사_성공_테스트() {
-        // given
-        CardRepository cardRepository = new CardRepository();
-        cardRepository.save(Card.of(CardRank.ACE, CardShape.HEART));
-
-        // when
-        Card card = Card.of(CardRank.TWO, CardShape.HEART);
-
-        // then
-        assertThat(cardRepository.isExist(card)).isEqualTo(false);
-    }
-
-    @Test
-    void 생성된_카드_중복_검사_실패_테스트() {
-        // given
-        CardRepository cardRepository = new CardRepository();
-        cardRepository.save(Card.of(CardRank.ACE, CardShape.HEART));
-
-        // when
-        Card card = Card.of(CardRank.ACE, CardShape.HEART);
-
-        // then
-        assertThat(cardRepository.isExist(card)).isEqualTo(true);
     }
 }
