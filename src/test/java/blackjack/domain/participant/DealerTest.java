192404aa5cbb686c0a82d4f5d05adc058f7648ab
dongheeHan <ark182818@gmail.com>
2026-03-04 17:27:46 +0900

    feat: 딜러(Dealer) 구현 - 16이하 추가 드로우 규칙

---
+++ src/test/java/blackjack/domain/participant/DealerTest.java
@@ -0,0 +1,52 @@
+package blackjack.domain.participant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Rank;
+import blackjack.domain.card.Suit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DealerTest {
+
+    @Test
+    @DisplayName("점수가 16 이하이면 카드를 받을 수 있다")
+    void canReceiveCard_returnsTrue_whenScoreIsEqualToSixteen() {
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(new Card(Suit.HEART, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.SIX));
+
+        assertThat(dealer.canReceiveCard()).isTrue();
+    }
+
+    @Test
+    @DisplayName("점수가 17 이상이면 카드를 받을 수 없다")
+    void canReceiveCard_returnsFalse_whenScoreIsEqualToSeventeen() {
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(new Card(Suit.HEART, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.SEVEN));
+
+        assertThat(dealer.canReceiveCard()).isFalse();
+    }
+
+    @Test
+    @DisplayName("점수가 16일 때 카드를 받을 수 있다")
+    void canReceiveCard_returnsTrue_whenScoreIsExactlySixteen() {
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(new Card(Suit.HEART, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.SIX));
+
+        assertThat(dealer.canReceiveCard()).isTrue();
+    }
+
+    @Test
+    @DisplayName("점수가 17일 때 카드를 받을 수 없다")
+    void canReceiveCard_returnsFalse_whenScoreIsExactlySeventeen() {
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(new Card(Suit.HEART, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.SEVEN));
+
+        assertThat(dealer.canReceiveCard()).isFalse();
+    }
+}
