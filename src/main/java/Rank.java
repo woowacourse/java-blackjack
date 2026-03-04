6975c3f8efcc2ea4c69871336a3a2e6e32752ea0
tjdakf <ksc73450056@gmail.com>
2026-03-04 20:37:00 +0900

    feat: J, Q, K, A 점수 처리 기능 추가

--- src/main/java/Rank.java
+++ src/main/java/Rank.java
@@ -1,15 +1,24 @@
 public enum Rank {
-    ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING
-}
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10);
+
+    private final int value;
+
+    Rank(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+    return value;
+}}
