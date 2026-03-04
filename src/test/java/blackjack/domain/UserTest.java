0c9a3836be84bfdc78de07e3e40564883cad453d
kangrae <kangrae0501@gmail.com>
2026-03-04 16:41:30 +0900

    test: 유저 이름이 5자가 넘을 경우 예외 발생 테스트 추가

--- src/test/java/blackjack/domain/UserTest.java
+++ src/test/java/blackjack/domain/UserTest.java
@@ -1,6 +1,7 @@
 package blackjack.domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,18 @@ public class UserTest {
         // when & then
         assertThatCode(() -> new User(name))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("유저이름이_5자가_넘을경우_예외_발생")
+    void 유저이름이_5자가_넘을경우_예외_발생() {
+        // given
+        String name = "흑곰흑곰흑곰";
+
+        // when & then
+        assertThatThrownBy(() -> new User(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("5자");
     }
 
 }
