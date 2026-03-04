6940d7a8f8549e70fe0340bf2af75d2c9b7cafed
박병훈 <leafloveu@naver.com>
2026-03-04 15:21:55 +0900

    chore: 초기 상태 복구

--- src/main/java/blackjack/BlackjackApplication.java
+++ src/main/java/blackjack/BlackjackApplication.java
@@ -1,18 +1,7 @@
 package blackjack;
 
-import blackjack.model.AceScoreCalculator;
-import java.util.Scanner;
-
 public class BlackjackApplication {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        AceScoreCalculator aceScoreCalculator = new AceScoreCalculator();
-
-        System.out.print("현재 점수를 입력하세요: ");
-        int currentScore = scanner.nextInt();
-
-        int aceScore = aceScoreCalculator.calculateAceScore(currentScore);
-        System.out.printf("ACE는 [%d]점으로 계산됩니다.\n", aceScore);
     }
 }
