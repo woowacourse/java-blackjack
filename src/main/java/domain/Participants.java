ab270cc595f295a46728161a62670e332e5f5ac0
Giyoul <cla12093@gmail.com>
2026-03-05 18:46:49 +0900

    feat(domain): add validate number of participants feature

--- src/main/java/domain/Participants.java
+++ src/main/java/domain/Participants.java
@@ -3,6 +3,20 @@ package domain;
 import java.util.List;
 
 public class Participants {
+    private static final Integer MAXIMUM_NUMBER_OF_PARTICIPANTS = 16;
+
     private List<User> participants;
     private Dealer dealer;
+
+    public Participants(List<String> parsedParticipantsName) {
+        validateParticipantsNumbers(parsedParticipantsName);
+    }
+
+    static void validateParticipantsNumbers(List<String> parsedParticipantsName) {
+        if (parsedParticipantsName.size() > MAXIMUM_NUMBER_OF_PARTICIPANTS) {
+            throw new IllegalArgumentException("[ERROR] 최대 참가 인원은 16명 이하여야 합니다.");
+        }
+    }
 }
+
+
