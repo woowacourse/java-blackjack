0b60d7077e8914fbfd002b849c3e820891b4def8
dahxxn <dahyeonhan4529@gmail.com>
2026-03-05 21:44:02 +0900

    test: Deck 객체 단위 테스트 추가
    - 카드를 지급  테스트 추가

--- src/test/java/blackjack/domain/DeckTest.java
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -1,8 +1,10 @@
 package blackjack.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.util.ArrayList;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DeckTest {
@@ -17,7 +19,7 @@ class DeckTest {
         }
         cards.add(TrumpCard.of(Suit.SPADE, Rank.ACE));
 
-        Assertions.assertThatThrownBy(() -> Deck.of(cards))
+        assertThatThrownBy(() -> Deck.of(cards))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("전체 카드 수는 52장이어야 합니다.");
     }
@@ -33,8 +35,40 @@ class DeckTest {
         cards.removeFirst();
         cards.add(TrumpCard.of(Suit.CLOVER, Rank.KING));
 
-        Assertions.assertThatThrownBy(() -> Deck.of(cards))
+        assertThatThrownBy(() -> Deck.of(cards))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("카드는 중복되면 안됩니다.");
+    }
+
+    @Test
+    void 카드를_지급할_때_카드가_없으면_예외_발생한다() {
+        List<TrumpCard> cards = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(TrumpCard.of(suit, rank));
+            }
+        }
+        Deck deck = Deck.of(cards);
+        for (int i = 0; i < 52; i++) {
+            deck.draw();
+        }
+
+        assertThatThrownBy(deck::draw)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("덱에 카드가 없습니다.");
+    }
+
+    @Test
+    void 카드를_지급하면_전체_카드_수가_줄어든다() {
+        List<TrumpCard> cards = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(TrumpCard.of(suit, rank));
+            }
+        }
+        Deck deck = Deck.of(cards);
+        TrumpCard draw = deck.draw();
+
+        assertThat(draw).isNotNull();
     }
 }
