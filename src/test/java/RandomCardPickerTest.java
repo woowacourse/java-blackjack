1dd4d145e8de6dc05aeb1ffcbe2e3e4d3b863d07
sun <sun007021@gmail.com>
2026-03-05 16:57:59 +0900

    style: 랜덤 카드 뽑기 테스트 변수명 변경

--- src/test/java/RandomCardPickerTest.java
+++ src/test/java/RandomCardPickerTest.java
@@ -10,22 +10,22 @@ public class RandomCardPickerTest {
     @Test
     void draw_random_card() {
         Random mockRandom = mock(Random.class);
-        RandomCardPicker cardDistributor = new RandomCardPicker(mockRandom);
+        RandomCardPicker randomCardPicker = new RandomCardPicker(mockRandom);
 
         when(mockRandom.nextInt(13)).thenReturn(0); // "2"
         when(mockRandom.nextInt(4)).thenReturn(0); // "하트"
 
-        assertThat(cardDistributor.drawCard()).isEqualTo(new Card("2", "하트"));
+        assertThat(randomCardPicker.drawCard()).isEqualTo(new Card("2", "하트"));
     }
 
     @Test
     void same_card_is_not_drawn_twice() {
-        RandomCardPicker cardDistributor = new RandomCardPicker(new Random());
+        RandomCardPicker randomCardPicker = new RandomCardPicker(new Random());
 
         Set<Card> drawn = new HashSet<>();
 
         for (int i = 0; i < 52; i++) {
-            Card card = cardDistributor.drawCard();
+            Card card = randomCardPicker.drawCard();
             assertThat(drawn).doesNotContain(card);
             drawn.add(card);
         }
