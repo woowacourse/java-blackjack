bf823566df159d8bd60f0342df824b31a7c85c2d
Jiihyun <cwlgusc@gmail.com>
2026-03-05 11:28:01 +0900

    test: 버스트 판정 테스트 추가

---
+++ src/test/java/domain/PlayerTest.java
@@ -0,0 +1,46 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.card.Card;
+import domain.card.Emblem;
+import domain.card.Grade;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+
+    @Test
+    void 카드의_합이_21이초과되면_버스트() {
+        // given
+        Hand hand = new Hand();
+        hand.receiveCard(new Card(Emblem.CLOVER, Grade.TEN));
+        hand.receiveCard(new Card(Emblem.CLOVER, Grade.TEN));
+        hand.receiveCard(new Card(Emblem.CLOVER, Grade.TEN));
+
+        Player player = new Player(new Name("test"), hand);
+        boolean expect = true;
+
+        // when
+        boolean result = player.isBust();
+
+        // then
+        assertThat(result).isEqualTo(expect);
+    }
+
+    @Test
+    void 카드의_합이_21이하이면_버스트가_아니다() {
+        // given
+        Hand hand = new Hand();
+        hand.receiveCard(new Card(Emblem.CLOVER, Grade.TEN));
+        hand.receiveCard(new Card(Emblem.CLOVER, Grade.TEN));
+
+        Player player = new Player(new Name("test"), hand);
+        boolean expect = false;
+
+        // when
+        boolean result = player.isBust();
+
+        // then
+        assertThat(result).isEqualTo(expect);
+    }
+}
