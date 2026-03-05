25c52928a20d075cc6df2c5342533628f9f9c61c
zakie <realzzaekkii@gmail.com>
2026-03-05 11:14:11 +0900

    feat: 트럼프 카드 객체 추가

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -1,4 +1,11 @@
 package domain;
 
 public class Card {
+    private final CardSuit cardSuit;
+    private final CardRank cardRank;
+
+    public Card(CardSuit cardSuit, CardRank cardRank) {
+        this.cardSuit = cardSuit;
+        this.cardRank = cardRank;
+    }
 }
