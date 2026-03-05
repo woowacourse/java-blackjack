d2cf20494dd0a858a6ef798b93d4b3f57af9bdc7
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-06 01:06:06 +0900

    test: player 이름 저장 테스트

---
+++ src/test/java/domain/player/GamblersTest.java
@@ -0,0 +1,27 @@
+package domain.player;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GamblersTest {
+
+    @Test
+    @DisplayName("이름 리스트를 통해 player 객체들이 제대로 생성되는지 테스트")
+    void 이름_정상_저장_테스트() {
+        // given
+        List<String> names = List.of("pobi", "coco");
+
+        // when
+        Gamblers gamblers = new Gamblers(names);
+
+        // then
+        boolean allContainName = names.stream()
+                .allMatch(name -> gamblers.containGambler(name) == true);
+
+        Assertions.assertThat(allContainName).isEqualTo(true);
+    }
+
+    // player에게 카드 추가할 건지 묻기(player의 카드 합이 burst가 안났을 경우에만 묻기)
+}
