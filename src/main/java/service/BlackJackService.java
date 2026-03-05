00ec2072febdf070a818eefcd56047ffe5ddc7fc
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-05 17:45:40 +0900

    feat(BlackJackService) : 게임 결과 기능 구현

--- src/main/java/service/BlackJackService.java
+++ src/main/java/service/BlackJackService.java
@@ -1,13 +1,21 @@
 package service;
 
+import java.util.Objects;
 import model.CardNumber;
 import model.Cards;
+import model.Dealer;
+import model.MatchStatus;
+import model.Participant;
 import model.Player;
+import model.Players;
+import model.PlayersWinning;
 import model.Scorer;
 import model.dto.Card;
+import model.dto.PlayerWinning;
 
 public class BlackJackService {
     private final Cards cards;
+
     public BlackJackService(Cards cards) {
         this.cards = cards;
     }
@@ -22,10 +30,22 @@ public class BlackJackService {
         player.addScore(score);
     }
 
-    public boolean isBust(Player player) {
-        Integer score = player.getResult().score();
+    public boolean isBust(Participant participant) {
+        Integer score = participant.getResult().score();
 
         return score > 21;
+    }
+
+    public PlayersWinning getGameResult(Players players, Dealer dealer) {
+
+        PlayersWinning playersWinning = new PlayersWinning();
+
+        for(Player player : players.getPlayers()) {
+            MatchStatus matchStatus = getPlayerResult(player, dealer);
+            playersWinning.add(new PlayerWinning(player.getResult().name(), matchStatus));
+        }
+
+        return playersWinning;
     }
 
     private Integer getScore(Card card, Integer score) {
@@ -33,5 +53,19 @@ public class BlackJackService {
             return Scorer.calculate(card, score);
         }
         return Scorer.calculate(card);
+    }
+
+
+    private MatchStatus getPlayerResult(Player player, Dealer dealer) {
+        if((isBust(dealer) && isBust(player)) || (Objects.equals(player.getResult().score(),
+                dealer.getResult().score()))) {
+            return MatchStatus.DRAW;
+        }
+
+        if(isBust(dealer) || (!isBust(player) && (player.getResult().score() > dealer.getResult().score()))) {
+            return MatchStatus.WIN;
+        }
+
+        return MatchStatus.LOSE;
     }
 }
