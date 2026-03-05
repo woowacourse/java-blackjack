ab9bdbc572c6d2e13d9401d86786570d7b75ecd4
Yeji Kim <yejikim.erica@gmail.com>
2026-03-05 18:01:55 +0900

    feat : add DRAW_CARD_OUT_OF_RANGE

--- src/main/java/common/ErrorMessage.java
+++ src/main/java/common/ErrorMessage.java
@@ -1,7 +1,8 @@
 package common;
 
 public enum ErrorMessage {
-    HIT_OR_STAND_VALUE_MIS_MATCH("y 혹은 n만 입력 가능합니다.");
+    HIT_OR_STAND_VALUE_MIS_MATCH("y 혹은 n만 입력 가능합니다."),
+    DRAW_CARD_OUT_OF_RANGE("양수 이상의 숫자 중 남은 카드 수 만큼만 선택 가능");
 
     private final String message;
 
