0b6c5d478cbfcf314cef2e817b77930bd341627a
smini <sm020320@naver.com>
2026-03-05 20:06:03 +0900

    feat: Ace 점수 판단 및 카드의 숫자 합 계산 기능 구현

---
+++ src/main/java/blackjack/model/ScoreCalculator.java
@@ -0,0 +1,29 @@
+package blackjack.model;
+
+import java.util.List;
+
+public class ScoreCalculator {
+
+    public int getCardScore(List<Card> cards) {
+        long aceCount = cards.stream()
+                .filter(card -> Number.ACE.equals(card.getNumber()))
+                .count();
+
+        int score = cards.stream()
+                .mapToInt(Card::getNumberValue)
+                .sum();
+
+        if (aceCount != 0) {
+            return aceTranslate(score, aceCount);
+        }
+        return score;
+    }
+
+    private int aceTranslate(int score, long aceCount) {
+        while (score <= 11 && aceCount > 0) {
+            score += 10;
+            aceCount--;
+        }
+        return score;
+    }
+}
