a63a2c264eae721e89a68e787bd24ea8b5285c39
kangrae <kangrae0501@gmail.com>
2026-03-05 11:31:53 +0900

    feat: User가 Card를 가질 수 있도록 필드 추가

--- src/main/java/blackjack/domain/User.java
+++ src/main/java/blackjack/domain/User.java
@@ -1,12 +1,17 @@
 package blackjack.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
     private final String name;
+    private final List<Card> cards;
 
     public User(String name) {
         validate(name);
         this.name = name;
+        this.cards = new ArrayList<>();
     }
 
     private void validate(String name) {
