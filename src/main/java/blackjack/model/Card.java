46ce21fca675716e99ac8ba7f7286357809de8b9
smini <sm020320@naver.com>
2026-03-05 17:32:16 +0900

    feat: 카드 한 장 생성 구현

---
+++ src/main/java/blackjack/model/Card.java
@@ -0,0 +1,29 @@
+package blackjack.model;
+
+import java.util.Objects;
+
+public class Card {
+    private final Figure figure; // 모양, 스페이드, 하트 등등
+    private final Number number; // a->k
+
+    public Card(Figure figure, Number number) {
+        this.figure = figure;
+        this.number = number;
+    }
+
+    public String getCard() {
+        return number.getName() + figure.getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return figure == card.figure && number == card.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(figure, number);
+    }
+}
