b7266b1dbd806d07075c51f3ec97223f819961c2
sun <sun007021@gmail.com>
2026-03-05 17:57:54 +0900

    test: 버스트 판정 테스트

---
+++ src/test/java/BustTest.java
@@ -0,0 +1,48 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class BustTest {
+
+    @Test
+    void player_busts_when_sum_exceeds_21() {
+        Player player = new Player("Alice");
+        player.receiveOneCard(new Card("2", "하트"));
+        player.receiveOneCard(new Card("Q", "스페이드"));
+        player.receiveOneCard(new Card("10", "클로버"));
+
+        assertThat(player.isBust().isEqualTo(true));
+    }
+
+    @Test
+    void dealer_busts_when_sum_exceeds_21() {
+        Dealer dealer = new Dealer();
+        dealer.receiveOneCard(new Card("2", "하트"));
+        dealer.receiveOneCard(new Card("Q", "스페이드"));
+        dealer.receiveOneCard(new Card("10", "클로버"));
+
+        assertThat(dealer.isBust().isEqualTo(true));
+    }
+
+    @Test
+    void player_not_busts_when_sum_21_or_less() {
+        Player player = new Player();
+        player.receiveOneCard(new Card("A", "하트"));
+        player.receiveOneCard(new Card("Q", "스페이드"));
+        player.receiveOneCard(new Card("10", "클로버"));
+
+        assertThat(player.isBust().isEqualTo(false));
+    }
+
+    @Test
+    void dealer_not_busts_when_sum_21_or_less() {
+        Dealer dealer = new Dealer();
+        dealer.receiveOneCard(new Card("A", "하트"));
+        dealer.receiveOneCard(new Card("Q", "스페이드"));
+        dealer.receiveOneCard(new Card("10", "클로버"));
+
+        assertThat(dealer.isBust().isEqualTo(false));
+    }
+
+
+}
