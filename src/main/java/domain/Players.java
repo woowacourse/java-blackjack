7bd5736a3282a66521bdbaed45ef2d9c3d75b0dd
Jinriro <riro12.hanyang.ac.kr>
2026-03-05 11:39:20 +0900

    feat: Players 테스트 구현 및 엔티티 생성

---
+++ src/main/java/domain/Players.java
@@ -0,0 +1,18 @@
+package domain;
+
+import java.util.List;
+
+public class Players {
+
+    private final List<String> players;
+
+    public Players(List<String> players) {
+        validate(players);
+        this.players = players;
+    }
+
+    private void validate(List<String> players) {
+        if (players.size() > 5)
+            throw new IllegalArgumentException("플레이어 인원 수는 5명 이하여야 합니다.");
+    }
+}
