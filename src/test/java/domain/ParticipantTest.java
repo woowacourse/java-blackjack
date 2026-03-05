443b9843d561266b4019609162dc8409bf4d83b3
ghkdtjfxkd <syt1128@gmail.com>
2026-03-05 11:56:24 +0900

    test: 참여자 이름 반환 테스트 추가

---
+++ src/test/java/domain/ParticipantTest.java
@@ -0,0 +1,30 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class ParticipantTest {
+
+    @ParameterizedTest
+    @MethodSource("participantsAndNames")
+    @DisplayName("참여자는 이름을 반환할 수 있어야 한다.")
+    void 참여자_이름_반환(String expected, Participant participant) {
+        String actual = participant.name();
+
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> participantsAndNames() {
+        return Stream.of(
+                Arguments.arguments("딜러", new Dealer("딜러")),
+                Arguments.arguments("pobi", new Player("pobi")),
+                Arguments.arguments("jason", new Player("jason")),
+                Arguments.arguments("tars", new Player("tars"))
+        );
+    }
+}
