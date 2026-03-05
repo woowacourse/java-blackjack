fc5be25bec300987e5a290d5e7a85c8a81131da7
dahxxn <dahyeonhan4529@gmail.com>
2026-03-05 20:15:47 +0900

    test: TrumpCard 단위 테스트 추가
    - 랭크와 수트 중 하나라도 null 이면 예외 처리 테스트
    - 조건에 맞는 랭크와 수트가 들어오면 정상 생성 테스트

--- src/test/java/blackjack/domain/TrumpCardTest.java
+++ src/test/java/blackjack/domain/TrumpCardTest.java
@@ -1,4 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class TrumpCardTest {
-  
+
+    @Test
+    void 랭크가_null_이면_예외_발생한다() {
+        Assertions.assertThatThrownBy(() -> TrumpCard.of(Suit.of("하트"), null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Rank와 Suit 중 하나라도 null이 올 수 없습니다.");
+    }
+
+    @Test
+    void 수트가_null_이면_예외_발생한다() {
+        Assertions.assertThatThrownBy(() -> TrumpCard.of(null, Rank.of("2")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Rank와 Suit 중 하나라도 null이 올 수 없습니다.");
+    }
+
+    @Test
+    void 랭크와_수트_둘_다_null_이면_예외_발생한다() {
+        Assertions.assertThatThrownBy(() -> TrumpCard.of(null, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Rank와 Suit 중 하나라도 null이 올 수 없습니다.");
+    }
+
+    @Test
+    void 조건에_맞는_랭크와_수트가_들어오면_정상_생성된다() {
+        TrumpCard trumpCard = TrumpCard.of(Suit.of("하트"), Rank.of("3"));
+        assertThat(trumpCard).isNotNull();
+    }
 }
