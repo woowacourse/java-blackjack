959f02e1c0416b2a965a1898a56bef46b6e2a421
tjdakf <ksc73450056@gmail.com>
2026-03-05 21:55:16 +0900

    refactor: 매직 넘버 상수 처리

--- src/main/java/ScoreCalculator.java
+++ src/main/java/ScoreCalculator.java
@@ -1,6 +1,11 @@
 import java.util.List;
 
 public class ScoreCalculator {
+
+
+    public static final int BLACKJACK_SCORE = 21;
+    public static final int ACE_BONUS_SCORE = 10;
+
     private int sumScore(List<Card> hand) {
         int score = 0;
         for (Card card : hand) {
@@ -11,19 +16,16 @@ public class ScoreCalculator {
 
     public int calculateScore(List<Card> hand) {
         int currentScore = sumScore(hand);
-
-        Card findCard
-                = hand.stream().filter(c -> c.getRank() == 1).findAny().orElse(null);
-
+        Card findCard = hand.stream().filter(c -> c.getRank() == 1).findAny().orElse(null);
         if (findCard == null) {
             return currentScore;
         }
 
-        if (currentScore + 10 > 21) {
+        if (currentScore + ACE_BONUS_SCORE > BLACKJACK_SCORE) {
             return currentScore;
         }
 
-        return currentScore + 10;
+        return currentScore + ACE_BONUS_SCORE;
     }
 
     public boolean isBust(int score) {
--- src/test/java/GameManagerTest.java
+++ src/test/java/GameManagerTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
