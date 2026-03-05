d5547ff4460f3be29496f02f83f438320b781d7d
나의찬 <cab07036@naver.com>
2026-03-05 11:30:28 +0900

    feat(Card): 랜덤 모양과 랭크를 통해 카드 생성 기능 구현
    랜덤으로 생성한 모양과 랭크를 통해 카드를 생성하는 기능을 구현하였습니다.

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,23 @@
+package domain;
+
+public class Card {
+    private CardRank cardRank;
+    private CardShape cardShape;
+
+    private Card(CardRank cardRank, CardShape cardShape) {
+        this.cardRank = cardRank;
+        this.cardShape = cardShape;
+    }
+
+    public static Card of(CardRank cardRank, CardShape cardShape) {
+        return new Card(cardRank, cardShape);
+    }
+
+    public CardRank getCardRank() {
+        return cardRank;
+    }
+
+    public CardShape getCardShape() {
+        return cardShape;
+    }
+}
