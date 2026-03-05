363d2de1af5386297605b16329023d4843c3ba86
Giyoul <cla12093@gmail.com>
2026-03-05 17:46:24 +0900

    test(test): add participants name input validation test

--- src/test/java/util/ParserTest.java
+++ src/test/java/util/ParserTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 
 public class ParserTest {
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
     @Test
     void 빈_참가자_이름_예외_테스트() {
         // given
@@ -13,9 +15,31 @@ public class ParserTest {
         // when & then
         assertThatThrownBy(() ->
                 Parser.validateNonLiteralInput(participantsName))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith(ERROR_PREFIX);
     }
 
     @Test
-    void 참가자_이름_숫자_예외_테스트()
+    void 참가자_이름_숫자_예외_테스트() {
+        // given
+        String participantsName = "123";
+
+        // when & then
+        assertThatThrownBy(() ->
+                Parser.validateNonLiteralInput(participantsName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith(ERROR_PREFIX);
+    }
+
+    @Test
+    void 쉼표_외의_특수문자_예외_테스트() {
+        // given
+        String participantsName = "영기:라이";
+
+        // when & then
+        assertThatThrownBy(() ->
+                Parser.validateNonLiteralInput(participantsName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith(ERROR_PREFIX);
+    }
 }
