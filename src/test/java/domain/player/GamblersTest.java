77cbf6bacdf9fd07d164e6ea345fbf59c237f8e1
haechanmoon <moonhc.dev@gmail.com>
2026-03-05 19:24:36 +0900

    test(GamblerTest) : 딜러 및 사용자 승패 결과 도출 테스트 추가

--- src/test/java/domain/player/GamblersTest.java
+++ src/test/java/domain/player/GamblersTest.java
@@ -1,5 +1,6 @@
 package domain.player;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
