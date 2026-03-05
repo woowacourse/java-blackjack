2879d4a96aad720b57f14fb88a5601c6396b3ac9
Jinriro <riro12.hanyang.ac.kr>
2026-03-05 19:00:59 +0900

    refactor: Players 일급컬렉션으로 변경

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -1,20 +1,29 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Players {
 
-    private final List<String> players;
+    private final List<Player> players;
 
-    public Players(List<String> players) {
-        validate(players);
-        this.players = players;
+    public Players(List<String> playerNames) {
+        validate(playerNames);
+        this.players = from(playerNames);
+    }
+
+    private List<Player> from(List<String> playerNames) {
+        List<Player> players = new ArrayList<>();
+
+        for (String name : playerNames) {
+            players.add(new Player(name));
+        }
+
+        return players;
     }
 
     private void validate(List<String> players) {
         if (players.size() > 5)
             throw new IllegalArgumentException("플레이어 인원 수는 5명 이하여야 합니다.");
     }
-
-
 }
