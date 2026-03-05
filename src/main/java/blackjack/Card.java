18b928b5f7b39061eb74593047081d41bb008c16
softmoca <acyc0909@gmail.com>
2026-03-05 17:44:00 +0900

    feat(Card) : CardPoint와 CardPattern을 가지는 Card 생성 구현

---
+++ src/main/java/blackjack/Card.java
@@ -0,0 +1,21 @@
+package blackjack;
+
+public class Card {
+
+
+    private final CardPoint cardPoint;
+    private final CardPattern cardPattern;
+
+    public Card(CardPoint cardPoint, CardPattern cardPattern) {
+        this.cardPoint = cardPoint;
+        this.cardPattern = cardPattern;
+    }
+
+    public int getCardPoint() {
+        return cardPoint.getPoint();
+    }
+
+    public String getCardPointName() {
+        return cardPoint.getName();
+    }
+}
