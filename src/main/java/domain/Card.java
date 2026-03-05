a9f05284c7f45ef4f5cac6fc09574cbcbcdea314
yeo-li <parkseongyeol2110@gmail.com>
2026-03-05 21:31:36 +0900

    feat(Card): equals() 및 hashCode() 메서드 구현

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -2,6 +2,7 @@ package domain;
 
 import constant.Rank;
 import constant.Suit;
+import java.util.Objects;
 
 public class Card {
 
@@ -11,5 +12,19 @@ public class Card {
     public Card(constant.Rank rank, Suit suit) {
         Rank = rank;
         this.suit = suit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return Rank == card.Rank && suit == card.suit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Rank, suit);
     }
 }
