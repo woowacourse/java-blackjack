3f4c22867a190567b400f17806218f10f09cd86d
최용준 <yj9107vv@gmail.com>
2026-03-05 22:25:32 +0900

    test(PlayerTest): ACE 1/11 판정 여부 테스트
    - 플레이어 점수 합계가 21초과 시 ACE 포함한 경우 점수 보정 테스트

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,27 +1,43 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
 
-    private final List<Card> cards = List.of(
-            Card.of(Rank.THREE, Suit.DIAMOND),
-            Card.of(Rank.NINE, Suit.CLOVER),
-            Card.of(Rank.EIGHT, Suit.DIAMOND)
-    );
-
     @Test
     @DisplayName("플레이어의 카드 점수 합계 정상 테스트")
     void 플레이어_카드_점수_합계_정상_테스트() {
+        List<Card> cards = List.of(
+                Card.of(Rank.THREE, Suit.DIAMOND),
+                Card.of(Rank.NINE, Suit.CLOVER),
+                Card.of(Rank.EIGHT, Suit.DIAMOND));
+
         int cardScore = cards.stream()
                 .mapToInt(Card::getScore)
                 .sum();
+
+        int expect = 20;
+        assertThat(cardScore).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("플레이어 카드의 합이 21 초과 시 ACE 값 1로 정의 테스트")
+    void ACE_카드_1_점수_판정_테스트() {
+        List<Card> cards = List.of(
+                Card.of(Rank.TWO, Suit.DIAMOND),
+                Card.of(Rank.NINE, Suit.CLOVER),
+                Card.of(Rank.EIGHT, Suit.DIAMOND),
+                Card.of(Rank.ACE, Suit.HEART));
+
+        int cardScore = Player.of(cards).calculateScore();
+
+        if (cardScore > 21 && cards.stream().anyMatch(Card::isAce)) {
+            cardScore -= 10;
+        }
 
         int expect = 20;
         assertThat(cardScore).isEqualTo(expect);
