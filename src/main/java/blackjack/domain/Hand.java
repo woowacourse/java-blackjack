fe6a1ab80ff63fe0bca7fa2c84289b6ead3c61ff
dahxxn <dahyeonhan4529@gmail.com>
2026-03-06 02:06:52 +0900

    feat: Hand 객체 생성 구현
    - 카드 개수를 반환하는 기능 구현
    - 카드 총 점수를 계산하는 기능 구현
    - 21을 초과할 때 ACE를 1로 변환하는 로직 구현

--- src/main/java/blackjack/domain/Hand.java
+++ src/main/java/blackjack/domain/Hand.java
@@ -1,2 +1,55 @@
-package blackjack.domain;public class Hand {
+package blackjack.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+    private static final int ACE_DIFFERENCE = 10;
+    private static final int BLACKJACK_THRESHOLD = 21;
+
+    private final List<TrumpCard> cards;
+
+    private Hand(List<TrumpCard> cards) {
+        this.cards = List.copyOf(cards);
+    }
+
+    public static Hand init() {
+        return new Hand(new ArrayList<>());
+    }
+
+    public Hand receive(TrumpCard card) {
+        List<TrumpCard> newCards = new ArrayList<>(cards);
+        newCards.add(card);
+        return new Hand(newCards);
+    }
+
+    public int countCards() {
+        return cards.size();
+    }
+
+    public int calculateScore() {
+        int totalScore = sumCardScores();
+        return adjustForAces(totalScore);
+    }
+
+    private int sumCardScores() {
+        return cards.stream()
+                .mapToInt(TrumpCard::score)
+                .sum();
+    }
+
+    private int adjustForAces(int totalScore) {
+        int aceCount = countAces();
+        while (aceCount > 0 && totalScore > BLACKJACK_THRESHOLD) {
+            totalScore -= ACE_DIFFERENCE;
+            aceCount--;
+        }
+        return totalScore;
+    }
+
+    private int countAces() {
+        return (int) cards.stream()
+                .filter(TrumpCard::isAce)
+                .count();
+    }
 }
