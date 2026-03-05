366e163a446a63a440e49e506b3d85825497f5ab
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-05 16:37:00 +0900

    test(cardNumber): 입력받은 카드 숫자 검증 정상 테스트 작성

--- src/test/java/domain/CardNumberTest.java
+++ src/test/java/domain/CardNumberTest.java
@@ -5,7 +5,6 @@ import domain.exception.OutOfBoundException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CardNumberTest {
