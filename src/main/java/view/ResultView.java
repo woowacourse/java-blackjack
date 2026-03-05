354fb28ae7973633c9db74ae4c397d6e46a4b121
김가현 <hyeonnni@naver.com>
2026-03-05 22:40:09 +0900

    refactor(view): ResultView에 DealerPlayersDTO 파라미터 도입

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -6,12 +6,15 @@ import domain.Dealer;
 import domain.Outcome;
 import domain.Player;
 import domain.Players;
+import dto.DealerPlayersDTO;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import message.IOMessage;
 
 public class ResultView {
-    public void printGameStart(Players players, Dealer dealer) {
+    public void printGameStart(DealerPlayersDTO gameContext) {
+        Players players = gameContext.players();
+        Dealer dealer = gameContext.dealer();
         System.out.printf("딜러와 %s에게 2장을 나누었습니다.%n", joinPlayerNames(players));
         System.out.printf("딜러카드: %s%n", CardSuitMap.getCardName(dealer.getCardList().get(0)));
         IntStream.range(0, players.getSize())
@@ -23,7 +26,9 @@ public class ResultView {
                 ));
     }
 
-    public void printResult(Players players, Dealer dealer) {
+    public void printResult(DealerPlayersDTO gameContext) {
+        Players players = gameContext.players();
+        Dealer dealer = gameContext.dealer();
         System.out.println("딜러카드: " + joinCardNames(dealer.getCardList()) + " - 결과: " + dealer.getResult());
         IntStream.range(0, players.getSize())
                 .mapToObj(players::getPlayer)
@@ -35,7 +40,9 @@ public class ResultView {
                 ));
     }
 
-    public void printWinner(Players players, Dealer dealer) {
+    public void printWinner(DealerPlayersDTO gameContext) {
+        Players players = gameContext.players();
+        Dealer dealer = gameContext.dealer();
         System.out.println();
         System.out.println(IOMessage.WINNING_STATISTICS.message());
         printDealerOutcome(dealer);
