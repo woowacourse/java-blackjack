6d8e36c99b622717268c6a57d283674ca11e454d
Jinriro <riro12.hanyang.ac.kr>
2026-03-05 17:12:14 +0900

    feat: Deck 테스트 구현 및 엔티티 생성

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Card {
+
+    private final Rank rank;
+    private final Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+}
---
+++ src/main/java/domain/Deck.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.*;
+
+public class Deck {
+
+    private final List<Card> cards = new ArrayList<>();
+
+    public Deck() {
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(new Card(rank, suit));
+            }
+        }
+
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        if (isAvailable()) {
+            return cards.removeFirst();
+        }
+
+        throw new IllegalArgumentException("더 이상 카드가 존재하지 않습니다.");
+    }
+
+    private boolean isAvailable() {
+        return !cards.isEmpty();
+    }
+}
