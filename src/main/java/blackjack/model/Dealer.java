d601a812562d96da589284b3a33ec5322b9b8604
박병훈 <leafloveu@naver.com>
2026-03-05 16:50:36 +0900

    feat(Dealer): 카드 뽑는 기능 구현

---
+++ src/main/java/blackjack/model/Dealer.java
@@ -0,0 +1,24 @@
+package blackjack.model;
+
+public class Dealer {
+    private static final int PICK_THRESHOLD = 16;
+
+    private final Hands hands;
+
+    private Dealer(Hands hands) {
+        if (hands == null) {
+            throw new IllegalArgumentException("hands가 null입니다.");
+        }
+
+        this.hands = hands;
+    }
+
+    public static Dealer create() {
+        return new Dealer(Hands.empty());
+    }
+
+    //카드 덱에서 카드를 한 장 뽑아서 핸즈에 추가한다.
+    public void pickACard(CardDeck cardDeck) {
+        hands.addACard(cardDeck.draw());
+    }
+}
