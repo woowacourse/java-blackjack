81c0667db6a5340f4cb1cb03b521b243df7af6a7
snow___shower <wonsb25@gmail.com>
2026-03-05 20:18:09 +0900

    feat: 플레이어 기능 추가

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,33 @@
+package domain;
+
+import java.util.List;
+
+public class Player {
+    private final String name;
+    private final Hand hand;
+
+    public Player(String name, Hand hand) {
+        this.name = name;
+        this.hand = hand;
+    }
+
+    public void keepCard(Card card){
+        hand.addCard(card);
+    }
+
+    public Integer handSize(){
+        return hand.getHandSize();
+    }
+
+    public Integer getTotalCardScore(){
+        return hand.calculateTotalScore();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Card> getHand() {
+        return hand.getHand();
+    }
+}
