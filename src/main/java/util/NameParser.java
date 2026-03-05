88ff16fe62a6bf9f7001501415e484100ffe325d
심호경 <ghrudtla@naver.com>
2026-03-05 21:03:31 +0900

    feat: 참가자 이름을 파싱하는 NameParser 구현

---
+++ src/main/java/util/NameParser.java
@@ -0,0 +1,12 @@
+package util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NameParser {
+    public static List<String> parse(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .toList();
+    }
+}
