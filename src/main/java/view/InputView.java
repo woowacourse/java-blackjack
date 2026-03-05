9a1f44b3e16a63004743eaa7e39edbb3e572eb10
yeo-li <parkseongyeol2110@gmail.com>
2026-03-05 20:18:38 +0900

    refactor(InputView): BufferedReader를 Scanner로 변경
    BufferedReader를 사용하면 IOException을 try-catch 해야하는데, Scanner를 사용하면 예외처리를 하지 않아도 되어 변경했습니다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,21 +1,19 @@
 package view;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class InputView {
 
-    private final BufferedReader bufferedReader = new BufferedReader(
-        new InputStreamReader(System.in));
+    private final Scanner scanner = new Scanner(System.in);
 
-    public String inputPlayers() throws IOException {
+
+    public String inputPlayers() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return bufferedReader.readLine();
+        return scanner.nextLine();
     }
 
-    public String inputHitOrStand(String name) throws IOException {
+    public String inputHitOrStand(String name) {
         System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n", name);
-        return bufferedReader.readLine();
+        return scanner.nextLine();
     }
 }
