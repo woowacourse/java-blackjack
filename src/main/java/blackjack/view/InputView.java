51809df4fe9ff68b20f2776dfcfef4f92c87ff58
smini <sm020320@naver.com>
2026-03-05 10:53:26 +0900

    feat: 참여할 플레이어 이름 입력 기능 구현

---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,17 @@
+package blackjack.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner sc;
+
+    public InputView() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public String getName() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        return sc.nextLine();
+    }
+}
