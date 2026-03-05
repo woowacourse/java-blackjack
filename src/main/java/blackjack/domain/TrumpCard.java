52320cdd54b88221fb3174d5061886f6af1d07fb
dahxxn <dahyeonhan4529@gmail.com>
2026-03-05 20:17:02 +0900

    feat: TrumpCard 객체 생성 구현
    - 랭크와 수트 중 하나라도 null인 경우 예외 처리

--- src/main/java/blackjack/domain/TrumpCard.java
+++ src/main/java/blackjack/domain/TrumpCard.java
@@ -1,2 +1,27 @@
-package blackjack.domain;public class TrumpCard {
+package blackjack.domain;
+
+public class TrumpCard {
+    private final Suit suit;
+    private final Rank rank;
+
+    private TrumpCard(Suit suit, Rank rank) {
+        validate(suit, rank);
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public static TrumpCard of(Suit suit, Rank rank) {
+        return new TrumpCard(suit, rank);
+    }
+
+    private void validate(Suit suit, Rank rank) {
+        validateSuitOrRankIsNull(suit, rank);
+
+    }
+
+    private void validateSuitOrRankIsNull(Suit suit, Rank rank) {
+        if (suit == null || rank == null) {
+            throw new IllegalArgumentException("Rank와 Suit 중 하나라도 null이 올 수 없습니다.");
+        }
+    }
 }
