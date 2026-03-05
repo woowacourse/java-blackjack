c776de220a533fba6af881df4ac79fc1ad68153c
bee9827 <ehfrhfo9494@naver.com>
2026-03-05 13:12:55 +0900

    Revert "feat: 사용자의 핸드를 관리하는 BlackjackHand 생성"
    This reverts commit ce64721cd2803b2b69f58d053b3f0366bfd704ac.

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,13 +1,3 @@
 public record Card(Rank rank, Suit suit) {
-    public boolean isAce() {
-        return Rank.isAce(rank);
-    }
 
-    public boolean isFaceCard() {
-        return Rank.isFaceCard(rank);
-    }
-
-    public Integer getScore(){
-        return rank.getScore();
-    }
 }
--- src/main/java/ErrorMessage.java
+++ src/main/java/ErrorMessage.java
@@ -1,12 +1,7 @@
 public enum ErrorMessage {
-    DECK_SIZE("카드 사이즈는 %d 이어야 합니다.".formatted(Deck.CARD_SIZE)),
-    DECK_DUPLICATE("카드는 중복될 수 없습니다."),
-    INDEX_RANGE("인덱스의 범위가 올바르지 않습니다."),
-
-    //입력에 의한 예외가 아님. 코딩이 잘못된거임.
-    HANDS_CARDS_SIZE("처음 게임 시작 시, 두장을 나눠줘야한다."),
-    ACE_SCORE("ACE는 스코어가 변동된다."),
-    ;
+    ERROR_DECK_SIZE("카드 사이즈는 %d 이어야 합니다.".formatted(Deck.CARD_SIZE)),
+    ERROR_DECK_DUPLICATE("카드는 중복될 수 없습니다."),
+    ERROR_INDEX_RANGE("인덱스의 범위가 올바르지 않습니다.");
 
     private final String message;
 
