110157d99d54b01dbff46a283354a0371b76d5e4
kangrae <kangrae0501@gmail.com>
2026-03-05 16:51:55 +0900

    feat: 딜러 생성 기능 구현

---
+++ src/main/java/blackjack/domain/Dealer.java
@@ -0,0 +1,15 @@
+package blackjack.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealer {
+
+    private final List<Card> cards;
+
+    public Dealer() {
+        this.cards = new ArrayList<>();
+    }
+
+
+}
