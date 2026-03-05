23c2e3178466d83656084b48892fa70afbcc429a
EveryPine <kenton.lim@gmail.com>
2026-03-05 11:43:38 +0900

    feat(parser): 닉네임 입력 문자열 파싱 기능 추가

---
+++ src/main/java/utils/Parser.java
@@ -0,0 +1,12 @@
+package utils;
+
+import java.util.List;
+
+public class Parser {
+
+    public static List<String> parseNickname(String string) {
+        final String delimiter = ",";
+
+        return List.of(string.split(delimiter));
+    }
+}
