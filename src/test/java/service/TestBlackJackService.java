d27058e8c2bfa094cd06758ea8aa14482c6484d1
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-05 17:38:54 +0900

    test(BlackJackService) : 참가자 승패 판정, 카드 드로우 테스트 코드 추가

---
+++ src/test/java/service/TestBlackJackService.java
@@ -0,0 +1,69 @@
+package service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import model.Cards;
+import model.Dealer;
+import model.Player;
+import model.PlayerName;
+import model.Players;
+import model.PlayersWinning;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class TestBlackJackService {
+    /*
+        카드 뽑기 void
+         - 카드를 뽑는다.
+         - 뽑은 카드가 일반이면, 그냥 / ace면 이전 점수를 가져와 점수값을 가져온다.
+         - 플레이어 카드풀 업데이트한다.
+         - 해당 점수를 업데이트한다.
+
+        버스트 판정 (플레이어)
+         - 플레이어 스코어가 21인지 여부 확인해서, boolean 리턴
+
+         승패 판정 -> 결과 dto 만들어서 리턴하는 메서드
+     */
+    @Test
+    public void 카드_뽑기_정상_작동() {
+        //given
+        Cards cards = new Cards();
+        BlackJackService blackJackService = new BlackJackService(cards);
+
+        Player player = new Player(new PlayerName("player1"));
+
+        //when
+        blackJackService.draw(player);
+
+        //then
+        assertThat(player.getResult().score()).isGreaterThan(0);
+        assertThat(player.getResult().deck().size()).isEqualTo(1);
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({"22,24,무","10,10,무","22,1,승","10,11,승","1,22,패","11,10,패"})
+    public void 블랙잭_참가자_승_패_판정_정상_작동(Integer dealerScore, Integer playerScore, String result) {
+        //given
+        Cards cards = new Cards();
+        BlackJackService blackJackService = new BlackJackService(cards);
+
+        Player player = new Player(new PlayerName("player1"));
+        Players players = new Players(List.of(player));
+
+        Dealer dealer = new Dealer();
+
+
+        //when
+        dealer.addScore(dealerScore);
+        player.addScore(playerScore);
+
+        PlayersWinning gameResult = blackJackService.getGameResult(players, dealer);
+
+
+        //then
+        assertThat(gameResult.getPlayersWinnings().getFirst().matchStatus().getStatus()).isEqualTo(result);
+    }
+}
