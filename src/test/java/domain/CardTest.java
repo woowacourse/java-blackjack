648fe9b4f3957aef03b8a627c495e70a3db16ab4
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-03-05 17:57:54 +0900

    test: 카드 덱이 52장 생성되었는지 테스트

--- src/test/java/domain/CardTest.java
+++ src/test/java/domain/CardTest.java
@@ -1,5 +1,25 @@
 package domain;
 
-public class CardTest {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CardTest {
+    Deck deck;
+
+    @BeforeEach
+    void beforeEach() {
+        deck = new Deck();
+    }
+
+    @DisplayName("덱 생성 성공")
+    @Test
+    void 덱에_52장의_카드_생성() {
+        deck.init();
+        assertThat(deck.cards.size()).isEqualTo(52);
+    }
 }
