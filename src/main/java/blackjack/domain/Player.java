c4f819ffbf4367e2a9d28f543675190cffab9eba
dahxxn <dahyeonhan4529@gmail.com>
2026-03-06 03:24:51 +0900

    feat: Player 객체 생성 구현
    - Player 객체를 생성하는 정적 팩토리 메서드 추가
    - 카드를 받아 새로운 Player를 반환하는 불변 객체 패턴 구현
    - 플레이어 이름을 반환하는 기능 구현
    - 보유한 카드 개수를 반환하는 기능 구현
    - 현재 점수를 계산하는 기능 구현

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -1,2 +1,32 @@
-package blackjack.domain;public class Player {
+package blackjack.domain;
+
+public class Player {
+    private final Name name;
+    private final Hand hand;
+
+    private Player(Name name, Hand hand) {
+        this.name = name;
+        this.hand = hand;
+    }
+
+    public static Player of(Name name) {
+        return new Player(name, Hand.init());
+    }
+
+    public Player receive(TrumpCard card) {
+        Hand newHand = this.hand.receive(card);
+        return new Player(name, newHand);
+    }
+
+    public int countCards() {
+        return hand.countCards();
+    }
+
+    public int score() {
+        return hand.calculateScore();
+    }
+
+    public String name() {
+        return name.getName();
+    }
 }
