f40f00f547015945bc50fe7c7d414a6073b8d2d6
kangrae <kangrae0501@gmail.com>
2026-03-05 20:02:14 +0900

    test: 게임 시작 시 카드 2장을 나눠주는 테스트 추가

---
+++ src/test/java/blackjack/service/GameServiceTest.java
@@ -0,0 +1,29 @@
+package blackjack.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.domain.Dealer;
+import blackjack.domain.Deck;
+import blackjack.domain.User;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameServiceTest {
+
+    @Test
+    @DisplayName("게임 시작 세팅 테스트")
+    void 게임_시작_세팅_테스트() {
+        // given
+        GameService gameService = new GameService();
+        User user = new User("흑곰");
+        Dealer dealer = new Dealer(new Deck());
+
+        // when
+        gameService.settingCards(List.of(user), dealer);
+
+        // then
+        assertThat(user.getCards().size()).isEqualTo(2);
+    }
+
+}
