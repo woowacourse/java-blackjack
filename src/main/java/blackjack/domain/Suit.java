e9915ae2e361056804e0f01ebd00c946b7c1369f
dahxxn <dahyeonhan4529@gmail.com>
2026-03-05 16:50:30 +0900

    refactor: Suit 생성 로직 수정
    - Map을 만들어 생성 시 판별 로직 시간을 O(N) -> O(1) 시간으로 단축
    - 변수 네이밍 판별시 어려움이 있어 name -> koreanName으로 수정

--- src/main/java/blackjack/domain/Suit.java
+++ src/main/java/blackjack/domain/Suit.java
@@ -1,24 +1,28 @@
 package blackjack.domain;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public enum Suit {
     SPADE("스페이드"),
     HEART("하트"),
     DIAMOND("다이아몬드"),
     CLOVER("클로버");
 
-    private final String name;
+    private final String koreanName;
 
-    Suit(String name) {
-        this.name = name;
+    private static final Map<String, Suit> SUIT_MAP = Arrays.stream(values())
+            .collect(Collectors.toMap(suit -> suit.koreanName, suit -> suit));
+
+    Suit(String koreanName) {
+        this.koreanName = koreanName;
     }
 
-    public static Suit of(String name) {
-        for (Suit suit : Suit.values()) {
-            if (suit.name.equals(name)) {
-                return suit;
-            }
-        }
-        throw new IllegalArgumentException("존재하지 않는 카드 이름입니다.");
+    public static Suit of(String koreanName) {
+        return Optional.ofNullable(SUIT_MAP.get(koreanName))
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 카드 이름입니다."));
     }
 }
 
