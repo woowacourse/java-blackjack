efa4c003c97dec25a4cf220dc5819d18479278aa
Giyoul <cla12093@gmail.com>
2026-03-05 18:12:48 +0900

    feat(validation): add participants name input format validate feature

--- src/main/java/util/Parser.java
+++ src/main/java/util/Parser.java
@@ -1,10 +1,17 @@
 package util;
 
+import java.util.regex.Pattern;
+
 public class Parser {
+    private static final Pattern CONTAINS_DIGIT = Pattern.compile(".*\\d.*");
+    private static final Pattern VALID_INPUT = Pattern.compile("^[a-zA-Z가-힣,]+$");
+
     // 쉼표 기준으로 분리
     // 예외 처리
     public void parseParticipantsName(String participantsName) {
-
+        validateEmptyInput(participantsName);
+        validateNonLiteralInput(participantsName);
+        validateInvalidSymbolInput(participantsName);
     }
 
     public void validateParticipantsName(String participantsName) {
@@ -20,9 +27,14 @@ public class Parser {
     }
 
     static void validateNonLiteralInput(String participantsName) {
-
+        if (CONTAINS_DIGIT.matcher(participantsName).matches()) {
+            throw new IllegalArgumentException("[ERROR] 참가자 이름에는 숫자를 포함할 수 없습니다.");
+        }
     }
 
     static void validateInvalidSymbolInput(String participantsName) {
+        if (!VALID_INPUT.matcher(participantsName).matches()) {
+            throw new IllegalArgumentException("[ERROR] 참가자 이름에는 쉼표를 제외한 특수문자를 사용할 수 없습니다.");
+        }
     }
 }
