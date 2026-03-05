e53ce3170d8bed536e85c3c820af4cda7edea2d8
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-05 20:16:55 +0900

    fix(InputView) : 오타 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,7 +1,7 @@
 package view;
 
 public class InputView {
-    private final static String NAME_REQUEST_TEXT = "게임에 참여할 사람의 이름을 입력하세요.(쉼펴 기준으로 분리)";
+    private final static String NAME_REQUEST_TEXT = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private final static String DRAW_REQUEST_TEXT = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니요는 n)";
 
     public void printNameRequest() {
