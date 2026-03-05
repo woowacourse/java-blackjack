34d11446cbb9230eda4096c0db1db707e2658125
구민서 <minseo12379@gmail.com>
2026-03-05 18:56:46 +0900

    feat: 카드 점수 계산 구현 Co-authored-by: alstj2384 <ass91160@naver.com>

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,5 +1,14 @@
 package domain;
 
 public enum Rank {
-    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, J, Q, K;
+    ACE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), J(10), Q(10), K(10);
+    private final int value;
+
+    Rank(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
