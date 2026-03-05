d41f20d8217c3f19c0daa9a7fd71101340d83f84
smini <sm020320@naver.com>
2026-03-05 11:09:56 +0900

    feat: 플레이어 도메인 구현

---
+++ src/main/java/blackjack/model/Player.java
@@ -0,0 +1,13 @@
+package blackjack.model;
+
+public class Player {
+    private final Name name;
+
+    public Player(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+}
