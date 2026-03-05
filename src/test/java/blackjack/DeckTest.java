5144b7a701fdf1b20fbfe80f8391062f7396164b
softmoca <acyc0909@gmail.com>
2026-03-05 17:53:30 +0900

    test(Deck) : 52장의 카드를 가지는 Deck 생성 테스트

---
+++ src/test/java/blackjack/DeckTest.java
@@ -0,0 +1,21 @@
+package blackjack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class DeckTest {
+
+
+    @Test
+    void 덱은_총52장의_카드를_가진다() {
+        Deck deck = new Deck();
+        assertThat(deck.getCount()).isEqualTo(52);
+        assertThat(deck.getCount()).isNotEqualTo(51);
+        assertThat(deck.getCount()).isNotEqualTo(53);
+    }
+
+
+
+
+}
