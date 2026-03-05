10e59c051968e9c744e64525aab36bf4dbe384e2
snow___shower <wonsb25@gmail.com>
2026-03-05 20:34:07 +0900

    test: 딜러 테스트 추가

---
+++ src/test/java/DealerTest.java
@@ -0,0 +1,49 @@
+import domain.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class DealerTest {
+
+    Hand dummyHand;
+    String name;
+
+    @BeforeEach
+    void init() {
+        dummyHand = new Hand();
+        name = "딜러";
+    }
+
+    @Test
+    void 핸드가_16_이하면_카드를_뽑는다() {
+        Card card = new Card(Rank.FOUR, Pattern.CLOVER);
+        Dealer dealer = new Dealer(name, dummyHand);
+        Integer beforeSize = dealer.handSize();
+
+        if (dealer.dealerRule()) {
+            dealer.keepCard(card);
+        }
+        Integer afterSize = dealer.handSize();
+
+        assertThat(beforeSize + 1).isEqualTo(afterSize);
+    }
+
+    @Test
+    void 핸드가_17_이상이면_카드를_뽑지_않는다() {
+        Card card1 = new Card(Rank.JACK, Pattern.CLOVER);
+        Card card2 = new Card(Rank.QUEEN, Pattern.CLOVER);
+        Card card3 = new Card(Rank.KING, Pattern.CLOVER);
+        dummyHand.addCard(card1);
+        dummyHand.addCard(card2);
+        Dealer dealer = new Dealer(name, dummyHand);
+        Integer beforeSize = dealer.handSize();
+
+        if (dealer.dealerRule()) {
+            dealer.keepCard(card3);
+        }
+        Integer afterSize = dealer.handSize();
+
+        assertThat(beforeSize).isEqualTo(afterSize);
+    }
+}
