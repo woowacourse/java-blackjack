4e2335d6ba7251691f0763ba9a2e56e2493ff058
leejaeheon <jaeheon@leejaeheons-MacBook-Air.local>
2026-03-05 17:51:13 +0900

    feat(domain): 카드를 한 장 뽑는 기능
    세부사항:
    - 덱에 카드가 존재하지 않는 경우 예외 발생

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -4,29 +4,39 @@ import domain.card.Card;
 import domain.card.Rank;
 import domain.card.Suit;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
 
 public class Deck {
-    private List<Card> cards = new ArrayList<>();
+    private Stack<Card> cards = new Stack<>();
 
-    public Deck() {
-        setUp();
+    private Deck(Stack<Card> cards) {
+        this.cards = cards;
     }
 
-    private void setUp() {
+    public static Deck of() {
+        Stack<Card> cards = new Stack<>();
+
+        prepareCards(cards);
+        Collections.shuffle(List.of(cards));
+
+        return new Deck(cards);
+    }
+
+    private static void prepareCards(Stack<Card> cards) {
         for (Rank rank : Rank.values()) {
             for (Suit suit : Suit.values()) {
                 Card card = new Card(rank, suit);
                 cards.add(card);
             }
         }
-
-        Collections.shuffle(cards);
     }
 
     public int size() {
         return cards.size();
+    }
+
+    public Card draw() {
+        return cards.pop();
     }
 }
--- src/test/java/domain/DeckTest.java
+++ src/test/java/domain/DeckTest.java
@@ -3,12 +3,36 @@ package domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DeckTest {
     @Test
     void 중복되지_않은_52장의_카드_묶음을_만든다() {
-        Deck deck = new Deck();
-
+        Deck deck = Deck.of();
         assertThat(deck.size()).isEqualTo(52);
+    }
+
+    @Test
+    void 덱이_비었을_때_예외를_발생한다() {
+        Deck deck = Deck.of();
+        int deckSize = deck.size();
+
+        for (int i = 0; i < deckSize; i++) {
+            deck.draw();
+        }
+
+        assertThatThrownBy(() -> deck.draw())
+                .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    void 덱에서_카드를_한_장_뽑으면_덱의_사이즈가_감소한다() {
+        Deck deck = Deck.of();
+        int size = deck.size();
+
+        deck.draw();
+        int changedSize = deck.size();
+
+        assertThat(changedSize).isEqualTo(size - 1);
     }
 }
