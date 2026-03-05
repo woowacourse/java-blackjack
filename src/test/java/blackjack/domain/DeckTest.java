83ea8f3382a5999e5e21c4665dba37177d8c30e7
kangrae <kangrae0501@gmail.com>
2026-03-05 10:51:02 +0900

    test: 덱 생성 테스트 추가

---
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -0,0 +1,17 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DeckTest {
+
+    @Test
+    @DisplayName("덱 생성 테스트")
+    void 덱_생성_테스트() {
+        // when & then
+        assertThatCode(() -> Deck.generate())
+                .doesNotThrowAnyException();
+    }
+}
