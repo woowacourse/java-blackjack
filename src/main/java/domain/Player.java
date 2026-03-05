27efe56d908bfd9db4a65b74f8d430bee7b808b2
zakie <realzzaekkii@gmail.com>
2026-03-05 17:23:16 +0900

    feat: 딜러인지 확인하는 속성 추가

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class Player {
     private final String name;
     private final HandCards handCards;
+    private final boolean isDealer;
 
-    public Player(String name, HandCards handCards) {
+    public Player(String name, HandCards handCards, boolean isDealer) {
         this.name = name;
         this.handCards = handCards;
+        this.isDealer = isDealer;
     }
 
     public void addHandCard(Card card) {
