263dd850df549b1137101aa0365b48101857974f
sun <sun007021@gmail.com>
2026-03-05 17:37:35 +0900

    test: 불필요한 테스트(플레이어와 딜러에게 카드 분배) 삭제

--- src/test/java/CardDistributorTest.java
+++ src/test/java/CardDistributorTest.java
@@ -1,3 +1,4 @@
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -6,33 +7,4 @@ import org.junit.jupiter.api.Test;
 
 public class CardDistributorTest {
 
-    @Test
-    void distribute_one_card_to_player() {
-        Random mockRandom = mock(Random.class);
-        RandomCardPicker randomCardPicker = new RandomCardPicker(mockRandom);
-        CardDistributor cardDistributor = new CardDistributor(randomCardPicker);
-
-        when(mockRandom.nextInt(13)).thenReturn(0); // "2"
-        when(mockRandom.nextInt(4)).thenReturn(0); // "하트"
-
-        Player player = new Player("Player1");
-        cardDistributor.distributeCardTo(player);
-
-        assertThat(player.getCards()).containsExactly(new Card("2", "하트"));
-    }
-
-    @Test
-    void distribute_one_card_to_dealer() {
-        Random mockRandom = mock(Random.class);
-        RandomCardPicker randomCardPicker = new RandomCardPicker(mockRandom);
-        CardDistributor cardDistributor = new CardDistributor(randomCardPicker);
-
-        when(mockRandom.nextInt(13)).thenReturn(0); // "2"
-        when(mockRandom.nextInt(4)).thenReturn(0); // "하트"
-
-        Dealer dealer = new Dealer();
-        cardDistributor.distributeCardTo(dealer);
-
-        assertThat(dealer.getCards()).containsExactly(new Card("2", "하트"));
-    }
 }
