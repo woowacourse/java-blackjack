cbedd0e9d893d52b3006fe348d6759ba40dc5f34
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-03-05 20:26:04 +0900

    test: 덱 셔플 기능 테스트

--- src/test/java/domain/DeckTest.java
+++ src/test/java/domain/DeckTest.java
@@ -2,13 +2,13 @@ package domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import java.util.Arrays;
-import java.util.List;
+import constant.Rank;
+import constant.Suit;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CardTest {
+public class DeckTest {
     Deck deck;
 
     @BeforeEach
@@ -21,5 +21,17 @@ public class CardTest {
     void 덱에_52장의_카드_생성() {
         deck.init();
         assertThat(deck.cards.size()).isEqualTo(52);
+    }
+
+    @DisplayName("덱이 잘 섞였는지 확인")
+    @Test
+    void 섞인_덱에서_첫번째_카드_추출() {
+        Card testCard = new Card(Rank.ACE, Suit.HEART);
+
+        deck.init();
+        deck.shuffle();
+        Card randomCard = deck.draw();
+
+        assertThat(testCard).isNotEqualTo(randomCard);
     }
 }
