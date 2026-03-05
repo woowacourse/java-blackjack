47f6441e0c46337735d039743013dbde22ea8df8
yeo-li <parkseongyeol2110@gmail.com>
2026-03-05 12:12:23 +0900

    feat(Player): validate() 메서드 구현

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,34 @@
+package domain;
+
+public class Player {
+
+    private final String name;
+    private final Hand hand;
+
+    public Player(String name) {
+        validate(name);
+        this.name = name;
+        this.hand = new Hand();
+    }
+
+    private void validate(String name) {
+        validateNotBlank(name);
+        validatePlayerNameLength(name);
+    }
+
+    private void validatePlayerNameLength(String name) {
+        if (!(2 <= name.length() && name.length() <= 5)) {
+            throw new IllegalArgumentException("[ERROR] 게임 참가자의 이름은 2~5글자 사이여야 합니다.");
+        }
+    }
+
+    private void validateNotBlank(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 게임 참가자의 이름은 공백이 될 수 없습니다.");
+        }
+    }
+
+    // HandDto 생성 및 반환 -> dto 생성은 어디서?
+
+    //
+}
