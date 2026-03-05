f70f37118d20b55cff45e5c3f3836a018fd36eff
심호경 <ghrudtla@naver.com>
2026-03-05 21:02:54 +0900

    feat: Deck에서 카드를 뽑는 draw 메서드 구현

---
+++ src/main/java/domain/Deck.java
@@ -0,0 +1,44 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    private final List<Card> cards;
+
+    private Deck(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public static Deck createDeck() {
+        List<Card> cards = generateAllCards();
+        Collections.shuffle(cards);
+        return new Deck(cards);
+    }
+
+    private static List<Card> generateAllCards() {
+        List<Card> cards = new ArrayList<>();
+        for (CardShape shape : CardShape.values()) {
+            addCardsByShape(cards, shape);
+        }
+        return cards;
+    }
+
+    private static void addCardsByShape(List<Card> cards, CardShape shape) {
+        for (CardNumber number : CardNumber.values()) {
+            cards.add(Card.of(number, shape));
+        }
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public Card draw() {
+        if (cards.isEmpty()) {
+            throw new IllegalArgumentException("덱에 남은 카드가 없습니다.");
+        }
+        return cards.removeFirst();
+    }
+}
