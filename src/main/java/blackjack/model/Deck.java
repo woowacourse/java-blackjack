7b8115f5327919c3118f3ee256dabcca7298a2dc
smini <sm020320@naver.com>
2026-03-05 17:33:09 +0900

    feat: 카드 덱 생성 및 셔플 구현

---
+++ src/main/java/blackjack/model/Deck.java
@@ -0,0 +1,39 @@
+package blackjack.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    private List<Card> cards; // 52장을 가지고 있음
+
+    public Deck() {
+        this.cards = createDeck();
+    }
+
+    public List<Card> createDeck() {
+        List<Card> cards = new ArrayList<>();
+
+        for (int i = 0; i < Figure.getSize(); i++) {
+            int index = i;
+            // TODO: 코드 가독성 올리기 (2026. 3. 5.)
+            cards.addAll(Arrays.stream(Number.values())
+                    .map(num -> new Card(Figure.values()[index], num))
+                    .toList());
+        }
+        return cards;
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public int getSize() {
+        return cards.size();
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+}
