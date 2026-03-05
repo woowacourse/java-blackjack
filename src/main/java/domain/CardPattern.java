48242ec601425f5357d08ed41cbb3d15daf3a1c4
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-05 17:03:15 +0900

    feat(cardPattern): 입력받은 카드 문양 검증 및 생성 기능 구현

---
+++ src/main/java/domain/CardPattern.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public enum CardPattern {
+    SPADE("스페이드"),
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    CLUB("클로버");
+
+    private String name;
+
+    CardPattern(String name) {
+        this.name = name;
+    }
+
+    public static CardPattern matchCardPattern(String value) {
+        return Arrays.stream(CardPattern.values()).filter(pattern ->  pattern.name.equals(value))
+                .findAny()
+                .orElseThrow(NoSuchElementException::new);
+    }
+}
