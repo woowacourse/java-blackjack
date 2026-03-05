fc6bd39e75d604e87a452ba29271eecbb6ce8e83
dahxxn <dahyeonhan4529@gmail.com>
2026-03-05 17:20:54 +0900

    feat: Rank객체 생성 로직 구현
    - 랭크 내의 범위가 아니면 예외 처리

---
+++ src/main/java/blackjack/domain/Rank.java
@@ -0,0 +1,38 @@
+package blackjack.domain;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public enum Rank {
+    ACE("A", 11),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    QUEEN("Q", 10),
+    JACK("J", 10),
+    KING("K", 10);
+
+    private final String rankName;
+    private final int value;
+
+    private static final Map<String, Rank> RANK_MAP = Arrays.stream(values())
+            .collect(Collectors.toMap(rank -> rank.rankName, rank -> rank));
+
+    Rank(String rankName, int value) {
+        this.rankName = rankName;
+        this.value = value;
+    }
+
+    public static Rank of(String rankName) {
+        return Optional.ofNullable(RANK_MAP.get(rankName))
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 카드 이름입니다."));
+    }
+}
