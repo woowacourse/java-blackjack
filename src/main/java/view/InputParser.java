c3eca9eec9f069ae4bc3379d859426a220d2a7e1
zakie <realzzaekkii@gmail.com>
2026-03-05 11:11:14 +0900

    feat: 문자열 형태의 플레이어이름을 리스트 형태로 변환하는 기능 추가

---
+++ src/main/java/view/InputParser.java
@@ -0,0 +1,26 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputParser {
+
+    public static List<String> parsePlayers(String strPlayers) {
+
+        String[] split = strPlayers.split(",", -1);
+
+        List<String> players = new ArrayList<>();
+
+        for (String name : split) {
+            name = name.trim();
+
+            if (name.length() < 2 || name.length() > 10) {
+                throw new IllegalArgumentException("플레이어의 이름은 2글자 이상 10글자 이하여야 합니다.");
+            }
+
+            players.add(name);
+        }
+
+        return players;
+    }
+}
