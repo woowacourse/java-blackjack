8582f6d3697abe8ec10d93ef90e1d9a1c4c6858d
Yeji Kim <yejikim.erica@gmail.com>
2026-03-05 17:09:24 +0900

    feat : add card and enum class for card

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Card {
+    private final CardShape cardShape;
+    private final CardContents cardContents;
+
+    public Card(CardShape cardShape, CardContents cardContents) {
+        this.cardShape = cardShape;
+        this.cardContents = cardContents;
+    }
+
+    //getter setter 테스트 ???
+
+
+}
