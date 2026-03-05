9a8291895ae79c356352e4c15cc1b30a6bce3b78
zakie <realzzaekkii@gmail.com>
2026-03-05 11:11:45 +0900

    feat: 트럼프 카드의 문양 enum 추가

---
+++ src/main/java/domain/CardSuit.java
@@ -0,0 +1,14 @@
+package domain;
+
+public enum CardSuit {
+    SPADE("스페이드"),
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    CLUB("클로버");
+
+    private final String description;
+
+    CardSuit(String description) {
+        this.description = description;
+    }
+}
