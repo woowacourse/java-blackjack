9bdd13709af6e5bec50f91522b9c2bc2b0275f06
dahxxn <dahyeonhan4529@gmail.com>
2026-03-05 11:47:47 +0900

    test: Suit 기능 단위 테스트 추가
    - 스페이드, 하트, 다이아몬드, 클로버를 제외한 다른 문자가 들어오면 예외
      처리한다.

--- src/main/java/blackjack/domain/Suit.java
+++ src/main/java/blackjack/domain/Suit.java
@@ -1,2 +1,19 @@
-package blackjack.domain;public enum Suit {
+package blackjack.domain;
+
+public enum Suit {
+    SPADE("스페이드"),
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    CLOVER("클로버");
+
+    private final String name;
+
+    Suit(String name) {
+        this.name = name;
+    }
+
+    public static Suit of(String name) {
+        return SPADE;
+    }
 }
+
--- src/test/java/blackjack/domain/SuitTest.java
+++ src/test/java/blackjack/domain/SuitTest.java
@@ -1,4 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+package blackjack.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class SuitTest {
-  
+
+    @Test
+    void 스페이드_하트_다이아몬드_클로버를_제외한_다른_문자가_들어오면_예외가_처리한다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Suit.of("아무값");
+        });
+    }
 }
