0c86045f1d85e0362d9e825747dcf69ff2a71994
구민서 <minseo12379@gmail.com>
2026-03-05 20:26:59 +0900

    feat: 플레이어 승패 판정 구현 Co-authored-by: alstj2384 <ass91160@naver.com>

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -8,4 +8,17 @@ public class Player {
         this.name = name;
         this.cards = new Cards();
     }
+
+    public void add(Card card){
+        cards.add(card);
+    }
+
+    public boolean isPlayerWin(int target){
+        // 버스트판정 확인??
+        int sum = cards.getTotalSum();
+        if(cards.decideBurst(sum)){
+            return false;
+        }
+        return target < sum;
+    }
 }
--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
@@ -10,4 +11,23 @@ public class PlayerTest {
         assertDoesNotThrow(() -> new Player("minseo"));
     }
 
+    @Test
+    void 플레이어의_합계가_제시된_값보다_낮으면_거짓을_반환한다(){
+        Player player = new Player("minseo");
+        player.add(new Card(Suit.CLUB, Rank.TEN));
+        player.add(new Card(Suit.CLUB, Rank.ACE));
+
+        boolean result = player.isPlayerWin(20);
+        Assertions.assertEquals(result, true);
+    }
+
+    @Test
+    void 플레이어의_합계가_제시된_값보다_높으면_참을_반환한다(){
+        Player player = new Player("minseo");
+        player.add(new Card(Suit.CLUB, Rank.NINE));
+        player.add(new Card(Suit.CLUB, Rank.ACE));
+
+        boolean result = player.isPlayerWin(21);
+        Assertions.assertEquals(result, false);
+    }
 }
