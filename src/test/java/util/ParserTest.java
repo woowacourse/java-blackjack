71036f83f83ea29ba2e1f5ecbde72fc07c34fecf
yeo-li <parkseongyeol2110@gmail.com>
2026-03-05 11:09:56 +0900

    test(ParserTest): parseInput() 메서드 테스트 코드 추가

--- src/test/java/util/ParserTest.java
+++ src/test/java/util/ParserTest.java
@@ -9,7 +9,7 @@ class ParserTest {
 
     @Nested
     class ParseInputTest {
-        
+
         @Test
         void 입력값이_구분자_기준으로_나누어져야_한다() {
 
@@ -39,6 +39,23 @@ class ParserTest {
 
             // then
             List<String> expecteds = List.of("a", "b", "c");
+            for (int i = 0; i < 3; i++) {
+                Assertions.assertEquals(expecteds.get(i), actuals.get(i));
+            }
+        }
+
+        @Test
+        void 파싱한_입력값_중간에_공백이_있다면_제거하지_않는다() {
+
+            // given
+            String input = "    a b ,b  c ,c   d ";
+            String delimiter = ",";
+
+            // when
+            List<String> actuals = Parser.parseInput(input, delimiter);
+
+            // then
+            List<String> expecteds = List.of("a b", "b  c", "c   d");
             for (int i = 0; i < 3; i++) {
                 Assertions.assertEquals(expecteds.get(i), actuals.get(i));
             }
