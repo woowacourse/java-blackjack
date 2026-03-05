868a727fdfcadcf110a218434a6dd267b04eeb97
kangrae <kangrae0501@gmail.com>
2026-03-05 10:45:47 +0900

    feat: 카드 값 enum 생성

---
+++ src/main/java/blackjack/domain/CardValue.java
@@ -0,0 +1,24 @@
+package blackjack.domain;
+
+public enum CardValue {
+
+    A(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    J(10),
+    Q(10),
+    K(10);
+
+    private final int value;
+
+    CardValue(int value) {
+        this.value = value;
+    }
+}
