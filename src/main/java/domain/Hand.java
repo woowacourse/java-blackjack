4c3e2d531833e28656395d2e4b483748becff1ea
ghkdtjfxkd <syt1128@gmail.com>
2026-03-05 11:25:58 +0900

    feat: 카드 뽑기 기능 추가

--- src/main/java/domain/Hand.java
+++ src/main/java/domain/Hand.java
@@ -1,5 +1,29 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Hand {
 
+    private final List<Card> cards;
+
+    public Hand() {
+        this.cards = new ArrayList<>();
+    }
+
+    void drawCard() {
+        cards.add(createCard());
+    }
+
+    private Card createCard() {
+        return new Card(
+                CardRank.values()[new Random().nextInt(CardRank.values().length)],
+                CardMark.values()[new Random().nextInt(CardMark.values().length)]
+        );
+    }
+
+    List<Card> cards() {
+        return List.copyOf(cards);
+    }
 }
