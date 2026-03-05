24e056238984a47887c077cfe0b42eefbcd7af4d
softmoca <acyc0909@gmail.com>
2026-03-05 20:20:06 +0900

    feat(GameResult) : 게임 결과 생성 구현

---
+++ src/main/java/blackjack/GameResult.java
@@ -0,0 +1,15 @@
+package blackjack;
+
+public enum GameResult {
+    WIN("승"),
+    TIE("무"),
+    LOSE("패")
+    ;
+
+    private final String name;
+
+    GameResult(String name) {
+        this.name = name;
+    }
+
+}
