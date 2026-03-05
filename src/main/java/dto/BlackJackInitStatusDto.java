778262cd8c108e520064b1735768f716afea38b7
gleaming9 <gleaming9@khu.ac.kr>
2026-03-05 21:18:57 +0900

    feat(dto): 초기 상태 Dto 구현

---
+++ src/main/java/dto/BlackJackInitStatusDto.java
@@ -0,0 +1,40 @@
+package dto;
+
+import domain.Dealer;
+import domain.Hand;
+import domain.Player;
+import domain.card.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record BlackJackInitStatusDto(List<String> initStatus) {
+    public BlackJackInitStatusDto(Dealer dealer, List<Player> players) {
+        this(new ArrayList<>());
+
+        initStatus.add(getInitHeaderString(players.stream()
+                        .map(Player::getName)
+                        .toList()));
+
+        initStatus.add(getHandOutputString("딜러", dealer.getHand()));
+        for(Player player : players){
+            initStatus.add(getHandOutputString(player.getName(),player.getHand()));
+        }
+    }
+
+    private String getInitHeaderString(List<String> names){
+        return "딜러와 " + String.join(", ", names) + "에게 2장을 나누었습니다.";
+    }
+
+    private String getHandOutputString(String name, Hand hand) {
+        return name + "카드: " + getHandString(hand);
+    }
+
+    // todo : hand의 toString으로 구현하는게 맞는가? (토론)
+    private String getHandString(Hand hand) {
+        return hand.getCards().stream()
+                .map(Card::getCardName)
+                .collect(Collectors.joining(", "));
+    }
+}
---
+++ src/test/java/dto/BlackJackInitStatusDtoTest.java
@@ -0,0 +1,50 @@
+package dto;
+
+import domain.Dealer;
+import domain.Player;
+import domain.card.Card;
+import domain.card.Rank;
+import domain.card.Suit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlackJackInitStatusDtoTest {
+
+    List<Player> players;
+    Dealer dealer;
+
+    @BeforeEach
+    void setUp() {
+        players = new ArrayList<>();
+        Player player1 = new Player("봉구스");
+        player1.draw(new Card(Suit.CLUBS, Rank.ACE));
+        player1.draw(new Card(Suit.DIAMONDS, Rank.KING));
+
+        Player player2 = new Player("시오");
+        player2.draw(new Card(Suit.HEARTS, Rank.NUM5));
+        player2.draw(new Card(Suit.SPADES, Rank.JACK));
+        players.add(player1);
+        players.add(player2);
+
+        dealer = new Dealer();
+        dealer.draw(new Card(Suit.HEARTS, Rank.NUM4));
+        dealer.draw(new Card(Suit.CLUBS, Rank.NUM8));
+    }
+
+    @Test
+    void initStatus() {
+        // given, when
+        BlackJackInitStatusDto blackJackInitStatusDto = new BlackJackInitStatusDto(dealer, players);
+
+        // then
+        assertEquals("딜러와 봉구스, 시오에게 2장을 나누었습니다.", blackJackInitStatusDto.initStatus().get(0));
+        assertEquals("딜러카드: 4하트, 8클로버", blackJackInitStatusDto.initStatus().get(1));
+        assertEquals("봉구스카드: A클로버, K다이아몬드", blackJackInitStatusDto.initStatus().get(2));
+        assertEquals("시오카드: 5하트, J스페이드", blackJackInitStatusDto.initStatus().get(3));
+    }
+}
