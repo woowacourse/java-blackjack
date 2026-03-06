f34c1fa2cbe6937b36ccfc68743a9ccce030994f
sun <sun007021@gmail.com>
2026-03-06 11:11:37 +0900

    test: 딜러 카드 합 17 이상인지 판단 테스트

--- src/test/java/DealerTest.java
+++ src/test/java/DealerTest.java
@@ -16,4 +16,17 @@ public class DealerTest {
         dealer2.receiveOneCard(new Card("10", "스페이드"));
         assertThat(dealer2.calculateTotalScore()).isEqualTo(13);
     }
+
+    @Test
+    void is_dealer_done_when_total_score_is_17_or_more() {
+        Dealer dealer1 = new Dealer();
+        dealer1.receiveOneCard(new Card("10", "하트"));
+        dealer1.receiveOneCard(new Card("7", "스페이드"));
+        assertThat(dealer1.isDealerDone()).isEqualTo(true);
+
+        Dealer dealer2 = new Dealer();
+        dealer2.receiveOneCard(new Card("2", "하트"));
+        dealer2.receiveOneCard(new Card("7", "스페이드"));
+        assertThat(dealer2.isDealerDone()).isEqualTo(false);
+    }
 }
