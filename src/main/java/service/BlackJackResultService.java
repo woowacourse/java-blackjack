14624290ef46c8083d2151388f257018d3515bf2
gleaming9 <gleaming9@khu.ac.kr>
2026-03-06 18:11:53 +0900

    refactor: 메서드 분리 및 리팩터링
    - 승패 비교 로직을 담당하는 calculateWinningStatus 메서드 구현
    - 기존에 for문으로 동작하던 로직을 stream으로 변경

--- src/main/java/service/BlackJackResultService.java
+++ src/main/java/service/BlackJackResultService.java
@@ -6,9 +6,10 @@ import domain.WinningStatus;
 import dto.FinalResultDto;
 import dto.ScoreResultDto;
 
-import java.util.List;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BlackJackResultService {
 
@@ -21,32 +22,29 @@ public class BlackJackResultService {
         return FinalResultDto.from(playerResults);
     }
 
-    // todo : stream 리팩토링
     private Map<String, WinningStatus> createPlayerResults(Dealer dealer, List<Player> players) {
-        Map<String, WinningStatus> map = new LinkedHashMap<>();
-        for(Player player : players){
-            if(player.isBurst()){
-                map.put(player.getName(), WinningStatus.LOSE);
-                continue;
-            }
-
-            if(dealer.isBurst()){
-                map.put(player.getName(), WinningStatus.WIN);
-                continue;
-            }
-
-            if(dealer.getHand().getSum() > player.getHand().getSum()){
-                map.put(player.getName(), WinningStatus.LOSE);
-            }
-            if(dealer.getHand().getSum() == player.getHand().getSum()){
-                map.put(player.getName(), WinningStatus.DRAW);
-            }
-            if(dealer.getHand().getSum() < player.getHand().getSum()){
-                map.put(player.getName(), WinningStatus.WIN);
-            }
-        }
-        return map;
+        return players.stream()
+                .collect(Collectors.toMap(
+                        Player::getName,
+                        player -> calculateWinningStatus(dealer, player),
+                        (x, y) -> y, LinkedHashMap::new)
+                );
     }
 
+    private WinningStatus calculateWinningStatus(Dealer dealer, Player player) {
+        if (player.isBurst()) {
+            return WinningStatus.LOSE;
+        }
+        if (dealer.isBurst()) {
+            return WinningStatus.WIN;
+        }
+        if (dealer.getHand().getSum() > player.getHand().getSum()) {
+            return WinningStatus.LOSE;
+        }
+        if (dealer.getHand().getSum() == player.getHand().getSum()) {
+            return WinningStatus.DRAW;
+        }
+        return WinningStatus.WIN;
+    }
 }
 
