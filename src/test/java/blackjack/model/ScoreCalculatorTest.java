2e0843e2e5d7306fc6faeea26b3e3e333110af34
smini <sm020320@naver.com>
2026-03-06 16:26:10 +0900

    test: 버스트 판정 기능 테스트 검증

--- src/test/java/blackjack/model/ScoreCalculatorTest.java
+++ src/test/java/blackjack/model/ScoreCalculatorTest.java
@@ -1,6 +1,6 @@
 package blackjack.model;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +10,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ScoreCalculatorTest {
 
+    ScoreCalculator scoreCalculator;
+
+    @BeforeEach
+    void setUp() {
+        scoreCalculator = new ScoreCalculator();
+    }
+
     @Test
     @DisplayName("카드의 숫자 합을 계산한다")
     void calculateScoreTest() {
         // given
-        ScoreCalculator scoreCalculator = new ScoreCalculator();
         Deck deck = new Deck();
         List<Card> cardsOne = List.of(deck.giveCard(), deck.giveCard());
         List<Card> cardsTwo = List.of(deck.giveCard(), deck.giveCard());
@@ -28,4 +34,21 @@ class ScoreCalculatorTest {
         assertThat(cardScoreTwo).isEqualTo(7);
     }
 
+    @Test
+    @DisplayName("21이 넘으면 버스트 처리한다.")
+    void isBurstTest() {
+        // given
+        int score = 23;
+        // when & then
+        assertThat(scoreCalculator.isBurst(score)).isTrue();
+    }
+
+    @Test
+    @DisplayName("21이 넘지 않으면 버스트 처리하지 않는다.")
+    void isBurstTest2() {
+        // given
+        int score = 17;
+        // when & then
+        assertThat(scoreCalculator.isBurst(score)).isFalse();
+    }
 }
