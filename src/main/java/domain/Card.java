f7622cf6e5dd70116435f729d4202385c46a372b
yeo-li <parkseongyeol2110@gmail.com>
2026-03-06 21:54:17 +0900

    feat(Card): 카드 점수 계산 및 카드명 반환 기능 구현

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -6,11 +6,11 @@ import java.util.Objects;
 
 public class Card {
 
-    private final Rank Rank;
+    private final Rank rank;
     private final Suit suit;
 
     public Card(constant.Rank rank, Suit suit) {
-        Rank = rank;
+        this.rank = rank;
         this.suit = suit;
     }
 
@@ -20,11 +20,31 @@ public class Card {
             return false;
         }
         Card card = (Card) o;
-        return Rank == card.Rank && suit == card.suit;
+        return rank == card.rank && suit == card.suit;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(Rank, suit);
+        return Objects.hash(rank, suit);
+    }
+    
+    public int calculateScore() {
+        if (rank.equals(Rank.J) || rank.equals(Rank.Q) || rank.equals(Rank.K)) {
+            return 10;
+        }
+
+        if (rank.equals(Rank.ACE)) {
+            return 11;
+        }
+
+        return Integer.parseInt(rank.getRank());
+    }
+
+    public boolean isAce() {
+        return rank.equals(Rank.ACE);
+    }
+
+    public String getName() {
+        return rank.getRank() + suit.getSuit();
     }
 }
