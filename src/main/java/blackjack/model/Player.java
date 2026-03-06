09176f4619b22a4f96255066cbe0a5745f8e0dbc
Kim, Joon <kjoon418@naver.com>
2026-03-06 16:52:07 +0900

    feat: Player에 이름 검증 로직 추가

--- src/main/java/blackjack/model/Player.java
+++ src/main/java/blackjack/model/Player.java
@@ -8,8 +8,15 @@ public class Player {
     private final Hand hand;
 
     public Player(String name, Hand hand) {
+        validate(name);
         this.name = name;
         this.hand = hand;
+    }
+
+    private void validate(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("플레이어 이름은 null 이거나 empty 일 수 없습니다.");
+        }
     }
 
     public String getName() {
--- src/test/java/blackjack/model/PlayerTest.java
+++ src/test/java/blackjack/model/PlayerTest.java
@@ -1,6 +1,7 @@
 package blackjack.model;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,5 +21,25 @@ class PlayerTest {
 
         // then
         assertThat(player.getName()).isEqualTo(playerName);
+    }
+
+    @Test
+    void 이름이_null인_경우_에러를_던진다() {
+        // given
+        String playerName = null;
+
+        // when & then
+        assertThatThrownBy(() -> new Player(playerName, hand))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름이_blank인_경우_에러를_던진다() {
+        // given
+        String playerName = "";
+
+        // when & then
+        assertThatThrownBy(() -> new Player(playerName, hand))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
