3c5a69e2e0ac1202eaf1c3bf8cbe841566920eed
최용준 <yj9107vv@gmail.com>
2026-03-06 22:16:51 +0900

    test(PlayerTest): ace 1/11 처리 및, 플레이어 카드 개수 테스트

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -33,7 +33,7 @@ class PlayerTest {
                 Card.of(Rank.EIGHT, Suit.DIAMOND),
                 Card.of(Rank.ACE, Suit.HEART));
 
-        int cardScore = Player.of(cards).calculateScore();
+        int cardScore = Player.of(cards, "워니").calculateScore();
 
         if (cardScore > 21 && cards.stream().anyMatch(Card::isAce)) {
             cardScore -= 10;
@@ -41,6 +41,44 @@ class PlayerTest {
 
         int expect = 20;
         assertThat(cardScore).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("ACE가 2개 이상인 경우 ACE 1/11 처리")
+    void ACE_2개_이상_정상_테스트() {
+        List<Card> cards = List.of(
+                Card.of(Rank.ACE, Suit.DIAMOND),
+                Card.of(Rank.NINE, Suit.CLOVER),
+                Card.of(Rank.EIGHT, Suit.DIAMOND),
+                Card.of(Rank.ACE, Suit.HEART));
+
+        Player player = Player.of(cards, "포비");
+
+        int aceCount = (int) cards.stream().filter(Card::isAce).count();
+        int cardScore = cards.stream()
+                .mapToInt(Card::getScore)
+                .sum();
+
+        for (int i = 0; i < aceCount; i++) {
+            if (cardScore <= 21) break;
+            cardScore -= 10;
+        }
+
+
+        int expect = 19;
+        assertThat(cardScore).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("플레이어 객체 생성 시 2장의 카드를 보유한지 테스트")
+    void 카드_2장_보유_테스트() {
+        Cards cards = Cards.of();
+        Player player = Player.of(cards.drawInitialHand(), "pobi");
+
+        int cardSize = player.getCardsInfo().size();
+
+        int expect = 2;
+        assertThat(cardSize).isEqualTo(expect);
     }
 
 }
