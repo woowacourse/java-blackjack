b4a7f5e240d2fc862cbfa2db35e9feb581380731
yeo-li <parkseongyeol2110@gmail.com>
2026-03-06 21:54:31 +0900

    test(DealerTest): getOnlyFirstHand 테스트 코드 추가

---
+++ src/test/java/domain/DealerTest.java
@@ -0,0 +1,38 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import constant.Rank;
+import constant.Suit;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class DealerTest {
+
+    @Nested
+    class GetOnlyFirstHandTest {
+
+        @Nested
+        class Success {
+
+            @Test
+            void 딜러의_첫번째_카드만_담긴_손패를_반환해야_한다() {
+
+                // given
+                Dealer dealer = new Dealer("딜러");
+                Card firstCard = new Card(Rank.TEN, Suit.HEART);
+                Card secondCard = new Card(Rank.ACE, Suit.SPADE);
+                dealer.addCard(firstCard);
+                dealer.addCard(secondCard);
+
+                // when
+                Hand actual = dealer.getOnlyFirstHand();
+
+                // then
+                assertThat(actual.getCardNames())
+                    .hasSize(1)
+                    .containsExactly(firstCard.getName());
+            }
+        }
+    }
+}
