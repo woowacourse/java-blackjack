c24ae9522687a8d0284efec26958edda7cfbe64b
최용준 <yj9107vv@gmail.com>
2026-03-06 22:15:58 +0900

    feat(Dealer): 딜러 클래스 객체 생성
    - 정적 팩토리 메서드
    - 점수 계산 메서드 calculateScore()
    - ace 판정 전 점수 계산 메서드 calculateRawScore()
    - ace 관련 메서드 countAce(), adjustForAce(), isBustWithAce()
    - 딜러 카드 정보 반환 메서드 getDealerInfo(), getDealerInitialInfo()

---
+++ src/main/java/domain/Dealer.java
@@ -0,0 +1,70 @@
+package domain;
+
+import java.util.List;
+import meesage.OutputMessage;
+
+public class Dealer {
+
+    private static final String NAME = "딜러";
+
+    private List<Card> cards;
+
+    private Dealer(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public static Dealer of(List<Card> cards) {
+        return new Dealer(cards);
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public int calculateScore() {
+        int cardScore = calculateRawScore();
+        int aceCount = countAce();
+
+        for (int i = 0; i < aceCount; i++) {
+            cardScore = adjustForAce(cardScore);
+        }
+
+        return cardScore;
+    }
+
+    private int calculateRawScore() {
+        return cards.stream()
+                .mapToInt(Card::getScore)
+                .sum();
+    }
+
+    private int countAce() {
+        return (int) cards.stream().filter(Card::isAce).count();
+    }
+
+    private int adjustForAce(int cardScore) {
+        if (isBustWithAce(cardScore)) {
+            cardScore -= 10;
+        }
+        return cardScore;
+    }
+
+    private boolean isBustWithAce(int cardScore) {
+        return cardScore > 21;
+    }
+
+    public List<String> getCardsInfo() {
+        return cards.stream()
+                .map(Card::getName)
+                .toList();
+    }
+
+    public String getDealerInfo() {
+        return NAME + OutputMessage.CARD_TEXT.getMessage() + OutputMessage.format(getCardsInfo());
+    }
+
+    public String getDealerInitialInfo() {
+        return NAME + OutputMessage.CARD_TEXT.getMessage() + getCardsInfo().getFirst();
+    }
+
+}
