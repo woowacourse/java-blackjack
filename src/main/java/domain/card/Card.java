7245ceda2396f4fbac222ce73572bf081719c6fc
koreaioi <koreaioi@gachon.ac.kr>
2026-03-07 01:48:29 +0900

    refactor: 불변객체 Card를 record로 변경

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -1,31 +1,12 @@
 package domain.card;
 
-import java.util.Objects;
-
-public class Card {
-
-    private final CardDenomination denomination;
-    private final CardEmblem emblem;
-
-    private Card(CardDenomination denomination, CardEmblem emblem) {
-        this.denomination = denomination;
-        this.emblem = emblem;
-    }
+public record Card(
+        CardDenomination denomination,
+        CardEmblem emblem
+) {
 
     public static Card of(CardDenomination denomination, CardEmblem emblem) {
         return new Card(denomination, emblem);
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (object == null || getClass() != object.getClass()) return false;
-        Card card = (Card) object;
-        return denomination == card.denomination && emblem == card.emblem;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(denomination, emblem);
     }
 
     public String toDisplay() {
