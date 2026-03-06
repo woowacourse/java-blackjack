55443ec3079f18847971d3a812a1d0651192a694
snow___shower <wonsb25@gmail.com>
2026-03-06 21:37:52 +0900

    fix: 한 명도 입력받을 수 있도록 수정

--- src/main/java/util/Parser.java
+++ src/main/java/util/Parser.java
@@ -5,13 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Parser {
-    private static final String SEPARATOR_ERR_MSG = "구분자는 ,로 해야합니다.";
+    public static List<String> separateBySeparator(String input) {
 
-    public static List<String> separateBySeparator(String input, String separator) {
-        if (!input.contains(separator)) {
-            throw new IllegalArgumentException(SEPARATOR_ERR_MSG);
-        }
-        return Arrays.stream(input.split(separator))
+        return Arrays.stream(input.split(","))
                 .map(String::trim)
                 .collect(Collectors.toList());
     }
