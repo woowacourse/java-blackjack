61d9f36460d010a83b169aa6f1df31a06a437e23
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-03-06 14:04:51 +0900

    test(domain): 버스트 판단 여부 테스트 추가

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -50,6 +50,7 @@ class PlayerTest {
     }
 
     @DisplayName("ACE가 하나는 11이고 하나는 1이어야 함")
+    @Test
     void 카드_합계_에이스_11_1_테스트() {
         player.add(new Card(CardShape.SPADE, CardRank.ACE));
         player.add(new Card(CardShape.SPADE, CardRank.ACE));
@@ -60,6 +61,7 @@ class PlayerTest {
     }
 
     @DisplayName("ACE가 둘 다 1이어야 함")
+    @Test
     void 카드_합계_에이스_2개_1_테스트() {
         player.add(new Card(CardShape.SPADE, CardRank.ACE));
         player.add(new Card(CardShape.SPADE, CardRank.ACE));
@@ -67,5 +69,26 @@ class PlayerTest {
 
         int result = player.calculateScore();
         assertThat(result).isEqualTo(12);
+    }
+
+    @DisplayName("Burst 여부 판단 - burst")
+    @Test
+    void Burst_여부_판단_burst_정상_테스트() {
+        player.add(new Card(CardShape.SPADE, CardRank.TEN));
+        player.add(new Card(CardShape.HEART, CardRank.TWO));
+        player.add(new Card(CardShape.HEART, CardRank.TEN));
+
+        boolean result = player.isBurst();
+        assertThat(result).isEqualTo(true);
+    }
+
+    @DisplayName("Burst 여부 판단 - not Burst")
+    @Test
+    void Burst_여부_판단_not_burst_정상_테스트() {
+        player.add(new Card(CardShape.SPADE, CardRank.TEN));
+        player.add(new Card(CardShape.HEART, CardRank.TWO));
+
+        boolean result = player.isBurst();
+        assertThat(result).isEqualTo(false);
     }
 }
