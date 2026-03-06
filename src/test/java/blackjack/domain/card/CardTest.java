0ef4c1fd3b3660c278d32f61eb878609b6b8af99
dongheeHan <ark182818@gmail.com>
2026-03-06 09:55:57 +0900

    test: 테스트 메서드명을 한글에서 영어로 변경

--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -9,21 +9,21 @@ class CardTest {
 
     @Test
     @DisplayName("ACE 랭크의 카드는 isAce가 참이다")
-    void ACE_랭크의_카드는_isAce가_참이다() {
+    void isAce_ShouldReturnTrue_WhenRankIsAce() {
         Card aceCard = new Card(Suit.CLUB, Rank.ACE);
         assertThat(aceCard.isAce()).isTrue();
     }
 
     @Test
     @DisplayName("ACE가 아닌 랭크의 카드는 isAce가 거짓이다")
-    void ACE가_아닌_랭크의_카드는_isAce가_거짓이다() {
+    void isAce_ShouldReturnFalse_WhenRankIsNotAce() {
         Card kingCard = new Card(Suit.CLUB, Rank.KING);
         assertThat(kingCard.isAce()).isFalse();
     }
 
     @Test
     @DisplayName("ACE 바로 다음 순서인 TWO 카드는 isAce가 거짓이다")
-    void ACE_바로_다음_순서인_TWO_카드는_isAce가_거짓이다() {
+    void isAce_ShouldReturnFalse_WhenRankIsTwo() {
         Card twoCard = new Card(Suit.CLUB, Rank.TWO);
         assertThat(twoCard.isAce()).isFalse();
     }
--- src/test/java/blackjack/domain/card/RankTest.java
+++ src/test/java/blackjack/domain/card/RankTest.java
@@ -9,19 +9,19 @@ class RankTest {
 
     @Test
     @DisplayName("ACE 카드는 isAce가 참이다")
-    void ACE_카드는_isAce가_참이다() {
+    void isAce_ShouldReturnTrue_ForAce() {
         assertThat(Rank.ACE.isAce()).isTrue();
     }
 
     @Test
     @DisplayName("ACE가 아닌 카드는 isAce가 거짓이다")
-    void ACE가_아닌_카드는_isAce가_거짓이다() {
+    void isAce_ShouldReturnFalse_ForNonAce() {
         assertThat(Rank.KING.isAce()).isFalse();
     }
 
     @Test
     @DisplayName("ACE 바로 다음 순서인 TWO는 isAce가 거짓이다")
-    void ACE_바로_다음_순서인_TWO는_isAce가_거짓이다() {
+    void isAce_ShouldReturnFalse_ForTwo() {
         assertThat(Rank.TWO.isAce()).isFalse();
     }
 }
