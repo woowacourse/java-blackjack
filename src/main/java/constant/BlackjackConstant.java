5aba33e7d91b0eeac0c2008945af1c652bdc244f
zakie <realzzaekkii@gmail.com>
2026-03-06 16:49:07 +0900

    feat: 딜러가 카드를 추가로 뽑는 기준에 대한 상수 추가

--- src/main/java/constant/BlackjackConstant.java
+++ src/main/java/constant/BlackjackConstant.java
@@ -4,4 +4,5 @@ public class BlackjackConstant {
     public static final int INIT_DRAW_COUNT = 2;
     public static final int HIT_DRAW_COUNT = 1;
     public static final int BUST_BOUND = 21;
+    public static final int DEALER_DRAW_BOUND = 16;
 }
