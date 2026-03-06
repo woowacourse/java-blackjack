b78c19bee75349be9f73033b12819b9d6af9950f
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-06 13:13:37 +0900

    refactor(OutputView) : 출력 메시지 상수화

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -13,8 +13,17 @@ import model.dto.PlayerWinning;
 
 public class OutputView {
 
+    private static final String CARD_TEXT = "카드: ";
+    private static final String DEALER_DRAW_ONE_CARD_TEXT = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    private static final String FINAL_RESULT_TEXT = "## 최종 승패";
+    private static final String DEALER_INIT_DECK_TEXT = "딜러와 ";
+    private static final String INIT_DECK_TEXT = "에게 2장을 나누었습니다.";
+    private static final String DEALER_CARD_TEXT = "딜러카드: ";
+    private static final String SCORE_TEXT = " - 결과: ";
+    private static final String DEALER_TEXT = "딜러:";
+
     public static void printInitDeck(List<PlayerResult> players, PlayerResult dealer) {
-        List<String> playerNames = players.stream().map(p -> p.name().get()).toList();
+        List<String> playerNames = players.stream().map(p -> p.name().value()).toList();
         printInitDeckDrawMessage(playerNames);
         printDealerInitDeck(dealer.deck().getFirst());
         printPlayersCurrentDeck(players);
@@ -23,11 +32,11 @@ public class OutputView {
 
     public static void printPlayerCurrentDeck(PlayerResult playerResult) {
         List<String> cardString = playerResult.deck().stream().map(Card::getString).toList();
-        System.out.println(playerResult.name().get() + "카드: " + String.join(", ", cardString));
+        System.out.println(playerResult.name().value() + CARD_TEXT + String.join(", ", cardString));
     }
 
     public static void printDealerCardDrawMessage() {
-        System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+        System.out.println(DEALER_DRAW_ONE_CARD_TEXT);
         printNewLine();
     }
 
@@ -44,18 +53,18 @@ public class OutputView {
     }
 
     public static void printResult(ParticipantWinning participantWinning) {
-        System.out.println("## 최종 승패");
+        System.out.println(FINAL_RESULT_TEXT);
         printDealerResult(participantWinning.dealerWinning());
         printPlayersResult(participantWinning.playersWinning());
     }
 
 
     private static void printInitDeckDrawMessage(List<String> players) {
-        System.out.println("딜러와 " + String.join(", ", players) + "에게 2장을 나누었습니다.");
+        System.out.println(DEALER_INIT_DECK_TEXT + String.join(", ", players) + INIT_DECK_TEXT);
     }
 
     private static void printDealerInitDeck(Card card) {
-        System.out.println("딜러카드: " + card.getString());
+        System.out.println(DEALER_CARD_TEXT + card.getString());
     }
 
     private static void printPlayersCurrentDeck(List<PlayerResult> players) {
@@ -66,11 +75,11 @@ public class OutputView {
 
     private static void printPlayerScore(PlayerResult playerResult) {
         List<String> cardString = playerResult.deck().stream().map(Card::getString).toList();
-        System.out.println(playerResult.name().get() + "카드: " + String.join(", ", cardString) + " - 결과: " + playerResult.score());
+        System.out.println(playerResult.name().value() + CARD_TEXT + String.join(", ", cardString) + SCORE_TEXT + playerResult.score());
     }
 
     private static void printDealerResult(DealerWinning dealerWinning) {
-        System.out.print("딜러:");
+        System.out.print(DEALER_TEXT);
         for(Entry<MatchStatus, Integer> matchStatus : dealerWinning.getDealerWinning().entrySet()) {
             printStatusResult(matchStatus.getKey(), matchStatus.getValue());
         }
@@ -92,7 +101,7 @@ public class OutputView {
     }
 
     private static void printPlayerResult(PlayerWinning playerWinning) {
-        System.out.println(playerWinning.name().get() + ": " + playerWinning.matchStatus().getStatus());
+        System.out.println(playerWinning.name().value() + ": " + playerWinning.matchStatus().getStatus());
     }
 
 
