229cc230ef55e594e99a523d5e7dc0486523e2e4
koreaioi <koreaioi@gachon.ac.kr>
2026-03-06 21:04:31 +0900

    feat: PlayerName 검증 및 예외메세지 추가

--- src/main/java/domain/player/PlayerName.java
+++ src/main/java/domain/player/PlayerName.java
@@ -5,13 +5,20 @@ public record PlayerName(
 ) {
 
     public static PlayerName from(String name) {
+        validateNameIsBlank(name);
         validateNameLength(name);
         return new PlayerName(name);
     }
 
+    private static void validateNameIsBlank(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 빈칸일 수 없습니다.");
+        }
+    }
+
     private static void validateNameLength(String name) {
         if(name.length() > 5) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("이름 1자 이상, 5자이하의 문자입니다.");
         }
     }
 
