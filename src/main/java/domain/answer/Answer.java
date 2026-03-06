873ffdd7e52e912a6593186f7bde4b23d6d7bee6
koreaioi <koreaioi@gachon.ac.kr>
2026-03-06 13:36:35 +0900

    feat: 사용자 대답 Answer enum 추가

---
+++ src/main/java/domain/answer/Answer.java
@@ -0,0 +1,29 @@
+package domain.answer;
+
+import java.util.Arrays;
+
+public enum Answer {
+
+    YES("y"),
+    NO("n")
+    ;
+
+    private final String answer;
+
+    private Answer(String ansewr) {
+        this.answer = ansewr;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public static Answer from(String value) {
+        return Arrays.stream(Answer.values())
+                .filter(v -> v.answer.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("y 또는 n을 입력해주세요.")
+                );
+    }
+
+}
