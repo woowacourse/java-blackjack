51607db0af779f4e4f7aaa413d6c790344095744
sun <sun007021@gmail.com>
2026-03-06 11:03:26 +0900

    test: 딜러 카드 점수 합계 반환 테스트

---
+++ src/test/java/DealerTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+public class DealerTest {
+
+    @Test
+    void calculate_dealer_total_score() {
+        Dealer dealer1 = new Dealer();
+        dealer1.receiveOneCard(new Card("A", "하트"));
+        dealer1.receiveOneCard(new Card("Q", "스페이드"));
+        assertThat(dealer1.calculateTotalScore()).isEqualTo(21);
+
+        Dealer dealer2 = new Dealer();
+        dealer2.receiveOneCard(new Card("3", "하트"));
+        dealer2.receiveOneCard(new Card("10", "스페이드"));
+        assertThat(dealer2.calculateTotalScore()).isEqualTo(13);
+    }
+}
