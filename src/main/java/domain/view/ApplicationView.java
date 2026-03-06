8741831dbe8c32ad3bc09e671cd2aaf34bdb335c
koreaioi <koreaioi@gachon.ac.kr>
2026-03-06 13:38:00 +0900

    feat: ApplicationView 추가

---
+++ src/main/java/domain/view/ApplicationView.java
@@ -0,0 +1,25 @@
+package domain.view;
+
+import java.util.function.Supplier;
+
+public class ApplicationView {
+
+    public InputReader reader;
+    public OutputWriter writer;
+
+    public ApplicationView(InputReader reader, OutputWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    private <T> T retry(Supplier<T> task) {
+        while (true) {
+            try {
+                return task.get();
+            } catch (IllegalArgumentException e) {
+                writer.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+}
