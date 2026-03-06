5ad6a3f4c59addcb440d3a274ed5c077eb6e18c2
심호경 <ghrudtla@naver.com>
2026-03-06 18:12:08 +0900

    feat: CardNumber 도메인에 Symbol 추가

--- src/main/java/domain/CardNumber.java
+++ src/main/java/domain/CardNumber.java
@@ -1,26 +1,33 @@
 package domain;
 
 public enum CardNumber {
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    J(10),
-    Q(10),
-    K(10);
+    ACE("A", 1),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    J("J", 10),
+    Q("Q", 10),
+    K("K", 10);
 
+    private String symbol;
     private final int value;
-    CardNumber(int value) {
+
+    CardNumber(String symbol, int value) {
+        this.symbol = symbol;
         this.value = value;
     }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
     public int getValue() {
         return value;
     }
-
 }
