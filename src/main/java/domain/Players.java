9ca1e73b3e808aaef0a12a1fff71e86f80152e1e
심호경 <ghrudtla@naver.com>
2026-03-06 18:17:36 +0900

    feat: Player 리스트를 모아놓은 Players 도메인 구현

---
+++ src/main/java/domain/Players.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.util.List;
+
+public class Players {
+    private final List<Player> players;
+
+    private Players(List<Player> players) {
+        this.players = List.copyOf(players);
+    }
+
+    public static Players from(List<Player> players) {
+        return new Players(players);
+    }
+
+    public List<Player> getPlayers() {
+        return List.copyOf(players);
+    }
+}
