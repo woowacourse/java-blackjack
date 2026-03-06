0bd7b24e55432bf1e5039b8328e7145705f36119
Minjae Kang <rkdalswoals@gmail.com>
2026-03-06 11:22:56 +0900

    feat: suit 필드 추가, cardnumber 상수 추가

--- src/main/java/domain/Suit.java
+++ src/main/java/domain/Suit.java
@@ -1,9 +1,15 @@
 package domain;
 
 public enum Suit {
-    CLOVER,
-    HEART,
-    DIAMOND,
-    SPADE
+    CLOVER("클로버"),
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    SPADE("스페이스")
     ;
+
+    private final String value;
+
+    Suit(String value) {
+        this.value = value;
+    }
 }
