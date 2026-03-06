63d7f7fca83c1e0679cec2cad9172aab05a275a8
yeo-li <parkseongyeol2110@gmail.com>
2026-03-06 22:03:50 +0900

    feat(BlackjackApplication): 블랙잭 어플리케이션 구현 완료

---
+++ src/main/java/BlackjackApplication.java
@@ -0,0 +1,9 @@
+import controller.BlackjackController;
+
+public class BlackjackApplication {
+
+    public static void main(String[] args) {
+        BlackjackController blackjackController = new BlackjackController();
+        blackjackController.start();
+    }
+}
