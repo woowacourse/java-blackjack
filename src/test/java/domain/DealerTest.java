17ae51f06d8a92c7d5878b8b2ce924766dfbb905
EveryPine <kenton.lim@gmail.com>
2026-03-06 13:22:16 +0900

    test(dealer): 딜러 피치 기능 테스트 추가

---
+++ src/test/java/domain/DealerTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import blackjack.domain.Dealer;
+import blackjack.domain.Hand;
+import blackjack.domain.Player;
+import blackjack.domain.Trump;
+import java.util.List;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DealerTest {
+
+    @Test
+    @DisplayName("딜러 pitch 테스트")
+    void 딜러_피치_테스트() {
+        Trump trump = new Trump();
+        Dealer dealer = new Dealer(new Hand(), trump);
+        List<Player> players = List.of(
+            new Player("pobi"),
+            new Player("jason")
+        );
+        int expected = 13 * 4 - (players.size() + 1) * 2;
+
+        dealer.pitch(players);
+
+        assertThat(trump).extracting("deck")
+            .asInstanceOf(InstanceOfAssertFactories.LIST)
+            .hasSize(expected);
+    }
+
+}
