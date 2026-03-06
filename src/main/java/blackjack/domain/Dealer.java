65cdc52214802677e568b6c1173b27496d6be9f2
kangrae <kangrae0501@gmail.com>
2026-03-06 16:52:37 +0900

    refactor: Player와 Dealer를 상속관계로 변경
    - Dealer는 Player의 기능을 모두 가짐으로 변경

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -1,45 +1,11 @@
 package blackjack.domain;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Dealer extends Player {
 
-public class Dealer {
-
-    private final List<Card> cards;
-    private final PlayerName name;
+    private static final String DEALER_NAME = "딜러";
 
     public Dealer() {
-        this.cards = new ArrayList<>();
-        this.name = new PlayerName("딜러");
-    }
-
-    public void bring(Card card) {
-        cards.add(card);
-    }
-
-    public List<String> getCardsName() {
-        List<String> cardsName = new ArrayList<>();
-        for (Card card : cards) {
-            cardsName.add(card.getName());
-        }
-
-        return cardsName;
-    }
-
-    public String getName() {
-        return name.name();
-    }
-
-    public int calculateCardsValue() {
-        int sum = 0;
-        for (Card card : cards) {
-            sum += card.getValue();
-        }
-        return sum;
-    }
-
-    public boolean isBurst() {
-        return calculateCardsValue() > 21;
+        super(DEALER_NAME);
     }
 
 }
