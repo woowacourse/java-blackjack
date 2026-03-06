df03b798334c178a96e5e882ba3a8a6bbe2a1476
심호경 <ghrudtla@naver.com>
2026-03-06 18:24:42 +0900

    feat: 카드 점수를 계산하는 ScoreCalculator 도메인 구현

---
+++ src/main/java/domain/ScoreCalculator.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.List;
+
+public class ScoreCalculator {
+    public static int calculate(List<Card> cards) {
+        int sum = 0;
+        int aceCount = 0;
+        for (Card card : cards) {
+            sum += card.getCardNumber().getValue();
+            if (card.getCardNumber() == CardNumber.ACE) {
+                aceCount++;
+            }
+        }
+
+        for (int i = 0; i < aceCount; i++) {
+            if (sum + 10 < 21) {
+                sum += 10;
+            }
+        }
+        return sum;
+    }
+}
