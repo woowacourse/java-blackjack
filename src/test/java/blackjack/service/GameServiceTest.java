cf1f0bfc4418a6afe592bcb93625cf2f11a8f895
kangrae <kangrae0501@gmail.com>
2026-03-06 16:28:38 +0900

    fix: Ace 처리 기능 추가로 인해 발생한 테스트 코드 수정

--- src/test/java/blackjack/service/GameServiceTest.java
+++ src/test/java/blackjack/service/GameServiceTest.java
@@ -2,9 +2,12 @@ package blackjack.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import blackjack.domain.Card;
+import blackjack.domain.CardValue;
 import blackjack.domain.Dealer;
 import blackjack.domain.Deck;
 import blackjack.domain.Player;
+import blackjack.domain.Shape;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,8 +36,8 @@ class GameServiceTest {
         GameService gameService = new GameService();
         Player player = new Player("흑곰");
         Dealer dealer = new Dealer(new Deck());
-        player.bring(dealer.bringCard());
-        dealer.bring(dealer.bringCard());
+        player.bring(new Card(CardValue.SEVEN, Shape.DIAMOND));
+        dealer.bring(new Card(CardValue.EIGHT, Shape.DIAMOND));
 
         // when
         boolean isDealerWinning = gameService.isDealerWinning(player, dealer);
