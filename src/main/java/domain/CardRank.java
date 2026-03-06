5908484f99f1c0a92e84e082b96f9b47af05f03d
zakie <realzzaekkii@gmail.com>
2026-03-06 14:40:30 +0900

    feat: 트럼프 카드 랭크에 점수리스트 필드 추가

--- src/main/java/domain/CardRank.java
+++ src/main/java/domain/CardRank.java
@@ -1,14 +1,28 @@
 package domain;
 
+import java.util.List;
+
 public enum CardRank {
-    ACE("A"), TWO("2"), THREE("3"),
-    FOUR("4"), FIVE("5"), SIX("6"),
-    SEVEN("7"), EIGHT("8"), NINE("9"),
-    TEN("10"), JACK("J"), QUEEN("Q"), KING("K");
+    ACE("A", List.of(1, 11)),
+    TWO("2", List.of(2)),
+    THREE("3", List.of(3)),
+    FOUR("4", List.of(4)),
+    FIVE("5", List.of(5)),
+    SIX("6", List.of(6)),
+    SEVEN("7", List.of(7)),
+    EIGHT("8", List.of(8)),
+    NINE("9", List.of(9)),
+    TEN("10", List.of(10)),
+    JACK("J", List.of(10)),
+    QUEEN("Q", List.of(10)),
+    KING("K", List.of(10));
 
-    private final String value;
+    private final String description;
+    private final List<Integer> scores;
 
-    CardRank(String value) {
-        this.value = value;
+    private CardRank(String description, List<Integer> scores) {
+        this.description = description;
+        this.scores = scores;
     }
+
 }
