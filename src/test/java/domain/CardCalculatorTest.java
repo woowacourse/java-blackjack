2d6850678914949ebdb87603515ae1894660dfbf
나의찬 <cab07036@naver.com>
2026-03-06 23:20:56 +0900

    test(Deck): 플레이어 덱 합산 테스트 코드 작성
    플레이어 덱 합산 시에 적절한 값으로 판단되는지 테스트 코드를 작성하였습니다.
    이때, ACE도 게임에서 버스트가 안되는 유리한 방향으로 처리되는지 검증하였습니다.

--- src/test/java/domain/CardCalculatorTest.java
+++ src/test/java/domain/CardCalculatorTest.java
@@ -28,4 +28,38 @@ public class CardCalculatorTest {
     }
 
     // TODO: ACE 예외 케이스랑 엣지케이스들 추가
+    @Test
+    void 플레이어덱_합산_ACE_1로_처리_테스트() {
+        // given
+        Player phobi = Player.of("phobi");
+
+        List<Card> cards = new ArrayList<>();
+        cards.add(Card.of(CardRank.ACE, CardShape.HEART));
+        cards.add(Card.of(CardRank.EIGHT, CardShape.HEART));
+        cards.add(Card.of(CardRank.NINE, CardShape.HEART));
+
+        // when
+        phobi.assignDeck(Deck.of(cards));
+
+        // then
+        assertThat(phobi.getDeckSum()).isEqualTo(18);
+    }
+
+    @Test
+    void 플레이어덱_합산_ACE_11로_처리_테스트() {
+        // given
+        Player phobi = Player.of("phobi");
+
+        List<Card> cards = new ArrayList<>();
+        cards.add(Card.of(CardRank.ACE, CardShape.HEART));
+        cards.add(Card.of(CardRank.TWO, CardShape.HEART));
+        cards.add(Card.of(CardRank.THREE, CardShape.HEART));
+
+        // when
+        phobi.assignDeck(Deck.of(cards));
+        phobi.calculateFinalSum();
+
+        // then
+        assertThat(phobi.getDeckSum()).isEqualTo(16);
+    }
 }
