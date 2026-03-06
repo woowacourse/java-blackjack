1baae0c6c006c1c1e7afeaa91eae992a0a2d2380
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-06 15:42:03 +0900

    test: 이름 카드 정보 반환 테스트

--- src/test/java/domain/player/PlayerTest.java
+++ src/test/java/domain/player/PlayerTest.java
@@ -1,6 +1,11 @@
 package domain.player;
 
 import domain.card.Card;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -42,7 +47,6 @@ class PlayerTest {
         Assertions.assertThat(player.isBust()).isEqualTo(true);
     }
 
-
     // 합산
     @Test
     @DisplayName("카드 값 합산")
@@ -61,6 +65,25 @@ class PlayerTest {
 
         // then
         Assertions.assertThat(player.getTotalValue()).isEqualTo(22);
+    }
+
+    @Test
+    @DisplayName("플레이어 이름과 카드 정보를 문자열 리스트로 반환하는지 검증")
+    void 이름_카드_정보_문자열_반환_테스트() {
+        // given
+        Player player = new Player("pobi");
+        player.addCard(new Card("K", "다이아몬드"));
+        player.addCard(new Card("2", "하트"));
+        player.addCard(new Card("3", "클로버"));
+
+        Map<String, List<String>> temp = new HashMap<>();
+        temp.put("pobi", List.of("K다이아몬드", "2하트", "3클로버"));
+
+        // when
+        Map<String, List<String>> nameAndCardInfo = player.getNameAndCardsInfo();
+
+        // then
+        Assertions.assertThat(nameAndCardInfo.equals(temp)).isEqualTo(true);
     }
 
 }
