fdc758c89e3cc1b6ddb8ca3f104f05a1775daec5
wonsang.choi <wonsang.choi@e4net.net>
2026-03-06 18:13:08 +0900

    test(Dealer): 딜러 테스트 케이스 추가

--- src/test/java/team/blackjack/domain/DealerTest.java
+++ src/test/java/team/blackjack/domain/DealerTest.java
@@ -1,43 +1,26 @@
 package team.blackjack.domain;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
 
 class DealerTest {
 
     @Test
     void hit하면_핸드에_카드가_추가된다() {
         Dealer dealer = new Dealer();
-        List<Card> cards = List.of(
-                new Card(Suit.HEARTS, Rank.ACE),
-                new Card(Suit.HEARTS, Rank.KING)
-        );
-
-        for (Card card : cards) {
-            dealer.hit(card);
-        }
+        dealer.hit(Card.ACE_OF_HEARTS);
+        dealer.hit(Card.KING_OF_HEARTS);
 
         assertThat(dealer.getHand().getCards())
-                .extracting(Card::getRank, Card::getSuit)
-                .containsExactly(
-                        tuple(Rank.ACE, Suit.HEARTS),
-                        tuple(Rank.KING, Suit.HEARTS));
+                .containsExactly(Card.ACE_OF_HEARTS, Card.KING_OF_HEARTS);
     }
-
     @Test
     void 킹과_에이스를_각각_1장씩_받은_딜러의_점수는_21로_정상_계산된다() {
         Dealer dealer = new Dealer();
-        List<Card> cards = List.of(
-                new Card(Suit.HEARTS, Rank.ACE),
-                new Card(Suit.CLUBS, Rank.KING)
-        );
 
-        for (Card card : cards) {
-            dealer.hit(card);
-        }
+        dealer.hit(Card.ACE_OF_HEARTS);
+        dealer.hit(Card.KING_OF_CLUBS);
 
         assertThat(dealer.getScore()).isEqualTo(21);
     }
