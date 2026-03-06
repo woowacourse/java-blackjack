5af858b96b02af95416327c08c41cf28b8ca5955
Minjae Kang <rkdalswoals@gmail.com>
2026-03-06 18:48:23 +0900

    test: 플레이어이름 테스트 추가

---
+++ src/test/java/domain/player/NameTest.java
@@ -0,0 +1,26 @@
+package domain.player;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class NameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", ""})
+    void 이름이_공백이면_예외처리(String input) {
+        Assertions.assertThatThrownBy(() -> Name.from(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @Test
+    void 이름이_10글자_이상이면_예외처리() {
+        String input = "1234567890!";
+
+        Assertions.assertThatThrownBy(() -> Name.from(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
