40353f4a3c624e45271617629b29b1cc66959daa
koreaioi <koreaioi@gachon.ac.kr>
2026-03-06 14:04:04 +0900

    feat: PlayerHandDto에 Dealer로 만들 수 있도록 추가

--- src/main/java/domain/player/dto/PlayerHandDto.java
+++ src/main/java/domain/player/dto/PlayerHandDto.java
@@ -1,9 +1,11 @@
 package domain.player.dto;
 
+import domain.dealer.Dealer;
 import domain.player.Player;
 
 import java.util.List;
 
+// TODO 추후 Gamer로 개선 예정.
 public record PlayerHandDto(
         String playerName,
         List<String> handOnCards
@@ -13,4 +15,11 @@ public record PlayerHandDto(
         List<String> handOnCards = player.disPlayMyCardBundle();
         return new PlayerHandDto(playerName, handOnCards);
     }
+
+    public static PlayerHandDto of(Dealer dealer) {
+        String playerName = dealer.toDisplayMyName();
+        List<String> handOnCards = List.of(dealer.disPlayMyCardBundle().getFirst());
+        return new PlayerHandDto(playerName, handOnCards);
+    }
+
 }
