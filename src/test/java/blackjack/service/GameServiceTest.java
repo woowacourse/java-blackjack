0a77fb5839279e919b11490ca55d69fb6d9fd53d
kangrae <kangrae0501@gmail.com>
2026-03-06 14:52:51 +0900

    test: 승패 판단 테스트 추가

--- src/test/java/blackjack/service/GameServiceTest.java
+++ src/test/java/blackjack/service/GameServiceTest.java
@@ -26,4 +26,21 @@ class GameServiceTest {
         assertThat(user.getCards().size()).isEqualTo(2);
     }
 
+    @Test
+    @DisplayName("승패 판단 테스트")
+    void 승패_판단_테스트() {
+        // given
+        GameService gameService = new GameService();
+        User user = new User("흑곰");
+        Dealer dealer = new Dealer(new Deck());
+        user.bring(dealer.bringCard());
+        dealer.bring(dealer.bringCard());
+
+        // when
+        boolean isDealerWinning = gameService.isDealerWinning(user, dealer);
+
+        // then
+        assertThat(isDealerWinning).isTrue();
+    }
+
 }
