1217dc866ba61ba1e16f20a4cd5c76c2d45ca002
박병훈 <leafloveu@naver.com>
2026-03-06 14:48:17 +0900

    feat(validate): 이름이 공백으로 시작하거나 끝나는 경우 예외 처리 추가

--- src/main/java/blackjack/view/InputValidator.java
+++ src/main/java/blackjack/view/InputValidator.java
@@ -6,6 +6,13 @@ import java.util.List;
 public final class InputValidator {
 
     public static void validatePlayerNames(final List<String> names) {
+        boolean hasInvalidName = names.stream()
+                .anyMatch(name -> name.startsWith(" ") || name.endsWith(" "));
+
+        if (hasInvalidName) {
+            throw new IllegalArgumentException("이름이 공백으로 시작하거나 끝납니다.");
+        }
+
         if (new HashSet<>(names).size() < names.size()) {
             throw new IllegalArgumentException("중복된 플레이어 이름이 존재합니다.");
         }
