9bc8d1ad7fe89cd27bf5f47554511843ddeee183
snow___shower <wonsb25@gmail.com>
2026-03-06 15:42:04 +0900

    test: 블랙잭 승패 판정 로직이 담긴 테스트 추가

---
+++ src/test/java/BlackJackServiceTest.java
@@ -0,0 +1,55 @@
+import domain.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.BlackJackService;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BlackJackServiceTest {
+
+    Dealer dealer;
+    Player winPlayer;
+    Player defeatPlayer;
+    Player drawPlayer;
+    Players players;
+
+    @BeforeEach
+    void init() {
+        dealer = new Dealer(new Hand());
+        winPlayer = new Player("이김", new Hand());
+        defeatPlayer = new Player("짐", new Hand());
+        drawPlayer = new Player("비김", new Hand());
+        dealer.keepCard(new Card(Rank.FIVE, Pattern.CLOVER));
+        winPlayer.keepCard(new Card(Rank.SIX, Pattern.CLOVER));
+        defeatPlayer.keepCard(new Card(Rank.FOUR, Pattern.CLOVER));
+        drawPlayer.keepCard(new Card(Rank.FIVE, Pattern.SPADE));
+        players = new Players(List.of(winPlayer, defeatPlayer, drawPlayer));
+    }
+
+    @Test
+    void 딜러의_총합이_플레이어의_총합보다_낮으면_플레이어가_승리한다() {
+        BlackJackService blackJackService = new BlackJackService();
+        Map<String, ResultInfo> result = blackJackService.calculateResult(dealer, players);
+        ResultInfo info = result.get(winPlayer.getName());
+        assertThat(info).isEqualTo(ResultInfo.WIN);
+    }
+
+    @Test
+    void 딜러의_총합이_플레이어의_총합보다_높으면_플레이어가_패배한다() {
+        BlackJackService blackJackService = new BlackJackService();
+        Map<String, ResultInfo> result = blackJackService.calculateResult(dealer, players);
+        ResultInfo info = result.get(defeatPlayer.getName());
+        assertThat(info).isEqualTo(ResultInfo.DEFEAT);
+    }
+
+    @Test
+    void 딜러의_총합과_플레이어의_총합이_같으면_비긴다() {
+        BlackJackService blackJackService = new BlackJackService();
+        Map<String, ResultInfo> result = blackJackService.calculateResult(dealer, players);
+        ResultInfo info = result.get(drawPlayer.getName());
+        assertThat(info).isEqualTo(ResultInfo.DRAW);
+    }
+}
