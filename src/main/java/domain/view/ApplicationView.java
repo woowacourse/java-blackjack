38b643ecbcc52377a1cdd6fb5838ec45312f3148
koreaioi <koreaioi@gachon.ac.kr>
2026-03-06 13:38:56 +0900

    feat: ApplicationView 플레이어 입력 구현

--- src/main/java/domain/view/ApplicationView.java
+++ src/main/java/domain/view/ApplicationView.java
@@ -1,5 +1,10 @@
 package domain.view;
 
+import domain.player.PlayerName;
+import domain.player.dto.PlayerHandDto;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ApplicationView {
@@ -12,6 +17,17 @@ public class ApplicationView {
         this.writer = writer;
     }
 
+    public List<PlayerName> requestPlayerNames() {
+        return retry(() -> {
+            writer.printInputNameGuideMessage();
+            String names = reader.readInput();
+            return Arrays.stream(names.split(","))
+                    .map(String::trim)
+                    .map(PlayerName::from)
+                    .toList();
+        });
+    }
+
     private <T> T retry(Supplier<T> task) {
         while (true) {
             try {
@@ -20,6 +36,10 @@ public class ApplicationView {
                 writer.printErrorMessage(e.getMessage());
             }
         }
+    }
+
+    private String joining(List<String> strings) {
+        return String.join(", ", strings);
     }
 
 }
