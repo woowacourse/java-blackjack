9d06d5297e701d4a488b2a6b490606e263b89a88
gleaming9 <gleaming9@khu.ac.kr>
2026-03-06 16:30:58 +0900

    feat(config): 의존성 주입 객체 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,11 @@
+import config.AppConfig;
 import controller.BlackJackController;
 
 public class Application {
 
     public static void main(String[] args) {
-        BlackJackController blackJackController = new BlackJackController();
+        AppConfig appConfig = new AppConfig();
+        BlackJackController blackJackController = appConfig.blackJackController();
 
         blackJackController.run();
     }
