b5310e105c3356c05dfe5ae6d84f286b50d5bbf9
Kim, Joon <kjoon418@naver.com>
2026-03-06 22:41:02 +0900

    refactor: 매직 리터럴 상수화

--- src/main/java/blackjack/model/AceAdjustPolicy.java
+++ src/main/java/blackjack/model/AceAdjustPolicy.java
@@ -3,6 +3,9 @@ package blackjack.model;
 import java.util.List;
 
 public class AceAdjustPolicy {
+
+    private static final int ADJUST_VALUE = 10;
+
     private final BustPolicy bustPolicy;
 
     public AceAdjustPolicy(BustPolicy bustPolicy) {
@@ -11,8 +14,8 @@ public class AceAdjustPolicy {
 
     public int adjust(int sum, List<Card> cards) {
         boolean containAce = cards.stream().anyMatch(x -> x.rank() == Rank.ACE);
-        if (!bustPolicy.isBust(sum + 10) && containAce) {
-            return sum + 10;
+        if (!bustPolicy.isBust(sum + ADJUST_VALUE) && containAce) {
+            return sum + ADJUST_VALUE;
         }
         return sum;
     }
