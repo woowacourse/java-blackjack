2fc35ba5b536bc5010821e3d88783315879367ad
zakie <realzzaekkii@gmail.com>
2026-03-06 17:12:34 +0900

    feat: 딜러의 최종 점수를 계산하는 기능추가

--- src/main/java/service/BlackjackService.java
+++ src/main/java/service/BlackjackService.java
@@ -2,6 +2,7 @@ package service;
 
 import domain.Card;
 import domain.CardDeck;
+import domain.Participant;
 import infra.CardShuffler;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,5 +26,30 @@ public class BlackjackService {
         }
 
         return cards;
+    }
+
+    public DealerFinalResult getDealerFinalResult(Participant dealer, List<Participant> players) {
+
+        int dealerScore = dealer.getScore();
+
+        int winCount = 0;
+        int drawCount = 0;
+        int loseCount = 0;
+        for (Participant player : players) {
+            int playerScore = player.getScore();
+            if (player.isBust() || (!dealer.isBust() && dealerScore > playerScore)) {
+                winCount++;
+                continue;
+            }
+
+            if (!dealer.isBust() && dealerScore == playerScore) {
+                drawCount++;
+                continue;
+            }
+
+            loseCount++;
+        }
+
+        return new DealerFinalResult(winCount, drawCount, loseCount);
     }
 }
--- src/test/java/service/BlackjackServiceTest.java
+++ src/test/java/service/BlackjackServiceTest.java
@@ -4,7 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.Card;
 import domain.CardDeck;
+import domain.CardRank;
+import domain.CardSuit;
+import domain.HandCards;
+import domain.Participant;
+import domain.Participants;
 import infra.CardShuffler;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,5 +41,39 @@ class BlackjackServiceTest {
         public int shuffleCardDeck(int deckSize) {
             return 0;
         }
+    }
+
+    @Test
+    @DisplayName("딜러와 플레이어들의 카드를 비교해서 딜러의 결과를 반환한다.")
+    public void 딜러_결과_계산_성공() throws Exception {
+        // given
+        Participants participants = initParticipants();
+
+        // when
+        DealerFinalResult dealerFinalResult = blackjackService.getDealerFinalResult(participants.getDealer(),
+                participants.getPlayers());
+
+        // then
+        assertThat(dealerFinalResult.win()).isEqualTo(1);
+        assertThat(dealerFinalResult.draw()).isEqualTo(0);
+        assertThat(dealerFinalResult.lose()).isEqualTo(1);
+    }
+
+    private static Participants initParticipants() {
+        Participant pobi = new Participant("포비", new HandCards(new ArrayList<>()), false);
+        pobi.addHandCard(new Card(CardSuit.HEART, CardRank.TWO));
+        pobi.addHandCard(new Card(CardSuit.SPADE, CardRank.EIGHT));
+        pobi.addHandCard(new Card(CardSuit.CLUB, CardRank.ACE));
+
+        Participant jason = new Participant("제이슨", new HandCards(new ArrayList<>()), false);
+        jason.addHandCard(new Card(CardSuit.CLUB, CardRank.SEVEN));
+        jason.addHandCard(new Card(CardSuit.SPADE, CardRank.KING));
+
+        Participants participants = new Participants(new ArrayList<>(List.of(pobi, jason)));
+        Participant dealer = participants.getDealer();
+        dealer.addHandCard(new Card(CardSuit.DIAMOND, CardRank.THREE));
+        dealer.addHandCard(new Card(CardSuit.CLUB, CardRank.NINE));
+        dealer.addHandCard(new Card(CardSuit.DIAMOND, CardRank.EIGHT));
+        return participants;
     }
 }
