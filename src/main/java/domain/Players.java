56ec80f9f0fc636b3780c85534dd0636d84c6fd4
최용준 <yj9107vv@gmail.com>
2026-03-06 22:20:36 +0900

    feat(Players): 플레이어 일급 컬랙션
    - 플레이어들의 정보 반환 메서드 getUserNames(), getPlayerInfo()

---
+++ src/main/java/domain/Players.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Players {
+
+    private final List<Player> players;
+
+    private Players(Cards cards, List<String> userNames) {
+        List<Player> players = new ArrayList<>();
+
+        for (int i = 0; i < userNames.size(); i++) {
+            players.add(Player.of(cards.drawInitialHand(), userNames.get(i)));
+        }
+        this.players = players;
+    }
+
+    public static Players of(Cards cards, List<String> userNames) {
+        return new Players(cards, userNames);
+    }
+
+    public List<String> getUserNames() {
+        return players.stream()
+                .map(Player::getName)
+                .toList();
+    }
+
+    public List<String> getPlayerInfo() {
+        return players.stream()
+                .map(Player::getPlayerInfo)
+                .toList();
+    }
+
+}
