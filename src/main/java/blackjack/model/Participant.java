63406ffa31a9388dafc3a220398c282520e2a95c
smini <sm020320@naver.com>
2026-03-06 14:58:58 +0900

    refactor: 점수 상태 추가

--- src/main/java/blackjack/model/Participant.java
+++ src/main/java/blackjack/model/Participant.java
@@ -1,21 +1,36 @@
 package blackjack.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public abstract class Participant {
     protected final List<Card> cards;
+    protected int score;
 
     public Participant() {
         this.cards = new ArrayList<>();
+        this.score = 0;
     }
 
     public void receiveCard(Card card) {
         cards.add(card);
     }
 
+    public void addScore(int score) {
+        this.score += score;
+    }
+
     public int getSize() {
         return cards.size();
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public List<Card> getCards() {
+        return Collections.unmodifiableList(cards);
     }
 
     public abstract boolean canReceive(int score);
