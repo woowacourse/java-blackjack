160cc41b33cd117d8a573c0265c305c1c8ae7403
Yeji Kim <yejikim.erica@gmail.com>
2026-03-06 11:05:10 +0900

    test: add dealer constructor test

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -8,6 +10,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DealerTest {
+    @Test
+    @DisplayName("Dealer를 생성할 때 오류 발생 안함")
+    void dealer_create_success() {
+        CardCreationStrategy fixedCardCreationStrategy = new CardCreationStrategy() {
+            @Override
+            public List<Card> create() {
+                Card spadeJ = new Card(CardShape.스페이드, CardContents.J);
+                Card clover5 = new Card(CardShape.클로버, CardContents.FIVE);
+
+                return new ArrayList<>(List.of(spadeJ, clover5));
+            }
+        };
+        Deck deck = Deck.createDeck(fixedCardCreationStrategy);
+        Deck participantDeck = Deck.createParticipantDeck(deck);
+        assertDoesNotThrow(
+                () -> new Dealer(participantDeck)
+        );
+    }
+
     @Test
     @DisplayName("딜러는 카드의 합이 16 이하면 카드를 한 장 더 받는다")
     void addCardWhenSumBelowMinimum() {
