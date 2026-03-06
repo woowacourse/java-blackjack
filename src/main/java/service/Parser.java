fd0a2ac3e267db36331ba9e23c3c85ae7a938bbe
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-06 17:41:19 +0900

    feat: 게임 참가자 이름 분리 기능 구현

--- src/main/java/service/Parser.java
+++ src/main/java/service/Parser.java
@@ -4,5 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
+    private String delimeter;
+    public Parser(String delimeter) {
+        this.delimeter = delimeter;
+    }
 
+    public List<String> parse(String inputNames) {
+        return Arrays.stream(inputNames.split(delimeter))
+                .map(String::trim)
+                .toList();
+    }
 }
