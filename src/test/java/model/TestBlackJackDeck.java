2502a6c5994aee88dcea11b94579ada362c00ec6
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-06 16:05:07 +0900

    test(BlackJackDeck) : 남은 카드가 0장 일 때 드로우 시 에러 테스트 코드 추가

--- src/test/java/model/TestBlackJackDeck.java
+++ src/test/java/model/TestBlackJackDeck.java
@@ -1,7 +1,9 @@
 package model;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import constant.ErrorMessage;
 import org.junit.jupiter.api.Test;
 
 public class TestBlackJackDeck {
@@ -11,5 +13,17 @@ public class TestBlackJackDeck {
 
         assertThat(blackJackDeck.draw().cardNumber()).isEqualTo(CardNumber.ACE);
         assertThat(blackJackDeck.draw().shape()).isEqualTo(Shape.HEART);
+    }
+
+    @Test
+    public void 남은_카드가_0장_일_때_드로우() {
+        BlackJackDeck blackJackDeck = new BlackJackDeck();
+
+        for(int i = 0; i < 52; i++) {
+            blackJackDeck.draw();
+        }
+
+        assertThatThrownBy(blackJackDeck::draw).isExactlyInstanceOf(IllegalArgumentException.class).hasMessage(
+                ErrorMessage.NO_CARD_IN_DECK.getMessage());
     }
 }
