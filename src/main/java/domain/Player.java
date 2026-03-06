30ef159a9caec1573502b9fc0d1fbbfc99a642e4
Yeji Kim <yejikim.erica@gmail.com>
2026-03-06 17:30:01 +0900

    feat: add Player class

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Player extends Participant {
+
+    public Player(Deck participantDeck, String name) {
+        super(participantDeck, name);
+    }
+    
+}
---
+++ src/test/java/domain/PlayerTest.java
@@ -0,0 +1,57 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+    @Test
+    @DisplayName("Player를 생성할 때 오류 발생 안함")
+    void player_create_success() {
+        CardCreationStrategy fixedCardCreationStrategy = new CardCreationStrategy() {
+            @Override
+            public List<Card> create() {
+                Card spadeJ = new Card(CardShape.스페이드, CardContents.J);
+                Card clover5 = new Card(CardShape.클로버, CardContents.FIVE);
+
+                return new ArrayList<>(List.of(spadeJ, clover5));
+            }
+        };
+        Deck deck = Deck.createDeck(fixedCardCreationStrategy);
+        Deck participantDeck = Deck.createParticipantDeck(deck);
+        String name = "pobi";
+
+        assertDoesNotThrow(
+                () -> new Player(participantDeck, name)
+        );
+    }
+
+    @Test
+    @DisplayName("플레이어가 카드를 한 장 더 받는다")
+    void addCardWhenSumBelowMinimum() {
+        //given
+        CardCreationStrategy playerCardCreationStrategy = () -> {
+            Card spadeJ = new Card(CardShape.스페이드, CardContents.J);
+            return new ArrayList<>(List.of(spadeJ));
+        };
+        CardCreationStrategy totalCardCreationStrategy = () -> {
+            Card spadeA = new Card(CardShape.스페이드, CardContents.A);
+            Card heartA = new Card(CardShape.하트, CardContents.TWO);
+            return new ArrayList<>(List.of(spadeA, heartA));
+        };
+        Deck playerDeck = Deck.createDeck(playerCardCreationStrategy);
+        String testPlayerName = "pobi";
+        Deck totalDeck = Deck.createDeck(totalCardCreationStrategy);
+        Player player = new Player(playerDeck, testPlayerName);
+
+        //when
+        int result = player.addCard(totalDeck.drawCard());
+
+        //then
+        Assertions.assertThat(result).isEqualTo(2);
+    }
+}
