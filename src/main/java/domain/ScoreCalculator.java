f8b898838b1246b35dd657d41e4465fded37ab1b
심호경 <ghrudtla@naver.com>
2026-03-06 18:24:42 +0900

    feat: 카드 점수를 계산하는 ScoreCalculator 도메인 구현

--- src/main/java/domain/ScoreCalculator.java
+++ src/main/java/domain/ScoreCalculator.java
@@ -3,42 +3,21 @@ package domain;
 import java.util.List;
 
 public class ScoreCalculator {
-    private static final int ACE_BONUS = 10;
-    private static final int BUST_LIMIT_SCORE = 21;
-
     public static int calculate(List<Card> cards) {
-        int baseSum = calculateBaseSum(cards);
-        int aceCount = countAce(cards);
-        return applyAceLogic(baseSum, aceCount);
-    }
-
-    private static int calculateBaseSum(List<Card> cards) {
         int sum = 0;
+        int aceCount = 0;
         for (Card card : cards) {
             sum += card.getCardNumber().getValue();
+            if (card.getCardNumber() == CardNumber.ACE) {
+                aceCount++;
+            }
+        }
+
+        for (int i = 0; i < aceCount; i++) {
+            if (sum + 10 < 21) {
+                sum += 10;
+            }
         }
         return sum;
-    }
-
-    private static int countAce(List<Card> cards) {
-        return (int) cards.stream()
-                .map(Card::getCardNumber)
-                .filter(cardNumber -> cardNumber == CardNumber.ACE)
-                .count();
-    }
-
-    private static int applyAceLogic(int baseSum, int aceCount) {
-        int score = baseSum;
-        for (int i = 0; i < aceCount; i++) {
-            score = addAceBonus(score);
-        }
-        return score;
-    }
-
-    private static int addAceBonus(int score) {
-        if (score + ACE_BONUS <= BUST_LIMIT_SCORE) {
-            return score + ACE_BONUS;
-        }
-        return score;
     }
 }
