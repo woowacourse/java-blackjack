71471ea004aba9b2681da309a294906ec169c599
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-06 09:57:40 +0900

    test: 이름 리스트 문자열 검증하는 테스트 생성

---
+++ src/main/java/dto/NameRequestDto.java
@@ -0,0 +1,5 @@
+package dto;
+
+public record NameRequestDto() {
+
+}
---
+++ src/test/java/dto/NameRequestDtoTest.java
@@ -0,0 +1,26 @@
+package dto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NameRequestDtoTest {
+    // null 검증 isBlank()
+
+    @Test
+    @DisplayName("이름 입력 문자열이 null이면 에러 발생 검증")
+    void name_is_not_null() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new NameRequestDto(null));
+
+    }
+
+    @Test
+    @DisplayName("이름 입력 문자열이 빈 문자열(\"\")이면 에러 발생 검증")
+    void name_is_not_blank() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new NameRequestDto(""));
+    }
+}
