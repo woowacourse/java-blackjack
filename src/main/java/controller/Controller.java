0d1eabcd94d1ecf17b4d60205f506e69c8bd5688
ghkdtjfxkd <syt1128@gmail.com>
2026-03-06 13:31:52 +0900

    feat: 플레이어 참가 셋팅 기능 추가

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,6 +1,14 @@
 package controller;
 
 import domain.GameTable;
+import domain.Hand;
+import domain.Participant;
+import domain.Player;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import strategy.RandomStrategy;
+import view.InputView;
 
 public class Controller {
 
@@ -9,5 +17,30 @@ public class Controller {
     public void run() {
         setupPhase();
         gamePhase();
+    }
+
+    private void setupPhase() {
+        gameTable = new GameTable();
+        dealerSetup();
+        playerSetup();
+    }
+
+    private void dealerSetup() {
+    }
+
+    private void playerSetup() {
+        String playerNames = InputView.readPlayers();
+        List<String> names = Arrays.stream(playerNames.split(",")).toList();
+
+        for (String name : names) {
+            Hand hand = new Hand(new RandomStrategy(), new ArrayList<>());
+            Player player = new Player(name, hand);
+            player.draw();
+            player.draw();
+            gameTable.addPlayer(new Player(name, hand));
+        }
+    }
+
+    private void gamePhase() {
     }
 }
--- src/main/java/domain/GameTable.java
+++ src/main/java/domain/GameTable.java
@@ -1,5 +1,19 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameTable {
+
+    private final List<Participant> participants;
+
+    public GameTable() {
+        this.participants = new ArrayList<>();
+
+    }
+
+    public void addPlayer(Participant player) {
+        participants.add(player);
+    }
 
 }
