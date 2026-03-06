611f264ebdde13bfbc8215e0f1c4e17ccb102791
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-06 17:01:09 +0900

    refactor(Dealer) : 딜러 이름 상수화

--- src/main/java/model/Dealer.java
+++ src/main/java/model/Dealer.java
@@ -3,7 +3,9 @@ package model;
 import model.dto.PlayerName;
 
 public class Dealer extends Participant{
+    private static final String DEALER_NAME = "딜러";
+
     public Dealer() {
-        super(new PlayerName("딜러"));
+        super(new PlayerName(DEALER_NAME));
     }
 }
