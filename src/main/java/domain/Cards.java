4730a4b55909199bd062a0822cece86ed7a772ca
최용준 <yj9107vv@gmail.com>
2026-03-06 20:24:46 +0900

    feat(Cards): 카드 덱에서 드로우 기능 추가
    - 2장 드로우 drawInitialHand() 추가
    - 1장 드로우 draw() 추가

--- src/main/java/domain/Cards.java
+++ src/main/java/domain/Cards.java
@@ -2,11 +2,13 @@ package domain;
 
 import static java.util.Arrays.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Cards {
 
-    private final List<Card> cards;
+    private List<Card> cards;
 
     private Cards() {
         this.cards = generateCards();
@@ -17,10 +19,20 @@ public class Cards {
     }
 
     private List<Card> generateCards() {
-        return stream(Rank.values())
+        return new ArrayList<>(stream(Rank.values())
                 .flatMap(rank -> stream(Suit.values())
-                        .map(suit -> Card.of(suit, rank)))
+                        .map(suit -> Card.of(rank, suit)))
+                .toList());
+    }
+
+    public List<Card> drawInitialHand() {
+        return Stream.generate(this::draw)
+                .limit(2)
                 .toList();
+    }
+
+    public Card draw() {
+        return cards.removeFirst();
     }
 
 }
