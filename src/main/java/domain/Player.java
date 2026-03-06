4fe4121418b53aac372b7ed46a6f13aea6c50eb4
최용준 <yj9107vv@gmail.com>
2026-03-06 22:19:19 +0900

    feat(Player): Player 점수 계산 및 정보 반환 기능 추가
    - name 필드 추가
    - 점수 계산 로직 calculateScore() 수정
    - 정보 반환 메서드 getPlayerInfo(), getCardsInfo()

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,17 +1,20 @@
 package domain;
 
 import java.util.List;
+import meesage.OutputMessage;
 
 public class Player {
 
     private List<Card> cards;
+    private final String name;
 
-    private Player(List<Card> cards) {
+    private Player(List<Card> cards, String name) {
         this.cards = cards;
+        this.name = name;
     }
 
-    public static Player of(List<Card> cards) {
-        return new Player(cards);
+    public static Player of(List<Card> cards, String name) {
+        return new Player(cards, name);
     }
 
     public void addCard(Card card) {
@@ -19,18 +22,48 @@ public class Player {
     }
 
     public int calculateScore() {
-        int cardScore = cards.stream()
-                .mapToInt(Card::getScore)
-                .sum();
+        int cardScore = calculateRawScore();
+        int aceCount = countAce();
 
-        if (isBustWithAce(cardScore)) {
-            cardScore -= 10;
+        for (int i = 0; i < aceCount; i++) {
+            cardScore = adjustForAce(cardScore);
         }
 
         return cardScore;
     }
 
+    private int calculateRawScore() {
+        return cards.stream()
+                .mapToInt(Card::getScore)
+                .sum();
+    }
+
+    private int countAce() {
+        return (int) cards.stream().filter(Card::isAce).count();
+    }
+
+    private int adjustForAce(int cardScore) {
+        if (isBustWithAce(cardScore)) {
+            cardScore -= 10;
+        }
+        return cardScore;
+    }
+
     private boolean isBustWithAce(int cardScore) {
-        return cardScore > 21 && cards.stream().anyMatch(Card::isAce);
+        return cardScore > 21;
+    }
+
+    public List<String> getCardsInfo() {
+        return cards.stream()
+                .map(Card::getName)
+                .toList();
+    }
+
+    public String getPlayerInfo() {
+        return getName() + OutputMessage.CARD_TEXT.getMessage() + OutputMessage.format(getCardsInfo());
+    }
+
+    public String getName() {
+        return name;
     }
 }
