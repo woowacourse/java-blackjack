e06746167b0afa25dca316999753dbdb81e8498f
tjdakf <ksc73450056@gmail.com>
2026-03-07 16:00:50 +0900

    test: 덱 상태 관련 테스트 코드 수정
    - 테스트 메서드 이름 변경
    - 덱 초기 카드 순서 테스트 로직 변경

--- src/test/java/domain/DeckTest.java
+++ src/test/java/domain/DeckTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import domain.constant.Rank;
+import domain.constant.Suit;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,32 +12,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DeckTest {
 
     @Test
-    void 카드를_52장_생성한다() {
+    void 덱은_52장의_카드로_구성된다() {
         Deck deck = new Deck();
 
-        int deckSize = deck.size();
+        int result = deck.size();
 
-        assertThat(deckSize).isEqualTo(52);
+        assertThat(result).isEqualTo(52);
     }
 
     @Test
-    void 카드는_숫자순에서_무늬순으로_생성된다() {
+    void 덱의_초기_카드는_무늬순_숫자순으로_정렬되어있다() {
         Deck deck = new Deck();
-        int deckSize = deck.size();
 
         List<Card> result = new ArrayList<>();
+        int deckSize = deck.size();
         for (int i = 0; i < deckSize; i++) {
-            Card card = deck.draw();
-            result.add(card);
+            result.add(deck.draw());
         }
 
-        assertThat(result.toString()).contains(
-                "[A스페이드, 2스페이드, 3스페이드, 4스페이드, 5스페이드, 6스페이드, 7스페이드, 8스페이드, 9스페이드, 10스페이드, " +
-                        "J스페이드, Q스페이드, K스페이드, A하트, 2하트, 3하트, 4하트, 5하트, 6하트, 7하트, 8하트, 9하트, 10하트," +
-                        " J하트, Q하트, K하트, A다이아몬드, 2다이아몬드, 3다이아몬드, 4다이아몬드, 5다이아몬드, 6다이아몬드, 7다이아몬드," +
-                        " 8다이아몬드, 9다이아몬드, 10다이아몬드, J다이아몬드, Q다이아몬드, K다이아몬드, A클로버, 2클로버, 3클로버, 4클로버," +
-                        " 5클로버, 6클로버, 7클로버, 8클로버, 9클로버, 10클로버, J클로버, Q클로버, K클로버]"
-        );
+        List<Card> expectedDeck = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                expectedDeck.add(new Card(rank, suit));
+            }
+        }
+
+        assertThat(result).containsExactlyElementsOf(expectedDeck);
     }
 
     @Test
