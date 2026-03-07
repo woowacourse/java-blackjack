3a87f113003c910c95f527f800f6a45dc121f43c
yeo-li <parkseongyeol2110@gmail.com>
2026-03-07 16:03:51 +0900

    refactor(ErrorMessage): 에러 메세지 정책 상수화

--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -1,10 +1,17 @@
 package exception;
 
+import constant.HitOrStand;
+import constant.PolicyConstant;
+
 public enum ErrorMessage {
     PLAYER_DUPLICATED("게임 참가자의 이름은 중복 되어선 안됩니다."),
-    PLAYER_COUNT_OUT_OF_RANGE("게임 참가자의 수는 2~8명 사이여야 합니다."),
-    INVALID_YES_NO_INPUT("\"y\" 또는 \"n\"을 입력해야 합니다."),
-    PLAYER_NAME_LENGTH_OUT_OF_RANGE("게임 참가자의 이름은 2~5글자 사이여야 합니다."),
+    PLAYER_COUNT_OUT_OF_RANGE(
+        String.format("게임 참가자의 수는 %d~%d명 사이여야 합니다.", PolicyConstant.PLAYER_MIN_COUNT, PolicyConstant.PLAYER_MAX_COUNT)),
+    INVALID_YES_NO_INPUT(
+        String.format("%s 또는 %s을 입력해야 합니다.", HitOrStand.HIT.getHitOrStand(), HitOrStand.STAND.getHitOrStand())),
+    PLAYER_NAME_LENGTH_OUT_OF_RANGE(
+        String.format("게임 참가자의 이름은 %d~%d글자 사이여야 합니다.", PolicyConstant.PLAYER_NAME_MIN_LENGTH,
+            PolicyConstant.PLAYER_NAME_MAX_LENGTH)),
     PLAYER_NAME_BLANK("게임 참가자의 이름은 공백이 될 수 없습니다."),
     ;
 
