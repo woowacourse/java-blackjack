26ee0b899479f3053fb470576611022102f1e32f
choubung <choubung@sookmyung.ac.kr>
2026-03-08 01:16:58 +0900

    Feat(Player): Player와 PlayerGroups 도메인 생성

--- src/main/java/domain/player/Player.java
+++ src/main/java/domain/player/Player.java
@@ -1,4 +1,17 @@
-package domain.vo;
+package domain.player;
+
+import domain.vo.Name;
 
 public class Player {
+    private Name name;
+    private HandCards handCards = new HandCards();
+
+    public Player(Name name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name.getName();
+    }
+
 }
--- src/main/java/domain/player/PlayerGroups.java
+++ src/main/java/domain/player/PlayerGroups.java
@@ -1,4 +1,23 @@
 package domain.player;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerGroups {
+    private List<Player> players;
+
+    public PlayerGroups(List<Player> players) {
+        validatePlayerNum(players);
+        this.players = new ArrayList<>(players);
+    }
+
+    public int getPlayerGroupSize() {
+        return players.size();
+    }
+
+    private void validatePlayerNum(List<Player> players){
+        if (players.size() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
