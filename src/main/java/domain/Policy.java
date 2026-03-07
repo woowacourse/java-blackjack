9e106b389e72c97066a1fd83a34cb049cb02678c
최용준 <yj9107vv@gmail.com>
2026-03-07 17:37:51 +0900

    feat(Policy): 블랙잭 규칙 하드코딩 상수화

---
+++ src/main/java/domain/Policy.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class Policy {
+
+    public static final int FIRST_DRAW_SIZE = 2;
+    public static final int DEALER_HIT_THRESHOLD = 16;
+    public static final int BUST_THRESHOLD = 21;
+    public static final int ACE_HIGH_LOW_DIFF = 10;
+    public static final int BUST_SCORE = 0;
+}
