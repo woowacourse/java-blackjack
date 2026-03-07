b0a7be853dafd9609e8b164e001c8c04915f3bc1
Yeji Kim <yejikim.erica@gmail.com>
2026-03-07 18:09:41 +0900

    refactor : change user input method to Scanner
    BufferedReader 사용 시 IOException 관련 문제가 존재. 해당 문제를 핸들링 하는 것 보다 속도가 다소 느리더라도 Scanner 을 이용하여 문제를 핸들링 하지 않게 되는 것이 더 좋다고 판단하여 변경함

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,29 +1,32 @@
 package view;
 
 import common.ErrorMessage;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class InputView {
     private static final String DELIMITER = ",";
     private static final List<String> HIT_OR_STAND = List.of("y", "n");
+    private static final Scanner sc = new Scanner(System.in);
 
-    public List<String> readNames() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        return Arrays.stream(br.readLine().split(DELIMITER)).toList();
+    public List<String> readNames() {
+        String line = sc.nextLine();
+        validateIsBlank(line);
+        return Arrays.stream(line.split(DELIMITER)).toList();
     }
 
-    public String readHitOrStand() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine().trim();
-
+    public String readHitOrStand() {
+        String input = sc.nextLine().trim();
+        validateIsBlank(input);
         validateHitOrStandValue(input);
+        return input;
+    }
 
-        return br.readLine().trim();
+    private void validateIsBlank(String line) {
+        if (line.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_ALLOW_EMPTY_INPUT.getMessage());
+        }
     }
 
     private void validateHitOrStandValue(String input) {
