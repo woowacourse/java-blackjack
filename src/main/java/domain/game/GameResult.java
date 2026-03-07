240f236219e998c5bdceb4da650146194482c0ca
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-07 18:48:15 +0900

    feat: 승패 결과 판정 기능 구현

--- src/main/java/domain/game/GameResult.java
+++ src/main/java/domain/game/GameResult.java
@@ -1,4 +1,28 @@
 package domain.game;
 
 public enum GameResult {
+
+    WIN("승"),
+    LOSE("패"),
+    DRAW("무");
+
+    private String gameResult;
+
+    GameResult(String gameResult) {
+        this.gameResult = gameResult;
+    }
+
+    public static GameResult determine(int dealerScore, int gamblerScore) {
+        if(dealerScore > gamblerScore) {
+            return WIN;
+        }
+        if(dealerScore < gamblerScore) {
+            return LOSE;
+        }
+        return DRAW;
+    }
+
+    public String getGameResult() {
+        return gameResult;
+    }
 }
