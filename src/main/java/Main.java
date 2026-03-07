ff92de5a3c0ccdfa5f0c1b967eba6c29be68b983
구민서 <minseo12379@gmail.com>
2026-03-07 16:37:39 +0900

    style: 스타일 정리 Co-authored-by: alstj2384 <ass91160@naver.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,11 @@
 import domain.BlackjackGame;
-import domain.Rank;
-import domain.Suit;
-import domain.card.Card;
-import domain.participant.Participant;
-import domain.participant.Player;
 import view.InputView;
 import view.ResultView;
 
 public class Main {
     public static void main(String[] args) {
-        BlackjackController blackjackController = new BlackjackController(new InputView(), new ResultView(), new BlackjackGame());
+        BlackjackController blackjackController = new BlackjackController(new InputView(), new ResultView(),
+                new BlackjackGame());
         blackjackController.run();
     }
 }
