dfc0c6946b8c62e497cf2df256cf884bf865608b
구민서 <minseo12379@gmail.com>
2026-03-07 16:29:05 +0900

    feat: 싱글톤 scanner 구현 Co-authored-by: alstj2384 <ass91160@naver.com>

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,25 +3,22 @@ package view;
 import domain.ExceptionMessage;
 import domain.participant.Name;
 import java.util.List;
-import java.util.Scanner;
+import util.Console;
 
 public class InputView {
-    public List<String> readPlayerNames(){
+    public List<String> readPlayerNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        String input = Console.readLine();
         String[] split = input.split(",");
         return List.of(split);
     }
 
-    public String readHitStand(Name name){
+    public String readHitStand(Name name) {
         System.out.println(name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
-        Scanner scanner = new Scanner(System.in);
-
-        if(!(name.equals("y") || name.equals("n"))){
+        String input = Console.readLine();
+        if (!(input.equals("y") || input.equals("n"))) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_HIT_STAND_RESPONSE.getMessage());
         }
-
-        return scanner.nextLine();
+        return input;
     }
 }
