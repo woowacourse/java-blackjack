c1cb6943cef99fbfae11822a764fcc4d523960bc
minzzun99 <skg06123@naver.com>
2026-03-07 17:57:35 +0900

    feat: 플레이어별 최종 결과 관리를 위한 FinalResult 구현

---
+++ src/main/java/domain/FinalResult.java
@@ -0,0 +1,23 @@
+package domain;
+
+public class FinalResult {
+    private final Name name;
+    private final ResultType resultType;
+
+    private FinalResult(Name name, ResultType resultType) {
+        this.name = name;
+        this.resultType = resultType;
+    }
+
+    public static FinalResult from(Name name, ResultType resultType) {
+        return new FinalResult(name, resultType);
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public ResultType getResultType() {
+        return resultType;
+    }
+}
