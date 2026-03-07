52506d29c0fdbd0e07f433e6328b62deebdc038c
minzzun99 <skg06123@naver.com>
2026-03-07 17:56:41 +0900

    fix: 점수 계산 로직 수정
    - 기존 21 미만일 경우에만 더했던 로직은 21 이하로 변경

--- src/main/java/domain/ScoreCalculator.java
+++ src/main/java/domain/ScoreCalculator.java
@@ -3,18 +3,19 @@ package domain;
 import java.util.List;
 
 public class ScoreCalculator {
+    // TODO 계산 로직 리팩토링 필요
     public static int calculate(List<Card> cards) {
         int sum = 0;
         int aceCount = 0;
         for (Card card : cards) {
             sum += card.getCardNumber().getValue();
-            if (card.getCardNumber() == CardNumber.ACE) {
+            if (card.getCardNumber().equals(CardNumber.ACE)) {
                 aceCount++;
             }
         }
 
         for (int i = 0; i < aceCount; i++) {
-            if (sum + 10 < 21) {
+            if (sum + 10 <= 21) {
                 sum += 10;
             }
         }
