eacc8f2d995d50fcaa1ef6c15c7ef0fb288aa6ee
구민서 <minseo12379@gmail.com>
2026-03-07 16:09:42 +0900

    refactor: Rank 클래스에서 메서드 추출을 통해 의미 부각 Co-authored-by: alstj2384 <ass91160@naver.com>

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -5,7 +5,20 @@ import static domain.Constant.ACE_MIN_VALUE;
 import static domain.Constant.BLACKJACK_MAX_NUMBER;
 
 public enum Rank {
-    ACE("A", ACE_MIN_VALUE), TWO("2", 2), THREE("3", 3), FOUR("4", 4), FIVE("5", 5), SIX("6", 6), SEVEN("7", 7), EIGHT("8", 8), NINE("9", 9), TEN("10", 10), J("J", 10), Q("Q", 10), K("K", 10);
+    ACE("A", ACE_MIN_VALUE),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    J("J", 10),
+    Q("Q", 10),
+    K("K", 10);
+
     private final String displayValue;
     private final int scoreValue;
 
@@ -14,20 +27,23 @@ public enum Rank {
         this.scoreValue = scoreValue;
     }
 
+    public static int decideAceValue(int sum, int leftAce) {
+        if (sum + ACE_MAX_VALUE <= calculateThreshold(leftAce)) {
+            return ACE_MAX_VALUE;
+        }
+        return ACE_MIN_VALUE;
+    }
+
+    private static int calculateThreshold(int leftAce) {
+        return BLACKJACK_MAX_NUMBER + 1 - leftAce;
+    }
+
     public int getScoreValue() {
         return scoreValue;
     }
 
     public String getDisplayValue() {
         return displayValue;
-    }
-
-    // ace를 제외한 합계가 15이다 -> 매개변수로 15를 주고, ace 넣을수있냐?
-    public static int decideAceValue(int sum, int leftAce) {
-        if (sum + ACE_MAX_VALUE <= (BLACKJACK_MAX_NUMBER+1-leftAce)) {
-            return ACE_MAX_VALUE;
-        }
-        return ACE_MIN_VALUE;
     }
 
     public boolean isAce() {
