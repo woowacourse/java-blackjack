0ccde13a9bc793609267f54f179074c5f6e8e035
choubung <choubung@sookmyung.ac.kr>
2026-03-08 01:38:44 +0900

    Test(GameService): 플레이어 등록 테스트 작성

--- src/test/java/service/GameServiceTest.java
+++ src/test/java/service/GameServiceTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class GameServiceTest {
-  
+    GameService gameService;
+
+    public GameServiceTest() {
+        this.gameService = new GameService();
+    }
+
+    @Test
+    void 플레이어_등록_테스트(){
+        List<String> playerNames = new ArrayList<>(List.of("pobi", "jason"));
+
+        gameService.joinPlayers(playerNames);
+        assertThat(gameService.getPlayerGroupSize()).isEqualTo(playerNames.size() + 1); // +1은 딜러
+    }
 }
