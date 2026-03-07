495039c81202102df7471553cc9683d9141f899d
minzzun99 <skg06123@naver.com>
2026-03-07 16:08:40 +0900

    feat: GameController 및 Application 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,8 @@
-import java.util.Random;
+import controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-
+        GameController gameController = new GameController();
+        gameController.start();
     }
 }
