6b43566b555bbbf172d77a39e31b35dd1463c1f4
구민서 <minseo12379@gmail.com>
2026-03-07 17:25:21 +0900

    test(DealerTest): 테스트 추가 Co-authored-by: alstj2384 <ass91160@naver.com>

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -2,6 +2,7 @@ package domain.card;
 
 import domain.Rank;
 import domain.Suit;
+import java.util.Objects;
 
 public class Card {
     private final Suit suit;
@@ -29,5 +30,19 @@ public class Card {
     @Override
     public String toString() {
         return rank.getDisplayValue() + suit.getValue();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return suit == card.suit && rank == card.rank;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(suit, rank);
     }
 }
--- src/test/java/domain/participant/DealerTest.java
+++ src/test/java/domain/participant/DealerTest.java
@@ -1,12 +1,49 @@
 package domain.participant;
 
+import static domain.Constant.DEALER_HIT_STAND_BOUNDARY;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import domain.Rank;
+import domain.Suit;
+import domain.card.Card;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DealerTest {
     @Test
     void 정상적으로_생성되어야_한다(){
         assertDoesNotThrow(() -> new Dealer("딜러"));
+    }
+
+    @Test
+    void 딜러_카드_중_첫번째_카드만_반환해야_한다(){
+        Dealer dealer = new Dealer("딜러");
+
+        dealer.add(new Card(Suit.CLUB, Rank.K));
+        dealer.add(new Card(Suit.CLUB, Rank.ACE));
+        dealer.add(new Card(Suit.CLUB, Rank.NINE));
+        Card firstCard = dealer.getFirstCard();
+
+        Assertions.assertEquals(firstCard, new Card(Suit.CLUB, Rank.K));
+    }
+
+    @Test
+    void 딜러의_합계가_기준보다_작으면_참을_반환해야_한다(){
+        Dealer dealer = new Dealer("딜러");
+
+        dealer.add(new Card(Suit.CLUB, Rank.FOUR));
+        dealer.add(new Card(Suit.CLUB, Rank.ACE));
+
+        Assertions.assertEquals(dealer.decideHitStand(DEALER_HIT_STAND_BOUNDARY), true);
+    }
+
+    @Test
+    void 딜러의_합계가_기준보다_크면_거짓을_반환해야_한다(){
+        Dealer dealer = new Dealer("딜러");
+
+        dealer.add(new Card(Suit.CLUB, Rank.SIX));
+        dealer.add(new Card(Suit.CLUB, Rank.ACE));
+
+        Assertions.assertEquals(dealer.decideHitStand(DEALER_HIT_STAND_BOUNDARY), false);
     }
 }
