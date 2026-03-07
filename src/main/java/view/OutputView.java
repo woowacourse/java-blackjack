5aa15e800585041e2c583502558aa87b32609c58
MODUGGAGI <kiroro0814@naver.com>
2026-03-07 13:03:39 +0900

    feat(view): 입출력 구현
    입출력을 위한 InputView, OutputView를 구현하였습니다.

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,84 @@
+package view;
+
+import domain.enums.Result;
+import dto.CardDto;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class OutputView {
+
+    private static final String LINE_SEPARATOR = "\n";
+    private static final String STRING_JOIN_DELIMITER = ", ";
+    private static final String DISTRIBUTE_INITIAL_CARD = "딜러와 %s에게 2장을 나누었습니다.";
+    private static final String DEALER_CARD = "딜러카드: %s";
+    private static final String PLAYER_CARD = "%s카드: %s";
+    private static final String SCORE = " - 결과: %d";
+    private static final String DEALER_DRAW = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    private static final String FINAL_RESULT = "## 최종 승패";
+    private static final String DEALER_RESULT = "딜러: %d승 %d패 %d무";
+    private static final String PLAYER_RESULT = "%s: %s";
+
+    public void printPlayers(List<CardDto> dealerCard, Map<String, List<CardDto>> playerCards) {
+        String playerNames = String.join(STRING_JOIN_DELIMITER, playerCards.keySet());
+
+        System.out.printf(LINE_SEPARATOR + DISTRIBUTE_INITIAL_CARD + LINE_SEPARATOR, playerNames);
+        printDealerFirstCard(dealerCard.getFirst());
+        for (String playerName : playerCards.keySet()) {
+            printPlayerCard(playerName, playerCards.get(playerName));
+        }
+        System.out.print(LINE_SEPARATOR);
+    }
+
+    private void printDealerFirstCard(CardDto dealerCard) {
+        System.out.printf(DEALER_CARD + LINE_SEPARATOR, dealerCard.rank().getRank() + dealerCard.suit().getSuit());
+    }
+
+    private void printDealerCard(List<CardDto> dealerCard) {
+        String card = collectCards(dealerCard);
+
+        System.out.printf(DEALER_CARD + LINE_SEPARATOR, card);
+    }
+
+    public void printPlayerCard(String name, List<CardDto> playerCard) {
+        String card = collectCards(playerCard);
+
+        System.out.printf(PLAYER_CARD + LINE_SEPARATOR, name, card);
+    }
+
+    public void printDealerCardWithScore(List<CardDto> dealerCard, int score) {
+        String card = collectCards(dealerCard);
+
+        System.out.printf(LINE_SEPARATOR + DEALER_CARD + SCORE + LINE_SEPARATOR, card, score);
+    }
+
+    public void printPlayerCardWithScore(String name, List<CardDto> playerCard, int score) {
+        String card = collectCards(playerCard);
+
+        System.out.printf(PLAYER_CARD + SCORE + LINE_SEPARATOR, name, card, score);
+    }
+
+    private String collectCards(List<CardDto> cardDtos) {
+        return cardDtos.stream()
+                .map(cardDto -> cardDto.rank().getRank() + cardDto.suit().getSuit())
+                .collect(Collectors.joining(STRING_JOIN_DELIMITER));
+    }
+
+    public void printDealerHit() {
+        System.out.println(LINE_SEPARATOR + DEALER_DRAW);
+    }
+
+    public void printGameResult(Map<Result, Integer> dealerResult, Map<String, Result> playerResults) {
+        System.out.println(LINE_SEPARATOR + FINAL_RESULT);
+
+        int winCount = dealerResult.getOrDefault(Result.WIN, 0);
+        int loseCount = dealerResult.getOrDefault(Result.LOSE, 0);
+        int drawCount = dealerResult.getOrDefault(Result.DRAW, 0);
+
+        System.out.printf(DEALER_RESULT + LINE_SEPARATOR, winCount, loseCount, drawCount);
+        for (String playerName : playerResults.keySet()) {
+            System.out.printf(PLAYER_RESULT + LINE_SEPARATOR, playerName,
+                    playerResults.get(playerName).getDescription());
+        }
+    }
+}
