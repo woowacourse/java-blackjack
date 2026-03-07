a95d56a91858f74e25b1c1a4abf95b9b8886e188
JinGoo <zzgg3040@naver.com>
2026-03-07 21:23:38 +0900

    refactor: 덱 생성 시 스트림 활용

--- src/main/java/blackjack/model/ShuffledCardsGenerator.java
+++ src/main/java/blackjack/model/ShuffledCardsGenerator.java
@@ -3,22 +3,15 @@ package blackjack.model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShuffledCardsGenerator implements CardsGenerator {
 
     @Override
     public List<Card> create() {
-        List<Card> cards = new ArrayList<>();
-
-        List<Rank> ranks = Arrays.asList(Rank.values());
-        List<Suit> suits = Arrays.asList(Suit.values());
-
-        for (Rank rank : ranks) {
-            for (Suit suit : suits) {
-                cards.add(new Card(rank, suit));
-            }
-        }
-        return cards;
+        return Arrays.stream(Rank.values())
+            .flatMap(rank -> Arrays.stream(Suit.values())
+                .map(suit -> new Card(rank, suit)))
+            .collect(Collectors.toCollection(ArrayList::new));
     }
-
 }
