b2801a688a465e89eff18b1d93c4a32934a80a4d
minzzun99 <skg06123@naver.com>
2026-03-08 15:34:47 +0900

    refactor: 줄바꿈 제거 및 공백 추가

--- src/main/java/domain/ScoreCalculator.java
+++ src/main/java/domain/ScoreCalculator.java
@@ -7,7 +7,6 @@ public class ScoreCalculator {
     private static final int BUST_LIMIT_SCORE = 21;
 
     public static int calculate(List<Card> cards) {
-
         int baseSum = calculateBaseSum(cards);
         int aceCount = countAce(cards);
         return applyAceLogic(baseSum, aceCount);
@@ -15,34 +14,32 @@ public class ScoreCalculator {
 
 
     private static int calculateBaseSum(List<Card> cards) {
-        int sum=0;
-        for(Card card: cards) {
-            sum+=card.getCardNumber().getValue();
+        int sum = 0;
+        for (Card card : cards) {
+            sum += card.getCardNumber().getValue();
         }
         return sum;
     }
 
     private static int countAce(List<Card> cards) {
-        return (int)cards.stream()
+        return (int) cards.stream()
                 .map(Card::getCardNumber)
-                .filter(cardNumber->cardNumber==CardNumber.ACE)
+                .filter(cardNumber -> cardNumber == CardNumber.ACE)
                 .count();
     }
 
     private static int applyAceLogic(int baseSum, int aceCount) {
         int score = baseSum;
-        for(int i=0; i<aceCount; i++) {
+        for (int i = 0; i < aceCount; i++) {
             score = addAceBonus(score);
         }
         return score;
     }
 
     private static int addAceBonus(int score) {
-        if(score+ACE_BONUS<=BUST_LIMIT_SCORE){
+        if (score + ACE_BONUS <= BUST_LIMIT_SCORE) {
             return score + ACE_BONUS;
         }
         return score;
     }
-
-
 }
