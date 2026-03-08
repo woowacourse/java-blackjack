0fa789ccfda5feef262f58588a0b8cb37c5b14e2
Chocoding1 <galmeagi2@naver.com>
2026-03-08 15:09:45 +0900

    refactor: name 값객체 생성에 따른 수정

--- src/main/java/blackjack/util/PlayerParser.java
+++ src/main/java/blackjack/util/PlayerParser.java
@@ -9,14 +9,9 @@ import java.util.List;
 
 public class PlayerParser {
 
-    private static final String PLAYER_NAME_REGREX = "^[a-zA-Z가-힣]*$";
-
     public static List<Player> parse(String names) {
         return Arrays.stream(names.split(","))
                 .map(String::trim)
-                .peek((name) -> Validator.validateRegrex(PLAYER_NAME_REGREX, name,
-                        ERROR_INVALID_PLAYER_NAME.getErrorMessage()))
-                .peek((name) -> Validator.validateEmpty(name, ERROR_EMPTY_INPUT.getErrorMessage()))
                 .map(Player::new)
                 .toList();
     }
