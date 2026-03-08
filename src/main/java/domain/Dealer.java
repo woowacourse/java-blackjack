ea79a35029854751f78829d71806eb36d90c13d9
심호경 <ghrudtla@naver.com>
2026-03-08 15:04:34 +0900

    feat: Player와 중복된 메서드 제거해서 다시 구현

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -2,30 +2,21 @@ package domain;
 
 import java.util.List;
 
-public class Dealer {
-    private final Hand hand;
+public class Dealer extends Participant{
+    private static final int DEALER_CARD_SUM_THRESHOLD=16;
 
     private Dealer(Hand hand) {
-        this.hand = hand;
+        super(hand);
     }
 
     public static Dealer from(Hand hand) {
         return new Dealer(hand);
     }
 
-    public List<Card> getHandCards() {
-        return hand.getCards();
-    }
-
-    public Hand getHand() {
-        return hand;
-    }
 
     public boolean checkThreshold() {
-        return hand.getScore().getValue() <= 16;
+        return getHand().getScore().getValue() <= DEALER_CARD_SUM_THRESHOLD;
     }
 
-    public void addHandCard(Card card) {
-        hand.addCard(card);
-    }
+
 }
