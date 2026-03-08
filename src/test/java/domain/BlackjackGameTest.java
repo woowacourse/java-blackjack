054b8a89c40fe3015793123d49a9b822b826d555
심호경 <ghrudtla@naver.com>
2026-03-08 15:11:38 +0900

    test: 카드,딜러,플레이어 초기화와 게임 규칙을 가지는 Blackjack 테스트 구현

---
+++ src/test/java/domain/BlackjackGameTest.java
@@ -0,0 +1,44 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BlackjackGameTest {
+
+    @Test
+    @DisplayName("게임 시작 시 딜러와 모든 플레이어는 처음 카드 2장을 받는다")
+    void 게임_시작시_딜러와_플레이어에게_카드2장_배분() {
+        List<String> names = List.of("pobi", "jason");
+
+        BlackjackGame blackjackGame = BlackjackGame.start(names);
+
+        Dealer dealer = blackjackGame.getDealer();
+        Players players = blackjackGame.getPlayers();
+
+        assertThat(dealer.getHand().getCards()).hasSize(2);
+        assertThat(players.getPlayers()).hasSize(2);
+
+        for (Player player : players.getPlayers()) {
+            assertThat(player.getHand().getCards()).hasSize(2);
+        }
+    }
+
+
+
+    @Test
+    @DisplayName("모든 플레이어가 버스트면 딜러는 카드를 더 뽑지 않는다")
+    void 모든_플레이어_버스트면_딜러는_카드뽑는걸_중단한다(){
+        BlackjackGame blackjackGame = BlackjackGame.start(List.of("pobi"));
+        Player player = blackjackGame.getPlayers().getPlayers().getFirst();
+
+        while (!player.isBust()) {
+            blackjackGame.addPlayerCard(player);
+        }
+
+        boolean result = blackjackGame.shouldDealerDraw();
+        assertThat(result).isFalse();
+    }
+}
