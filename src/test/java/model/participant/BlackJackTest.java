5d82f93c2848e92bb50765ffce65069da76a3437
HyoYoonNam <dev.rudevico@gmail.com>
2026-03-08 20:57:46 +0900

    test: 테스트 코드 파일 구조 변경

--- src/test/java/model/participant/BlackJackTest.java
+++ src/test/java/model/participant/BlackJackTest.java
@@ -1,11 +1,11 @@
+package model.participant;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import java.util.Map;
 import model.BlackJack;
 import model.Card;
-import model.participant.Dealer;
-import model.participant.Participant;
 import model.Participants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
