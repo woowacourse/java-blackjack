c42f539eb3e1014879552c8856ef73a870f266e3
haeyoon1 <hynam1220@naver.com>
2026-03-09 00:52:00 +0900

    feat: 사용자 이름 입력 시 검증 로직 추가
    - 문자열 파싱 시 공백 제거
    - 사용자 이름 1개 이상 입력 필요

--- src/main/java/meesage/ErrorMessage.java
+++ src/main/java/meesage/ErrorMessage.java
@@ -2,7 +2,9 @@ package meesage;
 
 public enum ErrorMessage {
 
-    EMPTY_INPUT("[ERROR] 사용자 이름은 공백을 입력할 수 없습니다.");
+    EMPTY_INPUT("[ERROR] 사용자 이름은 공백을 입력할 수 없습니다."),
+    INVALID_NAMES_SIZE("[ERROR] 하나 이상의 사용자 이름을 입력하세요."),
+    ;
 
     private final String message;
 
--- src/main/java/utils/InputParser.java
+++ src/main/java/utils/InputParser.java
@@ -1,6 +1,8 @@
 package utils;
 
+import java.util.Arrays;
 import java.util.List;
+
 import meesage.ErrorMessage;
 
 public class InputParser {
@@ -9,12 +11,25 @@ public class InputParser {
 
     public static List<String> splitByDelimiter(String input) {
         validate(input);
-        return List.of(input.split(DELIMITER));
+
+        List<String> names = Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .filter(name -> !name.isBlank())
+                .toList();
+        validateNames(names);
+
+        return names;
     }
 
     private static void validate(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
+        }
+    }
+
+    private static void validateNames(List<String> names) {
+        if (names.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALIDE_NAMES_SIZE.getMessage());
         }
     }
 }
