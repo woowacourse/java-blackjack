2bc3200d8101e305bf1fac4a24ce1a2e272a7852
조상준 <juncho12011201@gmail.com>
2026-03-08 16:34:27 +0900

    refactor(config): 전역 설정 파일 싱글톤 구현 방식 수정

--- src/main/java/team/blackjack/config/AppConfig.java
+++ src/main/java/team/blackjack/config/AppConfig.java
@@ -4,21 +4,13 @@ import team.blackjack.controller.BlackJackController;
 import team.blackjack.service.BlackJackService;
 
 public class AppConfig {
-    private static volatile AppConfig instance;
+    private static final AppConfig INSTANCE = new AppConfig();
 
     private AppConfig() {
     }
 
     public static AppConfig getInstance() {
-        if (instance == null) {
-            synchronized (AppConfig.class) {
-                if (instance == null) {
-                    instance = new AppConfig();
-                }
-            }
-        }
-
-        return instance;
+        return INSTANCE;
     }
 
     public BlackJackService blackJackService() {
