0337cad6653a5a6e209e2a48e7b2f66a57c1a8ac
심호경 <ghrudtla@naver.com>
2026-03-08 15:07:06 +0900

    refactor: 계산 로직 인덴트 1로 리팩토링

--- src/main/java/domain/ScoreCalculator.java
+++ src/main/java/domain/ScoreCalculator.java
@@ -4,21 +4,47 @@ import java.util.List;
 
 public class ScoreCalculator {
     // TODO 계산 로직 리팩토링 필요
-    public static int calculate(List<Card> cards) {
-        int sum = 0;
-        int aceCount = 0;
-        for (Card card : cards) {
-            sum += card.getCardNumber().getValue();
-            if (card.getCardNumber().equals(CardNumber.ACE)) {
-                aceCount++;
-            }
-        }
 
-        for (int i = 0; i < aceCount; i++) {
-            if (sum + 10 <= 21) {
-                sum += 10;
-            }
+    private static final int ACE_BONUS = 10;
+    private static final int BUST_LIMIT_SCORE = 21;
+
+    public static int calculate(List<Card> cards) {
+
+        int baseSum = calculateBaseSum(cards);
+        int aceCount = countAce(cards);
+        return applyAceLogic(baseSum, aceCount);
+    }
+
+
+    private static int calculateBaseSum(List<Card> cards) {
+        int sum=0;
+        for(Card card: cards) {
+            sum+=card.getCardNumber().getValue();
         }
         return sum;
     }
+
+    private static int countAce(List<Card> cards) {
+        return (int)cards.stream()
+                .map(Card::getCardNumber)
+                .filter(cardNumber->cardNumber==CardNumber.ACE)
+                .count();
+    }
+
+    private static int applyAceLogic(int baseSum, int aceCount) {
+        int score = baseSum;
+        for(int i=0; i<aceCount; i++) {
+            score = addAceBonus(score);
+        }
+        return score;
+    }
+
+    private static int addAceBonus(int score) {
+        if(score+ACE_BONUS<=BUST_LIMIT_SCORE){
+            return score + ACE_BONUS;
+        }
+        return score;
+    }
+
+
 }
