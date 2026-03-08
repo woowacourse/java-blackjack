1965b4b1ef78788b6463cb22b3b81263e89a1b7f
tjdakf <ksc73450056@gmail.com>
2026-03-08 11:39:58 +0900

    test: Card 테스트 코드 리팩터링

--- src/test/java/domain/CardTest.java
+++ src/test/java/domain/CardTest.java
@@ -7,11 +7,12 @@ import java.util.List;
 import static domain.constant.Rank.*;
 import static domain.constant.Suit.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 class CardTest {
 
     @Test
-    void 카드의_점수를_제공한다() {
+    void 카드의_이름과_기본_점수를_제공한다() {
         List<Card> cards = List.of(
                 new Card(ACE, SPADE),
                 new Card(FOUR, HEART),
@@ -20,27 +21,14 @@ class CardTest {
         );
 
         assertThat(cards)
-                .extracting(Card::getScore)
-                .containsExactly(1, 4, 10, 10);
-    }
-
-    @Test
-    void 카드의_이름을_제공한다() {
-        List<Card> cards = List.of(
-                new Card(ACE, SPADE),
-                new Card(FOUR, HEART),
-                new Card(TEN, DIAMOND),
-                new Card(QUEEN, CLUB)
-        );
-
-        assertThat(cards)
-                .extracting(Card::getName)
+                .extracting(
+                        Card::getName,
+                        Card::getScore)
                 .containsExactly(
-                        "A스페이드",
-                        "4하트",
-                        "10다이아몬드",
-                        "Q클로버"
+                        tuple("A스페이드", 1),
+                        tuple("4하트", 4),
+                        tuple("10다이아몬드", 10),
+                        tuple("Q클로버", 10)
                 );
     }
-
 }
