44e10cf6134962542d610ffcda8323ee428ea77a
Yeji Kim <yejikim.erica@gmail.com>
2026-03-08 18:49:31 +0900

    feat: add name validation feature

--- src/main/java/domain/Participant.java
+++ src/main/java/domain/Participant.java
@@ -1,14 +1,34 @@
 package domain;
 
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 public abstract class Participant {
     private final Deck deck;
     private final String name;
 
     protected Participant(Deck participantDeck, String name) {
+        validateName(name);
         this.deck = participantDeck;
         this.name = name;
+    }
+
+    private static void validateName(String name) {
+        Pattern NAME_PATTERN = Pattern.compile("^[a-zA-Z가-힣]+$");
+        validateIsNotBlank(name);
+        validateKoreanAndEnglish(name, NAME_PATTERN);
+    }
+
+    private static void validateKoreanAndEnglish(String name, Pattern NAME_PATTERN) {
+        if (!NAME_PATTERN.matcher(name).matches()) {
+            throw new IllegalArgumentException(ErrorMessage.ONLY_KO_AND_ENG.getMessage());
+        }
+    }
+
+    private static void validateIsNotBlank(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_ALLOW_EMPTY_INPUT.getMessage());
+        }
     }
 
     public Deck getDeck() {
