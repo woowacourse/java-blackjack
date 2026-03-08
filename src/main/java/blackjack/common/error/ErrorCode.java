0b1a549c8b67db251d913e191dd1bbcd059cf568
박병훈 <leafloveu@naver.com>
2026-03-08 23:20:21 +0900

    refactor(cardDeck): CardDeck.of()및 validate 메서드 리팩토링
    - forEach 대신 flatMap 사용
    - 카드 덱 개수 검증 제거
    - validate 메서드 분리 및 예외 메시지 추가

--- src/main/java/blackjack/common/error/ErrorCode.java
+++ src/main/java/blackjack/common/error/ErrorCode.java
@@ -2,7 +2,9 @@ package blackjack.common.error;
 
 public enum ErrorCode {
 
-    NULL_OR_EMPTY_CARDS("카드가 존재하지 않습니다.");
+    NULL_OR_EMPTY_CARDS("카드가 존재하지 않습니다."),
+    NULL_PICK_STRATEGY("PickStrategy가 null 입니다."),
+    EMPTY_CARD_DECK("카드 덱에 더 이상 카드가 존재하지 않습니다.");
 
     private final String message;
 
--- src/main/java/blackjack/model/cardDeck/CardDeck.java
+++ src/main/java/blackjack/model/cardDeck/CardDeck.java
@@ -1,47 +1,54 @@
 package blackjack.model.cardDeck;
 
+import blackjack.common.error.ErrorCode;
 import blackjack.model.card.Card;
 import blackjack.model.card.Rank;
 import blackjack.model.card.Suit;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CardDeck {
 
-    private static final int CARD_DECK_SIZE = 52;
-
     private final List<Card> cards;
     private final PickStrategy pickStrategy;
 
     public CardDeck(List<Card> cards, PickStrategy pickStrategy) {
-        if (cards.isEmpty()) {
-            throw new IllegalStateException("cards가 null입니다.");
-        }
+        validateCards(cards);
+        validatePickStrategy(pickStrategy);
+
         this.cards = cards;
         this.pickStrategy = pickStrategy;
     }
 
     public static CardDeck of(PickStrategy pickStrategy) {
-        List<Card> cards = new ArrayList<>();
-
-        Arrays.stream(Suit.values())
-                .forEach(suit -> Arrays.stream(Rank.values())
-                        .forEach(rank -> cards.add(Card.opened(rank, suit))));
-
-        if (cards.size() != CARD_DECK_SIZE) {
-            throw new IllegalStateException("덱이 잘 못 생성됐습니다.");
-        }
+        List<Card> cards = Arrays.stream(Suit.values())
+                .flatMap(suit -> Arrays.stream(Rank.values())
+                        .map(rank -> Card.opened(rank, suit)))
+                .toList();
 
         return new CardDeck(cards, pickStrategy);
     }
 
-    // 카드 덱에서 1장 가져온다.
     public Card pick() {
-        if (cards.isEmpty()) {
-            throw new IllegalStateException("카드 덱에 카드의 개수가 부족합니다.");
-        }
-
+        validateCanPick();
         return pickStrategy.pick(cards);
+    }
+
+    private void validateCanPick() {
+        if (cards.isEmpty()) {
+            throw new IllegalStateException(ErrorCode.EMPTY_CARD_DECK.getMessage());
+        }
+    }
+
+    private void validateCards(List<Card> cards) {
+        if (cards == null || cards.isEmpty()) {
+            throw new IllegalStateException(ErrorCode.NULL_OR_EMPTY_CARDS.getMessage());
+        }
+    }
+
+    private void validatePickStrategy(PickStrategy pickStrategy) {
+        if (pickStrategy == null) {
+            throw new IllegalArgumentException(ErrorCode.NULL_PICK_STRATEGY.getMessage());
+        }
     }
 }
