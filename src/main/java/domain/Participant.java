9bb0cfd49f80d05f4304f9d6b28ce394bfbe2ef8
심호경 <ghrudtla@naver.com>
2026-03-08 15:04:09 +0900

    feat: Dealer와 Player의 공통된 메서드를 제거하기 위한 Participant 클래스 추가

---
+++ src/main/java/domain/Participant.java
@@ -0,0 +1,28 @@
+package domain;
+
+import java.util.List;
+
+public abstract class Participant {
+    private final Hand hand;
+
+    protected Participant(Hand hand){
+        this.hand=hand;
+    }
+
+    public List<Card> getHandCards(){
+        return hand.getCards();
+    }
+
+    public Hand getHand(){
+        return hand;
+    }
+
+    public void addHandCard(Card card){
+        hand.addCard(card);
+    }
+
+    public boolean isBust(){
+        return hand.getScore().isBust();
+    }
+
+}
