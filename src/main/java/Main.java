c8818cd6a1bf6d26675e1bd5fb8f222224182d02
Yeji Kim <yejikim.erica@gmail.com>
2026-03-08 19:40:25 +0900

    feat: assemble components for running

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import controller.BlackJackController;
+import domain.CardCreationStrategy;
+import domain.RandomCardCreationStrategy;
+import view.InputView;
+import view.OutputView;
+
 public class Main {
     public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        CardCreationStrategy strategy = new RandomCardCreationStrategy();
 
+        new BlackJackController(inputView, outputView, strategy).doGame();
     }
 }
