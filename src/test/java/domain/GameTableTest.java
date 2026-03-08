0c44af13a82a471ff7fb613d8a9e0fd715967225
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-08 14:57:01 +0900

    refactor(GameTable): Member분리에 따른 메서드 분리

--- src/test/java/domain/GameTableTest.java
+++ src/test/java/domain/GameTableTest.java
@@ -1,7 +1,6 @@
 package domain;
 
 import domain.card.Card;
-import domain.table.GameTable;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -10,15 +9,17 @@ import org.junit.jupiter.api.Test;
 class GameTableTest {
 
     private final GameTable gameTable;
-    private final String playerName = "포비";
 
     public GameTableTest() {
-        this.gameTable = new GameTable(List.of(playerName));
+        this.gameTable = new GameTable();
     }
 
     @DisplayName("카드의 총합이 21보다 크면 CurrentResult의 isBust는 true이다.")
     @Test
     void checkCurrentTest_playerHasCardSumOf22_returnTrue() {
+        String playerName = "포비";
+        gameTable.joinPlayer(List.of(playerName));
+
         gameTable.draw(playerName, new Card("10","클로버"));
         gameTable.draw(playerName, new Card("5", "하트"));
         gameTable.draw(playerName, new Card("7", "하트"));
@@ -29,6 +30,9 @@ class GameTableTest {
     @DisplayName("카드의 총합이 21보다 작으면 CurrentResult의 isBust는 false이다.")
     @Test
     void checkCurrentTest_playerHasCardSumOf20_returnFalse() {
+        String playerName = "포비";
+        gameTable.joinPlayer(List.of(playerName));
+
         gameTable.draw(playerName, new Card("10","클로버"));
         gameTable.draw(playerName, new Card("5", "하트"));
         gameTable.draw(playerName, new Card("5", "스페이드"));
