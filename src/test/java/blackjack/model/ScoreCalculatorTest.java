07bcf7d05d75f2bbeff5d16411df02c35389cb29
smini <sm020320@naver.com>
2026-03-08 20:52:45 +0900

    test: 에이스 계산 관련 테스트 케이스 추가

--- src/test/java/blackjack/model/ScoreCalculatorTest.java
+++ src/test/java/blackjack/model/ScoreCalculatorTest.java
@@ -26,4 +26,31 @@ class ScoreCalculatorTest {
         assertThat(score.getScore()).isEqualTo(20);
         assertThat(score2.getScore()).isEqualTo(12);
     }
+
+    @Test
+    @DisplayName("에이스가 2개일 때, 합이 21을 넘지 않도록 하나는 1로, 하나는 11로 계산한다")
+    void calculateAceTwoTest() {
+        // given
+        List<Card> cards = List.of(
+                new Card(Figure.SPADE, Number.ACE),
+                new Card(Figure.HEART, Number.ACE));
+        // when
+        Score score = ScoreCalculator.calculate(cards);
+        // then
+        assertThat(score.getScore()).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("에이스가 2개여도 다른 카드와의 합이 21을 넘으면 둘 다 1로 계산한다")
+    void calculateAceTwo() {
+        // given
+        List<Card> cards = List.of(
+                new Card(Figure.HEART, Number.ACE),
+                new Card(Figure.DIAMOND, Number.ACE),
+                new Card(Figure.CLOVER, Number.SIX));
+        // when
+        Score score = ScoreCalculator.calculate(cards);
+        // then
+        assertThat(score.getScore()).isEqualTo(18);
+    }
 }
