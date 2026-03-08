c9d045c768b6ebd0e9f43b16d203cb25122a75e5
심호경 <ghrudtla@naver.com>
2026-03-08 15:13:44 +0900

    refactor: 카드,플레이어,딜러를 초기화 하는 메서드를 Blackjack 도메인으로 옮겨 책임 분리

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,11 +1,8 @@
 package controller;
 
-import domain.Card;
+import domain.BlackjackGame;
 import domain.Dealer;
-import domain.Deck;
 import domain.GameResultCalculator;
-import domain.Hand;
-import domain.Name;
 import domain.Player;
 import domain.Players;
 import domain.TotalFinalResult;
@@ -14,35 +11,29 @@ import dto.DealerResultDto;
 import dto.PlayerDto;
 import dto.PlayersDto;
 import dto.TotalFinalResultsDto;
-import java.util.ArrayList;
 import java.util.List;
 import view.InputView;
 import view.OutputView;
 
 public class GameController {
-    private static final int INITIAL_CARD_COUNT = 2;
-
-    private final Deck deck;
-
-    public GameController() {
-        this.deck = Deck.createDeck();
-    }
 
     public void start() {
-        // 카드 초기화
-        Dealer dealer = initDealer();
-        Players players = initPlayers();
+        List<String> names = InputView.readParticipants();
+        BlackjackGame blackjackGame = BlackjackGame.start(names);
 
-        // 카드 출력
+
+        Dealer dealer = blackjackGame.getDealer();
+        Players players = blackjackGame.getPlayers();
+
         PlayersDto playersDto = PlayersDto.from(players);
         OutputView.printHandOutMessage(playersDto);
         OutputView.printCardStatus(playersDto, DealerResultDto.from(dealer));
 
         // 플레이어 카드 추가 수령
-        addPlayersCard(players);
+        addPlayersCard(blackjackGame, players);
 
         // 딜러 게임 진행
-        addDealerCards(dealer, players);
+        addDealerCards(blackjackGame);
 
         // 카드 결과, 점수 출력
         printCardResults(DealerResultDto.from(dealer), PlayersDto.from(players));
@@ -51,66 +42,23 @@ public class GameController {
         printFinalResults(players, dealer);
     }
 
-    // 플레이어들 초기화
-    private Players initPlayers() {
-        List<String> names = InputView.readParticipants();
-        List<Player> players = new ArrayList<>();
-        for (String name : names) {
-            players.add(initPlayer(name));
-        }
-
-        return Players.from(players);
-    }
-
-    // 플레이어 초기 카드
-    private Player initPlayer(String name) {
-        return Player.of(Name.from(name), new Hand(initCards()));
-    }
-
-    private List<Card> initCards() {
-        List<Card> cards = new ArrayList<>();
-        for (int count = 0; count < INITIAL_CARD_COUNT; count++) {
-            cards.add(deck.draw());
-        }
-        return cards;
-    }
-
-
-    private Dealer initDealer() {
-        return Dealer.from(new Hand(initCards()));
-    }
-
-    // 플레이어 전체 카드 추가
-    private void addPlayersCard(Players players) {
+    private void addPlayersCard(BlackjackGame blackjackGame, Players players) {
         for (Player player : players.getPlayers()) {
-            checkBustPlayer(player);
+            addPlayerCards(blackjackGame, player);
         }
     }
 
-    private void checkBustPlayer(Player player) {
+    private void addPlayerCards(BlackjackGame blackjackGame, Player player) {
         while (!player.isBust() && InputView.checkAddCard(player.getName().getName())) {
-            addPlayerCard(player);
+            blackjackGame.addPlayerCard(player);
+            OutputView.printPlayerCardStatus(PlayerDto.from(player));
         }
     }
 
-    // 각 플레이어별 카드추가
-    private void addPlayerCard(Player player) {
-        player.addHandCard(deck.draw());
-        OutputView.printPlayerCardStatus(PlayerDto.from(player));
-    }
-
-    // 딜러 카드 추가하기 (반복)
-    private void addDealerCards(Dealer dealer, Players players) {
-        if (dealer.checkThreshold() && !players.isAllBust()) {
+    private void addDealerCards(BlackjackGame blackjackGame) {
+        if (blackjackGame.shouldDealerDraw()) {
             OutputView.printAddDealerCardMessage();
-            addDealerCard(dealer);
-        }
-    }
-
-    // 딜러 카드 추가하기
-    private void addDealerCard(Dealer dealer) {
-        while (dealer.checkThreshold()) {
-            dealer.addHandCard(deck.draw());
+            blackjackGame.playDealerTurn();
         }
     }
 
