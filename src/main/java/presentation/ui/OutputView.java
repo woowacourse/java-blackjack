6946c7be2598eda8a3cec345653753f018517046
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-08 14:57:37 +0900

    refactor(OutputView): 출력 로직 수정

--- src/main/java/presentation/ui/OutputView.java
+++ src/main/java/presentation/ui/OutputView.java
@@ -2,11 +2,13 @@ package presentation.ui;
 
 import static constant.Word.*;
 
-import application.dto.RoundResult;
-import domain.Card;
-import domain.dto.GameResult;
+import application.dto.GameResult;
+import domain.RoundResult;
+import domain.card.Card;
 import domain.dto.MemberStatus;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class OutputView {
@@ -18,12 +20,12 @@ public class OutputView {
         System.out.println();
     }
 
-    public void printCurrentCard(String playerName, RoundResult roundResult) {
-        String cards = roundResult.cards().stream().map(Card::cardName).collect(Collectors.joining(", "));
-        System.out.println(CARD_STATUS.format(playerName, cards));
+    public void printCurrentCard(String playerName, List<Card> cards) {
+        String cardNames = cards.stream().map(Card::cardName).collect(Collectors.joining(", "));
+        System.out.println(CARD_STATUS.format(playerName, cardNames));
     }
 
-    public void printDealerDrawOut() {
+    public void printDealerDrawResult() {
         System.out.println();
         System.out.println(DEALER_DRAW_MESSAGE.format());
         System.out.println();
@@ -34,9 +36,28 @@ public class OutputView {
         System.out.println();
     }
 
-    public void printGameResult(List<GameResult> gameResults) {
+    public void printGameResult(GameResult gameResult) {
         System.out.println(FINAL_GAME_RESULT_MESSAGE.format());
-        gameResults.forEach(this::printMemberResult);
+        printDealerGameResult(
+                gameResult.dealerWinAmount(),
+                gameResult.dealerLoseAmount()
+        );
+        printPlayerGameResult(gameResult.playerResults());
+    }
+
+    private void printDealerGameResult(int winAmount, int loseAmount) {
+        System.out.println(DEALER_GAME_RESULT.format(winAmount, loseAmount));
+    }
+
+    private void printPlayerGameResult(Map<String, RoundResult> roundResults) {
+        for (Entry<String, RoundResult> round : roundResults.entrySet()) {
+            System.out.println(
+                    ROUND_RESULT.format(
+                            round.getKey(),
+                            round.getValue().result()
+                    )
+            );
+        }
     }
 
     private void printDistributeMessage(List<MemberStatus> playerStatuses) {
@@ -72,27 +93,5 @@ public class OutputView {
     private void printFinalMemberCardAndResult(MemberStatus status) {
         String cards = status.cards().stream().map(Card::cardName).collect(Collectors.joining(", "));
         System.out.println(CARD_STATUS.format(status.playerName(), cards) + RESULT_MESSAGE.format(status.totalValue()));
-    }
-
-    private void printMemberResult(GameResult gameResult) {
-        String name = gameResult.name();
-        List<Boolean> results = gameResult.result();
-        if (name.equals(DEALER.format())) {
-            printDealerResult(results, name);
-            return;
-        }
-        if (results.getFirst()) {
-            System.out.println(PLAYER_GAME_WIN.format(name));
-            return;
-        }
-        System.out.println(PLAYER_GAME_LOSE.format(name));
-    }
-
-    private static void printDealerResult(List<Boolean> results, String name) {
-        int win = Math.toIntExact(results.stream().filter(result -> result == true)
-                .count());
-        int lose = Math.toIntExact(results.stream().filter(result -> result == false)
-                .count());
-        System.out.println(DEALER_GAME_RESULT.format(name, win, lose));
     }
 }
