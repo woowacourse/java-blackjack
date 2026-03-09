aed59d15e425954593dfedda58898a2619cfed4c
Huiyeongkim <huiyeong9619@naver.com>
2026-03-09 12:59:51 +0900

    refactor: static final 순으로 변경

--- src/main/java/util/BlackJackConstant.java
+++ src/main/java/util/BlackJackConstant.java
@@ -2,8 +2,8 @@ package util;
 
 public class BlackJackConstant {
 
-    public final static int BLACKJACK_SCORE = 21;
-    public final static int INIT_HAND_SIZE = 2;
+    public static final int BLACKJACK_SCORE = 21;
+    public static final int INIT_HAND_SIZE = 2;
     public static final int ACE_ADJUSTMENT_VALUE = 10;
     public static final int DEALER_HIT_LIMIT = 16;
     public static final int MAX_NAME_LENGTH = 8;
