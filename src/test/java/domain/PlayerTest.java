656f4be3fdabe40233213cfbf80ab0aad84b5262
최용준 <yj9107vv@gmail.com>
2026-03-09 20:57:31 +0900

    test(PlayerTest): 단위 테스트 추가
    - 플레이어 합계 21 초과 여부
    - 플레이의 보유한 카드 종류가 올바른지 테스트
    - 플레이어 점수가 21 초과인 경우 점수 처리 테스트

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,13 +12,33 @@ class PlayerTest {
     private final String userName = "pobi";
     private final Cards cards = Cards.of();
 
-
     @Test
     @DisplayName("플레이어 객체 생성 시 2장의 카드를 보유한지 테스트")
     void holding_two_card_success() {
         Player player = Player.of(Cards.of().drawInitialHand(), userName);
 
         assertThat(player.getCardsInfo()).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("플레이어 합계가 21 초과 여부 테스트")
+    void isBust_player_score_success() {
+        Player player = Player.of(cards.drawInitialHand(), userName);
+
+        assertThat(player.isBust(22)).isTrue();
+    }
+
+    @Test
+    @DisplayName("플레이어의 보유한 카드들의 종류가 올바르게 반환하는지 테스트")
+    void have_card_type_return_value_correct() {
+        List<Card> initialCards = new ArrayList<>(List.of(
+                Card.of(Rank.TEN, Suit.DIAMOND),
+                Card.of(Rank.FIVE, Suit.CLOVER)));
+        Player player = Player.of(initialCards, userName);
+
+        List<String> expect = List.of(Rank.TEN.getName() + Suit.DIAMOND.getName(),
+                Rank.FIVE.getName() + Suit.CLOVER.getName());
+        assertThat(player.getCardsInfo()).isEqualTo(expect);
     }
 
     @Test
@@ -32,6 +53,18 @@ class PlayerTest {
         int cardScore = Player.of(cards, userName).calculateScore();
 
         assertThat(cardScore).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("플레이어 ACE 판정 이후 점수가 21 초과이면 0점 처리")
+    void isBust_final_score_zero_success() {
+        List<Card> cards = List.of(
+                Card.of(Rank.FIVE, Suit.DIAMOND),
+                Card.of(Rank.NINE, Suit.CLOVER),
+                Card.of(Rank.EIGHT, Suit.DIAMOND));
+        Player player = Player.of(cards, userName);
+
+        assertThat(player.getScoreOrZeroIfBust()).isZero();
     }
 
     @Test
