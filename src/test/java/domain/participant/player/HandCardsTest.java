c2e0bde9ad404909b1fafe82a8b13a97a5631e26
choubung <choubung@sookmyung.ac.kr>
2026-03-09 22:16:43 +0900

    Test(HandCards): 손패 계산 로직 테스트 작성

--- src/test/java/domain/participant/player/HandCardsTest.java
+++ src/test/java/domain/participant/player/HandCardsTest.java
@@ -1,4 +1,43 @@
-import static org.junit.jupiter.api.Assertions.*;
+package domain.participant.player;
+
+import domain.card.Card;
+import domain.card.TrumpNumber;
+import domain.card.TrumpSuit;
+import domain.participant.HandCards;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class HandCardsTest {
-  
+    @DisplayName("복잡한 로직 및 리스트 검증")
+    @ParameterizedTest
+    @MethodSource("provideHandCardsData") // ★ 아래 static 메서드 이름과 같아야 함!
+    void complexTest(List<Card> cards, int exceptResult) {
+        HandCards handCards = new HandCards();
+
+        for (Card card : cards) {
+            handCards.drawCard(card);
+        }
+
+        assertThat(handCards.calculateCardsScore()).isEqualTo(exceptResult);
+    }
+
+    static Stream<Arguments> provideHandCardsData() {
+        return Stream.of(
+                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.NINE), new Card(TrumpSuit.CLUB, TrumpNumber.NINE)), 18), // ACE 없는 경우
+                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.JACK), new Card(TrumpSuit.CLUB, TrumpNumber.ACE)), 21), // ACE가 11으로 처리되어야 하는 경우
+                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.SEVEN), new Card(TrumpSuit.CLUB, TrumpNumber.NINE),
+                        new Card(TrumpSuit.CLUB, TrumpNumber.ACE)), 17), // ACE가 1로 처리되어야 하는 경우
+                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.ACE), new Card(TrumpSuit.CLUB, TrumpNumber.ACE),
+                        new Card(TrumpSuit.CLUB, TrumpNumber.NINE)), 21), // ACE가 두 개이며, 하나는 11, 하나는 1로 처리되어야 하는 경우
+                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.ACE), new Card(TrumpSuit.CLUB, TrumpNumber.ACE),
+                        new Card(TrumpSuit.CLUB, TrumpNumber.NINE)), 21)// ACE가 두 개이며, 둘 다 1로 처리되어야 하는 경우
+        );
+    }
 }
