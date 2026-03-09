93e880f74950c803b88147c1c8738d4d06763ee8
Jihyun3478 <leejihyun3478@gmail.com>
2026-03-10 02:21:31 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -1,12 +1,6 @@
 package view;
 
 public class InputValidator {
-    public static void validateInputEmpty(String input) {
-        if (input.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 입력값이 비어있습니다.");
-        }
-    }
-
     public static void validateHitOption(String input) {
         if (!input.equals("y") && !input.equals("n")) {
             throw new IllegalArgumentException("[ERROR] y 또는 n을 입력해주세요.");
