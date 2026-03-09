1341acc5489e40ee8891b370e4984ab1f57d075b
JohnPrk <uo3641493@gmail.com>
2026-03-09 13:10:36 +0900

    fix : @EnumSource을 이용해서 enum의 다른 값들도 함께 테스트할 수 있도록 수정

--- src/test/java/blackjack/domain/card/RankTest.java
+++ src/test/java/blackjack/domain/card/RankTest.java
@@ -3,6 +3,9 @@ package blackjack.domain.card;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class RankTest {
 
@@ -11,13 +14,9 @@ class RankTest {
         assertThat(Rank.ACE.isAce()).isTrue();
     }
 
-    @Test
-    void ACE가_아닌_카드는_isAce가_거짓이다() {
-        assertThat(Rank.KING.isAce()).isFalse();
-    }
-
-    @Test
-    void ACE_바로_다음_순서인_TWO는_isAce가_거짓이다() {
-        assertThat(Rank.TWO.isAce()).isFalse();
+    @ParameterizedTest
+    @EnumSource(value = Rank.class, names = {"TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING"})
+    void ACE가_아닌_카드는_isAce가_거짓이다(Rank rank) {
+        assertThat(rank.isAce()).isFalse();
     }
 }
