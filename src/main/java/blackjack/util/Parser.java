2dc8311a249750ac72434963477faab528bf3ea3
dahxxn <dahyeonhan4529@gmail.com>
2026-03-09 13:06:10 +0900

    feat: Parser y 혹은 n 검증 메서드 추가
    - 입력값이 y 혹은 n 인지에 대한 검증 후 true/false 반환

--- src/main/java/blackjack/util/Parser.java
+++ src/main/java/blackjack/util/Parser.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
+    private static final String YES = "y";
+    private static final String NO = "n";
+
+    public static void notEmpty(String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("입력이 비어있습니다.");
+        }
+    }
+
     public static List<String> splitDelimiter(String playerNames) {
         String[] values = playerNames.split(",");
 
@@ -15,10 +24,14 @@ public class Parser {
         return splitResult;
     }
 
-    public static void notEmpty(String value) {
-        if (value == null || value.isBlank()) {
-            throw new IllegalArgumentException("입력이 비어있습니다.");
+    public static boolean yesOrNo(String value) {
+        if (value.equals(YES)) {
+            return true;
         }
+        if (value.equals(NO)) {
+            return false;
+        }
+        throw new IllegalArgumentException("y 혹은 n으로 입력해야 합니다.");
     }
 
 }
