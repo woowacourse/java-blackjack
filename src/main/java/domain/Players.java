ba63f5b091898416dd24a337389d373562ebfe81
picetea44 <picetea44@gmail.com>
2026-03-09 11:28:22 +0900

    feat: Players 승패 판정 기능 추가

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -1,10 +1,13 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Players {
     private static final String DEALER_NAME = "딜러";
+    private static final int BUST_THRESHOLD = 21;
 
     private final List<Player> playerList;
 
@@ -17,6 +20,37 @@ public class Players {
         for (String name : names) {
             playerList.add(new Player(name));
         }
+    }
+
+    public Player getDealer() {
+        return playerList.get(0);
+    }
+
+    public List<Player> getGamePlayers() {
+        return playerList.subList(1, playerList.size());
+    }
+
+    public Map<Player, Result> judge() {
+        int dealerScore = getDealer().calculateScore();
+        boolean dealerBust = dealerScore > BUST_THRESHOLD;
+        Map<Player, Result> results = new LinkedHashMap<>();
+        for (Player player : getGamePlayers()) {
+            results.put(player, judgePlayer(player.calculateScore(), dealerScore, dealerBust));
+        }
+        return results;
+    }
+
+    private Result judgePlayer(int playerScore, int dealerScore, boolean dealerBust) {
+        if (playerScore > BUST_THRESHOLD) {
+            return Result.LOSE;
+        }
+        if (dealerBust) {
+            return Result.WIN;
+        }
+        if (playerScore > dealerScore) {
+            return Result.WIN;
+        }
+        return Result.LOSE;
     }
 
     public int getSize() {
