2a6d0c15fe669a6b44c9d9202743cc39c8ea82fe
xxbeann <yjb4945@gmail.com>
2026-03-09 17:38:20 +0900

    refactor: testCase 메서드명 변경

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -17,7 +17,7 @@ class InputViewTest {
 
     @Test
     @DisplayName("플레이어의 수는 8명을 넘을 수 없다.")
-    void player_limit() {
+    void askPlayerNames_NinePlayers_ThrowsException() {
         command("가,나,다,라,마,바,사,아,차");
         assertThrows(IllegalArgumentException.class, () -> {
             InputView.askPlayerNames();
@@ -26,7 +26,7 @@ class InputViewTest {
 
     @Test
     @DisplayName("입력은 y와 n만 허용한다.")
-    void yes_or_no(){
+    void askContinue_InputOtherThanYAndN_ThrowsException() {
         command("dongkey");
         String player = "JeongKong";
         assertThrows(IllegalArgumentException.class, () -> {
