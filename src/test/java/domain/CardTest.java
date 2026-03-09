75a4168fa857afddf76ad2e757c5ff4d0e5de050
Giyoul <cla12093@gmail.com>
2026-03-09 14:25:23 +0900

    test(domain): add domain tests

---
+++ src/test/java/domain/CardTest.java
@@ -0,0 +1,70 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import vo.Rank;
+import vo.Suit;
+
+public class CardTest {
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
+    @Test
+    void 에이스_카드_점수는_11() {
+        // given
+        Card card = new Card(Suit.SPADE, Rank.ACE);
+
+        // when
+        int score = card.getCardScore();
+
+        // then
+        assertThat(score).isEqualTo(11);
+    }
+
+    @Test
+    void 킹_카드_점수는_10() {
+        // given
+        Card card = new Card(Suit.HEART, Rank.KING);
+
+        // when
+        int score = card.getCardScore();
+
+        // then
+        assertThat(score).isEqualTo(10);
+    }
+
+    @Test
+    void 숫자_카드_점수_반환() {
+        // given
+        Card card = new Card(Suit.DIAMOND, Rank.SEVEN);
+
+        // when
+        int score = card.getCardScore();
+
+        // then
+        assertThat(score).isEqualTo(7);
+    }
+
+    @Test
+    void 에이스_카드_확인() {
+        // given
+        Card aceCard = new Card(Suit.SPADE, Rank.ACE);
+        Card kingCard = new Card(Suit.SPADE, Rank.KING);
+
+        // when & then
+        assertThat(aceCard.isAceCard()).isTrue();
+        assertThat(kingCard.isAceCard()).isFalse();
+    }
+
+    @Test
+    void 카드_표시_이름_반환() {
+        // given
+        Card card = new Card(Suit.SPADE, Rank.ACE);
+
+        // when
+        String displayName = card.getDisplayName();
+
+        // then
+        assertThat(displayName).isEqualTo("A스페이드");
+    }
+}
---
+++ src/test/java/domain/DealerTest.java
@@ -0,0 +1,132 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import vo.GameResult;
+import vo.Rank;
+import vo.Suit;
+
+public class DealerTest {
+
+    private Dealer dealer;
+
+    @BeforeEach
+    void setUp() {
+        dealer = new Dealer();
+    }
+
+    @Test
+    void 유저_버스트_패() {
+        // given
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.HEART, Rank.SEVEN));
+        dealer.calculateScore();
+        int userScore = 22;
+
+        // when
+        GameResult result = dealer.judgeUserWin(userScore);
+
+        // then
+        assertThat(result).isEqualTo(GameResult.LOSE);
+    }
+
+    @Test
+    void 유저_블랙잭_승() {
+        // given
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.HEART, Rank.SEVEN));
+        dealer.calculateScore();
+        int userScore = 21;
+
+        // when
+        GameResult result = dealer.judgeUserWin(userScore);
+
+        // then
+        assertThat(result).isEqualTo(GameResult.WIN);
+    }
+
+    @Test
+    void 딜러_버스트_유저_승() {
+        // given
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.KING));
+        dealer.receiveCard(new Card(Suit.HEART, Rank.QUEEN));
+        dealer.receiveCard(new Card(Suit.DIAMOND, Rank.TWO));
+        dealer.calculateScore();
+        int userScore = 18;
+
+        // when
+        GameResult result = dealer.judgeUserWin(userScore);
+
+        // then
+        assertThat(result).isEqualTo(GameResult.WIN);
+    }
+
+    @Test
+    void 유저_점수_높으면_승() {
+        // given
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.HEART, Rank.FIVE));
+        dealer.calculateScore();
+        int userScore = 18;
+
+        // when
+        GameResult result = dealer.judgeUserWin(userScore);
+
+        // then
+        assertThat(result).isEqualTo(GameResult.WIN);
+    }
+
+    @Test
+    void 유저_점수_낮으면_패() {
+        // given
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.HEART, Rank.EIGHT));
+        dealer.calculateScore();
+        int userScore = 15;
+
+        // when
+        GameResult result = dealer.judgeUserWin(userScore);
+
+        // then
+        assertThat(result).isEqualTo(GameResult.LOSE);
+    }
+
+    @Test
+    void 딜러_점수_같으면_패() {
+        // given
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.HEART, Rank.EIGHT));
+        dealer.calculateScore();
+        int userScore = 18;
+
+        // when
+        GameResult result = dealer.judgeUserWin(userScore);
+
+        // then
+        assertThat(result).isEqualTo(GameResult.LOSE);
+    }
+
+    @Test
+    void 딜러_16이하_히트_필요() {
+        // given
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.HEART, Rank.SIX));
+        dealer.calculateScore();
+
+        // when & then
+        assertThat(dealer.determineDealerDealMore()).isTrue();
+    }
+
+    @Test
+    void 딜러_17이상_히트_불필요() {
+        // given
+        dealer.receiveCard(new Card(Suit.SPADE, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.HEART, Rank.SEVEN));
+        dealer.calculateScore();
+
+        // when & then
+        assertThat(dealer.determineDealerDealMore()).isFalse();
+    }
+}
