255db89f973600e388476e47c7ba5a1c3d91e4e8
khyej <khyej.h@gmail.com>
2026-03-09 16:49:57 +0900

    test: InputParserTest 수정

--- src/test/java/blackjack/util/InputParserTest.java
+++ src/test/java/blackjack/util/InputParserTest.java
@@ -1,44 +1,19 @@
 package blackjack.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import blackjack.model.Player;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-class PlayerParserTest {
+class InputParserTest {
 
     @Test
-    @DisplayName("플레이어 이름 파싱 및 플레이어 객체 생성")
-    void test_parse_success() {
+    @DisplayName("쉼표를 기준으로 문자열을 분리한다.")
+    void test_parse_input() {
         String input = "pobi,james";
 
-        List<Player> players = PlayerParser.parse(input);
-
-        assertThat(players.size()).isEqualTo(2);
-        assertThat(players)
-                .extracting("name")
-                .contains("pobi", "james");
-    }
-
-    @Test
-    @DisplayName("플레이어의 이름에 영어와 한글이 아닌 문자가 존재하는 경우 예외 발생")
-    void test_parse_fail_when_invalid_name() {
-        String invalidInput = "pobi,james*";
-
-        assertThatThrownBy(() -> PlayerParser.parse(invalidInput))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"", "    ", "  ,  ,  "})
-    @DisplayName("플레이어의 이름이 공백인 경우 예외 발생")
-    void test_parse_fail_when_name_is_null(String emptyName) {
-        assertThatThrownBy(() -> PlayerParser.parse(emptyName))
-                .isInstanceOf(IllegalArgumentException.class);
+        List<String> result = InputParser.parse(input);
+        assertThat(result).containsExactly("pobi", "james");
     }
 }
