07faa8ec55d23a837f5f8f69c7a4344ff3fe1ef8
나의찬 <cab07036@naver.com>
2026-03-09 23:06:49 +0900

    refactor(Card): 카드 생성 로직 수정
    카드 생성시 현재 재귀 호출로 인해 52장의 모든 카드가 생성되었을 시 StackOverFlow 오류가 날 가능성이 있습니다.
    
    이를 해결하기 위해서 재귀 호출 대신에 (반복문+최대 횟수 제한) 으로 로직을 수정하였습니다.

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -2,7 +2,8 @@ package constant;
 
 public enum ErrorMessage {
 
-    INVALID_INPUT_PATTERN("입력 형식이 잘못됐습니다.");
+    INVALID_INPUT_PATTERN("입력 형식이 잘못됐습니다."),
+    EXCEEDED_MAX_TRY("최대 횟수를 초과 하였습니다.");
 
     private String message;
 
