8472af59e3f10e390cbf01f68d0d58211b33e1e9
armd482 <armd482@naver.com>
2026-03-09 16:53:57 +0900

    test: 승패 로직 테스트 코드 수정

--- src/test/java/service/TestBlackJackService.java
+++ src/test/java/service/TestBlackJackService.java
@@ -5,17 +5,35 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import java.util.List;
 import java.util.Map;
 import model.BlackJackDeck;
+import model.CardNumber;
 import model.Dealer;
 import model.MatchStatus;
 import model.Player;
-import model.dto.PlayerName;
+import model.PlayerName;
 import model.Players;
+import model.Shape;
+import model.dto.Card;
 import model.dto.ParticipantWinning;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class TestBlackJackService {
+    private BlackJackService blackJackService;
+
+    private Player player;
+    private Dealer dealer;
+
+
+    @BeforeEach
+    public void setUp() {
+        BlackJackDeck cards = new BlackJackDeck();
+        blackJackService = new BlackJackService(cards);
+
+        player = new Player(new PlayerName("player"));
+        dealer = new Dealer();
+    }
 
     @Test
     public void 카드_뽑기_정상_작동() {
@@ -30,54 +48,108 @@ public class TestBlackJackService {
         assertThat(player.getResult().deck().size()).isEqualTo(1);
     }
 
-    @ParameterizedTest
-    @CsvSource({"21, false", "22, true"})
-    public void 버스트_판정_정상_작동(Integer score, boolean result) {
-        BlackJackService blackJackService = new BlackJackService(new BlackJackDeck());
+    @Test
+    public void 둘_다_버스트가_아니면서_점수가_높은_경우_정상_작동() {
+        dealer.addCard(new Card(Shape.CLOVER, CardNumber.KING));
+        dealer.addCard(new Card(Shape.CLOVER, CardNumber.EIGHT));
 
-        Player player = new Player(new PlayerName("player"));
-        player.addScore(score);
+        player.addCard(new Card(Shape.CLOVER, CardNumber.KING));
 
-        assertThat(blackJackService.isBust(player)).isEqualTo(result);
+        ParticipantWinning winning = blackJackService.getGameResult(new Players(List.of(player)), dealer);
+
+        Map<MatchStatus, Integer> dealerWinning = winning.dealerWinning().getDealerWinning();
+
+        assertThat(dealerWinning.get(MatchStatus.WIN)).isEqualTo(1);
+        assertThat(dealerWinning.get(MatchStatus.DRAW)).isEqualTo(0);
+        assertThat(dealerWinning.get(MatchStatus.LOSE)).isEqualTo(0);
     }
 
+    @Test
+    public void 둘_다_버스트가_아니면서_점수가_같은_경우_정상_작동() {
+        dealer.addCard(new Card(Shape.CLOVER, CardNumber.KING));
+        dealer.addCard(new Card(Shape.DIAMOND, CardNumber.KING));
 
-    @ParameterizedTest
-    @CsvSource({"22,24,무","10,10,무","22,1,승","10,11,승","1,22,패","21,10,패"})
-    public void 블랙잭_참가자_딜러_승_패_판정_정상_작동(Integer dealerScore, Integer playerScore, String result) {
-        BlackJackDeck cards = new BlackJackDeck();
-        BlackJackService blackJackService = new BlackJackService(cards);
+        player.addCard(new Card(Shape.CLOVER, CardNumber.KING));
+        player.addCard(new Card(Shape.DIAMOND, CardNumber.KING));
 
-        Player player = new Player(new PlayerName("player1"));
-        Players players = new Players(List.of(player));
+        ParticipantWinning winning = blackJackService.getGameResult(new Players(List.of(player)), dealer);
 
-        Dealer dealer = new Dealer();
-
-        dealer.addScore(dealerScore);
-        player.addScore(playerScore);
-
-        ParticipantWinning gameResult = blackJackService.getGameResult(players, dealer);
-        Map<MatchStatus, Integer> dealerWinning = gameResult.dealerWinning().getDealerWinning();
-
-        assertThat(gameResult.playersWinning().getPlayersWinnings().getFirst().matchStatus().getStatus()).isEqualTo(result);
-
-        if(result.equals("승")) {
-            assertThat(dealerWinning.get(MatchStatus.WIN)).isEqualTo(0);
-            assertThat(dealerWinning.get(MatchStatus.LOSE)).isEqualTo(1);
-            assertThat(dealerWinning.get(MatchStatus.DRAW)).isEqualTo(0);
-            return;
-        }
-
-        if(result.equals("패")) {
-            assertThat(dealerWinning.get(MatchStatus.WIN)).isEqualTo(1);
-            assertThat(dealerWinning.get(MatchStatus.LOSE)).isEqualTo(0);
-            assertThat(dealerWinning.get(MatchStatus.DRAW)).isEqualTo(0);
-            return;
-        }
+        Map<MatchStatus, Integer> dealerWinning = winning.dealerWinning().getDealerWinning();
 
         assertThat(dealerWinning.get(MatchStatus.WIN)).isEqualTo(0);
-        assertThat(dealerWinning.get(MatchStatus.LOSE)).isEqualTo(0);
         assertThat(dealerWinning.get(MatchStatus.DRAW)).isEqualTo(1);
+        assertThat(dealerWinning.get(MatchStatus.LOSE)).isEqualTo(0);
     }
 
+    @Test
+    public void 둘_다_버스트가_아니면서_점수가_낮은_경우_정상_작동() {
+        dealer.addCard(new Card(Shape.CLOVER, CardNumber.KING));
+        dealer.addCard(new Card(Shape.CLOVER, CardNumber.EIGHT));
+
+        player.addCard(new Card(Shape.CLOVER, CardNumber.KING));
+        player.addCard(new Card(Shape.DIAMOND, CardNumber.KING));
+
+        ParticipantWinning winning = blackJackService.getGameResult(new Players(List.of(player)), dealer);
+
+        Map<MatchStatus, Integer> dealerWinning = winning.dealerWinning().getDealerWinning();
+
+        assertThat(dealerWinning.get(MatchStatus.WIN)).isEqualTo(0);
+        assertThat(dealerWinning.get(MatchStatus.DRAW)).isEqualTo(0);
+        assertThat(dealerWinning.get(MatchStatus.LOSE)).isEqualTo(1);
+    }
+
+    @Test
+    public void 플레이어만_버스트인_경우_정상_작동() {
+        dealer.addCard(new Card(Shape.CLOVER, CardNumber.KING));
+        dealer.addCard(new Card(Shape.CLOVER, CardNumber.EIGHT));
+
+        player.addCard(new Card(Shape.CLOVER, CardNumber.KING));
+        player.addCard(new Card(Shape.DIAMOND, CardNumber.KING));
+        player.addCard(new Card(Shape.HEART, CardNumber.KING));
+
+        ParticipantWinning winning = blackJackService.getGameResult(new Players(List.of(player)), dealer);
+
+        Map<MatchStatus, Integer> dealerWinning = winning.dealerWinning().getDealerWinning();
+
+        assertThat(dealerWinning.get(MatchStatus.WIN)).isEqualTo(1);
+        assertThat(dealerWinning.get(MatchStatus.DRAW)).isEqualTo(0);
+        assertThat(dealerWinning.get(MatchStatus.LOSE)).isEqualTo(0);
+    }
+
+    @Test
+    public void 딜러만_버스트인_경우_정상_작동() {
+        dealer.addCard(new Card(Shape.CLOVER, CardNumber.KING));
+        dealer.addCard(new Card(Shape.DIAMOND, CardNumber.KING));
+        dealer.addCard(new Card(Shape.HEART, CardNumber.KING));
+
+        player.addCard(new Card(Shape.CLOVER, CardNumber.KING));
+        player.addCard(new Card(Shape.CLOVER, CardNumber.EIGHT));
+
+        ParticipantWinning winning = blackJackService.getGameResult(new Players(List.of(player)), dealer);
+
+        Map<MatchStatus, Integer> dealerWinning = winning.dealerWinning().getDealerWinning();
+
+        assertThat(dealerWinning.get(MatchStatus.WIN)).isEqualTo(0);
+        assertThat(dealerWinning.get(MatchStatus.DRAW)).isEqualTo(0);
+        assertThat(dealerWinning.get(MatchStatus.LOSE)).isEqualTo(1);
+    }
+
+    @Test
+    public void 둘_다_버스트인_경우_정상_작동() {
+        dealer.addCard(new Card(Shape.CLOVER, CardNumber.KING));
+        dealer.addCard(new Card(Shape.DIAMOND, CardNumber.KING));
+        dealer.addCard(new Card(Shape.HEART, CardNumber.KING));
+
+        player.addCard(new Card(Shape.CLOVER, CardNumber.KING));
+        player.addCard(new Card(Shape.DIAMOND, CardNumber.KING));
+        player.addCard(new Card(Shape.HEART, CardNumber.KING));
+
+        ParticipantWinning winning = blackJackService.getGameResult(new Players(List.of(player)), dealer);
+
+        Map<MatchStatus, Integer> dealerWinning = winning.dealerWinning().getDealerWinning();
+
+        assertThat(dealerWinning.get(MatchStatus.WIN)).isEqualTo(0);
+        assertThat(dealerWinning.get(MatchStatus.DRAW)).isEqualTo(1);
+        assertThat(dealerWinning.get(MatchStatus.LOSE)).isEqualTo(0);
+    }
 }
