d63af69031bfc95c1a581eb993f79f8b5e413134
Jihyun3478 <leejihyun3478@gmail.com>
2026-03-09 23:20:43 +0900

    refactor: Card 클래스 필드 final 키워드 추가

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,9 +1,13 @@
 package controller;
 
-import domain.*;
-
-import java.util.*;
-
+import domain.Dealer;
+import domain.Deck;
+import domain.GameResult;
+import domain.Player;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import util.InputParser;
 import view.InputView;
 import view.OutputView;
