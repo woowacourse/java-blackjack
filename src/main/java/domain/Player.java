9a3b57a5637d68405263f29fddef7eae659888ea
Yoonhojoon <dbsghwns1209@gmail.com>
2026-03-09 13:23:49 +0900

    feat: Player 기능 구현

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -4,11 +4,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 public abstract class Player {
+
+    private static final int HIT_SCORE = 16;
+    private static final int BLACKJACK_SCORE = 21;
+
     private final List<Card> hand = new ArrayList<>();
+    private int score = 0;
 
     public void receiveInitCard(List<Card> cards) {
         hand.addAll(cards);
+    }
+
+    public int getScore() {
+        return score;
     }
 
     public void receiveCard(Card card) {
@@ -19,4 +29,47 @@ public abstract class Player {
         return Collections.unmodifiableList(hand);
     }
 
+    public void calculateScore() {
+        int score = 0;
+        int aceCount = 0;
+        for (Card card: hand) {
+            if (!card.isAce()) {
+                score += card.getValue();
+                continue;
+            }
+            aceCount++;
+        }
+        for(int i = 0; i< aceCount; i++){
+            score += calculateOptimalAceScore(score);
+        }
+        this.score = score;
+    }
+
+    public int calculateOptimalAceScore(int sum) {
+        if (sum > 10) {
+            return 1;
+        }
+        return 11;
+    }
+
+    public boolean isBurst() {
+        if(score > BLACKJACK_SCORE) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isHit() {
+        if (score <= HIT_SCORE) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isBlackjack() {
+        if(score == BLACKJACK_SCORE) {
+            return true;
+        }
+        return false;
+    }
 }
