e23d63947f03ea952fe222703a642747ecf545d1
SooHyeon Lee <dalsu4594@naver.com>
2026-03-09 14:42:46 +0900

    test: TrumpCard 예외 처리 테스트 수정
    - 예외 메시지 수정 후 테스트 예외 수정
    - 예외값이 맞지 않는 문제 발견 후 수정

--- src/test/java/blackjack/domain/TrumpCardTest.java
+++ src/test/java/blackjack/domain/TrumpCardTest.java
@@ -5,27 +5,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 class TrumpCardTest {
 
     @Test
     void 랭크가_null_이면_예외_발생한다() {
         Assertions.assertThatThrownBy(() -> TrumpCard.of(Suit.of("하트"), null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Rank와 Suit 중 하나라도 null이 올 수 없습니다.");
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("카드의 수트와 랭크는 null일 수 없습니다.");
     }
 
     @Test
     void 수트가_null_이면_예외_발생한다() {
         Assertions.assertThatThrownBy(() -> TrumpCard.of(null, Rank.of("2")))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Rank와 Suit 중 하나라도 null이 올 수 없습니다.");
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("카드의 수트와 랭크는 null일 수 없습니다.");
     }
 
     @Test
     void 랭크와_수트_둘_다_null_이면_예외_발생한다() {
         Assertions.assertThatThrownBy(() -> TrumpCard.of(null, null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Rank와 Suit 중 하나라도 null이 올 수 없습니다.");
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("카드의 수트와 랭크는 null일 수 없습니다.");
     }
 
     @Test
