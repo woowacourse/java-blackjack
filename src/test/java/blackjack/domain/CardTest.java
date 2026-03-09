fa2b55336d87650c7692c56ef4f950abc0bcd386
jyt6640 <jyt6640@gmail.com>
2026-03-09 15:05:05 +0900

    refactor: 카드 생성 테스트 수정

--- src/test/java/blackjack/domain/CardTest.java
+++ src/test/java/blackjack/domain/CardTest.java
@@ -1,20 +1,25 @@
 package blackjack.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CardTest {
 
     @Test
-    void 카드_생성_테스트() {
+    @DisplayName("카드가 올바르게 생성하는지에 대한 테스트")
+    void createCard() {
         // given
         CardValue value = CardValue.ACE;
         CardShape cardShape = CardShape.DIAMOND;
 
-        // when & then
-        assertThatCode(() -> new Card(value, cardShape))
-                .doesNotThrowAnyException();
+        // when
+        Card card = new Card(value, cardShape);
+
+        // then
+        assertThat("A다이아몬드").isEqualTo(card.getName());
     }
 
 }
