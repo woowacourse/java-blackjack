93831afa8a3341bbb26eb8f209012223e1bf9d9a
khcho96 <khcho1492@gmail.com>
2026-03-09 22:35:10 +0900

    refactor(Player): Participant 상속

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,54 +1,15 @@
 package domain;
 
-import constant.PolicyConstant;
-import exception.ErrorMessage;
+public class Player extends Participant {
 
-public class Player {
-
-    protected final String name;
-    protected final Hand hand;
+    private final PlayerName playerName;
 
     public Player(String name) {
-        validate(name);
-        this.name = name;
-        this.hand = new Hand();
-    }
-
-    private void validate(String name) {
-        validateNotBlank(name);
-        validatePlayerNameLength(name);
-    }
-
-    private void validatePlayerNameLength(String name) {
-        if (!(PolicyConstant.PLAYER_NAME_MIN_LENGTH <= name.length()
-            && name.length() <= PolicyConstant.PLAYER_NAME_MAX_LENGTH)) {
-            throw new IllegalArgumentException(ErrorMessage.PLAYER_NAME_LENGTH_OUT_OF_RANGE.getMessage());
-        }
-    }
-
-    private void validateNotBlank(String name) {
-        if (name.isBlank()) {
-            throw new IllegalArgumentException(ErrorMessage.PLAYER_NAME_BLANK.getMessage());
-        }
+        super();
+        this.playerName = new PlayerName(name);
     }
 
     public String getName() {
-        return name;
-    }
-
-    public Hand getHand() {
-        return hand;
-    }
-
-    public void addCard(Card card) {
-        hand.addCard(card);
-    }
-
-    public boolean isBust() {
-        return hand.calculateScore() > PolicyConstant.BLACKJACK_SCORE;
-    }
-
-    public int calculateScore() {
-        return hand.calculateScore();
+        return playerName.name();
     }
 }
