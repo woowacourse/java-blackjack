88eef41f4ab68ddbd9f2f224f22ac0b1598be6c9
alstj2384 <ass91160@naver.com>
2026-03-09 13:25:38 +0900

    refactor(Constant): 상수에 final 키워드를 추가하여 변수 변경 방지

--- src/main/java/domain/Constant.java
+++ src/main/java/domain/Constant.java
@@ -1,12 +1,12 @@
 package domain;
 
 public class Constant {
-    public static int BLACKJACK_MAX_NUMBER = 21;
-    public static int DEFAULT_HAND_NUMBER = 2;
-    public static int ACE_MAX_VALUE = 11;
-    public static int ACE_MIN_VALUE = 1;
-    public static int DEALER_HIT_STAND_BOUNDARY = 16;
-    public static int NAME_MIN_LENGTH = 1;
-    public static int NAME_MAX_LENGTH = 10;
-    public static String DELIMITER = ", ";
+    public static final int BLACKJACK_MAX_NUMBER = 21;
+    public static final int DEFAULT_HAND_NUMBER = 2;
+    public static final int ACE_MAX_VALUE = 11;
+    public static final int ACE_MIN_VALUE = 1;
+    public static final int DEALER_HIT_STAND_BOUNDARY = 16;
+    public static final int NAME_MIN_LENGTH = 1;
+    public static final int NAME_MAX_LENGTH = 10;
+    public static final String DELIMITER = ", ";
 }
