46b7cf7af6af4aeb2640564fd1bfe0cbd5a5eb32
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-09 22:23:15 +0900

    refactor: InputView Method 인스턴스로 변환

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
 public class InputView {
     private static Scanner sc = new Scanner(System.in);
 
-    public static NameRequestDto askGamblerNames() {
+    public NameRequestDto askGamblerNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         return new NameRequestDto(sc.nextLine());
     }
 
-    public static AgreementRequestDto askHitOrStand(String name) {
+    public AgreementRequestDto askHitOrStand(String name) {
         System.out.println(name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
         return new AgreementRequestDto(sc.nextLine());
     }
