b7c0a52bb7cf373fe4c451d91c6d2de8ba175298
나의찬 <cab07036@naver.com>
2026-03-09 13:00:23 +0900

    refactor(Person): Person 인터페이스에서 추상 클래스로 수정
    기존에 인터페이스로 추상화 되어있던 설계를 추상 클래스로 변경하였습니다.
    1. 추상클래스로 변경하여 불필요한 코드 중복을 제거하였습니다.
    2. 버스트인지 생존인지 검사를 Deck에 책임을 위임하였습니다.

--- src/main/java/domain/model/Dealer.java
+++ src/main/java/domain/model/Dealer.java
@@ -1,50 +1,11 @@
 package domain.model;
 
-public class Dealer implements Person {
-
-    private Deck deck;
-    private int finalSum = 0;
-
+public class Dealer extends Person {
     private Dealer(Deck deck) {
-        this.deck = deck;
+        super(deck);
     }
 
     public static Dealer of(Deck deck) {
         return new Dealer(deck);
-    }
-
-    public Deck getDeck() {
-        return deck;
-    }
-
-    public int getFinalSum() {
-        return finalSum;
-    }
-
-    public void calculateFinalSum() {
-        finalSum = deck.calculateFinalSum();
-    }
-
-    public boolean isBurst() {
-        return deck.getDeckStatus() == DeckStatus.BURST;
-    }
-
-    public boolean isAlive() {
-        return deck.getDeckStatus() == DeckStatus.ALIVE;
-    }
-
-    @Override
-    public int getDeckSum() {
-        return deck.getSum();
-    }
-
-    @Override
-    public int getDeckSize() {
-        return deck.getSize();
-    }
-
-    @Override
-    public void appendCard(Card card) {
-        deck.append(card);
     }
 }
