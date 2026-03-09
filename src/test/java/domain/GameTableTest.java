c70bfe790d554ad936b853399e237f818a9d8288
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-10 02:41:26 +0900

    test(GameTable): Member 추상화에 따른 draw 메서드 수정

--- src/test/java/domain/GameTableTest.java
+++ src/test/java/domain/GameTableTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import domain.card.Card;
+import domain.table.GameTable;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -9,20 +10,18 @@ import org.junit.jupiter.api.Test;
 class GameTableTest {
 
     private final GameTable gameTable;
+    private final String playerName = "포비";
 
     public GameTableTest() {
-        this.gameTable = new GameTable();
+        this.gameTable = new GameTable(List.of(playerName));
     }
 
     @DisplayName("카드의 총합이 21보다 크면 CurrentResult의 isBust는 true이다.")
     @Test
     void checkCurrentTest_playerHasCardSumOf22_returnTrue() {
-        String playerName = "포비";
-        gameTable.joinPlayer(List.of(playerName));
-
-        gameTable.drawByName(playerName, new Card("10","클로버"));
-        gameTable.drawByName(playerName, new Card("5", "하트"));
-        gameTable.drawByName(playerName, new Card("7", "하트"));
+        gameTable.draw(playerName, new Card("10","클로버"));
+        gameTable.draw(playerName, new Card("5", "하트"));
+        gameTable.draw(playerName, new Card("7", "하트"));
 
         Assertions.assertTrue(gameTable.checkBust("포비"));
     }
@@ -30,12 +29,9 @@ class GameTableTest {
     @DisplayName("카드의 총합이 21보다 작으면 CurrentResult의 isBust는 false이다.")
     @Test
     void checkCurrentTest_playerHasCardSumOf20_returnFalse() {
-        String playerName = "포비";
-        gameTable.joinPlayer(List.of(playerName));
-
-        gameTable.drawByName(playerName, new Card("10","클로버"));
-        gameTable.drawByName(playerName, new Card("5", "하트"));
-        gameTable.drawByName(playerName, new Card("5", "스페이드"));
+        gameTable.draw(playerName, new Card("10","클로버"));
+        gameTable.draw(playerName, new Card("5", "하트"));
+        gameTable.draw(playerName, new Card("5", "스페이드"));
 
         Assertions.assertFalse(gameTable.checkBust("포비"));
     }
