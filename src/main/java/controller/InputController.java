480629791172bee719445c2f1403f7f39b8be811
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-09 14:26:41 +0900

    refactor(input) : MVC 패턴의 각 역할에 맞게 코드 구조 수정

--- src/main/java/controller/InputController.java
+++ src/main/java/controller/InputController.java
@@ -1,41 +1,26 @@
 package controller;
 
-import constant.ErrorMessage;
-import java.util.Arrays;
+import dto.PlayerName;
 import java.util.List;
-import java.util.Scanner;
 import model.Agreement;
 import model.Player;
-import dto.PlayerName;
 import model.Players;
 import view.InputView;
 
 public class InputController {
-    private static final String NAME_SPLIT_REGEX = ",";
-
-    private final Scanner scanner = new Scanner(System.in);
 
     public Players getPlayersName() {
-        InputView.printNameRequest();
-        String nameInput = getInput();
-        List<Player> players = Arrays.stream(nameInput.split(NAME_SPLIT_REGEX))
-                .map((name) -> new Player(new PlayerName(name)))
+        List<String> stringPlayers = InputView.getNameRequest();
+        List<Player> players = stringPlayers.stream()
+                .map(player -> new Player(new PlayerName(player)))
                 .toList();
 
         return new Players(players);
     }
 
     public boolean getCondition(String name) {
-        InputView.printDrawRequest(name);
-        return new Agreement(getInput()).get();
+        String input = InputView.getDrawRequest(name);
+        return new Agreement(input).get();
     }
 
-    private String getInput() {
-        String input = scanner.nextLine();
-
-        if(input.isBlank()) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_IS_BLANK.getMessage());
-        }
-        return input;
-    }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,14 +1,35 @@
 package view;
 
+import constant.ErrorMessage;
+import java.util.List;
+import java.util.Scanner;
+
 public class InputView {
     private final static String NAME_REQUEST_TEXT = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private final static String DRAW_REQUEST_TEXT = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니요는 n)";
+    private static final String NAME_SPLIT_REGEX = ",";
 
-    public static void printNameRequest() {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static List<String> getNameRequest() {
         System.out.println(NAME_REQUEST_TEXT);
+        String nameInput = getInput();
+
+        return List.of(nameInput.split(NAME_SPLIT_REGEX));
     }
 
-    public static void printDrawRequest(String name) {
+    public static String getDrawRequest(String name) {
         System.out.println(name + DRAW_REQUEST_TEXT);
+
+        return getInput();
+    }
+
+    private static String getInput() {
+        String input = scanner.nextLine();
+
+        if(input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_IS_BLANK.getMessage());
+        }
+        return input;
     }
 }
