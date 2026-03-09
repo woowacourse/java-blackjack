216067358bd3ee3928c156de4a54b7f77b7d8757
poketopa <lhs5427ll@gmail.com>
2026-03-09 23:06:09 +0900

    refactor(domain): DuplicationSet 삭제 및 Outcome 클래스 enum 이름 변경

--- src/main/java/domain/Outcome.java
+++ src/main/java/domain/Outcome.java
@@ -1,5 +1,15 @@
 package domain;
 
 public enum Outcome {
-    승, 패, 무
+    WIN("승"), LOSE("패"), DRAW("무");
+
+    private final String result;
+
+    Outcome(final String result) {
+        this.result = result;
+    }
+
+    public String result(){
+        return result;
+    }
 }
