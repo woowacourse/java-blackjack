0ada731074cccc5dbdda1c3a4f0431cc4a30a14b
koreaioi <koreaioi@gachon.ac.kr>
2026-03-09 15:36:41 +0900

    feat: ErrorMessage Prefix 추가

--- src/main/java/domain/common/exception/BlackjackGameException.java
+++ src/main/java/domain/common/exception/BlackjackGameException.java
@@ -1,7 +1,11 @@
-package domain.common.exception;
+    package domain.common.exception;
 
-public class BlackjackGameException extends IllegalArgumentException {
-    public BlackjackGameException(ExceptionInformation exceptionInformation) {
-        super(exceptionInformation.getErrorMessage());
+    public class BlackjackGameException extends IllegalArgumentException {
+
+        private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
+
+        public BlackjackGameException(ExceptionInformation exceptionInformation) {
+            super(ERROR_MESSAGE_PREFIX + exceptionInformation.getErrorMessage());
+        }
+
     }
-}
