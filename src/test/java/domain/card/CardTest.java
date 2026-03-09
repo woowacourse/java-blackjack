8ca6ef2517570c45ed6c55f6f0b304018935ff2e
choubung <choubung@sookmyung.ac.kr>
2026-03-09 17:30:41 +0900

    Test(Card): 카드 정보 출력용 문자열로 변환 테스트 작성

---
+++ src/test/java/domain/card/CardTest.java
@@ -0,0 +1,26 @@
+package domain.card;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CardTest {
+    @DisplayName("카드_정보_한국어로_변환_테스트")
+    @ParameterizedTest
+    @MethodSource("provideCardData")
+    void complexTest(Card card, String korName) {
+        assertThat(card.cardToKorName()).isEqualTo(korName);
+    }
+
+    static Stream<Arguments> provideCardData() {
+        return Stream.of(
+                Arguments.of(new Card(TrumpSuit.HEART, TrumpNumber.ACE), "A하트"),
+                Arguments.of(new Card(TrumpSuit.DIAMOND, TrumpNumber.SEVEN), "7다이아몬드"),
+                Arguments.of(new Card(TrumpSuit.SPADE, TrumpNumber.JACK), "J스페이드")
+        );
+    }
+}
