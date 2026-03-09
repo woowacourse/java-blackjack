e76bec30a29d456a2daf1ed74f2cac1ba80c4f93
choubung <choubung@sookmyung.ac.kr>
2026-03-10 00:04:51 +0900

    Feat(Dealer): 딜러 승패 전적 계산 로직 구현

--- src/main/java/domain/participant/Dealer.java
+++ src/main/java/domain/participant/Dealer.java
@@ -1,19 +1,53 @@
-package domain;
+package domain.participant;
 
 import domain.vo.Name;
 
-public class Dealer extends Participant{
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Dealer extends Participant {
+    private Map<WinStatus, Integer> record = new HashMap() {{
+        put(WinStatus.WIN, 0);
+        put(WinStatus.DRAW, 0);
+        put(WinStatus.LOSS, 0);
+    }};
+
     private static final int DEALER_HIT_CONDITION = 17;
 
-    public Dealer() {
+    public Dealer(HandCards handCards) {
+        super(handCards);
         this.name = new Name("딜러");
     }
 
     public boolean isDealerHit(){
-        if (getMyScore() < DEALER_HIT_CONDITION) {
+        if (getScore() < DEALER_HIT_CONDITION) {
             return true;
         }
 
         return false;
+    }
+
+    public List<String> getCards() {
+        return handCards.cardsToString();
+    }
+
+    public Map<WinStatus, Integer> getRecord() {
+        return record;
+    }
+
+    @Override
+    public void finalizeResult(int playerScore) {
+        if (this.isBust() || this.getScore() < playerScore) {
+            record.put(WinStatus.LOSS, record.get(WinStatus.LOSS) + 1);
+        }
+
+        if (this.getScore() > playerScore) {
+            record.put(WinStatus.WIN, record.get(WinStatus.WIN) + 1);
+        }
+
+        if (this.getScore() == playerScore) {
+            record.put(WinStatus.DRAW, record.get(WinStatus.DRAW) + 1);
+        }
     }
 }
