0ce3b44fbafa655e34174a27d7d2b257696308a5
kangrae <kangrae0501@gmail.com>
2026-03-09 16:52:57 +0900

    feat: 플레이어끼리 중복된 이름이 없는지 검사하는 기능 구현

--- src/main/java/blackjack/domain/Players.java
+++ src/main/java/blackjack/domain/Players.java
@@ -8,6 +8,7 @@ public class Players {
 
     private Players(List<String> playersName) {
         validateNumberOfPlayers(playersName);
+        validateUniqueName(playersName);
         this.players = playersName.stream()
                 .map(Player::new)
                 .toList();
@@ -43,6 +44,15 @@ public class Players {
     private void validateNumberOfPlayers(List<String> playersName) {
         if (playersName.size() > 5) {
             throw new IllegalArgumentException("게임에 참가하는 플레이어는 5인 이하입니다. (딜러 제외)");
+        }
+    }
+
+    private void validateUniqueName(List<String> playersName) {
+        if (playersName.stream().distinct().count() != playersName.size()) {
+            throw new IllegalArgumentException("게임에 참가하는 플레이어끼리는 이름에 중복이 없어야합니다.");
+        }
+        if (playersName.stream().anyMatch("딜러"::equals)) {
+            throw new IllegalArgumentException("플레이어 이름은 딜러일 수 없습니다.");
         }
     }
 
