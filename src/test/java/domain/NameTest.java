62d5490cf32f1039d05dff858b76504812ef157d
xxbeann <yjb4945@gmail.com>
2026-03-09 17:46:59 +0900

    refactor: 에러메시지와 예외가 일치하는지 확인하는 로직 추가
    객체 생성시 에러가 터질 때, 정확한 예외 문구와 일치하는지 확인하는 로직을 추가했습니다.

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -11,35 +11,41 @@ class NameTest {
     @DisplayName("이름은 비어있거나 공백일 수 없다.")
     void validateBlank_InputBlank_ThrowsException() {
         String name = " ";
-        assertThrows(IllegalArgumentException.class, () -> {
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             new Name(name);
         });
+        assertEquals("이름은 비어있거나 공백일 수 없습니다.", exception.getMessage());
     }
 
     @Test
     @DisplayName("이름은 영어만 허용된다.")
     void validateEnglish_InputKorean_ThrowsException() {
         String name = "한글";
-        assertThrows(IllegalArgumentException.class, () -> {
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             new Name(name);
         });
+        assertEquals("이름은 영어만 허용됩니다.", exception.getMessage());
     }
 
     @Test
     @DisplayName("이름은 2글자 이상이어야한다.")
     void validateBlank_InputOneLength_ThrowsException() {
-        String name = "1";
-        assertThrows(IllegalArgumentException.class, () -> {
+        String name = "a";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             new Name(name);
         });
+        assertEquals("이름은 2~5글자만 허용됩니다.", exception.getMessage());
     }
 
     @Test
     @DisplayName("이름은 5글자 이하여야한다.")
     void validateBlank_InputSixLength_ThrowsException() {
-        String name = "123456";
-        assertThrows(IllegalArgumentException.class, () -> {
+        String name = "abcdefg";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             new Name(name);
         });
+        assertEquals("이름은 2~5글자만 허용됩니다.", exception.getMessage());
     }
 }
