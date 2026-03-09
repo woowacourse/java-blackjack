9be662ba30c90267287d83d4ce8ec27790bd79ba
toctoce <toctoce@naver.com>
2026-03-09 18:44:04 +0900

    refactor(Hand):ˆ 필드를 final화

--- src/main/java/domain/Hand.java
+++ src/main/java/domain/Hand.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hand {
-    private List<Card> cards;
+    private final List<Card> cards;
 
     public Hand() {
         cards = new ArrayList<>();
