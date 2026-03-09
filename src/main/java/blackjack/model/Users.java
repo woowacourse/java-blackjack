b85d895e1c0b2813fd167df7d05e16317ead377a
Chocoding1 <galmeagi2@naver.com>
2026-03-09 17:19:49 +0900

    refactor: GameResultCalculator 클래스 삭세 & Users 클래스로 역할 위임

--- src/main/java/blackjack/model/Users.java
+++ src/main/java/blackjack/model/Users.java
@@ -27,4 +27,71 @@ public class Users {
         users.add(dealer);
         return users;
     }
+
+    public void determineWinner() {
+        for (Player player : players) {
+            if (calculateWhenBlackjack(player, dealer)) {
+                continue;
+            }
+
+            if (calculateWhenBust(player, dealer)) {
+                continue;
+            }
+
+            calculateWhenNormal(player, dealer);
+        }
+    }
+
+    private boolean calculateWhenBlackjack(Player player, Dealer dealer) {
+        if (player.isBlackjack() && dealer.isBlackjack()) {
+            dealer.addResult(GameResult.DRAW);
+            return true;
+        }
+
+        if (player.isBlackjack()) {
+            player.mark(GameResult.WIN);
+            dealer.addResult(GameResult.LOSE);
+            return true;
+        }
+
+        if (dealer.isBlackjack()) {
+            player.mark(GameResult.LOSE);
+            dealer.addResult(GameResult.WIN);
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean calculateWhenBust(Player player, Dealer dealer) {
+        if (player.isBust()) {
+            dealer.addResult(GameResult.WIN);
+            player.mark(GameResult.LOSE);
+            return true;
+        }
+
+        if (dealer.isBust()) {
+            dealer.addResult(GameResult.LOSE);
+            player.mark(GameResult.WIN);
+            return true;
+        }
+
+        return false;
+    }
+
+    private static void calculateWhenNormal(Player player, Dealer dealer) {
+        if (player.totalScore() > dealer.totalScore()) {
+            dealer.addResult(GameResult.LOSE);
+            player.mark(GameResult.WIN);
+            return;
+        }
+
+        if (player.totalScore() < dealer.totalScore()) {
+            dealer.addResult(GameResult.WIN);
+            player.mark(GameResult.LOSE);
+            return;
+        }
+
+        dealer.addResult(GameResult.DRAW);
+    }
 }
