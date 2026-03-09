926e477669c21575f8a38d76d1691031dc27ec04
koreaioi <koreaioi@gachon.ac.kr>
2026-03-09 13:51:04 +0900

    feat: Dealer에 PlayerName 인스턴스 변수를 추가

--- src/main/java/domain/dealer/Dealer.java
+++ src/main/java/domain/dealer/Dealer.java
@@ -4,6 +4,7 @@ import domain.card.Card;
 import domain.card.CardBundle;
 import domain.card.CardDeck;
 import domain.player.Player;
+import domain.player.PlayerName;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -11,21 +12,27 @@ import java.util.stream.Stream;
 public class Dealer {
 
     private static final String CANNOT_HAND_OUT_CARDS_ERROR = "딜러가 카드를 나눠줄 수 없습니다.";
-    private static final String DEALER_NAME = "딜러"; // NOTE Player와 추상화를 위해서는 수정 필요
+    private static final String DEFAULT_DEALER_NAME = "딜러"; // NOTE Player와 추상화를 위해서는 수정 필요
     private static final int ADDITIONAL_DRAW_CONDITION = 16;
     private static final int INITIAL_DEAL_COUNT = 2;
     private static final int HIT_COUNT = 1;
 
+    private final PlayerName name;
     private final CardDeck cardDeck;
     private CardBundle cardBundle;
 
-    private Dealer(CardDeck cardDeck) {
+    private Dealer(PlayerName name, CardDeck cardDeck) {
+        this.name = name;
         this.cardBundle = CardBundle.empty();
         this.cardDeck = cardDeck;
     }
 
+    public static Dealer from(PlayerName name, CardDeck cardDeck) {
+        return new Dealer(name, cardDeck);
+    }
+
     public static Dealer from(CardDeck cardDeck) {
-        return new Dealer(cardDeck);
+        return new Dealer(PlayerName.from(DEFAULT_DEALER_NAME), cardDeck);
     }
 
     public CardBundle dealCardToPlayer(Player player) {
@@ -82,7 +89,7 @@ public class Dealer {
     }
 
     public String toDisplayMyName() {
-        return DEALER_NAME;
+        return name.name();
     }
 
     public List<String> disPlayMyCardBundle() {
