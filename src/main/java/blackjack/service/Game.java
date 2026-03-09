bd289268a1a122e1b4e84fe4d36e434213d32f8b
haeseon lee <haeseon4816@gmail.com>
2026-03-09 15:40:05 +0900

    fix: 잘못된 버스트 승패판정 룰 변경

--- src/main/java/blackjack/service/Game.java
+++ src/main/java/blackjack/service/Game.java
@@ -4,6 +4,7 @@ import blackjack.domain.Dealer;
 import blackjack.domain.GameResult;
 import blackjack.domain.Player;
 import blackjack.domain.ScoreCompareResult;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -55,24 +56,18 @@ public class Game {
         boolean isDealerBust = dealer.isBust();
 
         if (isPlayerBust || isDealerBust) {
-            return compareScoreWhenBust(isPlayerBust, isDealerBust);
+            return compareScoreWhenBust(isPlayerBust);
         }
 
         return compareScoreWhenNotBust(player.calculateTotalScore(), dealer.calculateTotalScore());
 
     }
 
-    private ScoreCompareResult compareScoreWhenBust(boolean isPlayerBust, boolean isDealerBust) {
-        if (isPlayerBust && isDealerBust) {
-            return ScoreCompareResult.PUSH;
-        }
+    private ScoreCompareResult compareScoreWhenBust(boolean isPlayerBust) {
         if (isPlayerBust) {
             return ScoreCompareResult.DEALER_WIN;
         }
-        if (isDealerBust) {
-            return ScoreCompareResult.PLAYER_WIN;
-        }
-        throw new IllegalArgumentException("버스트인 사람이 1명은 포함되어야 합니다.");
+        return ScoreCompareResult.PLAYER_WIN;
     }
 
     private ScoreCompareResult compareScoreWhenNotBust(int playerTotalScore, int dealerTotalScore) {
