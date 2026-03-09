64a8d085e7657d7ea58956e79967476507ac8171
Chocoding1 <galmeagi2@naver.com>
2026-03-09 15:42:39 +0900

    refactor: 클래스명 변경 & CardCalculator 책임 위임

--- src/main/java/blackjack/model/Hand.java
+++ src/main/java/blackjack/model/Hand.java
@@ -1,21 +1,71 @@
 package blackjack.model;
 
+import static blackjack.model.Constant.ACE_SCORE_ELEVEN;
+import static blackjack.model.Constant.ACE_SCORE_ONE;
+import static blackjack.model.Constant.INIT_CARDS_END_IDX;
+import static blackjack.model.Constant.INIT_CARDS_START_IDX;
+import static blackjack.model.Constant.BLACKJACK_SCORE;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class CardStatus {
+public class Hand {
 
     private final List<Card> cards;
 
-    public CardStatus() {
+    public Hand() {
         this.cards = new ArrayList<>();
     }
 
     public List<Card> getCards() {
-        return cards;
+        return List.copyOf(cards);
     }
 
     public void addCard(Card card) {
         cards.add(card);
+    }
+
+    public boolean isBlackjack() {
+        List<Card> initCards = cards.subList(INIT_CARDS_START_IDX, INIT_CARDS_END_IDX);
+        return totalScore(initCards) == BLACKJACK_SCORE;
+    }
+
+    public boolean isBust() {
+        return totalScore(cards) > BLACKJACK_SCORE;
+    }
+
+    public boolean isHitAvailable() {
+        return totalScore(cards) >= BLACKJACK_SCORE;
+    }
+
+    public int calculateTotalScore() {
+        return totalScore(cards);
+    }
+
+    public int totalScore(List<Card> cards) {
+        Map<Boolean, List<Card>> partitioned = cards.stream()
+                .collect(Collectors.partitioningBy(card -> card.getRank() == Rank.ACE));
+
+        List<Card> aceCards = partitioned.get(true);      // Ace인 카드들
+        List<Card> nonAceCards = partitioned.get(false);// Ace가 아닌 카드들
+        int totalScoreNonAce = nonAceCards.stream()
+                .mapToInt(Card::score)
+                .sum();
+
+        return addAceScore(aceCards, totalScoreNonAce);
+    }
+
+    int addAceScore(List<Card> aceCards, int totalScore) {
+        for (int i = 0; i < aceCards.size(); i++) {
+            if (totalScore + ACE_SCORE_ELEVEN > BLACKJACK_SCORE) {
+                totalScore += ACE_SCORE_ONE;
+                continue;
+            }
+            totalScore += ACE_SCORE_ELEVEN;
+        }
+
+        return totalScore;
     }
 }
