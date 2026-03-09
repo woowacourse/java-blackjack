cb7a4e84d6b617bfd7d02f1c2a45cd485d9deb58
wontop02 <wjy0502kr@naver.com>
2026-03-09 13:40:42 +0900

    refactor(controller): game 진행 로직 service로 이동
    game을 진행하는 로직을 service로 이동했습니다.

--- src/main/java/controller/BlackjackController.java
+++ src/main/java/controller/BlackjackController.java
@@ -1,84 +1,72 @@
 package controller;
 
 import domain.Game;
-import domain.card.CardGenerator;
 import domain.card.Deck;
 import domain.enums.Result;
-import domain.participant.Dealer;
 import domain.participant.Players;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import service.BlackjackService;
 import util.InputParser;
 import view.InputView;
 import view.OutputView;
 
 public class BlackjackController {
-    private final InputView inputView;
-    private final OutputView outputView;
+    private final BlackjackService blackjackService;
 
-    public BlackjackController(InputView inputView, OutputView outputView) {
-        this.inputView = inputView;
-        this.outputView = outputView;
+    public BlackjackController(BlackjackService blackjackService) {
+        this.blackjackService = blackjackService;
     }
 
     public void start() {
-        Players players = makePlayers();
-        Deck deck = new Deck(CardGenerator.generateCards());
-        Game game = new Game(players, new Dealer());
+        Players players = blackjackService.makePlayers(askPlayerNames());
+        Deck deck = blackjackService.makeDeck();
+        Game game = blackjackService.makeGame(players);
 
-        game.initializeGame(deck);
-        outputView.printPlayers(game.getDealerCard(), game.getAllPlayerCard());
+        blackjackService.initializeGame(game, deck);
+        OutputView.printPlayers(game.getDealerCard(), game.getAllPlayersCards());
 
-        List<String> playersName = players.getAllPlayersName();
-        playTurn(game, playersName, deck);
+        List<String> playersName = players.getAllPlayerNames();
+        playTurn(game, deck);
         printResult(game, playersName);
     }
 
-    private Players makePlayers() {
-        String input = inputView.askPlayerNames();
-        List<String> names = InputParser.parseNames(input);
-        return new Players(names);
+    private List<String> askPlayerNames() {
+        String input = InputView.askPlayerNames();
+        return InputParser.parseNames(input);
     }
 
-    private void playTurn(Game game, List<String> playersName, Deck deck) {
-        for (String name : playersName) {
+    private void playTurn(Game game, Deck deck) {
+        for (String name : game.getAllPlayerNames()) {
             playPlayerTurn(game, name, deck);
         }
-        playDealerTurn(game, deck);
+        blackjackService.playDealerTurn(game, deck);
     }
-
 
     private void playPlayerTurn(Game game, String name, Deck deck) {
         boolean shouldContinue = true;
         while (shouldContinue && !game.isPlayerBust(name)) {
             shouldContinue = isPlayerWantHit(name);
-            game.playerHit(name, deck, shouldContinue);
-            outputView.printPlayerCard(name, game.getPlayerCard(name));
+            blackjackService.playPlayerTurn(game, name, deck, shouldContinue);
+            OutputView.printPlayerCard(name, game.getPlayerCards(name));
         }
     }
 
     private boolean isPlayerWantHit(String name) {
-        String input = inputView.askPlayerHit(name);
+        String input = InputView.askPlayerHit(name);
         return InputParser.parseHitAnswer(input);
     }
 
-    private void playDealerTurn(Game game, Deck deck) {
-        while (!game.isDealerBust()) {
-            game.dealerHit(deck);
-            outputView.printDealerHit();
-        }
-    }
-
     private void printResult(Game game, List<String> playerNames) {
-        outputView.printDealerCardWithScore(game.getDealerCard(), game.getDealerScore());
+        OutputView.printDealerCardWithScore(game.getDealerCard(), game.getDealerScore());
 
         Map<String, Result> playerResults = new LinkedHashMap<>();
         for (String name : playerNames) {
-            outputView.printPlayerCardWithScore(name, game.getPlayerCard(name), game.getPlayerScore(name));
+            OutputView.printPlayerCardWithScore(name, game.getPlayerCards(name), game.getPlayerScore(name));
             playerResults.put(name, game.getPlayerResult(name));
         }
 
-        outputView.printGameResult(game.getDealerResult(), playerResults);
+        OutputView.printGameResult(game.getDealerResult(), playerResults);
     }
 }
