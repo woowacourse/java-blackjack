ed0162475b19476f11c48a356e2cfb4be02babfc
2Jaeheon <jaeheon0826@jbnu.ac.kr>
2026-03-09 20:20:50 +0900

    test(dealer): 딜러가 카드를 뽑을 수 있는지 경곗값 테스트
    Signed-off-by: 2Jaeheon <jaeheon0826@jbnu.ac.kr>

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -14,20 +14,40 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DealerTest {
-    Participant dealer;
-
-    @BeforeEach
-    void setUp() {
-        List<Card> testCards = new ArrayList<>();
-        testCards.add(new Card(Rank.EIGHT, Suit.HEART));
-        testCards.add(new Card(Rank.EIGHT, Suit.CLOVER));
-        dealer = new Dealer();
-    }
-
     @Test
     void 딜러는_카드의_합이_16_이하인_경우_카드를_한_장_받는다(){
+        Dealer dealer = createDealer(Rank.JACK, Rank.TWO);
+
         boolean canDraw = dealer.canDraw();
 
         assertThat(canDraw).isTrue();
+    }
+
+    @Test
+    void 딜러는_카드의_합이_16을_초과하는_경우_카드를_받을_수_없다() {
+        Dealer dealer = createDealer(Rank.TEN, Rank.JACK, Rank.QUEEN);
+
+        boolean receivable = dealer.canDraw();
+
+        assertThat(receivable).isFalse();
+    }
+
+    @Test
+    void 딜러는_카드의_합이_16인_경우_카드를_한_장_받는다() {
+        Dealer dealer = createDealer(Rank.TEN, Rank.SIX);
+
+        boolean receivable = dealer.canDraw();
+
+        assertThat(receivable).isTrue();
+    }
+
+    private Dealer createDealer(Rank... ranks) {
+        Dealer dealer = new Dealer();
+        for (Rank rank : ranks) {
+            Card card = new Card(rank, Suit.CLOVER);
+            dealer.receive(card);
+        }
+
+        return dealer;
     }
 }
