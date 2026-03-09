8854a69521a142ef6ad27b3bf3dd476cf11db522
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-03-09 13:15:44 +0900

    refactor: 구분자 상수처리

--- src/main/java/util/Parser.java
+++ src/main/java/util/Parser.java
@@ -5,9 +5,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Parser {
-    public static List<String> separateBySeparator(String input) {
+    private static final String SEPARATOR = ",";
 
-        return Arrays.stream(input.split(","))
+    public static List<String> separateBySeparator(String input) {
+        return Arrays.stream(input.split(SEPARATOR))
                 .map(String::trim)
                 .collect(Collectors.toList());
     }
