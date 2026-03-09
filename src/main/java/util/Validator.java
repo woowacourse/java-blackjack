fe883c9c58c9a74eb48cd6ecf2b3ea80328c0bcb
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-03-09 13:16:09 +0900

    fix: 입력값에 대한 조건식 논리 오류 수정 및 메서드네이밍 변경

--- src/main/java/util/Validator.java
+++ src/main/java/util/Validator.java
@@ -6,15 +6,19 @@ public class Validator {
     private static final String HIT_COMMAND = "y";
     private static final String STAND_COMMAND = "n";
 
-    public static void validateInput(String input) {
-        if (input != null && input.isBlank()) {
-            throw new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE);
-        }
+    public static void validateInputName(String input) {
+        validateInputIsNotNullOrBlank(input);
     }
 
     public static void validateChoiceInput(String input) {
-        validateInput(input);
+        validateInputIsNotNullOrBlank(input);
         isNotYAndN(input);
+    }
+
+    private static void validateInputIsNotNullOrBlank(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE);
+        }
     }
 
     private static void isNotYAndN(String input) {
@@ -22,5 +26,4 @@ public class Validator {
             throw new IllegalArgumentException(NOT_Y_AND_N_ERROR_MESSAGE);
         }
     }
-
 }
