7e5694eaed2e91472f867b0e70b3200386c17fb6
smini <sm020320@naver.com>
2026-03-10 20:26:28 +0900

    feat: 카드 비어있음 관련 에러 메시지 추가

--- src/main/java/blackjack/exception/ErrorMessage.java
+++ src/main/java/blackjack/exception/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     DUPLICATED("이름은 중복될 수 없습니다."),
     OUT_OF_NAME_LENGTH("이름 길이는 최소 2자 이상, 5자 이하여야 합니다."),
     INVALID_PLAYERS("플레이어는 2명 이상 5명 이하여야 합니다."),
-    INVALID_INPUT("유효하지 않은 입력입니다.");
+    INVALID_INPUT("유효하지 않은 입력입니다."),
+    CARD_EMPTY("카드가 비어있습니다.");
 
     private final String message;
 
