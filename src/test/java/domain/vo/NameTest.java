328c6cfd35416ba78e1f7ac94f199c06384d1bf2
ChoSeongHyeon <00seong116@naver.com>
2026-03-11 02:23:04 +0900

    test(NameTest): 예외 메시지도 검증 추가

--- src/test/java/domain/vo/NameTest.java
+++ src/test/java/domain/vo/NameTest.java
@@ -1,7 +1,6 @@
 package domain.vo;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -16,18 +15,11 @@ class NameTest {
         assertThat(name.getName()).isEqualTo(input);
     }
 
-//    @Test
-//    void 이름이_딜러일때_예외 (){
-//        String input = "딜러";
-//
-//        Assertions.assertThatThrownBy(() -> new Name(input))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
-
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "!", "1000j"})
     void 이름에_영어_한글_외_입력시_예외 (String input) {
         Assertions.assertThatThrownBy(() -> new Name(input))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 플레이어 이름은 영어와 한글만 가능합니다.");
     }
 }
