e33f2fe14b4a34e803346ba6e433726382b2434c
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-10 22:17:42 +0900

    test: Dealer 객체의 카드 값 합과 드로우 임계치 비교 테스트

--- src/test/java/domain/player/DealerTest.java
+++ src/test/java/domain/player/DealerTest.java
@@ -7,15 +7,44 @@ import org.junit.jupiter.api.Test;
 
 class DealerTest {
     @Test
-    @DisplayName("딜러 카드 값의 합이 드로우 임계치 이하인지 true 반환하는지 테스트")
-    void 딜러_카드_합_드로우_임계치_이하_테스트() {
+    @DisplayName("딜러 카드 값의 합이 드로우 임계치 미만일 때 true를 정상적으로 반환함을 테스트")
+    void 딜러_카드_합_드로우_임계치_미만_true_반환_테스트() {
         //given
         Dealer dealer = new Dealer("딜러");
 
         //when
-        dealer.addCard(new Card("A", "하트"));
+        dealer.addCard(new Card("9", "하트"));
 
         //then
         Assertions.assertThat(dealer.isBelowDrawThreshold()).isEqualTo(true);
     }
+
+    @Test
+    @DisplayName("딜러 카드 값의 합이 드로우 임계치와 일치할 때 true를 정상적으로 반환함을 테스트")
+    void 딜러_카드_합_드로우_임계치_일치_true_반환_테스트() {
+        //given
+        Dealer dealer = new Dealer("딜러");
+
+        //when
+        dealer.addCard(new Card("K", "하트"));
+        dealer.addCard(new Card("6", "하트"));
+
+        //then
+        Assertions.assertThat(dealer.isBelowDrawThreshold()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("딜러 카드 값의 합이 드로우 임계치 초과일 때 false를 정상적으로 반환함을 테스트")
+    void 딜러_카드_합_드로우_임계치_초과_false_반환_테스트() {
+        //given
+        Dealer dealer = new Dealer("딜러");
+
+        //when
+        dealer.addCard(new Card("K", "하트"));
+        dealer.addCard(new Card("J", "다이아몬드"));
+
+        //then
+        Assertions.assertThat(dealer.isBelowDrawThreshold()).isEqualTo(false);
+    }
+
 }
