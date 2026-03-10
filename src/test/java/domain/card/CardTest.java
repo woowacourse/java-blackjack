9e28bcf2e1e1fa0c8a28e12c12283c187d639216
JunHyung1206 <pjh120610@naver.com>
2026-03-10 09:36:05 +0900

    test: 카드에 대한 테스트 변수명 수정

--- src/test/java/domain/card/CardTest.java
+++ src/test/java/domain/card/CardTest.java
@@ -4,32 +4,32 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CardTest {
+class CardTest {
     @Test
     @DisplayName("카드에 적힌 숫자가 2~10이면 숫자를 그대로 반환한다.")
     void cardNumberTest() {
         Card card = new Card(CardNumber.TWO, CardShape.CLUB);
-        int number = card.getBaseScore();
+        int baseScore = card.getBaseScore();
 
-        Assertions.assertThat(number).isEqualTo(2);
+        Assertions.assertThat(baseScore).isEqualTo(2);
     }
 
     @Test
     @DisplayName("카드에 적힌 숫자가 J,Q,K이면 10을 반환된다.")
     void cardAlphabetTest() {
         Card card = new Card(CardNumber.JACK, CardShape.CLUB);
-        int number = card.getBaseScore();
+        int baseScore = card.getBaseScore();
 
-        Assertions.assertThat(number).isEqualTo(10);
+        Assertions.assertThat(baseScore).isEqualTo(10);
     }
 
     @Test
     @DisplayName("카드에 적힌 숫자가 A이면 1로 반환한다.")
     void cardAceTest() {
         Card card = new Card(CardNumber.ACE, CardShape.CLUB);
-        int number = card.getBaseScore();
+        int baseScore = card.getBaseScore();
 
-        Assertions.assertThat(number).isEqualTo(1);
+        Assertions.assertThat(baseScore).isEqualTo(1);
     }
 
 }
