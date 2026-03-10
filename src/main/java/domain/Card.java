ffc97a4db053206bc12f054557ef90f3c824fb73
김가현 <hyeonnni@naver.com>
2026-03-10 23:49:22 +0900

    refactor(domain): Card 객체 내 점수 계산 및 이름 반환 로직 추가

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -1,25 +1,37 @@
 package domain;
 
-import java.util.Random;
-
 public class Card {
-    private static final int CARD_BOUND = 52;
     private final int card;
 
-    public Card() {
-        this.card = drawCard();
+    public Card(int card) {
+        this.card = card;
     }
 
-    private int drawCard() {
-        Random random = new Random();
-        return random.nextInt(CARD_BOUND);
+    public String getCardName() {
+        int shape = card / 13;
+        int number = card % 13;
+        return CardSuitMap.getSuit(shape) + CardSuitMap.getRank(number);
+    }
+
+    public int getScore() {
+        int cardNumber = card % 13;
+
+        if (1 <= cardNumber && cardNumber <= 9) {
+            return cardNumber + 1;
+        }
+
+        if (cardNumber >= 10) {
+            return 10;
+        }
+
+        return 11;
+    }
+
+    public boolean isAce() {
+        return card % 13 == 0;
     }
 
     public int getCard() {
         return card;
-    }
-
-    public boolean checkAce() {
-        return card % 13 == 0;
     }
 }
