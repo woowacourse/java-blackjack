8cda7032303f92e365c3ef97da144023b99c020b
haeyoon1 <hynam1220@naver.com>
2026-03-10 14:48:51 +0900

    feat: 카드 shuffle 전략 인터페이스 생성

---
+++ src/main/java/domain/NoShuffleStrategy.java
@@ -0,0 +1,9 @@
+package domain;
+
+import java.util.List;
+
+public class NoShuffleStrategy implements CardShuffleStrategy{
+    @Override
+    public void shuffle(List<Card> cards) {
+    }
+}
---
+++ src/main/java/domain/RandomCardStrategy.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RandomCardStrategy implements CardShuffleStrategy{
+
+    @Override
+    public void shuffle(List<Card> cards) {
+        Collections.shuffle(cards);
+    }
+}
