23c338c396c5cabcc83297e041655484bee5b4a4
gleaming9 <gleaming9@khu.ac.kr>
2026-03-10 21:46:29 +0900

    fix(Deck): Scanner의 접근제어자를 private으로 수정 및 final 키워드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
     private static final String START_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String YN_FORMAT = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
 
-    static Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
     public static List<String> askPlayerNames() {
         System.out.println(START_MESSAGE);
