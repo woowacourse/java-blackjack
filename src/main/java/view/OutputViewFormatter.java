f2d8b40ee817a358e69bea7f2685d21ecbe20ec0
minzzun99 <skg06123@naver.com>
2026-03-10 15:57:14 +0900

    refactor: DTO의 출력 포멧 변환 로직을 OutputViewFormatter 클래스로 분리

---
+++ src/main/java/view/OutputViewFormatter.java
@@ -0,0 +1,32 @@
+package view;
+
+import dto.DealerFinalResultDto;
+import dto.PlayerDto;
+import dto.ResultDto;
+import java.util.List;
+
+public class OutputViewFormatter {
+    public static String formatDealerResult(DealerFinalResultDto dealerFinalResultDto) {
+        if (dealerFinalResultDto.drawCount() > 0) {
+            return String.format("딜러: %d승 %d무 %d패%n", dealerFinalResultDto.winCount(), dealerFinalResultDto.drawCount(),
+                    dealerFinalResultDto.loseCount());
+        }
+        return String.format("딜러: %d승 %d패%n", dealerFinalResultDto.winCount(), dealerFinalResultDto.loseCount());
+    }
+
+    public static String formatDealerCardStatus(ResultDto resultDto) {
+        return String.format("딜러카드: %s%n", resultDto.cards().getFirst());
+    }
+
+    public static String formatPlayerCardStatus(PlayerDto playerDto) {
+        return String.format("%s카드: %s%n", playerDto.name(), getCardStatusFormat(playerDto.resultDto().cards()));
+    }
+
+    public static String getCardStatusFormat(List<String> cards) {
+        return String.join(", ", cards);
+    }
+
+    public static String formatCardResult(String name, ResultDto resultDto) {
+        return String.format("%n%s카드: %s - 결과: %d", name, resultDto.cards(), resultDto.score());
+    }
+}
