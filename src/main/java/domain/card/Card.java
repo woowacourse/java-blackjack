907c9d869d9ba4ac9200aa12c4b8dba7be1fc12f
frombunny <rucy011121@gmail.com>
2026-03-10 23:22:59 +0900

    feat(domain): Card 클래스를 레코드로 선언

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -1,14 +1,9 @@
 package domain.card;
 
-public class Card {
-    private final Rank rank;
-    private final Suit suit;
-
-    public Card(Rank rank, Suit suit) {
-        this.rank = rank;
-        this.suit = suit;
-    }
-
+public record Card(
+        Rank rank,
+        Suit suit
+) {
     public boolean isAce() {
         return rank.equals(Rank.ACE);
     }
