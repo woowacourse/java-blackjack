e8f43837e1643ba2e116987dd189b1df7c0c0949
minzzun99 <skg06123@naver.com>
2026-03-10 15:57:14 +0900

    refactor: DTO의 출력 포멧 변환 로직을 OutputViewFormatter 클래스로 분리

--- src/main/java/dto/DealerFinalResultDto.java
+++ src/main/java/dto/DealerFinalResultDto.java
@@ -4,12 +4,14 @@ import domain.ResultType;
 import domain.TotalFinalResult;
 
 public record DealerFinalResultDto(
-        String result
+        int winCount,
+        int drawCount,
+        int loseCount
 ) {
     public static DealerFinalResultDto from(TotalFinalResult totalFinalResult) {
-        return new DealerFinalResultDto(checkDrawCount(countResult(totalFinalResult, ResultType.LOSE),
+        return new DealerFinalResultDto(countResult(totalFinalResult, ResultType.LOSE),
                 countResult(totalFinalResult, ResultType.DRAW),
-                countResult(totalFinalResult, ResultType.WIN)));
+                countResult(totalFinalResult, ResultType.WIN));
     }
 
     private static int countResult(TotalFinalResult totalFinalResult, ResultType resultType) {
@@ -18,10 +20,5 @@ public record DealerFinalResultDto(
                 .count();
     }
 
-    private static String checkDrawCount(int winCount, int drawCount, int loseCount) {
-        if (drawCount > 0) {
-            return String.format("딜러: %d승 %d무 %d패%n", winCount, drawCount, loseCount);
-        }
-        return String.format("딜러: %d승 %d패%n", winCount, loseCount);
-    }
+
 }
--- src/main/java/view/OutputViewFormatter.java
+++ src/main/java/view/OutputViewFormatter.java
@@ -1,13 +1,12 @@
 package view;
 
 import dto.DealerFinalResultDto;
-import dto.FinalResultDto;
 import dto.PlayerDto;
 import dto.ResultDto;
 import java.util.List;
 
 public class OutputViewFormatter {
-    public String formatDealerResult(DealerFinalResultDto dealerFinalResultDto) {
+    public static String formatDealerResult(DealerFinalResultDto dealerFinalResultDto) {
         if (dealerFinalResultDto.drawCount() > 0) {
             return String.format("딜러: %d승 %d무 %d패%n", dealerFinalResultDto.winCount(), dealerFinalResultDto.drawCount(),
                     dealerFinalResultDto.loseCount());
@@ -15,23 +14,19 @@ public class OutputViewFormatter {
         return String.format("딜러: %d승 %d패%n", dealerFinalResultDto.winCount(), dealerFinalResultDto.loseCount());
     }
 
-    public String formatDealerCardStatus(ResultDto resultDto) {
+    public static String formatDealerCardStatus(ResultDto resultDto) {
         return String.format("딜러카드: %s%n", resultDto.cards().getFirst());
     }
 
-    public String formatPlayerCardStatus(PlayerDto playerDto) {
+    public static String formatPlayerCardStatus(PlayerDto playerDto) {
         return String.format("%s카드: %s%n", playerDto.name(), getCardStatusFormat(playerDto.resultDto().cards()));
     }
 
-    public String getCardStatusFormat(List<String> cards) {
+    public static String getCardStatusFormat(List<String> cards) {
         return String.join(", ", cards);
     }
 
-    public String formatCardResult(String name, ResultDto resultDto) {
-        return String.format("%n%s카드: %s - 결과: %d", name, getCardStatusFormat(resultDto.cards()), resultDto.score());
-    }
-
-    public String formatTotalResult(FinalResultDto finalResultDto) {
-        return String.format("%s: %s%n", finalResultDto.name(), finalResultDto.result());
+    public static String formatCardResult(String name, ResultDto resultDto) {
+        return String.format("%n%s카드: %s - 결과: %d", name, resultDto.cards(), resultDto.score());
     }
 }
