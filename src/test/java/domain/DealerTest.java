fc953cad904e952dad71a04b645258d6571bb27c
khcho96 <khcho1492@gmail.com>
2026-03-10 17:04:20 +0900

    test(domain): DealerTest 갱신

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -2,9 +2,9 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import constant.PolicyConstant;
 import constant.Rank;
 import constant.Suit;
+import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -20,17 +20,17 @@ class DealerTest {
             void 딜러의_첫번째_카드만_담긴_손패를_반환해야_한다() {
 
                 // given
-                Dealer dealer = new Dealer(PolicyConstant.DEALER_NAME);
+                Dealer dealer = new Dealer();
                 Card firstCard = new Card(Rank.TEN, Suit.HEART);
                 Card secondCard = new Card(Rank.ACE, Suit.SPADE);
                 dealer.addCard(firstCard);
                 dealer.addCard(secondCard);
 
                 // when
-                Hand actual = dealer.getOnlyFirstHand();
+                List<String> actual = dealer.getOnlyFirstHand();
 
                 // then
-                assertThat(actual.getCardNames())
+                assertThat(actual)
                     .hasSize(1)
                     .containsExactly(firstCard.getName());
             }
