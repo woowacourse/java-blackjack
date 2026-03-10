08ce10982b602cc20b524377c23526d9a364f1f0
Chocoding1 <galmeagi2@naver.com>
2026-03-10 14:33:34 +0900

    refactor: GameResult 필드 제거

--- src/main/java/blackjack/model/Player.java
+++ src/main/java/blackjack/model/Player.java
@@ -2,19 +2,8 @@ package blackjack.model;
 
 
 public class Player extends User {
-    private GameResult gameResult;
-
     public Player(String name) {
         super(name);
-        this.gameResult = GameResult.DRAW;
-    }
-
-    public GameResult getGameResult() {
-        return gameResult;
-    }
-
-    public void mark(GameResult gameResult) {
-        this.gameResult = gameResult;
     }
 
     @Override
