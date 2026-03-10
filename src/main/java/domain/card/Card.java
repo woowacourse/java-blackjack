d41d5f409ad51b5cd0df5685149ffff215c78a96
ChoSeongHyeon <00seong116@naver.com>
2026-03-11 00:33:12 +0900

    refactor(Card): final 키워드 추가

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -1,22 +1,20 @@
 package domain.card;
 
 public class Card {
-    private TrumpSuit suit;
-    private TrumpNumber number;
+    private final TrumpSuit suit;
+    private final TrumpNumber number;
 
     public Card(TrumpSuit suit, TrumpNumber number) {
         this.suit = suit;
         this.number = number;
     }
 
-    // 카드 value 반환
     public int getValue(){
         return number.getValue();
     }
 
-    // ACE인지 아닌지 반환
     public boolean isAce(){
-        return number==TrumpNumber.ACE;
+        return number == TrumpNumber.ACE;
     }
 
     @Override
