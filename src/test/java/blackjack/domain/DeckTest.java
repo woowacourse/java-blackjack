5ec236f1a5f8368a0b0afc9813cb52345ce5d6e6
softmoca <acyc0909@gmail.com>
2026-03-10 16:11:38 +0900

    test : Deck 생성시 랜던한 shuffle에 대한 상태검증 테스트 추가

--- src/test/java/blackjack/domain/DeckTest.java
+++ src/test/java/blackjack/domain/DeckTest.java
@@ -1,17 +1,34 @@
 package blackjack.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class DeckTest {
-    
+
     @Test
     void 덱은_총52장의_카드를_가진다() {
         Deck deck = new Deck();
         assertThat(deck.getCount()).isEqualTo(52);
         assertThat(deck.getCount()).isNotEqualTo(51);
         assertThat(deck.getCount()).isNotEqualTo(53);
+    }
+
+    @Test
+    void 덱에는_중복없이_52장의_카드가_있다() {
+        Deck deck = new Deck();
+        List<Card> cards = new ArrayList<>();
+
+        for (int i = 0; i < 52; i++) {
+            cards.add(deck.draw());
+        }
+
+        assertEquals(52, cards.size());
+        assertEquals(52, new HashSet<>(cards).size());
     }
 
 }
