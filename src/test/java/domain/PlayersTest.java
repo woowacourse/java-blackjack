1caf04553cde8bcadb96e88c22d3cf22fb4bb982
kdongsu5509 <kod66170@gmail.com>
2026-03-10 23:05:05 +0900

    test : add duplication logic test code and change constructor of players at test

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,13 +1,16 @@
 package domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import common.ErrorMessage;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
-import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class PlayersTest {
@@ -20,37 +23,52 @@ class PlayersTest {
         totalDeck = Deck.createDeck(totalCardCreationStrategy);
     }
 
-    @Test
-    @DisplayName("생성 잘 한다")
-    void of_good() {
-        //given
-        List<String> testPlayerNames = List.of("pobi", "terry", "rati", "gump");
+    @Nested
+    class ofTest {
+        @Test
+        @DisplayName("생성 잘 한다")
+        void of_good() {
+            //given
+            List<String> testPlayerNames = List.of("pobi", "terry", "rati", "gump");
 
-        //when, then
-        assertDoesNotThrow(
-                () -> Players.of(testPlayerNames, totalDeck)
-        );
+            //when, then
+            assertDoesNotThrow(
+                    () -> Players.of(testPlayerNames, totalDeck)
+            );
+        }
+
+        @Test
+        @DisplayName("이름이 중복되면 오류가 발생한다")
+        void of_fail_duplication() {
+            //given
+            List<String> testPlayerNames = List.of("pobi", "pobi");
+
+            //when && then
+            assertThatThrownBy(() -> Players.of(testPlayerNames, totalDeck))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ErrorMessage.NAME_UNIQUENESS_ERR.getMessage());
+        }
     }
 
-    @Test
-    @DisplayName("getDecksPerUser에서 잘 가져온다")
-    void getDecksPerUser_success() {
-        //given
-        List<String> testPlayerNames = List.of("pobi");
-        List<Card> expectPobiCards = List.of(
-                new Card(CardShape.스페이드, CardContents.A),
-                new Card(CardShape.스페이드, CardContents.TWO)
-        );
-
-        //when
-        Players players = Players.of(testPlayerNames, totalDeck);
-        Map<String, List<Card>> result = players.getDecksPerPlayer();
-
-        //then
-        assertEquals(expectPobiCards, result.get("pobi"));
-    }
-
-    private List<Card> createSampleCards() {
+    //    @Test
+//    @DisplayName("getDecksPerUser에서 잘 가져온다")
+//    void getDecksPerUser_success() {
+//        //given
+//        List<String> testPlayerNames = List.of("pobi");
+//        List<Card> expectPobiCards = List.of(
+//                new Card(CardShape.스페이드, CardContents.A),
+//                new Card(CardShape.스페이드, CardContents.TWO)
+//        );
+//
+//        //when
+//        Players players = Players.of(testPlayerNames, totalDeck);
+//        Map<String, List<Card>> result = players.getDecksPerPlayer();
+//
+//        //then
+//        assertEquals(expectPobiCards, result.get("pobi"));
+//    }
+//
+    private Deque<Card> createSampleCards() {
         CardShape[] shapes = CardShape.values();
         CardContents[] contents = CardContents.values();
 
@@ -61,7 +79,7 @@ class PlayersTest {
             }
         }
 
-        return sampleCards;
+        return new ArrayDeque<>(sampleCards);
     }
 
 }
