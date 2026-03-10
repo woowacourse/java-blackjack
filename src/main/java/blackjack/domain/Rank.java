2ede5167c61821b5546e5a1c9d3ac02352022e77
nn98 <jkllhgb@gmail.com>
2026-03-10 17:32:11 +0900

    refacotr(enum): 커맨드 입력용 enum 제외 출력용 양식 제거

--- src/main/java/blackjack/domain/Rank.java
+++ src/main/java/blackjack/domain/Rank.java
@@ -1,34 +1,7 @@
 package blackjack.domain;
 
 public enum Rank {
-    ACE(1, "A"),
-    TWO(2, "2"),
-    THREE(3, "3"),
-    FOUR(4, "4"),
-    FIVE(5, "5"),
-    SIX(6, "6"),
-    SEVEN(7, "7"),
-    EIGHT(8, "8"),
-    NINE(9, "9"),
-    TEN(10, "10"),
-    JACK(10, "J"),
-    QUEEN(10, "Q"),
-    KING(10, "K"),
-    ;
-    
-    private final int score;
-    private final String name;
-    
-    Rank(int score, String name) {
-        this.score = score;
-        this.name = name;
-    }
-    
-    public int getScore() {
-        return score;
-    }
-    
-    public String getRankName() {
-        return name;
-    }
+    ACE, TWO, THREE, FOUR, FIVE,
+    SIX, SEVEN, EIGHT, NINE, TEN,
+    JACK, QUEEN, KING,
 }
--- src/main/java/blackjack/domain/Suit.java
+++ src/main/java/blackjack/domain/Suit.java
@@ -1,19 +1,5 @@
 package blackjack.domain;
 
 public enum Suit {
-    SPADE("스페이드"),
-    DIAMOND("다이아몬드"),
-    HEART("하트"),
-    CLOVER("클로버"),
-    ;
-    
-    private final String displayName;
-    
-    Suit(String displayName) {
-        this.displayName = displayName;
-    }
-    
-    public String getSuitDisplayName() {
-        return displayName;
-    }
+    SPADE, DIAMOND, HEART, CLOVER
 }
