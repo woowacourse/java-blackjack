1e513b70c3381492c6a62b912a677e0b21c4da0b
khcho96 <khcho1492@gmail.com>
2026-03-10 16:58:54 +0900

    refactor(util): Parser 분할 리스트 생성 로직 단순화

--- src/main/java/util/Parser.java
+++ src/main/java/util/Parser.java
@@ -1,13 +1,12 @@
 package util;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
 
     public static List<String> parseInput(String input, String delimiter) {
-        List<String> parsedInput = new ArrayList<>(Arrays.asList(input.split(delimiter, -1)));
+        List<String> parsedInput = new ArrayList<>(List.of(input.split(delimiter, -1)));
         return parsedInput.stream()
             .map(String::strip)
             .toList();
