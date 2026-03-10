a3ac0b04109f1ae7bff44cf2fef26fc9d9d5b8a1
jhk01007 <jhk001007@naver.com>
2026-03-10 16:07:35 +0900

    refactor: 에러 메시지에 해결 방법을 포함시키고, 부정형을 긍정형으로 변경

--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -1,11 +1,17 @@
 package exception;
 
+import constant.BlackjackConstant;
+
+import static constant.BlackjackConstant.*;
+
 public enum ErrorMessage {
-    PLAYER_NAME_LENGTH_ERROR("플레이어의 이름은 2글자 이상 10글자 이하여야 합니다."),
-    INPUT_EMPTY_ERROR("빈 값을 입력할 수 없습니다."),
+    PLAYER_NAME_LENGTH_ERROR(String.format(
+            "플레이어의 이름은 %s글자 이상 %s글자 이하여야 합니다.", NAME_MINIMUM_LENGTH, NAME_MAX_LENGTH)),
+    INPUT_EMPTY_ERROR("특정 값을 입력해주세요."),
     INVALID_HIT_STAND_INPUT_ERROR("y 또는 n만 입력 가능합니다."),
     DEALER_NOT_FOUND_ERROR("딜러가 존재하지 않습니다."),
-    PLAYER_COUNT_OUT_OF_RANGE("참여자 수 제한에 맞지 않습니다.")
+    PLAYER_COUNT_OUT_OF_RANGE(String.format(
+            "참여자수는 최소%s명 최대 %s명이어야 합니다.", MINIMUM_PLAYER_BOUND, MAXIMUM_PLAYER_BOUND))
     ;
 
     private final String message;
