b4e0f4f65dc506811558583752bd93507f0c1a6c
jhk01007 <jhk001007@naver.com>
2026-03-10 16:18:14 +0900

    test: 이름 검증 단위테스트 추가

---
+++ src/test/java/domain/NameTest.java
@@ -0,0 +1,33 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static exception.ErrorMessage.PLAYER_NAME_LENGTH_ERROR;
+import static org.assertj.core.api.Assertions.*;
+
+
+class NameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ab", "abcdefghij"})
+    @DisplayName("이름은 2글자 이상 10글자 이하여야 한다.")
+    public void name_success(String n) throws Exception {
+        // given // when // then
+        assertThatCode(() -> new Name(n))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "abcdefghijk"})
+    @DisplayName("이름은 2글자 미만이거나 10글자 초과면 예외가 발생한다.")
+    public void name_fail(String n) throws Exception {
+        // given // when // then
+        assertThatThrownBy(() -> new Name(n))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(PLAYER_NAME_LENGTH_ERROR.getMessage());
+    }
+
+}
