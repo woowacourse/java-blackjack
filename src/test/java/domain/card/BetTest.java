e1811a6576f8459eb55c2e828aa7bdae1017d952
Jinriro <riro12.hanyang.ac.kr>
2026-03-10 22:44:12 +0900

    feat: 배팅 금액을 관리하는 Bet 객체 추가

--- src/test/java/domain/card/BetTest.java
+++ src/test/java/domain/card/BetTest.java
@@ -1,2 +1,68 @@
-package domain.card;public class BetTest {
+package domain.card;
+
+import domain.MatchResult;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BetTest {
+
+    @Test
+    @DisplayName("배팅 금액을 저장한다.")
+    void setAmountTest() {
+        // given
+        int amount = 3000;
+
+        // when
+        Bet bet = new Bet(amount);
+
+        // then
+        Assertions.assertEquals(bet.getAmount(), amount);
+    }
+
+    @Test
+    @DisplayName("배팅 금액은 양수여야 한다.")
+    void setAmount_validateNonNegativeAmountTest() {
+        // given
+        int amount = -3000;
+
+
+        // when - then
+        assertThrows(IllegalArgumentException.class, () -> new Bet(amount));
+    }
+
+    @Test
+    @DisplayName("배팅 금액은 최대 배팅 금액을 초과할 수 없다.")
+    void setAmount_validateMaxBetAmountTest() {
+        // given
+        int overBetAmount = 1000_000_000;
+
+        // when - then
+        assertThrows(IllegalArgumentException.class, () -> new Bet(overBetAmount));
+    }
+
+    @ParameterizedTest
+    @DisplayName("배팅 금액을 승패 결과에 따라 계산한다.")
+    @CsvSource({
+            "LOSE, false, -3000",
+            "DRAW, false, 0",
+            "WIN, false, 3000",
+            "WIN, true, 4500"
+    })
+    void calculateProfit_test(MatchResult matchResult, boolean isBlackJack, int expectedProfit) {
+        // given
+        int amount = 3000;
+
+        Bet bet = new Bet(amount);
+
+        // when
+        int profit = bet.calculateProfit(matchResult, isBlackJack);
+
+        // then
+        Assertions.assertEquals(expectedProfit, profit);
+    }
 }
