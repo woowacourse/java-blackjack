e126837d3691aa1a2fc1448e73cc434b1f5137d4
poketopa <lhs5427ll@gmail.com>
2026-03-11 00:29:52 +0900

    feat(domain): Participant 추상 클래스 추가

---
+++ src/main/java/domain/Participant.java
@@ -0,0 +1,29 @@
+package domain;
+
+public abstract class Participant {
+    private final Hand hand;
+
+    protected Participant() {
+        this.hand = new Hand();
+    }
+
+    public void drawCard(Cards cards) {
+        hand.addCard(cards.draw());
+    }
+
+    public Hand getCardList() {
+        return hand;
+    }
+
+    public Score getScore() {
+        return hand.getScore();
+    }
+
+    public int getResult() {
+        return hand.getResult();
+    }
+
+    public boolean checkBust() {
+        return hand.checkBust();
+    }
+}
