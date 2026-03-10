027f49a6b4fe9de58b55a5a6b734f2dcc2306b04
smini <sm020320@naver.com>
2026-03-10 17:08:57 +0900

    feat: 배팅 금액 입력 기능 추가

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -21,4 +21,9 @@ public class InputView {
         System.out.println(player.getName() + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
         return sc.nextLine();
     }
+
+    public String getBettingAmount(Player player) {
+        System.out.println(player.getName() + "의 배팅 금액은?");
+        return sc.nextLine();
+    }
 }
