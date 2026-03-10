18519039645d1c53093d9cf046bccfac9f372ee0
Yoonhojoon <dbsghwns1209@gmail.com>
2026-03-10 16:38:03 +0900

    test: 베팅 금액 정산 테스트 코드 작성

--- src/test/java/strategy/BettingRuleTest.java
+++ src/test/java/strategy/BettingRuleTest.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
-class DefaultBettingRuleTest {
+class BettingRuleTest {
 
-    private DefaultBettingRule bettingRule;
+    private BettingRule bettingRule;
 
     @BeforeEach
     void setUp() {
