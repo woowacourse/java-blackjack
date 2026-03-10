7d93f37692216f196043b6203825faae2740e7e7
snow___shower <wonsb25@gmail.com>
2026-03-10 23:14:36 +0900

    test: Money 테스트 추가

---
+++ src/test/java/MoneyTest.java
@@ -0,0 +1,12 @@
+import domain.participant.Money;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+
+    @Test
+    void 배팅금이_양수가_아니면_오류가_발생한다() {
+        Assertions.assertThatThrownBy(()->new Money(-10))
+                .hasMessageContaining("[ERROR] 배팅금은 0보다 커야 합니다!");
+    }
+}
