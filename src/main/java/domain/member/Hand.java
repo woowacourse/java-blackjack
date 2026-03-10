2b4ace3b6f195849782d1ea6296e943e8c79d226
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-10 20:15:51 +0900

    refactor(Hand): 읽기전용으로 반환하여 방어적 복사 처리

--- src/main/java/domain/member/Hand.java
+++ src/main/java/domain/member/Hand.java
@@ -2,6 +2,7 @@ package domain.member;
 
 import domain.card.Card;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Hand {
@@ -16,7 +17,7 @@ public class Hand {
     }
 
     public List<Card> getCards() {
-        return cards;
+        return Collections.unmodifiableList(cards);
     }
 
     public void appendCard(Card card) {
