c7290e6d25a17229bc6d02876f2e3f35eb3a7246
EveryPine <kenton.lim@gmail.com>
2026-03-10 14:21:23 +0900

    feat(application): 앱 종료 직전 Scanner 종료 로직 추가

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -1,12 +1,15 @@
 package blackjack;
 
 import blackjack.controller.BlackjackController;
+import blackjack.utils.Console;
 
 public class Application {
 
     public static void main(String[] args) {
         final BlackjackController blackjackController = new BlackjackController();
         blackjackController.run();
+
+        Console.close();
     }
 
 }
