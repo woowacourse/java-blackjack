e6dbcd1491dd2f1e0216d9c2b271807f18be100c
김가현 <hyeonnni@naver.com>
2026-03-10 23:47:19 +0900

    feat(domain): 플레이어 기반 딜러 승패 역산 로직 구현

---
+++ src/main/java/domain/GameResult.java
@@ -0,0 +1,24 @@
+package domain;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class GameResult {
+    private final Map<Player, Outcome> playerResults;
+
+    public GameResult(final Map<Player, Outcome> playerResults) {
+        this.playerResults = playerResults;
+    }
+
+    public Map<Outcome, Integer> getDealerResult() {
+        Map<Outcome, Integer> dealerResult = new EnumMap<>(Outcome.class);
+        for (Outcome outcome : Outcome.values()) {
+            dealerResult.put(outcome, 0);
+        }
+        for (Outcome playerOutcome : playerResults.values()) {
+            Outcome dealerOutcome = playerOutcome.opposite();
+            dealerResult.put(dealerOutcome, dealerResult.get(dealerOutcome) + 1);
+        }
+        return dealerResult;
+    }
+}
