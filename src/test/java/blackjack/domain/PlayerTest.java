e45c17c11d20611116298f60cd5327cff641fbd8
Chaerin Lee <rin@Rin-MacBook-Air.local>
2026-03-10 18:21:44 +0900

    test(Player): 버스트일 경우 카드를 추가 불가 테스트 추가

--- src/test/java/blackjack/domain/PlayerTest.java
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -13,4 +13,14 @@ public class PlayerTest {
         assertThat(player.getCardCount()).isEqualTo(1);
     }
 
+    @Test
+    void 플레이어_버스트로_카드를_받을_수_없다() {
+        Player player = new Player("이산");
+        player.recieveCard(new Card(CardPoint.KING, CardPattern.DIAMOND));
+        player.recieveCard(new Card(CardPoint.EIGHT, CardPattern.CLUB));
+        player.recieveCard(new Card(CardPoint.FOUR, CardPattern.HEART));
+        player.recieveCard(new Card(CardPoint.THREE, CardPattern.SPADE));
+        assertThat(player.getCardCount()).isEqualTo(3);
+    }
+
 }
