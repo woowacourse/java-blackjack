8f61342ef7acd8406d173a476d7bc49a19c3c745
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-10 23:25:55 +0900

    feat(BettingAmount): 배팅 금액 클래스 생성

---
+++ src/main/java/domain/member/BettingAmount.java
@@ -0,0 +1,19 @@
+package domain.member;
+
+public class BettingAmount {
+
+    private static final double BONUS_RATE = 1.5;
+    private int amount;
+
+    public BettingAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void applyBonus() {
+        this.amount = (int) (amount * BONUS_RATE);
+    }
+}
