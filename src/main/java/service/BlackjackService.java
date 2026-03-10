efe4dfa6e12a336a4b5a70fa15fe695ad8267e1a
khcho96 <khcho1492@gmail.com>
2026-03-10 16:58:21 +0900

    refactor(service): participants 상태 기반으로 게임 흐름 관리하도록 리팩터링

--- src/main/java/service/BlackjackService.java
+++ src/main/java/service/BlackjackService.java
@@ -3,77 +3,66 @@ package service;
 import constant.HitOrStand;
 import constant.PolicyConstant;
 import constant.Result;
-import converter.BlackjackConverter;
 import domain.Card;
 import domain.CardMachine;
 import domain.Dealer;
+import domain.Participants;
 import domain.Player;
-import dto.BlackjackResultDto;
+import domain.Players;
+import dto.BlackjackStatisticsDto;
 import dto.DealerResultDto;
-import dto.HandDto;
+import dto.ParticipantDto;
 import dto.PlayerResultDto;
-import dto.PlayersDto;
 import exception.ErrorMessage;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 
 public class BlackjackService {
 
-    private final BlackjackConverter blackjackConverter;
     private final CardMachine cardMachine;
+    private Participants participants;
 
-    public BlackjackService(BlackjackConverter blackjackConverter) {
-        this.blackjackConverter = blackjackConverter;
+    public BlackjackService() {
         this.cardMachine = new CardMachine();
     }
 
-    public PlayersDto createPlayers(List<String> names) {
-        validatePlayerNames(names);
-        List<Player> players = new ArrayList<>();
-        for (String name : names) {
-            players.add(new Player(name));
-        }
-        return blackjackConverter.convertPlayersDto(players, new Dealer(PolicyConstant.DEALER_NAME));
+    public void createPlayers(List<String> names) {
+        Dealer dealer = new Dealer();
+        Players players = new Players(names);
+        participants = new Participants(dealer, players);
     }
 
-    private void validatePlayerNames(List<String> names) {
-        validatePlayerCount(names);
-        validatePlayerCount(names.size());
-    }
-
-    private void validatePlayerCount(List<String> names) {
-        if (new HashSet<>(names).size() != names.size()) {
-            throw new IllegalArgumentException(ErrorMessage.PLAYER_DUPLICATED.getMessage());
-        }
-    }
-
-    private void validatePlayerCount(int playerCount) {
-        if (!(PolicyConstant.PLAYER_MIN_COUNT <= playerCount
-            && playerCount <= PolicyConstant.PLAYER_MAX_COUNT)) {
-            throw new IllegalArgumentException(ErrorMessage.PLAYER_COUNT_OUT_OF_RANGE.getMessage());
-        }
-    }
-
-    public Card drawCard() {
-        return cardMachine.drawCard();
-    }
-
-    public PlayersDto dealInitialCards(PlayersDto playersDto) {
-        List<Player> players = playersDto.players();
-        for (Player player : players) {
+    public void dealInitialCards() {
+        participants.dealer().addCard(drawCard());
+        participants.dealer().addCard(drawCard());
+        for (Player player : participants.players().getPlayers()) {
             player.addCard(drawCard());
             player.addCard(drawCard());
         }
-        Dealer dealer = playersDto.dealer();
-        dealer.addCard(drawCard());
-        dealer.addCard(drawCard());
-        return blackjackConverter.convertPlayersDto(players, dealer);
     }
 
-    public boolean drawDealerCard(Dealer dealer) {
-        if (dealer.calculateScore() <= PolicyConstant.DEALER_HIT_MAX_SCORE) {
+    public List<ParticipantDto> generateInitialParticipantDtoList() {
+        List<ParticipantDto> participantDtoList = new ArrayList<>();
+
+        ParticipantDto dealerDto =  participants.generateInitialDealerDto();
+        participantDtoList.add(dealerDto);
+
+        List<ParticipantDto> playersDto = participants.generatePlayersDto();
+        participantDtoList.addAll(playersDto);
+
+        return participantDtoList;
+    }
+
+    public void validateHitOrStand(String hitOrStand) {
+        if (!hitOrStand.strip().equals(HitOrStand.HIT.getHitOrStand()) && !hitOrStand.strip()
+                .equals(HitOrStand.STAND.getHitOrStand())) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_YES_NO_INPUT.getMessage());
+        }
+    }
+
+    public boolean drawDealerCard(ParticipantDto dealerDto) {
+        if (dealerDto.score() <= PolicyConstant.DEALER_HIT_MAX_SCORE) {
+            Dealer dealer = participants.getDealer();
             dealer.addCard(drawCard());
             return true;
         }
@@ -81,19 +70,24 @@ public class BlackjackService {
         return false;
     }
 
-    public List<PlayerResultDto> calculatePlayerResults(PlayersDto playersDto) {
-        Dealer dealer = playersDto.dealer();
-        List<PlayerResultDto> playerResultDtoList = new ArrayList<>();
-        for (Player player : playersDto.players()) {
-            Result result = calculatePlayerResult(dealer, player);
-            playerResultDtoList.add(
-                blackjackConverter.convertPlayerResultDto(player.getName(), result));
-        }
-        return playerResultDtoList;
+    public Card drawCard() {
+        return cardMachine.drawCard();
     }
 
-    public DealerResultDto calculateDealerResult(PlayersDto playersDto) {
-        List<PlayerResultDto> playerResultDtoList = calculatePlayerResults(playersDto);
+    public List<ParticipantDto> getBlackjackResult() {
+        List<ParticipantDto> participantDtoList = new ArrayList<>();
+
+        ParticipantDto dealerDto =  participants.generateDealerDto();
+        participantDtoList.add(dealerDto);
+
+        List<ParticipantDto> playersDto = participants.generatePlayersDto();
+        participantDtoList.addAll(playersDto);
+
+        return participantDtoList;
+    }
+
+    public BlackjackStatisticsDto getBlackjackStatistics() {
+        List<PlayerResultDto> playerResultDtoList = calculatePlayerResults();
         int win = 0, draw = 0, lose = 0;
         for (PlayerResultDto playerResultDto : playerResultDtoList) {
             Result result = playerResultDto.result();
@@ -101,14 +95,17 @@ public class BlackjackService {
             draw += judgeResult(result, Result.DRAW);
             lose += judgeResult(result, Result.WIN);
         }
-        return new DealerResultDto(win, draw, lose);
+        return new BlackjackStatisticsDto(new DealerResultDto(win, draw, lose), playerResultDtoList);
     }
 
-    private int judgeResult(Result result, Result playerResult) {
-        if (result.equals(playerResult)) {
-            return 1;
+    public List<PlayerResultDto> calculatePlayerResults() {
+        Dealer dealer = participants.dealer();
+        List<PlayerResultDto> playerResultDtoList = new ArrayList<>();
+        for (Player player : participants.players().getPlayers()) {
+            Result result = calculatePlayerResult(dealer, player);
+            playerResultDtoList.add(new PlayerResultDto(player.getName(), result));
         }
-        return 0;
+        return playerResultDtoList;
     }
 
     private Result calculatePlayerResult(Dealer dealer, Player player) {
@@ -130,50 +127,24 @@ public class BlackjackService {
         return Result.LOSE;
     }
 
-    public HandDto generateHandDto(Player player) {
-        return blackjackConverter.convertHandDto(player);
-    }
-
-    private int calculateScore(Player player) {
-        return player.calculateScore();
-    }
-
-    public List<BlackjackResultDto> generateBlackjackResultDto(PlayersDto playersDto) {
-        List<BlackjackResultDto> blackjackResultDtoList = new ArrayList<>();
-        addResult(playersDto.dealer(), blackjackResultDtoList);
-        for (Player player : playersDto.players()) {
-            addResult(player, blackjackResultDtoList);
+    private int judgeResult(Result result, Result playerResult) {
+        if (result.equals(playerResult)) {
+            return 1;
         }
-        return Collections.unmodifiableList(blackjackResultDtoList);
+        return 0;
     }
 
-    private void addResult(Player player, List<BlackjackResultDto> blackjackResultDtoList) {
-        HandDto handDto = blackjackConverter.convertHandDto(player);
-        int score = calculateScore(player);
-        blackjackResultDtoList.add(
-            blackjackConverter.convertBlackjackResultDto(handDto, score));
-    }
-
-    public void validateHitOrStand(String hitOrStand) {
-        if (!hitOrStand.strip().equals(HitOrStand.HIT.getHitOrStand()) && !hitOrStand.strip()
-            .equals(HitOrStand.STAND.getHitOrStand())) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_YES_NO_INPUT.getMessage());
-        }
-    }
-
-    public boolean shouldRepeat(Player player, String hitOrStand) {
-        return hitOrStand.strip().equals(HitOrStand.HIT.getHitOrStand()) && !player.isBust();
-    }
-
-    public void updatePlayer(Player player) {
+    public ParticipantDto updatePlayer(String name) {
+        Player player = participants.getPlayer(name);
         player.addCard(drawCard());
+        return new ParticipantDto(name, player.getHand(), player.calculateScore());
     }
 
-    public boolean isNo(String hitOrStand) {
+    public boolean isHit(String name, String hitOrStand) {
+        return hitOrStand.strip().equals(HitOrStand.HIT.getHitOrStand());
+    }
+
+    public boolean isStand(String hitOrStand) {
         return hitOrStand.equals(HitOrStand.STAND.getHitOrStand());
-    }
-
-    public List<HandDto> generaterHandDtoList(PlayersDto playersDto) {
-        return blackjackConverter.convertHandDtoList(playersDto);
     }
 }
