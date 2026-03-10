cfeb418ad3a535cccc1ae85cec3527481b718afd
김가현 <hyeonnni@naver.com>
2026-03-10 23:49:13 +0900

    refactor(domain): 플레이어 객체 구조 개선

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,83 +1,12 @@
 package domain;
 
-public final class Player {
-    private final Name name;
-    private final CardList cardList;
-    private final Score score;
-    private int resultScore;
-    private int aceCount;
-    private Outcome outcome;
-
+public class Player extends Participant {
     public Player(String name) {
-        this.name = new Name(name);
-        this.cardList = new CardList();
-        this.score = new Score();
-        this.aceCount = 0;
-        this.resultScore = 0;
+        super(name);
     }
 
-    public void drawCard() {
-        Card card = cardList.draw();
-        while (isDuplicated(card)) {
-            card = cardList.draw();
-        }
-        saveDrawnCard(card);
-    }
-
-    public Outcome getOutcome() {
-        return outcome;
-    }
-
-    public void setOutcome(Outcome playerOutCome) {
-        outcome = playerOutCome;
-    }
-
-    public int getResultScore() {
-        return resultScore;
-    }
-
-    public void setResultScore(int number) {
-        resultScore = number;
-    }
-
-    public String getName() {
-        return name.getName();
-    }
-
-    public CardList getCardList() {
-        return cardList;
-    }
-
-    public Score getScore() {
-        return score;
-    }
-
-    public int getResult() {
-        return score.getScore();
-    }
-
-    public boolean checkBust() {
-        while (canLowerAceScore()) {
-            score.subScore(10);
-            aceCount--;
-        }
-        return score.isBust();
-    }
-
-    private boolean isDuplicated(Card card) {
-        return DuplicationSet.duplicationCards.contains(card.getCard());
-    }
-
-    private void saveDrawnCard(Card card) {
-        DuplicationSet.duplicationCards.add(card.getCard());
-        score.addScore(CardSuitMap.getScore(card.getCard()));
-        cardList.add(card);
-        if (card.checkAce()) {
-            aceCount += 1;
-        }
-    }
-
-    private boolean canLowerAceScore() {
-        return score.getScore() > 21 && aceCount > 0;
+    @Override
+    public boolean canDraw() {
+        return !getScore().isBust();
     }
 }
