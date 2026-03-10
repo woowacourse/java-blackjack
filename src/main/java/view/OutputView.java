bc8bfb52a0e3f8ba11bd98667bab5435aee3cf8a
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-10 14:36:07 +0900

    refactor(OutputView): 변경된 출력형식에 맞춰 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -15,7 +15,7 @@ public class OutputView {
 
     private static final String CARD_TEXT = "카드: ";
     private static final String DEALER_DRAW_ONE_CARD_TEXT = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
-    private static final String FINAL_RESULT_TEXT = "## 최종 승패";
+    private static final String FINAL_RESULT_TEXT = "## 최종 수익";
     private static final String DEALER_INIT_DECK_TEXT = "딜러와 ";
     private static final String INIT_DECK_TEXT = "에게 2장을 나누었습니다.";
     private static final String DEALER_CARD_TEXT = "딜러카드: ";
@@ -101,7 +101,7 @@ public class OutputView {
     }
 
     private static void printPlayerResult(PlayerWinning playerWinning) {
-        System.out.println(playerWinning.name() + RESULT_DELIMITER + playerWinning.matchStatus().getStatus());
+        System.out.println(playerWinning.name() + RESULT_DELIMITER + playerWinning.profit());
     }
 
 }
