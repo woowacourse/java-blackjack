0d0fd822889239d877a9bbeafc33b982e8ede52b
Chaerin Lee <rin@Rin-MacBook-Air.local>
2026-03-10 15:58:48 +0900

    test(BlackJackGame): 게임 시작시 참여자 모두 카드 2장씩 받는 테스트코드 추가

---
+++ src/test/java/blackjack/domain/BlackJackGameTest.java
@@ -0,0 +1,25 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class BlackJackGameTest {
+
+    @Test
+    public void 게임이_시작되면_참여자_모두_카드를_2장씩_받는다() {
+        // arrange
+        Players players = new Players(List.of("이산", "모카"));
+        Dealer dealer = new Dealer();
+        Deck deck = new Deck();
+        BlackJackGame blackJackGame = new BlackJackGame(players, dealer, deck);
+
+        // act
+        blackJackGame.initDraw();
+
+        // assert
+        assertThat(deck.getCount()).isEqualTo(46);
+    }
+
+}
