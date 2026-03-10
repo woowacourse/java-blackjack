c375bae7ed434da3ae3392a1268cc87d48069d4a
Chocoding1 <galmeagi2@naver.com>
2026-03-10 14:36:28 +0900

    refactor: Controller 책임 분리

--- src/main/java/blackjack/controller/BlackjackController.java
+++ src/main/java/blackjack/controller/BlackjackController.java
@@ -1,86 +1,54 @@
 package blackjack.controller;
 
-import static blackjack.model.Constant.DEALER_ADD_CARD_STAND;
-import static blackjack.model.Constant.TWENTY_ONE;
 import static blackjack.util.ExceptionHandler.retryUntilSuccess;
 
-import blackjack.model.CardCalculator;
+import blackjack.model.HitCommand;
 import blackjack.model.CardProvider;
 import blackjack.model.Dealer;
-import blackjack.model.ErrorMessage;
-import blackjack.model.GameResultCalculator;
-import blackjack.model.GameSummary;
 import blackjack.model.Player;
-import blackjack.model.User;
-import blackjack.util.PlayerParser;
-import blackjack.util.Validator;
+import blackjack.model.PlayersGameResult;
+import blackjack.model.Users;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class BlackjackController {
 
-    private static final String Y_N_REGREX = "^[yYnN]$";
-
     private final CardProvider cardProvider;
-    private final CardCalculator cardCalculator;
-    private final GameResultCalculator gameResultCalculator;
 
-    public BlackjackController(CardProvider cardProvider, CardCalculator cardCalculator,
-                               GameResultCalculator gameResultCalculator) {
+    public BlackjackController(CardProvider cardProvider) {
         this.cardProvider = cardProvider;
-        this.cardCalculator = cardCalculator;
-        this.gameResultCalculator = gameResultCalculator;
     }
 
     public void run() {
-        List<Player> players = retryUntilSuccess(() -> {
-            String input = InputView.readPlayerName();
-            return PlayerParser.parse(input);
-        });
-        Dealer dealer = new Dealer();
+        Users users = retryUntilSuccess(this::createUsers);
 
-        provideInitCardsPrint(players, dealer);
+        provideInitCardsAndPrint(users);
 
-        hit(players, dealer);
+        hit(users);
 
-        List<User> users = new ArrayList<>(players);
-        users.addFirst(dealer);
+        printHandStatus(users);
 
-        List<GameSummary> gameSummaries = createGameSummaryAndPrint(users);
-
-        createGameResultAndPrint(gameSummaries, users);
+        printGameResult(users);
 
         InputView.closeScanner();
     }
 
-    private void provideInitCardsPrint(List<Player> players, Dealer dealer) {
-        cardProvider.provideInitCards(players, dealer);
-        OutputView.printInitCards(players, dealer);
+    private Users createUsers() {
+        String input = InputView.readPlayerName();
+        return new Users(input);
     }
 
-    private void createGameResultAndPrint(List<GameSummary> gameSummaries, List<User> users) {
-        gameResultCalculator.calculate(gameSummaries);
-        OutputView.printGameResult(users);
+    private void provideInitCardsAndPrint(Users users) {
+        cardProvider.provideInitCards(users);
+        OutputView.printInitCards(users);
     }
 
-    private List<GameSummary> createGameSummaryAndPrint(List<User> users) {
-        List<GameSummary> gameSummaries = new ArrayList<>();
-        for (User user : users) {
-            int totalScore = cardCalculator.totalScore(user.cards());
-            boolean bust = cardCalculator.calculateBust(totalScore);
-            boolean blackjack = cardCalculator.calculateBlackjack(user.cards());
+    private void hit(Users users) {
+        List<Player> players = users.getPlayers();
+        Dealer dealer = users.getDealer();
 
-            GameSummary gameSummary = new GameSummary(user, totalScore, bust, blackjack);
-            gameSummaries.add(gameSummary);
-            OutputView.printCardStatus(gameSummary);
-        }
-        return gameSummaries;
-    }
-
-    public void hit(List<Player> players, Dealer dealer) {
         for (Player player : players) {
             while (retryUntilSuccess(() -> checkY(player)) && checkAddCard(player)) {
                 cardProvider.provideOneCard(player);
@@ -88,24 +56,32 @@ public class BlackjackController {
             }
         }
 
-        while (cardCalculator.totalScore(dealer.cards()) < DEALER_ADD_CARD_STAND) {
-            OutputView.printDealerHit();
+        while (dealer.isHitAvailable()) {
             cardProvider.provideOneCard(dealer);
+            OutputView.printDealerHit();
         }
     }
 
-    private static boolean checkY(Player player) {
+    private void printHandStatus(Users users) {
+        OutputView.printHandStatus(users);
+    }
+
+    private void printGameResult(Users users) {
+        PlayersGameResult playersGameResult = users.determineWinner();
+        OutputView.printGameResult(playersGameResult, users);
+    }
+
+    private boolean checkY(Player player) {
         String input = InputView.readCardAdd(player).trim();
-        Validator.validateEmpty(input, ErrorMessage.ERROR_EMPTY_INPUT.getErrorMessage());
-        Validator.validateRegrex(Y_N_REGREX, input, ErrorMessage.ERROR_NOT_Y_N_INPUT.getErrorMessage());
-        return input.equals("y");
+        HitCommand hitCommand = new HitCommand(input);
+        return hitCommand.isY();
     }
 
-    boolean checkAddCard(Player player) {
-        if (cardCalculator.totalScore(player.cards()) >= TWENTY_ONE) {
-            OutputView.printCantAddCard();
-            return false;
+    private boolean checkAddCard(Player player) {
+        if (player.isHitAvailable()) {
+            return true;
         }
-        return true;
+        OutputView.printCantHit();
+        return false;
     }
 }
