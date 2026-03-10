c17427fa9613232621c1ae5bccce47986ac56ed4
Chaerin Lee <rin@Rin-MacBook-Air.local>
2026-03-10 16:56:57 +0900

    test(Dealer): 딜러의 카드 합 17 넘는지 여부 테스트 추가

--- src/test/java/blackjack/domain/DealerTest.java
+++ src/test/java/blackjack/domain/DealerTest.java
@@ -1,6 +1,8 @@
 package blackjack.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +14,29 @@ public class DealerTest {
         dealer.recieveCard(new Card(CardPoint.ACE, CardPattern.DIAMOND));
         assertThat(dealer.getCardCount()).isEqualTo(1);
     }
-    
+
+    @Test
+    void 딜러의_카드의_합이_17_이상이다_에이스_포함() {
+        Dealer dealer = new Dealer();
+        dealer.hand.addCard(new Card(CardPoint.ACE, CardPattern.DIAMOND));
+        dealer.hand.addCard(new Card(CardPoint.SIX, CardPattern.CLUB));
+        assertTrue(dealer.isOver17());
+    }
+
+    @Test
+    void 딜러의_카드의_합이_17_이상이다_에이스_미포함() {
+        Dealer dealer = new Dealer();
+        dealer.hand.addCard(new Card(CardPoint.TEN, CardPattern.DIAMOND));
+        dealer.hand.addCard(new Card(CardPoint.FIVE, CardPattern.CLUB));
+        dealer.hand.addCard(new Card(CardPoint.FIVE, CardPattern.CLUB));
+        assertTrue(dealer.isOver17());
+    }
+
+    @Test
+    void 딜러의_카드의_합이_17_미만이다() {
+        Dealer dealer = new Dealer();
+        dealer.hand.addCard(new Card(CardPoint.TEN, CardPattern.DIAMOND));
+        dealer.hand.addCard(new Card(CardPoint.FIVE, CardPattern.CLUB));
+        assertFalse(dealer.isOver17());
+    }
 }
