5baee7ec45590e920bb8b439ae544cf85261f757
kkb <gimgyubin1225@gmail.com>
2026-03-10 21:56:05 +0900

    refactor: OutputWriter 상수를 static final로 변경

--- src/main/java/view/OutputWriter.java
+++ src/main/java/view/OutputWriter.java
@@ -2,17 +2,17 @@ package view;
 
 public class OutputWriter {
 
-    private final String LINE_SEPARATOR = System.lineSeparator();
-    private final String INPUT_NAME_GUIDE_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-    private final String DEAL_INITIAL_CARD_MESSAGE = "딜러와 %s에게 %d장을 나누었습니다.";
-    private final String ASK_DRAW_CARD_GUIDE_MESSAGE = "%s는 한 장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
-    private final String DEALER_ADDITIONAL_DRAW_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
-    private final String FINAL_RESULT_TITLE_MESSAGE = "## 최종 승패";
-    private final String FINAL_RESULT_PLAYER = "%s: %s";
-    private final String FINAL_RESULT_DEALER = "딜러: %s";
-    private final String PLAYER_HAND_MESSAGE = "%s카드: %s";
-    private final String CARD_SUM_RESULT_MESSAGE = "%s카드: %s - 결과: %d";
-    private final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final String INPUT_NAME_GUIDE_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String DEAL_INITIAL_CARD_MESSAGE = "딜러와 %s에게 %d장을 나누었습니다.";
+    private static final String ASK_DRAW_CARD_GUIDE_MESSAGE = "%s는 한 장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String DEALER_ADDITIONAL_DRAW_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    private static final String FINAL_RESULT_TITLE_MESSAGE = "## 최종 승패";
+    private static final String FINAL_RESULT_PLAYER = "%s: %s";
+    private static final String FINAL_RESULT_DEALER = "딜러: %s";
+    private static final String PLAYER_HAND_MESSAGE = "%s카드: %s";
+    private static final String CARD_SUM_RESULT_MESSAGE = "%s카드: %s - 결과: %d";
+    private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
 
     public void printInputNameGuideMessage() {
         System.out.println(INPUT_NAME_GUIDE_MESSAGE);
