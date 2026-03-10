bd04654bef9a177c38e897adaaf3d29aa9f98bbd
dongheeHan <ark182818@gmail.com>
2026-03-10 21:00:32 +0900

    feat: GameResult에 BLACKJACK 추가 및 수익 계산 책임 부여

--- src/main/java/blackjack/domain/result/GameResult.java
+++ src/main/java/blackjack/domain/result/GameResult.java
@@ -1,32 +1,44 @@
 package blackjack.domain.result;
 
+import blackjack.domain.betting.BettingMoney;
 import blackjack.domain.hand.Score;
+import blackjack.domain.participant.Dealer;
+import blackjack.domain.participant.Player;
 
 public enum GameResult {
 
-    WIN("승"),
-    DRAW("무"),
-    LOSE("패"),
+    BLACKJACK("블랙잭", 1.5),
+    WIN("승", 1.0),
+    DRAW("무", 0.0),
+    LOSE("패", -1.0),
     ;
 
     private final String displayName;
+    private final double profitMultiplier;
 
-    GameResult(final String displayName) {
+    GameResult(final String displayName, final double profitMultiplier) {
         this.displayName = displayName;
+        this.profitMultiplier = profitMultiplier;
     }
 
-    public String getDisplayName() {
-        return displayName;
+    public int calculateProfit(final BettingMoney bettingMoney) {
+        return (int) (bettingMoney.getAmount() * profitMultiplier);
     }
 
-    public static GameResult of(final Score playerScore, final Score dealerScore) {
-        if (playerScore.isBust()) {
+    public static GameResult of(final Player player, final Dealer dealer) {
+        if (player.isBlackjack() && dealer.isBlackjack()) {
+            return DRAW;
+        }
+        if (player.isBlackjack()) {
+            return BLACKJACK;
+        }
+        if (player.isBust()) {
             return LOSE;
         }
-        if (dealerScore.isBust()) {
+        if (dealer.isBust()) {
             return WIN;
         }
-        return compare(playerScore, dealerScore);
+        return compare(player.calculateScore(), dealer.calculateScore());
     }
 
     private static GameResult compare(final Score playerScore, final Score dealerScore) {
@@ -35,16 +47,6 @@ public enum GameResult {
         }
         if (playerScore.isLessThan(dealerScore)) {
             return LOSE;
-        }
-        return DRAW;
-    }
-
-    public GameResult reverse() {
-        if (this == WIN) {
-            return LOSE;
-        }
-        if (this == LOSE) {
-            return WIN;
         }
         return DRAW;
     }
