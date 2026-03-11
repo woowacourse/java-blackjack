40037cf3ebc6c350b103499864cba8073963b632
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-03-11 14:33:26 +0900

    refactor: import문 와일드카드(*) 제거

--- src/main/java/domain/result/Result.java
+++ src/main/java/domain/result/Result.java
@@ -1,6 +1,10 @@
 package domain.result;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Result {
     private static final int INIT_COUNT = 0;
