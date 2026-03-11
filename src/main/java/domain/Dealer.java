ca0d65283521903c94531588305b2b33ec7c84c5
dahye <dahye9011@naver.com>
2026-03-11 19:30:09 +0900

    fix: 결과 경우의 수 조건 수정

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -1,13 +1,11 @@
 package domain;
 
 import vo.GameResult;
-import vo.Money;
 
 public class Dealer {
     private static final Integer MAXIMUM_TOTAL_SCORE = 21;
 
     private final Hand hand;
-    private Money profit;
 
     public Dealer() {
         this.hand = new Hand();
@@ -34,25 +32,33 @@ public class Dealer {
     }
 
     public GameResult judgeResultForUser(int userTotalScore) {
+        // 유저 버스트
         if (userTotalScore > MAXIMUM_TOTAL_SCORE) {
             return GameResult.LOSE;
         }
 
+        // 딜러와 유저 둘 다 블랙잭
+        if ((userTotalScore == MAXIMUM_TOTAL_SCORE) && (hand.getHandTotalScore() == MAXIMUM_TOTAL_SCORE)) {
+            return GameResult.DRAW;
+        }
+
+        // 유저만 블랙잭
         if (userTotalScore == MAXIMUM_TOTAL_SCORE) {
             return GameResult.BLACKJACK;
         }
 
+        // 딜러만 블랙잭
         if (hand.getHandTotalScore() == MAXIMUM_TOTAL_SCORE) {
             return GameResult.LOSE_BY_BLACKJACK;
         }
 
-        // 딜러의 점수가 21을 초과하는 경우, 유저 승
+        // 딜러 버스트
         if (hand.getHandTotalScore() > MAXIMUM_TOTAL_SCORE) {
             return GameResult.WIN;
         }
 
-        // 딜러의 점수가 21보다 작으면서, 유저의 점수가 딜러의 점수보다 큰 경우, 유저 승
-        if (userTotalScore > hand.getHandTotalScore() && (hand.getHandTotalScore() < MAXIMUM_TOTAL_SCORE)) {
+        // 둘 다 21 이하
+        if (userTotalScore > hand.getHandTotalScore()) {
             return GameResult.WIN;
         }
 
