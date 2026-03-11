92bef665456c411a7f9b211cd2a3cddc93a2ecdc
EveryPine <kenton.lim@gmail.com>
2026-03-11 17:04:35 +0900

    test(bet): 베팅 객체 생성 테스트 추가

---
+++ src/test/java/domain/BetTest.java
@@ -0,0 +1,54 @@
+package domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class BetTest {
+
+    @Nested
+    @DisplayName("생성 테스트")
+    class 생성_테스트 {
+
+        @Test
+        @DisplayName("정상 테스트")
+        void 정상_테스트() {
+            int amount = 10000;
+
+            assertDoesNotThrow(() -> new Bet(amount));
+        }
+
+        @Test
+        @DisplayName("베팅금이 5,000 미만인 경우")
+        void 베팅금이_5000_미만인_경우() {
+            int amount = 1000;
+
+            assertThatThrownBy(() -> new Bet(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("베팅금은 5,000 이상이어야 합니다.");
+        }
+
+        @Test
+        @DisplayName("베팅금이 100,000,000 초과인 경우")
+        void 베팅금이_5000_미만인_경우() {
+            int amount = 100_001_000;
+
+            assertThatThrownBy(() -> new Bet(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("베팅금은 100,000,000 이하여야 합니다.");
+        }
+
+        @Test
+        @DisplayName("베팅금 단위가 1,000이 아닌 경우")
+        void 베팅금이_5000_미만인_경우() {
+            int amount = 5001;
+
+            assertThatThrownBy(() -> new Bet(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("베팅금은 1,000 단위여야 합니다.");
+        }
+    }
+}
