0f9910c8349691013f04b67519314b6aea933a0e
zakie <realzzaekkii@gmail.com>
2026-03-11 14:20:20 +0900

    refactor: Name 클래스를 record로 변경

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -2,21 +2,14 @@ package domain;
 
 import static exception.ErrorMessage.PLAYER_NAME_LENGTH_ERROR;
 
-public class Name {
-
-    // TODO: record 타입으로 수정
-
-    private final String name;
-
+public record Name(
+        String name
+) {
     // TODO: 검증 테스트 추가
     public Name(final String name) {
         if (name.length() < 2 || name.length() > 10) { // FIXME: 하드코딩된 길이 하한, 상한 값 상수화
             throw new IllegalArgumentException(PLAYER_NAME_LENGTH_ERROR.getMessage());
         }
         this.name = name;
-    }
-
-    public String getName() {
-        return name;
     }
 }
