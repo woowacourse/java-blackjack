b92127f5f31e07a4572b92e53614e4c8db519f27
Chaerin Lee <rin@Rin-MacBook-Air.local>
2026-03-11 14:37:09 +0900

    refacotr: 불필요한 테스트코드 삭제

--- src/test/java/blackjack/domain/ParticipantTest.java
+++ src/test/java/blackjack/domain/ParticipantTest.java
@@ -1,8 +1,5 @@
 package blackjack.domain;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 public class ParticipantTest {
@@ -11,42 +8,5 @@ public class ParticipantTest {
         Participant participant = new Player("이산", new Hand());
         Deck deck = new Deck();
         participant.receiveCard(deck.draw());
-    }
-
-    @Test
-    void 참여자가_히트를_할_수_있는_경우_플레이어() {
-        Hand hand = new Hand();
-        hand.addCard(new Card(CardPoint.KING, CardPattern.SPADE));
-        hand.addCard(new Card(CardPoint.JACK, CardPattern.HEART));
-        Participant player = new Player("이산", hand);
-        assertTrue(player.canHit());
-    }
-
-    @Test
-    void 참여자가_히트를_할_수_없는_경우_플레이어() {
-        Hand hand = new Hand();
-        hand.addCard(new Card(CardPoint.KING, CardPattern.SPADE));
-        hand.addCard(new Card(CardPoint.TWO, CardPattern.HEART));
-        hand.addCard(new Card(CardPoint.NINE, CardPattern.DIAMOND));
-        Participant player = new Player("이산", hand);
-        assertFalse(player.canHit());
-    }
-
-    @Test
-    void 참여자가_히트를_할_수_있는_경우_딜러() {
-        Hand hand = new Hand();
-        hand.addCard(new Card(CardPoint.KING, CardPattern.SPADE));
-        hand.addCard(new Card(CardPoint.SIX, CardPattern.HEART));
-        Participant dealer = new Dealer(hand);
-        assertTrue(dealer.canHit());
-    }
-
-    @Test
-    void 참여자가_히트를_할_수_없는_경우_딜러() {
-        Hand hand = new Hand();
-        hand.addCard(new Card(CardPoint.KING, CardPattern.SPADE));
-        hand.addCard(new Card(CardPoint.SEVEN, CardPattern.HEART));
-        Participant dealer = new Dealer(hand);
-        assertFalse(dealer.canHit());
     }
 }
