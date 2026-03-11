aee237930086f00b81c66483f8eaff7dfe15cf38
zakie <realzzaekkii@gmail.com>
2026-03-11 14:23:06 +0900

    refactor: 숫자 하드코딩값 상수화

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -5,9 +5,12 @@ import static exception.ErrorMessage.PLAYER_NAME_LENGTH_ERROR;
 public record Name(
         String name
 ) {
+    public static final int MINIMUM_BOUND = 2;
+    public static final int MAXIMUM_BOUND = 10;
+
     // TODO: 검증 테스트 추가
     public Name(final String name) {
-        if (name.length() < 2 || name.length() > 10) { // FIXME: 하드코딩된 길이 하한, 상한 값 상수화
+        if ((name.length() < MINIMUM_BOUND) || (name.length() > MAXIMUM_BOUND)) {
             throw new IllegalArgumentException(PLAYER_NAME_LENGTH_ERROR.getMessage());
         }
         this.name = name;
