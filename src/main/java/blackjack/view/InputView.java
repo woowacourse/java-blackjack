d16f268e0cab353ae76a557251f939eb82d3d6eb
sinequanonn <dleck288@gmail.com>
2026-03-11 21:22:47 +0900

    refactor: InputView 리팩토링
    - 참여할 플레이어 입력 List<String> 반환
    - 이름 스플릿 기능 위임

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -3,32 +3,41 @@ package blackjack.view;
 import blackjack.domain.Player;
 import blackjack.exception.ErrorMessage;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String DELIMITER = ",";
 
-    private final Scanner sc;
+    private final Scanner scanner;
 
     public InputView() {
-        this.sc = new Scanner(System.in);
+        this.scanner = new Scanner(System.in);
     }
 
-    public String getName() {
+    public List<String> readNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return sc.nextLine();
+        return split(scanner.nextLine());
     }
 
-    public String getReceiveCard(Player player) {
+    public String readReceiveCard(Player player) {
         System.out.println(player.getName() + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
-        return sc.nextLine();
+        return scanner.nextLine();
     }
 
-    public int inputBettingAmount(Player player) {
-        System.out.println("\n" + player.getName() + "의 배팅 금액은?");
+    public int readBettingAmount(String name) {
+        System.out.println("\n" + name + "의 배팅 금액은?");
         try {
-            return Integer.parseInt(sc.nextLine());
+            return Integer.parseInt(scanner.nextLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_BETTING_AMOUNT_INPUT.getMessage());
         }
+    }
+
+    private List<String> split(String input) {
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .toList();
     }
 }
