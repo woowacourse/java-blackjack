b0cfb40d0e6c31350fc68243c1a75c45c659aa29
zakie <realzzaekkii@gmail.com>
2026-03-11 16:29:46 +0900

    refactor: 셔플러가 카드 리스트를 섞도록 기능 변경

--- src/main/java/domain/card/CardShuffler.java
+++
@@ -1,6 +0,0 @@
-package domain.card;
-
-// TODO: Q. infra 계층이 꼭 필요했나? -> 외부 세계와 연결되는 계층 -> shuffle이 외부 세계는 아니긴 해
-public interface CardShuffler {
-    int shuffleCardDeck(int deckSize);
-}
---
+++ src/main/java/domain/card/Shuffler.java
@@ -0,0 +1,7 @@
+package domain.card;
+
+import java.util.List;
+
+public interface Shuffler {
+    void shuffle(List<Card> list);
+}
