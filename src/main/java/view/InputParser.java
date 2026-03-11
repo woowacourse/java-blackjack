77c2ca96b8daa272ab45db14dc4be58dfe5aa021
wontop02 <wjy0502kr@naver.com>
2026-03-11 21:55:56 +0900

    feat(parser): 배팅 금액의 형식 검증, int 범위 내인지 검증하는 기능 구현

--- src/main/java/view/InputParser.java
+++ src/main/java/view/InputParser.java
@@ -1,9 +1,12 @@
 package view;
 
+import static message.ErrorMessage.BET_AMOUNT_OUT_OF_RANGE;
 import static view.constant.ViewRule.YES_ANSWER;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+import message.ErrorMessage;
 
 public class InputParser {
 
@@ -16,5 +19,22 @@ public class InputParser {
 
     public static boolean parseHitAnswer(String input) {
         return YES_ANSWER.contains(input);
+    }
+
+    public static int parseBetAmount(String input) {
+        try {
+            validateBetAmountIntRange(input);
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(ErrorMessage.BET_AMOUNT_INVALID_FORMAT.getMessage());
+        }
+    }
+
+    private static void validateBetAmountIntRange(String input) {
+        BigInteger value = new BigInteger(input);
+        if (value.compareTo(BigInteger.valueOf(Integer.MIN_VALUE)) < 0
+                || value.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) > 0) {
+            throw new IllegalArgumentException(BET_AMOUNT_OUT_OF_RANGE.getMessage());
+        }
     }
 }
