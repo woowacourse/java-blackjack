5f0e5cbf9582d4b34d41be8f0249d0f18569fa50
nn98 <jkllhgb@gmail.com>
2026-03-11 17:52:01 +0900

    feat(view): 입출력/파싱 배팅 처리, 예외처리 추가

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -14,10 +14,30 @@ public class InputView {
         return scanner.nextLine();
     }
 
-    public List<String> parsePlayerNames() {
-        return Arrays.stream(readLine().split(DELIMITER))
+    public List<String> readPlayerNames() {
+        List<String> playerNames = Arrays.stream(readLine().split(DELIMITER))
                 .map(String::trim)
                 .toList();
+        validatePlayerNames(playerNames);
+        return playerNames;
+    }
+
+    private void validatePlayerNames(List<String> playerNames) {
+        boolean isBlankNameExist = playerNames.stream().anyMatch(String::isBlank);
+        if (isBlankNameExist) {
+            throw new IllegalArgumentException("플레이어 닉네임은 공백일 수 없습니다.");
+        }
+        if (playerNames.isEmpty()) {
+            throw new IllegalArgumentException("한 명 이상의 플레이어 닉네임을 입력해주세요");
+        }
+    }
+
+    public int readPlayerBettingAmount() {
+        try {
+            return Integer.parseInt(readLine());
+        } catch (NumberFormatException exception) {
+            throw new NumberFormatException("배팅 금액은 숫자로 입력해주세요.");
+        }
     }
 
     public boolean getUserCommand() {
