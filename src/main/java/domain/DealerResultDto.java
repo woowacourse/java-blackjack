03491837d3d0a9fb434d058e29dd61e2b43aeba3
haeyoon1 <hynam1220@naver.com>
2026-03-11 14:39:13 +0900

    feat: dealer 승패무 저장하는 dto 생성

---
+++ src/main/java/domain/DealerResultDto.java
@@ -0,0 +1,11 @@
+package domain;
+
+public record DealerResultDto (
+        int win,
+        int draw,
+        int lose
+){
+    public static DealerResultDto of(int win, int draw, int lose) {
+        return new DealerResultDto(win, draw, lose);
+    }
+}
