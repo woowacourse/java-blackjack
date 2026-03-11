7d5c767b98d5225e5810e8b9839917ca0213caf2
smini <sm020320@naver.com>
2026-03-11 21:40:08 +0900

    refactor: ErrorMessage에서 중복 이름 관련 에러 메시지 제거

--- src/main/java/blackjack/exception/ErrorMessage.java
+++ src/main/java/blackjack/exception/ErrorMessage.java
@@ -1,7 +1,6 @@
 package blackjack.exception;
 
 public enum ErrorMessage {
-    DUPLICATED("이름은 중복될 수 없습니다."),
     OUT_OF_NAME_LENGTH("이름 길이는 최소 2자 이상, 5자 이하여야 합니다."),
     INVALID_PLAYERS("플레이어는 2명 이상 5명 이하여야 합니다."),
     INVALID_INPUT("유효하지 않은 입력입니다."),
