a89ce90b5e06befc933360306b36f59d0cf56e91
nn98 <jkllhgb@gmail.com>
2026-03-11 16:10:00 +0900

    feat(gameResult): 승패 판정 일원화를 위한 반전 기능 추가

--- src/main/java/blackjack/domain/participant/GameResult.java
+++ src/main/java/blackjack/domain/participant/GameResult.java
@@ -1,5 +1,15 @@
 package blackjack.domain.participant;
 
 public enum GameResult {
-    WIN, DRAW, LOSE
+    WIN, DRAW, LOSE;
+
+    public static GameResult reverse(GameResult gameResult) {
+        if (gameResult == GameResult.WIN) {
+            return GameResult.LOSE;
+        }
+        if (gameResult == GameResult.LOSE) {
+            return GameResult.WIN;
+        }
+        return GameResult.DRAW;
+    }
 }
