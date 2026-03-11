33aca5b8482a171ba093e87d8d8a6110efd2dc59
smini <sm020320@naver.com>
2026-03-11 09:18:11 +0900

    feat: Player에 배팅 금액 검증 로직 및 BettingAmount 필드 추가

--- src/main/java/blackjack/model/Player.java
+++ src/main/java/blackjack/model/Player.java
@@ -1,10 +1,15 @@
 package blackjack.model;
 
+import blackjack.exception.ErrorMessage;
+
 public class Player extends Participant {
     private final Name name;
+    private final BettingAmount bettingAmount;
 
-    public Player(String name) {
+    public Player(String name, double money) {
         this.name = new Name(name);
+        validateMoney(money);
+        this.bettingAmount = new BettingAmount(money);
     }
 
     public String getName() {
@@ -14,5 +19,11 @@ public class Player extends Participant {
     @Override
     public boolean canReceive() {
         return getScore().isPlayerHitScore();
+    }
+
+    private void validateMoney(double money) {
+        if (money <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.AMOUNT_NOT_ZERO.getMessage());
+        }
     }
 }
