3050f4b827a504109d9acc71c7f4acc0a048aa02
Minjae Kang <rkdalswoals@gmail.com>
2026-03-11 18:35:23 +0900

    refactor: 카드덱 생성 이중 for문 수정

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -4,8 +4,10 @@ import domain.card.Card;
 import domain.card.Rank;
 import domain.card.Suit;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Deck {
     private final List<Card> cards;
@@ -15,15 +17,12 @@ public class Deck {
     }
 
     public static Deck create() {
-        List<Card> cards = new ArrayList<>();
-
-        for (Suit suit : Suit.values()) {
-            for (Rank cardNumber : Rank.values()) {
-                cards.add(new Card(suit, cardNumber));
-            }
-        }
-
-        return new Deck(cards);
+        return new Deck(
+                Arrays.stream(Suit.values())
+                        .flatMap(suit -> Arrays.stream(Rank.values())
+                                .map(rank -> new Card(suit, rank)))
+                        .collect(Collectors.toCollection(ArrayList::new))
+        );
     }
 
     public Card pop() {
