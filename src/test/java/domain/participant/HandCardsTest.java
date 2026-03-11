dc5ee8a2effce6bd0cf463a3d48368c875915770
choubung <choubung@sookmyung.ac.kr>
2026-03-11 11:22:32 +0900

    Test(HandCards): ace 계산 테스트 값을 경계값(최소값, 최대값)으로 변경

--- src/test/java/domain/participant/HandCardsTest.java
+++ src/test/java/domain/participant/HandCardsTest.java
@@ -13,6 +13,21 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class HandCardsTest {
+    static Stream<Arguments> provideHandCardsData() {
+        return Stream.of(
+                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.NINE), new Card(TrumpSuit.CLUB, TrumpNumber.NINE)), 18), // ACE 없는 경우
+                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.JACK), new Card(TrumpSuit.CLUB, TrumpNumber.ACE)), 21), // ACE가 11으로 처리되어야 하는 경우
+                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.SEVEN), new Card(TrumpSuit.CLUB, TrumpNumber.FOUR),
+                        new Card(TrumpSuit.CLUB, TrumpNumber.ACE)), 12), // ACE가 1로 처리되어야 하는 경우 (1)
+                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.TEN), new Card(TrumpSuit.CLUB, TrumpNumber.TEN),
+                        new Card(TrumpSuit.CLUB, TrumpNumber.ACE)), 21), // ACE가 1로 처리되어야 하는 경우 (2)
+                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.ACE), new Card(TrumpSuit.CLUB, TrumpNumber.ACE),
+                        new Card(TrumpSuit.CLUB, TrumpNumber.NINE)), 21), // ACE가 두 개이며, 하나는 11, 하나는 1로 처리되어야 하는 경우
+                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.ACE), new Card(TrumpSuit.CLUB, TrumpNumber.ACE),
+                        new Card(TrumpSuit.CLUB, TrumpNumber.NINE)), 21)// ACE가 두 개이며, 둘 다 1로 처리되어야 하는 경우
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("provideHandCardsData")
     void ace_값_계산_테스트(List<Card> cards, int exceptResult) {
@@ -23,18 +38,5 @@ class HandCardsTest {
         }
 
         assertThat(handCards.calculateCardsScore()).isEqualTo(exceptResult);
-    }
-
-    static Stream<Arguments> provideHandCardsData() {
-        return Stream.of(
-                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.NINE), new Card(TrumpSuit.CLUB, TrumpNumber.NINE)), 18), // ACE 없는 경우
-                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.JACK), new Card(TrumpSuit.CLUB, TrumpNumber.ACE)), 21), // ACE가 11으로 처리되어야 하는 경우
-                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.SEVEN), new Card(TrumpSuit.CLUB, TrumpNumber.NINE),
-                        new Card(TrumpSuit.CLUB, TrumpNumber.ACE)), 17), // ACE가 1로 처리되어야 하는 경우
-                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.ACE), new Card(TrumpSuit.CLUB, TrumpNumber.ACE),
-                        new Card(TrumpSuit.CLUB, TrumpNumber.NINE)), 21), // ACE가 두 개이며, 하나는 11, 하나는 1로 처리되어야 하는 경우
-                Arguments.of(List.of(new Card(TrumpSuit.HEART, TrumpNumber.ACE), new Card(TrumpSuit.CLUB, TrumpNumber.ACE),
-                        new Card(TrumpSuit.CLUB, TrumpNumber.NINE)), 21)// ACE가 두 개이며, 둘 다 1로 처리되어야 하는 경우
-        );
     }
 }
