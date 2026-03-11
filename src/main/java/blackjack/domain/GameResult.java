fe604735d232f11014d2d5d82893280cdb97ee72
EveryPine <kenton.lim@gmail.com>
2026-03-11 20:53:48 +0900

    feat(game-result): 블랙잭 게임 결과 판정 기능 추가

--- src/main/java/blackjack/domain/GameResult.java
+++ src/main/java/blackjack/domain/GameResult.java
@@ -3,6 +3,7 @@ package blackjack.domain;
 public enum GameResult {
 
     WIN("승"),
+    BLACKJACK("블랙잭"),
     DRAW("무"),
     LOSE("패");
 
@@ -17,15 +18,43 @@ public enum GameResult {
     }
 
     public static GameResult calculate(final Player player, final Dealer dealer) {
-        if (player.isBurst()) {
-            return GameResult.LOSE;
-        }
-        if (dealer.isBurst() || dealer.getScore() < player.getScore()) {
+        if (isWin(player, dealer)) {
             return GameResult.WIN;
         }
-        if (dealer.getScore() == player.getScore()) {
+        if (isBlackjack(player, dealer)) {
+            return GameResult.BLACKJACK;
+        }
+        if (isDraw(player, dealer)) {
             return GameResult.DRAW;
         }
         return GameResult.LOSE;
+    }
+
+    private static boolean isWin(final Player player, final Dealer dealer) {
+        if (player.isBlackjack()) {
+            return false;
+        }
+        if (dealer.isBurst() && !player.isBurst()) {
+            return true;
+        }
+        if (!dealer.isBurst() && !player.isBurst() &&
+            player.getScore() > dealer.getScore()) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isBlackjack(final Player player, final Dealer dealer) {
+        if (!dealer.isBlackjack() && player.isBlackjack()) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isDraw(final Player player, final Dealer dealer) {
+        if (!player.isBurst() && !dealer.isBurst()) {
+            return dealer.getScore() == player.getScore();
+        }
+        return false;
     }
 }
