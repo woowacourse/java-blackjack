1c7b8b73ab697d1fbbf094bf1c390181c89ec173
snow___shower <wonsb25@gmail.com>
2026-03-11 15:49:06 +0900

    test: 먼저 버스트된 플레이어의 패배 처리와 딜러 버스트시 결과 테스트 추가

--- src/test/java/BlackJackServiceTest.java
+++ src/test/java/BlackJackServiceTest.java
@@ -18,7 +18,9 @@ public class BlackJackServiceTest {
     Player winPlayer;
     Player defeatPlayer;
     Player drawPlayer;
+    Player bustPlayer;
     Players players;
+    BlackJackService blackJackService;
 
     @BeforeEach
     void init() {
@@ -26,34 +28,57 @@ public class BlackJackServiceTest {
         winPlayer = new Player(new ParticipantInfo("이김", new Hand()), new Money(0));
         defeatPlayer = new Player(new ParticipantInfo("짐", new Hand()),new Money(0));
         drawPlayer = new Player(new ParticipantInfo("비김", new Hand()),new Money(0));
+        bustPlayer=new Player(new ParticipantInfo("버스트", new Hand()), new Money(0));
         dealer.keepCard(new Card(Rank.FIVE, Pattern.CLOVER));
         winPlayer.keepCard(new Card(Rank.SIX, Pattern.CLOVER));
         defeatPlayer.keepCard(new Card(Rank.FOUR, Pattern.CLOVER));
         drawPlayer.keepCard(new Card(Rank.FIVE, Pattern.SPADE));
-        players = new Players(List.of(winPlayer, defeatPlayer, drawPlayer));
+        players = new Players(List.of(winPlayer, defeatPlayer, drawPlayer, bustPlayer));
+        blackJackService=new BlackJackService();
+
     }
 
     @Test
     void 딜러의_총합이_플레이어의_총합보다_낮으면_플레이어가_승리한다() {
-        BlackJackService blackJackService = new BlackJackService();
         Result result = blackJackService.calculateResult(dealer, players);
-        ResultInfo info = result.getPlayersResult().get(winPlayer.getName());
+        ResultInfo info = result.getPlayersResult().get(winPlayer);
         assertThat(info).isEqualTo(ResultInfo.WIN);
     }
 
     @Test
     void 딜러의_총합이_플레이어의_총합보다_높으면_플레이어가_패배한다() {
-        BlackJackService blackJackService = new BlackJackService();
         Result result = blackJackService.calculateResult(dealer, players);
-        ResultInfo info = result.getPlayersResult().get(defeatPlayer.getName());
+        ResultInfo info = result.getPlayersResult().get(defeatPlayer);
         assertThat(info).isEqualTo(ResultInfo.DEFEAT);
     }
 
     @Test
     void 딜러의_총합과_플레이어의_총합이_같으면_비긴다() {
-        BlackJackService blackJackService = new BlackJackService();
         Result result = blackJackService.calculateResult(dealer, players);
-        ResultInfo info = result.getPlayersResult().get(drawPlayer.getName());
+        ResultInfo info = result.getPlayersResult().get(drawPlayer);
         assertThat(info).isEqualTo(ResultInfo.DRAW);
+    }
+
+    @Test
+    void 먼저_버스트된_플레이어는_딜러의_결과에_관계없이_패배한다() {
+        Result result = blackJackService.calculateResult(dealer, players);
+        bustPlayer.keepCard(new Card(Rank.JACK, Pattern.CLOVER));
+        bustPlayer.keepCard(new Card(Rank.JACK, Pattern.SPADE));
+        bustPlayer.keepCard(new Card(Rank.JACK, Pattern.HEART));
+        dealer.keepCard(new Card(Rank.KING, Pattern.HEART));
+        dealer.keepCard(new Card(Rank.QUEEN, Pattern.HEART));
+
+        ResultInfo info=result.getPlayersResult().get(bustPlayer);
+        assertThat(info).isEqualTo(ResultInfo.DEFEAT);
+    }
+
+    @Test
+    void 딜러가_버스트시_딜러보다_낮은_점수의_플레이어들은_무조건_승리한다(){
+        dealer.keepCard(new Card(Rank.KING, Pattern.HEART));
+        dealer.keepCard(new Card(Rank.QUEEN, Pattern.HEART));
+        Result result = blackJackService.calculateResult(dealer, players);
+
+        ResultInfo info=result.getPlayersResult().get(defeatPlayer);
+        assertThat(info).isEqualTo(ResultInfo.WIN);
     }
 }
