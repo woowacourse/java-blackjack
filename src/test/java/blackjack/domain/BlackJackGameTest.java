54ecbd6fb91cf545bddb7a6b8e5cc5c60f2e542c
Chaerin Lee <rin@Rin-MacBook-Air.local>
2026-03-11 19:14:21 +0900

    refactor: 테스트 코드 수정

--- src/test/java/blackjack/domain/BlackJackGameTest.java
+++ src/test/java/blackjack/domain/BlackJackGameTest.java
@@ -3,6 +3,7 @@ package blackjack.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 public class BlackJackGameTest {
@@ -10,7 +11,8 @@ public class BlackJackGameTest {
     @Test
     void 게임이_시작되면_참여자_모두_카드를_2장씩_받는다() {
         // arrange
-        Players players = new Players(List.of("이산", "모카"));
+        List<String> names = List.of("이산", "모카", "바니", "소낙눈");
+        Players players = new Players(names);
         Dealer dealer = new Dealer();
         Deck deck = new Deck();
         BlackJackGame blackJackGame = new BlackJackGame(players, dealer, deck);
@@ -19,7 +21,10 @@ public class BlackJackGameTest {
         blackJackGame.initDraw();
 
         // assert
-        assertThat(deck.getCount()).isEqualTo(46);
+        List<Participant> allParticipants = Stream.concat(
+                Stream.of(dealer), players.getPlayers().stream()
+        ).toList();
+        assertThat(allParticipants).allSatisfy(participant ->
+                assertThat(participant.getCards()).hasSize(2));
     }
-
 }
