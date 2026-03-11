8d589efb16a466d625d8115981066ad2f46af3d3
EveryPine <kenton.lim@gmail.com>
2026-03-11 19:26:16 +0900

    test(game-result): 블랙잭 게임 결과 계산 테스트 추가

--- src/test/java/blackjack/domain/GameResultTest.java
+++ src/test/java/blackjack/domain/GameResultTest.java
@@ -11,8 +11,126 @@ import org.junit.jupiter.api.Test;
 class GameResultTest {
 
     @Nested
-    @DisplayName("승패 판정 테스트")
-    class 승패_판정_테스트 {
+    @DisplayName("게임 결과 판정 테스트")
+    class 게임_결과_판정_테스트 {
+
+        @Test
+        @DisplayName("딜러 블랙잭, 플레이어 블랙잭")
+        void 딜러_블랙잭_플레이어_블랙잭() {
+            Trump trump = new Trump();
+            Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.ACE),
+                new Card(Suit.SPADE, Denomination.KING)));
+            Dealer dealer = new Dealer(dealerHand, Status.STAY, trump);
+            Hand playerHand = new Hand(List.of(
+                new Card(Suit.SPADE, Denomination.ACE),
+                new Card(Suit.HEART, Denomination.KING)));
+            Player player = new Player(playerHand, Status.STAY, "pobi");
+            GameResult expected = GameResult.DRAW;
+
+            GameResult actual = GameResult.calculate(player, dealer);
+
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("딜러 블랙잭, 플레이어 스테이")
+        void 딜러_블랙잭_플레이어_스테이() {
+            Trump trump = new Trump();
+            Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.ACE),
+                new Card(Suit.SPADE, Denomination.KING)));
+            Dealer dealer = new Dealer(dealerHand, Status.STAY, trump);
+            Hand playerHand = new Hand(List.of(
+                new Card(Suit.SPADE, Denomination.ACE),
+                new Card(Suit.HEART, Denomination.EIGHT)));
+            Player player = new Player(playerHand, Status.STAY, "pobi");
+            GameResult expected = GameResult.LOSE;
+
+            GameResult actual = GameResult.calculate(player, dealer);
+
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("딜러 블랙잭, 플레이어 버스트")
+        void 딜러_블랙잭_플레이어_버스트() {
+            Trump trump = new Trump();
+            Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.ACE),
+                new Card(Suit.SPADE, Denomination.KING)));
+            Dealer dealer = new Dealer(dealerHand, Status.STAY, trump);
+            Hand playerHand = new Hand(List.of(
+                new Card(Suit.SPADE, Denomination.TWO),
+                new Card(Suit.HEART, Denomination.KING),
+                new Card(Suit.SPADE, Denomination.FIVE),
+                new Card(Suit.HEART, Denomination.FIVE)));
+            Player player = new Player(playerHand, Status.BURST, "pobi");
+            GameResult expected = GameResult.LOSE;
+
+            GameResult actual = GameResult.calculate(player, dealer);
+
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("딜러 스테이, 플레이어 블랙잭")
+        void 딜러_스테이_플레이어_블랙잭() {
+            Trump trump = new Trump();
+            Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.KING),
+                new Card(Suit.SPADE, Denomination.KING)));
+            Dealer dealer = new Dealer(dealerHand, Status.STAY, trump);
+            Hand playerHand = new Hand(List.of(
+                new Card(Suit.SPADE, Denomination.ACE),
+                new Card(Suit.HEART, Denomination.KING)));
+            Player player = new Player(playerHand, Status.STAY, "pobi");
+            GameResult expected = GameResult.BLACKJACK;
+
+            GameResult actual = GameResult.calculate(player, dealer);
+
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("딜러 스테이, 플레이어 버스트")
+        void 딜러_스테이_플레이어_버스트() {
+            Trump trump = new Trump();
+            Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.TEN),
+                new Card(Suit.SPADE, Denomination.TEN)));
+            Dealer dealer = new Dealer(dealerHand, Status.STAY, trump);
+            Hand playerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.TEN),
+                new Card(Suit.SPADE, Denomination.SIX),
+                new Card(Suit.DIAMOND, Denomination.KING)));
+            Player player = new Player(playerHand, Status.BURST, "pobi");
+            GameResult expected = GameResult.LOSE;
+
+            GameResult actual = GameResult.calculate(player, dealer);
+
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("딜러 버스트, 플레이어 블랙잭")
+        void 딜러_버스트_플레이어_블랙잭() {
+            Trump trump = new Trump();
+            Hand dealerHand = new Hand(List.of(
+                new Card(Suit.DIAMOND, Denomination.THREE),
+                new Card(Suit.SPADE, Denomination.KING),
+                new Card(Suit.SPADE, Denomination.NINE)));
+            Dealer dealer = new Dealer(dealerHand, Status.BURST, trump);
+            Hand playerHand = new Hand(List.of(
+                new Card(Suit.SPADE, Denomination.ACE),
+                new Card(Suit.HEART, Denomination.KING)));
+            Player player = new Player(playerHand, Status.STAY, "pobi");
+            GameResult expected = GameResult.BLACKJACK;
+
+            GameResult actual = GameResult.calculate(player, dealer);
+
+            assertThat(actual).isEqualTo(expected);
+        }
 
         @Test
         @DisplayName("딜러 버스트, 플레이어 스테이")
@@ -43,26 +161,6 @@ class GameResultTest {
                 new Card(Suit.SPADE, Denomination.SIX),
                 new Card(Suit.DIAMOND, Denomination.KING)));
             Dealer dealer = new Dealer(dealerHand, Status.BURST, trump);
-            Hand playerHand = new Hand(List.of(
-                new Card(Suit.DIAMOND, Denomination.TEN),
-                new Card(Suit.SPADE, Denomination.SIX),
-                new Card(Suit.DIAMOND, Denomination.KING)));
-            Player player = new Player(playerHand, Status.BURST, "pobi");
-            GameResult expected = GameResult.LOSE;
-
-            GameResult actual = GameResult.calculate(player, dealer);
-
-            assertThat(actual).isEqualTo(expected);
-        }
-
-        @Test
-        @DisplayName("딜러 스테이, 플레이어 버스트")
-        void 딜러_스테이_플레이어_버스트() {
-            Trump trump = new Trump();
-            Hand dealerHand = new Hand(List.of(
-                new Card(Suit.DIAMOND, Denomination.TEN),
-                new Card(Suit.SPADE, Denomination.TEN)));
-            Dealer dealer = new Dealer(dealerHand, Status.STAY, trump);
             Hand playerHand = new Hand(List.of(
                 new Card(Suit.DIAMOND, Denomination.TEN),
                 new Card(Suit.SPADE, Denomination.SIX),
