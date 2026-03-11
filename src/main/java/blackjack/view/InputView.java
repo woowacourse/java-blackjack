2dc6d4d42ab807782d339f652d62981d4b7f15d0
Chaerin Lee <rin@Rin-MacBook-Air.local>
2026-03-11 17:44:48 +0900

    refactor: Scanner private final로 수정

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    Scanner sc = new Scanner(System.in);
+    private final Scanner sc = new Scanner(System.in);
 
     public List<String> readNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
