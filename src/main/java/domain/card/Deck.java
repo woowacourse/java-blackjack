4156883bef73888494bcebfbfd844572a96b42c5
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-03-11 11:14:40 +0900

    fix(Deck): drawCard()에서 idx 범위 오류 버그 해결

---
+++ src/main/java/domain/card/Deck.java
@@ -0,0 +1,47 @@
+package domain.card;
+
+import domain.RandomValueGenerator;
+import java.util.HashSet;
+import java.util.List;
+import util.ErrorMessage;
+
+public class Deck {
+    public static final int CARD_SIZE = 52;
+
+    private final List<Card> cards;
+    private final RandomValueGenerator randomValueGenerator;
+
+    public Deck(List<Card> cards, RandomValueGenerator randomValueGenerator) {
+        validateSize(cards);
+        validateDuplicate(cards);
+        this.cards = cards;
+        this.randomValueGenerator = randomValueGenerator;
+    }
+
+    private void validateSize(List<Card> cards) {
+        if (cards.size() != CARD_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.DECK_SIZE.getMessage());
+        }
+    }
+
+    private void validateDuplicate(List<Card> cards) {
+        if (new HashSet<>(cards).size() != cards.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DECK_DUPLICATE.getMessage());
+        }
+    }
+
+    public Card drawCard() {
+        int idx = randomValueGenerator.generate(cards.size());
+
+        Card card = cards.get(idx);
+        cards.remove(idx);
+
+        return card;
+    }
+
+    public boolean contains(Card card) {
+        return cards.contains(card);
+    }
+
+
+}
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -4,9 +4,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import domain.RandomValueGenerator;
 import domain.RandomValueGeneratorImpl;
 import domain.card.Card;
+import domain.card.Deck;
 import domain.card.Rank;
 import domain.card.Suit;
-import domain.player.Deck;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import util.ErrorMessage;
 
@@ -50,40 +49,23 @@ class DeckTest {
 
     }
 
-    @Nested
-    @DisplayName("passCard(): ")
-    class PassCard {
 
-        public static Stream<Arguments> drawCard() {
-            return Stream.of(
-                    Arguments.of(0, TestDefaults.getCard(0)),
-                    Arguments.of(2, TestDefaults.getCard(2)),
-                    Arguments.of(51, TestDefaults.getCard(51))
-            );
-        }
+    public static Stream<Arguments> drawCard() {
+        return Stream.of(
+                Arguments.of(0, TestDefaults.getCard(0)),
+                Arguments.of(2, TestDefaults.getCard(2)),
+                Arguments.of(51, TestDefaults.getCard(51))
+        );
+    }
 
-        @DisplayName("인덱스에 맞는 카드를 나눠준다.")
-        @ParameterizedTest
-        @MethodSource
-        void drawCard(int idx, Card expected) {
-            Deck deck = TestDefaults.createDeck(bound -> idx);
+    @DisplayName("인덱스에 맞는 카드를 나눠준다.")
+    @ParameterizedTest
+    @MethodSource
+    void drawCard(int idx, Card expected) {
+        Deck deck = TestDefaults.createDeck(bound -> idx);
 
-            assertThat(deck.drawCard()).isEqualTo(expected);
-            assertThat(deck.contains(expected)).isFalse();
-        }
-
-        @DisplayName("[예외] - cards의 사이즈 이상의 값이 들어오면 예외를 반환한다.")
-        @ParameterizedTest
-        @CsvSource({
-                "-1",
-                "53"
-        })
-        void invalidSize(int idx) {
-            Deck deck = TestDefaults.createDeck(bound -> idx);
-            assertThatThrownBy(deck::drawCard)
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage(ErrorMessage.INDEX_RANGE.getMessage());
-        }
+        assertThat(deck.drawCard()).isEqualTo(expected);
+        assertThat(deck.contains(expected)).isFalse();
     }
 
 
