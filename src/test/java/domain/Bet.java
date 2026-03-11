e0bf9cb267225903adba3c7fa96d7a369fe50398
MODUGGAGI <kiroro0814@naver.com>
2026-03-11 14:19:33 +0900

    feat(calculate-profit): 승리 시 수익금 계산 구현
    플레이어가 승리시 얻게 되는 수익금을 계산하는 로직을 구현하고 그에 따라 테스트를 추가했습니다.

--- src/test/java/domain/Bet.java
+++ src/test/java/domain/Bet.java
@@ -1,5 +1,6 @@
 package domain;
 
+import domain.enums.GameResult;
 import domain.participant.Name;
 import java.util.HashMap;
 import java.util.List;
@@ -8,30 +9,40 @@ import java.util.Map;
 public class Bet {
 
     private final Map<Name, Integer> betHistory = new HashMap<>();
+    private final Map<Name, Integer> betProfit = new HashMap<>();
 
     public Bet(List<Name> playerNames) {
         playerNames.forEach(
-                playerName -> betHistory.put(playerName, 0)
+                playerName -> {
+                    betHistory.put(playerName, 0);
+                    betProfit.put(playerName, 0);
+                }
         );
     }
 
-    public void bettingMoney(Name firstPlayer, int bettingMoney) {
-        validatePlayer(firstPlayer);
+    public void bettingMoney(Name playerName, int bettingMoney) {
+        Name foundPlayer = validatePlayer(playerName);
         validateBettingMoney(bettingMoney);
+        betHistory.put(playerName, bettingMoney);
     }
 
-    private void validatePlayer(Name firstPlayer) {
-        boolean notMatch = betHistory.keySet().stream()
-                .noneMatch(name -> name.equals(firstPlayer));
-
-        if (notMatch) {
-            throw new IllegalArgumentException("[ERROR] 게임에 참여한 플레이어만 배팅이 가능합니다.");
-        }
+    private Name validatePlayer(Name playerName) {
+        return betHistory.keySet().stream()
+                .filter(name -> name.equals(playerName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 게임에 참여한 플레이어만 배팅이 가능합니다."));
     }
 
     private void validateBettingMoney(int bettingMoney) {
         if (bettingMoney <= 0) {
             throw new IllegalArgumentException("[ERROR] 배팅 금액은 양수값만 가능합니다.");
         }
+    }
+
+    public int calculateProfit(Name firstPlayer, GameResult gameResult) {
+        Name foundPlayer = validatePlayer(firstPlayer);
+        int betAmount = betHistory.get(foundPlayer);
+        betProfit.put(foundPlayer, betAmount);
+        return betAmount;
     }
 }
--- src/test/java/domain/BetTest.java
+++ src/test/java/domain/BetTest.java
@@ -1,7 +1,9 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import domain.enums.GameResult;
 import domain.participant.Name;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,7 +28,6 @@ public class BetTest {
     @DisplayName("게임에 참여한 플레이어만 값을 배팅할 수 없다.")
     @Test
     void 플레이어_금액_배팅_테스트() {
-
         Name unknownPlayer = new Name("신원미상");
 
         assertThatThrownBy(() -> bet.bettingMoney(unknownPlayer, 10_000))
@@ -41,5 +42,16 @@ public class BetTest {
         assertThatThrownBy(() -> bet.bettingMoney(firstPlayer, bettingMoney))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 배팅 금액은 양수값만 가능합니다.");
+    }
+
+    @DisplayName("플레이어 승리 시 원금 + 1배의 수익")
+    @Test
+    void 플레이어_승리_시_1배의_수익() {
+        //given
+        //when
+        bet.bettingMoney(firstPlayer, 10_000);
+        int profit = bet.calculateProfit(firstPlayer, GameResult.WIN);
+        //then
+        assertThat(profit).isEqualTo(10_000);
     }
 }
