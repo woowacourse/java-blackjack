6f28201ce17bde59484d8b5ded929ec9fc292900
최용준 <yj9107vv@gmail.com>
2026-03-11 15:20:34 +0900

    test(BlackjackResultTest): 플레이어/딜러 승패 여부 테스트 추가

--- src/test/java/domain/BlackjackResultTest.java
+++ src/test/java/domain/BlackjackResultTest.java
@@ -2,24 +2,38 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BlackjackResultTest {
 
+    private final Cards deck = Cards.of();
+    private final List<Card> initialCards = new ArrayList<>(List.of(
+            Card.of(Rank.ACE, Suit.DIAMOND),
+            Card.of(Rank.FOUR, Suit.CLOVER)));
+
     private final List<String> userNames = List.of("pobi", "jason");
+    private final Players players = Players.of(deck, userNames);
+    private final Dealer dealer = Dealer.of(initialCards);
+    private final BlackjackResult blackjackResult = BlackjackResult.of(dealer, players);
 
     @Test
     @DisplayName("BlackjackResult 객체 생성 시 플레이어들의 승패 저장 확인")
     void player_result_test() {
-        Cards cards = Cards.of();
-        Dealer dealer = Dealer.of(cards.drawInitialHand());
-        Players players = Players.of(cards, userNames);
+        assertThat(blackjackResult.getPlayersResult()).hasSize(userNames.size());
+    }
 
-        BlackjackResult result = BlackjackResult.of(dealer, players);
+    @Test
+    @DisplayName("플레이어들 승리 결과, 딜러는 플레이어 승/패 횟수의 반대 결과")
+    void player_isBust_result_lose() {
+        assertThat(blackjackResult.getPlayersResult()).isEqualTo(List.of(WinOrLose.LOSE, WinOrLose.LOSE));
+    }
 
-        int expect = userNames.size();
-        assertThat(result.getPlayersResult()).hasSize(expect);
+    @Test
+    @DisplayName("딜러의 결과는 플레이어들 승/패 여부 횟수의 반대이다.")
+    void dealer_result() {
+        assertThat(blackjackResult.countDealerWinOrLoseReversePlayerResult(WinOrLose.LOSE)).isEqualTo(2);
     }
 }
