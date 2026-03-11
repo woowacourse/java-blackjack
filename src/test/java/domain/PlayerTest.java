d6b59dd83a0db0c96dfe4a44437b387d350d4c62
EveryPine <kenton.lim@gmail.com>
2026-03-11 17:37:26 +0900

    test(player): 베팅 테스트 추가

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,8 +1,10 @@
 package domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import blackjack.domain.Bet;
 import blackjack.domain.Hand;
 import blackjack.domain.Player;
 import blackjack.domain.Status;
@@ -37,6 +39,40 @@ public class PlayerTest {
             assertThatThrownBy(() ->  new Player(new Hand(new ArrayList<>()), Status.HIT, "jasonjasonj"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("닉네임은 10자 이하여야 합니다.");
+        }
+    }
+
+    @Nested
+    @DisplayName("베팅 테스트")
+    class 베팅_테스트 {
+
+        @Test
+        @DisplayName("정상 테스트")
+        void 정상_테스트() {
+            Hand hand = new Hand(new ArrayList<>());
+            Status status = Status.HIT;
+            Player player = new Player(hand, status, "pobi");
+            Bet expected = new Bet(10000);
+
+            player.bet(expected);
+
+            assertThat(player).extracting("bet")
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("이미 베팅금을 입력한 플레이어인 경우")
+        void 이미_베팅금을_입력한_플레이어인_경우() {
+            Hand hand = new Hand(new ArrayList<>());
+            Status status = Status.HIT;
+            Player player = new Player(hand, status, "pobi");
+            Bet bet = new Bet(10000);
+            player.bet(bet);
+
+            assertThatThrownBy(() -> player.bet(bet))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("해당 플레이어는 이미 베팅금을 입력하였습니다.");
         }
     }
 }
