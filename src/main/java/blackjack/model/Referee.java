9aa14870c095e41c98a1df5fd78a54496d233ac1
smini <sm020320@naver.com>
2026-03-11 19:49:35 +0900

    feat: 블랙잭 및 카드 초과 여부에 따른 승패 판단 로직 추가

--- src/main/java/blackjack/model/Referee.java
+++ src/main/java/blackjack/model/Referee.java
@@ -6,15 +6,27 @@ public class Referee {
         Score playerScore = player.getScore();
         Score dealerScore = dealer.getScore();
 
+        if (player.isBlackjack() && dealer.isBlackjack()) {
+            return GameResult.DRAW;
+        }
         if (player.isBlackjack()) {
             return GameResult.BLACKJACK;
         }
-
-        if ((player.isBust() && dealer.isBust()) || (playerScore.isSame(dealerScore))) {
-            return GameResult.DRAW;
+        if (dealer.isBlackjack()) {
+            return GameResult.LOSE;
         }
 
-        if (player.isBust() || (!dealer.isBust() && (playerScore.isLess(dealerScore)))) {
+        if (player.isBust()) {
+            return GameResult.LOSE;
+        }
+        if (dealer.isBust()) {
+            return GameResult.WIN;
+        }
+
+        if (playerScore.isSame(dealerScore)) {
+            return GameResult.DRAW;
+        }
+        if (playerScore.isLess(dealerScore)) {
             return GameResult.LOSE;
         }
 
