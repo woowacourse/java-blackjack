cb4530761c767cd8ba7e0b835a53f819dc019d00
Chaerin Lee <rin@Rin-MacBook-Air.local>
2026-03-11 15:04:57 +0900

    test(Player): 승패 결정 책임을 사용자에게

--- src/test/java/blackjack/domain/PlayerTest.java
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -23,4 +23,77 @@ public class PlayerTest {
         assertThat(player.getCardCount()).isEqualTo(3);
     }
 
+    @Test
+    void 플레이어는_버스트로_패이다() {
+        Player player = new Player("이산");
+        player.receiveCard(new Card(CardPoint.KING, CardPattern.DIAMOND));
+        player.receiveCard(new Card(CardPoint.QUEEN, CardPattern.CLUB));
+        player.receiveCard(new Card(CardPoint.TWO, CardPattern.SPADE));
+
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(new Card(CardPoint.EIGHT, CardPattern.SPADE));
+        dealer.receiveCard(new Card(CardPoint.TEN, CardPattern.HEART));
+
+        GameResult result = player.compareResult(dealer);
+        assertThat(result).isEqualTo(GameResult.LOSE);
+    }
+
+    @Test
+    void 딜러의_버스트로_플레이어_승이다() {
+        Player player = new Player("이산");
+        player.receiveCard(new Card(CardPoint.KING, CardPattern.DIAMOND));
+        player.receiveCard(new Card(CardPoint.QUEEN, CardPattern.CLUB));
+
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(new Card(CardPoint.SIX, CardPattern.SPADE));
+        dealer.receiveCard(new Card(CardPoint.SEVEN, CardPattern.HEART));
+        dealer.receiveCard(new Card(CardPoint.NINE, CardPattern.CLUB));
+
+        GameResult result = player.compareResult(dealer);
+        assertThat(result).isEqualTo(GameResult.WIN);
+    }
+
+    @Test
+    void 딜러와_플레이어의_포인트를_비교한다_플레이어_승() {
+        Player player = new Player("이산");
+        player.receiveCard(new Card(CardPoint.KING, CardPattern.DIAMOND));
+        player.receiveCard(new Card(CardPoint.QUEEN, CardPattern.CLUB));
+
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(new Card(CardPoint.SEVEN, CardPattern.HEART));
+        dealer.receiveCard(new Card(CardPoint.ACE, CardPattern.CLUB));
+
+        GameResult result = player.compareResult(dealer);
+        assertThat(result).isEqualTo(GameResult.WIN);
+    }
+
+    @Test
+    void 딜러와_플레이어의_포인트를_비교한다_무승부() {
+        Player player = new Player("이산");
+        player.receiveCard(new Card(CardPoint.KING, CardPattern.DIAMOND));
+        player.receiveCard(new Card(CardPoint.QUEEN, CardPattern.CLUB));
+
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(new Card(CardPoint.SEVEN, CardPattern.HEART));
+        dealer.receiveCard(new Card(CardPoint.NINE, CardPattern.CLUB));
+        dealer.receiveCard(new Card(CardPoint.FOUR, CardPattern.DIAMOND));
+
+        GameResult result = player.compareResult(dealer);
+        assertThat(result).isEqualTo(GameResult.TIE);
+    }
+
+    @Test
+    void 딜러와_플레이어의_포인트를_비교한다_플레이어_패() {
+        Player player = new Player("이산");
+        player.receiveCard(new Card(CardPoint.KING, CardPattern.DIAMOND));
+        player.receiveCard(new Card(CardPoint.QUEEN, CardPattern.CLUB));
+
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(new Card(CardPoint.ACE, CardPattern.CLUB));
+        dealer.receiveCard(new Card(CardPoint.JACK, CardPattern.DIAMOND));
+
+        GameResult result = player.compareResult(dealer);
+        assertThat(result).isEqualTo(GameResult.LOSE);
+    }
+
 }
