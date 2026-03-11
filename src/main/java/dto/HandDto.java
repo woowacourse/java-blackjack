0dbdca84f8901029a76e466d850b2037801415ab
toctoce <toctoce@naver.com>
2026-03-11 23:03:50 +0900

    feat(HandDto): 다양한 생성자 구현

--- src/main/java/dto/HandDto.java
+++ src/main/java/dto/HandDto.java
@@ -6,6 +6,17 @@ import java.util.List;
 
 public record HandDto(List<String> cards) {
 
+    public static HandDto from(Card card) {
+        return new HandDto(List.of(card.getCardName()));
+    }
+
+    public static HandDto from(List<Card> cards) {
+        List<String> transformedCards = cards.stream()
+                .map(Card::getCardName)
+                .toList();
+        return new HandDto(transformedCards);
+    }
+
     public static HandDto from(Hand hand) {
         List<String> cards = hand.getCards().stream()
                 .map(Card::getCardName)
