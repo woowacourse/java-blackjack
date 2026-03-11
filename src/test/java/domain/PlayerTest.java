e72a971eda064416ffb78c0ee9033d423602d8f0
xxbeann <yjb4945@gmail.com>
2026-03-12 00:01:53 +0900

    test: isWin, isBurst 테스트 추가

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -15,7 +15,9 @@ class PlayerTest {
         player.receiveCard(new Card(Shape.HEART, Number.JACK));
         player.receiveCard(new Card(Shape.DIAMOND, Number.TWO));
 
-        assertEquals(true, player.isBurst());
+        boolean result = player.isBurst();
+
+        assertEquals(true, result);
     }
 
     @Test
@@ -26,11 +28,94 @@ class PlayerTest {
         player.receiveCard(new Card(Shape.HEART, Number.JACK));
         player.receiveCard(new Card(Shape.DIAMOND, Number.TWO));
 
-        assertEquals(false, player.isBurst());
+        boolean result = player.isBurst();
+
+        assertEquals(false, result);
     }
 
     @Test
-    @DisplayName("")
-    void isWin() {
+    @DisplayName("둘 다 버스트되지 않고, 플레이어 점수가 딜러 점수보다 높으면 플레이어가 승리한다.")
+    void isWin_PlayerScoreHigher_ReturnTrue() {
+        Player player = new Player(new Name("pobi"));
+        Dealer dealer = new Dealer(new Name("딜러"));
+
+        player.receiveCard(new Card(Shape.SPADE, Number.TEN));
+        player.receiveCard(new Card(Shape.HEART, Number.JACK));
+
+        dealer.receiveCard(new Card(Shape.DIAMOND, Number.TEN));
+        dealer.receiveCard(new Card(Shape.CLUB, Number.EIGHT));
+
+        boolean result = player.isWin(dealer);
+
+        assertEquals(true, result);
+    }
+
+    @Test
+    @DisplayName("둘 다 버스트되지 않고, 플레이어 점수와 딜러 점수가 같으면 플레이어가 승리한다.")
+    void isWin_PlayerScoreSame_ReturnTrue() {
+        Player player = new Player(new Name("pobi"));
+        Dealer dealer = new Dealer(new Name("딜러"));
+
+        player.receiveCard(new Card(Shape.SPADE, Number.TEN));
+        player.receiveCard(new Card(Shape.HEART, Number.JACK));
+
+        dealer.receiveCard(new Card(Shape.DIAMOND, Number.TEN));
+        dealer.receiveCard(new Card(Shape.CLUB, Number.JACK));
+
+        boolean result = player.isWin(dealer);
+
+        assertEquals(true, result);
+    }
+
+    @Test
+    @DisplayName("둘 다 버스트되지 않고, 플레이어 점수보다 딜러 점수가 높으면 플레이어가 패배한다.")
+    void isWin_DealerScoreHigher_ReturnFalse() {
+        Player player = new Player(new Name("pobi"));
+        Dealer dealer = new Dealer(new Name("딜러"));
+
+        player.receiveCard(new Card(Shape.SPADE, Number.TEN));
+
+        dealer.receiveCard(new Card(Shape.DIAMOND, Number.TEN));
+        dealer.receiveCard(new Card(Shape.CLUB, Number.JACK));
+
+        boolean result = player.isWin(dealer);
+
+        assertEquals(false, result);
+    }
+
+    @Test
+    @DisplayName("플레이어가 버스트 되면, 딜러 점수 상관없이 패배한다.")
+    void isWin_PlayerBurst_ReturnFalse() {
+        Player player = new Player(new Name("pobi"));
+        Dealer dealer = new Dealer(new Name("딜러"));
+
+        player.receiveCard(new Card(Shape.SPADE, Number.TWO));
+        player.receiveCard(new Card(Shape.HEART, Number.JACK));
+        player.receiveCard(new Card(Shape.CLUB, Number.JACK));
+
+        dealer.receiveCard(new Card(Shape.DIAMOND, Number.TEN));
+        dealer.receiveCard(new Card(Shape.CLUB, Number.EIGHT));
+
+        boolean result = player.isWin(dealer);
+
+        assertEquals(false, result);
+    }
+
+    @Test
+    @DisplayName("플레이어가 버스트 되지 않을 때, 딜러가 버스트되면 플레이어가 승리한다.")
+    void isWin_DealerBurst_ReturnTrue() {
+        Player player = new Player(new Name("pobi"));
+        Dealer dealer = new Dealer(new Name("딜러"));
+
+        player.receiveCard(new Card(Shape.SPADE, Number.TEN));
+        player.receiveCard(new Card(Shape.HEART, Number.JACK));
+
+        dealer.receiveCard(new Card(Shape.DIAMOND, Number.TEN));
+        dealer.receiveCard(new Card(Shape.CLUB, Number.TWO));
+        dealer.receiveCard(new Card(Shape.HEART, Number.TEN));
+
+        boolean result = player.isWin(dealer);
+
+        assertEquals(true, result);
     }
 }
