069df475a55c91b92608e2c8f99dbfbecf201b4d
armd482 <armd482@naver.com>
2026-03-11 16:17:24 +0900

    test: 배팅 금액 테스트 코드 추가

---
+++ src/test/java/model/TestBetPrice.java
@@ -0,0 +1,23 @@
+package model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import constant.ErrorMessage;
+import org.junit.jupiter.api.Test;
+
+public class TestBetPrice {
+    @Test
+    public void 정상_작동() {
+        BetPrice betPrice = new BetPrice(10000);
+        assertThat(betPrice.value()).isEqualTo(10000);
+    }
+
+    @Test
+    public void 음수_및_제로_입력_예외() {
+        assertThatThrownBy(() -> new BetPrice(0)).isExactlyInstanceOf(IllegalArgumentException.class).hasMessage(
+                ErrorMessage.NO_NEGATIVE_BET.getMessage());
+        assertThatThrownBy(() -> new BetPrice(-10000)).isExactlyInstanceOf(IllegalArgumentException.class).hasMessage(
+                ErrorMessage.NO_NEGATIVE_BET.getMessage());
+    }
+}
