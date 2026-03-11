996a1390c73f5825951c0b2f8f18763a37c9004d
sauter001 <lsj457@naver.com>
2026-03-11 11:27:00 +0900

    feat: MatchCase에 블랙잭(BLACKJACK_WIN) 및 수익률 추가

--- src/main/java/domain/result/MatchCase.java
+++ src/main/java/domain/result/MatchCase.java
@@ -3,20 +3,27 @@ package domain.result;
 import java.util.function.Consumer;
 
 public enum MatchCase {
-    WIN("승", BlackjackResult::increaseDealerLoseCount),
-    LOSE("패", BlackjackResult::increaseDealerWinCount),
-    DRAW("무", BlackjackResult::increaseDrawCount);
+    BLACKJACK_WIN("승", 1.5, BlackjackResult::increaseDealerLoseCount),
+    WIN("승", 1, BlackjackResult::increaseDealerLoseCount),
+    LOSE("패", -1, BlackjackResult::increaseDealerWinCount),
+    DRAW("무", 0, BlackjackResult::increaseDrawCount);
 
     private final String korDisplayName;
+    private final double benefitRate;
     private final Consumer<BlackjackResult> counter;
 
-    MatchCase(String korDisplayName,  Consumer<BlackjackResult> counter) {
+    MatchCase(String korDisplayName, double benefitRate, Consumer<BlackjackResult> counter) {
         this.korDisplayName = korDisplayName;
+        this.benefitRate = benefitRate;
         this.counter = counter;
     }
 
     public String getKorDisplayName() {
         return korDisplayName;
+    }
+
+    public double getBenefitRate() {
+        return benefitRate;
     }
 
     public void increaseMatchCountOf(BlackjackResult blackjackResult) {
