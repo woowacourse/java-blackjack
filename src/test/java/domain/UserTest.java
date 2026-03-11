b675b978307be267799fe20976a578a8e55272ee
dahye <dahye9011@naver.com>
2026-03-12 00:33:29 +0900

    test: 테스트 케이스 추가

--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -3,10 +3,13 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import vo.GameResult;
 import vo.Money;
+import vo.Rank;
+import vo.Suit;
 
 public class UserTest {
     @ParameterizedTest
@@ -27,5 +30,30 @@ public class UserTest {
 
         // then
         assertThat(profit).isEqualTo(new Money(expectedAmount));
+    }
+
+    @Test
+    @DisplayName("처음 받은 두 장의 카드 합이 21점인 경우, 블랙잭이다.")
+    void isBlackjack_True() {
+        // given
+        User user = new User("라이", new Money(10000));
+        user.receiveCard(new Card(Suit.SPADE, Rank.ACE));
+        user.receiveCard(new Card(Suit.HEART, Rank.JACK));
+
+        // when & then
+        assertThat(user.isBlackjack()).isTrue();
+    }
+
+    @Test
+    @DisplayName("카드가 3장 이상이고, 합이 21점인 경우, 블랙잭이 아니다.")
+    void isBlackjack_False_With3Cards() {
+        // given
+        User user = new User("라이", new Money(10000));
+        user.receiveCard(new Card(Suit.SPADE, Rank.SEVEN));
+        user.receiveCard(new Card(Suit.HEART, Rank.SEVEN));
+        user.receiveCard(new Card(Suit.CLUB, Rank.SEVEN));
+
+        // when & then
+        assertThat(user.isBlackjack()).isFalse();
     }
 }
