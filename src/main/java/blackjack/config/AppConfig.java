b83ef09f475c6185e3a7399cedd22543fabedec8
kangrae <kangrae0501@gmail.com>
2026-03-11 10:42:53 +0900

    refactor: OutputView도 객체로써 관리되도록 변경
    - outputView를 지닌 객체만 출력이 가능하게하기 위함

--- src/main/java/blackjack/config/AppConfig.java
+++ src/main/java/blackjack/config/AppConfig.java
@@ -2,18 +2,20 @@ package blackjack.config;
 
 import blackjack.controller.BlackjackController;
 import blackjack.view.InputView;
+import blackjack.view.OutputView;
 
 public class AppConfig {
 
     private BlackjackController controller;
     private InputView inputView;
+    private OutputView outputView;
 
     public AppConfig() {
     }
 
     public BlackjackController controller() {
         if (controller == null) {
-            this.controller = new BlackjackController(inputView());
+            this.controller = new BlackjackController(inputView(), outputView());
         }
         return controller;
     }
@@ -23,6 +25,13 @@ public class AppConfig {
             this.inputView = new InputView();
         }
         return inputView;
+    }
+
+    private OutputView outputView() {
+        if (outputView == null) {
+            this.outputView = new OutputView();
+        }
+        return outputView;
     }
 
 }
