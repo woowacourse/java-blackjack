77c0ced39ae32d25844fb90c3acef250b2b1c2a3
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-03-11 14:56:26 +0900

    feat: 베팅테이블 기능 추가

---
+++ src/main/java/domain/bet/BetTable.java
@@ -0,0 +1,29 @@
+package domain.bet;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BetTable {
+    private final Map<String, Integer> bettingTable;
+
+    public BetTable() {
+        this.bettingTable = new HashMap<>();
+    }
+
+    public void placeBet(String name, int amount) {
+        bettingTable.put(name, amount);
+    }
+
+    public int getAmountByName(String name) {
+        return bettingTable.get(name);
+    }
+
+    public List<Integer> getAmountByPlayers() {
+        return List.copyOf(bettingTable.values().stream().toList());
+    }
+
+    public void clearBet(){
+        bettingTable.clear();
+    }
+}
