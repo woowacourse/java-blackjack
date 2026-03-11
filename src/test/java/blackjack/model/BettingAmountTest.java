254aae29078da72b2696e2fefc32145d537427d2
smini <sm020320@naver.com>
2026-03-11 11:24:05 +0900

    test: BettingAmount 테스트 코드 추가

---
+++ src/test/java/blackjack/model/BettingAmountTest.java
@@ -0,0 +1,45 @@
+package blackjack.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BettingAmountTest {
+
+    @Test
+    @DisplayName("플레이어가 패배하면 배팅 금액을 잃는다.")
+    void loseAmountTest() {
+        // given
+        Player player = new Player("luke", 1000);
+        // when & then
+        assertThat(player.getBettingAmount(GameResult.LOSE)).isEqualTo(-1000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 승리하면 배팅 금액을 잃지 않는다.")
+    void winAmountTest() {
+        // given
+        Player player = new Player("luke", 1000);
+        // when & then
+        assertThat(player.getBettingAmount(GameResult.WIN)).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 무승부면 배팅 금액을 유지된다.")
+    void drawAmountTest() {
+        // given
+        Player player = new Player("luke", 1000);
+        // when & then
+        assertThat(player.getBettingAmount(GameResult.DRAW)).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("플레이어가 블랙잭이면 배팅 금액의 1.5배를 얻는다.")
+    void blackjackAmountTest() {
+        // given
+        Player player = new Player("luke", 1000);
+        // when & then
+        assertThat(player.getBettingAmount(GameResult.BLACK_JACK)).isEqualTo(1500);
+    }
+}
