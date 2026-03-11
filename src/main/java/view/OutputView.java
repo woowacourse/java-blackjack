e1188ee6ef1883ec4a65fcb7133d8ccf9f6f3b56
khcho96 <khcho1492@gmail.com>
2026-03-12 08:26:43 +0900

    refactor(OutputView): DELIMITER 상수 설정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,5 @@
 package view;
 
-import constant.PolicyConstant;
 import constant.Result;
 import dto.BlackjackStatisticsDto;
 import dto.DealerResultDto;
@@ -10,6 +9,7 @@ import java.util.List;
 
 public class OutputView {
 
+    private static final String DELIMITER = ", ";
     private static final String PRINT_PLAYERS_MESSAGE = "\n딜러와 %s에게 2장을 나누었습니다.\n";
     private static final String PRINT_HAND_MESSAGE = "%s카드: %s";
     private static final String PRINT_DEALER_HIT = "\n딜러는 16이하라 한장의 카드를 더 받았습니다.";
@@ -23,7 +23,7 @@ public class OutputView {
             .map(ParticipantDto::name)
             .filter(name -> !name.equals(PolicyConstant.DEALER_NAME))
             .toList();
-        System.out.printf(PRINT_PLAYERS_MESSAGE, String.join(PolicyConstant.DELIMITER + " ", names));
+        System.out.printf(PRINT_PLAYERS_MESSAGE, String.join(DELIMITER + " ", names));
     }
 
     public void printHandList(List<ParticipantDto> participantDtoList) {
@@ -34,7 +34,7 @@ public class OutputView {
     }
 
     public void printHand(String name, List<String> hand) {
-        System.out.printf(PRINT_HAND_MESSAGE, name, String.join(PolicyConstant.DELIMITER + " ", hand));
+        System.out.printf(PRINT_HAND_MESSAGE, name, String.join(DELIMITER + " ", hand));
     }
 
     public void printlnHand(String name, List<String> hand) {
