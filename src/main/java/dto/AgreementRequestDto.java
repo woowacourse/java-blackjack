d8dc26f6df11a70cf0086ebfe431920107b05616
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-11 17:40:19 +0900

    refactor: AgreementRequestDto null/공백 입력 함수 이름 변경

--- src/main/java/dto/AgreementRequestDto.java
+++ src/main/java/dto/AgreementRequestDto.java
@@ -3,11 +3,11 @@ package dto;
 public record AgreementRequestDto(String agreement) {
 
     public AgreementRequestDto {
-        validateNameIsNotNullAndIsNotBlank(agreement);
+        validateAgreementIsNotNullAndNotBlank(agreement);
         validateAgreementIsYOrN(agreement);
     }
 
-    private void validateNameIsNotNullAndIsNotBlank(String agreement) {
+    private void validateAgreementIsNotNullAndNotBlank(String agreement) {
         if (agreement == null || agreement.isBlank()) {
             throw new IllegalArgumentException("y/n 입력은 null 또는 공백이면 안됩니다");
         }
