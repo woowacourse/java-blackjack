add2b32ce7750f8174986b3f7797824a5408bd8e
wontop02 <wjy0502kr@naver.com>
2026-03-11 23:14:57 +0900

    feat(view): 배팅 금액 요청하는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,6 +9,7 @@ import message.ErrorMessage;
 
 public class InputView {
     private static final String ENTER_PLAYER_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String ENTER_PLAYER_BET_AMOUNT = "%s의 배팅 금액은?";
     private static final String ASK_HIT_OR_NOT = "%s는 한장의 카드를 더 받겠습니까?(예: %s, 아니오: %s)";
 
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -18,6 +19,11 @@ public class InputView {
 
     public static String askPlayerNames() {
         System.out.println(ENTER_PLAYER_NAME);
+        return SCANNER.nextLine();
+    }
+
+    public static String askBetAmount(String name) {
+        System.out.printf(ENTER_PLAYER_BET_AMOUNT + LINE_SEPARATOR, name);
         return SCANNER.nextLine();
     }
 
