ddb054dfdbc5dfb44b4bd4fde92e9bbce6eeca0c
wontop02 <wjy0502kr@naver.com>
2026-03-11 12:51:33 +0900

    refactor(test): players, dealer에 직접 카드 넣지 않고 game 메서드만 사용
    players와 dealer를 사용해 직접 객체에 카드를 넣어주는 방식을, game 메서드만 사용해서 카드를 넣어주는 방식으로 변경했습니다.

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -7,7 +7,6 @@ import domain.card.Deck;
 import domain.enums.Rank;
 import domain.enums.Result;
 import domain.enums.Suit;
-import domain.participant.Dealer;
 import domain.participant.Players;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,21 +24,19 @@ public class GameTest {
         private Game onePlayerGame;
         private Players twoPlayers;
         private Players onePlayer;
-        private Dealer dealer = new Dealer();
         private Deck deck;
 
         @BeforeEach
         void setUp() {
-
             deck = new Deck(List.of(new Card(Rank.FIVE, Suit.CLOVER), new Card(Rank.FIVE, Suit.HEART),
                     new Card(Rank.SIX, Suit.CLOVER), new Card(Rank.SEVEN, Suit.CLOVER),
                     new Card(Rank.FOUR, Suit.SPADE), new Card(Rank.SEVEN, Suit.HEART)));
 
             twoPlayers = new Players(List.of("피즈", "스타크"));
-            twoPlayerGame = new Game(twoPlayers, dealer);
+            twoPlayerGame = new Game(twoPlayers);
 
             onePlayer = new Players(List.of("피즈"));
-            onePlayerGame = new Game(onePlayer, dealer);
+            onePlayerGame = new Game(onePlayer);
         }
 
         @DisplayName("게임 시작 후 모든 플레이어가 2장의 카드를 분배 받는다.")
@@ -53,7 +50,7 @@ public class GameTest {
             assertThat(twoPlayers.getPlayerCards("피즈").size()).isEqualTo(2);
             assertThat(twoPlayers.getPlayerCards("스타크").size()).isEqualTo(2);
 
-            assertThat(dealer.getCards().size()).isEqualTo(2);
+            assertThat(twoPlayerGame.getDealerCards().size()).isEqualTo(2);
         }
 
         @DisplayName("플레이어의 카드 총합이 21미만이고 히트 요청 시 한장을 더 분배한다.")
@@ -73,15 +70,16 @@ public class GameTest {
         @DisplayName("딜러의 카드 총합이 17미만이면 한장을 더 분배한다.")
         @Test
         void 딜러의_카드_총합이_17미만이면_한장을_더_분배한다() {
-            //given
-            //when
-            dealer.addCard(new Card(Rank.ACE, Suit.CLOVER));
-            dealer.addCard(new Card(Rank.FOUR, Suit.CLOVER));
-            dealer.addCard(new Card(Rank.EIGHT, Suit.CLOVER));
-            //then
+            deck = new Deck(List.of(new Card(Rank.ACE, Suit.CLOVER), new Card(Rank.FOUR, Suit.CLOVER),
+                    new Card(Rank.EIGHT, Suit.CLOVER), new Card(Rank.SEVEN, Suit.HEART)));
+
+            onePlayerGame.dealerHit(deck);
+            onePlayerGame.dealerHit(deck);
             onePlayerGame.dealerHit(deck);
 
-            assertThat(dealer.getCards().size()).isEqualTo(4);
+            onePlayerGame.dealerHit(deck);
+
+            assertThat(onePlayerGame.getDealerCards().size()).isEqualTo(4);
         }
 
         @DisplayName("플레이어가 버스트 되면 플레이어는 패배하고 딜러는 승리한다.")
@@ -98,12 +96,17 @@ public class GameTest {
         @DisplayName("플레이어가 버스트 되지 않았을 때 플레이어의 점수가 더 높으면 플레이어가 승리하고 딜러는 패배한다.")
         @Test
         void 플레이어_점수_더_높으면_플레이어_승리한다() {
-            onePlayer.distributeCard("피즈", new Card(Rank.JACK, Suit.CLOVER));
-            onePlayer.distributeCard("피즈", new Card(Rank.QUEEN, Suit.CLOVER));
-            onePlayer.distributeCard("피즈", new Card(Rank.ACE, Suit.CLOVER));
+            Deck playerDeck = new Deck(List.of(new Card(Rank.JACK, Suit.CLOVER), new Card(Rank.QUEEN, Suit.CLOVER),
+                    new Card(Rank.ACE, Suit.CLOVER)));
+            onePlayerGame.playerHit("피즈", playerDeck, true);
+            onePlayerGame.playerHit("피즈", playerDeck, true);
+            onePlayerGame.playerHit("피즈", playerDeck, true);
 
-            dealer.addCard(new Card(Rank.JACK, Suit.HEART));
-            dealer.addCard(new Card(Rank.QUEEN, Suit.HEART));
+            Deck dealerDeck = new Deck(List.of(new Card(Rank.ACE, Suit.CLOVER), new Card(Rank.FOUR, Suit.CLOVER),
+                    new Card(Rank.EIGHT, Suit.CLOVER), new Card(Rank.SEVEN, Suit.HEART)));
+            onePlayerGame.dealerHit(dealerDeck);
+            onePlayerGame.dealerHit(dealerDeck);
+            onePlayerGame.dealerHit(dealerDeck);
 
             assertThat(onePlayerGame.getPlayerResult("피즈")).isEqualTo(Result.WIN);
             assertThat(onePlayerGame.getDealerResult().get(Result.LOSE)).isEqualTo(1);
@@ -112,13 +115,15 @@ public class GameTest {
         @DisplayName("플레이어가 버스트 되지 않았을 때 딜러가 버스트된 경우 플레이어가 승리한다.")
         @Test
         void 딜러가_버스트된_경우_플레이어가_승리한다() {
+            Deck playerDeck = new Deck(List.of(new Card(Rank.JACK, Suit.CLOVER), new Card(Rank.QUEEN, Suit.CLOVER)));
+            onePlayerGame.playerHit("피즈", playerDeck, true);
+            onePlayerGame.playerHit("피즈", playerDeck, true);
 
-            onePlayer.distributeCard("피즈", new Card(Rank.JACK, Suit.CLOVER));
-            onePlayer.distributeCard("피즈", new Card(Rank.QUEEN, Suit.CLOVER));
-
-            dealer.addCard(new Card(Rank.JACK, Suit.HEART));
-            dealer.addCard(new Card(Rank.SIX, Suit.HEART));
-            dealer.addCard(new Card(Rank.SEVEN, Suit.HEART));
+            Deck dealerDeck = new Deck(List.of(new Card(Rank.JACK, Suit.HEART), new Card(Rank.SIX, Suit.HEART),
+                    new Card(Rank.SEVEN, Suit.HEART)));
+            onePlayerGame.dealerHit(dealerDeck);
+            onePlayerGame.dealerHit(dealerDeck);
+            onePlayerGame.dealerHit(dealerDeck);
 
             assertThat(onePlayerGame.getPlayerResult("피즈")).isEqualTo(Result.WIN);
             assertThat(onePlayerGame.getDealerResult().get(Result.LOSE)).isEqualTo(1);
@@ -127,11 +132,15 @@ public class GameTest {
         @DisplayName("플레이어가 버스트 되지 않았을 때 플레이어의 점수가 더 낮으면 플레이어가 패배하고 딜러는 승리한다.")
         @Test
         void 플레이어_점수_더_높으면_플레이어_패배한다() {
-            onePlayer.distributeCard("피즈", new Card(Rank.JACK, Suit.CLOVER));
-            onePlayer.distributeCard("피즈", new Card(Rank.NINE, Suit.CLOVER));
+            Deck playerDeck = new Deck(List.of(new Card(Rank.JACK, Suit.CLOVER), new Card(Rank.NINE, Suit.CLOVER)));
+            onePlayerGame.playerHit("피즈", playerDeck, true);
+            onePlayerGame.playerHit("피즈", playerDeck, true);
 
-            dealer.addCard(new Card(Rank.JACK, Suit.HEART));
-            dealer.addCard(new Card(Rank.QUEEN, Suit.HEART));
+            Deck dealerDeck = new Deck(List.of(new Card(Rank.JACK, Suit.HEART), new Card(Rank.SIX, Suit.HEART),
+                    new Card(Rank.FOUR, Suit.HEART)));
+            onePlayerGame.dealerHit(dealerDeck);
+            onePlayerGame.dealerHit(dealerDeck);
+            onePlayerGame.dealerHit(dealerDeck);
 
             assertThat(onePlayerGame.getPlayerResult("피즈")).isEqualTo(Result.LOSE);
             assertThat(onePlayerGame.getDealerResult().get(Result.WIN)).isEqualTo(1);
@@ -140,14 +149,14 @@ public class GameTest {
         @DisplayName("플레이어가 버스트 되지 않았을 때 플레이어와 딜러의 점수가 같으면 무승부가 된다.")
         @Test
         void 플레이어_딜러_점수_같으면_무승부_된다() {
+            Deck playerDeck = new Deck(List.of(new Card(Rank.JACK, Suit.CLOVER), new Card(Rank.NINE, Suit.CLOVER)));
+            onePlayerGame.playerHit("피즈", playerDeck, true);
+            onePlayerGame.playerHit("피즈", playerDeck, true);
 
-            onePlayer.distributeCard("피즈", new Card(Rank.JACK, Suit.CLOVER));
-            onePlayer.distributeCard("피즈", new Card(Rank.QUEEN, Suit.CLOVER));
+            Deck dealerDeck = new Deck(List.of(new Card(Rank.JACK, Suit.HEART), new Card(Rank.NINE, Suit.HEART)));
+            onePlayerGame.dealerHit(dealerDeck);
+            onePlayerGame.dealerHit(dealerDeck);
 
-            dealer.addCard(new Card(Rank.JACK, Suit.HEART));
-            dealer.addCard(new Card(Rank.QUEEN, Suit.HEART));
-
-            //then
             assertThat(onePlayerGame.getPlayerResult("피즈")).isEqualTo(Result.DRAW);
             assertThat(onePlayerGame.getDealerResult().get(Result.DRAW)).isEqualTo(1);
         }
@@ -155,30 +164,26 @@ public class GameTest {
         @DisplayName("플레이어와 딜러가 분배된 카드를 알맞게 가지고 있는지 확인한다.")
         @Test
         void 플레이어_딜러_카드_정상_분배_확인한다() {
-            //given
             onePlayerGame.initializeGame(deck);
-            //when
-            onePlayerGame.distributeCard("피즈", deck);
-            onePlayerGame.distributeCard(deck);
-            //then
+            onePlayerGame.playerHit("피즈", deck, true);
 
-            List<Card> fizzCard = onePlayer.getPlayerCards("피즈");
-            List<Rank> expectedFizzRank = List.of(Rank.FIVE, Rank.SIX, Rank.FOUR);
-            List<Suit> expectedFizzSuit = List.of(Suit.CLOVER, Suit.CLOVER, Suit.SPADE);
+            List<Card> fizzCards = onePlayerGame.getPlayerCards("피즈");
+            List<Card> expectedFizzCards = List.of(new Card(Rank.FIVE, Suit.CLOVER), new Card(Rank.SIX, Suit.CLOVER),
+                    new Card(Rank.FOUR, Suit.SPADE));
 
-            for (int i = 0; i < 3; i++) {
-                assertThat(fizzCard.get(i).rank()).isEqualTo(expectedFizzRank.get(i));
-                assertThat(fizzCard.get(i).suit()).isEqualTo(expectedFizzSuit.get(i));
-            }
+            assertThat(fizzCards).isEqualTo(expectedFizzCards);
 
-            List<Card> dealerCard = dealer.getCards();
-            List<Rank> expectedDealerRank = List.of(Rank.FIVE, Rank.SEVEN, Rank.SEVEN);
-            List<Suit> expectedDealerSuit = List.of(Suit.HEART, Suit.CLOVER, Suit.HEART);
+            onePlayerGame.dealerHit(deck);
 
-            for (int i = 0; i < 3; i++) {
-                assertThat(dealerCard.get(i).rank()).isEqualTo(expectedDealerRank.get(i));
-                assertThat(dealerCard.get(i).suit()).isEqualTo(expectedDealerSuit.get(i));
-            }
+            List<Card> dealerCard = onePlayerGame.getDealerCards();
+            List<Card> expectedDealerCards = List.of(new Card(Rank.FIVE, Suit.HEART), new Card(Rank.SEVEN, Suit.CLOVER),
+                    new Card(Rank.SEVEN, Suit.HEART));
+
+            deck = new Deck(List.of(new Card(Rank.FIVE, Suit.CLOVER), new Card(Rank.FIVE, Suit.HEART),
+                    new Card(Rank.SIX, Suit.CLOVER), new Card(Rank.SEVEN, Suit.CLOVER),
+                    new Card(Rank.FOUR, Suit.SPADE), new Card(Rank.SEVEN, Suit.HEART)));
+
+            assertThat(dealerCard).isEqualTo(expectedDealerCards);
         }
     }
 }
