ef96ebedf6e8f3752b7a850930b787e9f1dc057b
나의찬 <cab07036@naver.com>
2026-03-11 15:39:16 +0900

    feat(PlayerBetting): 플레이어 배팅 기능 구현
    플레이어가 생성된 뒤에 배팅을 추가하면, 해당 플레이어의 해당되는 배팅이 제대로 저장되도록 구현하였습니다.

---
+++ src/main/java/domain/model/PlayerBetting.java
@@ -0,0 +1,33 @@
+package domain.model;
+
+import static constant.ErrorMessage.INVALID_BETTING_VALUE;
+
+public class PlayerBetting {
+
+    private final Player player;
+    private final int value;
+
+    private PlayerBetting(Player player, int value) {
+        this.player = player;
+        this.value = value;
+    }
+
+    public static PlayerBetting of(Player player, int value) {
+        validateValue(value);
+        return new PlayerBetting(player, value);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    private static void validateValue(int value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException(INVALID_BETTING_VALUE.getMessage());
+        }
+    }
+}
