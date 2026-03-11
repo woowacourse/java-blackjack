67552e6b09ee3a61bcaa03e03f7959735c4e33b9
jhk01007 <jhk001007@naver.com>
2026-03-11 13:01:59 +0900

    feat: 블랙잭 게임 결과를 계산하는 기능 추가

--- src/main/java/domain/pariticipant/Participants.java
+++ src/main/java/domain/pariticipant/Participants.java
@@ -4,8 +4,14 @@ package domain.pariticipant;
 import domain.card.CardShuffler;
 import domain.card.Deck;
 import domain.card.Hand;
+import domain.result.DealerMatchResult;
+import domain.result.MatchCase;
+import domain.result.MatchResult;
+import domain.result.PlayerMatchResult;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static constant.BlackjackConstant.DEALER_NAME;
 
@@ -19,8 +25,78 @@ public class Participants {
         this.players = players;
     }
 
+    public Participants(Dealer dealer, Players players) {
+        this.dealer = dealer;
+        this.players = players;
+    }
+
+    public Dealer getDealer() {
+        return dealer;
+    }
+
+    public Players getPlayers() {
+        return players;
+    }
+
     public void drawInitialCards(Deck deck, CardShuffler cardShuffler) {
         dealer.drawInitialCards(deck, cardShuffler);
         players.drawInitialCards(deck, cardShuffler);
+    }
+
+    public MatchResult calculateMatchResult() {
+        int dealerWinCount = 0;
+        int dealerDrawCount = 0;
+        int dealerLoseCount = 0;
+        Map<Player, MatchCase> playerMatchResult = new HashMap<>();
+        for (Player player : players.getPlayers()) {
+
+            if (isDealerWin(player)) { // 딜러승, 플레이어 패배
+                dealerWinCount = ifDealerWin(player, dealerWinCount, playerMatchResult);
+                continue;
+            }
+
+            if (isDealerDraw(player)) { // 무승부
+                dealerDrawCount = ifDealerDraw(player, dealerDrawCount, playerMatchResult);
+                continue;
+            }
+
+            dealerLoseCount = ifDealerLose(player, dealerLoseCount, playerMatchResult);
+        }
+
+        return new MatchResult(
+                new PlayerMatchResult(playerMatchResult),
+                new DealerMatchResult(dealerWinCount, dealerDrawCount, dealerLoseCount)
+        );
+    }
+
+    private boolean isDealerDraw(Player player) {
+        return !dealer.isBust()
+                && (dealer.getScore() == player.getScore())
+                && ((player.isBlackjack() && dealer.isBlackjack())
+                || (!player.isBlackjack() && !dealer.isBlackjack()));
+    }
+
+    private boolean isDealerWin(Player player) {
+        return player.isBust()
+                || (!dealer.isBust() && dealer.getScore() > player.getScore())
+                || (dealer.isBlackjack() && !player.isBlackjack());
+    }
+
+    private static int ifDealerWin(Player player, int dealerWinCount, Map<Player, MatchCase> playerMatchResult) {
+        dealerWinCount++;
+        playerMatchResult.put(player, MatchCase.LOSE);
+        return dealerWinCount;
+    }
+
+    private static int ifDealerDraw(Player player, int dealerDrawCount, Map<Player, MatchCase> playerMatchResult) {
+        dealerDrawCount++;
+        playerMatchResult.put(player, MatchCase.DRAW);
+        return dealerDrawCount;
+    }
+
+    private static int ifDealerLose(Player player, int dealerLoseCount, Map<Player, MatchCase> playerMatchResult) {
+        dealerLoseCount++; // 딜러 패배, 플레이어 승
+        playerMatchResult.put(player, MatchCase.WIN);
+        return dealerLoseCount;
     }
 }
