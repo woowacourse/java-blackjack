b62c9cfa76e2b1f545bc797756f81ddd583e634d
haeyoon1 <hynam1220@naver.com>
2026-03-12 21:27:41 +0900

    test: BlackjackResultTest 작성 중 (임시커밋)

--- src/test/java/domain/BlackjackResultTest.java
+++ src/test/java/domain/BlackjackResultTest.java
@@ -2,74 +2,50 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import domain.shuffle.NoShuffleStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class BlackjackResultTest {
 
+    private final List<Card> cardList = List.of(
+            Card.of(Rank.NINE, Suit.DIAMOND),
+            Card.of(Rank.NINE, Suit.CLOVER),
+            Card.of(Rank.EIGHT, Suit.DIAMOND));
+    private final String name = "포비";
+
     @Test
-    @DisplayName("딜러와 플레이어 모두 bust 되지 않은 경우의 승패를 판정한다.")
-    void 딜러_플레이어_모두_bust_되지_않은_경우_승패_판정_테스트() {
-        int dealerScore = 20;
-        int playerScore = 19;
-        GameResult playerResult = GameResult.DRAW;
+    @DisplayName("Dealer 수익 정상 계산 여부를 판정한다.")
+    void 딜러_수익_정상_계산_테스트(){
+        Deck deck = Deck.of(new NoShuffleStrategy());
+        Player player = Player.of(Cards.of(cardList), name);
+        Dealer dealer = Dealer.of(deck.drawInitialHand());
 
-        if (playerScore > dealerScore) {
-            playerResult = GameResult.WIN;
-        }
-        if (playerScore < dealerScore) {
-            playerResult = GameResult.LOSE;
-        }
+        BlackjackResult blackjackResult = BlackjackResult.of();
+        blackjackResult.add(player, 1000L);
+        //blackjackResult.resolveResult(player, dealer);
+        long dealerProfit = blackjackResult.dealerProfit();
 
-        GameResult expect = GameResult.LOSE;
-        assertThat(playerResult).isEqualTo(expect);
+        long expect = 1000L;
+        //assertThat(dealerProfit).isEqualTo(expect);
     }
 
     @Test
-    @DisplayName("딜러만 bust된 경우의 승패를 판정한다.")
-    void 딜러_bust_된_경우_승패_판정_테스트() {
-        int dealerScore = 23;
-        int playerScore = 19;
-        GameResult playerResult = GameResult.DRAW;
+    @DisplayName("Player 수익 정상 계산 여부를 판정한다.")
+    void 플레이어_수익_정상_계산_테스트(){
+        Deck deck = Deck.of(new NoShuffleStrategy());
+        Player player = Player.of(Cards.of(cardList), name);
+        Dealer dealer = Dealer.of(deck.drawInitialHand());
 
-        if (dealerScore > Policy.BLACKJACK_NUMBER){
-            dealerScore = Policy.BUST_SCORE;
-        }
-        if (playerScore > Policy.BLACKJACK_NUMBER){
-            playerScore = Policy.BUST_SCORE;
-        }
-        if (playerScore > dealerScore) {
-            playerResult = GameResult.WIN;
-        }
-        if (playerScore < dealerScore) {
-            playerResult = GameResult.LOSE;
-        }
 
-        GameResult expect = GameResult.WIN;
-        assertThat(playerResult).isEqualTo(expect);
-    }
+        BlackjackResult blackjackResult = BlackjackResult.of();
+        blackjackResult.add(player, 1000L);
+        //blackjackResult.resolveResult(player, dealer);
+        //long dealerProfit = blackjackResult.playerProfit(blackjackResult);
 
-    @Test
-    @DisplayName("딜러와 플레이어 모두 bust 된 경우의 승패를 판정한다.")
-    void 딜러_플레이어_모두_bust_된_경우_승패_판정_테스트() {
-        int dealerScore = 23;
-        int playerScore = 22;
-        GameResult playerResult = GameResult.DRAW;
-
-        if (dealerScore > Policy.BLACKJACK_NUMBER){
-            dealerScore = Policy.BUST_SCORE;
-        }
-        if (playerScore > Policy.BLACKJACK_NUMBER){
-            playerScore = Policy.BUST_SCORE;
-        }
-        if (playerScore > dealerScore) {
-            playerResult = GameResult.WIN;
-        }
-        if (playerScore < dealerScore) {
-            playerResult = GameResult.LOSE;
-        }
-
-        GameResult expect = GameResult.DRAW;
-        assertThat(playerResult).isEqualTo(expect);
+        long expect = 1000L;
+        //assertThat(dealerProfit).isEqualTo(expect);
     }
 }
