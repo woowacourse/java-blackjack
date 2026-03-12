ffc8fa6d9aee437e70bfe28273f13f0df217c0b8
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-12 20:15:40 +0900

    test(BettingAmountRequestDto): 베팅 금액 null 입력 에러 발생 테스트

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -16,4 +16,5 @@ public class InputView {
         System.out.println(name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
         return new AgreementRequestDto(sc.nextLine());
     }
+
 }
---
+++ src/test/java/dto/BettingAmountRequestDtoTest.java
@@ -0,0 +1,17 @@
+package dto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BettingAmountRequestDtoTest {
+
+    @Test
+    @DisplayName("베팅 금액 입력 문자열이 null이면 에러 발생 검증")
+    void 베팅_금액_null_에러_발생_테스트() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new BettingAmountRequestDto(null));
+    }
+}
