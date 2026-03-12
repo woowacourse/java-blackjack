94ab014cd5efb4a03718b2c0aaf334467b8875a6
haeyoon1 <hynam1220@naver.com>
2026-03-12 18:03:42 +0900

    test: InputParserTest 예외 테스트케이스 추가

--- src/test/java/utils/InputParserTest.java
+++ src/test/java/utils/InputParserTest.java
@@ -3,6 +3,7 @@ package utils;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+
 import view.mesage.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -14,14 +15,12 @@ public class InputParserTest {
     @DisplayName("사용자 입력 값 구분자(',') 분리 검증")
     class SplitByDelimiter {
 
-        private static final String DELIMITER = ",";
-
         @Test
         @DisplayName("사용자 이름 입력 값이 구분자를 통해 정상 분리")
         void 사용자_이름_입력값_분리_정상_테스트() {
             String input = "pobi,jason";
 
-            List<String> usernames = List.of(input.split(DELIMITER));
+            List<String> usernames = InputParser.splitByDelimiter(input);
 
             List<String> expect = List.of("pobi", "jason");
             assertThat(usernames).isEqualTo(expect);
@@ -29,15 +28,32 @@ public class InputParserTest {
 
         @Test
         @DisplayName("사용자 이름 입력 값이 공백인 경우 구분자 분리 실패")
-        void 사용자_이름_입력값_실패_테스트() {
+        void 사용자_빈_이름_입력값_실패_테스트() {
             String input = "";
 
             assertThatIllegalArgumentException()
-                    .isThrownBy(() -> {
-                        if (input.isEmpty())
-                            throw new IllegalArgumentException(ErrorMessage.EMPTY_NAME_INPUT.getMessage());
-                    })
+                    .isThrownBy(() -> InputParser.splitByDelimiter(input))
                     .withMessage(ErrorMessage.EMPTY_NAME_INPUT.getMessage());
+        }
+
+        @Test
+        @DisplayName("유효한 사용자 0명 입력했을 시 에러 발생")
+        void 유효한_사용자_0명_입력_실패_테스트() {
+            String input = "   , ,";
+
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> InputParser.splitByDelimiter(input))
+                    .withMessage(ErrorMessage.INVALID_NAMES_EMPTY.getMessage());
+        }
+
+        @Test
+        @DisplayName("사용자 25명 초과 입력했을 시 에러 발생")
+        void 사용자_26명_입력_실패_테스트() {
+            String input = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> InputParser.splitByDelimiter(input))
+                    .withMessage(ErrorMessage.INVALID_NAMES_EXCEED_LIMIT.getMessage());
         }
     }
 }
