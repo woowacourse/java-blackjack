2778aa643008b0e14b1a1f59324d75b7ca111b90
khcho96 <khcho1492@gmail.com>
2026-03-12 15:40:24 +0900

    refactor(PlayerName): 상수 private 처리

--- src/main/java/domain/participant/PlayerName.java
+++ src/main/java/domain/participant/PlayerName.java
@@ -4,8 +4,8 @@ import exception.BlackjackException;
 
 public record PlayerName(String name) {
 
-    public static final int PLAYER_NAME_MIN_LENGTH = 2;
-    public static final int PLAYER_NAME_MAX_LENGTH = 5;
+    private static final int PLAYER_NAME_MIN_LENGTH = 2;
+    private static final int PLAYER_NAME_MAX_LENGTH = 5;
     public static final String PLAYER_NAME_LENGTH_OUT_OF_RANGE =
             String.format("게임 참가자의 이름은 %d~%d글자 사이여야 합니다.", PLAYER_NAME_MIN_LENGTH, PLAYER_NAME_MAX_LENGTH);
     public static final String PLAYER_NAME_BLANK = "게임 참가자의 이름은 공백이 될 수 없습니다.";
