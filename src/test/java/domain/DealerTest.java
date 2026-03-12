cfe89d77ab4fd1771d79b89728a4a626844990a4
kdongsu5509 <kod66170@gmail.com>
2026-03-12 13:30:11 +0900

    fix : uncomment addCard test
    - 기존 코드 주석 해제
    - 이전의 변경에 따른 테스트 로직 내부 메서드 이름 수정

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -1,69 +1,47 @@
 package domain;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DealerTest {
+
+    private static Hand dealerHand = Hand.of(
+            new Card(CardShape.스페이드, CardContents.J),
+            new Card(CardShape.클로버, CardContents.FIVE)
+    );
+
     @Test
     @DisplayName("Dealer를 생성할 때 오류 발생 안함")
     void dealer_create_success() {
-        Hand dealerHand = Hand.of(
-                new Card(CardShape.스페이드, CardContents.J),
-                new Card(CardShape.클로버, CardContents.FIVE)
-        );
-
         assertDoesNotThrow(() -> Dealer.from(dealerHand));
     }
 
-//    @Test
-//    @DisplayName("딜러는 카드의 합이 16 이하면 카드를 한 장 더 받는다")
-//    void addCard_Dealer_success() {
-//        //given
-//        CardCreationStrategy dealerCardCreationStrategy = () -> {
-//            Card spadeJ = new Card(CardShape.스페이드, CardContents.J);
-//            return new ArrayList<>(List.of(spadeJ));
-//        };
-//        CardCreationStrategy totalCardCreationStrategy = () -> {
-//            Card spadeA = new Card(CardShape.스페이드, CardContents.A);
-//            Card heartA = new Card(CardShape.하트, CardContents.TWO);
-//            return new ArrayList<>(List.of(spadeA, heartA));
-//        };
-//        Deck dealerDeck = Deck.createDeck(dealerCardCreationStrategy);
-//        Deck totalDeck = Deck.createDeck(totalCardCreationStrategy);
-//        Dealer dealer = new Dealer(dealerDeck);
-//
-//        //when
-//        Optional<Card> result = dealer.addCard(totalDeck);
-//
-//        //then
-//        Assertions.assertThat(result.isPresent()).isTrue();
-//    }
-//
-//
-//    @Test
-//    @DisplayName("딜러는 카드의 합이 16 이상 -> Optional<Empty>를 반환")
-//    void addCard_Dealer_Optional_empty() {
-//        //given
-//        CardCreationStrategy dealerCardCreationStrategy = () -> {
-//            Card spadeJ = new Card(CardShape.스페이드, CardContents.J);
-//            Card heartJ = new Card(CardShape.하트, CardContents.J);
-//            return new ArrayList<>(List.of(spadeJ, heartJ));
-//        };
-//        CardCreationStrategy totalCardCreationStrategy = () -> {
-//            Card spadeA = new Card(CardShape.스페이드, CardContents.A);
-//            Card heartA = new Card(CardShape.하트, CardContents.TWO);
-//            return new ArrayList<>(List.of(spadeA, heartA));
-//        };
-//        Deck dealerDeck = Deck.createDeck(dealerCardCreationStrategy);
-//        Deck totalDeck = Deck.createDeck(totalCardCreationStrategy);
-//        Dealer dealer = new Dealer(dealerDeck);
-//
-//        //when
-//        Optional<Card> result = dealer.addCard(totalDeck);
-//
-//        //then
-//        Assertions.assertThat(result.isPresent()).isFalse();
-//    }
+    @Test
+    @DisplayName("딜러는 카드의 합이 16 이하면 카드를 한 장 더 받고 새로운 Dealer를 반환한다")
+    void addCard_success() {
+        //given
+        CardCreationStrategy onlyJCardCreation = () -> {
+            Card spadeA = new Card(CardShape.스페이드, CardContents.J);
+            return new ArrayDeque<>(List.of(spadeA));
+        };
+        Deck totalDeck = Deck.createDeck(onlyJCardCreation);
+
+        Hand dealerHandSixteen = Hand.of(
+                new Card(CardShape.하트, CardContents.SIX),
+                new Card(CardShape.스페이드, CardContents.TEN)
+        );
+        Dealer dealer = Dealer.from(dealerHandSixteen);
+
+        //when
+        Optional<Dealer> result = dealer.addCard(totalDeck::drawCard);
+
+        //then
+        assertTrue(result.isPresent());
+    }
 }
