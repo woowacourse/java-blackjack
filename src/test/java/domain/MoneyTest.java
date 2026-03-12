33f94e50f6de48afcc3e18dbe3dd2e96bb7461e5
gleaming9 <gleaming9@khu.ac.kr>
2026-03-12 15:41:49 +0900

    test(money): 음수,0 입력 예외 검증 추가 및 메서드명 변경

--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -1,7 +1,6 @@
 package domain;
 
 import exception.ErrorMessage;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,11 +10,21 @@ class MoneyTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"3.14", "돈", ""})
-    void 정수가_아닌_값을_베팅_금액에_입력하면_에러가_발생한다(String input){
+    void 베팅_금액에_정수가_아닌_값을_입력하면_에러가_발생한다(String input){
         // given
         // when,then
         assertThatThrownBy(() -> new Money(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.INVALID_MONEY.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "0"})
+    void 베팅_금액에_0이하인_값을_입력하면_에러가_발생한다(String input){
+        // given
+        // when,then
+        assertThatThrownBy(() -> new Money(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NEGATIVE_MONEY.getMessage());
     }
 }
