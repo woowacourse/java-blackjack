bab42d9e0c1b9608dcef717cca3332716c7f803a
Sumin <new.sumyang@gmail.com>
2026-03-12 14:56:35 +0900

    test: 테스트 컨벤션 통일 및 중복 기능 테스트 제거

--- src/test/java/blackjack/domain/ParticipantsTest.java
+++ src/test/java/blackjack/domain/ParticipantsTest.java
@@ -17,6 +17,7 @@ class ParticipantsTest {
     @Test
     @DisplayName("초기 분배 시 플레이어에게 두 장의 카드가 분배된다.")
     void distributeCardsSuccessfully() {
+        // given
         PlayerBettingRequest playerBettingRequest = PlayerBettingRequest.of("pobi", "1000");
         PlayersBettingRequest playersBettingRequest = PlayersBettingRequest.from(List.of(playerBettingRequest));
         Players players = Players.makePlayers(playersBettingRequest);
@@ -24,16 +25,19 @@ class ParticipantsTest {
         Participants participants = new Participants(players, dealer);
         PlayingCards deck = PlayingCards.createShuffledDeck();
 
+        // when
         participants.distributeCards(deck);
         List<Player> allPlayers = players.getAllPlayers();
         List<String> playerCards = Arrays.stream(allPlayers.getFirst().getCardStatus().split(",")).toList();
 
+        // then
         assertThat(playerCards).hasSize(2);
     }
 
     @Test
     @DisplayName("덱에 남은 카드가 부족할 때 초기 분배를 시도하면 예외가 발생한다.")
     void distributeCardsThrowsExceptionOnShortDeck() {
+        // given
         PlayerBettingRequest pobiRequest = PlayerBettingRequest.of("pobi", "1000");
         PlayerBettingRequest jasonRequest = PlayerBettingRequest.of("jason", "2000");
         PlayersBettingRequest playersBettingRequest = PlayersBettingRequest.from(List.of(pobiRequest, jasonRequest));
@@ -44,25 +48,9 @@ class ParticipantsTest {
             new Card(Rank.THREE, Suit.SPADE)
         ));
 
+        // when & then
         assertThatThrownBy(() -> participants.distributeCards(shortDeck))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("뽑을 카드가 남아있지 않습니다.");
-    }
-
-    @Test
-    @DisplayName("딜러가 카드를 더 받아야 하는 상황인지 정확히 판단한다.")
-    void isDealerDrawWorksCorrectly() {
-        PlayerBettingRequest playerBettingRequest = PlayerBettingRequest.of("pobi", "1000");
-        PlayersBettingRequest playersBettingRequest = PlayersBettingRequest.from(List.of(playerBettingRequest));
-        Players players = Players.makePlayers(playersBettingRequest);
-        Participants participants = new Participants(players, Dealer.from());
-        PlayingCards dealerCards16 = PlayingCards.from(List.of(
-            new Card(Rank.TEN, Suit.SPADE),
-            new Card(Rank.SIX, Suit.HEART)
-        ));
-
-        participants.dealerDraw(dealerCards16);
-
-        assertThat(participants.isDealerDraw()).isTrue();
     }
 }
