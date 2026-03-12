96606e6377616966a1708850c72dca17c4a7c067
sinequanonn <dleck288@gmail.com>
2026-03-12 09:44:10 +0900

    refactor: Players 생성 책임 외부로 꺼내기

--- src/main/java/blackjack/domain/Players.java
+++ src/main/java/blackjack/domain/Players.java
@@ -2,23 +2,14 @@ package blackjack.domain;
 
 import blackjack.exception.ErrorMessage;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Players {
-    private static final String DELIMITER = ",";
     private static final int PLAYERS_MAX_LENGTH = 5;
     private static final int PLAYERS_MIN_LENGTH = 2;
 
     private final List<Player> players;
-
-    public static Players from(String names) {
-        List<Player> players = split(names).stream()
-                .map(Player::new)
-                .toList();
-        return new Players(players);
-    }
 
     public Players(List<Player> players) {
         validatePlayersNumber(players);
@@ -35,9 +26,5 @@ public class Players {
         }
     }
 
-    private static List<String> split(String input) {
-        return Arrays.stream(input.split(DELIMITER))
-                .map(String::trim)
-                .toList();
-    }
+
 }
