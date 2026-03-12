fe9a495a5e5be5bb99f407e305c5ec85660aeda4
zakie <realzzaekkii@gmail.com>
2026-03-13 00:21:48 +0900

    test: Dealer 및 Player draw 조건 검증 테스트 추가

---
+++ src/test/java/domain/participant/DealerTest.java
@@ -0,0 +1,42 @@
+package domain.participant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.card.Card;
+import domain.card.CardRank;
+import domain.card.CardSuit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DealerTest {
+
+    @Test
+    @DisplayName("딜러는 카드 합이 16 이하라면 카드를 더 뽑는다.")
+    void 딜러는_카드_합이_16이하라면_draw한다() {
+        // given
+        final Dealer dealer = new Dealer();
+        dealer.draw(new Card(CardSuit.SPADE, CardRank.TEN));
+        dealer.draw(new Card(CardSuit.SPADE, CardRank.SIX));
+
+        // when
+        final boolean result = dealer.shouldDraw();
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("딜러는 카드 합이 17 이상이면 카드를 뽑지 않는다.")
+    void 딜러는_카드_합이_17이상이면_draw하지_않는다() {
+        // given
+        final Dealer dealer = new Dealer();
+        dealer.draw(new Card(CardSuit.SPADE, CardRank.TEN));
+        dealer.draw(new Card(CardSuit.SPADE, CardRank.SEVEN));
+
+        // when
+        final boolean result = dealer.shouldDraw();
+
+        // then
+        assertThat(result).isFalse();
+    }
+}
