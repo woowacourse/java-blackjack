50455df8020a785c070863598b3b5f287cf2e667
sun <sun007021@gmail.com>
2026-03-12 15:02:34 +0900

    test: 플레이어가 블랙잭으로 승리시 수익은 배팅금액의 1.5배

--- src/test/java/PlayerBetTest.java
+++ src/test/java/PlayerBetTest.java
@@ -1,5 +1,6 @@
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import blackjack.domain.Card;
 import blackjack.domain.Player;
 import blackjack.domain.PlayerBetting;
 import blackjack.domain.ScoreCompareResult;
@@ -13,5 +14,23 @@ public class PlayerBetTest {
         PlayerBetting playerBetting = new PlayerBetting(player, 1000);
 
         assertThat(playerBetting.calculateProfit(ScoreCompareResult.PLAYER_WIN)).isEqualTo(1000);
+    }
+
+    @Test
+    void calculate_profit_when_player_win_with_blackjack() {
+        Player player = createPlayer("player", "A:스페이드", "10:하트");
+
+        PlayerBetting playerBetting = new PlayerBetting(player, 1000);
+
+        assertThat(playerBetting.calculateProfit(ScoreCompareResult.PLAYER_WIN)).isEqualTo(1500);
+    }
+
+    private Player createPlayer(String name, String... cards) {
+        Player player = new Player(name);
+        for (String card : cards) {
+            String[] parts = card.split(":");
+            player.receiveOneCard(new Card(parts[0], parts[1]));
+        }
+        return player;
     }
 }
