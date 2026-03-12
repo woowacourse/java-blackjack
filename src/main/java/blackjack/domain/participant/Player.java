82742336bb7d3ee6e6cf4619db5109ca7d579bc9
nn98 <jkllhgb@gmail.com>
2026-03-12 13:24:45 +0900

    refactor(player): 배팅금 검증 책임 이관 뷰 -> 도메인

--- src/main/java/blackjack/domain/participant/Player.java
+++ src/main/java/blackjack/domain/participant/Player.java
@@ -1,20 +1,30 @@
 package blackjack.domain.participant;
 
 public class Player extends Participant {
-
+    
     private final int bettingAmount;
     private boolean stopDrawing;
-
+    
     private Player(String nickname, int bettingAmount) {
         super(nickname);
+        validateBettingAmount(bettingAmount);
         this.stopDrawing = false;
         this.bettingAmount = bettingAmount;
     }
-
+    
     public static Player from(String nickname, int bettingAmount) {
         return new Player(nickname, bettingAmount);
     }
-
+    
+    private void validateBettingAmount(int bettingAmount) {
+        if (bettingAmount < 0) {
+            throw new IllegalArgumentException("배팅 금액은 1 이상의 양수여야 합니다.");
+        }
+        if (bettingAmount == 0) {
+            throw new IllegalArgumentException("배팅 금액은 0일 수 없습니다.");
+        }
+    }
+    
     @Override
     public boolean isDrawable() {
         if (stopDrawing || isBusted()) {
@@ -22,7 +32,7 @@ public class Player extends Participant {
         }
         return !hand.isBlackjack();
     }
-
+    
     public void stand() {
         stopDrawing = true;
     }
--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -22,20 +22,9 @@ public class InputView {
     
     public int readPlayerBettingAmount() {
         try {
-            int bettingAmount = Integer.parseInt(readLine());
-            validateBettingAmount(bettingAmount);
-            return bettingAmount;
+            return Integer.parseInt(readLine());
         } catch (NumberFormatException exception) {
             throw new NumberFormatException("배팅 금액은 숫자로 입력해주세요.");
-        }
-    }
-    
-    private void validateBettingAmount(int bettingAmount) {
-        if (bettingAmount < 0) {
-            throw new IllegalArgumentException("배팅 금액은 1 이상의 양수여야 합니다.");
-        }
-        if (bettingAmount == 0) {
-            throw new IllegalArgumentException("배팅 금액은 0일 수 없습니다.");
         }
     }
     
