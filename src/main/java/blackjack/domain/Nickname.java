627ec4eb3b9c1219a4325655486da369759d575d
Junha Park <poiu7825@pusan.ac.kr>
2026-03-12 13:47:56 +0900

    feat: 플레이어 이름 클래스 추가

---
+++ src/main/java/blackjack/domain/Nickname.java
@@ -0,0 +1,24 @@
+package blackjack.domain;
+
+public class Nickname implements Name{
+    private static final int NICKNAME_MINIMUM_LENGTH = 4;
+    private static final int NICKNAME_MAXIMUM_LENGTH = 10;
+
+    private final String nickname;
+
+    public Nickname(String nickname) {
+        validateNicknameLength(nickname);
+        this.nickname = nickname;
+    }
+
+    private void validateNicknameLength(final String nickname) {
+        if (nickname.length() > NICKNAME_MAXIMUM_LENGTH || nickname.length() < NICKNAME_MINIMUM_LENGTH) {
+            throw new IllegalArgumentException("닉네임은 4~10자 이어야 합니다.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nickname;
+    }
+}
