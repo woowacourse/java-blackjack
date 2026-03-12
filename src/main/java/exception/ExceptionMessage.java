3a79b03c1679b65b2c22b70b3c51003079d2b0e7
haechanmoon <moonhc.dev@gmail.com>
2026-03-12 14:39:21 +0900

    feat(ExceptionMessage): MONEY_INPUT_ERROR 값 추가

--- src/main/java/exception/ExceptionMessage.java
+++ src/main/java/exception/ExceptionMessage.java
@@ -2,7 +2,8 @@ package exception;
 
 public enum ExceptionMessage {
     EMPTY_CARD_DECK("[ERROR] 카드가 모두 소진되었습니다."),
-    INPUT_ERROR("[ERROR] 잘못된 입력입니다.");
+    INPUT_ERROR("[ERROR] 잘못된 입력입니다."),
+    MONEY_INPUT_ERROR("[ERROR] 1000 이상의 숫자를 입력해주시기 바랍니다.");
 
     private final String message;
 
