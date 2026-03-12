735ac9b2c30e0982f23136f24d18b9b1c2b65658
K-iise <kimyungi1004@naver.com>
2026-03-12 19:06:25 +0900

    refactor: 승패 판정, 배팅 테스트 수정

--- src/test/java/domain/BattingMoneyTest.java
+++ src/test/java/domain/BattingMoneyTest.java
@@ -1,6 +1,8 @@
 package domain;
 
+import java.lang.reflect.Field;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -8,7 +10,7 @@ public class BattingMoneyTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1000, 200000, 1000000})
-    public void 정상_범위의_배팅금액_은_생성된다(int money){
+    public void 정상_범위의_배팅금액_은_생성된다(int money) {
         // when & then
         Assertions.assertDoesNotThrow(() -> new BattingMoney(money));
     }
@@ -19,5 +21,23 @@ public class BattingMoneyTest {
         // when & then
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new BattingMoney(money));
+    }
+
+    @Test
+    public void 생성된_배팅금액_은_변경되지_않는다() throws Exception {
+        // given
+        Class<BattingMoney> battingMoneyClass = (Class<BattingMoney>) Class.forName("domain.BattingMoney");
+        Field recordMoney = battingMoneyClass.getDeclaredField("money");
+        // when & then
+        Assertions.assertThrows(IllegalAccessException.class, () -> recordMoney.set("money", 30000));
+    }
+
+    @Test
+    public void 배팅금액이_같은_경우_동일하게_취급된다() {
+        // given
+        BattingMoney battingMoney1 = new BattingMoney(1000);
+        BattingMoney battingMoney2 = new BattingMoney(1000);
+        // when & then
+        Assertions.assertEquals(battingMoney1, battingMoney2);
     }
 }
--- src/test/java/domain/RefereeTest.java
+++ src/test/java/domain/RefereeTest.java
@@ -1,49 +1,33 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
-import domain.card.Card;
-import domain.card.Emblem;
-import domain.card.Grade;
-import domain.participant.Hand;
-import domain.participant.Name;
-import domain.participant.Participant;
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
+import domain.card.Deck;
+import domain.card.RandomShuffleStrategy;
+import domain.participant.Dealer;
+import domain.participant.Player;
+import domain.participant.Players;
+import domain.result.GameResult;
+import domain.result.Referee;
 import java.util.List;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class RefereeTest {
 
     @Test
-    void 딜러와_플레이어의_승패를_비교한다() {
+    void 딜러와_참여중인_모든_플레이어의_승패_여부를_판정한다() {
         // given
-
-        Hand dealerHand = new Hand();
-        Participant participant = new Participant(new Name("dealer"), dealerHand);
-        Dealer dealer = new Dealer(participant);
-
-        Hand playerHand = new Hand();
-        Participant player = new Participant(new Name("player"), playerHand);
-
-        dealerHand.receiveCard(new Card(Emblem.CLOVER, Grade.TEN));
-        playerHand.receiveCard(new Card(Emblem.CLOVER, Grade.ACE));
-
-        List<Participant> players = new ArrayList<>(List.of(player));
-        Participants participants = new Participants(players);
+        Deck deck = new Deck(new RandomShuffleStrategy());
 
         Referee referee = new Referee();
+        List<Player> playerList = List.of(Player.from("토리", 10000),
+                Player.from("이안", 20000), Player.from("에덴", 30000));
+        Players players = new Players(playerList);
 
-        // when
-        GameStatistics statistics = referee.judge(dealer, participants);
+        Dealer dealer = new Dealer(deck);
+        Map<Player, GameResult> gameResultMap = referee.judge(dealer, players);
 
-        // then
-        assertAll(
-                () -> assertThat(statistics.findGameResultBy(player)).isEqualTo(GameResult.WIN),
-                () -> assertThat(statistics.getDealerResult().get(GameResult.WIN)).isEqualTo(0),
-                () -> assertThat(statistics.getDealerResult().get(GameResult.LOSE)).isEqualTo(1),
-                () -> assertThat(statistics.getDealerResult().get(GameResult.DRAW)).isEqualTo(0)
-        );
+        // when & then
+        Assertions.assertThat(gameResultMap.size()).isEqualTo(3);
     }
 }
