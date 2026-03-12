e89c98cadaa045952780e006d916d67c611633bc
Chaerin Lee <rin@Rin-MacBook-Air.local>
2026-03-12 13:35:41 +0900

    feat(GameResult): 이익률 계산 필드 추가

--- src/main/java/blackjack/domain/GameResult.java
+++ src/main/java/blackjack/domain/GameResult.java
@@ -1,18 +1,24 @@
 package blackjack.domain;
 
 public enum GameResult {
-    WIN("승"),
-    TIE("무"),
-    LOSE("패")
-    ;
+    WIN("승", 1),
+    TIE("무", 0),
+    LOSE("패", -1),
+    BLACKJACK_WIN("블랙잭", 1.5);
 
     private final String name;
+    private final double earningRate;
 
-    GameResult(String name) {
+    GameResult(String name, double earningRate) {
         this.name = name;
+        this.earningRate = earningRate;
     }
 
     public String getName() {
         return name;
+    }
+
+    public double getEarningRate() {
+        return earningRate;
     }
 }
