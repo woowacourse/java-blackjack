8c1dc59b0788a0811c2fc312120f713dfe4c9c63
minzzun99 <skg06123@naver.com>
2026-03-12 14:49:39 +0900

    test: HandTest 작성
    - 점수 계산 확인 테스트
    - 블랙잭 확인 테스트
    - 버스트 확인 테스트

---
+++ src/test/java/domain/HandTest.java
@@ -0,0 +1,66 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class HandTest {
+
+    @Test
+    void 점수_계산_확인_테스트() {
+        Hand hand = new Hand(new ArrayList<>(List.of(
+                Card.of(CardNumber.ACE, CardShape.CLOVER),
+                Card.of(CardNumber.ACE, CardShape.DIAMOND))));
+
+        assertThat(hand.getScore().value()).isEqualTo(12);
+    }
+
+    @Test
+    void 블랙잭인_경우_확인() {
+        Hand blackjack = new Hand(new ArrayList<>(List.of(
+                Card.of(CardNumber.J, CardShape.CLOVER),
+                Card.of(CardNumber.ACE, CardShape.DIAMOND))));
+
+        assertThat(blackjack.isBlackjack()).isTrue();
+    }
+
+    @Test
+    void 블랙잭_점수이지만_블랙잭이_아닌_경우() {
+        Hand blackjack = new Hand(new ArrayList<>(List.of(
+                Card.of(CardNumber.EIGHT, CardShape.CLOVER),
+                Card.of(CardNumber.THREE, CardShape.CLOVER),
+                Card.of(CardNumber.J, CardShape.DIAMOND))));
+
+        assertThat(blackjack.isBlackjack()).isFalse();
+    }
+
+    @Test
+    void 블랙잭이_아닌_경우() {
+        Hand blackjack = new Hand(new ArrayList<>(List.of(
+                Card.of(CardNumber.THREE, CardShape.CLOVER),
+                Card.of(CardNumber.J, CardShape.DIAMOND))));
+
+        assertThat(blackjack.isBlackjack()).isFalse();
+    }
+
+    @Test
+    void 버스트_여부_확인() {
+        Hand hand = new Hand(new ArrayList<>(List.of(
+                Card.of(CardNumber.J, CardShape.CLOVER),
+                Card.of(CardNumber.Q, CardShape.CLOVER),
+                Card.of(CardNumber.K, CardShape.DIAMOND))));
+
+        assertThat(hand.isBust()).isTrue();
+    }
+
+    @Test
+    void 버스트가_아닌_경우() {
+        Hand hand = new Hand(new ArrayList<>(List.of(
+                Card.of(CardNumber.J, CardShape.CLOVER),
+                Card.of(CardNumber.K, CardShape.DIAMOND))));
+
+        assertThat(hand.isBust()).isFalse();
+    }
+}
