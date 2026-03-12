9ecf63953550ac2c2bcaa63d25ddebf674845a6c
jhk01007 <jhk001007@naver.com>
2026-03-12 11:56:21 +0900

    refactor: 입력 실패시 에러 메시지를 출력하고 그 곳부터 다시 입력받도록 수정

--- src/main/java/domain/pariticipant/BettingAmount.java
+++ src/main/java/domain/pariticipant/BettingAmount.java
@@ -6,7 +6,7 @@ import static exception.ErrorMessage.BETTING_LIMIT_OUT_OF_BOUNDS_ERROR;
 public record BettingAmount(long bettingAmount) {
 
     public BettingAmount {
-        if (bettingAmount < 0 || bettingAmount > BETTING_LIMIT) {
+        if (bettingAmount <= 0 || bettingAmount > BETTING_LIMIT) {
             throw new IllegalArgumentException(BETTING_LIMIT_OUT_OF_BOUNDS_ERROR.getMessage());
         }
     }
