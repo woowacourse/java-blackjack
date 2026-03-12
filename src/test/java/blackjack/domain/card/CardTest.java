3d6d9e072a4b3efaa6391d7e07bf73fc5a52f9be
sinequanonn <dleck288@gmail.com>
2026-03-12 14:36:53 +0900

    test: Ace 처리 로직 및 동등성 테스트 작성

--- src/test/java/blackjack/domain/card/CardTest.java
+++ src/test/java/blackjack/domain/card/CardTest.java
@@ -19,4 +19,50 @@ class CardTest {
         assertThat(card.getCardName()).isEqualTo("5스페이드");
     }
 
+    @Test
+    @DisplayName("Figure와 Number가 동시에 같으면 Card이다.")
+    void cardIsSameTest() {
+        // given
+        Card card = new Card(Figure.SPADE, Number.ACE);
+        Card card2 = new Card(Figure.SPADE, Number.ACE);
+
+        // when & then
+        assertThat(card.equals(card2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("Figure와 Number 중 다른 게 포함하면 Card는 다르다.")
+    void cardIsNotSameTest() {
+        // given
+        Card card = new Card(Figure.SPADE, Number.ACE);
+        Card card2 = new Card(Figure.SPADE, Number.FIVE);
+        Card card3 = new Card(Figure.HEART, Number.FIVE);
+
+        // when & then
+        assertThat(card.equals(card2)).isFalse();
+        assertThat(card2.equals(card3)).isFalse();
+        assertThat(card3.equals(card)).isFalse();
+    }
+
+    @Test
+    @DisplayName("Card가 Ace라면 참을 반환한다.")
+    void getTrueIfCardIsAce() {
+        // given
+        Card card = new Card(Figure.SPADE, Number.ACE);
+
+        // when & then
+        assertThat(card.isAce()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Card가 Ace가 아니라면 거짓을 반환한다.")
+    void getFalseIfCardIsAce() {
+        // given
+        Card card = new Card(Figure.SPADE, Number.KING);
+
+        // when & then
+        assertThat(card.isAce()).isFalse();
+    }
+
+
 }
