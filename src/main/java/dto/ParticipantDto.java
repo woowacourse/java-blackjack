a1d0ff41591f082d92e693c47136b729ade41760
khcho96 <khcho1492@gmail.com>
2026-03-12 14:55:48 +0900

    feat(ParticipantDto): DTO 생성 및 반환하는 정적 팩토리 메서드 구현

--- src/main/java/dto/ParticipantDto.java
+++ src/main/java/dto/ParticipantDto.java
@@ -1,5 +1,8 @@
 package dto;
 
+import domain.Card;
+import domain.Dealer;
+import domain.Player;
 import java.util.List;
 
 public record ParticipantDto(
@@ -8,4 +11,25 @@ public record ParticipantDto(
         int score
 ) {
 
+    public static ParticipantDto from(Dealer dealer) {
+        return new ParticipantDto(dealer.getName(), convertHandString(dealer.getHand()), dealer.calculateScore());
+    }
+
+    public static ParticipantDto from(Dealer dealer, boolean isOnlyFirst) {
+        List<String> hand = convertHandString(dealer.getHand());
+        if (isOnlyFirst) {
+            hand = convertHandString(dealer.getOnlyFirstHand());
+        }
+        return new ParticipantDto(dealer.getName(), hand, dealer.calculateScore());
+    }
+
+    public static ParticipantDto from(Player player) {
+        return new ParticipantDto(player.getName(), convertHandString(player.getHand()), player.calculateScore());
+    }
+
+    private static List<String> convertHandString(List<Card> cards) {
+        return cards.stream()
+                .map(card -> card.rank().getRank() + card.suit().getSuit())
+                .toList();
+    }
 }
