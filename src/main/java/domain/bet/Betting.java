7888561fb5cf985b50721311d22b7dba361a7863
bee9827 <ehfrhfo9494@naver.com>
2026-03-12 23:13:50 +0900

    feat: Betting 금액단위 제약사항 추가

--- src/main/java/domain/bet/Betting.java
+++ src/main/java/domain/bet/Betting.java
@@ -3,4 +3,15 @@ package domain.bet;
 public record Betting(
         Integer amount
 ) {
+    private static final int UNIT = 1000;
+
+    public Betting {
+        validateAmount(amount);
+    }
+
+    private void validateAmount(Integer amount) {
+        if (amount % UNIT != 0) {
+            throw new IllegalArgumentException("돈은 %d원 단위로 입력해 주세요.".formatted(UNIT));
+        }
+    }
 }
