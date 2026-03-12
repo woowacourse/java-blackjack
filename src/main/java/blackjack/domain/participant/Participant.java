1aaaff2fcdffaf1b3fa9b6eb363fea0c41a00e24
nn98 <jkllhgb@gmail.com>
2026-03-12 09:55:00 +0900

    fix(draw): 누락된 not 추가

--- src/main/java/blackjack/domain/participant/Participant.java
+++ src/main/java/blackjack/domain/participant/Participant.java
@@ -6,53 +6,53 @@ import blackjack.domain.card.Hand;
 import java.util.List;
 
 public class Participant {
-    
+
     private static final int FIRST_DRAW_COUNT = 2;
-    
+
     protected final String nickname;
     protected final Hand hand;
-    
+
     protected Participant(String nickname) {
         validate(nickname);
         this.nickname = nickname;
         this.hand = new Hand();
     }
-    
+
     private void validate(String nickname) {
         if (nickname.isBlank()) {
             throw new IllegalArgumentException("플레이어 이름은 공백이 될 수 없습니다.");
         }
     }
-    
+
     public void drawInitialCards(Deck deck) {
         List<Card> initialCards = deck.drawCards(FIRST_DRAW_COUNT);
         hand.addCards(initialCards);
     }
-    
+
     public void drawCard(Deck deck) {
-        if (isDrawable()) {
+        if (!isDrawable()) {
             throw new IllegalStateException("더이상 카드를 받을 수 없는 참가자입니다.");
         }
         Card card = deck.drawCard();
         hand.addCard(card);
     }
-    
+
     public String getNickname() {
         return nickname;
     }
-    
+
     public List<Card> getCards() {
         return hand.getCards();
     }
-    
+
     public boolean isDrawable() {
         return !isBusted();
     }
-    
+
     public boolean isBusted() {
         return hand.isBusted();
     }
-    
+
     public int getScore() {
         return hand.getTotalScore();
     }
