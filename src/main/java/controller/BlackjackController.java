a861b01fd90a99edc9dcf683e93fb4262c55d44f
mvg01 <dkms625@naver.com>
2026-03-12 15:04:47 +0900

    refactor: GamePhase들을 리스트로 관리하여 컨트롤러 구조 단순화

--- src/main/java/controller/BlackjackController.java
+++ src/main/java/controller/BlackjackController.java
@@ -1,34 +1,28 @@
 package controller;
 
+import java.util.List;
+
 public class BlackjackController {
 
     private GameContext gameContext;
-    private EnterPlayerPhase enterPlayerPhase;
-    private BettingPhase bettingPhase;
-    private InitialDealPhase initialDealPhase;
-    private PlayerTurnPhase playerTurnPhase;
-    private DealerTurnPhase dealerTurnPhase;
-    private CalculateScorePhase calculateScorePhase;
-    private ShowBettingResultPhase showBettingResultPhase;
+    private List<GamePhase> gamePhaseList;
 
     public BlackjackController() {
         gameContext = new GameContext();
-        enterPlayerPhase = new EnterPlayerPhase();
-        bettingPhase = new BettingPhase();
-        initialDealPhase = new InitialDealPhase();
-        playerTurnPhase = new PlayerTurnPhase();
-        dealerTurnPhase = new DealerTurnPhase();
-        calculateScorePhase = new CalculateScorePhase();
-        showBettingResultPhase = new ShowBettingResultPhase();
+        gamePhaseList = List.of(
+                new EnterPlayerPhase(),
+                new BettingPhase(),
+                new InitialDealPhase(),
+                new PlayerTurnPhase(),
+                new DealerTurnPhase(),
+                new CalculateScorePhase(),
+                new ShowBettingResultPhase()
+        );
     }
 
     public void run() {
-        enterPlayerPhase.execute(gameContext);
-        bettingPhase.execute(gameContext);
-        initialDealPhase.execute(gameContext);
-        playerTurnPhase.execute(gameContext);
-        dealerTurnPhase.execute(gameContext);
-        calculateScorePhase.execute(gameContext);
-        showBettingResultPhase.execute(gameContext);
+        for (GamePhase gamePhase : gamePhaseList) {
+            gamePhase.execute(gameContext);
+        }
     }
 }
