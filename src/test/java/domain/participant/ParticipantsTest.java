64fc5335eabf0b4cad357e1fb2b9414899533830
khcho96 <khcho1492@gmail.com>
2026-03-12 17:10:57 +0900

    refactor(ParticipantsTest): 패키지 위치 이동

--- src/test/java/domain/participant/ParticipantsTest.java
+++ src/test/java/domain/participant/ParticipantsTest.java
@@ -1,11 +1,7 @@
-package domain;
+package domain.participant;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import domain.participant.Dealer;
-import domain.participant.Participants;
-import domain.participant.Player;
-import domain.participant.Players;
 import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
