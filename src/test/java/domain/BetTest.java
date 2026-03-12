24cad6ed3748097e0f4f164ea5d68bbcfa43076e
Minjae Kang <rkdalswoals@gmail.com>
2026-03-13 01:04:30 +0900

    fix: 베팅 금액 테스트 수정

--- src/test/java/domain/BetTest.java
+++ src/test/java/domain/BetTest.java
@@ -7,25 +7,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class BatTest {
+class BetTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {10_000, 20_000, 1_000_000, 0})
-    void 베팅_금액은_만원_단위로_생성_가능하다(int amount) {
+    @ValueSource(ints = {10_000, 20_000, 1_000_000})
+    void 베팅_금액_생성은_양수만_가능하다(int amount) {
         assertThatCode(() -> new Bet(amount))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {9999, 10001, 15000, -15000})
-    void 베팅_금액이_10000원_단위가_아니면_예외가_발생한다(int amount) {
+    @ValueSource(ints = {-9999, -10001, 0, -15000})
+    void 베팅_금액이_양수가_아니면_예외가_발생한다(int amount) {
         assertThatThrownBy(() -> new Bet(amount))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 베팅_금액은_음수를_가질_수_있다() {
-        assertThatCode(() -> new Bet(-10000))
-                .doesNotThrowAnyException();
     }
 }
