aea06abb9e8f05874106099300eb5e6d4dc33f56
koreaioi <koreaioi@gachon.ac.kr>
2026-03-12 16:00:09 +0900

    refactor: 블랙잭 정책 초기설계 주석 제거

--- src/main/java/domain/betting/policy/BettingPolicy.java
+++ src/main/java/domain/betting/policy/BettingPolicy.java
@@ -5,26 +5,6 @@ import domain.betting.BettingRate;
 import domain.gamer.Dealer;
 import domain.gamer.Player;
 
-/**
- * - Processing (게임 진행 중)
- * - Game End (게임 종료)
- * - Player BlackJack
- *      Dealer BlackJack -> 1 times
- *      Dealer Bust      -> 1.5 times
- *      Dealer Stop      -> 1.5 times
- * - Player Bust
- *      Dealer Bust      -> 1 times
- *      Dealer Blackjack -> -1 times
- *      Dealer Stop      -> -1 times
- * - Player Stop
- *      Dealer Stop
- *          Compare Win  -> 1 times
- *          Compare Lose -> -1 times
- *
- *  전략 패턴?
- *  상태 패턴?
- * */
-
 public abstract class BettingPolicy implements Comparable<BettingPolicy>{
 
     protected BettingResult bettingResult;
