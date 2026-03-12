bfd9c37a9bf96dcbe415682494dbff66f3292ea8
kdongsu5509 <kod66170@gmail.com>
2026-03-12 14:01:20 +0900

    fix : rename to BlackJackGame

--- src/main/java/domain/BlackJackGame.java
+++ src/main/java/domain/BlackJackGame.java
@@ -1,19 +1,22 @@
 package domain;
 
 import common.ErrorMessage;
+import dto.DealerResultDto;
+import dto.GameResultDto;
 import dto.ParticipantDto;
+import dto.PlayerResultDto;
 import java.util.List;
 import java.util.Optional;
 
 public class BlackJackGame {
     private final Deck totalDeck;
-    private final Players players;
+    private final MultiPlayers multiPlayers;
     private Dealer dealer;
 
-    private BlackJackGame(Deck totalDeck, Dealer dealer, Players players) {
+    private BlackJackGame(Deck totalDeck, Dealer dealer, MultiPlayers multiPlayers) {
         this.totalDeck = totalDeck;
         this.dealer = dealer;
-        this.players = players;
+        this.multiPlayers = multiPlayers;
     }
 
     public static BlackJackGame ready(List<String> playerNames, CardCreationStrategy strategy) {
@@ -21,7 +24,7 @@ public class BlackJackGame {
         return new BlackJackGame(
                 totalDeck,
                 createNewDealer(totalDeck),
-                Players.of(playerNames, totalDeck)
+                MultiPlayers.of(playerNames, totalDeck)
         );
     }
 
@@ -33,19 +36,19 @@ public class BlackJackGame {
     }
 
     public Optional<Player> whoseTurn() {
-        return players.findNotStayPlayer();
+        return multiPlayers.findNotStayPlayer();
     }
 
     public ParticipantDto doHitProcess() {
-        Player newPlayer = players.findNotStayPlayer()
-                .map(player -> players.executeHit(player, totalDeck::drawCard))
+        Player newPlayer = multiPlayers.findNotStayPlayer()
+                .map(player -> multiPlayers.executeHit(player, totalDeck::drawCard))
                 .orElseThrow(() -> new IllegalStateException(ErrorMessage.NO_MORE_PLAYABLE_PLAYER.getMessage()));
         return ParticipantDto.from(newPlayer);
     }
 
     public ParticipantDto doStandProcess() {
-        Player newPlayer = players.findNotStayPlayer()
-                .map(players::executeStand)
+        Player newPlayer = multiPlayers.findNotStayPlayer()
+                .map(multiPlayers::executeStand)
                 .orElseThrow(() -> new IllegalStateException(ErrorMessage.NO_MORE_PLAYABLE_PLAYER.getMessage()));
         return ParticipantDto.from(newPlayer);
     }
@@ -57,79 +60,21 @@ public class BlackJackGame {
     }
 
     public List<ParticipantDto> getPlayersGameSettingStates() {
-        return players.getInitialStates();
+        return multiPlayers.getInitialStates();
     }
 
     public ParticipantDto getDealerGameSettingState() {
         return ParticipantDto.consistWithInitialInfo(dealer);
     }
 
+    public GameResultDto getGameResults() {
+        List<PlayerResultDto> playersGameResults = multiPlayers.checkPlayersGameResult(dealer);
+        DealerResultDto dealerGameResult = DealerResultDto.from(dealer, playersGameResults);
+
+        return new GameResultDto(dealerGameResult, playersGameResults);
+    }
+
     private void updateDealer(Dealer newDealer) {
         this.dealer = newDealer;
     }
-//
-//    public void play(GameDelegate observer) {
-//        List<Player> individualPlayers = players.getPlayers();
-//        for (Player player : individualPlayers) {
-//            while (!player.isBust() && observer.askDrawCard(player.getName())) {
-//                player.addCard(totalDeck);
-//                observer.showPlayerCards(ParticipantDto.from(player));
-//            }
-//        }
-//        while (dealer.addCard(totalDeck).isPresent()) {
-//            observer.showDealerOneMoreCardMessage();
-//        }
-//    }
-//
-//    public void end(GameDelegate delegate) {
-//        GameResultDto result = this.calculateResult();
-//        delegate.showGameResult(result);
-//    }
-//
-//    private GameResultDto calculateResult() {
-//        int dealerScore = dealer.calculateHandSum();
-//        boolean isDealerBust = dealer.isBust();
-//
-//        Map<Player, Result> playerWinLossResults = consistPlayerWinLossResults(dealerScore, isDealerBust);
-//        Map<Result, Integer> dealerWinLossResults = consistDealerResult(playerWinLossResults);
-//
-//        return GameResultDto.from(
-//                dealer,
-//                players,
-//                dealerWinLossResults,
-//                playerWinLossResults
-//        );
-//    }
-//
-//    private Map<Player, Result> consistPlayerWinLossResults(int dealerScore, boolean isDealerBust) {
-//        Map<Player, Result> playerWinLossResults = new LinkedHashMap<>();
-//        List<Player> playingPlayers = players.getPlayers();
-//
-//        for (Player specificPlayer : playingPlayers) {
-//            Result specificPlayerResult = determinePlayerResult(dealerScore, isDealerBust, specificPlayer);
-//            playerWinLossResults.put(specificPlayer, specificPlayerResult);
-//        }
-//
-//        return playerWinLossResults;
-//    }
-//
-//    private Result determinePlayerResult(int dealerScore, boolean isDealerBust, Player specificPlayer) {
-//        return Result.determinePlayerResult(
-//                dealerScore,
-//                specificPlayer.calculateHandSum(),
-//                isDealerBust,
-//                specificPlayer.isBust()
-//        );
-//    }
-//
-//    private Map<Result, Integer> consistDealerResult(Map<Player, Result> playerWinLossResults) {
-//        Map<Result, Integer> dealerWinLossResults = new HashMap<>();
-//        List<Player> playingPlayers = playerWinLossResults.keySet().stream().toList();
-//        for (Player player : playingPlayers) {
-//            Result dealerResult = playerWinLossResults.get(player).reverse();
-//            int currentValue = dealerWinLossResults.getOrDefault(dealerResult, 0);
-//            dealerWinLossResults.put(dealerResult, currentValue + 1);
-//        }
-//        return dealerWinLossResults;
-//    }
 }
---
+++ src/test/java/domain/BlackJackGameTest.java
@@ -0,0 +1,132 @@
+//package domain;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+//import static org.mockito.Mockito.atLeast;
+//import static org.mockito.Mockito.times;
+//import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.when;
+//
+//import dto.GameResultDto;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.LinkedHashMap;
+//import java.util.List;
+//import java.util.Map;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.ArgumentCaptor;
+//import org.mockito.Captor;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//@ExtendWith(MockitoExtension.class)
+//class BlackJackGameTest {
+//    private static final List<String> TEST_PLAYER_NAMES = List.of("pobi", "terry", "rati", "gump");
+//    private static final CardCreationStrategy STRATEGY = BlackJackGameTest::createSampleCards;
+//    @Mock
+//    GameDelegate gameDelegate;
+//    @Captor
+//    ArgumentCaptor<GameResultDto> gameResultDtoArgumentCaptor;
+//
+//    private static List<Card> createSampleCards() {
+//        CardShape[] shapes = CardShape.values();
+//        CardContents[] contents = CardContents.values();
+//
+//        List<Card> sampleCards = new ArrayList<>();
+//        for (CardShape cardShape : shapes) {
+//            for (CardContents content : contents) {
+//                sampleCards.add(new Card(cardShape, content));
+//            }
+//        }
+//
+//        return sampleCards;
+//    }
+//
+//    @Test
+//    @DisplayName("생성 잘 한다")
+//    void ready_good() {
+//        //given
+//
+//        //when, then
+//        assertDoesNotThrow(
+//                () -> BlackJackGame.ready(gameDelegate)
+//        );
+//    }
+//
+//    @Test
+//    @DisplayName("게임 플레이 잘함")
+//    void play_success() {
+//        //given
+//        when(gameDelegate.askPlayerNames()).thenReturn(TEST_PLAYER_NAMES);
+//        BlackJackGame blackJackGame = BlackJackGame.ready(gameDelegate);
+//
+//        //when
+//        blackJackGame.play(gameDelegate);
+//
+//        //then
+//        for (String name : TEST_PLAYER_NAMES) {
+//            verify(gameDelegate, atLeast(1)).askDrawCard(name);
+//        }
+//        verify(gameDelegate, atLeast(1)).showDealerOneMoreCardMessage();
+//
+//    }
+//
+//    @Test
+//    @DisplayName("결과 계산 잘함")
+//    void calculate_success() {
+//        //given
+//        CardCreationStrategy strategy = new CardCreationStrategy() {
+//            @Override
+//            public List<Card> create() {
+//                return new ArrayList<>(
+//                        List.of(
+//                                new Card(CardShape.하트, CardContents.J),
+//                                new Card(CardShape.하트, CardContents.EIGHT),
+//                                new Card(CardShape.다이아몬드, CardContents.J),
+//                                new Card(CardShape.다이아몬드, CardContents.THREE),
+//                                new Card(CardShape.스페이드, CardContents.J),
+//                                new Card(CardShape.스페이드, CardContents.THREE),
+//                                new Card(CardShape.클로버, CardContents.J),
+//                                new Card(CardShape.클로버, CardContents.NINE),
+//                                new Card(CardShape.하트, CardContents.K),
+//                                new Card(CardShape.하트, CardContents.NINE)
+//                        )
+//                );
+//            }
+//        };
+//
+//        when(gameDelegate.askPlayerNames()).thenReturn(TEST_PLAYER_NAMES);
+//        BlackJackGame blackJackGame = BlackJackGame.ready(gameDelegate);
+//        Map<String, Integer> expectDealerWinLossResults = consistExpectDealerWinLossResults();
+//        Map<String, String> expectPlayerWinLossResults = consistExpectPlayerWinLossResults(TEST_PLAYER_NAMES);
+//
+//        //when
+//        blackJackGame.play(gameDelegate);
+//        blackJackGame.end(gameDelegate);
+//
+//        //then
+//        verify(gameDelegate, times(1))
+//                .showGameResult(gameResultDtoArgumentCaptor.capture());
+//        GameResultDto result = gameResultDtoArgumentCaptor.getValue();
+//        assertThat(result.dealerWinLossResults()).isEqualTo(expectDealerWinLossResults);
+//        assertThat(result.playerWinLossResults()).isEqualTo(expectPlayerWinLossResults);
+//    }
+//
+//    private Map<String, String> consistExpectPlayerWinLossResults(List<String> testPlayerNames) {
+//        Map<String, String> expectPlayerWinLossResults = new LinkedHashMap<>();
+//        expectPlayerWinLossResults.put("pobi", "패");
+//        expectPlayerWinLossResults.put("terry", "패");
+//        expectPlayerWinLossResults.put("rati", "승");
+//        expectPlayerWinLossResults.put("gump", "승");
+//        return expectPlayerWinLossResults;
+//    }
+//
+//    private Map<String, Integer> consistExpectDealerWinLossResults() {
+//        Map<String, Integer> expectDealerWinLossResults = new HashMap<>();
+//        expectDealerWinLossResults.put("승", 2);
+//        expectDealerWinLossResults.put("패", 2);
+//        return expectDealerWinLossResults;
+//    }
+//}
