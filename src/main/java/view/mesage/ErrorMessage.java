0cee96cca3f14aec5d07b970fbb2eeab825277ba
haeyoon1 <hynam1220@naver.com>
2026-03-12 22:42:59 +0900

    test: 덱에 남은 카드가 없을 시 오류 반환 테스트 작성

--- src/main/java/view/mesage/ErrorMessage.java
+++ src/main/java/view/mesage/ErrorMessage.java
@@ -7,6 +7,7 @@ public enum ErrorMessage {
     EMPTY_BET_AMOUNT_INPUT("[ERROR]배팅 금액을 입력하세요."),
     INVALID_BET_AMOUNT("[ERROR]배팅 금액은 0보다 큰 정수만 입력 가능합니다."),
     INVALID_YES_OR_NO_INPUT("[ERROR] 카드를 추가할 경우 y, 추가하지 않을 경우 n을 입력하세요."),
+    EMPTY_DECK("[ERROR] 카드 덱에 남아있는 카드가 없습니다."),
     ;
 
     private final String message;
--- src/test/java/domain/DeckTest.java
+++ src/test/java/domain/DeckTest.java
@@ -5,12 +5,14 @@ import static org.assertj.core.api.Assertions.*;
 import domain.shuffle.NoShuffleStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import utils.InputParser;
+import view.mesage.ErrorMessage;
 
 class DeckTest {
 
     @Test
     @DisplayName("카드 덱에서 2장을 정상적으로 draw 하는지 테스트한다.")
-    void 카드_덱_2장_draw_확인_테스트(){
+    void 카드_덱_2장_draw_확인_테스트() {
         Deck deck = Deck.of(new NoShuffleStrategy());
 
         int drawSize = deck.drawInitialHand().size();
@@ -19,4 +21,17 @@ class DeckTest {
         assertThat(drawSize).isEqualTo(expect);
     }
 
+    @Test
+    @DisplayName("카드 덱에서 더이상 draw할 카드가 없을 시 에러를 반환한다.")
+    void 카드_덱_크기가_0일시_draw_오류_테스트() {
+        Deck deck = Deck.of(new NoShuffleStrategy());
+
+        for (int i = 0; i < 26; i++) {
+            deck.drawInitialHand();
+        }
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(deck::draw)
+                .withMessage(ErrorMessage.EMPTY_DECK.getMessage());
+    }
 }
