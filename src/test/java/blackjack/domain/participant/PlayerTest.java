b8012f55ba3b94175b1962c3b7fc7ab67a10774b
Sumin <new.sumyang@gmail.com>
2026-03-12 14:49:04 +0900

    test: 테스트 컨벤션 일치

--- src/test/java/blackjack/domain/participant/PlayerTest.java
+++ src/test/java/blackjack/domain/participant/PlayerTest.java
@@ -18,64 +18,60 @@ class PlayerTest {
     @DisplayName("플레이어가 정상적으로 생성된다.")
     @Test
     void createPlayer() {
+        // given
         Player player = new Player("pobi", Role.PLAYER, 1000);
 
-        assertThat(player.getNickname()).isEqualTo("pobi");
-        assertThat(player.isDealer()).isFalse();
-    }
-
-    @DisplayName("플레이어가 카드를 더 받을 수 있는지 확인한다.")
-    @Test
-    void isDrawable() {
-        Player player = new Player("pobi", Role.PLAYER, 1000);
-
-        assertThat(player.isDrawable()).isTrue();
-
-        PlayingCards bustedCards = PlayingCards.from(List.of(
-            new Card(Rank.TEN, Suit.SPADE),
-            new Card(Rank.TEN, Suit.HEART),
-            new Card(Rank.TWO, Suit.DIAMOND)
-        ));
-        player.receiveCard(bustedCards);
-
-        assertThat(player.isDrawable()).isFalse();
+        // when & then
+        assertAll(
+            () -> assertThat(player.getNickname()).isEqualTo("pobi"),
+            () -> assertThat(player.isDealer()).isFalse()
+        );
     }
 
     @DisplayName("플레이어의 점수가 21점 미만이면 카드를 받을 수 있다.")
     @Test
     void isDrawableWhenNotBustedOrNotBlackjack() {
+        // given
         Player player = new Player("pobi", Role.PLAYER, 1000);
         PlayingCards blackjackCards = PlayingCards.from(List.of(
             new Card(Rank.EIGHT, Suit.SPADE),
             new Card(Rank.NINE, Suit.HEART)
         ));
 
+        // when
         player.receiveCard(blackjackCards);
 
+        // then
         assertThat(player.isDrawable()).isTrue();
     }
 
     @DisplayName("플레이어의 점수가 21점 이상이면 더 이상 카드를 받을 수 없다.")
     @Test
     void isNotDrawableWhenBustedOrBlackjack() {
+        // given
         Player player = new Player("pobi", Role.PLAYER, 1000);
         PlayingCards blackjackCards = PlayingCards.from(List.of(
             new Card(Rank.ACE, Suit.SPADE),
             new Card(Rank.JACK, Suit.HEART)
         ));
 
+        // when
         player.receiveCard(blackjackCards);
 
+        // then
         assertThat(player.isDrawable()).isFalse();
     }
 
     @DisplayName("플레이어가 명시적으로 stop()을 호출하면 점수와 상관없이 카드를 받을 수 없다.")
     @Test
     void stopDrawing() {
+        // given
         Player player = new Player("pobi", Role.PLAYER, 1000);
 
+        // when
         player.stop();
 
+        // then
         assertThat(player.isDrawable()).isFalse();
     }
 
