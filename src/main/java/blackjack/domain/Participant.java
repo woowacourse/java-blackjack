bfcdb40afdff909a1662d0480fbf5575d7c82283
dahxxn <dahyeonhan4529@gmail.com>
2026-03-12 17:50:43 +0900

    feat: Participant 블랙잭 판별 로직 구현
    - 처음 2장 가진 카드의 합이 21(블랙잭)인 경우 true를 반환한다.
    - 3장 가진 카드의 합이 21인 경우 false를 반환한다.
    - 처음 2장 가진 카드의 합이 21이 아닌 경우 false를 반환한다.

--- src/main/java/blackjack/domain/Participant.java
+++ src/main/java/blackjack/domain/Participant.java
@@ -5,6 +5,8 @@ import java.util.Objects;
 
 public abstract class Participant {
     protected static final int BLACKJACK_THRESHOLD = 21;
+    protected static final int INITIAL_CARD_COUNT = 2;
+
     protected final Hand hand;
 
     protected Participant(Hand hand) {
@@ -40,6 +42,10 @@ public abstract class Participant {
 
     public void receiveCard(TrumpCard card) {
         hand.receive(card);
+    }
+
+    public boolean isBlackjack() {
+        return countCards() == INITIAL_CARD_COUNT && score() == BLACKJACK_THRESHOLD;
     }
 
 }
