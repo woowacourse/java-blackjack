a19c9919375c25a90fb290628fc74c6cde7640af
MODUGGAGI <kiroro0814@naver.com>
2026-03-12 19:34:39 +0900

    refactor(remove): 주석 처리된 코드 제거
    주석 처리된 테스트 코드를 제거하였습니다.

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -9,7 +9,6 @@ import domain.enums.Rank;
 import domain.enums.Suit;
 import domain.participant.Dealer;
 import domain.participant.Name;
-import domain.participant.Players;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -48,11 +47,10 @@ public class GameTest {
             //given
             //when
             twoPlayerGame.initializeGame(deck);
-            Players players = twoPlayerGame.getPlayers();
             //then
             assertSoftly(softly -> {
-                assertThat(players.getPlayerCards(new Name("피즈")).size()).isEqualTo(2);
-                assertThat(players.getPlayerCards(new Name("스타크")).size()).isEqualTo(2);
+                assertThat(twoPlayerGame.getPlayerCard(new Name("피즈")).size()).isEqualTo(2);
+                assertThat(twoPlayerGame.getPlayerCard(new Name("스타크")).size()).isEqualTo(2);
 
                 assertThat(dealer.getHand().size()).isEqualTo(2);
             });
@@ -74,9 +72,8 @@ public class GameTest {
             );
             //then
             onePlayerGame.playPlayerTurn(new Name("피즈"), deck, true);
-            Players players = onePlayerGame.getPlayers();
 
-            assertThat(players.getPlayerCards(new Name("피즈")).size()).isEqualTo(4);
+            assertThat(onePlayerGame.getPlayerCard(new Name("피즈")).size()).isEqualTo(4);
         }
 
         @DisplayName("플레이어가 히트를 원하지 않으면 카드를 추가로 받지 않는다.")
@@ -84,14 +81,13 @@ public class GameTest {
         void 플레이어_히트_거절시_카드_추가되지_않는다() {
             //given
             onePlayerGame.initializeGame(deck);
-            Players players = onePlayerGame.getPlayers();
-            int beforeCardCount = players.getPlayerCards(new Name("피즈")).size();
+            int beforeCardCount = onePlayerGame.getPlayerCard(new Name("피즈")).size();
 
             //when
             onePlayerGame.playPlayerTurn(new Name("피즈"), deck, false);
 
             //then
-            assertThat(players.getPlayerCards(new Name("피즈")).size()).isEqualTo(beforeCardCount);
+            assertThat(onePlayerGame.getPlayerCard(new Name("피즈")).size()).isEqualTo(beforeCardCount);
         }
 
         @DisplayName("딜러의 카드 총합이 17미만이면 한장을 더 분배한다.")
@@ -116,10 +112,9 @@ public class GameTest {
             //when
             onePlayerGame.playPlayerTurn(new Name("피즈"), deck, true);
             onePlayerGame.playDealerTurn(deck);
-            Players players = onePlayerGame.getPlayers();
             //then
             assertCardDistribution(
-                    players.getPlayerCards(new Name("피즈")),
+                    onePlayerGame.getPlayerCard(new Name("피즈")),
                     List.of(Rank.FIVE, Rank.FIVE, Rank.FOUR),
                     List.of("클로버", "하트", "스페이드")
             );
@@ -157,138 +152,6 @@ public class GameTest {
             }
         }
     }
-
-/*    @Nested
-    @DisplayName("게임 결과 정상 판정")
-    class gameResultJudgementSuccess {
-        @DisplayName("플레이어가 버스트 되면 플레이어는 패배하고 딜러는 승리한다.")
-        @Test
-        void 플레이어_버스트_시_무조건_플레이어는_패배한다() {
-            distributePlayerCards(onePlayerGame, new Name("피즈"),
-                    new Card(Rank.JACK, Suit.CLOVER),
-                    new Card(Rank.QUEEN, Suit.CLOVER),
-                    new Card(Rank.EIGHT, Suit.CLOVER)
-            );
-
-            Players players = onePlayerGame.getPlayers();
-            int dealerWinCount = GameResult.calculateDealerResult(players.decideAllResults(dealer)).get(GameResult.WIN);
-
-            assertSoftly(softly -> {
-                assertThat(onePlayerGame.getPlayerResult(new Name("피즈"))).isEqualTo(GameResult.LOSE);
-                assertThat(dealerWinCount).isEqualTo(1);
-            });
-        }
-
-        @DisplayName("플레이어가 버스트 되지 않았을 때 플레이어의 점수가 더 높으면 플레이어가 승리하고 딜러는 패배한다.")
-        @Test
-        void 플레이어_점수_더_높으면_플레이어_승리한다() {
-            distributePlayerCards(onePlayerGame, new Name("피즈"),
-                    new Card(Rank.JACK, Suit.CLOVER),
-                    new Card(Rank.QUEEN, Suit.CLOVER),
-                    new Card(Rank.ACE, Suit.CLOVER)
-            );
-
-            dealer.addCard(new Card(Rank.JACK, Suit.HEART));
-            dealer.addCard(new Card(Rank.QUEEN, Suit.HEART));
-
-            assertSoftly(softly -> {
-                assertThat(onePlayerGame.getPlayerResult(new Name("피즈"))).isEqualTo(GameResult.WIN);
-                assertThat(onePlayerGame.getDealerResult().get(GameResult.LOSE)).isEqualTo(1);
-            });
-        }
-
-        @DisplayName("플레이어가 버스트 되지 않았을 때 딜러가 버스트된 경우 플레이어가 승리한다.")
-        @Test
-        void 딜러가_버스트된_경우_플레이어가_승리한다() {
-
-            distributePlayerCards(onePlayerGame, new Name("피즈"),
-                    new Card(Rank.JACK, Suit.CLOVER),
-                    new Card(Rank.QUEEN, Suit.CLOVER)
-            );
-
-            dealer.addCard(new Card(Rank.JACK, Suit.HEART));
-            dealer.addCard(new Card(Rank.SIX, Suit.HEART));
-            dealer.addCard(new Card(Rank.SEVEN, Suit.HEART));
-
-            assertSoftly(softly -> {
-                assertThat(onePlayerGame.getPlayerResult(new Name("피즈"))).isEqualTo(GameResult.WIN);
-                assertThat(onePlayerGame.getDealerResult().get(GameResult.LOSE)).isEqualTo(1);
-            });
-        }
-
-        @DisplayName("플레이어가 버스트 되지 않았을 때 플레이어의 점수가 더 낮으면 플레이어가 패배하고 딜러는 승리한다.")
-        @Test
-        void 플레이어_점수_더_높으면_플레이어_패배한다() {
-            distributePlayerCards(onePlayerGame, new Name("피즈"),
-                    new Card(Rank.JACK, Suit.CLOVER),
-                    new Card(Rank.NINE, Suit.CLOVER)
-            );
-
-            dealer.addCard(new Card(Rank.JACK, Suit.HEART));
-            dealer.addCard(new Card(Rank.QUEEN, Suit.HEART));
-
-            assertSoftly(softly -> {
-                assertThat(onePlayerGame.getPlayerResult(new Name("피즈"))).isEqualTo(GameResult.LOSE);
-                assertThat(onePlayerGame.getDealerResult().get(GameResult.WIN)).isEqualTo(1);
-            });
-        }
-
-        @DisplayName("플레이어가 버스트 되지 않았을 때 플레이어와 딜러의 점수가 같으면 무승부가 된다.")
-        @Test
-        void 플레이어_딜러_점수_같으면_무승부_된다() {
-
-            distributePlayerCards(onePlayerGame, new Name("피즈"),
-                    new Card(Rank.JACK, Suit.CLOVER),
-                    new Card(Rank.QUEEN, Suit.CLOVER)
-            );
-
-            dealer.addCard(new Card(Rank.JACK, Suit.HEART));
-            dealer.addCard(new Card(Rank.QUEEN, Suit.HEART));
-
-            //then
-            assertSoftly(softly -> {
-                assertThat(onePlayerGame.getPlayerResult(new Name("피즈"))).isEqualTo(GameResult.DRAW);
-                assertThat(onePlayerGame.getDealerResult().get(GameResult.DRAW)).isEqualTo(1);
-            });
-        }
-
-        @DisplayName("플레이어가 카드 2장으로 블랙잭이 되어 승리하면 BLACKJACK_WIN이 된다")
-        @Test
-        void 플레이어_카드_2장_블랙잭_승리_시_블랙잭_승리_된다() {
-            //given
-            distributePlayerCards(onePlayerGame, new Name("피즈"),
-                    new Card(Rank.JACK, Suit.CLOVER),
-                    new Card(Rank.ACE, Suit.CLOVER)
-            );
-
-            dealer.addCard(new Card(Rank.JACK, Suit.HEART));
-            dealer.addCard(new Card(Rank.QUEEN, Suit.HEART));
-
-            assertSoftly(softly -> {
-                assertThat(onePlayerGame.getPlayerResult(new Name("피즈"))).isEqualTo(GameResult.BLACKJACK_WIN);
-                assertThat(onePlayerGame.getDealerResult().get(GameResult.LOSE)).isEqualTo(1);
-            });
-        }
-
-        @DisplayName("딜러가 블랙잭이고 플레이어가 카드 3장으로 21점이 되면 플레이어가 패배한다.")
-        @Test
-        void 딜러_블랙잭_플레이어_패배() {
-            //given
-            distributePlayerCards(onePlayerGame, new Name("피즈"),
-                    new Card(Rank.JACK, Suit.CLOVER),
-                    new Card(Rank.EIGHT, Suit.CLOVER),
-                    new Card(Rank.THREE, Suit.CLOVER)
-            );
-
-            dealer.addCard(new Card(Rank.JACK, Suit.HEART));
-            dealer.addCard(new Card(Rank.ACE, Suit.HEART));
-
-            assertSoftly(softly -> {
-                assertThat(onePlayerGame.getPlayerResult(new Name("피즈"))).isEqualTo(GameResult.LOSE);
-                assertThat(onePlayerGame.getDealerResult().get(GameResult.WIN)).isEqualTo(1);
-            });
-        }
-    }*/
 
     private void distributePlayerCards(Game game, Name name, Card... cards) {
         Deck testDeck = new Deck(Arrays.asList(cards));
