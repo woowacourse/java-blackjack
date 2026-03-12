e0d0707e7f88e8ce6f18a4e0efb6dd4ae64eb05e
mvg01 <dkms625@naver.com>
2026-03-12 13:24:53 +0900

    refactor: 의미 없는 필드 삭제

--- src/main/java/controller/DealerTurnPhase.java
+++ src/main/java/controller/DealerTurnPhase.java
@@ -5,16 +5,14 @@ import model.Dealer;
 import view.OutputView;
 
 public class DealerTurnPhase implements GamePhase {
-    private Dealer dealer;
-    private CardDispenser cardDispenser;
 
     public DealerTurnPhase() {
     }
 
     @Override
     public void execute(GameContext gameContext) {
-        this.dealer = gameContext.getDealer();
-        this.cardDispenser = gameContext.getCardDispenser();
+        Dealer dealer = gameContext.getDealer();
+        CardDispenser cardDispenser = gameContext.getCardDispenser();
         while (dealer.canHit()) {
             OutputView.printToOpenDealerNewCard(dealer.getName());
             cardDispenser.dispenseOneCard(dealer);
