6ac1af800db424e2d84a21da1d80d0ec05d649fb
Junha Park <poiu7825@pusan.ac.kr>
2026-03-12 13:48:25 +0900

    refactor: 플레이어 이름 클래스 추가에 따른 코드 수정

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -1,24 +1,12 @@
 package blackjack.domain;
 
-import java.util.List;
-
 public class Player extends Participant {
-    private static final int NICKNAME_MINIMUM_LENGTH = 4;
-    private static final int NICKNAME_MAXIMUM_LENGTH = 10;
 
-    private final String nickname;
-
+    private final Nickname nickname;
 
     public Player(final Hand hand, final Status status, final String nickname) {
         super(hand, status);
-        validateNicknameLength(nickname);
-        this.nickname = nickname;
-    }
-
-    private void validateNicknameLength(final String nickname) {
-        if (nickname.length() > NICKNAME_MAXIMUM_LENGTH || nickname.length() < NICKNAME_MINIMUM_LENGTH) {
-            throw new IllegalArgumentException("닉네임은 4~10자 이어야 합니다.");
-        }
+        this.nickname = new Nickname(nickname);
     }
 
     public GameResult calculateGameResult(Dealer dealer) {
@@ -35,7 +23,7 @@ public class Player extends Participant {
     }
 
     @Override
-    public String getNickname() {
+    public Name getName() {
         return nickname;
     }
 
