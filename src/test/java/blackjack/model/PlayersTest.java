b6c55c2117dceb0b05b751cf537d376386bd93f4
smini <sm020320@naver.com>
2026-03-12 15:45:58 +0900

    test: Players 테스트에 카드 분배 및 승패 판단 테스트 추가

--- src/test/java/blackjack/model/PlayersTest.java
+++ src/test/java/blackjack/model/PlayersTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -39,4 +40,44 @@ class PlayersTest {
                 .hasMessage(ErrorMessage.INVALID_PLAYERS.getMessage());
     }
 
+    @Test
+    @DisplayName("카드를 2장을 나눠준다.")
+    void giveTwoCardsTest() {
+        // given
+        Deck deck = new Deck();
+        Player luke = new Player("luke", 1000);
+        Player sm = new Player("sm", 1000);
+        Players players = new Players(List.of(luke, sm));
+        // when
+        players.giveTwoCards(deck);
+        // then
+        assertThat(luke.getHandCards().size()).isEqualTo(2);
+        assertThat(sm.getHandCards().size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("모든 플레이어의 게임 결과를 심판이 판정하여 Map으로 반환한다.")
+    void judgeAllTest() {
+        // given
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(new Card(Figure.SPADE, Number.TEN));
+        dealer.receiveCard(new Card(Figure.HEART, Number.TEN));
+
+        Player luke = new Player("luke", 1000);
+        luke.receiveCard(new Card(Figure.DIAMOND, Number.ACE));
+        luke.receiveCard(new Card(Figure.CLOVER, Number.TEN));
+
+        Player sm = new Player("sm", 1000);
+        sm.receiveCard(new Card(Figure.HEART, Number.TWO));
+        sm.receiveCard(new Card(Figure.SPADE, Number.EIGHT));
+
+        Players players = new Players(List.of(luke, sm));
+        Referee referee = new Referee();
+        // when
+        Map<Player, GameResult> results = players.judgeAll(dealer, referee);
+        // then
+        assertThat(results).hasSize(2);
+        assertThat(results.get(luke)).isEqualTo(GameResult.BLACKJACK);
+        assertThat(results.get(sm)).isEqualTo(GameResult.LOSE);
+    }
 }
