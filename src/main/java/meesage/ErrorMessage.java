644cd1c077aec3fe44ceb328fac640e86fe34813
haeyoon1 <hynam1220@naver.com>
2026-03-12 16:42:58 +0900

    feat: error메시지 추가

--- src/main/java/meesage/ErrorMessage.java
+++ src/main/java/meesage/ErrorMessage.java
@@ -2,9 +2,11 @@ package meesage;
 
 public enum ErrorMessage {
 
-    EMPTY_INPUT("[ERROR] 사용자 이름은 공백을 입력할 수 없습니다."),
+    EMPTY_NAME_INPUT("[ERROR] 사용자 이름은 공백을 입력할 수 없습니다."),
     INVALID_NAMES_EMPTY("[ERROR] 하나 이상의 사용자 이름을 입력하세요."),
     INVALID_NAMES_EXCEED_LIMIT("[ERROR] 25명 이하의 사용자 이름을 입력하세요."),
+    EMPTY_BET_AMOUNT_INPUT("[ERROR]배팅 금액을 입력하세요."),
+    INVALID_BET_AMOUNT("[ERROR]배팅 금액은 0보다 큰 정수만 입력 가능합니다."),
     INVALID_YES_OR_NO_INPUT("[ERROR] 카드를 추가할 경우 y, 추가하지 않을 경우 n을 입력하세요."),
     ;
 
