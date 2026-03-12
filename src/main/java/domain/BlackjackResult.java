404ab774335f492b2c2dbc6622d250021a869c21
haeyoon1 <hynam1220@naver.com>
2026-03-12 16:47:45 +0900

    feat: 플레이어 별 BetResult를 담은 BlackjackResult 클래스 수정
    - add: 플레이어 별 BetResult 초기화하는 메서드 추가
    - resolveResults, resolveResult: 플레이어와 딜러의 카드를 비교해 최종 수익을 업데이트
    - compareResult: 플레이어와 딜러의 점수를 비교해 승패무 여부를 반환
    - dealerProfit: 딜러의 최종 수익을 반환
    - playerProfit: 플레이어의 최종 수익을 반환

--- src/main/java/domain/BlackjackResult.java
+++ src/main/java/domain/BlackjackResult.java
@@ -5,26 +5,39 @@ import java.util.Map;
 
 public class BlackjackResult {
 
-    private final Map<Player, GameResult> playerResult = new LinkedHashMap<>();
+    private final Map<Player, BetResult> playerBets = new LinkedHashMap<>();
 
-    private BlackjackResult(Dealer dealer, Players players) {
-        compareResult(dealer, players);
+    private BlackjackResult() {
     }
 
-    public static BlackjackResult of(Dealer dealer, Players players) {
-        return new BlackjackResult(dealer, players);
+    public static BlackjackResult of() {
+        return new BlackjackResult();
     }
 
-    public void compareResult(Dealer dealer, Players players) {
+    public void add(Player player, long betAmount) {
+        if (player.getCards().calculateScore() == 21) {
+            playerBets.put(player, BetResult.withBetAmount((long) (betAmount * 1.5)));
+            return;
+        }
+        playerBets.put(player, BetResult.withBetAmount(betAmount));
+    }
+
+    public void resolveResults(Players players, Dealer dealer) {
         for (Player player : players.getPlayers()) {
-            int playerScore = player.getScoreOrZeroIfBust();
-            int dealerScore = dealer.getScoreOrZeroIfBust();
-
-            playerResult.put(player, judgeResult(playerScore, dealerScore));
+            resolveResult(player, dealer);
         }
     }
 
-    private GameResult judgeResult(int playerScore, int dealerScore) {
+    private void resolveResult(Player player, Dealer dealer) {
+        BetResult betResult = playerBets.get(player);
+        GameResult gameResult = compareResult(dealer, player);
+        playerBets.put(player, betResult.withGameResult(gameResult));
+    }
+
+    public GameResult compareResult(Dealer dealer, Player player) {
+        int playerScore = player.getScoreOrZeroIfBust();
+        int dealerScore = dealer.getScoreOrZeroIfBust();
+
         if (playerScore > dealerScore) {
             return GameResult.WIN;
         }
@@ -32,25 +45,29 @@ public class BlackjackResult {
             return GameResult.LOSE;
         }
         return GameResult.DRAW;
-
     }
 
-    public DealerResultDto getDealerResult() {
-        return DealerResultDto.of(
-                countPlayerResult(GameResult.LOSE),
-                countPlayerResult(GameResult.DRAW),
-                countPlayerResult(GameResult.WIN)
-        );
+    public long dealerProfit() {
+        long dealerProfit = 0;
+        for (Map.Entry<Player, BetResult> playerBet : playerBets.entrySet()) {
+            dealerProfit += playerProfit(playerBet);
+        }
+        return -1 * dealerProfit;
     }
 
-    private int countPlayerResult(GameResult gameResult) {
-        return (int) playerResult.values()
-                .stream()
-                .filter(value -> value == gameResult)
-                .count();
+    public long playerProfit(Map.Entry<Player, BetResult> playerBet) {
+        BetResult betResult = playerBet.getValue();
+        GameResult gameResult = betResult.getGameResult();
+        if (gameResult == GameResult.WIN) {
+            return betResult.getBetAmount();
+        }
+        if (gameResult == GameResult.LOSE) {
+            return -1 * betResult.getBetAmount();
+        }
+        return 0;
     }
 
-    public Map<Player, GameResult> getPlayerResults() {
-        return playerResult;
+    public Map<Player, BetResult> getPlayerBets() {
+        return playerBets;
     }
 }
