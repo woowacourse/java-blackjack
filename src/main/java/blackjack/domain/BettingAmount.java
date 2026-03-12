9467c35c9cf04dee2dac153d141c6fca0618f96e
jyt6640 <jyt6640@gmail.com>
2026-03-12 15:02:04 +0900

    feat: 배팅 금액이 0원일 경우 예외 반환 기능 구현

--- src/main/java/blackjack/domain/BettingAmount.java
+++ src/main/java/blackjack/domain/BettingAmount.java
@@ -11,11 +11,18 @@ public class BettingAmount {
 
     private void validate(int amount) {
         minus(amount);
+        zero(amount);
     }
 
     private void minus(int amount) {
         if (amount < 0) {
             throw new IllegalArgumentException("[ERROR] 베팅 금액은 음수일 수 없습니다.");
+        }
+    }
+
+    private void zero(int amount) {
+        if (amount == 0) {
+            throw new IllegalArgumentException("[ERROR] 배팅 금액은 0일 수 없습니다.");
         }
     }
 }
