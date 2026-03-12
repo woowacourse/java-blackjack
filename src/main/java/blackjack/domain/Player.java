9c9214c68a1a0d380de1d86716250c5871b096ed
sinequanonn <dleck288@gmail.com>
2026-03-12 09:35:05 +0900

    refactor: 플레이어에게 배팅 금액 상태 추가

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -4,13 +4,19 @@ public class Player extends Participant {
     private static final int MAX_CARD_SUM = 21;
 
     private final Name name;
+    private final BettingAmount bettingAmount;
 
-    public Player(String name) {
+    public Player(String name, BettingAmount bettingAmount) {
         this.name = new Name(name);
+        this.bettingAmount = bettingAmount;
     }
 
     public String getName() {
         return name.getName();
+    }
+
+    public int calculateProfit(double ratio) {
+        return bettingAmount.calculateProfit(ratio);
     }
 
     @Override
