4ba605ba1582daf97d0b1369e8d67e33feb15ad1
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-12 13:56:48 +0900

    refactor(BlackJackService): 플레이어 결과 반환 메서드 통합

--- src/main/java/service/BlackJackService.java
+++ src/main/java/service/BlackJackService.java
@@ -68,49 +68,36 @@ public class BlackJackService {
     }
 
     private MatchStatus getPlayerResult(Player player, Dealer dealer) {
-        MatchStatus blackJackResult = checkBlackJack(player, dealer);
-        if (blackJackResult != null) {
-            return blackJackResult;
-        }
+        boolean isPlayerBlackJack = isBlackJack(player);
+        boolean isDealerBlackJack = isBlackJack(dealer);
+        boolean isPlayerBust = isBust(player);
+        boolean isDealerBust = isBust(dealer);
+        int playerScore = player.getScore();
+        int dealerScore = dealer.getScore();
 
-        MatchStatus bustResult = checkBust(player, dealer);
-        if (bustResult != null) {
-            return bustResult;
-        }
-
-        return compareScore(player, dealer);
-    }
-
-    private MatchStatus checkBlackJack(Player player, Dealer dealer) {
-        if (isBlackJack(player) && isBlackJack(dealer)) {
+        if (isPlayerBlackJack && isDealerBlackJack) {
             return MatchStatus.DRAW;
         }
-        if (isBlackJack(player)) {
+        if (isPlayerBlackJack) {
             return MatchStatus.BLACKJACK;
         }
-        if (isBlackJack(dealer)) {
+        if (isDealerBlackJack) {
             return MatchStatus.LOSE;
         }
-        return null;
-    }
-
-    private MatchStatus checkBust(Player player, Dealer dealer) {
-        if (isBust(player)) {
+        if (isPlayerBust) {
             return MatchStatus.LOSE;
         }
-        if (isBust(dealer)) {
+        if (isDealerBust) {
             return MatchStatus.WIN;
         }
-        return null;
+        if (playerScore > dealerScore) {
+            return MatchStatus.WIN;
+        }
+        if (playerScore < dealerScore) {
+            return MatchStatus.LOSE;
+        }
+        return MatchStatus.DRAW;
     }
 
-    private MatchStatus compareScore(Player player, Dealer dealer) {
-        if (Objects.equals(player.getScore(), dealer.getScore())) {
-            return MatchStatus.DRAW;
-        }
-        if (player.getScore() > dealer.getScore()) {
-            return MatchStatus.WIN;
-        }
-        return MatchStatus.LOSE;
-    }
+
 }
