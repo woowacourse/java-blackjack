5a9a8d37483dae2f2b2b1aa85ddfcb0dff702e91
sun <sun007021@gmail.com>
2026-03-12 14:46:57 +0900

    test: 플레이어가 블랙잭이 아닌 점수로 승리시 수익 계산

---
+++ src/test/java/PlayerBetTest.java
@@ -0,0 +1,16 @@
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import blackjack.domain.Player;
+import blackjack.domain.ScoreCompareResult;
+import org.junit.jupiter.api.Test;
+
+public class PlayerBetTest {
+    @Test
+    void calculate_profit_when_player_win_except_blackjack() {
+        Player player = new Player("player");
+
+        PlayerBetting playerBetting = new PlayerBetting(player, 1000);
+
+        assertThat(playerBetting.calculateProfit(ScoreCompareResult.PLAYER_WIN)).isEqualTo(1000);
+    }
+}
