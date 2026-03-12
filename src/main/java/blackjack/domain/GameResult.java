49b9c499aede6f3cf4df449dffb0c220c1316bfc
Chaerin Lee <rin@Rin-MacBook-Air.local>
2026-03-12 18:54:50 +0900

    refactor: 미사용 메서드 정리

--- src/main/java/blackjack/domain/GameResult.java
+++ src/main/java/blackjack/domain/GameResult.java
@@ -1,7 +1,5 @@
 package blackjack.domain;
 
-import java.util.Arrays;
-
 public enum GameResult {
     WIN("승", 1),
     TIE("무", 0),
@@ -22,12 +20,5 @@ public enum GameResult {
 
     public double getEarningRate() {
         return earningRate;
-    }
-
-    public static GameResult fromName(String name) {
-        return Arrays.stream(GameResult.values())
-                .filter(result -> result.getName().equals(name))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("해당하는 게임 결과를 찾을 수 없습니다: " + name));
     }
 }
