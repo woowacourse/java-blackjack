7c7e7d9bc4551e89b25fee8c06e9b8d64c5f4d6f
dahxxn <dahyeonhan4529@gmail.com>
2026-03-12 17:46:33 +0900

    refactor: BetAmount 최소/최대금액 매직넘버 상수 설정

--- src/main/java/blackjack/domain/BetAmount.java
+++ src/main/java/blackjack/domain/BetAmount.java
@@ -1,6 +1,8 @@
 package blackjack.domain;
 
 public class BetAmount {
+    private static final int MINIMUM_AMOUNT = 1;
+    private static final int MAXIMUM_AMOUNT = 100000;
     private final int money;
 
     private BetAmount(int money) {
@@ -18,13 +20,13 @@ public class BetAmount {
     }
 
     private void validateMoneyOverMinimum(int money) {
-        if (money <= 0) {
+        if (money < MINIMUM_AMOUNT) {
             throw new IllegalArgumentException("배팅 금액은 최소 0원 이상이어야 합니다.");
         }
     }
 
     private void validateMoneyUnderMaximum(int money) {
-        if (money > 100000) {
+        if (money > MAXIMUM_AMOUNT) {
             throw new IllegalArgumentException("배팅 금액은 최대 100000원 이하이어야 합니다.");
         }
     }
