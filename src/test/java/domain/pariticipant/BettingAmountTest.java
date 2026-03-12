dc6c68ae0a59ee21161adeeb8cdb1a7fef2a3c7d
jhk01007 <jhk001007@naver.com>
2026-03-12 12:59:58 +0900

    test: DisplayName 수정

--- src/test/java/domain/pariticipant/BettingAmountTest.java
+++ src/test/java/domain/pariticipant/BettingAmountTest.java
@@ -1,10 +1,6 @@
 package domain.pariticipant;
 
-import constant.BlackjackConstant;
-import exception.ErrorMessage;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,10 +11,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BettingAmountTest {
 
-
     @ParameterizedTest
     @ValueSource(longs = {1, BETTING_LIMIT})
-    @DisplayName("베팅 금액은 0보다 크고 1,000,000 이하여야 한다.")
+    @DisplayName("베팅 금액은 0보다 크고 " + BETTING_LIMIT + " 이하여야 한다.")
     public void createBettingAmount_success(long bettingAmount) {
         // when // then
         assertThatCode(() -> new BettingAmount(bettingAmount))
@@ -27,7 +22,7 @@ class BettingAmountTest {
 
     @ParameterizedTest
     @ValueSource(longs = {0, BETTING_LIMIT + 1})
-    @DisplayName("베팅 금액은 0이하거나 1,000,000 보다 크면 예외가 발생한다.")
+    @DisplayName("베팅 금액은 0이하거나 " + BETTING_LIMIT + " 보다 크면 예외가 발생한다.")
     public void createBettingAmount_fail(long bettingAmount) {
         // when // then
         assertThatThrownBy(() -> new BettingAmount(bettingAmount))
