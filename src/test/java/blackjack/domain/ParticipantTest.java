f4a5c8ac93c5254137b29e1427db3ae9c3beffcb
Chaerin Lee <rin@Rin-MacBook-Air.local>
2026-03-12 12:45:06 +0900

    test(Participant): 누락된 검증문 추가

--- src/test/java/blackjack/domain/ParticipantTest.java
+++ src/test/java/blackjack/domain/ParticipantTest.java
@@ -1,5 +1,7 @@
 package blackjack.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 public class ParticipantTest {
@@ -8,5 +10,6 @@ public class ParticipantTest {
         Participant participant = new Player("이산", new Hand());
         Deck deck = new Deck();
         participant.receiveCard(deck.draw());
+        assertThat(participant.getCardCount()).isEqualTo(1);
     }
 }
