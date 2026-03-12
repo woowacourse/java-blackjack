19419c90203ca312567d631e27d73be31303258e
Sumin <new.sumyang@gmail.com>
2026-03-12 13:24:34 +0900

    test: ParticipantResult dto 기능 테스트 추가

---
+++ src/test/java/blackjack/dto/ParticipantResultTest.java
@@ -0,0 +1,65 @@
+package blackjack.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import blackjack.domain.Card;
+import blackjack.domain.PlayingCards;
+import blackjack.domain.Rank;
+import blackjack.domain.Suit;
+import blackjack.domain.participant.Player;
+import blackjack.domain.participant.Role;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ParticipantResultTest {
+
+    @Test
+    @DisplayName("참가자 결과 dto 생성을 확인한다.")
+    void makeParticipantResult() {
+        // given
+        Player player = new Player("boye", Role.PLAYER, 100000);
+        player.receiveCard(PlayingCards.from(List.of(new Card(Rank.TEN, Suit.SPADE))));
+
+        // when
+        ParticipantResult participantResult = ParticipantResult.from(player);
+
+        // then
+        assertAll(
+            () -> assertThat(participantResult.nickname()).isEqualTo("boye"),
+            () -> assertThat(participantResult.cardStatus()).isEqualTo("10스페이드"),
+            () -> assertThat(participantResult.totalScore()).isEqualTo(10)
+        );
+    }
+
+    @Test
+    @DisplayName("카드 결과 string 생성을 확인한다.")
+    void makeParticipantResultToString() {
+        // given
+        Player player = new Player("boye", Role.PLAYER, 100000);
+        player.receiveCard(PlayingCards.from(List.of(new Card(Rank.TEN, Suit.SPADE))));
+        ParticipantResult participantResult = ParticipantResult.from(player);
+
+        // when
+        String participantResultString = participantResult.toString();
+
+        // then
+        assertThat(participantResultString).isEqualTo("boye카드: 10스페이드");
+    }
+
+    @Test
+    @DisplayName("결과값을 포함한 카드 결과 string 생성을 확인한다.")
+    void participantResultFullString() {
+        // given
+        Player player = new Player("boye", Role.PLAYER, 100000);
+        player.receiveCard(PlayingCards.from(List.of(new Card(Rank.TEN, Suit.SPADE))));
+        ParticipantResult participantResult = ParticipantResult.from(player);
+
+        // when
+        String participantResultString = participantResult.toFullString();
+
+        // then
+        assertThat(participantResultString).isEqualTo("boye카드: 10스페이드 - 결과: 10");
+    }
+}
