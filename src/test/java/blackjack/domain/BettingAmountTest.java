e67c2ddaa082bbb8a6f62bf948c542f42b4243a8
jyt6640 <jyt6640@gmail.com>
2026-03-12 14:16:39 +0900

    test: 배팅 금액이 음수이면 에러 발생 테스트 추가

---
+++ src/test/java/blackjack/domain/BettingAmountTest.java
@@ -0,0 +1,23 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import blackjack.domain.deck.BettingAmount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BettingAmountTest {
+
+    @Test
+    @DisplayName("배팅 금액이 양수인지 검증")
+    void validate_plus() {
+        // given
+        int input = -10000;
+
+        // when & then
+        assertThatThrownBy(() -> new BettingAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("음수");
+
+    }
+}
