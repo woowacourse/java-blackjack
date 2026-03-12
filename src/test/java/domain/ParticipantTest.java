aba4c9ebb075261128dd729ca408eae757b6d3ef
symflee <symflee@gmail.com>
2026-03-12 14:34:09 +0900

    test(ParticipatnTest): 테스트 내부의 의존성 제거

--- src/test/java/domain/ParticipantTest.java
+++ src/test/java/domain/ParticipantTest.java
@@ -1,67 +1,50 @@
 package domain;
 
+import domain.card.Card;
+import domain.card.Rank;
+import domain.card.Suit;
 import domain.participant.Dealer;
 import domain.participant.Name;
 import domain.participant.Participant;
 import domain.participant.Player;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ParticipantTest {
 
     @Test
     void 플레이어의_핸드_점수가_21을_초과하면_카드를_더_받을_수_없다() {
-        GameManager gameManager = new GameManager(Deck.create());
-
-        // A K Q J 10 9 8 7 6 순서 고정
-        Player player = new Player(Name.from("나무"));
-
-        gameManager.dealCard(player); // A
-        gameManager.dealCard(player); // K
-        gameManager.dealCard(player); // Q
-        gameManager.dealCard(player); // J
+        Player player = new Player(Name.from("고래"));
+        player.addCard(new Card(Suit.SPADE, Rank.TWO));
+        player.addCard(new Card(Suit.SPADE, Rank.KING));
+        player.addCard(new Card(Suit.DIAMOND, Rank.KING));
 
         Assertions.assertThat(player.canReceive()).isFalse();
     }
 
     @Test
     void 플레이어의_핸드_점수가_21_이하면_카드를_더_받을_수_있다() {
-        GameManager gameManager = new GameManager(Deck.create());
-
-        // A K Q J 10 9 8 7 6 순서 고정
         Player player = new Player(Name.from("나무"));
-
-        gameManager.dealCard(player); // A
-        gameManager.dealCard(player); // K
-        gameManager.dealCard(player); // Q
+        player.addCard(new Card(Suit.SPADE, Rank.ACE));
+        player.addCard(new Card(Suit.SPADE, Rank.KING));
 
         Assertions.assertThat(player.canReceive()).isTrue();
     }
 
     @Test
     void 딜러의_핸드_점수가_16_이하면_카드를_더_받을_수_있다() {
-        GameManager gameManager = new GameManager(Deck.create());
-
-        // A K Q J 10 9 8 7 6 순서 고정
         Dealer dealer = new Dealer();
+        dealer.addCard(new Card(Suit.SPADE, Rank.TEN));
+        dealer.addCard(new Card(Suit.SPADE, Rank.SIX));
 
-        gameManager.dealCard(dealer); // A
-        gameManager.dealCard(dealer); // K
-
-        Assertions.assertThat(dealer.canReceive()).isFalse();
+        Assertions.assertThat(dealer.canReceive()).isTrue();
     }
 
     @Test
     void 딜러의_핸드_점수가_17_이상이면_카드를_더_받을_수_없다() {
-        GameManager gameManager = new GameManager(Deck.create());
-
-        // A K Q J 10 9 8 7 6 순서 고정
         Dealer dealer = new Dealer();
-
-        gameManager.dealCard(dealer); // A
-        gameManager.dealCard(dealer); // K
-        gameManager.dealCard(dealer); // Q
+        dealer.addCard(new Card(Suit.SPADE, Rank.TEN));
+        dealer.addCard(new Card(Suit.SPADE, Rank.SEVEN));
 
         Assertions.assertThat(dealer.canReceive()).isFalse();
     }
