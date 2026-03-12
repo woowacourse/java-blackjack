0c3b1651a71d1da2955e9b83e50a8e6409082ba7
tjdakf <ksc73450056@gmail.com>
2026-03-12 17:11:11 +0900

    feat: 베팅 금액 단위 예외 처리 기능 추가

--- src/main/java/domain/Bet.java
+++ src/main/java/domain/Bet.java
@@ -2,10 +2,28 @@ package domain;
 
 public class Bet {
 
+    private static final int UNIT_OF_BET = 1000;
     private final int amount;
 
     public Bet(String amount) {
-        this.amount = parseAmount(amount);
+        this.amount = validate(amount);
+    }
+
+    public int getBetAmount() {
+        return amount;
+    }
+
+    private int validate(String amount) {
+        int parsedAmount = parseAmount(amount);
+        validateUnit(parsedAmount);
+
+        return parsedAmount;
+    }
+
+    private void validateUnit(int parsedAmount) {
+        if (parsedAmount % UNIT_OF_BET != 0) {
+            throw new IllegalArgumentException("[ERROR] 베팅 금액은 1000원 단위로만 입력 가능합니다.");
+        }
     }
 
     private int parseAmount(String amount) {
@@ -15,5 +33,4 @@ public class Bet {
             throw new IllegalArgumentException("[ERROR] 베팅 금액은 숫자만 입력 가능합니다.");
         }
     }
-
 }
--- src/test/java/domain/BetTest.java
+++ src/test/java/domain/BetTest.java
@@ -2,14 +2,31 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BetTest {
 
     @Test
+    void 베팅_금액을_저장한다() {
+        Bet bet = new Bet("30000");
+
+        int betAmount = bet.getBetAmount();
+
+        assertThat(betAmount).isEqualTo(30000);
+    }
+
+    @Test
     void 베팅_금액을_숫자가_아닌_값으로_입력하면_예외가_발생한다() {
         assertThatThrownBy(
                 () -> new Bet("abc")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 베팅_금액의_단위가_1000이_아니면_예외가_발생한다() {
+        assertThatThrownBy(
+                () -> new Bet("1500")
         ).isInstanceOf(IllegalArgumentException.class);
     }
 }
