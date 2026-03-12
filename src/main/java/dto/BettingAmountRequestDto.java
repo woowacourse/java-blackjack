41ac81fe83aaf602fa642ca017ed6b9a23004e81
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-12 20:25:51 +0900

    feat(BettingAmountRequestDto): 베팅 금액 null, 공백 입력 에러 발생 기능

--- src/main/java/dto/BettingAmountRequestDto.java
+++ src/main/java/dto/BettingAmountRequestDto.java
@@ -1,4 +1,14 @@
 package dto;
 
 public record BettingAmountRequestDto(String bettingAmount) {
+
+    public BettingAmountRequestDto {
+        validateBettingAmountIsNotNullAndNotBlank(bettingAmount);
+    }
+
+    private void validateBettingAmountIsNotNullAndNotBlank(String bettingAmount) {
+        if (bettingAmount == null || bettingAmount.isBlank()) {
+            throw new IllegalArgumentException("베팅 금액은 null 또는 공백이면 안 됩니다.");
+        }
+    }
 }
