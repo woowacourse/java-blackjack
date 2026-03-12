a071ec990664db846fbbec9de964679305efd4e7
symflee <symflee@gmail.com>
2026-03-12 13:14:24 +0900

    refactor: Enum Suit에서 카드 문자열 표현 책임 분리

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -5,12 +5,7 @@ public record Card(Suit suit, Rank cardNumber) {
         return this.cardNumber == Rank.ACE;
     }
 
-    public String suitValue() {
-        return this.suit.getValue();
-    }
-
     public String symbol() {
         return this.cardNumber.getSymbol();
     }
-
 }
