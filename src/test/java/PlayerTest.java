152762a8310f8ed72466831dec7aa741f8ad5f70
sun <sun007021@gmail.com>
2026-03-12 11:58:14 +0900

    test: 플레이어 블랙잭 판단

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -17,4 +17,20 @@ public class PlayerTest {
         player2.receiveOneCard(new Card("10", "스페이드"));
         assertThat(player2.calculateTotalScore()).isEqualTo(13);
     }
+
+    @Test
+    void player_is_blackjack_when_score_is_21() {
+        Player player = new Player("player");
+        player.receiveOneCard(new Card("A", "하트"));
+        player.receiveOneCard(new Card("J", "스페이드"));
+        assertThat(player.isBlackjack()).isTrue();
+    }
+
+    @Test
+    void player_is_not_blackjack_when_score_is_20() {
+        Player player = new Player("player");
+        player.receiveOneCard(new Card("Q", "하트"));
+        player.receiveOneCard(new Card("J", "스페이드"));
+        assertThat(player.isBlackjack()).isFalse();
+    }
 }
