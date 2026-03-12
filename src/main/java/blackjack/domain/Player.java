1efb71c2d53f938428afa458201e1e5d10d102b8
haeseon lee <haeseon4816@gmail.com>
2026-03-12 16:19:28 +0900

    refactor: Player 불필요한 필드 & 메서드 삭제

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -1,14 +1,7 @@
 package blackjack.domain;
 
 public class Player extends Participant {
-    private final String name;
-
     public Player(String name) {
         super(name);
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
     }
 }
