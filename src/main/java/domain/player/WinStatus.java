a0ca40e0a18871015ceecb8679db6ddeeb0b77ae
ChoSeongHyeon <00seong116@naver.com>
2026-03-12 22:53:30 +0900

    feat(WinStatus): 배율 추가

--- src/main/java/domain/player/WinStatus.java
+++ src/main/java/domain/player/WinStatus.java
@@ -1,17 +1,24 @@
 package domain.player;
 
 public enum WinStatus {
-    WIN("승"),
-    DRAW("무"),
-    LOSE("패");
+    WIN("승", 1.0),
+    DRAW("무", 0),
+    LOSE("패", -1.0),
+    BLACKJACK_WIN("승", 1.5);
 
     private final String status;
+    private final double earningsRate;
 
-    WinStatus(String status) {
+    WinStatus(String status, double earningsRate) {
         this.status = status;
+        this.earningsRate = earningsRate;
     }
 
     public String getStatus() {
         return status;
+    }
+
+    public double getEarningsRate() {
+        return earningsRate;
     }
 }
