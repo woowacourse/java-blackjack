53d2e4dfc19129f2f1d575caaa75aab52690908f
jyt6640 <jyt6640@gmail.com>
2026-03-12 14:38:09 +0900

    feat: 게임 결과에 따른 수익 반환 기능 구현

---
+++ src/main/java/blackjack/domain/GameResultType.java
@@ -0,0 +1,19 @@
+package blackjack.domain;
+
+public enum GameResultType {
+
+    BLACKJACK_WIN(1.5),
+    WIN(1.0),
+    DRAW(0.0),
+    LOSE(-1.0);
+
+    private final double profit;
+
+    GameResultType(double profit) {
+        this.profit = profit;
+    }
+
+    public int calculateProfit(int amount) {
+        return (int) (amount * profit);
+    }
+}
