2cfeb41022936e0bc982cacbdf37821cabd342d7
jhk01007 <jhk001007@naver.com>
2026-03-12 13:10:09 +0900

    refactor: 은(는) 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -20,7 +20,7 @@ public class InputView {
     }
 
     public boolean readHitOrStand(String name) {
-        System.out.println(name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+        System.out.println(name + "은(는) 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
         String input = scanner.nextLine();
 
         validateIsBlank(input);
