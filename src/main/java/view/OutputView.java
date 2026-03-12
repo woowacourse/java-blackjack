efa16eb43e30b7eb2b0dc7d9c463155566eec565
최용준 <yj9107vv@gmail.com>
2026-03-12 17:24:40 +0900

    feat(OutputView): count만큼 반복 출력 메서드 추가
    - printRepeated()

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,12 +4,18 @@ public class OutputView {
 
     private static final String lineSeparator = System.lineSeparator();
 
-    public void println(String message) {
-        System.out.println(message);
+    public void println(String outputMessage) {
+        System.out.println(outputMessage);
     }
 
     public void printfList(String outputMessage, String value) {
         System.out.printf(outputMessage, value);
+    }
+
+    public void printRepeated(String outputMessage, int repeatCount) {
+        for (int i = 0; i < repeatCount; i++) {
+            println(outputMessage);
+        }
     }
 
     public void separatorLine() {
