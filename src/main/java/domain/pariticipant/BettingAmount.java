b14e19cd9c32c6e7e1f56901b8aadb893c62fd36
jhk01007 <jhk001007@naver.com>
2026-03-12 11:35:31 +0900

    feat: 베팅 상한 추가

--- src/main/java/domain/pariticipant/BettingAmount.java
+++ src/main/java/domain/pariticipant/BettingAmount.java
@@ -1,10 +1,13 @@
 package domain.pariticipant;
 
+import static constant.BlackjackConstant.BETTING_LIMIT;
+import static exception.ErrorMessage.BETTING_LIMIT_OUT_OF_BOUNDS_ERROR;
+
 public record BettingAmount(long bettingAmount) {
 
     public BettingAmount {
-        if (bettingAmount <= 0 || bettingAmount > 1_000_000_000) {
-            throw new IllegalArgumentException("");
+        if (bettingAmount < 0 || bettingAmount > BETTING_LIMIT) {
+            throw new IllegalArgumentException(BETTING_LIMIT_OUT_OF_BOUNDS_ERROR.getMessage());
         }
     }
 }
