5904942bdc4a34aea0287ca2beae906b27b67e61
smini <sm020320@naver.com>
2026-03-12 09:57:47 +0900

    refactor: 승패 판단 로직 세분화 및 관련 메서드 분리

--- src/main/java/blackjack/model/Referee.java
+++ src/main/java/blackjack/model/Referee.java
@@ -6,6 +6,45 @@ public class Referee {
         Score playerScore = player.getScore();
         Score dealerScore = dealer.getScore();
 
+        GameResult blackjackResult = blackjackJudgement(player, dealer);
+        if (blackjackResult != null) {
+            return blackjackResult;
+        }
+
+        GameResult bustResult = bustJudgement(player, dealer);
+        if (bustResult != null) {
+            return bustResult;
+        }
+
+        GameResult scoreResult = ScoreJudgement(playerScore, dealerScore);
+        if (scoreResult != null) {
+            return scoreResult;
+        }
+
+        return GameResult.WIN;
+    }
+
+    private GameResult ScoreJudgement(Score playerScore, Score dealerScore) {
+        if (playerScore.isSame(dealerScore)) {
+            return GameResult.DRAW;
+        }
+        if (playerScore.isLess(dealerScore)) {
+            return GameResult.LOSE;
+        }
+        return null;
+    }
+
+    private GameResult bustJudgement(Player player, Dealer dealer) {
+        if (player.isBust()) {
+            return GameResult.LOSE;
+        }
+        if (dealer.isBust()) {
+            return GameResult.WIN;
+        }
+        return null;
+    }
+
+    private GameResult blackjackJudgement(Player player, Dealer dealer) {
         if (player.isBlackjack() && dealer.isBlackjack()) {
             return GameResult.DRAW;
         }
@@ -15,21 +54,6 @@ public class Referee {
         if (dealer.isBlackjack()) {
             return GameResult.LOSE;
         }
-
-        if (player.isBust()) {
-            return GameResult.LOSE;
-        }
-        if (dealer.isBust()) {
-            return GameResult.WIN;
-        }
-
-        if (playerScore.isSame(dealerScore)) {
-            return GameResult.DRAW;
-        }
-        if (playerScore.isLess(dealerScore)) {
-            return GameResult.LOSE;
-        }
-
-        return GameResult.WIN;
+        return null;
     }
 }
