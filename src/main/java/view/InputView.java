c3079cb41507c9068d6a1874b88d9a4076043eff
이동훈 <higgs95@naver.com>
2026-03-14 02:14:36 +0900

    refactor: Scanner 재사용을 위한 클래스 변수 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,9 +5,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private static Scanner sc = new Scanner(System.in);
 
     public static List<String> askPlayerNames() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
 
         String input = sc.nextLine();
@@ -17,7 +17,6 @@ public class InputView {
     }
 
     public static String askContinue(String player) {
-        Scanner sc = new Scanner(System.in);
         System.out.println(player + "는 한장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)");
         String input = sc.nextLine();
         validateContinueResponse(input);
