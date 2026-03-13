cbf803b229e95627fa6fb1a3c1724e8567c758ca
JinGoo <zzgg3040@naver.com>
2026-03-14 02:11:41 +0900

    refactor: score 에서 comparable 구현 제거

--- src/main/java/blackjack/domain/result/Score.java
+++ src/main/java/blackjack/domain/result/Score.java
@@ -1,14 +1,9 @@
 package blackjack.domain.result;
 
-public record Score(int value) implements Comparable<Score> {
-
-    @Override
-    public int compareTo(Score other) {
-        return Integer.compare(this.value, other.value);
-    }
+public record Score(int value) {
 
     public boolean isBiggerThan(Score other) {
-        return compareTo(other) > 0;
+        return this.value > other.value;
     }
 
     public boolean isBiggerThan(final int otherValue) {
