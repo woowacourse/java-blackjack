4ff24ce7a07f75f8e07643c7e65f8ede57704934
김가현 <hyeonnni@naver.com>
2026-03-13 17:46:13 +0900

    test(util): NameParser 반환 타입 변경에 맞춰 테스트 코드 수정

--- src/test/java/util/NameParserTest.java
+++ src/test/java/util/NameParserTest.java
@@ -2,7 +2,7 @@ package util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import domain.Players;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,16 +10,16 @@ class NameParserTest {
     @Test
     @DisplayName("이름 문자열을 쉼표로 분리해 플레이어 수를 만든다")
     void makePlayersSize() {
-        Players players = NameParser.makeNameList("pobi,jason");
+        List<String> names = NameParser.makeNameList("pobi,jason");
 
-        assertEquals(2, players.getSize());
+        assertEquals(2, names.size());
     }
 
     @Test
     @DisplayName("쉼표 뒤 공백을 제거해 이름을 저장한다")
     void trimPlayerName() {
-        Players players = NameParser.makeNameList("pobi, jason");
+        List<String> names = NameParser.makeNameList("pobi, jason");
 
-        assertEquals("jason", players.getPlayer(1).getName());
+        assertEquals("jason", names.get(1));
     }
 }
