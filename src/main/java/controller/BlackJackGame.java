d74b5896eb5ab188bdd95f730013b7b1e65776a2
김가현 <hyeonnni@naver.com>
2026-03-13 17:41:01 +0900

    refactor(controller): 게임 진행 흐름 개선 및 배팅 로직 추가

--- src/main/java/controller/BlackJackGame.java
+++ src/main/java/controller/BlackJackGame.java
@@ -1,24 +1,20 @@
 package controller;
 
-import domain.Card;
-import domain.Dealer;
-import domain.Deck;
-import domain.GameResult;
-import domain.Outcome;
-import domain.Participant;
-import domain.Player;
-import domain.Players;
-import dto.AllOutcomeDto;
+import domain.card.Deck;
+import domain.card.DeckFactory;
+import domain.participant.Dealer;
+import domain.participant.Money;
+import domain.participant.Player;
+import domain.participant.Players;
+import domain.result.Outcome;
 import dto.FinalScoreDto;
 import dto.InitialDto;
+import dto.MoneyDTO;
 import dto.ParticipantDto;
 import dto.ParticipantScoreDto;
 import dto.PlayerOutcomeDto;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import util.NameParser;
 import view.InputView;
@@ -34,46 +30,61 @@ public class BlackJackGame {
     }
 
     public void run() {
-        Deck deck = createDeck();
+        Deck deck = DeckFactory.create();
         Dealer dealer = new Dealer();
-        Players players = NameParser.makeNameList(inputView.getParticipant());
-        initialDraw(deck, dealer, players);
-        printInitialStatus(dealer, players);
-        players.getPlayers().forEach(player -> drawPlayerTurn(deck, player));
-        drawDealerTurn(deck, dealer);
-        printFinalScore(dealer, players);
-        printFinalWinner(dealer, players);
+        Players players = setupPlayers();
+        playGame(deck, dealer, players);
     }
 
-    private Deck createDeck() {
-        List<Card> cards = IntStream.range(0, 52)
-                .mapToObj(Card::new)
-                .collect(Collectors.toList());
-        return new Deck(cards);
+    private void playGame(Deck deck, Dealer dealer, Players players) {
+        initialDraw(deck, dealer, players);
+        printInitialStatus(dealer, players);
+        if (dealer.isFirstBlackJack()) {
+            printResults(dealer, players);
+            return;
+        }
+        players.players().forEach(player -> drawPlayerTurn(deck, player));
+        drawDealerTurn(deck, dealer);
+        printResults(dealer, players);
+    }
+
+    private void printResults(Dealer dealer, Players players) {
+        printFinalScore(dealer, players);
+        printFinalProfit(dealer, players);
+    }
+
+    private Players setupPlayers() {
+        List<String> playerNames = NameParser.makeNameList(inputView.getParticipant());
+        List<Player> players = new ArrayList<>();
+        for (String name : playerNames) {
+            String money = inputView.getMoney(name);
+            players.add(new Player(name, new Money(money)));
+        }
+        return new Players(players);
     }
 
     private void initialDraw(Deck deck, Dealer dealer, Players players) {
         IntStream.range(0, 2).forEach(i -> {
             dealer.drawCard(deck.draw());
-            players.getPlayers().forEach(player -> player.drawCard(deck.draw()));
+            players.players().forEach(player -> player.drawCard(deck.draw()));
         });
     }
 
     private void printInitialStatus(Dealer dealer, Players players) {
-        ParticipantDto dealerDto = new ParticipantDto(dealer.getName(), getCardNames(dealer));
-        List<ParticipantDto> participantDtos = players.getPlayers().stream()
-                .map(player -> new ParticipantDto(player.getName(), getCardNames(player)))
-                .collect(Collectors.toList());
-        String joinedNames = players.getPlayers().stream()
-                .map(Player::getName)
-                .collect(Collectors.joining(", "));
-        resultView.printGameStart(new InitialDto(joinedNames, dealerDto, participantDtos));
+        ParticipantDto dealerDto = ParticipantDto.from(dealer);
+        List<ParticipantDto> participants =
+                players.players()
+                        .stream()
+                        .map(ParticipantDto::from)
+                        .toList();
+        String joinedNames = players.getJoinedNames();
+        resultView.printGameStart(new InitialDto(joinedNames, dealerDto, participants));
     }
 
     private void drawPlayerTurn(Deck deck, Player player) {
         while (player.canDraw() && inputView.getMoreCards(player.getName()).equals("y")) {
             player.drawCard(deck.draw());
-            resultView.printParticipantMoreCard(new ParticipantDto(player.getName(), getCardNames(player)));
+            resultView.printParticipantMoreCard(ParticipantDto.from(player));
         }
     }
 
@@ -84,39 +95,25 @@ public class BlackJackGame {
         }
     }
 
-    private void printFinalWinner(Dealer dealer, Players players) {
-        Map<Player, Outcome> playerResults = new LinkedHashMap<>();
-        List<PlayerOutcomeDto> outcomeDtos = new ArrayList<>();
-        for (Player player : players.getPlayers()) {
-            Outcome outcome = Outcome.decideWinner(player.getScore(), dealer.getScore());
-            playerResults.put(player, outcome);
-            outcomeDtos.add(new PlayerOutcomeDto(player.getName(), outcome.getName()));
-        }
-        GameResult gameResult = new GameResult(playerResults);
-        resultView.printWinner(new AllOutcomeDto(gameResult.getDealerResult(), outcomeDtos));
-    }
-
     private void printFinalScore(Dealer dealer, Players players) {
-        ParticipantScoreDto dealerScoreDto = createScoreDto(dealer);
-        List<ParticipantScoreDto> playerScoreDtos = players.getPlayers().stream()
-                .map(this::createScoreDto)
-                .collect(Collectors.toList());
+        ParticipantScoreDto dealerScoreDto = ParticipantScoreDto.from(dealer);
+        List<ParticipantScoreDto> playerScoreDtos =
+                players.players()
+                        .stream()
+                        .map(ParticipantScoreDto::from)
+                        .toList();
         resultView.printResult(new FinalScoreDto(dealerScoreDto, playerScoreDtos));
     }
 
-    private ParticipantScoreDto createScoreDto(Participant participant) {
-        return new ParticipantScoreDto(
-                participant.getName(),
-                getCardNames(participant),
-                participant.getScore().getGameScore()
-        );
-    }
-
-    private List<String> getCardNames(Participant participant) {
-        List<String> cardNames = new ArrayList<>();
-        for (Card card : participant.getHand().getCards()) {
-            cardNames.add(card.getCardName());
+    private void printFinalProfit(Dealer dealer, Players players) {
+        int dealerProfit = 0;
+        List<PlayerOutcomeDto> outcomeDtos = new ArrayList<>();
+        for (Player player : players.players()) {
+            Outcome outcome = Outcome.decideWinner(player, dealer);
+            Money playerProfit = player.getMoney().multiply(outcome.getRate());
+            dealerProfit -= playerProfit.getAmount();
+            outcomeDtos.add(PlayerOutcomeDto.of(player, playerProfit));
         }
-        return cardNames;
+        resultView.printScore(new MoneyDTO(String.valueOf(dealerProfit), outcomeDtos));
     }
 }
