82b830368c82f880c0cdd3727db1aeb3cdd9eaa6
snow___shower <wonsb25@gmail.com>
2026-03-13 15:46:24 +0900

    test: BlackJackServieTest 내 중복된 초기화 로직 제거 및 경계값으로 테스트 케이스 변경

--- src/test/java/BlackJackServiceTest.java
+++ src/test/java/BlackJackServiceTest.java
@@ -15,71 +15,85 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BlackJackServiceTest {
 
-    Dealer dealer;
-    Player winPlayer;
-    Player defeatPlayer;
-    Player drawPlayer;
-    Player bustPlayer;
-    Players players;
-    BlackJackService blackJackService;
+    private final BlackJackService blackJackService = new BlackJackService();
+    private Dealer dealer;
 
     @BeforeEach
     void init() {
         dealer = new Dealer();
-        winPlayer = new Player(new ParticipantInfo("이김", new Hand()), new Money(BigDecimal.valueOf(0)));
-        defeatPlayer = new Player(new ParticipantInfo("짐", new Hand()), new Money(BigDecimal.valueOf(0)));
-        drawPlayer = new Player(new ParticipantInfo("비김", new Hand()), new Money(BigDecimal.valueOf(0)));
-        bustPlayer = new Player(new ParticipantInfo("버스트", new Hand()), new Money(BigDecimal.valueOf(0)));
-        dealer.keepCard(new Card(Rank.FIVE, Pattern.CLOVER));
-        winPlayer.keepCard(new Card(Rank.SIX, Pattern.CLOVER));
-        defeatPlayer.keepCard(new Card(Rank.FOUR, Pattern.CLOVER));
-        drawPlayer.keepCard(new Card(Rank.FIVE, Pattern.SPADE));
-        players = new Players(List.of(winPlayer, defeatPlayer, drawPlayer, bustPlayer));
-        blackJackService = new BlackJackService();
+    }
 
+    private Player dummyPlayer(String name) {
+        return new Player(new ParticipantInfo(name, new Hand()), new Money(BigDecimal.ZERO));
     }
 
     @Test
     void 딜러의_총합이_플레이어의_총합보다_낮으면_플레이어가_승리한다() {
+        Player player = dummyPlayer("이김");
+        Players players = new Players(List.of(player));
+        player.keepCard(new Card(Rank.SIX, Pattern.CLOVER));
+        dealer.keepCard(new Card(Rank.FIVE, Pattern.CLOVER));
+
         Result result = blackJackService.calculateResult(dealer, players);
-        GameResult info = result.getPlayersResult().get(winPlayer);
+        GameResult info = result.getPlayersResult().get(player);
+
         assertThat(info).isEqualTo(GameResult.WIN);
     }
 
     @Test
     void 딜러의_총합이_플레이어의_총합보다_높으면_플레이어가_패배한다() {
+        Player player = dummyPlayer("짐");
+        Players players = new Players(List.of(player));
+        player.keepCard(new Card(Rank.FOUR, Pattern.CLOVER));
+        dealer.keepCard(new Card(Rank.FIVE, Pattern.CLOVER));
+
         Result result = blackJackService.calculateResult(dealer, players);
-        GameResult info = result.getPlayersResult().get(defeatPlayer);
+        GameResult info = result.getPlayersResult().get(player);
+
         assertThat(info).isEqualTo(GameResult.DEFEAT);
     }
 
     @Test
     void 딜러의_총합과_플레이어의_총합이_같으면_비긴다() {
+        Player player = dummyPlayer("비김");
+        Players players = new Players(List.of(player));
+        player.keepCard(new Card(Rank.FIVE, Pattern.SPADE));
+        dealer.keepCard(new Card(Rank.FIVE, Pattern.CLOVER));
+
         Result result = blackJackService.calculateResult(dealer, players);
-        GameResult info = result.getPlayersResult().get(drawPlayer);
+        GameResult info = result.getPlayersResult().get(player);
+
         assertThat(info).isEqualTo(GameResult.PUSH);
     }
 
     @Test
     void 먼저_버스트된_플레이어는_딜러의_결과에_관계없이_패배한다() {
-        Result result = blackJackService.calculateResult(dealer, players);
-        bustPlayer.keepCard(new Card(Rank.JACK, Pattern.CLOVER));
-        bustPlayer.keepCard(new Card(Rank.JACK, Pattern.SPADE));
-        bustPlayer.keepCard(new Card(Rank.JACK, Pattern.HEART));
+        Player player = dummyPlayer("버스트됨");
+        Players players = new Players(List.of(player));
+        player.keepCard(new Card(Rank.JACK, Pattern.CLOVER));
+        player.keepCard(new Card(Rank.JACK, Pattern.SPADE));
+        player.keepCard(new Card(Rank.TWO, Pattern.HEART));
         dealer.keepCard(new Card(Rank.KING, Pattern.HEART));
         dealer.keepCard(new Card(Rank.QUEEN, Pattern.HEART));
 
-        GameResult info = result.getPlayersResult().get(bustPlayer);
+        Result result = blackJackService.calculateResult(dealer, players);
+        GameResult info = result.getPlayersResult().get(player);
+
         assertThat(info).isEqualTo(GameResult.DEFEAT);
     }
 
     @Test
     void 딜러가_버스트시_딜러보다_낮은_점수의_플레이어들은_무조건_승리한다() {
+        Player player = dummyPlayer("이김당함");
+        Players players = new Players(List.of(player));
+        player.keepCard(new Card(Rank.FOUR, Pattern.CLOVER));
+        dealer.keepCard(new Card(Rank.TWO, Pattern.SPADE));
         dealer.keepCard(new Card(Rank.KING, Pattern.HEART));
         dealer.keepCard(new Card(Rank.QUEEN, Pattern.HEART));
-        Result result = blackJackService.calculateResult(dealer, players);
 
-        GameResult info = result.getPlayersResult().get(defeatPlayer);
+        Result result = blackJackService.calculateResult(dealer, players);
+        GameResult info = result.getPlayersResult().get(player);
+
         assertThat(info).isEqualTo(GameResult.WIN);
     }
 }
