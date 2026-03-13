ac7eb2e9d2fc385e6a879d47864b1d50114da2c6
김가현 <hyeonnni@naver.com>
2026-03-13 16:46:21 +0900

    refactor(domain): Name 클래스 패키지 구조 변경 및 record로 전환

--- src/main/java/domain/participant/Name.java
+++ src/main/java/domain/participant/Name.java
@@ -1,25 +1,16 @@
-package domain;
+package domain.participant;
 
-public final class Name {
-    private final String name;
-
-    public Name(String name) {
+public record Name(String name) {
+    public Name {
         validate(name);
-        this.name = name;
     }
 
     private void validate(String name) {
         if (name == null) {
             throw new IllegalArgumentException("이름은 비어 있거나 공백만 있을 수 없습니다.");
         }
-
         if (name.isBlank()) {
             throw new IllegalArgumentException("이름은 비어 있거나 공백만 있을 수 없습니다.");
         }
     }
-
-    public String getName() {
-        return name;
-    }
 }
-
