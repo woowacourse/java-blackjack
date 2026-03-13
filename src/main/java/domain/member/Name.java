027694d23bfb14f52c05ca4152e658a02f5b6eab
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-13 10:29:38 +0900

    refactor(Name): 메서드 순서 변경 및 이름 일치 검증 추가

--- src/main/java/domain/member/Name.java
+++ src/main/java/domain/member/Name.java
@@ -1,5 +1,7 @@
 package domain.member;
 
+import java.util.Objects;
+
 public class Name {
 
     private final String value;
@@ -9,13 +11,17 @@ public class Name {
         this.value = value;
     }
 
-    private void validate(String value) {
-        if (value == null || value.isBlank()) {
-            throw new IllegalArgumentException("이름은 빈 값일 수 없습니다.");
-        }
+    public boolean isName(String name) {
+        return Objects.equals(this.value, name);
     }
 
     public String getValue() {
         return value;
+    }
+
+    private void validate(String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("이름은 빈 값일 수 없습니다.");
+        }
     }
 }
