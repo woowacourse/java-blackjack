32eeef46ca90dcdf0ed7de4181a521d2099e1ba4
choiyoung69 <choiyoung609@gmail.com>
2026-03-13 13:05:45 +0900

    feat: 베팅 금액을 gambler가 가지도록 기능 추가

--- src/main/java/domain/player/Gambler.java
+++ src/main/java/domain/player/Gambler.java
@@ -1,7 +1,28 @@
 package domain.player;
 
 public class Gambler extends Participant{
-    public Gambler(String name) {
+
+    private final Long betAmount;
+
+    public Gambler(String name, Long betAmount) {
         super(name);
+        validateBetAmount(betAmount);
+    }
+
+    private void validateBetAmount(Long betAmount) {
+        validateBetAmountIsPositive(betAmount);
+        validateAmountDoesNotExceedMaxAmount(betAmount)
+    }
+
+    private void validateBetAmountIsPositive(Long betAmount) {
+        if (betAmount > 0) {
+
+        }
+    }
+
+    private void validateAmountDoesNotExceedMaxAmount(Long betAmount) {
+        if (betAmount > 10_000_000_000L) {
+            throw new IllegalArgumentException("베팅 금액은 100억을 넘지 않는다");
+        }
     }
 }
