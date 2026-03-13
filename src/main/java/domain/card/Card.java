1da503b7447d68dcc7ae35d6bfb78affb250ffac
김가현 <hyeonnni@naver.com>
2026-03-13 16:37:22 +0900

    refactor(domain): Card 클래스를 record로 전환

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -1,12 +1,6 @@
-package domain;
+package domain.card;
 
-public class Card {
-    private final int card;
-
-    public Card(int card) {
-        this.card = card;
-    }
-
+public record Card(int card) {
     public String getCardName() {
         int shape = card / 13;
         int number = card % 13;
@@ -15,23 +9,16 @@ public class Card {
 
     public int getScore() {
         int cardNumber = card % 13;
-
         if (1 <= cardNumber && cardNumber <= 9) {
             return cardNumber + 1;
         }
-
         if (cardNumber >= 10) {
             return 10;
         }
-
         return 11;
     }
 
     public boolean isAce() {
         return card % 13 == 0;
-    }
-
-    public int getCard() {
-        return card;
     }
 }
