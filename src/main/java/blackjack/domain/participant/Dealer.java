80b431aa5d60425fae62f5ed4c8377d73b27a1ad
kangrae <kangrae0501@gmail.com>
2026-03-13 16:22:47 +0900

    refactor: 접근 제어자 변경
    - protected로 하여 딜러 이름은 participant만 알도록 수정

--- src/main/java/blackjack/domain/participant/Dealer.java
+++ src/main/java/blackjack/domain/participant/Dealer.java
@@ -2,7 +2,7 @@ package blackjack.domain.participant;
 
 public final class Dealer extends Participant {
 
-    public static final String DEALER_NAME = "딜러";
+    protected static final String DEALER_NAME = "딜러";
     private static final int DEALER_CAN_DRAW_THRESHOLD = 17;
 
     public Dealer() {
