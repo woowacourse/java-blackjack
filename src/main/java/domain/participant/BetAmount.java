7f3f8519faa6d62f8ac6b08a6447029d8a316952
khcho96 <khcho1492@gmail.com>
2026-03-13 16:54:38 +0900

    feat(BetAmount): 배팅 금액을 저장 및 검증하는  BetAmount 틀래스 생성

---
+++ src/main/java/domain/participant/BetAmount.java
@@ -0,0 +1,43 @@
+package domain.participant;
+
+public class BetAmount {
+
+    public static final String INVALID_BET_AMOUNT_NUMBER = "배팅 금액은 숫자여야 합니다.";
+    public static final String INVALID_BET_AMOUNT_POSITIVE = "배팅 금액은 양수여야 합니다.";
+
+    private final int betAmount;
+
+    public BetAmount(String betAmountInput) {
+        validate(betAmountInput);
+        this.betAmount = Integer.parseInt(betAmountInput);
+    }
+
+    private void validate(String betAmountInput) {
+        validateNumber(betAmountInput);
+        validatePositive(betAmountInput);
+    }
+
+    private void validateNumber(String betAmountInput) {
+        if (isNotNumber(betAmountInput)) {
+            throw new IllegalArgumentException(INVALID_BET_AMOUNT_NUMBER);
+        }
+    }
+
+    private static boolean isNotNumber(String betAmountInput) {
+        return !betAmountInput.matches("-*\\d+");
+    }
+
+    private void validatePositive(String betAmountInput) {
+        if (isPositive(betAmountInput)) {
+            throw new IllegalArgumentException(INVALID_BET_AMOUNT_POSITIVE);
+        }
+    }
+
+    private static boolean isPositive(String betAmountInput) {
+        return Integer.parseInt(betAmountInput) <= 0;
+    }
+
+    public int getBetAmount() {
+        return betAmount;
+    }
+}
