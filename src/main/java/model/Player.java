63899a257213a92ad376458b8b91ce2063c5a9f6
armd482 <armd482@naver.com>
2026-03-13 17:14:01 +0900

    refactor: 배팅 이익 계산 로직 player로 변경

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -3,7 +3,10 @@ package model;
 import static model.Dealer.DEALER_NAME;
 
 import constant.ErrorMessage;
+import java.math.BigDecimal;
 import java.util.Objects;
+import model.dto.PlayerResult;
+import model.dto.PlayerWinning;
 
 public class Player extends Participant {
     private Integer betAmount = 0;
@@ -25,12 +28,19 @@ public class Player extends Participant {
         return Objects.hash(this.getResult().name());
     }
 
-    public void setBetAmount(Integer betAmount) {
-        this.betAmount = betAmount;
+    @Override
+    public PlayerResult getResult() {
+        PlayerResult result = super.getResult();
+        return new PlayerResult(result.name(), result.deck(), result.score(), betAmount);
     }
 
-    public Integer getBetAmount() {
-        return this.betAmount;
+    public void setBetAmount(BetPrice betPrice) {
+        this.betAmount = betPrice.value();
+    }
+
+    public PlayerWinning getBetResult(Dealer dealer) {
+        Integer profit = calculateBetAmount(dealer);
+        return new PlayerWinning(getName(), profit);
     }
 
     private static PlayerName validate(PlayerName name) {
@@ -38,5 +48,41 @@ public class Player extends Participant {
             throw new IllegalArgumentException(ErrorMessage.NO_PLAYER_NAME_DEALER.getMessage());
         }
         return name;
+    }
+
+    private Integer calculateBetAmount(Dealer dealer) {
+        return calculateBustBetAmount(dealer).intValue();
+    }
+
+    private BigDecimal calculateBustBetAmount(Dealer dealer) {
+        if(super.isBust()) {
+            return BigDecimal.valueOf(-betAmount);
+        }
+
+        return calculateBlackJackBetAmount(dealer);
+    }
+
+    private BigDecimal calculateBlackJackBetAmount(Dealer dealer) {
+
+        if(super.isBlackJack() && dealer.isBlackJack()) {
+            return BigDecimal.ZERO;
+        }
+
+        if(super.isBlackJack()) {
+            return BigDecimal.valueOf(betAmount).multiply(BigDecimal.valueOf(1.5));
+        }
+
+        return calculateRegularBetAmount(dealer);
+    }
+
+    private BigDecimal calculateRegularBetAmount(Dealer dealer) {
+        if(super.isMoreThanScore(dealer) || dealer.isBust()) {
+            return BigDecimal.valueOf(betAmount);
+        }
+
+        if(super.isLessThanScore(dealer)) {
+            return BigDecimal.valueOf(betAmount);
+        }
+        return BigDecimal.ZERO;
     }
 }
