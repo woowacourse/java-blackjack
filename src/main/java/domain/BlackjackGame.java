f644e0ab058a4850e647915e5416e9f29e38d949
구민서 <minseo12379@gmail.com>
2026-03-13 19:13:27 +0900

    refactor: 상수를 클래스 상단으로 이동

--- src/main/java/domain/BlackjackGame.java
+++ src/main/java/domain/BlackjackGame.java
@@ -9,12 +9,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BlackjackGame {
+    public static final int INITIAL_CARD_COUNT = 2;
     public static int DEALER_HIT_STAND_BOUNDARY = 16;
     private final List<Player> players;
     private final Dealer dealer;
     private final Deck deck;
 
-    public static final int INITIAL_CARD_COUNT = 2;
 
     public BlackjackGame() {
         this.players = new ArrayList<>();
