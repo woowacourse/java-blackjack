4422cabdb4084116d1885e9d3a60e61cf71743fe
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-13 20:52:09 +0900

    test(Gamblers): 겜블러 이름과 베팅금액 저장 및 생성 테스트

--- src/test/java/domain/player/GamblersTest.java
+++ src/test/java/domain/player/GamblersTest.java
@@ -1,27 +1,30 @@
-//package domain.player;
-//
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//
-//import java.util.List;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//class GamblersTest {
-//
-//    @Test
-//    @DisplayName("이름 리스트를 통해 player 객체들이 제대로 생성되는지 테스트")
-//    void 이름_정상_저장_테스트() {
-//        // given
-//        List<String> names = List.of("pobi", "coco");
-//
-//        // when
-//        Gamblers gamblers = new Gamblers(names);
-//
-//        // then
-//        boolean allContainName = names.stream()
-//                .allMatch(name -> gamblers.containGambler(name));
-//
-//        Assertions.assertThat(allContainName).isEqualTo(true);
-//    }
-//}
+package domain.player;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import domain.betting.Betting;
+import domain.betting.BettingAmount;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GamblersTest {
+
+    @Test
+    @DisplayName("이름, 베팅금액 정상 저장 테스트")
+    void 겜블러_이름_베팅금액_정상_저장_생성_테스트() {
+        // given
+        Map<String, BettingAmount> gamblerBettingInfo = new HashMap<String, BettingAmount>() {};
+        gamblerBettingInfo.put("pobi", new BettingAmount(10000));
+        gamblerBettingInfo.put("coco", new BettingAmount(20000));
+
+        // when
+        Gamblers gamblers = new Gamblers(gamblerBettingInfo);
+
+        // then
+        Assertions.assertThat(gamblers.getGamblersSize()).isEqualTo(2);
+    }
+}
