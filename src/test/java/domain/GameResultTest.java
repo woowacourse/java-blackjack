b99020b6ca6278c76bbfa7b4a2e38fe22421f3e7
Junha Park <poiu7825@pusan.ac.kr>
2026-03-13 15:21:30 +0900

    test: 게임 결과에 따른 배팅 금액 처리 테스트 추가

---
+++ src/test/java/domain/GameResultTest.java
@@ -0,0 +1,47 @@
+package domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import blackjack.domain.BettingMoney;
+import blackjack.domain.GameResult;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GameResultTest {
+
+    @Test
+    @DisplayName("승리 시, 배팅 금액 1배 반환")
+    void 승리_처리_테스트() {
+        GameResult gameResult = GameResult.WIN;
+        BettingMoney bettingMoney = new BettingMoney("10000");
+        double expected = 10000;
+
+        double actual = gameResult.calculateProfit(bettingMoney);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("패비 시, 배팅 금액 1배 반납")
+    void 패배_처리_테스트() {
+        GameResult gameResult = GameResult.LOSE;
+        BettingMoney bettingMoney = new BettingMoney("10000");
+        double expected = -10000;
+
+        double actual = gameResult.calculateProfit(bettingMoney);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("블랙잭 시, 배팅 금액 1.5배 반환")
+    void 블랙잭_처리_테스트() {
+        GameResult gameResult = GameResult.BLACKJACK;
+        BettingMoney bettingMoney = new BettingMoney("10000");
+        double expected = 15000;
+
+        double actual = gameResult.calculateProfit(bettingMoney);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
