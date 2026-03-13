8b734ea7726c2a5bfb4e701512f2d9714ab467de
bee9827 <ehfrhfo9494@naver.com>
2026-03-13 17:36:53 +0900

    refactor: 안쓰는 메서드 인스턴스 변수 정리

--- src/main/java/domain/state/Result.java
+++ src/main/java/domain/state/Result.java
@@ -2,14 +2,7 @@ package domain.state;
 
 public enum Result {
     //여기서 금액 회수 or 1.5배 or 2배
-    WIN("승"),
-    DRAW("무"),
-    LOSE("패"),
-    ;
-
-    private final String displayName;
-
-    Result(String displayName) {
-        this.displayName = displayName;
-    }
+    WIN,
+    DRAW,
+    LOSE,
 }
