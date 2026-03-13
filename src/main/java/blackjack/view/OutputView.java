4de66f239e87e67ab471e472f2d94b633248b7b7
EveryPine <kenton.lim@gmail.com>
2026-03-13 15:48:17 +0900

    refactor(output): OutputView의 모든 메소드 내 dto 파라미터의 네이밍 구체화

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -16,17 +16,17 @@ public class OutputView {
         System.out.println(ERROR_PREFIX + message);
     }
 
-    public static void printStartMessage(final StartMessageDto dto) {
+    public static void printStartMessage(final StartMessageDto startMessageDto) {
         System.out.printf("딜러와 %s에게 %d장을 나누었습니다.\n",
-            String.join(", ", dto.playerNicknames()), DEALER_DISTRIBUTE_COUNT);
+            String.join(", ", startMessageDto.playerNicknames()), DEALER_DISTRIBUTE_COUNT);
 
-        System.out.printf("딜러카드: %s\n", String.join(", ", dto.dealerOpenedCardNames()));
-        dto.playerCardNamesMap().forEach((playerNickname, cardNames) ->
+        System.out.printf("딜러카드: %s\n", String.join(", ", startMessageDto.dealerOpenedCardNames()));
+        startMessageDto.playerCardNamesMap().forEach((playerNickname, cardNames) ->
             System.out.printf("%s카드: %s\n", playerNickname, String.join(", ", cardNames)));
     }
 
-    public static void printCardStatus(final CardStatusDto dto) {
-        System.out.println(participantHandFormat(dto));
+    public static void printCardStatus(final CardStatusDto cardStatusDto) {
+        System.out.println(participantHandFormat(cardStatusDto));
     }
 
 
@@ -34,24 +34,24 @@ public class OutputView {
         System.out.printf("딜러는 %d이하라 한장의 카드를 더 받았습니다.\n", DEALER_HIT_THRESHOLD);
     }
 
-    public static void printFinalStatus(final FinalStatusDto dto) {
-        dto.participantNicknames().forEach(nickname ->
+    public static void printFinalStatus(final FinalStatusDto finalStatusDto) {
+        finalStatusDto.participantNicknames().forEach(nickname ->
             System.out.printf("%s 카드: %s - 결과: %s\n",
                 nickname,
-                String.join(", ", dto.participantCardNamesMap().get(nickname)),
-                dto.participantScoreMap().get(nickname)));
+                String.join(", ", finalStatusDto.participantCardNamesMap().get(nickname)),
+                finalStatusDto.participantScoreMap().get(nickname)));
     }
 
-    public static void printFinalProfits(final FinalProfitsDto dto) {
+    public static void printFinalProfits(final FinalProfitsDto finalProfitsDto) {
         System.out.println("## 최종 수익");
-        System.out.printf("딜러: %d\n", dto.dealerProfit());
-        dto.nicknameProfitMap().forEach((nickname, profit) ->
+        System.out.printf("딜러: %d\n", finalProfitsDto.dealerProfit());
+        finalProfitsDto.nicknameProfitMap().forEach((nickname, profit) ->
             System.out.printf("%s: %d\n", nickname, profit));
     }
 
-    private static String participantHandFormat(final CardStatusDto dto) {
+    private static String participantHandFormat(final CardStatusDto cardStatusDto) {
         return String.format("%s카드: %s",
-            dto.nickname(),
-            String.join(", ", dto.cardNames()));
+            cardStatusDto.nickname(),
+            String.join(", ", cardStatusDto.cardNames()));
     }
 }
