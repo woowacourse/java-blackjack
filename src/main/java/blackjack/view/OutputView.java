cd5533a57b51c7b95f07c2f2f749e79f0c703837
Kim, Joon <kjoon418@naver.com>
2026-03-13 13:46:52 +0900

    refactor: `OutputView`의 라벨 파싱 기능을 별도의 객체로 분리

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,62 +1,19 @@
 package blackjack.view;
 
-import static blackjack.model.card.Rank.ACE;
-import static blackjack.model.card.Rank.EIGHT;
-import static blackjack.model.card.Rank.FIVE;
-import static blackjack.model.card.Rank.FOUR;
-import static blackjack.model.card.Rank.JACK;
-import static blackjack.model.card.Rank.KING;
-import static blackjack.model.card.Rank.NINE;
-import static blackjack.model.card.Rank.QUEEN;
-import static blackjack.model.card.Rank.SEVEN;
-import static blackjack.model.card.Rank.SIX;
-import static blackjack.model.card.Rank.TEN;
-import static blackjack.model.card.Rank.THREE;
-import static blackjack.model.card.Rank.TWO;
-import static blackjack.model.card.Suit.CLUB;
-import static blackjack.model.card.Suit.DIAMOND;
-import static blackjack.model.card.Suit.HEART;
-import static blackjack.model.card.Suit.SPADE;
-import static java.util.Map.entry;
-
-import blackjack.model.card.Rank;
-import blackjack.model.card.Suit;
 import blackjack.view.dto.CardDto;
 import blackjack.view.dto.DealerScoreDto;
 import blackjack.view.dto.PlayerDto;
 import blackjack.view.dto.PlayerScoreDto;
 import blackjack.view.dto.ResultDto;
+import blackjack.view.label.RankLabel;
+import blackjack.view.label.SuitLabel;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
 
     private static final String DELIMITER = ", ";
     private static final String DEALER_NAME = "딜러";
-
-    private static final Map<Rank, String> RANK_LABELS = Map.ofEntries(
-            entry(ACE, "A"),
-            entry(TWO, "2"),
-            entry(THREE, "3"),
-            entry(FOUR, "4"),
-            entry(FIVE, "5"),
-            entry(SIX, "6"),
-            entry(SEVEN, "7"),
-            entry(EIGHT, "8"),
-            entry(NINE, "9"),
-            entry(TEN, "10"),
-            entry(KING, "K"),
-            entry(QUEEN, "Q"),
-            entry(JACK, "J")
-    );
-
-    private static final Map<Suit, String> SUIT_LABELS = Map.of(
-            DIAMOND, "다이아몬드",
-            CLUB, "클로버",
-            SPADE, "스페이드",
-            HEART, "하트"
-    );
 
     public void printInitialDeal(List<CardDto> dealerCards, List<PlayerDto> players) {
         String joinedPlayerNames = players.stream()
@@ -143,8 +100,8 @@ public class OutputView {
     }
 
     private String parseCardToOutput(CardDto card) {
-        String rank = RANK_LABELS.get(card.rank());
-        String suit = SUIT_LABELS.get(card.suit());
+        String rank = RankLabel.from(card.rank());
+        String suit = SuitLabel.from(card.suit());
 
         return rank + suit;
     }
