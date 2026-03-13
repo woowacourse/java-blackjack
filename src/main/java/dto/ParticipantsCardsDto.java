248b2759f47f35f58619463fb65fd0bca4b1eebb
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-03-13 17:21:04 +0900

    refactor(dto): 미사용 클래스 제거 후 수정

--- src/main/java/dto/ParticipantsCardsDto.java
+++ src/main/java/dto/ParticipantsCardsDto.java
@@ -1,24 +1,25 @@
 package dto;
 
 import domain.card.Card;
-import domain.player.Dealer;
-import domain.player.Player;
 import java.util.List;
 
 public record ParticipantsCardsDto(String name, List<String> cards) {
-    public static ParticipantsCardsDto playerFromEntity(Player player) {
-        return new ParticipantsCardsDto(player.getName(), cardToString(player.getCards()));
+
+    public static ParticipantsCardsDto ofDealerOpen(String dealerName, List<Card> cards) {
+        return new ParticipantsCardsDto(dealerName, cardToString(cards));
     }
 
-    public static ParticipantsCardsDto dealerFromEntity(Dealer dealer) {
-        List<Card> showOnlyFirst = List.of(dealer.getCards().getFirst());
-        return new ParticipantsCardsDto(dealer.getName(), cardToString(showOnlyFirst));
+    public static ParticipantsCardsDto ofDealerAll(String dealerName, List<Card> cards) {
+        return new ParticipantsCardsDto(dealerName, cardToString(cards));
+    }
+
+    public static ParticipantsCardsDto ofPlayer(String playerName, List<Card> cards) {
+        return new ParticipantsCardsDto(playerName, cardToString(cards));
     }
 
     private static List<String> cardToString(List<Card> hands) {
         return hands.stream()
                 .map(card -> card.rank().getDisplayName() + card.suit().getDisplayName())
                 .toList();
-
     }
 }
