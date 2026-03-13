8c9082497c7fc510c91236fcee0578e91a649774
armd482 <armd482@naver.com>
2026-03-13 17:14:35 +0900

    refactor: 플레이어 결과 배팅 금액 추가

--- src/main/java/model/dto/PlayerResult.java
+++ src/main/java/model/dto/PlayerResult.java
@@ -2,4 +2,4 @@ package model.dto;
 
 import java.util.List;
 
-public record PlayerResult(String name, List<String> deck, Integer score) {}
+public record PlayerResult(String name, List<String> deck, Integer score, Integer bet) {}
