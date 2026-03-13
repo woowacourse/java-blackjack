e79e2a004a824f24f4a94b68bf08e0c8dae97eb8
armd482 <armd482@naver.com>
2026-03-13 17:19:54 +0900

    fix: 배팅 금액 10억 이하로만 받도록 변경

--- src/main/java/model/BetPrice.java
+++ src/main/java/model/BetPrice.java
@@ -8,7 +8,7 @@ public record BetPrice(Integer value) {
     }
 
     private static void validate(Integer value) {
-        if(value <= 0) {
+        if(value <= 0 || value > 1000000000) {
             throw new IllegalArgumentException(ErrorMessage.NO_NEGATIVE_BET.getMessage());
         }
     }
