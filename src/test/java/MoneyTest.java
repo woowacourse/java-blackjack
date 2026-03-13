7d861aa3f53cfd57a915fde4c54e29f8f2c713b7
snow___shower <wonsb25@gmail.com>
2026-03-13 15:04:47 +0900

    test: Money 성공 테스트 추가

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -4,11 +4,21 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class MoneyTest {
 
     @Test
     void 배팅금이_양수가_아니면_오류가_발생한다() {
         Assertions.assertThatThrownBy(() -> new Money(BigDecimal.valueOf(-10)))
                 .hasMessageContaining("[ERROR] 배팅금은 0보다 커야 합니다!");
+    }
+
+    @Test
+    void 배팅금_객체는_자신의_배팅금을_알고_있다() {
+        BigDecimal amount = BigDecimal.valueOf(10000);
+        Money money = new Money(BigDecimal.valueOf(10000));
+
+        assertThat(money.getBettingMoney()).isEqualByComparingTo(amount);
     }
 }
