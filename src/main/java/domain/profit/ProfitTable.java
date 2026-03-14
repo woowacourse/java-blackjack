daebedb267237968d3c434f1a56882d0a226e9e4
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-03-14 22:05:13 +0900

    feat: 수익을 저장하는 테이블 추가

---
+++ src/main/java/domain/profit/ProfitTable.java
@@ -0,0 +1,34 @@
+package domain.profit;
+
+import domain.participant.Player;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class ProfitTable {
+    private final Map<String, Integer> profitTable;
+
+    public ProfitTable() {
+        this.profitTable = new HashMap<>();
+    }
+
+    public int findProfitByPlayerName(String name) {
+        return profitTable.get(name);
+    }
+
+    public void recodeProfit(Player player, int profit) {
+        profitTable.put(player.getName(), profit);
+    }
+
+    public Set<Map.Entry<String, Integer>> findProfitTableEntry() {
+        return Set.copyOf(profitTable.entrySet());
+    }
+
+    public int dealerCalculateProfit() {
+        return profitTable.values().stream()
+                .mapToInt(value -> -value)
+                .sum();
+    }
+}
