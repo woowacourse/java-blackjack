c23dd69021be7ede9c0646003e1dbac3cf753652
smiinii <sm020320@naver.com>
2026-03-14 21:40:50 +0900

    test: Score 관련 테스트 케이스 추가

--- src/test/java/blackjack/model/ScoreTest.java
+++ src/test/java/blackjack/model/ScoreTest.java
@@ -44,4 +44,42 @@ class ScoreTest {
         assertThat(score1.isMaxScore()).isFalse();
         assertThat(score2.isMaxScore()).isFalse();
     }
+
+    @Test
+    @DisplayName("딜러의 점수가 16점 이하이면 히트할 수 있다.")
+    void isDealerHitScoreTest() {
+        // give & when & then
+        assertThat(new Score(16).isDealerHitScore()).isTrue();
+        assertThat(new Score(17).isDealerHitScore()).isFalse();
+    }
+
+    @Test
+    @DisplayName("플레이어의 점수가 21점 이하이면 히트할 수 있다.")
+    void isPlayerHitScoreTest() {
+        // give & when & then
+        assertThat(new Score(21).isPlayerHitScore()).isTrue();
+        assertThat(new Score(22).isPlayerHitScore()).isFalse();
+    }
+
+    @Test
+    @DisplayName("두 스코어의 값이 같으면 True를 반환한다.")
+    void isSameTest() {
+        // give
+        Score score1 = new Score(20);
+        Score score2 = new Score(20);
+        // when & then
+        assertThat(score1.isSame(score2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("내 스코어가 상대보다 작으면 True를 반환한다.")
+    void isLessTest() {
+        // given
+        Score smallScore = new Score(19);
+        Score largeScore = new Score(20);
+        Score sameScore = new Score(20);
+        // when & then
+        assertThat(smallScore.isLess(largeScore)).isTrue();
+        assertThat(largeScore.isLess(sameScore)).isFalse();
+    }
 }
