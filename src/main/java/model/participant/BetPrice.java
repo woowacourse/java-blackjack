4614dd7f52464333840cb8a2fa8594580896f6db
armd482 <armd482@naver.com>
2026-03-14 16:41:29 +0900

    refactor: 배팅금액 max 상수화

--- src/main/java/model/participant/BetPrice.java
+++ src/main/java/model/participant/BetPrice.java
@@ -3,12 +3,14 @@ package model.participant;
 import constant.ErrorMessage;
 
 public record BetPrice(Integer value) {
+    private static final Integer MAX_BET = 100000000;
+
     public BetPrice {
         validate(value);
     }
 
     private static void validate(Integer value) {
-        if(value <= 0 || value > 1000000000) {
+        if(value <= 0 || value > MAX_BET) {
             throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE_BET.getMessage());
         }
     }
