c19e80f2c137925eadacff71ef537934555fb47b
Yeji Kim <yejikim.erica@gmail.com>
2026-03-15 01:19:32 +0900

    feat(InputView): 배팅 금액 사용자 입력값 유효성 검사 기능 추가
    - 배팅 금액이 공백인 경우 예외 처리
    - 숫자가 아닌 문자를 입력한 경우 예외 처리
    - 배팅 금액이 정수 범위를 초과하는 경우 예외 처리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,16 +8,49 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public List<String> readNames() {
-        String line = scanner.nextLine();
+        String line = readLineAndTrim();
         validateIsBlank(line);
         List<String> names = Arrays.stream(line.split("\\s*,\\s*")).toList();
         names.forEach(this::validateIsBlank);
         return names;
     }
 
+    private String readLineAndTrim() {
+        return scanner.nextLine().trim();
+    }
+
+    private void validateIsBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("공백은 허용되지 않습니다");
+        }
+    }
+
+    public int readBetAmount() {
+        String line = readLineAndTrim();
+        validateIsBlank(line);
+        validateIsNumeric(line);
+        return validateAndParseToInt(line);
+    }
+
+    private void validateIsNumeric(String input) {
+        if (input.matches("-?\\d+")) {
+            return;
+        }
+        throw new IllegalArgumentException("숫자가 아닌 문자를 입력할 수 없습니다.");
+    }
+
+    private int validateAndParseToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("배팅 금액은 정수 범위를 초과할 수 없습니다.");
+        }
+    }
+
     public boolean readHitOrStand() {
-        String input = scanner.nextLine().trim();
+        String input = readLineAndTrim();
         validateIsBlank(input);
+        input = input.trim();
         if (input.equals("y")) {
             return true;
         }
@@ -25,11 +58,5 @@ public class InputView {
             return false;
         }
         throw new IllegalArgumentException("y 혹은 n만 입력 가능합니다.");
-    }
-
-    private void validateIsBlank(String input) {
-        if (input.isBlank()) {
-            throw new IllegalArgumentException("공백은 허용되지 않습니다");
-        }
     }
 }
