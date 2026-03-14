e325a08e75d06c20793a1513ba146ddefdd503f8
choubung <choubung@sookmyung.ac.kr>
2026-03-14 12:09:59 +0900

    Feat: 수익 계산 로직 추가 BlackjackService와 BettingTable 변경

--- src/main/java/domain/BettingTable.java
+++ src/main/java/domain/BettingTable.java
@@ -1,4 +1,40 @@
 package domain;
 
+import domain.participant.WinStatus;
+import domain.participant.player.Player;
+import domain.vo.Money;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class BettingTable {
+    private static final double BLACKJACK_BONUS = 1.5;
+    private final Map<Player, Money> bettingTable = new HashMap<>();
+
+    public void placeBet(Player player, Money money) {
+        bettingTable.put(player, money);
+    }
+
+    public Money calculateProfit(Player player) {
+        if (player.getWinStatus() == WinStatus.WIN && player.isBlackjack()) {
+            return new Money(bettingTable.get(player).getValueOf() * BLACKJACK_BONUS);
+        }
+
+        if (player.getWinStatus() == WinStatus.LOSS) {
+            return new Money(0);
+        }
+
+        return bettingTable.get(player);
+    }
+
+    public Money getDealerProfit() {
+        return new Money(bettingTable.values()
+                .stream()
+                .mapToInt(money -> money.getValueOf())
+                .sum());
+    }
+
+    public Money getBettingMoneyByPlayerName(Player targetPlayer) {
+        return bettingTable.get(targetPlayer);
+    }
 }
--- src/main/java/service/BlackjackService.java
+++ src/main/java/service/BlackjackService.java
@@ -1,15 +1,22 @@
 package service;
 
+import domain.BettingTable;
 import domain.card.CardDeck;
 import domain.participant.Dealer;
 import domain.participant.HandCards;
 import domain.participant.player.Player;
+import domain.vo.Money;
 import dto.DealerInfoDto;
 
 public class BlackjackService {
     private static final int START_CARD_COUNT = 2;
     private final Dealer dealer = new Dealer(new HandCards());
     private final CardDeck cardDeck = new CardDeck();
+    private final BettingTable bettingTable = new BettingTable();
+
+    public void placeBet(Player player, Money money) {
+        bettingTable.placeBet(player, money);
+    }
 
     public void dealDealerCardsOut() {
         for (int i = 0; i < START_CARD_COUNT; i++) {
@@ -42,5 +49,13 @@ public class BlackjackService {
     public void finalizeGameResult(Player player) {
         player.calculateResult(dealer);
         dealer.saveResult(player.getWinStatus());
+    }
+
+    public Money getProfit(Player player) {
+        return bettingTable.calculateProfit(player);
+    }
+
+    public Money getDealerProfit() {
+        return bettingTable.getDealerProfit();
     }
 }
