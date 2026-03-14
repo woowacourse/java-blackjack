214ee2d2a1dd6287d97c39822a6929131ea4aeca
khcho96 <khcho1492@gmail.com>
2026-03-14 22:41:50 +0900

    refactor(PlayerName): isPositive를 isNotPositive로 메서드명 수정 및 NUMBER_FORMAT 상수화

--- src/main/java/domain/participant/BetAmount.java
+++ src/main/java/domain/participant/BetAmount.java
@@ -2,8 +2,9 @@ package domain.participant;
 
 public class BetAmount {
 
-    public static final String INVALID_BET_AMOUNT_NUMBER = "배팅 금액은 숫자여야 합니다.";
-    public static final String INVALID_BET_AMOUNT_POSITIVE = "배팅 금액은 양수여야 합니다.";
+    private static final String INVALID_BET_AMOUNT_NUMBER = "배팅 금액은 숫자여야 합니다.";
+    private static final String INVALID_BET_AMOUNT_POSITIVE = "배팅 금액은 양수여야 합니다.";
+    private static final String NUMBER_FORMAT = "-?\\d+";
 
     private final int betAmount;
 
@@ -24,16 +25,16 @@ public class BetAmount {
     }
 
     private static boolean isNotNumber(String betAmountInput) {
-        return !betAmountInput.matches("-*\\d+");
+        return !betAmountInput.matches(NUMBER_FORMAT);
     }
 
     private void validatePositive(String betAmountInput) {
-        if (isPositive(betAmountInput)) {
+        if (isNotPositive(betAmountInput)) {
             throw new IllegalArgumentException(INVALID_BET_AMOUNT_POSITIVE);
         }
     }
 
-    private static boolean isPositive(String betAmountInput) {
+    private static boolean isNotPositive(String betAmountInput) {
         return Integer.parseInt(betAmountInput) <= 0;
     }
 
