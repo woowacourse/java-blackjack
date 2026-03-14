3cb6fff07cb90b6b6e54e23dbed5a6bb48910f23
Andrew Kim <38521355+nn98@users.noreply.github.com>
2026-03-14 18:50:26 +0900

    refactor(players): 순수한 DTO 로직만, 닉네임 전달

--- src/main/java/blackjack/view/dto/PlayerNicknames.java
+++ src/main/java/blackjack/view/dto/PlayerNicknames.java
@@ -1,4 +1,14 @@
 package blackjack.view.dto;
 
-public class PlayerNicknames {
+import blackjack.domain.participant.Player;
+import java.util.List;
+
+public record PlayerNicknames(List<String> nicknames) {
+
+    public static PlayerNicknames from(List<Player> players) {
+        List<String> playerNicknames = players.stream()
+                .map(Player::getNickname)
+                .toList();
+        return new PlayerNicknames(playerNicknames);
+    }
 }
