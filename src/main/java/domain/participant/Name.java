7d82e85f789f7ccc4e80b0c0f1427c50fc65fee2
alstj2384 <ass91160@naver.com>
2026-03-14 11:31:36 +0900

    refactor(Name): 이름 길이 예외 메시지에 상수를 이용하여 표현하도록 수정

--- src/main/java/domain/participant/Name.java
+++ src/main/java/domain/participant/Name.java
@@ -1,10 +1,11 @@
 package domain.participant;
 
 public class Name {
-    private static final String BLANK_NAME_NOT_ALLOWED = "[ERROR] 빈 값을 입력할 수 없습니다.";
-    private static final String NAME_OUT_OF_RANGE = String.format("[ERROR] 이름은 %d ~ %d자 내여야 합니다.", 1, 10);
     private static final int NAME_MIN_LENGTH = 1;
     private static final int NAME_MAX_LENGTH = 10;
+    private static final String BLANK_NAME_NOT_ALLOWED = "[ERROR] 빈 값을 입력할 수 없습니다.";
+    private static final String NAME_OUT_OF_RANGE = String.format("[ERROR] 이름은 %d ~ %d자 내여야 합니다.", NAME_MIN_LENGTH,
+            NAME_MAX_LENGTH);
     private final String value;
 
     public Name(String name) {
