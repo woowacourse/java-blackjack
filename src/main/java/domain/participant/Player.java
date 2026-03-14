efba60292bcb6ed4498cc0c451f8cc360c49fc23
Huiyeongkim <huiyeong9619@naver.com>
2026-03-14 13:12:39 +0900

    refactor: money Optional로 변경

--- src/main/java/domain/participant/Player.java
+++ src/main/java/domain/participant/Player.java
@@ -2,6 +2,7 @@ package domain.participant;
 
 import domain.money.Money;
 
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 import static util.BlackJackConstant.MAX_NAME_LENGTH;
@@ -11,7 +12,7 @@ public class Player extends Participant {
     private static final String STRING_REGEX = "^[a-zA-Z]*$";
 
     private final String name;
-    private Money money = null;
+    private Optional<Money> money = Optional.empty();
 
     public Player(String name) {
         validateNameLength(name);
@@ -20,11 +21,10 @@ public class Player extends Participant {
     }
 
     public void bet(Integer money) {
-        if (this.money != null) {
+        if (this.money.isPresent()) {
             throw new IllegalArgumentException("베팅 금액은 변경할 수 없습니다.");
         }
-
-        this.money = new Money(money);
+        this.money = Optional.of(new Money(money));
     }
 
     public String getName() {
@@ -32,7 +32,8 @@ public class Player extends Participant {
     }
 
     public Money getMoney() {
-        return money;
+        return money.orElseThrow(() ->
+                new IllegalStateException("베팅 금액이 설정되지 않았습니다."));
     }
 
     private void validateNameLength(String name) {
