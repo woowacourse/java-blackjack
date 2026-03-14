577950a998acab1ea15dcfd0d84071a50dccb2b2
MODUGGAGI <kiroro0814@naver.com>
2026-03-15 08:58:50 +0900

    refactor(remove-test): 테스트 이동
    이름의 유효성 검증을 값 객체로 이동하며 미처 지우지 못한 테스트를 제거했습니다.

--- src/test/java/domain/participant/PlayerTest.java
+++ src/test/java/domain/participant/PlayerTest.java
@@ -1,7 +1,5 @@
 package domain.participant;
 
-import static message.ErrorMessage.PLAYER_NAME_OUT_OF_RANGE;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,20 +7,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayerTest {
 
-    @DisplayName("플레이어 이름의 길이가 1미만, 5초과인 경우 예외가 발생합니다.")
+    @DisplayName("플레이어 정상 생성 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"tony stark", "", "number"})
+    @ValueSource(strings = {"tony", "fizz"})
     void 플레이어_이름_예외_테스트(String name) {
-        Assertions.assertThatThrownBy(() -> new Player(name))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(PLAYER_NAME_OUT_OF_RANGE.getMessage());
-    }
-
-    @DisplayName("플레이어 이름이 1이상 5이하일 경우 정상 생성됩니다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"stark", "fizz", "스타크", "피즈"})
-    void 플레이어_정상_생성_테스트(String name) {
         Player player = new Player(name);
-        Assertions.assertThat(player.getName()).isEqualTo(name);
+
+        Name playerName = player.getName();
+
+        Assertions.assertThat(playerName).isEqualTo(new Name(name));
     }
 }
