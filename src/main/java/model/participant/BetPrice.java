80574f8b39b1a75d604716fde1d8d55a879c1c27
armd482 <armd482@naver.com>
2026-03-14 17:46:24 +0900

    refactor: BetPrice 타입 변경

--- src/main/java/model/participant/BetPrice.java
+++ src/main/java/model/participant/BetPrice.java
@@ -2,14 +2,14 @@ package model.participant;
 
 import constant.ErrorMessage;
 
-public record BetPrice(Integer value) {
-    private static final Integer MAX_BET = 100000000;
+public record BetPrice(int value) {
+    private static final int MAX_BET = 100000000;
 
     public BetPrice {
         validate(value);
     }
 
-    private static void validate(Integer value) {
+    private static void validate(int value) {
         if(value <= 0 || value > MAX_BET) {
             throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE_BET.getMessage());
         }
