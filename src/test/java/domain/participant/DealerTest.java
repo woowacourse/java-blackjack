c779f8c9b55e75e70f2dbded53264c0eb75c9a58
khcho96 <khcho1492@gmail.com>
2026-03-14 13:22:42 +0900

    test(DealerTest): 테스트 코드 작성

--- src/test/java/domain/participant/DealerTest.java
+++ src/test/java/domain/participant/DealerTest.java
@@ -36,4 +36,58 @@ class DealerTest {
             }
         }
     }
+
+    @Nested
+    class ShouldHitTest {
+
+        @Nested
+        class Success {
+
+            @Test
+            void 딜러_점수가_16_이하면_카드를_더_뽑는다() {
+
+                // given
+                Dealer dealer = new Dealer();
+                dealer.addCard(new Card(Rank.TEN, Suit.HEART));
+                dealer.addCard(new Card(Rank.SIX, Suit.SPADE));
+
+                // when
+                boolean actual = dealer.shouldHit();
+
+                // then
+                assertThat(actual).isTrue();
+            }
+
+            @Test
+            void 딜러_점수가_17이면_카드를_더_뽑지_않는다() {
+
+                // given
+                Dealer dealer = new Dealer();
+                dealer.addCard(new Card(Rank.TEN, Suit.HEART));
+                dealer.addCard(new Card(Rank.SEVEN, Suit.SPADE));
+
+                // when
+                boolean actual = dealer.shouldHit();
+
+                // then
+                assertThat(actual).isFalse();
+            }
+
+            @Test
+            void 딜러_점수가_21_초과면_카드를_더_뽑지_않는다() {
+
+                // given
+                Dealer dealer = new Dealer();
+                dealer.addCard(new Card(Rank.TEN, Suit.HEART));
+                dealer.addCard(new Card(Rank.NINE, Suit.SPADE));
+                dealer.addCard(new Card(Rank.THREE, Suit.CLOVER));
+
+                // when
+                boolean actual = dealer.shouldHit();
+
+                // then
+                assertThat(actual).isFalse();
+            }
+        }
+    }
 }
