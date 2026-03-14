cffc79485bf385ff53596b50e2c34580eb554713
koreaioi <koreaioi@gachon.ac.kr>
2026-03-15 05:42:18 +0900

    refactor: 수익금 테스트 코드를 ProfitsTest로 이동

--- src/test/java/domain/betting/BettingsTest.java
+++ src/test/java/domain/betting/BettingsTest.java
@@ -25,23 +25,6 @@ public class BettingsTest {
     }
 
     @Test
-    void 딜러의_수익금을_구할때는_플레이어_수익금_누적합에_마이너스1을_곱한다() {
-        int tenThousand = 1000;
-        BettingMoney bettingMoney = BettingMoney.bet(tenThousand);
-        Player testPlayer = Player.from(new PlayerName("test"));
-        Profits profits = new Profits();
-
-        Profit profit = bettingMoneyToProfit(bettingMoney, BettingRate.PLAYER_WIN.getBettingRate());
-        profits.settleProfit(testPlayer.getGamerName(), profit);
-
-        Profit actualDealerProfit = profits.calculateDealerProfit();
-        Profit expectedDealerProfit = new Profit(tenThousand).reverseProfit();
-
-        Assertions.assertThat(actualDealerProfit)
-                .isEqualTo(expectedDealerProfit);
-    }
-
-    @Test
     void 플레이어가_베팅을_두번이상할_수_없다() {
         int tenThousand = 1000;
         BettingMoney bettingMoney = BettingMoney.bet(tenThousand);
@@ -53,10 +36,5 @@ public class BettingsTest {
             bettings.bet(testPlayer.getGamerName(), bettingMoney);
         }).isInstanceOf(BettingException.class);
     }
-
-    public Profit bettingMoneyToProfit(BettingMoney bettingMoney, double rate) {
-        return new Profit(bettingMoney.withRate(rate));
-    }
-
 
 }
--- src/test/java/domain/betting/ProfitsTest.java
+++ src/test/java/domain/betting/ProfitsTest.java
@@ -23,6 +23,23 @@ public class ProfitsTest {
         }).isInstanceOf(BettingException.class);
     }
 
+    @Test
+    void 딜러의_수익금을_구할때는_플레이어_수익금_누적합에_마이너스1을_곱한다() {
+        int tenThousand = 1000;
+        BettingMoney bettingMoney = BettingMoney.bet(tenThousand);
+        Player testPlayer = Player.from(new PlayerName("test"));
+        Profits profits = new Profits();
+
+        Profit profit = bettingMoneyToProfit(bettingMoney, BettingRate.PLAYER_WIN.getBettingRate());
+        profits.settleProfit(testPlayer.getGamerName(), profit);
+
+        Profit actualDealerProfit = profits.calculateDealerProfit();
+        Profit expectedDealerProfit = new Profit(tenThousand).reverseProfit();
+
+        Assertions.assertThat(actualDealerProfit)
+                .isEqualTo(expectedDealerProfit);
+    }
+
     private Profit bettingMoneyToProfit(BettingMoney bettingMoney, double rate) {
         return new Profit(bettingMoney.withRate(rate));
     }
