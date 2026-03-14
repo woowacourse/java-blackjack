8e3a382561f08b65fea55a281613b8cf08a1285f
Andrew Kim <38521355+nn98@users.noreply.github.com>
2026-03-15 03:03:31 +0900

    refactor(runner): feedback05 - 실행 흐름 관리 이관에 따른 불필요 상태 제거

--- src/main/java/blackjack/domain/participant/Player.java
+++ src/main/java/blackjack/domain/participant/Player.java
@@ -4,26 +4,14 @@ import static blackjack.domain.participant.Dealer.DEALER_NICKNAME;
 
 public class Player extends Participant {
 
-    private boolean stand;
-
     public Player(String nickname) {
         super(nickname);
         validate(nickname);
-        this.stand = false;
     }
 
     private void validate(String nickname) {
         if (nickname.equals(DEALER_NICKNAME)) {
             throw new IllegalArgumentException("사용 불가능한 플레이어 닉네임입니다.");
         }
-    }
-
-    @Override
-    public boolean isDrawable() {
-        return !stand && super.isDrawable();
-    }
-
-    public void stand() {
-        stand = true;
     }
 }
