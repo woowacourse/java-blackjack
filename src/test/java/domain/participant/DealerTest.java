490eae1b8b7eb676be0371bcc913c016f2ac6102
alstj2384 <ass91160@naver.com>
2026-03-14 18:09:02 +0900

    test(Dealer): 테스트 케이스 추가

--- src/test/java/domain/participant/DealerTest.java
+++ src/test/java/domain/participant/DealerTest.java
@@ -6,7 +6,8 @@ import domain.Rank;
 import domain.Score;
 import domain.Suit;
 import domain.card.Card;
-import org.junit.jupiter.api.Assertions;
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DealerTest {
@@ -16,7 +17,7 @@ public class DealerTest {
     }
 
     @Test
-    void 딜러_카드_중_첫번째_카드만_반환해야_한다() {
+    void 손패_중_첫번째_카드만_반환해야_한다() {
         Dealer dealer = Dealer.createReady();
 
         dealer.addCard(Card.of(Suit.CLUB, Rank.K));
@@ -24,26 +25,26 @@ public class DealerTest {
         dealer.addCard(Card.of(Suit.CLUB, Rank.NINE));
         Card firstCard = dealer.getFirstCard();
 
-        Assertions.assertEquals(firstCard, Card.of(Suit.CLUB, Rank.K));
+        Assertions.assertThat(firstCard).isEqualTo(Card.of(Suit.CLUB, Rank.K));
     }
 
     @Test
     void 딜러의_합계가_기준보다_작으면_참을_반환해야_한다() {
         Dealer dealer = Dealer.createReady();
 
-        dealer.addCard(Card.of(Suit.CLUB, Rank.FOUR));
-        dealer.addCard(Card.of(Suit.CLUB, Rank.ACE));
+        List<Card> sum15Cards = List.of(Card.of(Suit.CLUB, Rank.FOUR), Card.of(Suit.CLUB, Rank.ACE));
+        dealer.addCards(sum15Cards);
 
-        Assertions.assertEquals(dealer.isHittable(Score.DEALER_HIT_STAND_BOUNDARY), true);
+        Assertions.assertThat(dealer.isHittable(Score.DEALER_HIT_STAND_BOUNDARY)).isTrue();
     }
 
     @Test
     void 딜러의_합계가_기준보다_크면_거짓을_반환해야_한다() {
         Dealer dealer = Dealer.createReady();
 
-        dealer.addCard(Card.of(Suit.CLUB, Rank.SIX));
-        dealer.addCard(Card.of(Suit.CLUB, Rank.ACE));
+        List<Card> sum17Cards = List.of(Card.of(Suit.CLUB, Rank.SIX), Card.of(Suit.CLUB, Rank.ACE));
+        dealer.addCards(sum17Cards);
 
-        Assertions.assertEquals(dealer.isHittable(Score.DEALER_HIT_STAND_BOUNDARY), false);
+        Assertions.assertThat(dealer.isHittable(Score.DEALER_HIT_STAND_BOUNDARY)).isFalse();
     }
 }
