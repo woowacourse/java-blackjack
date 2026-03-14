ba0163a075a9087b1b55a733b8b42818c5222d50
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-14 16:06:54 +0900

    test(Member): 초기화 변경에 따른 수정

--- src/test/java/domain/MemberTest.java
+++ src/test/java/domain/MemberTest.java
@@ -1,9 +1,12 @@
 package domain;
 
+import domain.member.BettingAmount;
+import domain.member.DealerRole;
+import domain.member.Member;
+import domain.member.Name;
+import domain.member.PlayerRole;
 import domain.vo.RoundResult;
 import domain.card.Card;
-import domain.member.Dealer;
-import domain.member.Player;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,8 +16,8 @@ public class MemberTest {
     @DisplayName("플레이어가 이기면 RoundResult를 반환한다.")
     @Test
     void winnerTest_isPlayerThenDecide_returnMember() {
-        Dealer dealer = new Dealer();
-        Player player = new Player("브리", 1000);
+        Member dealer = new Member(new Name("포비"), new DealerRole());
+        Member player = new Member(new Name("브리"), new PlayerRole(new BettingAmount(1000)));
 
         dealer.receiveCard(new Card("2", "하트"));
         dealer.receiveCard(new Card("3","클로버"));
