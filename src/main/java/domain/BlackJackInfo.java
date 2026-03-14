b44b121782a4458721d29796b481c7d1ff5b11a6
JunHyung1206 <pjh120610@naver.com>
2026-03-14 09:54:00 +0900

    fix: final로 수정

--- src/main/java/domain/BlackJackInfo.java
+++ src/main/java/domain/BlackJackInfo.java
@@ -1,8 +1,8 @@
 package domain;
 
 public final class BlackJackInfo {
-    public static int BLACKJACK_SCORE = 21;
-    public static int SOFT_HAND_PROCESS_SCORE = 10;
-    public static int DEALER_THRESHOLD_SCORE = 16;
-    public static int FIRST_CARD_COUNT = 2;
+    public static final int BLACKJACK_SCORE = 21;
+    public static final int SOFT_HAND_PROCESS_SCORE = 10;
+    public static final int DEALER_THRESHOLD_SCORE = 16;
+    public static final int FIRST_CARD_COUNT = 2;
 }
