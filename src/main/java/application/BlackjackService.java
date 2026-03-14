132e809571b1460ed92a716e91a0b395e50fc659
JYL35 <dlwodbs1223@naver.com>
2026-03-15 01:03:22 +0900

    refactor(Service): 변경된 GameTable에 맞게 수정

--- src/main/java/application/BlackjackService.java
+++ src/main/java/application/BlackjackService.java
@@ -2,11 +2,15 @@ package application;
 
 import domain.card.Card;
 import domain.GameTable;
+import domain.member.Money;
 import dto.RoundResult;
 import domain.card.StandardDeck;
 import dto.GameResult;
 import dto.MemberStatus;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BlackjackService {
 
@@ -15,15 +19,20 @@ public class BlackjackService {
     public BlackjackService() {
     }
 
-    public void initializeGame(List<String> playerNames) {
-        this.gameTable = new GameTable(playerNames, new StandardDeck());
+    public void initializeGame(Map<String, Integer> playerBetAmounts) {
+        Map<String, Money> playerBets = new HashMap<>();
+        for (String name : playerBetAmounts.keySet()) {
+            Money betMoney = new Money(playerBetAmounts.get(name));
+            playerBets.put(name, betMoney);
+        }
+        this.gameTable = new GameTable(playerBets, new StandardDeck());
         gameTable.distributeInitCard();
     }
 
     public RoundResult startOneRound(String memberName) {
         List<Card> playerCards = getGameTable().drawForMember(memberName);
 
-        boolean isBust = getGameTable().checkBust(memberName);
+        boolean isBust = getGameTable().isPlayerBust(memberName);
 
         return new RoundResult(playerCards, isBust);
     }
