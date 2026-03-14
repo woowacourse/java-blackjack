6d34ab4399a1b616674fcf16ea85939db52cc126
Andrew Kim <38521355+nn98@users.noreply.github.com>
2026-03-15 01:56:10 +0900

    refactor(bet): feedback03 - 배팅 금액 단위, 상한 추가

--- src/main/java/blackjack/domain/bet/Bet.java
+++ src/main/java/blackjack/domain/bet/Bet.java
@@ -3,6 +3,7 @@ package blackjack.domain.bet;
 public class Bet {
 
     private final int UNIT_AMOUNT = 100;
+    private final int MAX_AMOUNT = 1000000;
 
     private final int amount;
 
@@ -16,7 +17,11 @@ public class Bet {
             throw new IllegalArgumentException("배팅 금액은 1 이상의 양수여야 합니다.");
         }
         if (amount % UNIT_AMOUNT != 0) {
-            throw new IllegalArgumentException(String.format("배팅 금액은 %d 단위여야 합니다", UNIT_AMOUNT));
+            throw new IllegalArgumentException(String.format("배팅 금액은 %d 단위여야 합니다.", UNIT_AMOUNT));
+        }
+        if (amount > MAX_AMOUNT) {
+            throw new IllegalArgumentException(
+                    String.format("배팅 금액은 %d 이하여야 합니다.%n(도박문제 전문상담 국번없이 1336)", MAX_AMOUNT));
         }
     }
 
