ec56a6732b6f861b2393eedf7b56f27b950f1e3f
alstj2384 <ass91160@naver.com>
2026-03-14 18:12:16 +0900

    test(Name): 테스트 케이스 추가

--- src/test/java/domain/participant/NameTest.java
+++ src/test/java/domain/participant/NameTest.java
@@ -5,12 +5,20 @@ import org.junit.jupiter.api.Test;
 
 public class NameTest {
     @Test
-    void 이름이_공백인_경우_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> Name.valueOf(" ")).isInstanceOf(IllegalArgumentException.class);
+    void Null로_생성할_경우_예외가_발생한다() {
+        String emptyString = null;
+        Assertions.assertThatThrownBy(() -> Name.valueOf(emptyString)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 이름이_10자를_초과하는_경우_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> Name.valueOf("0123456789a")).isInstanceOf(IllegalArgumentException.class);
+    void 공백으로_생성할_경우_예외가_발생한다() {
+        String emptyString = " ";
+        Assertions.assertThatThrownBy(() -> Name.valueOf(emptyString)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 길이가_범위를_초과하는_경우_예외가_발생한다() {
+        String length10String = "01234567890";
+        Assertions.assertThatThrownBy(() -> Name.valueOf(length10String)).isInstanceOf(IllegalArgumentException.class);
     }
 }
