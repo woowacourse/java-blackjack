406cbcc27ec659224dcd5b87027ab64773092866
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-14 16:05:42 +0900

    refactor(inputView): 입력 반환값 수정

--- src/main/java/presentation/ui/InputView.java
+++ src/main/java/presentation/ui/InputView.java
@@ -1,5 +1,7 @@
 package presentation.ui;
 
+import domain.member.BettingAmount;
+import domain.member.Name;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,16 +16,22 @@ public class InputView {
         this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    public int readBettingAmount(String playerName) {
+    public BettingAmount readBettingAmount(String playerName) {
         try {
             System.out.printf("%s의 배팅 금액은?\n", playerName);
-            return Integer.parseInt(bufferedReader.readLine());
+            return new BettingAmount(Integer.parseInt(bufferedReader.readLine()));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public List<String> readPlayerNames() {
+    public List<Name> readPlayerNames() {
+        return parsePlayerNames().stream()
+                .map(Name::new)
+                .toList();
+    }
+
+    public List<String> parsePlayerNames() {
         try {
             System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
             return Stream.of(bufferedReader.readLine().split(","))
@@ -34,7 +42,7 @@ public class InputView {
         }
     }
 
-    public Boolean playContinue(String playerName) {
+    public boolean playContinue(String playerName) {
         System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n", playerName);
         try {
             String answer = bufferedReader.readLine();
