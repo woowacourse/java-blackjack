2036729d10ba5819af4e64f5cbdd370a4124d9f7
Soojin6943 <sjhan0711@gmail.com>
2026-03-14 21:16:53 +0900

    refactor(input): 누락된 테스트 추가

--- src/test/java/view/InputValidatorTest.java
+++ src/test/java/view/InputValidatorTest.java
@@ -1,11 +1,12 @@
 package view;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import util.InputValidator;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class InputValidatorTest {
 
@@ -23,6 +24,30 @@ public class InputValidatorTest {
     void 크루_이름_성공_케이스(String input) {
         assertThat(InputValidator.validatePlayerNames(input))
                 .isTrue();
+    }
+
+    @Test
+    void 베팅_금액_성공_테스트() {
+        String betAmount = "20000";
+
+        assertThat(InputValidator.validateBetAmount(betAmount))
+                .isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 베팅_금액_실패_테스트(String input) {
+        assertThatThrownBy(() -> InputValidator.validateBetAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력 형식이 잘못됐습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"99", "101"})
+    void 베팅_금액_단위_실패_테스트(String input) {
+        assertThatThrownBy(() -> InputValidator.validateBetAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("베팅 금액은 10 단위로 입력해야 합니다.");
     }
 
 }
