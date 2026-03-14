72d918eea94fedb2c50cb3516210c0cea903c327
haeseon lee <haeseon4816@gmail.com>
2026-03-14 13:35:45 +0900

    refactor: Participant 클래서 메서드 위치 조정 및 상수 설정

--- src/main/java/blackjack/domain/Participant.java
+++ src/main/java/blackjack/domain/Participant.java
@@ -5,20 +5,15 @@ import java.util.List;
 
 public abstract class Participant {
 
+    private static final int BLACKJACK_SCORE = 21;
+    private static final int BLACKJACK_SIZE = 2;
+
     protected final Name name;
     protected final Cards drawnCards;
 
     protected Participant(String name) {
         this.name = new Name(name);
         this.drawnCards = new Cards();
-    }
-
-    public String getName() {
-        return name.getName();
-    }
-
-    public boolean isDealerNotDone() {
-        return false;
     }
 
     public static Participant createPlayer(String name, Money money) {
@@ -33,27 +28,35 @@ public abstract class Participant {
         drawnCards.addCard(card);
     }
 
-    public List<String> getCardNames() {
-        return drawnCards.getCardNames();
+    public boolean isDealerNotDone() {
+        return false;
     }
 
     public boolean isBust() {
-        return drawnCards.sumScore() > 21;
+        return drawnCards.sumScore() > BLACKJACK_SCORE;
     }
 
     public boolean isBlackjack() {
-        return drawnCards.sumScore() == 21 && drawnCards.getSize() == 2;
+        return drawnCards.sumScore() == BLACKJACK_SCORE && drawnCards.getSize() == BLACKJACK_SIZE;
     }
 
     public int calculateTotalScore() {
         return drawnCards.sumScore();
     }
 
+    public int calculateFinalProfit(Participant dealer) {
+        return 0;
+    }
+
     public GameResult judgeResult(List<Participant> players, Participant dealer) {
         return new GameResult(new HashMap<>(), new HashMap<>());
     }
 
-    public int calculateFinalProfit(Participant dealer) {
-        return 0;
+    public String getName() {
+        return name.getName();
+    }
+
+    public List<String> getCardNames() {
+        return drawnCards.getCardNames();
     }
 }
