ab85fbcd234c7506eb8337f5c1564e332e36bca0
gleaming9 <gleaming9@khu.ac.kr>
2026-03-14 20:44:01 +0900

    refactor: 헬퍼메서드 제거 후 직접 Map 생성하도록 로직 변경

--- src/test/java/domain/BettingTableTest.java
+++ src/test/java/domain/BettingTableTest.java
@@ -9,20 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BettingTableTest {
 
-    private Map<Player, Money> createMoneyTable(Player... players) {
-        Map<Player, Money> moneyTable = new LinkedHashMap<>();
-        for (Player player : players) {
-            moneyTable.put(player, new Money(1000L));
-        }
-        return moneyTable;
-    }
-
     @Test
     void 승리시_베팅금액은_1배로_정산된다() {
         // given
         Player player1 = new Player("시오");
         Player player2 = new Player("봉구스");
-        BettingTable bettingTable = new BettingTable(createMoneyTable(player1,player2));
+        Map<Player, Money> bets = Map.of(
+                player1, new Money(1000L),
+                player2, new Money(2000L)
+        );
+        BettingTable bettingTable = new BettingTable(bets);
 
         // when
         bettingTable.settleBet(player1, WinningStatus.WIN);
@@ -30,14 +26,17 @@ class BettingTableTest {
 
         // then
         assertEquals(1000L, bettingTable.getPlayerMoneyAmount(player1));
-        assertEquals(1000L, bettingTable.getPlayerMoneyAmount(player2));
+        assertEquals(2000L, bettingTable.getPlayerMoneyAmount(player2));
     }
 
     @Test
     void 패배시_베팅금액은_1배로_차감된다() {
         // given
         Player player1 = new Player("시오");
-        BettingTable bettingTable = new BettingTable(createMoneyTable(player1));
+        Map<Player, Money> bets = Map.of(
+                player1, new Money(1000L)
+        );
+        BettingTable bettingTable = new BettingTable(bets);
 
         // when
         bettingTable.settleBet(player1, WinningStatus.LOSE);
@@ -50,7 +49,10 @@ class BettingTableTest {
     void 블랙잭일_때_베팅금액은_일점오배로_정산된다() {
         // given
         Player player1 = new Player("시오");
-        BettingTable bettingTable = new BettingTable(createMoneyTable(player1));
+        Map<Player, Money> bets = Map.of(
+                player1, new Money(1000L)
+        );
+        BettingTable bettingTable = new BettingTable(bets);
 
         // when
         bettingTable.settleBet(player1, WinningStatus.BLACKJACK_WIN);
@@ -64,14 +66,18 @@ class BettingTableTest {
         // given
         Player player1 = new Player("시오");
         Player player2 = new Player("봉구스");
-        BettingTable bettingTable = new BettingTable(createMoneyTable(player1,player2));
+        Map<Player, Money> bets = Map.of(
+                player1, new Money(1000L),
+                player2, new Money(2000L)
+        );
+        BettingTable bettingTable = new BettingTable(bets);
 
         // when
         bettingTable.settleBet(player1, WinningStatus.WIN);
         bettingTable.settleBet(player2, WinningStatus.WIN);
 
         // then
-        assertEquals(-2000L, bettingTable.calculateDealerProfit());
+        assertEquals(-3000L, bettingTable.calculateDealerProfit());
     }
 
     @Test
@@ -79,28 +85,36 @@ class BettingTableTest {
         // given
         Player player1 = new Player("시오");
         Player player2 = new Player("봉구스");
-        BettingTable bettingTable = new BettingTable(createMoneyTable(player1,player2));
+        Map<Player, Money> bets = Map.of(
+                player1, new Money(2000L),
+                player2, new Money(3000L)
+        );
+        BettingTable bettingTable = new BettingTable(bets);
 
         // when
         bettingTable.settleBet(player1, WinningStatus.LOSE);
         bettingTable.settleBet(player2, WinningStatus.LOSE);
 
         // then
-        assertEquals(2000L, bettingTable.calculateDealerProfit());
+        assertEquals(5000L, bettingTable.calculateDealerProfit());
     }
 
     @Test
-    void 블랙잭_으로_승리한_플레이어들의_베팅금액만큼_딜러수익은_차감된다() {
+    void 블랙잭으로_승리한_플레이어들의_베팅금액만큼_딜러수익은_차감된다() {
         // given
         Player player1 = new Player("시오");
         Player player2 = new Player("봉구스");
-        BettingTable bettingTable = new BettingTable(createMoneyTable(player1,player2));
+        Map<Player, Money> bets = Map.of(
+                player1, new Money(1000L),
+                player2, new Money(2000L)
+        );
+        BettingTable bettingTable = new BettingTable(bets);
 
         // when
         bettingTable.settleBet(player1, WinningStatus.BLACKJACK_WIN);
         bettingTable.settleBet(player2, WinningStatus.BLACKJACK_WIN);
 
         // then
-        assertEquals(-3000L, bettingTable.calculateDealerProfit());
+        assertEquals(-4500L, bettingTable.calculateDealerProfit());
     }
 }
