bb119be4de3562aad316779bdac6ef7777b83e20
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-03-14 21:58:59 +0900

    test: 메서드 네이밍 명확하게 하기

--- src/test/java/profit/ProfitCalculatorTest.java
+++ src/test/java/profit/ProfitCalculatorTest.java
@@ -1,4 +1,4 @@
-package result;
+package profit;
 
 import domain.bet.BetTable;
 import domain.card.Card;
@@ -8,7 +8,9 @@ import domain.participant.Dealer;
 import domain.participant.Hand;
 import domain.participant.Player;
 import domain.participant.PlayerName;
-import domain.result.ProfitCalculator;
+import domain.profit.ProfitCalculator;
+import domain.profit.ProfitTable;
+import domain.result.Result;
 import domain.result.ResultInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,6 +22,7 @@ class ProfitCalculatorTest {
     Player player;
     Dealer dealer;
     BetTable betTable;
+    ProfitTable profitTable;
 
     @BeforeEach
     void init() {
@@ -36,96 +39,97 @@ class ProfitCalculatorTest {
 
         betTable = new BetTable();
         betTable.recodeAmount(player.getName(), 10000);
+
+        profitTable = new ProfitTable();
     }
 
     @Test
-    @DisplayName("플레이어가 게임을 이겼을 때 수익(베팅한 금액만큼)을 베팅테이블에 기록한다.")
-    void calculateProfit_returnsBetProfit_whenPlayerIsWin() {
+    @DisplayName("플레이어가 게임을 이겼을 때 수익(베팅한 금액만큼)을 계산한다.")
+    void playerCalculateProfit_returnsBetProfit_whenPlayerIsWin() {
+        Result result = new Result();
+        result.setPlayerResult(player.getName(), ResultInfo.WIN);
         ProfitCalculator calculator = new ProfitCalculator(betTable);
         int expectedProfit = 10000;
 
-        calculator.playerCalculateProfit(ResultInfo.WIN, player);
+        int profit = calculator.playerCalculateProfit(result, player);
 
-        assertThat(betTable.getAmountByName(player.getName())).isEqualTo(expectedProfit);
+        assertThat(profit).isEqualTo(expectedProfit);
     }
 
     @Test
-    @DisplayName("플레이어가 게임을 졌을 때 수익(베팅한 금액의 마이너스만큼)을 베팅테이블에 기록한다.")
-    void calculateProfit_returnsBetLoss_whenPlayerIsDefeat() {
+    @DisplayName("플레이어가 게임을 졌을 때 수익(베팅한 금액의 마이너스만큼)을 계산한다.")
+    void playerCalculateProfit_returnsBetLoss_whenPlayerIsDefeat() {
+        Result result = new Result();
+        result.setPlayerResult(player.getName(), ResultInfo.DEFEAT);
         ProfitCalculator calculator = new ProfitCalculator(betTable);
         int expectedProfit = -10000;
 
-        calculator.playerCalculateProfit(ResultInfo.DEFEAT, player);
+        int profit = calculator.playerCalculateProfit(result, player);
 
-        assertThat(betTable.getAmountByName(player.getName())).isEqualTo(expectedProfit);
+        assertThat(profit).isEqualTo(expectedProfit);
     }
 
-
     @Test
-    @DisplayName("플레이어가 게임을 비겼을 때 수익(0원)을 베팅테이블에 기록한다.")
-    void calculateProfit_returnsZero_whenPlayerAndDealerIsDraw() {
+    @DisplayName("플레이어가 게임을 비겼을 때 수익(0원)을 계산한다.")
+    void playerCalculateProfit_returnsZero_whenPlayerAndDealerIsDraw() {
+        Result result = new Result();
+        result.setPlayerResult(player.getName(), ResultInfo.DRAW);
         ProfitCalculator calculator = new ProfitCalculator(betTable);
         int expectedProfit = 0;
 
-        calculator.playerCalculateProfit(ResultInfo.DRAW, player);
+        int profit = calculator.playerCalculateProfit(result, player);
 
-        assertThat(betTable.getAmountByName(player.getName())).isEqualTo(expectedProfit);
+        assertThat(profit).isEqualTo(expectedProfit);
     }
 
     @Test
-    @DisplayName("플레이어가 블랙잭으로 이겼을 때 수익(베팅한 금액의 1.5배)을 베팅테이블에 기록한다.")
-    void calculateProfit_returnsOneAndHalfBet_whenPlayerIsBlackjack() {
+    @DisplayName("플레이어가 블랙잭으로 이겼을 때 수익(베팅한 금액의 0.5배)을 계산한다.")
+    void playerCalculateProfit_returnsZeroAndHalfBet_whenPlayerIsBlackjack() {
+        // given
         Player player2 = new Player(new PlayerName("user2"), new Hand());
+
         betTable.recodeAmount(player2.getName(), 15000);
+
         player2.keepCard(new Card(Rank.ACE, Pattern.CLOVER));
         player2.keepCard(new Card(Rank.JACK, Pattern.HEART));
+
+        Result result = new Result();
+        result.setPlayerResult(player2.getName(), ResultInfo.BLACKJACK_WIN);
+
         int expectedProfit = 22500 - 15000;
 
+        // when
         ProfitCalculator calculator = new ProfitCalculator(betTable);
-        calculator.playerCalculateProfit(ResultInfo.BLACKJACK_WIN, player2);
+        int profit = calculator.playerCalculateProfit(result, player2);
 
-        assertThat(betTable.getAmountByName(player2.getName())).isEqualTo(expectedProfit);
+        // then
+        assertThat(profit).isEqualTo(expectedProfit);
     }
 
     @Test
-    @DisplayName("플레이어와 딜러가 둘 다 블랙잭이면 플레이어의 수익(0원)을 베팅테이블에 기록한다.")
-    void calculateProfit_returnsZero_whenPlayerAndDealerIsBlackjack() {
+    @DisplayName("플레이어와 딜러가 둘 다 블랙잭이면 플레이어의 수익(0원)을 계산한다.")
+    void playerCalculateProfit_returnsZero_whenPlayerAndDealerIsBlackjack() {
         // given
         Dealer dealer2 = new Dealer(new Hand());
         Player player2 = new Player(new PlayerName("user2"), new Hand());
+
+        betTable.recodeAmount(player2.getName(), 15000);
 
         dealer2.keepCard(new Card(Rank.ACE, Pattern.CLOVER));
         dealer2.keepCard(new Card(Rank.KING, Pattern.CLOVER));
         player2.keepCard(new Card(Rank.ACE, Pattern.HEART));
         player2.keepCard(new Card(Rank.JACK, Pattern.HEART));
 
-        betTable.recodeAmount(player2.getName(), 15000);
+        Result result = new Result();
+        result.setPlayerResult(player2.getName(), ResultInfo.DRAW);
 
         int expectedProfit = 0;
 
         // when
         ProfitCalculator calculator = new ProfitCalculator(betTable);
-        calculator.playerCalculateProfit(ResultInfo.DRAW, player2);
+        int profit = calculator.playerCalculateProfit(result, player2);
 
         // then
-        assertThat(betTable.getAmountByName(player2.getName())).isEqualTo(expectedProfit);
-    }
-
-    @Test
-    @DisplayName("딜러의 수익을 계산할 수 있다")
-    void calculateProfit_returnsDealerProfit() {
-        Player player2 = new Player(new PlayerName("user2"), new Hand());
-        betTable.recodeAmount(player2.getName(), 15000);
-        player2.keepCard(new Card(Rank.TWO, Pattern.SPADE));
-        player2.keepCard(new Card(Rank.THREE, Pattern.SPADE));
-        int expectedProfit = 5000;
-
-        // 딜러 수익 작성
-        ProfitCalculator calculator = new ProfitCalculator(betTable);
-        calculator.playerCalculateProfit(ResultInfo.WIN, player);
-        calculator.playerCalculateProfit(ResultInfo.DEFEAT, player2);
-        int dealerProfit = calculator.dealerCalculateProfit();
-
-        assertThat(dealerProfit).isEqualTo(expectedProfit);
+        assertThat(profit).isEqualTo(expectedProfit);
     }
 }
