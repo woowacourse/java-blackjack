4e86649a8ce6d078d72d2b8f3fa593353ea89689
Jihyun3478 <leejihyun3478@gmail.com>
2026-03-14 18:48:01 +0900

    test: CardTest 컴파일 오류 수정

--- src/test/java/domain/card/CardTest.java
+++ src/test/java/domain/card/CardTest.java
@@ -9,7 +9,7 @@ public class CardTest {
     @DisplayName("카드에 적힌 숫자가 2~10이면 숫자를 그대로 반환한다.")
     void cardNumberTest() {
         Card card = new Card(CardValue.TWO, CardSuit.CLUB);
-        int number = card.getScore();
+        int number = card.getCardValue().getValue();
 
         Assertions.assertThat(number).isEqualTo(2);
     }
@@ -18,7 +18,7 @@ public class CardTest {
     @DisplayName("카드에 적힌 숫자가 J,Q,K이면 10을 반환된다.")
     void cardAlphabetTest() {
         Card card = new Card(CardValue.JACK, CardSuit.CLUB);
-        int number = card.getScore();
+        int number = card.getCardValue().getValue();
 
         Assertions.assertThat(number).isEqualTo(10);
     }
@@ -27,7 +27,7 @@ public class CardTest {
     @DisplayName("카드에 적힌 숫자가 A이면 1또는 11을 반환한다,")
     void cardAceTest() {
         Card card = new Card(CardValue.ACE, CardSuit.CLUB);
-        int number = card.getScore();
+        int number = card.getCardValue().getValue();
 
         Assertions.assertThat(number).isEqualTo(1);
     }
