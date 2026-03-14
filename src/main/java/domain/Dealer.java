bcad2c8b6904d3df5814ec9ab53008d86825a718
gleaming9 <gleaming9@khu.ac.kr>
2026-03-14 18:15:06 +0900

    refactor: Dealer 클래스에서 불필요한 메서드 제거 및 canHit 메서드 유지

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -2,8 +2,6 @@ package domain;
 
 import domain.card.Card;
 
-import java.util.List;
-
 public class Dealer extends Participant {
     private static final int HIT_LIMIT = 17;
     private static final String DEALER_NAME = "딜러";
@@ -12,32 +10,12 @@ public class Dealer extends Participant {
         super(DEALER_NAME);
     }
 
-    public boolean isBurst() {
-        return hand.isBurst();
-    }
-
     @Override
     public boolean canHit() {
         return hand.getSum() < HIT_LIMIT;
     }
 
-    public void draw(Card card) {
-        hand.addCard(card);
-    }
-
-    public List<Card> getCards() {
-        return hand.getCards();
-    }
-
     public Card getFirstCard() {
         return hand.getFirstCard();
-    }
-
-    public int getHandSize() {
-        return hand.getSize();
-    }
-
-    public int getScore() {
-        return hand.getSum();
     }
 }
