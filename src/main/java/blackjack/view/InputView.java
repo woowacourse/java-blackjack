1e0aa745d4ee00d52cb0daf7dbc5f1e6008e7cd3
softmoca <acyc0909@gmail.com>
2026-03-15 23:06:46 +0900

    refactor: System.lineSeparator() 적용 및 출력 로직 중복 제거

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String lineSeparator = System.lineSeparator();
 
     public Players readPlayers() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
@@ -21,17 +22,16 @@ public class InputView {
                 .map(String::trim)
                 .map(name -> new Player(new Name(name), readBetAmount(name)))
                 .collect(Collectors.toList());
-
         return new Players(players);
     }
 
     private BetAmount readBetAmount(String name) {
-        System.out.println(name + "의 배팅 금액은?");
+        System.out.println(lineSeparator + name + "의 배팅 금액은?");
         return new BetAmount(Integer.parseInt(scanner.nextLine().trim()));
     }
 
     public boolean readHitAnswer(String name) {
-        System.out.printf("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)%n", name);
+        System.out.printf(lineSeparator + "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)" + lineSeparator, name);
         String input = scanner.nextLine();
         validateHitAnswer(input);
 
--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -7,49 +7,42 @@ import blackjack.domain.Players;
 import java.util.Map;
 
 public class OutputView {
+    private static final String lineSeparator = System.lineSeparator();
 
     public void printInitDraw(Players players, Dealer dealer) {
-        System.out.printf("%n딜러와 %s에게 2장을 나누었습니다.%n",
+        System.out.printf(lineSeparator + "딜러와 %s에게 2장을 나누었습니다." + lineSeparator,
                 String.join(", ", players.getNames()));
+        System.out.printf("딜러카드: %s" + lineSeparator, dealer.getFirstCardNames());
 
-        System.out.printf("딜러카드: %s%n", dealer.getFirstCardNames());
-
-        //플레이어 카드 출력
         for (Player player : players.getPlayers()) {
-            System.out.printf("%s카드: %s%n", player.getName(), player.getCardNames());
+            printCard(player);
         }
-
     }
 
     public void printCard(Player player) {
-        System.out.printf("%s카드: %s%n", player.getName(), player.getCardNames());
+        System.out.printf("%s카드: %s" + lineSeparator, player.getName(), player.getCardNames());
     }
 
     public void printDealerDraw() {
-        System.out.println("\n딜러는 16이하라 한장의 카드를 더 받았습니다.");
+        System.out.println(lineSeparator + "딜러는 16이하라 한장의 카드를 더 받았습니다.");
     }
 
     public void printFinalCardResult(Dealer dealer, Players players) {
-
-        System.out.printf("%n딜러카드: %s - 결과: %d%n",
+        System.out.printf(lineSeparator + "딜러카드: %s - 결과: %d" + lineSeparator,
                 String.join(", ", dealer.getCardNames()), dealer.getTotalPoint());
         for (Player player : players.getPlayers()) {
-            System.out.printf("%s카드: %s - 결과: %d%n", player.getName(), player.getCardNames(), player.getTotalPoint());
+            System.out.printf("%s카드: %s - 결과: %d" + lineSeparator, player.getName(), player.getCardNames(),
+                    player.getTotalPoint());
         }
-
-
     }
 
     public void printFinalGameResult(FinalIncome finalIncome) {
         System.out.println("\n## 최종 수익");
-
         System.out.println("딜러: " + finalIncome.getDealerIncome());
 
         for (Map.Entry<Player, Integer> entry : finalIncome.getPlayerIncomeResults().entrySet()) {
             System.out.println(entry.getKey().getName() + ": " + entry.getValue());
         }
-
-
     }
 
 }
