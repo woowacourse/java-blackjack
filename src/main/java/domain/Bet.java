d3ff5634d3682c3892a956a8cb5b7403fbaf34eb
전원준 <jwjjwj1960@naver.com>
2026-03-15 13:34:03 +0900

    feat: 중복 베팅 검증 로직 추가

--- src/main/java/domain/Bet.java
+++ src/main/java/domain/Bet.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Bet {
+    private static final String DUPLICATE_BETTING = "베팅은 중복할 수 없습니다.";
     private final Map<String, Integer> betting;
 
     public Bet() {
@@ -11,10 +12,17 @@ public class Bet {
     }
 
     public void addBetting(String name, int bettingAmount) {
+        if (certainKey(name)) {
+            throw new IllegalArgumentException(DUPLICATE_BETTING);
+        }
         betting.putIfAbsent(name, bettingAmount);
     }
 
     public int bettingAmount(String name) {
         return betting.get(name);
+    }
+
+    private boolean certainKey(String name) {
+        return betting.containsKey(name);
     }
 }
