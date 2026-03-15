058951f6e11538fa9a77615aab8aa430f901ddde
haeyoon1 <hynam1220@naver.com>
2026-03-15 17:02:08 +0900

    feat: 베팅 금액을 담는 불변객체 생성

---
+++ src/main/java/domain/BetAmount.java
@@ -0,0 +1,29 @@
+package domain;
+
+import view.mesage.ErrorMessage;
+
+public class BetAmount {
+    private final Long betAmount;
+
+    private BetAmount(String betAmount) {
+        validateBetAmount(betAmount);
+        this.betAmount = Long.parseLong(betAmount);
+    }
+
+    public static BetAmount of(String betAmount) {
+        return new BetAmount(betAmount);
+    }
+
+    private void validateBetAmount(String betInput) {
+        if (betInput == null) {
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_BET_AMOUNT_INPUT.getMessage());
+        }
+        if (!betInput.matches("[1-9][0-9]*")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_BET_AMOUNT.getMessage());
+        }
+    }
+
+    public Long getBetAmount() {
+        return betAmount;
+    }
+}
