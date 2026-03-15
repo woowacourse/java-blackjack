a9368a330b9b75f5b27c9259a2aad06b9fe5ee2c
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-03-15 16:51:00 +0900

    refactor: 사용하지 않는 필드와 메서드 정리

--- src/main/java/domain/result/ResultInfo.java
+++ src/main/java/domain/result/ResultInfo.java
@@ -1,40 +1,30 @@
 package domain.result;
 
 public enum ResultInfo {
-    WIN(0) {
+    WIN {
         @Override
         public int calculateProfit(int amount) {
             return amount;
         }
     },
-    DRAW(1) {
+    DRAW {
         @Override
         public int calculateProfit(int amount) {
             return 0;
         }
     },
-    DEFEAT(2) {
+    DEFEAT {
         @Override
         public int calculateProfit(int amount) {
             return -amount;
         }
     },
-    BLACKJACK_WIN(3) {
+    BLACKJACK_WIN {
         @Override
         public int calculateProfit(int amount) {
             return (int) (amount * 1.5);
         }
     };
-
-    private final int code;
-
-    ResultInfo(int code) {
-        this.code = code;
-    }
-
-    public int getCode() {
-        return code;
-    }
 
     public abstract int calculateProfit(int amount);
 }
