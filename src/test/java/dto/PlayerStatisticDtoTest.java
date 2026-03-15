55e5d337779597f91a6e3bbebb22ec03a6f9f8f2
khcho96 <khcho1492@gmail.com>
2026-03-15 18:41:43 +0900

    test(PlayerStatisticDtoTest): 테스트 코드 작성

--- src/test/java/dto/PlayerStatisticDtoTest.java
+++ src/test/java/dto/PlayerStatisticDtoTest.java
@@ -2,7 +2,9 @@ package dto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import domain.participant.BetAmount;
 import domain.participant.Player;
+import domain.participant.PlayerName;
 import org.junit.jupiter.api.Test;
 
 class PlayerStatisticDtoTest {
@@ -10,7 +12,7 @@ class PlayerStatisticDtoTest {
     @Test
     void of로_생성하면_플레이어_이름과_수익을_반환한다() {
         // given
-        Player player = new Player("jacob");
+        Player player = new Player(new PlayerName("jacob"), new BetAmount("1000"));
         int profit = 1200;
 
         // when
