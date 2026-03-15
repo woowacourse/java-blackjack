81439bedb3af58843050920080b97546cb17ea5a
khcho96 <khcho1492@gmail.com>
2026-03-15 23:55:24 +0900

    feat(BlackjackControllerFactory): BlackjackGameManager 생성시 의존성 주입을 위한 cardMachine() 및 blackjackJudge() 메서드 구현

--- src/main/java/factory/BlackjackControllerFactory.java
+++ src/main/java/factory/BlackjackControllerFactory.java
@@ -1,7 +1,9 @@
 package factory;
 
 import controller.BlackjackController;
+import domain.card.CardMachine;
 import domain.game.BlackjackGameManager;
+import domain.game.BlackjackJudge;
 import view.InputView;
 import view.OutputView;
 
@@ -12,7 +14,15 @@ public class BlackjackControllerFactory {
     }
 
     public BlackjackGameManager blackjackGameManager() {
-        return new BlackjackGameManager();
+        return new BlackjackGameManager(cardMachine(), blackjackJudge());
+    }
+
+    public CardMachine cardMachine() {
+        return new CardMachine();
+    }
+
+    public BlackjackJudge blackjackJudge() {
+        return new BlackjackJudge();
     }
 
     private InputView inputView() {
