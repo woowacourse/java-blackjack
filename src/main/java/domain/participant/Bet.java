39e3a95f8fd2e54ddc1f5297e9e52f9e3ca15d56
sauter001 <lsj457@naver.com>
2026-03-15 22:22:56 +0900

    fix: 베팅금 상한 검증로직 수정

--- src/main/java/domain/participant/Bet.java
+++ src/main/java/domain/participant/Bet.java
@@ -15,7 +15,7 @@ public record Bet(long amount) {
             throw new OutOfRangeException(String.format("베팅 금액은 최소 %d원은 넘어야 합니다.", MIN_BETTING_AMOUNT));
         }
 
-        if (amount == MAX_BETTING_AMOUNT) {
+        if (amount >= MAX_BETTING_AMOUNT) {
             throw new OutOfRangeException(String.format("베팅 금액은 %d원을 넘을 수 없습니다.", MAX_BETTING_AMOUNT));
         }
     }
