c2152ac01485a135332fb7f5492bfa86939ba103
K-iise <kimyungi1004@naver.com>
2026-03-15 17:11:43 +0900

    refactor: 딜러 수익계산 Stream 변경

--- src/main/java/domain/result/GameProfit.java
+++ src/main/java/domain/result/GameProfit.java
@@ -6,7 +6,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class GameProfit {
-    private final Map<String, Integer> profitMoney = new LinkedHashMap<>();
+    private final Map<String, Long> profitMoney = new LinkedHashMap<>();
 
     public GameProfit(Map<Player, GameResult> gameResultMap) {
         initializeMap(gameResultMap);
@@ -18,21 +18,19 @@ public class GameProfit {
         }
     }
 
-    public void addProfitMoney(Player player, GameResult gameResult) {
-        int battingResult = player.calculateBattingProfit(gameResult.getProfit());
+    private void addProfitMoney(Player player, GameResult gameResult) {
+        long battingResult = player.calculateBattingProfit(gameResult.getProfit());
         profitMoney.put(player.getName(), battingResult);
     }
 
-    public Map<String, Integer> getPlayerProfit() {
+    public Map<String, Long> getPlayerProfit() {
         return Collections.unmodifiableMap(profitMoney);
     }
 
-    public int getDealerProfit() {
-        int dealerProfit = 0;
-        for (Map.Entry<String, Integer> player : profitMoney.entrySet()) {
-            dealerProfit += player.getValue();
-        }
-        return -dealerProfit;
+    public long getDealerProfit() {
+        return -profitMoney.values().stream()
+                .mapToLong(Long::longValue)
+                .sum();
     }
 
 
