e57d428c964412570b5f44aec21f5117388c1049
poketopa <lhs5427ll@gmail.com>
2026-03-15 13:12:54 +0900

    feat(domain): State 클래스 추가

--- src/main/java/domain/State.java
+++ src/main/java/domain/State.java
@@ -1,4 +1,51 @@
 package domain;
 
-public enum Stat {
+public enum State {
+    NORMAL {
+        @Override
+        Outcome against(State dealerState, int playerScore, int dealerScore) {
+            if (playerScore > dealerScore) {
+                return Outcome.WIN;
+            }
+
+            if (playerScore < dealerScore) {
+                return Outcome.LOSE;
+            }
+
+            return Outcome.DRAW;
+        }
+    },
+    BLACKJACK {
+        @Override
+        Outcome against(State dealerState, int playerScore, int dealerScore) {
+            if (dealerState == BLACKJACK) {
+                return Outcome.DRAW;
+            }
+
+            return Outcome.WIN;
+        }
+    },
+    BUST {
+        @Override
+        Outcome against(State dealerState, int playerScore, int dealerScore) {
+            return Outcome.LOSE;
+        }
+    };
+
+    abstract Outcome against(State state, int playerScore, int dealerScore);
+
+    public static State getState(int score, int cardCount) {
+        if (score == 21) {
+            if (cardCount == 2) {
+                return State.BLACKJACK;
+            }
+            return State.NORMAL;
+        }
+
+        if (score < 21) {
+            return NORMAL;
+        }
+
+        return BUST;
+    }
 }
