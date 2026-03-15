ab1ee9619114f9fc8057e4b3fe10df86beadbf88
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-15 12:59:58 +0900

    refactor(Main): Player와 Dealer 구분

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 import domain.BlackjackGame;
 import domain.member.BettingAmount;
 import domain.member.Member;
-import domain.member.Members;
+import domain.member.Player;
+import domain.member.Players;
 import domain.member.Name;
-import domain.member.PlayerRole;
 import java.util.ArrayList;
 import java.util.List;
 import presentation.dto.GameResult;
@@ -28,15 +28,15 @@ public class Main {
 
     private static BlackjackGame init(InputView inputView) {
         List<Name> playerNames = inputView.readPlayerNames();
-        Members members = new Members(initPlayer(inputView, playerNames));
+        Players members = new Players(initPlayer(inputView, playerNames));
         return new BlackjackGame(members);
     }
 
-    private static List<Member> initPlayer(InputView inputView, List<Name> playerNames) {
-        List<Member> players = new ArrayList<>();
+    private static List<Player> initPlayer(InputView inputView, List<Name> playerNames) {
+        List<Player> players = new ArrayList<>();
         for (Name playerName : playerNames) {
             BettingAmount amount = inputView.readBettingAmount(playerName.getValue());
-            players.add(new Member(playerName, new PlayerRole(amount)));
+            players.add(new Player(new Member(playerName), amount));
         }
         return players;
     }
@@ -46,11 +46,11 @@ public class Main {
         outputView.printInitialStatus(HitInfo.firstCardFrom(game.getDealer()), HitInfo.firstCardFrom(game.getPlayers()));
         game.applyBlackjackBonus();
         game.getPlayers().stream()
-                .filter(member -> !member.hasBlackjack())
-                .forEach(member -> askToDraw(member, inputView, outputView, game));
+                .filter(player -> !player.hasBlackjack())
+                .forEach(player -> askToDraw(player, inputView, outputView, game));
     }
 
-    private static void askToDraw(Member player, InputView inputView, OutputView outputView, BlackjackGame game) {
+    private static void askToDraw(Player player, InputView inputView, OutputView outputView, BlackjackGame game) {
         while (!player.hasBust() && inputView.playContinue(player.getName())) {
             game.drawPlayer(player);
             outputView.printHandCard(HitInfo.from(player));
@@ -58,7 +58,7 @@ public class Main {
         printBustOrStay(player, outputView);
     }
 
-    private static void printBustOrStay(Member player, OutputView outputView) {
+    private static void printBustOrStay(Player player, OutputView outputView) {
         if (player.hasBust()) {
             outputView.printBustMessage(player.getName());
             return;
