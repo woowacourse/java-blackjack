2fcc54ec5ee3ffe128330c0684b4fb85794a935b
sauter001 <lsj457@naver.com>
2026-03-15 13:36:47 +0900

    refactor: MatchCase enum이 MatchCondition에 승패 판정을 대응하도록 변경

--- src/main/java/domain/result/MatchCase.java
+++ src/main/java/domain/result/MatchCase.java
@@ -1,21 +1,29 @@
 package domain.result;
 
+import domain.participant.Dealer;
+import domain.participant.Player;
+import utils.MatchCondition;
+
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 
 public enum MatchCase {
-    BLACKJACK_WIN("승", 1.5, BlackjackResult::increaseDealerLoseCount),
-    WIN("승", 1, BlackjackResult::increaseDealerLoseCount),
-    LOSE("패", -1, BlackjackResult::increaseDealerWinCount),
-    DRAW("무", 0, BlackjackResult::increaseDrawCount);
+    LOSE("패", -1, BlackjackResult::increaseDealerWinCount, MatchCondition::isPlayerLose),
+    DRAW("무", 0, BlackjackResult::increaseDrawCount, MatchCondition::isPlayerDraw),
+    BLACKJACK_WIN("승", 1.5, BlackjackResult::increaseDealerLoseCount, MatchCondition::isPlayerBlackjackWin),
+    WIN("승", 1, BlackjackResult::increaseDealerLoseCount, (dealer, player) -> true);
 
     private final String korDisplayName;
     private final double benefitRate;
     private final Consumer<BlackjackResult> counter;
+    private final BiPredicate<Dealer, Player> judgeCriteria;
 
-    MatchCase(String korDisplayName, double benefitRate, Consumer<BlackjackResult> counter) {
+
+    MatchCase(String korDisplayName, double benefitRate, Consumer<BlackjackResult> counter, BiPredicate<Dealer, Player> judgeCriteria) {
         this.korDisplayName = korDisplayName;
         this.benefitRate = benefitRate;
         this.counter = counter;
+        this.judgeCriteria = judgeCriteria;
     }
 
     public String getKorDisplayName() {
@@ -28,5 +36,9 @@ public enum MatchCase {
 
     public void increaseMatchCountOf(BlackjackResult blackjackResult) {
         counter.accept(blackjackResult);
+    }
+
+    public boolean isMatch(Dealer dealer, Player player) {
+        return judgeCriteria.test(dealer, player);
     }
 }
--- src/main/java/domain/result/MatchJudge.java
+++ src/main/java/domain/result/MatchJudge.java
@@ -3,18 +3,7 @@ package domain.result;
 import domain.participant.Dealer;
 import domain.participant.Player;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.function.BiPredicate;
-
 public class MatchJudge {
-    private static final Map<MatchCase, BiPredicate<Dealer, Player>> JUDGE_CRITERIA =
-            new LinkedHashMap<>(Map.of(
-                    MatchCase.LOSE, MatchJudge::isPlayerLose,
-                    MatchCase.DRAW, MatchJudge::isPlayerScoreEqualsDealer,
-                    MatchCase.BLACKJACK_WIN, MatchJudge::isPlayerWinWithBlackjack
-            ));
-
     private final Dealer dealer;
     private final Player player;
 
@@ -23,23 +12,12 @@ public class MatchJudge {
         this.player = player;
     }
 
-    private static boolean isPlayerWinWithBlackjack(Dealer dealer, Player player) {
-        return !dealer.isBlackjack() && !isPlayerLose(dealer, player) && player.isBlackjack();
-    }
-
-    private static boolean isPlayerLose(Dealer dealer, Player player) {
-        return player.isBust() || (!dealer.isBust() && player.getFinalScore() < dealer.getFinalScore());
-    }
-
-    private static boolean isPlayerScoreEqualsDealer(Dealer dealer, Player player) {
-        return !(player.isBust() || dealer.isBust()) && (player.getFinalScore() == dealer.getFinalScore());
-    }
-
     public MatchCase judge() {
-        return JUDGE_CRITERIA.entrySet().stream()
-                .filter(entry -> entry.getValue().test(this.dealer, this.player))
-                .map(Map.Entry::getKey)
-                .findFirst()
-                .orElse(MatchCase.WIN);
+        for (MatchCase matchCase : MatchCase.values()) {
+            if (matchCase.isMatch(dealer, player)) {
+                return matchCase;
+            }
+        }
+        return MatchCase.WIN;
     }
 }
