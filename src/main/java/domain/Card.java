2f61a2c6c4c583282af60448b4441b5470d76c33
poketopa <lhs5427ll@gmail.com>
2026-03-15 09:08:11 +0900

    refactor(Card): suit / rank NOT NULL 조건 추가

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -1,12 +1,14 @@
 package domain;
 
+import java.util.Objects;
+
 public class Card {
     private final Suit suit;
     private final Rank rank;
 
     public Card(final Suit suit, final Rank rank) {
-        this.suit = suit;
-        this.rank = rank;
+        this.suit = Objects.requireNonNull(suit, "suit must not be null");
+        this.rank = Objects.requireNonNull(rank, "rank must not be null");
     }
 
     public int getScore() {
