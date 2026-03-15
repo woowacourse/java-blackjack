5e1d5331cc59113482161689f4fd50d28f5ac1d5
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-03-15 10:02:57 +0900

    test: assertJ의 기능을 활용하여 테스트 수정

--- src/test/java/bet/BetTableTest.java
+++ src/test/java/bet/BetTableTest.java
@@ -4,6 +4,7 @@ import domain.bet.BetTable;
 import domain.participant.Hand;
 import domain.participant.Player;
 import domain.participant.PlayerName;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,14 +13,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BetTableTest {
     @Test
     @DisplayName("플레이어는 베팅 금액을 설정할 수 있다.")
-    void recodeAmount_setsBetAmount() {
-        Player player = new Player(new PlayerName("user1"), new Hand());
+    void recordAmount_setsBetAmount() {
         BetTable betTable = new BetTable();
         int amount = 10000;
 
-        betTable.recodeAmount(player.getName(), amount);
-        int betAmount = betTable.findBetAmount(player);
-        assertThat(betAmount).isEqualTo(amount);
+        betTable.recordAmount("user1", amount);
+
+        assertThat(betTable)
+                .extracting("bettingTable", InstanceOfAssertFactories.MAP)
+                .containsEntry("user1", amount);
     }
 
     @Test
@@ -28,7 +30,7 @@ class BetTableTest {
         Player player = new Player(new PlayerName("user1"), new Hand());
         BetTable betTable = new BetTable();
         int amount = 10000;
-        betTable.recodeAmount(player.getName(), amount);
+        betTable.recordAmount(player.getName(), amount);
 
         int betAmount = betTable.findBetAmount(player);
         assertThat(betAmount).isEqualTo(amount);
