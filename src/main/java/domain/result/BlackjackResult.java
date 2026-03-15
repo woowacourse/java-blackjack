105d5b4b340da44b80393e49f7cd3716b5a6db3b
sauter001 <lsj457@naver.com>
2026-03-15 21:49:37 +0900

    refactor: 사용되지 않는 `DealerMatchCount` 제거

--- src/main/java/domain/result/BlackjackResult.java
+++ src/main/java/domain/result/BlackjackResult.java
@@ -10,8 +10,6 @@ import java.util.Map;
 
 public class BlackjackResult {
     private final Map<Player, MatchCase> playerResultMap = new LinkedHashMap<>();
-    private final DealerMatchCount dealerMatchCount = new DealerMatchCount();
-
     private BlackjackResult(Dealer dealer, Players players) {
         calculateMatchResult(dealer, players);
     }
@@ -26,23 +24,10 @@ public class BlackjackResult {
         MatchJudge matchJudge = new MatchJudge(dealer, player);
         MatchCase matchCase = matchJudge.judge();
         playerResultMap.put(player, matchCase);
-        matchCase.increaseMatchCountOf(this);
     }
 
     public static BlackjackResult from(Dealer dealer, Players players) {
         return new BlackjackResult(dealer, players);
-    }
-
-    public void increaseDealerWinCount() {
-        this.dealerMatchCount.increaseWinCount();
-    }
-
-    public void increaseDrawCount() {
-        this.dealerMatchCount.increaseDrawCount();
-    }
-
-    public void increaseDealerLoseCount() {
-        this.dealerMatchCount.increaseLoseCount();
     }
 
     public Map<Player, MatchCase> getPlayerResultMap() {
--- src/main/java/domain/result/MatchCase.java
+++ src/main/java/domain/result/MatchCase.java
@@ -5,24 +5,21 @@ import domain.participant.Player;
 import utils.MatchCondition;
 
 import java.util.function.BiPredicate;
-import java.util.function.Consumer;
 
 public enum MatchCase {
-    LOSE("패", -1, BlackjackResult::increaseDealerWinCount, MatchCondition::isPlayerLose),
-    DRAW("무", 0, BlackjackResult::increaseDrawCount, MatchCondition::isPlayerDraw),
-    BLACKJACK_WIN("승", 1.5, BlackjackResult::increaseDealerLoseCount, MatchCondition::isPlayerBlackjackWin),
-    WIN("승", 1, BlackjackResult::increaseDealerLoseCount, (dealer, player) -> true);
+    LOSE("패", -1, MatchCondition::isPlayerLose),
+    DRAW("무", 0, MatchCondition::isPlayerDraw),
+    BLACKJACK_WIN("승", 1.5, MatchCondition::isPlayerBlackjackWin),
+    WIN("승", 1, (dealer, player) -> true);
 
     private final String korDisplayName;
     private final double benefitRate;
-    private final Consumer<BlackjackResult> counter;
     private final BiPredicate<Dealer, Player> judgeCriteria;
 
 
-    MatchCase(String korDisplayName, double benefitRate, Consumer<BlackjackResult> counter, BiPredicate<Dealer, Player> judgeCriteria) {
+    MatchCase(String korDisplayName, double benefitRate, BiPredicate<Dealer, Player> judgeCriteria) {
         this.korDisplayName = korDisplayName;
         this.benefitRate = benefitRate;
-        this.counter = counter;
         this.judgeCriteria = judgeCriteria;
     }
 
@@ -32,10 +29,6 @@ public enum MatchCase {
 
     public double getBenefitRate() {
         return benefitRate;
-    }
-
-    public void increaseMatchCountOf(BlackjackResult blackjackResult) {
-        counter.accept(blackjackResult);
     }
 
     public boolean isMatch(Dealer dealer, Player player) {
