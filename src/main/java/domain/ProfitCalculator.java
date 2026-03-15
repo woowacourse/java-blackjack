545e9edd48c61ed06215a49fac7955e948f8d3e5
심호경 <ghrudtla@naver.com>
2026-03-15 11:16:18 +0900

    feat: 수익 계산을 담당하는 ProfitCalculator 클래스 작성

---
+++ src/main/java/domain/ProfitCalculator.java
@@ -0,0 +1,47 @@
+package domain;
+
+public class ProfitCalculator {
+    private static final double BLACKJACK_PROFIT_RATE = 1.5;
+
+    public static double calculatePlayerProfit(Player player, Dealer dealer) {
+        int bettingMoney = player.getBettingMoneyValue();
+        int playerScore = player.getScoreValue();
+        int dealerScore = dealer.getScoreValue();
+
+        if (player.isBlackjack() && dealer.isBlackjack()) {
+            return 0;
+        }
+        if (player.isBlackjack()) {
+            return bettingMoney * BLACKJACK_PROFIT_RATE;
+        }
+        if (dealer.isBlackjack()) {
+            return -bettingMoney;
+        }
+        if (player.isBust()) {
+            return -bettingMoney;
+        }
+        if (dealer.isBust()) {
+            return bettingMoney;
+        }
+        if (playerScore > dealerScore) {
+            return bettingMoney;
+        }
+        if (playerScore < dealerScore) {
+            return -bettingMoney;
+        }
+        return 0;
+    }
+
+    public static double calculateDealerProfit(Players players, Dealer dealer) {
+        return -players.getPlayers().stream()
+                .mapToDouble(player -> calculatePlayerProfit(player, dealer))
+                .sum();
+    }
+
+    public static String formatProfit(double profit) {
+        if (profit == (long) profit) {
+            return String.valueOf((long) profit);
+        }
+        return String.valueOf(profit);
+    }
+}
