bb11f00d7f12c885f04c6c1b67588e97272e2b8b
Chocoding1 <galmeagi2@naver.com>
2026-03-15 13:08:10 +0900

    refactor: 에러 메시지 내용 수정

--- src/main/java/blackjack/model/bet/BetAmount.java
+++ src/main/java/blackjack/model/bet/BetAmount.java
@@ -4,7 +4,7 @@ public class BetAmount {
 
     private static final String ERROR_EMPTY_INPUT = "입력값은 공백일 수 없습니다.";
     private static final String ERROR_BET_AMOUNT_NOT_INTEGER = "배팅 금액은 숫자 형태로 입력해야 합니다.";
-    private static final String ERROR_BET_AMOUNT_NOT_POSITIVE = "배팅 금액은 0 이상이어야 합니다.";
+    private static final String ERROR_BET_AMOUNT_NOT_POSITIVE = "배팅 금액은 1원 이상이어야 합니다.";
 
     private final int amount;
 
