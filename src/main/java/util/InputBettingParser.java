89341d4ec8915fe1800ba03c7c54b7135f7ebbb2
JunHyung1206 <pjh120610@naver.com>
2026-03-15 17:30:47 +0900

    fix: InputNumberParser 수정
    - trim을 통해서 앞뒤의 공백을 제거
    - 숫자가 아닌 다른 값이 나왔을 때, 숫자만 입력해달라고 메시지 변경
    - 각 값에 대해서 테스트 추가

--- src/main/java/util/InputBettingParser.java
+++ src/main/java/util/InputBettingParser.java
@@ -8,9 +8,12 @@ public class InputBettingParser {
 
     public static Money parseBettingMoney(String inputBettingMoney) {
         try {
-            long bettingMoney = Long.parseLong(inputBettingMoney);
+            long bettingMoney = Long.parseLong(inputBettingMoney.trim());
             return new Money(bettingMoney);
-        } catch (IllegalArgumentException exception) {
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("숫자로만 입력해야합니다.");
+        }
+        catch (IllegalArgumentException exception) {
             throw new IllegalArgumentException(exception.getMessage());
         }
     }
