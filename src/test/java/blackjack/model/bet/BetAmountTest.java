fdf622c52c247f553b901416e8b0f75c6262ac9b
Chocoding1 <galmeagi2@naver.com>
2026-03-15 14:10:04 +0900

    test: 미사용 변수 및 중복 테스트 클래스 제거

--- src/test/java/blackjack/model/bet/BetAmountTest.java
+++ src/test/java/blackjack/model/bet/BetAmountTest.java
@@ -14,9 +14,6 @@ class BetAmountTest {
     @ValueSource(strings = {"1", "10"})
     @DisplayName("배팅 금액 정상 생성")
     void createBetAmount_success(String input) {
-        // given
-        Player player = new Player("name");
-
         //when & then
         assertDoesNotThrow(() -> new BetAmount(input));
     }
@@ -25,9 +22,6 @@ class BetAmountTest {
     @ValueSource(strings = {"", " "})
     @DisplayName("입력값이 공백일 경우 예외 발생")
     void createBetAmount_fail_when_input_is_empty(String emptyInput) {
-        // given
-        Player player = new Player("name");
-
         //when & then
         Assertions.assertThatThrownBy(() -> new BetAmount(emptyInput))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -37,9 +31,6 @@ class BetAmountTest {
     @ValueSource(strings = {"a", "1a", "123*"})
     @DisplayName("입력값이 숫자 형태가 아닐 경우 예외 발생")
     void createBetAmount_fail_when_input_is_not_number(String stringInput) {
-        // given
-        Player player = new Player("name");
-
         //when & then
         Assertions.assertThatThrownBy(() -> new BetAmount(stringInput))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -49,9 +40,6 @@ class BetAmountTest {
     @ValueSource(strings = {"0", "-1"})
     @DisplayName("입력값이 양수가 아닐 경우 예외 발생")
     void createBetAmount_fail_when_input_is_not_positive(String negativeInput) {
-        // given
-        Player player = new Player("name");
-
         //when & then
         Assertions.assertThatThrownBy(() -> new BetAmount(negativeInput))
                 .isInstanceOf(IllegalArgumentException.class);
