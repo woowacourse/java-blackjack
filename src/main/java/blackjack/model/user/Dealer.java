26b13e7512d997a006c47a9f313132feb9484155
Chocoding1 <galmeagi2@naver.com>
2026-03-15 13:14:34 +0900

    refactor: 미사용 필드, 메서드 제거

--- src/main/java/blackjack/model/user/Dealer.java
+++ src/main/java/blackjack/model/user/Dealer.java
@@ -1,25 +1,11 @@
 package blackjack.model.user;
 
-import blackjack.model.gameresult.GameResult;
-import java.util.EnumMap;
-
 public class Dealer extends User {
 
     private static final int DEALER_ADD_CARD_STAND = 17;
 
-    private final EnumMap<GameResult, Integer> gameResults;
-
     public Dealer() {
         super("딜러");
-        gameResults = new EnumMap<>(GameResult.class);
-    }
-
-    public EnumMap<GameResult, Integer> getGameResults() {
-        return gameResults;
-    }
-
-    public void addResult(GameResult gameResult) {
-        gameResults.merge(gameResult, 1, Integer::sum);
     }
 
     @Override
