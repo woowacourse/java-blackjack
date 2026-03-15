51b6ffdd2d3be8b3ebb98bb36d6bb06d94b081de
K-iise <kimyungi1004@naver.com>
2026-03-15 18:30:26 +0900

    refactor: ShuffleStrategy 반환 데이터 타입 변경

--- src/main/java/domain/card/RandomShuffleStrategy.java
+++ src/main/java/domain/card/RandomShuffleStrategy.java
@@ -1,11 +1,14 @@
 package domain.card;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class RandomShuffleStrategy implements ShuffleStrategy {
     @Override
-    public void shuffle(List<Card> cards) {
-        Collections.shuffle(cards);
+    public List<Card> shuffle(List<Card> cards) {
+        List<Card> shuffledCard = new ArrayList<>(cards);
+        Collections.shuffle(shuffledCard);
+        return Collections.unmodifiableList(shuffledCard);
     }
 }
