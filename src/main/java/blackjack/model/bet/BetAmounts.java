ee03f6c659f1201cff1b0079595b3cc46240a80b
Chocoding1 <galmeagi2@naver.com>
2026-03-15 13:11:29 +0900

    refactor: 방어적 복사 적용

--- src/main/java/blackjack/model/bet/BetAmounts.java
+++ src/main/java/blackjack/model/bet/BetAmounts.java
@@ -9,7 +9,7 @@ public class BetAmounts {
     private final Map<Player, BetAmount> betAmounts;
 
     public BetAmounts(Map<Player, BetAmount> betAmounts) {
-        this.betAmounts = betAmounts;
+        this.betAmounts = Map.copyOf(betAmounts);
     }
 
     public int calculateProfit(Player player, GameResult gameResult) {
