69cb0abaa0cbcf10a1849a673f160b9186d0ca33
Huiyeongkim <huiyeong9619@naver.com>
2026-03-16 11:15:14 +0900

    refactor: calculateBlackJackEarnings 메소드 추출

--- src/main/java/domain/money/BettingResult.java
+++ src/main/java/domain/money/BettingResult.java
@@ -14,7 +14,7 @@ public class BettingResult {
 
     public static BettingResult from(int betAmount, MatchResult matchResult, boolean isBlackJack) {
         if (matchResult == MatchResult.WIN && isBlackJack) {
-            return new BettingResult((int) (betAmount * BLACKJACK_MULTIPLIER));
+            return new BettingResult(calculateBlackJackEarnings(betAmount));
         }
         if (matchResult == MatchResult.WIN) {
             return new BettingResult(betAmount);
@@ -31,5 +31,9 @@ public class BettingResult {
 
     public int reverse() {
         return -earnings;
+    }
+
+    private static int calculateBlackJackEarnings(int betAmount) {
+        return (int) (betAmount * BLACKJACK_MULTIPLIER);
     }
 }
