7ce8aeec86cf01d47451c818d8d7771266bb4432
Jihyun3478 <leejihyun3478@gmail.com>
2026-03-16 13:04:07 +0900

    feat: 베팅 금액이 10원 단위가 아니면, 예외가 발생한다.

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -1,16 +1,24 @@
 package domain;
 
 import static exception.ErrorMessage.MONEY_INVALID_RANGE;
+import static exception.ErrorMessage.MONEY_INVALID_UNIT;
 
 public class Money {
     private static final int MINIMUM_BET_AMOUNT = 1_000;
     private static final int MAXIMUM_BET_AMOUNT = 300_000;
+    private static final int MONEY_DIVIDE_UNIT = 10;
+    private static final double MONEY_MULTIPLY_PERCENT = 1.5;
 
     private final int betAmount;
 
     public Money(int betAmount) {
-        validateRange(betAmount);
+        validate(betAmount);
         this.betAmount = betAmount;
+    }
+
+    private void validate(int betAmount) {
+        validateRange(betAmount);
+        validateUnit(betAmount);
     }
 
     private void validateRange(int betAmount) {
@@ -19,8 +27,14 @@ public class Money {
         }
     }
 
+    private void validateUnit(int betAmount) {
+        if (betAmount % MONEY_DIVIDE_UNIT != 0) {
+            throw new IllegalArgumentException(MONEY_INVALID_UNIT.getMessage());
+        }
+    }
+
     public int multiply() {
-        return (int) (betAmount * 1.5);
+        return (int) (betAmount * MONEY_MULTIPLY_PERCENT);
     }
 
     public int add() {
--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -5,8 +5,9 @@ public enum ErrorMessage {
     PLAYER_NAME_IS_DUPLICATE("플레이어의 이름은 중복되지 않아야 합니다."),
     PLAYERS_INVALID_COUNT("플레이어의 수는 1명 이상 8명 이하여야 합니다."),
 
-    MONEY_INVALID_FORMAT("배팅 금액은 숫자만 가능합니다."),
-    MONEY_INVALID_RANGE("배팅 금액은 1,000원 이상 300,000원 이하로 입력해주세요."),
+    MONEY_INVALID_FORMAT("베팅 금액은 숫자만 가능합니다."),
+    MONEY_INVALID_RANGE("베팅 금액은 1,000원 이상 300,000원 이하로 입력해주세요."),
+    MONEY_INVALID_UNIT("베팅 금액은 10원 단위이어야 합니다."),
 
     CARD_SUIT_NOT_EXIST("존재하지 않는 카드 문양입니다."),
     CARD_VALUE_NOT_EXIST("존재하지 않는 카드 점수입니다.")
