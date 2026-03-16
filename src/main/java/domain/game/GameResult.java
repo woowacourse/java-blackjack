5f4a64c5c3b92e3300d6fca796a6ad837e191a64
2Jaeheon <jaeheon0826@jbnu.ac.kr>
2026-03-16 09:33:10 +0900

    refactor: 게임결과 로직 단순화
    Signed-off-by: 2Jaeheon <jaeheon0826@jbnu.ac.kr>

--- src/main/java/domain/game/GameResult.java
+++ src/main/java/domain/game/GameResult.java
@@ -6,28 +6,20 @@ import domain.participant.Players;
 import java.util.List;
 
 public record GameResult(int dealerProfit, List<PlayerGameResult> playerResults) {
+
     public static GameResult from(Players players, Dealer dealer) {
-        return new GameResult(createPlayerResults(players, dealer));
-    }
-
-    private GameResult(List<PlayerGameResult> playerResults) {
-        this(calculateDealerProfit(playerResults), playerResults);
-    }
-
-    private static List<PlayerGameResult> createPlayerResults(Players players, Dealer dealer) {
-        return players.getPlayers().stream()
+        List<PlayerGameResult> playerResults = players.getPlayers().stream()
                 .map(player -> createPlayerResult(player, dealer))
                 .toList();
+
+        int dealerProfit = -1 * calculatePlayersProfit(playerResults);
+
+        return new GameResult(dealerProfit, playerResults);
     }
 
     private static PlayerGameResult createPlayerResult(Player player, Dealer dealer) {
         WinningStatus status = WinningStatus.of(player, dealer);
         return new PlayerGameResult(player.name(), player.profit(status));
-    }
-
-    private static int calculateDealerProfit(List<PlayerGameResult> playerResults) {
-        return -1 * calculatePlayersProfit(playerResults);
-
     }
 
     private static int calculatePlayersProfit(List<PlayerGameResult> playerResults) {
