ef7a634c8c34b66ecdcd657d564077a94c6634da
김가현 <hyeonnni@naver.com>
2026-03-16 21:51:36 +0900

    refactor(util): 플레이어 중복 이름 검증 로직 추가

--- src/main/java/util/NameParser.java
+++ src/main/java/util/NameParser.java
@@ -1,13 +1,23 @@
 package util;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 public class NameParser {
+    private NameParser() {
+
+    }
+
     public static List<String> makeNameList(String input) {
-        return Arrays.stream(input.split(","))
+        List<String> names = Arrays.stream(input.split(","))
                 .map(String::trim)
-                .collect(Collectors.toList());
+                .toList();
+        Set<String> uniqueNames = new HashSet<>(names);
+        if (names.size() != uniqueNames.size()) {
+            throw new IllegalArgumentException("중복된 이름은 허용하지 않습니다.");
+        }
+        return names;
     }
 }
