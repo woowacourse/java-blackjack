d8503c30cd4f32e1a3137dffff5082fb25de44a6
ChoSeongHyeon <00seong116@naver.com>
2026-03-16 23:26:15 +0900

    refactor(Dealer): 딜러 초기 cost 매직넘버 수정

--- src/main/java/domain/player/Dealer.java
+++ src/main/java/domain/player/Dealer.java
@@ -5,10 +5,11 @@ import domain.vo.Name;
 
 public class Dealer extends Participant {
     private static final int DEALER_STAY_THRESHOLD = 17;
+    private static final int DEALER_INIT_COST = 0;
 
     public Dealer() {
         super(new Name("딜러"));
-        setCost(new Cost(0));
+        setCost(new Cost(DEALER_INIT_COST));
     }
 
     public boolean isHit() {
