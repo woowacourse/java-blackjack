cf98c02404eb5b865736302aa872bbf34c090004
김가현 <hyeonnni@naver.com>
2026-03-16 21:51:06 +0900

    refactor(domain): 수익 계산 메서드 추가

--- src/main/java/domain/participant/Player.java
+++ src/main/java/domain/participant/Player.java
@@ -1,5 +1,7 @@
 package domain.participant;
 
+import domain.result.Outcome;
+
 public class Player extends Participant {
     private final Money money;
 
@@ -8,12 +10,12 @@ public class Player extends Participant {
         this.money = money;
     }
 
-    public Money getMoney() {
-        return money;
-    }
-
     @Override
     public boolean canDraw() {
         return !getScore().isBust() && !getScore().isBlackjack();
+    }
+
+    public Money calculateProfit(Outcome outcome) {
+        return this.money.multiply(outcome.getRate());
     }
 }
