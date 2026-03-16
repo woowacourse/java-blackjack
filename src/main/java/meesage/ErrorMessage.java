8ef98eca667b5b9e7d896b07a12358903ceda294
최용준 <yj9107vv@gmail.com>
2026-03-16 09:56:00 +0900

    feat(ErrorMessage): 숫자 입력 오류 메시지 추가

--- src/main/java/meesage/ErrorMessage.java
+++ src/main/java/meesage/ErrorMessage.java
@@ -2,7 +2,9 @@ package meesage;
 
 public enum ErrorMessage {
 
-    EMPTY_INPUT("[ERROR] 사용자 이름은 공백을 입력할 수 없습니다.");
+    EMPTY_INPUT("[ERROR] 사용자 이름은 공백을 입력할 수 없습니다."),
+    NOT_STRICT_NUMERIC("[ERROR] 입력값은 숫자여야 합니다.(0, 음수, 공백 X)"),
+    ;
 
     private final String message;
 
