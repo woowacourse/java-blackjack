ef4018cb7f5f951a7ab495892dbb116437fe15a2
김가현 <hyeonnni@naver.com>
2026-03-16 22:10:19 +0900

    refactor(dto): from() 메서드 추가

--- src/main/java/dto/PlayerOutcomeDto.java
+++ src/main/java/dto/PlayerOutcomeDto.java
@@ -4,7 +4,7 @@ import domain.participant.Money;
 import domain.participant.Player;
 
 public record PlayerOutcomeDto(String name, String profit) {
-    public static PlayerOutcomeDto of(Player player, Money profit) {
+    public static PlayerOutcomeDto from(Player player, Money profit) {
         return new PlayerOutcomeDto(
                 player.getName(),
                 String.valueOf(profit.getAmount())
