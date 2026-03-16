f0aa6963193d557c6c5345d52e48d4f68304dfa3
이동훈 <higgs95@naver.com>
2026-03-16 15:09:07 +0900

    refactor: CardInfoDto 불필요한 팩토리 메서드 삭제

--- src/main/java/dto/CardInfoDto.java
+++ src/main/java/dto/CardInfoDto.java
@@ -1,8 +1,4 @@
 package dto;
 
 public record CardInfoDto(String numberDisplayName, String shape) {
-
-    public static CardInfoDto from(String numberDisplayName, String shape) {
-        return new CardInfoDto(numberDisplayName, shape);
-    }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -23,7 +23,7 @@ public class OutputView {
     }
 
     public static void printDealerMessage() {
-        System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.\n");
+        System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.%n");
     }
 
     public static void printGameResult(List<ParticipantGameResultDto> participantGameResultDtos) {
