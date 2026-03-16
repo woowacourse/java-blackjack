be1725c802a5a10666d0e89a123a371b03cee182
김가현 <hyeonnni@naver.com>
2026-03-16 22:10:06 +0900

    refactor(dto): from() 메서드 추가

--- src/main/java/dto/InitialDto.java
+++ src/main/java/dto/InitialDto.java
@@ -2,9 +2,8 @@ package dto;
 
 import java.util.List;
 
-public record InitialDto(
-        String joinedNames,
-        ParticipantDto dealer,
-        List<ParticipantDto> players
-) {
+public record InitialDto(String joinedNames, String firstCardName, List<ParticipantDto> players) {
+    public static InitialDto from(String joinedNames, String firstCardName, List<ParticipantDto> players) {
+        return new InitialDto(joinedNames, firstCardName, players);
+    }
 }
