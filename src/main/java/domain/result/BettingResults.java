250aa70aedd6b7bff47557b2e39157719e155739
kkb <gimgyubin1225@gmail.com>
2026-03-16 15:43:38 +0900

    chore: 컨벤션 적용을 위해 공백 추가

--- src/main/java/domain/result/BettingResults.java
+++ src/main/java/domain/result/BettingResults.java
@@ -2,7 +2,7 @@ package domain.result;
 
 import java.util.List;
 
-public record BettingResults (
+public record BettingResults(
         BettingResult dealerBettingResult,
         List<BettingResult> playerBettingResults
 ) {
