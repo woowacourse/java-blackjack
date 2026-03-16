92b5b12064c5f38beb744df1db90134fefd860d9
Huiyeongkim <huiyeong9619@naver.com>
2026-03-16 16:07:14 +0900

    refactor: Player 생성 검증 로직 Validator로 이동

--- src/main/java/domain/participant/Player.java
+++ src/main/java/domain/participant/Player.java
@@ -1,21 +1,16 @@
 package domain.participant;
 
 import domain.money.Money;
-
-import java.util.regex.Pattern;
-
-import static util.BlackJackConstant.MAX_NAME_LENGTH;
+import util.Validator;
 
 public class Player extends Participant {
-
-    private static final String STRING_REGEX = "^[a-zA-Z]*$";
 
     private final String name;
     private final Money money;
 
     public Player(String name, Integer money) {
-        validateNameLength(name);
-        validateOnlyEnglish(name);
+        Validator.validateNameLength(name);
+        Validator.validateNameEng(name);
         this.name = name;
         this.money = new Money(money);
     }
@@ -26,17 +21,5 @@ public class Player extends Participant {
 
     public Money getMoney() {
         return money;
-    }
-
-    private void validateNameLength(String name) {
-        if (name.isEmpty() || name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("플레이어 이름은 1글자 이상 8글자 이하여야 합니다.");
-        }
-    }
-
-    private void validateOnlyEnglish(String name) {
-        if (!Pattern.matches(STRING_REGEX, name.trim())) {
-            throw new IllegalArgumentException("플레이어 이름은 영문자만 포함되어야 합니다.");
-        }
     }
 }
