c386db6ea9b11d50b2ba99fc030dc18c7099cdad
toctoce <toctoce@naver.com>
2026-03-16 16:05:20 +0900

    fix(OutputView): static이 아니도록 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,17 +9,17 @@ import dto.ScoreResultDto;
 
 public class OutputView {
     // todo : OS 종속적이지 않은 줄바꿈 문자 사용
-    public static final String HEADER_FORMAT = "딜러와 %s에게 2장을 나누었습니다.\n";
-    public static final String DEALER_STATUS_FORMAT = "딜러카드: %s\n";
-    public static final String PLAYER_STATUS_FORMAT = "%s카드: %s\n";
-    public static final String DEALER_COUNT_FORMAT = "딜러: %d\n";
-    public static final String PLAYER_RESULT_FORMAT = "%s: %d\n";
-    public static final String DEALER_NAME = "딜러";
-    public static final String SCORE_RESULT_FORMAT = "%s카드: %s - 결과: %s\n";
-    private static final String DEALER_HIT_MESSAGE = "\n딜러는 16이하라 한장의 카드를 더 받았습니다.";
-    private static final String FINAL_RESULT_HEADER = "## 최종 수익";
+    private final String HEADER_FORMAT = "딜러와 %s에게 2장을 나누었습니다.\n";
+    private final String DEALER_STATUS_FORMAT = "딜러카드: %s\n";
+    private final String PLAYER_STATUS_FORMAT = "%s카드: %s\n";
+    private final String DEALER_COUNT_FORMAT = "딜러: %d\n";
+    private final String PLAYER_RESULT_FORMAT = "%s: %d\n";
+    private final String DEALER_NAME = "딜러";
+    private final String SCORE_RESULT_FORMAT = "%s카드: %s - 결과: %s\n";
+    private final String DEALER_HIT_MESSAGE = "\n딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    private final String FINAL_RESULT_HEADER = "## 최종 수익";
 
-    public static void printInitMessage(InitStatusDto initStatusDto) {
+    public void printInitMessage(InitStatusDto initStatusDto) {
         System.out.println();
         System.out.printf(HEADER_FORMAT, String.join(", ", initStatusDto.names()));
         System.out.printf(DEALER_STATUS_FORMAT, getHandString(initStatusDto.dealerHandDto()));
@@ -30,15 +30,15 @@ public class OutputView {
         System.out.println();
     }
 
-    public static void printHandOutput(String name, HandDto handDto) {
+    public void printHandOutput(String name, HandDto handDto) {
         System.out.printf(PLAYER_STATUS_FORMAT, name, getHandString(handDto));
     }
 
-    public static void printDealerHitMessage() {
+    public void printDealerHitMessage() {
         System.out.println(DEALER_HIT_MESSAGE);
     }
 
-    public static void printScoreResult(ScoreResultDto scoreResultDto) {
+    public void printScoreResult(ScoreResultDto scoreResultDto) {
         System.out.println();
         System.out.printf(SCORE_RESULT_FORMAT, DEALER_NAME, getHandString(scoreResultDto.dealerHandDto()),
                 scoreResultDto.dealerScore());
@@ -51,7 +51,7 @@ public class OutputView {
         System.out.println();
     }
 
-    public static void printFinalResult(FinalResultDto finalResultDto) {
+    public void printFinalResult(FinalResultDto finalResultDto) {
         System.out.println(FINAL_RESULT_HEADER);
         System.out.printf(DEALER_COUNT_FORMAT, finalResultDto.dealerProfit());
 
@@ -59,7 +59,7 @@ public class OutputView {
                 System.out.printf(PLAYER_RESULT_FORMAT, dto.name(), dto.profit()));
     }
 
-    private static String getHandString(HandDto handDto) {
+    private String getHandString(HandDto handDto) {
         return String.join(", ", handDto.cards());
     }
 }
