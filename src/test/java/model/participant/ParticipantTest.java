27a0cdc9f96b290784cd958016b8408fddeee2af
armd482 <armd482@naver.com>
2026-03-16 14:52:01 +0900

    test: Participant 테스트 이름 검증 외 나머지 제거

--- src/test/java/model/participant/ParticipantTest.java
+++ src/test/java/model/participant/ParticipantTest.java
@@ -1,10 +1,6 @@
 package model.participant;
 
-import dto.result.ParticipantCurrentHand;
-import java.util.List;
-import model.card.Card;
-import model.card.CardNumber;
-import model.card.Shape;
+import dto.status.PlayerName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -12,25 +8,9 @@ import static org.assertj.core.api.Assertions.*;
 public class ParticipantTest {
 
     @Test
-    public void 결과_출력_정상_작동() {
-        Participant participant = new Participant("jason");
+    public void 이름_출력_정상_작동() {
+        Participant participant = new Participant(new PlayerName("jason"));
 
-        ParticipantCurrentHand result = participant.getCurrentHand();
-
-        assertThat(result.name()).isEqualTo("jason");
-        assertThat(result.deck().isEmpty()).isTrue();
-        assertThat(result.score()).isEqualTo(0);
-    }
-
-    @Test
-    public void 카드_받기_정상_작동() {
-        Card card = new Card(Shape.CLOVER, CardNumber.EIGHT);
-        Participant participant = new Participant("jason");
-        participant.addCard(card);
-
-        List<String> deck = participant.getCurrentHand().deck();
-
-        assertThat(deck).contains(card.getString());
-        assertThat(deck.size()).isEqualTo(1);
+        assertThat(participant.getName()).isEqualTo("jason");
     }
 }
