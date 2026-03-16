dfd598fe78563dc4f1d3bc9d8438fb559a154e56
Jihyun3478 <leejihyun3478@gmail.com>
2026-03-16 12:51:57 +0900

    refactor: 딜러가 카드를 받기 위한 조건 상수화

--- src/main/java/domain/participant/Dealer.java
+++ src/main/java/domain/participant/Dealer.java
@@ -4,11 +4,13 @@ import domain.card.Card;
 import java.util.List;
 
 public class Dealer extends Participant {
+    public static final int RECEIVE_CARD_CONDITION = 16;
+
     public Dealer(List<Card> hand) {
         super(hand);
     }
 
     public boolean canReceiveCard() {
-        return calculateScore() <= 16;
+        return calculateScore() <= RECEIVE_CARD_CONDITION;
     }
 }
