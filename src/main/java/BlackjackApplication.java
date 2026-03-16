dd752c01a717d68c41a00b856a9c03dded99fbf3
khcho96 <khcho1492@gmail.com>
2026-03-16 12:31:44 +0900

    rename(BlackjackApplication): BlackjackFactory 클래스 이름 변경으로 인한 코드 수정

--- src/main/java/BlackjackApplication.java
+++ src/main/java/BlackjackApplication.java
@@ -1,13 +1,13 @@
 import domain.game.BlackjackGameManager;
-import factory.BlackjackControllerFactory;
+import factory.BlackjackFactory;
 import controller.BlackjackController;
 
 public class BlackjackApplication {
 
     public static void main(String[] args) {
-        BlackjackControllerFactory blackjackControllerFactory = new BlackjackControllerFactory();
-        BlackjackController blackjackController = blackjackControllerFactory.blackjackController();
-        BlackjackGameManager blackjackGameManager = blackjackControllerFactory.blackjackGameManager();
+        BlackjackFactory blackjackFactory = new BlackjackFactory();
+        BlackjackController blackjackController = blackjackFactory.blackjackController();
+        BlackjackGameManager blackjackGameManager = blackjackFactory.blackjackGameManager();
         blackjackController.start(blackjackGameManager);
     }
 }
