ee3e3423b005d19e918d333a12b02cad7dc907eb
최용준 <yj9107vv@gmail.com>
2026-03-16 16:54:37 +0900

    feat: 배팅 금액을 BigDecimal로 변경 및 배팅 금액 잃음 구현 및 테스트 추가
    - int, long, double보다 정확한 소수 계산을 위한 BigDecimal 사용
    - 금액을 잃는 경우인 applyLoseAmount() 추가

--- src/main/java/domain/BettingAmount.java
+++ src/main/java/domain/BettingAmount.java
@@ -1,22 +1,28 @@
 package domain;
 
+import java.math.BigDecimal;
+
 public class BettingAmount {
 
-    private long amount;
+    private BigDecimal amount;
 
-    private BettingAmount(int amount) {
+    private BettingAmount(BigDecimal amount) {
         this.amount = amount;
     }
 
-    public static BettingAmount of(int amount) {
+    public static BettingAmount of(BigDecimal amount) {
         return new BettingAmount(amount);
     }
 
     public void applyBlackjackBonus() {
-        amount = (int) (amount * 1.5);
+        amount = amount.multiply(new BigDecimal("1.5"));
     }
 
-    public long value() {
+    public void applyLoseAmount() {
+        amount = amount.negate();
+    }
+
+    public BigDecimal value() {
         return amount;
     }
 }
--- src/test/java/domain/BettingAmountTest.java
+++ src/test/java/domain/BettingAmountTest.java
@@ -2,19 +2,32 @@ package domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BettingAmountTest {
 
+    private final BigDecimal amount = new BigDecimal("10000");
+
     @Test
     @DisplayName("배팅 금액 1.5배 증가")
     void bettingAmount_one_and_half_increase() {
-        BettingAmount bettingAmount = BettingAmount.of(10000);
+        BettingAmount bettingAmount = BettingAmount.of(amount);
 
         bettingAmount.applyBlackjackBonus();
 
-        assertThat(bettingAmount.value()).isEqualTo(15000);
+        assertThat(bettingAmount.value()).isEqualTo(new BigDecimal("15000"));
+    }
+
+    @Test
+    @DisplayName("배팅 금액 잃음 (현 배팅 금액에서 음수로)")
+    void bettingAmount_lose() {
+        BettingAmount bettingAmount = BettingAmount.of(amount);
+
+        bettingAmount.applyLoseAmount();
+
+        assertThat(bettingAmount.value()).isEqualTo(new BigDecimal(-10_000));
     }
 
 }
