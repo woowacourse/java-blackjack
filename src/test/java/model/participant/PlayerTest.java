72502b608865026ce8b0b6e874f02ab7a89d6977
armd482 <armd482@naver.com>
2026-03-16 14:39:35 +0900

    test: player 생성자 형식 변경 사항 반영 및 검증 전략 주입 테스트 추가

--- src/test/java/model/participant/PlayerTest.java
+++ src/test/java/model/participant/PlayerTest.java
@@ -1,23 +1,35 @@
 package model.participant;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import dto.status.PlayerName;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
     @Test
     public void 정상_작동() {
-        String name = "player1";
-        Player player = new Player(name);
+        Player player = new Player(new PlayerName("player1"), null);
 
-        assertThat(player.getCurrentHand().name()).isEqualTo(name);
+        assertThat(player.getCurrentHand().name()).isEqualTo("player");
     }
 
     @Test
     public void 동일한_플레이어_검증_정상_작동() {
-        Player player1 = new Player("player");
-        Player player2 = new Player("player");
+        Player player1 = new Player(new PlayerName("player"), null);
+        Player player2 = new Player(new PlayerName("player"), null);
 
         assertThat(player1.equals(player2)).isTrue();
+    }
+
+    @Test
+    public void 검증_전략_정상_작동() {
+        NameValidator nameValidator = (String name) -> {
+            if(name.equals("테스트")) {
+                throw new IllegalArgumentException("테스트 이름을 사용할 수 없습니다.");
+            }
+        };
+
+        assertThatThrownBy(() -> new Player(new PlayerName("테스트"), nameValidator)).isExactlyInstanceOf(IllegalArgumentException.class).hasMessage("테스트 이름을 사용할 수 없습니다.");
     }
 }
