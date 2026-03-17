f11f550e59d645837b3894efca5e0c66d0e26e48
toctoce <toctoce@naver.com>
2026-03-17 22:12:54 +0900

    feat(Players): 플레이어 리스트 일급컬렉션으로 구현

---
+++ src/main/java/domain/participant/Players.java
@@ -0,0 +1,21 @@
+package domain.participant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public record Players(List<Player> players) {
+
+    public Players() {
+        this(List.of());
+    }
+
+    public Players(List<Player> players) {
+        this.players = List.copyOf(players);
+    }
+
+    public Players addPlayer(Player player) {
+        List<Player> players = new ArrayList<>(this.players);
+        Players.this.players.add(player);
+        return new Players(players);
+    }
+}
