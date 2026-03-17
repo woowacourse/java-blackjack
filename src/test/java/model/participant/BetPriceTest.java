bb7a139763e0764026dec35647fcaf96e5601b88
armd482 <armd482@naver.com>
2026-03-17 17:12:43 +0900

    test: 배팅 금액 최댓값 제약 테스트 코드 추가

--- src/test/java/model/participant/BetPriceTest.java
+++ src/test/java/model/participant/BetPriceTest.java
@@ -16,8 +16,15 @@ public class BetPriceTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0, -10000})
-    public void 범위_밖_입력_예외(int price) {
-        assertThatThrownBy(() -> new BetPrice(price)).isExactlyInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessage.OUT_OF_RANGE_BET.getMessage());
+    @ValueSource(longs = {0, -10000})
+    public void 양의_정수가_아닐_때_예외(long price) {
+        assertThatThrownBy(() -> new BetPrice(price)).isExactlyInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessage.NEGATIVE_BET.getMessage());
+    }
+
+    @Test
+    public void 범위_밖의_양의_정수일_때_예외() {
+        assertThatThrownBy(
+                () -> new BetPrice(9_223_372_036_854_775_807L)).isExactlyInstanceOf(
+                IllegalArgumentException.class).hasMessage(ErrorMessage.OUT_OF_MAXIMUM_RANGE_BET.getMessage());
     }
 }
