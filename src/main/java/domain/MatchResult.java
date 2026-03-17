5918ed7cbfe8f74fd906a972cc9382deabd0c8fe
haechanmoon <moonhc.dev@gmail.com>
2026-03-17 14:33:52 +0900

    refactor: 요구사항 변경으로 인한 승패String 제거 및 미사용 DTO클래스 제거

--- src/main/java/domain/MatchResult.java
+++ src/main/java/domain/MatchResult.java
@@ -1,21 +1,15 @@
 package domain;
 
 public enum MatchResult {
-    BLACKJACK("블랙잭", 1.5),
-    WIN("승", 1.0),
-    LOSE("패", -1.0),
-    DRAW("무", 0.0);
+    BLACKJACK(1.5),
+    WIN(1.0),
+    LOSE(-1.0),
+    DRAW(0.0);
 
-    private final String name;
     private final double ratio;
 
-    MatchResult(String name, double ratio) {
-        this.name = name;
+    MatchResult(double ratio) {
         this.ratio = ratio;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public int calculateIncome(BettingMoney bettingMoney) {
