b57a07df78c3cc7f06945390b01ccc866d5655a6
yeo-li <parkseongyeol2110@gmail.com>
2026-03-17 19:42:30 +0900

    refactor(PlayerAction): from() 메서드 enum값 변경에 대응할 수 있도록 변경

--- src/main/java/constant/PlayerAction.java
+++ src/main/java/constant/PlayerAction.java
@@ -16,31 +16,17 @@ public enum PlayerAction {
         return action;
     }
 
-    public boolean isHit() {
-        return this == HIT;
-    }
-
     public boolean isStand() {
         return this == STAND;
     }
 
     public static PlayerAction from(String input) {
-        validate(input);
-        if (input.equals(HIT.getAction())) {
-            return PlayerAction.HIT;
-        }
-        return PlayerAction.STAND;
-    }
-
-    private static void validate(String input) {
-        validateYesNoActionInput(input);
-    }
-
-    private static void validateYesNoActionInput(String input) {
         String normalized = input.strip();
-        if (!normalized.equals(PlayerAction.HIT.getAction()) && !normalized
-            .equals(PlayerAction.STAND.getAction())) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_YES_NO_INPUT.getMessage());
+        for (PlayerAction playerAction : PlayerAction.values()) {
+            if (normalized.equals(playerAction.getAction())) {
+                return playerAction;
+            }
         }
+        throw new IllegalArgumentException(ErrorMessage.INVALID_YES_NO_INPUT.getMessage());
     }
 }
