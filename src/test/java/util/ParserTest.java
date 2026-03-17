385c7e36b298c8023466b5f14e513d3fb600a8f0
dahye <dahye9011@naver.com>
2026-03-17 13:09:16 +0900

    test: 베팅 금액 빈 입력 테스트 케이스 추가

--- src/test/java/util/ParserTest.java
+++ src/test/java/util/ParserTest.java
@@ -6,10 +6,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class ParserTest {
-    public static final String ERROR_PREFIX = "[ERROR] ";
-
     @Test
     @DisplayName("플레이어의 이름을 공백 제거 후, 쉼표(,)로 구분한다.")
     void 플레이어_이름_파싱_테스트() {
@@ -24,6 +24,19 @@ public class ParserTest {
         assertThat(parsedPlayersName).containsExactly("라이", "영기");
     }
 
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("베팅 금액이 비어 있는 경우, IllegalArgumentException이 발생한다.")
+    void 베팅_금액_빈_입력_테스트(String bettingMoney) {
+        // given
+        Parser parser = new Parser();
+
+        // when & then
+        assertThatThrownBy(() -> parser.parseBettingMoney(bettingMoney))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 배팅 금액은 숫자 외의 문자를 포함할 수 없습니다.");
+    }
+
     @Test
     @DisplayName("배팅 금액에 숫자 외의 문자가 포함되는 경우, IllegalArgumentException이 발생한다.")
     void 배팅_금액_숫자_외의_문자_예외_테스트() {
@@ -34,6 +47,6 @@ public class ParserTest {
         // when & then
         assertThatThrownBy(() -> parser.parseBettingMoney(bettingMoney))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageStartingWith(ERROR_PREFIX);
+                .hasMessage("[ERROR] 배팅 금액은 숫자 외의 문자를 포함할 수 없습니다.");
     }
 }
