2b16e95281f49d0388a684a2b585d53c37fe5d4f
zakie <realzzaekkii@gmail.com>
2026-03-18 02:09:43 +0900

    refactor: 변수를 2개 이하로 갖도록 수정

--- src/main/java/domain/participant/Participant.java
+++ src/main/java/domain/participant/Participant.java
@@ -2,48 +2,46 @@ package domain.participant;
 
 import domain.card.Card;
 import domain.card.CurrentHand;
-import domain.card.Hand;
 import java.util.List;
 
 public abstract class Participant {
-    private final Name name;
-    private final Hand hand;
 
-    public Participant(final Name name) {
-        this.name = name;
-        this.hand = new Hand();
+    private final ParticipantState state;
+
+    protected Participant(final Name name) {
+        this.state = new ParticipantState(name);
     }
 
 
     public void draw(final Card card) {
-        hand.addCard(card);
+        state.draw(card);
     }
 
 
     public abstract boolean isDrawable();
 
     public boolean isBust() {
-        return hand.isBust();
+        return state.isBust();
     }
 
     public boolean isBlackjack() {
-        return hand.isBlackjack();
+        return state.isBlackjack();
     }
 
 
     public int getScore() {
-        return hand.getScore();
+        return state.getScore();
     }
 
     public String getName() {
-        return name.name();
+        return state.getName();
     }
 
     public List<Card> getHand() {
-        return hand.getHand();
+        return state.getHand();
     }
 
     public CurrentHand getCurrentHand() {
-        return new CurrentHand(getName(), getHand());
+        return state.getCurrentHand();
     }
 }
---
+++ src/main/java/domain/participant/ParticipantState.java
@@ -0,0 +1,46 @@
+package domain.participant;
+
+import domain.card.Card;
+import domain.card.CurrentHand;
+import domain.card.Hand;
+import java.util.List;
+
+public class ParticipantState {
+    private final Name name;
+    private final Hand hand;
+
+    public ParticipantState(final Name name) {
+        this.name = name;
+        this.hand = new Hand();
+    }
+
+    public void draw(final Card card) {
+        hand.addCard(card);
+    }
+
+    
+    public boolean isBust() {
+        return hand.isBust();
+    }
+
+    public boolean isBlackjack() {
+        return hand.isBlackjack();
+    }
+
+
+    public int getScore() {
+        return hand.getScore();
+    }
+
+    public String getName() {
+        return name.name();
+    }
+
+    public List<Card> getHand() {
+        return hand.getHand();
+    }
+
+    public CurrentHand getCurrentHand() {
+        return new CurrentHand(getName(), getHand());
+    }
+}
