68c7fd24968c0844f3a2598910ed2e58208e36f6
frombunny <rucy011121@gmail.com>
2026-03-17 16:00:49 +0900

    refactor: 매직넘버를 상수로 추출

--- src/main/java/domain/game/ProfitCalculator.java
+++ src/main/java/domain/game/ProfitCalculator.java
@@ -6,12 +6,14 @@ import domain.participant.Player;
 import java.math.BigDecimal;
 
 public class ProfitCalculator {
+    public static final double BETTING_MULTIPLIER = 1.5;
+
     public static BigDecimal calculatePlayerProfit(Player player, Dealer dealer) {
         WinningStatus winningStatus = WinningStatus.of(player, dealer);
         BigDecimal betAmount = player.betAmount();
 
         if (player.isBlackjack() && winningStatus == WinningStatus.WIN) {
-            return betAmount.multiply(BigDecimal.valueOf(1.5));
+            return betAmount.multiply(BigDecimal.valueOf(BETTING_MULTIPLIER));
         }
 
         if (winningStatus == WinningStatus.WIN) {
