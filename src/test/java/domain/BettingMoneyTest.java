ea71a2525c143cc8863e33912668c686a1d0b2c7
심호경 <ghrudtla@naver.com>
2026-03-17 23:22:06 +0900

    refactor: 0을 기준으로 경계값 검증 테스트
    - parameterizedTest 적용
    - 블랙박스 테스트 경계값 분석 적용

--- src/test/java/domain/BettingMoneyTest.java
+++ src/test/java/domain/BettingMoneyTest.java
@@ -1,24 +1,25 @@
 package domain;
 
 import domain.participant.BettingMoney;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BettingMoneyTest {
 
-    @Test
-    void 베팅금액을_생성할_수_있다() {
-        BettingMoney bettingMoney = BettingMoney.of(1000);
-        assertThat(bettingMoney.getMoney()).isEqualTo(1000);
+    @ParameterizedTest(name = "금액이 {0}원 일 때, 배팅 머니가 생성된다")
+    @ValueSource(ints = {1, 2})
+    void 배팅금액이_0원_초과이면_배팅머니를_만들_수_있다(int validMoney) {
+        BettingMoney bettingMoney = BettingMoney.of(validMoney);
+        assertThat(bettingMoney.getMoney()).isEqualTo(validMoney);
     }
 
-    @Test
-    void 베팅금액이_0이하면_예외가_발생한다() {
-        assertThatThrownBy(() -> BettingMoney.of(0))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> BettingMoney.of(-1000))
+    @ParameterizedTest(name = "금액이 {0}원 일 때, 배팅 머니는 생성할 수 없다")
+    @ValueSource(ints = {0, -1})
+    void 배팅금액이_0이하면_예외가_발생한다(int invalidMoney) {
+        assertThatThrownBy(() -> BettingMoney.of(invalidMoney))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
