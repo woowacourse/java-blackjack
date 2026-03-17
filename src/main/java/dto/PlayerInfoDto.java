3b4cc9233835e63a1b06838c9d17888219d1d3da
choubung <choubung@sookmyung.ac.kr>
2026-03-17 19:33:20 +0900

    Refactor(dto): 기본 정보 dto와 결과 dto를 분리

--- src/main/java/dto/PlayerInfoDto.java
+++ src/main/java/dto/PlayerInfoDto.java
@@ -1,20 +1,14 @@
 package dto;
 
-import domain.participant.WinStatus;
-
 import java.util.List;
 
 public class PlayerInfoDto {
-    private String name;
-    private List<String> cards;
-    private int score;
-    private WinStatus winStatus;
+    private final String name;
+    private final List<String> cards;
 
-    public PlayerInfoDto(String name, List<String> cards, int score, WinStatus winStatus) {
+    public PlayerInfoDto(String name, List<String> cards) {
         this.name = name;
         this.cards = cards;
-        this.score = score;
-        this.winStatus = winStatus;
     }
 
     public String getName() {
@@ -23,13 +17,5 @@ public class PlayerInfoDto {
 
     public List<String> getCards() {
         return cards;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public WinStatus getWinStatus() {
-        return winStatus;
     }
 }
--- src/main/java/dto/PlayerResultDto.java
+++ src/main/java/dto/PlayerResultDto.java
@@ -1,4 +1,42 @@
 package dto;
 
+import domain.participant.WinStatus;
+import domain.vo.Money;
+
+import java.util.List;
+
 public class PlayerResultDto {
+    private final String name;
+    private final List<String> cards;
+    private final int score;
+    private final WinStatus winStatus;
+    private final Money profit;
+
+    public PlayerResultDto(String name, List<String> cards, int score, WinStatus winStatus, Money profit) {
+        this.name = name;
+        this.cards = cards;
+        this.score = score;
+        this.winStatus = winStatus;
+        this.profit = profit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getCards() {
+        return cards;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public WinStatus getWinStatus() {
+        return winStatus;
+    }
+
+    public Money getProfit() {
+        return profit;
+    }
 }
