ffd552025f1f4720c96e5027491e885d4b540bb2
dahye <dahye9011@naver.com>
2026-03-17 14:09:26 +0900

    test: User 메서드 테스트 케이스 추가

--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -2,21 +2,52 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import fixture.UserFixture;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import vo.Money;
 import vo.Rank;
 import vo.Suit;
 
 public class UserTest {
     @Test
-    @DisplayName("처음 받은 두 장의 카드 합이 21점인 경우, 블랙잭이다.")
-    void isBlackjack_True() {
+    @DisplayName("유저가 카드를 받는다. - receiveCard(Card card) + getCardsDisplay()")
+    void 유저_카드_받기_테스트() {
         // given
-        User user = new User("라이", new Money(10000));
+        User user = UserFixture.createDefaultUser();
+
+        // when
         user.receiveCard(new Card(Suit.SPADE, Rank.ACE));
         user.receiveCard(new Card(Suit.HEART, Rank.JACK));
+        String exceptedResult = "A스페이드, J하트";
+
+        // then
+        assertThat(user.getCardsDisplay()).isEqualTo(exceptedResult);
+    }
+
+    @Test
+    @DisplayName("유저가 가진 카드 점수를 계산한다. - calculateScore() + getTotalScore()")
+    void 유저_카드_점수_계산_테스트() {
+        // given
+        User user = UserFixture.createDefaultUser();
+        user.receiveCard(new Card(Suit.SPADE, Rank.ACE));
+        user.receiveCard(new Card(Suit.HEART, Rank.JACK));
+
+        // when
         user.calculateScore();
+
+        // then
+        assertThat(user.getTotalScore()).isEqualTo(21);
+    }
+
+    @Test
+    @DisplayName("처음 받은 두 장의 카드 합이 21점인 경우, 블랙잭이다.")
+    void 유저_카드_두_장_블랙잭_테스트() {
+        // given
+        User user = UserFixture.createUserWithCards(
+                List.of(new Card(Suit.SPADE, Rank.ACE),
+                        new Card(Suit.CLUB, Rank.JACK))
+        );
 
         // when & then
         assertThat(user.isBlackjack()).isTrue();
@@ -24,15 +55,29 @@ public class UserTest {
 
     @Test
     @DisplayName("카드가 3장 이상이고, 합이 21점인 경우, 블랙잭이 아니다.")
-    void isBlackjack_False_With3Cards() {
+    void 유저_카드_두_장_초과_블랙잭_아님_테스트() {
         // given
-        User user = new User("라이", new Money(10000));
-        user.receiveCard(new Card(Suit.SPADE, Rank.SEVEN));
-        user.receiveCard(new Card(Suit.HEART, Rank.SEVEN));
-        user.receiveCard(new Card(Suit.CLUB, Rank.SEVEN));
-        user.calculateScore();
+        User user = UserFixture.createUserWithCards(
+                List.of(new Card(Suit.SPADE, Rank.SEVEN),
+                        new Card(Suit.CLUB, Rank.SEVEN),
+                        new Card(Suit.HEART, Rank.SEVEN))
+        );
 
         // when & then
         assertThat(user.isBlackjack()).isFalse();
+    }
+
+    @Test
+    @DisplayName("카드 합이 21점 초과인 경우, 버스트다.")
+    void 유저_카드_버스트_테스트() {
+        // given
+        User user = UserFixture.createUserWithCards(
+                List.of(new Card(Suit.SPADE, Rank.SEVEN),
+                        new Card(Suit.CLUB, Rank.SEVEN),
+                        new Card(Suit.CLUB, Rank.JACK))
+        );
+
+        // when & then
+        assertThat(user.isBust()).isTrue();
     }
 }
