ae264465efe2d116df4358694e81bd8efdbe30e1
poketopa <lhs5427ll@gmail.com>
2026-03-17 18:00:11 +0900

    feat(view): 배팅금 입력 메서드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,6 +2,9 @@ package view;
 
 import domain.state.HitStand;
 import domain.participant.Player;
+import dto.domain.PlayerNameAndBettingDto;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import message.IOMessage;
 
@@ -23,6 +26,24 @@ public class InputView {
             }
 
             System.out.println(IOMessage.INVALID_HIT_OR_STAND_DECISION.message());
+        }
+    }
+
+    public List<PlayerNameAndBettingDto> getPlayerBetting(List<String> playerNames){
+        return playerNames.stream()
+                .map(this::readPlayerBetting)
+                .toList();
+    }
+
+    private PlayerNameAndBettingDto readPlayerBetting(String name) {
+        while (true) {
+            System.out.println(name + "의 배팅 금액은?");
+            final String input = scanner.nextLine().trim();
+            try {
+                return new PlayerNameAndBettingDto(name, Integer.parseInt(input));
+            } catch (NumberFormatException e) {
+                System.out.println("숫자만 입력해주세요.");
+            }
         }
     }
 }
