4dc228416ef2bc466e11e5b8b4baa3ff1c5453e7
zakie <realzzaekkii@gmail.com>
2026-03-17 13:09:16 +0900

    refactor: 베팅 단위 매직넘버 상수화

--- src/main/java/domain/participant/BetAmount.java
+++ src/main/java/domain/participant/BetAmount.java
@@ -6,16 +6,17 @@ public record BetAmount(
     public static final int MINIMUM_BET_AMOUNT = 0;
     public static final int MAXIMUM_BET_AMOUNT = 1_000_000;
 
+    private static final int BET_UNIT = 10;
     private static final String BET_AMOUNT_OUT_OF_RANGE =
             "베팅 금액은 " + MINIMUM_BET_AMOUNT + "원 이상 " + MAXIMUM_BET_AMOUNT + "원 이하여야 합니다.";
-    private static final String BET_AMOUNT_NOT_DIVIDED_BY_TEN = "베팅 금액은 10의 배수여야 합니다.";
+    private static final String INVALID_BET_UNIT = "베팅 금액은 " + BET_UNIT + "단위여야 합니다.";
 
     public BetAmount {
         validateBetAmountRange(betAmount);
-        validateBetAmountDividedByTen(betAmount);
+        validateBetUnit(betAmount);
     }
 
-    
+
     // TODO: 베팅 금액 검증 테스트 추가
     private void validateBetAmountRange(final int betAmount) {
         if (betAmount < MINIMUM_BET_AMOUNT || betAmount > MAXIMUM_BET_AMOUNT) {
@@ -23,9 +24,9 @@ public record BetAmount(
         }
     }
 
-    private void validateBetAmountDividedByTen(final int betAmount) {
-        if (betAmount % 10 != 0) {
-            throw new IllegalArgumentException(BET_AMOUNT_NOT_DIVIDED_BY_TEN);
+    private void validateBetUnit(final int betAmount) {
+        if (betAmount % BET_UNIT != 0) {
+            throw new IllegalArgumentException(INVALID_BET_UNIT);
         }
     }
 }
