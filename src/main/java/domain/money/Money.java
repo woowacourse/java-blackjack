f3e983f4672060611ee88adb8b18406adb483906
Huiyeongkim <huiyeong9619@naver.com>
2026-03-17 23:23:03 +0900

    refactor: 도메인과 inputView 따로 검증

--- src/main/java/domain/money/Money.java
+++ src/main/java/domain/money/Money.java
@@ -1,17 +1,21 @@
 package domain.money;
 
-import util.Validator;
-
 public class Money {
 
     private final Integer money;
 
     public Money(Integer money) {
-        Validator.validateMoney(money);
+        validateMoney(money);
         this.money = money;
     }
 
     public Integer getValue() {
         return money;
+    }
+
+    private void validateMoney(Integer money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 다시 입력해주세요.");
+        }
     }
 }
--- src/main/java/domain/participant/Player.java
+++ src/main/java/domain/participant/Player.java
@@ -1,16 +1,21 @@
 package domain.participant;
 
 import domain.money.Money;
-import util.Validator;
+
+import java.util.regex.Pattern;
+
+import static util.BlackJackConstant.MAX_NAME_LENGTH;
 
 public class Player extends Participant {
+
+    private static final String STRING_REGEX = "^[a-zA-Z]*$";
 
     private final String name;
     private final Money money;
 
     public Player(String name, Integer money) {
-        Validator.validateNameLength(name);
-        Validator.validateNameEng(name);
+        validateNameLength(name);
+        validateNameEng(name);
         this.name = name;
         this.money = new Money(money);
     }
@@ -21,5 +26,17 @@ public class Player extends Participant {
 
     public Money getMoney() {
         return money;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.isEmpty() || name.length() > MAX_NAME_LENGTH)  {
+            throw new IllegalArgumentException("잘못된 입력입니다. 다시 입력해주세요.");
+        }
+    }
+
+    private void validateNameEng(String name) {
+        if (!Pattern.matches(STRING_REGEX, name)) {
+            throw new IllegalArgumentException("플레이어 이름은 영문자만 포함되어야 합니다.");
+        }
     }
 }
