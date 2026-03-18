91f7b5f8634daeda9f094df321c65a51019a6902
poketopa <lhs5427ll@gmail.com>
2026-03-18 12:38:51 +0900

    feat(controller): 결과 출력 메서드 및 dto 변환 기능 추가

--- src/main/java/controller/BlackJackController.java
+++ src/main/java/controller/BlackJackController.java
@@ -1,14 +1,26 @@
 package controller;
 
+import domain.card.Card;
 import domain.game.BlackJackGame;
 import domain.card.Cards;
 import domain.participant.Dealer;
 import domain.game.GameResult;
 import domain.participant.Player;
 import domain.participant.Players;
+import domain.state.Outcome;
 import dto.domain.PlayerNameAndBettingDto;
+import dto.view.DealerStatDto;
+import dto.view.ParticipantProfitDto;
+import dto.view.ParticipantResultDto;
+import dto.view.ParticipantStatsDto;
+import dto.view.PlayerCardsDto;
+import dto.view.PlayerOutcomeDto;
+import dto.view.PlayerProfitDto;
+import dto.view.ResultDto;
+import dto.view.StartBlackJackDto;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import util.NameParser;
 import view.InputView;
 import view.ResultView;
@@ -30,7 +42,7 @@ public class BlackJackController {
         final Dealer dealer = game.dealer();
         final Cards cards = game.cards();
 
-        resultView.printGameStartMessage(players, dealer);
+        resultView.printGameStartMessage(toStartBlackJackDto(players, dealer));
         drawPlayersTurn(players, cards);
         drawDealerTurn(dealer, cards);
         printResult(game);
@@ -74,19 +86,93 @@ public class BlackJackController {
     private void printResult(BlackJackGame game) {
         final Players players = game.players();
         final Dealer dealer = game.dealer();
+        final GameResult gameResult = game.calculateResult();
         printGameResult(players, dealer);
-        printWinnerResult(game);
+        resultView.printWinner(toParticipantStatsDto(players, gameResult));
+        resultView.printFinalProfit(toParticipantProfitDto(players));
     }
 
     private void printGameResult(Players players, Dealer dealer) {
-        resultView.printResult(players, dealer);
+        resultView.printResult(toResultDto(players, dealer));
         if (dealer.checkBust()) {
             resultView.printDealerBust();
         }
     }
 
-    private void printWinnerResult(BlackJackGame game) {
-        final GameResult gameResult = game.calculateResult();
-        resultView.printWinner(game.players(), gameResult);
+    private StartBlackJackDto toStartBlackJackDto(Players players, Dealer dealer) {
+        return new StartBlackJackDto(
+                formatCardName(dealer.getCardList().get(0)),
+                toPlayerCardsDtos(players)
+        );
+    }
+
+    private List<PlayerCardsDto> toPlayerCardsDtos(Players players) {
+        final List<PlayerCardsDto> playerCardsDtos = new ArrayList<>();
+        players.forEachPlayer(player -> playerCardsDtos.add(new PlayerCardsDto(
+                player.getName(),
+                toCardNames(player.getCardList())
+        )));
+        return playerCardsDtos;
+    }
+
+    private ResultDto toResultDto(Players players, Dealer dealer) {
+        final ParticipantResultDto dealerResult = new ParticipantResultDto(
+                "딜러",
+                resultView.joinCardNames(dealer.getCardList()),
+                dealer.getScore()
+        );
+        final List<ParticipantResultDto> playerResults = new ArrayList<>();
+        players.forEachPlayer(player -> playerResults.add(new ParticipantResultDto(
+                player.getName(),
+                resultView.joinCardNames(player.getCardList()),
+                player.getScore()
+        )));
+        return new ResultDto(dealerResult, playerResults);
+    }
+
+    private ParticipantStatsDto toParticipantStatsDto(Players players, GameResult gameResult) {
+        return new ParticipantStatsDto(
+                toDealerStatDto(gameResult),
+                toPlayerOutcomeDtos(players, gameResult)
+        );
+    }
+
+    private DealerStatDto toDealerStatDto(GameResult gameResult) {
+        final int dealerWinCount = gameResult.getDealerCount(Outcome.DEFAULT_WIN)
+                + gameResult.getDealerCount(Outcome.BLACKJACK_WIN);
+        final int dealerLoseCount = gameResult.getDealerCount(Outcome.LOSE);
+        final int dealerDrawCount = gameResult.getDealerCount(Outcome.DRAW);
+        return new DealerStatDto(dealerWinCount, dealerLoseCount, dealerDrawCount);
+    }
+
+    private List<PlayerOutcomeDto> toPlayerOutcomeDtos(Players players, GameResult gameResult) {
+        final List<PlayerOutcomeDto> playerOutcomeDtos = new ArrayList<>();
+        players.forEachPlayer(player -> playerOutcomeDtos.add(new PlayerOutcomeDto(
+                player.getName(),
+                gameResult.getPlayerOutcome(player.getName()).result()
+        )));
+        return playerOutcomeDtos;
+    }
+
+    private ParticipantProfitDto toParticipantProfitDto(Players players) {
+        final List<PlayerProfitDto> playerProfitDtos = new ArrayList<>();
+        players.forEachPlayer(player -> playerProfitDtos.add(new PlayerProfitDto(
+                player.getName(),
+                player.getBalance()
+        )));
+        final int dealerProfit = -playerProfitDtos.stream()
+                .mapToInt(PlayerProfitDto::profit)
+                .sum();
+        return new ParticipantProfitDto(dealerProfit, playerProfitDtos);
+    }
+
+    private List<String> toCardNames(List<Card> cards) {
+        return cards.stream()
+                .map(this::formatCardName)
+                .collect(Collectors.toList());
+    }
+
+    private String formatCardName(Card card) {
+        return card.getRank().symbol() + card.getSuit().suit();
     }
 }
