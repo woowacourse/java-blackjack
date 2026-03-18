52ba0a3a86e1e1acbbf2c3a9bef8fdba424e6b7c
이동훈 <higgs95@naver.com>
2026-03-18 16:04:58 +0900

    test: 배당에 따른 수익 계산 테스트 추가

--- src/test/java/domain/ResultTest.java
+++ src/test/java/domain/ResultTest.java
@@ -1,3 +1,4 @@
+
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,19 +22,54 @@ class ResultTest {
         playerResults.put(jason, MatchResult.WIN);
 
         Result result = new Result(dealer, playerResults);
+
         assertThat(result.getDealerProfit()).isEqualTo(-10000);
     }
 
     @Test
-    @DisplayName("플레이어 수익은 배팅 금액에 배당률을 곱한 값이다.")
-    void getPlayerProfit() {
+    @DisplayName("플레이어가 블랙잭이면 배팅 금액의 1.5배를 얻는다.")
+    void getPlayerProfit_blackjack() {
+        Player player = createPlayer(110);
+        Result result = createResult(player, MatchResult.BLACKJACK);
+
+        assertThat(result.getPlayerProfit(player)).isEqualTo(165);
+    }
+
+    @Test
+    @DisplayName("플레이어가 승리하면 배팅 금액만큼 얻는다.")
+    void getPlayerProfit_win() {
+        Player player = createPlayer(110);
+        Result result = createResult(player, MatchResult.WIN);
+
+        assertThat(result.getPlayerProfit(player)).isEqualTo(110);
+    }
+
+    @Test
+    @DisplayName("플레이어가 패배하면 배팅 금액만큼 잃는다.")
+    void getPlayerProfit_lose() {
+        Player player = createPlayer(110);
+        Result result = createResult(player, MatchResult.LOSE);
+
+        assertThat(result.getPlayerProfit(player)).isEqualTo(-110);
+    }
+
+    @Test
+    @DisplayName("플레이어가 무승부면 수익은 0이다.")
+    void getPlayerProfit_draw() {
+        Player player = createPlayer(110);
+        Result result = createResult(player, MatchResult.DRAW);
+
+        assertThat(result.getPlayerProfit(player)).isEqualTo(0);
+    }
+
+    private Result createResult(Player player, MatchResult matchResult) {
         Dealer dealer = Dealer.create();
-        Player pobi = Player.from("pobi", new Money(10000));
-
         Map<Player, MatchResult> playerResults = new LinkedHashMap<>();
-        playerResults.put(pobi, MatchResult.BLACKJACK);
+        playerResults.put(player, matchResult);
+        return new Result(dealer, playerResults);
+    }
 
-        Result result = new Result(dealer, playerResults);
-        assertThat(result.getPlayerProfit(pobi)).isEqualTo(15000);
+    private Player createPlayer(int bettingMoney) {
+        return Player.from("pobi", new Money(bettingMoney));
     }
 }
