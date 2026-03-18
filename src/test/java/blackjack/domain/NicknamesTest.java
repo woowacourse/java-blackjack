90d2a34d18a1910594c20ada7e6f30f93bef22cb
Sumin <new.sumyang@gmail.com>
2026-03-18 16:18:54 +0900

    test: 입력된 닉네임이 없을 경우 검증 테스트 추가

--- src/test/java/blackjack/domain/NicknamesTest.java
+++ src/test/java/blackjack/domain/NicknamesTest.java
@@ -9,6 +9,18 @@ import org.junit.jupiter.api.Test;
 class NicknamesTest {
 
     @Test
+    @DisplayName("플레이어 이름이 없으면 예외가 발생한다.")
+    void emptyNicknameException() {
+        // given
+        List<String> nicknames = List.of();
+
+        // when & then
+        assertThatThrownBy(() -> Nicknames.from(nicknames))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("플레이어 이름은 1명 이상 입력해야 합니다.");
+    }
+
+    @Test
     @DisplayName("중복된 닉네임이 존재하면 예외가 발생한다.")
     void duplicateNicknameException() {
         // given
