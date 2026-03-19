044c14aa26b18f4bc874138335f12e6e33e8e6ec
poketopa <lhs5427ll@gmail.com>
2026-03-19 11:55:47 +0900

    feat(test): 플레이어가 BLACKJACK이 아닌 상태에서 점수가 21점일 때 드로우 종료 테스트 추가

--- src/test/java/domain/participant/PlayerTest.java
+++ src/test/java/domain/participant/PlayerTest.java
@@ -1,8 +1,13 @@
 package domain.participant;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import domain.card.Card;
 import domain.card.Cards;
+import domain.card.Rank;
+import domain.card.Suit;
 import java.util.Random;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,5 +22,25 @@ public class PlayerTest {
         player.drawCard(cards);
 
         assertEquals(1, player.getCardList().size());
+    }
+
+    @Test
+    @DisplayName("플레이어 점수가 21점이면 더 이상 HIT 할 수 없다")
+    void cannotHitWhenScoreIsTwentyOne() {
+        final Player player = new Player("pobi", 1000);
+        player.addCardForTest(new Card(Suit.SPADE, Rank.KING));
+        player.addCardForTest(new Card(Suit.HEART, Rank.ACE));
+
+        assertFalse(player.canHit());
+    }
+
+    @Test
+    @DisplayName("플레이어 점수가 21점 미만이면 HIT 할 수 있다")
+    void canHitWhenScoreIsLessThanTwentyOne() {
+        final Player player = new Player("pobi", 1000);
+        player.addCardForTest(new Card(Suit.SPADE, Rank.TEN));
+        player.addCardForTest(new Card(Suit.HEART, Rank.NINE));
+
+        assertTrue(player.canHit());
     }
 }
