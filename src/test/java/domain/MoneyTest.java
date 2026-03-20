cae9a352a3aa5ce047cb7be38bc27695db409d27
Jihyun3478 <leejihyun3478@gmail.com>
2026-03-20 11:33:05 +0900

    test: 통과하지 않는 테스트 수정

--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -1,5 +1,8 @@
 package domain;
 
+import static domain.Money.MAXIMUM_BET_AMOUNT;
+import static domain.Money.MINIMUM_BET_AMOUNT;
+import static domain.Money.MONEY_DIVIDE_UNIT;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import exception.ErrorMessage;
@@ -12,7 +15,7 @@ public class MoneyTest {
     void moneyIsOutOfRangeTest() {
         assertThatThrownBy(() -> new Money(0))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.MONEY_INVALID_RANGE.getMessage());
+                .hasMessageContaining(ErrorMessage.MONEY_INVALID_RANGE.getMessage(MINIMUM_BET_AMOUNT, MAXIMUM_BET_AMOUNT));
     }
 
     @Test
@@ -20,6 +23,6 @@ public class MoneyTest {
     void moneyUnitIsNotTenTest() {
         assertThatThrownBy(() -> new Money(10005))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.MONEY_INVALID_UNIT.getMessage());
+                .hasMessageContaining(ErrorMessage.MONEY_INVALID_UNIT.getMessage(MONEY_DIVIDE_UNIT));
     }
 }
--- src/test/java/domain/participant/PlayersTest.java
+++ src/test/java/domain/participant/PlayersTest.java
@@ -1,5 +1,7 @@
 package domain.participant;
 
+import static domain.participant.Players.MAXIMUM_PLAYER_COUNT;
+import static domain.participant.Players.MINIMUM_PLAYER_COUNT;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import domain.Money;
@@ -32,8 +34,23 @@ class PlayersTest {
     @Test
     @DisplayName("플레이어의 수가 1명 이상 8명 이하가 아닐 경우, 예외가 발생한다.")
     void playersCountIsOutOfRangeTest() {
-        assertThatThrownBy(() -> new Players(null))
+        List<Card> cardsList = List.of(
+                new Card(CardScore.EIGHT, CardSuit.CLUB),
+                new Card(CardScore.FOUR, CardSuit.CLUB)
+        );
+        Cards cards = new Cards(cardsList);
+        Player player1 = new Player(cards, "pobi", new Money(10000));
+        Player player2 = new Player(cards, "woni", new Money(10000));
+        Player player3 = new Player(cards, "stark", new Money(10000));
+        Player player4 = new Player(cards, "downer", new Money(10000));
+        Player player5 = new Player(cards, "chorok", new Money(10000));
+        Player player6 = new Player(cards, "logi", new Money(10000));
+        Player player7 = new Player(cards, "neo", new Money(10000));
+        Player player8 = new Player(cards, "brown", new Money(10000));
+        Player player9 = new Player(cards, "lisa", new Money(10000));
+
+        assertThatThrownBy(() -> new Players(List.of(player1, player2, player3, player4, player5, player6, player7, player8, player9)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.PLAYERS_INVALID_COUNT.getMessage());
+                .hasMessage(ErrorMessage.PLAYERS_INVALID_COUNT.getMessage(MINIMUM_PLAYER_COUNT, MAXIMUM_PLAYER_COUNT));
     }
 }
