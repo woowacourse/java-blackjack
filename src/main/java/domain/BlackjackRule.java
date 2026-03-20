56c24f2a808166d0f11966bb46906bc83ecb5dbd
dahye <dahye9011@naver.com>
2026-03-20 09:34:01 +0900

    refactor: 에이스 상수 숫자로 수정

--- src/main/java/domain/BlackjackRule.java
+++ src/main/java/domain/BlackjackRule.java
@@ -5,9 +5,7 @@ public class BlackjackRule {
     public static final int MAX_PARTICIPANTS_COUNT = 16;
     public static final int DEALER_MAX_HIT_SCORE = 16;
     public static final int BLACKJACK_CARD_COUNT = 2;
-    private static final int ACE_MAX_SCORE = 11;
-    private static final int ACE_MIN_SCORE = 1;
-    public static final int ACE_SCORE_DIFFERENCE = ACE_MAX_SCORE - ACE_MIN_SCORE;
+    public static final int ACE_SCORE_DIFFERENCE = 10;
 
     private BlackjackRule() {}
 }
