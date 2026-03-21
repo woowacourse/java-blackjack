112984c68af49d33cfe654f161eb477e03285eb3
Yeji Kim <yejikim.erica@gmail.com>
2026-03-21 15:56:19 +0900

    test: Name의 객체 생성 시나리오 테스트를 위해 파라미터 주입 어노테이션 추가
    NameTest에서 Name 생성자 파라미터에 Null 또는 빈 문자열이 입력받아 들어온 경우 오류가 정상적으로 발생하는지 확인할
    수 있도록 어노테이션을 추가함

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class NameTest {
@@ -21,7 +22,8 @@ class NameTest {
 
     @ParameterizedTest
     @DisplayName("이름이 공백이거나 공백이나 특수문자가 포함된 경우 오류가 발생한다.")
-    @ValueSource(strings = {"", " ", "공 백문자포함", "특수문자포함!"})
+    @NullAndEmptySource
+    @ValueSource(strings = {"", " ", "공 백문자포함", "특수문자포함!", "\n"})
     void shouldThrowExceptionForInvalidName(String name) {
         // when & then
         assertThatThrownBy(
