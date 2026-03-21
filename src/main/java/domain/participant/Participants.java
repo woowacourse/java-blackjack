3e76a314a1863a189dbbc25fda54e4973b2e51aa
khcho96 <khcho1492@gmail.com>
2026-03-21 12:07:11 +0900

    feat(Participants): 플레이어의 이름 목록과 베팅금액 목록의 크기 검증 로직 추가

--- src/main/java/domain/participant/Participants.java
+++ src/main/java/domain/participant/Participants.java
@@ -1,15 +1,25 @@
 package domain.participant;
 
 import domain.card.Card;
+import exception.BlackjackException;
 import java.util.List;
 import java.util.function.Supplier;
 
 public record Participants(Dealer dealer, Players players) {
 
+    public static final String DIFFERENT_SIZE_PLAYER_BET_AMOUNT = "플레이어 목록과 베팅금액 목록의 크기가 다릅니다.";
+
     public static Participants of(List<PlayerName> playerNames, List<BetAmount> betAmounts) {
+        validateSize(playerNames, betAmounts);
         Dealer dealer = new Dealer();
         Players players = new Players(playerNames, betAmounts);
         return new Participants(dealer, players);
+    }
+
+    private static void validateSize(List<PlayerName> playerNames, List<BetAmount> betAmounts) {
+        if (playerNames.size() != betAmounts.size()) {
+            throw new BlackjackException(DIFFERENT_SIZE_PLAYER_BET_AMOUNT);
+        }
     }
 
     public boolean dealerShouldHit() {
