631a774073178c138f26bcc98b599d38720f069e
khcho96 <khcho1492@gmail.com>
2026-03-21 20:20:44 +0900

    refactor(BlackjackGame): dto 삭제로 인한 메서드 반환값을 도메인으로 변경

--- src/main/java/domain/game/BlackjackGame.java
+++ src/main/java/domain/game/BlackjackGame.java
@@ -2,12 +2,9 @@ package domain.game;
 
 import domain.card.Card;
 import domain.card.CardMachine;
+import domain.participant.Dealer;
 import domain.participant.Participants;
 import domain.participant.Player;
-import dto.BlackjackResultDto;
-import dto.BlackjackStatisticsDto;
-import dto.ParticipantDto;
-import dto.PlayerStatisticDto;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,43 +37,33 @@ public class BlackjackGame {
         return cardMachine.drawCard();
     }
 
-    public ParticipantDto updatePlayer(String name) {
-        Player player = participants.drawCardsByPlayer(name, this::drawCard);
-        return ParticipantDto.from(player);
+    public Player updatePlayer(String name) {
+        return participants.drawCardsByPlayer(name, this::drawCard);
     }
 
-    public ParticipantDto generateInitialDealerDto() {
-        return ParticipantDto.from(participants.dealer(), true);
+    public Dealer getDealer() {
+        return participants.dealer();
     }
 
-    public ParticipantDto generateDealerDto() {
-        return ParticipantDto.from(participants.dealer());
+    public List<Player> getPlayers() {
+        return participants.players().getPlayers();
     }
 
-    public List<ParticipantDto> generatePlayerDtoList() {
-        return participants.players().getPlayers().stream()
-                .map(ParticipantDto::from)
-                .toList();
-    }
-
-    public BlackjackResultDto getBlackjackResult() {
-        ParticipantDto dealerResultDto = generateDealerDto();
-        List<ParticipantDto> playerResultDtoList = generatePlayerDtoList();
-
-        return BlackjackResultDto.of(dealerResultDto, playerResultDtoList);
-    }
-
-    public BlackjackStatisticsDto getBlackjackStatistics() {
-        List<PlayerStatisticDto> playerStatisticDtoList = new ArrayList<>();
+    public BlackjackStatistics calculateStatistics() {
+        List<PlayerProfit> playerProfits = new ArrayList<>();
         int dealerProfit = 0;
         for (Player player : participants.players().getPlayers()) {
-            Result result = blackjackJudge.judgePlayerResult(participants.dealer(), player);
-            int playerProfit = result.getProfit(player.getBetAmount());
-            dealerProfit += playerProfit * -1;
-            playerStatisticDtoList.add(PlayerStatisticDto.of(player, playerProfit));
+            PlayerResult playerResult = blackjackJudge.judgePlayerResult(participants.dealer(), player);
+            int playerProfit = playerResult.getProfit(player.getBetAmount());
+            dealerProfit += getDealerProfitFrom(playerProfit);
+            playerProfits.add(new PlayerProfit(player.getName(), playerProfit));
         }
 
-        return BlackjackStatisticsDto.of(dealerProfit, playerStatisticDtoList);
+        return new BlackjackStatistics(dealerProfit, playerProfits);
+    }
+
+    private static int getDealerProfitFrom(int playerProfit) {
+        return -playerProfit;
     }
 
     public boolean isPlayerBust(String name) {
