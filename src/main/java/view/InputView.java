2cac55dfa58a18857ea019e2c3f9d0cee24da510
khcho96 <khcho1492@gmail.com>
2026-03-22 21:10:31 +0900

    feat(InputView): 배팅 금액 입력 범위 안내 문구 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
 
     private static final String INPUT_PLAYERS_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String INPUT_HIT_OR_STAND = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
-    private static final String INPUT_BET_AMOUNT = "%s의 배팅 금액은?\n";
+    private static final String INPUT_BET_AMOUNT = "%s의 배팅 금액은? (1,000 ~ 300,000,000)\n";
 
     private final Scanner scanner = new Scanner(System.in);
 
